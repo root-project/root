d4450c9353b502d4b9748ed976d27ea1d92d657c
Fons Rademakers <Fons.Rademakers@cern.ch>
2000-05-29 10:42:34 +0000

    increased MAXDIRS to 256
    git-svn-id: http://root.cern.ch/svn/root/trunk@48 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/rmkdepend/def.h
+++ build/rmkdepend/def.h
@@ -43,7 +43,7 @@ in this Software without prior written authorization from the X Consortium.
 
 #define MAXDEFINES	512
 #define MAXFILES	4096
-#define MAXDIRS		64
+#define MAXDIRS		256
 #define SYMTABINC	10	/* must be > 1 for define() to work right */
 #define	TRUE		1
 #define	FALSE		0
