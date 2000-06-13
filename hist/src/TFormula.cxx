fdc578acfebba7da37dac5d7f0993afe92853ed0
Rene Brun <Rene.Brun@cern.ch>
2000-06-13 10:39:20 +0000

    - Several changes in TFormula.cxx to use Double_t instead of Float_t.
    git-svn-id: http://root.cern.ch/svn/root/trunk@138 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/src/TFormula.cxx
+++ hist/src/TFormula.cxx
@@ -1,4 +1,4 @@
-// @(#)root/hist:$Name:  $:$Id: TFormula.cxx,v 1.2 2000/05/18 17:10:08 brun Exp $
+// @(#)root/hist:$Name:  $:$Id: TFormula.cxx,v 1.3 2000/05/26 06:43:01 brun Exp $
 // Author: Nicolas Brun   19/08/95
 
 /*************************************************************************
@@ -1712,13 +1712,13 @@ Double_t TFormula::EvalPar(Double_t *x, Double_t *params)
           pos++;
           inter=action/100-20;
           int1=action-inter*100;
-          intermede2=Float_t((x[inter]-fParams[int1-2000])/fParams[int1-1999]);
+          intermede2=Double_t((x[inter]-fParams[int1-2000])/fParams[int1-1999]);
           tab[pos-1] = fParams[int1-2001]*TMath::Exp(-0.5*intermede2*intermede2);
 //*-*- xygaus
     } else if (action > 2500 && action < 2600) {
           pos++;
-          intermede1=Float_t((x[0]-fParams[action-2500])/fParams[action-2499]);
-          intermede2=Float_t((x[1]-fParams[action-2498])/fParams[action-2497]);
+          intermede1=Double_t((x[0]-fParams[action-2500])/fParams[action-2499]);
+          intermede2=Double_t((x[1]-fParams[action-2498])/fParams[action-2497]);
           tab[pos-1] = fParams[action-2501]*TMath::Exp(-0.5*(intermede1*intermede1+intermede2*intermede2));
 //*-*- landau, xlandau, ylandau or zlandau
     } else if (action > 4000 && action < 4500) {
