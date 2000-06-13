b8275e5a78531814eb6ec4bb665bd28b3583f710
Rene Brun <Rene.Brun@cern.ch>
2000-06-13 12:28:49 +0000

    - Mods in TView    All members changed from Float_t to Double_t    new TView constructor with arguments in double precision.    All member function parameters changed from Float_t to Double_t.    New functions:    virtual void  NDCtoWC(Double_t *pn, Double_t *pw);    virtual void  NormalWCtoNDC(Double_t *pw, Double_t *pn);    virtual void  WCtoNDC(Double_t *pw, Double_t *pn);
    git-svn-id: http://root.cern.ch/svn/root/trunk@181 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/TView.h
+++ base/inc/TView.h
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name$:$Id$
+// @(#)root/base:$Name:  $:$Id: TView.h,v 1.1.1.1 2000/05/16 17:00:39 rdm Exp $
 // Author: Rene Brun, Nenad Buncic, Evgueni Tcherniaev, Olivier Couet   18/08/95
 
 /*************************************************************************
@@ -36,71 +36,76 @@ class TView : public TObject, public TAttLine {
 
 
 protected:
+        Double_t        fLatitude;         //View angle latitude
+        Double_t        fLongitude;        //View angle longitude
+        Double_t        fPsi;              //View angle psi
+        Double_t        fTN[12];           //
+        Double_t        fTB[12];           //
+        Double_t        fRmax[3];          //Upper limits of object
+        Double_t        fRmin[3];          //Lower limits of object
+        Double_t        fTnorm[12];        //Transformation matrix
+        Double_t        fTback[12];        //Back transformation matrix
+        Double_t        fX1[3];            //First coordinate of X axis
+        Double_t        fX2[3];            //Second coordinate of X axis
+        Double_t        fY1[3];            //First coordinate of Y axis
+        Double_t        fY2[3];            //Second coordinate of Y axis
+        Double_t        fZ1[3];            //First coordinate of Z axis
+        Double_t        fZ2[3];            //Second coordinate of Z axis
         Int_t           fSystem;           //Coordinate system
-        Float_t         fLatitude;         //View angle latitude
-        Float_t         fLongitude;        //View angle longitude
-        Float_t         fPsi;              //View angle psi
-        Float_t         fTN[12];           //
-        Float_t         fTB[12];           //
-        Float_t         fRmax[3];          //Upper limits of object
-        Float_t         fRmin[3];          //Lower limits of object
-        Float_t         fTnorm[12];        //Transformation matrix
-        Float_t         fTback[12];        //Back transformation matrix
-        Float_t         fX1[3];            //First coordinate of X axis
-        Float_t         fX2[3];            //Second coordinate of X axis
-        Float_t         fY1[3];            //First coordinate of Y axis
-        Float_t         fY2[3];            //Second coordinate of Y axis
-        Float_t         fZ1[3];            //First coordinate of Z axis
-        Float_t         fZ2[3];            //Second coordinate of Z axis
         TSeqCollection *fOutline;          //Collection of outline's objects
         Bool_t          fDefaultOutline;   //Set to TRUE if outline is default cube
         Bool_t          fAutoRange;        //Set to TRUE if range computed automatically
-        void            ResetView(Float_t longitude, Float_t latitude, Float_t psi, Int_t &irep);
+        void            ResetView(Double_t longitude, Double_t latitude, Double_t psi, Int_t &irep);
 
 
 public:
                 TView();
                 TView(Int_t system);
                 TView(Float_t *rmin, Float_t *rmax, Int_t system = 1);
+                TView(Double_t *rmin, Double_t *rmax, Int_t system = 1);
 virtual         ~TView();
-   virtual void AxisVertex(Float_t ang, Float_t *av, Int_t &ix1, Int_t &ix2, Int_t &iy1, Int_t &iy2, Int_t &iz1, Int_t &iz2);
-   virtual void DefineViewDirection(Float_t *s, Float_t *c,
+   virtual void AxisVertex(Double_t ang, Double_t *av, Int_t &ix1, Int_t &ix2, Int_t &iy1, Int_t &iy2, Int_t &iz1, Int_t &iz2);
+   virtual void DefineViewDirection(Double_t *s, Double_t *c,
                                     Double_t cosphi, Double_t sinphi,
                                     Double_t costhe, Double_t sinthe,
                                     Double_t cospsi, Double_t sinpsi,
-                                    Float_t *tnorm,  Float_t *tback);
+                                    Double_t *tnorm, Double_t *tback);
    virtual void  ExecuteEvent(Int_t event, Int_t px, Int_t py);
    virtual void  ExecuteRotateView(Int_t event, Int_t px, Int_t py);
-   virtual void  FindScope(Float_t *scale, Float_t *center, Int_t &irep);
-   virtual Int_t GetDistancetoAxis(Int_t axis, Int_t px, Int_t py, Float_t &ratio);
+   virtual void  FindScope(Double_t *scale, Double_t *center, Int_t &irep);
+   virtual Int_t GetDistancetoAxis(Int_t axis, Int_t px, Int_t py, Double_t &ratio);
 Bool_t           GetAutoRange() {return fAutoRange;}
-Float_t          GetLatitude() {return fLatitude;}
-Float_t          GetLongitude() {return fLongitude;}
-Float_t          GetPsi() {return fPsi;}
+Double_t         GetLatitude() {return fLatitude;}
+Double_t         GetLongitude() {return fLongitude;}
+Double_t         GetPsi() {return fPsi;}
    virtual void  GetRange (Float_t *min, Float_t *max);
-Float_t         *GetRmax() {return fRmax;}
-Float_t         *GetRmin() {return fRmin;}
+   virtual void  GetRange (Double_t *min, Double_t *max);
+Double_t        *GetRmax() {return fRmax;}
+Double_t        *GetRmin() {return fRmin;}
 TSeqCollection  *GetOutline() {return fOutline; }
-Float_t         *GetTN() {return fTN;}
-Float_t         *GetTnorm() {return fTnorm;}
+Double_t        *GetTN() {return fTN;}
+Double_t        *GetTnorm() {return fTnorm;}
   Int_t          GetSystem() {return fSystem;}
-   virtual void  FindNormal(Float_t x, Float_t  y, Float_t z, Float_t &zn);
-   virtual void  FindPhiSectors(Int_t iopt, Int_t &kphi, Float_t *aphi, Int_t &iphi1, Int_t &iphi2);
-   virtual void  FindThetaSectors(Int_t iopt, Float_t phi, Int_t &kth, Float_t *ath, Int_t &ith1, Int_t &ith2);
+   virtual void  FindNormal(Double_t x, Double_t  y, Double_t z, Double_t &zn);
+   virtual void  FindPhiSectors(Int_t iopt, Int_t &kphi, Double_t *aphi, Int_t &iphi1, Int_t &iphi2);
+   virtual void  FindThetaSectors(Int_t iopt, Double_t phi, Int_t &kth, Double_t *ath, Int_t &ith1, Int_t &ith2);
    virtual void  NDCtoWC(Float_t *pn, Float_t *pw);
+   virtual void  NDCtoWC(Double_t *pn, Double_t *pw);
    virtual void  NormalWCtoNDC(Float_t *pw, Float_t *pn);
-   virtual void  PadRange(Float_t rback);
+   virtual void  NormalWCtoNDC(Double_t *pw, Double_t *pn);
+   virtual void  PadRange(Double_t rback);
    virtual void  SetAutoRange(Bool_t autorange=kTRUE) {fAutoRange=autorange;}
-   virtual void  SetAxisNDC(Float_t *x1, Float_t *x2, Float_t *y1, Float_t *y2, Float_t *z1, Float_t *z2);
-   void          SetLatitude(Float_t latitude) {fLatitude = latitude;}
-   void          SetLongitude(Float_t longitude) {fLongitude = longitude;}
-   void          SetPsi(Float_t psi) {fPsi = psi;}
+   virtual void  SetAxisNDC(Double_t *x1, Double_t *x2, Double_t *y1, Double_t *y2, Double_t *z1, Double_t *z2);
+   void          SetLatitude(Double_t latitude) {fLatitude = latitude;}
+   void          SetLongitude(Double_t longitude) {fLongitude = longitude;}
+   void          SetPsi(Double_t psi) {fPsi = psi;}
    virtual void  SetOutlineToCube();
-   virtual void  SetRange(Float_t *min, Float_t *max);
-   virtual void  SetRange(Float_t x0, Float_t y0, Float_t z0, Float_t x1, Float_t y1, Float_t z1, Int_t flag=0);
+   virtual void  SetRange(Double_t *min, Double_t *max);
+   virtual void  SetRange(Double_t x0, Double_t y0, Double_t z0, Double_t x1, Double_t y1, Double_t z1, Int_t flag=0);
    virtual void  SetSystem(Int_t system) {fSystem = system;}
-   virtual void  SetView(Float_t longitude, Float_t latitude, Float_t psi, Int_t &irep);
+   virtual void  SetView(Double_t longitude, Double_t latitude, Double_t psi, Int_t &irep);
    virtual void  WCtoNDC(Float_t *pw, Float_t *pn);
+   virtual void  WCtoNDC(Double_t *pw, Double_t *pn);
 
 //--
     virtual void MoveViewCommand(Char_t chCode, Int_t count=1);
@@ -112,10 +117,10 @@ Float_t         *GetTnorm() {return fTnorm;}
     virtual void FrontView(TVirtualPad *pad=0);
             void Front();                          // *MENU*
 
-    virtual void ZoomView(TVirtualPad *pad=0, Float_t zoomFactor = 1.25 );
-    virtual void UnzoomView(TVirtualPad *pad=0,Float_t unZoomFactor = 1.25);
+    virtual void ZoomView(TVirtualPad *pad=0, Double_t zoomFactor = 1.25 );
+    virtual void UnzoomView(TVirtualPad *pad=0,Double_t unZoomFactor = 1.25);
 
-    virtual void RotateView(Float_t phi, Float_t theta, TVirtualPad *pad=0);
+    virtual void RotateView(Double_t phi, Double_t theta, TVirtualPad *pad=0);
     virtual void SideView(TVirtualPad *pad=0);
             void Side();                          // *MENU*
     virtual void TopView(TVirtualPad *pad=0);
