37d8838d763dc1297e9b42a737a7c557ad3b7aa4
Rene Brun <Rene.Brun@cern.ch>
2000-06-13 10:58:50 +0000

    - Mods in TGraphAsymmErrors.h   The internal dynamic arrays fEXlow,fEXhigh,fEYlow,fEYhigh  have their type changed from Float_t to Double_t   class VersionID changed from 1 to 2.   New constructor:         TGraphAsymmErrors(Int_t n);   The arguments of the other constructors are now mandatory:         TGraphAsymmErrors(Int_t n, Float_t *x, Float_t *y, Float_t *exl=0, Float_t *exh=0, Float_t *eyl=0, Float_t *eyh=0);         TGraphAsymmErrors(Int_t n, Double_t *x, Double_t *y, Double_t *exl=0, Double_t *exh=0, Double_t *eyl=0, Double_t *eyh=0);    The following functions have their arguments or return type    changed from Float_t to Double_t:         virtual void    ComputeRange(Double_t &xmin, Double_t &ymin, Double_t &xmax, Double_t &ymax);         Double_t        GetErrorX(Int_t bin);         Double_t        GetErrorY(Int_t bin);         Double_t       *GetEXlow()  {return fEXlow;}         Double_t       *GetEXhigh() {return fEXhigh;}         Double_t       *GetEYlow()  {return fEYlow;}         Double_t       *GetEYhigh() {return fEYhigh;}         virtual void    SetPoint(Int_t i, Double_t x, Double_t y);         virtual void    SetPointError(Int_t i, Double_t exl, Double_t exh, Double_t eyl, Double_t eyh);
    - Mods in TGraphAsymmErrors.cxx. See changes in TGraphAsymmErrors.h.
      TGraphAsymmErrors::Streamer modified to read old objects written in single precision.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@151 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf/inc/TGraphAsymmErrors.h
+++ graf/inc/TGraphAsymmErrors.h
@@ -1,4 +1,4 @@
-// @(#)root/graf:$Name$:$Id$
+// @(#)root/graf:$Name:  $:$Id: TGraphAsymmErrors.h,v 1.1.1.1 2000/05/16 17:00:50 rdm Exp $
 // Author: Rene Brun   03/03/99
 
 /*************************************************************************
@@ -28,29 +28,31 @@
 class TGraphAsymmErrors : public TGraph {
 
 protected:
-    Float_t    *fEXlow;        //[fNpoints] array of X low errors
-    Float_t    *fEXhigh;       //[fNpoints] array of X high errors
-    Float_t    *fEYlow;        //[fNpoints] array of Y low errors
-    Float_t    *fEYhigh;       //[fNpoints] array of Y high errors
+    Double_t    *fEXlow;        //[fNpoints] array of X low errors
+    Double_t    *fEXhigh;       //[fNpoints] array of X high errors
+    Double_t    *fEYlow;        //[fNpoints] array of Y low errors
+    Double_t    *fEYhigh;       //[fNpoints] array of Y high errors
 
 public:
         TGraphAsymmErrors();
-        TGraphAsymmErrors(Int_t n, Float_t *x=0, Float_t *y=0, Float_t *exl=0, Float_t *exh=0, Float_t *eyl=0, Float_t *eyh=0);
+        TGraphAsymmErrors(Int_t n);
+        TGraphAsymmErrors(Int_t n, Float_t *x, Float_t *y, Float_t *exl=0, Float_t *exh=0, Float_t *eyl=0, Float_t *eyh=0);
+        TGraphAsymmErrors(Int_t n, Double_t *x, Double_t *y, Double_t *exl=0, Double_t *exh=0, Double_t *eyl=0, Double_t *eyh=0);
         virtual ~TGraphAsymmErrors();
-        virtual void    ComputeRange(Float_t &xmin, Float_t &ymin, Float_t &xmax, Float_t &ymax);
-        Float_t         GetErrorX(Int_t bin);
-        Float_t         GetErrorY(Int_t bin);
-        Float_t         *GetEXlow()  {return fEXlow;}
-        Float_t         *GetEXhigh() {return fEXhigh;}
-        Float_t         *GetEYlow()  {return fEYlow;}
-        Float_t         *GetEYhigh() {return fEYhigh;}
+        virtual void    ComputeRange(Double_t &xmin, Double_t &ymin, Double_t &xmax, Double_t &ymax);
+        Double_t        GetErrorX(Int_t bin);
+        Double_t        GetErrorY(Int_t bin);
+        Double_t       *GetEXlow()  {return fEXlow;}
+        Double_t       *GetEXhigh() {return fEXhigh;}
+        Double_t       *GetEYlow()  {return fEYlow;}
+        Double_t       *GetEYhigh() {return fEYhigh;}
         virtual void    Paint(Option_t *chopt="");
         virtual void    Print(Option_t *chopt="");
         virtual void    SavePrimitive(ofstream &out, Option_t *option);
-        virtual void    SetPoint(Int_t i, Float_t x, Float_t y);
-        virtual void    SetPointError(Int_t i, Float_t exl, Float_t exh, Float_t eyl, Float_t eyh);
+        virtual void    SetPoint(Int_t i, Double_t x, Double_t y);
+        virtual void    SetPointError(Int_t i, Double_t exl, Double_t exh, Double_t eyl, Double_t eyh);
 
-        ClassDef(TGraphAsymmErrors,1)  //a Graph with asymmetric error bars
+        ClassDef(TGraphAsymmErrors,2)  //a Graph with asymmetric error bars
 };
 
 #endif
