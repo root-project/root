8a45586f07e3ce5d015a5621d6b77fa2a9c7549b
Rene Brun <Rene.Brun@cern.ch>
2000-07-13 19:22:46 +0000

    New class TSelectorCint
    git-svn-id: http://root.cern.ch/svn/root/trunk@376 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tree/inc/TSelectorCint.h
@@ -0,0 +1,54 @@
+// @(#)root/treeplayer:$Name:  $:$Id: TSelector.h,v 1.2 2000/07/10 06:12:15 brun Exp $
+// Author: Rene Brun   05/02/97
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TSelectorCint
+#define ROOT_TSelectorCint
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TSelectorCint                                                        //
+//                                                                      //
+// A utility class for Trees selections.  (via interpreter)             //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifndef ROOT_TSelector
+#include "TSelector.h"
+#endif
+
+class G__CallFunc;
+class G__ClassInfo;
+
+class TSelectorCint : public TSelector {
+
+protected:
+   G__CallFunc   *fFuncBegin;    //!
+   G__CallFunc   *fFuncTerm;     //!
+   G__CallFunc   *fFuncCut;      //!
+   G__CallFunc   *fFuncFill;     //!
+   TSelector     *fIntSelector;  //Pointer to interpreted selector (if interpreted)
+   
+public:
+   TSelectorCint();
+   virtual            ~TSelectorCint();
+   virtual void        Build(TSelector *iselector, G__ClassInfo *cl);
+   virtual void        ExecuteBegin(TTree *tree);
+   virtual Bool_t      ExecuteProcessCut(Int_t entry);
+   virtual void        ExecuteProcessFill(Int_t entry);
+   virtual void        ExecuteTerminate();
+
+   ClassDef(TSelectorCint,0)  //A utility class for Trees selections. (interpreted)
+};
+
+#endif
+
---
+++ tree/src/TSelectorCint.cxx
@@ -0,0 +1,105 @@
+// @(#)root/treeplayer:$Name:  $:$Id: TSelector.cxx,v 1.2 2000/07/10 06:12:15 brun Exp $
+// Author: Rene Brun   05/02/97
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// This class is a special version of TSelector for user interpreted    //
+// classes.                                                             //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#include "TROOT.h"
+#include "TTree.h"
+#include "TSelectorCint.h"
+#include "Api.h"
+
+ClassImp(TSelectorCint)
+
+//______________________________________________________________________________
+TSelectorCint::TSelectorCint(): TSelector()
+{
+   // Default constructor for a Selector.
+
+   fFuncBegin   = 0;
+   fFuncTerm    = 0;
+   fFuncCut     = 0;
+   fFuncFill    = 0;
+   fIntSelector = 0;
+   
+}
+
+//______________________________________________________________________________
+TSelectorCint::~TSelectorCint()
+{
+   // destructor for a Selector.
+
+   delete fFuncBegin;
+   delete fFuncTerm;
+   delete fFuncCut;
+   delete fFuncFill;
+}
+
+//______________________________________________________________________________
+void TSelectorCint::Build(TSelector *iselector, G__ClassInfo *cl)
+{
+   // Initialize the CallFunc objects when selector is interpreted
+
+   fIntSelector = iselector;
+   fFuncBegin   = new G__CallFunc();
+   fFuncTerm    = new G__CallFunc();
+   fFuncCut     = new G__CallFunc();
+   fFuncFill    = new G__CallFunc();
+   Long_t offset = 0;
+   fFuncBegin->SetFuncProto(cl,"Begin","",&offset);
+   fFuncTerm->SetFuncProto (cl,"Terminate","",&offset);
+   fFuncCut->SetFuncProto  (cl,"ProcessCut","int",&offset);
+   fFuncFill->SetFuncProto (cl,"ProcessFill","int",&offset);
+}
+
+
+//______________________________________________________________________________
+void TSelectorCint::ExecuteBegin(TTree *tree)
+{
+   // Invoke the Begin function via the interpreter
+
+   fFuncBegin->SetArg((Long_t)tree);
+   fFuncBegin->ExecInt(fIntSelector);
+}
+
+
+//______________________________________________________________________________
+Bool_t TSelectorCint::ExecuteProcessCut(Int_t entry)
+{
+   // Invoke the ProcessCut function via the interpreter
+
+   fFuncCut->SetArgArray((Long_t*)&entry);
+   Int_t sel = fFuncCut->ExecInt(fIntSelector);
+   return (Bool_t)sel;
+}
+
+
+//______________________________________________________________________________
+void TSelectorCint::ExecuteProcessFill(Int_t entry)
+{
+   // Invoke the ProcessFill function via the interpreter
+
+   fFuncFill->SetArgArray((Long_t*)&entry);
+   fFuncFill->Exec(fIntSelector);
+}
+
+
+//______________________________________________________________________________
+void TSelectorCint::ExecuteTerminate()
+{
+   // Invoke the Terminate function via the interpreter
+
+   fFuncTerm->Exec(fIntSelector);
+}
