80c9a5b9f1e7ecdcdde94719b553115721ba6104
Fons Rademakers <Fons.Rademakers@cern.ch>
2000-08-08 09:15:07 +0000

    fix a typo (defined instead of define) affecting the generation of compiledata.h on AIX
    git-svn-id: http://root.cern.ch/svn/root/trunk@432 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/compiledata.sh
+++ build/unix/compiledata.sh
@@ -39,14 +39,14 @@ echo "Running $COMPILEDATA"
 echo "/* This is file is automatically generated */" > __compiledata
 echo "#define BUILD_NODE \""`uname -a`"\" " >> __compiledata
 echo "#define COMPILER \""`type $CXX`"\" " >> __compiledata
-if [ "$CUSTOMSHARED" = "" ]; then 
+if [ "$CUSTOMSHARED" = "" ]; then
    echo "#define MAKESHAREDLIB  \"$CXX -c $OPT $CXXFLAGS \$IncludePath \$SourceFiles ; $CXX \$ObjectFiles $SOFLAGS $LDFLAGS -o \$SharedLib\"" >> __compiledata
 else
-   echo "#defined MAKESHAREDLIB \"$CUSTOMSHARED\"" >> __compiledata
+   echo "#define MAKESHAREDLIB \"$CUSTOMSHARED\"" >> __compiledata
 fi
-if [ "$CUSTOMEXE" = "" ]; then 
+if [ "$CUSTOMEXE" = "" ]; then
    echo "#define MAKEEXE \"$CXX -c $OPT $CXXFLAGS \$IncludePath \$SourceFiles; $CXX \$ObjectFiles $LDFLAGS -o \$ExeName \$LinkedLibs $SYSLIBS\""  >> __compiledata
-else 
+else
    echo "#define MAKEEXE \"$CUSTOMEXE\"" >> __compiledata
 fi
 echo "#define LINKEDLIBS \"-L$LIBDIR $ROOTLIBS $RINTLIBS \""  >> __compiledata
@@ -65,7 +65,7 @@ if [ -r $COMPILEDATA ]; then
 else
    echo "Making $COMPILEDATA"
    mv __compiledata $COMPILEDATA; fi
-) 
+)
 
 exit 0
 
