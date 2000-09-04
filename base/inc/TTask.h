6bcc4f37cc70e51b200e211c8b79b74d21b94da9
Rene Brun <Rene.Brun@cern.ch>
2000-09-04 10:39:57 +0000

    Add a new Class TTask. TTask is a base class that can be used to build a complex tree of Tasks. Each TTask derived class may contain other TTasks that can be executed recursively, such that a complex program can be dynamically built and executed by invoking the services of the top level Task or one of its subtasks.
    Use the TTask::Add function to add a subtask to an existing TTask.
    To execute a TTask, one calls the ExecuteTask function. ExecuteTask will
    call recursively:
         - the TTask::Execute function of the derived class
         - TTask::ExecuteTasks to execute for each task the list of its subtasks.
    If the top level task is added to the list of Root
    browsable objects, the tree of tasks can be visualized by the Root browser.
    The browser can be used to start a task, set break points at the beginning
    of a task or when the task has completed. At a breakpoint, data structures
    generated by the execution up this point may be inspected asyncronously
    and then the execution can be resumed by selecting the "Continue" function
    of a task.
    
    A TTask tree may be made persistent, saving the status of all the tasks.
    See the class description for an example.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@554 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ base/inc/TTask.h
@@ -0,0 +1,69 @@
+// Author: Rene Brun   02/09/2000
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TTask
+#define ROOT_TTask
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TTask                                                                //
+//                                                                      //
+// Base class for recursive execution of tasks.                         //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ROOT_TNamed
+#include "TNamed.h"
+#endif
+
+class TBrowser;
+   
+class TTask : public TNamed {
+
+public:
+
+  TTask();
+  TTask(const char* name, const char *title);
+  TTask(const TTask &task);
+  virtual ~TTask();
+
+  // Main functions
+          void  Abort();  // *MENU*
+          void  Add(TTask *task) {fTasks->Add(task);}
+  virtual void  Browse(TBrowser *b);
+          void  CleanTasks();
+  virtual void  Clear(Option_t *option="");
+  virtual void  Continue(); // *MENU*
+  virtual void  Execute(Option_t *option);
+          void  ExecuteTask(Option_t *option="0");  // *MENU*
+          void  ExecuteTasks(Option_t *option);
+          Int_t GetBreakin() {return fBreakin;}
+          Int_t GetBreakout() {return fBreakout;}
+         Bool_t IsFolder() { return kTRUE; }
+  virtual void  ls(Option_t *option="*");  // *MENU*
+          void  SetBreakin(Int_t breakin=1)   {fBreakin = breakin;} // *TOGGLE*
+          void  SetBreakout(Int_t breakout=1) {fBreakout=breakout;} // *TOGGLE*
+  TList        *GetListOfTasks() {return fTasks;}
+ 
+  // Data members
+protected:      
+  
+  TList        *fTasks;        //List of Tasks
+  static TTask *fBeginTask;    //pointer to task initiator
+  static TTask *fBreakPoint;   //pointer to current break point
+  TString       fOption;       //Option specified in ExecuteTask
+  Int_t         fBreakin;      //=1 if a break point set at task extry
+  Int_t         fBreakout;     //=1 if a break point set at task exit
+  Bool_t        fHasExecuted;  //True if task has executed
+    
+  ClassDef(TTask,1)  //Base class for tasks
+};
+#endif
