27e5c69d109461a6ac406743d15a192ee570e3da
Rene Brun <Rene.Brun@cern.ch>
2000-09-06 15:15:18 +0000

    Add new class TGenPhaseSpace developped by Valerio Filippini. //  Utility class to generate n-body event //  according to fermi lorentz-invariant phase space //  events are generated in their own center-of-mass,
    git-svn-id: http://root.cern.ch/svn/root/trunk@592 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ eg/inc/TGenPhaseSpace.h
@@ -0,0 +1,42 @@
+// @(#)root/eg:$Name:  $:$Id: TGenPhaseSpace.h,v 1.2 2000/09/06 13:07:28 brun Exp $
+// Author: Rene Brun , Valerio Filippini  06/09/2000 
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//   Phase Space Generator, based on the GENBOD routine of CERNLIB           //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+   
+#ifndef ROOT_TGenPhaseSpace
+#define ROOT_TGenPhaseSpace
+
+#include "TLorentzVector.h"
+
+class TGenPhaseSpace : public TObject {
+ private:  
+  Int_t        fNt;        // number of decay particles
+  Double_t     fMass[18];  // masses of particles
+  Double_t     fBeta[3];   // betas of decaying particle
+  Double_t     fTeCmTm;    // total energy in the C.M. minus the total mass
+  Double_t     fWtMax;     // maximum weigth 
+
+  TLorentzVector  fDecPro[18];    // 
+
+  Double_t pdk(Double_t a, Double_t b, Double_t c);  
+
+ public:
+  TGenPhaseSpace() {}
+  virtual ~TGenPhaseSpace() {}
+
+  Bool_t          SetDecay(TLorentzVector &P, Int_t nt, Double_t *mass, Option_t *opt="");
+  Double_t        Generate();
+  TLorentzVector *GetDecay(Int_t n); 
+
+  Int_t    GetNt()      const { return fNt;}
+  Double_t GetWtMax()   const { return fWtMax;}
+
+  ClassDef(TGenPhaseSpace,1) //Simple Phase Space Generator
+};
+
+#endif
+
