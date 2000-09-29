1fddac80fb77dac2fbfb02edd21a95fb7a36ce92
Fons Rademakers <Fons.Rademakers@cern.ch>
2000-09-29 08:51:57 +0000

    moved TGHorizontal3DLine and TGVerticl3DLine to their own files (before they were part of TGToolBar).
    git-svn-id: http://root.cern.ch/svn/root/trunk@682 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ gui/src/TG3DLine.cxx
@@ -0,0 +1,27 @@
+// @(#)root/gui:$Name:$:$Id:$
+// Author: Fons Rademakers   6/09/2000
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TGHorizontal3DLine and TGVertical3DLine                              //
+//                                                                      //
+// A horizontal 3D line is a line that typically separates a toolbar    //
+// from the menubar.                                                    //
+// A vertical 3D line is a line that can be used to separate groups of  //
+// widgets.                                                             //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#include "TG3DLine.h"
+
+
+ClassImp(TGHorizontal3DLine)
+ClassImp(TGVertical3DLine)
