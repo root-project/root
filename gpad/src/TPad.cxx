934f6bd0835c5d1a50421f46186d81d45137b953
Rene Brun <Rene.Brun@cern.ch>
2000-10-09 13:58:07 +0000

    Mody TPad::DrawFrame to SetBit(kCanDelete) for the histogram used to draw the frame.
    git-svn-id: http://root.cern.ch/svn/root/trunk@733 27541ba8-7e3a-0410-8455-c3a389f83636

--- gpad/src/TPad.cxx
+++ gpad/src/TPad.cxx
@@ -1,4 +1,4 @@
-// @(#)root/gpad:$Name:  $:$Id: TPad.cxx,v 1.17 2000/09/29 07:22:38 brun Exp $
+// @(#)root/gpad:$Name:  $:$Id: TPad.cxx,v 1.18 2000/10/08 16:02:29 rdm Exp $
 // Author: Rene Brun   12/12/94
 
 /*************************************************************************
@@ -1487,6 +1487,7 @@ TH1F *TPad::DrawFrame(Double_t xmin, Double_t ymin, Double_t xmax, Double_t ymax
    if (hframe) delete hframe;
    hframe = new TH1F("hframe",title,1000,xmin,xmax);
    hframe->SetBit(TH1::kNoStats);
+   hframe->SetBit(kCanDelete);
    hframe->SetMinimum(ymin);
    hframe->SetMaximum(ymax);
    hframe->SetDirectory(0);
