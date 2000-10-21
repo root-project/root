139ac909765a3a8b092a77e324f40f97b99c7c72
Fons Rademakers <Fons.Rademakers@cern.ch>
2000-10-21 09:17:52 +0000

    remove reference to GuiTypes.h and KeySymbols.h.
    git-svn-id: http://root.cern.ch/svn/root/trunk@806 27541ba8-7e3a-0410-8455-c3a389f83636

--- gui/Module.mk
+++ gui/Module.mk
@@ -39,7 +39,7 @@ GUIH2        := TGObject.h TGScrollBar.h TGCanvas.h TGListBox.h TGComboBox.h \
 GUIH3        := TRootGuiFactory.h TRootApplication.h TRootCanvas.h \
                 TRootBrowser.h TRootContextMenu.h TRootDialog.h \
                 TRootControlBar.h TRootHelpDialog.h TRootEmbeddedCanvas.h
-GUIH4        := GuiTypes.h KeySymbols.h HelpText.h
+GUIH4        := HelpText.h
 GUIH1        := $(patsubst %,$(MODDIRI)/%,$(GUIH1))
 GUIH2        := $(patsubst %,$(MODDIRI)/%,$(GUIH2))
 GUIH3        := $(patsubst %,$(MODDIRI)/%,$(GUIH3))
