8c312287f5a94b1829478c1bb69397cb9a494edd
Rene Brun <Rene.Brun@cern.ch>
2000-11-21 20:21:18 +0000

    Change fIsEditable by fEditable.
    git-svn-id: http://root.cern.ch/svn/root/trunk@949 27541ba8-7e3a-0410-8455-c3a389f83636

--- gpad/src/TDialogCanvas.cxx
+++ gpad/src/TDialogCanvas.cxx
@@ -1,4 +1,4 @@
-// @(#)root/gpad:$Name:  $:$Id: TDialogCanvas.cxx,v 1.1.1.1 2000/05/16 17:00:41 rdm Exp $
+// @(#)root/gpad:$Name:  $:$Id: TDialogCanvas.cxx,v 1.2 2000/06/13 12:16:22 brun Exp $
 // Author: Rene Brun   03/07/96
 
 /*************************************************************************
@@ -43,9 +43,9 @@ TDialogCanvas::TDialogCanvas(const char *name, const char *title, UInt_t ww, UIn
 //*-*                    ========================
 
    SetFillColor(36);
-   fIsEditable = kFALSE;
-   fRefObject  = 0;
-   fRefPad     = 0;
+   fEditable  = kFALSE;
+   fRefObject = 0;
+   fRefPad    = 0;
 }
 
 //______________________________________________________________________________
