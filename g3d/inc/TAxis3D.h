851f27bad4be60ea53377112a91ea27312fe2f16
Rene Brun <Rene.Brun@cern.ch>
2000-11-21 20:11:23 +0000

    Cleanup header file
    git-svn-id: http://root.cern.ch/svn/root/trunk@935 27541ba8-7e3a-0410-8455-c3a389f83636

--- g3d/inc/TAxis3D.h
+++ g3d/inc/TAxis3D.h
@@ -1,4 +1,4 @@
-// @(#)root/g3d:$Name:  $:$Id: TAxis3D.h,v 1.2 2000/06/13 12:18:41 brun Exp $
+// @(#)root/g3d:$Name:  $:$Id: TAxis3D.h,v 1.3 2000/09/05 09:21:22 brun Exp $
 // Author: Valery Fine(fine@mail.cern.ch)   07/01/2000
 
 /*************************************************************************
@@ -10,7 +10,7 @@
  *************************************************************************/
 #ifndef ROOT_TAxis3D
 #define ROOT_TAxis3D
-// $Id: TAxis3D.h,v 1.2 2000/06/13 12:18:41 brun Exp $
+// $Id: TAxis3D.h,v 1.3 2000/09/05 09:21:22 brun Exp $
 // ***********************************************************************
 // * C++ class library to paint axis "arround" TView object
 // * Copyright(c) 1997~1999  [BNL] Brookhaven National Laboratory, STAR, All rights reserved
@@ -50,17 +50,17 @@ class TVirtualPad;
 class TView;
 class TAxis3D : public TNamed  {
 
-protected:
-    TAxis       fAxis[3];             //X/Y/Z axice
-    TString     fOption;              // Options (is not use yet)
-    static  const Char_t *rulerName;  // The default object name
 private:
     Int_t   AxisChoice(Option_t *axis);
     void    Build();
 
 protected:
-            TAxis  *fSelected; //!  The selected axis to play with
-            Bool_t  fZoomMode; // Zoom mode for the entire parent TPad
+    TAxis           fAxis[3];             //X/Y/Z axis
+    TString         fOption;              // Options (is not use yet)
+    static  const Char_t *rulerName;      // The default object name
+    TAxis          *fSelected;            //!  The selected axis to play with
+            Bool_t  fZoomMode;            // Zoom mode for the entire parent TPad
+
     virtual void    Copy(TObject &hnew);
             void    InitSet();
             Bool_t  SwitchZoom();
@@ -103,21 +103,21 @@ public:
     static Double_t *PixeltoXYZ(Double_t px, Double_t py, Double_t *point3D, TView *view =0);
     virtual void     SavePrimitive(ofstream &out, Option_t *option);
 
-    virtual void     SetAxisColor(Color_t color=1, Option_t *axis="*"); //*MENU*
+    virtual void     SetAxisColor(Color_t color=1, Option_t *axis="*"); // *MENU*
     virtual void     SetAxisRange(Double_t xmin, Double_t xmax, Option_t *axis="*");
 
-    virtual void     SetLabelColor(Color_t color=1, Option_t *axis="*");//*MENU*
-    virtual void     SetLabelFont(Style_t font=62, Option_t *axis="*"); //*MENU*
-    virtual void     SetLabelOffset(Float_t offset=0.005, Option_t *axis="*"); //*MENU*
-    virtual void     SetLabelSize(Float_t size=0.02, Option_t *axis="*"); //*MENU*
+    virtual void     SetLabelColor(Color_t color=1, Option_t *axis="*");// *MENU*
+    virtual void     SetLabelFont(Style_t font=62, Option_t *axis="*"); // *MENU*
+    virtual void     SetLabelOffset(Float_t offset=0.005, Option_t *axis="*"); // *MENU*
+    virtual void     SetLabelSize(Float_t size=0.02, Option_t *axis="*"); // *MENU*
 
-    virtual void     SetNdivisions(Int_t n=510, Option_t *axis="*"); //*MENU*
+    virtual void     SetNdivisions(Int_t n=510, Option_t *axis="*"); // *MENU*
     virtual void     SetOption(Option_t *option=" ") {fOption = option;}
-    virtual void     SetTickLength(Float_t length=0.02, Option_t *axis="*"); //*MENU*
-    virtual void     SetTitleOffset(Float_t offset=1, Option_t *axis="*"); //*MENU*
-    virtual void     SetXTitle(char *title) {fAxis[0].SetTitle(title);} //*MENU*
-    virtual void     SetYTitle(char *title) {fAxis[1].SetTitle(title);} //*MENU*
-    virtual void     SetZTitle(char *title) {fAxis[2].SetTitle(title);} //*MENU*
+    virtual void     SetTickLength(Float_t length=0.02, Option_t *axis="*"); // *MENU*
+    virtual void     SetTitleOffset(Float_t offset=1, Option_t *axis="*"); // *MENU*
+    virtual void     SetXTitle(const char *title) {fAxis[0].SetTitle(title);} // *MENU*
+    virtual void     SetYTitle(const char *title) {fAxis[1].SetTitle(title);} // *MENU*
+    virtual void     SetZTitle(const char *title) {fAxis[2].SetTitle(title);} // *MENU*
     static  TAxis3D *ToggleRulers(TVirtualPad *pad=0);
     static  TAxis3D *ToggleZoom(TVirtualPad *pad=0);
     void             UseCurrentStyle();
@@ -127,37 +127,5 @@ public:
 
 
 inline Bool_t TAxis3D::SwitchZoom(){Bool_t s = fZoomMode; fZoomMode = !fZoomMode; return s;}
-//__________________________________________________________________________
-// $Log: TAxis3D.h,v $
-// Revision 1.2  2000/06/13 12:18:41  brun
-// Upgrade from Float_t to Double_t. The following functions have been modified:
-//     static Double_t *PixeltoXYZ(Double_t px, Double_t py, Double_t *point3D, TView *view =0);
-//     virtual void     SetAxisRange(Double_t xmin, Double_t xmax, Option_t *axis="*");
-//
-// Revision 1.1.1.1  2000/05/16 17:00:43  rdm
-// Initial import of ROOT into CVS
-//
-// Revision 1.7  1999/12/12 17:25:52  fine
-// smart zooming
-//
-// Revision 1.6  1999/12/12 01:07:22  fine
-// remove the compilation warnings
-//
-// Revision 1.5  1999/12/09 20:42:59  fine
-// Zoom
-//
-// Revision 1.4  1999/12/02 02:46:58  fine
-// Axis coloring
-//
-// Revision 1.3  1999/11/30 20:09:53  fine
-// new static method to present rulers
-//
-// Revision 1.2  1999/11/30 01:44:23  fine
-// Z axis fixed
-//
-// Revision 1.1  1999/11/29 19:49:57  fine
-// ROOT class: TAxis3D. To be moved to ROOT later
-//
-//__________________________________________________________________________
 
 #endif
