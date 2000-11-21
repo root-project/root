08f79366d556fcc6c359b31b8ad45a107a0302b7
Rene Brun <Rene.Brun@cern.ch>
2000-11-21 20:13:28 +0000

    Increment class version from 1 to 2 Reorder data members. Cleanup header file
    git-svn-id: http://root.cern.ch/svn/root/trunk@938 27541ba8-7e3a-0410-8455-c3a389f83636

--- g3d/inc/TPCON.h
+++ g3d/inc/TPCON.h
@@ -1,4 +1,4 @@
-// @(#)root/g3d:$Name$:$Id$
+// @(#)root/g3d:$Name:  $:$Id: TPCON.h,v 1.1.1.1 2000/05/16 17:00:43 rdm Exp $
 // Author: Nenad Buncic   29/09/95
 
 /*************************************************************************
@@ -36,16 +36,17 @@ class TPCON : public TShape {
 
     protected:
 
-        Double_t   *fSiTab;     //! Table of sin(fPhi1) .... sin(fPhil+fDphi1)
-        Double_t   *fCoTab;     //! Table of cos(fPhi1) .... cos(fPhil+fDphi1)
+        Double_t   *fSiTab;       //! Table of sin(fPhi1) .... sin(fPhil+fDphi1)
+        Double_t   *fCoTab;       //! Table of cos(fPhi1) .... cos(fPhil+fDphi1)
 
-        Float_t fPhi1;         // lower phi limit
-        Float_t fDphi1;        // range in phi
-        Int_t   fNz;           // number of z segments
-        Float_t *fRmin;        //[fNz] pointer to array of inside radiuses
-        Float_t *fRmax;        //[fNz] pointer to array of outside radiuses
-        Float_t *fDz;          //[fNz] pointer to array of half lengths in z
-        Int_t   fNdiv;         // number of divisions
+        Float_t     fPhi1;        // lower phi limit
+        Float_t     fDphi1;       // range in phi
+        Int_t       fNdiv;        // number of divisions
+        Int_t       fNz;          // number of z segments
+        Float_t    *fRmin;        //[fNz] pointer to array of inside radiuses
+        Float_t    *fRmax;        //[fNz] pointer to array of outside radiuses
+        Float_t    *fDz;          //[fNz] pointer to array of half lengths in z
+        
         virtual void    MakeTableOfCoSin();  // Create the table of the fSiTab; fCoTab
         virtual void    FillTableOfCoSin(Double_t phi, Double_t angstep,Int_t n); // Fill the table of cosin
         virtual void    PaintGLPoints(Float_t *vertex);
@@ -55,22 +56,22 @@ class TPCON : public TShape {
         TPCON(const char *name, const char *title, const char *material, Float_t phi1, Float_t dphi1, Int_t nz);
         virtual ~TPCON();
 
-        virtual void    DefineSection(Int_t secNum, Float_t z, Float_t rmin, Float_t rmax);
-        virtual Int_t   DistancetoPrimitive(Int_t px, Int_t py);
-        virtual Int_t   GetNumberOfDivisions () const {if (fNdiv) return fNdiv; else return kDiv;}
-        virtual Float_t GetPhi1()  {return fPhi1;}
-        virtual Float_t GetDhi1()  {return fDphi1;}
-        virtual Int_t   GetNz()    {return fNz;}
-        virtual Float_t *GetRmin() {return fRmin;}
-        virtual Float_t *GetRmax() {return fRmax;}
-        virtual Float_t *GetDz()   {return fDz;}
-        virtual Int_t   GetNdiv()  {return fNdiv;}
-        virtual void    Paint(Option_t *option);
-        virtual void    SetNumberOfDivisions (Int_t p);
-        virtual void    SetPoints(Float_t *buff);
-        virtual void    Sizeof3D() const;
+        virtual void     DefineSection(Int_t secNum, Float_t z, Float_t rmin, Float_t rmax);
+        virtual Int_t    DistancetoPrimitive(Int_t px, Int_t py);
+        virtual Int_t    GetNumberOfDivisions () const {if (fNdiv) return fNdiv; else return kDiv;}
+        virtual Float_t  GetPhi1()  {return fPhi1;}
+        virtual Float_t  GetDhi1()  {return fDphi1;}
+        virtual Int_t    GetNz()    {return fNz;}
+        virtual Float_t *GetRmin()  {return fRmin;}
+        virtual Float_t *GetRmax()  {return fRmax;}
+        virtual Float_t *GetDz()    {return fDz;}
+        virtual Int_t    GetNdiv()  {return fNdiv;}
+        virtual void     Paint(Option_t *option);
+        virtual void     SetNumberOfDivisions (Int_t p);
+        virtual void     SetPoints(Float_t *buff);
+        virtual void     Sizeof3D() const;
 
-        ClassDef(TPCON,1)  //PCON shape
+        ClassDef(TPCON,2)  //PCON shape
 };
 
 #endif
