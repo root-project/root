f9eecc0f5beb7197356cdab92d0e64fe4999b209
Rene Brun <Rene.Brun@cern.ch>
2000-11-21 21:01:48 +0000

    iAdd new classes:  #pragma link C++ class TGLVTreeEntry+;  #pragma link C++ class TGTreeLVC+;  #pragma link C++ class TTreeView+;  #pragma link C++ class TGSelectBox+;
    git-svn-id: http://root.cern.ch/svn/root/trunk@991 27541ba8-7e3a-0410-8455-c3a389f83636

--- treeviewer/inc/LinkDef.h
+++ treeviewer/inc/LinkDef.h
@@ -1,4 +1,4 @@
-/* @(#)root/treeviewer:$Name:  $:$Id: LinkDef.h,v 1.1.1.1 2000/05/16 17:00:45 rdm Exp $ */
+/* @(#)root/treeviewer:$Name:  $:$Id: LinkDef.h,v 1.2 2000/10/31 11:19:57 brun Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -16,5 +16,9 @@
 
 #pragma link C++ class TPaveVar+;
 #pragma link C++ class TTreeViewer-;
+#pragma link C++ class TGLVTreeEntry+;
+#pragma link C++ class TGTreeLVC+;
+#pragma link C++ class TTreeView+;
+#pragma link C++ class TGSelectBox+;
 
 #endif
