d0adf82885e8d650575f9290ca2e2620e240bec5
Rene Brun <Rene.Brun@cern.ch>
2000-11-21 20:52:54 +0000

    Declare option "+" for TTreePlayer
    git-svn-id: http://root.cern.ch/svn/root/trunk@986 27541ba8-7e3a-0410-8455-c3a389f83636

--- treeplayer/inc/LinkDef.h
+++ treeplayer/inc/LinkDef.h
@@ -1,4 +1,4 @@
-/* @(#)root/treeplayer:$Name:  $:$Id: LinkDef.h,v 1.1.1.1 2000/05/16 17:00:44 rdm Exp $ */
+/* @(#)root/treeplayer:$Name:  $:$Id: LinkDef.h,v 1.2 2000/07/06 17:20:52 brun Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -14,7 +14,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class TTreePlayer;
+#pragma link C++ class TTreePlayer+;
 #pragma link C++ class TPacketGenerator;
 #pragma link C++ class TTreeFormula-;
 
