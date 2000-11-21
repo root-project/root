a2d66ef839da929990c286cde783a0ce087e922d
Rene Brun <Rene.Brun@cern.ch>
2000-11-21 16:22:29 +0000

    Add new non persistent data member fStreamer. This points to the current Streamer function for one data member. New functions GetStreamer and SetStreamer
    git-svn-id: http://root.cern.ch/svn/root/trunk@914 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/TRealData.h
+++ base/inc/TRealData.h
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name$:$Id$
+// @(#)root/base:$Name:  $:$Id: TRealData.h,v 1.1.1.1 2000/05/16 17:00:39 rdm Exp $
 // Author: Rene Brun   05/03/95
 
 /*************************************************************************
@@ -35,17 +35,20 @@ class TRealData : public TObject {
 
 private:
    TDataMember *fDataMember;         //pointer to data member descriptor
-   Int_t       fThisOffset;          //offset with the THIS object pointer
-   TString     fName;                //Concatenated names of this realdata
-
+   Int_t        fThisOffset;         //offset with the THIS object pointer
+   TString      fName;               //Concatenated names of this realdata
+   void        *fStreamer;           //!pointer to STL Streamer function
+   
 public:
    TRealData();
    TRealData(const char *name, Int_t offset, TDataMember *datamember);
    virtual     ~TRealData();
    virtual const char *GetName() const {return fName.Data();}
    TDataMember *GetDataMember() {return fDataMember;}
-   Int_t       GetThisOffset() {return fThisOffset;}
-   void        WriteRealData(void *pointer, char *&buffer);
+   void        *GetStreamer() {return fStreamer;}
+   Int_t        GetThisOffset() {return fThisOffset;}
+   void         SetStreamer(void *p) {fStreamer = p;}
+   void         WriteRealData(void *pointer, char *&buffer);
 
    ClassDef(TRealData,0)  //Description of persistent data members
 };
