0b5d4802ce2571b1b1d1059f43e8a6b0fc464bc2
Rene Brun <Rene.Brun@cern.ch>
2000-12-13 15:27:51 +0000

    Set Root version number to 3.00/00
    git-svn-id: http://root.cern.ch/svn/root/trunk@1206 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "2.26/00"
-#define ROOT_VERSION_CODE 137728
+#define ROOT_RELEASE "3.00/00"
+#define ROOT_VERSION_CODE 196608
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
