e7a0e618b2decaf9bafb62d43010b661573a0a5a
Rene Brun <Rene.Brun@cern.ch>
2001-01-16 16:22:57 +0000

    switch on automatic Streamer for TLeafElement
    git-svn-id: http://root.cern.ch/svn/root/trunk@1393 27541ba8-7e3a-0410-8455-c3a389f83636

--- tree/inc/LinkDef.h
+++ tree/inc/LinkDef.h
@@ -1,4 +1,4 @@
-/* @(#)root/tree:$Name:  $:$Id: LinkDef.h,v 1.5 2000/11/21 20:46:22 brun Exp $ */
+/* @(#)root/tree:$Name:  $:$Id: LinkDef.h,v 1.6 2001/01/15 07:26:40 brun Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -24,7 +24,7 @@
 #pragma link C++ class TCut+;
 #pragma link C++ class TEventList-;
 #pragma link C++ class TLeaf-;
-#pragma link C++ class TLeafElement-;
+#pragma link C++ class TLeafElement+;
 #pragma link C++ class TLeafObject-;
 #pragma link C++ class TLeafB+;
 #pragma link C++ class TLeafC+;
