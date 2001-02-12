e99af2e44bd7d16b3143eda17e1bdce8c488240b
Fons Rademakers <Fons.Rademakers@cern.ch>
2001-02-12 14:19:50 +0000

    close server socket properly so script can be re-executed in the same session.
    git-svn-id: http://root.cern.ch/svn/root/trunk@1585 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/pserv.C
+++ tutorials/pserv.C
@@ -1,4 +1,4 @@
-void pserv(int niter = 100, int bsize = 500000)
+void pserv()
 {
    // Server program to test parallel sockets.
    //
@@ -11,14 +11,23 @@ void pserv(int niter = 100, int bsize = 500000)
    // Open a parallel server socket looking for connections on a named
    // service or on a specified port.
    //TPServerSocket *ss = new TServerSocket("rootserv", kTRUE);
-   TPServerSocket *ss = new TPServerSocket(9090, kTRUE,
-                                           TServerSocket::kDefaultBacklog,
-                                           256000);
+   TPServerSocket *ss = new TPServerSocket(9090, kTRUE);
 
    // Accept a connection and return a full-duplex communication socket.
    TPSocket *sock = ss->Accept();
+   delete ss;
+
+   int niter, bsize;
+   sock->Recv(niter, bsize);
+
+   printf("Receive %d buffers of %d bytes over %d parallel sockets...\n",
+          niter, bsize, sock->GetSize());
 
    char *buf = new char[bsize];
+
+   // start timer
+   TStopwatch timer;
+   timer.Start();
 
    // accept data from client
    for (int i = 0; i < niter; i++) {
@@ -28,8 +37,20 @@ void pserv(int niter = 100, int bsize = 500000)
          printf("error receiving\n");
          break;
       }
+      if (buf[0] != 65) {
+         printf("received data corrupted\n");
+         break;
+      }
    }
 
    delete sock;
    delete [] buf;
+
+   // stop timer and print results
+   timer.Stop();
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+
+   printf("%d bytes received in %f seconds\n", niter*bsize, rtime);
+   printf("%5.2f MB/s\n", Double_t(niter*bsize/1024/1024)/rtime);
 }
