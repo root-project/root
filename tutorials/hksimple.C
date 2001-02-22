e27ae29030c336ad88bf8ec5627c7477cd3d4d8c
Rene Brun <Rene.Brun@cern.ch>
2001-02-22 08:48:42 +0000

    Some changes in the graphics attributes. A Gaussian fit added to the 3 histograms to illustrate the advantage of TH1K.
    git-svn-id: http://root.cern.ch/svn/root/trunk@1659 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/hksimple.C
+++ tutorials/hksimple.C
@@ -6,34 +6,39 @@ void hksimple()
 //*-*  This script illustrates the advantages of a TH1K histogram
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-  TH1 *hpx[3];
-  int j;
 
 // Create a new canvas.
-  c1 = new TCanvas("c1","Dynamic Filling Example",200,10,700,500);
+  c1 = new TCanvas("c1","Dynamic Filling Example",200,10,600,900);
   c1->SetFillColor(42);
-  c1->GetFrame()->SetFillColor(21);
-  c1->GetFrame()->SetBorderSize(6);
-  c1->GetFrame()->SetBorderMode(-1);
 
 // Create a normal histogram and two TH1K histograms
-  hpx[0]    = new TH1F("hp0","This is the px distribution",1000,-4,4);
-  hpx[1]    = new TH1K("hk1","This is the px distribution",1000,-4,4);
-  hpx[2]    = new TH1K("hk2","This is the px distribution",1000,-4,4,16);
+  TH1 *hpx[3];
+  hpx[0]    = new TH1F("hp0","Normal histogram",1000,-4,4);
+  hpx[1]    = new TH1K("hk1","Nearest Neighboor of order 3",1000,-4,4);
+  hpx[2]    = new TH1K("hk2","Nearest Neighboor of order 16",1000,-4,4,16);
   c1->Divide(1,3);
-   for (j=0;j<3;j++) {c1->cd(j+1); hpx[j]->Draw();hpx[j]->SetFillColor(48);}
+  Int_t j;
+  for (j=0;j<3;j++) {
+     c1->cd(j+1); 
+     gPad->SetFrameFillColor(33);
+     hpx[j]->SetFillColor(48);
+     hpx[j]->Draw();
+  }
 
 // Fill histograms randomly
   gRandom->SetSeed();
   Float_t px, py, pz;
   const Int_t kUPDATE = 10;
-  for (Int_t i = 0; i <= 600; i++) {
+  for (Int_t i = 0; i <= 300; i++) {
      gRandom->Rannor(px,py);
      for (j=0;j<3;j++) {hpx[j]->Fill(px);}
      if (i && (i%kUPDATE) == 0) {
            padRefresh(c1);
      }
   }
+
+  for (j=0;j<3;j++) hpx[j]->Fit("gaus","","");
+  padRefresh(c1);
 }
 void padRefresh(TPad *pad,int flag)
 {
