f7f94b7a0a47686a67541c4b9b8c5f2d73f2e43f
Rene Brun <Rene.Brun@cern.ch>
2001-03-01 13:35:08 +0000

    New version of the bench test suite including presentation of results with graphics.
    git-svn-id: http://root.cern.ch/svn/root/trunk@1725 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/bench.cxx
+++ test/bench.cxx
@@ -14,6 +14,10 @@
 // The test prints a summary table comparing performances for all above cases
 // (CPU, file size, compression factors).
 // Reference numbers on a Pentium III 650 Mhz machine are given as reference.
+//
+// A canvas is created showing the same results in a graphical form.
+// The bench can be run in batch mode (bench -b).
+// A Postscript file bench.ps is also produced.
 //      Author:  Rene Brun
    
 #include "TROOT.h"
@@ -22,12 +26,75 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TSystem.h"
+#include "TCanvas.h"
+#include "TPaveText.h"
+#include "TArrow.h"
+#include "TH1.h"
+#include "TBox.h"
+#include "TStyle.h"
+#include "TText.h"
+#include "TRint.h"
 
 #include "TBench.h"
+
+void showhist(const char *title, const char *ytitle, float a, float b, float c, float d, float ar, float br, float cr, float dr)
+{
+   //function to display one result unit
+   TH1F *h = new TH1F("h",title,4,0,4);
+   h->SetDirectory(0);
+   h->GetXaxis()->SetLabelFont(22);
+   h->GetXaxis()->SetLabelOffset(99);
+   h->GetXaxis()->SetNdivisions(4);
+   h->GetYaxis()->SetNdivisions(5);
+   h->GetYaxis()->SetLabelSize(.1);
+   h->SetStats(0);
+   gPad->SetGrid();
+   gPad->SetBottomMargin(0.05);
+   gPad->SetTopMargin(0.16);
+   gPad->SetLeftMargin(0.14);
+   gPad->SetRightMargin(0.05);
+   if (strlen(ytitle)) {
+      h->GetYaxis()->SetTitle(ytitle);
+      h->GetYaxis()->SetTitleOffset(0.7);
+      h->GetYaxis()->SetTitleSize(0.09);
+   } else {
+      h->GetYaxis()->SetLabelOffset(99);
+   }
+   
+   float ymax = a;
+   if (b  > ymax) ymax = b;
+   if (c  > ymax) ymax = c;
+   if (d  > ymax) ymax = d;
+   if (ar > ymax) ymax = ar;
+   if (br > ymax) ymax = br;
+   if (cr > ymax) ymax = cr;
+   if (dr > ymax) ymax = dr;
+   if (b > ymax) ymax = b;
+   ymax *=1.05;
+   h->SetMinimum(0);
+   h->SetMaximum(ymax);
+   h->Draw();
+   float dx  = 0.4;
+   float dxr = 0.1;
+   TBox *boxa  = new TBox(0.5-dx ,0,0.5+dx ,a); boxa->SetFillColor(46);  boxa->Draw();
+   TBox *boxb  = new TBox(1.5-dx ,0,1.5+dx ,b); boxb->SetFillColor(5);  boxb->Draw();
+   TBox *boxc  = new TBox(2.5-dx ,0,2.5+dx ,c); boxc->SetFillColor(6);  boxc->Draw();
+   TBox *boxd  = new TBox(3.5-dx ,0,3.5+dx ,d); boxd->SetFillColor(2);  boxd->Draw();
+   TBox *boxar = new TBox(0.5-dxr,0,0.5+dxr,ar); boxar->SetFillColor(1); boxar->Draw();
+   TBox *boxbr = new TBox(1.5-dxr,0,1.5+dxr,br); boxbr->SetFillColor(1); boxbr->Draw();
+   TBox *boxcr = new TBox(2.5-dxr,0,2.5+dxr,cr); boxcr->SetFillColor(1); boxcr->Draw();
+   TBox *boxdr = new TBox(3.5-dxr,0,3.5+dxr,dr); boxdr->SetFillColor(1); boxdr->Draw();
+   boxa->SetFillStyle(3013);
+   boxb->SetFillStyle(3010);
+   boxc->SetFillStyle(3014);
+   boxd->SetFillStyle(3012);
+}
 
 int main(int argc, char** argv) {
   TROOT root("bench","Benchmarking STL vector against TClonesArray");
 
+  TRint *theApp = new TRint("Rint", &argc, argv, 0, 0);
+  
   int nhits       = 1000;
   int nevents     = 400;
   Float_t cx;
@@ -164,18 +231,22 @@ int main(int argc, char** argv) {
   gSystem->Exec("rm -f /tmp/bench.root");
   
   //print all results
+  char line1[100], line2[100];
   printf("\n");
   printf("******************************************************************************\n");
-  printf("*  Comparing STL vector with TClonesArray: Root%-8s  %8d/%4d       *\n",gROOT->GetVersion(),gROOT->GetVersionDate(),gROOT->GetVersionTime());
+  sprintf(line1,"Comparing STL vector with TClonesArray: Root %-8s",gROOT->GetVersion());
+  printf("*       %s                *\n",line1);
   Bool_t UNIX = strcmp(gSystem->GetName(), "Unix") == 0;
   if (UNIX) {
      FILE *fp = gSystem->OpenPipe("uname -a", "r");
      char line[60];
      fgets(line,60,fp); line[59] = 0;
+     sprintf(line2,"%s",line);
      printf("*  %s\n",line);
      gSystem->ClosePipe(fp);
   } else {
      const char *os = gSystem->Getenv("OS");
+     sprintf(line2,"Windows");
      if (!os) printf("*  Windows 95\n");
      else     printf("*  %s %s \n",os,gSystem->Getenv("PROCESSOR_IDENTIFIER"));
   }
@@ -211,4 +282,76 @@ int main(int argc, char** argv) {
   printf("* TClonesArray(TObjHit)         %6.2f        1.53    %6.2f        2.14     *\n",cp8r,cp9r);
   printf("* TClonesArray(TObjHit) split   %6.2f        1.35    %6.2f        1.94     *\n",cp10r,cp11r);
   printf("******************************************************************************\n");
+
+  // show results with graphics
+   gStyle->SetTitleH(0.12);
+   gStyle->SetTitleW(0.8);
+   gStyle->SetFrameFillColor(33);
+   TCanvas *cbench = new TCanvas("cbench","Results of Root benchmark",10,10,600,800);
+   TPaveText *head = new TPaveText(.05,.81,.95,.99);
+   head->SetFillColor(42);
+   head->SetTextAlign(22);
+   head->AddText(line1);
+   head->AddText(line2);
+   head->AddText("Reference machine pcnotebrun.cern.ch  RedHat Linux 6.1");
+   head->AddText("(Pentium III 650 Mhz 256 Mbytes RAM, IDE disk)");
+   head->AddText("(send your results to rootdev@root.cern.ch)");
+   head->Draw();
+   TPad *pmain = new TPad("pmain","pmain",0,0,1,.8);
+   pmain->SetFillColor(20);
+   pmain->Draw();
+   pmain->Divide(2,4);
+   pmain->cd(1);
+   showhist("legend","",5,4,3,2,9,6,4,3);
+   TText t;
+   t.SetTextFont(23);
+   t.SetTextSize(15);
+   t.DrawText(1.0,8.0,"vector<THit>");
+   t.DrawText(1.5,6.8,"vector<THit*>");
+   t.DrawText(2.5,5.6,"TClonesArray");
+   t.DrawText(2.8,4.5,"-nosplit");
+   t.DrawText(3.1,3.4,"-split");
+   TArrow *arrow = new TArrow(-0.0519789,8.04398,0.327782,6.94924,0.03,"|>");
+   arrow->SetFillColor(1);
+   arrow->SetFillStyle(1001);
+   arrow->Draw();
+   TPaveText *pt = new TPaveText(0.02,0.65,0.16,0.80,"brNDC");
+   pt->AddText("Ref.");
+   pt->AddText("machine");
+   pt->Draw();
+   arrow = new TArrow(1.19087,7.87556,0.828376,5.43344,0.03,"|>");
+   arrow->SetFillColor(1);
+   arrow->SetFillStyle(1001);
+   arrow->Draw();
+   arrow = new TArrow(2.05397,6.61239,1.82956,4.33869,0.03,"|>");
+   arrow->SetFillColor(1);
+   arrow->SetFillStyle(1001);
+   arrow->Draw();
+   arrow = new TArrow(2.8998,4.50712,2.72718,3.41237,0.03,"|>");
+   arrow->SetFillColor(1);
+   arrow->SetFillStyle(1001);
+   arrow->Draw();
+   arrow = new TArrow(3.24504,3.24395,3.2623,2.40184,0.03,"|>");
+   arrow->SetFillColor(1);
+   arrow->SetFillStyle(1001);
+   arrow->Draw(); 
+   Float_t z = 1.e-6;
+   pmain->cd(2);
+   showhist("Time to fill collections","seconds",cp1,cp4,cp7,cp7,1.88,1.81,1.60,1.60);
+   pmain->cd(3);
+   showhist("File size no compression","Megabytes",z*nbytes1,z*nbytes5,z*nbytes8,z*nbytes10, 42.05,42.08,39.69,39.78);
+   pmain->cd(4);
+   showhist("File size compression 1","Megabytes",z*nbytes3,z*nbytes6,z*nbytes9,z*nbytes11,9.21,9.21,6.09,5.93);
+   pmain->cd(5);
+   showhist("Time to write no compression","seconds",cp2w-cp1,cp5w-cp1,cp8w-cp1,cp10w-cp1,1.76,1.77,1.30,1.30);
+   pmain->cd(6);
+   showhist("Time to write compression 1","seconds",cp3w-cp1,cp6w-cp1,cp9w-cp1,cp11w-cp1,9.68,9.70,7.62,6.01);
+   pmain->cd(7);
+   showhist("Time to read no compression","seconds",cp2r,cp5r,cp8r,cp10r,2.21,2.04,1.58,1.36);
+   pmain->cd(8);
+   showhist("Time to read compression 1","seconds",cp3r,cp6r,cp9r,cp11r,3.46,3.17,2.12,1.95);   
+   cbench->Print();
+   
+   theApp->Run();
+
 }
