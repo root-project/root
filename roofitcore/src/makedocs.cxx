55092854bed010842ab8fd903d92bbf223cecd30
David Kirkby <dkirkby@uci.edu>
2001-03-20 01:47:39 +0000

    add some how-to instructions in a README file and a macro for making html documentation
    git-svn-id: http://root.cern.ch/svn/root/trunk@1835 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ roofitcore/src/makedocs.cxx
@@ -0,0 +1,13 @@
+void makedocs(const char *version, const char *where= "./html") {
+
+  const char *bfarch= gSystem->Getenv("BFARCH");
+  TString sourceDir("RELEASE/RooFitCore:RELEASE/tmp/");
+  sourceDir.Append(bfarch);
+  sourceDir.Append("/RooFitCore");
+  gEnv->SetValue("Root.Html.SourceDir",sourceDir.Data());
+  gEnv->SetValue("Root.Html.OutputDir",where);
+  gEnv->SetValue("Root.Html.LastUpdate","*    File: ");
+
+  THtml docMaker;
+  docMaker.MakeAll(kFALSE,"Roo*");
+}
