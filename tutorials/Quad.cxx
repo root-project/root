f9c22629d3fa44274489e9925a8c2d5d5be9e9f0
Rene Brun <Rene.Brun@cern.ch>
2001-05-11 07:09:06 +0000

    Remove invisible characters in the Quad example files.
    git-svn-id: http://root.cern.ch/svn/root/trunk@2169 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/Quad.cxx
+++ tutorials/Quad.cxx
@@ -1,71 +1,42 @@
 #include <iostream.h>
 #include <math.h>
 #include "Quad.h"
-     
-  Quad::Quad(Float_t a, Float_t b, Float_t c) {
 
-    fA = a;
+Quad::Quad(Float_t a,Float_t b,Float_t c)
+{
+   fA = a;
+   fB = b;
+   fC = c;
+}
 
-    fB = b;
+Quad::~Quad()
+{
+   cout << "deleting object with coeffts: "
+        << fA << "," << fB << "," << fC << endl;
+}
 
-    fC = c;
+void Quad::Solve() const
+{
+   Float_t temp = fB*fB -4*fA*fC;
+   if (temp > 0) {
+      temp = sqrt(temp);
+      cout << "There are two roots: "
+           << ( -fB - temp ) / (2.*fA)
+           << " and "
+           << ( -fB + temp ) / (2.*fA)
+           << endl;
+   } else {
+      if (temp == 0) {
+         cout << "There are two equal roots: "
+         << -fB / (2.*fA) << endl;
+      } else {
+         cout << "There are no roots" << endl;
+      }
+   }
+}
 
-  }
-
- 
-  Quad::~Quad() {
-
-    cout << "deleting object with coeffts: "
-
-       << fA << "," << fB << "," << fC << endl;
-
-  }
-
- 
-
-  Float_t Quad::Evaluate(Float_t x) const {
-
-    return fA*x*x + fB*x + fC;
-
-  }
-
-
-  void Quad::Solve() const {
-
-    Float_t temp = fB*fB - 4.*fA*fC;  
-
-    if ( temp > 0. ) {
-
-      temp = sqrt( temp );
-
-      cout << "There are two roots: "
-
-           << ( -fB - temp ) / (2.*fA)
-
-           << " and "
-           << ( -fB + temp ) / (2.*fA)
-
-           << endl;
-
-    }
-
-    else {
-
-      if ( temp == 0. ) {
-
-        cout << "There are two equal roots: "
-
-                              << -fB / (2.*fA) << endl;
-
-      }
-
-      else {
-
-        cout << "There are no roots" << endl;
-
-      }
-
-    }   
-
-  }
-
+Float_t Quad::Evaluate(Float_t x) const
+{
+  return fA*x*x + fB*x + fC;
+  return 0;
+}
--- tutorials/Quad.h
+++ tutorials/Quad.h
@@ -1,21 +1,15 @@
+#include "TObject.h"
+   
 class Quad {
 
-      public:
+public:
+   Quad(Float_t a, Float_t b, Float_t c);
+   ~Quad();
+   Float_t Evaluate(Float_t x) const;
+   void Solve() const;
 
-          Quad(Float_t a, Float_t b, Float_t c);
-
-         ~Quad();
-
-          Float_t Evaluate(Float_t x) const;
-
-          void Solve() const;
-
-        private:
-
-          Float_t fA;
-
-          Float_t fB;
-
-          Float_t fC;
-
+private:
+   Float_t fA;
+   Float_t fB;
+   Float_t fC;
 };
