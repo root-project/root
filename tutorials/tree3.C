0734a92b8f7cb78ab2108957f043e1e1e6699a23
Rene Brun <Rene.Brun@cern.ch>
2001-05-19 20:28:12 +0000

    Add an example of Tree friendship.
    git-svn-id: http://root.cern.ch/svn/root/trunk@2240 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/tree3.C
+++ tutorials/tree3.C
@@ -1,11 +1,11 @@
-void tree3() {
+void tree3w() {
 // Example of a Tree where branches are variable length arrays
+// A second Tree is created and filled in parallel.
 // Run this script with 
 //   .x tree3.C
-// then in a new session, use, eg the TreeViewer to histogram
-// the ntuple entries
-//  root > TFile f("tree3.root")
-//  root > t3->StartViewer()
+// In the function treer, the first Tree is open.
+// The second Tree is declared friend of the first tree.
+// TTree::Draw is called with variables from both Trees.
 //
 //  Author: Rene Brun
       
@@ -16,9 +16,11 @@ void tree3() {
    Float_t px[kMaxTrack]; 
    Float_t py[kMaxTrack]; 
    Float_t pz[kMaxTrack]; 
+   Float_t pt[kMaxTrack]; 
    Float_t zv[kMaxTrack];
    Float_t chi2[kMaxTrack];
-  
+   Double_t sumstat;
+     
    TFile f("tree3.root","recreate");
    TTree *t3 = new TTree("t3","Reconst ntuple");
    t3->Branch("ntrack",&ntrack,"ntrack/I");
@@ -30,9 +32,16 @@ void tree3() {
    t3->Branch("zv",zv,"zv[ntrack]/F");
    t3->Branch("chi2",chi2,"chi2[ntrack]/F");
 
+   TFile fr("tree3f.root","recreate");
+   TTree *t3f = new TTree("t3f","a friend Tree");
+   t3f->Branch("ntrack",&ntrack,"ntrack/I");
+   t3f->Branch("sumstat",&sumstat,"sumstat/D");
+   t3f->Branch("pt",pt,"pt[ntrack]/F");
+
    for (Int_t i=0;i<1000;i++) {
       Int_t nt = gRandom->Rndm()*(kMaxTrack-1);
       ntrack = nt;
+      sumstat = 0;
       for (Int_t n=0;n<nt;n++) {
          stat[n] = n%3;
          sign[n] = i%2;
@@ -41,10 +50,29 @@ void tree3() {
          pz[n]   = gRandom->Gaus(10,5);
          zv[n]   = gRandom->Gaus(100,2);
          chi2[n] = gRandom->Gaus(0,.01);
+         sumstat += chi2[n];
+         pt[n]   = TMath::Sqrt(px[n]*px[n] + py[n]*py[n]);
       }
       t3->Fill();
+      t3f->Fill();
    }   
    t3->Print();
+   f.cd();
    t3->Write();
+   fr.cd();
+   t3f->Write();
 }
-      
+
+void tree3r()
+{
+   TFile *f = new TFile("tree3.root");
+   TTree *t3 = (TTree*)f->Get("t3");
+   t3->AddFriend("t3f","tree3f.root");
+   t3->Draw("pz","pt>3");
+}
+
+void tree3()
+{
+   tree3w();
+   tree3r();
+}      
