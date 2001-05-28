2eadb00b7a9b62a9015723955767117806fb0247
Rene Brun <Rene.Brun@cern.ch>
2001-05-28 06:26:27 +0000

    Redefine functions DrawClone, Delete, SetDrawOption to not appear in the context menu.
    git-svn-id: http://root.cern.ch/svn/root/trunk@2298 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/inc/TAxis.h
+++ hist/inc/TAxis.h
@@ -1,4 +1,4 @@
-// @(#)root/hist:$Name:  $:$Id: TAxis.h,v 1.8 2001/02/07 20:58:40 brun Exp $
+// @(#)root/hist:$Name:  $:$Id: TAxis.h,v 1.9 2001/02/28 07:51:56 brun Exp $
 // Author: Rene Brun   12/12/94
 
 /*************************************************************************
@@ -57,44 +57,47 @@ public:
         TAxis(Int_t nbins, const Axis_t *xbins);
         TAxis(const TAxis &axis);
         virtual ~TAxis();
-        virtual void    CenterTitle(Bool_t center=kTRUE);  // *MENU*
+        virtual void     CenterTitle(Bool_t center=kTRUE);  // *MENU*
         const char      *ChooseTimeFormat(Double_t axislength=0);
-        virtual void    Copy(TObject &axis);
-        virtual Int_t   DistancetoPrimitive(Int_t px, Int_t py);
-        virtual void    ExecuteEvent(Int_t event, Int_t px, Int_t py);
-        virtual Int_t   FindBin(Axis_t x);
-        virtual Int_t   FindFixBin(Axis_t x) const;
-        virtual Axis_t  GetBinCenter(Int_t bin) const;
-        virtual char  *GetBinLabel(Int_t bin) const;
-        virtual Axis_t  GetBinLowEdge(Int_t bin) const;
-        virtual Axis_t  GetBinUpEdge(Int_t bin) const;
-        virtual Axis_t  GetBinWidth(Int_t bin) const;
-        virtual void    GetCenter(Axis_t *center);
-        virtual void    GetLabel(char *label);
-        virtual void    GetLowEdge(Axis_t *edge);
-                Int_t   GetNbins() const { return fNbins; }
+        virtual void     Copy(TObject &axis);
+        virtual void     Delete(Option_t *option="") {;}
+        virtual Int_t    DistancetoPrimitive(Int_t px, Int_t py);
+        virtual TObject *DrawClone(Option_t *option="") const {return 0;}
+        virtual void     ExecuteEvent(Int_t event, Int_t px, Int_t py);
+        virtual Int_t    FindBin(Axis_t x);
+        virtual Int_t    FindFixBin(Axis_t x) const;
+        virtual Axis_t   GetBinCenter(Int_t bin) const;
+        virtual char    *GetBinLabel(Int_t bin) const;
+        virtual Axis_t   GetBinLowEdge(Int_t bin) const;
+        virtual Axis_t   GetBinUpEdge(Int_t bin) const;
+        virtual Axis_t   GetBinWidth(Int_t bin) const;
+        virtual void     GetCenter(Axis_t *center);
+        virtual void     GetLabel(char *label);
+        virtual void     GetLowEdge(Axis_t *edge);
+                Int_t    GetNbins() const { return fNbins; }
         virtual TObject *GetParent() const {return fParent;}
-        virtual Bool_t  GetTimeDisplay() const {return fTimeDisplay;}
+        virtual Bool_t   GetTimeDisplay() const {return fTimeDisplay;}
         virtual const char  *GetTimeFormat() const {return fTimeFormat.Data();}
-         const char    *GetTitle() const {return fTitle.Data();}
-              TArrayD  *GetXbins() {return &fXbins;}
-                 Int_t  GetFirst() const;
-                 Int_t  GetLast() const;
-                Axis_t  GetXmin() const {return fXmin;}
-                Axis_t  GetXmax() const {return fXmax;}
-        virtual void    RotateTitle(Bool_t rotate=kTRUE); // *MENU*
-        virtual void    SaveAttributes(ofstream &out, const char *name, const char *subname);
-        virtual void    Set(Int_t nbins, Axis_t xmin, Axis_t xmax);
-        virtual void    Set(Int_t nbins, const Float_t *xbins);
-        virtual void    Set(Int_t nbins, const Axis_t *xbins);
-        virtual void    SetBinLabel(Int_t bin, char *label);
-        virtual void    SetLabel(const char *label);
-        virtual void    SetLimits(Axis_t xmin, Axis_t xmax);
-        virtual void    SetParent(TObject *obj) {fParent = obj;}
-        virtual void    SetRange(Int_t first=0, Int_t last=0);  // *MENU*
-        virtual void    SetTimeDisplay(Int_t value) {fTimeDisplay = value;} // *TOGGLE*
-        virtual void    SetTimeFormat(const char *format="");  // *MENU*
-        virtual void    UnZoom();  // *MENU*
+         const char     *GetTitle() const {return fTitle.Data();}
+              TArrayD   *GetXbins() {return &fXbins;}
+                 Int_t   GetFirst() const;
+                 Int_t   GetLast() const;
+                Axis_t   GetXmin() const {return fXmin;}
+                Axis_t   GetXmax() const {return fXmax;}
+        virtual void     RotateTitle(Bool_t rotate=kTRUE); // *MENU*
+        virtual void     SaveAttributes(ofstream &out, const char *name, const char *subname);
+        virtual void     Set(Int_t nbins, Axis_t xmin, Axis_t xmax);
+        virtual void     Set(Int_t nbins, const Float_t *xbins);
+        virtual void     Set(Int_t nbins, const Axis_t *xbins);
+        virtual void     SetBinLabel(Int_t bin, char *label);
+        virtual void     SetDrawOption(Option_t *option="") {;}
+        virtual void     SetLabel(const char *label);
+        virtual void     SetLimits(Axis_t xmin, Axis_t xmax);
+        virtual void     SetParent(TObject *obj) {fParent = obj;}
+        virtual void     SetRange(Int_t first=0, Int_t last=0);  // *MENU*
+        virtual void     SetTimeDisplay(Int_t value) {fTimeDisplay = value;} // *TOGGLE*
+        virtual void     SetTimeFormat(const char *format="");  // *MENU*
+        virtual void     UnZoom();  // *MENU*
 
         ClassDef(TAxis,6)  //Axis class
 };
