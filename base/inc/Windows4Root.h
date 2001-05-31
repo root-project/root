4fa544ea58254b754d914177e741121ab130ee33
Fons Rademakers <Fons.Rademakers@cern.ch>
2001-05-31 10:04:30 +0000

    add some extra undef's for new TVirtualX methods.
    git-svn-id: http://root.cern.ch/svn/root/trunk@2335 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/Windows4Root.h
+++ base/inc/Windows4Root.h
@@ -1,4 +1,4 @@
-/* @(#)root/base:$Name$:$Id$ */
+/* @(#)root/base:$Name:  $:$Id: Windows4Root.h,v 1.1.1.1 2000/05/16 17:00:39 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -7,6 +7,7 @@
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
+
 #ifndef ROOT_Windows4Root
 #define ROOT_Windows4Root
 
@@ -14,20 +15,21 @@
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
 // This include file is necessary to solve a problem with the original  //
-//  Windows.h file from Microsoft                                       //
-// The native Windows.h redefines the following names:                  //
+// windows.h file from Microsoft.                                       //
+// The native windows.h redefines a.o. the following names:             //
 //     RemoveDirectory                                                  //
 //     GetClassName                                                     //
 //     GetTextAlign                                                     //
 //     GetTextColor                                                     //
 //                                                                      //
-//   This include file references the original Windows.h file           //
-//   and undefined these symbols in exit.                               //
+// This include file references the original windows.h file             //
+// and undefines these symbols.                                         //
+//                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
 #ifndef __CINT__
-#include <windows.h>
 
+#include <windows.h>
 
 
 #undef OpenSemaphore
@@ -42,68 +44,79 @@
 #undef UpdateWindow
 #undef SetClipRegion
 
+#undef ClearWindow
+#undef ClosePixmap
+#undef CloseWindow
+#undef CopyPixmap
+#undef DrawBox
+#undef DrawCellArray
+#undef DrawFillArea
+#undef DrawLine
+#undef DrawPolyLine
+#undef DrawPolyMarker
+#undef DrawText
+#undef GetCharacterUp
 
-    #undef      ClearWindow
-    #undef      ClosePixmap
-    #undef      CloseWindow
-    #undef      CopyPixmap
-    #undef      DrawBox
-    #undef      DrawCellArray
-    #undef      DrawFillArea
-    #undef      DrawLine
-    #undef      DrawPolyLine
-    #undef      DrawPolyMarker
-    #undef      DrawText
-    #undef      GetCharacterUp
+#undef GetDoubleBuffer
+#undef GetPixel
+#undef GetPlanes
+#undef GetRGB
+#undef GetTextExtent
+#undef InitWindow
+#undef AddWindow
+#undef RemoveWindow
+#undef MoveWindow
+#undef OpenPixmap
+#undef PutByte
+#undef QueryPointer
+#undef RescaleWindow
+#undef ResizePixmap
+#undef ResizeWindow
+#undef SelectWindow
+#undef SetCharacterUp
+#undef SetClipOFF
+#undef SetClipRegion
+#undef SetCursor
+#undef SetDrawMode
+#undef SetFillColor
+#undef SetFillStyle
+#undef SetLineColor
+#undef SetLineType
+#undef SetLineStyle
+#undef SetLineWidth
+#undef SetMarkerColor
+#undef SetMarkerSize
+#undef SetMarkerStyle
+#undef SetRGB
+#undef SetTextAlign
+#undef SetTextColor
+#undef SetTextFont
+#undef SetTextFont
+#undef SetTextSize
+#undef UpdateWindow
+#undef Warp
+#undef WritePixmap
+#undef CreateWindow
+#undef CreateRegion
+#undef DestroyRegion
+#undef UnionRectWithRegion
+#undef PolygonRegion
+#undef UnionRegion
+#undef IntersectRegion
+#undef SubtractRegion
+#undef XorRegion
+#undef EmptyRegion
+#undef PointInRegion
+#undef EqualRegion
+#undef GetRegionBox
 
-    #undef      GetDoubleBuffer
-    #undef      GetPixel
-    #undef      GetPlanes
-    #undef      GetRGB
-    #undef      GetTextExtent
-    #undef      InitWindow
-    #undef      MoveWindow
-    #undef      OpenPixmap
-    #undef      PutByte
-    #undef      QueryPointer
-    #undef      RescaleWindow
-    #undef      ResizePixmap
-    #undef      ResizeWindow
-    #undef      SelectWindow
-    #undef      SetCharacterUp
-    #undef      SetClipOFF
-    #undef      SetClipRegion
-    #undef      SetCursor
-    #undef      SetDrawMode
-    #undef      SetFillColor
-    #undef      SetFillStyle
-    #undef      SetLineColor
-    #undef      SetLineType
-    #undef      SetLineStyle
-    #undef      SetLineWidth
-    #undef      SetMarkerColor
-    #undef      SetMarkerSize
-    #undef      SetMarkerStyle
-    #undef      SetRGB
-    #undef      SetTextAlign
-    #undef      SetTextColor
-    #undef      SetTextFont
-    #undef      SetTextFont
-    #undef      SetTextSize
-    #undef      UpdateWindow
-    #undef      Warp
-    #undef      WritePixmap
-
-    #undef      CreateWindow
-
-#   ifndef ROOT_TGWin32Object
-#      undef GetObject
-#      undef GetClassInfo
-#   endif
+#ifndef ROOT_TGWin32Object
+#   undef GetObject
+#   undef GetClassInfo
+#endif
 
 #else
    typedef HANDLE void *
 #endif
-
 
 #endif
