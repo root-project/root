f2bb1d57ee68b432d74ed6dc80ef35816e330800
Rene Brun <Rene.Brun@cern.ch>
2001-07-04 16:36:07 +0000

    Minor change in Initialize. PrintFast called with 6 instead of 5 (one character was truncated).
    git-svn-id: http://root.cern.ch/svn/root/trunk@2523 27541ba8-7e3a-0410-8455-c3a389f83636

--- postscript/src/TPostScript.cxx
+++ postscript/src/TPostScript.cxx
@@ -1,4 +1,4 @@
-// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.11 2001/06/28 17:09:18 brun Exp $
+// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.12 2001/07/04 14:17:17 brun Exp $
 // Author: Rene Brun, Olivier Couet, Pierre Juillot   29/11/94
 
 /*************************************************************************
@@ -1092,13 +1092,13 @@ void TPostScript::DrawHatch(Float_t, Float_t, Int_t, Double_t *, Double_t *)
 }
 
 //______________________________________________________________________________
-// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.11 2001/06/28 17:09:18 brun Exp $
+// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.12 2001/07/04 14:17:17 brun Exp $
 // Author: P.Juillot   13/08/92
 void TPostScript::FontEncode()
 {
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*Font Reencoding*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 //*-*                          ================
-// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.11 2001/06/28 17:09:18 brun Exp $
+// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.12 2001/07/04 14:17:17 brun Exp $
 // Author: P.Juillot   13/08/92
 
   PrintStr("@/reencdict 24 dict def");
@@ -1800,7 +1800,7 @@ void TPostScript::SetFillPatterns(Int_t ipat, Int_t color)
 {
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*Patterns definition*-*-*-*-*-*-*-*-*-*-*-*-*
 //*-*                          ===================
-// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.11 2001/06/28 17:09:18 brun Exp $
+// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.12 2001/07/04 14:17:17 brun Exp $
 // Author: O.Couet   16/07/99
 //*-*
 //*-* Define the pattern ipat in the current PS file. ipat can vary from
@@ -3130,7 +3130,7 @@ void TPostScript::Zone()
 //*-*-              Picture Initialisation
 //*-*
   SaveRestore(1);
-  PrintFast(5," 0 0 t");
+  PrintFast(6," 0 0 t");
   fRed     = -1;
   fGreen   = -1;
   fBlue    = -1;
