f9381f4397d7f04844043182bdc32f37b80eff4e
Rene Brun <Rene.Brun@cern.ch>
2001-07-09 20:38:51 +0000

    Changes to take into account the new convention for split in MainEvent
    git-svn-id: http://root.cern.ch/svn/root/trunk@2549 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/MakeFiles.sh
+++ test/MakeFiles.sh
@@ -5,22 +5,20 @@ tracks=600
 comp=0
 action=1
 
-./Event $size $comp 0 $action $tracks 1
+./Event $size $comp 0 $action $tracks
 mv Event.root Event.new.split0.root
 
-./Event $size $comp 1 $action $tracks 1
+./Event $size $comp 1 $action $tracks
 mv Event.root Event.new.split1.root
 
-./Event $size $comp 9 $action $tracks 1
+./Event $size $comp 9 $action $tracks
 mv Event.root Event.new.split9.root
 
-./Event $size $comp 0 $action $tracks 0 
+./Event $size $comp -1 $action $tracks
 mv Event.root Event.old.streamed.root
 
-./Event $size $comp 1 $action $tracks 0
+./Event $size $comp -2 $action $tracks
 mv Event.root Event.old.split.root
 
 # Next step is to do something like
 # root -q 'MakeRef.C("Event.old.split.root");'
-
-
