208d40a02e86b2fb750375dbbef5ddbe6ced5592
Wouter Verkerke <verkerke@nikhef.nl>
2001-08-01 01:27:55 +0000

    o RooNameSet
       New class that serves as a 'sterile' RooArgSet, containing only
       the names of the ArgSet it is constructed from. Using the select(inputList)
       function, a RooArgSet containing the (sub)set of inputList that are
       named in the RooNameSet can be recreated.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@2621 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ roofitcore/inc/RooNameSet.h
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Project: BaBar detector at the SLAC PEP-II B-factory
+ * Package: RooFitCore
+ *    File: $Id$
+ * Authors:
+ *   DK, David Kirkby, Stanford University, kirkby@hep.stanford.edu
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu
+ * History:
+ *   07-Mar-2001 WV Created initial version
+ *
+ * Copyright (C) 2001 University of California
+ *****************************************************************************/
+#ifndef ROO_NAME_SET
+#define ROO_NAME_SET
+
+#include "TList.h"
+#include "TString.h"
+#include "TClass.h"
+#include "RooFitCore/RooAbsArg.hh"
+#include "RooFitCore/RooPrintable.hh"
+class RooArgSet ;
+
+class RooNameSet : public TList, public RooPrintable {
+public:
+
+  // Constructors, assignment etc.
+  RooNameSet();
+  RooNameSet(const RooArgSet& argSet);
+  RooNameSet(const RooNameSet& other) ;
+  virtual TObject* Clone(const char* newname=0) { return new RooNameSet(*this) ; }
+  ~RooNameSet() ;
+
+  void refill(const RooArgSet& argSet) ;
+  RooArgSet* select(const RooArgSet& input) ;
+  Bool_t operator==(const RooNameSet& other) ;
+
+  virtual void printToStream(ostream &os, PrintOption opt= Standard, TString indent= "") const;
+  inline virtual void Print(Option_t *options= 0) const {
+    printToStream(defaultStream(),parseOptions(options));
+  }
+
+  TList _nameList ;
+
+protected:
+
+  TIterator* _nameIter ; //! do not persist
+
+  ClassDef(RooNameSet,1) // A sterile version of RooArgSet, containing only the names of the contained RooAbsArgs
+};
+
+#endif
---
+++ roofitcore/src/RooNameSet.cxx
@@ -0,0 +1,123 @@
+/*****************************************************************************
+ * Project: BaBar detector at the SLAC PEP-II B-factory
+ * Package: RooFitCore
+ *    File: $Id$
+ * Authors:
+ *   DK, David Kirkby, Stanford University, kirkby@hep.stanford.edu
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu
+ * History:
+ *   07-Mar-2001 WV Created initial version
+ *
+ * Copyright (C) 2001 University of California
+ *****************************************************************************/
+
+
+#include "TObjString.h"
+#include "RooFitCore/RooNameSet.hh"
+#include "RooFitCore/RooArgSet.hh"
+
+ClassImp(RooNameSet)
+;
+
+RooNameSet::RooNameSet()
+{
+  _nameIter = _nameList.MakeIterator() ;
+}
+
+
+
+
+RooNameSet::RooNameSet(const RooArgSet& argSet)  
+{
+  refill(argSet) ;
+  _nameIter = _nameList.MakeIterator() ;
+}
+
+
+
+RooNameSet::RooNameSet(const RooNameSet& other) : _nameList()
+{
+  other._nameIter->Reset() ;
+  TObject* obj;
+  while(obj=other._nameIter->Next()) {
+    _nameList.Add(obj->Clone()) ;
+  }
+}
+
+
+void RooNameSet::refill(const RooArgSet& argSet) 
+{
+  _nameList.Delete() ;
+  TIterator* iter = argSet.MakeIterator() ;
+  RooAbsArg* arg ;
+  while(arg=(RooAbsArg*)iter->Next()) {    
+    _nameList.Add(new TObjString(arg->GetName())) ;
+  }
+  _nameList.Sort() ;
+
+  delete iter ;
+}
+
+
+
+RooNameSet::~RooNameSet() 
+{
+  delete _nameIter ;
+  _nameList.Delete() ;
+}
+
+
+
+
+RooArgSet* RooNameSet::select(const RooArgSet& input)
+{
+  RooArgSet* output = new RooArgSet ;
+  _nameIter->Reset() ;
+  TObjString* str ;
+  while(str=(TObjString*)_nameIter->Next()) {
+    RooAbsArg* arg = input.find(str->String()) ;
+    if (!arg) {
+      cout << "RooNameSet::select ERROR: no argument named " 
+	   << str->String() << " found in input data set" << endl ;
+      continue ;
+    }
+    output->add(*arg) ;
+  }
+}
+
+Bool_t RooNameSet::operator==(const RooNameSet& other) 
+{
+  // Check comparison againt self
+  if (&other==this) return kTRUE ;
+
+  // First check for equal length
+  if (_nameList.GetSize() != other._nameList.GetSize()) return kFALSE ;
+
+  // Then check for equal contents. Lists are sorted, just need to compare equal slots
+  _nameIter->Reset() ;
+  other._nameIter->Reset() ;
+  TObject *str1, *str2 ;
+  while(str1=_nameIter->Next()) {
+    str2 = other._nameIter->Next() ;
+    if (!str1->IsEqual(str2)) return kFALSE ;
+  }
+
+  return kTRUE ;
+}
+
+
+void RooNameSet::printToStream(ostream &os, PrintOption opt, TString indent) const{
+  TObjString* str ;
+  Bool_t first(kTRUE) ;
+  os << indent ;
+  _nameIter->Reset() ;
+  while(str=(TObjString*)_nameIter->Next()) {
+    if (first) { 
+      first=kFALSE ;
+    } else {
+      os << "," ;
+    }
+    os << str->String() ;
+  }
+  os << endl ;
+}
