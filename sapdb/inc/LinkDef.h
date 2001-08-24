db9580c5a1a6c30ed611d81a3abbbb738124ab2e
Fons Rademakers <Fons.Rademakers@cern.ch>
2001-08-24 16:41:19 +0000

    SQL interface to SAP DB (http://www.sapdb.org/). SAP DB is an open source (GPL) RDBMS that provides full DB functionality and can be easily used instead of Oracle. These classes are based on the ODBC call interface and are provided by Mark Hemberger (Marc.Hemberger@realtech.de).
    SAP DB is an open, SQL-based, relational database system that provides
    high availability and performance scaling from small to very large
    implementations. In addition, SAP DB goes beyond relational database
    technology by offering object orientation as well as support for managing
    unstructured data. It supports open standards including SQL, JDBC and ODBC;
    access from Perl and Python; and HTTP-based services with HTML or XML content.
    SAP DB (formerly known as ADABAS) has gone Open Source. SAP DB Version
    7.3.00 is available for Linux, Windows NT/Windows 2000, Solaris, HP-UX,
    Tru64, AIX. For those who don't want to pay for an Oracle-like database
    with comparable functionality (and better than MySQL ;-)
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@2738 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ sapdb/inc/LinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TSapDBServer;
+#pragma link C++ class TSapDBResult;
+#pragma link C++ class TSapDBRow;
+
+#endif
