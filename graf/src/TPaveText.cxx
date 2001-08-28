b8a4c34d6acf75cf201be6d606d1cc97fa9f972a
Rene Brun <Rene.Brun@cern.ch>
2001-08-28 16:31:24 +0000

    Pass correct type Int_t to PixeltoY
    git-svn-id: http://root.cern.ch/svn/root/trunk@2760 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf/src/TPaveText.cxx
+++ graf/src/TPaveText.cxx
@@ -1,4 +1,4 @@
-// @(#)root/graf:$Name:  $:$Id: TPaveText.cxx,v 1.8 2001/05/31 15:45:45 rdm Exp $
+// @(#)root/graf:$Name:  $:$Id: TPaveText.cxx,v 1.9 2001/06/05 11:32:59 rdm Exp $
 // Author: Rene Brun   20/10/95
 
 /*************************************************************************
@@ -426,7 +426,7 @@ void TPaveText::PaintPrimitives(Int_t mode)
    }
    Double_t yfont;
    if (GetTextFont()%10 > 2)
-      yfont = (gPad->PixeltoY(-textsize)-gPad->PixeltoY(0))/(y2-y1)*dy;
+      yfont = (gPad->PixeltoY(Int_t(-textsize))-gPad->PixeltoY(0))/(y2-y1)*dy;
    else
       yfont = textsize*dy;
    Double_t ytext = fY2 + 0.5*yspace;
