dbbdad34f7d580c9c0f5f54a95f09121189a5cfb
Rene Brun <Rene.Brun@cern.ch>
2001-09-26 11:18:39 +0000

    Correct stress6 for the modification in TProfile. The size of the Postscript file is now bigger.
    git-svn-id: http://root.cern.ch/svn/root/trunk@2864 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stress.cxx
+++ test/stress.cxx
@@ -1,4 +1,4 @@
-// @(#)root/test:$Name:  $:$Id: stress.cxx,v 1.23 2001/04/30 17:49:49 rdm Exp $
+// @(#)root/test:$Name:  $:$Id: stress.cxx,v 1.24 2001/05/31 09:27:29 brun Exp $
 // Author: Rene Brun   05/11/98
 
 /////////////////////////////////////////////////////////////////
@@ -1481,7 +1481,7 @@ void stress16()
    FILE *fp = fopen("stress_lhcb.ps","r");
    char line[260];
    Int_t nlines = 0;
-   Int_t nlinesGood = 2961;
+   Int_t nlinesGood = 3895;
    while (fgets(line,255,fp)) {
       nlines++;
    }
