ccdbb6e3539653be256e92747e2dc10fa7e8e88a
Fons Rademakers <Fons.Rademakers@cern.ch>
2001-10-02 10:13:25 +0000

    remove some comment to fakestrm for gcc3.
    git-svn-id: http://root.cern.ch/svn/root/trunk@2944 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/Module.mk
+++ cint/Module.mk
@@ -119,7 +119,6 @@ endif
 ifeq ($(GCCVERS),3)
 CINTS2       := $(filter-out $(MODDIRS)/libstrm.%,$(CINTS2))
 CINTS2       += $(MODDIRS)/gcc3strm.cxx
-#CINTS2       += $(MODDIRS)/fakestrm.cxx
 endif
 
 CINTS        := $(CINTS1) $(CINTS2)
