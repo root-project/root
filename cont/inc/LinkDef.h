412702219a6e86c4c3244a65eb18b311b26ad0f1
Rene Brun <Rene.Brun@cern.ch>
2001-10-03 19:55:27 +0000

    First version of TRefArray (not yet fully operational). Mainly to test portability of code. For more details on TRefArray, see comments in TRefArray.cxx.
    The TRefArray is expected to play an important role in ROOT I/O and Trees.
    The main advantage of TRefArray (see also TRef class) is to allow arrays
    of references to TObjects in different I/O transactions without writing
    the referenced objects each time.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@2965 27541ba8-7e3a-0410-8455-c3a389f83636

--- cont/inc/LinkDef.h
+++ cont/inc/LinkDef.h
@@ -1,4 +1,4 @@
-/* @(#)root/cont:$Name:  $:$Id: LinkDef.h,v 1.3 2001/02/08 16:58:21 brun Exp $ */
+/* @(#)root/cont:$Name:  $:$Id: LinkDef.h,v 1.4 2001/05/21 11:18:02 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -51,5 +51,7 @@
 #pragma link C++ class TSortedList;
 #pragma link C++ class TExMap;
 #pragma link C++ class TExMapIter;
+#pragma link C++ class TRefArray-;
+#pragma link C++ class TRefArrayIter;
 
 #endif
