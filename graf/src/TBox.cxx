2eca9733de79d686589307a035f54d0ba1e4b7c2
Fons Rademakers <Fons.Rademakers@cern.ch>
2001-11-11 15:37:09 +0000

    remove one blank in format in Print().
    git-svn-id: http://root.cern.ch/svn/root/trunk@3203 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf/src/TBox.cxx
+++ graf/src/TBox.cxx
@@ -1,4 +1,4 @@
-// @(#)root/graf:$Name:  $:$Id: TBox.cxx,v 1.5 2000/12/13 15:13:50 brun Exp $
+// @(#)root/graf:$Name:  $:$Id: TBox.cxx,v 1.6 2001/06/05 14:51:07 rdm Exp $
 // Author: Rene Brun   12/12/94
 
 /*************************************************************************
@@ -548,7 +548,7 @@ void TBox::Print(Option_t *) const
 //*-*-*-*-*-*-*-*-*-*-*Dump this box with its attributes*-*-*-*-*-*-*-*-*-*
 //*-*                  =================================
 
-   printf("%s  X1= %f Y1=%f X2=%f Y2=%f",IsA()->GetName(),fX1,fY1,fX2,fY2);
+   printf("%s  X1=%f Y1=%f X2=%f Y2=%f",IsA()->GetName(),fX1,fY1,fX2,fY2);
    if (GetLineColor() != 1) printf(" Color=%d",GetLineColor());
    if (GetLineStyle() != 1) printf(" Style=%d",GetLineStyle());
    if (GetLineWidth() != 1) printf(" Width=%d",GetLineWidth());
