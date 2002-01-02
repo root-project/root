ba81174754d5343be6c205626e94c066144fb24c
Rene Brun <Rene.Brun@cern.ch>
2002-01-02 21:48:52 +0000

    Rewrite this example to read the new data file cernstaff.dat instead of staff.dat
    git-svn-id: http://root.cern.ch/svn/root/trunk@3565 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/staff.C
+++ tutorials/staff.C
@@ -1,43 +1,44 @@
-{
-//   example of macro to read data from an ascii file and
-//   create a root file with an histogram and an ntuple.
-
-   gROOT->Reset();
-
+// example of macro to read data from an ascii file and
+// create a root file with a Tree.
+// see also a variant in cernbuild.C
+// Author: Rene Brun
+void staff() {
+   
    struct staff_t {
-                Int_t cat;
-                Int_t division;
-                Int_t flag;
-                Int_t age;
-                Int_t service;
-                Int_t children;
-                Int_t grade;
-                Int_t step;
-                Int_t nation;
-                Int_t hrweek;
-                Int_t cost;
+      Int_t           Category;
+      UInt_t          Flag;
+      Int_t           Age;
+      Int_t           Service;
+      Int_t           Children;
+      Int_t           Grade;
+      Int_t           Step;
+      Int_t           Hrweek;
+      Int_t           Cost;
+      Char_t          Division[4];
+      Char_t          Nation[3];
     };
 
    staff_t staff;
 
-   FILE *fp = fopen("staff.dat","r");
+   //The input file cern.dat is a copy of the CERN staff data base
+   //from 1988
+   FILE *fp = fopen("cernstaff.dat","r");
 
-   char line[81];
+   char line[80];
 
    TFile *f = new TFile("staff.root","RECREATE");
-   TTree *tree = new TTree("tree","staff data from ascii file");
-   tree->Branch("staff",&staff.cat,"cat/I:division:flag:age:service:children:grade:step:nation:hrweek:cost");
-    //note that the above branch declaration may also be replaced by
-    // staff_t *staff1=&staff;
-    // tree->Branch("staff","staff_t",&staff1);
+   TTree *tree = new TTree("T","staff data from ascii file");
+   tree->Branch("staff",&staff.Category,"Category/I:Flag:Age:Service:Children:Grade:Step:Hrweek:Cost");
+   tree->Branch("Division",staff.Division,"Division/C");
+   tree->Branch("Nation",staff.Nation,"Nation/C");
+    //note that the branches Division and Nation cannot be on the first branch
    while (fgets(&line,80,fp)) {
-      sscanf(&line[0] ,"%d%d%d%d", &staff.cat,&staff.division,&staff.flag,&staff.age);
-      sscanf(&line[13],"%d%d%d%d", &staff.service,&staff.children,&staff.grade,&staff.step);
-      sscanf(&line[24],"%d%d%d",   &staff.nation,&staff.hrweek,&staff.cost);
+      sscanf(&line[0],"%d %d %d %d %d",&staff.Category,&staff.Flag,&staff.Age,&staff.Service,&staff.Children);
+      sscanf(&line[32],"%d %d  %d %d %s %s",&staff.Grade,&staff.Step,&staff.Hrweek,&staff.Cost,staff.Division,staff.Nation);
       tree->Fill();
    }
    tree->Print();
+   tree->Write();
 
    fclose(fp);
-   f->Write();
 }
