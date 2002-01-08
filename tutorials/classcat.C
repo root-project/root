0cde7e8aad54d1f66d435cb1b369a383cc1baaf3
Rene Brun <Rene.Brun@cern.ch>
2002-01-08 09:43:11 +0000

    Add auto load of shared libs. Add new classes.
    git-svn-id: http://root.cern.ch/svn/root/trunk@3608 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/classcat.C
+++ tutorials/classcat.C
@@ -2,62 +2,75 @@
 // macro generating the Root class categories inheritance trees
 
    gROOT->Reset();
+   gSystem->Load("libProof");
+   gSystem->Load("libTreePlayer");
+   gSystem->Load("libTreeViewer");
+   gSystem->Load("libEG");
+   gSystem->Load("libEGPythia6");
+   gSystem->Load("libPainter");
+   gSystem->Load("libPhysics");
+   
    TCanvas c1("c1");
    TClassTree ct("ClassTree");
 
    //base classes
    ct.Draw("*TDirectory:*TCollection:TFree:TIter*:TObjLink:TStorage:*TKey:*TEnv:*TSystem:TSystem*:*TStopwatch:TDatime:TTim*TBenchmark*TRandom:TMath:TObjectTable:TStrin*");
-   c1.Print("html/gif/classcat_base.gif");
-   c1.Print("html/ps/classcat_base.ps");
+   c1.Print("classcat_base.gif");
+   c1.Print("classcat_base.ps");
 
    //container classes
    ct.Draw("*TDirectory:*TCollection:TFree:TIter*:TObjLink:TStorage:*TKey:TObjectTable");
-   c1.Print("html/gif/classcat_container.gif");
-   c1.Print("html/ps/classcat_container.ps");
+   c1.Print("classcat_container.gif");
+   c1.Print("classcat_container.ps");
 
    //histogram and minimisation classes
    ct.Draw("*TH1:*TFormula:TMinuit:TAxis");
-   c1.Print("html/gif/classcat_hist.gif");
-   c1.Print("html/ps/classcat_hist.ps");
+   c1.Print("classcat_hist.gif");
+   c1.Print("classcat_hist.ps");
 
    //tree classes
-   ct.Draw("*TTree:*TBranch:*TLeaf:*TKey:*TDirectory:TCut*:TEventList:TChain*");
-   c1.Print("html/gif/classcat_tree.gif");
-   c1.Print("html/ps/classcat_tree.ps");
+   ct.Draw("*TTree:*TBranch:*TLeaf:*TKey:*TDirectory:TCut*:TEventList:TChain*:TTreeFormula:*TSelector:TTree*");
+   c1.Print("classcat_tree.gif");
+   c1.Print("classcat_tree.ps");
 
    //2-d graphics classes
    ct.Draw("*TBox:TStyle:*TVirtualX:TGaxis:*TText:TPostScript:*TFormula:TColor:*TEllipse:*TLine:*TMarker:*TGraph:TPoly*");
-   c1.Print("html/gif/classcat_graf2d.gif");
-   c1.Print("html/ps/classcat_graf2d.ps");
+   c1.Print("classcat_graf2d.gif");
+   c1.Print("classcat_graf2d.ps");
+
+   //pad/canvas graphics classes
+   ct.Draw("*TVirtualPad");
+   c1.Print("classcat_gpad.gif");
+   c1.Print("classcat_gpad.ps");
 
    //3-d graphics classes
    ct.Draw("*TShape:*TNode:TGeometry:TLego:TView:TPoly*:TRotMatrix:*TMaterial");
-   c1.Print("html/gif/classcat_graf3d.gif");
-   c1.Print("html/ps/classcat_graf3d.ps");
+   c1.Print("classcat_graf3d.gif");
+   c1.Print("classcat_graf3d.ps");
 
    //GUI classes
    ct.Draw("*TGFrame");
-   c1.Print("html/gif/classcat_gui.gif");
-   c1.Print("html/ps/classcat_gui.ps");
+   c1.Print("classcat_gui.gif");
+   c1.Print("classcat_gui.ps");
 
    //Browser, viewer, inspector classes
    ct.Draw("*TBrowser:TTreeViewer:TClassTree");
-   c1.Print("html/gif/classcat_browse.gif");
-   c1.Print("html/ps/classcat_browse.ps");
+   c1.Print("classcat_browse.gif");
+   c1.Print("classcat_browse.ps");
 
    //meta classes
-   ct.Draw("*TDictionary:TTogg*:*TCint:TMethodCall:*TInterpreter:TClassTable:TObjectTable");
-   c1.Print("html/gif/classcat_meta.gif");
-   c1.Print("html/ps/classcat_meta.ps");
+   ct.Draw("*TDictionary:TTogg*:*TCint:TMethodCall:*TInterpreter:TClassTable:TObjectTable:TStream*");
+   c1.Print("classcat_meta.gif");
+   c1.Print("classcat_meta.ps");
 
    //net classes
    ct.Draw("TInetAddress:*TSocket:*TBuffer:*TMonitor:*TUrl:TNetFile:TWebFile");
-   c1.Print("html/gif/classcat_net.gif");
-   c1.Print("html/ps/classcat_net.ps");
+   c1.Print("classcat_net.gif");
+   c1.Print("classcat_net.ps");
 
    //linear algebra and matrix classes
    ct.Draw("TMatr*:TLazymatrix:*TVector");
-   c1.Print("html/gif/classcat_linalgebra.gif");
-   c1.Print("html/ps/classcat_linalgebra.ps");
+   c1.Print("classcat_linalgebra.gif");
+   c1.Print("classcat_linalgebra.ps");
 
 }
