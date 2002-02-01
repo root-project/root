e22aa8bd52275f32603c308783487b9ed4f2b97a
Rene Brun <Rene.Brun@cern.ch>
2002-02-01 07:12:37 +0000

    Add a missing parenthesis in one expression.
    git-svn-id: http://root.cern.ch/svn/root/trunk@3825 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/latex2.C
+++ tutorials/latex2.C
@@ -11,7 +11,7 @@
    l.SetTextSize(0.1);
    l.DrawLatex(0.5,0.95,"e^{+}e^{-}#rightarrowZ^{0}#rightarrowI#bar{I}, q#bar{q}");
    l.DrawLatex(0.5,0.75,"|#vec{a}#bullet#vec{b}|=#Sigmaa^{i}_{jk}+b^{bj}_{i}");
-   l.DrawLatex(0.5,0.5,"i(#partial_{#mu}#bar{#psi}#gamma^{#mu}+m#bar{#psi}=0#Leftrightarrow(#Box+m^{2})#psi=0");
+   l.DrawLatex(0.5,0.5,"i(#partial_{#mu}#bar{#psi}#gamma^{#mu}+m#bar{#psi})=0#Leftrightarrow(#Box+m^{2})#psi=0");
    l.DrawLatex(0.5,0.3,"L_{em}=eJ^{#mu}_{em}A_{#mu} , J^{#mu}_{em}=#bar{I}#gamma_{#mu}I , M^{j}_{i}=#SigmaA_{#alpha}#tau^{#alphaj}_{i}");
    c1->Print("latex2.ps");
 }
