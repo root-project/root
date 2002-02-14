ee855611eb8ad372e8e0a5b62b96b5fae423cee3
Rene Brun <Rene.Brun@cern.ch>
2002-02-14 18:04:15 +0000

    Several mods by Olivier Couet . The class TPostScript and TSVG derive from TVirtualPS. Both classes have several functions in common. These functions previously in TPostScript have been moved to TVirtualPS.
    git-svn-id: http://root.cern.ch/svn/root/trunk@3987 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/src/TVirtualPS.cxx
+++ base/src/TVirtualPS.cxx
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name$:$Id$
+// @(#)root/base:$Name:  $:$Id: TVirtualPS.cxx,v 1.1.1.1 2000/05/16 17:00:39 rdm Exp $
 // Author: Rene Brun   05/09/99
 
 /*************************************************************************
@@ -8,10 +8,20 @@
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
+ 
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TVirtualPS is an abstract interface to a Postscript and SVG drivers  //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
 
+#include <fstream.h>
+ 
 #include "TVirtualPS.h"
 
 TVirtualPS *gVirtualPS = 0;
+
+const Int_t  kMaxBuffer = 250;
 
 ClassImp(TVirtualPS)
 
@@ -23,22 +33,108 @@ ClassImp(TVirtualPS)
 //______________________________________________________________________________
 TVirtualPS::TVirtualPS()
 {
-//*-*-*-*-*-*-*-*-*-*-*VirtualPS default constructor*-*-*-*-*-*-*-*-*-*-*-*
-//*-*                  ==============================
+   // VirtualPS default constructor
 }
 
 //______________________________________________________________________________
 TVirtualPS::TVirtualPS(const char *name, Int_t)
           : TNamed(name,"Postscript interface")
 {
-//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*VirtualPS constructor*-*-*-*-*-*-*-*-*-*-*-*-*
-
+   // VirtualPS constructor
 }
 
 //______________________________________________________________________________
 TVirtualPS::~TVirtualPS()
 {
-//*-*-*-*-*-*-*-*-*-*-*VirtualPS destructor*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-//*-*                  =====================
+   // VirtualPS destructor
 }
 
+//______________________________________________________________________________
+void TVirtualPS::PrintStr(const char *str)
+{
+   // Output the string str in the output buffer
+
+   Int_t len = strlen(str);
+   if (len == 0) return;
+   if( str[0] == '@') {
+      if( fLenBuffer ) {
+         fStream->write(fBuffer, fLenBuffer);
+         fStream->write("\n",1);
+      }
+      if ( len < 2)  {
+         fBuffer[0] = ' ';
+      } else {
+         strcpy(fBuffer, str+1);
+      }
+      fLenBuffer = len-1;
+      fPrinted = kTRUE;
+      return;
+   }
+ 
+   if( str[len-1] == '@') {
+      if( fLenBuffer ) {
+         fStream->write(fBuffer, fLenBuffer);
+         fStream->write("\n",1);
+      }
+      fStream->write(str, len-1);
+      fStream->write("\n",1);
+      fLenBuffer = 0;
+      fPrinted = kTRUE;
+      return;
+   }
+ 
+   if( (len + fLenBuffer ) > kMaxBuffer) {
+      fStream->write(fBuffer, fLenBuffer);
+      fStream->write("\n",1);
+      strcpy(fBuffer, str);
+      fLenBuffer = len;
+   } else {
+      strcpy(fBuffer + fLenBuffer, str);
+      fLenBuffer += len;
+   }
+   fPrinted = kTRUE;
+}
+
+//______________________________________________________________________________
+void TVirtualPS::PrintFast(Int_t len, const char *str)
+{
+   // Fast version of Print
+
+   if( (len + fLenBuffer ) > kMaxBuffer) {
+      fStream->write(fBuffer, fLenBuffer);
+      fStream->write("\n",1);
+      strcpy(fBuffer, str);
+      fLenBuffer = len;
+   } else {
+      strcpy(fBuffer + fLenBuffer, str);
+      fLenBuffer += len;
+   }
+   fPrinted = kTRUE;
+}
+
+//______________________________________________________________________________
+void TVirtualPS::WriteInteger(Int_t n, Bool_t space )
+{
+   // Write one Integer to the file
+   //
+   // n: Integer to be written in the file.
+   // space: If TRUE, a space in written before the integer.
+
+   char str[15];
+   if (space) {
+      sprintf(str," %d", n);
+   } else {
+      sprintf(str,"%d", n);
+   }
+   PrintStr(str);
+}
+
+//______________________________________________________________________________
+void TVirtualPS::WriteReal(Float_t z)
+{
+   // Write a Real number to the file
+
+   char str[15];
+   sprintf(str," %g", z);
+   PrintStr(str);
+}
