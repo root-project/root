8aade47c7d6b84836705a19de76aa6a3bd496c94
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-02-14 16:12:52 +0000

    introduce new abstract class TVirtualMutex in base and move TLockGuard from thread to base. TMutex now derives from TVirtualMutex. Also add zero overhead macros to be used to place mutexes (zero overhead in case ROOT is not compiled with thread support). Added, example, global container mutex which should be initialized when libThread is loaded to point to a real mutex. To be done and tested by Mathieu de Naurois.
    git-svn-id: http://root.cern.ch/svn/root/trunk@3984 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ base/src/TVirtualMutex.cxx
@@ -0,0 +1,41 @@
+// @(#)root/base:$Name:$:$Id:$
+// Author: Fons Rademakers   14/02/2002
+
+/*************************************************************************
+ * Copyright (C) 1995-2002, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TVirtualMutex                                                        //
+//                                                                      //
+// This class implements a mutex interface. The actual work is done via //
+// TMutex which is available as soon as the thread library is loaded.   //
+//                                                                      //
+// and                                                                  //
+//                                                                      //
+// TLockGuard                                                           //
+//                                                                      //
+// This class provides mutex resource management in a guaranteed and    //
+// exception safe way. Use like this:                                   //
+// {                                                                    //
+//    TLockGuard guard(mutex);                                          //
+//    ... // do something                                               //
+// }                                                                    //
+// when guard goes out of scope the mutex is unlocked in the TLockGuard //
+// destructor. The exception mechanism takes care of calling the dtors  //
+// of local objects so it is exception safe.                            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#include "TVirtualMutex.h"
+
+ClassImp(TVirtualMutex)
+ClassImp(TLockGuard)
+
+
+TVirtualMutex *gContainerMutex = 0;
