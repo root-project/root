ac84e4f77fad7bfb37a62941d3cac70a29b39f19
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-02-22 10:45:31 +0000

    correct build procedure for libHbook on Windows.
    git-svn-id: http://root.cern.ch/svn/root/trunk@4061 27541ba8-7e3a-0410-8455-c3a389f83636

--- hbook/Module.mk
+++ hbook/Module.mk
@@ -20,7 +20,9 @@ HBOOKDH      := $(HBOOKDS:.cxx=.h)
 HBOOKH       := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 HBOOKS1      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 HBOOKS2      := $(MODDIRS)/hntvar2.f
-HBOOKO       := $(HBOOKS1:.cxx=.o) $(HBOOKS2:.f=.o)
+HBOOKO1      := $(HBOOKS1:.cxx=.o)
+HBOOKO2      := $(HBOOKS2:.f=.o)
+HBOOKO       := $(HBOOKO1) $(HBOOKO2)
 
 HBOOKDEP     := $(HBOOKS1:.cxx=.d) $(HBOOKDO:.o=.d)
 
@@ -37,10 +39,11 @@ INCLUDEFILES += $(HBOOKDEP)
 include/%.h:    $(HBOOKDIRI)/%.h
 		cp $< $@
 
-$(HBOOKLIB):    $(HBOOKO) $(HBOOKDO) $(MAINLIBS)
+$(HBOOKLIB):    $(HBOOKO) $(HBOOKDO) $(MAINLIBS) $(HBOOKLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libHbook.$(SOEXT) $@ "$(HBOOKO) $(HBOOKDO)" \
-		   "$(CERNLIBDIR) $(CERNLIBS) $(HBOOKLIBEXTRA) $(F77LIBS)"
+		   "$(SOFLAGS)" libHbook.$(SOEXT) $@ "$(HBOOKO1) $(HBOOKDO)" \
+		   "$(HBOOKO2) $(CERNLIBDIR) $(CERNLIBS) $(HBOOKLIBEXTRA) \
+		    $(F77LIBS)"
 
 $(HBOOKDS):     $(HBOOKH) $(HBOOKL) $(ROOTCINTTMP)
 		@echo "Generating dictionary $@..."
