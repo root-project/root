c860a62582131489a5da109aac0b072854cb8aba
Rene Brun <Rene.Brun@cern.ch>
2002-03-26 08:24:01 +0000

    New classes called by TTreeFormula
    git-svn-id: http://root.cern.ch/svn/root/trunk@4244 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ treeplayer/inc/TTreeFormulaManager.h
@@ -0,0 +1,83 @@
+// @(#)root/treeplayer:$Name:  $:$Id: $
+// Author: Philippe Canal   20/03/02
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers and al.        *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TTreeFormulaManager
+#define ROOT_TTreeFormulaManager
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TTreeFormulaManager                                                  //
+//                                                                      //
+// A class coordinating several TTreeFormula objects.                   //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifndef ROOT_TObjArray
+#include "TObjArray.h"
+#endif
+
+#include "TTreeFormula.h"
+
+class TArrayI;
+
+
+class TTreeFormulaManager : public TObject {
+private:
+   TObjArray   fFormulas;
+   Int_t       fMultiplicity;     // Indicator of the variability of the formula    
+   Bool_t      fMultiVarDim;      // True if one of the variable has 2 variable size dimensions.
+   Int_t       fNdata;            //! Last value calculated by GetNdata
+   
+   //the next line should be: mutable Int_t fCumulUsedSizes[kMAXFORMDIM+1]; See GetNdata()
+   Int_t       fCumulUsedSizes[kMAXFORMDIM+1];      //Accumulated size of lower dimensions as seen for this entry
+   TArrayI    *fCumulUsedVarDims;                   //fCumulUsedSizes(1) for multi variable dimensions case
+   //the next line should be: mutable Int_t fUsedSizes[kMAXFORMDIM+1]; See GetNdata()
+   Int_t       fUsedSizes[kMAXFORMDIM+1];           //Actual size of the dimensions as seen for this entry.
+   TArrayI    *fVarDims[kMAXFORMDIM+1];             //List of variable sizes dimensions.
+   Int_t       fVirtUsedSizes[kMAXFORMDIM+1];       //Virtual size of lower dimensions as seen for this formula
+
+   Bool_t      fNeedSync;         // Indicate whether a new formula has been added since the last synchronization
+
+   friend TTreeFormula;
+
+private:   
+   // Not implemented yet
+   TTreeFormulaManager(const TTreeFormulaManager&);
+   TTreeFormulaManager& operator=(const TTreeFormulaManager&);
+
+   // Deletion is the sole responsability of the TTreeFormulas
+   ~TTreeFormulaManager();
+
+protected:
+
+   virtual void       AddVarDims(Int_t virt_dim);
+   virtual void       CancelDimension(Int_t virt_dim);
+   virtual void       EnableMultiVarDims();
+   virtual void       UpdateFormulaLeaves();
+   virtual void       UpdateUsedSize(Int_t &virt_dim, Int_t vsize);
+
+public:
+   TTreeFormulaManager();
+
+   virtual void       Add(TTreeFormula*);
+   virtual Int_t      GetMultiplicity() const {return fMultiplicity;}
+   virtual Int_t      GetNdata();
+   virtual void       Remove(TTreeFormula*);
+   virtual bool       Sync();
+
+   ClassDef(TTreeFormulaManager,0) // A class coordinating several TTreeFormula objects.
+};
+
+
+#endif // ROOT_TTreeFormulaManager
+
