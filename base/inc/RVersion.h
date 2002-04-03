59d1018912bda47b2a97d5999ec45cbd51fdd26b
Rene Brun <Rene.Brun@cern.ch>
2002-04-03 10:44:59 +0000

    Stamp version 3.03/03
    git-svn-id: http://root.cern.ch/svn/root/trunk@4296 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "3.03/02"
-#define ROOT_VERSION_CODE 197378
+#define ROOT_RELEASE "3.03/03"
+#define ROOT_VERSION_CODE 197379
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
