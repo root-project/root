c9acb400e15f6bef6c78b25b030aa7bda673ca49
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-04-15 11:12:18 +0000

    remove include of not needed internal PostgreSQL header.
    git-svn-id: http://root.cern.ch/svn/root/trunk@4368 27541ba8-7e3a-0410-8455-c3a389f83636

--- pgsql/inc/TPgSQLResult.h
+++ pgsql/inc/TPgSQLResult.h
@@ -1,4 +1,4 @@
-// @(#)root/pgsql:$Name:$:$Id:$
+// @(#)root/pgsql:$Name:  $:$Id: TPgSQLResult.h,v 1.1 2001/06/14 23:19:15 rdm Exp $
 // Author: g.p.ciceri <gp.ciceri@acm.org> 01/06/2001
 
 /*************************************************************************
@@ -19,7 +19,6 @@
 #if !defined(__CINT__)
 #include <sys/time.h>
 #include <libpq-fe.h>
-#include <libpq-int.h>
 #else
 struct PGresult;
 #endif
