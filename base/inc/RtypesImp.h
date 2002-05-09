fc83d7f92157ff12f1e545150e633648aaf3c581
Rene Brun <Rene.Brun@cern.ch>
2002-05-09 20:32:27 +0000

    Forgot to add the new includes
    git-svn-id: http://root.cern.ch/svn/root/trunk@4499 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ base/inc/RtypesImp.h
@@ -0,0 +1,45 @@
+
+#ifndef G__DICTIONARY
+#error RtypesImp.h should only be included by ROOT dictionaries.
+#endif
+
+namespace ROOT {
+   const InitBehavior *DefineBehavior(void * /*parent_type*/,
+                                      void * /*actual_type*/);
+
+#if 0
+   template <class T> GenericClassInfo &GenerateInitInstance(const T*) {
+      ::Warning("GenerateInitInstance", "No Dictionary Created for this class");
+      static GenericClassInfo nullClassInfo("NoDict",0,"",0,typeid(T),0,0,0,0,0,0);
+      return nullClassInfo;
+   }
+#endif
+
+   //TClass* TObject_IsA(
+
+   inline void GenericShowMembers(const char *topClassName, 
+                                  void *obj, TMemberInspector &R__insp, char *R__parent) {
+      // This could be faster if we implemented this either as a templated function
+      // either by rootcint-generated code using the typeid (i.e. the difference is
+      // a lookup a in TList instead of in a map.
+      TClass *top = gROOT->GetClass(topClassName);
+      if (top) {
+         ShowMembersFunc_t show = top->GetShowMembersWrapper();
+         if (show) show(obj, R__insp, R__parent);
+      }
+   }
+
+
+#if 0
+   template <class T> const InitBehavior *ClassInfo<T>::fgAction       = 0;
+   template <class T> TClass             *ClassInfo<T>::fgClass        = 0;
+   template <class T> const char         *ClassInfo<T>::fgClassName    = 0;
+   template <class T> Int_t               ClassInfo<T>::fgVersion      = 0;
+   template <class T> const char         *ClassInfo<T>::fgImplFileName = 0;
+   template <class T> Int_t               ClassInfo<T>::fgImplFileLine = 0;
+   template <class T> const char         *ClassInfo<T>::fgDeclFileName = 0;
+   template <class T> Int_t               ClassInfo<T>::fgDeclFileLine = 0;
+   template <class T> typename ClassInfo<T>::ShowMembersFunc_t  ClassInfo<T>::fgShowMembers  = 0;
+#endif
+}
+
