071a0618698176d7c9eb76f5c5b864181df3a604
Rene Brun <Rene.Brun@cern.ch>
2002-05-18 11:02:49 +0000

    Add new includes just for coherency. They simply include the base class (do nothing)
    git-svn-id: http://root.cern.ch/svn/root/trunk@4568 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ hist/inc/TH2S.h
@@ -0,0 +1,28 @@
+// @(#)root/hist:$Name:  $:$Id: TH2S.h,v 1.32 2002/04/26 10:18:54 brun Exp $
+// Author: Rene Brun   18/05/2002
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TH2S
+#define ROOT_TH2S
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TH2S                                                                 //
+//                                                                      //
+// 2-Dim histogram with a short per channel                             //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ROOT_TH2
+#include "TH2.h"
+#endif
+
+#endif
