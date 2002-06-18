bfaeb148be30af76d4097beaab967195a786b478
Rene Brun <Rene.Brun@cern.ch>
2002-06-18 21:34:40 +0000

    Move to version 3.02/06
    git-svn-id: http://root.cern.ch/svn/root/trunk@4751 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "3.03/05"
-#define ROOT_VERSION_CODE 197381
+#define ROOT_RELEASE "3.03/06"
+#define ROOT_VERSION_CODE 197382
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
