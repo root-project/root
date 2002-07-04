fb9311ffee3fbe632e784c412cfb6da977f54553
Rene Brun <Rene.Brun@cern.ch>
2002-07-04 17:04:33 +0000

    Add a dummy include TLego.h including the new class TPainter3dAlgorithms and add a typedef TPainter3dAlgorithms TLego. TLego was used directly by a few applications, ef CDF event display.
    git-svn-id: http://root.cern.ch/svn/root/trunk@4820 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ histpainter/inc/TLego.h
@@ -0,0 +1,22 @@
+// @(#)root/histpainter:$Name:  $:$Id: TLego.h,v 1.4 2001/02/13 07:54:00 brun Exp $
+// Author: Rene Brun   26/12/94
+
+#ifndef ROOT_TLego
+#define ROOT_TLego
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TLego                                                                //
+//                                                                      //
+// This include provided only for back compatibility                    //
+// The class TLego has been renamed TPainter3dAlgorithms                //
+// Use TPainter3dAlgorithms.h instead                                   //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+#include "TPainter3dAlgorithms.h"
+typedef TPainter3dAlgorithms TLego;
+
+#endif
