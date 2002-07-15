2b8c4763231d2ff02352594d7f81a3d6662236cc
Rene Brun <Rene.Brun@cern.ch>
2002-07-15 10:46:45 +0000

    Remove unused function GetStats
    git-svn-id: http://root.cern.ch/svn/root/trunk@4909 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/inc/TVirtualHistPainter.h
+++ hist/inc/TVirtualHistPainter.h
@@ -1,4 +1,4 @@
-// @(#)root/hist:$Name:  $:$Id: TVirtualHistPainter.h,v 1.3 2001/12/09 17:33:49 brun Exp $
+// @(#)root/hist:$Name:  $:$Id: TVirtualHistPainter.h,v 1.4 2002/02/07 09:10:23 brun Exp $
 // Author: Rene Brun   30/08/99
 
 /*************************************************************************
@@ -28,7 +28,6 @@
 class TH1;
 class TF1;
 class TObjArray;
-class TPaveStats;
 
 class TVirtualHistPainter : public TObject {
 
@@ -45,7 +44,6 @@ public:
     virtual void       FitPanel() = 0;
     virtual char      *GetObjectInfo(Int_t px, Int_t py) const = 0;
     virtual TObjArray *GetStack() const = 0;
-    virtual TPaveStats*GetStats() const = 0;
     virtual void       Paint(Option_t *option="") = 0;
     virtual void       PaintStat(Int_t dostat, TF1 *fit) = 0;
     virtual void       SetHistogram(TH1 *h) = 0;
