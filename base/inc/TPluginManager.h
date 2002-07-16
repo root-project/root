4279a4cbe088cad332f8c44b69bf58b046cda7c0
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-07-16 13:57:14 +0000

    extended plugin manager that now supports also the specification of an ctor, to instantiate an object of the specified class, in the external file (typically rootrc). This ctor is called via TPluginHandler::ExecPlugin().
    git-svn-id: http://root.cern.ch/svn/root/trunk@4933 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/TPluginManager.h
+++ base/inc/TPluginManager.h
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name:  $:$Id: TPluginManager.h,v 1.1 2002/01/27 13:53:35 rdm Exp $
+// @(#)root/base:$Name:  $:$Id: TPluginManager.h,v 1.2 2002/01/27 15:53:40 rdm Exp $
 // Author: Fons Rademakers   26/1/2002
 
 /*************************************************************************
@@ -20,25 +20,37 @@
 // This class implements a plugin library manager. It keeps track of    //
 // a list of plugin handlers. A plugin handler knows which plugin       //
 // library to load to get a specific class that is used to extend the   //
-// functionality of a specific base class. For example, to extend the   //
-// base class TFile to be able to read RFIO files one needs to load     //
-// the plugin library libRFIO.so which defines the TRFIOFile class.     //
-// This loading should be triggered when a given URI contains a         //
-// regular expression defined by the handler. Handlers can be defined   //
-// for example as resources in the .rootrc file, e.g.:                  //
+// functionality of a specific base class and how to create an object   //
+// of this class. For example, to extend the base class TFile to be     //
+// able to read RFIO files one needs to load the plugin library         //
+// libRFIO.so which defines the TRFIOFile class. This loading should    //
+// be triggered when a given URI contains a regular expression defined  //
+// by the handler. Handlers can be defined for example as resources     //
+// in the .rootrc file, e.g.:                                           //
 //                                                                      //
-//   Plugin.TFile:       ^rfio:    TRFIOFile     RFIO                   //
-//   Plugin.TSQLServer:  ^mysql:   TMySQLServer  MySQL                  //
-//   +Plugin.TSQLServer: ^pgsql:   TPgSQLServer  PgSQL                  //
+//   Plugin.TFile:       ^rfio:   TRFIOFile    RFIO   "<constructor>"   //
+//   Plugin.TSQLServer:  ^mysql:  TMySQLServer MySQL  "<constructor>"   //
+//   +Plugin.TSQLServer: ^pgsql:  TPgSQLServer PgSQL  "<constructor>"   //
+//   Plugin.TVirtualFitter: *     TFitter      Minuit "TFitter(Int_t)"  //
+//                                                                      //
+// Where the + in front of Plugin.TSQLServer says that it extends the   //
+// existing definition of TSQLServer, usefull when there is more than   //
+// one plugin that can extend the same base class. The "<constructor>"  //
+// should be the constructor or static method that generates an         //
+// instance of the specified class. The * is a placeholder in case      //
+// there is no need for a URI to differentiate between different        //
+// plugins for the same base class. For the default plugins see         //
+// $ROOTSYS/etc/system.rootrc.                                          //
 //                                                                      //
 // Plugin handlers can also be registered at run time, e.g.:            //
 //                                                                      //
 //   gROOT->GetPluginManager()->AddHandler("TSQLServer", "^sapdb:",     //
-//                                         "TSapDBServer", "SapDB");    //
+//                                         "TSapDBServer", "SapDB",     //
+//             "TSapDBServer(const char*,const char*, const char*)");   //
 //                                                                      //
 // A list of currently defined handlers can be printed using:           //
 //                                                                      //
-//    gROOT->GetPluginManager()->Print();                               //
+//   gROOT->GetPluginManager()->Print(); // use option="a" to see ctors //
 //                                                                      //
 // The use of the plugin library manager removes all textual references //
 // to hard-coded class and library names and the resulting dependencies //
@@ -56,6 +68,7 @@
 
 class TEnv;
 class TList;
+class TMethodCall;
 class TPluginManager;
 
 
@@ -64,27 +77,35 @@ class TPluginHandler : public TObject {
 friend class TPluginManager;
 
 private:
-   TString  fBase;      // base class which will be extended by plugin
-   TString  fRegexp;    // regular expression which must be matched in URI
-   TString  fClass;     // class to be loaded from plugin library
-   TString  fPlugin;    // plugin library which should contain fClass
+   TString      fBase;      // base class which will be extended by plugin
+   TString      fRegexp;    // regular expression which must be matched in URI
+   TString      fClass;     // class to be loaded from plugin library
+   TString      fPlugin;    // plugin library which should contain fClass
+   TString      fCtor;      // ctor used to instantiate object of fClass
+   TMethodCall *fCallEnv;   //!ctor method call environment
+   TMethod     *fMethod;    //!ctor method
+   Int_t        fCanCall;   //!if 1 fCallEnv is ok, -1 fCallEnv is not ok
 
-   TPluginHandler() { }
+   TPluginHandler() { fCallEnv = 0; fCanCall = 0; }
    TPluginHandler(const char *base, const char *regexp,
-                  const char *className, const char *pluginName);
+                  const char *className, const char *pluginName,
+                  const char *ctor);
 
-   ~TPluginHandler() { }
+   ~TPluginHandler();
 
    const char *GetBase() const { return fBase; }
    const char *GetRegexp() const { return fRegexp; }
    const char *GetPlugin() const { return fPlugin; }
+   const char *GetCtor() const { return fCtor; }
 
    Bool_t CanHandle(const char *base, const char *uri);
+   void   SetupCallEnv();
 
 public:
    const char *GetClass() const { return fClass; }
    Int_t       CheckPlugin();
    Int_t       LoadPlugin();
+   Long_t      ExecPlugin(Int_t nargs, ...);
 
    ClassDef(TPluginHandler,1)  // Handler for plugin libraries
 };
@@ -101,10 +122,11 @@ public:
 
    void   LoadHandlersFromEnv(TEnv *env);
    void   AddHandler(const char *base, const char *regexp,
-                     const char *className, const char *pluginName);
+                     const char *className, const char *pluginName,
+                     const char *ctor);
    void   RemoveHandler(const char *base, const char *regexp = 0);
 
-   TPluginHandler *FindHandler(const char *base, const char *uri);
+   TPluginHandler *FindHandler(const char *base, const char *uri = 0);
 
    void   Print(Option_t *opt = "") const;
 
--- base/src/TPluginManager.cxx
+++ base/src/TPluginManager.cxx
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name:  $:$Id: TPluginManager.cxx,v 1.5 2002/02/23 10:35:56 brun Exp $
+// @(#)root/base:$Name:  $:$Id: TPluginManager.cxx,v 1.6 2002/05/13 10:39:43 rdm Exp $
 // Author: Fons Rademakers   26/1/2002
 
 /*************************************************************************
@@ -16,25 +16,37 @@
 // This class implements a plugin library manager. It keeps track of    //
 // a list of plugin handlers. A plugin handler knows which plugin       //
 // library to load to get a specific class that is used to extend the   //
-// functionality of a specific base class. For example, to extend the   //
-// base class TFile to be able to read RFIO files one needs to load     //
-// the plugin library libRFIO.so which defines the TRFIOFile class.     //
-// This loading should be triggered when a given URI contains a         //
-// regular expression defined by the handler. Handlers can be defined   //
-// for example as resources in the .rootrc file, e.g.:                  //
+// functionality of a specific base class and how to create an object   //
+// of this class. For example, to extend the base class TFile to be     //
+// able to read RFIO files one needs to load the plugin library         //
+// libRFIO.so which defines the TRFIOFile class. This loading should    //
+// be triggered when a given URI contains a regular expression defined  //
+// by the handler. Handlers can be defined for example as resources     //
+// in the .rootrc file, e.g.:                                           //
 //                                                                      //
-//   Plugin.TFile:       ^rfio:    TRFIOFile     RFIO                   //
-//   Plugin.TSQLServer:  ^mysql:   TMySQLServer  MySQL                  //
-//   +Plugin.TSQLServer: ^pgsql:   TPgSQLServer  PgSQL                  //
+//   Plugin.TFile:       ^rfio:   TRFIOFile    RFIO   "<constructor>"   //
+//   Plugin.TSQLServer:  ^mysql:  TMySQLServer MySQL  "<constructor>"   //
+//   +Plugin.TSQLServer: ^pgsql:  TPgSQLServer PgSQL  "<constructor>"   //
+//   Plugin.TVirtualFitter: *     TFitter      Minuit "TFitter(Int_t)"  //
+//                                                                      //
+// Where the + in front of Plugin.TSQLServer says that it extends the   //
+// existing definition of TSQLServer, usefull when there is more than   //
+// one plugin that can extend the same base class. The "<constructor>"  //
+// should be the constructor or static method that generates an         //
+// instance of the specified class. The * is a placeholder in case      //
+// there is no need for a URI to differentiate between different        //
+// plugins for the same base class. For the default plugins see         //
+// $ROOTSYS/etc/system.rootrc.                                          //
 //                                                                      //
 // Plugin handlers can also be registered at run time, e.g.:            //
 //                                                                      //
 //   gROOT->GetPluginManager()->AddHandler("TSQLServer", "^sapdb:",     //
-//                                         "TSapDBServer", "SapDB");    //
+//                                         "TSapDBServer", "SapDB",     //
+//             "TSapDBServer(const char*,const char*, const char*)");   //
 //                                                                      //
 // A list of currently defined handlers can be printed using:           //
 //                                                                      //
-//    gROOT->GetPluginManager()->Print();                               //
+//   gROOT->GetPluginManager()->Print(); // use option="a" to see ctors //
 //                                                                      //
 // The use of the plugin library manager removes all textual references //
 // to hard-coded class and library names and the resulting dependencies //
@@ -49,6 +61,12 @@
 #include "TROOT.h"
 #include "TList.h"
 #include "TOrdCollection.h"
+#include "Varargs.h"
+#include "TClass.h"
+#include "TMethod.h"
+#include "TMethodArg.h"
+#include "TDataType.h"
+#include "TMethodCall.h"
 
 ClassImp(TPluginHandler)
 ClassImp(TPluginManager)
@@ -56,7 +74,8 @@ ClassImp(TPluginManager)
 
 //______________________________________________________________________________
 TPluginHandler::TPluginHandler(const char *base, const char *regexp,
-                               const char *className, const char *pluginName)
+                               const char *className, const char *pluginName,
+                               const char *ctor)
 {
    // Create a plugin handler. Called by TPluginManager.
 
@@ -64,15 +83,30 @@ TPluginHandler::TPluginHandler(const char *base, const char *regexp,
    fRegexp  = regexp;
    fClass   = className;
    fPlugin  = pluginName;
+   fCtor    = ctor;
+   fCallEnv = 0;
+   fCanCall = 0;
+}
+
+//______________________________________________________________________________
+TPluginHandler::~TPluginHandler()
+{
+   // Cleanup plugin handler object.
+
+   delete fCallEnv;
 }
 
 //______________________________________________________________________________
 Bool_t TPluginHandler::CanHandle(const char *base, const char *uri)
 {
    // Check if regular expression appears in the URI, if so return kTRUE.
+   // If URI = 0 always return kTRUE.
 
    if (fBase != base)
       return kFALSE;
+
+   if (!uri)
+      return kTRUE;
 
    TRegexp re(fRegexp, kFALSE);
    TString ruri = uri;
@@ -80,6 +114,45 @@ Bool_t TPluginHandler::CanHandle(const char *base, const char *uri)
    if (ruri.Index(re) != kNPOS)
       return kTRUE;
    return kFALSE;
+}
+
+//______________________________________________________________________________
+void TPluginHandler::SetupCallEnv()
+{
+   // Setup ctor or static method call environment.
+
+   fCanCall = -1;
+
+   // check if class exists
+   TClass *cl = gROOT->GetClass(fClass);
+   if (!cl) {
+      Error("SetupCallEnv", "class %s not found in plugin %s", fClass.Data(),
+            fPlugin.Data());
+      return;
+   }
+
+   // split method and prototype strings
+   TString method = fCtor(0, fCtor.Index("("));
+   TString proto  = fCtor(fCtor.Index("(")+1, fCtor.Index(")")-fCtor.Index("(")-1);
+
+   fMethod = cl->GetMethodWithPrototype(method, proto);
+   if (!fMethod) {
+      Error("SetupCallEnv", "method %s not found in class %s", method.Data(),
+            fClass.Data());
+      return;
+   }
+
+   if (!(fMethod->Property() & kIsPublic)) {
+      Error("SetupCallEnv", "method %s in not public", method.Data());
+      return;
+   }
+
+   fCallEnv = new TMethodCall;
+   fCallEnv->InitWithPrototype(cl, method, proto);
+
+   fCanCall = 1;
+
+   return;
 }
 
 //______________________________________________________________________________
@@ -100,6 +173,86 @@ Int_t TPluginHandler::LoadPlugin()
    return gROOT->LoadClass(fClass, fPlugin);
 }
 
+//______________________________________________________________________________
+Long_t TPluginHandler::ExecPlugin(Int_t va_(nargs), ...)
+{
+   // Execute ctor for this plugin and return pointer to object of specific
+   // class. User must cast the returned long to the correct class.
+   // This method accepts a variable number of arguments to be passed
+   // to the ctor, where nargs is the number of arguments, followed
+   // by nargs arguments. Returns 0 in case of error.
+
+   if (fCtor.IsNull()) {
+      Error("ExecPlugin", "no ctor specified for this handler %s", fClass.Data());
+      return 0;
+   }
+
+   if (!fCallEnv && fCanCall == 0)
+      SetupCallEnv();
+
+   if (fCanCall == -1)
+      return 0;
+
+   if (nargs != fMethod->GetNargs()) {
+      Error("ExecPlugin", "nargs (%d) not consistent with expected number of arguments (%d)",
+            nargs, fMethod->GetNargs());
+      return 0;
+   }
+
+   Long_t *args = 0;
+
+   if (nargs > 0) {
+      TIter next(fMethod->GetListOfMethodArgs());
+      TMethodArg *arg;
+
+      va_list ap;
+      va_start(ap, va_(nargs));
+
+      args = new Long_t[nargs];
+
+      for (int i = 0; i < nargs; i++) {
+         union {
+           float f;
+           long  l;
+         } u;
+         arg = (TMethodArg*) next();
+         TString type = arg->GetFullTypeName();
+         TDataType *dt = gROOT->GetType(type);
+         if (dt)
+            type = dt->GetFullTypeName();
+         if (arg->Property() & (kIsPointer | kIsArray | kIsReference))
+            args[i] = (Long_t) va_arg(ap, void*);
+         else if (type == "bool")
+            args[i] = (Long_t) va_arg(ap, bool);
+         else if (type == "char" || type == "unsigned char")
+            args[i] = (Long_t) va_arg(ap, char);
+         else if (type == "short" || type == "unsigned short")
+            args[i] = (Long_t) va_arg(ap, short);
+         else if (type == "int" || type == "unsigned int")
+            args[i] = (Long_t) va_arg(ap, int);
+         else if (type == "long" || type == "unsigned long")
+            args[i] = (Long_t) va_arg(ap, long);
+         else if (type == "float") {
+            u.f = va_arg(ap, float);
+            args[i] = (Long_t) u.l;
+         } else if (type == "double") {
+            u.f = (float) va_arg(ap, double);
+            args[i] = (Long_t) u.l;
+         }
+      }
+
+      va_end(ap);
+
+      fCallEnv->SetParamPtrs(args);
+   }
+
+   Long_t ret;
+   fCallEnv->Execute(ret);
+
+   delete [] args;
+
+   return ret;
+}
 
 //______________________________________________________________________________
 TPluginManager::~TPluginManager()
@@ -113,9 +266,9 @@ TPluginManager::~TPluginManager()
 void TPluginManager::LoadHandlersFromEnv(TEnv *env)
 {
    // Load plugin handlers specified in config file, like:
-   //    Plugin.TFile:       ^rfio:    TRFIOFile      RFIO
-   //    Plugin.TSQLServer:  ^mysql:   TMySQLServer   MySQL
-   //    +Plugin.TSQLServer: ^pgsql:   TPgSQLServer   PgSQL
+   //    Plugin.TFile:       ^rfio:    TRFIOFile      RFIO  "TRFIOFile(...)"
+   //    Plugin.TSQLServer:  ^mysql:   TMySQLServer   MySQL "TMySQLServer(...)"
+   //    +Plugin.TSQLServer: ^pgsql:   TPgSQLServer   PgSQL "TPgSQLServer(...)"
    // The + allows the extension of an already defined resource (see TEnv).
 
    if (!env) return;
@@ -132,13 +285,15 @@ void TPluginManager::LoadHandlersFromEnv(TEnv *env)
             char *v = StrDup(val);
             s += 7;
             while (1) {
-               TString regexp = strtok(!cnt ? v : 0, ",; ");
+               TString regexp = strtok(!cnt ? v : 0, "; ");
                if (regexp.IsNull()) break;
-               TString clss   = strtok(0, ",; ");
+               TString clss   = strtok(0, "; ");
                if (clss.IsNull()) break;
-               TString plugin = strtok(0, ",; ");
+               TString plugin = strtok(0, "; ");
                if (plugin.IsNull()) break;
-               AddHandler(s, regexp, clss, plugin);
+               strtok(0, ";\"");
+               TString ctor = strtok(0, ";\"");
+               AddHandler(s, regexp, clss, plugin, ctor);
                cnt++;
             }
             delete [] v;
@@ -149,7 +304,8 @@ void TPluginManager::LoadHandlersFromEnv(TEnv *env)
 
 //______________________________________________________________________________
 void TPluginManager::AddHandler(const char *base, const char *regexp,
-                               const char *className, const char *pluginName)
+                                const char *className, const char *pluginName,
+                                const char *ctor)
 {
    // Add plugin handler to the list of handlers. If there is already a
    // handler defined for the same base and regexp it will be replaced.
@@ -162,7 +318,8 @@ void TPluginManager::AddHandler(const char *base, const char *regexp,
    // make sure there is no previous handler for the same case
    RemoveHandler(base, regexp);
 
-   TPluginHandler *h = new TPluginHandler(base, regexp, className, pluginName);
+   TPluginHandler *h = new TPluginHandler(base, regexp, className,
+                                          pluginName, ctor);
    fHandlers->Add(h);
 }
 
@@ -191,7 +348,8 @@ void TPluginManager::RemoveHandler(const char *base, const char *regexp)
 TPluginHandler *TPluginManager::FindHandler(const char *base, const char *uri)
 {
    // Returns the handler if there exists a handler for the specified URI.
-   // Returns 0 in case handler is not found.
+   // The uri can be 0 in which case the first matching plugin handler
+   // will be returned. Returns 0 in case handler is not found.
 
    if (!fHandlers) return 0;
 
@@ -207,15 +365,16 @@ TPluginHandler *TPluginManager::FindHandler(const char *base, const char *uri)
    }
 
    if (gDebug > 0)
-      Info("FindHandler", "did not find plugin for handling %s", uri);
+      Info("FindHandler", "did not find plugin for %s", h->GetClass());
 
    return 0;
 }
 
 //______________________________________________________________________________
-void TPluginManager::Print(Option_t *) const
+void TPluginManager::Print(Option_t *opt) const
 {
-   // Print list of registered plugin handlers.
+   // Print list of registered plugin handlers. If option is "a" print
+   // also the ctor's that will be used.
 
    if (!fHandlers) return;
 
@@ -223,15 +382,17 @@ void TPluginManager::Print(Option_t *) const
    TPluginHandler *h;
 
    printf("=====================================================================\n");
-   printf("Base               Regexp          Class              Plugin\n");
+   printf("Base                 Regexp        Class              Plugin\n");
    printf("=====================================================================\n");
 
    while ((h = (TPluginHandler*) next())) {
       const char *exist = "";
       if (h->CheckPlugin() == -1)
          exist = " [*]";
-      printf("%-18s %-15s %-18s %s%s\n", h->fBase.Data(), h->fRegexp.Data(),
+      printf("%-20s %-13s %-18s %s%s\n", h->fBase.Data(), h->fRegexp.Data(),
              h->fClass.Data(), h->fPlugin.Data(), exist);
+      if (strchr(opt, 'a'))
+         printf("  [Ctor: %s]\n", h->fCtor.Data());
    }
    printf("=====================================================================\n");
    printf("[*] plugin not available\n");
