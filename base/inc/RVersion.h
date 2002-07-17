c314d1a110e01c91b3ee631376be0bc45033bccc
Rene Brun <Rene.Brun@cern.ch>
2002-07-17 11:48:06 +0000

    Change version number to 3.03/07
    git-svn-id: http://root.cern.ch/svn/root/trunk@4954 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "3.03/06"
-#define ROOT_VERSION_CODE 197382
+#define ROOT_RELEASE "3.03/07"
+#define ROOT_VERSION_CODE 197383
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
