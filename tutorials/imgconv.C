99d452e1c2a50a4898e4282ea17064bec9d49a3e
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-08-11 09:35:33 +0000

    some more basic image handling examples.
    git-svn-id: http://root.cern.ch/svn/root/trunk@5113 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tutorials/imgconv.C
@@ -0,0 +1,16 @@
+void imgconv()
+{
+   // Open rose512.jpg and save it in the following formats:
+   //  .png, .gif, .xpm and tiff.
+
+   TImage *img = TImage::Open("rose512.jpg");
+   if (!img) {
+      printf("Could not create an image... exit\n");
+      return;
+   }
+
+   img->WriteImage("rose512.png");
+   img->WriteImage("rose512.gif");
+   img->WriteImage("rose512.xpm");
+   img->WriteImage("rose512.tiff");
+}
