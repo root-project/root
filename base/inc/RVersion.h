3c4191dfd795be23372a98a3277796605e53d888
Rene Brun <Rene.Brun@cern.ch>
2002-08-20 16:18:48 +0000

    Stamp version 3.03/08. We hope that this new version will be the PRO release.
    git-svn-id: http://root.cern.ch/svn/root/trunk@5166 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "3.03/07"
-#define ROOT_VERSION_CODE 197383
+#define ROOT_RELEASE "3.03/08"
+#define ROOT_VERSION_CODE 197384
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
