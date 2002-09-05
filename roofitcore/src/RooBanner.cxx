d293336885b90aaa890f4d2114ea85c0766922c3
Wouter Verkerke <verkerke@nikhef.nl>
2002-09-05 05:09:50 +0000

    o LICENSE
        - Minor formatting. Current version copied to roofit.sourceforge.net
    
      o RooBanner
    
        - Update copyright messages and web address.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@5213 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofitcore/src/RooBanner.cxx
+++ roofitcore/src/RooBanner.cxx
@@ -9,7 +9,7 @@ Int_t doBanner()
   cout << endl
        << "\033[1mRooFit -- Developed by Wouter Verkerke and David Kirkby\033[0m " << endl 
               << "          Copyright (C) 2001-2002 University of California & Stanford University" << endl 
-              << "          http://www.slac.stanford.edu/BFROOT/www/Computing/Offline/ROOT/RooFit" << endl << endl ;
+              << "          All rights reserved, please read http://roofit.sourceforge.net/license.txt" << endl << endl ;
   return 0 ;
 }
 
