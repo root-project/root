1b3adbbfbde379ba013d4675819fa185bed396e5
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-09-11 09:19:49 +0000

    more complete example for AliEn usage.
    git-svn-id: http://root.cern.ch/svn/root/trunk@5263 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/alien.C
+++ tutorials/alien.C
@@ -1,32 +1,57 @@
 void alien()
 {
-   TGrid *alien = TGrid::Connect("alien", gSystem->Getenv("USER"), "", "-domain=cern.ch");
+   TString testdir = "root-test3";
+   int nfiles      = 10;
+
+   // connect to AliEn
+   TGrid *alien = TGrid::Connect("alien", gSystem->Getenv("USER"), "",
+                                 "-domain=cern.ch");
    if (alien->IsZombie()) {
       delete alien;
       return;
    }
 
+   // get info on AliEn version
+   printf("Using AliEn version %s\n", alien->GetInfo());
+
+   // print current working directory
    printf("Current directory is %s\n", alien->Pwd());
 
+   // check if directory exists
    Long_t size, flags, modtime;
-   if (alien->GetPathInfo("root-test", &size, &flags, &modtime) == 0)
-      alien->Rmdir("root-test");
+   if (alien->GetPathInfo(testdir, &size, &flags, &modtime) == 0) {
+      // delete existing directory
+      alien->Rmdir(testdir);
+   }
 
-   alien->Mkdir("root-test");
+   // create a directory
+   alien->Mkdir(testdir);
 
-   alien->Cd("root-test");
+   // change directory
+   alien->Cd(testdir);
 
    printf("Current directory is %s\n", alien->Pwd());
 
+   // insert nfiles into the catalog
    Int_t i;
    char lfn[32], pfn[256];
-   for (i = 0; i < 10; i++) {
+   for (i = 0; i < nfiles; i++) {
       sprintf(lfn, "test-%d.root", i);
       sprintf(pfn, "rfio:/castor/cern.ch/user/r/rdm/mytest-%d.root", i);
       alien->AddFile(lfn, pfn, 1000000000);
    }
 
+   // list the contents of a directory
    alien->ls("", "l");
+
+   // get physical file name from lfn
+   for (i = 0; i < nfiles; i++) {
+      sprintf(lfn, "test-%d.root", i);
+      char *pf = alien->GetPhysicalFileName(lfn);
+      if (i == nfiles-1)
+         printf("last pfn retrieved is: %s\n", pf);
+      delete [] pf;
+   }
 
    delete alien;
 }
