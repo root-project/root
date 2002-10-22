961cfd27c397bc70a09d9b1b1d25c1650bcceaf7
Philippe Canal <pcanal@fnal.gov>
2002-10-22 10:08:23 +0000

    use new linkdef style
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@19 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/bigevent/EventLinkDef.h
+++ root/io/bigevent/EventLinkDef.h
@@ -1,8 +1,8 @@
 #ifdef __CINT__
 
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
+//#ppragma link off all globals;
+//#ppragma link off all classes;
+//#ppragma link off all functions;
 
 #pragma link C++ class EventHeader+;
 #pragma link C++ class Event+;
@@ -11,4 +11,6 @@
 #pragma link C++ class BigTrack+;
 #pragma link C++ class UShortVector+;
 
+//#ppragma link C++ class vector<TAxis*>;
+//#ppragma link C++ class pair<float,float>;
 #endif
