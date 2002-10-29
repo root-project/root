8a57b14b08a928d8f6f365718e329a510a240f1f
Rene Brun <Rene.Brun@cern.ch>
2002-10-29 08:25:54 +0000

    Undo changes introduced by mistake in 3.03/09 in functions ProjectionX and ProjectionY in the algorithm computing the errors for the projected histogram.
    git-svn-id: http://root.cern.ch/svn/root/trunk@5510 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/src/TH2.cxx
+++ hist/src/TH2.cxx
@@ -1,4 +1,4 @@
-// @(#)root/hist:$Name:  $:$Id: TH2.cxx,v 1.32 2002/09/10 21:21:46 brun Exp $
+// @(#)root/hist:$Name:  $:$Id: TH2.cxx,v 1.33 2002/09/17 08:31:47 brun Exp $
 // Author: Rene Brun   26/12/94
 
 /*************************************************************************
@@ -1292,12 +1292,13 @@ TH1D *TH2::ProjectionX(const char *name, Int_t firstybin, Int_t lastybin, Option
   if (pname != name)  delete [] pname;
 
 // Fill the projected histogram
-  Double_t cont,err2;
+  Double_t cont,err,err2;
   for (Int_t binx =0;binx<=nx+1;binx++) {
      err2 = 0;
      for (Int_t biny=firstybin;biny<=lastybin;biny++) {
         cont  = GetCellContent(binx,biny);
-        err2 += cont*cont;
+        err   = GetCellError(binx,biny);
+        err2 += err*err;
         if (cont) {
            h1->Fill(fXaxis.GetBinCenter(binx), cont);
         }
@@ -1365,12 +1366,13 @@ TH1D *TH2::ProjectionY(const char *name, Int_t firstxbin, Int_t lastxbin, Option
   if (pname != name)  delete [] pname;
 
 // Fill the projected histogram
-  Double_t cont,err2;
+  Double_t cont,err,err2;
   for (Int_t biny =0;biny<=ny+1;biny++) {
      err2 = 0;
      for (Int_t binx=firstxbin;binx<=lastxbin;binx++) {
         cont  = GetCellContent(binx,biny);
-        err2 += cont*cont;
+        err   = GetCellError(binx,biny);
+        err2 += err*err;
         if (cont) {
            h1->Fill(fYaxis.GetBinCenter(biny), cont);
         }
