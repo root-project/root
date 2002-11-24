5a4899b784babd4109a2a11a7fc12af39adf979b
Fons Rademakers <Fons.Rademakers@cern.ch>
2002-11-24 22:42:31 +0000

    OpenLDAP interface by Evgenia Smirnova and Oleksandr Grebenyuk of GSI. Several memory leaks removed from original code.
    git-svn-id: http://root.cern.ch/svn/root/trunk@5649 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ ldap/Module.mk
@@ -0,0 +1,65 @@
+# Module.mk for ldap module
+# Copyright (c) 2002 Rene Brun and Fons Rademakers
+#
+# Author: Fons Rademakers, 20/11/2002
+
+MODDIR       := ldap
+MODDIRS      := $(MODDIR)/src
+MODDIRI      := $(MODDIR)/inc
+
+LDAPDIR      := $(MODDIR)
+LDAPDIRS     := $(LDAPDIR)/src
+LDAPDIRI     := $(LDAPDIR)/inc
+
+##### libRLDAP #####
+LDAPL        := $(MODDIRI)/LinkDef.h
+LDAPDS       := $(MODDIRS)/G__LDAP.cxx
+LDAPDO       := $(LDAPDS:.cxx=.o)
+LDAPDH       := $(LDAPDS:.cxx=.h)
+
+LDAPH        := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+LDAPS        := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
+LDAPO        := $(LDAPS:.cxx=.o)
+
+LDAPDEP      := $(LDAPO:.o=.d) $(LDAPDO:.o=.d)
+
+LDAPLIB      := $(LPATH)/libRLDAP.$(SOEXT)
+
+# used in the main Makefile
+ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(LDAPH))
+ALLLIBS     += $(LDAPLIB)
+
+# include all dependency files
+INCLUDEFILES += $(LDAPDEP)
+
+##### local rules #####
+include/%.h:    $(LDAPDIRI)/%.h
+		cp $< $@
+
+$(LDAPLIB):     $(LDAPO) $(LDAPDO) $(MAINLIBS)
+		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
+		   "$(SOFLAGS)" libRLDAP.$(SOEXT) $@ "$(LDAPO) $(LDAPDO)" \
+		   "$(LDAPLIBEXTRA) $(LDAPLIBDIR) $(LDAPCLILIB)"
+
+$(LDAPDS):      $(LDAPH) $(LDAPL) $(ROOTCINTTMP)
+		@echo "Generating dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -c $(LDAPH) $(LDAPL)
+
+$(LDAPDO):      $(LDAPDS)
+		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
+
+all-ldap:       $(LDAPLIB)
+
+clean-ldap:
+		@rm -f $(LDAPO) $(LDAPDO)
+
+clean::         clean-ldap
+
+distclean-ldap: clean-ldap
+		@rm -f $(LDAPDEP) $(LDAPDS) $(LDAPDH) $(LDAPLIB)
+
+distclean::     distclean-ldap
+
+##### extra rules ######
+$(LDAPO): %.o: %.cxx
+	$(CXX) $(OPT) $(CXXFLAGS) -I$(LDAPINCDIR) -o $@ -c $<
