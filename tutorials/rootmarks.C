f17b5354b543e329dc1ec1892efbf5b229adfda5
Rene Brun <Rene.Brun@cern.ch>
2003-01-13 10:57:46 +0000

    rootmarks.C tuned
    git-svn-id: http://root.cern.ch/svn/root/trunk@5892 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/rootmarks.C
+++ tutorials/rootmarks.C
@@ -20,11 +20,11 @@
    Float_t hsimple_ct = gBenchmark->GetCpuTime("hsimple");
    if (hsimple_rt > 0) {
       if (batch) {
-         rtmark = norm*(0.27/hsimple_rt);
-         cpmark = norm*(0.27/hsimple_ct);
+         rtmark = norm*(0.29/hsimple_rt);
+         cpmark = norm*(0.28/hsimple_ct);
       } else {
-         rtmark = norm*(0.8/hsimple_rt);
-         cpmark = norm*(0.8/hsimple_ct);
+         rtmark = norm*(0.99/hsimple_rt);
+         cpmark = norm*(0.43/hsimple_ct);
       }
       printf("hsimple     = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -33,11 +33,11 @@
    Float_t hsum_ct = gBenchmark->GetCpuTime("hsum");
    if (hsum_rt > 0) {
       if (batch) {
-         rtmark = norm*(0.15/hsum_rt);
-         cpmark = norm*(0.13/hsum_ct);
+         rtmark = norm*(0.16/hsum_rt);
+         cpmark = norm*(0.15/hsum_ct);
       } else {
-         rtmark = norm*(1.25/hsum_rt);
-         cpmark = norm*(0.75/hsum_ct);
+         rtmark = norm*(0.99/hsum_rt);
+         cpmark = norm*(0.24/hsum_ct);
       }
       printf("hsum        = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -46,11 +46,11 @@
    Float_t fillrandom_ct = gBenchmark->GetCpuTime("fillrandom");
    if (fillrandom_rt > 0) {
       if (batch) {
-         rtmark = norm*(0.01/fillrandom_rt);
+         rtmark = norm*(0.02/fillrandom_rt);
          cpmark = norm*(0.01/fillrandom_ct);
       } else {
-         rtmark = norm*(0.21/fillrandom_rt);
-         cpmark = norm*(0.06/fillrandom_ct);
+         rtmark = norm*(0.48/fillrandom_rt);
+         cpmark = norm*(0.04/fillrandom_ct);
       }
       printf("fillrandom  = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -59,11 +59,11 @@
    Float_t fit1_ct = gBenchmark->GetCpuTime("fit1");
    if (fit1_rt > 0) {
       if (batch) {
-         rtmark = norm*(0.03/fit1_rt);
+         rtmark = norm*(0.04/fit1_rt);
          cpmark = norm*(0.03/fit1_ct);
       } else {
-         rtmark = norm*(0.20/fit1_rt);
-         cpmark = norm*(0.13/fit1_ct);
+         rtmark = norm*(0.13/fit1_rt);
+         cpmark = norm*(0.03/fit1_ct);
       }
       printf("fit1        = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -72,11 +72,11 @@
    Float_t tornado_ct = gBenchmark->GetCpuTime("tornado");
    if (tornado_rt > 0) {
       if (batch) {
-         rtmark = norm*(0.04/tornado_rt);
+         rtmark = norm*(0.05/tornado_rt);
          cpmark = norm*(0.04/tornado_ct);
       } else {
-         rtmark = norm*(0.20/tornado_rt);
-         cpmark = norm*(0.12/tornado_ct);
+         rtmark = norm*(0.11/tornado_rt);
+         cpmark = norm*(0.03/tornado_ct);
       }
       printf("tornado     = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -84,15 +84,15 @@
    Float_t na49_rt = gBenchmark->GetRealTime("na49");
    Float_t na49_ct = gBenchmark->GetCpuTime("na49");
    if (na49_rt > 0) {
-      rtmark = norm*(1.31/na49_rt);
-      cpmark = norm*(1.31/na49_ct);
+      rtmark = norm*(1.39/na49_rt);
+      cpmark = norm*(1.39/na49_ct);
       printf("na49        = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
 
    Float_t geometry_rt = gBenchmark->GetRealTime("geometry");
    Float_t geometry_ct = gBenchmark->GetCpuTime("geometry");
    if (geometry_rt > 0) {
-      rtmark = norm*(0.18/geometry_rt);
+      rtmark = norm*(0.19/geometry_rt);
       cpmark = norm*(0.18/geometry_ct);
       printf("geometry    = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -104,8 +104,8 @@
          rtmark = norm*(0.03/na49view_rt);
          cpmark = norm*(0.03/na49view_ct);
       } else {
-         rtmark = norm*(0.73/na49view_rt);
-         cpmark = norm*(0.18/na49view_ct);
+         rtmark = norm*(0.33/na49view_rt);
+         cpmark = norm*(0.05/na49view_ct);
       }
       printf("na49view    = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -114,11 +114,11 @@
    Float_t ntuple1_ct = gBenchmark->GetCpuTime("ntuple1");
    if (ntuple1_rt > 0) {
       if (batch) {
-         rtmark = norm*(0.23/ntuple1_rt);
-         cpmark = norm*(0.23/ntuple1_ct);
+         rtmark = norm*(0.29/ntuple1_rt);
+         cpmark = norm*(0.27/ntuple1_ct);
       } else {
-         rtmark = norm*(1.18/ntuple1_rt);
-         cpmark = norm*(0.88/ntuple1_ct);
+         rtmark = norm*(1.79/ntuple1_rt);
+         cpmark = norm*(0.28/ntuple1_ct);
       }
       printf("ntuple1     = %7.2f RealMARKS,  = %7.2f CpuMARKS\n",rtmark,cpmark);
    }
@@ -126,11 +126,11 @@
    if (rtall) {
       Float_t rtbrun, cpbrun;
       if (batch) {
-         rtbrun    = 2.25;
-         cpbrun    = 2.23;
+         rtbrun    = 2.41;
+         cpbrun    = 2.35;
       } else {
-         rtbrun    = 6.83;
-         cpbrun    = 2.43;
+         rtbrun    = 6.38;
+         cpbrun    = 2.72;
       }
       Float_t rootmarks = norm*(rtbrun+cpbrun)/(rtall+cpall);
       printf("\n");
