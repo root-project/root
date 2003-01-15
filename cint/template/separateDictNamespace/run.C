403d8bf8707a5dbefa9033bab15a1b084d3efeb0
Philippe Canal <pcanal@fnal.gov>
2003-01-15 17:24:18 +0000

    first import
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@39 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ cint/template/separateDictNamespace/run.C
@@ -0,0 +1,8 @@
+{
+gSystem->Load("libmaster");
+gROOT->ProcessLine(".class Master::Container");
+gSystem->Load("libslave1.so");
+gROOT->ProcessLine(".class Master::Container");
+gSystem->Load("libslave2.so");
+gROOT->ProcessLine(".class Master::Container");
+}
