875c1a7e2b244edd20dc2976b0bd541a7d5efe12
Rene Brun <Rene.Brun@cern.ch>
2003-01-31 11:21:50 +0000

    Increment version to 3.05/02
    git-svn-id: http://root.cern.ch/svn/root/trunk@6029 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "3.05/01"
-#define ROOT_VERSION_CODE 197889
+#define ROOT_RELEASE "3.05/02"
+#define ROOT_VERSION_CODE 197890
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
