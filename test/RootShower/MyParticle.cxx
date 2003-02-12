44386a26bfbb5eaa8340bc63689638da87724bf6
Rene Brun <Rene.Brun@cern.ch>
2003-02-12 08:10:55 +0000

    Fix in MyParticle: to circumvent an overloading problem on HP-UX Ambiguous overloaded function declaration; default arguments make this     function indistinguishable from previous one. Clashing function "MyParticle::MyParticle(int,int,int,int,const     TVector3 &,const TVector3 &)" was previously declared at ["MyParticle.h", line 45].     MyParticle::MyParticle(Int_t id, Int_t pType,Int_t pStat,Int_t pDecayType,co
    git-svn-id: http://root.cern.ch/svn/root/trunk@6105 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/RootShower/MyParticle.cxx
+++ test/RootShower/MyParticle.cxx
@@ -47,7 +47,7 @@ MyParticle::MyParticle() : TParticle()
 
 //______________________________________________________________________________
 MyParticle::MyParticle(Int_t id, Int_t pType,Int_t pStat,Int_t pDecayType,const TVector3 &pLocation,
-                       const TVector3 &pMomentum,Double_t pPassed = 0.0, Double_t pDecayLen = 0.0, 
+                       const TVector3 &pMomentum,Double_t pPassed, Double_t pDecayLen = 0.0, 
                        Double_t pEnergy = 0.0)
           : TParticle(pType, pStat, 1, 0, 0, 0, pMomentum.x(), pMomentum.y(), 
                         pMomentum.z(), pEnergy, pLocation.x(), pLocation.y(), 
