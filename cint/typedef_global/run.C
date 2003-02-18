c6e083650c327882a6f2bb14767c116595b5e088
Philippe Canal <pcanal@fnal.gov>
2003-02-18 17:51:24 +0000

    misc update
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@47 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/typedef_global/run.C
+++ cint/typedef_global/run.C
@@ -1,5 +1,5 @@
 {
 gSystem->Load("myclbad");
 gROOT->ProcessLine(".typedef MyClass<Toy>::value_type");
-gROOT->ProcessLine("gROOT->GetClass(\"MyClass<Toy>::value_type\")");
+if (gROOT->GetClass("MyClass<Toy>::value_type") == 0) fprintf(stdout,"Error MyClass<Toy>::value_type  not retrieveable by gROOT::GetClass\n");
 }
