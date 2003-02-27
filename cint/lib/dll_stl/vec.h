2b931d921bc4fedf3b1c304b0bb9407b045771ff
Rene Brun <Rene.Brun@cern.ch>
2003-02-27 20:26:13 +0000

    From Philippe fix  to compile the cintdlls with KCC
    git-svn-id: http://root.cern.ch/svn/root/trunk@6206 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/lib/dll_stl/vec.h
+++ cint/lib/dll_stl/vec.h
@@ -26,7 +26,7 @@ using namespace std;
 #pragma link C++ class vector<int>;
 #pragma link C++ class vector<long>;
 #pragma link C++ class vector<double>;
-#if (G__GNUC<3 || G__GNUC_MINOR<1) 
+#if (G__GNUC<3 || G__GNUC_MINOR<1) && !defined(G__KCC)
 // gcc3.1,3.2 has a problem with iterator<void*,...,void&>
 #pragma link C++ class vector<void*>;
 #endif
