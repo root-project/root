0ba09a115de36ae743937f6d1e02755508def3e2
Philippe Canal <pcanal@fnal.gov>
2003-02-27 20:46:23 +0000

    add a test for passing interpreted level std::string to compile code
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@50 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ cint/stl/string/run.C
@@ -0,0 +1,20 @@
+{
+gROOT->ProcessLine(".L t01.C+g");
+#include <string>
+
+string mystring("test string");
+
+if (!t01(mystring)) {
+   fprintf(stderr,"fail to pass by reference\n");
+   gSystem->Exit(1);
+}
+if (!t01val(mystring) ) {
+   fprintf(stderr,"fail to pass by value\n");
+   gSystem->Exit(1);
+}
+if (!t01p(&mystring) ) {
+   fprintf(stderr,"fail to pass by address\n");
+   gSystem->Exit(1);
+}
+
+}
