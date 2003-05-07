fdf6796d3fe203736e003b5079654913156630ac
Rene Brun <Rene.Brun@cern.ch>
2003-05-07 16:20:18 +0000

    Preset parameters for fitFcn to have the same result between Minuit and Fumili.
    git-svn-id: http://root.cern.ch/svn/root/trunk@6567 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/FittingDemo.C
+++ tutorials/FittingDemo.C
@@ -54,6 +54,7 @@ void FittingDemo() {
    // this results in an ok fit for the polynomial function
    // however the non-linear part (lorenzian) does not 
    // respond well.
+   fitFcn->SetParameters(1,1,1,1,1,1);
    histo->Fit("fitFcn","0");
    
    // second try: set start values for some parameters
