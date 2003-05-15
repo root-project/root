b910b9271993addf0c646f2542c6d511722b365f
Rene Brun <Rene.Brun@cern.ch>
2003-05-15 20:01:30 +0000

    Use Long64_t instead of long long for portability reasons. On Windows Long64_t is a typedef to __int64
    git-svn-id: http://root.cern.ch/svn/root/trunk@6637 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/src/TPluginManager.cxx
+++ base/src/TPluginManager.cxx
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name:  $:$Id: TPluginManager.cxx,v 1.17 2003/05/15 18:02:35 rdm Exp $
+// @(#)root/base:$Name:  $:$Id: TPluginManager.cxx,v 1.18 2003/05/15 19:41:38 brun Exp $
 // Author: Fons Rademakers   26/1/2002
 
 /*************************************************************************
@@ -274,7 +274,7 @@ Long_t TPluginHandler::ExecPlugin(Int_t va_(nargs), ...)
          else if (type == "long" || type == "unsigned long")
             args[i] = (Long_t) va_arg(ap, long);
          else if (type == "long long" || type == "unsigned long long")
-            args[i] = (Long_t) va_arg(ap, long long);
+            args[i] = (Long_t) va_arg(ap, Long64_t);
          else if (type == "float") {
             u.f = (Float_t) va_arg(ap, double);  // float is promoted to double
             args[i] = (Long_t) u.l;
