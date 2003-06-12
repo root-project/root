0b833ff72b75f26e443fbe7adb33f4f621c1964e
Philippe Canal <pcanal@fnal.gov>
2003-06-12 22:30:28 +0000

    add test for formula calling a function
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@71 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/treeformula/function/testcl.C
@@ -0,0 +1,27 @@
+class testcl {
+public:
+   static double calc(double x, double y) { return x*y; }
+   static TObject *crap(double x) {
+      return gROOT;
+   }
+   
+   static int stuff(double x) {
+      return *gROOT;
+   }
+};
+
+double mycalc(double t) {
+   return 2*t;
+};
+
+double mycalc(double t, double y) {
+   return y*t;
+};
+
+TObject *crap(double x) {
+   return gROOT;
+};
+
+TObject crap2(double x) {
+   return *gROOT;
+};
