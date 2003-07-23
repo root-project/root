a0d837c28b9ab84c08309de946dd89c00145f845
Philippe Canal <pcanal@fnal.gov>
2003-07-23 03:39:33 +0000

    embedded object test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@80 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/addresses/embread.C
@@ -0,0 +1,12 @@
+{
+gSystem->Load("test.dll");
+TFile *f    = new TFile("Embedded.root");
+TTree *tree = (TTree*)f->Get("T");
+Normal_objects* obj = new Normal_objects();
+TBranch *branch  = tree->GetBranch("B");
+branch->SetAddress(&obj);
+Int_t nevent = Int_t(tree->GetEntries());
+Int_t nselected = 0;
+Int_t nb = 0;
+for (Int_t i=0;i<nevent;i++) { printf("%d %d\n", tree->GetEvent(i), obj); obj->dump(); }
+}
---
+++ root/tree/addresses/embwrite.C
@@ -0,0 +1,20 @@
+{
+gSystem->Load("test.dll");
+TFile *f    = new TFile("Embedded.root", "RECREATE", "Root Embedded test");
+TTree *tree = new TTree("T","An example of a ROOT tree");
+Normal_objects* obj = new Normal_objects();
+TBranch *b = tree->Branch("B", "Normal_objects", &obj);
+
+for(int i=0; i < 10; ++i) {
+   obj->initData(i);
+   printf("%d\n",tree->Fill());
+}
+
+b->Print();
+b->Write();
+tree->Print();
+tree->Write();
+f->Print();
+f->Close();
+delete f;
+}
