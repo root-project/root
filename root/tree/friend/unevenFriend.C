39f4dee4cf73e8b0c68e8c5f9c4b14d56d92ea22
Philippe Canal <pcanal@fnal.gov>
2003-07-25 22:46:26 +0000

    port to windows
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@89 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/friend/unevenFriend.C
+++ root/tree/friend/unevenFriend.C
@@ -14,6 +14,7 @@ chain->AddFriend(friend,"friendglobal");
 chain->Draw("TrigL1CJT[0]","TrigL1CJT[0]==1",""); //doen't work
 
 if (htemp->GetMean()!=1) {
+   fprintf(stdout,"histogram must has wrong mean\n");
    gApplication->Terminate(1);
 } else {
    fprintf(stdout,"histogram must have been correctly created\n");
