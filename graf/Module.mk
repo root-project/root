499cb84a3f17b9bffb62327281a1b6554b14ad91
Fons Rademakers <Fons.Rademakers@cern.ch>
2003-08-29 10:45:08 +0000

    compile graf dictionaries with -g on alphacxx6 to work around compiler bug.
    git-svn-id: http://root.cern.ch/svn/root/trunk@7178 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf/Module.mk
+++ graf/Module.mk
@@ -56,10 +56,17 @@ $(GRAFDS2):     $(GRAFH) $(GRAFL2) $(ROOTCINTTMP)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(GRAFH) $(GRAFL2)
 
+ifneq ($(ARCH),alphacxx6)
 $(GRAFDO1):     $(GRAFDS1)
 		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
 $(GRAFDO2):     $(GRAFDS2) $(FREETYPELIB)
 		$(CXX) $(NOOPT) -I$(FREETYPEDIRI) $(CXXFLAGS) -I. -o $@ -c $<
+else
+$(GRAFDO1):     $(GRAFDS1)
+		$(CXX) $(DEBUGFLAGS) $(CXXFLAGS) -I. -o $@ -c $<
+$(GRAFDO2):     $(GRAFDS2) $(FREETYPELIB)
+		$(CXX) $(DEBUGFLAGS) -I$(FREETYPEDIRI) $(CXXFLAGS) -I. -o $@ -c $<
+endif
 
 all-graf:       $(GRAFLIB)
 
