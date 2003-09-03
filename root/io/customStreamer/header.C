370f2449192e7530c3dc9989bcdb577779bb2e53
Philippe Canal <pcanal@fnal.gov>
2003-09-03 17:01:20 +0000

    simple test for custom streamer
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@113 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/customStreamer/header.C
@@ -0,0 +1,17 @@
+#include "header.h"
+#include "Rtypes.h"
+
+#ifndef __CINT__
+RootClassVersion(Hard2Stream,2);
+#endif
+
+#ifdef __CINT__
+#pragma link C++ class Hard2Stream-;
+#endif
+
+#include <iostream>
+
+void Hard2Stream::print() {
+   std::cout << "Hard2Stream: " << (void*)this << std::endl;
+   std::cout << "val : " << getVal() << std::endl;
+}
