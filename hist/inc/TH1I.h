c5cb0ff8e7cc74fc89a072bc000b43136e553968
Rene Brun <Rene.Brun@cern.ch>
2003-09-08 12:50:23 +0000

    For consistency, add TH1I.h
    git-svn-id: http://root.cern.ch/svn/root/trunk@7230 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ hist/inc/TH1I.h
@@ -0,0 +1,28 @@
+// @(#)root/hist:$Name:  $:$Id: TH1I.h,v 1.1 2002/05/18 11:02:49 brun Exp $
+// Author: Rene Brun   08/09/2003
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TH1I
+#define ROOT_TH1I
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TH1I                                                                 //
+//                                                                      //
+// 1-Dim histogram with a 4 bits integer per channel                    //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ROOT_TH1
+#include "TH1.h"
+#endif
+
+#endif
