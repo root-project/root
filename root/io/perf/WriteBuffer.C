cc2dd557f359c2ad685fc703ecafd334fe294383
Philippe Canal <pcanal@fnal.gov>
2003-10-14 15:01:26 +0000

    add finer grain test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@129 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/perf/WriteBuffer.C
+++ root/io/perf/WriteBuffer.C
@@ -1,5 +1,6 @@
 #include "TBuffer.h"
 #include "TClass.h"
+#include "TStreamerInfo.h"
 
 class allints {
 public:
@@ -45,6 +46,64 @@ void WriteBuffer(int siz=10) {
 
 }
 
+void WriteBufferMix(int siz=10) {
+
+   TBuffer b(TBuffer::kWrite,32000);
+
+   floatint obj;
+   TClass *cl = gROOT->GetClass(typeid(obj));
+   
+   for(int i=0; i<siz; ++i) {
+      if (i % 1000000 == 0 ) b.Reset();
+      cl->Streamer(&obj,b);
+   }
+
+}
+
+void InfoWriteBuffer(int siz=10) {
+
+   TBuffer b(TBuffer::kWrite,32000);
+
+   allints obj;
+   char *pointer = (char*)&obj;
+   TClass *cl = gROOT->GetClass(typeid(obj));
+   TStreamerInfo *info = cl->GetStreamerInfo();
+   
+   for(int i=0; i<siz; ++i) {
+      if (i % 1000000 == 0 ) b.Reset();
+      // cl->Streamer(&obj,b);
+#if ROOT_VERSION_CODE<= 199169
+      info->WriteBuffer(b, (char*)(&obj),-1);
+#else
+      info->WriteBufferAux(b, &pointer,-1, 1, 0, 0);
+#endif
+   }
+
+}
+
+void InfoWriteBufferMix(int siz=10) {
+
+   TBuffer b(TBuffer::kWrite,32000);
+
+   floatint obj;
+   char *pointer = (char*)&obj;
+   TClass *cl = gROOT->GetClass(typeid(obj));
+   TStreamerInfo *info = cl->GetStreamerInfo();
+   
+   for(int i=0; i<siz; ++i) {
+      if (i % 1000000 == 0 ) b.Reset();
+      // cl->Streamer(&obj,b);
+#if ROOT_VERSION_CODE<= 199169
+      info->WriteBuffer(b, (char*)(&obj),-1);
+#else
+      info->WriteBufferAux(b, &pointer,-1, 1, 0, 0);
+#endif
+   }
+
+}
+
+
+
 #ifdef __MAKECINT__
 #pragma link C++ class allints+;
 #pragma link C++ class floatint+;
@@ -54,15 +113,23 @@ void WriteBuffer(int siz=10) {
 #ifndef __CINT__
 int main(int argc,char**argv) {
 
-   if (argc!=2) {
+   if (argc!=3) {
       fprintf(stderr,"WriteBuffer requires 1 argument:\n");
-      fprintf(stderr,"WriteBuffer <samplesize>\n");
+      fprintf(stderr,"WriteBuffer <test type> <samplesize>\n");
       return 1;
    }
+   
+   int kase = atoi(argv[1]);
+   int z = atoi(argv[2]);
 
-   int z = atoi(argv[1]);
-
-   WriteBuffer(z);
+   switch (kase) {
+      case 1: WriteBuffer(z); break;
+      case 2: InfoWriteBuffer(z); break; 
+         
+      case 11: WriteBufferMix(z); break;
+      case 12: InfoWriteBufferMix(z); break; 
+         
+   }
 
    return 0;
 }
