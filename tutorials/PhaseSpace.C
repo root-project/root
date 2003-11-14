376568e4890c128208101ee2cb341e874daa64a5
Rene Brun <Rene.Brun@cern.ch>
2003-11-14 22:34:14 +0000

    In gSystem->Load, replace libPhysics.so by libPhysics to get the script running on Windows and aix.
    git-svn-id: http://root.cern.ch/svn/root/trunk@7602 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/PhaseSpace.C
+++ tutorials/PhaseSpace.C
@@ -1,7 +1,7 @@
 {
 // example of use of TGenPhaseSpace by Valerio Filippini
    gROOT.Reset();
-   gSystem.Load("libPhysics.so");
+   gSystem.Load("libPhysics");
 
    TLorentzVector target(0.0, 0.0, 0.0, 0.938);
    TLorentzVector beam(0.0, 0.0, .65, .65);
