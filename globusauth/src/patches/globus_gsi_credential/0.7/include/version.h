c9bed5c3362b956812062061c0ad577659aff5b8
Fons Rademakers <Fons.Rademakers@cern.ch>
2003-11-18 19:40:08 +0000

    From Gerri: configure  o the definition of gsiversion had disappeared at some point    causing a problem in compilation because R__GLBS22 was not    defined; reintroduced.  o overall polishing of the globus part with cleaner way outs    in case of unsuccessful configuration
    README/README.GLOBUS
     o documentation updated
    
    The new files contain the header files needed by the patch so that
    the globus source tree is not needed.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@7615 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ globusauth/src/patches/globus_gsi_credential/0.7/include/version.h
@@ -0,0 +1,9 @@
+static
+globus_version_t local_version = 
+{
+    0,
+    7,
+    1044475376,
+    28
+};
+
---
+++ globusauth/src/patches/globus_gsi_credential/0.8/include/version.h
@@ -0,0 +1,9 @@
+static
+globus_version_t local_version = 
+{
+    0,
+    8,
+    1044475321,
+    1
+};
+
