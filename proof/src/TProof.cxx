e8251f5f72f092b4cda9768dee57e06fcd3ea128
Fons Rademakers <Fons.Rademakers@cern.ch>
2003-11-25 11:34:59 +0000

    use option "a" in Print() to print status of all slaves.
    git-svn-id: http://root.cern.ch/svn/root/trunk@7646 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/src/TProof.cxx
+++ proof/src/TProof.cxx
@@ -1,4 +1,4 @@
-// @(#)root/proof:$Name:  $:$Id: TProof.cxx,v 1.56 2003/09/27 19:08:50 rdm Exp $
+// @(#)root/proof:$Name:  $:$Id: TProof.cxx,v 1.57 2003/10/07 14:03:03 rdm Exp $
 // Author: Fons Rademakers   13/02/97
 
 /*************************************************************************
@@ -1182,7 +1182,7 @@ void TProof::Print(Option_t *option) const
       Printf("Total MB's processed:     %.2f", float(GetBytesRead())/(1024*1024));
       Printf("Total real time used (s): %.3f", GetRealTime());
       Printf("Total CPU time used (s):  %.3f", GetCpuTime());
-      if (GetNumberOfSlaves()) {
+      if (TString(option).Contains("a") && GetNumberOfSlaves()) {
          Printf("List of slaves:");
          fSlaves->ForEach(TSlave,Print)(option);
       }
