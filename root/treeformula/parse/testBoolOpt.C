1bceeeae70af1e9e14151ae08e6e5e3c41cc5e14
Philippe Canal <pcanal@fnal.gov>
2003-12-11 20:57:55 +0000

    add test for bool operation optimization
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@138 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/testBoolOpt.C
+++ root/treeformula/parse/testBoolOpt.C
@@ -11,29 +11,48 @@ bool left() {
 void testBoolOpt() {
    // .L testBoolOpt.C
 
+   TFormula * for = new TFormula("for","left() || right()");
+   for->Print();
+   TFormula * fand = new TFormula("fand","!left() && !right()");
+   fand->Print();
+
    fprintf(stderr,"regular C++: left() || right()\n");
    if ( left() || right() ) {}
 
-   TFormula * for = new TFormula("for","left() || right()");
-   for->Print();
    fprintf(stderr,"TFormula: left() || right()\n");
    for->Eval(0);
 
    fprintf(stderr,"regular C++: !left() && !right()\n");
    if ( !left() && !right() ) {}
 
-   TFormula * fand = new TFormula("fand","!left() && !right()");
-   fand->Print();
    fprintf(stderr,"TFormula: !left() && !right()\n");
    fand->Eval(0);
 
+   TFormula * for2 = new TFormula("for2","!left() || right()");
+
+   fprintf(stderr,"regular C++: !left() || right()\n");
+   if ( !left() || right() ) {}
+
+   fprintf(stderr,"TFormula: !left() || right()\n");
+   for2->Eval(0);
+   
+
+   TFormula * fand2 = new TFormula("fand2","left() && !right()");
+
+   fprintf(stderr,"regular C++: left() && !right()\n");
+   if ( left() && !right() ) {}
+
+   fprintf(stderr,"TFormula: left() && !right()\n");
+   fand2->Eval(0);
+
+   TFormula * fcomp = new TFormula("fcomp","left()+ (!left() && !right()) + right()");
 
    fprintf(stderr,"regular C++: left()+ (!left() && !right()) + right()\n");
    bool h = left() + ( !left() && !right() ) + right();
 
-   TFormula * fcomp = new TFormula("fcomp","left()+ (!left() && !right()) + right()");
-   fcomp->Print();
    fprintf(stderr,"TFormula: left()+ (!left() && !right()) + right()\n");
    fcomp->Eval(0);
+
+   
 
 }
