ec57dd9135889f7ce37b1c4c43b79d73fe9e38cf
Rene Brun <Rene.Brun@cern.ch>
2004-01-05 17:52:01 +0000

    Fix for VC++6
    git-svn-id: http://root.cern.ch/svn/root/trunk@7844 27541ba8-7e3a-0410-8455-c3a389f83636

--- tree/src/TLeafL.cxx
+++ tree/src/TLeafL.cxx
@@ -1,4 +1,4 @@
-// @(#)root/tree:$Name:  $:$Id: TLeafL.cxx,v 1.1 2003/12/19 13:40:26 brun Exp $
+// @(#)root/tree:$Name:  $:$Id: TLeafL.cxx,v 1.2 2003/12/19 16:10:18 brun Exp $
 // Author: Rene Brun   12/01/96
 
 /*************************************************************************
@@ -108,7 +108,7 @@ Double_t TLeafL::GetValue(Int_t i) const
 // if leaf is a simple type, i must be set to 0
 // if leaf is an array, i is the array element number to be returned
 
-   if (fIsUnsigned) return (Double_t)(ULong64_t)fValue[i];
+   if (fIsUnsigned) return (Double_t)((ULong64_t)fValue[i]);
    return fValue[i];
 }
 
