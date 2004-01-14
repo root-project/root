93e53f313ac18bbefbf152380e0952e6948505bc
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-01-14 08:45:25 +0000

    re-apply Maartens patch that got lost in Philippe's mega patch.
    git-svn-id: http://root.cern.ch/svn/root/trunk@7909 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/src/TProofServ.cxx
+++ proof/src/TProofServ.cxx
@@ -1,4 +1,4 @@
-// @(#)root/proof:$Name:  $:$Id: TProofServ.cxx,v 1.63 2003/12/30 13:16:51 brun Exp $
+// @(#)root/proof:$Name:  $:$Id: TProofServ.cxx,v 1.65 2004/01/10 10:52:30 brun Exp $
 // Author: Fons Rademakers   16/02/97
 
 /*************************************************************************
@@ -252,13 +252,16 @@ TProofServ::TProofServ(int *argc, char **argv)
    fOrdinal         = -1;
    fGroupId         = -1;
    fGroupSize       = 0;
-   fLogLevel        = 0;
+   fLogLevel        = gEnv->GetValue("Proof.DebugLevel",0);
    fRealTime        = 0.0;
    fCpuTime         = 0.0;
    fProof           = 0;
    fSocket          = new TSocket(0);
    fEnabledPackages = new TList;
    fEnabledPackages->SetOwner();
+
+   gProofDebugLevel = gEnv->GetValue("Proof.DebugLevel",0);
+   gProofDebugMask = (TProofDebug::EProofDebugMask) gEnv->GetValue("Proof.DebugMask",~0);
 
    GetOptions(argc, argv);
 
@@ -451,11 +454,16 @@ TDSetElement *TProofServ::GetNextPacket()
    req << fLatency.RealTime() << fCompute.RealTime() << fCompute.CpuTime();
 
    fLatency.Start();
-   fSocket->Send(req);
+   Int_t rc = fSocket->Send(req);
+   if (rc <= 0) {
+      Error("GetNextPacket","Send() failed, returned %d", rc);
+      return 0;
+   }
 
    TMessage *mess;
-   if (fSocket->Recv(mess) < 0) {
+   if ((rc = fSocket->Recv(mess)) <= 0) {
       fLatency.Stop();
+      Error("GetNextPacket","Recv) failed, returned %d", rc);
       return 0;
    }
 
@@ -942,6 +950,10 @@ void TProofServ::HandleSocketInput()
                   // add package to list of include directories to be searched
                   // by ACliC
                   gSystem->AddIncludePath(TString("-I") + package);
+
+                  // add package to list of include directories to be searched
+                  // by CINT
+                  gROOT->ProcessLine(TString(".include ") + package);
 
                   // if successful add to list and propagate to slaves
                   if (!status) {
