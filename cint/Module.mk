36e17482252ce912c9608250cd9f2039db8a1b7c
Rene Brun <Rene.Brun@cern.ch>
2004-01-23 21:35:56 +0000

    Frokm Philippe:
    using iccstrm and longif3
    for vc6 and vc70, iostream on the CINT prompt now works nicely on all MS compilers.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@7995 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/Module.mk
+++ cint/Module.mk
@@ -115,16 +115,16 @@ CINTS2       += $(MODDIRS)/fakestrm.cxx
 endif
 ifeq ($(PLATFORM),win32)
 CINTS1       += $(MODDIRS)/winnt.c
+CINTS2       := $(filter-out $(MODDIRS)/longif.%,$(CINTS2))
+CINTS2       += $(MODDIRS)/longif3.cxx
 ifeq ($(VC_MAJOR),13)
 ifeq ($(VC_MINOR),10)
 CINTS2       += $(MODDIRS)/vc7strm.cxx
-CINTS2       := $(filter-out $(MODDIRS)/longif.%,$(CINTS2))
-CINTS2       += $(MODDIRS)/longif3.cxx
 else
-CINTS2       += $(MODDIRS)/vcstrm.cxx
+CINTS2       += $(MODDIRS)/iccstrm.cxx
 endif
 else
-CINTS2       += $(MODDIRS)/vcstrm.cxx
+CINTS2       += $(MODDIRS)/iccstrm.cxx
 endif
 endif
 ifeq ($(PLATFORM),vms)
