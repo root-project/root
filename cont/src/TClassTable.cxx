e863c9a294e596ba7b688e6553c346a133b04c5d
Rene Brun <Rene.Brun@cern.ch>
2004-01-30 18:27:29 +0000

    Patch from Philippe fixing a problem reported by Axel Naumann.
    git-svn-id: http://root.cern.ch/svn/root/trunk@8095 27541ba8-7e3a-0410-8455-c3a389f83636

--- cont/src/TClassTable.cxx
+++ cont/src/TClassTable.cxx
@@ -1,4 +1,4 @@
-// @(#)root/cont:$Name:  $:$Id: TClassTable.cxx,v 1.24 2004/01/29 23:08:16 brun Exp $
+// @(#)root/cont:$Name:  $:$Id: TClassTable.cxx,v 1.25 2004/01/30 08:12:56 brun Exp $
 // Author: Fons Rademakers   11/08/95
 
 /*************************************************************************
@@ -508,7 +508,7 @@ void ROOT::RemoveClass(const char *cname)
      // We still keep the TClass object around because TFile needs to
      // get to the TStreamerInfo.
      if (gROOT && gROOT->GetListOfClasses()) {
-        TClass *cl = gROOT->GetClass(cname, kFALSE);
+        TClass *cl = dynamic_cast<TClass*>(gROOT->GetListOfClasses()->FindObject(cname));
         if (cl) cl->SetUnloaded();
      }
      TClassTable::Remove(cname);
