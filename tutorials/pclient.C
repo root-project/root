0d17fe2fbcaec45dbf31172308def4ab11339e12
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-02-04 10:13:06 +0000

    use by default localhost (customize ip address for real WAN tests).
    git-svn-id: http://root.cern.ch/svn/root/trunk@8116 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/pclient.C
+++ tutorials/pclient.C
@@ -11,7 +11,7 @@ void pclient(int niter = 100, int bsize = 500000, int nsocks = 5)
    // "localhost" in the TPSocket ctor below to the desried hostname.
 
    // Open connection to server
-   TPSocket *sock = new TPSocket("192.168.1.2", 9090, nsocks);
+   TPSocket *sock = new TPSocket("localhost", 9090, nsocks);
    //TPSocket *sock = new TPSocket("pcroot2", 9090, nsocks);
 
    char *buf = new char[bsize];
@@ -19,7 +19,7 @@ void pclient(int niter = 100, int bsize = 500000, int nsocks = 5)
 
    sock->Send(niter, bsize);
 
-   // send dat to server
+   // send data to server
    for (int i = 0; i < niter; i++) {
       int ret = sock->SendRaw(buf, bsize);
       if (ret < 0) {
