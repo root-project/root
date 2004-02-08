a8d636db68ddf6801873c40c368cbdf411471948
Rene Brun <Rene.Brun@cern.ch>
2004-02-08 15:46:38 +0000

    New class TRolke by Jan Conrad. See class documentation
    git-svn-id: http://root.cern.ch/svn/root/trunk@8153 27541ba8-7e3a-0410-8455-c3a389f83636

--- physics/inc/LinkDef.h
+++ physics/inc/LinkDef.h
@@ -1,4 +1,4 @@
-/* @(#)root/physics:$Name:  $:$Id: LinkDef.h,v 1.2 2000/09/08 16:42:12 brun Exp $ */
+/* @(#)root/physics:$Name:  $:$Id: LinkDef.h,v 1.3 2002/02/18 10:07:42 brun Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -32,6 +32,7 @@
 #pragma link C++ class TRotation+;
 #pragma link C++ class TGenPhaseSpace+;
 #pragma link C++ class TFeldmanCousins+;
+#pragma link C++ class TRolke+;
 
 #endif
 
