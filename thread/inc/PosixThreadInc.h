c83d96e3d3d69c993dc3820bc4fa5914a7092e3c
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-02-13 16:49:00 +0000

    compile on MacOS X by default also with pthreads, like on Linux.
    git-svn-id: http://root.cern.ch/svn/root/trunk@8198 27541ba8-7e3a-0410-8455-c3a389f83636

--- thread/inc/PosixThreadInc.h
+++ thread/inc/PosixThreadInc.h
@@ -1,4 +1,4 @@
-/* @(#)root/thread:$Name:  $:$Id: PosixThreadInc.h,v 1.4 2000/12/11 16:16:19 rdm Exp $ */
+/* @(#)root/thread:$Name:  $:$Id: PosixThreadInc.h,v 1.5 2001/04/03 10:40:24 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -32,7 +32,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#if defined(R__LINUX) || defined(R__SOLARIS) || defined(R__ALPHA)
+#if defined(R__LINUX) || defined(R__SOLARIS) || defined(R__ALPHA) || \
+    defined(R__MACOSX)
 #define PthreadDraftVersion 10
 #elif defined(R__HPUX) || defined(R__AIX)
 #define PthreadDraftVersion 4
