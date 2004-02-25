7d7b158b7447c8a7837c7e8548c4c0f809523693
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-02-25 08:49:54 +0000

    updated version of rlibmap with some suggestions from Axel. Win32 case still to be implemented.
    git-svn-id: http://root.cern.ch/svn/root/trunk@8302 27541ba8-7e3a-0410-8455-c3a389f83636

--- utils/src/rlibmap.cxx
+++ utils/src/rlibmap.cxx
@@ -14,14 +14,51 @@
 // Usage: rlibmap [-f] [-o <libmapfile>] <sofile> <sofile> ...
 // -f: output full library path name (not needed when ROOT library
 //     search path is used)
-// -o: output to write to specified file, otherwise to stdout
+// -o: write output to specified file, otherwise to stdout
+// -r: replace existing entries in the specified file
 
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <string>
 #include <map>
 
-const char *usage = "Usage: %s [-f] [-o <libmapfile>] <sofile> <sofile> ...\n";
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
+#endif
+#if (defined(__FreeBSD__) && (__FreeBSD__ < 4)) || \
+    (defined(__APPLE__) && (!defined(MAC_OS_X_VERSION_10_3) || \
+     (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_3)))
+#include <sys/file.h>
+#define lockf(fd, op, sz)   flock((fd), (op))
+#ifndef F_LOCK
+#define F_LOCK             (LOCK_EX | LOCK_NB)
+#endif
+#ifndef F_ULOCK
+#define F_ULOCK             LOCK_UN
+#endif
+#endif
+
+#if defined(__CYGWIN__) && defined(__GNUC__)
+#define F_LOCK F_WRLCK
+#define F_ULOCK F_UNLCK
+static int fcntl_lockf(int fd, int op, off_t off)
+{
+   flock fl;
+   fl.l_whence = SEEK_SET;
+   fl.l_start  = off;
+   fl.l_len    = 0;       // whole file
+   fl.l_pid    = getpid();
+   fl.l_type   = op;
+   return fcntl(fd, F_SETLK, &fl);
+}
+#define lockf fcntl_lockf
+#endif
+
+const char *usage = "Usage: %s [-f] [<-r|-o> <libmapfile>] <sofile> <sofile> ...\n";
 
 #if defined(__linux) || defined(__FreeBSD__)
 #if defined(__INTEL_COMPILER) || (__GNUC__ >= 3)
@@ -62,7 +99,60 @@ const char  kDefined = 'T';
 using namespace std;
 
 
-int libmap(const char *lib, int fullpath, FILE *fp)
+int removelibs(char **libs, bool fullpath, FILE *fp)
+{
+   // Remove entries from the map file for the specified libs.
+
+   fseek(fp, 0, SEEK_SET);
+
+   // get file size
+   struct stat sbuf;
+   fstat(fileno(fp), &sbuf);
+   size_t siz = sbuf.st_size;
+
+   // read file and remove lines matching specified libs
+   char *fbuf = new char[siz+1];
+   char *fptr = fbuf;
+
+   while (fgets(fptr, siz - size_t(fptr-fbuf), fp)) {
+      int i = 0;
+      while (libs[i]) {
+         const char *libbase = libs[i];
+         if (!fullpath) {
+            if ((libbase = strrchr(libs[i], '/')))
+               libbase++;
+            else
+               libbase = libs[i];
+         }
+
+         if (!strstr(fptr, libbase)) {
+            fptr += strlen(fptr);
+            if (*(fptr-1) != '\n') {
+               *fptr = '\n';
+               fptr++;
+            }
+         }
+
+         i++;
+      }
+   }
+
+   ftruncate(fileno(fp), 0);
+
+   // write remaining lines back
+   if (fptr != fbuf) {
+      fseek(fp, 0, SEEK_SET);
+      fwrite(fbuf, 1, size_t(fptr-fbuf), fp);
+   }
+
+   delete [] fbuf;
+
+   fseek(fp, 0, SEEK_END);
+
+   return 0;
+}
+
+int libmap(const char *lib, bool fullpath, FILE *fp)
 {
    // Write libmap. Returns -1 in case of error.
 
@@ -107,7 +197,13 @@ int libmap(const char *lib, int fullpath, FILE *fp)
             r--;
             if (r && *r == ':') {
                *r = 0;
-               string cls = symbol;
+               string cls;
+               // check for nested class or class in namespace or both
+               char *r1;
+               if ((r1 = strrchr(symbol, ':')) && *(r1-1) == ':')
+                  cls = r1+1;
+               else
+                  cls = symbol;
                r += 2;
                char *s;
                if ((s = strchr(r, '[')) || (s = strchr(r, '('))) {
@@ -129,11 +225,13 @@ int libmap(const char *lib, int fullpath, FILE *fp)
    fclose(pf);
 #endif
 
-   const char *libbase = strrchr(lib, '/');
-   if (libbase && !fullpath)
-      libbase++;
-   else
-      libbase = lib;
+   const char *libbase = lib;
+   if (!fullpath) {
+      if ((libbase = strrchr(lib, '/')))
+         libbase++;
+      else
+         libbase = lib;
+   }
 
    map<string,bool>::const_iterator it;
    for (it = unique.begin(); it != unique.end(); it++) {
@@ -148,18 +246,29 @@ int libmap(const char *lib, int fullpath, FILE *fp)
 int main(int argc, char **argv)
 {
    char **libs  = 0;
-   int fullpath = 0;
+   bool fullpath = false;
+   bool replace  = false;
    FILE *fp     = stdout;
 
    if (argc > 1) {
       int ic = 1;
       if (!strcmp(argv[ic], "-f")) {
-         fullpath = 1;
+         fullpath = true;
          ic++;
       }
       if (!strcmp(argv[ic], "-o")) {
          ic++;
          fp = fopen(argv[ic], "w");
+         if (!fp) {
+            fprintf(stderr, "cannot open output file %s\n", argv[ic]);
+            return 1;
+         }
+         ic++;
+      }
+      if (!strcmp(argv[ic], "-r")) {
+         replace = true;
+         ic++;
+         fp = fopen(argv[ic], "a+");
          if (!fp) {
             fprintf(stderr, "cannot open output file %s\n", argv[ic]);
             return 1;
@@ -181,10 +290,36 @@ int main(int argc, char **argv)
       return 1;
    }
 
+   if (replace) {
+#if !defined(WIN32) && !defined(__CYGWIN__)
+      // lock file
+      if (lockf(fileno(fp), F_LOCK, (off_t)1) == -1) {
+         fprintf(stderr, "rlibmap: error locking output file\n");
+         fclose(fp);
+         return 1;
+      }
+#endif
+
+      // remove entries for libs to be processed
+      removelibs(libs, fullpath, fp);
+   }
+
    int i = 0;
    while (libs[i]) {
       libmap(libs[i], fullpath, fp);
       i++;
+   }
+
+   if (replace) {
+#if !defined(WIN32) && !defined(__CYGWIN__)
+      // remove lock
+      lseek(fileno(fp), 0, SEEK_SET);
+      if (lockf(fileno(fp), F_ULOCK, (off_t)1) == -1) {
+         fprintf(stderr, "rlibmap: error unlocking output file\n");
+         fclose(fp);
+         return 1;
+      }
+#endif
    }
 
    if (fp != stdout)
