11b6b9f05a258ba2f99c19f090df6dc68675d073
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-03-12 16:07:34 +0000

    don't redefine common in case of MacOS X.
    git-svn-id: http://root.cern.ch/svn/root/trunk@8386 27541ba8-7e3a-0410-8455-c3a389f83636

--- eg/inc/Hepevt.h
+++ eg/inc/Hepevt.h
@@ -1,4 +1,4 @@
-/* @(#)root/eg:$Name:  $:$Id: Hepevt.h,v 1.2 2002/08/07 08:13:57 brun Exp $ */
+/* @(#)root/eg:$Name:  $:$Id: Hepevt.h,v 1.3 2004/03/12 08:35:53 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -7,8 +7,6 @@
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
-
-
 
 #ifndef ROOT_HepEvt
 #define ROOT_HepEvt
@@ -34,7 +32,9 @@ typedef struct {
 
 COMMON_BLOCK_DEF(HEPEVT_DEF,HEPEVT);
 
-//HEPEVT_DEF HEPEVT;
+#ifndef __APPLE__
+HEPEVT_DEF HEPEVT;
+#endif
 
 }
 
