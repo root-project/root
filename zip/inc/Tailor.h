e9b044a26221c89695962ff3b3b760b036786462
Rene Brun <Rene.Brun@cern.ch>
2004-03-17 18:05:57 +0000

    Replace C++ style comments by C style comments
    git-svn-id: http://root.cern.ch/svn/root/trunk@8421 27541ba8-7e3a-0410-8455-c3a389f83636

--- zip/inc/Tailor.h
+++ zip/inc/Tailor.h
@@ -1,4 +1,4 @@
-/* @(#)root/zip:$Name:  $:$Id: Tailor.h,v 1.1.1.1 2000/05/16 17:00:47 rdm Exp $ */
+/* @(#)root/zip:$Name:  $:$Id: Tailor.h,v 1.2 2004/03/17 17:34:01 brun Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -170,15 +170,17 @@
 #    include <unistd.h>
 #  endif
    typedef size_t extent;
-/// This definition of voidp is in conflict with the zlib one (zconf.h)
-/// voidp is used only in zlib code.
-///   typedef void voidp;
+/* This definition of voidp is in conflict with the zlib one (zconf.h)
+   voidp is used only in zlib code.
+   typedef void voidp;
+*/
 #else /* !MODERN */
    typedef unsigned int extent;
 #  define void int
-/// This definition of voidp is in conflict with the zlib one (zconf.h)
-/// voidp is used only in zlib code.
-///   typedef char voidp;
+/* This definition of voidp is in conflict with the zlib one (zconf.h)
+   voidp is used only in zlib code.
+   typedef char voidp;
+*/
 #endif /* ?MODERN */
 
 /* Get types and stat */
