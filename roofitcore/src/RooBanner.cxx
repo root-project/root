9afa7ce5aa7b46d5f5e33160b2f859dae755c054
Wouter Verkerke <verkerke@nikhef.nl>
2004-04-05 23:04:00 +0000

    o RooBanner
        - Encode version number 1.04 in banner
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@8539 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofitcore/src/RooBanner.cxx
+++ roofitcore/src/RooBanner.cxx
@@ -7,7 +7,7 @@
 Int_t doBanner()
 {
   cout << endl
-       << "\033[1mRooFit -- Developed by Wouter Verkerke and David Kirkby\033[0m " << endl 
+       << "\033[1mRooFit -- Version 1.04, developed by Wouter Verkerke and David Kirkby\033[0m " << endl 
               << "          Copyright (C) 2000-2004 University of California & Stanford University" << endl 
               << "          All rights reserved, please read http://roofit.sourceforge.net/license.txt" << endl << endl ;
   return 0 ;
