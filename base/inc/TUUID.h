acd96cc453696993beabe3404d35bebbc7ebd3c1
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-04-15 22:33:53 +0000

    add macro undefining GetCurrentTime on Win32.
    git-svn-id: http://root.cern.ch/svn/root/trunk@8592 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/TUUID.h
+++ base/inc/TUUID.h
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name:  $:$Id: TUUID.h,v 1.9 2002/08/05 12:07:19 rdm Exp $
+// @(#)root/base:$Name:  $:$Id: TUUID.h,v 1.10 2004/04/15 10:13:41 rdm Exp $
 // Author: Fons Rademakers   30/9/2001
 
 /*************************************************************************
@@ -27,6 +27,9 @@
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
+#ifdef WIN32
+#undef GetCurrentTime
+#endif
 #ifndef ROOT_Rtypes
 #include "Rtypes.h"
 #endif
