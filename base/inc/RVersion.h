a6a303be198501dfb14e82437aadddf4bdf25615
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-04-27 12:45:17 +0000

    Version 3.10/03 created on the v3-10-02-patches branch.
    git-svn-id: http://root.cern.ch/svn/root/branches/v3-10-02-patches@8710 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "3.10/02"
-#define ROOT_VERSION_CODE 199170
+#define ROOT_RELEASE "3.10/03"
+#define ROOT_VERSION_CODE 199171
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
