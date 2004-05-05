f73b5be8f015fbfbd4130005294642117b46c3df
Philippe Canal <pcanal@fnal.gov>
2004-05-05 15:18:45 +0000

    add a test on SetBranchAddress
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@198 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/cloning/run.C
+++ root/tree/cloning/run.C
@@ -1,81 +1,93 @@
-{
-  bool result = true;
+ {
+    bool result = true;
 
-  TChain *chain = new TChain("ntuple");
-  chain->Add("hsimple1.root");
-  chain->Add("hsimple2.root");
+    TChain *chain = new TChain("ntuple");
+    chain->Add("hsimple1.root");
+    chain->Add("hsimple2.root");
+    
+    TTree *tupleClone1 = (TTree*)chain->CopyTree("");
+    TTree *tupleClone2 = (TTree*)chain->CopyTree("");
+    
+    
+    TLeaf *origleaf  = chain->GetLeaf("pz");
+    TLeaf *clone1leaf = tupleClone1->GetLeaf("pz");
+    
+    if (origleaf->GetValuePointer() != clone1leaf->GetValuePointer()) {
+       cerr << "We have a problem since the address of the value is different in the original and in the first copy!" << endl;
+       result = false;
+    }
+    
+    TLeaf *clone2leaf = tupleClone2->GetLeaf("pz");
+    
+    if (origleaf->GetValuePointer() != clone2leaf->GetValuePointer()) {
+       cerr << "We have a problem since the address of the value is different in the original and in the second copy!" << endl;
+       result = false;
+    }
 
-  TTree *tupleClone1 = (TTree*)chain->CopyTree("");
-  TTree *tupleClone2 = (TTree*)chain->CopyTree("");
-
-
-  TLeaf *origleaf  = chain->GetLeaf("pz");
-  TLeaf *clone1leaf = tupleClone1->GetLeaf("pz");
-
-  if (origleaf->GetValuePointer() != clone1leaf->GetValuePointer()) {
-     cerr << "We have a problem since the address of the value is different in the original and in the first copy!" << endl;
-     result = false;
-  }
-
-  TLeaf *clone2leaf = tupleClone2->GetLeaf("pz");
-
-  if (origleaf->GetValuePointer() != clone2leaf->GetValuePointer()) {
-     cerr << "We have a problem since the address of the value is different in the original and in the second copy!" << endl;
-     result = false;
-  }
-
-  // We know all 3 are TNuple and pz is the 3rd 'arguments'
-
-  TNtuple *tuple = dynamic_cast<TNtuple*>(chain->GetTree());
-  if ( (&(tuple->GetArgs()[2])) != origleaf->GetValuePointer()) {
-     cerr << "Error: the original should own the memory\n";
-     result = false;
-  }
-
-  
-  tuple = dynamic_cast<TNtuple*>(tupleClone1);
-  if ( (&(tuple->GetArgs()[2])) == clone1leaf->GetValuePointer() ) {
-     cerr << "Error: the 1st clone should NOT own the memory\n";
-     result = false;
-  }
-
-  tuple = dynamic_cast<TNtuple*>(tupleClone2);
-  if ( (&(tuple->GetArgs()[2])) == clone2leaf->GetValuePointer() ) {
-     cerr << "Error: the 2nd clone should NOT own the memory\n";
-     result = false;
-  }
-
-  TTree *tupleClone3 = (TTree*)chain->GetTree()->CloneTree(0);
-  chain->LoadTree(0);
-  origleaf  = chain->GetLeaf("pz");
-  clone3leaf = tupleClone3->GetLeaf("pz");
-
-  if (origleaf->GetValuePointer() != clone3leaf->GetValuePointer()) {
-     cerr << "We have a problem since the address of the value is different in the original and in the thrid copy!" << endl;
-     result = false;
-  }
-
-
-  delete chain;
-
-  clone1leaf = tupleClone1->GetLeaf("pz");
-
-  clone2leaf = tupleClone2->GetLeaf("pz");
-
-  tuple = dynamic_cast<TNtuple*>(tupleClone1);
-  if ( (&(tuple->GetArgs()[2])) != clone1leaf->GetValuePointer() ) {
-     cerr << "Error: the 1st clone should own the memory\n";
-     result = false;
-  }
-
-  tuple = dynamic_cast<TNtuple*>(tupleClone2);
-  if ( (&(tuple->GetArgs()[2])) != clone2leaf->GetValuePointer() ) {
-     cerr << "Error: the 2nd clone should own the memory\n";
-     result = false;
-  }
-  if (!result) gApplication->Terminate(1);
+    // We know all 3 are TNuple and pz is the 3rd 'arguments'
+    
+    TNtuple *tuple = dynamic_cast<TNtuple*>(chain->GetTree());
+    if ( (&(tuple->GetArgs()[2])) != origleaf->GetValuePointer()) {
+       cerr << "Error: the original should own the memory\n";
+       result = false;
+    }
 
   
+    tuple = dynamic_cast<TNtuple*>(tupleClone1);
+    if ( (&(tuple->GetArgs()[2])) == clone1leaf->GetValuePointer() ) {
+       cerr << "Error: the 1st clone should NOT own the memory\n";
+       result = false;
+    }
+
+    tuple = dynamic_cast<TNtuple*>(tupleClone2);
+    if ( (&(tuple->GetArgs()[2])) == clone2leaf->GetValuePointer() ) {
+       cerr << "Error: the 2nd clone should NOT own the memory\n";
+       result = false;
+    }
+    
+    TTree *tupleClone3 = (TTree*)chain->GetTree()->CloneTree(0);
+    chain->LoadTree(0);
+    origleaf  = chain->GetLeaf("pz");
+    clone3leaf = tupleClone3->GetLeaf("pz");
+    
+    if (origleaf->GetValuePointer() != clone3leaf->GetValuePointer()) {
+       cerr << "We have a problem since the address of the value is different in the original and in the third copy!" << endl;
+       result = false;
+    }
+    
+    float py;
+    chain->SetBranchAddress("py",&py);
+    origleaf  = chain->GetLeaf("py");
+    clone1leaf = tupleClone1->GetLeaf("py");
+    clone2leaf = tupleClone2->GetLeaf("py");
+    clone3leaf = tupleClone3->GetLeaf("py");
+
+    if (   origleaf->GetValuePointer() != clone1leaf->GetValuePointer()
+        || origleaf->GetValuePointer() != clone2leaf->GetValuePointer()
+           || origleaf->GetValuePointer() != clone3leaf->GetValuePointer() ) {
+
+       cerr << "We have a problem since the address of the value is different in the original and in one of the three copy!" << endl;
+       result = false;
+    }  
 
 
+    delete chain;
+    
+    clone1leaf = tupleClone1->GetLeaf("pz");
+    
+    clone2leaf = tupleClone2->GetLeaf("pz");
+    
+    tuple = dynamic_cast<TNtuple*>(tupleClone1);
+    if ( (&(tuple->GetArgs()[2])) != clone1leaf->GetValuePointer() ) {
+       cerr << "Error: the 1st clone should own the memory\n";
+       result = false;
+    }
+    
+    tuple = dynamic_cast<TNtuple*>(tupleClone2);
+    if ( (&(tuple->GetArgs()[2])) != clone2leaf->GetValuePointer() ) {
+       cerr << "Error: the 2nd clone should own the memory\n";
+       result = false;
+    }
+    
+    if (!result) gApplication->Terminate(1);
 }
