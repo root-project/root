3899852ec12aa1412bd1f646e5e0a769659fd38c
Rene Brun <Rene.Brun@cern.ch>
2004-05-12 08:23:07 +0000

    Reintroduce fix preventing a compilation problem on Solaris
    git-svn-id: http://root.cern.ch/svn/root/trunk@8888 27541ba8-7e3a-0410-8455-c3a389f83636

--- xml/src/TXMLEngine.cxx
+++ xml/src/TXMLEngine.cxx
@@ -1,4 +1,4 @@
-// @(#)root/xml:$Name:  $:$Id: TXMLEngine.cxx,v 1.2 2004/05/10 23:50:27 rdm Exp $
+// @(#)root/xml:$Name:  $:$Id: TXMLEngine.cxx,v 1.4 2004/05/11 18:52:17 brun Exp $
 // Author: Sergey Linev  10.05.2004
 
 /*************************************************************************
@@ -196,8 +196,11 @@ xmlDocPointer TXMLEngine::ParseFile(const char* filename) {
 Bool_t TXMLEngine::ValidateDocument(xmlDocPointer doc, Bool_t doout) {
 	xmlValidCtxt cvp;
 	cvp.userData = doout ? (void *) stderr : 0;
-	cvp.error    = doout ? (xmlValidityErrorFunc) fprintf : 0;
-	cvp.warning  = doout ? (xmlValidityWarningFunc) fprintf : 0;
+//	cvp.error    = doout ? (xmlValidityErrorFunc) fprintf : 0;
+//	cvp.warning  = doout ? (xmlValidityWarningFunc) fprintf : 0;
+// debug output excluded to solve problem for Solaris compilation
+	cvp.error    = 0;
+	cvp.warning  = 0;
 
     xmlDocPtr docptr = (xmlDocPtr) doc;
 
