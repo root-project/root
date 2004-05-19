92e797beb948726634b38634d87b308fe073bbca
Philippe Canal <pcanal@fnal.gov>
2004-05-19 19:47:59 +0000

    more test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@207 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/array/indexMultiDim.C
+++ root/treeformula/array/indexMultiDim.C
@@ -23,11 +23,15 @@ c->Divide(1,2);
 }
 {
    c->cd(1);
-   int v1 = t->Draw("Vnt_fmcvtx_r[Vnt_vtx_i[0]-1][]>>histo1","Vnt_fmckin_id<50");
+   delete histo1;
+   delete histo2;
+   int v1 = t->Draw("Vnt_fmcvtx_r[Vnt_vtx_i[0]-1][]>>histo1","Vnt_fmckin_id[Vnt_vtx_i[0]-1]<50");
+   histo1 = (TH1F*)gDirectory->Get("histo1");
    int e1 = histo1->GetEntries();
    int m1 = histo1->GetMean();
    c->cd(2);
-   int v2 = t->Draw("Vnt_fmcvtx_r[][]>>histo2","Vnt_fmckin_id<50&&(int(Iteration$/3)==(Vnt_vtx_i[0]-1))");
+   int v2 = t->Draw("Vnt_fmcvtx_r[][]>>histo2","Vnt_fmckin_id[]<50&&(int(Iteration$/3)==(Vnt_vtx_i[0]-1))");
+   histo2 = (TH1F*)gDirectory->Get("histo2");
    int e2 = histo2->GetEntries();
    int m2 = histo2->GetMean();
    if (v1!=v2) {
@@ -43,5 +47,35 @@ c->Divide(1,2);
       fprintf(stderr,"mean 1 is %d while mean 2 is %d\n",m1,m2);
    }
 }
+{
+   c->cd(1);
+   delete histo1;
+   delete histo2;
+   int v1 = t->Draw("Vnt_fmcvtx_r[Vnt_vtx_i[]-1][]>>histo1","Vnt_fmckin_id[Vnt_vtx_i[]-1]<50");
+   histo1 = (TH1F*)gDirectory->Get("histo1");
+   int e1 = histo1->GetEntries();
+   int m1 = histo1->GetMean();
+   c->cd(2);
+   int v2 = t->Draw("Vnt_fmcvtx_r[][]>>histo2","Vnt_fmckin_id[]<50&&(int(Iteration$/3)==(Vnt_vtx_i[]-1))");
+   histo2 = (TH1F*)gDirectory->Get("histo2");
+   int e2 = histo2->GetEntries();
+   int m2 = histo2->GetMean();
+   if (v1!=v2) {
+      result = false;
+      fprintf(stderr,"draw return 1 is %d while draw return 2 is %d\n",v1,v2);
+   }
+   if (e1!=e2) {
+      result = false;
+      fprintf(stderr,"entries 1 is %d while entries 2 is %d\n",e1,e2);
+   }
+   if (m1!=m2) {
+      result = false;
+      fprintf(stderr,"mean 1 is %d while mean 2 is %d\n",m1,m2);
+   }
+}
+
+// T->Draw("fPx[abs(fNpoint-65)]+fMeasures[]","","",1,0)
+// T->Draw("fPx[abs(fNpoint-65)]+fMeasures[fMeasures]","","",1,0)
+
 return !result; // this is used by the makefile
 }
