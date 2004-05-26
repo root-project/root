708f4086acdefec004a40a9046cd67bb587e8f14
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-05-26 10:31:24 +0000

    some layout changes.
    git-svn-id: http://root.cern.ch/svn/root/trunk@9017 27541ba8-7e3a-0410-8455-c3a389f83636

--- quadp/Module.mk
+++ quadp/Module.mk
@@ -3,27 +3,27 @@
 #
 # Author: Fons Rademakers, Eddy Offermann, 21/05/2003
 
-MODDIR   := quadp
-MODDIRS  := $(MODDIR)/src
-MODDIRI  := $(MODDIR)/inc
+MODDIR       := quadp
+MODDIRS      := $(MODDIR)/src
+MODDIRI      := $(MODDIR)/inc
 
-QUADPDIR    := $(MODDIR)
-QUADPDIRS   := $(QUADPDIR)/src
-QUADPDIRI   := $(QUADPDIR)/inc
+QUADPDIR     := $(MODDIR)
+QUADPDIRS    := $(QUADPDIR)/src
+QUADPDIRI    := $(QUADPDIR)/inc
 
 ##### libQuadp #####
-QUADPL      := $(MODDIRI)/LinkDef.h
-QUADPDS     := $(MODDIRS)/G__Quadp.cxx
-QUADPDO     := $(QUADPDS:.cxx=.o)
-QUADPDH     := $(QUADPDS:.cxx=.h)
+QUADPL       := $(MODDIRI)/LinkDef.h
+QUADPDS      := $(MODDIRS)/G__Quadp.cxx
+QUADPDO      := $(QUADPDS:.cxx=.o)
+QUADPDH      := $(QUADPDS:.cxx=.h)
 
-QUADPH      := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
-QUADPS      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
-QUADPO      := $(QUADPS:.cxx=.o)
+QUADPH       := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+QUADPS       := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
+QUADPO       := $(QUADPS:.cxx=.o)
 
-QUADPDEP    := $(QUADPO:.o=.d) $(QUADPDO:.o=.d)
+QUADPDEP     := $(QUADPO:.o=.d) $(QUADPDO:.o=.d)
 
-QUADPLIB    := $(LPATH)/libQuadp.$(SOEXT)
+QUADPLIB     := $(LPATH)/libQuadp.$(SOEXT)
 
 # used in the main Makefile
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(QUADPH))
@@ -36,21 +36,21 @@ INCLUDEFILES += $(QUADPDEP)
 include/%.h:    $(QUADPDIRI)/%.h
 		cp $< $@
 
-$(QUADPLIB):   $(QUADPO) $(QUADPDO) $(MAINLIBS) $(QUADPLIBDEP)
+$(QUADPLIB):    $(QUADPO) $(QUADPDO) $(MAINLIBS) $(QUADPLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
 		   "$(SOFLAGS)" libQuadp.$(SOEXT) $@ "$(QUADPO) $(QUADPDO)" \
 		   "$(QUADPLIBEXTRA)"
 
-$(QUADPDS):    $(QUADPH) $(QUADPL) $(ROOTCINTTMP)
+$(QUADPDS):     $(QUADPH) $(QUADPL) $(ROOTCINTTMP)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(QUADPH) $(QUADPL)
 
-$(QUADPDO):    $(QUADPDS)
+$(QUADPDO):     $(QUADPDS)
 		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
 
-all-quadp:     $(QUADPLIB)
+all-quadp:      $(QUADPLIB)
 
-map-quadp:     $(RLIBMAP)
+map-quadp:      $(RLIBMAP)
 		$(RLIBMAP) -r $(ROOTMAP) -l $(QUADPLIB) \
 		   -d $(QUADPLIBDEP) -c $(QUADPL)
 
