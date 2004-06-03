bbd0db9d8b3329cacffc55cdca9a5e63911ed510
Rene Brun <Rene.Brun@cern.ch>
2004-06-03 12:41:14 +0000

    Declare the class TPython instead of the static functions. With this change, make map will add TPython in system.rootmap and it is possible to do directly root > TPython::Prompt()
    git-svn-id: http://root.cern.ch/svn/root/trunk@9107 27541ba8-7e3a-0410-8455-c3a389f83636

--- pyroot/inc/LinkDef.h
+++ pyroot/inc/LinkDef.h
@@ -4,9 +4,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ function TPython::Exec;
-#pragma link C++ function TPython::Eval;
-#pragma link C++ function TPython::Bind;
-#pragma link C++ function TPython::Prompt;
+#pragma link C++ class TPython;
 
 #endif
