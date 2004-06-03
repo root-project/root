9b154fae235cedc46416db9606e18f49155e420f
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-06-03 22:46:46 +0000

    correct a few typos in comments.
    git-svn-id: http://root.cern.ch/svn/root/trunk@9117 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/Module.mk
+++ base/Module.mk
@@ -34,14 +34,14 @@ BASEH        := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 BASES        := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 BASEO        := $(BASES:.cxx=.o)
 
-BASEDEP      := $(BASEO:.o=.d) $(BASEDO:.o=.d)
-
-# ManualBase4 only need to be regenerated (and then changed manualy) when
+# ManualBase4 only needs to be regenerated (and then changed manually) when
 # the dictionary interface changes
 BASEL4       := $(MODDIRI)/LinkDef4.h
 BASEDS4      := $(MODDIRS)/ManualBase4.cxx
 BASEDO4      := $(BASEDS4:.cxx=.o)
 BASEH4       := TDirectory.h
+
+BASEDEP      := $(BASEO:.o=.d) $(BASEDO:.o=.d)
 
 # used in the main Makefile
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(BASEH))
@@ -62,7 +62,9 @@ $(BASEDS2):     $(BASEH1) $(BASEL2) $(ROOTCINTTMP)
 $(BASEDS3):     $(BASEH3) $(BASEL3) $(ROOTCINTTMP)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(BASEH3) $(BASEL3)
-$(BASEDS4):     # intentionally left empty .. should be called only on demand after deleting the file
+# pre-requisites intentionally not specified... should be called only
+# on demand after deleting the file
+$(BASEDS4):
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(BASEH4) $(BASEL4)
 
