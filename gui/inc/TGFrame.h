8337e4884da406c995f6a231ce8b7ac48d5909fe
Rene Brun <Rene.Brun@cern.ch>
2004-06-04 16:05:52 +0000

    Remove a typo at line 41 (a "S" character)
    git-svn-id: http://root.cern.ch/svn/root/trunk@9125 27541ba8-7e3a-0410-8455-c3a389f83636

--- gui/inc/TGFrame.h
+++ gui/inc/TGFrame.h
@@ -1,4 +1,4 @@
-// @(#)root/gui:$Name:  $:$Id: TGFrame.h,v 1.38 2004/04/06 21:24:06 rdm Exp $
+// @(#)root/gui:$Name:  $:$Id: TGFrame.h,v 1.39 2004/06/04 15:57:28 rdm Exp $
 // Author: Fons Rademakers   03/01/98
 
 /*************************************************************************
@@ -38,7 +38,7 @@
 #include "TGFont.h"
 #endif
 #ifndef ROOT_TGLayout
-#include "TGLayout.h"s
+#include "TGLayout.h"
 #endif
 #ifndef ROOT_TGString
 #include "TGString.h"
