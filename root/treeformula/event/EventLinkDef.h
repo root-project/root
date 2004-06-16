1c0d349f8e6ad2a0ae45b36a8ee3312eb7252e81
Philippe Canal <pcanal@fnal.gov>
2004-06-16 19:50:05 +0000

    update test to indicate known failure
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@291 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/event/EventLinkDef.h
+++ root/treeformula/event/EventLinkDef.h
@@ -1,18 +1,24 @@
 #ifdef __CINT__
 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedef;
+
 #pragma link C++ class EventHeader+;
 #pragma link C++ class Event+;
 #pragma link C++ class HistogramManager+;
 #pragma link C++ class Track+;
 
-#pragma link C++ nestedclasses;
-#pragma link C++ nestedtypedef;
-
 #pragma link C++ class vector<EventHeader>;
-#pragma link C++ class vector<EventHeader>::iterator-;
 #pragma link C++ class vector<Track*>;
+
+#pragma ifdef G__INTEL_COMPILER
+#pragma link C++ typedef vector<EventHeader>::iterator;
+#pragma link C++ typedef vector<Track*>::iterator;
+#pragma else
+#pragma link C++ class vector<EventHeader>::iterator-;
 #pragma link C++ class vector<Track*>::iterator-;
 #pragma link C++ function operator!=(vector<EventHeader>::iterator,vector<EventHeader>::iterator);
 #pragma link C++ function operator!=(vector<Track*>::iterator,vector<Track*>::iterator);
+#pragma endif
 
 #endif
