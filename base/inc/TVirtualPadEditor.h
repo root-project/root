1a85659e622540b449132ba81fe75e822be90e9d
Rene Brun <Rene.Brun@cern.ch>
2004-06-18 15:49:37 +0000

    From Ilka: - new interface method DeleteEditors()
    git-svn-id: http://root.cern.ch/svn/root/trunk@9253 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/TVirtualPadEditor.h
+++ base/inc/TVirtualPadEditor.h
@@ -47,6 +47,7 @@ public:
    virtual void     Build() { }
    virtual void     Show() { }
    virtual void     Hide() { }
+   virtual void     DeleteEditors() { }
 
    // methods related to the new editor interface
 
