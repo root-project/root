92f54e7940bfaa8a0c181090b678575c72bc169d
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-06-23 08:32:51 +0000

    add script that shows how to make a PROOF package the will build libEvent on PROOF.
    git-svn-id: http://root.cern.ch/svn/root/trunk@9295 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ test/make_event_par.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+#
+# Make the event.par file to be used to analyse Event objects with PROOF.
+#
+# Usage: sh make_event_par.sh
+#
+# Creates the PAR file "event.par" which can be used in PROOF via the
+# package manager like:
+#   gProof->UploadPackage("event.par")
+#   gProof->EnablePackage("event")
+#
+# Command to check that package is active and that libEvent.so is loaded:
+#   gProof->ShowPackages()
+#   gProof->ShowEnabledPackages()
+#   gProof->Exec("gSystem->ListLibraries()")
+#
+
+EDIR=event
+
+mkdir $EDIR
+cp Event.cxx Event.h EventLinkDef.h Makefile Makefile.arch $EDIR
+mkdir $EDIR/PROOF-INF
+cd $EDIR/PROOF-INF
+
+cat > BUILD.sh <<EOF
+#! /bin/sh
+
+make libEvent.so
+EOF
+
+cat > SETUP.C <<EOF
+void SETUP()
+{
+   gSystem->Load("libEvent");
+}
+EOF
+
+chmod 755 BUILD.sh
+
+cd ../..
+
+tar zcvf event.par $EDIR
+
+rm -rf event
+
+exit 0
