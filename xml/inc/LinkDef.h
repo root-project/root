a77fecbbbcef00b46f111d50d3dc0bc15fd5d111
Rene Brun <Rene.Brun@cern.ch>
2004-06-25 16:55:08 +0000

    reintroduce LinkDef.h with class TXMLPlayer
    git-svn-id: http://root.cern.ch/svn/root/trunk@9319 27541ba8-7e3a-0410-8455-c3a389f83636

--- xml/inc/LinkDef.h
+++ xml/inc/LinkDef.h
@@ -1,4 +1,4 @@
-/* @(#)root/xml:$Name:  $:$Id: LinkDef.h,v 1.3 2004/06/25 16:30:16 brun Exp $ */
+/* @(#)root/xml:$Name:  $:$Id: LinkDef.h,v 1.4 2004/06/25 16:50:54 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2004, Rene Brun and Fons Rademakers.               *
@@ -20,5 +20,6 @@
 #pragma link C++ class TXMLFile;
 #pragma link C++ class TXMLKey;
 #pragma link C++ class TXMLDtdGenerator;
+#pragma link C++ class TXMLPlayer;
 
 #endif
