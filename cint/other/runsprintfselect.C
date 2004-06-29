16e1f8d404ab7d4d50d66b8ed3ddd6a1925dba1f
Philippe Canal <pcanal@fnal.gov>
2004-06-29 15:37:58 +0000

    test %1\*\d
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@356 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ cint/other/runsprintfselect.C
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <iostream>
+using namespace std;
+
+void runsprintfselec() {
+
+ char str[1000];
+ const double EPSILON = 0.5;
+ Int_t filterNumber=0;
+
+ sprintf(str, "(%3$i-%1$f)<filterNumber && filterNumber<(%2$i+%1$f)",
+         EPSILON, filterNumber, 5);
+
+ cout<<str<<endl;
+
+}
+
