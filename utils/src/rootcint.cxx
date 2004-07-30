6de4e8062e21f9cf7aeb0801d9eea2ce77166fd7
Rene Brun <Rene.Brun@cern.ch>
2004-07-30 20:25:29 +0000

    Fix a format problem in a fprintf statement
    git-svn-id: http://root.cern.ch/svn/root/trunk@9640 27541ba8-7e3a-0410-8455-c3a389f83636

--- utils/src/rootcint.cxx
+++ utils/src/rootcint.cxx
@@ -1,4 +1,4 @@
-// @(#)root/utils:$Name:  $:$Id: rootcint.cxx,v 1.180 2004/07/28 04:56:42 brun Exp $
+// @(#)root/utils:$Name:  $:$Id: rootcint.cxx,v 1.181 2004/07/30 19:09:52 brun Exp $
 // Author: Fons Rademakers   13/07/96
 
 /*************************************************************************
@@ -2292,7 +2292,7 @@ void WriteNamespaceInit(G__ClassInfo &cl)
           strstr(methodinfo.FileName(),"Rtypes.h") == 0) {
          fprintf(fp, "GetClassVersion<%s >(), ",classname.c_str());
       } else {
-         fprintf(fp, "0 /*version*/, ",classname.c_str());
+         fprintf(fp, "0 /*version*/);
       }
    }
 
