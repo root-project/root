395c8e1511005e65b265e348e780656fd19eac79
Philippe Canal <pcanal@fnal.gov>
2004-08-02 21:37:22 +0000

    cint not longer generate an entry for the non existing destructor of a namespace
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@369 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/parsing/operatorLessThanTemplate.C
+++ cint/parsing/operatorLessThanTemplate.C
@@ -1,6 +1,7 @@
 
 #include <map>
 #include <string>
+#include <list>
 #include <iostream>
 #include <typeinfo>
 
@@ -10,158 +11,187 @@ using std::map;
 using std::string;
 using std::ostream;
 using std::type_info;
+using std::list;
 
+template <class T> class genie__Wrapper {
+public: 
+   T val;
+};
 
 namespace genie {
-
+   
+   template <class T> class Wrapper {
+   public: 
+      T val;
+   };
+   
    class Algorithm;
-
-class RegistryItemI 
-{
-public:
-
-  virtual const type_info & TypeInfo(void)  const        = 0;
-  virtual void              Print(ostream& stream) const = 0;
-
-protected:
-
-  RegistryItemI()          { }
-  virtual ~RegistryItemI() { }
-};
-
-template<class T> class RegistryItem;
-template<class T>
-       ostream & operator << (ostream & stream, const RegistryItem<T> & rec)
-{ return stream; };
-
-template<class T> bool operator< (const RegistryItem<T> & lhs, const RegistryItem<T> & rhs) 
-{ return false; };
-
-template<class T> class RegistryItem : public RegistryItemI {
-
-public:
-
-  RegistryItem() { };
-  RegistryItem(T item) {};
-  ~RegistryItem() { }
-
-  const type_info & TypeInfo (void) const { return typeid(fItem); }
-  const T &         Data     (void) const { return fItem;         }
-
-  void Print(ostream& stream) const {};
-
-  friend ostream & operator<< <T>(ostream & stream, const RegistryItem<T> & rec); 
-  friend bool operator< <T>(const RegistryItem<T> & lhs, const RegistryItem<T> & rhs); 
-
-private:
-
-  T fItem;    
-};
-
-class Registry {
-
-public:
-
-  Registry() {};
-  Registry(const char * name, bool isReadOnly = true) {};
-  Registry(const Registry &) {};
-  virtual ~Registry() {}
-
-  //-- RegistryI interface implementation
-  
-  void   Lock        (void) {}
-  void   UnLock      (void) {}
-  bool   LockStatus  (void) const { return false; }
-  void   Set         (const char * key, bool         item) {}
-  void   Set         (string       key, bool         item) {}
-  void   Set         (const char * key, int          item) {}
-  void   Set         (string       key, int          item) {}
-  void   Set         (const char * key, double       item) {}
-  void   Set         (string       key, double       item) {}
-  void   Set         (const char * key, const char * item) {}
-  void   Set         (const char * key, string       item) {}
-  void   Set         (string       key, string       item) {}
-  void   Get         (const char * key, bool &       item) const {}
-  void   Get         (string       key, bool &       item) const {}
-  void   Get         (const char * key, int &        item) const {}
-  void   Get         (string       key, int &        item) const {}
-  void   Get         (const char * key, double &     item) const {}
-  void   Get         (string       key, double &     item) const {}
-  void   Get         (const char * key, string &     item) const {}
-  void   Get         (string       key, string &     item) const {}
-  int    NEntries    (void) const { return 0; }
-  bool   Exists      (const char * key) const {return false; }
-  bool   Exists      (string       key) const {return false; }
-  bool   DeleteEntry (const char * key) {return false; }
-  bool   DeleteEntry (string       key) {return false; }
-  void   SetName     (const char * name) {}
-  void   SetName     (string name) {}
-  string Name        (void) const { return ""; }
-  void   Print       (ostream & stream) const {}
-  
- friend ostream& operator<<(ostream& stream, const Registry& registry);
-  
-  void operator () (const char * key,  int          item) {}
-  void operator () (string       key,  int          item) {}
-  void operator () (const char * key,  bool         item) {}
-  void operator () (string       key,  bool         item) {}
-  void operator () (const char * key,  double       item) {}  
-  void operator () (string       key,  double       item) {}
-  void operator () (const char * key,  string       item) {}  
-  void operator () (string       key,  string       item) {}
-  void operator () (const char * key,  const char * item) {}
-  
-  Registry * operator =  (const Registry & reg) { return this; }
-
-private:
-
-  string fName;
-  bool   fIsReadOnly;
-
-  map<string, genie::RegistryItemI *> fRegistry;
-};
-
-ostream& operator<<(ostream& stream, const Registry& registry) {
-   return stream;
-}
-
-class ConfigPool {
-
-public:
-
-  static ConfigPool * Instance() { return 0; }
-
-  Registry * FindRegistry(const Algorithm * algorithm) const { return 0; }
-  
-  void Print(ostream & stream) const {}
-  
-   friend ostream& operator<<(ostream& stream, const ConfigPool & config_pool);
-
-private:
-
-  ConfigPool() {} 
-  ConfigPool(const ConfigPool & config_pool) {}
-  virtual ~ConfigPool() {}
-
-  bool LoadXMLConfig(void) { return false; }
-    
-//   static ConfigPool * fInstance;
-
-  map<string, genie::Registry *> fRegistryPool;    //-- algorithm/param_set -> Registry
-  
-  struct Cleaner {
-      void DummyMethodAndSilentCompiler() { }
-      ~Cleaner() {
-      }
-  };
-
-  friend struct Cleaner;
-};
-
-ostream& operator<<(ostream& stream, const ConfigPool & config_pool) {
-   return stream;
-}
-
+   
+   class RegistryItemI 
+   {
+   public:
+      
+      virtual const type_info & TypeInfo(void)  const        = 0;
+      virtual void              Print(ostream& stream) const = 0;
+      
+   protected:
+      
+      RegistryItemI()          { }
+      virtual ~RegistryItemI() { }
+   };
+   
+   template<class T> class RegistryItem;
+   template<class T>
+   ostream & operator << (ostream & stream, const RegistryItem<T> & rec)
+   { return stream; };
+   
+   template<class T> class RegistryItem;
+   template<class T>
+   ostream & operator >> (ostream & stream, const RegistryItem<T> & rec)
+   { return stream; };
+   
+   template<class T> bool operator< (const RegistryItem<T> & lhs, const RegistryItem<T> & rhs) 
+   { return false; };
+   
+   template<class T> bool operator> (const RegistryItem<T> & lhs, const RegistryItem<T> & rhs) 
+   { return false; };
+   
+   template<class T> class RegistryItem : public RegistryItemI {
+      
+   public:
+      
+      RegistryItem() { };
+      RegistryItem(T item) {};
+      ~RegistryItem() { }
+      
+      const type_info & TypeInfo (void) const { return typeid(fItem); }
+      const T &         Data     (void) const { return fItem;         }
+      
+      void Print(ostream& stream) const {};
+      
+      friend ostream & operator<< <T>(ostream & stream, const RegistryItem<T> & rec); 
+      friend ostream & operator>> <T>(ostream & stream, const RegistryItem<T> & rec); 
+      friend bool operator< <T>(const RegistryItem<T> & lhs, const RegistryItem<T> & rhs); 
+      friend bool operator> <T>(const RegistryItem<T> & lhs, const RegistryItem<T> & rhs); 
+      
+   private:
+      
+      T fItem;    
+   };
+   
+   class Registry {
+      
+   public:
+      
+      Registry() {};
+      Registry(const char * name, bool isReadOnly = true) {};
+      Registry(const Registry &) {};
+      virtual ~Registry() {}
+      
+      //-- RegistryI interface implementation
+      
+      void   Lock        (void) {}
+      void   UnLock      (void) {}
+      bool   LockStatus  (void) const { return false; }
+      void   Set         (const char * key, bool         item) {}
+      void   Set         (string       key, bool         item) {}
+      void   Set         (const char * key, int          item) {}
+      void   Set         (string       key, int          item) {}
+      void   Set         (const char * key, double       item) {}
+      void   Set         (string       key, double       item) {}
+      void   Set         (const char * key, const char * item) {}
+      void   Set         (const char * key, string       item) {}
+      void   Set         (string       key, string       item) {}
+      void   Get         (const char * key, bool &       item) const {}
+      void   Get         (string       key, bool &       item) const {}
+      void   Get         (const char * key, int &        item) const {}
+      void   Get         (string       key, int &        item) const {}
+      void   Get         (const char * key, double &     item) const {}
+      void   Get         (string       key, double &     item) const {}
+      void   Get         (const char * key, string &     item) const {}
+      void   Get         (string       key, string &     item) const {}
+      int    NEntries    (void) const { return 0; }
+      bool   Exists      (const char * key) const {return false; }
+      bool   Exists      (string       key) const {return false; }
+      bool   DeleteEntry (const char * key) {return false; }
+      bool   DeleteEntry (string       key) {return false; }
+      void   SetName     (const char * name) {}
+      void   SetName     (string name) {}
+      string Name        (void) const { return ""; }
+      void   Print       (ostream & stream) const {}
+      
+      friend ostream& operator<<(ostream& stream, const Registry& registry);
+      friend ostream& operator>>(ostream& stream, const Registry& registry);
+      
+      void operator () (const char * key,  int          item) {}
+      void operator () (string       key,  int          item) {}
+      void operator () (const char * key,  bool         item) {}
+      void operator () (string       key,  bool         item) {}
+      void operator () (const char * key,  double       item) {}  
+      void operator () (string       key,  double       item) {}
+      void operator () (const char * key,  string       item) {}  
+      void operator () (string       key,  string       item) {}
+      void operator () (const char * key,  const char * item) {}
+      
+      Registry * operator =  (const Registry & reg) { return this; }
+      
+   private:
+      
+      string fName;
+      bool   fIsReadOnly;
+      
+      map<string, genie::RegistryItemI *> fRegistry;
+   };
+   
+   ostream& operator<<(ostream& stream, const Registry& registry) {
+      return stream;
+   }
+   
+   ostream& operator>>(ostream& stream, const Registry& registry) {
+      return stream;
+   }
+   
+   class ConfigPool {
+      
+   public:
+      
+      static ConfigPool * Instance() { return 0; }
+      
+      Registry * FindRegistry(const Algorithm * algorithm) const { return 0; }
+      
+      void Print(ostream & stream) const {}
+      
+      friend ostream& operator<<(ostream& stream, const ConfigPool & config_pool);
+      friend ostream& operator>>(ostream& stream, const ConfigPool & config_pool);
+      
+   private:
+      
+      ConfigPool() {} 
+      ConfigPool(const ConfigPool & config_pool) {}
+      virtual ~ConfigPool() {}
+      
+      bool LoadXMLConfig(void) { return false; }
+      
+      //   static ConfigPool * fInstance;
+      
+      map<string, genie::Registry *> fRegistryPool;    //-- algorithm/param_set -> Registry
+      
+      struct Cleaner {
+         void DummyMethodAndSilentCompiler() { }
+         ~Cleaner() {
+         }
+      };
+      
+      friend struct Cleaner;
+   };
+   
+   ostream& operator<<(ostream& stream, const ConfigPool & config_pool) {
+      return stream;
+   }
+   ostream& operator>>(ostream& stream, const ConfigPool & config_pool) {
+      return stream;
+   }
+   
 }      // genie namespace
 
 
@@ -175,6 +205,9 @@ ostream& operator<<(ostream& stream, const ConfigPool & config_pool) {
 #pragma link C++ class genie::RegistryItem<int>+;
 #pragma link C++ class genie::RegistryItem<double>+;
 #pragma link C++ class genie::RegistryItem<string>+;
+// #pragma link C++ class genie::RegistryItem<list<double> >+; // wont work because the friend parsing does not handle multiple template param
+// #pragma link C++ class genie::RegistryItem<genie::Wrapper<double> >+; // wont work because the friend parsing does not handle scope in  template param
+#pragma link C++ class genie::RegistryItem<genie__Wrapper<double> >+; // wont work because the friend parsing does not handle scope in  template param
 #pragma link C++ class genie::RegistryItemI;
 
 #endif // __MAKECINT__
