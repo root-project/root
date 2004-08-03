2ea9a3fff32a5cbf82e7bdfeabaaeb58dd36e8b3
Rene Brun <Rene.Brun@cern.ch>
2004-08-03 16:04:37 +0000

    fix typos/comments
    git-svn-id: http://root.cern.ch/svn/root/trunk@9666 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/TBuffer3D.h
+++ base/inc/TBuffer3D.h
@@ -2,7 +2,7 @@
 // Author: Olivier Couet   05/05/04
 
 /*************************************************************************
- * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2004, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
