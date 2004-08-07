31ba98fbe04707b89102314cea9e67b870be3fff
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-08-07 23:49:31 +0000

    remove patch errorneously applied twice.
    git-svn-id: http://root.cern.ch/svn/root/trunk@9690 27541ba8-7e3a-0410-8455-c3a389f83636

--- rint/src/TRint.cxx
+++ rint/src/TRint.cxx
@@ -1,4 +1,4 @@
-// @(#)root/rint:$Name:  $:$Id: TRint.cxx,v 1.39 2004/08/06 05:46:32 brun Exp $
+// @(#)root/rint:$Name:  $:$Id: TRint.cxx,v 1.40 2004/08/07 18:19:12 rdm Exp $
 // Author: Rene Brun   17/02/95
 
 /*************************************************************************
@@ -127,11 +127,6 @@ TRint::TRint(const char *appClassName, Int_t *argc, char **argv, void *options,
    // Everybody expects iostream to be available, so load it...
    ProcessLine("#include <iostream>", kTRUE);
    ProcessLine("#include <_string>", kTRUE); // for std::string iostream.
-
-   // Disallow the interpretation of Rtypes.h, TError.h and TGenericClassInfo.h
-   ProcessLine("#define ROOT_Rtypes 0");
-   ProcessLine("#define ROOT_TError 0");
-   ProcessLine("#define ROOT_TGenericClassInfo 0");
 
    // Allow the usage of ClassDef and ClassImp in interpreted macros
    ProcessLine("#include <RtypesCint.h>", kTRUE);
