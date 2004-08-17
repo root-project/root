580d8dcdfa0ce2d237659eacbd9750ad217efceb
Philippe Canal <pcanal@fnal.gov>
2004-08-17 19:03:03 +0000

    accept current limitation
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@382 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/array/runmultidim.C
+++ root/treeformula/array/runmultidim.C
@@ -5,24 +5,40 @@ FillNtp();
 TFile *_file0 = TFile::Open("NtpRecord.root"); 
 TTree *tree = (TTree*)_file0->Get("Ntp");
 
-tree->Scan("fEvents.fNShower");
-tree->Scan("fEvents[].fShwInd");
-tree->Scan("fEvents.fEventNo");
-tree->Scan("fEvents.fEventNo:fEvents.fShwInd");
-tree->Scan("fEvents.fEventNo*100+fEvents[].fShwInd[]");
-tree->Scan("fEvents.fEventNo:fEvents[].fShwInd[]","fEvents[].fNShower > 0");
-tree->Scan("fEvents.fEventNo:fEvents[].fShwInd[]:fShowers","fEvents[].fNShower > 0");
-tree->Scan("fEvents.fEventNo:fEvents[].fShwInd[]:fShowers[fEvents[].fShwInd[0]].fEnergy","fEvents[].fNShower > 0");
-tree->Scan("fShowers[fEvents[].fShwInd[0]].fEnergy","fEvents[].fNShower > 0");
-tree->Scan("fEvents.fEventNo:fShowers[fEvents[].fShwInd[]].fEnergy","");
+tree->Scan("fEvents.fNShower","","colsize=20");
+tree->Scan("fEvents[].fShwInd","","colsize=20");
+tree->Scan("fEvents.fEventNo","","colsize=20");
+tree->Scan("fEvents.fEventNo:fEvents.fShwInd","","colsize=20");
+tree->Scan("fEvents.fEventNo*100+fEvents[].fShwInd[]","","col=03.9");
+tree->Scan("fEvents.fEventNo:fEvents[].fShwInd[]","fEvents[].fNShower > 0","colsize=20");
+tree->Scan("fEvents.fEventNo:fEvents[].fShwInd[]:fShowers","fEvents[].fNShower > 0","colsize=20");
+tree->Scan("fEvents[].fShwInd[0]","fEvents[].fNShower > 0","colsize=20");
+tree->Scan("fEvents.fEventNo:fShowers[fEvents[].fShwInd[]].fEnergy","","colsize=20");
+tree->Scan("fEvents.fEventNo*1000+fEvents[].fShwInd[]*100:fShowers[fEvents[].fShwInd[0]].fEnergy","","colsize=20");
+tree->Scan("fEvents[].fNShower:fShowers[fEvents[].fShwInd[0]].fEnergy","","colsize=20"); 
+tree->Scan("fShowers[fEvents[].fShwInd[0]].fEnergy","","colsize=20");
+tree->Scan("fEvents[].fShwInd[0]","","colsize=20");
+
+
+tree->Scan("fEvents.fEventNo:fEvents[].fNShower:fShowers[fEvents[].fShwInd[]].fEnergy","fEvents[].fShwInd[]*0==0","colsize=20");
 
 return;
 // Wrong
-tree->Scan("fShowers[fEvents[].fShwInd[0]].fEnergy");
-tree->Scan("fEvents[].fShwInd[0]");
-tree->Scan("fShowers[fEvents[].fShwInd[]].fEnergy","fEvents[].fNShower > 0");
-tree->Scan("fEvents[].fNShower:fShowers[fEvents[].fShwInd[0]].fEnergy"); 
-tree->Scan("fEvents[].fNShower:fShowers[fEvents[].fShwInd[]].fEnergy","1");
+tree->Scan("fShowers[fEvents[].fShwInd[]].fEnergy","fEvents[].fNShower > 0"); // to fix remove condition
+tree->Scan("fEvents[].fNShower:fShowers[fEvents[].fShwInd[]].fEnergy","1"); // to fix replace cond : fEvents[].fShwInd[]*0==0
+
+tree->Scan("fEvents.fEventNo:fEvents[].fShwInd[]:fShowers[fEvents[].fShwInd[0]].fEnergy","fEvents[].fNShower > 0"); // can NOT find a correction!
+tree->Scan("fShowers[fEvents[].fShwInd[0]].fEnergy","fEvents[].fNShower > 0","colsize=20"); // to fix remove condition
+
+// wrong size:
+root [55]   tree->Scan("fEvents.fEventNo:fShowers[fEvents[].fShwInd[]].fEnergy","fEvents[].fShwInd[]*0==0","col=18");    
+*******************************************************
+*    Row   * Instance *   fEvents.fEventNo * fShowers *
+*******************************************************
+*        0 *        0 *                  0 *       10 *
+*        0 *        1 *                  0 *       20 *
+*        0 *        2 *                  2 *       30 *
+*******************************************************
 
 
 }
