cd9d031761642948457e599f042fca9e1a5d686c
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-09-16 20:51:06 +0000

    excluded chirp and gl directories.
    git-svn-id: http://root.cern.ch/svn/root/trunk@10078 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makestatic.sh
+++ build/unix/makestatic.sh
@@ -20,8 +20,8 @@ PROOFAEXE=bin/proofserva
 rm -f $ROOTALIB $ROOTAEXE $PROOFAEXE
 
 excl="main proofd rootd rootx pythia pythia6 venus mysql pgsql rfio sapdb \
-      hbook newdelete table utils srputils krb5auth dcache x11ttf \
-      alien asimage ldap pyroot qt qtroot quadp ruby vmc xml"
+      hbook newdelete table utils srputils krb5auth chirp dcache x11ttf \
+      alien asimage ldap pyroot qt qtroot quadp ruby vmc xml gl"
 
 objs=""
 gobjs=""
