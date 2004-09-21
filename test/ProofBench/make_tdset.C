680c93b7c814bd16dc52dba27efc8b7ab49fe7c0
Rene Brun <Rene.Brun@cern.ch>
2004-09-21 19:23:01 +0000

    From  Maarten Ballintijn - Simplify use of_tdset.C
    git-svn-id: http://root.cern.ch/svn/root/trunk@10128 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/ProofBench/make_tdset.C
+++ test/ProofBench/make_tdset.C
@@ -1,12 +1,11 @@
-TDSet *make_tdset(const Char_t* basedir, Int_t files_per_node, Float_t proc_fraction)
+TDSet *make_tdset(const Char_t* basedir, Int_t files_per_slave)
 {
    // This script creates a TDSet object that can be used to process
    // the files generated with the make_event_trees.C script.
    // Conventions for file names made by that script are assumed.
    //
-   // basedir:        location of files local to proof slaves
-   // files_per_node: number of files on each node
-   // proc_fraction:  fraction of files to be processed (gt 0 and le 1)
+   // basedir:         location of files local to proof slaves
+   // files_per_slave: number of files per slave to process
 
    if (!gProof) {
       cout << "Must Start PROOF before using make_tdset.C" << endl;
@@ -18,32 +17,32 @@ TDSet *make_tdset(const Char_t* basedir, Int_t files_per_node, Float_t proc_frac
       return 0;
    }
 
-   if (files_per_node <= 0) {
-      cout << "files_per_node must be > 0" << endl;
+   if (files_per_slave <= 0) {
+      cout << "files_per_slave must be > 0" << endl;
       return kFALSE;
    }
 
-   if (proc_fraction <= 0  || proc_fraction > 1) {
-      cout << "proc_fraction must be greater than 0" << endl;
-      cout << "and less than or equal to 1" << endl;
-      return 0;
-   }
-
    THashList nodelist;
+   TArrayI nslaves;
    nodelist.SetOwner();
    TList* l = gProof->GetSlaveInfo();
    for(Int_t i=0 ; i < l->GetSize() ; i++){
       TSlaveInfo* si = dynamic_cast<TSlaveInfo*>(l->At(i));
-      if(!nodelist.FindObject(si->fHostName.Data()))
+      TObjString* host = 0;
+      if(host = dynamic_cast<TObjString*>(nodelist.FindObject(si->fHostName.Data()))) {
+         Int_t index = nodelist.IndexOf(host);
+         nslaves[index]++;
+      } else {
          nodelist.Add(new TObjString(si->fHostName.Data()));
+         nslaves.Set(1+nslaves.GetSize());
+         nslaves[nslaves.GetSize()-1] = 1;
+      }
    }
 
    TDSet *d = new TDSet("TTree","EventTree");
    for(Int_t i=0; i < nodelist.GetSize() ; i++){
       TObjString* node = dynamic_cast<TObjString*>(nodelist.At(i));
-      for(Int_t j=1; j <= files_per_node; j++) {
-         Float_t val = (i*files_per_node+j)*proc_fraction;
-         if (TMath::Abs(val-TMath::Nint(val)) < (1.-proc_fraction)/2.) continue;
+      for(Int_t j=1; j <= files_per_slave*nslaves[i]; j++) {
 
          TString filestr = "root://";
          filestr += node->GetName();
