9f2c338155e98b90891ec7daadec88b5e38f4b4d
Rene Brun <Rene.Brun@cern.ch>
2004-09-23 06:37:40 +0000

    Add documentation (from Valeriy's notes)
    git-svn-id: http://root.cern.ch/svn/root/trunk@10155 27541ba8-7e3a-0410-8455-c3a389f83636

--- guibuilder/src/TGuiBuilder.cxx
+++ guibuilder/src/TGuiBuilder.cxx
@@ -1,4 +1,4 @@
-// @(#)root/guibuilder:$Name:  $:$Id: TGuiBuilder.cxx,v 1.16 2004/09/22 12:34:10 brun Exp $
+// @(#)root/guibuilder:$Name:  $:$Id: TGuiBuilder.cxx,v 1.17 2004/09/22 18:05:04 brun Exp $
 // Author: Valeriy Onuchin   12/09/04
 
 /*************************************************************************
@@ -41,6 +41,67 @@
 // TGuiBuilder                                                          
 //                                                                      
 //
+//  ************************************************
+//                ROOT GUI Builder principles
+//  ************************************************
+//
+//  With the GUI builder, we try to make the next step from WYSIWYG
+//  to embedded editing concept - WYSIWYE ("what you see is what you edit"). 
+//  The ROOT GUI Builder allows modifying real GUI objects.  
+//  For example, one can edit the existing GUI application $ROOTSYS/tutorials/guitest.C.
+//  GUI components can be added to a design area from a widget palette,
+//  or can be borrowed from another application.
+//  One can drag and and drop TCanvas's menu bar into the application.  
+//  GUI objects can be resized and dragged, copied and pasted. 
+//  ROOT GUI Builder allows changing the layout, snap to grid, change object's 
+//  layout order via the GUI Builder toolbar, or by options in the right-click 
+//  context menus.
+//  A final design can be immediatly tested and used, or saved as a C++ macro.
+//  For example, it's possible to rearrange buttons in control bar,
+//  add separators etc. and continue to use a new fancy control bar in the 
+//  application.
+//
+//  ************************************************
+//
+//  The following is a short description of the GUI Builder actions and key shortcuts:
+//
+//   o Press Ctrl-Double-Click to start/stop edit mode
+//   o Press Double-Click to activate quick edit action (defined in root.mimes)
+//
+//                 Selection, grabbing, dropping
+//       ************************************************
+//    It is possible to select, drag any frame and drop it to any frame
+//
+//   o Click left mouse button or Ctrl-Click to select an object to edit.
+//   o Press right mouse button to activate context menu
+//   o Mutiple selection (grabbing):
+//      - draw lasso and press Return key
+//      - press Shift key and draw lasso
+//   o Dropping:
+//      - select frame and press Ctrl-Return key
+//   o Changing layout order:
+//      - select frame and use arrow keys to change layout order
+//   o Alignment:
+//      - draw lasso and press arrow keys (or Shift-Arrow key) to align frames
+//
+//                    Key shortcuts
+//       ************************************************
+//   o Del       - delete selected frame
+//   o Shift-Del - crop action
+//   o Ctrl-X    - cut action
+//   o Ctrl-C    - copy action
+//   o Ctrl-V    - paste action
+//   o Ctrl-R    - replace action
+//   o Ctrl-L    - compact layout
+//   o Ctrl-B    - break layout
+//   o Ctrl-H    - switch horizontal-vertical layout
+//   o Ctrl-G    - switch on/off grid
+//   o Ctrl-S    - save action
+//   o Ctrl-O    - open and execute a ROOT macro file.  GUI components created 
+//                 after macro execution will be emebedded to currently edited 
+//                 design area.   
+//   o Ctrl-N    - create new main frame
+//
 //Begin_Html
 /*
 <img src="gif/RootGuiBuilder.gif">
@@ -66,7 +127,7 @@ enum EMenuIds {
 
 const char gHelpBuilder[] = "\
  o Press Ctrl-Double-Click to start/stop edit mode\n\
- o Press Double-Click to activate quick edit action (definded in root.mimes)\n\
+ o Press Double-Click to activate quick edit action (defined in root.mimes)\n\
 \n\
                Selection, grabbing, dropping\n\
      ************************************************\n\
@@ -74,7 +135,7 @@ const char gHelpBuilder[] = "\
 \n\
  o Press left mouse button Click or Ctrl-Click to select an object to edit.\n\
  o Press right mouse button to activate context menu\n\
- o Mutilple selection (grabbing):\n\
+ o Mutiple selection (grabbing):\n\
       - draw lasso and press Return key\n\
       - press Shift key and draw lasso\n\
  o Dropping:\n\
