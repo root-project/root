e8c0fb011100dc0d7012d1c7d70aca33fa3b3099
Philippe Canal <pcanal@fnal.gov>
2004-10-08 10:15:10 +0000

    reenable on windows now that we know how to build library from .o using ACLiC
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@424 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/template/separateDictNamespace/run.C
+++ cint/template/separateDictNamespace/run.C
@@ -2,8 +2,8 @@
 gSystem->Setenv("LINES","-1");
 gSystem->Load("libmaster");
 gROOT->ProcessLine(".class Master::Container");
-gSystem->Load("libslave1.so");
+gSystem->Load("libslave1");
 gROOT->ProcessLine(".class Master::Container");
-gSystem->Load("libslave2.so");
+gSystem->Load("libslave2");
 gROOT->ProcessLine(".class Master::Container");
 }
