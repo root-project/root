8226ce7a19abc1367ec4b234ef8e13c00f3acd06
Philippe Canal <pcanal@fnal.gov>
2004-10-08 09:44:37 +0000

    work around a weirdness in windows :(
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@421 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/runBoolOrder.C
+++ root/treeformula/parse/runBoolOrder.C
@@ -1,4 +1,5 @@
 {
+   gROOT->ProcessLine(".exception");
 	if (gFile!=0) {
 	   mk->Scan("iem.mt:iem.i:Emcl[iem.i]._HMx8:iem.dphi_met","(iem.dphi_met>0.5) && (Emcl[iem.i]._HMx8<20)","colsize=18",2,211);
 	   mk->Scan("iem.mt:iem.i:Emcl[iem.i]._HMx8:iem.dphi_met","(Emcl[iem.i]._HMx8<20) && (iem.dphi_met>0.5)","colsize=18",2,211);
@@ -7,12 +8,13 @@
    TFile *_file0 = TFile::Open("mksm.root");
    mk->AddFriend("TMBTree","sm.root");
 	int res01 = mk->Draw("iem.mt>>h_iem_Emcl","(iem.dphi_met>0.5) && (Emcl[iem.i]._HMx8<20)");
-	int res02 = mk->Draw("iem.mt>>h_Emcl_iem","(Emcl[iem.i]._HMx8<20) && (iem.dphi_met>0.5)");
+   //int res01 = mk->Draw("iem.mt>>h_iem_Emcl","(iem.dphi_met>0.5) && (Emcl[iem.i]._HMx8<20)");
+   int res02 = mk->Draw("iem.mt>>h_Emcl_iem","(Emcl[iem.i]._HMx8<20) && (iem.dphi_met>0.5)");
    if (res01!=res02) {
       cout << "Error: order of selections affects the results " << res01 << " vs " << res02 << "\n";
 	}
-/*
-mk->Scan("iem.mt:iem.i:Emcl[iem.i]._HMx8:iem.dphi_met","(iem.dphi_met>0.5) && (Emcl[iem.i]._HMx8<20)","colsize=15",50,200)
-mk->Scan("iem.mt:iem.i:Emcl[iem.i]._HMx8:iem.dphi_met","(Emcl[iem.i]._HMx8<20) && (iem.dphi_met>0.5)","colsize=15",50,200)
-*/
+///*
+//mk->Scan("iem.mt:iem.i:Emcl[iem.i]._HMx8:iem.dphi_met","(iem.dphi_met>0.5) && (Emcl[iem.i]._HMx8<20)","colsize=15",50,200)
+//mk->Scan("iem.mt:iem.i:Emcl[iem.i]._HMx8:iem.dphi_met","(Emcl[iem.i]._HMx8<20) && (iem.dphi_met>0.5)","colsize=15",50,200)
+//*/
 }
