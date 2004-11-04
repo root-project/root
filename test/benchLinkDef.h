d62dee2351cdbc34d89459a9c4c2aaa98317b811
Rene Brun <Rene.Brun@cern.ch>
2004-11-04 22:27:18 +0000

    Use the option "+" to force the new style Streamer for all classes in bench.
    git-svn-id: http://root.cern.ch/svn/root/trunk@10478 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/benchLinkDef.h
+++ test/benchLinkDef.h
@@ -6,25 +6,25 @@
 
 #pragma link C++ class THit!+;
 #pragma link C++ class TObjHit+;
-#pragma link C++ class TSTLhit;
-#pragma link C++ class TSTLhitList;
-#pragma link C++ class TSTLhitDeque;
-#pragma link C++ class TSTLhitSet;
-#pragma link C++ class TSTLhitMultiset;
-#pragma link C++ class TSTLhitMap;
-#pragma link C++ class TSTLhitMultiMap;
+#pragma link C++ class TSTLhit+;
+#pragma link C++ class TSTLhitList+;
+#pragma link C++ class TSTLhitDeque+;
+#pragma link C++ class TSTLhitSet+;
+#pragma link C++ class TSTLhitMultiset+;
+#pragma link C++ class TSTLhitMap+;
+#pragma link C++ class TSTLhitMultiMap+;
 //#pragma link C++ class TSTLhitHashSet;
 //#pragma link C++ class TSTLhitHashMultiset;
-#pragma link C++ class pair<int,THit>;
+#pragma link C++ class pair<int,THit>+;
 
-#pragma link C++ class TSTLhitStar;
-#pragma link C++ class TSTLhitStarList;
-#pragma link C++ class TSTLhitStarDeque;
-#pragma link C++ class TSTLhitStarSet;
-#pragma link C++ class TSTLhitStarMultiSet;
-#pragma link C++ class TSTLhitStarMap;
-#pragma link C++ class TSTLhitStarMultiMap;
-#pragma link C++ class pair<int,THit*>;
+#pragma link C++ class TSTLhitStar+;
+#pragma link C++ class TSTLhitStarList+;
+#pragma link C++ class TSTLhitStarDeque+;
+#pragma link C++ class TSTLhitStarSet+;
+#pragma link C++ class TSTLhitStarMultiSet+;
+#pragma link C++ class TSTLhitStarMap+;
+#pragma link C++ class TSTLhitStarMultiMap+;
+#pragma link C++ class pair<int,THit*>+;
 
 #pragma link C++ class TCloneshit+;
 
