dd6f38b57b42e95b01cd85da63a3d896cb0bed1a
Philippe Canal <pcanal@fnal.gov>
2004-11-17 17:32:08 +0000

    new test for geometry
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@489 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/geo/runreadGeo.C
@@ -0,0 +1,8 @@
+{
+  TGeoManager::Import("alice.root");
+  TGeoManager::Import("atlas.root");
+  TGeoManager::Import("cms.root");
+  TGeoManager::Import("hades.root");
+  TGeoManager::Import("lhcbfull.root");
+  TGeoManager::Import("lhcbnobool.root");
+}
