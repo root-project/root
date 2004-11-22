1be566fe43ea80430ad2b0564ddd8e5a15e97596
Rene Brun <Rene.Brun@cern.ch>
2004-11-22 16:01:34 +0000

    Starting Development version 4.01/05
    git-svn-id: http://root.cern.ch/svn/root/trunk@10586 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "4.01/04"
-#define ROOT_VERSION_CODE 262404
+#define ROOT_RELEASE "4.01/05"
+#define ROOT_VERSION_CODE 262405
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
