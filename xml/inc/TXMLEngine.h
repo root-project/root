5a9705aadbf4fcf5ca00f6c85104d0f5155d4d6f
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-12-22 16:50:08 +0000

    Reintroduce proper cvs tokens.
    git-svn-id: http://root.cern.ch/svn/root/trunk@10843 27541ba8-7e3a-0410-8455-c3a389f83636

--- xml/inc/TXMLEngine.h
+++ xml/inc/TXMLEngine.h
@@ -1,3 +1,14 @@
+// @(#)root/xml:$Name$:$Id$
+// Author: Sergey Linev  10.05.2004
+
+/*************************************************************************
+ * Copyright (C) 1995-2004, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 #ifndef ROOT_TXMLEngine
 #define ROOT_TXMLEngine
 
