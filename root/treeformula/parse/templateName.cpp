6467daab82811fb2543c78e6bbd8f455d5a6e389
Philippe Canal <pcanal@fnal.gov>
2005-01-05 20:34:11 +0000

    complete proxy test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@513 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/templateName.cpp
+++ root/treeformula/parse/templateName.cpp
@@ -14,22 +14,26 @@ struct C : B<void> { int j; };
 void plot_my_i()
 {
   C c, *cp(&c);
+  C c2, *cp2(&c2);
   
   TTree *t = new TTree("mytree", "Icecube", 10000);
 
   t->Branch("my_c", "C", &cp);
-  t->Branch("my_c2", "C", &cp);
+  t->Branch("my_c2", "C", &cp2);
   t->Branch("plot_my_i.", "C", &cp);
-  t->Branch("against_my_i.", "C", &cp);
+  t->Branch("against_my_i.", "C", &cp2);
 
   for (int i=0; i<100; i++)
     {
       c.i=i*13;
+      c2.i=i*26;
       t->Fill();
     }
   t->Print();
   t->Draw("forproxy.C+","","goff");
   t->Draw("plot_my_i.B<void>.i:against_my_i.B<void>.i");
+  t->Draw("plot_my_i.i:against_my_i.i");
+  t->ResetBranchAddresses();
   //t->StartViewer();
 }
 
@@ -37,7 +41,7 @@ struct C2 { int i; };
 void makeclass()
 {
   C2 c, *cp(&c);
-  TTree *t = new TTree("mytree", "foo", 10000);
+  TTree *t = new TTree("mytreemk", "foo", 10000);
   t->Branch("my_c", "C2", &cp);
   t->Branch("other_c.", "C2", &cp);
   t->Fill();
@@ -45,6 +49,7 @@ void makeclass()
   t->MakeClass("cmakeclass");
   t->MakeProxy("cmakeproxy","forproxy.C");
   t->Draw("forproxy.C+","","goff");
+  t->ResetBranchAddresses();
 }
 
 void templateName() { plot_my_i(); makeclass(); };
