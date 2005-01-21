ef027b12cbe8c4e35ffc2c468ab5283c0a509d45
Philippe Canal <pcanal@fnal.gov>
2005-01-21 20:46:25 +0000

    request nohist
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@532 7c735d0d-823a-0410-acfd-cd159556d4bf

--- experiments/treeproxytest/testProxy.C
+++ experiments/treeproxytest/testProxy.C
@@ -4,7 +4,7 @@ TFile* mcfile = new TFile("f24100001_0000.sntp.R1.7.root","READ");
 
 TTree* mctree = (TTree*)(mcfile->Get("NtpMC"));
 
-mctree -> MakeProxy("analyzeNtp","print.C","");
+mctree -> MakeProxy("analyzeNtp","print.C","","nohist");
 mctree -> Process("analyzeNtp.h+","",20);
 
 
