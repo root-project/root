366773d6afdb8a07008eb5287efeaf14c8b82d3c
Philippe Canal <pcanal@fnal.gov>
2005-01-25 18:35:02 +0000

    remove platform dependent useless warning
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@540 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/addresses/runtreeBranch.C
+++ root/tree/addresses/runtreeBranch.C
@@ -2,5 +2,6 @@
 // Fill out the code of the actual test
    gROOT->ProcessLine(".L userClass.C+");
    gROOT->ProcessLine(".x treeBranch.C");
+   gROOT->ProcessLine(".U treeBranch.C");
    gROOT->ProcessLine(".x treeBranch.C+");
 }
