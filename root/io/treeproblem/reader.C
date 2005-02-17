beb825cf08a721a95591307f92bd02dd47b9f069
Philippe Canal <pcanal@fnal.gov>
2005-02-17 14:38:01 +0000

    disable SetBranchAddress check
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@560 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/treeproblem/reader.C
+++ root/io/treeproblem/reader.C
@@ -29,7 +29,7 @@ int reader()
   TFile* file     = TFile::Open("file.root", "READ"); 
   TTree* tree     = (TTree*)file->Get("tree");
   tree->SetMakeClass(1);
-  tree->SetBranchAddress("foo", &foo_);
+  tree->SetBranchAddress("foo", (void*)&foo_);
   tree->SetBranchAddress("foo.fFoo", &foo_fFoo);
   
   Int_t  n = Int_t(tree->GetEntries());
