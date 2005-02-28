007a1e33c662cae971feb3d8294d8a460e0d9dce
Philippe Canal <pcanal@fnal.gov>
2005-02-28 16:15:30 +0000

    rename TParameter into Parameter
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@577 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/template/longlong.C
+++ cint/template/longlong.C
@@ -1,16 +1,16 @@
 #include "TObject.h"
 template <class AParamType>
-class TParameter : public TObject {
+class Parameter : public TObject {
 
 private:
    AParamType  fVal;
-   ClassDef(TParameter,1);
+   ClassDef(Parameter,1);
 };
 
 #ifdef __MAKECINT__
-#pragma link C++ class TParameter<Long64_t>+;
-#pragma link C++ class TParameter<ULong64_t>+;
-#pragma link C++ class TParameter<long long>+;
-#pragma link C++ class TParameter<unsigned long long>+;
+#pragma link C++ class Parameter<Long64_t>+;
+#pragma link C++ class Parameter<ULong64_t>+;
+#pragma link C++ class Parameter<long long>+;
+#pragma link C++ class Parameter<unsigned long long>+;
 //#pragma link C++ class TParameter<const int>+;
 #endif
