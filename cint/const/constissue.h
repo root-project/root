51d21f5a8d256dbe38099eb51a826481099e82b4
Philippe Canal <pcanal@fnal.gov>
2005-03-04 16:18:08 +0000

    add one more test for dealing with const template argument .. still not complete ;(
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@579 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/const/constissue.h
+++ cint/const/constissue.h
@@ -1,7 +1,24 @@
 int get_histogram(char const* , char const* ){return 0; }
 template<class T> void get_histogram(T const & t, char const * name);
 #ifdef __CINT__
+// Can't remember what this is actually testing but it should do
+// any harm anyway!
 #pragma link C++ function get_histogram;
+#endif
+
+#include <map>
+
+class TriggerCharacterizer {
+ public:
+  TriggerCharacterizer() {}
+ private:
+  typedef std::map<char const*, unsigned long> trigger_counting_map;
+
+  trigger_counting_map m_map; 
+};
+
+#ifdef __CINT__
+//#pragma link C++ class TriggerCharacterizer::trigger_counting_map;
 #endif
 
 void constissue() {}
