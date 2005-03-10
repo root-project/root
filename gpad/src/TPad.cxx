91d9769db1b3491f1ca57b5f292257ced448c110
Rene Brun <Rene.Brun@cern.ch>
2005-03-10 09:51:46 +0000

    From Richard Maunder: Remove automatic creation of TViewerPad3D from the TPad constructor
    git-svn-id: http://root.cern.ch/svn/root/trunk@11277 27541ba8-7e3a-0410-8455-c3a389f83636

--- gpad/src/TPad.cxx
+++ gpad/src/TPad.cxx
@@ -1,4 +1,4 @@
-// @(#)root/gpad:$Name:  $:$Id: TPad.cxx,v 1.164 2005/02/23 11:47:54 brun Exp $
+// @(#)root/gpad:$Name:  $:$Id: TPad.cxx,v 1.165 2005/03/09 18:19:26 brun Exp $
 // Author: Rene Brun   12/12/94
 
 /*************************************************************************
@@ -178,8 +178,7 @@ TPad::TPad()
    fWNDC    = 1;
    fHNDC    = 1;
    
-   // Must always create a 3D viewer at present - to be revisited
-   fViewer3D = new TViewer3DPad(*this);
+   fViewer3D = 0;
    
    //the following line is temporarily disabled. It has side effects
    //when the pad is a TDrawPanelHist or a TFitPanel.
@@ -242,8 +241,7 @@ TPad::TPad(const char *name, const char *title, Double_t xlow,
    fFixedAspectRatio = kFALSE;
    fAspectRatio      = 0.;
 
-   // Must always create a 3D viewer at present - to be revisited
-   fViewer3D = new TViewer3DPad(*this);
+   fViewer3D = 0;
 
 //*-*- Set default world coordinates to NDC [0,1]
    fX1 = 0;
