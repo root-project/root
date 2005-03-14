97b1c4597191ce2256d70678cdfae6df017dcebe
Fons Rademakers <Fons.Rademakers@cern.ch>
2005-03-14 15:37:05 +0000

    add the saxexample.xml file to the distribution files.
    git-svn-id: http://root.cern.ch/svn/root/trunk@11329 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makedist.sh
+++ build/unix/makedist.sh
@@ -40,6 +40,7 @@ $TAR $TARFILE $EXCLUDE $dir/LICENSE $dir/README $dir/bin \
    $dir/tutorials/stock.root $dir/tutorials/worldmap.jpg \
    $dir/tutorials/mditestbg.xpm $dir/tutorials/fore.xpm \
    $dir/tutorials/runcatalog.sql $dir/tutorials/*.py $dir/tutorials/*.rb \
+   $dir/tutorials/saxexample.xml \
    $dir/test/*.cxx $dir/test/*.h $dir/test/Makefile* $dir/test/README \
    $dir/test/RootShower/*.h $dir/test/RootShower/*.cxx \
    $dir/test/RootShower/*.rc $dir/test/RootShower/*.ico \
