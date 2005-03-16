d83535414e06fc0c4baa5ad0da8642745b25ed05
Philippe Canal <pcanal@fnal.gov>
2005-03-16 04:10:06 +0000

    add test for TClonesArray containing referenced objects in a file with more 255 pids
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@605 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/references/A.C
@@ -0,0 +1,14 @@
+#include "TRef.h"
+
+class A : public TObject { 
+public: 
+A(A* other = 0) { _o = other; }
+
+TRef _o; 
+
+ClassDef(A, 1)
+};
+
+#if defined(__MAKECINT__)
+#pragma link C++ class A+;
+#endif
