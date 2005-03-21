1f4aae1b2d8aa08fa5b20cb70db6d1577703228e
Fons Rademakers <Fons.Rademakers@cern.ch>
2005-03-21 21:34:37 +0000

    From Christian Holm Christensen: changed files for the new debian and rpm packaging.
    git-svn-id: http://root.cern.ch/svn/root/trunk@11397 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/package/lib/makedebdir.sh
+++ build/package/lib/makedebdir.sh
@@ -1,162 +1,22 @@
 #!/bin/sh
 #
-# $Id: makedebdir.sh,v 1.7 2003/01/22 11:23:02 rdm Exp $
+# $Id: makedebdir.sh,v 1.8 2004/12/06 18:32:36 rdm Exp $
 #
 # Make the debian packaging directory 
 #
-### echo %%% Various needed variables 
-base=root
-tgtdir="debian"
-cmndir=build/package/common
-libdir=build/package/lib
-debdir=build/package/debian
-vrsfil=build/version_number
-
-### echo %%% Installation directories 
-prefix=usr
-etcdir=etc/root
-docdir=${prefix}/share/doc/root-doc 
-
-### echo %%% Packages ordered by preference
-pkgs="root-daemon root-zebra root-gl root-mysql root-pgsql root-table root-shift root-cint root-bin libroot-dev libroot"
-pkgs=`./configure linuxdeb --pkglist --enable-soversion --enable-table --enable-thread --enable-shared | sed -n 's,packages: ,,p'`
-### echo %%% Package list is: $pkgs
-lvls="preinst postinst prerm postrm"
-
-
-### echo %%% ROOT version 
-major=`sed 's|\(.*\)\..*/.*|\1|' < ${vrsfil}`
-minor=`sed 's|.*\.\(.*\)/.*|\1|' < ${vrsfil}`
-revis=`sed 's|.*\..*/\(.*\)|\1|' < ${vrsfil}`
-versi="${major}.${minor}.${revis}"
 
 ### echo %%% Make the directory 
-mkdir -p ${tgtdir} 
+mkdir -p debian
 
-### echo %%% Copy the README file to the directory 
-sed -e "s,@prefix@,/${prefix},g" \
-    -e "s,@etcdir@,/${etcdir},g" \
-    < ${cmndir}/README > ${tgtdir}/root-doc.README.Debian 
+### echo %%% Copy files to directory, making subsitutions if needed
+cp -a build/package/debian/* debian/
+chmod a+x debian/rules 
+chmod a+x build/package/lib/*
 
-### echo %%% Copy root-bin menu file
-sed -e "s,@prefix@,/${prefix},g" \
-    -e "s,@etcdir@,/${etcdir},g" \
-    < ${debdir}/root-bin.menu.in > ${tgtdir}/root-bin.menu
-
-### echo %%% Copy watch file 
-cp ${debdir}/watch ${tgtdir}
-
-### echo %%% Copy mime file 
-cp ${debdir}/root-bin.mime ${tgtdir}
-
-### echo %%% make the changelog 
-${libdir}/makedebchangelog.sh $tgtdir $debdir $versi
-
-### echo %%% make the toplevel copyright file 
-${libdir}/makedebcopyright.sh $tgtdir $debdir $cmndir 
-
-### echo %%% Copy the header of the control file to debian/control 
-if [ ! -f ${debdir}/head.control.in ] ; then 
-    echo "$0: Couldn't find ${debdir}/head.control.in - very bad" 
-    echo "Giving up. Something is very screwy"
-    exit 10
-fi
-
-### echoo %%% But first we have to insert the build dependencies
-bd=""
-for i in $pkgs; do 
-    case $i in 
-    # Since we always have libxpm4-dev first, we can add a comma freely. 
-    # That is, we don't have to worry if the entry is the first in the
-    # list, because it never is. Thank god for that. 
-    root-gl)     bd="${bd}, libgl-dev" ;; 
-    root-mysql)  bd="${bd}, libmysqlclient-dev" ;;
-    root-pgsql)  bd="${bd}, postgresql-dev" ;;
-    root-pythia) bd="${bd}, libpythia-dev" ;; 
-    *) ;;
-    esac
-done
-
-### echo %%% Now insert the line 
-sed "s/@build-depends@/${bd},/" < ${debdir}/head.control.in \
-    > ${tgtdir}/control
-echo "" >> ${tgtdir}/control
-
-### echo %%% Make the sub-package stuff
-for i in ${pkgs} ; do 
-    echo "Processing for package $i ... "
-    ### echo %%% First append to the control file 
-    ${libdir}/makedebcontrol.sh $tgtdir $debdir $cmndir $i
-
-    ### echo %%% Append to the shlibs.local file 
-    ${libdir}/makedebshlocal.sh $tgtdir $debdir $cmndir $versi $major $minor $i
-
-    ### echo %%% Then make the file lists
-    ${libdir}/makedebfiles.sh $tgtdir $cmndir $prefix $etcdir $docdir $i      
-    ${libdir}/makedebconffiles.sh $tgtdir $cmndir $etcdir $i      
-    ${libdir}/makedebdocs.sh $tgtdir $cmndir $prefix $etcdir $docdir $i      
-    ${libdir}/makedebexamples.sh $tgtdir $cmndir $prefix $etcdir $docdir $i
-
-    ### echo %%% Make copyright file 
-    ${libdir}/makedebcopyright.sh $tgtdir $debdir $cmndir $i 
-
-    ### echo %%% make the kinds of scripts 
-    for j in $lvls ; do 
-        ${libdir}/makedebscr.sh $tgtdir $debdir $cmndir \
-            $prefix $etcdir $docdir $i $j 
-    done 
-
-    ### echo %%% Update the rules file 
-    # if [ "x$i" != "xtask" ] ; then 
-    # 	${libdir}/makedebrules.sh $i 
-    # fi
-done 
-
-### echo %%% Copy the skeleton rules file to 
-if [ ! -f ${debdir}/rules.in ] ; then 
-    echo "$0: I cannot find the ESSENTIAL file ${debdir}/rules.in"
-    echo "Giving up. Something is very screwy"
-    exit 10
-fi
-### echo %%% Make the rules file 
-sed -e "s,@prefix@,/${prefix},g" \
-    -e "s,@etcdir@,/${etcdir},g" \
-    -e "s,@docdir@,/${docdir},g" \
-    -e "s,@pkgs@,${pkgs},g"      \
-    < ${debdir}/rules.in > ${tgtdir}/rules
-chmod 755 ${tgtdir}/rules
+### echo %%% Make skeleton control file 
+# cat build/package/debian/control.in build/package/common/*.control \
+#     > debian/control.in 
 
 #
-# $Log: makedebdir.sh,v $
-# Revision 1.7  2003/01/22 11:23:02  rdm
-# big patch adding full freetype 2 TTF support to the ROOT core and X11
-# and Win32gdk displaying modules. TTF support provided by Olivier Couet
-# and Bertrand Bellenot (for Win32), build system updated by me.
-# Also fixes the RH8.0 installation problems of rootd and proofd.
-#
-# Revision 1.6  2002/07/31 20:45:10  rdm
-# modified files for Debian and RedHat packaging. By Christian Holm.
-#
-# Revision 1.5  2002/05/27 16:27:56  rdm
-# rename libStar to libTable.
-#
-# Revision 1.4  2002/05/14 15:45:28  rdm
-# several Debian related packaging and build changes. By Christian Holm.
-#
-# Revision 1.3  2002/01/22 10:53:28  rdm
-# port to Debian distribution of GNU/Hurd by Christian Holm.
-#
-# Revision 1.2  2002/01/20 14:23:52  rdm
-# Mega patch by Christian Holm concerning the configure, build and
-# Debian and RedHat packaging scripts. The configure script has been
-# rationalized (introduction of two shell functions to find package
-# headers and libraries). Extensive update of the INSTALL writeup,
-# including description of all new packages (SapDB, PgSql, etc.).
-# More options to the root-config script. Man page for memprobe.
-# Big overhaul of the Debian and RedHat packaging scripts, supporting
-# the new libraries.
-#
-# Revision 1.1  2001/04/23 14:11:47  rdm
-# part of the debian and redhat build system.
-#
+# EOF
 #
--- build/package/lib/makerpmspec.sh
+++ build/package/lib/makerpmspec.sh
@@ -1,95 +1,101 @@
 #!/bin/sh -e 
 #
-# $Id: makerpmspec.sh,v 1.5 2002/07/31 20:45:10 rdm Exp $
+# $Id: makerpmspec.sh,v 1.6 2003/01/22 11:23:02 rdm Exp $
 #
 # Make the rpm spec file in ../root.spec
 #
 #
 ### echo %%% Some general variables 
-base=root
-cmndir=build/package/common
-libdir=build/package/lib
-rpmdir=build/package/rpm 
-vrsfil=build/version_number
 tgtdir=rpm
-curdir=`pwd`
 
 ### echo %%% Packages ordered by preference
-pkgs="root-daemon root-zebra root-gl root-mysql root-pgsql root-table root-shift root-cint root-bin libroot-dev libroot"
-pkgs=`./configure linux --pkglist --enable-soversion --enable-table --enable-thread --enable-shared | sed -n 's,packages: ,,p'`
+pkglist=`./configure --pkglist					\
+		  --enable-table				\
+		  --enable-ruby					\
+		  --enable-qt					\
+		  --enable-pythia				\
+		  --enable-xrootd				\
+		  --enable-shared				\
+		  --enable-soversion				\
+		  --enable-explicitlink				\
+		  --disable-rpath				\
+		  --disable-afs					\
+		  --disable-srp					\
+		  --disable-builtin-freetype			\
+		  --disable-builtin-afterimage			\
+		| sed -n 's/packages: //p'` 
+builddepends=`build/package/lib/makebuilddepend.sh rpm $pkglist`
+dpkglist="`echo $pkglist | sed -e 's/ *ttf-root[-a-z]* *//g' -e 's/ /, /g'`, root-ttf"
 
 # ROOT version 
-major=`sed 's|\(.*\)\..*/.*|\1|' < ${vrsfil}`
-minor=`sed 's|.*\.\(.*\)/.*|\1|' < ${vrsfil}`
-revis=`sed 's|.*\..*/\(.*\)|\1|' < ${vrsfil}`
-versi="${major}.${minor}.${revis}"
+version=`cat build/version_number | tr '/' '.'`
 
-### echo %%% Make the directory 
-# mkdir -p ${tgtdir} 
-
-### echo %%% Copy the README file to the directory 
-cp ${cmndir}/README ${rpmdir}/README.Redhat
-
-### echo %%% Copy the header of the spec file to rpm/root.spec
-if [ ! -f ${rpmdir}/head.spec.in ] ; then 
-    echo "$0: Couldn't find ${rpmdir}/head.spec.in - very bad" 
-    echo "Giving up. Something is very screwy"
-    exit 10
-fi
 ### echo %%% make sure we've got a fresh file 
 rm -f root.spec
 
-### echo %%% First thing to do, is to write version number. 
-sed "s/@version@/${versi}/" < ${rpmdir}/head.spec.in > root.spec
+### echo %%% Write header stuff 
+sed -e "s/@version@/${version}/" \
+    -e "s/@builddepends@/${builddepends}/" \
+    -e "s/@pkglist@/${dpkglist}/" \
+    < build/package/rpm/spec.in > root.spec
+
+# Write out sub-package information 
+for p in $pkglist ; do 
+    echo "Adding package $p to spec file"
+    short=`sed -n 's/Description: //p' < build/package/common/$p.control`
+    long=`sed -n -e 's/^ \./ /' -e 's/^ //p' < build/package/common/$p.control`
+    cat >> root.spec <<-EOF
+	# -----------------------------------------------
+	# Package $p
+	%package -n $p
+	Summary: $short
+	Group: Applications/Physics
+	EOF
+    case $p in 
+	ttf-root*) 
+	    echo "Provides: root-ttf" 				>> root.spec 
+	    ;;
+	*xrootd|*rootd)
+	    echo "Provides: root-file-server" 			>> root.spec 
+	    ;;
+	*minuit|*fumili)
+	    echo "Provides: root-fitter"			>> root.spec
+	    ;;
+    esac
+    case $p in 
+	root-bin) 
+	    echo "Requires: root-fitter, libroot-dev" 		>> root.spec 
+	    ;;
+	libroot) 
+	    echo "Requires: root-ttf"				>> root.spec 
+	    ;; 
+	*rootd) 
+	    echo "Prefix: %_prefix" >> root.spec 
+	    ;; 
+    esac
+    cat >> root.spec <<-EOF
+	%description -n $p
+	$long
+
+	%files -n $p -f rpm/$p.install
+
+	EOF
+
+    for s in post postun pre preun ; do 
+	if test -f build/package/rpm/$p.$s ; then 
+	    echo "%$s -n $p" 			>> root.spec
+	    cat build/package/rpm/$p.$s		>> root.spec
 echo "" >> root.spec
-
-### echo %%% Make the sub-package stuff
-for i in ${pkgs} ; do 
-    echo "Processing for package $i ... "
-
-    ### echo %%% first insert the missing peices
-    ${libdir}/makerpmspecs.sh $tgtdir $cmndir $rpmdir $i >> root.spec
+	fi
 done 
-
-### echo %%% finally cat the footer to the spec file 
 echo "" >> root.spec
-
-### echo %%% Cat the tail of the file to the to rpm/root.spec
-if [ ! -f ${rpmdir}/tail.spec.in ] ; then 
-    echo "$0: Couldn't find ${rpmdir}/tail.spec.in - very bad" 
-    echo "Giving up. Something is very screwy"
-    exit 10
-fi
-
-### echo %%% and finally the last part 
-sed -e "s|@libdir@|${libdir}|" \
-    -e "s|@cmndir@|${cmndir}|" \
-    -e "s|@tgtdir@|${tgtdir}|" \
-    -e "s|@pkglist@|${pkgs}|" \
-    < ${rpmdir}/tail.spec.in >> root.spec
+done 
+cat >> root.spec <<EOF
+#
+# End of ROOT spec file
+#
+EOF
 
 #
-# $Log: makerpmspec.sh,v $
-# Revision 1.5  2002/07/31 20:45:10  rdm
-# modified files for Debian and RedHat packaging. By Christian Holm.
-#
-# Revision 1.4  2002/05/27 16:27:56  rdm
-# rename libStar to libTable.
-#
-# Revision 1.3  2002/01/22 10:53:28  rdm
-# port to Debian distribution of GNU/Hurd by Christian Holm.
-#
-# Revision 1.2  2002/01/20 14:23:52  rdm
-# Mega patch by Christian Holm concerning the configure, build and
-# Debian and RedHat packaging scripts. The configure script has been
-# rationalized (introduction of two shell functions to find package
-# headers and libraries). Extensive update of the INSTALL writeup,
-# including description of all new packages (SapDB, PgSql, etc.).
-# More options to the root-config script. Man page for memprobe.
-# Big overhaul of the Debian and RedHat packaging scripts, supporting
-# the new libraries.
-#
-# Revision 1.1  2001/04/23 14:11:47  rdm
-# part of the debian and redhat build system.
-#
+# EOF
 #
