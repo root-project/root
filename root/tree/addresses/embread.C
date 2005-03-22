641a2f44045f4086bcdf41daee195e0ca2580e17
Philippe Canal <pcanal@fnal.gov>
2005-03-22 23:17:03 +0000

    remove 64bit warning
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@624 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/addresses/embread.C
+++ root/tree/addresses/embread.C
@@ -5,8 +5,8 @@ TTree *tree = (TTree*)f->Get("T");
 Normal_objects* obj = new Normal_objects();
 TBranch *branch  = tree->GetBranch("B");
 branch->SetAddress(&obj);
-Int_t nevent = Int_t(tree->GetEntries());
+Int_t nevent = (Int_t)(tree->GetEntries());
 Int_t nselected = 0;
 Int_t nb = 0;
-for (Int_t i=0;i<nevent;i++) { printf("%d %d \n", i, tree->GetEvent(i)); obj->dump(); }
+for (Int_t i=0;i<nevent;i++) { printf("%d %ld \n", i, tree->GetEvent(i)); obj->dump(); }
 }
