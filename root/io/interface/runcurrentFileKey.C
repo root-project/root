cb125950f5e9cb2a6192f8c3011af47d9d9bcf65
Philippe Canal <pcanal@fnal.gov>
2005-04-07 13:43:21 +0000

    add a test for using TKey when current directory is not the wished for file
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@648 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/interface/runcurrentFileKey.C
@@ -0,0 +1,8 @@
+{
+   // Fill out the code of the actual test
+   TH1::AddDirectory(kFALSE);
+   TH1F * h = new TH1F("h","h",100,0,10);
+   TFile *f = new TFile("output.root","RECREATE");
+   TFile *readonly = new TFile("Toplevel.root","READ");
+   f->WriteTObject(h); // This would also fails with WriteObject or WriteObjectAny
+}
