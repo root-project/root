f57a61e4046145f6caaa7bb5910b8069d0c560b2
Rene Brun <Rene.Brun@cern.ch>
2005-04-07 15:54:09 +0000

    Start development cycle 4.03/05
    git-svn-id: http://root.cern.ch/svn/root/trunk@11524 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "4.03/04"
-#define ROOT_VERSION_CODE 262916
+#define ROOT_RELEASE "4.03/05"
+#define ROOT_VERSION_CODE 262917
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
