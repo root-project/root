1dcd5f4b96711c7127c36cdff489111ee781b01d
Rene Brun <Rene.Brun@cern.ch>
2005-04-23 11:01:34 +0000

    Rename hworld.cxx to hworld2.cxx Introduce a simplified version of hworld.cxx (Valeriy)
    git-svn-id: http://root.cern.ch/svn/root/trunk@11661 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/hworld2.cxx
+++ test/hworld2.cxx
@@ -15,13 +15,25 @@ int main(int argc, char **argv)
    TApplication theApp("App", &argc, argv);
 
    TCanvas *c = new TCanvas("c", "The Hello Canvas", 400, 400);
-   //c->Connect("Closed()", "TApplication", &theApp, "Terminate()");
 
    TPaveLabel *hello = new TPaveLabel(0.2,0.4,0.8,0.6,"Hello World");
    hello->Draw();
    TPaveLabel *quit = new TPaveLabel(0.2,0.2,0.8,0.3,"Close via menu File/Quit");
    quit->Draw();
    c->Update();
+
+   // Enter event loop, one can now interact with the objects in
+   // the canvas. Select "Exit ROOT" from Canvas "File" menu to exit
+   // the event loop and execute the next statements.
+   theApp.Run(kTRUE);
+
+   TLine *l = new TLine(0.1,0.2,0.5,0.9);
+   l->Draw();
+   quit->SetLabel("Select File/Quit again to exit app");
+   c->Update();
+
+   // Here we don't return from the eventloop. "Exit ROOT" will quit the app.
    theApp.Run();
+
    return 0;
 }
