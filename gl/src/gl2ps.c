dcdaf332e27dd8243a4c8207b92cdb6032249c5a
Rene Brun <Rene.Brun@cern.ch>
2005-04-30 19:47:41 +0000

    Variables 'backprim' and 'frontprim' may be used uninitialized in gl2psBuildBspTree
    git-svn-id: http://root.cern.ch/svn/root/trunk@11710 27541ba8-7e3a-0410-8455-c3a389f83636

--- gl/src/gl2ps.c
+++ gl/src/gl2ps.c
@@ -1,4 +1,4 @@
-/* $Id: gl2ps.c,v 1.200 2004/12/21 22:25:47 geuzaine Exp $ */
+/* $Id: gl2ps.c,v 1.1 2005/01/20 17:28:14 brun Exp $ */
 /*
  * GL2PS, an OpenGL to PostScript Printing Library
  * Copyright (C) 1999-2004 Christophe Geuzaine <geuz@geuz.org>
@@ -1208,7 +1208,7 @@ static GLboolean gl2psLess(GLfloat f1, GLfloat f2)
 
 static void gl2psBuildBspTree(GL2PSbsptree *tree, GL2PSlist *primitives)
 {
-  GL2PSprimitive *prim, *frontprim, *backprim;
+  GL2PSprimitive *prim=NULL, *frontprim=NULL, *backprim=NULL;
   GL2PSlist *frontlist, *backlist;
   GLint i, index;
 
