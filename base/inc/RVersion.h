f7c436a5b1596ef71c4811b960419659f0ddb4b9
Rene Brun <Rene.Brun@cern.ch>
2005-05-06 08:56:08 +0000

    Starting Major New version 5
    git-svn-id: http://root.cern.ch/svn/root/trunk@11739 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,8 +14,8 @@
  *
 */
 
-#define ROOT_RELEASE "4.04/02"
-#define ROOT_VERSION_CODE 263170
+#define ROOT_RELEASE "5.01/01"
+#define ROOT_VERSION_CODE 327937
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
