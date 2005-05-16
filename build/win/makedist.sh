d9ec7bcc88773770915fc3ba5f0e523ea453e699
Fons Rademakers <Fons.Rademakers@cern.ch>
2005-05-16 15:13:19 +0000

    take care of new gallery.root and person.xml and person.dtd.
    git-svn-id: http://root.cern.ch/svn/root/trunk@11798 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/win/makedist.sh
+++ build/win/makedist.sh
@@ -31,11 +31,12 @@ $TAR $TARFILE $EXCLUDE $dir/LICENSE $dir/README $dir/bin \
    $dir/include $dir/lib $dir/cint/MAKEINFO $dir/cint/include \
    $dir/cint/lib $dir/cint/stl $dir/tutorials/*.cxx $dir/tutorials/*.C \
    $dir/tutorials/*.h $dir/tutorials/*.dat $dir/tutorials/mlpHiggs.root \
-   $dir/tutorials/galaxy.pal.root $dir/tutorials/galaxy.root \
+   $dir/tutorials/gallery.root $dir/tutorials/galaxy.root \
    $dir/tutorials/stock.root $dir/tutorials/worldmap.jpg \
    $dir/tutorials/mditestbg.xpm $dir/tutorials/fore.xpm \
    $dir/tutorials/runcatalog.sql $dir/tutorials/*.py $dir/tutorials/*.rb \
-   $dir/tutorials/saxexample.xml \
+   $dir/tutorials/saxexample.xml $dir/tutorials/person.xml \
+   $dir/tutorials/person.dtd \
    $dir/test/*.cxx $dir/test/*.h $dir/test/Makefile* $dir/test/README \
    $dir/test/RootShower/*.h $dir/test/RootShower/*.cxx \
    $dir/test/RootShower/*.rc $dir/test/RootShower/*.ico \
