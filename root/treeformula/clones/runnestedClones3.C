9b6e40c799e6a2e07f7603184ab6d665d9658c90
Philippe Canal <pcanal@fnal.gov>
2005-05-18 21:12:03 +0000

    add one additional test for nested TClonesArray
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@688 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/treeformula/clones/runnestedClones3.C
@@ -0,0 +1,7 @@
+{
+// Fill out the code of the actual test
+   TFile *_file0 = TFile::Open("nesting3.root");
+   outTTree->Scan("GTT2.mvdrHits@.size()","","",10);
+   outTTree->Scan("GTT2.mvdrHits.r","","",10);
+   return 0;
+}
