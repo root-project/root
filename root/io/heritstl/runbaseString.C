e2b3e7ea32951714e2013942b713d7987aa8a969
Philippe Canal <pcanal@fnal.gov>
2005-06-09 18:48:28 +0000

    add a test for class inheriting from std::string
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@704 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/heritstl/runbaseString.C
@@ -0,0 +1,38 @@
+#include <string>
+#include "TFile.h"
+#include <iostream>
+
+struct MyString : public std::string {
+   MyString() {}
+   MyString(const char *input) : string(input) {}
+  virtual ~MyString() {}
+};
+
+void writeBaseString() 
+{
+   // gROOT->LoadMacro("t.C+")
+   MyString str("hello");
+   TFile fo("strtest.root","RECREATE");
+   fo.WriteObject(&str,"str");
+   fo.Close();
+}
+
+void readBaseString() 
+{
+   //gROOT->LoadMacro("t.C+")
+   TFile fi("strtest.root");
+   MyString* s;
+   fi.GetObject("str",s);
+}
+
+void runbaseString(int what = 1) 
+{
+   switch(what) {
+   case 0: writeBaseString();
+      break;
+   case 1: readBaseString();
+      break;
+   default:
+      std::cerr << "Unexpected command value " << what << std::endl;
+   }
+}
