11675a29991ade1019f1774919ccd82afe1b39c2
Rene Brun <Rene.Brun@cern.ch>
2005-06-22 18:32:06 +0000

    From Wouter Verkerke New version 2.01.Module.mk has changed nontrivially because the dictionary code is now built in 3 chunks instead of 2.
    git-svn-id: http://root.cern.ch/svn/root/trunk@12122 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/Module.mk
+++ roofit/Module.mk
@@ -11,19 +11,22 @@ ROOFITDIR    := $(MODDIR)
 ROOFITDIRS   := $(ROOFITDIR)/src
 ROOFITDIRI   := $(ROOFITDIR)/inc
 
-ROOFITVERS   := roofit_2.00
+ROOFITVERS   := roofit_2.01
 ROOFITSRCS   := $(MODDIR)/$(ROOFITVERS).src.tgz
 ROOFITETAG   := $(MODDIR)/headers.d
 
 ##### libRooFit #####
 ROOFITL1     := $(MODDIRI)/LinkDef1.h
 ROOFITL2     := $(MODDIRI)/LinkDef2.h
+ROOFITL3     := $(MODDIRI)/LinkDef3.h
 ROOFITDS1    := $(MODDIRS)/G__Roofit1.cxx
 ROOFITDS2    := $(MODDIRS)/G__Roofit2.cxx
+ROOFITDS3    := $(MODDIRS)/G__Roofit3.cxx
 ROOFITDO1    := $(ROOFITDS1:.cxx=.o)
 ROOFITDO2    := $(ROOFITDS2:.cxx=.o)
-ROOFITDS     := $(ROOFITDS1) $(ROOFITDS2)
-ROOFITDO     := $(ROOFITDO1) $(ROOFITDO2)
+ROOFITDO3    := $(ROOFITDS3:.cxx=.o)
+ROOFITDS     := $(ROOFITDS1) $(ROOFITDS2) $(ROOFITDS3)
+ROOFITDO     := $(ROOFITDO1) $(ROOFITDO2) $(ROOFITDO3)
 ROOFITDH     := $(ROOFITDS:.cxx=.h)
 
 ROOFITH1     := Roo1DTable.h Roo2DKeysPdf.h RooAbsAnaConvPdf.h RooAbsArg.h RooAbsBinning.h RooAbsCategory.h RooAbsCategoryLValue.h \
@@ -34,30 +37,31 @@ ROOFITH1     := Roo1DTable.h Roo2DKeysPdf.h RooAbsAnaConvPdf.h RooAbsArg.h RooAb
                 RooBCPEffDecay.h RooBCPGenDecay.h RooBDecay.h RooBifurGauss.h RooBinning.h RooBlindTools.h RooBMixDecay.h \
                 RooBreitWigner.h RooBrentRootFinder.h RooBukinPdf.h RooCategory.h RooCategoryProxy.h RooCatType.h RooCBShape.h \
                 RooChebychev.h RooChi2Var.h RooClassFactory.h RooCmdArg.h RooCmdConfig.h RooComplex.h RooConstVar.h RooConvCoefVar.h \
-                RooConvGenContext.h RooConvIntegrandBinding.h RooCurve.h RooCustomizer.h RooDataHist.h RooDataProjBinding.h \
-                RooDataSet.h RooDecay.h RooDirItem.h RooDouble.h RooDstD0BG.h RooEffGenContext.h RooEfficiency.h RooEffProd.h \
-                RooEllipse.h RooErrorHandler.h RooErrorVar.h RooExponential.h RooExtendPdf.h RooFitResult.h RooFormula.h \
-                RooFormulaVar.h RooGaussian.h RooGaussKronrodIntegrator1D.h RooGaussModel.h RooGenCategory.h RooGenContext.h \
-                RooGenericPdf.h RooGenProdProj.h RooGExpModel.h RooGraphEdge.h RooGraphNode.h RooGraphSpring.h RooGrid.h \
-                RooGlobalFunc.h
-ROOFITH2     := RooHashTable.h RooHistError.h RooHist.h RooHistPdf.h RooHtml.h RooImproperIntegrator1D.h RooIntegrator1D.h \
-                RooIntegrator2D.h RooIntegratorBinding.h RooInt.h RooInvTransform.h RooKeysPdf.h RooLandau.h RooLinearVar.h \
-                RooLinkedListElem.h RooLinkedList.h RooLinkedListIter.h RooLinTransBinning.h RooList.h RooListProxy.h \
-                RooMapCatEntry.h RooMappedCategory.h RooMath.h RooMCIntegrator.h RooMCStudy.h RooMinuit.h RooMPSentinel.h \
-                RooMultiCategory.h RooMultiCatIter.h RooNameReg.h RooNameSet.h RooNLLVar.h RooNonCPEigenDecay.h RooNormListManager.h \
-                RooNormManager.h RooNormSetCache.h RooNovosibirsk.h RooNumber.h RooNumConvolution.h RooNumConvPdf.h \
-                RooNumIntConfig.h RooNumIntFactory.h RooParametricStepFunction.h RooPlotable.h RooPlot.h RooPolynomial.h \
-                RooPolyVar.h RooPrintable.h RooProdGenContext.h RooProdPdf.h RooProduct.h RooPullVar.h RooQuasiRandomGenerator.h \
-                RooRandom.h RooRangeBinning.h RooRealAnalytic.h RooRealBinding.h RooRealConstant.h RooRealIntegral.h \
-                RooRealMPFE.h RooRealProxy.h RooRealSumPdf.h RooRealVar.h RooRefCountList.h RooResolutionModel.h RooScaledFunc.h \
-                RooSegmentedIntegrator1D.h RooSegmentedIntegrator2D.h RooSetPair.h RooSetProxy.h RooSimGenContext.h \
-                RooSimPdfBuilder.h RooSimultaneous.h RooStreamParser.h RooStringVar.h RooSuperCategory.h RooTable.h \
-                RooThreshEntry.h RooThresholdCategory.h RooTObjWrap.h RooTrace.h RooTreeData.h RooTruthModel.h RooUnblindCPAsymVar.h \
-                RooUnblindOffset.h RooUnblindPrecision.h RooUnblindUniform.h RooUniformBinning.h RooVoigtian.h  RooFit.h
+                RooConvGenContext.h RooConvIntegrandBinding.h RooCurve.h RooCustomizer.h  RooGlobalFunc.h
+ROOFITH2     := RooDataHist.h RooDataProjBinding.h RooDataSet.h RooDecay.h RooDirItem.h RooDouble.h RooDstD0BG.h RooEffGenContext.h \
+                RooEfficiency.h RooEffProd.h RooEllipse.h RooErrorHandler.h RooErrorVar.h RooExponential.h RooExtendPdf.h \
+                RooFitResult.h RooFormula.h RooFormulaVar.h RooGaussian.h RooGaussKronrodIntegrator1D.h RooGaussModel.h \
+                RooGenCategory.h RooGenContext.h RooGenericPdf.h RooGenProdProj.h RooGExpModel.h RooGraphEdge.h RooGraphNode.h \
+                RooGraphSpring.h RooGrid.h RooHashTable.h RooHistError.h RooHist.h RooHistPdf.h RooHtml.h RooImproperIntegrator1D.h \
+                RooIntegrator1D.h RooIntegrator2D.h RooIntegratorBinding.h RooInt.h RooInvTransform.h RooKeysPdf.h \
+                RooLandau.h RooLinearVar.h RooLinkedListElem.h RooLinkedList.h RooLinkedListIter.h RooLinTransBinning.h \
+                RooList.h RooListProxy.h RooMapCatEntry.h RooMappedCategory.h RooMath.h RooMCIntegrator.h RooMCStudy.h \
+                RooMinuit.h RooMPSentinel.h RooMultiCategory.h RooMultiCatIter.h  RooFit.h
+ROOFITH3     := RooNameReg.h RooNameSet.h RooNLLVar.h RooNonCPEigenDecay.h RooNormListManager.h RooNormManager.h RooNormSetCache.h \
+                RooNovosibirsk.h RooNumber.h RooNumConvolution.h RooNumConvPdf.h RooNumIntConfig.h RooNumIntFactory.h \
+                RooParametricStepFunction.h RooPlotable.h RooPlot.h RooPolynomial.h RooPolyVar.h RooPrintable.h RooProdGenContext.h \
+                RooProdPdf.h RooProduct.h RooPullVar.h RooQuasiRandomGenerator.h RooRandom.h RooRangeBinning.h RooRealAnalytic.h \
+                RooRealBinding.h RooRealConstant.h RooRealIntegral.h RooRealMPFE.h RooRealProxy.h RooRealSumPdf.h RooRealVar.h \
+                RooRefCountList.h RooResolutionModel.h RooScaledFunc.h RooSegmentedIntegrator1D.h RooSegmentedIntegrator2D.h \
+                RooSetPair.h RooSetProxy.h RooSimGenContext.h RooSimPdfBuilder.h RooSimultaneous.h RooStreamParser.h \
+                RooStringVar.h RooSuperCategory.h RooTable.h RooThreshEntry.h RooThresholdCategory.h RooTObjWrap.h \
+                RooTrace.h RooTreeData.h RooTruthModel.h RooUnblindCPAsymVar.h RooUnblindOffset.h RooUnblindPrecision.h \
+                RooUnblindUniform.h RooUniformBinning.h RooVoigtian.h 
 
 ROOFITH1     := $(patsubst %,$(MODDIRI)/%,$(ROOFITH1))
 ROOFITH2     := $(patsubst %,$(MODDIRI)/%,$(ROOFITH2))
-ROOFITH      := $(ROOFITH1) $(ROOFITH2)
+ROOFITH3     := $(patsubst %,$(MODDIRI)/%,$(ROOFITH3))
+ROOFITH      := $(ROOFITH1) $(ROOFITH2) $(ROOFITH3)
 ROOFITS      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 ROOFITO      := $(ROOFITS:.cxx=.o)
 ROOFITO      := $(ROOFITS:.cxx=.o)
@@ -103,9 +107,14 @@ $(ROOFITLIB):   $(ROOFITO) $(ROOFITDO) $(MAINLIBS) $(ROOFITLIBDEP)
 $(ROOFITDS1):   $(ROOFITETAG) $(ROOFITH1) $(ROOFITL1) $(ROOTCINTTMP)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(ROOFITH1) $(ROOFITL1)
+
 $(ROOFITDS2):   $(ROOFITETAG) $(ROOFITH2) $(ROOFITL2) $(ROOTCINTTMP)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(ROOFITH2) $(ROOFITL2)
+
+$(ROOFITDS3):   $(ROOFITETAG) $(ROOFITH3) $(ROOFITL3) $(ROOTCINTTMP)
+		@echo "Generating dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -c $(ROOFITH3) $(ROOFITL3)
 
 $(ROOFITDO1):   $(ROOFITDS1)
 		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
@@ -113,11 +122,14 @@ $(ROOFITDO1):   $(ROOFITDS1)
 $(ROOFITDO2):   $(ROOFITDS2)
 		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
 
+$(ROOFITDO3):   $(ROOFITDS3)
+		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
+
 all-roofit:     $(ROOFITLIB)
 
 map-roofit:     $(RLIBMAP)
 		$(RLIBMAP) -r $(ROOTMAP) -l $(ROOFITLIB) \
-		   -d $(ROOFITLIBDEP) -c $(ROOFITL1) $(ROOFITL2)
+		   -d $(ROOFITLIBDEP) -c $(ROOFITL1) $(ROOFITL2) $(ROOFITL3)
 
 map::           map-roofit
 
