663380932e21385ec593f581d6ed3f16cc3d83ca
Rene Brun <Rene.Brun@cern.ch>
2005-06-23 12:01:40 +0000

    New version from Wouter fixing problems with gcc4.0
    git-svn-id: http://root.cern.ch/svn/root/trunk@12150 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/Module.mk
+++ roofit/Module.mk
@@ -11,7 +11,7 @@ ROOFITDIR    := $(MODDIR)
 ROOFITDIRS   := $(ROOFITDIR)/src
 ROOFITDIRI   := $(ROOFITDIR)/inc
 
-ROOFITVERS   := roofit_2.02
+ROOFITVERS   := roofit_2.03
 ROOFITSRCS   := $(MODDIR)/$(ROOFITVERS).src.tgz
 ROOFITETAG   := $(MODDIR)/headers.d
 
