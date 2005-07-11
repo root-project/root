188cdc37a5f7ab56216f2c8e3da57c72ee46cef3
Fons Rademakers <Fons.Rademakers@cern.ch>
2005-07-11 15:43:10 +0000

    From Axel: bash has a parser bug; the attached patch is a workaround for wingcc_ld.sh. See http://cygwin.com/ml/cygwin/2005-07/msg00357.html.
    git-svn-id: http://root.cern.ch/svn/root/trunk@12286 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/wingcc_ld.sh
+++ build/unix/wingcc_ld.sh
@@ -7,20 +7,20 @@ args=
 isdll=0
 while [ "$1" != "" ]; do
    case "$1" in
-   -o) args="$args $1"; shift; 
-       dllname="$1"; dllbase=`basename $1`; 
-       if [ "`echo $dllname | sed 's{^lib/.*\.dll${{'`" != "$dllname" ]; then
+   -o) args="$args $1"; shift;
+       dllname="$1"; dllbase=`basename $1`;
+       if [ "`echo $dllname | sed 's#^lib/.*\.dll$##'`" != "$dllname" ]; then
 	   isdll=1
-	   args="$args bin/$dllbase" 
+	   args="$args bin/$dllbase"
        else
-	   args="$args $1" 
+	   args="$args $1"
        fi ;;
    *) args="$args $1" ;;
    esac
    shift
 done
 
-# 
+#
 g++ $args \
   && ( if [ "$isdll" != "0" ]; then \
           ln -sf ../bin/$dllbase $dllname; \
