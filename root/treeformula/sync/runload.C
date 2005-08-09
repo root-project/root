bc2619604fe87ad647b5b9e857b7d8e4f22195ec
Philippe Canal <pcanal@fnal.gov>
2005-08-09 20:16:39 +0000

    add test for new TTreeFormula data loading mechanism
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@742 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/treeformula/sync/runload.C
@@ -0,0 +1,11 @@
+{
+// Fill out the code of the actual test
+   gROOT->ProcessLine(".L loadcode.C+");
+   jetwrite();
+   TFile *_file0 = TFile::Open("jetclass.root");
+   new TCanvas;
+   t.Draw("jet[].v_x[jet.nbest]","jet.nbest>=0");
+   new TCanvas;
+   t.Draw("jet[].v_x[jet.nbest+(jet.nbest==-1)]","");
+   return 0;
+}
