49ba837342678d92d1fe98b4f7d031846926e54e
Rene Brun <Rene.Brun@cern.ch>
2005-09-19 08:27:08 +0000

    Add missing module DistFunc
    git-svn-id: http://root.cern.ch/svn/root/trunk@12760 27541ba8-7e3a-0410-8455-c3a389f83636

--- mathcore/Module.mk
+++ mathcore/Module.mk
@@ -35,6 +35,7 @@ MATHCOREDH1  :=  $(MODDIRI)/Math/Vector3D.h \
                  $(MODDIRI)/Math/Quaternion.h \
                  $(MODDIRI)/Math/SpecFuncMathCore.h \
                  $(MODDIRI)/Math/ProbFuncMathCore.h \
+                 $(MODDIRI)/Math/DistFunc.h \
                  $(MODDIRI)/Math/VectorUtil_Cint.h
 
 
