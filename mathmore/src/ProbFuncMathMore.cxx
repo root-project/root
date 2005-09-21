a19bb5f1f8246a90f648050243f7741c1ea02801
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2005-09-21 09:23:42 +0000

    use correct exposed gsl include file and not internal one
    git-svn-id: http://root.cern.ch/svn/root/trunk@12791 27541ba8-7e3a-0410-8455-c3a389f83636

--- mathmore/src/ProbFuncMathMore.cxx
+++ mathmore/src/ProbFuncMathMore.cxx
@@ -1,10 +1,10 @@
-// @(#)root/mathmore:$Name:  $:$Id: ProbFuncMathMore.cxx,v 1.1 2005/09/08 07:14:56 brun Exp $
+// @(#)root/mathmore:$Name:  $:$Id: ProbFuncMathMore.cxx,v 1.2 2005/09/18 20:41:25 brun Exp $
 // Authors: L. Moneta, A. Zsenei   08/2005 
 
 
 #include <cmath>
 #include "Math/ProbFuncMathMore.h"
-#include "cdf/gsl_cdf.h"
+#include "gsl/gsl_cdf.h"
 
 
 namespace ROOT {
