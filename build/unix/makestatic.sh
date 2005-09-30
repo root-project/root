ff0bf30b15300a1d6f7a7fd6afc044e7132e8a24
Fons Rademakers <Fons.Rademakers@cern.ch>
2005-09-30 09:50:39 +0000

    exclude rpdutils and mathmore from libRoot.a.
    git-svn-id: http://root.cern.ch/svn/root/trunk@12880 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makestatic.sh
+++ build/unix/makestatic.sh
@@ -22,7 +22,7 @@ rm -f $ROOTALIB $ROOTAEXE $PROOFAEXE
 excl="main proofd rootd rootx pythia pythia6 venus mysql pgsql rfio sapdb \
       hbook newdelete table utils srputils krb5auth chirp dcache x11ttf \
       alien asimage ldap pyroot qt qtroot quadp ruby vmc xml xmlparser gl \
-      roofit oracle netx auth"
+      roofit oracle netx auth rpdutils mathmore"
 
 objs=""
 gobjs=""
