15c9eaad01d169ae4c15a21696d51c864bc5679a
Philippe Canal <pcanal@fnal.gov>
2005-11-14 17:53:00 +0000

    fix for parallel build
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@785 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/meta/DrawTest.sh
+++ root/meta/DrawTest.sh
@@ -4,8 +4,8 @@
 #root -b -q 'dt_MakeRef.C("Event.new.split9.root");'
 if [ "x`which gmake 2>/dev/null | awk '{if ($1~/gmake/) print $1;}'`" != "x" ]
 then
-   gmake -f dt_Makefile drawtest
+   gmake -f dt_Makefile drawtest -j 1
 else
-   make -f dt_Makefile drawtest
+   make -f dt_Makefile drawtest -j 1
 fi
 ./dt_RunDrawTest.sh
