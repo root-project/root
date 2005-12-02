5ec5ecb09a6cd62bf81ddcc610f3a61ba87dba52
Rene Brun <Rene.Brun@cern.ch>
2005-12-02 11:15:44 +0000

    call TTree::BranchRef by default
    git-svn-id: http://root.cern.ch/svn/root/trunk@13449 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/MainEvent.cxx
+++ test/MainEvent.cxx
@@ -1,4 +1,4 @@
-// @(#)root/test:$Name:  $:$Id: MainEvent.cxx,v 1.27 2003/12/30 18:31:13 brun Exp $
+// @(#)root/test:$Name:  $:$Id: MainEvent.cxx,v 1.28 2005/01/12 07:50:02 brun Exp $
 // Author: Rene Brun   19/01/97
 
 ////////////////////////////////////////////////////////////////////////
@@ -209,6 +209,7 @@ int main(int argc, char **argv)
       TTree::SetBranchStyle(branchStyle);
       TBranch *branch = tree->Branch("event", &event, bufsize,split);
       branch->SetAutoDelete(kFALSE);
+      tree->BranchRef();
       Float_t ptmin = 1;
 
       for (ev = 0; ev < nevent; ev++) {
