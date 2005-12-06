5a88e46972c43e35172b592a5ccfb3473cc1254e
Olivier Couet <Olivier.Couet@cern.ch>
2005-12-06 14:55:55 +0000

    - New version showing more options.
    git-svn-id: http://root.cern.ch/svn/root/trunk@13504 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/glsurfaces.C
+++ tutorials/glsurfaces.C
@@ -5,33 +5,34 @@
    
    gStyle->SetCanvasPreferGL(true);
 
+   gROOT->ProcessLine(".x surfaces.C");
+
    // after this command all legos surfaces are automatically rendered with
    // OpenGL.
    //
 
-   TCanvas *c1 = new TCanvas("c1","Surfaces Drawing Options with gl",200,10,700,700);
-   c1->SetFillColor(42);
+   TCanvas *c2 = new TCanvas("c2","Surfaces Drawing Options with gl",700,10,700,700);
+   c2->SetFillColor(42);
    gStyle->SetFrameFillColor(42);
 
-   c1->Divide(2, 2);
-   TPad *pad = static_cast<TPad *>(c1->cd(1));
+   c2->Divide(2, 2);
 
+   c2->cd(1);
    TF2 *fun1 = new TF2("fun1","x * x - y * y - 1", -6., 6., -6., 6.);
    fun1->SetFillColor(kRed);
-   fun1->Draw("surf4");
+   fun1->Draw("surf2");
 
-   pad = static_cast<TPad *>(c1->cd(2));
-
+   c2->cd(2);
    TF2 *fun2 = new TF2("fun2","x * x + y * y - 1", -6., 6., -6., 6.);
    fun2->SetFillColor(kGreen);
-   fun2->Draw("surf");
+   fun2->Draw("surf1");
 
-   pad = static_cast<TPad *>(c1->cd(3));
+   c2->cd(3);
    TF2 *fun3 = new TF2("fun3","sin(x) / x * cos(y) * y", -6., 6., -6., 6.);
    fun3->SetFillColor(kWhite);
    fun3->Draw("surf");
 
-   pad = static_cast<TPad *>(c1->cd(4));
+   c2->cd(4);
    TF2 *fun4 = new TF2("fun4","sqrt(1 - y * y - x * x)", -1., 1., -1., 1.);
    fun4->SetFillColor(kMagenta);
    fun4->Draw("surf4");
