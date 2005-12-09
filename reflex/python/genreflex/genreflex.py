eedff9f6976e6058b1b4e307b7dc79b78aa734f5
Stefan Roiser <Stefan.Roiser@cern.ch>
2005-12-09 07:10:24 +0000

    fix import of gccxmlpath.py file
    git-svn-id: http://root.cern.ch/svn/root/trunk@13572 27541ba8-7e3a-0410-8455-c3a389f83636

--- reflex/python/genreflex/genreflex.py
+++ reflex/python/genreflex/genreflex.py
@@ -24,11 +24,6 @@ class genreflex:
     self.selector        = None
     self.gccxml          = ''
     self.quiet           = False
-    try:
-      import gccxmlpath
-      self.gccxmlpath = gccxmlpath.gccxmlpath
-    except:
-      pass
 #----------------------------------------------------------------------------------
   def usage(self, status = 1) :
     print 'Usage:'
@@ -178,6 +173,11 @@ class genreflex:
         self.usage()
       for l in open(self.select).readlines() : classes.append(l[:-1])
     #----------GCCXML command------------------------------
+    try:
+      import gccxmlpath
+      self.gccxmlpath = gccxmlpath.gccxmlpath
+    except:
+      pass
     if self.gccxmlpath :
       self.gccxml = self.gccxmlpath + os.sep + 'gccxml'
     elif self.which('gccxml') :
