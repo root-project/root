ef8b48c87b028227485f46b6ad22180a64933b50
Olivier Couet <Olivier.Couet@cern.ch>
2006-01-10 16:34:07 +0000

    From Timur:
    - TF3 can be drawn using GL in Pad. See examples gltf3.C and glsurfaces.C
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@13814 27541ba8-7e3a-0410-8455-c3a389f83636

--- gl/inc/TGLHistPainter.h
+++ gl/inc/TGLHistPainter.h
@@ -20,7 +20,11 @@
 #include "TGLUtil.h"
 #endif
 
-namespace Root_GL {
+class TString;
+class TAxis;
+class TF3;
+
+namespace RootGL {
 
    template<class T>
    class T2DArray : public std::vector<T> {
@@ -49,20 +53,19 @@ namespace Root_GL {
       }
    };
    
+   struct TGLTriFace {
+      TGLVertex3 fXYZ[3];
+      TGLVector3 fNormals[3];
+   };
 }
 
-class TString;
-class TAxis;
-
-class TGLHistPainter : public TVirtualHistPainter, public TVirtualGLPainter {
-public:
-   enum {kTexLength = 16};
-
+class TGLHistPainter : public TVirtualHistPainter, private TVirtualGLPainter{
 private:
    //If gl hist painter does not support Paint option
    //it has to delegate Paint call to default hist painter
    TVirtualHistPainter *fDefaultPainter;
    TH1                 *fHist;
+   mutable TF3         *fF3;
 
    enum EGLPaintOption {
       kLego,//for lego and lego1
@@ -71,15 +74,25 @@ private:
       kSurface1,
       kSurface2,
       kSurface4,
+      kTF3,
       kUnsupported
    };
-   
+
    EGLPaintOption       fLastOption;
-   
+
+   enum EGLTF3Style {
+      kDefault,
+      kMaple0,
+      kMaple1,
+      kMaple2
+   };
+
+   EGLTF3Style         fTF3Style;
+
    TAxis               *fAxisX;
    TAxis               *fAxisY;
    TAxis               *fAxisZ;
-   
+
    Double_t             fMinX;
    Double_t             fMaxX;
    Double_t             fScaleX;
@@ -107,126 +120,110 @@ private:
    Int_t                fLastBinX;
    Int_t                fFirstBinY;
    Int_t                fLastBinY;
+   Int_t                fFirstBinZ;
+   Int_t                fLastBinZ;
 
    Bool_t               fLogX;
    Bool_t               fLogY;
    Bool_t               fLogZ;
-   
+
    std::vector<Double_t> fX;
    std::vector<Double_t> fY;
+   std::vector<Double_t> fZ;
 
-   Root_GL::T2DArray<TGLVertex3> fMesh;
-   Root_GL::T2DArray<std::pair<TGLVector3, TGLVector3> > fFaceNormals;
-   Root_GL::T2DArray<TGLVector3> fAverageNormals;
-   
+   RootGL::T2DArray<TGLVertex3> fMesh;
+   RootGL::T2DArray<std::pair<TGLVector3, TGLVector3> > fFaceNormals;
+   RootGL::T2DArray<TGLVector3> fAverageNormals;
+
+   std::vector<RootGL::TGLTriFace> fF3Mesh;
+
    std::vector<Double_t> fZLevels;
 
-   Int_t               fGLDevice;
-   mutable TGLVertex3  f2DAxes[8];
+   Int_t                fGLDevice;
+   mutable TGLVertex3   f2DAxes[8];
 
-   mutable Bool_t      f2DPass;
-   mutable UInt_t      fTextureName;
+   mutable Bool_t       f2DPass;
+   mutable UInt_t       fTextureName;
 
-   UChar_t             fTexture[kTexLength * 4];
+   std::vector<UChar_t> fTexture;
+   void (TGLHistPainter::*fCurrentPainter)()const;
+   mutable Int_t        fFrontPoint;
 
 public:
    TGLHistPainter(TH1 *hist);
    ~TGLHistPainter();
    
    //TVirtualHistPainter final overriders
-   Int_t    DistancetoPrimitive(Int_t px, Int_t py);
-   void     DrawPanel();
-   void     ExecuteEvent(Int_t event, Int_t px, Int_t py);
-   void     FitPanel();
-   TList   *GetContourList(Double_t contour)const;
-   char    *GetObjectInfo(Int_t px, Int_t py)const;
-   TList   *GetStack()const;
-   Bool_t   IsInside(Int_t x, Int_t y);
-   Bool_t   IsInside(Double_t x, Double_t y);
-   
-   void     Paint(Option_t *option);
-   void     Paint();
-   
-   void     PaintStat(Int_t dostat, TF1 *fit);
-   void     ProcessMessage(const char *mess, const TObject *obj);
-   void     SetHistogram(TH1 *hist);
-   void     SetStack(TList *stack);
-   Int_t    MakeCuts(char *cutsOpt);
-   
+   Int_t          DistancetoPrimitive(Int_t px, Int_t py);
+   void           DrawPanel();
+   void           ExecuteEvent(Int_t event, Int_t px, Int_t py);
+   void           FitPanel();
+   TList         *GetContourList(Double_t contour)const;
+   char          *GetObjectInfo(Int_t px, Int_t py)const;
+   TList         *GetStack()const;
+   Bool_t         IsInside(Int_t x, Int_t y);
+   Bool_t         IsInside(Double_t x, Double_t y);
+
+   void           Paint(Option_t *option);
+
+   void           PaintStat(Int_t dostat, TF1 *fit);
+   void           ProcessMessage(const char *mess, const TObject *obj);
+   void           SetHistogram(TH1 *hist);
+   void           SetStack(TList *stack);
+   Int_t          MakeCuts(char *cutsOpt);
+
 private:
+   EGLPaintOption SetPaintFunction(const TString &option);
+   //TVirtualGLPainter's final overrider
+   void           Paint();
+   //Texture for lego2/surf1/surf2 options
+   void           SetTexture();
+   void           EnableTexture()const;
+   void           DisableTexture()const;
+
+   void           SetZLevels();
+   void           AdjustScales();
+
+   Bool_t         SetVertices();
+   Bool_t         SetAxes();
+   void           SetTable();
+   void           SetMesh();
+   void           SetNormals();
+   void           SetAverageNormals();
+   void           SetTF3Mesh();
+
+   void           InitGL()const;
+
+   void           CalculateTransformation();
+
+   void           DrawLego()const;
+   void           DrawSurface()const;
+   void           DrawTF3()const;
+
+   void           DrawFrame()const;
+   void           DrawBackPlane(Int_t plane)const;
+   void           DrawProfile(Int_t plane)const;
+   void           DrawLegoProfileX(Int_t plane)const;
+   void           DrawLegoProfileY(Int_t plane)const;
+   void           DrawSurfaceProfileX(Int_t plane)const;
+   void           DrawSurfaceProfileY(Int_t plane)const;
+   void           DrawGrid(Int_t plane)const;
+   void           DrawZeroPlane()const;
+   void           DrawAxes()const;
+
+   Int_t          FrontPoint()const;
+
+   void           SetPlotColor()const;
+   void           SetCamera()const;
+   void           SetTransformation()const;
+   void           ClearBuffers()const;
+
+   Bool_t         Select(Int_t x, Int_t y)const;
+   void           SelectAxes(Int_t front, Int_t x, Int_t y)const;
 
    TGLHistPainter(const TGLHistPainter &);
    TGLHistPainter &operator = (const TGLHistPainter &);
 
-   void InitDefaultPainter();
-   static EGLPaintOption GetPaintOption(const TString &option);
-
-   //inner painting stuff
-   Bool_t         InitPainter();
-   void           InitTexture();
-   Bool_t         SetSizes();
-   static Bool_t  SetAxisRange(const TAxis *axis, Bool_t log, Int_t &first, Int_t &last,
-                               Double_t &min, Double_t &max);
-   void           SetZLevels();
-   void           AdjustScales();
-
-   void           FillVertices();
-   void           SetNormals();
-   void           SetAverageNormals();
-
-   void           InitGL()const;
-   
-   void           SetGLParameters();
-
-   void           PaintLego()const;
-   void           PaintLego2()const;
-   void           PaintSurface()const;
-   void           PaintSurface4()const;
-   void           PaintSurface1()const;
-
-   void           DrawFrame(Int_t frontPoint)const;
-   void           DrawBackPlane(Int_t plane)const;
-   void           DrawProfile(Int_t plane)const;
-   void           DrawLegoProfileX(Int_t plane)const;
-   void           DrawSurfaceProfileX(Int_t plane)const;
-   void           DrawSurfaceProfileY(Int_t plane)const;
-   void           DrawLegoProfileY(Int_t plane)const;
-   void           DrawGrid(Int_t plane)const;
-   void           DrawZeroPlane()const;
-   void           DrawAxes(Int_t frontPoint)const;
-   Int_t          FrontPoint()const;
-
-   typedef std::pair<Double_t, Double_t> PD_t;
-   
-   PD_t           GetMaxRowContent(Int_t row)const;
-   PD_t           GetMaxColumnContent(Int_t column)const;
-
-   void           SetCamera()const;
-   void           SetTransformation()const;
-
-   void           ClearBuffer()const;
-
-   Bool_t         Select(Int_t x, Int_t y)const;
-   void           SelectAxes(Int_t front, Int_t x, Int_t y)const;           
-   //
-   static void    DrawBoxFront(Double_t xmin, Double_t xmax, Double_t ymin, 
-                               Double_t ymax, Double_t zmin, Double_t zmax,
-                               Int_t frontPoint);
-   void           DrawBoxFrontTextured(Double_t xmin, Double_t xmax, Double_t ymin, 
-                                       Double_t ymax, Double_t zmin, Double_t zmax,
-                                       Int_t frontPoint)const;
-   static void    DrawFlatFace(const TGLVertex3 &v1, const TGLVertex3 &v2, 
-                               const TGLVertex3 &v3, const TGLVector3 &normal);
-   static void    DrawFace(const TGLVertex3 &v1, const TGLVertex3 &v2, const TGLVertex3 &v3,
-                           const TGLVector3 &norm1, const TGLVector3 &norm2, const TGLVector3 &norm3);
-   static void    DrawFaceTextured(const TGLVertex3 &v1, const TGLVertex3 &v2, 
-                                   const TGLVertex3 &v3, const TGLVector3 &norm1,
-                                   const TGLVector3 &norm2, const TGLVector3 &norm3,
-                                   Double_t zMin, Double_t zMax);
-   static void    DrawQuadOutline(const TGLVertex3 &v1, const TGLVertex3 &v2,
-                                  const TGLVertex3 &v3, const TGLVertex3 &v4);
-
-   //
    ClassDef(TGLHistPainter, 0) //GL hist painter
 };
 
--- tutorials/glsurfaces.C
+++ tutorials/glsurfaces.C
@@ -1,15 +1,54 @@
 {
-   //
-   // This example draws 4 surfaces using OpenGL in pad. The commands used
-   // are exactly the same as with a normal pad. The only command to add is:
+   // This example draws 6 surfaces using OpenGL in pad (one is remake of a classic surfaces.C, another contains 4 surfaces). 
+   // The commands used are exactly the same as with a normal pad. 
+   // The only command to add is: gStyle->SetCanvasPreferGL(true);
+
+   gROOT->Reset();
    
+   // after this command all legos surfaces (surf/srf1/surf2/surf4/tf3 options)
+   // are automatically rendered with OpenGL.
    gStyle->SetCanvasPreferGL(true);
 
-   gROOT->ProcessLine(".x surfaces.C");
+   TCanvas *c1 = new TCanvas("c1","Surfaces Drawing Options",200,10,700,900);
+   c1->SetFillColor(42);
+   gStyle->SetFrameFillColor(42);
+   title = new TPaveText(0.2, 0.96, 0.8, 0.995);
+   title->SetFillColor(33);
+   title->AddText("Examples of Surface options");
+   title->Draw();
 
-   // after this command all legos surfaces are automatically rendered with
-   // OpenGL.
-   //
+   TPad *pad1 = new TPad("pad1","Gouraud shading", 0.03, 0.50, 0.98, 0.95, 21);
+   TPad *pad2 = new TPad("pad2","Color mesh", 0.03, 0.02, 0.98, 0.48, 21);
+   pad1->Draw();
+   pad2->Draw();
+   // We generate a 2-D function
+   TF2 *f2 = new TF2("f2","x**2 + y**2 - x**3 -8*x*y**4", -1., 1.2, -1.5, 1.5);
+   // Draw this function in pad1 with Gouraud shading option
+   pad1->cd();
+   pad1->SetLogz();
+   f2->SetFillColor(45);
+   f2->Draw("surf4");
+
+   TF2 *f2clone = new TF2("f2","x**2 + y**2 - x**3 -8*x*y**4", -1., 1.2, -1.5, 1.5);
+   // Draw this function in pad2 with color mesh option
+   pad2->cd();
+   pad2->SetLogz();
+   f2clone->Draw("surf1");
+   
+   //add axis titles. The titles are set on the intermediate
+   //histogram used for visualisation. We must force this histogram
+   //to be created, then force the redrawing of the two pads
+   pad2->Update();
+   f2->GetHistogram()->GetXaxis()->SetTitle("x title");
+   f2->GetHistogram()->GetYaxis()->SetTitle("y title");
+   f2->GetHistogram()->GetXaxis()->SetTitleOffset(1.4);
+   f2->GetHistogram()->GetYaxis()->SetTitleOffset(1.4);
+   f2clone->GetHistogram()->GetXaxis()->SetTitle("x title");
+   f2clone->GetHistogram()->GetYaxis()->SetTitle("y title");
+   f2clone->GetHistogram()->GetXaxis()->SetTitleOffset(1.4);
+   f2clone->GetHistogram()->GetYaxis()->SetTitleOffset(1.4);
+   pad1->Modified();
+   pad2->Modified();
 
    TCanvas *c2 = new TCanvas("c2","Surfaces Drawing Options with gl",700,10,700,700);
    c2->SetFillColor(42);
@@ -33,7 +72,7 @@
    fun3->Draw("surf");
 
    c2->cd(4);
-   TF2 *fun4 = new TF2("fun4","sqrt(1 - y * y - x * x)", -1., 1., -1., 1.);
+   TF2 *fun4 = new TF3("TF3 sample","x * x + y * y + z * z - 4", -2.5, 2.5, -2.5, 2.5, -2.5, 2.5);
    fun4->SetFillColor(kMagenta);
-   fun4->Draw("surf4");
+   fun4->Draw();//tf3 option
 }
