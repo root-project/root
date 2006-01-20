32224db12a7221e119e9617959a5963ed8d21b79
Stefan Roiser <Stefan.Roiser@cern.ch>
2006-01-20 17:27:05 +0000

    several changes for the rootcint wrapper to genreflex - include more standard headers by default - change cpp selection name - exclude templated abstract classes (for the time being) - remove -DSYSV from the invocation to gccxml - remove trailing spaces before the semi colon in the Linkdef statements
    git-svn-id: http://root.cern.ch/svn/root/trunk@13879 27541ba8-7e3a-0410-8455-c3a389f83636

--- reflex/python/genreflex/genreflex-rootcint.py
+++ reflex/python/genreflex/genreflex-rootcint.py
@@ -11,6 +11,12 @@ class genreflex_rootcint:
     self.sel_classes = []
     self.sel_classesT = []
     self.gccxml_ppopts = []
+    # this list is quite unfortunate but at the moment gccxml (0.6.0_patch3) will
+    # need instances of templated classes to generate information. This will of
+    # course not work for abstract ones. And there is no way to retrieve this info
+    # from the Linkdef information only, also needed for templated classes without
+    # default constructor
+    self.abstractTemplateClasses = ['TMatrixTBase','TMatrixTLazy','TMatrixTSymLazy','ROOT::Math::WrappedFunction']
     
   def usage(self):
     pass
@@ -79,13 +85,19 @@ class genreflex_rootcint:
             if pl[3] == 'class':
               rl = ' '.join(pl[4:])
               rl = rl[:rl.find(';')]
-              while rl[-1] in ['+','-','!'] : rl = rl[:-1]
+              while rl[-1] in ['+','-','!',' '] : rl = rl[:-1]
               if rl.find('<') != -1 : self.sel_classesT.append(rl)
               else                  : self.sel_classes.append(rl)
 
   def gen_temp_header(self):
     self.dict_header = self.dict_filename.split('.')[0]+'.h'
     hh = open(self.dict_header,'w')
+
+    hh.write("#include <stddef.h>\n")
+    hh.write("#include <stdio.h>\n")
+    hh.write("#include <stdlib.h>\n")
+    hh.write("#include <math.h>\n")
+    hh.write("#include <string.h>\n")
 
     hh.write('using namespace std;\n\n')
 
@@ -96,7 +108,7 @@ class genreflex_rootcint:
     for f in self.header_files:
       hh.write('#include "%s"\n' % f)
 
-    hh.write('\n\nnamespace ROOT {\n  namespace Reflex {\n    namespace Select {\n')
+    hh.write('\n\nnamespace ROOT {\n  namespace Reflex {\n    namespace Selection {\n')
     for c in self.sel_classes:
       # let's assume there are no namespaces in ROOT
       # inner classes are automatically selected by genreflex
@@ -108,8 +120,9 @@ class genreflex_rootcint:
       hh.write('namespace {\n')
       i = 0
       for c in self.sel_classesT:
-        hh.write('  %s inst%d;\n' % (c,i))
-        i += 1
+	if c.split('<')[0].strip() not in self.abstractTemplateClasses:
+	  hh.write('  %s inst%d;\n' % (c,i))
+	  i += 1
       hh.write('}\n\n')
     
 if __name__ == "__main__":
@@ -120,8 +133,11 @@ if __name__ == "__main__":
   rc.gen_temp_header()
 
   gr = genreflex.genreflex()
-  gr_args = ['',rc.dict_header,'-o',rc.dict_filename,'--comments','-I.','-Iinclude','-DTRUE=1','-DFALSE=0','-Dexternalref=extern','-DSYSV','-D__MAKECINT__','-DR__EXTERN=extern']
+  # ,'-D__MAKECINT__'
+  gr_args = ['',rc.dict_header,'--quiet','-o',rc.dict_filename,'--comments','-I.','-Iinclude','-DTRUE=1','-DFALSE=0','-Dexternalref=extern','-DR__EXTERN=extern']
   gr_args += rc.gccxml_ppopts
+  for arg in gr_args :
+    if arg in ('-DSYSV',) : gr_args.remove(arg)
   gr.parse_args(gr_args)
   gr.check_files_dirs()
   gr.process_files()
