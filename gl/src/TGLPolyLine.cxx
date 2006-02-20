b27a7abfe1c73b0742c7aae268517e574daf09e2
Rene Brun <Rene.Brun@cern.ch>
2006-02-20 11:10:06 +0000

    From Richard: New files
    git-svn-id: http://root.cern.ch/svn/root/trunk@14065 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ gl/src/TGLPolyLine.cxx
@@ -0,0 +1,48 @@
+// @(#)root/gl:$Name:  $:$Id: TGLPloyLine.cxx $
+// Author:  Timur Pocheptsov  03/08/2004
+// NOTE: This code moved from obsoleted TGLSceneObject.h / .cxx - see these
+// attic files for previous CVS history
+
+/*************************************************************************
+ * Copyright (C) 1995-2006, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+#include "TGLPolyLine.h"
+#include "TGLDrawFlags.h"
+#include "TGLIncludes.h"
+
+#include "TBuffer3D.h"
+#include "TBuffer3DTypes.h"
+
+// For debug tracing
+#include "TClass.h" 
+#include "TError.h"
+
+ClassImp(TGLPolyLine)
+
+//______________________________________________________________________________
+TGLPolyLine::TGLPolyLine(const TBuffer3D & buffer) :
+   TGLLogicalShape(buffer),
+   fVertices(buffer.fPnts, buffer.fPnts + 3 * buffer.NbPnts())
+{
+   //
+}
+
+//______________________________________________________________________________
+void TGLPolyLine::DirectDraw(const TGLDrawFlags & flags) const
+{
+   // Debug tracing
+   if (gDebug > 4) {
+      Info("TGLPolyLine::DirectDraw", "this %d (class %s) LOD %d", this, IsA()->GetName(), flags.LOD());
+   }
+
+   glBegin(GL_LINE_STRIP);
+
+   for (UInt_t i = 0; i < fVertices.size(); i += 3)
+      glVertex3d(fVertices[i], fVertices[i + 1], fVertices[i + 2]);
+
+   glEnd();
+}
