86ef2e5cbe119fc6b673630a2a2c573ef12b0def
Philippe Canal <pcanal@fnal.gov>
2006-03-02 19:29:41 +0000

    add Nick's CopyTree test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@846 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/cloning/rundeepClass.C
@@ -0,0 +1,13 @@
+{
+  TFile OriginalFile("ver_40200.root");
+  TFile CopyFile("CopyTree.root");
+
+  TTree* OriginalTree = (TTree*) OriginalFile.Get("NtpSt");
+  TTree* CopyTree     = (TTree*) CopyFile.Get("NtpSt"); 
+
+  OriginalTree.Scan("fHeader.fVldContext.fDetector:fHeader.fVldContext.fSimFlag:fHeader.fVldContext.fTimeStamp.fSec","","colsize=20",10);
+  CopyTree.Scan("fHeader.fVldContext.fDetector:fHeader.fVldContext.fSimFlag:fHeader.fVldContext.fTimeStamp.fSec","","colsize=20",10);
+
+  return 0;  
+}
+
