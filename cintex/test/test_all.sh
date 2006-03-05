95f21105ae1cdc6ec63c060d65fc88759272126f
Stefan Roiser <Stefan.Roiser@cern.ch>
2006-03-05 21:43:26 +0000

    From Pere: Fixes for cintex test suite
    git-svn-id: http://root.cern.ch/svn/root/trunk@14170 27541ba8-7e3a-0410-8455-c3a389f83636

--- cintex/test/test_all.sh
+++ cintex/test/test_all.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 export ROOTSYS=`pwd`
-export PYTHONDIR=$1/..
+export PYTHONDIR=$1/../../bin
 
 export PATH=${ROOTSYS}/bin:${PATH}
-export PYTHONPATH=${ROOTSYS}/lib;${PYTHONPATH}
+export PYTHONPATH=${ROOTSYS}/lib:${PYTHONPATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ROOTSYS}/lib:${PYTHONDIR}/../lib:${ROOTSYS}/cintex/test/dict
 
 bin/root -b -q -l cintex/test/test_Cintex.C
 bin/root -b -q -l cintex/test/test_Persistency.C
-${PYTHONDIR}/python cintex/test/test_PyCintex_basics.py
+${PYTHONDIR}/python cintex/test/test_PyCintex_basics.py -b
