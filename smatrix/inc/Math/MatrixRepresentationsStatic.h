c462564565876ed6ce5712925863b57a6ef16934
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2006-03-17 15:11:35 +0000

    create the RowOffsets structure in constructor of MatRepSym and store a local pointer to it. This should preserve performance in Offset() method and there is no need to have the specialization of the structure. The problem reported by LHCb shoould then be fixed.
    git-svn-id: http://root.cern.ch/svn/root/trunk@14298 27541ba8-7e3a-0410-8455-c3a389f83636

--- smatrix/inc/Math/MatrixRepresentationsStatic.h
+++ smatrix/inc/Math/MatrixRepresentationsStatic.h
@@ -1,4 +1,4 @@
-// @(#)root/smatrix:$Name:  $:$Id: MatrixRepresentationsStatic.h,v 1.3 2006/03/08 15:11:06 moneta Exp $
+// @(#)root/smatrix:$Name:  $:$Id: MatrixRepresentationsStatic.h,v 1.4 2006/03/14 17:11:19 moneta Exp $
 // Authors: L. Moneta, J. Palacios    2006  
 
 #ifndef ROOT_Math_MatrixRepresentationsStatic_h
@@ -87,6 +87,15 @@ namespace ROOT {
     };
     
     
+//     template<unigned int D>
+//     struct Creator { 
+//       static const RowOffsets<D> & Offsets() {
+// 	static RowOffsets<D> off;
+// 	return off;
+//       }
+
+
+
     template<unsigned int D>
     struct RowOffsets {
       RowOffsets() {
@@ -106,77 +115,13 @@ namespace ROOT {
       int fOff[D*D];
     };
 
-    // offset specializations for small matrix sizes
-    // offset specializations for small matrix sizes
-
-    int ind2[4] = { 0 , 1  ,  1 , 2   };
-    template<>
-    struct RowOffsets<2> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind2[i*2+j]; }
-      inline int apply(unsigned int i) const { return ind2[i]; }
-    };
-
-    int ind3[9] = { 0 , 1 , 3 , 1 , 2 , 4 , 3 , 4 , 5   };
-    template<>
-    struct RowOffsets<3> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind3[i*3+j]; }
-      inline int apply(unsigned int i) const { return ind3[i]; }
-    };
-
-    int ind4[16] = { 0 , 1 , 3 , 6 ,  1 , 2 , 4 , 7  ,  3 , 4 , 5 , 8  ,  6 , 7 , 8 , 9   };
-    template<>
-    struct RowOffsets<4> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind4[i*4+j]; }
-      inline int apply(unsigned int i) const { return ind4[i]; }
-    };
-
-    int ind5[25] = { 0 , 1 , 3 , 6 , 10 ,  1 , 2 , 4 , 7 , 11 ,  3 , 4 , 5 , 8 , 12 ,  6 , 7 , 8 , 9 , 13 ,  10 , 11 , 12 , 13 , 14   };                       
-    template<>
-    struct RowOffsets<5> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind5[i*5+j]; }
-      inline int apply(unsigned int i) const { return ind5[i]; }
-    };
-
-    int ind6[36] = { 0 , 1 , 3 , 6 , 10 , 15 , 1 , 2 , 4 , 7 , 11 , 16 ,  3 , 4 , 5 , 8 , 12 , 17 ,  6 , 7 , 8 , 9 , 13 , 18 ,  10 , 11 , 12 , 13 , 14 , 19 ,  15 , 16 , 17 , 18 , 19 , 20   };
-    template<>
-    struct RowOffsets<6> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind6[i*6+j]; }
-      inline int apply(unsigned int i) const { return ind6[i]; }
-    };
-
-    int ind7[49] = { 0 , 1 , 3 , 6 , 10 , 15 , 21 , 1 , 2 , 4 , 7 , 11 , 16 , 22 , 3 , 4 , 5 , 8 , 12 , 17 , 23 , 6 , 7 , 8 , 9 , 13 , 18 , 24 , 10 , 11 , 12 , 13 , 14 , 19 , 25 , 15 , 16 , 17 , 18 , 19 , 20 , 26 , 21 , 22 , 23 , 24 , 25 , 26 , 27 };
-    template<>
-    struct RowOffsets<7> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind7[i*7+j]; }
-      inline int apply(unsigned int i) const { return ind7[i]; }
-    };
-
-    int ind8[64] = { 0 , 1 , 3 , 6 , 10 , 15 , 21 , 28 , 1 , 2 , 4 , 7 , 11 , 16 , 22 , 29 , 3 , 4 , 5 , 8 , 12 , 17 , 23 , 30 , 6 , 7 , 8 , 9 , 13 , 18 , 24 , 31 , 10 , 11 , 12 , 13 , 14 , 19 , 25 , 32 , 15 , 16 , 17 , 18 , 19 , 20 , 26 , 33 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 34 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 };
-    template<>
-    struct RowOffsets<8> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind8[i*8+j]; }
-      inline int apply(unsigned int i) const { return ind8[i]; }
-    };
-
-    int ind9[81] = { 0 , 1 , 3 , 6 , 10 , 15 , 21 , 28 , 36 , 1 , 2 , 4 , 7 , 11 , 16 , 22 , 29 , 37 , 3 , 4 , 5 , 8 , 12 , 17 , 23 , 30 , 38 , 6 , 7 , 8 , 9 , 13 , 18 , 24 , 31 , 39 , 10 , 11 , 12 , 13 , 14 , 19 , 25 , 32 , 40 , 15 , 16 , 17 , 18 , 19 , 20 , 26 , 33 , 41 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 34 , 42 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 , 43 , 36 , 37 , 38 , 39 , 40 , 41 , 42 , 43 , 44 };
-    template<>
-    struct RowOffsets<9> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind9[i*9+j]; }
-      inline int apply(unsigned int i) const { return ind9[i]; }
-    };
-
-    int ind10[100] = { 0 , 1 , 3 , 6 , 10 , 15 , 21 , 28 , 36 , 45 , 1 , 2 , 4 , 7 , 11 , 16 , 22 , 29 , 37 , 46 , 3 , 4 , 5 , 8 , 12 , 17 , 23 , 30 , 38 , 47 , 6 , 7 , 8 , 9 , 13 , 18 , 24 , 31 , 39 , 48 , 10 , 11 , 12 , 13 , 14 , 19 , 25 , 32 , 40 , 49 , 15 , 16 , 17 , 18 , 19 , 20 , 26 , 33 , 41 , 50 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 34 , 42 , 51 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 , 43 , 52 , 36 , 37 , 38 , 39 , 40 , 41 , 42 , 43 , 44 , 53 , 45 , 46 , 47 , 48 , 49 , 50 , 51 , 52 , 53 , 54 };
-    template<>
-    struct RowOffsets<10> {
-      inline int operator()(unsigned int i, unsigned int j) const { return ind10[i*10+j]; }
-      inline int apply(unsigned int i) const { return ind10[i]; }
-    };
-
 
     template <class T, unsigned int D>
     class MatRepSym {
 
     public: 
+
+      MatRepSym() :fOff(0) { CreateOffsets(); } 
 
       typedef T  value_type;
 
@@ -242,13 +187,18 @@ namespace ROOT {
       };
 
       
-      inline static const RowOffsets<D> & Offsets() {
+      void CreateOffsets() {
 	static RowOffsets<D> off;
-	return off;
+	fOff = &off;
+      }
+      
+      inline const RowOffsets<D> & Offsets() const {
+	return *fOff;
       }
 
     private:
       T fArray[kSize];
+      RowOffsets<D> * fOff; 
 
     };
 
