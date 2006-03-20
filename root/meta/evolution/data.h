b4a7ae71961fa3f1d6c39be932ef11bfde6a12f1
Philippe Canal <pcanal@fnal.gov>
2006-03-20 17:30:54 +0000

    add more testing
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@879 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/meta/evolution/data.h
+++ root/meta/evolution/data.h
@@ -1,54 +1,66 @@
 #if defined(VERSION_ONE)
 class Tdata {
-   int myvar;
+   int myvar_one;
 public:
-   Tdata() : myvar(0) {}
+   Tdata() : myvar_one(0) {}
    ClassDef(Tdata,1);   
 };
 class data {
-   int myvar;
+   int myvar_one;
 public:
-   data() : myvar(0) {}
+   data() : myvar_one(0) {}
 };
 #elif defined(VERSION_TWO)
 class Tdata {
-   double mydouble;
+   double mydouble_two;
 public:
-   Tdata() : mydouble(0) {}
+   Tdata() : mydouble_two(0) {}
    ClassDef(Tdata,1);   
 };
 class data {
-   double mydouble;
+   double mydouble_two;
 public:
-   data() : mydouble(0) {}
+   data() : mydouble_two(0) {}
 };
-#elif defined(VERSION_THREE)
+#elif defined(VERSION_THREE) || defined (VERSION_FIVE)
 class Tdata {
    double mydouble;
    int more;
+   int three;
 public:
-   Tdata() : mydouble(0),more(0) {}
-   ClassDef(Tdata,1);   
+   Tdata() : mydouble(0),more(0),three(0) {}
+#if defined (VERSION_FIVE)
+   ClassDef(Tdata,2);
+#else
+   ClassDef(Tdata,1);
+#endif
 };
 class data {
    double mydouble;
    int more;
+   int three;
 public:
-   data() : mydouble(0),more(0) {}
+   data() : mydouble(0),more(0),three(0) {}
 };
-#elif defined(VERSION_FOUR)
+#elif defined(VERSION_FOUR) || defined (VERSION_SIX)
 class Tdata {
    double mydouble;
    double more;
+   int four;
 public:
-   Tdata() : mydouble(0),more(0) {}
-   ClassDef(Tdata,1);   
+   Tdata() : mydouble(0),more(0),four(0) {}
+#if defined (VERSION_SIX)
+   ClassDef(Tdata,2);
+#else
+   ClassDef(Tdata,1);
+#endif
 };
 class data {
    double mydouble;
    double more;
+   int four;
 public:
-   data() : mydouble(0),more(0) {}
+   data() : mydouble(0),more(0),four(0) {}
 };
 #else
 #error missing case
---
+++ root/meta/evolution/data6.C
@@ -0,0 +1,2 @@
+#define VERSION_SIX
+#include "data.h"
