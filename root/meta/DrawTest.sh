c1ec737be2ecbbe92742e394cdfeb57bbea1f657
Philippe Canal <pcanal@fnal.gov>
2006-05-12 19:10:28 +0000

    improve makefile
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@916 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/meta/DrawTest.sh
+++ root/meta/DrawTest.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
 #dt_MakeFiles.sh
-#root -b -q 'dt_MakeRef.C("Event.new.split9.root");'
+#root -b -q 'dt_MakeRef.C("Event.new.split9.root");'e
+MAKEFLAGS=`echo "$MAKEFLAGS" | sed -e 's/V[A-Z]*=[^\w]*//' `
+export MAKEFLAGS
+unset V
+unset VERBOSE
+
 if [ "x`which gmake 2>/dev/null | awk '{if ($1~/gmake/) print $1;}'`" != "x" ]
 then
    gmake -f dt_Makefile drawtest -j 1
