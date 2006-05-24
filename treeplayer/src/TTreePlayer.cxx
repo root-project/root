1047448cbca8ce9222631689b6fcae0a15de32f5
Philippe Canal <pcanal@fnal.gov>
2006-05-24 13:43:10 +0000

    Avoid spurrious message like: Error in <TWinNTSystem::ExpandFileName>: input: Sum$(1), output: Sum$(1)
    git-svn-id: http://root.cern.ch/svn/root/trunk@15163 27541ba8-7e3a-0410-8455-c3a389f83636

--- treeplayer/src/TTreePlayer.cxx
+++ treeplayer/src/TTreePlayer.cxx
@@ -1,4 +1,4 @@
-// @(#)root/treeplayer:$Name:  $:$Id: TTreePlayer.cxx,v 1.207 2006/03/20 21:34:22 pcanal Exp $
+// @(#)root/treeplayer:$Name:  $:$Id: TTreePlayer.cxx,v 1.208 2006/03/29 10:36:27 brun Exp $
 // Author: Rene Brun   12/01/96
 
 /*************************************************************************
@@ -909,7 +909,7 @@ Long64_t TTreePlayer::DrawSelect(const char *varexp0, const char *selection, Opt
       possibleFilename = selection;
       if (possibleFilename.Index("Alt$")<0 && possibleFilename.Index("Entries$")<0
           && possibleFilename.Index("Length$")<0  && possibleFilename.Index("Entry$")<0
-          && possibleFilename.Index("Iteration$")<0
+          && possibleFilename.Index("Iteration$")<0 && possibleFilename.Index("Sum$")<0
           && gSystem->IsFileInIncludePath(possibleFilename.Data())) {
 
          Error("DrawSelect",
