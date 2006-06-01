e4708facf09c7175b3611e8f857c5902d5254dd0
Rene Brun <Rene.Brun@cern.ch>
2006-06-01 07:09:14 +0000

    Start development cycle 5.11/07
    git-svn-id: http://root.cern.ch/svn/root/trunk@15275 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "5.11/06"
+#define ROOT_RELEASE "5.11/07"
 #define ROOT_RELEASE_DATE "Jun  1 2006"
-#define ROOT_RELEASE_TIME "06:47:30"
-#define ROOT_VERSION_CODE 330502
+#define ROOT_RELEASE_TIME "08:59:34"
+#define ROOT_VERSION_CODE 330503
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
