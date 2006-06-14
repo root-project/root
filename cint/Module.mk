32478fa655f02d6652c8d9c8fd0f442edce10198
Philippe Canal <pcanal@fnal.gov>
2006-06-14 18:16:41 +0000

    From Axel: adapt to new CINT build system and implement better cintdlls target (parallel build friendly)
    git-svn-id: http://root.cern.ch/svn/root/trunk@15433 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/Module.mk
+++ cint/Module.mk
@@ -14,6 +14,8 @@ CINTDIRM     := $(CINTDIR)/main
 CINTDIRT     := $(CINTDIR)/tool
 CINTDIRL     := $(CINTDIR)/lib
 CINTDIRDLLS  := $(CINTDIR)/include
+CINTDIRSTL   := $(CINTDIR)/stl
+CINTDIRDLLSTL:= $(CINTDIRL)/dll_stl
 
 ##### libCint #####
 CINTH        := $(wildcard $(MODDIRI)/*.h)
@@ -24,6 +26,8 @@ CINTS1       += $(CINTDIRM)/G__setup.c
 
 CINTALLO     := $(CINTS1:.c=.o) $(CINTS2:.cxx=.o)
 CINTALLDEP   := $(CINTALLO:.o=.d)
+
+CINTCONF     := include/configcint.h
 
 CINTS1       := $(filter-out $(MODDIRS)/dlfcn.%,$(CINTS1))
 
@@ -203,8 +207,8 @@ CINTTMP      := $(CINTDIRM)/cint_tmp$(EXEEXT)
 CINT         := bin/cint$(EXEEXT)
 
 ##### makecint #####
-MAKECINTS    := $(CINTDIRT)/makecint.c
-MAKECINTO    := $(MAKECINTS:.c=.o)
+MAKECINTS    := $(CINTDIRT)/makecint.cxx
+MAKECINTO    := $(MAKECINTS:.cxx=.o)
 MAKECINT     := bin/makecint$(EXEEXT)
 
 ##### iosenum.h #####
@@ -224,7 +228,9 @@ endif
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(CINTH))
 
 # include all dependency files
-INCLUDEFILES += $(CINTDEP) $(CINTEXEDEP)
+INCLUDEFILES += $(CINTDEP) $(CINTEXEDEP) $(CINTCONF)
+
+CINTCXXFLAGS += -DG__HAVE_CONFIG -DG__NOMAKEINFO -DG__CINTBODY
 
 ##### local rules #####
 include/%.h:    $(CINTDIRI)/%.h
@@ -248,7 +254,7 @@ $(MAKECINT):    $(MAKECINTO)
 $(IOSENUM):     $(IOSENUMA)
 		cp $< $@
 
-$(IOSENUMA):    $(CINTTMP) $(MAKEINFO)
+$(IOSENUMA):    $(CINTTMP)
 		@(if [ ! -r $@ ]; then \
 			echo "Making $@..."; \
 			$(CINTTMP) $(CINTTMPINC) $(IOSENUMC) > /dev/null; \
@@ -278,6 +284,8 @@ $(CINTDIRS)/sun5strm.o: CINTCXXFLAGS += -I$(CINTDIRL)/snstream
 $(CINTDIRS)/vcstrm.o:   CINTCXXFLAGS += -I$(CINTDIRL)/vcstream
 $(CINTDIRS)/%strm.o:    CINTCXXFLAGS += -I$(CINTDIRL)/$(notdir $(basename $@))
 
+$(MAKECINTO) $(CINTALLO): $(CINTCONF)
+
 $(CINTDIRS)/v6_stdstrct.o:     CINTCXXFLAGS += -I$(CINTDIRL)/stdstrct
 $(CINTDIRS)/v6_loadfile_tmp.o: CINTCXXFLAGS += -UHAVE_CONFIG -DROOTBUILD
 
@@ -285,10 +293,289 @@ $(CINTDIRS)/v6_loadfile_tmp.cxx: $(CINTDIRS)/v6_loadfile.cxx
 	cp -f $< $@
 
 ##### cintdlls ######
+# no: string valarray, limits iterator pair vectorbool
+CINTDLLNAMES = vector list deque map map2 set multimap multimap2 multiset \
+           stack queue exception complex stdfunc stdcxxfunc
+ifneq ($(PLATFORM),win32)
+# FIX THEM!
+  CINTDLLNAMES += posix ipc
+endif
+# ".dll", not ".$(SOEXT)"!
+CINTDLLS = $(subst $(CINTDIRSTL)/ipc.dll,$(CINTDIRDLLS)/sys/ipc.$(SOEXT),\
+  $(subst $(CINTDIRSTL)/posix.dll,$(CINTDIRDLLS)/posix.$(SOEXT),\
+  $(addprefix $(CINTDIRSTL)/,$(addsuffix .dll,$(CINTDLLNAMES)))))
 
-$(CINTDIRDLLS)/stdfunc.dll: $(CINTTMP) $(ROOTCINTTMPEXE) cint/lib/stdstrct/stdfunc.h
-	@$(MAKECINTDLL) $(PLATFORM) C stdfunc stdstrct stdfunc.h \
-           "$(CINTTMP)" "$(ROOTCINTTMP)" \
-	   "$(MAKELIB)" "$(CXX)" "$(CC)" "$(LD)" "$(OPT)" "$(CINTCXXFLAGS)" \
-	   "$(CINTCFLAGS)" "$(LDFLAGS)" "$(SOFLAGS)" "$(SOEXT)" "$(COMPILER)" \
-	   "$(CXXOUT)"
+# these need dictionaries
+ifneq ($(findstring vector,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libvectorDict.$(SOEXT)
+$(CINTDIRSTL)/vector.dll: metautils/src/stlLoader_vector.o
+endif
+ifneq ($(findstring list,$(CINTDLLS)),)
+CINTDICTDLLS += lib/liblistDict.$(SOEXT)
+$(CINTDIRSTL)/list.dll: metautils/src/stlLoader_list.o
+endif
+ifneq ($(findstring deque,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libdequeDict.$(SOEXT)
+$(CINTDIRSTL)/deque.dll: metautils/src/stlLoader_deque.o
+endif
+ifneq ($(findstring map,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libmapDict.$(SOEXT)
+CINTDICTDLLS += lib/libmap2Dict.$(SOEXT)
+$(CINTDIRSTL)/map.dll: metautils/src/stlLoader_map.o
+$(CINTDIRSTL)/map.dll: metautils/src/stlLoader_map2.o
+endif
+ifneq ($(findstring set,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libsetDict.$(SOEXT)
+$(CINTDIRSTL)/set.dll: metautils/src/stlLoader_set.o
+endif
+ifneq ($(findstring multimap,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libmultimapDict.$(SOEXT)
+CINTDICTDLLS += lib/libmultimap2Dict.$(SOEXT)
+$(CINTDIRSTL)/multimap.dll: metautils/src/stlLoader_multimap.o
+$(CINTDIRSTL)/multimap.dll: metautils/src/stlLoader_multimap2.o
+endif
+ifneq ($(findstring multiset,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libmultisetDict.$(SOEXT)
+$(CINTDIRSTL)/multiset.dll: metautils/src/stlLoader_multiset.o
+endif
+ifneq ($(findstring valarray,$(CINTDLLS)),)
+CINTDICTDLLS += lib/libvalarrayDict.$(SOEXT)
+$(CINTDIRSTL)/valarray.dll: metautils/src/stlLoader_valarray.o
+endif
+
+
+ALLCINTDLLS = $(CINTDLLS) $(CINTDICTDLLS)
+
+cintdlls_new: $(ALLCINTDLLS)
+
+$(CINTDIRDLLSTL)/G__cpp_vector.cxx:	$(CINTDIRL)/dll_stl/vec.h
+$(CINTDIRDLLSTL)/G__cpp_list.cxx:	$(CINTDIRL)/dll_stl/lst.h
+$(CINTDIRDLLSTL)/G__cpp_deque.cxx:	$(CINTDIRL)/dll_stl/dqu.h
+$(CINTDIRDLLSTL)/G__cpp_map.cxx:	$(CINTDIRL)/dll_stl/mp.h
+$(CINTDIRDLLSTL)/G__cpp_map2.cxx:	$(CINTDIRL)/dll_stl/mp.h
+$(CINTDIRDLLSTL)/G__cpp_set.cxx:	$(CINTDIRL)/dll_stl/st.h
+$(CINTDIRDLLSTL)/G__cpp_multimap.cxx:	$(CINTDIRL)/dll_stl/multmp.h
+$(CINTDIRDLLSTL)/G__cpp_multimap2.cxx:	$(CINTDIRL)/dll_stl/multmp.h
+$(CINTDIRDLLSTL)/G__cpp_multiset.cxx:	$(CINTDIRL)/dll_stl/multst.h
+$(CINTDIRDLLSTL)/G__cpp_stack.cxx:	$(CINTDIRL)/dll_stl/stk.h
+$(CINTDIRDLLSTL)/G__cpp_queue.cxx:	$(CINTDIRL)/dll_stl/que.h
+$(CINTDIRDLLSTL)/G__cpp_exception.cxx:	$(CINTDIRL)/dll_stl/cinteh.h
+$(CINTDIRDLLSTL)/G__cpp_complex.cxx:	$(CINTDIRL)/dll_stl/cmplx.h
+$(CINTDIRDLLSTL)/G__cpp_limits.cxx:	$(CINTDIRL)/dll_stl/clim.h
+$(CINTDIRDLLSTL)/G__cpp_iterator.cxx:	$(CINTDIRL)/dll_stl/iter.h
+$(CINTDIRDLLSTL)/G__cpp_pair.cxx:	$(CINTDIRL)/dll_stl/pr.h
+$(CINTDIRDLLSTL)/G__cpp_string.cxx:	$(CINTDIRL)/dll_stl/str.h
+$(CINTDIRDLLSTL)/G__cpp_valarray.cxx:	$(CINTDIRL)/dll_stl/vary.h
+$(CINTDIRDLLSTL)/G__cpp_vectorbool.cxx: $(CINTDIRL)/dll_stl/vecbool.h
+$(CINTDIRDLLSTL)/G__cpp_stdcxxfunc.cxx: $(CINTDIRL)/stdstrct/stdcxxfunc.h
+#$(CINTDIRDLLSTL)/G__c_stdfunc.cxx:	$(CINTDIRL)/stdstrct/stdfunc.h
+$(CINTDIRDLLSTL)/G__c_posix.c:		$(CINTDIRL)/posix/exten.h
+$(CINTDIRDLLSTL)/G__c_ipc.c:		$(CINTDIRL)/ipc/ipcif.h
+
+
+FAVOR_SYSINC := -I-
+ifeq ($(PLATFORM),sgi)
+   FAVOR_SYSINC=
+endif
+CINTDLLINCDIRS := -I. -I$(CINTDIRDLLSTL) $(FAVOR_SYSINC)
+ifeq ($(GCC_MAJOR),4)
+  CINTDLLINCDIRS := -iquote. -iquote$(CINTDIRDLLSTL)
+endif
+$(CINTDLLS): CINTCXXFLAGS += $(CINTDLLINCDIRS)
+
+##### all cintdlls end on .dll
+ifneq ($(SOEXT),dll)
+CINTDLLSOEXTCMD = mv $(@:.dll=.$(SOEXT)) $@
+ifeq ($(PLATFORM),maxosx)
+ifeq ($(subst $(MACOSX_MINOR),,456789),456789)
+# MACOSX_MINOR < 4
+  CINTDLLSOEXTCMD += ;mv $(@:.dll=.so) $@;rm -f $(@:.dll=.so)
+endif
+endif # macosx
+endif # need to mv to .dll
+##### all cintdlls end on .dll - END
+
+# Filter out the explicit link flag
+ifneq ($(subst build/unix/makelib.sh,,$(MAKELIB)),$(MAKELIB))
+  $(CINTDLLS): MAKELIB := $(subst -x,,$(MAKELIB))
+endif
+
+$(CINTDIRSTL)/%.dll: $(CINTDIRDLLSTL)/G__cpp_%.o
+	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $(@:.dll=.$(SOEXT)) $^
+	$(CINTDLLSOEXTCMD)
+
+.PRECIOUS: $(addprefix metautils/src/stlLoader_,$(addsuffix .o,$(CINTDLLNAMES)))\
+	   $(addprefix $(CINTDIRDLLSTL)/G__cpp_,$(addsuffix .o,$(CINTDLLNAMES)))
+
+metautils/src/stlLoader_%.o: metautils/src/stlLoader.cc
+	$(CXX) $(OPT) $(CINTCXXFLAGS) $(INCDIRS) -DWHAT=\"$*\" $(CXXOUT)$@ -c $<
+
+#$(CINTDIRDLLSTL)/G__cpp_%.o: $(CINTDIRDLLSTL)/G__cpp_%.cxx
+#	$(CXX) $(OPT) $(CINTCXXFLAGS) $(CXXOUT)$@ -c $<
+
+$(CINTDIRDLLSTL)/G__cpp_%.cxx: $(ORDER_) $(CINTTMP)
+	$(CINTTMP) -w1 -z$(notdir $*) -n$@ $(subst $*,,$(patsubst %map2,-DG__MAP2,$*)) \
+            -D__MAKECINT__ -DG__MAKECINT -I$(CINTDIRDLLSTL) -I$(CINTDIRL) \
+            -c-1 -A -Z0 $(filter %.h,$^)
+
+$(CINTDIRL)/G__c_%.c: $(ORDER_)$(CINTTMP)
+	$(CINTTMP) -K -w1 -z$(notdir $*) -n$@ -D__MAKECINT__ -DG__MAKECINT \
+        -c-2 -Z0 $(filter %.h,$^) 
+
+$(CINTDIRL)/G__c_%.o: CINTCFLAGS += -I. -DG__SYSTYPES_H
+#$(CINTDIRL)/G__c_%.o: $(CINTDIRL)/G__c_%.c
+#	$(CC) $(OPT) $(CINTCFLAGS) -I. $(CXXOUT)$@ -c $<
+
+##### posix special treatment
+$(CINTDIRL)/posix/exten.o: $(CINTDIRL)/posix/exten.c
+	$(CC) $(OPT) $(CINTCFLAGS) -I. $(CXXOUT)$@ -c $<
+
+$(CINTDIRL)/G__c_posix.c: $(CINTDIRDLLS)/sys/types.h cint/lib/posix/exten.h
+
+$(CINTDIRDLLS)/posix.$(SOEXT): $(CINTDIRL)/G__c_posix.o metautils/src/stlLoader_posix.o \
+	                      $(CINTDIRL)/posix/exten.o
+	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $@ $^
+
+$(CINTDIRDLLS)/sys/types.h: $(CINTDIRL)/posix/mktypes$(EXEEXT)
+	(cd $(dir $<) && \
+	./$(notdir $<))
+	cp -f $(CINTDIRDLLS)/systypes.h $@
+
+$(CINTDIRL)/posix/mktypes$(EXEEXT): $(CINTDIRL)/posix/mktypes.c
+	$(CC) $(OPT) $(CXXOUT)$@ $<
+##### posix special treatment - END
+
+##### ipc special treatment
+$(CINTDIRDLLS)/sys/ipc.$(SOEXT): $(CINTDIRL)/G__c_ipc.o metautils/src/stlLoader_ipc.o
+	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $@ $^
+##### ipc special treatment - END
+
+##### dictionaries
+.PRECIOUS: $(CINTDIRDLLSTL)/rootcint_%.cxx
+$(CINTDIRDLLSTL)/rootcint_%.cxx: $(ORDER_) $(ROOTCINTTMPEXE)
+	$(ROOTCINTTMP) -f $@ -c \
+	$(subst $*,,$(patsubst %map2,-DG__MAP2,$*)) $(subst multi,,${*:2=}) \
+	metautils/src/${*:2=}Linkdef.h
+
+$(patsubst lib/lib%Dict.$(SOEXT),$(CINTDIRDLLSTL)/rootcint_%.o,$(CINTDICTDLLS)): CINTCXXFLAGS += -I.
+
+$(CINTDICTDLLS): lib/lib%Dict.$(SOEXT): $(CINTDIRDLLSTL)/rootcint_%.o
+	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $@ $^
+ifeq ($(PLATFORM),maxosx)
+ifeq ($(subst $(MACOSX_MINOR),,456789),456789)
+	rm -f $@
+else
+	mv -f $@ $(basename $@).so
+endif
+endif
+##### dictionaries - END
+
+##### configcint.h
+ifeq ($(CPPPREP),)
+# cannot use "CPPPREP?=", as someone might set "CPPPREP="
+  CPPPREP = $(CXX) -E -C
+endif
+$(CINTCONF): config/Makefile.$(ARCH)
+	-rm -f $@
+	echo '# Generated by cint/Module.mk'
+	@( case $(ARCH) in \
+	  win32) \
+	    if [ $(VC_MAJOR) -ge 14 ]; then \
+	       echo '#define G__CFG_ARCH "msvc8"' >> $@; \
+	    else \
+	       echo '#define G__CFG_ARCH "msvc7"' >> $@; \
+	    fi && \
+	    echo '#define G__CFG_LDOUT "-out:"' >> $@ && \
+	    echo '#define G__CFG_LIBL "lib@imp@"' >> $@ && \
+	    echo '#define G__CFG_LIBEXT ".lib"' >> $@ && \
+	    echo '#define G__CFG_IMPLIBEXT ".lib"' >> $@ && \
+	    echo '#define G__CFG_MANGLEPATHS "cygpath -m "' >> $@ && \
+	    echo '#define G__CFG_PLATFORMO "v6_winnt"' >> $@ && \
+	    echo '#define G__CFG_AR "lib /OUT:"' >> $@ ;;\
+	  *) case $(ARCH) in \
+	     macosx) \
+	       echo '#define G__CFG_ARCH "macgcc"' >> $@ && \
+	       echo '#define G__CFG_IMPLIBEXT ".dylib"' >> $@ ;;\
+	     win32gcc) \
+	       echo '#define G__CFG_ARCH "cygwin"' >> $@ && \
+	       echo '#define G__CFG_IMPLIBEXT ".a"' >> $@ ;; \
+	     *)	\
+	       echo '#define G__CFG_ARCH "$(ARCH)"' >> $@ && \
+	       echo '#define G__CFG_IMPLIBEXT ".a"' >> $@ ;; \
+	    esac && \
+	    echo '#define G__CFG_LDOUT "-o "' >> $@ && \
+	    echo '#define G__CFG_LIBL "-l@imp@"' >> $@ && \
+	    echo '#define G__CFG_LIBEXT ".a"' >> $@ && \
+	    echo '#define G__CFG_MANGLEPATHS "echo "' >> $@ && \
+	    echo '#define G__CFG_PLATFORMO ""' >> $@ && \
+	    echo '#define G__CFG_AR "ar qcs"' >> $@ ;; \
+	esac && \
+	echo '#define G__CFG_CC "$(CC)"' >> $@ && \
+	echo '#define G__CFG_CFLAGS "$(CINTCFLAGS)"' >> $@ && \
+	echo '#define G__CFG_CMACROS "$(filter -D%,$(CINTCFLAGS))"' >> $@ && \
+	echo '#define G__CFG_COMP "-c "' >> $@ && \
+	echo '#define G__CFG_CPP "$(CPPPREP)"' >> $@ && \
+	echo '#define G__CFG_COUT "$(CXXOUT)"' >> $@ && \
+	echo '#define G__CFG_COUTEXE "$(CXXOUT)"' >> $@ && \
+	echo '#define G__CFG_INCP "-I"' >> $@ && \
+	echo '#define G__CFG_CXX "$(CXX)"' >> $@ && \
+	echo '#define G__CFG_CXXFLAGS "$(CINTCXXFLAGS)"' >> $@ && \
+	echo '#define G__CFG_CXXMACROS "$(filter -D%,$(CINTCXXFLAGS))"' >> $@ && \
+	echo '#define G__CFG_LD "$(LD)"' >> $@ && \
+	echo '#define G__CFG_LDFLAGS "$(LDFLAGS)"' >> $@ && \
+	echo '#define G__CFG_LIBP "-L"' >> $@ && \
+	echo '#define G__CFG_SOFLAGS "$(SOFLAGS)"' >> $@ && \
+	echo '#define G__CFG_SOOUT G__CFG_LDOUT' >> $@ && \
+	echo '#define G__CFG_OBJEXT ".o"' >> $@ && \
+	echo '#define G__CFG_EXEEXT "$(EXEEXT)"' >> $@ && \
+	echo '#define G__CFG_SOEXT ".$(SOEXT)"' >> $@ && \
+	echo '#define G__CFG_DEBUG "$(DEBUG)"' >> $@ && \
+	echo '#define G__CFG_DEFAULTLIBS "$(SYSLIBS)"' >> $@ && \
+	echo '#define G__CFG_DEFAULTLIBS "$(SYSLIBS)"' >> $@ && \
+	echo '#define G__CFG_CINTEXINCDIR "./"' >> $@ && \
+	echo '#define G__CFG_CINTEXLIBDIR "lib/"' >> $@ && \
+	echo '#define G__CFG_REFLEXINCDIR "./"' >> $@ && \
+	echo '#define G__CFG_REFLEXLIBDIR "lib"' >> $@ && \
+	echo '#define G__CFG_STREAMDIR "$(patsubst $(MODDIRS)/%strm.cxx,%,$(filter $(MODDIRS)/%strm.cxx, $(CINTS2)))"' >> $@ && \
+	echo '#define G__CFG_EXPLLINK "$(EXPLICITLINK)"' >> $@ && \
+	echo '#define G__CFG_BUILDREADLINE ""' >> $@ && \
+	echo '#define G__CFG_READLINELIBDIR ""' >> $@ && \
+	echo '#define G__CFG_READLINEINCDIR ""' >> $@ && \
+	echo '#define G__CFG_NEEDCURSES ""' >> $@ && \
+	echo '#define G__CFG_RM "rm -f"' >> $@ && \
+	echo '#define G__CFG_MV "mv -f"' >> $@ && \
+	echo '#define G__CFG_INPUTMODE "root"' >> $@ && \
+	echo '#define G__CFG_INPUTMODELOCK "off"' >> $@ && \
+	echo '#define G__CFG_MAKEIMPLIB ""' >> $@ && \
+	echo '#define G__CFG_CINTSYSDIR "/home/axel/cint"' >> $@ && \
+	echo '#define G__CFG_HAVE_CONFIG "1"' >> $@ && \
+	echo '#define CINTSYSDIR "C:/cygwin/home/axel/cint"' >> $@ && \
+	echo '#define G__CFG_MAKEIMPLIB ""' >> $@ && \
+	echo '#define G__CFG_HAVE_CONFIG "1"' >> $@ \
+	)
+##### configcint.h - END
+
+##### clean
+.PHONY: distclean-cintdlls clean-cintdlls
+clean:: clean-cintdlls
+distclean:: distclean-cintdlls
+
+# remove only .o, .dll, .$(SOEXT)
+clean-cintdlls:
+	for cintdll in $(CINTDLLNAMES); do \
+	  rm -f $(CINTDIRDLLSTL)/rootcint_$${cintdll}.o \
+	  $(CINTDIRDLLSTL)/G__cpp_$${cintdll}.o \
+	  metautils/src/stlLoader_$${cintdll}.o; done
+	rm -f $(ALLCINTDLLS) \
+	  $(CINTDIRL)/posix/exten.o $(CINTDIRSTL)/posix.dll \
+	  $(CINTDIRL)/posix/G__c_posix.o
+
+# remove generated code, too.
+distclean-cintdlls: clean-cintdlls
+	for cintdll in $(CINTDLLNAMES); do \
+	  rm -f $(CINTDIRDLLSTL)/rootcint_$${cintdll}.* \
+	  $(CINTDIRDLLSTL)/G__cpp_$${cintdll}.* \
+	  metautils/src/stlLoader_$${cintdll}.*; done
+	rm -f $(ALLCINTDLLS) \
+	  $(CINTDIRL)/posix/G__c_posix.*
+##### clean - END
