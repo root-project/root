9695cc66e167468345585592e21cd7c5cc96ddce
Philippe Canal <pcanal@fnal.gov>
2006-06-19 16:14:34 +0000

    add test for e*e-pp
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@932 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/runexpotest.C
+++ root/treeformula/parse/runexpotest.C
@@ -2,9 +2,15 @@
 //("sshl1e + sshl2e")
 TTree *t = new TTree("T","T");
 float one,two;
+one = 1.5;
+two = 2.0;
 t->Branch("var1e",&one,"var1e/F");
 t->Branch("var2e",&two,"var2e/F");
 t->Branch("e3",&two,"e3/F");
+t->Branch("e",&two,"e/F");
+t->Branch("pp",&one,"pp/F");
+t->Fill();
+t->Fill();
 
 if (TClass::GetDict("TTreeFormula")==0) gSystem->Load("libTreePlayer");
 
@@ -32,4 +38,19 @@ f6->Print();
 TTreeFormula * f7= new TTreeFormula("testing","e3",T);
 f7->Print();
 
+TTreeFormula *t1 = new TTreeFormula("t1","e*e-pp*pp",T);
+t1->Print();
+
+TTreeFormula *t2 = new TTreeFormula("t2","(e*e)-pp*pp",T);
+t2->Print();
+
+myhist = new TH1F("myhisto","test",100,-10,10);
+T->Draw("abs(((e*e)-pp*pp)-(e*e-pp*pp))>0.0001 >> myhisto","","goff");
+if (myhist->GetMean()!=0 || myhist->GetRMS()!=0)
+    printf("Error: TTreeFormula does not think that (e*e)-pp*pp)==(e*e-pp*pp) [%f,%f]\n",
+           myhist->GetMean(), myhist->GetRMS());
+    return 0;
+}
+
+return 1;
 }
