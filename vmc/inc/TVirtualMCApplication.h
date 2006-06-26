1de0594fc81614dccbb00335087bbbde4246fd3c
Rene Brun <Rene.Brun@cern.ch>
2006-06-26 15:35:38 +0000

    From Ivana: - Added member function description where missing
    git-svn-id: http://root.cern.ch/svn/root/trunk@15557 27541ba8-7e3a-0410-8455-c3a389f83636

--- vmc/inc/TVirtualMCApplication.h
+++ vmc/inc/TVirtualMCApplication.h
@@ -1,4 +1,4 @@
-// @(#)root/vmc:$Name:  $:$Id: TVirtualMCApplication.h,v 1.3 2005/05/17 12:44:52 brun Exp $
+// @(#)root/vmc:$Name:  $:$Id: TVirtualMCApplication.h,v 1.4 2006/05/13 20:57:20 brun Exp $
 // Author: Ivana Hrivnacova, 23/03/2002
 
 #ifndef ROOT_TVirtualMCApplication
@@ -14,34 +14,71 @@
 class TVirtualMCApplication : public TNamed {
 
 public:
+   // Standard constructor
    TVirtualMCApplication(const char *name, const char *title);
+
+   // Default constructor
    TVirtualMCApplication();
+
+   // Destructor
    virtual ~TVirtualMCApplication();
   
-   // static access method
+   // Static access method
    static TVirtualMCApplication* Instance(); 
 
+   //
    // methods
+   //
+   
+   // Construct user geometry   
    virtual void ConstructGeometry() = 0;
+   
+   // Define parameters for optical processes (optional)
    virtual void ConstructOpGeometry() {}
+   
+   // Initialize geometry
+   // (Usually used to define sensitive volumes IDs)
    virtual void InitGeometry() = 0;
+   
+   // Add user defined particles (optional)
    virtual void AddParticles() {}
+   
+   // Generate primary particles
    virtual void GeneratePrimaries() = 0;
+   
+   // Define actions at the beginning of the event
    virtual void BeginEvent() = 0;
+   
+   // Define actions at the beginning of the primary track
    virtual void BeginPrimary() = 0;
+   
+   // Define actions at the beginning of each track
    virtual void PreTrack() = 0;
+   
+   // Define action at each step 
    virtual void Stepping() = 0;
+   
+   // Define actions at the end of each track
    virtual void PostTrack() = 0;
+
+   // Define actions at the end of the primary track
    virtual void FinishPrimary() = 0;
+
+   // Define actions at the end of the event
    virtual void FinishEvent() = 0;
-    
+   
+   // Define maximum radius for tracking (optional)
    virtual Double_t TrackingRmax() const { return DBL_MAX; }
+
+   // Define maximum z for tracking (optional)
    virtual Double_t TrackingZmax() const { return DBL_MAX; } 
+
+   // Calculate user field \a b at point \a x
    virtual void     Field(const Double_t* x, Double_t* b) const = 0;
 
 private:
    // static data members
-   static TVirtualMCApplication* fgInstance; //singleton instance  
+   static TVirtualMCApplication* fgInstance; // singleton instance  
 
    ClassDef(TVirtualMCApplication,1)  //Interface to MonteCarlo application
 };
--- vmc/inc/TVirtualMCStack.h
+++ vmc/inc/TVirtualMCStack.h
@@ -1,4 +1,4 @@
-// @(#)root/vmc:$Name:  $:$Id: TVirtualMCStack.h,v 1.3 2005/09/04 09:25:01 brun Exp $
+// @(#)root/vmc:$Name:  $:$Id: TVirtualMCStack.h,v 1.4 2006/05/13 20:57:20 brun Exp $
 // Authors: Ivana Hrivnacova 13/04/2002
 
 #ifndef ROOT_TVirtualMCStack
@@ -17,8 +17,10 @@ class TParticle;
 class TVirtualMCStack : public TObject {
   
 public:
-   // creators, destructors
+   // Constructor
    TVirtualMCStack();
+   
+   // Destructor
    virtual ~TVirtualMCStack();
 
    // 
@@ -26,20 +28,18 @@ public:
    //
     
    // Create a new particle and push into stack;
-   // Arguments:
    // toBeDone   - 1 if particles should go to tracking, 0 otherwise
    // parent     - number of the parent track, -1 if track is primary
    // pdg        - PDG encoding
    // px, py, pz - particle momentum [GeV/c]
    // e          - total energy [GeV]
-   // vx, vy, vx - position [cm]
+   // vx, vy, vz - position [cm]
    // tof        - time of flight [s]    
    // polx, poly, polz - polarization
    // mech       - creator process VMC code 
    // ntr        - track number (is filled by the stack
    // weight     - particle weight 
    // is         - generation status code 
-   //
    virtual void  PushTrack(Int_t toBeDone, Int_t parent, Int_t pdg,
   	              Double_t px, Double_t py, Double_t pz, Double_t e,
                       Double_t vx, Double_t vy, Double_t vz, Double_t tof,
@@ -48,19 +48,20 @@ public:
                       Int_t is) = 0;
 
    // The stack has to provide two pop mechanisms:
-   // PopNextTrack() - pops all particles with toBeDone = 1,
-   //                  both primaries and seconadies
-   // PopPrimaryForTracking() - pops only primary particles with toBeDone = 1,
-   //                  stacking of secondaries is done by MC
-   //
+   // The first pop mechanism required.
+   // Pop all particles with toBeDone = 1, both primaries and seconadies
    virtual TParticle* PopNextTrack(Int_t& itrack) = 0;
+
+   // The second pop mechanism required.
+   // Pop only primary particles with toBeDone = 1, stacking of secondaries 
+   // is done by MC
    virtual TParticle* PopPrimaryForTracking(Int_t i) = 0;    
 
    //
    // Set methods
    //
     
-   // Set current track number
+   // Set the current track number
    virtual void       SetCurrentTrack(Int_t trackNumber) = 0;                           
 
    //
