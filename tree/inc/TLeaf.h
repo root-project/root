04e8cafb3e5b5013ce17efeecdd391ad5282a877
Philippe Canal <pcanal@fnal.gov>
2006-07-13 05:30:48 +0000

    From Paul Russo:
         o (Constructor) Remove 64 char length limit on branch name.
    
         o (ResetAddress) Improve documentation.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@15789 27541ba8-7e3a-0410-8455-c3a389f83636

--- tree/inc/TLeaf.h
+++ tree/inc/TLeaf.h
@@ -1,4 +1,4 @@
-// @(#)root/tree:$Name:  $:$Id: TLeaf.h,v 1.11 2005/11/11 22:16:04 pcanal Exp $
+// @(#)root/tree:$Name:  $:$Id: TLeaf.h,v 1.12 2006/05/23 04:47:42 brun Exp $
 // Author: Rene Brun   12/01/96
 
 /*************************************************************************
@@ -44,28 +44,28 @@ protected:
    Int_t       fOffset;          //  Offset in ClonesArray object (if one)
    Bool_t      fIsRange;         //  (=kTRUE if leaf has a range, kFALSE otherwise)
    Bool_t      fIsUnsigned;      //  (=kTRUE if unsigned, kFALSE otherwise)
-   TLeaf      *fLeafCount;       //  Pointer to Leaf count if variable length
-   TBranch    *fBranch;          //! Pointer to supporting branch
+   TLeaf      *fLeafCount;       //  Pointer to Leaf count if variable length (we do not own the counter)
+   TBranch    *fBranch;          //! Pointer to supporting branch (we do not own the branch)
 
    TLeaf(const TLeaf&);
    TLeaf& operator=(const TLeaf&);
 
 public:
-   enum { 
-      kIndirectAddress = BIT(11), // Addresses passed via pointer
-      kNewValue = BIT(12) 
+   enum {
+      kIndirectAddress = BIT(11), // Data member is a pointer to an array of basic types.
+      kNewValue = BIT(12)         // Set if we own the value buffer and so must delete it ourselves.
    };
 
    TLeaf();
-   TLeaf(const char *name, const char *type);
+   TLeaf(const char* name, const char* type);
    virtual ~TLeaf();
 
-   virtual void     Browse(TBrowser *b);
-   virtual void     Export(TClonesArray *list, Int_t n);
-   virtual void     FillBasket(TBuffer &b);
+   virtual void     Browse(TBrowser* b);
+   virtual void     Export(TClonesArray*, Int_t) {}
+   virtual void     FillBasket(TBuffer& b);
    TBranch         *GetBranch() const { return fBranch; }
    virtual TLeaf   *GetLeafCount() const { return fLeafCount; }
-   virtual TLeaf   *GetLeafCounter(Int_t &countval) const;
+   virtual TLeaf   *GetLeafCounter(Int_t& countval) const;
    virtual Int_t    GetLen() const;
    virtual Int_t    GetLenStatic() const { return fLen; }
    virtual Int_t    GetLenType() const { return fLenType; }
@@ -75,30 +75,29 @@ public:
    virtual Int_t    GetOffset() const { return fOffset; }
    virtual void    *GetValuePointer() const { return 0; }
    virtual const char *GetTypeName() const { return ""; }
-   virtual Double_t GetValue(Int_t i=0) const;
-   virtual void     Import(TClonesArray *list, Int_t n);
-   virtual Bool_t   IsOnTerminalBranch() const    { return kTRUE; }
-   virtual Bool_t   IsRange() const    { return fIsRange; }
+   virtual Double_t GetValue(Int_t i = 0) const;
+   virtual void     Import(TClonesArray*, Int_t) {}
+   virtual Bool_t   IsOnTerminalBranch() const { return kTRUE; }
+   virtual Bool_t   IsRange() const { return fIsRange; }
    virtual Bool_t   IsUnsigned() const { return fIsUnsigned; }
-   virtual void     PrintValue(Int_t =0) const {;}
-   virtual void     ReadBasket(TBuffer &) {;}
-   virtual void     ReadBasketExport(TBuffer &, TClonesArray *, Int_t) {;}
-   virtual void     ReadValue(ifstream & /*s*/) {;}
-           Int_t    ResetAddress(void *add, Bool_t destructor = kFALSE);
-   virtual void     SetAddress(void *add=0);
-   virtual void     SetBranch(TBranch *branch) { fBranch = branch; }
-   virtual void     SetLeafCount(TLeaf *leaf);
-   virtual void     SetLen(Int_t len=1) { fLen=len; }
-   virtual void     SetOffset(Int_t offset=0) { fOffset = offset; }
-   virtual void     SetRange(Bool_t range=kTRUE) { fIsRange = range; }
+   virtual void     PrintValue(Int_t i = 0) const;
+   virtual void     ReadBasket(TBuffer&) {}
+   virtual void     ReadBasketExport(TBuffer&, TClonesArray*, Int_t) {}
+   virtual void     ReadValue(ifstream& /*s*/) {}
+           Int_t    ResetAddress(void* add, Bool_t destructor = kFALSE);
+   virtual void     SetAddress(void* add = 0);
+   virtual void     SetBranch(TBranch* branch) { fBranch = branch; }
+   virtual void     SetLeafCount(TLeaf* leaf);
+   virtual void     SetLen(Int_t len = 1) { fLen = len; }
+   virtual void     SetOffset(Int_t offset = 0) { fOffset = offset; }
+   virtual void     SetRange(Bool_t range = kTRUE) { fIsRange = range; }
    virtual void     SetUnsigned() { fIsUnsigned = kTRUE; }
 
    ClassDef(TLeaf,2);  //Leaf: description of a Branch data type
 };
 
-inline void     TLeaf::Export(TClonesArray *, Int_t) { }
-inline Double_t TLeaf::GetValue(Int_t) const { return 0; }
-inline void     TLeaf::Import(TClonesArray *, Int_t) { }
-inline void     TLeaf::SetAddress(void *) { }
+inline Double_t TLeaf::GetValue(Int_t /*i = 0*/) const { return 0.0; }
+inline void     TLeaf::PrintValue(Int_t /* i = 0*/) const {}
+inline void     TLeaf::SetAddress(void* /* add = 0 */) {}
 
 #endif
