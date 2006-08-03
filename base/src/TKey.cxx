01bb6965557fcc63d5d2e535b89f57e025922731
Philippe Canal <pcanal@fnal.gov>
2006-08-03 21:17:44 +0000

    Set the wiggle room for the buffer being compressed to the same value as in TBasket.cxx.  This is some time needed if the object's 'compression' lead to a bigger size
    git-svn-id: http://root.cern.ch/svn/root/trunk@15915 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/src/TKey.cxx
+++ base/src/TKey.cxx
@@ -1,4 +1,4 @@
-// @(#)root/base:$Name:  $:$Id: TKey.cxx,v 1.58 2006/05/26 09:01:58 brun Exp $
+// @(#)root/base:$Name:  $:$Id: TKey.cxx,v 1.59 2006/07/09 05:27:53 brun Exp $
 // Author: Rene Brun   28/12/94
 
 /*************************************************************************
@@ -212,7 +212,7 @@ TKey::TKey(const TObject *obj, const char *name, Int_t bufsize, TDirectory* moth
    if (cxlevel && fObjlen > 256) {
       if (cxlevel == 2) cxlevel--;
       Int_t nbuffers = fObjlen/kMAXBUF;
-      Int_t buflen = TMath::Max(512,fKeylen + fObjlen + 9*nbuffers + 8); //add 8 bytes in case object is placed in a deleted gap
+      Int_t buflen = TMath::Max(512,fKeylen + fObjlen + 9*nbuffers + 28); //add 28 bytes in case object is placed in a deleted gap
       fBuffer = new char[buflen];
       char *objbuf = fBufferRef->Buffer() + fKeylen;
       char *bufcur = &fBuffer[fKeylen];
@@ -297,7 +297,7 @@ TKey::TKey(const void *obj, const TClass *cl, const char *name, Int_t bufsize, T
    if (cxlevel && fObjlen > 256) {
       if (cxlevel == 2) cxlevel--;
       Int_t nbuffers = fObjlen/kMAXBUF;
-      Int_t buflen = TMath::Max(512,fKeylen + fObjlen + 9*nbuffers + 8); //add 8 bytes in case object is placed in a deleted gap
+      Int_t buflen = TMath::Max(512,fKeylen + fObjlen + 9*nbuffers + 28); //add 28 bytes in case object is placed in a deleted gap
       fBuffer = new char[buflen];
       char *objbuf = fBufferRef->Buffer() + fKeylen;
       char *bufcur = &fBuffer[fKeylen];
