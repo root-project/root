ee2a9578f1cf144dd84bae8f6bff424136ffcabf
Fons Rademakers <Fons.Rademakers@cern.ch>
2006-08-03 15:58:26 +0000

    Make version 5.12/00a.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-12-00-patches@15910 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,9 +14,9 @@
  *
 */
 
-#define ROOT_RELEASE "5.12/00"
-#define ROOT_RELEASE_DATE "Jul 10 2006"
-#define ROOT_RELEASE_TIME "18:49:28"
+#define ROOT_RELEASE "5.12/00a"
+#define ROOT_RELEASE_DATE "Aug  3 2006"
+#define ROOT_RELEASE_TIME "17:32:49"
 #define ROOT_VERSION_CODE 330752
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
