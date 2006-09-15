b1ee2261f8bddd237d6da1add0161e6eda254a4e
Rene Brun <Rene.Brun@cern.ch>
2006-09-15 10:05:30 +0000

    Update with new results
    git-svn-id: http://root.cern.ch/svn/root/trunk@16275 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressHepix.cxx
+++ test/stressHepix.cxx
@@ -1,4 +1,4 @@
-// @(#)root/test:$Name:  $:$Id: stressHepix.cxx,v 1.4 2006/09/13 06:53:14 brun Exp $
+// @(#)root/test:$Name:  $:$Id: stressHepix.cxx,v 1.5 2006/09/14 07:21:37 brun Exp $
 // Author: Rene Brun   12/09/2006
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -32,18 +32,19 @@
 ///
 ///
 ///Starting stressHepix benchmark (details will be in stressHepix.log)
-///Takes 558 seconds on a  500 rootmarks machine (IBM Thinkpad centrino 1.4GHz VC++7.1)
-///Takes 368 seconds on a  800 rootmarks reference machine (P IV 2.8 GHz, SLC3 gcc3.2.3)
-///Takes 294 seconds on a  924 rootmarks machine (MacBookPro 1.8GHz gcc4.0.1)
-///Takes 253 seconds on a 1056 rootmarks machine (MacBookPro 1.8GHz icc9.1)
-///Takes 218 seconds on a 1550 rootmarks machine (AMD64/280, FC5 gcc4.1)
-///Takes 149 seconds on a 1828 rootmarks machine (MacBookPro 3.0GHz icc9.1)
+///Takes 442 CP seconds on a  500 rootmarks machine (IBM Thinkpad centrino 1.4GHz VC++7.1)
+///Takes 278 CP seconds on a  800 rootmarks reference machine (P IV 2.8 GHz, SLC3 gcc3.2.3)
+///Takes 239 CP seconds on a  924 rootmarks machine (MacBook 2.0GHz gcc4.0.1)
+///Takes 209 CP seconds on a 1056 rootmarks machine (MacBook 2.0GHz icc9.1)
+///Takes 147 CP seconds on a 1512 rootmarks machine (MacPro 3.0GHz gcc4.0.1)
+///Takes 142 CP seconds on a 1550 rootmarks machine (AMD64/280, FC5 gcc4.1)
+///Takes 121 CP seconds on a 1828 rootmarks machine (MacPro 3.0GHz icc9.1)
 ///
-///Running : stressFit Minuit  2000, (takes 11 seconds on the ref machine)
-///Running : stressLinear, (takes 26 seconds on the ref machine)
-///Running : stressGeometry, (takes 77 seconds on the ref machine)
-///Running : stressSpectrum 1000, (takes 116 seconds on the ref machine)
-///Running : stress -b 3000, (takes 138 seconds on the ref machine)
+///Running : stressFit Minuit  2000, (takes 11 RT seconds on the ref machine)
+///Running : stressLinear, (takes 26 RT seconds on the ref machine)
+///Running : stressGeometry, (takes 77 RT seconds on the ref machine)
+///Running : stressSpectrum 1000, (takes 116 RT seconds on the ref machine)
+///Running : stress -b 3000, (takes 138 RT seconds on the ref machine)
 ///
 ///
 ///****************************************************************************
@@ -69,7 +70,7 @@
 #include <TStopwatch.h>
 
 void runTest(const char *atest, int estimate) {
-   printf("Running : %s, (takes %d seconds on the ref machine)\n",atest,estimate);
+   printf("Running : %s, (takes %d RT seconds on the ref machine)\n",atest,estimate);
    gSystem->Exec(Form("%s >>stressHepix.log",atest));
 }
 
@@ -77,12 +78,13 @@ int main(int argc, char **argv)
 {
    TApplication theApp("App", &argc, argv);
    printf("\n\nStarting stressHepix benchmark (details will be in stressHepix.log)\n");
-   printf("Takes %d seconds on a  500 rootmarks machine (IBM Thinkpad centrino 1.4GHz VC++7.1)\n",56+41+70+209+182);
-   printf("Takes %d seconds on a  800 rootmarks reference machine (P IV 2.8 GHz, SLC3 gcc3.2.3)\n",11+26+77+116+138);
-   printf("Takes %d seconds on a  924 rootmarks machine (MacBookPro 1.8GHz gcc4.0.1)\n",10+31+47+80+126);
-   printf("Takes %d seconds on a 1056 rootmarks machine (MacBookPro 1.8GHz icc9.1)\n",8+26+38+75+106);
-   printf("Takes %d seconds on a 1550 rootmarks machine (AMD64/280, FC5 gcc4.1)\n",6+18+50+43+101);
-   printf("Takes %d seconds on a 1828 rootmarks machine (MacBookPro 3.0GHz icc9.1)\n\n",149);
+   printf("Takes 442 CP seconds on a  500 rootmarks machine (IBM Thinkpad centrino 1.4GHz VC++7.1)\n");
+   printf("Takes 278 CP seconds on a  800 rootmarks reference machine (P IV 2.8 GHz, SLC3 gcc3.2.3)\n");
+   printf("Takes 239 CP seconds on a  924 rootmarks machine (MacBook 2.0GHz gcc4.0.1)\n");
+   printf("Takes 209 CP seconds on a 1056 rootmarks machine (MacBook 2.0GHz icc9.1)\n");
+   printf("Takes 147 CP seconds on a 1512 rootmarks machine (MacPro 3.0GHz gcc4.0.1)\n");
+   printf("Takes 142 CP seconds on a 1550 rootmarks machine (AMD64/280, FC5 gcc4.1)\n");
+   printf("Takes 121 CP seconds on a 1828 rootmarks machine (MacPro 3.0GHz icc9.1)\n\n");
    if (gSystem->AccessPathName("atlas.root")) {
       printf("\nPreparing geometry files from http://root.cern.ch\n\n");
       gSystem->Exec("stressGeometry >stressHepix.log");
