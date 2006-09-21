a823206927ecf7c9847b3e40c304cf5101d1181b
Philippe Canal <pcanal@fnal.gov>
2006-09-21 15:36:19 +0000

    add chain
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@967 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/friend/runchainBranchStatus.C
+++ root/tree/friend/runchainBranchStatus.C
@@ -9,4 +9,13 @@ t2->Branch("b",&one,"one/I");
 t1->AddFriend(t2);
 t1->SetBranchStatus("c",0);
 t1->SetBranchStatus("t2.b",0);
+
+t3 = new TTree("t3","");
+t3->Branch("a",&one,"one/I");
+t3->Branch("b",&one,"one/I");
+c1 = new TChain("t1");
+c1->AddFriend(t2);
+c1->SetBranchStatus("c",0);
+c1->SetBranchStatus("t2.b",0);
+
 }
