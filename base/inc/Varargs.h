37481bca17b54b26044645931c04e021c016b2fb
Fons Rademakers <Fons.Rademakers@cern.ch>
2006-10-05 21:06:35 +0000

    move R__VA_COPY from TString.cxx to Varargs.h where it belongs.
    git-svn-id: http://root.cern.ch/svn/root/trunk@16455 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/Varargs.h
+++ base/inc/Varargs.h
@@ -1,4 +1,4 @@
-/* @(#)root/base:$Name:  $:$Id: Varargs.h,v 1.2 2001/01/18 11:26:50 rdm Exp $ */
+/* @(#)root/base:$Name:  $:$Id: Varargs.h,v 1.3 2005/03/13 15:05:31 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -16,28 +16,45 @@
 #endif
 
 #if defined(sparc) && defined(__CC_ATT301__)
-        extern "C" __builtin_va_arg_incr(...);
-        typedef char *va_list;
+
+   extern "C" __builtin_va_arg_incr(...);
+   typedef char *va_list;
 #   define va_end(ap)
 #   define va_start(ap, parmN) ap= (char*)(&parmN+1)
 #   define va_arg(ap, mode) ((mode*) __builtin_va_arg_incr((mode*)ap))[0]
 #   define va_(arg) __builtin_va_alist
 
 #   include <stdio.h>
-extern "C" {
-        int vfprintf(FILE*, const char *fmt, va_list ap);
-        char *vsprintf(char*, const char *fmt, va_list ap);
-};
+
+   extern "C" {
+      int vfprintf(FILE*, const char *fmt, va_list ap);
+      char *vsprintf(char*, const char *fmt, va_list ap);
+   };
 
 #else
-#   ifndef __CINT__
-#   include <stdarg.h>
-#   endif
+
 #   if defined(sparc) && !defined(__GNUG__) && !defined(__CC_SUN21__) && !defined(__SVR4)
-#       define va_(arg) __builtin_va_alist
+#      define va_(arg) __builtin_va_alist
 #   else
-#       define va_(arg) arg
+#      define va_(arg) arg
 #   endif
+
+#endif
+
+#if !defined(R__VA_COPY)
+#  if defined(__GNUC__)
+#     define R__VA_COPY(to, from) __va_copy((to), (from))
+#  elif defined(__va_copy)
+#     define R__VA_COPY(to, from) __va_copy((to), (from))
+#  elif defined(va_copy)
+#     define R__VA_COPY(to, from) va_copy((to), (from))
+#  elif defined (R__VA_COPY_AS_ARRAY)
+#     define R__VA_COPY(to, from) memmove((to), (from), sizeof(va_list))
+#  elif defined(_WIN32) && _MSC_VER < 1310
+#     define R__VA_COPY(to, from) (*(to) = *(from))
+#  else
+#     define R__VA_COPY(to, from) ((to) = (from))
+#  endif
 #endif
 
 #endif
