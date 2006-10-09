239cd08ef0dc601ad6612a890142c4d8279b8dbf
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2006-10-09 13:59:23 +0000

    add golden section algorithm in new minim1D test
    git-svn-id: http://root.cern.ch/svn/root/trunk@16483 27541ba8-7e3a-0410-8455-c3a389f83636

--- mathmore/test/testMinimization1D.cxx
+++ mathmore/test/testMinimization1D.cxx
@@ -1,7 +1,7 @@
 #include "Math/Polynomial.h"
 #include "Math/Minimizer1D.h"
 #include "Math/WrappedFunction.h"
-#include "TF1.h"
+//#include "TF1.h"
 #include <iostream>
 
 
@@ -19,16 +19,30 @@ void testMinimization1D() {
   f->SetParameters(p);
 
 
+  { 
+     // default (Brent) 
+     ROOT::Math::Minimizer1D min;
+     min.SetFunction(*f,1,-10,10); 
+     int status = min.Minimize(100,0.01,0.01); 
+     std::cout << "test Min1D " << min.Name() << "  Return code " << status << std::endl; 
+     
+     std::cout.precision(20);
+  
+     std::cout << "Found minimum: x = " << min.XMinimum() << "  f(x) = " << min.FValMinimum() << std::endl;
 
-  ROOT::Math::Minimizer1D min;
-  min.SetFunction(*f,1,-10,10); 
-  int status = min.Minimize(100,0.01,0.01); 
-  std::cout << "Return code " << status << std::endl; 
+  }
+  {
+     // Golden Section
+     ROOT::Math::Minimizer1D min(ROOT::Math::Minim1D::GOLDENSECTION);
+     min.SetFunction(*f,1,-10,10); 
+     int status = min.Minimize(100,0.01,0.01); 
+     std::cout << "test Min1D " << min.Name() << "  Return code " << status << std::endl; 
+     
+     std::cout.precision(20);
+  
+     std::cout << "Found minimum: x = " << min.XMinimum() << "  f(x) = " << min.FValMinimum() << std::endl;
 
-  std::cout.precision(20);
-
-  std::cout << "Found minimum: x = " << min.XMinimum() << "  f(x) = " << min.FValMinimum() << std::endl;
-
+  }
 
  
 }
