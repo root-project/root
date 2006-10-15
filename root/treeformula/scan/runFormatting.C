3012bfc39ea6a9cee5aa6f5dffb426ca41b3edc6
Philippe Canal <pcanal@fnal.gov>
2006-10-15 17:24:47 +0000

    test for extended TTree::Scan formatting option
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@976 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/treeformula/scan/runFormatting.C
@@ -0,0 +1,47 @@
+void runFormatting(int mode = 0)
+{
+   if (mode==1 || mode ==2) {
+      makeTree();
+      if (mode==1) return;
+   }
+   TFile* file = TFile::Open("testScanIn.root");
+   if(file == NULL) {
+      makeTree();
+      file = TFile::Open("testScanIn.root");
+   }
+
+   TTree* tree = (TTree*) file->Get("tree");
+   tree->Scan("val:flag:flag:c:cstr", "", "col=::#x:c:");
+}
+
+TTree* makeTree()
+{
+  Info("testScan", "Making testScanIn.root...");
+  TFile* file = TFile::Open("testScanIn.root", "RECREATE");
+  TTree* tree = new TTree("tree", "tree");
+
+  double val = 0.0;
+  int flag = 0x1;
+  char c = 'a';
+  char *c2 = new char[4];
+  c2[0] = 'i';
+  c2[1] = '0';
+  c2[2] = '0';
+  c2[3] = 0;
+  tree->Branch("val", &val, "val/D");
+  tree->Branch("flag", &flag, "flag/I");
+  tree->Branch("c", &c, "c/b");
+  tree->Branch("cstr", c2, "cstr/C");
+
+  for(int i=0; i<10; i++) {
+    tree->Fill();
+    val += 1.1;
+    flag <<= 1;
+    c++;
+    (c2[2])++;
+  }
+
+  tree->Fill();
+  tree->Write();
+  file->Close();
+}
