ad9d82b1f23493a817009a4e6a89eeae5cc955be
Fons Rademakers <Fons.Rademakers@cern.ch>
2006-10-27 09:15:11 +0000

    Set version to v5-13-04b.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-13-04-patches@16638 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,9 +14,9 @@
  *
 */
 
-#define ROOT_RELEASE "5.13/04a"
-#define ROOT_RELEASE_DATE "Oct 18 2006"
-#define ROOT_RELEASE_TIME "10:26:38"
+#define ROOT_RELEASE "5.13/04b"
+#define ROOT_RELEASE_DATE "Oct 27 2006"
+#define ROOT_RELEASE_TIME "11:08:45"
 #define ROOT_VERSION_CODE 331012
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
