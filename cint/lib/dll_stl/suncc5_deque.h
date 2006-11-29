430e273b520ce53ff59d8e6455985bc379d3ec86
Rene Brun <Rene.Brun@cern.ch>
2006-11-29 14:50:58 +0000

    From Axel: Fix error pos for string, deque dll now that CINT actually parses them because __SUNPRO_CC is now defined for makecint.
    git-svn-id: http://root.cern.ch/svn/root/trunk@16979 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/lib/dll_stl/suncc5_deque.h
+++ cint/lib/dll_stl/suncc5_deque.h
@@ -8,12 +8,16 @@
 #ifndef G__SUNCC5_DEQUE_H
 #define G__SUNCC5_DEQUE_H
 
+#ifdef __CINT__
+#define _RWSTDExport
+#endif
+
 #if (__SUNPRO_CC>=1280)
 namespace __rwstd {
 #ifdef _RWSTD_LOCALIZED_ERRORS
   const unsigned int _RWSTDExport rwse_OutOfRange = 0;
 #else
-  const char _RWSTDExportFunc(*) rwse_OutOfRange = 0;
+  const char * rwse_OutOfRange = 0;
 #endif // _RWSTD_LOCALIZED_ERRORS
 }
 #endif
--- cint/lib/dll_stl/suncc5_string.h
+++ cint/lib/dll_stl/suncc5_string.h
@@ -8,6 +8,10 @@
 #ifndef G__SUNCC5_STRING_H
 #define G__SUNCC5_STRING_H
 
+#ifdef __CINT__
+#define _RWSTDExport
+#endif
+
 #if (__SUNPRO_CC>=1280)
 //#define _RWSTD_COMPILE_INSTANTIATE
 namespace __rwstd {
@@ -18,11 +22,11 @@ namespace __rwstd {
   const unsigned int _RWSTDExport __rwse_StringIndexOutOfRange=0;
   const unsigned int _RWSTDExport __rwse_UnexpectedNullPtr=0;
 #else
-  const char _RWSTDExportFunc(*) __rwse_InvalidSizeParam=0;
-  const char _RWSTDExportFunc(*) __rwse_PosBeyondEndOfString=0;
-  const char _RWSTDExportFunc(*) __rwse_ResultLenInvalid=0;
-  const char _RWSTDExportFunc(*) __rwse_StringIndexOutOfRange=0;
-  const char _RWSTDExportFunc(*) __rwse_UnexpectedNullPtr=0;
+  const char * __rwse_InvalidSizeParam=0;
+  const char * __rwse_PosBeyondEndOfString=0;
+  const char * __rwse_ResultLenInvalid=0;
+  const char * __rwse_StringIndexOutOfRange=0;
+  const char * __rwse_UnexpectedNullPtr=0;
 #endif
 }
 #endif
