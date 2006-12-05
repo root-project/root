f99b0c5441e3b1e32a8b6fbc5a1bd157e8852d5d
Unknown Author <root-dev@cern.ch>
2006-12-05 13:39:56 +0000

    This commit was manufactured by cvs2svn to create tag 'v5-16-17'.
    git-svn-id: http://root.cern.ch/svn/root/tags/v5-16-17@17044 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofitcore/inc/RooBanner.h
+++
@@ -1,1 +0,0 @@
-// dummy file to force compilation of .cc
--- smatrix/test/stressOperations.cxx
+++
@@ -1,4 +0,0 @@
-// run testOperations in loop mode from size 2x2 up to 30x30
-#define TEST_ALL_MATRIX_SIZES
-//#define NITER 10
-#include "testOperations.cxx"
