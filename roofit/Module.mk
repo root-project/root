da278b2a1b53b1f139337a5263c0aff806a993df
Rene Brun <Rene.Brun@cern.ch>
2006-12-09 08:30:21 +0000

    From Wouter Verkerke: New version 2.10 of Roofit introduced.
    **** Fri Oct 6 11:51:26 2006 UTC by wverkerke  ****
    
      - RooFitResult
    
         o Change conditional inclusion macro from TMatrixF forward declaration
           as current version of rootcint appears to choke on ROOT_VERSION macro
    
      - RooHtml
    
         o Follow change in THtml signature
    
    
      - RooMCStudy
    
         o Modify class to support concept of 'study modules' that can modify
           behaviour of RooMCStudy at various points in fit/generation cycle
           and that can write additional information to the fitParDataSet()
           summary dataset
    
      - RooAbsMCStudyModule
    
         o Abstract base class for RooMCStudy add-on modules that defines
           functions to be called at initialization, before each run,
           before each generation step, between each generation and fit step,
           after each fit step and at the end of each run. This class provides
           protected accessor functions to various internal configuration details
           of the RooMCStudy object to which it is attached.
    
      - RooDLLSignificancMCSModule
    
         o RooMCStudy add-on module that calculates the delta(-log(L)) between
           the nominal fit performed in the RooMCStudy and a refit with a parameter
           set to a null hypothesis (eg. nsig set to zero). The null-hypotesis
           likelihood, the difference between the nominal and null-hypo likelihood
           and the significance associated with that difference are added to the
           fitParDataSet() of the RooMCStudy. NB: The significance is calculated
           using (possible naive) Gaussian assumptions of the sampling distributions
           (i.e. signif=sqrt(2*Delta(LL)))
    
      - RooRandomizeParamMCSModule
    
         o RooMCStudy add-on module that allows to scan some of the generator
           parameters during the run. A generator parameter can be sampled from
           uniform distribution in a given range, or from a gaussian distribution
           with given mean and sigma. For every generator parameter that is varied
           a variable is added to the fitParDataSet() of RooMCStudy that indicated
           the actual generator value for each toy experiment. Additionally you
           can choose to vary the sum of a set of parameters uniformly or in a
           Gaussian way. In this mode a common scaling factor is applied to each
           specified parameter so that the sum of these parameters will add up
           to the target value sampled from either uniform or Gaussian distribution.
    
      - RooFitCore_Linkdef
    
         o Add nested utility classes of RooRandomizeParamMCSModule
    
    
    *** Thu Dec 7 21:21:33 2006 UTC by wverkerke   ***
    
    
       o RooRealVar
    
          - Do not instantiate shared properties until the're actually needed
    
          - Add method to delete shared properties (used in RooFitResult)
    
          - Add static null property to be written for RRVs without properties
    
    
        o RooRealVarSharedProperties,RooSharedProperties
    
          - Add ctor with uuid string
    
    
        o RooFitResult
    
          - Remove all shared properties of RRVs inside object to facilitate
            easy handling of large number of RFRs (e.g. in trees). Not doing
            so leaves them all connected through the shared properties list
            and will slow down operations
    
    *** Fri Dec 8 15:50:40 2006 UTC by wverkerke  ***
    
       o RooAbsHist
    
         - Fix mistake in documentation of Binning()
    
    
       o RooDataHist
    
         - Fix bug in handling of Z ranges
    
    
       o RooAdaptiveGaussKronrodIntegrator1D,RooGaussKronrodIntegrator1D
    
         - Import fix from Fons
    
    
       o RooHist
    
         - Protect possible occurence of division by zero in makeResidHist
    
    
       o RooHtml
    
         - Import fixes from ROOT team
    
    
       o RooBanner
    
         - Increment version tag to v2.10
    
    
    *** Fri Dec 8 17:52:17 2006 UTC by wverkerke ***
    
      o RooBDecay
    
         - Updated generateEvent() method from Gerhard
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@17129 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/Module.mk
+++ roofit/Module.mk
@@ -1,5 +1,5 @@
 # ROOT Module.mk for roofit module
-# Copyright (c) 2005 Wouter Verkerke & Fons Rademakers
+# Copyright (c) 2005 Wouter Verkerke
 #
 # Author: Wouter Verkerke, 18/4/2005
 
@@ -11,7 +11,7 @@ ROOFITDIR    := $(MODDIR)
 ROOFITDIRS   := $(ROOFITDIR)/src
 ROOFITDIRI   := $(ROOFITDIR)/inc
 
-ROOFITVERS   := roofit_2.09
+ROOFITVERS   := roofit_2.10
 ROOFITSRCS   := $(MODDIR)/$(ROOFITVERS).src.tgz
 ROOFITETAG   := $(MODDIR)/headers.d
 
@@ -31,40 +31,42 @@ ROOFITDH     := $(ROOFITDS:.cxx=.h)
 
 ROOFITH1     := Roo1DTable.h Roo2DKeysPdf.h RooAbsAnaConvPdf.h RooAbsArg.h RooAbsBinning.h RooAbsCategory.h RooAbsCategoryLValue.h \
                 RooAbsCollection.h RooAbsData.h RooAbsFunc.h RooAbsGenContext.h RooAbsGoodnessOfFit.h RooAbsHiddenReal.h \
-                RooAbsIntegrator.h RooAbsLValue.h RooAbsOptGoodnessOfFit.h RooAbsPdf.h RooAbsProxy.h RooAbsReal.h RooAbsRealLValue.h \
-                RooAbsRootFinder.h RooAbsString.h RooAcceptReject.h RooAdaptiveGaussKronrodIntegrator1D.h RooAddGenContext.h \
-                RooAddition.h RooAddModel.h RooAddPdf.h RooAICRegistry.h RooArgList.h RooArgProxy.h RooArgSet.h RooArgusBG.h \
-                RooBCPEffDecay.h RooBCPGenDecay.h RooBDecay.h RooBifurGauss.h RooBinning.h RooBlindTools.h RooBMixDecay.h \
-                RooBreitWigner.h RooBrentRootFinder.h RooBukinPdf.h RooCategory.h RooCategoryProxy.h RooCategorySharedProperties.h \
-                RooCatType.h RooCBShape.h RooChebychev.h RooChi2Var.h RooClassFactory.h RooCmdArg.h RooCmdConfig.h RooComplex.h \
-                RooConstVar.h RooConvCoefVar.h RooConvGenContext.h RooConvIntegrandBinding.h RooCurve.h RooCustomizer.h \
-                RooGlobalFunc.h
-ROOFITH2     := RooDataHist.h RooDataProjBinding.h RooDataSet.h RooDecay.h RooDirItem.h RooDouble.h RooDstD0BG.h RooEffGenContext.h \
-                RooEfficiency.h RooEffProd.h RooEllipse.h RooErrorHandler.h RooErrorVar.h RooExponential.h RooExtendPdf.h \
-                RooFitResult.h RooFormula.h RooFormulaVar.h RooGaussian.h RooGaussKronrodIntegrator1D.h RooGaussModel.h \
-                RooGenCategory.h RooGenContext.h RooGenericPdf.h RooGenProdProj.h RooGExpModel.h RooGraphEdge.h RooGraphNode.h \
-                RooGraphSpring.h RooGrid.h RooHashTable.h RooHistError.h RooHist.h RooHistPdf.h RooHtml.h RooImproperIntegrator1D.h \
-                RooIntegrator1D.h RooIntegrator2D.h RooIntegratorBinding.h RooInt.h RooInvTransform.h RooKeysPdf.h \
-                RooLandau.h RooLinearVar.h RooLinkedListElem.h RooLinkedList.h RooLinkedListIter.h RooLinTransBinning.h \
-                RooList.h RooListProxy.h RooMapCatEntry.h RooMappedCategory.h RooMath.h RooMCIntegrator.h RooMCStudy.h \
-                RooMinuit.h RooMPSentinel.h RooMultiCategory.h RooMultiCatIter.h RooNameReg.h  RooFit.h
+                RooAbsIntegrator.h RooAbsLValue.h RooAbsMCStudyModule.h RooAbsOptGoodnessOfFit.h RooAbsPdf.h RooAbsProxy.h \
+                RooAbsReal.h RooAbsRealLValue.h RooAbsRootFinder.h RooAbsString.h RooAcceptReject.h RooAdaptiveGaussKronrodIntegrator1D.h \
+                RooAddGenContext.h RooAddition.h RooAddModel.h RooAddPdf.h RooAICRegistry.h RooArgList.h RooArgProxy.h \
+                RooArgSet.h RooArgusBG.h RooBCPEffDecay.h RooBCPGenDecay.h RooBDecay.h RooBifurGauss.h RooBinning.h RooBlindTools.h \
+                RooBMixDecay.h RooBreitWigner.h RooBrentRootFinder.h RooBukinPdf.h RooCategory.h RooCategoryProxy.h \
+                RooCategorySharedProperties.h RooCatType.h RooCBShape.h RooChebychev.h RooChi2Var.h RooClassFactory.h \
+                RooCmdArg.h RooCmdConfig.h RooComplex.h RooConstVar.h RooConvCoefVar.h RooConvGenContext.h RooConvIntegrandBinding.h \
+                RooCurve.h RooCustomizer.h  RooGlobalFunc.h
+ROOFITH2     := RooDataHist.h RooDataProjBinding.h RooDataSet.h RooDecay.h RooDirItem.h RooDLLSignificanceMCSModule.h \
+                RooDouble.h RooDstD0BG.h RooEffGenContext.h RooEfficiency.h RooEffProd.h RooEllipse.h RooErrorHandler.h \
+                RooErrorVar.h RooExponential.h RooExtendPdf.h RooFitResult.h RooFormula.h RooFormulaVar.h RooGaussian.h \
+                RooGaussKronrodIntegrator1D.h RooGaussModel.h RooGenCategory.h RooGenContext.h RooGenericPdf.h RooGenProdProj.h \
+                RooGExpModel.h RooGraphEdge.h RooGraphNode.h RooGraphSpring.h RooGrid.h RooHashTable.h RooHistError.h \
+                RooHist.h RooHistPdf.h RooHtml.h RooImproperIntegrator1D.h RooIntegrator1D.h RooIntegrator2D.h RooIntegratorBinding.h \
+                RooInt.h RooInvTransform.h RooKeysPdf.h RooLandau.h RooLinearVar.h RooLinkedListElem.h RooLinkedList.h \
+                RooLinkedListIter.h RooLinTransBinning.h RooList.h RooListProxy.h RooMapCatEntry.h RooMappedCategory.h \
+                RooMath.h RooMCIntegrator.h RooMCStudy.h RooMinuit.h RooMPSentinel.h RooMultiCategory.h RooMultiCatIter.h \
+                RooNameReg.h  RooFit.h
 ROOFITH3     := RooNameSet.h RooNLLVar.h RooNonCPEigenDecay.h RooNormListManager.h RooNormManager.h RooNormSetCache.h \
                 RooNovosibirsk.h RooNumber.h RooNumConvolution.h RooNumConvPdf.h RooNumIntConfig.h RooNumIntFactory.h \
                 RooParametricStepFunction.h RooPlotable.h RooPlot.h RooPolynomial.h RooPolyVar.h RooPrintable.h RooProdGenContext.h \
-                RooProdPdf.h RooProduct.h RooPullVar.h RooQuasiRandomGenerator.h RooRandom.h RooRangeBinning.h RooRealAnalytic.h \
-                RooRealBinding.h RooRealConstant.h RooRealIntegral.h RooRealMPFE.h RooRealProxy.h RooRealSumPdf.h RooRealVar.h \
-                RooRealVarSharedProperties.h RooRefCountList.h RooResolutionModel.h RooScaledFunc.h RooSegmentedIntegrator1D.h \
-                RooSegmentedIntegrator2D.h RooSetPair.h RooSetProxy.h RooSharedProperties.h RooSharedPropertiesList.h \
-                RooSimGenContext.h RooSimPdfBuilder.h RooSimultaneous.h RooStreamParser.h RooStringVar.h RooSuperCategory.h \
-                RooTable.h RooThreshEntry.h RooThresholdCategory.h RooTObjWrap.h RooTrace.h RooTreeData.h RooTruthModel.h \
-                RooUnblindCPAsymVar.h RooUnblindOffset.h RooUnblindPrecision.h RooUnblindUniform.h RooUniformBinning.h \
-                RooVoigtian.h
+                RooProdPdf.h RooProduct.h RooPullVar.h RooQuasiRandomGenerator.h RooRandom.h RooRandomizeParamMCSModule.h \
+                RooRangeBinning.h RooRealAnalytic.h RooRealBinding.h RooRealConstant.h RooRealIntegral.h RooRealMPFE.h \
+                RooRealProxy.h RooRealSumPdf.h RooRealVar.h RooRealVarSharedProperties.h RooRefCountList.h RooResolutionModel.h \
+                RooScaledFunc.h RooSegmentedIntegrator1D.h RooSegmentedIntegrator2D.h RooSetPair.h RooSetProxy.h \
+                RooSharedProperties.h RooSharedPropertiesList.h RooSimGenContext.h RooSimPdfBuilder.h RooSimultaneous.h \
+                RooStreamParser.h RooStringVar.h RooSuperCategory.h RooTable.h RooThreshEntry.h RooThresholdCategory.h \
+                RooTObjWrap.h RooTrace.h RooTreeData.h RooTruthModel.h RooUnblindCPAsymVar.h RooUnblindOffset.h RooUnblindPrecision.h \
+                RooUnblindUniform.h RooUniformBinning.h RooVoigtian.h 
 
 ROOFITH1     := $(patsubst %,$(MODDIRI)/%,$(ROOFITH1))
 ROOFITH2     := $(patsubst %,$(MODDIRI)/%,$(ROOFITH2))
 ROOFITH3     := $(patsubst %,$(MODDIRI)/%,$(ROOFITH3))
 ROOFITH      := $(ROOFITH1) $(ROOFITH2) $(ROOFITH3)
 ROOFITS      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
+ROOFITO      := $(ROOFITS:.cxx=.o)
 ROOFITO      := $(ROOFITS:.cxx=.o)
 
 ROOFITDEP    := $(ROOFITO:.o=.d) $(ROOFITDO:.o=.d)
@@ -81,12 +83,6 @@ INCLUDEFILES += $(ROOFITDEP)
 endif
 
 ##### local rules #####
-
-# force untar in first pass, so that in second pass ROOFITS is correctly set
-ifeq ($(findstring $(MAKECMDGOALS),distclean maintainer-clean),)
--include $(ROOFITETAG)
-endif
-
 $(ROOFITETAG): $(ROOFITSRCS)
 		@(if [ ! -d src ]; then \
 		   echo "*** Extracting roofit source ..."; \
@@ -100,10 +96,10 @@ $(ROOFITETAG): $(ROOFITSRCS)
 		   touch $$etag ; \
 		fi)
 
-$(ROOFITH) $(ROOFITL1) $(ROOFITL2) $(ROOFITL3): $(ROOFITETAG)
+$(ROOFITH) $(ROOFITL1) $(ROOFITL2) $(ROOFITL3):     $(ROOFITETAG)
 
 # Use static rule here instead of implicit rule
-$(ROOFITINCH):  include/%.h: $(ROOFITDIRI)/%.h $(ROOFITETAG)
+$(ROOFITINCH):  include/%.h: $(ROOFITDIRI)/%.h
 		cp $< $@
 
 $(ROOFITLIB):   $(ROOFITO) $(ROOFITDO) $(ORDER_) $(MAINLIBS) $(ROOFITLIBDEP)
