38a0c722d624e710bf9caa2c4f37b26ba17f0c8f
Wim Lavrijsen <WLavrijsen@lbl.gov>
2007-01-20 01:17:34 +0000

    new test of TPyException bugfix found by Scott
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1023 7c735d0d-823a-0410-acfd-cd159556d4bf

--- python/regression/PyROOT_regressiontests.py
+++ python/regression/PyROOT_regressiontests.py
@@ -1,7 +1,7 @@
 # File: roottest/python/regression/PyROOT_regressiontests.py
 # Author: Wim Lavrijsen (LBNL, WLavrijsen@lbl.gov)
 # Created: 01/02/07
-# Last: 01/02/07
+# Last: 01/16/07
 
 """Regression tests, lacking a better place, for PyROOT package."""
 
@@ -9,7 +9,8 @@ import os, sys, unittest
 from ROOT import *
 
 __all__ = [
-   'Regression1TwiceImportStarTestCase'
+   'Regression1TwiceImportStarTestCase',
+   'Regression2PyExceptionTestcase'
 ]
 
 
@@ -21,6 +22,28 @@ class Regression1TwiceImportStarTestCase( unittest.TestCase ):
       """Test handling of twice 'from ROOT import*'"""
 
       x = TestTChain()        # TestTChain defined in Amir.py
+
+
+### "from ROOT import *" done in import-*-ed module ==========================
+class Regression2PyExceptionTestcase( unittest.TestCase ):
+   def test1RaiseAndTrapPyException( self ):
+      """Test handling of a thrown TPyException object"""
+
+      gROOT.LoadMacro( "Scott.C+" )
+
+    # test of not overloaded global function
+      self.assertRaises( SyntaxError, ThrowPyException )
+      try:
+         ThrowPyException()
+      except SyntaxError, e:
+         self.assertEqual( str(e), "test error message" )
+
+    # test of overloaded function
+      self.assertRaises( SyntaxError, MyThrowingClass.ThrowPyException, 1 )
+      try:
+         MyThrowingClass.ThrowPyException( 1 )
+      except SyntaxError, e:
+         self.assertEqual( str(e), "overloaded int test error message" )
 
 
 ## actual test run
