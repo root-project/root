4926c316c2c1d43654b4a52c67ac7414f6df5fe8
Rene Brun <Rene.Brun@cern.ch>
2007-01-25 11:46:20 +0000

    New abstract interface class for the I/O sub-system. This class is designed to remove direct dependencies of the directories base, meta and cont from the concrete I/O classes that will be moved to a separate directory "io" in the near future. TVirtualIO::GetIO is a static function to be called by the base/cont/meta classes doind I/O. When thsi function is called for the first time, it instantiate the concrete implementation (TFileIO) via the PluginManager.
    git-svn-id: http://root.cern.ch/svn/root/trunk@17496 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ base/inc/TVirtualIO.h
@@ -0,0 +1,66 @@
+// @(#)root/base:$Name:  $:$Id: TVirtualIO.h,v 1.3 2006/10/27 16:21:11 brun Exp $
+// Author: Rene Brun 24/01/2007
+
+#ifndef ROOT_TVirtualIO
+#define ROOT_TVirtualIO
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      
+// TVirtualIO                                                       
+//                                                                     
+// TVirtualIO is an interface class for File I/O operations.
+// The class is used to decouple the base classes from the I/O packages.
+// The concrete I/O sub-system is dynamically linked by the PluginManager.
+// The default implementation TFileIO can be changed in system.rootrc.
+//                                     
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ROOT_TObject
+#include "TObject.h"
+#endif
+
+#ifndef ROOT_TString
+#include "TString.h"
+#endif
+
+class TProcessID;
+class TRefTable;
+
+class TVirtualIO: public TObject {
+
+ protected:
+   static TVirtualIO *fgIO;      //Pointer to concrete I/O class
+   static TString     fgDefault; //Name of the default I/O system
+   
+   TVirtualIO(const TVirtualIO& io);
+   TVirtualIO& operator=(const TVirtualIO& io);
+   
+ public:
+
+   TVirtualIO();
+   virtual ~TVirtualIO();
+
+   virtual TObject    *CloneObject(const TObject *obj) = 0;
+   virtual TObject    *FindObjectAny(const char *name) const = 0;
+   virtual TProcessID *GetLastProcessID(TBuffer &b, TRefTable *reftable) const = 0;
+   virtual UInt_t      GetTRefExecId() = 0;
+   virtual Int_t       ReadObject(TObject *obj, const char *name) = 0;
+   virtual TObject    *Open(const char *name, Option_t *option = "",
+                            const char *ftitle = "", Int_t compress = 1,
+                            Int_t netopt = 0) = 0;
+   virtual UShort_t    ReadProcessID (TBuffer &b, TProcessID *pid) = 0;
+   virtual void        ReadRefUniqueID(TBuffer &b, TObject *obj) = 0;    
+   virtual Int_t       SaveObjectAs(const TObject *obj, const char *filename="", Option_t *option="") = 0;
+   virtual void        SetRefAction(TObject *ref, TObject *parent) = 0;
+   virtual Int_t       WriteObject(const TObject *obj, const char *name=0, Int_t opt=0, Int_t bufsiz=0) const = 0;
+   virtual UShort_t    WriteProcessID(TBuffer &b, TProcessID *pid) = 0;
+   virtual void        WriteRefUniqueID(TBuffer &b, TObject *obj) = 0;
+   
+   static TVirtualIO  *GetIO();
+   static const char  *GetDefaultIO();
+   static void         SetDefaultIO(const char *name ="");
+
+   ClassDef(TVirtualIO, 0); //abstract interface for File I/O operations
+};
+
+#endif
