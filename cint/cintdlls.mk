2da319950a5b957aa5f16c188078a604b67e06f7
Rene Brun <Rene.Brun@cern.ch>
2007-02-01 09:46:54 +0000

    From Bertrand: Fix cintdlls build problem on Windows (was trying to link against libCore.dll).
    git-svn-id: http://root.cern.ch/svn/root/trunk@17604 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cintdlls.mk
+++ cint/cintdlls.mk
@@ -188,7 +188,7 @@ $(CINTDIRDLLSTL)/rootcint_%.cxx: $(ROOTCINTTMPEXE)
 $(patsubst lib/lib%Dict.$(SOEXT),$(CINTDIRDLLSTL)/rootcint_%.o,$(CINTDICTDLLS)): CINTCXXFLAGS += -I.
 
 $(CINTDICTDLLS): lib/lib%Dict.$(SOEXT): $(CINTDIRDLLSTL)/rootcint_%.o $(CINTDLLDICTDEP)
-	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $@ $^
+	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $@ $<
 ifeq ($(PLATFORM),maxosx)
 ifeq ($(subst $(MACOSX_MINOR),,456789),456789)
 	rm -f $@
