b44040f88ce4e9ed2dadb07893917748215467c0
Rene Brun <Rene.Brun@cern.ch>
2007-02-03 08:16:34 +0000

    In TMinuit::Contour set the last point equal to first point in order to cle the contour line.
    git-svn-id: http://root.cern.ch/svn/root/trunk@17650 27541ba8-7e3a-0410-8455-c3a389f83636

--- minuit/src/TMinuit.cxx
+++ minuit/src/TMinuit.cxx
@@ -1,4 +1,4 @@
-// @(#)root/minuit:$Name:  $:$Id: TMinuit.cxx,v 1.55 2006/11/01 07:31:57 brun Exp $
+// @(#)root/minuit:$Name:  $:$Id: TMinuit.cxx,v 1.56 2006/12/12 13:56:54 couet Exp $
 // Author: Rene Brun, Frederick James   12/08/95
 
 /*************************************************************************
@@ -626,7 +626,8 @@ TObject *TMinuit::Contour(Int_t npoints, Int_t pa1, Int_t pa2)
   // to n^2. The fcn function has to be set before the routine is called.
   //
   // The TGraph object is created via the interpreter. The user must cast it
-  // to a TGraph*
+  // to a TGraph*. Note that the TGraph is created with npoints+1 in order to
+  // close the contour (setting last point equal to first point).
   //
   // You can find an example in $ROOTSYS/tutorials/fit/fitcont.C
 
@@ -654,7 +655,7 @@ TObject *TMinuit::Contour(Int_t npoints, Int_t pa1, Int_t pa2)
    TPluginHandler *h;
    if ((h = gROOT->GetPluginManager()->FindHandler("TMinuitGraph"))) {
       if (h->LoadPlugin() != -1)
-      gr = (TObject*)h->ExecPlugin(3,npoints,xcoor,ycoor);
+      gr = (TObject*)h->ExecPlugin(3,npoints+1,xcoor,ycoor);
    }
    delete [] xcoor;
    delete [] ycoor;
