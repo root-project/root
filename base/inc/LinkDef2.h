197f41f550d5fcb7fdfba9dc13ccae5e17d9ed96
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-02-08 19:06:40 +0000

    for some reason the statement: #include "cint/lib/dll_stl/str.h" causes rootcint to crash after the recent changes:
    Match rank: file     line  signature
    *  2000002 prec_stl/vector  -1 void swap(TString&,TString&);
      ffffffff prec_stl/vector  -1 void swap(unsigned int&,unsigned int&);
    *  2000002 prec_stl/string  -1 void swap(string&,string&);
    Warning: #pragma link, function swap(string&lhs,string&rhs) not found cint/lib/dll_stl/str.h:91:
    
    removing it seems to have no side effects.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@17804 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/LinkDef2.h
+++ base/inc/LinkDef2.h
@@ -1,4 +1,4 @@
-/* @(#)root/base:$Name:  $:$Id: LinkDef2.h,v 1.37 2007/02/08 18:16:01 rdm Exp $ */
+/* @(#)root/base:$Name:  $:$Id: LinkDef2.h,v 1.38 2007/02/08 18:38:52 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -9,10 +9,6 @@
  *************************************************************************/
 
 #ifdef __CINT__
-
-#ifndef G__KCC
-#include "cint/lib/dll_stl/str.h"
-#endif
 
 #pragma extra_include "vector";
 #pragma extra_include "string";
