dda356693129ad18c6946230cbae6d84667aa006
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-02-10 11:09:33 +0000

    add TParameter<Float_t>.
    git-svn-id: http://root.cern.ch/svn/root/trunk@17850 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/LinkDef3.h
+++ base/inc/LinkDef3.h
@@ -1,4 +1,4 @@
-/* @(#)root/base:$Name:  $:$Id: LinkDef3.h,v 1.27 2006/11/28 12:10:52 rdm Exp $ */
+/* @(#)root/base:$Name:  $:$Id: LinkDef3.h,v 1.28 2007/02/08 18:16:01 rdm Exp $ */
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -249,6 +249,7 @@
 #pragma link C++ class std::pair<const double,char*>+;
 
 #pragma extra_include "Rtypes.h";
+#pragma link C++ class TParameter<Float_t>+;
 #pragma link C++ class TParameter<Double_t>+;
 #pragma link C++ class TParameter<Int_t>+;
 #pragma link C++ class TParameter<Long_t>+;
