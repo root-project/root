32658efcd0401e3535f55d34dd1130f37373995c
Rene Brun <Rene.Brun@cern.ch>
2007-03-01 07:54:24 +0000

    In TMultiGraph::SavePrimitive the TPaveStats object (when one) was added twice to the list of functions.
    git-svn-id: http://root.cern.ch/svn/root/trunk@18115 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf/src/TMultiGraph.cxx
+++ graf/src/TMultiGraph.cxx
@@ -1,4 +1,4 @@
-// @(#)root/graf:$Name:  $:$Id: TMultiGraph.cxx,v 1.36 2007/01/15 16:10:10 brun Exp $
+// @(#)root/graf:$Name:  $:$Id: TMultiGraph.cxx,v 1.37 2007/01/30 11:49:14 brun Exp $
 // Author: Rene Brun   12/10/2000
 
 /*************************************************************************
@@ -1205,9 +1205,6 @@ void TMultiGraph::SavePrimitive(ostream &out, Option_t *option /*= ""*/)
       while (lnk) {
          g = lnk->GetObject();
          g->SavePrimitive(out,"multigraph");
-
-         out<<"   multigraph->Add(graph,"<<quote<<lnk->GetOption()<<quote<<");"<<endl;
-
          lnk = (TObjOptLink*)lnk->Next();
 
       }
