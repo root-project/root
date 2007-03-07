e958de4fc2f75cac3e605a733c1b8ea1c92c92b0
Rene Brun <Rene.Brun@cern.ch>
2007-03-07 09:51:03 +0000

    Fix coding conventions
    git-svn-id: http://root.cern.ch/svn/root/trunk@18177 27541ba8-7e3a-0410-8455-c3a389f83636

--- postscript/src/TPostScript.cxx
+++ postscript/src/TPostScript.cxx
@@ -1,4 +1,4 @@
-// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.70 2006/12/21 10:57:55 couet Exp $
+// @(#)root/postscript:$Name:  $:$Id: TPostScript.cxx,v 1.71 2007/03/05 12:37:17 couet Exp $
 // Author: Rene Brun, Olivier Couet, Pierre Juillot   29/11/94
 
 /*************************************************************************
@@ -2444,7 +2444,7 @@ void TPostScript::Text(Double_t xx, Double_t yy, const char *chars)
    t.SetTextFont(fTextFont);
    t.GetTextExtent(w, h, chars);
    Double_t charsLength = gPad->AbsPixeltoX(w)-gPad->AbsPixeltoX(0);
-   Int_t PScharsLength = XtoPS(charsLength)-XtoPS(0);
+   Int_t psCharsLength = XtoPS(charsLength)-XtoPS(0);
 
    // Text angle.
    Int_t psangle = Int_t(0.5 + fTextAngle);
@@ -2468,8 +2468,8 @@ void TPostScript::Text(Double_t xx, Double_t yy, const char *chars)
    WriteInteger(XtoPS(x));
    WriteInteger(YtoPS(y));
    PrintStr(Form(" t %d r ", psangle));
-   if(txalh == 2) PrintStr(Form(" %d 0 t ", -PScharsLength/2));
-   if(txalh == 3) PrintStr(Form(" %d 0 t ", -PScharsLength));
+   if(txalh == 2) PrintStr(Form(" %d 0 t ", -psCharsLength/2));
+   if(txalh == 3) PrintStr(Form(" %d 0 t ", -psCharsLength));
    PrintStr(psfont[font-1]);
    PrintStr(Form(" findfont %g sf 0 0 m (",fontsize));
 
