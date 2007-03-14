8a19b4c3fc533df64789a4cfd08bf2fc7fd61d15
Rene Brun <Rene.Brun@cern.ch>
2007-03-14 10:27:20 +0000

    Fix a portability problem (MAC)
    git-svn-id: http://root.cern.ch/svn/root/trunk@18259 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/src/global.h
+++ cint/src/global.h
@@ -270,7 +270,9 @@ extern int G__globalcomp;       /* make compiled func's global table */
 extern int G__store_globalcomp;
 extern long G__globalvarpointer; /* make compiled func's global table */
 
+#ifdef __cplusplus
 extern struct G__filetable G__srcfile[G__MAXFILE];
+#endif
 extern int G__nfile;
 
 extern int G__nobreak;
