f50f9bfdc94a3f6fe4868e4d05262539813ba48a
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2007-03-19 13:36:37 +0000

    test with fumili2
    git-svn-id: http://root.cern.ch/svn/root/trunk@18345 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/fit/minuit2GausFit.C
+++ tutorials/fit/minuit2GausFit.C
@@ -1,4 +1,4 @@
-// @(#)root/minuit2:$Name:  $:$Id: minuit2GausFit.C,v 1.1 2006/12/11 21:56:34 brun Exp $
+// @(#)root/minuit2:$Name:  $:$Id: minuit2GausFit.C,v 1.1 2005/10/27 14:11:07 brun Exp $
 // Author: L. Moneta    10/2005  
 
 /**********************************************************************
@@ -26,13 +26,25 @@ void testGausFit( std::string type = "Minuit2", int n = 1000) {
 
   TVirtualFitter::SetDefaultFitter(type.c_str() );
 
-  TH1D * h1 = new TH1D("h1","fit histo 1",100, -5, 5. );
+  std::string name; 
+  name = "h1_" + type; 
+  TH1D * h1 = new TH1D(name.c_str(),"Chi2 Fit",100, -5, 5. );
+  name = "h2_" + type; 
+  TH1D * h2 = new TH1D(name.c_str(),"Chi2 Fit with Minos Error",100, -5, 5. );
+  name = "h3_" + type; 
+  TH1D * h3 = new TH1D(name.c_str(),"Chi2 Fit with Integral and Minos",100, -5, 5. );
+  name = "h4_" + type; 
+  TH1D * h4 = new TH1D(name.c_str(),"Likelihood Fit with Minos Error",100, -5, 5. );
 
   gStyle->SetOptStat(1111111);
   gStyle->SetOptFit(1111111);    
 
   for (int i = 0; i < n; ++i) { 
-     h1->Fill( gRandom->Gaus(0,1) ); 
+     double x = gRandom->Gaus(0,1); 
+     h1->Fill( x ); 
+     h2->Fill( x ); 
+     h3->Fill( x ); 
+     h4->Fill( x ); 
   }
   
   std::string cname = type + "Canvas" ;    
@@ -40,29 +52,21 @@ void testGausFit( std::string type = "Minuit2", int n = 1000) {
   TCanvas *c1 = new TCanvas(cname.c_str(),cname.c_str(),10,10,900,900);
   c1->Divide(2,2);
   
-  TH1D * h2 = new TH1D(*h1); 
-  TH1D * h3 = new TH1D(*h1); 
-  TH1D * h4 = new TH1D(*h1); 
   c1->cd(1);
   cout << "\nDo Fit 1\n";
-
-  h1->Fit("gaus",""); 
-  h1->SetName("Chi2 Fit");
+  h1->Fit("gaus","Q"); 
   h1->Draw();
   c1->cd(2);
   cout << "\nDo Fit 2\n";
   h2->Fit("gaus","VE"); 
-  h2->SetName("Chi2 Fit with Minos Erros");
   h2->Draw();
   c1->cd(3);
   cout << "\nDo Fit 3\n";
   h3->Fit("gaus","IE"); 
-  h3->SetName("Chi2 Fit with Integral and Minos");
   h3->Draw();
   c1->cd(4);
   cout << "\nDo Fit 4\n";
   h4->Fit("gaus","VLE"); 
-  h4->SetName("Likelihood Fit with Minos Erros");
   h4->Draw();
 
 }
