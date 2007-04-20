d0ab8ec3410c808e450ca283043c0f429e4c592b
Rene Brun <Rene.Brun@cern.ch>
2007-04-20 12:06:28 +0000

    From Philippe: Fix a compilation error (with intel icc)
    git-svn-id: http://root.cern.ch/svn/root/trunk@18538 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/inc/Option.h
+++ tmva/inc/Option.h
@@ -1,4 +1,4 @@
-// @(#)root/tmva $Id: Option.h,v 1.7 2007/01/16 14:28:35 brun Exp $   
+// @(#)root/tmva $Id: Option.h,v 1.8 2007/04/19 06:53:01 brun Exp $   
 // Author: Andreas Hoecker, Joerg Stelzer, Helge Voss
 
 /**********************************************************************************
@@ -147,7 +147,7 @@ namespace TMVA {
       virtual Bool_t IsArrayOpt() const { return kTRUE; }
       virtual Int_t  GetArraySize() const { return fSize; }
    
-      using OptionBase::Print;
+      using Option<T>::Print;
       virtual void Print(ostream&, Int_t levelofdetail=0) const;
 
       virtual Bool_t SetValue(const TString& val, Int_t i=0);
