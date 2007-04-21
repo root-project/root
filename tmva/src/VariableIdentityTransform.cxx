d8f292a2f3cb59b3c0e179f79b2648e84abc38af
Rene Brun <Rene.Brun@cern.ch>
2007-04-21 07:45:57 +0000

    This class was forgotten in the new revision.
    git-svn-id: http://root.cern.ch/svn/root/trunk@18546 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tmva/src/VariableIdentityTransform.cxx
@@ -0,0 +1,60 @@
+// @(#)root/tmva $\Id$
+// Author: Andreas Hoecker, Joerg Stelzer, Helge Voss
+
+/**********************************************************************************
+ * Project: TMVA - a Root-integrated toolkit for multivariate data analysis       *
+ * Package: TMVA                                                                  *
+ * Class  : VariableIdentityTransform                                             *
+ * Web    : http://tmva.sourceforge.net                                           *
+ *                                                                                *
+ * Description:                                                                   *
+ *      Implementation (see header for description)                               *
+ *                                                                                *
+ * Authors (alphabetical):                                                        *
+ *      Andreas Hoecker <Andreas.Hocker@cern.ch> - CERN, Switzerland              *
+ *      Joerg Stelzer   <Joerg.Stelzer@cern.ch>  - CERN, Switzerland              *
+ *      Helge Voss      <Helge.Voss@cern.ch>     - MPI-K Heidelberg, Germany      *
+ *                                                                                *
+ * Copyright (c) 2005:                                                            *
+ *      CERN, Switzerland,                                                        *
+ *      MPI-K Heidelberg, Germany ,                                               *
+ *                                                                                *
+ * Redistribution and use in source and binary forms, with or without             *
+ * modification, are permitted according to the terms listed in LICENSE           *
+ * (http://tmva.sourceforge.net/LICENSE)                                          *
+ **********************************************************************************/
+
+
+#include "TMVA/VariableIdentityTransform.h"
+
+ClassImp(TMVA::VariableIdentityTransform)
+
+//_______________________________________________________________________
+TMVA::VariableIdentityTransform::VariableIdentityTransform( std::vector<VariableInfo>& varinfo )
+   : VariableTransformBase( varinfo, Types::kNone )
+{
+   // constructor
+   SetName("NoTransform");
+}
+
+//_______________________________________________________________________
+Bool_t TMVA::VariableIdentityTransform::PrepareTransformation( TTree* inputTree)
+{
+   // the identity does not need to be prepared, only calculate the normalization
+   if (!IsEnabled() || IsCreated()) return kTRUE;
+
+   SetCreated( kTRUE );
+
+   CalcNorm( inputTree );
+
+   return kTRUE;
+}
+
+//_______________________________________________________________________
+void TMVA::VariableIdentityTransform::ApplyTransformation( Types::ESBType ) const
+{
+   // this transformation doesn't do anything
+   if      (fEvent == fEventRaw) return;
+   else if (fEvent != 0)         delete fEvent;
+   else    fEvent = fEventRaw;
+}
