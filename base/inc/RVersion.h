1e7ac638e742f2fccb9b3c3ad4e89d576e5184ee
Rene Brun <Rene.Brun@cern.ch>
2007-05-30 05:50:25 +0000

    Stamp development version 5.15/08
    git-svn-id: http://root.cern.ch/svn/root/trunk@18910 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "5.15/07"
-#define ROOT_RELEASE_DATE "Apr 25 2007"
-#define ROOT_RELEASE_TIME "14:56:54"
-#define ROOT_VERSION_CODE 331527
+#define ROOT_RELEASE "5.15/08"
+#define ROOT_RELEASE_DATE "May 30 2007"
+#define ROOT_RELEASE_TIME "07:03:39"
+#define ROOT_VERSION_CODE 331528
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
