2cc7087cf8675d30cc76ba744fd64c5e11ca5ef7
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-05-30 11:18:19 +0000

    dependency on UNURANLIBDEP was missing causing failure of parallel build with explicit linking.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-14-00-patches@18914 27541ba8-7e3a-0410-8455-c3a389f83636

--- unuran/Module.mk
+++ unuran/Module.mk
@@ -105,7 +105,7 @@ $(UNURANETAG):	$(UNRSRCS)
 		GNUMAKE=$(MAKE) ./configure --prefix=`pwd`/$(MODDIRS)/$(UNRVERS) CC="$$ACC"  \
 		CFLAGS="$$ACFLAGS";)
 
-$(UNURANLIB):   $(UNRCFG) $(UNRO) $(UNURANO) $(UNURANDO) $(ORDER_) $(MAINLIBS)
+$(UNURANLIB):   $(UNRCFG) $(UNRO) $(UNURANO) $(UNURANDO) $(ORDER_) $(MAINLIBS) $(UNURANLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)"  \
 		   "$(SOFLAGS)" libUnuran.$(SOEXT) $@     \
 		   "$(UNURANO) $(UNURANDO)"             \
