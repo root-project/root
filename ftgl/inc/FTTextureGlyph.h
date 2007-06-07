053c769bc35ee1b186d1451033cad6c31d88ac03
Rene Brun <Rene.Brun@cern.ch>
2007-06-07 06:54:20 +0000

    Fix a compilation error with gcc4.1 (thanks Robert Hatcher).
    git-svn-id: http://root.cern.ch/svn/root/trunk@18967 27541ba8-7e3a-0410-8455-c3a389f83636

--- ftgl/inc/FTTextureGlyph.h
+++ ftgl/inc/FTTextureGlyph.h
@@ -52,7 +52,7 @@ class FTGL_EXPORT FTTextureGlyph : public FTGlyph
          * Reset the currently active texture to zero to get into a known state before
          * drawing a string. This is to get round possible threading issues.
          */
-        static void FTTextureGlyph::ResetActiveTexture(){ activeTextureID = 0;}
+        static void ResetActiveTexture(){ activeTextureID = 0;}
         
     private:
         /**
