86f71b8ead78e5c0edde1013cebf46509290d503
Rene Brun <Rene.Brun@cern.ch>
2007-06-12 10:25:23 +0000

    From Timur: Add missing include
    git-svn-id: http://root.cern.ch/svn/root/trunk@19005 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ gl/inc/TGLContextPrivate.h
@@ -0,0 +1,58 @@
+#ifndef ROOT_TGLContextPrivate
+#define ROOT_TGLContextPrivate
+
+#ifndef ROOT_TGLIncludes
+#include "TGLIncludes.h"
+#endif
+#ifndef ROOT_TGLContext
+#include "TGLContext.h"
+#endif
+
+#ifdef WIN32
+
+class TGLContext::TGLContextPrivate {
+public:
+   HWND        fHWND;
+   HDC         fHDC;
+   HGLRC       fGLContext;
+
+   TGLContextPrivate() 
+      : fHWND(0), 
+        fHDC(0), 
+        fGLContext(0)
+   {
+   }
+
+private:
+   TGLContextPrivate(const TGLContextPrivate &);
+   TGLContextPrivate &operator = (const TGLContextPrivate &);
+};
+
+
+#else
+
+class TGLContext::TGLContextPrivate {
+public:
+   Display     *fDpy;
+   XVisualInfo *fVisualInfo;
+   GLXContext   fGLContext;
+   Int_t        fWindowIndex;
+   GLXPbuffer   fPBDC;
+   
+   TGLContextPrivate()
+      : fDpy(0),
+        fVisualInfo(0),
+        fGLContext(0),
+        fWindowIndex(-1),
+        fPBDC(0)
+   {
+   }
+   
+private:
+   TGLContextPrivate(const TGLContextPrivate &);
+   TGLContextPrivate &operator = (const TGLContextPrivate &);
+};
+
+#endif
+
+#endif
