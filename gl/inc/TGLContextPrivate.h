37006e752cf955143ca828e82a21695760531364
Rene Brun <Rene.Brun@cern.ch>
2007-06-18 07:11:50 +0000

    Eliminate EOL characters
    git-svn-id: http://root.cern.ch/svn/root/trunk@19051 27541ba8-7e3a-0410-8455-c3a389f83636

--- gl/inc/TGLContextPrivate.h
+++ gl/inc/TGLContextPrivate.h
@@ -1,4 +1,4 @@
-// @(#)root/gl:$Name:  $:$Id: TGLContextPrivate.h,v 1.2 2007/06/18 07:02:16 brun Exp $
+// @(#)root/gl:$Name:  $:$Id: TGLContextPrivate.h,v 1.3 2007/06/18 07:06:17 brun Exp $
 // Author:  Timur Pocheptsov, Matevz Tadel, June 2007
 
 #ifndef ROOT_TGLContextPrivate
