3dd725f272030dd720b2ee6d797af228adb64f01
Axel Naumann <Axel.Naumann@cern.ch>
2007-06-22 13:34:13 +0000

    Invert the precedence of the rootmaps read by the plugin service: first ones (as defined by LD_LIBRARY_PATH) have now highest precedence. Allow warnings to be issued about conflicting directives (to cerr for now) if Debug() is set.
    git-svn-id: http://root.cern.ch/svn/root/trunk@19133 27541ba8-7e3a-0410-8455-c3a389f83636

--- reflex/src/PluginFactoryMap.cxx
+++ reflex/src/PluginFactoryMap.cxx
@@ -1,4 +1,4 @@
-// @(#)root/reflex:$Name:  $:$Id: PluginFactoryMap.cxx,v 1.3 2006/12/02 09:07:04 brun Exp $
+// @(#)root/reflex:$Name:  $:$Id: PluginFactoryMap.cxx,v 1.4 2007/04/03 08:25:40 axel Exp $
 // Author: Pere Mato 2006
 
 // Copyright CERN, CH-1211 Geneva 23, 2004-2006, All rights reserved.
@@ -20,6 +20,7 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <set>
 
 using namespace ROOT::Reflex;
 using namespace std;
@@ -37,7 +38,8 @@ using namespace std;
 #define PATHSEP ":"
 #endif
 
-typedef std::map<std::string,std::list<std::string> > Map_t;
+typedef std::list<std::string> Directive_t;
+typedef std::map<std::string, Directive_t> Map_t;
 
 //-------------------------------------------------------------------------------
 static Map_t & sMap() {
@@ -46,6 +48,43 @@ static Map_t & sMap() {
    static Map_t s_map;
    return s_map;
 }
+
+//-------------------------------------------------------------------------------
+static void DumpFactoryDirective(std::ostream& out, 
+                                 const Directive_t& directive) {
+//-------------------------------------------------------------------------------
+// Dump a directive to out.
+   bool first = true;
+   for (Directive_t::const_iterator iLib = directive.begin();
+        iLib != directive.end(); ++iLib) {
+      if (!first) 
+         out << ", ";
+      else first = false;
+      out << *iLib;
+   }
+}
+
+//-------------------------------------------------------------------------------
+bool ConflictingDirective(const Directive_t& lhs, const Directive_t& rhs) {
+//-------------------------------------------------------------------------------
+// Check for inequality of directives, disregarding order of all but first.
+   if (*lhs.begin() != *rhs.begin()) return true;
+   if (lhs.size() < 2 || lhs.size() < 2) return false; // first entry equal, and it's all we have
+
+   set<string> setLHS, setRHS;
+   // can't use insert(iter, iter) because of solaris :-/
+   for (Directive_t::const_iterator iLHS = ++lhs.begin(); iLHS != lhs.end(); ++iLHS)
+      setLHS.insert(*iLHS);
+   for (Directive_t::const_iterator iRHS = ++rhs.begin(); iRHS != rhs.end(); ++iRHS)
+      setRHS.insert(*iRHS);
+   if (setLHS.size() != setRHS.size()) return true;
+
+   for (set<string>::const_iterator iSetLHS = setLHS.begin();
+        iSetLHS != setLHS.end(); ++iSetLHS)
+      if (setRHS.find(*iSetLHS) == setRHS.end()) return true;
+   return false;
+}
+
 
 int ROOT::Reflex::PluginFactoryMap::fgDebugLevel = 0;
 
@@ -104,17 +143,41 @@ void ROOT::Reflex::PluginFactoryMap::FillMap(const std::string& filename) {
          string cname = line.substr(8,pc-8);
          string::size_type pv = line.substr(pc+1).find_first_not_of(' ');
          string vlibs = line.substr(pc+1+pv);
-         list<string> libs;
+         Directive_t libs;
          for(char* t=strtok((char*)vlibs.c_str()," "); t; t = strtok(0," "))
             libs.push_back(t);
-         // Inserting name in map 
-         sMap()[cname] = libs;
-         
-         if ( Debug() > 1 ) {
-            cout << "FactoryMap:    Name " << cname << ": ";
-            for ( list<string>::iterator i = libs.begin(); i != libs.end(); i++ )
-               cout << *i << " ";
-            cout << endl;
+
+         // Check whether cname already has a directive,
+         // warn and ignore this one if it's conflicting
+         Map_t::const_iterator iPreviousDirective = sMap().find(cname);
+         if (iPreviousDirective != sMap().end()) {
+            if (ConflictingDirective(libs, iPreviousDirective->second)) {
+               if (Debug()) {
+                  cerr << "ROOT::Reflex::PluginFactoryMap::FillMap() - WARNING: "
+                       << "conflicting directives for " << cname << endl
+                       << "  Previous: \"";
+                  DumpFactoryDirective(cerr, iPreviousDirective->second);
+                  cerr << "\"" << endl
+                       << "  Directive in " << filename << ": \"";
+                  DumpFactoryDirective(cerr, libs);
+                  cerr << "\"" << endl
+                       << "  Previous takes precedence." << endl;
+               }
+            } else 
+               if ( Debug() > 1 ) {
+                  cout << "FactoryMap: copy of directive detected for Name " << cname << ": ";
+                  DumpFactoryDirective(cout, libs);
+                  cout << endl;
+               }
+         } else {
+            // Inserting name in map 
+            sMap()[cname] = libs;
+
+            if ( Debug() > 1 ) {
+               cout << "FactoryMap:    Name " << cname << ": ";
+               DumpFactoryDirective(cout, libs);
+               cout << endl;
+            }
          }
       }
    }
