1e1265582bc766bcd1d3d8eaf920bc37629e2972
Ilka Antcheva <Ilka.Antcheva@cern.ch>
2007-06-22 15:02:00 +0000

    From Valeri Fine: minor fix in #if statement
    git-svn-id: http://root.cern.ch/svn/root/trunk@19136 27541ba8-7e3a-0410-8455-c3a389f83636

--- qt/src/TGQt.cxx
+++ qt/src/TGQt.cxx
@@ -1,4 +1,4 @@
-// @(#)root/qt:$Name:  $:$Id: TGQt.cxx,v 1.41 2007/06/20 07:01:04 antcheva Exp $
+// @(#)root/qt:$Name:  $:$Id: TGQt.cxx,v 1.42 2007/06/22 07:27:50 antcheva Exp $
 // Author: Valeri Fine   21/01/2002
 
 /*************************************************************************
@@ -729,7 +729,7 @@ Bool_t TGQt::Init(void* /*display*/)
 {
    //*-*-*-*-*-*-*-*-*-*-*-*-*-*Qt GUI initialization-*-*-*-*-*-*-*-*-*-*-*-*-*-*
    //*-*                        ========================                      *-*
-   fprintf(stderr,"** $Id: TGQt.cxx,v 1.161 2007/06/21 01:13:45 fine Exp $ this=%p\n",this);
+   fprintf(stderr,"** $Id: TGQt.cxx,v 1.42 2007/06/22 07:27:50 antcheva Exp $ this=%p\n",this);
 #if QT_VERSION >= 0x40000
 #ifndef R__QTWIN32
    extern void qt_x11_set_global_double_buffer(bool);
@@ -1667,7 +1667,7 @@ ULong_t  TGQt::GetPixel(Color_t cindex)
    // see: GQTGUI.cxx:QtColor() also
    ULong_t rootPixel = 0;
    QColor color = ColorIndex(cindex);
-#if (ROOT_VERSION_CODE < ROOT_VERSION(5,12,0) ) || defined R__WIN32
+#ifdef R__WIN32
    rootPixel =                    ( color.blue () & 255 );
    rootPixel = (rootPixel << 8) | ( color.green() & 255 ) ;
    rootPixel = (rootPixel << 8) | ( color.red  () & 255 );
