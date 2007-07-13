f3fa26e93386c3acc060baa97bf7e959930c65e1
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-07-13 10:17:43 +0000

    From Axel: by default, cd on windows doesn't change drives.
    git-svn-id: http://root.cern.ch/svn/root/trunk@19334 27541ba8-7e3a-0410-8455-c3a389f83636

--- config/thisroot.bat
+++ config/thisroot.bat
@@ -6,9 +6,9 @@ rem Author: Axel Naumann, 10/07/2007
 set OLDPATH=%CD%
 set THIS=%0
 set THIS=%THIS:~0,-12%.
-cd %THIS%\..
+cd /D %THIS%\..
 set ROOTSYS=%CD%
-cd %OLDPATH%
+cd /D %OLDPATH%
 set PATH=%ROOTSYS%\bin;%PATH%
 set OLDPATH=
 set THIS=
