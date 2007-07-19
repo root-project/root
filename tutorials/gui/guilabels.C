ae43d416a04860518bd29342203f0c6e8b2bf502
Ilka Antcheva <Ilka.Antcheva@cern.ch>
2007-07-19 12:43:42 +0000

    Added an example of a label with fixed size in pixels.
    git-svn-id: http://root.cern.ch/svn/root/trunk@19403 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/gui/guilabels.C
+++ tutorials/gui/guilabels.C
@@ -16,7 +16,7 @@
 class MyMainFrame : public TGMainFrame {
 
 private:
-   TGLabel       *fLbl1, *fLbl2, *fLbl3;
+   TGLabel       *fLbl1, *fLbl2, *fLbl3, *fLbl4;
    TGTextButton  *fToggle;
 public:
    MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h);
@@ -34,11 +34,13 @@ void MyMainFrame::DoSwitch()
       fLbl1->Enable();
       fLbl2->Enable();
       fLbl3->Enable();
+      fLbl4->Enable();
    } else {
    printf("Disabled labels\n");
       fLbl1->Disable();
       fLbl2->Disable();
       fLbl3->Disable();
+      fLbl4->Disable();
    }
 }
 
@@ -61,27 +63,32 @@ MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) :
     GCValues_t   gval;
     gval.fMask = kGCBackground | kGCFont | kGCForeground;
     gval.fFont = font->GetFontHandle();
-    gClient->GetColorByName("red", gval.fBackground);
+    gClient->GetColorByName("yellow", gval.fBackground);
     fTextGC = gClient->GetGC(&gval, kTRUE);
    
    
-   ULong_t bcolor, ycolor, rcolor;
+   ULong_t bcolor, ycolor;
    gClient->GetColorByName("yellow", ycolor);
-   gClient->GetColorByName("red", rcolor);
    gClient->GetColorByName("blue", bcolor);
 
    // Create a main frame
-   fLbl1 = new TGLabel(this, "HasBackground", fTextGC->GetGC(), labelfont, kChildFrame, bcolor);
-   AddFrame(fLbl1, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+   fLbl1 = new TGLabel(this, "OwnFont & Bck/ForgrColor", fTextGC->GetGC(), labelfont, kChildFrame, bcolor);
+   AddFrame(fLbl1, new TGLayoutHints(kLHintsNormal, 5, 5, 3, 4));
    fLbl1->SetTextColor(ycolor);
 
-
-   fLbl2 = new TGLabel(this, "Has Own Font + FColor", fTextGC->GetGC(), labelfont);
+   fLbl2 = new TGLabel(this, "Own Font & ForegroundColor", fTextGC->GetGC(), labelfont);
    AddFrame(fLbl2,  new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
-   fLbl2->SetTextColor(rcolor);
-
+   fLbl2->SetTextColor(ycolor);
+   
    fLbl3 = new TGLabel(this, "Normal Label");
    AddFrame(fLbl3,  new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+
+   fLbl4 = new TGLabel(this, "Multi-line label, resized\nto 300x80 pixels", 
+                       fTextGC->GetGC(), labelfont, kChildFrame, bcolor);
+   AddFrame(fLbl4, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+   fLbl4->SetTextColor(ycolor);
+   fLbl4->ChangeOptions(fLbl4->GetOptions() | kFixedSize);
+   fLbl4->Resize(350, 80);
 
    // Create a horizontal frame containing two buttons
    TGTextButton *toggle = new TGTextButton(this, "&Toggle Labels");
