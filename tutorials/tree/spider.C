8d16fdabaa03c99964b1291b8958ecaf2eecd9a3
Rene Brun <Rene.Brun@cern.ch>
2007-08-16 15:59:58 +0000

    From Bastien: script illustrating the use of the TSpider class
    git-svn-id: http://root.cern.ch/svn/root/trunk@19629 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tutorials/tree/spider.C
@@ -0,0 +1,23 @@
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TNtuple.h"
+#include "TSpider.h"
+   
+// script illustrating the use of the TSpider class
+//Author: Bastien Dallapiazza
+void spider() {
+   TCanvas *c1 = new TCanvas("c1","TSpider example",200,10,700,700);
+   TFile *f = new TFile("$ROOTSYS/tutorials/hsimple.root");
+   if (!f || f->IsZombie()) {
+      printf("Please run <ROOT location>/tutorials/hsimple.C before.");
+      return;
+   }
+   TNtuple* ntuple = (TNtuple*)f->Get("ntuple");
+   TString varexp = "px:py:pz:random:sin(px):log(px/py):log(pz)";
+   TString select = "px>0 && py>0 && pz>0";
+   TString options = "average";
+   TSpider *spider = new TSpider(ntuple,varexp.Data(),select.Data(),options.Data());
+   spider->Draw();
+   c1->ToggleEditor();
+   c1->Selected(c1,spider,1);
+}
