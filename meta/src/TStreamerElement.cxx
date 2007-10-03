09e4344558079cabbb8aeb868012bfd91d912c0e
Rene Brun <Rene.Brun@cern.ch>
2007-10-03 14:16:39 +0000

    Make symmetric the use of GetRange for Float16 and Double32
    git-svn-id: http://root.cern.ch/svn/root/trunk@20190 27541ba8-7e3a-0410-8455-c3a389f83636

--- meta/src/TStreamerElement.cxx
+++ meta/src/TStreamerElement.cxx
@@ -190,11 +190,7 @@ TStreamerElement::TStreamerElement(const char *name, const char *title, Int_t of
    for (Int_t i=0;i<5;i++) fMaxIndex[i] = 0;
    if (fTypeName == "Float16_t" || fTypeName == "Float16_t*") {
       GetRange(title,fXmin,fXmax,fFactor);
-      //by default set number of bits to 12 if not specified
-      //printf("--name=%s, fXmin=%g, fXmax=%g, fFactor=%g\n",name,fXmin,fXmax,fFactor);
-      if (fFactor==0 && fXmin <= fXmax) fXmin=12.1; //rounding!
-      //printf("name=%s, fXmin=%g, fXmax=%g, fFactor=%g\n",name,fXmin,fXmax,fFactor);
-      SetBit(kHasRange);
+      if (fFactor > 0 || fXmin > 0) SetBit(kHasRange);
    }
    if (fTypeName == "Double32_t" || fTypeName == "Double32_t*") {
       GetRange(title,fXmin,fXmax,fFactor);
