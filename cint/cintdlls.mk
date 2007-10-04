e422327d86cb2327e40380f18c785c7a0659f0ea
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-10-04 22:43:28 +0000

    fix some macosx issues with cleaning up the cint/stl/*.so's.
    git-svn-id: http://root.cern.ch/svn/root/trunk@20219 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cintdlls.mk
+++ cint/cintdlls.mk
@@ -110,7 +110,7 @@ $(CINTDLLS): CINTCXXFLAGS += $(CINTDLLINCDIRS)
 ##### all cintdlls end on .dll
 ifneq ($(SOEXT),dll)
 CINTDLLSOEXTCMD = mv $(@:.dll=.$(SOEXT)) $@
-ifeq ($(PLATFORM),maxosx)
+ifeq ($(PLATFORM),macosx)
 ifeq ($(subst $(MACOSX_MINOR),,456789),456789)
 # MACOSX_MINOR < 4
   CINTDLLSOEXTCMD += ;mv $(@:.dll=.so) $@;rm -f $(@:.dll=.so)
@@ -196,7 +196,7 @@ $(patsubst lib/lib%Dict.$(SOEXT),$(CINTDIRDLLSTL)/rootcint_%.o,$(CINTDICTDLLS)):
 
 $(CINTDICTDLLS): lib/lib%Dict.$(SOEXT): $(CINTDIRDLLSTL)/rootcint_%.o $(ORDER_) $(MAINLIBS)
 	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $@) $@ $(filter-out $(MAINLIBS),$^)
-ifeq ($(PLATFORM),maxosx)
+ifeq ($(PLATFORM),macosx)
 ifeq ($(subst $(MACOSX_MINOR),,456789),456789)
 	rm -f $@
 else
@@ -228,6 +228,9 @@ distclean-cintdlls: clean-cintdlls
 	  metautils/src/stlLoader_$${cintdll}.*; done)
 	@rm -f $(ALLCINTDLLS) \
 	  $(CINTDIRL)/posix/G__c_posix.* $(CINTDIRL)/posix/mktypes$(EXEEXT)
+ifeq ($(PLATFORM),macosx)
+	@rm -f  $(CINTDIRSTL)/*.so
+endif
 
 distclean:: distclean-cintdlls
 
