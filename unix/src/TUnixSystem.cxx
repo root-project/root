e2dd72515ad75fbcc9d34d802827a0b1a1935239
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-10-24 14:30:19 +0000

    Don't return after handling kSigChild in DispatchSignals().
    git-svn-id: http://root.cern.ch/svn/root/trunk@20466 27541ba8-7e3a-0410-8455-c3a389f83636

--- unix/src/TUnixSystem.cxx
+++ unix/src/TUnixSystem.cxx
@@ -965,7 +965,7 @@ void TUnixSystem::DispatchSignals(ESignals sig)
       break;
    case kSigChild:
       CheckChilds();
-      return;
+      break;
    case kSigBus:
    case kSigSegmentationViolation:
    case kSigIllegalInstruction:
