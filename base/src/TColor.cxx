d3e92e5de7e76c1ded2af7218adc9bc20b7f0c9f
Rene Brun <Rene.Brun@cern.ch>
2007-10-24 06:36:20 +0000

    From Valeri Fine: Fix a wrong definition of palette[48] (was 112 instead of 2).
    git-svn-id: http://root.cern.ch/svn/root/trunk@20459 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/src/TColor.cxx
+++ base/src/TColor.cxx
@@ -1322,7 +1322,7 @@ void TColor::SetPalette(Int_t ncolors, Int_t *colors)
                         21,22,23,24,25,26,27,28,29,30, 8,
                         31,32,33,34,35,36,37,38,39,40, 9,
                         41,42,43,44,45,47,48,49,46,50, 2,
-                         7, 6, 5, 4, 3, 112,1};
+                         7, 6, 5, 4, 3, 2,1};
    // set default palette (pad type)
    if (ncolors <= 0) {
       ncolors = 50;
