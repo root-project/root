025cf1a666fe17f32de9e6d613a5d4e012030e15
Rene Brun <Rene.Brun@cern.ch>
2007-11-07 11:36:35 +0000

    From Lorenzo: fix for a change in GSLIntegrator constructor to use Gauss integration rule
    git-svn-id: http://root.cern.ch/svn/root/trunk@20698 27541ba8-7e3a-0410-8455-c3a389f83636

--- etc/plugins/ROOT@@Math@@VirtualIntegrator/P010_GSLIntegrator.C
+++ etc/plugins/ROOT@@Math@@VirtualIntegrator/P010_GSLIntegrator.C
@@ -1,5 +1,5 @@
 void P010_GSLIntegrator()
 {
    gPluginMgr->AddHandler("ROOT::Math::VirtualIntegrator", "GSLIntegrator", "ROOT::Math::GSLIntegrator",
-      "MathMore", "GSLIntegrator(const char *, double, double, unsigned int)");
+      "MathMore", "GSLIntegrator(const char *, int, double, double, unsigned int)");
 }
