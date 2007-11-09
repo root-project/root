9b263e6f3074680d9fbaa13bffa40a9bf13961b9
Ilka Antcheva <Ilka.Antcheva@cern.ch>
2007-11-09 13:07:15 +0000

    From Valeri Fine: the bug fix:. the fEvent data-member was not initialized at ctor
    git-svn-id: http://root.cern.ch/svn/root/trunk@20740 27541ba8-7e3a-0410-8455-c3a389f83636

--- qt/inc/TQUserEvent.h
+++ qt/inc/TQUserEvent.h
@@ -34,7 +34,8 @@ public:
 #if QT_VERSION >= 0x40000
    Event_t *data() const { return fEvent;}
    void setData(Event_t *ev) { delete data(); fEvent=ev;}
-   TQUserEvent(const Event_t &pData) : QEvent(Type(QEvent::User+Type(1)))
+   TQUserEvent(const Event_t &pData) : QEvent(Type(QEvent::User+Type(1))),
+		   fEvent(0)
 #else
    TQUserEvent(const Event_t &pData) : QCustomEvent(Id(),0)
 #endif
