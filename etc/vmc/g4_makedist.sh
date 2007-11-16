df9cc57c2ec803e5947a9292d0ce562cd7866ae7
Fons Rademakers <Fons.Rademakers@cern.ch>
2007-11-16 15:48:58 +0000

    From Ivana: common VMC Makefile and build scripts.
    git-svn-id: http://root.cern.ch/svn/root/trunk@20845 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ etc/vmc/g4_makedist.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# $Id$
+
+# Script to produce source and optionally binary distribution of geant4_vmc.
+# Called by main Makefile.
+#
+# According to: 
+# $ROOTSYS/build/unix/makedist.sh
+# Author: Fons Rademakers, 29/2/2000
+#
+# Usage: makedist.sh [gcc_version] [lib]
+#
+# By I.Hrivnacova, 7/10/2002
+
+CURDIR=`pwd`
+
+# gmake is called from geant4_vmc/source
+cd ../..
+
+if [ "x$1" = "xlib" ]; then
+   GCC_VERS=""
+   MAKELIB="geant4_vmc/lib"
+elif [ "x$2" = "xlib" ]; then
+   GCC_VERS=$1
+   MAKELIB="geant4_vmc/lib"
+else
+   GCC_VERS=$1
+fi
+VERSION=`cat geant4_vmc/config/version_number`
+MACHINE=`root-config --arch`
+if [ "x$MAKELIB" = "xgeant4_vmc/lib" ]; then
+   if [ "x$GCC_VERS" = "x" ]; then  
+      TYPE=$MACHINE.
+   else
+      TYPE=$MACHINE.$GCC_VERS.
+   fi
+else   
+  TYPE=""
+fi  
+TARFILE=geant4_vmc.$VERSION.$TYPE"tar"
+
+TAR=`which gtar`
+dum=`echo $TAR | grep "no gtar"`
+stat=$?
+if [ "$TAR" = '' ] || [ $stat = 0 ]; then
+   TAR="tar cvf"
+   rm -f $TARFILE.gz
+   EXCLUDE=
+else 
+   TAR=$TAR" zcvf"
+   rm -f $TARFILE.gz
+   TARFILE=$TARFILE".gz"
+   EXCLUDE="--exclude CVS"
+fi
+
+$TAR $TARFILE $EXCLUDE geant4_vmc/README geant4_vmc/"history" geant4_vmc/config  \
+   geant4_vmc/"include" geant4_vmc/"source" geant4_vmc/examples $MAKELIB
+cd $CURDIR
+
+exit 0
