70258f57e580c0aed9c0f44977206150b52cfffd
Axel Naumann <Axel.Naumann@cern.ch>
2007-11-20 12:09:48 +0000

    Add more nastiness: * CINT fails to parse functions that take a value as default parameter if that value is not eyt defined. * Make sure default arguments being function calls are only evaluated when the evaulation is needed, and exactly once * ensure that lookup and storage of statics used in default parameter evaluation works
    And be a bit more verbose in diff results (each function prints whether it's interpreted or compiled)
    These tests now fail completely (even to parse, several lookup problems) for interpreted functions.
    
    
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1192 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/function/Params.h
+++ cint/function/Params.h
@@ -1,13 +1,28 @@
+int gCountingCalls = 0;
+
 class Base {
  public:
-   Base(){};
+   Base(const char* mode = ""): fMode(mode){};
    virtual ~Base(){};
-   virtual int FunctionX(int b=0, int c=5) {printf("Base::FunctionX(int b=%d, int c=%d)\n", b, c);}
+   virtual void FunctionX(int b=0, int c=5) {printf("%s: Base::FunctionX(int b=%d, int c=%d)\n", fMode, b, c);}
+
+   void CINTCannotHandleDefaultArgThatsNotYetParsed(int a = GetCountingCalls()) {};
+   static int GetCountingCalls() {return ++fgCountingCalls;}
+   virtual void FunctionY(int arg0 = ++gCountingCalls, float arg1 = Base::GetCountingCalls() ) {
+      printf("%s: Base::FunctionY(int arg0=%d, float arg1=%d.)\n", fMode, arg0, (int)arg1);}
+
+
+   const char* fMode;
+   static int fgCountingCalls;
 };
+
+int Base::fgCountingCalls = 0;
 
 class Derived: public Base {
  public:
-   Derived(){};
+   Derived(const char* mode = ""): Base(mode){};
    virtual ~Derived(){};
-   virtual int FunctionX(int b=1, int c=6) {printf("Derived::FunctionX(int b=%d, int c=%d)\n", b, c);}
+   virtual void FunctionX(int b=1, int c=6) {printf("%s: Derived::FunctionX(int b=%d, int c=%d)\n", fMode, b, c);}
+   virtual void FunctionY(int arg0 = ++gCountingCalls, float arg1 = Base::GetCountingCalls() ) {
+      printf("%s: Derived::FunctionY(int arg0=%d, float arg1=%d.)\n", fMode, arg0, (int)arg1);}
 };
--- cint/function/rundefaultParams.C
+++ cint/function/rundefaultParams.C
@@ -2,13 +2,13 @@
   // check interpreted funcs (default params)
   cout << "Interpeted code:" << endl;
   gROOT->ProcessLine(".L Params.h");
-  gROOT->ProcessLine(".x testDefaultParams.C");
+  gROOT->ProcessLine(".x testDefaultParams.C(\"interpreted\")");
   gROOT->ProcessLine(".U Params.h");
 
   // check compiled funcs (default params)
   cout << endl;
   cout << "Compiled code:" << endl;
   gROOT->ProcessLine(".L Params.h+");
-  gROOT->ProcessLine(".x testDefaultParams.C");
+  gROOT->ProcessLine(".x testDefaultParams.C(\"compiled\")");
   return 0;
 }
