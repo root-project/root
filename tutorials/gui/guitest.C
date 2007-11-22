e1372b556604362bd146a6fcb840e3af6323f527
Ilka Antcheva <Ilka.Antcheva@cern.ch>
2007-11-22 13:47:28 +0000

    Do not reuse the same cascaded menu on different level.
    git-svn-id: http://root.cern.ch/svn/root/trunk@20981 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/gui/guitest.C
+++ tutorials/gui/guitest.C
@@ -632,7 +632,6 @@ TestMainFrame::TestMainFrame(const TGWindow *p, UInt_t w, UInt_t h)
    fCascadeMenu->AddEntry("ID = 5&3", 53);
    fCascadeMenu->AddSeparator();
    fCascadeMenu->AddPopup("&Cascaded 1", fCascade1Menu);
-   fCascadeMenu->AddPopup("C&ascaded 2", fCascade2Menu);
 
    fMenuTest = new TGPopupMenu(gClient->GetRoot());
    fMenuTest->AddLabel("Test different features...");
