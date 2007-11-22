008be906501537f1686e6272e01d5bdb0e0426b6
Wim Lavrijsen <WLavrijsen@lbl.gov>
2007-11-22 00:29:24 +0000

    test for PyROOT API access
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1197 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ python/cint/runPyAPITest.C
@@ -0,0 +1,16 @@
+{
+   int eil = gErrorIgnoreLevel;
+   gErrorIgnoreLevel = 3000;
+   gSystem->Load( "libPyROOT" );
+   gErrorIgnoreLevel = eil;
+
+   TObject* o = new TObject;
+   void* a = TPython::ObjectProxy_FromVoidPtr( o, "TObject" );
+   printf( "OBC:  should be true:  %d\n", TPython::ObjectProxy_Check( a ) );
+   printf( "OBCE: should be false: %d\n", TPython::ObjectProxy_CheckExact( a ) );
+
+   Bool_t match = ( o == TPython::ObjectProxy_AsVoidPtr( a ) );
+   printf( "VPC:  should be true:  %d\n", match );
+
+   return 0;
+}
