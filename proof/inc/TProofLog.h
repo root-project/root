51c500025ec2f051b74125a3afeaccc985773115
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2007-11-22 14:47:31 +0000

    Add getters for the list of log elements and the element TMacro
    git-svn-id: http://root.cern.ch/svn/root/trunk@20987 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/inc/TProofLog.h
+++ proof/inc/TProofLog.h
@@ -53,13 +53,14 @@ public:
    virtual ~TProofLog();
 
    void Display(const char *ord = "*", Int_t from = -10, Int_t to = -1);
+   TList *GetListOfLogs() const { return fElem; }
+   Int_t Grep(const char *txt, Int_t from = 0);
    void Print(Option_t *opt = 0) const;
    void Prt(const char *what);
    Int_t Retrieve(const char *ord = "*",
                   TProofLog::ERetrieveOpt opt = TProofLog::kTrailing,
                   const char *fname = 0, const char *pattern = 0);
    Int_t Save(const char *ord = "*", const char *fname = 0);
-   Int_t Grep(const char *txt, Int_t from = 0);
 
    // Where to log
    void SetLogToBox(Bool_t lgbox = kFALSE) { SetBit(kLogToBox, lgbox); }
@@ -88,11 +89,12 @@ public:
    virtual ~TProofLogElem();
 
    void Display(Int_t from = 0, Int_t to = -1);
+   TMacro *GetMacro() const { return fMacro; }
+   Int_t Grep(const char *txt, TString &res, Int_t from = 0);
    void Print(Option_t *opt = 0) const;
    void Prt(const char *what);
    Int_t Retrieve(TProofLog::ERetrieveOpt opt = TProofLog::kTrailing,
                   const char *pattern = 0);
-   Int_t Grep(const char *txt, TString &res, Int_t from = 0);
 
    static Long64_t GetMaxTransferSize();
    static void     SetMaxTransferSize(Long64_t maxsz);
