d646dbb9cad9be98f49b46f1188c92812e3c6015
Wouter Verkerke <verkerke@nikhef.nl>
2007-11-27 21:16:28 +0000

    o Module.mk
        -   Forgotten last commit
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@21083 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofitcore/Module.mk
+++ roofitcore/Module.mk
@@ -29,8 +29,8 @@ ROOFITCOREDH   := $(ROOFITCOREDS:.cxx=.h)
 ROOFITCOREH1   := Roo1DTable.h RooAbsArg.h RooAbsBinning.h RooAbsCategory.h \
                   RooAbsCategoryLValue.h RooAbsCollection.h \
                   RooAbsData.h RooAbsFunc.h RooAbsGenContext.h \
-                  RooAbsGoodnessOfFit.h RooAbsHiddenReal.h RooAbsIntegrator.h \
-                  RooAbsLValue.h RooAbsMCStudyModule.h RooAbsOptGoodnessOfFit.h \
+                  RooAbsTestStatistic.h RooAbsHiddenReal.h RooAbsIntegrator.h \
+                  RooAbsLValue.h RooAbsMCStudyModule.h RooAbsOptTestStatistic.h \
                   RooAbsPdf.h RooAbsProxy.h RooAbsReal.h \
                   RooAbsRealLValue.h RooAbsRootFinder.h RooAbsString.h \
                   RooAcceptReject.h RooAdaptiveGaussKronrodIntegrator1D.h \
@@ -48,10 +48,9 @@ ROOFITCOREH1   := Roo1DTable.h RooAbsArg.h RooAbsBinning.h RooAbsCategory.h \
 ROOFITCOREH2   := RooDouble.h RooEffGenContext.h RooEllipse.h RooErrorHandler.h \
                   RooErrorVar.h RooFit.h RooFitResult.h RooFormula.h \
                   RooFormulaVar.h RooGaussKronrodIntegrator1D.h RooGenCategory.h \
-                  RooGenContext.h RooGenericPdf.h RooGenProdProj.h \
-                  RooGlobalFunc.h RooGraphEdge.h RooGraphNode.h RooGraphSpring.h \
+                  RooGenContext.h RooGenericPdf.h RooGenProdProj.h RooGlobalFunc.h  \
                   RooGrid.h RooHashTable.h RooHistError.h \
-                  RooHist.h RooHtml.h RooImproperIntegrator1D.h \
+                  RooHist.h RooImproperIntegrator1D.h \
                   RooIntegrator1D.h RooIntegrator2D.h RooIntegratorBinding.h \
                   RooInt.h RooInvTransform.h RooLinearVar.h RooLinkedListElem.h \
                   RooLinkedList.h RooLinkedListIter.h RooLinTransBinning.h RooList.h \
