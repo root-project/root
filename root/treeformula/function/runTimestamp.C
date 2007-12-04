0d028a72e4aca45c550ed45e9c49201a27f6734b
Philippe Canal <pcanal@fnal.gov>
2007-12-04 16:01:00 +0000

    extend test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1216 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/function/runTimestamp.C
+++ root/treeformula/function/runTimestamp.C
@@ -3,8 +3,11 @@
   TTimeStamp* ts = new TTimeStamp;
   TTree* tree = new TTree("EventShape","Event shape global variables");
   tree->Branch("time", "TTimeStamp", &ts, 32000, 0);
+  tree->Branch("timesplit", "TTimeStamp", &ts, 32000, 9);
   tree->Fill();
   EventShape->Scan("time.AsDouble()");
   EventShape->Scan("time");
+  EventShape->Scan("timesplit.AsDouble()");
+  EventShape->Scan("timesplit");
   return 0;
 }
