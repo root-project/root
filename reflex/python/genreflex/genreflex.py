9180b56700e815f8e5f03e9607a3fac320579a65
Axel Naumann <Axel.Naumann@cern.ch>
2007-12-10 16:36:31 +0000

    Next try to find a solution for option quotes to work on both windows cmd.exe and bash, after python has mangled them. The problem only exists because -DA="two words" needs to be supported - and the quotes need to be kept! Solution: if '=' is followed by '"' and the option ends on '"' then escape the double quotes.   genreflex ... "-DSTRING=\"a string\"" works now on both windows and linux. Also print the gccxml invocation command if debug is set.
    git-svn-id: http://root.cern.ch/svn/root/trunk@21302 27541ba8-7e3a-0410-8455-c3a389f83636

--- reflex/python/genreflex/genreflex.py
+++ reflex/python/genreflex/genreflex.py
@@ -179,7 +179,11 @@ class genreflex:
       if o in ('--rootmap-lib',):
         self.rootmaplib = a
       if o in ('-I', '-U', '-D', '-P', '-C') :
-        self.cppopt += '\'' + o + a + '\' '
+        # escape quotes; we need to use " because of windows cmd
+        poseq = a.find('=')
+        if poseq > 0 and a[poseq + 1] == '"' and a[-1] == '"' :
+          a = a[0:poseq + 1] + '\\' + a[poseq+1:-1] + '\\"'
+        self.cppopt += '"' + o + a + '" '
 #----------------------------------------------------------------------------------
   def check_files_dirs(self):
     #---Check existance of input files--------------------
@@ -290,6 +294,7 @@ class genreflex:
         dicfile = os.path.join(self.outputDir,name+file_extension)
       #---------------Parse the header file with GCC_XML
       cmd  = '%s %s %s -fxml=%s %s -D__REFLEX__' %(self.gccxml, self.gccxmlopt, source, xmlfile, self.cppopt)
+      if 'debug' in self.opts : print '--->> genreflex: INFO: invoking ', cmd
       if not self.quiet : print '--->> genreflex: INFO: Parsing file %s with GCC_XML' % source,
       status = os.system(cmd)
       if status :
