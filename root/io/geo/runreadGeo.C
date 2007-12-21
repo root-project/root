c2ad711d22d4bf91d4e7f4e9b2c972bdb97e2157
Rene Brun <Rene.Brun@cern.ch>
2007-12-21 10:36:28 +0000

    From Andrei: New version of the geometries tar file with the new voxelization and a new reference file.
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1249 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/geo/runreadGeo.C
+++ root/io/geo/runreadGeo.C
@@ -1,8 +1,9 @@
 {
   TGeoManager::Import("aleph.root");
-  TGeoManager::Import("ams.root");
   TGeoManager::Import("alice.root");
+  TGeoManager::Import("ams.root");
   TGeoManager::Import("atlas.root");
+  TGeoManager::Import("babar.root");
   TGeoManager::Import("brahms.root"); 
   TGeoManager::Import("btev.root"); 
   TGeoManager::Import("cdf.root");
@@ -10,12 +11,10 @@
   TGeoManager::Import("d0.root");
   TGeoManager::Import("e907.root"); 
   TGeoManager::Import("gem.root"); 
-  if (0) TGeoManager::Import("hades.root");
+  TGeoManager::Import("hades.root");
   TGeoManager::Import("lhcbfull.root");
-  if (0) TGeoManager::Import("lhcbnobool.root");
-  if (0) TGeoManager::Import("phenix.root"); 
+  TGeoManager::Import("phenix.root"); 
   TGeoManager::Import("phobos.root"); 
+  TGeoManager::Import("star.root");
   TGeoManager::Import("tesla.root");
-  p=TGeoManager::Import("star.root");
-  (p==0);
 }
