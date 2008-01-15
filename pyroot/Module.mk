f91e38296627b4dede34f7bbf60ba31d0397487c
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-01-15 16:54:17 +0000

    From Axel: patch allowing to use Python >= 2.5 on Windows (sic). For the record:   from http://docs.python.org/whatsnew/ports.html:   .dll is no longer supported as a filename extension   for extension modules. .pyd is now the only filename   extension that will be searched for. (no comments) Thanks to Wim for pointing out this.
    git-svn-id: http://root.cern.ch/svn/root/trunk@21716 27541ba8-7e3a-0410-8455-c3a389f83636

--- pyroot/Module.mk
+++ pyroot/Module.mk
@@ -24,6 +24,9 @@ PYROOTO      := $(PYROOTS:.cxx=.o)
 PYROOTDEP    := $(PYROOTO:.o=.d) $(PYROOTDO:.o=.d)
 
 PYROOTLIB    := $(LPATH)/libPyROOT.$(SOEXT)
+ifeq ($(ARCH),win32)
+PYROOTPYD    := bin/$(notdir $(PYROOTLIB:.$(SOEXT)=.pyd))
+endif
 PYROOTMAP    := $(PYROOTLIB:.$(SOEXT)=.rootmap)
 
 ROOTPYS      := $(wildcard $(MODDIR)/*.py)
@@ -60,6 +63,9 @@ $(PYROOTLIB):   $(PYROOTO) $(PYROOTDO) $(ROOTPY) $(ROOTPYC) $(ROOTPYO) \
 		  "$(ROOTULIBS) $(RPATH) $(ROOTLIBS) \
 		   $(PYTHONLIBDIR) $(PYTHONLIB) \
 		   $(OSTHREADLIBDIR) $(OSTHREADLIB)" "$(PYTHONLIBFLAGS)"
+ifeq ($(ARCH),win32)
+		cp -f bin/$(notdir $@) $(PYROOTPYD)
+endif
 
 $(PYROOTDS):    $(PYROOTH) $(PYROOTL) $(ROOTCINTTMPEXE)
 		@echo "Generating dictionary $@..."
@@ -78,7 +84,8 @@ clean::         clean-pyroot
 
 distclean-pyroot: clean-pyroot
 		@rm -f $(PYROOTDEP) $(PYROOTDS) $(PYROOTDH) $(PYROOTLIB) \
-		   $(ROOTPY) $(ROOTPYC) $(ROOTPYO) $(PYROOTMAP)
+		   $(ROOTPY) $(ROOTPYC) $(ROOTPYO) $(PYROOTMAP) \
+		   $(PYROOTPYD)
 
 distclean::     distclean-pyroot
 
