e66225c1989c79fa07722b9f25feecabb5f2c731
Philippe Canal <pcanal@fnal.gov>
2008-01-21 03:34:49 +0000

    add hostname, cpu model name and cpu mhz to the roottest timing file
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1297 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/recordtiming.cc
+++ scripts/recordtiming.cc
@@ -10,34 +10,62 @@
 #include "TTree.h"
 #include <stdio.h>
 #include "TDatime.h"
+#include "TEnv.h"
+#include "TSystem.h"
+#include "TBranchElement.h"
+#include "TStreamerInfo.h"
 
 class TTestResult {
 public:
    TTestResult() : fRunId(0),fTestId(0), fDuration(0) {}
-   TTestResult(UInt_t runid, UInt_t testid, const char *test, double duration) : fRunId(runid), fTestId(testid), fTestName(test),fDuration(duration) {
-      // set time stamp
+   TTestResult(UInt_t runid, UInt_t testid, const char *test, double duration, const char *model, Double_t mhz) : 
+        fRunId(runid), 
+        fTestId(testid), 
+        fTestName(test),
+        fDuration(duration),
+        fHostModel(model),
+        fHostMhz(mhz) {
+      fHostName = gSystem->HostName();
    }
    TDatime    fDate;     //||
    UInt_t     fRunId;
    UInt_t     fTestId;
    TString    fTestName;
    Double32_t fDuration;
+   TString    fHostName;
+   TString    fHostModel;
+   Double32_t fHostMhz;
+
+   ClassDef(TTestResult,3);
 };
 
 void recordtiming(const char *roottesthome, UInt_t runid, UInt_t testid, const char *testdir, double duration) 
 {
    TString stestdir( testdir );
    stestdir.Remove(0,strlen(roottesthome));
+
    TString logfile( roottesthome );
    logfile.Append( '/' );
    logfile.Append( "roottesttiming.root" );
    
    // fprintf(stderr,"%s %g %s\n",stestdir.Data(), duration,logfile.Data() );
+   TEnv env(Form("%s/roottest.arch",roottesthome));
+   TString modelname = env.GetValue("modelname","");
+   double mhz = env.GetValue("cpuMHz",0.00);
+   
+   TTestResult *res = new TTestResult(runid,testid,stestdir,duration,modelname,mhz);
 
    TFile *f = TFile::Open(logfile,"UPDATE");
    TTree *t; f->GetObject("timing",t);
-   TTestResult *res = new TTestResult(runid,testid,stestdir,duration);
-
+   if (t!=0) {
+      TBranchElement *b = (TBranchElement*)t->GetBranch("test");
+      TStreamerInfo *info = b->GetInfo();
+      if (info->GetClassVersion() < 3) {
+        t->Write(Form("timing_v%d",info->GetClassVersion()));
+        delete t;
+        t = 0;
+      }
+   }
    if (t==0) {
       t = new TTree("timing","roottest timing");
       t->Branch("test",&res);
