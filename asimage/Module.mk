4d0ba5938c81a5b11a82cc82f9ac1c0b7f530d7d
Olivier Couet <Olivier.Couet@cern.ch>
2008-02-15 09:16:54 +0000

    - Mods to avoid generate many printouts from libAfterImage in case ROOTBUILD=debug
    git-svn-id: http://root.cern.ch/svn/root/trunk@22154 27541ba8-7e3a-0410-8455-c3a389f83636

--- asimage/Module.mk
+++ asimage/Module.mk
@@ -40,11 +40,12 @@ ifeq ($(MACOSX_MINOR),3)
 ASEXTRALIB   += -lz
 endif
 ASTEPDEP     := $(ASTEPLIB)
-ifeq (debug,$(findstring debug,$(ROOTBUILD)))
-ASTEPDBG      = "--enable-gdb"
-else
+##### To trigger the debug printouts for libafterimage when ROOTBUILD=debug
+##### ifeq (debug,$(findstring debug,$(ROOTBUILD)))
+##### ASTEPDBG      = "--enable-gdb"
+##### else
 ASTEPDBG      =
-endif
+##### endif
 else
 ASTEPLIBA    := $(ASLIBDIR) $(ASLIB)
 ASTEPLIB     := $(ASLIBDIR) $(ASLIB)
