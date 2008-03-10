90f3f63988bd2f6c062ac57ea82654273357ad33
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-03-10 16:09:27 +0000

    tag version v5-18-00b.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-18-00-patches@22573 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "5.18/00a"
-#define ROOT_RELEASE_DATE "Feb 20 2008"
-#define ROOT_RELEASE_TIME "11:13:41"
-#define ROOT_SVN_REVISION 22235
+#define ROOT_RELEASE "5.18/00b"
+#define ROOT_RELEASE_DATE "Mar 10 2008"
+#define ROOT_RELEASE_TIME "16:11:59"
+#define ROOT_SVN_REVISION 22563
 #define ROOT_SVN_BRANCH "branches/v5-18-00-patches"
 #define ROOT_VERSION_CODE 332288
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
