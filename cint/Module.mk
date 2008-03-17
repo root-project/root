0d5d249e5968a5dfb650a00f629b20850ca8ac0a
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-03-17 09:09:38 +0000

    use gcc4strm.cxx when using icc 10.1 and bigger. These compilers require gcc 4.
    git-svn-id: http://root.cern.ch/svn/root/trunk@22677 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/Module.mk
+++ cint/Module.mk
@@ -155,10 +155,14 @@ endif
 ifeq ($(CXXCMD),icc)
 CINTS2       := $(filter-out $(MODDIRS)/libstrm.%,$(CINTS2))
 CINTS2       := $(filter-out $(MODDIRS)/longif.%,$(CINTS2))
+ifneq ($(ICC_GE_101),)
+CINTS2       += $(MODDIRS)/gcc4strm.cxx
+else
 ifneq ($(ICC_GE_9),)
 CINTS2       += $(MODDIRS)/gcc3strm.cxx
 else
 CINTS2       += $(MODDIRS)/iccstrm.cxx
+endif
 endif
 CINTS2       += $(MODDIRS)/longif3.cxx
 endif
