f3247a48bf73cbe6b2237dd52d31dc6ff7d5e100
Philippe Canal <pcanal@fnal.gov>
2008-03-20 13:41:38 +0000

    add svn:ignore(s)
    git-svn-id: http://root.cern.ch/svn/root/trunk@22777 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cint/include/limits.h
+++ cint/cint/include/limits.h
@@ -5,14 +5,14 @@
 #define 	CHAR_MIN (-128)
 #define 	INT_MAX (2147483647)
 #define 	INT_MIN (-2147483648)
-#define 	LONG_MAX (2147483647)
-#define 	LONG_MIN (-2147483648)
+#define 	LONG_MAX (9223372036854775807)
+#define 	LONG_MIN (-9223372036854775808)
 #define 	SCHAR_MAX (127)
 #define 	SCHAR_MIN (-128)
 #define 	SHRT_MAX (32767)
 #define 	SHRT_MIN (-32768)
 #define 	UCHAR_MAX (255U)
 const unsigned int  	UINT_MAX =(4294967295U);
-const unsigned long 	ULONG_MAX =(4294967295U);
+const unsigned long 	ULONG_MAX =(18446744073709551615U);
 #define 	USHRT_MAX (65535U)
 #endif
