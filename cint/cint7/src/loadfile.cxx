0359b58c561732d32f59b5bccecfa2b2e7544989
Philippe Canal <pcanal@fnal.gov>
2008-04-08 21:37:30 +0000

    fix the value return for G__getcintsysdir in the ROOTBUILD (it needs to return /build1/pcanal/rootcint7/cint, the remainder of G__loadfile will add the core version sub directory name
    git-svn-id: http://root.cern.ch/svn/root/trunk@23056 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cint7/src/loadfile.cxx
+++ cint/cint7/src/loadfile.cxx
@@ -452,7 +452,7 @@ int Cint::Internal::G__getcintsysdir()
       sprintf(G__cintsysdir, "%s", CINTINCDIR);
 #  else
       if(G__UseCINTSYSDIR) strcpy(G__cintsysdir,env);
-      else                 sprintf(G__cintsysdir, "%s/%s", env, G__CFG_COREVERSION);
+      else                 sprintf(G__cintsysdir, "%s/%s", env, "cint");
 #  endif
 # endif /* ROOTBUILD */
 
