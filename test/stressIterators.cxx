6e2ff1358b47c4fe060c87df211de89eed2ad8f7
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-04-18 23:42:15 +0000

    From Anar: - the SFind functor has been simplified and now become a fully copyable functor - added  tests for  the following  cases:    * TMap with std::for_each (Full iteration: from the Begin up to the End)    * TMap with std::for_each (Partial iteration: from the Begin up to the      3rd element)    * TMap with std::find_if    * TMap with std::count_if - cosmetic changes
    git-svn-id: http://root.cern.ch/svn/root/trunk@23329 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressIterators.cxx
+++ test/stressIterators.cxx
@@ -4,12 +4,16 @@
 //----------------------------------------------------------------
 // This is a tests ROOT Iterators and STL algorithms.
 // The test project covers the following cases:
-// 1 - TList with std::for_each
-// 2 - TList with std::find_if
-// 3 - TList with std::count_if
-// 4 - TObjArray with std::for_each
-// 5 - TObjArray with std::find_if
-// 6 - TObjArray with std::count_if
+// 1  - TList with std::for_each (Full iteration: from the Begin up to the End)
+// 2  - TList with std::find_if
+// 3  - TList with std::count_if
+// 4  - TObjArray with std::for_each (Full iteration: from the Begin up to the End)
+// 5  - TObjArray with std::find_if
+// 6  - TObjArray with std::count_if
+// 7  - TMap with std::for_each (Full iteration: from the Begin up to the End)
+// 8  - TMap with std::for_each (Partial iteration: from the Begin up to the 3rd element)
+// 9  - TMap with std::find_if
+// 10 - TMap with std::count_if
 
 
 
@@ -22,104 +26,21 @@
 #include "TList.h"
 #include "TObjString.h"
 #include "TObjArray.h"
+#include "TMap.h"
+// Local
+#include "stressIterators.h"
 
 using namespace std;
-
-static Int_t gCount = 0;
-
-//______________________________________________________________________________
-struct SEnumFunctor {
-   bool operator()(TObject *aObj) throw(exception) {     
-      if (!aObj)
-         throw invalid_argument("SEnumFunctor: aObj is a NULL pointer");
-
-      TObjString *str(dynamic_cast<TObjString*>(aObj));
-      if (!str)
-         throw runtime_error("SEnumFunctor: Container's element is not a TObjString object.");
-
-      ++gCount;
-      cout << str->String().Data() << endl;
-      return true;
-   }
-};
-
-//______________________________________________________________________________
-struct SFind {
-   SFind(const TString &aStr): fToFind(aStr) {
-   }
-   bool operator()(TObject *aObj) {
-      TObjString *str(dynamic_cast<TObjString*>(aObj));
-      return !str->String().CompareTo(fToFind);
-   }
-private:
-   const TString fToFind;
-};
-
-//______________________________________________________________________________
-// Checking TList with for_each algorithm
-template<class T>
-void TestContainer_for_each(const T &container, Int_t aSize) throw(exception)
-{
-   gCount = 0; // TODO: using gCount is a very bad method. Needs to be revised.
-
-   TIter iter(&container);
-   for_each(iter.Begin(), TIter::End(), SEnumFunctor());
-   if (aSize != gCount)
-      throw runtime_error("Test case <TestList_for_each> has failed.");
-   cout << "->> Ok." << endl;
-}
-
-//______________________________________________________________________________
-// Checking a ROOT container with find_if algorithm
-template<class T>
-void TestContainer_find_if(const T &container, const TString &aToFind) throw(exception)
-{
-   typedef TIterCategory<T> iterator_t;
-
-   SFind func(aToFind);
-   iterator_t iter(&container);
-   iterator_t found(
-      find_if(iter.Begin(), iterator_t::End(), func)
-   );
-   if (!(*found))
-      throw runtime_error("Test case <TestContainer_find_if> has failed.");
-
-   TObjString *str(dynamic_cast<TObjString*>(*found));
-   if (!str)
-      throw runtime_error("Test case <TestContainer_find_if> has failed.");
-
-   std::cout << "I found: " << str->String().Data() << std::endl;
-   cout << "->> Ok." << endl;
-}
-
-//______________________________________________________________________________
-// Checking a ROOT container with count_if algorithm
-template<class T>
-void TestContainer_count_if(const T &container, const TString &aToFind) throw(exception)
-{
-   typedef TIterCategory<T> iterator_t;
-
-   SFind func(aToFind);
-   iterator_t iter(&container);
-   typename iterator_t::difference_type cnt(
-      count_if(iter.Begin(), iterator_t::End(), func)
-   );
-
-   if (1 != cnt)
-      throw runtime_error("Test case <TestContainer_count_if> has failed.");
-
-   cout << "->> Ok." << endl;
-}
 
 //______________________________________________________________________________
 void stressIterators() throw(exception)
 {
    const Int_t size = 15;
-   
+
    ostringstream ss;
 
    // TList
-   TList list;   
+   TList list;
    for (int i = 0; i < size; ++i) {
       ss << "test string #" << i;
       TObjString *s(new TObjString(ss.str().c_str()));
@@ -127,17 +48,18 @@ void stressIterators() throw(exception)
       ss.str("");
    }
 
-   cout << "====================================" << endl;
+   cout << "#1 ====================================" << endl;
    cout << "-----> " << "TestContainer_for_each<TList>(list, list.GetSize())" << endl;
    TestContainer_for_each<TList>(list, list.GetSize());
 
-   cout << "====================================" << endl;
+   cout << "\n#2 ====================================" << endl;
    cout << "-----> " << "TestContainer_find_if<TList>(list, \"test string #3\")" << endl;
    TestContainer_find_if<TList>(list, "test string #3");
 
-   cout << "====================================" << endl;
-   cout << "-----> " << "TestContainer_count_if<TList>(list, \"test string #3\")" << endl;
-   TestContainer_count_if<TList>(list, "test string #3");
+   cout << "\n#3 ====================================" << endl;
+   cout << "-----> " << "TestContainer_count_if<TList>(list, \"test string #3\", 1)" << endl;
+   // we suppose to find exactly one match
+   TestContainer_count_if<TList>(list, "test string #3", 1);
 
 
    // TObjArray
@@ -149,17 +71,41 @@ void stressIterators() throw(exception)
       ss.str("");
    }
 
-   cout << "====================================" << endl;
+   cout << "\n#4 ====================================" << endl;
    cout << "-----> " << "TestContainer_for_each<TObjArray>(obj_array, obj_array.GetSize())" << endl;
-   TestContainer_for_each<TObjArray>(obj_array, obj_array.GetEntriesFast());
-   
-   cout << "====================================" << endl;
+   TestContainer_for_each<TObjArray>(obj_array, obj_array.GetSize());
+
+   cout << "\n#5 ====================================" << endl;
    cout << "-----> " << "TestContainer_find_if<TObjArray>(obj_array, \"test string #3\")" << endl;
    TestContainer_find_if<TObjArray>(obj_array, "test string #3");
 
-   cout << "====================================" << endl;
-   cout << "-----> " << "TestContainer_count_if<TObjArray>(obj_array, \"test string #3\")" << endl;
-   TestContainer_count_if<TObjArray>(obj_array, "test string #3");
+   cout << "\n#6 ====================================" << endl;
+   cout << "-----> " << "TestContainer_count_if<TObjArray>(obj_array, \"test string #3\", 1)" << endl;
+   // we suppose to find exactly one match
+   TestContainer_count_if<TObjArray>(obj_array, "test string #3", 1);
+
+   // TMap
+   const char * const cszValue("value");
+   TMap map_container(size);
+   for (int i = 0; i < size; ++i) {
+      ss << "test string #" << i;
+      TObjString *s(new TObjString(ss.str().c_str()));
+      map_container.Add(s, new TObjString(cszValue));
+      ss.str("");
+   }
+
+   cout << "\n#7 ====================================" << endl;
+   cout << "-----> " << "TestContainer_for_each<TMap>(map_container, map_container.GetSize())" << endl;
+   TestContainer_for_each<TMap>(map_container, map_container.GetSize());
+   cout << "\n#8 ====================================" << endl;
+   cout << "-----> " << "TestContainer_for_each2<TMap>(map_container)" << endl;
+   TestContainer_for_each2<TMap>(map_container);
+   cout << "\n#9 ====================================" << endl;
+   cout << "-----> " << "TestContainer_find_if<TMap>(map_container, cszValue)" << endl;
+   TestContainer_find_if<TMap>(map_container, cszValue);
+   cout << "\n#10 ====================================" << endl;
+   cout << "-----> " << "TestContainer_count_if<TMap>(map_container, cszValue, map_container.GetSize());" << endl;
+   TestContainer_count_if<TMap>(map_container, cszValue, map_container.GetSize());
 }
 
 //______________________________________________________________________________
