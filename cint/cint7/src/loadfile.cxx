6d68b6977e1a5ecf87a7c0cd06ce3fd22a397aaf
Philippe Canal <pcanal@fnal.gov>
2008-04-19 09:26:54 +0000

    No need to warn about multiple request for cintdlls inclusion, just ignore the new requests
    git-svn-id: http://root.cern.ch/svn/root/trunk@23331 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cint7/src/loadfile.cxx
+++ cint/cint7/src/loadfile.cxx
@@ -1325,7 +1325,26 @@ extern "C" int G__loadfile(const char *filenamein)
        ){
       if(G__prerun==0 || G__debugtrace)
         if(G__dispmsg>=G__DISPNOTE) {
-          G__fprinterr(G__serr,"Note: File \"%s\" already loaded\n",filename);
+            static const char *excludelist [] = {
+                "stdfunc.dll","stdcxxfunc.dll","posix.dll","ipc.dll","posix.dll"
+                "string.dll","vector.dll","vectorbool.dll","list.dll","deque.dll",
+                "map.dll", "map2.dll","set.dll","multimap.dll","multimap2.dll",
+                "multiset.dll","stack.dll","queue.dll","valarray.dll",
+                "exception.dll","stdexcept.dll","complex.dll","climits.dll" };
+            static const unsigned int excludelistsize = sizeof(excludelist)/sizeof(excludelist[0]);
+            static int excludelen[excludelistsize] = {-1};
+            if (excludelen[0] == -1) {
+               for (unsigned int i = 0; i < excludelistsize; ++i)
+                  excludelen[i] = strlen(excludelist[i]);
+            }
+            bool cintdlls = false;
+            int len = strlen(filename);
+            for (unsigned int i = 0; !cintdlls && i < excludelistsize; ++i) {
+               if (len>excludelen[i]) {
+                  cintdlls = (!strncmp(filename+len-excludelen[i], excludelist[i], excludelen[i]));
+               }
+            }
+            if (!cintdlls) G__fprinterr(G__serr,"Note: File \"%s\" already loaded\n",filename);
         }
       /******************************************************
        * restore input file information to G__ifile
