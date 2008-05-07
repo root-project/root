e2893952fe0d4eae7dc624719181e330b84a7fbd
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-05-07 14:16:23 +0000

    From Wim: Do not remove DISPLAY from environ; this may lead to some warnings, but those can be fixed by the user, whereas that is not the case for setting DISPLAY back.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-18-00-patches@23680 27541ba8-7e3a-0410-8455-c3a389f83636

--- pyroot/ROOT.py
+++ pyroot/ROOT.py
@@ -2,7 +2,7 @@ from __future__ import generators
 # @(#)root/pyroot:$Id$
 # Author: Wim Lavrijsen (WLavrijsen@lbl.gov)
 # Created: 02/20/03
-# Last: 07/02/07
+# Last: 05/06/08
 
 """PyROOT user module.
 
@@ -66,10 +66,6 @@ try:
 except:
  # module readline typically doesn't exist on non-Unix platforms
    pass
-
-## remove DISPLAY variable in batch mode as to not confuse early ROOT calls
-if hasattr(sys,'argv') and '-b' in sys.argv and os.environ.has_key( 'DISPLAY' ):
-   del os.environ[ 'DISPLAY' ]       
 
 ## special filter on MacOS X (warnings caused by linking that is still required)
 if sys.platform == 'darwin':
