083245a501d93970a036aac1ddbdefa503de0cfc
Rene Brun <Rene.Brun@cern.ch>
2008-05-14 07:26:02 +0000

    Implement changes required by the new directory structure
    git-svn-id: http://root.cern.ch/svn/root/trunk@23835 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makeversion.sh
+++ build/unix/makeversion.sh
@@ -12,21 +12,21 @@ $CINT $SCRIPT
 
 if test "x`uname | grep -i cygwin`" != "x"; then
     echo 'Need to run "dos2unix base/inc/RVersion.h"'
-    dos2unix base/inc/RVersion.h
+    dos2unix core/base/inc/RVersion.h
 fi
 
 echo "New version is `cat build/version_number`. Updating dependencies..."
 
 # compile all files that were out-of-date prior to makeversion.sh
-make -o base/inc/RVersion.h
+make -o core/base/inc/RVersion.h
 
 # touch all files that don't need recompilation (need to do this 3 times
 # to walk through chain of dependencies)
 make -s -t; make -s -t; make -s -t
 
-# recompile only base/src/TROOT.cxx
-touch base/src/TROOT.cxx
-touch base/inc/TVersionCheck.h
+# recompile only core/base/src/TROOT.cxx
+touch core/base/src/TROOT.cxx
+touch core/base/inc/TVersionCheck.h
 touch rootx/src/rootxx.cxx
 make
 
