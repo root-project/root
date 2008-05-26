4eb2ec3fefbe8ef711fa249c1746d6e517e6bbc4
Rene Brun <Rene.Brun@cern.ch>
2008-05-26 07:25:13 +0000

    From Federico: some efffc++ fixes
    git-svn-id: http://root.cern.ch/svn/root/trunk@24005 27541ba8-7e3a-0410-8455-c3a389f83636

--- net/net/inc/TSocket.h
+++ net/net/inc/TSocket.h
@@ -101,7 +101,8 @@ protected:
 
    TSocket() : fAddress(), fBytesRecv(0), fBytesSent(0), fCompress(0),
       fLocalAddress(), fRemoteProtocol(), fSecContext(0), fService(),
-      fServType(kSOCKD), fSocket(-1), fTcpWindowSize(0), fUrl() { }
+     fServType(kSOCKD), fSocket(-1), fTcpWindowSize(0), fUrl(),
+     fBitsInfo () { }
 
    Bool_t       Authenticate(const char *user);
    void         SetDescriptor(Int_t desc) { fSocket = desc; }
