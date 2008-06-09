10ba721894717caac53b15eab968abe6e5f33193
Wouter Verkerke <verkerke@nikhef.nl>
2008-06-09 10:06:35 +0000

    o RooEffProd
         - Fix bug in calculation of normalization for p.d.f.s where shape depends
           on choice of observables (reported by Tristan)
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@24185 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/roofitcore/inc/RooEffProd.h
+++ roofit/roofitcore/inc/RooEffProd.h
@@ -15,11 +15,12 @@
 #include "RooAbsPdf.h"
 #include "RooAbsReal.h"
 #include "RooRealProxy.h"
+#include "RooObjCacheManager.h"
 
 class RooEffProd: public RooAbsPdf {
 public:
   // Constructors, assignment etc
-  inline RooEffProd() { };
+  inline RooEffProd() : _nset(0) { };
   virtual ~RooEffProd();
   RooEffProd(const char *name, const char *title, RooAbsPdf& pdf, RooAbsReal& efficiency);
   RooEffProd(const RooEffProd& other, const char* name=0);
@@ -28,16 +29,44 @@ public:
 
   virtual RooAbsGenContext* genContext(const RooArgSet &vars, const RooDataSet *prototype,
                                        const RooArgSet* auxProto, Bool_t verbose) const;
+
+  virtual Double_t getVal(const RooArgSet* set=0) const ;
+
+  virtual Bool_t forceAnalyticalInt(const RooAbsArg& /*dep*/) const { return kTRUE ; }
+  Int_t getAnalyticalIntegralWN(RooArgSet& allVars, RooArgSet& numVars, const RooArgSet* normSet, const char* rangeName=0) const ;
+  Double_t analyticalIntegralWN(Int_t code, const RooArgSet* normSet, const char* rangeName=0) const ;
+  
 protected:
+  
+  virtual RooAbsReal* createNormIntegral(const RooArgSet& iset, const RooNumIntConfig& cfg, const char* rangeName=0) const  
+    { return createIntegral(iset,iset,cfg,rangeName) ; } 
+
   const RooAbsPdf* pdf() const { const RooAbsPdf* p = dynamic_cast<const RooAbsPdf*>(&_pdf.arg()); assert(p!=0); return p; }
   const RooAbsReal* eff() const { const RooAbsReal* a = dynamic_cast<const RooAbsReal*>( &_eff.arg()); assert(a!=0); return a;}
 
   // Function evaluation
   virtual Double_t evaluate() const ;
 
+  class CacheElem : public RooAbsCacheElement {
+  public:
+    CacheElem() : _clone(0), _int(0) {} 
+    virtual ~CacheElem() { delete _int ; delete _clone ; }
+    // Payload
+    RooArgSet   _intObs ;
+    RooEffProd* _clone ;
+    RooAbsReal* _int ;    
+    // Cache management functions
+    virtual RooArgList containedArgs(Action) ;
+  } ;
+  mutable RooObjCacheManager _cacheMgr ; // The cache manager
+
+
   // the real stuff...
   RooRealProxy _pdf ;     // pdf
   RooRealProxy _eff;      // efficiency
+  mutable const RooArgSet* _nset  ; //!
+
+  RooArgSet* _fixedNset ; //! do not persist
 
   ClassDef(RooEffProd,1) // Product of PDF with efficiency function with optimized generator context
 };
--- roofit/roofitcore/src/RooEffProd.cxx
+++ roofit/roofitcore/src/RooEffProd.cxx
@@ -21,6 +21,8 @@
 #include "RooFit.h"
 #include "RooEffProd.h"
 #include "RooEffGenContext.h"
+#include "RooNameReg.h"
+#include "RooRealVar.h"
 
 ClassImp(RooEffProd)
   ;
@@ -28,8 +30,11 @@ ClassImp(RooEffProd)
 RooEffProd::RooEffProd(const char *name, const char *title, 
                              RooAbsPdf& inPdf, RooAbsReal& inEff) :
   RooAbsPdf(name,title),
+  _cacheMgr(this,10),
   _pdf("pdf","pre-efficiency pdf", this,inPdf),
-  _eff("eff","efficiency function",this,inEff)
+  _eff("eff","efficiency function",this,inEff),
+  _nset(0),
+  _fixedNset(0)
 {  
 
 }
@@ -37,8 +42,11 @@ RooEffProd::RooEffProd(const char *name, const char *title,
 
 RooEffProd::RooEffProd(const RooEffProd& other, const char* name) : 
   RooAbsPdf(other, name),
+  _cacheMgr(other._cacheMgr,this),
   _pdf("pdf",this,other._pdf),
-  _eff("acc",this,other._eff)
+  _eff("acc",this,other._eff),
+  _nset(0),
+  _fixedNset(0) 
 {
 }
 
@@ -47,9 +55,15 @@ RooEffProd::~RooEffProd()
 {
 }
 
+Double_t RooEffProd::getVal(const RooArgSet* set) const 
+{  
+  _nset = _fixedNset ? _fixedNset : set ;
+  return RooAbsPdf::getVal(set) ;
+}
+
 Double_t RooEffProd::evaluate() const
 {
-    return eff()->getVal() * pdf()->getVal();
+    return eff()->getVal() * pdf()->getVal(_nset);
 }
 
 RooAbsGenContext* RooEffProd::genContext(const RooArgSet &vars, const RooDataSet *prototype,
@@ -58,4 +72,77 @@ RooAbsGenContext* RooEffProd::genContext(const RooArgSet &vars, const RooDataSet
   assert(pdf()!=0);
   assert(eff()!=0);
   return new RooEffGenContext(*this,*pdf(),*eff(),vars,prototype,auxProto,verbose) ;
+}
+
+
+Int_t RooEffProd::getAnalyticalIntegralWN(RooArgSet& allVars, RooArgSet& analVars, 
+					  const RooArgSet* normSet, const char* rangeName) const 
+{
+  // No special handling required if a normalization set is given
+  if (normSet && normSet->getSize()>0) {    
+    return 0 ;
+  }
+  // No special handling required if running with a fixed normalization set
+  if (_fixedNset) {
+    return 0 ;
+  }
+
+  // Special handling of integrals w/o normalization set. We need to pass _a_ normalization set
+  // to pdf().getVal(nset) in evaluate() because for certain p.d.fs the shape depends on the
+  // chosen normalization set. This functions correctly automatically for plain getVal() calls,
+  // however when the (numeric) normalization is calculated, getVal() is called without any normalization
+  // set causing the normalization to be calculated for a possibly different shape. To fix that
+  // integrals over a RooEffProd without normalization setup are explicitly handled here. These integrals
+  // are calculated using a clone of the RooEffProd that has a fixed normalization set passed to the
+  // underlying p.d.f regardless of the normalization set passed to getVal(). Here the set of observables
+  // over which is integrated is passed.
+
+  // Declare that we can analytically integrate all requested observables
+  analVars.add(allVars) ;
+
+  // Check if cache was previously created
+  Int_t sterileIndex(-1) ;
+  CacheElem* cache = (CacheElem*) _cacheMgr.getObj(&allVars,&allVars,&sterileIndex,RooNameReg::ptr(rangeName)) ;
+  if (cache) {
+    return _cacheMgr.lastIndex()+1;
+  }
+
+
+  // Construct cache with clone of p.d.f that has fixed normalization set that is passed to input pdf
+  cache = new CacheElem ;
+  cache->_intObs.addClone(allVars) ;
+  cache->_clone = (RooEffProd*) clone(Form("%s_clone",GetName())) ;
+  cache->_clone->_fixedNset = &cache->_intObs ;
+  cache->_int = cache->_clone->createIntegral(cache->_intObs) ;
+
+  // Store cache and return index as code
+  Int_t code = _cacheMgr.setObj(&allVars,&allVars,(RooAbsCacheElement*)cache,RooNameReg::ptr(rangeName)) ; 
+
+  return code+1 ;
+}
+
+
+
+
+Double_t RooEffProd::analyticalIntegralWN(Int_t code, const RooArgSet* normSet, const char* /*rangeName*/) const 
+{
+  // Return analytical integral defined by given scenario code
+
+  // No integration scenario
+  if (code==0) {
+    return getVal(normSet) ;
+  }
+
+  // Partial integration scenarios
+  CacheElem* cache = (CacheElem*) _cacheMgr.getObjByIndex(code-1) ;
+  return cache->_int->getVal() ;
+}
+
+
+RooArgList RooEffProd::CacheElem::containedArgs(Action) 
+{
+  RooArgList ret(_intObs) ;
+  ret.add(*_int) ;
+  ret.add(*_clone) ;
+  return ret ;
 }
