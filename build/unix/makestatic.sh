b740d09a4879f347df43793ca7aa9a392635562a
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-06-12 16:50:42 +0000

    add vmc to libRoota (needed by ALICE in static aliroot version).
    git-svn-id: http://root.cern.ch/svn/root/trunk@24256 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makestatic.sh
+++ build/unix/makestatic.sh
@@ -24,7 +24,7 @@ excl="main proof/proofd net/rootd net/xrootd rootx montecarlo/pythia6 \
       hist/hbook core/newdelete misc/table core/utils net/srputils \
       net/krb5auth net/globusauth io/chirp io/dcache net/alien \
       graf2d/asimage net/ldap graf2d/qt gui/qtroot math/quadp \
-      bindings/pyroot bindings/ruby montecarlo/vmc \
+      bindings/pyroot bindings/ruby \
       io/xmlparser graf3d/gl graf3d/ftgl roofit/roofit roofit/roofitcore \
       sql/oracle net/netx net/auth net/rpdutils math/mathmore \
       math/minuit2 io/gfal net/monalisa proof/proofx math/fftw gui/qtgsi \
