d6984a43703e99930fcdc188f48d4a4cf3dc9385
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2008-06-20 08:35:47 +0000

    - add instantiation of some extra template methods in Rotation and Transform3D classes.   Unfortunatly explicit constructors cannot be added since arenot supported by CINT. One should then use in   Python or CINT dummy constructor + operator=.
    git-svn-id: http://root.cern.ch/svn/root/trunk@24404 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/genvector/inc/Math/LinkDef_Rotation.h
+++ math/genvector/inc/Math/LinkDef_Rotation.h
@@ -14,7 +14,18 @@
 #pragma link C++ function  ROOT::Math::Rotation3D::operator* (const ROOT::Math::XYZVector &);
 #pragma link C++ function  ROOT::Math::Rotation3D::operator* (const ROOT::Math::XYZTVector &);
 // axis angle
+// this explicit template ctor are not found by CINT
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::Rotation3D &);
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::EulerAngles &);
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::RotationZYX &);
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::RotationX &);
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::RotationY &);
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::RotationZ &);
+// #pragma link C++ function  ROOT::Math::AxisAngle::AxisAngle (const ROOT::Math::Quaternion &);
+
 #pragma link C++ function  ROOT::Math::AxisAngle::operator= (const ROOT::Math::Rotation3D &);
+#pragma link C++ function  ROOT::Math::AxisAngle::operator= (const ROOT::Math::EulerAngles &);
+#pragma link C++ function  ROOT::Math::AxisAngle::operator= (const ROOT::Math::RotationZYX &);
 #pragma link C++ function  ROOT::Math::AxisAngle::operator= (const ROOT::Math::RotationX &);
 #pragma link C++ function  ROOT::Math::AxisAngle::operator= (const ROOT::Math::RotationY &);
 #pragma link C++ function  ROOT::Math::AxisAngle::operator= (const ROOT::Math::RotationZ &);
@@ -27,33 +38,60 @@
 #pragma link C++ function  ROOT::Math::AxisAngle::operator* (const ROOT::Math::XYZVector &);
 #pragma link C++ function  ROOT::Math::AxisAngle::operator* (const ROOT::Math::XYZTVector &);
 // Euler angles 
-#pragma link C++ function  ROOT::Math::EulerAngles::operator= (ROOT::Math::Rotation3D);
-// #pragma link C++ function  ROOT::Math::EulerAngles::operator= (ROOT::Math::RotationX); // not yet dir. impl.
-// #pragma link C++ function  ROOT::Math::EulerAngles::operator= (ROOT::Math::RotationY);
-// #pragma link C++ function  ROOT::Math::EulerAngles::operator= (ROOT::Math::RotationZ);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::Rotation3D &);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::AxisAngle &);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::RotationZYX &);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::RotationX &);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::RotationY &);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::RotationZ &);
+// #pragma link C++ function  ROOT::Math::EulerAngles::EulerAngles (const ROOT::Math::Quaternion &);
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::Rotation3D &)
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::AxisAngle &)
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::Quaternion &)
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::RotationZYX &)
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::RotationX &);
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::RotationY &);
+#pragma link C++ function  ROOT::Math::EulerAngles::operator= (const ROOT::Math::RotationZ &);
 #pragma link C++ function  ROOT::Math::EulerAngles::SetComponents (double *, double *);
 #pragma link C++ function  ROOT::Math::EulerAngles::GetComponents (double *);
 #pragma link C++ function  ROOT::Math::EulerAngles::operator* (const ROOT::Math::XYZPoint &);
 #pragma link C++ function  ROOT::Math::EulerAngles::operator* (const ROOT::Math::XYZVector &);
 #pragma link C++ function  ROOT::Math::EulerAngles::operator* (const ROOT::Math::XYZTVector &);
 // quaternion 
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::Rotation3D);
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::AxisAngle);
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::EulerAngles);
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::RotationZYX);
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::RotationX);
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::RotationY);
-#pragma link C++ function  ROOT::Math::Quaternion::operator= (ROOT::Math::RotationZ);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::Rotation3D &);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::AxisAngle &);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::EulerAngles &);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::RotationZYX &);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::RotationX &);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::RotationY &);
+// #pragma link C++ function  ROOT::Math::Quaternion::Quaternion (const ROOT::Math::RotationZ &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::Rotation3D &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::AxisAngle &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::EulerAngles &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::RotationZYX &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::RotationX &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::RotationY &);
+#pragma link C++ function  ROOT::Math::Quaternion::operator= (const ROOT::Math::RotationZ &);
 #pragma link C++ function  ROOT::Math::Quaternion::SetComponents (double *, double *);
 #pragma link C++ function  ROOT::Math::Quaternion::GetComponents (double *);
 #pragma link C++ function  ROOT::Math::Quaternion::operator* (const ROOT::Math::XYZPoint &);
 #pragma link C++ function  ROOT::Math::Quaternion::operator* (const ROOT::Math::XYZVector &);
 #pragma link C++ function  ROOT::Math::Quaternion::operator* (const ROOT::Math::XYZTVector &);
 // rotation ZYX
-#pragma link C++ function  ROOT::Math::RotationZYX::operator= (ROOT::Math::Rotation3D);
-#pragma link C++ function  ROOT::Math::RotationZYX::operator= (ROOT::Math::RotationX);
-#pragma link C++ function  ROOT::Math::RotationZYX::operator= (ROOT::Math::RotationY);
-#pragma link C++ function  ROOT::Math::RotationZYX::operator= (ROOT::Math::RotationZ);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::Rotation3D &);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::AxisAngle &);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::EulerAngles &);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::Quaternion &);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::RotationX &);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::RotationY &);
+// #pragma link C++ function  ROOT::Math::RotationZYX::RotationZYX (const ROOT::Math::RotationZ &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::Rotation3D &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::AxisAngle &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::EulerAngles &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::Quaternion &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::RotationX &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::RotationY &);
+#pragma link C++ function  ROOT::Math::RotationZYX::operator= (const ROOT::Math::RotationZ &);
 #pragma link C++ function  ROOT::Math::RotationZYX::SetComponents (double *, double *);
 #pragma link C++ function  ROOT::Math::RotationZYX::GetComponents (double *);
 #pragma link C++ function  ROOT::Math::RotationZYX::operator* (const ROOT::Math::XYZPoint &);
@@ -73,9 +111,28 @@
 #pragma link C++ function  ROOT::Math::RotationZ::operator* (const ROOT::Math::XYZTVector &);
 
 // transform3D
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::Rotation3D &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::AxisAngle &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::EulerAngles &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::RotationZYX &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::Quaternion &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::RotationX &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::RotationY &,const ROOT::Math::Translation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::Transform3D (const ROOT::Math::RotationZ &,const ROOT::Math::Translation3D &);
 #pragma link C++ function  ROOT::Math::Transform3D::Transform3D (double *, double*);
+
 #pragma link C++ function  ROOT::Math::Transform3D::SetComponents (double *, double *);
 #pragma link C++ function  ROOT::Math::Transform3D::GetComponents (double *);
+//#pragma link C++ function  ROOT::Math::Transform3D::Rotation< ROOT::Math::RotationZYX>();
+#pragma link C++ function  ROOT::Math::Transform3D::GetRotation ( ROOT::Math::Rotation3D &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetRotation ( ROOT::Math::RotationZYX &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetRotation ( ROOT::Math::AxisAngle &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetRotation ( ROOT::Math::EulerAngles &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetRotation ( ROOT::Math::Quaternion &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetDecomposition ( ROOT::Math::RotationZYX &,ROOT::Math::XYZVector &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetDecomposition ( ROOT::Math::AxisAngle &,ROOT::Math::XYZVector &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetDecomposition ( ROOT::Math::EulerAngles &,ROOT::Math::XYZVector &);
+#pragma link C++ function  ROOT::Math::Transform3D::GetDecomposition ( ROOT::Math::Quaternion &,ROOT::Math::XYZVector &);
 #pragma link C++ function  ROOT::Math::Transform3D::operator* (const ROOT::Math::XYZPoint &);
 #pragma link C++ function  ROOT::Math::Transform3D::operator* (const ROOT::Math::XYZVector &);
 #pragma link C++ function  ROOT::Math::Transform3D::operator* (const ROOT::Math::XYZTVector &);
