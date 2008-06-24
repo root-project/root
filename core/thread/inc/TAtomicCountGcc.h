e56f5e01b4c601318616b21b8d58222ddb8366ef
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-06-24 16:29:45 +0000

    Apple behaves as expected with the 10.6 beta (remove previous correction).
    git-svn-id: http://root.cern.ch/svn/root/trunk@24518 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/thread/inc/TAtomicCountGcc.h
+++ core/thread/inc/TAtomicCountGcc.h
@@ -28,7 +28,7 @@
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
-#if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 2) || defined(__APPLE__)
+#if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 2)
 #include <bits/atomicity.h>
 #else
 #include <ext/atomicity.h>
