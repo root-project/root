3ee0e2fd6088302b0fc73e0b702aa9e64bdba4c1
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2008-07-08 14:17:19 +0000

    Fix warnings about shadowed variables
    git-svn-id: http://root.cern.ch/svn/root/trunk@24713 27541ba8-7e3a-0410-8455-c3a389f83636

--- net/auth/src/TAuthenticate.cxx
+++ net/auth/src/TAuthenticate.cxx
@@ -2565,8 +2565,8 @@ Int_t TAuthenticate::ClearAuth(TString &user, TString &passwd, Bool_t &pwdhash)
          if (prompt == 1 || pashash.Length() == 0) {
 
             if (passwd == "") {
-               TString prompt(Form("%s@%s password: ", user.Data(),fRemote.Data()));
-               char *pwd = PromptPasswd(prompt);
+               TString xp(Form("%s@%s password: ", user.Data(),fRemote.Data()));
+               char *pwd = PromptPasswd(xp);
                passwd = TString(pwd);
                delete [] pwd;
                if (passwd == "") {
@@ -2781,8 +2781,8 @@ Int_t TAuthenticate::ClearAuth(TString &user, TString &passwd, Bool_t &pwdhash)
       // Prepare passwd to send
    badpass1:
       if (passwd == "") {
-         TString prompt(Form("%s@%s password: ", user.Data(),fRemote.Data()));
-         passwd = PromptPasswd(prompt);
+         TString xp(Form("%s@%s password: ", user.Data(),fRemote.Data()));
+         passwd = PromptPasswd(xp);
          if (passwd == "")
             Error("ClearAuth", "password not set");
       }
