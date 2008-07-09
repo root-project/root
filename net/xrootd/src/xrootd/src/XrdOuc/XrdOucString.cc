62b7cbd7e885a61c78e8e3d6ad7c1f2419faaf9a
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2008-07-09 10:12:00 +0000

    Correctly set the size after a call to XOSINTFORM
    git-svn-id: http://root.cern.ch/svn/root/trunk@24736 27541ba8-7e3a-0410-8455-c3a389f83636

--- net/xrootd/src/xrootd/src/XrdOuc/XrdOucString.cc
+++ net/xrootd/src/xrootd/src/XrdOuc/XrdOucString.cc
@@ -211,6 +211,7 @@ int XrdOucString::form(const char *fmt, ...)
 
    // Decode the arguments
    XOSINTFORM(fmt, str);
+   siz = buf_len;
 
    // Re-adjust the length
    len = strlen(str);
@@ -1258,10 +1259,6 @@ bool XrdOucString::isdigit(int from, int to)
    if (to < from) to = len - 1;
 
    char *c = str + from;
-
-   // Skip initial '-'
-   if (*c == '-') c++;
-
    while (c <= str + to) {
       if (*c < 48 || *c > 57) return 0;
       c++;
