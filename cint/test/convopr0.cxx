a7a3f5167249032a7892c2315c4b9ecf6e4c2a48
Paul Russo <russo@fnal.gov>
2008-07-17 23:50:12 +0000

    Fix char strint literal usage.
    git-svn-id: http://root.cern.ch/svn/root/trunk@24878 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/test/convopr0.cxx
+++ cint/test/convopr0.cxx
@@ -5,35 +5,46 @@
  * For the licensing terms see the file COPYING
  *
  ************************************************************************/
-#ifdef __hpux
-#include <iostream.h>
-#else
-#include <iostream>
-using namespace std;
-#endif
-#include <stdio.h>
 
-class A {
- unsigned int a;
- char *p;
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+private:
+   unsigned int a;
+   const char* p;
 public:
- A(unsigned int ain,char* pin) { a=ain; p=pin; }
- operator unsigned int() { return a;}
- operator char*() { return p;}
+   A(unsigned int ain, const char* pin)
+   {
+      a = ain;
+      p = pin;
+   }
+   operator unsigned int()
+   {
+      return a;
+   }
+   operator const char*()
+   {
+      return p;
+   }
 };
 
-int main() {
-  A x(123,"abcde");
-  unsigned int a= (unsigned int)x ;
-  char *p = (char*)x;
-  for(int i=0;i<5;i++) {
-    a = (unsigned int)x;
-    p = (char*)x;
-    cout <<  p << a << endl;
-    printf("<%s>\n",(char*)x);
-    cout << "a=" << (unsigned int)x << endl;
-    cout << "p=" << (char*)x << endl;
-  }
-  return 0;
+int main()
+{
+   A x(123, "abcde");
+   unsigned int a = (unsigned int) x;
+   const char* p = (const char*) x;
+   for (int i = 0; i < 5; ++i) {
+      a = (unsigned int) x;
+      p = (const char*) x;
+      cout <<  p << a << endl;
+      printf("<%s>\n", (const char*) x);
+      cout << "a=" << (unsigned int) x << endl;
+      cout << "p=" << (const char*) x << endl;
+   }
+   return 0;
 }
 
