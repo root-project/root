0ee147787f57cafd9db0ff2cac40e0bf686eb344
Philippe Canal <pcanal@fnal.gov>
2008-08-06 08:03:34 +0000

    Repair declaration of operator< <double>( [operator<< <double> was working]
    git-svn-id: http://root.cern.ch/svn/root/trunk@25068 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cint7/src/ifunc.cxx
+++ cint/cint7/src/ifunc.cxx
@@ -552,9 +552,10 @@ void Cint::Internal::G__make_ifunctable(char* funcheader)
          ) {
             // we might have "operator< <>" or "operator< <double>" or "operator<< <>" or "operator<< <double>"
             // with the space missing
-            std::string::size_type pos = funcname.find("<<<");
+            std::string::size_type pos = funcname.find("<<");
             if (pos != std::string::npos) {
-               funcname.insert(pos + 2, 1, ' ');
+               if (funcname[pos+2]=='<') ++pos; // case "operator<< <double>"
+               funcname.insert(pos + 1, 1, ' ');
             }
          }
          // Find the first ">>" after the initial "operator>>" or "operator<<".
