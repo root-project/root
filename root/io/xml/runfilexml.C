f6b20f857e382432d586249005bcd3f118cd3971
Philippe Canal <pcanal@fnal.gov>
2008-09-19 16:21:55 +0000

    From Sergei:
    "basicxml" uses xml convertion to/from string without creation of file, but checks content of objects.
    "filexml" creates real xml file and store all kind of test objects in file, but does not check content of restored objects.
    
    File "test_classes.h" contains definitions of different classes, where most cases of data memebers are considered, 
    including all variants of STL containers. I guess, if one able to store/read such objects, one can be pretty sure that 
    xml I/O is working.
    
    
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1519 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/xml/runfilexml.C
@@ -0,0 +1,95 @@
+{
+// Fill out the code of the actual test
+
+   gROOT->ProcessLine(".L test_classes.h+");
+   
+   TFile *f = TFile::Open("file.xml", "recreate");
+   if (f==0) {
+      cout << "Cannot create file.xml" << endl;   
+      return;
+   }
+   
+   TXmlEx1* ex1 = new TXmlEx1;
+   TXmlEx2* ex2 = new TXmlEx2;
+   TXmlEx3* ex3 = new TXmlEx3;
+   TXmlEx4* ex4 = new TXmlEx4(true);
+   TXmlEx5* ex5 = new TXmlEx5(true);
+   TXmlEx6* ex6 = new TXmlEx6(true);
+   TXmlEx7* ex7 = new TXmlEx7(true);
+   TXmlEx8* ex8 = new TXmlEx8(true);
+   
+   TH1I* h1 = new TH1I("histo1","histo title", 100, -10., 10.);
+   h1->FillRandom("gaus",10000);
+   
+   TList* arr = new TList;
+   for(Int_t n=0;n<10;n++) {
+      TBox* b = new TBox(n*10,n*100,n*20,n*200);  
+      arr->Add(b, Form("option_%d_option",n));
+   }
+   
+   TClonesArray* clones = new TClonesArray("TBox",10);
+   for(int n=0;n<10;n++) 
+       new ((*clones)[n]) TBox(n*10,n*100,n*20,n*200);
+   
+   cout << "Writing objects to file " << endl;
+   
+   f->WriteObject(ex1, "ex1");
+   f->WriteObject(ex2, "ex2");
+   f->WriteObject(ex3, "ex3");
+   f->WriteObject(ex4, "ex4");
+   f->WriteObject(ex5, "ex5");
+   f->WriteObject(ex6, "ex6");
+   f->WriteObject(ex7, "ex7");
+   f->WriteObject(ex8, "ex8");
+   h1->Write("histo");
+   h1->SetDirectory(0);
+   arr->Write("arr",TObject::kSingleKey);
+   clones->Write("clones",TObject::kSingleKey);
+   delete f; f = 0;
+   
+   delete ex1; ex1 = 0;
+   delete ex2; ex2 = 0;
+   delete ex3; ex3 = 0;
+   delete ex4; ex4 = 0;
+   delete ex5; ex5 = 0;
+   delete ex6; ex6 = 0;
+   delete ex7; ex7 = 0;
+   delete ex8; ex8 = 0;
+   delete h1;  h1 = 0;
+   delete arr; arr = 0;
+   delete clones; clones = 0;
+   
+   TFile *f = TFile::Open("file.xml");
+   if (f==0) {
+      cout << "Cannot open file.xml" << endl;   
+      return;
+   }
+   
+   f->GetObject("ex1", ex1);
+   f->GetObject("ex2", ex2);
+   f->GetObject("ex3", ex3);
+   f->GetObject("ex4", ex4);
+   f->GetObject("ex5", ex5);
+   f->GetObject("ex6", ex6);
+   f->GetObject("ex7", ex7);
+   f->GetObject("ex8", ex8);
+   f->GetObject("histo", h1);
+   f->GetObject("arr", arr);
+   f->GetObject("clones", clones);
+
+
+   cout << "ex1 = " << (ex1!=0) << endl;
+   cout << "ex2 = " << (ex2!=0) << endl;
+   cout << "ex3 = " << (ex3!=0) << endl;
+   cout << "ex4 = " << (ex4!=0) << endl;
+   cout << "ex5 = " << (ex5!=0) << endl;
+   cout << "ex6 = " << (ex6!=0) << endl;
+   cout << "ex7 = " << (ex7!=0) << endl;
+   cout << "ex8 = " << (ex8!=0) << endl;
+   cout << "h1 = " << (h1!=0) << endl;
+   cout << "arr = " << (arr!=0) << endl;
+   cout << "clones = " << (clones!=0) << endl;
+
+   delete f;
+   return 0;
+}
