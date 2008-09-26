e0ed2b260ba389ad08c3c2ddd31333f0dde924a4
Axel Naumann <Axel.Naumann@cern.ch>
2008-09-26 08:53:10 +0000

    turn autodict off for test instead of disabling test altogether
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1526 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/template/runtemplatefriend.C
+++ cint/template/runtemplatefriend.C
@@ -1,4 +1,7 @@
 {
+// fails due to CINT's autodict facility:
+// when dict for shared_ptr is generated no dict for the templated constructor is requested
+gROOT->ProcessLine(".autodict");
 gROOT->ProcessLine(".x templatefriend.cxx");
 gROOT->ProcessLine(".U templatefriend.cxx");
 gROOT->ProcessLine(".x templatefriend.cxx+");
