9d6dac81e1a91f69045ad94f25c26bdab1aab3b2
Rene Brun <Rene.Brun@cern.ch>
2008-10-02 14:37:40 +0000

    Starting development version 5.21/05
    git-svn-id: http://root.cern.ch/svn/root/trunk@25665 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.21/04"
+#define ROOT_RELEASE "5.21/05"
 #define ROOT_RELEASE_DATE "Oct  2 2008"
-#define ROOT_RELEASE_TIME "12:21:53"
-#define ROOT_SVN_REVISION 25661
+#define ROOT_RELEASE_TIME "15:29:40"
+#define ROOT_SVN_REVISION 25663
 #define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 333060
+#define ROOT_VERSION_CODE 333061
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
