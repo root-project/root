2fa2cf154029d6cc96f53d6e65cfec97d8f089bd
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2008-10-07 17:00:32 +0000

    Cope with C++ header dependencies cleanup in gcc4.3
    git-svn-id: http://root.cern.ch/svn/root/trunk@25729 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proof/src/TProofServ.cxx
+++ proof/proof/src/TProofServ.cxx
@@ -33,6 +33,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <cstdlib>
 
 #if (defined(__FreeBSD__) && (__FreeBSD__ < 4)) || \
     (defined(__APPLE__) && (!defined(MAC_OS_X_VERSION_10_3) || \
