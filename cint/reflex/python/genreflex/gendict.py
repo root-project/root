16307d0bdca207e224aacb6fce17be3e1373feef
Axel Naumann <Axel.Naumann@cern.ch>
2008-10-13 16:19:09 +0000

    From Josh Kelley and me: fix template <typename T> struct A{T f();} for T: char[9]; both A<T> and A<T>::f() could not handle arrays as template arg / return value
    git-svn-id: http://root.cern.ch/svn/root/trunk@25795 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/reflex/python/genreflex/gendict.py
+++ cint/reflex/python/genreflex/gendict.py
@@ -37,7 +37,7 @@ class genDictionary(object) :
     self.cppEnumSelect     = {}
     self.cppFunctionSelect = {}
     self.last_id    = ''
-    self.transtable = string.maketrans('<>&*,: ().$-', '__rp__s___dm')
+    self.transtable = string.maketrans('<>&*,: ().$-[]', '__rp__s___dm__')
     self.ignoremeth = ('rbegin', 'rend', '_Eq','_Lt', 'value_comp')
     self.x_id       = iter(xrange(sys.maxint))
     self.errors     = 0
@@ -1369,7 +1369,7 @@ class genDictionary(object) :
     elif elem == 'PointerType' :
       t = self.genTypeName(attrs['type'],enum, const, colon)
       if   t[-1] == ')' or t[-7:] == ') const' or t[-10:] == ') volatile' : s += t.replace('::*)','::**)').replace('::)','::*)').replace('(*)', '(**)').replace('()','(*)')
-      elif t[-1] == ']' or t[-7:] == ') const' or t[-10:] == ') volatile' : s += t[:t.find('[')] + '(*)' + t[t.find('['):]
+      elif t[-1] == ')' or t[-7:] == ') const' or t[-10:] == ') volatile' : s += t[:t.find('[')] + '(*)' + t[t.find('['):]
       else              : s += t + '*'
     elif elem == 'ReferenceType' :
       s += self.genTypeName(attrs['type'],enum, const, colon)+'&'
