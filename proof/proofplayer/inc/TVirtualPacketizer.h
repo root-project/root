343f77124b286453e725d17723755b66b2b4ae2b
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2008-10-16 07:37:29 +0000

    From Bertrand: fix several for warnings C4138 on Windows ('*/' found outside of comment)
    git-svn-id: http://root.cern.ch/svn/root/trunk@25839 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proofplayer/inc/TVirtualPacketizer.h
+++ proof/proofplayer/inc/TVirtualPacketizer.h
@@ -114,8 +114,8 @@ public:
    Double_t      GetCumProcTime() const { return fProgressStatus->GetProcTime(); }
    Float_t       GetInitTime() const { return fInitTime; }
    Float_t       GetProcTime() const { return fProcTime; }
-   virtual void  MarkBad(TSlave */*s*/, TProofProgressStatus */*status*/, TList **/*missingFiles*/) { return; }
-   virtual Int_t AddProcessed(TSlave */*sl*/, TProofProgressStatus */*st*/, TList **/*missingFiles*/) { return 0; }
+   virtual void  MarkBad(TSlave * /*s*/, TProofProgressStatus * /*status*/, TList ** /*missingFiles*/) { return; }
+   virtual Int_t AddProcessed(TSlave * /*sl*/, TProofProgressStatus * /*st*/, TList ** /*missingFiles*/) { return 0; }
    TProofProgressStatus *GetStatus() { return fProgressStatus; }
    void          SetProgressStatus(TProofProgressStatus *st) { fProgressStatus = st; }
    ClassDef(TVirtualPacketizer,0)  //Generate work packets for parallel processing
