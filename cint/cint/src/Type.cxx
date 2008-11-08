d110c0f40b9842500978ae0add3c04d5bd77fe01
Philippe Canal <pcanal@fnal.gov>
2008-11-08 23:08:31 +0000

    Fix typo in preprocessor statement noticed by gcc 4.4
    git-svn-id: http://root.cern.ch/svn/root/trunk@26109 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cint/src/Type.cxx
+++ cint/cint/src/Type.cxx
@@ -125,7 +125,7 @@ const char* Cint::G__TypeInfo::TrueName()
   strcpy(G__buf,
 	 G__type2string((int)type,(int)tagnum,-1,(int)reftype,(int)isconst));
   return(G__buf);
-#elif  !defind(G__OLDIMPLEMENTATION401)
+#elif  !defined(G__OLDIMPLEMENTATION401)
   return(G__type2string((int)type,(int)tagnum,-1,(int)reftype,(int)isconst));
 #else
   return(G__type2string((int)type,(int)tagnum,-1,(int)reftype));
@@ -138,7 +138,7 @@ const char* Cint::G__TypeInfo::Name()
   strcpy(G__buf,G__type2string((int)type,(int)tagnum,(int)typenum,(int)reftype
 			       ,(int)isconst));
   return(G__buf);
-#elif  !defind(G__OLDIMPLEMENTATION401)
+#elif  !defined(G__OLDIMPLEMENTATION401)
   return(G__type2string((int)type,(int)tagnum,(int)typenum,(int)reftype
 	,(int)isconst));
 #else
