b69c123f6869abc01d2624d40e823b8777a9929b
Philippe Canal <pcanal@fnal.gov>
2008-11-10 10:24:12 +0000

    extend test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1556 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/string/runstring.C
+++ root/treeformula/string/runstring.C
@@ -36,8 +36,8 @@ public:
 #endif
 
 void runstring() {
-//    TString *sone = new TString("sone");
-//    string *stwo = new string("stwo");
+   TString *sone = new TString("sone");
+   string *stwo = new string("stwo");
 
    vector<TString> *sthree = new vector<TString>;
    vector<string> *sfour = new vector<string>;
@@ -52,8 +52,8 @@ void runstring() {
    TFile *f = new TFile("string.root","RECREATE");
    TTree *t = new TTree("T","T");
 
-   //t->Branch("sone",&sone,32000,0);
-   //t->Branch("stwo",&stwo,32000,0);
+   t->Branch("sone",&sone,32000,0);
+   t->Branch("stwo",&stwo,32000,0);
    t->Branch("sthree",&sthree,32000,0);
    t->Branch("sfour",&sfour,32000,0);
    t->Branch("obj.",&m);
