7b530f7b93857046458fa8cc27c1f60ef3fdc8ab
Philippe Canal <pcanal@fnal.gov>
2008-11-22 19:37:58 +0000

    Update to work both with or without rootmap files generated by ACLiC
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1589 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/selector/simple/runtestLoadingSelector.C
+++ root/selector/simple/runtestLoadingSelector.C
@@ -1,5 +1,10 @@
 {
 gROOT->ProcessLine(".L testSel.C+");
+
+// Insure that the library is not loaded instead of the 
+// script
+gInterpreter->UnloadLibraryMap("testSelector_C");
+
 gROOT->ProcessLine(".L testSelector.C");
 bool res = runtest();
 if (!res) return !res;
