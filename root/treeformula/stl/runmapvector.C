57a4042bc3d8859f82ba0e01df914836deef65f1
Philippe Canal <pcanal@fnal.gov>
2008-11-22 15:26:15 +0000

    Update to work both with or without rootmap files generated by ACLiC
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1587 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/stl/runmapvector.C
+++ root/treeformula/stl/runmapvector.C
@@ -2,6 +2,7 @@
 
 void runmapvector(const char  *filename = "mapvector.root") 
 {
+   gInterpreter->UnloadLibraryMap("mapvector_C");
    TFile * f = TFile::Open(filename,"READ");
    TTree *tree; f->GetObject("T",tree);
    tree->Scan("data.first");
