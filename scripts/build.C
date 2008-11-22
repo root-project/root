3759212fe623b2d57d270b116f23e99551fb0a05
Philippe Canal <pcanal@fnal.gov>
2008-11-22 19:39:04 +0000

    Update to work both with or without rootmap files generated by ACLiC
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1591 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/build.C
+++ scripts/build.C
@@ -13,9 +13,16 @@ void build(const char *filename,const char *lib = 0, const char *obj = 0) {
       TObjArray *libs = liblist.Tokenize(" ");
       TIter iter(libs);
       TObjString *objstr;
+      TString libstolink(" $ObjectFiles ");
       while ( (objstr=(TObjString*)iter.Next()) ) {
          gSystem->Load(objstr->String());
+         TString libfile( gSystem->GetLibraries(objstr->String(),"DSL",kFALSE));
+         libstolink.Append(libfile);
+         libstolink.Append(" ");
       }
+      TString s = gSystem->GetMakeSharedLib();
+      s.ReplaceAll(" $ObjectFiles",libstolink);
+      gSystem->SetMakeSharedLib(s);
    }
    int result = gSystem->CompileMacro(filename,"kc");
    if (!result) gApplication->Terminate(1);
