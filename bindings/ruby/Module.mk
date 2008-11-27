72e0395d74a8a5e0589dcc600feb23160fb74f4e
Fons Rademakers <Fons.Rademakers@cern.ch>
2008-11-27 13:56:33 +0000

    build on Mac OS ruby64 (like python64) as there are no 64-bit version of these programs are available (the 64-bit run-time libs are there though). This has been fixed in SnowLeopard (10.6) so it only needed for 10.5.
    git-svn-id: http://root.cern.ch/svn/root/trunk@26496 27541ba8-7e3a-0410-8455-c3a389f83636

--- bindings/ruby/Module.mk
+++ bindings/ruby/Module.mk
@@ -12,6 +12,12 @@ RUBYROOTDIR    := $(MODDIR)
 RUBYROOTDIRS   := $(RUBYROOTDIR)/src
 RUBYROOTDIRI   := $(RUBYROOTDIR)/inc
 
+##### ruby64 #####
+RUBY64S        := $(MODDIRS)/ruby64.c
+RUBY64O        := $(RUBY64S:.c=.o)
+RUBY64         := bin/ruby64
+RUBY64DEP      := $(RUBY64O:.o=.d)
+
 ##### libRuby #####
 RUBYROOTL      := $(MODDIRI)/LinkDef.h
 RUBYROOTDS     := $(MODDIRS)/G__Ruby.cxx
@@ -31,6 +37,10 @@ RUBYROOTMAP    := $(RUBYROOTLIB:.$(SOEXT)=.rootmap)
 ALLHDRS        += $(patsubst $(MODDIRI)/%.h,include/%.h,$(RUBYROOTH))
 ALLLIBS        += $(RUBYROOTLIB)
 ALLMAPS        += $(RUBYROOTMAP)
+ifeq ($(ARCH),macosx64)
+ALLEXECS       += $(RUBY64)
+INCLUDEFILES   += $(RUBY64DEP)
+endif
 
 # include all dependency files
 INCLUDEFILES   += $(RUBYROOTDEP)
@@ -55,18 +65,27 @@ $(RUBYROOTMAP): $(RLIBMAP) $(MAKEFILEDEP) $(RUBYROOTL)
 		$(RLIBMAP) -o $(RUBYROOTMAP) -l $(RUBYROOTLIB) \
 		   -d $(RUBYROOTLIBDEPM) -c $(RUBYROOTL)
 
+$(RUBY64):      $(RUBY64O)
+		$(CC) $(LDFLAGS) -o $@ $(RUBY64O) \
+		   $(RUBYLIBDIR) $(RUBYLIB)
+
+ifeq ($(ARCH),macosx64)
+all-$(MODNAME): $(RUBYROOTLIB) $(RUBYROOTMAP) $(RUBY64)
+else
 all-$(MODNAME): $(RUBYROOTLIB) $(RUBYROOTMAP)
+endif
 
 clean-$(MODNAME):
-		@rm -f $(RUBYROOTO) $(RUBYROOTDO)
+		@rm -f $(RUBYROOTO) $(RUBYROOTDO) $(RUBY64O)
 
 clean::         clean-$(MODNAME)
 
 distclean-$(MODNAME): clean-$(MODNAME)
 		@rm -f $(RUBYROOTDEP) $(RUBYROOTDS) $(RUBYROOTDH) \
-		   $(RUBYROOTLIB) $(RUBYROOTMAP)
+		   $(RUBYROOTLIB) $(RUBYROOTMAP) $(RUBY64DEP) $(RUBY64)
 
 distclean::     distclean-$(MODNAME)
 
 ##### extra rules ######
 $(RUBYROOTO): CXXFLAGS += $(RUBYINCDIR:%=-I%) -Iinclude/cint
+$(RUBY64O): CFLAGS += $(RUBYINCDIR:%=-I%)
