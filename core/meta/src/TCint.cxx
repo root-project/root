952b3599353ad8e451a992575110f65669ad77b3
Philippe Canal <pcanal@fnal.gov>
2008-12-12 18:06:17 +0000

    From Axel:adapt -I path to TCint/TCint7. Fixes e.g. root/selector/simple with --enable-cint7.
    git-svn-id: http://root.cern.ch/svn/root/trunk@26885 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/meta/src/TCint.cxx
+++ core/meta/src/TCint.cxx
@@ -1994,6 +1994,16 @@ const char *TCint::GetIncludePath()
       const char *pathname = path.Name();
       fIncludePath.Append(" -I\"").Append(pathname).Append("\" ");
    }
+#ifdef ROOTINCDIR
+   fIncludePath.Append(" -I\"").Append(ROOTINCDIR);
+#else 
+   fIncludePath.Append(" -I\"").Append(gRootDir).Append("/include");
+#endif
+#ifdef R__BUILDING_CINT7
+   fIncludePath.Append("/cint7\" ");
+#else
+   fIncludePath.Append("/cint\" ");
+#endif
 
    return fIncludePath;
 }
