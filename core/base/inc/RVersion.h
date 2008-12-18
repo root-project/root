6ee6e5b06182b08e1bcef198a7f2d234b2ff9528
Rene Brun <Rene.Brun@cern.ch>
2008-12-18 11:31:18 +0000

    Start development cycle for new version 5.23/01 The first development version 5.23/01 is scheduled foe end of January 2009. The production release 5.24 is scheduled for end of June 2009.
    git-svn-id: http://root.cern.ch/svn/root/trunk@27003 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.22/00"
-#define ROOT_RELEASE_DATE "Dec 17 2008"
-#define ROOT_RELEASE_TIME "18:11:02"
-#define ROOT_SVN_REVISION 26997
+#define ROOT_RELEASE "5.23/01"
+#define ROOT_RELEASE_DATE "Dec 18 2008"
+#define ROOT_RELEASE_TIME "11:40:54"
+#define ROOT_SVN_REVISION 27001
 #define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 333312
+#define ROOT_VERSION_CODE 333569
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
