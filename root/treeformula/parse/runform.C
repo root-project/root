5f8517b8f4ea5aa84f5c36e0f2bf52975e5f21d1
Philippe Canal <pcanal@fnal.gov>
2009-01-23 16:43:52 +0000

    Well .. this is it ... tree->Draw(...,Form(...) ) is officially no longer supported.  Use tree->Draw(...,TString::Format(...) ) instead.
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1657 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/runform.C
+++ root/treeformula/parse/runform.C
@@ -3,7 +3,7 @@
   double x = 1;
   t.Branch("x", &x, "x/D");
   t.Fill();
-  t.Draw("x", Form(""));
-  t.Draw("x", Form(""));
+   t.Draw("x", TString::Format(""));
+   t.Draw("x", TString::Format(""));
   return 0;
 }
