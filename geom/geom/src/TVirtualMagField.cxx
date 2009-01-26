f24b16c7636583ef2697ef9a5eb917d690c7d89c
Fons Rademakers <Fons.Rademakers@cern.ch>
2009-01-26 17:07:51 +0000

    correct svn $Id$ field.
    git-svn-id: http://root.cern.ch/svn/root/trunk@27244 27541ba8-7e3a-0410-8455-c3a389f83636

--- geom/geom/src/TVirtualMagField.cxx
+++ geom/geom/src/TVirtualMagField.cxx
@@ -1,4 +1,4 @@
-// @(#)root/geom:$Id:$
+// @(#)root/geom:$Id$
 
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
@@ -13,14 +13,14 @@
 
 //______________________________________________________________________________
 // TVirtualMagField - ABC for magnetic field. Derived classes are encouraged to
-// use the TVirtualMagField named constructor and must implement the method: 
+// use the TVirtualMagField named constructor and must implement the method:
 //    Field(const Double_t *x, Double_t *B)
 //
 // A field object can be made global via:
 //    TGlobalMagField::Instance()->SetField(field)         [1]
 // A field which is made global is owned by the field manager. The used is not
 // allowed to delete it directly anymore (otherwise a Fatal() is issued). Global
-// field can be deleted by calling [1] with a different argument (which can be 
+// field can be deleted by calling [1] with a different argument (which can be
 // NULL). Otherwise the global field is deleted together with the field manager.
 //
 //______________________________________________________________________________
@@ -35,8 +35,8 @@ TVirtualMagField::~TVirtualMagField()
    if (global_field == this)
       Fatal("~TVirtualMagField", "Not allowed to delete a field once set global. \
              \n To delete the field call: TGeoGlobalMagField::Instance()->SetField(NULL)");
-}   
-   
+}
+
 //______________________________________________________________________________
 // TGeoUniformMagField - Implementation for uniform magnetic field.
 //______________________________________________________________________________
@@ -44,7 +44,7 @@ TVirtualMagField::~TVirtualMagField()
 ClassImp(TGeoUniformMagField)
 
 //______________________________________________________________________________
-TGeoUniformMagField::TGeoUniformMagField() 
+TGeoUniformMagField::TGeoUniformMagField()
                     :TVirtualMagField()
 {
 // Default constructor;
