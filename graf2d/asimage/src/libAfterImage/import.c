b6faf13e39aeae5e096197f69c85723656434d49
Olivier Couet <Olivier.Couet@cern.ch>
2009-01-30 16:15:57 +0000

    - From Sasha Vasko:   Fix a warning reported by gcc.
    git-svn-id: http://root.cern.ch/svn/root/trunk@27309 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/asimage/src/libAfterImage/import.c
+++ graf2d/asimage/src/libAfterImage/import.c
@@ -1653,8 +1653,8 @@ jpeg2ASImage( const char * path, ASImageImportParams *params )
 		prepare_scanline( im->width, 0, &buf, False );
 
 	/* Make a one-row-high sample array that will go away when done with image */
-	buffer =(*cinfo.mem->alloc_sarray)((j_common_ptr) & cinfo, JPOOL_IMAGE,
-										cinfo.output_width * cinfo.output_components, 1);
+	buffer = cinfo.mem->alloc_sarray((j_common_ptr) &cinfo, JPOOL_IMAGE,
+									cinfo.output_width * cinfo.output_components, 1);
 
 	/* Step 6: while (scan lines remain to be read) */
 	SHOW_TIME("loading initialization",started);
