23a30c36b0227cdf08b15120c5ddbe8799c0e8ff
Philippe Canal <pcanal@fnal.gov>
2009-02-27 17:14:01 +0000

    fix compilation
    git-svn-id: http://root.cern.ch/svn/root/trunk@27648 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/demo/makecint/exception/eh.h
+++ cint/demo/makecint/exception/eh.h
@@ -14,7 +14,7 @@ class MyException : public exception {
  public:
   char msg[100];
   MyException(const char* m) { strcpy(msg,m); }
-  virtual const char* what() const /* throw() */ { return(msg); }
+  virtual const char* what() const throw() { return(msg); }
 };
 
 void test(char* name) {
