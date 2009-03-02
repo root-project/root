56c5f3d53d4c7f724180bae0200808a3ddc26e7a
Wim Lavrijsen <WLavrijsen@lbl.gov>
2009-03-02 23:36:02 +0000

    adding __contains__ for std::map
    git-svn-id: http://root.cern.ch/svn/root/trunk@27673 27541ba8-7e3a-0410-8455-c3a389f83636

--- bindings/pyroot/src/Pythonize.cxx
+++ bindings/pyroot/src/Pythonize.cxx
@@ -775,6 +775,33 @@ namespace {
       return callSelfIndex( args, "_vector__at" );
    }
 
+//- map behavior as primitives ------------------------------------------------
+   PyObject* MapContains( PyObject*, PyObject* args )
+   {
+      PyObject* result = 0;
+
+      PyObject* iter = CallPySelfObjMethod( args, "find", "OO" );
+      if ( ObjectProxy_Check( iter ) ) {
+         PyObject* end = CallPySelfMethod( args, "end", "OO" ); // cheating on the arguments here ...
+         if ( ObjectProxy_Check( end ) ) {
+            if ( *(void**)((ObjectProxy*)iter)->GetObject() != *(void**)((ObjectProxy*)end)->GetObject() ) {
+               Py_INCREF( Py_True );
+               result = Py_True;
+            }
+         }
+         Py_XDECREF( end );
+      }
+      Py_XDECREF( iter );
+
+      if ( ! result ) {
+         PyErr_Clear();            // e.g. wrong argument type, which should always lead to False
+         Py_INCREF( Py_False );
+         result = Py_False;
+      }
+
+      return result;
+   }
+
 //- STL container iterator support --------------------------------------------
    PyObject* StlSequenceIter( PyObject*, PyObject* args )
    {
@@ -1720,6 +1747,12 @@ Bool_t PyROOT::Pythonize( PyObject* pyclass, const std::string& name )
    // provide a slice-able __getitem__, if possible
       if ( HasAttrDirect( pyclass, PyStrings::gVectorAt ) )
          Utility::AddToClass( pyclass, "__getitem__", (PyCFunction) VectorGetItem );
+
+      return kTRUE;
+   }
+
+   if ( IsTemplatedSTLClass( name, "map" ) ) {
+      Utility::AddToClass( pyclass, "__contains__", (PyCFunction) MapContains );
 
       return kTRUE;
    }
