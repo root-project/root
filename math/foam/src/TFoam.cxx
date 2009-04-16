0933ec2f3e6ac900b0d9350890d2dff832f40e68
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2009-04-16 16:41:32 +0000

    improt changes from Wouter in roostats branch: suppress printout at foam print level zero
    git-svn-id: http://root.cern.ch/svn/root/trunk@28260 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/foam/src/TFoam.cxx
+++ math/foam/src/TFoam.cxx
@@ -862,17 +862,20 @@ void TFoam::Grow()
          Int_t kEcho=10;
          if(fLastCe>=10000) kEcho=100;
          if( (fLastCe%kEcho)==0) {
-            if(fDim<10)
-               cout<<fDim<<flush;
-            else
-               cout<<"."<<flush;
-            if( (fLastCe%(100*kEcho))==0)  cout<<"|"<<fLastCe<<endl<<flush;
-         }
+	   if (fChat>0) {
+	     if(fDim<10)
+	       cout<<fDim<<flush;
+             else
+	       cout<<"."<<flush;
+	     if( (fLastCe%(100*kEcho))==0)  cout<<"|"<<fLastCe<<endl<<flush;
+	   }
+	 }
       }
-      //
       if( Divide( newCell )==0) break;  // and divide it into two
    }
-   cout<<endl<<flush;
+   if (fChat>0) {
+     cout<<endl<<flush;
+   }
    CheckAll(0);   // set arg=1 for more info
 }// Grow
 
