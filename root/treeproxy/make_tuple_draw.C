066c189b985915561a27f1b5883fbc4118432808
Philippe Canal <pcanal@fnal.gov>
2009-04-24 20:34:17 +0000

    slight test extension
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1703 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeproxy/make_tuple_draw.C
+++ root/treeproxy/make_tuple_draw.C
@@ -1,6 +1,11 @@
 Double_t make_tuple_draw() {
    cout.width(7);
    cout << std::setprecision(2)  << evt.val1 << ", " << evt.val2 << ", " << evt.val3 << endl;
-
+   int n = trk.N;
+   cout << n << endl;
+   for(int i=0; i<n; ++i) {
+      float f = trk.arr[i];
+      cout << i << " " << (int)f << endl;
+   }
    return evt.val1;
 }
