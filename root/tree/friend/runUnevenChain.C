56a54733f659d215817749b62721527594e10d31
Philippe Canal <pcanal@fnal.gov>
2009-05-08 18:45:54 +0000

    Add new test to check the case of 2 friend chains which have equal entries but do not have the same number of files
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1706 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/friend/runUnevenChain.C
@@ -0,0 +1,69 @@
+string chainOneStem = "chainOne";
+size_t numFilesInChainOne = 2;
+string chainTwoStem = "chainTwo";
+size_t numFilesInChainTwo = 3;
+
+void generateFile(string nameOfChain, size_t numberOfFiles, size_t numberOfEntriesInFile) {
+
+   Int_t valueToPlace = numberOfFiles*1000+numberOfEntriesInFile*100;
+   for (size_t i=0;i<numberOfFiles;i++) {
+      TFile* newfile = new TFile(Form("%s_%i.root", nameOfChain.c_str(), i), "RECREATE"); 
+      TTree*  outputTree = new TTree(nameOfChain.c_str(), nameOfChain.c_str());
+      TString branchname; branchname.Form("value%d_%d",numberOfFiles,numberOfEntriesInFile);
+      TString leafname; leafname.Form("leafvalue%d_%d/I",numberOfFiles,numberOfEntriesInFile);
+      outputTree->Branch(branchname, &valueToPlace,leafname);
+      for (size_t j=0;j<numberOfEntriesInFile;j++) {
+         outputTree->Fill();
+         valueToPlace++;
+      }
+      cout << nameOfChain << ", file " << i+1 << ", Entry number: " << outputTree->GetEntries() << endl;
+      outputTree->Write();
+      delete outputTree;
+      delete newfile;
+   } 
+   
+}
+
+void generateFiles() {
+  /* We put the number of events in this chain that equals the number of
+   * files in the other chain so that we end up with equal numbers of entries.*/
+  generateFile(chainOneStem, numFilesInChainOne, numFilesInChainTwo);
+  generateFile(chainTwoStem, numFilesInChainTwo, numFilesInChainOne);
+}
+
+void runUnevenChain() {
+
+   generateFiles();
+   
+   TChain firstChain(chainOneStem.c_str());
+   firstChain.Add(Form("%s*.root", chainOneStem.c_str()));
+   TChain secondChain(chainTwoStem.c_str());
+   secondChain.Add(Form("%s*.root", chainTwoStem.c_str()));
+   
+   Int_t first, second;
+   firstChain.SetBranchAddress("value2_3", &first);
+   secondChain.SetBranchAddress("value3_2", &second);
+   
+   cout << firstChain.GetName() << " has " << firstChain.GetEntries() << " entries\n";
+   cout << secondChain.GetName() << " has " << secondChain.GetEntries() << " entries\n";
+   
+   /* First output the trees to make sure they are correct. */ 
+   for(Int_t i=0;i<secondChain.GetEntries();i++) {
+      firstChain.GetEntry(i);
+      secondChain.GetEntry(i);
+      cout << first << " " << second << endl;
+   }
+   
+   /* Now add the friends and test friendship. */
+   cout << "Testing Friendship..." << endl;
+   firstChain.AddFriend(&secondChain);
+   for(Int_t i=0;i<firstChain.GetEntries();i++) {
+      firstChain.GetEntry(i);
+      if (first%100 != second%100) {
+         cout << "Chains not lined up, entry: " << i << endl;
+      }
+      cout << first << " " << second << endl;
+   }
+   firstChain.Scan("value2_3:value3_2");
+   
+} 
