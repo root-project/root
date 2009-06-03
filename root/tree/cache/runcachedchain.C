598009df9ff03f406eb6cc3fc89903678f0f5078
Philippe Canal <pcanal@fnal.gov>
2009-06-03 16:09:53 +0000

    add test for efficacity of the cache at the beginning of the new file in a chain
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1714 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/cache/runcachedchain.C
@@ -0,0 +1,22 @@
+{
+   c = new TChain("esdTree"); c->Add("AliESDs*.root");
+   c->SetCacheSize();
+   TTreeCache::SetLearnEntries(100);
+   for (int i=0; i<10; ++i) {
+      c->GetEntry(i);
+   }
+   TFile *curfile = c->GetCurrentFile(); 
+   ca = (TTreeCache*)curfile->GetCacheRead(); 
+   
+   
+   c->LoadTree(101);
+   TBranch *b = c->GetBranch("AliESDRun.fMagneticField");
+   Long64_t pos = b->GetBasketSeek(0);
+   Int_t res = ca->ReadBuffer(0,pos,10);
+   
+   if (res != 1) {
+      fprintf(stdout,"ERROR: Could not find the basket bytes for the first basket of AliESDRun.fMagneticField in the cache\n");
+      return 1;
+   }
+   return 0;
+}
