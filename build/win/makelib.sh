b23300550a0decf32e775cf780e207c0ce42db78
Philippe Canal <pcanal@fnal.gov>
2009-06-05 14:16:02 +0000

    From Axel: this fixes the linking of libCint7this fixes the linking of libCint7.
    git-svn-id: http://root.cern.ch/svn/root/trunk@28835 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/win/makelib.sh
+++ build/win/makelib.sh
@@ -79,6 +79,9 @@ if [ "$R__PLATFORM" = "win32" ]; then
       elif [ "$R__LIB" = "lib/libminicern.dll" ]; then
          cmd="$R__LD $R__SOFLAGS $R__LDFLAGS -o $dlldir/${name}.dll $R__OBJS \
               $libdir/${name}.exp $R__EXTRA $syslibs"
+      elif [ "$R__LIB" = "lib/libCint7.dll" ]; then
+         cmd="$R__LD $R__SOFLAGS $R__LDFLAGS -o $dlldir/${name}.dll $R__OBJS \
+              $libdir/${name}.exp $R__EXTRA $syslibs"
       else
          if [ "$(bin/root-config --dicttype)" != "cint" ]; then
              needReflex="lib/libCintex.lib lib/libReflex.lib"
