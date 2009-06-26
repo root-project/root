21ed7f3d48a9ebc5a3f054512593b39c0267259c
Fons Rademakers <Fons.Rademakers@cern.ch>
2009-06-26 10:11:55 +0000

    when making src tar generate etc/svninfo.txt as it is used by the Win build (and will only be generated when building from an svn managed source tree, which is not the case for the source distributed).
    git-svn-id: http://root.cern.ch/svn/root/trunk@29241 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makedistsrc.sh
+++ build/unix/makedistsrc.sh
@@ -15,6 +15,11 @@ cd $EXPDIR
 #svn co http://root.cern.ch/svn/root/tags/$CURVERS root
 svn co http://root.cern.ch/svn/root/trunk root
 
+# generate etc/svninfo.txt
+cd root
+build/unix/svninfo.sh
+cd ..
+
 # remove .svn directories containing extra copy of the code
 find root -depth -name .svn -exec rm -rf {} \;
 
