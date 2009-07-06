be2da36ae3960384b0e69af14fc5c2ee066c7cfb
Philippe Canal <pcanal@fnal.gov>
2009-07-06 23:59:09 +0000

    test r29364 of the trunk (TBits::operator== fix for 2 TBits of different length)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1725 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/collection/runtbits.C
+++ root/collection/runtbits.C
@@ -6,6 +6,79 @@
 using namespace std;
 #endif
 
+bool testEqual() {
+   bool result = true;
+   
+   TBits bits1, bits2;
+   
+   bits1.SetBitNumber(10);
+   bits1.ResetAllBits();
+   
+   bits1.SetBitNumber(1);
+   bits1.SetBitNumber(2);
+   
+   bits2.SetBitNumber(1);
+   
+   TBits bits_and = bits1 & bits2;
+   
+   bits_and.Print();
+   bits2.Print();
+   
+   if ( bits_and == bits2) {
+      printf("ok\n");
+   } else {
+      result = false;
+      printf("bug\n");
+   }
+   if ( bits_and != bits2) {
+      result = false;
+      printf("bug\n");
+   } else {
+      printf("ok\n");
+   }
+   
+   if ( bits2 == bits_and) {
+      printf("ok\n");
+   } else {
+      result = false;
+      printf("bug\n");
+   }
+   if ( bits2 != bits_and) {
+      result = false;
+      printf("bug\n");
+   } else {
+      printf("ok\n");
+   }
+   
+   bits_and.SetBitNumber(10);
+   if ( bits_and == bits2) {
+      result = false;
+      printf("bug\n");
+   } else {
+      printf("ok\n");
+   }
+   if ( bits_and != bits2) {
+      printf("ok\n");
+   } else {
+      result = false;
+      printf("bug\n");
+   }
+   if ( bits2 == bits_and) {
+      result = false;
+      printf("bug\n");
+   } else {
+      printf("ok\n");
+   }
+   if ( bits2 != bits_and) {
+      printf("ok\n");
+   } else {
+      result = false;
+      printf("bug\n");
+   }
+   return result;
+}
+
+
 bool runtbits() {
    TBits a;  a.SetBitNumber(2,1);  a.SetBitNumber(12,1); 
    TBits b;  b.SetBitNumber(2,1);  b.SetBitNumber(15,1); 
@@ -33,5 +106,6 @@ bool runtbits() {
    //bs[12]= 1;
    cout << bs << endl;
 #endif
+   testEqual();
    return 0;
 }
