905044f4e67bcdeaf20bd674d2eda33067312e0b
Philippe Canal <pcanal@fnal.gov>
2009-08-21 06:09:37 +0000

    test the fix for Savannah 54641
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1752 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/treeproblem/runucharshow.C
@@ -0,0 +1,14 @@
+{
+   UChar_t x = 200;
+   TTree tree("tree","tree");
+   tree.Branch("x",&x,"x/b");
+   tree.Branch("y",&x,"y/B");
+   tree.Fill();
+   x = 250;
+   tree.Fill();
+   tree.Show(0);
+   tree.Show(1);
+   tree.Scan("x:y");
+   return 0;
+}
+
