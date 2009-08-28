36ecec445fc73281acb656ff30a0f54bd49af79c
Fons Rademakers <Fons.Rademakers@cern.ch>
2009-08-28 18:06:52 +0000

    formatting and typo in comment.
    git-svn-id: http://root.cern.ch/svn/root/trunk@29970 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf3d/gviz3d/Module.mk
+++ graf3d/gviz3d/Module.mk
@@ -8,24 +8,24 @@ MODDIR       := graf3d/$(MODNAME)
 MODDIRS      := $(MODDIR)/src
 MODDIRI      := $(MODDIR)/inc
 
-GVIZ3DDIR        := $(MODDIR)
-GVIZ3DDIRS       := $(GVIZ3DDIR)/src
-GVIZ3DDIRI       := $(GVIZ3DDIR)/inc
+GVIZ3DDIR    := $(MODDIR)
+GVIZ3DDIRS   := $(GVIZ3DDIR)/src
+GVIZ3DDIRI   := $(GVIZ3DDIR)/inc
 
-##### libRGL #####
-GVIZ3DL          := $(MODDIRI)/LinkDef.h
-GVIZ3DDS         := $(MODDIRS)/G__Gviz3d.cxx
-GVIZ3DDO         := $(GVIZ3DDS:.cxx=.o)
-GVIZ3DDH         := $(GVIZ3DDS:.cxx=.h)
+##### libGviz3d #####
+GVIZ3DL      := $(MODDIRI)/LinkDef.h
+GVIZ3DDS     := $(MODDIRS)/G__Gviz3d.cxx
+GVIZ3DDO     := $(GVIZ3DDS:.cxx=.o)
+GVIZ3DDH     := $(GVIZ3DDS:.cxx=.h)
 
-GVIZ3DH          := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
-GVIZ3DS          := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
-GVIZ3DO        := $(GVIZ3DS:.cxx=.o)
+GVIZ3DH      := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+GVIZ3DS      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
+GVIZ3DO      := $(GVIZ3DS:.cxx=.o)
 
-GVIZ3DDEP      := $(GVIZ3DO:.o=.d) $(GVIZ3DDO:.o=.d)
+GVIZ3DDEP    := $(GVIZ3DO:.o=.d) $(GVIZ3DDO:.o=.d)
 
-GVIZ3DLIB      := $(LPATH)/libGviz3d.$(SOEXT)
-GVIZ3DMAP      := $(GVIZ3DLIB:.$(SOEXT)=.rootmap)
+GVIZ3DLIB    := $(LPATH)/libGviz3d.$(SOEXT)
+GVIZ3DMAP    := $(GVIZ3DLIB:.$(SOEXT)=.rootmap)
 
 # used in the main Makefile
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(GVIZ3DH))
@@ -41,16 +41,16 @@ INCLUDEFILES += $(GVIZ3DDEP)
 include/%.h:    $(GVIZ3DDIRI)/%.h
 		cp $< $@
 
-$(GVIZ3DLIB):     $(GVIZ3DO) $(GVIZ3DDO) $(ORDER_) $(MAINLIBS) $(GVIZ3DLIBDEP)
+$(GVIZ3DLIB):   $(GVIZ3DO) $(GVIZ3DDO) $(ORDER_) $(MAINLIBS) $(GVIZ3DLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
 		   "$(SOFLAGS)" libGviz3d.$(SOEXT) $@ "$(GVIZ3DO) $(GVIZ3DDO)" \
 		   "$(GVIZ3DLIBEXTRA)"
 
-$(GVIZ3DDS):      $(GVIZ3DH) $(GVIZ3DL) $(ROOTCINTTMPDEP)
+$(GVIZ3DDS):    $(GVIZ3DH) $(GVIZ3DL) $(ROOTCINTTMPDEP)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(GVIZ3DH) $(GVIZ3DL)
 
-$(GVIZ3DMAP):     $(RLIBMAP) $(MAKEFILEDEP) $(GVIZ3DL)
+$(GVIZ3DMAP):   $(RLIBMAP) $(MAKEFILEDEP) $(GVIZ3DL)
 		$(RLIBMAP) -o $(GVIZ3DMAP) -l $(GVIZ3DLIB) \
 		   -d $(GVIZ3DLIBDEPM) -c $(GVIZ3DL)
 
