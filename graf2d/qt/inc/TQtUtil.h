21d49964021b736f4c0396e1bf6aa284bc7c6cbd
Rene Brun <Rene.Brun@cern.ch>
2009-08-28 04:48:12 +0000

    From Valeri Fine: more changes to fix dependencies
    git-svn-id: http://root.cern.ch/svn/root/trunk@29951 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/qt/inc/TQtUtil.h
+++ graf2d/qt/inc/TQtUtil.h
@@ -10,62 +10,54 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#ifndef ROOT_TQtUtil
-#define ROOT_TQtUtil
+#ifndef ROOT_TQtUTIL
+#define ROOT_TQtUTIL
 
 #include "TGQt.h"
-#include "TCanvas.h"
+#include "TVirtualPad.h"
 #include "TCanvasImp.h"
 
-#include "qpixmap.h"
-#include "qwidget.h"
+#include <QtGui/QPixmap>
+#include <QtGui/QWidget>
 
 //----------------------------------------
 //      Q: How to get Qt pointer:
 //----------------------------------------
-
+namespace  TQtUtil {
+///
+/// Returns QPixmap backend for the given TVirtualPad 
+///
 //_______________________________________
-inline QPixmap *padPixmap(TPad *pad)
+inline QPixmap *padPixmap(TVirtualPad *pad)
 {     return (QPixmap *)TGQt::iwid(pad->GetPixmapID());   }
-//_______________________________________
-inline QWidget *canvasWidget(TCanvas *c)
-{  return (QWidget *)TGQt::iwid(c->GetCanvasID()) ; }
-//_______________________________________
-inline QWidget *canvasWidget(TCanvasImp *c)
-{ return (QWidget *) TGQt::iwid(((TQtCanvasImp *)c)->GetCanvasImpID()); }
-//_______________________________________
-inline QWidget *mainWidget(TCanvas *c)
-{  return canvasWidget(c->GetCanvasImp());}
 
+///
+/// Returns QWidget backend for the given TCanvas
+///  if "c" is not a TCanvas returns zero 
+///
+//_______________________________________
+inline QWidget *canvasWidget(TVirtualPad *c)
+{  return (QWidget *)TGQt::iwid(c->GetCanvasID()) ; }
 //----------------------------------------
 // Q: Get WIN32/X11 handles:
 //    (see function above and Qt manual also)
 //----------------------------------------
+///
+/// Returns system depended backend handle 
+/// for the given TVirtualPad 
+///
 //_______________________________________
-inline HDC wigdetHdc(TPad *pad)
+inline unsigned long  wigdetHdc(TVirtualPad *pad)
 {  return padPixmap(pad)->handle(); }
-//_______________________________________
-inline HDC wigdetHdc(TCanvas *c)
-{ return canvasWidget(c)->handle(); }
-//_______________________________________
-inline HDC wigdetHdc(TCanvasImp *c)
-{ return canvasWidget(c)->handle(); }
 
-#ifdef WIN32
-//_______________________________________
-inline HWND hwndWin32(TCanvas *c)
-{  return canvasWidget(c)->winId(); }
-//_______________________________________
-inline HWND hwndWin32(TCanvasImp *c)
-{ return canvasWidget(c)->winId(); }
-#else
-//_______________________________________
-inline Ulong_t hwndWin32(TCanvas *c)
-{  return canvasWidget(c)->winId(); }
-//_______________________________________
-inline Ulong_t hwndWin32(TCanvasImp *c)
-{ return canvasWidget(c)->winId(); }
-#endif
+///
+/// Returns system depended backend handle 
+/// for the given TCanvas
+///  if "c" is not a TCanvas returns zero 
 
+//_______________________________________
+inline unsigned long  hwndWin32(TVirtualPad *c)
+{  return canvasWidget(c)->winId(); }
+};
 #endif
 
