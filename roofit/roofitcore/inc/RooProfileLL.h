d09292bfd14ff03d6d2a5e1f24ea437131606a09
Wouter Verkerke <verkerke@nikhef.nl>
2009-09-24 15:03:35 +0000

    o  RooProfileLL
            - Add forgotten accessor to internal minuit object
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@30407 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/roofitcore/inc/RooProfileLL.h
+++ roofit/roofitcore/inc/RooProfileLL.h
@@ -31,6 +31,7 @@ public:
   void setAlwaysStartFromMin(Bool_t flag) { _startFromMin = flag ; }
   Bool_t alwaysStartFromMin() const { return _startFromMin ; }
 
+  RooMinuit* minuit() { return _minuit ; }
   RooAbsReal& nll() { return const_cast<RooAbsReal&>(_nll.arg()) ; }
   const RooArgSet& bestFitParams() const ;
 
