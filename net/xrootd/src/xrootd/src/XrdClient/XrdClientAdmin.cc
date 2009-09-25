2d74319274a5c55e56c8b2c31d840772242758c7
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2009-09-25 09:52:53 +0000

    Import from the trunk a crucial fix for bulk prepare requests
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-24-00-patches@30431 27541ba8-7e3a-0410-8455-c3a389f83636

--- net/xrootd/src/xrootd/src/XrdClient/XrdClientAdmin.cc
+++ net/xrootd/src/xrootd/src/XrdClient/XrdClientAdmin.cc
@@ -791,10 +791,11 @@ bool XrdClientAdmin::Prepare(vecString vs, kXR_char option, kXR_char prty)
    }
 
 
-   for (int i = 0; i < vs.GetSize()+50; i++) {
+   for (int i = 0; i < vs.GetSize()+50; i+=50) {
      joinStrings(buf, vs, i, i+49);
 
      if (!Prepare(buf.c_str(), option, prty)) return false;
+     buf = "";
    }
 
    return true;
