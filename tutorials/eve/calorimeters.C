fb9e528ee52e8710095fd71551d93ba621794606
Matevz Tadel <matevz.tadel@cern.ch>
2009-09-26 13:27:50 +0000

    calorimiters.C: Simplify layout. Move axample of TEveCaloDataVec class to macro cms_calo_detail.C.
    cms_calo_detail.C:
    Create calo data by reading existing historam and add irregular towers to demostrate usage of TEveCalDataVec class.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@30487 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/eve/calorimeters.C
+++ tutorials/eve/calorimeters.C
@@ -7,7 +7,7 @@
 
 const char* histFile = "http://amraktad.web.cern.ch/amraktad/cms_calo_hist.root";
 
-void calorimeters(Bool_t hdata = kTRUE)
+void calorimeters()
 {
    gSystem->IgnoreSignal(kSigSegmentationViolation, true);
 
@@ -19,139 +19,64 @@ void calorimeters(Bool_t hdata = kTRUE)
    TFile* hf = TFile::Open(histFile, "CACHEREAD");
    TH2F* ecalHist = (TH2F*)hf->Get("ecalLego");
    TH2F* hcalHist = (TH2F*)hf->Get("hcalLego");
-   TEveCaloData* data = 0;
-   if (hdata)
-   {
-      TEveCaloDataHist* hd = new TEveCaloDataHist();
-      Int_t slice;
-      slice = hd->AddHistogram(ecalHist);
-      hd->RefSliceInfo(slice).Setup("ECAL", 0.3, 41);
-      slice = hd->AddHistogram(hcalHist);
-      hd->RefSliceInfo(slice).Setup("HCAL", 0.1, 46);
-      data = hd;
-   }
-   else
-   {
-      data = MakeVecData(ecalHist, hcalHist);
-   }
+
+   TEveCaloDataHist* data = new TEveCaloDataHist();
+   data->AddHistogram(ecalHist);
+   data->RefSliceInfo(0).Setup("ECAL", 0.3, 41);
+   data->AddHistogram(hcalHist);
+   data->RefSliceInfo(1).Setup("HCAL", 0.1, 46);
+
    // axis attrib
    data->GetEtaBins()->SetTitleFont(120);
    data->GetEtaBins()->SetTitle("h");
    data->GetPhiBins()->SetTitleFont(120);
    data->GetPhiBins()->SetTitle("f");
 
-   TEveCalo3D* calo3d = MakeCalo3D(data, gEve->GetDefaultGLViewer(), gEve->GetEventScene());
+   MakeCaloLego(data);
 
-   // window mng
-   TEveWindowSlot  *slot  = 0;
-   TEveWindowFrame *frame = 0;
-   TEveViewer *v = 0;
+   BuildProjectedView(data);
 
-   // first tab
-   {
-      slot = TEveWindow::CreateWindowInTab(gEve->GetBrowser()->GetTabRight());
-      TEveWindowPack* packH = slot->MakePack();
-      packH->SetElementName("Projections");
-      packH->SetHorizontal();
-      packH->SetShowTitleBar(kFALSE);
-
-      slot = packH->NewSlot();
-      v = new TEveViewer("RPhi Viewer");
-      v->SpawnGLEmbeddedViewer();
-      slot->ReplaceWindow(v);
-      gEve->GetViewers()->AddElement(v);
-      TEveScene*  sP = gEve->SpawnNewScene("RPhi Scene");
-      v->AddScene(sP);
-      MakeCalo2D(calo3d, v->GetGLViewer(), sP, TEveProjection::kPT_RPhi);
-
-      slot = packH->NewSlot();
-      TEveWindowPack* pack1 = slot->MakePack();
-
-      slot = pack1->NewSlot();
-      v = new TEveViewer("3D Viewer");
-      v->SpawnGLEmbeddedViewer();
-      slot->ReplaceWindow(v);
-      gEve->GetViewers()->AddElement(v);
-      TEveScene*  s3 = gEve->SpawnNewScene("3D Scene");
-      v->AddScene(s3);
-      MakeCalo3D(data, v->GetGLViewer(), s3);
-
-      slot = pack1->NewSlot();
-      v = new TEveViewer("Rho Z");
-      v->SpawnGLEmbeddedViewer();
-      slot->ReplaceWindow(v);
-      gEve->GetViewers()->AddElement(v);
-      TEveScene*  s = gEve->SpawnNewScene("RhoZ Scene");
-      v->AddScene(s);
-      MakeCalo2D(calo3d, v->GetGLViewer(), s, TEveProjection::kPT_RhoZ);
-   }
-
-   // second tab
-   {
-      // eve calrimeter lego
-      slot =  TEveWindow::CreateWindowInTab(gEve->GetBrowser()->GetTabRight());
-      TEveWindowPack* pack =  slot->MakePack();
-      pack->SetShowTitleBar(kFALSE);
-      pack->SetElementName("Lego");
-      pack->SetHorizontal();
-      // eve calorimiter lego
-      slot = pack->NewSlotWithWeight(5);
-      v = new TEveViewer("LegoViewer");
-      v->SpawnGLViewer(gEve->GetEditor());
-      slot->ReplaceWindow(v);
-      gEve->GetViewers()->AddElement(v);
-      TEveScene*  sL = gEve->SpawnNewScene("Projected");
-      v->AddScene(sL);
-      MakeCaloLego(data, v->GetGLViewer(), sL);
-
-      // root 2D histogram
-      slot = pack->NewSlotWithWeight(3);
-      TEveWindowPack* pack3 =  slot->MakePack();
-      pack3->SetShowTitleBar(kFALSE);
-      ecalHist->SetFillColor(data->GetSliceColor(0));
-      hcalHist->SetFillColor(data->GetSliceColor(1));
-      TCanvas* can;
-      THStack *hs;
-      slot = pack3->NewSlot();
-      slot->StartEmbedding();
-      can = new TCanvas("3D Root Canvas");
-      if (can->GetShowEditor()) can->ToggleEditor();
-      can->cd();
-      THStack *hs = new THStack("hs","Stacked histograms");
-      hs->Add(ecalHist);;
-      hs->Add(hcalHist);
-      hs->Draw("lego1");
-      slot->StopEmbedding();
-      slot->SetShowTitleBar(kFALSE);
-     
-      slot = pack3->NewSlot();
-      slot->StartEmbedding();
-      can = new TCanvas("Top view Canvas");
-      if (can->GetShowEditor())can->ToggleEditor();
-      can->cd();
-      THStack *hs = new THStack("hsss","Stacked 1D histogssrams");
-      hs->Add(ecalHist);;
-      hs->Add(hcalHist);
-      hs->Draw("iso");
-      slot->StopEmbedding();
-      slot->SetShowTitleBar(kFALSE);
-   }
-
-   gEve->GetBrowser()->GetTabRight()->SetTab(1);
    gEve->Redraw3D(1);
 }
 
 //______________________________________________________________________________
-void MakeCaloLego(TEveCaloData* data, TGLViewer* v, TEveScene* s)
+void BuildProjectedView(TEveCaloData* data)
 {
-   // eta-phi histogram
+   // Create a new tab with 3D calorimiter
+   //  and projected (RPhi/RhoZ) calorimiter.
 
-   // histogram
+   TEveWindowSlot* slot;
+
+   slot = TEveWindow::CreateWindowInTab(gEve->GetBrowser()->GetTabRight());
+   TEveWindowPack* packH = slot->MakePack();
+   packH->SetElementName("Projections");
+   packH->SetHorizontal();
+   packH->SetShowTitleBar(kFALSE);
+
+   TEveWindowSlot* slotLeft = packH->NewSlot();
+   slot = packH->NewSlot();
+
+   TEveWindowPack* pack1 = slot->MakePack();
+   TEveWindowSlot* slotRightTop = pack1->NewSlot();
+   slotRightBottom = pack1->NewSlot();
+
+   // build scenes
+   TEveCalo3D* calo3d = MakeCalo3D(data, slotRightTop);
+   MakeCalo2D(calo3d, slotLeft, TEveProjection::kPT_RPhi);
+   MakeCalo2D(calo3d, slotRightBottom, TEveProjection::kPT_RhoZ);
+}
+
+//______________________________________________________________________________
+void MakeCaloLego(TEveCaloData* data)
+{
+   // Eta-phi lego view.
+
+   TGLViewer* v = gEve->GetDefaultGLViewer();
+   TEveScene* s = gEve->GetEventScene();
+   gStyle->SetPalette(1, 0);
+
    TEveCaloLego* lego = new TEveCaloLego(data);
    s->AddElement(lego);
-
-   gStyle->SetPalette(1, 0);
-   Bool_t usePalette = kFALSE;
    lego->Set2DMode(TEveCaloLego::kValSize);
 
    // lego->SetAutoRebin(kFALSE);
@@ -161,7 +86,7 @@ void MakeCaloLego(TEveCaloData* data, TGLViewer* v, TEveScene* s)
    Float_t sc = TMath::TwoPi();
    lego->RefMainTrans().SetScale(sc, sc, sc);
 
-   // add overlay lego draws scales in 2D
+   // draws scales
    TEveCaloLegoOverlay* overlay = new TEveCaloLegoOverlay();
    v->AddOverlayElement(overlay);
    overlay->SetCaloLego(lego);
@@ -172,9 +97,16 @@ void MakeCaloLego(TEveCaloData* data, TGLViewer* v, TEveScene* s)
 }
 
 //______________________________________________________________________________
-TEveCalo3D* MakeCalo3D(TEveCaloData* data, TGLViewer* v, TEveScene *s )
+TEveCalo3D* MakeCalo3D(TEveCaloData* data, TEveWindowSlot* slot)
 {
-   // 3D towers
+   // 3D catersian view. 
+
+   TEveViewer* v = new TEveViewer("3D Viewer");
+   v->SpawnGLEmbeddedViewer();
+   slot->ReplaceWindow(v);
+   gEve->GetViewers()->AddElement(v);
+   TEveScene*  s = gEve->SpawnNewScene("3D Scene");
+   v->AddScene(s);
 
    TEveCalo3D* calo3d = new TEveCalo3D(data);
    calo3d->SetName("Calorimter 3D");
@@ -183,16 +115,20 @@ TEveCalo3D* MakeCalo3D(TEveCaloData* data, TGLViewer* v, TEveScene *s )
    s->AddElement(calo3d);
 
    gEve->AddToListTree(calo3d, kTRUE);
-
    return calo3d;
 }
 
 //______________________________________________________________________________
-void MakeCalo2D(TEveCalo3D* calo3d, TGLViewer *v, TEveScene *s, TEveProjection::EPType_e t)
+void MakeCalo2D(TEveCalo3D* calo3d, TEveWindowSlot* slot, TEveProjection::EPType_e t)
 {
-   // projected calorimeter
+   // Projected calorimeter.
 
-   v->SetCurrentCamera(TGLViewer::kCameraOrthoXOY);
+   TEveViewer* v = new TEveViewer("2D Viewer");
+   v->SpawnGLEmbeddedViewer();
+   slot->ReplaceWindow(v);
+   gEve->GetViewers()->AddElement(v);
+   TEveScene*  s = gEve->SpawnNewScene("Scene");
+   v->AddScene(s);
 
    TEveProjectionManager* mng = new TEveProjectionManager();
    mng->SetProjection(t);
@@ -202,36 +138,8 @@ void MakeCalo2D(TEveCalo3D* calo3d, TGLViewer *v, TEveScene *s, TEveProjection::
    TEveCalo2D* calo2d = (TEveCalo2D*) mng->ImportElements(calo3d);
    s->AddElement(calo2d);
 
+   v->GetGLViewer()->SetCurrentCamera(TGLViewer::kCameraOrthoXOY);
+
    gEve->AddToListTree(mng, kTRUE);
    gEve->AddToListTree(calo2d, kTRUE);
-}
-
-//______________________________________________________________________________
-TEveCaloDataVec* MakeVecData(TH2* h1, TH2* h2)
-{
-   // Example how to fill data when bins can be iregular.
-
-  TEveCaloDataVec* data = new TEveCaloDataVec(2);
-
-  data->RefSliceInfo(0).Setup("ECAL", 0.3, kRed);
-  data->RefSliceInfo(1).Setup("HCAL", 0.1, kYellow);
-
-  TAxis *ax =  h1->GetXaxis();
-  TAxis *ay =  h1->GetYaxis();
-  for(Int_t i=1; i<=ax->GetNbins(); i++)
-  {
-    for(Int_t j=1; j<=ay->GetNbins(); j++)
-    {
-      data->AddTower(ax->GetBinLowEdge(i), ax->GetBinUpEdge(i),
-		     ay->GetBinLowEdge(j), ay->GetBinUpEdge(j));
-
-      data->FillSlice(0, h1->GetBinContent(i, j));
-      data->FillSlice(1, h2->GetBinContent(i, j));
-    }
-  }
-
-  data->SetAxisFromBins();
-  data->DataChanged();
-
-  return data;
 }
--- tutorials/eve/cms_calo_detail.C
+++ tutorials/eve/cms_calo_detail.C
@@ -1,148 +1,93 @@
-// @(#)root/eve:$Id: triangleset.C 26568 2008-12-01 20:55:50Z matevz $
+// @(#)root/eve:$Id:  26568 2008-12-01 20:55:50Z matevz $
 // Author: Alja Mrak-Tadel
 
 // Calorimeter detailed view by using TEveCaloDataVec as data-source.
+// Demonstrantes how to plot calorimiter data with irregular bins.
 
-#if defined(__CINT__) && !defined(__MAKECINT__)
-
-{
-   gSystem->CompileMacro("cms_calo_detail.C");
-   cms_calo_detail();
-}
-
-#else
-
-#include <TEveManager.h>
-#include <TEveTrans.h>
-#include <TEveCalo.h>
-#include <TEveCaloData.h>
-#include <TEveCaloLegoOverlay.h>
-#include <TEveLegoEventHandler.h>
-
-#include <TEveStraightLineSet.h>
-
-#include <TGLViewer.h>
-#include <TGLOverlayButton.h>
-
-#include <TAxis.h>
-
-#ifdef WIN32
-#include <Windows4root.h>
-#pragma comment(lib, "OpenGL32.lib")
-#endif
-
-
-class ButtFaker : public TGLOverlayButton
-{
-   ButtFaker(const ButtFaker&);            // Not implemented
-   ButtFaker& operator=(const ButtFaker&); // Not implemented
-
-public:
-   TEveCaloLego* fLego;
-
-   ButtFaker(TGLViewerBase *parent) :
-      TGLOverlayButton(parent, "FlipColors", 10, 200, 80, 16),
-      fLego(0)
-   {
-   }
-
-   virtual ~ButtFaker() {}
-
-   virtual void Clicked(TGLViewerBase*)
-   {
-      TEveCaloData* data = fLego->GetData();
-      if (data->GetSliceColor(0) == kRed)
-      {
-         fLego->SetDataSliceColor(1, kRed);
-         fLego->SetDataSliceColor(0, kYellow);
-      }
-      else
-      {
-         fLego->SetDataSliceColor(0, kRed);
-         fLego->SetDataSliceColor(1, kYellow);
-      }
-      gEve->Redraw3D();
-   } 
-
-   ClassDef(ButtFaker,0);
-};
-
+const char* histFile = "http://amraktad.web.cern.ch/amraktad/cms_calo_hist.root";
 
 void cms_calo_detail()
 {
    TEveManager::Create();
 
-   TGLViewer* v = gEve->GetDefaultGLViewer(); // Default
-   v->SetCurrentCamera(TGLViewer::kCameraOrthoXOY);
-   TEveLegoEventHandler* eh = new TEveLegoEventHandler("Lego", (TGWindow*)v->GetGLWidget(), (TObject*)v);
-   eh->fMode = TEveLegoEventHandler::kLocked;
-   v->SetEventHandler(eh);
-
    // data
-
-   TEveCaloDataVec* data = new TEveCaloDataVec(2);
-
-   data->RefSliceInfo(0).Setup("ECAL", 0.3, kRed);
-   data->RefSliceInfo(1).Setup("HCAL", 0.1, kYellow);
-
-   data->AddTower(0.12, 0.14, 0.45, 0.47);
-   data->FillSlice(0, 12);
-   data->FillSlice(1, 3);
-
-   data->AddTower(0.125, 0.145, 0.43, 0.45);
-   data->FillSlice(0, 4);
-   data->FillSlice(1, 7);
-
-   data->AddTower(0.10, 0.12, 0.45, 0.47);
-   data->FillSlice(0, 6);
-   data->FillSlice(1, 0);
-
-   data->SetAxisFromBins();
-   // set eta, phi axis title with symbol.ttf font
-   data->GetEtaBins()->SetTitle("X[cm]");
-   data->GetEtaBins()->SetTitleSize(0.1);
-   data->GetPhiBins()->SetTitle("Y[cm]");
-   data->GetPhiBins()->SetTitleColor(kGreen);
-   data->DataChanged();
+   TEveCaloDataVec* data = MakeVecData(10);
 
    // lego
    TEveCaloLego* lego = new TEveCaloLego(data);
-   // move to real-world coordinates
-   Double_t em, eM, pm, pM;
-   data->GetEtaLimits(em, eM);
-   data->GetPhiLimits(pm, pM);
-   lego->SetEta(em, eM);
-   lego->SetPhiWithRng((pm+pM)*0.5, pM-pm);
-   Double_t sc = ((eM - em) < (pM - pm)) ? (eM - em) : (pM - pm);
-   lego->InitMainTrans();
-   lego->RefMainTrans().SetScale(sc, sc, sc);
-   lego->RefMainTrans().SetPos((eM+em)*0.5, (pM+pm)*0.5, 0);
-
+   lego->SetElementName("Irregualar Lego");
    lego->SetAutoRebin(kFALSE);
-   lego->SetPlaneColor(kBlue-5);
-   lego->SetFontColor(kGray);
-   lego->Set2DMode(TEveCaloLego::kValSize);
-   lego->SetName("Calo Detail");
    gEve->AddElement(lego);
 
-   // add line to test real world coordinates
-   TEveStraightLineSet* ls = new TEveStraightLineSet();
-   ls->AddLine( em, pm, 0,  eM, pM, 0);
-   ls->SetLineColor(kBlue);
-   gEve->AddElement(ls);
+   TGLViewer* v = gEve->GetDefaultGLViewer(); // Default
+   v->SetCurrentCamera(TGLViewer::kCameraOrthoXOY);
+   TEveLegoEventHandler* eh = new TEveLegoEventHandler((TGWindow*)v->GetGLWidget(), (TObject*)v, lego);
+   v->SetEventHandler(eh);  
 
-   // overlay lego
-   TEveCaloLegoOverlay* overlay = new TEveCaloLegoOverlay();
-   overlay->SetCaloLego(lego);
-   v->AddOverlayElement(overlay);
-   gEve->AddElement(overlay);
-
-   // button overlay
-   ButtFaker* legend = new ButtFaker(v);
-   legend->fLego = lego;
-   v->AddOverlayElement(legend);
-
+   lego->Set2DMode(TEveCaloLego::kValSize);
    gEve->Redraw3D(kTRUE);
 }
 
-#endif
+//______________________________________________________________________________
+TEveCaloDataVec* MakeVecData(Int_t ncells=0)
+{
+   // Example how to fill data when bins can be iregular.
+   // If ncells = 0 (default) whole histogram is taken,
+   // otherwise just ncells cells around the maximum.
+
+   TFile::SetCacheFileDir(".");
+   TFile* hf = TFile::Open(histFile, "CACHEREAD");
+   TH2F* h1 = (TH2F*)hf->Get("ecalLego");
+   TH2F* h2 = (TH2F*)hf->Get("hcalLego");
+
+   TEveCaloDataVec* data = new TEveCaloDataVec(3);
+
+   data->RefSliceInfo(0).Setup("ECAL", 0.3, kRed);
+   data->RefSliceInfo(1).Setup("HCAL", 0.1, kYellow);
+   data->RefSliceInfo(2).Setup("OTHER", 0, kCyan);
+
+   TAxis *ax =  h1->GetXaxis();
+   TAxis *ay =  h1->GetYaxis();
+
+   Int_t xm = 1, xM = ax->GetNbins();
+   Int_t ym = 1, yM = ay->GetNbins();
+   if (ncells != 0)
+   {
+      Int_t cx, cy, cz;
+      h1->GetMaximumBin(cx, cy, cz);
+      xm = TMath::Max(xm, cx-ncells);
+      xM = TMath::Min(xM, cx+ncells);
+      ym = TMath::Max(ym, cy-ncells);
+      yM = TMath::Min(yM, cy+ncells);
+   }
+
+   for(Int_t i=xm; i<=xM; i++)
+   {
+      for(Int_t j=ym; j<=yM; j++)
+      {
+         data->AddTower(ax->GetBinLowEdge(i), ax->GetBinUpEdge(i),
+                        ay->GetBinLowEdge(j), ay->GetBinUpEdge(j));
+
+         data->FillSlice(0, h1->GetBinContent(i, j));
+         data->FillSlice(1, h2->GetBinContent(i, j));
+      }
+   }      
+
+   // Add irregularities
+   //
+   Float_t off = 0.02;
+   i = cx + 5; j = cy+2;
+   data->AddTower(ax->GetBinLowEdge(i) -off, ax->GetBinUpEdge(i)+off,
+                  ay->GetBinLowEdge(j) -off, ay->GetBinUpEdge(j)+off);
+   data->FillSlice(2, 2.);
+
+   i = cx-3; j = cy+3;
+   data->AddTower(ax->GetBinLowEdge(i) -off, ax->GetBinUpEdge(i)+off,
+                  ay->GetBinLowEdge(j) -off, ay->GetBinUpEdge(j)+off);
+   data->FillSlice(2, 12.);
+   
+   data->DataChanged();
+   data->SetAxisFromBins(0.001, 0.001);
+   return data;
+}
+
