74580109ba8be1b36ba979ec5b61ab12fdf24b27
Rene Brun <Rene.Brun@cern.ch>
2009-10-14 12:15:47 +0000

    Remove linearIO from the list of files to distribute
    git-svn-id: http://root.cern.ch/svn/root/trunk@30723 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/distfilelist.sh
+++ build/unix/distfilelist.sh
@@ -39,7 +39,7 @@ WILDCARDS="LICENSE README bin \
    include lib man cint/cint/include tutorials \
    cint/cint/lib cint/cint/stl geom/gdml/*.py \
    test/*.cxx test/*.h test/Makefile* test/README \
-   test/*.C test/*.sh test/dt_Makefile test/linearIO.root \
+   test/*.C test/*.sh test/dt_Makefile \
    test/RootShower/*.h test/RootShower/*.cxx \
    test/RootShower/*.rc test/RootShower/*.ico \
    test/RootShower/*.png test/RootShower/Makefile* \
