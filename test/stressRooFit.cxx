dd0e5bb4d0dbda50c6752e468903ebc18abca40c
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2009-11-27 13:24:16 +0000

    From Wouter: Disable the writing of the CERF cache file in stressRooFit (solves a problem on Windows in the nightlies)
    git-svn-id: http://root.cern.ch/svn/root/trunk@31449 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressRooFit.cxx
+++ test/stressRooFit.cxx
@@ -23,6 +23,7 @@
 #include "RooHist.h"
 #include "RooRandom.h"
 #include "RooTrace.h"
+#include "RooMath.h"
 #include <string>
 #include <list>
 #include <iostream>
@@ -872,6 +873,10 @@ int main(int argc,const char *argv[])
 
     cout << "stressRooFit: WARNING running in write mode, but reference file is web file, writing local file instead: " << refFileName << endl ;
   }
+
+  // Disable caching of complex error function calculation, as we don't 
+  // want to write out the cache file as part of the validation procedure
+  RooMath::cacheCERF(kFALSE) ;
 
   gBenchmark = new TBenchmark();
   stressRooFit(refFileName.c_str(),doWrite,doVerbose,oneTest,dryRun,doDump);  
