9a3aa84cb8406c16748cb7a4eec17c15a2562679
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2009-11-30 18:18:56 +0000

    Use Printf where relevant
    git-svn-id: http://root.cern.ch/svn/root/trunk@31473 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proof/src/TDSet.cxx
+++ proof/proof/src/TDSet.cxx
@@ -188,16 +188,12 @@ void TDSetElement::Print(Option_t *opt) const
    // Print a TDSetElement. When option="a" print full data.
 
    if (opt && opt[0] == 'a') {
-      cout << IsA()->GetName()
-           << " file=\"" << GetName()
-           << "\" dir=\"" << fDirectory
-           << "\" obj=\"" << GetTitle()
-           << "\" first=" << fFirst
-           << " num=" << fNum
-           << " msd=\"" << fMsd
-           << "\"" << endl;
-   } else
-      cout << "\tLFN: " << GetName() << endl;
+      Printf("%s file=\"%s\" dir=\"%s\" obj=\"%s\" first=%lld num=%lld msd=\"%s\"",
+             IsA()->GetName(), GetName(), fDirectory.Data(), GetTitle(),
+             fFirst, fNum, fMsd.Data());
+   } else {
+      Printf("\tLFN: %s", GetName());
+   }
 }
 
 //______________________________________________________________________________
@@ -822,9 +818,8 @@ void TDSet::Print(const Option_t *opt) const
 {
    // Print TDSet basic or full data. When option="a" print full data.
 
-   cout <<"OBJ: " << IsA()->GetName() << "\ttype " << GetName() << "\t"
-        << fObjName << "\tin " << GetTitle()
-        << "\telements " << GetListOfElements()->GetSize() << endl;
+   Printf("OBJ: %s\ttype %s\t%s\tin %s\telements %d", IsA()->GetName(), GetName(),
+          fObjName.Data(), GetTitle(), GetListOfElements()->GetSize());
 
    if (opt && opt[0] == 'a') {
       TIter next(GetListOfElements());
