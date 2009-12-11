f99c007601095dca885ccbe77faab237f19b7e7e
Philippe Canal <pcanal@fnal.gov>
2009-12-11 11:38:40 +0000

    try twice the performance test before really failing
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1844 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/io/perf/slowreading/trytwice.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+callroot=$1
+library=$2
+file=$3
+log=$4
+firstlog=first.attempt.$log
+
+if [ "x$CMDECHO" != "x@" ] ; then set -x; fi
+
+$callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)" > $log 2>&1 || ( mv $log $firstlog ; $callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)"  > $log 2>&1 )
+exit $?
