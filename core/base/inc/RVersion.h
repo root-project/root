90fb12cff3c3761839163f74988351b93ea5661b
Rene Brun <Rene.Brun@cern.ch>
2009-12-14 17:07:24 +0000

    Stamp production release 5.26/00
    git-svn-id: http://root.cern.ch/svn/root/trunk@31881 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.25/05"
-#define ROOT_RELEASE_DATE "Nov 24 2009"
-#define ROOT_RELEASE_TIME "11:57:38"
-#define ROOT_SVN_REVISION 31401
+#define ROOT_RELEASE "5.26/00"
+#define ROOT_RELEASE_DATE "Dec 14 2009"
+#define ROOT_RELEASE_TIME "15:43:39"
+#define ROOT_SVN_REVISION 31878
 #define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 334085
+#define ROOT_VERSION_CODE 334336
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
