b6be56a2b8a2edc287f8e36aeecc63a59114a2c8
Olivier Couet <Olivier.Couet@cern.ch>
2010-01-08 16:41:18 +0000

    - Draw clones of the graphs to avoid deletions in case the 1st   pad is redrawn.
    git-svn-id: http://root.cern.ch/svn/root/trunk@31981 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/hist/ContourList.C
+++ tutorials/hist/ContourList.C
@@ -7,28 +7,27 @@
 //   Modified by O.Couet (Nov. 26, 2004)
 
 TCanvas *ContourList(){
- 
-   const Double_t PI = TMath::Pi(); 
-    
+
+   const Double_t PI = TMath::Pi();
+
    TCanvas* c = new TCanvas("c","Contour List",0,0,600,600);
    c->SetRightMargin(0.15);
    c->SetTopMargin(0.15);
- 
+
    Int_t i, j, TotalConts;
-    
+
    Int_t nZsamples   = 80;
    Int_t nPhiSamples = 80;
 
-   Double_t HofZwavelength = 4.0;       // 4 meters 
+   Double_t HofZwavelength = 4.0;       // 4 meters
    Double_t dZ             =  HofZwavelength/(Double_t)(nZsamples - 1);
    Double_t dPhi           = 2*PI/(Double_t)(nPhiSamples - 1);
- 
+
    TArrayD z(nZsamples);
    TArrayD HofZ(nZsamples);
    TArrayD phi(nPhiSamples);
    TArrayD FofPhi(nPhiSamples);
 
-   
    // Discretized Z and Phi Values
    for ( i = 0; i < nZsamples; i++) {
       z[i] = (i)*dZ - HofZwavelength/2.0;
@@ -36,27 +35,27 @@ TCanvas *ContourList(){
    }
 
    for(Int_t i=0; i < nPhiSamples; i++){
-      phi[i] = (i)*dPhi;      
-      FofPhi[i] = sin(phi[i]); 
+      phi[i] = (i)*dPhi;
+      FofPhi[i] = sin(phi[i]);
    }
 
    // Create Histogram
-   TH2D *HistStreamFn = new TH2D("HstreamFn", 
-   "#splitline{Histogram with negative and positive contents. Six contours are defined.}{It is plotted with options CONT LIST to retrieve the contours points in TGraphs}", 
+   TH2D *HistStreamFn = new TH2D("HstreamFn",
+   "#splitline{Histogram with negative and positive contents. Six contours are defined.}{It is plotted with options CONT LIST to retrieve the contours points in TGraphs}",
    nZsamples, z[0], z[nZsamples-1], nPhiSamples, phi[0], phi[nPhiSamples-1]);
-     
+
    // Load Histogram Data
    for (Int_t i = 0; i < nZsamples; i++) {
       for(Int_t j = 0; j < nPhiSamples; j++){
          HistStreamFn->SetBinContent(i,j, HofZ[i]*FofPhi[j]);
-      } 
+      }
    }
 
-   gStyle->SetPalette(1); 
+   gStyle->SetPalette(1);
    gStyle->SetOptStat(0);
    gStyle->SetTitleW(0.99);
    gStyle->SetTitleH(0.08);
-   
+
    Double_t contours[6];
    contours[0] = -0.7;
    contours[1] = -0.5;
@@ -64,21 +63,22 @@ TCanvas *ContourList(){
    contours[3] =  0.1;
    contours[4] =  0.4;
    contours[5] =  0.8;
-     
-   HistStreamFn->SetContour(6, contours); 
- 
+
+   HistStreamFn->SetContour(6, contours);
+
    // Draw contours as filled regions, and Save points
    HistStreamFn->Draw("CONT Z LIST");
    c->Update(); // Needed to force the plotting and retrieve the contours in TGraphs
-    
+
    // Get Contours
    TObjArray *conts = (TObjArray*)gROOT->GetListOfSpecials()->FindObject("contours");
    TList* contLevel = NULL;
-   TGraph* curv     = NULL;    
+   TGraph* curv     = NULL;
+   TGraph* gc       = NULL;
+
    Int_t nGraphs    = 0;
    Int_t TotalConts = 0;
-    
-   
+
    if (conts == NULL){
       printf("*** No Contours Were Extracted!\n");
       TotalConts = 0;
@@ -88,15 +88,15 @@ TCanvas *ContourList(){
    }
 
    printf("TotalConts = %d\n", TotalConts);
-        
-   for(i = 0; i < TotalConts; i++){       
+
+   for(i = 0; i < TotalConts; i++){
       contLevel = (TList*)conts->At(i);
       printf("Contour %d has %d Graphs\n", i, contLevel->GetSize());
       nGraphs += contLevel->GetSize();
    }
-    
+
    nGraphs = 0;
-    
+
    TCanvas* c1 = new TCanvas("c1","Contour List",610,0,600,600);
    c1->SetTopMargin(0.15);
    TH2F *hr = new TH2F("hr",
@@ -108,8 +108,8 @@ TCanvas *ContourList(){
    TLatex l;
    l.SetTextSize(0.03);
    char val[20];
-   
-   for(i = 0; i < TotalConts; i++){       
+
+   for(i = 0; i < TotalConts; i++){
       contLevel = (TList*)conts->At(i);
       if (i<3) z0 = contours[2-i];
       else     z0 = contours[i];
@@ -123,10 +123,15 @@ TCanvas *ContourList(){
          if (z0>0) curv->SetLineColor(kBlue);
          nGraphs ++;
          printf("\tGraph: %d  -- %d Elements\n", nGraphs,curv->GetN());
-         curv->Draw("C");
+
+	 // Draw clones of the graphs to avoid deletions in case the 1st
+	 // pad is redrawn.
+         gc = (TGraph*)curv->Clone();
+         gc->Draw("C");
+
          sprintf(val,"%g",z0);
          l.DrawLatex(x0,y0,val);
-         curv = (TGraph*)contLevel->After(curv); // Get Next graph 
+         curv = (TGraph*)contLevel->After(curv); // Get Next graph
       }
    }
    c1->Update();
@@ -143,14 +148,14 @@ Double_t SawTooth(Double_t x, Double_t WaveLen){
 // WaveLen, symmetric about x = 0, and with amplitude = 1. Each segment
 // is 1/4 of the wavelength.
 //
-//           |  
+//           |
 //      /\   |
 //     /  \  |
 //    /    \ |
-//   /      \          
+//   /      \
 //  /--------\--------/------------
 //           |\      /
-//           | \    / 
+//           | \    /
 //           |  \  /
 //           |   \/
 //
