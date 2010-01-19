35aeb8e528329add774be812523e01536c88407c
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-01-19 11:23:36 +0000

    make version v5.26.00.a.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-26-00-patches@32051 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,11 +14,11 @@
  *
 */
 
-#define ROOT_RELEASE "5.26/00"
-#define ROOT_RELEASE_DATE "Dec 14 2009"
-#define ROOT_RELEASE_TIME "15:43:39"
-#define ROOT_SVN_REVISION 31878
-#define ROOT_SVN_BRANCH "trunk"
+#define ROOT_RELEASE "5.26/00a"
+#define ROOT_RELEASE_DATE "Jan 19 2010"
+#define ROOT_RELEASE_TIME "11:17:17"
+#define ROOT_SVN_REVISION 32048
+#define ROOT_SVN_BRANCH "branches/v5-26-00-patches"
 #define ROOT_VERSION_CODE 334336
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
