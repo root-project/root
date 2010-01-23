c6113e523822ed4ae65f8c2243e98aaa9602e46f
Philippe Canal <pcanal@fnal.gov>
2010-01-23 17:21:33 +0000

    In case of error, wait 5 seconds before retrying
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1867 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/perf/slowreading/trytwice.sh
+++ root/io/perf/slowreading/trytwice.sh
@@ -8,5 +8,5 @@ firstlog=first.attempt.$log
 
 if [ "x$CMDECHO" != "x@" ] ; then set -x; fi
 
-$callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)" > $log 2>&1 || ( mv $log $firstlog ; $callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)"  > $log 2>&1 )
+$callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)" > $log 2>&1 || ( mv $log $firstlog ; sleep 5; $callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)"  > $log 2>&1 )
 exit $?
