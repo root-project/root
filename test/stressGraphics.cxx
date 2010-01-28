70b82e3121d72940ec4996b4cd382cadf56fb427
Olivier Couet <Olivier.Couet@cern.ch>
2010-01-28 14:35:04 +0000

    - Modify the 2nd latex test to adjust better indices.
    git-svn-id: http://root.cern.ch/svn/root/trunk@32136 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressGraphics.cxx
+++ test/stressGraphics.cxx
@@ -871,7 +871,7 @@ void tlatex2()
    l.DrawLatex(0.5,0.95,"e^{+}e^{-}#rightarrowZ^{0}#rightarrowI#bar{I}, q#bar{q}");
    l.DrawLatex(0.5,0.75,"|#vec{a}#bullet#vec{b}|=#Sigmaa^{i}_{jk}+b^{bj}_{i}");
    l.DrawLatex(0.5,0.5,"i(#partial_{#mu}#bar{#psi}#gamma^{#mu}+m#bar{#psi})=0#Leftrightarrow(#Box+m^{2})#psi=0");
-   l.DrawLatex(0.5,0.3,"L_{em}=eJ^{#mu}_{em}A_{#mu} , J^{#mu}_{em}=#bar{I}#gamma_{#mu}I , M^{j}_{i}=#SigmaA_{#alpha}#tau^{#alphaj}_{i}");
+   l.DrawLatex(0.5,0.3,"L_{em}=eJ^{#mu}_{em}A_{#mu} , ^{}J^{#mu}_{em}=#bar{I}#gamma_{#mu}I , M^{j}_{i}=#SigmaA_{#alpha}#tau^{#alphaj}_{i}");
 
    TestReport1(C, "TLatex 2");
    DoCcode(C);
