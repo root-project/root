9aa21faebbf2ef87fc96d0be39cb4a3f593192c7
Axel Naumann <Axel.Naumann@cern.ch>
2010-01-28 13:35:13 +0000

    Take explicitly set ROOTTEST_HOME into account. Fixes issue with roottest's root not named "roottest". Print an error message if rootlogon.C cannot determine ROOTTEST_HOME.
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1880 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treedraw/rootlogon.C
+++ root/treedraw/rootlogon.C
@@ -1,7 +1,7 @@
 {
-   TString roottest_home = gSystem->pwd();
-      fprintf(stderr,"we are here\n");
-   {
+   TString roottest_home = gSystem->Getenv("ROOTTEST_HOME");
+   if (roottest_home == "") {
+      roottest_home = gSystem->pwd();
       Int_t pos = roottest_home.Index("/roottest/");
       if (pos==TString::kNPOS) {
          pos = roottest_home.Index("\\roottest\\");
@@ -12,6 +12,10 @@
                next = roottest_home.Index("\\roottest\\",pos+1);
             }
             pos += strlen("\\roottest\\");
+         } else {
+            printf("ERROR in rootlogon.C: cannot determine ROOTTEST_HOME!\n"
+                   "Please rename the roottest root directory, it should be called \"roottest\"\n");
+            exit(1);
          }
       } else {
          Int_t next = pos;
@@ -24,7 +28,11 @@
       if (pos!=TString::kNPOS) {
          roottest_home.Remove(pos);
       }
-      gROOT->ProcessLine(Form(".L %sscripts/utils.cc+",roottest_home.Data()));
    }
-   
+   if (!roottest_home.EndsWith("/")) {
+      roottest_home += "/";
+   }
+   {
+      gROOT->ProcessLine(Form(".L %sscripts/utils.cc+",roottest_home.Data()));   
+   }
 }
