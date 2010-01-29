c2158cfab77bcf63763c933f92662786506f38be
Axel Naumann <Axel.Naumann@cern.ch>
2010-01-29 15:18:09 +0000

    Unhandled exception (Coverity)
    git-svn-id: http://root.cern.ch/svn/root/trunk@32147 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/reflex/src/genmap/genmap.cxx
+++ cint/reflex/src/genmap/genmap.cxx
@@ -167,17 +167,24 @@ main(int argc,
       map_gen.genHeader();
       set<string> used_names;
 
-      for (Member_Iterator it = factories.FunctionMember_Begin();
-           it != factories.FunctionMember_End(); ++it) {
-         //string cname = it->Properties().PropertyAsString("name");
-         string cname = it->Name();
+      try {
+         for (Member_Iterator it = factories.FunctionMember_Begin();
+              it != factories.FunctionMember_End(); ++it) {
+            //string cname = it->Properties().PropertyAsString("name");
+            string cname = it->Name();
 
-         if (used_names.insert(cname).second) {
-            map_gen.genFactory(cname, "");
+            if (used_names.insert(cname).second) {
+               map_gen.genFactory(cname, "");
+            }
          }
+         map_gen.genTrailer();
+         return 0;
       }
-      map_gen.genTrailer();
-      return 0;
+      catch (std::exception& e) {
+         cerr << "GENMAP: error creating map " << rootmap << ": "
+              << e.what() << endl;
+      }
+      return 1;
    }
    cout << "library does not contain plugin factories" << endl;
    return 0;
