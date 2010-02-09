c89706ad2aefa761c8636007bf7f882b716301fe
Joerg Stelzer <joerg.stelzer@cern.ch>
2010-02-09 00:18:39 +0000

    TMVA version 4.0.5
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-26-00-patches@32293 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/inc/Version.h
+++ tmva/inc/Version.h
@@ -38,10 +38,10 @@
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
-#define TMVA_RELEASE      "4.0.4"
-#define TMVA_RELEASE_DATE "Dec 11, 2009"
-#define TMVA_RELEASE_TIME "10:19:58"
-#define TMVA_VERSION_CODE 262148
+#define TMVA_RELEASE      "4.0.5"
+#define TMVA_RELEASE_DATE "Feb 09, 2010"
+#define TMVA_RELEASE_TIME "01:15:21"
+#define TMVA_VERSION_CODE 262149
 #define TMVA_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
