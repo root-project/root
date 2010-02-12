3d097925100dddf6b4150329acf9fdee55438626
Philippe Canal <pcanal@fnal.gov>
2010-02-12 15:07:10 +0000

    Add support for genreflex. Fix MakeProject generation for cms files. Fix running with no library (avoiding double delete). Changed the MakeProject directory name to now be lib[gen]FILENAME (without the .root extension)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1889 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/perf/userdatasets/readfile.C
+++ root/io/perf/userdatasets/readfile.C
@@ -5,8 +5,9 @@
 #include "TTreePerfStats.h"
 #include "TVirtualStreamerInfo.h"
 #include "TStreamerElement.h"
+#include "TROOT.h"
 
-TFile *openFileAndLib(const char *i_filename)
+TFile *openFileAndLib(const char *i_filename, bool loadlibrary, bool genreflex)
 {
    // Load library if any
    TString libdir(i_filename);
@@ -14,15 +15,25 @@ TFile *openFileAndLib(const char *i_filename)
    if (pos != kNPOS) {
       libdir.Remove(pos);
    }
+   
+   if (genreflex) {
+      gSystem->Load("libCintex");
+      gROOT->ProcessLine("ROOT::Cintex::Cintex::Enable()");
+      libdir.Prepend("gen");
+   }
+   libdir.Prepend("lib");
+
    bool haslibrary = !gSystem->AccessPathName(libdir,kReadPermission);
-   if (haslibrary) {
-      gSystem->Load(TString::Format("%s/%s",libdir.Data(),libdir.Data()));
+   if (loadlibrary && haslibrary) {
+      if ( gSystem->Load(TString::Format("%s/%s",libdir.Data(),libdir.Data())) < 0) {
+         return 0;
+      }
    }
                     
    // open the local if any
    TFile *file;
    TString filename(i_filename);
-   if (gSystem->AccessPathName(filename,kReadPermission) ) {
+   if (gSystem->AccessPathName(filename,kReadPermission) && filename.Index(":") == kNPOS) {
       // otherwise open the http file
       filename.Prepend("http://root.cern.ch/files/");
    }
@@ -31,17 +42,36 @@ TFile *openFileAndLib(const char *i_filename)
    if (!file) return 0;
    
    // if library not load yet, generate the code, compile it and load it.
-   if (!haslibrary) {
-      // Fix HepMC if needed.
+   if (loadlibrary && !haslibrary) {
+      // Fix HepMC
       TVirtualStreamerInfo *info = (TVirtualStreamerInfo*)file->GetStreamerInfoCache()->FindObject("HepMC::GenVertex");
       if (info) {
          TObject *el = info->GetElements()->FindObject("m_event");
          if (el) el->SetBit(TStreamerElement::kDoNotDelete);
       }
+      // Fix CMS Ref class.
+      info = (TVirtualStreamerInfo*)file->GetStreamerInfoCache()->FindObject("edm::RefToBase<reco::Track>");
+      if (info) {
+         TObject *el = info->GetElements()->FindObject("holder_");
+         if (el) el->SetBit(TStreamerElement::kDoNotDelete);
+      }
       
-      file->MakeProject(libdir.Data(),"*","NEW+");
-      gSystem->Load(TString::Format("%s/%s",libdir.Data(),libdir.Data()));
-   }      
+      if (genreflex) {
+         file->MakeProject(libdir.Data(),"*","NEW+genreflex");
+      } else {
+         file->MakeProject(libdir.Data(),"*","NEW+");
+      }
+      if ( gSystem->Load(TString::Format("%s/%s",libdir.Data(),libdir.Data())) < 0) {
+         return 0;
+      }
+   } else {
+      // Fix HepMC
+      TClass *clGenVertex = TClass::GetClass("HepMC::GenVertex");
+      if (clGenVertex && clGenVertex->GetStreamerInfo()) {
+         TObject *el = clGenVertex->GetStreamerInfo()->GetElements()->FindObject("m_event");
+         if (el) el->SetBit(TStreamerElement::kDoNotDelete);
+      }
+   }
    
    return file;
 }
@@ -56,7 +86,7 @@ TTree *getTree(TFile *file, const char *treename) {
    // Try the known names :)
    const char *names [] = { "E","Events","CollectionTree" };
    
-   for (unsigned int i = 0; i < sizeof(names); ++i) {
+   for (unsigned int i = 0; i < sizeof(names)/sizeof(names[0]); ++i) {
       file->GetObject(names[i],tree);
       if (tree) return tree;
    }
@@ -64,17 +94,33 @@ TTree *getTree(TFile *file, const char *treename) {
 }
 
 
-void readfile(const char *filename, const char *treename /* = 0 */, Int_t cachesize=-1);
+void readfile(const char *filename, const char *options /* = 0 */, Int_t cachesize=-1);
 
 void readfile(const char *filename = "lhcb2.root", Int_t cachesize=-1) {
    readfile(filename,0,cachesize);
 }
 
-void readfile(const char *filename, const char *treename /* = 0 */, Int_t cachesize /* =-1 */) {
+void readfile(const char *filename, const char *options /* = 0 */, Int_t cachesize /* =-1 */) 
+{
+   // The support options are:
+   //   nolib : do not load any library.
+   //   genreflex : use a reflex dictionary.
+   //   tree=somename : use a non standard name for the dictionary, this _must_ be the last options.
+   
+   TString opt(options);
+   bool genreflex = opt.Contains("genreflex");
+   bool loadlibrary = !opt.Contains("nolib");
+   Ssiz_t pos = opt.Index("tree=");
+   const char *treename = 0;
+   if ( pos != kNPOS) {
+      treename = &(opt[pos]);
+   }
    
    //gSystem->Load("lhcbdir/lhcbdir");  //shared lib generated with TFile::MakeProject
-   TFile *file = openFileAndLib(filename);
+   TFile *file = openFileAndLib(filename,loadlibrary,genreflex);
 
+   if (file==0) return;
+   
    TTree *T = getTree(file,treename);
 
    TFile::SetReadaheadSize(0);
@@ -97,7 +143,9 @@ void readfile(const char *filename, const char *treename /* = 0 */, Int_t caches
       //if (r.Rndm() > 0.01) continue;
       T->GetEntry(i);
    }
-   ps->SaveAs("lhcb2_ioperf.root");
+   TString psfilename(filename);
+   psfilename.ReplaceAll(".root","_ioperf.root");
+   ps->SaveAs(psfilename);
    //ps->Draw();
    ps->Print();
    T->PrintCacheStats();
