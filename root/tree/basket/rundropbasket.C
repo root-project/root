da36530c828f749b0f9dfcaa7da5748d6930f2dc
Philippe Canal <pcanal@fnal.gov>
2010-02-12 23:35:04 +0000

    Test that old file can still be read even after calling DropBaskets
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1891 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/basket/rundropbasket.C
@@ -0,0 +1,10 @@
+{
+TFile *_file0 = TFile::Open("DataTest3_cel.root");
+gDirectory->cd("DataSet");
+TTree *tree; gDirectory->GetObject("TestA1.cel",tree);
+cout.flush(); cerr.flush();
+tree->Scan("fX","","",3,3);
+tree->DropBaskets();
+tree->Scan("fX","","",3,3);
+return 0;
+}
