b640eba362de5e2f32e35c1f124c98e3b4a09c02
Philippe Canal <pcanal@fnal.gov>
2010-02-25 15:30:04 +0000

    undo wrong patch
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1904 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/perf/userdatasets/runall.sh
+++ root/io/perf/userdatasets/runall.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
-drop_caches
    root.exe -b -l -q readfile.C+\(\"atlasFlushed.root\",\"\"\)
-drop_caches
    root.exe -b -l -q readfile.C+\(\"atlasFlushed.root\",\"nolib\"\)
-drop_caches
    root.exe -b -l -q readfile.C+\(\"atlasFlushed.root\",\"genreflex\"\)
-drop_caches
 
    root.exe -b -l -q readfile.C+\(\"lhcb2.root\",\"\"\)
-drop_caches
    root.exe -b -l -q readfile.C+\(\"lhcb2.root\",\"nolib\"\)
-drop_caches
    root.exe -b -l -q readfile.C+\(\"lhcb2.root\",\"genreflex\"\)
-drop_caches
 
    root.exe -b -l -q readfile.C+\(\"cmsflush.root\",\"\"\)
-drop_caches
    root.exe -b -l -q readfile.C+\(\"cmsflush.root\",\"nolib\"\)
-drop_caches
    root.exe -b -l -q readfile.C+\(\"cmsflush.root\",\"genreflex\"\)
-drop_caches
 
 
