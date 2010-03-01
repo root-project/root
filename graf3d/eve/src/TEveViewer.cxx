56b8584142f75712d7fa3c57789ad5b2bf88a57f
Matevz Tadel <matevz.tadel@cern.ch>
2010-03-01 19:46:12 +0000

    Use gVirtualX->SupportsExtension() to determine if we are running on an OSX display server.
    git-svn-id: http://root.cern.ch/svn/root/trunk@32431 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf3d/eve/src/TEveViewer.cxx
+++ graf3d/eve/src/TEveViewer.cxx
@@ -91,14 +91,8 @@ void TEveViewer::InitInternal()
    // Initialize static data-members according to running conditions.
 
    // Determine if display is running on a mac.
-   // This is also works for ssh connection mac->linux.
-#ifndef WIN32
-   TString s = gSystem->GetFromPipe("xdpyinfo");
-   if (s.Index("Apple-WM") != kNPOS)
-   {
-      fgRecreateGlOnDockOps = kTRUE;
-   }
-#endif
+   // This also works for ssh connection mac->linux.
+   fgRecreateGlOnDockOps = (gVirtualX->SupportsExtension("Apple-WM") == 1);
 
    fgInitInternal = kTRUE;
 }
