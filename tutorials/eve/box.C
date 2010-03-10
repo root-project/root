b991fcd3541bfc2fc053ee98d1511517fc30d465
Matevz Tadel <matevz.tadel@cern.ch>
2010-03-10 17:05:57 +0000

    Add arguments to the tutorial specifying dimensions and origin of the box.
    git-svn-id: http://root.cern.ch/svn/root/trunk@32529 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/eve/box.C
+++ tutorials/eve/box.C
@@ -4,25 +4,24 @@
 // Demonstrates usage of TEveBox class.
 
 
-TEveBox* box()
+TEveBox* box(Float_t a=10, Float_t d=5, Float_t x=0, Float_t y=0, Float_t z=0)
 {
    TEveManager::Create();
 
-   TRandom r(0);
+   TRandom& r = * gRandom;
 
    TEveBox* b = new TEveBox;
    b->SetMainColor(kCyan);
    b->SetMainTransparency(0);
 
-   Float_t a = 10, d = 5;
-   b->SetVertex(0, -a + r.Uniform(-d, d), -a + r.Uniform(-d, d), -a + r.Uniform(-d, d));
-   b->SetVertex(1, -a + r.Uniform(-d, d),  a + r.Uniform(-d, d), -a + r.Uniform(-d, d));
-   b->SetVertex(2,  a + r.Uniform(-d, d),  a + r.Uniform(-d, d), -a + r.Uniform(-d, d));
-   b->SetVertex(3,  a + r.Uniform(-d, d), -a + r.Uniform(-d, d), -a + r.Uniform(-d, d));
-   b->SetVertex(4, -a + r.Uniform(-d, d), -a + r.Uniform(-d, d),  a + r.Uniform(-d, d));
-   b->SetVertex(5, -a + r.Uniform(-d, d),  a + r.Uniform(-d, d),  a + r.Uniform(-d, d));
-   b->SetVertex(6,  a + r.Uniform(-d, d),  a + r.Uniform(-d, d),  a + r.Uniform(-d, d));
-   b->SetVertex(7,  a + r.Uniform(-d, d), -a + r.Uniform(-d, d),  a + r.Uniform(-d, d));
+   b->SetVertex(0, x - a + r.Uniform(-d, d), y - a + r.Uniform(-d, d), z - a + r.Uniform(-d, d));
+   b->SetVertex(1, x - a + r.Uniform(-d, d), y + a + r.Uniform(-d, d), z - a + r.Uniform(-d, d));
+   b->SetVertex(2, x + a + r.Uniform(-d, d), y + a + r.Uniform(-d, d), z - a + r.Uniform(-d, d));
+   b->SetVertex(3, x + a + r.Uniform(-d, d), y - a + r.Uniform(-d, d), z - a + r.Uniform(-d, d));
+   b->SetVertex(4, x - a + r.Uniform(-d, d), y - a + r.Uniform(-d, d), z + a + r.Uniform(-d, d));
+   b->SetVertex(5, x - a + r.Uniform(-d, d), y + a + r.Uniform(-d, d), z + a + r.Uniform(-d, d));
+   b->SetVertex(6, x + a + r.Uniform(-d, d), y + a + r.Uniform(-d, d), z + a + r.Uniform(-d, d));
+   b->SetVertex(7, x + a + r.Uniform(-d, d), y - a + r.Uniform(-d, d), z + a + r.Uniform(-d, d));
 
    gEve->AddElement(b);
    gEve->Redraw3D(kTRUE);
