1a2d7acfe5d9c9aa858d769fcc2beb8677ae2ca3
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-03-11 14:55:51 +0000

    use MACOSX_DEPLOYMENT_TARGET if set in the shell. Allows easier building for older MacOS X platforms without having to change any makefiles.
    git-svn-id: http://root.cern.ch/svn/root/trunk@32554 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makelib.sh
+++ build/unix/makelib.sh
@@ -115,7 +115,9 @@ elif [ $PLATFORM = "macosx" ]; then
    export DYLD_LIBRARY_PATH=`pwd`/lib:$DYLD_LIBRARY_PATH
    if [ $macosx_minor -ge 3 ]; then
       unset LD_PREBIND
-      export MACOSX_DEPLOYMENT_TARGET=10.$macosx_minor
+      if [ "$MACOSX_DEPLOYMENT_TARGET" = "" ]; then
+         export MACOSX_DEPLOYMENT_TARGET=10.$macosx_minor
+      fi
    fi
    # We need two library files: a .dylib to link to and a .so to load
    BUNDLE=`echo $LIB | sed s/.dylib/.so/`
