52f4632ec2f2fe91bdad873acb683b5ac5111335
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2010-03-25 14:49:48 +0000

    add testInversion for smatrix
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1954 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/math/smatrix/runtestInversion.C
@@ -0,0 +1,11 @@
+{
+// compile and run the test in smatrix directory
+   gROOT->ProcessLine(".L $ROOTSYS/math/smatrix/test/testInversion.cxx+");
+   int ret = testInversion(); 
+   if (ret == 0)
+      std::cout << "testInversion OK" << std::endl;
+   else
+      std::cerr << "testInversion  FAILED !" << std::endl;
+
+   return 0;  // need to always return zero if checking log file  
+}
