9b469e6e2033f5ba4a39d1b2fda113074e1ff274
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2010-03-25 14:22:54 +0000

    improve test for inversion of matrices
    git-svn-id: http://root.cern.ch/svn/root/trunk@32761 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/smatrix/test/testInversion.cxx
+++ math/smatrix/test/testInversion.cxx
@@ -287,11 +287,14 @@ double invert2( const std::vector<M* >  & matlist, double & time,std::string s) 
 }
 
 bool equal(double d1, double d2, double stol = 10000) { 
+   std::cout.precision(12);  // tolerance is 1E-12
    double eps = stol * std::numeric_limits<double>::epsilon(); 
-   if ( std::abs(d1) > 0) 
-      return  ( std::abs( d1-d2) < eps * std::max(d1,d2) ); 
-   else 
+   if ( std::abs(d1) > 0 && std::abs(d2) > 0 )
+      return  ( std::abs( d1-d2) < eps * std::max(std::abs(d1), std::abs(d2) ) ); 
+   else if ( d1 == 0 )  
       return std::abs(d2) < eps; 
+   else // d2 = 0
+      return std::abs(d1) < eps; 
 }
 
 // test matrices symmetric and positive defines 
@@ -304,25 +307,32 @@ bool stressSymPosInversion(int n, bool selftest ) {
    std::vector<TMatrixDSym *> v2(n); 
    generate(v2);
 
+
    bool iret = true; 
    double time = 0;
    double s1 = invert<SymMatrix, Choleski> (v1, time,"SMatrix Chol");
    double s2 = invert<SymMatrix, BK> (v1, time,"SMatrix   BK");
    double s3 = invert<SymMatrix, Cramer> (v1, time,"SMatrix Cram");
-   iret  |=  ( equal(s1,s2) && equal(s1,s3) ); 
-   if (!iret) 
+   bool ok = ( equal(s1,s2) && equal(s1,s3) );  
+   if (!ok) { 
       std::cout << "result SMatrix choleski  " << s1 << " BK   " << s2 << " cramer " << s3 << std::endl;
-   std::cout << std::endl; 
+      std::cerr <<"Error:  inversion test for SMatrix FAILED ! " << std::endl;
+   }
+   iret  &= ok; 
+   std::cout << std::endl;    
    
    double m1 = invert<TMatrixDSym, Choleski> (v2, time,"TMatrix Chol");
    double m2 = invert<TMatrixDSym, BK> (v2, time,"TMatrix   BK");
    double m3 = invert<TMatrixDSym, Cramer> (v2, time,"TMatrix Cram");
    double m4 = invert<TMatrixDSym, Default> (v2, time,"TMatrix  Def");
 
-   iret |= ( equal(m1,m2) && equal(m1,m3) && equal(m1,m4) );
-   if (!iret) 
+   ok =  ( equal(m1,m2) && equal(m1,m3) && equal(m1,m4) );
+   if (!ok) {
       std::cout << "result TMatrix choleski  " << m1 << " BK   " << m2 
                 << " cramer " << m3 << " default " << m4 << std::endl;
+      std::cerr <<"Error:  inversion test for TMatrix FAILED ! " << std::endl;
+   }
+   iret  &= ok; 
    std::cout << std::endl; 
    
    
@@ -332,30 +342,36 @@ bool stressSymPosInversion(int n, bool selftest ) {
       double s11 = invert2<SymMatrix, Choleski> (v1, time,"SMatrix Chol");
       double s12 = invert2<SymMatrix, BK> (v1, time,"SMatrix   BK");
       double s13 = invert2<SymMatrix, Cramer> (v1, time,"SMatrix Cram");
-      iret  |=  ( equal(s11,s12) && equal(s11,s13) ); 
-      if (!iret) 
+      ok =  ( equal(s11,s12) && equal(s11,s13) ); 
+      if (!ok) {
          std::cout << "result SMatrix choleski  " << s11 << " BK   " << s12 << " cramer " << s13 << std::endl;
+         std::cerr <<"Error:  self inversion test for SMatrix FAILED ! " << std::endl;
+      }
+      iret  &= ok; 
       std::cout << std::endl; 
       
       double m13 = invert2<TMatrixDSym, Cramer> (v2, time,"TMatrix Cram");
       double m14 = invert2<TMatrixDSym, Default> (v2, time,"TMatrix  Def");
-      iret  |=  ( equal(m13,m14)  ); 
-      if (!iret) 
+      ok =  ( equal(m13,m14)  ); 
+      if (!ok) { 
          std::cout << "result TMatrix  cramer " << m13 << " default " << m14 << std::endl;      
+         std::cerr <<"Error:  self inversion test for TMatrix FAILED ! " << std::endl;
+      }
+      iret  &= ok; 
       std::cout << std::endl; 
    }
 
    return iret; 
 }
 
-bool stressInversion(int n = 1000000) { 
+bool testInversion(int n = 100000) { 
    bool ok = stressSymPosInversion(n, doSelfTest); 
-   std::cout << "Test inversion of positive defined matrix ....... "; 
-   if (ok) std::cout << "OK \n"; 
-   else std::cout << "FAILED \n"; 
+   std::cerr << "Test inversion of positive defined matrix ....... "; 
+   if (ok) std::cerr << "OK \n"; 
+   else std::cerr << "FAILED \n"; 
    return ok;
 }
 
 int main() { 
-   return (stressInversion() ) ? 0 : -1; 
+   return (testInversion() ) ? 0 : -1; 
 }
