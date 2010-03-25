99e1d624a022d879b5b697c4ab60765f8bf222dd
Axel Naumann <Axel.Naumann@cern.ch>
2010-03-25 14:40:13 +0000

    OLDPWD is a very bad local variable name (it's a bash variable)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1951 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/run-from-buildbot.sh
+++ scripts/run-from-buildbot.sh
@@ -3,14 +3,15 @@
 # roottest needs several variables to be defined.
 # Pushing them through the buildbot config is more difficult than just
 # calculating them here. This script will thus invoke roottest for buildbot.
+# Axel, 2010-03-25
 
-OLDPWD=$PWD
+STARTPWD=$PWD
 
 # We might be building roottest for roottest-Ubuntu1004-64bit-nightly
 # That wants to test ROOT-Ubuntu1004-64bit-nightly, so that's the ROOT
 # version we need to set up.
 
-# pwd is ..../ROOT-Ubuntu1004-64bit-nightly/build, so cd down:
+# pwd is ..../ROOT-Ubuntu1004-64bit-nightly/build, so cd up:
 cd ..
 # and this is the slave that runs us:
 BBARCH=`basename $PWD`
@@ -19,9 +20,10 @@ BBARCH=../ROOT-${BBARCH#*-}/build
 # we cd into its build directory and set ROOT up
 cd  $BBARCH || (echo Cannot find directory $BBARCH from `pwd`; exit 1)
 . bin/thisroot.sh || (echo Cannot find ROOT setup script in `pwd`; exit 1)
+echo Set up ROOT in $ROOTSYS
 
 # cd back to where we started
-cd $OLDPWD
+cd $STARTPWD
 
 # some more env vars
 # Did Philippe fix it? export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
