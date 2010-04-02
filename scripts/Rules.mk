ce8742e8056b3b61fef29cbffca0702ff344e118
Philippe Canal <pcanal@fnal.gov>
2010-04-02 21:36:15 +0000

    Stop/fail a test if a required test library is not built properly.  Shorten the output of make VERBOSE=true by using a new script handleError.sh
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@1984 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/Rules.mk
+++ scripts/Rules.mk
@@ -484,19 +484,10 @@ ROOTCINT = $(ROOT_LOC)/bin/rootcint$(ExeSuf)
 UTILS_LIBS =  $(ROOTTEST_LOC)scripts/utils_cc.$(DllSuf) $(ROOTTEST_LOC)scripts/recordtiming_cc.$(DllSuf)
 
 $(ROOTTEST_LOC)scripts/utils_cc.$(DllSuf) : $(ROOTTEST_LOC)scripts/utils.cc $(ROOTCORELIBS) $(ROOTCINT) $(ROOTV)
-	$(CMDECHO) $(CALLROOTEXEBUILD) -q -l -b $(ROOTTEST_HOME)/scripts/build.C\(\"$(ROOTTEST_HOME)scripts/utils.cc\"\) > $(ROOTTEST_LOC)scripts/utils_cc.build.log 2>&1 ; \
-	result = $$? ; if test $result -ne 0 ; then \
-	then \
-	  cat $(ROOTTEST_LOC)scripts/utils_cc.build.log ; exit $$result; \
-	else if test -f $@ ; then touch $@ ; fi ; \
-	fi
+	$(CMDECHO) $(CALLROOTEXEBUILD) -q -l -b $(ROOTTEST_HOME)/scripts/build.C\(\"$(ROOTTEST_HOME)scripts/utils.cc\"\) > $(ROOTTEST_LOC)scripts/utils_cc.build.log 2>&1 || handleError.sh $$?  $(ROOTTEST_LOC)scripts/utils_cc.build.log
 
 $(ROOTTEST_LOC)scripts/recordtiming_cc.$(DllSuf) : $(ROOTTEST_LOC)scripts/recordtiming.cc $(ROOTCORELIBS) $(ROOTCINT) $(ROOTV)
-	$(CMDECHO) $(CALLROOTEXEBUILD) -q -l -b $(ROOTTEST_HOME)/scripts/build.C\(\"$(ROOTTEST_HOME)scripts/recordtiming.cc\"\) > $(ROOTTEST_LOC)scripts/recordtiming_cc.build.log 2>&1 ; \
-	result = $$? ; if test $result -ne 0 ; then \
-	  cat $(ROOTTEST_LOC)scripts/recordtiming_cc.build.log ; exit $$result; \
-	else if test -f $@ ; then touch $@ ; fi ; \
-	fi
+	$(CMDECHO) $(CALLROOTEXEBUILD) -q -l -b $(ROOTTEST_HOME)/scripts/build.C\(\"$(ROOTTEST_HOME)scripts/recordtiming.cc\"\) > $(ROOTTEST_LOC)scripts/recordtiming_cc.build.log 2>&1 || handleError.sh $$?  $(ROOTTEST_LOC)scripts/recordtiming_cc.build.log 
 
 override ROOTMAP = $(ROOT_LOC)/etc/system.rootmap
 
