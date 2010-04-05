7b7e3a620f2791ee4416c451a7e576c083e6d8f1
Philippe Canal <pcanal@fnal.gov>
2010-04-05 14:30:26 +0000

    Add test for revision 32881
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2002 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/query/runarray.C
@@ -0,0 +1,11 @@
+{
+   TTree *t = new TTree("T","T");
+   int arr[2];
+   t->Branch("arr",&arr[0],"arr[2]/I");
+   arr[0] = 1;
+   arr[1] = 2;
+   t->Fill();
+   t->Scan("arr");
+   r = t->Query("arr");
+   while( ( row = r->Next() ) ) { cout << row->GetField(0) << endl; delete row ; row = 0; }
+}
