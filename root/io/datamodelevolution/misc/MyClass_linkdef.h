df18c86b6f0bee661b09011e7a94acb90f2b2f16
Philippe Canal <pcanal@fnal.gov>
2010-04-07 00:32:14 +0000

    remove stray character (fail on icc)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2003 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/datamodelevolution/misc/MyClass_linkdef.h
+++ root/io/datamodelevolution/misc/MyClass_linkdef.h
@@ -5,7 +5,7 @@
 #pragma link C++ class MyClass+;
 
 #pragma read sourceClass="MyClass" version="[1-]" source="TObjArray fArray;" \
-  targetClass="MyClass" \ target="fArray" \
+  targetClass="MyClass" target="fArray" \
   code="{ fArray = onfile.fArray; fArray.Compress(); }"
 
 #endif
