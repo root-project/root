44f4190e3c9f3981c7c292959dfcd0d6a427a475
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2010-04-16 15:23:32 +0000

    add new directory mathcore and a new test (testRootFinder)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2016 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/math/mathcore/runtestRootFinder.C
@@ -0,0 +1,11 @@
+{
+   // Compile and run the code of math/mathcore/test directory
+   gROOT->ProcessLine(".L $ROOTSYS/math/mathcore/test/testRootFinder.cxx+");
+   int ret = testRootFinder(); 
+   if (ret == 0)
+      std::cout << "testRooFinder OK" << std::endl;
+   else
+      std::cerr << "testRootFinder  FAILED !" << std::endl;
+
+   return 0;  // need to always return zero if checking log file 
+}
