5d0d3b2951fbc821600782565f3e218793a1033f
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-04-26 08:16:00 +0000

    From Axel: For Buildbot: run configure if needed.
    git-svn-id: http://root.cern.ch/svn/root/trunk@33195 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ build/unix/buildbot.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# Run configure if configure hasn't been run or if the configuration
+# switches have changed.
+# Axel, 2010-04-26
+
+if ! test  -f config.status; then
+    ./configure "$@" || exit $?
+else
+    ARGS="$@"
+    while [ "x$1" != "x" ]; do
+        if ! grep -e "$1" config.status > /dev/null 2>&1; then
+            ./configure $ARGS || exit $?
+            break
+        fi
+        shift
+    done
+fi
+
+make -j2
