93dd049fcb96d0eaaafc39c76c55cdcb90013cd6
Rene Brun <Rene.Brun@cern.ch>
2010-04-26 12:47:11 +0000

    Better use of TString::Format
    git-svn-id: http://root.cern.ch/svn/root/trunk@33213 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/hist/src/TH1.cxx
+++ hist/hist/src/TH1.cxx
@@ -7724,9 +7724,9 @@ TH1* TH1::TransformHisto(TVirtualFFT *fft, TH1* h_output,  Option_t *option)
    else {
       TString name = TString::Format("out_%s", opt.Data());
       if (fft->GetNdim()==1)
-         hout = new TH1D(name.Data(), name.Data(),n[0], 0, n[0]);
+         hout = new TH1D(name, name,n[0], 0, n[0]);
       else if (fft->GetNdim()==2)
-         hout = new TH2D(name.Data(), name.Data(), n[0], 0, n[0], n[1], 0, n[1]);
+         hout = new TH2D(name, name, n[0], 0, n[0], n[1], 0, n[1]);
    }
    TString type=fft->GetType();
    Int_t ind[2];
@@ -9030,9 +9030,9 @@ TH1 *R__H(Int_t hid)
    //   h_id if id <0
 
    TString hname;
-   if(hid >= 0) hname = TString::Format("h%d",hid);
-   else         hname = TString::Format("h_%d",hid);
-   return (TH1*)gDirectory->Get(hname.Data());
+   if(hid >= 0) hname.Form("h%d",hid);
+   else         hname.Form("h_%d",hid);
+   return (TH1*)gDirectory->Get(hname);
 }
 
 //______________________________________________________________________________
