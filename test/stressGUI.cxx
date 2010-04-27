2605c26a67f46485ca035c6ce24308cbb2e7cc58
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2010-04-27 12:46:32 +0000

    Use a TString and gSystem->GetFromPipe instead of gSystem->OpenPipe and fgets (solve the compiler warning: ignoring return value of 'char* fgets...)
    git-svn-id: http://root.cern.ch/svn/root/trunk@33239 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressGUI.cxx
+++ test/stressGUI.cxx
@@ -297,13 +297,20 @@ void stressGUI()
       //Print table with results
       Bool_t UNIX = strcmp(gSystem->GetName(), "Unix") == 0;
       if (UNIX) {
-         FILE *fp = gSystem->OpenPipe("uname -a", "r");
-         Char_t line[60];
-         fgets(line,60,fp); line[59] = 0;
-         printf("*  SYS: %s\n",line);
-         gSystem->ClosePipe(fp);
+         TString sp = gSystem->GetFromPipe("uname -a");
+         sp.Resize(60);
+         printf("*  SYS: %s\n",sp.Data());
+         if (strstr(gSystem->GetBuildNode(),"Linux")) {
+            sp = gSystem->GetFromPipe("lsb_release -d -s");
+            printf("*  SYS: %s\n",sp.Data());
+         }
+         if (strstr(gSystem->GetBuildNode(),"Darwin")) {
+            sp  = gSystem->GetFromPipe("sw_vers -productVersion");
+            sp += " Mac OS X ";
+            printf("*  SYS: %s\n",sp.Data());
+         }
       } else {
-         const Char_t *os = gSystem->Getenv("OS");
+         const char *os = gSystem->Getenv("OS");
          if (!os) printf("*  SYS: Windows 95\n");
          else     printf("*  SYS: %s %s \n",os,gSystem->Getenv("PROCESSOR_IDENTIFIER"));
       }
