eadfd2e7e6e52cd6f74a41ed7ad1563b7586768a
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-04-28 15:54:20 +0000

    Generate ChangeLog only from v5-27-01.
    git-svn-id: http://root.cern.ch/svn/root/trunk@33285 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makechangelog.sh
+++ build/unix/makechangelog.sh
@@ -7,6 +7,8 @@ echo "Generating README/ChangeLog from SVN logs..."
 echo ""
 
 # Generate ChangeLog from version v5-12-00 till now
-$SVN2CL -i -f README/ChangeLog -r HEAD:15807
+#$SVN2CL -i -f README/ChangeLog -r HEAD:15807
+# Generate ChangeLof from version v5-27-01 till now
+$SVN2CL -i -f README/ChangeLog -r HEAD:31884
 
 exit 0
