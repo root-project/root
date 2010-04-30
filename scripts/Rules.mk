ed11378419654aa603b230f1bd8eee17d012051e
Philippe Canal <pcanal@fnal.gov>
2010-04-30 14:10:06 +0000

    avoid spurrious message about no files to be includes
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2047 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/Rules.mk
+++ scripts/Rules.mk
@@ -85,7 +85,10 @@ ALL_LIBRARIES += *.d *.o *.obj *.so *.def *.exp *.dll *.lib dummy.C \
 .PHONY: clean removefiles tests all test $(TEST_TARGETS) $(TEST_TARGETS_DIR) utils check logs.tar.gz
 
 include $(ROOTTEST_HOME)/scripts/Common.mk
--include $(wildcard *.d)
+DEPENDENCIES_INCLUDES := $(wildcard *.d)
+ifneq ($(DEPENDENCIES_INCLUDES),)
+   -include $(wildcard *.d)
+endif
 
 ifeq ($(MAKECMDGOALS),cleantest)
 	TESTGOAL = cleantest
