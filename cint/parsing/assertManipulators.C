60c6c6b0bdf97001d6d0f8f9174f3dc8230d546c
Philippe Canal <pcanal@fnal.gov>
2010-05-19 20:01:44 +0000

    test r33585
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2067 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ cint/parsing/assertManipulators.C
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void assertManipulators() {
+   double mark = 5.0;
+   std::cout << " mark is " << fixed << mark << std::endl;
+   std::cout << " mark is " << scientific << mark << std::endl;
+}
