ca1e2496dcf0da31b6565614a546a457c9bab0fa
Philippe Canal <pcanal@fnal.gov>
2010-05-20 14:41:19 +0000

    Work around old compiler problem with whether or not to generate a default constructor (by skipping the shadow class)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2070 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/aclic/misc/assertmyfun.cxx
+++ root/aclic/misc/assertmyfun.cxx
@@ -2,13 +2,15 @@
 #include "Fit/Chi2FCN.h"
 
 class MyFCN: public ROOT::Fit::Chi2Function{
-  public:
+private:
+    MyFCN(); // Not implemented
+public:
     MyFCN(const ROOT::Fit::BinData&  data, const ROOT::Fit::Chi2Function::IModelFunction&  func): ROOT::Fit::Chi2Function(data,func) {}
     ~MyFCN(){}
 };
 
 #ifdef __MAKECINT__
-#pragma link C++ class MyFCN+;
+#pragma link C++ class MyFCN-;
 #endif
 
 int assertmyfun() {
