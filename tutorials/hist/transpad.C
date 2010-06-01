1a03e314d8a065290c1867f01bb23697fb5a27e0
Olivier Couet <Olivier.Couet@cern.ch>
2010-06-01 09:52:50 +0000

    - Option "][" is required to plot the red histogram.
    git-svn-id: http://root.cern.ch/svn/root/trunk@33686 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/hist/transpad.C
+++ tutorials/hist/transpad.C
@@ -37,7 +37,7 @@ void transpad() {
    pad2->Draw();
    pad2->cd();
    h2->SetLineColor(kRed);
-   h2->Draw("sames");
+   h2->Draw("][sames");
    pad2->Update();
    TPaveStats *ps2 = (TPaveStats*)h2->GetListOfFunctions()->FindObject("stats");
    ps2->SetX1NDC(0.65); ps2->SetX2NDC(0.85);
