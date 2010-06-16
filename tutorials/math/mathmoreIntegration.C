72220b660a46d977efbc1832e791a8a363027113
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2010-06-16 15:06:48 +0000

    fix a compilation error on some compiler
    git-svn-id: http://root.cern.ch/svn/root/trunk@33948 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/math/mathmoreIntegration.C
+++ tutorials/math/mathmoreIntegration.C
@@ -28,6 +28,8 @@
 #include "TStopwatch.h"
 #include "TF1.h"
 
+#include <limits>
+
 //!calculates exact integral of Breit Wigner distribution
 //!and compares with existing methods
 
@@ -111,8 +113,9 @@ void  DrawCumulative(double x1, double x2, int n = 100){
    }
 
    // alternative method using ROOT::Math::Functor class 
-   ROOT::Math::Functor1D f1(& TMath::BreitWigner);
-  
+   ROOT::Math::Functor1D f1(& func);
+ 
+ 
    ROOT::Math::Integrator ig(f1, ROOT::Math::IntegrationOneDim::kADAPTIVE,1.E-12,1.E-12);
  
    TH1D *cum1 = new TH1D("cum1", "", n, x1, x2); 
