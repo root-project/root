397db7c2163b1c75dd20691eb447cfb25f0de06b
Matevz Tadel <matevz.tadel@cern.ch>
2010-06-17 11:37:52 +0000

    Remove unused variable.
    git-svn-id: http://root.cern.ch/svn/root/trunk@33956 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf3d/eve/src/TEveGeoShape.cxx
+++ graf3d/eve/src/TEveGeoShape.cxx
@@ -375,7 +375,6 @@ void TEveGeoShapeProjected::SetProjection(TEveProjectionManager* mng,
 
    TEveProjected::SetProjection(mng, model);
 
-   TEveGeoShape* gre = dynamic_cast<TEveGeoShape*>(fProjectable);
    CopyVizParams(dynamic_cast<TEveElement*>(model));
 }
 
