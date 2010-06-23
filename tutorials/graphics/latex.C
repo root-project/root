cb014a821152c75d34a84278886a3b4e84151fdc
Olivier Couet <Olivier.Couet@cern.ch>
2010-06-23 09:07:21 +0000

    - cosmetics
    git-svn-id: http://root.cern.ch/svn/root/trunk@34073 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/graphics/latex.C
+++ tutorials/graphics/latex.C
@@ -12,14 +12,14 @@ void latex() {
    TLatex l;
    l.SetTextAlign(12);
    l.SetTextSize(0.04);
-   l.DrawLatex(0.1,0.9,"1)   C(x) = d #sqrt{#frac{2}{#lambdaD}}  \
+   l.DrawLatex(0.1,0.9,"1)   C(x) = d #sqrt{#frac{2}{#lambdaD}}\
    #int^{x}_{0}cos(#frac{#pi}{2}t^{2})dt");
-   l.DrawLatex(0.1,0.7,"2)   C(x) = d #sqrt{#frac{2}{#lambdaD}}  \
+   l.DrawLatex(0.1,0.7,"2)   C(x) = d #sqrt{#frac{2}{#lambdaD}}\
    #int^{x}cos(#frac{#pi}{2}t^{2})dt");
    l.DrawLatex(0.1,0.5,"3)   R = |A|^{2} = #frac{1}{2}#left(#[]{#frac{1}{2}+\
    C(V)}^{2}+#[]{#frac{1}{2}+S(V)}^{2}#right)");
-   l.DrawLatex(0.1,0.3,"4)   F(t) = #sum_{i=-#infty}^{#infty}A(i)cos#[]\
-   {#frac{i}{t+i}}");
+   l.DrawLatex(0.1,0.3,
+   "4)   F(t) = #sum_{i=-#infty}^{#infty}A(i)cos#[]{#frac{i}{t+i}}");
    l.DrawLatex(0.1,0.1,"5)   {}_{3}^{7}Li");
    c1->Print("latex.ps");
 }
