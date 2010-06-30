6b41c4888c3e08de309a423f6026ef5b5791d2b3
Rene Brun <Rene.Brun@cern.ch>
2010-06-30 12:27:16 +0000

    Fix format in Warning statements
    git-svn-id: http://root.cern.ch/svn/root/trunk@34232 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/hist/src/TF1.cxx
+++ hist/hist/src/TF1.cxx
@@ -611,7 +611,7 @@ TF1::TF1(const char *name,void *fcn, Double_t xmin, Double_t xmax, Int_t npar)
          Error("TF1","No function found with the signature %s(Double_t*,Double_t*)",funcname);
       }
    } else {
-      Error("TF1","can not find any function at the address 0x%lx. This function requested for %s",fcn,name);
+      Error("TF1","can not find any function at the address 0x%lx. This function requested for %s",(Long_t)fcn,name);
    }
 
 
@@ -981,7 +981,7 @@ void TF1::CreateFromCintClass(const char *name,void *ptr, Double_t xmin, Double_
             Error("TF1","No function found in class %s with the signature operator() (Double_t*,Double_t*) or Eval(Double_t*,Double_t*)",className);
       }
    } else {
-      Error("TF1","can not find any class with name %s at the address 0x%lx",className,ptr);
+      Error("TF1","can not find any class with name %s at the address 0x%lx",className,(Long_t)ptr);
    }
 
 
