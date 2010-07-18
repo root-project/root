734a2e9bcc78209276a127d8906595d9c4fc4f41
Philippe Canal <pcanal@fnal.gov>
2010-07-18 13:53:46 +0000

    Increase verbosity in case of error
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2103 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/perf/slowreading/trytwice.sh
+++ root/io/perf/slowreading/trytwice.sh
@@ -9,4 +9,11 @@ firstlog=first.attempt.$log
 if [ "x$CMDECHO" != "x@" ] ; then set -x; fi
 
 $callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)" > $log 2>&1 || ( mv $log $firstlog ; sleep 5; $callroot -q -b -l "Read.C(\"$library\", \"$file\", 0)"  > $log 2>&1 )
-exit $?
+result=$?
+if [ $result -gt 0 ] ; then
+   if [ -e $firstlog ] ; then 
+      cat $firstlog
+   fi
+   cat $log
+fi
+exit $result
