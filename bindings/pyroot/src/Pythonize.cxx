1dee123e48e369bd4090d40bcdcf65e1e5411bf1
Wim Lavrijsen <WLavrijsen@lbl.gov>
2010-07-26 21:44:50 +0000

    Symmetrize TObject::IsEqual on __eq__ and __ne__
    git-svn-id: http://root.cern.ch/svn/root/trunk@34606 27541ba8-7e3a-0410-8455-c3a389f83636

--- bindings/pyroot/src/Pythonize.cxx
+++ bindings/pyroot/src/Pythonize.cxx
@@ -151,6 +151,20 @@ namespace {
       return result;
    }
 
+//____________________________________________________________________________
+   inline PyObject* BoolNot( PyObject* value )
+   {
+      if ( value == Py_True ) {
+         Py_INCREF( Py_False );
+         Py_DECREF( value );
+         return Py_False;
+      } else {
+         Py_INCREF( Py_True );
+         Py_DECREF( value );
+         return Py_True;
+      }
+   }
+
 //- "smart pointer" behavior ---------------------------------------------------
    PyObject* DeRefGetAttr( PyObject* self, PyObject* name )
    {
@@ -222,6 +236,15 @@ namespace {
          return ObjectProxy_Type.tp_richcompare( self, obj, Py_EQ );
 
       return CallPyObjMethod( self, "IsEqual", obj );
+   }
+
+//____________________________________________________________________________
+   PyObject* TObjectIsNotEqual( PyObject* self, PyObject* obj )
+   {
+      if ( ! ObjectProxy_Check( obj ) || ! ((ObjectProxy*)obj)->fObject )
+         return ObjectProxy_Type.tp_richcompare( self, obj, Py_NE );
+
+      return BoolNot( CallPyObjMethod( self, "IsEqual", obj ) );
    }
 
 //____________________________________________________________________________
@@ -1717,6 +1740,7 @@ Bool_t PyROOT::Pythonize( PyObject* pyclass, const std::string& name )
    // comparing for lists
       Utility::AddToClass( pyclass, "__cmp__", (PyCFunction) TObjectCompare, METH_O );
       Utility::AddToClass( pyclass, "__eq__",  (PyCFunction) TObjectIsEqual, METH_O );
+      Utility::AddToClass( pyclass, "__ne__",  (PyCFunction) TObjectIsNotEqual, METH_O );
 
       return kTRUE;
    }
