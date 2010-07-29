6795089f94a001dbe1321000cc91ebab6fc7d730
Wim Lavrijsen <WLavrijsen@lbl.gov>
2010-07-29 22:05:36 +0000

    Syncing after testing on Windows
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2113 7c735d0d-823a-0410-acfd-cd159556d4bf

--- python/memory/PyROOT_memorytests.py
+++ python/memory/PyROOT_memorytests.py
@@ -62,25 +62,77 @@ class Memory1TestCase( unittest.TestCase ):
       self.assertEqual( MemTester.counter, 1 )
       MemTester.CallRef( a );
       self.assertEqual( MemTester.counter, 1 )
+
+      MemTester.CallRef._mempolicy = kMemoryStrict
+      MemTester.CallRef( a );
+      self.assertEqual( MemTester.counter, 1 )
+
+      MemTester.CallRef._mempolicy = kMemoryHeuristics
+      MemTester.CallRef( a );
+      self.assertEqual( MemTester.counter, 1 )
+
       del a
       self.assertEqual( MemTester.counter, 0 )
 
       MemTester.CallConstRef( MemTester() )
       self.assertEqual( MemTester.counter, 0 )
 
-    # give up ownership in case of non-const pointer call only
+    # give up ownership in case of non-const pointer call only, unless overridden
       MemTester.CallConstPtr( MemTester() )
       self.assertEqual( MemTester.counter, 0 )
 
-      b = MemTester()
+      MemTester.CallConstPtr._mempolicy = kMemoryStrict
+      MemTester.CallConstPtr( MemTester() )
+      self.assertEqual( MemTester.counter, 0 )
+
+      MemTester.CallConstPtr._mempolicy = kMemoryHeuristics
+      MemTester.CallConstPtr( MemTester() )
+      self.assertEqual( MemTester.counter, 0 )
+
+      b1 = MemTester()
       self.assertEqual( MemTester.counter, 1 )
-      MemTester.CallPtr( b );
+      MemTester.CallPtr( b1 );
       self.assertEqual( MemTester.counter, 1 )
-      del b
+      del b1
       self.assertEqual( MemTester.counter, 1 )
 
+      b2 = MemTester()
+      self.assertEqual( MemTester.counter, 2 )
+      SetMemoryPolicy( kMemoryStrict )
+      MemTester.CallPtr( b2 );
+      self.assertEqual( MemTester.counter, 2 )
+      del b2
+      self.assertEqual( MemTester.counter, 1 )
+
+      b3 = MemTester()
+      self.assertEqual( MemTester.counter, 2 )
+      SetMemoryPolicy( kMemoryHeuristics )
+      MemTester.CallPtr( b3 );
+      self.assertEqual( MemTester.counter, 2 )
+      del b3
+      self.assertEqual( MemTester.counter, 2 )
+
+      b4 = MemTester()
+      self.assertEqual( MemTester.counter, 3 )
+      MemTester.CallPtr._mempolicy = kMemoryStrict
+      MemTester.CallPtr( b4 );
+      self.assertEqual( MemTester.counter, 3 )
+      del b4
+      self.assertEqual( MemTester.counter, 2 )
+
+      b5 = MemTester()
+      self.assertEqual( MemTester.counter, 3 )
+      SetMemoryPolicy( kMemoryStrict )
+      MemTester.CallPtr._mempolicy = kMemoryHeuristics
+      MemTester.CallPtr( b5 );
+      self.assertEqual( MemTester.counter, 3 )
+      del b5
+      self.assertEqual( MemTester.counter, 3 )
+
     # test explicit destruction
+      SetMemoryPolicy( kMemoryHeuristics )
       MemTester().counter = 1      # silly way of setting it to 0
+      MemTester.CallPtr._mempolicy = kMemoryHeuristics
       self.assertEqual( MemTester.counter, 0 )
       c = MemTester()
       self.assertEqual( MemTester.counter, 1 )
@@ -91,6 +143,7 @@ class Memory1TestCase( unittest.TestCase ):
       self.assertEqual( MemTester.counter, 0 )
       del c             # c not derived from TObject, no notification
       self.assertEqual( MemTester.counter, 0 )
+
 
 ## actual test run
 if __name__ == '__main__':
