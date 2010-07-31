041ad67b95ee7faf40e8668d78a1fba6434ce2b4
Rene Brun <Rene.Brun@cern.ch>
2010-07-31 06:25:29 +0000

    In TMath::AreEqualRel take intoaccount the case when the two arguments may be null. see: http://root.cern.ch/phpBB3/viewtopic.php?f=3&t=10920`
    git-svn-id: http://root.cern.ch/svn/root/trunk@34684 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/mathcore/inc/TMath.h
+++ math/mathcore/inc/TMath.h
@@ -176,7 +176,7 @@ namespace TMath {
    }
    inline Bool_t AreEqualRel(Double_t af, Double_t bf, Double_t relPrec) {
       //return kTRUE if relative difference between af and bf is less than relPrec
-      return TMath::Abs(af-bf) < 0.5*relPrec*(TMath::Abs(af)+TMath::Abs(bf));
+      return TMath::Abs(af-bf) <= 0.5*relPrec*(TMath::Abs(af)+TMath::Abs(bf));
    }
 
    /* ******************** */
