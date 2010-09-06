1c51c3ff1bc5e71a2f84c677abc41f5a0c7ef2fc
Olivier Couet <Olivier.Couet@cern.ch>
2010-09-06 09:55:17 +0000

    - Small fix
    git-svn-id: http://root.cern.ch/svn/root/trunk@35158 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/doc/macros/fonts.C
+++ core/base/doc/macros/fonts.C
@@ -7,7 +7,7 @@ TCanvas * fonts()
                                                                                 
    double y = 0.95;
    for (int f = 12; f<=152; f+=10) {
-      if (f!=142) drawtext(0.02,y, f,"ABCDFGF abcdefgh 0123456789 @#$");
+      if (f!=142) drawtext(0.02,y, f,"ABCDEFGH abcdefgh 0123456789 @#$");
       else drawtext(0.02,y, f,"ABCD efgh 01234 @#$");
       y- = 0.065;
    }
@@ -19,5 +19,6 @@ void drawtext(double x, double y, int f, char *s)
    TLatex *t = new TLatex(x,y,Form("#font[41]{%d :} %s",f,s));
    t->SetTextFont(f);
    t->SetTextAlign(12);
+   t->SetTextSize(0.048);
    t->Draw();
 }
