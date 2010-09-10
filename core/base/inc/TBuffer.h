e753e8c75d06773cb15e825bae03d13b217ccb86
Philippe Canal <pcanal@fnal.gov>
2010-09-10 12:40:53 +0000

    Make TBuffer::Expand public, to allow external object (TBaskset) to re-size the buffer pre-emptively
    git-svn-id: http://root.cern.ch/svn/root/trunk@35224 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/TBuffer.h
+++ core/base/inc/TBuffer.h
@@ -53,8 +53,6 @@ protected:
    TBuffer(const TBuffer &);           // not implemented
    void operator=(const TBuffer &);    // not implemented
 
-   void Expand(Int_t newsize);  // expand buffer to newsize
-
    Int_t Read(const char *name) { return TObject::Read(name); }
    Int_t Write(const char *name, Int_t opt, Int_t bufs)
                               { return TObject::Write(name, opt, bufs); }
@@ -87,6 +85,7 @@ public:
    Int_t    BufferSize() const { return fBufSize; }
    void     DetachBuffer() { fBuffer = 0; }
    Int_t    Length()     const { return (Int_t)(fBufCur - fBuffer); }
+   void     Expand(Int_t newsize);  // expand buffer to newsize
 
    virtual Bool_t     CheckObject(const TObject *obj) = 0;
    virtual Bool_t     CheckObject(const void *obj, const TClass *ptrClass) = 0;
