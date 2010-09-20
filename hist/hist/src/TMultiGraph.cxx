a5273a46bca2dc19f47af35ea62268fd58c801e9
Rene Brun <Rene.Brun@cern.ch>
2010-09-20 21:46:00 +0000

    Fix calls to strlcpy
    git-svn-id: http://root.cern.ch/svn/root/trunk@35495 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/hist/src/TMultiGraph.cxx
+++ hist/hist/src/TMultiGraph.cxx
@@ -786,12 +786,12 @@ void TMultiGraph::Paint(Option_t *option)
             lastx  = fHistogram->GetXaxis()->GetLast();
             if (nch) {
                xtitle = new char[nch+1];
-               strlcpy(xtitle,fHistogram->GetXaxis()->GetTitle(),nch+1);
+               strlcpy(xtitle,fHistogram->GetXaxis()->GetTitle(),nch);
             }
             nch = strlen(fHistogram->GetYaxis()->GetTitle());
             if (nch) {
                ytitle = new char[nch+1];
-               strlcpy(ytitle,fHistogram->GetYaxis()->GetTitle(),nch+1);
+               strlcpy(ytitle,fHistogram->GetYaxis()->GetTitle(),nch);
             }
             delete fHistogram;
             fHistogram = 0;
