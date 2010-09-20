6af2d2d3ff4a555b7ff9d24763ab3b34279e6199
Rene Brun <Rene.Brun@cern.ch>
2010-09-20 08:01:27 +0000

    From Bertrand: Fix uninitialized variables (coverity)
    git-svn-id: http://root.cern.ch/svn/root/trunk@35422 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/TControlBarImp.h
+++ core/base/inc/TControlBarImp.h
@@ -38,8 +38,8 @@ protected:
    TControlBarButton *fClicked; //Last clicked button 
    
 public:
-   TControlBarImp(TControlBar *c, const char * = "") : fControlBar(c), fClicked(0) { }
-   TControlBarImp(TControlBar *c, const char *, Int_t, Int_t) : fControlBar(c), fClicked(0) { }
+   TControlBarImp(TControlBar *c, const char * = "") : fControlBar(c), fXpos(0), fYpos(0), fClicked(0) { }
+   TControlBarImp(TControlBar *c, const char *, Int_t, Int_t) : fControlBar(c), fXpos(0), fYpos(0), fClicked(0) { }
    virtual ~TControlBarImp() { }
 
    virtual TControlBar *GetControlBar() { return fControlBar; }
