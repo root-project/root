1d989a1c13b3cf9861fb560b39da42565ad592cb
Rene Brun <Rene.Brun@cern.ch>
2010-09-21 12:41:48 +0000

    Fix secure coding coverity
    git-svn-id: http://root.cern.ch/svn/root/trunk@35530 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/graf/src/TLink.cxx
+++ graf2d/graf/src/TLink.cxx
@@ -51,7 +51,7 @@ TLink::TLink(Double_t x, Double_t y, void *pointer)
 
    fLink  = pointer;
    static char line[16];
-   sprintf(line,"->%lx ", (Long_t)pointer);
+   snprintf(line,16,"->%lx ", (Long_t)pointer);
    SetTitle(line);
 }
 
