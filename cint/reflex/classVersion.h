78463c52c30892852821f58d266596bf7725b8db
Axel Naumann <Axel.Naumann@cern.ch>
2010-09-21 09:40:13 +0000

    Add several tests for class version / TObject inheritance / missing dictionaries with classdef, following r35512,35513. Simplify driver build (no CINT parsing of Reflex dictionaries etc)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2138 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/reflex/classVersion.h
+++ cint/reflex/classVersion.h
@@ -1,9 +1,35 @@
+#include "TObject.h"
+
 class WithClassVersion {
  public:
    int hasAClassVersion;
 };
 
-#ifndef __CINT__
+class WithClassDef {
+   ClassDef(WithClassDef, 12);
+};
+
+class NoIO {
+   ClassDef(NoIO, 0);
+};
+
+class FromTObject: public TObject {
+   ClassDef(FromTObject, 13);
+};
+
+class NoDictionary {
+   ClassDef(NoDictionary, 14);
+};
+TClass* NoDictionary::Class() { return 0; }
+const char* NoDictionary::Class_Name() { return 0; }
+void NoDictionary::Dictionary() {}
+void NoDictionary::ShowMembers(TMemberInspector&) {}
+void NoDictionary::Streamer(TBuffer&) {}
+int NoDictionary::ImplFileLine() { return -1; }
+const char* NoDictionary::ImplFileName() { return 0; }
+
+class NoDictionaryTObj: public TObject {
+};
 
 #include <vector>
 #include <string>
@@ -15,5 +41,3 @@ template <class T> class MyTemp {
 template class std::vector<std::string>;
 template class std::vector<WithClassVersion>;
 template class MyTemp<std::vector<std::string> >;
-
-#endif
--- cint/reflex/classVersion_test.C
+++ cint/reflex/classVersion_test.C
@@ -5,14 +5,25 @@
 using namespace ROOT::Cintex;
 using namespace ROOT::Reflex;
 
+void checkVersion(const char* name, short version, bool inhFromTObj = false) {
+   printf("\nCLASS %s, version %d, isATObject=%d\n", name,
+          (int)version, (int)inhFromTObj);
+   TClass* cl = TClass::GetClass(name);
+   RflxAssert(cl);
+   RflxEqual(cl->GetClassVersion(), version);
+   RflxEqual(inhFromTObj, cl->InheritsFrom(TObject::Class()));
+}
+
 void classVersion_test() {
    Type tWithClassVersion = Type::ByName("WithClassVersion");
    RflxAssert(tWithClassVersion);
    Cintex::Enable();
-   TClass* cl = TClass::GetClass("WithClassVersion");
-   RflxEqual(cl->GetClassVersion(), 42);
+   checkVersion("WithClassVersion", 42);
+   checkVersion("WithClassDef", 12);
+   checkVersion("NoIO", 0);
+   checkVersion("FromTObject", 13, true);
+   RflxAssert(!TClass::GetClass("NoDictionary"));
+   RflxAssert(!TClass::GetClass("NoDictionaryTObj"));
 
-   cl = TClass::GetClass("MyTemp<std::vector<std::string> >");
-   // fprintf(stdout,"cl=%p\n",cl);
-
+   RflxAssert(TClass::GetClass("MyTemp<std::vector<std::string> >"));
 }
