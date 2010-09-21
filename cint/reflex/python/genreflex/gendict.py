449e831bab426aed4abfe27cd5ab79cdd878e926
Axel Naumann <Axel.Naumann@cern.ch>
2010-09-21 08:52:29 +0000

    Propagate the correct class version to TClass by invoking Class_Version() from ClasDef.
    git-svn-id: http://root.cern.ch/svn/root/trunk@35512 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/reflex/python/genreflex/gendict.py
+++ cint/reflex/python/genreflex/gendict.py
@@ -2631,6 +2631,7 @@ def ClassDefImplementation(selclasses, self) :
       extraval = '!RAW!' + str(derivesFromTObject)
       if attrs.has_key('extra') : attrs['extra']['ClassDef'] = extraval
       else                      : attrs['extra'] = {'ClassDef': extraval}
+      attrs['extra']['ClassVersion'] = '!RAW!' + clname + '::Class_Version()'
       id = attrs['id']
       template = ''
       namespacelevel = 0
