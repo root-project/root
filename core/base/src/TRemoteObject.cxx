c29931260709a241042cff9ff515dff3402a5a2a
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-09-22 14:15:45 +0000

    fix coverity 10701.
    git-svn-id: http://root.cern.ch/svn/root/trunk@35590 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/src/TRemoteObject.cxx
+++ core/base/src/TRemoteObject.cxx
@@ -90,7 +90,7 @@ void TRemoteObject::Browse(TBrowser *b)
          b->SetRefreshFlag(kFALSE);
       gApplication->SetBit(TApplication::kProcessRemotely);
       TObject *obj = (TObject *)gROOT->ProcessLine(Form("((TApplicationServer *)gApplication)->BrowseKey(\"%s\");", GetName()));
-      if (b && obj) {
+      if (obj) {
          if (obj->IsA()->GetMethodWithPrototype("SetDirectory", "TDirectory*"))
             gROOT->ProcessLine(Form("((%s *)0x%lx)->SetDirectory(0);", obj->ClassName(), (ULong_t)obj));
          obj->Browse(b);
