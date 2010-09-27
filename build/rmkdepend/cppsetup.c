4f1e6c3e720ad8f55a8fc79ea8fce33366c5b85d
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-09-27 12:58:54 +0000

    fix coverity 13498.
    git-svn-id: http://root.cern.ch/svn/root/trunk@35791 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/rmkdepend/cppsetup.c
+++ build/rmkdepend/cppsetup.c
@@ -29,7 +29,7 @@ in this Software without prior written authorization from the X Consortium.
 
 #include "def.h"
 
-#ifdef	CPP
+#ifdef CPP
 /*
  * This file is strictly for the sake of cpy.y and yylex.c (if
  * you indeed have the source for cpp).
@@ -49,196 +49,197 @@ in this Software without prior written authorization from the X Consortium.
 /*
  * These variables used by cpy.y and yylex.c
  */
-extern char	*outp, *inp, *newp, *pend;
-extern char	*ptrtab;
-extern char	fastab[];
-extern char	slotab[];
+extern char *outp, *inp, *newp, *pend;
+extern char *ptrtab;
+extern char fastab[];
+extern char slotab[];
 
 /*
  * cppsetup
  */
-struct filepointer	*currentfile;
-struct inclist		*currentinc;
+struct filepointer *currentfile;
+struct inclist  *currentinc;
 
 int
 cppsetup(line, filep, inc)
-	register char	*line;
-	register struct filepointer	*filep;
-	register struct inclist		*inc;
+register char *line;
+register struct filepointer *filep;
+register struct inclist  *inc;
 {
-	register char *p, savec;
-	static boolean setupdone = FALSE;
-	boolean	value;
+   register char *p, savec;
+   static boolean setupdone = FALSE;
+   boolean value;
 
-	if (!setupdone) {
-		cpp_varsetup();
-		setupdone = TRUE;
-	}
+   if (!setupdone) {
+      cpp_varsetup();
+      setupdone = TRUE;
+   }
 
-	currentfile = filep;
-	currentinc = inc;
-	inp = newp = line;
-	for (p=newp; *p; p++)
-		;
+   currentfile = filep;
+   currentinc = inc;
+   inp = newp = line;
+   for (p = newp; *p; p++)
+      ;
 
-	/*
-	 * put a newline back on the end, and set up pend, etc.
-	 */
-	*p++ = '\n';
-	savec = *p;
-	*p = '\0';
-	pend = p;
+   /*
+    * put a newline back on the end, and set up pend, etc.
+    */
+   *p++ = '\n';
+   savec = *p;
+   *p = '\0';
+   pend = p;
 
-	ptrtab = slotab+COFF;
-	*--inp = SALT;
-	outp=inp;
-	value = yyparse();
-	*p = savec;
-	return(value);
+   ptrtab = slotab + COFF;
+   *--inp = SALT;
+   outp = inp;
+   value = yyparse();
+   *p = savec;
+   return(value);
 }
 
 struct symtab **lookup(symbol)
-	char	*symbol;
+         char *symbol;
 {
-	static struct symtab    *undefined;
-	struct symtab   **sp;
+   static struct symtab    *undefined;
+   struct symtab   **sp;
 
-	sp = isdefined(symbol, currentinc, NULL);
-	if (sp == NULL) {
-		sp = &undefined;
-		(*sp)->s_value = NULL;
-	}
-	return (sp);
+   sp = isdefined(symbol, currentinc, NULL);
+   if (sp == NULL) {
+      sp = &undefined;
+      (*sp)->s_value = NULL;
+   }
+   return (sp);
 }
 
-pperror(tag, x0,x1,x2,x3,x4)
-	int	tag,x0,x1,x2,x3,x4;
+pperror(tag, x0, x1, x2, x3, x4)
+int tag, x0, x1, x2, x3, x4;
 {
-	warning("\"%s\", line %d: ", currentinc->i_file, currentfile->f_line);
-	warning(x0,x1,x2,x3,x4);
+   warning("\"%s\", line %d: ", currentinc->i_file, currentfile->f_line);
+   warning(x0, x1, x2, x3, x4);
 }
 
 
 yyerror(s)
-	register char	*s;
+register char *s;
 {
-	fatalerr("Fatal error: %s\n", s);
+   fatalerr("Fatal error: %s\n", s);
 }
 #else /* not CPP */
 
 #include "ifparser.h"
 struct _parse_data {
-    struct filepointer *filep;
-    struct inclist *inc;
-    const char *line;
+   struct filepointer *filep;
+   struct inclist *inc;
+   const char *line;
 };
 
 static const char *
-my_if_errors (ip, cp, expecting)
-    IfParser *ip;
-    const char *cp;
-    const char *expecting;
+my_if_errors(ip, cp, expecting)
+IfParser *ip;
+const char *cp;
+const char *expecting;
 {
-    struct _parse_data *pd = (struct _parse_data *) ip->data;
-    int lineno = pd->filep->f_line;
-    char *filename = pd->inc->i_file;
-    char prefix[300];
-    int prefixlen;
-    int i;
+   struct _parse_data *pd = (struct _parse_data *) ip->data;
+   int lineno = pd->filep->f_line;
+   char *filename = pd->inc->i_file;
+   char *prefix;
+   int prefixlen;
+   int i;
 
-    sprintf (prefix, "\"%s\":%d", filename, lineno);
-    prefixlen = strlen(prefix);
-    fprintf (stderr, "%s: warning: %s", prefix, pd->line);
-    i = cp - pd->line;
-    if (i > 0 && pd->line[i-1] != '\n') {
-	putc ('\n', stderr);
-    }
-    for (i += prefixlen + 11; i > 0; i--) {
-	putc (' ', stderr);
-    }
-    fprintf (stderr, "^--- expecting %s\n", expecting);
-    return NULL;
+   prefix = (char*)malloc(strlen(filename) + 32);
+   sprintf(prefix, "\"%s\":%d", filename, lineno);
+   prefixlen = strlen(prefix);
+   fprintf(stderr, "%s: warning: %s", prefix, pd->line);
+   i = cp - pd->line;
+   if (i > 0 && pd->line[i-1] != '\n') {
+      putc('\n', stderr);
+   }
+   for (i += prefixlen + 11; i > 0; i--) {
+      putc(' ', stderr);
+   }
+   fprintf(stderr, "^--- expecting %s\n", expecting);
+   free(prefix);
+   return NULL;
 }
 
 
 #define MAXNAMELEN 256
 
 static struct symtab **
-lookup_variable (ip, var, len)
-    IfParser *ip;
-    const char *var;
-    int len;
+         lookup_variable(ip, var, len)
+         IfParser *ip;
+const char *var;
+int len;
 {
-    char tmpbuf[MAXNAMELEN + 1];
-    struct _parse_data *pd = (struct _parse_data *) ip->data;
+   char tmpbuf[MAXNAMELEN + 1];
+   struct _parse_data *pd = (struct _parse_data *) ip->data;
 
-    if (len > MAXNAMELEN)
-	return 0;
+   if (len > MAXNAMELEN)
+      return 0;
 
-    strncpy (tmpbuf, var, len);
-    tmpbuf[len] = '\0';
-    return isdefined (tmpbuf, pd->inc, NULL);
+   strncpy(tmpbuf, var, len);
+   tmpbuf[len] = '\0';
+   return isdefined(tmpbuf, pd->inc, NULL);
 }
 
 
 static int
-my_eval_defined (ip, var, len)
-    IfParser *ip;
-    const char *var;
-    int len;
+my_eval_defined(ip, var, len)
+IfParser *ip;
+const char *var;
+int len;
 {
-    if (lookup_variable (ip, var, len))
-	return 1;
-    else
-	return 0;
+   if (lookup_variable(ip, var, len))
+      return 1;
+   else
+      return 0;
 }
 
 #define isvarfirstletter(ccc) (isalpha(ccc) || (ccc) == '_')
 
 static long
-my_eval_variable (ip, var, len)
-    IfParser *ip;
-    const char *var;
-    int len;
+my_eval_variable(ip, var, len)
+IfParser *ip;
+const char *var;
+int len;
 {
-    struct symtab **s;
+   struct symtab **s;
 
-    s = lookup_variable (ip, var, len);
-    if (!s)
-	return 0;
-    do {
-	var = (*s)->s_value;
-	if (!isvarfirstletter(*var))
-	    break;
-	s = lookup_variable (ip, var, (int)strlen(var));
-    } while (s);
+   s = lookup_variable(ip, var, len);
+   if (!s)
+      return 0;
+   do {
+      var = (*s)->s_value;
+      if (!isvarfirstletter(*var))
+         break;
+      s = lookup_variable(ip, var, (int)strlen(var));
+   } while (s);
 
-    return strtol(var, NULL, 0);
+   return strtol(var, NULL, 0);
 }
 
 
 int cppsetup(line, filep, inc)
-	register char	*line;
-	register struct filepointer	*filep;
-	register struct inclist		*inc;
+register char *line;
+register struct filepointer *filep;
+register struct inclist  *inc;
 {
-    IfParser ip;
-    struct _parse_data pd;
-    long val = 0;
+   IfParser ip;
+   struct _parse_data pd;
+   long val = 0;
 
-    pd.filep = filep;
-    pd.inc = inc;
-    pd.line = line;
-    ip.funcs.handle_error = my_if_errors;
-    ip.funcs.eval_defined = my_eval_defined;
-    ip.funcs.eval_variable = my_eval_variable;
-    ip.data = (char *) &pd;
+   pd.filep = filep;
+   pd.inc = inc;
+   pd.line = line;
+   ip.funcs.handle_error = my_if_errors;
+   ip.funcs.eval_defined = my_eval_defined;
+   ip.funcs.eval_variable = my_eval_variable;
+   ip.data = (char *) & pd;
 
-    (void) ParseIfExpression (&ip, line, &val);
-    if (val)
-	return IF;
-    else
-	return IFFALSE;
+   (void) ParseIfExpression(&ip, line, &val);
+   if (val)
+      return IF;
+   else
+      return IFFALSE;
 }
 #endif /* CPP */
-
--- cint/tool/rmkdepend/include.c
+++ cint/tool/rmkdepend/include.c
@@ -29,55 +29,55 @@ in this Software without prior written authorization from the X Consortium.
 
 #include "def.h"
 
-extern struct	inclist	inclist[ MAXFILES ],
-			*inclistp;
-extern char	*includedirs[ ];
-extern char	*notdotdot[ ];
+extern struct inclist inclist[ MAXFILES ],
+            *inclistp;
+extern char *includedirs[ ];
+extern char *notdotdot[ ];
 extern boolean show_where_not;
 extern boolean warn_multiple;
 
 
 boolean
 isdot(p)
-	register char	*p;
+register char *p;
 {
-	if(p && *p++ == '.' && *p++ == '\0')
-		return(TRUE);
-	return(FALSE);
+   if (p && *p++ == '.' && *p++ == '\0')
+      return(TRUE);
+   return(FALSE);
 }
 
 boolean
 isdotdot(p)
-	register char	*p;
+register char *p;
 {
-	if(p && *p++ == '.' && *p++ == '.' && *p++ == '\0')
-		return(TRUE);
-	return(FALSE);
+   if (p && *p++ == '.' && *p++ == '.' && *p++ == '\0')
+      return(TRUE);
+   return(FALSE);
 }
 
 boolean
 issymbolic(dir, component)
-	register char	*dir, *component;
+register char *dir, *component;
 {
 #ifdef S_IFLNK
-	struct stat	st;
-	char	buf[ BUFSIZ ], **pp;
+   struct stat st;
+   char buf[ BUFSIZ ], **pp;
 
-	sprintf(buf, "%s%s%s", dir, *dir ? "/" : "", component);
-	for (pp=notdotdot; *pp; pp++)
-		if (strcmp(*pp, buf) == 0)
-			return (TRUE);
-	if (lstat(buf, &st) == 0
-	&& (st.st_mode & S_IFMT) == S_IFLNK) {
-		*pp++ = copy(buf);
-		if (pp >= &notdotdot[ MAXDIRS ])
-			fatalerr("out of .. dirs, increase MAXDIRS\n");
-		return(TRUE);
-	}
+   sprintf(buf, "%s%s%s", dir, *dir ? "/" : "", component);
+   for (pp = notdotdot; *pp; pp++)
+      if (strcmp(*pp, buf) == 0)
+         return (TRUE);
+   if (lstat(buf, &st) == 0
+         && (st.st_mode & S_IFMT) == S_IFLNK) {
+      *pp++ = copy(buf);
+      if (pp >= &notdotdot[ MAXDIRS ])
+         fatalerr("out of .. dirs, increase MAXDIRS\n");
+      return(TRUE);
+   }
 #else
-	if (dir && component) { }  /* use arguments */
+   if (dir && component) { }  /* use arguments */
 #endif
-	return(FALSE);
+   return(FALSE);
 }
 
 /*
@@ -87,251 +87,248 @@ issymbolic(dir, component)
  */
 void
 remove_dotdot(path)
-	char	*path;
+char *path;
 {
-	register char	*end, *from, *to, **cp;
-	char		*components[ MAXFILES ],
-			newpath[ BUFSIZ ];
-	boolean		component_copied;
+   register char *end, *from, *to, **cp;
+   char  *components[ MAXFILES ],
+   newpath[ BUFSIZ ];
+   boolean  component_copied;
 
-	/*
-	 * slice path up into components.
-	 */
-	to = newpath;
-	if (*path == '/')
-		*to++ = '/';
-	*to = '\0';
-	cp = components;
-	for (from=end=path; *end; end++)
-		if (*end == '/') {
-			while (*end == '/')
-				*end++ = '\0';
-			if (*from)
-				*cp++ = from;
-			from = end;
-		}
-	*cp++ = from;
-	*cp = NULL;
+   /*
+    * slice path up into components.
+    */
+   to = newpath;
+   if (*path == '/')
+      *to++ = '/';
+   *to = '\0';
+   cp = components;
+   for (from = end = path; *end; end++)
+      if (*end == '/') {
+         while (*end == '/')
+            *end++ = '\0';
+         if (*from)
+            *cp++ = from;
+         from = end;
+      }
+   *cp++ = from;
+   *cp = NULL;
 
-	/*
-	 * Recursively remove all 'x/..' component pairs.
-	 */
-	cp = components;
-	while(*cp) {
-		if (!isdot(*cp) && !isdotdot(*cp) && isdotdot(*(cp+1))
-		    && !issymbolic(newpath, *cp))
-		{
-		    char **fp = cp + 2;
-		    char **tp = cp;
+   /*
+    * Recursively remove all 'x/..' component pairs.
+    */
+   cp = components;
+   while (*cp) {
+      if (!isdot(*cp) && !isdotdot(*cp) && isdotdot(*(cp + 1))
+            && !issymbolic(newpath, *cp)) {
+         char **fp = cp + 2;
+         char **tp = cp;
 
-		    do
-			*tp++ = *fp; /* move all the pointers down */
-		    while (*fp++);
-		    if (cp != components)
-			cp--;	/* go back and check for nested ".." */
-		} else {
-		    cp++;
-		}
-	}
-	/*
-	 * Concatenate the remaining path elements.
-	 */
-	cp = components;
-	component_copied = FALSE;
-	while(*cp) {
-		if (component_copied)
-			*to++ = '/';
-		component_copied = TRUE;
-		for (from = *cp; *from; )
-			*to++ = *from++;
-		*to = '\0';
-		cp++;
-	}
-	*to++ = '\0';
+         do
+            *tp++ = *fp; /* move all the pointers down */
+         while (*fp++);
+         if (cp != components)
+            cp--; /* go back and check for nested ".." */
+      } else {
+         cp++;
+      }
+   }
+   /*
+    * Concatenate the remaining path elements.
+    */
+   cp = components;
+   component_copied = FALSE;
+   while (*cp) {
+      if (component_copied)
+         *to++ = '/';
+      component_copied = TRUE;
+      for (from = *cp; *from;)
+         *to++ = *from++;
+      *to = '\0';
+      cp++;
+   }
+   *to++ = '\0';
 
-	/*
-	 * copy the reconstituted path back to our pointer.
-	 */
-	strcpy(path, newpath);
+   /*
+    * copy the reconstituted path back to our pointer.
+    */
+   strcpy(path, newpath);
 }
 
 /*
  * Add an include file to the list of those included by 'file'.
  */
 struct inclist *newinclude(newfile, incstring)
-	register char	*newfile, *incstring;
+         register char *newfile, *incstring;
 {
-	register struct inclist	*ip;
+   register struct inclist *ip;
 
-	/*
-	 * First, put this file on the global list of include files.
-	 */
-	ip = inclistp++;
-	if (inclistp == inclist + MAXFILES - 1)
-		fatalerr("out of space: increase MAXFILES\n");
-	ip->i_file = copy(newfile);
+   /*
+    * First, put this file on the global list of include files.
+    */
+   ip = inclistp++;
+   if (inclistp == inclist + MAXFILES - 1)
+      fatalerr("out of space: increase MAXFILES\n");
+   ip->i_file = copy(newfile);
 
-	if (incstring == NULL)
-		ip->i_incstring = ip->i_file;
-	else
-		ip->i_incstring = copy(incstring);
+   if (incstring == NULL)
+      ip->i_incstring = ip->i_file;
+   else
+      ip->i_incstring = copy(incstring);
 
-	return(ip);
+   return(ip);
 }
 
 void
 included_by(ip, newfile)
-	register struct inclist	*ip, *newfile;
+register struct inclist *ip, *newfile;
 {
-	register int i;
+   register int i;
 
-	if (ip == NULL)
-		return;
-	/*
-	 * Put this include file (newfile) on the list of files included
-	 * by 'file'.  If 'file' is NULL, then it is not an include
-	 * file itself (i.e. was probably mentioned on the command line).
-	 * If it is already on the list, don't stick it on again.
-	 */
-	if (ip->i_list == NULL) {
-		ip->i_list = (struct inclist **)
-			malloc(sizeof(struct inclist *) * ++ip->i_listlen);
-		ip->i_merged = (boolean *)
-		    malloc(sizeof(boolean) * ip->i_listlen);
-	} else {
-		for (i=0; i<ip->i_listlen; i++)
-			if (ip->i_list[ i ] == newfile) {
-			    i = strlen(newfile->i_file);
-			    if (!(ip->i_flags & INCLUDED_SYM) &&
-				!(i > 2 &&
-				  newfile->i_file[i-1] == 'c' &&
-				  newfile->i_file[i-2] == '.'))
-			    {
-				/* only bitch if ip has */
-				/* no #include SYMBOL lines  */
-				/* and is not a .c file */
-				if (warn_multiple)
-				{
-					warning("%s includes %s more than once!\n",
-						ip->i_file, newfile->i_file);
-					warning1("Already have\n");
-					for (i=0; i<ip->i_listlen; i++)
-						warning1("\t%s\n", ip->i_list[i]->i_file);
-				}
-			    }
-			    return;
-			}
-		ip->i_list = (struct inclist **) realloc(ip->i_list,
-			sizeof(struct inclist *) * ++ip->i_listlen);
-		ip->i_merged = (boolean *)
-		    realloc(ip->i_merged, sizeof(boolean) * ip->i_listlen);
-	}
-	ip->i_list[ ip->i_listlen-1 ] = newfile;
-	ip->i_merged[ ip->i_listlen-1 ] = FALSE;
+   if (ip == NULL)
+      return;
+   /*
+    * Put this include file (newfile) on the list of files included
+    * by 'file'.  If 'file' is NULL, then it is not an include
+    * file itself (i.e. was probably mentioned on the command line).
+    * If it is already on the list, don't stick it on again.
+    */
+   if (ip->i_list == NULL) {
+      ip->i_list = (struct inclist **)
+            malloc(sizeof(struct inclist *) * ++ip->i_listlen);
+      ip->i_merged = (boolean *)
+            malloc(sizeof(boolean) * ip->i_listlen);
+   } else {
+      for (i = 0; i < ip->i_listlen; i++)
+         if (ip->i_list[ i ] == newfile) {
+            i = strlen(newfile->i_file);
+            if (!(ip->i_flags & INCLUDED_SYM) &&
+                  !(i > 2 &&
+                    newfile->i_file[i-1] == 'c' &&
+                    newfile->i_file[i-2] == '.')) {
+               /* only bitch if ip has */
+               /* no #include SYMBOL lines  */
+               /* and is not a .c file */
+               if (warn_multiple) {
+                  warning("%s includes %s more than once!\n",
+                          ip->i_file, newfile->i_file);
+                  warning1("Already have\n");
+                  for (i = 0; i < ip->i_listlen; i++)
+                     warning1("\t%s\n", ip->i_list[i]->i_file);
+               }
+            }
+            return;
+         }
+      ip->i_list = (struct inclist **) realloc(ip->i_list,
+            sizeof(struct inclist *) * ++ip->i_listlen);
+      ip->i_merged = (boolean *)
+            realloc(ip->i_merged, sizeof(boolean) * ip->i_listlen);
+   }
+   ip->i_list[ ip->i_listlen-1 ] = newfile;
+   ip->i_merged[ ip->i_listlen-1 ] = FALSE;
 }
 
 void
-inc_clean ()
+inc_clean()
 {
-	register struct inclist *ip;
+   register struct inclist *ip;
 
-	for (ip = inclist; ip < inclistp; ip++) {
-		ip->i_flags &= ~MARKED;
-		ip->i_flags &= ~SEARCHED;
-		undefine_all(ip);
-	}
+   for (ip = inclist; ip < inclistp; ip++) {
+      ip->i_flags &= ~MARKED;
+      ip->i_flags &= ~SEARCHED;
+      undefine_all(ip);
+   }
 }
 
 struct inclist *inc_path(file, include, dot)
-	register char	*file,
-			*include;
-	boolean	dot;
+         register char *file,
+         *include;
+boolean dot;
 {
-	static char	path[ BUFSIZ ];
-	register char		**pp, *p;
-	register struct inclist	*ip;
-	struct stat	st;
-	boolean	found = FALSE;
+   static char path[ BUFSIZ ];
+   register char  **pp, *p;
+   register struct inclist *ip;
+   struct stat st;
+   boolean found = FALSE;
 
-	/*
-	 * Check all previously found include files for a path that
-	 * has already been expanded.
-	 */
-	for (ip = inclist; ip->i_file; ip++)
-	    if ((strcmp(ip->i_incstring, include) == 0) &&
-		!(ip->i_flags & INCLUDED_SYM))
-	    {
-		found = TRUE;
-		break;
-	    }
+   /*
+    * Check all previously found include files for a path that
+    * has already been expanded.
+    */
+   for (ip = inclist; ip->i_file; ip++)
+      if ((strcmp(ip->i_incstring, include) == 0) &&
+            !(ip->i_flags & INCLUDED_SYM)) {
+         found = TRUE;
+         break;
+      }
 
-	/*
-	 * If the path was surrounded by "" or is an absolute path,
-	 * then check the exact path provided.
-	 */
-	if (!found && (dot || *include == '/')) {
+   /*
+    * If the path was surrounded by "" or is an absolute path,
+    * then check the exact path provided.
+    */
+   if (!found && (dot || *include == '/')) {
 #ifdef _WIN32
-		if (stat(include, &st) == 0 && (st.st_mode & S_IFREG)) {
+      if (stat(include, &st) == 0 && (st.st_mode & S_IFREG)) {
 #else
-		if (stat(include, &st) == 0 && S_ISREG(st.st_mode)) {
+      if (stat(include, &st) == 0 && S_ISREG(st.st_mode)) {
 #endif
-			ip = newinclude(include, include);
-			found = TRUE;
-		}
-		else if (show_where_not)
-			warning1("\tnot in %s\n", include);
-	}
+         ip = newinclude(include, include);
+         found = TRUE;
+      } else if (show_where_not)
+         warning1("\tnot in %s\n", include);
+   }
 
-	/*
-	 * If the path was surrounded by "" see if this include file is in the
-	 * directory of the file being parsed.
-	 */
-	if (!found && dot) {
-		for (p=file+strlen(file); p>file; p--)
-			if (*p == '/')
-				break;
-		if (p == file)
-			strcpy(path, include);
-		else {
-			strncpy(path, file, (p-file) + 1);
-			path[ (p-file) + 1 ] = '\0';
-			strcpy(path + (p-file) + 1, include);
-		}
-		remove_dotdot(path);
+   /*
+    * If the path was surrounded by "" see if this include file is in the
+    * directory of the file being parsed.
+    */
+   if (!found && dot) {
+      for (p = file + strlen(file); p > file; p--)
+         if (*p == '/')
+            break;
+      if (p == file)
+         strncpy(path, include, sizeof(path));
+      else {
+         strncpy(path, file, (p - file) + 1);
+         path[(p-file) + 1 ] = '\0';
+         strncpy(path + (p - file) + 1, include, sizeof(path) - (p - file + 1));
+      }
+      remove_dotdot(path);
 #ifdef _WIN32
-		if (stat(path, &st) == 0 && (st.st_mode & S_IFREG)) {
+      if (stat(path, &st) == 0 && (st.st_mode & S_IFREG)) {
 #else
-		if (stat(path, &st) == 0 && S_ISREG(st.st_mode)) {
+      if (stat(path, &st) == 0 && S_ISREG(st.st_mode)) {
 #endif
-			ip = newinclude(path, include);
-			found = TRUE;
-		}
-		else if (show_where_not)
-			warning1("\tnot in %s\n", path);
-	}
+         ip = newinclude(path, include);
+         found = TRUE;
+      } else if (show_where_not)
+         warning1("\tnot in %s\n", path);
+   }
 
-	/*
-	 * Check the include directories specified. (standard include dir
-	 * should be at the end.)
-	 */
-	if (!found)
-		for (pp = includedirs; *pp; pp++) {
-			sprintf(path, "%s/%s", *pp, include);
-			remove_dotdot(path);
+   /*
+    * Check the include directories specified. (standard include dir
+    * should be at the end.)
+    */
+   if (!found)
+      for (pp = includedirs; *pp; pp++) {
+         if (strlen(*pp) + strlen(include) + 2 > sizeof(path)) {
+            warning1("\t%s/%s too long\n", *pp, include);
+            continue;
+         }
+         sprintf(path, "%s/%s", *pp, include);
+         remove_dotdot(path);
 #ifdef _WIN32
-			if (stat(path, &st) == 0 && (st.st_mode & S_IFREG)) {
+         if (stat(path, &st) == 0 && (st.st_mode & S_IFREG)) {
 #else
-			if (stat(path, &st) == 0 && S_ISREG(st.st_mode)) {
+         if (stat(path, &st) == 0 && S_ISREG(st.st_mode)) {
 #endif
-				ip = newinclude(path, include);
-				found = TRUE;
-				break;
-			}
-			else if (show_where_not)
-				warning1("\tnot in %s\n", path);
-		}
+            ip = newinclude(path, include);
+            found = TRUE;
+            break;
+         } else if (show_where_not)
+            warning1("\tnot in %s\n", path);
+      }
 
-	if (!found)
-		ip = NULL;
-	return(ip);
+   if (!found)
+      ip = NULL;
+   return(ip);
 }
