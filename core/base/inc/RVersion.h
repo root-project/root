048e8dc80aca95ffad34bf902b01f1306cd210db
Rene Brun <Rene.Brun@cern.ch>
2010-09-29 13:52:15 +0000

    Start development version 5.27/07. This is the last development step before the production release 5.28 scheduled for December 15
    git-svn-id: http://root.cern.ch/svn/root/trunk@35858 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.27/06"
+#define ROOT_RELEASE "5.27/07"
 #define ROOT_RELEASE_DATE "Sep 29 2010"
-#define ROOT_RELEASE_TIME "10:42:31"
-#define ROOT_SVN_REVISION 35855
+#define ROOT_RELEASE_TIME "13:18:11"
+#define ROOT_SVN_REVISION 35856
 #define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 334598
+#define ROOT_VERSION_CODE 334599
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
