bebd8c4d685c1f789e4d420ab84f27168e05da5e
Rene Brun <Rene.Brun@cern.ch>
2010-09-30 09:11:00 +0000

    Fix Dereference before null check. coverity CID 10692
    git-svn-id: http://root.cern.ch/svn/root/trunk@35891 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/qt/src/TGQt.cxx
+++ graf2d/qt/src/TGQt.cxx
@@ -1365,6 +1365,7 @@ void  TGQt::DrawBox(int x1, int y1, int x2, int y2, EBoxMode mode)
 
    static const int Q3=0;
 #endif
+   if (!fSelectedWindow ) return;
    TQtLock lock;
    // Some workaround to fix issue from TBox::ExecuteEvent case pC. 
    // The reason of the problem has not been found yet.
@@ -1385,18 +1386,15 @@ void  TGQt::DrawBox(int x1, int y1, int x2, int y2, EBoxMode mode)
       return;
    }
 
-   if (fSelectedWindow )
-   {
-      if ((mode == kHollow) || (fQBrush->style() == Qt::NoBrush) )
-      { 
-         TQtPainter p(this,TQtPainter::kUpdatePen);
-         p.setBrush(Qt::NoBrush);
-         p.drawRect(x1,y2,x2-x1+Q3,y1-y2+Q3);
-      } else if (fQBrush->GetColor().alpha() ) {
-         TQtPainter p(this);
-         if (fQBrush->style() != Qt::SolidPattern) p.setPen(fQBrush->GetColor());
-         p.fillRect(x1,y2,x2-x1,y1-y2,*fQBrush);
-      }
+   if ((mode == kHollow) || (fQBrush->style() == Qt::NoBrush) )
+   { 
+      TQtPainter p(this,TQtPainter::kUpdatePen);
+      p.setBrush(Qt::NoBrush);
+      p.drawRect(x1,y2,x2-x1+Q3,y1-y2+Q3);
+   } else if (fQBrush->GetColor().alpha() ) {
+      TQtPainter p(this);
+      if (fQBrush->style() != Qt::SolidPattern) p.setPen(fQBrush->GetColor());
+      p.fillRect(x1,y2,x2-x1,y1-y2,*fQBrush);
    }
 }
 
