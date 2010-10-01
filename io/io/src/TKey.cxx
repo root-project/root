f47946b405e8be94672d03d77aa148aa47ccec86
Philippe Canal <pcanal@fnal.gov>
2010-10-01 10:31:39 +0000

    Fix cov 11612
    git-svn-id: http://root.cern.ch/svn/root/trunk@35987 27541ba8-7e3a-0410-8455-c3a389f83636

--- io/io/src/TKey.cxx
+++ io/io/src/TKey.cxx
@@ -730,7 +730,7 @@ TObject *TKey::ReadObj()
    if (gROOT->GetForceStyle()) tobj->UseCurrentStyle();
 
    if (cl->InheritsFrom(TDirectoryFile::Class())) {
-      TDirectory *dir = dynamic_cast<TDirectoryFile*>(tobj);
+      TDirectory *dir = static_cast<TDirectoryFile*>(tobj);
       dir->SetName(GetName());
       dir->SetTitle(GetTitle());
       dir->SetMother(fMotherDir);
