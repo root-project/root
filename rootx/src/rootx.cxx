77f79c840cefee4d738749f5a9b533673fa75240
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-10-01 12:37:43 +0000

    fix coverity 12341.
    git-svn-id: http://root.cern.ch/svn/root/trunk@35996 27541ba8-7e3a-0410-8455-c3a389f83636

--- rootx/src/rootx.cxx
+++ rootx/src/rootx.cxx
@@ -143,8 +143,11 @@ static int ReadUtmp()
    }
 
    n_read = fread(gUtmpContents, 1, size, utmp);
-   if (!ferror(utmp) && fclose(utmp) != EOF && n_read == size)
-      return size / sizeof(STRUCT_UTMP);
+   if (!ferror(utmp)) {
+      if (fclose(utmp) != EOF && n_read == size)
+         return size / sizeof(STRUCT_UTMP);
+   } else
+      fclose(utmp);
 
    free(gUtmpContents);
    gUtmpContents = 0;
