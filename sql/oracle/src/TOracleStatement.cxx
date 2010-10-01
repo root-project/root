root-project/root

commit 2060a32378d992f356a1bdfb9e4d4b4f3cec80e8

fix coverity report
git-svn-id: http://root.cern.ch/svn/root/trunk@35211 27541ba8-7e3a-0410-8455-c3a389f83636

sql/oracle/src/TOracleStatement.cxx
@@ -701,7 +701,7 @@ Bool_t TOracleStatement::NextResultRow()
         fBuffer[n].strbuf = 0;
         fBuffer[n].strbufsize = -1;
       }
-      if (!fResult->next()) {
+      if (!fResult->next() != oracle::occi::ResultSet::END_OF_FETCH) {
          fWorkingMode = 0;
          CloseBuffer();
          return kFALSE;

sql/oracle/src/TOracleResult.cxx
@@ -169,7 +169,7 @@ TSQLRow *TOracleResult::Next()
 
    // if select query,
    try {
-      if (fResult->next()) {
+      if (fResult->next() != oracle::occi::ResultSet::END_OF_FETCH) {
          fRowCount++;
          return new TOracleRow(fResult, fFieldInfo);
       } else

commit 2c53969caca352c6e3ade6d34f2a3be1ebe3ae6d

Fix bug introduced in r35211 (and re-mentioned by cov 10050)
git-svn-id: http://root.cern.ch/svn/root/trunk@35983 27541ba8-7e3a-0410-8455-c3a389f83636

sql/oracle/src/TOracleStatement.cxx
@@ -700,7 +700,7 @@ Bool_t TOracleStatement::NextResultRow()
         fBuffer[n].strbuf = 0;
         fBuffer[n].strbufsize = -1;
       }
-      if (!fResult->next() != oracle::occi::ResultSet::END_OF_FETCH) {
+      if (fResult->next() == oracle::occi::ResultSet::END_OF_FETCH) {
          fWorkingMode = 0;
          CloseBuffer();
          return kFALSE;