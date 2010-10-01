9558cbc25f0ec9bff77192e83527f6156d1c7309
Olivier Couet <Olivier.Couet@cern.ch>
2010-10-01 13:14:33 +0000

    - strncat ->strcat
    git-svn-id: http://root.cern.ch/svn/root/trunk@36009 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/graf/src/TGaxis.cxx
+++ graf2d/graf/src/TGaxis.cxx
@@ -1491,13 +1491,13 @@ L110:
 
                   if (label[first] == '.') { //check if '.' is preceeded by a digit
                      strncpy(chtemp, "0",256);
-                     strncat(chtemp, &label[first], 256);
+                     strcat(chtemp, &label[first]);
                      strncpy(label, chtemp,256);
                      first = 1; last = strlen(label);
                   }
                   if (label[first] == '-' && label[first+1] == '.') {
                      strncpy(chtemp, "-0",256);
-                     strncat(chtemp, &label[first+1], 256);
+                     strcat(chtemp, &label[first+1]);
                      strncpy(label, chtemp, 256);
                      first = 1; last = strlen(label);
                   }
