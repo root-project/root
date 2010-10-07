1310f4a8adb40c66d74d600f7a7c3119929b2be5
Philippe Canal <pcanal@fnal.gov>
2010-10-07 19:10:02 +0000

    Work around linking issues on MacOS
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2170 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/newstl/execPospelovPrintFile.C
+++ root/io/newstl/execPospelovPrintFile.C
@@ -2,9 +2,12 @@
    gROOT->ProcessLine(".L PospelovPrintFile.C");
 //   TFile *_file0 = TFile::Open("pospelov.2010.mc10_7TeV.pool.root");
 //   _file0->MakeProject("pospelov","*","RECREATE++");
-   gSystem->Load("pospelov/pospelov.so");
-   gInterpreter->GenerateDictionary("vector<CaloLocalHadCoeff::LocalHadArea>","pospelov/CaloLocalHadCoeff.h");
-   gInterpreter->GenerateDictionary("vector<CaloLocalHadCoeff::LocalHadDimension>","pospelov/CaloLocalHadCoeff.h");
+   gSystem->Load("pospelov/pospelov");
+   // Work around a linking problem on MacOS.
+   gSystem->cd("pospelov");
+   gInterpreter->GenerateDictionary("vector<CaloLocalHadCoeff::LocalHadArea>","CaloLocalHadCoeff.h");
+   gInterpreter->GenerateDictionary("vector<CaloLocalHadCoeff::LocalHadDimension>","CaloLocalHadCoeff.h");
+   gSystem->cd("..");
    PospelovPrintFile();
    PospelovPrintFile();
 }
