b6dd488f354d72654a20e58f8c074e78e9b790e9
Olivier Couet <Olivier.Couet@cern.ch>
2010-10-21 16:15:46 +0000

    - From Axel: fix a problem with boolean
    git-svn-id: http://root.cern.ch/svn/root/trunk@36399 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/asimage/src/libAfterImage/libjpeg/jconfig.h
+++ graf2d/asimage/src/libAfterImage/libjpeg/jconfig.h
@@ -3,8 +3,8 @@
 /* see jconfig.txt for explanations */
 
 #define HAVE_PROTOTYPES 1
-/* #define HAVE_UNSIGNED_CHAR 1 */
-/* #define HAVE_UNSIGNED_SHORT 1 */
+#define HAVE_UNSIGNED_CHAR 1
+#define HAVE_UNSIGNED_SHORT 1
 /* #undef void */
 /* #undef const */
 /* #undef CHAR_IS_UNSIGNED */
--- graf2d/asimage/src/libAfterImage/libjpeg/jmorecfg.h
+++ graf2d/asimage/src/libAfterImage/libjpeg/jmorecfg.h
@@ -236,7 +236,7 @@ typedef unsigned int JDIMENSION;
  */
 
 #ifndef HAVE_BOOLEAN
-typedef int boolean;
+typedef unsigned char boolean;
 #endif
 #ifndef FALSE			/* in case these macros already exist */
 #define FALSE	0		/* values of boolean */
