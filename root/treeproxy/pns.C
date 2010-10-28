fb58c99b490c9fdf5190860aaf698f808853a873
Philippe Canal <pcanal@fnal.gov>
2010-10-28 20:52:59 +0000

    Test of MakeProxy with Stl containers of object (need revision 36449)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2180 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/treeproxy/pns.C
@@ -0,0 +1,7 @@
+double pns() 
+{
+   printf("trajPos.fP.fX[1]=%3.2f\n",trajPos.fP.fX[1]); 
+   printf("trajPos.fP[1].fX=%3.2f\n",trajPos.fP[1].fX); 
+   printf("trajPos[1].fP.fX=%3.2f\n",trajPos[1].fP.fX); 
+   return 1; 
+}
