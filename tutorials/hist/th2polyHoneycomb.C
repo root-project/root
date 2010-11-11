c791313d46296644f604d90f776f5c330337f108
Olivier Couet <Olivier.Couet@cern.ch>
2010-11-11 16:20:03 +0000

    - New example of th2poly
    git-svn-id: http://root.cern.ch/svn/root/trunk@36601 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tutorials/hist/th2polyHoneycomb.C
@@ -0,0 +1,17 @@
+//This tutorial illustrates how to create an histogram with hexagonal
+//bins (TH2Poly), fill it and draw it using GL. 
+//
+//Author: Olivier Couet
+
+void th2polyHoneycomb(){
+   TH2Poly *hc = new TH2Poly();
+   hc->Honeycomb(0,0,.1,25,25);
+   gStyle->SetPalette(1);
+
+   TRandom ran;
+   for (int i = 0; i<30000; i++) {
+      hc->Fill(ran.Gaus(2.,1), ran.Gaus(2.,1));
+   }
+
+   hc->Draw("glhp");
+}
