6ccf5eb4d2128eef158f4733d71c4ad789b21373
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-11-19 16:57:10 +0000

    fix erroneous checkin.
    git-svn-id: http://root.cern.ch/svn/root/trunk@36793 27541ba8-7e3a-0410-8455-c3a389f83636

--- tree/tree/inc/LinkDef.h
+++ tree/tree/inc/LinkDef.h
@@ -63,7 +63,6 @@
 #pragma link C++ class TCollectionMethodBrowsable+;
 #pragma link C++ class TSelectorScalar+;
 #pragma link C++ class TQueryResult+;
-#pragma link C++ class TProcessor+;
 #pragma link C++ class TBranchSTL+;
 #pragma link C++ class TIndArray+;
 
