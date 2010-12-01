0362cbfc799593599ac313257eaa3fbe99e8de4a
Eckhard von Toerne <evt@physik.uni-bonn.de>
2010-12-01 10:11:05 +0000

    fix for coverity defect 23294
    git-svn-id: http://root.cern.ch/svn/root/trunk@37140 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/src/DecisionTree.cxx
+++ tmva/src/DecisionTree.cxx
@@ -928,6 +928,8 @@ Double_t TMVA::DecisionTree::TrainNodeFast( const vector<TMVA::Event*> & eventSa
       
       
       fisherCoeff = this->GetFisherCoefficients(eventSample, nFisherVars, mapVarInFisher);
+      delete [] useVarInFisher;
+      delete [] mapVarInFisher;
    }
 
 
