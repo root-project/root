9d8d589b2e918c95964f53be501f4ef362a06f27
Philippe Canal <pcanal@fnal.gov>
2010-12-01 22:39:05 +0000

    From Gerri: Allow to pass the location of the H1 file via the script argument in addition of the H1 environment variable
    git-svn-id: http://root.cern.ch/svn/root/trunk@37163 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/tree/h1chain.C
+++ tutorials/tree/h1chain.C
@@ -1,9 +1,14 @@
+//Creates a TChain to be used by the h1analysis.C class
+//the symbol H1 must point to a directory where the H1 data sets
+//have been installed.
+
+TChain chain("h42");
+
+void h1chain(const char *h1dir = 0)
 {
-   //creates a TChain to be used by the h1analysis.C class
-   //the symbol H1 must point to a directory where the H1 data sets
-   //have been installed
-   
-   TChain chain("h42");
+   if (h1dir) {
+      gSystem->Setenv("H1",h1dir);
+   }
    chain.SetCacheSize(20*1024*1024);
    chain.Add("$H1/dstarmb.root");
    chain.Add("$H1/dstarp1a.root");
