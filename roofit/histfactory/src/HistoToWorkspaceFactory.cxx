5c23115fe39183d1948a177977657fa65102dbf9
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2010-12-06 15:22:35 +0000

    fix a compilation warning
    git-svn-id: http://root.cern.ch/svn/root/trunk@37337 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/histfactory/src/HistoToWorkspaceFactory.cxx
+++ roofit/histfactory/src/HistoToWorkspaceFactory.cxx
@@ -257,7 +257,7 @@ namespace HistFactory{
         // remove "doRatio" and name can be changed when ws gets imported to the combined model.
         std::stringstream range;
         range<<"["<<itr->val<<","<<itr->low<<","<<itr->high<<"]";
-        RooRealVar* var = 0;
+        //RooRealVar* var = 0;
 
         string varname;
         if(!prodNames.empty()) prodNames+=",";
@@ -267,6 +267,7 @@ namespace HistFactory{
         else {
           varname=itr->name;
         }
+        //var = (RooRealVar*) // not used  
         proto->factory((varname+range.str()).c_str());
         prodNames+=varname;
       }
