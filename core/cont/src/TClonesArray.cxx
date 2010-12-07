c09e0e16c86ced460700835671e2bec071072a5a
Fons Rademakers <Fons.Rademakers@cern.ch>
2010-12-07 08:09:27 +0000

    Avoid potential segv in MulitSort. Fixes issue 76110.
    git-svn-id: http://root.cern.ch/svn/root/trunk@37360 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/cont/src/TClonesArray.cxx
+++ core/cont/src/TClonesArray.cxx
@@ -53,27 +53,27 @@
 //                                                                      //
 //  NOTE 1
 //  ======
-// C/C++ offer the possibility to allocate and delete memory. 
-// Forgetting to delete allocated memory is a programming error that originate "leaks", 
-// i.e. the memory of your process grows and eventually your program crashes. 
+// C/C++ offer the possibility to allocate and delete memory.
+// Forgetting to delete allocated memory is a programming error that originate "leaks",
+// i.e. the memory of your process grows and eventually your program crashes.
 // Even if you *always* delete the allocated memory, the recovered space may not be
-// efficiently reused. The process knows that there are portions of free memory, 
-// but when you allocate it again, a fresh piece of memory is grabbed. 
-// Your program is free from semantic errors, but the total memory of your process 
-// still grows. Moreover your program's memory is full of "holes" which reduce the 
-// efficiency of memory access, this is called "memory fragmentation". 
+// efficiently reused. The process knows that there are portions of free memory,
+// but when you allocate it again, a fresh piece of memory is grabbed.
+// Your program is free from semantic errors, but the total memory of your process
+// still grows. Moreover your program's memory is full of "holes" which reduce the
+// efficiency of memory access, this is called "memory fragmentation".
 // Moreover new / delete are expensive operations in terms of CPU time.
 //
-// Without entering into technical details, the TClonesArray allow to "reuse" the 
-// same portion of memory for new/delete avoiding memory fragmentation and memory 
-// growth and improving by orders of magnitude the performance. Every time the 
-// memory of the TClonesArray has to be reused, the Clear() method is employed. 
-// To provide its benefits, each TClonesArray must be allocated *once* per process 
+// Without entering into technical details, the TClonesArray allow to "reuse" the
+// same portion of memory for new/delete avoiding memory fragmentation and memory
+// growth and improving by orders of magnitude the performance. Every time the
+// memory of the TClonesArray has to be reused, the Clear() method is employed.
+// To provide its benefits, each TClonesArray must be allocated *once* per process
 // and disposed of (deleted) *only when not needed any more*.
-// So a job should see *only one* deletion for each TClonesArray, which should be 
-// "Cleared()" in between several times. Keep deleting a TClonesArray is a double 
+// So a job should see *only one* deletion for each TClonesArray, which should be
+// "Cleared()" in between several times. Keep deleting a TClonesArray is a double
 // waste. Not only you do not avoid memory fragmentation, but you worsen it because
-// the TClonesArray itself is a rather heavy structure, and there is quite some 
+// the TClonesArray itself is a rather heavy structure, and there is quite some
 // code in the destructor, so you have more memory fragmentation and slower code.
 //
 //  NOTE 2
@@ -81,17 +81,17 @@
 //
 // When investigating misuse of TClonesArray, please make sure of the following:
 //
-//    * Use Clear() or Clear("C") instead of Delete(). This will improve program 
+//    * Use Clear() or Clear("C") instead of Delete(). This will improve program
 //      execution time.
-//    * TClonesArray object classes containing pointers allocate memory. 
-//      To avoid causing memory leaks, special Clear("C") must be used for 
-//      clearing TClonesArray. When option "C" is specified, ROOT automatically 
+//    * TClonesArray object classes containing pointers allocate memory.
+//      To avoid causing memory leaks, special Clear("C") must be used for
+//      clearing TClonesArray. When option "C" is specified, ROOT automatically
 //      executes the Clear() method (by default it is empty contained in TObject).
-//      This method must be overridden in the relevant TClonesArray object class, 
+//      This method must be overridden in the relevant TClonesArray object class,
 //      implementing the reset procedure for pointer objects.
-//    * To reduce memory fragmentation, please make sure that the TClonesArrays 
-//      are not destroyed and created on every event. 
-//      They must only be constructed/destructed at the beginning/end of the run. 
+//    * To reduce memory fragmentation, please make sure that the TClonesArrays
+//      are not destroyed and created on every event.
+//      They must only be constructed/destructed at the beginning/end of the run.
 //////////////////////////////////////////////////////////////////////////
 
 #include <stdlib.h>
@@ -872,11 +872,11 @@ TObject *TClonesArray::New(Int_t idx)
 // (jadetwiler@lbl.gov)
 //
 //______________________________________________________________________________
-void TClonesArray::AbsorbObjects(TClonesArray* tc) 
+void TClonesArray::AbsorbObjects(TClonesArray* tc)
 {
    // Directly move the object pointers from tc to this without cloning
    // (copying).
-   // "this" takes over ownership of all of tc's object pointers. tc is left 
+   // "this" takes over ownership of all of tc's object pointers. tc is left
    // empty upon return
 
    // tests
@@ -887,7 +887,7 @@ void TClonesArray::AbsorbObjects(TClonesArray* tc)
    }
 
    // cache the sorted status
-   bool wasSorted = IsSorted() && tc->IsSorted() && 
+   bool wasSorted = IsSorted() && tc->IsSorted() &&
                     (Last() == NULL || Last()->Compare(tc->First()) == -1);
 
    // expand this
@@ -927,45 +927,37 @@ void TClonesArray::MultiSort(Int_t nTCs, TClonesArray** tcs, Int_t upto)
             return;
          }
       }
-      if(sortedCheck && i > 1) {
-         if(fCont[i]->Compare(fCont[i-1]) < 0) sortedCheck = false;
+      if (sortedCheck && i > 1) {
+         if(ObjCompare(fCont[i], fCont[i-1]) < 0) sortedCheck = false;
       }
    }
-   if(sortedCheck) {
+   if (sortedCheck) {
       fSorted = true;
       return;
    }
- 
-   for(int i=0; i<nTCs; i++) {
-      if(tcs[i] == this) {
+
+   for (int i=0; i<nTCs; i++) {
+      if (tcs[i] == this) {
          Error("MultiSort", "tcs[%d] = \"this\"", i);
          return;
       }
-      if(tcs[i]->GetEntriesFast() != GetEntriesFast()) {
+      if (tcs[i]->GetEntriesFast() != GetEntriesFast()) {
          Error("MultiSort", "tcs[%d] has length %d != length of this (%d)",
-         i, tcs[i]->GetEntriesFast(), this->GetEntriesFast());
+               i, tcs[i]->GetEntriesFast(), this->GetEntriesFast());
          return;
       }
    }
- 
+
    int nBs = nTCs*2+1;
    TObject*** b = new TObject**[nBs];
-   for(int i=0; i<nTCs; i++) {
-      b[2*i] = tcs[i]->fCont;
+   for (int i = 0; i < nTCs; i++) {
+      b[2*i]   = tcs[i]->fCont;
       b[2*i+1] = tcs[i]->fKeep->fCont;
    }
    b[nBs-1] = fKeep->fCont;
    QSort(fCont, nBs, b, 0, TMath::Min(nentries, upto-fLowerBound));
    delete [] b;
- 
+
    fLast = -2;
    fSorted = kTRUE;
-} 
-
-
-
-
-
-
-
-
+}
