a1a1a4358eeea7dccaf6fe9175c36632f192bb4e
Philippe Canal <pcanal@fnal.gov>
2011-01-31 22:14:09 +0000

    Test revison 37920
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2237 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/tree/fastcloning/execmisstop.C
@@ -0,0 +1,8 @@
+{
+   int i;
+   TTree *in = new TTree("in",""); in->Branch("i",&i);
+   TTree *out = in->CloneTree(-1); out->Branch("j",&i);
+   TTreeCloner cloner_warn(in,out,"");
+   TTreeCloner cloner_quiet(in,out,"",TTreeCloner::kIgnoreMissingTopLevel);
+   return cloner_quiet.IsValid() ? 0 : 1;
+}
