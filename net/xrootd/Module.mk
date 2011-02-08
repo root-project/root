5c17e5fec42594bc5030633e5c4b7fd6a25bc4f2
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2011-02-08 09:30:15 +0000

    Fix the STLport Solaris issue in the right place
    git-svn-id: http://root.cern.ch/svn/root/trunk@38001 27541ba8-7e3a-0410-8455-c3a389f83636

--- net/xrootd/Module.mk
+++ net/xrootd/Module.mk
@@ -127,13 +127,13 @@ endif
 		macosx64:*)      xopt="--ccflavour=macos64";; \
 		macosxicc:*)     xopt="--ccflavour=icc";; \
 		macosx*:*)       xopt="--ccflavour=macos";; \
-		solaris64*:*:i86pc:*) xopt="--ccflavour=sunCCamd64 --use-xrd-strlcpy";; \
-                solaris*:5.11:i86pc:*) xopt="--ccflavour=sunCCi86pc --use-xrd-strlcpy";; \
-                solaris*:5.1*:i86pc:*) xopt="--use-xrd-strlcpy";; \
-                solaris*:*:i86pc:*) xopt="--ccflavour=sunCCi86pc --use-xrd-strlcpy";; \
-		solaris*:5.8)    xopt="--ccflavour=sunCC";; \
-		solaris*:5.9)    xopt="--ccflavour=sunCC";; \
-		solaris*:*)      xopt="--ccflavour=sunCC --use-xrd-strlcpy";; \
+		solaris64*:*:i86pc:*) xopt="--ccflavour=sunCCamd64 --use-xrd-strlcpy --use-sun-stl4port";; \
+                solaris*:5.11:i86pc:*) xopt="--ccflavour=sunCCi86pc --use-xrd-strlcpy --use-sun-stl4port";; \
+                solaris*:5.1*:i86pc:*) xopt="--use-xrd-strlcpy --use-sun-stl4port";; \
+                solaris*:*:i86pc:*) xopt="--ccflavour=sunCCi86pc --use-xrd-strlcpy --use-sun-stl4port";; \
+		solaris*:5.8)    xopt="--ccflavour=sunCC --use-sun-stl4port";; \
+		solaris*:5.9)    xopt="--ccflavour=sunCC --use-sun-stl4port";; \
+		solaris*:*)      xopt="--ccflavour=sunCC --use-xrd-strlcpy --use-sun-stl4port";; \
 		win32gcc:*)      xopt="win32gcc";; \
 		*)               xopt="";; \
 		esac; \
