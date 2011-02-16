689d457f6362e8134ac48f3e9b8981e003ca3f8b
Eckhard von Toerne <evt@physik.uni-bonn.de>
2011-02-16 10:10:10 +0000

    fix coverity issue
    git-svn-id: http://root.cern.ch/svn/root/trunk@38083 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/src/DecisionTree.cxx
+++ tmva/src/DecisionTree.cxx
@@ -96,6 +96,7 @@ TMVA::DecisionTree::DecisionTree():
    fMinLinCorrForFisher (1),
    fUseExclusiveVars (kTRUE),
    fSepType        (NULL),
+   fRegType        (NULL),
    fMinSize        (0),
    fMinSepGain (0),
    fUseSearchTree(kFALSE),
@@ -128,6 +129,7 @@ TMVA::DecisionTree::DecisionTree( TMVA::SeparationBase *sepType, Int_t minSize, 
    fMinLinCorrForFisher (1),
    fUseExclusiveVars (kTRUE),
    fSepType        (sepType),
+   fRegType        (NULL),
    fMinSize        (minSize),
    fMinSepGain     (0),
    fUseSearchTree  (kFALSE),
@@ -174,6 +176,7 @@ TMVA::DecisionTree::DecisionTree( const DecisionTree &d ):
    fMinLinCorrForFisher (d.fMinLinCorrForFisher),
    fUseExclusiveVars (d.fUseExclusiveVars),
    fSepType    (d.fSepType),
+   fRegType    (d.fRegType),
    fMinSize    (d.fMinSize),
    fMinSepGain (d.fMinSepGain),
    fUseSearchTree  (d.fUseSearchTree),
@@ -203,7 +206,7 @@ TMVA::DecisionTree::~DecisionTree()
 {
    // destructor
 
-   // desctruction of the tree nodes done in the "base class" BinaryTree
+   // destruction of the tree nodes done in the "base class" BinaryTree
 
    if (fMyTrandom) delete fMyTrandom;
 }
