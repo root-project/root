2e725de9d675c459ff50b8573fecfbf4c9ee45b6
Olivier Couet <Olivier.Couet@cern.ch>
2011-02-23 13:16:09 +0000

    - Protection added against a division by 0   in TText::ExecuteEvent (From: hans.dalsgaard@cern.ch)
    git-svn-id: http://root.cern.ch/svn/root/trunk@38193 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/graf/src/TText.cxx
+++ graf2d/graf/src/TText.cxx
@@ -170,7 +170,7 @@ void TText::ExecuteEvent(Int_t event, Int_t px, Int_t py)
    Double_t lambda, x2,y2;
    Double_t dpx,dpy,xp1,yp1;
    Int_t cBoxX[4], cBoxY[4], part;
-
+   Double_t div = 0;
    if (!gPad->IsEditable()) return;
    switch (event) {
 
@@ -194,8 +194,9 @@ void TText::ExecuteEvent(Int_t event, Int_t px, Int_t py)
       resize = kFALSE;
       turn   = kFALSE;
       GetControlBox(px1, py1, -theta, cBoxX, cBoxY);
-      part   = (Int_t)(3*((px-cBoxX[0])*co-(py-cBoxY[0])*si)/
-                      ((cBoxX[3]-cBoxX[0])*co-(cBoxY[3]-cBoxY[0])*si));
+      div    = ((cBoxX[3]-cBoxX[0])*co-(cBoxY[3]-cBoxY[0])*si);
+      if (TMath::Abs(div) > 1e-8) part = (Int_t)(3*((px-cBoxX[0])*co-(py-cBoxY[0])*si)/ div);
+      else part = 0;
       switch (part) {
       case 0:
          if (halign == 3) {
