9a5bad1d28b71b317265e58e683d7faa141a26c5
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-03-21 14:31:49 +0000

    From Antonio: add some more modules to be excluded from a static build.
    git-svn-id: http://root.cern.ch/svn/root/trunk@38539 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/staticobjectlist.sh
+++ build/unix/staticobjectlist.sh
@@ -12,18 +12,18 @@ if [ "$1" = "-d" ]; then
    dictonly="yes"
 fi
 
-excl="main proof/proofd net/rootd net/xrootd rootx montecarlo/pythia6 \
-      montecarlo/pythia8 sql/mysql sql/pgsql io/rfio sql/sapdb \
-      hist/hbook core/newdelete misc/table core/utils net/srputils \
+excl="main proof/proofd proof/clarens proof/peac net/rootd net/xrootd rootx \
+      montecarlo/pythia6 montecarlo/pythia8 sql/mysql sql/pgsql io/rfio \
+      sql/sapdb hist/hbook core/newdelete misc/table core/utils net/srputils \
       net/krb5auth net/globusauth io/chirp io/dcache net/alien \
       graf2d/asimage net/ldap graf2d/qt gui/qtroot math/quadp \
       bindings/pyroot bindings/ruby tmva math/genetic \
       io/xmlparser graf3d/gl graf3d/ftgl roofit/roofit roofit/roofitcore \
-      roofit/roostats sql/oracle net/netx net/auth net/rpdutils math/mathmore \
-      math/minuit2 io/gfal net/monalisa proof/proofx math/fftw gui/qtgsi \
-      sql/odbc io/castor math/unuran geom/gdml montecarlo/g4root \
-      graf2d/gviz graf3d/gviz3d graf3d/eve net/glite misc/minicern \
-      misc/memstat net/bonjour graf2d/fitsio"
+      roofit/roostats roofit/histfactory sql/oracle net/netx net/auth \
+      net/rpdutils math/mathmore math/minuit2 io/gfal net/monalisa \
+      proof/proofx math/fftw gui/qtgsi sql/odbc io/castor math/unuran \
+      geom/gdml montecarlo/g4root graf2d/gviz graf3d/gviz3d graf3d/eve \
+      net/glite misc/minicern misc/memstat net/bonjour graf2d/fitsio"
 
 objs=""
 gobjs=""
