bad21468cdeaaaaf5132eabe2d64c36a3acc10e6
Philippe Canal <pcanal@fnal.gov>
2011-03-24 00:41:08 +0000

    Update following r38595
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2258 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/chain/runsubdir.C
+++ root/tree/chain/runsubdir.C
@@ -4,22 +4,22 @@ void add() {
    cout << "Trying subdir.root/data*.root\n";
    TChain c("tree");
    c.Add("subdir.root/data*.root");
-   c.ls();
+   c.ls("noaddr");
    c.Print();
    cout << "Trying subdir.root/data*.root/tree\n";
    TChain c2("tree");
    c2.Add("subdir.root/data*.root/tree");
-   c2.ls();
+   c2.ls("noaddr");
    c2.Print();
    cout << "Trying subdir.root/data1.root/tree\n";
    TChain c3("tree");
    c3.Add("subdir.root/data1.root/tree");
-   c3.ls();
+   c3.ls("noaddr");
    c3.Print();
    cout << "Trying subdir.root/data2.root\n";
    TChain c4("tree");
    c4.Add("subdir.root/data2.root");
-   c4.ls();
+   c4.ls("noaddr");
    c4.Print();
 }
 
@@ -29,12 +29,12 @@ void addfile() {
    cout << "Trying subdir.root/data1.root with a tree name\n";
    TChain c3("tree");
    c3.AddFile("subdir.root/data1.root",TChain::kBigNumber,"tree");
-   c3.ls();
+   c3.ls("noaddr");
    c3.Print();
    cout << "Trying subdir.root/data2.root\n";
    TChain c4("tree");
    c4.AddFile("subdir.root/data2.root");
-   c4.ls();
+   c4.ls("noaddr");
    c4.Print();
 }
 
