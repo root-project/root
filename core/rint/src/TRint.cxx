1cf63b0ebc09649c0aad601022cd6510b0a03763
Philippe Canal <pcanal@fnal.gov>
2011-03-24 17:29:21 +0000

    Move the call to CloseFiles and ResetGlobal after the execution of logoff.C (as intended by revision 38548)
    git-svn-id: http://root.cern.ch/svn/root/trunk@38613 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/rint/src/TRint.cxx
+++ core/rint/src/TRint.cxx
@@ -619,9 +619,6 @@ void TRint::Terminate(Int_t status)
       delete gTabCom;
       gTabCom = 0;
 
-      gROOT->CloseFiles(); // Close any files or sockets before emptying CINT.
-      gInterpreter->ResetGlobals();
-
       //Execute logoff macro
       const char *logoff;
       logoff = gEnv->GetValue("Rint.Logoff", (char*)0);
@@ -631,6 +628,10 @@ void TRint::Terminate(Int_t status)
             ProcessFile(logoff);
          delete [] mac;
       }
+
+      gROOT->CloseFiles(); // Close any files or sockets before emptying CINT.
+      gInterpreter->ResetGlobals();
+
       TApplication::Terminate(status);
    }
 }
