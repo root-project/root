e2366d06ccc718f560f9e1ba48101dd002c61d97
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2011-03-25 11:37:29 +0000

    Forgot a crucial file ...
    git-svn-id: http://root.cern.ch/svn/root/trunk@38624 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ net/xrootd/src/xrootd/src/XrdVersion.hh
@@ -0,0 +1,17 @@
+// this file is automatically updated by the genversion.sh script
+// if you touch anything make sure that it still works
+
+#ifndef __XRD_VERSION_H__
+#define __XRD_VERSION_H__
+
+#define XrdVERSION  "v3.0.3"
+
+#if XrdDEBUG
+#define XrdVSTRING XrdVERSION "_dbg"
+#else
+#define XrdVSTRING XrdVERSION
+#endif
+
+#define XrdDEFAULTPORT 1094;
+
+#endif
