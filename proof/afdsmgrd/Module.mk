b5c23d69f764de2560d0f89b93e01f2949548156
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2011-04-11 09:56:38 +0000

    From D. Berzano: import new version 0.9.0 of afdsmgrd
    git-svn-id: http://root.cern.ch/svn/root/trunk@38792 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/afdsmgrd/Module.mk
+++ proof/afdsmgrd/Module.mk
@@ -1,34 +1,26 @@
 # Module.mk for afdsmgrd module
 # Copyright (c) 2004 Rene Brun and Fons Rademakers
 #
-# Author: Dario Berzano, 12/01/2011
+# Author: Dario Berzano <dario.berzano@cern.ch>, 01/04/2011
 
 MODNAME      := afdsmgrd
 MODDIR       := $(ROOT_SRCDIR)/proof/$(MODNAME)
 
 AFDSMGRDDIR  := $(MODDIR)
 AFDSMGRDBIN  := bin/afdsmgrd
-AFDSMGRDTAR  := $(shell cd $(AFDSMGRDDIR) && echo afdsmgrd-v*.tar.gz)
-
-# List of pairs source file - destination file. Source files are relative to
-# either build/src or src dir (automatically choosen), destination files are
-# relative to $(ROOT_SRCDIR)
-AFDSMGRDCP := src/afdsmgrd                              $(AFDSMGRDBIN) \
-              etc/init.d/afdsmgrd                       etc/proof/afdsmgrdctl \
-              etc/sysconfig/afdsmgrd                    etc/proof/afdsmgrd_sysconfig.conf \
-              etc/xrootd/afdsmgrd.cf.example            etc/proof/afdsmgrd.cf.example \
-              etc/xrootd/single_dataset_src.cf.example  etc/proof/afdsmgrd_single_dataset_src.cf.example
+AFDSMGRDTAR  := $(shell cd $(AFDSMGRDDIR) && ls -1 afdsmgrd-v*.tar.gz | tail -n1)
 
 # This one triggers the build from ROOT
 ALLEXECS     += $(AFDSMGRDBIN)
 
+# Phony targets
 .PHONY: all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME)
 
 # The tar file is unpacked and the project is configured and built through
 # afdsmgrd's custom built system. Proper variables are passed to ./configure,
-# including the option to enable/disable ApMon, and files are installed into
-# proper ROOT places
-$(AFDSMGRDBIN):
+# including the option to enable/disable ApMon, and files are installed under
+# $ROOTSYS properly directly by invoking "make install"
+$(AFDSMGRDBIN): $(ALLLIBS)
 	@( cd $(AFDSMGRDDIR) && \
 	   if [ ! -d afdsmgrd ]; then \
 	     echo "*** Unpacking afdsmgrd tarball ***" ; \
@@ -37,24 +29,12 @@ $(AFDSMGRDBIN):
 	   cd afdsmgrd && \
 	   if [ ! -d build ]; then \
 	     ./configure --with-rootsys="$(ROOT_SRCDIR)" "$(AFDSMGRDAPMON)" \
-	       --prefix="$(ROOT_SRCDIR)" ; \
+	       --prefix="$(ROOT_SRCDIR)" --root-mode ; \
 	   fi && \
-	   make ; \
-	 )
-	@( P=0 ; \
-	   for F in $(AFDSMGRDCP) ; do \
-	     if test x$$P = x0 ; then \
-	       P=1 ; S="$(AFDSMGRDDIR)/afdsmgrd/build/$$F" ; \
-	       if [ ! -e "$$S" ]; then \
-	         S="$(AFDSMGRDDIR)/afdsmgrd/$$F" ; \
-	       fi ; \
-	     else \
-	       P=0 ; D="$(ROOT_SRCDIR)/$$F" ; \
-	       rsync -a "$$S" "$$D" ; \
-	     fi ; \
-	   done ; \
+	   make install ; \
 	 )
 
+# Default target invoked when building this module
 all-$(MODNAME): $(AFDSMGRDBIN)
 
 # Simple cleanup of afdsmgrd. The "clean" target of afdsmgrd is called without
@@ -65,16 +45,20 @@ clean-$(MODNAME):
 	   cd "$(AFDSMGRDDIR)" ; \
 	   if [ -d afdsmgrd ]; then \
 	     echo "*** Cleaning up afdsmgrd ***" ; \
-	     cd afdsmgrd ; \
+	     cd afdsmgrd && \
 	     make clean ; \
 	   fi ; \
 	)
 
+# Append this clean-afdsmgrd action to the global "clean" target in main
+# Makefile
 clean:: clean-$(MODNAME)
 
 # Deep cleanup of afdsmgrd. If the unpack directory exists, it is removed and a
 # message is printed out. If the directory does not exist (afdsmgrd was never
 # built before) it does nothing without printing a single message
+#
+# TODO: add make uninstall to CMake
 distclean-$(MODNAME):
 	@( cd "$(AFDSMGRDDIR)" ; \
 	   if [ -d afdsmgrd ]; then \
@@ -82,16 +66,7 @@ distclean-$(MODNAME):
 	     rm -r afdsmgrd ; \
 	   fi ; \
 	)
-	@( P=0 ; \
-	   for F in $(AFDSMGRDCP) ; do \
-	     if test ! x$$P = x0 ; then \
-	       P=1 ; \
-	     else \
-	       P=0 ; \
-	       rm -f "$(ROOT_SRCDIR)/$$F" ; \
-	     fi ; \
-	   done ; \
-	 )
 
-
+# Append this distclean-afdsmgrd action to the global "distclean" target in
+# main Makefile
 distclean:: distclean-$(MODNAME)
