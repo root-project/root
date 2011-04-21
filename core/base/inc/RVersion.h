4537fcbc32855f63580b98fc7c22ca7407ab5763
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-04-21 21:30:40 +0000

    Start development cycle 5.29/03.
    git-svn-id: http://root.cern.ch/svn/root/trunk@39013 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,14 +14,14 @@
  *
 */
 
-#define ROOT_RELEASE "5.29/02"
+#define ROOT_RELEASE "5.29/03"
 #define ROOT_RELEASE_DATE "Apr 21 2011"
-#define ROOT_RELEASE_TIME "14:56:23"
-#define ROOT_SVN_REVISION 39003
+#define ROOT_RELEASE_TIME "23:27:59"
+#define ROOT_SVN_REVISION 39006
 #define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 335106
+#define ROOT_VERSION_CODE 335107
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_FULL_VERSION_CODE 85787136
+#define ROOT_FULL_VERSION_CODE 85787392
 #define ROOT_FULL_VERSION(a,b,c,p) (((a) << 24) + ((b) << 16) + ((c) << 8) + (p))
 
 #endif
