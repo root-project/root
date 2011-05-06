bf3d10a6efa557f225c3284de86d3df3bcc9a3e5
Philippe Canal <pcanal@fnal.gov>
2011-05-06 20:21:50 +0000

    Import revision 39134 and 39136 from the trunk: Avoid the error messages similar to:   Error in <TBufferFile::ReadVersion>: Could not find the StreamerInfo with a checksum of -849380031 for the class "NeverWritten" in memberwise.root. which is spurrious when it is issued when unstreaming a memberwised streamed collection which is always empty (Introduce TBuffer::ReadVersionForMemberWise for this purpose).
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-28-00-patches@39140 27541ba8-7e3a-0410-8455-c3a389f83636

--- io/io/src/TBufferFile.cxx
+++ io/io/src/TBufferFile.cxx
@@ -2636,7 +2636,7 @@ void TBufferFile::SkipVersion(const TClass *cl)
                   Error("ReadVersion", "Could not find the StreamerInfo with a checksum of %d for the class \"%s\" in %s.",
                         checksum, cl->GetName(), ((TFile*)fParent)->GetName());
                } else {
-                  Error("ReadVersion", "Could not find the StreamerInfo with a checksum of %d for the class \"%s\"( buffer with no parent)",
+                  Error("ReadVersion", "Could not find the StreamerInfo with a checksum of %d for the class \"%s\" (buffer with no parent)",
                         checksum, cl->GetName());
                }
                return;
@@ -2736,10 +2736,10 @@ Version_t TBufferFile::ReadVersion(UInt_t *startpos, UInt_t *bcnt, const TClass 
                      version = cl->GetClassVersion();
                   } else {
                      if (fParent) {
-                        Error("ReadVersion", "Could not find the StreamerInfo with a checksum of %d for the class \"%s\" in %s.",
+                        Error("ReadVersion", "Could not find the StreamerInfo with a checksum of 0x%x for the class \"%s\" in %s.",
                               checksum, cl->GetName(), ((TFile*)fParent)->GetName());
                      } else {
-                        Error("ReadVersion", "Could not find the StreamerInfo with a checksum of %d for the class \"%s\"( buffer with no parent)",
+                        Error("ReadVersion", "Could not find the StreamerInfo with a checksum of 0x%x for the class \"%s\" (buffer with no parent)",
                               checksum, cl->GetName());
                      }
                      return 0;
@@ -2765,8 +2765,80 @@ Version_t TBufferFile::ReadVersion(UInt_t *startpos, UInt_t *bcnt, const TClass 
                if (vinfo) {
                   version = vinfo->GetClassVersion();
                } else {
-                  Error("ReadVersion", "Could not find the StreamerInfo with a checksum of %d for the class \"%s\" in %s.",
+                  Error("ReadVersion", "Could not find the StreamerInfo with a checksum of 0x%x for the class \"%s\" in %s.",
                         checksum, cl->GetName(), ((TFile*)fParent)->GetName());
+                  return 0;
+               }
+            }
+            else  {
+               Error("ReadVersion", "Class %s not known to file %s.",
+                 cl->GetName(), ((TFile*)fParent)->GetName());
+               version = 0;
+            }
+         }
+      }
+   }
+   return version;
+}
+
+//______________________________________________________________________________
+Version_t TBufferFile::ReadVersionForMemberWise(const TClass *cl)
+{
+   // Read class version from I/O buffer ; to be used when streaming out
+   // memberwise streamed collection where we do not care (not save) about
+   // the byte count and can safely ignore missing streamerInfo (since they
+   // usually indicate empty collections).
+
+   Version_t version;
+
+   // not interested in byte count
+   frombuf(this->fBufCur,&version);
+
+   if (version<=1) {
+      if (version <= 0)  {
+         if (cl) {
+            if (cl->GetClassVersion() != 0) {
+               UInt_t checksum = 0;
+               frombuf(this->fBufCur,&checksum);
+               TStreamerInfo *vinfo = (TStreamerInfo*)cl->FindStreamerInfo(checksum);
+               if (vinfo) {
+                  return vinfo->TStreamerInfo::GetClassVersion(); // Try to get inlining.
+               } else {
+                  // There are some cases (for example when the buffer was stored outside of
+                  // a ROOT file) where we do not have a TStreamerInfo.  If the checksum is
+                  // the one from the current class, we can still assume that we can read
+                  // the data so let use it.
+                  if (checksum==cl->GetCheckSum() || checksum==cl->GetCheckSum(1)) {
+                     version = cl->GetClassVersion();
+                  } else {
+                     // If we can not find the streamerInfo this means that 
+                     // we do not actully need it (the collection is always empty
+                     // in this file), so no need to issue a warning.
+                     return 0;
+                  }
+               }
+            }
+         } else { // of if (cl) {
+            UInt_t checksum = 0;
+            frombuf(this->fBufCur,&checksum);            
+         }
+      }  else if (version == 1 && fParent && ((TFile*)fParent)->GetVersion()<40000 && cl && cl->GetClassVersion() != 0) {
+         // We could have a file created using a Foreign class before
+         // the introduction of the CheckSum.  We need to check
+         if ((!cl->IsLoaded() || cl->IsForeign()) &&
+            cl->GetStreamerInfos()->GetLast()>1 ) {
+
+            const TList *list = ((TFile*)fParent)->GetStreamerInfoCache();
+            const TStreamerInfo *local = (TStreamerInfo*)list->FindObject(cl->GetName());
+            if ( local )  {
+               UInt_t checksum = local->GetCheckSum();
+               TStreamerInfo *vinfo = (TStreamerInfo*)cl->FindStreamerInfo(checksum);
+               if (vinfo) {
+                  version = vinfo->GetClassVersion();
+               } else {
+                  // If we can not find the streamerInfo this means that 
+                  // we do not actully need it (the collection is always empty
+                  // in this file), so no need to issue a warning.
                   return 0;
                }
             }
