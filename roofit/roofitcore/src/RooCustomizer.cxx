c98454025f765b930c367a08784b8ad4333cebb6
Wouter Verkerke <verkerke@nikhef.nl>
2011-05-25 17:04:03 +0000

    o RooCustomizer
       - Fix Windows compilation issue
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@39378 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/roofitcore/src/RooCustomizer.cxx
+++ roofit/roofitcore/src/RooCustomizer.cxx
@@ -124,6 +124,31 @@
 #include "RooConstVar.h"
 #include "RooRealConstant.h"
 
+
+#ifndef _WIN32
+#include <strings.h>
+#else
+static char *strtok_r(char *s1, const char *s2, char **lasts)
+{
+  char *ret;
+  
+  if (s1 == NULL)
+    s1 = *lasts;
+  while(*s1 && strchr(s2, *s1))
+    ++s1;
+  if(*s1 == '\0')
+    return NULL;
+  ret = s1;
+  while(*s1 && !strchr(s2, *s1))
+    ++s1;
+  if(*s1)
+    *s1++ = '\0';
+  *lasts = s1;
+  return ret;
+}
+#endif
+
+
 ClassImp(RooCustomizer) 
 ;
 
