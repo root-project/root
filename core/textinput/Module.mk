b78eefa3bddbc5dfaf522eddf8769becca807c6c
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-05-26 15:56:52 +0000

    fix for out-of-source build.
    git-svn-id: http://root.cern.ch/svn/root/trunk@39425 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/textinput/Module.mk
+++ core/textinput/Module.mk
@@ -3,35 +3,35 @@
 #
 # Author: Fons Rademakers, 29/2/2000
 
-MODNAME      := textinput
-MODDIR       := $(ROOT_SRCDIR)/core/$(MODNAME)
-MODDIRS      := $(MODDIR)/src
-MODDIRI      := $(MODDIR)/inc
+MODNAME       := textinput
+MODDIR        := $(ROOT_SRCDIR)/core/$(MODNAME)
+MODDIRS       := $(MODDIR)/src
+MODDIRI       := $(MODDIR)/inc
 
-TEXTINPUTDIR      := $(MODDIR)
-TEXTINPUTDIRS     := $(TEXTINPUTDIR)/src
-TEXTINPUTDIRI     := $(TEXTINPUTDIR)/inc
+TEXTINPUTDIR  := $(MODDIR)
+TEXTINPUTDIRS := $(TEXTINPUTDIR)/src
+TEXTINPUTDIRI := $(TEXTINPUTDIR)/inc
 
 ##### libTextInput - part of libCore #####
-TEXTINPUTL        := $(MODDIRI)/LinkDef.h
-TEXTINPUTDS       := $(call stripsrc,$(MODDIRS)/G__TextInput.cxx)
-TEXTINPUTDO       := $(TEXTINPUTDS:.cxx=.o)
-TEXTINPUTDH       := $(TEXTINPUTDS:.cxx=.h)
+TEXTINPUTL    := $(MODDIRI)/LinkDef.h
+TEXTINPUTDS   := $(call stripsrc,$(MODDIRS)/G__TextInput.cxx)
+TEXTINPUTDO   := $(TEXTINPUTDS:.cxx=.o)
+TEXTINPUTDH   := $(TEXTINPUTDS:.cxx=.h)
 
-LTEXTINPUTS       := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/textinput/*.cpp))
-LTEXTINPUTO       := $(call stripsrc,$(LTEXTINPUTS:.cpp=.o))
+LTEXTINPUTS   := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/textinput/*.cpp))
+LTEXTINPUTO   := $(call stripsrc,$(LTEXTINPUTS:.cpp=.o))
 
-TEXTINPUTH        := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
-TEXTINPUTS        := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
-TEXTINPUTO        := $(call stripsrc,$(TEXTINPUTS:.cxx=.o)) $(LTEXTINPUTO)
+TEXTINPUTH    := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+TEXTINPUTS    := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
+TEXTINPUTO    := $(call stripsrc,$(TEXTINPUTS:.cxx=.o)) $(LTEXTINPUTO)
 
-TEXTINPUTDEP      := $(TEXTINPUTO:.o=.d) $(TEXTINPUTDO:.o=.d)
+TEXTINPUTDEP  := $(TEXTINPUTO:.o=.d) $(TEXTINPUTDO:.o=.d) $(LTEXTINPUTO:.o=.d)
 
 # used in the main Makefile
-ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(TEXTINPUTH))
+ALLHDRS       += $(patsubst $(MODDIRI)/%.h,include/%.h,$(TEXTINPUTH))
 
 # include all dependency files
-INCLUDEFILES += $(TEXTINPUTDEP)
+INCLUDEFILES  += $(TEXTINPUTDEP)
 
 ##### local rules #####
 .PHONY:         all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME)
@@ -39,17 +39,18 @@ INCLUDEFILES += $(TEXTINPUTDEP)
 include/%.h:    $(TEXTINPUTDIRI)/%.h
 		cp $< $@
 
-$(LTEXTINPUTO): %.o: %.cpp
+$(LTEXTINPUTO): %.o: $(ROOT_SRCDIR)/%.cpp
+		$(MAKEDIR)
 		$(MAKEDEP) -R -f$*.d -Y -w 1000 -- $(CXXFLAGS) -D__cplusplus -- $<
 		$(CXX) $(OPT) $(CXXFLAGS) -I$(TEXTINPUTDIRS) $(CXXOUT)$@ -c $<
 
-$(TEXTINPUTDS):      $(TEXTINPUTH) $(TEXTINPUTL) $(ROOTCINTTMPDEP)
+$(TEXTINPUTDS): $(TEXTINPUTH) $(TEXTINPUTL) $(ROOTCINTTMPDEP)
 		$(MAKEDIR)
 		@echo "Generating dictionary $@..."
 		$(ROOTCINTTMP) -f $@ -c $(TEXTINPUTH) $(TEXTINPUTL)
 
 clean-$(MODNAME):
-		@rm -f $(TEXTINPUTO) $(TEXTINPUTDO)
+		@rm -f $(TEXTINPUTO) $(TEXTINPUTDO) $(LTEXTINPUTO)
 
 clean::         clean-$(MODNAME)
 
@@ -58,4 +59,5 @@ distclean-$(MODNAME): clean-$(MODNAME)
 
 distclean::     distclean-$(MODNAME)
 
+##### extra rules ######
 $(TEXTINPUTO): CXXFLAGS += -I$(TEXTINPUTDIRS)
