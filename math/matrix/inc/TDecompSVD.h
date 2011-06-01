b2dc4f4d37e05ec210965fd315d4af63f8eae1f9
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2011-06-01 08:41:51 +0000

    move the virtual functions GetNCols and GetNrows in the impelmentation file. No need to have them inlined
    git-svn-id: http://root.cern.ch/svn/root/trunk@39525 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/matrix/inc/TDecompSVD.h
+++ math/matrix/inc/TDecompSVD.h
@@ -52,8 +52,8 @@ public :
    virtual ~TDecompSVD() {}
 
            const TMatrixD  GetMatrix ();
-   Int_t     GetNrows  () const { return fU.GetNrows(); }
-   Int_t     GetNcols  () const { return fV.GetNcols(); }
+   virtual Int_t     GetNrows  () const;
+   virtual Int_t     GetNcols  () const;
            const TMatrixD &GetU      ()       { if ( !TestBit(kDecomposed) ) Decompose();
                                                  return fU; }
            const TMatrixD &GetV      ()       { if ( !TestBit(kDecomposed) ) Decompose();
