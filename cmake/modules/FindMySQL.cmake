8e3556b20e42134aba4b2750f349f3e834826879
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-06-17 16:59:57 +0000

    From Pere: This patch fixes the problem reported by Jan at https://savannah.cern.ch/bugs/?83017
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-30-00-patches@39804 27541ba8-7e3a-0410-8455-c3a389f83636

--- cmake/modules/FindMySQL.cmake
+++ cmake/modules/FindMySQL.cmake
@@ -4,51 +4,68 @@
 #  MYSQL_LIBRARIES   - List of libraries when using MySQL.
 #  MYSQL_FOUND       - True if MySQL found.
 
-IF (MYSQL_INCLUDE_DIR)
+if(MYSQL_INCLUDE_DIR OR MYSQL_)
   # Already in cache, be silent
   SET(MYSQL_FIND_QUIETLY TRUE)
-ENDIF (MYSQL_INCLUDE_DIR)
+endif()
 
-FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
-  /usr/local/mysql/include
-  /usr/local/include/mysql
-  /usr/local/include
-  /usr/include/mysql
-  /usr/include
-  /usr/mysql/include
-  $ENV{MYSQL_DIR}/include
-)
+if(NOT WIN32)
+  find_program(MYSQL_CONFIG_EXECUTABLE mysql_config
+    /usr/bin/
+    /usr/local/bin
+    $ENV{MYSQL_DIR}/bin
+  )
+endif()
 
-SET(MYSQL_NAMES mysqlclient mysqlclient_r)
-FIND_LIBRARY(MYSQL_LIBRARY
-  NAMES ${MYSQL_NAMES}
-  PATHS /usr/local/mysql/lib /usr/local/lib /usr/lib $ENV{MYSQL_DIR}/lib $ENV{MYSQL_DIR}/lib/opt
-)
+if(MYSQL_CONFIG_EXECUTABLE)
+  execute_process(COMMAND ${MYSQL_CONFIG_EXECUTABLE} --cflags OUTPUT_VARIABLE MYSQL_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+  separate_arguments(MYSQL_CFLAGS)
+  string( REGEX MATCH "-I[^;]+" MYSQL_INCLUDE_DIR "${MYSQL_CFLAGS}" )
+  string( REPLACE "-I" "" MYSQL_INCLUDE_DIR "${MYSQL_INCLUDE_DIR}")
+  string( REGEX REPLACE "-I[^;]+;" "" MYSQL_CFLAGS "${MYSQL_CFLAGS}" )
+  execute_process(COMMAND ${MYSQL_CONFIG_EXECUTABLE} --libs OUTPUT_VARIABLE MYSQL_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
+else()
+  find_path(MYSQL_INCLUDE_DIR mysql.h
+    /usr/local/mysql/include
+    /usr/local/include/mysql
+    /usr/local/include
+    /usr/include/mysql
+    /usr/include
+    /usr/mysql/include
+    $ENV{MYSQL_DIR}/include
+  )
+  set(MYSQL_NAMES mysqlclient mysqlclient_r)
+  find_library(MYSQL_LIBRARY NAMES ${MYSQL_NAMES}
+    PATHS /usr/local/mysql/lib /usr/local/lib /usr/lib $ENV{MYSQL_DIR}/lib $ENV{MYSQL_DIR}/lib/opt
+  )
+  set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+endif()
 
-IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-  SET(MYSQL_FOUND TRUE)
-  SET(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
-  iF(WIN32)
-    STRING(REPLACE mysqlclient libmysql libmysql ${MYSQL_LIBRARY})
-    SET(MYSQL_LIBRARIES ${libmysql} ${MYSQL_LIBRARIES})
-  ENDIF()
-ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-  SET(MYSQL_FOUND FALSE)
-  SET(MYSQL_LIBRARIES )
-ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+  set(MYSQL_FOUND TRUE)
+  if(WIN32)
+    string(REPLACE mysqlclient libmysql libmysql ${MYSQL_LIBRARY})
+    set(MYSQL_LIBRARIES ${libmysql} ${MYSQL_LIBRARIES})
+  endif()
+else()
+  set(MYSQL_FOUND FALSE)
+  set(MYSQL_LIBRARIES )
+endif()
 
-IF (MYSQL_FOUND)
-  IF (NOT MYSQL_FIND_QUIETLY)
-    MESSAGE(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
-  ENDIF (NOT MYSQL_FIND_QUIETLY)
-ELSE (MYSQL_FOUND)
-  IF (MYSQL_FIND_REQUIRED)
-    MESSAGE(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
-    MESSAGE(FATAL_ERROR "Could NOT find MySQL library")
-  ENDIF (MYSQL_FIND_REQUIRED)
-ENDIF (MYSQL_FOUND)
+if(MYSQL_FOUND)
+  if(NOT MYSQL_FIND_QUIETLY)
+    message(STATUS "Found MySQL libraries: ${MYSQL_LIBRARIES}")
+    message(STATUS "Found MySQL includes: ${MYSQL_INCLUDE_DIR}")
+  endif()
+else()
+  if(MYSQL_FIND_REQUIRED)
+    message(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
+    message(FATAL_ERROR "Could NOT find MySQL library")
+  endif()
+endif()
 
-MARK_AS_ADVANCED(
+mark_as_advanced(
+  MYSQL_CONFIG_EXECUTABLE
   MYSQL_LIBRARY
   MYSQL_INCLUDE_DIR
 )
