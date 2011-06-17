79f17901637c5fdb822607eecc1e0a8e5099d2fa
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-06-17 16:58:52 +0000

    From Pere: This patch fixes the problem reported by Jan at https://savannah.cern.ch/bugs/?83017
    git-svn-id: http://root.cern.ch/svn/root/trunk@39803 27541ba8-7e3a-0410-8455-c3a389f83636

--- cmake/modules/FindFFTW.cmake
+++ cmake/modules/FindFFTW.cmake
@@ -1,37 +1,42 @@
 # Find the FFTW includes and library.
 # 
 # This module defines
-# FFTW_INCLUDE_DIR, where to locate Pythia.h file
-# FFTW_LIBRARIES, the libraries to link against to use Pythia6
-# FFTW_FOUND.  If false, you cannot build anything that requires Pythia6.
-# FFTW_LIBRARY, where to find the libpythia8 library.
+# FFTW_INCLUDE_DIR, where to locate fftw3.h file
+# FFTW_LIBRARIES, the libraries to link against to use fftw3
+# FFTW_FOUND.  If false, you cannot build anything that requires fftw3.
+# FFTW_LIBRARY, where to find the libfftw3 library.
 
 set(FFTW_FOUND 0)
+if(FFTW_LIBRARY AND FFTW_INCLUDE_DIR)
+  set(FFTW_FIND_QUIETLY TRUE)
+endif()
 
 find_path(FFTW_INCLUDE_DIR fftw3.h
   $ENV{FFTW_DIR}/include
-  /usr/include
+  $ENV{FFTW3} $ENV{FFTW3}/include $ENV{FFTW3}/api
   /usr/local/include
-  /opt/include
-  /usr/apps/include
+  /usr/include
+  /opt/fftw3/include
   DOC "Specify the directory containing fftw3.h"
 )
 
-find_library(FFTW_LIBRARY NAMES libfftw3 libfftw3-3 PATHS
+find_library(FFTW_LIBRARY NAMES fftw3 fftw3-3 PATHS
   $ENV{FFTW_DIR}/lib
-  /usr/lib 
+  $ENV{FFTW3} $ENV{FFTW3}/lib $ENV{FFTW3}/.libs
   /usr/local/lib
-  /opt/lib
-  /sw/lib
+  /usr/lib 
+  /opt/fftw3/lib
   DOC "Specify the fttw3 library here."
 )
 
 if(FFTW_INCLUDE_DIR AND FFTW_LIBRARY)
   set(FFTW_FOUND 1 )
-  message(STATUS "Found fftw3 library at ${FFTW_LIBRARY}")
+  if(NOT FFTW_FIND_QUIETLY)
+     message(STATUS "Found fftw3 includes at ${FFTW_INCLUDE_DIR}")
+     message(STATUS "Found fftw3 library at ${FFTW_LIBRARY}")
+  endif()
 endif()
-
 
 set(FFTW_LIBRARIES ${FFTW_LIBRARY})
 
-MARK_AS_ADVANCED(FFTW_FOUND FFTW_LIBRARY FFTW_INCLUDE_DIR)
+mark_as_advanced(FFTW_FOUND FFTW_LIBRARY FFTW_INCLUDE_DIR)
