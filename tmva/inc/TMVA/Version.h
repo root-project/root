a32a21e8dc9a8defd522f2b9ef7fd2f8e7524de3
Eckhard von Toerne <evt@physik.uni-bonn.de>
2011-06-21 09:09:52 +0000

    adjusting makefile to reflect move of include directory, updating Version.h
    git-svn-id: http://root.cern.ch/svn/root/trunk@39846 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/inc/TMVA/Version.h
+++ tmva/inc/TMVA/Version.h
@@ -41,10 +41,10 @@
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
-#define TMVA_RELEASE      "4.1.1"
-#define TMVA_RELEASE_DATE "Apr 21, 2011"
-#define TMVA_RELEASE_TIME "12:00:00"
-#define TMVA_VERSION_CODE 262401
+#define TMVA_RELEASE      "4.1.2"
+#define TMVA_RELEASE_DATE "Jun 21, 2011"
+#define TMVA_RELEASE_TIME "11:02:58"
+#define TMVA_VERSION_CODE 262402
 #define TMVA_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
