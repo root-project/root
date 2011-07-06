78bc674856c666f1072fd68d8dd7f517157d0ac0
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-07-06 13:51:41 +0000

    before generating the html version of the reference manual generate the files tutorials/hsimple.root and tutorials/tree/cernstaff.root which are needed by some of the scripts. Fixes issue 83868.
    git-svn-id: http://root.cern.ch/svn/root/trunk@40146 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/makehtml.sh
+++ build/unix/makehtml.sh
@@ -2,6 +2,19 @@
 
 ROOT=bin/root.exe
 
+dir=`pwd`
+cd tutorials
+# we need tutorials/hsimple.root
+if [ ! -f hsimple.root ]; then
+   $ROOT -l -b -q hsimple.C
+fi
+cd tree
+# we need tutorials/tree/cernstaff.root
+if [ ! -f cernstaff.root ]; then
+   $ROOT -l -b -q cernbuild.C
+fi
+cd $dir
+
 echo ""
 echo "Generating doc in directory htmldoc/..."
 echo ""
