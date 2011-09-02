0a847e1d00acde2f88166bc04274b79d42e1f4a5
Axel Naumann <Axel.Naumann@cern.ch>
2011-09-02 13:54:19 +0000

    Replace malloc /... symbols instead of using malloc hooks: they are not thread safe. Fixes sporadic issue with prefetch test. Instead of atexit(), use d'tor.
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2354 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/pt_mymalloc.cpp
+++ scripts/pt_mymalloc.cpp
@@ -12,184 +12,144 @@
 #include <string>
 #include <errno.h>
 #include <pthread.h>
+#include <dlfcn.h>
 
 using namespace std;
-static void *(*old_malloc_hook)(size_t, const void *);
-static void (*old_free_hook)(void*, const void *);
-static void *(*old_realloc_hook)(void *, size_t, const void *);
-static void init_my_hooks(void);
-static void *my_malloc_hook(size_t, const void *);
-static void my_free_hook (void*, const void *);
-static void *my_realloc_hook(void *, size_t, const void *);
-static void writeMem(void);
 
-int fd;
-long arr [4];
-char* fifoname;
-pthread_mutex_t pt_mutex = PTHREAD_MUTEX_INITIALIZER;
+struct PerfTrackMallocInterposition {
+   PerfTrackMallocInterposition(): fFifoFD(-1), fPerfData() {
+      SetFunc((void**)&fPMalloc, "malloc");
+      SetFunc((void**)&fPRealloc, "realloc");
+      SetFunc((void**)&fPFree, "free");
 
-/* Override initializing hook from the C library. */
-void (*__malloc_initialize_hook)(void) = init_my_hooks;
+      pthread_mutex_init(&fgPTMutex, 0);
 
-static void init_my_hooks(void) {
-   static char envPreload[] = "LD_PRELOAD=";
-   putenv(envPreload);
+      static char envPreload[] = "LD_PRELOAD=";
+      putenv(envPreload);
 
-   pthread_mutex_init(&pt_mutex, 0);
-  {
-    fifoname = getenv("PT_FIFONAME");
-    /*
-    char *path=NULL;
-    path = getcwd(NULL, 0);
-    stringstream sstm;
-    sstm << path << "/pt_myfifo_" << getpid();
-    fifoname = new char[sstm.str().length() + 1];
-    strcpy(fifoname, sstm.str().c_str());
-    */
-  }
+      const char* fifoname = getenv("PT_FIFONAME");
+      if (fifoname) {
+         fFifoFD = open(fifoname, O_WRONLY);
+         if (fFifoFD < 0) printf( "Error opening file: %s\n", strerror( errno ) ); 
+      } else {
+         printf("Error: PT_FIFONAME not set!\n");
+         int * i = (int*)0x123;
+         *i = 12;
+      }
+      fPerfData[3]=699692586; // for collector to know that stored values are valid
 
-  fd= open(fifoname, O_WRONLY);
-  if( fd < 0 ) printf( "Error opening file: %s\n", strerror( errno ) ); 
-  arr[3]=699692586; // for collector to know that stored values are valid
-  
-  atexit(writeMem);
- 
-  old_malloc_hook = __malloc_hook;
-  __malloc_hook = my_malloc_hook;
-  old_free_hook = __free_hook;
-  __free_hook = my_free_hook;
-  old_realloc_hook = __realloc_hook;
-  __realloc_hook = my_realloc_hook;
+   }
+
+   ~PerfTrackMallocInterposition() {
+      pthread_mutex_destroy(&fgPTMutex);
+
+      if (fFifoFD >= 0 && write(fFifoFD, &fPerfData, sizeof(fPerfData))==-1)
+         printf("Error in writing %s\n", strerror(errno));
+   }
+
+   void SetFunc(void** ppFunc, const char* name) const {
+      *ppFunc = dlsym(RTLD_NEXT, name);
+      const char* error = dlerror();
+      if (error != NULL) {
+         fputs(error, stderr);
+         exit(1);
+      }
+   }
+
+   void* (*fPMalloc)(size_t);
+   void* (*fPRealloc)(void*, size_t);
+   void  (*fPFree)(void*);
+
+   int fFifoFD; // file decriptor of FIFO
+   long fPerfData[4]; // 0: heap, 1: maxheap, 2: sum allocs, 3: 699692586
+   static pthread_mutex_t fgPTMutex;
+};
+
+pthread_mutex_t PerfTrackMallocInterposition::fgPTMutex = PTHREAD_MUTEX_INITIALIZER;
+
+PerfTrackMallocInterposition& gPT() {
+   static PerfTrackMallocInterposition pt;
+   return pt;
 }
 
-static void *my_malloc_hook(size_t size, const void *caller)
+void *malloc(size_t size)
 {
-  pthread_mutex_lock(&pt_mutex);
+  pthread_mutex_lock(&gPT().fgPTMutex);
+  char* result = (char *)(*gPT().fPMalloc)(size+sizeof(int)+sizeof(size_t));
 
-  char *result;
-
-  __malloc_hook = old_malloc_hook;
-  __free_hook = old_free_hook;
-  __realloc_hook = old_realloc_hook;
-	   
-  result = (char *)malloc(size+sizeof(int)+sizeof(size_t));
-  old_malloc_hook = __malloc_hook;
-  old_free_hook = __free_hook;
-  old_realloc_hook = __realloc_hook;
-	   
-  arr[0]+=size; // heap
-  arr[2]+=size; // only allocs
-  if (arr[0]>arr[1]) arr[1]=arr[0]; // maximum (peak) heap
-	   
+  gPT().fPerfData[0]+=size; // heap
+  gPT().fPerfData[2]+=size; // only allocs
+  if (gPT().fPerfData[0]>gPT().fPerfData[1]) gPT().fPerfData[1]=gPT().fPerfData[0]; // maximum (peak) heap
+  pthread_mutex_unlock(&gPT().fgPTMutex);
+   
   *(int *)result=699692586;
   *(size_t *)(result+sizeof(int))=size;
-
-  __malloc_hook = my_malloc_hook;
-  __free_hook = my_free_hook;
-  __realloc_hook = my_realloc_hook;
-
-  pthread_mutex_unlock(&pt_mutex);
 
   return (void*) (result+sizeof(int)+sizeof(size_t));
 }
 
-static void my_free_hook (void *ptr, const void *caller)
+void free(void *ptr)
 {
   if (ptr==0) return;
 
-  pthread_mutex_lock(&pt_mutex);
-
-  __free_hook = old_free_hook;
-  __malloc_hook = old_malloc_hook;
-  __realloc_hook = old_realloc_hook;
-       
   int v1=*(int *) ((char*)ptr-sizeof(int)-sizeof(size_t));      
   if (v1!=699692586){
-    free(ptr);
-    old_free_hook = __free_hook;
-    old_malloc_hook = __malloc_hook;
-    old_realloc_hook = __realloc_hook;
-    __free_hook = my_free_hook;
-    __malloc_hook = my_malloc_hook;
-    __realloc_hook = my_realloc_hook;
-    pthread_mutex_unlock(&pt_mutex);
-    return;
+     (*gPT().fPFree)(ptr);
+     return;
   }
   size_t v2=* (size_t *) ((char*)ptr-sizeof(size_t));
-  arr[0]-=v2;
- 
-  free ((char*)ptr-sizeof(int)-sizeof(size_t)); 
-  old_free_hook = __free_hook;
-  old_malloc_hook = __malloc_hook;
-  old_realloc_hook = __realloc_hook;
-  __free_hook = my_free_hook;
-  __malloc_hook = my_malloc_hook;
-  __realloc_hook = my_realloc_hook;
 
-  pthread_mutex_unlock(&pt_mutex);
+  pthread_mutex_lock(&gPT().fgPTMutex);
+
+  gPT().fPerfData[0]-=v2;
+ 
+  (*gPT().fPFree)((char*)ptr-sizeof(int)-sizeof(size_t)); 
+
+  pthread_mutex_unlock(&gPT().fgPTMutex);
 }
 
-static void *my_realloc_hook(void *ptr, size_t size, const void *caller)
+void *realloc(void *ptr, size_t size)
 {
   char *result;
-
-  pthread_mutex_lock(&pt_mutex);
-
-  __malloc_hook = old_malloc_hook;
-  __free_hook = old_free_hook;
-  __realloc_hook = old_realloc_hook;
-
   int v1=699692586;
   if (ptr!=0) v1=*(int *) ((char*)ptr-sizeof(int)-sizeof(size_t));
+
+  pthread_mutex_lock(&gPT().fgPTMutex);
   if (v1!=699692586 || ptr==0){ 
     if (ptr==0 && size!=0){ // behaves as malloc   
-      arr[0]+=size; // heap mem
-      arr[2]+=size; // only allocs
-      if (arr[0]>arr[1]) arr[1]=arr[0]; // maximum heap
+      gPT().fPerfData[0]+=size; // heap mem
+      gPT().fPerfData[2]+=size; // only allocs
+      if (gPT().fPerfData[0]>gPT().fPerfData[1]) gPT().fPerfData[1]=gPT().fPerfData[0]; // maximum heap
 	       
-      result=(char *)realloc(0,size+sizeof(int)+sizeof(size_t));       
+      result=(char *)(*gPT().fPRealloc)(0,size+sizeof(int)+sizeof(size_t));       
       *(int *)result=699692586;
       *(size_t *)(result+sizeof(int))=size;
     }
     else{  
-      result=(char *)realloc(ptr,size); // old realloc call 
+      result=(char *)(*gPT().fPRealloc)(ptr,size); // old realloc call 
     }
   }
   else
     {
       size_t v2=*(size_t *) ((char*)ptr-sizeof(size_t));
       if (size==0){ // behaves as free
-	arr[0]-=v2;
-	result=(char *)realloc((char*)ptr-sizeof(int)-sizeof(size_t),0);
+	gPT().fPerfData[0]-=v2;
+	result=(char *)(*gPT().fPRealloc)((char*)ptr-sizeof(int)-sizeof(size_t),0);
       }
       else{
-	arr[0]=arr[0]+size-v2; // heap
-	arr[2]=arr[2]+size-v2; // only allocs
-	if (arr[0]>arr[1]) arr[1]=arr[0]; // maximum heap
+	gPT().fPerfData[0]=gPT().fPerfData[0]+size-v2; // heap
+	gPT().fPerfData[2]=gPT().fPerfData[2]+size-v2; // only allocs
+	if (gPT().fPerfData[0]>gPT().fPerfData[1]) gPT().fPerfData[1]=gPT().fPerfData[0]; // maximum heap
 		 
-	result = (char *)realloc((char*)ptr-sizeof(int)-sizeof(size_t),size+sizeof(int)+sizeof(size_t));
+	result = (char *)(*gPT().fPRealloc)((char*)ptr-sizeof(int)-sizeof(size_t),size+sizeof(int)+sizeof(size_t));
 	*(int *)result=699692586;
 	*(size_t *)(result+sizeof(int))=size;
       }
     }
-  old_malloc_hook = __malloc_hook;
-  old_free_hook = __free_hook;
-  old_realloc_hook = __realloc_hook;
-  __malloc_hook = my_malloc_hook;
-  __free_hook = my_free_hook;
-  __realloc_hook = my_realloc_hook;
 		 
- pthread_mutex_unlock(&pt_mutex); 
+ pthread_mutex_unlock(&gPT().fgPTMutex); 
 
  if (v1!=699692586 || size==0) return (void*) (result); 
   else return (void*) (result+sizeof(int)+sizeof(size_t)); 
 }
 
-static void writeMem(void)
-{
-  
-  pthread_mutex_destroy(&pt_mutex);
-
-  if (write(fd,&arr,4*sizeof(long))==-1)
-      printf("Error in writing %s\n", strerror( errno ));
-}
