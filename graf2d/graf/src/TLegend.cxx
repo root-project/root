440f115aca2c9f9e8c6c36c1b829802c2e1587b5
Olivier Couet <Olivier.Couet@cern.ch>
2011-09-07 12:43:34 +0000

    Improve help.
    git-svn-id: http://root.cern.ch/svn/root/trunk@40824 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/graf/src/TLegend.cxx
+++ graf2d/graf/src/TLegend.cxx
@@ -132,6 +132,29 @@ Begin_Macro(source)
    return c2;
 }
 End_Macro
+Begin_Html
+It is possible to draw the legend entries over several columns using
+the method <tt>SetNColumns()</tt> like in the following example.
+
+End_Html
+Begin_Macro(source)
+{
+   TCanvas *c3 = new TCanvas("c2","c2",500,300);
+ 
+   TLegend* leg = new TLegend(0.2, 0.2, .8, .8);
+   TH1* h = new TH1F("", "", 1, 0, 1);
+ 
+   leg-> SetNColumns(2);
+ 
+   leg->AddEntry(h, "Column 1 line 1", "l");
+   leg->AddEntry(h, "Column 2 line 1", "l");
+   leg->AddEntry(h, "Column 1 line 2", "l");
+   leg->AddEntry(h, "Column 2 line 2", "l");
+ 
+   leg->Draw();
+   return c3;
+}
+End_Macro
 */
 
 
