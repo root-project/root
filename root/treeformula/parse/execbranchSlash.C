98909a7701986f810911da8b023739c965241cbf
Philippe Canal <pcanal@fnal.gov>
2011-09-23 16:34:31 +0000

    Improve test portability
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2387 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/execbranchSlash.C
+++ root/treeformula/parse/execbranchSlash.C
@@ -11,13 +11,12 @@ TTree *createTree() {
    TTree *T = new TTree("T", "");
 
    double val;
-   gRandom->SetSeed(10);
 
    T->Branch(bname.c_str(), &val, "val/D");
    T->Branch("simple",&val);
 
    for (unsigned i=0; i<5; ++i) {
-      val = gRandom->Rndm();
+      val = (i+1) / 13.0;
       T->Fill();
    }
    T->ResetBranchAddresses();
@@ -35,7 +34,7 @@ void execbranchSlash()
    T->SetBranchAddress(bname.c_str(), &val);
    for (int i = 0; i<T->GetEntries(); i++) {
       T->GetEntry(i);
-      std::cout << i <<'\t'<< val << std::endl;
+      fprintf(stdout,"%d\t%0.6f\n",i,val);
    }
    
    std::cout << "PRINTING WITH SCAN AGAIN AFTER RENAME\n";
