2262893bfea943c3f8aee85bde52c5b3b73af5ed
Olivier Couet <Olivier.Couet@cern.ch>
2011-10-05 16:17:46 +0000

    Fix coverity report (uninitialized variables)
    git-svn-id: http://root.cern.ch/svn/root/trunk@41170 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/postscript/src/TPDF.cxx
+++ graf2d/postscript/src/TPDF.cxx
@@ -2193,7 +2193,7 @@ void TPDF::Text(Double_t xx, Double_t yy, const char *chars)
 
    if (txalh > 1) {
       TText t;
-      UInt_t w, h;
+      UInt_t w=0, h;
       t.SetTextSize(fTextSize);
       t.SetTextFont(fTextFont);
       t.GetTextExtent(w, h, chars);
@@ -2267,7 +2267,7 @@ void TPDF::Text(Double_t xx, Double_t yy, const char *chars)
    TText t;
    t.SetTextSize(fTextSize * scale);
    t.SetTextFont(fTextFont);
-   UInt_t wa1, wa0;
+   UInt_t wa1=0, wa0=0;
    t.GetTextAdvance(wa0, chars, kFALSE);
    t.GetTextAdvance(wa1, chars);
    t.TAttText::Modify();
@@ -2278,7 +2278,7 @@ void TPDF::Text(Double_t xx, Double_t yy, const char *chars)
    if (kerning) {
         charDeltas = new Int_t[len];
         for (Int_t i = 0;i < len;i++) {
-            UInt_t ww;
+            UInt_t ww=0;
             t.GetTextAdvance(ww, chars + i);
             charDeltas[i] = wa1 - ww;
         }
@@ -2289,7 +2289,7 @@ void TPDF::Text(Double_t xx, Double_t yy, const char *chars)
         tmp[1] = 0;
         for (Int_t i = 1;i < len;i++) {
             tmp[0] = chars[i-1];
-            UInt_t width;
+            UInt_t width=0;
             t.GetTextAdvance(width, &tmp[0], kFALSE);
             Double_t wwl = gPad->AbsPixeltoX(width - charDeltas[i]) - gPad->AbsPixeltoX(0);
             wwl -= 0.5*(gPad->AbsPixeltoX(1) - gPad->AbsPixeltoX(0)); // half a pixel ~ rounding error
