b93ab08b0f9ea2c3680b00116794a8ccbd218437
Olivier Couet <Olivier.Couet@cern.ch>
2011-10-06 11:00:21 +0000

    Fix coverity reports (UNINIT)
    git-svn-id: http://root.cern.ch/svn/root/trunk@41195 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/graf/src/TPaveText.cxx
+++ graf2d/graf/src/TPaveText.cxx
@@ -367,7 +367,7 @@ void TPaveText::InsertLine()
    // Add a new lineine at the mouse position.
 
    if (!gPad->IsEditable()) return;
-   Double_t ymouse, yobj;
+   Double_t ymouse=0, yobj;
    TObject *obj = GetObject(ymouse, yobj); //get object pointed by the mouse
    Double_t yline = (ymouse-fY1)/(fY2-fY1);
    TLine *newline = AddLine(0,yline,0,yline);
