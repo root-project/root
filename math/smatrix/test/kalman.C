cdaa2f7a343085f043bdbc623887039886173b24
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2011-10-11 07:12:27 +0000

    fix for https://savannah.cern.ch/bugs/?87633
    git-svn-id: http://root.cern.ch/svn/root/trunk@41315 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/smatrix/test/kalman.C
+++ math/smatrix/test/kalman.C
@@ -125,7 +125,7 @@ void kalman_do(const char *machine,int sym, int cut) {
    ts.SetTextSize(0.031);
    tt.SetTextColor(kRed);
    tt.SetTextSize(0.031);
-   char text[10];
+   char text[20];
    ts.SetTextAlign(22);
    for (i=0;i<=nx;i++) {
       line.DrawLine(xmin+i*dx,ymin,xmin+i*dx,ymax);
