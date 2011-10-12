91e669d2f68f17278a9f2df8eaebb76a93e1618a
Olivier Couet <Olivier.Couet@cern.ch>
2011-10-12 13:57:42 +0000

    Fix Coverty report #32930
    git-svn-id: http://root.cern.ch/svn/root/trunk@41360 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf3d/g3d/src/TMarker3DBox.cxx
+++ graf3d/g3d/src/TMarker3DBox.cxx
@@ -209,7 +209,9 @@ void TMarker3DBox::Paint(Option_t * /* option */ )
    buffer.SetSectionsValid(TBuffer3D::kCore);
 
    // We fill kCore and kRawSizes on first pass and try with viewer
-   Int_t reqSections = gPad->GetViewer3D()->AddObject(buffer);
+   TVirtualViewer3D * viewer3D = gPad->GetViewer3D();
+   if (!viewer3D) return;
+   Int_t reqSections = viewer3D->AddObject(buffer);
    if (reqSections == TBuffer3D::kNone) {
       return;
    }
@@ -281,7 +283,7 @@ void TMarker3DBox::Paint(Option_t * /* option */ )
       TAttFill::Modify();
    }
 
-   gPad->GetViewer3D()->AddObject(buffer);
+   viewer3D->AddObject(buffer);
 }
 
 
