e18f6e7f2738d2b83614718ab44ab4ea5aacd30b
Axel Naumann <Axel.Naumann@cern.ch>
2011-10-12 07:03:27 +0000

    Add missing getter for fClassName; reimplementing TBranch::GetClassName().
    git-svn-id: http://root.cern.ch/svn/root/trunk@41347 27541ba8-7e3a-0410-8455-c3a389f83636

--- tree/tree/inc/TBranchClones.h
+++ tree/tree/inc/TBranchClones.h
@@ -50,6 +50,7 @@ public:
    
    virtual void    Browse(TBrowser *b);
    virtual Int_t   Fill();
+   virtual const char* GetClassName() const { return fClassName; }
    virtual Int_t   GetEntry(Long64_t entry=0, Int_t getall = 0);
    virtual Int_t   GetN() const {return fN;}
    TClonesArray    *GetList() const {return fList;}
