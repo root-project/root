b6c3e201c241779db5ac9e94f3845a38c36615d0
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2011-10-14 12:08:55 +0000

    Add more flexibility to the definition of the library path seen by proofserv. So far   to avoid ambiguites in some cases, $ROOTSYS/lib was removed and the one of the ROOT   version chosen was added later on in front. This proved to be to aggressive in some   cases. Now by default the path is the same seen by xproofd; the new directive   xpd.filterlibpaths (see http://root.cern.ch/drupal/content/configuration-reference-guide#filterlibpaths).   allows to remove selected subpaths.
    git-svn-id: http://root.cern.ch/svn/root/trunk@41393 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proofd/inc/XrdProofdManager.h
+++ proof/proofd/inc/XrdProofdManager.h
@@ -170,6 +170,10 @@ class XrdProofdManager : public XrdProofdConfig {
    bool              fMultiUser;      // Allow/disallow multi-user mode
    bool              fChangeOwn;      // TRUE is ownership has to be changed
 
+   // Lib paths for proofserv
+   bool              fRemoveROOTLibPaths; // If true the existing ROOT lib paths are removed
+   XrdOucHash<XrdOucString> fLibPathsToRemove;  // Additional paths to be removed
+
    //
    // Lists
    std::list<XrdOucString *> fMastersAllowed; // list of master (domains) allowed
@@ -180,6 +184,7 @@ class XrdProofdManager : public XrdProofdConfig {
    int               DoDirectiveAllowedUsers(char *, XrdOucStream *, bool);
    int               DoDirectiveDataDir(char *, XrdOucStream *, bool);
    int               DoDirectiveDataSetSrc(char *, XrdOucStream *, bool);
+   int               DoDirectiveFilterLibPaths(char *, XrdOucStream *, bool);
    int               DoDirectiveGroupfile(char *, XrdOucStream *, bool);
    int               DoDirectiveMaxOldLogs(char *, XrdOucStream *, bool);
    int               DoDirectiveMultiUser(char *, XrdOucStream *, bool);
