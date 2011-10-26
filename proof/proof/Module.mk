084ca134cbd98b5cf381aff81638f889d5167426
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2011-10-26 10:45:25 +0000

    - Import patches in configure and net/xrootd/src/xrootd/configure.classic to support     smooth configuration on Ubuntu 11.04 or newer (automatic detection of the new     location of the system libraries).   - Adapt patch #39078 to bypass a compilation problem with gcc 4.5.2 on linux .
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-27-06-patches@41589 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proof/Module.mk
+++ proof/proof/Module.mk
@@ -66,5 +66,15 @@ distclean-$(MODNAME): clean-$(MODNAME)
 
 distclean::     distclean-$(MODNAME)
 
+##### extra rules ######
+ifeq ($(PLATFORM),linux)
+ifeq ($(GCC_VERS_FULL),gcc-4.5.2)
+# ifneq ($(filter -O%,$(OPT)),)
+   $(PROOFDIRS)/TDataSetManager.o: OPT = -O
+   $(PROOFDIRS)/TDSet.o: OPT = -O
+# endif
+endif
+endif
+
 # Optimize dictionary with stl containers.
 $(PROOFDO): NOOPT = $(OPT)
