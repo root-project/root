100e43b39581be4c5201896ddb915123838565fa
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2011-11-04 17:22:48 +0000

    Import patches 41785 and 41786:    - Fix issue in TPacketizerAdaptive and TPacketizer preventi proper selection of      event sub-ranges, i.e. when processing num != -1 entries starting at first > 0 .    - In WriteDataSet, remove call to Sort() which modifies the order of the initial      collection, uselessly hardening any comparison and confusing any expectation.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-30-00-patches@41787 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proofplayer/src/TPacketizer.cxx
+++ proof/proofplayer/src/TPacketizer.cxx
@@ -445,31 +445,38 @@ TPacketizer::TPacketizer(TDSet *dset, TList *slaves, Long64_t first,
             continue; // break ??
          }
 
-         if (cur <= first) {
-            // If this element contains the start of the global range
-            // adjust its start and number of entries
-            e->SetFirst( eFirst + (first - cur) );
-            e->SetNum( e->GetNum() - (first - cur) );
-            PDB(kPacketizer,2)
-               Info("TPacketizer", " --> adjust start %lld and end %lld",
-                    eFirst + (first - cur), first + num - cur);
-            cur += eNum;
-            eNum = e->GetNum();
+         Bool_t inRange = kFALSE;
+         if (cur <= first || (num != -1 && (first+num <= cur+eNum))) {
 
-         } else  if (num != -1 && (first+num <= cur+eNum)) {
-            // If this element contains the end of the global range
-            // adjust its number of entries
-            e->SetNum( first + num - cur );
-            PDB(kPacketizer,2)
-               Info("TPacketizer", " --> adjust end %lld", first + num - cur);
-            cur += eNum;
-            eNum = e->GetNum();
+            if (cur <= first) {
+               // If this element contains the start of the global range
+               // adjust its start and number of entries
+               e->SetFirst( eFirst + (first - cur) );
+               e->SetNum( e->GetNum() - (first - cur) );
+               PDB(kPacketizer,2)
+                  Info("TPacketizer", " --> adjust start %lld and end %lld",
+                       eFirst + (first - cur), first + num - cur);
+               inRange = kTRUE;
+            }
+            if (num != -1 && (first+num <= cur+eNum)) {
+               // If this element contains the end of the global range
+               // adjust its number of entries
+               e->SetNum( first + num - e->GetFirst() - cur );
+               PDB(kPacketizer,2)
+                  Info("TPacketizer", " --> adjust end %lld", first + num - cur);
+               inRange = kTRUE;
+            }
 
          } else {
             // Increment the counter ...
             PDB(kPacketizer,2)
                Info("TPacketizer", " --> increment 'cur' by %lld", eNum);
             cur += eNum;
+         }
+         // Re-adjust eNum and cur, if needed
+         if (inRange) {
+            cur += eNum;
+            eNum = e->GetNum();
          }
 
       } else {
