6788838aff1ac94691837a466d03dddaa8eae933
Fons Rademakers <Fons.Rademakers@cern.ch>
2011-11-10 15:43:24 +0000

    don't remove .gif's from tutorials/doc directory.
    git-svn-id: http://root.cern.ch/svn/root/trunk@41878 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/distfilelist.sh
+++ build/unix/distfilelist.sh
@@ -23,7 +23,7 @@ mv -f tutorials/mlp/mlpHiggs.root tutorials/mlp/mlpHiggs.root-
 mv -f tutorials/quadp/stock.root tutorials/quadp/stock.root-
 find tutorials -name "*.root" -exec rm -rf {} \; >/dev/null 2>&1;true
 find tutorials -name "*.ps" -exec rm -rf {} \; >/dev/null 2>&1;true
-find tutorials -name "*.gif" -exec rm -rf {} \; >/dev/null 2>&1;true
+find tutorials -path tutorials/doc -prune -o -name "*.gif" -exec rm -rf {} \; >/dev/null 2>&1;true
 find tutorials -name "so_locations" -exec rm -rf {} \; >/dev/null 2>&1;true
 find tutorials -name "pca.C" -exec rm -rf {} \; >/dev/null 2>&1;true
 find tutorials -name "*.so" -exec rm -rf {} \; >/dev/null 2>&1;true
