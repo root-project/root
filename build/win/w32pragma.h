1dc25b54b0477d0d5b6ea5546e35bf0dd335a2d2
Axel Naumann <Axel.Naumann@cern.ch>
2011-11-10 16:50:29 +0000

    Silence "X needs to have dll-interface to be used by clients of class Y" warnings.
    git-svn-id: http://root.cern.ch/svn/root/trunk@41884 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/win/w32pragma.h
+++ build/win/w32pragma.h
@@ -49,6 +49,8 @@
 #pragma warning (disable: 4748)
 /* function(): resolved overload was found by argument-dependent lookup */
 #pragma warning (disable: 4675)
+/* X needs to have dll-interface to be used by clients of class Y */
+#pragma warning (disable: 4251)
 
 /* function is hidden */
 #pragma warning (3: 4266)
