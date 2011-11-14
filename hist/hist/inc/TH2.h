723dc133cf1b8dff31ae098dcd14bf4c723cc075
Rene Brun <Rene.Brun@cern.ch>
2011-11-14 16:54:29 +0000

    Set a default value (nbins=1) for teh argument of TH2::SetShowProjectionX and Y.
    git-svn-id: http://root.cern.ch/svn/root/trunk@41968 27541ba8-7e3a-0410-8455-c3a389f83636

--- hist/hist/inc/TH2.h
+++ hist/hist/inc/TH2.h
@@ -105,8 +105,8 @@ public:
          TH1D      *ProjectionY(const char *name="_py", Int_t firstxbin=0, Int_t lastxbin=-1, Option_t *option="") const; // *MENU*
    virtual void     PutStats(Double_t *stats);
    virtual void     Reset(Option_t *option="");
-   virtual void     SetShowProjectionX(Int_t nbins);  // *MENU*
-   virtual void     SetShowProjectionY(Int_t nbins);  // *MENU*
+   virtual void     SetShowProjectionX(Int_t nbins=1);  // *MENU*
+   virtual void     SetShowProjectionY(Int_t nbins=1);  // *MENU*
    virtual TH1     *ShowBackground(Int_t niter=20, Option_t *option="same");
    virtual Int_t    ShowPeaks(Double_t sigma=2, Option_t *option="", Double_t threshold=0.05); // *MENU*
    virtual void     Smooth(Int_t ntimes=1, Option_t *option=""); // *MENU*
