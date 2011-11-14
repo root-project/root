b9d4f8d4246ccd776429702be850bb93ca543cc1
Andrei Gheata <Andrei.Gheata@cern.ch>
2011-11-14 08:19:12 +0000

    Fix sequence: td = GetThreadData followed by Voxelize which invalidates the thread data pointed by td
    git-svn-id: http://root.cern.ch/svn/root/trunk@41956 27541ba8-7e3a-0410-8455-c3a389f83636

--- geom/geom/src/TGeoVoxelFinder.cxx
+++ geom/geom/src/TGeoVoxelFinder.cxx
@@ -44,6 +44,10 @@ TGeoVoxelFinder::ThreadData_t::ThreadData_t() :
    fNcandidates(0), fCurrentVoxel(0), fCheckList(0), fBits1(0)
 {
    // Constructor.
+   fSlices[0] = fSlices[1] = fSlices[2] = 0;
+   fInc[0] = fInc[1] = fInc[2] = 0;
+   fInvdir[0] = fInvdir[1] = fInvdir[2] = 0.;
+   fLimits[0] = fLimits[1] = fLimits[2] = 0;
 }
 
 //______________________________________________________________________________
@@ -670,11 +674,11 @@ Int_t *TGeoVoxelFinder::GetNextCandidates(Double_t *point, Int_t &ncheck, Int_t 
 {
 // Returns list of new candidates in next voxel. If NULL, nowhere to
 // go next. 
-   ThreadData_t& td = GetThreadData(tid);
    if (NeedRebuild()) {
       Voxelize();
       fVolume->FindOverlaps();
    }   
+   ThreadData_t& td = GetThreadData(tid);
    ncheck = 0;
    if (td.fLimits[0]<0) return 0;
    if (td.fLimits[1]<0) return 0;
@@ -1231,12 +1235,12 @@ Int_t *TGeoVoxelFinder::GetNextCandidates(Double_t *point, Int_t &ncheck, Int_t 
 void TGeoVoxelFinder::SortCrossedVoxels(Double_t *point, Double_t *dir, Int_t tid)
 {
 // get the list in the next voxel crossed by a ray
-   ThreadData_t& td = GetThreadData(tid);
    if (NeedRebuild()) {
       TGeoVoxelFinder *vox = (TGeoVoxelFinder*)this;
       vox->Voxelize();
       fVolume->FindOverlaps();
    }   
+   ThreadData_t& td = GetThreadData(tid);
    td.fCurrentVoxel = 0;
 //   printf("###Sort crossed voxels for %s\n", fVolume->GetName());
    td.fNcandidates = 0;
@@ -1364,11 +1368,11 @@ void TGeoVoxelFinder::SortCrossedVoxels(Double_t *point, Double_t *dir, Int_t ti
 Int_t *TGeoVoxelFinder::GetCheckList(Double_t *point, Int_t &nelem, Int_t tid)
 {
 // get the list of daughter indices for which point is inside their bbox
-   ThreadData_t& td = GetThreadData(tid);
    if (NeedRebuild()) {
       Voxelize();
       fVolume->FindOverlaps();
    }   
+   ThreadData_t& td = GetThreadData(tid);
    if (fVolume->GetNdaughters() == 1) {
       if (fXb) {
          if (point[0]<fXb[0] || point[0]>fXb[1]) return 0;
@@ -1525,11 +1529,11 @@ Int_t *TGeoVoxelFinder::GetNextVoxel(Double_t *point, Double_t * /*dir*/, Int_t 
 {
 // get the list of new candidates for the next voxel crossed by current ray
 //   printf("### GetNextVoxel\n");
-   ThreadData_t& td = GetThreadData(tid);
    if (NeedRebuild()) {
       Voxelize();
       fVolume->FindOverlaps();
    }   
+   ThreadData_t& td = GetThreadData(tid);
    if (td.fCurrentVoxel==0) {
 //      printf(">>> first voxel, %i candidates\n", ncheck);
 //      printf("   bits[0]=%i\n", gGeoManager->GetBits()[0]);
