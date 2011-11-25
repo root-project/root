6fbc1f1d402590602da8e5fd6ffea32673557b96
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2011-11-25 12:14:18 +0000

    Test it in language-independent way
    git-svn-id: http://root.cern.ch/svn/root/trunk@42240 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/test/Prompt/ValuePrinter/Assignments.C
+++ cint/cling/test/Prompt/ValuePrinter/Assignments.C
@@ -14,23 +14,22 @@ std::string s("xyz")
 // CHECK: (std::string) @0x{{[0-9A-Fa-f]{8,}.}}
 // CHECK: c_str: "xyz"
 
+#include <limits.h>
 class Outer { 
 public: 
   struct Inner { 
     enum E{ 
-      // Note max and min ints are platform dependent since we cannot use 
-      //limits.h
-      A = 2147483647,
+      A = INT_MAX,
       B = 2, 
       C = 2,
-      D = -2147483648 
+      D = INT_MIN
     } ABC; 
   }; 
 };
 Outer::Inner::C
 // CHECK: (enum Outer::Inner::E const) @0x{{[0-9A-Fa-f].*}}
-// CHECK: (Outer::Inner::E::B) ? (Outer::Inner::E::C) : (int) 2
+// CHECK: (Outer::Inner::E::B) ? (Outer::Inner::E::C) : (int) {{[0-9].*}} 
 Outer::Inner::D
 // CHECK: (enum Outer::Inner::E const) @0x{{[0-9A-Fa-f].*}}
-// CHECK: (Outer::Inner::E::D) : (int) -2147483648
+// CHECK: (Outer::Inner::E::D) : (int) -{{[0-9].*}} 
 .q
