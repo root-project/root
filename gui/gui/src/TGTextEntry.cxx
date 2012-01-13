6a79ba3a037e2c30a4d599768a598bffcfd26c4d
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2012-01-13 13:27:07 +0000

    Silent harmless coverity reports #36013 (dereference null return)
    git-svn-id: http://root.cern.ch/svn/root/trunk@42627 27541ba8-7e3a-0410-8455-c3a389f83636

--- gui/gui/src/TGTextEntry.cxx
+++ gui/gui/src/TGTextEntry.cxx
@@ -1825,7 +1825,9 @@ void TGTextEntry::SavePrimitive(ostream &out, Option_t *option /*= ""*/)
    // font + GC
    option = GetName()+5;         // unique digit id of the name
    TString parGC, parFont;
+   // coverity [null_returns]
    parFont.Form("%s::GetDefaultFontStruct()",IsA()->GetName());
+   // coverity [null_returns]
    parGC.Form("%s::GetDefaultGC()()",IsA()->GetName());
 
    if ((GetDefaultFontStruct() != fFontStruct) || (GetDefaultGC()() != fNormGC.GetGC())) {
