2332c83a989cde1c1b2e2b5f89d7bc0b6c56cfc9
Axel Naumann <Axel.Naumann@cern.ch>
2012-02-10 07:55:12 +0000

    Be more verbose on the switch / case fall through, also to help Coverity.
    git-svn-id: http://root.cern.ch/svn/root/trunk@42954 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/src/TString.cxx
+++ core/base/src/TString.cxx
@@ -708,23 +708,22 @@ namespace {
       uint64_t k2 = 0;
 
       switch(len & 15) {
-         // intentionally falling through in all cases
-         case 15: k2 ^= uint64_t(tail[14]) << 48;
-         case 14: k2 ^= uint64_t(tail[13]) << 40;
-         case 13: k2 ^= uint64_t(tail[12]) << 32;
-         case 12: k2 ^= uint64_t(tail[11]) << 24;
-         case 11: k2 ^= uint64_t(tail[10]) << 16;
-         case 10: k2 ^= uint64_t(tail[ 9]) << 8;
+      case 15: k2 ^= uint64_t(tail[14]) << 48;    // fall through
+         case 14: k2 ^= uint64_t(tail[13]) << 40; // fall through
+         case 13: k2 ^= uint64_t(tail[12]) << 32; // fall through
+         case 12: k2 ^= uint64_t(tail[11]) << 24; // fall through
+         case 11: k2 ^= uint64_t(tail[10]) << 16; // fall through
+         case 10: k2 ^= uint64_t(tail[ 9]) << 8;  // fall through
          case  9: k2 ^= uint64_t(tail[ 8]) << 0;
             k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
-
-         case  8: k1 ^= uint64_t(tail[ 7]) << 56;
-         case  7: k1 ^= uint64_t(tail[ 6]) << 48;
-         case  6: k1 ^= uint64_t(tail[ 5]) << 40;
-         case  5: k1 ^= uint64_t(tail[ 4]) << 32;
-         case  4: k1 ^= uint64_t(tail[ 3]) << 24;
-         case  3: k1 ^= uint64_t(tail[ 2]) << 16;
-         case  2: k1 ^= uint64_t(tail[ 1]) << 8;
+                                                  // fall through
+         case  8: k1 ^= uint64_t(tail[ 7]) << 56; // fall through
+         case  7: k1 ^= uint64_t(tail[ 6]) << 48; // fall through
+         case  6: k1 ^= uint64_t(tail[ 5]) << 40; // fall through
+         case  5: k1 ^= uint64_t(tail[ 4]) << 32; // fall through
+         case  4: k1 ^= uint64_t(tail[ 3]) << 24; // fall through
+         case  3: k1 ^= uint64_t(tail[ 2]) << 16; // fall through
+         case  2: k1 ^= uint64_t(tail[ 1]) << 8;  // fall through
          case  1: k1 ^= uint64_t(tail[ 0]) << 0;
             k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
       };
