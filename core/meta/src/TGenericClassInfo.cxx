bc96ffc63e8f77b1512f45ddab8f8f28eb7593b5
Philippe Canal <pcanal@fnal.gov>
2012-02-14 19:50:16 +0000

    Import revision 43008 from the trunk: Add missing implementation (TGenericClassInfo::GetDirectoryAutoAdd)
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-30-00-patches@43010 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/meta/src/TGenericClassInfo.cxx
+++ core/meta/src/TGenericClassInfo.cxx
@@ -602,5 +602,11 @@ namespace ROOT {
       return fDestructor;
    }
 
+   DirAutoAdd_t  TGenericClassInfo::GetDirectoryAutoAdd() const
+   {
+      // Get the wrapper around the directory-auto-add function .
+
+      return fDirAutoAdd;
+   }
 
 }
