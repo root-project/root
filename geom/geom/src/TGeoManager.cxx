cc34049552d4697e91b790f88aff5bbd6d419757
Andrei Gheata <Andrei.Gheata@cern.ch>
2012-02-14 16:08:39 +0000

    Fix for compilation error on i686-apple-darwin11-llvm-g++-4.2
    git-svn-id: http://root.cern.ch/svn/root/trunk@43005 27541ba8-7e3a-0410-8455-c3a389f83636

--- geom/geom/src/TGeoManager.cxx
+++ geom/geom/src/TGeoManager.cxx
@@ -950,8 +950,9 @@ Int_t TGeoManager::ThreadId()
 {
 // Translates the current thread id to an ordinal number. This can be used to
 // manage data which is pspecific for a given thread.
-   static __thread Int_t tid = -1;
-   if (tid > -1) return tid;
+//   static __thread Int_t tid = -1;
+//   if (tid > -1) return tid;
+   Int_t tid = 0;
    if (gGeoManager && !gGeoManager->IsMultiThread()) return 0;
    Long_t selfId = TThread::SelfId();
    TGeoManager::ThreadsMapIt_t it = fgThreadId->find(selfId);
