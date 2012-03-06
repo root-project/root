1d87641911481eb49ceb54add3799425a00d0752
Fons Rademakers <Fons.Rademakers@cern.ch>
2012-03-06 09:59:40 +0000

    Stamp development version 5.33/02.
    git-svn-id: http://root.cern.ch/svn/root/trunk@43250 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.33/01"
-#define ROOT_RELEASE_DATE "Nov  3 2011"
-#define ROOT_RELEASE_TIME "18:36:41"
-#define ROOT_SVN_REVISION 41764
+#define ROOT_RELEASE "5.33/02"
+#define ROOT_RELEASE_DATE "Mar  6 2012"
+#define ROOT_RELEASE_TIME "10:30:14"
+#define ROOT_SVN_REVISION 43245
 #define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 336129
+#define ROOT_VERSION_CODE 336130
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
