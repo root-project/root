54c1bc61c782285e3e5b660e231ff93a05aea142
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-03-13 13:25:05 +0000

    Remove one of the expected-to-fail constructs. It is not expected to fail any more because of a patch in clang that keeps the last lexer in the preprocessor's lexer stack alive.
    git-svn-id: http://root.cern.ch/svn/root/trunk@43344 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/test/ErrorRecovery/CurrentFailures.C
+++ cint/cling/test/ErrorRecovery/CurrentFailures.C
@@ -14,15 +14,3 @@ class MyClass {
 #include "Overloads.h"
 
 #include "SubsequentDecls.h"
-
-// The issue is that when we don't have semicolon in the end of the input
-// in the case of cling where we have incremental compilation the parser 
-// encounters an error and tries to recover by looking for so called safe tokens
-// or anchors (e.g ';' or EOF). The input doesn't have semicolon and we play 
-// with Lexer's EOF tokens. My assumption is that while trying to recover
-// and continue parsing the Lexer enters sort-of error state and it breaks with // assertion
-extern "C" int printf // expected-error {{expected ';' after top level declarator}}
-
-a;
-
-
