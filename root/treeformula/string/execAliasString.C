0e6881c044aff98e0935164324d690c9a497a0d4
Philippe Canal <pcanal@fnal.gov>
2012-03-27 15:48:49 +0000

    add test of using strings in TTree's alias and TTreeFormula
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2508 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/treeformula/string/execAliasString.C
@@ -0,0 +1,37 @@
+{
+   bool error = false;
+   tree = new TTree ("tree", "tree");
+   tree.ReadFile ("test.txt","",' ');
+   tree.SetAlias("first", "(name==\"16A\")");
+   tree.SetAlias("second", "count==2");
+   tree.Scan();
+   
+   Long64_t res = tree.Draw("name", "first");
+   if (res != 1) {
+      error = true;
+      fprintf(stdout,"Error: Draw(\"name\", \"first\") returned %d instead of 1\n",res);
+   }
+   res = tree.Draw("name", "second");
+   if (res != 1) {
+      error = true;
+      fprintf(stdout,"Error: Draw(\"name\", \"second\") returned %d instead of 1\n",res);
+   }
+   res = tree.Draw("name", "first");
+   if (res != 1) {
+      error = true;
+      fprintf(stdout,"Error: Second run of Draw(\"name\", \"first\") returned %d instead of 1\n",res);
+   }
+   res = tree.Draw("name", "(name==\"16A\")");
+   if (res != 1) {
+      error = true;
+      fprintf(stdout,"Error: Draw(\"name\", \"(name==\\\"16A\\\")\") returned %d instead of 1\n",res);
+   }
+   tree->Scan("first?name:\"not first\" ");
+   tree->Scan("first?name:\"not first\" ","!first");
+   tree->Scan("name:count:first:second");
+   tree->Scan("name:count:first:second","first");
+   tree->Scan("name:count:first:second","second");
+   
+   return error;
+   
+}
