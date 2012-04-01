2af02f6ebd6d683af4a4fd56b20a9c32cfaa67ec
Axel Naumann <Axel.Naumann@cern.ch>
2012-04-01 14:35:23 +0000

    Be a verbose zombie if existing file cannot be deleted on recreate instead of silently failing.
    git-svn-id: http://root.cern.ch/svn/root/trunk@43601 27541ba8-7e3a-0410-8455-c3a389f83636

--- io/io/src/TFile.cxx
+++ io/io/src/TFile.cxx
@@ -414,8 +414,12 @@ TFile::TFile(const char *fname1, Option_t *option, const char *ftitle, Int_t com
    }
 
    if (recreate) {
-      if (!gSystem->AccessPathName(fname, kFileExists))
-         gSystem->Unlink(fname);
+      if (!gSystem->AccessPathName(fname, kFileExists)) {
+         if (gSystem->Unlink(fname) != 0) {
+            Error("TFile", "error deleting existing %s", fname);
+            goto zombie;
+         }
+      }
       recreate = kFALSE;
       create   = kTRUE;
       fOption  = "CREATE";
