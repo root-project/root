456742f470796a45b7cff469d3980f1a1671e00e
Axel Naumann <Axel.Naumann@cern.ch>
2012-04-03 13:49:12 +0000

    Fix missing symbols in libEvent on Ubuntu / gold
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2517 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/Rules.mk
+++ scripts/Rules.mk
@@ -626,7 +626,7 @@ else
 MAKELIB       = $(ROOTSYS)/build/unix/makelib.sh $(MKLIBOPTIONS)
 endif
 
-ROOTCORELIBS_LIST = Core Cint Tree Hist TreePlayer
+ROOTCORELIBS_LIST = TreePlayer Tree Graf Hist Physics Core Cint
 ROOTCORELIBS = $(addprefix $(ROOT_LOC)/lib/lib,$(addsuffix .$(LibSuf),$(ROOTCORELIBS_LIST)))
 ROOTCINT = $(ROOT_LOC)/bin/rootcint$(ExeSuf)
 
