5a19303c375ead0fcbaa15a14118c07cbe325356
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2012-04-16 10:53:41 +0000

    Use more appropriate timing to let time to slow X to synchronize...
    git-svn-id: http://root.cern.ch/svn/root/trunk@43756 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressGUI.cxx
+++ test/stressGUI.cxx
@@ -412,7 +412,7 @@ void ProcessFrame(TGFrame *f, const char *title)
    // Save a capture of frame f in a png file.
 
    gClient->HandleInput();
-   gSystem->Sleep(50);
+   gSystem->Sleep(250);
    gSystem->ProcessEvents();
    gErrorIgnoreLevel = 9999;
 
@@ -1875,6 +1875,7 @@ void testFontDlg()
                                         &prop, "", 0, kFALSE);
    dlg->SetWMPosition(0, 0);
    dlg->MapWindow();
+   gSystem->Sleep(250);
    ProcessFrame((TGMainFrame*)dlg, "Font Dialog");
    delete dlg;
 }
@@ -2292,6 +2293,7 @@ const char *excluded[] = {
    "QtMultiFileDialog",
    "QtPrintDialog",
    "calendar",
+   "customContextMenu.C",
    "customTH1Fmenu",
    "exec_macro",
    "guitest0",
