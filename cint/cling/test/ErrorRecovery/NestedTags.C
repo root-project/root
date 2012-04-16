62b0376dac20cf29f44e4b3d9cacfdc921b6e87d
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-04-16 10:19:53 +0000

    Allow the test to succeed on 32-bit platforms as well
    git-svn-id: http://root.cern.ch/svn/root/trunk@43754 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/test/ErrorRecovery/NestedTags.C
+++ cint/cling/test/ErrorRecovery/NestedTags.C
@@ -9,7 +9,8 @@ namespace Outer { struct Inner { enum E{i = 2}; }; };
 .rawInput
 
 Outer::Inner::i
-// CHECK: (enum Outer::Inner::E const) @0x{{[0-9A-Fa-f]{8,}.}}
+// CHECK: (enum Outer::Inner::E const) @0x{{[0-9A-Fa-f]{7,12}.}} 
+// CHECK: (Outer::Inner::E::i) : (int) 2
 
 enum A{a}; // 
 enum A{a}; // expected-error {{redefinition of 'A'}} expected-note {{previous definition is here}}
