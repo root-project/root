c182ca7e8531e9eca6a41e8499e47b5c76917553
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-04-18 13:35:31 +0000

    Remove declaration which is recovered correctly from the failures.
    git-svn-id: http://root.cern.ch/svn/root/trunk@43821 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/test/ErrorRecovery/CurrentFailures.C
+++ cint/cling/test/ErrorRecovery/CurrentFailures.C
@@ -1,14 +1,5 @@
 // RUN: cat %s | %cling -I%p | FileCheck %s
 // XFAIL: *
 
-// Class defined in the wrapper. Most probably the DeclExtractor pulls it out
-// in a wrong way.
-class MyClass {
-  struct {
-    int a;
-    error_here; // expected-error {{C++ requires a type specifier for all declarations}}
-  };
-};
-
-
 #include "Overloads.h"
+error_here;
