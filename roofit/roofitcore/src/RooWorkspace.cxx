1bbe524c43b592f150d6f42fc1285e6b6e815818
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2012-05-29 08:57:11 +0000

    from Gabriel: improve error messages by using quotes for object names
    git-svn-id: http://root.cern.ch/svn/root/trunk@44326 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/roofitcore/src/RooWorkspace.cxx
+++ roofit/roofitcore/src/RooWorkspace.cxx
@@ -782,8 +782,8 @@ Bool_t RooWorkspace::defineSet(const char* name, const RooArgSet& aset, Bool_t i
       if (importMissing) {
 	import(*sarg) ;
       } else {
-	coutE(InputArguments) << "RooWorkspace::defineSet(" << GetName() << ") ERROR set constituent " << sarg->GetName() 
-			      << " is not in workspace and importMissing option is disabled" << endl ;
+	coutE(InputArguments) << "RooWorkspace::defineSet(" << GetName() << ") ERROR set constituent \"" << sarg->GetName() 
+			      << "\" is not in workspace and importMissing option is disabled" << endl ;
 	return kTRUE ;
       }
     }
@@ -822,8 +822,8 @@ Bool_t RooWorkspace::defineSet(const char* name, const char* contentList)
   while(token) {
     // If missing, either import or report error
     if (!arg(token)) {
-      coutE(InputArguments) << "RooWorkspace::defineSet(" << GetName() << ") ERROR proposed set constituent " << token
-			    << " is not in workspace" << endl ;
+      coutE(InputArguments) << "RooWorkspace::defineSet(" << GetName() << ") ERROR proposed set constituent \"" << token
+			    << "\" is not in workspace" << endl ;
       return kTRUE ;
     }
     wsargs.add(*arg(token)) ;    
@@ -855,8 +855,8 @@ Bool_t RooWorkspace::extendSet(const char* name, const char* newContents)
   while(token) {
     // If missing, either import or report error
     if (!arg(token)) {
-      coutE(InputArguments) << "RooWorkspace::defineSet(" << GetName() << ") ERROR proposed set constituent " << token
-			    << " is not in workspace" << endl ;
+      coutE(InputArguments) << "RooWorkspace::defineSet(" << GetName() << ") ERROR proposed set constituent \"" << token
+			    << "\" is not in workspace" << endl ;
       return kTRUE ;
     }
     wsargs.add(*arg(token)) ;    
@@ -1221,7 +1221,7 @@ RooArgSet RooWorkspace::argSet(const char* nameList) const
     if (oneArg) {
       ret.add(*oneArg) ;
     } else {
-      coutE(InputArguments) << " RooWorkspace::argSet(" << GetName() << ") no RooAbsArg named " << token << " in workspace" << endl ;
+      coutE(InputArguments) << " RooWorkspace::argSet(" << GetName() << ") no RooAbsArg named \"" << token << "\" in workspace" << endl ;
     }
     token = strtok(0,",") ; 
   }
