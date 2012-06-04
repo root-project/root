4a3135f7a8aacbd20dfe2b9c33235c9c55ab1387
Axel Naumann <Axel.Naumann@cern.ch>
2012-06-04 13:46:50 +0000

    Add essential part of r44507: remove "using namespace std" from Riostream.h
    git-svn-id: http://root.cern.ch/svn/root/trunk@44510 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/Riostream.h
+++ core/base/inc/Riostream.h
@@ -2,7 +2,7 @@
 // Author: Fons Rademakers   23/1/02
 
 /*************************************************************************
- * Copyright (C) 1995-2002, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2012, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -17,38 +17,12 @@
 //                                                                      //
 // Riostream                                                            //
 //                                                                      //
-// This headers is only supposed to be used in implementation files.    //
-// Never in headers, since it has "using namespace std".                //
-// In headers use the companion Riosfwd.h.                              //
+// Backward compatibility header, #includes fstream, iostream, iomanip. //
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
-#ifndef ROOT_RConfig
-#include "RConfig.h"
-#endif
-
-#if defined(R__ANSISTREAM)
-#   include <fstream>
-#   include <iostream>
-#   include <iomanip>
-    using namespace std;
-#else
-#   include <fstream.h>
-#   include <iostream.h>
-#   include <iomanip.h>
-#endif
-
-#if defined(_MSC_VER) && (_MSC_VER <= 1200)
-static std::ostream& operator<<(std::ostream& os, __int64 i)
-{
-   char buf[20];
-   sprintf(buf,"%I64d", i);
-   os << buf;
-   return os;
-}
-
-static std::ostream& operator<<(std::ostream& os, unsigned __int64 i)
-{ return os << (__int64) i; }
-#endif
+#include <fstream>
+#include <iostream>
+#include <iomanip>
 
 #endif
