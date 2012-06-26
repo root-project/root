882d1bcb5f16c4adad63a316c98022c9d3419044
Philippe Canal <pcanal@fnal.gov>
2012-06-26 20:10:54 +0000

    Since TMemFile::ResetAfterMerge keeps the live object around, it does not need to delete the map of caches
    git-svn-id: http://root.cern.ch/svn/root/trunk@44749 27541ba8-7e3a-0410-8455-c3a389f83636

--- io/io/src/TMemFile.cxx
+++ io/io/src/TMemFile.cxx
@@ -317,8 +317,6 @@ void TMemFile::ResetAfterMerge(TFileMergeInfo *info)
    fNProcessIDs  = 0;
    fOffset       = 0;
    fCacheRead    = 0;
-   delete fCacheReadMap;
-   fCacheReadMap = 0;
    fCacheWrite   = 0;
    fReadCalls    = 0;
    if (fFree) {
