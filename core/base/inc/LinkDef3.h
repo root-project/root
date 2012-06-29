c98bad2681ea6771ea2e9dabd5a534ba04228944
Philippe Canal <pcanal@fnal.gov>
2012-06-29 08:05:24 +0000

    gProof is now only in libProof
    git-svn-id: http://root.cern.ch/svn/root/trunk@44813 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/base/inc/LinkDef3.h
+++ core/base/inc/LinkDef3.h
@@ -100,7 +100,6 @@
 #pragma link C++ global kCutBuffer;
 #pragma link C++ global kMaxPixel;
 
-#pragma link C++ global gProof;
 #pragma link C++ global gPerfStats;
 #pragma link C++ global gMonitoringWriter;
 #pragma link C++ global gMonitoringReader;
