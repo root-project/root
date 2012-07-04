5bb2501082fa526ebbb7a04702ba9a80ebdbc871
Philippe Canal <pcanal@fnal.gov>
2012-07-04 16:03:26 +0000

    Fix typo
    git-svn-id: http://root.cern.ch/svn/root/trunk@44890 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/metautils/src/valarrayLinkdef.h
+++ core/metautils/src/valarrayLinkdef.h
@@ -5,5 +5,5 @@ using namespace std;
 
 #pragma create TClass valarray<int>;
 #pragma create TClass valarray<long>;
-#pragma create TClass valarry<float>;
+#pragma create TClass valarray<float>;
 #pragma create TClass valarray<double>;
