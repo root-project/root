362c1722401e1b2f85a0dd791f039531db6b4921
Philippe Canal <pcanal@fnal.gov>
2012-07-06 15:16:53 +0000

    Fix typo
    git-svn-id: http://root.cern.ch/svn/root/trunk@44947 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/lib/Utils/AST.cpp
+++ cint/cling/lib/Utils/AST.cpp
@@ -66,7 +66,7 @@ namespace utils {
           desArgs.push_back(TemplateArgument(GetPartiallyDesugaredType(Ctx,
                                                                        SubTy,
                                                                   TypesToSkip)));
-         else 
+         } else 
             desArgs.push_back(*I);
       }
       
