1b5f7f51178573d19b949f224fb770c7faf8acd4
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2012-07-23 14:05:49 +0000

    Due to some reason Cocoa changes z-stack order for a borderless windows if a user switches between applications using alt-tab (or other ways). This results in a quite disgusting behavior of popup windows. Try to save/restore order of top-level windows in a NSApplicationDelegate methods -applicationWillResignActive:, -applicationDidBecomeActive: (experimental code).
    git-svn-id: http://root.cern.ch/svn/root/trunk@45161 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/cocoa/src/ROOTApplicationDelegate.mm
+++ graf2d/cocoa/src/ROOTApplicationDelegate.mm
@@ -1,12 +1,19 @@
 #import "ROOTApplicationDelegate.h"
+#import "QuartzWindow.h"
+#import "CocoaUtils.h"
+#import "TGWindow.h"
+#import "TGClient.h"
 
-@implementation ROOTApplicationDelegate
+@implementation ROOTApplicationDelegate {
+   NSMutableArray *fWindowStack;
+}
 
 //______________________________________________________________________________
 - (id) init
 {
    if (self = [super init]) {
       [NSApp setDelegate : self];
+      fWindowStack = [[NSMutableArray alloc] init];
    }
    
    return self;
@@ -16,6 +23,7 @@
 - (void) dealloc
 {
    [NSApp setDelegate : nil];//?
+   [fWindowStack release];
    [super dealloc];
 }
 
@@ -25,15 +33,62 @@
 //______________________________________________________________________________
 - (void) applicationWillResignActive : (NSNotification *) aNotification
 {
+   //Popup windows, menus, color-selectors, etc. - they all have
+   //a problem: due to some reason, Cocoa changes the z-stack order
+   //of such a window during switching application (using alt-tab).
+   //This leads to a very annoying effect: you open a menu, alt-tab,
+   //alt-tab back and ... popup or menu is now behind the main window.
+   //I have to save/restore this z-stack order here.
+
    (void) aNotification;
-   //NSLog(@"applicationWillResignActive");
+
+   if (!gClient) {
+      NSLog(@"ROOTApplicationDelegate: -applicationWillResignActive:, gClient is null");
+      return;
+   }
+
+   const ROOT::MacOSX::Util::AutoreleasePool pool;
+
+   NSArray * const orderedWindows = [NSApp orderedWindows];
+
+   [fWindowStack removeAllObjects];
+   for (NSWindow *nsWindow in orderedWindows) {
+      if (![nsWindow isKindOfClass : [QuartzWindow class]])
+         continue;
+
+      QuartzWindow *qWindow = (QuartzWindow *)nsWindow;
+
+      if (TGWindow *rootWindow = gClient->GetWindowById(qWindow.fID)) {
+         if (!rootWindow->InheritsFrom("TGToolTip")) {
+            if (qWindow.fMapState == kIsViewable)
+               [fWindowStack addObject : nsWindow];
+         }
+      }
+   }
 }
 
 //______________________________________________________________________________
 - (void) applicationDidBecomeActive : (NSNotification *) aNotification
 {
+   //Popup windows, menus, color-selectors, etc. - they all have
+   //a problem: due to some reason, Cocoa changes the z-stack order
+   //of such a window during switching application (using alt-tab).
+   //This leads to a very annoying effect: you open a menu, alt-tab,
+   //alt-tab back and ... popup or menu is now behind the main window.
+   //I have to save/restore this z-stack order here.
+
    (void) aNotification;
-   //NSLog(@"applicationDidBecomeActive");
+
+   if (![fWindowStack count])
+      return;
+
+   const ROOT::MacOSX::Util::AutoreleasePool pool;
+
+   NSEnumerator * const reverseEnumerator = [fWindowStack reverseObjectEnumerator];
+   for (QuartzWindow *qw in reverseEnumerator)
+      [qw mapRaised];
+
+   [fWindowStack removeAllObjects];
 }
 
 @end
