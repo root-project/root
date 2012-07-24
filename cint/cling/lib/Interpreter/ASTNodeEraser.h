ad7cc39c8ad2656955a166f32abe65d0044d9f8d
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-07-24 08:48:27 +0000

    Make revert declaration private. We should be working only on transaction level.
    git-svn-id: http://root.cern.ch/svn/root/trunk@45177 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/lib/Interpreter/ASTNodeEraser.h
+++ cint/cling/lib/Interpreter/ASTNodeEraser.h
@@ -17,7 +17,8 @@ namespace cling {
   class DeclReverter;
   class Transaction;
 
-  /// \brief A simple eraser class that removes already created AST Nodes.
+  ///\brief A simple eraser class that removes already created AST Nodes.
+  ///
   class ASTNodeEraser {
   private:
     clang::Sema* m_Sema;
@@ -25,20 +26,6 @@ namespace cling {
   public:
     ASTNodeEraser(clang::Sema* S);
     ~ASTNodeEraser();
-
-    ///\brief Removes given declaration from the AST.
-    ///
-    /// Removing includes reseting various internal stuctures in the compiler to
-    /// their previous states. For example it resets the lookup tables if the 
-    /// declaration has name and can be looked up; Reverts the redeclaration 
-    /// chain if the declaration was redeclarable and so on.
-    /// Note1: that the code generated for the declaration is not removed yet.
-    /// Note2: does not do dependency analysis.
-    ///
-    ///\param[in] D - The declaration to be removed.
-    ///\returns true on success.
-    ///
-    bool RevertDecl(clang::Decl* D);
 
     ///\brief Rolls back given transaction from the AST.
     ///
@@ -53,6 +40,21 @@ namespace cling {
     ///\returns true on success.
     ///
     bool RevertTransaction(const Transaction* T);
+
+  protected:
+    ///\brief Removes given declaration from the AST.
+    ///
+    /// Removing includes reseting various internal stuctures in the compiler to
+    /// their previous states. For example it resets the lookup tables if the 
+    /// declaration has name and can be looked up; Reverts the redeclaration 
+    /// chain if the declaration was redeclarable and so on.
+    /// Note1: that the code generated for the declaration is not removed yet.
+    /// Note2: does not do dependency analysis.
+    ///
+    ///\param[in] D - The declaration to be removed.
+    ///\returns true on success.
+    ///
+    bool RevertDecl(clang::Decl* D);
   };
 } // end namespace cling
 
