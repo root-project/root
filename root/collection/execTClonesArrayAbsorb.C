be149b7ba3ab1a700e4363ecf6f7588cedd4fa93
Philippe Canal <pcanal@fnal.gov>
2012-07-25 02:07:33 +0000

    minimal test of TClonesArray::AbsorbObjects
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2544 7c735d0d-823a-0410-acfd-cd159556d4bf

---
+++ root/collection/execTClonesArrayAbsorb.C
@@ -0,0 +1,7 @@
+{
+tc = new TClonesArray("TNamed",5); for(int i=0; i<20; ++i) tc->ConstructedAt(i);
+tc2 = new TClonesArray("TNamed",5); for(int i=0; i<20; ++i) tc2->ConstructedAt(i);
+tc->AbsorbObjects(tc2);
+delete tc2;
+delete tc;
+}
