c35d989727faa88c2103533e3ed4007919fd0d10
Philippe Canal <pcanal@fnal.gov>
2012-07-31 21:19:22 +0000

    From Brian: For CMS backward compatibility, allow SetCacheRead to disconnect the cache from being the current cache, but keep the file attached to the cache.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@45322 27541ba8-7e3a-0410-8455-c3a389f83636

--- tree/tree/inc/TTreeCache.h
+++ tree/tree/inc/TTreeCache.h
@@ -82,7 +82,7 @@ public:
    virtual Int_t       ReadBufferPrefetch(char *buf, Long64_t pos, Int_t len);
    virtual void        ResetCache();
    virtual void        SetEntryRange(Long64_t emin,   Long64_t emax);
-   virtual void        SetFile(TFile *file);
+   virtual void        SetFile(TFile *file, TFile::ECacheAction action=TFile::kDisconnect);
    static void         SetLearnEntries(Int_t n = 10);
    void                StartLearningPhase();
    virtual void        StopLearningPhase();
