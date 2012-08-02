602b0b74b1b2aebc748c6aac3b84e0435ee33465
Axel Naumann <Axel.Naumann@cern.ch>
2012-08-02 14:31:51 +0000

    Remove XFAIL - this works. Expand test.
    git-svn-id: http://root.cern.ch/svn/root/trunk@45351 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/test/SourceCall/unresolved.C
+++ cint/cling/test/SourceCall/unresolved.C
@@ -2,10 +2,24 @@
 // RUN: cat %s | %cling 2>&1 | FileCheck %s
 // Test handling and recovery from calling an unresolved symbol.
 
-// due to segv on int a = 12: XFAIL: *
 .rawInput
-int foo(); //CHECK: Error: Symbol '
+int foo(); // extern C++
 .rawInput
+extern "C" int functionWithoutDefinition();
+
+int i = 42;
+i = functionWithoutDefinition();
+// CHECK: Error: Symbol 'functionWithoutDefinition' unresolved
+i = foo();
+// CHECK: Error: Symbol '{{.*}}foo{{.*}}' unresolved
+
+extern "C" int printf(const char* fmt, ...);
+printf("got i=%d\n", i); // CHECK: got i=42
+int a = 12// CHECK: (int) 12
+
 foo()
-int a = 12 // SEGVs
-foo()
+// CHECK: Error: Symbol '{{.*}}foo{{.*}}' unresolved
+functionWithoutDefinition();
+// CHECK: Error: Symbol 'functionWithoutDefinition' unresolved
+
+i = 13 //CHECK: (int) 13
