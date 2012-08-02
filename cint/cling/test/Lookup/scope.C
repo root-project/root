e451b7631d05255e11bd9470057f62280825fdf6
Paul Russo <russo@fnal.gov>
2012-08-02 22:02:00 +0000

    Test looking up a class in a nested namespace scope.
    Test class template instantiation.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@45366 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/test/Lookup/scope.C
+++ cint/cling/test/Lookup/scope.C
@@ -1,14 +1,78 @@
-// RUN: cat %s | %cling | FileCheck %s
+// RUN: cat %s | %cling 2>&1 | FileCheck %s
 // Test Interpreter::lookupScope()
 #include "cling/Interpreter/Interpreter.h"
+#include "clang/AST/Decl.h"
 #include "clang/AST/Type.h"
+#include "llvm/Support/Casting.h"
+#include <cstdio>
+using namespace std;
+
+.rawInput 1
+class A {};
+.rawInput 0
+
+const clang::Decl* cl_A = gCling->lookupScope("A");
+printf("cl_A: 0x%lx\n", (unsigned long) cl_A);
+//CHECK: cl_A: 0x{{[1-9a-f][0-9a-f]*$}}
+llvm::cast<clang::NamedDecl>(cl_A)->getQualifiedNameAsString().c_str()
+//CHECK-NEXT: (const char * const) "A"
+
+
+
+.rawInput 1
+namespace N {
+class A {};
+}
+.rawInput 0
+
+const clang::Decl* cl_A_in_N = gCling->lookupScope("N::A");
+printf("cl_A_in_N: 0x%lx\n", (unsigned long) cl_A_in_N);
+//CHECK: cl_A_in_N: 0x{{[1-9a-f][0-9a-f]*$}}
+llvm::cast<clang::NamedDecl>(cl_A_in_N)->getQualifiedNameAsString().c_str()
+//CHECK-NEXT: (const char * const) "N::A"
+
+
+
+.rawInput 1
+namespace N {
+namespace M {
+namespace P {
+class A {};
+}
+}
+}
+.rawInput 0
+
+const clang::Decl* cl_A_in_NMP = gCling->lookupScope("N::M::P::A");
+cl_A_in_NMP
+//CHECK: (const clang::Decl *) 0x{{[1-9a-f][0-9a-f]*$}}
+llvm::cast<clang::NamedDecl>(cl_A_in_NMP)->getQualifiedNameAsString().c_str()
+//CHECK-NEXT: (const char * const) "N::M::P::A"
+
+
+.rawInput 1
+template <class T> class B { T b; };
+.rawInput 0
+
+const clang::Decl* cl_B_int = gCling->lookupScope("B<int>");
+printf("cl_B_int: 0x%lx\n", (unsigned long) cl_B_int);
+//CHECK-NEXT: cl_B_int: 0x{{[1-9a-f][0-9a-f]*$}}
+
+
+
+//
+//  Test optional returned type is as spelled by the user.
+//
 
 typedef int Int_t;
+
 .rawInput 1
 template <typename T> struct W { T member; };
 .rawInput 0
+
 const clang::Type* resType = 0;
 gCling->lookupScope("W<Int_t>", &resType);
 //resType->dump(); 
 clang::QualType(resType,0).getAsString().c_str()
-//CHECK: (const char * const) "W<Int_t>"
+//CHECK-NEXT: (const char * const) "W<Int_t>"
+
