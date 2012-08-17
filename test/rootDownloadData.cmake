bbc751ead41f2f54c1c4a2c30052e40aaa8e8e8e
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2012-08-17 15:28:54 +0000

    Import patch 45658 to make stressProof reading data from local disks in ctest
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@45659 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ test/rootDownloadData.cmake
@@ -0,0 +1,28 @@
+#---------------------------------------------------------------------------------------------------
+# ROOT download test data files
+#   Script arguments: 
+#     DST destination directory
+
+if(NOT DST)
+  set(DST .) 
+endif()
+
+set(rootsite http://root.cern.ch/files)
+set(timeout 100)
+
+if(NOT EXISTS ${DST}/h1)
+  foreach(f dstarmb.root dstarp1a.root dstarp1b.root dstarp2.root)
+    file(DOWNLOAD ${rootsite}/h1/${f} ${DST}/h1/${f} TIMEOUT ${timeout} SHOW_PROGRESS)
+  endforeach()
+else()
+  message(STATUS "Already existing files in ${DST}/h1")  
+endif()
+
+if(NOT EXISTS ${DST}/event)
+  foreach(n 1 2 3 4 5 6 7 8 9 10)
+    file(DOWNLOAD ${rootsite}/data/event_${n}.root ${DST}/event/event_${n}.root TIMEOUT ${timeout} SHOW_PROGRESS)
+  endforeach()
+else()
+  message(STATUS "Already existing files in ${DST}/event")  
+endif()
+
