ebd1033811d58a74e441637b2cf71c06a00b1422
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2012-08-17 18:24:01 +0000

    Make sure that all running counters are off when -noprogress is passed
    git-svn-id: http://root.cern.ch/svn/root/trunk@45660 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressProof.cxx
+++ test/stressProof.cxx
@@ -1348,9 +1348,11 @@ Int_t PT_H1AssertFiles(const char *h1src)
             printf("\n >>> Test failure: file %s not found in archive %s\n", src.Data(), h1src);
             return -1;
          }
-         gSystem->RedirectOutput(0, 0, &gRH);
-         printf("%d\b", i);
-         gSystem->RedirectOutput(glogfile, "a", &gRH);
+         if (guseprogress) {
+            gSystem->RedirectOutput(0, 0, &gRH);
+            printf("%d\b", i);
+            gSystem->RedirectOutput(glogfile, "a", &gRH);
+         }
       }
       gh1sep = '#';
    } else {
@@ -1363,9 +1365,11 @@ Int_t PT_H1AssertFiles(const char *h1src)
             printf("\n >>> Test failure: file %s does not exist\n", src.Data());
             return -1;
          }
-         gSystem->RedirectOutput(0, 0, &gRH);
-         printf("%d\b", i);
-         gSystem->RedirectOutput(glogfile, "a", &gRH);
+         if (guseprogress) {
+            gSystem->RedirectOutput(0, 0, &gRH);
+            printf("%d\b", i);
+            gSystem->RedirectOutput(glogfile, "a", &gRH);
+         }
       }
    }
    gh1src = h1src;
@@ -1449,9 +1453,11 @@ Int_t PT_EventAssertFiles(const char *eventsrc, Int_t nf = 10)
             printf("\n >>> Test failure: problems retrieving %s\n", src.Data());
             return -1;
          }
-         gSystem->RedirectOutput(0, 0, &gRH);
-         printf("%d\b", i);
-         gSystem->RedirectOutput(glogfile, "a", &gRH);
+         if (guseprogress) {
+            gSystem->RedirectOutput(0, 0, &gRH);
+            printf("%d\b", i);
+            gSystem->RedirectOutput(glogfile, "a", &gRH);
+         }
       }
       geventlocal = kTRUE;
       // Done
@@ -1467,9 +1473,11 @@ Int_t PT_EventAssertFiles(const char *eventsrc, Int_t nf = 10)
          printf("\n >>> Test failure: file %s does not exist\n", src.Data());
          return -1;
       }
-      gSystem->RedirectOutput(0, 0, &gRH);
-      printf("%d\b", i);
-      gSystem->RedirectOutput(glogfile, "a", &gRH);
+      if (guseprogress) {
+         gSystem->RedirectOutput(0, 0, &gRH);
+         printf("%d\b", i);
+         gSystem->RedirectOutput(glogfile, "a", &gRH);
+      }
    }
    geventsrc = eventsrc;
 
