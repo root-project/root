db14c9061831c210c68923caa594eef7a27a034b
Fons Rademakers <Fons.Rademakers@cern.ch>
2012-08-29 14:55:37 +0000

    Script to update LLVM/clang vendor branch and to merge changes into root/interpreter/llvm. The merge step is not yet automatic as we need to get the vendor branch revision numbers for LLVM and clang. See ## lines.
    git-svn-id: http://root.cern.ch/svn/root/trunk@45738 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ build/unix/updatellvm.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+if [ $# -ne 1 ]; then
+   echo "Must specify LLVM/clang revision number"
+fi
+
+GOLDEN=$1
+
+cd
+rm -rf llvm-export
+svn export -r $GOLDEN http://llvm.org/svn/llvm-project/llvm/trunk llvm-export
+
+rm -rf clang-export
+svn export -r $GOLDEN http://llvm.org/svn/llvm-project/cfe/trunk clang-export
+
+# will automatically commit into vendor branch
+$ROOTSYS/build/unix/svn_load_dirs.pl https://root.cern.ch/svn/root/vendors llvm llvm-export -m "Update LLVM to r$GOLDEN."
+
+# will automatically commit into vendor branch
+$ROOTSYS/build/unix/svn_load_dirs.pl https://root.cern.ch/svn/root/vendors clang clang-export -m "Update Clang to r$GOLDEN."
+
+# Both of the 'svn_load_dirs.pl' steps should have given you a final revision
+# number for their checkins. We need to merge those changes into 'trunk/'.
+##cd $ROOTSYS/interpreter/llvm/src
+##svn merge -c $LLVM_REV https://root.cern.ch/svn/root/vendors/llvm .
+
+##cd $ROOTSYS/interpreter/llvm/src/tools/clang
+##svn merge -c $CLANG_REV https://root.cern.ch/svn/root/vendors/clang .
+
+# now apply the patches from 'interpreter/cling/patches'
+# and run 'make all-llvm' in '$ROOTSYS' and next 'make test' from
+# 'interpreter/llvm/obj'. 
