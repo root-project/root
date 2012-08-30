1c295be0e38f1eb518e34bdf97aa74610fccd474
Fons Rademakers <Fons.Rademakers@cern.ch>
2012-08-30 15:33:51 +0000

    typo, must be --without-x.
    git-svn-id: http://root.cern.ch/svn/root/trunk@45763 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/asimage/Module.mk
+++ graf2d/asimage/Module.mk
@@ -171,7 +171,7 @@ else
 			PNGINCDIR="--with-png-includes=$(ASPNGINCDIR)"; \
 		fi; \
 		if [ "$(BUILDCOCOA)" = "yes" ]; then \
-			JPEGINCDIR="--disable-x --with-builtin-jpeg"; \
+			JPEGINCDIR="--without-x --with-builtin-jpeg"; \
 			PNGINCDIR="--with-builtin-png"; \
 		fi; \
 		if [ "$(ASTIFFINCDIR)" = "--with-tiff=no" ]; then \
