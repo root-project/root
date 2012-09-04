bca20c95f9487f4677ad7f0f5e3ab1f48e4eb655
Axel Naumann <Axel.Naumann@cern.ch>
2012-09-04 07:50:48 +0000

    Force _ZN5cling7runtime8internal15DynamicExprInfo7getExprEv to be unresolved so the export is picked up.
    git-svn-id: http://root.cern.ch/svn/root/trunk@45812 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cling/tools/libcling/Dummy.cpp
+++ cint/cling/tools/libcling/Dummy.cpp
@@ -2,6 +2,7 @@
 #include "cling/Interpreter/Interpreter.h"
 #include "cling/Interpreter/Callable.h"
 #include "cling/Interpreter/CValuePrinter.h"
+#include "cling/Interpreter/DynamicExprInfo.h"
 #include "cling/Interpreter/LookupHelper.h"
 #include "cling/Interpreter/ValuePrinter.h"
 #include "cling/Interpreter/ValuePrinterInfo.h"
@@ -27,5 +28,7 @@ void libcling__symbol_requester(const clang::FunctionDecl& Decl,
    h.findScope("");
    h.findFunctionProto(0, "", "");
    h.findFunctionArgs(0, "", "");
+   cling::runtime::internal::DynamicExprInfo DEI(0,0,false);
+   DEI.getExpr();
 }
 }
