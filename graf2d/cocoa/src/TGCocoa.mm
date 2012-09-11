6f6a6129bf40ee72345e1cf5ddfbff3bf457772f
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2012-09-11 14:32:34 +0000

    The last commit from dev branch had broken GL, fix it back.
    git-svn-id: http://root.cern.ch/svn/root/trunk@45930 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/cocoa/src/TGCocoa.mm
+++ graf2d/cocoa/src/TGCocoa.mm
@@ -32,7 +32,7 @@
 #include "CocoaUtils.h"
 #include "X11Events.h"
 #include "X11Buffer.h"
-//#include "TGLFormat.h"
+#include "TGLFormat.h"
 #include "TGClient.h"
 #include "TGWindow.h"
 #include "TSystem.h"
@@ -2920,7 +2920,7 @@ void TGCocoa::QueryPointer(Window_t winID, Window_t &rootWinID, Window_t &childW
 //OpenGL management.
 
 //______________________________________________________________________________
-Window_t TGCocoa::CreateOpenGLWindow(Window_t parentID, UInt_t width, UInt_t height, const std::vector<std::pair<UInt_t, Int_t> > &/*formatComponents*/)
+Window_t TGCocoa::CreateOpenGLWindow(Window_t parentID, UInt_t width, UInt_t height, const std::vector<std::pair<UInt_t, Int_t> > &formatComponents)
 {
    //ROOT never creates GL widgets with 'root' as a parent (so not top-level gl-windows).
    //If this change, assert must be deleted.
@@ -2932,7 +2932,7 @@ Window_t TGCocoa::CreateOpenGLWindow(Window_t parentID, UInt_t width, UInt_t hei
    
 
    std::vector<NSOpenGLPixelFormatAttribute> attribs;
-  /* for (size_type i = 0, e = formatComponents.size(); i < e; ++i) {
+   for (size_type i = 0, e = formatComponents.size(); i < e; ++i) {
       const component_type &comp = formatComponents[i];
       
       if (comp.first == TGLFormat::kDoubleBuffer) {
@@ -2952,7 +2952,7 @@ Window_t TGCocoa::CreateOpenGLWindow(Window_t parentID, UInt_t width, UInt_t hei
          attribs.push_back(NSOpenGLPFASamples);
          attribs.push_back(comp.second ? comp.second : 4);
       }
-   }*/
+   }
    
    attribs.push_back(NSOpenGLPFAAccelerated);//??? I think, TGLWidget always wants this.
    attribs.push_back(0);
