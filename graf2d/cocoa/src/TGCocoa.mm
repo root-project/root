3df29210e6a5421b8f543397b7a20848a0342f3d
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2012-09-20 14:26:07 +0000

    Remove ^p symbols from the  text line to draw (they are introduced by TGText::ReTab).
    git-svn-id: http://root.cern.ch/svn/root/trunk@46087 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/cocoa/src/TGCocoa.mm
+++ graf2d/cocoa/src/TGCocoa.mm
@@ -2027,6 +2027,9 @@ void TGCocoa::DrawStringAux(Drawable_t wid, const GCValues_t &gcVals, Int_t x, I
 
    //Do a simple text layout using CGGlyphs.
    std::vector<UniChar> unichars(text, text + len);
+   //TGText::ReTab inserts this non-graphical symbols (which leads to CoreText error).
+   unichars.erase(std::remove(unichars.begin(), unichars.end(), 16), unichars.end());
+   
    Quartz::DrawTextLineNoKerning(ctx, (CTFontRef)gcVals.fFont, unichars, x,  X11::LocalYROOTToCocoa(drawable, y));
 }
 
