b36da5d493cdc003ab2b93a0135b30c84e93e77d
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2012-09-21 13:31:03 +0000

    from Gabriel: print a final message when some tests are failing
    git-svn-id: http://root.cern.ch/svn/root/trunk@46107 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressRooStats.cxx
+++ test/stressRooStats.cxx
@@ -51,19 +51,16 @@ using namespace RooFit ;
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*//
 
 
-//------------------------------------------------------------------------
-void StatusPrint(const Int_t id, const TString &title, const Int_t status, const Int_t lineWidth)
-{
-   // Print test program number and its title
-   TString header = TString::Format("Test %d : %s ", id, title.Data());
-   cout << left << setw(lineWidth) << setfill('.') << header << " " << (status > 0 ? "OK" : (status < 0 ? "SKIPPED" : "FAILED")) << endl;
-}
-
 //______________________________________________________________________________
 Int_t stressRooStats(const char* refFile, Bool_t writeRef, Int_t verbose, Bool_t allTests, Bool_t oneTest, Int_t testNumber, Bool_t dryRun, Bool_t doDump, Bool_t doTreeStore)
 {
    // width of lines when printing test results
    const Int_t lineWidth = 120;
+
+   // global test suite status
+   // 0     = all tests passed
+   // n < 0 = n tests failed
+   Int_t testSuiteStatus = 0;
 
    // Save memory directory location
    RooUnitTest::setMemDir(gDirectory) ;
@@ -215,7 +212,14 @@ Int_t stressRooStats(const char* refFile, Bool_t writeRef, Int_t verbose, Bool_t
                if (doDump) {
                   (*iter)->setDebug(kTRUE);
                }
-               StatusPrint(i, (*iter)->GetName(), (*iter)->isTestAvailable() ? (*iter)->runTest() : -1, lineWidth);
+
+               Bool_t testPassed = (*iter)->runTest();
+               
+               // See convention for testSuiteStatus near the beginning of function
+               if(testPassed == kFALSE) testSuiteStatus--;
+      
+               cout << left << setw(lineWidth) << setfill('.') << TString::Format("Test %d : %s ", i, (*iter)->GetName()) 
+                    << " " << (testPassed ? "OK" : "FAILED" ) << endl;
             }
             delete *iter;
          }
@@ -229,7 +233,18 @@ Int_t stressRooStats(const char* refFile, Bool_t writeRef, Int_t verbose, Bool_t
    gBenchmark->Stop("stressRooStats");
 
 
-   //Print table with results
+   // Print global test suite status
+   // See convention for testSuiteStatus near the beginning of function
+   cout << setw(lineWidth) << setfill('*') << "" << endl;
+   cout << left << setw(lineWidth) << setfill('.') << "SUMMARY : stressRooStats test suite " << " " 
+        << (testSuiteStatus < 0 ? "FAILED" : "OK") << endl;
+   if (testSuiteStatus < 0) {
+      cout << "         " << -testSuiteStatus << (testSuiteStatus < -1 ? " tests " : " test ") 
+           << "failed" << endl;
+   }
+
+
+   // Print table with results
    Bool_t UNIX = strcmp(gSystem->GetName(), "Unix") == 0;
    cout << setw(lineWidth) << setfill('*') << "" << endl;
    if (UNIX) {
@@ -275,7 +290,7 @@ Int_t stressRooStats(const char* refFile, Bool_t writeRef, Int_t verbose, Bool_t
    delete gBenchmark ;
    gBenchmark = 0 ;
 
-   return 0;
+   return testSuiteStatus;
 }
 
 //_____________________________batch only_____________________
@@ -367,8 +382,8 @@ int main(int argc, const char *argv[])
    RooMath::cacheCERF(kFALSE) ;
 
    gBenchmark = new TBenchmark();
-   stressRooStats(refFileName.c_str(), doWrite, verbose, allTests, oneTest, testNumber, dryRun, doDump, doTreeStore);
-   return 0;
+
+   return stressRooStats(refFileName.c_str(), doWrite, verbose, allTests, oneTest, testNumber, dryRun, doDump, doTreeStore);
 }
 
 #endif
