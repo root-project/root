e4129a72c1ed724aed550bdcd702c3fe77304765
Philippe Canal <pcanal@fnal.gov>
2012-10-05 18:23:03 +0000

    Extend the test to test local caching of the file (However it is disabled for now)
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2560 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/prefetching/runPrefetchReading.C
+++ root/io/prefetching/runPrefetchReading.C
@@ -11,11 +11,12 @@
 #include "TFileCacheRead.h"
 #include "TTreeCache.h"
 #include "TError.h"
+#include "TSystemDirectory.h"
 
-Int_t runPrefetchReading()
+Int_t runPrefetchReading(bool caching = false)
 {
-  //const char *options = 0;
-   Int_t freq = 1000;
+   //const char *options = 0;
+   Int_t freq = 1000; 
    Int_t cachesize = -1;
    Float_t percententries = 1.00;
    Float_t percentbranches = 1.00;
@@ -23,6 +24,12 @@ Int_t runPrefetchReading()
    
    //set the reading mode to async prefetching
    gEnv->SetValue("TFile.AsyncPrefetching", 1);
+
+   //enable the local caching of blocks
+   TString cachedir="file:/tmp/xcache/";
+   // or using xrootd on port 2000
+   // TString cachedir="root://localhost:2000//tmp/xrdcache1/";
+   if (caching) gEnv->SetValue("Cache.Directory", cachedir.Data());
    
    // open the local if any
    TString filename("atlasFlushed.root");
@@ -30,6 +37,7 @@ Int_t runPrefetchReading()
       // otherwise open the http file
       filename.Prepend("http://root.cern.ch/files/");
       //filename.Prepend("root://cache01.usatlas.bnl.gov//data/test1/");
+      //filename.Prepend( "root://pcitdss1401//tmp/" );
       //filename.Prepend("http://www-root.fnal.gov/files/");
       //filename.Prepend("http://oink.fnal.gov/distro/roottest/");
    }
@@ -81,14 +89,13 @@ Int_t runPrefetchReading()
       }
       T->StopCacheLearningPhase();
    }
-  
-//   T->GetBranch("m_vec")->GetEntry(0);
-//   T->GetBranch("m_vec")->GetEntry(1);
-//   T->GetBranch("m_vec")->GetEntry(2);
-//   return 0;
+
+   //...........................................................................
+   // First read, with saving the info in cache
+   //...........................................................................
    fprintf(stderr,"Setup done. Starting to read the entries\n");
    TRandom r;
-   for (Long64_t i=efirst;i<elast;i++) {
+   for (Long64_t i = efirst; i < elast; i++) {
      //if (i%100 == 0 || i>2000) fprintf(stderr,"i.debug = %lld\n",i);
      // if (i==2000) gDebug = 7;
      if (i % freq == 0){
@@ -108,7 +115,87 @@ Int_t runPrefetchReading()
        }
      }
    }
+
+   fprintf(stderr,"Done reading for the first pass, now closing the file\n");
+   file->Close();
+   delete file;
+
+   //...........................................................................
+   // Second read, actually reading the data from cache
+   //...........................................................................
+   fprintf(stderr,"Opening the file for the 2nd pass\n");
+   file = TFile::Open( filename, "TIMEOUT=30" );
+   if (!file || file->IsZombie()) return 1;
  
+   fprintf(stderr,"The file has been opened, setting up the TTree\n");
+   // Try the known names :)
+   for (unsigned int i = 0; i < sizeof(names)/sizeof(names[0]); ++i) {
+      file->GetObject(names[i], T);
+      if (T) break;
+   }
+   if (T==0) {
+     Error("runPrefetchReading","Could not find a tree which the conventional names in %s.",filename.Data());
+     return 2;
+   }
+
+   TFile::SetReadaheadSize(0);  // (256*1024);
+   nentries = T->GetEntries();
+
+   efirst = 0;
+   elast  = efirst+nentries;
+
+   if (cachesize == -2) {
+      gEnv->SetValue("TFile.AsyncReading", 0);
+      cachesize = -1;
+   }
+   T->SetCacheSize(cachesize);
+
+   if (cachesize != 0) {
+      T->SetCacheEntryRange(efirst,elast);
+      if (percentbranches < 1.00) {
+         int nb = T->GetListOfBranches()->GetEntries();
+         int incr = nb * percentbranches;
+         for(int b=0;b < nb; b += incr) T->AddBranchToCache(((TBranch*)T->GetListOfBranches()->At(b)),kTRUE);
+      } else {
+         T->AddBranchToCache("*");
+      }
+      T->StopCacheLearningPhase();
+   }
+      
+   fprintf(stderr,"Setup done, starting the 2nd reading.\n");
+   for (Long64_t i = efirst; i < elast; i++) {
+     if (i % freq == 0){
+       // for (Long64_t i=elast-1;i>=efirst;i--) {
+       if (i%freq == 0 || i==(elast-1)) fprintf(stderr,"i = %lld\n",i);
+       if (r.Rndm() > percententries) continue;
+       T->LoadTree(i);
+       if (percentbranches < 1.00) {
+         int nb = T->GetListOfBranches()->GetEntries();
+         int incr = nb * percentbranches;
+         
+         for(int b=0;b<nb; b += incr) {
+           ((TBranch*)T->GetListOfBranches()->At(b))->GetEntry(i);
+         }
+         
+         int count = 0;
+         int maxcount = 1000 + 100 ;
+         for(int x = 0; x < maxcount; ++x ) {
+           /* waste cpu */
+           count = sin(cos((double)count));
+         }         
+       } else {
+         T->GetEntry(i);
+       }
+     }
+   }
+   fprintf(stderr, "Done with the 2nd reading\n");
+
    fprintf(stderr, "fPrefetchedBlocks = %lli\n", file->GetCacheRead()->GetPrefetchedBlocks());
+
+   fprintf(stderr, "Delete tmp directory: /tmp/xcache\n" );
+   if (caching) system( "rm -r /tmp/xcache" );
+   
+   file->Close();
+   delete file;
    return 0;
 }
