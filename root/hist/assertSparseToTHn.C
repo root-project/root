02cd0479232d30959867cd3fb4f728dfdfe6eb24
Axel Naumann <Axel.Naumann@cern.ch>
2012-10-10 15:40:29 +0000

    Make test a bit more trigger-happy: compare num entries of hists.
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2569 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/hist/assertSparseToTHn.C
+++ root/hist/assertSparseToTHn.C
@@ -1,8 +1,9 @@
-void assertSparseToTHn()
+int assertSparseToTHn()
 {
   TFile *_file0 = TFile::Open("./effectiveCorrection.root");
   THnSparseF* sp = static_cast<THnSparseF*>(_file0->Get("pbar_Corr0"));
-  THn *hh = NULL;
-  hh = THn::CreateHn("foo","bar",sp);
+  sp->Dump();
+  THn *hh = THn::CreateHn("foo","bar",sp);
   hh->Dump();
+  return sp->GetEntries() == hh->GetEntries() ? 0 : 1;
 }  
