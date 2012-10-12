e088a78c83c943f3568e55e1899cba62970581c1
Philippe Canal <pcanal@fnal.gov>
2012-10-12 17:21:23 +0000

    Implement gInterpreter only solution when cling is enabled (but slightly slower for cint)
    git-svn-id: http://root.cern.ch/svn/root/trunk@46514 27541ba8-7e3a-0410-8455-c3a389f83636

--- misc/table/src/TTable.cxx
+++ misc/table/src/TTable.cxx
@@ -130,6 +130,8 @@
 # include <float.h>
 #endif
 
+#include "RConfigure.h"
+
 //#if ROOT_VERSION_CODE >= ROOT_VERSION(3,03,5)
 #include "Riosfwd.h"
 #include "Riostream.h"
@@ -146,7 +148,9 @@
 #include "TBrowser.h"
 #include "TString.h"
 #include "TInterpreter.h"
+#ifndef R__HAS_CLING
 #include "Api.h"
+#endif
 #include "TDataSetIter.h"
 #include "TTable.h"
 #include "TTableDescriptor.h"
@@ -790,7 +794,9 @@ Bool_t TTable::EntryLoop(const Char_t *exprFileName,Int_t &action, TObject *obj
    //   where  i$ - meta variable to set current row index
    //          n$ - meta variable to set the total table size
    const Char_t *funcName = "SelectionQWERTY";
+#ifndef R__HAS_CLING 
 #define BYTECODE
+#endif
 #ifdef BYTECODE
    const Char_t *argtypes = "Float_t *,float **, int&, int& ";
    Long_t offset;
@@ -821,6 +827,12 @@ Bool_t TTable::EntryLoop(const Char_t *exprFileName,Int_t &action, TObject *obj
    callfunc.SetArg((Long_t)(addressArray));  // give 'void    *addressArray[]' as 2nd argument
    callfunc.SetArg((Long_t)(&i));            // give 'int& i$'                 as 3nd argument
    callfunc.SetArg((Long_t)(&nRows));        // give 'int& n$= nRows           as 4th argument
+#elif defined(R__HAS_CLING)
+   const Char_t *argtypes = "Float_t *,float **, int&, int& ";
+   Long_t offset;
+   ClassInfo_t *globals = gInterpreter->ClassInfo_Factory();
+   CallFunc_t *callfunc = gInterpreter->CallFunc_Factory();
+   gInterpreter->CallFunc_SetFunc(callfunc,globals,funcName,argtypes,&offset);
 #else
    char buf[200];
    sprintf(buf,"%s((Float_t*)(%ld),(void**)(%ld),*(int*)(%ld),*(int*)(%ld))"
@@ -832,6 +844,8 @@ Bool_t TTable::EntryLoop(const Char_t *exprFileName,Int_t &action, TObject *obj
 
 #ifdef BYTECODE
 #  define CALLMETHOD callfunc.Exec(0);
+#elif defined(R__HAS_CLING)
+#  define CALLMETHOD gInterpreter->CallFunc_Exec(callfunc,0);
 #else
 #  define CALLMETHOD gInterpreter->ProcessLineFast(buf);
 #endif
@@ -1022,6 +1036,10 @@ Bool_t TTable::EntryLoop(const Char_t *exprFileName,Int_t &action, TObject *obj
             break;
       };
    }
+#if defined(R__HAS_CLING)
+   gInterpreter->CallFunc_Delete(callfunc);
+   gInterpreter->ClassInfo_Delete(globals);
+#endif
    gInterpreter->UnloadFile((Char_t *)exprFileName);
    delete [] addressArray;
    return kTRUE;
