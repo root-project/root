b8a884e8fd4f9f0c5e9ea7db47a2503c1f05c776
Fons Rademakers <Fons.Rademakers@cern.ch>
2012-10-30 14:40:42 +0000

    small fixes to correct some dependencies on the new libmathtext.a.
    git-svn-id: http://root.cern.ch/svn/root/trunk@46900 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/mathtext/Module.mk
+++ graf2d/mathtext/Module.mk
@@ -1,7 +1,7 @@
 # Module.mk for mathtex module
-# Copyright (c) 2011 Rene Brun and Fons Rademakers
+# Copyright (c) 2012 Rene Brun and Fons Rademakers
 #
-# Author: 
+# Author: Olivier Couet, 30/10/2012
 
 MODNAME      := mathtext
 MODDIR       := $(ROOT_SRCDIR)/graf2d/$(MODNAME)
@@ -17,15 +17,17 @@ MATHTEXTH    := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 MATHTEXTS    := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 MATHTEXTO    := $(call stripsrc,$(MATHTEXTS:.cxx=.o))
 
-MATHTEXTDEP  := $(MATHTEXTO:.o=.d) $(MATHTEXTDO:.o=.d)
+MATHTEXTDEP  := $(MATHTEXTO:.o=.d)
 
 ifeq ($(PLATFORM),win32)
 MATHTEXTLIB  := $(LPATH)\libmathtext.lib
-ARCHIVE      := link.exe -lib /out:
+MATHTEXTAR   := link.exe -lib /out:
 else
 MATHTEXTLIB  := $(LPATH)/libmathtext.a
-ARCHIVE      := $(AR) cru 
+MATHTEXTAR   := $(AR) cru 
 endif
+
+MATHTEXTLIBDEP := $(MATHTEXTLIB)
 
 # used in the main Makefile
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(MATHTEXTH))
@@ -34,18 +36,14 @@ ALLLIBS     += $(MATHTEXTLIB)
 # include all dependency files
 INCLUDEFILES += $(MATHTEXTDEP)
 
-MATHTEXTINC  := $(MATHTEXTDIRI:%=-I%)
-MATHTEXTDEP  := $(MATHTEXTLIB)
-MATHTEXTLDFLAGS :=
-
 ##### local rules #####
 .PHONY:         all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME)
 
 include/%.h:    $(MATHTEXTDIRI)/%.h
 		cp $< $@
 
-$(MATHTEXTLIB): $(MATHTEXTO) $(ORDER_)
-		$(ARCHIVE)$@ $(MATHTEXTO)
+$(MATHTEXTLIB): $(MATHTEXTO)
+		$(MATHTEXTAR)$@ $(MATHTEXTO)
 
 all-$(MODNAME): $(MATHTEXTLIB)
 
@@ -55,8 +53,7 @@ clean-$(MODNAME):
 clean::         clean-$(MODNAME)
 
 distclean-$(MODNAME): clean-$(MODNAME)
-		@rm -f $(MATHTEXTDEP) $(MATHTEXTDS) $(MATHTEXTDH) \
-	$(MATHTEXTLIB)
+		@rm -f $(MATHTEXTDEP) $(MATHTEXTLIB)
 
 distclean::     distclean-$(MODNAME)
 
