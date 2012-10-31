03fce4fcbeeb70a564e6bebd8dce1c560da69166
Fons Rademakers <Fons.Rademakers@cern.ch>
2012-10-31 21:42:00 +0000

    get rid of the large LLVM and clang test directories which we don't need.
    git-svn-id: http://root.cern.ch/svn/root/trunk@46946 27541ba8-7e3a-0410-8455-c3a389f83636

--- interpreter/llvm/src/tools/clang/test/Analysis/complex-init-list.cpp
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -analyze -analyzer-checker=core -analyzer-output=text -verify %s
-
-// Do not crash on initialization to complex numbers.
-void init_complex() {
-  _Complex float valid1 = { 0.0f, 0.0f };
-}
