9d464c1e913dcf870518ce286f2b0bb21ef36d03
Philippe Canal <pcanal@fnal.gov>
2012-11-20 05:29:41 +0000

    Avoid compiler warning
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2585 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/array/array1.C
+++ cint/array/array1.C
@@ -1,10 +1,10 @@
 {
 const int n1 = 1;
 TString a1[n1];   
-fprintf(stdout,"a1 is 0x%x 0x%x\n",a1,&(a1[0]));
+fprintf(stdout,"a1 is 0x%lx 0x%lx\n",a1,&(a1[0]));
 const int n2 = 2;
 TString a2[n2];   
-fprintf(stdout,"a2 is 0x%x 0x%x\n",a2,&(a2[0]));
+fprintf(stdout,"a2 is 0x%lx 0x%lx\n",a2,&(a2[0]));
 gROOT->ProcessLine(".g a1");
 gROOT->ProcessLine(".g a2");
 } 
