161b1380de8f33c83a8594eccf0e75d01c9ce9d5
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2012-11-29 14:56:45 +0000

    merge from trunk new impelmentation of analytical integral of Poisson mean
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@47710 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/roofit/src/RooPoisson.cxx
+++ roofit/roofit/src/RooPoisson.cxx
@@ -79,6 +79,7 @@ Double_t RooPoisson::evaluate() const
 Int_t RooPoisson::getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars, const char* /*rangeName*/) const 
 {
   if (matchArgs(allVars,analVars,x)) return 1 ;
+  if (matchArgs(allVars, analVars, mean)) return 2;
   return 0 ;
 }
 
@@ -87,57 +88,73 @@ Int_t RooPoisson::getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars,
 //_____________________________________________________________________________
 Double_t RooPoisson::analyticalIntegral(Int_t code, const char* rangeName) const 
 {
-  assert(code==1) ;
+  assert(code == 1 || code == 2) ;
 
   if(_protectNegative && mean<0) 
     return exp(-2*mean); // make it fall quickly
 
-  // Implement integral over x as summation. Add special handling in case
-  // range boundaries are not on integer values of x
-  Double_t xmin = x.min(rangeName) ;
-  Double_t xmax = x.max(rangeName) ;
+  if (code == 1) {
+    // Implement integral over x as summation. Add special handling in case
+    // range boundaries are not on integer values of x
+    Double_t xmin = x.min(rangeName) ;
+    Double_t xmax = x.max(rangeName) ;
 
-  // Protect against negative lower boundaries
-  if (xmin<0) xmin=0 ;
+    // Protect against negative lower boundaries
+    if (xmin<0) xmin=0 ;
 
-  Int_t ixmin = Int_t (xmin) ;
-  Int_t ixmax = Int_t (xmax)+1 ;
+    Int_t ixmin = Int_t (xmin) ;
+    Int_t ixmax = Int_t (xmax)+1 ;
 
-  Double_t fracLoBin = 1-(xmin-ixmin) ;
-  Double_t fracHiBin = 1-(ixmax-xmax) ;
+    Double_t fracLoBin = 1-(xmin-ixmin) ;
+    Double_t fracHiBin = 1-(ixmax-xmax) ;
 
-  if (!x.hasMax()) {
-    if (xmin<1e-6) {
-      return 1 ;
-    } else {
+    if (!x.hasMax()) {
+      if (xmin<1e-6) {
+        return 1 ;
+      } else {
       
       // Return 1 minus integral from 0 to x.min() 
 
-      if(ixmin == 0){ // first bin
-	return TMath::Poisson(0, mean)*(xmin-0);
-      }      
-      Double_t sum(0) ;
-      sum += TMath::Poisson(0,mean)*fracLoBin ;
-      sum+= ROOT::Math::poisson_cdf(ixmin-2, mean) - ROOT::Math::poisson_cdf(0,mean) ;
-      sum += TMath::Poisson(ixmin-1,mean)*fracHiBin ;
-      return 1-sum ;
+        if(ixmin == 0){ // first bin
+	       return TMath::Poisson(0, mean)*(xmin-0);
+        }      
+        Double_t sum(0) ;
+        sum += TMath::Poisson(0,mean)*fracLoBin ;
+        sum+= ROOT::Math::poisson_cdf(ixmin-2, mean) - ROOT::Math::poisson_cdf(0,mean) ;
+        sum += TMath::Poisson(ixmin-1,mean)*fracHiBin ;
+        return 1-sum ;
+      }
     }
-  }
   
-  if(ixmin == ixmax-1){ // first bin
-    return TMath::Poisson(ixmin, mean)*(xmax-xmin);
-  }  
+    if(ixmin == ixmax-1){ // first bin
+      return TMath::Poisson(ixmin, mean)*(xmax-xmin);
+    }  
 
-  Double_t sum(0) ;
-  sum += TMath::Poisson(ixmin,mean)*fracLoBin ;
-  if (RooNumber::isInfinite(xmax)){
-    sum+= 1.-ROOT::Math::poisson_cdf(ixmin,mean) ;
-  }  else {
-    sum+= ROOT::Math::poisson_cdf(ixmax-2, mean) - ROOT::Math::poisson_cdf(ixmin,mean) ;
-    sum += TMath::Poisson(ixmax-1,mean)*fracHiBin ;
-  }
+    Double_t sum(0) ;
+    sum += TMath::Poisson(ixmin,mean)*fracLoBin ;
+    if (RooNumber::isInfinite(xmax)){
+      sum+= 1.-ROOT::Math::poisson_cdf(ixmin,mean) ;
+    }  else {
+      sum+= ROOT::Math::poisson_cdf(ixmax-2, mean) - ROOT::Math::poisson_cdf(ixmin,mean) ;
+      sum += TMath::Poisson(ixmax-1,mean)*fracHiBin ;
+    }
   
-  return sum ;
+    return sum ;
+
+  } else if(code == 2) {
+   
+    // the integral with respect to the mean is the integral of a gamma distribution 
+    Double_t mean_min = mean.min(rangeName);
+    Double_t mean_max = mean.max(rangeName);
+     
+    Double_t ix;
+    if(_noRounding) ix = x + 1;
+    else ix = Int_t(TMath::Floor(x)) + 1.0; // negative ix does not need protection (gamma returns 0.0)
+    
+    return ROOT::Math::gamma_cdf(mean_max, ix, 1.0) - ROOT::Math::gamma_cdf(mean_min, ix, 1.0);
+  }
+
+  return 0;
 
 }
 
