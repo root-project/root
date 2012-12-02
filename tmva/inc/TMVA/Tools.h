425625e9b2bfa8bf74144fdea7ee7f0b4c5d2d4b
Eckhard von Toerne <evt@physik.uni-bonn.de>
2012-12-02 15:07:49 +0000

    fix problem with dictionary compilation. Write namespace TMVA:: explicitly in Tools::CalcCovarianceMatrices
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@47774 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/inc/TMVA/Tools.h
+++ tmva/inc/TMVA/Tools.h
@@ -138,8 +138,8 @@ namespace TMVA {
 
       // returns the covariance matrix of of the different classes (and the sum) 
       // given the event sample
-      std::vector<TMatrixDSym*>* CalcCovarianceMatrices( const std::vector<Event*>& events, Int_t maxCls, VariableTransformBase* transformBase=0 );
-      std::vector<TMatrixDSym*>* CalcCovarianceMatrices( const std::vector<const Event*>& events, Int_t maxCls, VariableTransformBase* transformBase=0 );
+      std::vector<TMatrixDSym*>* CalcCovarianceMatrices( const std::vector<TMVA::Event*>& events, Int_t maxCls, VariableTransformBase* transformBase=0 );
+      std::vector<TMatrixDSym*>* CalcCovarianceMatrices( const std::vector<const TMVA::Event*>& events, Int_t maxCls, VariableTransformBase* transformBase=0 );
 
 
       // turns covariance into correlation matrix
