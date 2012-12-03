e82e8fae4be276a422009112b67e8bf072dd4a59
Philippe Canal <pcanal@fnal.gov>
2012-12-03 02:30:33 +0000

    extend the list of missing feature
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2622 7c735d0d-823a-0410-acfd-cd159556d4bf

--- scripts/FixCling.mk
+++ scripts/FixCling.mk
@@ -4,14 +4,22 @@
 #
 
 CALLROOTEXE = root.exe
+CALLROOTEXE += -e "\#define ClingWorkAroundMissingImplicitAuto"
 CALLROOTEXE += -e "\#define ClingWorkAroundMissingDynamicScope"
 CALLROOTEXE += -e "\#define ClingWorkAroundMissingUnloading"
 CALLROOTEXE += -e "\#define ClingWorkAroundMissingAutoLoading"
 CALLROOTEXE += -e "\#define ClingWorkAroundMissingSmartInclude"
+CALLROOTEXE += -e "\#define ClingWorkAroundErracticValuePrinter"
+CALLROOTEXE += -e "\#define ClingWorkAroundBrokenUnnamedReturn"
+CALLROOTEXE += -e "\#define ClingWorkAroundUnnamedIncorrectInitOrder"
 
 # variable to be used in Makefiles.
 
+ClingWorkAroundMissingImplicitAuto = yes
 ClingWorkAroundMissingDynamicScope = yes
 ClingWorkAroundMissingUnloading = yes
 ClingWorkAroundMissingAutoLoading = yes
 ClingWorkAroundMissingSmartInclude = yes
+ClingWorkAroundErracticValuePrinter = yes
+ClingWorkAroundBrokenUnnamedReturn = yes
+ClingWorkAroundUnnamedIncorrectInitOrder = yes # See https://savannah.cern.ch/bugs/index.php?99210
