3ebb907b84c240e64edf19f1c38a2a4fa76f7309
Philippe Canal <pcanal@fnal.gov>
2012-12-03 16:22:10 +0000

    Work around cling
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2623 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/array/array1.C
+++ cint/array/array1.C
@@ -1,10 +1,18 @@
 {
 const int n1 = 1;
-TString a1[n1];   
+TString a1[n1];
+#ifdef ClingWorkAroundPrintfIssues
+fprintf(stderr,"a1 is 0x%lx 0x%lx\n",(size_t)a1,(size_t)&(a1[0]));
+#else
 fprintf(stdout,"a1 is 0x%lx 0x%lx\n",(size_t)a1,(size_t)&(a1[0]));
+#endif
 const int n2 = 2;
 TString a2[n2];   
+#ifdef ClingWorkAroundPrintfIssues
+fprintf(stderr,"a2 is 0x%lx 0x%lx\n",(size_t)a2,(size_t)&(a2[0]));
+#else
 fprintf(stdout,"a2 is 0x%lx 0x%lx\n",(size_t)a2,(size_t)&(a2[0]));
+#endif
 gROOT->ProcessLine(".g a1");
 gROOT->ProcessLine(".g a2");
 } 
