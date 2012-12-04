2e63af657fbea5d19215a2478d736b66ca7060b2
Philippe Canal <pcanal@fnal.gov>
2012-12-04 06:28:36 +0000

    Work around cling missing features
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2634 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/clones/runnestedClones3.C
+++ root/treeformula/clones/runnestedClones3.C
@@ -1,7 +1,14 @@
 {
 // Fill out the code of the actual test
    TFile *_file0 = TFile::Open("nesting3.root");
+#ifdef ClingWorkAroundMissingDynamicScope
+   TTree *outTTree; _file0->GetObject("outTTree",outTTree);
+#endif
    outTTree->Scan("GTT2.mvdrHits@.size()","","",10);
    outTTree->Scan("GTT2.mvdrHits.r","","",10);
+#ifdef ClingWorkAroundBrokenUnnamedReturn
+   int res = 0;
+#else
    return 0;
+#endif
 }
