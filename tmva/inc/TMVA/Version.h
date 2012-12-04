e76d765f96a83a81ebaa8d5a8bdd52a73c72bbf7
Helge Voss <Helge.Voss@cern.ch>
2012-12-04 13:19:34 +0000

    updated the version number to 4.1.4
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@47816 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/inc/TMVA/Version.h
+++ tmva/inc/TMVA/Version.h
@@ -41,10 +41,10 @@
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
 
-#define TMVA_RELEASE      "4.1.3"
-#define TMVA_RELEASE_DATE "May 04, 2012"
-#define TMVA_RELEASE_TIME "15:00:00"
-#define TMVA_VERSION_CODE 262403
+#define TMVA_RELEASE      "4.1.4"
+#define TMVA_RELEASE_DATE "Dec 04, 2012"
+#define TMVA_RELEASE_TIME "14:19:17"
+#define TMVA_VERSION_CODE 262404
 #define TMVA_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
