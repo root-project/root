ca1fcdc36e16041f62b115a232f46dfd392aefff
Philippe Canal <pcanal@fnal.gov>
2012-12-06 21:23:48 +0000

    workaround for cling
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2678 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/collection/execTClonesArrayAbsorb.C
+++ root/collection/execTClonesArrayAbsorb.C
@@ -1,4 +1,7 @@
 {
+#ifdef ClingWorkAroundMissingImplicitAuto
+   TClonesArray *tc, *tc2;
+#endif
 tc = new TClonesArray("TNamed",5); for(int i=0; i<20; ++i) tc->ConstructedAt(i);
 tc2 = new TClonesArray("TNamed",5); for(int i=0; i<20; ++i) tc2->ConstructedAt(i);
 tc->AbsorbObjects(tc2);
