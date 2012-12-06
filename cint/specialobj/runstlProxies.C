d5a271789009163039056455ad2368d30845ca59
Philippe Canal <pcanal@fnal.gov>
2012-12-06 17:59:25 +0000

    more workaround
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2670 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/specialobj/runstlProxies.C
+++ cint/specialobj/runstlProxies.C
@@ -2,7 +2,20 @@
    gROOT->ProcessLine(".L AthIndex.h+");
    gROOT->ProcessLine(".L T0Result.h+");
    gROOT->ProcessLine(".L calib.h+");
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder   
+   TFile* f = 0;
+   f = TFile::Open("tmp.root", "");
+#else
    TFile* f = TFile::Open("tmp.root", "");
+#endif
+#ifdef ClingWorkAroundMissingDynamicScope
+   f->Get("calib");
+#else
    calib;
+#endif
+#ifdef ClingWorkAroundBrokenUnnamedReturn
+   bool ret = 0;
+#else
    return 0;
+#endif
 }
--- cint/specialobj/write.C
+++ cint/specialobj/write.C
@@ -1,8 +1,14 @@
 {
-gROOT->ProcessLine(".L AthIndex.h+");
-gROOT->ProcessLine(".L T0Result.h+");
-gROOT->ProcessLine(".L calib.h+");
+   gROOT->ProcessLine(".L AthIndex.h+");
+   gROOT->ProcessLine(".L T0Result.h+");
+   gROOT->ProcessLine(".L calib.h+");
+   
+#ifdef ClingWorkAroundMissingDynamicScope
+   TObject *calib = 0;
+   calib = (TObject*)gROOT->ProcessLine("new RTCalib;");
+#else
    RTCalib *calib = new RTCalib;
+#endif
    TFile* f = TFile::Open("tmp.root", "recreate");
    calib->Write();
    f->Close();
