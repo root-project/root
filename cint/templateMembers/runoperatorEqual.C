f169ba4ad4c0da08cadcf781ac62ffad099e3385
Philippe Canal <pcanal@fnal.gov>
2012-12-06 13:37:51 +0000

    CINT does not understand MyTemplate<>
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2667 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/templateMembers/runoperatorEqual.C
+++ cint/templateMembers/runoperatorEqual.C
@@ -1,5 +1,9 @@
 {
 gSystem->Setenv("LINES","-1");
 gROOT->ProcessLine(".L operatorEqual.C+");
+#ifdef __CINT__
+gROOT->ProcessLine(".class StThreeVector");
+#else
 gROOT->ProcessLine(".class StThreeVector<>");
+#endif
 }
