45ed3c3676d5468a7004e5b444a616edfbce093b
Philippe Canal <pcanal@fnal.gov>
2012-12-07 18:54:34 +0000

    Disable (and justify) part of the test about #pragma link namespace
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2698 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/meta/runnospace.C
+++ root/meta/runnospace.C
@@ -5,6 +5,14 @@
       cerr << "Could not retrive the class named AddSpace::Simple\n";
    }
    if (gROOT->GetClass("AddSpace")) {
-      cerr << "We found the namespace AddSpace.  The test is not complete!\n";
+      // The test is about being able to find the class even-though
+      // the user did not have an explicit pragma include of for
+      // the namespace.  With cling, we no longer filter which 
+      // symbol are known to the interpreter dictionary (AST nodes),
+      // it is non longer possible to have the situation where
+      // it is forgotten.  In addition, we can always create a
+      // TClass for namespace from its NamespaceDecl AST node
+      // so this test is moot.
+      // cerr << "We found the namespace AddSpace.  The test is not complete!\n";
    }
 }
