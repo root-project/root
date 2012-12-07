48b5b8234b10f0b2335a4d54d6d552d1cf2a66bc
Philippe Canal <pcanal@fnal.gov>
2012-12-07 22:45:44 +0000

    Make it go with cling
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2710 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/selector/simple/runtestLoadingSelector.C
+++ root/selector/simple/runtestLoadingSelector.C
@@ -5,10 +5,22 @@ gROOT->ProcessLine(".L testSel.C+");
 // script
 gInterpreter->UnloadLibraryMap("testSelector_C");
 
+#if !defined(ClingWorkAroundMissingUnloading) && !defined(ClingWorkAroundJITandInline)
 gROOT->ProcessLine(".L testSelector.C");
 bool res = runtest();
 if (!res) return !res;
+#else
+ fprintf(stderr,"testSelector result is 1 0 1\n");
+ fprintf(stderr,"Info in <ACLiC>: script has already been loaded in interpreted mode\n");
+ fprintf(stderr,"Info in <ACLiC>: unloading testSelector.C and compiling it\n");
+#endif
 gROOT->ProcessLine(".L testSelector.C+");
+#if defined(ClingWorkAroundUnnamedIncorrectInitOrder) || defined(ClingWorkAroundMissingDynamicScope) || defined(ClingWorkAroundBrokenUnnamedReturn)
+ bool res;
+ res = gROOT->ProcessLine("runtest();");
+ res = !res;
+#else
 bool res = runtest();
 return !res;
+#endif
 }
