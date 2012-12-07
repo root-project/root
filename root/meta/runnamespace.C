3deb60337bddf524572db71cde58a794df2a5752
Philippe Canal <pcanal@fnal.gov>
2012-12-07 21:57:05 +0000

    Work around for cling
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2702 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/meta/runnamespace.C
+++ root/meta/runnamespace.C
@@ -2,7 +2,11 @@
 // Fill out the code of the actual test
    gROOT->ProcessLine(".L fornamespace.C+");
 gROOT->GetClass("MySpace::MyClass");
+#ifdef ClingWorkAroundMissingDynamicScope
+   gROOT->ProcessLine("using namespace MySpace;");
+#else
 using namespace MySpace;
+#endif
 gROOT->GetClass("MyClass");
 gROOT->GetClass("MySpace::MyClass")->Print();
 gROOT->GetClass("MyClass")->Print();
