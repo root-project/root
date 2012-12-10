5713f6bec3249314b04612c814fbf86a9fc94029
Philippe Canal <pcanal@fnal.gov>
2012-12-10 22:37:00 +0000

    G__BIT_ISCPPCOMPILED no longer exist
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2725 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/selector/simple/testSel.C
+++ root/selector/simple/testSel.C
@@ -5,7 +5,7 @@ bool runtest() {
    int  valid = gInterpreter->ClassInfo_IsValid(cl);
    long property = gInterpreter->ClassInfo_Property(cl);
    void *p =  gInterpreter->ClassInfo_New(cl);
-   fprintf(stderr,"testSelector result is %d %ld %d\n",valid, property&G__BIT_ISCPPCOMPILED,p!=0);
+   fprintf(stderr,"testSelector result is %d %ld %d\n",valid, property&kIsCPPCompiled,p!=0);
    gInterpreter->ClassInfo_Delete(cl);
    return p!=0;
 }
