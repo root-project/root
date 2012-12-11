2ead6a37d4c20f49707f41ab807fad264b3af2fc
Philippe Canal <pcanal@fnal.gov>
2012-12-11 17:00:35 +0000

    Lastest update to the unnamed macro make the https://savannah.cern.ch/bugs/index.php?99246 issue a problem for this test
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2728 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/stl/map/stringMap.cxx
+++ cint/stl/map/stringMap.cxx
@@ -1,6 +1,12 @@
+#ifndef ClingWorkAroundUnnamedInclude
 {
 #include <map>
 #include <string>
+#else
+#include <map>
+#include <string>
+void stringMap() {
+#endif
 
 TClass *cl = gROOT->GetClass("map<string,double>");
 if (cl->GetClassInfo()==0) {
@@ -10,5 +16,7 @@ if (cl->GetClassInfo()==0) {
 map<string,double> m;
 m["howdy"]=3.14159;
 cout<<m["howdy"]<<endl;
+#ifndef ClingWorkAroundUnnamedInclude
 return 0;
+#endif
 }
