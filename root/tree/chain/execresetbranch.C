6b44aacd11ebd3d46ec2df738d17d633ff8d6290
Philippe Canal <pcanal@fnal.gov>
2012-12-12 17:50:41 +0000

    Add cling-workaround
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2736 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/chain/execresetbranch.C
+++ root/tree/chain/execresetbranch.C
@@ -20,17 +20,23 @@ static int gValue = 0;
 void create_file (const string &fname)
 {
 	cout << "Making ntuple file " << fname << endl;
+#ifdef ClingWorkAroundMissingImplicitAuto
+	TFile* f = new TFile (fname.c_str(), "RECREATE");
+   
+	TTree* t = new TTree ("ntup", "Ntuple of one");
+#else
 	auto f = new TFile (fname.c_str(), "RECREATE");
-
+   
 	auto t = new TTree ("ntup", "Ntuple of one");
+#endif
 	t->Branch("val", &gValue);
-
+   
 	for (int i = 0; i < 10; i++)
 	{
 		gValue = i;
 		t->Fill();
 	}
-
+   
 	f->Write();
 	f->Close();
 }
@@ -40,45 +46,49 @@ void execresetbranch()
 	/// Create the two files
 	create_file ("n1.root");
 	create_file ("n2.root");
-
+   
 	/// Create the chain that we will be reading in
+#ifdef ClingWorkAroundMissingImplicitAuto
+	TChain* c = new TChain("ntup");
+#else
 	auto c = new TChain("ntup");
+#endif
 	c->AddFile("n1.root");
 	c->AddFile("n2.root");
-
+   
 	TBranch *b;
 	c->SetBranchAddress("val", &gValue, &b);
-
+   
 	int ent = c->GetEntries();
 	cout << "Total Entries " << ent << endl;
-
+   
 	/// Run through most of the index
 	int index = 0;
 	for (index = 0; index < ent - 50; index++) {
 		c->GetEntry(index);
 	}
-
+   
 	/// Now, reset the address and then re-go
 	// b->ResetAddress(); - doesn't seem to do anything
-        // Any operation done directly on a branch of a TTree inside a TChain
-        // had unpredicable behavior.
+   // Any operation done directly on a branch of a TTree inside a TChain
+   // had unpredicable behavior.
 	for (index = 0; index < ent; index++) {
 		c->GetEntry(index);
 		cout << gValue << endl;
 	}
-
-        gValue = -1;
+   
+   gValue = -1;
 	c->ResetBranchAddress(b); // use do not work This doesn't either
 	for (index = 0; index < ent; index++) {
 		c->GetEntry(index);
 		cout << gValue << endl;
 	}
 	
-        gValue = -1;
-        c->ResetBranchAddresses(); // THis actuall works!!
+   gValue = -1;
+   c->ResetBranchAddresses(); // THis actuall works!!
 	for (index = 0; index < ent; index++) {
 		c->GetEntry(index);
 		cout << gValue << endl;
 	}
-
+   
 }
