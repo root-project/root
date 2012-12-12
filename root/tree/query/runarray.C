83c7db924b3900a2cfa62a9de518e41b1f029faf
Philippe Canal <pcanal@fnal.gov>
2012-12-12 16:25:21 +0000

    Add cling-workaround
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2733 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/query/runarray.C
+++ root/tree/query/runarray.C
@@ -6,6 +6,8 @@
    arr[1] = 2;
    t->Fill();
    t->Scan("arr");
+   TSQLResult *r;
    r = t->Query("arr");
+   TSQLRow *row;
    while( ( row = r->Next() ) ) { cout << row->GetField(0) << endl; delete row ; row = 0; }
 }
