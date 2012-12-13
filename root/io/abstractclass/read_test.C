113611adf9e268c32fc63009c831fca91086d903
Philippe Canal <pcanal@fnal.gov>
2012-12-13 19:47:48 +0000

    add cling workaround
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2752 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/abstractclass/read_test.C
+++ root/io/abstractclass/read_test.C
@@ -1,15 +1,30 @@
 {
 gROOT->Reset();
 
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+if (1) {
+#endif
 gSystem->Load("libData");
 
 // create and initialize file
 TFile *hfile = TFile::Open("data.root","READ");
 
+#ifdef ClingWorkAroundMissingDynamicScope
+gROOT->ProcessLine(
+"DataBlock1 *db1 = (DataBlock1 *) hfile->Get(\"DataBlock1\");"
+"DataBlock2 *db2 = (DataBlock2 *) hfile->Get(\"DataBlock2\");"
+"db1->Print();"
+"db2->Print();"
+);
+#else
 DataBlock1 *db1 = (DataBlock1 *) hfile->Get("DataBlock1");
 DataBlock2 *db2 = (DataBlock2 *) hfile->Get("DataBlock2");
 
 db1->Print();
 db2->Print();
+#endif
 
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+
+#endif
 }
