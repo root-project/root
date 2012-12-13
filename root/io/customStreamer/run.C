497f285fff25afa324152bc44045fdad57b6113a
Philippe Canal <pcanal@fnal.gov>
2012-12-13 19:46:37 +0000

    add cling workaround
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2751 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/customStreamer/run.C
+++ root/io/customStreamer/run.C
@@ -1,10 +1,32 @@
 {
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   if (1) {
+#endif
    gROOT->ProcessLine(".L header.C+");
    TClass *cl = gROOT->GetClass("Hard2Stream");
 // cl->SetStreamer(new TStreamer(hard2StreamStreamer));
 //setStreamer();
 
    cout << "Hard2Stream version #" << cl->GetClassVersion() << endl;
+#ifdef ClingWorkAroundMissingDynamicScope
+gROOT->ProcessLine(
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   " if (1) {"
+#endif
+   "TClass *cl = gROOT->GetClass(\"Hard2Stream\");"
+   "TBufferFile buf(TBuffer::kWrite);"
+   "Hard2Stream myobj(33.33);"
+   "myobj.print();"
+   "buf.WriteObjectAny(&myobj,cl);"
+   "buf.SetReadMode();"
+   "buf.Reset(); "
+   "Hard2Stream* readobj = (Hard2Stream*) buf.ReadObjectAny(0);"
+   "readobj->print();"
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   " } "
+#endif
+);
+#else
    TBufferFile buf(TBuffer::kWrite);
    Hard2Stream myobj(33.33);
    myobj.print();
@@ -12,8 +34,12 @@
    buf.SetReadMode();
    buf.Reset(); 
    Hard2Stream* readobj = (Hard2Stream*) buf.ReadObjectAny(0);
-   readobj.print();
+   readobj->print();
+#endif
 
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+}
+#endif
 }
 
    
