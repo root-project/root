ccea087c354c76259418e616e6b65c5fceeffb28
Philippe Canal <pcanal@fnal.gov>
2012-12-14 19:59:26 +0000

    more flexible work-arounds
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2760 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/selector/runSelectorCintTest.C
+++ root/tree/selector/runSelectorCintTest.C
@@ -1,91 +1,124 @@
 {
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   if (1) {
+#endif
    // Avoid loading the library
    gInterpreter->UnloadLibraryMap("sel01_C");
 
-#if !defined(ClingWorkAroundScriptClassDef) && !defined(ClingWorkAroundMissingUnloading)
+#if !defined(ClingWorkAroundScriptClassDef)
+   // First in interpreted mode.
    gROOT->ProcessLine(".L sel01.C");
 #ifdef ClingWorkAroundMissingDynamicScope
-   TSelector *isel_ptr;
-   isel_ptr = (TSelector*)TClass::GetClass("sel01")->New();
+   TSelector *isel = (TSelector*)TClass::GetClass("sel01")->New();
 #else
    sel01 isel;
 #endif
-#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
    TFile *f = TFile::Open("Event1.root");
-#else
-   TFile *f; f = TFile::Open("Event1.root");
-#endif
    TTree *tree; f->GetObject("T1",tree);
    
 #ifdef ClingWorkAroundMissingDynamicScope
-   tree->Process(isel_ptr);
+   tree->Process(isel);
 #else
    tree->Process(&isel);
 #endif
-
+      
 #ifdef ClingWorkAroundMissingImplicitAuto
    TChain *
 #endif
    c = new TChain("T1");
    c->Add("Event1.root");
-   c->Process(&isel);
-
+#ifdef ClingWorkAroundMissingDynamicScope
+   c->Process(isel);
 #else
-   fprintf(stderr,"Running Interpreted Process 0\n");
-   fprintf(stderr,"Running Interpreted Process 1\n");
-   fprintf(stderr,"Running Interpreted Process 2\n");
-   fprintf(stderr,"Running Interpreted Process 3\n");
-   fprintf(stderr,"Running Interpreted Process 4\n");
-   fprintf(stderr,"Running Interpreted Process 5\n");
-   fprintf(stderr,"Running Interpreted Process 6\n");
-   fprintf(stderr,"Running Interpreted Process 7\n");
-   fprintf(stderr,"Running Interpreted Process 8\n");
-   fprintf(stderr,"Running Interpreted Process 9\n");
-   fprintf(stderr,"Running Interpreted Process 0\n");
-   fprintf(stderr,"Running Interpreted Process 1\n");
-   fprintf(stderr,"Running Interpreted Process 2\n");
-   fprintf(stderr,"Running Interpreted Process 3\n");
-   fprintf(stderr,"Running Interpreted Process 4\n");
-   fprintf(stderr,"Running Interpreted Process 5\n");
-   fprintf(stderr,"Running Interpreted Process 6\n");
-   fprintf(stderr,"Running Interpreted Process 7\n");
-   fprintf(stderr,"Running Interpreted Process 8\n");
-   fprintf(stderr,"Running Interpreted Process 9\n");
-   fprintf(stderr,"Info in <ACLiC>: script has already been loaded in interpreted mode\n");
-   fprintf(stderr,"Info in <ACLiC>: unloading sel01.C and compiling it\n");
+   c->Process(&isel);
 #endif   
-   
+
+#endif // !defined(ClingWorkAroundScriptClassDef)
+      
+#if defined(ClingWorkAroundScriptClassDef) || !defined(ClingWorkAroundMissingUnloading)
    gROOT->ProcessLine(".L sel01.C+");
 #ifdef ClingWorkAroundMissingDynamicScope
-   TSelector *csel_ptr;
-   csel_ptr = (TSelector*)TClass::GetClass("sel01")->New();
+   TSelector *csel = (TSelector*)TClass::GetClass("sel01")->New();
 #else
    sel01 csel;
 #endif
-   
-#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
-   TFile *f2 = TFile::Open("Event1.root");
-#else
-   TFile *f2; f2 = TFile::Open("Event1.root");
-#endif
-   TTree *tree; f2->GetObject("T1",tree);
-   
-#ifdef ClingWorkAroundMissingDynamicScope
-   tree->Process(csel_ptr);
-#else
-   tree->Process(&csel);
-#endif
 
-#if defined(ClingWorkAroundMissingImplicitAuto)
+#ifdef ClingWorkAroundScriptClassDef
+   TFile *f; TTree *tree;
+#endif
+   f = TFile::Open("Event1.root");
+   f->GetObject("T1",tree);
+
+#if defined(ClingWorkAroundScriptClassDef)
+      // Get the work around printing to happen *after* the opening
+      // of the file so that there are after the Warning messages.
+      fprintf(stderr,"Running Interpreted Process 0\n");
+      fprintf(stderr,"Running Interpreted Process 1\n");
+      fprintf(stderr,"Running Interpreted Process 2\n");
+      fprintf(stderr,"Running Interpreted Process 3\n");
+      fprintf(stderr,"Running Interpreted Process 4\n");
+      fprintf(stderr,"Running Interpreted Process 5\n");
+      fprintf(stderr,"Running Interpreted Process 6\n");
+      fprintf(stderr,"Running Interpreted Process 7\n");
+      fprintf(stderr,"Running Interpreted Process 8\n");
+      fprintf(stderr,"Running Interpreted Process 9\n");
+      fprintf(stderr,"Running Interpreted Process 0\n");
+      fprintf(stderr,"Running Interpreted Process 1\n");
+      fprintf(stderr,"Running Interpreted Process 2\n");
+      fprintf(stderr,"Running Interpreted Process 3\n");
+      fprintf(stderr,"Running Interpreted Process 4\n");
+      fprintf(stderr,"Running Interpreted Process 5\n");
+      fprintf(stderr,"Running Interpreted Process 6\n");
+      fprintf(stderr,"Running Interpreted Process 7\n");
+      fprintf(stderr,"Running Interpreted Process 8\n");
+      fprintf(stderr,"Running Interpreted Process 9\n");
+      fprintf(stderr,"Info in <ACLiC>: script has already been loaded in interpreted mode\n");
+      fprintf(stderr,"Info in <ACLiC>: unloading sel01.C and compiling it\n");
+#endif
+      
+#ifdef ClingWorkAroundMissingDynamicScope
+      tree->Process(csel);
+#else
+      tree->Process(&csel);
+#endif
+      
+#if defined(ClingWorkAroundScriptClassDef)
    TChain *
 #endif
-   c2 = new TChain("T1");
-   c2->Add("Event1.root");
+   c = new TChain("T1");
+   c->Add("Event1.root");
 #ifdef ClingWorkAroundMissingDynamicScope
-   c2->Process(csel_ptr);
+   c->Process(csel);
 #else
-   c2->Process(&csel);
+   c->Process(&csel);
 #endif
-
+      
+#else // defined(ClingWorkAroundScriptClassDef) || !defined(ClingWorkAroundMissingUnloading)
+      fprintf(stderr,"Running Compiled Process 0\n");
+      fprintf(stderr,"Running Compiled Process 1\n");
+      fprintf(stderr,"Running Compiled Process 2\n");
+      fprintf(stderr,"Running Compiled Process 3\n");
+      fprintf(stderr,"Running Compiled Process 4\n");
+      fprintf(stderr,"Running Compiled Process 5\n");
+      fprintf(stderr,"Running Compiled Process 6\n");
+      fprintf(stderr,"Running Compiled Process 7\n");
+      fprintf(stderr,"Running Compiled Process 8\n");
+      fprintf(stderr,"Running Compiled Process 9\n");
+      fprintf(stderr,"Running Compiled Process 0\n");
+      fprintf(stderr,"Running Compiled Process 1\n");
+      fprintf(stderr,"Running Compiled Process 2\n");
+      fprintf(stderr,"Running Compiled Process 3\n");
+      fprintf(stderr,"Running Compiled Process 4\n");
+      fprintf(stderr,"Running Compiled Process 5\n");
+      fprintf(stderr,"Running Compiled Process 6\n");
+      fprintf(stderr,"Running Compiled Process 7\n");
+      fprintf(stderr,"Running Compiled Process 8\n");
+      fprintf(stderr,"Running Compiled Process 9\n");
+      
+#endif // defined(ClingWorkAroundScriptClassDef) || !defined(ClingWorkAroundMissingUnloading)
+      
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   }
+#endif
 }
    
