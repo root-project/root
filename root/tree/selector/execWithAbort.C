d5c8989bfcfb014a496f4eff6b45fc4e47cd2125
Philippe Canal <pcanal@fnal.gov>
2012-12-14 20:01:15 +0000

    Add missing work arounds
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2762 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/tree/selector/execWithAbort.C
+++ root/tree/selector/execWithAbort.C
@@ -1,17 +1,28 @@
 {
-// Avoid loading the library
-gInterpreter->UnloadLibraryMap("selabort_C");
-TChain ch("T");
-ch.Add("Event1.root/T1");
-ch.Add("Event2.root/T2");
-ch.Add("Event3.root/T3");
-ch.Process("selabort.C","thefirstoption");
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   if (1) {
+#endif
+   // Avoid loading the library
+   gInterpreter->UnloadLibraryMap("selabort_C");
+   TChain ch("T");
+   ch.Add("Event1.root/T1");
+   ch.Add("Event2.root/T2");
+   ch.Add("Event3.root/T3");
+   ch.Process("selabort.C","thefirstoption");
 
-TSelector *sel = TSelector::GetSelector("selabort.C");
-ch.Process(sel,"theoptions");
-#ifdef ClingWorkAroundBrokenUnnamedReturn
-int res = 0;
+#ifdef ClingWorkAroundMissingUnloading
+   TSelector *sel = static_cast<TSelector*>(TClass::GetClass("selabort")->New());
 #else
-return 0;
+   TSelector *sel = TSelector::GetSelector("selabort.C");
+#endif
+   ch.Process(sel,"theoptions");
+
+#ifdef ClingWorkAroundUnnamedIncorrectInitOrder
+   }
+#endif
+#ifdef ClingWorkAroundBrokenUnnamedReturn
+   gApplication->Terminate(0);
+#else
+   return 0;
 #endif
 }
