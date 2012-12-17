1a69e40938ada47deeaacba9fe4351a6f3b25709
Philippe Canal <pcanal@fnal.gov>
2012-12-17 21:51:26 +0000

    updates for cling
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2795 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/newClassDef/current/Run.C
+++ root/io/newClassDef/current/Run.C
@@ -1,23 +1,36 @@
 void Run() {
-
-  //TString library = ".L namespace.";
-  //gROOT->ProcessLine(library+dllsuf);
-  //library = ".L template.";
-  //gROOT->ProcessLine(library+dllsuf);
-  //library = ".L nstemplate.";
-  //// gROOT->ProcessLine(library+dllsuf);
-
-  //library = ".L InheritMulti.";
-  //gROOT->ProcessLine(library+dllsuf);
+   
+   //TString library = ".L namespace.";
+   //gROOT->ProcessLine(library+dllsuf);
+   //library = ".L template.";
+   //gROOT->ProcessLine(library+dllsuf);
+   //library = ".L nstemplate.";
+   //// gROOT->ProcessLine(library+dllsuf);
+   
+   //library = ".L InheritMulti.";
+   //gROOT->ProcessLine(library+dllsuf);
    gSystem->Load("./namespace");
    gSystem->Load("./template");
    gSystem->Load("./InheritMulti");
-
-  namespace_driver();
-  template_driver();
-  //nstemplate_driver();
-  if (! InheritMulti_driver() ) exit(1);
-
-  gROOT->ProcessLine(".L array.cxx+");
-  array_driver();
+   
+#ifdef ClingWorkAroundMissingDynamicScope
+   gROOT->ProcessLine(
+   "namespace_driver();"
+   "template_driver();"
+   //nstemplate_driver();
+   "if (! InheritMulti_driver() ) exit(1);"
+                      );
+#else
+   namespace_driver();
+   template_driver();
+   //nstemplate_driver();
+   if (! InheritMulti_driver() ) exit(1);
+#endif
+   
+   gROOT->ProcessLine(".L array.cxx+");
+#ifdef ClingWorkAroundMissingDynamicScope
+   gROOT->ProcessLine("array_driver();");
+#else
+   array_driver();
+#endif
 }
--- root/io/newClassDef/new/Run.C
+++ root/io/newClassDef/new/Run.C
@@ -5,11 +5,19 @@ void Run() {
    gSystem->Load("./nstemplate");
    gSystem->Load("./InheritMulti");
 
+#ifdef ClingWorkAroundMissingDynamicScope
+   gROOT->ProcessLine(
+   "namespace_driver();"
+   "template_driver();"
+   "nstemplate_driver();"
 
+   "if (! InheritMulti_driver() ) exit(1);"
+                      );
+#else
    namespace_driver();
    template_driver();
    nstemplate_driver();
-
+   
    if (! InheritMulti_driver() ) exit(1);
-
+#endif   
 }
