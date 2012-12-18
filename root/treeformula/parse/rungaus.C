0816adff1cffccae4ed8c25871992aaf21bd4f98
Philippe Canal <pcanal@fnal.gov>
2012-12-18 06:53:54 +0000

    Add workaround ClingWorkAroundCallfuncAndConversion
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2800 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/rungaus.C
+++ root/treeformula/parse/rungaus.C
@@ -4,7 +4,11 @@
 #endif
 gROOT->ProcessLine(".L gausbug.cxx");
 TF1 *test = new TF1("test","gaussianfunction(x)",-10.0, 10.0);
+#ifdef ClingWorkAroundCallfuncAndConversion
+cout << test->Eval(3.2) << endl;
+#else
 cout << test->Eval(3) << endl;
+#endif
 gApplication->Terminate(0);
 #ifdef ClingWorkAroundUnnamedIncorrectInitOrder
    }
