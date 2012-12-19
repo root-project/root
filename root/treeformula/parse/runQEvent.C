12000838465df64087fa603138c136b097db5881
Philippe Canal <pcanal@fnal.gov>
2012-12-19 14:22:48 +0000

    more ClingWorkAroundJITandInline
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2822 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/parse/runQEvent.C
+++ root/treeformula/parse/runQEvent.C
@@ -7,9 +7,13 @@ void runQEvent() {
     const Int_t n = 100;
     UInt_t *array = new UInt_t[n];
 
+#ifdef ClingWorkAroundJITandInline
     for(Int_t i(0); i<n; i++)
-        array[i] = 300*TMath::Abs(TMath::Sin(TMath::TwoPi()*i/n));
-
+        array[i] = 300*TMath::Abs(sin(6.28318530717959*i/n));
+#else
+   for(Int_t i(0); i<n; i++)
+      array[i] = 300*TMath::Abs(TMath::Sin(TMath::TwoPi()*i/n));
+#endif
     QRawTriggerPulse* tp = new QRawTriggerPulse(n,array);
 
     TFile *f = new TFile("myTest.root","recreate");
