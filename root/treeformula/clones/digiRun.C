7841fc03ca501f71646603b263265444fe6a0354
Philippe Canal <pcanal@fnal.gov>
2012-12-19 14:47:01 +0000

    Better version of ClingWorkAroundBrokenUnnamedReturn (the previous one failed with the improvement related to the  initialization orde
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2825 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/clones/digiRun.C
+++ root/treeformula/clones/digiRun.C
@@ -10,7 +10,7 @@ new TFile("digi.root");
 int n = Digi->BuildIndex("m_runId", "m_eventId");
 #endif
 #ifdef ClingWorkAroundBrokenUnnamedReturn
-   int res = (n>0); // to signal succesfull we need a zero!
+   gApplication->Terminate(!(n>0)); // to signal succesfull we need a zero!
 #else
 return ! (n>0); // to signal succesfull we need a zero!
 #endif
