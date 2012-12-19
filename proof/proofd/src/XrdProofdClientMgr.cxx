c6b279655970670faa6e0d4e79c33f2efa7c3b6b
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2012-12-19 11:37:42 +0000

    Turn-off role checking for localhost connections (not required and limiting daemon test applications)
    git-svn-id: http://root.cern.ch/svn/root/trunk@48122 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proofd/src/XrdProofdClientMgr.cxx
+++ proof/proofd/src/XrdProofdClientMgr.cxx
@@ -348,6 +348,13 @@ int XrdProofdClientMgr::Login(XrdProofdProtocol *p)
       return 0;
    }
 
+   TRACE(ALL," hostname: '"<<p->Link()->Host()<<"'");
+   //
+   // Check if in any-server mode (localhost connections always are)
+   bool anyserver = (fMgr->SrvType() == kXPD_AnyServer ||
+                     !strcmp(p->Link()->Host(), "localhost") ||
+                     !strcmp(p->Link()->Host(), "127.0.0.0")) ? 1 : 0;
+   
    // Find out the connection type: 'i', internal, means this is a proofsrv calling back.
    bool needauth = 0;
    bool ismaster = (fMgr->SrvType() == kXPD_TopMaster || fMgr->SrvType() == kXPD_Master) ? 1 : 0;
@@ -361,7 +368,7 @@ int XrdProofdClientMgr::Login(XrdProofdProtocol *p)
       response->SetTag("int");
       break;
    case 'M':
-      if (fMgr->SrvType() == kXPD_AnyServer || ismaster) {
+      if (anyserver || ismaster) {
          p->SetConnType(kXPD_ClientMaster);
          needauth = 1;
          response->SetTag("m2c");
@@ -373,7 +380,7 @@ int XrdProofdClientMgr::Login(XrdProofdProtocol *p)
       }
       break;
    case 'm':
-      if (fMgr->SrvType() == kXPD_AnyServer || ismaster) {
+      if (anyserver || ismaster) {
          p->SetConnType(kXPD_MasterMaster);
          needauth = 1;
          response->SetTag("m2m");
@@ -398,7 +405,7 @@ int XrdProofdClientMgr::Login(XrdProofdProtocol *p)
       }
       break;
    case 's':
-      if (fMgr->SrvType() == kXPD_AnyServer || fMgr->SrvType() == kXPD_MasterWorker) {
+      if (anyserver || fMgr->SrvType() == kXPD_MasterWorker) {
          p->SetConnType(kXPD_MasterWorker);
          needauth = 1;
          response->SetTag("w2m");
