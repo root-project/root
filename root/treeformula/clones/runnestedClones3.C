d3e5fa7fb6e29d0939cf3d0fe4749e7d92cc29b0
Philippe Canal <pcanal@fnal.gov>
2012-12-19 13:28:34 +0000

    add ClingWorkAroundCallfuncAndInline
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2820 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/treeformula/clones/runnestedClones3.C
+++ root/treeformula/clones/runnestedClones3.C
@@ -4,7 +4,11 @@
 #ifdef ClingWorkAroundMissingDynamicScope
    TTree *outTTree; _file0->GetObject("outTTree",outTTree);
 #endif
+#ifdef ClingWorkAroundCallfuncAndInline
+   outTTree->Scan("GTT2.mvdrHits@.GetEntries()","","",10);
+#else
    outTTree->Scan("GTT2.mvdrHits@.size()","","",10);
+#endif
    outTTree->Scan("GTT2.mvdrHits.r","","",10);
 #ifdef ClingWorkAroundBrokenUnnamedReturn
    int res = 0;
