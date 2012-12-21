c8d0a60a8910c9268b24af650890a4e41f704016
Fons Rademakers <Fons.Rademakers@cern.ch>
2012-12-21 17:01:19 +0000

    don't delete tutorials/proof/ntprndm.root.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@48183 27541ba8-7e3a-0410-8455-c3a389f83636

--- build/unix/distfilelist.sh
+++ build/unix/distfilelist.sh
@@ -21,6 +21,7 @@ fi
 mv -f tutorials/gallery.root tutorials/gallery.root-
 mv -f tutorials/mlp/mlpHiggs.root tutorials/mlp/mlpHiggs.root-
 mv -f tutorials/quadp/stock.root tutorials/quadp/stock.root-
+mv -f tutorials/proof/ntprndm.root tutorials/proof/ntprndm.root-
 find tutorials -name "*.root" -exec rm -rf {} \; >/dev/null 2>&1;true
 find tutorials -name "*.ps" -exec rm -rf {} \; >/dev/null 2>&1;true
 find tutorials -path tutorials/doc -prune -o -name "*.gif" -exec rm -rf {} \; >/dev/null 2>&1;true
@@ -33,6 +34,7 @@ find tutorials -name "work.pcl" -exec rm -rf {} \; >/dev/null 2>&1;true
 mv -f tutorials/gallery.root- tutorials/gallery.root
 mv -f tutorials/mlp/mlpHiggs.root- tutorials/mlp/mlpHiggs.root
 mv -f tutorials/quadp/stock.root- tutorials/quadp/stock.root
+mv -f tutorials/proof/ntprndm.root- tutorials/proof/ntprndm.root
 
 # mixture of files, wildcards, and directories
 WILDCARDS="LICENSE README bin \
