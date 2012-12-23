c487325c760ec021aa7a8d6be9717769e218e2d3
Philippe Canal <pcanal@fnal.gov>
2012-12-23 05:55:14 +0000

    work around autoloading issues
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2833 7c735d0d-823a-0410-acfd-cd159556d4bf

--- cint/parsing/runtestTable.C
+++ cint/parsing/runtestTable.C
@@ -1,4 +1,9 @@
 {
+#ifndef ClingWorkAroundNoDotOptimization
 gROOT->ProcessLine(".O 0");
+#endif
+#ifdef ClingWorkAroundMissingAutoLoading
+gSystem->Load("libTable");
+#endif
 gROOT->ProcessLine(".x testTable.C");
 }
