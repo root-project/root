b0c5881fb889cfa3b41ce541af2c71b06570e950
Philippe Canal <pcanal@fnal.gov>
2013-01-14 20:45:43 +0000

    add missing workarounds
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2845 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/rootcint/runtemplateKeyword.C
+++ root/io/rootcint/runtemplateKeyword.C
@@ -1,5 +1,9 @@
 {
 // Fill out the code of the actual test
 gROOT->ProcessLine(".L templateKeyword.C+");
+#ifdef ClingWorkAroundMissingAutoLoading
+gROOT->ProcessLine("MyTest<double> d;");
+#else
 MyTest<double> d;
+#endif
 }
