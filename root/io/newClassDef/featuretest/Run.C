dbc2c016e84f682d55264da5b01c75b8493067e3
Philippe Canal <pcanal@fnal.gov>
2013-01-14 20:14:23 +0000

    add workarounds
    git-svn-id: http://root.cern.ch/svn/roottest/trunk@2843 7c735d0d-823a-0410-acfd-cd159556d4bf

--- root/io/newClassDef/featuretest/Run.C
+++ root/io/newClassDef/featuretest/Run.C
@@ -1,7 +1,11 @@
 {
 
 gROOT->ProcessLine(".L typeidtest.C++");
+#ifdef ClingWorkAroundMissingDynamicScope
+if (gROOT->ProcessLine("typeidtest();")==0) gApplication->Terminate(1);
+#else
 if (typeidtest()==0) gApplication->Terminate(1);
+#endif
 
 gROOT->ProcessLine(".x Class.C++");
 gROOT->ProcessLine(".x ClassTrick.C++");
