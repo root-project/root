e560e9c933e367643f1026cf54b385c390dfa7e5
Olivier Couet <Olivier.Couet@cern.ch>
2013-01-22 13:40:56 +0000

    apply the patch suggested here: https://savannah.cern.ch/bugs/index.php?99968
    git-svn-id: http://root.cern.ch/svn/root/trunk@48368 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/graf/src/TText.cxx
+++ graf2d/graf/src/TText.cxx
@@ -784,7 +784,7 @@ void TText::SetMbTitle(const wchar_t *title)
 {
    // Change (i.e. set) the title of the TNamed.
 
-   char *mb_title = new char[MB_CUR_MAX * wcslen(title) + 1];
+   char *mb_title = new char[MB_CUR_MAX * wcslen(title) + 1]();
    char *p = mb_title;
    size_t length = wcslen(title);
    for (size_t i = 0; i < length; i++) {
