7adba0b06bd0db53aedf8b29042a7b4551afa41e
Wouter Verkerke <verkerke@nikhef.nl>
2013-01-29 15:16:53 +0000

    Small adjustment in invocation of chi-squared - don't override default arguments
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@48433 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/stressRooFit_tests.cxx
+++ test/stressRooFit_tests.cxx
@@ -3484,7 +3484,7 @@ public:
   // NB: Within the usual approximations of a chi2 fit, a chi2 fit to weighted
   // data using sum-of-weights-squared errors does give correct error
   // estimates
-  RooChi2Var chi2("chi2","chi2",p2,*binnedData,DataError(RooAbsData::SumW2)) ;
+  RooChi2Var chi2("chi2","chi2",p2,*binnedData) ;
   RooMinuit m(chi2) ;
   m.migrad() ;
   m.hesse() ;
