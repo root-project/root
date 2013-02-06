39bfb0600b63715393f0b4ca077fcb8e9d222e09
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-02-06 11:44:30 +0000

    TransformProcessType can fail, when gSystem's ProcessEvents is called from python (Apple's system version), error code is -50 (paramErr, which (possibly) means, that process is already foreground).
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@48479 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/cocoa/src/TGCocoa.mm
+++ graf2d/cocoa/src/TGCocoa.mm
@@ -4190,7 +4190,11 @@ bool TGCocoa::MakeProcessForeground()
       ProcessSerialNumber psn = {0, kCurrentProcess};
 
       const OSStatus res1 = TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-      if (res1 != noErr) {
+
+      //When TGCocoa's functions are called from the python (Apple's system version),
+      //TransformProcessType fails with paramErr (looks like process is _already_ foreground),
+      //why is it a paramErr - I've no idea.
+      if (res1 != noErr && res1 != paramErr) {
          Error("MakeProcessForeground", "TransformProcessType failed with code %d", res1);
          return false;
       }
