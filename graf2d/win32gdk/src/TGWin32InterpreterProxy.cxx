8a87a1485f8b838817d7fe992b864b2070b7c9ed
Philippe Canal <pcanal@fnal.gov>
2013-02-10 02:58:28 +0000

    Add windows update for TInterpreter
    git-svn-id: http://root.cern.ch/svn/root/trunk@48524 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/win32gdk/src/TGWin32InterpreterProxy.cxx
+++ graf2d/win32gdk/src/TGWin32InterpreterProxy.cxx
@@ -72,7 +72,7 @@ void TGWin32InterpreterProxy::InspectMembers(TMemberInspector& insp, void* obj, 
    DEBUG_PROFILE_PROXY_STOP(InspectMembers)
 }
 
-RETURN_METHOD_ARG2(Interpreter,TClass*,GenerateTClass,const char *,classname,Bool_t,silent); 
+RETURN_METHOD_ARG3(Interpreter,TClass*,GenerateTClass,const char *,classname,Bool_t,emulation,Bool_t,silent);
 RETURN_METHOD_ARG2(Interpreter,TClass*,GenerateTClass,ClassInfo_t *,classinfo,Bool_t,silent); 
 RETURN_METHOD_ARG3(Interpreter,Int_t,GenerateDictionary,const char*,classes,const char*,headers,const char*,options); 
 RETURN_METHOD_ARG0(Interpreter,char*,GetPrompt)
