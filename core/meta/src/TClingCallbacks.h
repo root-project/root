2e63c6b9e38bbf02120bb39f88def24a1b178536
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2013-02-11 12:55:01 +0000

    Move the DynamicIDHandler into separate source file so that it could be used while implementing custom interpreter callbacks.
    git-svn-id: http://root.cern.ch/svn/root/trunk@48527 27541ba8-7e3a-0410-8455-c3a389f83636

--- core/meta/src/TClingCallbacks.h
+++ core/meta/src/TClingCallbacks.h
@@ -2,7 +2,7 @@
 // Author: Vassil Vassilev   7/10/2012
 
 /*************************************************************************
- * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2013, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
