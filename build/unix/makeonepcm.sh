91ee864e0de1d3460c2fbbc4bc8a38783adbd00f
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-02-14 23:46:03 +0000

    Procedure to create one large pcm: ./build/unix/makeonepcm.sh makeonepch.sh creates the files:    include/all.h    include/all.h.pch makeonepcm.sh first runs makeonepch.sh and then uses rootcling to create:    include/allLinkDef.h    lib/allDict_rdict.pcm    allDict.h    allDict.cxx
    Currently tested on OSX, updates for Linux coming asap.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@48585 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ build/unix/makeonepcm.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# create first one large pch
+./build/unix/makeonepch.sh
+err=$?
+if [ $err -ne 0 ]; then
+   echo "Failed to make one pch"
+   exit $err
+fi
+
+# create one large allLinkDef.h
+rm -f include/allLinkDef.h
+
+# create allLinkDef.h including all LinkDefs
+find . -name \*LinkDef\*.h | sed -e 's|^|#include "|' -e 's|$|"|' > alldefs.h
+
+mv alldefs.h include/allLinkDef.h
+
+# generate one large pcm
+rm -f allDict.h allDict.cxx lib/allDict_rdict.pcm
+core/utils/src/rootcling_tmp -f allDict.cxx -c include/all.h include/allLinkDef.h
+
+exit $?
