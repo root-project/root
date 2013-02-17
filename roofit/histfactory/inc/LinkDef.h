6af202f7199d0ed865921f167b0775d3d1bf962b
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-02-17 18:58:23 +0000

    pragma defined_in causing problems in rootcling. Commented out for the time being (roofit is the only module using this feature).
    git-svn-id: http://root.cern.ch/svn/root/trunk@48613 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/histfactory/inc/LinkDef.h
+++ roofit/histfactory/inc/LinkDef.h
@@ -44,9 +44,11 @@
 
 // make dictionary for all the C++ classes defined in these following files
 #ifndef _WIN32   // on Windows rootcint -I seemes not to work 
+#ifndef __CLING__
 #pragma link C++ defined_in "RooStats/HistFactory/MakeModelAndMeasurementsFast.h"; 
 #pragma link C++ defined_in "RooStats/HistFactory/Systematics.h"; 
 #pragma link C++ defined_in "RooStats/HistFactory/HistFactoryModelUtils.h"; 
+#endif
 #else 
 #pragma link C++ defined_in "roofit/histfactory/inc/RooStats/HistFactory/MakeModelAndMeasurementsFast.h"; 
 #pragma link C++ defined_in "roofit/histfactory/inc/RooStats/HistFactory/Systematics.h"; 
