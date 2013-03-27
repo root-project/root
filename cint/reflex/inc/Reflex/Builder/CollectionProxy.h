07c3f4f8e49830479864b6c2f65c9af21f1ef23d
Philippe Canal <pcanal@fnal.gov>
2013-03-27 01:03:44 +0000

    Fix another windows warnings
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@48949 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/reflex/inc/Reflex/Builder/CollectionProxy.h
+++ cint/reflex/inc/Reflex/Builder/CollectionProxy.h
@@ -206,7 +206,7 @@ namespace Reflex  {
             assert(0 && "Intentionally not implemented, do not use.");
             return 0;
          }
-         static void destruct1(void  */* iter_ptr */) {
+         static void destruct1(void  * /* iter_ptr */) {
             // Nothing to do
          }
          static void destruct2(void * /* begin_ptr */, void * /* end_ptr */) {
