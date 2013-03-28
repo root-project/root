2d8eae58de67cbb9cf838bcc2a1e0a09ed124345
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-03-28 13:53:29 +0000

    From Lifeng Sun: The build system fails to generate iosenum.h on non-official supported platform, the attached patch fixes this issue.
    git-svn-id: http://root.cern.ch/svn/root/branches/v5-34-00-patches@48975 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/cint/Module.mk
+++ cint/cint/Module.mk
@@ -285,15 +285,17 @@ $(MAKECINT):    $(MAKECINTO)
 $(IOSENUM): 
 		$(MAKEDIR)
 		@(if [ ! -r $(IOSENUMA) ]; then \
-			echo "Missing $(IOSENUMA), run: make $IOSENUMA)"; \
-		else \
-			cp $(IOSENUMA) $@; \
-		fi)
+			echo "Missing $(IOSENUMA), run: make $(IOSENUMA)"; \
+			$(MAKE) $(IOSENUMA); \
+		fi; \
+		cp $(IOSENUMA) $@)
 
 $(IOSENUMA):    $(CINTTMP)
 		@(if [ ! -r $@ ]; then \
 			echo "Making $@..."; \
-			$(CINTTMP) $(CINTTMPINC) -Iinclude $(IOSENUMC) > /dev/null; \
+			$(CINTTMP) $(CINTTMPINC) \
+			        -I$(ROOT_SRCDIR)/cint/cint/inc -Iinclude \
+				$(IOSENUMC) > /dev/null; \
 			mv iosenum.h $@; \
 		else \
 			touch $@; \
