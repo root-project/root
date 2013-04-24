c02c92e14c7306a1737c4f8ff4c12120be534f03
Axel Naumann <Axel.Naumann@cern.ch>
2013-04-24 20:48:42 +0000

    Remove left-over diff.
    git-svn-id: http://root.cern.ch/svn/root/trunk@49331 27541ba8-7e3a-0410-8455-c3a389f83636

--- interpreter/llvm/src/tools/clang/include/clang/AST/DeclBase.h
+++ interpreter/llvm/src/tools/clang/include/clang/AST/DeclBase.h
@@ -1508,9 +1508,6 @@ public:
   /// declarations lexically in this context.
   void setHasExternalLexicalStorage(bool ES = true) {
     ExternalLexicalStorage = ES;
-#ifdef AXEL_LOOKUP_CHANGES
-    setMustBuildLookupTable();
-#endif
   }
 
   /// \brief Whether this DeclContext has external storage containing
