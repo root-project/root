3cdc597d293574efdc3b67c773921d296ec4274e
Philippe Canal <pcanal@fnal.gov>
2013-04-25 09:31:34 -0500

    Correct the work-around to be used and reduce it to the necessary parts

--- root/meta/evolution/writefile.C
+++ root/meta/evolution/writefile.C
@@ -1,15 +1,13 @@
 void writefile(int version = 1) {
    gROOT->ProcessLine(Form(".L data%d.C+",version));
-#if defined(ClingWorkAroundMissingAutoLoading)
-   TFile *f;
-   f = new TFile(Form("data%d.root",version),"RECREATE");
+   TFile *f = new TFile(Form("data%d.root",version),"RECREATE");
+#if defined(ClingWorkAroundMissingDynamicScope)
    gROOT->ProcessLine(TString::Format("TFile *f = (TFile*)%p;\n",f));
    gROOT->ProcessLine("data *a = new data; f->WriteObject(a,\"myobj\");"); 
    gROOT->ProcessLine("Tdata *b = new Tdata;  f->WriteObject(b,\"myTobj\");");
 #else
    data *a = new data;
    Tdata *b = new Tdata;
-   TFile *f = new TFile(Form("data%d.root",version),"RECREATE");
    f->WriteObject(a,"myobj");
    f->WriteObject(b,"myTobj");
 #endif
