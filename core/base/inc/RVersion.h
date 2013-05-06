fe29e8d064ac60d9d9736a276c9a6fbe0e082982
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-05-07 00:35:20 +0200

    update RVersion.h, remove ROOT_SVN_REVISION.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -17,8 +17,8 @@
 #define ROOT_RELEASE "5.34/07"
 #define ROOT_RELEASE_DATE "Apr 26 2013"
 #define ROOT_RELEASE_TIME "12:51:17"
-#define ROOT_SVN_REVISION 49358
-#define ROOT_SVN_BRANCH "branches/v5-34-00-patches"
+#define ROOT_GIT_COMMIT aa2b805
+#define ROOT_GIT_BRANCH "branches/v5-34-00-patches"
 #define ROOT_VERSION_CODE 336391
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
