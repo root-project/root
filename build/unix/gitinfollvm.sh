8950d6f891babe20cdfe97d123fbfff897d50103
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-05-07 23:55:01 +0200

    rewrite svninfollvm.sh to use git and rename.

--- build/unix/gitinfollvm.sh
+++ build/unix/gitinfollvm.sh
@@ -3,38 +3,38 @@
 # Check version of interpreter/llvm directory and if it is changed force
 # a make in the LLVM tree. It creates the file
 #    interpreter/llvm/obj/llvmrev.txt
-# containing the current interpreter/llvm rev number.
-# If the current rev is different from the stored rev, force a make
+# containing the current interpreter/llvm SHA1.
+# If the current SHA1 is different from the stored SHA1, force a make
 # in the LLVM tree. Script returns 1 when make has to be forced, 0 otherwise.
 # Exit status is always 0.
 
 ret=0
 dir=
-dotsvn=".svn"
-svn=svn
+dotgit=".git"
+git=git
 if [ $# = 1 ]; then
    if [ -x /bin/cygpath ]; then
       dir=`cygpath -u $1`
-      svn=/usr/bin/svn
+      git=/usr/bin/git
    else
       dir=$1
    fi
-   dotsvn="$dir/.svn"
+   dotgit="$dir/.git"
 fi
 
-# if we don't see the .svn directory, just return
-if test ! -d $dotsvn; then
+# if we don't see the .git directory, just return
+if test ! -d $dotgit; then
    echo $ret
    exit 0;
 fi
 
 OUT=interpreter/llvm/obj/llvmrev.txt
 revold=
-rev=`$svn info $dir | awk '/Last Changed Rev:/ { print $4 }'`
+rev=`$git --git-dir=$dotgit log -1 --pretty=format:"%h" -- interpreter/llvm`
 
 if [ -f $OUT ]; then
    revold=`cat $OUT`
-   if [ $rev -ne $revold ]; then
+   if [ $rev != $revold ]; then
       ret=1
    fi
 else
--- interpreter/llvm/Module.mk
+++ interpreter/llvm/Module.mk
@@ -58,9 +58,9 @@ endif
 FORCELLVM := 0
 ifeq ($(findstring $(MAKECMDGOALS),clean distclean maintainer-clean dist distsrc),)
 ifeq ($(findstring clean-,$(MAKECMDGOALS)),)
-ifeq ($(shell which svn 2>&1 | sed -ne "s@.*/svn@svn@p"),svn)
-FORCELLVM := $(shell bash $(ROOT_SRCDIR)/build/unix/svninfollvm.sh $(ROOT_SRCDIR)/interpreter/llvm)
-ENDLLVMBUILD := && cd ../../.. && ( $(ROOT_SRCDIR)/build/unix/svninfollvm.sh $(ROOT_SRCDIR)/interpreter/llvm > /dev/null )
+ifeq ($(shell which git 2>&1 | sed -ne "s@.*/git@git@p"),git)
+FORCELLVM := $(shell bash $(ROOT_SRCDIR)/build/unix/gitinfollvm.sh $(ROOT_SRCDIR))
+ENDLLVMBUILD := && cd ../../.. && ( $(ROOT_SRCDIR)/build/unix/gitinfollvm.sh $(ROOT_SRCDIR) > /dev/null )
 endif
 endif
 endif
