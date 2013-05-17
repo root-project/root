dcf4119b2fd8c0ac02e2daa6a70e242207651c4b
Wim Lavrijsen <wlav@pc3-wlav.lbl.gov>
2013-05-17 12:58:43 -0700

    Cling workaround: special case to resolve string <-> basic_string<char>

--- bindings/pyroot/src/RootWrapper.cxx
+++ bindings/pyroot/src/RootWrapper.cxx
@@ -230,8 +230,15 @@ int PyROOT::BuildRootClassDict( const TScopeAdapter& klass, PyObject* pyclass ) 
 
    // special case trackers
       Bool_t setupSetItem = kFALSE;
-      Bool_t isConstructor =
-         TClassEdit::ShortType( mtName.c_str(), TClassEdit::kDropAlloc ) == clName;
+   // CLING WORKAROUND --
+      Bool_t isConstructor = kFALSE;
+      if ( clName == "string" ) {
+         isConstructor = mtName == "basic_string<char>";
+      } else {
+   // -- CLING WORKAROUND
+         isConstructor =
+            TClassEdit::ShortType( mtName.c_str(), TClassEdit::kDropAlloc ) == clName;
+      }
 
    // filter empty names (happens for namespaces, is bug?)
       if ( mtName == "" )
