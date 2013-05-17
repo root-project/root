4306cefd3f36e412bc07bfe97157d8b938cc38bb
Wim Lavrijsen <wlav@pc3-wlav.lbl.gov>
2013-05-17 09:46:02 -0700

    only use TSetItemHolder for non-const by-ref returns

--- bindings/pyroot/src/RootWrapper.cxx
+++ bindings/pyroot/src/RootWrapper.cxx
@@ -246,10 +246,14 @@ int PyROOT::BuildRootClassDict( const TScopeAdapter& klass, PyObject* pyclass ) 
 
    // operator[]/() returning a reference type will be used for __setitem__
       if ( mtName == "__call__" || mtName == "__getitem__" ) {
-         std::string cpd = Utility::Compound(
-            method.TypeOf().ReturnType().Name( Rflx::QUALIFIED | Rflx::SCOPED | Rflx::FINAL ) );
-         if ( ! cpd.empty() && cpd[ cpd.size() - 1 ] == '&' )
-            setupSetItem = kTRUE;
+         std::string qual_return = method.TypeOf().ReturnType().Name(
+            Rflx::QUALIFIED | Rflx::SCOPED | Rflx::FINAL );
+         if ( qual_return.find( "const", 0, 5 ) == std::string::npos ) {
+            std::string cpd = Utility::Compound( qual_return );
+            if ( ! cpd.empty() && cpd[ cpd.size() - 1 ] == '&' ) {
+               setupSetItem = kTRUE;
+            }
+         }
       }
 
    // decide on method type: member or static (which includes globals)
