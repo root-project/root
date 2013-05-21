5093896260e2b60d6bf86bf00dcc8b2b20c367c4
Vassil Vassilev <vvasilev@cern.ch>
2013-05-21 23:02:17 +0200

    Second try.

--- root/hist/profile/runcopy.C
+++ root/hist/profile/runcopy.C
@@ -1,12 +1,9 @@
 {
-#ifdef ClingWorkAroundMissingImplicitAuto
+#if defined(ClingWorkAroundMissingImplicitAuto) || defined(ClingWorkAroundExtraParensWithImplicitAuto)
   TProfile2D*a,*b;
 #endif
   a = new TProfile2D();
-
-#ifdef ClingWorkAroundExtraParensWithImplicitAuto
   b = new TProfile2D(*a);
-#endif
   b->Print();
 }
  
