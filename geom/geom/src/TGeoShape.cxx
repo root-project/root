dcb9ae9bf0ab2e723e254076f4258e6882eb570b
Andrei Gheata <Andrei.Gheata@cern.ch>
2013-05-24 09:22:10 +0200

    Precision fix for TGeoShape::SafetySeg

--- geom/geom/src/TGeoShape.cxx
+++ geom/geom/src/TGeoShape.cxx
@@ -479,7 +479,10 @@ Double_t TGeoShape::SafetySeg(Double_t r, Double_t z, Double_t r1, Double_t z1, 
    Double_t crossp = (z2-z1)*(r-r1)-(z-z1)*(r2-r1);
    crossp *= (outer) ? 1. : -1.;
    // Positive crossp means point on the requested side of the (1,2) segment
-   if (crossp < 0) return TGeoShape::Big();
+   if (crossp < 0) {
+      if (((z-z1)*(z2-z)) > 0) return 0;
+      return TGeoShape::Big();
+   }   
    // Compute (1,P) dot (1,2)
    Double_t c1 = (z-z1)*(z2-z1)+(r-r1)*(r2-r1);
    // Negative c1 means point (1) is closest
