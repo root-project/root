a587c9aab230101668e61e6a5c686fb1eb062994
Philippe Canal <pcanal@fnal.gov>
2013-06-04 15:44:17 -0500

    Add small test of TInterpreter::SetClassSharedLibs

--- root/meta/runautoload.C
+++ root/meta/runautoload.C
@@ -5,5 +5,14 @@ if (gROOT->GetClass("NotReal")!=0) {
 }
 if (gROOT->GetClass("TVector3")==0) {
    fprintf(stderr,"Error: Failed to load TVector3\n");
+} 
+//gInterpreter->SetClassSharedLibs("Event","libByHand.so");
+gInterpreter->SetClassSharedLibs("Event","Event_cxx.so");
+TString res = gInterpreter->GetClassSharedLibs("Event");
+if (res != "Event_cxx.so") {
+   fprintf(stderr,"Error: hand registration of library did not work, found %s\n",res.Data());
+}
+if (TClass::GetClass("Event")==0) {
+   fprintf(stderr,"Error: Event_cxx.so was not loaded properly, can not find Event.\n");
 }
 }
