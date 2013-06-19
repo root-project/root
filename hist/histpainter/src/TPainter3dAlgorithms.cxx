0f6544e60f7b914050fe6dc62baffd7fd1fdf2a4
Olivier Couet <olivier.couet@cern.ch>
2013-06-19 09:32:34 +0200

    A "," was used instead of a ";"

--- hist/histpainter/src/TPainter3dAlgorithms.cxx
+++ hist/histpainter/src/TPainter3dAlgorithms.cxx
@@ -111,7 +111,7 @@ TPainter3dAlgorithms::TPainter3dAlgorithms(): TObject(), TAttLine(1,1,1), TAttFi
    }
 
    for (i=0;i<fNStack;i++) { fColorMain[i] = 1; fColorDark[i] = 1; fEdgeColor[i] = 1; fEdgeStyle[i] = 1; fEdgeWidth[i] = 1; }
-   for (i=0;i<3;i++)       { fRmin[i] = 0, fRmax[i] = 1; }
+   for (i=0;i<3;i++)       { fRmin[i] = 0; fRmax[i] = 1; }
    for (i=0;i<4;i++)       { fYls[i] = 0; }
 
    for (i=0;i<30;i++)      { fJmask[i] = 0; }
@@ -195,7 +195,7 @@ TPainter3dAlgorithms::TPainter3dAlgorithms(Double_t *rmin, Double_t *rmax, Int_t
    }
 
    for (i=0;i<fNStack;i++) { fColorMain[i] = 1; fColorDark[i] = 1; fEdgeColor[i] = 1; fEdgeStyle[i] = 1; fEdgeWidth[i] = 1; }
-   for (i=0;i<3;i++)       { fRmin[i] = rmin[i], fRmax[i] = rmax[i]; }
+   for (i=0;i<3;i++)       { fRmin[i] = rmin[i]; fRmax[i] = rmax[i]; }
    for (i=0;i<4;i++)       { fYls[i] = 0; }
 
    for (i=0;i<30;i++)      { fJmask[i] = 0; }
