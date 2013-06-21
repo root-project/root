f9f1ae3024b3ec064ba763a2b2c8de5fb5fe5e92
Olivier Couet <olivier.couet@cern.ch>
2013-06-21 09:52:38 +0200

    New tutorial showing how to create a 2D histogram from an image.

---
+++ tutorials/image/image2hist.C
@@ -0,0 +1,23 @@
+//Create a 2-D histogram from an image.
+//Author: Olivier Couet
+
+void image2hist()
+{
+   TASImage image("rose512.jpg");
+   UInt_t yPixels = image.GetHeight();
+   UInt_t xPixels = image.GetWidth();
+   UInt_t *argb   = image.GetArgbArray();
+
+   TH2D* h = new TH2D("h","Rose histogram",xPixels,-1,1,yPixels,-1,1);
+
+   for (int row=0; row<xPixels; ++row) {
+      for (int col=0; col<yPixels; ++col) {
+         int index = col*xPixels+row;
+         float grey = float(argb[index]&0xff)/256;
+         h->SetBinContent(row+1,yPixels-col,grey);
+      }
+   }
+   
+   gStyle->SetPalette(53);
+   h->Draw("colz");
+}
