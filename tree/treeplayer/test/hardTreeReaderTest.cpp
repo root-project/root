c49a1d0095f2fc36b571d8c7ff991d6a51da11e2
Anders E <strykejernene@gmail.com>
2013-06-27 15:15:43 +0200

    Code cleanup for makeTree

--- tree/treeplayer/test/hardTreeReaderTest.cpp
+++ tree/treeplayer/test/hardTreeReaderTest.cpp
@@ -43,15 +43,11 @@ void makeTree(){
 	TTree *myTree = new TTree("HardTree", "This is hard to read");
 
 	A myObject0 (NUM_CONSTANT);
-	A myObject1 (NUM_CONSTANT);
-	A myObject99 (NUM_CONSTANT);
-	A myObject101 (NUM_CONSTANT);
-	//A branchForMembers (42);
 
 	myTree->Branch("A0.",	"A", 	&myObject0,		32000, 0);
-	myTree->Branch("A1.",	"A", 	&myObject1,		32000, 1);
-	myTree->Branch("A99.",	"A", 	&myObject99,	32000, 99);
-	myTree->Branch("A101.",	"A", 	&myObject101,	32000, 101);
+	myTree->Branch("A1.",	"A", 	&myObject0,		32000, 1);
+	myTree->Branch("A99.",	"A", 	&myObject0,	32000, 99);
+	myTree->Branch("A101.",	"A", 	&myObject0,	32000, 101);
 
 	// myTree->Branch("vectorB_0.",	&branchForMembers.vectorB,	32000, 0);
 	// myTree->Branch("vectorB_99.",	&branchForMembers.vectorB,	32000, 99);
@@ -60,11 +56,11 @@ void makeTree(){
 	// myTree->Branch("vectorBStar_99.",	&branchForMembers.vectorBStar,	32000, 99);
 	// myTree->Branch("vectorBStar_101.",	&branchForMembers.vectorBStar,	32000, 101);
 
-	myTree->Branch("vectorB_0.",	&myObject99.vectorB,	32000, 0);
-	myTree->Branch("vectorB_99.",	&myObject99.vectorB,	32000, 99);
+	myTree->Branch("vectorB_0.",	&myObject0.vectorB,	32000, 0);
+	myTree->Branch("vectorB_99.",	&myObject0.vectorB,	32000, 99);
 
-	myTree->Branch("vectorBStar_0.",	&myObject99.vectorBStar,	32000, 0);
-	myTree->Branch("vectorBStar_99.",	&myObject99.vectorBStar,	32000, 99);
+	myTree->Branch("vectorBStar_0.",	&myObject0.vectorBStar,	32000, 0);
+	myTree->Branch("vectorBStar_99.",	&myObject0.vectorBStar,	32000, 99);
 	//myTree->Branch("vectorBStar_101.",	&branchForMembers.vectorBStar,	32000, 101); // Breaks Fill()
 
 
@@ -73,128 +69,55 @@ void makeTree(){
 
 		// Clear old values
 		myObject0.ResetVectorB();
-		myObject1.ResetVectorB();
-		myObject99.ResetVectorB();
-		myObject101.ResetVectorB();
-
-		// Integer
-		// printf("Setting nums\n");
-		// myObject0.num = i;
-		// myObject1.num = i*2;
-		// myObject99.num = i*3;
-		// myObject101.num = i*4;
-
-		// myObject0.num = i;
-		// myObject1.num = i*2;
-		// myObject99.num = i*3;
-		// myObject101.num = i*4;
 
 		printf("Setting BObject\n");
 		myObject0.BObject.dummy = i;
-		myObject1.BObject.dummy = i;
-		myObject99.BObject.dummy = i;
-		myObject101.BObject.dummy = i;
-
-		// std::vector<Int_t> *list1 = new std::vector<Int_t>();
-		// list1->push_back(2);
-		// printf("ListSize: %lu\n", list1->size());
-
-		// std::vector<B> *myList = new std::vector<B>();
-
-		// printf("Size: %lu\n", myList->size());
 
 		for (int j = 0; j < LIST_ENTRIES; ++j){
 			// Vector of objects
 			B obj (i*j);
 			printf("Adding %i to vectorB\n", i*j);
-			myObject0.AddToVectorB(obj); 	//obj.dummy++;
-			myObject1.AddToVectorB(obj); 	//obj.dummy++;
-			myObject99.AddToVectorB(obj); 	//obj.dummy++;
-			myObject101.AddToVectorB(obj); 	//obj.dummy++;
+			myObject0.AddToVectorB(obj);
 
 			obj.dummy *= 2;
 			// Hangs makeTree
 			printf("Adding %i to vectorStarB\n",  obj.dummy);
-			myObject0.AddToVectorStarB(obj); 	 //obj.dummy++;
-			myObject1.AddToVectorStarB(obj); 	 //obj.dummy++;
-			myObject99.AddToVectorStarB(obj); 	 //obj.dummy++;
-			myObject101.AddToVectorStarB(obj);
-
-			// myList->push_back(obj);
+			myObject0.AddToVectorStarB(obj);
 
 			// Vector of pointers
 			printf("Adding %i to vectorBStar\n", i*j*2);
 			B *ptr = new B(i*j*3);
 			myObject0.AddToVectorBStar(ptr);
-			myObject1.AddToVectorBStar(ptr);
-			myObject99.AddToVectorBStar(ptr);
-			//myObject101.AddToVectorBStar(ptr); // Breaks readTree (segfault)
 		}
-
-		// myObject0.SetVectorStarB(myList);
-		// myObject1.SetVectorStarB(myList);
-		// myObject99.SetVectorStarB(myList);
-		// myObject101.SetVectorStarB(myList);
 
 		printf("Setting BStar\n");
 		B *objPtr = new B(i*2);
 		myObject0.SetBStar(objPtr);
-		myObject1.SetBStar(objPtr);
-		myObject99.SetBStar(objPtr);
-		myObject101.SetBStar(objPtr);
 
 		printf("Filling BArray\n");
-		// B myArray[] = {B(12), B(11), B(10), B(9), B(8), B(7), B(6), B(5), B(4), B(3), B(2), B(1)}; // Breaks makeTree (segFault @ end)
 		B *myArray = new B[12];
 		for (int j = 0; j < 12; ++j){
 			myArray[j].dummy = i * j * 4;
 		}
 		myObject0.FillBArray(myArray);
-		myObject1.FillBArray(myArray);
-		myObject99.FillBArray(myArray);
-		myObject101.FillBArray(myArray);
 		delete [] myArray;
 
 		printf("Filling BStarArray\n");
 		for (int j = 0; j < NUM_CONSTANT; ++j){
 			myObject0.BStarArray[j] = i*j*5;
-			myObject1.BStarArray[j] = i*j*5;
-			myObject99.BStarArray[j] = i*j*5;
-			myObject101.BStarArray[j] = i*j*5;
 		}
 
 		printf("Filling BClonesArray\n");
 		for (int j = 0; j < LIST_ENTRIES; ++j ){
 			((B*)myObject0.BClonesArray.New(j))->dummy = i*j*6;
-			((B*)myObject1.BClonesArray.New(j))->dummy = i*j*6;
-			((B*)myObject99.BClonesArray.New(j))->dummy = i*j*6;
-			((B*)myObject101.BClonesArray.New(j))->dummy = i*j*6;
 		}
 
-		//printf("vectorB size: %i\n", myObject0.vectorB.size());
 		printf("Filling tree\n");
 		myTree->Fill();
 
-		// myObject0.ResetBStarArray();
-		// myObject1.ResetBStarArray();
-		// myObject99.ResetBStarArray();
-		// myObject101.ResetBStarArray();
-
-		// Hangs makeTree
-		// myObject0.vectorStarB = new std::vector<B>();
-		// myObject1.vectorStarB = new std::vector<B>();
-		// myObject99.vectorStarB = new std::vector<B>();
-		// myObject101.vectorStarB = new std::vector<B>();
-
 		myObject0.ResetVectorStarB();
-		myObject1.ResetVectorStarB();
-		myObject99.ResetVectorStarB();
-		myObject101.ResetVectorStarB();
 
 		myObject0.vectorBStar.clear();
-		myObject1.vectorBStar.clear();
-		myObject99.vectorBStar.clear();
-		myObject101.vectorBStar.clear();
 
 		printf("Entry created\n");
 	}
