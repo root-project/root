00393a45007225e3841345d54ac75a94dd6c0b97
Dario Berzano <dario.berzano@cern.ch>
2013-08-06 20:05:06 +0200

    PROOF: dynamic/include path on a subset of wrks
    AddDynamicPath() and AddIncludePath() functions accepted a list of
    workers: the Broadcast() was correctly sent to the subset, but the
    Collect() was performed on all active workers. This is not OK when
    adding workers dynamically since we want to stop the packets events
    until the new workers are initialized, and a broad Collect() resumes
    packets processing.

--- proof/proof/src/TProof.cxx
+++ proof/proof/src/TProof.cxx
@@ -9087,11 +9087,14 @@ Int_t TProof::AddDynamicPath(const char *libpath, Bool_t onClient, TList *wrks)
       m << TString("-");
 
    // Forward the request
-   if (wrks)
+   if (wrks) {
       Broadcast(m, wrks);
-   else
+      Collect(wrks, fCollectTimeout);
+   }
+   else {
       Broadcast(m);
-   Collect(kActive, fCollectTimeout);
+      Collect(kActive, fCollectTimeout);
+   }
 
    return 0;
 }
@@ -9124,11 +9127,14 @@ Int_t TProof::AddIncludePath(const char *incpath, Bool_t onClient, TList *wrks)
       m << TString("-");
 
    // Forward the request
-   if (wrks)
+   if (wrks) {
       Broadcast(m, wrks);
-   else
+      Collect(wrks, fCollectTimeout);
+   }
+   else {
       Broadcast(m);
-   Collect(kActive, fCollectTimeout);
+      Collect(kActive, fCollectTimeout);
+   }
 
    return 0;
 }
