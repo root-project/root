3e6a55c9db7cfe8e9db2829a7bb9d1d6148ae615
Andrei Gheata <Andrei.Gheata@cern.ch>
2013-08-07 15:58:36 -0500

    Added TBits::LastSetBit and TBits::LastNullBit analogous to TBits::FirstSetBit and TBits::FirstNullBit

--- core/cont/inc/TBits.h
+++ core/cont/inc/TBits.h
@@ -136,6 +136,8 @@ public:
    UInt_t  CountBits(UInt_t startBit=0)     const ;  // return number of bits set to 1
    UInt_t  FirstNullBit(UInt_t startBit=0)  const;
    UInt_t  FirstSetBit(UInt_t startBit=0)   const;
+   UInt_t  LastNullBit(UInt_t startBit=999999999) const;
+   UInt_t  LastSetBit(UInt_t startBit=999999999)  const;
    UInt_t  GetNbits()      const { return fNbits; }
    UInt_t  GetNbytes()     const { return fNbytes; }
 
