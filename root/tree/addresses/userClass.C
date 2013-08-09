17fe1165c6f106edc5a0115348a2d22ab15998b6
Axel Naumann <Axel.Naumann@cern.ch>
2013-08-09 09:48:31 +0200

    Better fix for unused-private-var warning.

--- root/tree/addresses/userClass.C
+++ root/tree/addresses/userClass.C
@@ -9,12 +9,12 @@ public:
    int t;
 };
 class BottomOne : public TopLevel {
-   BottomOne(): b(0) {}
+public:
    int b;
 };
 #if !defined(__MAKECINT__) && !defined(__ROOTCLING__)
 class BottomMissing : public TopLevel {
-   BottomMissing(): c() {}
+public:
    int c;
 };
 #endif
