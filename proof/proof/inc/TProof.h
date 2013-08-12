root-project/root

commit 00393a45007225e3841345d54ac75a94dd6c0b97

PROOF: dynamic/include path on a subset of wrks
AddDynamicPath() and AddIncludePath() functions accepted a list of
workers: the Broadcast() was correctly sent to the subset, but the
Collect() was performed on all active workers. This is not OK when
adding workers dynamically since we want to stop the packets events
until the new workers are initialized, and a broad Collect() resumes
packets processing.

proof/proof/src/TProof.cxx
@@ -9087,11 +9087,14 @@ Int_t TProof::AddDynamicPath(const char *libpath, Bool_t onClient, TList *wrks)
       m << TString("-");
 
    // Forward the request
-   if (wrks)
+   if (wrks) {
       Broadcast(m, wrks);
-   else
+      Collect(wrks, fCollectTimeout);
+   }
+   else {
       Broadcast(m);
-   Collect(kActive, fCollectTimeout);
+      Collect(kActive, fCollectTimeout);
+   }
 
    return 0;
 }
@@ -9124,11 +9127,14 @@ Int_t TProof::AddIncludePath(const char *incpath, Bool_t onClient, TList *wrks)
       m << TString("-");
 
    // Forward the request
-   if (wrks)
+   if (wrks) {
       Broadcast(m, wrks);
-   else
+      Collect(wrks, fCollectTimeout);
+   }
+   else {
       Broadcast(m);
-   Collect(kActive, fCollectTimeout);
+      Collect(kActive, fCollectTimeout);
+   }
 
    return 0;
 }

commit 8113eb107c99b201b7b64d0f9d66bce8e077a1b0

Fix for non-dynamic PROOF startup hangup
Problem was introduced in 00393a4. This is a quick fix that restores
previous state. A more complete solution is to be found.

proof/proof/inc/TProof.h
@@ -897,8 +897,8 @@ public:
    virtual Int_t Load(const char *macro, Bool_t notOnClient = kFALSE, Bool_t uniqueOnly = kTRUE,
                       TList *wrks = 0);
 
-   Int_t       AddDynamicPath(const char *libpath, Bool_t onClient = kFALSE, TList *wrks = 0);
-   Int_t       AddIncludePath(const char *incpath, Bool_t onClient = kFALSE, TList *wrks = 0);
+   Int_t       AddDynamicPath(const char *libpath, Bool_t onClient = kFALSE, TList *wrks = 0, Bool_t doCollect = kTRUE);
+   Int_t       AddIncludePath(const char *incpath, Bool_t onClient = kFALSE, TList *wrks = 0, Bool_t doCollect = kTRUE);
    Int_t       RemoveDynamicPath(const char *libpath, Bool_t onClient = kFALSE);
    Int_t       RemoveIncludePath(const char *incpath, Bool_t onClient = kFALSE);
 

proof/proof/src/TProof.cxx
@@ -1409,14 +1409,14 @@ Int_t TProof::AddWorkers(TList *workerList)
    dyn.ReplaceAll("\"", " ");
    PDB(kGlobal, 3)
       Info("AddWorkers", "Will invoke AddDynamicPath() on selected workers");
-   AddDynamicPath(dyn, kFALSE, addedWorkers);
+   AddDynamicPath(dyn, kFALSE, addedWorkers, fDynamicStartup);
 
    TString inc = gSystem->GetIncludePath();
    inc.ReplaceAll("-I", " ");
    inc.ReplaceAll("\"", " ");
    PDB(kGlobal, 3)
       Info("AddWorkers", "Will invoke AddIncludePath() on selected workers");
-   AddIncludePath(inc, kFALSE, addedWorkers);
+   AddIncludePath(inc, kFALSE, addedWorkers, fDynamicStartup);
 
    // Update list of current workers
    PDB(kGlobal, 3)
@@ -9092,7 +9092,8 @@ Int_t TProof::Load(const char *macro, Bool_t notOnClient, Bool_t uniqueWorkers,
 }
 
 //______________________________________________________________________________
-Int_t TProof::AddDynamicPath(const char *libpath, Bool_t onClient, TList *wrks)
+Int_t TProof::AddDynamicPath(const char *libpath, Bool_t onClient, TList *wrks,
+   Bool_t doCollect)
 {
    // Add 'libpath' to the lib path search.
    // Multiple paths can be specified at once separating them with a comma or
@@ -9121,7 +9122,8 @@ Int_t TProof::AddDynamicPath(const char *libpath, Bool_t onClient, TList *wrks)
    // Forward the request
    if (wrks) {
       Broadcast(m, wrks);
-      Collect(wrks, fCollectTimeout);
+      if (doCollect)
+         Collect(wrks, fCollectTimeout);
    }
    else {
       Broadcast(m);
@@ -9132,7 +9134,8 @@ Int_t TProof::AddDynamicPath(const char *libpath, Bool_t onClient, TList *wrks)
 }
 
 //______________________________________________________________________________
-Int_t TProof::AddIncludePath(const char *incpath, Bool_t onClient, TList *wrks)
+Int_t TProof::AddIncludePath(const char *incpath, Bool_t onClient, TList *wrks,
+   Bool_t doCollect)
 {
    // Add 'incpath' to the inc path search.
    // Multiple paths can be specified at once separating them with a comma or
@@ -9161,7 +9164,8 @@ Int_t TProof::AddIncludePath(const char *incpath, Bool_t onClient, TList *wrks)
    // Forward the request
    if (wrks) {
       Broadcast(m, wrks);
-      Collect(wrks, fCollectTimeout);
+      if (doCollect)
+         Collect(wrks, fCollectTimeout);
    }
    else {
       Broadcast(m);