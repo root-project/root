root-project/root

commit 6388fefc5ff681a0289166c8804ddc3a59d51075

Class version and user defined properties xml2TClass
The percolation of all kind of properties and class version to
TClass is now in place.
The version is a special case. It has to be included in the selection
rule in the XML reader. Indeed the class selection rules are used to
set the version of particular TClass instances.
The genral properties are percolated via the AST modifying in the
dictionary the TAttributeMap of the TClass associated to a particular
class.

core/utils/src/rootcling.cxx
@@ -357,7 +357,7 @@ void AnnotateFieldDecl(clang::NamedDecl& decl,
    std::string varName;
    for(std::list<VariableSelectionRule>::const_iterator it = fieldSelRules.begin();
        it != fieldSelRules.end(); ++it){
-      it->GetAttributeValue("name",varName);
+      if ( ! it->GetAttributeValue("name",varName)) continue;
       if (declName == varName){ // we have the rule!
          // Let's extract the attributes
          BaseSelectionRule::AttributesMap_t attrMap( it->GetAttributes() );
@@ -370,7 +370,7 @@ void AnnotateFieldDecl(clang::NamedDecl& decl,
             if (name == "transient" && value == "true") // special case
                userDefinedProperty="//!";
             else
-               userDefinedProperty=name+"@@@"+value;
+               userDefinedProperty=name+ROOT::TMetaUtils::PropertyNameValSeparator+value;
             std::cout << varName << " " << userDefinedProperty << std::endl;
             decl.addAttr(new (C) clang::AnnotateAttr(commentRange, C, userDefinedProperty));
          }
@@ -394,8 +394,14 @@ void AnnotateDecl(clang::CXXRecordDecl &CXXRD,
 
    SourceRange commentRange;
 
+   std::string declName;
+   const std::string thisClassName(CXXRD.getName());
+
+   if (genreflex::verbose)
+      std::cout << "Inspecting class declaration" << thisClassName << " for annotations\n";
+
    // Fetch the selection rule associated to this class
-   clang::Decl* declBaseClassPtr = static_cast<clang::Decl*>(&CXXRD);   
+   clang::Decl* declBaseClassPtr = static_cast<clang::Decl*>(&CXXRD);
    const BaseSelectionRule* thisClassBaseSelectionRule (selectionRules.IsDeclSelected(declBaseClassPtr));
    BaseSelectionRule::AttributesMap_t attrMap;
    // If the rule is there
@@ -407,12 +413,14 @@ void AnnotateDecl(clang::CXXRecordDecl &CXXRD,
       std::string userDefinedProperty;
       for(iter = attrMap.begin();iter!=attrMap.end();iter++){
          const std::string& name = iter->first;
-         if (name == "name") continue; 
+         if (name == "name") continue;
          const std::string& value = iter->second;
-         userDefinedProperty=name+"@@@"+value;
+         userDefinedProperty=name+ROOT::TMetaUtils::PropertyNameValSeparator+value;
+         if (genreflex::verbose) std::cout << " * " << userDefinedProperty << std::endl;
          CXXRD.addAttr(new (C) AnnotateAttr(commentRange, C, userDefinedProperty));
       }
    }
+   
 
    const std::string thisClassName(CXXRD.getName());
 
@@ -434,8 +442,9 @@ void AnnotateDecl(clang::CXXRecordDecl &CXXRD,
          SourceLocation maybeMacroLoc = (*I)->getLocation();
          bool isClassDefMacro = maybeMacroLoc.isMacroID() && S.findMacroSpelling(maybeMacroLoc, "ClassDef");
          if (isClassDefMacro) {
-            while (isa<NamedDecl>(*I) && cast<NamedDecl>(*I)->getName() != "DeclFileLine") // FIXME Why While?
+            while (isa<NamedDecl>(*I) && cast<NamedDecl>(*I)->getName() != "DeclFileLine") {
                ++I;
+            }
          }
 
          comment = ROOT::TMetaUtils::GetComment(**I, &commentSLoc);
@@ -3273,6 +3282,7 @@ int RootCling(int argc,
    (*dictHdrOut) << "#define G__DICTIONARY\n";
    (*dictHdrOut) << "#include \"RConfig.h\"\n"
                  << "#include \"TClass.h\"\n"
+                 << "#include \"TClassAttributeMap.h\"\n"
                  << "#include \"TInterpreter.h\"\n"
                  << "#include \"TROOT.h\"\n"
                  << "#include \"TBuffer.h\"\n"

core/metautils/inc/TMetaUtils.h
@@ -52,6 +52,27 @@ namespace cling {
 namespace ROOT {
    namespace TMetaUtils {
 
+      // Convention used to separate name/value of properties in the ast annotations
+      static const std::string PropertyNameValSeparator("@@@");
+      
+      inline bool IsInt(const std::string& s){
+
+         size_t minusPos = s.find_first_of("-");
+         
+         // Count minuses
+         bool minusFound = false;
+         for (size_t i = 0; i < s.size(); i++){
+            if (s[i] == '-'){
+               if (minusFound) return false;
+                  minusFound=true;
+            }
+         }
+         
+         return s.find_first_not_of("-0123456789")==std::string::npos &&
+                !s.empty() &&
+                (minusPos == std::string::npos || minusPos == 0);
+      }
+
       class TNormalizedCtxt {
          typedef llvm::SmallSet<const clang::Type*, 4> TypesCont_t;
       private:

core/metautils/src/TMetaUtils.cxx
@@ -1085,6 +1085,9 @@ void ROOT::TMetaUtils::WriteClassInit(std::ostream& finalString, const ROOT::TMe
       }
    }
 
+
+
+   
    //--------------------------------------------------------------------------
    // Process the read raw rules
    //--------------------------------------------------------------------------
@@ -1284,9 +1287,43 @@ void ROOT::TMetaUtils::WriteClassInit(std::ostream& finalString, const ROOT::TMe
       // Now manipulate tclass in order to percolate the properties expressed as
       // annotations of the decls.
       std::string manipString;
-      
+      std::size_t substrFound;
+      std::string attribute_s;
+      const int separatorLength(ROOT::TMetaUtils::PropertyNameValSeparator.size());
       // Class properties
-      // To be filled when the TClass interface allows this
+      bool attrMapExtracted = false;
+      if (decl->hasAttrs()){
+         // Loop on the attributes
+         for (clang::Decl::attr_iterator attrIt = decl->attr_begin();
+              attrIt!=decl->attr_end();++attrIt){
+            // Extract name
+            clang::AnnotateAttr* annAttr = clang::dyn_cast<clang::AnnotateAttr>(*attrIt);
+            if (!annAttr) continue;
+            attribute_s = annAttr->getAnnotation();
+            // if separator found, extract name and value
+            substrFound = attribute_s.find(ROOT::TMetaUtils::PropertyNameValSeparator);
+            if (substrFound==std::string::npos) continue;
+            size_t EndPart1 = attribute_s.find_first_of(ROOT::TMetaUtils::PropertyNameValSeparator)  ;          
+            std::string attrName (attribute_s.substr(0, EndPart1));
+            std::string attrValue (attribute_s.substr(EndPart1 + separatorLength));
+            // A general property
+            // 1) We need to create the property map (in the gen code)
+            // 2) we need to take out the map (in the gen code)
+            // 3) We need to bookkep the fact that the map is created and out (in this source)
+            // 4) We fill the map (in the gen code)
+            if (!attrMapExtracted){
+               manipString+="      theClass->CreateAttributeMap();\n";
+               manipString+="      TClassAttributeMap* attrMap( theClass->GetAttributeMap() );\n";
+               attrMapExtracted=true;
+            }
+            // If not an int, transform it in a string (for the gen code)
+            if (!ROOT::TMetaUtils::IsInt(attrValue)){
+               attrValue = "\""+attrValue+"\"";
+            }
+
+            manipString+="      attrMap->AddProperty(\""+attrName +"\","+attrValue+");\n";
+         }         
+      } // end of class has properties
 
       // Member properties
       // NOTE: Only transiency propagated
@@ -1298,18 +1335,19 @@ void ROOT::TMetaUtils::WriteClassInit(std::ostream& finalString, const ROOT::TMe
             && (clang::isa<clang::FieldDecl>(*internalDeclIt) ||
                 clang::isa<clang::VarDecl>(*internalDeclIt)))) continue; // Check if it's a var or a field
          // Now let's check the attributes of the var/field
+         if (!internalDeclIt->hasAttrs()) continue;
          for (clang::Decl::attr_iterator attrIt = internalDeclIt->attr_begin();
               attrIt!=internalDeclIt->attr_end();++attrIt){
             // Convert the attribute to AnnotateAttr if possible
             clang::AnnotateAttr* annAttr = clang::dyn_cast<clang::AnnotateAttr>(*attrIt);
             if (!annAttr) continue;
             // Let's see if it's a transient attribute
-            std::string attribute_s (annAttr->getAnnotation());
-            std::string attributeNoSpaces_s (attribute_s);         
+            attribute_s = annAttr->getAnnotation();
+            std::string attributeNoSpaces_s (attribute_s);
             std::remove(attributeNoSpaces_s.begin(), attributeNoSpaces_s.end(), ' ');
             // 1) Let's see if it's a //!
-            std::size_t found = attributeNoSpaces_s.find("//!");
-            if (found != 0) continue;
+            substrFound = attributeNoSpaces_s.find("//!");
+            if (substrFound != 0) continue;
             // 2) Add the modification lines to the manipString.
             //    Get the TDataMamber and then set it Transient
             clang::NamedDecl* namedInternalDecl = clang::dyn_cast<clang::NamedDecl> (*internalDeclIt);
@@ -1326,10 +1364,10 @@ void ROOT::TMetaUtils::WriteClassInit(std::ostream& finalString, const ROOT::TMe
       } // End loop on internal declarations
       
          
-         finalString << "   static void " << mappedname << "_TClassManip(TClass* " << (manipString.empty() ? "":"theClass") << "){\n"
+      finalString << "   static void " << mappedname << "_TClassManip(TClass* " << (manipString.empty() ? "":"theClass") << "){\n"
                   << manipString
                   << "   }\n\n";
-   }
+   } // End of !ClassInfo__HasMethod(decl,"Dictionary") || R__IsTemplate(*decl)) 
 
    finalString << "} // end of namespace ROOT" << "\n" << "\n";
 }

core/metautils/src/XMLReader.cxx
@@ -595,6 +595,11 @@ bool XMLReader::Parse(std::ifstream &file, SelectionRules& out)
                // DEBUG std::cout<<"Attributes:"<<std::endl;
                for (int i = 0, n = attr.size(); i < n; ++i) {
                   // DEBUG std::cout << "\tAttrName[" << i << "]: " << attr[i].fName << " | AttrValue["<<i<<"]: "<<attr[i].fValue<<std::endl;
+
+                  // Set the class version
+                  if (tagKind == kClass && "version" == attr[i].fName && csr){
+                     csr->SetRequestedVersionNumber(atoi(attr[i].fValue.c_str()));
+                  }
                   
                   if (tagKind == kClass || tagKind == kProperties || tagKind == kEnum || tagKind == kFunction || 
                       tagKind == kVariable) {

commit ad78943b75a472e469f4c2f2cb75e27c0e3b082b

Fix small typo (double decl)
Removed double declaration introduced by mistake in
6388fefc5ff681a0289166c8804ddc3a59d51075

core/utils/src/rootcling.cxx
@@ -421,9 +421,6 @@ void AnnotateDecl(clang::CXXRecordDecl &CXXRD,
       }
    }
    
-
-   const std::string thisClassName(CXXRD.getName());
-
    if (genreflex::verbose)
       std::cout << "Inspecting class declaration" << thisClassName << " for annotations\n";
 