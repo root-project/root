892bf9f4b7810000544630e0c38305bf82bbf815
Pere Mato <pere.mato@cern.ch>
2013-09-10 11:59:06 +0200

    Improved the implementation of FindKerberos5.cmake module. Fixes  ROOT-5486

--- cmake/modules/FindKerberos5.cmake
+++ cmake/modules/FindKerberos5.cmake
@@ -2,66 +2,40 @@
 #  Check for libkrb5.a
 #
 #  KRB5_INCLUDE_DIR - where to find krb5.h, etc.
-#  KRB5_LIBRARIES   - List of libraries when using ....
+#  KRB5_INCLUDE_DIRS (not cached)
+#  KRB5_LIBRARIES   - List of libraries when using Kerberos5
+#  KRB5_INIT        - kinit command
 #  KRB5_FOUND       - True if Kerberos 5 libraries found.
 
-set(KRB5_FOUND FALSE)
-set(KBR5_LIBRARIES)
-
 find_path(KRB5_INCLUDE_DIR NAMES krb5.h 
-  /usr/kerberos/include
-  /usr/krb5/include
-  /usr/local/kerberos/include
-  /usr/include 
-  /usr/include/kerberosV 
-  /usr/local/include
-  $ENV{KRB5_DIR}/include
-)
+  HINTS  $ENV{KRB5_DIR} ${KRB5_DIR}
+  PATH_SUFFIXES include kerberos krb5)
+
+set(KRB5_INCLUDE_DIRS ${KRB5_INCLUDE_DIR})
+
+find_library(KRB5_LIBRARY NAMES krb5
+  HINTS $ENV{KRB5_DIR} ${KRB5_DIR}
+  PATH_SUFFIXES kerberos krb5)
+set(KRB5_LIBRARIES ${KRB5_LIBRARY})
 
 find_library(KRB5_MIT_LIBRARY NAMES k5crypto
-  /usr/kerberos/lib
-  /usr/krb5/lib 
-  /usr/local/kerberos/lib
-  /usr/lib64 
-  /usr/lib 
-  /usr/local/lib
-  $ENV{KRB5_DIR}/lib
-)
+  HINTS $ENV{KRB5_DIR} ${KRB5_DIR}
+  PATH_SUFFIXES kerberos krb5)
+
 if(KRB5_MIT_LIBRARY)
   set(KRB5_LIBRARIES ${KRB5_LIBRARIES} ${KRB5_MIT_LIBRARY})
 endif()
- 
-find_library(KRB5_LIBRARY NAMES krb5
-  /usr/kerberos/lib
-  /usr/krb5/lib 
-  /usr/local/kerberos/lib
-  /usr/lib64 
-  /usr/lib 
-  /usr/local/lib
-  $ENV{KRB5_DIR}/lib
-)
-if(KRB5_LIBRARY)
-  set(KRB5_LIBRARIES ${KRB5_LIBRARIES} ${KRB5_LIBRARY})
-endif()
+
 
 find_program(KRB5_INIT NAMES kinit
-  /usr/kerberos/bin
-  /usr/krb5/bin 
-  /usr/local/kerberos/bin
-  /usr/bin
-  /usr/local/bin
-  $ENV{KRB5_DIR}/bin
-)
+  HINTS $ENV{KRB5_DIR}/bin ${KRB5_DIR}/bin
+  PATH_SUFFIXES kerberos krb5)
 
-if(KRB5_INCLUDE_DIR AND KRB5_LIBRARIES)
-  set(KRB5_FOUND TRUE)
-endif()
+include(FindPackageHandleStandardArgs)
 
-mark_as_advanced(
-  KRB5_INCLUDE_DIR
-  KRB5_MIT_LIBRARY
-  KRB5_LIBRARY
-  KRB5_INIT
-)
+find_package_handle_standard_args( KRB5 DEFAULT_MSG KRB5_LIBRARY KRB5_INCLUDE_DIR )
+
+mark_as_advanced( KRB5_INCLUDE_DIR KRB5_MIT_LIBRARY KRB5_LIBRARY KRB5_INIT)
+
 
 
