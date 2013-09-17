74f9136c1e2ff08befd9109bc7fa8d9aed7da4a3
Vassil Vassilev <vvasilev@cern.ch>
2013-09-17 16:25:52 +0200

    Instead of removing the FileEntry 'just' invalidate its cache.
    When there is an error introduced by #include-ing a file we should be able to
    hit the disk (in cling's context) the next time. The user could fix it in the
    meanwhile.
    
    Newer clang supports a flag which can be used to mark the user files as volatile.
    This means that the file stat will be invalidated, causing clang to hit the
    disk and fetch the new content of the FileEntry.
    
    The complication comes from the fact that when the file size of the file entry
    is invalidated the cache in the SourceManager is not syncronized, thus clang
    issued an error. The patch in clang checks if the file size == 0 and the
    modification time == 0 this means that we are in cling's context and have to sync
    the cache and continue with no errors.
    
    This fixes an issue exposed by Jerome's implementation. Before we removed the
    entire FileEntry to achieve the same behavior, however the SourceManager kept
    the reference to it. This leads to seg faults when iterating over the included
    files (eg. .files or .storeState)

--- interpreter/llvm/src/tools/clang/lib/Basic/SourceManager.cpp
+++ interpreter/llvm/src/tools/clang/lib/Basic/SourceManager.cpp
@@ -132,6 +132,14 @@ const llvm::MemoryBuffer *ContentCache::getBuffer(DiagnosticsEngine &Diag,
     if (Invalid) *Invalid = true;
     return Buffer.getPointer();
   }
+
+  // If there is no information about the size and the modification time,
+  // this means that we are in cling-like context and we should sync the 
+  // FileEntry and the invalidated cache.
+  if (!ContentsEntry->getSize() && !ContentsEntry->getModificationTime()) {
+     FileManager::modifyFileEntry(const_cast<FileEntry*>(ContentsEntry), 
+                                  getRawBuffer()->getBufferSize(), time(0));
+  }
   
   // Check that the file's size is the same as in the file entry (which may
   // have come from a stat cache).
