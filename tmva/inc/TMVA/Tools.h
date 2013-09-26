073ebb2adf14f23401cec96a635a960229f87455
Helge Voss <Helge.Voss@cern.ch>
2013-09-26 15:46:45 +0200

    increase default xml buffer size to avoid frequent problesm with that.. sorry, don't know yet how to make that easily more flexible

--- tmva/inc/TMVA/Tools.h
+++ tmva/inc/TMVA/Tools.h
@@ -268,7 +268,7 @@ namespace TMVA {
       const char* GetName     ( void* node );
 
       TXMLEngine& xmlengine() { return *fXMLEngine; }
-      int xmlenginebuffersize() { return 1000000; }
+      int xmlenginebuffersize() { return 10000000; }
       TXMLEngine* fXMLEngine;
 
    private:
