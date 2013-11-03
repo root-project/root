29c5e4f7bbf3e050198e891a62328e27a4f2db0d
Philippe Canal <pcanal@fnal.gov>
2013-11-03 09:26:13 -0600

    gccxml and libReflex are gone

--- scripts/Reflex.mk
+++ scripts/Reflex.mk
@@ -13,12 +13,6 @@ else
 GENREFLEX = genreflex
 endif
 
-# We only run these tests if libCintex exists and gccxml is found
-ifeq ($(findstring cintex,$(shell root-config --features)),)
-HAVEGCCXML:="nocintex"
-endif
-HAVEGCCXML?=$(shell which gccxml 2>/dev/null)
-
 # The dictionary:
 %_rflx.cpp: %_selection.xml %.h
 	$(CMDECHO) $(GENREFLEX) $*.h -s $*_selection.xml $(GENREFLEXFLAGS) -I"$(ROOTSYS)/include" $(GENREFLEXCXXFLAGS) > $@.log || (cat $@.log && exit 1)
@@ -26,17 +20,9 @@ HAVEGCCXML?=$(shell which gccxml 2>/dev/null)
 %_cint.cpp: %.h %LinkDef.h
 	$(CMDECHO) rootcint -f $@ -c $^
 
-ifeq ($(R__EXPLICITLINK),yes)
-lib%_dictrflx.$(DllSuf): %_rflx.$(ObjSuf) $(ROOT_LOC)/lib/libReflex.$(LibSuf)
-else
 lib%_dictrflx.$(DllSuf): %_rflx.$(ObjSuf)
-endif
 	$(BuildFromObj)
 
 lib%_dictcint.$(DllSuf): %_cint.$(ObjSuf)
 	$(BuildFromObj)
 
-ifeq ($(findstring gccxml,$(notdir $(HAVEGCCXML))),gccxml)
-
-
-endif
