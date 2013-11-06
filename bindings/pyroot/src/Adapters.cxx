e309210f4f96d46aecd50fb35b39a850bb84dbc5
Wim Lavrijsen <WLavrijsen@lbl.gov>
2013-11-06 15:07:09 -0800

    remove workaround to drop scopes from method names as this is now consistent

--- bindings/pyroot/src/Adapters.cxx
+++ bindings/pyroot/src/Adapters.cxx
@@ -113,17 +113,6 @@ std::string PyROOT::TMemberAdapter::Name( unsigned int mod ) const
    } else if ( mod & Rflx::FINAL )
       return Utility::ResolveTypedef( fMember->GetName() );
 
-// CLING WORKAROUND -- TMethod names are fully scoped ...
-   TMethod* m = (TMethod*)*this;
-   if (m && m->GetClass()) {
-      std::string scoped_name = m->GetName();
-      std::string class_name = m->GetClass()->GetName();
-      std::string::size_type pos = scoped_name.find(class_name + "::");
-      if (pos == 0)      // only accept found at start
-         scoped_name = scoped_name.substr(class_name.size() + 2 /* for :: */, std::string::npos);
-      return scoped_name;
-   }
-
 // CLING WORKAROUND -- should not be null, but can be due #100389
    if ( fMember != 0 )
       return fMember->GetName();
