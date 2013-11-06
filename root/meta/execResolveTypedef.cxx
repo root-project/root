1969745d2fccdfac808af53b3f19d39bd7278833
Philippe Canal <pcanal@fnal.gov>
2013-11-06 11:07:06 -0600

    extend (a tiny bit) the TClassEdit direct test

--- root/meta/execResolveTypedef.cxx
+++ root/meta/execResolveTypedef.cxx
@@ -2,9 +2,11 @@
 #include <TClassEdit.h>
 
 namespace A1 { namespace B2 { namespace C3 { typedef int what; } } }
+namespace NS { typedef int IntNS_t; }
 
 int execResolveTypedef()
 {
    printf("The following should be 'int': %s\n",TClassEdit::ResolveTypedef("A1::B2::C3::what").c_str());
+   printf("The following should be 'const int': %s\n",TClassEdit::ResolveTypedef("const NS::IntNS_t").c_str());
    return 0;
 }
