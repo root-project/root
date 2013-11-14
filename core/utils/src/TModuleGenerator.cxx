93479bb83ce00ac47c8c6773249bfbe05d0a94f9
Danilo Piparo <danilo.piparo@cern.ch>
2013-11-14 08:15:06 +0100

    Fix ROOT-5696 + improvement in header inlining
    Two more warning ignore pragmas were added (the warning are still of course
    shown at dict generation and compilation time, just not at library load time).
    In addition the whole set of warning ignores is inlined only if the header
    is inlined. This should save some Kb on disk and RAM :)

--- core/utils/src/TModuleGenerator.cxx
+++ core/utils/src/TModuleGenerator.cxx
@@ -358,32 +358,36 @@ void TModuleGenerator::WriteRegistrationSource(std::ostream& out, bool inlineHea
                                             std::istreambuf_iterator<char>());
          inlinedHeader += headerFileAsStr;
       }
+      // ROOT-5657
+      // Temporary fix, waiting for the pcms.
+      // Recover old genreflex behaviour, i.e. do not print warnings due to glitches
+      // in the headers at runtime. This is not synonym of ignoring warnings as they
+      // will be printed at dictionary generation time.
+      // In order to do this we leverage the diagnostic pragmas and, since there is no
+      // way to express as a pragma the option "-Wno-deprecated" the
+      // _BACKWARD_BACKWARD_WARNING_H macro, used to avoid to go through
+      // backward/backward_warning.h.
+      // The definition of the preprocessor macro ROOT_DICT_PRINT_WARNINGS re-enables
+      // the warning printing at runtime. It is supposed to be used only for debugging
+      // purposes.
+      payloadCode+="#ifndef ROOT_DICT_PRINT_WARNINGS\n"
+         "#pragma clang diagnostic push\n"
+         "#pragma clang diagnostic ignored \"-Wall\"\n"
+         "#pragma clang diagnostic ignored \"-Wextra\"\n"
+         "#pragma clang diagnostic ignored \"-Wpedantic\"\n"
+         "#pragma clang diagnostic warning \"-Wno-deprecated\"\n"
+         "#pragma clang diagnostic ignored \"-Wdeprecated-register\"\n"
+         "#pragma clang diagnostic ignored \"-Wconstexpr-not-const\"\n"
+         
+         "#define _BACKWARD_BACKWARD_WARNING_H\n"
+         "#endif\n"+
+         inlinedHeader+"\n"
+         "#ifndef ROOT_DICT_PRINT_WARNINGS\n"
+         "#undef  _BACKWARD_BACKWARD_WARNING_H\n"
+         "#pragma clang diagnostic pop\n"
+         "#endif\n";
    }
-   // ROOT-5657
-   // Temporary fix, waiting for the pcms.
-   // Recover old genreflex behaviour, i.e. do not print warnings due to glitches 
-   // in the headers at runtime. This is not synonym of ignoring warnings as they 
-   // will be printed at dictionary generation time.
-   // In order to do this we leverage the diagnostic pragmas and, since there is no 
-   // way to express as a pragma the option "-Wno-deprecated" the 
-   // _BACKWARD_BACKWARD_WARNING_H macro, used to avoid to go through 
-   // backward/backward_warning.h. 
-   // The definition of the preprocessor macro ROOT_DICT_PRINT_WARNINGS re-enables
-   // the warning printing at runtime. It is supposed to be used only for debugging 
-   // purposes.
-   payloadCode+="#ifndef ROOT_DICT_PRINT_WARNINGS\n"
-                "#pragma clang diagnostic push\n"
-                "#pragma clang diagnostic ignored \"-Wall\"\n"
-                "#pragma clang diagnostic ignored \"-Wextra\"\n"
-                "#pragma clang diagnostic ignored \"-Wpedantic\"\n"
-                "#pragma clang diagnostic warning \"-Wno-deprecated\"\n"
-                "#define _BACKWARD_BACKWARD_WARNING_H\n"
-                "#endif\n"+
-                inlinedHeader+"\n"
-                "#ifndef ROOT_DICT_PRINT_WARNINGS\n"
-                "#undef  _BACKWARD_BACKWARD_WARNING_H\n"
-                "#pragma clang diagnostic pop\n"
-                "#endif\n";
+
 
    // Make it usable as string
    ConvertToCppString(payloadCode);
