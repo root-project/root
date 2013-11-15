92a096800ff70fb1645cb6a6736e05ae02271e3f
Dario Berzano <dario.berzano@cern.ch>
2013-11-15 16:50:51 +0100

    TDirectory parse speedup (buffer overflow safe)

--- core/base/inc/TDirectory.h
+++ core/base/inc/TDirectory.h
@@ -200,7 +200,7 @@ public:
    virtual void        WriteKeys() {}
 
    static Bool_t       Cd(const char *path);
-   static void         DecodeNameCycle(const char *namecycle, char *name, Short_t &cycle, Ssiz_t maxlen = 0);
+   static void         DecodeNameCycle(const char *namecycle, char *name, Short_t &cycle, const Ssiz_t maxlen = 0);
    static void         EncodeNameCycle(char *buffer, const char *name, Short_t cycle);
 
    ClassDef(TDirectory,5)  //Describe directory structure in memory
