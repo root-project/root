b7a8e1cbd352de93c52a57105597dfc57c30d6bc
Philippe Canal <pcanal@fnal.gov>
2013-11-19 21:29:13 -0600

    Revert "Increment version number of TStreamerSTL"
    This reverts commit b0671ac807e0e4df505851c9cac65456dcdb47e7.
    
    Conflicts:
    	core/meta/src/TStreamerElement.cxx

--- core/meta/src/TStreamerElement.cxx
+++ core/meta/src/TStreamerElement.cxx
@@ -1844,17 +1844,15 @@ void TStreamerSTL::Streamer(TBuffer &R__b)
          R__b >> fCtype;
          R__b.CheckByteCount(R__s, R__c, TStreamerSTL::IsA());
       }
-      if (R__v < 4) {
-         if (fSTLtype == ROOT::kSTLmultimap || fSTLtype == ROOT::kSTLset) {
-            // For a long time those where inverted in TStreamerElement
-            // compared to the other definitions.  When we moved to version '4',
-            // this got standardized, but we now need to fix it.
-
-            if (fTypeName.BeginsWith("std::set") || fTypeName.BeginsWith("set")) {
-               fSTLtype = ROOT::kSTLset;
-            } else if (fTypeName.BeginsWith("std::multimap") || fTypeName.BeginsWith("multimap")) {
-               fSTLtype = ROOT::kSTLmultimap;
-            }
+      if (fSTLtype == ROOT::kSTLmultimap || fSTLtype == ROOT::kSTLset) {
+         // For a long time those where inverted in TStreamerElement
+         // compared to the other definitions.  When we moved to version '4',
+         // this got standardized, but we now need to fix it.
+         
+         if (fTypeName.BeginsWith("std::set") || fTypeName.BeginsWith("set")) {
+            fSTLtype = ROOT::kSTLset;
+         } else if (fTypeName.BeginsWith("std::multimap") || fTypeName.BeginsWith("multimap")) {
+            fSTLtype = ROOT::kSTLmultimap;
          }
       }
 
