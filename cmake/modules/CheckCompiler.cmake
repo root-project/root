4d92f818a1d05a2ac7ccd6299e50a84601215934
Pere Mato <mato@cern.ch>
2013-11-22 12:00:28 +0100

    Adding new option 'fortran' to enable disable the fortran components (CMake). Issue ROOT-5668

--- cmake/modules/CheckCompiler.cmake
+++ cmake/modules/CheckCompiler.cmake
@@ -2,8 +2,8 @@
 #  CheckCompiler.cmake
 #---------------------------------------------------------------------------------------------------
 
-#---Enable FORTRAN (unfortunatelly is not nowt possible in all cases)-------------------------------
-if(NOT WIN32 AND NOT CMAKE_GENERATOR STREQUAL Xcode AND NOT CMAKE_GENERATOR STREQUAL Ninja)
+#---Enable FORTRAN (unfortunatelly is not not possible in all cases)-------------------------------
+if(fortran AND NOT WIN32 AND NOT CMAKE_GENERATOR STREQUAL Xcode AND NOT CMAKE_GENERATOR STREQUAL Ninja)
   #--Work-around for CMake issue 0009220
   if(DEFINED CMAKE_Fortran_COMPILER AND CMAKE_Fortran_COMPILER MATCHES "^$")
     set(CMAKE_Fortran_COMPILER CMAKE_Fortran_COMPILER-NOTFOUND)
