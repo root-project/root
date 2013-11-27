0e467da8654c20d1876ca253050f4a071dbfffe9
Pere Mato <mato@cern.ch>
2013-11-27 19:52:19 +0100

    Fixes for ROOT-5755 (CMake)

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -35,8 +35,8 @@ endif()
 
 if(soversion)
   set(ROOT_LIBRARY_PROPERTIES ${ROOT_LIBRARY_PROPERTIES}
-      VERSION ${ROOT_VERSION}
-      SOVERSION ${ROOT_MAJOR_VERSION}
+      VERSION ${ROOT_MAJOR_VERSION}.${ROOT_MINOR_VERSION}
+      #SOVERSION ${ROOT_MAJOR_VERSION}
       SUFFIX ${libsuffix}
       PREFIX ${libprefix} )
 else()
