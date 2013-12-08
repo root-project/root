0196f3b94fa31057864fc146b68c20aaedc846e6
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-12-08 13:14:34 +0100

    check return value of setresuid(). Fixes ROOT-5816.

--- net/rpdutils/src/rpdutils.cxx
+++ net/rpdutils/src/rpdutils.cxx
@@ -1520,7 +1520,8 @@ int RpdCheckOffSet(int Sec, const char *User, const char *Host, int RemId,
 
       // Reset original IDs
       if (getuid() != fromUid)
-         setresuid(fromUid,fromEUid,pw->pw_uid);
+         if (setresuid(fromUid, fromEUid, pw->pw_uid) == -1)
+            goodOfs = 0;
 
    } else {
       // Since we could not set the user IDs, we will
