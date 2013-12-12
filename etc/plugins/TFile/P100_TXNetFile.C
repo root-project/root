b1f8c20a5818b4ddb10c06ea3ea8ff9b7160f94f
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-12-13 00:13:49 +0100

    correction in libNetxNG library name.

--- etc/plugins/TFile/P100_TXNetFile.C
+++ etc/plugins/TFile/P100_TXNetFile.C
@@ -2,7 +2,7 @@ void P100_TXNetFile()
 {
    if (!gEnv->GetValue( "XNet.UseOldClient", 0)) {
       gPluginMgr->AddHandler("TFile", "^[x]?root:", "TNetXNGFile",
-         "NetXNG", "TNetXNGFile(const char*,Option_t*,const char*,Int_t,Int_t,Bool_t)");
+         "NetxNG", "TNetXNGFile(const char*,Option_t*,const char*,Int_t,Int_t,Bool_t)");
    } else {
       gPluginMgr->AddHandler("TFile", "^[x]?root:", "TXNetFile",
          "Netx", "TXNetFile(const char*,Option_t*,const char*,Int_t,Int_t,Bool_t)");
--- etc/plugins/TSystem/P040_TXNetSystem.C
+++ etc/plugins/TSystem/P040_TXNetSystem.C
@@ -2,7 +2,7 @@ void P040_TXNetSystem()
 {
    if (!gEnv->GetValue("XNet.UseOldClient", 0)) {
       gPluginMgr->AddHandler("TSystem", "^[x]?root:", "TNetXNGSystem",
-         "NetXNG", "TNetXNGSystem(const char *,Bool_t)");
+         "NetxNG", "TNetXNGSystem(const char *,Bool_t)");
    } else {
       gPluginMgr->AddHandler("TSystem", "^[x]?root:", "TXNetSystem",
          "Netx", "TXNetSystem(const char *,Bool_t)");
