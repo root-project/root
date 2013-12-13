e311202171f952e636b90361f1f8f15b7c21ed4c
Vassil Vassilev <vvasilev@cern.ch>
2013-12-13 09:29:02 +0100

    Add comments.

--- root/meta/autoloading/runMissingEntriesROOT-5759.C
+++ root/meta/autoloading/runMissingEntriesROOT-5759.C
@@ -1,4 +1,6 @@
 {
-gROOT->ProcessLine( "#include \"does_not_exist.h\"" );
-gROOT->ProcessLine("cool d;");
+   // Try to autoload a non existing class. The autoloading should not try
+   // crash the system. A reproducer of ROOT-5759.
+   gROOT->ProcessLine( "#include \"does_not_exist.h\"" );
+   gROOT->ProcessLine("cool d;");
 }
