0115368a8c024e4dae8e8f219409d5f25288072c
Philippe Canal <pcanal@fnal.gov>
2013-12-16 18:03:22 -0600

    Add test of ROOT-5857 (failing to update TClass for template of Double32_t

---
+++ root/meta/tclass/execTmpltD.cxx
@@ -0,0 +1,20 @@
+#include "tmplt.h"
+
+class Holder {
+public:
+  Wrapper<double> fValue;
+  Wrapper<long long> fIntValue;
+  ClassDef(Holder,3);
+};
+
+#ifdef __ROOTCLING__
+#pragma link C++ class Wrapper<double>+;
+#pragma link C++ class Wrapper<long long>+;
+#endif
+
+int execTmpltD()
+{
+   writeFile<Holder>("tmpltd.root");
+   return 0;
+}
+
---
+++ root/meta/tclass/execTmpltD32.cxx
@@ -0,0 +1,19 @@
+#include "tmplt.h"
+
+class Holder {
+public:
+  Wrapper<Double32_t> fValue;
+
+  ClassDef(Holder,4);
+};
+
+#ifdef __ROOTCLING__
+#pragma link C++ class Wrapper<Double32_t>+;
+#endif
+
+int execTmpltD32()
+{
+   writeFile<Holder>("tmpltd32.root");
+   return 0;
+}
+
