8141ba31af2d81f22440b68b63da72bcb4d2305d
Danilo Piparo <danilo.piparo@cern.ch>
2014-01-06 11:30:32 +0100

    Test for the fix of ROOT-5881

---
+++ root/meta/genreflex/ROOT-5881/execstringIO.C
@@ -0,0 +1,46 @@
+#include "TFile.h"
+#include "TTree.h"
+#include <string>
+#include <iostream>
+
+int readTree(){
+
+  TFile myFile("stringTree.root");
+  TTree* myTree = (TTree*) myFile.GetObjectChecked("stringTree","TTree");
+  if (!myTree) return 1;
+  std::string* myString = new std::string();
+  myTree->SetBranchAddress("myString", &myString);
+
+  for (int i=0;i<myTree->GetEntries();++i){
+    myTree->GetEntry(i);
+    std::cout << "Entry " << i << " " << *myString << std::endl;
+    }
+  myFile.Close();
+  return 0;
+  
+}
+
+int writeTree(){
+
+  const std::string myStringTempl("myString_");
+  std::string myString;
+
+  TFile ofile("stringTree.root","RECREATE");
+  TTree theTree("stringTree","stringTree");
+  theTree.Branch("myString",&myString);
+
+  for (int i=0;i<100;i++){
+    myString=myStringTempl+std::to_string(i);
+    theTree.Fill();
+    }
+  theTree.Write();
+  ofile.Close();
+  return 0;
+}
+
+int execstringIO(){
+ int writeRet = writeTree();
+ int readRet = readTree();
+ return writeRet + readRet;
+ 
+}
