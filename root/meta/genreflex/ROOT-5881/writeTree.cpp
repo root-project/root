3b021580bbf5ebf52515955b19cde1e7e3bb91fb
Danilo Piparo <danilo.piparo@cern.ch>
2014-01-06 17:44:36 +0100

    Properly Reproduce the 5881 issue
    an executble linked to the library of the dictionaries is needed
    to reproduce the issue.
    If not, the ROOT string dictionary is always the first to be loaded.

---
+++ root/meta/genreflex/ROOT-5881/writeTree.cpp
@@ -0,0 +1,25 @@
+#include "TFile.h"
+#include "TTree.h"
+
+#include <string>
+
+void writeTree(){
+
+  const std::string myStringTempl("myString_");
+  std::string myString;
+
+  TFile ofile("stringTree.root","RECREATE");
+  TTree theTree("stringTree","stringTree");
+  theTree.Branch("myString",&myString);
+
+  for (int i=0;i<10;i++){
+    myString=myStringTempl+std::to_string(i);
+    theTree.Fill();
+    }
+  theTree.Write();
+  ofile.Close();
+}
+
+int main(){
+ writeTree();
+}
