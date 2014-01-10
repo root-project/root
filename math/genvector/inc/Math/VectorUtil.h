e52e8734a4431dea6a559d9f3fe77b67659784df
Axel Naumann <Axel.Naumann@cern.ch>
2014-01-10 17:17:50 +0100

    Trigger rebuild of dictionary after removal of VectorUtil_Cint.h.

--- math/genvector/inc/Math/VectorUtil.h
+++ math/genvector/inc/Math/VectorUtil.h
@@ -1,8 +1,8 @@
 // @(#)root/mathcore:$Id$
-// Authors: W. Brown, M. Fischler, L. Moneta    2005  
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
 
-#ifndef ROOT_Math_VectorUtil 
-#define ROOT_Math_VectorUtil 
+#ifndef ROOT_Math_VectorUtil
+#define ROOT_Math_VectorUtil
 
 
 #include "Math/GenVector/VectorUtil.h"
