dd25696fd9723809e9e128e3626dcb3841d7e0c0
Olivier Couet <olivier.couet@cern.ch>
2014-01-14 15:06:27 +0100

    Fix coverity report 50810

--- graf2d/postscript/src/TPostScript.cxx
+++ graf2d/postscript/src/TPostScript.cxx
@@ -2834,6 +2834,10 @@ void TPostScript::Text(Double_t xx, Double_t yy, const wchar_t *chars)
 
       // Embed the fonts at the right place
       FILE *sg = fopen(tmpname.Data(),"r");
+      if (sg == 0) {
+         Error("Text", "Cannot open file: %s\n", tmpname.Data());
+         return;
+      }
       char line[255];
       while (fgets(line,255,sg)) {
          if (strstr(line,"EndComments")) PrintStr("%%DocumentNeededResources: ProcSet (FontSetInit)@");
