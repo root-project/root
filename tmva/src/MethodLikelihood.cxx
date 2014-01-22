4c19f12b6a0a15dabfcf48721ce9ee6145de4fab
Helge Voss <Helge.Voss@cern.ch>
2014-01-22 14:13:50 +0100

    ok. yet another initialisation-order warning

--- tmva/src/MethodLikelihood.cxx
+++ tmva/src/MethodLikelihood.cxx
@@ -173,9 +173,9 @@ TMVA::MethodLikelihood::MethodLikelihood( DataSetInfo& theData,
    fNsmooth       ( 2 ),
    fNsmoothVarS   ( 0 ),
    fNsmoothVarB   ( 0 ),
+   fAverageEvtPerBin( 0 ),
    fAverageEvtPerBinVarS (0), 
    fAverageEvtPerBinVarB (0),
-   fAverageEvtPerBin( 0 ),
    fKDEfineFactor ( 0 ),
    fInterpolateString(0)
 {
