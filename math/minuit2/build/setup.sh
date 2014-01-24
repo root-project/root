bd7327cd8821000a167effd23560054b13873457
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2014-01-24 17:32:57 +0100

    More fixes for Minuit2 builds

--- math/minuit2/build/setup.sh
+++ math/minuit2/build/setup.sh
@@ -17,7 +17,18 @@ cp -p ../mathcore/inc/Math/Functor.h inc/Math/.
 cp -p ../mathcore/inc/Math/IFunction.h inc/Math/.
 cp -p ../mathcore/inc/Math/IFunctionfwd.h inc/Math/.
 cp -p ../mathcore/inc/Math/Minimizer.h inc/Math/.
+cp -p ../mathcore/inc/Math/FitMethodFunction.h inc/Math/.
 cp -p ../mathcore/inc/Math/WrappedFunction.h inc/Math/.
+cp -p ../mathcore/inc/Math/MinimizerOptions.h inc/Math/.
+cp -p ../mathcore/inc/Math/IOptions.h inc/Math/.
+cp -p ../mathcore/inc/Math/Error.h inc/Math/.
+cp -p ../mathcore/inc/Math/GenAlgoOptions.h inc/Math/.
+cp -p ../mathcore/src/MinimizerOptions.cxx src/.
+cp -p ../mathcore/src/GenAlgoOptions.cxx src/.
+
+#hack Minimizer options to define a macro
+sed -i '1s/^/#define MATH_NO_PLUGIN_MANAGER\n/' src/MinimizerOptions.cxx 
+
 cp -p build/inc_Math_Makefile.am inc/Math/Makefile.am
 
 ./autogen
