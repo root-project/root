c33446d73569d34a51938e284c56aef61d954437
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2014-01-27 14:04:53 +0100

    Set the correct option when selecting plugin for OpenGL manager.

--- gui/gui/src/TRootEmbeddedCanvas.cxx
+++ gui/gui/src/TRootEmbeddedCanvas.cxx
@@ -119,6 +119,8 @@ TRootEmbeddedCanvas::TRootEmbeddedCanvas(const char *name, const TGWindow *p,
          TString x = "win32";
          if (gVirtualX->InheritsFrom("TGX11"))
             x = "x11";
+         else if (gVirtualX->InheritsFrom("TGCocoa"))
+            x = "osx";
 
          TPluginHandler *ph = gROOT->GetPluginManager()->FindHandler("TGLManager", x);
 
