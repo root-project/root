0f37665c4c5e5fa85edcb6ad76ed8504a8997cfe
agheata <Andrei.Gheata@cern.ch>
2014-01-29 10:47:25 +0100

    fixed tutorial macros (cherry picked from commit b9de4fa167857101cb7914324325bfdabbab9474)

--- tutorials/geom/geomAlice.C
+++ tutorials/geom/geomAlice.C
@@ -7,28 +7,40 @@
       
 void geomAlice()
 {
-   TGeoManager::Import("http://root.cern.ch/files/alice.root");
-   //gGeoManager->DefaultColors();
-   gGeoManager->GetVolume("HUFL")->InvisibleAll();
-   gGeoManager->GetVolume("HUWA")->InvisibleAll();
-   gGeoManager->GetVolume("ITSV")->InvisibleAll();
-   gGeoManager->GetVolume("ZDC")->InvisibleAll();
-   gGeoManager->GetVolume("ZEM")->InvisibleAll();
-   gGeoManager->GetVolume("XEN1")->InvisibleAll();
-   gGeoManager->GetVolume("HBW1")->InvisibleAll();
-   gGeoManager->GetVolume("HHW1")->InvisibleAll();
-   gGeoManager->GetVolume("HHW3")->InvisibleAll();
-   gGeoManager->GetVolume("HHW2")->InvisibleAll();
-   gGeoManager->GetVolume("HHF1")->InvisibleAll();
-   gGeoManager->GetVolume("HHF2")->InvisibleAll();
-   gGeoManager->GetVolume("HPIL")->InvisibleAll();
-   gGeoManager->GetVolume("HMBS")->InvisibleAll();
-   gGeoManager->GetVolume("HHC1")->InvisibleAll();
+   TGeoManager::Import("http://root.cern.ch/files/alice2.root");
+   gGeoManager->DefaultColors();
+//   gGeoManager->SetVisLevel(4);
+   gGeoManager->GetVolume("HALL")->InvisibleAll();
+   gGeoManager->GetVolume("ZDCC")->InvisibleAll();
+   gGeoManager->GetVolume("ZDCA")->InvisibleAll();
    gGeoManager->GetVolume("L3MO")->InvisibleAll();
-   gGeoManager->GetVolume("DY1")->SetTransparency(90);
-   gGeoManager->GetVolume("DY2")->SetTransparency(90);
-   gGeoManager->GetVolume("DY11")->SetTransparency(70);
-   gGeoManager->GetVolume("DY22")->SetTransparency(70);
+   gGeoManager->GetVolume("YOUT1")->InvisibleAll();
+   gGeoManager->GetVolume("YOUT2")->InvisibleAll();
+   gGeoManager->GetVolume("YSAA")->InvisibleAll();
+   gGeoManager->GetVolume("RB24")->InvisibleAll();
+   gGeoManager->GetVolume("RB26Pipe")->InvisibleAll();
+   gGeoManager->GetVolume("DDIP")->InvisibleAll();
+   gGeoManager->GetVolume("DCM0")->InvisibleAll();
+//   gGeoManager->GetVolume("PPRD")->InvisibleAll();
+   gGeoManager->GetVolume("BRS1")->InvisibleAll();
+   gGeoManager->GetVolume("BRS4")->InvisibleAll();
+//   gGeoManager->GetVolume("Dipole")->InvisibleAll();
+   gGeoManager->GetVolume("ZN1")->InvisibleAll();
+   gGeoManager->GetVolume("Q13T")->InvisibleAll();
+   gGeoManager->GetVolume("ZP1")->InvisibleAll();
+   gGeoManager->GetVolume("QTD1")->InvisibleAll();
+   gGeoManager->GetVolume("QTD2")->InvisibleAll();
+   gGeoManager->GetVolume("QBS7")->InvisibleAll();
+   gGeoManager->GetVolume("QA07")->InvisibleAll();
+   gGeoManager->GetVolume("MD1V")->InvisibleAll();
+   gGeoManager->GetVolume("QTD3")->InvisibleAll();
+   gGeoManager->GetVolume("QTD4")->InvisibleAll();
+   gGeoManager->GetVolume("QTD5")->InvisibleAll();
+   gGeoManager->GetVolume("QBS3")->InvisibleAll();
+   gGeoManager->GetVolume("QBS4")->InvisibleAll();
+   gGeoManager->GetVolume("QBS5")->InvisibleAll();
+   gGeoManager->GetVolume("QBS6")->InvisibleAll();
+   
    gGeoManager->GetVolume("ALIC")->Draw("ogl");
    new TBrowser;
 }
--- tutorials/geom/geomAlice_itsv.C
+++ tutorials/geom/geomAlice_itsv.C
@@ -6,21 +6,8 @@
 // Author: Rene Brun
       
 void geomAlice_itsv() {
-   TGeoManager::Import("http://root.cern.ch/files/alice.root");
-   //gGeoManager->DefaultColors();
-   gGeoManager->GetVolume("IT56")->InvisibleAll();
-   gGeoManager->GetVolume("I018")->InvisibleAll();
-   gGeoManager->GetVolume("I090")->InvisibleAll();
-   gGeoManager->GetVolume("I093")->InvisibleAll();
-   gGeoManager->GetVolume("I099")->InvisibleAll();
-   gGeoManager->GetVolume("I200")->InvisibleAll();
-   gGeoManager->GetVolume("IC01")->InvisibleAll();
-   gGeoManager->GetVolume("IC02")->InvisibleAll();
-   gGeoManager->GetVolume("I651")->InvisibleAll();
-   gGeoManager->GetVolume("ICY1")->SetTransparency(90);
-   gGeoManager->GetVolume("ICY2")->SetTransparency(90);
-   gGeoManager->GetVolume("I215")->SetTransparency(50);
-   gGeoManager->GetVolume("I212")->SetTransparency(50);
+   TGeoManager::Import("http://root.cern.ch/files/alice2.root");
+   gGeoManager->DefaultColors();
    gGeoManager->GetVolume("ITSV")->Draw("ogl");
    new TBrowser;
 }
