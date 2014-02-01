854bf880b5384ad3955909ab07f90dea2892cbb4
Christopher Jones <cdj@fnal.gov>
2014-02-01 16:53:10 +0100

    Protect access to TROOT::GetListOfFiles()
    When new files are opened or closed, gROOT->GetListOfFiles() is updated.
    If the opening/closing happens on different threads then we need to
    serialize all access to that list. This changes uses gROOTMutex to
    serialize those accesses.

--- io/io/src/TFile.cxx
+++ io/io/src/TFile.cxx
@@ -4580,6 +4580,7 @@ TFile::EAsyncOpenStatus TFile::GetAsyncOpenStatus(const char* name)
    }
 
    // Check also the list of files open
+   R__LOCKGUARD2(gROOTMutex);
    TSeqCollection *of = gROOT->GetListOfFiles();
    if (of && (of->GetSize() > 0)) {
       TIter nxf(of);
@@ -4626,6 +4627,7 @@ const TUrl *TFile::GetEndpointUrl(const char* name)
    }
 
    // Check also the list of files open
+   R__LOCKGUARD2(gROOTMutex);
    TSeqCollection *of = gROOT->GetListOfFiles();
    if (of && (of->GetSize() > 0)) {
       TIter nxf(of);
