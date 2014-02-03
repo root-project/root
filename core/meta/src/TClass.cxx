ae1d5e8e74337fe49b2ec65e12e1f270341518d2
Christopher Jones <cdj@fnal.gov>
2014-02-03 23:12:33 +0100

    Use gCINTMutex to protect access to TBaseClass::Property
    Helgrind found a race condition involving TClass::GetBaseClassOffsetRecurse
    calling TBaseClass::Property.

--- core/meta/src/TClass.cxx
+++ core/meta/src/TClass.cxx
@@ -2354,8 +2354,11 @@ Int_t TClass::GetBaseClassOffsetRecurse(const TClass *cl)
       c = inh->GetClassPointer(kTRUE); // kFALSE);
       if (c) {
          if (cl == c) {
-            if ((inh->Property() & G__BIT_ISVIRTUALBASE) != 0)
-               return -2;
+	    {
+               R__LOCKGUARD(gCINTMutex);
+               if ((inh->Property() & G__BIT_ISVIRTUALBASE) != 0)
+                  return -2;
+	    }
             return inh->GetDelta();
          }
          off = c->GetBaseClassOffsetRecurse(cl);
