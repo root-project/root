6acd22ccc7cf488aad0b62122d7e731d6a52b089
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2014-02-05 15:14:45 +0100

    Make TH1::Copy public , to be able to do deep copy of TH1 pointers without using TH1::Clone

--- hist/hist/inc/TH1.h
+++ hist/hist/inc/TH1.h
@@ -144,7 +144,6 @@ protected:
    TH1(const char *name,const char *title,Int_t nbinsx,Double_t xlow,Double_t xup);
    TH1(const char *name,const char *title,Int_t nbinsx,const Float_t *xbins);
    TH1(const char *name,const char *title,Int_t nbinsx,const Double_t *xbins);
-   virtual void     Copy(TObject &hnew) const;
    virtual Int_t    BufferFill(Double_t x, Double_t w);
    virtual Bool_t   FindNewAxisLimits(const TAxis* axis, const Double_t point, Double_t& newMin, Double_t &newMax);
    virtual void     SavePrimitiveHelp(std::ostream &out, const char *hname, Option_t *option = "");
@@ -200,6 +199,7 @@ public:
    virtual Double_t Chisquare(TF1 * f1, Option_t *option = "") const;
    virtual void     ClearUnderflowAndOverflow();
    virtual Double_t ComputeIntegral(Bool_t onlyPositive = false);
+   virtual void     Copy(TObject &hnew) const;
    virtual void     DirectoryAutoAdd(TDirectory *);
    virtual Int_t    DistancetoPrimitive(Int_t px, Int_t py);
    virtual Bool_t   Divide(TF1 *f1, Double_t c1=1);
