c6ae571c1da2af8b6ede16b0ea39a9d522b46aa8
Philippe Canal <pcanal@fnal.gov>
2014-02-13 10:21:47 -0600

    white space
    Conflicts:
    	root/io/datamodelevolution/misc/writeTree.C

--- root/io/datamodelevolution/misc/writeTree.C
+++ root/io/datamodelevolution/misc/writeTree.C
@@ -3,26 +3,26 @@ void writeTree(const char* filename = "treeTest.root")
 {
 #ifndef ClingWorkAroundMissingDynamicScope
    gROOT->ProcessLine(".L MyClass.cxx+");
-	gROOT->ProcessLine(".L SomeClass.cxx+");
+   gROOT->ProcessLine(".L SomeClass.cxx+");
 #endif
 
-	TFile file(filename, "RECREATE");
-	TTree tree("testtree", "Tree with test objects");
-	MyClass* myobj = new MyClass;
-	tree.Branch("myObjects", &myobj);
+   TFile file(filename, "RECREATE");
+   TTree tree("testtree", "Tree with test objects");
+   MyClass* myobj = new MyClass;
+   tree.Branch("myObjects", &myobj);
 	
-	myobj->Add(new TNamed("aaa", "AAAAAAAA"));
-	myobj->Add(new SomeClass("bbb"));
+   myobj->Add(new TNamed("aaa", "AAAAAAAA"));
+   myobj->Add(new SomeClass("bbb"));
    myobj->Add((TObject*)1);
-	myobj->Add(new TNamed("ccc", "CCCCCCCC"));
-	myobj->Add(new SomeClass("ddd"));
+   myobj->Add(new TNamed("ccc", "CCCCCCCC"));
+   myobj->Add(new SomeClass("ddd"));
    myobj->Array()[2] = 0;
    //gDebug = 7;
-	tree.Fill();
+   tree.Fill();
    //gDebug = 0;
    printf("Number of array elements: %d\n",myobj->Array().GetEntriesFast());
    // myobj->Array().ls();
-	myobj->Print();
+   myobj->Print();
 	
-	file.Write();
+   file.Write();
 }
