ef5aea58380b0ce4ed6dc59443c26891fe5a39a6
Christopher Jones <cdj@fnal.gov>
2014-02-13 18:12:30 +0100

    Thread safety fixes for TUnixSystem
    TSystem changed the last error string to be a class thread_local
    so this code matches that change.
    Caching of external path is now a thread_local to avoid race conditions.
    Caching of initial time is done via an atomic.

--- core/unix/src/TUnixSystem.cxx
+++ core/unix/src/TUnixSystem.cxx
@@ -37,6 +37,9 @@
 #include "TVirtualMutex.h"
 #include "TObjArray.h"
 #include <map>
+#if __cplusplus > 199711L
+#include <atomic>
+#endif
 
 //#define G__OLDEXPAND
 
@@ -439,7 +442,11 @@ static void SigHandler(ESignals sig)
 //______________________________________________________________________________
 static const char *GetExePath()
 {
+#if __cplusplus > 199711L
+   static thread_local TString exepath;
+#else
    static TString exepath;
+#endif
    if (exepath == "") {
 #if defined(R__MACOSX)
       exepath = _dyld_get_image_name(0);
@@ -583,6 +590,11 @@ static void DylibAdded(const struct mach_header *mh, intptr_t /* vmaddr_slide */
 }
 #endif
 
+#if __cplusplus > 199711L
+#define LAST_ERROR_STRING fgLastErrorString
+#else
+#define LAST_ERROR_STRING fLastErrorString
+#endif
 
 ClassImp(TUnixSystem)
 
@@ -728,8 +740,9 @@ const char *TUnixSystem::GetError()
    // Return system error string.
 
    Int_t err = GetErrno();
-   if (err == 0 && fLastErrorString != "")
-      return fLastErrorString;
+   if (err == 0 && LAST_ERROR_STRING != "")
+      return LAST_ERROR_STRING;
+
 #if defined(R__SOLARIS) || defined (R__LINUX) || defined(R__AIX) || \
     defined(R__FBSD) || defined(R__OBSD) || defined(R__HURD)
    return strerror(err);
@@ -1583,7 +1596,8 @@ Bool_t TUnixSystem::AccessPathName(const char *path, EAccessMode mode)
 
    if (::access(StripOffProto(path, "file:"), mode) == 0)
       return kFALSE;
-   fLastErrorString = GetError();
+   LAST_ERROR_STRING = GetError();
+
    return kTRUE;
 }
 
@@ -1630,7 +1644,7 @@ int TUnixSystem::Rename(const char *f, const char *t)
    // Rename a file. Returns 0 when successful, -1 in case of failure.
 
    int ret = ::rename(f, t);
-   fLastErrorString = GetError();
+   LAST_ERROR_STRING = GetError();
    return ret;
 }
 
@@ -1832,7 +1846,7 @@ needshell:
       } else {
          hd = UnixHomedirectory(0);
          if (hd == 0) {
-            fLastErrorString = GetError();
+            LAST_ERROR_STRING = GetError();
             return kTRUE;
          }
          cmd += hd;
@@ -1842,7 +1856,7 @@ needshell:
       cmd += stuffedPat;
 
    if ((pf = ::popen(cmd.Data(), "r")) == 0) {
-      fLastErrorString = GetError();
+      LAST_ERROR_STRING = GetError();
       return kTRUE;
    }
 
@@ -1865,7 +1879,7 @@ again:
    while (ch != EOF) {
       ch = fgetc(pf);
       if (ch == ' ' || ch == '\t') {
-         fLastErrorString = "expression ambigous";
+         LAST_ERROR_STRING = "expression ambigous";
          ::pclose(pf);
          return kTRUE;
       }
@@ -3776,8 +3790,13 @@ void TUnixSystem::UnixIgnoreSignal(ESignals sig, Bool_t ignr)
    // If ignr is true ignore the specified signal, else restore previous
    // behaviour.
 
+#if __cplusplus > 199711L
+   static thread_local Bool_t ignoreSig[kMAXSIGNALS] = { kFALSE };
+   static thread_local struct sigaction oldsigact[kMAXSIGNALS];
+#else
    static Bool_t ignoreSig[kMAXSIGNALS] = { kFALSE };
    static struct sigaction oldsigact[kMAXSIGNALS];
+#endif
 
    if (ignr != ignoreSig[sig]) {
       ignoreSig[sig] = ignr;
@@ -3881,7 +3900,11 @@ Long64_t TUnixSystem::UnixNow()
 {
    // Get current time in milliseconds since 0:00 Jan 1 1995.
 
+#if __cplusplus > 199711L
+   static std::atomic<time_t> jan95{0};
+#else
    static time_t jan95 = 0;
+#endif
    if (!jan95) {
       struct tm tp;
       tp.tm_year  = 95;
