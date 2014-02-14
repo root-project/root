bba6aff8010c1a073d913b1b7d68f294dd519b40
Olivier Couet <olivier.couet@cern.ch>
2014-02-14 17:54:14 +0100

    New example

---
+++ tutorials/graphics/diamond.C
@@ -0,0 +1,11 @@
+//Draw a diamond
+//Author: Olivier Couet
+{
+   TCanvas *c = new TCanvas("c");
+   TDiamond *d = new TDiamond(.05,.1,.95,.8);
+
+   d->AddText("A TDiamond can contain any text.");
+
+   d->Draw();
+   return c;
+} 
