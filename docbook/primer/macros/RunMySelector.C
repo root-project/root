a430b3752542e29092f4efe8ce9520e713276cc2
Fons Rademakers <Fons.Rademakers@cern.ch>
2014-02-25 10:35:36 +0100

    remove docbook directory with old docbook xml version of the users guide.

--- docbook/primer/macros/RunMySelector.C
+++
@@ -1,7 +0,0 @@
-{// set up a TChain
-TChain *ch=new TChain("cond_data", "My Chain for Example N-Tuple");
- ch->Add("conductivity_experiment*.root");
-// eventually, start Proof Lite on cores
-TProof::Open("workers=4");
-ch->SetProof();
-ch->Process("MySelector.C+");}
