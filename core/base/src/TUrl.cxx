root-project/root

commit 728b1579974ecc310e0c963e74f77bf2ebe8be90

Handle URL's like: http://host?opt=1. Fixes ROOT-5915.
Also add new method HasOption(const char *key) which returns true if the
specified key appears in the URL option lists. In the above URL
HasOption("opt") returns true.

core/base/src/TUrl.cxx
@@ -1,4 +1,4 @@
-// @(#)root/base:$Id$
+// @(#)root/base:$Id: a94734bd8505d0a8f1833346da666b196a797388 $
 // Author: Fons Rademakers   17/01/97
 
 /*************************************************************************
@@ -111,7 +111,7 @@ void TUrl::SetUrl(const char *url, Bool_t defaultIsFile)
 
    fOptionsMap = 0;
 
-   if (!url || !strlen(url)) {
+   if (!url || !url[0]) {
       fPort = -1;
       return;
    }
@@ -178,7 +178,7 @@ tryfile:
 
    u = u0;
 
-   char *t, *s2;
+   char *x, *t, *s2;
    // allow x:/path as Windows filename
    if ((s = strstr(u, ":/")) && u+1 != s) {
       if (*(s+2) != '/') {
@@ -207,7 +207,9 @@ tryfile:
    u = s;
    t = s;
 again:
-   if ((s = strchr(t, '@')) && (s < strchr(t, '/') || !strchr(t, '/'))) {
+   //if ((s = strchr(t, '@')) && (s < strchr(t, '/') || !strchr(t, '/'))) {
+   if ((s = strchr(t, '@')) && (((x = strchr(t, '/')) && s < x) ||
+       ((x = strchr(t, '?')) && s < x) || ((x = strchr(t, '#')) && s < x))) {
       if (*(s-1) == '\\') {
          t = s+1;
          goto again;
@@ -232,7 +234,7 @@ again:
 
    // Find host
    u = s;
-   if ((s = strchr(u, ':')) || (s = strchr(u, '/'))) {
+   if ((s = strchr(u, ':')) || (s = strchr(u, '/')) || (s = strchr(u, '?')) || (s = strchr(u, '#'))) {
       if ((strchr (u, ':') > strchr(u, '/')) && (strchr (u, '/')))
          s = strchr(u, '/');
       sav = *s;
@@ -247,7 +249,7 @@ again:
             goto cleanup;
          }
          u = s;
-         if ((s = strchr(u, '/'))) {
+         if ((s = strchr(u, '/')) || (s = strchr(u, '?')) || (s = strchr(u, '#'))) {
             sav = *s;
             *s = 0;
             fPort = atoi(u);
@@ -619,14 +621,17 @@ void TUrl::ParseOptions() const
    for (Int_t n = 0; n < objOptions->GetEntries(); n++) {
       TString loption = ((TObjString *) objOptions->At(n))->GetName();
       TObjArray *objTags = loption.Tokenize("=");
-         if (objTags->GetEntries() == 2) {
+      if (!fOptionsMap) {
+         fOptionsMap = new TMap;
+         fOptionsMap->SetOwnerKeyValue();
+      }
+      if (objTags->GetEntries() == 2) {
          TString key = ((TObjString *) objTags->At(0))->GetName();
          TString value = ((TObjString *) objTags->At(1))->GetName();
-         if (!fOptionsMap) {
-            fOptionsMap = new TMap;
-            fOptionsMap->SetOwnerKeyValue();
-         }
          fOptionsMap->Add(new TObjString(key), new TObjString(value));
+      } else {
+         TString key = ((TObjString *) objTags->At(0))->GetName();
+         fOptionsMap->Add(new TObjString(key), 0);
       }
       delete objTags;
    }
@@ -656,6 +661,19 @@ Int_t TUrl::GetIntValueFromOptions(const char *key) const
    ParseOptions();
    TObject *option = fOptionsMap ? fOptionsMap->GetValue(key) : 0;
    return (option ? (atoi(((TObjString*)fOptionsMap->GetValue(key))->GetName())) : -1);
+}
+
+//______________________________________________________________________________
+Bool_t TUrl::HasOption(const char *key) const
+{
+   // Returns true if the given key appears in the URL options list.
+   
+   if (!key) return kFALSE;
+   ParseOptions();
+   
+   if (fOptionsMap && fOptionsMap->FindObject(key))
+      return kTRUE;
+   return kFALSE;
 }
 
 //______________________________________________________________________________

core/base/inc/TUrl.h
@@ -80,6 +80,7 @@ public:
    const char *GetOptions() const { return fOptions; }
    const char *GetValueFromOptions(const char *key) const;
    Int_t       GetIntValueFromOptions(const char *key) const;
+   Bool_t      HasOption(const char *key) const;
    void        ParseOptions() const;
    void        CleanRelativePath();
    const char *GetFileAndOptions() const;

commit 6d5d2e1fab4a53af5629b96194a61b37dc5d346c

Fix TUrl bug inadvertently introduced in 728b157
URLs like "user@host" returned:

  GetUser() --> "" instead of "user"
  GetHost() --> "user@host" instead of "host"

This bug prevented PROOF from working at all.

core/base/src/TUrl.cxx
@@ -207,8 +207,12 @@ tryfile:
    u = s;
    t = s;
 again:
-   if ((s = strchr(t, '@')) && (((x = strchr(t, '/')) && s < x) ||
-       ((x = strchr(t, '?')) && s < x) || ((x = strchr(t, '#')) && s < x))) {
+   if ((s = strchr(t, '@')) && (
+       ((x = strchr(t, '/')) && s < x) ||
+       ((x = strchr(t, '?')) && s < x) ||
+       ((x = strchr(t, '#')) && s < x) ||
+       (!strchr(t, '/'))
+      )) {
       if (*(s-1) == '\\') {
          t = s+1;
          goto again;