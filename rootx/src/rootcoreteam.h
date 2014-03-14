a18de90de5ef80a1785dea6f2ca1be26e0fddc1d
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2014-03-14 15:05:21 +0100

    Make it even simpler for a script to replace.

--- rootx/src/rootcoreteam.h
+++ rootx/src/rootcoreteam.h
@@ -5,16 +5,18 @@ namespace ROOT {
 namespace ROOTX {
 
 //This string will be updated by external script, reading names from http://root.cern.ch/gitstats/authors.html.
+//The names are sorted in alphabetical order.
 //The string has an internal linkage (it has a definition here, not in rootxx.cxx or rootx-cocoa.mm files.
 //So this header can be included in different places (as soon as you know what you're doing).
 //Please, do not modify this file.
 
+const char * gROOTCoreTeam =
 //[STRINGTOREPLACE
-const char * gROOTCoreTeam = "Andrei Gheata, Axel Naumann, Bertrand Bellenot, Cristina Cristescu,"
-                             " Danilo Piparo, Fons Rademakers, Gerardo Ganis, Ilka Antcheva,"
-                             " Lorenzo Moneta, Matevz Tadel, Olivier Couet, Paul Russo, Pere Mato,"
-                             " Philippe Canal, Rene Brun, Timur Pocheptsov, Valeri Onuchin,"
-                             " Vassil Vassilev, Wim Lavrijsen, Wouter Verkerke\n\n";
+"Andrei Gheata, Axel Naumann, Bertrand Bellenot, Cristina Cristescu,"
+" Danilo Piparo, Fons Rademakers, Gerardo Ganis, Ilka Antcheva,"
+" Lorenzo Moneta, Matevz Tadel, Olivier Couet, Paul Russo, Pere Mato,"
+" Philippe Canal, Rene Brun, Timur Pocheptsov, Valeri Onuchin,"
+" Vassil Vassilev, Wim Lavrijsen, Wouter Verkerke.\n\n";
 //STRINGTOREPLACE]
 
 }
