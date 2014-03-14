be3dfed354ad50a27b82fa651c20c6e31def4a7a
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2014-03-14 12:12:36 +0100

    make display of graph an option. This should fix problem on Windows

--- math/mathcore/test/kDTreeTest.cxx
+++ math/mathcore/test/kDTreeTest.cxx
@@ -8,8 +8,11 @@
   //
 */
 
+#include <iostream>
 //#include <malloc.h>
 #include "TSystem.h"
+#include "TApplication.h"
+#include "TCanvas.h"
 #include "TMatrixD.h"
 #include "TRandom.h"
 #include "TGraph.h"
@@ -26,7 +29,8 @@ void TestSpeed(Int_t npower2 = 20, Int_t bsize = 10);
 //void TestkdtreeIF(Int_t npoints=1000, Int_t bsize=9, Int_t nloop=1000, Int_t mode = 2);
 //void TestSizeIF(Int_t nsec=36, Int_t nrows=159, Int_t npoints=1000,  Int_t bsize=10, Int_t mode=1);
 
-
+int verbose = 0;
+bool showGraphics = false; 
 
 Float_t Mem()
 {
@@ -249,7 +253,11 @@ void TestSpeed(Int_t npower2, Int_t bsize)
     //timer.Print("u");
     delete kdtree;
   }
-  g->Draw("apl");
+  if (showGraphics) {
+     g->Draw("apl");
+     if (gPad) gPad->Update();
+  }
+  if (verbose) g->Print();
   delete[] data0;
   return;
 }
@@ -541,7 +549,35 @@ void TestRange()
 
 
 //______________________________________________________________________
-int main() { 
+int main(int argc,  char *argv[]) {  
+
+
+   // Parse command line arguments
+   for (Int_t i = 1 ;  i < argc ; i++) {
+      std::string arg = argv[i] ;
+      
+      if (arg == "-v") {
+         std::cout << "kDTreeTest: running in verbose mode" << std::endl;
+         verbose = 1;
+      } 
+      if (arg == "-g") {
+         std::cout << "kDTreeTest: running showing the graphics" << std::endl;
+         showGraphics = true;
+      } 
+   }
+
+   TApplication* theApp = 0;
+   if ( showGraphics )
+      theApp = new TApplication("App",&argc,argv);
+
    kDTreeTest();
+   
+   if ( showGraphics )
+   {
+      theApp->Run();
+      delete theApp;
+      theApp = 0;
+   }
+
    return 0; 
 }
