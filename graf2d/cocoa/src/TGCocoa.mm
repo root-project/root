86d82b224a0fa365d17bf32fb16ed2716d0d9dff
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2014-03-17 10:37:52 +0100

    No need in a scope guard - I do not change any state, just clear context, nothing
    to rollback.

--- graf2d/cocoa/src/TGCocoa.mm
+++ graf2d/cocoa/src/TGCocoa.mm
@@ -629,7 +629,7 @@ void TGCocoa::ClearWindow()
       //obtain real pad's color and check for its transparency.
       CGContextRef pixmapCtx = drawable.fContext;
       assert(pixmapCtx != 0 && "ClearWindow, pixmap's context is null");
-      const Quartz::CGStateGuard ctxGuard(pixmapCtx);
+      //const Quartz::CGStateGuard ctxGuard(pixmapCtx);
       //CGContextSetRGBFillColor(pixmapCtx, 1., 1., 1., 1.);
       //CGContextFillRect(pixmapCtx, CGRectMake(0, 0, drawable.fWidth, drawable.fHeight));
       //Now we really clear!
