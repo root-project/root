28564527b6263a214d604e5ca31925c2b0c14876
Axel Naumann <Axel.Naumann@cern.ch>
2014-03-18 09:16:49 +0100

    Assert there is no llvm before libCling is loaded (side-effect of ROOT-6125).

--- core/base/src/TROOT.cxx
+++ core/base/src/TROOT.cxx
@@ -1613,6 +1613,21 @@ void TROOT::InitInterpreter()
    // Initialize the interpreter. Should be called only after main(),
    // to make sure LLVM/Clang is fully initialized.
 
+   // Make sure no llvm symbols are visible before loading libCling. If they
+   // exist libCling will use those and not ours, causing havoc in the
+   // interpreter. Look for an extern "C" symbol to avoid mangling; look for a
+   // symbol from llvm because clang builds on top, so users would likely
+   // have also their own llvm symbols when providing their own clang.
+   void *LLVMEnablePrettyStackTraceAddr = 0;
+   // Can't use gSystem->DynFindSymbol() because that iterates over all *known*
+   // libraries which is not the same!
+   LLVMEnablePrettyStackTraceAddr = dlsym(RTLD_DEFAULT, "LLVMEnablePrettyStackTrace");
+   if (LLVMEnablePrettyStackTraceAddr) {
+      Error("InitInterpreter()", "LLVM SYMBOLS ARE EXPOSED TO CLING! "
+            "This will cause problems; please hide them or dlopen() them "
+            "after the call to TROOT::InitInterpreter()!");
+   }
+
    char *libcling = gSystem->DynamicPathName("libCling");
 
    gInterpreterLib = dlopen(libcling, RTLD_LAZY|RTLD_LOCAL);
