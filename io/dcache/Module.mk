4b71c477827801ee7722bf3d320a76c6d1a2a4f0
Philippe Canal <pcanal@fnal.gov>
2014-03-19 15:45:01 -0500

    suppress warnings

--- io/dcache/Module.mk
+++ io/dcache/Module.mk
@@ -71,4 +71,4 @@ distclean-$(MODNAME): clean-$(MODNAME)
 distclean::     distclean-$(MODNAME)
 
 ##### extra rules ######
-$(DCACHEO) $(DCACHEDO): CXXFLAGS += $(DCAPINCDIR:%=-I%)
+$(DCACHEO) $(DCACHEDO): CXXFLAGS := $(filter-out -Wall,$(CXXFLAGS)) $(DCAPINCDIR:%=-I%)
