a4db65b52a41e713f56f949352ffb4a93ff25c59
Philippe Canal <pcanal@fnal.gov>
2014-03-19 16:36:47 -0500

    Ignore warning in external library (dcache)

--- io/dcache/Module.mk
+++ io/dcache/Module.mk
@@ -68,3 +68,7 @@ distclean::     distclean-$(MODNAME)
 
 ##### extra rules ######
 $(DCACHEO) $(DCACHEDO): CXXFLAGS := $(filter-out -Wall,$(CXXFLAGS)) $(DCAPINCDIR:%=-I%)
+ifneq ($(CXX:g++=),$(CXX))
+   $(DCACHEO) $(DCACHEDO):  CXXFLAGS += -Wno-ignored-qualifiers
+endif
+
