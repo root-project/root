87a639283a377b0413c557747df2b1b0349d88bf
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2014-03-19 20:50:04 +0100

    Fix compilation error on cygwin/gcc4.8.2 (error: 'to_string' is not a member of 'std')

--- core/metautils/src/TClassEdit.cxx
+++ core/metautils/src/TClassEdit.cxx
@@ -274,7 +274,7 @@ void TClassEdit::TSplitType::ShortType(std::string &answ, int mode)
          }
       }
    }
-#if __cplusplus >= 201103L
+#if __cplusplus >= 201103L && !defined (__CYGWIN__)
    else {
       if ( (mode & kDropStlDefault) && (narg >= 3)) {
          unsigned int offset = (0==strncmp("const ",fElements[0].c_str(),6)) ? 6 : 0;
