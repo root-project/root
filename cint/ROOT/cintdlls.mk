6dca10d7e7b453e75e3c80ef8266a6796d4acd35
Philippe Canal <pcanal@fnal.gov>
2014-03-19 19:37:42 -0500

    Suppress linker warning

--- cint/ROOT/cintdlls.mk
+++ cint/ROOT/cintdlls.mk
@@ -176,8 +176,12 @@ ifneq ($(subst win,,$(ARCH)),$(ARCH))
 	@rm -f $(@:.dll=.lib) $(@:.dll=.exp) # remove import libs
 endif
 
+ifeq ($(GCC_MAJOR),4)
+RemoveLinkerWarning := 2>&1 | grep -v -e 'G__c_stdfunc.o: In function' -e 'warning: .* is dangerous' 
+endif
+
 $(CINTDLLDIRDLLS)/%.dll: $(CINTDLLDIRL)/G__c_%.o
-	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $(@:.dll=.$(SOEXT))) $(@:.dll=.$(SOEXT)) "$(filter-out $(MAINLIBS),$^)" "$(CINTDLLLIBLINK)"
+	@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" $(notdir $(@:.dll=.$(SOEXT))) $(@:.dll=.$(SOEXT)) "$(filter-out $(MAINLIBS),$^)" "$(CINTDLLLIBLINK)" $(RemoveLinkerWarning)
 	$(CINTDLLSOEXTCMD)
 
 core/metautils/src/stlLoader_%.cc: $(ROOT_SRCDIR)/core/metautils/src/stlLoader.cc
