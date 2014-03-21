3f02adccd1ac125c269338610d1f7e92a2502962
Philippe Canal <pcanal@fnal.gov>
2014-03-21 11:58:33 -0500

    Add missing dependency

--- scripts/Reflex.mk
+++ scripts/Reflex.mk
@@ -16,15 +16,15 @@ endif
 .PRECIOUS: %_rflx.cpp %_cint.cpp %.o
 
 # The dictionary:
-%_rflx.cpp: %_selection.xml %.h
+%_rflx.cpp: %_selection.xml %.h $(ROOTCINT) $(ROOTV)
 	$(CMDECHO) $(GENREFLEX) $*.h -s $*_selection.xml $(GENREFLEXFLAGS) -I"$(ROOTSYS)/include" $(GENREFLEXCXXFLAGS) > $@.log 2>&1 || (cat $@.log && exit 1)
 
-%_cling.cpp: %.h %LinkDef.h
+%_cling.cpp: %.h %LinkDef.h $(ROOTCINT) $(ROOTV)
 	$(CMDECHO) rootcint -f $@ -c $^  > $@.log 2>&1 || (cat $@.log && exit 1)
 
-lib%_dictrflx.$(DllSuf): %_rflx.$(ObjSuf)
+lib%_dictrflx.$(DllSuf): %_rflx.$(ObjSuf) $(ROOTCORELIBS) $(ROOTCINT) $(ROOTV)
 	$(BuildFromObj)
 
-lib%_dictcint.$(DllSuf): %_cint.$(ObjSuf)
+lib%_dictcint.$(DllSuf): %_cint.$(ObjSuf) $(ROOTCORELIBS) $(ROOTCINT) $(ROOTV)
 	$(BuildFromObj)
 
