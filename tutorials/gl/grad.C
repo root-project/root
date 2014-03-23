cd428ca7ba4864912b2a85c1ce67683fe1c8153c
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2014-03-23 11:26:39 +0100

    New GL tutorials demonstrating a linear gradient fill.

---
+++ tutorials/gl/grad.C
@@ -0,0 +1,89 @@
+//This macro shows how to create and use linear gradients to fill
+//a histogram or a pad.
+//Requires OpenGL.
+
+//Includes for ACLiC (cling does not need them).
+#include "TColorGradient.h"
+#include "TVirtualX.h"
+#include "TCanvas.h"
+#include "TColor.h"
+#include "TError.h"
+#include "TH1F.h"
+
+//Aux. functions for tutorials/gl.
+#include "customcolorgl.h"
+
+//______________________________________________________________________
+void grad()
+{
+   //1. Try to 'allocate' free indices for our custom colors.
+   //We can use hard-coded indices like 1001, 1002, 1003, ... but
+   //I prefer to find free indices in the ROOT's color table
+   //to avoid possible conflicts with other tutorials.
+   Int_t colorIndices[5] = {};
+   if (ROOT::GLTutorials::FindFreeCustomColorIndices(colorIndices) != 5) {
+      Error("grad", "failed to create new custom colors");
+      return;
+   }
+
+   gStyle->SetCanvasPreferGL(kTRUE);
+
+   //2. Test if we have a right GUI back-end.
+   TCanvas * const cnv = new TCanvas("gradient demo 1", "gradient demo 1", 100, 100, 600, 600);
+   //After canvas was created, gVirtualX should be non-null.
+   if (!cnv->UseGL()) {
+      ::Error("grad", "This macro requires OpenGL");
+      delete cnv;
+      return;
+   }
+   
+   typedef TColorGradient::Point Point;
+   
+   //3. Create custom colors.
+   const Int_t frameGradient = colorIndices[2];//this gradient is a mixture of colorIndices[0] and colorIndices[1]
+   //Fill color for a pad frame:
+   {
+      new TColor(colorIndices[0], 0.25, 0.25, 0.25, "special pad color1", 0.55);
+      new TColor(colorIndices[1], 1., 1., 1., "special pad color2", 0.05);
+
+      const Double_t locations[] = {0., 0.2, 0.8, 1.};
+      const Color_t gradientIndices[4] = {colorIndices[0], colorIndices[1], colorIndices[1], colorIndices[0]};
+
+      //Gradient for a pad's frame.
+      TLinearGradient * const gradFill1 = new TLinearGradient(frameGradient, 4, locations, gradientIndices);
+      //Horizontal:
+      gradFill1->SetStartEnd(Point(0., 0.), Point(1., 0.));
+   }
+
+   const Int_t padGradient = colorIndices[3];
+   //Fill color for a pad:
+   {
+      const Double_t locations[] = {0., 1.};
+      const Color_t gradientIndices[4] = {30, 38};//We create a gradient from system colors.
+      
+      //Gradient for a pad.
+      TLinearGradient * const gradFill2 = new TLinearGradient(padGradient, 2, locations, gradientIndices);
+      //Vertical:
+      gradFill2->SetStartEnd(Point(0., 0.), Point(0., 1));
+   }
+   
+   const Int_t histGradient = colorIndices[4];
+   //Fill color for a histogram:
+   {
+      const Color_t gradientIndices[3] = {kYellow, kOrange, kRed};
+      const Double_t locations[3] = {0., 0.5, 1.};
+      
+      //Gradient for a histogram.
+      TLinearGradient * const gradFill3 = new TLinearGradient(histGradient, 3, locations, gradientIndices);
+      //Vertical:
+      gradFill3->SetStartEnd(Point(0., 0.), Point(0., 1.));
+   }
+
+   cnv->SetFillColor(padGradient);
+   cnv->SetFrameFillColor(frameGradient);
+   
+   TH1F * const hist = new TH1F("a1", "b1", 20, -3., 3.);
+   hist->SetFillColor(histGradient);
+   hist->FillRandom("gaus", 100000);
+   hist->Draw();
+}
---
+++ tutorials/gl/grad2.C
@@ -0,0 +1,75 @@
+//Gradient fill with transparency and "SAME" option.
+//Requires OpenGL.
+
+//Includes for ACLiC (cling does not need them).
+#include "TColorGradient.h"
+#include "TCanvas.h"
+#include "TError.h"
+#include "TColor.h"
+#include "TH1F.h"
+
+//Aux. functions for tutorials/cocoa.
+#include "customcolorgl.h"
+
+void grad2()
+{
+   //1. 'Allocate' free indices for our custom colors.
+   //We can use hard-coded indices like 1001, 1002, 1003 ... but
+   //I prefer to find free indices in the ROOT's color table
+   //to avoid possible conflicts with other tutorials.
+   Int_t freeIndices[4] = {};
+   if (ROOT::GLTutorials::FindFreeCustomColorIndices(freeIndices) != 4) {
+      Error("grad2", "can not allocate new custom colors");
+      return;
+   }
+   
+   gStyle->SetCanvasPreferGL(kTRUE);
+
+   //'Aliases' (instead of freeIndices[someIndex])
+   const Int_t customRed = freeIndices[0], grad1 = freeIndices[1];
+   const Int_t customGreen = freeIndices[2], grad2 = freeIndices[3];
+
+   //2. Check that we are ROOT with Cocoa back-end enabled.
+   TCanvas * const cnv = new TCanvas("gradiend demo 2", "gradient demo 2", 100, 100, 800, 600);
+   //After canvas was created, gVirtualX should be non-null.
+   if (gVirtualX && !gVirtualX->InheritsFrom("TGCocoa")) {
+      ::Error("grad2", "This macro requires OpenGL");
+      //Unfortunately, we can not remove the colors we added :(
+      delete cnv;
+      return;
+   }
+
+   //3. Now that we 'allocated' all indices and checked back-end's type, let's create our custom colors colors:
+   //   a) Custom semi-transparent red.
+   new TColor(customRed, 1., 0., 0., "red", 0.5);
+
+   //   b) Gradient (from our semi-transparent red to ROOT's kOrange).
+   const Double_t locations[] = {0., 1.};
+   const Color_t idx1[] = {customRed, kOrange};
+   TLinearGradient * const gradFill1 = new TLinearGradient(grad1, 2, locations, idx1);
+   
+   typedef TColorGradient::Point Point;
+   //Starting and ending points for a gradient fill (it's a vertical gradient):
+   gradFill1->SetStartEnd(Point(0., 0.), Point(0., 1));
+
+   //   c) Custom semi-transparent green.
+   new TColor(customGreen, 0., 1., 0., "green", 0.5);
+
+   //   d) Gradient from ROOT's kBlue to our custom green.
+   const Color_t idx2[] = {customGreen, kBlue};
+   
+   TLinearGradient * const gradFill2 = new TLinearGradient(grad2, 2, locations, idx2);
+   //Vertical gradient fill.
+   gradFill2->SetStartEnd(Point(0., 0), Point(0., 1.));
+
+   TH1F * hist = new TH1F("a2", "b2", 10, -2., 3.);
+   TH1F * hist2 = new TH1F("c3", "d3", 10, -3., 3.);
+   hist->FillRandom("landau", 100000);
+   hist2->FillRandom("gaus", 100000);
+
+   hist->SetFillColor(grad1);
+   hist2->SetFillColor(grad2);
+
+   hist2->Draw();
+   hist->Draw("SAME");
+}
