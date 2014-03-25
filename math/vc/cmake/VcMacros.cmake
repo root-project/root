712a8c693b9483d7d105d388646b820b323736ed
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2014-03-25 16:29:57 +0100

    Re-apply patch to disable warnings on Clang

--- math/vc/cmake/VcMacros.cmake
+++ math/vc/cmake/VcMacros.cmake
@@ -376,6 +376,9 @@ macro(vc_set_preferred_compiler_flags)
       vc_add_compiler_flag(Vc_DEFINITIONS "-Wno-local-type-template-args")
       vc_add_compiler_flag(Vc_DEFINITIONS "-Wno-unnamed-type-template-args")
 
+      # disable this warning appearing from version 3.4 (5.1) 
+      vc_add_compiler_flag(Vc_DEFINITIONS "-Wno-unused-function")
+
       AddCompilerFlag(-stdlib=libc++)
    endif()
 
