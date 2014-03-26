369bcaca297c1cd5d907f32bece5ce2eb278fc23
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2014-03-26 12:37:15 +0100

    max/min instead of ?:

--- graf2d/quartz/src/QuartzFillArea.mm
+++ graf2d/quartz/src/QuartzFillArea.mm
@@ -172,14 +172,13 @@ bool CalculateGradientRadiuses(const TRadialGradient *grad,
    CGFloat endRadius = grad->GetR2();
 
    if (grad->GetCoordinateMode() == TColorGradient::kObjectBoundingMode) {
-      const CGFloat scale = bbox.size.width < bbox.size.height ?
-                            bbox.size.height : bbox.size.width;
+      const CGFloat scale = std::max(bbox.size.width, bbox.size.height);
       
       startRadius *= scale;
       endRadius *= scale;
    } else {
-      const CGFloat scale = sizeOfDrawable.width < sizeOfDrawable.height ?
-                            sizeOfDrawable.height : sizeOfDrawable.width;
+      const CGFloat scale = std::max(sizeOfDrawable.width, sizeOfDrawable.height);
+
       startRadius *= scale;
       endRadius *= scale;
    }
@@ -217,15 +216,11 @@ bool CalculateSimpleRadialGradientParameters(const TRadialGradient *grad,
    CGPoint center = CGPointMake(grad->GetCenter().fX, grad->GetCenter().fY);
 
    if (grad->GetCoordinateMode() == TColorGradient::kObjectBoundingMode) {
-      const CGFloat scale = std::max(bbox.size.width, bbox.size.height);
-
-      radius *= scale;
+      radius *= std::max(bbox.size.width, bbox.size.height);
       center.x = bbox.size.width * center.x + bbox.origin.x;
       center.y = bbox.size.height * center.y + bbox.origin.y;
    } else {
-      const CGFloat scale = std::max(sizeOfDrawable.width, sizeOfDrawable.height);
-      
-      radius *= scale;
+      radius *= std::max(sizeOfDrawable.width, sizeOfDrawable.height);
       center.x *= sizeOfDrawable.width;
       center.y *= sizeOfDrawable.height;
    }
