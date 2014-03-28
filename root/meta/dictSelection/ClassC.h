016d0efc7235edaefe47b032785d605bdac4e6d8
Danilo Piparo <danilo.piparo@cern.ch>
2014-03-28 13:46:21 +0100

    Added adaptation of test provided by Atlas for the dict selection

---
+++ root/meta/dictSelection/ClassC.h
@@ -0,0 +1,26 @@
+// Dear emacs, this is -*- c++ -*-
+/*
+This test has been provided by Attila Krasznahorkay.
+*/
+#ifndef DICTRULES_CLASSC_H
+#define DICTRULES_CLASSC_H
+
+namespace Atlas {
+
+   /// A simple class that uses the default traits
+   ///
+   /// This is another class that is used as a template type for ClassA.
+   ///
+   /// @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
+   ///
+   class ClassC {
+
+   public:
+      /// Default constructor
+      ClassC() {}
+
+   }; // class ClassC
+
+} // namespace Atlas
+
+#endif // DICTRULES_CLASSC_H
---
+++ root/meta/dictSelection/execAtlasTest.C
@@ -0,0 +1,19 @@
+#include "utils.h"
+
+int execAtlasTest(){
+
+   std::cout << "Checking normalisation of names:\n";
+   
+   // As a warm-up
+   printNames("std::string");
+   printNames("std::vector<std::string>");
+   
+   // Load the atlas lib and check some names
+   if (0!=gSystem->Load("libAtlasTest_dictrflx.so"))
+      std::cerr << "Error loading dictionary library.\n";
+   
+   printNames("Atlas::ClassA<Atlas::ClassB, float>");
+   printNames("Atlas::ClassA<Atlas::ClassC, int>");
+   
+return 0;
+}
