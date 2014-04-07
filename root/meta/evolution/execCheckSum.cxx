7a6bc432f8dcb30b391f0e30f18e4c98c78774f8
Philippe Canal <pcanal@fnal.gov>
2014-04-07 03:46:48 -0500

    more testing

--- root/meta/evolution/execCheckSum.cxx
+++ root/meta/evolution/execCheckSum.cxx
@@ -10,14 +10,17 @@ public:
    HasTypeDef() : fInt1(1),fInt2(2) {}
    virtual ~HasTypeDef() {}
 
+   typedef TNamed Named_t;
    std::vector<Int_t>    fVec1;
    std::vector<MyLong_t> fVec2;
+   std::vector<Named_t>  fVec3;
    MyLong_t fInt1;
    Int_t    fInt2;
    void Print() {
       fprintf(stderr,"The %s object has:\n",Class()->GetName());
       fprintf(stderr,"  fVec1.size() : %ld\n", fVec1.size());
       fprintf(stderr,"  fVec2.size() : %ld\n", fVec2.size());
+      fprintf(stderr,"  fVec3.size() : %ld\n", fVec3.size());
       fprintf(stderr,"  fInt1        : %ld\n", fInt1);
       fprintf(stderr,"  fInt2        : %d\n", fInt2);
    }
@@ -41,7 +44,10 @@ void writeFile(const char *filename = "checksum.root")
 void readFile(const char *filename = "checksum.root")
 {
    TFile *f = TFile::Open(filename,"READ");
-
+   if (!f) {
+      fprintf(stderr,"Could not open %s\n",filename);
+      return;
+   }
    TClass *c = TClass::GetClass("HasTypeDef");
    fprintf(stderr,"In memory the checksum is: 0x%x\n",c->GetCheckSum());
    TVirtualStreamerInfo *info =  (TVirtualStreamerInfo*)f->GetStreamerInfoList()->FindObject("HasTypeDef");
