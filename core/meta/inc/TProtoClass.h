b28ba2e429158002e3b1959083d80bcd3e7bb2c8
Axel Naumann <Axel.Naumann@cern.ch>
2014-04-28 18:41:50 +0200

    TProtoClass: a persistent TClass. Add a dictionary and it will be able to do I/O.

---
+++ core/meta/inc/TProtoClass.h
@@ -0,0 +1,48 @@
+// @(#)root/meta:$Id$
+// Author: Axel Naumann 2014-04-28
+
+/*************************************************************************
+ * Copyright (C) 1995-2014, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+
+#ifndef ROOT_TProtoClass
+#define ROOT_TProtoClass
+
+#ifndef ROOT_TNamed
+#include "TNamed.h"
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TProtoClass                                                          //
+//                                                                      //
+// Stores enough information to create a TClass from a dictionary       //
+// without interpreter information.                                     //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+class TProtoClass: public TNamed {
+public:
+   TList              *fRealData; // TRealData for members + bases
+   TList              *fBase;     // List of base classes
+   TListOfDataMembers *fData;     // List of data members
+   UInt_t              fCheckSum; // Checksum of data members and base classes
+   Int_t               fSizeof;   // Size of the class
+   Int_t               fCanSplit; // Whether this class can be split
+   Long_t              fProperty; // Class properties, see EProperties
+
+   TProtoClass(const TProtoClass&) = delete;
+   TProtoClass& operator=(const TProtoClass&) = delete;
+
+   TProtoClass() {}
+   virtual ~TProtoClass(); // implemented in TClass.cxx to pin vtable
+
+   ClassDef(TProtoClass,2); //Persistent TClass
+};
+
+#endif
