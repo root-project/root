8a6296362e9a66ce67839dfb8a5406e1cd3996a5
Philippe Canal <pcanal@fnal.gov>
2014-05-07 18:56:51 -0500

    Test for missingCheckSum ROOT-6271

--- root/meta/evolution/execMissingCheckSum.cxx
+++ root/meta/evolution/execMissingCheckSum.cxx
@@ -6,11 +6,30 @@ public:
    Int_t fValue;
 };
 
+#include <stdint.h>
+#include <vector>
+
+struct HcalFlagHFDigiTimeParam{
+   HcalFlagHFDigiTimeParam() : mId(0), mHFdigiflagFirstSample(0), mHFdigiflagSamplesToAdd(0),mHFdigiflagExpectedPeak(0),mHFdigiflagMinEthreshold(0.0) {}
+
+   uint32_t mId; // detector ID
+   uint32_t mHFdigiflagFirstSample;         // first sample used in NTS calculation
+   uint32_t mHFdigiflagSamplesToAdd;        // # of sampels to use in NTS calculation
+   uint32_t mHFdigiflagExpectedPeak;        // expected peak position; used for calculating TS(peak)
+   double    mHFdigiflagMinEthreshold;       // minimum energy for flagged rechit
+   std::vector<double> mHFdigiflagCoefficients; // coefficients used to parameterize TS(peak)/NTS threshold:  [0]-exp([1]+[2]*E+....)
+};
+
 void execMissingCheckSum()
 {
-  TFile *f = new TFile("missingCheckSum.root");
-  if (!f->Get("obj")) printf("Error: could not read the object (1)\n");
-  ((TObjArray*)TClass::GetClass("MyClass")->GetStreamerInfos())->RemoveAt(1);
-  if (!f->Get("obj")) printf("Error: could not read the object (2)\n");
+   TFile *f = new TFile("missingCheckSum.root");
+   if (!f->Get("obj")) printf("Error: could not read the object (1)\n");
+   ((TObjArray*)TClass::GetClass("MyClass")->GetStreamerInfos())->RemoveAt(1);
+   if (!f->Get("obj")) printf("Error: could not read the object (2)\n");
+   
+   f = new TFile("missingCheckSum2.root");
+   f->Get("timeParam");
+  ((TObjArray*)TClass::GetClass("HcalFlagHFDigiTimeParam")->GetStreamerInfos())->RemoveAt(1);
+   f->Get("timeParam");
 }
 
