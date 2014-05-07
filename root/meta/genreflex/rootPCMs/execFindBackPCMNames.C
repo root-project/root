79db28740b59f1b15667f2c19b1ce75db3453073
Danilo Piparo <danilo.piparo@cern.ch>
2014-05-07 13:27:19 +0200

    A test for ROOT-6197: persist the rootpcm names in the pcm rootfile

---
+++ root/meta/genreflex/rootPCMs/execFindBackPCMNames.C
@@ -0,0 +1,16 @@
+
+int execFindBackPCMNames(){
+
+   std::vector<std::string>* pcmNames = nullptr;
+   TFile f("classesFindBackPCMNames_rflx_rdict.pcm");
+   f.ls();
+   f.GetObject("__AncestorPCMsNames",pcmNames);
+   if (!pcmNames) return 1;
+   std::cout << "Ancestor pcms:\n";
+   for (auto const & name: *pcmNames){
+      std::cout << name << std::endl;
+   }
+
+   return 0;
+
+}
