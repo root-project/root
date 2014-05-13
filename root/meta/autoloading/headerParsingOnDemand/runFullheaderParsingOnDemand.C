0af09bb0e1e2bbe04d7c4806a6d3a6462ae4f941
Philippe Canal <pcanal@fnal.gov>
2014-05-12 22:49:40 -0500

    Call GetClassInfo to trigger autoparsing

--- root/meta/autoloading/headerParsingOnDemand/runFullheaderParsingOnDemand.C
+++ root/meta/autoloading/headerParsingOnDemand/runFullheaderParsingOnDemand.C
@@ -1,17 +1,17 @@
 void runFullheaderParsingOnDemand(){
    gDebug=1;
 
-   TClass::GetClass("myClass0<E>");
-   TClass::GetClass("myClass1<E>");
-   TClass::GetClass("myClass1<int>");
-   TClass::GetClass("myClass2<E>");
-   TClass::GetClass("myClass2<float>");
-   TClass::GetClass("myClass3<E>");
-   TClass::GetClass("myClass4<E>");
-   TClass::GetClass("myClass5<E>");
-   TClass::GetClass("myClass6<E>");
-   TClass::GetClass("myClass7<E>");
-   TClass::GetClass("myClass8<E>");
+   TClass::GetClass("myClass0<E>")->GetClassInfo();
+   TClass::GetClass("myClass1<E>")->GetClassInfo();
+   TClass::GetClass("myClass1<int>")->GetClassInfo();
+   TClass::GetClass("myClass2<E>")->GetClassInfo();
+   TClass::GetClass("myClass2<float>")->GetClassInfo();
+   TClass::GetClass("myClass3<E>")->GetClassInfo();
+   TClass::GetClass("myClass4<E>")->GetClassInfo();
+   TClass::GetClass("myClass5<E>")->GetClassInfo();
+   TClass::GetClass("myClass6<E>")->GetClassInfo();
+   TClass::GetClass("myClass7<E>")->GetClassInfo();
+   TClass::GetClass("myClass8<E>")->GetClassInfo();
 
    std::cout << "\n";
 }
