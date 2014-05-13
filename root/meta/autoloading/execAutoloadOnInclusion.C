a84f7f9cacce92e3b458d3ff833ab33382dd9ed7
Danilo Piparo <danilo.piparo@cern.ch>
2014-05-13 16:17:50 +0200

    Fix EC

--- root/meta/autoloading/execAutoloadOnInclusion.C
+++ root/meta/autoloading/execAutoloadOnInclusion.C
@@ -2,6 +2,9 @@
 
 void execAutoloadOnInclusion(){
 
-gSystem->ListLibraries();
+ if ( 1 == gSystem->Load("libHist.so") )
+    std::cout << "libHist.so already loaded through header inclusion!\n";
+ else
+    std::cout << "Error: libHist.so should have been already up!\n";
 
 }
