28231d3dccfdfb46692779204f3a04ed4395386e
Olivier Couet <olivier.couet@cern.ch>
2014-05-19 17:24:59 +0200

    fix https://sft.its.cern.ch/jira/browse/ROOT-6303

--- hist/hist/src/TGraphErrors.cxx
+++ hist/hist/src/TGraphErrors.cxx
@@ -313,10 +313,10 @@ TGraphErrors::TGraphErrors(const char *filename, const char *format, Option_t *o
 
       // Looping
       while (std::getline(infile, line, '\n')) {
-         if (line[line.size() - 1] == char(13)) {  // removing DOS CR character 
-            line.erase(line.end() - 1, line.end()) ;
-         }
          if (line != "") {
+            if (line[line.size() - 1] == char(13)) {  // removing DOS CR character 
+               line.erase(line.end() - 1, line.end()) ;
+            }
             token = strtok(const_cast<char*>(line.c_str()), option) ;
             while (token != NULL && value_idx < ntokensToBeSaved) {
                if (isTokenToBeSaved[token_idx]) {
