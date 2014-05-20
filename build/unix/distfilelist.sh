3d693f8b33dc61cf2c969b6403fcd8f5f5affb0b
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2014-05-20 17:56:45 +0200

    Add stressHistFactory_ref.root and stressRooStats_ref.root in the binary distribution

--- build/unix/distfilelist.sh
+++ build/unix/distfilelist.sh
@@ -42,7 +42,7 @@ WILDCARDS="LICENSE README bin \
    cint/cint/include tutorials \
    cint/cint/lib cint/cint/stl geom/gdml/*.py \
    test/*.cxx test/*.h test/Makefile* test/*.rootmap \
-   test/*.C test/*.sh test/dt_Makefile test/*.ref \
+   test/*.C test/*.sh test/dt_Makefile test/*.ref test/*_ref.root \
    test/README test/*.txt test/*.xml \
    test/RootShower/*.h test/RootShower/*.cxx \
    test/RootShower/*.rc test/RootShower/*.ico \
