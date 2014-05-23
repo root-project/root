1d4a8fb3e2632db5c0a72f7bf25993702f0f7357
Olivier Couet <olivier.couet@cern.ch>
2014-05-23 17:16:14 +0200

    Improve transparency help.

--- core/base/src/TAttLine.cxx
+++ core/base/src/TAttLine.cxx
@@ -54,6 +54,22 @@ Begin_Macro(source)
 End_Macro
 
 Begin_Html
+
+<h4>Color transparency</h4>
+<tt>SetLineColorAlpha()</tt>, allows to set a transparent color.
+In the following example the line color of the histogram <tt>histo</tt>
+is set to blue with a transparency of 35%. The color <tt>kBlue</tt>
+itself remains fully opaque. 
+<p>
+<pre>
+histo->SetLineColorAlpha(kBlue, 0.35);
+</pre>
+<p>  
+The transparency is available on all platforms when the <tt>flagOpenGL.CanvasPreferGL</tt> is set to <tt>1</tt>
+in <tt>$ROOTSYS/etc/system.rootrc</tt>, or on Mac with the Cocoa backend. On the file output
+it is visible with PDF, PNG, Gif, JPEG, SVG ... but not PostScript.
+
+
 <a name="L2"></a><h3>Line Width</h3>
 The line width is expressed in pixel units.
 The line width of any class inheriting from <tt>TAttLine</tt> can
