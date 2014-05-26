59b741ba3dcf98a569a776a2da05248734879f49
Olivier Couet <olivier.couet@cern.ch>
2014-05-26 15:08:40 +0200

    rename y1 into py1 (“y1” is already defined internally and cling returns “a double defined” error)

--- tutorials/graphs/multigraph.C
+++ tutorials/graphs/multigraph.C
@@ -10,11 +10,11 @@
 
       // create first graph
    const Int_t n1 = 10;
-   Double_t x1[]  = {-0.1, 0.05, 0.25, 0.35, 0.5, 0.61,0.7,0.85,0.89,0.95};
-   Double_t y1[]  = {-1,2.9,5.6,7.4,9,9.6,8.7,6.3,4.5,1};
+   Double_t px1[] = {-0.1, 0.05, 0.25, 0.35, 0.5, 0.61,0.7,0.85,0.89,0.95};
+   Double_t py1[] = {-1,2.9,5.6,7.4,9,9.6,8.7,6.3,4.5,1};
    Double_t ex1[] = {.05,.1,.07,.07,.04,.05,.06,.07,.08,.05};
    Double_t ey1[] = {.8,.7,.6,.5,.4,.4,.5,.6,.7,.8};
-   TGraphErrors *gr1 = new TGraphErrors(n1,x1,y1,ex1,ey1);
+   TGraphErrors *gr1 = new TGraphErrors(n1,px1,py1,ex1,ey1);
    gr1->SetMarkerColor(kBlue);
    gr1->SetMarkerStyle(21);
    gr1->Fit("pol6","q");
