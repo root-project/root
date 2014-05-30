0b1185a9c85dd296bc2a177139819881b43ad34d
Philippe Canal <pcanal@fnal.gov>
2014-05-30 05:35:04 -0500

    Add test for http://root.cern.ch/phpBB3/viewtopic.php?t=18049

---
+++ root/treeformula/sync/CUTpad1pad2.C
@@ -0,0 +1,27 @@
+{
+//========= Macro generated from object: CUTpad1pad2/Graph
+//========= by ROOT version5.34/14
+   
+   TCutG *cutg = new TCutG("CUTpad1pad2",16);
+   cutg->SetVarX("pad2");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,993.851,1015.08);
+   cutg->SetPoint(1,843.04,934.917);
+   cutg->SetPoint(2,719.649,829.244);
+   cutg->SetPoint(3,660.239,701.707);
+   cutg->SetPoint(4,619.109,614.253);
+   cutg->SetPoint(5,609.968,508.579);
+   cutg->SetPoint(6,662.524,461.208);
+   cutg->SetPoint(7,822.475,464.852);
+   cutg->SetPoint(8,1012.13,523.155);
+   cutg->SetPoint(9,1240.63,647.048);
+   cutg->SetPoint(10,1400.58,778.229);
+   cutg->SetPoint(11,1457.71,858.395);
+   cutg->SetPoint(12,1437.14,975);
+   cutg->SetPoint(13,1300.04,1069.74);
+   cutg->SetPoint(14,1169.8,1069.74);
+   cutg->SetPoint(15,993.851,1015.08);
+//   cutg->Draw("");
+}
