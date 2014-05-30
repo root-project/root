d9d739153148bdfe605b283cbaf1033af5f7d486
Philippe Canal <pcanal@fnal.gov>
2014-05-30 06:19:06 -0500

    Port to v6 ...

--- root/treeformula/sync/execScanFail.C
+++ root/treeformula/sync/execScanFail.C
@@ -1,4 +1,4 @@
-{
+int execScanFail() {
   TFile *f = new TFile("run00155-short.root");
   //gROOT->ProcessLine(".x read-gates-98MeV-forMelamine.C");
   //gROOT->ProcessLine(".x chain-Melamine-98MeV.C");
@@ -7,7 +7,7 @@
   gROOT->ProcessLine(".x CUTpad1tof.C");
 
   TTree *t; f->GetObject("DATA",t);
-  if (!t) return 0;
+  if (!t) return 1;
   Long64_t res = t->Scan("ThSCAT:X1pos:CUTpad1pad2:CUTpad1tof","CUTpad1pad2 * CUTpad1tof * ThSCAT>0","col=6.3:8.6");
 
   //DATA->Draw("ThSCAT:X1pos>>hThSCATvsX1","CUTpad1pad2 && CUTpad1tof && (ThSCAT>0)","col");
