9d81bc82237ea621705fba196eb3447f40ff9952
Danilo Piparo <danilo.piparo@cern.ch>
2014-06-11 12:22:19 +0200

    Make sure that the list of bases is complete.

--- core/utils/src/rootclingTCling.cxx
+++ core/utils/src/rootclingTCling.cxx
@@ -89,6 +89,8 @@ bool CloseStreamerInfoROOTFile()
       if (cl->GetCollectionProxy())
          continue;
       cl->Property(); // Force initialization of the bits and property fields.
+      cl->GetListOfBases();
+
       protoClasses.AddLast(new TProtoClass(cl));
    }
 
