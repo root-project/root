d62abfa0d4b58ba880c139684a1ff9a80fe5dbb2
Philipp Schoppe <philipp.schoppe@cern.ch>
2014-06-12 09:20:55 +0200

    Remove process substitution from custom_diff.sh.

--- scripts/custom_diff.sh
+++ scripts/custom_diff.sh
@@ -3,5 +3,22 @@
 FILEOUT="$1"
 FILECMP="$2"
 
-set +o posix
-diff -u -w <(grep -v Processing "$FILEOUT") <(grep -v Processing "$FILECMP")
+prefix1='/tmp/roottest_fifo1'
+prefix2='/tmp/roottest_fifo2'
+
+fifo1=$prefix1$RANDOM
+fifo2=$prefix2$RANDOM
+
+mkfifo "$fifo1"
+mkfifo "$fifo2"
+
+grep -v Processing "$FILEOUT" > "$fifo1" &
+grep -v Processing "$FILECMP" > "$fifo2" &
+
+diff -u -w "$fifo1" "$fifo2"
+
+rc=$?
+
+rm "$fifo1" "$fifo2"
+
+exit $rc
