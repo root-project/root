a6e512d9287e60e24026e6fa8c5a6250956c3653
Wim Lavrijsen <WLavrijsen@lbl.gov>
2014-06-16 15:20:27 -0700

    add more pre-defined dispatch functions

--- bindings/pyroot/src/TPyDispatcher.cxx
+++ bindings/pyroot/src/TPyDispatcher.cxx
@@ -3,12 +3,15 @@
 // Bindings
 #include "PyROOT.h"
 #include "TPyDispatcher.h"
+#include "TPython.h"
 
 // ROOT
 #include "TObject.h"
 
 // Standard
 #include <stdarg.h>
+
+#include <iostream>
 
 
 //______________________________________________________________________________
@@ -67,7 +70,7 @@ PyObject* TPyDispatcher::DispatchVA( const char* format, ... )
 {
 // Dispatch the arguments to the held callable python object, using format to
 // interpret the types of the arguments. Note that format is in python style,
-// not in C printf style. See: http://docs.python.org/api/arg-parsing.html .
+// not in C printf style. See: https://docs.python.org/2/c-api/arg.html .
    PyObject* args = 0;
 
    if ( format ) {
@@ -90,6 +93,176 @@ PyObject* TPyDispatcher::DispatchVA( const char* format, ... )
       }
 
    }
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::DispatchVA1( const char* clname, void* obj, const char* format, ... )
+{
+   PyObject* pyobj = TPython::ObjectProxy_FromVoidPtr( obj, clname );
+   if ( ! pyobj ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   std::cout << " HERE: " << clname << " " << obj << " " << format << " " << std::endl;
+
+   PyObject* args = 0;
+
+   if ( format ) {
+      va_list va;
+      va_start( va, format );
+
+      args = Py_VaBuildValue( (char*)format, va );
+
+      va_end( va );
+
+      if ( ! args ) {
+         PyErr_Print();
+         return 0;
+      }
+
+      if ( ! PyTuple_Check( args ) ) {    // if only one arg ...
+         PyObject* t = PyTuple_New( 2 );
+         PyTuple_SET_ITEM( t, 0, pyobj );
+         PyTuple_SET_ITEM( t, 1, args );
+         args = t;
+      } else {
+         PyObject* t = PyTuple_New( PyTuple_GET_SIZE( args ) + 1 );
+         PyTuple_SET_ITEM( t, 0, pyobj );
+         for ( int i = 0; i < PyTuple_GET_SIZE( args ); i++ ) {
+            PyObject* item = PyTuple_GET_ITEM( args, i );
+            Py_INCREF( item );
+            PyTuple_SET_ITEM( t, i+1, item );
+         }
+         Py_DECREF( args );
+         args = t;
+      }
+   } else {
+      args = PyTuple_New( 1 );
+      PyTuple_SET_ITEM( args, 0, pyobj );
+   }
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::Dispatch( TPad* selpad, TObject* selected, Int_t event )
+{
+   PyObject* args = PyTuple_New( 3 );
+   PyTuple_SET_ITEM( args, 0, TPython::ObjectProxy_FromVoidPtr( selpad, "TPad" ) );
+   PyTuple_SET_ITEM( args, 1, TPython::ObjectProxy_FromVoidPtr( selected, "TObject" ) );
+   PyTuple_SET_ITEM( args, 2, PyInt_FromLong( event ) );
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::Dispatch( Int_t event, Int_t x, Int_t y, TObject* selected )
+{
+   PyObject* args = PyTuple_New( 4 );
+   PyTuple_SET_ITEM( args, 0, PyInt_FromLong( event ) );
+   PyTuple_SET_ITEM( args, 1, PyInt_FromLong( x ) );
+   PyTuple_SET_ITEM( args, 2, PyInt_FromLong( y ) );
+   PyTuple_SET_ITEM( args, 3, TPython::ObjectProxy_FromVoidPtr( selected, "TObject" ) );
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::Dispatch( TVirtualPad* pad, TObject* obj, Int_t event )
+{
+   PyObject* args = PyTuple_New( 3 );
+   PyTuple_SET_ITEM( args, 0, TPython::ObjectProxy_FromVoidPtr( pad, "TVirtualPad" ) );
+   PyTuple_SET_ITEM( args, 1, TPython::ObjectProxy_FromVoidPtr( obj, "TObject" ) );
+   PyTuple_SET_ITEM( args, 2, PyInt_FromLong( event ) );
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::Dispatch( TGListTreeItem* item, TDNDData* data )
+{
+   PyObject* args = PyTuple_New( 2 );
+   PyTuple_SET_ITEM( args, 0, TPython::ObjectProxy_FromVoidPtr( item, "TGListTreeItem" ) );
+   PyTuple_SET_ITEM( args, 1, TPython::ObjectProxy_FromVoidPtr( data, "TDNDData" ) );
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::Dispatch( const char* name, const TList* attr )
+{
+   PyObject* args = PyTuple_New( 2 );
+   PyTuple_SET_ITEM( args, 0, PyString_FromString( name ) );
+   PyTuple_SET_ITEM( args, 1, TPython::ObjectProxy_FromVoidPtr( (void*)attr, "TList" ) );
+
+   PyObject* result = PyObject_CallObject( fCallable, args );
+   Py_XDECREF( args );
+
+   if ( ! result ) {
+      PyErr_Print();
+      return 0;
+   }
+
+   return result;
+}
+
+//____________________________________________________________________________
+PyObject* TPyDispatcher::Dispatch( TSlave* slave, TProofProgressInfo* pi )
+{
+   PyObject* args = PyTuple_New( 2 );
+   PyTuple_SET_ITEM( args, 0, TPython::ObjectProxy_FromVoidPtr( slave, "TSlave" ) );
+   PyTuple_SET_ITEM( args, 1, TPython::ObjectProxy_FromVoidPtr( pi, "TProofProgressInfo" ) );
 
    PyObject* result = PyObject_CallObject( fCallable, args );
    Py_XDECREF( args );
