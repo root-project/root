7cabe42e761268e517bd2eb8e2505061330f8178
Philippe Canal <pcanal@fnal.gov>
2014-06-20 08:21:12 -0500

    Update CINT limitation url

--- core/base/src/TSystem.cxx
+++ core/base/src/TSystem.cxx
@@ -3611,7 +3611,7 @@ int TSystem::CompileMacro(const char *filename, Option_t *opt,
          if (!compilationResult) {
             ::Info("ACLiC","The compiler has not found any problem with your macro.\n"
             "\tProbably your macro uses something rootcint can't parse.\n"
-            "\tCheck http://root.cern.ch/viewvc/trunk/cint/doc/limitati.txt for Cint's limitations.");
+            "\tCheck http://root.cern.ch/viewvc/branches/v5-34-00-patches/cint/doc/limitati.txt for Cint's limitations.");
             TString objfile=expFileName;
             Ssiz_t len=objfile.Length();
             objfile.Replace(len-extension.Length(), len, GetObjExt());
