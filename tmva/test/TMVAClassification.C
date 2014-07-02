fb1db24e00f82c3d6d596e5a8bdcf56da3343fd4
Pere Mato <pere.mato@cern.ch>
2014-07-02 08:39:18 +0200

    Changes to illustrate how to add a macro parameter in a test definition.

--- tmva/test/TMVAClassification.C
+++ tmva/test/TMVAClassification.C
@@ -69,13 +69,9 @@ void TMVAClassification( TString myMethodList = "" )
    TMVA::Tools::Instance();
 
    // to get access to the GUI and all tmva macros
-    TString tmva_dir(TString(gRootDir) + "/tmva");
-    if(gSystem->Getenv("TMVASYS"))
-       tmva_dir = TString(gSystem->Getenv("TMVASYS"));
-    gROOT->SetMacroPath(tmva_dir + "/test/:" + gROOT->GetMacroPath() );
-    gROOT->ProcessLine(".L TMVAGui.C");
-
-
+   TString thisdir = gSystem->DirName(gInterpreter->GetCurrentMacroName());
+   gROOT->SetMacroPath(thisdir + ":" + gROOT->GetMacroPath());
+   gROOT->ProcessLine(".L TMVAGui.C");
 
    // Default MVA methods to be trained + tested
    std::map<std::string,int> Use;
