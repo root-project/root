9b395ab2d91795375b2fc025862c09b4d1de2a2f
Danilo Piparo <danilo.piparo@cern.ch>
2014-07-07 11:43:51 +0200

    Allow test to succed with delayed payload parsing

--- root/io/newdelete/runClasses.C
+++ root/io/newdelete/runClasses.C
@@ -1,5 +1,7 @@
 {
    gSystem->Load("./Classes");
+   // This is necessary with delayed header parsing until functions are fwd declared
+   gROOT->ProcessLine("#include \"classesFuncFwdDecl.h\"");
 #ifdef ClingWorkAroundMissingDynamicScope
    gSystem->Exit(!gROOT->ProcessLine("Classes();"));
 #else
