84b89239e6ddf41852ec8e4fe150b023ad0d36c5
Philippe Canal <pcanal@fnal.gov>
2014-07-11 15:11:50 -0500

    Fix ROOT-6445 (TThread initialization prevent TApplication::Terminate() without user input)
    if the TThread Timer frequency is less or equal to kItimerResolution, TUnixSystem::DispatchOneEvent i
    can not exit and have its caller react to the other TTimer's actions (like the request
    to stop the event loop) until there is another type of event.

--- core/thread/inc/TThread.h
+++ core/thread/inc/TThread.h
@@ -212,7 +212,10 @@ public:
 
 class TThreadTimer : public TTimer {
 public:
-   TThreadTimer(Long_t ms = 10);
+   // if this time is less or equal to kItimerResolution, TUnixSystem::DispatchOneEvent i
+   // can not exit and have its caller react to the other TTimer's actions (like the request
+   // to stop the event loop) until there is another type of event.
+   TThreadTimer(Long_t ms = kItimerResolution + 10);
    Bool_t Notify();
 };
 
