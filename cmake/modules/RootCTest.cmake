b0e1312e597e4c16a2e8d743ca6c683dffdb5e66
Pere Mato <pere.mato@cern.ch>
2014-07-18 14:21:51 +0200

    Ensure that the test executables for not pollute the /bin and /lib directories also for Windows :-)

--- cmake/modules/RootCTest.cmake
+++ cmake/modules/RootCTest.cmake
@@ -30,6 +30,15 @@ unset(CMAKE_LIBRARY_OUTPUT_DIRECTORY)
 unset(CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
 unset(CMAKE_RUNTIME_OUTPUT_DIRECTORY)
 
+if(WIN32)
+  foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+    string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+    unset(CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG})
+    unset(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG})
+    unset(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG})
+  endforeach()
+endif()
+
 #--Add all subdirectories with tests-----------------------------------------------------------
 
 get_property(test_dirs GLOBAL PROPERTY ROOT_TEST_SUBDIRS)
