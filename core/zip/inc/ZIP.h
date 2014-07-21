927598b50e42cae0398a2a88a242eb944fff5e1f
Philippe Canal <pcanal@fnal.gov>
2014-07-21 17:39:35 -0500

    Attempt to fix windows build (__thread gcc only keyword is used)

--- core/zip/inc/ZIP.h
+++ core/zip/inc/ZIP.h
@@ -110,7 +110,11 @@ unsigned R__bi_reverse OF((unsigned value, int length));
 void     R__bi_windup  OF((void));
 void     R__copy_block OF((char far *buf, unsigned len, int header));
 int      R__seekable   OF((void));
+#ifdef _MSC_VER  
+extern __declspec( thread )  int (*R__read_buf) OF((char *buf, unsigned size));
+#else
 extern   __thread int (*R__read_buf) OF((char *buf, unsigned size));
+#endif
 ulg      R__memcompress OF((char *tgt, ulg tgtsize, char *src, ulg srcsize));
 void     R__error      OF((char *h));
 
