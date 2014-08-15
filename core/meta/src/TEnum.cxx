f1219d24482b6b9a7dd5a3e5d2631591c8cec138
Danilo Piparo <danilo.piparo@cern.ch>
2014-08-15 15:20:13 +0200

    Correct coding conventions.

--- core/meta/src/TEnum.cxx
+++ core/meta/src/TEnum.cxx
@@ -27,8 +27,8 @@
 ClassImp(TEnum)
 
 //______________________________________________________________________________
-TEnum::TEnum(const char* name, void* info, TClass* cls)
-   :fInfo(info), fClass(cls)
+TEnum::TEnum(const char *name, void *info, TClass *cls)
+   : fInfo(info), fClass(cls)
 {
    //Constructor for TEnum class.
    //It take the name of the TEnum type, specification if it is global
@@ -48,7 +48,7 @@ TEnum::~TEnum()
 }
 
 //______________________________________________________________________________
-void TEnum::AddConstant(TEnumConstant* constant)
+void TEnum::AddConstant(TEnumConstant *constant)
 {
    //Add a EnumConstant to the list of constants of the Enum Type.
    fConstantList.Add(constant);
@@ -83,52 +83,52 @@ Long_t TEnum::Property() const
 //______________________________________________________________________________
 void TEnum::Update(DeclId_t id)
 {
-   fInfo = (void*)id;
+   fInfo = (void *)id;
 }
 
 //______________________________________________________________________________
-TEnum* TEnum::GetEnum(const std::type_info& ti)
+TEnum *TEnum::GetEnum(const std::type_info &ti)
 {
-   int errorCode=0;
-   char* demangledEnumName = TClassEdit::DemangleName(ti.name(),errorCode);
+   int errorCode = 0;
+   char *demangledEnumName = TClassEdit::DemangleName(ti.name(), errorCode);
 
-   if (errorCode != 0){
-      if (!demangledEnumName){
+   if (errorCode != 0) {
+      if (!demangledEnumName) {
          free(demangledEnumName);
       }
       std::cerr << "ERROR TEnum::GetEnum - A problem occurred while demangling name.\n";
       return nullptr;
    }
 
-   const char* constDemangledEnumName = demangledEnumName;
-   TEnum* en = TEnum::GetEnum(constDemangledEnumName);
+   const char *constDemangledEnumName = demangledEnumName;
+   TEnum *en = TEnum::GetEnum(constDemangledEnumName);
    free(demangledEnumName);
    return en;
 
 }
 
 //______________________________________________________________________________
-TEnum* TEnum::GetEnum(const char* enumName)
+TEnum *TEnum::GetEnum(const char *enumName)
 {
 
-   const char* lastPos = strrchr(enumName,':');
+   const char *lastPos = strrchr(enumName, ':');
 
-   if (lastPos != nullptr){
+   if (lastPos != nullptr) {
       // We have a scope
       // All of this C gymnastic is to avoid allocations on the heap
-      const char* enName = lastPos+1;
-      auto enScopeNameSize = ((Long64_t)lastPos-(Long64_t)enumName)/sizeof(char) - 1;
-      char enScopeName[enScopeNameSize+1]; // +1 for the terminating character '\0'
-      strncpy(enScopeName, enumName, enScopeNameSize );
-      enScopeName[enScopeNameSize]='\0';
-      if (TClass* scope = TClass::GetClass(enScopeName)){
-         if (TEnum* en = static_cast<TEnum*>(scope->GetListOfEnums()->FindObject(enName))){
+      const char *enName = lastPos + 1;
+      auto enScopeNameSize = ((Long64_t)lastPos - (Long64_t)enumName) / sizeof(char) - 1;
+      char enScopeName[enScopeNameSize + 1]; // +1 for the terminating character '\0'
+      strncpy(enScopeName, enumName, enScopeNameSize);
+      enScopeName[enScopeNameSize] = '\0';
+      if (TClass *scope = TClass::GetClass(enScopeName)) {
+         if (TEnum *en = static_cast<TEnum *>(scope->GetListOfEnums()->FindObject(enName))) {
             return en;
          }
       }
    } else {
       // We don't have any scope: this is a global enum
-      if (TEnum* en = static_cast<TEnum*>(gROOT->GetListOfEnums()->FindObject(enumName)))
+      if (TEnum *en = static_cast<TEnum *>(gROOT->GetListOfEnums()->FindObject(enumName)))
          return en;
    }
 
