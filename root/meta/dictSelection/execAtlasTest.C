917bcc62e0fd4ce6be06d35217f2109fe67a76cc
Danilo Piparo <danilo.piparo@cern.ch>
2014-08-18 14:11:16 +0200

    Remove workaround of https://sft.its.cern.ch/jira/browse/ROOT-6319

--- root/meta/dictSelection/execAtlasTest.C
+++ root/meta/dictSelection/execAtlasTest.C
@@ -3,11 +3,11 @@
 int execAtlasTest(){
 
    std::cout << "Checking normalisation of names:\n";
-   
+
    // As a warm-up
    printNames("std::string");
    printNames("std::vector<std::string>");
-   
+
    // Load the atlas lib and check some names
    if (0!=gSystem->Load("libAtlasTest_dictrflx.so"))
       std::cerr << "Error loading dictionary library.\n";
@@ -18,6 +18,6 @@ int execAtlasTest(){
 
    printNames("Atlas::ClassA<Atlas::ClassB, float>");
    printNames("Atlas::ClassA<Atlas::ClassC, int>");
-   
+
 return 0;
 }
