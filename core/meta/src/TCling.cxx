5ba13c93caba5b62fd6fe521e6ee645db29b81fa
Danilo Piparo <danilo.piparo@cern.ch>
2014-08-20 18:13:41 +0200

    Fix header parsing on demand test.

--- core/meta/src/TCling.cxx
+++ core/meta/src/TCling.cxx
@@ -4668,11 +4668,12 @@ Int_t TCling::AutoParse(const char *cls)
    Int_t nHheadersParsed = 0;
 
    // Loop on the possible autoparse keys
-   std::vector<std::string> autoparseKeys({cls});
+   std::vector<std::string> autoparseKeys;
    if (strchr(cls, '<')) {
       int nestedLoc = 0;
       TClassEdit::GetSplit(cls, autoparseKeys, nestedLoc, TClassEdit::kDropTrailStar);
    }
+   autoparseKeys.emplace_back(cls);
 
    for (const auto & apKeyStr : autoparseKeys) {
       const char *apKey = apKeyStr.c_str();
