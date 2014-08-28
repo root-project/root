12971d59ce80b5ddf082bcc038b870d99a4f295d
Pere Mato <mato@cern.ch>
2014-08-28 17:32:48 +0200

    Ignoring test tutorial-roofit-rf509_wsinteractive for windoes

--- tutorials/CTestCustom.cmake
+++ tutorials/CTestCustom.cmake
@@ -1,5 +1,4 @@
 #---Custom CTest settings---------------------------------------------------
-
-#set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} tutorial-pyroot-zdemo)
-
-
+if(WIN32)
+  list(APPEND CTEST_CUSTOM_TESTS_IGNORE tutorial-roofit-rf509_wsinteractive)   #  workspaces as namespace does not work on windows 
+endif()
