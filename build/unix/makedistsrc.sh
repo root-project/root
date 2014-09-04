d9f8a0bbbd7c5fab2ed33d64d8b19e4ed8a60809
Pere Mato <pere.mato@cern.ch>
2014-09-04 17:53:51 +0200

    Avoid alias for git commands

--- build/unix/makedistsrc.sh
+++ build/unix/makedistsrc.sh
@@ -5,7 +5,7 @@ ROOTVERS=`cat build/version_number | sed -e 's/\//\./'`
 TYPE=source
 TARFILE=root_v$ROOTVERS.$TYPE.tar
 
-git co $CURVERS
+git checkout $CURVERS
 # generate etc/gitinfo.txt
 build/unix/gitinfo.sh
 
