bc31502beadd8f827ab42c9e206229a1b679c153
Danilo Piparo <danilo.piparo@cern.ch>
2014-09-05 21:51:25 +0200

    Update reference comparison script for ctest.

--- root/meta/autoloading/headerParsingOnDemand/FullheaderParsingOnDemand_convert.sh
+++ root/meta/autoloading/headerParsingOnDemand/FullheaderParsingOnDemand_convert.sh
@@ -1,3 +1,4 @@
 grep -v "Warning in <TInterpreter::RegisterModule()>: Header" \
 | grep -v "status 0" | grep -v "memory" \
-| grep -v "__functional_base"|grep -v "string" |grep -v TRint
+| grep -v "__functional_base"|grep -v "string" |grep -v TRint \
+|grep -v "dtor called for"
