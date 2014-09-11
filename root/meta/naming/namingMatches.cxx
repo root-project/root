d97a348de7b4be4e4b4f3a93ff3f75cda304856d
Philippe Canal <pcanal@fnal.gov>
2014-09-11 11:23:24 -0500

    Add that we find the class (and its info) given various spelling

---
+++ root/meta/naming/namingMatches.cxx
@@ -0,0 +1,41 @@
+#ifndef namingMatches_cxx
+#define namingMatches_cxx
+
+#include <vector>
+#include <list>
+#include <atomic>
+
+class Object {};
+
+template <typename T, typename O = Object> class Wrapper {};
+
+template <typename T> class Container
+{
+public:
+   vector<Wrapper<T> > fValues;
+};
+
+class HolderAuto {
+public:
+   list<Wrapper<int> > fValues;
+};
+
+class Holder {
+public:
+   typedef vector<Wrapper<int> > value_t;
+   value_t fContainer;
+   vector<Wrapper<int> > fValues;
+   //vector<Wrapper<int, Object> > fValues2;
+   std::atomic<vector<Wrapper<int> >*> fAtom;
+};
+
+#endif
+
+#ifdef __ROOTCLING__
+#pragma link C++ class Wrapper<int>+;
+#pragma link C++ class Object+;
+#pragma link C++ class vector<Wrapper<int> >+;
+#pragma link C++ class atomic<vector<Wrapper<int> >*>;
+#pragma link C++ class Container<int>+;
+#endif
+
