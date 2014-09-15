0efc33ba121351ac6dd5f42d137cdf1a04225ec1
Pere Mato <mato@cern.ch>
2014-09-15 12:26:46 +0200

    Avoid re-building simple.root while other tests are also re-building it. Disable some vc-sse tests for icc compiler.

--- test/CTestCustom.cmake
+++ test/CTestCustom.cmake
@@ -1,7 +1,15 @@
 #---Custom CTest settings---------------------------------------------------
 
-set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} test-stressgui)
+set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} 
+    test-stressgui)
 
 if(WIN32)
-  set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} test-tcollex)
+  set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} 
+      test-tcollex)
 endif()
+
+if(CTEST_BUILD_NAME MATCHES icc14)  #  sse tests of vc fail for icc compiler
+  set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE}
+      vc-math_sse
+      vc-math_VC_LOG_ILP2_sse
+      vc-math_VC_LOG_ILP_sse)
