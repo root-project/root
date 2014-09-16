18cafb18f60b885629879014ea1da9e35d3a0bb2
Pere Mato <mato@cern.ch>
2014-09-16 17:54:16 +0200

    Disable vc (see) tests for MacOSX 10.7. Move the copying of CTestCustom.cmake to main CMakeLists.txt.

--- test/CTestCustom.cmake
+++ test/CTestCustom.cmake
@@ -15,3 +15,17 @@ if(CTEST_BUILD_NAME MATCHES icc14)  #  sse tests of vc fail for icc compiler
       vc-math_VC_LOG_ILP_sse)
 endif()
 
+
+if(CTEST_BUILD_NAME MATCHES mac107)  #  sse tests of vc fail for mac107 compiler
+  set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE}
+      vc-gather_VC_USE_BSF_GATHERS_sse
+      vc-gather_VC_USE_SET_GATHERS_sse
+      vc-load-sse
+      vc-math-sse
+      vc-math_VC_LOG_ILP2_sse
+      vc-math_VC_LOG_ILP_sse
+      vc-scalaraccess_sse
+      vc-swizzles_sse
+      vc-utils_sse)
+endif()
+
