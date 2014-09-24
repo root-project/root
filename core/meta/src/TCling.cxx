89bd5e8e78db0188667f7ec6c4af14be0eb4b98d
Danilo Piparo <danilo.piparo@cern.ch>
2014-09-24 14:52:26 +0200

    Turn off association of headers to pcms: not needed by ROOT pcms
    and problematic for CMS and Atlas: ROOT-6739

--- core/meta/src/TCling.cxx
+++ core/meta/src/TCling.cxx
@@ -1306,8 +1306,8 @@ bool TCling::LoadPCM(TString pcmFileName,
    // Note: Declaring the relationship between the module (pcm) and the header
    // probably does not yet make sense since the pcm is 'only' a root file.
    // We also have to review if we still need to do this with the delay loading.
-   clang::CompilerInstance* CI = fInterpreter->getCI();
-   ROOT::TMetaUtils::declareModuleMap(CI, pcmFileName, headers);
+   // clang::CompilerInstance* CI = fInterpreter->getCI();
+   // ROOT::TMetaUtils::declareModuleMap(CI, pcmFileName, headers);
    return kTRUE;
 }
 
