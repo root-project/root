5b42938e416c2eab7af1985237bca2ed5474dea0
Christopher Jones <chrisdjones15@gmail.com>
2014-10-14 13:36:32 -0500

    Synchronize construction of TApplication
    It was possible for multiple threads to attempt construction of a
    TApplication or for one thread to think the TApplication was already
    constructed while another thread was in the process of doing the
    construction.
    
    Conflicts:
    	core/base/inc/TROOT.h

--- core/base/inc/TROOT.h
+++ core/base/inc/TROOT.h
@@ -35,6 +35,8 @@
 #include "RConfigure.h"
 #endif
 
+#include <atomic>
+
 class TClass;
 class TCanvas;
 class TColor;
@@ -96,7 +98,11 @@ protected:
    TString         fGitBranch;            //Git branch
    TString         fGitDate;              //Date and time when make was run
    Int_t           fTimer;                //Timer flag
+#if __cplusplus >= 201103L
+   std::atomic<TApplication*> fApplication;         //Pointer to current application
+#else
    TApplication    *fApplication;         //Pointer to current application
+#endif
    TInterpreter    *fInterpreter;         //Command interpreter
    Bool_t          fBatch;                //True if session without graphics
    Bool_t          fEditHistograms;       //True if histograms can be edited with the mouse
