d9a7340eae4e0ebfba7b50e8db3ad41b7f580bd7
Pere Mato <pere.mato@cern.ch>
2014-10-15 23:34:50 +0200

    Disable test-stressproof for the time being

--- test/CTestCustom.cmake
+++ test/CTestCustom.cmake
@@ -1,6 +1,6 @@
 #---Custom CTest settings---------------------------------------------------
 
-set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} test-stressgui)
+set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} test-stressgui test-stressproof)
 
 if(WIN32)
   set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} test-tcollex)
