888bb5b6d5afccf7cdcd4c00b54cbb8d78ff191e
Axel Naumann <Axel.Naumann@cern.ch>
2014-10-16 13:45:34 +0200

    Enable make dist for out of source builds.

--- build/unix/makedistsrc.sh
+++ build/unix/makedistsrc.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
 
-CURVERS=`cat build/version_number | sed -e "s/^/v/" -e "s/\./-/" -e "s/\//-/"`
-ROOTVERS=`cat build/version_number | sed -e 's/\//\./'`
+ROOTSRCDIR=$1
+
+CURVERS=`cat $ROOTSRCDIR/build/version_number | sed -e "s/^/v/" -e "s/\./-/" -e "s/\//-/"`
+ROOTVERS=`cat $ROOTSRCDIR/build/version_number | sed -e 's/\//\./'`
 TYPE=source
 TARFILE=root_v$ROOTVERS.$TYPE.tar
 
-git checkout $CURVERS
+( cd $ROOTSRCDIR; git checkout $CURVERS )
 # generate etc/gitinfo.txt
-build/unix/gitinfo.sh
+$ROOTSRCDIR/build/unix/gitinfo.sh
 
-git archive -v -o ../$TARFILE --prefix=root-$ROOTVERS/ $CURVERS
+( cd $ROOTSRCDIR; git archive -v -o ../$TARFILE --prefix=root-$ROOTVERS/ $CURVERS )
 
 mkdir -p etc/root-$ROOTVERS/etc
 cp etc/gitinfo.txt etc/root-$ROOTVERS/etc/
