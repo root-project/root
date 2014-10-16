a35bea435c37e8ec478fa32d83be58ef42776cee
Axel Naumann <Axel.Naumann@cern.ch>
2014-10-16 10:12:28 +0200

    Split makeonepcm into configuration, generation; parametrize output file name.
    makepchinput.sh determines which headers, linkdefs, flags are to be used for
    ROOT's PCH. It is called by ROOT's Makefile.
    
    makepch.sh uses that information to produce a PCH; it takes optional additional
    arguments to include user headers in a user PCH replacing the default one from
    ROOT.

---
+++ build/unix/makepch.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# Build a pch for the headers and linkdefs in root-build-dir/etc/dictpch/.
+# root-build-dir is first tried as ./ - if that doesn't exist, $ROOTSYS
+# is taken as root-build-dir.
+#
+# $1: PCH output file name
+# $2: cxxflags (optional; required if extra headers are supplied)
+# $3: extra headers to be included in the PCH (optional)
+#
+# exit code 1 for invocation errors; else exit code of rootcling invocation.
+#
+# Copyright (c) 2014 Rene Brun and Fons Rademakers
+# Author: Axel Naumann <axel@cern.ch>, 2014-10-16
+
+rootdir=.
+cfgdir=etc/dictpch
+allheaders=$cfgdir/allHeaders.h
+alllinkdefs=$cfgdir/allLinkdefs.h
+cppflags=$cfgdir/allCppflags.txt
+pch=$1
+shift
+
+if [ "x$pch" = "x" ]; then
+    echo 'Output PCH file name must be passed as first argument!' >& 2
+    exit 1
+fi
+
+if ! [ -f $rootdir/$allheaders ]; then
+    rootdir=$ROOTSYS
+    if ! [ -f $rootdir/$allheaders ]; then
+        echo 'Neither ./'$allheaders' nor $ROOTSYS/'$allheaders' exists!' >& 2
+        exit 1
+    fi
+fi
+
+cxxflags="-D__CLING__ -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -I$rootdir/include -I$rootdir/etc -I$rootdir/etc/cling `cat $cppflags`"
+
+if ! [ "x$1" = "x" ]; then
+    cxxflags="$cxxflags $1"
+fi
+
+# generate one large pcm
+rm -f allDict.* lib/allDict_rdict.pc*
+touch allDict.cxx.h
+core/utils/src/rootcling_tmp -1 -f allDict.cxx -noDictSelection -c $cxxflags $allheaders $@ $alllinkdefs
+res=$?
+if [ $res -eq 0 ] ; then
+  mv allDict_rdict.pch $pch
+  res=$?
+fi
+rm -f allDict.* lib/allDict_rdict.pc*
+
+exit $res
---
+++ build/unix/makepchinput.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+#
+# Extract the input needed to build a PCH for the main (enabled) ROOT modules.
+# Script takes as argument the source directory path, the set of enabled
+# modules and extra headers (from cling) to be included in the PCH.
+#
+# Copyright (c) 2014 Rene Brun and Fons Rademakers
+# Author: Axel Naumann <axel@cern.ch>, 2014-10-16
+
+srcdir=$1
+shift
+modules=$1
+shift
+
+# Remove leftover files from old versions of this script.
+rm -f include/allHeaders.h include/allHeaders.h.pch include/allLinkDef.h all.h cppflags.txt include/allLinkDef.h etc/allDict.cxx etc/allDict.cxx.h
+
+outdir=etc/dictpch
+allheaders=$outdir/allHeaders.h
+alllinkdefs=$outdir/allLinkdefs.h
+cppflags=$outdir/allCppflags.txt
+
+mkdir -p $outdir
+rm -f $allheaders $alllinkdefs
+
+while ! [ "x$1" = "x" ]; do
+    echo '#include "'$1'"' >> $allheaders
+    shift
+done
+
+for dict in `find $modules -name 'G__*.cxx' 2> /dev/null | grep -v /G__Cling.cxx  | grep -v core/metautils/src/G__std_`; do
+    dirname=`dirname $dict`                   # to get foo/src
+    dirname=`echo $dirname | sed -e 's,/src$,,' -e 's,^[.]/,,' ` # to get foo/
+
+    case $dirname in
+        graf2d/qt | math/fftw | math/foam | math/fumili | math/mlp | math/quadp | math/splot | math/unuran | math/vc | math/vdt) continue;;
+
+        interpreter/* | core/* | io/io | net/net | math/* | hist/* | tree/* | graf2d/* | graf3d/gl | gui/gui | gui/fitpanel | rootx | bindings/pyroot | roofit/* | tmva | main) ;;
+
+        *) continue;;
+    esac
+
+    # Check if selmodules already contains the dirname.
+    # Happens for instance for math/smatrix with its two (32bit and 64 bit)
+    # dictionaries.
+    if ! ( echo $selmodules | grep "$dirname " > /dev/null ); then
+        selmodules="$selmodules$dirname "
+    fi
+
+    awk 'BEGIN{START=-1} /includePaths\[\] = {/, /^0$/ { if (START==-1) START=NR; else if ($0 != "0") { sub(/",/,"",$0); sub(/^"/,"-I",$0); print $0 } }' $dict >> $cppflags.tmp
+    echo "// $dict" >> $allheaders
+#     awk 'BEGIN{START=-1} /payloadCode =/, /^;$/ { if (START==-1) START=NR; else if ($1 != ";") { code=substr($0,2); sub(/\\n"/,"",code); print code } }' $dict >> $allheaders
+    awk 'BEGIN{START=-1} /headers\[\] = {/, /^0$/ { if (START==-1) START=NR; else if ($0 != "0") { sub(/,/,"",$0); print "#include",$0 } }' $dict >> $allheaders
+
+    if ! test "$dirname" = "`echo $dirname| sed 's,/qt,,'`"; then
+        # something qt; undef emit afterwards
+        cat <<EOF >> $allheaders
+#ifdef emit
+# undef emit
+#endif
+#ifdef signals
+# undef signals
+#endif
+EOF
+    elif ! test "$dirname" = "`echo $dirname| sed 's,net/ldap,,'`"; then
+        # ldap; undef Debug afterwards
+        cat <<EOF >> $allheaders
+#ifdef Debug
+# undef Debug
+#endif
+#ifdef GSL_SUCCESS
+# undef GSL_SUCCESS
+#endif
+EOF
+    fi
+
+    find $srcdir/$dirname/inc/ -name '*LinkDef*.h' | \
+        sed -e 's|^|#include "|' -e 's|$|"|' >> $alllinkdefs
+done
+
+echo
+echo Generating PCH for ${selmodules}
+echo
