a9c69e25b7c6668590da2a84d61abcbc94280191
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-11-10 16:17:32 +0000

    fixes for cmake and configure building systems with cmaes

--- etc/plugins/ROOT@@Math@@Minimizer/P090_TCMAESMinimizer.C
+++ etc/plugins/ROOT@@Math@@Minimizer/P090_TCMAESMinimizer.C
@@ -1,5 +1,5 @@
 void P090_TCMAESMinimizer()
 {
   gPluginMgr->AddHandler("ROOT::Math::Minimizer", "cmaes", "ROOT::cmaes::TCMAESMinimizer",
-			 "cmaesroot", "TCMAESMinimizer(const char *)");
+			 "cmaes_root", "TCMAESMinimizer(const char *)");
 }
