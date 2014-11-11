db8f8bb27c6b6f88fd9afa72392cfbe779f06c05
Gordon Watts <gordon@gordonandpaula.net>
2014-11-11 02:54:31 +0100

    Fix up x64 porting issues
    - Using DWORD s as pointers doesn't work.
    - int and size_t have changed relative sizes, causing lots of build
    warnings.

--- build/win/bindexplib/bindexplib.cxx
+++ build/win/bindexplib/bindexplib.cxx
@@ -149,7 +149,7 @@ GetSZStorageClass(BYTE storageClass)
 void AddHex(std::string& buf, long val, bool caps=false)
 {
    buf += "0x";
-   int len=buf.length();
+   size_t len=buf.length();
    while (val) {
       char hex = (char)(val & 16);
       val = val >> 4;
@@ -340,7 +340,7 @@ DumpExternals(PIMAGE_SYMBOL pSymbolTable, FILE *fout, unsigned cSymbols)
 + * Utility func, strstr with size
 + */
 const char* StrNStr(const char* start, const char* find, size_t &size) {
-   int len;
+   size_t len;
    const char* hint;
 
    if (!start || !find || !size) {
@@ -530,14 +530,14 @@ DumpObjFile(PIMAGE_FILE_HEADER pImageFileHeader, FILE *fout, int full, int fort)
 {
    PIMAGE_SYMBOL PCOFFSymbolTable;
    PIMAGE_SECTION_HEADER PCOFFSectionHeaders;
-   DWORD COFFSymbolCount;
+   DWORD_PTR COFFSymbolCount;
 
    PCOFFSymbolTable = (PIMAGE_SYMBOL)
-      ((DWORD)pImageFileHeader + pImageFileHeader->PointerToSymbolTable);
+      ((DWORD_PTR)pImageFileHeader + pImageFileHeader->PointerToSymbolTable);
    COFFSymbolCount = pImageFileHeader->NumberOfSymbols;
 
    PCOFFSectionHeaders = (PIMAGE_SECTION_HEADER)
-      ((DWORD)pImageFileHeader          +
+      ((DWORD_PTR)pImageFileHeader          +
       IMAGE_SIZEOF_FILE_HEADER +
       pImageFileHeader->SizeOfOptionalHeader);
 
@@ -721,7 +721,7 @@ Usage:
          */
          TCHAR *filename = argv[arg];
          TCHAR path[2048];
-         int i = strlen(filename);
+         size_t i = strlen(filename);
          i--;
          while( filename[i] != '\\' && filename[i] != '/'  && i >=0) i--;
          do
