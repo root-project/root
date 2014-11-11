9e1852692b9d7548b06fe7ab437f4dc725dd734d
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2014-11-11 11:42:58 +0100

    Fix TStatistic for merging dataset and when the weights are equal to zero (see http://root.cern.ch/phpBB3/viewtopic.php?f=3&t=18856) Add also a test program for the class

--- math/mathcore/inc/TStatistic.h
+++ math/mathcore/inc/TStatistic.h
@@ -49,11 +49,12 @@ private:
    Long64_t    fN;       // Number of fills
    Double_t    fW;       // Sum of weights
    Double_t    fW2;      // Sum of weights**2
-   Double_t    fMean;    // Mean
+   Double_t    fM;       // Sum of elements ( i.e. mean * sum_of_weights)
    Double_t    fM2;      // Second order momentum
 
 public:
-   TStatistic(const char *name = "") : fName(name), fN(0), fW(0.), fW2(0.), fMean(0.), fM2(0.) { }
+
+   TStatistic(const char *name = "") : fName(name), fN(0), fW(0.), fW2(0.), fM(0.), fM2(0.) { }
    TStatistic(const char *name, Int_t n, const Double_t *val, const Double_t *w = 0);
    ~TStatistic() { }
 
@@ -61,64 +62,27 @@ public:
    const char    *GetName() const { return fName; }
    ULong_t        Hash() const { return fName.Hash(); }
 
-   inline const Long64_t &GetN() const { return fN; }
-   inline const Double_t &GetM2() const { return fM2; }
-   inline const Double_t &GetMean() const { return fMean; }
-   inline       Double_t  GetMeanErr() const { if (fW > 0.) return TMath::Sqrt(fM2 / fW2 / fW);
-                                               return 0.; }
-   inline       Double_t  GetRMS() const { if (fW > 0.) { return TMath::Sqrt(fM2 / fW); } return -1; }
-   inline       Double_t  GetVar() const { if (fW > 0.) {
-                                            if (fN > 1) { return (fM2 / fW)*(fN / (fN-1.)); }
-                                            return 0; } return -1.; }
-   inline       Double_t  GetVarN() const { if (fW > 0.) { return fM2 / fW; } return -1.; }
-   inline const Double_t &GetW() const { return fW; }
-   inline const Double_t &GetW2() const { return fW2; }
+   inline       Long64_t GetN() const { return fN; }
+   inline       Long64_t GetNeff() const { return fW*fW/fW2; }
+   inline       Double_t GetM2() const { return fM2; }
+   inline       Double_t GetMean() const { return (fW > 0) ? fM/fW : 0; }
+   inline       Double_t GetMeanErr() const { return  (fW > 0.) ?  TMath::Sqrt( GetVar()/ GetNeff() ) : 0; }
+   inline       Double_t GetRMS() const { double var = GetVar(); return (var>0) ? TMath::Sqrt(var) : -1; }  
+   inline       Double_t GetVar() const { return (fW>0) ? ( (fN>1) ? (fM2 / fW)*(fN / (fN-1.)) : 0 ) : -1; }
+   inline       Double_t GetW() const { return fW; }
+   inline       Double_t GetW2() const { return fW2; }
 
    // Merging
    Int_t Merge(TCollection *in);
 
    // Fill
-   inline void Fill(const Double_t &val, Double_t w = 1.) {
-      fN++;
-      // Incremental quantities
-      Double_t tW = w + fW;
-      Double_t dt = val - fMean ;
-      Double_t rr = dt * w / tW ;
-      fMean += rr;
-      fM2 += fW * dt * rr;
-      fW = tW;
-      fW2 += w*w;
-   }
+   void Fill(Double_t val, Double_t w = 1.);
 
    // Print
+   void Print(Option_t * = "") const;
    void ls(Option_t *opt = "") const { Print(opt); }
-   void Print(Option_t * = "") const {
-      // Print this parameter content
-      TROOT::IndentLevel();
-      Printf(" OBJ: TStatistic\t %s = %.3g +- %.3g \t RMS = %.3g \t N = %lld",
-             fName.Data(), fMean, GetMeanErr(), GetRMS(), fN);
-   }
 
-   ClassDef(TStatistic,1)  //Named statistical variable
+   ClassDef(TStatistic,2)  //Named statistical variable
 };
 
-// Implementation of Merge
-inline Int_t TStatistic::Merge(TCollection *in) {
-   // Merge objects in the list.
-   // Returns the number of objects that were in the list.
-   TIter nxo(in);
-   Int_t n = 0;
-   while (TObject *o = nxo()) {
-      TStatistic *c = dynamic_cast<TStatistic *>(o);
-      if (c) {
-         fMean += c->GetMean();
-         fW += c->GetW();
-         fW2 += c->GetW2();
-         fM2 += c->GetM2();
-         fN += c->GetN();
-         n++;
-      }
-   }
-   return n;
-}
 #endif
--- math/mathcore/src/TStatistic.cxx
+++ math/mathcore/src/TStatistic.cxx
@@ -26,7 +26,7 @@ templateClassImp(TStatistic)
 
 //______________________________________________________________________________
 TStatistic::TStatistic(const char *name, Int_t n, const Double_t *val, const Double_t *w)
-         : fName(name), fN(0), fW(0.), fW2(0.), fMean(0.), fM2(0.)
+         : fName(name), fN(0), fW(0.), fW2(0.), fM(0.), fM2(0.)
 {
    // Constructor from a vector of values
 
@@ -39,4 +39,65 @@ TStatistic::TStatistic(const char *name, Int_t n, const Double_t *val, const Dou
          }
       }
    }
+}
+
+void TStatistic::Fill(Double_t val, Double_t w) {
+      // Incremental quantities
+   // use formula 1.4 in Chan-Golub, LeVeque
+   // Algorithms for computing the Sample Variance (1983)
+   // genralized by LM for the case of weights 
+
+   if (w == 0) return;
+
+   fN++;
+
+   Double_t tW = fW + w;
+   fM += w * val; 
+
+//      Double_t dt = val - fM ;
+   if (tW == 0) {
+      Warning("Fill","Sum of weights is zero - ignore current data point");
+      fN--;
+      return;
+   }
+   if (fW != 0) {  // from the second time
+      Double_t rr = ( tW * val - fM);
+      fM2 += w * rr * rr / (tW * fW);
+   }
+   fW = tW;
+   fW2 += w*w;
+}
+
+
+void TStatistic::Print(Option_t *) const {
+   // Print this parameter content
+   TROOT::IndentLevel();
+   Printf(" OBJ: TStatistic\t %s = %.5g +- %.4g \t RMS = %.5g \t N = %lld",
+          fName.Data(), GetMean(), GetMeanErr(), GetRMS(), fN);
+}
+
+
+// Implementation of Merge
+Int_t TStatistic::Merge(TCollection *in) {
+   // Merge objects in the list.
+   // Returns the number of objects that were in the list.
+   TIter nxo(in);
+   Int_t n = 0;
+   while (TObject *o = nxo()) {
+      TStatistic *c = dynamic_cast<TStatistic *>(o);
+      if (c) {
+         if (fW == 0 || c->fW == 0 || ((fW + c->fW) == 0) ) {
+            Error("Merge","Zero sum of weights - cannot merge data from %s",c->GetName() );
+            continue;
+         }
+         double temp = (c->fW)/(fW) * fM - c->fM; 
+         fM2 += c->fM2 + fW/(c->fW*(c->fW + fW) ) * temp * temp;  
+         fM  += c->fM;
+         fW  += c->fW;
+         fW2 += c->fW2;
+         fN  += c->fN;
+         n++;
+      }
+   }
+   return n;
 }
