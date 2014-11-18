0363a643238118ae95bf633f70b5088373ee9a36
Axel Naumann <Axel.Naumann@cern.ch>
2014-11-18 17:16:17 +0100

    Follow new named macro behavior (ROOT-6802).

--- cling/specialobj/runf02.C
+++ cling/specialobj/runf02.C
@@ -1,4 +1,4 @@
 // Test ROOT-6345: dynamic scope must repair if statements.
 void runf02(bool verbose = true) {
-  if (verbose) UNKNOWN("max\n"); // should complain about UNKNOWN
+  gROOT->ProcessLine("if (true) UNKNOWN(42)"); // should complain about UNKNOWN
 }
