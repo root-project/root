b9c40b15da0b37a2ecd9dedbbf9944090d381d0a
Pere Mato <pere.mato@cern.ch>
2014-11-25 11:47:31 +0100

    Use stderr for both prints inside and outside the loop

--- cling/controls/loopbreak.C
+++ cling/controls/loopbreak.C
@@ -1,7 +1,7 @@
 void loopbreak() {
   Int_t i=0;
   while (i++<100) {
-     printf ("in loop, i=%d\n",i);
+     fprintf (stderr,"in loop, i=%d\n",i);
      break;
   }
   fprintf (stderr,"after loop, i=%d\n",i);
