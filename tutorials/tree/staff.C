b382507da8dc67c07e1cd3d31076e383280e31f0
Pere Mato <pere.mato@cern.ch>
2014-12-02 13:17:03 +0100

    More on test dependencies and fix a typo in staff.C

--- tutorials/tree/staff.C
+++ tutorials/tree/staff.C
@@ -1,7 +1,7 @@
 void staff() {
    //to create cernstaff.root, execute tutorial $ROOTSYS/tree/cernbuild.C
    TString tutdir = gROOT->GetTutorialsDir();
-   TFile *f = TFile::Open(tutdir + /tree/cernstaff.root");
+   TFile *f = TFile::Open(tutdir + "/tree/cernstaff.root");
    TTree *T = (TTree*)f->Get("T");
    T->Draw("Grade:Age:Cost:Division:Nation","","gl5d");
 }
