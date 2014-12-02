7324bc64a007529539c6f91014ae2a05822cedfc
Sergey Linev <S.Linev@gsi.de>
2014-12-02 15:33:52 +0100

    1. Implement 'p' and 'p0' drawing options for TH1 2. Further improvements with drawing optimization 3. First try of drag-and-drop functionality for drawing superimpose 4. Handle attributes like TAttLine, TAttMarker, TAttFill at central place, use d3.call() functionality to apply attributes
    Signed-off-by: Bertrand Bellenot <bertrand.bellenot@cern.ch>

--- etc/http/scripts/JSRootCore.js
+++ etc/http/scripts/JSRootCore.js
@@ -14,7 +14,7 @@
 
    JSROOT = {};
 
-   JSROOT.version = "3.1 dev 25/11/2014";
+   JSROOT.version = "3.1 dev 2/12/2014";
 
    JSROOT.source_dir  = function(){
       var scripts = document.getElementsByTagName('script');
