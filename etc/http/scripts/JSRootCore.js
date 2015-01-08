b3bafe0c94d8db699745bc4c2d46601f79d33110
Sergey Linev <S.Linev@gsi.de>
2015-01-08 16:12:45 +0100

    jsroot: improve objects overlap
    Even when several TH1/TGraph/TF1 objects
    with fill attribute overlap each other,
    one able to get tooltip for underlying objects.
    
    Signed-off-by: Bertrand Bellenot <bertrand.bellenot@cern.ch>

--- etc/http/scripts/JSRootCore.js
+++ etc/http/scripts/JSRootCore.js
@@ -14,7 +14,7 @@
 
    JSROOT = {};
 
-   JSROOT.version = "3.2 dev 6/01/2015";
+   JSROOT.version = "3.2 dev 8/01/2015";
 
    JSROOT.source_dir = function(){
       var scripts = document.getElementsByTagName('script');
