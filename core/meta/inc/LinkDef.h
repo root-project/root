0be6bc49bd80d7373e76841eeed2744deb8a5b36
Christopher Jones <cdj@fnal.gov>
2015-02-15 18:07:27 +0100

    Made TListOfFunctions thread safe
    TlistOfFunctions can update itself over time, even from calls to
    const functions. Since the class instances are available globally
    they need to be threads safe. This required that all 'const' functions
    take the global lock. In addition, a new iterator was needed which
    takes the global lock on the call to Next().

--- core/meta/inc/LinkDef.h
+++ core/meta/inc/LinkDef.h
@@ -66,6 +66,7 @@
 #pragma link C++ class std::vector<std::pair<Int_t, Int_t> >+;
 #pragma link C++ class TFileMergeInfo;
 #pragma link C++ class TListOfFunctions+;
+#pragma link C++ class TListOfFunctionsIter;
 #pragma link C++ class TListOfFunctionTemplates+;
 #pragma link C++ class TListOfDataMembers-;
 #pragma link C++ class TListOfEnums+;
