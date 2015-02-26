4077ca3e5a562861d133ed0cae09ce1cc55e4f2b
Philippe Canal <pcanal@fnal.gov>
2015-02-26 14:09:52 -0600

    Cache whether the return type is a record type

--- core/meta/src/TClingCallFunc.cxx
+++ core/meta/src/TClingCallFunc.cxx
@@ -2132,8 +2132,8 @@ T TClingCallFunc::ExecT(void *address)
       // Sometimes we are called on a function returning void!
       return 0;
    }
-   const FunctionDecl *decl = fMethod->GetMethodDecl();
-   if (decl->getReturnType().getCanonicalType()->isRecordType())
+
+   if (fReturnIsRecordType)
       ((TCling *)gCling)->RegisterTemporary(ret);
    return sv_to<T>(ret);
 }
@@ -2306,6 +2306,8 @@ TInterpreter::CallFuncIFacePtr_t TClingCallFunc::IFacePtr()
       } else {
          fWrapper = make_wrapper();
       }
+
+      fReturnIsRecordType = decl->getReturnType().getCanonicalType()->isRecordType();
    }
    return TInterpreter::CallFuncIFacePtr_t(fWrapper);
 }
