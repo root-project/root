ada2a8067a052abd775faff178fe213ab076043e
Philippe Canal <pcanal@fnal.gov>
2015-02-26 12:53:35 -0600

    Add direct constructor from TClingMethodInfo to TClingCallFunc

--- core/meta/src/TClingCallFunc.h
+++ core/meta/src/TClingCallFunc.h
@@ -134,6 +134,12 @@ public:
       fMethod = new TClingMethodInfo(interp);
    }
 
+   explicit TClingCallFunc(TClingMethodInfo &minfo, const ROOT::TMetaUtils::TNormalizedCtxt &normCtxt)
+   : fInterp(minfo.GetInterpreter()), fNormCtxt(normCtxt), fWrapper(0), fIgnoreExtraArgs(false)
+   {
+      fMethod = new TClingMethodInfo(minfo);
+   }
+
    TClingCallFunc(const TClingCallFunc &rhs)
       : fInterp(rhs.fInterp), fNormCtxt(rhs.fNormCtxt), fWrapper(rhs.fWrapper), fArgVals(rhs.fArgVals),
         fIgnoreExtraArgs(rhs.fIgnoreExtraArgs)
