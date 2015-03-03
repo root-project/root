211a5b1e5e342f136337aea52783345d13b42dea
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2015-03-03 10:09:09 +0100

    Fix default constructor initialisation of RooTFn* classes. This fixes he problem reported at https://root.cern.ch/phpBB3/viewtopic.php?f=15&t=19314

--- roofit/roofit/src/RooTFnPdfBinding.cxx
+++ roofit/roofit/src/RooTFnPdfBinding.cxx
@@ -17,19 +17,19 @@ using namespace std;
 
 ClassImp(RooTFnPdfBinding) 
 
-RooTFnPdfBinding::RooTFnPdfBinding(const char *name, const char *title, TF1* _func, const RooArgList& _list) :
+RooTFnPdfBinding::RooTFnPdfBinding(const char *name, const char *title, TF1* func, const RooArgList& list) :
   RooAbsPdf(name,title), 
-  list("params","params",this),
-  func(_func)
+  _list("params","params",this),
+  _func(func)
 { 
-  list.add(_list) ;
+  _list.add(list) ;
 } 
 
 
 RooTFnPdfBinding::RooTFnPdfBinding(const RooTFnPdfBinding& other, const char* name) :  
   RooAbsPdf(other,name), 
-  list("params",this,other.list),
-  func(other.func)
+  _list("params",this,other._list),
+  _func(other._func)
 { 
 } 
 
@@ -37,10 +37,10 @@ RooTFnPdfBinding::RooTFnPdfBinding(const RooTFnPdfBinding& other, const char* na
 
 Double_t RooTFnPdfBinding::evaluate() const 
 { 
-  Double_t x = list.at(0) ? ((RooAbsReal*)list.at(0))->getVal() : 0 ;
-  Double_t y = list.at(1) ? ((RooAbsReal*)list.at(1))->getVal() : 0 ;
-  Double_t z = list.at(2) ? ((RooAbsReal*)list.at(2))->getVal() : 0 ;
-  return func->Eval(x,y,z) ;
+  Double_t x = _list.at(0) ? ((RooAbsReal*)_list.at(0))->getVal() : 0 ;
+  Double_t y = _list.at(1) ? ((RooAbsReal*)_list.at(1))->getVal() : 0 ;
+  Double_t z = _list.at(2) ? ((RooAbsReal*)_list.at(2))->getVal() : 0 ;
+  return _func->Eval(x,y,z) ;
 } 
 
 
@@ -48,7 +48,7 @@ Double_t RooTFnPdfBinding::evaluate() const
 void RooTFnPdfBinding::printArgs(ostream& os) const 
 {
   // Print object arguments and name/address of function pointer
-  os << "[ TFn={" << func->GetName() << "=" << func->GetTitle() << "} " ;    
+  os << "[ TFn={" << _func->GetName() << "=" << _func->GetTitle() << "} " ;    
   for (Int_t i=0 ; i<numProxies() ; i++) {
     RooAbsProxy* p = getProxy(i) ;
     if (!TString(p->name()).BeginsWith("!")) {
