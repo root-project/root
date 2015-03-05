21a1293d84ced49f30783b87afd9a8cc98b9631d
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2015-03-05 15:51:16 +0100

    Use proper batch script syntax

--- build/win/githeader.bat
+++ build/win/githeader.bat
@@ -6,8 +6,8 @@ rem what SHA1 and at what date/time we executed make.
 for /f "delims=" %%a in ('powershell.exe -command "& {Get-Content .\etc\gitinfo.txt | select -First 1}"') do set GIT_BRANCH=%%a
 for /f "delims=" %%a in ('powershell.exe -command "& {Get-Content .\etc\gitinfo.txt | select -First 2 | select -Last 1}"') do set GIT_COMMIT=%%a
 
-echo '#ifndef ROOT_RGITCOMMIT_H' > $1
-echo '#define ROOT_RGITCOMMIT_H' >> $1
-echo '#define ROOT_GIT_BRANCH "%GIT_BRANCH%"' >> $1
-echo '#define ROOT_GIT_COMMIT "%GIT_COMMIT%"' >> $1
-echo '#endif' >> $1
+echo #ifndef ROOT_RGITCOMMIT_H > %1
+echo #define ROOT_RGITCOMMIT_H >> %1
+echo #define ROOT_GIT_BRANCH "%GIT_BRANCH%" >> %1
+echo #define ROOT_GIT_COMMIT "%GIT_COMMIT%" >> %1
+echo #endif >> %1
