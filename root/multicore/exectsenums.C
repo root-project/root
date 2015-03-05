491f9c44dfe183eb32a44ae6c6858b901cbda8f8
Danilo Piparo <danilo.piparo@cern.ch>
2015-03-05 10:57:21 +0100

    Stress mt access to typesystem with enums

---
+++ root/multicore/exectsenums.C
@@ -0,0 +1,44 @@
+
+class tsStringlist {
+public:
+   void addString(const std::string& str){
+   std::lock_guard<std::mutex> lg(fMutex);
+      fNames.push_back(str);
+   }
+   const std::list<std::string>& getStrings() const {return fNames;}
+private:
+   std::list<std::string> fNames;
+   std::mutex fMutex;
+
+};
+
+void exectsenums (){
+   TThread::Initialize();
+   vector<thread> threads;
+   auto enumNames {"enum1",
+                   "enum2",
+                   "enum3",
+                   "enum4",
+                   "enum5",
+                   "enumns::enum1",
+                   "enumns::enum2",
+                   "enumns::enum3",
+                   "enumns::enum4",
+                   "enumns::enum5"};
+   tsStringlist names;
+   for (auto&& enName : enumNames){
+      threads.emplace_back([&](){
+         auto en = TEnum::GetEnum(enName);
+         names.addString(TEnum::GetEnum(enName)->GetQualifiedName());
+
+      });
+   }
+
+   for (auto&& t : threads)
+      t.join();
+
+   std::list<std::string> namesList (names.getStrings());
+   namesList.sort();
+   for (auto&& name:namesList) printf("Enum called %s was found\n",name.c_str());
+
+}
