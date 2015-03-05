8b21ed05132192b54e9ca3ff47338b323fc5eda4
Sergey Linev <S.Linev@gsi.de>
2015-03-05 09:42:54 +0100

    jsroot: use SVG output in MathJax
    Instead of foreignObject and HTML output of MathJax
    'misuse' SVG convertion of MathJax. Main workaround behind - after
    MathJax SVG element produced, it is extracted from hidden
    HTML element and inserted into ROOT SVG hierarchy.
    Works with Firefox, IE9, Chrome
    
    Signed-off-by: Bertrand Bellenot <bertrand.bellenot@cern.ch>

--- etc/http/scripts/JSRootPainter.js
+++ etc/http/scripts/JSRootPainter.js
@@ -752,7 +752,9 @@
       '#Box' : '',
       '#parallel' : '',
       '#perp' : '\u22A5',
-      '#odot' : ''
+      '#odot' : '',
+      '#left' : '',
+      '#right' : ''
    };
 
    JSROOT.Painter.translateLaTeX = function(string) {
@@ -811,12 +813,6 @@
       // function translate ROOT TLatex into MathJax format
 
       if (kind!=2) {
-
-         for (var x in JSROOT.Painter.symbols_map) {
-            var y = "\\" + x.substr(1);
-            str = str.replace(new RegExp(x,'g'), y);
-         }
-
          str = str.replace(/#frac/g, "\\frac");
          str = str.replace(/#left{/g, "\\left\\{");
          str = str.replace(/#right}/g, "\\right\\}");
@@ -824,6 +820,11 @@
          str = str.replace(/#right/g, "\\right");
          // processing of #[] #{} should be done
          str = str.replace(/#\[\]/g, "\\[]");
+
+         for (var x in JSROOT.Painter.symbols_map) {
+            var y = "\\" + x.substr(1);
+            str = str.replace(new RegExp(x,'g'), y);
+         }
       } else {
          str = str.replace(/\\\^/g, "\\hat");
       }
@@ -918,7 +919,7 @@
       }
    }
 
-   JSROOT.TObjectPainter.prototype.RecreateDrawG = function(take_pad, layer) {
+   JSROOT.TObjectPainter.prototype.RecreateDrawG = function(take_pad, layer, normalg) {
       //this.RemoveDrawG();
 
       if (this.draw_g)
@@ -936,15 +937,19 @@
 
          if (!this.draw_g) {
             if (layer==null) layer = ".main_layer";
-            this.draw_g = frame.select(layer).append("svg");
+            if (normalg)
+               this.draw_g = frame.select(layer).append("g");
+            else
+               this.draw_g = frame.select(layer).append("svg");
          }
 
-         this.draw_g.attr("x", 0)
-                    .attr("y", 0)
-                    .attr("width",w)
-                    .attr("height", h)
-                    .attr("viewBox", "0 0 " + w + " " + h)
-                    .attr('overflow', 'hidden');
+         if (!normalg)
+            this.draw_g.attr("x", 0)
+                       .attr("y", 0)
+                       .attr("width",w)
+                       .attr("height", h)
+                       .attr("viewBox", "0 0 " + w + " " + h)
+                       .attr('overflow', 'hidden');
       }
    }
 
@@ -1425,7 +1430,7 @@
       draw_g.call(font.func);
 
       draw_g.property('text_font', font);
-      draw_g.property('mathjax_cnt', 1); // one should wait until last call
+      draw_g.property('mathjax_use', false);
       draw_g.property('text_factor', 0.);
       draw_g.property('max_text_width', 0); // keep maximal text width, use it later
    }
@@ -1436,73 +1441,62 @@
       if (value && (value > draw_g.property('text_factor'))) draw_g.property('text_factor', value);
    }
 
-   JSROOT.TObjectPainter.prototype.FinishTextDrawing = function(entry, draw_g) {
+   JSROOT.TObjectPainter.prototype.FinishTextDrawing = function(draw_g) {
+
       if (!draw_g) draw_g = this.draw_g;
 
-      if (entry != null) {
+      var svgs = null;
 
-         MathJax.Hub.Typeset(entry.node());
+      if (draw_g.property('mathjax_use')) {
+         draw_g.property('mathjax_use', false);
+         draw_g.property('_painter', this);
 
-         var scale = entry.property('_scale');
-         var fo = entry.property('_fo'); entry.property('_fo', null);
-         var align = entry.property('_align');
+         var missing = false;
+         svgs = draw_g.selectAll(".math_svg");
 
-         var prnt = entry.node();
-         if (scale) prnt = prnt.parentNode;
-         // getBoundingClientRect do not work for div with width/height attributes, check childs
-         // var rect = prnt.getBoundingClientRect();
-         var chlds = prnt.childNodes;
-         var left = 100000000, right = 0, top = 100000000, bottom = 0;
-         for (var n=0;n<chlds.length+1;n++) {
-            var rrr = null;
-            if (n<chlds.length) {
-               if (typeof chlds[n]['getBoundingClientRect'] != 'function') continue;
-               rrr = chlds[n].getBoundingClientRect();
-            } else {
-               if ((left<right) && (top<bottom)) continue;
-               rrr = prnt.getBoundingClientRect();
-            }
+         svgs.each(function() {
+            var fo_g = d3.select(this);
+            if (fo_g.node().parentNode !== draw_g.node()) return;
+            var entry = fo_g.property('_element');
+            if (d3.select(entry).select("svg").empty()) missing = true;
+         });
 
-            if ((rrr.left==rrr.right) || (rrr.top==rrr.bottom)) continue;
-
-            left = Math.min(left, parseInt(rrr.left));
-            right = Math.max(right, parseInt(rrr.right));
-            top = Math.min(top,parseInt(rrr.top));
-            bottom = Math.max(bottom, parseInt(rrr.bottom));
-            //console.log(n+ "  left = " + rrr.left + " right = " + rrr.right + " top = " + rrr.top + " bottom = " + rrr.bottom);
-         }
-         var real_w = right - left, real_h = bottom - top;
-
-         // console.log("childs width = " + real_w + "  left = " +left + " right = " + right + " rotate = " + rotate);
-         // console.log("childs height = " + real_h + " top = " + top + " bottom = " + bottom);
-
-         if (real_w > draw_g.property('max_text_width')) draw_g.property('max_text_width', real_w);
-         if (!scale) {
-            // only after drawing performed one could calculate size and adjust position
-            var dx = 0, dy = 0;
-            if (entry.property('_rotate')) {
-               if (align[1] == 'middle') dy = -real_w/2; else
-               if (align[1] == 'top') dy = -real_w;
-               if (align[0] == 'middle') dx = -real_h/2; else
-               if (align[0] == 'end') dx = -real_h;
-            } else {
-               if (align[1] == 'middle') dy = -real_h/2; else
-               if (align[1] == 'top') dy = -real_h;
-               if (align[0] == 'middle') dx = -real_w/2; else
-               if (align[0] == 'end') dx = -real_w;
-            }
-            if (dx != 0) { dx += parseInt(fo.attr('x')); fo.attr('x', dx); }
-            if (dy != 0) { dy += parseInt(fo.attr('y')); fo.attr('y', dy); }
-            return; // no need to continue when scaling not performed
-         }
-
-         this.TextScaleFactor(1.*real_w / parseInt(fo.attr('width')), draw_g);
-         this.TextScaleFactor(1.*real_h / parseInt(fo.attr('height')), draw_g);
+         // is any svg missing we shold wait until drawing is really finished
+         if (missing)
+            return JSROOT.AssertPrerequisites('mathjax', { _this:draw_g, func: function() {
+               if (typeof MathJax != 'object') return;
+               MathJax.Hub.Queue(["FinishTextDrawing", this.property('_painter'), this]);
+            }});
       }
 
-      var cnt = draw_g.property('mathjax_cnt') - 1;
-      draw_g.property('mathjax_cnt', cnt);
-      if (cnt > 0) return 0;
+      if (svgs==null) svgs = draw_g.selectAll(".math_svg");
+
+      var painter = this;
+
+      // first remove dummy divs and check scaling coefficient
+      svgs.each(function() {
+         var fo_g = d3.select(this);
+         if (fo_g.node().parentNode !== draw_g.node()) return;
+         var entry = fo_g.property('_element'); fo_g.property('_element', null);
+
+         var vvv = d3.select(entry).select("svg");
+         if (vvv.empty()) {
+            console.log('MathJax SVG ouptut error');
+            return;
+         }
+
+         vvv.remove();
+         document.body.removeChild(entry);
+
+         fo_g.append(function() { return vvv.node(); });
+
+         if (fo_g.property('_scale')) {
+            var box = fo_g.node().getBBox();
+            var real_w = parseInt(box.width), real_h = parseInt(box.height);
+            painter.TextScaleFactor(1.*real_w / parseInt(fo_g.attr('width')), draw_g);
+            painter.TextScaleFactor(1.*real_h / parseInt(fo_g.attr('height')), draw_g);
+         }
+      });
 
       var f = draw_g.property('text_factor');
       var font = draw_g.property('text_font');
@@ -1510,6 +1504,33 @@
          font.size = Math.floor(font.size/f);
          draw_g.call(font.func);
       }
+
+      svgs.each(function() {
+         var fo_g = d3.select(this);
+         // only direct parent
+         if (fo_g.node().parentNode !== draw_g.node()) return;
+         var box = fo_g.node().getBBox();
+         var real_w = parseInt(box.width), real_h = parseInt(box.height);
+         var align = fo_g.property('_align');
+         var fo_w = parseInt(fo_g.attr('width')), fo_h = parseInt(fo_g.attr('height'));
+         var fo_x = parseInt(fo_g.attr('x')), fo_y = parseInt(fo_g.attr('y'));
+
+         if (fo_g.property('_scale')) {
+            if (align[0] == 'middle') fo_x += (fo_w - real_w)/2; else
+            if (align[0] == 'end') fo_x +=  (fo_w - real_w);
+            if (align[1] == 'middle') fo_y += (fo_h - real_h)/2; else
+            if (align[1] == 'bottom') fo_y += (fo_h - real_h);
+         } else {
+            if (align[0] == 'middle') fo_x -= real_w/2; else
+            if (align[0] == 'end') fo_x -= real_w;
+            if (align[1] == 'middle') fo_y -= real_h/2; else
+            if (align[1] == 'top') fo_y -= real_h;
+         }
+
+         fo_g.attr('x', fo_x).attr('y', fo_y)  // use x/y while transform used for rotation
+             .attr('width', real_w+10).attr('height', real_h+10)  // width and height required by Chrome
+             .attr('visibility', null);
+      });
 
       return draw_g.property('max_text_width');
    }
@@ -1525,6 +1546,7 @@
          align = ['start', 'middle'];
          if ((align_arg / 10) >= 3) align[0] = 'end'; else
          if ((align_arg / 10) >= 2) align[0] = 'middle';
+         if ((align_arg % 10) == 0) align[1] = 'top'; else
          if ((align_arg % 10) == 1) align[1] = 'top'; else
          if ((align_arg % 10) == 3) align[1] = 'bottom';
       }
@@ -1571,8 +1593,7 @@
             // if (align[0]=="end") txt.attr("x", (x + real_w).toFixed(1));
             // if (align[1]=="middle") txt.attr("y", (y-real_h/2).toFixed(1)); else
 
-            if ((align[1]=="bottom") && (h==0)) { txt.attr("y", (y-real_h).toFixed(1)); console.log('shift y due to vertical align'); }
-
+            if ((align[1]=="bottom") && (h==0)) txt.attr("y", (y+real_h).toFixed(1));
          }
 
          if (real_w > draw_g.property('max_text_width')) draw_g.property('max_text_width', real_w);
@@ -1592,48 +1613,30 @@
          w = this.pad_width(); h = this.pad_height(); // artifical values, big enough to see output
       }
 
-      var fo = draw_g.append("foreignObject").attr("width", w).attr("height", h);
-      this.SetForeignObjectPosition(fo, x, y);
-      if (rotate) fo.attr("transform", "rotate(270, 0, 0)");
+      var fo_g = draw_g.append("svg")
+                       .attr('x',x).attr('y',y)  // set x,y, width,height attribute to be able apply alignment later
+                       .attr('width',w).attr('height',h)
+                       .attr('class', 'math_svg')
+                       .attr('visibility','hidden')
+                       .property('_scale', scale)
+                       .property('_rotate', rotate)
+                       .property('_align', align);
 
-      label = JSROOT.Painter.translateMath(label, latex_kind);
+      if (rotate) fo_g.attr("transform", "rotate(270, 0, 0)");
 
-      var entry = fo.append("xhtml:div");
-      if (scale) {
-         var tr = "";
-         entry = entry.style("width", w+"px")
-                      .style("height", h+"px")
-                      .append("xhtml:div")
-                      .style('position','absolute');
-         switch (align[0]) {
-            case 'left' : entry.style('left','0%'); break;
-            case 'middle' : entry.style('left','50%'); tr = "translateX(-50%) "; break;
-            case 'right' :  entry.style('left','100%'); tr = "translateX(-100%) "; break;
-         }
-         switch (align[1]) {
-            case 'top': entry.style('top','0%'); break;
-            case 'middle' : entry.style('top','50%'); tr+="translateY(-50%)"; break;
-            case 'bottom' :  entry.style('top','100%'); tr+="translateY(-100%)"; break;
-         }
-         if (tr.length>0) entry.style('transform', tr);
-      }
+      var element = document.createElement("div");
+      d3.select(element).style("visibility", "hidden")
+                        .style("overflow", "hidden")
+                        .style("position", "absolute")
+                        .html(JSROOT.Painter.translateMath(label, latex_kind));
+      document.body.appendChild(element)
 
+      draw_g.property('mathjax_use', true);  // one need to know that mathjax is used
+      fo_g.property('_element', element);
 
-      entry.style("color", tcolor ? tcolor : null);
-
-      entry.property("_painter", this)
-          .property("_scale", scale)
-          .property("_align", align) // keep align for the end
-          .property("_fo", fo) // keep foreign object till the end
-          .property("_rotate", rotate) // keep rotate attr the end
-          .html(label);
-
-      var cnt = draw_g.property('mathjax_cnt')+1;
-      draw_g.property('mathjax_cnt', cnt);
-
-      JSROOT.AssertPrerequisites('mathjax', { _this:entry, func: function() {
-         if (typeof MathJax != 'object') return;
-         MathJax.Hub.Queue(["FinishTextDrawing", this.property('_painter'), this]);
+      JSROOT.AssertPrerequisites('mathjax', { _this:element, func: function() {
+         if (typeof MathJax == 'object')
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, this]);
       }});
 
       return 0;
@@ -1734,6 +1737,7 @@
          frame_g.append('svg:g').attr('class','grid_layer');
          frame_g.append('svg:g').attr('class','main_layer');
          frame_g.append('svg:g').attr('class','axis_layer');
+         frame_g.append('svg:g').attr('class','upper_layer');
       } else {
          top_rect = frame_g.select("rect");
       }
@@ -2800,7 +2804,7 @@
 
          this.DrawText(22, 0, 0, width*0.5, h*0.04, pavetext.fLabel, tcolor, 1, lbl_g);
 
-         this.FinishTextDrawing(null, lbl_g);
+         this.FinishTextDrawing(lbl_g);
       }
 
       this.AddDrag({ obj:pavetext, redraw:'DrawPaveText' });
@@ -4114,7 +4118,7 @@
 
           if (res<=0) shrink_forbidden = true;
 
-          this.FinishTextDrawing(null, xax_g);
+          this.FinishTextDrawing(xax_g);
       }
 
       /* Y-axis label */
@@ -4130,7 +4134,7 @@
 
          if (res<=0) shrink_forbidden = true;
 
-         this.FinishTextDrawing(null, yax_g);
+         this.FinishTextDrawing(yax_g);
       }
 
       var xAxisColor = this.histo['fXaxis']['fAxisColor'];
@@ -6764,7 +6768,7 @@
          pos_y = h/2;
       }
 
-      this.RecreateDrawG(use_pad, use_pad ? ".text_layer" : ".axis_layer");
+      this.RecreateDrawG(use_pad, use_pad ? ".text_layer" : ".upper_layer", true);
 
       var tcolor = JSROOT.Painter.root_colors[this.text['fTextColor']];
 
