ee549ae01236980af0880a02e700411c969785db
Danilo Piparo <danilo.piparo@cern.ch>
2015-03-06 09:25:26 +0100

    A bunch of MT tests (thanks enric!)
    o Filling of TProfile
    o Filling of TH1F
    o TTree Read
    o TTree Write
    o TClass methods getting
    o TFormula
    
    This last one is disabled on the master as it fails for the new
    TFormula but it's a perfect opportunity for test driven development!
    Some of this tests were directly imported from CMS.

---
+++ root/multicore/tclass_methods.cpp
@@ -0,0 +1,81 @@
+#include "TClass.h"
+#include "TThread.h"
+#include "TObject.h"
+#include "TVirtualStreamerInfo.h"
+#include <thread>
+#include <memory>
+#include <atomic>
+#include <cassert>
+#include <iostream>
+
+void printHelp(const char* iName, int iDefaultNThreads)
+{
+  std::cout << iName <<" [number of threads] \n\n"
+            <<"If no arguments are given "<<iDefaultNThreads<<" threads will be used"<<std::endl;
+}
+
+int parseOptionsForNumberOfThreads(int argc, char** argv)
+{
+  constexpr int kDefaultNThreads = 4;
+  int returnValue = kDefaultNThreads;
+  if( argc == 2 ) {
+    if(strcmp("-h",argv[1]) ==0) {
+      printHelp(argv[0],kDefaultNThreads);
+      exit( 0 );
+    }
+
+    returnValue = atoi(argv[1]);
+ }
+
+  if( argc > 2) {
+    printHelp(argv[0],kDefaultNThreads);
+    exit(1);
+  }
+  return returnValue ;
+}
+
+int main(int argc, char** argv)
+{
+  const int kNThreads = parseOptionsForNumberOfThreads(argc,argv);
+
+  std::atomic<bool> canStart{false};
+  std::vector<std::thread> threads;
+
+  std::atomic<int> classWasGotten{0};
+  std::atomic<int> firstMethodGotten{0};
+
+  TThread::Initialize();
+  //When threading, also have to keep ROOT from logging all TObjects into a list
+  TObject::SetObjectStat(false);
+
+  //Have to avoid having Streamers modify themselves after they have been used
+  TVirtualStreamerInfo::Optimize(false);
+
+
+  for(int i=0; i<kNThreads; ++i) {
+    threads.emplace_back([i,&canStart,&classWasGotten,&firstMethodGotten]() {
+        static thread_local TThread guard;
+        ++classWasGotten;
+        ++firstMethodGotten;
+        while(not canStart) {}
+        auto thingClass = TClass::GetClass("edmtest::Simple");
+        --classWasGotten;
+        while(classWasGotten !=0) {}
+
+        TMethod* method = thingClass->GetMethodWithPrototype("id","",true /*is const*/, ROOT::kConversionMatch);
+        --firstMethodGotten;
+        while(firstMethodGotten !=0) {}
+        TMethod* method2 = thingClass->GetMethodWithPrototype("operator=","edmtest::Simple const&",false /*is const*/, ROOT::kConversionMatch);
+
+        assert(nullptr != method);
+        assert(nullptr != method2);
+      });
+  }
+  canStart = true;
+
+  for(auto& thread: threads) {
+    thread.join();
+  }
+
+  return 0;
+}
