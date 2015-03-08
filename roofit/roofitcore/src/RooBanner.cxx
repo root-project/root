43b6c775123c371bf02fe7d744e0be8612499f27
Philippe Canal <pcanal@fnal.gov>
2015-03-08 11:58:53 -0500

    Allow run-time disable of RooFit Banner via rootrc/gEnv.
    Set the value of
       RooFit.Banner
    either via a rootrc file or a direct call to gEnv to disable (or enable) the printing of the RooFit banner.

--- roofit/roofitcore/src/RooBanner.cxx
+++ roofit/roofitcore/src/RooBanner.cxx
@@ -2,6 +2,7 @@
 
 #include "Rtypes.h"
 #include "Riostream.h"
+#include "TEnv.h"
 
 //////////////////////////////////////////////////////////////////////////////
 // 
@@ -18,10 +19,13 @@ Int_t doBanner()
 
 {
 #ifndef __ROOFIT_NOBANNER
-  cout << endl
-  << "\033[1mRooFit v" << VTAG << " -- Developed by Wouter Verkerke and David Kirkby\033[0m " << endl 
-              << "                Copyright (C) 2000-2013 NIKHEF, University of California & Stanford University" << endl 
-              << "                All rights reserved, please read http://roofit.sourceforge.net/license.txt" << endl << endl ;
+   if (gEnv->GetValue("RooFit.Banner", 1)) {
+       cout << endl
+      << "\033[1mRooFit v" << VTAG << " -- Developed by Wouter Verkerke and David Kirkby\033[0m " << endl
+      << "                Copyright (C) 2000-2013 NIKHEF, University of California & Stanford University" << endl
+      << "                All rights reserved, please read http://roofit.sourceforge.net/license.txt" << endl
+      << endl ;
+   }
 #endif
   return 0 ;
 }
