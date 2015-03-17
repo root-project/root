876c2f71386bceb6400e09becdd8627d9d01c6f9
Danilo Piparo <danilo.piparo@cern.ch>
2015-03-17 14:21:55 +0100

    Add test for the changes in TCling*Info which uses TFormula (ROOT-7188)
    but *DISABLE* it, letting the test running sequentially. This must
    be done until cling based TFormula is made thread safe.

---
+++ root/multicore/exectstformula.C
@@ -0,0 +1,27 @@
+void exectstformula(){
+
+   TThread::Initialize();
+
+   vector<const char*> formulae {"1./(1.+(4.61587e+06*(((1./(0.5*TMath::Max(1.e-6,x+1.)))-1.)/1.16042e+07)))",
+                                 "1./(1.+(3.41484e+06*(((1./(0.5*TMath::Max(1.e-6,x+1.)))-1.)/1.2025e+07)))",
+                                 "1./(1.+(3.58903e+06*(((1./(0.5*TMath::Max(1.e-6,x+1.)))-1.)/2.35101e+07)))"};
+   // atomic<bool> fire(false); Disable for master
+   atomic<bool> fire(true);
+
+   auto f = [&] (int i){
+     auto name = TString::Format("f%i",i);
+     while (!fire.load());
+     new TFormula(name,formulae[i%3]); // <-- we do not care about the leak: we increase contention @ construction time
+   };
+
+   vector<thread> threads;
+   for (int i=0;i<6;i++){
+      f(i);
+//       threads.emplace_back(f,i);
+   }
+
+//    fire = true; again, master in is not thread safe
+
+   for (auto&& t:threads) t.join();
+
+}
