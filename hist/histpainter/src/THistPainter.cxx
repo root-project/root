ef6694cfa7b1fb346e38001b138bd0bb7cdf43ad
Olivier Couet <olivier.couet@cern.ch>
2015-03-18 15:04:37 +0100

    Update doc

--- hist/histpainter/src/THistPainter.cxx
+++ hist/histpainter/src/THistPainter.cxx
@@ -1336,6 +1336,24 @@ Begin_Macro(source)
 }
 End_Macro
 Begin_Html
+<p>
+The option <tt>COL</tt> can be combined with the option <tt>POL/tt>:
+End_Html
+Begin_Macro(source)
+{
+   TCanvas *c1 = new TCanvas("c1","c1",600,400);
+   TH2F *hcol1 = new TH2F("hcol1","Option COLor combined with POL",40,-4,4,40,-4,4);
+   Float_t px, py;
+   for (Int_t i = 0; i < 25000; i++) {
+      gRandom->Rannor(px,py);
+      hcol1->Fill(px,py);
+   }
+   gStyle->SetPalette(54);
+   hcol1->Draw("COLZPOL");
+   return c1;
+}
+End_Macro
+Begin_Html
 
 <a name="HP140"></a><h3>The CANDLE option</h3>
 
