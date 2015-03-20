e88832f9e2c687a300f740b80ebb0bc42ac987ed
Danilo Piparo <danilo.piparo@cern.ch>
2015-03-20 14:00:48 +0100

    Enable tformula mt test

--- root/multicore/exectstformula.C
+++ root/multicore/exectstformula.C
@@ -5,8 +5,7 @@ void exectstformula(){
    vector<const char*> formulae {"1./(1.+(4.61587e+06*(((1./(0.5*TMath::Max(1.e-6,x+1.)))-1.)/1.16042e+07)))",
                                  "1./(1.+(3.41484e+06*(((1./(0.5*TMath::Max(1.e-6,x+1.)))-1.)/1.2025e+07)))",
                                  "1./(1.+(3.58903e+06*(((1./(0.5*TMath::Max(1.e-6,x+1.)))-1.)/2.35101e+07)))"};
-   // atomic<bool> fire(false); Disable for master
-   atomic<bool> fire(true);
+   atomic<bool> fire(false);
 
    auto f = [&] (int i){
      auto name = TString::Format("f%i",i);
@@ -16,11 +15,10 @@ void exectstformula(){
 
    vector<thread> threads;
    for (int i=0;i<6;i++){
-      f(i);
-//       threads.emplace_back(f,i);
+      threads.emplace_back(f,i);
    }
 
-//    fire = true; again, master in is not thread safe
+   fire = true;
 
    for (auto&& t:threads) t.join();
 
