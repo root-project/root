fc92b039245bc269546274da3534997c51d48ae8
Axel Naumann <Axel.Naumann@cern.ch>
2015-03-25 20:18:18 +0100

    Update ROOT version files to v6.02/07 to follow new/old versioning.
    Now, odd patch number are between tags; tags will always have even
    patch numbers. Thus the next tag will be v6.02/08.
    
    This allows users to determine whether they are building against a
    tag or the ROOT sources after a tag, and it allows us to determine
    whether a report is from a tagged build or a snapshot.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.02/05"
-#define ROOT_RELEASE_DATE "Feb  9 2015"
-#define ROOT_RELEASE_TIME "09:57:16"
+#define ROOT_RELEASE "6.02/07"
+#define ROOT_RELEASE_DATE "Mar 25 2015"
+#define ROOT_RELEASE_TIME "20:18:18"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,2,5) /* 393733 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,2,7) /* 393735 */
 
 #endif
