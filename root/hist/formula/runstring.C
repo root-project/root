17e96e4d589819e499b653dedc67321fa3c255eb
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2015-03-31 17:53:24 +0200

    Use ROOT::v5::TFormula instead of TFormulaOld

--- root/hist/formula/runstring.C
+++ root/hist/formula/runstring.C
@@ -3,13 +3,13 @@ int runstring()
    int result = 0;
    // works but produce a warning
    //TFormula* pf1 = new TFormula("f1","\"abc\" == \"def\"");
-   TFormulaOld* pf1 = new TFormulaOld("f1","\"abc\" == \"def\"");
+   ROOT::v5::TFormula* pf1 = new ROOT::v5::TFormula("f1","\"abc\" == \"def\"");
    int i = pf1->Eval(0,0,0,0);
    if (i!=0) {
       cerr << "== is not working for strings\n";
       result = 1;
    }
-   TFormulaOld* pf2 = new TFormulaOld("f2","strstr(\"abc\",\"def\") != 0");
+   ROOT::v5::TFormula* pf2 = new ROOT::v5::TFormula("f2","strstr(\"abc\",\"def\") != 0");
    i = pf2->Eval(0,0,0,0);
    if (i!=0) {
       cerr << "strstr is not working for strings\n";
@@ -17,7 +17,7 @@ int runstring()
    }
    // same as above
    //TFormula* pf3 = new TFormula("f3","\"abc\" != \"def\"");
-   TFormulaOld* pf3 = new TFormulaOld("f3","\"abc\" != \"def\"");
+   ROOT::v5::TFormula* pf3 = new ROOT::v5::TFormula("f3","\"abc\" != \"def\"");
    i = pf3->Eval(0,0,0,0);
    if (i!=1) {
       cerr << "!= is not working for strings\n";
--- root/treeformula/parse/testString.C
+++ root/treeformula/parse/testString.C
@@ -1,31 +1,31 @@
 
 int execTestString() {
-   TFormulaOld *f = new TFormulaOld("f0","\"a+b\"");
+   ROOT::v5::TFormula *f = new ROOT::v5::TFormula("f0","\"a+b\"");
    if (f->GetNdim()!=1) return 0;
    Int_t res = f->Eval(0);
    if (res!=0) return 0;   
 
-   f = new TFormulaOld("f1","strstr(\"a+b\",\"a+b\")");
+   f = new ROOT::v5::TFormula("f1","strstr(\"a+b\",\"a+b\")");
    if (f->GetNdim()!=1) return 0;
    res = f->Eval(0);
    if (res!=1) return 0;
 
-   f = new TFormulaOld("f2","\"af(d+gd\"+2");
+   f = new ROOT::v5::TFormula("f2","\"af(d+gd\"+2");
    if (f->GetNdim()!=0) return 0;
    res = f->Eval(0);
    if (res!=2) return 0;
 
-   f = new TFormulaOld("f3","strstr(\"a\",\"a+b\")");
+   f = new ROOT::v5::TFormula("f3","strstr(\"a\",\"a+b\")");
    if (f->GetNdim()!=1) return 0;
    res = f->Eval(0);
    if (res!=0) return 0;
 
-   f = new TFormulaOld("f4","(strstr(\"a\",\"a+b\")+8)");
+   f = new ROOT::v5::TFormula("f4","(strstr(\"a\",\"a+b\")+8)");
    if (f->GetNdim()!=1) return 0;
    res = f->Eval(0);
    if (res!=8) return 0;
 
-   f = new TFormulaOld("f5","strstr(\"a + b\",\"a + b\")");
+   f = new ROOT::v5::TFormula("f5","strstr(\"a + b\",\"a + b\")");
    if (f->GetNdim()!=1) return 0;
    res = f->Eval(0);
    if (res!=1) return 0;
