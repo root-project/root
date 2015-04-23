7dc0774baaef8b092ad0b1e3eaad7f482ad9a459
Pere Mato <pere.mato@cern.ch>
2015-04-23 18:30:25 +0200

    Update ROOT version files to v5.34/30.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,13 +14,13 @@
  *
 */
 
-#define ROOT_RELEASE "5.34/29"
-#define ROOT_RELEASE_DATE "Mar 24 2015"
-#define ROOT_RELEASE_TIME "11:21:58"
+#define ROOT_RELEASE "5.34/30"
+#define ROOT_RELEASE_DATE "Apr 23 2015"
+#define ROOT_RELEASE_TIME "18:30:24"
 #define ROOT_SVN_REVISION 49361
-#define ROOT_GIT_COMMIT "v5-34-28"
-#define ROOT_GIT_BRANCH "v5-34-28"
-#define ROOT_VERSION_CODE 336413
+#define ROOT_GIT_COMMIT "v5-34-28-57-gec27989"
+#define ROOT_GIT_BRANCH "heads/v5-34-00-patches"
+#define ROOT_VERSION_CODE 336414
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
