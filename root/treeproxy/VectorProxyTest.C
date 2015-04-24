87f88e2fc920284a17c17b898e2f0f294cd86a16
Philippe Canal <pcanal@fnal.gov>
2015-04-24 14:13:32 -0500

    Add test for ROOT-7239

---
+++ root/treeproxy/VectorProxyTest.C
@@ -0,0 +1,42 @@
+#include "VectorProxyTest.h"
+
+//================================================================================
+void ProxyTest_Begin(TTree*)
+{
+}
+
+//================================================================================
+void ProxyTest_SlaveBegin(TTree*)
+{
+}
+
+//================================================================================
+Bool_t ProxyTest_Notify() { return kTRUE; }
+
+//================================================================================
+Bool_t ProxyTest_Process(Long64_t) 
+{ 
+  for (size_t ii=0; ii<vecOfVecOfInt->size(); ii++) {
+    const std::vector<int>& vecOfInt = vecOfVecOfInt->at(ii);
+    for (size_t kk=0; kk<vecOfInt.size(); kk++) {
+      printf("ii=%2lu kk=%2lu content=%2d\n", ii, kk, vecOfInt.at(kk));
+    }
+  }
+  
+  return kTRUE;
+}
+
+//================================================================================
+void ProxyTest_Terminate()
+{
+}
+
+//================================================================================
+void ProxyTest_SlaveTerminate()
+{
+}
+
+//================================================================================
+Double_t VectorProxyTest() {
+  return 0.0;
+}
