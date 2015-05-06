22376ff74f6d9287939457d80feecf45286d5ec2
Danilo Piparo <danilo.piparo@cern.ch>
2015-05-06 13:28:02 +0200

    Make tutorial more telling about the results of the fitslices operation.

--- tutorials/tree/h1analysis.C
+++ tutorials/tree/h1analysis.C
@@ -342,7 +342,9 @@ void h1analysis::Terminate()
       delete gROOT->GetFunction("f2");
    TF1 *f2 = new TF1("f2",fdm2,0.139,0.17,2);
    f2->SetParameters(10000, 10);
-   h2->FitSlicesX(f2,0,-1,1,"qln");
+   // Restrict to three bins in this example
+   Info("Fit Slices","Restricting fit to two bins only in this example...");
+   h2->FitSlicesX(f2,30,33,1,"l");
    TH1D *h2_1 = (TH1D*)gDirectory->Get("h2_1");
    h2_1->GetXaxis()->SetTitle("#tau[ps]");
    h2_1->SetMarkerStyle(21);
