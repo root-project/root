9c7170798c1221e55541ad35f439cbc66daad285
pspe <peter.speckmayer@gmx.at>
2015-05-08 14:58:03 +0200

    modified:   tmva/inc/LinkDef1.h 	new file:   tmva/inc/TMVA/MethodNN.h 	new file:   tmva/inc/TMVA/Monitoring.h 	new file:   tmva/inc/TMVA/NeuralNet.h 	new file:   tmva/inc/TMVA/NeuralNet_i.h 	new file:   tmva/inc/TMVA/Pattern.h 	modified:   tmva/inc/TMVA/Types.h 	new file:   tmva/src/MethodNN.cxx 	new file:   tmva/src/NeuralNet.cxx

--- tmva/tmva/inc/TMVA/Types.h
+++ tmva/tmva/inc/TMVA/Types.h
@@ -100,14 +100,15 @@ namespace TMVA {
          kLD             ,
          kPlugins        ,
          kCategory       ,
-         kMaxMethod      ,
          kPyRandomForest ,
          kPyAdaBoost     ,
          kPyGTB          ,
          kC50            ,
          kRSNNS          ,
          kRSVM           ,
-         kRXGB       
+         kRXGB           ,
+         kNN             ,
+         kMaxMethod
       };
 
       // available variable transformations
