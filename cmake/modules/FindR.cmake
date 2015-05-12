aebf5c8698ec57593e8a45ed7fa57e718c778da3
Pere Mato <pere.mato@cern.ch>
2015-05-12 09:37:49 +0200

    Simplify findR.cmake and follow the standard.

--- cmake/modules/FindR.cmake
+++ cmake/modules/FindR.cmake
@@ -1,82 +1,66 @@
-#Cmake module to find R 
+# CMake module to find R
 # - Try to find R
 # Once done, this will define
 #
 #  R_FOUND - system has R
 #  R_INCLUDE_DIRS - the R include directories
 #  R_LIBRARIES - link these to use R
-#Autor: Omar Andres Zapata Mesa 31/05/2013
-
-message(STATUS "Looking for R")
+#  R_ROOT_DIR - As reported by R
+# Autor: Omar Andres Zapata Mesa 31/05/2013
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   set(CMAKE_FIND_APPBUNDLE "LAST")
 endif()
 
-find_program ( R_EXECUTABLE
-               NAMES R R.exe
-              )
+find_program(R_EXECUTABLE NAMES R R.exe)
 
-MESSAGE(STATUS "R_EXECUTABLE=${R_EXECUTABLE}")
+#---searching R installtion unsing R executable
+if(R_EXECUTABLE)
+  execute_process(COMMAND ${R_EXECUTABLE} RHOME
+                  OUTPUT_VARIABLE R_ROOT_DIR
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-#searching flags unsing R executable              
-if ( R_EXECUTABLE )
-  execute_process ( COMMAND echo "cat(Sys.getenv(\"R_HOME\"))"
-                    COMMAND ${R_EXECUTABLE} --vanilla --slave
-                    OUTPUT_VARIABLE R_HOME
-                    ERROR_VARIABLE R_HOME_ERR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE
-                  )
+  find_path(R_INCLUDE_DIR R.h
+            HINTS ${R_ROOT_DIR}
+            PATHS /usr/local/lib /usr/local/lib64 /usr/share
+            PATH_SUFFIXES include R/include
+            DOC "Path to file R.h")
 
- execute_process ( COMMAND ${R_EXECUTABLE} CMD config --cppflags
-                   OUTPUT_VARIABLE R_INCLUDE_DIR
-                   ERROR_VARIABLE  R_INCLUDE_DIR_ERR
-                   OUTPUT_STRIP_TRAILING_WHITESPACE
-                  )
-                  
-if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  set(R_LIBRARY "-L${R_HOME}/lib -lR")
-else()
-  execute_process ( COMMAND ${R_EXECUTABLE} CMD config --ldflags
-                    OUTPUT_VARIABLE R_LIBRARY
-                    OUTPUT_STRIP_TRAILING_WHITESPACE
-                  )
+  find_library(R_LIBRARY R
+            HINTS ${R_ROOT_DIR}/lib
+            DOC "R library (example libR.a, libR.dylib, etc.).")
 endif()
 
-
-MESSAGE(STATUS "R_HOME=${R_HOME}")
-MESSAGE(STATUS "R_INCLUDE_DIR=${R_INCLUDE_DIR}")
-MESSAGE(STATUS "R_LIBRARY=${R_LIBRARY}")
-
-else()              
-set(R_PKGCONF_INCLUDE_DIRS  
-      "/usr/local/include" "/usr/include" 
-      "/opt/R/include" "/usr/share/R/include"  
-      "/usr/local/share/R/include")
-
-# Include dir
-find_path(R_INCLUDE_DIR
-  NAMES R.h
-  PATHS ${R_PKGCONF_INCLUDE_DIRS}
-)
-
-set(R_PKGCONF_LIBRARY_DIRS  "/usr/local/lib" "/usr/lib"
-    "/opt/R/lib" "/usr/lib/R/lib/" "/usr/lib/R/lib/" )
-# Finally the library itself
-find_library(R_LIBRARY
-  NAMES R
-  PATHS ${R_PKGCONF_LIBRARY_DIRS}
-)
-
-endif ( R_EXECUTABLE )
-
-# Setting up the results 
-set(R_INCLUDE_DIRS ${R_INCLUDE_DIR})
+#---setting include dirs and libraries
 set(R_LIBRARIES ${R_LIBRARY})
-if (("${R_INCLUDE_DIR}" STREQUAL "") OR ("${R_LIBRARY}" STREQUAL ""))
-  set(R_FOUND FALSE)
-  message(STATUS "Looking for R -- not found ")
-else()
-  set(R_FOUND TRUE)
-  message(STATUS "Looking for R -- found")
-endif()
+set(R_INCLUDE_DIRS ${R_INCLUDE_DIR})
+foreach(_cpt ${R_FIND_COMPONENTS})
+  execute_process(COMMAND echo "cat(find.package('${_cpt}'))"
+                  COMMAND ${R_EXECUTABLE} --vanilla --slave
+                  OUTPUT_VARIABLE _cpt_path
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  find_library(R_${_cpt}_LIBRARY
+               ${_cpt}.so ${_cpt}.dylib
+               HINTS ${_cpt_path}/libs)
+  if(R_${_cpt}_LIBRARY)
+    mark_as_advanced(R_${_cpt}_LIBRARY)
+    list(APPEND R_LIBRARIES ${R_${_cpt}_LIBRARY})
+  endif()
+
+  find_path(R_${_cpt}_INCLUDE_DIR ${_cpt}.h HINTS  ${_cpt_path} PATH_SUFFIXES include R/include)
+  if(R_${_cpt}_INCLUDE_DIR)
+    mark_as_advanced(R_${_cpt}_INCLUDE_DIR)
+    list(APPEND R_INCLUDE_DIRS ${R_${_cpt}_INCLUDE_DIR})
+  endif()
+
+  if(R_${_cpt}_INCLUDE_DIR AND R_${_cpt}_LIBRARY)
+    list(REMOVE_ITEM R_FIND_COMPONENTS ${_cpt})
+  endif()
+endforeach()
+
+# Handle the QUIETLY and REQUIRED arguments and set R_FOUND to TRUE if all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(R DEFAULT_MSG R_EXECUTABLE R_INCLUDE_DIR R_LIBRARY)
+mark_as_advanced(R_FOUND R_EXECUTABLE R_INCLUDE_DIR R_LIBRARY)
+
