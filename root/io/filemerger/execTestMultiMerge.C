978289140646ce290f6f7a6701338bf6beeddff2
Philippe Canal <pcanal@fnal.gov>
2015-05-22 15:18:59 -0500

    Add test of hadd -f206 (specifying lzma compression)

--- root/io/filemerger/execTestMultiMerge.C
+++ root/io/filemerger/execTestMultiMerge.C
@@ -10,5 +10,22 @@ int execTestMultiMerge()
    file->cd("named");
    gDirectory->ls();
    file->Get("MyList")->Print();
+
+   delete file;
+
+   file = TFile::Open("mzfile1-4.root");
+   file->ls();
+   file->cd("hist");
+   gDirectory->ls();
+   gDirectory->Get("Gaus")->Print();
+   file->cd("named");
+   gDirectory->ls();
+   file->Get("MyList")->Print();
+   if (file->GetCompressionSettings() != 206) {
+      Error("execTestMultiMerge","Compression level of %s should have been %d but is %d\n",file->GetName(), 206, file->GetCompressionSettings() );
+      return 1;
+   }
+
+
    return 0;
 }
