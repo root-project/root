78cc0d85efab15bba6c2fe701d2b28cdbaea3a69
Axel Naumann <Axel.Naumann@cern.ch>
2015-06-02 17:20:30 +0200

    Fix message.
    (cherry picked from commit d68194b11ceb3f55fd30b0a848c476436f713648)

--- build/unix/makereleasenotes.sh
+++ build/unix/makereleasenotes.sh
@@ -9,7 +9,7 @@ DIR="README/ReleaseNotes"
 pandoc -f markdown -t html -s -S -f markdown --toc -H $ROOTSRCDIR/documentation/users-guide/css/github.css --mathjax \
 $ROOTSRCDIR/$DIR/v${VERS}/index.md -o $DIR/v${VERS}/index.html
 
-echo "Generated $DIR/v${v}/index.html"
+echo "Generated $DIR/v${VERS}/index.html"
 echo ""
 
 exit 0
