8c206e552f9e1315d64b9a8aa7fddc342e105626
Axel Naumann <Axel.Naumann@cern.ch>
2015-06-02 09:36:35 +0200

    Update makereleasenotes.sh to reflect combined index.md.

--- build/unix/makereleasenotes.sh
+++ build/unix/makereleasenotes.sh
@@ -1,63 +1,12 @@
 #! /bin/sh
 
-PACKAGES="core io net sql tree proof hist interpreter bindings math roofit \
-          tmva geom montecarlo gui graf2d graf3d html misc tutorials cmake"
-
 VERS="604"
 
-OUTDIR="README/ReleaseNotes"
-
-echo ""
-echo "Generating $OUTDIR from package docs..."
-
-# make output dir
-if [ ! -d $OUTDIR ]; then
-   mkdir $OUTDIR
-fi
-
-# make clean version directories
-for v in $VERS; do
-   if [ -d $OUTDIR/v${v} ]; then
-      rm -rf $OUTDIR/v${v}
-   fi
-   mkdir $OUTDIR/v${v}
-done
-
-# write header note
-for v in $VERS; do
-   if [ -r doc/v${v}/index.md ]; then
-      cat doc/v${v}/index.md > $OUTDIR/v${v}/index.md
-   fi
-done
-
-# write package notes
-for i in $PACKAGES; do
-   for v in $VERS; do
-      if [ -r ${i}/doc/v${v}/index.md ]; then
-         cat ${i}/doc/v${v}/index.md >> $OUTDIR/v${v}/index.md
-         for img in `ls ${i}/doc/v${v}/*`; do 
-            if [ $img != ${i}/doc/v${v}/index.md ]; then
-               cp ${img} $OUTDIR/v${v}
-            fi
-         done
-      fi
-   done
-done
-
-# write trailer note
-echo ""
-for v in $VERS; do
-   if [ -r doc/v${v}/Trailer.md ]; then
-      cat doc/v${v}/Trailer.md >> $OUTDIR/v${v}/index.md
-      echo "Generated $OUTDIR/v${v}/index.md"
-   fi
-done
-echo ""
-
+DIR="README/ReleaseNotes"
 pandoc -f markdown -t html -s -S -f markdown --toc -H documentation/users-guide/css/github.css --mathjax \
-$OUTDIR/v${v}/index.md -o $OUTDIR/v${v}/index.html
+$DIR/v${v}/index.md -o $DIR/v${v}/index.html
 
-echo "Generated $OUTDIR/v${v}/index.html"
+echo "Generated $DIR/v${v}/index.html"
 echo ""
 
 exit 0
