03d6b637c01fa7af3c04723b6bd6e4f75c71e4ab
Axel Naumann <Axel.Naumann@cern.ch>
2015-06-02 17:08:15 +0200

    Fix script; enable out-of-source.

--- build/unix/makereleasenotes.sh
+++ build/unix/makereleasenotes.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
 VERS="604"
+ROOTSRCDIR=`dirname $0` # .../build/unix
+ROOTSRCDIR=`dirname $ROOTSRCDIR` # .../build
+ROOTSRCDIR=`dirname $ROOTSRCDIR` # ...
 
 DIR="README/ReleaseNotes"
-pandoc -f markdown -t html -s -S -f markdown --toc -H documentation/users-guide/css/github.css --mathjax \
-$DIR/v${v}/index.md -o $DIR/v${v}/index.html
+pandoc -f markdown -t html -s -S -f markdown --toc -H $ROOTSRCDIR/documentation/users-guide/css/github.css --mathjax \
+$ROOTSRCDIR/$DIR/v${VERS}/index.md -o $DIR/v${VERS}/index.html
 
 echo "Generated $DIR/v${v}/index.html"
 echo ""
