9377cba2a22b2d95b3eb81ee284785f972a8df1d
Danilo Piparo <danilo.piparo@cern.ch>
2015-06-12 14:18:56 +0200

    Test for browsing classes with fields of a non accessible type

---
+++ root/meta/tclass/execBrowseNonAccessibleType.C
@@ -0,0 +1,21 @@
+
+template<class T>
+void browse(TBrowser* b, const char* name){
+   auto c=TClass::GetClass(name);
+   T a;
+   c->Browse(&a,b);
+}
+
+void execBrowseNonAccessibleType(){
+
+TCanvas c("","",100,100);
+
+TBrowser b;
+
+browse<TString>(&b,"TString");
+browse<TH1F>(&b,"TH1F");
+browse<THtml>(&b,"THtml");
+browse<TCanvas>(&b,"TCanvas");
+browse<TRolke>(&b,"TRolke");
+
+}
