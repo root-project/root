6c5ee53c9df9a5f50ae2b2d7b1fda680342e28b4
Pere Mato <pere.mato@cern.ch>
2015-06-29 19:17:09 +0200

    Added define R__ACCESS_IN_SYMBOL when compiling CINT generated dictionaries with gcc version > 4. This is needed to avoid #define private public that breaks a C++11 header file in gcc 5.1.

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -266,6 +266,9 @@ function(ROOT_GENERATE_DICTIONARY dictionary)
   if(CXX_HAS_Wno-strict-aliasing)
     set_source_files_properties( ${dictionary}.cxx COMPILE_FLAGS -Wno-strict-aliasing)
   endif()
+  if(${GCC_MAJOR} GREATER 4)
+    set_source_files_properties( ${dictionary}.cxx PROPERTIES COMPILE_DEFINITIONS R__ACCESS_IN_SYMBOL)
+  endif()
 
 endfunction()
 
