ea6a95b17689d466e87790e35272e282eb4d7850
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2015-06-29 11:49:26 +0200

    Remove kCanRebin bit Add default case to SetCanExtend

--- tmva/tmva/src/DecisionTree.cxx
+++ tmva/tmva/src/DecisionTree.cxx
@@ -1336,8 +1336,6 @@ Double_t TMVA::DecisionTree::TrainNodeFast( const EventConstList & eventSample,
    delete [] separationGain;
    delete [] cutIndex;
 
-   delete [] nBins; 
-
    return separationGainTotal;
 
 }
