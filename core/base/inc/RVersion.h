f54bbec9b6befe221627f11ed8e0b9d7c03bf7b6
Pere Mato <pere.mato@cern.ch>
2015-07-14 13:57:49 +0200

    Update ROOT version files to v6.04/03.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.04/02"
+#define ROOT_RELEASE "6.04/03"
 #define ROOT_RELEASE_DATE "Jul 14 2015"
-#define ROOT_RELEASE_TIME "12:54:00"
+#define ROOT_RELEASE_TIME "13:57:49"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,4,2) /* 394242 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,4,3) /* 394243 */
 
 #endif
