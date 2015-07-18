4d5c7632c8d12a32b530899905f24896c2caa3d9
Danilo Piparo <danilo.piparo@cern.ch>
2015-07-18 05:57:09 +0200

    Return proper code if pch generation fails

--- etc/dictpch/makepch.py
+++ etc/dictpch/makepch.py
@@ -17,6 +17,7 @@
 
 import sys
 import os
+import subprocess
 import shutil
 
 #-------------------------------------------------------------------------------
@@ -102,12 +103,12 @@ def makepch():
                                                                          extraHeaders,
                                                                          alllinkdefsFilename)
 
-   ret = os.system(command)
+   ret = subprocess.call(command,shell=True)
    if ret == 0:
       shutil.move("allDict_rdict.pch",pchFileName)
       os.unlink("allDict.cxx")
 
-   sys.exit(ret)
+   return ret
 
 if __name__ == "__main__":
    ret = makepch()
