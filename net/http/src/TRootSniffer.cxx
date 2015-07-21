8488feeadd7901b9dbbea2d7ac6a4d70ecdff469
Sergey Linev <S.Linev@gsi.de>
2015-07-21 16:22:18 +0200

    http: extract only direct data members from real data
    Signed-off-by: Bertrand Bellenot <bertrand.bellenot@cern.ch>

--- net/http/src/TRootSniffer.cxx
+++ net/http/src/TRootSniffer.cxx
@@ -39,6 +39,7 @@
 
 #include <stdlib.h>
 #include <vector>
+#include <string.h>
 
 const char *item_prop_kind = "_kind";
 const char *item_prop_more = "_more";
@@ -591,7 +592,7 @@ void TRootSniffer::ScanObjectMemebers(TRootSnifferScanRec &rec, TClass *cl, char
    TIter iter(cl->GetListOfRealData());
    while ((obj = iter()) != 0) {
       TRealData *rdata = dynamic_cast<TRealData *>(obj);
-      if (rdata == 0) continue;
+      if ((rdata == 0) || strchr(rdata->GetName(),'.')) continue;
 
       TDataMember *member = rdata->GetDataMember();
       // exclude enum or static variables
