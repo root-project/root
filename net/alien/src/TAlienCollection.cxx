5363490de1a936a44ee41c3247ac78feaa9e91ec
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-07-22 10:53:43 +0200

    Fix unused variable warning.

--- net/alien/src/TAlienCollection.cxx
+++ net/alien/src/TAlienCollection.cxx
@@ -1329,9 +1329,7 @@ Bool_t TAlienCollection::ExportXML(TFile * exportfile, Bool_t selected,
    filegroups->Reset();
    UInt_t groupcnt = 0;
    while ((nextgroup = (TMap *) filegroups->Next())) {
-      Bool_t isonline;
       Bool_t isselected;
-      isonline = kTRUE;
       isselected = kFALSE;
       TMap *attributes;
       TIterator *nextfile = nextgroup->MakeIterator();
@@ -1344,9 +1342,6 @@ Bool_t TAlienCollection::ExportXML(TFile * exportfile, Bool_t selected,
                isselected = kTRUE;
             }
 
-            if (!IsOnline(attributes->GetName())) {
-               isonline = kFALSE;
-            }
          }
       }
       if ((!selected) || isselected) {
