8cfd3325fe47e453e9d8be779fbb606e605028ad
Philippe Canal <pcanal@fnal.gov>
2015-07-29 13:52:09 -0500

    Disable part of: Test ACLiC's resilience to have a file named 'new' in the current directory

--- root/aclic/misc/runrelpath.C
+++ root/aclic/misc/runrelpath.C
@@ -1,7 +1,16 @@
 {
    gErrorIgnoreLevel = kWarning;
-   gInterpreter->AddIncludePath("nest/subdir1");
-   gROOT->ProcessLine(".include nest/subdir2");
+   const char *path = gSystem->DirName(gInterpreter->GetCurrentMacroName());
+
+   if (strcmp(path,gSystem->pwd()) == 0) {
+      //cout << "Local branch\n";
+      gInterpreter->AddIncludePath("nest/subdir1");
+      gROOT->ProcessLine(".include nest/subdir2");
+   } else {
+      //cout << "Remote branch\n";
+      gInterpreter->AddIncludePath(TString::Format("%s/nest/subdir1",path));
+      gROOT->ProcessLine(TString::Format(".include %s/nest/subdir2",path));
+   }
    gROOT->ProcessLine(".L script.C++");
 }
 
