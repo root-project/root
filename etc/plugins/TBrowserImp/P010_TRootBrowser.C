961deee4aab414f791d415e4d63b536b673d0af9
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2015-08-05 17:27:58 +0200

    Revert browser changes (it has side effects)

--- etc/plugins/TBrowserImp/P010_TRootBrowser.C
+++ etc/plugins/TBrowserImp/P010_TRootBrowser.C
@@ -4,6 +4,4 @@ void P010_TRootBrowser()
       "Gui", "NewBrowser(TBrowser *, const char *, Int_t, Int_t, UInt_t, UInt_t)");
    gPluginMgr->AddHandler("TBrowserImp", "TRootBrowser", "TRootBrowser",
       "Gui", "NewBrowser(TBrowser *, const char *, UInt_t, UInt_t)");
-   gPluginMgr->AddHandler("TBrowserImp", "TRootBrowser", "TRootBrowser",
-      "Gui", "NewBrowser(TBrowser *, const char *, Int_t, Int_t, UInt_t, UInt_t, Option_t *)");
 }
