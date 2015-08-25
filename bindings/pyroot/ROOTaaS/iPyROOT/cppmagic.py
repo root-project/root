307e0c4bbd7e76c9a8becf39df539413fef20e60
Danilo Piparo <danilo.piparo@cern.ch>
2015-08-25 15:18:45 +0200

    Add line magic %cpp

--- bindings/pyroot/ROOTaaS/iPyROOT/cppmagic.py
+++ bindings/pyroot/ROOTaaS/iPyROOT/cppmagic.py
@@ -4,12 +4,13 @@ import utils
 
 @ipym.magics_class
 class CppMagics(ipym.Magics):
-    @ipym.cell_magic
+    @ipym.line_cell_magic
     def cpp(self, line, cell=None):
         """Inject into root."""
-        if cell:
+        if cell is None: # this is a line magic
+            utils.processCppCode(line)
+        else:
             utils.processCppCode(cell)
 
 def load_ipython_extension(ipython):
     ipython.register_magics(CppMagics)
-
