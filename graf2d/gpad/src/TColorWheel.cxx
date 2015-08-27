1f3872a3c26f741fc8c33dd8ce77d305b739a336
Olivier Couet <olivier.couet@cern.ch>
2015-08-27 15:04:19 +0200

    Doxygen

--- graf2d/gpad/src/TColorWheel.cxx
+++ graf2d/gpad/src/TColorWheel.cxx
@@ -22,36 +22,39 @@
 
 ClassImp(TColorWheel)
 
-////////////////////////////////////////////////////////////////////////////////
+/** \class TColorWheel
+TColorWheel : Draw the ROOT Color Wheel.
 
-/* Begin_Html
-<center><h2>TColorWheel : Draw the ROOT Color Wheel.</h2></center>
 The wheel contains the recommended 216 colors to be used in web applications.
 The colors in the Color Wheel are created by TColor::CreateColorWheel.
-<p>Using this color set for your text, background or graphics will give your
+
+Using this color set for your text, background or graphics will give your
 application a consistent appearance across different platforms and browsers.
-<p>Colors are grouped by hue, the aspect most important in human perception.
+
+Colors are grouped by hue, the aspect most important in human perception.
 Touching color chips have the same hue, but with different brightness and vividness.
-<p>Colors of slightly different hues <b>clash</b>. If you intend to display
+
+Colors of slightly different hues __clash__. If you intend to display
 colors of the same hue together, you should pick them from the same group.
-<p>Each color chip is identified by a mnemonic (e.g. kYellow) and a number.
-The keywords, kRed, kBlue, kYellow, kPink, etc are defined in the header file <b>Rtypes.h</b>
+
+Each color chip is identified by a mnemonic (e.g. kYellow) and a number.
+The keywords, kRed, kBlue, kYellow, kPink, etc are defined in the header file __Rtypes.h__
 that is included in all ROOT other header files. We strongly recommend to use these keywords
 in your code instead of hardcoded color numbers, e.g.:
-<pre>
+~~~ {.cpp}
    myObject.SetFillColor(kRed);
    myObject.SetFillColor(kYellow-10);
    myLine.SetLineColor(kMagenta+2);
-</pre>
+~~~
 
-End_Html
 Begin_Macro(source)
 {
    TColorWheel *w = new TColorWheel();
    w->Draw();
    return w->GetCanvas();
 }
-End_Macro */
+End_Macro
+*/
 
 ////////////////////////////////////////////////////////////////////////////////
 /// constructor
@@ -107,7 +110,6 @@ Int_t TColorWheel::DistancetoPrimitive(Int_t px, Int_t py)
    if (px+py < 0) return 1;
    return 0;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Paint the color wheel
