108bd4b7e9f62cdc1a32aaac71d85a03118d7317
Danilo Piparo <danilo.piparo@cern.ch>
2015-08-31 00:28:13 +0200

    Add doctest tests

--- bindings/pyroot/ROOTaaS/iPyROOT/aclicmagic.py
+++ bindings/pyroot/ROOTaaS/iPyROOT/aclicmagic.py
@@ -3,10 +3,20 @@ import utils
 from hashlib import sha1
 
 def _codeToFilename(code):
+    '''Convert code to a unique file name
+
+    >>> _codeToFilename("int f(i){return i*i;}")
+    'dbf7e731.C'
+    '''
     fileNameBase = sha1(code).hexdigest()[0:8]
     return fileNameBase + ".C"
 
 def _dumpToUniqueFile(code):
+    '''Dump code to file whose name is unique
+
+    >>> _codeToFilename("int f(i){return i*i;}")
+    'dbf7e731.C'
+    '''
     fileName = _codeToFilename(code)
     ofile = open (fileName,'w')
     ofile.write(code)
