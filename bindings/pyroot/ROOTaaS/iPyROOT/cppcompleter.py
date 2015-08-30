31cddad50f90bfbaf74bfbaebce7e6cae3736634
Danilo Piparo <danilo.piparo@cern.ch>
2015-08-31 00:29:44 +0200

    Add doctest tests, fix on osx

--- bindings/pyroot/ROOTaaS/iPyROOT/cppcompleter.py
+++ bindings/pyroot/ROOTaaS/iPyROOT/cppcompleter.py
@@ -18,21 +18,99 @@ class CppCompleter(object):
     '''
     Completer which interfaces to the TTabCom of ROOT. It is activated
     (deactivated) upon the load(unload) of the load of the extension.
+
+    >>> comp = CppCompleter()
+    >>> comp.activate()
+    >>> for suggestion in comp._completeImpl("TH"):
+    ...     print suggestion
+    THashList
+    THashTable
+    THashTableIter
+    THelix
+    THYPE
+    TH1
+    TH1C
+    TH1S
+    TH1I
+    TH1F
+    TH1D
+    TH3
+    TH3C
+    TH3S
+    TH3I
+    TH3F
+    TH3D
+    TH2GL
+    TH3GL
+    TH1K
+    TH2
+    TH2C
+    TH2S
+    TH2I
+    TH2F
+    TH2D
+    TH2PolyBin
+    TH2Poly
+    THLimitsFinder
+    THnBase
+    THnIter
+    THn
+    THnSparseArrayChunk
+    THnSparse
+    THStack
+    THistPainter
+    >>> garbage = ROOT.gInterpreter.ProcessLine("TH1F* h;")
+    >>> for suggestion in comp._completeImpl("h->GetA"):
+    ...     print suggestion
+    h->GetAsymmetry
+    h->GetAxisColor
+    h->GetArray
+    h->GetAt
+    >>> comp.deactivate()
+    >>> for suggestion in comp._completeImpl("TG"):
+    ...     print suggestion
     '''
 
     def __init__(self):
-        ROOT.gInterpreter.Declare(_TTabComHookCode)
+        self.hook = None 
         self.active = True
         self.firstActivation = True
+        self.accessors = [".", "->", "::"]
 
     def activate(self):
         self.active = True
         if self.firstActivation:
-           utils._loadLibrary("libRint")
-           self.firstActivation = False
+            utils.declareCppCode('#include "dlfcn.h"')
+            dlOpenRint = 'dlopen("libRint.so",RTLD_NOW);'
+            utils.processCppCode(dlOpenRint)
+            utils.declareCppCode(_TTabComHookCode)
+            self.hook = ROOT._TTabComHook
+            self.firstActivation = False
 
     def deactivate(self):
         self.active = False
+
+    def _getSuggestions(self,line):
+        if self.active:
+            return self.hook(line)
+        return []
+
+    def _getLastAccessorPos(self,line):
+        accessorPos = -1
+        for accessor in self.accessors:
+            tmpAccessorPos = line.rfind(accessor)
+            if accessorPos < tmpAccessorPos:
+                accessorPos = tmpAccessorPos+len(accessor)
+        return accessorPos
+
+    def _completeImpl(self, line):
+        suggestions = self._getSuggestions(line)
+        print "LINE: %s SUGGESTIONS: %s" %(line,sorted(suggestions))
+        if not suggestions: return []
+        accessorPos = self._getLastAccessorPos(line)
+        if accessorPos > 0:
+            suggestions = [line[:accessorPos]+sugg for sugg in suggestions]
+        return suggestions
 
     def complete(self, ip, event) :
         '''
@@ -42,21 +120,15 @@ class CppCompleter(object):
         myGraph.Set<tab> will return "myGraph.Set+suggestion in the list of
         suggestions.
         '''
-        if self.active:
-           suggestions = ROOT._TTabComHook(event.line)
-           accessors = [".", "->", "::"]
-           if any(accessor in event.line for accessor in accessors):
-               suggestions = [event.line+sugg for sugg in suggestions]
-           return suggestions
-        else:
-           return []
+        return self._completeImpl(event.line)
 
 
 _cppCompleter = CppCompleter()
 
 def load_ipython_extension(ipython):
     _cppCompleter.activate()
-    ipython.set_hook('complete_command', _cppCompleter.complete,re_key=r"(.+)")
+    ipython.set_hook('complete_command', _cppCompleter.complete, re_key=r"(.*)")
 
 def unload_ipython_extension(ipython):
     _cppCompleter.deactivate()
+
