dba7aabd10ec0a8edba60f1017a6b596cb4ed53b
Danilo Piparo <danilo.piparo@cern.ch>
2015-09-01 23:55:02 +0200

    Start testing notebooks
    The notebook is run locally and the output is saved as a notebook. Then the
    output notebook is compared with the original one using the python difflib
    module. If the notebooks do not differ modulo
    
    * the welcome banner
    * the new line at the end of the file
    
    the test passes.
    
    Two tiny notebooks have been added to the directory. They are small, about
    1.5 kB big. This is size is comparable with simple C++ test programs but
    notebooks can get quickly quite big if one takes into account markdown cells,
    inlined images, jsROOT visualisation. For this reason it would be desirable
    to download with cmake platform independent commands the notebooks from the web.
    The list of notebooks is written down explicitely with the goal of avoiding
    tests silently "disappearing" and not failing in presence of download issues.

---
+++ python/ROOTaaS/nbdiff.py
@@ -0,0 +1,52 @@
+import difflib
+import subprocess
+
+nbExtension=".ipynb"
+convCmdTmpl = "ipython nbconvert nbconvert  --to notebook --ExecutePreprocessor.enabled=True %s --output %s"
+
+# Replace the criterion according to which a line shall be skipped
+def customLineJunkFilter(line):
+	# Skip the banner and empty lines
+	isJunk = "Welcome to ROOTaaS 6." in line
+	return not isJunk
+
+def getFilteredLines(fileName):
+    filteredLines =  filter(customLineJunkFilter, open(fileName).readlines())
+    # Sometimes the jupyter server adds a new line at the end of the notebook
+    # and nbconvert does not.
+    lastLine = filteredLines[-1]
+    if lastLine[-1] != "\n": filteredLines[-1] += "\n"
+    return filteredLines
+
+def compareNotebooks(inNBName,outNBName):
+    inNBLines = getFilteredLines(inNBName)
+    outNBLines = getFilteredLines(outNBName)
+    areDifferent = False
+    for line in difflib.unified_diff(inNBLines, outNBLines, fromfile=inNBName, tofile=outNBName):
+        areDifferent = True
+        sys.stdout.write(line)
+    if areDifferent: print "\n"
+    return areDifferent
+
+def canReproduceNotebook(inNBName):
+    outNBName = inNBName.replace(nbExtension,"_out"+nbExtension)
+    convCmd = convCmdTmpl %(inNBName,outNBName)
+    subprocess.check_output(convCmd.split())
+    return compareNotebooks(inNBName,outNBName)
+
+def isInputNotebookFileName(filename):
+    if not filename.endswith(".ipynb"):
+    	print "Notebook files shall have the %s extension" %nbExtension
+    	return False
+    return True
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+    	print "Usage: nbdiff.py myNotebook.ipynb"
+        sys.exit(1)
+    nbFileName = sys.argv[1]
+    if not isInputNotebookFileName(nbFileName):
+        sys.exit(1)
+    retCode = canReproduceNotebook(nbFileName)
+    sys.exit(retCode)
