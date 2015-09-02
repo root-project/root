c00b2ef7202b7059fe94c6f1fa1bd95be3ef0d75
Pere Mato <pere.mato@cern.ch>
2015-09-02 23:16:40 +0200

    Implemented options minimal and gminimal. Fixes ROOT-7605 and ROOT-7606

--- cmake/modules/RootBuildOptions.cmake
+++ cmake/modules/RootBuildOptions.cmake
@@ -197,6 +197,18 @@ option(all "Enable all optional components" OFF)
 option(testing "Enable testing with CTest" OFF)
 option(roottest "Include roottest, if roottest exists in root or if it is a sibling directory." OFF)
 
+#---Apply minimal or gminimal------------------------------------------------------------------
+foreach(opt ${root_build_options})
+  if(opt MATCHES "thread|cxx11|cling|builtin_llvm|builtin_ftgl|explicitlink")
+    continue()
+  endif()
+  if(minimal)
+    set(${opt} OFF CACHE BOOL "" FORCE)
+  elseif(gminimal AND NOT opt MATCHES "x11|cocoa")
+    set(${opt} OFF CACHE BOOL "" FORCE)
+  endif()
+endforeach()
+
 #---Avoid creating dependencies to 'non-statndard' header files -------------------------------
 include_regular_expression("^[^.]+$|[.]h$|[.]icc$|[.]hxx$|[.]hpp$")
 
