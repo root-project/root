e2cee5d76496483c6787f68afa8e119ed983d3b8
Axel Naumann <Axel.Naumann@cern.ch>
2015-09-07 17:33:28 +0200

    Add an explicit warning that this is code in prototype stage.

--- tutorials/v7/perf.cxx
+++ tutorials/v7/perf.cxx
@@ -2,6 +2,7 @@
 /// \ingroup Tutorials
 /// \author Axel Naumann <axel@cern.ch>
 /// \date 2015-07-08
+/// \warning This is part of the ROOT 7 prototype! It will change without notice, it might do evil. Feedback is welcome!
 
 /*************************************************************************
  * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
