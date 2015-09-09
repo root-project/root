a9a7f330006e2d93d2a0f56ef1e25349a5592a96
Philippe Canal <pcanal@fnal.gov>
2015-09-09 00:07:57 -0500

    Suppress more diagnostics.

--- interpreter/cling/lib/Interpreter/LookupHelper.cpp
+++ interpreter/cling/lib/Interpreter/LookupHelper.cpp
@@ -42,6 +42,7 @@ namespace clang {
     decltype(Parser::TemplateIds) OldTemplateIds;
     bool ResetIncrementalProcessing;
     bool OldSuppressAllDiagnostics;
+    bool OldPPSuppressAllDiagnostics;
     bool OldSpellChecking;
     SourceLocation OldPrevTokLocation;
     unsigned short OldParenCount, OldBracketCount, OldBraceCount;
@@ -54,7 +55,9 @@ namespace clang {
       : P(&p), PP(p.getPreprocessor()),
         ResetIncrementalProcessing(p.getPreprocessor()
                                    .isIncrementalProcessingEnabled()),
-        OldSuppressAllDiagnostics(p.getPreprocessor().getDiagnostics()
+        OldSuppressAllDiagnostics(P->getActions().getDiagnostics()
+                                  .getSuppressAllDiagnostics()),
+        OldPPSuppressAllDiagnostics(p.getPreprocessor().getDiagnostics()
                                   .getSuppressAllDiagnostics()),
         OldSpellChecking(p.getPreprocessor().getLangOpts().SpellChecking),
         OldPrevTokLocation(p.PrevTokLocation),
@@ -83,7 +86,9 @@ namespace clang {
       PP.enableIncrementalProcessing(ResetIncrementalProcessing);
       // Doesn't reset the diagnostic mappings
       P->getActions().getDiagnostics().Reset(/*soft=*/true);
-      PP.getDiagnostics().setSuppressAllDiagnostics(OldSuppressAllDiagnostics);
+      P->getActions().getDiagnostics().setSuppressAllDiagnostics(OldSuppressAllDiagnostics);
+      PP.getDiagnostics().Reset(/*soft=*/true);
+      PP.getDiagnostics().setSuppressAllDiagnostics(OldPPSuppressAllDiagnostics);
       const_cast<LangOptions&>(PP.getLangOpts()).SpellChecking =
          OldSpellChecking;
 
@@ -128,6 +133,8 @@ namespace cling {
     //
     //  Tell the diagnostic engine to ignore all diagnostics.
     //
+    P.getActions().getDiagnostics().setSuppressAllDiagnostics(
+                                      diagOnOff == LookupHelper::NoDiagnostics);
     PP.getDiagnostics().setSuppressAllDiagnostics(
                                       diagOnOff == LookupHelper::NoDiagnostics);
     //
