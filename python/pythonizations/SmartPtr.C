784918bfedf91f2ae9c1cffcab261a18a241b202
Philippe Canal <pcanal@fnal.gov>
2015-09-10 15:49:33 -0500

    avoid warning about unused parameter

--- python/pythonizations/SmartPtr.C
+++ python/pythonizations/SmartPtr.C
@@ -4,15 +4,15 @@ auto_ptr<MyShareable> gime_mine() { return mine; }
 auto_ptr<MyShareable>* gime_mine_ptr() { return &mine; }
 auto_ptr<MyShareable>& gime_mine_ref() { return mine; }
 
-void pass_mine_sp(auto_ptr<MyShareable> p)
+void pass_mine_sp(auto_ptr<MyShareable> )
 //{ cout << "Use count " << p->use_count() << "\n"; }
 { } // cout << "pass_mine_sp: underlying ptr " << p.get() << "\n"; }
 
-void pass_mine_sp_ref(auto_ptr<MyShareable>& p)
+void pass_mine_sp_ref(auto_ptr<MyShareable>& )
 //{ cout << "Use count " << p->use_count() << "\n"; }
 { } // cout << "pass_mine_sp_ref: underlying ptr " << p.get() << "\n"; }
 
-void pass_mine_sp_ptr(auto_ptr<MyShareable>* p)
+void pass_mine_sp_ptr(auto_ptr<MyShareable>* )
 //{ cout << "Use count " << p->use_count() << "\n"; }
 { } // cout << "pass_mine_sp_ptr: underlying ptr " << p->get() << "\n"; }
 
