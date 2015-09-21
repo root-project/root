be5aaffd1984a81e0848bdbcd912fd3b49575be5
Philippe Canal <pcanal@fnal.gov>
2015-09-21 03:10:25 +0200

    Fix python execution (broken by the use of std::numeric_limits.

--- tree/tree/inc/TVirtualTreePlayer.h
+++ tree/tree/inc/TVirtualTreePlayer.h
@@ -43,7 +43,10 @@ private:
 
 public:
    // Used as the max value for any TTree range operation.
-   static constexpr Long64_t kMaxEntries = std::numeric_limits<Long64_t>::max();
+   static constexpr Long64_t kMaxEntries = LLONG_MAX;
+   // Note: using
+   //   static constexpr Long64_t kMaxEntries = std::numeric_limits<Long64_t>::max();
+   // leads to a seg-fault in clang seen in most roottest's PyROOT executions ...
 
    TVirtualTreePlayer() { }
    virtual ~TVirtualTreePlayer();
