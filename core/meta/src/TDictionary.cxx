910d40e8e5b4bb7241850ec7fedf4014d9a4f4de
Olivier Couet <olivier.couet@cern.ch>
2015-09-22 16:03:24 +0200

    Doxygen & spelling

--- core/meta/src/TDictionary.cxx
+++ core/meta/src/TDictionary.cxx
@@ -9,34 +9,29 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// TDictionary                                                          //
-//                                                                      //
-// This class defines an abstract interface that must be implemented    //
-// by all classes that contain dictionary information.                  //
-//                                                                      //
-// The dictionary is defined by the followling classes:                 //
-// TDataType                              (typedef definitions)         //
-// TGlobal                                (global variables)            //
-// TGlobalFunc                            (global functions)            //
-// TClass                                 (classes)                     //
-//    TBaseClass                          (base classes)                //
-//    TDataMember                         (class datamembers)           //
-//    TMethod                             (class methods)               //
-//       TMethodArg                       (method arguments)            //
-//                                                                      //
-// All the above classes implement the TDictionary abstract interface.  //
-// Note: the indentation shows aggregation not inheritance.             //
-//                                                                      //
-// TMethodCall                            (method call environment)     //
-//                                                                      //
-//Begin_Html
-/*
-<img src="gif/tdictionary_classtree.gif">
+/** \class TDictionary
+
+This class defines an abstract interface that must be implemented
+by all classes that contain dictionary information.
+
+The dictionary is defined by the following classes:
+~~~ {.cpp}
+TDataType                              (typedef definitions)
+TGlobal                                (global variables)
+TGlobalFunc                            (global functions)
+TClass                                 (classes)
+   TBaseClass                          (base classes)
+   TDataMember                         (class datamembers)
+   TMethod                             (class methods)
+      TMethodArg                       (method arguments)
+~~~
+All the above classes implement the TDictionary abstract interface.
+Note: the indentation shows aggregation not inheritance.
+~~~ {.cpp}
+TMethodCall                            (method call environment)
+~~~
+\image html base_tdictionary.png
 */
-//End_Html
-//////////////////////////////////////////////////////////////////////////
 
 #include "TDictionary.h"
 #include "TClass.h"
