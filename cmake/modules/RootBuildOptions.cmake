96be867770fc3e9143b6badb78f1e86610ce0273
Pere Mato <pere.mato@cern.ch>
2015-09-30 11:47:26 +0200

    Fine tune RPATH behavior for MacOSX

--- cmake/modules/RootBuildOptions.cmake
+++ cmake/modules/RootBuildOptions.cmake
@@ -217,15 +217,15 @@ include(RootInstallDirs)
 #  When building, don't use the install RPATH already (but later on when installing)
 set(CMAKE_SKIP_BUILD_RPATH FALSE)         # don't skip the full RPATH for the build tree
 set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) # use always the build RPATH for the build tree
+set(CMAKE_MACOSX_RPATH TRUE)              # use RPATH for MacOSX
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE) # point to directories outside the build tree to the install RPATH
 
 # Check whether to add RPATH to the installation (the build tree always has the RPATH enabled)
 if(rpath)
   set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_FULL_LIBDIR}) # install LIBDIR
   set(CMAKE_SKIP_INSTALL_RPATH FALSE)          # don't skip the full RPATH for the install tree
-  set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)  # point to directories outside the build tree to the install RPATH
 else()
   set(CMAKE_SKIP_INSTALL_RPATH TRUE)           # skip the full RPATH for the install tree
-  set(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE) # point to directories outside the build tree to the install RPATH
 endif()
 
 
