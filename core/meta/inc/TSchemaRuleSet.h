cabaf201e0ce5327e0cb5c4a0ca7648c6b73ae64
Philippe Canal <pcanal@fnal.gov>
2015-10-02 10:06:42 -0500

    white space

--- core/meta/inc/TSchemaRuleSet.h
+++ core/meta/inc/TSchemaRuleSet.h
@@ -18,57 +18,57 @@ namespace ROOT {
 
    class TSchemaRuleSet: public TObject
    {
+   public:
+
+      class TMatches: public std::vector<const TSchemaRule*>
+      {
       public:
+         operator bool() { return !empty(); }
+         const TSchemaRule* GetRuleWithSource( const TString& name ) const;
+         const TSchemaRule* GetRuleWithTarget( const TString& name ) const;
+         Bool_t       HasRuleWithSource( const TString& name, Bool_t needingAlloc ) const;
+         Bool_t       HasRuleWithTarget( const TString& name, Bool_t willset ) const;
+      };
 
-         class TMatches: public std::vector<const TSchemaRule*>
-         {
-         public:
-            operator bool() { return !empty(); }
-            const TSchemaRule* GetRuleWithSource( const TString& name ) const;
-            const TSchemaRule* GetRuleWithTarget( const TString& name ) const;
-            Bool_t       HasRuleWithSource( const TString& name, Bool_t needingAlloc ) const;
-            Bool_t       HasRuleWithTarget( const TString& name, Bool_t willset ) const;
-         };
+      enum EConsistencyCheck {
+         kNoCheck       = 0,
+         kCheckAll      = 1,
+         kCheckConflict = 2
+      };
 
-         enum EConsistencyCheck {
-            kNoCheck       = 0,
-            kCheckAll      = 1,
-            kCheckConflict = 2
-         };
+      TSchemaRuleSet();
+      virtual ~TSchemaRuleSet();
 
-         TSchemaRuleSet();
-         virtual ~TSchemaRuleSet();
+      Bool_t              AddRule( TSchemaRule* rule, EConsistencyCheck checkConsistency = kCheckAll, TString *errmsg = 0 );
+      Bool_t              AddRules( TSchemaRuleSet* rules, EConsistencyCheck checkConsistency = kCheckAll, TString *errmsg = 0);
+      Bool_t              HasRuleWithSourceClass( const TString &source) const;
+      const TMatches FindRules( const TString &source ) const;
+      const TMatches FindRules( const TString &source, Int_t version ) const;
+      const TMatches FindRules( const TString &source, UInt_t checksum ) const;
+      const TMatches FindRules( const TString &source, Int_t version, UInt_t checksum ) const;
+      TClass*             GetClass();
+      UInt_t              GetClassCheckSum() const;
+      TString             GetClassName() const;
+      Int_t               GetClassVersion() const;
+      const TObjArray*    GetRules() const;
+      const TObjArray*    GetPersistentRules() const;
+      void                RemoveRule( TSchemaRule* rule );
+      void                RemoveRules( TObjArray* rules );
+      void                SetClass( TClass* cls );
 
-         Bool_t              AddRule( TSchemaRule* rule, EConsistencyCheck checkConsistency = kCheckAll, TString *errmsg = 0 );
-         Bool_t              AddRules( TSchemaRuleSet* rules, EConsistencyCheck checkConsistency = kCheckAll, TString *errmsg = 0);
-         Bool_t              HasRuleWithSourceClass( const TString &source) const;
-         const TMatches FindRules( const TString &source ) const;
-         const TMatches FindRules( const TString &source, Int_t version ) const;
-         const TMatches FindRules( const TString &source, UInt_t checksum ) const;
-         const TMatches FindRules( const TString &source, Int_t version, UInt_t checksum ) const;
-         TClass*             GetClass();
-         UInt_t              GetClassCheckSum() const;
-         TString             GetClassName() const;
-         Int_t               GetClassVersion() const;
-         const TObjArray*    GetRules() const;
-         const TObjArray*    GetPersistentRules() const;
-         void                RemoveRule( TSchemaRule* rule );
-         void                RemoveRules( TObjArray* rules );
-         void                SetClass( TClass* cls );
+      void                ls(Option_t *option="") const;
+      void                AsString(TString &out) const;
 
-         void                ls(Option_t *option="") const;
-         void                AsString(TString &out) const;
+      ClassDef( TSchemaRuleSet, 1 )
 
-         ClassDef( TSchemaRuleSet, 1 )
-
-      private:
-         TObjArray*                             fPersistentRules; //  Array of the rules that will be embeded in the file
-         TObjArray*                             fRemainingRules;  //! Array of non-persisten rules - just for cleanup purposes - owns the elements
-         TObjArray*                             fAllRules;        //! Array of all rules
-         TClassRef                              fClass;           //! Target class pointer (for consistency checking)
-         TString                                fClassName;       //  Target class name
-         Int_t                                  fVersion;         //  Target class version
-         UInt_t                                 fCheckSum;        //  Target class checksum
+   private:
+      TObjArray* fPersistentRules; //  Array of the rules that will be embeded in the file
+      TObjArray* fRemainingRules;  //! Array of non-persisten rules - just for cleanup purposes - owns the elements
+      TObjArray* fAllRules;        //! Array of all rules
+      TClassRef  fClass;           //! Target class pointer (for consistency checking)
+      TString    fClassName;       //  Target class name
+      Int_t      fVersion;         //  Target class version
+      UInt_t     fCheckSum;        //  Target class checksum
    };
 
 } // End of Namespace ROOT
