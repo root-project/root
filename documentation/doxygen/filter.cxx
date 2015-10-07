062be31ed56cccd3587003a37d2f0b10c507593c
Olivier Couet <olivier.couet@cern.ch>
2015-10-07 10:47:30 +0200

    Rewrite filter.cxx without using ROOT classes (mainly TString and TSystem).

--- documentation/doxygen/filter.cxx
+++ documentation/doxygen/filter.cxx
@@ -5,83 +5,42 @@
 //    filters doc files.
 //
 
-
+#include <stdio.h>
+#include <string>
+#include <iostream>
 #include <stdlib.h>
-#include <Riostream.h>
-#include <time.h>
-#include <TString.h>
-#include <TROOT.h>
-#include <TError.h>
-#include <TRandom.h>
-#include <TBenchmark.h>
-#include <TSystem.h>
-#include <TApplication.h>
-#include <TDatime.h>
-#include <TFile.h>
-#include <TF1.h>
-#include "TF2.h"
-#include <TF3.h>
-#include <TH2.h>
-#include <TNtuple.h>
-#include <TProfile.h>
-#include "TString.h"
+#include <stdarg.h>
+#include <memory>
 
-#include <TStyle.h>
-#include <TCanvas.h>
-#include <TColor.h>
-#include <TFrame.h>
-#include <TPostScript.h>
-#include <TPDF.h>
-#include <TLine.h>
-#include <TMarker.h>
-#include <TPolyLine.h>
-#include <TLatex.h>
-#include <TMathText.h>
-#include <TLegend.h>
-#include <TEllipse.h>
-#include <TCurlyArc.h>
-#include <TArc.h>
-#include <TPaveText.h>
-#include <TPaveStats.h>
-#include <TPaveLabel.h>
-#include <TGaxis.h>
-#include <TGraph.h>
-#include <TGraphErrors.h>
-#include <TGraphAsymmErrors.h>
-#include <TGraphBentErrors.h>
-#include <TMultiGraph.h>
-#include <TGraph2D.h>
-#include <TParallelCoord.h>
-#include <TImage.h>
-#include <TMath.h>
-#include <TSystem.h>
-
+using namespace std;
 
 // Auxiliary functions
-void    GetClassName();
-void    StandardizeKeywords();
-void    ExecuteMacro();
-void    ExecuteCommand(TString);
-
+void   GetClassName();
+void   StandardizeKeywords();
+void   ExecuteMacro();
+void   ExecuteCommand(string);
+void   ReplaceAll(string&, const string&, const string&);
+string StringFormat(const string fmt_str, ...);
+bool   EndsWith(string const &, string const &);
+bool   BeginsWith(const string&, const string&);
 
 // Global variables.
-char    gLine[255];   // Current line in the current input file
-TString gFileName;    // Input file name
-TString gLineString;  // Current line (as a TString) in the current input file
-TString gClassName;   // Current class name
-TString gImageName;   // Current image name
-TString gMacroName;   // Current macro name
-TString gCwd;         // Current working directory
-TString gOutDir;      // Output directory
-Bool_t  gHeader;      // True if the input file is a header
-Bool_t  gSource;      // True if the input file is a source file
-Bool_t  gImageSource; // True the source of the current macro should be shown
-Bool_t  gInClassDef;
-Bool_t  gClass;
-Int_t   gInMacro;
-Int_t   gImageID;
-Int_t   gMacroID;
-
+char   gLine[255];   // Current line in the current input file
+string gFileName;    // Input file name
+string gLineString;  // Current line (as a string) in the current input file
+string gClassName;   // Current class name
+string gImageName;   // Current image name
+string gMacroName;   // Current macro name
+string gCwd;         // Current working directory
+string gOutDir;      // Output directory
+bool   gHeader;      // True if the input file is a header
+bool   gSource;      // True if the input file is a source file
+bool   gImageSource; // True the source of the current macro should be shown
+bool   gInClassDef;
+bool   gClass;
+int    gInMacro;
+int    gImageID;
+int    gMacroID;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Filter ROOT files for Doxygen.
@@ -91,27 +50,28 @@ int main(int argc, char *argv[])
    // Initialisation
 
    gFileName    = argv[1];
-   gHeader      = kFALSE;
-   gSource      = kFALSE;
-   gInClassDef  = kFALSE;
-   gClass       = kFALSE;
-   gImageSource = kFALSE;
+   gHeader      = false;
+   gSource      = false;
+   gInClassDef  = false;
+   gClass       = false;
+   gImageSource = false;
    gInMacro     = 0;
    gImageID     = 0;
    gMacroID     = 0;
-   if (gFileName.EndsWith(".cxx")) gSource = kTRUE;
-   if (gFileName.EndsWith(".h"))   gHeader = kTRUE;
+   if (EndsWith(gFileName,".cxx")) gSource = true;
+   if (EndsWith(gFileName,".h"))   gHeader = true;
    GetClassName();
 
    // Retrieve the current working directory
-   gCwd = gFileName(0, gFileName.Last('/'));
+   int last = gFileName.rfind("/");
+   gCwd     = gFileName.substr(0,last);
 
    // Retrieve the output directory
-   gOutDir = gSystem->Getenv("DOXYGEN_OUTPUT_DIRECTORY");
-   gOutDir.ReplaceAll("\"","");
+   gOutDir = getenv("DOXYGEN_OUTPUT_DIRECTORY");
+   ReplaceAll(gOutDir,"\"","");
 
    // Open the input file name.
-   FILE *f = fopen(gFileName.Data(),"r");
+   FILE *f = fopen(gFileName.c_str(),"r");
 
    // File for inline macros.
    FILE *m = 0;
@@ -121,14 +81,10 @@ int main(int argc, char *argv[])
       while (fgets(gLine,255,f)) {
          gLineString = gLine;
 
-         if (gLineString.BeginsWith("class"))    gInClassDef = kTRUE;
-         if (gLineString.Index("ClassDef") >= 0) gInClassDef = kFALSE;
+         if (BeginsWith(gLineString,"class"))              gInClassDef = true;
+         if (gLineString.find("ClassDef") != string::npos) gInClassDef = false;
 
-//         if (gInClassDef && gLineString.Index("//") >= 0) {
-//            gLineString.ReplaceAll("//","///<");
-//         }
-
-         printf("%s",gLineString.Data());
+         printf("%s",gLineString.c_str());
       }
       fclose(f);
       return 0;
@@ -140,55 +96,53 @@ int main(int argc, char *argv[])
          gLineString = gLine;
          StandardizeKeywords();
 
-         if (gLineString.Index("/*! \\class") >= 0 ||
-             gLineString.Index("/// \\class") >= 0 ||
-             gLineString.Index("/** \\class") >= 0 ||
-             gLineString.Index("///! \\class") >= 0) gClass = kTRUE;
+         if (gLineString.find("/*! \\class") != string::npos ||
+             gLineString.find("/// \\class") != string::npos ||
+             gLineString.find("/** \\class") != string::npos ||
+             gLineString.find("///! \\class") != string::npos) gClass = true;
 
-         if (gLineString.Index("begin_html") >= 0) {
+         if (gLineString.find("begin_html") != string::npos) {
             if (!gClass) {
-               gLineString = TString::Format("/*! \\class %s\n",gClassName.Data());
-               gClass = kTRUE;
+               gLineString = StringFormat("/*! \\class %s\n",gClassName.c_str());
+               gClass = true;
             } else {
-               gLineString.ReplaceAll("begin_html","");
+               ReplaceAll(gLineString,"begin_html","");
             }
          }
 
-         if (gLineString.Index("end_html") >= 0) {
-            gLineString.ReplaceAll("end_html","");
+         if (gLineString.find("end_html") != string::npos) {
+            ReplaceAll(gLineString,"end_html","");
          }
 
-         if (gLineString.Index("End_Macro") >= 0) {
-            gLineString.ReplaceAll("End_Macro","");
-            gImageSource = kFALSE;
+         if (gLineString.find("End_Macro") != string::npos) {
+            ReplaceAll(gLineString,"End_Macro","");
+            gImageSource = false;
             gInMacro = 0;
             if (m) {
                fclose(m);
                m = 0;
-               ExecuteCommand(TString::Format("root -l -b -q \"makeimage.C(\\\"%s\\\",\\\"%s\\\",\\\"%s\\\")\""
-                                              , TString::Format("%s_%3.3d.C", gClassName.Data(), gMacroID).Data()
-                                              , TString::Format("%s_%3.3d.png", gClassName.Data(), gImageID).Data()
-                                              , gOutDir.Data()));
-               ExecuteCommand(TString::Format("rm %s_%3.3d.C", gClassName.Data(), gMacroID).Data());
+               ExecuteCommand(StringFormat("root -l -b -q \"makeimage.C(\\\"%s\\\",\\\"%s\\\",\\\"%s\\\")\""
+                                              , StringFormat("%s_%3.3d.C", gClassName.c_str(), gMacroID).c_str()
+                                              , StringFormat("%s_%3.3d.png", gClassName.c_str(), gImageID).c_str()
+                                              , gOutDir.c_str()));
+               ExecuteCommand(StringFormat("rm %s_%3.3d.C", gClassName.c_str(), gMacroID));
             }
          }
 
          if (gInMacro) {
             if (gInMacro == 1) {
-               if (gLineString.EndsWith(".C\n")) {
+               if (EndsWith(gLineString,".C\n")) {
                   ExecuteMacro();
                   gInMacro++;
                } else {
                   gMacroID++;
-                  m = fopen(TString::Format("%s_%3.3d.C", gClassName.Data()
-                                                        , gMacroID)
-                                                        , "w");
-                  if (m) fprintf(m,"%s",gLineString.Data());
-                  if (gLineString.BeginsWith("{")) {
+                  m = fopen(StringFormat("%s_%3.3d.C", gClassName.c_str(), gMacroID).c_str(), "w");
+                  if (m) fprintf(m,"%s",gLineString.c_str());
+                  if (BeginsWith(gLineString,"{")) {
                      if (gImageSource) {
-                        gLineString.ReplaceAll("{"
-                                                , TString::Format("\\include %s_%3.3d.C"
-                                                , gClassName.Data()
+                        ReplaceAll(gLineString,"{"
+                                                , StringFormat("\\include %s_%3.3d.C"
+                                                , gClassName.c_str()
                                                 , gMacroID));
                      } else {
                         gLineString = "\n";
@@ -197,11 +151,11 @@ int main(int argc, char *argv[])
                   gInMacro++;
                }
             } else {
-               if (m) fprintf(m,"%s",gLineString.Data());
-               if (gLineString.BeginsWith("}")) {
-                  gLineString.ReplaceAll("}"
-                                          , TString::Format("\\image html %s_%3.3d.png"
-                                          , gClassName.Data()
+               if (m) fprintf(m,"%s",gLineString.c_str());
+               if (BeginsWith(gLineString,"}")) {
+                  ReplaceAll(gLineString,"}"
+                                          , StringFormat("\\image html %s_%3.3d.png"
+                                          , gClassName.c_str()
                                           , gImageID));
                } else {
                   gLineString = "\n";
@@ -210,14 +164,14 @@ int main(int argc, char *argv[])
             }
          }
 
-         if (gLineString.Index("Begin_Macro") >= 0) {
-            if (gLineString.Index("source") >= 0)    gImageSource = kTRUE;
+         if (gLineString.find("Begin_Macro") != string::npos) {
+            if (gLineString.find("source") != string::npos)    gImageSource = true;
             gImageID++;
             gInMacro++;
             gLineString = "\n";
          }
 
-         printf("%s",gLineString.Data());
+         printf("%s",gLineString.c_str());
       }
       fclose(f);
       return 0;
@@ -226,31 +180,30 @@ int main(int argc, char *argv[])
    // Output anything not header nor source
    while (fgets(gLine,255,f)) {
       gLineString = gLine;
-      printf("%s",gLineString.Data());
+      printf("%s",gLineString.c_str());
    }
    fclose(f);
    return 0;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Retrieve the class name.
 
 void GetClassName()
 {
-   Int_t i1 = 0;
-   Int_t i2 = 0;
+   int i1 = 0;
+   int i2 = 0;
 
-   FILE *f = fopen(gFileName.Data(),"r");
+   FILE *f = fopen(gFileName.c_str(),"r");
 
    // File header.
    if (gHeader) {
       while (fgets(gLine,255,f)) {
          gLineString = gLine;
-         if (gLineString.Index("ClassDef") >= 0) {
-            i1 = gLineString.Index("(")+1;
-            i2 = gLineString.Index(",")-1;
-            gClassName = gLineString(i1,i2-i1+1);
+         if (gLineString.find("ClassDef") != string::npos) {
+            i1         = gLineString.find("(")+1;
+            i2         = gLineString.find(",")-1;
+            gClassName = gLineString.substr(i1,i2-i1+1);
             fclose(f);
             return;
          }
@@ -261,10 +214,10 @@ void GetClassName()
    if (gSource) {
       while (fgets(gLine,255,f)) {
          gLineString = gLine;
-         if (gLineString.Index("ClassImp") >= 0) {
-            i1 = gLineString.Index("(")+1;
-            i2 = gLineString.Index(")")-1;
-            gClassName = gLineString(i1,i2-i1+1);
+         if (gLineString.find("ClassImp") != string::npos) {
+            i1         = gLineString.find("(")+1;
+            i2         = gLineString.find(")")-1;
+            gClassName = gLineString.substr(i1,i2-i1+1);
             fclose(f);
             return;
          }
@@ -276,21 +229,19 @@ void GetClassName()
    return;
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
 /// Standardize the THTML keywords to ease the parsing.
 
 void StandardizeKeywords()
 {
-   gLineString.ReplaceAll("End_Html","end_html");
-   gLineString.ReplaceAll("End_html","end_html");
-   gLineString.ReplaceAll("end_html ","end_html");
-   gLineString.ReplaceAll("Begin_Html","begin_html");
-   gLineString.ReplaceAll("Begin_html","begin_html");
-   gLineString.ReplaceAll("<big>","");
-   gLineString.ReplaceAll("</big>","");
+   ReplaceAll(gLineString,"End_Html","end_html");
+   ReplaceAll(gLineString,"End_html","end_html");
+   ReplaceAll(gLineString,"end_html ","end_html");
+   ReplaceAll(gLineString,"Begin_Html","begin_html");
+   ReplaceAll(gLineString,"Begin_html","begin_html");
+   ReplaceAll(gLineString,"<big>","");
+   ReplaceAll(gLineString,"</big>","");
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Execute the macro in gLineString and produce the corresponding picture
@@ -298,44 +249,97 @@ void StandardizeKeywords()
 void ExecuteMacro()
 {
    // Name of the next Image to be generated
-   gImageName = TString::Format("%s_%3.3d.png", gClassName.Data()
+   gImageName = StringFormat("%s_%3.3d.png", gClassName.c_str()
                                               , gImageID);
 
    // Retrieve the macro to be executed.
-   if (gLineString.Index("../../..") >= 0) {
-      gLineString.ReplaceAll("../../..","../..");
+   if (gLineString.find("../../..") != string::npos) {
+      ReplaceAll(gLineString,"../../..","../..");
    } else {
-      gLineString.Prepend(TString::Format("%s/../doc/macros/",gCwd.Data()));
+      gLineString.insert(0, StringFormat("%s/../doc/macros/",gCwd.c_str()));
    }
-   Int_t i1 = gLineString.Last('/')+1;
-   Int_t i2 = gLineString.Last('C');
-   gMacroName = gLineString(i1,i2-i1+1);
+   int i1     = gLineString.rfind('/')+1;
+   int i2     = gLineString.rfind('C');
+   gMacroName = gLineString.substr(i1,i2-i1+1);
 
    // Build the ROOT command to be executed.
-   gLineString.Prepend(TString::Format("root -l -b -q \"makeimage.C(\\\""));
-   Int_t l = gLineString.Length();
-   gLineString.Replace(l-2,1,TString::Format("C\\\",\\\"%s\\\",\\\"%s\\\")\"",
-                                             gImageName.Data(),
-                                             gOutDir.Data()));
+   gLineString.insert(0, StringFormat("root -l -b -q \"makeimage.C(\\\""));
+   int l = gLineString.length();
+   gLineString.replace(l-2,1,StringFormat("C\\\",\\\"%s\\\",\\\"%s\\\")\"", gImageName.c_str(), gOutDir.c_str()));
 
    ExecuteCommand(gLineString);
    if (gImageSource) {
-      gLineString = TString::Format("\\include %s\n\\image html %s\n", gMacroName.Data(),
-                                                                       gImageName.Data());
+      gLineString = StringFormat("\\include %s\n\\image html %s\n", gMacroName.c_str(), gImageName.c_str());
    } else {
-      gLineString = TString::Format("\n\\image html %s\n", gImageName.Data());
+      gLineString = StringFormat("\n\\image html %s\n", gImageName.c_str());
    }
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Execute a command making sure stdout will not go in the doxygen file.
 
-void ExecuteCommand(TString command)
+void ExecuteCommand(string command)
 {
    int o = dup(fileno(stdout));
    freopen("stdout.dat","a",stdout);
-   gSystem->Exec(command.Data());
+   system(command.c_str());
    dup2(o,fileno(stdout));
    close(o);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Replace all instances of of a string with another string.
+
+void ReplaceAll(string& str, const string& from, const string& to) {
+   if (from.empty()) return;
+   string wsRet;
+   wsRet.reserve(str.length());
+   size_t start_pos = 0, pos;
+   while ((pos = str.find(from, start_pos)) != string::npos) {
+      wsRet += str.substr(start_pos, pos - start_pos);
+      wsRet += to;
+      pos += from.length();
+      start_pos = pos;
+   }
+   wsRet += str.substr(start_pos);
+   str.swap(wsRet);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// std::string formatting like sprintf
+
+string StringFormat(const string fmt_str, ...) {
+   int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+   string str;
+   unique_ptr<char[]> formatted;
+   va_list ap;
+   while (1) {
+      formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
+      strcpy(&formatted[0], fmt_str.c_str());
+      va_start(ap, fmt_str);
+      final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+      va_end(ap);
+      if (final_n < 0 || final_n >= n) n += abs(final_n - n + 1);
+      else break;
+   }
+   return string(formatted.get());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// find if a string ends with another string
+
+bool EndsWith(string const &fullString, string const &ending) {
+   if (fullString.length() >= ending.length()) {
+      return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+   } else {
+      return false;
+   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// find if a string begins with another string
+
+bool BeginsWith(const string& haystack, const string& needle) {
+   return needle.length() <= haystack.length()
+          && equal(needle.begin(), needle.end(), haystack.begin());
 }
