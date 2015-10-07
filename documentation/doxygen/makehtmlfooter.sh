c8f7dd840acc38401225a3647acbece1fbab5a87
Olivier Couet <olivier.couet@cern.ch>
2015-10-07 16:07:50 +0200

    Add the doxygen version in the footer... (good to know as it seems they do not all behave the same ....)

--- documentation/doxygen/makehtmlfooter.sh
+++ documentation/doxygen/makehtmlfooter.sh
@@ -8,7 +8,7 @@ echo '<div id="footer" style="background-color:#DDDDDD;">'
 echo '<small>'
 echo '<img class="footer" src="rootlogo_s.gif" alt="root"/></a>'
 # Doxygen unconditionally adds a space in front of $DOXYGEN_ROOT_VERSION
-echo 'ROOT'$DOXYGEN_ROOT_VERSION' - Reference Guide Generated on $datetime.'
+echo 'ROOT'$DOXYGEN_ROOT_VERSION' - Reference Guide Generated on $datetime using Doxygen '`doxygen --version`'.'
 echo '</small>'
 echo '</div>'
 echo '</body>'
