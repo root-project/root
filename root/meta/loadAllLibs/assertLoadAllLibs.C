27211e87a6f5112339312ec6e00c91a66dd58972
Pere Mato <pere.mato@cern.ch>
2015-10-28 17:01:11 +0100

    reduce the number of libraries to load

--- root/meta/loadAllLibs/assertLoadAllLibs.C
+++ root/meta/loadAllLibs/assertLoadAllLibs.C
@@ -11,6 +11,7 @@ int assertLoadAllLibs()
 
    auto libList = getLibrariesList();
    libList.unique();
+   libList.remove_if([](std::string){ static int n = 0; return (n++ % 2 == 0);});
    loadLibrariesInList(libList);
  
    return 0;
