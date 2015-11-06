8804f07dad03ff9c621e3cec362e04a1e46a5867
David Smith <David.Smith@cern.ch>
2015-11-06 10:13:01 +0100

    Fix bad placement of member definition under preprocessor conditional

--- net/netxng/inc/TNetXNGSystem.h
+++ net/netxng/inc/TNetXNGSystem.h
@@ -37,12 +37,12 @@ class TNetXNGSystem: public TSystem {
 
 private:
    std::set<void *>    fDirPtrs;
+   static THashList   fgAddrFQDN;  // Cache of addresses to FQDNs
+   static TMutex      fgAddrMutex; // Serialise access to the FQDN list
 #ifndef __CINT__
 private:
    XrdCl::URL        *fUrl;        // URL of this TSystem
    XrdCl::FileSystem *fFileSystem; // Cached for convenience
-   static THashList   fgAddrFQDN;  // Cache of addresses to FQDNs
-   static TMutex      fgAddrMutex; // Serialise access to the FQDN list
 
 #endif
 
