1109ae3921296cab9f8cfeda4e4e90ea44ee462e
Jochen Klein <jochen.klein@cern.ch>
2015-11-27 14:37:33 +0100

    add constructor for struct Environ required to suppress warning/error when compiling with -Werror=effc++

--- core/cont/inc/TCollectionProxyInfo.h
+++ core/cont/inc/TCollectionProxyInfo.h
@@ -224,6 +224,7 @@ namespace ROOT {
       size_t              fSpace;
    };
    template <typename T> struct Environ : public EnvironBase {
+      Environ() : fIterator() {}
       typedef T           Iter_t;
       Iter_t              fIterator;
       T& iter() { return fIterator; }
