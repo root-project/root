96cecc1353623e1ecf473a5c3a4449a919ebf7a6
Olivier Couet <olivier.couet@cern.ch>
2015-12-07 14:45:28 +0100

    If a macro generates several canvases they are all dumped as .png named pictx_xxx.png

--- documentation/doxygen/makeimage.C
+++ documentation/doxygen/makeimage.C
@@ -1,4 +1,6 @@
-// Generates the ImageName output of the macro MacroName
+/// Generates the ImageName output of the macro MacroName
+
+#include <stdio.h>
 
 void makeimage(const char *MacroName, const char *ImageName, const char *OutDir, bool cp)
 {
@@ -7,8 +9,13 @@ void makeimage(const char *MacroName, const char *ImageName, const char *OutDir,
 
    TIter iCanvas(gROOT->GetListOfCanvases());
    TVirtualPad* pad = 0;
-
+   int ImageNum = 0;
    while ((pad = (TVirtualPad*) iCanvas())) {
-      pad->SaveAs(TString::Format("%s/html/%s",OutDir,ImageName));
+      ImageNum++;
+      pad->SaveAs(TString::Format("%s/html/pict%d_%s",OutDir,ImageNum,ImageName));
    }
+
+   FILE *f = fopen("NumberOfImages.dat", "w");
+   fprintf(f,"%d\n",ImageNum);
+   fclose(f);
 }
