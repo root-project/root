6898c840d4ff3b4a04ce10f942078e8f9e6a83eb
Axel Naumann <Axel.Naumann@cern.ch>
2015-12-08 20:03:01 +0100

    Use pandoc JIRA filter.

--- build/unix/makereleasenotes.sh
+++ build/unix/makereleasenotes.sh
@@ -6,7 +6,7 @@ ROOTSRCDIR=`dirname $ROOTSRCDIR` # .../build
 ROOTSRCDIR=`dirname $ROOTSRCDIR` # ...
 
 DIR="README/ReleaseNotes"
-pandoc -f markdown -t html -s -S -f markdown --toc -H $ROOTSRCDIR/documentation/users-guide/css/github.css --mathjax \
+pandoc -f markdown -t html --filter $ROOTSRCDIR/build/unix/pandoc-jira.sh -s -S -f markdown --toc -H $ROOTSRCDIR/documentation/users-guide/css/github.css --mathjax \
 $ROOTSRCDIR/$DIR/v${VERS}/index.md -o $DIR/v${VERS}/index.html
 
 echo "Generated $DIR/v${VERS}/index.html"
