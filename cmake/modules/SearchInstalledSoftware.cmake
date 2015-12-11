b997a932610b5916a10ece6e019890ebbea6f70c
Pere Mato <pere.mato@cern.ch>
2015-12-11 12:10:22 +0100

    Handle correctly missing ZLIB for all platforms. Still some pats of ROOT will not build correctly.

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -27,9 +27,7 @@ if(NOT builtin_zlib)
    endif()
 endif()
 if(builtin_zlib)
-  if(WIN32)
-    set(ZLIB_LIBRARY "")
-  endif()
+  set(ZLIB_LIBRARY "" CACHE PATH "" FORCE)
 endif()
 
 #---Check for Freetype---------------------------------------------------------------
