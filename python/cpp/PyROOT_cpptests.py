638e785e2ca6f6ef89d4aed2fbab0660466fde6e
Philippe Canal <pcanal@fnal.gov>
2015-12-11 23:39:19 -0600

    Add test for ROOT-7783

--- python/cpp/PyROOT_cpptests.py
+++ python/cpp/PyROOT_cpptests.py
@@ -48,11 +48,19 @@ class Cpp1LanguageFeatureTestCase( MyTestCase ):
       t2 = TLorentzVector( 0., 0., 0.,  0. )
       t3 = TLorentzVector( t1 )
 
+      t4 = TLorentzVector(0, 0, 0, 0)
+      t4.__init__(TLorentzVector(0, 1, 2, 3))
+      # the following should work exactly as above, but no longer does on some version of ROOT 6
+      t5 = TLorentzVector(0, 0, 0, 0)
+      TLorentzVector.__init__(t5, TLorentzVector(0, 1, 2, 3))
+
       self.assertEqual( t1, t3 )
       self.assertNotEqual( t1, t2 )
+      #self.assertNotEqual( t4, t5 )
 
       for i in range(4):
          self.assertEqual( t1[i], t3[i] )
+         self.assertEqual( t4[i], t5[i] )
 
    def test04ObjectValidity( self ):
       """Test object validity checking"""
