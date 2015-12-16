05d43a85f256ad1bd1728138fad46a8392c2475e
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2015-12-16 11:30:12 +0100

    Fix path to terminal from ROOTbook

--- etc/notebook/custom/custom.js
+++ etc/notebook/custom/custom.js
@@ -2,5 +2,5 @@
 
 // Terminal button
 $(document).ready(function() {
-    $('div#header-container').append("<a href='terminals/1' class='btn btn-default btn-sm navbar-btn pull-right' style='margin-right: 4px; margin-left: 2px;'>Terminal</a>");
+    $('div#header-container').append("<a href='/terminals/1' class='btn btn-default btn-sm navbar-btn pull-right' style='margin-right: 4px; margin-left: 2px;'>Terminal</a>");
 });
