9c11b9e01791b62a73a263f7b4b766b554f4f700
Axel Naumann <Axel.Naumann@cern.ch>
2015-12-17 15:53:29 +0100

    Add dummy unit test for histograms to exercise "the system".

---
+++ root/hist/unit/add.cxx
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include "TH1.h"
+#include "ROOT/v7/TH1.h"
+
+// Tests the number of bins
+TEST(AxisTest, NumBins) {
+  EXPECT_EQ(0, 0);
+}
+
+// Tests the axis range
+TEST(AxisTest, RangeFromTo) {
+  EXPECT_EQ(1, 1);
+}
