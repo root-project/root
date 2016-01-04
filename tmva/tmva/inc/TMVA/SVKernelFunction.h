60b942c80f5f9ca87c3d41cbfed1ac6753f7d3b1
tjsteven <t.j.stevenson@qmul.ac.uk>
2016-01-04 10:53:20 +0000

    Add header for new SVM kernel functions

--- tmva/tmva/inc/TMVA/SVKernelFunction.h
+++ tmva/tmva/inc/TMVA/SVKernelFunction.h
@@ -39,25 +39,35 @@ namespace TMVA {
 
    public:
 
+     enum EKernelType { kLinear , kRBF, kPolynomial, kSigmoidal, kMultiGauss, kProd, kSum};
+
       SVKernelFunction();
       SVKernelFunction( Float_t );
+      SVKernelFunction( EKernelType, Float_t, Float_t=0);
+      SVKernelFunction( std::vector<float> params );
+      SVKernelFunction(EKernelType k, std::vector<EKernelType> kernels, std::vector<Float_t> gammas, Float_t gamma, Float_t order, Float_t theta);
       ~SVKernelFunction();
       
       Float_t Evaluate( SVEvent* ev1, SVEvent* ev2 );
 
-      enum EKernelType { kLinear , kRBF, kPolynomial, kSigmoidal };
+      //      enum EKernelType { kLinear , kRBF, kPolynomial, kSigmoidal };
 
       void setCompatibilityParams(EKernelType k, UInt_t order, Float_t theta, Float_t kappa);
          
    private:
 
       Float_t fGamma;   // documentation
-
+      
+      // TJS add vector of gammas for multidimensional gaussian
+      std::vector<Float_t> fmGamma;
+      
       // kernel, order, theta, and kappa are for backward compatibility
       EKernelType fKernel;
       UInt_t      fOrder;
       Float_t     fTheta;
       Float_t     fKappa;
+
+      std::vector<EKernelType> fKernelsList;
    };
 }
 
