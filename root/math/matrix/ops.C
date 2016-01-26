0d9c2a8c90139f82680f99ee5ffeda82204f9e30
Axel Naumann <Axel.Naumann@cern.ch>
2016-01-26 11:07:43 +0100

    Just test the type equality instead of relying on output.

--- root/math/matrix/ops.C
+++ root/math/matrix/ops.C
@@ -1,8 +1,18 @@
+#include <iostream>
+#include <TVectorD.h>
+
 // ROOT-7739
-{
+int ops() {
+  int ret = 0;
   double a = 2;
   TVectorD b(2);
-  gROOT->ProcessLine("a*b");
-  gROOT->ProcessLine("b*a");
-  exit(0);
+  if (!std::is_same<decltype(a*b), TVectorD>::value) {
+    std::cerr << "double * TVectorD is not TVectorD\n";
+    ++ret;
+  }
+  if (!std::is_same<decltype(b*a), TVectorD>::value) {
+    std::cerr << "TVectorD * double is not TVectorD\n";
+    ++ret;
+  }
+  return ret;
 }
