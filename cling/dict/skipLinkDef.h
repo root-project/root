897709d47902348f56f191addfdaaf296f95a4e5
Philippe Canal <pcanal@fnal.gov>
2016-02-18 16:33:30 -0600

    Add test for ROOT-7879

---
+++ cling/dict/skipLinkDef.h
@@ -0,0 +1,8 @@
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+
+#include "skippingHeader.h"
+
+#pragma link C++ class MyClass+;
