864bd84ee8906ad9a0df328ae97b9e2895293d50
Philippe Canal <pcanal@fnal.gov>
2016-02-18 16:37:09 -0600

    Add a means to customize which file are ignored in ForwardDeclPrinter.
    This will also rootcling to tell cling's ForwardDeclPrinter to consider the file included inside a LinkDef file as
    being top level headers (rather than listing the linkdef itself\!).

--- interpreter/cling/include/cling/Interpreter/Interpreter.h
+++ interpreter/cling/include/cling/Interpreter/Interpreter.h
@@ -48,6 +48,7 @@ namespace clang {
   class Sema;
   class SourceLocation;
   class SourceManager;
+  class PresumedLoc;
 }
 
 namespace cling {
@@ -74,6 +75,9 @@ namespace cling {
   ///
   class Interpreter {
   public:
+     // IgnoreFilesFunc_t takes a const reference to avoid having to
+     // include the actual definition of PresumedLoc.
+     using IgnoreFilesFunc_t = bool (*)(const clang::PresumedLoc&);
 
     ///\brief Pushes a new transaction, which will collect the decls that came
     /// within the scope of the RAII object. Calls commit transaction at
@@ -644,7 +648,9 @@ namespace cling {
     void forwardDeclare(Transaction& T, clang::Sema& S,
                         llvm::raw_ostream& out,
                         bool enableMacros = false,
-                        llvm::raw_ostream* logs = 0) const;
+                        llvm::raw_ostream* logs = 0,
+                        IgnoreFilesFunc_t ignoreFiles =
+                          [](const clang::PresumedLoc&) { return false;}) const;
 
     friend class runtime::internal::LifetimeHandler;
     // FIXME: workaround until JIT supports exceptions
