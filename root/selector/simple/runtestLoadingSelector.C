1ac19ec6aff6bab5de583cd297a9f439775c26e9
Philippe Canal <pcanal@fnal.gov>
2016-02-26 14:49:51 -0600

    White space

--- root/selector/simple/runtestLoadingSelector.C
+++ root/selector/simple/runtestLoadingSelector.C
@@ -10,15 +10,15 @@ gROOT->ProcessLine(".L testSelector.C");
 bool res = runtest();
 if (!res) return !res;
 #else
- fprintf(stderr,"testSelector result is 1 0 1\n");
- fprintf(stderr,"Info in <ACLiC>: script has already been loaded in interpreted mode\n");
- fprintf(stderr,"Info in <ACLiC>: unloading testSelector.C and compiling it\n");
+fprintf(stderr,"testSelector result is 1 0 1\n");
+fprintf(stderr,"Info in <ACLiC>: script has already been loaded in interpreted mode\n");
+fprintf(stderr,"Info in <ACLiC>: unloading testSelector.C and compiling it\n");
 #endif
 gROOT->ProcessLine(".L testSelector.C+");
 #if defined(ClingWorkAroundMissingDynamicScope) || defined(ClingWorkAroundBrokenUnnamedReturn)
- bool res;
- res = gROOT->ProcessLine("runtest();");
- res = !res;
+bool res;
+res = gROOT->ProcessLine("runtest();");
+res = !res;
 #else
 bool res = runtest();
 return !res;
