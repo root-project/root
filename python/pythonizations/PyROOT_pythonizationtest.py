ab89eb467390e05755785bb5876cbf0e1206722c
Philippe Canal <pcanal@fnal.gov>
2016-02-29 16:27:25 -0600

    Don't hide real errors.

--- python/pythonizations/PyROOT_pythonizationtest.py
+++ python/pythonizations/PyROOT_pythonizationtest.py
@@ -98,7 +98,7 @@ class TestClassROOT_PYTHONIZATIONS:
         ROOT.gEnv.SetValue("RooFit.Banner", "0")
         try:
             ws = ROOT.RooWorkspace('w')
-        except (AttributeError, TypeError) as e:
+        except AttributeError as e:
             if 'RooWorkspace' in str(e):
                 return            # no roofit enabled, just declare success
             raise
