1db6e72162f5e982e5e0e46221d1a47b0a6d629b
David Smith <David.Smith@cern.ch>
2016-03-01 08:51:47 +0100

    ROOT-8031. Do not automatically setup read cache during TTree::Fill()

--- tree/tree/src/TTree.cxx
+++ tree/tree/src/TTree.cxx
@@ -4366,9 +4366,6 @@ void TTree::DropBuffers(Int_t)
 
 Int_t TTree::Fill()
 {
-   // create cache if wanted
-   if (fCacheDoAutoInit) SetCacheSizeAux();
-
    Int_t nbytes = 0;
    Int_t nerror = 0;
    Int_t nb = fBranches.GetEntriesFast();
