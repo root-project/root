cf973e423d40301ca3a7e7247bc79b81682851cc
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2016-03-04 15:44:13 +0100

    Proof: fine tuning + doxygen tags

--- proof/proof/inc/TPackMgr.h
+++ proof/proof/inc/TPackMgr.h
@@ -95,7 +95,7 @@ public:
 
    void              Show(const char *title = 0);
    Int_t             Clean(const char *pack);
-   Int_t             Remove(const char *pack = 0);
+   Int_t             Remove(const char *pack = 0, Bool_t dolock = kTRUE);
    TList            *GetList() const;
 
    void              ShowEnabled(const char *title = 0);
