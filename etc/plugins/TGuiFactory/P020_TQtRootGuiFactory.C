9e67605992457ff3eba87858555cfa8b44c783bd
Axel Naumann <Axel.Naumann@cern.ch>
2016-03-15 20:31:23 +0100

    Revert "Prevent parsing for TQtRootGuiFactory plugin (ATLAS online)."
    This reverts commit 3a0eeb3866fa380221815fbe5d6cddc32a78442d.
    It doesn't work yet.

--- etc/plugins/TGuiFactory/P020_TQtRootGuiFactory.C
+++ etc/plugins/TGuiFactory/P020_TQtRootGuiFactory.C
@@ -1,7 +1,5 @@
-TGuiFactory* ROOT_Plugin_TQtRootGuiFactory();
-
 void P020_TQtRootGuiFactory()
 {
    gPluginMgr->AddHandler("TGuiFactory", "qt", "TQtRootGuiFactory",
-      "QtRoot", "::ROOT_Plugin_TQtRootGuiFactory()");
+      "QtRoot", "TQtRootGuiFactory()");
 }
