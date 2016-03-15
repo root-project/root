ffcdfd70cb99322fa999ee7c9afb011e1bc5e6ab
Danilo Piparo <danilo.piparo@cern.ch>
2016-03-15 08:54:02 +0100

    Translate makeImage.C in Python
    to workaround ROOT-8053

---
+++ documentation/doxygen/makeimage.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import ROOT
+import os
+
+def makeimage(MacroName, ImageName, OutDir, cp, py) 
+    '''Generates the ImageName output of the macro MacroName'''
+    if py: exec(MacroName)
+    else: ROOT.gInterpreter.ProcessLine(".x " + MacroName)
+
+    if cp:
+        MN = MacroName.split("(")[0]
+        os.cp("%s","%s/macros", MN, OutDir)
+    
+    canvases = ROOT.gROOT.GetListOfCanvases()
+    for ImageNum,can in enumerate(canvases):
+        can.SaveAs("%s/html/pict%d_%s" %(OutDir,ImageNum,ImageName)
+
+    with open("NumberOfImages.dat","w") as f:
+        f.Write("%d\n" %ImageNum)    
+
+if __name__ == "__main__":
+    from sys import argv
+    makeimage(argv[1], argv[2], argv[3], bool(argv[4]), bool(argv[5]))
+    
