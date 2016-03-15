5e232be819dbf18f7b832620342ee25b8eb5b45e
Olivier Couet <olivier.couet@cern.ch>
2016-03-15 11:00:58 +0100

    Fix a syntax error

--- documentation/doxygen/makeimage.py
+++ documentation/doxygen/makeimage.py
@@ -3,7 +3,7 @@
 import ROOT
 import os
 
-def makeimage(MacroName, ImageName, OutDir, cp, py) 
+def makeimage(MacroName, ImageName, OutDir, cp, py):
     '''Generates the ImageName output of the macro MacroName'''
     if py: exec(MacroName)
     else: ROOT.gInterpreter.ProcessLine(".x " + MacroName)
@@ -11,15 +11,15 @@ def makeimage(MacroName, ImageName, OutDir, cp, py)
     if cp:
         MN = MacroName.split("(")[0]
         os.cp("%s","%s/macros", MN, OutDir)
-    
+
     canvases = ROOT.gROOT.GetListOfCanvases()
     for ImageNum,can in enumerate(canvases):
         can.SaveAs("%s/html/pict%d_%s" %(OutDir,ImageNum,ImageName)
 
-    with open("NumberOfImages.dat","w") as f:
-        f.Write("%d\n" %ImageNum)    
+    with open("NumberOfImages.dat",'w') as f:
+        f.Write("%d\n" %ImageNum)
 
 if __name__ == "__main__":
     from sys import argv
     makeimage(argv[1], argv[2], argv[3], bool(argv[4]), bool(argv[5]))
-    
+
