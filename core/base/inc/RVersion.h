19cce1576cf86afae65ca96deb1f7921641a1ed9
Axel Naumann <Axel.Naumann@cern.ch>
2016-03-17 15:49:14 +0100

    Update ROOT version files to v6.04/16.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.04/15"
-#define ROOT_RELEASE_DATE "Feb  3 2016"
-#define ROOT_RELEASE_TIME "10:21:07"
+#define ROOT_RELEASE "6.04/16"
+#define ROOT_RELEASE_DATE "Mar 17 2016"
+#define ROOT_RELEASE_TIME "15:49:11"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,4,15) /* 394255 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,4,16) /* 394256 */
 
 #endif
