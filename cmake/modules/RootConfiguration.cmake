161cb19d6bd0e8fbee007f101612f57eb3c49eb5
Pere Mato <pere.mato@cern.ch>
2016-03-22 09:56:20 +0100

    Fix for ROOT-7867 - memprobe script is broken when using CMake

--- cmake/modules/RootConfiguration.cmake
+++ cmake/modules/RootConfiguration.cmake
@@ -403,6 +403,10 @@ set(buildeditline ${value${editline}})
 set(cppunit)
 set(dicttype ${ROOT_DICTTYPE})
 
+
+find_program(PERL_EXECUTABLE perl)
+set(perl ${PERL_EXECUTABLE})
+
 #---RConfigure-------------------------------------------------------------------------------------------------
 set(setresuid undef)
 if(mathmore)
