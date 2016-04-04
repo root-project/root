356d6af1f69ea19bf15a858f2cf4cf63bd8c69c9
lawrenceleejr <lawrenceleejr@gmail.com>
2016-04-04 19:15:10 +0200

    moving index-based skimming to within the CopyTree() call and removing python loop

--- main/python/cmdLineUtils.py
+++ main/python/cmdLineUtils.py
@@ -788,24 +788,17 @@ def _copyTreeSubset(sourceFile,sourcePathSplit,destFile,destPathSplit,firstEvent
     retcode = changeDirectory(destFile,destPathSplit)
     if retcode != 0: return retcode
 
-    smallTree = bigTree.CloneTree(0)
     if lastEvent == -1:
         lastEvent = nbrEntries-1
-    isNtuple = bigTree.InheritsFrom(ROOT.TNtuple.Class())
-    for i in range(firstEvent, lastEvent+1):
-        bigTree.GetEntry(i)
-        if isNtuple:
-            super(ROOT.TNtuple,smallTree).Fill()
-        else:
-            smallTree.Fill()
+    numberOfEntries = (lastEvent-firstEvent)+1
 
-    # "Skim" events based on branch values
-    if selectionString:
-        outputTree = smallTree.CopyTree(selectionString)
-    else:
-        outputTree = smallTree
+    # "Skim" events based on branch values using selectionString
+    # as well as selecting a range of events by index
+    outputTree = bigTree.CopyTree(selectionString,"",numberOfEntries,firstEvent)
 
-    # "Slim" tree by removing branches
+    # "Slim" tree by removing branches - 
+    # This is done after the skimming to allow for the user to skim on a 
+    # branch they no longer need to keep
     if branchexclude:
         _setBranchStatus(outputTree,branchexclude,0)
     if branchinclude:
@@ -815,6 +808,7 @@ def _copyTreeSubset(sourceFile,sourcePathSplit,destFile,destPathSplit,firstEvent
 
     outputTree.Write()
     return retcode
+
 
 def _copyTreeSubsets(fileName, pathSplitList, destFile, destPathSplit, first, last, selectionString,
     branchinclude, branchexclude):
