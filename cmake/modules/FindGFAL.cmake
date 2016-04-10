55628355a5e0a77440ecbfb4f24ba08c8dfa17b1
Mattias Ellert <mattias.ellert@fysast.uu.se>
2016-04-10 18:14:58 +0200

    Fix insufficiencies in the cmake dependency search compared to configure
     - Build RFIO using dpm libraries if castor libraries are not available
     - Add additional include and library search paths
     - Add missing glib header path in GFAL module
     - Search also for globus libraries wouthout the flavour in the name
     - Add missing io/hdfs/CMakeLists.txt
     - net/globusauth has no installed headers - remove ROOT_INSTALL_HEADERS()

--- cmake/modules/FindGFAL.cmake
+++ cmake/modules/FindGFAL.cmake
@@ -6,15 +6,18 @@
 #  GFAL_INCLUDE_DIRS (not cached)
 #  GFAL_LIBRARIES
 
-find_path(GFAL_INCLUDE_DIR NAMES gfal_api.h HINTS ${GFAL_DIR}/include $ENV{GFAL_DIR}/include)
+find_path(GFAL_INCLUDE_DIR NAMES gfal_api.h HINTS ${GFAL_DIR}/include $ENV{GFAL_DIR}/include /usr/include/gfal2 /usr/include)
 find_library(GFAL_LIBRARIES NAMES gfal gfal2 HINTS ${GFAL_DIR}/lib $ENV{GFAL_DIR}/lib)
-find_path(SRM_IFCE_INCLUDE_DIR  gfal_srm_ifce_types.h HINTS ${SRM_IFCE_DIR}/include $ENV{SRM_IFCE_DIR}/include)
+find_path(SRM_IFCE_INCLUDE_DIR gfal_srm_ifce_types.h HINTS ${SRM_IFCE_DIR}/include $ENV{SRM_IFCE_DIR}/include /usr/include)
 
-set(GFAL_INCLUDE_DIRS ${GFAL_INCLUDE_DIR} ${SRM_IFCE_INCLUDE_DIR})
+find_package(PkgConfig)
+pkg_check_modules (GLIB2 glib-2.0)
+
+set(GFAL_INCLUDE_DIRS ${GFAL_INCLUDE_DIR} ${SRM_IFCE_INCLUDE_DIR} ${GLIB2_INCLUDE_DIRS})
 
 # handle the QUIETLY and REQUIRED arguments and set GFAL_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GFAL DEFAULT_MSG GFAL_INCLUDE_DIR SRM_IFCE_INCLUDE_DIR GFAL_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GFAL DEFAULT_MSG GFAL_LIBRARIES GFAL_INCLUDE_DIR SRM_IFCE_INCLUDE_DIR GLIB2_INCLUDE_DIRS)
 
 mark_as_advanced(GFAL_FOUND GFAL_INCLUDE_DIR GFAL_LIBRARIES SRM_IFCE_INCLUDE_DIR)
