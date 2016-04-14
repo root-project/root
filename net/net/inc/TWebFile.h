174513adfd1e8dcd5bb79a63c75a4210092b5008
Sergey Linev <S.Linev@gsi.de>
2016-04-14 09:52:44 +0200

    webfile: keep downloaded file content in cache (max 2GB), reuse it in following requests
    Signed-off-by: Sergey Linev <S.Linev@gsi.de>
    Signed-off-by: Philippe Canal <pcanal@fnal.gov>

--- net/net/inc/TWebFile.h
+++ net/net/inc/TWebFile.h
@@ -57,6 +57,8 @@ protected:
    TString           fBasicUrl;         // basic url without authentication and options
    TUrl              fUrlOrg;           // save original url in case of temp redirection
    TString           fBasicUrlOrg;      // save original url in case of temp redirection
+   void             *fFullCache;        //! complete content of the file, some http server may return complete content
+   Long64_t          fFullCacheSize;    //! size of the cached content
 
    static TUrl       fgProxy;           // globally set proxy URL
 
@@ -69,6 +71,7 @@ protected:
    virtual const char *HttpTerminator(const char *start, const char *peeked, Int_t peeklen);
    virtual Int_t       GetFromWeb(char *buf, Int_t len, const TString &msg);
    virtual Int_t       GetFromWeb10(char *buf, Int_t len, const TString &msg, Int_t nseg = 0, Long64_t *seg_pos = 0, Int_t *seg_len = 0);
+   virtual Int_t       GetFromCache(char *buf, Int_t len, Int_t nseg, Long64_t *seg_pos, Int_t *seg_len);
    virtual Bool_t      ReadBuffer10(char *buf, Int_t len);
    virtual Bool_t      ReadBuffers10(char *buf, Long64_t *pos, Int_t *len, Int_t nbuf);
    virtual void        SetMsgReadBuffer10(const char *redirectLocation = 0, Bool_t tempRedirect = kFALSE);
