de9c5bf45a30bbb8c36f508ee7235d02a2909c10
Philippe Canal <pcanal@fnal.gov>
2016-04-25 11:55:31 -0500

    Suppress warning about misleading indentation

--- core/base/src/TROOT.cxx
+++ core/base/src/TROOT.cxx
@@ -1014,7 +1014,8 @@ TObject *TROOT::FindObject(const char *name) const
    while ((obj=next())) {
       temp = obj->FindObject(name);         if (temp) return temp;
    }
-   if (gDirectory) temp = gDirectory->Get(name); if (temp) return temp;
+   if (gDirectory) temp = gDirectory->Get(name);
+   if (temp) return temp;
    if (gPad) {
       TVirtualPad *canvas = gPad->GetVirtCanvas();
       if (fCanvases->FindObject(canvas)) {  //this check in case call from TCanvas ctor
