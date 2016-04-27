3cde51fe9158296d50e1fcaa90998de0326b483f
Vassil Vassilev <vvasilev@cern.ch>
2016-04-27 18:13:41 +0200

    Allow glew.c to be able to include glew.h.

--- graf3d/glew/isystem/OpenGL/gl.h
+++ graf3d/glew/isystem/OpenGL/gl.h
@@ -1,3 +1,3 @@
-#if !defined(ROOT_TGLIncludes) && !__has_feature(modules)
+#if !defined(ROOT_TGLIncludes) && !defined(GLEW_BUILD) && !__has_feature(modules)
 #error "You shouldn't #include gl.h directly. Please use TGLIncludes.h instead."
 #endif
