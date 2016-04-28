d71abe2f25b30c49387d6711e0fb9f14b2f1f747
Vassil Vassilev <vvasilev@cern.ch>
2016-04-28 12:00:17 +0200

    Another try to appease the builds.

--- graf3d/glew/isystem/GL/gl.h
+++ graf3d/glew/isystem/GL/gl.h
@@ -1,5 +1,8 @@
 #if !defined(ROOT_TGLIncludes)
-#if !defined (__building_module) || !__building_module(OpenGL)
-#error "You shouldn't #include gl.h directly. Please use TGLIncludes.h instead."
+#ifndef __building_module
+# define __building_module(X) 0
 #endif
+#  if !__building_module(OpenGL)
+#  error "You shouldn't #include gl.h directly. Please use TGLIncludes.h instead."
+#  endif
 #endif
