a510e3a7812f09b50597bc42741e8f7ad69ac552
Vassil Vassilev <vvasilev@cern.ch>
2016-04-28 10:25:43 +0200

    Spell the conditions correctly. Thanks Bertrand!

--- graf3d/glew/isystem/OpenGL/gl.h
+++ graf3d/glew/isystem/OpenGL/gl.h
@@ -1,3 +1,5 @@
-#if !defined(ROOT_TGLIncludes) && !defined(GLEW_BUILD) && !__has_feature(modules)
+#if !defined(ROOT_TGLIncludes)
+#if !defined (__building_module) || !__building_module(OpenGL)
 #error "You shouldn't #include gl.h directly. Please use TGLIncludes.h instead."
+#endif
 #endif
