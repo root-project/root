770330e8024436be5c9c2c84008ec95bd4aea2f8
Philippe Canal <pcanal@fnal.gov>
2016-05-04 12:25:21 -0500

    Extent warning suppressions to gcc >= v5

--- bindings/pyroot/src/TMemoryRegulator.cxx
+++ bindings/pyroot/src/TMemoryRegulator.cxx
@@ -49,7 +49,7 @@ namespace {
    };
 
 // silence warning about some cast operations
-#if defined(__GNUC__) && __GNUC__ >= 4 && ((__GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ >= 1) || (__GNUC_MINOR__ >= 3)) && !__INTEL_COMPILER
+#if defined(__GNUC__) && (__GNUC__ >= 5 || (__GNUC__ >= 4 && ((__GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ >= 1) || (__GNUC_MINOR__ >= 3)))) && !__INTEL_COMPILER
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
 #endif
 
