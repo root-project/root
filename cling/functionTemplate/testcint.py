84945d5f7e9f75a6b01f4ac89416409eb6cd78d2
Axel Naumann <Axel.Naumann@cern.ch>
2016-05-11 21:12:18 +0200

    Python 3 compat (ROOT-7532).

--- cling/functionTemplate/testcint.py
+++ cling/functionTemplate/testcint.py
@@ -1,16 +1,17 @@
 from ROOT import *
 from sys import stdout
+from __future__ import print_function
 
 def printme(o):
-    print "t now %g %d %d" % (o.get("double")(), o.get("int")(), o.get("float")())
+    print("t now %g %d %d" % (o.get("double")(), o.get("int")(), o.get("float")()))
     stdout.flush() 
 
 #gSystem.Load("libG__t.so")
 gROOT.ProcessLine(".L t.h+")
 sortedMethods = [ item for item in t.__dict__.keys() if item[0:2] != '__' ]
 sortedMethods.sort()
-print "# just a comment"
-print sortedMethods
+print("# just a comment")
+print(sortedMethods)
 stdout.flush() 
 o = t()
 printme(o)
