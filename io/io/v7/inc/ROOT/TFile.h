1d7133b00437133b991d802e8ba9584d18f8d8f1
Axel Naumann <Axel.Naumann@cern.ch>
2016-05-27 16:45:34 +0200

    Simplify TFile; let TFilePtr wrap a shared_ptr; move storage incarnations into source.

--- io/io/v7/inc/ROOT/TFile.h
+++ io/io/v7/inc/ROOT/TFile.h
@@ -5,7 +5,7 @@
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
 
 /*************************************************************************
- * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2016, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -26,109 +26,41 @@
 namespace ROOT {
 namespace Experimental {
 
-namespace Internal {
-/** \class TFileImplBase
- Base class for storage-specific ROOT file implementations.
+class TFilePtr;
 
- A class deriving from `TFileImplBase` is an object store: it can serialize any
+namespace Internal {
+class TFileStorageInterface;
+class TFileSharedPtrCtor;
+}
+
+/** \class TFile
+ A ROOT file.
+
+ A ROOT file is an object store: it can serialize any
  object for which ROOT I/O is available (generally: an object which has a
  dictionary), and it stores the object's data under a key name.
 
  */
-class TFileImplBase: public TDirectory {
-protected:
+class TFile: public TDirectory {
+private:
+  std::unique_ptr<Internal::TFileStorageInterface> fStorage; ///< Storage backend.
+
+  TFile(std::unique_ptr<Internal::TFileStorageInterface>&& storage):
+    fStorage(std::move(storage)) {}
+
   /// Serialize the object at address, using the object's TClass.
   //FIXME: what about `cl` "pointing" to a base class?
-  virtual void WriteMemoryWithType(std::string_view name, const void* address,
-    TClass* cl) = 0;
+  void WriteMemoryWithType(std::string_view name, const void *address,
+                           TClass *cl);
+
+  friend Internal::TFileSharedPtrCtor;
 
 public:
-  /// Must not call Write() of all attached objects:
-  /// some might not be needed to be written or writing might be aborted due to
-  /// an exception; require explicit Write().
-  ~TFileImplBase() = default;
 
-  /// Save all objects associated with this directory (including file header) to
-  /// the storage medium.
-  virtual void Flush() = 0;
-
-  /// Flush() and make the file non-writable: close it.
-  virtual void Close() = 0;
-
-  /// Read the object for a key. `T` must be the object's type.
-  /// This will re-read the object for each call, returning a new copy; whether
-  /// the `TDirectory` is managing an object attached to this key or not.
-  /// \returns a `unique_ptr` to the object.
-  /// \throws TDirectoryUnknownKey if no object is stored under this name.
-  /// \throws TDirectoryTypeMismatch if the object stored under this name is of
-  ///   a type different from `T`.
-  template <class T>
-  std::unique_ptr<T> Read(std::string_view name) {
-    // FIXME: need separate collections for a TDirectory's key/value and registered objects. Here, we want to emit a read and must look through the key/values without attaching an object to the TDirectory.
-    if (const Internal::TDirectoryEntryPtrBase* dep = Find(name.to_string())) {
-      // FIXME: implement upcast!
-      // FIXME: do not register read object in TDirectory
-      // FIXME: implement actual read
-      if (auto depT = dynamic_cast<const Internal::TDirectoryEntryPtr<T>*>(dep)) {
-        //FIXME: for now, copy out of whatever the TDirectory manages.
-        return std::make_unique<T>(*depT->GetPointer());
-      }
-      // FIXME: add expected versus actual type name as c'tor args
-      throw TDirectoryTypeMismatch(name.to_string());
-    }
-    throw TDirectoryUnknownKey(name.to_string());
-    return std::unique_ptr<T>(); // never happens
-  }
-
-
-  /// Write an object that is not lifetime managed by this TFileImplBase.
-  template <class T>
-  void Write(std::string_view name, const T& obj) {
-    WriteMemoryWithType(name, &obj, TClass::GetClass(typeid(T)));
-  }
-
-  /// Write an object that is not lifetime managed by this TFileImplBase.
-  template <class T>
-  void Write(std::string_view name, const T* obj) {
-    WriteMemoryWithType(name, obj, TClass::GetClass(typeid(T)));
-  }
-
-  /// Write an object that is already lifetime managed by this TFileImplBase.
-  void Write(std::string_view name) {
-    const Internal::TDirectoryEntryPtrBase* dep = Find(name.to_string());
-    WriteMemoryWithType(name, dep->GetObjectAddr(), dep->GetType());
-  }
-
-  /// Hand over lifetime management of an object to this TFileImplBase, and
-  /// write it.
-  template <class T>
-  void Write(std::string_view name, std::shared_ptr<T>&& obj) {
-    Add(name, obj);
-    // FIXME: use an iterator from the insertion to write instead of a second name lookup.
-    Write(name);
-  }
-};
-}
-
-
-/**
- \class TFilePtr
- \brief Points to an object that stores or reads objects in ROOT's binary
- format.
-
- FIXME: implement async open; likely using std::future, possibly removing the
- Option_t element.
-
- */
-
-class TFilePtr {
-std::shared_ptr<Internal::TFileImplBase> fImpl;
-
-public:
-  /// Options for TFilePtr construction.
+  /// Options for TFile construction.
   struct Options_t {
     /// Default constructor needed for member inits.
-    Options_t() {}
+    Options_t() { }
 
     /// Whether the file should be opened asynchronously, if available.
     bool fAsynchronousOpen = false;
@@ -151,25 +83,29 @@ public:
   /// Open a file with `name` for reading.
   ///
   /// \note: Synchronizes multi-threaded accesses through locks.
-  static TFilePtr Open(std::string_view name, const Options_t& opts = Options_t());
+  static TFilePtr Open(std::string_view name,
+                       const Options_t &opts = Options_t());
 
   /// Open an existing file with `name` for reading and writing. If a file with
   /// that name does not exist, an invalid TFilePtr will be returned.
   ///
   /// \note: Synchronizes multi-threaded accesses through locks.
-  static TFilePtr OpenForUpdate(std::string_view name, const Options_t& opts = Options_t());
+  static TFilePtr OpenForUpdate(std::string_view name,
+                                const Options_t &opts = Options_t());
 
   /// Open a file with `name` for reading and writing. Fail (return an invalid
   /// `TFilePtr`) if a file with this name already exists.
   ///
   /// \note: Synchronizes multi-threaded accesses through locks.
-  static TFilePtr Create(std::string_view name, const Options_t& opts = Options_t());
+  static TFilePtr Create(std::string_view name,
+                         const Options_t &opts = Options_t());
 
   /// Open a file with `name` for reading and writing. If a file with this name
   /// already exists, delete it and create a new one. Else simply create a new file.
   ///
   /// \note: Synchronizes multi-threaded accesses through locks.
-  static TFilePtr Recreate(std::string_view name, const Options_t& opts = Options_t());
+  static TFilePtr Recreate(std::string_view name,
+                           const Options_t &opts = Options_t());
 
   ///\}
 
@@ -181,18 +117,102 @@ public:
   /// Get the directory used for cached reads.
   static std::string GetCacheDir();
 
-  /// Constructed by Open etc.
-  TFilePtr(std::unique_ptr<Internal::TFileImplBase>&&);
 
+  /// Must not call Write() of all attached objects:
+  /// some might not be needed to be written or writing might be aborted due to
+  /// an exception; require explicit Write().
+  ~TFile() = default;
+
+  /// Save all objects associated with this directory (including file header) to
+  /// the storage medium.
+  void Flush();
+
+  /// Flush() and make the file non-writable: close it.
+  void Close();
+
+  /// Read the object for a key. `T` must be the object's type.
+  /// This will re-read the object for each call, returning a new copy; whether
+  /// the `TDirectory` is managing an object attached to this key or not.
+  /// \returns a `unique_ptr` to the object.
+  /// \throws TDirectoryUnknownKey if no object is stored under this name.
+  /// \throws TDirectoryTypeMismatch if the object stored under this name is of
+  ///   a type different from `T`.
+  template<class T>
+  std::unique_ptr <T> Read(std::string_view name) {
+    // FIXME: need separate collections for a TDirectory's key/value and registered objects. Here, we want to emit a read and must look through the key/values without attaching an object to the TDirectory.
+    if (const Internal::TDirectoryEntryPtrBase *dep = Find(name.to_string())) {
+      // FIXME: implement upcast!
+      // FIXME: do not register read object in TDirectory
+      // FIXME: implement actual read
+      if (auto depT = dynamic_cast<const Internal::TDirectoryEntryPtr <T> *>(dep)) {
+        //FIXME: for now, copy out of whatever the TDirectory manages.
+        return std::make_unique<T>(*depT->GetPointer());
+      }
+      // FIXME: add expected versus actual type name as c'tor args
+      throw TDirectoryTypeMismatch(name.to_string());
+    }
+    throw TDirectoryUnknownKey(name.to_string());
+    return std::unique_ptr<T>(); // never happens
+  }
+
+
+  /// Write an object that is not lifetime managed by this TFileImplBase.
+  template<class T>
+  void Write(std::string_view name, const T &obj) {
+    WriteMemoryWithType(name, &obj, TClass::GetClass(typeid(T)));
+  }
+
+  /// Write an object that is not lifetime managed by this TFileImplBase.
+  template<class T>
+  void Write(std::string_view name, const T *obj) {
+    WriteMemoryWithType(name, obj, TClass::GetClass(typeid(T)));
+  }
+
+  /// Write an object that is already lifetime managed by this TFileImplBase.
+  void Write(std::string_view name) {
+    const Internal::TDirectoryEntryPtrBase *dep = Find(name.to_string());
+    WriteMemoryWithType(name, dep->GetObjectAddr(), dep->GetType());
+  }
+
+  /// Hand over lifetime management of an object to this TFileImplBase, and
+  /// write it.
+  template<class T>
+  void Write(std::string_view name, std::shared_ptr <T> &&obj) {
+    Add(name, obj);
+    // FIXME: use an iterator from the insertion to write instead of a second name lookup.
+    Write(name);
+  }
+};
+
+/**
+ \class TFilePtr
+ \brief Points to an object that stores or reads objects in ROOT's binary
+ format.
+
+ FIXME: implement async open; likely using std::future, possibly removing the
+ Option_t element.
+
+ */
+
+class TFilePtr {
+private:
+  std::shared_ptr<TFile> fFile;
+
+  /// Constructed by Open etc.
+  TFilePtr(std::shared_ptr<TFile>&&);
+
+  friend class TFile;
+
+public:
   /// Dereference the file pointer, giving access to the TFileImplBase object.
-  Internal::TFileImplBase* operator ->() { return fImpl.get(); }
+  TFile* operator ->() { return fFile.get(); }
 
   /// Dereference the file pointer, giving access to the TFileImplBase object.
   /// const overload.
-  const Internal::TFileImplBase* operator ->() const { return fImpl.get(); }
+  const TFile* operator ->() const { return fFile.get(); }
 
   /// Check the validity of the file pointer.
-  operator bool() const { return fImpl.get(); }
+  operator bool() const { return fFile.get(); }
 };
 
 } // namespace Experimental
--- io/io/v7/src/TFile.cxx
+++ io/io/v7/src/TFile.cxx
@@ -5,7 +5,7 @@
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
 
 /*************************************************************************
- * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2016, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -24,15 +24,41 @@ ROOT::Experimental::TDirectory& ROOT::Experimental::TDirectory::Heap() {
   return heapDir;
 }
 
+namespace ROOT {
+namespace Experimental {
+namespace Internal {
+// This will have to move to some "semi-internal" header.
+/** \class TFileStorageInterface
+ Base class for TFile storage backends.
+ */
+class TFileStorageInterface {
+public:
+  virtual void Flush() = 0;
+  virtual void Close() = 0;
+  virtual ~TFileStorageInterface() = default;
+  virtual void WriteMemoryWithType(std::string_view name, const void *address,
+                                   TClass *cl) = 0;
+};
+
+class TFileSharedPtrCtor: public ROOT::Experimental::TFile {
+public:
+  TFileSharedPtrCtor(std::unique_ptr<TFileStorageInterface>&& storage):
+    TFile(std::move(storage)) {}
+};
+} // namespace Internal
+} // namespace Experimenal
+} // namespace ROOT
+
+
 namespace {
 /// We cannot afford users not closing their files. Yes, we return a unique_ptr -
 /// but that might be stored in an object that itself leaks. That would leave
 /// the TFile unclosed and data corrupted / not written. Instead, keep a
 /// collection of all opened writable TFiles and close them at destruction time,
 /// explicitly.
-static void AddFilesToClose(std::weak_ptr<ROOT::Experimental::Internal::TFileImplBase> pFile) {
+static void AddFilesToClose(std::weak_ptr<ROOT::Experimental::TFile> pFile) {
   struct CloseFiles_t {
-    std::vector<std::weak_ptr<ROOT::Experimental::Internal::TFileImplBase>> fFiles;
+    std::vector<std::weak_ptr<ROOT::Experimental::TFile>> fFiles;
     std::mutex fMutex;
     ~CloseFiles_t() {
       for (auto& wFile: fFiles) {
@@ -48,14 +74,15 @@ static void AddFilesToClose(std::weak_ptr<ROOT::Experimental::Internal::TFileImp
   closer.fFiles.emplace_back(pFile);
 }
 
-/** \class TFSFile
- TFileImplBase for a file-system (POSIX) style TFile.
+
+/** \class TV6Storage
+ TFile for a ROOT v6 storage backend.
  */
-class TFileSystemFile: public ROOT::Experimental::Internal::TFileImplBase {
+class TV6Storage: public ROOT::Experimental::Internal::TFileStorageInterface {
   ::TFile* fOldFile;
 
 public:
-  TFileSystemFile(const std::string& name, const std::string& mode):
+  TV6Storage(const std::string& name, const std::string& mode):
     fOldFile(::TFile::Open(name.c_str(), mode.c_str())) {
   }
 
@@ -63,7 +90,7 @@ public:
 
   void Close() final { fOldFile->Close(); }
 
-  ~TFileSystemFile() {
+  ~TV6Storage() {
     delete fOldFile;
   }
 
@@ -73,15 +100,15 @@ public:
 };
 }
 
-ROOT::Experimental::TFilePtr::TFilePtr(std::unique_ptr<ROOT::Experimental::Internal::TFileImplBase>&& impl):
-fImpl(std::move(impl))
+ROOT::Experimental::TFilePtr::TFilePtr(std::shared_ptr<ROOT::Experimental::TFile>&& file):
+fFile(std::move(file))
 {
-  AddFilesToClose(fImpl);
+  AddFilesToClose(fFile);
 }
 
 namespace {
 static std::string GetV6TFileOpts(const char* mode,
-                                  const ROOT::Experimental::TFilePtr::Options_t& opts) {
+                                  const ROOT::Experimental::TFile::Options_t& opts) {
   std::string ret(mode);
   if (opts.fCachedRead)
     ret += " CACHEREAD ";
@@ -95,9 +122,9 @@ static std::mutex& GetCacheDirMutex() {
   return sMutex;
 }
 
-static ROOT::Experimental::TFilePtr
+static std::unique_ptr<ROOT::Experimental::Internal::TFileStorageInterface>
 OpenV6TFile(std::string_view name, const char* mode,
-            const ROOT::Experimental::TFilePtr::Options_t& opts) {
+            const ROOT::Experimental::TFile::Options_t& opts) {
   // Set and re-set the cache dir.
   // FIXME: do not modify a static here, pass this to the underlying Open.
   struct SetCacheDirRAII_t {
@@ -115,48 +142,66 @@ OpenV6TFile(std::string_view name, const char* mode,
     }
   } setCacheDirRAII(opts.fCachedRead);
 
-  std::unique_ptr<TFileSystemFile> fsf
-    = std::make_unique<TFileSystemFile>(name.to_string(), GetV6TFileOpts(mode, opts));
-  return ROOT::Experimental::TFilePtr(std::move(fsf));
+  auto v6storage = std::make_unique<TV6Storage>(name.to_string(), GetV6TFileOpts(mode, opts));
+
+  using namespace ROOT::Experimental::Internal;
+  return std::unique_ptr<TFileStorageInterface>{std::move(v6storage)};
 }
 }
 
 ROOT::Experimental::TFilePtr
-ROOT::Experimental::TFilePtr::Open(std::string_view name,
-                                   const Options_t& opts /*= Options_t()*/) {
+ROOT::Experimental::TFile::Open(std::string_view name,
+                                const Options_t& opts /*= Options_t()*/) {
   // will become delegation to TFileSystemFile, TWebFile etc.
-  return OpenV6TFile(name, "READ", opts);
+  using namespace Internal;
+  auto file = std::make_shared<TFileSharedPtrCtor>(OpenV6TFile(name, "READ", opts));
+  return ROOT::Experimental::TFilePtr(std::move(file));
 }
 
 ROOT::Experimental::TFilePtr
-ROOT::Experimental::TFilePtr::Create(std::string_view name,
-                                     const Options_t& opts /*= Options_t()*/) {
+ROOT::Experimental::TFile::Create(std::string_view name,
+                                  const Options_t& opts /*= Options_t()*/) {
   // will become delegation to TFileSystemFile, TWebFile etc.
-  return OpenV6TFile(name, "CREATE", opts);
+  using namespace Internal;
+  auto file = std::make_shared<TFileSharedPtrCtor>(OpenV6TFile(name, "CREATE", opts));
+  return ROOT::Experimental::TFilePtr(std::move(file));
 }
 
 ROOT::Experimental::TFilePtr
-ROOT::Experimental::TFilePtr::Recreate(std::string_view name,
-                                       const Options_t& opts /*= Options_t()*/) {
+ROOT::Experimental::TFile::Recreate(std::string_view name,
+                                    const Options_t& opts /*= Options_t()*/) {
   // will become delegation to TFileSystemFile, TWebFile etc.
-  return OpenV6TFile(name, "RECREATE", opts);
+  using namespace Internal;
+  auto file = std::make_shared<TFileSharedPtrCtor>(OpenV6TFile(name, "RECREATE", opts));
+  return ROOT::Experimental::TFilePtr(std::move(file));
 }
 
 ROOT::Experimental::TFilePtr
-ROOT::Experimental::TFilePtr::OpenForUpdate(std::string_view name,
-                                            const Options_t& opts /*= Options_t()*/) {
+ROOT::Experimental::TFile::OpenForUpdate(std::string_view name,
+                                         const Options_t& opts /*= Options_t()*/) {
   // will become delegation to TFileSystemFile, TWebFile etc.
-  return OpenV6TFile(name, "UPDATE", opts);
+  using namespace Internal;
+  auto file = std::make_shared<TFileSharedPtrCtor>(OpenV6TFile(name, "UPDATE", opts));
+  return ROOT::Experimental::TFilePtr(std::move(file));
 }
 
-std::string ROOT::Experimental::TFilePtr::SetCacheDir(std::string_view path) {
+std::string ROOT::Experimental::TFile::SetCacheDir(std::string_view path) {
   std::lock_guard<std::mutex> lock(GetCacheDirMutex());
 
-  std::string ret = TFile::GetCacheFileDir();
-  TFile::SetCacheFileDir(path.to_string().c_str());
+  std::string ret = ::TFile::GetCacheFileDir();
+  ::TFile::SetCacheFileDir(path.to_string().c_str());
   return ret;
 }
 
-std::string ROOT::Experimental::TFilePtr::GetCacheDir() {
-  return TFile::GetCacheFileDir();
+std::string ROOT::Experimental::TFile::GetCacheDir() {
+  std::lock_guard<std::mutex> lock(GetCacheDirMutex());
+  return ::TFile::GetCacheFileDir();
+}
+
+void ROOT::Experimental::TFile::Flush() { fStorage->Flush(); }
+void ROOT::Experimental::TFile::Close() { fStorage->Close(); }
+void ROOT::Experimental::TFile::WriteMemoryWithType(std::string_view name,
+                                                    const void *address,
+                                                    TClass *cl) {
+  fStorage->WriteMemoryWithType(name, address, cl);
 }
