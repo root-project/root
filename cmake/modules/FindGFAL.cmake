7c9048129b9cbd31db8cdcd9b043547c5e2579cb
Pere Mato <pere.mato@cern.ch>
2016-06-17 14:01:46 +0200

    Fixes to deal with  gfal version 1 and 2

--- cmake/modules/FindGFAL.cmake
+++ cmake/modules/FindGFAL.cmake
@@ -7,22 +7,27 @@
 #  GFAL_LIBRARIES (not cached)
 
 find_path(GFAL_INCLUDE_DIR NAMES gfal_api.h
-          PATH_SUFFIXES gfal2 gfal 
+          PATH_SUFFIXES . gfal gfal2
           HINTS ${GFAL_DIR}/include $ENV{GFAL_DIR}/include)
-find_library(GFAL_LIBRARY NAMES gfal gfal2 
+find_library(GFAL_LIBRARY NAMES gfal gfal2
              HINTS ${GFAL_DIR}/lib $ENV{GFAL_DIR}/lib)
 find_path(SRM_IFCE_INCLUDE_DIR  gfal_srm_ifce_types.h 
           HINTS ${SRM_IFCE_DIR}/include $ENV{SRM_IFCE_DIR}/include)
-find_path(GLIB_INCLUDE_DIR NAMES glib.h 
-          PATH_SUFFIXES glib-2.0 glib)
+
+if(GFAL_LIBRARY MATCHES gfal2)
+  find_path(GLIB_INCLUDE_DIR NAMES glib.h PATH_SUFFIXES glib-2.0 glib)
+  if(GLIB_INCLUDE_DIR)
+    list(APPEND GFAL_INCLUDE_DIRS ${GLIB_INCLUDE_DIR})
+  endif()
+endif()
 
 set(GFAL_LIBRARIES ${GFAL_LIBRARY})
-set(GFAL_INCLUDE_DIRS ${GFAL_INCLUDE_DIR} ${SRM_IFCE_INCLUDE_DIR} ${GLIB_INCLUDE_DIR})
+set(GFAL_INCLUDE_DIRS ${GFAL_INCLUDE_DIR} ${SRM_IFCE_INCLUDE_DIR})
+
 
 # handle the QUIETLY and REQUIRED arguments and set GFAL_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GFAL DEFAULT_MSG GFAL_INCLUDE_DIR 
-    SRM_IFCE_INCLUDE_DIR GLIB_INCLUDE_DIR GFAL_LIBRARY)
+find_package_handle_standard_args(GFAL DEFAULT_MSG GFAL_INCLUDE_DIR SRM_IFCE_INCLUDE_DIR GFAL_LIBRARY)
 
 mark_as_advanced(GFAL_FOUND GFAL_INCLUDE_DIR GFAL_LIBRARIES SRM_IFCE_INCLUDE_DIR GLIB_INCLUDE_DIR)
