dda8955eb657379ad265d971a7b308c5d5d39e67
Pere Mato <pere.mato@cern.ch>
2016-06-28 16:52:02 +0200

    Fix problem reported by gertsen - https://root.cern.ch/phpBB3/viewtopic.php?f=3&t=21919

--- etc/plugins/TSQLServer/P020_TPgSQLServer.C
+++ etc/plugins/TSQLServer/P020_TPgSQLServer.C
@@ -1,5 +1,5 @@
 void P020_TPgSQLServer()
 {
    gPluginMgr->AddHandler("TSQLServer", "^pgsql:", "TPgSQLServer",
-      "PgSQL", "TPgSQLServer(const char*,const char*,const char*)");
+      "RPgSQL", "TPgSQLServer(const char*,const char*,const char*)");
 }
