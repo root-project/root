57fb9f684a2e088f09067b5185754b840ddb5906
Patrick Bos <egpbos@gmail.com>
2016-07-04 14:04:11 +0200

    Added RooTestStatMPDriver.h to CMakeLists.

--- roofit/roofitcore/src/RooTestStatMPDriver.cxx
+++ roofit/roofitcore/src/RooTestStatMPDriver.cxx
@@ -4,10 +4,14 @@
  * @(#)root/roofitcore:$Id$
  * Authors:                                                                  *
  *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
- *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *   DK, David Kirkby,    UC Irvine,        dkirkby@uci.edu                  *
+ *   PB, Patrick Bos,     Netherlands eScience Center,                       *
+ *                                          p.bos@esciencecenter.nl          *
  *                                                                           *
  * Copyright (c) 2000-2005, Regents of the University of California          *
- *                          and Stanford University. All rights reserved.    *
+ *                          and Stanford University.                         *
+ *               2016,      Netherlands eScience Center.                     *
+ *                          All rights reserved.                             *
  *                                                                           *
  * Redistribution and use in source and binary forms,                        *
  * with or without modification, are permitted according to the terms        *
