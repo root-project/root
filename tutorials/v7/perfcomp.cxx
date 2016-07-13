be281252d41c9ef3cf5c0d98db84d81ae8ed5e1b
Olivier Couet <olivier.couet@cern.ch>
2016-07-13 16:35:04 +0200

    New doxygen group for v7 tutorials

--- tutorials/v7/perfcomp.cxx
+++ tutorials/v7/perfcomp.cxx
@@ -1,8 +1,11 @@
-/// \file perfcomp.cxx
-/// \ingroup Tutorials
-/// \author Axel Naumann <axel@cern.ch>
+/// \file
+/// \ingroup tutorial_v7
+///
+/// \macro_code
+///
 /// \date 2015-07-08
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \author Axel Naumann <axel@cern.ch>
 
 /*************************************************************************
  * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
