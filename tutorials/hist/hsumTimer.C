1c0e9b231730d1bbd802a239d6dfae9e527ab8e0
pmiquelm <paumm2006@yahoo.es>
2016-07-21 10:47:21 +0200

    Changed variable name

--- tutorials/hist/hsumTimer.C
+++ tutorials/hist/hsumTimer.C
@@ -13,14 +13,14 @@
 ///
 /// \author Rene Brun
 
-Float_t ratioratio;
+Float_t progressRatio;
 TSlider *slider;
 TCanvas *c1;
 
 void hsumUpdate()
 {
 // called when Timer times out
-   if (slider) slider->SetRange(0, ::ratioratio);
+   if (slider) slider->SetRange(0, ::progressRatio);
    c1->Modified();
    c1->Update();
 }
@@ -59,7 +59,7 @@ void hsumTimer(Int_t nfill=100000)
   Float_t xs1, xs2, xmain;
   gRandom->SetSeed();
   for (Int_t i=0; i<nfill; i++) {
-     ::ratioratio = Float_t(i)/Float_t(nfill);
+     ::progressRatio = Float_t(i)/Float_t(nfill);
      if (gSystem->ProcessEvents()) break;
      xmain = gRandom->Gaus(-1,1.5);
      xs1   = gRandom->Gaus(-0.5,0.5);
