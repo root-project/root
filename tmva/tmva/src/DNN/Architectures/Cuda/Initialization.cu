80f1cdde957e9dbfaf8c6c88a18f3b62d7c98547
Simon Pfreundschuh <simonpf@chalmers.se>
2016-08-15 08:12:42 +0200

    Some more fixed in MethoDNN.

--- tmva/tmva/src/DNN/Architectures/Cuda/Initialization.cu
+++ tmva/tmva/src/DNN/Architectures/Cuda/Initialization.cu
@@ -91,7 +91,7 @@ void TCuda::InitializeZero(TCudaMatrix & A)
    n = A.GetNcols();
    TMatrixT<CudaDouble_t> B(m, n);
 
-   for (size_t i = 0; i < m * n; i++) {
+   for (size_t i = 0; i < m; i++) {
       for (size_t j = 0; j < n ; j++) {
          B(i,j) = 0.0;
       }
