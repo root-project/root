a9d62e81d84e135d1b4daf66954e26231938b9db
Simon Pfreundschuh <simonpf@chalmers.se>
2016-08-17 11:43:02 +0200

    Optimized sum columns implementation.

--- tmva/tmva/src/DNN/Architectures/Cuda/Kernels.cu
+++ tmva/tmva/src/DNN/Architectures/Cuda/Kernels.cu
@@ -611,7 +611,7 @@ __global__ void InitializeCurandStates(unsigned long long seed,
    int i   = blockDim.y * blockIdx.y + threadIdx.y;
    int j   = blockDim.x * blockIdx.x + threadIdx.x;
    int tid = i * gridDim.x + j;
-   curand_init(seed, 0, tid, state + tid);
+   curand_init(seed + tid, 0, tid, state + tid);
 }
 
 //____________________________________________________________________________
