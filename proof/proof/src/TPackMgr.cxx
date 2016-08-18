9c747fa7ec7e9f9127916bd0f95af4b4fdfa1c49
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2016-08-18 13:10:13 +0200

    proof: fix resource leak found by Coverity

--- proof/proof/src/TPackMgr.cxx
+++ proof/proof/src/TPackMgr.cxx
@@ -848,6 +848,7 @@ Int_t TPackMgr::Install(const char *par, Bool_t rmold)
          if (*sums != *md5) install = kTRUE;
       }
    }
+   if (sums) delete sums;
 
    // Install if required
    if (install) {
@@ -858,7 +859,6 @@ Int_t TPackMgr::Install(const char *par, Bool_t rmold)
    }
    md5d = TMD5::FileChecksum(dest);
 
-   if (sums) delete sums;
    if (md5 && *md5 != *md5d)
       Warning("Install", "checksums do not match:\n\tdownloaded:\t%s\n\texpected:\t%s",
                          md5d->AsString(), md5->AsString());
