2fad7a78ce1acbe787d1e7ed39a282498dc78638
lgiommi <luca.giommi@cern.ch>
2016-08-24 00:21:36 +0200

    Add the class TSimpleAnalysis

--- tree/tree/inc/TSimpleAnalysis.h
+++ tree/tree/inc/TSimpleAnalysis.h
@@ -1,37 +1,78 @@
+// @(#)root/tree:$Id$
+// Author: Luca Giommi   22/08/16
 
-#ifndef ROOT_TSimpleAnalysis
-#define ROOT_TSimpleAnalysis
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
 
+#ifndef ROOT_TSimpleAnalysis2
+#define ROOT_TSimpleAnalysis2
 
+/** \class TSympleAnalysis
+A TSimpleAnalysis object permit to, given an input file or through
+command line, create an .root file in which are saved histograms
+that the user want to create.
+*/
+
+#ifndef ROOT_TFile
 #include "TFile.h"
+#endif
+
+#ifndef ROOT_TCanvas
 #include "TCanvas.h"
-#include <vector>
+#endif
+
+#ifndef ROOT_TChain
 #include "TChain.h"
+#endif
+
+#ifndef ROOT_TH1
 #include "TH1.h"
+#endif
+
+#ifndef ROOT_TError
+#include "TError.h"
+#endif
+
 #include <string>
 #include <iostream>
 #include <fstream>
-#include "TError.h"
+#include <vector>
 
 
-class TSimpleAnalysis {
-public:
 
-   std::string fInputName;
-   std::vector<std::string> fInputFiles;
-   std::vector<std::string> fExpressions;
-   std::vector<std::string> fHNames;
-   std::vector<std::string> fCut;
-   std::string fOutputFile;
-   std::string fTreeName;
-   ifstream in;
+
+class TSimpleAnalysis2 {
+private:
+
+   std::string fInputName;   ///< Name of the input file
+   std::vector<std::string> fInputFiles;   ///< .root input files
+   std::vector<std::string> fExpressions;   ///< What is showed by the histograms inside the output file
+   std::vector<std::string> fHNames;   ///< Names of the histograms
+   std::vector<std::string> fCut;   ///< Cuts added to the hisograms
+   std::string fOutputFile;   ///< Output file in which are stored the histograms
+   std::string fTreeName;   ///< Name of the input tree
+   ifstream in;   ///< Stream for the input file
+
+   //the elements of the enum refer to the different kinds of elements that are
+   //in the input file
+
    enum EReadingWhat {kReadingOutput,kReadingInput,kReadingTreeName,
                       kReadingExpressions,kEndOfFile};
-   const std::string kCutIntr="if";
-   Int_t fCounter=0;
+   static const std::string kCutIntr;   ///< The string that represents the starter point of the cut expresson
+   Int_t fCounter=0;   ///< Counter usefull for the reading file
 
+public:
+   ////////////////////////////////////////////////////////////////////////////////
+   ///Constructor for the case with the input file
+   ///
+   /// \param[in] kFile name of the input file that has to be read
 
-   TSimpleAnalysis(const std::string& kFile) {
+   TSimpleAnalysis2(const std::string& kFile) {
       fInputName = kFile;
       in.open(kFile);
       if(!in) {
@@ -40,7 +81,15 @@ public:
       }
    }
 
-   TSimpleAnalysis(const std::string& output, std::vector<std::string> inputFiles,
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Constructor for the case of command line parsing arguments
+   ///
+   /// \param[in] output name of the output file
+   /// \param[in] inputFiles name of the input .root files
+   /// \param[in] name name of the tree
+   /// \param[in] expressions what is showed in the histograms
+
+   TSimpleAnalysis2(const std::string& output, std::vector<std::string> inputFiles,
                  const std::string& name, std::vector<std::string> expressions) {
       cout<<"sono entrato nel costruttore parametrico"<<endl;
       fOutputFile=output;
@@ -49,7 +98,7 @@ public:
          fInputFiles.push_back(input);
       for (std::string expr: expressions) {
          cout<<expr<<endl;
-         std::size_t equal=expr.find("="); // what if no '='
+         std::size_t equal=expr.find("=");
          if (equal == std::string::npos) {
             ::Error("TSimpleAnalysis",
                     "Missing '=' in fExpressions in %s",expr.c_str());
@@ -80,7 +129,15 @@ public:
       }
    }
 
-   virtual ~TSimpleAnalysis(){}
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Virtual default denstructor
+
+   virtual ~TSimpleAnalysis2(){}
+
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Tell us if the name of some histograms that will be created are the same
+   ///
+   /// param[in] name name of the histograms
 
    void CheckHNames(std::vector<std::string> name) {
       Int_t err=0;
@@ -97,6 +154,8 @@ public:
          throw 1;
    }
 
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Return true if the input arguments create the output file correctly
 
    Bool_t Analysis() {
       cout<<"entro nella funzione"<<endl;
@@ -120,13 +179,23 @@ public:
       return true;
    }
 
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Return false if not a tree name
+   ///
+   /// param[in] line line read from the input file
 
-   // Return false if not a tree name
    Bool_t HandleTreeNameConfig(const std::string& line) {
       if (line.find("=") != std::string::npos)
          return false;
       else return true;
    }
+
+   ////////////////////////////////////////////////////////////////////////////////
+   /// It handles the expression lines of the input file in order to pass the
+   /// elements to the members of the object
+   ///
+   /// param[in] line line read from the input file
+   /// param[in] numbLine number of the input file line
 
    void HandlefExpressionConfig(std::string& line, int& numbLine) {
       std::size_t equal=line.find("="); // what if no '='
@@ -159,10 +228,21 @@ public:
       }
    }
 
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Delete white spaces in a string
+   ///
+   /// param[in] line line read from the input file
+
    Bool_t DeleteSpaces (std::string& line) {
       line.erase(std::remove(line.begin(),line.end(),' '),line.end());
       return 1;
    }
+
+   ////////////////////////////////////////////////////////////////////////////////
+   /// Skip subsequent empty lines in a string and returns the number of the
+   /// current line of the input file
+   ///
+   /// param[in] numbLine number of the input file line
 
    std::string SkipSubsequentEmptyLines(Int_t& numbLine) {
       std::string notEmptyLine;
@@ -175,6 +255,13 @@ public:
       return notEmptyLine;
    }
 
+   ////////////////////////////////////////////////////////////////////////////////
+   /// After the eventual skip of subsequent empty lines, returns true if the
+   /// line is a comment
+   ///
+   /// param[in] line line read from the input file
+   /// param[in] readingSection current section of the read file
+   /// param[in] numbLine number of the input file line
 
    Bool_t HandleLines(std::string& line, Int_t& readingSection, Int_t& numbLine) {
 
@@ -204,7 +291,8 @@ public:
 
    }
 
-
+   ////////////////////////////////////////////////////////////////////////////////
+   /// This function has the aim of setting the arguments read from the input file
 
    void Settings() {
 
@@ -250,17 +338,12 @@ public:
          case kEndOfFile: break;
          }
       }
-
-
-      //      if (readingSection != kEndOfFile)
-      // Houston-we-have-a-problem!
-      //checkSettings(); // do we have a file name? do we have an expression?
-
    }
 
-
-ClassDef(TSimpleAnalysis,0)
+   ClassDef(TSimpleAnalysis,0)
 
 };
+
+const std::string TSimpleAnalysis2::kCutIntr="if";
 
 #endif
