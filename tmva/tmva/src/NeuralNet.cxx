9f1dce56790d7a5a32a3cf58e97ced067da45768
Aditya Sharma <adi-sharma@users.noreply.github.com>
2016-08-25 21:02:58 +0530

    Successful build NeuralNet.cxx

--- tmva/tmva/src/NeuralNet.cxx
+++ tmva/tmva/src/NeuralNet.cxx
@@ -8,6 +8,17 @@ namespace TMVA
     namespace DNN
     {
 
+          int hasherFunction(int a)
+              {
+                 a = (a+0x7ed55d16) + (a<<12);
+                 a = (a^0xc761c23c) ^ (a>>19);
+                 a = (a+0x165667b1) + (a<<5);
+                 a = (a+0xd3a2646c) ^ (a<<9);
+                 a = (a+0xfd7046c5) + (a<<3);
+                 a = (a^0xb55a4f09) ^ (a>>16);
+                 return a;
+              }
+
 
 
         double gaussDouble (double mean, double sigma)
