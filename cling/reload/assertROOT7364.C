be41114c35998f8e239070b8a045f4082468f819
Axel Naumann <Axel.Naumann@cern.ch>
2016-08-26 16:11:50 +0200

    Make this a test of ROOT-8325.

--- cling/reload/assertROOT7364.C
+++ cling/reload/assertROOT7364.C
@@ -1,5 +1,5 @@
 int assertROOT7364() {
-  gROOT->ProcessLine("#include <map>");
+  gROOT->ProcessLine("#include <map>\n\n");
   gROOT->ProcessLine("std::map<int,std::string> p;");
   gROOT->ProcessLine("#include <string>");
   return 0;
