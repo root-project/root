929d19f09c4c36954c56ad3fe2b3fdad20703b7e
Danilo Piparo <danilo.piparo@cern.ch>
2016-08-26 16:53:45 +0200

    Adjust code to coding conventions.

--- tutorials/fit/line3Dfit.C
+++ tutorials/fit/line3Dfit.C
@@ -46,26 +46,25 @@ bool first = true;
 // function Object to be minimized
 struct SumDistance2 {
    // the TGraph is a data member of the object
-   TGraph2D * fGraph;
+   TGraph2D *fGraph;
 
-   SumDistance2(TGraph2D * g) : fGraph(g) {}
+   SumDistance2(TGraph2D *g) : fGraph(g) {}
 
-    // calculate distance line-point
-    double distance2(double x,double y,double z, const double *p) {
-    // distance line point is D= | (xp-x0) cross  ux |
-    // where ux is direction of line and x0 is a point in the line (like t = 0)
-    XYZVector xp(x,y,z);
-    XYZVector x0(p[0], p[2], 0. );
-    XYZVector x1(p[0] + p[1], p[2] + p[3], 1. );
-    XYZVector u = (x1-x0).Unit();
-    double d2 = ((xp-x0).Cross(u)) .Mag2();
-    return d2;
-    }
+   // calculate distance line-point
+   double distance2(double x,double y,double z, const double *p) {
+      // distance line point is D= | (xp-x0) cross  ux |
+      // where ux is direction of line and x0 is a point in the line (like t = 0)
+      XYZVector xp(x,y,z);
+      XYZVector x0(p[0], p[2], 0. );
+      XYZVector x1(p[0] + p[1], p[2] + p[3], 1. );
+      XYZVector u = (x1-x0).Unit();
+      double d2 = ((xp-x0).Cross(u)).Mag2();
+      return d2;
+   }
 
    // implementation of the function to be minimized
-   double operator() (const double * par) {
-
-      assert(fGraph    != 0);
+   double operator() (const double *par) {
+      assert(fGraph != 0);
       double * x = fGraph->GetX();
       double * y = fGraph->GetY();
       double * z = fGraph->GetZ();
@@ -75,8 +74,9 @@ struct SumDistance2 {
          double d = distance2(x[i],y[i],z[i],par);
          sum += d;
       }
-      if (first)
+      if (first) {
          std::cout << "Total Initial distance square = " << sum << std::endl;
+      }
       first = false;
       return sum;
    }
@@ -108,7 +108,7 @@ Int_t line3Dfit()
       double t = gRandom->Uniform(0,10);
       line(t,p0,x,y,z);
       double err = 1;
-    // do a gaussian smearing around the points in all coordinates
+      // do a gaussian smearing around the points in all coordinates
       x += gRandom->Gaus(0,err);
       y += gRandom->Gaus(0,err);
       z += gRandom->Gaus(0,err);
@@ -145,7 +145,6 @@ Int_t line3Dfit()
 
    // get fit parameters
    const double * parFit = result.GetParams();
-
 
    // draw the fitted line
    int n = 1000;
