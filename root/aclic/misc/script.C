cd3fd5aa37a7f3bdcfbdc7610cd4d36a9d9c1d1f
Philippe Canal <pcanal@fnal.gov>
2016-08-29 13:05:47 -0500

    Actually test AddIncludePath with a relative path ...

--- root/aclic/misc/script.C
+++ root/aclic/misc/script.C
@@ -3,9 +3,11 @@
 #include "nest/nested.h"
 #include "nested_dir1.h"
 #include "nested_dir2.h"
+#include "tempHeader.h" // in temp/subdir3/tempHeader.h
 
 int function() {
    defined_in_dir1_nested();
    defined_in_dir2_nested();
+   defined_in_temp_subdir3();
    return defined_in_nested();
 }
