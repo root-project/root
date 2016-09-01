761766bfec6f5810801ec8e0e0908310894819da
Sergey Linev <S.Linev@gsi.de>
2016-09-01 11:29:52 +0200

    add test of json with TPolyMarker3D
    Signed-off-by: Philippe Canal <pcanal@fnal.gov>

---
+++ root/io/json/runPolyMarker.C
@@ -0,0 +1,17 @@
+#include "TPolyMarker3D.h"
+#include "TBufferJSON.h"
+#include "TString.h"
+
+void runPolyMarker()
+{
+   // Check creation of JSON for object, where TObject is not first parent
+   // Also verify instrumented custom streamer of TPolyMarker3D
+
+   TPolyMarker3D* marker = new TPolyMarker3D(10);
+   for (Int_t n=0;n<10;++n)
+      marker->SetPoint(n, n*2, n*3, n*4);
+
+   TString json = TBufferJSON::ConvertToJSON(marker);
+
+   cout << json << endl;
+}
