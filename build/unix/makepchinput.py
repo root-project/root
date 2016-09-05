e4c7653302fa888305fc68fbc2453cfae0dc8a83
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2016-09-05 13:21:52 +0200

    build/unix: remove trailing blank
    Introduced in last commit, was breaking builds on Ubuntu 16.04 .

--- build/unix/makepchinput.py
+++ build/unix/makepchinput.py
@@ -48,7 +48,7 @@ def getParams():
    #print (', '.join(cxxflags))
    cxxflagsNoW = [flag for flag in cxxflags if (flag[0:2] != '-W' and flag[0:3] != '-wd' and \
                                                 flag[0:2] != '-x' and flag[0:3] != '-ax' and \
-                                                flag[0:2] != '-O' and flag[0:5] != '-arch') \ 
+                                                flag[0:2] != '-O' and flag[0:5] != '-arch') \
                                                 or flag[0:4] == '-Wno']
    #print (', '.join(cxxflagsNoW))
 
