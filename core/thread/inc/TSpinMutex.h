91698ffdf0710495e867d1de0c3dfa9fd3e7a5da
Danilo Piparo <danilo.piparo@cern.ch>
2016-09-15 14:03:08 +0200

    Add the ROOT::TSpinMutex class

---
+++ core/thread/inc/TSpinMutex.h
@@ -0,0 +1,18 @@
+#include <atomic>
+
+namespace ROOT {
+   /// A spin mutex class which respects the STL interface.
+   class TSpinMutex {
+   public:
+      constexpr TSpinMutex() noexcept {}
+      TSpinMutex(const TSpinMutex&) = delete;
+      TSpinMutex( TSpinMutex && ) = delete;
+      ~TSpinMutex(){}
+      void lock(){while (fAFlag.test_and_set(std::memory_order_acquire));}
+      void unlock(){fAFlag.clear(std::memory_order_release);}
+      bool try_lock(){return !fAFlag.test_and_set(std::memory_order_acquire);}
+
+   private:
+      std::atomic_flag fAFlag = ATOMIC_FLAG_INIT;
+   };
+}
