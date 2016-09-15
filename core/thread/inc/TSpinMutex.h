39f8ed0f2b1ef5d3e8572bc74f2fbe0a153e74ce
Danilo Piparo <danilo.piparo@cern.ch>
2016-09-15 14:50:19 +0200

    Make class more C++11 compliant
    o Do not delete move ctor
    o Mark ctor as default
    o Delete the assignment operator (as in std::mutex)
    Add also include guards.

--- core/thread/inc/TSpinMutex.h
+++ core/thread/inc/TSpinMutex.h
@@ -1,13 +1,16 @@
+#ifndef ROOT_TSpinMutex
+#define ROOT_TSpinMutex
+
 #include <atomic>
 
 namespace ROOT {
    /// A spin mutex class which respects the STL interface.
    class TSpinMutex {
    public:
-      TSpinMutex() noexcept {}
+      TSpinMutex() = default;
       TSpinMutex(const TSpinMutex&) = delete;
-      TSpinMutex( TSpinMutex && ) = delete;
       ~TSpinMutex(){}
+      TSpinMutex& operator=(const TSpinMutex&) = delete;
       void lock(){while (fAFlag.test_and_set(std::memory_order_acquire));}
       void unlock(){fAFlag.clear(std::memory_order_release);}
       bool try_lock(){return !fAFlag.test_and_set(std::memory_order_acquire);}
@@ -16,3 +19,5 @@ namespace ROOT {
       std::atomic_flag fAFlag = ATOMIC_FLAG_INIT;
    };
 }
+
+#endif
