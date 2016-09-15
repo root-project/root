59f9dc384b933b675803d93a5feff44a402492d5
Danilo Piparo <danilo.piparo@cern.ch>
2016-09-15 15:07:03 +0200

    Add test to count the include paths.

---
+++ root/meta/countIncludePaths.C
@@ -0,0 +1,24 @@
+int countSubstring(const std::string& str, const std::string& sub)
+{
+   if (sub.length() == 0) return 0;
+   int count = 0;
+   for (size_t offset = str.find(sub); offset != std::string::npos;
+      offset = str.find(sub, offset + sub.length())) { ++count; }
+   return count;
+}
+
+int countIncludePaths()
+{
+   for (auto lib : {"libMathCore", "libunordered_mapDict", "libmapDict", "libHist"})
+      gSystem->Load(lib);
+
+   std::string includePath(gSystem->GetIncludePath());
+   // At most 8
+   auto nPaths = countSubstring(includePath, "-I");
+   if (nPaths>8){
+      std::cerr << "The number of include paths is too high" << nPaths
+                << ". The number of \"-I\"s has been counted in the include path of ROOT (gSystem->GetIncludePath()=" << includePath << ")." << std::endl;
+      return nPaths;
+   }
+   return 0;
+}
