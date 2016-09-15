a174ee63e075afc1b29c6441e1db8580d3ff5f62
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2016-09-15 10:33:08 +0200

    tutorials/multicore: mp103_processSelector.C
    Make sure that the selector output list is cleaned after each run to avoid spurious errors
    from duplicate name checking in TSelectorList.
    Remove test with limited number of entries to avoid failed fits. This will be
    done elsewhere.
    Cosmetic changes.

--- tutorials/multicore/mp103_processSelector.C
+++ tutorials/multicore/mp103_processSelector.C
@@ -11,12 +11,13 @@ const char *fh1[] = {"http://root.cern.ch/files/h1/dstarmb.root",
                      "http://root.cern.ch/files/h1/dstarp2.root"};
 
 int mp103_processSelector(){
+
   // MacOSX may generate connection to WindowServer errors
   gROOT->SetBatch(kTRUE);
 
   TString selectorPath = gROOT->GetTutorialsDir();
   selectorPath += "/tree/h1analysis.C+";
-  std::cout<<"selector used is: "<< selectorPath<<"\n";
+  std::cout << "selector used is: "<< selectorPath<<"\n";
   TSelector *sel = TSelector::GetSelector(selectorPath);
 
   TFile *fp = TFile::Open(fh1[0]);
@@ -26,39 +27,28 @@ int mp103_processSelector(){
 
   //TProcPool::Process with a single tree
   TList* out = pool.ProcTree(*tree, *sel);;
+  sel->GetOutputList()->Delete();
 
   //TProcPool::Process with single file name and tree name
   //Note: we have less files than workers here
   out = pool.ProcTree(fh1[0], *sel, "h1");
   sel->GetOutputList()->Delete();
-  //TProcPool::Process with vector of files and tree name
-  //Note: we have more files than workers here (different behaviour)
+
+  // Prepare datasets: vector of files, TFileCollection
+  TFileCollection fc;
   std::vector<std::string> files;
   for (int i = 0; i < 4; i++) {
      files.push_back(fh1[i]);
+     fc.Add(new TFileInfo(fh1[i]));
   }
+
+  //TProcPool::Process with vector of files and tree name
+  //Note: we have more files than workers here (different behaviour)
   out = pool.ProcTree(files, *sel, "h1");
   sel->GetOutputList()->Delete();
 
-  //TProcPool::Process with single file name, tree name and entries limit
-  out = pool.ProcTree(fh1[0], *sel, "h1", 42);
-  sel->GetOutputList()->Delete();
-
-  //TProcPool::Process with vector of files, no tree name and no entries limit
-  out = pool.ProcTree(files, *sel);
-  sel->GetOutputList()->Delete();
-
-  //TProcPool::Process with TFileCollection, no tree name and entries limit
-  TFileCollection fc;
-  fc.Add(new TFileInfo(fh1[0]));
-  out = pool.ProcTree(fc, *sel, "", 42);
-  sel->GetOutputList()->Delete();
-
-  //TProcPool::Process with TChain, no tree name and no entries limit
-  TChain c;
-  c.Add(fh1[0]);
-  c.Add(fh1[1]);
-  out = pool.ProcTree(c, *sel);
+  //TProcPool::Process with TFileCollection, no tree name
+  out = pool.ProcTree(fc, *sel);
   sel->GetOutputList()->Delete();
 
   return 0;
