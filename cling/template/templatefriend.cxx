62caed66ff90da4554a9feb8acfae0e4fcd9d7d5
Danilo Piparo <danilo.piparo@cern.ch>
2016-09-16 11:07:02 +0200

    Do not use as test class name shared_ptr as this is part of the stl.
    This name was modified as clashes with the implementation of
    condition_variable_any are introduced.

--- cling/template/templatefriend.cxx
+++ cling/template/templatefriend.cxx
@@ -5,27 +5,22 @@
 #include <iostream>
 #include <typeinfo>
 
-// MSVC2010 pulls in std::tr1::shared_ptr, C++11 has std::shared_ptr
-// and Cling pulls in std,
-// which combined creates ambiguities.
-# define shared_ptr not_tr1_shared_ptr
-
 class Parent{ public: static const char *ClassName() { return "Parent"; } };
 
 class Child : public Parent { public: static const char *ClassName() { return "Child"; }};
 
-template <class T> class shared_ptr;
+template <class T> class my_shared_ptr;
 
 class testing {
-   template <class U> friend class shared_ptr;
+   template <class U> friend class my_shared_ptr;
 };
 
 template <class T>
-class shared_ptr
+class my_shared_ptr
 {
 
   T *theobject;
-  // this shared_ptr class is of course missing any
+  // this my_shared_ptr class is of course missing any
   // reference counting mechanism
    void PF(const char* func) {
       std::string clname;
@@ -36,23 +31,23 @@ class shared_ptr
       if (clname.find("Parent") != std::string::npos) clname = "Parent";
       else clname = "Child";
 #endif
-      std::cout << "shared_ptr<" << clname << ">::" << func << std::endl;
+      std::cout << "my_shared_ptr<" << clname << ">::" << func << std::endl;
    }
 
 public:
 
-  template <class U> friend class shared_ptr;
+  template <class U> friend class my_shared_ptr;
 
   const char *GetName() { return typeid(T).name(); }
 
-  shared_ptr(T* someobject)
+  my_shared_ptr(T* someobject)
   {
      PF("c'tor(T)");
     theobject = someobject;
   }
 
   template <class Y>
-  shared_ptr(shared_ptr<Y> const &rhs)
+  my_shared_ptr(my_shared_ptr<Y> const &rhs)
     : theobject(dynamic_cast<T*>(rhs.theobject))
   {
      PF("c'tor(Y)");
@@ -60,17 +55,17 @@ public:
 };
 
 #ifdef __MAKECINT__
-#pragma link C++ class shared_ptr<Parent>+;
-#pragma link C++ class shared_ptr<Child>+;
+#pragma link C++ class my_shared_ptr<Parent>+;
+#pragma link C++ class my_shared_ptr<Child>+;
 #endif
 int templatefriend()
 {
   // create shared pointer to child
-  shared_ptr<Child> child_p(new Child);
+  my_shared_ptr<Child> child_p(new Child);
 
   // create shared pointer to parent from
   // shared pointer to child, calls dynamic_cast<> in template constructor.
-  shared_ptr<Parent> parent_p(child_p);
+  my_shared_ptr<Parent> parent_p(child_p);
   return 0;
 }
 
