49ef1c04d2ee6c9fb3805442725f5cbdbe456295
Philippe Canal <pcanal@fnal.gov>
2016-09-18 20:54:54 +0200

    Coding conventions

--- root/io/uniquePointer/classes.h
+++ root/io/uniquePointer/classes.h
@@ -11,37 +11,37 @@
 
 class A{
 public:
-   A(): TH1FFixedUPtr(new TH1F("","b",64,0,4)),
-        TH1FPtr(new TH1F("","b",64,0,4))
+   A(): fTH1FFixedUPtr(new TH1F("","b",64,0,4)),
+        fTH1FPtr(new TH1F("","b",64,0,4))
    {
    };
 
-   A(const char* meta): TH1FBaseUPtr(new TH1F((std::string(meta)+"_base").c_str(),"b",64,0,4)),
-                        TH1FUPtr(new TH1F((std::string(meta)+"_var").c_str(),"b",64,0,4)),
-                        TH1FFixedUPtr(new TH1F((std::string(meta)+"_fixed").c_str(),"b",64,0,4)),
-                        TH1FPtr(new TH1F(meta,"b",64,0,4))
+   A(const char* meta): fTH1FBaseUPtr(new TH1F((std::string(meta)+"_base").c_str(),"b",64,0,4)),
+                        fTH1FUPtr(new TH1F((std::string(meta)+"_var").c_str(),"b",64,0,4)),
+                        fTH1FFixedUPtr(new TH1F((std::string(meta)+"_fixed").c_str(),"b",64,0,4)),
+                        fTH1FPtr(new TH1F(meta,"b",64,0,4))
    {
-      TH1FUPtrs.emplace_back(new TH1F((std::string(meta)+"_unique1").c_str(),"b",64,0,4));
-      TH1FUPtrs.emplace_back(new TH1F((std::string(meta)+"_unique2").c_str(),"b",64,0,4));
-      TH1FUPtrs.emplace_back(new TH1F((std::string(meta)+"_unique3").c_str(),"b",64,0,4));
+      fTH1FUPtrs.emplace_back(new TH1F((std::string(meta)+"_unique1").c_str(),"b",64,0,4));
+      fTH1FUPtrs.emplace_back(new TH1F((std::string(meta)+"_unique2").c_str(),"b",64,0,4));
+      fTH1FUPtrs.emplace_back(new TH1F((std::string(meta)+"_unique3").c_str(),"b",64,0,4));
       
       gRandom->SetSeed(1);
-      TH1FBaseUPtr->FillRandom("gaus");
+      fTH1FBaseUPtr->FillRandom("gaus");
       gRandom->SetSeed(1);
-      TH1FUPtr->FillRandom("gaus");
+      fTH1FUPtr->FillRandom("gaus");
       gRandom->SetSeed(1);
-      TH1FFixedUPtr->FillRandom("gaus");
+      fTH1FFixedUPtr->FillRandom("gaus");
       gRandom->SetSeed(1);
-      TH1FPtr->FillRandom("gaus");
+      fTH1FPtr->FillRandom("gaus");
 
-      for (auto&& h : TH1FUPtrs) {
+      for (auto&& h : fTH1FUPtrs) {
          gRandom->SetSeed(1);
          h->FillRandom("gaus");
       }
    }
 
    ~A(){
-      delete TH1FPtr;
+      delete fTH1FPtr;
    }
 
    void Randomize() {
@@ -53,30 +53,30 @@ public:
             h->SetBinContent(i, fabs(gRandom->Gaus(bContent,bVar)));
          }
       };
-      rndMizeBins(TH1FPtr);
-      rndMizeBins(TH1FUPtr.get());
-      for (auto&& h : TH1FUPtrs) rndMizeBins(h.get());
+      rndMizeBins(fTH1FPtr);
+      rndMizeBins(fTH1FUPtr.get());
+      for (auto&& h : fTH1FUPtrs) rndMizeBins(h.get());
    }
 
-   TH1F* GetHPtr() {return TH1FPtr;}
-   TH1*  GetHBaseUPtr() {return TH1FBaseUPtr.get();}
-   TH1F* GetHUPtr() {return TH1FUPtr.get();}
-   TH1F* GetHFixedUPtr() {return TH1FFixedUPtr.get();}
+   TH1F* GetHPtr() {return fTH1FPtr;}
+   TH1*  GetHBaseUPtr() {return fTH1FBaseUPtr.get();}
+   TH1F* GetHUPtr() {return fTH1FUPtr.get();}
+   TH1F* GetHFixedUPtr() {return fTH1FFixedUPtr.get();}
    TH1F* GetHUPtrAt(unsigned int i) {
-      //return TH1FUPtrs.at(i).get()
-      auto it = TH1FUPtrs.begin();
+      //return fTH1FUPtrs.at(i).get()
+      auto it = fTH1FUPtrs.begin();
       for (unsigned int j=0;j<i;++j) it++;
       return it->get();
       
    }
 
 private:
-   std::unique_ptr<TH1>  TH1FBaseUPtr;
-   std::unique_ptr<TH1F> TH1FUPtr;
-   std::unique_ptr<TH1F> TH1FFixedUPtr; //->
-   std::list<std::unique_ptr<TH1F>> TH1FUPtrs;
+   std::unique_ptr<TH1>  fTH1FBaseUPtr;
+   std::unique_ptr<TH1F> fTH1FUPtr;
+   std::unique_ptr<TH1F> fTH1FFixedUPtr; //->
+   std::list<std::unique_ptr<TH1F>> fTH1FUPtrs;
    double d = .5;
-   TH1F* TH1FPtr; // ->
+   TH1F* fTH1FPtr; // ->
 };
 
 #endif
