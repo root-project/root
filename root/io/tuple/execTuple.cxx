c7854448368bc4f4d94daddf71ac0df377aa121c
Philippe Canal <pcanal@fnal.gov>
2016-09-19 11:02:05 +0200

    White space

--- root/io/tuple/execTuple.cxx
+++ root/io/tuple/execTuple.cxx
@@ -48,8 +48,8 @@ almost_equal(T x, T y, int ulp)
    // the machine epsilon has to be scaled to the magnitude of the values used
    // and multiplied by the desired precision in ULPs (units in the last place)
    return std::abs(x-y) < std::numeric_limits<T>::epsilon() * std::abs(x+y) * ulp
-   // unless the result is subnormal
-   || std::abs(x-y) < std::numeric_limits<T>::min();
+          // unless the result is subnormal
+          || std::abs(x-y) < std::numeric_limits<T>::min();
 }
 
 bool tester(unsigned int component, double value, double expected)
