71f1edd4abf26645736f077fe945b39f9f156220
Axel Naumann <Axel.Naumann@cern.ch>
2016-09-23 12:13:06 +0200

    Add basic test for canv.Draw(hist).

---
+++ hist/hist/v7/unit/draw.cxx
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include "ROOT/THist.h"
+#include "ROOT/Canvas.h"
+
+using namespace ROOT::Experimental;
+
+// Test drawing of histograms.
+TEST(DrawTest, OneD) {
+   TAxisConfig xaxis{10, 0., 1.};
+   auto h = std::make_shared<TH1D>(xaxis);
+   TCanvas canv;
+   canv.Draw(h);
+   EXPECT_EQ(canv.GetPrimitives().size(), 1);
+}
+
+TEST(DrawTest, TwoD) {
+   TAxisConfig xaxis{10, 0., 1.};
+   TAxisConfig yaxis{{0., 1., 10., 100.}};
+   auto h = std::make_shared<TH2I>(xaxis, yaxis);
+   TCanvas canv;
+   canv.Draw(h);
+   EXPECT_EQ(canv.GetPrimitives().size(), 1);
+}
+
+TEST(DrawTest, ThreeD) {
+   TAxisConfig xaxis{{0., 1.}};
+   TAxisConfig yaxis{10, 0., 1.};
+   TAxisConfig zaxis{{0., 1., 10., 100.}};
+   auto h = std::make_shared<TH3F>(xaxis, yaxis, zaxis);
+   TCanvas canv;
+   canv.Draw(h);
+   EXPECT_EQ(canv.GetPrimitives().size(), 1);
+}
