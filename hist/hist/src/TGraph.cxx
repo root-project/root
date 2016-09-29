49bbebda008047cb694522798d6f8112dfb8b3b5
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-09-29 12:49:53 +0200

    avoid error when copy&pasting code from doxygen

--- hist/hist/src/TGraph.cxx
+++ hist/hist/src/TGraph.cxx
@@ -66,7 +66,7 @@ class. All details about the various painting options are given in this class.
 
 The picture below gives an example:
 Begin_Macro(source)
-{
+TCanvas* GraphExample() {
    TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",200,10,700,500);
    Double_t x[100], y[100];
    Int_t n = 20;
@@ -74,7 +74,7 @@ Begin_Macro(source)
      x[i] = i*0.1;
      y[i] = 10*sin(x[i]+0.2);
    }
-   gr = new TGraph(n,x,y);
+   TGraph* gr = new TGraph(n,x,y);
    gr->Draw("AC*");
    return c1;
 }
