97fc1503529382c259f8faa05cb4f6f8942bcdaf
Axel Naumann <Axel.Naumann@cern.ch>
2016-09-29 14:25:29 +0200

    Fix ownership doc.

--- graf2d/gpad/v7/inc/ROOT/TCanvas.hxx
+++ graf2d/gpad/v7/inc/ROOT/TCanvas.hxx
@@ -66,14 +66,16 @@ public:
   /// Outline the implementation in sources.
   ~TCanvas();
 
-  /// Add something to be painted. The pad claims shared ownership.
+  /// Add something to be painted.
+  /// The pad observes what's lifetime through a weak pointer.
   template<class T>
   void Draw(const std::shared_ptr<T>& what) {
     // Requires GetDrawable(what, options) to be known!
     fPrimitives.emplace_back(GetDrawable(what));
   }
 
-  /// Add something to be painted, with options. The pad claims shared ownership.
+  /// Add something to be painted, with options.
+  /// The pad observes what's lifetime through a weak pointer.
   template<class T, class OPTIONS>
   void Draw(const std::shared_ptr<T>& what, const OPTIONS &options) {
     // Requires GetDrawable(what, options) to be known!
@@ -101,7 +103,7 @@ public:
      fPrimitives.emplace_back(GetDrawable(std::make_unique<T>(what)));
    }
 
-   /// Add a copy of something to be painted, with options. The pad claims ownership.
+   /// Add a copy of something to be painted, with options.
    template<class T, class OPTIONS>
    void Draw(const T& what, const OPTIONS &options) {
      // Requires GetDrawable(what, options) to be known!
