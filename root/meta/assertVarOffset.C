4575457c0e24fc4221e932478f2ee5685a5e2032
Axel Naumann <Axel.Naumann@cern.ch>
2016-10-04 12:15:04 +0200

    Check address, not just value (ROOT-7459).

--- root/meta/assertVarOffset.C
+++ root/meta/assertVarOffset.C
@@ -1,4 +1,5 @@
 // Test ROOT-7306 and friends.
+// One friend being ROOT-7459.
 
 #include <iostream>
 
@@ -9,29 +10,33 @@ const float f2 = 2.72;
 
 template <class T>
 struct allowed_delta {
-   static constexpr T val = T();
+   using Delta_t = decltype (T() - T());
+   static constexpr Delta_t val = Delta_t();
 };
 
 template <>
 struct allowed_delta<float> {
+   using Delta_t = float;
    static constexpr float val = 1E-3;
 };
 template <>
 struct allowed_delta<double> {
+   using Delta_t = double;
    static constexpr double val = 1E-6;
 };
 
 
 template <class T>
-void assertEqual(T expect, T actual, T allowed = allowed_delta<T>::val) {
-   T delta = actual - expect;
-   if (delta * delta > allowed * allowed)
+void assertEqual(T expect, T actual, typename allowed_delta<T>::Delta_t allowed = allowed_delta<T>::val) {
+   auto delta = actual - expect;
+   if (   (delta > 0 && delta > allowed)
+       || (delta < 0 && -delta > allowed))
       std::cerr << "ERROR: expected " << expect << ", got " << actual
                 << std::endl;
 }
 
 int assertVarOffset() {
-   assertEqual(N1, *(int*)gROOT->GetGlobal("N1")->GetAddress());
+   assertEqual(&N1, (int*)gROOT->GetGlobal("N1")->GetAddress());
    assertEqual(N2, *(int*)gROOT->GetGlobal("N2")->GetAddress());
    assertEqual(f1, *(float*)gROOT->GetGlobal("f1")->GetAddress());
    assertEqual(f2, *(float*)gROOT->GetGlobal("f2")->GetAddress());
