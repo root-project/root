7a53954417b8c3e3ecf0c757b480e1346bb77067
Vassil Vassilev <vvasilev@cern.ch>
2016-10-14 18:42:55 +0200

    Move the preprocessor directives out of the function body.

--- tutorials/math/exampleMultiRoot.C
+++ tutorials/math/exampleMultiRoot.C
@@ -41,6 +41,8 @@
 
 #ifdef R__HAS_MATHMORE
 #include "Math/MultiRootFinder.h"
+#else
+#error libMathMore is not available - cannot run this tutorial
 #endif
 #include "Math/WrappedMultiTF1.h"
 #include "TF2.h"
@@ -52,10 +54,6 @@
 
 using namespace ROOT::Math;
 void exampleMultiRoot(const char * algo = 0, int printlevel = 1) {
-
-#ifndef R__HAS_MATHMORE
-   Error("exampleMultiRoot","libMathMore is not available - cannot run this tutorial");
-#else
    ROOT::Math::MultiRootFinder r(algo);
    //defining the function
    // use Rosenbrock functions
@@ -73,6 +71,4 @@ void exampleMultiRoot(const char * algo = 0, int printlevel = 1) {
    // starting point
    double x0[2]={-1,-1};
    r.Solve(x0);
-#endif
 }
-
