c74d5394ddd8d38954c2229e5fe0e8dd63461319
Axel Naumann <Axel.Naumann@cern.ch>
2016-10-19 18:08:41 +0200

    Add C++14 sized deallocation overloads; fixes GCC 6.2 warning.
    (cherry picked from commit 53e62c400ecca08000cff0f29948e9d617383b4a)

--- core/base/inc/TObject.h
+++ core/base/inc/TObject.h
@@ -147,6 +147,11 @@ public:
    void    *operator new[](size_t sz, void *vp) { return TStorage::ObjectAlloc(sz, vp); }
    void     operator delete(void *ptr);
    void     operator delete[](void *ptr);
+#ifdef R__USE_CXX14
+   // Sized deallocation.
+   void     operator delete(void*, size_t);
+   void     operator delete[](void*, size_t);
+#endif
 #ifdef R__PLACEMENTDELETE
    void     operator delete(void *ptr, void *vp);
    void     operator delete[](void *ptr, void *vp);
