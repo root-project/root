7e7652b0860f92b7ed7e0c9995cf10a1b74560b9
Olivier Couet <olivier.couet@cern.ch>
2016-10-19 16:15:00 +0200

    From: Sergey Linev <S.Linev@gsi.de> In TPad::SaveAs method json file extension is now handled

--- graf2d/gpad/src/TPad.cxx
+++ graf2d/gpad/src/TPad.cxx
@@ -4354,6 +4354,7 @@ void TPad::Print(const char *filenam, Option_t *option)
    //              "tiff" - a TIFF file is produced
    //               "cxx" - a C++ macro file is produced
    //               "xml" - a XML file
+   //              "json" - a JSON file
    //              "root" - a ROOT binary file
    //
    //     filename = 0 - filename  is defined by the GetName and its
@@ -4575,6 +4576,12 @@ void TPad::Print(const char *filenam, Option_t *option)
    //==============Save pad/canvas as a XML file================================
    if (strstr(opt,"xml")) {
       // Plugin XML driver
+      if (gDirectory) gDirectory->SaveObjectAs(this,psname.Data(),"");
+      return;
+   }
+
+   //==============Save pad/canvas as a JSON file================================
+   if (strstr(opt,"json")) {
       if (gDirectory) gDirectory->SaveObjectAs(this,psname.Data(),"");
       return;
    }
@@ -5142,6 +5149,7 @@ void TPad::SaveAs(const char *filename, Option_t * /*option*/) const
    //   if filename contains .C or .cxx, a C++ macro file is produced
    //   if filename contains .root, a Root file is produced
    //   if filename contains .xml, a XML file is produced
+   //   if filename contains .json, a JSON file is produced
    //
    //   See comments in TPad::Print for the Postscript formats
 
@@ -5169,6 +5177,8 @@ void TPad::SaveAs(const char *filename, Option_t * /*option*/) const
       ((TPad*)this)->Print(psname,"root");
    else if (psname.EndsWith(".xml"))
       ((TPad*)this)->Print(psname,"xml");
+   else if (psname.EndsWith(".json"))
+      ((TPad*)this)->Print(psname,"json");
    else if (psname.EndsWith(".eps"))
       ((TPad*)this)->Print(psname,"eps");
    else if (psname.EndsWith(".pdf"))
