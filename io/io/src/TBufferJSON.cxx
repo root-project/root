0960cc1c4f8785b7efc5f8102cc66883ae8d6f76
Sergey Linev <S.Linev@gsi.de>
2016-10-20 14:39:43 +0200

    json: test new kind of refs coding
    Use special object to code refs, current approach with strings is not
    safe.
    Requires newest JSROOT version, will be actiavted after JSROOT 4.8
    release.

--- io/io/src/TBufferJSON.cxx
+++ io/io/src/TBufferJSON.cxx
@@ -847,7 +847,10 @@ void TBufferJSON::JsonWriteObject(const void *obj, const TClass *cl, Bool_t chec
       if (check_map) {
          std::map<const void *, unsigned>::const_iterator iter = fJsonrMap.find(obj);
          if (iter != fJsonrMap.end()) {
+            // old-style refs, coded into string like "$ref12"
             AppendOutput(Form("\"$ref:%u\"", iter->second));
+            // new-style refs, coded into extra object {"ref":12}, auto-detected by JSROOT
+            // AppendOutput(Form("{\"ref\":%u}", iter->second));
             goto post_process;
          }
          fJsonrMap[obj] = fJsonrCnt;
