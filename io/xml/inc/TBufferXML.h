c2c35e8fb367b9aa6db7bd0304e3352a6dbf87f4
Philippe Canal <pcanal@fnal.gov>
2016-11-07 08:38:50 -0600

    Properly handle I/O of pointer to std::string.
    Like the STL collection a nullptr std::string is stored the same as a pointer to an 'empty' std::string.

--- io/xml/inc/TBufferXML.h
+++ io/xml/inc/TBufferXML.h
@@ -205,7 +205,7 @@ public:
    virtual   void     ReadDouble(Double_t   &d);
    virtual   void     ReadCharP(Char_t      *c);
    virtual   void     ReadTString(TString   &s);
-   virtual   void     ReadStdString(std::string &s);
+   virtual   void     ReadStdString(std::string *s);
 
    virtual   void     WriteBool(Bool_t       b);
    virtual   void     WriteChar(Char_t       c);
@@ -222,7 +222,7 @@ public:
    virtual   void     WriteDouble(Double_t   d);
    virtual   void     WriteCharP(const Char_t *c);
    virtual   void     WriteTString(const TString  &s);
-   virtual   void     WriteStdString(const std::string &s);
+   virtual   void     WriteStdString(const std::string *s);
 
    virtual Int_t ApplySequence(const TStreamerInfoActions::TActionSequence &sequence, void *object);
    virtual Int_t ApplySequenceVecPtr(const TStreamerInfoActions::TActionSequence &sequence, void *start_collection, void *end_collection);
