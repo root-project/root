a5a19c2f85526daf61cc1c17dc26f56184c116f5
Vassil Vassilev <vvasilev@cern.ch>
2016-11-12 17:13:44 -0800

    [cmake] Make paths of headers when building G__Core dictionary relative.
    Core is split differently than the rest of ROOT. We generate the target of
    G__Core from within core/base, thus CMAKE_CURRENT_SOURCE_DIR cannot make
    headers from the rest of core subfolders relative.

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -238,6 +238,12 @@ function(ROOT_GENERATE_DICTIONARY dictionary)
     endif()
   endforeach()
   string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/inc/" ""  rheaderfiles "${headerfiles}")
+  # Replace the non-standard folder layout of Core.
+  if (ARG_STAGE1 AND ${ARG_MODULE} STREQUAL "Core")
+    set(core_folders "base|clib|cont|doc|lzma|macosx|meta|metautils|multiproc|newdelete|pcre|rint|textinput|thread|unix|utils|winnt|zip")
+    string(REGEX REPLACE "${CMAKE_SOURCE_DIR}/core/(${core_folders})/inc/" ""  rheaderfiles "${rheaderfiles}")
+  endif()
+
   #---Get the list of include directories------------------
   get_directory_property(incdirs INCLUDE_DIRECTORIES)
   if(CMAKE_PROJECT_NAME STREQUAL ROOT)
