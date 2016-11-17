3fc3669a856c2855d717af8af655340c87e4e9ff
Philippe Canal <pcanal@fnal.gov>
2016-11-17 11:16:23 -0600

    In assertUnload.C test the success (or not) of ProcessLine calls

--- root/meta/callfunc/assertUnload.C
+++ root/meta/callfunc/assertUnload.C
@@ -2,7 +2,14 @@
 
 int assertUnload() {
    gInterpreter->Declare("int myFunc() { return 42; }");
-   gInterpreter->ProcessLine(".x assertUnloadHelper.C");
+
+   TInterpreter::EErrorCode status;
+   gInterpreter->ProcessLine(".x assertUnloadHelper.C",&status);
+   if (status != TInterpreter::EErrorCode::kNoError) {
+      std::cerr << "Loading of assertUnloadHelper.C failed\n";
+      return 1;
+   }
+
    TMethodCall mc(nullptr, "myFunc", "");
    long result = 0;
 
@@ -11,7 +18,11 @@ int assertUnload() {
       std::cerr << "myFunc() returned not 42 but " << result << '\n';
       return 1;
    }
-   gInterpreter->ProcessLine(".U assertUnloadHelper.C");
+   gInterpreter->ProcessLine(".U assertUnloadHelper.C",&status);
+   if (status != TInterpreter::EErrorCode::kNoError) {
+      std::cerr << "Unloading of assertUnloadHelper.C failed\n";
+      return 1;
+   }
 
    mc.Execute(result);
    if (result != 42) {
