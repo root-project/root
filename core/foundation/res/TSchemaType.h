60b1a2251c9750887e04a8c163bad819459e1ca1
Axel Naumann <Axel.Naumann@cern.ch>
2016-12-13 12:14:03 +0100

    Cling should never see this; add comment about libCore/libCling use; constness; default ctor = default.

--- core/foundation/res/TSchemaType.h
+++ core/foundation/res/TSchemaType.h
@@ -3,22 +3,19 @@
 #ifndef R__TSCHEMATYPE_H
 #define R__TSCHEMATYPE_H
 
-#if !defined(__CINT__)
-// Avoid clutering the dictionary (in particular with the STL declaration)
+// NOTE: #included by libCore and libCling. All symbols must be inline.
 
 #include <string>
 
 namespace ROOT {
 namespace Internal {
    struct TSchemaType {
-      TSchemaType() {}
+      TSchemaType() = default;
       TSchemaType(const char *type, const char *dim) : fType(type),fDimensions(dim) {}
-      TSchemaType(std::string &type, std::string &dim) : fType(type),fDimensions(dim) {}
+      TSchemaType(const std::string &type, const std::string &dim) : fType(type),fDimensions(dim) {}
       std::string fType;
       std::string fDimensions;
    };
 }
 }
-#endif // __CINT__
 #endif // R__TSCHEMATYPE_H
-
