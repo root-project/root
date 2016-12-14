67731253d21c6ab812ee639a789ba5f7f72d65c3
Xavier Valls <xaviervallspla@gmail.com>
2016-12-14 14:28:13 +0100

    Fix NDim and add WeightsPtr function in UnbinData

--- math/mathcore/inc/Fit/UnBinData.h
+++ math/mathcore/inc/Fit/UnBinData.h
@@ -259,14 +259,26 @@ public:
     assert( ipoint < fNPoints );
 
     if ( !fWeighted ) return 1.0;
-    return Coords(ipoint)[fDim-1];
+    return *GetCoordComponent(ipoint, fDim-1);
   }
+
+  const double * WeightsPtr( unsigned int ipoint ) const
+  {
+    assert( ipoint < fNPoints );
+
+    if ( !fWeighted ){
+       MATH_ERROR_MSG("UnBinData::WeightsPtr","The function is unweighted!");
+       return nullptr;
+    }
+    return GetCoordComponent(ipoint, fDim-1);
+  }
+
 
   /**
     return coordinate data dimension
   */
   unsigned int NDim() const
-  { return fWeighted ? fDim : fDim - 1; }
+  { return fWeighted ? fDim -1 : fDim; }
 
   bool IsWeighted() const
   {
