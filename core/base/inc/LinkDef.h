0f034d699c77ae4c7c989bf41405ff1b433e37ca
Axel Naumann <Axel.Naumann@cern.ch>
2016-12-15 08:09:18 +0100

    Verify that one of SYSTEM_TYPE_... is set.

--- core/base/inc/LinkDef.h
+++ core/base/inc/LinkDef.h
@@ -13,6 +13,8 @@
 #elif defined(SYSTEM_TYPE_macosx)
 #include "core/macosx/inc/LinkDef.h"
 #include "core/unix/inc/LinkDef.h"
-#else
+#elif defined(SYSTEM_TYPE_unix)
 #include "core/unix/inc/LinkDef.h"
+#else
+# error "Unsupported system type."
 #endif
