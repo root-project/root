3e18343d82421ccc5a7e7d4c780abdc157b0f71d
Axel Naumann <Axel.Naumann@cern.ch>
2016-12-21 16:09:30 +0100

    Test TTreeReader with TEntryList; expand SetEntriesRange() tests.

--- root/tree/reader/unit/basic.cxx
+++ root/tree/reader/unit/basic.cxx
@@ -1,3 +1,4 @@
+#include "TEntryListArray.h"
 #include "TLeaf.h"
 #include "TROOT.h"
 #include "TTree.h"
@@ -9,6 +10,7 @@
 
 TTree* MakeTree() {
    double x[3]{};
+   float z = 0.;
    struct {
       unsigned int ny;
       int* y = nullptr;
@@ -17,6 +19,7 @@ TTree* MakeTree() {
    TTree* tree = new TTree("T", "test tree");
    tree->Branch("one", &x, "x[3]/D");
    tree->Branch("two", &yData, "ny/i:y[ny]/I");
+   tree->Branch("three", &z, "z");
 
    x[1] = 42.;
    yData.ny = 42;
@@ -28,6 +31,12 @@ TTree* MakeTree() {
    yData.ny = 5;
    yData.y[4] = 7;
    tree->Fill();
+
+   for (int entry = 2; entry < 20; ++entry) {
+      z = entry * (1 - 2 * (entry % 2)); // +entry for even, -entry for odd
+      tree->Fill();
+   }
+
    delete [] yData.y;
 
    tree->ResetBranchAddresses();
@@ -47,7 +56,7 @@ TEST(TTreeReaderBasic, Interfaces) {
    EXPECT_NE(tr.begin(), tr.end());
    //MISSING: EXPECT_EQ(2, tr.end() - tr.begin());
    EXPECT_EQ(-1, tr.GetCurrentEntry());
-   EXPECT_EQ(2, tr.GetEntries(false));
+   EXPECT_EQ(20, tr.GetEntries(false));
    EXPECT_EQ(TTreeReader::kEntryNotLoaded, tr.GetEntryStatus());
    EXPECT_EQ(tree, tr.GetTree());
    EXPECT_FALSE(tr.IsChain());
@@ -66,7 +75,7 @@ TEST(TTreeReaderBasic, Interfaces) {
    EXPECT_NE(tr.begin(), tr.end());
    //MISSING: EXPECT_EQ(2, tr.end() - tr.begin());
    EXPECT_EQ(1, tr.GetCurrentEntry());
-   EXPECT_EQ(2, tr.GetEntries(false));
+   EXPECT_EQ(20, tr.GetEntries(false));
 
 
    EXPECT_EQ(5u, *ny);
@@ -84,6 +93,8 @@ TEST(TTreeReaderBasic, Interfaces) {
    EXPECT_DOUBLE_EQ(43., x[2]);
    //FAILS: EXPECT_EQ(7, y[4]);
 
+   for (int entry = 2; entry < 20; ++entry)
+      EXPECT_TRUE(tr.Next());
 
    EXPECT_FALSE(tr.Next());
    //FAILS: EXPECT_FALSE(ny.IsValid());
@@ -98,6 +109,9 @@ TEST(TTreeReaderBasic, ErrorProbing) {
    tr.SetTree(nullptr);
    EXPECT_EQ(TTreeReader::kEntryNoTree, tr.GetEntryStatus());
    EXPECT_EQ(nullptr, tr.GetTree());
+
+   TTreeReaderValue<double> val;
+   EXPECT_FALSE(val.IsValid());
 }
 
 
@@ -105,14 +119,144 @@ TEST(TTreeReaderBasic, Range) {
    TTree* tree = MakeTree();
    TTreeReader tr(tree);
 
-   EXPECT_EQ(TTreeReader::kEntryValid, tr.SetEntriesRange(1, 1));
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.SetEntriesRange(5, 8));
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(5, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(6, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(7, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   // Reached end:
+   EXPECT_FALSE(tr.Next());
+   EXPECT_EQ(8, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryBeyondEnd, tr.GetEntryStatus());
+
+   // Read beyond end:
+   EXPECT_FALSE(tr.Next());
+   EXPECT_EQ(TTreeReader::kEntryBeyondEnd, tr.GetEntryStatus());
+   EXPECT_EQ(9, tr.GetCurrentEntry());
+}
+
+TEST(TTreeReaderBasic, InvalidRange) {
+   TTree *tree = MakeTree();
+   TTreeReader tr(tree);
+
+   EXPECT_EQ(TTreeReader::kEntryNotFound, tr.SetEntriesRange(tree->GetEntries(), 0));
+
+   // Is SetEntriesRange() simply ignored as it should be?
+   EXPECT_TRUE(tr.Next());
    EXPECT_EQ(0, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+}
+
+
+TEST(TTreeReaderBasic, OneEntryRange) {
+   TTree* tree = MakeTree();
+   TTreeReader tr(tree);
+
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.SetEntriesRange(1, 2));
+
    EXPECT_TRUE(tr.Next());
    EXPECT_EQ(1, tr.GetCurrentEntry());
    EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
 
    // Read beyond end:
    EXPECT_FALSE(tr.Next());
+   EXPECT_EQ(TTreeReader::kEntryBeyondEnd, tr.GetEntryStatus());
+   EXPECT_EQ(2, tr.GetCurrentEntry());
+
+   for (int entry = 2; entry < tree->GetEntries(); ++entry)
+      EXPECT_FALSE(tr.Next());
+}
+
+
+TEST(TTreeReaderBasic, ZeroEntryRange) {
+   TTree* tree = MakeTree();
+   TTreeReader tr(tree);
+
+   // end is ignored:
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.SetEntriesRange(18, 18));
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(18, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(19, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   // Read beyond end:
+   EXPECT_FALSE(tr.Next());
+   // As the TTree only has up to entry 19, 20 is kEntryNotFound:
    EXPECT_EQ(TTreeReader::kEntryNotFound, tr.GetEntryStatus());
-   EXPECT_EQ(-1, tr.GetCurrentEntry());
+   EXPECT_EQ(20, tr.GetCurrentEntry());
+}
+
+
+TEST(TTreeReaderBasic, InvertedEntryRange) {
+   TTree* tree = MakeTree();
+   TTreeReader tr(tree);
+
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.SetEntriesRange(18, 3));
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(18, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   EXPECT_TRUE(tr.Next());
+   EXPECT_EQ(19, tr.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, tr.GetEntryStatus());
+
+   // Read beyond end:
+   EXPECT_FALSE(tr.Next());
+   // As the TTree only has up to entry 19, 20 is kEntryNotFound:
+   EXPECT_EQ(TTreeReader::kEntryNotFound, tr.GetEntryStatus());
+   EXPECT_EQ(20, tr.GetCurrentEntry());
+}
+
+
+TEST(TTreeReaderBasic, EntryList) {
+   // See https://root.cern.ch/phpBB3/viewtopic.php?f=3&t=22850&p=100796
+   TTree* tree = MakeTree();
+   EXPECT_EQ(9, tree->Draw(">>negZ","three.z<0", "entrylistarray"));
+   TEntryListArray* selected = (TEntryListArray*)gDirectory->Get("negZ");
+   TTreeReader aReader(tree, selected);
+
+   EXPECT_EQ(9, aReader.GetEntries(false));
+
+   TTreeReaderValue<float> z(aReader, "three.z");
+
+   int count = 0;
+   while (aReader.Next()) {
+      // Make sure all Next()-ed entries have z<0 as selected by the entry list.
+      EXPECT_LT(*z, 0);
+      ++count;
+   }
+   EXPECT_EQ(9, count);
+}
+
+TEST(TTreeReaderBasic, EntryListBeyondEnd) {
+   TTree* tree = MakeTree();
+   TEntryList selected;
+   // Add the last valid entry; a subsequent Next() must return false.
+   selected.Enter(tree->GetEntries() - 1);
+
+   TTreeReader aReader(tree, &selected);
+
+   EXPECT_EQ(1, aReader.GetEntries(false));
+
+   EXPECT_TRUE(aReader.Next());
+   EXPECT_EQ(0, aReader.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryValid, aReader.GetEntryStatus());
+
+   EXPECT_FALSE(aReader.Next());
+   EXPECT_EQ(1, aReader.GetCurrentEntry());
+   EXPECT_EQ(TTreeReader::kEntryNotFound, aReader.GetEntryStatus());
 }
