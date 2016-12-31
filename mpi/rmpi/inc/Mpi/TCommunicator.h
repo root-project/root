c02e53ab8d920a2428fbe6cbcb2d476a4a831292
Omar Zapata <Omar.Zapata@cern.ch>
2016-12-31 00:38:14 -0500

    ROOTMpi: added Bcast method for collective communication

--- mpi/rmpi/inc/Mpi/TCommunicator.h
+++ mpi/rmpi/inc/Mpi/TCommunicator.h
@@ -55,60 +55,90 @@ namespace ROOT {
             return fComm.Get_size();
          }
 
-         template<class Type> void Send(Type &obj, Int_t dest, Int_t tag) const;
-         template<class Type>  void Recv(Type &obj, Int_t source, Int_t tag) const; //must be changed by ROOOT::Mpi::TStatus& Recv(...)
+         template<class Type> void Send(Type &var, Int_t dest, Int_t tag) const;
+         template<class Type>  void Recv(Type &var, Int_t source, Int_t tag) const; //must be changed by ROOOT::Mpi::TStatus& Recv(...)
+
+         template<class Type> void Bcast(Type &var, Int_t root) const;
 
          ClassDef(TCommunicator, 1)
       };
 
       template<class T> MPI::Datatype TCommunicator::GetDataType() const
       {
-	if(typeid(T)==typeid(int)||typeid(T)==typeid(Int_t)) return MPI::INT;
-	if(typeid(T)==typeid(float)||typeid(T)==typeid(Float_t)) return MPI::FLOAT;
-	if(typeid(T)==typeid(double)||typeid(T)==typeid(Double_t)) return MPI::DOUBLE;
-	if(typeid(T)==typeid(bool)||typeid(T)==typeid(Bool_t)) return MPI::BYTE;
-	MPI::Datatype None;
-	
-	return None;
-	//TODO: error control here if type is not supported
+         if (typeid(T) == typeid(int) || typeid(T) == typeid(Int_t)) return MPI::INT;
+         if (typeid(T) == typeid(float) || typeid(T) == typeid(Float_t)) return MPI::FLOAT;
+         if (typeid(T) == typeid(double) || typeid(T) == typeid(Double_t)) return MPI::DOUBLE;
+         if (typeid(T) == typeid(bool) || typeid(T) == typeid(Bool_t)) return MPI::BYTE;
+         MPI::Datatype None;
+
+         return None;
+         //TODO: error control here if type is not supported
       }
 
-      
-      
+
+
       template<class Type> void TCommunicator::Send(Type &var, Int_t dest, Int_t tag) const
       {
-	if(std::is_class<Type>::value)
-	{
-         TMpiMessage msg;
-         msg.WriteObject(var);
-         const Char_t *buffer = msg.Buffer();
-         const UInt_t size = msg.BufferSize();
-         fComm.Send(&size, 1, MPI::INT, dest, tag);
-         fComm.Send(buffer, size, MPI::CHAR, dest, tag);
-	}else{
-	 fComm.Send(&var, 1, GetDataType<Type>(), dest, tag);
-          
-	}
+         if (std::is_class<Type>::value) {
+            TMpiMessage msg;
+            msg.WriteObject(var);
+            const Char_t *buffer = msg.Buffer();
+            const UInt_t size = msg.BufferSize();
+            fComm.Send(&size, 1, MPI::INT, dest, tag);
+            fComm.Send(buffer, size, MPI::CHAR, dest, tag);
+         } else {
+            fComm.Send(&var, 1, GetDataType<Type>(), dest, tag);
+         }
       }
       template<class Type>  void TCommunicator::Recv(Type &var, Int_t source, Int_t tag) const
       {
-	if(std::is_class<Type>::value)
-	{
-         UInt_t size = 0;
-         fComm.Recv(&size, 1, MPI::INT, source, tag);
+         if (std::is_class<Type>::value) {
+            UInt_t size = 0;
+            fComm.Recv(&size, 1, MPI::INT, source, tag);
 
-         Char_t *buffer = new Char_t[size];
-         fComm.Recv(buffer, size, MPI::CHAR, source, tag);
+            Char_t *buffer = new Char_t[size];
+            fComm.Recv(buffer, size, MPI::CHAR, source, tag);
 
 
-         TMpiMessage msg(buffer, size);
-         TClass *cl = gROOT->GetClass(typeid(var));
-         Type *obj_tmp = (Type *)msg.ReadObjectAny(cl);
-         memcpy(&var, obj_tmp, sizeof(Type));
-	}else{
-	 fComm.Recv(&var, 1, GetDataType<Type>(), source, tag); 
-	}
+            TMpiMessage msg(buffer, size);
+            auto cl = gROOT->GetClass(typeid(var));
+            auto obj_tmp = (Type *)msg.ReadObjectAny(cl);
+            memcpy((void*)&var,(void*)obj_tmp, sizeof(Type));
+         } else {
+            fComm.Recv(&var, 1, GetDataType<Type>(), source, tag);
+         }
       }
+
+      template<class Type> void TCommunicator::Bcast(Type &var, Int_t root) const
+      {
+         if (std::is_class<Type>::value) {
+            Char_t *buffer = nullptr ;
+            UInt_t size = 0;
+            if (GetRank() == root) {
+               TMpiMessage msg;
+	       msg.WriteObject(var);
+               size = msg.BufferSize();
+	       buffer = new Char_t[size];
+	       memcpy(buffer,msg.Buffer(), size * sizeof(Char_t));
+            }
+            fComm.Bcast(&size, 1, MPI::INT, root);
+            if (GetRank() != root) {
+               buffer = new Char_t[size];
+            }
+            fComm.Bcast(buffer, size, MPI::CHAR, root);
+            if (GetRank() != root) {
+               TMpiMessage msg(buffer, size);
+               auto cl = gROOT->GetClass(typeid(var));
+               auto obj_tmp = (Type *)msg.ReadObjectAny(cl);
+               memcpy((void*)&var,(void*)obj_tmp, sizeof(Type));
+            }
+
+         } else {
+            fComm.Bcast(&var, 1, GetDataType<Type>(), root);
+         }
+
+      }
+
    }
 
 }
