85d5dab19a216ed19c9062321d30d81630c17ab7
Sergey Linev <S.Linev@gsi.de>
2017-01-23 11:17:50 +0100

    Add test for JSON array compression

---
+++ root/io/json/runArrayCompress.C
@@ -0,0 +1,51 @@
+#include "TBufferJSON.h"
+#include "TArrayI.h"
+
+void runArrayCompress()
+{
+   // Check creation of JSON for object, where TObject is not first parent
+   // Also verify instrumented custom streamer of TPolyMarker3D
+
+   TArrayI arr(100);
+   for (Int_t n=0;n<arr.GetSize();++n) arr[n] = n;
+
+   cout << "Plain and compressed array" << endl;
+   TString json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 3);
+   cout << json << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 23);
+   cout << json << endl;
+
+   arr.Reset(0);
+
+   cout << "Empty array" << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 3);
+   cout << json << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 23);
+   cout << json << endl;
+
+   for (Int_t n=0;n<10;++n) { arr[n+17] = 7; arr[n+56] = 11; }
+
+   cout << "Array with many similar values" << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 3);
+   cout << json << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 23);
+   cout << json << endl;
+
+   arr.Reset(0);
+   for (Int_t n=0;n<10;++n) { arr[n+10] = n; arr[n+20] = 55; arr[n+30] = n; }
+
+   cout << "Similar values inside" << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 3);
+   cout << json << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 23);
+   cout << json << endl;
+
+   arr.Reset(0);
+   for (Int_t n=0;n<10;++n) { arr[n+10] = 11; arr[n+20] = n+7; arr[n+30] = 22; }
+   cout << "Similar values outside" << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 3);
+   cout << json << endl;
+   json = TBufferJSON::ConvertToJSON(&arr, arr.IsA(), 23);
+   cout << json << endl;
+
+}
