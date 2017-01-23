cdeb9d00f6bf987031183310a2c7ea56123fcd52
Xavier Valls <xaviervallspla@gmail.com>
2017-01-23 17:41:46 +0100

    Resolve tbb scheduler conflicts
    Both EnableEmplicitMT and TThreadExecutor implementations share the
    same task scheduler and the number of threads it has been set to run on
    
    This patch modifies TThreadExecutor so it:
    
    * avoids the termination (from TThreadExecutor) of the scheduler
      initialized by EnableEmplicitMT.
    * warns the user if she/he tries to change an already-set number of
      threads, as the scheduler will always take the value of its first
      initialization.

--- core/thread/src/TThreadExecutor.cxx
+++ core/thread/src/TThreadExecutor.cxx
@@ -1,15 +1,32 @@
 #include "ROOT/TThreadExecutor.hxx"
 #include "tbb/tbb.h"
+#include "TROOT.h"
+#include <iostream>
 
 namespace ROOT{
+
+  unsigned TThreadExecutor::fPoolSize = 0;
+
   TThreadExecutor::TThreadExecutor():fInitTBB(new tbb::task_scheduler_init()){
   }
 
   TThreadExecutor::TThreadExecutor(size_t nThreads):fInitTBB(new tbb::task_scheduler_init(nThreads)){
+    if (fPoolSize != 0){
+      auto cause = "TThreadExecutor";
+      if(ROOT::IsImplicitMTEnabled()){
+        fPoolSize = ROOT::GetImplicitMTPoolSize();
+        fPoolSize = 3;
+        cause = "ROOT::EnableImplicitMT";
+      }
+      std::cout<<"Can't change the number of threads specified by a previous "<<cause<<" instantiation. Proceding with "<<fPoolSize<<" threads"<<std::endl;
+    } else {
+      fPoolSize = nThreads;
+    }
   }
 
   TThreadExecutor::~TThreadExecutor() {
-    fInitTBB->terminate();
+    if(!ROOT::IsImplicitMTEnabled())
+      fInitTBB->terminate();
   }
 
   void TThreadExecutor::ParallelFor(unsigned int start, unsigned int end, unsigned step, const std::function<void(unsigned int i)> &f){
