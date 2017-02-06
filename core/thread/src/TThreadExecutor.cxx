71e49f4afd1fa9c11f473e0d1624b7e57a36cf74
Xavier Valls <xaviervallspla@gmail.com>
2017-02-06 14:21:45 +0100

    Fix warning display when not changing nThreads

--- core/thread/src/TThreadExecutor.cxx
+++ core/thread/src/TThreadExecutor.cxx
@@ -11,11 +11,11 @@ namespace ROOT{
   TThreadExecutor::TThreadExecutor():TThreadExecutor::TThreadExecutor(tbb::task_scheduler_init::default_num_threads()){}
 
   TThreadExecutor::TThreadExecutor(size_t nThreads):fInitTBB(new tbb::task_scheduler_init(nThreads)){
-    //ImplicitMT and TThreadExecutor share the same pool. If EnableImplicitMT has been called we need 
+    //ImplicitMT and TThreadExecutor share the same pool. If EnableImplicitMT has been called we need
     // to get the size of the already initialized pool of threads
     fgPoolSize += fgPoolSize == 0? ROOT::GetImplicitMTPoolSize(): 0;
 
-    if(fgPoolSize != 0){
+    if(fgPoolSize != 0 && fgPoolSize!=nThreads){
       Warning("TThreadExecutor::TThreadExecutor", "Can't change the number of threads specified by a previous instantiation of TThreadExecutor or EnableImplicitMT. Proceeding with %d threads", fgPoolSize);
     } else {
       fgPoolSize = nThreads;
