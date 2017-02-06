24ffaf5d44e99aa5a83bc95b8fc80e351feb02ec
Olivier Couet <olivier.couet@cern.ch>
2017-02-06 15:46:10 +0100

    - Doxygen formatting - White spaces - Spell Check

--- roofit/roostats/inc/RooStats/HypoTestPlot.h
+++ roofit/roostats/inc/RooStats/HypoTestPlot.h
@@ -25,19 +25,10 @@
 
 namespace RooStats {
 
-   /**
-
-      This class provides the plots for the result of a study performed with any of the
-      HypoTestCalculatorGeneric (e.g. HybridCalculator or FrequentistCalculator)  class.
-
-      \ingroup Roostats
-   */
-
-
 class HypoTestPlot: public SamplingDistPlot {
    public:
       /// Constructor
-   HypoTestPlot() : SamplingDistPlot() , fHypoTestResult(0) {}   // needed for IO 
+   HypoTestPlot() : SamplingDistPlot() , fHypoTestResult(0) {}   // needed for IO
       HypoTestPlot(HypoTestResult& result, Int_t bins=100, Option_t* opt = "NORMALIZE HIST");
       HypoTestPlot(HypoTestResult& result, Int_t bins, Double_t min, Double_t max, Option_t* opt = "NORMALIZE HIST");
       ~HypoTestPlot(void) {}
