dc0e0eee9a54f8cac9c25f50af3bae6ace49be25
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2017-02-07 12:20:20 +0100

    Add test for auto-loading of libMathMore

---
+++ root/math/mathmore/testLoadLibMathMore.C
@@ -0,0 +1,10 @@
+#include "Math/SpecFunc.h"
+#include "Math/ProbFunc.h"
+
+void testLoadLibMathMore() {
+
+   ROOT::Math::MathMoreLibrary::Load();
+   cout<< ROOT::Math::cyl_bessel_i(1,2)   <<endl;
+   cout<< ROOT::Math::noncentral_chisquared_pdf(2,3,1)  <<endl;
+
+}
