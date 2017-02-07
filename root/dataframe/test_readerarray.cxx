9314f0d33cf99bcce17ad2c70744cf3c31fc63e7
Danilo Piparo <danilo.piparo@cern.ch>
2017-02-07 12:55:59 +0100

    Test reader array

---
+++ root/dataframe/test_readerarray.cxx
@@ -0,0 +1,36 @@
+#include "ROOT/TDataFrame.hxx"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TTree.h"
+#include "TTreeReaderArray.h"
+#include "ROOT/TSeq.hxx"
+
+
+void fill_tree(const char* filename, const char* treeName) {
+   TFile f(filename,"RECREATE");
+   TTree t(treeName,treeName);
+   double b1[3];
+   t.Branch("b1", &b1, "b1[3]/D");
+   for(auto i : ROOT::TSeqI(10)) {
+      b1[0] = i;
+      b1[1] = i+1;
+      b1[2] = i+2;
+      t.Fill();
+   }
+   t.Write();
+   f.Close();
+   return;
+}
+
+int main() {
+   auto fileName = "myfile_readerarray.root";
+   auto treeName = "myTree";
+   fill_tree(fileName,treeName);
+
+   TFile f(fileName);
+   ROOT::Experimental::TDataFrame d(treeName, &f, {"b1"});
+   auto c = d.Filter([](std::array_view<double> a) { std::cout << a[0] << " " << a[1] << " " << a[2] << std::endl; return true; }).Count();
+   auto val = *c;
+   std::cout << "count " << val << std::endl;
+   return 0;
+}
