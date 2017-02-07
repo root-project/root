4a04e672216db7b8b4a4f0ae6ce0bf60a55ab92b
Danilo Piparo <danilo.piparo@cern.ch>
2017-02-07 16:32:13 +0100

    [TDF] Check for non split branch

---
+++ root/dataframe/test_splitcoll_arrayview.C
@@ -0,0 +1,56 @@
+#include "ROOT/TDataFrame.hxx"
+#include "TFile.h"
+#include "TTree.h"
+#include "ROOT/TSeq.hxx"
+
+#include <iostream>
+
+class A{
+public:
+   float a=0;
+   float b=0;
+   A(){};
+   A(float d) : a(d), b(2*d) {};
+   void Set(float d) {a = d; b = 2*d;}
+   ClassDef(A,2)
+};
+
+
+#pragma link C++ class std::vector<A>+;
+
+void fill_tree(const char* filename, const char* treeName) {
+   TFile f(filename,"RECREATE");
+   TTree t(treeName,treeName);
+   std::vector<A> v;
+   t.Branch("v", "vector<A>",&v, 32000, 2);
+   for(auto i : ROOT::TSeqI(10)) {
+      v.emplace_back(i);
+      t.Fill();
+   }
+   t.Write();
+   f.Close();
+   return;
+}
+
+int test_splitcoll_arrayview() {
+   auto fileName = "myfile_test_splitcoll_arrayview.root";
+   auto treeName = "myTree";
+   fill_tree(fileName,treeName);
+
+   TFile f(fileName);
+   try {
+      ROOT::Experimental::TDataFrame d(treeName, &f, {"v.a"});
+      auto c = d.Filter([](std::array_view<float> d) { for (auto v : d )std::cout << v << std::endl; return d[1] > 5; }).Count();
+      auto val = *c;
+      std::cout << "count " << val << std::endl;
+   } catch (const std::runtime_error& e) {
+      std::cout << "Exception caught: " << e.what() << std::endl;
+   }
+
+   ROOT::Experimental::TDataFrame d(treeName, &f, {"v"});
+   auto c = d.Filter([](std::array_view<A> d) {  for (auto v : d )std::cout << v.a << std::endl; return d[1].a > 5; }).Count();
+   auto val = *c;
+   std::cout << "count " << val << std::endl;
+
+   return 0;
+}
