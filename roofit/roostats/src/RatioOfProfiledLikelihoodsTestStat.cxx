507a02a7a12f607ee8c3b3863dfddc7b98a9bd4f
Olivier Couet <olivier.couet@cern.ch>
2017-02-09 17:20:14 +0100

    - Doxygen formatting - remove trailing spaces and TABs - spell check

--- roofit/roostats/src/RatioOfProfiledLikelihoodsTestStat.cxx
+++ roofit/roostats/src/RatioOfProfiledLikelihoodsTestStat.cxx
@@ -8,6 +8,41 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
+/** \class RooStats::RatioOfProfiledLikelihoodsTestStat
+    \ingroup Roostats
+
+TestStatistic that returns the ratio of profiled likelihoods.
+
+By default the calculation is:
+
+\f[
+        \log{    \frac{ \lambda(\mu_{alt}  , {conditional \: MLE \: for \: alt  \: nuisance}) }
+                      { \lambda(\mu_{null} , {conditional \: MLE \: for \: null \: nuisance}) } }
+\f]
+
+where \f$ \lambda \f$ is the profile likelihood ratio, so the
+MLE for the null and alternate are subtracted off.
+
+If `SetSubtractMLE(false)` then it calculates:
+
+\f[
+        \log{    \frac{ L(\mu_{alt}  , {conditional \: MLE \: for \: alt  \: nuisance}) }
+                      { L(\mu_{null} , {conditional \: MLE \: for \: null \: nuisance}) } }
+\f]
+
+where \f$ L \f$ is the Likelihood function.
+
+The values of the parameters of interest for the alternative
+hypothesis are taken at the time of the construction.
+If empty, it treats all free parameters as nuisance parameters.
+
+The value of the parameters of interest for the null hypotheses
+are given at each call of Evaluate.
+
+This test statistic is often called the Tevatron test statistic, because it has
+been used by the Tevatron experiments.
+*/
+
 #include "RooStats/RatioOfProfiledLikelihoodsTestStat.h"
 
 #include "RooStats/ProfileLikelihoodTestStat.h"
@@ -23,43 +58,44 @@ Bool_t RooStats::RatioOfProfiledLikelihoodsTestStat::fgAlwaysReuseNll = kTRUE ;
 
 void RooStats::RatioOfProfiledLikelihoodsTestStat::SetAlwaysReuseNLL(Bool_t flag) { fgAlwaysReuseNll = flag ; }
 
-    ////////////////////////////////////////////////////////////////////////////
-    /// returns -logL(poi, conditonal MLE of nuisance params)
-    /// subtract off the global MLE or not depending on the option
-    /// It is the numerator or the denominator of the ratio (depending on the pdf)
-    ///L.M. : not sure why this method is needed now
+////////////////////////////////////////////////////////////////////////////////
+/// returns -logL(poi, conditional MLE of nuisance params)
+/// subtract off the global MLE or not depending on the option
+/// It is the numerator or the denominator of the ratio (depending on the pdf)
+///
+/// L.M. : not sure why this method is needed now
 
 Double_t RooStats::RatioOfProfiledLikelihoodsTestStat::ProfiledLikelihood(RooAbsData& data, RooArgSet& poi, RooAbsPdf& pdf) {
-   int type = (fSubtractMLE) ? 0 : 2; 
-       
+   int type = (fSubtractMLE) ? 0 : 2;
+
    // null
    if ( &pdf == fNullProfile.GetPdf() )
       return fNullProfile.EvaluateProfileLikelihood(type, data, poi);
    else if (&pdf == fAltProfile.GetPdf() )
       return fAltProfile.EvaluateProfileLikelihood(type, data, poi);
-   
-   oocoutE((TObject*)NULL,InputArguments) << "RatioOfProfiledLikelihoods::ProfileLikelihood - invalid pdf used for computing the profiled likelihood - return NaN" 
+
+   oocoutE((TObject*)NULL,InputArguments) << "RatioOfProfiledLikelihoods::ProfileLikelihood - invalid pdf used for computing the profiled likelihood - return NaN"
                          << std::endl;
 
-   return TMath::QuietNaN(); 
-      
+   return TMath::QuietNaN();
+
 }
-    
+
 ////////////////////////////////////////////////////////////////////////////////
 /// evaluate the ratio of profile likelihood
 
 Double_t  RooStats::RatioOfProfiledLikelihoodsTestStat::Evaluate(RooAbsData& data, RooArgSet& nullParamsOfInterest) {
-   
-   int type = (fSubtractMLE) ? 0 : 2; 
-       
+
+   int type = (fSubtractMLE) ? 0 : 2;
+
    // null
    double nullNLL = fNullProfile.EvaluateProfileLikelihood(type, data, nullParamsOfInterest);
    const RooArgSet *nullset = fNullProfile.GetDetailedOutput();
-   
-   // alt 
+
+   // alt
    double altNLL = fAltProfile.EvaluateProfileLikelihood(type, data, *fAltPOI);
    const RooArgSet *altset = fAltProfile.GetDetailedOutput();
-   
+
    if (fDetailedOutput != NULL) {
       delete fDetailedOutput;
       fDetailedOutput = NULL;
@@ -78,7 +114,7 @@ Double_t  RooStats::RatioOfProfiledLikelihoodsTestStat::Evaluate(RooAbsData& dat
          fDetailedOutput->addOwned(*cloneVar);
       }
    }
-   
+
 /*
 // set variables back to where they were
 nullParamsOfInterest = *saveNullPOI;
