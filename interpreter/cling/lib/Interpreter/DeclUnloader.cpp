marsupial/cling

Fix unloading of using namespace declarations. Add test/CodeUnloading/UsingShadows.C

lib/Interpreter/DeclUnloader.cpp
@@ -43,34 +43,39 @@ void DeclUnloader::resetDefinitionData(TagDecl *decl) {
   }
 }
 
+// RedeclLink is a protected member, which we need access to in
+// removeRedeclFromChain (and VisitRedeclarable for checking in debug mode)
+template<typename DeclT>
+struct RedeclDerived : public Redeclarable<DeclT> {
+  typedef typename Redeclarable<DeclT>::DeclLink DeclLink_t;
+  static DeclLink_t& getLink(DeclT* R) {
+    Redeclarable<DeclT>* D = R;
+    return ((RedeclDerived*)D)->RedeclLink;
+  }
+  static void setLatest(DeclT* Latest) {
+    // Convert A -> Latest -> B into A -> Latest
+    getLink(Latest->getFirstDecl()).setLatest(Latest);
+  }
+  static void skipPrev(DeclT* Next) {
+    // Convert A -> B -> Next into A -> Next
+    DeclT* Skip = Next->getPreviousDecl();
+    getLink(Next).setPrevious(Skip->getPreviousDecl());
+  }
+  static void setFirst(DeclT* First) {
+    // Convert A -> First -> B into First -> B
+    DeclT* Latest = First->getMostRecentDecl();
+    getLink(First)
+      = DeclLink_t(DeclLink_t::LatestLink, First->getASTContext());
+    getLink(First).setLatest(Latest);
+  }
+  static DeclT *getNextRedeclaration(DeclT* R) {
+    return getLink(R).getNext(R);
+  }
+};
+
 // Copied and adapted from: ASTReaderDecl.cpp
 template<typename DeclT>
 void DeclUnloader::removeRedeclFromChain(DeclT* R) {
-  //RedeclLink is a protected member.
-  struct RedeclDerived : public Redeclarable<DeclT> {
-    typedef typename Redeclarable<DeclT>::DeclLink DeclLink_t;
-    static DeclLink_t& getLink(DeclT* R) {
-      Redeclarable<DeclT>* D = R;
-      return ((RedeclDerived*)D)->RedeclLink;
-    }
-    static void setLatest(DeclT* Latest) {
-      // Convert A -> Latest -> B into A -> Latest
-      getLink(Latest->getFirstDecl()).setLatest(Latest);
-    }
-    static void skipPrev(DeclT* Next) {
-      // Convert A -> B -> Next into A -> Next
-      DeclT* Skip = Next->getPreviousDecl();
-      getLink(Next).setPrevious(Skip->getPreviousDecl());
-    }
-    static void setFirst(DeclT* First) {
-      // Convert A -> First -> B into First -> B
-      DeclT* Latest = First->getMostRecentDecl();
-      getLink(First)
-        = DeclLink_t(DeclLink_t::LatestLink, First->getASTContext());
-      getLink(First).setLatest(Latest);
-    }
-  };
-
   assert(R != R->getFirstDecl() && "Cannot remove only redecl from chain");
 
   const bool isdef = isDefinition(R);
@@ -80,7 +85,7 @@ void DeclUnloader::removeRedeclFromChain(DeclT* R) {
   DeclT* Prev = R->getPreviousDecl();
   if (R == R->getMostRecentDecl()) {
     // A -> .. -> R
-    RedeclDerived::setLatest(Prev);
+    RedeclDerived<DeclT>::setLatest(Prev);
   } else {
     // Find the next redecl, starting at the end
     DeclT* Next = R->getMostRecentDecl();
@@ -93,11 +98,11 @@ void DeclUnloader::removeRedeclFromChain(DeclT* R) {
 
     if (R->getPreviousDecl()) {
       // A -> .. -> R -> .. -> Z
-      RedeclDerived::skipPrev(Next);
+      RedeclDerived<DeclT>::skipPrev(Next);
     } else {
       assert(R->getFirstDecl() == R && "Logic error");
       // R -> .. -> Z
-      RedeclDerived::setFirst(Next);
+      RedeclDerived<DeclT>::setFirst(Next);
     }
   }
   // If the decl was the definition, the other decl might have their
@@ -182,15 +187,25 @@ bool DeclUnloader::VisitRedeclarable(clang::Redeclarable<T>* R, DeclContext* DC)
 
 #ifndef NDEBUG
   // Validate redecl chain by iterating through it.
-  std::set<clang::Redeclarable<T>*> CheckUnique;
-  (void)CheckUnique;
-  for (auto RD: MostRecentNotThis->redecls()) {
-    assert(CheckUnique.insert(RD).second && "Dupe redecl chain element");
-    (void)RD;
+  // If we get here and T == clang::UsingShadowDecl
+  // for (auto RD: MostRecentNotThis->redecls()) won't work
+  // as the redecl_iterator has an ambigous isFirstDecl call
+  // So we had to pull the guts out of redecl_iterator into here
+
+  std::set<clang::Redeclarable<T>*> Unique;
+  T* Current = MostRecentNotThis;
+  T* Starter = Current;
+
+  while (Current) {
+    assert(Unique.insert(Current).second && "Dupe redecl chain element");
+    Current = RedeclDerived<T>::getNextRedeclaration(Current);
+    if (Current==Starter)
+      Current = nullptr;
   }
 #else
   (void)MostRecentNotThis; // templated function issues a lot -Wunused-variable
 #endif
+
   return true;
 }
 
@@ -479,8 +494,7 @@ bool DeclUnloader::VisitRedeclarable(clang::Redeclarable<T>* R, DeclContext* DC)
   bool DeclUnloader::VisitUsingShadowDecl(UsingShadowDecl* USD) {
     // UsingShadowDecl: NamedDecl, Redeclarable
     bool Successful = true;
-    // FIXME: This is needed when we have newest clang:
-    //Successful = VisitRedeclarable(USD, USD->getDeclContext());
+    Successful = VisitRedeclarable(USD, USD->getDeclContext());
     Successful &= VisitNamedDecl(USD);
 
     // Unregister from the using decl that it shadows.

test/CodeUnloading/UsingShadows.C
@@ -1,0 +1,25 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
+// RUN: cat %s | %cling 2>&1 | FileCheck -allow-empty %s
+
+
+namespace A { void foo(); }
+namespace B { using A::foo; }
+namespace B { using A::foo; }
+.storeState "AB2"
+
+namespace B { using A::foo; }
+namespace B { using A::foo; }
+.undo
+.undo
+
+.compareState "AB2"
+//CHECK-NOT: Differences
+
+.q

root-project/root

interpreter/cling/lib/Interpreter/DeclUnloader.cpp
@@ -42,34 +42,39 @@ void DeclUnloader::resetDefinitionData(TagDecl *decl) {
   }
 }
 
+// RedeclLink is a protected member, which we need access to in
+// removeRedeclFromChain (and VisitRedeclarable for checking in debug mode)
+template<typename DeclT>
+struct RedeclDerived : public Redeclarable<DeclT> {
+  typedef typename Redeclarable<DeclT>::DeclLink DeclLink_t;
+  static DeclLink_t& getLink(DeclT* LR) {
+    Redeclarable<DeclT>* D = LR;
+    return ((RedeclDerived*)D)->RedeclLink;
+  }
+  static void setLatest(DeclT* Latest) {
+    // Convert A -> Latest -> B into A -> Latest
+    getLink(Latest->getFirstDecl()).setLatest(Latest);
+  }
+  static void skipPrev(DeclT* Next) {
+    // Convert A -> B -> Next into A -> Next
+    DeclT* Skip = Next->getPreviousDecl();
+    getLink(Next).setPrevious(Skip->getPreviousDecl());
+  }
+  static void setFirst(DeclT* First) {
+    // Convert A -> First -> B into First -> B
+    DeclT* Latest = First->getMostRecentDecl();
+    getLink(First)
+      = DeclLink_t(DeclLink_t::LatestLink, First->getASTContext());
+    getLink(First).setLatest(Latest);
+  }
+  static DeclT *getNextRedeclaration(DeclT* R) {
+    return getLink(R).getNext(R);
+  }
+};
+
 // Copied and adapted from: ASTReaderDecl.cpp
 template<typename DeclT>
 void DeclUnloader::removeRedeclFromChain(DeclT* R) {
-  //RedeclLink is a protected member.
-  struct RedeclDerived : public Redeclarable<DeclT> {
-    typedef typename Redeclarable<DeclT>::DeclLink DeclLink_t;
-    static DeclLink_t& getLink(DeclT* LR) {
-      Redeclarable<DeclT>* D = LR;
-      return ((RedeclDerived*)D)->RedeclLink;
-    }
-    static void setLatest(DeclT* Latest) {
-      // Convert A -> Latest -> B into A -> Latest
-      getLink(Latest->getFirstDecl()).setLatest(Latest);
-    }
-    static void skipPrev(DeclT* Next) {
-      // Convert A -> B -> Next into A -> Next
-      DeclT* Skip = Next->getPreviousDecl();
-      getLink(Next).setPrevious(Skip->getPreviousDecl());
-    }
-    static void setFirst(DeclT* First) {
-      // Convert A -> First -> B into First -> B
-      DeclT* Latest = First->getMostRecentDecl();
-      getLink(First)
-        = DeclLink_t(DeclLink_t::LatestLink, First->getASTContext());
-      getLink(First).setLatest(Latest);
-    }
-  };
-
   assert(R != R->getFirstDecl() && "Cannot remove only redecl from chain");
 
   const bool isdef = isDefinition(R);
@@ -79,7 +84,7 @@ void DeclUnloader::removeRedeclFromChain(DeclT* R) {
   DeclT* Prev = R->getPreviousDecl();
   if (R == R->getMostRecentDecl()) {
     // A -> .. -> R
-    RedeclDerived::setLatest(Prev);
+    RedeclDerived<DeclT>::setLatest(Prev);
   } else {
     // Find the next redecl, starting at the end
     DeclT* Next = R->getMostRecentDecl();
@@ -92,11 +97,11 @@ void DeclUnloader::removeRedeclFromChain(DeclT* R) {
 
     if (R->getPreviousDecl()) {
       // A -> .. -> R -> .. -> Z
-      RedeclDerived::skipPrev(Next);
+      RedeclDerived<DeclT>::skipPrev(Next);
     } else {
       assert(R->getFirstDecl() == R && "Logic error");
       // R -> .. -> Z
-      RedeclDerived::setFirst(Next);
+      RedeclDerived<DeclT>::setFirst(Next);
     }
   }
   // If the decl was the definition, the other decl might have their
@@ -181,15 +186,25 @@ bool DeclUnloader::VisitRedeclarable(clang::Redeclarable<T>* R, DeclContext* DC)
 
 #ifndef NDEBUG
   // Validate redecl chain by iterating through it.
-  std::set<clang::Redeclarable<T>*> CheckUnique;
-  (void)CheckUnique;
-  for (auto RD: MostRecentNotThis->redecls()) {
-    assert(CheckUnique.insert(RD).second && "Dupe redecl chain element");
-    (void)RD;
+  // If we get here and T == clang::UsingShadowDecl
+  // for (auto RD: MostRecentNotThis->redecls()) won't work
+  // as the redecl_iterator has an ambigous isFirstDecl call
+  // So we had to pull the guts out of redecl_iterator into here
+
+  std::set<clang::Redeclarable<T>*> Unique;
+  T* Current = MostRecentNotThis;
+  T* Starter = Current;
+
+  while (Current) {
+    assert(Unique.insert(Current).second && "Dupe redecl chain element");
+    Current = RedeclDerived<T>::getNextRedeclaration(Current);
+    if (Current==Starter)
+      Current = nullptr;
   }
 #else
   (void)MostRecentNotThis; // templated function issues a lot -Wunused-variable
 #endif
+
   return true;
 }
 
@@ -478,8 +493,7 @@ bool DeclUnloader::VisitRedeclarable(clang::Redeclarable<T>* R, DeclContext* DC)
   bool DeclUnloader::VisitUsingShadowDecl(UsingShadowDecl* USD) {
     // UsingShadowDecl: NamedDecl, Redeclarable
     bool Successful = true;
-    // FIXME: This is needed when we have newest clang:
-    //Successful = VisitRedeclarable(USD, USD->getDeclContext());
+    Successful = VisitRedeclarable(USD, USD->getDeclContext());
     Successful &= VisitNamedDecl(USD);
 
     // Unregister from the using decl that it shadows.

interpreter/cling/test/CodeUnloading/UsingShadows.C
@@ -1,0 +1,33 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
+// RUN: cat %s | %cling 2>&1 | FileCheck -allow-empty %s
+
+
+namespace A { void foo(); }
+.storeState "TESTU1"
+namespace B { using A::foo; }
+.undo
+.compareState "TESTU1"
+// CHECK-NOT: Differences
+
+.storeState "TESTU2"
+namespace B { using A::foo; }
+.undo
+.compareState "TESTU2"
+// CHECK-NOT: Differences
+
+.storeState "TESTU3"
+namespace B { using A::foo; }
+.undo
+
+.compareState "TESTU3"
+// CHECK-NOT: Differences
+
+
+.q