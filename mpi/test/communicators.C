42f9b1698de7bff24ffcdebd23a4fe1bbad63957
Omar Andres Zapata Mesa <Omar.Zapata@cern.ch>
2017-02-12 19:50:22 -0500

    ROOTMpi: improved test for communicators.C

--- mpi/test/communicators.C
+++ mpi/test/communicators.C
@@ -74,8 +74,9 @@ void communicators()
       ocomm.AllReduce(&lrank, &results, 1, SUM);
       assert(results == 4);
    }
-
-//testing TIntraCommunicator
+//////////////////////////////
+//testing TInterCommunicator//
+//////////////////////////////
    auto key = comm.GetRank();
    auto color = comm.GetRank() % 2;
    gcomm = comm.Split(color, key);
@@ -94,7 +95,7 @@ void communicators()
 // std::cout<<icomm.GetRank()<<std::endl;
 // std::cout<<icomm.GetSize()<<std::endl;
 
-   if (rank % 2 == 0) {
+   if (rank % 2 == 0) {//pair ranks
       assert(icomm.GetRank() == 0 || icomm.GetRank() == 1);
       assert(icomm.GetSize() == 2);
 
@@ -102,7 +103,7 @@ void communicators()
 
       if (icomm.GetRank() == 0) icomm.Send(comm.GetSize(), 1, 12); //sending value to the second gruop
 
-   } else {
+   } else {//odd ranks
       assert(icomm.GetRank() == 0 || icomm.GetRank() == 1);
       assert(icomm.GetSize() == 2);
       assert(icomm.GetRemoteSize() == 2);
@@ -114,5 +115,42 @@ void communicators()
       }
    }
 
+   auto intracomm = icomm.Merge(rank % 2); //mergin first the pairs ranks must be {0,2,1,3}
+
+   assert(intracomm != COMM_NULL);
+   assert(intracomm.GetSize() == 4); //must be 4 (the two groups together)
+
+//testing merge the two groups in pair group
+   if (rank % 2 == 0) {
+      assert(intracomm.GetRank() == 0 || intracomm.GetRank() == 1); //the ranks pairs must have 0 and 1 ranks respect a COMM_WORLD
+   } else {
+      assert(intracomm.GetRank() == 2 || intracomm.GetRank() == 3);
+   }
+
+   intracomm = icomm.Merge(rank % 2 == 0 ? 1 : 0); //mergin first the odd ranks must be {2,0,3,1} NOTE: in pair positions odd ranks
+
+   assert(intracomm != COMM_NULL);
+   assert(intracomm.GetSize() == 4); //must be 4 (the two groups together)
+
+//testing merge the two groups in pair group
+   if (rank % 2 == 0) {
+      assert(intracomm.GetRank() == 2 || intracomm.GetRank() == 3); //the ranks pairs must have 3 and 2 ranks respect a COMM_WORLD
+   } else {
+      assert(intracomm.GetRank() == 0 || intracomm.GetRank() == 1);
+   }
+   
+   //clening the objects
+   icomm.Free();
+   pgroup.Free();
+   ogroup.Free();
+
+   assert(icomm==COMM_NULL);
+   assert(pgroup==GROUP_NULL);
+   assert(ogroup==GROUP_NULL);
+   
+//TODO:
+/* test remote group
+ * test Spawn after new design
+ */
 }
 
