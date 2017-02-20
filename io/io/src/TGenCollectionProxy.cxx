54b0abc01a9c8bac70a7ccd9008a0c63b0a945f2
Philippe Canal <pcanal@fnal.gov>
2017-02-20 15:37:03 -0600

    Remove kBOOL_t and thus remove inconsistencies.
    In 2004 kBool_t was the same value as kUChar_t (both 11).
    So when the collection proxy were introduced to distinguished the two, a new
    one was introduced kBOOL_t (value 21) to be used internally by the collection
    proxies.
    
    However this value is returned by TVirtualCollectionProxy::GetType and lead
    to incorrect behaviors, for example:
    
        std::cerr << TDataType::GetDataType(proxy->GetType())->AsString(proxy->At(i)) << "\n";
    
    as the value 21 is in TDataTypes.h the value of kDataTypeAliasUnsigned_t
    
    In 2005 (eab7cae079e), kBool_t was updated in TDataTypes.h (to the value 18)
    to be distinct from kUChar_t as (at the time) sizeof(bool) on MacOS was 4.
    However the code in collection proxy was never modernize to leverage this new
    distinction.
    
    Since the value is only recorded in the CollectionProxy objects and those
    are never persisted, we can safetly switch from kBOOL_t to kBool_t.

--- io/io/src/TGenCollectionProxy.cxx
+++ io/io/src/TGenCollectionProxy.cxx
@@ -390,9 +390,6 @@ TGenCollectionProxy::Value::Value(const std::string& inside_type, Bool_t silent)
          TDataType *fundType = (TDataType *)typeTable->THashTable::FindObject( intype.c_str() );
          if (fundType && fundType->GetType() < 0x17 && fundType->GetType() > 0) {
             fKind = (EDataType)fundType->GetType();
-            if ( 0 == strcmp("bool",fundType->GetFullTypeName()) ) {
-               fKind = (EDataType)kBOOL_t;
-            }
             // R__ASSERT((fKind>0 && fKind<0x17) || (fKind==-1&&(prop&kIsPointer)) );
 
             fCase |= kIsFundamental;
@@ -476,9 +473,6 @@ TGenCollectionProxy::Value::Value(const std::string& inside_type, Bool_t silent)
                      fKind = kInt_t;
                   } else {
                      fKind = (EDataType)fundType->GetType();
-                     if ( 0 == strcmp("bool",fundType->GetFullTypeName()) ) {
-                        fKind = (EDataType)kBOOL_t;
-                     }
                      fSize = gCling->TypeInfo_Size(ti);
                      R__ASSERT((fKind>0 && fKind<0x17) || (fKind==-1&&(prop&kIsPointer)) );
                   }
@@ -746,7 +740,7 @@ TVirtualCollectionProxy* TGenCollectionProxy::Generate() const
          return new TGenBitsetProxy(*this);
       }
       case ROOT::kSTLvector: {
-         if ((*fValue).fKind == (EDataType)kBOOL_t) {
+         if ((*fValue).fKind == kBool_t) {
             return new TGenVectorBoolProxy(*this);
          } else {
             return new TGenVectorProxy(*this);
