5bfd5715bfe6670dc2d25d158a85a1485ca7a372
Philippe Canal <pcanal@fnal.gov>
2017-02-20 15:24:10 -0600

    Test accessing std::bitset and std::vector<bool> directly via a (first gen) Collection Proxy.
    First gen(eration) has oppossed to the 'cloned' collection proxy used by TTree.  The 'cloning' operation for collection proxy actually
    produces a 'more' tuned collection proxy than the original (i.e. TGenVectorProxy as opposed to TGenCollectionProxy).

--- root/io/newstl/execBitset.cxx
+++ root/io/newstl/execBitset.cxx
@@ -1,6 +1,116 @@
-#include <bitset>
-#include "TFile.h"
 #include <iostream>
+
+#include "TClass.h"
+#include "TVirtualCollectionProxy.h"
+#include <bitset>
+
+#ifdef __ROOTCLING__
+#pragma link C++ class std::bitset<64>+;
+#endif
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <cassert>
+
+template <typename Coll> void runContentTest(TVirtualCollectionProxy *proxy, Coll &bs)
+{
+   if (!proxy){
+      std::cerr << "proxy error\n";
+      return;
+   }
+
+   if (proxy->GetProperties() & TVirtualCollectionProxy::kIsEmulated) {
+      std::cerr << "proxy is emulated (this is an error)\n";
+      return;
+   }
+
+
+   // assert some basic properties on proxy
+   // assert(proxy->GetCollectionType() == ROOT::kSTLbitset);
+   assert(proxy->GetIncrement() == 1);
+
+   TVirtualCollectionProxy::TPushPop helper(proxy,(void*)&bs);
+   if(proxy->Size() != bs.size()){
+      std::cerr << "size error; got " << proxy->Size() << "\t expected " << bs.size() << "\n";
+   }
+
+   Coll bs2(bs);
+
+   bool readout;
+   std::cout << "Using 2 push and 2 objects\n";
+   for(unsigned i=0;i<proxy->Size();++i) {
+      TVirtualCollectionProxy::TPushPop helper2(proxy,(void*)&bs2);
+      readout = *((bool*)(proxy->At(i)));
+      std::cout << "# " << i << " " << readout << " vs " <<  bs[i] << "\n";
+      assert( readout == (int)bs[i] );
+      //std::cerr << TDataType::GetDataType(proxy->GetType())->AsString(proxy->At(i)) << "\n";
+   }
+
+   std::cout << "Using the same object (and hence same CollectionProxy Env).\n";
+   for(unsigned i=0;i<proxy->Size();++i) {
+      TVirtualCollectionProxy::TPushPop helper2(proxy,(void*)&bs);
+      readout = *((bool*)(proxy->At(i)));
+      std::cout << "# " << i << " " << readout << " vs " <<  bs[i] << "\n";
+      assert( readout == (int)bs[i] );
+      //std::cerr << TDataType::GetDataType(proxy->GetType())->AsString(proxy->At(i)) << "\n";
+   }
+
+   std::cout << "Relying on the single push.\n";
+   for(unsigned i=0;i<proxy->Size();++i) {
+      readout = *((bool*)(proxy->At(i)));
+      std::cout << "# " << i << " " << readout << " vs " <<  bs[i] << "\n";
+      assert( readout == (int)bs[i] );
+      //std::cerr << TDataType::GetDataType(proxy->GetType())->AsString(proxy->At(i)) << "\n";
+   }
+   TDataType::GetDataType(proxy->GetType())->Print();
+   std::cout << "Using GetDataType AsString.\n";
+   for(unsigned i=0;i<proxy->Size();++i) {
+      readout = *((bool*)(proxy->At(i)));
+      std::cout << "# " << i << " " << readout << " vs " <<  bs[i] << "\n";
+      assert( readout == (int)bs[i] );
+      std::cerr << TDataType::GetDataType(proxy->GetType())->AsString(proxy->At(i)) << "\n";
+   }
+
+}
+
+template <typename Coll> void runtest(Coll &bs)
+{
+   // now get the TCLASS and access bitset via the proxys
+   TClass *cl  = TClass::GetClass(typeid(Coll));
+   if(!cl) {
+      std::cerr << "TClass error\n";
+      return;
+   }
+   // cl->Dump();
+
+   std::unique_ptr<TVirtualCollectionProxy> proxy(cl->GetCollectionProxy()->Generate());
+
+   std::cout << "Run with a copy of the collection proxy\n";
+   runContentTest(proxy.get(), bs);
+
+   std::cout << "Run with the original collection proxy\n";
+   runContentTest(cl->GetCollectionProxy(), bs);
+
+}
+
+void testbitset() {
+
+   std::bitset<64> bs;
+   for(int i=0;i<64;++i)
+      bs[i]=i%2;
+
+   runtest(bs);
+}
+
+void testvectorbool() {
+
+   std::vector<bool> bs;
+   for(int i=0;i<64;++i)
+      bs.push_back(i%2);
+   
+   runtest(bs);
+}
 
 class Something {
 public:
@@ -21,7 +131,9 @@ public:
    }
 };
 
-void execBitset() {
+#include "TFile.h"
+
+void trivialFile() {
    TFile *f = TFile::Open("bitset.root","RECREATE");
    Something s;
    s.Fill(0);
@@ -36,3 +148,8 @@ void execBitset() {
    else ptr->Print();
 }
 
+void execBitset() {
+   trivialFile();
+   testbitset();
+   testvectorbool();
+}
