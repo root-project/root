cb8687ab1e63faa313c6d0b1cba3a40bea876b5e
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-02-20 17:42:27 +0100

    [TDF] test_reduce: also check for support of objects with no def ctor

--- root/dataframe/test_reduce.C
+++ root/dataframe/test_reduce.C
@@ -3,13 +3,28 @@
 #include "TFile.h"
 #include "TTree.h"
 
+class NoDefCtor {
+   int fInt;
+public:
+   NoDefCtor() = delete;
+   NoDefCtor(TRootIOCtor*) {}
+   NoDefCtor(int i) : fInt(i) {}
+   NoDefCtor(const NoDefCtor& o) : fInt(o.fInt) {}
+   void SetInt(int i) { fInt = i; }
+   int GetInt() const { return fInt; }
+};
+
 void FillTree(const char* filename, const char* treeName) {
    TFile f(filename, "RECREATE");
    TTree t(treeName, treeName);
    int i;
+   NoDefCtor o(0);
    t.Branch("i", &i);
-   for(i = 1; i <= 10; ++i)
+   t.Branch("o", &o);
+   for(i = 1; i <= 10; ++i) {
+      o.SetInt(i);
       t.Fill();
+   }
    t.Write();
    f.Close();
 }
@@ -27,11 +42,10 @@ void test_reduce() {
    auto r = d.Reduce([](int a, int b) { return a + b; }, {"i"});
    auto rDefBranch = d.Filter([]() { return true; })
                       .Reduce([](int a, int b) { return a*b; }, {}, 1);
+   auto rNoDefCtor = d.Reduce(
+      [](const NoDefCtor& a, const NoDefCtor& b) { return NoDefCtor(a.GetInt() + b.GetInt()); },
+      {"o"},
+      NoDefCtor(0));
 
-   Info("test_reduce", "%d %d", *r, *rDefBranch);
-}
-
-int main() {
-   test_reduce();
-   return 0;
+   Info("test_reduce", "%d %d %d", *r, *rDefBranch, rNoDefCtor->GetInt());
 }
