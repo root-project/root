115d404ec5da34e3e14f2cacc3c2807877af571a
Pere Mato <pere.mato@cern.ch>
2017-02-22 16:34:33 +0100

    Ignore test tutorial-math-goftest for fix location installations

--- tutorials/CTestCustom.cmake
+++ tutorials/CTestCustom.cmake
@@ -2,3 +2,7 @@
 if(WIN32)
   list(APPEND CTEST_CUSTOM_TESTS_IGNORE tutorial-roofit-rf509_wsinteractive)   #  workspaces as namespace does not work on windows 
 endif()
+
+if("$ENV{MODE}" STREQUAL "install") # libraires installed in fix location <prefix>/lib/root
+  list(APPEND CTEST_CUSTOM_TESTS_IGNORE tutorial-math-goftest)
+endif()
