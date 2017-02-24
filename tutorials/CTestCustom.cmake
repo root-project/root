1c6bc6770f7dd9894e4c6fa84d0bcaa8ebb95cc9
Pere Mato <pere.mato@cern.ch>
2017-02-24 15:11:04 +0100

    Ignoring test in 'classic' using TTreeProcessorM{T,P}

--- tutorials/CTestCustom.cmake
+++ tutorials/CTestCustom.cmake
@@ -2,4 +2,8 @@
 
 set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} tutorial-pyroot-zdemo)
 
+if("$ENV{COMPILER}" STREQUAL "classic") #  TTreeProcessorM{T,P} are not available
+  list(APPEND CTEST_CUSTOM_TESTS_IGNORE tutorial-multicore-mp102_readNtuplesFillHistosAndFit)
+endif()
+
 
