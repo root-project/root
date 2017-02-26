1d7c1e31c05d0ead67f5f02989fee9d81bf0d049
Danilo Piparo <danilo.piparo@cern.ch>
2017-02-26 22:18:04 +0100

    [Doc] Make example in doc real C++

--- hist/hist/src/TProfile2D.cxx
+++ hist/hist/src/TProfile2D.cxx
@@ -54,8 +54,8 @@ ClassImp(TProfile2D)
  Example of a profile2D histogram
  ~~~~{.cpp}
  {
-    TCanvas *c1 = new TCanvas("c1","Profile histogram example",200,10,700,500);
-    hprof2d  = new TProfile2D("hprof2d","Profile of pz versus px and py",40,-4,4,40,-4,4,0,20);
+    auto c1 = new TCanvas("c1","Profile histogram example",200,10,700,500);
+    auto hprof2d  = new TProfile2D("hprof2d","Profile of pz versus px and py",40,-4,4,40,-4,4,0,20);
     Float_t px, py, pz;
     for ( Int_t i=0; i<25000; i++) {
        gRandom->Rannor(px,py);
