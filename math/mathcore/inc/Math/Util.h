5f819428ca47241bbe75afab7759c01091e15770
Xavier Valls <xaviervallspla@gmail.com>
2017-02-27 15:53:46 +0100

    Fix indentation :art:

--- math/mathcore/inc/Math/Util.h
+++ math/mathcore/inc/Math/Util.h
@@ -31,42 +31,43 @@ namespace ROOT {
    namespace Math {
 
 
-/**
-   namespace defining Utility functions needed by mathcore
-*/
-namespace Util {
+      /**
+         namespace defining Utility functions needed by mathcore
+      */
+      namespace Util {
 
-/**
-   Utility function for conversion to strings
-*/
-template<class T>
-std::string ToString(const T& val)
-{
-   std::ostringstream buf;
-   buf << val;
+         /**
+            Utility function for conversion to strings
+         */
+         template<class T>
+         std::string ToString(const T &val)
+         {
+            std::ostringstream buf;
+            buf << val;
 
-   std::string ret = buf.str();
-   return ret;
-}
+            std::string ret = buf.str();
+            return ret;
+         }
 
 
-/// safe evaluation of log(x) with a protections against negative or zero argument to the log
-/// smooth linear extrapolation below function values smaller than  epsilon
-/// (better than a simple cut-off)
-inline double EvalLog(double x) {
-   // evaluate the log
-#ifdef __CINT__
-   static const double epsilon = 2.*2.2250738585072014e-308;
-#else
-   static const double epsilon = 2.*std::numeric_limits<double>::min();
-#endif
-   if(x<= epsilon)
-      return x/epsilon + std::log(epsilon) - 1;
-   else
-      return std::log(x);
-}
+         /// safe evaluation of log(x) with a protections against negative or zero argument to the log
+         /// smooth linear extrapolation below function values smaller than  epsilon
+         /// (better than a simple cut-off)
+         inline double EvalLog(double x)
+         {
+            // evaluate the log
+         #ifdef __CINT__
+            static const double epsilon = 2.*2.2250738585072014e-308;
+         #else
+            static const double epsilon = 2.*std::numeric_limits<double>::min();
+         #endif
+            if (x <= epsilon)
+               return x / epsilon + std::log(epsilon) - 1;
+            else
+               return std::log(x);
+         }
 
-}  // end namespace Util
+      }  // end namespace Util
 
       template<class T>
       class KahanSum {
@@ -105,7 +106,6 @@ inline double EvalLog(double x) {
    } // end namespace Math
 
 } // end namespace ROOT
-
 
 
 #endif /* ROOT_Math_Util */
