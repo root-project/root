a33d5487b6a492266dd1c175a300bb373e492c43
Axel Naumann <Axel.Naumann@cern.ch>
2017-03-14 21:31:13 +0100

    Typo.

--- cling/const/constissue.h
+++ cling/const/constissue.h
@@ -1,7 +1,7 @@
 int get_histogram(char const* , char const* ){return 0; }
 template<class T> void get_histogram(T const & t, char const * name);
 #ifdef __CINT__
-// Can't remember what this is actually testing but it should do
+// Can't remember what this is actually testing but it should not do
 // any harm anyway!
 #pragma link C++ function get_histogram;
 #endif
