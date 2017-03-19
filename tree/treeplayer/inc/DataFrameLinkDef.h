caae4d2cbca85f232e17e819f4cb1130b965609c
Danilo Piparo <danilo.piparo@cern.ch>
2017-03-19 13:33:44 +0100

    [TDF] Trigger autoload for all tdf graph nodes

--- tree/treeplayer/inc/DataFrameLinkDef.h
+++ tree/treeplayer/inc/DataFrameLinkDef.h
@@ -8,6 +8,9 @@
 
 #ifdef __ROOTCLING__
 
+// All these are there for the autoloading
 #pragma link C++ class ROOT::Experimental::TDataFrame-;
+#pragma link C++ class ROOT::Experimental::TDataFrameInterface<ROOT::Detail::TDataFrameFilterBase>-
+#pragma link C++ class ROOT::Experimental::TDataFrameInterface<ROOT::Detail::TDataFrameBranchBase>-
 
 #endif
