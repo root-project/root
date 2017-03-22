a175856805cb212db3e92f2b6a1c5781c465b553
Brian Bockelman <bbockelm@cse.unl.edu>
2017-03-21 22:18:28 -0500

    Add support for bulk serialized IO.
    This allows an appropriately-typed base TLeaf to declare it supports
    serialized IO.
    
    With this, the user can read the serialized data from the branch
    directly and do their own deserialization.
    
    Intent is to use this for contexts where the underlying type is
    primitive and sufficiently basic to inline deserialization.

--- tree/tree/inc/TLeafF.h
+++ tree/tree/inc/TLeafF.h
@@ -50,6 +50,7 @@ public:
    virtual void    SetAddress(void *add=0);
 
    virtual bool    ReadBasketFast(TBuffer&, Long64_t);
+   virtual bool    ReadBasketSerialized(TBuffer&, Long64_t) {return GetDeserializeType() == kInPlace; }
 
    ClassDef(TLeafF,1);  //A TLeaf for a 32 bit floating point data type.
 };
