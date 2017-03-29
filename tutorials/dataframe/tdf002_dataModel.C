c63aefe703bce7f2328649bec4d28d6290d3d85d
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-03-29 14:06:18 +0200

    [TDF] Run clang-format on all TDataFrame tutorials

--- tutorials/dataframe/tdf002_dataModel.C
+++ tutorials/dataframe/tdf002_dataModel.C
@@ -30,13 +30,14 @@ using CylFourVector = ROOT::Math::RhoEtaPhiVector;
 
 // A simple helper function to fill a test tree: this makes the example
 // stand-alone.
-void fill_tree(const char* filename, const char* treeName) {
-   TFile f(filename,"RECREATE");
-   TTree t(treeName,treeName);
+void fill_tree(const char *filename, const char *treeName)
+{
+   TFile f(filename, "RECREATE");
+   TTree t(treeName, treeName);
    FourVectors tracks;
    t.Branch("tracks", &tracks);
 
-   const double M = 0.13957;  // set pi+ mass
+   const double M = 0.13957; // set pi+ mass
    TRandom3 R(1);
 
    for (int i = 0; i < 50; ++i) {
@@ -44,15 +45,15 @@ void fill_tree(const char* filename, const char* treeName) {
       tracks.clear();
       tracks.reserve(nPart);
       for (int j = 0; j < nPart; ++j) {
-         double px = R.Gaus(0,10);
-         double py = R.Gaus(0,10);
-         double pt = sqrt(px*px +py*py);
-         double eta = R.Uniform(-3,3);
-         double phi = R.Uniform(0.0 , 2*TMath::Pi() );
-         CylFourVector vcyl( pt, eta, phi);
+         double px = R.Gaus(0, 10);
+         double py = R.Gaus(0, 10);
+         double pt = sqrt(px * px + py * py);
+         double eta = R.Uniform(-3, 3);
+         double phi = R.Uniform(0.0, 2 * TMath::Pi());
+         CylFourVector vcyl(pt, eta, phi);
          // set energy
-         double E = sqrt( vcyl.R()*vcyl.R() + M*M);
-         FourVector q( vcyl.X(), vcyl.Y(), vcyl.Z(), E);
+         double E = sqrt(vcyl.R() * vcyl.R() + M * M);
+         FourVector q(vcyl.X(), vcyl.Y(), vcyl.Z(), E);
          // fill track vector
          tracks.emplace_back(q);
       }
@@ -64,12 +65,13 @@ void fill_tree(const char* filename, const char* treeName) {
    return;
 }
 
-int tdf002_dataModel() {
+int tdf002_dataModel()
+{
 
    // We prepare an input tree to run on
    auto fileName = "tdf002_dataModel.root";
    auto treeName = "myTree";
-   fill_tree(fileName,treeName);
+   fill_tree(fileName, treeName);
 
    // We read the tree from the file and create a TDataFrame, a class that
    // allows us to interact with the data contained in the tree.
@@ -78,9 +80,8 @@ int tdf002_dataModel() {
    // ## Operating on branches which are collection of objects
    // Here we deal with the simplest of the cuts: we decide to accept the event
    // only if the number of tracks is greater than 5.
-   auto n_cut = [](const FourVectors & tracks) { return tracks.size() > 8; };
-   auto nentries = d.Filter(n_cut, {"tracks"})
-                   .Count();
+   auto n_cut = [](const FourVectors &tracks) { return tracks.size() > 8; };
+   auto nentries = d.Filter(n_cut, {"tracks"}).Count();
 
    std::cout << *nentries << " passed all filters" << std::endl;
 
@@ -88,29 +89,27 @@ int tdf002_dataModel() {
    // quantity we are interested in.
    // In this example, we will cut on the number of tracks and plot their
    // transverse momentum.
-   auto getPt = [](const FourVectors& tracks) {
+   auto getPt = [](const FourVectors &tracks) {
       std::vector<double> pts;
       pts.reserve(tracks.size());
-      for (auto& t:tracks)
-         pts.emplace_back(t.Pt());
+      for (auto &t : tracks) pts.emplace_back(t.Pt());
       return pts;
-      };
+   };
 
    // We do the same for the weights.
-   auto getPtWeights = [](const FourVectors& tracks) {
+   auto getPtWeights = [](const FourVectors &tracks) {
       std::vector<double> ptsw;
       ptsw.reserve(tracks.size());
-      for (auto& t:tracks)
-         ptsw.emplace_back(1./t.Pt());
+      for (auto &t : tracks) ptsw.emplace_back(1. / t.Pt());
       return ptsw;
-      };
+   };
 
-   auto augmented_d = d.AddColumn("tracks_n", [](const FourVectors& tracks){return (int)tracks.size();})
-                       .Filter([](int tracks_n){return tracks_n > 2;}, {"tracks_n"})
-                       .AddColumn("tracks_pts", getPt)
-                       .AddColumn("tracks_pts_weights", getPtWeights);
+   auto augmented_d = d.AddColumn("tracks_n", [](const FourVectors &tracks) { return (int)tracks.size(); })
+                         .Filter([](int tracks_n) { return tracks_n > 2; }, {"tracks_n"})
+                         .AddColumn("tracks_pts", getPt)
+                         .AddColumn("tracks_pts_weights", getPtWeights);
 
-   auto trN = augmented_d.Histo1D("tracks_n",40,-.5,39.5);
+   auto trN = augmented_d.Histo1D("tracks_n", 40, -.5, 39.5);
    auto trPts = augmented_d.Histo1D("tracks_pts");
    auto trWPts = augmented_d.Histo1D<std::vector<double>, std::vector<double>>("tracks_pts", "tracks_pts_weights");
 
@@ -129,7 +128,7 @@ int tdf002_dataModel() {
    return 0;
 }
 
-int main(){
+int main()
+{
    return tdf002_dataModel();
 }
-
--- tutorials/dataframe/tdf004_cutFlowReport.C
+++ tutorials/dataframe/tdf004_cutFlowReport.C
@@ -25,16 +25,17 @@ using CylFourVector = ROOT::Math::RhoEtaPhiVector;
 
 // A simple helper function to fill a test tree: this makes the example
 // stand-alone.
-void fill_tree(const char* filename, const char* treeName) {
-   TFile f(filename,"RECREATE");
-   TTree t(treeName,treeName);
+void fill_tree(const char *filename, const char *treeName)
+{
+   TFile f(filename, "RECREATE");
+   TTree t(treeName, treeName);
    double b1;
    int b2;
    t.Branch("b1", &b1);
    t.Branch("b2", &b2);
-   for(int i = 0; i < 50; ++i) {
+   for (int i = 0; i < 50; ++i) {
       b1 = i;
-      b2 = i*i;
+      b2 = i * i;
       t.Fill();
    }
    t.Write();
@@ -42,12 +43,13 @@ void fill_tree(const char* filename, const char* treeName) {
    return;
 }
 
-void tdf004_cutFlowReport() {
+void tdf004_cutFlowReport()
+{
 
    // We prepare an input tree to run on
    auto fileName = "tdf004_cutFlowReport.root";
    auto treeName = "myTree";
-   fill_tree(fileName,treeName);
+   fill_tree(fileName, treeName);
 
    // We read the tree from the file and create a TDataFrame
    ROOT::Experimental::TDataFrame d(treeName, fileName, {"b1", "b2"});
@@ -62,22 +64,26 @@ void tdf004_cutFlowReport() {
    auto filtered1 = d.Filter(cut1, {"b1"}, "Cut1");
    auto filtered2 = d.Filter(cut2, {"b2"}, "Cut2");
 
-   auto augmented1 = filtered2.AddColumn("b3", [](double b1, int b2){ return b1/b2;});
+   auto augmented1 = filtered2.AddColumn("b3", [](double b1, int b2) { return b1 / b2; });
    auto cut3 = [](double x) { return x < .5; };
    auto filtered3 = augmented1.Filter(cut3, {"b3"}, "Cut3");
 
    // Statistics are retrieved through a call to the Report method:
-   // when Report is called on the main TDataFrame object, it prints stats for all named filters declared up to that point
-   // when called on a stored chain state (i.e. a chain/graph node), it prints stats for all named filters in the section
+   // when Report is called on the main TDataFrame object, it prints stats for all named filters declared up to that
+   // point
+   // when called on a stored chain state (i.e. a chain/graph node), it prints stats for all named filters in the
+   // section
    // of the chain between the main TDataFrame and that node (included).
-   // Stats are printed in the same order as named filters have been added to the graph, and refer to the latest event-loop that has been run using the relevant TDataFrame.
+   // Stats are printed in the same order as named filters have been added to the graph, and refer to the latest
+   // event-loop that has been run using the relevant TDataFrame.
    std::cout << "Cut3 stats:" << std::endl;
    filtered3.Report();
    std::cout << "All stats:" << std::endl;
    d.Report();
 }
 
-int main(){
+int main()
+{
    tdf004_cutFlowReport();
    return 0;
 }
