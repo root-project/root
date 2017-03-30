80da89296e943558b04d6186c2003e876b36be50
Frederich Munch <marsupial@users.noreply.github.com>
2017-03-30 03:46:13 -0400

    Pass mangled name to CodeGenerator::forgetDecl. From Roman Zulak and me.
    Ignore mangling errors on Windows; fixes -Xclang -verify on Windows.

--- interpreter/llvm/src/tools/clang/lib/CodeGen/ModuleBuilder.cpp
+++ interpreter/llvm/src/tools/clang/lib/CodeGen/ModuleBuilder.cpp
@@ -289,8 +289,7 @@ namespace clang {
       }
     }
 
-    void forgetDecl(const GlobalDecl& GD) {
-      StringRef MangledName = Builder->getMangledName(GD);
+    void forgetDecl(const GlobalDecl& GD, llvm::StringRef MangledName) {
       Builder->DeferredDecls.erase(MangledName);
     }
 
@@ -496,8 +495,9 @@ void CodeGenerator::forgetGlobal(llvm::GlobalValue* GV) {
   static_cast<CodeGeneratorImpl*>(this)->forgetGlobal(GV);
 }
 
-void CodeGenerator::forgetDecl(const GlobalDecl& GD) {
-  static_cast<CodeGeneratorImpl*>(this)->forgetDecl(GD);
+void CodeGenerator::forgetDecl(const GlobalDecl& GD,
+                               llvm::StringRef MangledName) {
+  static_cast<CodeGeneratorImpl*>(this)->forgetDecl(GD, MangledName);
 }
 
 
