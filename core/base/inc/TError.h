419c9b295ec6fce6f71946ece4d9ab7e0c25fead
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-03-30 15:00:34 +0200

    Keep clang-tidy modernize-use-bool-literals happy (#472)
    * Keep clang-tidy modernize-use-bool-literals happy
    
    clang-tidy with `modernize-use-bool-literals` complains about the fact that 0 is used in place of false. Given I assume there is no particular reason for using `0` rather than `false`, I suggest to change it to avoid the false positives.
    
    * Align TError.h to ROOT coding conventions

--- core/base/inc/TError.h
+++ core/base/inc/TError.h
@@ -93,14 +93,14 @@ extern void Obsolete(const char *function, const char *asOfVers, const char *rem
 R__EXTERN const char *kAssertMsg;
 R__EXTERN const char *kCheckMsg;
 
-#define R__ASSERT(e) \
-   do { \
+#define R__ASSERT(e)                                                     \
+   do {                                                                  \
       if (!(e)) ::Fatal("", kAssertMsg, _QUOTE_(e), __LINE__, __FILE__); \
-   } while (0)
-#define R__CHECK(e) \
-   do { \
+   } while (false)
+#define R__CHECK(e)                                                       \
+   do {                                                                   \
       if (!(e)) ::Warning("", kCheckMsg, _QUOTE_(e), __LINE__, __FILE__); \
-   } while (0)
+   } while (false)
 
 R__EXTERN Int_t  gErrorIgnoreLevel;
 R__EXTERN Int_t  gErrorAbortLevel;
