2f7c41ae1835897868eefd407cb24f849fef2586
Axel Naumann <Axel.Naumann@cern.ch>
2017-03-31 20:54:44 +0200

    Clear also EmittedDeferredDecls.

--- interpreter/llvm/src/tools/clang/lib/CodeGen/CodeGenModule.cpp
+++ interpreter/llvm/src/tools/clang/lib/CodeGen/CodeGenModule.cpp
@@ -353,6 +353,7 @@ void CodeGenModule::checkAliases() {
 
 void CodeGenModule::clear() {
   DeferredDeclsToEmit.clear();
+  EmittedDeferredDecls.clear();
   if (OpenMPRuntime)
     OpenMPRuntime->clear();
 }
