49b3f706731e1d0368a5360c1076386f1a695ca2
Danilo Piparo <danilo.piparo@cern.ch>
2017-04-01 17:03:15 +0200

    [IO] Increase coverage for ROOT-8736

--- root/io/stdarray/arrayHolder.h
+++ root/io/stdarray/arrayHolder.h
@@ -6,19 +6,20 @@
 #include <string>
 #include <sstream>
 
-class ArrayHolder {
+template<class T = int>
+class ArrayHolderT {
   public:
-   void Set(int a, int b, int c) {
+   void Set(T a, T b, T c) {
       m_a[0]=a;m_a[1]=b;m_a[2]=c;
       for (auto& v : {a,b,c}) {
          auto vprime=v+.5;
          m_v.emplace_back(vprime);
       }
    };
-    ArrayHolder(int a, int b, int c) {
+    ArrayHolderT(T a, T b, T c) {
        Set(a,b,c);
    };
-   ArrayHolder() {};
+   ArrayHolderT() {};
    std::string ToString() {
       std::stringstream ss;
       for (auto&& el : m_a) ss << el << "-";
@@ -28,11 +29,13 @@ class ArrayHolder {
    }
     std::vector<double> m_v {10,11};
 #ifdef ARRAYHOLDER_STDARRAY
-    std::array<int, 3> m_a {{0,0,0}};
+    std::array<T, 3> m_a {{0,0,0}};
 #else
-    int m_a[3] {0,0,0};
+    T m_a[3] {0,0,0};
 #endif
 };
+
+using ArrayHolder = ArrayHolderT<int>;
 
 class MetaArrayHolder {
   public:
@@ -80,7 +83,7 @@ private:
 
 
 #ifdef __ROOTCLING__
-#pragma link C++ class ArrayHolder+;
+#pragma link C++ class ArrayHolderT<int>+;
 #pragma link C++ class MetaArrayHolder+;
 #pragma link C++ class MetaArrayHolder2+;
 #pragma link C++ class vector<MetaArrayHolder2>+;
--- root/io/stdarray/namesAnalysis.C
+++ root/io/stdarray/namesAnalysis.C
@@ -2,14 +2,19 @@ void namesAnalysis() {
    string typenamebuf;
    int ndim;
    std::array<int,5> maxi {0,0,0,0,0};
-   auto names = {"array<int,3>",
+   auto names = {"C<array<bool,4>,array<int,5>>",
+                 "array<int,3>",
                  "array<array<T,4>,3>",
                  "array<array<array<L,16>,4>,3>",
                  "array<array<array<array<double,13>,16>,4>,3>",
-                 "array<array<array<array<array<c,18>,13>,16>,4>,3>"};
+                 "array<array<array<array<array<c,18>,13>,16>,4>,3>",
+                 "array<A<int>,12>",
+                 "array<array<A<array<int,6>>,4>,3>"};
    for (auto& name : names) {
+      maxi = {0,0,0,0,0};
       if(! TClassEdit::GetStdArrayProperties(name,typenamebuf,maxi,ndim)){
-         cerr << "ERROR: class \"" << name << "\" is not an STL array\n";
+         cout << "Class \"" << name << "\" is not an STL array\n";
+         continue;
       }
       cout << name << ":" << endl
            << " - typename: " << typenamebuf << endl
