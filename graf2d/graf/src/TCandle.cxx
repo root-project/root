121286a58cff2126658c4f755672c923e0db982f
Olivier Couet <olivier.couet@cern.ch>
2017-04-03 09:47:49 +0200

    Fix two coverity reports: - fAxisMin and fAxisMax were not initialised - the vector quantiles was not deleted in some cases

--- graf2d/graf/src/TCandle.cxx
+++ graf2d/graf/src/TCandle.cxx
@@ -52,6 +52,8 @@ TCandle::TCandle()
    fLogY          = 0;
    fNDrawPoints   = 0;
    fNHistoPoints  = 0;
+   fAxisMin       = 0.;
+   fAxisMax       = 0.;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -81,7 +83,8 @@ TCandle::TCandle(const Double_t candlePos, const Double_t candleWidth, Long64_t 
    fLogY          = 0;
    fNDrawPoints   = 0;
    fNHistoPoints  = 0;
-
+   fAxisMin       = 0.;
+   fAxisMax       = 0.;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -280,8 +283,8 @@ void TCandle::Calculate() {
 
    // Check if the quantiles are valid, seems the under- and overflow is taken
    // into account as well, we need to ignore this!
-   if (quantiles[0] >= quantiles[4]) return;
-   if (quantiles[1] >= quantiles[3]) return;
+   if (quantiles[0] >= quantiles[4]) { delete [] quantiles; return;}
+   if (quantiles[1] >= quantiles[3]) { delete [] quantiles; return;}
 
    // Definition of the candle in the standard case
    fBoxUp = quantiles[3];
