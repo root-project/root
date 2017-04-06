d8617c7815eae4d29e6c70d977a1dce3dc9b5509
moneta <lorenzo.moneta@cern.ch>
2017-04-06 16:20:48 +0200

    Restore some tests for histogram fitting

--- math/mathcore/test/fit/testFit.cxx
+++ math/mathcore/test/fit/testFit.cxx
@@ -929,8 +929,8 @@ int testFit(Test t, std::string name) {
 int main() {
 
    int iret = 0;
-  //  iret |= testFit( testHisto1DFit, "Histogram1D Fit");
-  //  iret |= testFit( testHisto1DPolFit, "Histogram1D Polynomial Fit");
+   iret |= testFit( testHisto1DFit, "Histogram1D Fit");
+   iret |= testFit( testHisto1DPolFit, "Histogram1D Polynomial Fit");
    iret |= testFit( testHisto2DFit, "Histogram2D Gradient Fit");
    iret |= testFit( testUnBin1DFit, "Unbin 1D Fit");
    iret |= testFit( testGraphFit, "Graph 1D Fit");
