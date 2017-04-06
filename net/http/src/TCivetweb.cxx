45cc5270ce65fbc32fc12bfef89bc3d2119d160c
Sergey Linev <S.Linev@gsi.de>
2017-04-06 10:45:44 +0200

    http: provide websocket id for all related operations
    One could have many web-sockets connections associated with given URL.
    websocket id allows to distinguish them

--- net/http/src/TCivetweb.cxx
+++ net/http/src/TCivetweb.cxx
@@ -37,6 +37,12 @@ class TCivetwebWSEngine : public THttpWSEngine {
       {
       }
 
+      virtual void* GetId() const
+      {
+         return (void *) fWSconn;
+      }
+
+
       virtual void ClearHandle()
       {
          fWSconn = 0;
@@ -68,6 +74,7 @@ int websocket_connect_handler(const struct mg_connection *conn, void*)
    THttpCallArg arg;
    arg.SetPathAndFileName(request_info->uri); // path and file name
    arg.SetQuery(request_info->query_string);  // query arguments
+   arg.SetWSId((void *) conn);
    arg.SetMethod("WS_CONNECT");
 
    Bool_t execres = serv->ExecuteHttp(&arg);
@@ -95,6 +102,7 @@ void websocket_ready_handler(struct mg_connection *conn, void*)
    arg.SetQuery(request_info->query_string);  // query arguments
    arg.SetMethod("WS_READY");
 
+   arg.SetWSId((void *) conn);
    arg.SetWSHandle(new TCivetwebWSEngine("websocket", "title", conn));
 
    serv->ExecuteHttp(&arg);
@@ -114,6 +122,7 @@ int websocket_data_handler(struct mg_connection *conn, int, char *data, size_t l
    THttpCallArg arg;
    arg.SetPathAndFileName(request_info->uri); // path and file name
    arg.SetQuery(request_info->query_string);  // query arguments
+   arg.SetWSId((void *) conn);
    arg.SetMethod("WS_DATA");
 
    void* buf = malloc(len+1); // one byte more for null-termination
@@ -152,6 +161,7 @@ void websocket_close_handler(const struct mg_connection *conn, void*)
    THttpCallArg arg;
    arg.SetPathAndFileName(request_info->uri); // path and file name
    arg.SetQuery(request_info->query_string);  // query arguments
+   arg.SetWSId((void *) conn);
    arg.SetMethod("WS_CLOSE");
 
    serv->ExecuteHttp(&arg);
