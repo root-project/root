fcb088528ddb51b13272204f6ad00881f34877c0
Filip Ilic <filip.ilic@cern.ch>
2017-04-12 17:33:56 +0200

    Extend TH2Poly, TH2PolyBin

--- hist/hist/inc/TH2Poly.h
+++ hist/hist/inc/TH2Poly.h
@@ -71,7 +71,7 @@ public:
    TH2Poly(const char *name,const char *title, Int_t nX, Double_t xlow, Double_t xup,  Int_t nY, Double_t ylow, Double_t yup);
    virtual ~TH2Poly();
 
-   Int_t        AddBin(TObject *poly);
+   virtual   Int_t        AddBin(TObject *poly);
    Int_t        AddBin(Int_t n, const Double_t *x, const Double_t *y);
    Int_t        AddBin(Double_t x1, Double_t y1, Double_t x2, Double_t  y2);
    virtual Bool_t Add(const TH1 *h1, Double_t c1);
@@ -80,9 +80,9 @@ public:
    void         ClearBinContents();                 // Clears the content of all bins
    TObject     *Clone(const char* newname = "") const;
    void         ChangePartition(Int_t n, Int_t m);  // Sets the number of partition cells to another value
-   Int_t        Fill(Double_t x,Double_t y);
-   Int_t        Fill(Double_t x,Double_t y, Double_t w);
-   Int_t        Fill(const char* name, Double_t w);
+   virtual Int_t        Fill(Double_t x,Double_t y);
+   virtual Int_t        Fill(Double_t x,Double_t y, Double_t w);
+   virtual Int_t        Fill(const char* name, Double_t w);
    void         FillN(Int_t ntimes, const Double_t* x, const Double_t* y, const Double_t* w, Int_t stride = 1);
    Int_t        Fill(Double_t){return -1;}                              //MayNotUse
    Int_t        Fill(Double_t , const char *, Double_t){return -1;}     //MayNotUse
@@ -154,5 +154,54 @@ protected:
 
    ClassDef(TH2Poly,1)  //2-Dim histogram with polygon bins
  };
+
+
+//////////////////TProfile2PolyBin.h/////////////////////////
+
+class TProfile2PolyBin: public TH2PolyBin {
+public:
+    TProfile2PolyBin();
+    TProfile2PolyBin(TObject* poly, Int_t bin_number);
+
+    virtual ~TProfile2PolyBin() {}
+
+    void UpdateAverage();
+
+    Double_t getFSumV() const       { return fSumV;   }
+    Double_t getFSumV2() const      { return fSumV2;  }
+    Double_t getFSumVW() const      { return fSumVW;  }
+    Double_t getFSumVW2() const     { return fSumVW2; }
+    Double_t getFNumEntries() const { return fSumVW2; }
+
+public:
+    Double_t fSumV;
+    Double_t fSumV2;
+    Double_t fSumVW;
+    Double_t fSumVW2;
+    Double_t fNumEntries;
+};
+
+//////////////////TProfile2Poly.h/////////////////////////
+
+class TProfile2Poly: public TH2Poly {
+public:
+    TProfile2Poly(){}
+    TProfile2Poly(const char *name, const char *title,
+                  Double_t xlow, Double_t xup,
+                  Double_t ylow, Double_t yup);
+
+    TProfile2Poly(const char *name,const char *title,
+                  Int_t nX, Double_t xlow, Double_t xup,
+                  Int_t nY, Double_t ylow, Double_t yup);
+
+    virtual ~TProfile2Poly() {}
+
+    using TH2Poly::AddBin;
+    virtual Int_t AddBin(TObject *poly) override;
+
+    using TH2Poly::Fill;
+    virtual Int_t Fill(Double_t xcoord, Double_t ycoord, Double_t value) override;
+    virtual Int_t Fill(Double_t xcoord, Double_t ycoord, Double_t value, Double_t weight);
+};
 
 #endif
--- hist/hist/src/TH2Poly.cxx
+++ hist/hist/src/TH2Poly.cxx
@@ -1489,3 +1489,155 @@ Bool_t TH2PolyBin::IsInside(Double_t x, Double_t y) const
 
    return in;
 }
+
+
+
+//////////////////TProfile2PolyBin.cpp/////////////////////////
+
+TProfile2PolyBin::TProfile2PolyBin() {
+    fSumV = 0;
+    fSumV2 = 0;
+    fSumVW = 0;
+    fSumVW2 = 0;
+
+    fNumEntries = 0;
+}
+
+TProfile2PolyBin::TProfile2PolyBin(TObject* poly, Int_t bin_number)
+    : TH2PolyBin(poly, bin_number){
+    fSumV = 0;
+    fSumV2 = 0;
+    fSumVW = 0;
+    fSumVW2 = 0;
+
+    fNumEntries = 0;
+}
+
+
+//////////////////TProfile2Poly.cpp/////////////////////////
+
+TProfile2Poly::TProfile2Poly(const char *name, const char *title,
+                             Double_t xlow, Double_t xup,
+                             Double_t ylow, Double_t yup)
+    : TH2Poly(name, title, xlow, xup, ylow, yup) {}
+
+TProfile2Poly::TProfile2Poly(const char *name, const char *title,
+                             Int_t nX, Double_t xlow, Double_t xup,
+                             Int_t nY, Double_t ylow, Double_t yup)
+    : TH2Poly(name, title, nX, xlow, xup, nY, ylow, yup) {}
+
+Int_t TProfile2Poly::AddBin(TObject *poly)
+{
+    if (!poly) return 0;
+
+    if (fBins == 0) {
+       fBins = new TList();
+       fBins->SetOwner();
+    }
+
+    fNcells++;
+    Int_t ibin = fNcells-kNOverflow;
+    TProfile2PolyBin *bin = new TProfile2PolyBin(poly, ibin);
+
+    // If the bin lies outside histogram boundaries, then extends the boundaries.
+    // Also changes the partition information accordingly
+    Bool_t flag = kFALSE;
+    if (fFloat) {
+       if (fXaxis.GetXmin() > bin->GetXMin()) {
+          fXaxis.Set(100, bin->GetXMin(), fXaxis.GetXmax());
+          flag = kTRUE;
+       }
+       if (fXaxis.GetXmax() < bin->GetXMax()) {
+          fXaxis.Set(100, fXaxis.GetXmin(), bin->GetXMax());
+          flag = kTRUE;
+       }
+       if (fYaxis.GetXmin() > bin->GetYMin()) {
+          fYaxis.Set(100, bin->GetYMin(), fYaxis.GetXmax());
+          flag = kTRUE;
+       }
+       if (fYaxis.GetXmax() < bin->GetYMax()) {
+          fYaxis.Set(100, fYaxis.GetXmin(), bin->GetYMax());
+          flag = kTRUE;
+       }
+       if (flag) ChangePartition(fCellX, fCellY);
+    } else {
+       /*Implement polygon clipping code here*/
+    }
+
+    fBins->Add((TObject*) bin);
+    SetNewBinAdded(kTRUE);
+
+    // Adds the bin to the partition matrix
+    AddBinToPartition(bin);
+
+    return ibin;
+}
+
+Int_t TProfile2Poly::Fill(Double_t xcoord, Double_t ycoord, Double_t value){
+    return Fill(xcoord, ycoord, value, 1);
+}
+
+Int_t TProfile2Poly::Fill(Double_t xcoord, Double_t ycoord, Double_t value, Double_t weight) {
+
+    //TODO: is this correct?
+    if (fNcells <= kNOverflow) return 0;
+    Int_t overflow = 0;
+    if      (ycoord > fYaxis.GetXmax()) overflow += -1;
+    else if (ycoord > fYaxis.GetXmin()) overflow += -4;
+    else                           overflow += -7;
+    if      (xcoord > fXaxis.GetXmax()) overflow += -2;
+    else if(xcoord > fXaxis.GetXmin())  overflow += -1;
+    if (overflow != -5) {
+        fOverflow[-overflow - 1]+= weight;
+        if (fSumw2.fN) fSumw2.fArray[-overflow - 1] += weight*weight;
+        return overflow;
+    }
+
+    // Finds the cell (x,y) coordinates belong to
+    Int_t n = (Int_t)(floor((xcoord-fXaxis.GetXmin())/fStepX));
+    Int_t m = (Int_t)(floor((ycoord-fYaxis.GetXmin())/fStepY));
+
+    // Make sure the array indices are correct.
+    if (n>=fCellX) n = fCellX-1;
+    if (m>=fCellY) m = fCellY-1;
+    if (n<0)       n = 0;
+    if (m<0)       m = 0;
+
+    //TODO: is this correct?
+    if (fIsEmpty[n+fCellX*m]) {
+        fOverflow[4]+= weight;
+        if (fSumw2.fN) fSumw2.fArray[4] += weight*weight;
+        return -5;
+    }
+
+    TProfile2PolyBin *bin;
+
+    TIter next(&fCells[n+fCellX*m]);
+    TObject *obj;
+
+    while ((obj=next())) {
+        bin  = (TProfile2PolyBin*)obj;
+        if (bin->IsInside(xcoord,ycoord)) {
+
+            fEntries++;
+
+            bin->fNumEntries++;
+            bin->fSumV += value;
+            bin->fSumVW += value*weight;
+
+            bin->UpdateAverage();
+
+            return bin->GetBinNumber();
+        }
+    }
+
+    fOverflow[4]+= weight;
+    if (fSumw2.fN) fSumw2.fArray[4] += weight*weight*value;
+    return -5;
+}
+
+void TProfile2PolyBin::UpdateAverage(){
+    fContent =  fSumV / fNumEntries;
+    SetChanged(true);
+}
+
