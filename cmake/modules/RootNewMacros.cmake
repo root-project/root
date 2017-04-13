461158ba4ab56e4fdd6723f023d0abd1118c1494
Raphael Isemann <teemperor@gmail.com>
2017-04-13 15:54:06 +0200

    PCM generation now uses the ROOT modulemap to build modules.
    This patch also untangles the generation code of PCMs and PCHs.
    Additionally we enable rootcling warnings during module generation because
    want them in the future when we try to adjust the working ROOT modulemap
    to the dictionary layout (i.e. fixing libCore and libThread).

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -376,9 +376,15 @@ function(ROOT_GENERATE_DICTIONARY dictionary)
     set(excludepathsargs ${excludepathsargs} -excludePath ${excludepath})
   endforeach()
 
+  set(genverbosity "")
+  # Set -v2 when generating modules in cxxmodules mode to get warnings if the
+  # modulemap doesn't fit to the structure of our dictionaries.
+  if (cxxmodules)
+      set(genverbosity "-v2")
+  endif(cxxmodules)
   #---call rootcint------------------------------------------
   add_custom_command(OUTPUT ${dictionary}.cxx ${pcm_name} ${rootmap_name}
-                     COMMAND ${command} -f  ${dictionary}.cxx ${newargs} ${excludepathsargs} ${rootmapargs}
+                     COMMAND ${command} ${genverbosity} -f  ${dictionary}.cxx ${newargs} ${excludepathsargs} ${rootmapargs}
                                         ${ARG_OPTIONS} ${definitions} ${includedirs} ${headerfiles} ${_linkdef}
                      IMPLICIT_DEPENDS CXX ${_linkdef} ${fullheaderfiles}
                      DEPENDS ${fullheaderfiles} ${_linkdef} ${ROOTCINTDEP})
