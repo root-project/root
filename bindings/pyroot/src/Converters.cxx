9112aa194e55df8fd96e0f8aace3e6f027212a8f
Pere Mato <peremato@users.noreply.github.com>
2017-04-20 23:41:46 +0200

    Fix for ROOT-8732 (#517)
    * Fix for ROOT-8732 - PyROOT cannot read/write C++ class fields of Double32_t
    
    * Try to please clang-format
    
    * Again :-(

--- bindings/pyroot/src/Converters.cxx
+++ bindings/pyroot/src/Converters.cxx
@@ -1541,6 +1541,7 @@ namespace {
 // converter factories for ROOT types
    typedef std::pair< const char*, ConverterFactory_t > NFp_t;
 
+   // clang-format off
    NFp_t factories_[] = {
    // factories for built-ins
       NFp_t( "bool",                      &CreateBoolConverter               ),
@@ -1621,8 +1622,10 @@ namespace {
       NFp_t( "void**",                    &CreateVoidPtrPtrConverter         ),
       NFp_t( "PyObject*",                 &CreatePyObjectConverter           ),
       NFp_t( "_object*",                  &CreatePyObjectConverter           ),
-      NFp_t( "FILE*",                     &CreateVoidArrayConverter          )
+      NFp_t( "FILE*",                     &CreateVoidArrayConverter          ),
+      NFp_t( "Double32_t",                &CreateDoubleConverter             )
    };
+   // clang-format on
 
    struct InitConvFactories_t {
    public:
