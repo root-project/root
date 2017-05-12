f5769da51559a8bb65cc3c9434cf08d53c02f89d
Olivier Couet <olivier.couet@cern.ch>
2017-05-12 15:49:15 +0200

    small doc update

--- core/base/src/TObject.cxx
+++ core/base/src/TObject.cxx
@@ -273,7 +273,7 @@ void TObject::DrawClass() const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Draw a clone of this object in the current pad
+/// Draw a clone of this object in the current selected pad
 
 TObject *TObject::DrawClone(Option_t *option) const
 {
