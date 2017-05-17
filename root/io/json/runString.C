cf466b87f0a8bfdee07021dd438793aa1226f393
Sergey Linev <S.Linev@gsi.de>
2017-05-17 09:50:33 +0200

    json: add testing for string types

---
+++ root/io/json/runString.C
@@ -0,0 +1,28 @@
+{
+// Fill out the code of the actual test
+#ifndef SECOND_RUN
+   gROOT->ProcessLine(".L test_classes.h+");
+#endif
+
+#if defined(ClingWorkAroundMissingDynamicScope) && !defined(SECOND_RUN)
+#define SECOND_RUN
+   gROOT->ProcessLine(".x runString.C");
+#else
+
+   TJsonEx4* ex4 = new TJsonEx4(true);
+
+   cout << " ====== string data types TJsonEx4 ===== " << endl;
+   cout << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4")) << endl << endl;
+   cout << " ============ selected data members ======== " << endl;
+   cout << "ex4.fStr1 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr1") << endl;
+   cout << "ex4.fStr2 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr2") << endl;
+   cout << "ex4.fStr3 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr3") << endl;
+   cout << "ex4.fStr4 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr4") << endl;
+
+#endif
+#ifdef ClingWorkAroundBrokenUnnamedReturn
+   gApplication->Terminate(0);
+#else
+   return 0;
+#endif
+}
