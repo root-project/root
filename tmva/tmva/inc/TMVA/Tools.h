356512f988e1e583272a1bf069d9507ed62405bf
David <David.Lange@cern.ch>
2017-05-18 18:25:54 +0200

    try again on clang fixes

--- tmva/tmva/inc/TMVA/Tools.h
+++ tmva/tmva/inc/TMVA/Tools.h
@@ -250,9 +250,9 @@ namespace TMVA {
       template<typename T>
          inline void ReadAttr    ( void* node, const char* , T& value );
       void        ReadAttr    ( void* node, const char* attrname, TString& value );
-      void ReadAttr(void *node, const char*, float& value);
-      void ReadAttr(void *node, const char*, int& value);  
-      void ReadAttr(void *node, const char*, short& value);
+      void ReadAttr(void *node, const char *, float &value);
+      void ReadAttr(void *node, const char *, int &value);
+      void ReadAttr(void *node, const char *, short &value);
 
       template<typename T>
          void        AddAttr     ( void* node, const char* , const T& value, Int_t precision = 16 );
@@ -293,7 +293,7 @@ template<typename T> void TMVA::Tools::ReadAttr( void* node, const char* attrnam
    const char *val = xmlengine().GetAttr(node, attrname);
    if (val == 0) {
       const char *nodename = xmlengine().GetNodeName(node);
-      Log() << kFATAL << "Trying to read non-existing attribute '" << attrname << "' from xml node '" << nodename << "'" 
+      Log() << kFATAL << "Trying to read non-existing attribute '" << attrname << "' from xml node '" << nodename << "'"
             << Endl;
    }
    std::stringstream s(val);
