eb41710e8801e671486354d2ba8f43a3abd18a83
Pere Mato <pere.mato@cern.ch>
2017-05-19 10:49:38 +0200

    Fix warning

--- test/testGenVectorVc.cxx
+++ test/testGenVectorVc.cxx
@@ -21,8 +21,8 @@
 int compare(double v1, double v2, const std::string &name = "", double scale = 1000.0)
 {
    //  ntest = ntest + 1;
-#ifdef __clang__ && __FAST_MATH__
-      scale *= 100;
+#if defined(__clang__) && defined(__FAST_MATH__)
+   scale *= 100;
 #endif
    // numerical double limit for epsilon
    const double eps     = scale * std::numeric_limits<double>::epsilon();
