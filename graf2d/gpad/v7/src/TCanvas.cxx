bace7d1e3ce09bafff2f86f064ff63e49c6942ad
Axel Naumann <Axel.Naumann@cern.ch>
2017-06-01 10:42:40 +0200

    The TCanvas does not paint itself anymore; use the TVirtualCanvasPainter for that.
    It's a virtual interface with a Generator type that can create TVirtualCanvasPainter objects
    coming out of a plugin library. That plugin library will be libROOTCanvasPainter.

--- graf2d/gpad/v7/src/TCanvas.cxx
+++ graf2d/gpad/v7/src/TCanvas.cxx
@@ -15,12 +15,6 @@
 
 #include "ROOT/TCanvas.hxx"
 
-#include "ROOT/TDrawable.hxx"
-#include "TCanvas.h"
-#include "TROOT.h"
-
-#include "ROOT/TLogger.hxx"
-
 #include <memory>
 
 namespace {
@@ -29,60 +23,12 @@ std::vector<std::shared_ptr<ROOT::Experimental::TCanvas>>& GetHeldCanvases() {
   static std::vector<std::shared_ptr<ROOT::Experimental::TCanvas>> sCanvases;
   return sCanvases;
 }
-
 }
 
-namespace ROOT {
-namespace Experimental {
-namespace Internal {
-
-class TV5CanvasAdaptor: public TObject {
-  ROOT::Experimental::TCanvas& fNewCanv;
-  ::TCanvas* fOldCanv; // ROOT owns them.
-
-public:
-  /// Construct an old TCanvas, append TV5CanvasAdaptor to its primitives.
-  /// That way, TV5CanvasAdaptor::Paint() is called when the TCanvas paints its
-  /// primitives, and TV5CanvasAdaptor::Paint() can forward to
-  /// Experimental::TCanvas::Paint().
-  TV5CanvasAdaptor(ROOT::Experimental::TCanvas& canv):
-    fNewCanv(canv),
-    fOldCanv(new ::TCanvas())
-  {
-    fOldCanv->SetTitle(canv.GetTitle().c_str());
-    AppendPad();
-  }
-
-  ~TV5CanvasAdaptor() {
-    // Make sure static destruction hasn't already destroyed the old TCanvases.
-    if (gROOT && gROOT->GetListOfCanvases() && !gROOT->GetListOfCanvases()->IsEmpty())
-      fOldCanv->RecursiveRemove(this);
-  }
-
-  void Paint(Option_t */*option*/="") override {
-    fNewCanv.Paint();
-  }
-};
-}
-}
-}
 
 const std::vector<std::shared_ptr<ROOT::Experimental::TCanvas>> &
 ROOT::Experimental::TCanvas::GetCanvases() {
   return GetHeldCanvases();
-}
-
-
-ROOT::Experimental::TCanvas::TCanvas() {
-  fAdaptor = std::make_unique<Internal::TV5CanvasAdaptor>(*this);
-}
-
-ROOT::Experimental::TCanvas::~TCanvas() = default;
-
-void ROOT::Experimental::TCanvas::Paint() {
-  for (auto&& drw: fPrimitives) {
-    drw->Paint(*this);
-  }
 }
 
 std::shared_ptr<ROOT::Experimental::TCanvas>
