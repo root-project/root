787b59b24bf09fbb22664d0df87583904eebeddc
Danilo Piparo <danilo.piparo@cern.ch>
2017-06-02 19:49:58 +0200

    [TDF] Test for snapshotting starting from N files

---
+++ root/dataframe/test_snapshotNFiles.C
@@ -0,0 +1,53 @@
+#include "TTree.h"
+#include "ROOT/TDataFrame.hxx"
+
+#include <iostream>
+
+void fill_file(const char *filename, const int n) {
+   TFile f(filename, "recreate");
+   TTree t("t","t");
+   int b;
+   t.Branch("b", &b);
+   for(b = 0; b < n; ++b)
+      t.Fill();
+   t.Write();
+}
+
+int test_snapshot2Files() {
+   fill_file("file_snapshot2Files_1.root",1);
+   fill_file("file_snapshot2Files_2.root",10);
+   // Single threaded
+   {
+      ROOT::Experimental::TDataFrame d("t", "file_snapshot2Files_*[1,2].root");
+      auto d2 = d.Snapshot("t", "outfile.root", "b");
+      d2.Foreach([](int b){std::cout << "b = " << b << std::endl; }, {"b"});
+
+   }
+   // Multithreaded
+#ifdef R__USE_IMT
+   {
+      ROOT::EnableImplicitMT(1); // 2 to have more files per worker
+      std::cout << "Now going MT\n";
+      fill_file("file_snapshot2Files_3.root",5);
+      fill_file("file_snapshot2Files_4.root",5);
+      fill_file("file_snapshot2Files_5.root",5);
+      fill_file("file_snapshot2Files_6.root",5);
+      fill_file("file_snapshot2Files_7.root",5);
+      fill_file("file_snapshot2Files_8.root",5);
+      std::cout << "Additional files written\n";
+      ROOT::Experimental::TDataFrame d("t", "file_snapshot2Files_*.root");
+      auto d2 = d.Snapshot("t", "outfile.root", "b");
+      d2.Foreach([](int b){std::cout << "b = " << b << std::endl; }, {"b"});
+   }
+   std::cout << "Temporary branch\n";
+   ROOT::Experimental::TDataFrame d("t", "file_snapshot2Files_*.root");
+   auto d2 = d.Define("x", "1+2");
+   auto d3 = d2.Snapshot("t", "outfile.root", {"x","b"});
+   d3.Foreach([](int b){std::cout << "b = " << b << std::endl; }, {"b"});
+#endif
+   return 0;
+}
+
+int main() {
+   return test_snapshot2Files();
+}
