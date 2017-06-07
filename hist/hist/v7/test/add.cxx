76de5f49189d80072c6157dce2ab276724479222
Axel Naumann <Axel.Naumann@cern.ch>
2017-06-07 13:06:00 +0200

    CoordArray_t: enforce that all coordinates are initialized.
    std::array, using aggregate init, allows *less* initializers with remaining ones being {} initialized.
    That is evil for coordinates, where we want {12.,13.,14.} for 3-dim coords, not 4-dim coord!
    Use the std::tuple approach of only making the ctor available if the right number of args is passed.
    Else, static_assert to be helpful.

--- hist/hist/v7/test/add.cxx
+++ hist/hist/v7/test/add.cxx
@@ -6,26 +6,26 @@
 TEST(HistAddTest, AddEmptyHist) {
   ROOT::Experimental::TH1F hFrom({100,0.,1});
   ROOT::Experimental::TH1F hTo({100,0.,1});
-  hFrom.Fill({{0.1111}}, .42f);
+  hFrom.Fill({0.1111}, .42f);
   ROOT::Experimental::Add(hTo, hFrom);
-  EXPECT_FLOAT_EQ(0.42f, hTo.GetBinContent({{0.1111}}));
+  EXPECT_FLOAT_EQ(0.42f, hTo.GetBinContent({0.1111}));
 }
 
 // Test "0 + x = x"
 TEST(HistAddTest, AddEmptySelf) {
    ROOT::Experimental::TH1F hFrom({100,0.,1});
    ROOT::Experimental::TH1F hTo({100,0.,1});
-   hTo.Fill({{0.1111}}, .42f);
+   hTo.Fill({0.1111}, .42f);
    ROOT::Experimental::Add(hTo, hFrom);
-   EXPECT_FLOAT_EQ(0.42f, hTo.GetBinContent({{0.1111}}));
+   EXPECT_FLOAT_EQ(0.42f, hTo.GetBinContent({0.1111}));
 }
 
 // Test "x + x = 2*x"
 TEST(HistAddTest, AddSelfHist) {
    ROOT::Experimental::TH1F hist({100,0.,1});
-   hist.Fill({{0.1111}}, .42f);
+   hist.Fill({0.1111}, .42f);
    ROOT::Experimental::Add(hist, hist);
-   EXPECT_FLOAT_EQ(0.84f, hist.GetBinContent({{0.1111}}));
+   EXPECT_FLOAT_EQ(0.84f, hist.GetBinContent({0.1111}));
 }
 
 // Test "x + y" with less STAT
@@ -33,10 +33,10 @@ TEST(HistAddTest, AddHist) {
    ROOT::Experimental::TH1F hFrom({100,0.,1});
    // Implicitly tests ROOT-8485.
    ROOT::Experimental::THist<1, float> hTo({100,0.,1});
-   hFrom.Fill({{0.1111}}, .42f);
-   hTo.Fill({{0.1111}}, .17f);
+   hFrom.Fill({0.1111}, .42f);
+   hTo.Fill({0.1111}, .17f);
    ROOT::Experimental::Add(hTo, hFrom);
-   EXPECT_FLOAT_EQ(0.59f, hTo.GetBinContent({{0.1111}}));
+   EXPECT_FLOAT_EQ(0.59f, hTo.GetBinContent({0.1111}));
 }
 
 // Test addition of a hist range
