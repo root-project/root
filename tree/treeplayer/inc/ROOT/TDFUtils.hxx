9b90d67af7893f35765c9c366c2d3e63c97b9b08
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-06-08 12:07:19 +0200

    [TypeTraits] Move TDF general-purpose utilities to TTypeTraits.hxx

--- tree/treeplayer/inc/ROOT/TDFUtils.hxx
+++ tree/treeplayer/inc/ROOT/TDFUtils.hxx
@@ -43,60 +43,23 @@ namespace Internal {
 namespace TDF {
 using namespace ROOT::Detail::TDF;
 
-template <typename... Types>
-struct TTypeList {
-   static constexpr std::size_t fgSize = sizeof...(Types);
+/// Compile-time integer sequence generator
+/// e.g. calling GenStaticSeq<3>::type() instantiates a StaticSeq<0,1,2>
+template <int...>
+struct StaticSeq {
 };
 
-// extract parameter types from a callable object
-template <typename T>
-struct TFunctionTraits {
-   using Args_t = typename TFunctionTraits<decltype(&T::operator())>::Args_t;
-   using ArgsNoDecay_t = typename TFunctionTraits<decltype(&T::operator())>::ArgsNoDecay_t;
-   using Ret_t = typename TFunctionTraits<decltype(&T::operator())>::Ret_t;
+template <int N, int... S>
+struct GenStaticSeq : GenStaticSeq<N - 1, N - 1, S...> {
 };
 
-// lambdas and std::function
-template <typename R, typename T, typename... Args>
-struct TFunctionTraits<R (T::*)(Args...) const> {
-   using Args_t = TTypeList<typename std::decay<Args>::type...>;
-   using ArgsNoDecay_t = TTypeList<Args...>;
-   using Ret_t = R;
+template <int... S>
+struct GenStaticSeq<0, S...> {
+   using type = StaticSeq<S...>;
 };
 
-// mutable lambdas and functor classes
-template <typename R, typename T, typename... Args>
-struct TFunctionTraits<R (T::*)(Args...)> {
-   using Args_t = TTypeList<typename std::decay<Args>::type...>;
-   using ArgsNoDecay_t = TTypeList<Args...>;
-   using Ret_t = R;
-};
-
-// function pointers
-template <typename R, typename... Args>
-struct TFunctionTraits<R (*)(Args...)> {
-   using Args_t = TTypeList<typename std::decay<Args>::type...>;
-   using ArgsNoDecay_t = TTypeList<Args...>;
-   using Ret_t = R;
-};
-
-// free functions
-template <typename R, typename... Args>
-struct TFunctionTraits<R(Args...)> {
-   using Args_t = TTypeList<typename std::decay<Args>::type...>;
-   using ArgsNoDecay_t = TTypeList<Args...>;
-   using Ret_t = R;
-};
-
-// remove first type from TTypeList
-template <typename>
-struct TRemoveFirst {
-};
-
-template <typename T, typename... Args>
-struct TRemoveFirst<TTypeList<T, Args...>> {
-   using Types_t = TTypeList<Args...>;
-};
+template <int... S>
+using GenStaticSeq_t = typename GenStaticSeq<S...>::type;
 
 // return wrapper around f that prepends an `unsigned int slot` parameter
 template <typename R, typename F, typename... Args>
@@ -104,63 +67,6 @@ std::function<R(unsigned int, Args...)> AddSlotParameter(F &f, TTypeList<Args...
 {
    return [f](unsigned int, Args... a) -> R { return f(a...); };
 }
-
-// compile-time integer sequence generator
-// e.g. calling TGenStaticSeq<3>::type() instantiates a TStaticSeq<0,1,2>
-template <int...>
-struct TStaticSeq {
-};
-
-template <int N, int... S>
-struct TGenStaticSeq : TGenStaticSeq<N - 1, N - 1, S...> {
-};
-
-template <int... S>
-struct TGenStaticSeq<0, S...> {
-   using Type_t = TStaticSeq<S...>;
-};
-
-template <typename T>
-struct TIsContainer {
-   using Test_t = typename std::decay<T>::type;
-
-   template <typename A>
-   static constexpr bool Test(A *pt, A const *cpt = nullptr, decltype(pt->begin()) * = nullptr,
-                              decltype(pt->end()) * = nullptr, decltype(cpt->begin()) * = nullptr,
-                              decltype(cpt->end()) * = nullptr, typename A::iterator *pi = nullptr,
-                              typename A::const_iterator *pci = nullptr)
-   {
-      using It_t = typename A::iterator;
-      using CIt_t = typename A::const_iterator;
-      using V_t = typename A::value_type;
-      return std::is_same<Test_t, std::vector<bool>>::value ||
-             (std::is_same<decltype(pt->begin()), It_t>::value && std::is_same<decltype(pt->end()), It_t>::value &&
-              std::is_same<decltype(cpt->begin()), CIt_t>::value && std::is_same<decltype(cpt->end()), CIt_t>::value &&
-              std::is_same<decltype(**pi), V_t &>::value && std::is_same<decltype(**pci), V_t const &>::value);
-   }
-
-   template <typename A>
-   static constexpr bool Test(...)
-   {
-      return false;
-   }
-
-   static const bool fgValue = Test<Test_t>(nullptr);
-};
-
-// Extract first of possibly many template parameters. For non-template types, the result is the type itself
-template <typename T>
-struct TExtractType {
-   using type = T;
-};
-
-template <typename T, template <typename...> class U, typename... Extras>
-struct TExtractType<U<T, Extras...>> {
-   using type = T;
-};
-
-template <typename T>
-using ExtractType_t = typename TExtractType<T>::type;
 
 template <typename BranchType, typename... Rest>
 struct TNeedJitting {
@@ -286,11 +192,6 @@ struct Mean {
 struct Fill {
 };
 }
-
-template <typename T, bool ISV7HISTO = !std::is_base_of<TH1, T>::value>
-struct TIsV7Histo {
-   const static bool fgValue = ISV7HISTO;
-};
 
 template <typename T, bool ISV7HISTO = TIsV7Histo<T>::fgValue>
 struct HistoUtils {
