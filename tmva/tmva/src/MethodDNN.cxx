7fef8933d9550c8622f12a635be7fe1790ecdd14
ashlaban <ketost@gmail.com>
2017-06-08 10:14:58 +0200

    Reduce severity warning for DNN with weights.

--- tmva/tmva/src/MethodDNN.cxx
+++ tmva/tmva/src/MethodDNN.cxx
@@ -127,7 +127,6 @@ void TMVA::MethodDNN::Init()
    for (UInt_t i = 0; i < numClasses; ++i) {
       if (dsi.GetWeightExpression(i) != TString("")) {
          Log() << kERROR << "Currently event weights are not considered properly by this method." << Endl;
-         Log() << kFATAL << "See above." << Endl;
       }
    }
 }
