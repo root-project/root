e481460e0e85e5959cf02d6a8acd0c4628fd67f4
Saurav Shekhar <sauravshekhar01@gmail.com>
2017-06-14 02:58:37 +0200

    RNNLayer, RecurrentNet with Forward and Backward added

--- tmva/tmva/inc/TMVA/Types.h
+++ tmva/tmva/inc/TMVA/Types.h
@@ -98,6 +98,7 @@ namespace TMVA {
          kPlugins        ,
          kCategory       ,
          kDNN            ,
+         kRNN            ,
          kPyRandomForest ,
          kPyAdaBoost     ,
          kPyGTB          ,
