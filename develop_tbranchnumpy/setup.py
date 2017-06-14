173e4dec41b33ca064fec7f60dc53be5fd3001f2
jpivarski <pivarski@fnal.gov>
2017-06-14 10:42:33 -0500

    check in tbranchnumpy here with the understanding that I'll 'git mv' it to the right place later, maintaining history

---
+++ develop_tbranchnumpy/setup.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+
+from setuptools import setup, find_packages, Extension
+import numpy.distutils.misc_util
+
+ROOTBIN = os.path.join("..", "root-install", "bin")
+
+def rootconfig(arg, filter, drop):
+    rootconfig = subprocess.Popen([os.path.join(ROOTBIN, "root-config"), arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if rootconfig.wait() != 0:
+        raise IOError(rootconfig.stderr.read())
+    return [x.strip()[drop:] for x in rootconfig.stdout.read().decode().split(" ") if filter(x)]
+
+setup(name = "tbranchnumpy",
+      version = "0.0.1",
+      packages = find_packages(),
+      scripts = [],
+      description = "",
+      long_description = """""",
+      author = "Jim Pivarski (DIANA-HEP)",
+      author_email = "pivarski@fnal.gov",
+      maintainer = "Jim Pivarski (DIANA-HEP)",
+      maintainer_email = "pivarski@fnal.gov",
+      url = "",
+      download_url = "",
+      license = "???",
+      # test_suite = "tests",
+      install_requires = ["numpy"],
+      tests_require = [],
+      classifiers = ["Development Status :: 2 - Pre-Alpha",
+                     "Environment :: Console",
+                     "Intended Audience :: Science/Research",
+                     "License :: OSI Approved :: Apache Software License",
+                     "Topic :: Scientific/Engineering :: Information Analysis",
+                     "Topic :: Scientific/Engineering :: Mathematics",
+                     "Topic :: Scientific/Engineering :: Physics",
+                     ],
+      platforms = "Any",
+      ext_modules = [Extension("tbranchnumpy",
+                               [os.path.join("tbranchnumpy.cxx")],
+                               include_dirs = rootconfig("--cflags", lambda x: x.startswith("-I"), 2) + numpy.distutils.misc_util.get_numpy_include_dirs(),
+                               library_dirs = rootconfig("--libdir", lambda x: True, 0),
+                               libraries = rootconfig("--libs", lambda x: x.startswith("-l"), 2),
+                               extra_compile_args = rootconfig("--cflags", lambda x: not x.startswith("-I"), 0),
+                               )],
+      )
---
+++ develop_tbranchnumpy/tbranchnumpy.cxx
@@ -0,0 +1,89 @@
+#include <iostream>
+
+#include <Python.h>
+#include <numpy/arrayobject.h>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TBranch.h>
+#include <TLeaf.h>
+#include <TBufferFile.h>
+
+static char module_docstring[] = "Tools for quickly filling Numpy arrays with TBranch contents.";
+static char fill_docstring[] = "Fills a given array with TBranch data.";
+
+static PyObject* fill(PyObject* self, PyObject* args);
+
+static PyMethodDef module_methods[] = {
+  {"fill", (PyCFunction)fill, METH_VARARGS, fill_docstring},
+  {NULL, NULL, 0, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef moduledef = {
+  PyModuleDef_HEAD_INIT,
+  "tbranchnumpy",
+  NULL,
+  0,
+  module_methods,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+PyMODINIT_FUNC PyInit_tbranchnumpy(void) {
+  PyObject* module = PyModule_Create(&moduledef);
+  if (module != NULL)
+    import_array();
+  return module;
+}
+#else
+PyMODINIT_FUNC inittbranchnumpy(void) {
+  PyObject* module = Py_InitModule3("tbranchnumpy", module_methods, module_docstring);
+  if (module != NULL)
+    import_array();
+}
+#endif
+
+static PyObject* fill(PyObject* self, PyObject* args) {
+  char* filePath;
+  char* treePath;
+  char* branchName;
+  PyObject* array;
+  Long64_t startingEntry = 0;
+  
+  if (!PyArg_ParseTuple(args, "sssO|L", &filePath, &treePath, &branchName, &array, &startingEntry)  ||  !PyArray_Check(array)) {
+    PyErr_SetString(PyExc_TypeError, "arguments are: filePath (str), treePath (str), branchName (str), array (Numpy); optionally startingEntry (int, default is 0)");
+    return NULL;
+  }
+
+  TFile* file = TFile::Open(filePath);
+  TTree* tree;
+  file->GetObject(treePath, tree);
+  Long64_t numEntries = tree->GetEntries();
+  TBranch* branch = tree->GetBranch(branchName);
+
+  // error checking is for wimps
+
+  char* arraydata = PyArray_BYTES(array);
+  size_t arrayindex = 0;
+  size_t arrayend = PyArray_NBYTES(array);
+
+  TBufferFile buffer(TBuffer::kWrite, 32*1024);
+
+  struct timeval startTime, endTime;
+
+  while (arrayindex < arrayend  &&  startingEntry < numEntries) {
+    Long64_t entries = branch->GetEntriesSerialized(startingEntry, buffer);
+    startingEntry += entries;
+
+    size_t tocopy = buffer.BufferSize();
+    if (arrayindex + tocopy > arrayend)
+      tocopy = arrayend - arrayindex;
+
+    arrayindex += tocopy;
+  }
+
+  return Py_BuildValue("O", Py_None);
+}
