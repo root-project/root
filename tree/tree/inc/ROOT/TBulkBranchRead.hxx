793f0d728957c35e8963bafd00f79322f078ce37
Brian Bockelman <bbockelm@cse.unl.edu>
2017-06-14 04:37:22 -0500

    Move bulk read API into experimental namespace.

---
+++ tree/tree/inc/ROOT/TBulkBranchRead.hxx
@@ -0,0 +1,41 @@
+// Author: Brian Bockelman 14/06/17
+
+/*************************************************************************
+ * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TBulkBranchRead
+#define ROOT_TBulkBranchRead
+
+#include "TBranch.h"
+
+namespace ROOT {
+namespace Experimental {
+namespace Internal {
+
+class TBulkBranchRead {
+
+   friend class ::TBranch;
+
+public:
+   Int_t  GetEntriesFast(Long64_t evt, TBuffer& user_buf) {return fParent.GetEntriesFast(evt, user_buf);}
+   Int_t  GetEntriesSerialized(Long64_t evt, TBuffer& user_buf) {return fParent.GetEntriesSerialized(evt, user_buf);}
+   Bool_t SupportsBulkRead() const {return fParent.SupportsBulkRead();}
+
+private:
+   TBulkBranchRead(TBranch &parent)
+      : fParent(parent)
+   {}
+
+   TBranch &fParent;
+};
+
+}  // Internal
+}  // Experimental
+}  // ROOT
+
+#endif  // ROOT_TBulkBranchRead
