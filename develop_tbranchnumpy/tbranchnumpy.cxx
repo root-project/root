633a28c0230421141c237c01fdf0f5524a119564
jpivarski <pivarski@fnal.gov>
2017-06-15 10:54:37 -0500

    implemented declare; untested

--- develop_tbranchnumpy/tbranchnumpy.cxx
+++ develop_tbranchnumpy/tbranchnumpy.cxx
@@ -5,25 +5,41 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
+#include <ROOT/TBulkBranchRead.hxx>
 #include <TBranch.h>
 #include <TBufferFile.h>
+#include <TClass.h>
+#include <TDataType.h>
 #include <TFile.h>
+#include <TLeafB.h>
+#include <TLeafB.h>
+#include <TLeafD.h>
+#include <TLeafF.h>
 #include <TLeaf.h>
+#include <TLeafI.h>
+#include <TLeafI.h>
+#include <TLeafL.h>
+#include <TLeafL.h>
+#include <TLeafO.h>
+#include <TLeafS.h>
+#include <TLeafS.h>
 #include <TObjArray.h>
 #include <TTree.h>
-#include <ROOT/TBulkBranchRead.hxx>
 
 /////////////////////////////////////////////////////// module
 
 static char module_docstring[] = "Tools for quickly loading TBranch contents into Numpy arrays.";
 static char clusters_docstring[] = "Returns a list of the first entry in each cluster followed by the total number of entries (the length of this list is the number of clusters + 1).";
+static char declare_docstring[] = "Declares an array (or set of arrays) that can be filled with a given TBranch.";
 static char fill_docstring[] = "Fills a given array (or set of arrays) with TBranch data.";
 
 static PyObject* clusters(PyObject* self, PyObject* args);
+static PyObject* declare(PyObject* self, PyObject* args);
 static PyObject* fill(PyObject* self, PyObject* args);
 
 static PyMethodDef module_methods[] = {
   {"clusters", (PyCFunction)clusters, METH_VARARGS, clusters_docstring},
+  {"declare", (PyCFunction)declare, METH_VARARGS, declare_docstring},
   {"fill", (PyCFunction)fill, METH_VARARGS, fill_docstring},
   {NULL, NULL, 0, NULL}
 };
@@ -56,6 +72,95 @@ PyMODINIT_FUNC inittbranchnumpy(void) {
 #endif
 
 /////////////////////////////////////////////////////// utils
+
+bool getfile(TFile* &file, char* filePath) {
+  file = TFile::Open(filePath);
+  if (file == NULL  ||  !file->IsOpen()) {
+    PyErr_Format(PyExc_IOError, "could not open file \"%s\"", filePath);
+    return false;
+  }
+  else
+    return true;
+}
+
+bool gettree(TTree* &tree, TFile* file, char* filePath, char* treePath) {
+  file->GetObject(treePath, tree);
+  if (tree == NULL) {
+    PyErr_Format(PyExc_IOError, "could not read tree \"%s\" from file \"%s\"", treePath, filePath);
+    return false;
+  }
+  else
+    return true;
+}
+
+bool getbranch(TBranch* &branch, TTree* tree, char* filePath, char* treePath, char* branchName) {
+  branch = tree->GetBranch(branchName);
+  if (branch == NULL) {
+    PyErr_Format(PyExc_IOError, "could not read branch \"%s\" from tree \"%s\" from file \"%s\"", branchName, treePath, filePath);
+    return false;
+  }
+  else
+    return true;
+}
+
+bool leaftype(TLeaf* leaf, const char* &dtype, Long64_t &size) {
+  if (leaf->IsA() == TLeafO::Class()) {
+    dtype = "bool"; size = 1; return true;
+  }
+  else if (leaf->IsA() == TLeafB::Class()  &&  leaf->IsUnsigned()) {
+    dtype = "u1";   size = 1; return true;
+  }
+  else if (leaf->IsA() == TLeafB::Class()) {
+    dtype = "i1";   size = 1; return true;
+  }
+  else if (leaf->IsA() == TLeafS::Class()  &&  leaf->IsUnsigned()) {
+    dtype = ">u2";  size = 2; return true;
+  }
+  else if (leaf->IsA() == TLeafS::Class()) {
+    dtype = ">i2";  size = 2; return true;
+  }
+  else if (leaf->IsA() == TLeafI::Class()  &&  leaf->IsUnsigned()) {
+    dtype = ">u4";  size = 4; return true;
+  }
+  else if (leaf->IsA() == TLeafI::Class()) {
+    dtype = ">i4";  size = 4; return true;
+  }
+  else if (leaf->IsA() == TLeafL::Class()  &&  leaf->IsUnsigned()) {
+    dtype = ">u8";  size = 8; return true;
+  }
+  else if (leaf->IsA() == TLeafL::Class()) {
+    dtype = ">i8";  size = 8; return true;
+  }
+  else if (leaf->IsA() == TLeafF::Class()) {
+    dtype = ">f4";  size = 4; return true;
+  }
+  else if (leaf->IsA() == TLeafD::Class()) {
+    dtype = ">f8";  size = 8; return true;
+  }
+  else {
+    TClass* expectedClass;
+    EDataType expectedType;
+    leaf->GetBranch()->GetExpectedType(expectedClass, expectedType);
+    switch (expectedType) {
+      case kBool_t:     dtype = "bool"; size = 1; return true;
+      case kUChar_t:    dtype = "u1";   size = 1; return true;
+      case kchar:       dtype = "i1";   size = 1; return true;
+      case kChar_t:     dtype = "i1";   size = 1; return true;
+      case kUShort_t:   dtype = ">u2";  size = 2; return true;
+      case kShort_t:    dtype = ">i2";  size = 2; return true;
+      case kUInt_t:     dtype = ">u4";  size = 4; return true;
+      case kInt_t:      dtype = ">i4";  size = 4; return true;
+      case kULong_t:    dtype = ">u8";  size = 8; return true;
+      case kLong_t:     dtype = ">i8";  size = 8; return true;
+      case kULong64_t:  dtype = ">u8";  size = 8; return true;
+      case kLong64_t:   dtype = ">i8";  size = 8; return true;
+      case kFloat_t:    dtype = ">f4";  size = 4; return true;
+      case kDouble32_t: dtype = ">f4";  size = 4; return true;
+      case kDouble_t:   dtype = ">f8";  size = 8; return true;
+    }
+  }
+  return false;
+}
 
 void getdim(TLeaf* leaf, std::vector<int>& dims, std::vector<std::string>& counters) {
   const char* title = leaf->GetTitle();
@@ -93,6 +198,27 @@ bool has_varlen(std::vector<std::string>& counters) {
   return false;
 }
 
+PyObject* makearray(int nd, npy_intp* dims, PyObject* spec) {
+  PyArray_Descr* dtype = PyArray_DescrFromType(0);
+
+  if (!PyArray_DescrConverter(spec, &dtype)) {
+    PyErr_SetString(PyExc_ValueError, "cannot create a dtype");
+    return NULL;
+  }
+
+  return PyArray_Empty(nd, dims, dtype, 0);
+}
+
+PyObject* makearray(int nd, npy_intp* dims, const char* spec) {
+  return makearray(nd, dims, PyUnicode_FromString(spec));
+}
+
+PyObject* makearray(npy_intp length, const char* spec) {
+  npy_intp dims[1];
+  dims[0] = length;
+  return makearray(1, dims, PyUnicode_FromString(spec));
+}
+
 /////////////////////////////////////////////////////// info functions
 
 PyObject* clusters_impl(TTree* tree) {
@@ -116,9 +242,12 @@ static PyObject* clusters(PyObject* self, PyObject* args) {
   // PyObject* pyroot_ttree;
 
   if (PyArg_ParseTuple(args, "ss", &filePath, &treePath)) {
-    TFile* file = TFile::Open(filePath);
+    TFile* file;
+    if (!getfile(file, filePath)) return NULL;
+
     TTree* tree;
-    file->GetObject(treePath, tree);
+    if (!gettree(tree, file, filePath, treePath)) return NULL;
+
     return clusters_impl(tree);
   }
 
@@ -130,6 +259,99 @@ static PyObject* clusters(PyObject* self, PyObject* args) {
 }
 
 /////////////////////////////////////////////////////// declare variants
+
+PyObject* declare_flat(TBranch* branch, TLeaf* leaf, bool wantCounter) {
+  const char* dtype;
+  Long64_t size;
+  if (!leaftype(leaf, dtype, size)) {
+    PyErr_Format(PyExc_TypeError, "the TLeaf \"%s\" cannot be converted to Numpy", leaf->GetName());
+    return NULL;
+  }
+  return makearray(branch->GetTotBytes() / size, dtype);
+}
+
+PyObject* declare_fixedlen(TBranch* branch, TLeaf* leaf, std::vector<int> &dims, bool wantCounter) {
+  // TODO: declare a multidimensional Numpy array
+  PyErr_SetString(PyExc_NotImplementedError, "fixedlen");
+  return NULL;
+}
+
+PyObject* declare_varlen(TBranch* branch, TLeaf* leaf, std::vector<int> &dims, std::vector<std::string> &counters, bool wantCounter) {
+  // TODO: declare a single Numpy array for the data (if !wantCounter) or a 2-tuple of arrays for data and counter
+  PyErr_SetString(PyExc_NotImplementedError, "varlen");
+  return NULL;
+}
+
+PyObject* declare_unileaf(TBranch* branch, TLeaf* leaf, bool wantCounter) {
+  std::vector<int> dims;
+  std::vector<std::string> counters;
+  getdim(leaf, dims, counters);
+
+  if (has_varlen(counters))
+    return declare_varlen(branch, leaf, dims, counters, wantCounter);
+  else if (!dims.empty())
+    return declare_fixedlen(branch, leaf, dims, wantCounter);
+  else
+    return declare_flat(branch, leaf, wantCounter);
+}
+
+PyObject* declare_multileaf(TBranch* branch, TObjArray* leaves, bool wantCounter) {
+  // TODO: declare a Numpy recarray
+  PyErr_SetString(PyExc_NotImplementedError, "multileaf");
+  return NULL;
+}
+
+PyObject* declare_unibranch(TBranch* branch, bool wantCounter) {
+  TObjArray* leaves = branch->GetListOfLeaves();
+  if (leaves->GetEntries() == 1)
+    return declare_unileaf(branch, dynamic_cast<TLeaf*>(leaves->First()), wantCounter);
+  else
+    return declare_multileaf(branch, leaves, wantCounter);
+}
+
+PyObject* declare_multibranch(TObjArray* branches, bool wantCounter) {
+  // TODO: declare a Python dict of arrays
+  PyErr_SetString(PyExc_NotImplementedError, "multibranch");
+  return NULL;
+}
+
+PyObject* declare_branch(TBranch* branch, bool wantCounter) {
+  TObjArray* subbranches = branch->GetListOfBranches();
+  if (subbranches->GetEntries() == 0)
+    return declare_unibranch(branch, wantCounter);
+  else
+    return declare_multibranch(subbranches, wantCounter);
+}
+
+static PyObject* declare(PyObject* self, PyObject* args) {
+  char* filePath;
+  char* treePath;
+  char* branchName;
+  // PyObject* pyroot_tbranch;
+  PyObject* wantCounter = NULL;
+
+  if (PyArg_ParseTuple(args, "sss|O", &filePath, &treePath, &branchName, &wantCounter)) {
+    TFile* file;
+    if (!getfile(file, filePath)) return NULL;
+
+    TTree* tree;
+    if (!gettree(tree, file, filePath, treePath)) return NULL;
+
+    TBranch* branch;
+    if (!getbranch(branch, tree, filePath, treePath, branchName)) return NULL;
+
+    if (wantCounter != NULL  &&  PyObject_IsTrue(wantCounter))
+      return declare_branch(branch, true);
+    else
+      return declare_branch(branch, false);
+  }
+
+  // TODO: check PyArg_ParseTuple(args, "O", &pyroot_tbranch) for a PyROOT tbranch
+  // else if () { }
+
+  PyErr_SetString(PyExc_TypeError, "either supply\n    filePath (str), treePath (str), and branchName (str)\nor\n    tbranch (PyROOT)");
+  return NULL;
+}
 
 /////////////////////////////////////////////////////// fill variants
 
@@ -176,13 +398,13 @@ bool fill_flat(TBranch* branch, TLeaf* leaf, PyObject* tofill, Long64_t starting
   return fill_impl(branch, tofill, startingEntry);
 }
 
-bool fill_fixedlen(TBranch* branch, TLeaf* leaf, std::vector<int>& dims, PyObject* tofill, Long64_t startingEntry) {
+bool fill_fixedlen(TBranch* branch, TLeaf* leaf, std::vector<int> &dims, PyObject* tofill, Long64_t startingEntry) {
   // TODO: fill a multidimensional Numpy array
   PyErr_SetString(PyExc_NotImplementedError, "fixedlen");
   return false;
 }
 
-bool fill_varlen(TBranch* branch, TLeaf* leaf, std::vector<int>& dims, std::vector<std::string>& counters, PyObject* tofill, Long64_t startingEntry) {
+bool fill_varlen(TBranch* branch, TLeaf* leaf, std::vector<int> &dims, std::vector<std::string> &counters, PyObject* tofill, Long64_t startingEntry) {
   // TODO: fill a single Numpy array with just the data or a 2-tuple of arrays with data and counter
   PyErr_SetString(PyExc_NotImplementedError, "varlen");
   return false;
@@ -195,7 +417,7 @@ bool fill_unileaf(TBranch* branch, TLeaf* leaf, PyObject* tofill, Long64_t start
 
   if (has_varlen(counters))
     return fill_varlen(branch, leaf, dims, counters, tofill, startingEntry);
-  else if (dims.size() > 0)
+  else if (!dims.empty())
     return fill_fixedlen(branch, leaf, dims, tofill, startingEntry);
   else
     return fill_flat(branch, leaf, tofill, startingEntry);
@@ -238,10 +460,14 @@ static PyObject* fill(PyObject* self, PyObject* args) {
   Long64_t startingEntry = 0;
   
   if (PyArg_ParseTuple(args, "sssO|L", &filePath, &treePath, &branchName, &tofill, &startingEntry)) {
-    TFile* file = TFile::Open(filePath);
+    TFile* file;
+    if (!getfile(file, filePath)) return NULL;
+
     TTree* tree;
-    file->GetObject(treePath, tree);
-    TBranch* branch = tree->GetBranch(branchName);
+    if (!gettree(tree, file, filePath, treePath)) return NULL;
+
+    TBranch* branch;
+    if (!getbranch(branch, tree, filePath, treePath, branchName)) return NULL;
 
     if (fill_branch(branch, tofill, startingEntry))
       return Py_BuildValue("O", Py_None);
