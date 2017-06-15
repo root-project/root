2fe7453cff317a27783255941b61d663a6aa45a5
Philippe Canal <pcanal@fnal.gov>
2017-06-15 17:53:46 +0200

    Make TMutex compliant with standard library

--- core/thread/inc/TMutex.h
+++ core/thread/inc/TMutex.h
@@ -47,6 +47,10 @@ public:
    Int_t  UnLock();
    Int_t  CleanUp();
 
+   // Compatibility with standard library
+   void lock() { TMutex::Lock(); }
+   void unlock() { TMutex::UnLock(); }
+
    TVirtualMutex *Factory(Bool_t recursive = kFALSE);
 
    ClassDef(TMutex,0)  // Mutex lock class
