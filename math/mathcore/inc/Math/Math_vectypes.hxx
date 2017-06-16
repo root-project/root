3e94dde056eb5b9e028aa64de7a13c228f0be4aa
Pere Mato <pere.mato@cern.ch>
2017-06-16 12:06:33 +0200

    Some provisional fixes to work with external Vc and VecCore

--- math/mathcore/inc/Math/Math_vectypes.hxx
+++ math/mathcore/inc/Math/Math_vectypes.hxx
@@ -1,9 +1,13 @@
 #include "RConfigure.h"
 #ifdef R__HAS_VECCORE
-#include <VecCore/VecCore>
+ 
+  #ifdef R__HAS_VC
+    #define VECCORE_ENABLE_VC 
+  #endif
+ 
+  #include <VecCore/VecCore>
 
-namespace ROOT {
-using Double_v = typename vecCore::backend::VcVector::Double_v;
-}
-
+  namespace ROOT {
+    using Double_v = typename vecCore::backend::VcVector::Double_v;
+  }
 #endif
