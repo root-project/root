2d6d709cf5e2105591e798a3a47c640b5d16adfa
Sergey Divakov <div.serge@gmail.com>
2017-06-23 14:07:50 +0300

    Added a simple tutorial for MultiTask BDT

---
+++ tutorials/tmva/TMVAMultiTaskRegression.C
@@ -0,0 +1,237 @@
+/// \author Andreas Hoecker
+
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+
+#include "TMVA/Tools.h"
+#include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/TMVARegGui.h"
+
+
+using namespace TMVA;
+
+void TMVAMultiRegression( TString myMethodList = "" )
+{
+   // The explicit loading of the shared libTMVA is done in TMVAlogon.C, defined in .rootrc
+   // if you use your private .rootrc, or run from a different directory, please copy the
+   // corresponding lines from .rootrc
+
+   // methods to be processed can be given as an argument; use format:
+   //
+   //     mylinux~> root -l TMVARegression.C\(\"myMethod1,myMethod2,myMethod3\"\)
+   //
+
+   //---------------------------------------------------------------
+   // This loads the library
+   TMVA::Tools::Instance();
+
+
+
+   // Default MVA methods to be trained + tested
+   std::map<std::string,int> Use;
+
+   // Methods which currently support multi-target regression:
+   // Mutidimensional likelihood and Nearest-Neighbour methods
+   Use["PDERS"]           = 0;
+   Use["PDEFoam"]         = 1;
+   Use["KNN"]             = 0;
+   
+   // Neural Network
+   Use["MLP"]             = 1;
+   //
+   // Boosted Decision Trees
+   Use["BDT"]             = 1;
+
+   std::cout << std::endl;
+   std::cout << "==> Start TMVARegression" << std::endl;
+
+   // Select methods (don't look at this code - not of interest)
+   if (myMethodList != "") {
+      for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) it->second = 0;
+
+      std::vector<TString> mlist = gTools().SplitString( myMethodList, ',' );
+      for (UInt_t i=0; i<mlist.size(); i++) {
+         std::string regMethod(mlist[i]);
+
+         if (Use.find(regMethod) == Use.end()) {
+            std::cout << "Method \"" << regMethod << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
+            for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) std::cout << it->first << " ";
+            std::cout << std::endl;
+            return;
+         }
+         Use[regMethod] = 1;
+      }
+   }
+
+   // --------------------------------------------------------------------------------------------------
+
+   // Here the preparation phase begins
+
+   // Create a new root output file
+   TString outfileName( "TMVAMultiReg.root" );
+   TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
+
+   // Create the factory object. Later you can choose the methods
+   // whose performance you'd like to investigate. The factory will
+   // then run the performance analysis for you.
+   //
+   // The first argument is the base of the name of all the
+   // weightfiles in the directory weight/
+   //
+   // The second argument is the output file for the training results
+   // All TMVA output can be suppressed by removing the "!" (not) in
+   // front of the "Silent" argument in the option string
+   TMVA::Factory *factory = new TMVA::Factory( "TMVAMultiRegression", outputFile,
+                                               "!V:!Silent:Color:DrawProgressBar:AnalysisType=Regression" );
+
+
+   TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
+   // If you wish to modify default settings
+   // (please check "src/Config.h" to see all available global options)
+   //
+   //     (TMVA::gConfig().GetVariablePlotting()).fTimesRMS = 8.0;
+   //     (TMVA::gConfig().GetIONames()).fWeightFileDir = "myWeightDirectory";
+
+   // Define the input variables that shall be used for the MVA training
+   // note that you may also use variable expressions, such as: "3*var1/var2*abs(var3)"
+   // [all types of expressions that can also be parsed by TTree::Draw( "expression" )]
+   dataloader->AddVariable( "var1", "Variable 1", "units", 'F' );
+   dataloader->AddVariable( "var2", "Variable 2", "units", 'F' );
+
+   // You can add so-called "Spectator variables", which are not used in the MVA training,
+   // but will appear in the final "TestTree" produced by TMVA. This TestTree will contain the
+   // input variables, the response values of all trained MVAs, and the spectator variables
+   dataloader->AddSpectator( "spec1:=var1*2",  "Spectator 1", "units", 'F' );
+   dataloader->AddSpectator( "spec2:=var1*3",  "Spectator 2", "units", 'F' );
+
+   // Add the variable carrying the regression target
+   dataloader->AddTarget( "fvalue" );
+   dataloader->AddTarget( "f3" );
+
+   // Read training and test data (see TMVAClassification for reading ASCII files)
+   // load the signal and background event samples from ROOT trees
+   TFile *input(0);
+   TString fname = "./files/tmva_multireg_example.root";
+   if (!gSystem->AccessPathName( fname )) {
+      input = TFile::Open( fname ); // check if file in local directory exists
+   }
+   else {
+      std::cout << "ERROR: could not open data file" << std::endl;
+      exit(1);
+   }
+   if (!input) {
+      std::cout << "ERROR: could not open data file" << std::endl;
+      exit(1);
+   }
+   std::cout << "--- TMVAMultiTaskRegression           : Using input file: " << input->GetName() << std::endl;
+
+   // Register the regression tree
+   
+   TTree *regTree = (TTree*)input->Get("MultiRegTree");
+
+   // global event weights per tree (see below for setting event-wise weights)
+   Double_t regWeight  = 1.0;
+
+   // You can add an arbitrary number of regression trees
+   dataloader->AddRegressionTree( regTree, regWeight );
+
+   // This would set individual event weights (the variables defined in the
+   // expression need to exist in the original TTree)
+   dataloader->SetWeightExpression( "var1", "Regression" );
+   // Apply additional cuts on the signal and background samples (can be different)
+   TCut mycut = ""; // for example: TCut mycut = "abs(var1)<0.5 && abs(var2-0.5)<1";
+   
+   // tell the DataLoader to use all remaining events in the trees after training for testing:
+   dataloader->PrepareTrainingAndTestTree( mycut,
+                                         "nTrain_Regression=1000:nTest_Regression=0:SplitMode=Random:NormMode=NumEvents:!V" );
+   //
+   //     dataloader->PrepareTrainingAndTestTree( mycut,
+   //            "nTrain_Regression=0:nTest_Regression=0:SplitMode=Random:NormMode=NumEvents:!V" );
+
+   // If no numbers of events are given, half of the events in the tree are used
+   // for training, and the other half for testing:
+   //
+   //     dataloader->PrepareTrainingAndTestTree( mycut, "SplitMode=random:!V" );
+
+   // Book MVA methods
+   //
+   // Please lookup the various method configuration options in the corresponding cxx files, eg:
+   // src/MethoCuts.cxx, etc, or here: http://tmva.sourceforge.net/optionRef.html
+   // it is possible to preset ranges in the option string in which the cut optimisation should be done:
+   // "...:CutRangeMin[2]=-1:CutRangeMax[2]=1"...", where [2] is the third input variable
+   
+   // PDE - RS method
+   if (Use["PDERS"])
+      factory->BookMethod( dataloader,  TMVA::Types::kPDERS, "PDERS",
+                           "!H:!V:NormTree=T:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=40:NEventsMax=60:VarTransform=None" );
+   // And the options strings for the MinMax and RMS methods, respectively:
+   //
+   //      "!H:!V:VolumeRangeMode=MinMax:DeltaFrac=0.2:KernelEstimator=Gauss:GaussSigma=0.3" );
+   //      "!H:!V:VolumeRangeMode=RMS:DeltaFrac=3:KernelEstimator=Gauss:GaussSigma=0.3" );
+
+   if (Use["PDEFoam"])
+       factory->BookMethod( dataloader,  TMVA::Types::kPDEFoam, "PDEFoam",
+			    "!H:!V:MultiTargetRegression=F:TargetSelection=Mpv:TailCut=0.001:VolFrac=0.0666:nActiveCells=500:nSampl=2000:nBin=5:Compress=T:Kernel=None:Nmin=10:VarTransform=None" );
+
+   // K-Nearest Neighbour classifier (KNN)
+   if (Use["KNN"])
+      factory->BookMethod( dataloader,  TMVA::Types::kKNN, "KNN",
+                           "nkNN=20:ScaleFrac=0.8:SigmaFact=1.0:Kernel=Gaus:UseKernel=F:UseWeight=T:!Trim" );
+
+   // Neural network (MLP)
+   if (Use["MLP"])
+      factory->BookMethod( dataloader,  TMVA::Types::kMLP, "MLP", "!H:!V:VarTransform=Norm:NeuronType=tanh:NCycles=20000:HiddenLayers=N+20:TestRate=6:TrainingMethod=BFGS:Sampling=0.3:SamplingEpoch=0.8:ConvergenceImprove=1e-6:ConvergenceTests=15:!UseRegulator" );
+
+   
+   // Boosted Decision Trees
+   // Right now the only supported Boosting Method for handling Multi-target regression is Bagging
+   if (Use["BDT"])
+     factory->BookMethod( dataloader,  TMVA::Types::kBDT, "BDT",
+                           "!H:!V:NTrees=100:MinNodeSize=1.0%:BoostType=Bagging:SeparationType=RegressionVariance:nCuts=20:PruneMethod=CostComplexity:PruneStrength=30" );
+   factory->TrainAllMethods();
+
+   // Evaluate all MVAs using the set of test events
+   factory->TestAllMethods();
+
+   // Evaluate and compare performance of all configured MVAs
+   factory->EvaluateAllMethods();
+
+   // --------------------------------------------------------------
+
+   // Save the output
+   outputFile->Close();
+
+   std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
+   std::cout << "==> TMVARegression is done!" << std::endl;
+
+   delete factory;
+   delete dataloader;
+
+   // Launch the GUI for the root macros
+   if (!gROOT->IsBatch()) TMVA::TMVARegGui( outfileName );
+}
+
+int main( int argc, char** argv )
+{
+   // Select methods (don't look at this code - not of interest)
+   TString methodList;
+   for (int i=1; i<argc; i++) {
+      TString regMethod(argv[i]);
+      if(regMethod=="-b" || regMethod=="--batch") continue;
+      if (!methodList.IsNull()) methodList += TString(",");
+      methodList += regMethod;
+   }
+   TMVAMultiRegression(methodList);
+   return 0;
+}
