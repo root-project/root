22b0db27b06bd8fee44d8c508035abbfe9a3781a
Omar Zapata <Omar.Zapata@cern.ch>
2017-06-27 17:43:56 -0500

    ROOTMpi: fixed TStatus documentation

--- mpi/rmpi/inc/Mpi/TStatus.h
+++ mpi/rmpi/inc/Mpi/TStatus.h
@@ -45,62 +45,24 @@ namespace ROOT {
 
      operator MPI_Status() const { return fStatus; }
 
-     /**
-     Method to get if the current process was cancelled
-          \return integer with the status value
-          */
      virtual Bool_t IsCancelled() const;
 
-     /**
-     Method to get the source of the process
-          \return integer with the rank or process id value
-          */
      virtual Int_t GetSource() const;
 
-     /**
-     Method to set the source to the status message
-          \param source integer with the rank or process id value
-          */
      virtual void SetSource(Int_t source);
 
-     /**
-     Method to get the tag id of the process
-          \return integer with the tag id
-          */
      virtual Int_t GetTag() const;
 
-     /**
-     Method to set the tag id to the status of the process
-          \param tag integer with the tag id
-          */
      virtual void SetTag(Int_t tag);
 
-     /**
-     Method to get the error id of the process
-          \return integer with the error id
-          */
      virtual Int_t GetError() const;
 
-     /**
-     Method to set the error id to the status message
-          \param error integer with the error id
-          */
      virtual void SetError(Int_t error);
 
-     /**
-     Method to set the cancelled flag to the status message
-          \param flag boolean to set the cancelled flag to the message
-          */
      virtual void SetCancelled(Bool_t flag);
-     /**
-     Returns the size of the message in bytes
-          */
+
      Int_t GetMsgSize() const;
 
-     /**
-      Method to set the size of the message in bytes
-          \param size size of the message in bytes
-          */
      void SetMsgSize(Int_t size);
 
      ClassDef(TStatus, 1)
--- mpi/rmpi/src/TStatus.cxx
+++ mpi/rmpi/src/TStatus.cxx
@@ -10,6 +10,10 @@ TStatus::TStatus(): fStatus() {}
 TStatus::TStatus(const TStatus &data): TObject(data), fStatus(data.fStatus) {}
 
 //______________________________________________________________________________
+/**
+ * Method to get if the current process was cancelled
+ * \return integer with the status value
+ */
 Bool_t TStatus::IsCancelled() const
 {
    Int_t t;
@@ -19,54 +23,91 @@ Bool_t TStatus::IsCancelled() const
 
 
 //______________________________________________________________________________
+/**
+ * Method to get the source of the process
+ * \return integer with the rank or process id value
+ */
 Int_t TStatus::GetSource() const
 {
    return  fStatus.MPI_SOURCE;
 }
 
 //______________________________________________________________________________
+/**
+* Method to set the source to the status message
+* \param source integer with the rank or process id value
+*/
 void TStatus::SetSource(Int_t source)
 {
    fStatus.MPI_SOURCE = source;
 }
 
 //______________________________________________________________________________
+/**
+* Method to get the tag id of the process
+* \return integer with the tag id
+*/
 Int_t TStatus::GetTag() const
 {
    return fStatus.MPI_TAG;
 }
 
 //______________________________________________________________________________
+/**
+* Method to set the tag id to the status of the process
+* \param tag integer with the tag id
+*/
 void TStatus::SetTag(Int_t tag)
 {
    fStatus.MPI_TAG = tag;
 }
 
 //______________________________________________________________________________
+/**
+ * Method to get the error id of the process
+ * \return integer with the error id
+ */
 Int_t TStatus::GetError() const
 {
    return fStatus.MPI_ERROR;
 }
 
 //______________________________________________________________________________
+/**
+ * Method to set the error id to the status message
+ * \param error integer with the error id
+ */
+
 void TStatus::SetError(Int_t error)
 {
    fStatus.MPI_ERROR = error;
 }
 
 //______________________________________________________________________________
+/**
+ * Method to set the cancelled flag to the status message
+ * \param flag boolean to set the cancelled flag to the message
+ */
+
 void TStatus::SetCancelled(Bool_t flag)
 {
    ROOT_MPI_CHECK_CALL(MPI_Status_set_cancelled, (&fStatus, (Int_t) flag), TStatus::Class_Name());
 }
 
 //______________________________________________________________________________
+/**
+ * Returns the size of the message in bytes
+ */
 Int_t TStatus::GetMsgSize() const
 {
    return fMsgSize;
 }
 
 //______________________________________________________________________________
+/**
+ * Method to set the size of the message in bytes
+ * \param size size of the message in bytes
+ */
 void TStatus::SetMsgSize(Int_t size)
 {
    fMsgSize = size;
