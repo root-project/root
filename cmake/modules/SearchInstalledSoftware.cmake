b9506569a01b13978224319aa9640bcb67e03b57
oshadura <oksana.shadura@cern.ch>
2017-07-03 20:46:26 +0200

    Fix builds on SL6/Ubuntu.
    Remove a compiler flag when compiling googlebenchmark. Link to rt.

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -1591,6 +1591,10 @@ if (testing)
   set_property(TARGET gmock PROPERTY IMPORTED_LOCATION ${_G_LIBRARY_PATH}/libgmock.a)
   set_property(TARGET gmock_main PROPERTY IMPORTED_LOCATION ${_G_LIBRARY_PATH}/libgmock_main.a)
 
+  ###################################
+  string(REPLACE "-Woverloaded-virtual" "" GBENCHMARK_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+  ##################################
+
   # Add google benchmarking tools.
   ExternalProject_Add(
     googlebenchmark
@@ -1608,7 +1612,7 @@ if (testing)
                   -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                   -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
                   -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                  -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+                  -DCMAKE_CXX_FLAGS=${GBENCHMARK_CMAKE_CXX_FLAGS}
                   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
     # Disable install step
     INSTALL_COMMAND ""
