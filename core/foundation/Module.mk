f4b1253da0c17033db04b41388fc419778bd18a3
Guilherme Amadio <amadio@cern.ch>
2017-07-03 13:49:01 +0200

    Fix classic build error for missing TypeTraits.hxx header
    Seen at: http://cdash.cern.ch/viewBuildError.php?buildid=370038
    
    include/ROOT/TDFActionHelpers.hxx:15:31:
      fatal error: ROOT/TypeTraits.hxx: No such file or directory
      #include "ROOT/TypeTraits.hxx"
                                   ^
    compilation terminated.
    In file included from include/ROOT/TDFInterface.hxx:14:0,
                     from /.../root/tree/treeplayer/src/TDFInterface.cxx

--- core/foundation/Module.mk
+++ core/foundation/Module.mk
@@ -23,6 +23,7 @@ FOUNDATIONTH     += $(MODDIRI)/root_std_complex.h
 FOUNDATIONTH     += $(MODDIRI)/libcpp_string_view.h
 FOUNDATIONTH     += $(MODDIRI)/RWrap_libcpp_string_view.h
 FOUNDATIONTH     += $(MODDIRI)/ROOT/RArrayView.hxx
+FOUNDATIONTH     += $(MODDIRI)/ROOT/TypeTraits.hxx
 FOUNDATIONTH     += $(MODDIRI)/ROOT/rhysd_array_view.hxx
 
 FOUNDATIONO     := $(call stripsrc,$(FOUNDATIONS:.cxx=.o))
