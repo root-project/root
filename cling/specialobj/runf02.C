5b891f7f288a6cfecee93f93f5b5e901e7659aa5
Axel Naumann <Axel.Naumann@cern.ch>
2017-07-03 12:37:57 +0200

    Add test for ROOT-7718.

--- cling/specialobj/runf02.C
+++ cling/specialobj/runf02.C
@@ -1,4 +1,7 @@
-// Test ROOT-6345: dynamic scope must repair if statements.
-void runf02(bool verbose = true) {
+int runf02(bool verbose = true) {
+  // Test ROOT-6345: dynamic scope must repair if statements.
   gROOT->ProcessLine("if (true) UNKNOWN(42)"); // should complain about UNKNOWN
+  // Test ROOT-7718: dynamic scope must repair array subscripts.
+  gROOT->ProcessLine("ThisReallyDoesNotExist[42]"); // should complain about ThisReallyDoesNotExist
+  return 0;
 }
