4d7dd705b5e09e1663baf9d70a3af0c62951111f
Axel Naumann <Axel.Naumann@cern.ch>
2017-07-06 14:31:11 +0200

    Mostly-clang-format tutorials/v7.

--- tutorials/v7/draw.cxx
+++ tutorials/v7/draw.cxx
@@ -4,7 +4,8 @@
 /// \macro_code
 ///
 /// \date 2015-03-22
-/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
 /// \author Axel Naumann <axel@cern.ch>
 
 /*************************************************************************
@@ -23,27 +24,28 @@ R__LOAD_LIBRARY(libGpad);
 #include "ROOT/TCanvas.hxx"
 #include "ROOT/TDirectory.hxx"
 
-void draw() {
-  using namespace ROOT;
+void draw()
+{
+   using namespace ROOT;
 
-  // Create the histogram.
-  Experimental::TAxisConfig xaxis("x", 10, 0., 1.);
-  Experimental::TAxisConfig yaxis("y", {0., 1., 2., 3.,10.});
-  auto pHist = std::make_shared<Experimental::TH2D>(xaxis, yaxis);
+   // Create the histogram.
+   Experimental::TAxisConfig xaxis("x", 10, 0., 1.);
+   Experimental::TAxisConfig yaxis("y", {0., 1., 2., 3., 10.});
+   auto pHist = std::make_shared<Experimental::TH2D>(xaxis, yaxis);
 
-  // Fill a few points.
-  pHist->Fill({0.01, 1.02});
-  pHist->Fill({0.54, 3.02});
-  pHist->Fill({0.98, 1.02});
-  pHist->Fill({1.90, 1.02});
-  pHist->Fill({0.75,-0.02});
+   // Fill a few points.
+   pHist->Fill({0.01, 1.02});
+   pHist->Fill({0.54, 3.02});
+   pHist->Fill({0.98, 1.02});
+   pHist->Fill({1.90, 1.02});
+   pHist->Fill({0.75, -0.02});
 
-  // Register the histogram with ROOT: now it lives even after draw() ends.
-  Experimental::TDirectory::Heap().Add("hist", pHist);
+   // Register the histogram with ROOT: now it lives even after draw() ends.
+   Experimental::TDirectory::Heap().Add("hist", pHist);
 
-  // Create a canvas to be displayed.
-  auto canvas = Experimental::TCanvas::Create("Canvas Title");
-  canvas->Draw(pHist);
+   // Create a canvas to be displayed.
+   auto canvas = Experimental::TCanvas::Create("Canvas Title");
+   canvas->Draw(pHist);
 
-  canvas->Show();
+   canvas->Show();
 }
--- tutorials/v7/simple.cxx
+++ tutorials/v7/simple.cxx
@@ -4,7 +4,8 @@
 /// \macro_code
 ///
 /// \date 2015-03-22
-/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
 /// \author Axel Naumann <axel@cern.ch>
 
 /*************************************************************************
@@ -19,30 +20,31 @@
 #include "ROOT/TFit.hxx"
 #include "ROOT/TFile.hxx"
 
-void simple() {
-  using namespace ROOT;
+void simple()
+{
+   using namespace ROOT;
 
-  // Create a 2D histogram with an X axis with equidistant bins, and a y axis
-  // with irregular binning.
-  Experimental::TAxisConfig xAxis(100, 0., 1.);
-  Experimental::TAxisConfig yAxis({0., 1., 2., 3.,10.});
-  Experimental::TH2D histFromVars(xAxis, yAxis);
+   // Create a 2D histogram with an X axis with equidistant bins, and a y axis
+   // with irregular binning.
+   Experimental::TAxisConfig xAxis(100, 0., 1.);
+   Experimental::TAxisConfig yAxis({0., 1., 2., 3., 10.});
+   Experimental::TH2D histFromVars(xAxis, yAxis);
 
-  // Or the short in-place version:
-  // Create a 2D histogram with an X axis with equidistant bins, and a y axis
-  // with irregular binning.
-  Experimental::TH2D hist({100, 0., 1.}, {{0., 1., 2., 3.,10.}});
+   // Or the short in-place version:
+   // Create a 2D histogram with an X axis with equidistant bins, and a y axis
+   // with irregular binning.
+   Experimental::TH2D hist({100, 0., 1.}, {{0., 1., 2., 3., 10.}});
 
-  // Fill weight 1. at the coordinate 0.01, 1.02.
-  hist.Fill({0.01, 1.02});
+   // Fill weight 1. at the coordinate 0.01, 1.02.
+   hist.Fill({0.01, 1.02});
 
-  // Fit the histogram.
-  Experimental::TFunction<2> func([](const std::array<double,2>& x,
-                             const std::array_view<double>& par)
-                          { return par[0]*x[0]*x[0] + (par[1]-x[1])*x[1]; });
+   // Fit the histogram.
+   Experimental::TFunction<2> func([](const std::array<double, 2> &x, const std::array_view<double> &par) {
+      return par[0] * x[0] * x[0] + (par[1] - x[1]) * x[1];
+   });
 
-  Experimental::TFitResult fitResult = Experimental::FitTo(hist, func, {{0., 1.}});
+   Experimental::TFitResult fitResult = Experimental::FitTo(hist, func, {{0., 1.}});
 
-  Experimental::TFilePtr file = Experimental::TFile::Recreate("hist.root");
-  file->Write("TheHist", hist);
+   Experimental::TFilePtr file = Experimental::TFile::Recreate("hist.root");
+   file->Write("TheHist", hist);
 }
