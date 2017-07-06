2ac621fdfe0ad4f2e9d703be51ec68847d0c0d2d
Axel Naumann <Axel.Naumann@cern.ch>
2017-07-06 14:41:26 +0200

    Mostly-clang-format math/mathcore/v7.

--- math/mathcore/v7/inc/ROOT/TFit.hxx
+++ math/mathcore/v7/inc/ROOT/TFit.hxx
@@ -2,7 +2,8 @@
 /// \ingroup MathCore ROOT7
 /// \author Axel Naumann <axel@cern.ch>
 /// \date 2015-09-06
-/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
 
 /*************************************************************************
  * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
@@ -26,22 +27,19 @@ namespace ROOT {
 namespace Experimental {
 
 class TFitResult {
-
 };
 
 template <int DIMENSION>
 class TFunction {
 public:
-  TFunction(std::function<double (const std::array<double, DIMENSION>&,
-                                  const std::array_view<double>& par)> func) {}
+   TFunction(std::function<double(const std::array<double, DIMENSION> &, const std::array_view<double> &par)> func) {}
 };
 
-template<int DIMENSIONS, class PRECISION,
-  template <int D_, class P_, template <class P__> class S_> class... STAT>
-TFitResult FitTo(const THist<DIMENSIONS, PRECISION, STAT...>& hist,
-               const TFunction<DIMENSIONS>& func,
-               std::array_view<double> paramInit){
-  return TFitResult();
+template <int DIMENSIONS, class PRECISION, template <int D_, class P_, template <class P__> class S_> class... STAT>
+TFitResult FitTo(const THist<DIMENSIONS, PRECISION, STAT...> &hist, const TFunction<DIMENSIONS> &func,
+                 std::array_view<double> paramInit)
+{
+   return TFitResult();
 }
 
 } // namespace Experimental
