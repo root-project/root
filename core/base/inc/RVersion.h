e403a424d42c57de163a93c596530c5dbe23dac6
Danilo Piparo <danilo.piparo@cern.ch>
2017-07-06 09:40:34 +0200

    Update ROOT version files to v6.10/02.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.10/01"
-#define ROOT_RELEASE_DATE "Jun 13 2017"
-#define ROOT_RELEASE_TIME "10:27:45"
+#define ROOT_RELEASE "6.10/02"
+#define ROOT_RELEASE_DATE "Jul  6 2017"
+#define ROOT_RELEASE_TIME "09:40:33"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,10,1) /* 395777 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,10,2) /* 395778 */
 
 #endif
