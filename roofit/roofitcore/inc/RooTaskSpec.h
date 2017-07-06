6f8a8c94536270037e3118f86cd58d70e59b764a
vincecr0ft <vincecroft@gmail.com>
2017-07-06 16:30:09 +0200

    Adding broken RooTaskSpec

---
+++ roofit/roofitcore/inc/RooTaskSpec.h
@@ -0,0 +1,43 @@
+/*****************************************************************************
+ * Project: RooFit                                                           *
+ * Package: RooFitCore                                                       *
+ *    File: $Id: RooRealMPFE.h,v 1.7 2007/05/11 09:11:30 verkerke Exp $
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2005, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+#ifndef ROO_TASK_SPEC
+#define ROO_TASK_SPEC
+
+#include "RooFit.h"
+#include <cstdlib>
+#include <sstream>
+#include "RooMsgService.h"
+#include "RooNLLVar.h"
+#include "RooAbsTestStatistic.h"
+#include "RooAbsOptTestStatistic.h"
+#include "RooAddition.h"
+#include "RooAbsTestStatistic.h"
+
+class RooTaskSpec {
+ public:
+  //  RooTaskSpec(const Int_t _fit_case, const pdfName name,const Bool_t binned);
+ RooTaskSpec(RooAbsPdf* nll);
+ // virtual TObject* clone(const char* newname) const { return new RooTaskSpec(*this,newname); }
+ // virtual ~RooTaskSpec();
+
+ private:
+ void _initialise(RooAbsPdf* nll);
+ Int_t _fit_case;
+ Bool_t _binned;
+ ClassDef(RooTaskSpec,0)
+};
+
+#endif
---
+++ roofit/roofitcore/src/RooTaskSpec.cxx
@@ -0,0 +1,85 @@
+/****************************************************************************
+ * Project: RooFit                                                           *
+ * Package: RooFitCore                                                       *
+ * @(#)root/roofitcore:$Id$
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2005, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+/**
+\file RooTaskSpec.cxx
+\class RooTaskSpec
+\ingroup Roofitcore
+
+RooTaskSpec is a precursor to an upgrade of the the multi-processor front-end for 
+parallel calculation of RooAbsReal objects. The RooTaskSpec should return a table
+containing the information that will be passed to the RooRealMPFE process. The
+first calculation that is returned is the case being calculated since Binned and
+Unbinned likelihood calcualtions need to be optimised for multiprocessing in very
+different ways.
+Several cases are possible:
+Either the return value of createNLL() is a RooAbsTestStatisic or a RooAddition of
+RooAbsTestStatistics and/or a RooConstraintSum. 
+Case 1: return value is a RooAbsTestStatistic -> this class returns the tools to
+analyse the  components of this likelihood.
+Case 2: Multiple return values. The first of which is always a RooAbsTestStatistic
+followed by either more RooAbsTestStatistics or a RooConstraintSum which also
+signifies the last term. Here we only consider the first term since the others
+represent odd and rare test cases. 
+ToDo: Simple use demonstration.
+**/
+
+
+#include "Riostream.h"
+#include "RooFit.h"
+#include <cstdlib>
+#include <sstream>
+#include "RooMsgService.h"
+#include "RooNLLVar.h"
+#include "RooAbsTestStatistic.h"
+#include "RooAbsOptTestStatistic.h"
+#include "RooAddition.h"
+#include "RooAbsTestStatistic.h"
+#include "RooTaskSpec.h"
+
+
+using namespace std;
+using namespace RooFit;
+
+ClassImp(RooTaskSpec);
+
+//RooTaskSpec::RooTaskSpec(const Int_t case, const pdfName name,const Bool_t binned);
+RooTaskSpec::RooTaskSpec(RooAbsPdf* nll){
+  _initialise(nll);
+}
+
+
+void RooTaskSpec::_initialise (RooAbsPdf* nll){
+  // Check if nll is a AbsTestStatistic
+  RooAbsOptTestStatistic* rats = dynamic_cast<RooAbsOptTestStatistic*>(nll) ;
+  if (rats) {
+    cout << " NLL is a RooAbsOptTestStatistic (Case 0)" << endl ;
+    _fit_case = 0;
+  } else {
+    RooAddition* ra = dynamic_cast<RooAddition*>(nll) ;
+    if (ra) {
+      _fit_case = 1;
+      rats = dynamic_cast<RooAbsOptTestStatistic*>(ra->_ownedList.at(0)) ;
+      if (!rats) {
+	cout << "ERROR: NLL is a RooAddition, but first element of addition is not a RooAbsOptTestStatistic!" << endl ;
+      }
+      cout << "NLL is a RooAddition (Case 1), first element is a RooAbsOptTestStatistic" << endl ;
+    } else {
+      cout << "ERROR: NLL is neither a RooAbsStatistic nor a RooAddition" << endl ;
+    }
+  }
+
+}
+ 
