3ca97e6129431b6a656c42813f6f4391e3d6b834
Axel Naumann <Axel.Naumann@cern.ch>
2017-07-06 14:31:11 +0200

    Mostly-clang-format tutorials/v7.

--- tutorials/v7/histops.cxx
+++ tutorials/v7/histops.cxx
@@ -4,7 +4,8 @@
 /// \macro_code
 ///
 /// \date 2015-08-08
-/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
 /// \author Axel Naumann <axel@cern.ch>
 
 /*************************************************************************
@@ -18,22 +19,22 @@
 #include "ROOT/THist.hxx"
 #include <iostream>
 
-void histops() {
+void histops()
+{
 
-  // Create a 2D histogram with an X axis with equidistant bins, and a y axis
-  // with irregular binning.
-  Experimental::TH2D hist1({100, 0., 1.}, {{0., 1., 2., 3.,10.}});
+   // Create a 2D histogram with an X axis with equidistant bins, and a y axis
+   // with irregular binning.
+   Experimental::TH2D hist1({100, 0., 1.}, {{0., 1., 2., 3., 10.}});
 
-  // Fill weight 1. at the coordinate 0.01, 1.02.
-  hist1.Fill({0.01, 1.02});
+   // Fill weight 1. at the coordinate 0.01, 1.02.
+   hist1.Fill({0.01, 1.02});
 
+   Experimental::TH2D hist2({{{10, 0., 1.}, {{0., 1., 2., 3., 10.}}}});
+   // Fill weight 1. at the coordinate 0.01, 1.02.
+   hist2.Fill({0.01, 1.02});
 
-  Experimental::TH2D hist2({{ {10, 0., 1.}, {{0., 1., 2., 3.,10.}} }});
-  // Fill weight 1. at the coordinate 0.01, 1.02.
-  hist2.Fill({0.01, 1.02});
+   Experimental::Add(hist1, hist2);
 
-  Experimental::Add(hist1, hist2);
-
-  int binidx = hist1.GetImpl()->GetBinIndex({0.01, 1.02});
-  std::cout << hist1.GetImpl()->GetBinContent(binidx) << std::endl;
+   int binidx = hist1.GetImpl()->GetBinIndex({0.01, 1.02});
+   std::cout << hist1.GetImpl()->GetBinContent(binidx) << std::endl;
 }
