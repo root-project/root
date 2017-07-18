d3cfed4a33d9e231e8ce4d6c3c3f17655fe9e454
Sergey Linev <S.Linev@gsi.de>
2017-07-18 10:26:53 +0200

    webgui: apply clang format for the CEF and Qt5 code

--- gui/canvaspainter/v7/cef/simple_app.cxx
+++ gui/canvaspainter/v7/cef/simple_app.cxx
@@ -17,52 +17,47 @@ namespace {
 // When using the Views framework this object provides the delegate
 // implementation for the CefWindow that hosts the Views-based browser.
 class SimpleWindowDelegate : public CefWindowDelegate {
- public:
-  explicit SimpleWindowDelegate(CefRefPtr<CefBrowserView> browser_view)
-      : browser_view_(browser_view) {
-  }
+public:
+   explicit SimpleWindowDelegate(CefRefPtr<CefBrowserView> browser_view) : browser_view_(browser_view) {}
 
-  void OnWindowCreated(CefRefPtr<CefWindow> window) OVERRIDE {
-    // Add the browser view and show the window.
-    window->AddChildView(browser_view_);
-    window->Show();
+   void OnWindowCreated(CefRefPtr<CefWindow> window) OVERRIDE
+   {
+      // Add the browser view and show the window.
+      window->AddChildView(browser_view_);
+      window->Show();
 
-    // Give keyboard focus to the browser view.
-    browser_view_->RequestFocus();
-  }
+      // Give keyboard focus to the browser view.
+      browser_view_->RequestFocus();
+   }
 
-  void OnWindowDestroyed(CefRefPtr<CefWindow> window) OVERRIDE {
-    browser_view_ = NULL;
-  }
+   void OnWindowDestroyed(CefRefPtr<CefWindow> window) OVERRIDE { browser_view_ = NULL; }
 
-  bool CanClose(CefRefPtr<CefWindow> window) OVERRIDE {
-    // Allow the window to close if the browser says it's OK.
-    CefRefPtr<CefBrowser> browser = browser_view_->GetBrowser();
-    if (browser)
-      return browser->GetHost()->TryCloseBrowser();
-    return true;
-  }
+   bool CanClose(CefRefPtr<CefWindow> window) OVERRIDE
+   {
+      // Allow the window to close if the browser says it's OK.
+      CefRefPtr<CefBrowser> browser = browser_view_->GetBrowser();
+      if (browser) return browser->GetHost()->TryCloseBrowser();
+      return true;
+   }
 
- private:
-  CefRefPtr<CefBrowserView> browser_view_;
+private:
+   CefRefPtr<CefBrowserView> browser_view_;
 
-  IMPLEMENT_REFCOUNTING(SimpleWindowDelegate);
-  DISALLOW_COPY_AND_ASSIGN(SimpleWindowDelegate);
+   IMPLEMENT_REFCOUNTING(SimpleWindowDelegate);
+   DISALLOW_COPY_AND_ASSIGN(SimpleWindowDelegate);
 };
 
-}  // namespace
+} // namespace
 
-SimpleApp::SimpleApp(const std::string &url, const std::string &cef_main) :
-   fUrl(url), fCefMain(cef_main) {
+SimpleApp::SimpleApp(const std::string &url, const std::string &cef_main) : fUrl(url), fCefMain(cef_main)
+{
 }
 
-SimpleApp::~SimpleApp() {
+SimpleApp::~SimpleApp()
+{
 }
 
-
-void SimpleApp::OnBeforeCommandLineProcessing(
-   const CefString& process_type,
-   CefRefPtr<CefCommandLine> command_line)
+void SimpleApp::OnBeforeCommandLineProcessing(const CefString &process_type, CefRefPtr<CefCommandLine> command_line)
 {
    std::string name = process_type.ToString();
    std::string prog = command_line->GetProgram().ToString();
@@ -70,9 +65,8 @@ void SimpleApp::OnBeforeCommandLineProcessing(
    printf("Start process %s %s\n", name.c_str(), prog.c_str());
 }
 
-
-void SimpleApp::OnBeforeChildProcessLaunch(
-        CefRefPtr<CefCommandLine> command_line) {
+void SimpleApp::OnBeforeChildProcessLaunch(CefRefPtr<CefCommandLine> command_line)
+{
 
    std::string newprog = fCefMain;
 
@@ -83,48 +77,44 @@ void SimpleApp::OnBeforeChildProcessLaunch(
    printf("OnBeforeChildProcessLaunch %s\n", prog.c_str());
 }
 
-
-
-
-
-void SimpleApp::OnContextInitialized() {
-  CEF_REQUIRE_UI_THREAD();
+void SimpleApp::OnContextInitialized()
+{
+   CEF_REQUIRE_UI_THREAD();
 
 #if defined(OS_WIN) || defined(OS_LINUX)
-  // Create the browser using the Views framework if "--use-views" is specified
-  // via the command-line. Otherwise, create the browser using the native
-  // platform framework. The Views framework is currently only supported on
-  // Windows and Linux.
-  const bool use_views = false;
+   // Create the browser using the Views framework if "--use-views" is specified
+   // via the command-line. Otherwise, create the browser using the native
+   // platform framework. The Views framework is currently only supported on
+   // Windows and Linux.
+   const bool use_views = false;
 #else
-  const bool use_views = false;
+   const bool use_views = false;
 #endif
 
-  // SimpleHandler implements browser-level callbacks.
-  CefRefPtr<SimpleHandler> handler(new SimpleHandler(use_views));
+   // SimpleHandler implements browser-level callbacks.
+   CefRefPtr<SimpleHandler> handler(new SimpleHandler(use_views));
 
-  // Specify CEF browser settings here.
-  CefBrowserSettings browser_settings;
+   // Specify CEF browser settings here.
+   CefBrowserSettings browser_settings;
 
-  if (use_views) {
-    // Create the BrowserView.
-    CefRefPtr<CefBrowserView> browser_view = CefBrowserView::CreateBrowserView(
-        handler, fUrl, browser_settings, NULL, NULL);
+   if (use_views) {
+      // Create the BrowserView.
+      CefRefPtr<CefBrowserView> browser_view =
+         CefBrowserView::CreateBrowserView(handler, fUrl, browser_settings, NULL, NULL);
 
-    // Create the Window. It will show itself after creation.
-    CefWindow::CreateTopLevelWindow(new SimpleWindowDelegate(browser_view));
-  } else {
-    // Information used when creating the native window.
-    CefWindowInfo window_info;
+      // Create the Window. It will show itself after creation.
+      CefWindow::CreateTopLevelWindow(new SimpleWindowDelegate(browser_view));
+   } else {
+      // Information used when creating the native window.
+      CefWindowInfo window_info;
 
 #if defined(OS_WIN)
-    // On Windows we need to specify certain flags that will be passed to
-    // CreateWindowEx().
-    window_info.SetAsPopup(NULL, "cefsimple");
+      // On Windows we need to specify certain flags that will be passed to
+      // CreateWindowEx().
+      window_info.SetAsPopup(NULL, "cefsimple");
 #endif
 
-    // Create the first browser window.
-    CefBrowserHost::CreateBrowser(window_info, handler, fUrl, browser_settings,
-                                  NULL);
-  }
+      // Create the first browser window.
+      CefBrowserHost::CreateBrowser(window_info, handler, fUrl, browser_settings, NULL);
+   }
 }
