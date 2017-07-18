250ea6b250368b07a209987189629601a89c94bb
IlievskiV <ilievski.vladimir@live.com>
2017-07-18 14:20:34 +0200

    feat: Define Conv and Max Pool Layer propagation GPU backend Define the Conv and Max Pool Layers Forward and Backward passes, for the GPU architecture

--- tmva/tmva/src/DNN/Architectures/Cuda/Propagation.cu
+++ tmva/tmva/src/DNN/Architectures/Cuda/Propagation.cu
@@ -128,5 +128,144 @@ void TCuda<AFloat>::Copy(TCudaMatrix<AFloat> & B,
                    m * n * sizeof(AFloat), cudaMemcpyDeviceToDevice, 0);
 }
 
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::Im2col(TCudaMatrix<AFloat> &A,
+                           TCudaMatrix<AFloat> &B,
+                           size_t imgHeight,
+                           size_t imgWidth,
+                           size_t fltHeight,
+                           size_t fltWidth,
+                           size_t strideRows,
+                           size_t strideCols,
+                           size_t zeroPaddingHeight,
+                           size_t zeroPaddingWidth)
+{
+
+}
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::RotateWeights(TCudaMatrix<AFloat> &A,
+                                  const TCudaMatrix<AFloat> &B,
+                                  size_t filterDepth,
+                                  size_t filterHeight,
+                                  size_t filterWidth,
+                                  size_t numFilters)
+{
+
+}
+
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::ConvLayerBackward(std::vector<TCudaMatrix<AFloat>> & activation_gradients_backward,
+                                      TCudaMatrix<AFloat> & weight_gradients,
+                                      TCudaMatrix<AFloat> & bias_gradients,
+                                      std::vector<TCudaMatrix<AFloat>> & df,
+                                      const std::vector<TCudaMatrix<AFloat>> & activation_gradients,
+                                      const TCudaMatrix<AFloat> & weights,
+                                      const std::vector<TCudaMatrix<AFloat>> & activation_backward,
+                                      size_t batchSize,
+                                      size_t inputHeight,
+                                      size_t inputWidth,
+                                      size_t depth,
+                                      size_t height,
+                                      size_t width,
+                                      size_t filterDepth,
+                                      size_t filterHeight,
+                                      size_t filterWidth,
+                                      size_t nLocalViews)
+{
+
+
+}
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::CalculateConvActivationGradients(
+                                    std::vector<TCudaMatrix<AFloat>> & activation_gradients_backward,
+                                    std::vector<TCudaMatrix<AFloat>> & df,
+                                    const TCudaMatrix<AFloat> & weights,
+                                    size_t batchSize,
+                                    size_t inputHeight,
+                                    size_t inputWidth,
+                                    size_t depth,
+                                    size_t height,
+                                    size_t width,
+                                    size_t filterDepth,
+                                    size_t filterHeight,
+                                    size_t filterWidth)
+{
+
+}
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::CalculateConvWeightGradients(TCudaMatrix<AFloat> & weight_gradients,
+                                                 std::vector<TCudaMatrix<AFloat>> & df,
+                                                 const std::vector<TCudaMatrix<AFloat>> & activations_backward,
+                                                 size_t batchSize,
+                                                 size_t inputHeight,
+                                                 size_t inputWidth,
+                                                 size_t depth,
+                                                 size_t height,
+                                                 size_t width,
+                                                 size_t filterDepth,
+                                                 size_t filterHeight,
+                                                 size_t filterWidth,
+                                                 size_t nLocalViews)
+{
+
+}
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::CalculateConvBiasGradients(TCudaMatrix<AFloat> & bias_gradients,
+                                               std::vector<TCudaMatrix<AFloat>> & df,
+                                               size_t batchSize,
+                                               size_t depth,
+                                               size_t nLocalViews)
+{
+
+}
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::AddConvBiases(TCudaMatrix<AFloat> &output,
+                                  const TCudaMatrix<AFloat> &biases)
+{
+
+}
+
+
+//____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::Downsample(TCudaMatrix<AFloat> &A,
+                               TCudaMatrix<AFloat> &B,
+                               const TCudaMatrix<AFloat> &C,
+                               size_t imgHeight,
+                               size_t imgWidth,
+                               size_t fltHeight,
+                               size_t fltWidth,
+                               size_t strideRows,
+                               size_t strideCols)
+{
+
+}
+
+/____________________________________________________________________________
+template<typename AFloat>
+void TCuda<AFloat>::MaxPoolLayerBackward(std::vector<TCudaMatrix<AFloat>> & activationGradientsBackward,
+                                         const std::vector<TCudaMatrix<AFloat>> & activationGradients,
+                                         const std::vector<TCudaMatrix<AFloat>> & indexMatrix,
+                                         size_t batchSize,
+                                         size_t depth,
+                                         size_t nLocalViews)
+{
+
+}
+
 } // namespace DNN
 } // namespace TMVA
