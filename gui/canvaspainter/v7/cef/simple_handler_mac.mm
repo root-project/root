1cc17f7e7c08d11413fbc3cd15609e89f61355de
Sergey Linev <S.Linev@gsi.de>
2017-07-18 14:24:22 +0200

    webgui: reformat not-yet-used mac source

--- gui/canvaspainter/v7/cef/simple_handler_mac.mm
+++ gui/canvaspainter/v7/cef/simple_handler_mac.mm
@@ -8,11 +8,11 @@
 
 #include "include/cef_browser.h"
 
-void SimpleHandler::PlatformTitleChange(CefRefPtr<CefBrowser> browser,
-                                        const CefString& title) {
-  NSView* view = (NSView*)browser->GetHost()->GetWindowHandle();
-  NSWindow* window = [view window];
-  std::string titleStr(title);
-  NSString* str = [NSString stringWithUTF8String:titleStr.c_str()];
-  [window setTitle:str];
+void SimpleHandler::PlatformTitleChange(CefRefPtr<CefBrowser> browser, const CefString &title)
+{
+   NSView *view = (NSView *)browser->GetHost()->GetWindowHandle();
+   NSWindow *window = [view window];
+   std::string titleStr(title);
+   NSString *str = [NSString stringWithUTF8String:titleStr.c_str()];
+   [window setTitle:str];
 }
