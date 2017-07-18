c6f14efe3dd596bb348a95ec86b04b418534381d
Sergey Linev <S.Linev@gsi.de>
2017-07-18 10:26:53 +0200

    webgui: apply clang format for the CEF and Qt5 code

--- gui/canvaspainter/v7/cef/simple_handler.cxx
+++ gui/canvaspainter/v7/cef/simple_handler.cxx
@@ -14,121 +14,117 @@
 #include "include/wrapper/cef_closure_task.h"
 #include "include/wrapper/cef_helpers.h"
 
-
 namespace {
 
-SimpleHandler* g_instance = NULL;
+SimpleHandler *g_instance = NULL;
 
-}  // namespace
+} // namespace
 
-SimpleHandler::SimpleHandler(bool use_views)
-    : use_views_(use_views),
-      is_closing_(false) {
-  DCHECK(!g_instance);
-  g_instance = this;
+SimpleHandler::SimpleHandler(bool use_views) : use_views_(use_views), is_closing_(false)
+{
+   DCHECK(!g_instance);
+   g_instance = this;
 }
 
-SimpleHandler::~SimpleHandler() {
-  g_instance = NULL;
+SimpleHandler::~SimpleHandler()
+{
+   g_instance = NULL;
 }
 
 // static
-SimpleHandler* SimpleHandler::GetInstance() {
-  return g_instance;
+SimpleHandler *SimpleHandler::GetInstance()
+{
+   return g_instance;
 }
 
-void SimpleHandler::OnTitleChange(CefRefPtr<CefBrowser> browser,
-                                  const CefString& title) {
-  CEF_REQUIRE_UI_THREAD();
+void SimpleHandler::OnTitleChange(CefRefPtr<CefBrowser> browser, const CefString &title)
+{
+   CEF_REQUIRE_UI_THREAD();
 
-  if (use_views_) {
-    // Set the title of the window using the Views framework.
-    CefRefPtr<CefBrowserView> browser_view =
-        CefBrowserView::GetForBrowser(browser);
-    if (browser_view) {
-      CefRefPtr<CefWindow> window = browser_view->GetWindow();
-      if (window)
-        window->SetTitle(title);
-    }
-  } else {
-    // Set the title of the window using platform APIs.
-    PlatformTitleChange(browser, title);
-  }
+   if (use_views_) {
+      // Set the title of the window using the Views framework.
+      CefRefPtr<CefBrowserView> browser_view = CefBrowserView::GetForBrowser(browser);
+      if (browser_view) {
+         CefRefPtr<CefWindow> window = browser_view->GetWindow();
+         if (window) window->SetTitle(title);
+      }
+   } else {
+      // Set the title of the window using platform APIs.
+      PlatformTitleChange(browser, title);
+   }
 }
 
-void SimpleHandler::OnAfterCreated(CefRefPtr<CefBrowser> browser) {
-  CEF_REQUIRE_UI_THREAD();
+void SimpleHandler::OnAfterCreated(CefRefPtr<CefBrowser> browser)
+{
+   CEF_REQUIRE_UI_THREAD();
 
-  // Add to the list of existing browsers.
-  browser_list_.push_back(browser);
+   // Add to the list of existing browsers.
+   browser_list_.push_back(browser);
 }
 
-bool SimpleHandler::DoClose(CefRefPtr<CefBrowser> browser) {
-  CEF_REQUIRE_UI_THREAD();
+bool SimpleHandler::DoClose(CefRefPtr<CefBrowser> browser)
+{
+   CEF_REQUIRE_UI_THREAD();
 
-  // Closing the main window requires special handling. See the DoClose()
-  // documentation in the CEF header for a detailed destription of this
-  // process.
-  if (browser_list_.size() == 1) {
-    // Set a flag to indicate that the window close should be allowed.
-    is_closing_ = true;
-  }
+   // Closing the main window requires special handling. See the DoClose()
+   // documentation in the CEF header for a detailed destription of this
+   // process.
+   if (browser_list_.size() == 1) {
+      // Set a flag to indicate that the window close should be allowed.
+      is_closing_ = true;
+   }
 
-  // Allow the close. For windowed browsers this will result in the OS close
-  // event being sent.
-  return false;
+   // Allow the close. For windowed browsers this will result in the OS close
+   // event being sent.
+   return false;
 }
 
-void SimpleHandler::OnBeforeClose(CefRefPtr<CefBrowser> browser) {
-  CEF_REQUIRE_UI_THREAD();
+void SimpleHandler::OnBeforeClose(CefRefPtr<CefBrowser> browser)
+{
+   CEF_REQUIRE_UI_THREAD();
 
-  // Remove from the list of existing browsers.
-  BrowserList::iterator bit = browser_list_.begin();
-  for (; bit != browser_list_.end(); ++bit) {
-    if ((*bit)->IsSame(browser)) {
-      browser_list_.erase(bit);
-      break;
-    }
-  }
+   // Remove from the list of existing browsers.
+   BrowserList::iterator bit = browser_list_.begin();
+   for (; bit != browser_list_.end(); ++bit) {
+      if ((*bit)->IsSame(browser)) {
+         browser_list_.erase(bit);
+         break;
+      }
+   }
 
-  if (browser_list_.empty()) {
-    // All browser windows have closed. Quit the application message loop.
-    CefQuitMessageLoop();
-  }
+   if (browser_list_.empty()) {
+      // All browser windows have closed. Quit the application message loop.
+      CefQuitMessageLoop();
+   }
 }
 
-void SimpleHandler::OnLoadError(CefRefPtr<CefBrowser> browser,
-                                CefRefPtr<CefFrame> frame,
-                                ErrorCode errorCode,
-                                const CefString& errorText,
-                                const CefString& failedUrl) {
-  CEF_REQUIRE_UI_THREAD();
+void SimpleHandler::OnLoadError(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, ErrorCode errorCode,
+                                const CefString &errorText, const CefString &failedUrl)
+{
+   CEF_REQUIRE_UI_THREAD();
 
-  // Don't display an error for downloaded files.
-  if (errorCode == ERR_ABORTED)
-    return;
+   // Don't display an error for downloaded files.
+   if (errorCode == ERR_ABORTED) return;
 
-  // Display a load error message.
-  std::stringstream ss;
-  ss << "<html><body bgcolor=\"white\">"
-        "<h2>Failed to load URL " << std::string(failedUrl) <<
-        " with error " << std::string(errorText) << " (" << errorCode <<
-        ").</h2></body></html>";
-  frame->LoadString(ss.str(), failedUrl);
+   // Display a load error message.
+   std::stringstream ss;
+   ss << "<html><body bgcolor=\"white\">"
+         "<h2>Failed to load URL "
+      << std::string(failedUrl) << " with error " << std::string(errorText) << " (" << errorCode
+      << ").</h2></body></html>";
+   frame->LoadString(ss.str(), failedUrl);
 }
 
-void SimpleHandler::CloseAllBrowsers(bool force_close) {
-  if (!CefCurrentlyOn(TID_UI)) {
-    // Execute on the UI thread.
-    CefPostTask(TID_UI,
-        base::Bind(&SimpleHandler::CloseAllBrowsers, this, force_close));
-    return;
-  }
+void SimpleHandler::CloseAllBrowsers(bool force_close)
+{
+   if (!CefCurrentlyOn(TID_UI)) {
+      // Execute on the UI thread.
+      CefPostTask(TID_UI, base::Bind(&SimpleHandler::CloseAllBrowsers, this, force_close));
+      return;
+   }
 
-  if (browser_list_.empty())
-    return;
+   if (browser_list_.empty()) return;
 
-  BrowserList::const_iterator it = browser_list_.begin();
-  for (; it != browser_list_.end(); ++it)
-    (*it)->GetHost()->CloseBrowser(force_close);
+   BrowserList::const_iterator it = browser_list_.begin();
+   for (; it != browser_list_.end(); ++it) (*it)->GetHost()->CloseBrowser(force_close);
 }
--- gui/canvaspainter/v7/qt5/rootwebpage.cpp
+++ gui/canvaspainter/v7/qt5/rootwebpage.cpp
@@ -2,7 +2,8 @@
 /// \ingroup CanvasPainter ROOT7
 /// \author Sergey Linev <S.Linev@gsi.de>
 /// \date 2017-06-29
-/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
 
 /*************************************************************************
  * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
@@ -12,13 +13,12 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-
 #include "rootwebpage.h"
 
 #include <stdio.h>
 
-void RootWebPage::javaScriptConsoleMessage(JavaScriptConsoleMessageLevel,
-                      const QString &message, int lineNumber, const QString &sourceID)
+void RootWebPage::javaScriptConsoleMessage(JavaScriptConsoleMessageLevel, const QString &message, int lineNumber,
+                                           const QString &sourceID)
 {
    QByteArray ba = message.toLatin1();
    QByteArray src = sourceID.toLatin1();
