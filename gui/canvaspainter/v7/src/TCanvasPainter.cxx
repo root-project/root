9023ece8e7344df1380e0e57875902986b1ef4bc
Sergey Linev <S.Linev@gsi.de>
2017-07-21 10:04:32 +0200

    webgui: add support of png image generation
    Contrary to SVG, it is binary format. 
    It is transfered in base64 coding and then decoded.

--- gui/canvaspainter/v7/src/TCanvasPainter.cxx
+++ gui/canvaspainter/v7/src/TCanvasPainter.cxx
@@ -35,6 +35,120 @@
 #include "TClass.h"
 #include "TBufferJSON.h"
 
+// =================================================================
+
+#include <ctype.h>
+
+/*
+   base64.cpp and base64.h
+   base64 encoding and decoding with C++.
+   Version: 1.01.00
+   Copyright (C) 2004-2017 René Nyffenegger
+   This source code is provided 'as-is', without any express or implied
+   warranty. In no event will the author be held liable for any damages
+   arising from the use of this software.
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+   1. The origin of this source code must not be misrepresented; you must not
+      claim that you wrote the original source code. If you use this source code
+      in a product, an acknowledgment in the product documentation would be
+      appreciated but is not required.
+   2. Altered source versions must be plainly marked as such, and must not be
+      misrepresented as being the original source code.
+   3. This notice may not be removed or altered from any source distribution.
+   René Nyffenegger rene.nyffenegger@adp-gmbh.ch
+*/
+
+#include <iostream>
+
+namespace {
+
+static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "abcdefghijklmnopqrstuvwxyz"
+                                        "0123456789+/";
+
+static inline bool is_base64(unsigned char c)
+{
+   return (isalnum(c) || (c == '+') || (c == '/'));
+}
+
+std::string base64_encode(unsigned char const *bytes_to_encode, unsigned int in_len)
+{
+   std::string ret;
+   int i = 0;
+   int j = 0;
+   unsigned char char_array_3[3];
+   unsigned char char_array_4[4];
+
+   while (in_len--) {
+      char_array_3[i++] = *(bytes_to_encode++);
+      if (i == 3) {
+         char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+         char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+         char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+         char_array_4[3] = char_array_3[2] & 0x3f;
+
+         for (i = 0; (i < 4); i++) ret += base64_chars[char_array_4[i]];
+         i = 0;
+      }
+   }
+
+   if (i) {
+      for (j = i; j < 3; j++) char_array_3[j] = '\0';
+
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+      for (j = 0; (j < i + 1); j++) ret += base64_chars[char_array_4[j]];
+
+      while ((i++ < 3)) ret += '=';
+   }
+
+   return ret;
+}
+
+std::string base64_decode(std::string const &encoded_string)
+{
+   int in_len = encoded_string.size();
+   int i = 0;
+   int j = 0;
+   int in_ = 0;
+   unsigned char char_array_4[4], char_array_3[3];
+   std::string ret;
+
+   while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+      char_array_4[i++] = encoded_string[in_];
+      in_++;
+      if (i == 4) {
+         for (i = 0; i < 4; i++) char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+         char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+         char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+         char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+         for (i = 0; (i < 3); i++) ret += char_array_3[i];
+         i = 0;
+      }
+   }
+
+   if (i) {
+      for (j = 0; j < i; j++) char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+
+      for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+   }
+
+   return ret;
+}
+
+} // namespace
+
+// ==================================================================
+
 namespace {
 
 /** \class TCanvasPainter
@@ -306,7 +420,7 @@ Bool_t TCanvasPainter::ProcessWS(THttpCallArg *arg)
       // TODO: temporary solution, should be removed later
       // used now to terminate ROOT session
       gROOT->ProcessLine(cdata + 5);
-   } else if (strncmp(cdata, "DONEIMG:", 8) == 0) {
+   } else if (strncmp(cdata, "DONESVG:", 8) == 0) {
       TString buf(cdata + 8);
       Int_t pos = buf.First(':');
       if (pos > 0) {
@@ -315,7 +429,25 @@ Bool_t TCanvasPainter::ProcessWS(THttpCallArg *arg)
          std::ofstream ofs(fname.Data());
          ofs.write(buf.Data(), buf.Length());
          ofs.close();
-         printf("Create file %s len %d\n", fname.Data(), buf.Length());
+         printf("Create SVG file %s len %d\n", fname.Data(), buf.Length());
+      }
+      conn->fReady = kTRUE;
+      CheckModifiedFlag();
+   } else if (strncmp(cdata, "DONEPNG:", 8) == 0) {
+      TString buf(cdata + 8);
+      Int_t pos = buf.First(':');
+      if (pos > 0) {
+         TString fname = buf(0, pos);
+         buf.Remove(0, pos + 1);
+
+         std::string png = base64_decode(buf.Data());
+
+         std::ofstream ofs(fname.Data());
+         ofs.write(png.c_str(), png.length());
+         ofs.close();
+         printf("Create PNG file %s len %d\n", fname.Data(), (int) png.length());
+      } else {
+         printf("Error when producing PNG image %s\n", buf.Data());
       }
       conn->fReady = kTRUE;
       CheckModifiedFlag();
