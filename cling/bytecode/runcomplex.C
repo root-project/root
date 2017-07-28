29941f7f354aa65e0ef820859d267874520e6a0f
Pere Mato <pere.mato@cern.ch>
2017-07-28 12:21:09 +0200

    Fix for ROOT-8829 - Any unexpected error output on 'assert' tests will make the test fail

--- cling/bytecode/runcomplex.C
+++ cling/bytecode/runcomplex.C
@@ -4,19 +4,18 @@ using namespace std;
 
 double Smith_x(double R, double Y)
 {
-    complex<double> Z(R,Y);
-    complex<double> Gamma = (Z-complex<double>(1.0,0.0))/(Z+complex<double>(1.0,0.0));
-    double result = Gamma.real();
-    return result;
+   complex<double> Z(R,Y);
+   complex<double> Gamma = (Z-complex<double>(1.0,0.0))/(Z+complex<double>(1.0,0.0));
+   double result = Gamma.real();
+   return result;
 }
 
 void runcomplex()
 {
-    for(int i=0; i<11; ++i)
-    {
-	double x = Smith_x(0.0, 0.0);
-	cerr<<"x = "<<x<<endl;
-    }
+   for(int i = 0; i < 11; ++i) {
+      double x = Smith_x(0.0, 0.0);
+      cout << "x = " << x << endl;
+   }
 }
 
 
--- root/core/assertROOT8542.C
+++ root/core/assertROOT8542.C
@@ -1,6 +1,7 @@
 // Test ROOT-8542.
 int assertROOT8542() {
    int interpError = 0;
+   gErrorIgnoreLevel = kBreak;
    gROOT->ProcessLine(".x ThisFileDoesNotExist.C", &interpError);
    if (interpError == TInterpreter::kNoError) {
       std::cerr << "TApplication did not set error flag when .x-ing a missing file!\n";
