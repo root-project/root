ac0de75b8a3e502f6e96c4e21c200e747c1dcc0b
Raphael Isemann <teemperor@gmail.com>
2017-07-28 13:43:04 +0200

    Use ROOT_STANDARD_LIBRARY_PACKAGE where possible [NFC]
    This refactors the CMake build files to no longer manually call
    the CMake functions for generating and linking the dicionairies.
    One reason is to reduce boilerplate, the other is that it is now
    no longer possible to have naming mismatches between the dictionary
    generation arguments and linking function arguments which was causing
    the race conditions we had in our build system.
    
    This also now follows more strictly the separation between dependencies
    like Math, Tree and Core and pure linking flags to external libraries.

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -816,13 +816,86 @@ function(ROOT_INSTALL_HEADERS)
 endfunction()
 
 #---------------------------------------------------------------------------------------------------
-#---ROOT_STANDARD_LIBRARY_PACKAGE(libname DEPENDENCIES lib1 lib2)
+#---ROOT_STANDARD_LIBRARY_PACKAGE(libname
+#                                 [NO_INSTALL_HEADERS]         : don't install headers for this package
+#                                 [STAGE1]                     : use rootcling_stage1 for generating
+#                                 HEADERS header1 header2      : if not specified, globbing for *.h is used)
+#                                 [NO_HEADERS]                 : don't glob to fill HEADERS variable
+#                                 SOURCES source1 source2      : if not specified, globbing for *.cxx is used)
+#                                 [NO_SOURCES]                 : don't glob to fill SOURCES variable
+#                                 [OBJECT_LIBRARY]             : use ROOT_OBJECT_LIBRARY to generate object files
+#                                                                and then use those for linking.
+#                                 LIBRARIES lib1 lib2          : linking flags such as dl, readline
+#                                 DEPENDENCIES lib1 lib2       : dependencies such as Core, MathCore
+#                                 BUILTINS builtin1 builtin2   : builtins like AFTERIMAGE
+#                                 LINKDEF LinkDef.h LinkDef2.h : linkdef files, default value is "LinkDef.h"
+#                                 DICTIONARY_OPTIONS option    : options passed to rootcling
+#                                 INSTALL_OPTIONS option       : options passed to install headers
+#                                )
 #---------------------------------------------------------------------------------------------------
 function(ROOT_STANDARD_LIBRARY_PACKAGE libname)
-  CMAKE_PARSE_ARGUMENTS(ARG "" "" "DEPENDENCIES;DICTIONARY_OPTIONS" ${ARGN})
-  ROOT_GENERATE_DICTIONARY(G__${libname} *.h MODULE ${libname} LINKDEF LinkDef.h OPTIONS ${ARG_DICTIONARY_OPTIONS})
-  ROOT_LINKER_LIBRARY(${libname} *.cxx G__${libname}.cxx DEPENDENCIES ${ARG_DEPENDENCIES})
-  ROOT_INSTALL_HEADERS()
+  set(options NO_INSTALL_HEADERS STAGE1 NO_HEADERS NO_SOURCES OBJECT_LIBRARY)
+  set(oneValueArgs)
+  set(multiValueArgs DEPENDENCIES HEADERS SOURCES BUILTINS LIBRARIES DICTIONARY_OPTIONS LINKDEF INSTALL_OPTIONS)
+  CMAKE_PARSE_ARGUMENTS(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  # Check if we have any unparsed arguments
+  if(ARG_UNPARSED_ARGUMENTS)
+    message(AUTHOR_WARNING "Unparsed arguments for ROOT_STANDARD_LIBRARY_PACKAGE: ${ARG_UNPARSED_ARGUMENTS}")
+  endif()
+  # Check that the user doesn't parse NO_HEADERS to disable globbing and HEADERS at the same time.
+  if (ARG_HEADERS AND ARG_NO_HEADERS)
+    message(AUTHOR_WARNING "HEADERS and NO_HEADERS arguments are mutually exclusive.")
+  endif()
+  if (ARG_SOURCES AND ARG_NO_SOURCES)
+    message(AUTHOR_WARNING "SOURCES and NO_SOURCES arguments are mutually exclusive.")
+  endif()
+
+  # Set default values
+  # If HEADERS/SOURCES are not parsed, we glob for those files.
+  if (NOT ARG_HEADERS AND NOT ARG_NO_HEADERS)
+    set(ARG_HEADERS "*.h")
+  endif()
+  if (NOT ARG_SOURCES AND NOT ARG_NO_SOURCES)
+    set(ARG_SOURCES "*.cxx")
+  endif()
+  if (NOT ARG_LINKDEF)
+    set(ARG_LINKDEF "LinkDef.h")
+  endif()
+
+  if (ARG_STAGE1)
+    set(STAGE1_FLAG "STAGE1")
+  endif()
+
+  ROOT_GENERATE_DICTIONARY(G__${libname} ${ARG_HEADERS}
+                          MODULE ${libname}
+                          ${STAGE1_FLAG}
+                          LINKDEF ${ARG_LINKDEF}
+                          OPTIONS ${ARG_DICTIONARY_OPTIONS}
+                          DEPENDENCIES ${ARG_DEPENDENCIES}
+                          BUILTINS ${ARG_BUILTINS}
+                          )
+
+  if (ARG_OBJECT_LIBRARY)
+    ROOT_OBJECT_LIBRARY(${libname}Objs ${ARG_SOURCES} G__${libname}.cxx)
+    ROOT_LINKER_LIBRARY(${libname} $<TARGET_OBJECTS:${libname}Objs>
+                        LIBRARIES ${ARG_LIBRARIES}
+                        DEPENDENCIES ${ARG_DEPENDENCIES}
+                        BUILTINS ${ARG_BUILTINS}
+                       )
+  else(ARG_OBJECT_LIBRARY)
+    ROOT_LINKER_LIBRARY(${libname} ${ARG_SOURCES} G__${libname}.cxx
+                        LIBRARIES ${ARG_LIBRARIES}
+                        DEPENDENCIES ${ARG_DEPENDENCIES}
+                        BUILTINS ${ARG_BUILTINS}
+                       )
+  endif(ARG_OBJECT_LIBRARY)
+
+  # Install headers if we have any headers and if the user didn't explicitly
+  # disabled this.
+  if (NOT ARG_NO_INSTALL_HEADERS OR ARG_NO_HEADERS)
+    ROOT_INSTALL_HEADERS(${ARG_INSTALL_OPTIONS})
+  endif()
 endfunction()
 
 #---------------------------------------------------------------------------------------------------
