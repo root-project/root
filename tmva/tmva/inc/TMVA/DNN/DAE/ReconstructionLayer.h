48844a09a8f3439451a2f38e39167bd69fecae25
ajatgd <akshayvashistha1995@gmail.com>
2017-07-31 22:43:19 +0530

    Refactor: Migrating layers to new general layer constructor, adding deepnet test for autoencoder

--- tmva/tmva/inc/TMVA/DNN/DAE/ReconstructionLayer.h
+++ tmva/tmva/inc/TMVA/DNN/DAE/ReconstructionLayer.h
@@ -50,9 +50,6 @@ public:
    using Matrix_t = typename Architecture_t::Matrix_t;
    using Scalar_t = typename Architecture_t::Scalar_t;
 
-   Matrix_t fHBiases; ///< bias associated with hidden layer
-
-   Matrix_t fVBiases;
 
    size_t fVisibleUnits; ///< total number of visible units
 
@@ -72,8 +69,6 @@ public:
 
    Scalar_t fDropoutProbability;
 
-   size_t fEpochs;
-
    std::vector<Matrix_t> fCorruptedInput; ///< corrupted Input Units
 
    std::vector<Matrix_t> fInput;
@@ -82,7 +77,7 @@ public:
    TReconstructionLayer(size_t BatchSize, size_t VisibleUnits,
                         size_t HiddenUnits, Scalar_t learningRate, EActivationFunction f,
                         std::vector<Matrix_t> weights, std::vector<Matrix_t> biases,
-                        Scalar_t CorruptionLevel, Scalar_t dropoutProbability, size_t epochs);
+                        Scalar_t CorruptionLevel, Scalar_t dropoutProbability);
 
    /*! Copy the denoise layer provided as a pointer */
    TReconstructionLayer(TReconstructionLayer<Architecture_t> *layer);
@@ -117,15 +112,8 @@ public:
    Scalar_t GetCorruptionLevel() const {return fCorruptionLevel;}
    Scalar_t GetDropoutProbability() const { return fDropoutProbability; }
    Scalar_t GetLearningRate() const {return fLearningRate; }
-   size_t GetEpochs() const {return fEpochs;}
 
    EActivationFunction GetActivationFunction() const { return fF; }
-
-   const Matrix_t &GetHBiases() const { return fHBiases; }
-   Matrix_t &GetHBiases() { return fHBiases; }
-
-   const Matrix_t &GetVBiases() const { return fVBiases; }
-   Matrix_t &GetVBiases() { return fVBiases; }
 
    const Matrix_t &GetVBiasError() const { return fVBiasError; }
    Matrix_t &GetVBiasError() { return fVBiasError; }
@@ -155,25 +143,20 @@ template <typename Architecture_t>
 TReconstructionLayer<Architecture_t>::TReconstructionLayer(size_t batchSize, size_t visibleUnits,
                            size_t hiddenUnits, Scalar_t learningRate, EActivationFunction f, std::vector<Matrix_t> weights,
                            std::vector<Matrix_t> biases,
-                           Scalar_t corruptionLevel, Scalar_t dropoutProbability, size_t epochs)
-   : VGeneralLayer<Architecture_t>(batchSize, 1, 1, 0, 0, 0, 0, 1, hiddenUnits,visibleUnits,2, visibleUnits,
-   1, batchSize, visibleUnits, 1, EInitialization::kZero),
+                           Scalar_t corruptionLevel, Scalar_t dropoutProbability)
+   : VGeneralLayer<Architecture_t>(batchSize, 1, 1, 0, 0, 0, 0, 1, {hiddenUnits},{visibleUnits},2, {hiddenUnits,visibleUnits},
+   {1,1}, batchSize, visibleUnits, 1, EInitialization::kZero),
    fVisibleUnits(visibleUnits),
    fHiddenUnits(hiddenUnits),
    fVBiasError(visibleUnits, 1),
    fHBiasError(hiddenUnits, 1),
-   fHBiases(hiddenUnits, 1),
-   fVBiases(visibleUnits,1),
    fType(3), fF(f), fCorruptedInput(), fCorruptionLevel(corruptionLevel),
    fDropoutProbability(dropoutProbability), fInput(),
-   fLearningRate(learningRate), fEpochs(epochs)
+   fLearningRate(learningRate)
 
 {
-   for(size_t i=0; i<1; i++)
-   {
-      Architecture_t::Copy(this->GetWeightsAt(i),weights[i]);
-   }
-
+   Architecture_t::Copy(this->GetWeightsAt(0),weights[0]);
+/*
    for(size_t i=0; i<2; i++)
    {
       Architecture_t::Copy(this->GetBiasesAt(i),biases[i]);
@@ -185,13 +168,21 @@ TReconstructionLayer<Architecture_t>::TReconstructionLayer(size_t batchSize, siz
    }
 
    Architecture_t::Copy(this->GetVBiases(), this->GetBiasesAt(1));
-
+*/
+   Architecture_t::Copy(this->GetBiasesAt(0),biases[0]);
 
    for (size_t i = 0; i < batchSize; i++)
    {
       fCorruptedInput.emplace_back(visibleUnits, 1);
       fInput.emplace_back(visibleUnits,1);
    }
+   std::cout<<"Default Constructor Reconstruction: "<<std::endl;
+   std::cout<<"visible units "<<visibleUnits<<std::endl;
+   std::cout<<"hidden units "<<hiddenUnits<<std::endl;
+   std::cout<<"weights rows: "<<this->GetWeightsAt(0).GetNrows()<<std::endl;
+   std::cout<<"weights cols: "<<this->GetWeightsAt(0).GetNcols()<<std::endl;
+   std::cout<<"Bias 0 rows "<<this->GetBiasesAt(0).GetNrows()<<std::endl;
+   std::cout<<"Bias 1 rows "<<this->GetBiasesAt(1).GetNrows()<<std::endl<<std::endl;
 
 }
 
@@ -201,22 +192,16 @@ TReconstructionLayer<Architecture_t>::TReconstructionLayer(TReconstructionLayer<
    : VGeneralLayer<Architecture_t>(layer),
    fVisibleUnits(layer->GetVisibleUnits()),
    fHiddenUnits(layer->GetHiddenUnits()),
-   fHBiases(layer->GetHiddenUnits(), 1),
-   fVBiases(layer->GetVisibleUnits(),1),
    fHBiasError(layer->GetHiddenUnits(), 1),
    fVBiasError(layer->GetVisibleUnits(), 1),fType(3),
    fF(layer->GetActivationFunction()), fCorruptedInput(),
    fCorruptionLevel(layer->GetCorruptionLevel()),
    fDropoutProbability(layer->GetDropoutProbability()),
-   fInput(), fLearningRate(layer->GetLearningRate()),
-   fEpochs(layer->GetEpochs())
+   fInput(), fLearningRate(layer->GetLearningRate())
 
 {
-   for(size_t i=0; i<1; i++)
-   {
-      Architecture_t::Copy(this->GetWeightsAt(i),layer->weights[i]);
-   }
-
+   Architecture_t::Copy(this->GetWeightsAt(0),layer->weights[0]);
+/*
    for(size_t i=0; i<2; i++)
    {
      Architecture_t::Copy(this->GetBiasesAt(i),layer->biases[i]);
@@ -228,7 +213,8 @@ TReconstructionLayer<Architecture_t>::TReconstructionLayer(TReconstructionLayer<
    }
 
    Architecture_t::Copy(this->GetVBiases(), this->GetBiasesAt(1));
-
+*/
+   Architecture_t::Copy(this->GetBiasesAt(0),layer->biases[0]);
 
    size_t batchSize = layer->GetBatchSize();
    for (size_t i = 0; i < batchSize ; i++)
@@ -245,8 +231,6 @@ TReconstructionLayer<Architecture_t>::TReconstructionLayer(const TReconstruction
    : VGeneralLayer<Architecture_t>(dae),
    fVisibleUnits(dae.fVisibleUnits),
    fHiddenUnits(dae.fHiddenUnits),
-   fHBiases(dae.fVisibleUnits, 1),
-   fVBiases(dae.fVBiases,1),
    fHBiasError(dae.fHiddenUnits, 1),
    fVBiasError(dae.fVisibleUnits,1),fType(3),
    fF(dae.fActivationFunction),
@@ -254,15 +238,11 @@ TReconstructionLayer<Architecture_t>::TReconstructionLayer(const TReconstruction
    fCorruptionLevel(dae.fCorruptionLevel),
    fDropoutProbability(dae.fDropoutProbability),
    fInput(),
-   fLearningRate(dae.fLearningRate),
-   fEpochs(dae.fEpochs)
+   fLearningRate(dae.fLearningRate)
 
 {
-   for(size_t i=0; i<1; i++)
-   {
-      Architecture_t::Copy(this->GetWeightsAt(i),dae.weights[i]);
-   }
-
+   Architecture_t::Copy(this->GetWeightsAt(0),dae.weights[0]);
+/*
    for(size_t i=0; i<2; i++)
    {
       Architecture_t::Copy(this->GetBiasesAt(i),dae.biases[i]);
@@ -274,6 +254,8 @@ TReconstructionLayer<Architecture_t>::TReconstructionLayer(const TReconstruction
    }
 
    Architecture_t::Copy(this->GetVBiases(), this->GetBiasesAt(1));
+*/
+   Architecture_t::Copy(this->GetBiasesAt(0),dae.biases[0]);
 
    size_t batchSize = dae.GetBatchSize();
    for (size_t i = 0; i < batchSize ; i++)
@@ -311,17 +293,18 @@ template <typename Architecture_t>
 auto TReconstructionLayer<Architecture_t>::Forward(std::vector<Matrix_t> input, bool applyDropout)
 -> void
 {
-
+   std::cout<<"Reconstruction Forward starts "<<std::endl;
    //Corruption(input, applyDropout);
    for (size_t i = 0; i < this->GetBatchSize(); i++)
    {
       Architecture_t::ReconstructInput(input[i],this->GetOutputAt(i),this->GetWeightsAt(0));
 
-      Architecture_t::AddBiases(this->GetOutputAt(i), this->GetVBiases());
+      Architecture_t::AddBiases(this->GetOutputAt(i), this->GetBiasesAt(1));
 
       evaluate<Architecture_t>(this->GetOutputAt(i), fF);
 
    }
+   std::cout<<"Reconstruction Forward ends "<<std::endl<<std::endl;
 
 }
 
@@ -333,27 +316,41 @@ auto inline TReconstructionLayer<Architecture_t>::Backward(std::vector<Matrix_t>
                                                      const std::vector<Matrix_t> &input)
 -> void
 {
-
+   std::cout<<"Reconstruction BackWard starts "<<std::endl;
    for(size_t i=0; i<this->GetBatchSize(); i++)
    {
-      Architecture_t::Copy(this->GetInputAt(i), input[i]);
+      for(size_t j=0; j<input[i].GetNrows(); j++)
+      {
+         for(size_t k=0; k<input[i].GetNcols(); k++)
+         {
+	          this->GetInputAt(i)(j,k) = input[i](j,k);
+	       }
+      }
    }
+
    Corruption(this->GetInput(), false);
    // have to pass epochs
    // check for same corruption
-   for(size_t epoch=0; epoch<this->GetEpochs(); epoch++)
+   std::cout<<"batch size: "<<this->GetBatchSize()<<std::endl;
+
+   std::cout<<"output size: "<<this->GetOutput().size()<<std::endl;
+   std::cout<<"output rows: "<<this->GetOutputAt(0).GetNrows()<<std::endl;
+   std::cout<<"output cols: "<<this->GetOutputAt(0).GetNcols()<<std::endl;
+   std::cout<<"Weights cols "<<this->GetWeightsAt(0).GetNcols()<<std::endl;
+   std::cout<<"Weights rows "<<this->GetWeightsAt(0).GetNrows()<<std::endl;
+
+   for (size_t i = 0; i < this->GetBatchSize(); i++)
    {
-      for (size_t i = 0; i < this->GetBatchSize(); i++)
-      {
-         Architecture_t::UpdateParams(this->GetInputAt(i), this->GetCorruptedInputAt(i),
-                                     compressedInput[i],
-                                     this->GetOutputAt(i),
-                                     this->GetVBiases(),
-                                     this->GetHBiases(), this->GetWeightsAt(0),
-                                     this->GetVBiasError(), this->GetHBiasError(),
-                                     this->GetLearningRate(), this->GetBatchSize());
-      }
+      Architecture_t::UpdateParams(this->GetInputAt(i), this->GetCorruptedInputAt(i),
+                                   compressedInput[i],
+                                   this->GetOutputAt(i),
+                                   this->GetBiasesAt(1),
+                                   this->GetBiasesAt(0), this->GetWeightsAt(0),
+                                   this->GetVBiasError(), this->GetHBiasError(),
+                                   this->GetLearningRate(), this->GetBatchSize());
    }
+
+   std::cout<<"Reconstruction Backward ends "<<std::endl<<std::endl;
 }
 //______________________________________________________________________________
 template<typename Architecture_t>
@@ -364,7 +361,7 @@ auto TReconstructionLayer<Architecture_t>::Print() const
             << "Input Units: " << this->GetVisibleUnits() << "\n"
             << "Hidden Units: " << this->GetHiddenUnits() << "\n";
    std::cout<<"Reconstructed Input "<<std::endl;
-   for(size_t i=0; i<this->GetBatchSize(); i++)
+   /*for(size_t i=0; i<this->GetBatchSize(); i++)
    {
       for(size_t j=0; j<this->GetOutputAt(i).GetNrows(); j++)
 	    {
@@ -379,7 +376,7 @@ auto TReconstructionLayer<Architecture_t>::Print() const
    std::cout<<this->GetBatchSize()<<std::endl;
    std::cout<<this->GetWeights().size()<<std::endl;
    std::cout<<this->GetOutput().size()<<std::endl;
-   std::cout<<this->GetInput().size()<<std::endl;
+   std::cout<<this->GetInput().size()<<std::endl;*/
 
 }
 
