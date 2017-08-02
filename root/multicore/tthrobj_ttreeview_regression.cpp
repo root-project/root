6659916e0fdf8f3ce3bab8a948d66cade50e8d06
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-08-02 18:50:31 +0200

    [IMT] Update tthrobj_ttreeview_regression after TTreeView API changes

--- root/multicore/tthrobj_ttreeview_regression.cpp
+++ root/multicore/tthrobj_ttreeview_regression.cpp
@@ -1,6 +1,7 @@
 #include "ROOT/TTreeProcessorMT.hxx" // for TTreeView
 #include "ROOT/TThreadedObject.hxx"
 #include "ROOT/TThreadExecutor.hxx"
+#include "RStringView.h"
 #include "TFile.h"
 #include "TTree.h"
 #include <iostream>
@@ -25,33 +26,28 @@ void FillTree(const char *filename, const char *treeName)
 
 int main()
 {
+   // This test checks that concurrent calls to TTreeView::SetCurrent (now TTreeView::FindOrOpenFile) are thread safe
+   // See ROOT-8787 for more details.
    auto treeName = "t";
    std::vector<std::string_view> fnames = {"tthrobj_ttreeview_regression_1.root",
                                            "tthrobj_ttreeview_regression_2.root"};
-   for (auto &fname : fnames) FillTree(fname.data(), treeName);
-   std::vector<size_t> args = {0, 1};
+   for (const auto &fname : fnames) FillTree(fname.data(), treeName);
+   std::vector<std::size_t> args = {0, 1};
 
    ROOT::EnableImplicitMT(2);
-   //ROOT::EnableThreadSafety();
    ROOT::TThreadExecutor pool;
 
    {
       ROOT::TThreadedObject<ROOT::Internal::TTreeView> treeView(fnames, treeName);
-      auto lambda = [&](size_t s) {
-         treeView->SetCurrent(s);
-      };
+      auto lambda = [&](std::size_t s) { treeView->FindOrOpenFile(s); };
       pool.Foreach(lambda, args);
       std::cout << "finished 0" << std::endl;
    }
 
    ROOT::TThreadedObject<ROOT::Internal::TTreeView> treeView(fnames, treeName);
-   auto lambda = [&](size_t s) {
-      treeView->SetCurrent(s);
-   };
+   auto lambda = [&](size_t s) { treeView->FindOrOpenFile(s); };
    pool.Foreach(lambda, args);
    std::cout << "finished 1" << std::endl;
 
    return 0;
 }
-
-int repro_stripped() {return main();}
