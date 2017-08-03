348d284cc1557390f214484c918a73f9cef33fa7
Danilo Piparo <danilo.piparo@cern.ch>
2017-08-03 21:42:28 +0200

    Revert "Add the year in "roots -l""
    Revert until tests are fixed
    
    This reverts commit 2fa8f94844782aab735e357f869eb64c52c0feff.

--- main/python/cmdLineUtils.py
+++ main/python/cmdLineUtils.py
@@ -932,12 +932,11 @@ def _rootLsPrintLongLs(keyList,indent,treeListing):
         datime = key.GetDatime()
         time = datime.GetTime()
         date = datime.GetDate()
-        year = datime.GetYear()
         time = _prepareTime(time)
         rec = \
             [key.GetClassName(), \
             MONTH[int(str(date)[4:6])]+" " +str(date)[6:]+ \
-            " "+time[:2]+":"+time[2:4]+" "+str(year)+" ", \
+            " "+time[:2]+":"+time[2:4], \
             key.GetName(), \
             "\""+key.GetTitle()+"\""]
         write(LONG_TEMPLATE.format(*rec,**dic),indent,end="\n")
