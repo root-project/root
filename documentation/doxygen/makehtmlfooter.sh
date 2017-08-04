c1f40a483c3fd43cd8511c30138a20847bbe18a4
Olivier Couet <olivier.couet@cern.ch>
2017-08-04 11:46:20 +0200

    Add "GVA Time"  next to the date in the footer

--- documentation/doxygen/makehtmlfooter.sh
+++ documentation/doxygen/makehtmlfooter.sh
@@ -8,7 +8,7 @@ echo '<div id="footer" style="background-color:#E5EBF3;">'
 echo '<small>'
 echo '<img class="footer" src="rootlogo_s.gif" alt="root"/></a>'
 # Doxygen unconditionally adds a space in front of $DOXYGEN_ROOT_VERSION
-echo 'ROOT'$DOXYGEN_ROOT_VERSION' - Reference Guide Generated on $datetime using Doxygen '`doxygen --version`'.'
+echo 'ROOT'$DOXYGEN_ROOT_VERSION' - Reference Guide Generated on $datetime (GVA Time) using Doxygen '`doxygen --version`'.'
 echo '</small>'
 echo '</div>'
 echo '</body>'
