ec55e614d8a711544e163535b1eb38ca04cf49ee
Brian Bockelman <bbockelm@cse.unl.edu>
2017-08-07 10:59:34 -0500

    Mark integers as being able to deserialize in-place.

--- tree/tree/inc/TLeafI.h
+++ tree/tree/inc/TLeafI.h
@@ -39,6 +39,7 @@ public:
 
    virtual void    Export(TClonesArray *list, Int_t n);
    virtual void    FillBasket(TBuffer &b);
+   virtual DeserializeType GetDeserializeType() const { return fLeafCount ? kDestructive : kInPlace; }
    const char     *GetTypeName() const;
    virtual Int_t   GetMaximum() const {return fMaximum;}
    virtual Int_t   GetMinimum() const {return fMinimum;}
