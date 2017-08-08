114364c6f7efebef26ec3c00c68324159530ac85
Danilo Piparo <danilo.piparo@cern.ch>
2017-08-08 10:54:09 +0200

    [TDF] Make tutorials macros w/o main function and includes

--- tutorials/dataframe/tdf004_cutFlowReport.C
+++ tutorials/dataframe/tdf004_cutFlowReport.C
@@ -8,16 +8,6 @@
 /// \date December 2016
 /// \author Danilo Piparo
 
-#include "Math/Vector3D.h"
-#include "Math/Vector4D.h"
-#include "TCanvas.h"
-#include "TMath.h"
-#include "TRandom3.h"
-#include "TFile.h"
-#include "TTree.h"
-
-#include "ROOT/TDataFrame.hxx"
-
 using FourVector = ROOT::Math::XYZTVector;
 using FourVectors = std::vector<FourVector>;
 using CylFourVector = ROOT::Math::RhoEtaPhiVector;
@@ -79,10 +69,4 @@ void tdf004_cutFlowReport()
    filtered3.Report();
    std::cout << "All stats:" << std::endl;
    d.Report();
-}
-
-int main()
-{
-   tdf004_cutFlowReport();
-   return 0;
 }
