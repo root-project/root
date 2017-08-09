324c339994ade05f12ee351fe6bbac5ea5424658
Axel Naumann <Axel.Naumann@cern.ch>
2017-08-09 18:30:01 +0200

    Use ProcessLine() to enable auto load. Document.

--- cling/dict/fwd-decl-stdless/execLessyTest.C
+++ cling/dict/fwd-decl-stdless/execLessyTest.C
@@ -1,3 +1,16 @@
+// CMS saw an issue with loading dictionaries that fwd declare std::less
+// (because of specializations that their code provides). Because Sema sees
+// re-decls of the default template arguments in these fwd decls it marks the
+// most recent redecl of std::less as invalid.
+// Subsequent use of std::less - in their case as a template template argument -
+// causes errors.
+//
+// This test is producing such a dictionary and using std::less after parsing
+// the fwd decls from the dictionary, as template template argument of UseLess.
+// If std::less is marked invalid, the return value of
+//   gInterpreter->ProcessLine("useless.get()")
+// will be 0.
+
 const char* code = R"CODE(
 template <int I, template <class> class L>
 struct UseLess{
@@ -9,20 +22,20 @@ UseLess<1001, std::less> useless;)CODE";
 
 int execLessyTest()
 {
-   // Trigger autoload.
-   edm::AJet a;
-
    // Check whether std::less became invalid:
    TInterpreter::EErrorCode err = TInterpreter::kNoError;
-   if (!gInterpreter->Declare(code))
+   gInterpreter->ProcessLine(code, &err);
+   if (err != TInterpreter::kNoError)
       return 1;
-   long res = gInterpreter->ProcessLine("useless.get()", &err);
 
+   long res = gInterpreter->ProcessLine("useless.get()", &err);
    if (err != TInterpreter::kNoError)
       return 2;
+
    if (res != 1001) {
       printf("RES=%ld\n", res);
       return 3;
    }
+
    return 0;
 }
