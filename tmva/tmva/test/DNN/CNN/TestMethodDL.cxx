a4b4072ca1339ff0e59d209b35e780d2adbd941b
IlievskiV <ilievski.vladimir@live.com>
2017-08-15 21:05:54 +0200

    test: Add test for testing Method DL for CPU

---
+++ tmva/tmva/test/DNN/CNN/TestMethodDL.cxx
@@ -0,0 +1,36 @@
+// @(#)root/tmva/tmva/cnn:$Id$
+// Author: Vladimir Ilievski
+
+/**********************************************************************************
+ * Project: TMVA - a Root-integrated toolkit for multivariate data analysis       *
+ * Package: TMVA                                                                  *
+ * Class  :                                                                       *
+ * Web    : http://tmva.sourceforge.net                                           *
+ *                                                                                *
+ * Description:                                                                   *
+ *      Testing Method DL for Conv Net for the Reference backend                  *
+ *                                                                                *
+ * Authors (alphabetical):                                                        *
+ *      Vladimir Ilievski      <ilievski.vladimir@live.com>  - CERN, Switzerland  *
+ *                                                                                *
+ * Copyright (c) 2005-2015:                                                       *
+ *      CERN, Switzerland                                                         *
+ *      U. of Victoria, Canada                                                    *
+ *      MPI-K Heidelberg, Germany                                                 *
+ *      U. of Bonn, Germany                                                       *
+ *                                                                                *
+ * Redistribution and use in source and binary forms, with or without             *
+ * modification, are permitted according to the terms listed in LICENSE           *
+ * (http://tmva.sourceforge.net/LICENSE)                                          *
+ **********************************************************************************/
+
+#include "TestMethodDL.h"
+#include "TString.h"
+
+int main()
+{
+   std::cout << "Testing Method DL for CPU backend: " << std::endl;
+
+   TString archCPU = "CPU";
+   testMethodDL(archCPU);
+}
