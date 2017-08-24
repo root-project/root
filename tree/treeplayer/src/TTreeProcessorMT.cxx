f2a21e3b961433d82e8d0efb85f4cfbb3351aed6
Danilo Piparo <danilo.piparo@cern.ch>
2017-08-24 08:00:32 +0200

    Add T*Executor and TTreeProcessorM{T,P} to the Parallelism group in Doxygen

--- tree/treeplayer/src/TTreeProcessorMT.cxx
+++ tree/treeplayer/src/TTreeProcessorMT.cxx
@@ -10,6 +10,7 @@
  *************************************************************************/
 
 /** \class ROOT::TTreeProcessorMT
+    \ingroup Parallelism
     \brief A class to process the entries of a TTree in parallel.
 
 By means of its Process method, ROOT::TTreeProcessorMT provides a way to process the
