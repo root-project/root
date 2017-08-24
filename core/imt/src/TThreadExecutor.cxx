e33ad85bb942c9a4d1b7cb61d27e17769e2ad09d
Danilo Piparo <danilo.piparo@cern.ch>
2017-08-24 08:00:32 +0200

    Add T*Executor and TTreeProcessorM{T,P} to the Parallelism group in Doxygen

--- core/imt/src/TThreadExecutor.cxx
+++ core/imt/src/TThreadExecutor.cxx
@@ -4,6 +4,7 @@
 //////////////////////////////////////////////////////////////////////////
 ///
 /// \class ROOT::TThreadExecutor
+/// \ingroup Parallelism
 /// \brief This class provides a simple interface to execute the same task
 /// multiple times in parallel, possibly with different arguments every
 /// time. This mimics the behaviour of python's pool.Map method.
