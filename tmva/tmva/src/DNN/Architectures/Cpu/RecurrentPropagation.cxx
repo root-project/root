6b54d1bb5d3e8c6d07fdf444403944f74588bcfd
Saurav Shekhar <sauravshekhar01@gmail.com>
2017-08-25 12:07:30 +0200

    MethodDL RNN Parser added

--- tmva/tmva/src/DNN/Architectures/Cpu/RecurrentPropagation.cxx
+++ tmva/tmva/src/DNN/Architectures/Cpu/RecurrentPropagation.cxx
@@ -25,16 +25,16 @@ namespace DNN
 {
   
 template<typename AFloat>
-auto RecurrentLayerBackward(TCpuMatrix<AFloat> & state_gradients_backward, // BxH
-                            TCpuMatrix<AFloat> & input_weight_gradients,
-                            TCpuMatrix<AFloat> & state_weight_gradients,
-                            TCpuMatrix<AFloat> & bias_gradients,
-                            TCpuMatrix<AFloat> & df, //DxH
-                            const TCpuMatrix<AFloat> & state, // BxH
-                            const TCpuMatrix<AFloat> & weights_input, // HxD 
-                            const TCpuMatrix<AFloat> & weights_state, // HxH
-                            const TCpuMatrix<AFloat> & input,  // BxD
-                            TCpuMatrix<AFloat> & input_gradient)
+auto TCpu<AFloat>::RecurrentLayerBackward(TCpuMatrix<AFloat> & state_gradients_backward, // BxH
+                                          TCpuMatrix<AFloat> & input_weight_gradients,
+                                          TCpuMatrix<AFloat> & state_weight_gradients,
+                                          TCpuMatrix<AFloat> & bias_gradients,
+                                          TCpuMatrix<AFloat> & df, //DxH
+                                          const TCpuMatrix<AFloat> & state, // BxH
+                                          const TCpuMatrix<AFloat> & weights_input, // HxD 
+                                          const TCpuMatrix<AFloat> & weights_state, // HxH
+                                          const TCpuMatrix<AFloat> & input,  // BxD
+                                          TCpuMatrix<AFloat> & input_gradient)
 -> TCpuMatrix<AFloat> &
 {
   // TODO
