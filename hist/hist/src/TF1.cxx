90572bf2a1e5fb8e32cebc06e590aeb61b766973
arthur-tsang <arthurltsang@gmail.com>
2017-08-28 10:46:37 +0200

    Formul params3 (#860)
    * add NSUM functionality to TF1
    
    * parse NSUM in TF1
    
    * uncommented cout-s
    
    * adding parameter specification to TFormula (WIP)
    
    * basic parsing of user-defined functions (need to make more robust)
    
    * fix parameter naming; partially fix ReplaceAllName
    
    * mostly fix ReplaceAllName; HandleUserFunctions works on easy cases
    
    * fix parameter naming
    
    * fix ReplaceAllName (again\!) ; add parameter-only option (not well tested)
    
    * easy parametrized function case working (still not robust)
    
    * replacements take place in parallel now
    
    * swaps working properly
    
    * add argument parsing for parametrized functions
    
    * add parsing test for user-functions with arguments
    
    * add parameter range shortcut (e.g. [1..3])
    
    * can parse variables given in form "x[N]"
    
    * inherit parameter values (iff parameter names match)
    
    * fix bug about undefined parameters when using NSUM
    
    * fix whitespace; add a few comments
    
    * run clang-format
    
    * add test for nested functions
    
    * apply clang-format
    
    * fix memory leaks
    
    * clang format (again\!)
    
    * clang-format (missed two lines last time!)
    
    * add documentation to TFormula

--- hist/hist/src/TF1.cxx
+++ hist/hist/src/TF1.cxx
@@ -527,15 +527,15 @@ TF1::TF1(const char *name, const char *formula, Double_t xmin, Double_t xmax, EA
       coeffNames->SetOwner(kTRUE);
       TString fullFormula("");
       for (int i = 0; i < formDense.Length(); ++i) {
-      	 if (formDense[i] == '(')
-      	    parenCount++;
-      	 else if (formDense[i] == ')')
-      	    parenCount--;
-      	 else if (formDense[i] == delimiter && parenCount == 0) {
-      	    // term goes from termStart to i
-      	    DefineNSUMTerm(newFuncs, coeffNames, fullFormula, formDense, termStart, i, xmin, xmax);
-      	    termStart = i + 1;
-      	 }
+         if (formDense[i] == '(')
+            parenCount++;
+         else if (formDense[i] == ')')
+            parenCount--;
+         else if (formDense[i] == delimiter && parenCount == 0) {
+            // term goes from termStart to i
+            DefineNSUMTerm(newFuncs, coeffNames, fullFormula, formDense, termStart, i, xmin, xmax);
+            termStart = i + 1;
+         }
       }
       DefineNSUMTerm(newFuncs, coeffNames, fullFormula, formDense, termStart, formDense.Length(), xmin, xmax);
 
@@ -555,12 +555,12 @@ TF1::TF1(const char *name, const char *formula, Double_t xmin, Double_t xmax, EA
 
       // Parameter names
       for (int i = 0; i < fNpar; i++) {
-      	 if (coeffNames->At(i) != nullptr) {
-	    TString coeffName = ((TObjString *)coeffNames->At(i))->GetString();
-	    this->SetParName(i, (const char *)coeffName);
-      	 } else {
-	    this->SetParName(i, normSum->GetParName(i));
-	 }
+         if (coeffNames->At(i) != nullptr) {
+            TString coeffName = ((TObjString *)coeffNames->At(i))->GetString();
+            this->SetParName(i, (const char *)coeffName);
+         } else {
+            this->SetParName(i, normSum->GetParName(i));
+         }
       }
       
    } else { // regular TFormula
@@ -754,10 +754,9 @@ Bool_t TF1::AddToGlobalList(Bool_t on)
 
 // Defines the formula that a given term uses, if not already defined,
 // and appends "sanitized" formula to `fullFormula` string
-void TF1::DefineNSUMTerm(TObjArray *newFuncs, TObjArray *coeffNames,
-			 TString &fullFormula,
-			 TString &formula, int termStart, int termEnd,
-			 Double_t xmin, Double_t xmax) {
+void TF1::DefineNSUMTerm(TObjArray *newFuncs, TObjArray *coeffNames, TString &fullFormula, TString &formula,
+                         int termStart, int termEnd, Double_t xmin, Double_t xmax)
+{
    TString originalTerm = formula(termStart, termEnd-termStart);
    int coeffLength = TermCoeffLength(originalTerm);
    if (coeffLength != -1)
