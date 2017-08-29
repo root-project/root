d0488fbfaa892ba7fa3fa0beab1ced186290339c
Saurav Shekhar <sauravshekhar01@gmail.com>
2017-08-29 06:13:27 +0200

    CPU (Blas) Support added

--- tmva/tmva/src/DNN/Architectures/Cpu/RecurrentPropagation.cxx
+++ tmva/tmva/src/DNN/Architectures/Cpu/RecurrentPropagation.cxx
@@ -37,7 +37,30 @@ auto TCpu<AFloat>::RecurrentLayerBackward(TCpuMatrix<AFloat> & state_gradients_b
                                           TCpuMatrix<AFloat> & input_gradient)
 -> TCpuMatrix<AFloat> &
 {
-  // TODO
+   // Compute element-wise product.
+   Hadamard(df, state_gradients_backward);  // B x H 
+   
+   // Input gradients.
+   if (input_gradient.GetNElements() > 0) Multiply(input_gradient, df, weights_input);
+
+   // State gradients.
+   if (state_gradients_backward.GetNElements() > 0) Multiply(state_gradients_backward, df, weights_state);
+
+   // Weights gradients
+   if (input_weight_gradients.GetNElements() > 0) {
+      TCpuMatrix<AFloat> tmp(input_weight_gradients);
+      TransposeMultiply(input_weight_gradients, df, input); // H x B . B x D
+      ScaleAdd(input_weight_gradients, tmp, 1);
+   }
+   if (state_weight_gradients.GetNElements() > 0) {
+      TCpuMatrix<AFloat> tmp(state_weight_gradients);
+      TransposeMultiply(state_weight_gradients, df, state); // H x B . B x H
+      ScaleAdd(state_weight_gradients, tmp, 1);
+   }
+
+   // Bias gradients.
+   if (bias_gradients.GetNElements() > 0) SumColumns(bias_gradients, df);
+   return input_gradient;
 }
 
 } // namespace DNN
