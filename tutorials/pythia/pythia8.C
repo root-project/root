320f4a004773410dce3553473a37888729ee117e
Martin Storo Nyflott <martin.storo.nyflott@cern.ch>
2017-08-29 15:10:44 +0200

    No reason to explicitly load pythia8 due to autoloading

--- tutorials/pythia/pythia8.C
+++ tutorials/pythia/pythia8.C
@@ -48,13 +48,6 @@ void pythia8(Int_t nev  = 100, Int_t ndeb = 1)
    }
 
 // Load libraries
-#ifndef G__WIN32 // Pythia8 is a static library on Windows
-   if (gSystem->Getenv("PYTHIA8")) {
-      gSystem->Load("$PYTHIA8/lib/libpythia8");
-   } else {
-      gSystem->Load("libpythia8");
-   }
-#endif
    gSystem->Load("libEG");
    gSystem->Load("libEGPythia8");
 // Histograms
