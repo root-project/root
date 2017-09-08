81efbe4aa19119b610264bc2c3f0077a05ab7c24
Danilo Piparo <danilo.piparo@cern.ch>
2017-09-08 06:34:50 +0200

    [TDF] More gtests, now also executed with implicit multithreading activated

--- tree/treeplayer/test/testTDF_simple.cxx
+++ tree/treeplayer/test/testTDF_simple.cxx
@@ -1,81 +1,16 @@
-#include "ROOT/TDataFrame.hxx"
-#include "TRandom.h"
-#include "TInterpreter.h"
+#define TEST_CATEGORY TDataFrameSeq
+#include "testTDF_simple_tests.hxx"
 
-#include "gtest/gtest.h"
+#ifdef R__USE_IMT
 
-using namespace ROOT::Experimental;
-
-/*
-Tests we need to cover:
-o define
-o jitted define
-o filter
-o jitted filter
-o named filter
-o named jitted filter
-o (defines) X filters
-o list of branches present but empty
- */
-
-TEST(TDataFrame, CreateEmpty)
+#include "TROOT.h"
+TEST(testTDF_simple, EnableImplicitMT)
 {
-   TDataFrame tdf(10);
+   ROOT::EnableImplicitMT();
 }
 
-// Define
+#undef TEST_CATEGORY
+#define TEST_CATEGORY TDataFrameMT
+#include "testTDF_simple_tests.hxx"
 
-TEST(TDataFrame, Define_lambda)
-{
-   TDataFrame tdf(10);
-   auto d = tdf.Define("i", [](){return 1;});
-   auto m = d.Mean("i");
-}
-
-int DefineFunction() {return 1;}
-
-TEST(TDataFrame, Define_function)
-{
-   TDataFrame tdf(10);
-   auto d = tdf.Define("i", DefineFunction);
-   auto m = d.Mean("i");
-}
-
-struct DefineStruct {
-   int operator()() {return 1;}
-};
-
-TEST(TDataFrame, Define_functor)
-{
-   TDataFrame tdf(10);
-   DefineStruct def;
-   auto d = tdf.Define("i", def);
-   auto m = d.Mean("i");
-}
-
-TEST(TDataFrame, Define_jitted)
-{
-   TDataFrame tdf(10);
-   auto d = tdf.Define("i", "1");
-   auto m = d.Mean("i");
-}
-
-TEST(TDataFrame, Define_jitted_complex)
-{
-   gInterpreter->ProcessLine("TRandom r(1);");
-   TDataFrame tdf(100);
-   auto d = tdf.Define("i", "r.Uniform(0.,8.)");
-   auto m = d.Mean("i");
-}
-
-
-// Filter
-
-TEST(TDataFrame, Define_Filter)
-{
-   TRandom r(1);
-   TDataFrame tdf(100);
-   auto d = tdf.Define("r", [&r](){return r.Uniform(0.,8.);});
-   auto df = d.Filter([](double x){ return x > 5;},{"r"});
-   auto m = df.Mean("r");
-}
+#endif
