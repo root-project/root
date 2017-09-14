48519801693a35ed481a3d3ee3c4f85dd32f335b
Pere Mato <pere.mato@cern.ch>
2017-09-14 11:24:24 +0200

    Initial chnages to support C++ list intializers in Python using tuples

--- bindings/pyroot/src/Converters.h
+++ bindings/pyroot/src/Converters.h
@@ -14,6 +14,7 @@
 
 namespace PyROOT {
 
+   class ObjectProxy;
    struct TParameter;
    struct TCallContext;
 
@@ -198,13 +199,14 @@ namespace PyROOT {
 
    class TRefCppObjectConverter : public TConverter  {
    public:
-      TRefCppObjectConverter( Cppyy::TCppType_t klass ) : fClass( klass ) {}
+      TRefCppObjectConverter( Cppyy::TCppType_t klass ) : fClass( klass ), fObjProxy(nullptr) {}
 
    public:
       virtual Bool_t SetArg( PyObject*, TParameter&, TCallContext* ctxt = 0 );
 
    protected:
       Cppyy::TCppType_t fClass;
+      ObjectProxy* fObjProxy;
    };
 
    template <bool ISREFERENCE>
