61d41c24a49085f27950aca3503c71585cdd955d
Philippe Canal <pcanal@fnal.gov>
2017-09-14 16:24:35 -0500

    Avoid loading troublesome library (libGviz).
    When combined with all other libraries (and in particular libRInterface via libTMVA), this
    can lead to a 'crash' due to running out of thread_local slots (or something like that).

--- root/core/execStatusBitsCheck.C
+++ root/core/execStatusBitsCheck.C
@@ -1,5 +1,20 @@
+void SkipLibrary(const char *libskip)
+{
+   TEnv* mapfile = gInterpreter->GetMapfile();
+   if (!mapfile || !mapfile->GetTable()) return 0;
+
+   for(auto rec : TRangeStaticCast<TEnvRec>(mapfile->GetTable())) {
+      if (strstr(rec->GetValue(),libskip)) {
+         mapfile->SetValue(rec->GetName(),"");
+      }
+   }
+}
+
+
 int execStatusBitsCheck(bool verbosity = false)
 {
+   SkipLibrary("libGviz");
+
    gSystem->LoadAllLibraries();
    printf("First verify verbose output in the case of TStreamerElement\n");
    ROOT::Detail::TStatusBitsChecker::Check("TStreamerElement",true);
