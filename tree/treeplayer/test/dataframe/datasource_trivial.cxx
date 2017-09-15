343162d7f241f9335c514eff04006cb40617f405
Danilo Piparo <danilo.piparo@cern.ch>
2017-09-15 15:50:08 +0200

    [TDF] Add the TTrivialDS and its unit tests
    The TTrivialDS is a very simple TDataSource, de facto a generator.
    It is useful as example and for testing the behaviour of the TDF in combination with
    external data sources.

---
+++ tree/treeplayer/test/dataframe/datasource_trivial.cxx
@@ -0,0 +1,69 @@
+#include "ROOT/TTrivialDS.hxx"
+
+#include "gtest/gtest.h"
+
+using namespace ROOT::Experimental;
+using namespace ROOT::Experimental::TDF;
+
+TEST(TTrivialDS, ColTypeNames)
+{
+   TTrivialDS tds(32);
+   tds.SetNSlots(1);
+
+   auto colName = tds.GetColumnNames()[0]; // We know it's one.
+   EXPECT_STREQ("col0", colName.c_str());
+   EXPECT_STREQ("ULong64_t", tds.GetTypeName("col0").c_str());
+
+   EXPECT_TRUE(tds.HasColumn("col0"));
+   EXPECT_FALSE(tds.HasColumn("col1"));
+}
+
+TEST(TTrivialDS, EntryRanges)
+{
+   TTrivialDS tds(32);
+   const auto nSlots = 4U;
+   tds.SetNSlots(nSlots);
+
+   auto ranges = tds.GetEntryRanges();
+
+   EXPECT_EQ(4U, ranges.size());
+   EXPECT_EQ(0U, ranges[0].first);
+   EXPECT_EQ(8U, ranges[0].second);
+   EXPECT_EQ(8U, ranges[1].first);
+   EXPECT_EQ(16U, ranges[1].second);
+   EXPECT_EQ(16U, ranges[2].first);
+   EXPECT_EQ(24U, ranges[2].second);
+   EXPECT_EQ(24U, ranges[3].first);
+   EXPECT_EQ(32U, ranges[3].second);
+}
+
+TEST(TTrivialDS, ColumnReaders)
+{
+   TTrivialDS tds(32);
+   const auto nSlots = 4U;
+   tds.SetNSlots(nSlots);
+   auto vals = tds.GetColumnReaders<ULong64_t>("col0");
+   auto ranges = tds.GetEntryRanges();
+   auto slot = 0U;
+   for (auto &&range : ranges) {
+      for (auto i : ROOT::TSeq<ULong64_t>(range.first, range.second)) {
+      tds.SetEntry(i, slot);
+      auto val = **vals[slot];
+      EXPECT_EQ(i, val);
+      }
+      slot++;
+  }
+}
+
+TEST(TTrivialDS, FromATDF)
+{
+  std::unique_ptr<TDataSource> tds(new TTrivialDS(32));
+  TDataFrame tdf(std::move(tds));
+  auto max = tdf.Max<ULong64_t>("col0");
+  auto min = tdf.Min<ULong64_t>("col0");
+  auto c = tdf.Count();
+
+  EXPECT_EQ(32U, *c);
+  EXPECT_DOUBLE_EQ(31., *max);
+  EXPECT_DOUBLE_EQ(0., *min);
+}
