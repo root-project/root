0ca0f078a587f6e6f43ef6be133df0be7b6036ae
Olivier Couet <olivier.couet@cern.ch>
2017-09-15 16:37:36 +0200

    Some X11 server are slower to sync.

--- graf2d/asimage/src/TASImage.cxx
+++ graf2d/asimage/src/TASImage.cxx
@@ -1116,9 +1116,10 @@ void TASImage::FromPad(TVirtualPad *pad, Int_t x, Int_t y, UInt_t w, UInt_t h)
       return;
    }
 
-   // synchronization
+   // X11 Synchronization
    gVirtualX->Update(1);
    if (!gThreadXAR) {
+      gSystem->Sleep(100);
       gSystem->ProcessEvents();
       gSystem->Sleep(10);
       gSystem->ProcessEvents();
@@ -6638,9 +6639,10 @@ void TASImage::FromWindow(Drawable_t wid, Int_t x, Int_t y, UInt_t w, UInt_t h)
    x = x < 0 ? 0 : x;
    y = y < 0 ? 0 : y;
 
-   // synchronization
+   // X11 Synchronization
    gVirtualX->Update(1);
    if (!gThreadXAR) {
+      gSystem->Sleep(10);
       gSystem->ProcessEvents();
       gSystem->Sleep(10);
       gSystem->ProcessEvents();
