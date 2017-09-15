379d037dbd787eefa5c77aeb2d475b884ff12a83
Olivier Couet <olivier.couet@cern.ch>
2017-09-15 10:55:00 +0200

    Improve SetWindowSize  synchronisation.
    When called many time in a row (>300) it could give wrong result with the X11 Backend.

--- gui/gui/src/TRootCanvas.cxx
+++ gui/gui/src/TRootCanvas.cxx
@@ -1296,7 +1296,7 @@ void TRootCanvas::SetWindowSize(UInt_t w, UInt_t h)
    gVirtualX->Update(1);
    if (!gThreadXAR) {
       gSystem->ProcessEvents();
-      gSystem->Sleep(10);
+      gSystem->Sleep(100);
       gSystem->ProcessEvents();
    }
 }
