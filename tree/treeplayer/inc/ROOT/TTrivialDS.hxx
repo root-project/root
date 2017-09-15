c3e2a0281d2dd99fdce74f84cdbf983406a5aaf7
Danilo Piparo <danilo.piparo@cern.ch>
2017-09-15 15:50:08 +0200

    [TDF] Add the TTrivialDS and its unit tests
    The TTrivialDS is a very simple TDataSource, de facto a generator.
    It is useful as example and for testing the behaviour of the TDF in combination with
    external data sources.

---
+++ tree/treeplayer/inc/ROOT/TTrivialDS.hxx
@@ -0,0 +1,76 @@
+#ifndef ROOT_TTRIVIALTDS
+#define ROOT_TTRIVIALTDS
+
+#include "ROOT/TDataFrame.hxx"
+#include "ROOT/TDataSource.hxx"
+#include "ROOT/TSeq.hxx"
+
+namespace ROOT {
+namespace Experimental {
+namespace TDF {
+
+class TTrivialDS final : public ROOT::Experimental::TDF::TDataSource {
+private:
+   unsigned int fNSlots = 0U;
+   ULong64_t fSize = 0ULL;
+   std::vector<std::pair<ULong64_t, ULong64_t>> fEntryRanges;
+   std::vector<std::string> fColNames{"col0"};
+   std::vector<ULong64_t> fCounter;
+   std::vector<ULong64_t *> fCounterAddr;
+   std::vector<void *> GetColumnReadersImpl(std::string_view name, const std::type_info &)
+   {
+      std::vector<void *> ret;
+      for (auto i : ROOT::TSeqU(fNSlots)) {
+         fCounterAddr[i] = &fCounter[i];
+         ret.emplace_back((void *)(&fCounterAddr[i]));
+      }
+      return ret;
+   }
+
+public:
+   TTrivialDS(ULong64_t size) : fSize(size) {}
+
+   ~TTrivialDS() {}
+
+   const std::vector<std::string> &GetColumnNames() const { return fColNames; }
+
+   bool HasColumn(std::string_view colName) const { return colName == fColNames[0]; }
+
+   std::string GetTypeName(std::string_view) const { return "ULong64_t"; }
+
+   const std::vector<std::pair<ULong64_t, ULong64_t>> &GetEntryRanges() const
+   {
+      if (fEntryRanges.empty()) {
+         throw std::runtime_error("No ranges are available. Did you set the number of slots?");
+      }
+      return fEntryRanges;
+   }
+   void SetEntry(ULong64_t entry, unsigned int slot) { fCounter[slot] = entry; }
+
+   void SetNSlots(unsigned int nSlots)
+   {
+      if (0U != fNSlots) {
+         return;
+      }
+
+      fNSlots = nSlots;
+      fCounter.resize(fNSlots);
+      fCounterAddr.resize(fNSlots);
+
+      auto chunckSize = fSize / fNSlots;
+      auto start = 0UL;
+      auto end = 0UL;
+      for (auto i : ROOT::TSeqUL(fNSlots)) {
+         start = end;
+         end += chunckSize;
+         fEntryRanges.emplace_back(start, end);
+      }
+      // TODO: redistribute reminder to all slots
+      fEntryRanges.back().second += fSize % fNSlots;
+   };
+};
+}
+}
+}
+
+#endif
