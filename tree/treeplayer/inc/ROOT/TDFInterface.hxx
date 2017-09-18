9cbd059b9df3ecb47ebb420d1260249fd0c9a57a
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-09-18 23:44:06 +0200

    [TDF] Avoid copying values of data-source columns

--- tree/treeplayer/inc/ROOT/TDFInterface.hxx
+++ tree/treeplayer/inc/ROOT/TDFInterface.hxx
@@ -1322,9 +1322,9 @@ private:
       assert(fDataSource != nullptr);
       const auto nSlots = fProxiedPtr->GetNSlots();
       auto readers = fDataSource->GetColumnReaders<T>(name, nSlots);
-      auto getValue = [readers](unsigned int slot) { return **readers[slot]; };
-      using NewCol_t = TDFDetail::TCustomColumn<decltype(getValue), true>;
-      lm.Book(std::make_shared<NewCol_t>(name, std::move(getValue), ColumnNames_t{}, &lm, true));
+      auto getValue = [readers](unsigned int slot) { return *readers[slot]; };
+      using NewCol_t = TDFDetail::TCustomColumn<decltype(getValue), /*PassSlotNumber=*/true>;
+      lm.Book(std::make_shared<NewCol_t>(name, std::move(getValue), ColumnNames_t{}, &lm, /*isDSColumn=*/true));
       lm.AddDataSourceColumn(name);
    }
 
