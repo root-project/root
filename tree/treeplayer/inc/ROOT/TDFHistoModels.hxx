b0000546db52990eb7a0f13762dbe69a7d636959
Danilo Piparo <danilo.piparo@cern.ch>
2017-09-20 22:21:25 +0200

    [TDF] Implement destructors of TH{1,2,3}Models non inline
    to solve the missing symbol issue of gtests on osx.

--- tree/treeplayer/inc/ROOT/TDFHistoModels.hxx
+++ tree/treeplayer/inc/ROOT/TDFHistoModels.hxx
@@ -31,7 +31,7 @@ struct TH1DModel {
 
    TH1DModel() = delete;
    TH1DModel(const TH1DModel &) = default;
-   ~TH1DModel() = default;
+   ~TH1DModel();
    TH1DModel(const ::TH1D &h);
    TH1DModel(const char *name, const char *title, int nbinsx, double xlow, double xup);
 };
@@ -48,7 +48,7 @@ struct TH2DModel {
 
    TH2DModel() = delete;
    TH2DModel(const TH2DModel &) = default;
-   ~TH2DModel() = default;
+   ~TH2DModel();
    TH2DModel(const ::TH2D &h);
    TH2DModel(const char *name, const char *title, int nbinsx, double xlow, double xup, int nbinsy, double ylow,
              double yup);
@@ -69,7 +69,7 @@ struct TH3DModel {
 
    TH3DModel() = delete;
    TH3DModel(const TH3DModel &) = default;
-   ~TH3DModel() = default;
+   ~TH3DModel();
    TH3DModel(const ::TH3D &h);
    TH3DModel(const char *name, const char *title, int nbinsx, double xlow, double xup, int nbinsy, double ylow,
              double yup, int nbinsz, double zlow, double zup);
