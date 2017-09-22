9571c13bf833fda10d05625414d34cab041613ff
Danilo Piparo <danilo.piparo@cern.ch>
2017-09-22 10:50:18 +0200

    [TDF] More tests for the parallelised access to the DSs

--- tree/treeplayer/test/dataframe/datasource_trivial.cxx
+++ tree/treeplayer/test/dataframe/datasource_trivial.cxx
@@ -101,9 +101,32 @@ TEST(TTrivialDS, FromATDFWithJitting)
 
 // NOW MT!-------------
 
+TEST(TTrivialDS, DefineSlotCheckMT)
+{
+   auto nSlots = 4U;
+   ROOT::EnableImplicitMT(nSlots);
+
+   std::hash<std::thread::id> hasher;
+   using H_t = decltype(hasher(std::this_thread::get_id()));
+
+   std::vector<H_t> ids(nSlots, 0);
+   std::unique_ptr<TDataSource> tds(new TTrivialDS(nSlots));
+   TDataFrame d(std::move(tds));
+   auto m = d.DefineSlot("x", [&](unsigned int slot) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                ids[slot] = hasher(std::this_thread::get_id());
+                return 1.;
+             }).Max("x");
+
+   EXPECT_EQ(1, *m); // just in case
+
+   std::set<H_t> s(ids.begin(), ids.end());
+   EXPECT_EQ(nSlots, s.size());
+   EXPECT_TRUE(s.end() == s.find(0));
+}
+
 TEST(TTrivialDS, FromATDFMT)
 {
-   ROOT::EnableImplicitMT(4);
    std::unique_ptr<TDataSource> tds(new TTrivialDS(320));
    TDataFrame tdf(std::move(tds));
    auto max = tdf.Max<ULong64_t>("col0");
