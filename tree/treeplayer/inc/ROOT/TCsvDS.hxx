2e4daf4897bd0a760b472dedac805febac5aac75
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2017-09-26 17:25:52 +0200

    CSV Data Source implementation.
    This commit provides the implementation of a new data source for
    TDataFrame that is able to read CSV files.
    
    Some of its features include:
    - Type inference (int, double, bool, string)
    - Configurable delimiter
    - Quoted fields & quote escaping support
    - File content stored in memory

---
+++ tree/treeplayer/inc/ROOT/TCsvDS.hxx
@@ -0,0 +1,54 @@
+#ifndef ROOT_TCSVTDS
+#define ROOT_TCSVTDS
+
+#include "ROOT/TDataSource.hxx"
+
+#include <map>
+#include <vector>
+#include <TRegexp.h>
+
+namespace ROOT {
+namespace Experimental {
+namespace TDF {
+
+class TCsvDS final : public ROOT::Experimental::TDF::TDataSource {
+
+private:
+   typedef std::vector<void *> Record;
+
+   unsigned int fNSlots = 0U;
+   std::string fFileName;
+   char fDelimiter;
+   std::vector<std::string> fHeaders;
+   std::map<std::string, std::string> fColTypes;
+   std::vector<std::vector<void *>> fColAddresses; // fColAddresses[column][slot]
+   std::vector<std::pair<ULong64_t, ULong64_t>> fEntryRanges;
+   std::vector<Record> fRecords; // fRecords[entry][column]
+
+   static TRegexp intRegex, doubleRegex1, doubleRegex2, trueRegex, falseRegex;
+   
+   void FillHeaders(std::string &);
+   void FillRecord(std::string &, Record &);
+   void GenerateHeaders(size_t);
+   std::vector<void *> GetColumnReadersImpl(std::string_view, const std::type_info &);
+   void InferColTypes(std::vector<std::string> &);
+   void InferType(std::string &, unsigned int);
+   std::vector<std::string> ParseColumns(std::string &);
+   size_t ParseValue(std::string &, std::vector<std::string> &, size_t);
+
+public:
+   TCsvDS(std::string_view fileName, bool readHeaders = true, char delimiter = ',');
+   ~TCsvDS();
+   const std::vector<std::string> &GetColumnNames() const;
+   const std::vector<std::pair<ULong64_t, ULong64_t>> &GetEntryRanges() const;
+   std::string GetTypeName(std::string_view colName) const;
+   bool HasColumn(std::string_view colName) const;
+   void SetEntry(unsigned int slot, ULong64_t entry);
+   void SetNSlots(unsigned int nSlots);
+};
+
+} // ns TDF
+} // ns Experimental
+} // ns ROOT
+
+#endif
