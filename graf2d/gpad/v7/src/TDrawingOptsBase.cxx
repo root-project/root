f56ec5660b428d50d60d4a00097a5fbe6ac42f3f
Axel Naumann <Axel.Naumann@cern.ch>
2017-09-26 16:03:06 +0200

    Add base class for drawing options.

---
+++ graf2d/gpad/v7/src/TDrawingOptsBase.cxx
@@ -0,0 +1,23 @@
+/// \file TFrame.cxx
+/// \ingroup Gpad ROOT7
+/// \author Axel Naumann <axel@cern.ch>
+/// \date 2017-09-26
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "ROOT/TDrawingOptsBase.hxx"
+
+#include <cassert>
+
+std::map<std::string, std::string>
+ROOT::Experimental::Internal::ReadDrawingOptsDefaultConfig(std::string_view /*section*/) {
+   assert(0 && "Not yet implemented!");
+}
