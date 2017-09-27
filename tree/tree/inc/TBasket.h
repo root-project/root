1c02e27bdeb3fa21388defc488b0a577c7a3455a
Brian Bockelman <bbockelm@cse.unl.edu>
2017-09-27 07:00:52 -0500

    Switch special value indicator from 1 to -1.

--- tree/tree/inc/TBasket.h
+++ tree/tree/inc/TBasket.h
@@ -63,7 +63,7 @@ protected:
    Bool_t      fReadEntryOffset{kFALSE}; ///<!Set to true if offset array was read from a file.
    Int_t      *fDisplacement{nullptr};   ///<![fNevBuf] Displacement of entries in fBuffer(TKey)
    Int_t      *fEntryOffset{nullptr}; ///<[fNevBuf] Offset of entries in fBuffer(TKey); generated at runtime.  Special value
-                                 /// of `1` indicates that the offset generation MUST be performed on first read.
+                                 /// of `-1` indicates that the offset generation MUST be performed on first read.
    TBranch    *fBranch{nullptr};    ///<Pointer to the basket support branch
    TBuffer    *fCompressedBufferRef{nullptr}; ///<! Compressed buffer.
    Bool_t      fOwnsCompressedBuffer{kFALSE};   ///<! Whether or not we own the compressed buffer.
@@ -106,7 +106,7 @@ public:
            Int_t  *GetDisplacement() const {return fDisplacement;}
            Int_t *GetEntryOffset()
            {
-              return R__likely(fEntryOffset != (Int_t *)1) ? fEntryOffset : GetCalculatedEntryOffset();
+              return R__likely(fEntryOffset != reinterpret_cast<Int_t *>(-1)) ? fEntryOffset : GetCalculatedEntryOffset();
            }
            Int_t   GetEntryPointer(Int_t Entry);
            Int_t   GetNevBuf() const {return fNevBuf;}
