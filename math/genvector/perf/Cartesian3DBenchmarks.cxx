08100218c72dddf36201ff9c5dda08639c4fc01a
Vassil Vassilev <v.g.vassilev@gmail.com>
2017-09-27 17:28:26 +0200

    Revert "Match the amount of work to the scalar versions."
    This reverts commit dfb5998101a5f0d30b68ce6030b845c8c63f98c0.

--- math/genvector/perf/Cartesian3DBenchmarks.cxx
+++ math/genvector/perf/Cartesian3DBenchmarks.cxx
@@ -6,9 +6,12 @@
 
 #include "benchmark/benchmark.h"
 
-#include <sstream>
+#include <random>
 
-// FIXME: Reduce the boilerplate code of each benchmark. Maybe setting up a Benchmark Fixture will help.
+static bool is_aligned(const void *__restrict__ ptr, size_t align)
+{
+   return (uintptr_t)ptr % align == 0;
+}
 
 static void BM_Cartesian3D_Sanity(benchmark::State &state)
 {
@@ -24,92 +27,76 @@ static void BM_Cartesian3D_CreateEmpty(benchmark::State &state)
 }
 BENCHMARK(BM_Cartesian3D_CreateEmpty);
 
-static void ComputeProcessedEntities(benchmark::State &state, size_t Size, double checksum)
+// template <typename T> class Cartesian3D { T x; T y; T z; /*...*/ };
+// // ROOT::Double_v -> double[2]
+// template <> class Cartesian3D<double[2]> { double[2] x; double[2] y; double[2] z; /*...*/ };
+
+// template <> class Cartesian3D<double[2]> { double[2] x = 1.; double[2] y = 2.; double[2] z = 3.; /*...*/ };
+
+static void ComputeProcessedEntities(benchmark::State &state, size_t Size)
 {
    const size_t items_processed = state.iterations() * state.range(0);
    state.SetItemsProcessed(items_processed);
    state.SetBytesProcessed(items_processed * Size);
-   std::stringstream ss;
-   ss << checksum;
-   state.SetLabel(ss.str());
 }
 
 template <typename T>
 static void BM_Cartesian3D_Theta(benchmark::State &state)
 {
    using namespace ROOT::Benchmark;
-   // We want the same random numbers for a benchmark family.
-   SetSeed(1);
    // Allocate N points
+   typename Data<T>::Vector Points(state.range(0));
    constexpr size_t VecSize = TypeSize<T>::Get();
-   typename Data<T>::Vector Points(state.range(0) / VecSize);
    ROOT::Math::Cartesian3D<T> c;
-   T checksum = T();
-   while (state.KeepRunning()) {
-      checksum = T();
-      for (auto &p : Points) {
-         c.SetCoordinates(p.X, p.Y, p.Z);
-         checksum += c.Theta();
+   while (state.KeepRunning())
+      for (size_t i = 0, e = Points.size(); i < e; i += VecSize) {
+         c.SetCoordinates(Points[i].X, Points[i].Y, Points[i].Z);
+         c.Theta();
       }
-   }
-   ComputeProcessedEntities(state, sizeof(T), VecTraits<T>::Sum(checksum));
+   ComputeProcessedEntities(state, sizeof(T));
 }
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, double)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, ROOT::Double_v)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, float)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, ROOT::Float_v)->Range(8 << 0, 8 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, double)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, ROOT::Double_v)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, float)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Theta, ROOT::Float_v)->Range(1 << 0, 1 << 10);
 
 template <typename T>
 static void BM_Cartesian3D_Phi(benchmark::State &state)
 {
    using namespace ROOT::Benchmark;
-   // We want the same random numbers for a benchmark family.
-   SetSeed(2);
    // Allocate N points
+   typename Data<T>::Vector Points(state.range(0));
    constexpr size_t VecSize = TypeSize<T>::Get();
-   typename Data<T>::Vector Points(state.range(0) / VecSize);
    ROOT::Math::Cartesian3D<T> c;
-   T checksum = T();
-   while (state.KeepRunning()) {
-      checksum = T();
-      for (auto &p : Points) {
-         c.SetCoordinates(p.X, p.Y, p.Z);
-         checksum += c.Phi();
+   while (state.KeepRunning())
+      for (size_t i = 0, e = Points.size(); i < e; i += VecSize) {
+         c.SetCoordinates(Points[i].X, Points[i].Y, Points[i].Z);
+         c.Phi();
       }
-   }
-   ComputeProcessedEntities(state, sizeof(T), VecTraits<T>::Sum(checksum));
+   ComputeProcessedEntities(state, sizeof(T));
 }
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, double)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, ROOT::Double_v)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, float)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, ROOT::Float_v)->Range(8 << 0, 8 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, double)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, ROOT::Double_v)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, float)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Phi, ROOT::Float_v)->Range(1 << 0, 1 << 10);
 
 template <typename T>
 static void BM_Cartesian3D_Mag2(benchmark::State &state)
 {
    using namespace ROOT::Benchmark;
-   // We want the same random numbers for a benchmark family.
-   SetSeed(3);
    // Allocate N points
+   typename Data<T>::Vector Points(state.range(0));
    constexpr size_t VecSize = TypeSize<T>::Get();
-   typename Data<T>::Vector Points(state.range(0) / VecSize);
    ROOT::Math::Cartesian3D<T> c;
-   T checksum = T();
-   while (state.KeepRunning()) {
-      checksum = T();
-      for (auto &p : Points) {
-         c.SetCoordinates(p.X, p.Y, p.Z);
-         checksum += c.Mag2();
+   while (state.KeepRunning())
+      for (size_t i = 0, e = Points.size(); i < e; i += VecSize) {
+         c.SetCoordinates(Points[i].X, Points[i].Y, Points[i].Z);
+         c.Mag2();
       }
-   }
-
-   ComputeProcessedEntities(state, sizeof(T), VecTraits<T>::Sum(checksum));
+   ComputeProcessedEntities(state, sizeof(T));
 }
-
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Mag2, double)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Mag2, ROOT::Double_v)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Mag2, float)->Range(8 << 0, 8 << 10);
-BENCHMARK_TEMPLATE(BM_Cartesian3D_Mag2, ROOT::Float_v)->Range(8 << 0, 8 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Mag2, double)->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BM_Cartesian3D_Mag2, ROOT::Double_v)->Range(1 << 0, 1 << 10);
 
 // Define our main.
 BENCHMARK_MAIN();
