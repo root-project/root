f459b98642616be909dc23f9af515022bfdf903e
Axel Naumann <Axel.Naumann@cern.ch>
2017-09-27 15:32:55 +0200

    Add predefined colors. Add TColor::Alpha with kOpaque and kTransparent.

--- graf2d/gpad/v7/inc/ROOT/TColor.hxx
+++ graf2d/gpad/v7/inc/ROOT/TColor.hxx
@@ -16,15 +16,28 @@
 #ifndef ROOT7_TColor
 #define ROOT7_TColor
 
-#include <RStringView.h>
+#include <array>
 
 namespace ROOT {
 namespace Experimental {
 
 /** \class ROOT::Experimental::TColor
-  A color: Red|Green|Blue|Alpha, or an position in a TPalette
+  A color: Red|Green|Blue|Alpha, or a position in a TPalette
   */
 class TColor {
+public:
+   /** \class ROOT::Experimental::TColor::TAlpha
+    The alpha value of a color: 0 is completely transparent, 1 is completely opaque.
+    */
+   struct Alpha {
+      float fVal;
+      explicit operator float() const { return fVal; }
+   };
+   /// An opaque color.
+   static constexpr Alpha kOpaque{1.};
+   /// A completely transparent color.
+   static constexpr Alpha kTransparent{0.};
+
 private:
    /// The "R" in RGBA (0 <= R <= 1), or the palette pos if fIsRGBA is `false`.
    float fRedOrPalettePos = 0.;
@@ -38,25 +51,45 @@ private:
    /// The "A" in RGBA (0 <= A <= 1). Unused if `!fIsRGBA`. `fAlpha == 0` means so transparent it's invisible,
    /// `fAlpha == 1` means completely opaque.
    float fAlpha = 1.;
+
    /// Whether this is an RGBA color or an index in the `TPad`'s `TPalette`.
    bool fIsRGBA = true;
 
 public:
+   using Predefined = std::array<float, 3>;
+
    // Default constructor: good old solid black.
-   TColor() = default;
+   constexpr TColor() = default;
 
    /// Initialize a TColor with red, green, blue and alpha component.
-   TColor(float r, float g, float b, float alpha = 1.): fRedOrPalettePos(r), fGreen(g), fBlue(b), fAlpha(alpha) {}
+   constexpr TColor(float r, float g, float b, float alpha): fRedOrPalettePos(r), fGreen(g), fBlue(b), fAlpha(alpha) {}
 
-   bool operator==(const TColor &rhs)
+   /// Initialize a TColor with red, green, blue and alpha component.
+   constexpr TColor(float r, float g, float b, Alpha alpha = kOpaque): TColor(r, g, b, alpha.fVal) {}
+
+   /// Initialize a TColor with red, green, blue and alpha component.
+   constexpr TColor(const Predefined &predef): TColor(predef[0], predef[1], predef[2]) {}
+
+   friend bool operator==(const TColor &lhs, const TColor &rhs)
    {
-      if (fIsRGBA != rhs.fIsRGBA)
+      if (lhs.fIsRGBA != rhs.fIsRGBA)
          return false;
-      if (fIsRGBA)
-         return fRedOrPalettePos == rhs.fRedOrPalettePos;
-      return fRedOrPalettePos == rhs.fRedOrPalettePos && fGreen == rhs.fGreen && fBlue == rhs.fBlue &&
-             fAlpha == rhs.fAlpha;
+      if (lhs.fIsRGBA)
+         return lhs.fRedOrPalettePos == rhs.fRedOrPalettePos;
+      return lhs.fRedOrPalettePos == rhs.fRedOrPalettePos && lhs.fGreen == rhs.fGreen && lhs.fBlue == rhs.fBlue &&
+             lhs.fAlpha == rhs.fAlpha;
    }
+
+   ///\{
+   ///\name Default colors
+
+   // Implemented in TPalette.cxx.
+   static constexpr Predefined kRed{{0.5, 0., 0.}};
+   static constexpr Predefined kGreen{{0., 0.5, 0.}};
+   static constexpr Predefined kBlue{{0., 0, 0.5}};
+   static constexpr Predefined kWhite{{1., 1, 1.}};
+   static constexpr Predefined kBlack{{0., 0., 0.}};
+   ///\}
 };
 
 #if 0
