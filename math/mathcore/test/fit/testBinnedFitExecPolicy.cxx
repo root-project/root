d24719e4d41078a5cdf7ab1c3d65e388e0b060bb
Xavier Valls <xavier.valls.pla@cern.ch>
2017-09-28 14:52:36 +0200

    Improve BinnedFit test (#1068)
    Add speedup printing, improve legibility, refactorization.

--- math/mathcore/test/fit/testBinnedFitExecPolicy.cxx
+++ math/mathcore/test/fit/testBinnedFitExecPolicy.cxx
@@ -1,24 +1,15 @@
 #include "TH1.h"
 #include "TF1.h"
-#include "TFile.h"
 #include "TRandom.h"
-#include "Math/IParamFunction.h"
 #include "TFitResult.h"
 #include "TError.h"
 #include "TROOT.h"
 #include "Math/MinimizerOptions.h"
-
 #include <chrono>
 
-bool compareResult(double v1, double v2, std::string s = "", double tol = 0.01)
-{
-   // compare v1 with reference v2
-   //  // give 1% tolerance
-   if (std::abs(v1 - v2) < tol * std::abs(v2)) return true;
-   std::cerr << s << " Failed comparison of fit results \t chi2 = " << v1 << "   it should be = " << v2 << std::endl;
-   return false;
-}
+double tolerance = 0.01;
 
+// Evaluating function
 template <class T>
 T func(const T *data, const double *params)
 {
@@ -26,8 +17,77 @@ T func(const T *data, const double *params)
           params[1] * exp(-(params[2] * (*data * (0.01)) - params[3] * ((*data) * (0.01)) * ((*data) * (0.01))));
 }
 
+struct FitModelData {
+   std::chrono::duration<double> refTime;
+   TFitResultPtr refValue;
+   std::string name;
+   std::vector<double> speedups;
+};
+
+// Function that calls the fit, checks for numerical correctness and computes speed up
+void benchmarkFit(TF1 *f, TH1D &h, const std::string &opt, const std::string &fit, FitModelData &model)
+{
+   std::chrono::time_point<std::chrono::system_clock> start, end;
+   auto msg = fit + " " + model.name + " Fit";
+   std::cout << "\n\n **" << msg << "**\n";
+   f->SetParameters(1, 1000, 7.5, 1.5);
+   start = std::chrono::system_clock::now();
+   auto result = h.Fit(f, opt.c_str());
+   end = std::chrono::system_clock::now();
+   std::chrono::duration<double> duration = end - start;
+
+   // Check if Fit succeeds
+   if ((Int_t)result != 0) {
+      Error("testBinnedFitExecPolicy", "%s failed!", msg.c_str());
+      exit(-1);
+   }
+
+   if (!fit.compare("Sequential")) {
+      // Save reference times and values
+      model.refTime = end - start;
+      model.refValue = result;
+   } else {
+      // Check if numerical result is within tolerance
+      if (std::abs(result->MinFcnValue() - model.refValue->MinFcnValue()) >
+          tolerance * std::abs(model.refValue->MinFcnValue())) {
+         Error("testBinnedFitExecPolicy", "%s : Failed comparison of fit results \t FCN = %f, it should be = %f",
+               msg.c_str(), result->MinFcnValue(), model.refValue->MinFcnValue());
+         exit(-1);
+      }
+      // Compute speedup
+      model.speedups.emplace_back(model.refTime.count() / duration.count() * result->NCalls() /
+                                  model.refValue->NCalls());
+   }
+
+   std::cout << "Time for the " << msg << ": " << duration.count() << std::endl;
+};
+
+void printSpeedUps(std::vector<FitModelData> &models)
+{
+   std::cout.precision(2);
+   std::string longestName =
+      std::max_element(models.begin(), models.end(), [](const FitModelData &m1, const FitModelData &m2) {
+         return m1.name.size() < m2.name.size();
+      })->name;
+   std::cout << std::endl << "\n   ***Speedups! (normalized to the number of calls)***" << std::endl;
+   std::cout << std::string(longestName.size() + 1, ' ') << "|  MT    "
+             << "|  VEC   "
+             << "| MT+VEC " << std::endl;
+   // Name field + value field + three bars + 1 extra
+   std::cout << std::string(longestName.size() + 1 + 8 * 3 + 3 + 1, '-') << std::endl;
+   for (auto m : models) {
+      std::cout << m.name << std::string(longestName.size() - m.name.size() + 1, ' ');
+      for (auto su : m.speedups)
+         std::cout << std::fixed << "|  " << su << "  ";
+      std::cout << std::endl;
+   }
+   std::cout << std::endl;
+}
+
 int main()
 {
+
+   ROOT::EnableImplicitMT();
 
    TF1 *f = new TF1("fvCore", func<double>, 100, 200, 4);
    f->SetParameters(1, 1000, 7.5, 1.5);
@@ -36,134 +96,39 @@ int main()
    TH1D h1f("h1f", "Test random numbers", 12801, 100, 200);
    gRandom->SetSeed(1);
    h1f.FillRandom("fvCore", 1000000);
-
    ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2");
 
-   std::chrono::time_point<std::chrono::system_clock> start, end;
-   std::chrono::duration<double> duration;
-   std::cout << "\n **FIT: Chi2 **\n\n";
-   start = std::chrono::system_clock::now();
-   auto r1 = h1f.Fit(f, "S");
-   if ((Int_t)r1 != 0) {
-      Error("testBinnedFitExecPolicy", "Sequential Chi2 Fit failed!");
-      return -1;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Sequential Chi2 Fit: " << duration.count() << std::endl;
+   enum EModel { kChi2 = 0, kPoisson };
+   std::vector<FitModelData> models(2);
+   models[kChi2].name = "Chi2";
+   models[kPoisson].name = "Binned Likelihood";
 
-   std::cout << "\n **FIT: Binned Likelihood **\n\n";
-   f->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto rL1 = h1f.Fit(f, "S L");
-   if ((Int_t)rL1 != 0) {
-      Error("testBinnedFitExecPolicy", "Sequential  Binned Likelihood Fit failed!");
-      return -1;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Sequential  Binned Likelihood Fit: " << duration.count() << std::endl;
+   // Start testing
+   auto fit = "Sequential";
+   benchmarkFit(f, h1f, "S SERIAL", fit, models[EModel::kChi2]);
+   benchmarkFit(f, h1f, "SERIAL S L", fit, models[EModel::kPoisson]);
 
 #ifdef R__USE_IMT
-   std::cout << "\n **FIT: Multithreaded Chi2 **\n\n";
-   f->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto r2 = h1f.Fit(f, "MULTITHREAD S");
-   if ((Int_t)r2 != 0) {
-      Error("testBinnedFitExecPolicy", "Multithreaded Chi2 Fit failed!");
-      return -1;
-   } else {
-      if (!compareResult(r2->MinFcnValue(), r1->MinFcnValue(), "Mutithreaded Chi2 Fit: "))
-         return 1;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Mutithreaded Chi2 Fit: " << duration.count() << std::endl;
+   fit = "Multithreaded";
+   benchmarkFit(f, h1f, "S", fit, models[EModel::kChi2]);
+   benchmarkFit(f, h1f, "S L", fit, models[EModel::kPoisson]);
 
-   std::cout << "\n **FIT: Multithreaded Binned Likelihood **\n\n";
-   f->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto rL2 = h1f.Fit(f, "MULTITHREAD S L");
-   if ((Int_t)rL2 != 0) {
-      Error("testBinnedFitExecPolicy", "Multithreaded Binned Likelihood Fit failed!");
-      return -1;
-   } else {
-      if (!compareResult(rL2->MinFcnValue(), rL1->MinFcnValue(), "Mutithreaded Binned Likelihood Fit (PoissonLogL): "))
-         return 2;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Mutithreaded Binned Likelihood Fit :" << duration.count() << std::endl;
 #endif
-
 #ifdef R__HAS_VECCORE
    TF1 *fvecCore = new TF1("fvCore", func<ROOT::Double_v>, 100, 200, 4);
 
-   std::cout << "\n **FIT: Vectorized Chi2 **\n\n";
-   fvecCore->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto r3 = h1f.Fit(fvecCore, "S");
-   if ((Int_t)r3 != 0) {
-      Error("testBinnedFitExecPolicy", "Vectorized Chi2 Fit failed!");
-      return -1;
-   } else {
-      if (!compareResult(r3->MinFcnValue(), r1->MinFcnValue(), "Vectorized Chi2 Fit: "))
-         return 3;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Vectorized Chi2 Fit: " << duration.count() << std::endl;
-
-   std::cout << "\n **FIT: Vectorized Binned Likelihood **\n\n";
-   fvecCore->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto rL3 = h1f.Fit(fvecCore, "S L");
-   if ((Int_t)rL3 != 0) {
-      Error("testBinnedFitExecPolicy", "Vectorized Binned Likelihood Fit failed!");
-      return -1;
-   } else {
-      if (!compareResult(rL3->MinFcnValue(), rL1->MinFcnValue(),
-                         "Vectorized Binned Likelihood Fit (PoissonLogL) Fit: "))
-         return 4;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Vectorized Binned Likelihood Fit: " << duration.count() << std::endl;
+   fit = "Vectorized";
+   benchmarkFit(fvecCore, h1f, "SERIAL S", fit, models[EModel::kChi2]);
+   benchmarkFit(fvecCore, h1f, "SERIAL S L", fit, models[EModel::kPoisson]);
 
 #ifdef R__USE_IMT
-   std::cout << "\n **FIT: Mutithreaded vectorized Chi2 **\n\n";
-   fvecCore->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto r4 = h1f.Fit(fvecCore, "MULTITHREAD S");
-   if ((Int_t)r4 != 0) {
-      Error("testBinnedFitExecPolicy", "Mutithreaded vectorized Chi2 Fit failed!");
-      return -1;
-   } else {
-      if (!compareResult(r4->MinFcnValue(), r1->MinFcnValue(), "Mutithreaded vectorized Chi2 Fit: "))
-         return 5;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the Mutithreaded vectorized Chi2 Fit: " << duration.count() << std::endl;
 
-   std::cout << "\n **FIT: Multithreaded and vectorized Binned Likelihood **\n\n";
-   fvecCore->SetParameters(1, 1000, 7.5, 1.5);
-   start = std::chrono::system_clock::now();
-   auto rL4 = h1f.Fit(fvecCore, "MULTITHREAD S L");
-   if ((Int_t)rL4 != 0) {
-      Error("testBinnedFitExecPolicy", "Multithreaded Binned Likelihood vectorized Fit failed!");
-      return -1;
-   } else {
-      if (!compareResult(rL4->MinFcnValue(), rL1->MinFcnValue(),
-                         "Mutithreaded vectorized Binned Likelihood Fit (PoissonLogL) Fit: "))
-         return 6;
-   }
-   end =  std::chrono::system_clock::now();
-   duration = end - start;
-   std::cout << "Time for the multithreaded+vectorized  Binned Likelihood Fit: " << duration.count() << std::endl;
-
+   fit = "Mutithreaded and vectorized";
+   benchmarkFit(fvecCore, h1f, "S", fit, models[EModel::kChi2]);
+   benchmarkFit(fvecCore, h1f, "S L", fit, models[EModel::kPoisson]);
 
 #endif
 #endif
+   printSpeedUps(models);
    return 0;
 }
