root-project/root

commit 9cf7391ec29c2fd73b336676313d7e0b203f09d7

Avoid checking all cases for auto flush and auto save in TTree::Fill()
If we have if (a || b || c || d), we are forced to evaluate all
conditions every time. However, since fAutoSave and fAutoFlush can
only be either positive or negative, computing each separately lets
us avoid a lot of expensive modulus operations, since both defaults
are negative (default fAutoFlush=-30000000 and fAutoSave=-300000000).
These values correspond roughtly to 30MB and 300MB, respectively.

tree/tree/src/TTree.cxx
@@ -4471,16 +4471,23 @@ Int_t TTree::Fill()
       Info("TTree::Fill", " - A: %d %lld %lld %lld %lld %lld %lld \n", nbytes, fEntries, fAutoFlush, fAutoSave,
            GetZipBytes(), fFlushedBytes, fSavedBytes);
 
+   bool autoFlush = false;
+   bool autoSave = false;
+
    if (fAutoFlush != 0 || fAutoSave != 0) {
       // Is it time to flush or autosave baskets?
       if (fFlushedBytes == 0) {
+         // If fFlushedBytes == 0, it means we never flushed or saved, so
+         // we need to check if it's time to do it and recompute the values
+         // of fAutoFlush and fAutoSave in terms of the number of entries.
          // Decision can be based initially either on the number of bytes
          // or the number of entries written.
          Long64_t zipBytes = GetZipBytes();
-         if ((fAutoFlush < 0 && zipBytes > -fAutoFlush) || (fAutoSave < 0 && zipBytes > -fAutoSave) ||
-             (fAutoFlush > 0 && fEntries % TMath::Max((Long64_t)1, fAutoFlush) == 0) ||
-             (fAutoSave > 0 && fEntries % TMath::Max((Long64_t)1, fAutoSave) == 0)) {
 
+         autoFlush = fAutoFlush < 0 ? (zipBytes > -fAutoFlush) : (fEntries % TMath::Max((Long64_t)1, fAutoFlush) == 0);
+         autoSave = fAutoSave < 0 ? (zipBytes > -fAutoSave) : (fEntries % TMath::Max((Long64_t)1, fAutoSave) == 0);
+
+         if (autoFlush || autoSave) {
             // First call FlushBasket to make sure that fTotBytes is up to date.
             FlushBaskets();
             OptimizeBaskets(GetTotBytes(), 1, "");

commit 74b94aa412e1a4f0c51228568d02563772b48409

Fix TTree::Fill() when fAutoFlush or fAutoSave == 0
Commit 9cf7391ec29c2fd73b336676313d7e0b203f09d7 introduced a bug that
leads to an autosave when fAutoSave is equal to zero. This caused a call
to OptimizeBaskets() in one of the file merger tests, which changed the
file sizes and broke the test.

tree/tree/src/TTree.cxx
@@ -4484,8 +4484,11 @@ Int_t TTree::Fill()
          // or the number of entries written.
          Long64_t zipBytes = GetZipBytes();
 
-         autoFlush = fAutoFlush < 0 ? (zipBytes > -fAutoFlush) : (fEntries % TMath::Max((Long64_t)1, fAutoFlush) == 0);
-         autoSave = fAutoSave < 0 ? (zipBytes > -fAutoSave) : (fEntries % TMath::Max((Long64_t)1, fAutoSave) == 0);
+         if (fAutoFlush)
+            autoFlush = fAutoFlush < 0 ? (zipBytes > -fAutoFlush) : fEntries % fAutoFlush == 0;
+
+         if (fAutoSave)
+            autoSave = fAutoSave < 0 ? (zipBytes > -fAutoSave) : fEntries % fAutoSave == 0;
 
          if (autoFlush || autoSave) {
             // First call FlushBasket to make sure that fTotBytes is up to date.