a105cfb07049b1b52fc2258d2ccb1e25a89dfaef
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2017-10-02 14:45:09 +0200

    formatting (clang-format)

--- hist/hist/src/AnalyticalIntegrals.cxx
+++ hist/hist/src/AnalyticalIntegrals.cxx
@@ -52,9 +52,10 @@ Double_t AnalyticalIntegral(TF1 *f, Double_t a, Double_t b)
       double mean  = p[1];
       double sigma = p[2];
       if (formula->TestBit(TFormula::kNormalized) ) 
-         result =  amp*( ROOT::Math::gaussian_cdf(xmax, sigma, mean)- ROOT::Math::gaussian_cdf(xmin, sigma, mean) );
+         result = amp * ( ROOT::Math::gaussian_cdf(xmax, sigma, mean) - ROOT::Math::gaussian_cdf(xmin, sigma, mean) );
       else
-         result =  amp*sqrt(2*TMath::Pi())*sigma*(ROOT::Math::gaussian_cdf(xmax, sigma, mean)- ROOT::Math::gaussian_cdf(xmin, sigma, mean));//
+         result = amp * sqrt(2*TMath::Pi()) * sigma *
+                   (ROOT::Math::gaussian_cdf(xmax, sigma, mean) - ROOT::Math::gaussian_cdf(xmin, sigma, mean)); //
    }
    else if (num == 400)//landau: root::math::landau(x,mpv=0,sigma=1,bool norm=false)
    {
