ce6ca1a3e8baba0f36076250729b6c0a7316e83a
Danilo Piparo <danilo.piparo@cern.ch>
2017-10-05 08:27:30 +0200

    [TDF] Cache: do not test static_assert w/ non copiable types

--- tree/treeplayer/test/dataframe/dataframe_cache.cxx
+++ tree/treeplayer/test/dataframe/dataframe_cache.cxx
@@ -5,8 +5,6 @@
 #include "TRandom.h"
 #include "TSystem.h"
 
-#include "TNonCopiableDS.hxx"
-
 #include "gtest/gtest.h"
 
 #include <algorithm>
@@ -199,20 +197,6 @@ TEST(Cache, Regex)
    EXPECT_EQ(3UL, *m);
 }
 
-TEST(Cache, NonCopiable)
-{
-   std::unique_ptr<TDataSource> tds(new NonCopiableDS());
-   TDataFrame tdf(std::move(tds));
-   int ret(1);
-   try {
-      tdf.Cache(NonCopiableDS::fgColumnName);
-   } catch (const std::runtime_error &e) {
-      ret = 0;
-   }
-   EXPECT_EQ(0, ret)
-      << "The static assert was not triggered even if caching of columns of a non copiable type was requested";
-}
-
 TEST(Cache, Carrays)
 {
    auto treeName = "t";
@@ -251,5 +235,4 @@ TEST(Cache, Carrays)
    cache.Foreach(checkArr, {"arr"});
 
    gSystem->Unlink(fileName);
-
 }
