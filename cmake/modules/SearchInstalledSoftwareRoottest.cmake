9d2dd3c42c48126435b5d51038bd214490637c1f
Oksana Shadura <oksana.shadura@cern.ch>
2017-10-05 19:11:39 +0200

    Adding missing SearchInstalledSoftwareRoottest.cmake file

---
+++ cmake/modules/SearchInstalledSoftwareRoottest.cmake
@@ -0,0 +1,47 @@
+include(ExternalProject)
+
+#---Download googletest--------------------------------------------------------------
+ExternalProject_Add(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG release-1.8.0
+    UPDATE_COMMAND ""
+    # TIMEOUT 10
+    # # Force separate output paths for debug and release builds to allow easy
+    # # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
+    # CMAKE_ARGS -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
+    #            -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
+    #            -Dgtest_force_shared_crt=ON
+    CMAKE_ARGS -G ${CMAKE_GENERATOR}
+                  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                  -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                  -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+                  -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                  -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+                  -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+    # Disable install step
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS ${_gtest_byproducts}
+    # Wrap download, configure and build steps in a script to log output
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON)
+
+  # Specify include dirs for gtest and gmock
+  ExternalProject_Get_Property(googletest source_dir)
+  set(GTEST_INCLUDE_DIR ${source_dir}/googletest/include)
+  set(GMOCK_INCLUDE_DIR ${source_dir}/googlemock/include)
+
+  # Libraries
+  ExternalProject_Get_Property(googletest binary_dir)
+  set(_G_LIBRARY_PATH ${binary_dir}/googlemock/)
+
+  # Register gtest, gtest_main, gmock, gmock_main
+  foreach (lib gtest gtest_main gmock gmock_main)
+    add_library(${lib} IMPORTED STATIC GLOBAL)
+    add_dependencies(${lib} googletest)
+  endforeach()
+  set_property(TARGET gtest PROPERTY IMPORTED_LOCATION ${_G_LIBRARY_PATH}/gtest/libgtest.a)
+  set_property(TARGET gtest_main PROPERTY IMPORTED_LOCATION ${_G_LIBRARY_PATH}/gtest/libgtest_main.a)
+  set_property(TARGET gmock PROPERTY IMPORTED_LOCATION ${_G_LIBRARY_PATH}/libgmock.a)
+  set_property(TARGET gmock_main PROPERTY IMPORTED_LOCATION ${_G_LIBRARY_PATH}/libgmock_main.a)
