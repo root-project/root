25dcd667ac2550e722239bcde43fca388d3ef1de
Danilo Piparo <danilo.piparo@cern.ch>
2017-10-05 23:27:13 +0200

    [TDF] Add unit test for friends contianing arrays

--- tree/treeplayer/test/dataframe/dataframe_friends.cxx
+++ tree/treeplayer/test/dataframe/dataframe_friends.cxx
@@ -1,5 +1,7 @@
 #include "ROOT/TDataFrame.hxx"
+#include "ROOT/TSeq.hxx"
 #include "TSystem.h"
+#include "TFile.h"
 #include "TTree.h"
 #include "gtest/gtest.h"
 
@@ -10,18 +12,30 @@ class TDFAndFriends : public ::testing::Test {
 protected:
    constexpr static auto kFile1 = "test_tdfandfriends.root";
    constexpr static auto kFile2 = "test_tdfandfriends2.root";
-
+   constexpr static auto kFile3 = "test_tdfandfriends3.root";
    static void SetUpTestCase()
    {
-      TDataFrame d(10);
+      TDataFrame d(4);
       d.Define("x", [] { return 1; }).Snapshot<int>("t", kFile1, {"x"});
       d.Define("y", [] { return 2; }).Snapshot<int>("t2", kFile2, {"y"});
+
+      TFile f(kFile3, "RECREATE");
+      TTree t("t3", "t3");
+      float arr[4];
+      t.Branch("arr", arr, "arr[4]/F");
+      for (auto i : ROOT::TSeqU(4)) {
+         for (auto j : ROOT::TSeqU(4)) {
+            arr[j] = i + j;
+         }
+         t.Fill();
+      }
+      t.Write();
    }
 
    static void TearDownTestCase()
    {
-      gSystem->Unlink("test_tdfandfriends.root");
-      gSystem->Unlink("test_tdfandfriends2.root");
+      for (auto fileName : {kFile1, kFile2, kFile3})
+         gSystem->Unlink(fileName);
    }
 };
 
@@ -51,4 +65,44 @@ TEST_F(TDFAndFriends, FriendByPointer)
    auto t = d.Take<int>("y");
    for (auto v : t)
       EXPECT_EQ(v, 2);
+}
+
+TEST_F(TDFAndFriends, FriendArrayByFile)
+{
+   TFile f1(kFile1);
+   TTree *t1 = static_cast<TTree *>(f1.Get("t"));
+   t1->AddFriend("t3", kFile3);
+   TDataFrame d(*t1);
+
+   int i(0);
+   auto checkArr = [&i](std::array_view<float> av) {
+      auto ifloat = float(i);
+      EXPECT_EQ(ifloat, av[0]);
+      EXPECT_EQ(ifloat + 1, av[1]);
+      EXPECT_EQ(ifloat + 2, av[2]);
+      EXPECT_EQ(ifloat + 3, av[3]);
+      i++;
+   };
+   d.Foreach(checkArr, {"arr"});
+}
+
+TEST_F(TDFAndFriends, FriendArrayByPointer)
+{
+   TFile f1(kFile1);
+   TTree *t1 = static_cast<TTree *>(f1.Get("t"));
+   TFile f3(kFile3);
+   TTree *t3 = static_cast<TTree *>(f2.Get("t3"));
+   t1->AddFriend(t3);
+   TDataFrame d(*t1);
+
+   int i(0);
+   auto checkArr = [&i](std::array_view<float> av) {
+      auto ifloat = float(i);
+      EXPECT_EQ(ifloat, av[0]);
+      EXPECT_EQ(ifloat + 1, av[1]);
+      EXPECT_EQ(ifloat + 2, av[2]);
+      EXPECT_EQ(ifloat + 3, av[3]);
+      i++;
+   };
+   d.Foreach(checkArr, {"arr"});
 }
