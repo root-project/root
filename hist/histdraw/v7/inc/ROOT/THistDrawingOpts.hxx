818254ef6c103a6445d2140588b55cc40d065eab
Axel Naumann <Axel.Naumann@cern.ch>
2017-10-11 15:58:09 +0200

    Separate options from attributes.

--- hist/histdraw/v7/inc/ROOT/THistDrawingOpts.hxx
+++ hist/histdraw/v7/inc/ROOT/THistDrawingOpts.hxx
@@ -21,29 +21,27 @@
 namespace ROOT {
 namespace Experimental {
 
-/** \class ROOT::Experimental::THistDrawingOptsBase
+/** \class ROOT::Experimental::THistCoreAttrs
 
-Stores drawing options for a histogram. This class contains the properties
-that are independent of the histogram dimensionality.
+Stores drawing attributes for a histogram. This class contains the properties
+that are independent of the histogram dimensionality, and used by at least one of
+the visualizations (scatter, bar, etc).
 
-Customize the defaults with `THistDrawingOpts<1>::SetLineColor(TColor::kRed);` or by modifying the
+Customize the defaults with `THistDrawingOpts<1>::Default().SetLineColor(TColor::kRed);` or by modifying the
 style configuration file `rootstylerc` (e.g. $ROOTSYS/etc/system.rootstylerc or ~/.rootstylerc).
  */
-template <class DERIVED>
-class THistDrawingOptsBase: public TDrawingOptsBase<DERIVED> {
+struct THistCoreAttrs {
    /// Index of the line color in TCanvas's color table.
-   //TOptsAttrRef<TColor> fLineColorIndex{*this, "Hist.Line.Color"};
-   //TOptsAttrRef<long long> fLineColorIndex{*this, "Hist.Line.Width"};
-   TLineDrawingOpts fLine{*this, "Hist", {kBlack, 3}};
+   //TDrawingAttrRef<TColor> fLineColorIndex{*this, "Hist.Line.Color"};
+   //TDrawingAttrRef<long long> fLineColorIndex{*this, "Hist.Line.Width"};
+   TLineAttrs fLine; ///< The histogram line attributes
+   TFillAttrs fFill; ///< The histogram fill attributes
    
-public:
-   THistDrawingOptsBase() = default;
-   THistDrawingOptsBase(TPadBase &pad): TDrawingOptsBase<DERIVED>(pad) {}
-
-   /// The color of the histogram line.
-   void SetLineColor(const TColor &col) { this->Update(fLineColorIndex, col); }
-   TColor &GetLineColor() { return this->Get(fLineColorIndex); }
-   const TColor &GetLineColor() const { return this->Get(fLineColorIndex); }
+   THistCoreAttrs() = default;
+   THistCoreAttrs(TDrawingOptsBaseNoDefault &opts, const std::string &name):
+      fLine{opts, name + ".Line", TColor::kBlack, TLineAttrs::Width{3}},
+      fFill{opts, name + ".Fill", TColor::kWhite}
+      {}
 };
 
 template <int DIMENSION>
@@ -53,43 +51,76 @@ class THistDrawingOpts {
    static_assert(DIMENSION < 3, "This should have been handled by the specializations below?!");
 };
 
+/** \class THistDrawingOptsBase
+ Core ingredients (that do not depend on the dimensionality) or histogram drawing options.
+ */
+
+template <class DERIVED>
+class THistDrawingOptsBase: public TDrawingOptsBase<DERIVED> {
+   THistCoreAttrs fHistAttrs{*this, "Hist"}; ///< Basic histogram attributes (line, fill etc)
+
+protected:
+   THistDrawingOptsBase() = default;
+   THistDrawingOptsBase(TPadBase &pad, const std::string &name):
+      TDrawingOptsBase<DERIVED>(pad, name + ".Hist") {}
+
+public:
+   /// The color of the histogram line.
+   void SetLineColor(const TColor &col) { this->Update(fHistAttrs.fLine.fColor, col); }
+   TColor &GetLineColor() { return this->Get(fHistAttrs.fLine.fColor); }
+   const TColor &GetLineColor() const { return this->Get(fHistAttrs.fLine.fColor); }
+
+   /// The width of the histogram line.
+   void SetLineWidth(TLineAttrs::Width width) { this->Update(fHistAttrs.fLine.fWidth, width); }
+   TLineAttrs::Width &GetLineWidth() { return this->Get(fHistAttrs.fLine.fWidth); }
+   const TLineAttrs::Width GetLineWidth() const { return this->Get(fHistAttrs.fLine.fWidth); }
+
+   /// The color of the histogram line.
+   void SetFillColor(const TColor &col) { this->Update(fHistAttrs.fFill.fColor, col); }
+   TColor &GetFillColor() { return this->Get(fHistAttrs.fFill.fColor); }
+   const TColor &GetFillColor() const { return this->Get(fHistAttrs.fFill.fColor); }
+};
+
 /** \class THistDrawingOpts<1>
  Drawing options for a 1D histogram.
  */
 template <>
 class THistDrawingOpts<1>: public THistDrawingOptsBase<THistDrawingOpts<1>> {
-   enum class EStyle { kErrors, kBar, kText };
-   TOptsAttrRef<long long> fStyle{*this, "Hist.1D.Style"};
+   THistCoreAttrs fHistAttrs{*this, "Hist"}; ///< Basic histogram attributes (line, fill etc)
+
+   enum class EStyle { kBar, kText };
+   TDrawingAttrRef<long long> fStyle{*this, "Style", 0, {"hist", "bar", "text"}};
 
 public:
    THistDrawingOpts() = default;
-   THistDrawingOpts(TPadBase &pad): THistDrawingOptsBase<THistDrawingOpts<1>>(pad) {}
+   explicit THistDrawingOpts(TPadBase &pad): THistDrawingOptsBase<THistDrawingOpts<1>>(pad, "1D") {}
+
 };
 
 /** \class THistDrawingOpts<2>
  Drawing options for a 1D histogram.
  */
 template <>
-class THistDrawingOpts<2>: public THistDrawingOptsBase<THistDrawingOpts<1>> {
-   enum class EStyle { kErrors, kBar, kText };
-   TOptsAttrRef<long long> fStyle{*this, "Hist.2D.Style"};
+class THistDrawingOpts<2>: public THistDrawingOptsBase<THistDrawingOpts<2>> {
+   enum class EStyle { kBox, kSurf, kText };
+   TDrawingAttrRef<long long> fStyle{*this, "Style", 0, {"box", "surf", "text"}};
 
 public:
    THistDrawingOpts() = default;
-   THistDrawingOpts(TPadBase &pad): THistDrawingOptsBase<THistDrawingOpts<1>>(pad) {}
+   explicit THistDrawingOpts(TPadBase &pad): THistDrawingOptsBase<THistDrawingOpts<2>>(pad, "2D") {}
 };
 
 /** \class THistDrawingOpts<3>
  Drawing options for a 1D histogram.
  */
 template <>
-class THistDrawingOpts<3>: public THistDrawingOptsBase<THistDrawingOpts<1>> {
-   enum class EStyle { kErrors, kBar, kText };
-   TOptsAttrRef<long long> fStyle{*this, "Hist.3D.Style"};
+class THistDrawingOpts<3>: public THistDrawingOptsBase<THistDrawingOpts<3>> {
+   enum class EStyle { kBox, kIso };
+   TDrawingAttrRef<long long> fStyle{*this, "Style", 0, {"box", "iso"}};
 
 public:
    THistDrawingOpts() = default;
-   THistDrawingOpts(TPadBase &pad): THistDrawingOptsBase<THistDrawingOpts<1>>(pad) {}
+   explicit THistDrawingOpts(TPadBase &pad): THistDrawingOptsBase<THistDrawingOpts<3>>(pad, "3D") {}
 };
 
 } // namespace Experimental
