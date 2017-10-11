40afb6e28134a6eda51aeb5f924acb79e7b0aaa0
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-10-11 10:55:30 +0200

    [TDF] Fix tdf008_createDataSetFromScratch

--- tutorials/dataframe/tdf008_createDataSetFromScratch.C
+++ tutorials/dataframe/tdf008_createDataSetFromScratch.C
@@ -13,8 +13,8 @@ void tdf008_createDataSetFromScratch()
    ROOT::Experimental::TDataFrame tdf(100);
 
    // We now fill it with random numbers
-   gRandom.SetSeed(1);
-   auto tdf_1 = tdf.Define("rnd", [&rnd]() { return gRandom->Gaus(); });
+   gRandom->SetSeed(1);
+   auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
 
    // And we write out the dataset on disk
    tdf_1.Snapshot("randomNumbers", "tdf008_createDataSetFromScratch.root");
