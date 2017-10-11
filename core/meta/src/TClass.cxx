fbc5c60932324e2c181c1b360f9b1da4f2308e46
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2017-10-11 15:13:51 +0200

    Use Error() instead of assert()
    As requested by Axel: Users might happen to call this as TClass::GetClass(""); we should be "nice" and return an error then instead of going on in release or terminating in debug.

--- core/meta/src/TClass.cxx
+++ core/meta/src/TClass.cxx
@@ -1328,8 +1328,8 @@ void TClass::Init(const char *name, Version_t cversion,
 {
    if (!gROOT)
       ::Fatal("TClass::TClass", "ROOT system not initialized");
-
-   assert(name != 0 && strlen(name) != 0 || 0 == "TClass::Init, the name parameter is invalid (null or empty)");
+   if (!name || !name[0])
+      ::Error("TClass::Init", "The name parameter is invalid (null or empty)");
 
    // Always strip the default STL template arguments (from any template argument or the class name)
    fName           = TClassEdit::ShortType(name, TClassEdit::kDropStlDefault).c_str();
