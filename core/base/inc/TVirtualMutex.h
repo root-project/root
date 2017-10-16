41e920d68e919e676c715788ac4ff0dbfd37d31f
Philippe Canal <pcanal@fnal.gov>
2017-10-16 16:29:56 -0500

    Even more harmless unused var warning suppression.

--- core/base/inc/TVirtualMutex.h
+++ core/base/inc/TVirtualMutex.h
@@ -109,9 +109,9 @@ public:
 #define R__LOCKGUARD_NAMED(name,mutex) TLockGuard _NAME2_(R__guard,name)(mutex)
 #define R__LOCKGUARD_UNLOCK(name) _NAME2_(R__guard,name).UnLock()
 #else
-#define R__LOCKGUARD(mutex)  if (mutex) { }
-#define R__LOCKGUARD_NAMED(name,mutex) if (mutex) { }
-#define R__LOCKGUARD2(mutex) if (mutex) { }
+#define R__LOCKGUARD(mutex)  (void)mutex; { }
+#define R__LOCKGUARD_NAMED(name,mutex) (void)mutex; { }
+#define R__LOCKGUARD2(mutex) (void)mutex; { }
 #define R__LOCKGUARD_UNLOCK(name) { }
 #endif
 
