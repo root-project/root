de6377df76eedd13dd9feacc92c1f73e2fe08b13
Philippe Canal <pcanal@fnal.gov>
2017-10-19 14:58:20 -0500

    Make write access to all 'thread-safe' THashList serial

--- core/cont/src/THashList.cxx
+++ core/cont/src/THashList.cxx
@@ -68,6 +68,8 @@ THashList::~THashList()
 
 void THashList::AddFirst(TObject *obj)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddFirst(obj);
    fTable->Add(obj);
 }
@@ -81,6 +83,8 @@ void THashList::AddFirst(TObject *obj)
 
 void THashList::AddFirst(TObject *obj, Option_t *opt)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddFirst(obj, opt);
    fTable->Add(obj);
 }
@@ -90,6 +94,8 @@ void THashList::AddFirst(TObject *obj, Option_t *opt)
 
 void THashList::AddLast(TObject *obj)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddLast(obj);
    fTable->Add(obj);
 }
@@ -103,6 +109,8 @@ void THashList::AddLast(TObject *obj)
 
 void THashList::AddLast(TObject *obj, Option_t *opt)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddLast(obj, opt);
    fTable->Add(obj);
 }
@@ -112,6 +120,8 @@ void THashList::AddLast(TObject *obj, Option_t *opt)
 
 void THashList::AddBefore(const TObject *before, TObject *obj)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddBefore(before, obj);
    fTable->AddBefore(before, obj);
 }
@@ -121,6 +131,8 @@ void THashList::AddBefore(const TObject *before, TObject *obj)
 
 void THashList::AddBefore(TObjLink *before, TObject *obj)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddBefore(before, obj);
    fTable->AddBefore(before->GetObject(), obj);
 }
@@ -130,6 +142,8 @@ void THashList::AddBefore(TObjLink *before, TObject *obj)
 
 void THashList::AddAfter(const TObject *after, TObject *obj)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddAfter(after, obj);
    fTable->Add(obj);
 }
@@ -139,6 +153,8 @@ void THashList::AddAfter(const TObject *after, TObject *obj)
 
 void THashList::AddAfter(TObjLink *after, TObject *obj)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddAfter(after, obj);
    fTable->Add(obj);
 }
@@ -148,6 +164,8 @@ void THashList::AddAfter(TObjLink *after, TObject *obj)
 
 void THashList::AddAt(TObject *obj, Int_t idx)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    TList::AddAt(obj, idx);
    fTable->Add(obj);
 }
@@ -159,6 +177,8 @@ void THashList::AddAt(TObject *obj, Int_t idx)
 
 Float_t THashList::AverageCollisions() const
 {
+   R__COLLECTION_READ_LOCKGUARD(ROOT::gCoreMutex);
+
    return fTable->AverageCollisions();
 }
 
@@ -168,6 +188,8 @@ Float_t THashList::AverageCollisions() const
 
 void THashList::Clear(Option_t *option)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    fTable->Clear("nodelete");  // clear table so not more lookups
    if (IsOwner())
       TList::Delete(option);
@@ -184,6 +206,8 @@ void THashList::Clear(Option_t *option)
 
 void THashList::Delete(Option_t *option)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    Bool_t slow = option ? (!strcmp(option, "slow") ? kTRUE : kFALSE) : kFALSE;
 
    if (!slow) {
@@ -233,6 +257,8 @@ void THashList::Delete(Option_t *option)
 
 TObject *THashList::FindObject(const char *name) const
 {
+   R__COLLECTION_READ_LOCKGUARD(ROOT::gCoreMutex);
+
    return fTable->FindObject(name);
 }
 
@@ -241,6 +267,8 @@ TObject *THashList::FindObject(const char *name) const
 
 TObject *THashList::FindObject(const TObject *obj) const
 {
+   R__COLLECTION_READ_LOCKGUARD(ROOT::gCoreMutex);
+
    return fTable->FindObject(obj);
 }
 
@@ -250,6 +278,8 @@ TObject *THashList::FindObject(const TObject *obj) const
 
 const TList *THashList::GetListForObject(const char *name) const
 {
+   R__COLLECTION_READ_LOCKGUARD(ROOT::gCoreMutex);
+
    return fTable->GetListForObject(name);
 }
 
@@ -259,6 +289,8 @@ const TList *THashList::GetListForObject(const char *name) const
 
 const TList *THashList::GetListForObject(const TObject *obj) const
 {
+   R__COLLECTION_READ_LOCKGUARD(ROOT::gCoreMutex);
+
    return fTable->GetListForObject(obj);
 }
 
@@ -274,6 +306,8 @@ const TList *THashList::GetListForObject(const TObject *obj) const
 void THashList::RecursiveRemove(TObject *obj)
 {
    if (!obj) return;
+
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
 
    if (obj->HasInconsistentHash()) {
       // Remove obj in the list itself
@@ -306,6 +340,8 @@ void THashList::RecursiveRemove(TObject *obj)
 
 void THashList::Rehash(Int_t newCapacity)
 {
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
    fTable->Rehash(newCapacity);
 }
 
@@ -314,8 +350,10 @@ void THashList::Rehash(Int_t newCapacity)
 
 TObject *THashList::Remove(TObject *obj)
 {
+   R__COLLECTION_READ_LOCKGUARD(ROOT::gCoreMutex);
    if (!obj || !fTable->FindObject(obj)) return 0;
 
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
    TList::Remove(obj);
    return fTable->Remove(obj);
 }
@@ -327,6 +365,7 @@ TObject *THashList::Remove(TObjLink *lnk)
 {
    if (!lnk) return 0;
 
+   R__COLLECTION_WRITE_LOCKGUARD(ROOT::gCoreMutex);
    TObject *obj = lnk->GetObject();
 
    TList::Remove(lnk);
