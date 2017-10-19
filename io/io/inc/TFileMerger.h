21e5e04ab07f0279933898e25fdd6d12f4918350
Brian Bockelman <bbockelm@cse.unl.edu>
2017-10-19 09:28:31 -0500

    Minor modernizations of the TFileMerger.
    - Do static initialization in the header where possible.
    - Remove unnecessary pointers to TList in the TFileMerger when the TList
      is owned by the TFileMerger, always created, and has the same lifetime.
      Avoids unnecessary heap allocations.
    - As the data members have changed, increase the `ClassDef` version.

--- io/io/inc/TFileMerger.h
+++ io/io/inc/TFileMerger.h
@@ -12,6 +12,7 @@
 #ifndef ROOT_TFileMerger
 #define ROOT_TFileMerger
 
+#include "TList.h"
 #include "TObject.h"
 #include "TString.h"
 #include "TStopwatch.h"
@@ -28,29 +29,31 @@ class TIOFeatures;
 
 class TFileMerger : public TObject {
 private:
-   TFileMerger(const TFileMerger&); // Not implemented
-   TFileMerger& operator=(const TFileMerger&); // Not implemented
+   using TIOFeatures = ROOT::TIOFeatures;
+
+   TFileMerger(const TFileMerger&) = delete;
+   TFileMerger& operator=(const TFileMerger&) = delete;
 
 protected:
-   TStopwatch     fWatch;            ///< Stop watch to measure file copy speed
-   TList         *fFileList;         ///< A list the file (TFile*) which shall be merged
-   TFile         *fOutputFile;       ///< The outputfile for merging
-   TString        fOutputFilename;   ///< The name of the outputfile for merging
-   Bool_t         fFastMethod;       ///< True if using Fast merging algorithm (default)
-   Bool_t         fNoTrees;          ///< True if Trees should not be merged (default is kFALSE)
-   Bool_t         fExplicitCompLevel;///< True if the user explicitly requested a compressio level change (default kFALSE)
-   Bool_t         fCompressionChange;///< True if the output and input have different compression level (default kFALSE)
-   Int_t          fPrintLevel;       ///< How much information to print out at run time
-   TString        fMergeOptions;     ///< Options (in string format) to be passed down to the Merge functions
-   ROOT::TIOFeatures *fIOFeatures{nullptr}; ///< IO features to use in the output file.
-   TString        fMsgPrefix;        ///< Prefix to be used when printing informational message (default TFileMerger)
+   TStopwatch     fWatch;                     ///< Stop watch to measure file copy speed
+   TList          fFileList;                  ///< A list the file (TFile*) which shall be merged
+   TFile         *fOutputFile{nullptr};       ///< The outputfile for merging
+   TString        fOutputFilename;            ///< The name of the outputfile for merging
+   Bool_t         fFastMethod{kTRUE};         ///< True if using Fast merging algorithm (default)
+   Bool_t         fNoTrees{kFALSE};           ///< True if Trees should not be merged (default is kFALSE)
+   Bool_t         fExplicitCompLevel{kFALSE}; ///< True if the user explicitly requested a compressio level change (default kFALSE)
+   Bool_t         fCompressionChange{kFALSE}; ///< True if the output and input have different compression level (default kFALSE)
+   Int_t          fPrintLevel{0};             ///< How much information to print out at run time
+   TString        fMergeOptions;              ///< Options (in string format) to be passed down to the Merge functions
+   TIOFeatures   *fIOFeatures{nullptr};       ///< IO features to use in the output file.
+   TString        fMsgPrefix{"TFileMerger"};  ///< Prefix to be used when printing informational message (default TFileMerger)
 
-   Int_t          fMaxOpenedFiles;  ///< Maximum number of files opened at the same time by the TFileMerger
-   Bool_t         fLocal;           ///< Makes local copies of merging files if True (default is kTRUE)
-   Bool_t         fHistoOneGo;      ///< Merger histos in one go (default is kTRUE)
-   TString        fObjectNames;     ///< List of object names to be either merged exclusively or skipped
-   TList         *fMergeList;       ///< list of TObjString containing the name of the files need to be merged
-   TList         *fExcessFiles;     ///<! List of TObjString containing the name of the files not yet added to fFileList due to user or system limitiation on the max number of files opened.
+   Int_t          fMaxOpenedFiles;            ///< Maximum number of files opened at the same time by the TFileMerger
+   Bool_t         fLocal;                     ///< Makes local copies of merging files if True (default is kTRUE)
+   Bool_t         fHistoOneGo;                ///< Merger histos in one go (default is kTRUE)
+   TString        fObjectNames;               ///< List of object names to be either merged exclusively or skipped
+   TList          fMergeList;                 ///< list of TObjString containing the name of the files need to be merged
+   TList          fExcessFiles;               ///<! List of TObjString containing the name of the files not yet added to fFileList due to user or system limitiation on the max number of files opened.
 
    Bool_t         OpenExcessFiles();
    virtual Bool_t AddFile(TFile *source, Bool_t own, Bool_t cpProgress);
@@ -71,6 +74,7 @@ public:
       kSkipListed     = BIT(5),        ///< Skip objects specified in fObjectNames list
       kKeepCompression= BIT(6)         ///< Keep compression level unchanged for each input files
    };
+
    TFileMerger(Bool_t isLocal = kTRUE, Bool_t histoOneGo = kTRUE);
    virtual ~TFileMerger();
 
@@ -78,7 +82,7 @@ public:
    void        SetPrintLevel(Int_t level) { fPrintLevel = level; }
    Bool_t      HasCompressionChange() const { return fCompressionChange; }
    const char *GetOutputFileName() const { return fOutputFilename; }
-   TList      *GetMergeList() const { return fMergeList;  }
+   TList      *GetMergeList() { return &fMergeList; }
    TFile      *GetOutputFile() const { return fOutputFile; }
    Int_t       GetMaxOpenedFiles() const { return fMaxOpenedFiles; }
    void        SetMaxOpenedFiles(Int_t newmax);
@@ -113,7 +117,7 @@ public:
    virtual void   SetNotrees(Bool_t notrees=kFALSE) {fNoTrees = notrees;}
    virtual void        RecursiveRemove(TObject *obj);
 
-   ClassDef(TFileMerger,5)  // File copying and merging services
+   ClassDef(TFileMerger, 6)  // File copying and merging services
 };
 
 #endif
