48b77c763e6c823d0ea55e9c61ee7d782e86e384
Brian Bockelman <bbockelm@cse.unl.edu>
2017-10-25 08:28:40 -0500

    Add ability to write out ROOT block headers from the engine.

--- core/zip/src/Compression.cxx
+++ core/zip/src/Compression.cxx
@@ -17,6 +17,23 @@ namespace ROOT {
 namespace Internal {
 
 CompressionEngine::~CompressionEngine() {}
+
+void
+CompressionEngine::WriteROOTHeader(void *buffer, const char alg[2], char version, int deflate_size, int inflate_size) {
+  char *tgt = static_cast<char *>(buffer);
+  tgt[0] = alg[0];
+  tgt[1] = alg[1];
+  tgt[2] = version;
+
+  tgt[3] = static_cast<char>(deflate_size & 0xff);
+  tgt[4] = static_cast<char>((deflate_size >> 8) & 0xff);
+  tgt[5] = static_cast<char>((deflate_size >> 16) & 0xff);
+
+  tgt[6] = static_cast<char>(inflate_size & 0xff);
+  tgt[7] = static_cast<char>((inflate_size >> 8) & 0xff);
+  tgt[8] = static_cast<char>((inflate_size >> 16) & 0xff);
+}
+
 DecompressionEngine::~DecompressionEngine() {}
 
 }
