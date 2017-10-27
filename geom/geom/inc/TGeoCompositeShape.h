a1e46734672aca46c8638dd3997a4bd58a0af433
Olivier Couet <olivier.couet@cern.ch>
2017-10-27 13:38:34 +0200

    Formatting

--- geom/geom/inc/TGeoCompositeShape.h
+++ geom/geom/inc/TGeoCompositeShape.h
@@ -14,13 +14,13 @@
 
 #include "TGeoBBox.h"
 
-////////////////////////////////////////////////////////////////////////////
-//                                                                        //
-// TGeoCompositeShape - composite shape class. A composite shape contains //
-//   a list of primitive shapes, the list of coresponding transformations //
-//   and a boolean finder handling boolean operations among components.   //
-//                                                                        //
-////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// TGeoCompositeShape - composite shape class. A composite shape contains  //
+//   a list of primitive shapes, the list of corresponding transformations //
+//   and a boolean finder handling boolean operations among components.    //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
 
 class TGeoBoolNode;
 
