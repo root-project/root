40a97baef26e660a61f15f789ddacb262f4d8e77
Xavier Valls <xaviervallspla@gmail.com>
2017-11-10 17:41:27 +0100

    Add new general and sequential executors.
    This commits changes the behaviour of existing executors and introduces
    new ones:
    
    * TExecutor: changes its functionality to be a general Executor, while
    TExecutorBaseImpl takes the role of the previous TExecutor. TExecutor
    now acts as a general interface to the executors. The executor it will
    resolve to is specified by a execution policy parameter in its
    constructor.
    
    * TExecutorBaseImpl: Plays the previous role of TExecutor.
    
    * TSequentialExecutor: provides a sequential implementation of the
    executor model, defined by TExecutorBaseImpl.

--- core/imt/inc/ROOT/TThreadExecutor.hxx
+++ core/imt/inc/ROOT/TThreadExecutor.hxx
@@ -22,17 +22,15 @@
 # endif
 #else
 
-#include "ROOT/TExecutor.hxx"
+#include "ROOT/TExecutorBaseImpl.hxx"
 #include "ROOT/TPoolManager.hxx"
 #include "TROOT.h"
-#include "TError.h"
-#include <functional>
 #include <memory>
 #include <numeric>
 
 namespace ROOT {
 
-   class TThreadExecutor: public TExecutor<TThreadExecutor> {
+   class TThreadExecutor: public TExecutorBaseImpl<TThreadExecutor> {
    public:
       explicit TThreadExecutor();
 
@@ -52,7 +50,7 @@ namespace ROOT {
       template<class F, class T>
       void Foreach(F func, std::vector<T> &args);
 
-      using TExecutor<TThreadExecutor>::Map;
+      using TExecutorBaseImpl<TThreadExecutor>::Map;
       template<class F, class Cond = noReferenceCond<F>>
       auto Map(F func, unsigned nTimes) -> std::vector<typename std::result_of<F()>::type>;
       template<class F, class INTEGER, class Cond = noReferenceCond<F, INTEGER>>
@@ -64,7 +62,7 @@ namespace ROOT {
       // // the late return types also check at compile-time whether redfunc is compatible with func,
       // // other than checking that func is compatible with the type of arguments.
       // // a static_assert check in TThreadExecutor::Reduce is used to check that redfunc is compatible with the type returned by func
-      using TExecutor<TThreadExecutor>::MapReduce;
+      using TExecutorBaseImpl<TThreadExecutor>::MapReduce;
       template<class F, class R, class Cond = noReferenceCond<F>>
       auto MapReduce(F func, unsigned nTimes, R redfunc) -> typename std::result_of<F()>::type;
       template<class F, class R, class Cond = noReferenceCond<F>>
@@ -80,7 +78,7 @@ namespace ROOT {
       template<class F, class T, class R, class Cond = noReferenceCond<F, T>>
       auto MapReduce(F func, std::vector<T> &args, R redfunc, unsigned nChunks) -> typename std::result_of<F(T)>::type;
 
-      using TExecutor<TThreadExecutor>::Reduce;
+      using TExecutorBaseImpl<TThreadExecutor>::Reduce;
       template<class T, class BINARYOP> auto Reduce(const std::vector<T> &objs, BINARYOP redfunc) -> decltype(redfunc(objs.front(), objs.front()));
       template<class T, class R> auto Reduce(const std::vector<T> &objs, R redfunc) -> decltype(redfunc(objs));
 
