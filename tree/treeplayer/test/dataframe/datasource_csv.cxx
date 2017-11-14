330ec9e166b5121bea879f1c47c23d8ee14214a0
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-11-14 14:32:50 +0100

    [TDF] Do not expect to always run `DefineSlot` on all slots
    This assumption caused a breakage in datasource_csv.cxx and in the
    same that caused breakages in datasource_root.cxx (fixed by
    commit 8c261b11ba161364edcfa677cd3b7c4a2ef623a8).

--- tree/treeplayer/test/dataframe/datasource_csv.cxx
+++ tree/treeplayer/test/dataframe/datasource_csv.cxx
@@ -149,26 +149,21 @@ TEST(TCsvDS, FromATDFWithJitting)
 
 TEST(TCsvDS, DefineSlotCheckMT)
 {
-   auto nSlots = 4U;
+   const auto nSlots = 4U;
    ROOT::EnableImplicitMT(nSlots);
 
-   std::hash<std::thread::id> hasher;
-   using H_t = decltype(hasher(std::this_thread::get_id()));
-
-   std::vector<H_t> ids(nSlots, 0);
+   std::vector<unsigned int> ids(nSlots, 0u);
    std::unique_ptr<TDataSource> tds(new TCsvDS(fileName0));
    TDataFrame d(std::move(tds));
    auto m = d.DefineSlot("x", [&](unsigned int slot) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                ids[slot] = hasher(std::this_thread::get_id());
-                return 1.;
+                ids[slot] = 1u;
+                return 1;
              }).Max("x");
-
    EXPECT_EQ(1, *m); // just in case
 
-   std::set<H_t> s(ids.begin(), ids.end());
-   EXPECT_EQ(nSlots, s.size());
-   EXPECT_TRUE(s.end() == s.find(0));
+   const auto nUsedSlots = std::accumulate(ids.begin(), ids.end(), 0u);
+   EXPECT_GT(nUsedSlots, 0u);
+   EXPECT_LE(nUsedSlots, nSlots);
 }
 
 TEST(TCsvDS, FromATDFMT)
--- tree/treeplayer/test/dataframe/datasource_trivial.cxx
+++ tree/treeplayer/test/dataframe/datasource_trivial.cxx
@@ -108,26 +108,21 @@ TEST(TTrivialDS, FromATDFWithJitting)
 
 TEST(TTrivialDS, DefineSlotCheckMT)
 {
-   auto nSlots = 4U;
+   const auto nSlots = 4U;
    ROOT::EnableImplicitMT(nSlots);
 
-   std::hash<std::thread::id> hasher;
-   using H_t = decltype(hasher(std::this_thread::get_id()));
-
-   std::vector<H_t> ids(nSlots, 0);
+   std::vector<unsigned int> ids(nSlots, 0u);
    std::unique_ptr<TDataSource> tds(new TTrivialDS(nSlots));
    TDataFrame d(std::move(tds));
    auto m = d.DefineSlot("x", [&](unsigned int slot) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                ids[slot] = hasher(std::this_thread::get_id());
-                return 1.;
+                ids[slot] = 1u;
+                return 1;
              }).Max("x");
-
    EXPECT_EQ(1, *m); // just in case
 
-   std::set<H_t> s(ids.begin(), ids.end());
-   EXPECT_EQ(nSlots, s.size());
-   EXPECT_TRUE(s.end() == s.find(0));
+   const auto nUsedSlots = std::accumulate(ids.begin(), ids.end(), 0u);
+   EXPECT_GT(nUsedSlots, 0u);
+   EXPECT_LE(nUsedSlots, nSlots);
 }
 
 TEST(TTrivialDS, FromATDFMT)
