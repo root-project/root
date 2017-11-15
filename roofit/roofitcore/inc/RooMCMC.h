2a6fe158aea13489fc57f92dc0d9c9e488c2173d
Oliver Dahme <o.dahme@gmail.com>
2017-11-15 15:30:43 +0100

    added comments
    Added comments to every function and parameter in the header file

--- roofit/roofitcore/inc/RooMCMC.h
+++ roofit/roofitcore/inc/RooMCMC.h
@@ -33,66 +33,65 @@ public:
   virtual ~RooMCMC() ;
    void setOffsetting(Bool_t flag) ;
 
-  Int_t mcmc(size_t npoints, size_t cutoff, const char* errorstrategy = "gaus");
-  TGraph* getProfile(const char* name, Bool_t cutoff = kTRUE);
-  TMultiGraph* getWalkDis(const char* name, Bool_t cutoff = kTRUE);
-  TH1F*   getWalkDisHis(const char* name,  Int_t nbinsx, Bool_t cutoff = kTRUE);
+  Int_t mcmc(size_t npoints, size_t cutoff, const char* errorstrategy = "gaus"); //minimizes function, cutoff points and calculates errors, "gaus" for symetric ones, and "interval" for asymetric ones.
+  TGraph* getProfile(const char* name, Bool_t cutoff = kTRUE);// returns a profile of function for a parameter, including or excluding cutoff-points
+  TMultiGraph* getWalkDis(const char* name, Bool_t cutoff = kTRUE);// returns the walk distribution for a parameter, including or excluding cutoff-points
+  TH1F*   getWalkDisHis(const char* name,  Int_t nbinsx, Bool_t cutoff = kTRUE); // returns histogram of the walk distribution including or excluding cutoff-points
   TH2D*   getCornerPlot(const char* name1, const char* name2, Int_t nbinsx, Int_t nbinsy, Bool_t cutoff = kTRUE);
-  Int_t changeCutoff(Int_t newCutoff);
-  Int_t printError(const char* name, Double_t conf = 0.682);
-  Int_t saveCandidatesAs(const char* name);
-  //Int_t saveCornerPlotAs(const char* pngname);
-  Int_t getPercentile(const char* name, Double_t conf = 0.682);
-  Int_t getGausErrors();
-  Int_t setPrintLevel(Int_t newLevel) ;
+  Int_t changeCutoff(Int_t newCutoff); // returns a scatter plot to check for correlations between two parameters
+  Int_t printError(const char* name, Double_t conf = 0.682); //prints symetric errors for a parameter at a defined confidence level
+  Int_t saveCandidatesAs(const char* name); // saves all points of the function into a file
+  Int_t getPercentile(const char* name, Double_t conf = 0.682); // prints asymetric errors at a defined confidence level
+  Int_t getGausErrors(); // prints symetric errors for all parameters and the correlation coefficients
+  Int_t setPrintLevel(Int_t newLevel) ; // set level of output (not implemented)
 
-  static void cleanup() ;
+  static void cleanup() ; //empty all point lists
   void setSeed(Double_t seed)
   {
     _seed = seed;
-  };
+  }; //set seed for random generator
   void setAlphaStar(Double_t newAlpha) {
     _alphaStar = newAlpha;
-  };
+  }; // change forced acceptance rate, not recommended
 
-  inline size_t getNPar() const { return _nPar ; }
-  std::vector<const char*> getNames();
+  inline size_t getNPar() const { return _nPar ; } // returns number of parameters
+  std::vector<const char*> getNames(); // returns vector of the parameter names
 
  protected:
-   virtual Bool_t setPdfParamVal(Int_t index, Double_t value, Bool_t verbose=kFALSE) ;
-   void setPdfParamErr(Int_t index, Double_t value) ;
-     void updateFloatVec() ;
-     void sortPointList(const char* name1) ;
-     Int_t getIndex(const char* name) ;
-     Double_t getMinList(const char* name);
-     Double_t getMaxList(const char* name);
+   virtual Bool_t setPdfParamVal(Int_t index, Double_t value, Bool_t verbose=kFALSE) ; // set pdf parameter values
+   void setPdfParamErr(Int_t index, Double_t value) ; // set pdf prameter errors
+     void updateFloatVec() ; // updates float vector
+     void sortPointList(const char* name1) ; // sorts point list according to a parameter
+     Int_t getIndex(const char* name) ; // returns internal index of a parameter
+     Double_t getMinList(const char* name); // returns minimal value of a parameter
+     Double_t getMaxList(const char* name); // returns maximum value of a parameter
      void setFileName(const TString name)
      {
        _fileName = name;
-     };
+     }; // set name of outputfile
 
  private:
-   Int_t       _printLevel ;
-   Int_t       _status ;
-   size_t       _nPar ;
-   Int_t       _cutoff;
-   RooArgList* _floatParamList ;
-   std::vector<RooAbsArg*> _floatParamVec ;
-   RooArgList* _initFloatParamList ;
-   RooArgList* _constParamList ;
-   RooArgList* _initConstParamList ;
-   RooArgList*  _bestParamList;
-   RooAbsReal* _func ;
-   std::vector<RooArgList*> _pointList;
-   std::vector<RooArgList*> _cutoffList;
-   std::vector<RooArgList*> _sortPointList;
-   TString _fileName;
-   Bool_t      _verbose ;
-   Bool_t     _gaus;
-   Bool_t     _interval;
-   Double_t   _seed = 0;
-   Double_t  _alphaStar = 0.234;
-   static TVirtualFitter *_theFitter ;
+   Int_t       _printLevel ; //level of printing
+   Int_t       _status ; //status level
+   size_t       _nPar ; //number of parameters
+   Int_t       _cutoff;//number of points before cutoff
+   RooArgList* _floatParamList ; //parameter list
+   std::vector<RooAbsArg*> _floatParamVec ; //vector of parameter list
+   RooArgList* _initFloatParamList ; //initial parameter list
+   RooArgList* _constParamList ; //list of constant parameters
+   RooArgList* _initConstParamList ; //intial list of constant parameters
+   RooArgList*  _bestParamList; //list of parameters with lowest function value
+   RooAbsReal* _func ; //function to be minimized
+   std::vector<RooArgList*> _pointList; //list of monte carlo markov chain points
+   std::vector<RooArgList*> _cutoffList; //list of points after cutoff
+   std::vector<RooArgList*> _sortPointList; //sorted list of points
+   TString _fileName; //name of output file
+   Bool_t      _verbose ; //turn verbosity on or off
+   Bool_t     _gaus; //turn gaus errors on or off in mcmc()
+   Bool_t     _interval; //turn asymetric errors on or off in mcmc()
+   Double_t   _seed = 0; //seed for random numbers generator
+   Double_t  _alphaStar = 0.234; //forced acceptance rate
+   static TVirtualFitter *_theFitter ; //fitter function
    RooMCMC(const RooMCMC&) ;
 
    ClassDef(RooMCMC,0) // RooFit minimizer based on Monte Carlo Markov Chain
