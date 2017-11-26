3717894c8de3d999319ae01fe77b377a2f777457
Juan Fernando Jaramillo Botero <juanfe@sphorofilia.opdevel.com>
2017-11-26 12:39:47 -0500

    Solve more commets on pull request comments from dpiparo

--- tutorials/math/principal.py
+++ tutorials/math/principal.py
@@ -15,72 +15,70 @@
 ##
 ## \authors Juan Fernando Jaramillo Botero
 
-from ROOT import TPrincipal, gRandom, TBrowser
+from ROOT import TPrincipal, gRandom, TBrowser, vector
 
 
-def principal(n=10, m=10000):
-    c = n / 5 + 1
+n = 10
+m = 10000
 
-    print ("*************************************************")
-    print ("*         Principal Component Analysis          *")
-    print ("*                                               *")
-    print ("*  Number of variables:           {0:4d}          *".format(n))
-    print ("*  Number of data points:         {0:8d}      *".format(m))
-    print ("*  Number of dependent variables: {0:4d}          *".format(c))
-    print ("*                                               *")
-    print ("*************************************************")
+c = n / 5 + 1
 
-    # Initilase the TPrincipal object. Use the empty string for the
-    # final argument, if you don't wan't the covariance
-    # matrix. Normalising the covariance matrix is a good idea if your
-    # variables have different orders of magnitude.
-    principal = TPrincipal(n, "ND")
+print ("""*************************************************")
+*         Principal Component Analysis          *
+*                                               *
+*  Number of variables:           {0:4d}          *
+*  Number of data points:         {1:8d}      *
+*  Number of dependent variables: {2:4d}          *
+*                                               *
+*************************************************""".format(n, m, c))
 
-    # Use a pseudo-random number generator
-    randumNum = gRandom
+# Initilase the TPrincipal object. Use the empty string for the
+# final argument, if you don't wan't the covariance
+# matrix. Normalising the covariance matrix is a good idea if your
+# variables have different orders of magnitude.
+principal = TPrincipal(n, "ND")
 
-    # Make the m data-points
-    # Make a variable to hold our data
-    # Allocate memory for the data point
-    data = ROOT.vector('double')()
-    for i in range(m):
-        # First we create the un-correlated, random variables, according
-        # to one of three distributions
-        for j in range(n - c):
-            if j % 3 == 0:
-                data.append(randumNum.Gaus(5, 1))
-            elif j % 3 == 1:
-                data.append(randumNum.Poisson(8))
-            else:
-                data.append(randumNum.Exp(2))
+# Use a pseudo-random number generator
+randumNum = gRandom
 
-        # Then we create the correlated variables
-        for j in range(c):
-            data.append(0)
-            for k in range(n - c - j):
-                data[n - c + j] += data[k]
+# Make the m data-points
+# Make a variable to hold our data
+# Allocate memory for the data point
+data = vector('double')()
+for i in range(m):
+    # First we create the un-correlated, random variables, according
+    # to one of three distributions
+    for j in range(n - c):
+        if j % 3 == 0:
+            data.push_back(randumNum.Gaus(5, 1))
+        elif j % 3 == 1:
+            data.push_back(randumNum.Poisson(8))
+        else:
+            data.push_back(randumNum.Exp(2))
 
-        # Finally we're ready to add this datapoint to the PCA
-        principal.AddRow(data.data())
+    # Then we create the correlated variables
+    for j in range(c):
+        data.push_back(0)
+        for k in range(n - c - j):
+            data[n - c + j] += data[k]
 
-    # Do the actual analysis
-    principal.MakePrincipals()
+    # Finally we're ready to add this datapoint to the PCA
+    principal.AddRow(data.data())
 
-    # Print out the result on
-    principal.Print()
+# Do the actual analysis
+principal.MakePrincipals()
 
-    # Test the PCA
-    principal.Test()
+# Print out the result on
+principal.Print()
 
-    # Make some histograms of the orginal, principal, residue, etc data
-    principal.MakeHistograms()
+# Test the PCA
+principal.Test()
 
-    # Make two functions to map between feature and pattern space
-    # Start a browser, so that we may browse the histograms generated
-    # above
-    principal.MakeCode()
-    b = TBrowser("principalBrowser", principal)
+# Make some histograms of the orginal, principal, residue, etc data
+principal.MakeHistograms()
 
-
-if __name__ == "__main__":
-    principal()
+# Make two functions to map between feature and pattern space
+# Start a browser, so that we may browse the histograms generated
+# above
+principal.MakeCode()
+b = TBrowser("principalBrowser", principal)
