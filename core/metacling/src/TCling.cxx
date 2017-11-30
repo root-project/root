57c235d03e263125f2fe62b07f27854df72900d8
Raphael Isemann <teemperor@gmail.com>
2017-11-30 22:01:52 +0100

    Also load GenVector

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1380,7 +1380,7 @@ void TCling::Initialize()
    if (fInterpreter->getCI()->getLangOpts().Modules && !IsFromRootCling()) {
       // Load modules that we can't automatically load via rootmap files as they
       // contain decls in namespaces which aren't supported.
-      LoadModules({"TMVA", "Gpad", "RooStats", "TreePlayer", "Graf"}, *fInterpreter);
+      LoadModules({"TMVA", "Gpad", "RooStats", "GenVector", "TreePlayer", "Graf"}, *fInterpreter);
    }
 }
 
