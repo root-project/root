88905c2ffa4c00b4c4820d8dffbf3edf2ff44d31
Raphael Isemann <teemperor@gmail.com>
2017-11-30 22:33:24 +0100

    Another attempt at loading from library path

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1143,18 +1143,18 @@ static bool isFromRootCling() {
   return foundSymbol;
 }
 
-static void loadModulePath(HeaderSearch& hdrSearch, const char* environmentVariable) {
-  const char* LD_LIBRARY_PATH = getenv(environmentVariable);
-  if (LD_LIBRARY_PATH) {
-     StringRef path = LD_LIBRARY_PATH;
+static void loadModulePath(HeaderSearch& hdrSearch, const char* inputPath) {
+  if (inputPath) {
+     StringRef path = inputPath;
      SmallVector<StringRef, 32> paths;
      path.split(paths, ":");
+
      for (StringRef path : paths) {
         SmallString<128> ModuleMapFilePath = path;
-        llvm::sys::path::append(ModuleMapFilePath, "module.modulemap");
 
-        if (auto file = hdrSearch.getFileMgr().getFile(ModuleMapFilePath))
+        if (auto file = hdrSearch.getFileMgr().getFile(ModuleMapFilePath)) {
            hdrSearch.loadModuleMapFile(file, false, FileID());
+        }
     }
   }
 }
@@ -1292,7 +1292,7 @@ TCling::TCling(const char *name, const char *title)
    if (fInterpreter->getCI()->getLangOpts().Modules) {
       HeaderSearch& hdrSearch = fInterpreter->getCI()->getPreprocessor().getHeaderSearchInfo();
       hdrSearch.loadTopLevelSystemModules();
-      loadModulePath(hdrSearch, "LD_LIBRARY_PATH");
+      loadModulePath(hdrSearch, gSystem->GetDynamicPath());
 
       // Setup core C++ modules if we have any to setup.
       LoadModules({"libc", "stl", "ROOT_Types", "Core", "RIO"}, *fInterpreter);
