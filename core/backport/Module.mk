fa985d869ac7393ee716ac138c1a3832e6d9e219
Axel Naumann <Axel.Naumann@cern.ch>
2017-11-30 10:58:58 +0100

    Add backport/Module.mk.

---
+++ core/backport/Module.mk
@@ -0,0 +1,55 @@
+# Module.mk for utilities for core/backport.
+# Copyright (c) 1995-2017 Rene Brun and Fons Rademakers
+#
+# Author: Axel Naumann, 2017-11-30
+
+MODNAME        := backport
+MODDIR         := $(ROOT_SRCDIR)/core/$(MODNAME)
+MODDIRS        := $(MODDIR)/src
+MODDIRI        := $(MODDIR)/inc
+
+FOUNDATIONDIR   := $(MODDIR)
+FOUNDATIONDIRS  := $(FOUNDATIONDIR)/src
+FOUNDATIONDIRI  := $(FOUNDATIONDIR)/inc
+FOUNDATIONDIRR  := $(FOUNDATIONDIR)/res
+
+BACKPORTTH       += $(MODDIRI)/libcpp_string_view.h
+BACKPORTTH       += $(MODDIRI)/RWrap_libcpp_string_view.h
+BACKPORTTH       += $(MODDIRI)/ROOT/span.hxx
+
+FOUNDATIONO     := $(call stripsrc,$(FOUNDATIONS:.cxx=.o))
+
+FOUNDATIONL     := $(MODDIRI)/LinkDef.h
+
+FOUNDATIONDEP   := $(FOUNDATIONO:.o=.d)
+
+# used in the main Makefile
+ALLHDRS     += $(patsubst $(MODDIRI)/%,include/%,$(FOUNDATIONH) $(FOUNDATIONTH))
+
+# include all dependency files
+INCLUDEFILES += $(FOUNDATIONDEP)
+
+##### local rules #####
+.PHONY:         all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME)
+
+include/%.h:    $(FOUNDATIONDIRI)/%.h
+		cp $< $@
+
+include/%.hxx:	$(FOUNDATIONDIRI)/%.hxx
+		mkdir -p include/ROOT
+		cp $< $@
+
+all-$(MODNAME): $(FOUNDATIONO)
+
+clean-$(MODNAME):
+		@rm -f $(FOUNDATIONO)
+
+clean::         clean-$(MODNAME)
+
+distclean-$(MODNAME): clean-$(MODNAME)
+		@rm -f $(FOUNDATIONDEP)
+
+distclean::     distclean-$(MODNAME)
+
+##### extra rules ######
+$(FOUNDATIONO): CXXFLAGS += -I$(FOUNDATIONDIRR)
