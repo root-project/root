1821a7c90937d99d59ddb8714f2d464ee8ad7935
Raphael Isemann <teemperor@gmail.com>
2017-11-30 10:20:34 +0100

    [cxxmodules] Add flag if module has C++ module alongside it

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1684,7 +1684,8 @@ void TCling::RegisterModule(const char* modulename,
                             void (*triggerFunc)(),
                             const FwdDeclArgsToKeepCollection_t& fwdDeclsArgToSkip,
                             const char** classesHeaders,
-                            Bool_t lateRegistration /*=false*/)
+                            Bool_t lateRegistration /*=false*/,
+                            Bool_t hasCxxModule /*=false*/)
 {
    const bool fromRootCling = isFromRootCling();
    // We need the dictionary initialization but we don't want to inject the
@@ -1924,7 +1925,7 @@ void TCling::RegisterModule(const char* modulename,
    clang::Sema &TheSema = fInterpreter->getSema();
 
    bool ModuleWasSuccessfullyLoaded = false;
-   if (TheSema.getLangOpts().Modules) {
+   if (hasCxxModule) {
       std::string ModuleName = llvm::StringRef(modulename).substr(3).str();
       ModuleWasSuccessfullyLoaded = LoadModule(ModuleName, *fInterpreter);
       if (!ModuleWasSuccessfullyLoaded) {
