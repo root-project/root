a791734845f4417f56a021f6370eb20c7c0241c3
Raphael Isemann <teemperor@gmail.com>
2017-12-01 11:43:16 +0100

    Also add cwd to prebuilt module path

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1292,6 +1292,7 @@ TCling::TCling(const char *name, const char *title)
       HeaderSearch& hdrSearch = fInterpreter->getCI()->getPreprocessor().getHeaderSearchInfo();
       hdrSearch.loadTopLevelSystemModules();
       loadModulePath(hdrSearch, gSystem->GetDynamicPath());
+      fInterpreter->getCI()->getHeaderSearchOpts().AddPrebuiltModulePath(".");
 
       // Setup core C++ modules if we have any to setup.
       LoadModules({"libc", "stl", "ROOT_Types", "Core", "RIO"}, *fInterpreter);
