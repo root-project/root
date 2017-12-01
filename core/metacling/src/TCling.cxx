67003b01755de4bbbe45d8e5e7efcc18fbdce05b
Raphael Isemann <teemperor@gmail.com>
2017-12-01 10:28:22 +0100

    Also preload TreeViewer

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1379,7 +1379,7 @@ void TCling::Initialize()
    if (fInterpreter->getCI()->getLangOpts().Modules && !IsFromRootCling()) {
       // Load modules that we can't automatically load via rootmap files as they
       // contain decls in namespaces which aren't supported.
-      LoadModules({"TMVA", "Gpad", "RooStats", "GenVector", "Hist", "Net", "TreePlayer", "Graf"}, *fInterpreter);
+      LoadModules({"TMVA", "Gpad", "RooStats", "GenVector", "Hist", "Net", "TreePlayer", "TreeViewer", "Graf"}, *fInterpreter);
    }
 }
 
