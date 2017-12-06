root-project/root

commit 8594906728abdedc563eacd04e9a6bdded77e837

Add SetAutoSave() method to TBufferMerger
This function lets users decide how much data should be accumulated
in buffers before being merged into the output file with TFileMerger.

io/io/src/TBufferMerger.cxx
@@ -21,7 +21,7 @@ namespace ROOT {
 namespace Experimental {
 
 TBufferMerger::TBufferMerger(const char *name, Option_t *option, Int_t compress)
-   : fName(name), fOption(option), fCompress(compress),
+   : fName(name), fOption(option), fCompress(compress), fAutoSave(0),
      fMergingThread(new std::thread([&]() { this->WriteOutputFile(); }))
 {
 }
@@ -63,9 +63,15 @@ void TBufferMerger::Push(TBufferFile *buffer)
    fDataAvailable.notify_one();
 }
 
+void TBufferMerger::SetAutoSave(size_t size)
+{
+   fAutoSave = size;
+}
+
 void TBufferMerger::WriteOutputFile()
 {
-   std::unique_ptr<TMemFile> memfile;
+   size_t buffered = 0;
+   std::vector<TMemFile *> memfiles;
    std::unique_ptr<TBufferFile> buffer;
    TFileMerger merger;
 
@@ -84,25 +90,36 @@ void TBufferMerger::WriteOutputFile()
       fQueue.pop();
       lock.unlock();
 
-      if (!buffer) return;
+      if (!buffer)
+         break;
 
       Long64_t length;
       buffer->SetReadMode();
       buffer->SetBufferOffset();
       buffer->ReadLong64(length);
+      buffered += length;
 
       {
          R__LOCKGUARD(gROOTMutex);
-         memfile.reset(new TMemFile(fName.c_str(), buffer->Buffer() + buffer->Length(), length, "read"));
+         memfiles.push_back(new TMemFile(fName.c_str(), buffer->Buffer() + buffer->Length(), length, "read"));
          buffer->SetBufferOffset(buffer->Length() + length);
-         merger.AddFile(memfile.get(), false);
-         merger.PartialMerge();
+         merger.AddFile(memfiles.back(), false);
+
+         if (buffered > fAutoSave) {
+            buffered = 0;
+            merger.PartialMerge();
+            merger.Reset();
+            memfiles.clear();
+         }
       }
-      merger.Reset();
 
       if (fCallback)
          fCallback();
    }
+
+   R__LOCKGUARD(gROOTMutex);
+   merger.PartialMerge();
+   merger.Reset();
 }
 
 } // namespace Experimental

io/io/inc/ROOT/TBufferMerger.hxx
@@ -75,6 +75,16 @@ public:
     */
    void RegisterCallback(const std::function<void(void)> &f);
 
+   /** By default, TBufferMerger will call TFileMerger::PartialMerge() for each
+    *  buffer pushed onto its merge queue. This function lets the user change
+    *  this behaviour by telling TBufferMerger to accumulate at least @param size
+    *  bytes in memory before performing a partial merge and flushing to disk.
+    *  This can be useful to avoid an excessive amount of work to happen in the
+    *  output thread, as the number of TTree headers (which require compression)
+    *  written to disk can be reduced.
+    */
+   void SetAutoSave(size_t size);
+
    friend class TBufferMergerFile;
 
 private:
@@ -93,6 +103,7 @@ private:
    const std::string fName;
    const std::string fOption;
    const Int_t fCompress;
+   size_t fAutoSave;                                             //< AutoSave only every fAutoSave bytes
    std::mutex fQueueMutex;                                       //< Mutex used to lock fQueue
    std::condition_variable fDataAvailable;                       //< Condition variable used to wait for data
    std::queue<TBufferFile *> fQueue;                             //< Queue to which data is pushed and merged

commit 9daf35ecbd9c5637bf04efe13f10c944a1b53ef6

Fix memory leak in TBufferMerger
memfiles.clear() needs std::unique_ptr<TMemFile> to free the memory.
Bug introduced by commit 8594906728abdedc563eacd04e9a6bdded77e837.
Closes ROOT-9133.

io/io/src/TBufferMerger.cxx
@@ -93,7 +93,7 @@ void TBufferMerger::SetAutoSave(size_t size)
 void TBufferMerger::WriteOutputFile()
 {
    size_t buffered = 0;
-   std::vector<TMemFile *> memfiles;
+   std::vector<std::unique_ptr<TMemFile>> memfiles;
    std::unique_ptr<TBufferFile> buffer;
    TFileMerger merger;
 
@@ -123,9 +123,9 @@ void TBufferMerger::WriteOutputFile()
 
       {
          R__LOCKGUARD(gROOTMutex);
-         memfiles.push_back(new TMemFile(fFile->GetName(), buffer->Buffer() + buffer->Length(), length, "read"));
+         memfiles.emplace_back(new TMemFile(fFile->GetName(), buffer->Buffer() + buffer->Length(), length, "read"));
          buffer->SetBufferOffset(buffer->Length() + length);
-         merger.AddFile(memfiles.back(), false);
+         merger.AddFile(memfiles.back().get(), false);
 
          if (buffered > fAutoSave) {
             buffered = 0;