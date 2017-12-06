9ea90c99e4e8158f4e13c4a9d228081652671053
Guilherme Amadio <amadio@cern.ch>
2017-12-06 10:33:35 +0100

    [graf2d/cocoa] Use unique_ptr instead of deprecated auto_ptr

--- graf2d/cocoa/src/X11Buffer.mm
+++ graf2d/cocoa/src/X11Buffer.mm
@@ -336,7 +336,7 @@ void CommandBuffer::AddDrawLine(Drawable_t wid, const GCValues_t &gc, Int_t x1,
 {
    try {
       //if this throws, I do not care.
-      std::auto_ptr<DrawLine> cmd(new DrawLine(wid, gc, Point(x1, y1), Point(x2, y2)));
+      std::unique_ptr<DrawLine> cmd(new DrawLine(wid, gc, Point(x1, y1), Point(x2, y2)));
       fCommands.push_back(cmd.get());//this can throw.
       cmd.release();
    } catch (const std::exception &) {
@@ -352,7 +352,7 @@ void CommandBuffer::AddDrawSegments(Drawable_t wid, const GCValues_t &gc,
    assert(nSegments > 0 && "AddDrawSegments, nSegments <= 0");
 
    try {
-      std::auto_ptr<DrawSegments> cmd(new DrawSegments(wid, gc, segments, nSegments));
+      std::unique_ptr<DrawSegments> cmd(new DrawSegments(wid, gc, segments, nSegments));
       fCommands.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -369,7 +369,7 @@ void CommandBuffer::AddClearArea(Window_t wid, Int_t x, Int_t y, UInt_t w, UInt_
       r.fY = y;
       r.fWidth = (UShort_t)w;
       r.fHeight = (UShort_t)h;
-      std::auto_ptr<ClearArea> cmd(new ClearArea(wid, r));//Can throw, nothing leaks.
+      std::unique_ptr<ClearArea> cmd(new ClearArea(wid, r));//Can throw, nothing leaks.
       fCommands.push_back(cmd.get());//this can throw.
       cmd.release();
    } catch (const std::exception &) {
@@ -389,7 +389,7 @@ void CommandBuffer::AddCopyArea(Drawable_t src, Drawable_t dst, const GCValues_t
       area.fWidth = (UShort_t)width;
       area.fHeight = (UShort_t)height;
       //Can throw, nothing leaks.
-      std::auto_ptr<CopyArea> cmd(new CopyArea(src, dst, gc, area, Point(dstX, dstY)));
+      std::unique_ptr<CopyArea> cmd(new CopyArea(src, dst, gc, area, Point(dstX, dstY)));
       fCommands.push_back(cmd.get());//this can throw.
       cmd.release();
    } catch (const std::exception &) {
@@ -405,7 +405,7 @@ void CommandBuffer::AddDrawString(Drawable_t wid, const GCValues_t &gc, Int_t x,
       if (len < 0)//Negative length can come from caller.
          len = std::strlen(text);
       const std::string substr(text, len);//Can throw.
-      std::auto_ptr<DrawString> cmd(new DrawString(wid, gc, Point(x, y), substr));//Can throw.
+      std::unique_ptr<DrawString> cmd(new DrawString(wid, gc, Point(x, y), substr));//Can throw.
       fCommands.push_back(cmd.get());//can throw.
       cmd.release();
    } catch (const std::exception &) {
@@ -423,7 +423,7 @@ void CommandBuffer::AddFillRectangle(Drawable_t wid, const GCValues_t &gc,
       r.fY = y;
       r.fWidth = (UShort_t)w;
       r.fHeight = (UShort_t)h;
-      std::auto_ptr<FillRectangle> cmd(new FillRectangle(wid, gc, r));
+      std::unique_ptr<FillRectangle> cmd(new FillRectangle(wid, gc, r));
       fCommands.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -441,7 +441,7 @@ void CommandBuffer::AddDrawRectangle(Drawable_t wid, const GCValues_t &gc,
       r.fY = y;
       r.fWidth = (UShort_t)w;
       r.fHeight = (UShort_t)h;
-      std::auto_ptr<DrawRectangle> cmd(new DrawRectangle(wid, gc, r));
+      std::unique_ptr<DrawRectangle> cmd(new DrawRectangle(wid, gc, r));
       fCommands.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -457,7 +457,7 @@ void CommandBuffer::AddFillPolygon(Drawable_t wid, const GCValues_t &gc,
    assert(nPoints > 0 && "AddFillPolygon, nPoints <= 0");
 
    try {
-      std::auto_ptr<FillPolygon> cmd(new FillPolygon(wid, gc, polygon, nPoints));
+      std::unique_ptr<FillPolygon> cmd(new FillPolygon(wid, gc, polygon, nPoints));
       fCommands.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -471,7 +471,7 @@ void CommandBuffer::AddUpdateWindow(QuartzView *view)
    assert(view != nil && "AddUpdateWindow, view parameter is nil");
 
    try {
-      std::auto_ptr<UpdateWindow> cmd(new UpdateWindow(view));
+      std::unique_ptr<UpdateWindow> cmd(new UpdateWindow(view));
       fCommands.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -483,7 +483,7 @@ void CommandBuffer::AddUpdateWindow(QuartzView *view)
 void CommandBuffer::AddDeletePixmap(Pixmap_t pixmapID)
 {
    try {
-      std::auto_ptr<DeletePixmap> cmd(new DeletePixmap(pixmapID));
+      std::unique_ptr<DeletePixmap> cmd(new DeletePixmap(pixmapID));
       fCommands.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -495,7 +495,7 @@ void CommandBuffer::AddDeletePixmap(Pixmap_t pixmapID)
 void CommandBuffer::AddDrawBoxXor(Window_t windowID, Int_t x1, Int_t y1, Int_t x2, Int_t y2)
 {
    try {
-      std::auto_ptr<DrawBoxXor> cmd(new DrawBoxXor(windowID, Point(x1, y1), Point(x2, y2)));
+      std::unique_ptr<DrawBoxXor> cmd(new DrawBoxXor(windowID, Point(x1, y1), Point(x2, y2)));
       fXorOps.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
@@ -507,7 +507,7 @@ void CommandBuffer::AddDrawBoxXor(Window_t windowID, Int_t x1, Int_t y1, Int_t x
 void CommandBuffer::AddDrawLineXor(Window_t windowID, Int_t x1, Int_t y1, Int_t x2, Int_t y2)
 {
    try {
-      std::auto_ptr<DrawLineXor> cmd(new DrawLineXor(windowID, Point(x1, y1), Point(x2, y2)));
+      std::unique_ptr<DrawLineXor> cmd(new DrawLineXor(windowID, Point(x1, y1), Point(x2, y2)));
       fXorOps.push_back(cmd.get());
       cmd.release();
    } catch (const std::exception &) {
