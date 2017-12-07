37da44fda7c8093864d4e272e470ad32199f03ac
moneta <lorenzo.moneta@cern.ch>
2017-12-07 11:32:59 +0100

    Fix tutorial to use TF3 when filling TH3 objects with FillRandom. Create the TF3 using the new TFormula capabilities for multi-dim functions

--- tutorials/gl/glbox.C
+++ tutorials/gl/glbox.C
@@ -22,13 +22,17 @@ void glbox()
    box1Pad->Draw();
 
    TH3F *h31 = new TH3F("h31", "h31", 10, -1, 1, 10, -1, 1, 10, -1, 1);
-   h31->FillRandom("gaus");
+   TF3 * gxy = new TF3("gaus2","xygaus");
+   gxy->SetParameters(1,0,1,0,0.3);
+   h31->FillRandom("gaus2");
    h31->SetFillColor(2);
    boxPad->cd();
    h31->Draw("glbox");
 
    TH3F *h32 = new TH3F("h32", "h32", 10, -2, 2, 10, -1, 1, 10, -3, 3);
-   h32->FillRandom("gaus");
+   TF3 * g3 = new TF3("gaus3","xyzgaus");
+   g3->SetParameters(1,0,1,0,1,0,1);
+   h32->FillRandom("gaus3");
    h32->SetFillColor(4);
    box1Pad->cd();
    h32->Draw("glbox1");
