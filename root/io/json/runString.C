13e24c3283b627b24ce2143113efd4b25c5b895e
Sergey Linev <S.Linev@gsi.de>
2017-12-14 16:01:31 +0100

    json: use ToJSON signature also for data members conversion

--- root/io/json/runString.C
+++ root/io/json/runString.C
@@ -14,10 +14,10 @@
    cout << " ====== string data types TJsonEx4 ===== " << endl;
    cout << TBufferJSON::ToJSON(ex4) << endl << endl;
    cout << " ============ selected data members ======== " << endl;
-   cout << "ex4.fStr1 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr1") << endl;
-   cout << "ex4.fStr2 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr2") << endl;
-   cout << "ex4.fStr3 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr3") << endl;
-   cout << "ex4.fStr4 = " << TBufferJSON::ConvertToJSON(ex4, gROOT->GetClass("TJsonEx4"), 0, "fStr4") << endl;
+   cout << "ex4.fStr1 = " << TBufferJSON::ToJSON(ex4, 0, "fStr1") << endl;
+   cout << "ex4.fStr2 = " << TBufferJSON::ToJSON(ex4, 0, "fStr2") << endl;
+   cout << "ex4.fStr3 = " << TBufferJSON::ToJSON(ex4, 0, "fStr3") << endl;
+   cout << "ex4.fStr4 = " << TBufferJSON::ToJSON(ex4, 0, "fStr4") << endl;
 
 #endif
 #ifdef ClingWorkAroundBrokenUnnamedReturn
