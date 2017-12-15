459ee1ca9f0a2747e073d3e3c09cc072671da01e
Sergey Linev <S.Linev@gsi.de>
2017-12-15 17:56:37 +0100

    json: update testing with STL classes

--- root/io/json/runSTL.C
+++ root/io/json/runSTL.C
@@ -9,23 +9,33 @@
    gROOT->ProcessLine(".x runSTL.C");
 #else
 
-   TJsonEx7* ex7 = new TJsonEx7(10);
-   TJsonEx8* ex8 = new TJsonEx8(10);
-   TJsonEx12* ex12 = new TJsonEx12();
-   ex12->SetValues(10);
-
-   TJsonEx13* ex13 = new TJsonEx13();
-   ex13->SetValues(10);
-
+   TJsonEx7 *ex7 = new TJsonEx7; ex7->Init();
+   TJsonEx8 *ex8 = new TJsonEx8; ex8->Init();
+   TJsonEx12 *ex12 = new TJsonEx12; ex12->Init();
+   TJsonEx13 *ex13 = new TJsonEx13; ex13->Init();
+   TString json;
 
    cout << " ====== different STL containers TJsonEx7 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex7) << endl << endl;
-   cout << " ====== STL as basec class TJsonEx8 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex8) << endl << endl;
+   json = TBufferJSON::ToJSON(ex7);
+   testJsonReading(json);
+   cout << json << endl << endl;
+   cout << " ====== STL as base class TJsonEx8 ===== " << endl;
+   json = TBufferJSON::ToJSON(ex8);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== STL vector of complex classes TJsonEx12 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex12) << endl << endl;
+   json = TBufferJSON::ToJSON(ex12);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== More exotic STL containers as set or map with TRef inside TJsonEx13 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex13) << endl << endl;
+   json = TBufferJSON::ToJSON(ex13);
+   testJsonReading(json);
+   cout << json << endl << endl;
+
+   delete ex7;
+   delete ex8;
+   delete ex12;
+   delete ex13;
 
 #endif
 #ifdef ClingWorkAroundBrokenUnnamedReturn
--- root/io/json/runSTL0.C
+++ root/io/json/runSTL0.C
@@ -9,22 +9,33 @@
    gROOT->ProcessLine(".x runSTL0.C");
 #else
 
-   TJsonEx7* ex7 = new TJsonEx7(0);
-   TJsonEx8* ex8 = new TJsonEx8(0);
-   TJsonEx12* ex12 = new TJsonEx12();
-   //ex12->SetValues(0);
-
-   TJsonEx13* ex13 = new TJsonEx13();
-   //ex13->SetValues(0);
+   TJsonEx7 *ex7 = new TJsonEx7;
+   TJsonEx8 *ex8 = new TJsonEx8;
+   TJsonEx12 *ex12 = new TJsonEx12;
+   TJsonEx13 *ex13 = new TJsonEx13;
+   TString json;
 
    cout << " ====== different STL containers TJsonEx7 (len=0) ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex7) << endl << endl;
-   cout << " ====== STL as basec class TJsonEx8 (len=0) ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex8) << endl << endl;
+   json = TBufferJSON::ToJSON(ex7);
+   testJsonReading(json);
+   cout << json << endl << endl;
+   cout << " ====== STL as base class TJsonEx8 (len=0) ===== " << endl;
+   json = TBufferJSON::ToJSON(ex8);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== STL vector of complex classes TJsonEx12 (len=0) ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex12) << endl << endl;
+   json = TBufferJSON::ToJSON(ex12);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== More exotic STL containers as set or map with TRef inside TJsonEx13 (len=0) ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex13) << endl << endl;
+   json = TBufferJSON::ToJSON(ex13);
+   testJsonReading(json);
+   cout << json << endl << endl;
+
+   delete ex7;
+   delete ex8;
+   delete ex12;
+   delete ex13;
 
 #endif
 #ifdef ClingWorkAroundBrokenUnnamedReturn
