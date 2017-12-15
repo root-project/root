8c718c190897e8c76f3f96b788f8a5ca5a17e5d8
Sergey Linev <S.Linev@gsi.de>
2017-12-15 18:15:27 +0100

    json: update testing of  ROOT collections like TList or TClonesArray

--- root/io/json/runRootClasses.C
+++ root/io/json/runRootClasses.C
@@ -1,43 +1,83 @@
 {
-   TH1I* h1 = new TH1I("histo1","histo title", 100, -10., 10.);
+// Fill out the code of the actual test
+#ifndef SECOND_RUN
+   gROOT->ProcessLine(".L test_classes.h+");
+#endif
+
+#if defined(ClingWorkAroundMissingDynamicScope) && !defined(SECOND_RUN)
+#define SECOND_RUN
+   gROOT->ProcessLine(".x runRootClasses.C");
+#else
+
+   TH1I *h1 = new TH1I("histo1","histo title", 100, -10., 10.);
    for (Int_t bin=1;bin<=100;++bin)
       h1->SetBinContent(bin, bin % 12);
+   h1->ResetBit(kMustCleanup); // reset bit while it is always done in TH1::Streamer()
 
-   TObject* obj = new TObject();
+   TObject *obj = new TObject();
 
-   TBox* box = new TBox(11,22,33,44);
+   TBox *box = new TBox(11,22,33,44);
 
-   TList* arr = new TList;
+   TList *arr = new TList;
    for(Int_t n=0;n<10;n++) {
       TBox* b = new TBox(n*10,n*100,n*20,n*200);
       arr->Add(b, Form("option_%d_option",n));
    }
 
-   TClonesArray* clones = new TClonesArray("TBox",10);
+   TClonesArray *clones = new TClonesArray("TBox",10);
    for(int n=0;n<10;n++)
        new ((*clones)[n]) TBox(n*10,n*100,n*20,n*200);
 
-   TMap* map = new TMap;
+   TMap *map = new TMap;
    for (int n=0;n<10;n++) {
       TObjString* str = new TObjString(Form("Str%d",n));
       TNamed* nnn = new TNamed(Form("Name%d",n), Form("Title%d",n));
       map->Add(str,nnn);
    }
 
+   TJsonEx14 *ex14 = new TJsonEx14; ex14->Init();
+
+   TString json;
 
    cout << " ====== TObject representation ===== " << endl;
-   cout << TBufferJSON::ToJSON(obj) << endl << endl;
+   json = TBufferJSON::ToJSON(obj);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== TH1I representation ===== " << endl;
-   cout << TBufferJSON::ToJSON(h1) << endl << endl;
+   json = TBufferJSON::ToJSON(h1);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== TBox representation ===== " << endl;
-   cout << TBufferJSON::ToJSON(box) << endl << endl;
+   json = TBufferJSON::ToJSON(box);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== TList representation ===== " << endl;
-   cout << TBufferJSON::ToJSON(arr) << endl << endl;
+   json = TBufferJSON::ToJSON(arr);
+   // testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== TClonesArray representation ===== " << endl;
-   cout << TBufferJSON::ToJSON(clones) << endl << endl;
+   json = TBufferJSON::ToJSON(clones);
+   // testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== TMap representation ===== " << endl;
-   cout << TBufferJSON::ToJSON(map) << endl << endl;
+   json = TBufferJSON::ToJSON(map);
+   // testJsonReading(json);
+   cout << json << endl << endl;
+   cout << " ====== TJsonEx14 with different ROOT collections ===== " << endl;
+   json = TBufferJSON::ToJSON(ex14);
+   testJsonReading(json);
+   cout << json << endl << endl;
 
+   delete obj;
+   delete h1;
+   delete box;
+   delete arr;
+   delete clones;
+   delete map;
+   delete ex14;
+
+
+#endif
 #ifdef ClingWorkAroundBrokenUnnamedReturn
    gApplication->Terminate(0);
 #else
