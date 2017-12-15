23e85c7791a5fa3da2f2f6bbb95b7033e0b6dd53
Sergey Linev <S.Linev@gsi.de>
2017-12-15 17:37:09 +0100

    json: update testing of basic data types (and array of)

--- root/io/json/runBasicTypes.C
+++ root/io/json/runBasicTypes.C
@@ -9,19 +9,28 @@
    gROOT->ProcessLine(".x runBasicTypes.C");
 #else
 
-   TJsonEx1* ex1 = new TJsonEx1;
-   TJsonEx2* ex2 = new TJsonEx2;
-   TJsonEx3* ex3 = new TJsonEx3(5);
-   TJsonEx11* ex11 = new TJsonEx11;
+   TJsonEx1* ex1 = new TJsonEx1; ex1->Init();
+   TJsonEx2* ex2 = new TJsonEx2; ex2->Init();
+   TJsonEx3* ex3 = new TJsonEx3; ex3->Init();
+   TJsonEx11* ex11 = new TJsonEx11; ex11->Init();
+   TString json;
 
    cout << " ====== basic data types TJsonEx1 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex1) << endl << endl;
+   json = TBufferJSON::ToJSON(ex1);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== inheritance from TJsonEx1, TJsonEx11 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex11) << endl << endl;
+   json = TBufferJSON::ToJSON(ex11);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== arrays of basic data types TJsonEx2 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex2) << endl << endl;
+   json = TBufferJSON::ToJSON(ex2);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== dynamic arrays of basic data types TJsonEx3 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex3) << endl << endl;
+   json = TBufferJSON::ToJSON(ex3);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ============ selected data members ======== " << endl;
    cout << "ex2.fInt = " << TBufferJSON::ToJSON(ex2, 0, "fInt") << endl;
    cout << "ex3.fChar = " << TBufferJSON::ToJSON(ex3, 0, "fChar") << endl;
