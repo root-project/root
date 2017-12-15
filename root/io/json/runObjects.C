52bf02329bc739cc407c9571ed1a1313e72ae9b7
Sergey Linev <S.Linev@gsi.de>
2017-12-15 17:46:00 +0100

    json: update testing objects as members, pointers and arrays

--- root/io/json/runObjects.C
+++ root/io/json/runObjects.C
@@ -9,22 +9,32 @@
    gROOT->ProcessLine(".x runObjects.C");
 #else
 
-   TJsonEx5* ex5 = new TJsonEx5(true);
-   TJsonEx6* ex6 = new TJsonEx6(true);
-   TJsonEx10* ex10 = new TJsonEx10();
-   ex10->SetValues();
+   TJsonEx5 *ex5 = new TJsonEx5(); ex5->Init();
+   TJsonEx6 *ex6 = new TJsonEx6(); ex6->Init();
+   TJsonEx10 *ex10 = new TJsonEx10(); ex10->Init();
+   TString json;
 
    cout << " ====== objects as class members TJsonEx5 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex5) << endl << endl;
+   json = TBufferJSON::ToJSON(ex5);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== arrays of objects as class members TJsonEx6 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex6) << endl << endl;
+   json = TBufferJSON::ToJSON(ex6);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ====== ROOT TObject/TNamed/TString as class members TJsonEx10 ===== " << endl;
-   cout << TBufferJSON::ToJSON(ex10) << endl << endl;
+   json = TBufferJSON::ToJSON(ex10);
+   testJsonReading(json);
+   cout << json << endl << endl;
    cout << " ============ selected data members ======== " << endl;
    cout << "ex5.fObj1 = " << TBufferJSON::ToJSON(ex5, 0, "fObj1") << endl;
    cout << "ex5.fPtr1 = " << TBufferJSON::ToJSON(ex5, 0, "fPtr1") << endl;
    cout << "ex5.fSafePtr1 = " << TBufferJSON::ToJSON(ex5, 0, "fSafePtr1") << endl;
    cout << "ex6.fObj1 = " << TBufferJSON::ToJSON(ex6, 0, "fObj1") << endl;
+
+   delete ex5;
+   delete ex6;
+   delete ex10;
 
 #endif
 #ifdef ClingWorkAroundBrokenUnnamedReturn
