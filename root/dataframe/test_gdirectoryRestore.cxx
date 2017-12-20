b89a22031bd5e4b8c523d69ea7c4ca14ea3d6c4b
Danilo Piparo <danilo.piparo@cern.ch>
2017-12-20 16:12:28 +0100

    Add test for ROOT-9116

---
+++ root/dataframe/test_gdirectoryRestore.cxx
@@ -0,0 +1,50 @@
+#include <ROOT/TDataFrame.hxx>
+#include <TFile.h>
+
+using namespace ROOT::Experimental;
+
+void writeHistNoInputFile()
+{
+   std::cout << "write hist no input file" << std::endl;
+   TDataFrame d(1);
+   auto counter = 0;
+   auto dd = d.Define("x", [&counter]() { return counter++; });
+   auto h = dd.Histo1D("x");
+
+   TFile f("gdirectoryRestore1.root", "RECREATE");
+   h->SetDirectory(&f);
+   h->Write();
+}
+
+void makeDataSet()
+{
+   std::cout << "make dataset" << std::endl;
+   TDataFrame d(1);
+   auto counter = 0;
+   auto dd = d.Define("x", [&counter]() { return counter++; });
+   dd.Snapshot<int>("t", "f.root", {"x"});
+}
+
+int writeHistWithInputFile()
+{
+   std::cout << "write hist with input file" << std::endl;
+   TDataFrame d("t", "f.root");
+   auto h = d.Histo1D("x");
+   TFile f("f2.root", "RECREATE");
+   h->SetDirectory(&f);
+   h->Write();
+   return nullptr == gDirectory;
+}
+
+int gdirectoryRestore()
+{
+   ROOT::EnableImplicitMT();
+   writeHistNoInputFile();
+   makeDataSet();
+   return writeHistWithInputFile();
+}
+
+int main()
+{
+   return gdirectoryRestore();
+}
