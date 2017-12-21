de44769ceb2659154c0b5a1efa98c03dfdc768eb
Sergey Linev <S.Linev@gsi.de>
2017-12-21 17:36:33 +0100

    text_io: move common methods to TBufferText

--- io/io/inc/TBufferText.h
+++ io/io/inc/TBufferText.h
@@ -17,6 +17,8 @@
 class TBufferText : public TBuffer {
 
 protected:
+   UShort_t  fPidOffset;
+
    TBufferText();
    TBufferText(TBuffer::EMode mode, TObject *parent = nullptr);
 
@@ -27,6 +29,35 @@ public:
    virtual UInt_t GetTRefExecId();
    virtual TProcessID *ReadProcessID(UShort_t pidf);
    virtual UShort_t WriteProcessID(TProcessID *pid);
+
+   virtual UShort_t GetPidOffset() const { return fPidOffset; }
+   virtual void SetPidOffset(UShort_t offset) { fPidOffset = offset; }
+
+   virtual void TagStreamerInfo(TVirtualStreamerInfo * /*info*/);
+
+   // virtual abstract TBuffer methods, which are not used in text streaming
+
+   virtual void Reset() { Error("Reset", "useless"); }
+   virtual void InitMap() { Error("InitMap", "useless"); }
+   virtual void ResetMap() { Error("ResetMap", "useless"); }
+   virtual void SetReadParam(Int_t /*mapsize*/) { Error("SetReadParam", "useless"); }
+   virtual void SetWriteParam(Int_t /*mapsize*/) { Error("SetWriteParam", "useless"); }
+
+   virtual Int_t GetBufferDisplacement() const
+   {
+      Error("GetBufferDisplacement", "useless");
+      return 0;
+   }
+   virtual void SetBufferDisplacement() { Error("SetBufferDisplacement", "useless"); }
+   virtual void SetBufferDisplacement(Int_t /*skipped*/) { Error("SetBufferDisplacement", "useless"); }
+
+   virtual Int_t ReadBuf(void * /*buf*/, Int_t /*max*/)
+   {
+      Error("ReadBuf", "useless in text streamers");
+      return 0;
+   }
+   virtual void WriteBuf(const void * /*buf*/, Int_t /*max*/) { Error("WriteBuf", "useless in text streamers"); }
+
 
    ClassDef(TBufferText, 0); // a TBuffer subclass for all text-based streamers
 };
