0d85529dd11ded242dd57ff9e97535987a1eff32
Axel Naumann <Axel.Naumann@cern.ch>
2018-01-10 11:27:10 +0100

    Revert "Try to fix test on macOS."; it doesn't help.
    This reverts commit 327a348c05ebf8f6eb1c5f9792709172c8e66b40.

--- hist/histdraw/v7/test/draw.cxx
+++ hist/histdraw/v7/test/draw.cxx
@@ -3,17 +3,11 @@
 #include "ROOT/TCanvas.hxx"
 #include "ROOT/TColor.hxx"
 
-class FixtureBase: public ::testing::Test {
-protected:
-   virtual void SetUp();
-};
-
-struct DrawTest: public FixtureBase {};
+using namespace ROOT::Experimental;
 
 // Test drawing of histograms.
-TEST_F(DrawTest, OneD)
+TEST(DrawTest, OneD)
 {
-   using namespace ROOT::Experimental;
    TAxisConfig xaxis{10, 0., 1.};
    auto h = std::make_shared<TH1D>(xaxis);
    TCanvas canv;
@@ -21,23 +15,21 @@ TEST_F(DrawTest, OneD)
    EXPECT_EQ(canv.GetPrimitives().size(), 1u);
 }
 
-TEST_F(DrawTest, TwoD)
+TEST(DrawTest, TwoD)
 {
-   using namespace ROOT::Experimental;
    TAxisConfig xaxis{10, 0., 1.};
    TAxisConfig yaxis{{0., 1., 10., 100.}};
    auto h = std::make_shared<TH2I>(xaxis, yaxis);
    TCanvas canv;
    canv.Draw(h);
-   // No THist copy c'tor:
+   // No THist copt c'tor:
    // canv.Draw(TH2F(xaxis, yaxis));
    canv.Draw(std::make_unique<TH2C>(xaxis, yaxis));
    EXPECT_EQ(canv.GetPrimitives().size(), 2u);
 }
 
-TEST_F(DrawTest, ThreeD)
+TEST(DrawTest, ThreeD)
 {
-   using namespace ROOT::Experimental;
    TAxisConfig xaxis{{0., 1.}};
    TAxisConfig yaxis{10, 0., 1.};
    TAxisConfig zaxis{{0., 1., 10., 100.}};
@@ -47,13 +39,9 @@ TEST_F(DrawTest, ThreeD)
    EXPECT_EQ(canv.GetPrimitives().size(), 1u);
 }
 
-
-struct DrawOptTest: public FixtureBase {};
-
 // Drawing options:
-TEST_F(DrawOptTest, OneD)
+TEST(DrawOptTest, OneD)
 {
-   using namespace ROOT::Experimental;
    TAxisConfig xaxis{10, 0., 1.};
    auto h = std::make_shared<TH1D>(xaxis);
    TCanvas canv;
@@ -62,11 +50,3 @@ TEST_F(DrawOptTest, OneD)
    TColor shouldBeRed = Opts.GetLineColor();
    EXPECT_EQ(shouldBeRed, TColor::kRed);
 }
-
-
-
-#include "TROOT.h" // for gROOT->SetBatch()
-
-void FixtureBase::SetUp() {
-   gROOT->SetBatch();
-};
