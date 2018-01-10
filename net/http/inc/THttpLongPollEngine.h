a617473af1c1da50e3b4e672877ae7874d6eec3a
Sergey Linev <S.Linev@gsi.de>
2018-01-10 09:14:43 +0100

    http: use std::vector<std::string> in LongPollEngine for the output queue

--- net/http/inc/THttpLongPollEngine.h
+++ net/http/inc/THttpLongPollEngine.h
@@ -15,15 +15,15 @@
 #include "THttpWSEngine.h"
 
 #include <string>
-#include <list>
+#include <vector>
 
 class THttpLongPollEngine : public THttpWSEngine {
 protected:
    THttpCallArg *fPoll;              ///!< polling request, which can be used for the next sending
-   std::list<std::string> fBuf;      ///!< entries submitted to client
+   std::vector<std::string> fQueue;  ///!< entries submitted to client
    static const char *gLongPollNope; ///!< default reply on the longpoll request
 public:
-   THttpLongPollEngine() : THttpWSEngine(), fPoll(nullptr), fBuf() {}
+   THttpLongPollEngine() : THttpWSEngine(), fPoll(nullptr), fQueue() {}
 
    virtual UInt_t GetId() const;
 
@@ -36,8 +36,6 @@ public:
    virtual Bool_t PreviewData(THttpCallArg &arg);
 
    virtual void PostProcess(THttpCallArg &arg);
-
-   ClassDef(THttpLongPollEngine, 0);
 };
 
 #endif
