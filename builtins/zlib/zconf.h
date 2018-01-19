00e82f8c8766c260c1ecc8ea906e09207e0923e4
Oksana Shadura <oksana.shadura@cern.ch>
2018-01-19 09:34:40 +0100

    Synchronizing zconf.h to latest zlib, fixing issues with Windows compilation

--- builtins/zlib/zconf.h
+++ builtins/zlib/zconf.h
@@ -408,11 +408,11 @@ typedef uLong FAR uLongf;
    typedef unsigned long z_crc_t;
 #endif
 
-#if 1    /* was set to #if 1 by ./configure */
+#if HAVE_UNISTD_H    /* was set to #if 1 by ./configure */
 #  define Z_HAVE_UNISTD_H
 #endif
 
-#if 1    /* was set to #if 1 by ./configure */
+#if HAVE_STDARG_H    /* was set to #if 1 by ./configure */
 #  define Z_HAVE_STDARG_H
 #endif
 
