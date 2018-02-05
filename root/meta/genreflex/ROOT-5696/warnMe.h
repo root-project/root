016278e13a49e26e34a29d0bde855b1ba73304e2
Philippe Canal <pcanal@fnal.gov>
2018-02-05 10:35:21 -0600

    In C++17 the warning about deprecated use of register is an error.
    Replacing it with an another warning provoking code

--- root/meta/genreflex/ROOT-5696/warnMe.h
+++ root/meta/genreflex/ROOT-5696/warnMe.h
@@ -12,6 +12,6 @@ class A{
 public:
 
    CONSTEXPR float _growth_factor() { return 1.5; } //from boost
-   int returnInt (int i) { register int a=12; return a*i;}
+   int returnInt (int i) { int a=12; if (a=12) return a*i; else return 0; }
 };
 
