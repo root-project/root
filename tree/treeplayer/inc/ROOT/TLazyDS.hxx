cc21449b280ad81885e798208bc02cd8ea252452
Danilo Piparo <danilo.piparo@cern.ch>
2018-02-07 21:21:52 +0100

    Add copyright statement

--- tree/treeplayer/inc/ROOT/TLazyDS.hxx
+++ tree/treeplayer/inc/ROOT/TLazyDS.hxx
@@ -1,3 +1,13 @@
+// Author: Enrico Guiraud, Danilo Piparo CERN  02/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 #ifndef ROOT_TLAZYDS
 #define ROOT_TLAZYDS
 
