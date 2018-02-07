664351816349fc5a53f31dc5d7f45dc2f54aa38b
Danilo Piparo <danilo.piparo@cern.ch>
2018-02-07 21:11:35 +0100

    [TDF] Add potentially common alias to TDataSource class and format code
    in addition, restore naming coding conventions where needed.
    Protect doxygen comments from clang-format.

--- tree/treeplayer/inc/ROOT/TDataSource.hxx
+++ tree/treeplayer/inc/ROOT/TDataSource.hxx
@@ -13,7 +13,7 @@
 
 #include "RStringView.h"
 #include "RtypesCore.h" // ULong64_t
-#include <algorithm> // std::transform
+#include <algorithm>    // std::transform
 #include <vector>
 #include <typeinfo>
 
@@ -21,6 +21,7 @@ namespace ROOT {
 namespace Experimental {
 namespace TDF {
 
+// clang-format off
 /**
 \class ROOT::Experimental::TDF::TDataSource
 \ingroup dataframe
@@ -49,31 +50,43 @@ Methods 3,8 are called once per event-loop, right before starting and right afte
 Methods 5,6,7 can be called concurrently from multiple threads, multiple times per event-loop.
 */
 class TDataSource {
+   // clang-format on
+protected:
+   using Record_t = std::vector<void *>;
+
 public:
    virtual ~TDataSource() = default;
 
+   // clang-format off
    /// \brief Inform TDataSource of the number of processing slots (i.e. worker threads) used by the associated TDataFrame.
    /// Slots numbers are used to simplify parallel execution: TDataFrame guarantees that different threads will always
    /// pass different slot values when calling methods concurrently.
+   // clang-format on
    virtual void SetNSlots(unsigned int nSlots) = 0;
 
+   // clang-format off
    /// \brief Returns a reference to the collection of the dataset's column names
+   // clang-format on
    virtual const std::vector<std::string> &GetColumnNames() const = 0;
 
    /// \brief Checks if the dataset has a certain column
    /// \param[in] columnName The name of the column
    virtual bool HasColumn(std::string_view) const = 0;
 
+   // clang-format off
    /// \brief Type of a column as a string, e.g. `GetTypeName("x") == "double"`. Required for jitting e.g. `df.Filter("x>0")`.
    /// \param[in] columnName The name of the column
+   // clang-format on
    virtual std::string GetTypeName(std::string_view) const = 0;
 
+   // clang-format off
    /// Called at most once per column by TDF. Return vector of pointers to pointers to column values - one per slot.
    /// \tparam T The type of the data stored in the column
    /// \param[in] columnName The name of the column
    ///
-   /// These pointers are veritable cursors: it's a responsibility of the TDataSource implementation that they point to the
-   /// "right" memory region.
+   /// These pointers are veritable cursors: it's a responsibility of the TDataSource implementation that they point to
+   /// the "right" memory region.
+   // clang-format on
    template <typename T>
    std::vector<T **> GetColumnReaders(std::string_view columnName)
    {
@@ -84,44 +97,55 @@ public:
       return typedVec;
    }
 
+   // clang-format off
    /// \brief Return ranges of entries to distribute to tasks.
-   /// They are required to be contiguous intervals with no entries skipped. Supposing a dataset with nEntries, the intervals
-   /// must start at 0 and end at nEntries, e.g. [0-5],[5-10] for 10 entries.
+   /// They are required to be contiguous intervals with no entries skipped. Supposing a dataset with nEntries, the
+   /// intervals must start at 0 and end at nEntries, e.g. [0-5],[5-10] for 10 entries.
+   // clang-format on
    virtual std::vector<std::pair<ULong64_t, ULong64_t>> GetEntryRanges() = 0;
 
+   // clang-format off
    /// \brief Advance the "cursors" returned by GetColumnReaders to the selected entry for a particular slot.
    /// \param[in] slot The data processing slot that needs to be considered
    /// \param[in] entry The entry which needs to be pointed to by the reader pointers
-   /// Slots are adopted to accommodate parallel data processing. Different workers will loop over different ranges and will
-   /// be labelled by different "slot" values.
+   /// Slots are adopted to accommodate parallel data processing. Different workers will loop over different ranges and
+   /// will be labelled by different "slot" values.
+   // clang-format on
    virtual void SetEntry(unsigned int slot, ULong64_t entry) = 0;
 
+   // clang-format off
    /// \brief Convenience method called before starting an event-loop.
    /// This method might be called multiple times over the lifetime of a TDataSource, since
    /// users can run multiple event-loops with the same TDataFrame.
    /// Ideally, `Initialise` should set the state of the TDataSource so that multiple identical event-loops
    /// will produce identical results.
+   // clang-format on
    virtual void Initialise() {}
 
+   // clang-format off
    /// \brief Convenience method called at the start of the data processing associated to a slot.
    /// \param[in] slot The data processing slot wihch needs to be initialised
    /// \param[in] firstEntry The first entry of the range that the task will process.
    /// This method might be called multiple times per thread per event-loop.
+   // clang-format on
    virtual void InitSlot(unsigned int /*slot*/, ULong64_t /*firstEntry*/) {}
 
+   // clang-format off
    /// \brief Convenience method called at the end of the data processing associated to a slot.
    /// \param[in] slot The data processing slot wihch needs to be finalised
    /// This method might be called multiple times per thread per event-loop.
+   // clang-format on
    virtual void FinaliseSlot(unsigned int /*slot*/) {}
 
+   // clang-format off
    /// \brief Convenience method called after concluding an event-loop.
    /// See Initialise for more details.
+   // clang-format on
    virtual void Finalise() {}
 
 protected:
    /// type-erased vector of pointers to pointers to column values - one per slot
-   virtual std::vector<void *>
-   GetColumnReadersImpl(std::string_view name, const std::type_info &) = 0;
+   virtual std::vector<void *> GetColumnReadersImpl(std::string_view name, const std::type_info &) = 0;
 };
 
 } // ns TDF
