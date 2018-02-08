efe0a610a6e313cd0c751ceced9c66addc5cabb4
Axel Naumann <Axel.Naumann@cern.ch>
2018-02-08 18:22:50 +0100

    Use shared_ptr instead of TCanvas attrib table and custom use count.

--- graf2d/primitives/v7/inc/ROOT/TDrawingAttrs.hxx
+++ graf2d/primitives/v7/inc/ROOT/TDrawingAttrs.hxx
@@ -16,180 +16,107 @@
 #ifndef ROOT7_TDrawingAttrs
 #define ROOT7_TDrawingAttrs
 
-#include <ROOT/TColor.hxx>
+#include <ROOT/TStyle.hxx>
 
-#include <RStringView.h>
-
-#include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace ROOT {
 namespace Experimental {
-class TCanvas;
-class TPadBase;
-class TDrawingOptsBaseNoDefault;
 
-namespace Internal {
+class TColor;
+class TStringAttr;
 
-template <class PRIMITIVE>
-class TDrawingAttrTable;
-}
+///\{
+/// Initialize an attribute `val` from a string value.
+///
+///\param[in] name - the attribute name, for diagnostic purposes.
+///\param[in] strval - the attribute value as a string.
+///\param[out] val - the value to be initialized.
+void InitializeAttrFromString(const std::string &name, const std::string &strval, int& val);
+void InitializeAttrFromString(const std::string &name, const std::string &strval, long long& val);
+void InitializeAttrFromString(const std::string &name, const std::string &strval, float& val);
+void InitializeAttrFromString(const std::string &name, const std::string &strval, std::string& val);
+///\}
 
-/** \class ROOT::Experimental::TDrawingAttrRef
- The `TCanvas` keep track of `TColor`s, integer and floating point attributes used by the drawing options,
- making them accessible from other drawing options. The index into the table of the active attributes is
- wrapped into `TDrawingAttrRef` to make them type-safe (i.e. distinct for `TColor`, `long long` and `double`).
+/** \class ROOT::Experimental::TDrawingAttrOrRef
+ A wrapper for a graphics attribute, for instance a `TColor`.
+ The `TTopmostPad` keeps track of shared attributes used by multiple drawing options by means of
+ `weak_ptr`s; `TDrawingAttrOrRef`s hold `shared_ptr`s to these.
+ The reference into the table of the shared attributes is wrapped into the reference of the `TDrawingAttrOrRef`
+ to make them type-safe (i.e. distinct for `TColor`, `long long` and `double`).
  */
 
-template <class PRIMITIVE>
-class TDrawingAttrRef {
+template <class ATTR>
+class TDrawingAttrOrRef {
 private:
-   size_t fIdx = (size_t)-1; ///< The index in the relevant attribute table of `TCanvas`.
+   std::shared_ptr<ATTR> fPtr; ///< The shared_ptr, shared with the relevant attribute table of `TTopmostPad`.
+   ATTR fAttr; ///< The attribute value, used if `!fPtr`.
 
-   /// Construct a reference given the index.
-   explicit TDrawingAttrRef(size_t idx): fIdx(idx) {}
+   /// Whether this attribute is shared (through `TTopmostPad`'s attribute table) with other `TDrawingAttrOrRef`
+   /// objects.
+   bool IsShared() const { return (bool) fPtr; }
 
-   friend class Internal::TDrawingAttrTable<PRIMITIVE>;
+   /// Share the attribute, potentially transforming this into a shared attribute.
+   std::shared_ptr<ATTR> GetSharedPtr() {
+      if (!IsShared())
+         fPtr = std::make_shared<ATTR>(std::move(fAttr));
+      return fPtr;
+   }
 
-public:
-   /// Construct an invalid reference.
-   TDrawingAttrRef() = default;
-
-   /// Construct a reference from its options object, name, default value and set of string options.
-   ///
-   /// Initializes the PRIMITIVE to the default value, as available in TDrawingOptsBase::GetDefaultCanvas(),
-   /// or to `deflt` if no entry exists under the attribute name. The attribute name is `opts.GetName() + "." + attrName`.
-   /// `optStrings` is only be used if `PRIMITIVE` is `long long`; the style setting is expected to be one of
-   /// the strings, with the attribute's value the index of the string.
-   TDrawingAttrRef(TDrawingOptsBaseNoDefault &opts, const std::string &attrName, const PRIMITIVE &deflt,
-                   const std::vector<std::string_view> &optStrings = {});
-
-   /// Get the underlying index.
-   operator size_t() const { return fIdx; }
-
-   /// Whether the reference is valid.
-   explicit operator bool() const { return fIdx != (size_t)-1; }
-};
-
-extern template class TDrawingAttrRef<TColor>;
-extern template class TDrawingAttrRef<long long>;
-extern template class TDrawingAttrRef<double>;
-
-namespace Internal {
-
-template <class PRIMITIVE>
-class TDrawingAttrAndUseCount {
-   /// The value.
-   PRIMITIVE fVal;
-   /// The value's use count.
-   size_t fUseCount = 1;
-
-   /// Clear the value; use count must be 0.
-   void Clear();
+   /// Construct a TDrawingAttrOrRef from an shared_ptr.
+   TDrawingAttrOrRef(const std::shared_ptr<ATTR> &otherPtr): fPtr(otherPtr) {}
 
 public:
-   /// Default constructor: a default-constructed value that is unused.
-   TDrawingAttrAndUseCount(): fVal(), fUseCount(0) {}
+   /// Construct a default, non-shared attribute. The default value gets read from the default style,
+   /// given the attribute's name.
+   TDrawingAttrOrRef(const std::string &name) {
+      InitializeAttrFromString(name, TStyle::GetCurrent().GetAttribute(name), fAttr);
+   }
 
-   /// Initialize with a value, setting use count to 1.
-   explicit TDrawingAttrAndUseCount(const PRIMITIVE &val): fVal(val) {}
+   /// Construct a default, non-shared attribute. The default value gets read from the default style,
+   /// given the attribute's name and arguments for the default attribute constructor, should no
+   /// style entry be found.
+   template <class... ARGS>
+   TDrawingAttrOrRef(const std::string &name, ARGS... args): fAttr(args...) {
+      InitializeAttrFromString(name, TStyle::GetCurrent().GetAttribute(name), fAttr);
+   }
 
-   /// Create a value, initializing use count to 1.
-   void Create(const PRIMITIVE &val);
+   /// Construct a *non-shared* attribute, copying the attribute's value.
+   TDrawingAttrOrRef(TDrawingAttrOrRef &other): fAttr(other.Get()) {}
 
-   /// Increase the use count; use count must be >= 1 before (i.e. does not create or "resurrect" values).
-   void IncrUse();
+   /// Move an attribute.
+   TDrawingAttrOrRef(TDrawingAttrOrRef &&other) = default;
 
-   /// Decrease the use count; use count must be >= 1 before the call. Calls Clear() if use count drops to 0.
-   void DecrUse();
+   /// Create a shared attribute.
+   TDrawingAttrOrRef Share() {
+      return GetSharedPtr();
+   }
 
-   /// Whether the use count is 0 and this object has space for a new value.
-   bool IsFree() const { return fUseCount == 0; }
+   /// Get the const attribute, whether it's shared or not.
+   const ATTR &Get() const {
+      if (IsShared())
+         return *fPtr;
+      return fAttr;
+   }
 
-   /// Value access (non-const).
-   PRIMITIVE &Get() { return fVal; }
+   /// Get the non-const attribute, whether it's shared or not.
+   ATTR &Get() {
+      if (IsShared())
+         return *fPtr;
+      return fAttr;
+   }
 
-   /// Value access (const).
-   const PRIMITIVE &Get() const { return fVal; }
-};
+   /// Convert to an ATTR (const).
+   explicit operator const ATTR& () const{ return Get(); }
+   /// Convert to an ATTR (non-const).
+   explicit operator ATTR& () { return Get(); }
 
-// Only these specializations are used and provided:
-extern template class TDrawingAttrAndUseCount<TColor>;
-extern template class TDrawingAttrAndUseCount<long long>;
-extern template class TDrawingAttrAndUseCount<double>;
-
-template <class PRIMITIVE>
-class TDrawingAttrTable {
-public:
-   using value_type = Internal::TDrawingAttrAndUseCount<PRIMITIVE>;
-
-private:
-   /// Table of attribute primitives. Slots can be freed and re-used.
-   /// Drawing options will reference slots in here through their index.
-   std::vector<value_type> fTable;
-
-public:
-   /// Register an attribute with the table.
-   /// \returns the index in the table.
-   TDrawingAttrRef<PRIMITIVE> Register(const PRIMITIVE &val);
-
-   /// Add a use of the attribute at table index idx.
-   void IncrUse(TDrawingAttrRef<PRIMITIVE> idx) { fTable[idx].IncrUse(); }
-
-   /// Remove a use of the attribute at table index idx.
-   void DecrUse(TDrawingAttrRef<PRIMITIVE> idx) { fTable[idx].DecrUse(); }
-
-   /// Update an existing attribute entry in the table.
-   void Update(TDrawingAttrRef<PRIMITIVE> idx, const PRIMITIVE &val) { fTable[idx] = value_type(val); }
-
-   /// Get the value at index `idx` (const version).
-   const PRIMITIVE &Get(TDrawingAttrRef<PRIMITIVE> idx) const { return fTable[idx].Get(); }
-
-   /// Get the value at index `idx` (non-const version).
-   PRIMITIVE &Get(TDrawingAttrRef<PRIMITIVE> idx) { return fTable[idx].Get(); }
-
-   /// Find the index belonging to the attribute at the given address and add a use.
-   /// \returns the reference to `val`, which might be `IsInvalid()` if `val` is not part of this table.
-   TDrawingAttrRef<PRIMITIVE> SameAs(const PRIMITIVE &val);
-
-   /// Access to the underlying attribute table (non-const version).
-   std::vector<value_type> &GetTable() { return fTable; }
-
-   /// Access to the underlying attribute table (const version).
-   const std::vector<value_type> &GetTable() const { return fTable; }
-};
-
-extern template class TDrawingAttrTable<TColor>;
-extern template class TDrawingAttrTable<long long>;
-extern template class TDrawingAttrTable<double>;
-} // namespace Internal
-
-struct TLineAttrs {
-   TDrawingAttrRef<TColor> fColor;    ///< Line color
-   TDrawingAttrRef<long long> fWidth; ///< Line width
-
-   struct Width {
-      long long fVal;
-      explicit operator long long() const { return fVal; }
-   };
-
-   TLineAttrs() = default;
-   /// Construct from the pad that holds our `TDrawable`, passing the configuration name of this line attribute.
-   TLineAttrs(TDrawingOptsBaseNoDefault &opts, const std::string &name, const TColor &col, Width width)
-      : fColor(opts, name + ".Color", col), fWidth(opts, name + ".Width", (long long)width)
-   {}
-};
-
-struct TFillAttrs {
-   TDrawingAttrRef<TColor> fColor; ///< Fill color
-
-   TFillAttrs() = default;
-
-   /// Construct from the pad that holds our `TDrawable`, passing the configuration name of this line attribute.
-   TFillAttrs(TDrawingOptsBaseNoDefault &opts, const std::string &name, const TColor &col)
-      : fColor(opts, name + ".Color", col)
-   {}
+   /// Assign an ATTR.
+   TDrawingAttrOrRef& operator=(const ATTR& attr) { Get() = attr; return *this; }
+   /// Move-assign an ATTR.
+   TDrawingAttrOrRef& operator=(ATTR&& attr) { Get() = std::move(attr); return *this; }
 };
 
 } // namespace Experimental
