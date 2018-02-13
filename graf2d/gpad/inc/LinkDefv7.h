48a6a252e5f6c22c6e96b108a4421dbc4d6e8685
Axel Naumann <Axel.Naumann@cern.ch>
2018-02-13 14:55:50 +0100

    Split linkdef into v6, v7.

---
+++ graf2d/gpad/inc/LinkDefv7.h
@@ -0,0 +1,45 @@
+/* @(#)root/gpad:$Id$ */
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class ROOT::Experimental::Detail::TMenuItem+;
+#pragma link C++ class std::vector<ROOT::Experimental::Detail::TMenuItem*>+;
+#pragma link C++ class ROOT::Experimental::Detail::TCheckedMenuItem+;
+#pragma link C++ class ROOT::Experimental::Detail::TMenuArgument+;
+#pragma link C++ class std::vector<ROOT::Experimental::Detail::TMenuArgument>+;
+#pragma link C++ class ROOT::Experimental::Detail::TArgsMenuItem+;
+#pragma link C++ class ROOT::Experimental::TMenuItems+;
+#pragma link C++ class ROOT::Experimental::TObjectDrawable+;
+#pragma link C++ class ROOT::Experimental::Detail::TPadUserCoordBase+;
+#pragma link C++ class ROOT::Experimental::Detail::TPadLinearUserCoord+;
+#pragma link C++ struct ROOT::Experimental::Internal::TPadHorizVert+;
+#pragma link C++ struct ROOT::Experimental::TPadExtent+;
+#pragma link C++ struct ROOT::Experimental::TPadPos+;
+#pragma link C++ class std::vector<std::unique_ptr<ROOT::Experimental::TDrawable>>+;
+#pragma link C++ class ROOT::Experimental::TPadBase+;
+#pragma link C++ class ROOT::Experimental::TPadDrawingOpts+;
+#pragma link C++ class ROOT::Experimental::TPad+;
+#pragma link C++ class ROOT::Experimental::TPadDrawable+;
+#pragma link C++ class ROOT::Experimental::TCanvas+;
+#pragma link C++ class ROOT::Experimental::TFrame+;
+#pragma link C++ class ROOT::Experimental::TPadLength+;
+#pragma link C++ class ROOT::Experimental::TPadLength::Pixel+;
+#pragma link C++ class ROOT::Experimental::TPadLength::Normal+;
+#pragma link C++ class ROOT::Experimental::TPadLength::User+;
+#pragma link C++ class ROOT::Experimental::TPadLength::CoordSysBase<ROOT::Experimental::TPadLength::Pixel>+;
+#pragma link C++ class ROOT::Experimental::TPadLength::CoordSysBase<ROOT::Experimental::TPadLength::Normal>+;
+#pragma link C++ class ROOT::Experimental::TPadLength::CoordSysBase<ROOT::Experimental::TPadLength::User>+;
+
+#endif
