7100edf57478452390e35365c29ad41e07d25100
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-02-15 16:03:05 +0100

    [TDF][NFC] Move TDFInterface utilities to TDFInterfaceUtils.hxx

---
+++ tree/treeplayer/src/TDFInterfaceUtils.cxx
@@ -0,0 +1,154 @@
+// Author: Enrico Guiraud, Danilo Piparo CERN  02/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2016, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/TDFInterfaceUtils.hxx>
+#include <TFriendElement.h>
+
+namespace ROOT {
+namespace Internal {
+namespace TDF {
+
+
+// The set here is used as a registry, the real list, which keeps the order, is
+// the one in the vector
+void GetBranchNamesImpl(TTree &t, std::set<std::string> &bNamesReg, ColumnNames_t &bNames,
+                        std::set<TTree *> &analysedTrees)
+{
+
+   if (!analysedTrees.insert(&t).second) {
+      return;
+   }
+
+   auto branches = t.GetListOfBranches();
+   if (branches) {
+      for (auto branchObj : *branches) {
+         auto name = branchObj->GetName();
+         if (bNamesReg.insert(name).second) {
+            bNames.emplace_back(name);
+         }
+      }
+   }
+
+   auto friendTrees = t.GetListOfFriends();
+
+   if (!friendTrees)
+      return;
+
+   for (auto friendTreeObj : *friendTrees) {
+      auto friendTree = ((TFriendElement *)friendTreeObj)->GetTree();
+      GetBranchNamesImpl(*friendTree, bNamesReg, bNames, analysedTrees);
+   }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// Get all the branches names, including the ones of the friend trees
+ColumnNames_t GetBranchNames(TTree &t)
+{
+   std::set<std::string> bNamesSet;
+   ColumnNames_t bNames;
+   std::set<TTree *> analysedTrees;
+   GetBranchNamesImpl(t, bNamesSet, bNames, analysedTrees);
+   return bNames;
+}
+
+void CheckCustomColumn(std::string_view definedCol, TTree *treePtr, const ColumnNames_t &customCols,
+                       const ColumnNames_t &dataSourceColumns)
+{
+   const std::string definedColStr(definedCol);
+   if (treePtr != nullptr) {
+      // check if definedCol is already present in TTree
+      const auto branch = treePtr->GetBranch(definedColStr.c_str());
+      if (branch != nullptr) {
+         const auto msg = "branch \"" + definedColStr + "\" already present in TTree";
+         throw std::runtime_error(msg);
+      }
+   }
+   // check if definedCol has already been `Define`d in the functional graph
+   if (std::find(customCols.begin(), customCols.end(), definedCol) != customCols.end()) {
+      const auto msg = "Redefinition of column \"" + definedColStr + "\"";
+      throw std::runtime_error(msg);
+   }
+   // check if definedCol is already present in the DataSource (but has not yet been `Define`d)
+   if (!dataSourceColumns.empty()) {
+      if (std::find(dataSourceColumns.begin(), dataSourceColumns.end(), definedCol) != dataSourceColumns.end()) {
+         const auto msg = "Redefinition of column \"" + definedColStr + "\" already present in the data-source";
+         throw std::runtime_error(msg);
+      }
+   }
+}
+
+void CheckSnapshot(unsigned int nTemplateParams, unsigned int nColumnNames)
+{
+   if (nTemplateParams != nColumnNames) {
+      std::string err_msg = "The number of template parameters specified for the snapshot is ";
+      err_msg += std::to_string(nTemplateParams);
+      err_msg += " while ";
+      err_msg += std::to_string(nColumnNames);
+      err_msg += " columns have been specified.";
+      throw std::runtime_error(err_msg);
+   }
+}
+
+/// Choose between local column names or default column names, throw in case of errors.
+const ColumnNames_t
+SelectColumns(unsigned int nRequiredNames, const ColumnNames_t &names, const ColumnNames_t &defaultNames)
+{
+   if (names.empty()) {
+      // use default column names
+      if (defaultNames.size() < nRequiredNames)
+         throw std::runtime_error(
+            std::to_string(nRequiredNames) + " column name" + (nRequiredNames == 1 ? " is" : "s are") +
+            " required but none were provided and the default list has size " + std::to_string(defaultNames.size()));
+      // return first nRequiredNames default column names
+      return ColumnNames_t(defaultNames.begin(), defaultNames.begin() + nRequiredNames);
+   } else {
+      // use column names provided by the user to this particular transformation/action
+      if (names.size() != nRequiredNames) {
+         auto msg = std::to_string(nRequiredNames) + " column name" + (nRequiredNames == 1 ? " is" : "s are") +
+                    " required but " + std::to_string(names.size()) + (names.size() == 1 ? " was" : " were") +
+                    " provided:";
+         for (const auto &name : names)
+            msg += " \"" + name + "\",";
+         msg.back() = '.';
+         throw std::runtime_error(msg);
+      }
+      return names;
+   }
+}
+
+ColumnNames_t FindUnknownColumns(const ColumnNames_t &requiredCols, TTree *tree, const ColumnNames_t &definedCols,
+                                 const ColumnNames_t &dataSourceColumns)
+{
+   ColumnNames_t unknownColumns;
+   for (auto &column : requiredCols) {
+      const auto isTreeBranch = (tree != nullptr && tree->GetBranch(column.c_str()) != nullptr);
+      if (isTreeBranch)
+         continue;
+      const auto isCustomColumn = std::find(definedCols.begin(), definedCols.end(), column) != definedCols.end();
+      if (isCustomColumn)
+         continue;
+      const auto isDataSourceColumn =
+         std::find(dataSourceColumns.begin(), dataSourceColumns.end(), column) != dataSourceColumns.end();
+      if (isDataSourceColumn)
+         continue;
+      unknownColumns.emplace_back(column);
+   }
+   return unknownColumns;
+}
+
+bool IsInternalColumn(std::string_view colName)
+{
+   return 0 == colName.find("tdf") && '_' == colName.back();
+}
+
+
+} // namespace TDF
+} // namespace Internal
+} // namespace ROOT
