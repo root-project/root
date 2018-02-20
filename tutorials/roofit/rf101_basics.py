16008c5d468bcb6231a81cc7fa66bfb71ab5a055
Clemens Lange <clemens.lange@cern.ch>
2018-02-20 17:25:34 +0100

    adjust headers and remove __main__ for first two examples

--- tutorials/roofit/rf101_basics.py
+++ tutorials/roofit/rf101_basics.py
@@ -1,73 +1,74 @@
+## \file
+## \ingroup tutorial_roofit
+## \notebook
+## This tutorial illustrates the basic features of RooFit.
+##
+## \macro_code
+##
+## \date February 2018
+## \author Clemens Lange
+## \author Wouter Verkerke (C version)
+
 import ROOT
 
+# Setup model
+# ---------------------
+# Declare variables x,mean,sigma with associated name, title, initial
+# value and allowed range
+x = ROOT.RooRealVar("x", "x", -10, 10)
+mean = ROOT.RooRealVar("mean", "mean of gaussian", 1, -10, 10)
+sigma = ROOT.RooRealVar("sigma", "width of gaussian", 1, 0.1, 10)
 
-def rf101_basics():
+# Build gaussian p.d.f in terms of x,mean and sigma
+gauss = ROOT.RooGaussian("gauss", "gaussian PDF", x, mean, sigma)
 
-    # S e t u p   m o d e l
-    # ---------------------
+# Construct plot frame in 'x'
+xframe = x.frame(ROOT.RooFit.Title("Gaussian p.d.f."))  # RooPlot
 
-    # Declare variables x,mean,sigma with associated name, title, initial
-    # value and allowed range
-    x = ROOT.RooRealVar("x", "x", -10, 10)
-    mean = ROOT.RooRealVar("mean", "mean of gaussian", 1, -10, 10)
-    sigma = ROOT.RooRealVar("sigma", "width of gaussian", 1, 0.1, 10)
+# Plot model and change parameter values
+# ---------------------------------------------------------------------------
+# Plot gauss in frame (i.e. in x)
+gauss.plotOn(xframe)
 
-    # Build gaussian p.d.f in terms of x,mean and sigma
-    gauss = ROOT.RooGaussian("gauss", "gaussian PDF", x, mean, sigma)
+# Change the value of sigma to 3
+sigma.setVal(3)
 
-    # Construct plot frame in 'x'
-    xframe = x.frame(ROOT.RooFit.Title("Gaussian p.d.f."))  # RooPlot
+# Plot gauss in frame (i.e. in x) and draw frame on canvas
+gauss.plotOn(xframe, ROOT.RooFit.LineColor(ROOT.kRed))
 
-    # P l o t   m o d e l   a n d   c h a n g e   p a r a m e t e r   v a l u e s
-    # ---------------------------------------------------------------------------
+# Generate events
+# -----------------------------
+# Generate a dataset of 1000 events in x from gauss
+data = gauss.generate(ROOT.RooArgSet(x), 10000)  # ROOT.RooDataSet
 
-    # Plot gauss in frame (i.e. in x)
-    gauss.plotOn(xframe)
+# Make a second plot frame in x and draw both the
+# data and the p.d.f in the frame
+xframe2 = x.frame(ROOT.RooFit.Title(
+    "Gaussian p.d.f. with data"))  # RooPlot
+data.plotOn(xframe2)
+gauss.plotOn(xframe2)
 
-    # Change the value of sigma to 3
-    sigma.setVal(3)
+# Fit model to data
+# -----------------------------
 
-    # Plot gauss in frame (i.e. in x) and draw frame on canvas
-    gauss.plotOn(xframe, ROOT.RooFit.LineColor(ROOT.kRed))
+# Fit pdf to data
+gauss.fitTo(data)
 
-    # G e n e r a t e   e v e n t s
-    # -----------------------------
+# Print values of mean and sigma (that now reflect fitted values and
+# errors)
+mean.Print()
+sigma.Print()
 
-    # Generate a dataset of 1000 events in x from gauss
-    data = gauss.generate(ROOT.RooArgSet(x), 10000)  # ROOT.RooDataSet
+# Draw all frames on a canvas
+c = ROOT.TCanvas("rf101_basics", "rf101_basics", 800, 400)
+c.Divide(2)
+c.cd(1)
+ROOT.gPad.SetLeftMargin(0.15)
+xframe.GetYaxis().SetTitleOffset(1.6)
+xframe.Draw()
+c.cd(2)
+ROOT.gPad.SetLeftMargin(0.15)
+xframe2.GetYaxis().SetTitleOffset(1.6)
+xframe2.Draw()
 
-    # Make a second plot frame in x and draw both the
-    # data and the p.d.f in the frame
-    xframe2 = x.frame(ROOT.RooFit.Title(
-        "Gaussian p.d.f. with data"))  # RooPlot
-    data.plotOn(xframe2)
-    gauss.plotOn(xframe2)
-
-    # F i t   m o d e l   t o   d a t a
-    # -----------------------------
-
-    # Fit pdf to data
-    gauss.fitTo(data)
-
-    # Print values of mean and sigma (that now reflect fitted values and
-    # errors)
-    mean.Print()
-    sigma.Print()
-
-    # Draw all frames on a canvas
-    c = ROOT.TCanvas("rf101_basics", "rf101_basics", 800, 400)
-    c.Divide(2)
-    c.cd(1)
-    ROOT.gPad.SetLeftMargin(0.15)
-    xframe.GetYaxis().SetTitleOffset(1.6)
-    xframe.Draw()
-    c.cd(2)
-    ROOT.gPad.SetLeftMargin(0.15)
-    xframe2.GetYaxis().SetTitleOffset(1.6)
-    xframe2.Draw()
-
-    c.SaveAs("rf101_basics.png")
-
-
-if __name__ == "__main__":
-    rf101_basics()
+c.SaveAs("rf101_basics.png")
--- tutorials/roofit/rf102_dataimport.py
+++ tutorials/roofit/rf102_dataimport.py
@@ -1,133 +1,18 @@
-# ####################################
-#
-# 'BASIC FUNCTIONALITY' ROOT.RooFit tutorial macro #102
-#
-# Importing data from ROOT ROOT.TTrees and ROOT.THx histograms
-#
-#
-#
-# 07/2008 - Wouter Verkerke
-#
-# ###################################/
+## \file
+## \ingroup tutorial_roofit
+## \notebook
+## 'BASIC FUNCTIONALITY' ROOT.RooFit tutorial macro #102
+## Importing data from ROOT ROOT.TTrees and ROOT.THx histograms
+##
+## \macro_code
+##
+## \date February 2018
+## \author Clemens Lange
+## \author Wouter Verkerke (C version)
+
 
 import ROOT
 from array import array
-
-
-def rf102_dataimport():
-
-    ############################
-    # I m p o r t i n g   R O O ROOT.T   h i s t o g r a m s  #
-    ############################
-
-    # I m p o r t   ROOT.T H 1   i n t o   a   R o o D a t a H i s t
-    # ---------------------------------------------------------
-
-    # Create a ROOT ROOT.TH1 histogram
-    hh = makeTH1()
-
-    # Declare observable x
-    x = ROOT.RooRealVar("x", "x", -10, 10)
-
-    # Create a binned dataset that imports contents of ROOT.TH1 and associates
-    # its contents to observable 'x'
-    dh = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(x),
-                          ROOT.RooFit.Import(hh))
-
-    # P l o t   a n d   f i t   a   R o o D a t a H i s t
-    # ---------------------------------------------------
-
-    # Make plot of binned dataset showing Poisson error bars (ROOT.RooFit
-    # default)
-    frame = x.frame(ROOT.RooFit.Title(
-        "Imported ROOT.TH1 with Poisson error bars"))
-    dh.plotOn(frame)
-
-    # Fit a Gaussian p.d.f to the data
-    mean = ROOT.RooRealVar("mean", "mean", 0, -10, 10)
-    sigma = ROOT.RooRealVar("sigma", "sigma", 3, 0.1, 10)
-    gauss = ROOT.RooGaussian("gauss", "gauss", x, mean, sigma)
-    gauss.fitTo(dh)
-    gauss.plotOn(frame)
-
-    # P l o t   a n d   f i t   a   R o o D a t a H i s t   w i t h   i n t e r n a l   e r r o r s
-    # ---------------------------------------------------------------------------------------------
-
-    # If histogram has custom error (i.e. its contents is does not originate from a Poisson process
-    # but e.g. is a sum of weighted events) you can data with symmetric 'sum-of-weights' error instead
-    # (same error bars as shown by ROOT)
-    frame2 = x.frame(ROOT.RooFit.Title(
-        "Imported ROOT.TH1 with internal errors"))
-    dh.plotOn(frame2, ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2))
-    gauss.plotOn(frame2)
-
-    # Please note that error bars shown (Poisson or SumW2) are for visualization only, the are NOT used
-    # in a maximum likelihood fit
-    #
-    # A (binned) ML fit will ALWAYS assume the Poisson error interpretation of data (the mathematical definition
-    # of likelihood does not take any external definition of errors). Data with non-unit weights can only be correctly
-    # fitted with a chi^2 fit (see rf602_chi2fit.C)
-
-    ########################
-    # I m p o r t i n g   R O O ROOT.T  ROOT.T ROOT.T r e e s   #
-    ########################
-
-    # I m p o r t   ROOT.T ROOT.T r e e   i n t o   a   R o o D a t a S e t
-    # -----------------------------------------------------------
-
-    tree = makeTTree()
-
-    # Define 2nd observable y
-    y = ROOT.RooRealVar("y", "y", -10, 10)
-
-    # Construct unbinned dataset importing tree branches x and y matching between branches and ROOT.RooRealVars
-    # is done by name of the branch/RRV
-    #
-    # Note that ONLY entries for which x,y have values within their allowed ranges as defined in
-    # ROOT.RooRealVar x and y are imported. Since the y values in the import tree are in the range [-15,15]
-    # and RRV y defines a range [-10,10] this means that the ROOT.RooDataSet
-    # below will have less entries than the ROOT.TTree 'tree'
-
-    ds = ROOT.RooDataSet("ds", "ds", ROOT.RooArgSet(x, y),
-                         ROOT.RooFit.Import(tree))
-
-    # P l o t   d a t a s e t   w i t h   m u l t i p l e   b i n n i n g   c h o i c e s
-    # ------------------------------------------------------------------------------------
-
-    # Print number of events in dataset
-    ds.Print()
-
-    # Print unbinned dataset with default frame binning (100 bins)
-    frame3 = y.frame(ROOT.RooFit.Title(
-        "Unbinned data shown in default frame binning"))
-    ds.plotOn(frame3)
-
-    # Print unbinned dataset with custom binning choice (20 bins)
-    frame4 = y.frame(ROOT.RooFit.Title(
-        "Unbinned data shown with custom binning"))
-    ds.plotOn(frame4, ROOT.RooFit.Binning(20))
-
-    # Draw all frames on a canvas
-    c = ROOT.TCanvas("rf102_dataimport", "rf102_dataimport", 800, 800)
-    c.Divide(2, 2)
-    c.cd(1)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame.GetYaxis().SetTitleOffset(1.4)
-    frame.Draw()
-    c.cd(2)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame2.GetYaxis().SetTitleOffset(1.4)
-    frame2.Draw()
-    c.cd(3)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame3.GetYaxis().SetTitleOffset(1.4)
-    frame3.Draw()
-    c.cd(4)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame4.GetYaxis().SetTitleOffset(1.4)
-    frame4.Draw()
-
-    c.SaveAs("rf102_dataimport.png")
 
 
 def makeTH1():
@@ -156,5 +41,109 @@ def makeTTree():
     return tree
 
 
-if __name__ == "__main__":
-    rf102_dataimport()
+############################
+# Importing ROOT histograms
+############################
+# Import ROOT TH1 into a RooDataHist
+# ---------------------------------------------------------
+# Create a ROOT TH1 histogram
+hh = makeTH1()
+
+# Declare observable x
+x = ROOT.RooRealVar("x", "x", -10, 10)
+
+# Create a binned dataset that imports contents of ROOT.TH1 and associates
+# its contents to observable 'x'
+dh = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(x),
+                        ROOT.RooFit.Import(hh))
+
+# Plot and fit a RooDataHist
+# ---------------------------------------------------
+# Make plot of binned dataset showing Poisson error bars (ROOT.RooFit
+# default)
+frame = x.frame(ROOT.RooFit.Title(
+    "Imported ROOT.TH1 with Poisson error bars"))
+dh.plotOn(frame)
+
+# Fit a Gaussian p.d.f to the data
+mean = ROOT.RooRealVar("mean", "mean", 0, -10, 10)
+sigma = ROOT.RooRealVar("sigma", "sigma", 3, 0.1, 10)
+gauss = ROOT.RooGaussian("gauss", "gauss", x, mean, sigma)
+gauss.fitTo(dh)
+gauss.plotOn(frame)
+
+# Plot and fit a RooDataHist with internal errors
+# ---------------------------------------------------------------------------------------------
+
+# If histogram has custom error (i.e. its contents is does not originate from a Poisson process
+# but e.g. is a sum of weighted events) you can data with symmetric 'sum-of-weights' error instead
+# (same error bars as shown by ROOT)
+frame2 = x.frame(ROOT.RooFit.Title(
+    "Imported ROOT.TH1 with internal errors"))
+dh.plotOn(frame2, ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2))
+gauss.plotOn(frame2)
+
+# Please note that error bars shown (Poisson or SumW2) are for visualization only, the are NOT used
+# in a maximum likelihood fit
+#
+# A (binned) ML fit will ALWAYS assume the Poisson error interpretation of data (the mathematical definition
+# of likelihood does not take any external definition of errors). Data with non-unit weights can only be correctly
+# fitted with a chi^2 fit (see rf602_chi2fit.C)
+########################
+# Importing ROOT TTrees#
+########################
+# Import ROOT TTree into a RooDataSet
+# -----------------------------------------------------------
+
+tree = makeTTree()
+
+# Define 2nd observable y
+y = ROOT.RooRealVar("y", "y", -10, 10)
+
+# Construct unbinned dataset importing tree branches x and y matching between branches and ROOT.RooRealVars
+# is done by name of the branch/RRV
+#
+# Note that ONLY entries for which x,y have values within their allowed ranges as defined in
+# ROOT.RooRealVar x and y are imported. Since the y values in the import tree are in the range [-15,15]
+# and RRV y defines a range [-10,10] this means that the ROOT.RooDataSet
+# below will have less entries than the ROOT.TTree 'tree'
+
+ds = ROOT.RooDataSet("ds", "ds", ROOT.RooArgSet(x, y),
+                        ROOT.RooFit.Import(tree))
+
+#Plot data set with multiple binning choices
+# ------------------------------------------------------------------------------------
+# Print number of events in dataset
+ds.Print()
+
+# Print unbinned dataset with default frame binning (100 bins)
+frame3 = y.frame(ROOT.RooFit.Title(
+    "Unbinned data shown in default frame binning"))
+ds.plotOn(frame3)
+
+# Print unbinned dataset with custom binning choice (20 bins)
+frame4 = y.frame(ROOT.RooFit.Title(
+    "Unbinned data shown with custom binning"))
+ds.plotOn(frame4, ROOT.RooFit.Binning(20))
+
+# Draw all frames on a canvas
+c = ROOT.TCanvas("rf102_dataimport", "rf102_dataimport", 800, 800)
+c.Divide(2, 2)
+c.cd(1)
+ROOT.gPad.SetLeftMargin(0.15)
+frame.GetYaxis().SetTitleOffset(1.4)
+frame.Draw()
+c.cd(2)
+ROOT.gPad.SetLeftMargin(0.15)
+frame2.GetYaxis().SetTitleOffset(1.4)
+frame2.Draw()
+c.cd(3)
+ROOT.gPad.SetLeftMargin(0.15)
+frame3.GetYaxis().SetTitleOffset(1.4)
+frame3.Draw()
+c.cd(4)
+ROOT.gPad.SetLeftMargin(0.15)
+frame4.GetYaxis().SetTitleOffset(1.4)
+frame4.Draw()
+
+c.SaveAs("rf102_dataimport.png")
