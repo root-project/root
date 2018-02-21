08ad983ef9a6caa7ac903200b721142a56d9fbe1
Philippe Canal <pcanal@fnal.gov>
2018-02-21 15:34:08 -0600

    [NFC] White space.

--- tree/tree/inc/TTreeCache.h
+++ tree/tree/inc/TTreeCache.h
@@ -38,39 +38,39 @@ public:
    enum EPrefillType { kNoPrefill, kAllBranches };
 
 protected:
-   Long64_t fEntryMin{0};      ///<! first entry in the cache
-   Long64_t fEntryMax{1};      ///<! last entry in the cache
-   Long64_t fEntryCurrent{-1}; ///<! current lowest entry number in the cache
-   Long64_t fEntryNext{-1};    ///<! next entry number where cache must be filled
-   Int_t fNbranches{0};        ///<! Number of branches in the cache
-   Int_t fNReadOk{0};          ///<  Number of blocks read and found in the cache
-   Int_t fNMissReadOk{0};      ///<  Number of blocks read, not found in the primary cache, and found in the secondary cache.
-   Int_t fNReadMiss{0};        ///<  Number of blocks read and not found in the cache
-   Int_t fNMissReadMiss{0};          ///<  Number of blocks read and not found in either cache.
-   Int_t fNReadPref{0};              ///<  Number of blocks that were prefetched
-   Int_t fNMissReadPref{0};          ///<  Number of blocks read into the secondary ("miss") cache.
-   TObjArray *fBranches{nullptr};    ///<! List of branches to be stored in the cache
-   TList *fBrNames{nullptr};         ///<! list of branch names in the cache
-   TTree *fTree{nullptr};            ///<! pointer to the current Tree
-   Bool_t fIsLearning{kTRUE};        ///<! true if cache is in learning mode
-   Bool_t fIsManual{kFALSE};         ///<! true if cache is StopLearningPhase was used
-   Bool_t fFirstBuffer{kTRUE};       ///<! true if first buffer is used for prefetching
-   Bool_t fOneTime{kFALSE};          ///<! used in the learning phase
-   Bool_t fReverseRead{kFALSE};      ///<! reading in reverse mode
-   Int_t fFillTimes{0};              ///<! how many times we can fill the current buffer
-   Bool_t fFirstTime{kTRUE};         ///<! save the fact that we processes the first entry
-   Long64_t fFirstEntry{-1};         ///<! save the value of the first entry
-   Bool_t fReadDirectionSet{kFALSE}; ///<! read direction established
-   Bool_t fEnabled{kTRUE};           ///<! cache enabled for cached reading
-   EPrefillType fPrefillType;        ///<  Whether a pre-filling is enabled (and if applicable which type)
-   static Int_t fgLearnEntries;      ///<  number of entries used for learning mode
-   Bool_t fAutoCreated{kFALSE};      ///<! true if cache was automatically created
+   Long64_t     fEntryMin{0};         ///<! first entry in the cache
+   Long64_t     fEntryMax{1};         ///<! last entry in the cache
+   Long64_t     fEntryCurrent{-1};    ///<! current lowest entry number in the cache
+   Long64_t     fEntryNext{-1};       ///<! next entry number where cache must be filled
+   Int_t        fNbranches{0};        ///<! Number of branches in the cache
+   Int_t        fNReadOk{0};          ///<  Number of blocks read and found in the cache
+   Int_t        fNMissReadOk{0};      ///<  Number of blocks read, not found in the primary cache, and found in the secondary cache.
+   Int_t        fNReadMiss{0};        ///<  Number of blocks read and not found in the cache
+   Int_t        fNMissReadMiss{0};    ///<  Number of blocks read and not found in either cache.
+   Int_t        fNReadPref{0};        ///<  Number of blocks that were prefetched
+   Int_t        fNMissReadPref{0};    ///<  Number of blocks read into the secondary ("miss") cache.
+   TObjArray   *fBranches{nullptr};   ///<! List of branches to be stored in the cache
+   TList       *fBrNames{nullptr};    ///<! list of branch names in the cache
+   TTree       *fTree{nullptr};       ///<! pointer to the current Tree
+   Bool_t       fIsLearning{kTRUE};   ///<! true if cache is in learning mode
+   Bool_t       fIsManual{kFALSE};    ///<! true if cache is StopLearningPhase was used
+   Bool_t       fFirstBuffer{kTRUE};  ///<! true if first buffer is used for prefetching
+   Bool_t       fOneTime{kFALSE};     ///<! used in the learning phase
+   Bool_t       fReverseRead{kFALSE}; ///<! reading in reverse mode
+   Int_t        fFillTimes{0};        ///<! how many times we can fill the current buffer
+   Bool_t       fFirstTime{kTRUE};    ///<! save the fact that we processes the first entry
+   Long64_t     fFirstEntry{-1};      ///<! save the value of the first entry
+   Bool_t       fReadDirectionSet{kFALSE}; ///<! read direction established
+   Bool_t       fEnabled{kTRUE};      ///<! cache enabled for cached reading
+   EPrefillType fPrefillType;         ///<  Whether a pre-filling is enabled (and if applicable which type)
+   static Int_t fgLearnEntries;       ///<  number of entries used for learning mode
+   Bool_t       fAutoCreated{kFALSE}; ///<! true if cache was automatically created
 
    // These members hold cached data for missed branches when miss optimization
    // is enabled.  Pointers are only initialized if the miss cache is enabled.
-   Bool_t fOptimizeMisses{kFALSE}; //! true if we should optimize cache misses.
-   Long64_t fFirstMiss{-1};        //! set to the event # of the first miss.
-   Long64_t fLastMiss{-1};         //! set to the event # of the last miss.
+   Bool_t   fOptimizeMisses{kFALSE}; ///<! true if we should optimize cache misses.
+   Long64_t fFirstMiss{-1};          ///<! set to the event # of the first miss.
+   Long64_t fLastMiss{-1};           ///<! set to the event # of the last miss.
 
    // Representation of a positioned buffer IO.
    // {0,0} designates the uninitialized - or invalid - buffer.
@@ -120,7 +120,7 @@ private:
    Bool_t CalculateMissCache();    // Calculate the appropriate miss cache to fetch; helper function for FillMissCache
    IOPos FindBranchBasketPos(TBranch &, Long64_t entry); // Given a branch and an entry, determine the file location
                                                          // (offset / size) of the corresponding basket.
-   TBranch *CalculateMissEntries(Long64_t, int, bool); // Given an file read, try to determine the corresponding branch.
+   TBranch *CalculateMissEntries(Long64_t, int, bool);   // Given an file read, try to determine the corresponding branch.
    Bool_t
    ProcessMiss(Long64_t pos, int len); // Given a file read not in the miss cache, handle (possibly) loading the data.
 
