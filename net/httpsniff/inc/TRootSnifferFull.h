f81fe4cd33ec04b9fb7b4470d6fcd484ee0fb860
Sergey Linev <S.Linev@gsi.de>
2018-02-26 09:15:05 +0100

    http: move streamer info and mem file into full version of TRootSniffer

--- net/httpsniff/inc/TRootSnifferFull.h
+++ net/httpsniff/inc/TRootSnifferFull.h
@@ -14,17 +14,38 @@
 
 #include "TRootSniffer.h"
 
+class TMemFile;
+
 class TRootSnifferFull : public TRootSniffer {
 protected:
+   TMemFile *fMemFile{nullptr}; ///<! file used to manage streamer infos
+   TList *fSinfo{nullptr};      ///<! last produced streamer info
+
    virtual void ScanObjectProperties(TRootSnifferScanRec &rec, TObject *obj);
 
    virtual void ScanKeyProperties(TRootSnifferScanRec &rec, TKey *key, TObject *&obj, TClass *&obj_class);
 
    virtual void ScanObjectChilds(TRootSnifferScanRec &rec, TObject *obj);
 
+   void CreateMemFile();
+
+   virtual Bool_t CanDrawClass(TClass *);
+
+   virtual Bool_t HasStreamerInfo() const { return kTRUE; }
+
 public:
    TRootSnifferFull(const char *name, const char *objpath = "Objects");
    virtual ~TRootSnifferFull();
+
+   virtual Bool_t IsStreamerInfoItem(const char *itemname);
+
+   virtual ULong_t GetStreamerInfoHash();
+
+   virtual ULong_t GetItemHash(const char *itemname);
+
+   virtual void *FindInHierarchy(const char *path, TClass **cl = nullptr, TDataMember **member = nullptr, Int_t *chld = nullptr);
+
+   virtual Bool_t ProduceBinary(const char *path, const char *options, void *&ptr, Long_t &length);
 
    virtual Bool_t ProduceImage(Int_t kind, const char *path, const char *options, void *&ptr, Long_t &length);
 
