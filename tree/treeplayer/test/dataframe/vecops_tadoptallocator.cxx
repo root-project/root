f29c4b5136a0f3a7053aa8c1d02710af8b096e12
Danilo Piparo <danilo.piparo@cern.ch>
2018-02-27 13:30:11 +0100

    [VecOps] Increase coverage of TVec and TAdoptAllocator tests

--- tree/treeplayer/test/dataframe/vecops_tadoptallocator.cxx
+++ tree/treeplayer/test/dataframe/vecops_tadoptallocator.cxx
@@ -12,11 +12,11 @@ TEST(TAdoptAllocator, ReusePointer)
    std::vector<double> vreference{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
    std::vector<double> vmodel(vreference);
-   TAdoptAllocator<double> alloc0(vmodel.data(), vmodel.size());
+   TAdoptAllocator<double> alloc0(vmodel.data());
 
    EXPECT_EQ(vmodel.data(), alloc0.allocate(123));
 
-   TAdoptAllocator<double> alloc1(vmodel.data(), vmodel.size());
+   TAdoptAllocator<double> alloc1(vmodel.data());
 
    std::vector<double, TAdoptAllocator<double>> v(alloc1);
    v.resize(vmodel.size());
@@ -66,10 +66,36 @@ TEST(TAdoptAllocator, NewAllocations)
    EXPECT_EQ(0U, copyCount);
 
    unsigned int dummy;
-   TAdoptAllocator<TCopySignal> alloc(model.data(), model.size());
+   TAdoptAllocator<TCopySignal> alloc(model.data());
    TVec<TCopySignal>::Impl_t v(model.size(), dummy, alloc);
 
    EXPECT_EQ(0U, copyCount);
    v.emplace_back(copyCount);
    EXPECT_EQ(8U, copyCount);
 }
+
+void CheckMove(std::vector<double, TAdoptAllocator<double>>&& vm, const std::vector<double, TAdoptAllocator<double>>& v) {
+   EXPECT_TRUE(vm.get_allocator() == v.get_allocator());
+}
+
+TEST(TAdoptAllocator, Traits)
+{
+   std::vector<double> vmodel {1.,2.,3.};
+   TAdoptAllocator<double> alloc0(vmodel.data());
+   TAdoptAllocator<double> alloc1(vmodel.data());
+   TVec<double>::Impl_t v0(3, 0., alloc0);
+   TVec<double>::Impl_t v1(3, 0., alloc1);
+
+   EXPECT_TRUE(v0.get_allocator() == v1.get_allocator()) << "Baseline test failed";
+
+   TVec<double>::Impl_t v2;
+   v2 = v0;
+   EXPECT_FALSE(v0.get_allocator() == v2.get_allocator());
+
+   swap(v1,v2);
+   EXPECT_TRUE(v0.get_allocator() == v2.get_allocator());
+
+   CheckMove(std::move(v0),v2);
+
+}
+
