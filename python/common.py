a79cb1acc95979b9c040cfed1a8a5193e9b7bafe
Guilherme Amadio <amadio@cern.ch>
2018-03-04 21:41:33 +0100

    Revert "Use system version of pytest if available"
    This reverts commit a4d0f8d00b45d15f91531f1da7723b10f7868cd1.
    Python3 externals need to be updated before this can be applied.

--- python/common.py
+++ python/common.py
@@ -10,13 +10,11 @@ import os, sys, unittest, warnings
 # avoid conflict wuth the local pytest and py modules and the one from LCG pytools
 warnings.filterwarnings('ignore', message=r'Module .*? is being added to sys\.path', append=True)
 
-try:
-   import pytest, pytest_cov
-except:
-   # pytest not found in the system, use local copy
-   toplevel = os.path.dirname(os.path.realpath(__file__))
-   sys.path.insert(0, os.path.join(toplevel, 'pytest'))
-   import pytest, pytest_cov
+
+# add local pytest dir to sys.path to make pytest available
+toplevel = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(toplevel, 'pytest'))
+
 
 if sys.hexversion >= 0x3000000:
    pylong = int
@@ -43,9 +41,18 @@ FIXCLING = '--fixcling' in sys.argv
 if 'FIXCLING' in os.environ:
    FIXCLING = os.environ['FIXCLING'] == 'yes'
 
+
 def run_pytest(test_file=None):
+    # add local pytest dir to sys.path
+    toplevel = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(0, os.path.join(toplevel, 'pytest'))
+    import pytest
+    import pytest_cov
+    # file to run, if any (search used otherwise)
     if '-i' in sys.argv:
         args = filter(lambda x: not x in (test_file, '-i'), sys.argv)
-    elif test_file:
-        args = test_file
+    else:
+        args = ['--color=no', '--result-log=stdout', '--minimal=yes']
+    if test_file: args += [test_file]
+    # actual test run
     return pytest.main(args, plugins=[pytest_cov])
