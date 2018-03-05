1ff6d4596f86dfafb01a517ae6a14e903c031dda
Danilo Piparo <danilo.piparo@cern.ch>
2018-03-05 15:42:19 +0100

    [TDF] Increase test coverage relative to reading of sub-branches

--- root/dataframe/test_read_leaves.cxx
+++ root/dataframe/test_read_leaves.cxx
@@ -1,13 +1,17 @@
 #include <ROOT/TDataFrame.hxx>
+#include <ROOT/TVec.hxx>
 #include <TTree.h>
 #include <TFile.h>
 
 #include "test_read_leaves.h"
 
 using namespace ROOT::Experimental;
+using namespace ROOT::Experimental::VecOps;
 
 int main()
 {
+   {
+   TFile f("test_read_leaves.root","RECREATE");
    TTree t("t", "t");
 
    V v{1, 2};
@@ -17,7 +21,12 @@ int main()
    W w;
    t.Branch("w", &w);
 
-   TDataFrame d(t);
-   d.Filter([] { return true; }, {"w.v.a", "w.v.b"}).Report();
+   t.Fill();
+   t.Write();
+   }
+
+   TDataFrame d("t", "test_read_leaves.root");
+   d.Filter([] (int, int) { return true; }, {"v.a", "v.b"}).Report();
    return 0;
 }
+
