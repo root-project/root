623b58d39c38006da41b46cb639c6a463a186b1b
Danilo Piparo <danilo.piparo@cern.ch>
2018-03-05 15:56:25 +0100

    [TDF][NFC] Formatting

--- root/dataframe/test_readTotemNtuple.C
+++ root/dataframe/test_readTotemNtuple.C
@@ -2,10 +2,8 @@ using namespace ROOT::Experimental;
 
 int test_readTotemNtuple() {
 
-   TDataFrame d("TotemNtuple", "Slimmed_TotemNTuple_9883.040.ntuple.root");
-   auto validate_x = [](double v){
-      std::cout << v << std::endl;
-   };
-   d.Foreach(validate_x, {"track_rp_3.x"});
-   return 0;
+  TDataFrame d("TotemNtuple", "Slimmed_TotemNTuple_9883.040.ntuple.root");
+  auto validate_x = [](double v) { std::cout << v << std::endl; };
+  d.Foreach(validate_x, {"track_rp_3.x"});
+  return 0;
 }
