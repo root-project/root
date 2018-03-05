7762c385ae55b9cecbfb9b63a64235fdb3662866
Danilo Piparo <danilo.piparo@cern.ch>
2018-03-05 17:31:10 +0100

    [TDF] More coverage

--- root/dataframe/test_read_leaves_nodict.cxx
+++ root/dataframe/test_read_leaves_nodict.cxx
@@ -9,21 +9,6 @@ using namespace ROOT::Experimental;
 
 int main()
 {
-   {
-   TFile f("test_read_leaves.root","RECREATE");
-   TTree t("t", "t");
-
-   V v{1, 2};
-   t.Branch("v", &v, "a/I:b/I");
-
-   gROOT->ProcessLine(".L test_read_leaves.h+");
-   W w;
-   t.Branch("w", &w);
-
-   t.Fill();
-   t.Write();
-   }
-
    TDataFrame d("t", "test_read_leaves.root");
    d.Filter([] (int, int) { return true; }, {"v.a", "v.b"}).Report();
    return 0;
