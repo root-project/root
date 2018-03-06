b43a54afb242699ae55937814d2b429bc2c6edee
Danilo Piparo <danilo.piparo@cern.ch>
2018-03-06 14:44:48 +0100

    [TDF] Test aliases of leaves used in jitted strings

--- root/dataframe/test_readTotemNtuple.C
+++ root/dataframe/test_readTotemNtuple.C
@@ -2,8 +2,15 @@ using namespace ROOT::Experimental;
 
 int test_readTotemNtuple() {
 
-  TDataFrame d("TotemNtuple", "Slimmed_TotemNTuple_9883.040.ntuple.root");
-  auto validate_x = [](double v) { std::cout << v << std::endl; };
-  d.Foreach(validate_x, {"track_rp_3.x"});
-  return 0;
+   TDataFrame d("TotemNtuple", "Slimmed_TotemNTuple_9883.040.ntuple.root");
+   auto validate_x = [](double v){
+      std::cout << v << std::endl;
+   };
+   d.Foreach(validate_x, {"track_rp_3.x"});
+
+   // now jitted
+   auto count = d.Alias("t_y", "track_rp_3.y").Filter("cout << t_y << endl; return true;").Count();
+   *count;
+
+   return 0;
 }
