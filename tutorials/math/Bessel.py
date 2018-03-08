68ea9fb1bcf7fd31b59831659a23d9dfacc0a2be
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2018-03-07 20:03:23 -0500

    Adjust to notebook format

--- tutorials/math/Bessel.py
+++ tutorials/math/Bessel.py
@@ -32,21 +32,25 @@ gPad.SetGrid()
 gPad.SetFrameFillColor(19)
 leg = TLegend(0.75, 0.7, 0.89, 0.89)
 
-n = 5 # number of functions in each pad
-# drawing the set of Bessel J functions
+# Drawing the set of Bessel J functions
+#
+# n is the number of functions in each pad
+n = 5
 JBessel = []
 for nu in range(n):
-   _jbessel = TF1("J_0", "ROOT::Math::cyl_bessel_j([0],x)", 0, 10)
-   _jbessel.SetParameters(nu, 0.0)
-   _jbessel.SetTitle("")
-   _jbessel.SetLineStyle(1)
-   _jbessel.SetLineWidth(3)
-   _jbessel.SetLineColor(nu + 1)
-   JBessel.append(_jbessel)
-#No se porque esta linea estaba asi: JBessel[0].TF1.GetXaxis().SetTitle("x")
-JBessel[0].GetXaxis().SetTitle("x")
-JBessel[0].GetXaxis().SetTitleSize(0.06)
-JBessel[0].GetXaxis().SetTitleOffset(.7)
+    _jbessel = TF1("J_0", "ROOT::Math::cyl_bessel_j([0],x)", 0, 10)
+    _jbessel.SetParameters(nu, 0.0)
+    _jbessel.SetTitle("")
+    _jbessel.SetLineStyle(1)
+    _jbessel.SetLineWidth(3)
+    _jbessel.SetLineColor(nu + 1)
+    JBessel.append(_jbessel)
+
+# Setting x axis for JBessel
+xaxis = JBessel[0].GetXaxis()
+xaxis.SetTitle("x")
+xaxis.SetTitleSize(0.06)
+xaxis.SetTitleOffset(.7)
 
 # setting the title in a label style
 p1 = TPaveLabel(.0, .90, .0 + .50, .90 + .10, "Bessel J functions", "NDC")
@@ -64,26 +68,26 @@ leg.AddEntry(JBessel[4].DrawCopy("same"), " J_4(x)", "l")
 leg.Draw()
 p1.Draw()
 
-# ------------------------------------------------
+# Set canvas 2
 DistCanvas.cd(2)
 gPad.SetGrid()
 gPad.SetFrameFillColor(19)
-
 leg2 = TLegend(0.75, 0.7, 0.89, 0.89)
-# ------------------------------------------------
+
 # Drawing Bessel k
 KBessel = []
 for nu in range(n):
-  _kbessel = TF1("J_0", "ROOT::Math::cyl_bessel_k([0],x)", 0, 10)
-  _kbessel.SetParameters(nu, 0.0)
-  _kbessel.SetTitle("Bessel K functions")
-  _kbessel.SetLineStyle(1)
-  _kbessel.SetLineWidth(3)
-  _kbessel.SetLineColor(nu+1)
-  KBessel.append(_kbessel)
-KBessel[0].GetXaxis().SetTitle("x")
-KBessel[0].GetXaxis().SetTitleSize(0.06)
-KBessel[0].GetXaxis().SetTitleOffset(.7)
+    _kbessel = TF1("J_0", "ROOT::Math::cyl_bessel_k([0],x)", 0, 10)
+    _kbessel.SetParameters(nu, 0.0)
+    _kbessel.SetTitle("Bessel K functions")
+    _kbessel.SetLineStyle(1)
+    _kbessel.SetLineWidth(3)
+    _kbessel.SetLineColor(nu+1)
+    KBessel.append(_kbessel)
+kxaxis = KBessel[0].GetXaxis()
+kxaxis.SetTitle("x")
+kxaxis.SetTitleSize(0.06)
+kxaxis.SetTitleOffset(.7)
 
 # setting title
 p2 = TPaveLabel(.0, .90, .0 + .50, .90 + .10, "Bessel K functions", "NDC")
@@ -99,26 +103,28 @@ leg2.AddEntry(KBessel[3].DrawCopy("same"), " K_3(x)", "l")
 leg2.AddEntry(KBessel[4].DrawCopy("same"), " K_4(x)", "l")
 leg2.Draw()
 p2.Draw()
-# ------------------------------------------------
+
+# Set canvas 3
 DistCanvas.cd(3)
 gPad.SetGrid()
 gPad.SetFrameFillColor(19)
 leg3 = TLegend(0.75, 0.7, 0.89, 0.89)
-# ------------------------------------------------
+
 # Drawing Bessel i
 iBessel = []
 for nu in range(5):
-  _ibessel= TF1("J_0", "ROOT::Math::cyl_bessel_i([0],x)", 0, 10)
-  _ibessel.SetParameters(nu, 0.0)
-  _ibessel.SetTitle("Bessel I functions")
-  _ibessel.SetLineStyle(1)
-  _ibessel.SetLineWidth(3)
-  _ibessel.SetLineColor(nu + 1)
-  iBessel.append(_ibessel)
+    _ibessel = TF1("J_0", "ROOT::Math::cyl_bessel_i([0],x)", 0, 10)
+    _ibessel.SetParameters(nu, 0.0)
+    _ibessel.SetTitle("Bessel I functions")
+    _ibessel.SetLineStyle(1)
+    _ibessel.SetLineWidth(3)
+    _ibessel.SetLineColor(nu + 1)
+    iBessel.append(_ibessel)
 
-iBessel[0].GetXaxis().SetTitle("x")
-iBessel[0].GetXaxis().SetTitleSize(0.06)
-iBessel[0].GetXaxis().SetTitleOffset(.7)
+iaxis = iBessel[0].GetXaxis()
+iaxis.SetTitle("x")
+iaxis.SetTitleSize(0.06)
+iaxis.SetTitleOffset(.7)
 
 # setting title
 p3 = TPaveLabel(.0, .90, .0 + .50, .90 + .10, "Bessel I functions", "NDC")
@@ -134,34 +140,35 @@ leg3.AddEntry(iBessel[3].DrawCopy("same"), " I_3(x)", "l")
 leg3.AddEntry(iBessel[4].DrawCopy("same"), " I_4(x)", "l")
 leg3.Draw()
 p3.Draw()
-# ------------------------------------------------
+
+# Set canvas 4
 DistCanvas.cd(4)
 gPad.SetGrid()
 gPad.SetFrameFillColor(19)
 leg4 = TLegend(0.75, 0.7, 0.89, 0.89)
-# ------------------------------------------------
+
 # Drawing sph_bessel
 jBessel = []
 for nu in range(5):
-  _jbessel = TF1("J_0", "ROOT::Math::sph_bessel([0],x)", 0, 10)
-  _jbessel.SetParameters(nu, 0.0)
-  _jbessel.SetTitle("Bessel j functions")
-  _jbessel.SetLineStyle(1)
-  _jbessel.SetLineWidth(3)
-  _jbessel.SetLineColor(nu+1)
-  jBessel.append(_jbessel)
-jBessel[0].GetXaxis().SetTitle("x")
-jBessel[0].GetXaxis().SetTitleSize(0.06)
-jBessel[0].GetXaxis().SetTitleOffset(.7)
+    _jbessel = TF1("J_0", "ROOT::Math::sph_bessel([0],x)", 0, 10)
+    _jbessel.SetParameters(nu, 0.0)
+    _jbessel.SetTitle("Bessel j functions")
+    _jbessel.SetLineStyle(1)
+    _jbessel.SetLineWidth(3)
+    _jbessel.SetLineColor(nu+1)
+    jBessel.append(_jbessel)
+jaxis = jBessel[0].GetXaxis()
+jaxis.SetTitle("x")
+jaxis.SetTitleSize(0.06)
+jaxis.SetTitleOffset(.7)
 
 # setting title
-p4 = TPaveLabel(.0, .90 , .0 + .50, .90 + .10, "Bessel j functions", "NDC")
+p4 = TPaveLabel(.0, .90, .0 + .50, .90 + .10, "Bessel j functions", "NDC")
 p4.SetFillColor(0)
 p4.SetTextFont(22)
 p4.SetTextColor(kBlack)
 
 # setting legend
-
 leg4.AddEntry(jBessel[0].DrawCopy(), " j_0(x)", "l")
 leg4.AddEntry(jBessel[1].DrawCopy("same"), " j_1(x)", "l")
 leg4.AddEntry(jBessel[2].DrawCopy("same"), " j_2(x)", "l")
