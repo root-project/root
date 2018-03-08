9cf35c045dc7af3d6e2c5f94fe5acb18ba17a88b
Bastian Beischer <beischer@physik.rwth-aachen.de>
2018-03-08 09:34:24 +0100

    Fix rootls with python3 (Fixes ROOT-9299).
    In python 3 'term_width / min_element_width' is a float and can't be passed to
    range a few lines below.

--- main/python/cmdLineUtils.py
+++ main/python/cmdLineUtils.py
@@ -1081,7 +1081,7 @@ def _rootLsPrintSimpleLs(keyList,indent,oneColumn):
     if max_element_width >= term_width: ncol,col_widths = 1,[1]
     else:
         # Start with max possible number of columns and reduce until it fits
-        ncol = 1 if oneColumn else min( len(keyList), term_width / min_element_width  )
+        ncol = 1 if oneColumn else min( len(keyList), term_width // min_element_width  )
         while True:
             col_widths = \
                 [ max( len(key.GetName()) + min_chars_between \
