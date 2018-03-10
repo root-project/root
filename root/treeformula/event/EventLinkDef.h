d61bf3fc517969fc4442b453bbb2b561b7431a27
Oksana Shadura <oksana.shadura@cern.ch>
2018-03-10 19:57:47 +0100

    Cleaning up not needed rules for generation of dictionaries, thanks for advice to D.Piparo

--- root/treeformula/event/EventLinkDef.h
+++ root/treeformula/event/EventLinkDef.h
@@ -11,17 +11,4 @@
 #pragma link C++ class vector<EventHeader>;
 #pragma link C++ class vector<Track*>;
 
-#ifdef __INTEL_COMPILER
-#pragma link C++ typedef vector<EventHeader>::iterator;
-#pragma link C++ typedef vector<Track*>::iterator;
-#elseif _WIN32
-// Intentionally empty because of vc8 (we do not compile the dictionary in
-// this dictionary with the 'right' switches.
-#else
-#pragma link C++ class vector<EventHeader>::iterator-;
-#pragma link C++ class vector<Track*>::iterator-;
-#pragma link C++ function operator!=(vector<EventHeader>::iterator,vector<EventHeader>::iterator);
-#pragma link C++ function operator!=(vector<Track*>::iterator,vector<Track*>::iterator);
-#endif
-
 #endif
