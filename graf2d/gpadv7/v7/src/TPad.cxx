913311e064065a2f748830481dc867e8251523e1
Sergey Linev <S.Linev@gsi.de>
2018-03-12 16:53:21 +0100

    webgui: add pad drawing opts to display item
    Should include different draw options for the pad itself

--- graf2d/gpadv7/v7/src/TPad.cxx
+++ graf2d/gpadv7/v7/src/TPad.cxx
@@ -109,5 +109,7 @@ void ROOT::Experimental::TPadDrawable::Paint(Internal::TPadPainter &toppad)
 
    painter.PaintDrawables(*fPad.get());
 
+   painter.fPadDisplayItem->SetDrawOpts(&GetOptions());
+
    toppad.AddDisplayItem(std::move(painter.fPadDisplayItem));
 }
