975cd5171733c685d99292cd8e2344f4711e4056
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-03-13 12:43:22 +0100

    [TDF] Avoid dependency on order of test execution
    test_read_leaves_nodict now creates its own file rather than
    re-using the one produced by test_read_leaves.

--- root/dataframe/test_read_leaves_nodict.cxx
+++ root/dataframe/test_read_leaves_nodict.cxx
@@ -9,7 +9,22 @@ using namespace ROOT::Experimental;
 
 int main()
 {
-   TDataFrame d("t", "test_read_leaves.root");
+   {
+      TFile f("test_read_leaves_nodict.root", "RECREATE");
+      TTree t("t", "t");
+
+      V v{1, 2};
+      t.Branch("v", &v, "a/I:b/I");
+
+      gROOT->ProcessLine(".L test_read_leaves.h+");
+      W w;
+      t.Branch("w", &w);
+
+      t.Fill();
+      t.Write();
+   }
+
+   TDataFrame d("t", "test_read_leaves_nodict.root");
    d.Filter(check_a_b, {"v.a", "v.b"}).Report();
    return 0;
 }
