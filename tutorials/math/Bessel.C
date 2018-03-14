351a7eb32bf9660045b27eb57c47180f13ba5c5e
Danilo Piparo <danilo.piparo@cern.ch>
2018-03-14 14:59:27 +0100

    Revert "[Math] Reduce to the minimum the usage of R__LOAD_LIBRARY in tutorials"
    This reverts commit bb8e0a6a682b256e328dd50fbab7aa9cb53fa619.
    This change is platform dependent! Everything works on ubuntu16, but fails
    on other platform. The change is reverted for 6.13, waiting to understand more
    about this difference.

--- tutorials/math/Bessel.C
+++ tutorials/math/Bessel.C
@@ -33,6 +33,8 @@
 
 void Bessel()
 {
+   R__LOAD_LIBRARY(libMathMore);
+
    TCanvas *DistCanvas = new TCanvas("DistCanvas", "Bessel functions example", 10, 10, 800, 600);
    DistCanvas->SetFillColor(17);
    DistCanvas->Divide(2, 2);
--- tutorials/math/LegendreAssoc.C
+++ tutorials/math/LegendreAssoc.C
@@ -34,6 +34,8 @@
 
 void LegendreAssoc()
 {
+   R__LOAD_LIBRARY(libMathMore);
+
    std::cout <<"Drawing associate Legendre Polynomials.." << std::endl;
    TCanvas *Canvas = new TCanvas("DistCanvas", "Associate Legendre polynomials", 10, 10, 800, 500);
    Canvas->Divide(2,1);
