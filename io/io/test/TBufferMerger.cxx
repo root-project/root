9d226a34507a33407efe47a309bbea9708ed6203
Guilherme Amadio <amadio@cern.ch>
2018-03-14 16:17:31 +0100

    Count tasks before launching them in case they are chained
    If a task triggers another task, and we don't count them before
    launching them, they may never be counted, which will result in
    an infinite loop. This only happens in single-threaded mode, that
    is, when TTaskGroup just calls the function instead of using TBB.

--- io/io/test/TBufferMerger.cxx
+++ io/io/test/TBufferMerger.cxx
@@ -341,12 +341,12 @@ TEST(TBufferMerger, RegisterCallbackTasks)
 
       TTaskGroup tg;
 
-      /* callback: launches new tasks when called */
+      /* callback: launch up to two new tasks when called */
       merger.RegisterCallback([&]() {
          int i = 0;
          while (launched < tasks && ++i <= 2) {
-            tg.Run(task);
             ++launched;
+            tg.Run(task);
          }
       });
 
