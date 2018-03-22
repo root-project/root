b45990c78d7379cbd9e9704fb5aa960cd69eab0b
Oksana Shadura <oksana.shadura@cern.ch>
2018-03-22 14:07:54 +0100

    Adding CMake macro checking version of binutils for correct handling of zlib compilation on SL6

--- builtins/zlib/adler32.c
+++ builtins/zlib/adler32.c
@@ -265,7 +265,7 @@ uLong ZEXPORT adler32_sse42(uLong adler, const Bytef *buf, uInt len)
     /* return recombined sums */
     return adler | (sum2 << 16);
 }
-
+#ifndef ROOT_NO_AVX2
 /* ========================================================================= */
 __attribute__ ((target ("avx2")))
 uLong ZEXPORT adler32_avx2(uLong adler, const Bytef *buf, uInt len)
@@ -364,33 +364,33 @@ uLong ZEXPORT adler32_avx2(uLong adler, const Bytef *buf, uInt len)
     /* return recombined sums */
     return adler | (sum2 << 16);
 }
+#endif
 
 void *resolve_adler32(void)
 {
   unsigned int eax, ebx, ecx, edx;
 	signed char has_sse42 = 0;
-	signed char has_avx2 = 0;
 
-	/* Collect CPU features */
+   /* Collect CPU features */
   if (!__get_cpuid (1, &eax, &ebx, &ecx, &edx))
     return adler32_default;
+
 	has_sse42 = ((ecx & bit_SSE4_2) != 0);
-#if defined(bit_AVX2)
-	if (__get_cpuid_max (0, NULL) < 7)
-		return adler32_default;
+  if (__get_cpuid_max (0, NULL) < 7)
+    return adler32_default;
+
 	__cpuid_count (7, 0, eax, ebx, ecx, edx);
-	has_avx2 = ((ebx & bit_AVX2) != 0);
-#endif /* defined(bit_AVX2) */
-
-	/* Pick AVX2 version */
+  /* Pick AVX2 version only if as supports AVX2 (not the case of Haswell)*/
+#if !defined(ROOT_NO_AVX2)
+	signed char has_avx2 = 0;
+	has_avx2 = ((ebx & bit_AVX2) != 0);	 
 	if (has_avx2)
-		return adler32_avx2;
-
+	  return adler32_avx2;
+#endif
   /* Pick SSE4.2 version */
   if (has_sse42)
     return adler32_sse42;
-
-	/* Fallback to default implementation */
+  /* Fallback to default implementation */
   return adler32_default;
 }
 
