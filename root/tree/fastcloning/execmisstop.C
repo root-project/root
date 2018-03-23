89ec4fb87221018f664b9b444ab1c2e6642f5189
Philippe Canal <pcanal@fnal.gov>
2018-03-23 15:06:09 -0500

    Use TMemFile rather unused physical file

--- root/tree/fastcloning/execmisstop.C
+++ root/tree/fastcloning/execmisstop.C
@@ -1,7 +1,7 @@
 {
    int i;
    TTree *in = new TTree("in",""); in->Branch("i",&i);
-   new TFile("execmisstop.root","RECREATE"); // Open a 'writeable' file.
+   new TMemFile("execmisstop.root","RECREATE"); // Open a 'writeable' file.
    TTree *out = in->CloneTree(-1); out->Branch("j",&i);
    TTreeCloner cloner_warn(in,out,"");
    TTreeCloner cloner_quiet(in,out,"",TTreeCloner::kIgnoreMissingTopLevel);
