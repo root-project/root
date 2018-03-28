a8b4ecac4cfada24cd412ff5bb5e3e7b7a925747
Philippe Canal <pcanal@fnal.gov>
2018-03-28 15:03:08 -0500

    Restore TTree::Draw ability to use function return a reference to object.
    For example std::vector::at

--- tree/treeplayer/inc/TFormLeafInfo.h
+++ tree/treeplayer/inc/TFormLeafInfo.h
@@ -367,6 +367,7 @@ class TFormLeafInfoMethod : public TFormLeafInfo {
    Bool_t       fIsByValue;
 
 public:
+   static TClass *ReturnTClass(TMethodCall *mc);
 
    TFormLeafInfoMethod(TClass* classptr = 0, TMethodCall *method = 0);
    TFormLeafInfoMethod(const TFormLeafInfoMethod& orig);
