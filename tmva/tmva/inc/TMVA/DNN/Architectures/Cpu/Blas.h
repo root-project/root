582583e5dfabed62e0e695e7bc74a63ec8f1773d
moneta <lorenzo.moneta@cern.ch>
2018-04-06 17:09:34 +0200

    Add possibility to use gslcblas when Blas is not found

--- tmva/tmva/inc/TMVA/DNN/Architectures/Cpu/Blas.h
+++ tmva/tmva/inc/TMVA/DNN/Architectures/Cpu/Blas.h
@@ -20,6 +20,7 @@
 
 #include <iostream>
 
+#ifndef DNN_USE_CBLAS 
 // External Library Routines
 //____________________________________________________________________________
 extern "C" void saxpy_(const int * n, const float * alpha, const float * x,
@@ -52,6 +53,10 @@ extern "C" void sgemm_(const char * transa, const char * transb,
                        const float * alpha, const float * A, const int * lda,
                        const float * B, const int * ldb, const float * beta,
                        float * C, const int * ldc);
+
+#else
+#include "gsl/gsl_cblas.h"
+#endif
 
 namespace TMVA
 {
@@ -92,12 +97,18 @@ inline void Ger(const int * m, const int * n, const Real_t * alpha,
 
 // Specializations
 //____________________________________________________________________________
+#ifndef DNN_USE_CBLAS
+   
 template<>
 inline void Axpy<double>(const int * n, const double * alpha,
                          const double * x, const int * incx,
                          double * y, const int * incy)
 {
-    daxpy_(n, alpha, x, incx, y, incy);
+#ifdef DNN_USE_CBLAS
+   cblas_daxpy(*n, *alpha, x, *incx, y, *incy);
+#else
+   daxpy_(n, alpha, x, incx, y, incy);
+#endif
 }
 
 template<>
@@ -164,6 +175,90 @@ inline void Ger<float>(const int * m, const int * n, const float * alpha,
    sger_(m, n, alpha, x, incx, y, incy, A, lda);
 }
 
+#else
+//--------------------------------------------------------
+// cblas implementation
+//-----------------------------------------------------------
+template<>
+inline void Axpy<double>(const int * n, const double * alpha,
+                         const double * x, const int * incx,
+                         double * y, const int * incy)
+{
+   cblas_daxpy(*n, *alpha, x, *incx, y, *incy);
+}
+
+template<>
+inline void Axpy<float>(const int * n, const float * alpha,
+                        const float * x, const int * incx,
+                        float * y, const int * incy)
+{
+   cblas_saxpy(*n, *alpha, x, *incx, y, *incy);
+}
+
+template<>
+inline void Gemv<double>(const char *trans, const int * m, const int * n,
+                         const double * alpha, const double * A, const int * lda,
+                         const double * x, const int * incx,
+                         const double * beta, double * y, const int * incy)
+{
+   CBLAS_TRANSPOSE kTrans = (*trans == 'T') ? CblasTrans : CblasNoTrans;
+   cblas_dgemv(CblasColMajor, kTrans, *m, *n, *alpha, A, *lda, x, *incx, *beta, y, *incy);
+}
+
+template<>
+inline void Gemv<float>(const char *trans, const int * m, const int * n,
+                        const float * alpha, const float * A, const int * lda,
+                        const float * x, const int * incx,
+                        const float * beta, float * y, const int * incy)
+{
+   CBLAS_TRANSPOSE kTrans = (*trans == 'T') ? CblasTrans : CblasNoTrans;
+   cblas_sgemv(CblasColMajor, kTrans, *m, *n, *alpha, A, *lda, x, *incx, *beta, y, *incy);
+}
+
+template<>
+inline void Gemm<double>(const char *transa, const char *transb,
+                         const int * m, const int * n, const int* k,
+                         const double * alpha, const double * A, const int * lda,
+                         const double * B, const int * ldb, const double * beta,
+                         double * C, const int * ldc)
+{
+   CBLAS_TRANSPOSE kTransA = (*transa == 'T') ? CblasTrans : CblasNoTrans;
+   CBLAS_TRANSPOSE kTransB = (*transb == 'T') ? CblasTrans : CblasNoTrans;
+   cblas_dgemm(CblasColMajor, kTransA, kTransB, *m, *n, *k, *alpha, A, *lda, B, *ldb, *beta, C, *ldc);
+}
+
+template<>
+inline void Gemm<float>(const char *transa, const char *transb,
+                        const int * m, const int * n, const int* k,
+                        const float * alpha, const float * A, const int * lda,
+                        const float * B, const int * ldb, const float * beta,
+                        float * C, const int * ldc)
+{
+   CBLAS_TRANSPOSE kTransA = (*transa == 'T') ? CblasTrans : CblasNoTrans;
+   CBLAS_TRANSPOSE kTransB = (*transb == 'T') ? CblasTrans : CblasNoTrans;
+   cblas_sgemm(CblasColMajor, kTransA, kTransB, *m, *n, *k, *alpha, A, *lda, B, *ldb, *beta, C, *ldc);
+}
+
+template <>
+inline void Ger<double>(const int * m, const int * n, const double * alpha,
+                        const double * x, const int * incx,
+                        const double * y, const int * incy,
+                        double * A, const int * lda)
+{
+   cblas_dger(CblasColMajor, *m, *n, *alpha, x, *incx, y, *incy, A, *lda);
+}
+
+template <>
+inline void Ger<float>(const int * m, const int * n, const float * alpha,
+                       const float * x, const int * incx,
+                       const float * y, const int * incy,
+                       float * A, const int * lda)
+{
+   cblas_sger(CblasColMajor, *m, *n, *alpha, x, *incx, y, *incy, A, *lda);
+}
+
+#endif
+   
 } // namespace Blas
 } // namespace DNN
 } // namespace TMVA
