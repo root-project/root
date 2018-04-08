f4de203884fd89764a67ad0ac7f4a9578c9532d7
Danilo Piparo <danilo.piparo@cern.ch>
2018-04-08 22:50:10 +0200

    [VecOps] Refactor: move TVec to math/vecops and TAdoptAllocator to core/foundation

---
+++ math/vecops/inc/LinkDef.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Author: Danilo Piparo - CERN
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __ROOTCLING__
+
+// TVec related dictionaries
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<double>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<float>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<UInt_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<ULong_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<ULong64_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<UShort_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<UChar_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<Int_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<Long_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<Long64_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<Short_t>::Impl_t+;
+#pragma link C++ class ROOT::Experimental::VecOps::TVec<Char_t>::Impl_t+;
+
+#endif
