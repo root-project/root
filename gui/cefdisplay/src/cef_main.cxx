954b13d5ae11767cfb0b6ae16eadcc5e43445084
Sergey Linev <S.Linev@gsi.de>
2018-04-13 16:06:40 +0200

    webgui: register extension in CEF browser process
    Such extension allows to recognize CEF automatically from JavaScript

--- gui/cefdisplay/src/cef_main.cxx
+++ gui/cefdisplay/src/cef_main.cxx
@@ -12,130 +12,29 @@
 #include <unistd.h>
 #endif
 
+#include "include/cef_render_process_handler.h"
 #include "include/base/cef_logging.h"
-
-#include "include/wrapper/cef_message_router.h"
-
-// #include "include/cef_process_message.h"
-
-class ROOTV8Handler : public CefV8Handler {
-public:
-   ROOTV8Handler() {}
-
-   virtual bool Execute(const CefString &name, CefRefPtr<CefV8Value> object, const CefV8ValueList &arguments,
-                        CefRefPtr<CefV8Value> &retval, CefString &exception) OVERRIDE
-   {
-      if (name == "ROOT_BATCH_FUNC") {
-         // Return my string value.
-         retval = CefV8Value::CreateString("My Value!");
-         printf("CALLING ROOT_BATCH_FUNC\n");
-         return true;
-      }
-
-      // Function does not exist.
-      return false;
-   }
-
-   // Provide the reference counting implementation for this class.
-   IMPLEMENT_REFCOUNTING(ROOTV8Handler);
-};
 
 // Implement application-level callbacks for the browser process.
 class MyRendererProcessApp : public CefApp, public CefRenderProcessHandler {
-private:
-   // Handles the renderer side of query routing.
-   CefRefPtr<CefMessageRouterRendererSide> message_router_;
 
 public:
-   MyRendererProcessApp() : CefApp(), CefRenderProcessHandler(), message_router_(0) {}
+   MyRendererProcessApp() : CefApp(), CefRenderProcessHandler() {}
    virtual ~MyRendererProcessApp() {}
 
-   // CefApp methods:
-   virtual CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() OVERRIDE { return this; }
-
-   // CefRenderProcessHandler methods:
-   void OnWebKitInitialized() OVERRIDE
-   {
-      // Create the renderer-side router for query handling.
-      CefMessageRouterConfig config;
-      message_router_ = CefMessageRouterRendererSide::Create(config);
-   }
-
-   // CefRenderProcessHandler methods
-   virtual void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
-                                 CefRefPtr<CefV8Context> context) OVERRIDE
-   {
-      printf("MyRendererProcessApp::OnContextCreated\n");
-
-      message_router_->OnContextCreated(browser, frame, context);
-
-      /*
-            // Retrieve the context's window object.
-            CefRefPtr<CefV8Value> object = context->GetGlobal();
-
-            // Create a new V8 string value. See the "Basic JS Types" section below.
-            CefRefPtr<CefV8Value> str = CefV8Value::CreateString("My Value!");
-
-            // Add the string to the window object as "window.myval". See the "JS Objects" section below.
-            object->SetValue("ROOT_BATCH_FLAG", str, V8_PROPERTY_ATTRIBUTE_NONE);
-
-            printf("ADD BATCH FALG\n");
-
-            CefRefPtr<CefV8Handler> handler = new ROOTV8Handler;
-            CefRefPtr<CefV8Value> func = CefV8Value::CreateFunction("ROOT_BATCH_FUNC", handler);
-
-            // Add the string to the window object as "window.myval". See the "JS Objects" section below.
-            object->SetValue("ROOT_BATCH_FUNC", func, V8_PROPERTY_ATTRIBUTE_NONE);
-
-            printf("ADD BATCH FUNC\n");
-
-            CefRefPtr<CefProcessMessage> msg = CefProcessMessage::Create("my_message");
-
-            // Retrieve the argument list object.
-            CefRefPtr<CefListValue> args = msg->GetArgumentList();
-
-            // Populate the argument values.
-            args->SetString(0, "my string");
-            args->SetInt(1, 10);
-
-            // Send the process message to the render process.
-            // Use PID_BROWSER instead when sending a message to the browser process.
-            browser->SendProcessMessage(PID_BROWSER, msg);
-      */
-   }
-
-   void OnContextReleased(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
-                          CefRefPtr<CefV8Context> context) OVERRIDE
-   {
-      message_router_->OnContextReleased(browser, frame, context);
-   }
-
-   bool OnProcessMessageReceived(CefRefPtr<CefBrowser> browser, CefProcessId source_process,
-                                 CefRefPtr<CefProcessMessage> message) OVERRIDE
-   {
-      return message_router_->OnProcessMessageReceived(browser, source_process, message);
-   }
-
-   void OnUncaughtException(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context,
-                            CefRefPtr<CefV8Exception> exception, CefRefPtr<CefV8StackTrace> stackTrace) OVERRIDE
-   {
-      printf("!!!!!! OnUncaughtException !!!!!\n");
-   }
+   virtual CefRefPtr< CefRenderProcessHandler > GetRenderProcessHandler() { return this; }
 
    void OnRegisterCustomSchemes(CefRawPtr<CefSchemeRegistrar> registrar) OVERRIDE
    {
       // registrar->AddCustomScheme("rootscheme", true, true, true, true, true, true);
-      registrar->AddCustomScheme("rootscheme", true, false, false, true, false, false);
    }
 
-   /*   virtual bool OnProcessMessageReceived(CefRefPtr<CefBrowser> browser, CefProcessId source_process,
-                                            CefRefPtr<CefProcessMessage> message) OVERRIDE
-      {
-         const std::string &message_name = message->GetName();
-         printf("MyRendererProcessApp::OnProcessMessageReceived %s\n", message_name.c_str());
-         return true;
-      }
-      */
+   virtual void OnWebKitInitialized()
+   {
+      // Register the extension.
+      CefRegisterExtension("v8/jsroot", "var jsroot_cef3_identifier = true;", NULL);
+   }
+
 
 private:
    // Include the default reference counting implementation.
@@ -166,8 +65,6 @@ int main(int argc, char *argv[])
       // The sub-process has completed so return here.
       return exit_code;
    }
-
-   printf("do nothing\n");
 
    return 0;
 }
