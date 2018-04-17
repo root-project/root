58733827a202f187c4a7a030938b3361fa2e9933
Stefan Wunsch <stefan.wunsch@cern.ch>
2018-04-17 15:03:39 +0200

    [PyROOT] Add TTree.AsMatrix functionality

--- bindings/pyroot/src/Pythonize.cxx
+++ bindings/pyroot/src/Pythonize.cxx
@@ -2696,7 +2696,7 @@ Bool_t PyROOT::Pythonize( PyObject* pyclass, const std::string& name )
 
    }
 
-   else if (name.find("TVectorT") != std::string::npos) {
+   else if ( name.substr(0,8) == "TVectorT" ) {
       // allow proper iteration
       Utility::AddToClass( pyclass, "__len__", "GetNoElements" );
       Utility::AddToClass( pyclass, "_getitem__unchecked", "__getitem__" );
