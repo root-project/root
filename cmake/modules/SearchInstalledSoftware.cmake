root-project/root

commit ef5957270231f462423e26f71ab49c35c9f08ed3

Move OpenSSL to new builtin mechanism

cmake/modules/SearchInstalledSoftware.cmake
@@ -574,43 +574,28 @@ if(xml)
 endif()
 
 #---Check for OpenSSL------------------------------------------------------------------
-if(ssl OR builtin_openssl)
-  if(builtin_openssl)
-    set(OPENSSL_VERSION 1.0.2o)
-    message(STATUS "Downloading and building OpenSSL version ${OPENSSL_VERSION}")
-    if(APPLE)
-      set(openssl_config_cmd ./Configure darwin64-x86_64-cc)
-    else()
-      set(openssl_config_cmd ./config)
-    endif()
-    set(OPENSSL_LIBRARIES ${CMAKE_BINARY_DIR}/OPENSSL-prefix/lib/libssl.a ${CMAKE_BINARY_DIR}/OPENSSL-prefix/lib/libcrypto.a)
-    ExternalProject_Add(
-      OPENSSL
-      URL ${lcgpackages}/openssl-${OPENSSL_VERSION}.tar.gz
-      URL_HASH SHA256=ec3f5c9714ba0fd45cb4e087301eb1336c317e0d20b575a125050470e8089e4d
-      CONFIGURE_COMMAND ${openssl_config_cmd} no-shared --prefix=<INSTALL_DIR>
-      BUILD_COMMAND make -j1 CC=${CMAKE_C_COMPILER}\ -fPIC
-      INSTALL_COMMAND make install_sw
-      BUILD_IN_SOURCE 1
-      LOG_BUILD 1 LOG_CONFIGURE 1 LOG_DOWNLOAD 1 LOG_INSTALL 1
-      BUILD_BYPRODUCTS ${OPENSSL_LIBRARIES}
-    )
-    set(OPENSSL_INCLUDE_DIR ${CMAKE_BINARY_DIR}/OPENSSL-prefix/include)
-    set(OPENSSL_PREFIX ${CMAKE_BINARY_DIR}/OPENSSL-prefix)
-    set(OPENSSL_TARGET OPENSSL)
-    set(ssl ON CACHE BOOL "" FORCE)
+foreach(suffix FOUND INCLUDE_DIR INCLUDE_DIRS LIBRARY LIBRARIES VERSION)
+  unset(OPENSSL_${suffix} CACHE)
+endforeach()
+
+if(ssl AND NOT builtin_openssl)
+  if(fail-on-missing)
+    find_package(OpenSSL REQUIRED)
   else()
-    message(STATUS "Looking for OpenSSL")
     find_package(OpenSSL)
-    if(NOT OPENSSL_FOUND)
-      if(fail-on-missing)
-        message(FATAL_ERROR "OpenSSL libraries not found and they are required (ssl option enabled)")
-      else()
-        message(STATUS "OpenSSL not found. Switching off ssl option")
-        set(ssl OFF CACHE BOOL "" FORCE)
-      endif()
+    if(NOT OPENSSL_FOUND AND NOT WIN32) # builtin OpenSSL does not work on Windows
+      message(STATUS "OpenSSL not found, switching ON 'builtin_openssl' option.")
+      set(builtin_openssl ON CACHE BOOL "" FORCE)
+    else()
+      message(STATUS "Switching OFF 'ssl' option.")
+      set(ssl OFF CACHE BOOL "" FORCE)
     endif()
   endif()
+endif()
+
+if(builtin_openssl)
+  list(APPEND ROOT_BUILTINS OpenSSL)
+  add_subdirectory(builtins/openssl)
 endif()
 
 #---Check for Castor-------------------------------------------------------------------

builtins/openssl/CMakeLists.txt
@@ -1,0 +1,43 @@
+include(ExternalProject)
+
+set(OPENSSL_VERSION "1.0.2o")
+set(OPENSSL_URL "http://lcgpackages.web.cern.ch/lcgpackages/tarFiles/sources/openssl-${OPENSSL_VERSION}.tar.gz")
+set(OPENSSL_URLHASH "SHA256=ec3f5c9714ba0fd45cb4e087301eb1336c317e0d20b575a125050470e8089e4d")
+set(OPENSSL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/OPENSSL-prefix)
+
+foreach(lib ssl crypto)
+  set(libname ${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
+  list(APPEND OPENSSL_LIBRARIES ${OPENSSL_PREFIX}/lib/${libname})
+endforeach()
+
+if(APPLE)
+  set(OPENSSL_CONFIG_CMD ./Configure darwin64-x86_64-cc)
+else()
+  set(OPENSSL_CONFIG_CMD ./config)
+endif()
+
+ExternalProject_Add(OPENSSL
+  URL ${OPENSSL_URL} URL_HASH ${OPENSSL_URLHASH}
+  CONFIGURE_COMMAND ${OPENSSL_CONFIG_CMD} no-shared --prefix=<INSTALL_DIR>
+  BUILD_COMMAND make -j1 CC=${CMAKE_C_COMPILER}\ -fPIC
+  INSTALL_COMMAND make install_sw
+  BUILD_IN_SOURCE 1
+  LOG_BUILD 1 LOG_CONFIGURE 1 LOG_DOWNLOAD 1 LOG_INSTALL 1
+  BUILD_BYPRODUCTS ${OPENSSL_LIBRARIES})
+
+set(OPENSSL_VERSION ${OPENSSL_VERSION} CACHE INTERNAL "" FORCE)
+set(OPENSSL_PREFIX ${OPENSSL_PREFIX} CACHE INTERNAL "" FORCE) # needed by Davix
+set(OPENSSL_INCLUDE_DIR ${OPENSSL_PREFIX}/include CACHE INTERNAL "" FORCE)
+set(OPENSSL_INCLUDE_DIRS ${OPENSSL_PREFIX}/include CACHE INTERNAL "" FORCE)
+set(OPENSSL_LIBRARIES ${OPENSSL_LIBRARIES} CACHE INTERNAL "" FORCE)
+
+add_library(rssl INTERFACE)
+target_include_directories(rssl INTERFACE $<BUILD_INTERFACE:${OPENSSL_INCLUDE_DIR}>)
+target_link_libraries(rssl INTERFACE $<BUILD_INTERFACE:${OPENSSL_LIBRARIES}>)
+add_dependencies(rssl OPENSSL)
+
+if(NOT TARGET OpenSSL::SSL)
+  add_library(OpenSSL::SSL ALIAS rssl)
+endif()
+
+set_property(GLOBAL APPEND PROPERTY ROOT_BUILTIN_TARGETS OPENSSL)

commit 8d7e77d20d127f4afa464cdfdd92df22762dcad8

Do not disable ssl when OpenSSL is actually found
This is a bug that has been introduced by commit
ef5957270231f462423e26f71ab49c35c9f08ed3.

cmake/modules/SearchInstalledSoftware.cmake
@@ -583,12 +583,14 @@ if(ssl AND NOT builtin_openssl)
     find_package(OpenSSL REQUIRED)
   else()
     find_package(OpenSSL)
-    if(NOT OPENSSL_FOUND AND NOT WIN32) # builtin OpenSSL does not work on Windows
-      message(STATUS "OpenSSL not found, switching ON 'builtin_openssl' option.")
-      set(builtin_openssl ON CACHE BOOL "" FORCE)
-    else()
-      message(STATUS "Switching OFF 'ssl' option.")
-      set(ssl OFF CACHE BOOL "" FORCE)
+    if(NOT OPENSSL_FOUND)
+      if(WIN32) # builtin OpenSSL does not work on Windows
+        message(STATUS "Switching OFF 'ssl' option.")
+        set(ssl OFF CACHE BOOL "" FORCE)
+      else()
+        message(STATUS "OpenSSL not found, switching ON 'builtin_openssl' option.")
+        set(builtin_openssl ON CACHE BOOL "" FORCE)
+      endif()
     endif()
   endif()
 endif()