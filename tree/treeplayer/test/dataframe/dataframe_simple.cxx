005f1916b07601eb311622fd2845af25485a9b15
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-04-23 01:16:54 +0200

    [TDF] Add test for TInterface::Book

--- tree/treeplayer/test/dataframe/dataframe_simple.cxx
+++ tree/treeplayer/test/dataframe/dataframe_simple.cxx
@@ -486,6 +486,35 @@ TEST_P(TDFSimpleTests, AggregateGraph)
    }
 }
 
+class MaxSlotHelper : public ROOT::Detail::TDF::TActionImpl<MaxSlotHelper> {
+   const std::shared_ptr<unsigned int> fMaxSlot; // final result
+   std::vector<unsigned int> fMaxSlots;          // per-thread partial results
+public:
+   MaxSlotHelper(unsigned int nSlots)
+      : fMaxSlot(std::make_shared<unsigned int>(std::numeric_limits<unsigned int>::lowest())),
+        fMaxSlots(nSlots, std::numeric_limits<unsigned int>::lowest())
+   {
+   }
+   MaxSlotHelper(MaxSlotHelper &&) = default;
+   MaxSlotHelper(const MaxSlotHelper &) = delete;
+   using ColumnTypes_t = ROOT::TypeTraits::TypeList<unsigned int>;
+   using Result_t = unsigned int;
+   ROOT::Detail::TDF::ColumnNames_t GetColumnNames() const { return {"tdfslot_"}; }
+   std::shared_ptr<unsigned int> GetResultPtr() const { return fMaxSlot; }
+   void Initialize() {}
+   void InitTask(TTreeReader *, unsigned int) {}
+   void Exec(unsigned int slot, unsigned int /*slot2*/) { fMaxSlots[slot] = std::max(fMaxSlots[slot], slot); }
+   void Finalize() { *fMaxSlot = *std::max_element(fMaxSlots.begin(), fMaxSlots.end()); }
+};
+
+TEST_P(TDFSimpleTests, BookCustomAction)
+{
+   TDataFrame d(1);
+   const auto nWorkers = std::max(1u, ROOT::GetImplicitMTPoolSize());
+   auto maxSlot = d.Book(MaxSlotHelper(nWorkers));
+   EXPECT_EQ(*maxSlot, nWorkers-1);
+}
+
 // run single-thread tests
 INSTANTIATE_TEST_CASE_P(Seq, TDFSimpleTests, ::testing::Values(false));
 
