5fddae3c44d3b0b4664db91b8e844815be919e8b
Matevz Tadel <mtadel@ucsd.edu>
2018-04-24 10:07:40 -0700

    Add VSD and VSDStructs.

---
+++ graf3d/eve7/src/TEveVSDStructs.cxx
@@ -0,0 +1,18 @@
+// @(#)root/eve:$Id$
+// Authors: Matevz Tadel & Alja Mrak-Tadel: 2006, 2007
+
+/*************************************************************************
+ * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "ROOT/TEveVSDStructs.hxx"
+
+using namespace ROOT::Experimental;
+namespace REX = ROOT::Experimental;
+
+template class TEveRecTrackT<Float_t>;
+template class TEveRecTrackT<Double_t>;
