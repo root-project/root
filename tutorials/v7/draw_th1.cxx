7ffad568e34e14f56d1771b4a48ea4037afc6362
Olivier Couet <olivier.couet@gmail.com>
2018-04-25 10:25:06 +0200

    Update tutorial to generate the png image

--- tutorials/v7/draw_th1.cxx
+++ tutorials/v7/draw_th1.cxx
@@ -1,6 +1,10 @@
 /// \file
 /// \ingroup tutorial_v7
 ///
+/// This macro generates a small V7 TH1D, fills it and draw it in a V7 canvas.
+/// The canvas is display in the web browser and the corresponding png picture
+/// is generated.
+///
 /// \macro_code
 ///
 /// \date 2015-03-22
@@ -15,34 +19,35 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-R__LOAD_LIBRARY(libGpad);
+R__LOAD_LIBRARY(libROOTGpadv7);
 
 #include "ROOT/THist.hxx"
 #include "ROOT/TCanvas.hxx"
 
 void draw_th1() {
-  using namespace ROOT;
+   using namespace ROOT;
 
-  // Create the histogram.
-  Experimental::TAxisConfig xaxis(10, 0., 10.);
-  auto pHist = std::make_shared<Experimental::TH1D>(xaxis);
-  auto pHist2 = std::make_shared<Experimental::TH1D>(xaxis);
+   // Create the histogram.
+   Experimental::TAxisConfig xaxis(10, 0., 10.);
+   auto pHist = std::make_shared<Experimental::TH1D>(xaxis);
+   auto pHist2 = std::make_shared<Experimental::TH1D>(xaxis);
 
-  // Fill a few points.
-  pHist->Fill(1);
-  pHist->Fill(2);
-  pHist->Fill(2);
-  pHist->Fill(3);
+   // Fill a few points.
+   pHist->Fill(1);
+   pHist->Fill(2);
+   pHist->Fill(2);
+   pHist->Fill(3);
 
-  pHist2->Fill(5);
-  pHist2->Fill(6);
-  pHist2->Fill(6);
-  pHist2->Fill(7);
+   pHist2->Fill(5);
+   pHist2->Fill(6);
+   pHist2->Fill(6);
+   pHist2->Fill(7);
 
-  // Create a canvas to be displayed.
-  auto canvas = Experimental::TCanvas::Create("Canvas Title");
-  canvas->Draw(pHist)->SetLineColor(Experimental::TColor::kRed);
-  canvas->Draw(pHist2)->SetLineColor(Experimental::TColor::kBlue);
+   // Create a canvas to be displayed.
+   auto canvas = Experimental::TCanvas::Create("Canvas Title");
+   canvas->Draw(pHist)->SetLineColor(Experimental::TColor::kRed);
+   canvas->Draw(pHist2)->SetLineColor(Experimental::TColor::kBlue);
 
-  canvas->Show();
+   canvas->Show();
+   canvas->SaveAs("th1.png");
 }
