58e31e6ce9e0690b32d9d63d6dc3b90b8ad8cb14
Olivier Couet <olivier.couet@gmail.com>
2018-04-25 12:59:24 +0200

    Use TPadPos in TLine

--- tutorials/v7/line.cxx
+++ tutorials/v7/line.cxx
@@ -13,6 +13,7 @@ R__LOAD_LIBRARY(libGpad);
 #include "ROOT/TCanvas.hxx"
 #include "ROOT/TColor.hxx"
 #include "ROOT/TLine.hxx"
+#include <ROOT/TPadPos.hxx>
 
 void line()
 {
@@ -21,14 +22,12 @@ void line()
    // Create a canvas to be displayed.
    auto canvas = Experimental::TCanvas::Create("Canvas Title");
 
-
-
    for (double i = 0; i < 360; i+=1) {
       double ang = i * TMath::Pi() / 180;
       double x = 0.3*TMath::Cos(ang) + 0.5;
       double y = 0.3*TMath::Sin(ang) + 0.5;
 
-      auto line = std::make_shared<Experimental::TLine>(0.5 ,0.5 ,x, y);
+      auto line = std::make_shared<Experimental::TLine>({0.5_normal, 0.5_normal} , {x_normal, y_normal});
 
       auto col = Experimental::TColor(0.0025*i, 0, 0);
       line->GetOptions().SetLineColor(col);
@@ -37,12 +36,12 @@ void line()
       canvas->Draw(line);
     }
 
-   auto line  = std::make_shared<Experimental::TLine>(0. ,0. ,1. ,1.);     canvas->Draw(line);
-   auto line1 = std::make_shared<Experimental::TLine>(0.1 ,0.1 ,0.9 ,0.1); canvas->Draw(line1);
-   auto line2 = std::make_shared<Experimental::TLine>(0.9 ,0.1 ,0.9 ,0.9); canvas->Draw(line2);
-   auto line3 = std::make_shared<Experimental::TLine>(0.9 ,0.9 ,0.1 ,0.9); canvas->Draw(line3);
-   auto line4 = std::make_shared<Experimental::TLine>(0.1 ,0.1 ,0.1 ,0.9); canvas->Draw(line4);
-   auto line0 = std::make_shared<Experimental::TLine>(0. ,1. ,1. ,0.);     canvas->Draw(line0);
+  auto line  = std::make_shared<Experimental::TLine>({0.0_normal, 0.0_normal} , {1.0_normal, 1.0_normal}); canvas->Draw(line);
+  auto line1 = std::make_shared<Experimental::TLine>({0.1_normal, 0.1_normal} , {0.9_normal, 0.1_normal}); canvas->Draw(line1);
+  auto line2 = std::make_shared<Experimental::TLine>({0.9_normal, 0.1_normal} , {0.9_normal, 0.9_normal}); canvas->Draw(line2);
+  auto line3 = std::make_shared<Experimental::TLine>({0.9_normal, 0.9_normal} , {0.1_normal, 0.9_normal}); canvas->Draw(line3);
+  auto line4 = std::make_shared<Experimental::TLine>({0.1_normal, 0.1_normal} , {0.1_normal, 0.9_normal}); canvas->Draw(line4);
+  auto line0 = std::make_shared<Experimental::TLine>({0.0_normal, 1.0_normal} , {1.0_normal, 0.0_normal}); canvas->Draw(line0);
 
    canvas->Show();
 }
