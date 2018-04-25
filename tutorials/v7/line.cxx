bd82d12800bb75ba9ffa8cb1e041800718a99541
Olivier Couet <olivier.couet@gmail.com>
2018-04-25 15:31:49 +0200

    New version of line.cxx. it compiles.
    Still do not know how to do "x_normal"

--- tutorials/v7/line.cxx
+++ tutorials/v7/line.cxx
@@ -8,7 +8,7 @@
 /// is welcome!
 /// \authors Olivier couet, Iliana Betsou
 
-R__LOAD_LIBRARY(libGpad);
+R__LOAD_LIBRARY(libROOTGpadv7);
 
 #include "ROOT/TCanvas.hxx"
 #include "ROOT/TColor.hxx"
@@ -18,16 +18,26 @@ R__LOAD_LIBRARY(libGpad);
 void line()
 {
    using namespace ROOT;
+   using namespace ROOT::Experimental;
 
    // Create a canvas to be displayed.
    auto canvas = Experimental::TCanvas::Create("Canvas Title");
 
+   TPadPos p1,p2;
+
+   p1.fHoriz = 0._normal;
+   p1.fVert  = 0._normal;
+
    for (double i = 0; i < 360; i+=1) {
       double ang = i * TMath::Pi() / 180;
-      double x = 0.3*TMath::Cos(ang) + 0.5;
-      double y = 0.3*TMath::Sin(ang) + 0.5;
+      double x   = 0.3*TMath::Cos(ang) + 0.5;
+      double y   = 0.3*TMath::Sin(ang) + 0.5;
 
-      auto line = std::make_shared<Experimental::TLine>({0.5_normal, 0.5_normal} , {x_normal, y_normal});
+      // temporary because x_normal and y_normal do not work
+      p2.fHoriz = 1._normal;
+      p2.fVert  = 1._normal;
+
+      auto line = std::make_shared<Experimental::TLine>(p1 , p2);
 
       auto col = Experimental::TColor(0.0025*i, 0, 0);
       line->GetOptions().SetLineColor(col);
@@ -36,12 +46,36 @@ void line()
       canvas->Draw(line);
     }
 
-  auto line  = std::make_shared<Experimental::TLine>({0.0_normal, 0.0_normal} , {1.0_normal, 1.0_normal}); canvas->Draw(line);
-  auto line1 = std::make_shared<Experimental::TLine>({0.1_normal, 0.1_normal} , {0.9_normal, 0.1_normal}); canvas->Draw(line1);
-  auto line2 = std::make_shared<Experimental::TLine>({0.9_normal, 0.1_normal} , {0.9_normal, 0.9_normal}); canvas->Draw(line2);
-  auto line3 = std::make_shared<Experimental::TLine>({0.9_normal, 0.9_normal} , {0.1_normal, 0.9_normal}); canvas->Draw(line3);
-  auto line4 = std::make_shared<Experimental::TLine>({0.1_normal, 0.1_normal} , {0.1_normal, 0.9_normal}); canvas->Draw(line4);
-  auto line0 = std::make_shared<Experimental::TLine>({0.0_normal, 1.0_normal} , {1.0_normal, 0.0_normal}); canvas->Draw(line0);
+   p1.fHoriz = 0._normal;
+   p1.fVert  = 0._normal;
+   p2.fHoriz = 1._normal;
+   p2.fVert  = 1._normal;
+   auto line  = std::make_shared<Experimental::TLine>(p1, p2); canvas->Draw(line);
+   p1.fHoriz = 0.1_normal;
+   p1.fVert  = 0.1_normal;
+   p2.fHoriz = 0.9_normal;
+   p2.fVert  = 0.1_normal;
+   auto line1 = std::make_shared<Experimental::TLine>(p1, p2); canvas->Draw(line1);
+   p1.fHoriz = 0.9_normal;
+   p1.fVert  = 0.1_normal;
+   p2.fHoriz = 0.9_normal;
+   p2.fVert  = 0.9_normal;
+   auto line2 = std::make_shared<Experimental::TLine>(p1, p2); canvas->Draw(line2);
+   p1.fHoriz = 0.9_normal;
+   p1.fVert  = 0.9_normal;
+   p2.fHoriz = 0.1_normal;
+   p2.fVert  = 0.9_normal;
+   auto line3 = std::make_shared<Experimental::TLine>(p1, p2); canvas->Draw(line3);
+   p1.fHoriz = 0.1_normal;
+   p1.fVert  = 0.1_normal;
+   p2.fHoriz = 0.1_normal;
+   p2.fVert  = 0.9_normal;
+   auto line4 = std::make_shared<Experimental::TLine>(p1, p2); canvas->Draw(line4);
+   p1.fHoriz = 0.0_normal;
+   p1.fVert  = 1.0_normal;
+   p2.fHoriz = 1.0_normal;
+   p2.fVert  = 0.0_normal;
+   auto line0 = std::make_shared<Experimental::TLine>(p1, p2); canvas->Draw(line0);
 
    canvas->Show();
 }
