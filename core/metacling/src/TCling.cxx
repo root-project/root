2a862a1826f5f29b9f21153e7a74c24bde99fd6f
Vassil Vassilev <v.g.vassilev@gmail.com>
2018-04-28 09:28:41 +0200

    [cxxmodules] Fix recent failures due to VecOps namespace lookups.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1260,6 +1260,8 @@ TCling::TCling(const char *name, const char *title)
 
       LoadModules({"ROOT_Foundation_C", "ROOT_Config", "ROOT_Foundation_Stage1_NoRTTI", "Core", "RIO"}, *fInterpreter);
 
+      LoadModules({"TreePlayer", "VecOps"}, *fInterpreter);
+
       // Check that the gROOT macro was exported by any core module.
       assert(fInterpreter->getMacro("gROOT") && "Couldn't load gROOT macro?");
 
