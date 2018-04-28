7840579b0972074a99f5f0f1f41f7c0ef9c91762
Guilherme Amadio <amadio@cern.ch>
2018-04-28 11:13:24 +0200

    Disable specializations of TVec templates for (unsigned) long long

--- math/vecops/inc/LinkDef.h
+++ math/vecops/inc/LinkDef.h
@@ -16,13 +16,11 @@
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<short>-;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<int>-;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<long>-;
-#pragma link C++ class ROOT::Experimental::VecOps::TVec<long long>-;
 
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned char>-;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned short>-;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned int>-;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned long>-;
-#pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned long long>-;
 
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<float>::Impl_t+;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<double>::Impl_t+;
@@ -31,12 +29,10 @@
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<short>::Impl_t+;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<int>::Impl_t+;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<long>::Impl_t+;
-#pragma link C++ class ROOT::Experimental::VecOps::TVec<long long>::Impl_t+;
 
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned char>::Impl_t+;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned short>::Impl_t+;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned int>::Impl_t+;
 #pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned long>::Impl_t+;
-#pragma link C++ class ROOT::Experimental::VecOps::TVec<unsigned long long>::Impl_t+;
 
 #endif
