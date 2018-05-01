68e15f3620578d657e7c4b64a769b83a709e220f
Danilo Piparo <danilo.piparo@cern.ch>
2018-05-01 23:29:07 +0200

    [TDF] Move TDF and related components out of TreePlayer into DataFrame
    this includes:
    - Data Sources
    - Utilities
    - Tests

---
+++ tree/dataframe/inc/LinkDef.h
@@ -0,0 +1,40 @@
+// Author: Danilo Piparo CERN, Enrico Guiraud  4/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                    *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __ROOTCLING__
+
+#pragma link C++ nestedtypedefs;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class ROOT::Experimental::TDataFrame-;
+#pragma link C++ class ROOT::Experimental::TDF::TInterface<ROOT::Detail::TDF::TFilterBase>-;
+#pragma link C++ class ROOT::Experimental::TDF::TInterface<ROOT::Detail::TDF::TCustomColumnBase>-;
+#pragma link C++ namespace ROOT::Internal::TDF;
+#pragma link C++ namespace ROOT::Detail::TDF;
+#pragma link C++ namespace ROOT::Experimental::TDF;
+#pragma link C++ class ROOT::Detail::TDF::TFilterBase-;
+#pragma link C++ class ROOT::Detail::TDF::TJittedFilter-;
+#pragma link C++ class ROOT::Detail::TDF::TCustomColumnBase-;
+#pragma link C++ class ROOT::Internal::TDF::CountHelper-;
+#pragma link C++ class ROOT::Detail::TDF::TRangeBase-;
+#pragma link C++ class ROOT::Detail::TDF::TLoopManager-;
+#pragma link C++ class ROOT::Experimental::TDF::TH1DModel-;
+#pragma link C++ class ROOT::Experimental::TDF::TH2DModel-;
+#pragma link C++ class ROOT::Experimental::TDF::TH3DModel-;
+#pragma link C++ class ROOT::Experimental::TDF::TProfile1DModel-;
+#pragma link C++ class ROOT::Experimental::TDF::TProfile2DModel-;
+#pragma link C++ class ROOT::Internal::TDF::TIgnoreErrorLevelRAII-;
+#pragma link C++ class ROOT::Experimental::TDF::TTrivialDS-;
+#pragma link C++ class ROOT::Experimental::TDF::TRootDS-;
+#pragma link C++ class ROOT::Experimental::TDF::TCsvDS-;
+
+#endif
+
+
