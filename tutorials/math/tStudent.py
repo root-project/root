53d038befe2b3fac1efecaf99f6c6ea3da8789b5
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2018-05-02 05:25:12 -0500

    Add comments to be notebook compliance

--- tutorials/math/tStudent.py
+++ tutorials/math/tStudent.py
@@ -20,21 +20,24 @@ from ROOT import TH1D, TF1, TCanvas, kRed, kBlue
 import ROOT
 
 
-# this is the way to force load of MathMore in Cling
+# This is the way to force load of MathMore in Cling
 ROOT.Math.MathMoreLibrary.Load()
 
+# Create the pdf and the cumulative distributions
 n = 100
 a = -5.
 b = 5.
 pdf = TF1("pdf", "ROOT::Math::tdistribution_pdf(x,3.0)", a, b)
 cum = TF1("cum", "ROOT::Math::tdistribution_cdf(x,3.0)", a, b)
 
+# Create the histogram and fill it with the quantiles
 quant = TH1D("quant", "", 9, 0, 0.9)
 
 for i in range(1, 10):
     quant.Fill((i-0.5)/10.0, ROOT.Math.tdistribution_quantile(0.1 * i,
                                                               3.0))
 
+# For each quantile fill with the pdf
 xx = [-1.5] + [quant.GetBinContent(i)for i in range(1, 9)] + [1.5]
 pdfq = []
 for i in range(9):
@@ -45,6 +48,8 @@ for i in range(9):
         x = j * (xx[i+1] - xx[i]) / nbin + xx[i]
         pdfq[i].SetBinContent(j, ROOT.Math.tdistribution_pdf(x, 3))
 
+# Create the Canvas and divide in four draws, for every draw set the line width
+# the title, and the line color.
 Canvas = TCanvas("DistCanvas", "Student Distribution graphs", 10, 10, 800, 700)
 pdf.SetTitle("Student t distribution function")
 cum.SetTitle("Cumulative for Student t")
@@ -67,6 +72,8 @@ pdfq[0].SetTitle("Student t & its quantiles")
 pdf.SetTitle("")
 pdf.Draw()
 pdfq[0].SetTitle("Student t & its quantiles")
+
+# Set the colors in every quantile.
 i = 1
 for pd in pdfq[1:]:
     pd.SetStats(0)
