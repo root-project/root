1a82f930dd242d984a55d89775bccc42fad2d7b8
Oksana Shadura <oksana.shadura@cern.ch>
2018-05-04 18:57:57 +0200

    Revert "Fixing warnings for zlib build on Aarch64,x86_64(Linux & Windows)"
    This reverts commit 71116468507ddfa06264b4d4e83826a5f272ffc8.

--- builtins/zlib/gzlib.c
+++ builtins/zlib/gzlib.c
@@ -3,10 +3,6 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-#ifndef _WIN32
-#include <unistd.h>
-#endif
-
 #include "gzguts.h"
 
 #if defined(_WIN32) && !defined(__BORLANDC__)
