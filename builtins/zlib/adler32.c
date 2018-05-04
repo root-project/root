aeed94f9d9ddb548c31b073b4ab5d9cdcd9cbba1
Oksana Shadura <oksana.shadura@cern.ch>
2018-05-04 18:57:44 +0200

    Revert "Fixing compilation on Mac OS X: the implementation of target attribute is using the STT_GNU_IFUNC symbol type, disabling its usage in zlib for Mac"
    This reverts commit 63345dab49441fdf6f9098ce797c7802816e6326.

--- builtins/zlib/adler32.c
+++ builtins/zlib/adler32.c
@@ -8,7 +8,7 @@
 
 #include "zutil.h"
 
-#if defined (__x86_64__) && defined (__linux__)
+#ifdef __x86_64__
 #include <xmmintrin.h>
 #include <tmmintrin.h>
 #include <immintrin.h>
@@ -144,7 +144,7 @@ uLong ZEXPORT adler32_default(uLong adler, const Bytef *buf, uInt len)
     return adler | (sum2 << 16);
 }
 
-#if defined (__x86_64__) && defined (__linux__)
+#ifdef __x86_64__
 
 #ifdef _MSC_VER
 
