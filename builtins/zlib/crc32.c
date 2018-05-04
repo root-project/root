fb91bf9d99d04881a216cc1f2ed8542fd70b6a4a
Oksana Shadura <oksana.shadura@cern.ch>
2018-05-04 18:59:20 +0200

    Revert "Fixing build on AArch62(v8-a) with crc-32 support (SIMD)"
    This reverts commit 8374c3c949c579e48c15575e804d314151fa25f9.

--- builtins/zlib/crc32.c
+++ builtins/zlib/crc32.c
@@ -21,17 +21,12 @@
   DYNAMIC_CRC_TABLE and MAKECRCH can be #defined to write out crc32.h.
  */
 
-#if(defined __aarch64__)
+#if(defined __aarch64__ &&  defined __ARM_ACLE)
 
-#pragma GCC push_options
-#if __ARM_ARCH >= 8
-#pragma GCC target ("arch=armv8-a+crc")
-#endif
-
-#if defined (__ARM_FEATURE_CRC32)
 #include <arm_neon.h>
-#include <arm_acle.h>
-
+#if (__ARM_ACLE >= 200)
+# include <arm_acle.h>
+#endif
 #include <stdint.h>
 #include <stddef.h>
 
@@ -52,8 +47,6 @@ uint32_t crc32(uint32_t crc, uint8_t *buf, size_t len) {
 
     return ~crc;
 }
-
-#endif // __ARM_FEATURE_CRC32
 
 #else
 
