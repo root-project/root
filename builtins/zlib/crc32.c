ea5fea2ff4f68186e13c64f08ddd2d79aaeb30a4
Oksana Shadura <oksana.shadura@cern.ch>
2018-05-04 18:59:09 +0200

    Revert "Adding support for ARMv8 (no crc32 support)"
    This reverts commit f028b5f5f85a4ebae9e69b9509a4ad9f55477707.

--- builtins/zlib/crc32.c
+++ builtins/zlib/crc32.c
@@ -21,6 +21,8 @@
   DYNAMIC_CRC_TABLE and MAKECRCH can be #defined to write out crc32.h.
  */
 
+#if(defined __aarch64__)
+
 #pragma GCC push_options
 #if __ARM_ARCH >= 8
 #pragma GCC target ("arch=armv8-a+crc")
@@ -50,6 +52,8 @@ uint32_t crc32(uint32_t crc, uint8_t *buf, size_t len) {
 
     return ~crc;
 }
+
+#endif // __ARM_FEATURE_CRC32
 
 #else
 
@@ -335,10 +339,9 @@ void *resolve_crc32(void)
 }
 
 /* This function needs to be resolved at load time */
-uLong crc32(unsigned long, const unsigned char FAR *, unsigned) __attribute__ ((ifunc ("resolve_crc32")));
-
-#else // if not x86_64
-
+uLong crc32(unsigned long, const unsigned char FAR *, unsigned)
+__attribute__ ((ifunc ("resolve_crc32")));
+#else
 uLong crc32(crc, buf, len)
     uLong crc;
     const Bytef *buf;
