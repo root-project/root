683063a7ac2342fe7f5bcca257cc8cd0f0f1e44c
Yuka Takahashi <yukatkh@gmail.com>
2018-05-14 11:31:45 +0200

    [cxxmodules] Fix failing nightly caused by renaming of TreePlayer

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1260,7 +1260,7 @@ TCling::TCling(const char *name, const char *title)
 
       LoadModules({"ROOT_Foundation_C", "ROOT_Config", "ROOT_Foundation_Stage1_NoRTTI", "Core", "RIO"}, *fInterpreter);
       if (!fromRootCling)
-         LoadModules({"TreePlayer", "ROOTVecOps"}, *fInterpreter);
+         LoadModules({"ROOTDataFrame", "ROOTVecOps"}, *fInterpreter);
 
       // Check that the gROOT macro was exported by any core module.
       assert(fInterpreter->getMacro("gROOT") && "Couldn't load gROOT macro?");
