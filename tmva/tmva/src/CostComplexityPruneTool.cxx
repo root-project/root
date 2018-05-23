68724f90fff2e1b1071db910b2866bb0b478b45e
Axel Naumann <Axel.Naumann@cern.ch>
2018-05-23 12:54:22 +0200

    Outline ~IPruneTool() to pin vtable.

--- tmva/tmva/src/CostComplexityPruneTool.cxx
+++ tmva/tmva/src/CostComplexityPruneTool.cxx
@@ -63,6 +63,9 @@ subtree, defined to be the subtree with the best quality index for the validatio
 using namespace TMVA;
 
 
+// Pin vtable of IPruneTool.
+IPruneTool::~IPruneTool() = default;
+
 ////////////////////////////////////////////////////////////////////////////////
 /// the constructor for the cost complexity pruning
 
