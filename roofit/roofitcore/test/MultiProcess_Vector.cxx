9fb99478746be803916e763408a34cb3d1b49aaa
E. G. Patrick Bos <egpbos@gmail.com>
2018-06-01 21:24:46 +0200

    Add GradMinimizer MultiProcess test

--- roofit/roofitcore/test/MultiProcess_Vector.cxx
+++ roofit/roofitcore/test/MultiProcess_Vector.cxx
@@ -37,6 +37,7 @@
 #include <RooAbsPdf.h>
 #include <RooDataSet.h>
 #include <RooMinimizer.h>
+#include <RooGradMinimizer.h>
 #include <RooFitResult.h>
 
 #include "gtest/gtest.h"
@@ -511,3 +512,78 @@ TEST(NLLMultiProcessVsMPFE, throwOnCreatingMPwithMPFE) {
 }
 
 
+class MultiProcessVsNominal : public ::testing::TestWithParam<std::tuple<std::size_t, std::size_t>> {};
+
+TEST_P(MultiProcessVsNominal, GradMinimizer) {
+  // do a minimization, but now using GradMinimizer and its MP version
+
+  // parameters
+  std::size_t NWorkers = std::get<0>(GetParam());
+//  RooFit::MultiProcess::NLLVarTask mp_task_mode = std::get<1>(GetParam());
+  std::size_t seed = std::get<1>(GetParam());
+
+  RooRandom::randomGenerator()->SetSeed(seed);
+
+  RooWorkspace w = RooWorkspace();
+
+  w.factory("Gaussian::g(x[-5,5],mu[0,-3,3],sigma[1])");
+  auto x = w.var("x");
+  RooAbsPdf *pdf = w.pdf("g");
+  RooRealVar *mu = w.var("mu");
+
+  RooDataSet *data = pdf->generate(RooArgSet(*x), 10000);
+  mu->setVal(-2.9);
+
+  auto nll = pdf->createNLL(*data);
+
+  // save initial values for the start of all minimizations
+  RooArgSet values = RooArgSet(*mu, *pdf, *nll);
+
+  RooArgSet *savedValues = dynamic_cast<RooArgSet *>(values.snapshot());
+  if (savedValues == nullptr) {
+    throw std::runtime_error("params->snapshot() cannot be casted to RooArgSet!");
+  }
+
+  // --------
+
+  RooGradMinimizer m0(*nll);
+  m0.setMinimizerType("Minuit2");
+
+  m0.setStrategy(0);
+  m0.setPrintLevel(-1);
+
+  m0.migrad();
+
+  RooFitResult *m0result = m0.lastMinuitFit();
+  double minNll0 = m0result->minNll();
+  double edm0 = m0result->edm();
+  double mu0 = mu->getVal();
+  double muerr0 = mu->getError();
+
+  values = *savedValues;
+
+  RooFit::MultiProcess::GradMinimizer m1(*nll);
+  m1.setMinimizerType("Minuit2");
+
+  m1.setStrategy(0);
+  m1.setPrintLevel(-1);
+
+  m1.migrad();
+
+  RooFitResult *m1result = m1.lastMinuitFit();
+  double minNll1 = m1result->minNll();
+  double edm1 = m1result->edm();
+  double mu1 = mu->getVal();
+  double muerr1 = mu->getError();
+
+  EXPECT_EQ(minNll0, minNll1);
+  EXPECT_EQ(mu0, mu1);
+  EXPECT_EQ(muerr0, muerr1);
+  EXPECT_EQ(edm0, edm1);
+}
+
+
+INSTANTIATE_TEST_CASE_P(NworkersSeed,
+                        MultiProcessVsNominal,
+                        ::testing::Combine(::testing::Values(1,2,3),  // number of workers
+                                           ::testing::Values(2,3)));  // random seed
