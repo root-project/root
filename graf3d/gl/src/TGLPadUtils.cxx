cde057e08255da39434b90b13daa00423c923858
Olivier Couet <olivier.couet@gmail.com>
2018-06-06 16:37:19 +0200

    Suppress gcc8 warnings (fix suggested by Timur).

--- graf3d/gl/src/TGLPadUtils.cxx
+++ graf3d/gl/src/TGLPadUtils.cxx
@@ -1170,6 +1170,9 @@ Tesselator::Tesselator(Bool_t dump)
    if (!tess)
       throw std::runtime_error("tesselator creation failed");
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+
    if (!dump) {
       gluTessCallback(tess, (GLenum)GLU_BEGIN,  (tess_t) glBegin);
       gluTessCallback(tess, (GLenum)GLU_END,    (tess_t) glEnd);
@@ -1179,6 +1182,8 @@ Tesselator::Tesselator(Bool_t dump)
       gluTessCallback(tess, (GLenum)GLU_END,    (tess_t) End);
       gluTessCallback(tess, (GLenum)GLU_VERTEX, (tess_t) Vertex);
    }
+
+#pragma GCC diagnostic pop
 
    gluTessProperty(tess, GLU_TESS_TOLERANCE, 1e-10);
    fTess = tess;
