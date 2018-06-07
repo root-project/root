7beeb034920db49a7566ab032aeca81af657254c
Guilherme Amadio <amadio@cern.ch>
2018-06-07 19:30:00 +0000

    Import cppyy into ROOT

---
+++ bindings/pyroot_experimental/cppyy/cppyy/bench/functioncalls.cxx
@@ -0,0 +1,16 @@
+#include "functioncalls.h"
+
+
+//- group: empty -------------------------------------------------------------
+void empty_call()
+{
+/* empty, to measure pure call overhead */
+}
+
+
+//- group: builtin-args ------------------------------------------------------
+void take_an_int(int /* unused */)
+{
+/* empty, to measure pure call overhead */
+}
+
