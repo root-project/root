668ea8acd9a5c67601f1d2e91c3ed763dcbf5838
Emmanouil Stergiadis <em.stergiadis@gmail.com>
2018-06-08 14:37:00 +0200

    Unified the worker method API calls to make sure all implementation expect the same arguments

--- tmva/tmva/inc/TMVA/DNN/Architectures/Cuda.h
+++ tmva/tmva/inc/TMVA/DNN/Architectures/Cuda.h
@@ -405,9 +405,15 @@ public:
    /** Perform the complete backward propagation step in a Pooling Layer. Based on the
     *  winning idices stored in the index matrix, it just forwards the actiovation
     *  gradients to the previous layer. */
-   static void MaxPoolLayerBackward(std::vector<TCudaMatrix<AFloat>> &activationGradientsBackward,
-                                    const std::vector<TCudaMatrix<AFloat>> &activationGradients,
-                                    const std::vector<TCudaMatrix<AFloat>> &indexMatrix, size_t batchSize, size_t depth,
+   static void MaxPoolLayerBackward(TCudaMatrix<AFloat> &activationGradientsBackward,
+                                    const TCudaMatrix<AFloat> &activationGradients,
+                                    const TCudaMatrix<AFloat> &indexMatrix,
+                                    size_t imgHeight,
+                                    size_t imgWidth,
+                                    size_t fltHeight,
+                                    size_t fltWidth,
+                                    size_t strideRows,
+                                    size_t strideCols,
                                     size_t nLocalViews);
 
    ///@}
