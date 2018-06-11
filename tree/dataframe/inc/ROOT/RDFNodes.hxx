c667e9111998749efaa4c218f8256c2d2ce1117d
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-06-11 15:22:37 +0200

    [DF] Add RNode class, meant to be a common base type for all RDF nodes
    There are a number of trivial operations that users often want to
    perform on dataframes that are surprisingly hard to get right, for
    example adding several Defines in a loop or conditionally adding a
    Filter depending on a runtime boolean (both use-cases are challenging in
    C++, trivial in python).
    
    Difficulties boil down to the fact that different dataframe nodes have
    different types (because their types incorporate e.g. the type of the
    callable passed to a Filter and the type of their parent node in the
    computation graph).
    
    This commit adds a common base class ROOT::RDF::RNode to all
    nodes of the graph (except leaves a.k.a results, which have a completely
    different interface), so that users can, for example:
    
    * take any dataframe node by reference in non-template functions as RNode&
    * keep dataframe nodes in `std::vector<RInterface<RNode>>`
    * keep non-const pointers to dataframe nodes
    
    and so on.
    
    Internal RDataFrame code is also simplified by the introduction of this
    common base class.
    The downside is that if this mechanism is abused users might end up with extra,
    unnecessary virtual calls in their event loop -- on the other hand, this
    mechanism should only be used in situations that before required either complex
    template magic or dirty and slow tricks.

--- tree/dataframe/inc/ROOT/RDFNodes.hxx
+++ tree/dataframe/inc/ROOT/RDFNodes.hxx
@@ -89,6 +89,21 @@ class RCustomColumnBase;
 class RFilterBase;
 class RRangeBase;
 
+class RLoopManager;
+
+/// Base class for non-leaf nodes of the computational graph.
+/// It only exposes the bare minimum interface required to work as a generic part of the computation graph.
+/// RDataFrames and results of transformations can be cast to this type via ROOT::RDF::ToCommonNodeType.
+class RNode {
+public:
+   virtual ~RNode() {}
+   virtual bool CheckFilters(unsigned int, Long64_t) = 0;
+   virtual void Report(ROOT::RDF::RCutFlowReport &) const = 0;
+   virtual void PartialReport(ROOT::RDF::RCutFlowReport &) const = 0;
+   virtual void IncrChildrenCount() = 0;
+   virtual void StopProcessing() = 0;
+};
+
 class RLoopManager {
    friend class ROOT::Internal::RDF::GraphDrawing::GraphCreatorHelper;
 
