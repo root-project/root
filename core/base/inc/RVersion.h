b0ab2dbb276d40b7f9840c6b66e6b689c29c210d
Axel Naumann <Axel.Naumann@cern.ch>
2018-06-13 12:12:55 +0200

    Update ROOT version files to v6.14/01.

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.14/00"
+#define ROOT_RELEASE "6.14/01"
 #define ROOT_RELEASE_DATE "Jun 13 2018"
-#define ROOT_RELEASE_TIME "12:10:39"
+#define ROOT_RELEASE_TIME "12:12:55"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,14,0) /* 396800 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,14,1) /* 396801 */
 
 #endif
