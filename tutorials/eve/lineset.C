a83839655c35b41c10317185354da2365d9ce65f
Olivier Couet <olivier.couet@gmail.com>
2018-06-14 11:15:48 +0200

    - use auto - formatting

--- tutorials/eve/lineset.C
+++ tutorials/eve/lineset.C
@@ -14,17 +14,14 @@ TEveStraightLineSet* lineset(Int_t nlines = 40, Int_t nmarkers = 4)
    TRandom r(0);
    Float_t s = 100;
 
-   TEveStraightLineSet* ls = new TEveStraightLineSet();
+   auto ls = new TEveStraightLineSet();
 
-   for(Int_t i = 0; i<nlines; i++)
-   {
+   for (Int_t i = 0; i<nlines; i++) {
       ls->AddLine( r.Uniform(-s,s), r.Uniform(-s,s), r.Uniform(-s,s),
                    r.Uniform(-s,s), r.Uniform(-s,s), r.Uniform(-s,s));
       // add random number of markers
       Int_t nm = Int_t(nmarkers* r.Rndm());
-      for(Int_t m = 0; m < nm; m++) {
-         ls->AddMarker(i, r.Rndm());
-      }
+      for (Int_t m = 0; m < nm; m++) ls->AddMarker(i, r.Rndm());
    }
 
    ls->SetMarkerSize(1.5);
@@ -43,17 +40,14 @@ TEveStraightLineSet* lineset_2d(Int_t nlines = 40, Int_t nmarkers = 4)
    TRandom r(0);
    Float_t s = 100;
 
-   TEveStraightLineSet* ls = new TEveStraightLineSet();
+   auto ls = new TEveStraightLineSet();
 
-   for(Int_t i = 0; i<nlines; i++)
-   {
+   for (Int_t i = 0; i<nlines; i++) {
       ls->AddLine( r.Uniform(-s,s), r.Uniform(-s,s), 0,
                    r.Uniform(-s,s), r.Uniform(-s,s), 0);
       // add random number of markers
       Int_t nm = Int_t(nmarkers* r.Rndm());
-      for(Int_t m = 0; m < nm; m++) {
-         ls->AddMarker(i, r.Rndm());
-      }
+      for (Int_t m = 0; m < nm; m++) ls->AddMarker(i, r.Rndm());
    }
 
    ls->SetMarkerSize(1.5);
