394777f8b08dd87b4ee4bca2ed4ea49a888bb4c7
Olivier Couet <olivier.couet@gmail.com>
2018-06-14 11:16:03 +0200

    use auto

--- tutorials/eve/overlay_palette.C
+++ tutorials/eve/overlay_palette.C
@@ -8,10 +8,8 @@
 void overlay_palette()
 {
    TEveManager::Create();
-
-   TEveRGBAPalette *p = new TEveRGBAPalette(0, 100);
-   TEveRGBAPaletteOverlay *po = new TEveRGBAPaletteOverlay(p, 0.55, 0.1, 0.4, 0.05);
-
-   TGLViewer* v = gEve->GetDefaultGLViewer();
+   auto p  = new TEveRGBAPalette(0, 100);
+   auto po = new TEveRGBAPaletteOverlay(p, 0.55, 0.1, 0.4, 0.05);
+   auto v  = gEve->GetDefaultGLViewer();
    v->AddOverlayElement(po);
 }
