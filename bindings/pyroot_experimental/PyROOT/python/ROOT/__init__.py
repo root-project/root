1f3d5c707fd7bdf30f4846ac196a59d7431c801d
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-06-22 14:18:17 +0200

    Follow PEP 8 style guide for indentation

--- bindings/pyroot_experimental/PyROOT/python/ROOT/__init__.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/__init__.py
@@ -7,11 +7,11 @@ import importlib
 
 # Pythonizor decorator to be used in pythonization modules
 def pythonization(fn):
-  cppyy.py.add_pythonization(fn)
+    cppyy.py.add_pythonization(fn)
 
 # Trigger the addition of the pythonizations
 for _, module_name, _ in  pkgutil.walk_packages(pyz.__path__):
-  module = importlib.import_module(pyz.__name__ + '.' + module_name)
+    module = importlib.import_module(pyz.__name__ + '.' + module_name)
 
 # Redirect ROOT to cppyy.gbl
 import sys
