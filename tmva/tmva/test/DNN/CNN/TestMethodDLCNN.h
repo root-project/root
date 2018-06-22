ec4a62b6c3f8e3937a4c94adde8e7a52f696543f
Lorenzo Moneta <moneta@olhswep13.cern.ch>
2018-06-22 12:30:22 +0200

    fix Dense layer part for TestMethodDL

--- tmva/tmva/test/DNN/CNN/TestMethodDLCNN.h
+++ tmva/tmva/test/DNN/CNN/TestMethodDLCNN.h
@@ -161,4 +161,114 @@ void testMethodDL_CNN(TString architectureStr)
    delete dataloader;
 }
 
+/** Testing the entire pipeline of the Method DL, when only a Multilayer Percepton
+ *  is constructed. */
+//______________________________________________________________________________
+void testMethodDL_DNN(TString architectureStr)
+{
+
+   ROOT::EnableImplicitMT(1);
+   TMVA::Config::Instance();
+   
+   TFile *input(0);
+   // TString fname = "tmva_class_example.root";
+   TString fname = "http://root.cern.ch/files/tmva_class_example.root";
+   TString fopt = "CACHEREAD";
+   input = TFile::Open(fname,fopt);
+
+   // Register the training and test trees
+   TTree *signalTree = (TTree *)input->Get("TreeS");
+   TTree *background = (TTree *)input->Get("TreeB");
+
+   TString outfileName("TMVA_DNN.root");
+   TFile *outputFile = TFile::Open(outfileName, "RECREATE");
+
+   TMVA::DataLoader *dataloader = new TMVA::DataLoader("dataset");
+
+   dataloader->AddVariable("myvar1 := var1+var2", 'F');
+   dataloader->AddVariable("myvar2 := var1-var2", "Expression 2", "", 'F');
+   dataloader->AddVariable("var3", "Variable 3", "units", 'F');
+   dataloader->AddVariable("var4", "Variable 4", "units", 'F');
+   dataloader->AddSpectator("spec1 := var1*2", "Spectator 1", "units", 'F');
+   dataloader->AddSpectator("spec2 := var1*3", "Spectator 2", "units", 'F');
+
+   // global event weights per tree
+   Double_t signalWeight = 1.0;
+   Double_t backgroundWeight = 1.0;
+
+   // Add signal and background trees
+   dataloader->AddSignalTree(signalTree, signalWeight);
+   dataloader->AddBackgroundTree(background, backgroundWeight);
+
+   dataloader->SetBackgroundWeightExpression("weight");
+
+   TCut mycuts = "";
+   TCut mycutb = "";
+
+   dataloader->PrepareTrainingAndTestTree(
+      mycuts, mycutb, "nTrain_Signal=1000:nTrain_Background=1000:SplitMode=Random:NormMode=NumEvents:!V");
+
+   // Input Layout
+   TString inputLayoutString("InputLayout=1|1|4");
+
+   // Batch Layout
+   TString batchLayoutString("BatchLayout=256|1|4");
+
+   // General layout.
+   TString layoutString("Layout=RESHAPE|1|1|4|FLAT,DENSE|10|TANH,DENSE|10|TANH,DENSE|10|TANH,DENSE|1|LINEAR");
+
+   // Training strategies.
+   TString training0("LearningRate=1e-2,Optimizer=SGD,Momentum=0.9,Repetitions=1,"
+                     "ConvergenceSteps=20,BatchSize=256,TestRepetitions=10,"
+                     "WeightDecay=1e-4,Regularization=L2,MaxEpochs=40"
+                     "DropConfig=0.0+0.5+0.5+0.5");
+   TString training1("LearningRate=1e-2,Optimizer=SGD,Momentum=0.9,Repetitions=1,"
+                     "ConvergenceSteps=10,BatchSize=256,TestRepetitions=5,"
+                     "WeightDecay=1e-4,Regularization=L2,MaxEpochs=20,"
+                     "DropConfig=0.0+0.0+0.0+0.0");
+   
+   TString trainingStrategyString("TrainingStrategy=");
+   trainingStrategyString += training0 + "|" + training1;// + "|" + training2;
+
+   // General Options.
+   TString dnnOptions("!H:V:ErrorStrategy=SUMOFSQUARES:"
+                      "WeightInitialization=XAVIERUNIFORM");
+
+   // Concatenate all option strings
+   dnnOptions.Append(":");
+   dnnOptions.Append(inputLayoutString);
+
+   dnnOptions.Append(":");
+   dnnOptions.Append(batchLayoutString);
+
+   dnnOptions.Append(":");
+   dnnOptions.Append(layoutString);
+
+   dnnOptions.Append(":");
+   dnnOptions.Append(trainingStrategyString);
+
+   dnnOptions.Append(":Architecture=");
+   dnnOptions.Append(architectureStr);
+
+   // create factory
+   TMVA::Factory *factory =
+      new TMVA::Factory("TMVAClassification", outputFile,
+                        "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification");
+
+   TString methodTitle = "DL_" + architectureStr;
+   factory->BookMethod(dataloader, TMVA::Types::kDL, methodTitle, dnnOptions);
+
+   // Train MVAs using the set of training events
+   factory->TrainAllMethods();
+
+   // Save the output
+   outputFile->Close();
+
+   std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
+   std::cout << "==> TMVAClassification is done!" << std::endl;
+
+   delete factory;
+   delete dataloader;
+}
+
 #endif
