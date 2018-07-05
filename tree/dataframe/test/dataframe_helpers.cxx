82264bff7a51c092b05d5359e991f4f0c271cc25
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-07-05 23:13:35 +0200

    [DF] Add a test for the PassAsVec RDF helper

--- tree/dataframe/test/dataframe_helpers.cxx
+++ tree/dataframe/test/dataframe_helpers.cxx
@@ -1,10 +1,15 @@
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RDFHelpers.hxx>
-#include "gtest/gtest.h"
+#include <ROOT/RVec.hxx>
+
 #include <algorithm>
+#include <deque>
 #include <vector>
+
+#include "gtest/gtest.h"
 using namespace ROOT;
 using namespace ROOT::RDF;
+using namespace ROOT::VecOps;
 
 struct TrueFunctor {
    bool operator()() const { return true; }
@@ -32,3 +37,15 @@ TEST(RDFHelpers, Not)
    EXPECT_EQ(1u, *RDataFrame(1).Filter(Not(Not(l))).Count());
 }
 
+TEST(RDFHelpers, PassAsVec)
+{
+   auto One = [] { return 1; };
+   auto df = RDataFrame(1).Define("one", One).Define("_1", One);
+
+   auto TwoOnes = [](const std::vector<int> &v) { return v.size() == 2 && v[0] == 1 && v[1] == 1; };
+   EXPECT_EQ(1u, *df.Filter(PassAsVec<2, int>(TwoOnes), {"one", "_1"}).Count());
+   auto TwoOnesRVec = [](const RVec<int> &v) { return v.size() == 2 && All(v == 1); };
+   EXPECT_EQ(1u, *df.Filter(PassAsVec<2, int>(TwoOnesRVec), {"one", "_1"}).Count());
+   auto TwoOnesDeque = [](const std::deque<int> &v) { return v.size() == 2 && v[0] == 1 && v[1] == 1; };
+   EXPECT_EQ(1u, *df.Filter(PassAsVec<2, int>(TwoOnesDeque), {"one", "_1"}).Count());
+}
