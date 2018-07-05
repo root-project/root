bc12ba52f4111bb4a229ef0180eee0395bf84a32
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-07-05 23:13:35 +0200

    [DF] Add a test for the MakeVec RDF helper

--- tree/dataframe/test/dataframe_helpers.cxx
+++ tree/dataframe/test/dataframe_helpers.cxx
@@ -1,5 +1,6 @@
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RDFHelpers.hxx>
+#include <ROOT/RIntegerSequence.hxx>
 #include "gtest/gtest.h"
 #include <algorithm>
 #include <vector>
@@ -32,3 +33,17 @@ TEST(RDFHelpers, Not)
    EXPECT_EQ(1u, *RDataFrame(1).Filter(Not(Not(l))).Count());
 }
 
+TEST(RDFHelpers, MakeVec)
+{
+   auto One = [] { return 1; };
+   auto df = RDataFrame(1).Define("one", One).Define("_1", One);
+
+   auto TwoOnes = [](const std::vector<int> &v) { return v.size() == 2 && v[0] == 1 && v[1] == 1; };
+#if R__HAS_VARIABLE_TEMPLATES
+   auto with_vec = df.Define("ones", MakeVec<2, int>::func, {"one", "_1"});
+#else
+   auto with_vec =
+      df.Define("ones", ROOT::Internal::RDF::MakeVecHelper<std::make_index_sequence<2>, int>::func, {"one", "_1"});
+#endif
+   EXPECT_EQ(1u, *with_vec.Filter(TwoOnes, {"ones"}).Count());
+}
