be9fca2635c40fc55cffc7ae0c0f267a786df10a
Fons Rademakers <Fons.Rademakers@cern.ch>
2018-07-12 17:31:16 +0300

    Restore FitSlices() call back to original parameters.
    Shows again same results as in the tutorials/dataframe/df101_h1Analysis.C
    script.

--- tutorials/tree/h1analysis.C
+++ tutorials/tree/h1analysis.C
@@ -366,9 +366,7 @@ void h1analysis::Terminate()
       delete gROOT->GetFunction("f2");
    TF1 *f2 = new TF1("f2",fdm2,0.139,0.17,2);
    f2->SetParameters(10000, 10);
-   // Restrict to three bins in this example
-   Info("Fit Slices","Restricting fit to two bins only in this example...");
-   h2->FitSlicesX(f2,10,20,10,"g5 l");
+   h2->FitSlicesX(f2,0,-1,1,"qln");
    TH1D *h2_1 = (TH1D*)gDirectory->Get("h2_1");
    h2_1->GetXaxis()->SetTitle("#tau[ps]");
    h2_1->SetMarkerStyle(21);
