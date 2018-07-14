d9b2b8f64387a77fb76f45780544b14a005323ea
Jakob Blomer <jblomer@cern.ch>
2018-07-14 12:58:47 +0200

    [DF] Draft of sqlite data source

--- tree/dataframe/inc/ROOT/RSqliteDS.hxx
+++ tree/dataframe/inc/ROOT/RSqliteDS.hxx
@@ -14,18 +14,53 @@
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RDataSource.hxx"
 
+#include <map>
+#include <memory>
+
+#include <sqlite3.h>
+
 namespace ROOT {
 
 namespace RDF {
 
 class RSqliteDS final : public ROOT::RDF::RDataSource {
+private:
+   enum class Types {
+     kInt,
+     kFloat,
+     kText,
+     kBlob,
+     kNull
+   };
+
+   struct Value_t {
+     Value_t() : fType(Types::kNull), fPtr(nullptr), fIsActive(false) {}
+
+     Types fType;
+     Long64_t fInt;
+     double fFloat;
+     std::string fText;
+     void *fPtr;
+     bool fIsActive;
+   };
+
+   void SqliteError(int errcode);
+
+   struct sqlite3 *fDb;
+   struct sqlite3_stmt *fQuery;
+   ULong64_t fNRow;
+   std::vector<std::string> fColumnNames;
+   std::vector<Types> fColumnTypes;
+   std::map<Types, std::string> fTypeNames;
+   std::vector<Value_t> fValues;
+
 public:
    RSqliteDS(std::string_view fileName, std::string_view query);
    ~RSqliteDS();
    virtual void SetNSlots(unsigned int nSlots) override;
    virtual const std::vector<std::string> &GetColumnNames() const override;
-   virtual bool HasColumn(std::string_view) const override;
-   virtual std::string GetTypeName(std::string_view) const override;
+   virtual bool HasColumn(std::string_view colName) const override;
+   virtual std::string GetTypeName(std::string_view colName) const override;
    virtual std::vector<std::pair<ULong64_t, ULong64_t>> GetEntryRanges() override;
    virtual bool SetEntry(unsigned int slot, ULong64_t entry) override;
 
