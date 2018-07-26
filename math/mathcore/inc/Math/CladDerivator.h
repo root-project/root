540974aae6a775b9bd7d901bd11d98f70ccc25e3
Vassil Vassilev <v.g.vassilev@gmail.com>
2018-07-26 12:51:32 +0200

    Enable the automatic differentiation library clad in ROOT.
    clad is a C++ plugin for clang and cling that implements automatic
    differentiation of user-defined functions by employing the chain rule in
    forward and reverse mode, coupled with source code transformation and AST
    constant fold.
    
    In mathematics and computer algebra, automatic differentiation (AD) is a
    set of techniques to numerically evaluate the derivative of a function
    specified by a computer program. AD exploits the fact that every computer
    program, no matter how complicated, executes a sequence of elementary
    arithmetic operations (addition, subtraction, multiplication, division, etc.)
    and elementary functions (exp, log, sin, cos, etc.). By applying the chain
    rule repeatedly to these operations, derivatives of arbitrary order can
    be computed automatically, accurately to working precision, and using at
    most a small constant factor more arithmetic operations than the original
    program.
    
    AD is an alternative technique to symbolic and numerical differentiation.
    These classical methods run into problems: symbolic differentiation leads
    to inefficient code (unless done carefully) and faces the difficulty of
    converting a computer program into a single expression, while numerical
    differentiation can introduce round-off errors in the discretization
    process and cancellation. Both classical methods have problems with
    calculating higher derivatives, where the complexity and errors increase.
    Finally, both classical methods are slow at computing the partial
    derivatives of a function with respect to many inputs, as is needed for
    gradient-based optimization algorithms. Automatic differentiation solves
    all of these problems, at the expense of introducing more software
    dependencies.
    
    This patch allows ROOT to interoperate with clad. Namely, users can ask
    the interpreter to produce a derivative or a gradient to a known function.
    
    An illustrative example code for first order derivative:
    
    root [0] #include "Math/CladDerivator.h"
    root [1] double my_pow2(double x) { return x*x; }
    root [2] auto meta_obj = clad::differentiate(my_pow2, /*wrt 1-st argument*/0);
    root [3] meta_obj.dump();
    The code is: double my_pow2_darg0(double x) {
        return (1. * x + x * 1.);
    }
    root [5] meta_obj.execute(1) // no iterations, at the cost of function call.
    (double) 2.0000000
    
    Learn more about clad at https://github.com/vgvassilev/clad
    
    Patch by Aleksandr Efremov and me!

---
+++ math/mathcore/inc/Math/CladDerivator.h
@@ -0,0 +1,27 @@
+/// \file CladDerivator.h
+///
+/// \brief The file is a bridge between ROOT and clad automatic differentiation
+/// plugin.
+///
+/// \author Vassil Vassilev <vvasilev@cern.ch>
+///
+/// \date July, 2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef CLAD_DERIVATOR
+#define CLAD_DERIVATOR
+
+#ifndef __CLING__
+  #error "This file must not be included by compiled programs."
+#endif //__CLING__
+
+#include <plugins/include/clad/Differentiator/Differentiator.h>
+
+#endif // CLAD_DERIVATOR
