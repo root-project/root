cc5b52de7a265e8e13fd0464d89550188dca3c11
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-08-02 15:52:10 +0200

    [DF] Remove unused method in RLoopManager

--- tree/dataframe/src/RDFNodes.cxx
+++ tree/dataframe/src/RDFNodes.cxx
@@ -651,11 +651,6 @@ TTree *RLoopManager::GetTree() const
    return fTree.get();
 }
 
-TDirectory *RLoopManager::GetDirectory() const
-{
-   return fDirPtr;
-}
-
 void RLoopManager::Book(const ActionBasePtr_t &actionPtr)
 {
    fBookedActions.emplace_back(actionPtr);
