694570b2358c66ed578e07dd2129d58cf3e0a62d
Clemens Lange <clemens.lange@cern.ch>
2018-08-07 13:53:09 +0200

    update headers, removing main function up to rf708

--- tutorials/roofit/rf703_effpdfprod.py
+++ tutorials/roofit/rf703_effpdfprod.py
@@ -1,85 +1,82 @@
-#####################################
-#
-# 'SPECIAL PDFS' RooFit tutorial macro #703
-#
-# Using a product of an (acceptance) efficiency and a p.d.f as p.d.f.
-#
-#
-#
-# 07/2008 - Wouter Verkerke
-#
-# /
+## \file
+## \ingroup tutorial_roofit
+## \notebook
+##
+## 'SPECIAL PDFS' RooFit tutorial macro #703
+##
+## Using a product of an (acceptance) efficiency and a p.d.f as p.d.f.
+##
+## \macro_code
+##
+## \date February 2018
+## \author Clemens Lange
+## \author Wouter Verkerke (C version)
 
 
 import ROOT
 
 
-def rf703_effpdfprod():
-    # D e f i n e   o b s e r v a b l e s   a n d   d e c a y   p d f
-    # ---------------------------------------------------------------
+# Define observables and decay pdf
+# ---------------------------------------------------------------
 
-    # Declare observables
-    t = ROOT.RooRealVar("t", "t", 0, 5)
+# Declare observables
+t = ROOT.RooRealVar("t", "t", 0, 5)
 
-    # Make pdf
-    tau = ROOT.RooRealVar("tau", "tau", -1.54, -4, -0.1)
-    model = ROOT.RooExponential("model", "model", t, tau)
+# Make pdf
+tau = ROOT.RooRealVar("tau", "tau", -1.54, -4, -0.1)
+model = ROOT.RooExponential("model", "model", t, tau)
 
-    # D e f i n e   e f f i c i e n c y   f u n c t i o n
-    # ---------------------------------------------------
+# Define efficiency function
+# ---------------------------------------------------
 
-    # Use error function to simulate turn-on slope
-    eff = ROOT.RooFormulaVar("eff", "0.5*(TMath::Erf((t-1)/0.5)+1)", ROOT.RooArgList(t))
+# Use error function to simulate turn-on slope
+eff = ROOT.RooFormulaVar("eff", "0.5*(TMath::Erf((t-1)/0.5)+1)", ROOT.RooArgList(t))
 
-    # D e f i n e   d e c a y   p d f   w i t h   e f f i c i e n c y
-    # ---------------------------------------------------------------
+# Define decay pdf with efficiency
+# ---------------------------------------------------------------
 
-    # Multiply pdf(t) with efficiency in t
-    modelEff = ROOT.RooEffProd("modelEff", "model with efficiency", model, eff)
+# Multiply pdf(t) with efficiency in t
+modelEff = ROOT.RooEffProd("modelEff", "model with efficiency", model, eff)
 
-    # P l o t   e f f i c i e n c y , d f
-    # ----------------------------------------
+# Plot efficiency, pdf
+# ----------------------------------------
 
-    frame1 = t.frame(ROOT.RooFit.Title("Efficiency"))
-    eff.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+frame1 = t.frame(ROOT.RooFit.Title("Efficiency"))
+eff.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
 
-    frame2 = t.frame(ROOT.RooFit.Title("Pdf with and without efficiency"))
+frame2 = t.frame(ROOT.RooFit.Title("Pdf with and without efficiency"))
 
-    model.plotOn(frame2, ROOT.RooFit.LineStyle(ROOT.kDashed))
-    modelEff.plotOn(frame2)
+model.plotOn(frame2, ROOT.RooFit.LineStyle(ROOT.kDashed))
+modelEff.plotOn(frame2)
 
-    # G e n e r a t e   t o y   d a t a , i t   m o d e l E f f   t o   d a t a
-    # ------------------------------------------------------------------------------
+# Generate toy data, fit model eff to data
+# ------------------------------------------------------------------------------
 
-    # Generate events. If the input pdf has an internal generator, internal generator
-    # is used and an accept/reject sampling on the efficiency is applied.
-    data = modelEff.generate(ROOT.RooArgSet(t), 10000)
+# Generate events. If the input pdf has an internal generator, internal generator
+# is used and an accept/reject sampling on the efficiency is applied.
+data = modelEff.generate(ROOT.RooArgSet(t), 10000)
 
-    # Fit pdf. The normalization integral is calculated numerically.
-    modelEff.fitTo(data)
+# Fit pdf. The normalization integral is calculated numerically.
+modelEff.fitTo(data)
 
-    # Plot generated data and overlay fitted pdf
-    frame3 = t.frame(ROOT.RooFit.Title("Fitted pdf with efficiency"))
-    data.plotOn(frame3)
-    modelEff.plotOn(frame3)
+# Plot generated data and overlay fitted pdf
+frame3 = t.frame(ROOT.RooFit.Title("Fitted pdf with efficiency"))
+data.plotOn(frame3)
+modelEff.plotOn(frame3)
 
-    c = ROOT.TCanvas("rf703_effpdfprod", "rf703_effpdfprod", 1200, 400)
-    c.Divide(3)
-    c.cd(1)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame1.GetYaxis().SetTitleOffset(1.4)
-    frame1.Draw()
-    c.cd(2)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame2.GetYaxis().SetTitleOffset(1.6)
-    frame2.Draw()
-    c.cd(3)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame3.GetYaxis().SetTitleOffset(1.6)
-    frame3.Draw()
+c = ROOT.TCanvas("rf703_effpdfprod", "rf703_effpdfprod", 1200, 400)
+c.Divide(3)
+c.cd(1)
+ROOT.gPad.SetLeftMargin(0.15)
+frame1.GetYaxis().SetTitleOffset(1.4)
+frame1.Draw()
+c.cd(2)
+ROOT.gPad.SetLeftMargin(0.15)
+frame2.GetYaxis().SetTitleOffset(1.6)
+frame2.Draw()
+c.cd(3)
+ROOT.gPad.SetLeftMargin(0.15)
+frame3.GetYaxis().SetTitleOffset(1.6)
+frame3.Draw()
 
-    c.SaveAs("rf703_effpdfprod.png")
-
-
-if __name__ == "__main__":
-    rf703_effpdfprod()
+c.SaveAs("rf703_effpdfprod.png")
--- tutorials/roofit/rf705_linearmorph.py
+++ tutorials/roofit/rf705_linearmorph.py
@@ -1,133 +1,128 @@
-#####################################
-#
-# 'SPECIAL PDFS' RooFit tutorial macro #705
-#
-# Linear interpolation between p.d.f shapes using the 'Alex Read' algorithm
-#
-#
-#
-# 07/2008 - Wouter Verkerke
-#
-# /
+## \file
+## \ingroup tutorial_roofit
+## \notebook
+##
+## 'SPECIAL PDFS' RooFit tutorial macro #705
+##
+## Linear interpolation between p.d.f shapes using the 'Alex Read' algorithm
+##
+## \macro_code
+##
+## \date February 2018
+## \author Clemens Lange
+## \author Wouter Verkerke (C version)
 
 
 import ROOT
 
 
-def rf705_linearmorph():
-    # C r e a t e   e n d   p o i n t   p d f   s h a p e s
-    # ------------------------------------------------------
+# Create end point pdf shapes
+# ------------------------------------------------------
 
-    # Observable
-    x = ROOT.RooRealVar("x", "x", -20, 20)
+# Observable
+x = ROOT.RooRealVar("x", "x", -20, 20)
 
-    # Lower end point shape: a Gaussian
-    g1mean = ROOT.RooRealVar("g1mean", "g1mean", -10)
-    g1 = ROOT.RooGaussian("g1", "g1", x, g1mean, ROOT.RooFit.RooConst(2))
+# Lower end point shape: a Gaussian
+g1mean = ROOT.RooRealVar("g1mean", "g1mean", -10)
+g1 = ROOT.RooGaussian("g1", "g1", x, g1mean, ROOT.RooFit.RooConst(2))
 
-    # Upper end point shape: a Polynomial
-    g2 = ROOT.RooPolynomial("g2", "g2", x, ROOT.RooArgList(
-        ROOT.RooFit.RooConst(-0.03), ROOT.RooFit.RooConst(-0.001)))
+# Upper end point shape: a Polynomial
+g2 = ROOT.RooPolynomial("g2", "g2", x, ROOT.RooArgList(
+    ROOT.RooFit.RooConst(-0.03), ROOT.RooFit.RooConst(-0.001)))
 
-    # C r e a t e   i n t e r p o l a t i n g   p d f
-    # -----------------------------------------------
+# Create interpolating pdf
+# -----------------------------------------------
 
-    # Create interpolation variable
-    alpha = ROOT.RooRealVar("alpha", "alpha", 0, 1.0)
+# Create interpolation variable
+alpha = ROOT.RooRealVar("alpha", "alpha", 0, 1.0)
 
-    # Specify sampling density on observable and interpolation variable
-    x.setBins(1000, "cache")
-    alpha.setBins(50, "cache")
+# Specify sampling density on observable and interpolation variable
+x.setBins(1000, "cache")
+alpha.setBins(50, "cache")
 
-    # Construct interpolating pdf in (x,a) represent g1(x) at a=a_min
-    # and g2(x) at a=a_max
-    lmorph = ROOT.RooIntegralMorph("lmorph", "lmorph", g1, g2, x, alpha)
+# Construct interpolating pdf in (x,a) represent g1(x) at a=a_min
+# and g2(x) at a=a_max
+lmorph = ROOT.RooIntegralMorph("lmorph", "lmorph", g1, g2, x, alpha)
 
-    # P l o t   i n t e r p o l a t i n g   p d f   a t   v a r i o u s   a l p h a
-    # -----------------------------------------------------------------------------
+# Plot interpolating pdf aat various alphas   a l p h a
+# -----------------------------------------------------------------------------
 
-    # Show end points as blue curves
-    frame1 = x.frame()
-    g1.plotOn(frame1)
-    g2.plotOn(frame1)
+# Show end points as blue curves
+frame1 = x.frame()
+g1.plotOn(frame1)
+g2.plotOn(frame1)
 
-    # Show interpolated shapes in red
-    alpha.setVal(0.125)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.25)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.375)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.50)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.625)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.75)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.875)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
-    alpha.setVal(0.95)
-    lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+# Show interpolated shapes in red
+alpha.setVal(0.125)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.25)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.375)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.50)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.625)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.75)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.875)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
+alpha.setVal(0.95)
+lmorph.plotOn(frame1, ROOT.RooFit.LineColor(ROOT.kRed))
 
-    # S h o w   2 D   d i s t r i b u t i o n   o f   p d f ( x , l p h a )
-    # -----------------------------------------------------------------------
+# Show 2D distribution of pdf(x,alpha)
+# -----------------------------------------------------------------------
 
-    # Create 2D histogram
-    hh = lmorph.createHistogram("hh", x, ROOT.RooFit.Binning(
-        40), ROOT.RooFit.YVar(alpha, ROOT.RooFit.Binning(40)))
-    hh.SetLineColor(ROOT.kBlue)
+# Create 2D histogram
+hh = lmorph.createHistogram("hh", x, ROOT.RooFit.Binning(
+    40), ROOT.RooFit.YVar(alpha, ROOT.RooFit.Binning(40)))
+hh.SetLineColor(ROOT.kBlue)
 
-    # F i t   p d f   t o   d a t a s e t   w i t h   a l p a = 0 . 8
-    # -----------------------------------------------------------------
+# Fit pdf to dataset with alpha=0.8
+# -----------------------------------------------------------------
 
-    # Generate a toy dataset alpha = 0.8
-    alpha.setVal(0.8)
-    data = lmorph.generate(ROOT.RooArgSet(x), 1000)
+# Generate a toy dataset alpha = 0.8
+alpha.setVal(0.8)
+data = lmorph.generate(ROOT.RooArgSet(x), 1000)
 
-    # Fit pdf to toy data
-    lmorph.setCacheAlpha(ROOT.kTRUE)
-    lmorph.fitTo(data, ROOT.RooFit.Verbose(ROOT.kTRUE))
+# Fit pdf to toy data
+lmorph.setCacheAlpha(ROOT.kTRUE)
+lmorph.fitTo(data, ROOT.RooFit.Verbose(ROOT.kTRUE))
 
-    # Plot fitted pdf and data overlaid
-    frame2 = x.frame(ROOT.RooFit.Bins(100))
-    data.plotOn(frame2)
-    lmorph.plotOn(frame2)
+# Plot fitted pdf and data overlaid
+frame2 = x.frame(ROOT.RooFit.Bins(100))
+data.plotOn(frame2)
+lmorph.plotOn(frame2)
 
-    # S c a n   - l o g ( L )   v s   a l p h a
-    # -----------------------------------------
+# Scan -log(L) vs alpha
+# -----------------------------------------
 
-    # Show scan -log(L) of dataset w.r.t alpha
-    frame3 = alpha.frame(ROOT.RooFit.Bins(100), ROOT.RooFit.Range(0.1, 0.9))
+# Show scan -log(L) of dataset w.r.t alpha
+frame3 = alpha.frame(ROOT.RooFit.Bins(100), ROOT.RooFit.Range(0.1, 0.9))
 
-    # Make 2D pdf of histogram
-    nll = ROOT.RooNLLVar("nll", "nll", lmorph, data)
-    nll.plotOn(frame3, ROOT.RooFit.ShiftToZero())
+# Make 2D pdf of histogram
+nll = ROOT.RooNLLVar("nll", "nll", lmorph, data)
+nll.plotOn(frame3, ROOT.RooFit.ShiftToZero())
 
-    lmorph.setCacheAlpha(ROOT.kFALSE)
+lmorph.setCacheAlpha(ROOT.kFALSE)
 
-    c = ROOT.TCanvas("rf705_linearmorph", "rf705_linearmorph", 800, 800)
-    c.Divide(2, 2)
-    c.cd(1)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame1.GetYaxis().SetTitleOffset(1.6)
-    frame1.Draw()
-    c.cd(2)
-    ROOT.gPad.SetLeftMargin(0.20)
-    hh.GetZaxis().SetTitleOffset(2.5)
-    hh.Draw("surf")
-    c.cd(3)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame3.GetYaxis().SetTitleOffset(1.4)
-    frame3.Draw()
-    c.cd(4)
-    ROOT.gPad.SetLeftMargin(0.15)
-    frame2.GetYaxis().SetTitleOffset(1.4)
-    frame2.Draw()
+c = ROOT.TCanvas("rf705_linearmorph", "rf705_linearmorph", 800, 800)
+c.Divide(2, 2)
+c.cd(1)
+ROOT.gPad.SetLeftMargin(0.15)
+frame1.GetYaxis().SetTitleOffset(1.6)
+frame1.Draw()
+c.cd(2)
+ROOT.gPad.SetLeftMargin(0.20)
+hh.GetZaxis().SetTitleOffset(2.5)
+hh.Draw("surf")
+c.cd(3)
+ROOT.gPad.SetLeftMargin(0.15)
+frame3.GetYaxis().SetTitleOffset(1.4)
+frame3.Draw()
+c.cd(4)
+ROOT.gPad.SetLeftMargin(0.15)
+frame2.GetYaxis().SetTitleOffset(1.4)
+frame2.Draw()
 
-    c.SaveAs("rf705_linearmorph.png")
-
-    return
-
-
-if __name__ == "__main__":
-    rf705_linearmorph()
+c.SaveAs("rf705_linearmorph.png")
