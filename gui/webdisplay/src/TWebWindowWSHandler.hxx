07c01e3de7426f36644275ac6af6fc068834c504
Sergey Linev <S.Linev@gsi.de>
2018-08-20 17:17:00 +0200

    webgui: extract WebWindow WS handler in separate class
    Now URL to the webwindow is shorter and like:
    http://server/win1/

---
+++ gui/webdisplay/src/TWebWindowWSHandler.hxx
@@ -0,0 +1,64 @@
+/// \file ROOT/TWebWindowWSHandler.hxx
+/// \ingroup WebGui ROOT7
+/// \author Sergey Linev <s.linev@gsi.de>
+/// \date 2018-08-20
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_TWebWindowWSHandler
+#define ROOT7_TWebWindowWSHandler
+
+#include "THttpWSHandler.h"
+
+#include <ROOT/TWebWindow.hxx>
+
+namespace ROOT {
+namespace Experimental {
+
+/// just wrapper to deliver websockets call-backs to the TWebWindow class
+
+class TWebWindowWSHandler : public THttpWSHandler {
+public:
+   TWebWindow &fWindow; ///<! window reference
+   bool fSenderMT;      ///<! support multithreading for senders
+
+   /// constructor
+   TWebWindowWSHandler(TWebWindow &wind, const char *name, bool sendermt = false)
+      : THttpWSHandler(name, "TWebWindow websockets handler"), fWindow(wind), fSenderMT(sendermt)
+   {
+   }
+
+   virtual ~TWebWindowWSHandler()
+   {
+   }
+
+   /// returns content of default web-page
+   /// THttpWSHandler interface
+   virtual TString GetDefaultPageContent() override { return IsDisabled() ? "" : fWindow.fDefaultPage.c_str(); }
+
+   /// Process websocket request - called from THttpServer thread
+   /// THttpWSHandler interface
+   virtual Bool_t ProcessWS(THttpCallArg *arg) override { return arg && !IsDisabled() ? fWindow.ProcessWS(*arg) : kFALSE; }
+
+   /// Allow processing of WS actions in arbitrary thread
+   virtual Bool_t AllowMTProcess() const { return kTRUE; }
+
+   /// Allows usage of multithreading in send operations
+   virtual Bool_t AllowMTSend() const { return fSenderMT; }
+
+   /// React on completion of multithreaded send operaiotn
+   virtual void CompleteMTSend(UInt_t wsid) { if (!IsDisabled()) fWindow.CompleteMTSend(wsid); }
+};
+
+} // namespace Experimental
+} // namespace ROOT
+
+#endif
