6a83491b481e683f35e535a78a9be819acace546
Elie Khairallah <eliekhairallah1996@gmail.com>
2018-08-24 13:11:49 +0200

    make the path compatible with windows

--- build/misc/argparse2help.py
+++ build/misc/argparse2help.py
@@ -65,7 +65,7 @@ def write_man(parser, fileName):
 if __name__ == "__main__":
 	path = os.path.expanduser(sys.argv[1])
 	splitPath = path.split("/")
-	sys.path.insert(0, "/".join(splitPath[:len(splitPath)-1]))
+	sys.path.insert(0, "/" + os.path.join(*splitPath[:len(splitPath)-1]))
 	i = importlib.import_module(splitPath[len(splitPath)-1].partition(".")[0])
 	parser = i.get_argparse()
 	listArgs = parser._actions
