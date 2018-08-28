61d50cf108db67d4fcf850bdf5b46426444d943e
Philippe Canal <pcanal@fnal.gov>
2018-08-28 14:58:59 +0200

    Remove left over work byproduct.
    Lock was moved deeper

--- tree/tree/src/TBranchElement.cxx
+++ tree/tree/src/TBranchElement.cxx
@@ -5248,7 +5248,6 @@ void TBranchElement::SetAddress(void* addr)
    // We do this only once because it depends only on
    // the type of our object, not on its address.
    if (!fInitOffsets) {
-      // R__LOCKGUARD(gInterpreterMutex);
       InitializeOffsets();
    }
 
