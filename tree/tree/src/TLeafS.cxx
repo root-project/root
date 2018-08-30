18f4580afbe18ca3c07b0a0a303bcc927bae1375
Brian Bockelman <bbockelm@cse.unl.edu>
2018-08-30 14:56:56 -0500

    [TTree] Switch from builtin functions to ROOT-provided macros.
    The ROOT-provided macros work across all platforms (__builtin_bswap
    isn't available in the Windows compiler) and properly handle the
    cases where byteswap isn't needed.

--- tree/tree/src/TLeafS.cxx
+++ tree/tree/src/TLeafS.cxx
@@ -20,6 +20,7 @@ A TLeaf for a 16 bit Integer data type.
 #include "TBuffer.h"
 #include "TClonesArray.h"
 #include "Riostream.h"
+#include "Bytes.h"
 
 ClassImp(TLeafS);
 
@@ -184,10 +185,12 @@ bool TLeafS::ReadBasketFast(TBuffer& input_buf, Long64_t N)
 {
    if (R__unlikely(fLeafCount)) {return false;}
 
-   Int_t *buf __attribute__((aligned(8))) = reinterpret_cast<Int_t*>(input_buf.GetCurrent());
+   Short_t *buf __attribute__((aligned(8))) = reinterpret_cast<Short_t*>(input_buf.GetCurrent());
 #ifdef R__BYTESWAP
    for (int idx=0; idx<fLen*N; idx++) {
-      buf[idx] = __builtin_bswap16(buf[idx]);
+      Short_t tmp = *reinterpret_cast<Short_t*>(buf + idx); // Makes a copy of the values; frombuf can't handle aliasing.
+      char *tmp_ptr = reinterpret_cast<char *>(&tmp);
+      frombuf(tmp_ptr, buf + idx);
    }
 #endif
    return true;
