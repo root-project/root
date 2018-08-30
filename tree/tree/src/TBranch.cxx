b95ef601612e91efade380cbf63c4ea6f82a2753
Brian Bockelman <bbockelm@cse.unl.edu>
2018-08-30 14:12:41 -0500

    [TTree] Fix case where there are no available baskets.
    If there are no available baskets, we ask `fBaskets` to iterate
    through the `TObjArray` to find the last element.
    
    Unfortunately, we never checked the return code for `GetLast()`
    and assumed there was always one basket in the array.  If there
    wasn't, then we would potentially segfault.

--- tree/tree/src/TBranch.cxx
+++ tree/tree/src/TBranch.cxx
@@ -1749,7 +1749,9 @@ TBasket* TBranch::GetFreshBasket(TBuffer* user_buffer)
          if (!basket) {
             fBaskets.SetLast(-2); // For recalculation of Last.
             oldindex = fBaskets.GetLast();
-            basket = (TBasket*)fBaskets.UncheckedAt(oldindex);
+            if (oldindex != fBaskets.LowerBound()-1) {
+               basket = (TBasket*)fBaskets.UncheckedAt(oldindex);
+            }
          }
          if (basket && fBasketBytes[oldindex]!=0) {
             if (basket == fCurrentBasket) {
