7aac1b7063a305706b73b5fc48ffe2caf49fb63e
Danilo Piparo <danilo.piparo@cern.ch>
2018-09-04 15:33:59 +0200

    [RooFit] Make test more robust against uninitialised variable value

--- roofit/roofitcore/test/simple.cxx
+++ roofit/roofitcore/test/simple.cxx
@@ -11,5 +11,11 @@ TEST(RooRealVar, PrintDefaultConstructed)
    std::stringstream s;
    RooRealVar v;
    v.printStream(s, v.defaultPrintContents(""),v.defaultPrintStyle(""));
-   EXPECT_STREQ(s.str().c_str(), "RooRealVar:: = 0 +/- (0,0)  L(0 - 0) B(0) \n");
+
+   auto resString = s.str();
+   auto separatorPos = resString.find("+/-");
+   auto resConstChar = resString.c_str();
+   auto resCutConstChar = resConstChar + separatorPos;
+
+   EXPECT_STREQ(resCutConstChar, "+/- (0,0)  L(0 - 0) B(0) \n");
 }
