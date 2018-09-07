7fcb947b37fda55e817ff9d435dd3b3c0bb6e250
Olivier Couet <olivier.couet@gmail.com>
2018-09-07 17:23:38 +0200

    The file containing the world map should be accessed remotely.

--- tutorials/sql/SQLiteIPLocation.C
+++ tutorials/sql/SQLiteIPLocation.C
@@ -29,7 +29,7 @@ void SQLiteIPLocation() {
 
    TSQLServer *db = TSQLServer::Connect("root_download_stats.sqlite", "", "");
 
-   auto F = new TFile("WMCountry.root");
+   TFile *F = TFile::Open("http://root.cern.ch/files/WM.root");
    TH2Poly *WM;
    WM = (TH2Poly*) F->Get("WM");
    const char *location = "SELECT IPLatitude, IPLongitude FROM accesslog;";
