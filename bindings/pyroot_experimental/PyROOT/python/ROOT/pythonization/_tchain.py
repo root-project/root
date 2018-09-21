3c7eef0a914213c4fd5e0068cb1902039057036b
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-09-21 10:41:57 +0200

    [Exp PyROOT] Add pythonization for TChain::SetBranchAddress

---
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_tchain.py
@@ -0,0 +1,18 @@
+
+from ROOT import pythonization
+
+from _ttree import _SetBranchAddress as TTreeSetBranchAddress
+
+# Pythonizor function
+@pythonization
+def pythonize_tchain(klass, name):
+    # Parameters:
+    # klass: class to be pythonized
+    # name: string containing the name of the class
+
+    if name == 'TChain':
+        # SetBranchAddress
+        klass._OriginalSetBranchAddress = klass.SetBranchAddress
+        klass.SetBranchAddress = TTreeSetBranchAddress
+
+    return True
