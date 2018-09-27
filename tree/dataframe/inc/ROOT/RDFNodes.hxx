5780975b7b6a4d6f3fa6d38897b8398e62d916b9
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-09-27 14:42:28 +0200

    [DF][NFC] Move RAction to its own header file

--- tree/dataframe/inc/ROOT/RDFNodes.hxx
+++ tree/dataframe/inc/ROOT/RDFNodes.hxx
@@ -13,6 +13,7 @@
 
 #include "ROOT/GraphNode.hxx"
 #include "ROOT/RActionBase.hxx"
+#include "ROOT/RDFAction.hxx"
 #include "ROOT/RDFColumnValue.hxx"
 #include "ROOT/RCustomColumnBase.hxx"
 #include "ROOT/RDataSource.hxx"
@@ -50,120 +51,6 @@ namespace GraphDrawing {
 std::shared_ptr<GraphNode>
 CreateDefineNode(const std::string &columnName, const RDFDetail::RCustomColumnBase *columnPtr);
 } // namespace GraphDrawing
-
-// fwd decl for RAction
-namespace GraphDrawing {
-bool CheckIfDefaultOrDSColumn(const std::string &name, const std::shared_ptr<RDFDetail::RCustomColumnBase> &column);
-} // ns GraphDrawing
-
-template <typename Helper, typename PrevDataFrame, typename ColumnTypes_t = typename Helper::ColumnTypes_t>
-class RAction final : public RActionBase {
-   using TypeInd_t = std::make_index_sequence<ColumnTypes_t::list_size>;
-
-   Helper fHelper;
-   const std::shared_ptr<PrevDataFrame> fPrevDataPtr;
-   PrevDataFrame &fPrevData;
-   std::vector<RDFValueTuple_t<ColumnTypes_t>> fValues;
-
-public:
-   RAction(Helper &&h, const ColumnNames_t &bl, std::shared_ptr<PrevDataFrame> pd,
-           const RBookedCustomColumns &customColumns)
-      : RActionBase(pd->GetLoopManagerUnchecked(), pd->GetLoopManagerUnchecked()->GetNSlots(), bl, customColumns),
-        fHelper(std::forward<Helper>(h)), fPrevDataPtr(std::move(pd)), fPrevData(*fPrevDataPtr), fValues(fNSlots)
-   {
-   }
-
-   RAction(const RAction &) = delete;
-   RAction &operator=(const RAction &) = delete;
-
-   void Initialize() final {
-      fHelper.Initialize();
-   }
-
-   void InitSlot(TTreeReader *r, unsigned int slot) final
-   {
-      for (auto &bookedBranch : fCustomColumns.GetColumns())
-         bookedBranch.second->InitSlot(r, slot);
-
-      InitRDFValues(slot, fValues[slot], r, fColumnNames, fCustomColumns, TypeInd_t());
-      fHelper.InitTask(r, slot);
-   }
-
-   void Run(unsigned int slot, Long64_t entry) final
-   {
-      // check if entry passes all filters
-      if (fPrevData.CheckFilters(slot, entry))
-         Exec(slot, entry, TypeInd_t());
-   }
-
-   template <std::size_t... S>
-   void Exec(unsigned int slot, Long64_t entry, std::index_sequence<S...>)
-   {
-      (void)entry; // avoid bogus 'unused parameter' warning in gcc4.9
-      fHelper.Exec(slot, std::get<S>(fValues[slot]).Get(entry)...);
-   }
-
-   void TriggerChildrenCount() final { fPrevData.IncrChildrenCount(); }
-
-   void FinalizeSlot(unsigned int slot) final
-   {
-      ClearValueReaders(slot);
-      for (auto &column : fCustomColumns.GetColumns()) {
-         column.second->ClearValueReaders(slot);
-      }
-      fHelper.CallFinalizeTask(slot);
-   }
-
-   void ClearValueReaders(unsigned int slot) { ResetRDFValueTuple(fValues[slot], TypeInd_t()); }
-
-   void Finalize() final
-   {
-      fHelper.Finalize();
-      fHasRun = true;
-   }
-
-   std::shared_ptr<RDFGraphDrawing::GraphNode> GetGraph()
-   {
-      auto prevNode = fPrevData.GetGraph();
-      auto prevColumns = prevNode->GetDefinedColumns();
-
-      // Action nodes do not need to ask an helper to create the graph nodes. They are never common nodes between
-      // multiple branches
-      auto thisNode = std::make_shared< RDFGraphDrawing::GraphNode>(fHelper.GetActionName());
-      auto evaluatedNode = thisNode;
-      for (auto &column : fCustomColumns.GetColumns()) {
-         /* Each column that this node has but the previous hadn't has been defined in between,
-          * so it has to be built and appended. */
-         if (RDFGraphDrawing::CheckIfDefaultOrDSColumn(column.first, column.second))
-            continue;
-         if (std::find(prevColumns.begin(), prevColumns.end(), column.first) == prevColumns.end()) {
-            auto defineNode = RDFGraphDrawing::CreateDefineNode(column.first, column.second.get());
-            evaluatedNode->SetPrevNode(defineNode);
-            evaluatedNode = defineNode;
-         }
-      }
-
-      thisNode->AddDefinedColumns(fCustomColumns.GetNames());
-      thisNode->SetAction(HasRun());
-      evaluatedNode->SetPrevNode(prevNode);
-      return thisNode;
-   }
-
-   /// This method is invoked to update a partial result during the event loop, right before passing the result to a
-   /// user-defined callback registered via RResultPtr::RegisterCallback
-   void *PartialUpdate(unsigned int slot) final { return PartialUpdateImpl(slot); }
-
-private:
-   // this overload is SFINAE'd out if Helper does not implement `PartialUpdate`
-   // the template parameter is required to defer instantiation of the method to SFINAE time
-   template <typename H = Helper>
-   auto PartialUpdateImpl(unsigned int slot) -> decltype(std::declval<H>().PartialUpdate(slot), (void *)(nullptr))
-   {
-      return &fHelper.PartialUpdate(slot);
-   }
-   // this one is always available but has lower precedence thanks to `...`
-   void *PartialUpdateImpl(...) { throw std::runtime_error("This action does not support callbacks yet!"); }
-};
 
 } // namespace RDF
 } // namespace Internal
