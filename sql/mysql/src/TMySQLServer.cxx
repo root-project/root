78135c396d163d8ea5ad8717951a9a348fb676d2
Sergey Linev <S.Linev@gsi.de>
2018-10-01 23:26:06 +0200

    mysql: use bool instead of my_bool type
    Fixes https://sft.its.cern.ch/jira/projects/ROOT/issues/ROOT-9624
    
    Description: https://bugs.mysql.com/bug.php?id=85131

--- sql/mysql/src/TMySQLServer.cxx
+++ sql/mysql/src/TMySQLServer.cxx
@@ -159,7 +159,7 @@ TMySQLServer::TMySQLServer(const char *db, const char *uid, const char *pw)
          if (opt.Contains("reconnect=")) {
            #if MYSQL_VERSION_ID >= 50013
             opt.Remove(0, 10);
-            my_bool reconnect_on = (opt=="1") || (opt=="true");
+            bool reconnect_on = (opt=="1") || (opt=="true");
             mysql_options(fMySQL, MYSQL_OPT_RECONNECT, (const char*) &reconnect_on);
             if (gDebug) Info("TMySQLServer","Set reconnect options %s", (reconnect_on ? "ON" : "OFF"));
            #else
