c59d9edcea17342afe56508ad721b0005d6e144e
Guilherme Amadio <amadio@cern.ch>
2018-10-01 09:59:58 +0200

    Remove obsolete etc/cmake/{FindROOT,RootUseFile}.cmake scripts
    These scripts were replaced by cmake/scripts/ROOTConfig.cmake.in
    and cmake/scripts/RootUseFile.cmake.in respectively.

--- etc/cmake/RootUseFile.cmake
+++
@@ -1,9 +0,0 @@
-#---Set Link and include directories--------------------------------------------------------------
-include_directories(${ROOT_INCLUDE_DIRS})
-link_directories(${ROOT_LIBRARY_DIR})
-
-#---Set Flags-------------------------------------------------------------------------------------
-add_definitions(${ROOT_DEFINITIONS})
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ROOT_CXX_FLAGS}")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ROOT_C_FLAGS}")
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${ROOT_fortran_FLAGS}")
