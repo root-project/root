584a35da549f50dd00b3d16a0f0c161dc100c13d
Enrico Guiraud <enrico.guiraud@cern.ch>
2018-10-03 10:19:55 +0200

    [DF][NFC] Update year range on copyright banner

--- tree/dataframe/inc/ROOT/RDFGraphUtils.hxx
+++ tree/dataframe/inc/ROOT/RDFGraphUtils.hxx
@@ -1,7 +1,7 @@
 // Author: Enrico Guiraud, Danilo Piparo, CERN, Massimo Tumolo Politecnico di Torino 08/2018
 
 /*************************************************************************
- * Copyright (C) 1995-2016, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
