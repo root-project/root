9d4c683e766c12db6de09e304ffe55678bd07e8a
Jakob Blomer <jblomer@cern.ch>
2018-10-04 16:32:33 +0200

    [Forest] Add initial source file skeleton

---
+++ tree/forest/v7/src/RTree.cxx
@@ -0,0 +1,58 @@
+/// \file RTree.cxx
+/// \ingroup Forest ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2018-10-04
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "ROOT/RTree.hxx"
+
+#include "ROOT/RTreeModel.hxx"
+#include "ROOT/RTreeStorage.hxx"
+
+#include <utility>
+
+ROOT::Experimental::Detail::RTree::RTree(std::shared_ptr<ROOT::Experimental::RTreeModel> model)
+   : fModel(model)
+   , fNentries(0)
+{
+}
+
+ROOT::Experimental::Detail::RTree::~RTree()
+{
+}
+
+
+ROOT::Experimental::RInputTree::RInputTree(
+   std::shared_ptr<ROOT::Experimental::RTreeModel> model,
+   std::unique_ptr<ROOT::Experimental::RTreeSource> source)
+   : ROOT::Experimental::Detail::RTree(model)
+   , fSource(std::move(source))
+{
+}
+
+
+ROOT::Experimental::RInputTree::~RInputTree()
+{
+}
+
+
+ROOT::Experimental::ROutputTree::ROutputTree(
+   std::shared_ptr<ROOT::Experimental::RTreeModel> model,
+   std::unique_ptr<ROOT::Experimental::RTreeSink> sink)
+   : ROOT::Experimental::Detail::RTree(model)
+   , fSink(std::move(sink))
+{
+}
+
+ROOT::Experimental::ROutputTree::~ROutputTree()
+{
+}
