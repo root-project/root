f47c3c62533e5ddab7a6bf52c961f39baecb1633
Jakob Blomer <jblomer@cern.ch>
2018-10-04 16:32:33 +0200

    [Forest] Add initial source file skeleton

---
+++ tree/forest/v7/inc/ROOT/RTreeStorage.hxx
@@ -0,0 +1,36 @@
+/// \file ROOT/RTreeStorage.hxx
+/// \ingroup Forest ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2018-07-19
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RTreeStorage
+#define ROOT7_RTreeStorage
+
+namespace ROOT {
+namespace Experimental {
+
+class RTreeStorage {
+};
+
+
+class RTreeSink : public RTreeStorage {
+};
+
+
+class RTreeSource : public RTreeStorage {
+};
+
+} // namespace Experimental
+} // namespace ROOT
+
+#endif
