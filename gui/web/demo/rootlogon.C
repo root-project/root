5af1965746bc4986eec3aafb9f8378e600f78688
Sergey Linev <S.Linev@gsi.de>
2018-10-10 15:47:02 +0200

    webgui6: remove no longer needed demos, most of graphical tutorials should work

--- gui/web/demo/rootlogon.C
+++
@@ -1,7 +0,0 @@
-{
-
-   printf("Running rootlogon.C for webgui \n");
-   gPluginMgr->AddHandler("TGuiFactory", "web2", "TWebGuiFactory", "WebGui", "TWebGuiFactory()");
-   gPluginMgr->AddHandler("TVirtualX", "web2", "TWebVirtualX", "WebGui", "TWebVirtualX(const char*,const char*)");
-   gEnv->SetValue("Gui.Factory", "web2");
-}
