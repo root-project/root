67f46f85c532295856150d1faded832c10b5f323
Vassil Vassilev <v.g.vassilev@gmail.com>
2018-10-14 16:47:28 +0100

    Move function inline-able definitions in the header file.
    The function bodies are simple enough and reasonable compiler should be able
    to inline them. The second benefit is that if they are in the header file
    they will be available to clad and it can compute automatically derivatives
    and gradients.

--- math/mathcore/inc/Math/PdfFuncMathCore.h
+++ math/mathcore/inc/Math/PdfFuncMathCore.h
@@ -37,6 +37,10 @@ Additional distributions are also provided by the
 #ifndef ROOT_Math_PdfFuncMathCore
 #define ROOT_Math_PdfFuncMathCore
 
+#include "Math/Math.h"
+#include "Math/SpecFuncMathCore.h"
+
+#include <limits>
 
 namespace ROOT {
 namespace Math {
@@ -75,7 +79,24 @@ namespace Math {
 
   */
 
-  double beta_pdf(double x, double a, double b);
+  inline double beta_pdf(double x, double a, double b) {
+    if (x < 0 || x > 1.0) return 0;
+    if (x == 0 ) {
+      // need this wor Windows
+      if (a < 1) return  std::numeric_limits<double>::infinity();
+      else if (a > 1) return  0;
+      else if ( a == 1) return b; // to avoid a nan from log(0)*0
+    }
+    if (x == 1 ) {
+      // need this wor Windows
+      if (b < 1) return  std::numeric_limits<double>::infinity();
+      else if (b > 1) return  0;
+      else if ( b == 1) return a; // to avoid a nan from log(0)*0
+    }
+    return std::exp( ROOT::Math::lgamma(a + b) - ROOT::Math::lgamma(a) - ROOT::Math::lgamma(b) +
+                     std::log(x) * (a -1.) + ROOT::Math::log1p(-x ) * (b - 1.) );
+  }
+
 
 
   /**
@@ -92,7 +113,16 @@ namespace Math {
 
   */
 
-  double binomial_pdf(unsigned int k, double p, unsigned int n);
+  inline double binomial_pdf(unsigned int k, double p, unsigned int n) {
+
+    if (k > n) {
+      return 0.0;
+    } else {
+      double coeff = ROOT::Math::lgamma(n+1) - ROOT::Math::lgamma(k+1) - ROOT::Math::lgamma(n-k+1);
+      return std::exp(coeff + k * std::log(p) + (n - k) * ROOT::Math::log1p(-p));
+    }
+  }
+
 
 
   /**
@@ -112,7 +142,17 @@ namespace Math {
 
   */
 
-  double negative_binomial_pdf(unsigned int k, double p, double n);
+  inline double negative_binomial_pdf(unsigned int k, double p, double n) {
+    // impelment in term of gamma function
+
+    if (n < 0)  return 0.0;
+    if (p < 0 || p > 1.0) return 0.0;
+
+    double coeff = ROOT::Math::lgamma(k+n) - ROOT::Math::lgamma(k+1.0) - ROOT::Math::lgamma(n);
+    return std::exp(coeff + n * std::log(p) + double(k) * ROOT::Math::log1p(-p));
+
+  }
+
 
 
 
@@ -129,7 +169,10 @@ namespace Math {
 
   */
 
-  double breitwigner_pdf(double x, double gamma, double x0 = 0);
+  inline double breitwigner_pdf(double x, double gamma, double x0 = 0) {
+    double gammahalf = gamma/2.0;
+    return gammahalf/(M_PI * ((x-x0)*(x-x0) + gammahalf*gammahalf));
+  }
 
 
 
@@ -151,7 +194,11 @@ namespace Math {
 
   */
 
-  double cauchy_pdf(double x, double b = 1, double x0 = 0);
+  inline double cauchy_pdf(double x, double b = 1, double x0 = 0) {
+
+    return b/(M_PI * ((x-x0)*(x-x0) + b*b));
+
+  }
 
 
 
@@ -171,7 +218,18 @@ namespace Math {
 
   */
 
-  double chisquared_pdf(double x, double r, double x0 = 0);
+  inline double chisquared_pdf(double x, double r, double x0 = 0) {
+
+    if ((x-x0) <  0) {
+      return 0.0;
+    }
+    double a = r/2 -1.;
+    // let return inf for case x  = x0 and treat special case of r = 2 otherwise will return nan
+    if (x == x0 && a == 0) return 0.5;
+
+    return std::exp ((r/2 - 1) * std::log((x-x0)/2) - (x-x0)/2 - ROOT::Math::lgamma(r/2))/2;
+
+  }
 
 
   /**
@@ -188,12 +246,12 @@ namespace Math {
 
   */
 
-  double crystalball_function(double x, double alpha, double n, double sigma, double x0 = 0);
+  inline double crystalball_function(double x, double alpha, double n, double sigma, double x0 = 0);
 
    /**
        pdf definition of the crystal_ball which is defined only for n > 1 otherwise integral is diverging
     */
-  double crystalball_pdf(double x, double alpha, double n, double sigma, double x0 = 0);
+  inline double crystalball_pdf(double x, double alpha, double n, double sigma, double x0 = 0);
 
   /**
 
@@ -210,7 +268,15 @@ namespace Math {
 
   */
 
-  double exponential_pdf(double x, double lambda, double x0 = 0);
+  inline double exponential_pdf(double x, double lambda, double x0 = 0) {
+
+    if ((x-x0) < 0) {
+      return 0.0;
+    } else {
+      return lambda * std::exp (-lambda * (x-x0));
+    }
+
+  }
 
 
 
@@ -230,7 +296,18 @@ namespace Math {
   */
 
 
-  double fdistribution_pdf(double x, double n, double m, double x0 = 0);
+  inline double fdistribution_pdf(double x, double n, double m, double x0 = 0) {
+
+    // function is defined only for both n and m > 0
+    if (n < 0 || m < 0)
+      return std::numeric_limits<double>::quiet_NaN();
+    if ((x-x0) < 0)
+      return 0.0;
+
+    return std::exp((n/2) * std::log(n) + (m/2) * std::log(m) + ROOT::Math::lgamma((n+m)/2) - ROOT::Math::lgamma(n/2) - ROOT::Math::lgamma(m/2)
+                    + (n/2 -1) * std::log(x-x0) - ((n+m)/2) * std::log(m +  n*(x-x0)) );
+
+   }
 
 
 
@@ -249,7 +326,25 @@ namespace Math {
 
   */
 
-  double gamma_pdf(double x, double alpha, double theta, double x0 = 0);
+  inline double gamma_pdf(double x, double alpha, double theta, double x0 = 0) {
+
+    if ((x-x0) < 0) {
+      return 0.0;
+    } else if ((x-x0) == 0) {
+
+      if (alpha == 1) {
+        return 1.0/theta;
+      } else {
+        return 0.0;
+      }
+
+    } else if (alpha == 1) {
+      return std::exp(-(x-x0)/theta)/theta;
+    } else {
+      return std::exp((alpha - 1) * std::log((x-x0)/theta) - (x-x0)/theta - ROOT::Math::lgamma(alpha))/theta;
+    }
+
+  }
 
 
 
@@ -269,7 +364,11 @@ namespace Math {
 
   */
 
-  double gaussian_pdf(double x, double sigma = 1, double x0 = 0);
+  inline double gaussian_pdf(double x, double sigma = 1, double x0 = 0) {
+
+    double tmp = (x-x0)/sigma;
+    return (1.0/(std::sqrt(2 * M_PI) * std::fabs(sigma))) * std::exp(-tmp*tmp/2);
+  }
 
    /**
 
@@ -288,7 +387,13 @@ namespace Math {
 
   */
 
-   double bigaussian_pdf(double x, double y, double sigmax = 1, double sigmay = 1, double rho = 0, double x0 = 0, double y0 = 0);
+  inline double bigaussian_pdf(double x, double y, double sigmax = 1, double sigmay = 1, double rho = 0, double x0 = 0, double y0 = 0) {
+    double u = (x-x0)/sigmax;
+    double v = (y-y0)/sigmay;
+    double c = 1. - rho*rho;
+    double z = u*u - 2.*rho*u*v + v*v;
+    return  1./(2 * M_PI * sigmax * sigmay * std::sqrt(c) ) * std::exp(- z / (2. * c) );
+  }
 
    /**
 
@@ -332,7 +437,15 @@ namespace Math {
 
   */
 
-  double lognormal_pdf(double x, double m, double s, double x0 = 0);
+  inline double lognormal_pdf(double x, double m, double s, double x0 = 0) {
+
+    if ((x-x0) <= 0) {
+      return 0.0;
+    } else {
+      double tmp = (std::log((x-x0)) - m)/s;
+      return 1.0 / ((x-x0) * std::fabs(s) * std::sqrt(2 * M_PI)) * std::exp(-(tmp * tmp) /2);
+    }
+   }
 
 
 
@@ -352,7 +465,12 @@ namespace Math {
 
   */
 
-  double normal_pdf(double x, double sigma =1, double x0 = 0);
+  inline double normal_pdf(double x, double sigma =1, double x0 = 0) {
+
+    double tmp = (x-x0)/sigma;
+    return (1.0/(std::sqrt(2 * M_PI) * std::fabs(sigma))) * std::exp(-tmp*tmp/2);
+
+  }
 
 
   /**
@@ -369,7 +487,18 @@ namespace Math {
 
   */
 
-  double poisson_pdf(unsigned int n, double mu);
+  inline double poisson_pdf(unsigned int n, double mu) {
+
+    if (n >  0)
+      return std::exp (n*std::log(mu) - ROOT::Math::lgamma(n+1) - mu);
+    else  {
+      //  when  n = 0 and mu = 0,  1 is returned
+      if (mu >= 0) return std::exp(-mu);
+      // return a nan for mu < 0 since it does not make sense
+      return std::log(mu);
+    }
+
+  }
 
 
 
@@ -388,7 +517,12 @@ namespace Math {
 
   */
 
-  double tdistribution_pdf(double x, double r, double x0 = 0);
+  inline double tdistribution_pdf(double x, double r, double x0 = 0) {
+
+    return (std::exp (ROOT::Math::lgamma((r + 1.0)/2.0) - ROOT::Math::lgamma(r/2.0)) / std::sqrt (M_PI * r))
+    * std::pow ((1.0 + (x-x0)*(x-x0)/r), -(r + 1.0)/2.0);
+
+   }
 
 
 
@@ -407,7 +541,17 @@ namespace Math {
 
   */
 
-  double uniform_pdf(double x, double a, double b, double x0 = 0);
+  inline double uniform_pdf(double x, double a, double b, double x0 = 0) {
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! when a=b
+
+    if ((x-x0) < b && (x-x0) >= a) {
+      return 1.0/(b - a);
+    } else {
+      return 0.0;
+    }
+
+  }
 
 
 
