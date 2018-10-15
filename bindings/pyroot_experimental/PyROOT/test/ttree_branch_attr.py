cde086acd0258968bcd46a762af5f6f7e5b1ef32
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-10-15 16:26:58 +0200

    [Exp PyROOT] Test branch-as-attribute syntax with TChains

--- bindings/pyroot_experimental/PyROOT/test/ttree_branch_attr.py
+++ bindings/pyroot_experimental/PyROOT/test/ttree_branch_attr.py
@@ -8,6 +8,9 @@ class TTreeBranchAttr(unittest.TestCase):
     """
     Test for the pythonization that allows to access top-level tree branches/leaves as attributes
     (i.e. `mytree.mybranch`)
+
+    Since this pythonization is common to TTree and its subclasses, TChain is
+    also tested here.
     """
 
     filename  = 'treebranchattr.root'
@@ -28,59 +31,70 @@ class TTreeBranchAttr(unittest.TestCase):
                          'RECREATE')
 
     # Helper
-    def get_file_and_tree(self):
+    def get_file_objects(self):
         f = ROOT.TFile(self.filename)
         t = f.Get(self.treename)
         # Prevent double deletion of the tree (Python and C++ TFile)
         SetOwnership(t, False)
 
-        # Read first entry
-        t.GetEntry(0)
+        c = ROOT.TChain(self.treename)
+        c.Add(self.filename)
+        c.Add(self.filename)
 
-        return f,t
+        # Read first entry
+        for ds in t,c:
+            ds.GetEntry(0)
+
+        return f,t,c
 
     # Tests
     def test_basic_type_branch(self):
-        f,t = self.get_file_and_tree()
+        f,t,c = self.get_file_objects()
 
-        self.assertEqual(t.floatb, self.more)
+        for ds in t,c:
+            self.assertEqual(ds.floatb, self.more)
 
     def test_array_branch(self):
-        f,t = self.get_file_and_tree()
+        f,t,c = self.get_file_objects()
 
-        a = t.arrayb
+        for ds in t,c:
+            a = ds.arrayb
         
-        for j in range(self.arraysize):
-            self.assertEqual(a[j], j)
+            for j in range(self.arraysize):
+                self.assertEqual(a[j], j)
 
     def test_vector_branch(self):
-        f,t = self.get_file_and_tree()
+        f,t,c = self.get_file_objects()
 
-        v = t.vectorb
+        for ds in t,c:
+            v = ds.vectorb
 
-        for j in range(self.arraysize):
-            self.assertEqual(v[j], j)
+            for j in range(self.arraysize):
+                self.assertEqual(v[j], j)
 
     def test_struct_branch(self):
-        f,t = self.get_file_and_tree()
+        f,t,c = self.get_file_objects()
 
-        ms = t.structb
+        for ds in t,c:
+            ms = ds.structb
 
-        self.assertEqual(ms.myint1, self.more)
-        self.assertEqual(ms.myint2, 0)
+            self.assertEqual(ms.myint1, self.more)
+            self.assertEqual(ms.myint2, 0)
 
     def test_struct_branch_leaflist(self):
-        f,t = self.get_file_and_tree()
+        f,t,c = self.get_file_objects()
 
-        self.assertEqual(t.myintll1, self.more)
-        self.assertEqual(t.myintll2, 0)
+        for ds in t,c:
+            self.assertEqual(ds.myintll1, self.more)
+            self.assertEqual(ds.myintll2, 0)
 
     def test_alias_branch(self):
-        f,t = self.get_file_and_tree()
+        f,t,c = self.get_file_objects()
 
-        t.SetAlias('myalias', 'floatb')
+        for ds in t,c:
+            ds.SetAlias('myalias', 'floatb')
 
-        self.assertEqual(t.myalias, t.floatb)
+            self.assertEqual(ds.myalias, ds.floatb)
 
 
 if __name__ == '__main__':
