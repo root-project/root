af95d808411ccfa4903e838b1651e531afbbc435
Sergey Linev <S.Linev@gsi.de>
2018-10-17 17:22:58 +0200

    webgui: introduce BrowserCreator and ChromeCreator
    This instances helps to launch and control browser instances, started by
    ROOT. Created handle can kill browser process when stopped. Also
    automatic directory remove can be implemented now correctly

--- gui/webdisplay/src/RWebDisplayHandle.cxx
+++ gui/webdisplay/src/RWebDisplayHandle.cxx
@@ -52,19 +52,239 @@ std::unique_ptr<ROOT::Experimental::RWebDisplayHandle::Creator> &ROOT::Experimen
 {
    auto &m = GetMap();
    auto search = m.find(name);
+   if (search == m.end()) {
+
+      if (libname == "ChromeCreator") {
+         m.emplace(name, std::make_unique<ChromeCreator>());
+      } else if (!libname.empty()) {
+         gSystem->Load(libname.c_str());
+      }
+
+      search = m.find(name); // try again
+   }
+
    if (search != m.end())
       return search->second;
-
-   if (!libname.empty()) {
-      gSystem->Load(libname.c_str());
-      search = m.find(name);
-      if (search != m.end())
-         return search->second;
-   }
 
    static std::unique_ptr<ROOT::Experimental::RWebDisplayHandle::Creator> dummy;
    return dummy;
 }
+
+namespace ROOT {
+namespace Experimental {
+
+class RWebBrowserHandle : public RWebDisplayHandle {
+
+#if !defined(_MSC_VER)
+   typedef pid_t browser_process_id;
+#else
+   typedef int browser_process_id;
+#endif
+   std::string fTmpDir;
+   bool fHasPid{false};
+   browser_process_id fPid;
+
+public:
+   RWebBrowserHandle(const std::string &url, const std::string &tmpdir) : RWebDisplayHandle(url), fTmpDir(tmpdir) {}
+
+   RWebBrowserHandle(const std::string &url, const std::string &tmpdir, browser_process_id pid)
+      : RWebDisplayHandle(url), fTmpDir(tmpdir), fHasPid(true), fPid(pid)
+   {
+   }
+
+   virtual ~RWebBrowserHandle()
+   {
+#if !defined(_MSC_VER)
+      if (fHasPid)
+         kill(fPid, SIGKILL);
+      if (!fTmpDir.empty())
+         gSystem->Exec(TString::Format("rm -rf %s", fTmpDir.c_str()));
+#else
+      if (fHasPid)
+         gSystem->Exec(TString::Format("taskkill /F /PID %d", fPid));
+      if (!fTmpDir.empty())
+         gSystem->Exec(TString::Format("rmdir /S /Q %s", fTmpDir.c_str()));
+#endif
+   }
+};
+
+} // namespace Experimental
+} // namespace ROOT
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+ROOT::Experimental::RWebDisplayHandle::BrowserCreator::BrowserCreator(bool dflt)
+{
+   if (!dflt) return;
+}
+
+
+void ROOT::Experimental::RWebDisplayHandle::BrowserCreator::TestProg(const std::string &nexttry, bool check_std_paths)
+{
+   if (nexttry.empty() || !fProg.empty())
+      return;
+
+   if (!gSystem->AccessPathName(nexttry.c_str(), kExecutePermission)) {
+      fProg = nexttry;
+      #ifdef R__MACOSX
+         fProg.ReplaceAll("%20"," ");
+      #endif
+      return;
+   }
+
+   if (!check_std_paths) return;
+
+#ifdef _MSC_VER
+   std::string ProgramFiles = gSystem->Getenv("ProgramFiles");
+   size_t pos = ProgramFiles.find(" (x86)");
+   if (pos != std::string::npos)
+      ProgramFiles.erase(pos, 6);
+   std::string ProgramFilesx86 = gSystem->Getenv("ProgramFiles(x86)");
+
+   if (!ProgramFiles.empty())
+      TestProg(ProgramFiles + nexttry, false);
+   if (!ProgramFilesx86.empty())
+      TestProg(ProgramFilesx86 + nexttry, false);
+#endif
+}
+
+
+std::unique_ptr<ROOT::Experimental::RWebDisplayHandle>
+ROOT::Experimental::RWebDisplayHandle::BrowserCreator::Make(THttpServer *serv, const std::string &url, bool batch, int width, int height)
+{
+   TString exec = batch ? fBatchExec.c_str() : fExec.c_str();
+
+   if (exec.Length() == 0)
+      return nullptr;
+
+   std::string swidth = std::to_string(width > 0 ? width : 800);
+   std::string sheight = std::to_string(height > 0 ? height : 600);
+   std::string rmdir;
+
+   exec.ReplaceAll("$url", url.c_str());
+   exec.ReplaceAll("$width", swidth.c_str());
+   exec.ReplaceAll("$height", sheight.c_str());
+
+   if (exec.Index("fork:") == 0) {
+      if (fProg.empty()) {
+         R__ERROR_HERE("WebDisplay") << "Fork instruction without executable";
+         return nullptr;
+      }
+
+      exec.Remove(0, 5);
+#if !defined(_MSC_VER)
+
+      std::unique_ptr<TObjArray> args(exec.Tokenize(" "));
+      if (!args || (args->GetLast()<=0)) {
+         R__ERROR_HERE("WebDisplay") << "Fork instruction is empty";
+         return nullptr;
+      }
+
+      std::vector<char *> argv;
+      argv.push_back((char *) fProg.c_str());
+      for (Int_t n = 0; n <= args->GetLast(); ++n)
+         argv.push_back((char *)args->At(n)->GetName());
+      argv.push_back(nullptr);
+
+      R__DEBUG_HERE("WebDisplay") << "Show web window in browser with posix_spawn:\n" << fProg << " " << exec;
+
+      pid_t pid;
+      int status = posix_spawn(&pid, argv[0], nullptr, nullptr, argv.data(), nullptr);
+      if (status != 0) {
+         R__ERROR_HERE("WebDisplay") << "Fail to launch " << argv[0];
+         return 0;
+      }
+
+      // add processid and rm dir
+
+      return std::make_unique<RWebBrowserHandle>(url, rmdir, pid);
+
+      // return win.AddProcId(batch_mode, key, std::string("pid:") + std::to_string((int)pid) + rmdir);
+
+#else
+      std::string tmp;
+      char c;
+      int pid;
+      if (!fProg.empty()) {
+         exec.Prepend(Form("wmic process call create \"%s", fProg.c_str()));
+      } else {
+         R__ERROR_HERE("WebDisplay") << "No Web browser found in Program Files!";
+         return nullptr;
+      }
+      exec.Append("\" | find \"ProcessId\" ");
+      TString process_id(gSystem->GetFromPipe(exec.Data()));
+      std::stringstream ss(process_id.Data());
+      ss >> tmp >> c >> pid;
+
+      // add processid and rm dir
+      return std::make_unique<RWebBrowserHandle>(url, rmdir, pid);
+
+      //return win.AddProcId(batch_mode, key, std::string("pid:") + std::to_string((int)pid) + rmdir);
+#endif
+   }
+
+   TString prog = fProg.c_str();
+
+#ifdef R__MACOSX
+   prog.ReplaceAll(" ", "\\ ");
+#endif
+
+#ifdef _MSC_VER
+   std::unique_ptr<TObjArray> args(exec.Tokenize(" "));
+   std::vector<char *> argv;
+   if (prog.EndsWith("chrome.exe"))
+      argv.push_back("chrome.exe");
+   else if (prog.EndsWith("firefox.exe"))
+      argv.push_back("firefox.exe");
+   for (Int_t n = 1; n <= args->GetLast(); ++n)
+      argv.push_back((char *)args->At(n)->GetName());
+   argv.push_back(nullptr);
+#endif
+
+   exec.ReplaceAll("$prog", prog.Data());
+
+   // unsigned connid = win.AddProcId(batch_mode, key, where + rmdir); // for now just application name
+
+   R__DEBUG_HERE("WebDisplay") << "NEW Showing web window in browser with:\n" << exec;
+
+#ifdef _MSC_VER
+   _spawnv(_P_NOWAIT, prog.Data(), argv.data());
+#else
+   gSystem->Exec(exec);
+#endif
+
+   // add rmdir if required
+   return std::make_unique<RWebBrowserHandle>(url, rmdir);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+ROOT::Experimental::RWebDisplayHandle::ChromeCreator::ChromeCreator()
+{
+   TestProg(gEnv->GetValue("WebGui.Chrome", ""));
+
+#ifdef _MSC_VER
+   TestProg("\\Google\\Chrome\\Application\\chrome.exe", true);
+#endif
+#ifdef R__MACOSX
+   TestProg("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+#endif
+#ifdef R__LINUX
+   TestProg("/usr/bin/chromium");
+   TestProg("/usr/bin/chromium-browser");
+   TestProg("/usr/bin/chrome-browser");
+#endif
+
+#ifdef _MSC_VER
+   fBatchExec = gEnv->GetValue("WebGui.ChromeBatch", "fork: --headless --disable-gpu $url");
+   fExec = gEnv->GetValue("WebGui.ChromeInteractive", "$prog --window-size=$width,$height --app=$url");
+#else
+   fBatchExec = gEnv->GetValue("WebGui.ChromeBatch", "fork:--headless $url");
+   fExec = gEnv->GetValue("WebGui.ChromeInteractive", "$prog --window-size=$width,$height --app=\'$url\' &");
+#endif
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /// checks if provided executable exists
@@ -173,6 +393,34 @@ unsigned ROOT::Experimental::RWebDisplayHandle::DisplayWindow(RWebWindow &win, b
    std::string swidth = std::to_string(win.GetWidth() ? win.GetWidth() : 800);
    std::string sheight = std::to_string(win.GetHeight() ? win.GetHeight() : 600);
    TString prog;
+
+   if ((kind == kNative) || (kind == kChrome)) {
+      auto &creator = FindCreator("chrome", "ChromeCreator");
+
+      if (creator) {
+
+         if (!win.fMgr->CreateServer(true)) {
+            R__ERROR_HERE("WebDisplay") << "Fail to start real HTTP server";
+            return 0;
+         }
+
+         addr = win.fMgr->GetServerAddr() + addr;
+
+         auto handle = creator->Make(win.fMgr->GetServer(), addr, batch_mode, win.GetWidth(), win.GetHeight());
+         if (!handle) {
+            R__ERROR_HERE("WebDisplay") << "Cannot create Qt5 Web window";
+            return 0;
+         }
+
+         return win.AddProcId(batch_mode, key, "chrome");
+      }
+
+      if (kind == kChrome) {
+         R__ERROR_HERE("WebDisplay") << "Qt5 libraries not found";
+         return 0;
+      }
+   }
+
 
    if ((kind == kNative) || (kind == kChrome)) {
       // see https://peter.sh/experiments/chromium-command-line-switches/
