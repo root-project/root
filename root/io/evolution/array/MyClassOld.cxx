c995cbd55e4410080268bdd1f30b913256bf8832
Danilo Piparo <danilo.piparo@cern.ch>
2018-10-18 15:37:08 +0200

    Adapt to the new warnings emitted by rootcling

--- root/io/evolution/array/MyClassOld.cxx
+++ root/io/evolution/array/MyClassOld.cxx
@@ -2,8 +2,8 @@
 #include "MyClass.cxx"
 
 #ifdef __MAKECINT__
-// to be included only if coompiling the new version of the class
+// to be included only if compiling the new version of the class
 #pragma read sourceClass="MyClass" targetClass="MyClass" \
-source="Int_t farray" version="[1]" target="fentries, farrayPointer" targetType="Int_t*, Int_t"\
+source="Int_t farray" version="[1]" target="fentries, farrayPointer" targetType="Int_t*, Int_t" \
 code="{fentries = 5; farrayPointer = new Int_t[fentries]; memcpy(farrayPointer, &(onfile.farray), fentries*sizeof(Int_t)); }"
 #endif
