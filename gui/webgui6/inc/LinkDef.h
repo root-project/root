16b9f7e9179239a7fd6c85d5c0525ca2f5bef4db
Sergey Linev <S.Linev@gsi.de>
2018-10-23 23:42:53 +0200

    webgui: introduce TWebPS class to redirect gVirtualPS calls
    Need to be used for 3D wrapping, where batch mode really used

--- gui/webgui6/inc/LinkDef.h
+++ gui/webgui6/inc/LinkDef.h
@@ -17,6 +17,7 @@
 #pragma link C++ class TWebGuiFactory+;
 #pragma link C++ class TWebCanvas+;
 #pragma link C++ class TWebPadPainter+;
+#pragma link C++ class TWebPS+;
 #pragma link C++ class TWebPainting+;
 #pragma link C++ class TWebPainterAttributes+;
 #pragma link C++ class TWebSnapshot+;
---
+++ gui/webgui6/src/TWebPS.cxx
@@ -0,0 +1,11 @@
+// Author:  Sergey Linev, GSI  23/10/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "TWebPS.h"
