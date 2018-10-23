57c53fb6fedba0334dfd063f05c40a8590cf5119
Philippe Canal <pcanal@fnal.gov>
2018-10-23 16:05:04 -0500

    Avoid setting fCollProxy multiple times.
    GetCollectionProxy during the setting of fCollProxy calls
    TBranchElement::GetInfoImp that in some cases sets fCollProxy
    and ends up recording it (sometimes) in the action sequence.
    When GetCollectionProxy sets it too (i.e. change it) there is
    now a disconnect between the branch and the action sequences that
    lead to the action sequence to used an unset collection proxy:
    
      Fatal in <TGenCollectionProxy>: Size> Logic error - no proxy object set.
      aborting

--- tree/tree/src/TBranchElement.cxx
+++ tree/tree/src/TBranchElement.cxx
@@ -2395,6 +2395,14 @@ TVirtualCollectionProxy* TBranchElement::GetCollectionProxy()
       } else {
          // We are not a top-level branch.
          TVirtualStreamerInfo* si = thiscast->GetInfoImp();
+         if (fCollProxy) {
+            // The GetInfo set fProxy for us, let's not
+            // redo it; the value of fCollProxy is possibly
+            // used/recorded is the actions sequences, so
+            // if we change it here, we would need to propagate
+            // the change.
+            return fCollProxy;
+         }
          TStreamerElement* se = si->GetElement(fID);
          cl = se->GetClassPointer();
       }
