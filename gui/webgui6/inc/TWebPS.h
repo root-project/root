8f8426de5eac3de2f190b87ab487b415ca7407fe
Sergey Linev <S.Linev@gsi.de>
2018-10-23 23:51:06 +0200

    webgui6: add attributes handling in TWebPS
    Simply recall TWebPadPainter class

--- gui/webgui6/inc/TWebPS.h
+++ gui/webgui6/inc/TWebPS.h
@@ -19,6 +19,52 @@ class TWebPS : public TVirtualPS {
    TWebPadPainter &fPainter;
 public:
    TWebPS(TWebPadPainter &p) : fPainter(p) {}
+
+   //Redirect calls to WebPainter
+   //Line attributes.
+   Color_t  GetLineColor() const { return fPainter.GetLineColor(); }
+   Style_t  GetLineStyle() const { return fPainter.GetLineStyle(); }
+   Width_t  GetLineWidth() const { return fPainter.GetLineWidth(); }
+
+   void     SetLineColor(Color_t lcolor) { fPainter.SetLineColor(lcolor); }
+   void     SetLineStyle(Style_t lstyle) { fPainter.SetLineStyle(lstyle); }
+   void     SetLineWidth(Width_t lwidth) { fPainter.SetLineWidth(lwidth); }
+
+   //Fill attributes.
+   Color_t  GetFillColor() const { return fPainter.GetFillColor(); }
+   Style_t  GetFillStyle() const { return fPainter.GetFillStyle(); }
+   Bool_t   IsTransparent() const { return fPainter.IsTransparent(); }
+
+   void     SetFillColor(Color_t fcolor)  { fPainter.SetFillColor(fcolor); }
+   void     SetFillStyle(Style_t fstyle) { fPainter.SetFillStyle(fstyle); }
+   void     SetOpacity(Int_t percent) { fPainter.SetOpacity(percent); }
+
+   //Text attributes.
+   Short_t  GetTextAlign() const { return fPainter.GetTextAlign(); }
+   Float_t  GetTextAngle() const { return fPainter.GetTextAngle(); }
+   Color_t  GetTextColor() const { return fPainter.GetTextColor(); }
+   Font_t   GetTextFont()  const { return fPainter.GetTextFont(); }
+   Float_t  GetTextSize()  const { return fPainter.GetTextSize(); }
+   Float_t  GetTextMagnitude() const { return fPainter.GetTextMagnitude(); }
+
+   void     SetTextAlign(Short_t align) { fPainter.SetTextAlign(align); }
+   void     SetTextAngle(Float_t tangle) { fPainter.SetTextAngle(tangle); }
+   void     SetTextColor(Color_t tcolor) { fPainter.SetTextColor(tcolor); }
+   void     SetTextFont(Font_t tfont) { fPainter.SetTextFont(tfont); }
+   void     SetTextSize(Float_t tsize) { fPainter.SetTextSize(tsize); }
+   void     SetTextSizePixels(Int_t npixels) { fPainter.SetTextSizePixels(npixels); }
+
+   //MISSING in base class - Marker attributes
+
+   Color_t   GetMarkerColor() const { return fPainter.GetMarkerColor(); }
+   Size_t    GetMarkerSize() const { return fPainter.GetMarkerSize(); }
+   Style_t   GetMarkerStyle() const { return fPainter.GetMarkerStyle(); }
+
+   void      SetMarkerColor(Color_t cindex) { fPainter.SetMarkerColor(cindex); }
+   void      SetMarkerSize(Float_t markersize) { fPainter.SetMarkerSize(markersize); }
+   void      SetMarkerStyle(Style_t markerstyle) { fPainter.SetMarkerStyle(markerstyle); }
+
+
    virtual void CellArrayBegin(Int_t W, Int_t H, Double_t x1, Double_t x2, Double_t y1, Double_t y2) {}
    virtual void CellArrayFill(Int_t r, Int_t g, Int_t b) {}
    virtual void CellArrayEnd()  {}
