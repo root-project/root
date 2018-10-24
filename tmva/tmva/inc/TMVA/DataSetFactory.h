925cebdf08a72b2f2314c40bf28457b87ca1d7f3
moneta <moneta@titanx.dyndns.cern.ch>
2018-10-24 18:42:55 +0200

    add new option for switching off  computation of correlation matrix

--- tmva/tmva/inc/TMVA/DataSetFactory.h
+++ tmva/tmva/inc/TMVA/DataSetFactory.h
@@ -273,7 +273,8 @@ namespace TMVA {
       TString                    fVerboseLevel;      // VerboseLevel
 
       // Printing
-      Bool_t fCorrelations; // Whether to print correlations or not
+      Bool_t fCorrelations = kFALSE;          // Whether to print correlations or not
+      Bool_t fComputeCorrelations = kFALSE;   // Whether to force computation of correlations or not
 
       Bool_t                     fScaleWithPreselEff; // how to deal with requested #events in connection with preselection cuts 
 
