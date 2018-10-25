efe5d14b8cf9782eba97aa0b5599e1c2c584d559
Vassil Vassilev <v.g.vassilev@gmail.com>
2018-10-25 15:22:39 +0200

    Inline crystalball funcs.

--- math/mathcore/inc/Math/PdfFuncMathCore.h
+++ math/mathcore/inc/Math/PdfFuncMathCore.h
@@ -244,12 +244,40 @@ namespace Math {
 
   */
 
-  inline double crystalball_function(double x, double alpha, double n, double sigma, double x0 = 0);
+  inline double crystalball_function(double x, double alpha, double n, double sigma, double mean = 0) {
+     // evaluate the crystal ball function
+     if (sigma < 0.)     return 0.;
+     double z = (x - mean)/sigma;
+     if (alpha < 0) z = -z;
+     double abs_alpha = std::abs(alpha);
+     // double C = n/abs_alpha * 1./(n-1.) * std::exp(-alpha*alpha/2.);
+     // double D = std::sqrt(M_PI/2.)*(1.+ROOT::Math::erf(abs_alpha/std::sqrt(2.)));
+     // double N = 1./(sigma*(C+D));
+     if (z  > - abs_alpha)
+        return std::exp(- 0.5 * z * z);
+     else {
+        //double A = std::pow(n/abs_alpha,n) * std::exp(-0.5*abs_alpha*abs_alpha);
+        double nDivAlpha = n/abs_alpha;
+        double AA =  std::exp(-0.5*abs_alpha*abs_alpha);
+        double B = nDivAlpha -abs_alpha;
+        double arg = nDivAlpha/(B-z);
+        return AA * std::pow(arg,n);
+     }
+   }
 
    /**
        pdf definition of the crystal_ball which is defined only for n > 1 otherwise integral is diverging
     */
-  inline double crystalball_pdf(double x, double alpha, double n, double sigma, double x0 = 0);
+  inline double crystalball_pdf(double x, double alpha, double n, double sigma, double mean = 0) {
+      // evaluation of the PDF ( is defined only for n >1)
+      if (sigma < 0.)     return 0.;
+      if ( n <= 1) return std::numeric_limits<double>::quiet_NaN();  // pdf is not normalized for n <=1
+      double abs_alpha = std::abs(alpha);
+      double C = n/abs_alpha * 1./(n-1.) * std::exp(-alpha*alpha/2.);
+      double D = std::sqrt(M_PI/2.)*(1.+ROOT::Math::erf(abs_alpha/std::sqrt(2.)));
+      double N = 1./(sigma*(C+D));
+      return N * crystalball_function(x,alpha,n,sigma,mean);
+   }
 
   /**
 
