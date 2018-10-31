ead1108832fef105a5f87a3b8c7a28678da08b73
Axel Naumann <Axel.Naumann@cern.ch>
2018-10-31 11:47:09 +0100

    Test ROOT-9738.

--- cling/specialobj/runf02.C
+++ cling/specialobj/runf02.C
@@ -3,5 +3,7 @@ int runf02(bool verbose = true) {
   gROOT->ProcessLine("if (true) UNKNOWN(42)"); // should complain about UNKNOWN
   // Test ROOT-7718: dynamic scope must repair array subscripts.
   gROOT->ProcessLine("ThisReallyDoesNotExist[42]"); // should complain about ThisReallyDoesNotExist
+  // Test ROOT-9738: dynamic scopes must repair binary ops
+  gROOT->ProcessLine("1?WhyAmI:0"); // should complain about WhyAmI
   return 0;
 }
