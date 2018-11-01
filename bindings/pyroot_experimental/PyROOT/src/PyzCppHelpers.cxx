0d8280c68a75ab5674665dbda5246fa710b89bba
Danilo Piparo <danilo.piparo@cern.ch>
2018-11-01 13:57:52 +0100

    [Exp PyROOT] Expose OP2TCLASS anf CallPyObjMethod helpers for pythonizations
    these are internal Cppyy utilities. In principle it would be better to
    expose them in cppyy and use them in PyROOT and not duplicate the code.
    This commit at least avoid to duplicate the code only once.

---
+++ bindings/pyroot_experimental/PyROOT/src/PyzCppHelpers.cxx
@@ -0,0 +1,29 @@
+// Author: Danilo Piparo CERN  08/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "PyzCppHelpers.hxx"
+
+#include "CPyCppyy.h"
+#include "CPPInstance.h"
+#include "TClass.h"
+
+PyObject *CallPyObjMethod(PyObject *obj, const char *meth, PyObject *arg1)
+{
+   // Helper; call method with signature: obj->meth(arg1).
+   Py_INCREF(obj);
+   PyObject *result = PyObject_CallMethod(obj, const_cast<char *>(meth), const_cast<char *>("O"), arg1);
+   Py_DECREF(obj);
+   return result;
+}
+
+TClass *OP2TCLASS(CPyCppyy::CPPInstance *pyobj)
+{
+   return TClass::GetClass(Cppyy::GetFinalName(pyobj->ObjectIsA()).c_str());
+}
