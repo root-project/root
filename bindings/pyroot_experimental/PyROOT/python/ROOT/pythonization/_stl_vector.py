dfa5ad9f9439d248fd2fcbf096ace26654ee5ff0
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-11-02 16:34:39 +0100

    Add missing headers to Exp PyROOT files

--- bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_stl_vector.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_stl_vector.py
@@ -1,3 +1,13 @@
+# Author: Stefan Wunsch CERN  08/2018
+
+################################################################################
+# Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.                      #
+# All rights reserved.                                                         #
+#                                                                              #
+# For the licensing terms see $ROOTSYS/LICENSE.                                #
+# For the list of contributors see $ROOTSYS/README/CREDITS.                    #
+################################################################################
+
 from ROOT import pythonization
 from ROOT.pythonization._rvec import add_array_interface_property
 
