0304a95289d11f8ffa299853341d87febf6badeb
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-11-02 16:34:39 +0100

    Add missing headers to Exp PyROOT files

--- bindings/pyroot_experimental/PyROOT/python/ROOT/__init__.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/__init__.py
@@ -1,3 +1,12 @@
+# Author: Enric Tejedor, Danilo Piparo CERN  06/2018
+
+################################################################################
+# Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.                      #
+# All rights reserved.                                                         #
+#                                                                              #
+# For the licensing terms see $ROOTSYS/LICENSE.                                #
+# For the list of contributors see $ROOTSYS/README/CREDITS.                    #
+################################################################################
 
 import cppyy
 import ROOT.pythonization as pyz
