322b07ff24932fb0e59114e30620e47501a84191
Danilo Piparo <danilo.piparo@cern.ch>
2018-11-09 10:25:19 +0100

    [IMT] Fix warning during dictionary generation in no-imt builds

--- core/imt/inc/LinkDef.h
+++ core/imt/inc/LinkDef.h
@@ -5,8 +5,11 @@
 #pragma link off all functions;
 
 // Only for the autoload, autoparse. No IO of these classes is foreseen!
+// Exclude in case ROOT does not have IMT support
+#ifdef R__USE_IMT
 #pragma link C++ class ROOT::Internal::TPoolManager-;
 #pragma link C++ class ROOT::TThreadExecutor-;
 #pragma link C++ class ROOT::Experimental::TTaskGroup-;
+#endif
 
 #endif
