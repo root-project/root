309f40a1ba2f1c57b73a9c37cb110560e629bcf4
Danilo Piparo <danilo.piparo@cern.ch>
2018-11-13 12:57:55 +0100

    [DF] Refactor signatures of RBookedCustomColumns

--- tree/dataframe/src/RDFBookedCustomColumns.cxx
+++ tree/dataframe/src/RDFBookedCustomColumns.cxx
@@ -4,23 +4,24 @@ namespace ROOT {
 namespace Internal {
 namespace RDF {
 
-bool RBookedCustomColumns::HasName(std::string name) const
+bool RBookedCustomColumns::HasName(std::string_view name) const
 {
-   return std::find(fCustomColumnsNames->begin(), fCustomColumnsNames->end(), name) != fCustomColumnsNames->end();
+   const auto ccolnamesEnd = fCustomColumnsNames->end();
+   return ccolnamesEnd != std::find(fCustomColumnsNames->begin(), ccolnamesEnd, name);
 }
 
 void RBookedCustomColumns::AddColumn(const std::shared_ptr<RDFDetail::RCustomColumnBase> &column,
-                                     const std::string_view &name)
+                                     std::string_view name)
 {
    auto newCols = std::make_shared<RCustomColumnBasePtrMap_t>(GetColumns());
    (*newCols)[std::string(name)] = column;
    fCustomColumns = newCols;
 }
 
-void RBookedCustomColumns::AddName(const std::string_view &name)
+void RBookedCustomColumns::AddName(std::string_view name)
 {
    auto newColsNames = std::make_shared<ColumnNames_t>(GetNames());
-   newColsNames->emplace_back(name);
+   newColsNames->emplace_back(std::string(name));
    fCustomColumnsNames = newColsNames;
 }
 
