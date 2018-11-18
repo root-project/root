b237f0b7fc258751d5d290375217250de45392dd
Jakob Blomer <jblomer@cern.ch>
2018-11-18 16:56:49 +0100

    [io] Add prototype implementation of RRawFilePosix

--- io/io/test/RRawFile.cxx
+++ io/io/test/RRawFile.cxx
@@ -1,12 +1,14 @@
 #include "RConfigure.h"
 #include "ROOT/RRawFile.hxx"
 
+#include <memory>
+
 #include "gtest/gtest.h"
 
 using namespace ROOT::Detail;
 
 TEST(RRawFile, CreateAndDestroy)
 {
-   RRawFile *f = RRawFile::Create("bla");
-   delete f;
+   std::unique_ptr<RRawFile> f(RRawFile::Create("file:///dev/null"));
+   EXPECT_EQ(0u, f->GetSize());
 }
