79e1db7ea02f1a3accebcf90651de2bde6df5047
Jakob Blomer <jblomer@cern.ch>
2018-11-20 18:54:46 +0100

    [io] Add C I/O implementation of RRawFile as initial Windows version

--- io/io/src/RRawFile.cxx
+++ io/io/src/RRawFile.cxx
@@ -157,10 +157,11 @@ std::uint64_t ROOT::Detail::RRawFilePosix::DoGetSize()
 {
    EnsureOpen();
    struct stat info;
+   info.st_size = 0;
    int res = fstat(filedes, &info);
-   if (res == 0)
-      return info.st_size;
-   throw std::runtime_error("Cannot call fstat on '" + fUrl + "', error: " + std::string(strerror(errno)));
+   if (res != 0)
+      throw std::runtime_error("Cannot call fstat on '" + fUrl + "', error: " + std::string(strerror(errno)));
+   return info.st_size;
 }
 
 
@@ -171,6 +172,64 @@ void ROOT::Detail::RRawFilePosix::EnsureOpen()
 
    filedes = open(GetLocation(fUrl).c_str(), O_RDONLY);
    if (filedes < 0) {
+      throw std::runtime_error("Cannot open '" + fUrl + "', error: " + std::string(strerror(errno)));
+   }
+}
+
+/******************************************************************************/
+
+ROOT::Detail::RRawFileCio::RRawFileCio(const std::string &url, ROOT::Detail::RRawFile::ROptions options)
+  : ROOT::Detail::RRawFile(url, options)
+  , fileptr(nullptr)
+{
+}
+
+ROOT::Detail::RRawFileCio::~RRawFileCio()
+{
+   if (fileptr != nullptr)
+      fclose(fileptr);
+}
+
+void ROOT::Detail::RRawFileCio::Seek(long offset, int whence)
+{
+   int res = fseek(fileptr, offset, whence);
+   if (res != 0)
+      throw std::runtime_error("Cannot seek in '" + fUrl + "', error: " + std::string(strerror(errno)));
+}
+
+size_t ROOT::Detail::RRawFileCio::DoPread(void *buffer, size_t nbytes, std::uint64_t offset)
+{
+   EnsureOpen();
+   if (offset != fFilePos)
+      Seek(offset, SEEK_SET);
+   size_t res = fread(buffer, 1, nbytes, fileptr);
+   if ((res < nbytes) && (ferror(fileptr) != 0)) {
+      clearerr(fileptr);
+      throw std::runtime_error("Cannot read from '" + fUrl + "', error: " + std::string(strerror(errno)));
+   }
+   return res;
+}
+
+
+std::uint64_t ROOT::Detail::RRawFileCio::DoGetSize()
+{
+   EnsureOpen();
+   Seek(0L, SEEK_END);
+   long size = ftell(fileptr);
+   if (size < 0)
+     throw std::runtime_error("Cannot tell position counter in '" + fUrl + "', error: " + std::string(strerror(errno)));
+   Seek(fFilePos, SEEK_SET);
+   return size;
+}
+
+
+void ROOT::Detail::RRawFileCio::EnsureOpen()
+{
+   if (fileptr != nullptr)
+      return;
+
+   fileptr = fopen(GetLocation(fUrl).c_str(), "r");
+   if (fileptr == nullptr) {
       throw std::runtime_error("Cannot open '" + fUrl + "', error: " + std::string(strerror(errno)));
    }
 }
