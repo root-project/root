f9cf832430e5f6ce99b0c67a9fbf7a0a1ca1c9f0
Olivier Couet <olivier.couet@gmail.com>
2018-11-29 16:59:17 +0100

    - spell check - white spaces

--- roofit/histfactory/src/hist2workspace.cxx
+++ roofit/histfactory/src/hist2workspace.cxx
@@ -1,5 +1,5 @@
 // @(#)root/roostats:$Id$
-// Author: Kyle Cranmer, George Lewis 
+// Author: Kyle Cranmer, George Lewis
 /*************************************************************************
  * Copyright (C) 1995-2008, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
@@ -38,7 +38,7 @@ namespace RooStats {
       // Fill them using the XML parser
       HistFactory::ConfigParser xmlParser;
       measurement_list = xmlParser.GetMeasurementsFromXML( input );
-	
+
       // At this point, we have all the information we need
       // from the xml files.
 
@@ -65,7 +65,7 @@ int main(int argc, char** argv) {
     std::cerr << "need input file" << std::endl;
     exit(1);
   }
-  
+
   if(argc==2){
     std::string input(argv[1]);
     if ( input == "-h" || input == "--help"){
@@ -89,7 +89,7 @@ int main(int argc, char** argv) {
         }
     }
   }
-  
+
   if(argc==3){
     std::string flag(argv[1]);
     std::string input(argv[2]);
@@ -111,9 +111,9 @@ int main(int argc, char** argv) {
 	exit(1);
       }
     }
-      
+
     else if(flag=="-number_counting_form") {
-      std::cout << "ERROR: 'number_counting_form' is now depricated." << std::endl;
+      std::cout << "ERROR: 'number_counting_form' is now deprecated." << std::endl;
       /*
       try {
 	//topDriver(input);
@@ -127,7 +127,7 @@ int main(int argc, char** argv) {
       */
       return 255;
     }
-    
+
     else {
       std::cerr << "Unrecognized flag.  " << std::endl;
       return 255;
