02e2f50fc782eb78dfb57a72e334ba0d13a215a4
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-12-03 14:38:11 +0100

    [Exp PyROOT] Add test for len in RooAbsCollection and subclasses

---
+++ bindings/pyroot_experimental/PyROOT/test/rooabscollection_len.py
@@ -0,0 +1,32 @@
+import unittest
+
+import ROOT
+
+
+class RooAbsCollectionLen(unittest.TestCase):
+    """
+    Test for the pythonization that allows to access the number of elements of a
+    RooAbsCollection (or subclass) by calling `len` on it.
+    """
+
+    num_elems = 3
+    rooabsarg_list = [ ROOT.RooStringVar(str(i),'','') for i in range(num_elems) ]
+    tlist = ROOT.TList()
+
+    # Setup
+    @classmethod
+    def setUpClass(cls):
+        for elem in cls.rooabsarg_list:
+            cls.tlist.Add(elem)
+
+    # Helpers
+    def check_len(self, c):
+        self.assertEqual(len(c), self.num_elems)
+        self.assertEqual(len(c), c.getSize())
+
+    # Tests
+    def test_rooarglist(self):
+        self.check_len(ROOT.RooArgList(self.tlist))
+
+    def test_rooargset(self):
+        self.check_len(ROOT.RooArgSet(self.tlist))
