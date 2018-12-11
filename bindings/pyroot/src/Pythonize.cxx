1cf031804fad0ee8895cc75b2f2b6f0e8473d3a0
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2018-12-11 15:51:24 +0100

    [PyROOT] Decrement reference for __repr__ method

--- bindings/pyroot/src/Pythonize.cxx
+++ bindings/pyroot/src/Pythonize.cxx
@@ -2275,7 +2275,9 @@ namespace {
       if (printResult.find("@0x") == 0) {
          // Fall back to __repr__ if we just get an address from cling
          auto method = PyObject_GetAttrString((PyObject*)self, "__repr__");
-         return PyObject_CallObject(method, nullptr);
+         auto res = PyObject_CallObject(method, nullptr);
+         Py_DECREF(method);
+         return res;
       } else {
          return PyROOT_PyUnicode_FromString(printResult.c_str());
       }
