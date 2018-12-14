21e74ff85148f89edd0508ea90d2d648067d735e
Sergey Linev <S.Linev@gsi.de>
2018-12-14 16:13:45 +0100

    eve7: create data for geometry hierarchical display

--- etc/eve7/Geom.controller.js
+++ etc/eve7/Geom.controller.js
@@ -12,9 +12,12 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
          
          console.log('GEOM CONTROLLER INIT');
          
-         console.log('USE CONNECTION', this.getView().getViewData().conn_handle);
+         this.websocket = this.getView().getViewData().conn_handle;
          
-         var data = {
+         this.websocket.SetReceiver(this);
+         this.websocket.Connect();
+         
+         this.data = {
                Nodes: [
                 {
                   title: "1",
@@ -31,9 +34,11 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
               ]
          };
          
+         this.descr = null; // description object from 
          
-         var model = new JSONModel(data);
-         this.getView().setModel(model);
+         
+         this.model = new JSONModel(this.data);
+         this.getView().setModel(this.model);
          
          // PART 2: instantiate Control and place it onto the page
 
@@ -46,7 +51,60 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
          
          this.getView().byId("mainSplitter").addContentArea(myControl);
       },
+      
+      /** Called when data comes via the websocket */
+      OnWebsocketMsg: function(handle, msg, offset) {
 
+         if (typeof msg != "string") {
+            // console.log('ArrayBuffer size ',
+            // msg.byteLength, 'offset', offset);
+            return;
+         }
+
+         console.log("msg len=", msg.length, " txt:", msg.substr(0,70), "...");
+
+         // this if first message
+         if (!this.descr) {
+            this.descr = JSROOT.parse(msg);
+            this.buildTree();
+            
+         } else {
+         }
+      },
+      
+      buildNode: function(indx) {
+         var node = this.tree_nodes[indx];
+         if (node) return node;
+         
+         node = { title: "any" };
+         this.tree_nodes[indx] = node;
+         return node;
+      },
+      
+      buildTree: function() {
+         if (!this.descr || !this.descr.fDesc) return;
+         
+         this.clones = new JSROOT.GEO.ClonedNodes(null, this.descr.fDesc);
+        
+         this.tree_nodes = [];
+         
+         this.data.Nodes = [ this.buildNode(0) ];
+         
+         console.log('data', this.data.Nodes);
+         
+         this.model.refresh();
+         
+      },
+
+      OnWebsocketClosed: function() {
+         // when connection closed, close panel as well
+         console.log('CLOSE WINDOW WHEN CONNECTION CLOSED');
+         
+         if (window) window.close();
+      },
+      
+      
+      
       onAfterRendering: function(){
       },
 
