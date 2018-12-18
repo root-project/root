4d419f7927405985c2666288a7da91746894bf46
Danilo Piparo <danilo.piparo@cern.ch>
2018-12-18 20:59:26 +0100

    [DF] Use spin mutex in RSlotStack instead of rw-spin lock
    This is a performance optimisation.
    We do not need a rw lock in this case because a write lock is needed
    both when a slot is taken out of the stack and when it's put back.
    The header of the class has also been made slimmer thanks to the usage
    of a fwd declaration.

--- tree/dataframe/inc/ROOT/RDF/RSlotStack.hxx
+++ tree/dataframe/inc/ROOT/RDF/RSlotStack.hxx
@@ -11,11 +11,11 @@
 #ifndef ROOT_RSLOTSTACK
 #define ROOT_RSLOTSTACK
 
-#include "ROOT/TRWSpinLock.hxx"
-
+#include <memory>
 #include <stack>
 
 namespace ROOT {
+class TSpinMutex;
 namespace Internal {
 namespace RDF {
 
@@ -27,7 +27,7 @@ class RSlotStack {
 private:
    const unsigned int fSize;
    std::stack<unsigned int> fStack;
-   ROOT::TRWSpinLock fRWLock;
+   std::unique_ptr<ROOT::TSpinMutex> fMutexPtr;
 
 public:
    RSlotStack() = delete;
