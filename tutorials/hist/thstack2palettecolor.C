1bd1fbd70301afffdcb4b7ab802fc3b9b0829f39
Olivier Couet <olivier.couet@cern.ch>
2018-12-20 15:01:29 +0100

    modernise example

--- tutorials/hist/thstack2palettecolor.C
+++ tutorials/hist/thstack2palettecolor.C
@@ -18,16 +18,16 @@
 
 void thstack2palettecolor () {
    gStyle->SetPalette(1);
-   TH2F * h1 = new TH2F("h1","h1",20,0,6,20,-4,4);
-   TH2F * h2 = new TH2F("h2","h1",20,0,6,20,-4,4);
-   TH2F * h3 = new TH2F("h3","h1",20,0,6,20,-4,4);
-   TH2F * h4 = new TH2F("h4","h1",20,0,6,20,-4,4);
-   TH2F * h5 = new TH2F("h5","h1",20,0,6,20,-4,4);
+   auto h1 = new TH2F("h1","h1",20,0,6,20,-4,4);
+   auto h2 = new TH2F("h2","h1",20,0,6,20,-4,4);
+   auto h3 = new TH2F("h3","h1",20,0,6,20,-4,4);
+   auto h4 = new TH2F("h4","h1",20,0,6,20,-4,4);
+   auto h5 = new TH2F("h5","h1",20,0,6,20,-4,4);
    h2->Fill(2.,0.,5);
    h3->Fill(3.,0.,10);
    h4->Fill(4.,0.,15);
    h5->Fill(5.,0.,20);
-   THStack * hs = new THStack("hs","Test of palette colored lego stack");
+   auto hs = new THStack("hs","Test of palette colored lego stack");
    hs->Add(h1);
    hs->Add(h2);
    hs->Add(h3);
