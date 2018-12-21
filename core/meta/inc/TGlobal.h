281a66eff18509607cf58c1c75dc342b5ffd0034
Sergey Linev <S.Linev@gsi.de>
2018-12-21 10:52:17 +0100

    TGlobalMappedFunciton: clang-format and nullptr

--- core/meta/inc/TGlobal.h
+++ core/meta/inc/TGlobal.h
@@ -32,7 +32,7 @@ private:
    DataMemberInfo_t  *fInfo;      //!pointer to CINT data member info
 
 public:
-   TGlobal(DataMemberInfo_t *info = 0);
+   TGlobal(DataMemberInfo_t *info = nullptr);
    TGlobal (const TGlobal &);
    TGlobal &operator=(const TGlobal &);
 
@@ -50,44 +50,42 @@ public:
    ClassDef(TGlobal,2)  //Global variable class
 };
 
-   //Class to map the "funcky" globals and be able to add them to the list of globals.
-   class TGlobalMappedFunction: public TGlobal {
-   public:
-      typedef void* (*GlobalFunc_t)();
-      typedef std::function<void*()> GlobalFunctor_t;
-      TGlobalMappedFunction(const char* name, const char* type,
-                            GlobalFunc_t funcPtr):fFuncPtr(funcPtr)
-      {
-         SetNameTitle(name, type);
-      }
-      TGlobalMappedFunction(const char* name, const char* type,
-                            void *dummyPtr, GlobalFunctor_t functor): fFuncPtr((GlobalFunc_t)dummyPtr), fFunctor(functor)
-      {
-         SetNameTitle(name, type);
-      }
-      virtual       ~TGlobalMappedFunction() {}
-      Int_t          GetArrayDim() const { return 0;}
-      DeclId_t       GetDeclId() const { return (DeclId_t)(fFuncPtr); } // Used as DeclId because of uniqueness
-      Int_t          GetMaxIndex(Int_t /*dim*/) const { return -1; }
-      void          *GetAddress() const { return !fFunctor ? (*fFuncPtr)() : fFunctor(); }
-      const char    *GetTypeName() const { return fTitle; }
-      const char    *GetFullTypeName() const { return fTitle; }
-      Long_t         Property() const { return 0; }
-      virtual bool   Update(DataMemberInfo_t * /*info*/) { return false; }
-      static  void   Add(TGlobalMappedFunction* gmf);
+// Class to map the "funcky" globals and be able to add them to the list of globals.
+class TGlobalMappedFunction : public TGlobal {
+public:
+   typedef void *(*GlobalFunc_t)();
+   typedef std::function<void *()> GlobalFunctor_t;
+   TGlobalMappedFunction(const char *name, const char *type, GlobalFunc_t funcPtr) : fFuncPtr(funcPtr)
+   {
+      SetNameTitle(name, type);
+   }
+   TGlobalMappedFunction(const char *name, const char *type, void *dummyPtr, GlobalFunctor_t functor)
+      : fFuncPtr((GlobalFunc_t)dummyPtr), fFunctor(functor)
+   {
+      SetNameTitle(name, type);
+   }
+   virtual ~TGlobalMappedFunction() = default;
+   Int_t GetArrayDim() const { return 0; }
+   DeclId_t GetDeclId() const { return (DeclId_t)(fFuncPtr); } // Used as DeclId because of uniqueness
+   Int_t GetMaxIndex(Int_t /*dim*/) const { return -1; }
+   void *GetAddress() const { return !fFunctor ? (*fFuncPtr)() : fFunctor(); }
+   const char *GetTypeName() const { return fTitle; }
+   const char *GetFullTypeName() const { return fTitle; }
+   Long_t Property() const { return 0; }
+   virtual bool Update(DataMemberInfo_t * /*info*/) { return false; }
+   static void Add(TGlobalMappedFunction *gmf);
 
-   private:
+private:
+   GlobalFunc_t fFuncPtr; // Function to call to get the address
 
-      GlobalFunc_t fFuncPtr; // Function to call to get the address
+   GlobalFunctor_t fFunctor; // functor which correctly returns pointer
 
-      GlobalFunctor_t fFunctor; // functor which correctly returns pointer
+   TGlobalMappedFunction &operator=(const TGlobal &); // not implemented.
+   // Some of the special ones are created before the list is create e.g gFile
+   // We need to buffer them.
+   static TList &GetEarlyRegisteredGlobals();
 
-      TGlobalMappedFunction &operator=(const TGlobal &); // not implemented.
-      // Some of the special ones are created before the list is create e.g gFile
-      // We need to buffer them.
-      static TList&  GetEarlyRegisteredGlobals();
-
-      friend class TROOT;
-   };
+   friend class TROOT;
+};
 
 #endif
