8144feafcb844977bcbbc2d8c006e76940fb3575
Danilo Piparo <danilo.piparo@cern.ch>
2018-12-21 14:13:14 +0100

    [RF][NFC] Correct the way in which authors are entered

--- tutorials/roofit/rf106_plotdecoration.py
+++ tutorials/roofit/rf106_plotdecoration.py
@@ -5,9 +5,7 @@
 ##
 ## \macro_code
 ##
-## \date February 2018
-## \author Clemens Lange
-## \author Wouter Verkerke (C++ version)
+## \author Clemens Lange, Wouter Verkerke (C++ version)
 
 import ROOT
 
