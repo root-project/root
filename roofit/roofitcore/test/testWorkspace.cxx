5928efb0105cdcb7d57b346ccca380b2f33c8a4a
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2019-01-08 08:51:27 +0100

    Add first RooWorkspace unit test.

---
+++ roofit/roofitcore/test/testWorkspace.cxx
@@ -0,0 +1,65 @@
+/// Tests for the RooWorkspace
+
+#include "RooGlobalFunc.h"
+#include "RooGaussian.h"
+#include "RooArgList.h"
+#include "RooRealVar.h"
+#include "RooAbsReal.h"
+#include "RooStats/ModelConfig.h"
+
+#include "TFile.h"
+
+#include "gtest/gtest.h"
+
+using namespace RooStats;
+
+/// ROOT-9777, cloning a RooWorkspace. The ModelConfig did not get updated
+/// when a workspace was cloned, and was hence pointing to a non-existing workspace.
+///
+TEST(RooWorkspace, CloneModelConfig_ROOT_9777)
+{
+   RooRealVar x("x", "x", 1, 0, 10);
+   RooRealVar mu("mu", "mu", 1, 0, 10);
+   RooRealVar sigma("sigma", "sigma", 1, 0, 10);
+
+   RooGaussian pdf("Gauss", "Gauss", x, mu, sigma);
+
+   {
+      TFile outfile("/tmp/ROOT-9777", "RECREATE");
+      
+      // now create the model config for this problem
+      RooWorkspace* w = new RooWorkspace("ws");
+      ModelConfig modelConfig("ModelConfig", w);
+      modelConfig.SetPdf(pdf);
+      modelConfig.SetParametersOfInterest(RooArgSet(sigma));
+      modelConfig.SetGlobalObservables(RooArgSet(mu));
+      w->import(modelConfig);
+
+      outfile.WriteObject(w, "ws");
+      delete w;
+   }
+   
+   RooWorkspace *w2;
+   {
+      TFile infile("/tmp/ROOT-9777", "READ");
+      RooWorkspace *w;
+      infile.GetObject("ws", w);
+      ASSERT_TRUE(w) << "Workspace not read from file.";
+
+      w2 = new RooWorkspace(*w);
+      delete w;
+   }
+   
+   w2->Print();
+
+   ModelConfig *mc = dynamic_cast<ModelConfig*>(w2->genobj("ModelConfig"));
+   ASSERT_TRUE(mc) << "ModelConfig not retrieved.";
+   mc->Print();
+
+   ASSERT_TRUE(mc->GetGlobalObservables()) << "GlobalObsevables in mc broken.";
+   mc->GetGlobalObservables()->Print();
+
+   ASSERT_TRUE(mc->GetParametersOfInterest()) << "ParametersOfInterest in mc broken.";
+   mc->GetParametersOfInterest()->Print();
+}
+
