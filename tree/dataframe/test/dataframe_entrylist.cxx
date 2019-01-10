a52f44c448e431ccf59f5f96964f79d7363cab5d
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-01-11 00:44:18 +0100

    [RDF] Add test for RDF and TEntryList
    Single and multi-threaded.

---
+++ tree/dataframe/test/dataframe_entrylist.cxx
@@ -0,0 +1,101 @@
+#include "ROOT/RDataFrame.hxx"
+#include "TChain.h"
+#include "TEntryList.h"
+#include "TFile.h"
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TTree.h"
+#include "gtest/gtest.h"
+
+#include <vector>
+
+// Write to disk file filename with TTree "t" with nEntries of branch "e" assuming integer values [0..nEntries).
+void MakeInputFile(const std::string &filename, int nEntries)
+{
+   const auto treename = "t";
+   auto d = ROOT::RDataFrame(nEntries)
+               .Define("e", [](ULong64_t e) { return int(e); }, {"rdfentry_"})
+               .Snapshot<int>(treename, filename, {"e"});
+}
+
+void TestTreeWithEntryList()
+{
+   const auto nEntries = 10;
+   const auto treename = "t";
+   const auto filename = "rdfentrylist.root";
+   MakeInputFile(filename, nEntries);
+
+   TEntryList elist("e", "e", treename, filename);
+   elist.Enter(0);
+   elist.Enter(nEntries - 1);
+
+   TFile f(filename);
+   TTree *t = nullptr;
+   f.GetObject(treename, t);
+   t->SetEntryList(&elist);
+
+   auto entries = ROOT::RDataFrame(*t).Take<int>("e");
+   EXPECT_EQ(*entries, std::vector<int>({0, nEntries - 1}));
+
+   gSystem->Unlink(filename);
+}
+
+void TestChainWithEntryList()
+{
+   const auto nEntries = 10;
+   const auto treename = "t";
+   const auto file1 = "rdfentrylist1.root";
+   MakeInputFile(file1, nEntries);
+   const auto file2 = "rdfentrylist2.root";
+   MakeInputFile(file2, nEntries);
+
+   TEntryList elist1("e", "e", treename, file1);
+   elist1.Enter(0);
+   elist1.Enter(2 * nEntries - 1);
+   TEntryList elist2("e", "e", treename, file2);
+   elist2.Enter(0);
+   elist2.Enter(2 * nEntries - 1);
+
+   // make a TEntryList that contains two TEntryLists in its list of TEntryLists,
+   // as required by TChain (see TEntryList's doc)
+   TEntryList elists;
+   elists.Add(&elist1);
+   elists.Add(&elist2);
+
+   TChain c(treename);
+   c.Add(file1, nEntries);
+   c.Add(file2, nEntries);
+   c.SetEntryList(&elists);
+
+   auto entries = ROOT::RDataFrame(c).Take<int>("e");
+   EXPECT_EQ(*entries, std::vector<int>({0, nEntries - 1, 0, nEntries - 1}));
+
+   gSystem->Unlink(file1);
+   gSystem->Unlink(file2);
+}
+
+TEST(RDFEntryList, Chain)
+{
+   TestChainWithEntryList();
+}
+
+TEST(RDFEntryList, Tree)
+{
+   TestTreeWithEntryList();
+}
+
+#ifdef R__USE_IMT
+TEST(RDFEntryList, ChainMT)
+{
+   ROOT::EnableImplicitMT();
+   TestChainWithEntryList();
+   ROOT::DisableImplicitMT();
+}
+
+TEST(RDFEntryList, TreeMT)
+{
+   ROOT::EnableImplicitMT();
+   TestTreeWithEntryList();
+   ROOT::DisableImplicitMT();
+}
+#endif
