4bdbcbc62a0c5e49b4e8c0432dc4d278bdd9b956
Sergey Linev <S.Linev@gsi.de>
2019-01-11 20:29:05 +0100

    jsroot: fixes in geo painter

--- etc/http/scripts/JSRootGeoBase.js
+++ etc/http/scripts/JSRootGeoBase.js
@@ -1662,7 +1662,7 @@
 
               matrix = new THREE.Matrix4();
 
-              switch (node.fFinder._typename.charAt[node.fFinder._typename.length-1]) {
+              switch (node.fFinder._typename[node.fFinder._typename.length-1]) {
                  case 'X': matrix.setPosition(new THREE.Vector3(_shift, 0, 0)); break;
                  case 'Y': matrix.setPosition(new THREE.Vector3(0, _shift, 0)); break;
                  case 'Z': matrix.setPosition(new THREE.Vector3(0, 0, _shift)); break;
