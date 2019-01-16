a7530fa23a226d1adb17e0ccb4053f44ec0907d8
Danilo Piparo <danilo.piparo@cern.ch>
2019-01-16 13:57:02 +0100

    [DF] Re-activate pyunittests for Cache
    this was possible given that now the invocation of templated methods
    is supported.
    The code of the tests was also improved.

--- tree/dataframe/test/dataframe_cache.py
+++ tree/dataframe/test/dataframe_cache.py
@@ -23,39 +23,44 @@ class Cache(unittest.TestCase):
       }'''
       ROOT.gInterpreter.Calc(code)
 
+    def tearDown(self):
+        ROOT.gSystem.Unlink("fileName.root")
+
     def test_TakeArrays(self):
         rdf = RDataFrame("t", "fileName.root")
         ColType_t = "ROOT::RVec<float>"
-        v = rdf.Take(ColType_t)("arr")
-        d = rdf.Take(ColType_t+", std::deque("+ColType_t+")")("arr")
+        vrp = rdf.Take(ColType_t)("arr")
+        drp = rdf.Take(ColType_t+", std::deque<"+ColType_t+">")("arr")
+        # Workaround until we do not understand why we cannot directly use the __getitem__ operator
+        v = vrp.GetValue()
+        d = drp.GetValue()
         # commented out until we do not understand iteration
         #l = rdf.Take(ColType_t+", std::list("+ColType_t+")")("arr")
 
-        ifloat = 0.
         for i in xrange(4):
-            vv = v[i];
-            dv = d[i];
+            vv = v[i]
+            dv = d[i]
             for j in xrange(4):
-                ref = ifloat + j;
-                self.assertEqual(ref, vv[j]);
-                self.assertEqual(ref, dv[j]);
+                ref = float(i + j);
+                self.assertEqual(ref, vv[j])
+                self.assertEqual(ref, dv[j])
 
     def test_Carrays(self):
        rdf = RDataFrame("t", "fileName.root")
        cache = rdf.Cache("arr")
        arr = cache.Take('ROOT::RVec<float>')("arr")
-       for e in arr:
+       for ievt, e in enumerate(arr):
            for i in xrange(4):
-               self.assertEqual(float(i), e[i]);
+               self.assertEqual(float(i + ievt), e[i])
 
     def test_EntryAndSlotColumns(self):
        rdf = RDataFrame(8)
        c = rdf.Filter("rdfentry_ % 2 == 0").Define("myEntry","rdfentry_").Cache()
-       ds_entries = c.Take('UInt64_t')('rdfentry_')
+       ds_entries = c.Take('ULong64_t')('rdfentry_')
        ref_ds_entries = [0,1,2,3]
        for e, eref in zip (ds_entries, ref_ds_entries):
            self.assertEqual(e, eref)
-       old_entries = c.Take('UInt64_t')('rdfentry_')
+       old_entries = c.Take('ULong64_t')('myEntry')
        ref_old_entries = [0,2,4,6]
        for e, eref in zip (old_entries, ref_old_entries):
            self.assertEqual(e, eref)
