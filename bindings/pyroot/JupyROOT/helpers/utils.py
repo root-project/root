ef618a942085e502922592e0c8ca4c5a86a620f4
Danilo Piparo <danilo.piparo@cern.ch>
2019-01-16 17:14:42 +0100

    [JupyROOT] Fix notebook execution failures on py3

--- bindings/pyroot/JupyROOT/helpers/utils.py
+++ bindings/pyroot/JupyROOT/helpers/utils.py
@@ -204,7 +204,7 @@ def _codeToFilename(code):
     >>> _codeToFilename("int f(i){return i*i;}")
     'dbf7e731.C'
     '''
-    fileNameBase = sha1(code.encode('utf-8')).hexdigest()[0:8]
+    fileNameBase = sha1(code).hexdigest()[0:8]
     return fileNameBase + ".C"
 
 def _dumpToUniqueFile(code):
@@ -215,7 +215,7 @@ def _dumpToUniqueFile(code):
     '''
     fileName = _codeToFilename(code)
     with open (fileName,'w') as ofile:
-      ofile.write(code)
+      ofile.write(code.decode('utf-8'))
     return fileName
 
 def isPlatformApple():
