2a188f03e3f8ba25641a7ef3c61c547d0282587f
Oksana Shadura <oksana.shadura@cern.ch>
2019-01-16 18:08:08 +0100

    Remove not used test (not used in CMake)

--- test/dt_wrap.C
+++
@@ -1,7 +0,0 @@
-R__LOAD_LIBRARY(dt_RunDrawTest.C+)
-
-void dt_wrap(const char* from, Int_t mode = 0, Int_t verboseLevel = 0) {
-   // gROOT->ProcessLine(".L dt_RunDrawTest.C+");
-   int status = !dt_RunDrawTest(from,mode,verboseLevel);
-   if (verboseLevel==0) gSystem->Exit(status);
-}
