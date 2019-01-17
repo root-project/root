e87c058ec497a8b574831c6a45a3d4774ff1b51d
Danilo Piparo <danilo.piparo@cern.ch>
2019-01-17 14:44:59 +0100

    [DF] Include explicitly TROOT.h in RDataFrame.hxx
    in order to allow to call ROOT::EnableImplicitMT() without including
    that header.

--- tree/dataframe/inc/ROOT/RDataFrame.hxx
+++ tree/dataframe/inc/ROOT/RDataFrame.hxx
@@ -16,6 +16,7 @@ ROOT's RDataFrame allows to analyse data stored in TTrees with a high level inte
 #ifndef ROOT_RDATAFRAME
 #define ROOT_RDATAFRAME
 
+#include "TROOT.h" // To allow ROOT::EnableImplicitMT without including ROOT.h
 #include "ROOT/RDF/RInterface.hxx"
 #include "ROOT/RDF/Utils.hxx"
 #include "ROOT/RStringView.hxx"
