0316f3f0d192875613705de3359957bf4e1a7dae
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-01-18 17:56:08 +0100

    [Exp PyROOT] Add Python-list-like pythonisations to TCollection

--- bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_tcollection.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_tcollection.py
@@ -1,7 +1,7 @@
-# Author: Enric Tejedor CERN  11/2018
+# Author: Enric Tejedor CERN  01/2019
 
 ################################################################################
-# Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.                      #
+# Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.                      #
 # All rights reserved.                                                         #
 #                                                                              #
 # For the licensing terms see $ROOTSYS/LICENSE.                                #
@@ -9,8 +9,47 @@
 ################################################################################
 
 from ROOT import pythonization
+from cppyy.gbl import TIter
 
 from ._generic import add_len
+
+
+# Python-list-like methods
+
+def remove_pyz(self, o):
+	# Parameters:
+    # self: collection
+    # o: object to remove from the collection
+	res = self.Remove(o)
+
+	if not res:
+		raise ValueError('list.remove(x): x not in list')
+
+def extend_pyz(self, c):
+	# Parameters:
+    # self: collection
+    # c: collection to extend self with
+	it = TIter(c)
+	o = it.Next()
+	while o:
+		self.Add(o)
+		o = it.Next()
+
+def count_pyz(self, o):
+	# Parameters:
+    # self: collection
+    # o: object to be counted in the collection
+	n = 0
+
+	it = TIter(self)
+	obj = it.Next()
+	while obj:
+		if obj == o:
+			n += 1
+		obj = it.Next()
+
+	return n
+
 
 @pythonization()
 def pythonize_tcollection(klass, name):
@@ -21,3 +60,9 @@ def pythonize_tcollection(klass, name):
     if name == 'TCollection':
         # Support `len(c)` as `c.GetEntries()`
         add_len(klass, 'GetEntries')
+
+        # Add Python lists methods
+        klass.append = klass.Add
+        klass.remove = remove_pyz
+        klass.extend = extend_pyz
+        klass.count = count_pyz
