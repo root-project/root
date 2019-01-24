6285a7ff57194cc621e546f2dd143d4afa25cf1e
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2019-01-24 14:55:02 +0100

    [RF] Fix rf511 python tutorial, remove unncessary lines in C++ version.

--- tutorials/roofit/rf511_wsfactory_basic.py
+++ tutorials/roofit/rf511_wsfactory_basic.py
@@ -56,6 +56,7 @@ else:
 
 # Make a dummy dataset p.d.f. 'model' and import it in the workspace
 data = w.pdf("model").generate(ROOT.RooArgSet(w.var("x")), 1000)
+# Cannot call 'import' directly because this is a python keyword:
 getattr(w, 'import')(data, ROOT.RooFit.Rename("data"))
 
 # Construct a KEYS p.d.f. passing a dataset name and an enum type defining the
@@ -64,10 +65,8 @@ getattr(w, 'import')(data, ROOT.RooFit.Rename("data"))
 # Workaround for pyROOT
 x = w.var("x")
 k = ROOT.RooKeysPdf("k", "k", x, data, ROOT.RooKeysPdf.NoMirror, 0.2)
-getattr(w, 'import')(k, ROOT.RooFit.Rename("k"))
+getattr(w, 'import')(k, ROOT.RooFit.RenameAllNodes("workspace"))
 
 # Print workspace contents
 w.Print()
 
-# Make workspace visible on command line
-ROOT.gDirectory.Add(w)
