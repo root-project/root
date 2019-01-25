0847a0d0b25bf9303e179c7079a740bdc889c347
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-01-25 14:13:31 +0100

    [Exp PyROOT] Make sure cppyy finds libraries if no env is set
    In macOS, SIP can prevent the setting of DYLD_LIBRARY_PATH from
    having any effect in the Python process. In that scenario,
    we need a fallback mechanism for cppyy to find libcppyy_backend
    and the libraries it depends on.
    
    This commit sets CPPYY_BACKEND_LIBRARY, which will be used by
    cppyy to dlopen libcppyy_backend, in absence of LD_LIBRARY_PATH
    and DYLD_LIBRARY_PATH.

--- bindings/pyroot_experimental/PyROOT/python/ROOT/__init__.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/__init__.py
@@ -8,6 +8,14 @@
 # For the list of contributors see $ROOTSYS/README/CREDITS.                    #
 ################################################################################
 
+# macOS SIP can prevent DYLD_LIBRARY_PATH from having any effect.
+# Set cppyy env variable here to make sure libraries are found.
+from os import environ, path
+if not any(var in environ for var in ('LD_LIBRARY_PATH','DYLD_LIBRARY_PATH')):
+    _lib_dir = path.dirname(path.dirname(__file__))
+    _lcb_path = path.join(_lib_dir, 'libcppyy_backend')
+    environ['CPPYY_BACKEND_LIBRARY'] = _lcb_path
+
 import cppyy
 import ROOT.pythonization as pyz
 
