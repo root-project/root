5cc2e2176a33d5d3eaa8ba3d4d81dc9a5753f586
Jakob Blomer <jblomer@cern.ch>
2019-01-29 15:11:24 +0100

    [forest] Write forest and field headers

--- tree/forest/v7/inc/ROOT/RTreeModel.hxx
+++ tree/forest/v7/inc/ROOT/RTreeModel.hxx
@@ -58,6 +58,8 @@ public:
 
    /// Mounts an existing model as a sub tree, which allows for composing of tree models
    std::shared_ptr<RTreeValueCollection> TreeFieldCollection(std::string_view fieldName, std::shared_ptr<RTreeModel> subModel);
+
+   const RTreeFieldCollection& GetRootField() const { return fRootField; }
 };
 
 } // namespace Exerimental
