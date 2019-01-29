da2e218c558fa8a9912f505086d71637ce1d8895
Sergey Linev <S.Linev@gsi.de>
2019-01-29 13:34:45 +0100

    meta: use functor for extracting pointer on TIterpreter::Instance

--- core/meta/src/TInterpreter.cxx
+++ core/meta/src/TInterpreter.cxx
@@ -18,10 +18,28 @@ interpreter.
 
 #include "TROOT.h"
 #include "TError.h"
+#include "TGlobal.h"
 
-TInterpreter*   (*gPtr2Interpreter)() = 0; // returns pointer to global object
-TInterpreter*   gCling = 0; // returns pointer to global TCling object
-static TInterpreter *gInterpreterLocal = 0; // The real holder of the pointer.
+
+TInterpreter*   (*gPtr2Interpreter)() = nullptr; // returns pointer to global object, NOT USED!
+TInterpreter*   gCling = nullptr; // returns pointer to global TCling object
+static TInterpreter *gInterpreterLocal = nullptr; // The real holder of the pointer.
+
+
+namespace {
+static struct AddPseudoGlobals {
+AddPseudoGlobals() {
+
+   // use special functor to extract pointer on gInterpreterLocal variable
+   TGlobalMappedFunction::MakeFunctor("gInterpreter", "TInterpreter*", TInterpreter::Instance, [] {
+      TInterpreter::Instance();
+      return (void *) &gInterpreterLocal;
+   });
+
+}
+} gAddPseudoGlobals;
+}
+
 
 ClassImp(TInterpreter);
 
