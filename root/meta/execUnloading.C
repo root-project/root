5b4ef526779f1f6a0eaedc7f5e32393c4c8b876b
Axel Naumann <Axel.Naumann@cern.ch>
2019-02-05 15:19:40 +0100

    Be more verbose about what is an error; output formatting.

--- root/meta/execUnloading.C
+++ root/meta/execUnloading.C
@@ -14,14 +14,14 @@ void execUnloading(){
    if (g->IsValid()) {
       cout << "<<i>> is valid.\n";
    } else {
-      cerr << "<<i>> is declared, but not valid.\n";
+      cerr << "ERROR: <<i>> is declared, but not valid.\n";
    }
    gROOT->ProcessLine(".undo 1");
    cout<< "Check validity after unloading, expected: false.\n";
    if (!g->IsValid()) {
       cout << "<<i>> unloaded.\n";
    } else {
-      cerr << "<<i>> is unloaded, but still valid.\n";
+      cerr << "ERROR: <<i>> is unloaded, but still valid.\n";
    }
    // Reload the object.
    gROOT->ProcessLine("int i;");
@@ -29,39 +29,38 @@ void execUnloading(){
    if (g->IsValid()) {
       cout << "<<i>> is valid after reloading.\n";
    } else {
-      cerr << "<<i>> is relaoded, but not valid.\n";
+      cerr << "ERROR: <<i>> is reloaded, but not valid.\n";
    }
    cout << "Check that the same obejct is used and not another created.\n";
    TGlobal* gReload = (TGlobal*)gROOT->GetListOfGlobals()->FindObject("i");
    if (g != gReload) {
-      cerr << "The object " << g->GetName()
+      cerr << "ERROR: The object " << g->GetName()
            << " is not reloaded, but created again!";
    }
 
-
    // ENUMS
    cout<< "\nUnloading of enum: EnumDecl and implicitly EnumConstDecl.\n";
-   TEnum* e;
+
    gROOT->ProcessLine("enum MyEnum{c1,c2};");
-   e = (TEnum*)gROOT->GetListOfEnums()->FindObject("MyEnum");
+   TEnum* e = (TEnum*)gROOT->GetListOfEnums()->FindObject("MyEnum");
    cout << "Check validity before unloading, expected: true.\n";
    if (e->IsValid()) {
       cout << "<<MyEnum>> is valid.\n";
    } else {
-      cerr << "<<MyEnum>> is declared, but not valid.\n";
+      cerr << "ERROR: <<MyEnum>> is declared, but not valid.\n";
    }
    gROOT->ProcessLine(".undo 1");
    // Verify the enum constants are invalidated from the list of globals.
    TGlobal* enumConstVar = (TGlobal*)gROOT->GetListOfGlobals()->FindObject("c1");
    if (enumConstVar != NULL) {
-      cerr << "The unloaded enum constant " << enumConstVar->GetName()
-           << "should not be in the list of globals.\n";
+      cerr << "ERROR: The unloaded enum constant " << enumConstVar->GetName()
+           << " should not be in the list of globals.\n";
    }
    cout << "Check validity after unloading, expected: false.\n";
    if (!e->IsValid()) {
       cout << "<<MyEnum>> unloaded.\n";
    } else {
-      cerr << "<<MyEnum>> is unloaded, but still valid.\n";
+      cerr << "ERROR: <<MyEnum>> is unloaded, but still valid.\n";
    }
    // Reload the object.
    gROOT->ProcessLine("enum MyEnum{c1,c2};");
@@ -69,13 +68,13 @@ void execUnloading(){
    if (e->IsValid()) {
       cout << "<<MyEnum>> is valid after reloading.\n";
    } else {
-      cerr << "<<MyEnum>> is relaoded, but not valid.\n";
+      cerr << "ERROR: <<MyEnum>> is reloaded, but not valid.\n";
    }
    // Check that the same obejct is used and not another created.
    TEnum* eReload = (TEnum*)gROOT->GetListOfEnums()->FindObject("MyEnum");
    if (e != eReload) {
-      cerr << "The object " << e->GetName()
-           << "is not reloaded, but created again!\n";
+      cerr << "ERROR: The object " << e->GetName()
+           << " is not reloaded, but created again!\n";
    }
 
 
@@ -90,14 +89,14 @@ void execUnloading(){
    if (f->IsValid()) {
       cout << "<<func>> is valid.\n";
    } else {
-      cerr << "<<func>> is declared, but not valid.\n";
+      cerr << "ERROR: <<func>> is declared, but not valid.\n";
    }
    gROOT->ProcessLine(".undo 1");
    cout << "Check validity after unloading, expected: false.\n";
    if (!f->IsValid()) {
       cout << "<<func>> unloaded.\n";
    } else {
-      cerr << "<<func>> is unloaded, but still valid.\n";
+      cerr << "ERROR: <<func>> is unloaded, but still valid.\n";
    }
    // Reload the object.
    gROOT->ProcessLine(".rawInput 1");
@@ -107,12 +106,12 @@ void execUnloading(){
    if (f->IsValid()) {
       cout << "<<func>> is valid after reloading.\n";
    } else {
-      cerr << "<<func>> is relaoded, but not valid.\n";
+      cerr << "ERROR: <<func>> is relaoded, but not valid.\n";
    }
    // Check that the same obejct is used and not another created.
    TFunction* fReload = (TFunction*)gROOT->GetListOfGlobalFunctions()->FindObject("func");
    if (f != fReload) {
-      cerr << "The object is not reloaded, but created again!\n";
+      cerr << "ERROR: The object is not reloaded, but created again!\n";
    }
 
    // CLASSES
@@ -127,13 +126,13 @@ void execUnloading(){
    if (dm->IsValid()) {
       cout << "<<data>> is valid.\n";
    } else {
-      cerr << "<<data>> is declared, but not valid.\n";
+      cerr << "ERROR: <<data>> is declared, but not valid.\n";
    }
    TMethod* m = (TMethod*)TClass::GetClass("MyClass")->GetListOfMethods()->FindObject("method");
    if (m->IsValid()) {
       cout << "<<method>> is valid.\n";
    } else {
-      cerr << "<<method>> is declared, but not valid.\n";
+      cerr << "ERROR: <<method>> is declared, but not valid.\n";
    }
    gROOT->ProcessLine(".undo 1");
    cout << "Check state of class, expected: \"1\" (kForwardDeclared).\n";
@@ -143,22 +142,22 @@ void execUnloading(){
    if (!dm->IsValid()) {
       cout << "<<data>> unloaded.\n";
    } else {
-      cerr << "<<data>> is unloaded, but still valid.\n";
+      cerr << "ERROR: <<data>> is unloaded, but still valid.\n";
    }
    if (!m->IsValid()) {
       cout << "<<method>> unloaded.\n";
    } else {
-      cerr << "<<method>> is unloaded, but still valid.\n";
+      cerr << "ERROR: <<method>> is unloaded, but still valid.\n";
    }
    TDataMember* unloadedDM = (TDataMember*)TClass::GetClass("MyClass")->GetListOfDataMembers()->FindObject("data");
    if (unloadedDM) {
-      cerr << "The unloaded data member " << unloadedDM->GetName()
-           << "should not be in the list of data members.\n";
+      cerr << "ERROR: The unloaded data member " << unloadedDM->GetName()
+           << " should not be in the list of data members.\n";
    }
    TMethod* unloadedM = (TMethod*)TClass::GetClass("MyClass")->GetListOfMethods()->FindObject("method");
    if (unloadedM) {
-      cerr << "The unloaded function member " << unloadedM->GetName()
-           << "should not be in the list of data members.\n";
+      cerr << "ERROR: The unloaded function member " << unloadedM->GetName()
+           << " should not be in the list of data members.\n";
    }
 
    // NAMESPACES
@@ -170,19 +169,19 @@ void execUnloading(){
    if (ndm->IsValid()) {
       cout << "<<ns::k>> is valid.\n";
    } else {
-      cerr << "<<ns::k>> is declared, but not valid.\n";
+      cerr << "ERROR: <<ns::k>> is declared, but not valid.\n";
    }
    gROOT->ProcessLine(".undo 1");
    cout << "Check validity after unloading, expected: false.\n";
    if (!ndm->IsValid()) {
       cout << "<<ns::k>> unloaded.\n";
    } else {
-      cerr << "<<ns::k>> is unloaded, but still valid.\n";
+      cerr << "ERROR: <<ns::k>> is unloaded, but still valid.\n";
    }
    TDataMember* unloadedNDM = (TDataMember*)TClass::GetClass("ns")->GetListOfDataMembers()->FindObject("k");
    if (unloadedNDM) {
-      cerr << "The unloaded namespace data member " << unloadedNDM->GetName()
-           << "should not be in the list of data members.\n";
+      cerr << "ERROR: The unloaded namespace data member " << unloadedNDM->GetName()
+           << " should not be in the list of data members.\n";
    }
 
 }
