e132d71b074c5e2daeb4a67b23d84ac974353f8b
Guilherme Amadio <amadio@cern.ch>
2019-02-06 11:10:04 +0100

    io: add GetMergeOptions()/SetMergeOptions() to TBufferMerger
    This is to allow the user to set the merging options to avoid
    recompression of input data into the output file when the
    compression algorithm or level are different.
    
    ROOT-9779 #resolve

--- io/io/src/TBufferMerger.cxx
+++ io/io/src/TBufferMerger.cxx
@@ -82,9 +82,20 @@ size_t TBufferMerger::GetAutoSave() const
    return fAutoSave;
 }
 
+const char *TBufferMerger::GetMergeOptions()
+{
+   return fMerger.GetMergeOptions();
+}
+
+
 void TBufferMerger::SetAutoSave(size_t size)
 {
    fAutoSave = size;
+}
+
+void TBufferMerger::SetMergeOptions(const TString& options)
+{
+   fMerger.SetMergeOptions(options);
 }
 
 void TBufferMerger::Merge()
