920172c642549a5f572a3852b4466c345939ed89
Guilherme Amadio <amadio@cern.ch>
2019-02-07 16:53:32 +0100

    cmake: remove CILIBS/CMAKE_M_LIBS/XLIBS variables (set but unused)

--- cmake/modules/SetUpLinux.cmake
+++ cmake/modules/SetUpLinux.cmake
@@ -86,10 +86,7 @@ else()
 endif()
 
 set(SYSLIBS "-lm -ldl ${CMAKE_THREAD_LIBS_INIT} -rdynamic")
-set(XLIBS "${XPMLIBDIR} ${XPMLIB} ${X11LIBDIR} -lXext -lX11")
-set(CILIBS "-lm -ldl -rdynamic")
 set(CRYPTLIBS "-lcrypt")
-set(CMAKE_M_LIBS -lm)
 # JIT must be able to resolve symbols from all ROOT binaries.
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -rdynamic")
 
