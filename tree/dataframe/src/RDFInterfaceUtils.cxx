8de1ea665eb923c78666d899f54ccd0a593bce26
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-02-09 15:50:15 +0100

    [DF] Support for jitted expressions with return and no ending ';'

--- tree/dataframe/src/RDFInterfaceUtils.cxx
+++ tree/dataframe/src/RDFInterfaceUtils.cxx
@@ -576,9 +576,9 @@ BuildLambdaString(const std::string &expr, const ColumnNames_t &vars, const Colu
       ss.seekp(-2, ss.cur);
 
    if (hasReturnStmt)
-      ss << "){\n" << expr << "\n}";
+      ss << "){\n" << expr << ";\n}";
    else
-      ss << "){return " << expr << "\n;}";
+      ss << "){return " << expr << ";\n}";
 
    return ss.str();
 }
