075f678f91a161325f6ccdd7542130aa96a91d89
Oksana Shadura <oksana.shadura@cern.ch>
2019-02-11 09:11:03 +0100

    Add custom add_subdirectory wrapper for ROOT subdirectories

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -43,6 +43,42 @@ endif()
 
 include(CMakeParseArguments)
 
+#----------------------------------------------------------------------------
+# canonicalize_tool_name(name output)
+#----------------------------------------------------------------------------
+# Make sure that we have a generic lower-case name of the module as an input.
+#
+function(canonicalize_dir_name name output)
+   string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "" nameStrip ${name})
+   string(REPLACE "-" "_" nameUNDERSCORE ${nameStrip})
+   string(TOLOWER ${nameUNDERSCORE} nameLOWER)
+   set(${output} "${nameLOWER}" PARENT_SCOPE)
+endfunction(canonicalize_dir_name)
+
+#----------------------------------------------------------------------------
+# add_root_subdirectory(name)
+#----------------------------------------------------------------------------
+# Custom add_subdirectory wrapper.
+# Takes in as parameter the subdirectory name.
+#
+function(add_subdirectory name)
+   # Make sure that we have a generic lower-case name of the module as an input
+   canonicalize_dir_name(${name} nameLOWER)
+   if(IS_ABSOLUTE ${name})
+      if(EXISTS "${name}")
+         _add_subdirectory(${name} ${ARGN})
+      else()
+         message(WARNING "Missing directory in add_subdirectory(${name})")
+      endif()
+   else()
+      if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${name}/CMakeLists.txt)
+         _add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/${name} ${ARGN})
+      else()
+         message(WARNING "Missing directory in add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/${name})")
+      endif()
+   endif()
+endfunction()
+
 #---------------------------------------------------------------------------------------------------
 #---ROOT_GLOB_FILES( <variable> [REALTIVE path] [FILTER regexp] <sources> ...)
 #---------------------------------------------------------------------------------------------------
