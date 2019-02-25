8f0e27eb6d40cac6c3c1e4df23c0f4d5ddf6aa4f
Wolf Behrenhoff <wolf@behrenhoff.de>
2019-02-25 11:30:37 +0100

    Do not crash when invalid TFormula is given
    Also add test cases for invalid formulae. And fix |= to &= - better all
    tests be successful instead of any one.

--- hist/hist/src/TFormula.cxx
+++ hist/hist/src/TFormula.cxx
@@ -779,6 +779,7 @@ prepareMethod(bool HasParameters, bool HasVariables, const char* FuncName,
 
 static TInterpreter::CallFuncIFacePtr_t::Generic_t
 prepareFuncPtr(TMethodCall *Method) {
+   if (!Method) return nullptr;
    CallFunc_t *callfunc = Method->GetCallFunc();
 
    if (!gCling->CallFunc_IsValid(callfunc)) {
