cf856c36abb86a956b423b91c582e9cfb8ac3bca
Sergey Linev <S.Linev@gsi.de>
2019-02-26 15:47:43 +0100

    eve7: remove ClassDef for REveViewContext
    class no longer derived from TObject, therefore classdef no longer
    needed

--- graf3d/eve7/inc/ROOT/REveViewContext.hxx
+++ graf3d/eve7/inc/ROOT/REveViewContext.hxx
@@ -1,3 +1,14 @@
+// @(#)root/eve7:$Id$
+// Author: Matevz Tadel, Alja Tadel 2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 #ifndef ROOT7_REveViewContext
 #define ROOT7_REveViewContext
 
@@ -9,26 +20,24 @@ class REveTrackPropagator;
 
 class REveViewContext  {
 private:
-   float m_R;
-   float m_Z;
-   REveTrackPropagator* m_trackPropagator;
-
-   REveTableViewInfo* fTableInfo;
+   float m_R{100};
+   float m_Z{100};
+   REveTrackPropagator *m_trackPropagator{nullptr};
+   REveTableViewInfo *fTableInfo{nullptr};
 
 public:
-   REveViewContext(): m_R(100), m_Z(100), m_trackPropagator(0), fTableInfo(0) {}
-   virtual ~REveViewContext(){}
+   REveViewContext() = default;
+   virtual ~REveViewContext() {}
 
    void SetBarrel(float r, float z) { m_R = r; m_Z = z; }
-   void SetTrackPropagator( REveTrackPropagator* p) {m_trackPropagator = p; }
-   void SetTableViewInfo(REveTableViewInfo* ti) { fTableInfo = ti; }
 
-   float GetMaxR() const {return m_R;}
-   float GetMaxZ() const {return m_Z;}
-   REveTrackPropagator* GetPropagator() const {return m_trackPropagator;}
-   REveTableViewInfo* GetTableViewInfo() const {return fTableInfo;}
+   void SetTrackPropagator(REveTrackPropagator *p) { m_trackPropagator = p; }
+   void SetTableViewInfo(REveTableViewInfo *ti) { fTableInfo = ti; }
 
-   ClassDef(REveViewContext, 0);
+   float GetMaxR() const { return m_R; }
+   float GetMaxZ() const { return m_Z; }
+   REveTrackPropagator *GetPropagator() const { return m_trackPropagator; }
+   REveTableViewInfo *GetTableViewInfo() const { return fTableInfo; }
 };
 }
 }
