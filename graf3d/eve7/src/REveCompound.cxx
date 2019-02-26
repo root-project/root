e5d9ff5d2fc14531660ba32f05cdb7f7e5d1ab11
Sergey Linev <S.Linev@gsi.de>
2019-02-26 18:35:28 +0100

    eve7: remove ClassDef in REveCompound

--- graf3d/eve7/src/REveCompound.cxx
+++ graf3d/eve7/src/REveCompound.cxx
@@ -1,8 +1,8 @@
 // @(#)root/eve:$Id$
-// Author: Matevz Tadel 2007
+// Author: Matevz Tadel 2007, 2018
 
 /*************************************************************************
- * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -10,6 +10,8 @@
  *************************************************************************/
 
 #include <ROOT/REveCompound.hxx>
+
+#include "TClass.h"
 
 using namespace ROOT::Experimental;
 namespace REX = ROOT::Experimental;
@@ -165,7 +167,7 @@ void REveCompound::FillImpliedSelectedSet(Set_t& impSelSet)
 
 TClass* REveCompound::ProjectedClass(const REveProjection*) const
 {
-   return REveCompoundProjected::Class();
+   return TClass::GetClass(typeid(REveCompoundProjected));
 }
 
 
