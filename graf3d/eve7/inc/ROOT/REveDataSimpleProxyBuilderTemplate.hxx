9b2b491aa12f926cf1924be79c9124f91f3deeb5
Sergey Linev <S.Linev@gsi.de>
2019-02-26 13:14:22 +0100

    eve7: suppress compiler warnings
    Comment out unused functions or args

--- graf3d/eve7/inc/ROOT/REveDataSimpleProxyBuilderTemplate.hxx
+++ graf3d/eve7/inc/ROOT/REveDataSimpleProxyBuilderTemplate.hxx
@@ -18,27 +18,27 @@ public:
 
 protected:
    using REveDataSimpleProxyBuilder::Build;
-   virtual void Build(const void*iData, REveElement* itemHolder, const REveViewContext* context)
+   virtual void Build(const void *iData, REveElement *itemHolder, const REveViewContext *context)
    {
-      if(0!=iData) {
+      if(iData) {
          Build(*reinterpret_cast<const T*> (iData), itemHolder, context);
       }
    }
- 
+
    using REveDataSimpleProxyBuilder::BuildViewType;
-   virtual void BuildViewType(const void*iData, REveElement* itemHolder, std::string viewType, const REveViewContext* context)
+   virtual void BuildViewType(const void *iData, REveElement *itemHolder, std::string viewType, const REveViewContext *context)
    {
-      if(0!=iData) {
+      if(iData) {
          BuildViewType(*reinterpret_cast<const T*> (iData), itemHolder, viewType, context);
       }
    }
-   
-   virtual void Build(const T& iData, REveElement* itemHolder, const REveViewContext* context)
+
+   virtual void Build(const T &/*iData*/, REveElement */*itemHolder*/, const REveViewContext */*context*/)
    {
       throw std::runtime_error("virtual Build(const T&, unsigned int, TEveElement&, const FWViewContext*) not implemented by inherited class.");
    }
-   
-   virtual void BuildViewType(const T& iData, REveElement* itemHolder, std::string viewType, const REveViewContext* context)
+
+   virtual void BuildViewType(const T &/*iData*/, REveElement */*itemHolder*/, std::string /*viewType*/, const REveViewContext */*context*/)
    {
       throw std::runtime_error("virtual BuildViewType(const T&, unsigned int, TEveElement&, const FWViewContext*) not implemented by inherited class.");
    }
