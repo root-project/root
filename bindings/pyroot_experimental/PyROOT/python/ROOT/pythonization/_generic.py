0778c11a4c1f6e58d8d92ee9a9a5ad3db7660820
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-02-27 16:57:23 +0100

    [Exp PyROOT] TVectorT: add __getitem__ pythonisation with checked bounds
    Throws IndexError if index is out of bounds and also makes it possible
    to iterate over a TVectorT.

--- bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_generic.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_generic.py
@@ -1,7 +1,7 @@
-# Author: Stefan Wunsch CERN  06/2018
+# Author: Stefan Wunsch, Enric Tejedor CERN  06/2018
 
 ################################################################################
-# Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.                      #
+# Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.                      #
 # All rights reserved.                                                         #
 #                                                                              #
 # For the licensing terms see $ROOTSYS/LICENSE.                                #
@@ -10,6 +10,28 @@
 
 from libROOTPython import AddPrettyPrintingPyz
 from ROOT import pythonization
+
+def _add_getitem_checked(klass):
+    # Parameters:
+    # - klass: class where to add a __getitem__ method that raises
+    # IndexError if index is out of range
+
+    def getitem_checked(o, i):
+        # Get item of `o` at `i` or raise IndexError if index is
+        # out of range.
+        # Assumes `o` has `__len__`.
+        # Parameters:
+        # - o: object
+        # - i: index to be checked in object
+        # Returns:
+        # - o[i]
+        if i >= 0 and i < len(o):
+            return o._getitem__unchecked(i)
+        else:
+            raise IndexError('index out of range')
+    
+    klass._getitem__unchecked = klass.__getitem__
+    klass.__getitem__ = getitem_checked
 
 
 @pythonization()
