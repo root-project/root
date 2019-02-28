53072b200c4d2765d83281e1fae4b8d5847f0c28
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-02-28 10:18:51 +0100

    [Exp PyROOT] TArray: add __getitem__ pythonisation with checked bounds
    Throws IndexError if index is out of bounds and also makes it possible
    to iterate over a TArray.

--- bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_tarray.py
+++ bindings/pyroot_experimental/PyROOT/python/ROOT/pythonization/_tarray.py
@@ -1,7 +1,7 @@
 # Author: Enric Tejedor CERN  11/2018
 
 ################################################################################
-# Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.                      #
+# Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.                      #
 # All rights reserved.                                                         #
 #                                                                              #
 # For the licensing terms see $ROOTSYS/LICENSE.                                #
@@ -9,6 +9,8 @@
 ################################################################################
 
 from ROOT import pythonization
+
+from ._generic import _add_getitem_checked
 
 
 @pythonization()
@@ -20,5 +22,12 @@ def pythonize_tarray(klass, name):
     if name == 'TArray':
         # Support `len(a)` as `a.GetSize()`
         klass.__len__ = klass.GetSize
+
+    elif name.startswith('TArray'):
+        # Add checked __getitem__. It has to be directly added to the TArray
+        # subclasses, which have a default __getitem__.
+        # The new __getitem__ allows to throw pythonic IndexError when index
+        # is out of range and to iterate over the array.
+        _add_getitem_checked(klass)
 
     return True
