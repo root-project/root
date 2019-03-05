d188d8df1457fb155ee62d0ccf432fe39df8b5f5
Sergey Linev <S.Linev@gsi.de>
2019-03-05 14:18:38 +0100

    webui: avoid globals "sap.m" and "sap.ui"  in ui5 code
    openui5 provides sap.ui.define(['module'], function(module) {}) to load
    necessary code. One should use it as much as possible

--- ui5/eve7/controller/Summary.controller.js
+++ ui5/eve7/controller/Summary.controller.js
@@ -6,8 +6,14 @@ sap.ui.define([
    "sap/m/StandardTreeItem",
    "sap/m/Input",
    "sap/m/CheckBox",
-   "sap/ui/layout/SplitterLayoutData"
-], function(Controller, JSONModel, Button, ColorPalettePopover, StandardTreeItem, mInput, mCheckBox, SplitterLayoutData) {
+   "sap/m/Panel",
+   "sap/m/Text",
+   "sap/ui/layout/SplitterLayoutData",
+   "sap/ui/layout/VerticalLayout",
+   "sap/ui/layout/HorizontalLayout"
+], function(Controller, JSONModel, Button, ColorPalettePopover, StandardTreeItem, 
+            mInput, mCheckBox, mPanel, mText, 
+            SplitterLayoutData, VerticalLayout, HorizontalLayout) {
 
    "use strict";
    
@@ -209,13 +215,13 @@ sap.ui.define([
          oTree.setIncludeItemInSelection(true);
 
          if (false) {
-            var oModel = new sap.ui.model.json.JSONModel();
+            var oModel = new JSONModel();
             oModel.setData([]);
             oModel.setSizeLimit(10000);
             this.getView().setModel(oModel, "treeModel");
 
          } else {
-            var oModel = new sap.ui.model.json.JSONModel();
+            var oModel = new JSONModel();
             oModel.setData([]);
             oModel.setSizeLimit(10000);
             this.getView().setModel(oModel, "treeModel");
@@ -429,7 +435,7 @@ sap.ui.define([
          sap.ui.getCore().setModel(this.model, "treeModel");
 
 
-         this.oProductModel = new sap.ui.model.json.JSONModel();
+         this.oProductModel = new JSONModel();
          this.oProductModel.setData([this._event]);
          sap.ui.getCore().setModel(this.oProductModel, "event");
       },
@@ -608,7 +614,7 @@ sap.ui.define([
       toggleEditor: function() {
          var pp = this.byId("sumSplitter");
          if (!this.ged) {
-            var panel = new sap.m.Panel("productDetailsPanel", { height: "100%" ,width : "97%"});
+            var panel = new mPanel("productDetailsPanel", { height: "100%" ,width : "97%"});
             panel.setHeaderText("ElementGED");
             panel.addStyleClass("sapUiSizeCompact");
 
@@ -745,14 +751,13 @@ sap.ui.define([
 
          if (widget) widget.data("myData", customData);
 
-         var label = new sap.m.Text(sId + "label", { text: { path: "ged>name" } });
+         var label = new mText(sId + "label", { text: { path: "ged>name" } });
          label.setWidth(this.maxLabelLength +"ex");
          label.addStyleClass("sapUiTinyMargin");
-         var HL = new sap.ui.layout.HorizontalLayout({
+         
+         return new HorizontalLayout({
             content : [label, widget]
          });
-
-         return HL;
       },
 
       handleColorSelect: function(event) {
