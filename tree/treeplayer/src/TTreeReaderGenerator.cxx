794d3b574c6ac05627849d11172bdaa29a920ea4
Philippe Canal <pcanal@fnal.gov>
2019-03-08 09:13:41 -0600

    Revert "Set offset of the used-for-write element in case of I/O rule on 'current' StreamerInfo."
    This reverts commit 505ac824e65af3afe2d6873f948e0e9a87460351.
    
    Commit contains more than intended.

--- tree/treeplayer/src/TTreeReaderGenerator.cxx
+++ tree/treeplayer/src/TTreeReaderGenerator.cxx
@@ -55,7 +55,7 @@ namespace Internal {
       if(BranchNeedsReader(branchName, parent, isLeaf)) {
          // Ignore unknown types
          if (dataType.EqualTo("")) {
-            Warning("TTreeReaderGenerator::AddReader", "Ignored branch %s because type is unsupported.", branchName.Data());
+            Warning("AddReader", "Ingored branch %s because type is unknown.", branchName.Data());
             return;
          }
          // Loop through existing readers to check duplicate branch names
@@ -995,7 +995,7 @@ Bool_t )CODE" << fClassname << R"CODE(::Process(Long64_t entry)
    //
    // The return value is currently not used.
 
-   fReader.SetLocalEntry(entry);
+   fReader.SetEntry(entry);
 
    return kTRUE;
 }
