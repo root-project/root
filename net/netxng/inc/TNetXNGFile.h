c27357672819bd353d22eec7a5e5d5b15fd0a9b4
Jan Knedlik <j.knedlik@gsi.de>
2019-03-12 17:34:07 +0100

    removed defaults from new redirection constructor

--- net/netxng/inc/TNetXNGFile.h
+++ net/netxng/inc/TNetXNGFile.h
@@ -57,8 +57,8 @@ public:
    TNetXNGFile() : TFile(),
       fFile(0), fUrl(0), fMode(XrdCl::OpenFlags::None), fInitCondVar(0),
       fReadvIorMax(0), fReadvIovMax(0) {}
-   TNetXNGFile(const char *url, const char *lurl, Option_t *mode = "", const char *title = "",
-               Int_t compress = 1, Int_t netopt = 0, Bool_t parallelopen = kFALSE);
+   TNetXNGFile(const char *url, const char *lurl, Option_t *mode , const char *title ,
+               Int_t compress , Int_t netopt , Bool_t parallelopen );
    TNetXNGFile(const char *url, Option_t *mode = "", const char *title = "",
                Int_t compress = 1, Int_t netopt = 0, Bool_t parallelopen = kFALSE);
 
