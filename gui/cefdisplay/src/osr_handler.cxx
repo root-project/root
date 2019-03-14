6b23b77fabdee96c1039a02c1ba535bf9a5c9e58
Sergey Linev <S.Linev@gsi.de>
2019-03-14 19:05:50 +0100

    webui: comments and copyright

--- gui/cefdisplay/src/osr_handler.cxx
+++ gui/cefdisplay/src/osr_handler.cxx
@@ -5,6 +5,15 @@
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
 /// is welcome!
 
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+
 #if !defined(_MSC_VER)
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wshadow"
