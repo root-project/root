d155e971b5ece35c75efbd7572bcbd8515f25cf8
Sergey Linev <S.Linev@gsi.de>
2019-03-15 17:07:06 +0100

    webui: ensure Qt5 works with custom scheme handle
    Qt5 now requires that custom "rootscheme:" registered before Qt
    application created. Therefore now there is no separated handlers for
    different window - single handler created once. Works now with only
    THttpServer instance (like in CEF), can be enhanced later for many of
    them.

--- gui/qt5webdisplay/rootwebview.cpp
+++ gui/qt5webdisplay/rootwebview.cpp
@@ -6,7 +6,7 @@
 /// is welcome!
 
 /*************************************************************************
- * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
