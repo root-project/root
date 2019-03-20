75b043c2148bc4d41481c0fd7c844a8a9a60a55e
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-03-20 17:35:24 +0100

    [Exp PyROOT] Generate dictionaries and rootmap file for CPyCppyy
    Also, make sure CPyCppyy headers are in the PCH.
    
    This commit contributes to fix the roottest-python-cling tests,
    which use TPython, now part of CPyCppyy.

---
+++ bindings/pyroot_experimental/cppyy/CPyCppyy/inc/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TPython;
+#pragma link C++ class TPyReturn;
+#pragma link C++ class TPyArg;
+
+#pragma link C++ namespace CPyCppyy;
+#pragma link C++ class CPyCppyy::TPyException;
+
+#endif
