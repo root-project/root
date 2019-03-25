05f3c2fc14bb2d422358e2a2ef5109570ccc3b6b
Stefan Wunsch <stefan.wunsch@cern.ch>
2019-03-25 15:40:43 +0100

    [VecOps] Add InvariantMass helper computing invariant mass of two four-vectors

--- tutorials/vecops/vo007_PhysicsHelpers.C
+++ tutorials/vecops/vo007_PhysicsHelpers.C
@@ -2,7 +2,7 @@
 /// \ingroup tutorial_vecops
 /// \notebook -nodraw
 /// In this tutorial we demonstrate RVec helpers for physics computations such
-/// as angle differences \f$\Delta \phi\f$.
+/// as angle differences \f$\Delta \phi\f$ and invariant mass.
 ///
 /// \macro_code
 /// \macro_output
@@ -30,6 +30,39 @@ void vo007_PhysicsHelpers()
 
    std::cout << "Phi values: " << phis << std::endl;
    for(std::size_t i = 0; i < idx[0].size(); i++) {
-      std::cout << "DeltaPhi(" << phis[idx[0][i]] << ", " << phis[idx[1][i]] << ") = " << dphi[i] << std::endl;
+      std::cout << "DeltaPhi(" << phis[idx[0][i]] << ", " << phis[idx[1][i]]
+                << ") = " << dphi[i] << std::endl;
    }
+
+   // The InvariantMass helper computes the invariant mass of a two particle system
+   // given the properties transverse momentum (pt), rapidity (eta), azimuth (phi)
+   // and mass.
+   RVec<float> pt1 = {40, 20, 30};
+   RVec<float> eta1 = {2.5, 0.5, -1.0};
+   RVec<float> phi1 = {-0.5, 0.0, 1.0};
+   RVec<float> mass1 = {10, 10, 10};
+
+   RVec<float> pt2 = {20, 10, 40};
+   RVec<float> eta2 = {0.5, -0.5, 1.0};
+   RVec<float> phi2 = {0.0, 1.0, -1.0};
+   RVec<float> mass2 = {2, 2, 2};
+
+   auto invMass = InvariantMass(pt1, eta1, phi1, mass1, pt2, eta2, phi2, mass2);
+
+   std::cout << std::endl;
+   for(std::size_t i = 0; i < pt1.size(); i++) {
+      std::cout << "InvariantMass("
+                << pt1[i] << ", " << eta1[i] << ", " << phi1[i] << ", " << mass1[i] << ", "
+                << pt2[i] << ", " << eta2[i] << ", " << phi2[i] << ", " << mass2[i]
+                << ") = " << invMass[i] << std::endl;
+   }
+
+   // The helper also accepts a single set of (pt, eta, phi, mass) vectors. Then,
+   // the invariant mass of all particles in the collection is computed.
+
+   auto invMass2 = InvariantMass(pt1, eta1, phi1, mass1);
+
+   std::cout << std::endl;
+   std::cout << "InvariantMass(" << pt1 << ", " << eta1 << ", " << phi1 << ", "
+             << mass1 << ") = " << invMass2 << std::endl;
 }
