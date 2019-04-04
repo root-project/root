32dddcb0390a2e8031019ba500e0e1e6af196366
Danilo Piparo <danilo.piparo@cern.ch>
2019-04-04 11:45:41 +0200

    [Tutorials] CsvDS: download csv in the macro and not in CMake
    therewith making them standalone.

--- tutorials/dataframe/df014_CSVDataSource.py
+++ tutorials/dataframe/df014_CSVDataSource.py
@@ -21,7 +21,10 @@ import ROOT
 
 # Let's first create a RDF that will read from the CSV file.
 # The types of the columns will be automatically inferred.
-fileName = "df014_CsvDataSource_MuRun2010B.csv"
+fileNameUrl = "http://root.cern.ch/files/tutorials/df014_CsvDataSource_MuRun2010B.csv"
+fileName = "df014_CsvDataSource_MuRun2010B_py.csv"
+ROOT.TFile.Cp(fileNameUrl, fileName)
+
 MakeCsvDataFrame = ROOT.ROOT.RDF.MakeCsvDataFrame
 tdf = MakeCsvDataFrame(fileName)
 
