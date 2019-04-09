fae9a4a851dd4f9d2c97ceb24c29ec796fd5a978
Axel Naumann <Axel.Naumann@cern.ch>
2019-04-09 11:47:30 +0200

    [tutorials] No need to heap-allocate TDatime-s, use gRandom. Thanks, Maarten!

--- tutorials/hist/candleplot.C
+++ tutorials/hist/candleplot.C
@@ -11,22 +11,21 @@
 void candleplot() {
 
    gStyle->SetTimeOffset(0);
-   auto rng = new TRandom();
-   auto dateBegin = new TDatime(2010,1,1,0,0,0);
-   auto dateEnd = new TDatime(2011,1,1,0,0,0);
+   TDatime dateBegin(2010,1,1,0,0,0);
+   TDatime dateEnd(2011,1,1,0,0,0);
 
-   auto h1 = new TH2I("h1","Machine A + B",12,dateBegin->Convert(),dateEnd->Convert(),1000,0,1000);
-   auto h2 = new TH2I("h2","Machine B",12,dateBegin->Convert(),dateEnd->Convert(),1000,0,1000);
+   auto h1 = new TH2I("h1","Machine A + B",12,dateBegin.Convert(),dateEnd.Convert(),1000,0,1000);
+   auto h2 = new TH2I("h2","Machine B",12,dateBegin.Convert(),dateEnd.Convert(),1000,0,1000);
 
    h1->GetXaxis()->SetTimeDisplay(1);
    h1->GetXaxis()->SetTimeFormat("%m/%y");
    h1->GetXaxis()->SetTitle("Date [month/year]");
 
    float Rand;
-   for (int i = dateBegin->Convert(); i < dateEnd->Convert(); i+=86400*30) {
+   for (int i = dateBegin.Convert(); i < dateEnd.Convert(); i+=86400*30) {
       for (int j = 0; j < 1000; j++) {
-         Rand = rng->Gaus(500+sin(i/10000000.)*100,50); h1->Fill(i,Rand);
-         Rand = rng->Gaus(500+sin(i/11000000.)*100,70); h2->Fill(i,Rand);
+         Rand = gRandom->Gaus(500+sin(i/10000000.)*100,50); h1->Fill(i,Rand);
+         Rand = gRandom->Gaus(500+sin(i/11000000.)*100,70); h2->Fill(i,Rand);
       }
    }
 
