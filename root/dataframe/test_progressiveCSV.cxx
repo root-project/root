928a8b8bfce015fdd957e4af9953c339240ebe00
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-04-12 14:21:43 +0200

    Change test to check when new lines of CSV file are read
    The previous strategy, which consisted in checking the resident
    memory of the process before and after running the RDF computation,
    was not reliable and made the test fail intermittently.

--- root/dataframe/test_progressiveCSV.cxx
+++ root/dataframe/test_progressiveCSV.cxx
@@ -1,47 +1,25 @@
 
-#include "TSystem.h"
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RCsvDS.hxx"
 #include <iostream>
-#include <cassert>
 
 int test_progressiveCSV()
 {
-   // Input file: 5000 rows (> 700 KB)
+   // Input file: 5000 lines
    auto fileName = "test_progressiveCSV.csv";
-   const auto numRows = 5000U;
-   const auto sizeInKB = 700.;
 
-   // Create a CSV data source that reads in chunks of 10 rows 
-   const auto chunkSize = 10LL;
-   auto tdf = ROOT::RDF::MakeCsvDataFrame(fileName, true, ',', chunkSize);
-   
-   ProcInfo_t info;
-   gSystem->GetProcInfo(&info);
-   auto memBefore = info.fMemResident; // KB
+   // Create a CSV data source that reads in chunks of 2000 lines
+   const auto chunkSize = 2000LL;
+   auto rdf = ROOT::RDF::MakeCsvDataFrame(fileName, true, ',', chunkSize);
 
-   assert(*tdf.Count() == numRows);
-
-   gSystem->GetProcInfo(&info);
-   auto memAfter = info.fMemResident;
-
-   // Check that indeed we read in chunks (consumed memory should be much less than 700 KB)
-   assert(memAfter - memBefore < sizeInKB);
-
+   auto rdflines = *rdf.Count();
+   std::cout << "Total num lines: " << rdflines << std::endl;
 
    // Now create a CSV data source that reads the entire file into memory at once
-   auto tdf2 = ROOT::RDF::MakeCsvDataFrame(fileName);
+   auto rdf2 = ROOT::RDF::MakeCsvDataFrame(fileName);
 
-   gSystem->GetProcInfo(&info);
-   memBefore = info.fMemResident;
-
-   assert(*tdf2.Count() == numRows);
-
-   gSystem->GetProcInfo(&info);
-   memAfter = info.fMemResident;
-
-   // Check that this time we allocated memory for all the rows 
-   assert(memAfter - memBefore > sizeInKB);   
+   rdflines = *rdf2.Count();
+   std::cout << "Total num lines: " << rdflines << std::endl;
 
    return 0; 
 }
