c065aa62ee40d932e249cacde98b97c69ec33c5e
Axel Naumann <Axel.Naumann@cern.ch>
2019-04-12 08:37:46 +0200

    [gpadv7] Implement comparison.

--- graf2d/gpadv7/v7/src/RDrawingAttr.cxx
+++ graf2d/gpadv7/v7/src/RDrawingAttr.cxx
@@ -48,13 +48,31 @@ std::string ROOT::Experimental::RDrawingAttrBase::GetValueString(const Path &pat
    return holder->GetAttrFromStyle(path);
 }
 
-bool ROOT::Experimental::RDrawingAttrBase::IsFromStyle(const Name& name) const
+bool ROOT::Experimental::RDrawingAttrBase::IsFromStyle(const Path& path) const
 {
    auto holder = GetHolderPtr().lock();
    if (!holder)
       return "";
 
-   return !holder->AtIf(GetPath() + name);
+   return !holder->AtIf(path);
+}
+
+bool ROOT::Experimental::RDrawingAttrBase::IsFromStyle(const Name& name) const
+{
+   return IsFromStyle(GetPath() + name);
+}
+
+bool ROOT::Experimental::RDrawingAttrBase::operator==(const RDrawingAttrBase &other) const
+{
+   auto thisHolder = GetHolderPtr().lock();
+   auto otherHolder = other.GetHolderPtr().lock();
+   if (!thisHolder && !otherHolder)
+      return true;
+   if (!thisHolder != !otherHolder)
+      return false;
+
+   // We have valid holders for both.
+   return thisHolder->Equal(*otherHolder.get(), GetPath(), other.GetPath());
 }
 
 float ROOT::Experimental::FromAttributeString(const std::string &val, const std::string &/*name*/, float *)
@@ -97,8 +115,48 @@ const std::string *ROOT::Experimental::RDrawingAttrHolder::AtIf(const Name_t &at
 
 std::string ROOT::Experimental::RDrawingAttrHolder::GetAttrFromStyle(const Name_t &attrName)
 {
-    
    R__WARNING_HERE("Graf2d") << "Failed to get attribute for "
       << attrName.fStr << ": not yet implemented!";
    return "";
+}
+
+namespace {
+template <class MAP>
+static std::vector<typename MAP::const_iterator> GetItersStartingWith(const MAP& map, const std::string& stem)
+{
+   std::vector<typename MAP::const_iterator> ret;
+   for (auto i = map.begin(), e = map.end(); i !=e; ++i)
+      if (i->first.compare(0, stem.length(), stem) == 0) {
+         // Require i->first to be complete stem, or more but then stem followed by ".":
+         // stem "a.b", i->first can be "a.b" or "a.b.c.d"
+         if (stem.length() == i->first.length()
+             || i->first[stem.length()] == '.')
+         ret.emplace_back(i);
+      }
+   return ret;
+}
+}
+
+bool ROOT::Experimental::RDrawingAttrHolder::Equal(const RDrawingAttrHolder &other, const Name_t &thisName, const Name_t &otherName)
+{
+   std::vector<Map_t::const_iterator> thisIters = GetItersStartingWith(fAttrNameVals, thisName.Str());
+   std::vector<Map_t::const_iterator> otherIters = GetItersStartingWith(other.fAttrNameVals, otherName.Str());
+
+   if (thisIters.size() != otherIters.size())
+      return false;
+
+   for (auto thisIter: thisIters) {
+      // thisIters and otherIters have equal size. If any element in thisIters does not exist
+      // in other.fAttrNameVals then they are not equal (if other.fAttrNameVals has an entry that
+      // does not exist in this->fAttrNameVals, there must also be a key in this->fAttrNameVals
+      // that does not exist in other.fAttrNameVals or the counts of thisIters and otherIters
+      // would differ).
+      // If all keys' values are equal, thisIters and otherIters are equal.
+      auto otherIter = other.fAttrNameVals.find(thisIter->first);
+      if (otherIter == other.fAttrNameVals.end())
+         return false;
+      if (thisIter->second != otherIter->second)
+         return false;
+   }
+   return true;
 }
