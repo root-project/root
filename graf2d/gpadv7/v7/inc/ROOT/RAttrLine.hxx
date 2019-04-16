a2ee475663221bdb60694e420c984a7456e8e883
Sergey Linev <S.Linev@gsi.de>
2019-04-16 14:10:35 +0200

    v7attr: adjust header comments and include files order

--- graf2d/gpadv7/v7/inc/ROOT/RAttrLine.hxx
+++ graf2d/gpadv7/v7/inc/ROOT/RAttrLine.hxx
@@ -1,12 +1,12 @@
 /// \file ROOT/RAttrLine.hxx
-/// \ingroup Graf ROOT7
+/// \ingroup Gpad ROOT7
 /// \author Axel Naumann <axel@cern.ch>
 /// \date 2018-10-12
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
 /// is welcome!
 
 /*************************************************************************
- * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -16,8 +16,8 @@
 #ifndef ROOT7_RAttrLine
 #define ROOT7_RAttrLine
 
-#include <ROOT/RColor.hxx>
 #include <ROOT/RDrawingAttr.hxx>
+#include <ROOT/RColor.hxx>
 
 namespace ROOT {
 namespace Experimental {
