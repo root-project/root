7b27cbe9f4a9ff8857d88eb1c45e1d0f8741fa87
Danilo Piparo <danilo.piparo@cern.ch>
2019-04-16 12:08:31 +0200

    [TTreeProcessorMT][ROOT-9948] Add test

--- tree/treeplayer/test/treeprocmt/treeprocessormt.cxx
+++ tree/treeplayer/test/treeprocmt/treeprocessormt.cxx
@@ -171,3 +171,15 @@ TEST(TreeProcessorMT, MaxTasks)
    gSystem->Unlink(filename);
    ROOT::DisableImplicitMT();
 }
+
+TEST(TreeProcessorMT, PathName)
+{
+   auto fname = "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/ZZTo4mu.root";
+   auto f = std::unique_ptr<TFile>(TFile::Open(fname));
+   auto tree = f->Get<TTree>("Events");
+   ROOT::TTreeProcessorMT p(*tree);
+   std::atomic<unsigned int> n (0U);
+   auto func = [&n](TTreeReader &t) { while (t.Next()) n++;};
+   p.Process(func);
+   EXPECT_EQ(n.load(), 1499064U) << "Wrong number of events processed!\n";
+}
