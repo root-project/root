60023be613c361fc5d44896003c8402216ca909b
Danilo Piparo <danilo.piparo@cern.ch>
2019-04-20 01:08:05 +0200

    [TTreeSQL] Use explicit conversion to const char*

--- tree/tree/src/TTreeSQL.cxx
+++ tree/tree/src/TTreeSQL.cxx
@@ -461,7 +461,7 @@ void TTreeSQL::CreateBranches()
 
       if (br == 0)
       {
-         Error("CreateBranches", "Skipped %s", branchName);
+         Error("CreateBranches", "Skipped %s", branchName.Data());
          continue;
       }
 
