d33c2e6ec3d7908810578be1fc143510d1250e3f
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2019-04-25 11:09:29 +0200

    Add ROOT7 RBrowser using OpenUI5
    First implementation of the ROOT7 Browser, using OpenUI5 Web GUI
    To try it:
    root[0] #include "ROOT/RBrowser.hxx"
    root[1] using namespace ROOT:Experimental;
    root[2] RBrowser browser;

---
+++ gui/browserv7/inc/LinkDef.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __CINT__
+
+#pragma link C++ struct ROOT::Experimental::RBrowserComboBoxItem+;
+#pragma link C++ class std::vector<ROOT::Experimental::RBrowserTreeViewItem>+;
+#pragma link C++ struct ROOT::Experimental::RBrowserModel+;
+#pragma link C++ class ROOT::Experimental::RBrowser+;
+#pragma link C++ class ROOT::Experimental::RRootBrowserReply+;
+#pragma link C++ class ROOT::Experimental::RRootBrowserItem+;
+
+#endif
