ffae4d2aac07d62165d9ee79feca5c4fa4dcf441
Danilo Piparo <danilo.piparo@cern.ch>
2019-04-26 12:59:35 +0200

    [RDF] Add C++ and Python Stats tutorials

---
+++ tutorials/dataframe/df031_Stats.py
@@ -0,0 +1,27 @@
+## \file
+## \ingroup tutorial_dataframe
+## \notebook
+##
+## Extract the statistics relative to RDataFrame columns and store them
+##  in TStatistic instances.
+##
+## \macro_code
+## \macro_output
+##
+## \date April 2019
+## \author Danilo Piparo
+
+import ROOT
+
+# Create a data frame and add two columns: one for the values and one for the weight.
+r = ROOT.ROOT.RDataFrame(256);
+rr = r.Define("v", "rdfentry_")\
+      .Define("w", "return 1./(v+1)")
+    
+# Now extract the statistics, weighted, unweighted
+stats_iu = rr.Stats("v")
+stats_iw = rr.Stats("v", "w")
+
+# Now print them: they are all identical of course!
+stats_iu.Print()
+stats_iw.Print()
