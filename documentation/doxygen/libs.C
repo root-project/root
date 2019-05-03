db30239746d479b7e40adab3746ffa4feb7f7a68
Olivier Couet <olivier.couet@cern.ch>
2019-05-03 16:20:32 +0200

    Fix ROOT-9760

--- documentation/doxygen/libs.C
+++ documentation/doxygen/libs.C
@@ -4,9 +4,33 @@ void libs(TString classname)
 
    // Find in which library classname sits
    classname.ReplaceAll("_1",":");
+   classname.ReplaceAll("_01"," ");
+   int i = classname.Index("_3");
+   if (i>0) classname.Remove(i,classname.Length()-i);
+
    libname = gInterpreter->GetClassSharedLibs(classname.Data());
 
-   if(!libname) return;
+   // Library was not found, try to find it from a template in $ROOTSYS/lib/*.rootmap
+   if (!libname) {
+      gSystem->Exec(Form("grep %s $ROOTSYS/lib/*.rootmap | grep -m 1 map:class | sed -e 's/^.*class //' > classname.txt",classname.Data()));
+      FILE *f = fopen("classname.txt", "r");
+      char c[160];
+      char *str = fgets(c,160,f);
+      fclose(f);
+      remove("classname.txt");
+      if (!str) {
+         printf("%s cannot be found in any of the .rootmap files\n", classname.Data());
+         remove("libslist.dot");
+         return;
+      }
+      TString cname = c;
+      cname.Remove(cname.Length()-1, 1);
+      libname = gInterpreter->GetClassSharedLibs(cname.Data());
+      if (!libname) {
+         printf("Cannot find library for the class %s\n",cname.Data());
+         return;
+      }
+   }
 
    // Print the library name in a external file
    TString mainlib = libname;
