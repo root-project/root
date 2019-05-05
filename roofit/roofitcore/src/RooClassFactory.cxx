7c38ed515bf013d1e0076ccfb2cbe1a7be893eb7
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-05-05 15:58:54 +0300

    [tcling] Call EnableAutoLoading when we are set up.
    We should not expect users to know when is the best time to enable
    autoloading in the interpreter.

--- roofit/roofitcore/src/RooClassFactory.cxx
+++ roofit/roofitcore/src/RooClassFactory.cxx
@@ -105,10 +105,6 @@ Bool_t RooClassFactory::makeAndCompilePdf(const char* name, const char* expressi
     return ret ;
   }
 
-  if (gInterpreter->GetRootMapFiles()==0) {
-    gInterpreter->EnableAutoLoading() ;
-  }
-
   TInterpreter::EErrorCode ecode;
   gInterpreter->ProcessLineSynch(Form(".L %s.cxx+",name),&ecode) ;
   return (ecode!=TInterpreter::kNoError) ;
@@ -151,10 +147,6 @@ Bool_t RooClassFactory::makeAndCompileFunction(const char* name, const char* exp
   Bool_t ret = makeFunction(name,realArgNames.c_str(),catArgNames.c_str(),expression,intExpression?kTRUE:kFALSE,intExpression) ;
   if (ret) {
     return ret ;
-  }
-
-  if (gInterpreter->GetRootMapFiles()==0) {
-    gInterpreter->EnableAutoLoading() ;
   }
 
   TInterpreter::EErrorCode ecode;
@@ -215,10 +207,6 @@ RooAbsReal* RooClassFactory::makeFunctionInstance(const char* name, const char* 
 
 RooAbsReal* RooClassFactory::makeFunctionInstance(const char* className, const char* name, const char* expression, const RooArgList& vars, const char* intExpression)
 {
-  if (gInterpreter->GetRootMapFiles()==0) {
-    gInterpreter->EnableAutoLoading() ;
-  }
-
   // Use class factory to compile and link specialized function
   Bool_t error = makeAndCompileFunction(className,expression,vars,intExpression) ;
 
@@ -315,10 +303,6 @@ RooAbsPdf* RooClassFactory::makePdfInstance(const char* name, const char* expres
 RooAbsPdf* RooClassFactory::makePdfInstance(const char* className, const char* name, const char* expression,
 					    const RooArgList& vars, const char* intExpression)
 {
-  if (gInterpreter->GetRootMapFiles()==0) {
-    gInterpreter->EnableAutoLoading() ;
-  }
-
   // Use class factory to compile and link specialized function
   Bool_t error = makeAndCompilePdf(className,expression,vars,intExpression) ;
 
