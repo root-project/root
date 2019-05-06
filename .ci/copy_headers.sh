0e0ce96324c7c1d1795f4ddcc8273cc266abec17
Guilherme Amadio <amadio@cern.ch>
2019-05-06 15:48:24 +0200

    travis: disable unavailable options now enabled by all=ON

--- .ci/copy_headers.sh
+++ .ci/copy_headers.sh
@@ -5,7 +5,8 @@ set -ex
 # We need to put in place all relevant headers before running clang-tidy.
 mkdir ../build
 cd ../build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dall=On -Dtesting=On -Dx11=Off ../root
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dall=On -Dtesting=On -Dx11=Off -Dalien=Off \
+      -Dcuda=Off -Dtmva-gpu=Off ../root
 # We need to prebuild a minimal set of targets which are responsible for header copy
 # or generation.
 make -j4 move_headers intrinsics_gen ClangCommentCommandList ClangCommentCommandInfo \
