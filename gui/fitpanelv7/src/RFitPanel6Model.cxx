8663940cd16ff3367fb975d92b341ed7786e44bc
Sergey Linev <S.Linev@gsi.de>
2019-05-07 09:31:12 +0200

    web fitpanel: extract RFitPanel6Model into extra class
    Apply clang-format to all sources
    Simplify LinkDef

---
+++ gui/fitpanelv7/src/RFitPanel6Model.cxx
@@ -0,0 +1,17 @@
+/// \file RFitPanel6Model.cxx
+/// \ingroup WebGui ROOT7
+/// \author Sergey Linev <S.Linev@gsi.de>
+/// \author Iliana Betsou <Iliana.Betsou@cern.ch>
+/// \date 2019-04-11
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RFitPanel6Model.hxx>
