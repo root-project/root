5546f7812b21f3160929d5766bc6411f0bd53a3a
Guilherme Amadio <amadio@cern.ch>
2019-05-10 08:20:58 +0200

    Revert "[RVec] Optimise SBO avoiding useless resizes and assignments"
    This reverts commit ff37657825602bc7107f999c65ba8bd0cf55b4cc.

--- math/vecops/inc/ROOT/RVec.hxx
+++ math/vecops/inc/ROOT/RVec.hxx
@@ -117,7 +117,7 @@ void EmplaceBack(std::vector<bool> &v, Args &&... args)
    v.push_back(std::forward<Args>(args)...);
 }
 
-// This template and specialisation allow to create vectors of size 0 and with T
+// This template and specialisation allow to create vectors of size 0 and with T 
 // w/o a default constructor
 template <typename RVec_t, std::size_t BufferSize = RVec_t::fgBufferSize>
 class RStorageVectorFactory {
@@ -128,7 +128,7 @@ public:
                                 ::ROOT::Detail::VecOps::MakeAdoptAllocator(buf, BufferSize));
       v.resize(0);
       return v;
-   }
+}
 };
 
 template <typename RVec_t>
@@ -152,7 +152,7 @@ A RVec is a container designed to make analysis of values' collections fast and 
 Its storage is contiguous in memory and its interface is designed such to resemble to the one
 of the stl vector. In addition the interface features methods and external functions to ease
 the manipulation and analysis of the data in the RVec.
-RVec implements for arithmetic types a small buffer optimisation, relying on the stack rather
+RVec implements for arithmetic types a small buffer optimisation, relying on the stack rather 
 than the heap to represent small collections.
 
 \htmlonly
@@ -323,15 +323,16 @@ public:
 private:
    // We need this class for the case where fgBufferSize is 0, otherwise array<NonCopiable, 0>
    // will not compile on macos.
-   class RArrayPlaceHolder {
-   public:
-      std::size_t size() { return 0U; }
-      T *data() { return nullptr; }
+   class RArrayPlaceHolder
+   {
+      public:
+      std::size_t size() {return 0U;}
+      T *data() {return nullptr;}
    };
 
    /// The type of the buffer the RVec keeps for small buffer optimisation.
    /// If the size of the buffer is 0, a place holder internal type is used, if not an std::array
-   /// is used because its content resides on the stack. The RAdoptAllocator adopts if needed this
+   /// is used because its content resides on the stack. The RAdoptAllocator adopts if needed this 
    /// memory.
    using Buffer_t = typename std::conditional<fgBufferSize == 0, RArrayPlaceHolder, std::array<T, fgBufferSize>>::type;
 
@@ -360,6 +361,10 @@ private:
       return thisBufSize && v.size() <= thisBufSize;
    }
 
+   /// Helper method to know whether the allocator is adopting a memory region which is
+   /// not the small buffer.
+   bool IsAdoptingExternalMemory() { return ::ROOT::Detail::VecOps::IsAdoptingExternalMemory(fAlloc); }
+
 public:
    // constructors
 
@@ -373,9 +378,19 @@ public:
    // than such size to then resize back to count.
    // This step is carried out in order for the capacity of fData to be equal to the size of the
    // buffer.
-   explicit RVec(size_type count) : fData(count, T(), fAlloc) {}
+   explicit RVec(size_type count) : fData(count <= fgBufferSize ? fgBufferSize : 0, T(), fAlloc)
+   {
+      resize(count);
+   }
 
-   RVec(size_type count, const T &value) : fData(count, value, fAlloc) {}
+   RVec(size_type count, const T &value) : fData(count <= fgBufferSize ? fgBufferSize : 0, value, fAlloc)
+   {
+      if (CanUseBuffer(count)) {
+         resize(count);
+      } else {
+         resize(count, value);
+      }
+   }
 
    RVec(const RVec &v) : fData(v.size() <= fgBufferSize ? fgBufferSize : 0, T(), fAlloc)
    {
@@ -389,12 +404,16 @@ public:
       std::copy(v.begin(), v.end(), fData.begin());
    }
 
-   RVec(RVec<T> &&v)
+   RVec(RVec<T> &&v) : fData(v.size() <= fgBufferSize ? fgBufferSize : 0, T(), fAlloc)
    {
-      if (CanUseBuffer(v)) {
-         fData.assign(v.fData.begin(), v.fData.end());
-      } else {
+      if (v.IsAdoptingExternalMemory())
+      {
          fAlloc = std::move(v.fAlloc);
+         fData = std::move(v.fData);
+      } else if (CanUseBuffer(v)) {
+         resize(v.size());
+         std::copy(v.begin(), v.end(), fData.begin());
+      } else {
          fData = std::move(v.fData);
       }
    }
@@ -412,7 +431,12 @@ public:
    // assignment
    RVec<T> &operator=(const RVec<T> &v)
    {
-      fData.assign(v.fData.begin(), v.fData.end());
+      if (CanUseBuffer(v.size())) {
+         resize(v.size());
+         std::copy(v.begin(), v.end(), fData.begin());
+      } else {
+         fData = v.fData;
+      }
       return *this;
    }
 
