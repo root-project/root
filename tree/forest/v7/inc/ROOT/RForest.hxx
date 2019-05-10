ef5d5d70f3a1a9ecbf5aaf2d9285c3f22c7dc783
Jakob Blomer <jblomer@cern.ch>
2019-05-10 11:37:43 +0200

    [forest] fix copyright year

--- tree/forest/v7/inc/ROOT/RForest.hxx
+++ tree/forest/v7/inc/ROOT/RForest.hxx
@@ -6,7 +6,7 @@
 /// is welcome!
 
 /*************************************************************************
- * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
