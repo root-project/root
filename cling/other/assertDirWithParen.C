60862e37d0ce22c9bebe78b79e9d344b705095fa
Axel Naumann <Axel.Naumann@cern.ch>
2019-05-14 18:07:27 +0200

    Check for .x with trailing semicolon.

--- cling/other/assertDirWithParen.C
+++ cling/other/assertDirWithParen.C
@@ -14,8 +14,12 @@ int assertDirWithParen()
 {
   bool haveError = false;
   haveError |= !check("", 17);
+  // Unsupported: haveError |= !check(";", 17);
   haveError |= !check("()", 17);
+  haveError |= !check("();", 17);
+
   haveError |= !check("(42)", 42);
+  haveError |= !check("(43);", 43);
   haveError |= !check("(17 + 42)", 59);
   haveError |= !check("((12) + (43))", 55);
       
