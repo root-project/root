19106c50ce55a7b0005f9e93eb55931af28c4ba2
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-05-15 11:56:21 +0200

    [Exp PyROOT] Implement TObject __eq__ and __ne__ in C++
    The equality and inequality operators are better implemented in C++,
    since we need to need to rely on Cppyy's rich comparison if the object
    we are comparing ourselves with is not a Python proxy or if it contains
    a null pointer. For example, we need to support the comparison to None.
    
    The rest of comparison operators (i.e. those that define order)
    can be implemented in Python, throwing a NotImplemented exception
    if we are not comparing two proxies to TObject or derivate.

--- bindings/pyroot_experimental/PyROOT/src/PyzCppHelpers.cxx
+++ bindings/pyroot_experimental/PyROOT/src/PyzCppHelpers.cxx
@@ -21,12 +21,33 @@ pythonizations.
 #include "CPPInstance.h"
 #include "TClass.h"
 
+// Call method with signature: obj->meth()
 PyObject *CallPyObjMethod(PyObject *obj, const char *meth)
 {
-   // Helper; call method with signature: obj->meth()
-   return PyObject_CallMethod(obj, const_cast<char*>(meth), const_cast<char*>(""));
+   return PyObject_CallMethod(obj, const_cast<char *>(meth), const_cast<char *>(""));
 }
 
+// Call method with signature: obj->meth(arg1)
+PyObject *CallPyObjMethod(PyObject *obj, const char *meth, PyObject *arg1)
+{
+   return PyObject_CallMethod(obj, const_cast<char *>(meth), const_cast<char *>("O"), arg1);
+}
+
+// Convert generic python object into a boolean value
+PyObject *BoolNot(PyObject *value)
+{
+   if (PyObject_IsTrue(value) == 1) {
+      Py_INCREF(Py_False);
+      Py_DECREF(value);
+      return Py_False;
+   } else {
+      Py_INCREF(Py_True);
+      Py_XDECREF(value);
+      return Py_True;
+   }
+}
+
+// Get the TClass of the C++ object proxied by pyobj
 TClass *GetTClass(const CPyCppyy::CPPInstance *pyobj)
 {
    return TClass::GetClass(Cppyy::GetFinalName(pyobj->ObjectIsA()).c_str());
