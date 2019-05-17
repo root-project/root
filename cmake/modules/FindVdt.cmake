ce3656b3a67d301ac9494462822e8fdce4fabba5
Guilherme Amadio <amadio@cern.ch>
2019-05-17 09:56:03 +0200

    FindVdt.cmake: create imported target and detect version
    Old versions of Vdt (0.4.1 and below) do not export version information,
    but ROOT needs 0.4.0 at least, because of the vdt/tanh.h header, so we
    deduce the version based on the existence of that header and require the
    minimum version that ROOT needs. This file now also creates an imported
    target that can be used within ROOT to link against VDT with dependencies
    taken care of automatically. Fixes ROOT-9885.
    
    (cherry picked from commit ca754b0fedd2eebf97d316a65e6bf69e0e97a5c8)

--- cmake/modules/FindVdt.cmake
+++ cmake/modules/FindVdt.cmake
@@ -1,20 +1,76 @@
-# - Locate vdt library
-# Defines:
+#.rst:
+# FindVdt
+# -------
 #
-#  VDT_FOUND
-#  VDT_INCLUDE_DIRS
-#  VDT_LIBRARIES
+# Find the Vdt library header and define variables.
+#
+# Imported Targets
+# ^^^^^^^^^^^^^^^^
+#
+# This module defines :prop_tgt:`IMPORTED` target ``VDT::VDT``,
+# if Vdt has been found.
+#
+# Result Variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module defines the following variables:
+#
+# ::
+#
+#   VDT_FOUND          - True if Vdt is found.
+#   VDT_INCLUDE_DIRS   - Where to find vdt/vdtMath.h
+#
+# ::
+#
+#   VDT_VERSION        - The version of Vdt found (x.y.z)
+#   VDT_VERSION_MAJOR  - The major version of Vdt
+#   VDT_VERSION_MINOR  - The minor version of Vdt
+#   VDT_VERSION_PATCH  - The patch version of Vdt
+#
 
-find_path(VDT_INCLUDE_DIR NAMES vdt/vdtMath.h  HINTS ${VDT_DIR}/include /usr/include/vdt)
-find_library(VDT_LIBRARY NAMES vdt HINTS ${VDT_DIR}/lib/ $ENV{VDT_DIR}/lib/ /usr/lib)
+if(NOT VDT_INCLUDE_DIR)
+  find_path(VDT_INCLUDE_DIR NAME vdt/vdtMath.h PATH_SUFFIXES include)
+endif()
 
-set(VDT_INCLUDE_DIRS ${VDT_INCLUDE_DIR})
-set(VDT_LIBRARIES ${VDT_LIBRARY})
+if(NOT VDT_LIBRARY)
+  find_library(VDT_LIBRARY NAMES vdt)
+endif()
 
+mark_as_advanced(VDT_INCLUDE_DIR VDT_LIBRARY)
 
-# handle the QUIETLY and REQUIRED arguments and set VDT_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(VDT DEFAULT_MSG VDT_INCLUDE_DIR VDT_LIBRARY)
+if(VDT_INCLUDE_DIR)
+  file(STRINGS "${VDT_INCLUDE_DIR}/vdt/vdtMath.h" VDT_H REGEX "^#define VDT_VERSION_[A-Z]+[ ]+[0-9]+.*$")
+  string(REGEX REPLACE ".+VDT_VERSION_MAJOR[ ]+([0-9]+).*$" "\\1" VDT_VERSION_MAJOR "${VDT_H}")
+  string(REGEX REPLACE ".+VDT_VERSION_MINOR[ ]+([0-9]+).*$" "\\1" VDT_VERSION_MINOR "${VDT_H}")
+  string(REGEX REPLACE ".+VDT_VERSION_PATCH[ ]+([0-9]+).*$" "\\1" VDT_VERSION_PATCH "${VDT_H}")
+  set(VDT_VERSION "${VDT_VERSION_MAJOR}.${VDT_VERSION_MINOR}.${VDT_VERSION_PATCH}")
+  if("${VDT_VERSION}" STREQUAL "..")
+    if(EXISTS "${VDT_INCLUDE_DIR}/vdt/tanh.h")
+      set(VDT_VERSION "0.4")
+    else()
+      set(VDT_VERSION "0.3")
+    endif()
+  endif()
+endif()
 
-mark_as_advanced(VDT_FOUND VDT_INCLUDE_DIRS VDT_LIBRARIES)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Vdt FOUND_VAR VDT_FOUND
+  REQUIRED_VARS VDT_INCLUDE_DIR VDT_LIBRARY VERSION_VAR VDT_VERSION)
+
+if(VDT_FOUND)
+  set(VDT_INCLUDE_DIRS ${VDT_INCLUDE_DIR})
+
+  if(NOT VDT_LIBRARIES)
+    set(VDT_LIBRARIES ${VDT_LIBRARY})
+  endif()
+
+  if(NOT TARGET VDT::VDT)
+    add_library(VDT::VDT UNKNOWN IMPORTED)
+
+    set_target_properties(VDT::VDT
+      PROPERTIES
+        IMPORTED_LOCATION "${VDT_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${VDT_INCLUDE_DIRS}"
+    )
+  endif()
+endif()
