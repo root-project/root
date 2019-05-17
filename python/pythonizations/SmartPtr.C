6bad2bfd2f04ceb63c568f2eedebb209950cf2d0
Axel Naumann <Axel.Naumann@cern.ch>
2019-05-17 13:09:21 +0200

    auto_ptr to shared_ptr; the former is now gone.

--- python/pythonizations/SmartPtr.C
+++ python/pythonizations/SmartPtr.C
@@ -1,26 +1,21 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
 #include "SmartPtr.h"
 
-auto_ptr<MyShareable> gime_mine() { return mine; }
-auto_ptr<MyShareable>* gime_mine_ptr() { return &mine; }
-auto_ptr<MyShareable>& gime_mine_ref() { return mine; }
+shared_ptr<MyShareable> gime_mine() { return mine; }
+shared_ptr<MyShareable>* gime_mine_ptr() { return &mine; }
+shared_ptr<MyShareable>& gime_mine_ref() { return mine; }
 
-void pass_mine_sp(auto_ptr<MyShareable> )
+void pass_mine_sp(shared_ptr<MyShareable> )
 //{ cout << "Use count " << p->use_count() << "\n"; }
 { } // cout << "pass_mine_sp: underlying ptr " << p.get() << "\n"; }
 
-void pass_mine_sp_ref(auto_ptr<MyShareable>& )
+void pass_mine_sp_ref(shared_ptr<MyShareable>& )
 //{ cout << "Use count " << p->use_count() << "\n"; }
 { } // cout << "pass_mine_sp_ref: underlying ptr " << p.get() << "\n"; }
 
-void pass_mine_sp_ptr(auto_ptr<MyShareable>* )
+void pass_mine_sp_ptr(shared_ptr<MyShareable>* )
 //{ cout << "Use count " << p->use_count() << "\n"; }
 { } // cout << "pass_mine_sp_ptr: underlying ptr " << p->get() << "\n"; }
 
 void pass_mine_rp(MyShareable) {}
 void pass_mine_rp_ref(const MyShareable&) {}
 void pass_mine_rp_ptr(const MyShareable*) {}
-
-#pragma GCC diagnostic pop
