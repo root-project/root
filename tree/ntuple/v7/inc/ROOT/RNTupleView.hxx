a484b85af5fc3798ac277d1657727e95ce7be047
Jakob Blomer <jblomer@cern.ch>
2019-05-21 01:08:42 +0200

    [forest -> ntuple] fix file header comments

--- tree/ntuple/v7/inc/ROOT/RNTupleView.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleView.hxx
@@ -1,5 +1,5 @@
-/// \file ROOT/RForestView.hxx
-/// \ingroup Forest ROOT7
+/// \file ROOT/RNTupleView.hxx
+/// \ingroup NTuple ROOT7
 /// \author Jakob Blomer <jblomer@cern.ch>
 /// \date 2018-10-05
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
--- tree/ntuple/v7/src/RPage.cxx
+++ tree/ntuple/v7/src/RPage.cxx
@@ -1,5 +1,5 @@
 /// \file RPage.cxx
-/// \ingroup Forest ROOT7
+/// \ingroup NTuple ROOT7
 /// \author Jakob Blomer <jblomer@cern.ch>
 /// \date 2018-10-04
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
