49b81f202fb6ca4185935b6e86b27affdeac6d9d
Danilo Piparo <danilo.piparo@cern.ch>
2019-05-26 09:31:18 +0200

    [TTree][NFC] Clarify leaflist syntax for storing Double32_t/Float16_t arrays

--- tree/tree/src/TTree.cxx
+++ tree/tree/src/TTree.cxx
@@ -96,8 +96,12 @@ In the following, the details about the creation of different types of branches 
   padding strategy).
 - In case of the truncated floating point types (Float16_t and Double32_t) you can
   furthermore specify the range in the style [xmin,xmax] or [xmin,xmax,nbits] after
-  the type character. See `TStreamerElement::GetRange()` for further information.
-
+  the type character. For example, for storing a variable size array `myArr` of 
+  `Double32_t` with values within a range of `[0, 2*pi]` and the size of which is 
+  stored in a branch called `myArrSize`, the syntax for the `leaflist` string would 
+  be: `myArr[myArrSize]/d[0,twopi]`. Of course the number of bits could be specified,
+  the standard rules of opaque typedefs annotation are valid. For example, if only
+  18 bits were sufficient, the syntax would become: `myArr[myArrSize]/d[0,twopi,18]`
 
 ### Case B
 
