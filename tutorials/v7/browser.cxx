d06481959c940f14ea2e275daec301e766d7969d
Sergey Linev <S.Linev@gsi.de>
2019-05-29 16:41:51 +0200

    rbrowser: add minimal tutoriual for RBrowser

---
+++ tutorials/v7/browser.cxx
@@ -0,0 +1,32 @@
+/// \file
+/// \ingroup tutorial_v7
+///
+/// \macro_code
+///
+/// \date 2019-05-29
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+/// \author Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
+/// \author Sergey Linev <S.Linev@gsi.de>
+
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RBrowser.hxx>
+#include <ROOT/RDirectory.hxx>
+
+using namespace ROOT::Experimental;
+
+void browser()
+{
+   // create browser
+   auto br = std::make_shared<RBrowser>();
+
+   // add to global list - avoid auto deletion
+   RDirectory::Heap().Add("browser", br);
+}
+
