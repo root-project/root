518075c4d31d7c5178610fd52b3599a361376d3c
Danilo Piparo <danilo.piparo@cern.ch>
2019-06-03 23:38:24 +0200

    [CMake] Make MLP optional, by default ON

--- cmake/modules/RootBuildOptions.cmake
+++ cmake/modules/RootBuildOptions.cmake
@@ -126,6 +126,7 @@ ROOT_BUILD_OPTION(macos_native OFF "Disable looking for libraries, includes and 
 ROOT_BUILD_OPTION(mathmore ON "Build libMathMore extended math library (requires GSL)")
 ROOT_BUILD_OPTION(memory_termination OFF "Free internal ROOT memory before process termination (experimental, used for leak checking)")
 ROOT_BUILD_OPTION(memstat OFF "Build memory statistics utility (helps to detect memory leaks)")
+ROOT_BUILD_OPTION(mlp ON "Enable support for TMultilayerPerceptron classes' federation")
 ROOT_BUILD_OPTION(minuit2 OFF "Build Minuit2 minimization library")
 ROOT_BUILD_OPTION(monalisa OFF "Enable support for monitoring with Monalisa (requires libapmoncpp)")
 ROOT_BUILD_OPTION(mysql ON "Enable support for MySQL databases")
