610a87c0a2d2339cbeea08dd8fc0daf29650ee18
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2019-06-03 11:38:30 +0200

    fix memory leak when fitting histograms using linear fitter and functions built from expression using special operator ++. This fixes ROOT-10147

--- hist/hist/src/TH1.cxx
+++ hist/hist/src/TH1.cxx
@@ -3800,16 +3800,16 @@ TFitResultPtr TH1::Fit(const char *fname ,Option_t *option ,Option_t *goption, D
    Int_t ndim=GetDimension();
    if (linear){
       if (ndim<2){
-         f1=new TF1(fname, fname, xxmin, xxmax);
-         return Fit(f1,option,goption,xxmin,xxmax);
+         TF1 f1(fname, fname, xxmin, xxmax);
+         return Fit(&f1,option,goption,xxmin,xxmax);
       }
       else if (ndim<3){
-         f2=new TF2(fname, fname);
-         return Fit(f2,option,goption,xxmin,xxmax);
+         TF2 f2(fname, fname);
+         return Fit(&f2,option,goption,xxmin,xxmax);
       }
       else{
-         f3=new TF3(fname, fname);
-         return Fit(f3,option,goption,xxmin,xxmax);
+         TF3 f3(fname, fname);
+         return Fit(&f3,option,goption,xxmin,xxmax);
       }
    }
 
