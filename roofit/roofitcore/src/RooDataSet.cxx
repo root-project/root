235483e3c1432be37c28c9ff14a9f13a36e0515d
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2019-07-01 11:00:31 +0200

    [RF] Improve documentation of data storage classes.
    Also some cleanups.

--- roofit/roofitcore/src/RooDataSet.cxx
+++ roofit/roofitcore/src/RooDataSet.cxx
@@ -28,23 +28,23 @@ There are two storage backends:
 cannot be serialised if the dataset exceeds a size of 1 Gb
 - RooTreeDataStore: Uses a TTree, which can be file backed if a file is opened
 before creating the dataset. This significantly reduces the memory pressure, as the
-baskets of the tree can be written to a file.
-Enable tree-backed storage similar to this:
-```
-TFile outputFile("filename.root", "RECREATE");
-RooAbsData::setDefaultStorageType(RooAbsData::Tree);
-RooDataSet mydata(...);
-```
-or convert an existing memory-backed data storage:
-```
-RooDataSet mydata(...);
+baskets of the tree can be written to a file, and only the basket that's currently
+being read stays in RAM.
+  - Enable tree-backed storage similar to this:
+  ```
+  TFile outputFile("filename.root", "RECREATE");
+  RooAbsData::setDefaultStorageType(RooAbsData::Tree);
+  RooDataSet mydata(...);
+  ```
+  - Or convert an existing memory-backed data storage:
+  ```
+  RooDataSet mydata(...);
 
-TFile outputFile("filename.root", "RECREATE");
-mydata.convertToTreeStore();
-```
+  TFile outputFile("filename.root", "RECREATE");
+  mydata.convertToTreeStore();
+  ```
 
-Tree-backed storage can be converted like this:
-`RooAbsData::convertToTreeStore()`.
+For the inverse conversion, see `RooAbsData::convertToVectorStore()`.
 
 **/
 
