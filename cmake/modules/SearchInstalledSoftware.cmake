dacba774e0731706508fbe37a624a2e4bd25ba8c
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-07-21 19:02:08 +0200

    [cmake] Use --depth=1 when cloning gtest.

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -1472,6 +1472,7 @@ if (testing)
   ExternalProject_Add(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_SHALLOW 1
     GIT_TAG release-1.8.0
     UPDATE_COMMAND ""
     # TIMEOUT 10
