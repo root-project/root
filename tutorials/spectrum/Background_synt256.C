31c8b50ad052d936d4f564a1dcc1c4358552c04f
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2019-07-31 18:02:01 +0200

    Fix definition ofarray sizes also for the other TSpectrum2 new tutorials

--- tutorials/spectrum/Background_synt256.C
+++ tutorials/spectrum/Background_synt256.C
@@ -12,8 +12,8 @@
 
 void Background_synt256() {
    Int_t i, j;
-   Double_t nbinsx = 64;
-   Double_t nbinsy = 64;
+   const Int_t nbinsx = 64;
+   const Int_t nbinsy = 64;
    Double_t xmin = 0;
    Double_t xmax = (Double_t)nbinsx;
    Double_t ymin = 0;
