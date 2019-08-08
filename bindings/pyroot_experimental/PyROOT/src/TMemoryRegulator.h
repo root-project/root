1912e96d059fec46a60e27c97137ea961d89c9c9
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-08-08 10:13:13 +0200

    [Exp PyROOT] Implement custom memory regulator on top on Cppyy's

---
+++ bindings/pyroot_experimental/PyROOT/src/TMemoryRegulator.h
@@ -0,0 +1,65 @@
+
+// Author: Enric Tejedor CERN  08/2019
+
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef PYROOT_TMEMORYREGULATOR_H
+#define PYROOT_TMEMORYREGULATOR_H
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TMemoryRegulator                                                     //
+//                                                                      //
+// Sets hooks in Cppyy's MemoryRegulator to keep track of the TObjects  //
+// that are constructed and destructed. For those objects, a map is     //
+// filled, where the key is the address of the object and the value is  //
+// the class to which the object belongs.                               //
+//                                                                      //
+// The TMemoryRegulator object, created in PyROOTWrapper.cxx, is added  //
+// to the list of cleanups and its RecursiveRemove method is called by  //
+// ROOT to manage the memory of TObjects being deleted.                 //
+// In RecursiveRemove, the object being deleted is already a TNamed, so //
+// the information about its actual class is not available anymore.     //
+// To solve the problem, the map above is used to know the class of the //
+// object, so that Cppyy's RecursiveRemove can be called passing the    //
+// class as argument.                                                   //
+//////////////////////////////////////////////////////////////////////////
+
+// ROOT
+#include "TObject.h"
+#include "TClass.h"
+
+// Bindings
+#include "CPyCppyy.h"
+#include "MemoryRegulator.h"
+
+// Stl
+#include <map>
+
+namespace PyROOT {
+
+typedef std::map<Cppyy::TCppObject_t, Cppyy::TCppType_t> ObjectMap_t;
+
+class TMemoryRegulator : public TObject {
+private:
+   static ObjectMap_t fObjectMap; // key: object address; value: object class id
+
+   static std::pair<bool, bool> RegisterHook(Cppyy::TCppObject_t, Cppyy::TCppType_t);
+
+   static std::pair<bool, bool> UnregisterHook(Cppyy::TCppObject_t, Cppyy::TCppType_t);
+
+public:
+   TMemoryRegulator();
+
+   virtual void RecursiveRemove(TObject *);
+};
+
+} // namespace PyROOT
+
+#endif // !PYROOT_TMEMORYREGULATOR_H
