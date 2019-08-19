6f2fe15167366fc77d44bb7a9cd109dfa455fb1e
Sergey Linev <S.Linev@gsi.de>
2019-08-19 11:20:56 +0200

    [tbuffer] mark TBufferText methods final/override
    Make usage of complex TBuffer virtual interface more safe

--- io/io/inc/TBufferText.h
+++ io/io/inc/TBufferText.h
@@ -29,92 +29,92 @@ public:
 
    // virtual TBuffer methods, which are generic for all text-based streamers
 
-   virtual void StreamObject(void *obj, const std::type_info &typeinfo, const TClass *onFileClass = nullptr);
-   virtual void StreamObject(void *obj, const char *className, const TClass *onFileClass = nullptr);
-   virtual void StreamObject(TObject *obj);
+   void StreamObject(void *obj, const std::type_info &typeinfo, const TClass *onFileClass = nullptr) override;
+   void StreamObject(void *obj, const char *className, const TClass *onFileClass = nullptr) override;
+   void StreamObject(TObject *obj) override;
    using TBuffer::StreamObject;
 
-   virtual Int_t ApplySequence(const TStreamerInfoActions::TActionSequence &sequence, void *object);
-   virtual Int_t ApplySequenceVecPtr(const TStreamerInfoActions::TActionSequence &sequence, void *start_collection,
-                                     void *end_collection);
-   virtual Int_t
-   ApplySequence(const TStreamerInfoActions::TActionSequence &sequence, void *start_collection, void *end_collection);
+   Int_t ApplySequence(const TStreamerInfoActions::TActionSequence &sequence, void *object) final;
+   Int_t ApplySequenceVecPtr(const TStreamerInfoActions::TActionSequence &sequence, void *start_collection,
+                                     void *end_collection) final;
+   Int_t
+   ApplySequence(const TStreamerInfoActions::TActionSequence &sequence, void *start_collection, void *end_collection) final;
 
-   virtual void ReadFloat16(Float_t *f, TStreamerElement *ele = nullptr);
-   virtual void WriteFloat16(Float_t *f, TStreamerElement *ele = nullptr);
-   virtual void ReadDouble32(Double_t *d, TStreamerElement *ele = nullptr);
-   virtual void WriteDouble32(Double_t *d, TStreamerElement *ele = nullptr);
-   virtual void ReadWithFactor(Float_t *ptr, Double_t factor, Double_t minvalue);
-   virtual void ReadWithNbits(Float_t *ptr, Int_t nbits);
-   virtual void ReadWithFactor(Double_t *ptr, Double_t factor, Double_t minvalue);
-   virtual void ReadWithNbits(Double_t *ptr, Int_t nbits);
+   void ReadFloat16(Float_t *f, TStreamerElement *ele = nullptr) final;
+   void WriteFloat16(Float_t *f, TStreamerElement *ele = nullptr) final;
+   void ReadDouble32(Double_t *d, TStreamerElement *ele = nullptr) final;
+   void WriteDouble32(Double_t *d, TStreamerElement *ele = nullptr) final;
+   void ReadWithFactor(Float_t *ptr, Double_t factor, Double_t minvalue) final;
+   void ReadWithNbits(Float_t *ptr, Int_t nbits) final;
+   void ReadWithFactor(Double_t *ptr, Double_t factor, Double_t minvalue) final;
+   void ReadWithNbits(Double_t *ptr, Int_t nbits) final;
 
-   virtual Int_t ReadArrayFloat16(Float_t *&f, TStreamerElement *ele = nullptr);
-   virtual Int_t ReadArrayDouble32(Double_t *&d, TStreamerElement *ele = nullptr);
+   Int_t ReadArrayFloat16(Float_t *&f, TStreamerElement *ele = nullptr) override;
+   Int_t ReadArrayDouble32(Double_t *&d, TStreamerElement *ele = nullptr) override;
 
-   virtual Int_t ReadStaticArrayFloat16(Float_t *f, TStreamerElement *ele = nullptr);
-   virtual Int_t ReadStaticArrayDouble32(Double_t *d, TStreamerElement *ele = nullptr);
+   Int_t ReadStaticArrayFloat16(Float_t *f, TStreamerElement *ele = nullptr) final;
+   Int_t ReadStaticArrayDouble32(Double_t *d, TStreamerElement *ele = nullptr) final;
 
-   virtual void ReadFastArrayFloat16(Float_t *f, Int_t n, TStreamerElement *ele = nullptr);
-   virtual void ReadFastArrayDouble32(Double_t *d, Int_t n, TStreamerElement *ele = nullptr);
-   virtual void ReadFastArrayWithFactor(Float_t *ptr, Int_t n, Double_t factor, Double_t minvalue);
-   virtual void ReadFastArrayWithNbits(Float_t *ptr, Int_t n, Int_t nbits);
-   virtual void ReadFastArrayWithFactor(Double_t *ptr, Int_t n, Double_t factor, Double_t minvalue);
-   virtual void ReadFastArrayWithNbits(Double_t *ptr, Int_t n, Int_t nbits);
+   void ReadFastArrayFloat16(Float_t *f, Int_t n, TStreamerElement *ele = nullptr) final;
+   void ReadFastArrayDouble32(Double_t *d, Int_t n, TStreamerElement *ele = nullptr) final;
+   void ReadFastArrayWithFactor(Float_t *ptr, Int_t n, Double_t factor, Double_t minvalue) final;
+   void ReadFastArrayWithNbits(Float_t *ptr, Int_t n, Int_t nbits) final;
+   void ReadFastArrayWithFactor(Double_t *ptr, Int_t n, Double_t factor, Double_t minvalue) final;
+   void ReadFastArrayWithNbits(Double_t *ptr, Int_t n, Int_t nbits) final;
 
-   virtual void WriteArrayFloat16(const Float_t *f, Int_t n, TStreamerElement *ele = nullptr);
-   virtual void WriteArrayDouble32(const Double_t *d, Int_t n, TStreamerElement *ele = nullptr);
+   void WriteArrayFloat16(const Float_t *f, Int_t n, TStreamerElement *ele = nullptr) final;
+   void WriteArrayDouble32(const Double_t *d, Int_t n, TStreamerElement *ele = nullptr) final;
 
-   virtual void WriteFastArrayFloat16(const Float_t *d, Int_t n, TStreamerElement *ele = nullptr);
-   virtual void WriteFastArrayDouble32(const Double_t *d, Int_t n, TStreamerElement *ele = nullptr);
+   void WriteFastArrayFloat16(const Float_t *d, Int_t n, TStreamerElement *ele = nullptr) final;
+   void WriteFastArrayDouble32(const Double_t *d, Int_t n, TStreamerElement *ele = nullptr) final;
 
    // Utilities for TClass
-   virtual Int_t ReadClassBuffer(const TClass * /*cl*/, void * /*pointer*/, const TClass * /*onfile_class*/ = nullptr);
-   virtual Int_t ReadClassBuffer(const TClass * /*cl*/, void * /*pointer*/, Int_t /*version*/, UInt_t /*start*/,
-                                 UInt_t /*count*/, const TClass * /*onfile_class*/ = nullptr);
-   virtual Int_t WriteClassBuffer(const TClass *cl, void *pointer);
+   Int_t ReadClassBuffer(const TClass * /*cl*/, void * /*pointer*/, const TClass * /*onfile_class*/ = nullptr) override;
+   Int_t ReadClassBuffer(const TClass * /*cl*/, void * /*pointer*/, Int_t /*version*/, UInt_t /*start*/,
+                                 UInt_t /*count*/, const TClass * /*onfile_class*/ = nullptr) override;
+   Int_t WriteClassBuffer(const TClass *cl, void *pointer) override;
 
    // virtual abstract TBuffer methods, which are not used in text streaming
 
-   virtual Int_t CheckByteCount(UInt_t /* startpos */, UInt_t /* bcnt */, const TClass * /* clss */) { return 0; }
-   virtual Int_t CheckByteCount(UInt_t /* startpos */, UInt_t /* bcnt */, const char * /* classname */) { return 0; }
-   virtual void SetByteCount(UInt_t /* cntpos */, Bool_t /* packInVersion */ = kFALSE) {}
-   virtual void SkipVersion(const TClass *cl = nullptr);
-   virtual Version_t ReadVersionNoCheckSum(UInt_t *, UInt_t *) { return 0; }
+   Int_t CheckByteCount(UInt_t /* startpos */, UInt_t /* bcnt */, const TClass * /* clss */) final { return 0; }
+   Int_t CheckByteCount(UInt_t /* startpos */, UInt_t /* bcnt */, const char * /* classname */) final { return 0; }
+   void SetByteCount(UInt_t /* cntpos */, Bool_t /* packInVersion */ = kFALSE) final {}
+   void SkipVersion(const TClass *cl = nullptr) final;
+   Version_t ReadVersionNoCheckSum(UInt_t *, UInt_t *) final { return 0; }
 
-   virtual Int_t ReadBuf(void * /*buf*/, Int_t /*max*/)
+   Int_t ReadBuf(void * /*buf*/, Int_t /*max*/) final
    {
       Error("ReadBuf", "useless in text streamers");
       return 0;
    }
-   virtual void WriteBuf(const void * /*buf*/, Int_t /*max*/) { Error("WriteBuf", "useless in text streamers"); }
+   void WriteBuf(const void * /*buf*/, Int_t /*max*/) final { Error("WriteBuf", "useless in text streamers"); }
 
-   virtual char *ReadString(char * /*s*/, Int_t /*max*/)
+   char *ReadString(char * /*s*/, Int_t /*max*/) final
    {
       Error("ReadString", "useless");
       return nullptr;
    }
-   virtual void WriteString(const char * /*s*/) { Error("WriteString", "useless"); }
+   void WriteString(const char * /*s*/) final { Error("WriteString", "useless"); }
 
-   virtual Version_t ReadVersionForMemberWise(const TClass * /*cl*/ = nullptr)
+   Version_t ReadVersionForMemberWise(const TClass * /*cl*/ = nullptr) final
    {
       Error("ReadVersionForMemberWise", "not defined in text-based streamers");
       return 0;
    }
-   virtual UInt_t WriteVersionMemberWise(const TClass * /*cl*/, Bool_t /*useBcnt*/ = kFALSE)
+   UInt_t WriteVersionMemberWise(const TClass * /*cl*/, Bool_t /*useBcnt*/ = kFALSE) final
    {
       Error("WriteVersionMemberWise", "not defined in text-based streamers");
       return 0;
    }
 
-   virtual TObject *ReadObject(const TClass * /*cl*/)
+   TObject *ReadObject(const TClass * /*cl*/) final
    {
       Error("ReadObject", "not yet implemented for text-based streamers");
       return nullptr;
    }
 
    // Utilities for TClass
-   virtual Int_t ReadClassEmulated(const TClass * /*cl*/, void * /*object*/, const TClass * /*onfile_class*/ = nullptr)
+   Int_t ReadClassEmulated(const TClass * /*cl*/, void * /*object*/, const TClass * /*onfile_class*/ = nullptr) final
    {
       Error("ReadClassEmulated", "not defined in text-based streamers");
       return 0;
@@ -137,7 +137,7 @@ protected:
    static const char *fgFloatFmt;  ///<!  printf argument for floats, either "%f" or "%e" or "%10f" and so on
    static const char *fgDoubleFmt; ///<!  printf argument for doubles, either "%f" or "%e" or "%10f" and so on
 
-   ClassDef(TBufferText, 0); // a TBuffer subclass for all text-based streamers
+   ClassDefOverride(TBufferText, 0); // a TBuffer subclass for all text-based streamers
 };
 
 #endif
