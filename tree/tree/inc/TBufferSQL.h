4bc1946861f4d63fd9f2bf37d163edaf1c8e548f
Sergey Linev <S.Linev@gsi.de>
2019-08-19 13:01:23 +0200

    [tbuffer] provide final specifier for TBufferSQL methods

--- tree/tree/inc/TBufferSQL.h
+++ tree/tree/inc/TBufferSQL.h
@@ -21,20 +21,17 @@
 //////////////////////////////////////////////////////////////////////////
 
 #include "TBufferFile.h"
-#include "TString.h"
 
-
-class TSQLResult;
 class TSQLRow;
 
-class TBufferSQL : public TBufferFile {
+class TBufferSQL final : public TBufferFile {
 
 private:
    std::vector<Int_t>::const_iterator fIter;
 
-   std::vector<Int_t>  *fColumnVec;   //!
-   TString             *fInsertQuery; //!
-   TSQLRow            **fRowPtr;      //!
+   std::vector<Int_t>  *fColumnVec{nullptr};   //!
+   TString             *fInsertQuery{nullptr}; //!
+   TSQLRow            **fRowPtr{nullptr};      //!
 
    // TBuffer objects cannot be copied or assigned
    TBufferSQL(const TBufferSQL &);        // not implemented
@@ -49,85 +46,85 @@ public:
 
    void ResetOffset();
 
-   virtual   void     ReadBool(Bool_t       &b);
-   virtual   void     ReadChar(Char_t       &c);
-   virtual   void     ReadUChar(UChar_t     &c);
-   virtual   void     ReadShort(Short_t     &s);
-   virtual   void     ReadUShort(UShort_t   &s);
-   virtual   void     ReadInt(Int_t         &i);
-   virtual   void     ReadUInt(UInt_t       &i);
-   virtual   void     ReadLong(Long_t       &l);
-   virtual   void     ReadULong(ULong_t     &l);
-   virtual   void     ReadLong64(Long64_t   &l);
-   virtual   void     ReadULong64(ULong64_t &l);
-   virtual   void     ReadFloat(Float_t     &f);
-   virtual   void     ReadDouble(Double_t   &d);
-   virtual   void     ReadCharP(Char_t      *c);
-   virtual   void     ReadTString(TString   &s);
-   virtual   void     ReadStdString(std::string *s);
-   using              TBuffer::ReadStdString;
-   virtual   void     ReadCharStar(char* &s);
+   void     ReadBool(Bool_t       &b) final;
+   void     ReadChar(Char_t       &c) final;
+   void     ReadUChar(UChar_t     &c) final;
+   void     ReadShort(Short_t     &s) final;
+   void     ReadUShort(UShort_t   &s) final;
+   void     ReadInt(Int_t         &i) final;
+   void     ReadUInt(UInt_t       &i) final;
+   void     ReadLong(Long_t       &l) final;
+   void     ReadULong(ULong_t     &l) final;
+   void     ReadLong64(Long64_t   &l) final;
+   void     ReadULong64(ULong64_t &l) final;
+   void     ReadFloat(Float_t     &f) final;
+   void     ReadDouble(Double_t   &d) final;
+   void     ReadCharP(Char_t      *c) final;
+   void     ReadTString(TString   &s) final;
+   void     ReadStdString(std::string *s) final;
+   using    TBuffer::ReadStdString;
+   void     ReadCharStar(char* &s) final;
 
-   virtual   void     WriteBool(Bool_t       b);
-   virtual   void     WriteChar(Char_t       c);
-   virtual   void     WriteUChar(UChar_t     c);
-   virtual   void     WriteShort(Short_t     s);
-   virtual   void     WriteUShort(UShort_t   s);
-   virtual   void     WriteInt(Int_t         i);
-   virtual   void     WriteUInt(UInt_t       i);
-   virtual   void     WriteLong(Long_t       l);
-   virtual   void     WriteULong(ULong_t     l);
-   virtual   void     WriteLong64(Long64_t   l);
-   virtual   void     WriteULong64(ULong64_t l);
-   virtual   void     WriteFloat(Float_t     f);
-   virtual   void     WriteDouble(Double_t   d);
-   virtual   void     WriteCharP(const Char_t *c);
-   virtual   void     WriteTString(const TString  &s);
-   virtual   void     WriteStdString(const std::string *s);
-   using              TBuffer::WriteStdString;
-   virtual   void     WriteCharStar(char *s);
+   void     WriteBool(Bool_t       b) final;
+   void     WriteChar(Char_t       c) final;
+   void     WriteUChar(UChar_t     c) final;
+   void     WriteShort(Short_t     s) final;
+   void     WriteUShort(UShort_t   s) final;
+   void     WriteInt(Int_t         i) final;
+   void     WriteUInt(UInt_t       i) final;
+   void     WriteLong(Long_t       l) final;
+   void     WriteULong(ULong_t     l) final;
+   void     WriteLong64(Long64_t   l) final;
+   void     WriteULong64(ULong64_t l) final;
+   void     WriteFloat(Float_t     f) final;
+   void     WriteDouble(Double_t   d) final;
+   void     WriteCharP(const Char_t *c) final;
+   void     WriteTString(const TString  &s) final;
+   void     WriteStdString(const std::string *s) final;
+   using    TBuffer::WriteStdString;
+   void     WriteCharStar(char *s) final;
 
-   virtual   void     WriteFastArray(const Bool_t    *b, Int_t n);
-   virtual   void     WriteFastArray(const Char_t    *c, Int_t n);
-   virtual   void     WriteFastArrayString(const Char_t   *c, Int_t n);
-   virtual   void     WriteFastArray(const UChar_t   *c, Int_t n);
-   virtual   void     WriteFastArray(const Short_t   *h, Int_t n);
-   virtual   void     WriteFastArray(const UShort_t  *h, Int_t n);
-   virtual   void     WriteFastArray(const Int_t     *i, Int_t n);
-   virtual   void     WriteFastArray(const UInt_t    *i, Int_t n);
-   virtual   void     WriteFastArray(const Long_t    *l, Int_t n);
-   virtual   void     WriteFastArray(const ULong_t   *l, Int_t n);
-   virtual   void     WriteFastArray(const Long64_t  *l, Int_t n);
-   virtual   void     WriteFastArray(const ULong64_t *l, Int_t n);
-   virtual   void     WriteFastArray(const Float_t   *f, Int_t n);
-   virtual   void     WriteFastArray(const Double_t  *d, Int_t n);
-   virtual   void     WriteFastArray(void  *start,  const TClass *cl, Int_t n=1, TMemberStreamer *s=0);
-   virtual   Int_t    WriteFastArray(void **startp, const TClass *cl, Int_t n=1, Bool_t isPreAlloc=kFALSE, TMemberStreamer *s=0);
+   void     WriteFastArray(const Bool_t    *b, Int_t n) final;
+   void     WriteFastArray(const Char_t    *c, Int_t n) final;
+   void     WriteFastArrayString(const Char_t   *c, Int_t n) final;
+   void     WriteFastArray(const UChar_t   *c, Int_t n) final;
+   void     WriteFastArray(const Short_t   *h, Int_t n) final;
+   void     WriteFastArray(const UShort_t  *h, Int_t n) final;
+   void     WriteFastArray(const Int_t     *i, Int_t n) final;
+   void     WriteFastArray(const UInt_t    *i, Int_t n) final;
+   void     WriteFastArray(const Long_t    *l, Int_t n) final;
+   void     WriteFastArray(const ULong_t   *l, Int_t n) final;
+   void     WriteFastArray(const Long64_t  *l, Int_t n) final;
+   void     WriteFastArray(const ULong64_t *l, Int_t n) final;
+   void     WriteFastArray(const Float_t   *f, Int_t n) final;
+   void     WriteFastArray(const Double_t  *d, Int_t n) final;
+   void     WriteFastArray(void  *start,  const TClass *cl, Int_t n=1, TMemberStreamer *s=nullptr) final;
+   Int_t    WriteFastArray(void **startp, const TClass *cl, Int_t n=1, Bool_t isPreAlloc=kFALSE, TMemberStreamer *s=nullptr) final;
 
-   virtual   void     ReadFastArray(Bool_t    *, Int_t );
-   virtual   void     ReadFastArray(Char_t    *, Int_t );
-   virtual   void     ReadFastArrayString(Char_t   *, Int_t );
-   virtual   void     ReadFastArray(UChar_t   *, Int_t );
-   virtual   void     ReadFastArray(Short_t   *, Int_t );
-   virtual   void     ReadFastArray(UShort_t  *, Int_t );
-   virtual   void     ReadFastArray(Int_t     *, Int_t );
-   virtual   void     ReadFastArray(UInt_t    *, Int_t );
-   virtual   void     ReadFastArray(Long_t    *, Int_t );
-   virtual   void     ReadFastArray(ULong_t   *, Int_t );
-   virtual   void     ReadFastArray(Long64_t  *, Int_t );
-   virtual   void     ReadFastArray(ULong64_t *, Int_t );
-   virtual   void     ReadFastArray(Float_t   *, Int_t );
-   virtual   void     ReadFastArray(Double_t  *, Int_t );
-   virtual   void     ReadFastArrayFloat16(Float_t  *f, Int_t n, TStreamerElement *ele=0);
-   virtual   void     ReadFastArrayDouble32(Double_t  *d, Int_t n, TStreamerElement *ele=0);
-   virtual   void     ReadFastArrayWithFactor(Float_t *ptr, Int_t n, Double_t factor, Double_t minvalue) ;
-   virtual   void     ReadFastArrayWithNbits(Float_t *ptr, Int_t n, Int_t nbits);
-   virtual   void     ReadFastArrayWithFactor(Double_t *ptr, Int_t n, Double_t factor, Double_t minvalue);
-   virtual   void     ReadFastArrayWithNbits(Double_t *ptr, Int_t n, Int_t nbits) ;
-   virtual   void     ReadFastArray(void  *, const TClass *, Int_t n=1, TMemberStreamer *s=0, const TClass *onFileClass=0);
-   virtual   void     ReadFastArray(void **, const TClass *, Int_t n=1, Bool_t isPreAlloc=kFALSE, TMemberStreamer *s=0, const TClass *onFileClass=0);
+   void     ReadFastArray(Bool_t    *, Int_t ) final;
+   void     ReadFastArray(Char_t    *, Int_t ) final;
+   void     ReadFastArrayString(Char_t   *, Int_t ) final;
+   void     ReadFastArray(UChar_t   *, Int_t ) final;
+   void     ReadFastArray(Short_t   *, Int_t ) final;
+   void     ReadFastArray(UShort_t  *, Int_t ) final;
+   void     ReadFastArray(Int_t     *, Int_t ) final;
+   void     ReadFastArray(UInt_t    *, Int_t ) final;
+   void     ReadFastArray(Long_t    *, Int_t ) final;
+   void     ReadFastArray(ULong_t   *, Int_t ) final;
+   void     ReadFastArray(Long64_t  *, Int_t ) final;
+   void     ReadFastArray(ULong64_t *, Int_t ) final;
+   void     ReadFastArray(Float_t   *, Int_t ) final;
+   void     ReadFastArray(Double_t  *, Int_t ) final;
+   void     ReadFastArrayFloat16(Float_t  *f, Int_t n, TStreamerElement *ele=nullptr) final;
+   void     ReadFastArrayDouble32(Double_t  *d, Int_t n, TStreamerElement *ele=nullptr) final;
+   void     ReadFastArrayWithFactor(Float_t *ptr, Int_t n, Double_t factor, Double_t minvalue)  final;
+   void     ReadFastArrayWithNbits(Float_t *ptr, Int_t n, Int_t nbits) final;
+   void     ReadFastArrayWithFactor(Double_t *ptr, Int_t n, Double_t factor, Double_t minvalue) final;
+   void     ReadFastArrayWithNbits(Double_t *ptr, Int_t n, Int_t nbits)  final;
+   void     ReadFastArray(void  *, const TClass *, Int_t n=1, TMemberStreamer *s=nullptr, const TClass *onFileClass=nullptr) final;
+   void     ReadFastArray(void **, const TClass *, Int_t n=1, Bool_t isPreAlloc=kFALSE, TMemberStreamer *s=nullptr, const TClass *onFileClass=nullptr) final;
 
-   ClassDef(TBufferSQL, 0); // Implementation of TBuffer to load and write to a SQL database
+   ClassDefOverride(TBufferSQL, 0); // Implementation of TBuffer to load and write to a SQL database
 
 };
 
