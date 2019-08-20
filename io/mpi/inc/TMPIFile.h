abc6d5c8f71c0f5590dcbb2921999def067868df
Yunsong Wang <yunsongwang@lbl.gov>
2019-08-20 15:59:57 -0700

    Add TMPIFile and update CMake for MPI

---
+++ io/mpi/inc/TMPIFile.h
@@ -0,0 +1,135 @@
+// @(#)root/io:$Id$
+// Author: Amit Bashyal, August 2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2009, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TMPIFile
+#define ROOT_TMPIFile
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TMPIFile                                                             //
+//                                                                      //
+// File Object derived from TMemFile.                                   //
+//                                                                      //
+// The TMPIFile class provides the ability to aggregate data across     //
+// many MPI ranks on a cluster into a single file. This can be useful   //
+// on HPCs or large clusters. The user must control the syncronization  //
+// of the data across multiple ranks via the Sync() function.           //
+// When Sync() is called, this triggers objects in the TFile space to   //
+// be communicated over MPI to a master writer which combines the data  //
+// before writing it to file.                                           //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ROOT_TClientInfo
+#include "TClientInfo.h"
+#endif
+#ifndef ROOT_TBits
+#include "TBits.h"
+#endif
+#ifndef ROOT_TFileMerger
+#include "TFileMerger.h"
+#endif
+#ifndef ROOT_TMemFile
+#include "TMemFile.h"
+#endif
+
+#include "mpi.h"
+
+#include <vector>
+
+class TMPIFile : public TMemFile {
+
+private:
+   Int_t fEndProcess = 0; // collector tracks number of exited processes
+   Int_t fSplitLevel;     // number of collectors to use
+   Int_t fMPIColor;       // used by MPI ranks to track which collector to use
+
+   Int_t fMPIGlobalRank;  // global rank number
+   Int_t fMPIGlobalSize;  // total ranks
+   Int_t fMPILocalRank;   // rank number in sub communicator
+   Int_t fMPILocalSize;   // number of ranks in sub communicator
+
+   MPI_Comm fSubComm;         // sub communicator handle
+   MPI_Request fMPIRequest;   // request place holder
+
+   TString fMPIFilename;      // output filename, only used by collector
+
+   char *fSendBuf = 0;        // message buffer, only used by worker
+
+   struct ParallelFileMerger : public TObject {
+   private:
+      using ClientColl_t = std::vector<TClientInfo>;
+
+      TString       fFilename;
+      TBits         fClientsContact;
+      UInt_t        fNClientsContact;
+      ClientColl_t  fClients;
+      TTimeStamp    fLastMerge;
+      TFileMerger   fMerger;
+      TClientInfo fClientInfo;
+   public:
+      ParallelFileMerger(const char *filename, Int_t compression_settings, Bool_t writeCache = kFALSE);
+      virtual ~ParallelFileMerger();
+
+      ULong_t Hash() const { return fFilename.Hash(); };
+      const char *GetName() const{ return fFilename; };
+
+      Bool_t InitialMerge(TFile *input);
+      Bool_t Merge();
+      Bool_t NeedMerge(Float_t clientThreshold);
+      Bool_t NeedFinalMerge() { return fClientsContact.CountBits() > 0; };
+      void RegisterClient(UInt_t clientID, TFile *file);
+
+   };
+
+   void SetOutputName();
+   void CheckSplitLevel();
+   void SplitMPIComm();
+   void UpdateEndProcess();
+
+   Bool_t IsReceived();
+
+public:
+   TMPIFile(const char *name, char *buffer, Long64_t size = 0, Option_t *option = "", 
+            Int_t split = 1, const char *ftitle = "", Int_t compress = 4);
+   TMPIFile(const char *name, Option_t *option = "", Int_t split = 1, 
+            const char *ftitle = "", Int_t compress = 4); // no complete implementation
+   virtual ~TMPIFile();
+
+   // some functions on MPI information
+   Int_t GetMPIGlobalSize() const { return fMPIGlobalSize; };
+   Int_t GetMPILocalSize() const { return fMPILocalSize; };
+   Int_t GetMPIGlobalRank() const { return fMPIGlobalRank; };
+   Int_t GetMPILocalRank() const { return fMPILocalRank; };
+   Int_t GetMPIColor() const { return fMPIColor; };
+   Int_t GetSplitLevel() const { return fSplitLevel; };
+
+   TString GetMPIFilename() const { return fMPIFilename; };
+
+   // Master Functions
+   void RunCollector(Bool_t cache = kFALSE);
+   static void R__MigrateKey(TDirectory *destination, TDirectory *source);
+   static void R__DeleteObject(TDirectory *dir, Bool_t withReset);
+   static Bool_t R__NeedInitialMerge(TDirectory *dir);
+   Bool_t IsCollector();
+
+   // Worker Functions
+   void CreateBufferAndSend();
+   // Empty Buffer to signal the end of job...
+   void CreateEmptyBufferAndSend();
+   void Sync();
+
+   // Finalize work and save output in disk.
+   void Close(Option_t *option="") final;
+
+   ClassDef(TMPIFile, 0)
+};
+#endif
---
+++ io/mpi/src/TClientInfo.cxx
@@ -0,0 +1,115 @@
+// @(#)root/io:$Id$
+// Author: Amit Bashyal, August 2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2002, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "TClientInfo.h"
+#include "TSystem.h"
+#include "TClass.h"
+#include "TKey.h"
+
+ClassImp(TClientInfo);
+
+TClientInfo::TClientInfo()
+    : fFile(0), fLocalName(), fContactsCount(0), fTimeSincePrevContact(0) {}
+
+TClientInfo::~TClientInfo() {}
+TClientInfo::TClientInfo(const char *filename, UInt_t clientId)
+    : fFile(0), fContactsCount(0), fTimeSincePrevContact(0) {
+  fLocalName.Form("%s-%d-%d", filename, clientId, gSystem->GetPid());
+}
+
+void TClientInfo::SetFile(TFile *file) {
+  {
+    // Register the new file as coming from this client.
+    if (file != fFile) {
+      if (fFile) {
+        R__MigrateKey(fFile, file);
+        // delete the previous memory file (if any)
+        delete file;
+      } else {
+        fFile = file;
+      }
+    }
+    TTimeStamp now;
+    fTimeSincePrevContact = now.AsDouble() - fLastContact.AsDouble();
+    fLastContact = now;
+    ++fContactsCount;
+  }
+}
+
+void TClientInfo::R__DeleteObject(TDirectory *dir, Bool_t withReset) {
+  if (dir == 0)
+    return;
+
+  TIter nextkey(dir->GetListOfKeys());
+  TKey *key;
+  while ((key = (TKey *)nextkey())) {
+    TClass *cl = TClass::GetClass(key->GetClassName());
+    if (cl->InheritsFrom(TDirectory::Class())) {
+      TDirectory *subdir =
+          (TDirectory *)dir->GetList()->FindObject(key->GetName());
+      if (!subdir) {
+        subdir = (TDirectory *)key->ReadObj();
+      }
+      R__DeleteObject(subdir, withReset);
+    } else {
+      Bool_t todelete = kFALSE;
+      if (withReset) {
+        todelete = (0 != cl->GetResetAfterMerge());
+      } else {
+        todelete = (0 == cl->GetResetAfterMerge());
+      }
+      if (todelete) {
+        key->Delete();
+        dir->GetListOfKeys()->Remove(key);
+        delete key;
+      }
+    }
+  }
+}
+
+void TClientInfo::R__MigrateKey(TDirectory *destination, TDirectory *source) {
+  if (destination == 0 || source == 0)
+    return;
+  TIter nextkey(source->GetListOfKeys());
+  // std::cout<<"TClientInfo::Trying to migrate the keys here"<<std::endl;
+  TKey *key;
+  while ((key = (TKey *)nextkey())) {
+    TClass *cl = TClass::GetClass(key->GetClassName());
+    if (cl->InheritsFrom(TDirectory::Class())) {
+      TDirectory *source_subdir =
+          (TDirectory *)source->GetList()->FindObject(key->GetName());
+      if (!source_subdir) {
+        source_subdir = (TDirectory *)key->ReadObj();
+      }
+      TDirectory *destination_subdir =
+          destination->GetDirectory(key->GetName());
+      if (!destination_subdir) {
+        destination_subdir = destination->mkdir(key->GetName());
+      }
+      R__MigrateKey(destination, source);
+    } else {
+      TKey *oldkey = destination->GetKey(key->GetName());
+      if (oldkey) {
+        oldkey->Delete();
+        delete oldkey;
+      }
+      TKey *newkey = new TKey(
+          destination, *key,
+          0 /* pidoffset */); // a priori the file are from the same client ..
+      destination->GetFile()->SumBuffer(newkey->GetObjlen());
+      newkey->WriteFile(0);
+      if (destination->GetFile()->TestBit(TFile::kWriteError)) {
+        return;
+      }
+    }
+  }
+  destination->SaveSelf();
+}
