ce147e1c79a5aba7f4b661812f7c3c59ce11fc3c
Emmanouil Michalainas <emmichal@cern.ch>
2019-08-21 14:07:56 +0200

    [RF] Vectorized RooBreitWigner.

--- roofit/roofit/src/RooBreitWigner.cxx
+++ roofit/roofit/src/RooBreitWigner.cxx
@@ -31,6 +31,7 @@ that models a non-relativistic Breit-Wigner shape
 #include "RooBreitWigner.h"
 #include "RooAbsReal.h"
 #include "RooRealVar.h"
+#include "BatchHelpers.h"
 // #include "RooFitTools/RooRandom.h"
 
 using namespace std;
@@ -63,6 +64,64 @@ Double_t RooBreitWigner::evaluate() const
 {
   Double_t arg= x - mean;
   return 1. / (arg*arg + 0.25*width*width);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace BreitWignerBatchEvaluate {
+//Author: Emmanouil Michalainas, CERN 21 August 2019
+
+template<class Tx, class Tmean, class Twidth>
+void compute(	size_t batchSize,
+              double * __restrict__ output,
+              Tx X, Tmean M, Twidth W)
+{
+  for (size_t i=0; i<batchSize; i++) {
+    const double arg = X[i]-M[i];
+    output[i] = 1 / (arg*arg + 0.25*W[i]*W[i]);
+  }
+}
+};
+
+RooSpan<double> RooBreitWigner::evaluateBatch(std::size_t begin, std::size_t batchSize) const {
+  using namespace BatchHelpers;
+  using namespace BreitWignerBatchEvaluate;
+  auto xData = x.getValBatch(begin, batchSize);
+  auto meanData = mean.getValBatch(begin, batchSize);
+  auto widthData = width.getValBatch(begin, batchSize);
+
+  batchSize = findSize({ xData, meanData, widthData });
+  auto output = _batchData.makeWritableBatchUnInit(begin, batchSize);
+
+  const bool batchX = !xData.empty();
+  const bool batchMean = !meanData.empty();
+  const bool batchWidth = !widthData.empty();
+  if (batchX && !batchMean && !batchWidth ) {
+    compute(batchSize, output.data(), xData, BracketAdapter<double>(mean), BracketAdapter<double>(width));
+  }
+  else if (!batchX && batchMean && !batchWidth ) {
+    compute(batchSize, output.data(), BracketAdapter<double>(x), meanData, BracketAdapter<double>(width));
+  }
+  else if (batchX && batchMean && !batchWidth ) {
+    compute(batchSize, output.data(), xData, meanData, BracketAdapter<double>(width));
+  }
+  else if (!batchX && !batchMean && batchWidth ) {
+    compute(batchSize, output.data(), BracketAdapter<double>(x), BracketAdapter<double>(mean), widthData);
+  }
+  else if (batchX && !batchMean && batchWidth ) {
+    compute(batchSize, output.data(), xData, BracketAdapter<double>(mean), widthData);
+  }
+  else if (!batchX && batchMean && batchWidth ) {
+    compute(batchSize, output.data(), BracketAdapter<double>(x), meanData, widthData);
+  }
+  else if (batchX && batchMean && batchWidth ) {
+    compute(batchSize, output.data(), xData, meanData, widthData);
+  }
+  else{
+    throw std::logic_error("Requested a batch computation, but no batch data available.");
+  }
+
+  return output;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
