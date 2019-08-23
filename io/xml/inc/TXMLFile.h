3e65ee0181fa96e580ec899e2a7bd8fac42767ca
Sergey Linev <S.Linev@gsi.de>
2019-08-23 14:31:04 +0200

    [xml] use c++11 interface for virtaul functions
    Use unique_ptr<TXMLEngine> as holder

--- io/xml/inc/TXMLFile.h
+++ io/xml/inc/TXMLFile.h
@@ -12,105 +12,106 @@
 #ifndef ROOT_TXMLFile
 #define ROOT_TXMLFile
 
-#include "Compression.h"
-#include "TXMLEngine.h"
 #include "TFile.h"
 #include "TXMLSetup.h"
+#include "TXMLEngine.h"
+#include "Compression.h"
+#include <memory>
 
 class TKeyXML;
 class TList;
 class TStreamerElement;
 class TStreamerInfo;
 
-class TXMLFile : public TFile, public TXMLSetup {
+class TXMLFile final : public TFile, public TXMLSetup {
 
 protected:
    void InitXmlFile(Bool_t create);
    // Interface to basic system I/O routines
-   virtual Int_t SysOpen(const char *, Int_t, UInt_t) { return 0; }
-   virtual Int_t SysClose(Int_t) { return 0; }
-   virtual Int_t SysRead(Int_t, void *, Int_t) { return 0; }
-   virtual Int_t SysWrite(Int_t, const void *, Int_t) { return 0; }
-   virtual Long64_t SysSeek(Int_t, Long64_t, Int_t) { return 0; }
-   virtual Int_t SysStat(Int_t, Long_t *, Long64_t *, Long_t *, Long_t *) { return 0; }
-   virtual Int_t SysSync(Int_t) { return 0; }
+   Int_t SysOpen(const char *, Int_t, UInt_t) final { return 0; }
+   Int_t SysClose(Int_t) final { return 0; }
+   Int_t SysRead(Int_t, void *, Int_t) final { return 0; }
+   Int_t SysWrite(Int_t, const void *, Int_t) final { return 0; }
+   Long64_t SysSeek(Int_t, Long64_t, Int_t) final { return 0; }
+   Int_t SysStat(Int_t, Long_t *, Long64_t *, Long_t *, Long_t *) final { return 0; }
+   Int_t SysSync(Int_t) final { return 0; }
 
    // Overwrite methods for directory I/O
-   virtual Long64_t DirCreateEntry(TDirectory *);
-   virtual Int_t DirReadKeys(TDirectory *);
-   virtual void DirWriteKeys(TDirectory *);
-   virtual void DirWriteHeader(TDirectory *);
+   Long64_t DirCreateEntry(TDirectory *) final;
+   Int_t DirReadKeys(TDirectory *) final;
+   void DirWriteKeys(TDirectory *) final;
+   void DirWriteHeader(TDirectory *) final;
 
-   InfoListRet GetStreamerInfoListImpl(bool lookupSICache);
+   InfoListRet GetStreamerInfoListImpl(bool lookupSICache) final;
 
 private:
    // let the compiler do the job. gcc complains when the following line is activated
-   // TXMLFile(const TXMLFile &) {}            //Files cannot be copied
-   void operator=(const TXMLFile &);
+   TXMLFile(const TXMLFile &);            // TXMLFile cannot be copied, not implemented
+   void operator=(const TXMLFile &);      // TXMLFile cannot be copied, not implemented
 
 public:
-   TXMLFile();
+   TXMLFile() = default;
    TXMLFile(const char *filename, Option_t *option = "read", const char *title = "title", Int_t compression = ROOT::RCompressionSetting::EDefaults::kUseGeneralPurpose);
    virtual ~TXMLFile();
 
-   virtual void Close(Option_t *option = ""); // *MENU*
-   virtual TKey *CreateKey(TDirectory *mother, const TObject *obj, const char *name, Int_t bufsize);
-   virtual TKey *CreateKey(TDirectory *mother, const void *obj, const TClass *cl, const char *name, Int_t bufsize);
-   virtual void DrawMap(const char * = "*", Option_t * = "") {}
-   virtual void FillBuffer(char *&) {}
-   virtual void Flush() {}
+   void Close(Option_t *option = "") final; // *MENU*
+   TKey *CreateKey(TDirectory *mother, const TObject *obj, const char *name, Int_t bufsize) final;
+   TKey *CreateKey(TDirectory *mother, const void *obj, const TClass *cl, const char *name, Int_t bufsize) final;
+   void DrawMap(const char * = "*", Option_t * = "") final {}
+   void FillBuffer(char *&) final {}
+   void Flush() final {}
 
-   virtual Long64_t GetEND() const { return 0; }
-   virtual Int_t GetErrno() const { return 0; }
-   virtual void ResetErrno() const {}
+   Long64_t GetEND() const final { return 0; }
+   Int_t GetErrno() const final { return 0; }
+   void ResetErrno() const final {}
 
-   virtual Int_t GetNfree() const { return 0; }
-   virtual Int_t GetNbytesInfo() const { return 0; }
-   virtual Int_t GetNbytesFree() const { return 0; }
-   virtual Long64_t GetSeekFree() const { return 0; }
-   virtual Long64_t GetSeekInfo() const { return 0; }
-   virtual Long64_t GetSize() const { return 0; }
+   Int_t GetNfree() const final { return 0; }
+   Int_t GetNbytesInfo() const final { return 0; }
+   Int_t GetNbytesFree() const final { return 0; }
+   Long64_t GetSeekFree() const final { return 0; }
+   Long64_t GetSeekInfo() const final { return 0; }
+   Long64_t GetSize() const final { return 0; }
 
    Int_t GetIOVersion() const { return fIOVersion; }
 
-   virtual Bool_t IsOpen() const;
+   Bool_t IsOpen() const final;
 
-   virtual void MakeFree(Long64_t, Long64_t) {}
-   virtual void MakeProject(const char *, const char * = "*", Option_t * = "new") {} // *MENU*
-   virtual void Map(Option_t *) {}                                                   //
-   virtual void Map() {}                                                             //
-   virtual void Paint(Option_t * = "") {}
-   virtual void Print(Option_t * = "") const {}
-   virtual Bool_t ReadBuffer(char *, Int_t) { return kFALSE; }
-   virtual Bool_t ReadBuffer(char *, Long64_t, Int_t) { return kFALSE; }
-   virtual void ReadFree() {}
-   virtual Int_t Recover() { return 0; }
-   virtual Int_t ReOpen(Option_t *mode);
-   virtual void Seek(Long64_t, ERelativeTo = kBeg) {}
+   void MakeFree(Long64_t, Long64_t) final {}
+   void MakeProject(const char *, const char * = "*", Option_t * = "new") final {} // *MENU*
+   void Map(Option_t *) final {}                                                   //
+   void Map() final {}                                                             //
+   void Paint(Option_t * = "") final {}
+   void Print(Option_t * = "") const final {}
+   Bool_t ReadBuffer(char *, Int_t) final { return kFALSE; }
+   Bool_t ReadBuffer(char *, Long64_t, Int_t) final { return kFALSE; }
+   void ReadFree() final {}
+   Int_t Recover() final { return 0; }
+   Int_t ReOpen(Option_t *mode) final;
+   void Seek(Long64_t, ERelativeTo = kBeg) final {}
 
-   virtual void SetEND(Long64_t) {}
-   virtual Int_t Sizeof() const { return 0; }
+   void SetEND(Long64_t) final {}
+   Int_t Sizeof() const final { return 0; }
 
-   virtual Bool_t WriteBuffer(const char *, Int_t) { return kFALSE; }
-   virtual Int_t Write(const char * = 0, Int_t = 0, Int_t = 0) { return 0; }
-   virtual Int_t Write(const char * = 0, Int_t = 0, Int_t = 0) const { return 0; }
-   virtual void WriteFree() {}
-   virtual void WriteHeader() {}
-   virtual void WriteStreamerInfo();
+   Bool_t WriteBuffer(const char *, Int_t) final { return kFALSE; }
+   Int_t Write(const char * = nullptr, Int_t = 0, Int_t = 0) final { return 0; }
+   Int_t Write(const char * = nullptr, Int_t = 0, Int_t = 0) const final { return 0; }
+   void WriteFree() final {}
+   void WriteHeader() final {}
+   void WriteStreamerInfo() final;
 
    // XML specific functions
 
-   virtual void SetXmlLayout(EXMLLayout layout);
-   virtual void SetStoreStreamerInfos(Bool_t iConvert = kTRUE);
-   virtual void SetUsedDtd(Bool_t use = kTRUE);
-   virtual void SetUseNamespaces(Bool_t iUseNamespaces = kTRUE);
+   void SetXmlLayout(EXMLLayout layout) final;
+   void SetStoreStreamerInfos(Bool_t iConvert = kTRUE) final;
+   void SetUsedDtd(Bool_t use = kTRUE) final;
+   void SetUseNamespaces(Bool_t iUseNamespaces = kTRUE) final;
 
    Bool_t AddXmlComment(const char *comment);
    Bool_t AddXmlStyleSheet(const char *href, const char *type = "text/css", const char *title = 0, int alternate = -1,
                            const char *media = 0, const char *charset = 0);
    Bool_t AddXmlLine(const char *line);
 
-   TXMLEngine *XML() { return fXML; }
+   TXMLEngine *XML() { return fXML.get(); }
 
 protected:
    // functions to store streamer infos
@@ -128,17 +129,17 @@ protected:
 
    static void ProduceFileNames(const char *filename, TString &fname, TString &dtdname);
 
-   XMLDocPointer_t fDoc; //!
+   XMLDocPointer_t fDoc{nullptr}; //!
 
-   XMLNodePointer_t fStreamerInfoNode; //!  pointer of node with streamer info data
+   XMLNodePointer_t fStreamerInfoNode{nullptr}; //!  pointer of node with streamer info data
 
-   TXMLEngine *fXML; //! object for interface with xml library
+   std::unique_ptr<TXMLEngine> fXML; //! object for interface with xml library
 
-   Int_t fIOVersion; //! indicates format of ROOT xml file
+   Int_t fIOVersion{0}; //! indicates format of ROOT xml file
 
-   Long64_t fKeyCounter; //! counter of created keys, used for keys id
+   Long64_t fKeyCounter{0}; //! counter of created keys, used for keys id
 
-   ClassDef(TXMLFile, 3) // ROOT file in XML format
+   ClassDefOverride(TXMLFile, 3) // ROOT file in XML format
 };
 
 #endif
