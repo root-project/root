0b9904e6a9bd4f6c727d4319a7cab980e2088f5d
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2019-08-26 17:55:30 +0200

    [RF] Remove unnecessary RooAbsData::getBatch().
    - getBatch() isn't used, any more, on datasets.
    - Fix spurious shadow warning in RooDataHist.

--- roofit/roofitcore/inc/RooAbsData.h
+++ roofit/roofitcore/inc/RooAbsData.h
@@ -91,7 +91,6 @@ public:
   virtual void weightError(Double_t& lo, Double_t& hi, ErrorType etype=Poisson) const ; 
   virtual const RooArgSet* get(Int_t index) const ;
 
-  virtual std::vector<RooSpan<const double>> getBatch(std::size_t first, std::size_t last) const = 0;
   virtual RooSpan<const double> getWeightBatch(std::size_t first, std::size_t last) const = 0;
 
   virtual Int_t numEntries() const ;
