bb3e5e0004a42250f9b3843b6cbd4ccb1d02aa17
Jakob Blomer <jblomer@cern.ch>
2019-08-27 14:23:10 +0200

    [ntuple] Fix linker error in unit test

--- tree/ntuple/v7/test/ntuple.cxx
+++ tree/ntuple/v7/test/ntuple.cxx
@@ -749,11 +749,11 @@ TEST(RNTuple, Descriptor)
    auto footerBuffer = new unsigned char[szFooter];
    reference.SerializeFooter(footerBuffer);
 
-   ASSERT_GE(szFooter, RNTupleDescriptor::kNBytesPostscript);
+   const auto nbytesPostscript = RNTupleDescriptor::kNBytesPostscript;
+   ASSERT_GE(szFooter, nbytesPostscript);
    std::uint32_t szPsHeader;
    std::uint32_t szPsFooter;
-   RNTupleDescriptor::LocateMetadata(footerBuffer + szFooter - RNTupleDescriptor::kNBytesPostscript,
-                                     szPsHeader, szPsFooter);
+   RNTupleDescriptor::LocateMetadata(footerBuffer + szFooter - nbytesPostscript, szPsHeader, szPsFooter);
    EXPECT_EQ(szHeader, szPsHeader);
    EXPECT_EQ(szFooter, szPsFooter);
 
