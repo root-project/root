573fd0e89210ab4dfa3d4530160bd3ba77dd20f9
Jakob Blomer <jblomer@cern.ch>
2019-08-27 21:44:54 +0200

    [ntuple] add skeleton for performance counters

---
+++ tree/ntuple/v7/src/RNTupleMetrics.cxx
@@ -0,0 +1,16 @@
+/// \file RNTupleMetrics.cxx
+/// \ingroup NTuple ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2019-08-27
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RNTupleMetrics.hxx>
