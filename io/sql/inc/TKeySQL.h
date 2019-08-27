608e337cf15d55d44b44a745b99022505ea92c99
Sergey Linev <S.Linev@gsi.de>
2019-08-27 08:42:12 +0200

    [sqlio] provide more c++11 in TSQLFile classes

--- io/sql/inc/TKeySQL.h
+++ io/sql/inc/TKeySQL.h
@@ -19,26 +19,26 @@ class TSQLFile;
 class TKeySQL final : public TKey {
 
 private:
-   TKeySQL(const TKeySQL &);            // TKeySQL objects are not copiable.
-   TKeySQL &operator=(const TKeySQL &); // TKeySQL objects are not copiable.
+   TKeySQL(const TKeySQL &) = delete;            // TKeySQL objects are not copiable.
+   TKeySQL &operator=(const TKeySQL &) = delete; // TKeySQL objects are not copiable.
 
 protected:
-   TKeySQL();
+   TKeySQL() = default;
 
    using TKey::Read;
 
    void StoreKeyObject(const void *obj, const TClass *cl);
    void *ReadKeyObject(void *obj, const TClass *expectedClass);
 
-   Long64_t fKeyId; ///<!  key identifier in KeysTables
-   Long64_t fObjId; ///<!  stored object identifier
+   Long64_t fKeyId{-1}; ///<!  key identifier in KeysTables
+   Long64_t fObjId{-1}; ///<!  stored object identifier
 
 public:
    TKeySQL(TDirectory *mother, const TObject *obj, const char *name, const char *title = nullptr);
    TKeySQL(TDirectory *mother, const void *obj, const TClass *cl, const char *name, const char *title = nullptr);
    TKeySQL(TDirectory *mother, Long64_t keyid, Long64_t objid, const char *name, const char *title,
            const char *keydatetime, Int_t cycle, const char *classname);
-   virtual ~TKeySQL();
+   virtual ~TKeySQL() = default;
 
    Bool_t IsKeyModified(const char *keyname, const char *keytitle, const char *keydatime, Int_t cycle, const char *classname);
 
