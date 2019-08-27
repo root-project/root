00ce0ab8e66f90faec8574b2fcd6d692cb95e4e7
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-08-27 13:49:51 +0200

    [Exp PyROOT] Add plotOn overloads of RooAbsData to RooDataHist
    This patch is to be merged into ROOT (modulo some modifications
    to cover cases that are still failing):
    https://bitbucket.org/wlav/cppyy-backend/src/master/cling/patches/using_decls.diff
    
    Without that patch, Cppyy does not automatically add to a class
    the method overloads that come from a 'using' statement.
    
    In the meantime, this commit adds a pythonisation that is
    needed for RooDataHist to see the overloads it is using from
    RooAbsData. Once the more general solution is implemented in the
    bindings, this pythonisation will be removed.

--- bindings/pyroot_experimental/PyROOT/src/PyzPythonHelpers.cxx
+++ bindings/pyroot_experimental/PyROOT/src/PyzPythonHelpers.cxx
@@ -19,7 +19,8 @@ PyROOT extension module.
 
 #include "CPyCppyy.h"
 #include "CPPInstance.h"
-#include "Utility.h"
+#include "CPPOverload.h"
+#include "PyStrings.h"
 
 #include "PyROOTPythonize.h"
 
@@ -105,3 +106,78 @@ PyObject *PyROOT::GetEndianess(PyObject * /* self */, PyObject * /* args */)
 #endif
 }
 
+using namespace CPyCppyy;
+
+// Helper to add base class methods to the derived class one
+static bool AddUsingToClass(PyObject *pyclass, const char *method)
+{
+   CPPOverload *derivedMethod = (CPPOverload *)PyObject_GetAttrString(pyclass, const_cast<char *>(method));
+   if (!CPPOverload_Check(derivedMethod)) {
+      Py_XDECREF(derivedMethod);
+      return false;
+   }
+
+   PyObject *mro = PyObject_GetAttr(pyclass, PyStrings::gMRO);
+   if (!mro || !PyTuple_Check(mro)) {
+      Py_XDECREF(mro);
+      Py_DECREF(derivedMethod);
+      return false;
+   }
+
+   CPPOverload *baseMethod = nullptr;
+   for (int i = 1; i < PyTuple_GET_SIZE(mro); ++i) {
+      baseMethod = (CPPOverload *)PyObject_GetAttrString(PyTuple_GET_ITEM(mro, i), const_cast<char *>(method));
+
+      if (!baseMethod) {
+         PyErr_Clear();
+         continue;
+      }
+
+      if (CPPOverload_Check(baseMethod))
+         break;
+
+      Py_DECREF(baseMethod);
+      baseMethod = nullptr;
+   }
+
+   Py_DECREF(mro);
+
+   if (!CPPOverload_Check(baseMethod)) {
+      Py_XDECREF(baseMethod);
+      Py_DECREF(derivedMethod);
+      return false;
+   }
+
+   for (PyCallable *pc : baseMethod->fMethodInfo->fMethods) {
+      derivedMethod->AdoptMethod(pc->Clone());
+   }
+
+   Py_DECREF(baseMethod);
+   Py_DECREF(derivedMethod);
+
+   return true;
+}
+
+////////////////////////////////////////////////////////////////////////////
+/// \brief Add base class overloads of a given method to a derived class
+/// \param[in] self Always null, since this is a module function.
+/// \param[in] args[0] Derived class.
+/// \param[in] args[1] Name of the method whose base class overloads to
+///                    inject in the derived class.
+///
+/// This function adds base class overloads to a derived class for a given
+/// method. This covers the 'using' case, which is not supported by default
+/// by the bindings.
+PyObject *PyROOT::AddUsingToClass(PyObject * /* self */, PyObject *args)
+{
+   // Get derived class to pythonize
+   PyObject *pyclass = PyTuple_GetItem(args, 0);
+
+   // Get method name where to add overloads
+   PyObject *pyname = PyTuple_GetItem(args, 1);
+   auto cppname = CPyCppyy_PyText_AsString(pyname);
+
+   AddUsingToClass(pyclass, cppname);
+
+   Py_RETURN_NONE;
+}
