b668bbf0a9f0c0db8f0dac6cf05d9eacc4fed63b
Philippe Canal <pcanal@fnal.gov>
2019-08-27 19:46:08 -0500

    Add missing brackes (fix else statement in TSystem::ExpandFileNam)

--- core/base/src/TSystem.cxx
+++ core/base/src/TSystem.cxx
@@ -1162,8 +1162,9 @@ again:
                strlcpy(x, p, kBufSize);
                x += kBufSize;
                break;
-            } else
+            } else {
                strcpy(x,p); x += lp; c = (b==c+1) ? e-1 : e;
+            }
          }
       }
    }
