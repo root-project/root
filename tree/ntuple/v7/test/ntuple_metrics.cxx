4231119175f543b2e880159513230ebf5d89c24f
Jakob Blomer <jblomer@cern.ch>
2019-08-29 04:21:24 +0200

    [ntuple] fix white spaces in unit test

--- tree/ntuple/v7/test/ntuple_metrics.cxx
+++ tree/ntuple/v7/test/ntuple_metrics.cxx
@@ -13,46 +13,46 @@ using RNTupleMetrics = ROOT::Experimental::Detail::RNTupleMetrics;
 
 TEST(Metrics, Counters)
 {
-	RNTupleMetrics metrics("test");
-	EXPECT_FALSE(metrics.IsEnabled());
+   RNTupleMetrics metrics("test");
+   EXPECT_FALSE(metrics.IsEnabled());
 
-	RNTuplePlainCounter *ctrOne = nullptr;
-	RNTupleAtomicCounter *ctrTwo = nullptr;
-	metrics.MakeCounter("plain", "s", "example 1", ctrOne);
-	metrics.MakeCounter("atomic", "s", "example 2", ctrTwo);
-	ASSERT_NE(nullptr, ctrOne);
-	ASSERT_NE(nullptr, ctrTwo);
-	EXPECT_TRUE(ctrOne->IsEnabled());
-	EXPECT_FALSE(ctrTwo->IsEnabled());
+   RNTuplePlainCounter *ctrOne = nullptr;
+   RNTupleAtomicCounter *ctrTwo = nullptr;
+   metrics.MakeCounter("plain", "s", "example 1", ctrOne);
+   metrics.MakeCounter("atomic", "s", "example 2", ctrTwo);
+   ASSERT_NE(nullptr, ctrOne);
+   ASSERT_NE(nullptr, ctrTwo);
+   EXPECT_TRUE(ctrOne->IsEnabled());
+   EXPECT_FALSE(ctrTwo->IsEnabled());
 
-	EXPECT_EQ(0, ctrOne->GetValue());
-	ctrOne->Inc();
-	ctrTwo->Inc();
-	EXPECT_EQ(1, ctrOne->GetValue());
-	EXPECT_EQ(0, ctrTwo->GetValue());
-	metrics.Enable();
-	EXPECT_TRUE(metrics.IsEnabled());
-	ctrTwo->Inc();
-	EXPECT_EQ(1, ctrTwo->XAdd(5));
-	EXPECT_EQ(1, ctrOne->GetValue());
-	EXPECT_EQ(6, ctrTwo->GetValue());
+   EXPECT_EQ(0, ctrOne->GetValue());
+   ctrOne->Inc();
+   ctrTwo->Inc();
+   EXPECT_EQ(1, ctrOne->GetValue());
+   EXPECT_EQ(0, ctrTwo->GetValue());
+   metrics.Enable();
+   EXPECT_TRUE(metrics.IsEnabled());
+   ctrTwo->Inc();
+   EXPECT_EQ(1, ctrTwo->XAdd(5));
+   EXPECT_EQ(1, ctrOne->GetValue());
+   EXPECT_EQ(6, ctrTwo->GetValue());
 }
 
 TEST(Metrics, Timer)
 {
-	RNTupleAtomicCounter ctrWallTime("wall time", "ns", "");
-	ROOT::Experimental::Detail::RNTupleTickCounter<RNTupleAtomicCounter> ctrCpuTicks("cpu time", "ns", "");
-	{
-		RNTupleAtomicTimer timer(ctrWallTime, ctrCpuTicks);
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	}
-	EXPECT_EQ(0U, ctrWallTime.GetValue());
-	EXPECT_EQ(0U, ctrCpuTicks.GetValue());
-	ctrWallTime.Enable();
-	ctrCpuTicks.Enable();
-	{
-		RNTupleAtomicTimer timer(ctrWallTime, ctrCpuTicks);
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	}
-	EXPECT_GT(ctrWallTime.GetValue(), 0U);
+   RNTupleAtomicCounter ctrWallTime("wall time", "ns", "");
+   ROOT::Experimental::Detail::RNTupleTickCounter<RNTupleAtomicCounter> ctrCpuTicks("cpu time", "ns", "");
+   {
+      RNTupleAtomicTimer timer(ctrWallTime, ctrCpuTicks);
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   }
+   EXPECT_EQ(0U, ctrWallTime.GetValue());
+   EXPECT_EQ(0U, ctrCpuTicks.GetValue());
+   ctrWallTime.Enable();
+   ctrCpuTicks.Enable();
+   {
+      RNTupleAtomicTimer timer(ctrWallTime, ctrCpuTicks);
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   }
+   EXPECT_GT(ctrWallTime.GetValue(), 0U);
 }
