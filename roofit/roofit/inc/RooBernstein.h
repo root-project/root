958fed1bdbb23d8e2d65104383ef26d264133a1f
Rahul Balasubramanian <rahul.balasubramanian@cern.ch>
2019-08-30 08:43:43 +0200

    bugfixes for RooBernstein

--- roofit/roofit/inc/RooBernstein.h
+++ roofit/roofit/inc/RooBernstein.h
@@ -39,7 +39,7 @@ private:
 
   RooRealProxy _x;
   RooListProxy _coefList ;
-  mutable TNamed* _refRangeName ;
+  const TNamed* _refRangeName ;
 
   Double_t evaluate() const;
   RooSpan<double> evaluateBatch(std::size_t begin, std::size_t batchSize) const;
