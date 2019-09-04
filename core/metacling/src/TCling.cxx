dbc68fa577f6983b62d060ab45a659f0fd78fcbf
Philippe Canal <pcanal@fnal.gov>
2019-09-04 11:10:21 -0500

    Move declaration next to use

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -6488,7 +6488,6 @@ void TCling::UpdateClassInfoWithDecl(const void* vTD)
          // Ignore declaration within a function.
          return;
       }
-      clang::QualType type( td->getTypeForDecl(), 0 );
 
       auto declName=ND->getNameAsString();
       if (!TClass::HasNoInfoOrEmuOrFwdDeclaredDecl(declName.c_str())){
@@ -6496,6 +6495,7 @@ void TCling::UpdateClassInfoWithDecl(const void* vTD)
          return;
       }
 
+      clang::QualType type( td->getTypeForDecl(), 0 );
       ROOT::TMetaUtils::GetNormalizedName(name, type, *fInterpreter, *fNormalizedCtxt);
    } else {
       name = ND->getNameAsString();
