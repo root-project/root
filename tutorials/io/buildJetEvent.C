335a3a63efe379a069a98b4e2a7ed3397efb45a8
Yunsong Wang <yunsongwang@lbl.gov>
2019-09-06 10:38:37 -0700

    Cleanup & code formatting

--- tutorials/io/buildJetEvent.C
+++ tutorials/io/buildJetEvent.C
@@ -1,4 +1,5 @@
-void buildJetEvent() {
+void buildJetEvent()
+{
    TString tutdir = gROOT->GetTutorialDir();
    gROOT->ProcessLine(".include " + tutdir + "/tree");
    gROOT->ProcessLine(".L " + tutdir + "/tree/JetEvent.cxx+");
