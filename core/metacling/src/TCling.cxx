ab69636e9f5c0637db31b2b22559479a92cdac06
Philippe Canal <pcanal@fnal.gov>
2019-09-18 10:53:47 -0500

    Indicates TCling's is shutting down in TCling's destructor

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1387,6 +1387,7 @@ TCling::TCling(const char *name, const char *title, const char* const argv[])
 
 TCling::~TCling()
 {
+   fIsShuttingDown = true;
    delete fMapfile;
 //    delete fMapNamespaces;
    delete fRootmapFiles;
