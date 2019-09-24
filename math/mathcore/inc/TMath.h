8f4278ee02c8a759aef23b4e16c6c3546614b0b6
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2019-09-24 11:00:40 +0200

    Specify input ariable names in TMath::Atan2. This fixes ROOT-10312

--- math/mathcore/inc/TMath.h
+++ math/mathcore/inc/TMath.h
@@ -360,7 +360,7 @@ inline Double_t TanH(Double_t);
 inline Double_t ASin(Double_t);
 inline Double_t ACos(Double_t);
 inline Double_t ATan(Double_t);
-inline Double_t ATan2(Double_t, Double_t);
+inline Double_t ATan2(Double_t y, Double_t x);
 Double_t ASinH(Double_t);
 Double_t ACosH(Double_t);
 Double_t ATanH(Double_t);
