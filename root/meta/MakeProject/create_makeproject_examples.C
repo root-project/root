205867872df9e257b3df4f67aa74597d4eda105a
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-09-25 08:07:17 +0300

    [cxxmodules] Enable and disable the modulemap file if needed.
    Patch by Oksana Shadura!

--- root/meta/MakeProject/create_makeproject_examples.C
+++ root/meta/MakeProject/create_makeproject_examples.C
@@ -2,14 +2,9 @@ R__LOAD_LIBRARY(stl_makeproject_test)
 
 #include <stl_makeproject_test.h>
 
-bool file_exists (const std::string &file_name) {
-   return gSystem->AccessPathName(file_name.c_str(), kWritePermission);
-}
 int create_makeproject_examples()
 {
-   if (file_exists("./disabled.module.modulemap")) {
-      gSystem->Rename( "./disabled.module.modulemap" , "./module.modulemap");
-   }
+   gSystem->Unlink("./stl_example.root");
    TFile _file0("stl_example.root", "RECREATE");
    SillyStlEvent *event = nullptr;
    TTree tree("T", "test tree");
@@ -20,6 +15,5 @@ int create_makeproject_examples()
    tree.Write();
    _file0.Close();
    gSystem->Unlink("./stl_makeproject_test.rootmap");
-   gSystem->Rename( "./module.modulemap" , "./disabled.module.modulemap");
    return 0;
 }
