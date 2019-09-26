e83059c562ffea55d7847487e8578a672143dccd
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2019-09-26 17:00:53 +0200

    Add a new gConfig.IONames fielf, fWrightFileDIrPrefix. This allows to have add a prefix for the directory to store the weights. By default they are stored in the directory starting with the dataset name.
    With the prefix they will be stored in weightfile_prefix/dataset_name/weight_file_name.
    This fixes ROOT-8887

--- tmva/tmva/inc/TMVA/Config.h
+++ tmva/tmva/inc/TMVA/Config.h
@@ -1,4 +1,4 @@
-// @(#)root/tmva $Id$   
+// @(#)root/tmva $Id$
 // Author: Andreas Hoecker, Joerg Stelzer, Fredrik Tegenfeldt, Helge Voss
 
 /**********************************************************************************
@@ -79,7 +79,7 @@ namespace TMVA {
 //      ROOT::TSequentialExecutor &GetSeqExecutor() { return *fSeqfPool; }
 #endif
       /// Get executor class for multi-thread usage
-      /// In case when  MT is not enabled will return a serial executor 
+      /// In case when  MT is not enabled will return a serial executor
       Executor & GetThreadExecutor() { return fExecutor; }
 
       /// Enable MT in TMVA (by default is on when ROOT::EnableImplicitMT() is set
@@ -90,7 +90,7 @@ namespace TMVA {
 
       ///Check if IMT is enabled
       Bool_t IsMTEnabled() const { return  fExecutor.GetPoolSize() > 1; }
-      
+
    public:
 
       class VariablePlotting;
@@ -119,13 +119,14 @@ namespace TMVA {
       class IONames {
 
       public:
-
+         // this is name of weight file directory
+         TString fWeightFileDirPrefix;
          TString fWeightFileDir;
          TString fWeightFileExtension;
          TString fOptionsReferenceFileDir;
       } fIONames; // Customisable weight file properties
-         
-      
+
+
    private:
 
       // private constructor
@@ -137,7 +138,7 @@ namespace TMVA {
       static std::atomic<Config*> fgConfigPtr;
 #else
       static Config* fgConfigPtr;
-#endif                  
+#endif
    private:
 
 #if __cplusplus > 199711L
@@ -155,7 +156,7 @@ namespace TMVA {
 #endif
       mutable MsgLogger* fLogger;   // message logger
       MsgLogger& Log() const { return *fLogger; }
-         
+
       ClassDef(Config,0); // Singleton class for global configuration settings
    };
 
