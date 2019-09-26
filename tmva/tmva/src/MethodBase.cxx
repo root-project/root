a8f3d8435f0b445efb910d2bef3bf0f616bd29e8
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2019-09-26 17:00:53 +0200

    Add a new gConfig.IONames fielf, fWrightFileDIrPrefix. This allows to have add a prefix for the directory to store the weights. By default they are stored in the directory starting with the dataset name.
    With the prefix they will be stored in weightfile_prefix/dataset_name/weight_file_name.
    This fixes ROOT-8887

--- tmva/tmva/src/MethodBase.cxx
+++ tmva/tmva/src/MethodBase.cxx
@@ -764,8 +764,8 @@ void TMVA::MethodBase::AddRegressionOutput(Types::ETreeType type)
    regRes->Resize( nEvents );
 
    // Drawing the progress bar every event was causing a huge slowdown in the evaluation time
-   // So we set some parameters to draw the progress bar a total of totalProgressDraws, i.e. only draw every 1 in 100 
-   
+   // So we set some parameters to draw the progress bar a total of totalProgressDraws, i.e. only draw every 1 in 100
+
    Int_t totalProgressDraws = 100; // total number of times to update the progress bar
    Int_t drawProgressEvery = 1;    // draw every nth event such that we have a total of totalProgressDraws
    if(nEvents >= totalProgressDraws) drawProgressEvery = nEvents/totalProgressDraws;
@@ -1985,7 +1985,7 @@ TDirectory* TMVA::MethodBase::BaseDir() const
          sdir = methodDir->mkdir(defaultDir);
          sdir->cd();
          // write weight file name into target file
-         if (fModelPersistence) { 
+         if (fModelPersistence) {
             TObjString wfilePath( gSystem->WorkingDirectory() );
             TObjString wfileName( GetWeightFileName() );
             wfilePath.Write( "TrainingPath" );
@@ -2042,7 +2042,7 @@ TDirectory *TMVA::MethodBase::MethodBaseDir() const
 void TMVA::MethodBase::SetWeightFileDir( TString fileDir )
 {
    fFileDir = fileDir;
-   gSystem->MakeDirectory( fFileDir );
+   gSystem->mkdir( fFileDir, kTRUE );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -3208,7 +3208,7 @@ void TMVA::MethodBase::MakeClass( const TString& theClassFileName ) const
           GetMethodType() != Types::kHMatrix) {
          fout << "         Transform( iV, -1 );" << std::endl;
       }
-      
+
       if(GetAnalysisType() == Types::kMulticlass) {
          fout << "         retval = GetMulticlassValues__( iV );" << std::endl;
       } else {
