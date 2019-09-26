a6d958c04e36ffbeecedf355061d04e4994b436d
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-09-26 11:55:56 +0200

    [Exp PyROOT] TFile: throw exception when trying to open a non-existing file
    An error is raised when ROOT.TFile(file_name) and
    ROOT.TFile.Open(file_name) try to open a non-existing file, as suggested
    in https://sft.its.cern.ch/jira/browse/ROOT-9915

--- bindings/pyroot_experimental/pythonizations/python/ROOT/pythonization/_tfile.py
+++ bindings/pyroot_experimental/pythonizations/python/ROOT/pythonization/_tfile.py
@@ -1,4 +1,4 @@
-# Author: Danilo Piparo CERN  08/2018
+# Author: Danilo Piparo, Massimiliano Galli CERN  08/2018
 
 ################################################################################
 # Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.                      #
@@ -15,12 +15,45 @@
 
 from libROOTPythonizations import AddFileOpenPyz
 from ROOT import pythonization
+from libcppyy import bind_object
+
+def _TFileConstructor(self, *args):
+    # Redefinition of ROOT.TFile(str, ...):
+    # check if the instance of TFile has IsZombie() = True
+    # and raise OSError if so.
+    # Parameters:
+    # self: instance of TFile class
+    # *args: arguments passed to the constructor
+    self._OriginalConstructor(*args)
+    if len(args) >= 1:
+        if self.IsZombie():
+            raise OSError('Failed to open file {}'.format(args[0]))
+
+def _TFileOpen(klass, *args):
+    # Redefinition of ROOT.TFile.Open(str, ...):
+    # check if the instance of TFile is a C++ nullptr and raise a
+    # OSError if this is the case.
+    # Parameters:
+    # klass: TFile class
+    # *args: arguments passed to the constructor
+    f = klass._OriginalOpen(*args)
+    if f == bind_object(0, klass):
+        # args[0] can be either a string or a TFileOpenHandle
+        raise OSError('Failed to open file {}'.format(str(args[0])))
+    return f
 
 # Pythonizor function
 @pythonization()
 def pythonize_tfile(klass, name):
 
     if name == 'TFile':
-       AddFileOpenPyz(klass)
+        # Pythonizations for TFile::Open
+        AddFileOpenPyz(klass)
+        klass._OriginalOpen = klass.Open
+        klass.Open = classmethod(_TFileOpen)
+
+        # Pythonization for TFile constructor
+        klass._OriginalConstructor = klass.__init__
+        klass.__init__ = _TFileConstructor
 
     return True
