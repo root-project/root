789ceadfc7bfc9116304575e618a4d68ea2d4ca9
Olivier Couet <olivier.couet@cern.ch>
2019-09-26 09:46:04 +0200

    In some cases vertical hatches drawn by PaintHatches disappeared.
    This was reported here ROOT-10094

--- graf2d/gpad/src/TPad.cxx
+++ graf2d/gpad/src/TPad.cxx
@@ -3924,8 +3924,8 @@ void TPad::PaintFillAreaNDC(Int_t n, Double_t *x, Double_t *y, Option_t *option)
 
 void TPad::PaintFillAreaHatches(Int_t nn, Double_t *xx, Double_t *yy, Int_t FillStyle)
 {
-   static Double_t ang1[10] = {0., 10., 20., 30., 45.,5., 60., 70., 80., 90.};
-   static Double_t ang2[10] = {180.,170.,160.,150.,135.,5.,120.,110.,100., 90.};
+   static Double_t ang1[10] = {  0., 10., 20., 30., 45.,5., 60., 70., 80., 89.99};
+   static Double_t ang2[10] = {180.,170.,160.,150.,135.,5.,120.,110.,100., 89.99};
 
    Int_t fasi  = FillStyle%1000;
    Int_t idSPA = (Int_t)(fasi/100);
@@ -3989,7 +3989,7 @@ void TPad::PaintHatches(Double_t dy, Double_t angle,
 {
    Int_t i, i1, i2, nbi, m, inv;
    Double_t ratiox, ratioy, ymin, ymax, yrot, ycur;
-   const Double_t angr  = TMath::Pi()*(180-angle)/180.;
+   const Double_t angr  = TMath::Pi()*(180.-angle)/180.;
    const Double_t epsil = 0.0001;
    const Int_t maxnbi = 100;
    Double_t xli[maxnbi], xlh[2], ylh[2], xt1, xt2, yt1, yt2;
@@ -3999,8 +3999,8 @@ void TPad::PaintHatches(Double_t dy, Double_t angle,
    Double_t rwxmax = gPad->GetX2();
    Double_t rwymin = gPad->GetY1();
    Double_t rwymax = gPad->GetY2();
-   ratiox = 1/(rwxmax-rwxmin);
-   ratioy = 1/(rwymax-rwymin);
+   ratiox = 1./(rwxmax-rwxmin);
+   ratioy = 1./(rwymax-rwymin);
 
    Double_t sina = TMath::Sin(angr), sinb;
    Double_t cosa = TMath::Cos(angr), cosb;
