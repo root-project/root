5c27e365773fcf4506f63da058be905eb28cd69e
Sergey Linev <S.Linev@gsi.de>
2019-09-30 13:04:27 +0200

    [rcolor] let use hex and plain color name
    RColor will support several formats. Now it is:
     hex - 6 digits like 00FF00 (default now)
     rgb - decimal R,G,B  like 0,255,0
     name - plain SVG name like "white" or "black"
     
    For all parameters one could specify alpha - double value between 0 and
    1.

--- graf2d/gpadv7/v7/inc/ROOT/RColor.hxx
+++ graf2d/gpadv7/v7/inc/ROOT/RColor.hxx
@@ -29,34 +29,112 @@ namespace Experimental {
 
 class RColor : public RAttrBase {
 
-   R__ATTR_CLASS(RColor, "color_", AddString("rgb", "0,0,0").AddDouble("a", 1.));
+   R__ATTR_CLASS(RColor, "color_", AddString("hex", "").AddString("rgb", "").AddString("name", "").AddDouble("a", 1.));
 
    using RGB_t = std::array<int, 3>;
 
-   RColor(int r, int g, int b) : RColor() { SetRGB(r, g, b); }
+protected:
+
+   std::string toHex(int v) const
+   {
+      static const char *digits = "0123456789ABCDEF";
+      if (v < 0)
+         v = 0;
+      else if (v > 255)
+         v = 255;
+
+      std::string res(2,'0');
+      res[0] = digits[v >> 4];
+      res[1] = digits[v & 0xf];
+      return res;
+   }
+
+public:
+
+   RColor(int r, int g, int b) : RColor() { SetHex(r, g, b); }
 
    RColor(int r, int g, int b, double alfa) : RColor()
    {
-      SetRGB(r, g, b);
+      SetHex(r, g, b);
       SetAlpha(alfa);
    }
 
-   RColor(const RGB_t &rgb) : RColor() { SetRGB(rgb[0], rgb[1], rgb[2]); }
+   RColor(const RGB_t &rgb) : RColor() { SetHex(rgb[0], rgb[1], rgb[2]); }
 
    std::string GetRGB() const { return GetValue<std::string>("rgb"); }
-   RColor &SetRGB(const std::string &_rgb) { SetValue("rgb", _rgb); return *this; }
-   RColor &SetRGB(int r, int g, int b) { return SetRGB(std::to_string(r) + "," + std::to_string(g) + "," + std::to_string(b)); }
+
+   RColor &SetRGB(int r, int g, int b)
+   {
+      return SetRGB(std::to_string(r) + "," + std::to_string(g) + "," + std::to_string(b));
+   }
+
+   RColor &SetRGB(const std::string &_rgb)
+   {
+      ClearValue("hex");
+      ClearValue("name");
+      SetValue("rgb", _rgb);
+      return *this;
+   }
+
+   /** Set r/g/b/ components of color as hex code, default for the color */
+   RColor &SetHex(int r, int g, int b)
+   {
+      return SetRGB(toHex(r) + toHex(g) + toHex(b));
+   }
+
+   /** Set color as hex string like 00FF00 */
+   RColor &SetHex(const std::string &_hex)
+   {
+      ClearValue("rgb");
+      ClearValue("name");
+      SetValue("hex", _hex);
+      return *this;
+   }
+
+   std::string GetHex() const { return GetValue<std::string>("hex"); }
+
+   RColor &SetName(const std::string &_name)
+   {
+      ClearValue("hex");
+      ClearValue("rgb");
+      SetValue("name", _name);
+      return *this;
+   }
+
+   std::string GetName() const { return GetValue<std::string>("name"); }
+
 
    double GetAlpha() const { return GetValue<double>("a"); }
    bool HasAlpha() const { return HasValue("a"); }
-   RColor &SetAlpha(double _alfa) { SetValue("a", _alfa); return *this; }
+   RColor &SetAlpha(double _alfa)
+   {
+      SetValue("a", _alfa);
+      return *this;
+   }
 
    std::string AsSVG() const
    {
+      bool has_alpha = HasAlpha();
+      double alpha = has_alpha ? GetAlpha() : 1.;
+
+      auto hex = GetHex();
+      if (!hex.empty()) {
+         std::string res = "#";
+         res.append(hex);
+         if (has_alpha) res.append(toHex((int) (alpha*255)));
+         return res;
+      }
+
       auto rgb = GetRGB();
-      if (HasAlpha())
-         return std::string("rgba(") + rgb + "," + std::to_string(GetAlpha()) + ")";
-       return std::string("rgb(") + rgb + ")";
+      if (!rgb.empty()) {
+         if (has_alpha)
+            return std::string("rgba(") + rgb + "," + std::to_string(alpha) + ")";
+         else
+            return std::string("rgb(") + rgb + ")";
+      }
+
+      // check that alpha is not specified
+      return GetName();
    }
 
    static constexpr RGB_t kRed{{255, 0, 0}};
@@ -68,6 +146,8 @@ class RColor : public RAttrBase {
    static constexpr double kOpaque{1.};
 
    friend bool operator==(const RColor& lhs, const RColor& rhs){ return (lhs.GetRGB() == rhs.GetRGB()) && (lhs.HasAlpha() == rhs.HasAlpha()) && (lhs.GetAlpha() == rhs.GetAlpha()); }
+
+
 
 };
 
