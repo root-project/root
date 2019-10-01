root-project/root

commit 61c9927f1ab547a131136d4bbb01ce6610c22c9f

[TTree] Disassociate basket from memory buffer for bulk IO
When the user provides a memory buffer (i.e., for bulk IO), we should
make sure to drop all references to that buffer in the TTree afterward.

tree/tree/src/TBranch.cxx
@@ -89,6 +89,7 @@ TBranch::TBranch()
 , fEntryOffsetLen(1000)
 , fWriteBasket(0)
 , fEntryNumber(0)
+, fExtraBasket(nullptr)
 , fOffset(0)
 , fMaxBaskets(10)
 , fNBaskets(0)
@@ -198,6 +199,7 @@ TBranch::TBranch(TTree *tree, const char *name, void *address, const char *leafl
 , fEntryOffsetLen(0)
 , fWriteBasket(0)
 , fEntryNumber(0)
+, fExtraBasket(nullptr)
 , fIOFeatures(tree ? tree->GetIOFeatures().GetFeatures() : 0)
 , fOffset(0)
 , fMaxBaskets(10)
@@ -251,6 +253,7 @@ TBranch::TBranch(TBranch *parent, const char *name, void *address, const char *l
 , fEntryOffsetLen(0)
 , fWriteBasket(0)
 , fEntryNumber(0)
+, fExtraBasket(nullptr)
 , fIOFeatures(parent->fIOFeatures)
 , fOffset(0)
 , fMaxBaskets(10)
@@ -1324,7 +1327,7 @@ Int_t TBranch::GetBasketAndFirst(TBasket*&basket, Long64_t &first,
 {
    Long64_t updatedNext = fNextBasketEntry;
    Long64_t entry = fReadEntry;
-   if (R__likely(fFirstBasketEntry <= entry && entry < fNextBasketEntry)) {
+   if (R__likely(fCurrentBasket && fFirstBasketEntry <= entry && entry < fNextBasketEntry)) {
       // We have found the basket containing this entry.
       // make sure basket buffers are in memory.
       basket = fCurrentBasket;
@@ -1375,7 +1378,12 @@ Int_t TBranch::GetBasketAndFirst(TBasket*&basket, Long64_t &first,
          fFirstBasketEntry = first;
          fNextBasketEntry = updatedNext;
       }
-      fCurrentBasket = basket;
+      if (user_buffer) {
+         fCurrentBasket = nullptr;
+         fBaskets[fReadBasket] = nullptr;
+      } else {
+         fCurrentBasket = basket;
+      }
    }
    return 1;
 }
@@ -1445,9 +1453,14 @@ Int_t TBranch::GetEntriesFast(Long64_t entry, TBuffer &user_buf)
    buf->SetBufferOffset(bufbegin);
 
    Int_t N = ((fNextBasketEntry < 0) ? fEntryNumber : fNextBasketEntry) - first;
-   //printf("Requesting %d events; fNextBasketEntry=%d; first=%d.\n", N, fNextBasketEntry, first);
+   //printf("Requesting %d events; fNextBasketEntry=%lld; first=%lld.\n", N, fNextBasketEntry, first);
    if (R__unlikely(!leaf->ReadBasketFast(*buf, N))) {printf("Leaf failed to read.\n"); return -1;}
    user_buf.SetBufferOffset(bufbegin);
+
+   fCurrentBasket = nullptr;
+   fBaskets[fReadBasket] = nullptr;
+   fExtraBasket = basket;
+   basket->DisownBuffer();
 
    return N;
 }
@@ -1492,7 +1505,7 @@ TBranch::GetEntriesSerialized(Long64_t entry, TBuffer &user_buf, TBuffer *count_
    buf->SetBufferOffset(bufbegin);
 
    Int_t N = ((fNextBasketEntry < 0) ? fEntryNumber : fNextBasketEntry) - first;
-   //printf("Requesting %d events; fNextBasketEntry=%d; first=%lld.\n", N, fNextBasketEntry, first);
+   //Info("GetEntriesSerialized", "Requesting %d events; fNextBasketEntry=%lld; first=%lld.\n", N, fNextBasketEntry, first);
 
    if (R__unlikely(!leaf->ReadBasketSerialized(*buf, N))) {
       printf("Leaf failed to read.\n");
@@ -1731,6 +1744,11 @@ TFile* TBranch::GetFile(Int_t mode)
 TBasket* TBranch::GetFreshBasket(TBuffer* user_buffer)
 {
    TBasket *basket = 0;
+   if (user_buffer && fExtraBasket) {
+      basket = fExtraBasket;
+      fExtraBasket = nullptr;
+      basket->AdoptBuffer(user_buffer);
+   }
    if (GetTree()->MemoryFull(0)) {
       if (fNBaskets==1) {
          // Steal the existing basket
@@ -1765,9 +1783,7 @@ TBasket* TBranch::GetFreshBasket(TBuffer* user_buffer)
    } else {
       basket = fTree->CreateBasket(this);
    }
-   if (user_buffer) {
-      user_buffer->SetSlaveBuffer(*basket->GetBufferRef());
-   }
+   basket->AdoptBuffer(user_buffer);
    return basket;
 }
 

tree/tree/inc/TBasket.h
@@ -108,8 +108,10 @@ public:
    TBasket(const char *name, const char *title, TBranch *branch);
    virtual ~TBasket();
 
+           void    AdoptBuffer(TBuffer *user_buffer);
    virtual void    AdjustSize(Int_t newsize);
    virtual void    DeleteEntryOffset();
+           void    DisownBuffer();
    virtual Int_t   DropBuffers();
    TBranch        *GetBranch() const {return fBranch;}
            Int_t   GetBufferSize() const {return fBufferSize;}

tree/tree/inc/TBranch.h
@@ -93,6 +93,7 @@ protected:
    Int_t       fEntryOffsetLen;   ///<  Initial Length of fEntryOffset table in the basket buffers
    Int_t       fWriteBasket;      ///<  Last basket number written
    Long64_t    fEntryNumber;      ///<  Current entry number (last one filled in this branch)
+   TBasket    *fExtraBasket;      ///<! Allocated basket not currently holding any data.
    TIOFeatures fIOFeatures;       ///<  IO features for newly-created baskets.
    Int_t       fOffset;           ///<  Offset of this branch
    Int_t       fMaxBaskets;       ///<  Maximum number of Baskets so far

tree/tree/src/TBasket.cxx
@@ -700,6 +700,26 @@ Int_t TBasket::ReadBasketBytes(Long64_t pos, TFile *file)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Disown all references to the internal buffer - some other object likely now
+/// owns it.
+///
+/// This TBasket is now useless and invalid until it is told to adopt a buffer.
+void TBasket::DisownBuffer()
+{
+   fBufferRef = NULL;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Adopt a buffer from an external entity
+void TBasket::AdoptBuffer(TBuffer *user_buffer)
+{
+   delete fBufferRef;
+   fBufferRef = user_buffer;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 /// Reset the basket to the starting state. i.e. as it was after calling
 /// the constructor (and potentially attaching a TBuffer.)
 /// Reduce memory used by fEntryOffset and the TBuffer if needed ..

commit 55235aefb8f0a9f9208d523cf72e6aa53be32866

Remove inadvertent deletion of TBuffer in TBranch::GetFreshBasket.
This problem was introduced by 61c9927f1ab547a131136d4bbb01ce6610c22c9f with the introduction of the call
to basket->AdoptBuffer(user_buffer); which was misplaced and in the normal case has user_buffer==nullptr resulting
in the TBuffer deletion.  With combined with a preexisting problem (leak of TBufferFile objects when TBranch::GetFreshBasket
is called with fBufferRef being a nullptr) lead to a memory leak when reading uncommpressed file.

tree/tree/src/TBranch.cxx
@@ -1471,6 +1471,7 @@ Int_t TBranch::GetBulkEntries(Long64_t entry, TBuffer &user_buf)
 
    fCurrentBasket = nullptr;
    fBaskets[fReadBasket] = nullptr;
+   R__ASSERT(fExtraBasket == nullptr && "fExtraBasket should have been set to nullptr by GetFreshBasket");
    fExtraBasket = basket;
    basket->DisownBuffer();
 
@@ -1767,44 +1768,46 @@ TBasket* TBranch::GetFreshBasket(TBuffer* user_buffer)
       basket = fExtraBasket;
       fExtraBasket = nullptr;
       basket->AdoptBuffer(user_buffer);
-   }
-   if (GetTree()->MemoryFull(0)) {
-      if (fNBaskets==1) {
-         // Steal the existing basket
-         Int_t oldindex = fBaskets.GetLast();
-         basket = (TBasket*)fBaskets.UncheckedAt(oldindex);
-         if (!basket) {
-            fBaskets.SetLast(-2); // For recalculation of Last.
-            oldindex = fBaskets.GetLast();
-            if (oldindex != fBaskets.LowerBound()-1) {
-               basket = (TBasket*)fBaskets.UncheckedAt(oldindex);
+   } else {
+      if (GetTree()->MemoryFull(0)) {
+         if (fNBaskets==1) {
+            // Steal the existing basket
+            Int_t oldindex = fBaskets.GetLast();
+            basket = (TBasket*)fBaskets.UncheckedAt(oldindex);
+            if (!basket) {
+               fBaskets.SetLast(-2); // For recalculation of Last.
+               oldindex = fBaskets.GetLast();
+               if (oldindex != fBaskets.LowerBound()-1) {
+                  basket = (TBasket*)fBaskets.UncheckedAt(oldindex);
+               }
             }
-         }
-         if (basket && fBasketBytes[oldindex]!=0) {
-            if (basket == fCurrentBasket) {
-               fCurrentBasket    = 0;
-               fFirstBasketEntry = -1;
-               fNextBasketEntry  = -1;
+            if (basket && fBasketBytes[oldindex]!=0) {
+               if (basket == fCurrentBasket) {
+                  fCurrentBasket    = 0;
+                  fFirstBasketEntry = -1;
+                  fNextBasketEntry  = -1;
+               }
+               fBaskets.AddAt(0,oldindex);
+               fBaskets.SetLast(-1);
+               fNBaskets = 0;
+            } else {
+               basket = fTree->CreateBasket(this);
             }
-            fBaskets.AddAt(0,oldindex);
-            fBaskets.SetLast(-1);
-            fNBaskets = 0;
+         } else if (fNBaskets == 0) {
+            // There is nothing to drop!
+            basket = fTree->CreateBasket(this);
          } else {
+            // Memory is full and there is more than one basket,
+            // Let DropBaskets do it job.
+            DropBaskets();
             basket = fTree->CreateBasket(this);
          }
-      } else if (fNBaskets == 0) {
-         // There is nothing to drop!
-         basket = fTree->CreateBasket(this);
       } else {
-         // Memory is full and there is more than one basket,
-         // Let DropBaskets do it job.
-         DropBaskets();
          basket = fTree->CreateBasket(this);
       }
-   } else {
-      basket = fTree->CreateBasket(this);
+      if (user_buffer)
+         basket->AdoptBuffer(user_buffer);
    }
-   basket->AdoptBuffer(user_buffer);
    return basket;
 }
 