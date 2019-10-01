64385aae2f52e079177977930131f26552e52788
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2019-10-01 17:02:42 +0200

    Fix compilation errors on Windows
    This should fix the errors LNK2019: unresolved external symbol "const ROOT::Experimental::RHistDrawable<1>::'vftable'"

--- hist/histdraw/v7/src/RHistDrawable.cxx
+++ hist/histdraw/v7/src/RHistDrawable.cxx
@@ -84,5 +84,66 @@ template class RHistPainterBase<3>;
 template class RHistDrawable<1>;
 template class RHistDrawable<2>;
 template class RHistDrawable<3>;
+
+std::shared_ptr<RHistDrawable<1>> GetDrawable(const std::shared_ptr<RH1D> &histimpl)
+{
+   return std::make_shared<RHistDrawable<1>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<1>> GetDrawable(const std::shared_ptr<RH1I> &histimpl)
+{
+   return std::make_shared<RHistDrawable<1>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<1>> GetDrawable(const std::shared_ptr<RH1C> &histimpl)
+{
+   return std::make_shared<RHistDrawable<1>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<1>> GetDrawable(const std::shared_ptr<RH1F> &histimpl)
+{
+   return std::make_shared<RHistDrawable<1>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<2>> GetDrawable(const std::shared_ptr<RH2D> &histimpl)
+{
+   return std::make_shared<RHistDrawable<2>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<2>> GetDrawable(const std::shared_ptr<RH2I> &histimpl)
+{
+   return std::make_shared<RHistDrawable<2>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<2>> GetDrawable(const std::shared_ptr<RH2C> &histimpl)
+{
+   return std::make_shared<RHistDrawable<2>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<2>> GetDrawable(const std::shared_ptr<RH2F> &histimpl)
+{
+   return std::make_shared<RHistDrawable<2>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<3>> GetDrawable(const std::shared_ptr<RH3D> &histimpl)
+{
+   return std::make_shared<RHistDrawable<3>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<3>> GetDrawable(const std::shared_ptr<RH3I> &histimpl)
+{
+   return std::make_shared<RHistDrawable<3>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<3>> GetDrawable(const std::shared_ptr<RH3C> &histimpl)
+{
+   return std::make_shared<RHistDrawable<3>>(histimpl);
+}
+
+std::shared_ptr<RHistDrawable<3>> GetDrawable(const std::shared_ptr<RH3F> &histimpl)
+{
+   return std::make_shared<RHistDrawable<3>>(histimpl);
+}
+
 } // namespace Experimental
 } // namespace ROOT
