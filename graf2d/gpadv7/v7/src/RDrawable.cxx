8053e56de30c49f7b66ed190f7f5bc2456f13b24
Sergey Linev <S.Linev@gsi.de>
2019-10-02 17:48:59 +0200

    [rdrawable] do not automatically assign identifier for drawable
    Now it is fully custom user-defined field, which potentially can be used
    for CSS selector expression like "#objid1". Remove all correspondent
    methods

--- graf2d/gpadv7/v7/src/RDrawable.cxx
+++ graf2d/gpadv7/v7/src/RDrawable.cxx
@@ -29,10 +29,22 @@ void ROOT::Experimental::RDrawable::Execute(const std::string &)
    assert(false && "Did not expect a menu item to be invoked!");
 }
 
+/////////////////////////////////////////////////////////////////////////////
+/// Preliminary method which checks if drawable matches with given selector
+/// Following selector are allowed:
+///   "type" or "#id" or ".class_name"
+///  Here type is drawable kind like 'rect' or 'pad'
+///       id is drawable identifier, specified with RDrawable::SetId() method
+///       class_name is drawable class name, specified with RDrawable::SetCssClass() method
+
 bool ROOT::Experimental::RDrawable::MatchSelector(const std::string &selector) const
 {
-   return (selector == fCssType) || (!fCssClass.empty() && (selector == std::string(".") + fCssClass));
+   return (selector == fCssType) || (!fCssClass.empty() && (selector == std::string(".") + fCssClass)) || (!fId.empty() && (selector == std::string("#") + fId));
 }
+
+/////////////////////////////////////////////////////////////////////////////
+/// Creates display item for drawable
+/// By default item contains drawble data itself
 
 std::unique_ptr<ROOT::Experimental::RDisplayItem> ROOT::Experimental::RDrawable::Display() const
 {
