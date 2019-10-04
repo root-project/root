5a0e932a0879b904d5b31c513a618a11f0d1d9b8
Axel Naumann <Axel.Naumann@cern.ch>
2019-10-04 12:59:57 +0200

    [cmake] Use FindPython for newer CMake, fixes MacOS 10.14 with Xcode 11:
    See https://gitlab.kitware.com/cmake/cmake/issues/19763

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -496,15 +496,90 @@ endif()
 
 message(STATUS "Looking for python")
 # Python is required by header and manpage generation
-find_package(PythonInterp ${python_version} REQUIRED)
 
-if(python)
-  find_package(PythonLibs ${python_version} REQUIRED)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
 
-  if(NOT "${PYTHONLIBS_VERSION_STRING}" MATCHES "${PYTHON_VERSION_STRING}")
-    message(FATAL_ERROR "Version mismatch between Python interpreter (${PYTHON_VERSION_STRING})"
-    " and libraries (${PYTHONLIBS_VERSION_STRING}).\nROOT cannot work with this configuration. "
-    "Please specify only PYTHON_EXECUTABLE to CMake with an absolute path to ensure matching versions are found.")
+  # Determine whether we should prefer Python 2 or Python 3:
+  set(PYTHON_PREFER_VERSION "3")
+  # Check whether old `find_package(PythonInterp)` variable was passed.
+  # If so, it will be passed to find_package(Python) below. Otherwise,
+  # check what `python` points to: Python 2 or 3:
+  if(NOT PYTHON_EXECUTABLE)
+    find_program(PYTHON_BINARY_IN_PATH "python")
+    if(PYTHON_BINARY_IN_PATH)
+      execute_process(COMMAND ${PYTHON_BINARY_IN_PATH} -c "import sys;print(sys.version_info[0])"
+                      OUTPUT_VARIABLE PYTHON_PREFER_VERSION
+                      ERROR_VARIABLE PYTHON_PREFER_VERSION_ERR)
+      if(PYTHON_PREFER_VERSION_ERR)
+        message(WARNING "Unable to determine version of ${PYTHON_BINARY_IN_PATH}: ${PYTHON_PREFER_VERSION_ERR}")
+      endif()
+      string(STRIP "${PYTHON_PREFER_VERSION}" PYTHON_PREFER_VERSION)
+    endif()
+  endif()
+
+  if(python)
+    set(REQUIRED_PYTHON_Development Development)
+  endif()
+
+  message(STATUS "Preferring Python version ${PYTHON_PREFER_VERSION}")
+
+  if("${PYTHON_PREFER_VERSION}" MATCHES "2")
+    # Means PYTHON_EXECUTABLE wasn't defined.
+    if(PYTHON_INCLUDE_DIRS AND NOT Python2_INCLUDE_DIRS)
+      set(Python2_INCLUDE_DIRS "${PYTHON_INCLUDE_DIRS}")
+    endif()
+    if(PYTHON_LIBRARIES AND NOT Python2_LIBRARIES)
+      set(Python2_LIBRARIES "${PYTHON_LIBRARIES}")
+    endif()
+    find_package(Python2 COMPONENTS Interpreter ${REQUIRED_PYTHON_Development} REQUIRED)
+    if(Python2_Development_FOUND)
+      # Re-run, now with NumPy, but not required:
+      find_package(Python2 COMPONENTS Interpreter Development NumPy)
+      # Compat with find_package(PythonInterp), find_package(PythonLibs)
+      set(PYTHON_INCLUDE_DIRS "${Python2_INCLUDE_DIRS}")
+      set(PYTHON_LIBRARIES "${Python2_LIBRARIES}")
+      set(PYTHON_VERSION_MAJOR "${Python2_VERSION_MAJOR}")
+      set(PYTHON_VERSION_MINOR "${Python2_VERSION_MINOR}")
+      set(NUMPY_FOUND ${Python2_NumPy_FOUND})
+      set(NUMPY_INCLUDE_DIRS "${Python2_NumPy_INCLUDE_DIRS}")
+    endif()
+  else()
+    if(PYTHON_EXECUTABLE AND NOT Python_EXECUTABLE)
+      set(Python_EXECUTABLE "${PYTHON_EXECUTABLE}")
+    endif()
+    if(PYTHON_INCLUDE_DIRS AND NOT Python_INCLUDE_DIRS)
+      set(Python_INCLUDE_DIRS "${PYTHON_INCLUDE_DIRS}")
+    endif()
+    if(PYTHON_LIBRARIES AND NOT Python_LIBRARIES)
+      set(Python_LIBRARIES "${PYTHON_LIBRARIES}")
+    endif()
+    find_package(Python COMPONENTS Interpreter ${REQUIRED_PYTHON_Development} REQUIRED)
+    if(Python_Development_FOUND)
+      # Re-run, now with NumPy, but not required:
+      find_package(Python COMPONENTS Interpreter Development NumPy)
+      # Compat with find_package(PythonInterp), find_package(PythonLibs), find_package(NumPy)
+      set(PYTHON_INCLUDE_DIRS "${Python_INCLUDE_DIRS}")
+      set(PYTHON_LIBRARIES "${Python_LIBRARIES}")
+      set(PYTHON_VERSION_MAJOR "${Python_VERSION_MAJOR}")
+      set(PYTHON_VERSION_MINOR "${Python_VERSION_MINOR}")
+      set(NUMPY_FOUND ${Python_NumPy_FOUND})
+      set(NUMPY_INCLUDE_DIRS "${Python_NumPy_INCLUDE_DIRS}")
+    endif()
+  endif()
+
+else()
+  find_package(PythonInterp ${python_version} REQUIRED)
+
+  if(python)
+    find_package(PythonLibs ${python_version} REQUIRED)
+
+    if(NOT "${PYTHONLIBS_VERSION_STRING}" MATCHES "${PYTHON_VERSION_STRING}")
+      message(FATAL_ERROR "Version mismatch between Python interpreter (${PYTHON_VERSION_STRING})"
+      " and libraries (${PYTHONLIBS_VERSION_STRING}).\nROOT cannot work with this configuration. "
+      "Please specify only PYTHON_EXECUTABLE to CMake with an absolute path to ensure matching versions are found.")
+    endif()
+
+    find_package(NumPy)
   endif()
 endif()
 
@@ -1477,8 +1552,6 @@ if(tmva)
   endif()
 
   if(python AND tmva-pymva)
-    message(STATUS "Looking for Numpy")
-    find_package(NumPy)
     if(fail-on-missing AND NOT NUMPY_FOUND)
       message(FATAL_ERROR "TMVA: numpy python package not found and tmva-pymva component required"
                           " (python executable: ${PYTHON_EXECUTABLE})")
