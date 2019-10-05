52317d899bbc295011491a1a0b0eaf5206b534b8
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-10-05 16:30:01 +0300

    Add a message for the long-deprecated Gtypes.h and Htypes.h headers.

--- core/base/inc/Htypes.h
+++ core/base/inc/Htypes.h
@@ -11,6 +11,11 @@
 #ifndef ROOT_Htypes
 #define ROOT_Htypes
 
+#warning "This header is deprecated. Please include Rtypes.h"
+
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,20,00)
+# error "Remove this deprecated file".
+#endif
 
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
