c9279d6952d4419daaf492573eaf8937792d4784
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-10-08 20:28:07 +0300

    ROOT_VERSION_CODE comparison code works only in C++ context.
    This patch fixes the case where the headers are included in C context which
    caused compilation failure

--- core/base/inc/Gtypes.h
+++ core/base/inc/Gtypes.h
@@ -13,9 +13,11 @@
 
 #warning "This header is deprecated. Please include Rtypes.h"
 
+#ifdef __cplusplus
 #if ROOT_VERSION_CODE >= ROOT_VERSION(6,20,00)
 # error "Remove this deprecated file".
 #endif
+#endif //__cplusplus
 
 
 //////////////////////////////////////////////////////////////////////////
