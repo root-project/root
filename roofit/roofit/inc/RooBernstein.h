96d2b8064e96585093ac8cd8806cc248094cb7dc
Rahul Balasubramanian <rahul.balasubramanian@cern.ch>
2019-10-08 14:46:25 +0200

    Revert "update bernstein for ranged fits"
    This reverts commit 19b3ebbbf0f28b91d81f45443261bf5f5c011fe2.

--- roofit/roofit/inc/RooBernstein.h
+++ roofit/roofit/inc/RooBernstein.h
@@ -33,17 +33,15 @@ public:
 
   Int_t getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars, const char* rangeName=0) const ;
   Double_t analyticalIntegral(Int_t code, const char* rangeName=0) const ;
-  virtual void selectNormalizationRange(const char* rangeName=0, Bool_t force=kFALSE) ;
 
 private:
 
   RooRealProxy _x;
   RooListProxy _coefList ;
-  mutable TNamed* _refRangeName ;
 
   Double_t evaluate() const;
 
-  ClassDef(RooBernstein,2) // Bernstein polynomial PDF
+  ClassDef(RooBernstein,1) // Bernstein polynomial PDF
 };
 
 #endif
