bfd4fdf21844e63f810c7cb74bf9a7635675abfe
Sergey Linev <S.Linev@gsi.de>
2019-10-11 18:22:22 +0200

    separate hist and histv7 library

--- hist/hist/inc/LinkDef.h
+++ hist/hist/inc/LinkDef.h
@@ -16,8 +16,6 @@
 
 #pragma link C++ enum EErrorType;
 
-
-
 #pragma link C++ class TAxis-;
 #pragma link C++ class TAxisModLab+;
 #pragma link C++ class TBinomialEfficiencyFitter+;
@@ -329,10 +327,7 @@
 #pragma link C++ function ROOT::Fit::FitResult::GetCovarianceMatrix<TMatrixDSym>( TMatrixDSym & );
 #pragma link C++ function ROOT::Fit::FitResult::GetCorrelationMatrix<TMatrixDSym>( TMatrixDSym & );
 
-
-
-
-// for having backward comptibility after new data member in TProfile
+// for having backward compatibility after new data member in TProfile
 #pragma read sourceClass="TProfile" version="[1-5]" targetClass="TProfile" \
   source="" target="fBinSumw2" \
   code="{ fBinSumw2.Reset(); }"
@@ -344,32 +339,5 @@
   code="{ fBinSumw2.Reset(); }"
 
 
-#ifdef ROOT7_RHist
-
-#pragma link C++ class ROOT::Experimental::RH1F+;
-#pragma link C++ class ROOT::Experimental::RH1D+;
-#pragma link C++ class ROOT::Experimental::RH2F+;
-#pragma link C++ class ROOT::Experimental::RH2D+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImpl<ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>,ROOT::Experimental::RAxisEquidistant>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImpl<ROOT::Experimental::Detail::RHistData<2,double,vector<double>, ROOT::Experimental::RHistStatContent, ROOT::Experimental::RHistStatUncertainty>, ROOT::Experimental::RAxisEquidistant, ROOT::Experimental::RAxisIrregular>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImplBase<ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImplBase<ROOT::Experimental::Detail::RHistData<2,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImplPrecisionAgnosticBase<1>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImplPrecisionAgnosticBase<2>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImplPrecisionAgnosticBase<3>+;
-#pragma link C++ class ROOT::Experimental::RHistStatContent<1,double>+;
-#pragma link C++ class ROOT::Experimental::RHistStatContent<2,double>+;
-#pragma link C++ class ROOT::Experimental::RHistStatContent<3,double>+;
-#pragma link C++ class ROOT::Experimental::RHistStatUncertainty<1,double>+;
-#pragma link C++ class ROOT::Experimental::RHistStatUncertainty<2,double>+;
-#pragma link C++ class ROOT::Experimental::RHistStatUncertainty<3,double>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistData<2,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>+;
-#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant>+;
-#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant,ROOT::Experimental::RAxisIrregular>+;
-#pragma link C++ class ROOT::Experimental::RAxisEquidistant+;
-#pragma link C++ class ROOT::Experimental::RAxisIrregular+;
-#pragma link C++ class ROOT::Experimental::RAxisBase+;
-#endif
 
 #endif
