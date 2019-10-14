5ba7f85967ca24b3d4d09302560394ef1975087b
Sergey Linev <S.Linev@gsi.de>
2019-10-14 22:56:09 +0200

    [rbrowsable] introduce major classes around RBrowsable
    Introduce "Info" class which holds object and provide basic info
    Introduce "LevelIter" to iterate over objects at single level
    Introduce "RDrawable" class which should use all these to iterate over

--- graf2d/gpadv7/inc/ROOT/RBrowsable.hxx
+++ graf2d/gpadv7/inc/ROOT/RBrowsable.hxx
@@ -19,6 +19,73 @@ class TClass;
 namespace ROOT {
 namespace Experimental {
 
+
+/** \class RBrowsableInfo
+\ingroup GpadROOT7
+\brief Basic information about RBrowsable
+\author Sergey Linev <S.Linev@gsi.de>
+\date 2019-10-14
+\warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+*/
+
+class RBrowsableInfo {
+public:
+   virtual ~RBrowsableInfo() = default;
+
+   /** Class information, must be provided in derived classes */
+   virtual const TClass *GetGlass() const = 0;
+
+   /** Name of RBrowsable, must be provided in derived classes */
+   virtual std::string GetName() const = 0;
+
+   /** Title of RBrowsable (optional) */
+   virtual std::string GetTitle() const { return ""; }
+
+   /** Number of childs in RBrowsable (optional) */
+   virtual int GetNumChilds() const { return 0; }
+};
+
+/** \class RBrowsableLevelIter
+\ingroup GpadROOT7
+\brief Iterator over single level hierarchy like TList
+\author Sergey Linev <S.Linev@gsi.de>
+\date 2019-10-14
+\warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+*/
+
+
+class RBrowsableLevelIter {
+public:
+   virtual ~RBrowsableLevelIter() = default;
+
+   /** Shift to next element */
+   virtual bool Next() { return false; }
+
+   /** Reset iterator to the first element, returns false if not supported */
+   virtual bool Reset() { return false; }
+
+   /** Returns information for current element */
+   virtual std::unique_ptr<RBrowsableInfo> GetInfo() { return nullptr; }
+};
+
+/** \class RBrowsable
+\ingroup GpadROOT7
+\brief Way to browse (hopefully) everything in ROOT
+\author Sergey Linev <S.Linev@gsi.de>
+\date 2019-10-14
+\warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+*/
+
+
+class RBrowsable {
+public:
+   RBrowsable() = default;
+   virtual ~RBrowsable() = default;
+
+
+};
+
+
 /** \class RBrowsableProvider
 \ingroup GpadROOT7
 \brief Provider of different browsing methods for supported classes
