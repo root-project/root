67cdbe9d494bfadae0285522652c295f7067efdb
Sergey Linev <S.Linev@gsi.de>
2019-10-17 16:26:46 +0200

    [rbrowsable] prepare splitting of different elements
    First one should spli SysFile and TDirectory browsing features

---
+++ gui/browserv7/inc/ROOT/RBrowsableTDirectory.hxx
@@ -0,0 +1,25 @@
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RBrowsableTDirectory
+#define ROOT7_RBrowsableTDirectory
+
+#include <ROOT/RBrowsable.hxx>
+
+class TDirectory;
+
+namespace ROOT {
+namespace Experimental {
+namespace Browsable {
+
+} // namespace Browsable
+} // namespace Experimental
+} // namespace ROOT
+
+
+#endif
