80329d2878f6347a605216296a1cf03e32c31aa6
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2019-10-23 16:56:02 +0200

    Fix Jira issue #ROOT-10369
    Revert the logic in the if statement replacing static_assert on Windows (fix Jira issue #ROOT-10369), add a comment explaining why the #ifdef is needed, and add a FIXME: check if the error is fixed when upgrading llvm/clang
    
    The #ifdef R__WIN32 is needed to fix the following error when generating libEve_rdict.pcm:
    
    C:/Users/bellenot/build/debug/include/TEveVector.h(62,21): error G34C21FBE: static_assert expression is not an integral constant expression [C:\Users\bellenot\build\debug\graf3d\eve\G__Eve.vcxproj]
            static_assert(offsetof(TEveVectorT, fZ) == offsetof(TEveVectorT, fX) + 2 * sizeof(TT),
                          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\ucrt\stddef.h:43:31: note: expanded from macro 'offsetof'
              #define offsetof(s,m) ((::size_t)&reinterpret_cast<char const volatile&>((((s*)0)->m)))
                                    ^
      C:/Users/bellenot/build/debug/include\TEveProjections.h:174:71: note: in instantiation of member function 'TEveVectorT<float>::Arr' requested here
         virtual Float_t*    GetProjectedCenter() { return fProjectedCenter.Arr(); }
                                                                            ^
      C:/Users/bellenot/build/debug/include/TEveVector.h:62:21: note: cast that performs the conversions of a reinterpret_cast is not allowed in a constant expression
            static_assert(offsetof(TEveVectorT, fZ) == offsetof(TEveVectorT, fX) + 2 * sizeof(TT),
                          ^
      C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\ucrt\stddef.h:43:32: note: expanded from macro 'offsetof'
              #define offsetof(s,m) ((::size_t)&reinterpret_cast<char const volatile&>((((s*)0)->m)))
                                     ^
    CUSTOMBUILD : error : C:/Users/bellenot/build/debug/bin/rootcling.exe: compilation failure

--- graf3d/eve/inc/TEveVector.h
+++ graf3d/eve/inc/TEveVector.h
@@ -38,15 +38,18 @@ public:
    void Dump() const;
 
 #ifdef R__WIN32
+   // This fixes the following rootcling error when generating the dictionary:
+   // error G34C21FBE: static_assert expression is not an integral constant expression
+   // FIXME: check if the error is fixed when upgrading llvm/clang 
    const TT *Arr() const
    {
-      if (offsetof(TEveVectorT, fZ) == offsetof(TEveVectorT, fX) + 2 * sizeof(TT))
+      if (offsetof(TEveVectorT, fZ) != offsetof(TEveVectorT, fX) + 2 * sizeof(TT))
          Error("TEveVectorT", "Subsequent nembers cannot be accessed as array!");
       return &fX;
    }
    TT *Arr()
    {
-      if (offsetof(TEveVectorT, fZ) == offsetof(TEveVectorT, fX) + 2 * sizeof(TT))
+      if (offsetof(TEveVectorT, fZ) != offsetof(TEveVectorT, fX) + 2 * sizeof(TT))
          Error("TEveVectorT", "Subsequent nembers cannot be accessed as array!");
       return &fX;
    }
