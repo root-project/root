aeecc19a380fa5f71ba4bdd44caf95eb1586923c
Philippe Canal <pcanal@fnal.gov>
2019-10-26 18:35:21 -0500

    Mark pointer TStreamerElement as 'CannotSplit'
    This prevents TBranchElement's GatherArtificialElements from
    infinitely recursing (pointlessly) through a setup like:
       class Vertex;
       class Event {
         Vertex *fVertex;
       };
       class Vertex {
         Event *fEvent;
       };

--- core/meta/src/TStreamerElement.cxx
+++ core/meta/src/TStreamerElement.cxx
@@ -263,6 +263,9 @@ Bool_t TStreamerElement::CannotSplit() const
    TClass *cl = GetClassPointer();
    if (!cl) return kFALSE;  //basic type
 
+   static TClassRef clonesArray("TClonesArray");
+   if (IsaPointer() && cl != clonesArray && !cl->GetCollectionProxy()) return kTRUE;
+
    switch(fType) {
       case TVirtualStreamerInfo::kAny    +TVirtualStreamerInfo::kOffsetL:
       case TVirtualStreamerInfo::kObject +TVirtualStreamerInfo::kOffsetL:
