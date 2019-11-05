2a0cd1541f72e69b91d26425e06bb9be2bc00c91
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-11-05 16:45:03 +0100

    [Exp PyROOT] Variable 'py_localruntimedir' set in a way that fits commit 38c49999

--- cmake/modules/RootBuildOptions.cmake
+++ cmake/modules/RootBuildOptions.cmake
@@ -478,6 +478,15 @@ else()
   endif()
 endif()
 
+# set variables necessary for MultiPython
+set(python_dir "python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}")
+if(WIN32)
+  set(py_localruntimedir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${python_dir})
+else()
+  set(py_localruntimedir ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${python_dir})
+endif()
+
+
 #---Add Installation Variables------------------------------------------------------------------
 include(RootInstallDirs)
 
