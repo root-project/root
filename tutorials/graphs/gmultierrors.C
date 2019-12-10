a5630ac1fa8458b19e9ffc1e668fcfecb2f7fa18
Simon Spies <simon.spies@sisesp.de>
2019-12-10 14:04:10 +0100

    Includes several changes, updates, improvements to the class TGraphMultiErrors. See the corresponding pull request 3452 for details.

--- tutorials/graphs/gmultierrors.C
+++ tutorials/graphs/gmultierrors.C
@@ -9,24 +9,22 @@
 /// \author Simon Spies
 
 void gmultierrors() {
-   TCanvas *c1 = new TCanvas("c1","A Simple Graph with multiple errors",200,10,700,500);
-
+   TCanvas *c1 = new TCanvas("c1", "A Simple Graph with multiple y-errors", 200, 10, 700, 500);
    c1->SetGrid();
    c1->GetFrame()->SetBorderSize(12);
 
-   const Int_t np = 5, ne = 2;
-   Double_t x[np]    = {0, 1, 2, 3, 4};
-   Double_t y[np]    = {0, 2, 4, 1, 3};
-   Double_t exl[np]  = {0.3, 0.3, 0.3, 0.3, 0.3};
-   Double_t exh[np]  = {0.3, 0.3, 0.3, 0.3, 0.3};
-   Double_t* eylstat = new double[np]  {1, 0.5, 1, 0.5, 1};
-   Double_t* eyhstat = new double[np]  {0.5, 1, 0.5, 1, 0.5};
-   Double_t* eylsys  = new double[np]  {0.5, 0.4, 0.8, 0.3, 1.2};
-   Double_t* eyhsys  = new double[np]  {0.6, 0.7, 0.6, 0.4, 0.8};
-   Double_t** eyl    = new double*[ne] {eylstat, eylsys};
-   Double_t** eyh    = new double*[ne] {eyhstat, eyhsys};
+   const Int_t np = 5;
+   Double_t x[np]       = {0, 1, 2, 3, 4};
+   Double_t y[np]       = {0, 2, 4, 1, 3};
+   Double_t exl[np]     = {0.3, 0.3, 0.3, 0.3, 0.3};
+   Double_t exh[np]     = {0.3, 0.3, 0.3, 0.3, 0.3};
+   Double_t eylstat[np] = {1, 0.5, 1, 0.5, 1};
+   Double_t eyhstat[np] = {0.5, 1, 0.5, 1, 0.5};
+   Double_t eylsys[np]  = {0.5, 0.4, 0.8, 0.3, 1.2};
+   Double_t eyhsys[np]  = {0.6, 0.7, 0.6, 0.4, 0.8};
 
-   TGraphMultiErrors* gme = new TGraphMultiErrors(5, 2, x, y, exl, exh, eyl, eyh);
+   TGraphMultiErrors* gme = new TGraphMultiErrors("gme", "TGraphMultiErrors Example", np, x, y, exl, exh, eylstat, eyhstat);
+   gme->AddYError(np, eylsys, eyhsys);
    gme->SetMarkerStyle(20);
    gme->SetLineColor(kRed);
    gme->GetAttLine(0)->SetLineColor(kRed);
