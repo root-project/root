8547ae790fae9dec29f342479403bb4250367531
Yunsong Wang <wangyunsong89@gmail.com>
2019-12-12 05:15:38 -0800

    Add tests for TMPIFile (#396)
    * Add TMPIFiles tests

---
+++ root/io/tmpifile/JetEventLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ class Jet + ;
+#pragma link C++ class Hit + ;
+#pragma link C++ class Track + ;
+#pragma link C++ class JetEvent + ;
+#endif
