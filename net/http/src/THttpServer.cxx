46e4fd593dbbfe9f493655288a01faf36675ec83
Sergey Linev <S.Linev@gsi.de>
2020-01-06 17:57:55 +0100

    [http] fix files reading on Windows
    One has to use binary mode to be able read file content,
    otherwise Windows may change lines ending and 
    result will not fit into the preallocated string

--- net/http/src/THttpServer.cxx
+++ net/http/src/THttpServer.cxx
@@ -1273,7 +1273,7 @@ char *THttpServer::ReadFileContent(const char *filename, Int_t &len)
 {
    len = 0;
 
-   std::ifstream is(filename);
+   std::ifstream is(filename, std::ios::in | std::ios::binary);
    if (!is)
       return nullptr;
 
@@ -1297,7 +1297,7 @@ char *THttpServer::ReadFileContent(const char *filename, Int_t &len)
 
 std::string THttpServer::ReadFileContent(const std::string &filename)
 {
-   std::ifstream is(filename);
+   std::ifstream is(filename, std::ios::in | std::ios::binary);
    std::string res;
    if (is) {
       is.seekg(0, std::ios::end);
