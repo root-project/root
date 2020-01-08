5fa18c99c49d7a6ff6afc82b15e731061cad9b0a
Olivier Couet <olivier.couet@cern.ch>
2020-01-08 15:48:23 +0100

    Header formatting was wrong

--- hist/hist/src/TGraphMultiErrors.cxx
+++ hist/hist/src/TGraphMultiErrors.cxx
@@ -39,38 +39,38 @@
 ClassImp(TGraphMultiErrors)
 
 /** \class TGraphMultiErrors
- TGraph with asymmetric error bars and multiple y error dimensions.
+    \ingroup Hist
+TGraph with asymmetric error bars and multiple y error dimensions.
 
- The TGraphMultiErrors painting is performed thanks to the TGraphPainter
- class. All details about the various painting options are given in this class.
+The TGraphMultiErrors painting is performed thanks to the TGraphPainter
+class. All details about the various painting options are given in this class.
 
- The picture below gives an example:
+The picture below gives an example:
 
- Begin_Macro(source)
- {
-    c1 = new TCanvas("c1", "A Simple Graph with multiple y-errors", 200, 10, 700, 500);
-    c1->SetGrid();
-    c1->GetFrame()->SetBorderSize(12);
-    const Int_t np = 5;
-    Double_t x[np]       = {0, 1, 2, 3, 4};
-    Double_t y[np]       = {0, 2, 4, 1, 3};
-    Double_t exl[np]     = {0.3, 0.3, 0.3, 0.3, 0.3};
-    Double_t exh[np]     = {0.3, 0.3, 0.3, 0.3, 0.3};
-    Double_t eylstat[np] = {1, 0.5, 1, 0.5, 1};
-    Double_t eyhstat[np] = {0.5, 1, 0.5, 1, 2};
-    Double_t eylsys[np]  = {0.5, 0.4, 0.8, 0.3, 1.2};
-    Double_t eyhsys[np]  = {0.6, 0.7, 0.6, 0.4, 0.8};
-    gme = new TGraphMultiErrors("gme", "TGraphMultiErrors Example", np, x, y, exl, exh, eylstat, eyhstat);
-    gme->AddYError(np, eylsys, eyhsys);
-    gme->SetMarkerStyle(20);
-    gme->SetLineColor(kRed);
-    gme->GetAttLine(0)->SetLineColor(kRed);
-    gme->GetAttLine(1)->SetLineColor(kBlue);
-    gme->GetAttFill(1)->SetFillStyle(0);
-    gme->Draw("APS ; Z ; 5 s=0.5");
-    return c1;
- }
- End_Macro
+Begin_Macro(source)
+{
+   auto c1 = new TCanvas("c1", "A Simple Graph with multiple y-errors", 200, 10, 700, 500);
+   c1->SetGrid();
+   c1->GetFrame()->SetBorderSize(12);
+   const Int_t np = 5;
+   Double_t x[np]       = {0, 1, 2, 3, 4};
+   Double_t y[np]       = {0, 2, 4, 1, 3};
+   Double_t exl[np]     = {0.3, 0.3, 0.3, 0.3, 0.3};
+   Double_t exh[np]     = {0.3, 0.3, 0.3, 0.3, 0.3};
+   Double_t eylstat[np] = {1, 0.5, 1, 0.5, 1};
+   Double_t eyhstat[np] = {0.5, 1, 0.5, 1, 2};
+   Double_t eylsys[np]  = {0.5, 0.4, 0.8, 0.3, 1.2};
+   Double_t eyhsys[np]  = {0.6, 0.7, 0.6, 0.4, 0.8};
+   auto gme = new TGraphMultiErrors("gme", "TGraphMultiErrors Example", np, x, y, exl, exh, eylstat, eyhstat);
+   gme->AddYError(np, eylsys, eyhsys);
+   gme->SetMarkerStyle(20);
+   gme->SetLineColor(kRed);
+   gme->GetAttLine(0)->SetLineColor(kRed);
+   gme->GetAttLine(1)->SetLineColor(kBlue);
+   gme->GetAttFill(1)->SetFillStyle(0);
+   gme->Draw("APS ; Z ; 5 s=0.5");
+}
+End_Macro
 */
 
 ///////////////////////////////////////////////////////////////////////////////
