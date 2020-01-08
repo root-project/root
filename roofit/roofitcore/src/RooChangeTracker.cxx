8630a9385d4ef5cff3ad5b1427e9554e1cfe66d4
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-01-08 17:54:15 +0100

    [RF] Modernise loops in RooChangeTracker.

--- roofit/roofitcore/src/RooChangeTracker.cxx
+++ roofit/roofitcore/src/RooChangeTracker.cxx
@@ -52,8 +52,6 @@ ClassImp(RooChangeTracker);
 
 RooChangeTracker::RooChangeTracker() : _checkVal(kFALSE), _init(kFALSE)
 {
-  _realSetIter = _realSet.createIterator() ;
-  _catSetIter = _catSet.createIterator() ;
 }
 
 
@@ -74,32 +72,23 @@ RooChangeTracker::RooChangeTracker(const char* name, const char* title, const Ro
   _checkVal(checkValues),
   _init(kFALSE)
 {
-  _realSetIter = _realSet.createIterator() ;
-  _catSetIter = _catSet.createIterator() ;
-
-  TIterator* iter = trackSet.createIterator() ;
-  RooAbsArg* arg ;
-  while((arg=(RooAbsArg*)iter->Next())) {
-    if (dynamic_cast<RooAbsReal*>(arg)) {
+for (const auto arg : trackSet) {
+    if (dynamic_cast<const RooAbsReal*>(arg)) {
       _realSet.add(*arg) ;      
     }
-    if (dynamic_cast<RooAbsCategory*>(arg)) {
+    if (dynamic_cast<const RooAbsCategory*>(arg)) {
       _catSet.add(*arg) ;      
     }
   }
-  delete iter ;
   
   if (_checkVal) {
-    RooAbsReal* real  ;
-    RooAbsCategory* cat  ;
-    Int_t i(0) ;
-    _realSetIter->Reset() ;
-    _catSetIter->Reset() ;
-    while((real=(RooAbsReal*)_realSetIter->Next())) {
+    for (unsigned int i=0; i < _realSet.size(); ++i) {
+      auto real = static_cast<const RooAbsReal*>(_realSet.at(i));
       _realRef[i++] = real->getVal() ;
     }
-    i=0 ;
-    while((cat=(RooAbsCategory*)_catSetIter->Next())) {
+
+    for (unsigned int i=0; i < _catSet.size(); ++i) {
+      auto cat = static_cast<const RooAbsCategory*>(_catSet.at(i));
       _catRef[i++] = cat->getIndex() ;
     }
   }
@@ -120,12 +109,6 @@ RooChangeTracker::RooChangeTracker(const RooChangeTracker& other, const char* na
   _checkVal(other._checkVal),
   _init(kFALSE)
 {
-  _realSetIter = _realSet.createIterator() ;
-  _catSetIter = _catSet.createIterator() ;
-
-//   _realSet.add(other._realSet) ;
-//   _catSet.add(other._catSet) ;
-
 }
 
 
@@ -157,32 +140,26 @@ Bool_t RooChangeTracker::hasChanged(Bool_t clearState)
   }
   
   // Compare values against reference
-  _realSetIter->Reset() ;  
-  _catSetIter->Reset() ;
-  RooAbsReal* real ;
-  RooAbsCategory* cat ;
-  Int_t i(0) ;
-
   if (clearState) {
 
     Bool_t valuesChanged(kFALSE) ;
 
     // Check if any of the real values changed
-    while ((real=(RooAbsReal*)_realSetIter->Next())) {
+    for (unsigned int i=0; i < _realSet.size(); ++i) {
+      auto real = static_cast<const RooAbsReal*>(_realSet.at(i));
       if (real->getVal() != _realRef[i]) {
-	// cout << "RooChangeTracker(" << this << "," << GetName() << ") value of " << real->GetName() << " has changed from " << _realRef[i] << " to " << real->getVal() << " clearState = " << (clearState?"T":"F") << endl ;
-	valuesChanged = kTRUE ;
-	_realRef[i] = real->getVal() ;
+        // cout << "RooChangeTracker(" << this << "," << GetName() << ") value of " << real->GetName() << " has changed from " << _realRef[i] << " to " << real->getVal() << " clearState = " << (clearState?"T":"F") << endl ;
+        valuesChanged = kTRUE ;
+        _realRef[i] = real->getVal() ;
       }
-      i++ ;
     }
     // Check if any of the categories changed
-    i=0 ;
-    while ((cat=(RooAbsCategory*)_catSetIter->Next())) {
-      if (cat->getIndex() != _catRef[i++]) {
-	// cout << "RooChangeTracker(" << this << "," << GetName() << ") value of " << cat->GetName() << " has changed from " << _catRef[i-1] << " to " << cat->getIndex() << endl ;
-	valuesChanged = kTRUE ;
-	_catRef[i-1] = cat->getIndex() ;
+    for (unsigned int i=0; i < _catSet.size(); ++i) {
+      auto cat = static_cast<const RooAbsCategory*>(_catSet.at(i));
+      if (cat->getIndex() != _catRef[i]) {
+        // cout << "RooChangeTracker(" << this << "," << GetName() << ") value of " << cat->GetName() << " has changed from " << _catRef[i-1] << " to " << cat->getIndex() << endl ;
+        valuesChanged = kTRUE ;
+        _catRef[i] = cat->getIndex() ;
       }
     }
 
@@ -203,16 +180,17 @@ Bool_t RooChangeTracker::hasChanged(Bool_t clearState)
     // Return true as soon as any input has changed
 
     // Check if any of the real values changed
-    while ((real=(RooAbsReal*)_realSetIter->Next())) {
-      if (real->getVal() != _realRef[i++]) {
-	return kTRUE ;
+    for (unsigned int i=0; i < _realSet.size(); ++i) {
+      auto real = static_cast<const RooAbsReal*>(_realSet.at(i));
+      if (real->getVal() != _realRef[i]) {
+        return kTRUE ;
       }
     }
     // Check if any of the categories changed
-    i=0 ;
-    while ((cat=(RooAbsCategory*)_catSetIter->Next())) {
-      if (cat->getIndex() != _catRef[i++]) {
-	return kTRUE ;
+    for (unsigned int i=0; i < _catSet.size(); ++i) {
+      auto cat = static_cast<const RooAbsCategory*>(_catSet.at(i));
+      if (cat->getIndex() != _catRef[i]) {
+        return kTRUE ;
       }
     }
    
@@ -228,8 +206,7 @@ Bool_t RooChangeTracker::hasChanged(Bool_t clearState)
 
 RooChangeTracker::~RooChangeTracker() 
 {
-  if (_realSetIter) delete _realSetIter ;
-  if (_catSetIter) delete _catSetIter ;
+
 }
 
 
