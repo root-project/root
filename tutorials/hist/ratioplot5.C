3a6172cada6fa7cd40af91e4044b92b43a6c80ac
Olivier Couet <olivier.couet@cern.ch>
2020-01-09 11:29:38 +0100

    Simplify ratio-plot examples.

--- tutorials/hist/ratioplot5.C
+++ tutorials/hist/ratioplot5.C
@@ -14,12 +14,10 @@ void ratioplot5()  {
    auto c1 = new TCanvas("c1", "fit residual simple");
    auto h1 = new TH1D("h1", "h1", 50, -5, 5);
    h1->FillRandom("gaus", 2000);
-   h1->Fit("gaus");
+   h1->Fit("gaus","0");
    h1->GetXaxis()->SetTitle("x");
    h1->GetYaxis()->SetTitle("y");
-   c1->Clear();
    auto rp1 = new TRatioPlot(h1);
    rp1->SetConfidenceIntervalColors(kBlue, kRed);
    rp1->Draw();
-   c1->Update();
 }
--- tutorials/hist/ratioplot6.C
+++ tutorials/hist/ratioplot6.C
@@ -15,18 +15,14 @@ void ratioplot6() {
    gPad->SetFrameFillStyle(0);
    auto h1 = new TH1D("h1", "h1", 50, -5, 5);
    h1->FillRandom("gaus", 5000);
-   TFitResultPtr fitres = h1->Fit("gaus", "S");
+   TFitResultPtr fitres = h1->Fit("gaus", "S0");
    h1->Sumw2();
    h1->GetXaxis()->SetTitle("x");
    h1->GetYaxis()->SetTitle("y");
-
-   c1->Clear();
-
    auto rp1 = new TRatioPlot(h1, "errfunc");
    rp1->SetGraphDrawOpt("L");
    rp1->SetSeparationMargin(0.0);
    rp1->Draw();
    rp1->GetLowerRefGraph()->SetMinimum(-2);
    rp1->GetLowerRefGraph()->SetMaximum(2);
-   c1->Update();
 }
