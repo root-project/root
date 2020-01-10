b47110747703b04e2686e8b9e985c01fba097d7f
Chris Burr <christopher.burr@cern.ch>
2020-01-10 18:47:22 +0100

    Disable SetLibraryPath for RPATH builds

--- rootx/src/rootx.cxx
+++ rootx/src/rootx.cxx
@@ -148,7 +148,7 @@ static void SetRootSys()
    }
 }
 
-
+#ifndef IS_RPATH_BUILD
 static void SetLibraryPath()
 {
 #ifdef ROOTPREFIX
@@ -207,6 +207,7 @@ static void SetLibraryPath()
    }
 #endif
 }
+#endif
 
 extern "C" {
    static void SigUsr1(int);
@@ -451,8 +452,10 @@ int main(int argc, char **argv)
       argvv[1+i] = argv[i];
    argvv[1+i] = 0;
 
+#ifndef IS_RPATH_BUILD
    // Make sure library path is set
    SetLibraryPath();
+#endif
 
    // Execute actual ROOT module
    execv(arg0, argvv);
