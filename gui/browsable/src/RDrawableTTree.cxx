f7909b49506b8bba23b03f87b32cc71548d575d7
Sergey Linev <S.Linev@gsi.de>
2020-01-13 14:59:52 +0100

    [rbrowsable] introduce draw provider for TTree
    For the moment only simple drawing of TLeaf object is implemented.
    No special browsing functionality yet

---
+++ gui/browsable/src/RDrawableTTree.cxx
@@ -0,0 +1,122 @@
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RDrawableProvider.hxx>
+
+
+#include "TLeaf.h"
+#include "TBranch.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TDirectory.h"
+
+#include "TVirtualPad.h"
+
+#include <ROOT/RCanvas.hxx>
+#include <ROOT/RObjectDrawable.hxx>
+
+
+using namespace ROOT::Experimental;
+
+/** Provider for drawing of ROOT6 classes */
+
+class RV6DrawProviderTTree : public RDrawableProvider {
+public:
+   RV6DrawProviderTTree()
+   {
+      RegisterV6(TLeaf::Class(), [](TVirtualPad *pad, std::unique_ptr<Browsable::RHolder> &obj, const std::string &opt) -> bool {
+
+         // try take object without ownership
+         auto tleaf = obj->get_object<TLeaf>();
+         if (!tleaf)
+            return false;
+
+         auto ttree = tleaf->GetBranch()->GetTree();
+         if (!ttree)
+            return false;
+
+
+         std::string expr = std::string(tleaf->GetName()) + ">>htemp_tree_draw";
+
+         ttree->Draw(expr.c_str(),"","goff");
+
+         if (!gDirectory)
+            return false;
+
+         auto htemp = dynamic_cast<TH1*>(gDirectory->FindObject("htemp_tree_draw"));
+
+         if (!htemp)
+            return false;
+
+         htemp->SetDirectory(nullptr);
+         htemp->SetName(tleaf->GetName());
+
+         pad->GetListOfPrimitives()->Clear();
+
+         pad->GetListOfPrimitives()->Add(htemp, opt.c_str());
+
+         return true;
+      });
+
+   }
+
+} newRV6DrawProviderTTree;
+
+
+/** Provider for drawing of ROOT7 classes */
+
+class RV7DrawProviderTTree : public RDrawableProvider {
+public:
+   RV7DrawProviderTTree()
+   {
+      RegisterV7(TLeaf::Class(), [](std::shared_ptr<RPadBase> &subpad, std::unique_ptr<Browsable::RHolder> &obj, const std::string &opt) -> bool {
+
+         // try take object without ownership
+         auto tleaf = obj->get_object<TLeaf>();
+         if (!tleaf)
+            return false;
+
+         auto ttree = tleaf->GetBranch()->GetTree();
+         if (!ttree)
+            return false;
+
+
+         std::string expr = std::string(tleaf->GetName()) + ">>htemp_tree_draw";
+
+         ttree->Draw(expr.c_str(),"","goff");
+
+         if (!gDirectory)
+            return false;
+
+         auto htemp = dynamic_cast<TH1*>(gDirectory->FindObject("htemp_tree_draw"));
+
+         if (!htemp)
+            return false;
+
+         htemp->SetDirectory(nullptr);
+         htemp->SetName(tleaf->GetName());
+
+         if (subpad->NumPrimitives() > 0) {
+            subpad->Wipe();
+            subpad->GetCanvas()->Modified();
+            subpad->GetCanvas()->Update(true);
+         }
+
+         std::shared_ptr<TH1> shared;
+         shared.reset(htemp);
+
+         subpad->Draw<RObjectDrawable>(shared, opt);
+
+
+         return true;
+      });
+
+   }
+
+} newRV7DrawProviderTTree;
+
