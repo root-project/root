6f11e131e818d2a721d5007fae423ba22c24828d
Sergey Linev <S.Linev@gsi.de>
2020-01-13 17:27:13 +0100

    [rbrowsable] introduce ROOTObjectDrawProvider library for TObject
    Implements generic methods for any TObject-derived classed for drawing
    on TCanvas/RCanvas

--- gui/browsable/src/RDrawableTTree.cxx
+++ gui/browsable/src/RDrawableTTree.cxx
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
