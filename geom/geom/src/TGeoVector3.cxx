2acb2d4d2a7aa36304a1c823944fce365f41dec2
Andrei Gheata <Andrei.Gheata@cern.ch>
2020-01-17 17:27:36 +0100

    Added visualization and persistency for tessellated shape

---
+++ geom/geom/src/TGeoVector3.cxx
@@ -0,0 +1,23 @@
+// @(#)root/geom:$Id$
+// Author: Andrei Gheata   15/01/2020
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+/** \class  TGeoVector3
+\ingroup Geometry_classes
+Simple 3-vector representation
+*/
+
+#include "TGeoVector3.h"
+
+std::ostream &operator<<(std::ostream &os, TGeoVector3 const &vec)
+{
+   os << "{" << vec[0] << ", " << vec[1] << ", " << vec[2] << "}";
+   return os;
+}
