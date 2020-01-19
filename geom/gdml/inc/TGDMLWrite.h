c9e4235e8539ccf52450b9b6677b163d2867c495
Andrei Gheata <Andrei.Gheata@cern.ch>
2020-01-19 15:29:53 +0100

    Added extra tessellated shape checks and support for writing in GDML format.

--- geom/gdml/inc/TGDMLWrite.h
+++ geom/gdml/inc/TGDMLWrite.h
@@ -33,6 +33,7 @@
 #include "TGeoBoolNode.h"
 #include "TGeoCompositeShape.h"
 #include "TGeoScaledShape.h"
+#include "TGeoTessellated.h"
 #include "TGeoManager.h"
 #include "TGDMLMatrix.h"
 
@@ -193,6 +194,7 @@ private:
    XMLNodePointer_t CreateXtrusionN(TGeoXtru * geoShape);
    XMLNodePointer_t CreateEllipsoidN(TGeoCompositeShape * geoShape, TString elName);
    XMLNodePointer_t CreateElConeN(TGeoScaledShape * geoShape);
+   XMLNodePointer_t CreateTessellatedN(TGeoTessellated * geoShape);
    XMLNodePointer_t CreateOpticalSurfaceN(TGeoOpticalSurface * geoSurf);
    XMLNodePointer_t CreateSkinSurfaceN(TGeoSkinSurface * geoSurf);
    XMLNodePointer_t CreateBorderSurfaceN(TGeoBorderSurface * geoSurf);
