a4d1ed1853cd1e3fa41e495786c214b3dc5583d1
Andrei Gheata <Andrei.Gheata@cern.ch>
2020-01-20 13:13:41 +0100

    Relating to TGeoVector3 as Vertex_t via a single type definition file, making future switch easy. Clang formatting.

--- geom/geom/inc/TGeoTessellated.h
+++ geom/geom/inc/TGeoTessellated.h
@@ -13,29 +13,33 @@
 #define ROOT_TGeoTessellated
 
 #include "TGeoVector3.h"
+#include "TGeoTypedefs.h"
 #include "TGeoBBox.h"
 
 class TGeoFacet {
-   using Vertex_t    = TGeoVector3;
-   using VertexVec_t = std::vector<Vertex_t>;
+   using Vertex_t = Tessellated::Vertex_t;
+   using VertexVec_t = Tessellated::VertexVec_t;
 
 private:
-   int     fIvert[4]    = {0};       // Vertex indices in the array
-   VertexVec_t *fVertices = nullptr;   //! array of vertices
-   int       fNvert       = 0;         // number of vertices (can be 3 or 4)
-   bool      fShared      = false;     // Vector of vertices shared flag
+   int fIvert[4] = {0};              // Vertex indices in the array
+   VertexVec_t *fVertices = nullptr; //! array of vertices
+   int fNvert = 0;                   // number of vertices (can be 3 or 4)
+   bool fShared = false;             // Vector of vertices shared flag
 
 public:
    TGeoFacet() {}
    TGeoFacet(const TGeoFacet &other);
 
-   ~TGeoFacet() { if (!fShared) delete fVertices; }
+   ~TGeoFacet()
+   {
+      if (!fShared)
+         delete fVertices;
+   }
 
-   const TGeoFacet &operator = (const TGeoFacet &other);
+   const TGeoFacet &operator=(const TGeoFacet &other);
 
    // Triangular facet
-   TGeoFacet(const Vertex_t &pt0, const Vertex_t &pt1, const Vertex_t &pt2)
-      : fIvert{0, 1, 2}
+   TGeoFacet(const Vertex_t &pt0, const Vertex_t &pt1, const Vertex_t &pt2) : fIvert{0, 1, 2}
    {
       fVertices = new VertexVec_t;
       fVertices->push_back(pt0);
@@ -45,20 +49,20 @@ public:
    }
 
    // Quadrilateral facet
-   TGeoFacet(const Vertex_t &pt0, const Vertex_t &pt1, const Vertex_t &pt2, const Vertex_t &pt3)
-      : fIvert{0, 1, 2, 3}
+   TGeoFacet(const Vertex_t &pt0, const Vertex_t &pt1, const Vertex_t &pt2, const Vertex_t &pt3) : fIvert{0, 1, 2, 3}
    {
       fVertices = new VertexVec_t;
       fVertices->push_back(pt0);
       fVertices->push_back(pt1);
       fVertices->push_back(pt2);
-      fVertices->push_back(pt3);      
+      fVertices->push_back(pt3);
       fNvert = 4;
    }
-   
+
    void SetVertices(VertexVec_t *vertices, int i0 = -1, int i1 = -1, int i2 = -1, int i3 = -1)
    {
-      if (!fShared) delete fVertices;
+      if (!fShared)
+         delete fVertices;
       fVertices = vertices;
       if (i0 >= 0) {
          fIvert[0] = i0;
@@ -66,9 +70,9 @@ public:
          fIvert[2] = i2;
          fIvert[3] = i3;
       }
-      fShared   = true;
+      fShared = true;
    }
-   
+
    Vertex_t ComputeNormal(bool &degenerated) const;
    int GetNvert() const { return fNvert; }
 
@@ -76,25 +80,27 @@ public:
    const Vertex_t &GetVertex(int ivert) const { return fVertices->operator[](fIvert[ivert]); }
 
    int GetVertexIndex(int ivert) const { return fIvert[ivert]; }
-   
+
    bool Check() const;
    bool IsNeighbour(const TGeoFacet &other, bool &flip) const;
 };
 
 std::ostream &operator<<(std::ostream &os, TGeoFacet const &facet);
 
-class TGeoTessellated : public TGeoBBox
-{
-  using Vertex_t = TGeoVector3;
+class TGeoTessellated : public TGeoBBox {
+
+public:
+  using Vertex_t = Tessellated::Vertex_t;
 
 private:
    int fNfacets = 0;
-   int fNvert   = 0;
-   int fNseg    = 0;
-   std::vector<Vertex_t>  fVertices;        // List of vertices
-   std::vector<TGeoFacet> fFacets;          // List of facets
+   int fNvert = 0;
+   int fNseg = 0;
+   std::vector<Vertex_t> fVertices; // List of vertices
+   std::vector<TGeoFacet> fFacets;  // List of facets
 
-   virtual void FillBuffer3D(TBuffer3D & buffer, int reqSections, Bool_t localFrame) const;
+   virtual void FillBuffer3D(TBuffer3D &buffer, int reqSections, Bool_t localFrame) const;
+
 public:
    // constructors
    TGeoTessellated() {}
@@ -102,35 +108,35 @@ public:
    // destructor
    virtual ~TGeoTessellated() {}
 
-   TGeoTessellated(const TGeoTessellated&);
-   TGeoTessellated& operator=(const TGeoTessellated&);
-   
+   TGeoTessellated(const TGeoTessellated &);
+   TGeoTessellated &operator=(const TGeoTessellated &);
+
    void ComputeBBox();
    void Close();
 
    void AddFacet(const Vertex_t &pt0, const Vertex_t &pt1, const Vertex_t &pt2);
    void AddFacet(const Vertex_t &pt0, const Vertex_t &pt1, const Vertex_t &pt2, const Vertex_t &pt3);
 
-   int  GetNfacets() const { return fFacets.size(); }
-   int  GetNsegments() const { return fNseg; }
-   int  GetNvertices() const { return fNvert; }
+   int GetNfacets() const { return fFacets.size(); }
+   int GetNsegments() const { return fNseg; }
+   int GetNvertices() const { return fNvert; }
    const TGeoFacet &GetFacet(int i) { return fFacets[i]; }
    const Vertex_t &GetVertex(int i) { return fVertices[i]; }
 
-   virtual void          AfterStreamer();
-   virtual int           DistancetoPrimitive(int, int) { return 99999; }
+   virtual void AfterStreamer();
+   virtual int DistancetoPrimitive(int, int) { return 99999; }
    virtual const TBuffer3D &GetBuffer3D(int reqSections, Bool_t localFrame) const;
-   virtual void          GetMeshNumbers(int &nvert, int &nsegs, int &npols) const;
-   virtual int           GetNmeshVertices() const { return fNvert; }
-   virtual void          InspectShape() const {}
-   virtual TBuffer3D    *MakeBuffer3D() const;
-   virtual void          SavePrimitive(std::ostream &, Option_t *) {}
-   virtual void          SetPoints(double *points) const;
-   virtual void          SetPoints(float *points) const;
-   virtual void          SetSegsAndPols(TBuffer3D &buff) const;
-   virtual void          Sizeof3D() const {}
-   
-   ClassDef(TGeoTessellated, 1)         // tessellated shape class
+   virtual void GetMeshNumbers(int &nvert, int &nsegs, int &npols) const;
+   virtual int GetNmeshVertices() const { return fNvert; }
+   virtual void InspectShape() const {}
+   virtual TBuffer3D *MakeBuffer3D() const;
+   virtual void SavePrimitive(std::ostream &, Option_t *) {}
+   virtual void SetPoints(double *points) const;
+   virtual void SetPoints(float *points) const;
+   virtual void SetSegsAndPols(TBuffer3D &buff) const;
+   virtual void Sizeof3D() const {}
+
+   ClassDef(TGeoTessellated, 1) // tessellated shape class
 };
 
 #endif
