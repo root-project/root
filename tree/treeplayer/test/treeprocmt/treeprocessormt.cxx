52c0510c146b123d135e0f37b7ee8c9059bf0de3
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-01-22 10:19:22 +0100

    Allow to run broken tests on Windows & add `FIXME` comments
    - Add a CMake flag (win_broken_tests) to enable broken tests on Windows
    - Add a couple of `FIXME` comments, for the following errors:
      - In TSystem::GetIncludePath(), with ACLiC:
        input_line_12:8:38: error: use of undeclared identifier 'IC'
        "C:/Users/bellenot/build/debug/etc" -IC:/Users/bellenot/build/debug/etc//cling -IC:/Users/bellenot/build/debug/include"",
                                             ^
        Error in <ACLiC>: Dictionary generation failed!
      - When compiling math/mathmore/test/StatFunction.cxx, it generates those errors:
        Math/Functor.h(182): error C2059: syntax error: '<'
        Math/Functor.h(182): error C2947: expecting '>' to terminate template-argument-list, found '<'
        Math/Functor.h(182): error C2062: type 'unknown-type' unexpected
        Math/Functor.h(181,-1847264676) : error C2039: 'Clone': is not a member of '`global namespace''
        Math/Functor.h(182): error C2143: syntax error: missing ';' before '{'
        Math/Functor.h(182): error C2447: '{': missing function header (old-style formal list?)
        Math/Functor.h(182): error C2059: syntax error: '<'
        Math/Functor.h(182): error C2947: expecting '>' to terminate template-argument-list, found '<'
        Math/Functor.h(182): error C2062: type 'unknown-type' unexpected
        Math/Functor.h(181,-1847263020) : error C2039: 'Clone': is not a member of '`global namespace''
        Math/Functor.h(182): error C2143: syntax error: missing ';' before '{'
        Math/Functor.h(182): error C2447: '{': missing function header (old-style formal list?)

--- tree/treeplayer/test/treeprocmt/treeprocessormt.cxx
+++ tree/treeplayer/test/treeprocmt/treeprocessormt.cxx
@@ -217,7 +217,7 @@ TEST(TreeProcessorMT, LimitNTasks_CheckClusters)
    ROOT::DisableImplicitMT();
 }
 
-#ifndef _MSC_VER
+#if !defined(_MSC_VER) || defined(_ENABLE_BROKEN_TESTS)
 TEST(TreeProcessorMT, PathName)
 {
    auto fname = "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/ZZTo4mu.root";
