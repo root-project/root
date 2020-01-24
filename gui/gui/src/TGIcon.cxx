e37cac19c10ed2a3d312f0341409a77aafe92ab3
Sergey Linev <S.Linev@gsi.de>
2020-01-24 13:49:23 +0100

    Fix memory leak in TGIcon
    One should delete result of gSystem->ExpandPathName()
    or use signature with TString

--- gui/gui/src/TGIcon.cxx
+++ gui/gui/src/TGIcon.cxx
@@ -193,7 +193,9 @@ void TGIcon::SetImagePath(const char *path)
    if (!path) {
       return;
    }
-   fPath = gSystem->ExpandPathName(gSystem->UnixPathName(path));
+
+   fPath = gSystem->UnixPathName(path);
+   gSystem->ExpandPathName(fPath);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -210,13 +212,14 @@ void TGIcon::SavePrimitive(std::ostream &out, Option_t *option /*= ""*/)
       return;
    }
 
-   const char *picname = fPic->GetName();
+   TString picname = gSystem->UnixPathName(fPic->GetName());
+   gSystem->ExpandPathName(picname);
 
    out <<"   TGIcon *";
    if (!fImage) {
       out << GetName() << " = new TGIcon(" << fParent->GetName()
          << ",gClient->GetPicture(" << quote
-         << gSystem->ExpandPathName(gSystem->UnixPathName(picname))   // if no path
+         << picname   // if no path
          << quote << ")" << "," << GetWidth() << "," << GetHeight();
       if (fBackground == GetDefaultFrameBackground()) {
          if (!GetOptions()) {
