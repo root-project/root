cab8a5f356f04abf35224d1681535e373bbe46e1
Sergey Linev <S.Linev@gsi.de>
2020-01-27 12:49:34 +0100

    Mark appropriate TUnixSystem methods as override

--- core/unix/inc/TUnixSystem.h
+++ core/unix/inc/TUnixSystem.h
@@ -34,8 +34,8 @@ private:
    void FillWithCwd(char *cwd) const;
 
 protected:
-   const char    *FindDynamicLibrary(TString &lib, Bool_t quiet = kFALSE);
-   const char    *GetLinkedLibraries();
+   const char    *FindDynamicLibrary(TString &lib, Bool_t quiet = kFALSE) override;
+   const char    *GetLinkedLibraries() override;
 
    // static functions providing semi-low level interface to raw Unix
    static int          UnixMakedir(const char *name);
@@ -74,146 +74,146 @@ public:
    virtual ~TUnixSystem();
 
    //---- Misc -------------------------------------------------
-   Bool_t            Init();
-   void              SetProgname(const char *name);
-   void              SetDisplay();
-   const char       *GetError();
-   const char       *HostName();
+   Bool_t            Init() override;
+   void              SetProgname(const char *name) override;
+   void              SetDisplay() override;
+   const char       *GetError() override;
+   const char       *HostName() override;
 
    //---- EventLoop --------------------------------------------
-   void              DispatchOneEvent(Bool_t pendingOnly = kFALSE);
-   Int_t             Select(TList *active, Long_t timeout);
-   Int_t             Select(TFileHandler *fh, Long_t timeout);
+   void              DispatchOneEvent(Bool_t pendingOnly = kFALSE) override;
+   Int_t             Select(TList *active, Long_t timeout) override;
+   Int_t             Select(TFileHandler *fh, Long_t timeout) override;
 
    //---- Handling of system events ----------------------------
    void              CheckChilds();
    Bool_t            CheckSignals(Bool_t sync);
    Bool_t            CheckDescriptors();
    void              DispatchSignals(ESignals sig);
-   void              AddSignalHandler(TSignalHandler *sh);
-   TSignalHandler   *RemoveSignalHandler(TSignalHandler *sh);
-   void              ResetSignal(ESignals sig, Bool_t reset = kTRUE);
-   void              ResetSignals();
-   void              IgnoreSignal(ESignals sig, Bool_t ignore = kTRUE);
-   void              SigAlarmInterruptsSyscalls(Bool_t set);
-   void              AddFileHandler(TFileHandler *fh);
-   TFileHandler     *RemoveFileHandler(TFileHandler *fh);
+   void              AddSignalHandler(TSignalHandler *sh) override;
+   TSignalHandler   *RemoveSignalHandler(TSignalHandler *sh) override;
+   void              ResetSignal(ESignals sig, Bool_t reset = kTRUE) override;
+   void              ResetSignals() override;
+   void              IgnoreSignal(ESignals sig, Bool_t ignore = kTRUE) override;
+   void              SigAlarmInterruptsSyscalls(Bool_t set) override;
+   void              AddFileHandler(TFileHandler *fh) override;
+   TFileHandler     *RemoveFileHandler(TFileHandler *fh) override;
 
    //---- Floating Point Exceptions Control --------------------
-   Int_t             GetFPEMask();
-   Int_t             SetFPEMask(Int_t mask = kDefaultMask);
+   Int_t             GetFPEMask() override;
+   Int_t             SetFPEMask(Int_t mask = kDefaultMask) override;
 
    //---- Time & Date ------------------------------------------
-   TTime             Now();
-   void              AddTimer(TTimer *ti);
-   TTimer           *RemoveTimer(TTimer *ti);
-   void              ResetTimer(TTimer *ti);
+   TTime             Now() override;
+   void              AddTimer(TTimer *ti) override;
+   TTimer           *RemoveTimer(TTimer *ti) override;
+   void              ResetTimer(TTimer *ti) override;
    Bool_t            DispatchTimers(Bool_t mode);
-   void              Sleep(UInt_t milliSec);
+   void              Sleep(UInt_t milliSec) override;
 
    //---- Processes --------------------------------------------
-   Int_t             Exec(const char *shellcmd);
-   FILE             *OpenPipe(const char *shellcmd, const char *mode);
-   int               ClosePipe(FILE *pipe);
-   void              Exit(int code, Bool_t mode = kTRUE);
-   void              Abort(int code = 0);
-   int               GetPid();
-   void              StackTrace();
+   Int_t             Exec(const char *shellcmd) override;
+   FILE             *OpenPipe(const char *shellcmd, const char *mode) override;
+   int               ClosePipe(FILE *pipe) override;
+   void              Exit(int code, Bool_t mode = kTRUE) override;
+   void              Abort(int code = 0) override;
+   int               GetPid() override;
+   void              StackTrace() override;
 
    //---- Directories ------------------------------------------
-   int               MakeDirectory(const char *name);
-   void             *OpenDirectory(const char *name);
-   void              FreeDirectory(void *dirp);
-   const char       *GetDirEntry(void *dirp);
-   Bool_t            ChangeDirectory(const char *path);
-   const char       *WorkingDirectory();
-   std::string       GetWorkingDirectory() const;
-   const char       *HomeDirectory(const char *userName = 0);
-   std::string       GetHomeDirectory(const char *userName = 0) const;
-   const char       *TempDirectory() const;
-   FILE             *TempFileName(TString &base, const char *dir = 0);
+   int               MakeDirectory(const char *name) override;
+   void             *OpenDirectory(const char *name) override;
+   void              FreeDirectory(void *dirp) override;
+   const char       *GetDirEntry(void *dirp) override;
+   Bool_t            ChangeDirectory(const char *path) override;
+   const char       *WorkingDirectory() override;
+   std::string       GetWorkingDirectory() const override;
+   const char       *HomeDirectory(const char *userName = 0) override;
+   std::string       GetHomeDirectory(const char *userName = 0) const override;
+   const char       *TempDirectory() const override;
+   FILE             *TempFileName(TString &base, const char *dir = 0) override;
 
    //---- Paths & Files ----------------------------------------
-   const char       *PrependPathName(const char *dir, TString& name);
-   Bool_t            ExpandPathName(TString &patbuf);
-   char             *ExpandPathName(const char *path);
-   Bool_t            AccessPathName(const char *path, EAccessMode mode = kFileExists);
-   Bool_t            IsPathLocal(const char *path);
-   int               CopyFile(const char *from, const char *to, Bool_t overwrite = kFALSE);
-   int               Rename(const char *from, const char *to);
-   int               Link(const char *from, const char *to);
-   int               Symlink(const char *from, const char *to);
-   int               Unlink(const char *name);
-   int               GetPathInfo(const char *path, FileStat_t &buf);
+   const char       *PrependPathName(const char *dir, TString& name) override;
+   Bool_t            ExpandPathName(TString &patbuf) override;
+   char             *ExpandPathName(const char *path) override;
+   Bool_t            AccessPathName(const char *path, EAccessMode mode = kFileExists) override;
+   Bool_t            IsPathLocal(const char *path) override;
+   int               CopyFile(const char *from, const char *to, Bool_t overwrite = kFALSE) override;
+   int               Rename(const char *from, const char *to) override;
+   int               Link(const char *from, const char *to) override;
+   int               Symlink(const char *from, const char *to) override;
+   int               Unlink(const char *name) override;
+   int               GetPathInfo(const char *path, FileStat_t &buf) override;
    int               GetFsInfo(const char *path, Long_t *id, Long_t *bsize,
-                               Long_t *blocks, Long_t *bfree);
-   int               Chmod(const char *file, UInt_t mode);
-   int               Umask(Int_t mask);
-   int               Utime(const char *file, Long_t modtime, Long_t actime);
-   const char       *FindFile(const char *search, TString& file, EAccessMode mode = kFileExists);
+                               Long_t *blocks, Long_t *bfree) override;
+   int               Chmod(const char *file, UInt_t mode) override;
+   int               Umask(Int_t mask) override;
+   int               Utime(const char *file, Long_t modtime, Long_t actime) override;
+   const char       *FindFile(const char *search, TString& file, EAccessMode mode = kFileExists) override;
 
    //---- Users & Groups ---------------------------------------
-   Int_t             GetUid(const char *user = 0);
-   Int_t             GetGid(const char *group = 0);
-   Int_t             GetEffectiveUid();
-   Int_t             GetEffectiveGid();
-   UserGroup_t      *GetUserInfo(Int_t uid);
-   UserGroup_t      *GetUserInfo(const char *user = 0);
-   UserGroup_t      *GetGroupInfo(Int_t gid);
-   UserGroup_t      *GetGroupInfo(const char *group = 0);
+   Int_t             GetUid(const char *user = 0) override;
+   Int_t             GetGid(const char *group = 0) override;
+   Int_t             GetEffectiveUid() override;
+   Int_t             GetEffectiveGid() override;
+   UserGroup_t      *GetUserInfo(Int_t uid) override;
+   UserGroup_t      *GetUserInfo(const char *user = 0) override;
+   UserGroup_t      *GetGroupInfo(Int_t gid) override;
+   UserGroup_t      *GetGroupInfo(const char *group = 0) override;
 
    //---- Environment Manipulation -----------------------------
-   const char       *Getenv(const char *name);
-   void              Setenv(const char *name, const char *value);
-   void              Unsetenv(const char *name);
+   const char       *Getenv(const char *name) override;
+   void              Setenv(const char *name, const char *value) override;
+   void              Unsetenv(const char *name) override;
 
    //---- System Logging ---------------------------------------
-   void              Openlog(const char *name, Int_t options, ELogFacility facility);
-   void              Syslog(ELogLevel level, const char *mess);
-   void              Closelog();
+   void              Openlog(const char *name, Int_t options, ELogFacility facility) override;
+   void              Syslog(ELogLevel level, const char *mess) override;
+   void              Closelog() override;
 
    //---- Standard Output redirection --------------------------
    Int_t             RedirectOutput(const char *name, const char *mode = "a",
-                                    RedirectHandle_t *h = 0);
+                                    RedirectHandle_t *h = 0) override;
 
    //---- Dynamic Loading --------------------------------------
-   void              AddDynamicPath(const char *lib);
-   const char       *GetDynamicPath();
-   void              SetDynamicPath(const char *lib);
-   Func_t            DynFindSymbol(const char *module, const char *entry);
-   int               Load(const char *module, const char *entry = "", Bool_t system = kFALSE);
-   void              Unload(const char *module);
-   void              ListSymbols(const char *module, const char *re = "");
-   void              ListLibraries(const char *regexp = "");
+   void              AddDynamicPath(const char *lib) override;
+   const char       *GetDynamicPath() override;
+   void              SetDynamicPath(const char *lib) override;
+   Func_t            DynFindSymbol(const char *module, const char *entry) override;
+   int               Load(const char *module, const char *entry = "", Bool_t system = kFALSE) override;
+   void              Unload(const char *module) override;
+   void              ListSymbols(const char *module, const char *re = "") override;
+   void              ListLibraries(const char *regexp = "") override;
 
    //---- RPC --------------------------------------------------
-   TInetAddress      GetHostByName(const char *server);
-   TInetAddress      GetPeerName(int sock);
-   TInetAddress      GetSockName(int sock);
-   int               GetServiceByName(const char *service);
-   char             *GetServiceByPort(int port);
+   TInetAddress      GetHostByName(const char *server) override;
+   TInetAddress      GetPeerName(int sock) override;
+   TInetAddress      GetSockName(int sock) override;
+   int               GetServiceByName(const char *service) override;
+   char             *GetServiceByPort(int port) override;
    int               ConnectService(const char *server, int port, int tcpwindowsize, const char *protocol = "tcp");
-   int               OpenConnection(const char *server, int port, int tcpwindowsize = -1, const char *protocol = "tcp");
-   int               AnnounceTcpService(int port, Bool_t reuse, int backlog, int tcpwindowsize = -1);
-   int               AnnounceUdpService(int port, int backlog);
-   int               AnnounceUnixService(int port, int backlog);
-   int               AnnounceUnixService(const char *sockpath, int backlog);
-   int               AcceptConnection(int sock);
-   void              CloseConnection(int sock, Bool_t force = kFALSE);
-   int               RecvRaw(int sock, void *buffer, int length, int flag);
-   int               SendRaw(int sock, const void *buffer, int length, int flag);
-   int               RecvBuf(int sock, void *buffer, int length);
-   int               SendBuf(int sock, const void *buffer, int length);
-   int               SetSockOpt(int sock, int option, int val);
-   int               GetSockOpt(int sock, int option, int *val);
+   int               OpenConnection(const char *server, int port, int tcpwindowsize = -1, const char *protocol = "tcp") override;
+   int               AnnounceTcpService(int port, Bool_t reuse, int backlog, int tcpwindowsize = -1) override;
+   int               AnnounceUdpService(int port, int backlog) override;
+   int               AnnounceUnixService(int port, int backlog) override;
+   int               AnnounceUnixService(const char *sockpath, int backlog) override;
+   int               AcceptConnection(int sock) override;
+   void              CloseConnection(int sock, Bool_t force = kFALSE) override;
+   int               RecvRaw(int sock, void *buffer, int length, int flag) override;
+   int               SendRaw(int sock, const void *buffer, int length, int flag) override;
+   int               RecvBuf(int sock, void *buffer, int length) override;
+   int               SendBuf(int sock, const void *buffer, int length) override;
+   int               SetSockOpt(int sock, int option, int val) override;
+   int               GetSockOpt(int sock, int option, int *val) override;
 
    //---- System, CPU and Memory info
-   int               GetSysInfo(SysInfo_t *info) const;
-   int               GetCpuInfo(CpuInfo_t *info, Int_t sampleTime = 1000) const;
-   int               GetMemInfo(MemInfo_t *info) const;
-   int               GetProcInfo(ProcInfo_t *info) const;
+   int               GetSysInfo(SysInfo_t *info) const override;
+   int               GetCpuInfo(CpuInfo_t *info, Int_t sampleTime = 1000) const override;
+   int               GetMemInfo(MemInfo_t *info) const override;
+   int               GetProcInfo(ProcInfo_t *info) const override;
 
-   ClassDef(TUnixSystem,0)  //Interface to Unix OS services
+   ClassDefOverride(TUnixSystem,0)  //Interface to Unix OS services
 };
 
 #endif
