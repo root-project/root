ec54f7f61bf9aaa67572691682f8c47a8f69c55d
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-01-31 11:43:14 +0100

    [ROOT-10534] Add GetNRuns feature to RDataFrame
    The feature tracks the number of runs executed by this RDataFrame instance,
    more precisely by this RLoopManager instance of the dataframe.

--- tree/dataframe/inc/ROOT/RDF/RInterface.hxx
+++ tree/dataframe/inc/ROOT/RDF/RInterface.hxx
@@ -1989,6 +1989,12 @@ public:
    /// ~~~
    unsigned int GetNSlots() const { return fLoopManager->GetNSlots(); }
 
+   /// \brief Gets the number of event loops run
+   /// \return The number of event loops run by this RDataFrame instance
+   ///
+   /// This method returns the number of events loops run so far by this RDataFrame instance.
+   unsigned int GetNRuns() const { return fLoopManager->GetNRuns(); }
+
    // clang-format off
    ////////////////////////////////////////////////////////////////////////////
    /// \brief Execute a user-defined accumulation operation on the processed column values in each processing slot
