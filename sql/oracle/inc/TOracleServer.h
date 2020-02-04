5e2f25c308de67b988d85773ede18de9080d53ce
Sergey Linev <S.Linev@gsi.de>
2020-02-04 17:32:36 +0100

    [oracle] more c++11 syntax for TOracleServer

--- sql/oracle/inc/TOracleServer.h
+++ sql/oracle/inc/TOracleServer.h
@@ -33,8 +33,8 @@ class Connection;
 class TOracleServer : public TSQLServer {
 
 private:
-   oracle::occi::Environment  *fEnv;    // environment of Oracle access
-   oracle::occi::Connection   *fConn;   // connection to Oracle server
+   oracle::occi::Environment  *fEnv{nullptr};    // environment of Oracle access
+   oracle::occi::Connection   *fConn{nullptr};   // connection to Oracle server
    TString       fInfo;  // info string with Oracle version information
 
    static const char* fgDatimeFormat; //! format for converting date and time stamps into string
@@ -43,33 +43,33 @@ public:
    TOracleServer(const char *db, const char *uid, const char *pw);
    ~TOracleServer();
 
-   void        Close(Option_t *opt="");
-   TSQLResult *Query(const char *sql);
-   Bool_t      Exec(const char* sql);
-   TSQLStatement *Statement(const char *sql, Int_t niter = 100);
-   Bool_t      IsConnected() const { return (fConn!=0) && (fEnv!=0); }
-   Bool_t      HasStatement() const { return kTRUE; }
-   Int_t       SelectDataBase(const char *dbname);
-   TSQLResult *GetDataBases(const char *wild = 0);
-   TSQLResult *GetTables(const char *dbname, const char *wild = 0);
-   TList      *GetTablesList(const char* wild = 0);
-   TSQLTableInfo *GetTableInfo(const char* tablename);
-   TSQLResult *GetColumns(const char *dbname, const char *table, const char *wild = 0);
-   Int_t       GetMaxIdentifierLength() { return 30; }
-   Int_t       CreateDataBase(const char *dbname);
-   Int_t       DropDataBase(const char *dbname);
-   Int_t       Reload();
-   Int_t       Shutdown();
-   const char *ServerInfo();
+   void        Close(Option_t *opt="") final;
+   TSQLResult *Query(const char *sql) final;
+   Bool_t      Exec(const char* sql) final;
+   TSQLStatement *Statement(const char *sql, Int_t niter = 100) final;
+   Bool_t      IsConnected() const final { return (fConn!=0) && (fEnv!=0); }
+   Bool_t      HasStatement() const final { return kTRUE; }
+   Int_t       SelectDataBase(const char *dbname) final;
+   TSQLResult *GetDataBases(const char *wild = nullptr) final;
+   TSQLResult *GetTables(const char *dbname, const char *wild = nullptr) final;
+   TList      *GetTablesList(const char* wild = nullptr) final;
+   TSQLTableInfo *GetTableInfo(const char* tablename) final;
+   TSQLResult *GetColumns(const char *dbname, const char *table, const char *wild = nullptr) final;
+   Int_t       GetMaxIdentifierLength() final { return 30; }
+   Int_t       CreateDataBase(const char *dbname) final;
+   Int_t       DropDataBase(const char *dbname) final;
+   Int_t       Reload() final;
+   Int_t       Shutdown() final;
+   const char *ServerInfo() final;
 
-   Bool_t      StartTransaction();
-   Bool_t      Commit();
-   Bool_t      Rollback();
+   Bool_t      StartTransaction() final;
+   Bool_t      Commit() final;
+   Bool_t      Rollback() final;
 
    static    void     SetDatimeFormat(const char* fmt = "MM/DD/YYYY, HH24:MI:SS");
    static const char* GetDatimeFormat();
 
-   ClassDef(TOracleServer,0)  // Connection to Oracle server
+   ClassDefOverride(TOracleServer,0)  // Connection to Oracle server
 };
 
 #endif
