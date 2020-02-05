0c0e4d555dc246c846ea120e0a96a834872de87e
Claire Guyot <claire.eloise.guyot@cern.ch>
2020-02-05 10:16:26 +0100

    [histv7] add test for RHist::Add: x-x=0

--- hist/histv7/test/add.cxx
+++ hist/histv7/test/add.cxx
@@ -28,6 +28,16 @@ TEST(HistAddTest, AddSelfHist) {
    EXPECT_FLOAT_EQ(0.84f, hist.GetBinContent({0.1111}));
 }
 
+// Test "x - x = 0"
+TEST(HistAddTest, SubstractSelfHist) {
+   ROOT::Experimental::RH1F hFrom({100,0.,1});
+   ROOT::Experimental::RH1F hTo({100,0.,1});
+   hFrom.Fill({0.1111}, -.42f);
+   hTo.Fill({0.1111}, .42f);
+   ROOT::Experimental::Add(hTo, hFrom);
+   EXPECT_FLOAT_EQ(.00f, hTo.GetBinContent({0.1111}));
+}
+
 // Test "x + y" with less STAT
 TEST(HistAddTest, AddHist) {
    ROOT::Experimental::RH1F hFrom({100,0.,1});
