baf2a8ccc6ec42d3e441862ed20d01276d6c3315
Carsten Burgard <cburgard@cern.ch>
2020-02-11 12:51:47 +0100

    reverted to correct file

---
+++ roofit/histfactory/inc/RooStats/HistFactory/RooJSONFactoryWSTool.h
@@ -0,0 +1,33 @@
+#include "RooWorkspace.h"
+#include <string>
+
+class RooJSONFactoryWSTool : public TNamed, RooPrintable {
+  RooWorkspace* _workspace;
+ public:
+  RooJSONFactoryWSTool(RooWorkspace& ws);
+  
+// interfaces for JSON and YAML reading/writing
+  Bool_t importJSON(const char* filename);
+  Bool_t importYML(const char* filename);
+  Bool_t importJSON(std::istream& os);
+  Bool_t importYML(std::istream& os);  
+  Bool_t exportJSON(const char* fileName) ;
+  Bool_t exportYML(const char* fileName) ;
+  Bool_t exportJSON(std::ostream& os);
+  Bool_t exportYML(std::ostream& os);
+  
+  static void loadFactoryExpressions(const std::string& fname);
+  static void clearFactoryExpressions();
+  static void printFactoryExpressions();
+protected:
+  template<class T> void importFunctions(const T& n);
+  template<class T> void importPdfs(const T& n);
+  template<class T> void importVariables(const T& n);
+  template<class T> void importDependants(const T& n);
+  
+  template<class T> void exportFunctions(const T& n);
+  template<class T> void exportPdfs(const T& n);
+  template<class T> void exportVariables(const T& n);
+  template<class T> void exportAll(const T& n);  
+  
+};
