ded332f9409058fc13315acb3fccbff4f93b9a9c
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-02-17 14:40:53 +0100

    [HF] Add test for reading legacy HistFactory models.

--- roofit/histfactory/test/testHistFactory.cxx
+++ roofit/histfactory/test/testHistFactory.cxx
@@ -2,6 +2,12 @@
 // Authors: Stephan Hageboeck, CERN  01/2019
 
 #include "RooStats/HistFactory/Sample.h"
+#include "RooStats/ModelConfig.h"
+#include "RooWorkspace.h"
+#include "RooArgSet.h"
+
+#include "TROOT.h"
+#include "TFile.h"
 #include "gtest/gtest.h"
 
 using namespace RooStats;
@@ -22,3 +28,56 @@ TEST(Sample, CopyAssignment)
   ASSERT_EQ(hist->GetNbinsX(), 10);
 }
 
+
+TEST(HistFactory, Read_ROOT6_16_Model) {
+  std::string filename = "./ref_6.16_example_UsingC_channel1_meas_model.root";
+  std::unique_ptr<TFile> file(TFile::Open(filename.c_str()));
+  if (!file || !file->IsOpen()) {
+    filename = TROOT::GetRootSys() + "/roofit/histfactory/test/" + filename;
+    file.reset(TFile::Open(filename.c_str()));
+  }
+
+  ASSERT_TRUE(file && file->IsOpen());
+  RooWorkspace* ws;
+  file->GetObject("channel1", ws);
+  ASSERT_NE(ws, nullptr);
+
+  auto mc = dynamic_cast<RooStats::ModelConfig*>(ws->obj("ModelConfig"));
+  ASSERT_NE(mc, nullptr);
+
+  RooAbsPdf* pdf = mc->GetPdf();
+  ASSERT_NE(pdf, nullptr);
+
+  const RooArgSet* obs = mc->GetObservables();
+  ASSERT_NE(obs, nullptr);
+
+  EXPECT_NEAR(pdf->getVal(), 0.17488817, 1.E-8);
+  EXPECT_NEAR(pdf->getVal(*obs), 0.95652174, 1.E-8);
+}
+
+
+TEST(HistFactory, Read_ROOT6_16_Combined_Model) {
+  std::string filename = "./ref_6.16_example_UsingC_combined_meas_model.root";
+  std::unique_ptr<TFile> file(TFile::Open(filename.c_str()));
+  if (!file || !file->IsOpen()) {
+    filename = TROOT::GetRootSys() + "/roofit/histfactory/test/" + filename;
+    file.reset(TFile::Open(filename.c_str()));
+  }
+
+  ASSERT_TRUE(file && file->IsOpen());
+  RooWorkspace* ws;
+  file->GetObject("combined", ws);
+  ASSERT_NE(ws, nullptr);
+
+  auto mc = dynamic_cast<RooStats::ModelConfig*>(ws->obj("ModelConfig"));
+  ASSERT_NE(mc, nullptr);
+
+  RooAbsPdf* pdf = mc->GetPdf();
+  ASSERT_NE(pdf, nullptr);
+
+  const RooArgSet* obs = mc->GetObservables();
+  ASSERT_NE(obs, nullptr);
+
+  EXPECT_NEAR(pdf->getVal(), 0.17488817, 1.E-8);
+  EXPECT_NEAR(pdf->getVal(*obs), 0.95652174, 1.E-8);
+}
