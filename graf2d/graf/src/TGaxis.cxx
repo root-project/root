a8bc860d5f143694d1c7910501a772c565a0e769
Olivier Couet <olivier.couet@cern.ch>
2020-02-18 10:18:47 +0100

    simplify example

--- graf2d/graf/src/TGaxis.cxx
+++ graf2d/graf/src/TGaxis.cxx
@@ -329,8 +329,7 @@ Begin_Macro(source)
 {
    Double_t pi = TMath::Pi();
    TF1*   f = new TF1("f","TMath::Cos(x/TMath::Pi())", -pi, pi);
-   TH1*   h = f->GetHistogram();
-   TAxis* a = h->GetXaxis();
+   TAxis* a = f->GetXaxis();
    a->SetNdivisions(-502);
    a->ChangeLabel(1,-1,-1,-1,-1,-1,"-#pi");
    a->ChangeLabel(-1,-1,-1,-1,-1,-1,"#pi");
