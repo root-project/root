85913094c5d5fe08bbe16349c23c0def7a394c3c
Sergey Linev <S.Linev@gsi.de>
2020-02-20 17:20:25 +0100

    [rframe] introduce RAttrAxis for axes drawing attributes
    Axes itself intergrated into histogram, but different drawing attributes
    will be configured via RAttrAxis. This will include line, text, grid
    attributes, but also log or invert scale

--- tutorials/v7/draw_frame.cxx
+++ tutorials/v7/draw_frame.cxx
@@ -19,15 +19,18 @@
 #include "ROOT/RCanvas.hxx"
 #include "ROOT/RColor.hxx"
 #include "ROOT/RHistDrawable.hxx"
+#include "ROOT/RStyle.hxx"
 
 // macro must be here while cling is not capable to load
 // library automatically for outlined function see ROOT-10336
 R__LOAD_LIBRARY(libROOTHistDraw)
 
+using namespace ROOT::Experimental;
+
+auto style = std::make_shared<RStyle>(); // keep here to avoid destroy when leaving function scope
+
 void draw_frame()
 {
-   using namespace ROOT::Experimental;
-
    // Create the histogram.
    RAxisConfig xaxis("x", 10, 0., 1.);
    RAxisConfig yaxis("y", {0., 1., 2., 3., 10.});
@@ -43,21 +46,34 @@ void draw_frame()
    // Create a canvas to be displayed.
    auto canvas = RCanvas::Create("Canvas Title");
 
-   auto frame = canvas->GetOrCreateFrame();
-   frame->AttrFill().SetColor(RColor::kBlue);
-   frame->AttrBorder().SetColor(RColor::kRed);
-   frame->AttrBorder().SetWidth(3);
-   frame->Margins().SetTop(0.3_normal);
-   frame->Margins().SetBottom(0.1_normal);
-   frame->Margins().SetLeft(0.2_normal);
-   frame->Margins().SetRight(0.2_normal);
+   // Make divisions
+   auto subpads = canvas->Divide(2,1);
 
-   auto draw1 = canvas->Draw(pHist);
+   // configure RFrame with direct API calls
+   auto frame1 = subpads[0][0]->GetOrCreateFrame();
+   frame1->AttrFill().SetColor(RColor::kBlue);
+   frame1->AttrBorder().SetColor(RColor::kRed);
+   frame1->AttrBorder().SetWidth(3);
+   frame1->Margins().SetTop(0.3_normal);
+   frame1->Margins().SetBottom(0.1_normal);
+   frame1->Margins().SetLeft(0.2_normal);
+   frame1->Margins().SetRight(0.2_normal);
+
+   frame1->AttrX().AttrLine().SetColor(RColor::kGreen);
+   frame1->AttrY().AttrLine().SetColor(RColor::kBlue);
+
+   auto draw1 = subpads[0][0]->Draw(pHist);
    draw1->AttrLine().SetColor(RColor::kRed);
 
-   auto other = std::make_shared<RH2D>(*pHist);
-   auto draw2 = canvas->Draw(other);
+   // create frame before drawing histograms
+   auto frame2 = subpads[1][0]->GetOrCreateFrame();
+
+   auto draw2 = subpads[1][0]->Draw(pHist);
    draw2->AttrLine().SetColor(RColor::kBlue).SetWidth(12);
+
+   style->ParseString("frame { margin_left: 0.3; margin_right: 0.3; x_line_color_name: blue; y_line_color_name: green; }");
+
+   canvas->UseStyle(style);
 
    canvas->Show();
 }
