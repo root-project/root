e1e04fd0b150cff23687881a8fbf5afec151aea0
Massimiliano Galli <massimiliano.galli@cern.ch>
2020-02-25 08:34:51 +0100

    [thisroot.sh][Exp PyROOT] Temporarily go back to 3de4a40
    Since we are getting rid of the subdirectories, the first step for what
    concerns the environment configuration is going back to the pre-existent
    situation.

--- config/thisroot.sh
+++ config/thisroot.sh
@@ -26,24 +26,6 @@ drop_from_path()
 
 clean_environment()
 {
-   # Assert that we got enough arguments
-   if test $# -ne 2 ; then
-      echo "clean_environment: needs 2 arguments"
-      return 1
-   fi
-
-   local exp_pyroot=$1
-   local pyroot_dir=$2
-
-   # Check if we are using ZSH
-   if [ ! -z $ZSH_VERSION ] ; then
-      # Check if nonomatch option is set, enable if not and save
-      # the initial status
-      if ! setopt | grep -q nonomatch; then
-         setopt +o nomatch
-         unset_nomatch=true
-      fi
-   fi
 
    if [ -n "${old_rootsys}" ] ; then
       if [ -n "${PATH}" ]; then
@@ -53,67 +35,22 @@ clean_environment()
       if [ -n "${LD_LIBRARY_PATH}" ]; then
          drop_from_path "$LD_LIBRARY_PATH" "${old_rootsys}/lib"
          LD_LIBRARY_PATH=$newpath
-         if [ ! -z "${exp_pyroot}" ] ; then
-            drop_from_path "$LD_LIBRARY_PATH" "$pyroot_dir"
-            LD_LIBRARY_PATH=$newpath
-            for pyroot_libs_dir in ${old_rootsys}/lib/python*
-            do
-               drop_from_path "$LD_LIBRARY_PATH" "$pyroot_libs_dir"
-               LD_LIBRARY_PATH=$newpath
-            done
-         fi
       fi
       if [ -n "${DYLD_LIBRARY_PATH}" ]; then
          drop_from_path "$DYLD_LIBRARY_PATH" "${old_rootsys}/lib"
          DYLD_LIBRARY_PATH=$newpath
-         if [ ! -z "${exp_pyroot}" ] ; then
-            drop_from_path "$DYLD_LIBRARY_PATH" "$pyroot_dir"
-            DYLD_LIBRARY_PATH=$newpath
-            for pyroot_libs_dir in ${old_rootsys}/lib/python*
-            do
-               drop_from_path "$DYLD_LIBRARY_PATH" "$pyroot_libs_dir"
-               DYLD_LIBRARY_PATH=$newpath
-            done
-         fi
       fi
       if [ -n "${SHLIB_PATH}" ]; then
          drop_from_path "$SHLIB_PATH" "${old_rootsys}/lib"
          SHLIB_PATH=$newpath
-         if [ ! -z "${exp_pyroot}" ] ; then
-            drop_from_path "$SHLIB_PATH" "$pyroot_dir"
-            SHLIB_PATH=$newpath
-            for pyroot_libs_dir in ${old_rootsys}/lib/python*
-            do
-               drop_from_path "$SHLIB_PATH" "$pyroot_libs_dir"
-               SHLIB_PATH=$newpath
-            done
-         fi
       fi
       if [ -n "${LIBPATH}" ]; then
          drop_from_path "$LIBPATH" "${old_rootsys}/lib"
          LIBPATH=$newpath
-         if [ ! -z "${exp_pyroot}" ] ; then
-            drop_from_path "$LIBPATH" "$pyroot_dir"
-            LIBPATH=$newpath
-            for pyroot_libs_dir in ${old_rootsys}/lib/python*
-            do
-               drop_from_path "$LIBPATH" "$pyroot_libs_dir"
-               LIBPATH=$newpath
-            done
-         fi
       fi
       if [ -n "${PYTHONPATH}" ]; then
          drop_from_path "$PYTHONPATH" "${old_rootsys}/lib"
          PYTHONPATH=$newpath
-         if [ ! -z "${exp_pyroot}" ] ; then
-            drop_from_path "$PYTHONPATH" "$pyroot_dir"
-            PYTHONPATH=$newpath
-            for pyroot_libs_dir in ${old_rootsys}/lib/python*
-            do
-               drop_from_path "$PYTHONPATH" "$pyroot_libs_dir"
-               PYTHONPATH=$newpath
-            done
-         fi
       fi
       if [ -n "${MANPATH}" ]; then
          drop_from_path "$MANPATH" "${old_rootsys}/man"
@@ -138,102 +75,54 @@ clean_environment()
          default_manpath=""
       fi
    fi
-
-   # Check value of $unset_nomatch and unset if needed
-   if [ ! -z "${unset_nomatch}" ]; then
-      setopt -o nomatch
-   fi
-
 }
 
 set_environment()
 {
-   # Assert that we got enough arguments
-   if test $# -ne 2 ; then
-      echo "set_environment: needs 2 arguments"
-      return 1
-   fi
-
-   local exp_pyroot=$1
-   local pyroot_dir=$2
-
    if [ -z "${PATH}" ]; then
       PATH=@bindir@; export PATH
    else
       PATH=@bindir@:$PATH; export PATH
    fi
 
-   if [ -z "${exp_pyroot}" ]; then
-      if [ -z "${LD_LIBRARY_PATH}" ]; then
-         LD_LIBRARY_PATH=@libdir@
-         export LD_LIBRARY_PATH       # Linux, ELF HP-UX
-      else
-         LD_LIBRARY_PATH=@libdir@:$LD_LIBRARY_PATH
-         export LD_LIBRARY_PATH
-      fi
-      if [ -z "${DYLD_LIBRARY_PATH}" ]; then
-         DYLD_LIBRARY_PATH=@libdir@
-         export DYLD_LIBRARY_PATH       # Linux, ELF HP-UX
-      else
-         DYLD_LIBRARY_PATH=@libdir@:$DYLD_LIBRARY_PATH
-         export DYLD_LIBRARY_PATH
-      fi
-      if [ -z "${SHLIB_PATH}" ]; then
-         SHLIB_PATH=@libdir@
-         export SHLIB_PATH       # Linux, ELF HP-UX
-      else
-         SHLIB_PATH=@libdir@:$SHLIB_PATH
-         export SHLIB_PATH
-      fi
-      if [ -z "${LIBPATH}" ]; then
-         LIBPATH=@libdir@
-         export LIBPATH       # Linux, ELF HP-UX
-      else
-         LIBPATH=@libdir@:$LIBPATH
-         export LIBPATH
-      fi
-      if [ -z "${PYTHONPATH}" ]; then
-         PYTHONPATH=@libdir@
-         export PYTHONPATH       # Linux, ELF HP-UX
-      else
-         PYTHONPATH=@libdir@:$PYTHONPATH
-         export PYTHONPATH
-      fi
+   if [ -z "${LD_LIBRARY_PATH}" ]; then
+      LD_LIBRARY_PATH=@libdir@
+      export LD_LIBRARY_PATH       # Linux, ELF HP-UX
    else
-      if [ -z "${LD_LIBRARY_PATH}" ]; then
-         LD_LIBRARY_PATH=@libdir@:$pyroot_dir
-         export LD_LIBRARY_PATH       # Linux, ELF HP-UX
-      else
-         LD_LIBRARY_PATH=@libdir@:$pyroot_dir:$LD_LIBRARY_PATH
-         export LD_LIBRARY_PATH
-      fi
-      if [ -z "${DYLD_LIBRARY_PATH}" ]; then
-         DYLD_LIBRARY_PATH=@libdir@:$pyroot_dir
-         export DYLD_LIBRARY_PATH       # Linux, ELF HP-UX
-      else
-         DYLD_LIBRARY_PATH=@libdir@:$pyroot_dir:$DYLD_LIBRARY_PATH
-         export DYLD_LIBRARY_PATH
-      fi
-      if [ -z "${SHLIB_PATH}" ]; then
-         SHLIB_PATH=@libdir@:$pyroot_dir
-         export SHLIB_PATH       # Linux, ELF HP-UX
-      else
-         SHLIB_PATH=@libdir@:$pyroot_dir:$SHLIB_PATH
-         export SHLIB_PATH
-      fi
-      if [ -z "${LIBPATH}" ]; then
-         LIBPATH=@libdir@:$pyroot_dir
-         export LIBPATH       # Linux, ELF HP-UX
-      else
-         LIBPATH=@libdir@:$pyroot_dir:$LIBPATH
-         export LIBPATH
-      fi
-      if [ -z "${PYTHONPATH}" ]; then
-         PYTHONPATH=$pyroot_dir
-         export PYTHONPATH       # Linux, ELF HP-UX
-      else
-         PYTHONPATH=$pyroot_dir:$PYTHONPATH
-      fi
+      LD_LIBRARY_PATH=@libdir@:$LD_LIBRARY_PATH
+      export LD_LIBRARY_PATH
+   fi
+
+   if [ -z "${DYLD_LIBRARY_PATH}" ]; then
+      DYLD_LIBRARY_PATH=@libdir@
+      export DYLD_LIBRARY_PATH       # Linux, ELF HP-UX
+   else
+      DYLD_LIBRARY_PATH=@libdir@:$DYLD_LIBRARY_PATH
+      export DYLD_LIBRARY_PATH
+   fi
+
+   if [ -z "${SHLIB_PATH}" ]; then
+      SHLIB_PATH=@libdir@
+      export SHLIB_PATH       # Linux, ELF HP-UX
+   else
+      SHLIB_PATH=@libdir@:$SHLIB_PATH
+      export SHLIB_PATH
+   fi
+
+   if [ -z "${LIBPATH}" ]; then
+      LIBPATH=@libdir@
+      export LIBPATH       # Linux, ELF HP-UX
+   else
+      LIBPATH=@libdir@:$LIBPATH
+      export LIBPATH
+   fi
+
+   if [ -z "${PYTHONPATH}" ]; then
+      PYTHONPATH=@libdir@
+      export PYTHONPATH       # Linux, ELF HP-UX
+   else
+      PYTHONPATH=@libdir@:$PYTHONPATH
+      export PYTHONPATH
    fi
 
    if [ -z "${MANPATH}" ]; then
@@ -287,50 +176,8 @@ else
 fi
 
 
-if [ -z "${ROOT_PYTHON_VERSION}" ] ; then
-   py_localruntimedir=@py_localruntimedir@
-   py_version=${py_localruntimedir#@localruntimedir@/python}
-   if [ -d "$ROOTSYS/lib/python${py_version}" ]; then
-      # Experimental PyROOT
-      ROOT_PYTHON_VERSION=${py_version}
-   fi
-else
-   # check if version exists and exit if not
-   if [ ! -d "@libdir@/python${ROOT_PYTHON_VERSION}" ]; then
-      echo ERROR: build with Python version "${ROOT_PYTHON_VERSION}" not found.
-      echo Available versions:
-      for version_path in @libdir@/python*
-      do
-         version_number=${version_path#@libdir@/python}
-         echo ${version_number}
-      done
-      return 1
-   fi
-fi
-
-
-# Check if the directory created in PyROOT experimental exists
-if [ -d "@libdir@/python${ROOT_PYTHON_VERSION}" ]; then
-   exp_pyroot=true
-fi
-# Check if an option EXP_PYROOT=true is passed by command line
-# this covers the case of installation directory that would not be
-# covered with the previous check
-if [ "$EXP_PYROOT" = true ]; then
-    exp_pyroot=true
-fi
-
-
-# Check if we are in build or installation directory
-if [ ! -d "$ROOTSYS/CMakeFiles" ]; then
-   pyroot_dir=@CMAKE_INSTALL_FULL_PYROOTDIR@
-else
-   pyroot_dir=@libdir@/python${ROOT_PYTHON_VERSION}
-fi
-
-
-clean_environment "${exp_pyroot}" "${pyroot_dir}"
-set_environment "${exp_pyroot}" "${pyroot_dir}"
+clean_environment
+set_environment
 
 
 # Prevent Cppyy from checking the PCH (and avoid warning)
@@ -340,13 +187,8 @@ if [ "x`root-config --arch | grep -v win32gcc | grep -i win32`" != "x" ]; then
    ROOTSYS="`cygpath -w $ROOTSYS`"
 fi
 
-
 unset old_rootsys
 unset thisroot
 unset -f drop_from_path
 unset -f clean_environment
 unset -f set_environment
-unset ROOT_PYTHON_VERSION
-unset pyroot_dir
-unset exp_pyroot
-unset EXP_PYROOT
