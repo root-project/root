df7106e4d4911f907efce45701d93f8a3b7e59c1
Sergey Linev <S.Linev@gsi.de>
2020-02-26 08:34:18 +0100

    [geom] use override syntax for TGeoBoolNodes
    More safe way to override virtual methods

--- geom/geom/src/TGeoBoolNode.cxx
+++ geom/geom/src/TGeoBoolNode.cxx
@@ -132,12 +132,12 @@ void TGeoBoolNode::SetSelected(Int_t sel)
 
 TGeoBoolNode::TGeoBoolNode()
 {
-   fLeft     = 0;
-   fRight    = 0;
-   fLeftMat  = 0;
-   fRightMat = 0;
+   fLeft     = nullptr;
+   fRight    = nullptr;
+   fLeftMat  = nullptr;
+   fRightMat = nullptr;
    fNpoints  = 0;
-   fPoints   = 0;
+   fPoints   = nullptr;
    fThreadSize = 0;
    CreateThreadData(1);
 }
@@ -147,12 +147,12 @@ TGeoBoolNode::TGeoBoolNode()
 
 TGeoBoolNode::TGeoBoolNode(const char *expr1, const char *expr2)
 {
-   fLeft     = 0;
-   fRight    = 0;
-   fLeftMat  = 0;
-   fRightMat = 0;
+   fLeft     = nullptr;
+   fRight    = nullptr;
+   fLeftMat  = nullptr;
+   fRightMat = nullptr;
    fNpoints  = 0;
-   fPoints   = 0;
+   fPoints   = nullptr;
    fThreadSize = 0;
    CreateThreadData(1);
    if (!MakeBranch(expr1, kTRUE)) {
@@ -172,7 +172,7 @@ TGeoBoolNode::TGeoBoolNode(TGeoShape *left, TGeoShape *right, TGeoMatrix *lmat, 
    fRight = right;
    fLeftMat = lmat;
    fNpoints  = 0;
-   fPoints   = 0;
+   fPoints   = nullptr;
    fThreadSize = 0;
    CreateThreadData(1);
    if (!fLeftMat) fLeftMat = gGeoIdentity;
@@ -229,7 +229,7 @@ Bool_t TGeoBoolNode::MakeBranch(const char *expr, Bool_t left)
       Error("MakeBranch", "invalid expression");
       return kFALSE;
    }
-   TGeoShape *shape = 0;
+   TGeoShape *shape = nullptr;
    TGeoMatrix *mat;
    TString newshape;
 
@@ -594,7 +594,7 @@ Int_t TGeoUnion::DistanceToPrimitive(Int_t /*px*/, Int_t /*py*/)
 /// Computes distance from a given point inside the shape to its boundary.
 
 Double_t TGeoUnion::DistFromInside(const Double_t *point, const Double_t *dir, Int_t iact,
-                              Double_t step, Double_t *safe) const
+                                   Double_t step, Double_t *safe) const
 {
    if (iact<3 && safe) {
       // compute safe distance
@@ -707,7 +707,7 @@ Double_t TGeoUnion::DistFromInside(const Double_t *point, const Double_t *dir, I
 /// Compute distance from a given outside point to the shape.
 
 Double_t TGeoUnion::DistFromOutside(const Double_t *point, const Double_t *dir, Int_t iact,
-                              Double_t step, Double_t *safe) const
+                                    Double_t step, Double_t *safe) const
 {
    if (iact<3 && safe) {
       // compute safe distance
