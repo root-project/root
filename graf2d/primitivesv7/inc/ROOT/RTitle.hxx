c96f4c0b9a160eed5b98c7425d2a7884440770a2
Sergey Linev <S.Linev@gsi.de>
2020-02-26 16:26:25 +0100

    [rframe] introduce RTitle class
    It is bound to RFrame coordinates and only margin and height can be
    specified

---
+++ graf2d/primitivesv7/inc/ROOT/RTitle.hxx
@@ -0,0 +1,101 @@
+/*************************************************************************
+ * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RTitle
+#define ROOT7_RTitle
+
+#include <ROOT/RDrawable.hxx>
+#include <ROOT/RAttrText.hxx>
+#include <ROOT/RPadPos.hxx>
+
+#include <memory>
+#include <string>
+
+namespace ROOT {
+namespace Experimental {
+
+/** \class ROOT::Experimental::RTitle
+\ingroup GrafROOT7
+\brief A title for the RFrame.
+\author Sergey Linev <s.linev@gsi.de>
+\date 2020-02-26
+\warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+*/
+
+
+class RTitle : public RDrawable {
+
+   class RTitleAttrs : public RAttrBase {
+      friend class RTitle;
+      R__ATTR_CLASS(RTitleAttrs, "", AddString("margin","0.02").AddString("height","0.05"));
+   };
+
+   std::string fText;                    ///< title to display
+   RAttrText  fAttrText{this, "text_"};  ///<! text attributes
+   RTitleAttrs fAttr{this,""};           ///<! title direct attributes
+
+public:
+
+   RTitle() : RDrawable("title") {}
+
+   RTitle(const std::string &txt) : RTitle() { fText = txt; }
+
+   RTitle &SetText(const std::string &t) { fText = t; return *this; }
+   const std::string &GetText() const { return fText; }
+
+   RTitle &SetMargin(const RPadLength &pos)
+   {
+      if (pos.Empty())
+         fAttr.ClearValue("margin");
+      else
+         fAttr.SetValue("margin", pos.AsString());
+
+      return *this;
+   }
+
+   RPadLength GetMargin() const
+   {
+      RPadLength res;
+      auto value = fAttr.GetValue<std::string>("margin");
+      if (!value.empty())
+         res.ParseString(value);
+      return res;
+   }
+
+   RTitle &SetHeight(const RPadLength &pos)
+   {
+      if (pos.Empty())
+         fAttr.ClearValue("height");
+      else
+         fAttr.SetValue("height", pos.AsString());
+
+      return *this;
+   }
+
+   RPadLength GetHeight() const
+   {
+      RPadLength res;
+      auto value = fAttr.GetValue<std::string>("height");
+      if (!value.empty())
+         res.ParseString(value);
+      return res;
+   }
+
+
+   const RAttrText &GetAttrText() const { return fAttrText; }
+   RTitle &SetAttrText(const RAttrText &attr) { fAttrText = attr; return *this; }
+   RAttrText &AttrText() { return fAttrText; }
+
+
+};
+
+
+} // namespace Experimental
+} // namespace ROOT
+
+#endif
