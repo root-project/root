fca20b62067b0c5faee044d98921e16f523c5668
Sergey Linev <S.Linev@gsi.de>
2020-02-27 09:16:25 +0100

    [eve7] introduce REveGeoPainter class
    Will be used to provide geo painter in web mode

---
+++ graf3d/eve7/src/REveGeoPainter.cxx
@@ -0,0 +1,24 @@
+// @(#)root/eve7:$Id$
+// Author: Sergey Linev, 27.02.2020
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/REveGeoPainter.hxx>
+
+using namespace ROOT::Experimental;
+
+REveGeoPainter::REveGeoPainter(TGeoManager *manager) : TVirtualGeoPainter(manager)
+{
+
+}
+
+REveGeoPainter::~REveGeoPainter()
+{
+
+}
