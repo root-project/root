6c633353062c53c274b0090d2a1dbc41169d0429
Sergey Linev <S.Linev@gsi.de>
2020-02-27 10:58:40 +0100

    [eve7] handle "s" draw option of TGeoVolume
    Show wireframes in web viewer

--- graf3d/eve7/src/REveGeomViewer.cxx
+++ graf3d/eve7/src/REveGeomViewer.cxx
@@ -131,7 +131,7 @@ void ROOT::Experimental::REveGeomViewer::SendGeometry(unsigned connid)
 
    auto &json = fDesc.GetDrawJson();
 
-   printf("Produce geometry JSON %d\n", (int) json.length());
+   R__DEBUG_HERE("webeve") << "Produce geometry JSON len: " << json.length();
 
    fWebWindow->Send(connid, json);
 }
@@ -144,8 +144,9 @@ void ROOT::Experimental::REveGeomViewer::SendGeometry(unsigned connid)
 void ROOT::Experimental::REveGeomViewer::SetDrawOptions(const std::string &opt)
 {
    fDesc.SetDrawOptions(opt);
-
-   fWebWindow->Send(0, "DROPT:"s + opt);
+   unsigned connid = fWebWindow->GetConnectionId();
+   if (connid)
+      fWebWindow->Send(connid, "DROPT:"s + opt);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
