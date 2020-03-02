38f006893fe58a5b6c315f35f317e5c59cacaf37
E. G. Patrick Bos <egpbos@gmail.com>
2020-03-02 16:53:03 +0100

    add test_ProcessManager

--- roofit/multiprocess/test/test_ProcessManager.cxx
+++ roofit/multiprocess/test/test_ProcessManager.cxx
@@ -11,3 +11,44 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
+
+#include <RooFit/MultiProcess/ProcessManager.h>  // ... JobManager::process_manager()
+
+#include "gtest/gtest.h"
+
+TEST(TestMPProcessManager, birthAndDeath)
+{
+   // Note that this test does not use terminate() or check for sigterm_received,
+   // it just destroys and exits itself on every process. This is not how it is
+   // used in real life. The checkState test below more closely follows real life
+   // usage.
+   auto pm = RooFit::MultiProcess::ProcessManager(2);
+   sleep(5);
+}
+
+TEST(TestMPProcessManager, checkState)
+{
+   std::size_t N_workers = 2;
+   auto master_pid = getpid();
+   auto pm = RooFit::MultiProcess::ProcessManager(N_workers);
+   ASSERT_TRUE(pm.is_initialized());
+   ASSERT_EQ(pm.N_workers(), N_workers);
+
+   if (pm.is_master()) {
+      EXPECT_EQ(getpid(), master_pid);
+      EXPECT_FALSE(pm.is_queue());
+      EXPECT_FALSE(pm.is_worker());
+      pm.terminate();
+   } else if (pm.is_queue()) {
+      EXPECT_FALSE(pm.is_master());
+      EXPECT_FALSE(pm.is_worker());
+      while(!pm.sigterm_received()) {}
+      std::_Exit(0);
+   } else if (pm.is_worker()) {
+      EXPECT_FALSE(pm.is_master());
+      EXPECT_FALSE(pm.is_queue());
+      EXPECT_TRUE(pm.worker_id() == 0 || pm.worker_id() == 1);
+      while(!pm.sigterm_received()) {}
+      std::_Exit(0);
+   }
+}
