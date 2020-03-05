60bb14091d88cba87ce236284a1c53f38ecf4752
Sergey Linev <S.Linev@gsi.de>
2020-03-05 18:33:19 +0100

    [rpalette] draw palette only with correspondent drawable
    Histogram drawable does not need to have reference - it uses palette
    drawn in the frame

--- graf2d/gpadv7/inc/ROOT/RPaletteDraw.hxx
+++ graf2d/gpadv7/inc/ROOT/RPaletteDraw.hxx
@@ -36,27 +36,20 @@ class RPaletteDraw final : public RDrawable {
       R__ATTR_CLASS(ROwnAttrs, "", AddString("margin","0.02").AddString("size","0.05"));
    };
 
-   Internal::RIOShared<RPalette> fPalette;  ///< I/O capable reference on palette
-   // RPalette   fPalette;                     ///  color palette to draw
+   RPalette   fPalette;                     ///  color palette to draw
    RAttrAxis  fAttrAxis{this, "axis_"};     ///<! axis attributes
-   ROwnAttrs fAttr{this,""};                ///<! own attributes
+   ROwnAttrs  fAttr{this,""};                ///<! own attributes
 
 protected:
 
    bool IsFrameRequired() const final { return true; }
 
-   void CollectShared(Internal::RIOSharedVector_t &vect) final { vect.emplace_back(&fPalette); }
-
    RPaletteDraw() : RDrawable("palette") {}
 
 public:
 
-   // RPaletteDraw(const RPalette &palette) : RPaletteDraw() { fPalette = palette; }
-   // const RPalette &GetPalette() const { return fPalette; }
-
-   RPaletteDraw(std::shared_ptr<RPalette> palette) : RPaletteDraw() { fPalette = palette; }
-
-   std::shared_ptr<RPalette> GetPalette() const { return fPalette.get_shared(); }
+   RPaletteDraw(const RPalette &palette) : RPaletteDraw() { fPalette = palette; }
+   const RPalette &GetPalette() const { return fPalette; }
 
    RPaletteDraw &SetMargin(const RPadLength &pos)
    {
@@ -100,6 +93,12 @@ public:
    RPaletteDraw &SetAttrAxis(const RAttrAxis &attr) { fAttrAxis = attr; return *this; }
    RAttrAxis &AttrAxis() { return fAttrAxis; }
 };
+
+//inline auto GetDrawable(const RPalette &palette)
+//{
+//   return std::make_shared<RPaletteDraw>(palette);
+//}
+
 
 } // namespace Experimental
 } // namespace ROOT
--- tutorials/v7/draw_rh2_colz.cxx
+++ tutorials/v7/draw_rh2_colz.cxx
@@ -1,17 +1,17 @@
 /// \file
 /// \ingroup tutorial_v7
 ///
-/// This macro generates a small V7 TH1D, fills it and draw it in a V7 canvas.
-/// The canvas is display in the web browser
+/// This macro generates a small V7 TH2D, fills it with random values and
+/// draw it in a V7 canvas, using configured web browser
 ///
 /// \macro_code
 ///
-/// \date 2015-03-22
+/// \date 2020-03-04
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
-/// \author Axel Naumann <axel@cern.ch>
+/// \author Sergey Linev <s.linev@gsi.de>
 
 /*************************************************************************
- * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -20,6 +20,9 @@
 
 #include "ROOT/RHistDrawable.hxx"
 #include "ROOT/RCanvas.hxx"
+#include "ROOT/RFrameTitle.hxx"
+#include "ROOT/RPaletteDraw.hxx"
+#include "ROOT/RFrame.hxx"
 #include "TRandom.h"
 
 // macro must be here while cling is not capable to load
@@ -28,25 +31,30 @@ R__LOAD_LIBRARY(libROOTHistDraw)
 
 using namespace ROOT::Experimental;
 
-void draw_rh1()
+void draw_rh2_colz()
 {
    // Create the histogram.
-   RAxisConfig xaxis(25, 0., 10.);
-   auto pHist = std::make_shared<RH1D>(xaxis);
-   auto pHist2 = std::make_shared<RH1D>(xaxis);
+   RAxisConfig xaxis("x", 20, 0., 10.);
+   RAxisConfig yaxis("y", 20, 0., 10.);
+   auto pHist = std::make_shared<RH2D>(xaxis, yaxis);
 
-   for (int n=0;n<1000;n++) {
-      pHist->Fill(gRandom->Gaus(3,0.8));
-      pHist2->Fill(gRandom->Gaus(7,1.2));
+   for (int n=0;n<10000;n++) {
+      pHist->Fill({gRandom->Gaus(5.,2.), gRandom->Gaus(5.,2.)});
    }
+
+   RPalette palette({{0., RColor::kWhite}, {.3, RColor::kRed}, {.7, RColor::kBlue}, {1., RColor::kGreen}});
 
    // Create a canvas to be displayed.
    auto canvas = RCanvas::Create("Canvas Title");
+
+   // should we made special style for frame with palette?
+   canvas->GetOrCreateFrame()->Margins().SetRight(0.2_normal);
+
+   canvas->Draw<RFrameTitle>("2D histogram with color palette");
+
+   canvas->Draw<RPaletteDraw>(palette);
+
    auto draw1 = canvas->Draw(pHist);
-   draw1->AttrLine().SetColor(RColor::kRed).SetWidth(2);
 
-   auto draw2 = canvas->Draw(pHist2);
-   draw2->AttrLine().SetColor(RColor::kBlue).SetWidth(4);
-
-   canvas->Show();
+   canvas->Show("1000x700");
 }
