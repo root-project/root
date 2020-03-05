972b407f7f435edfca57d68a7976a28ae809b94f
Sergey Linev <S.Linev@gsi.de>
2020-03-05 13:44:42 +0100

    [rhist] update LinkDef.h for more axis combinations

--- hist/histv7/inc/LinkDef.h
+++ hist/histv7/inc/LinkDef.h
@@ -18,8 +18,8 @@
 #pragma link C++ class ROOT::Experimental::RH1D+;
 #pragma link C++ class ROOT::Experimental::RH2F+;
 #pragma link C++ class ROOT::Experimental::RH2D+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImpl<ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>,ROOT::Experimental::RAxisEquidistant>+;
-#pragma link C++ class ROOT::Experimental::Detail::RHistImpl<ROOT::Experimental::Detail::RHistData<2,double,vector<double>, ROOT::Experimental::RHistStatContent, ROOT::Experimental::RHistStatUncertainty>, ROOT::Experimental::RAxisEquidistant, ROOT::Experimental::RAxisIrregular>+;
+// #pragma link C++ class ROOT::Experimental::Detail::RHistImpl<ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>,ROOT::Experimental::RAxisEquidistant>+;
+// #pragma link C++ class ROOT::Experimental::Detail::RHistImpl<ROOT::Experimental::Detail::RHistData<2,double,vector<double>, ROOT::Experimental::RHistStatContent, ROOT::Experimental::RHistStatUncertainty>, ROOT::Experimental::RAxisEquidistant, ROOT::Experimental::RAxisIrregular>+;
 #pragma link C++ class ROOT::Experimental::Detail::RHistImplBase<ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>>+;
 #pragma link C++ class ROOT::Experimental::Detail::RHistImplBase<ROOT::Experimental::Detail::RHistData<2,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>>+;
 #pragma link C++ class ROOT::Experimental::Detail::RHistImplPrecisionAgnosticBase<1>+;
@@ -33,10 +33,14 @@
 #pragma link C++ class ROOT::Experimental::RHistStatUncertainty<3,double>+;
 #pragma link C++ class ROOT::Experimental::Detail::RHistData<1,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>+;
 #pragma link C++ class ROOT::Experimental::Detail::RHistData<2,double,vector<double>,ROOT::Experimental::RHistStatContent,ROOT::Experimental::RHistStatUncertainty>+;
-#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant>+;
-#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant,ROOT::Experimental::RAxisIrregular>+;
+#pragma link C++ class ROOT::Experimental::RAxisBase+;
 #pragma link C++ class ROOT::Experimental::RAxisEquidistant+;
 #pragma link C++ class ROOT::Experimental::RAxisIrregular+;
-#pragma link C++ class ROOT::Experimental::RAxisBase+;
+#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant>+;
+#pragma link C++ class tuple<ROOT::Experimental::RAxisIrregular>+;
+#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant,ROOT::Experimental::RAxisIrregular>+;
+#pragma link C++ class tuple<ROOT::Experimental::RAxisIrregular,ROOT::Experimental::RAxisEquidistant>+;
+#pragma link C++ class tuple<ROOT::Experimental::RAxisEquidistant,ROOT::Experimental::RAxisEquidistant>+;
+#pragma link C++ class tuple<ROOT::Experimental::RAxisIrregular,ROOT::Experimental::RAxisIrregular>+;
 
 #endif
