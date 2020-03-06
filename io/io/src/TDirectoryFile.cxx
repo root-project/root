537671c063063b85c2d571995b36ff771b9c70fe
Michael Wilkinson <miwilkin@syr.edu>
2020-03-06 10:16:28 -0500

    [Docs] Clarify that kOverwrite, kWriteDelete, and Purge only replace metadata

--- io/io/src/TDirectoryFile.cxx
+++ io/io/src/TDirectoryFile.cxx
@@ -1232,6 +1232,9 @@ TDirectory *TDirectoryFile::mkdir(const char *name, const char *title, Bool_t re
 ///
 /// By default, only the highest cycle of a key is kept. Keys for which
 /// the "KEEP" flag has been set are not removed. See TKey::Keep().
+/// NOTE: This does not reduce the size of a TFile--
+/// only the metadata is replaced, effectively making the data invisible
+/// without deleting it.
 
 void TDirectoryFile::Purge(Short_t)
 {
