13d6166159f5160e0cb0fd3b3ddbe901a45a2a23
Sergey Linev <S.Linev@gsi.de>
2020-03-07 11:00:21 +0100

    [rpalette] fix GetColor method, provide test

--- graf2d/gpadv7/test/palette.cxx
+++ graf2d/gpadv7/test/palette.cxx
@@ -3,9 +3,20 @@
 #include "ROOT/RPalette.hxx"
 
 // Test palette interpolation
-TEST(Palette, Interpolate)
+TEST(Palette, DiscreteTrivial)
 {
    using namespace ROOT::Experimental;
    RPalette p1(RPalette::kDiscrete, {RColor::kWhite, RColor::kBlack});
    EXPECT_EQ(p1.GetColor(0.), RColor::kWhite);
+   EXPECT_EQ(p1.GetColor(1.), RColor::kBlack);
+}
+
+TEST(Palette, DiscretePlaced)
+{
+   using namespace ROOT::Experimental;
+   RPalette p1(RPalette::kDiscrete, {{0., RColor::kWhite}, {.3, RColor::kRed}, {.7, RColor::kBlue}, {1., RColor::kBlack}});
+   EXPECT_EQ(p1.GetColor(0.), RColor::kWhite);
+   EXPECT_EQ(p1.GetColor(.3), RColor::kRed);
+   EXPECT_EQ(p1.GetColor(.7), RColor::kBlue);
+   EXPECT_EQ(p1.GetColor(1.), RColor::kBlack);
 }
