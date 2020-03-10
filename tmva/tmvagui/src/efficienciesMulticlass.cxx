7d56b5d6de023a9aff2b6f3a5d011a50a0f35c4d
Sergey Linev <S.Linev@gsi.de>
2020-03-10 15:01:11 +0100

    Fix windows warning in tmvagui
    Not used variable

--- tmva/tmvagui/src/efficienciesMulticlass.cxx
+++ tmva/tmvagui/src/efficienciesMulticlass.cxx
@@ -224,7 +224,7 @@ void TMVA::plotEfficienciesMulticlass(roccurvelist_t rocCurves, classcanvasmap_t
          EfficiencyPlotWrapper *plotWrapper = classCanvasMap.at(classname);
          plotWrapper->addGraph(h);
          plotWrapper->addLegendEntry(methodTitle, h);
-      } catch (const std::out_of_range &oor) {
+      } catch (const std::out_of_range &) {
          cout << Form("ERROR: Class %s discovered among plots but was not found by TMVAMulticlassGui. Skipping.",
                       classname.Data())
               << endl;
