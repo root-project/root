33bfdb5fc7633efa8179d4e0bf749f3651961b81
Jakob Blomer <jblomer@cern.ch>
2020-03-11 17:56:53 +0100

    [ntuple] add skeleton for I/O scheduler

---
+++ tree/ntuple/v7/src/RCluster.cxx
@@ -0,0 +1,16 @@
+/// \file RCluster.cxx
+/// \ingroup NTuple ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2020-03-11
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RCluster.hxx>
