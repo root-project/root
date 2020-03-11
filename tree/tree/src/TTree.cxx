1c70014a225b7d112e12c801d4c187d1313f1387
Sergey Linev <S.Linev@gsi.de>
2020-03-11 16:45:19 +0100

    Adjust TTree.h to use expose less includes

--- tree/tree/src/TTree.cxx
+++ tree/tree/src/TTree.cxx
@@ -7699,6 +7699,16 @@ void TTree::RegisterExternalFriend(TFriendElement *fe)
    fExternalFriends->Add(fe);
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+/// Removes external friend
+
+void TTree::RemoveExternalFriend(TFriendElement *fe)
+{
+   if (fExternalFriends) fExternalFriends->Remove((TObject*)fe);
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 /// Remove a friend from the list of friends.
 
