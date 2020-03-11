b660bf28b76c217639fde7e3bef291686fb6812e
Sergey Linev <S.Linev@gsi.de>
2020-03-11 16:04:03 +0100

    Add missing TList.h in hist/operator/runcopy.C macro
    Appears when ROOT build with "less_includes" option

--- root/hist/operator/runcopy.C
+++ root/hist/operator/runcopy.C
@@ -1,5 +1,6 @@
 #include "TH1.h"
 #include "TDirectory.h"
+#include "TList.h"
 
 TH1F *h=0;
 TH1F *h2=0;
@@ -11,13 +12,13 @@ void runcopy()
    if (h2) delete h2;
    if (h3) delete h3;
    fprintf(stdout,"List has %d elements!\n",gDirectory->GetList()->GetSize());
-   
+
    h = new TH1F("h", "", 100, 0, 100);
    h2 = (TH1F *) h->Clone("h2");
    h3 = (TH1F *) h->Clone("h3");
 
    // do something
-   
+
    //h3->SetDirectory(0);
    *h3 = *h + *h2;
    //h3->SetName("h3");
