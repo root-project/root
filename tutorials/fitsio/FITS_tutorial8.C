ea604395a672411f0c3ddfe5bddf8365d8f1bebb
cosimoNigro <cosimonigro2@gmail.com>
2020-03-12 07:51:25 +0100

    changed input test files to rmf from sherpa test data

--- tutorials/fitsio/FITS_tutorial8.C
+++ tutorials/fitsio/FITS_tutorial8.C
@@ -12,19 +12,14 @@
 /// https://heasarc.gsfc.nasa.gov/docs/heasarc/caldb/docs/memos/cal_gen_92_002/cal_gen_92_002.html#tth_sEc3.1
 ///
 /// Variable-length arrays are used to return the non-null row elements of a 
-/// sparse matrix representing the PDF of an energy estimator. 
+/// sparse matrix containing the probability of detecting a photon of a given 
+/// energy at a given detector channel.
 ///
-/// Such a format is a standard in spectral data for X-ray astronomy instruments
-/// and recently has been adopted by gamma-ray astronomy instruments.
-/// The file we use indeed represents the migration matrix of the MAGIC 
-/// gamma-ray telescope and is taken from the publicly available data of the 
-/// following publication 
-/// https://ui.adsabs.harvard.edu/abs/2019A%26A...625A..10N/abstract
-/// The file, included here as an example, is available in the github repository
-/// associated with the publication
-/// https://github.com/open-gamma-ray-astro/joint-crab
-/// under this path 
-/// https://github.com/open-gamma-ray-astro/joint-crab/blob/master/results/spectra/magic/rmf_obs5029747.fits
+/// The test data file, rmf.fits, is taken from the test data of the sherpa X-ray 
+/// analysis tools 
+/// https://cxc.harvard.edu/sherpa/
+/// the original file is available under the repository
+/// https://github.com/sherpa/sherpa-test-data
 /// 
 /// \macro_code
 /// \macro_output
@@ -35,12 +30,12 @@ void FITS_tutorial8()
 { 
    // let us read the first header data unit, opening the file with a FITS viewer
    // we see that the following cells contain variable-length arrays with values 
-   // row 10 column "F_CHAN": (14, 16)
-   // row 10 column "N_CHAN": (1, 1)
-   // row 10 column "MATRIX" : (0.49981028, 0.5001897)
+   // row 215 column "F_CHAN": (1, 68)
+   // row 215 column "N_CHAN": (66, 130)
+   // row 215 column "MATRIX" : (5.8425176E-6, 7.290097E-6, 8.188037E-6, 9.157882E-6, 1.018355E-5, ..)    
    TString dir = gROOT->GetTutorialDir();
-   TFITSHDU* hdu = new TFITSHDU(dir + "/fitsio/rmf_obs5029747.fits", 1);
-   int rownum = 9; // FITS tables are indexed starting from 1
+   TFITSHDU* hdu = new TFITSHDU(dir + "/fitsio/rmf.fits", 1);
+   int rownum = 214; // FITS tables are indexed starting from 1
    TString colname1 = "F_CHAN";
    TString colname2 = "N_CHAN";
    TString colname3 = "MATRIX";
@@ -53,8 +48,9 @@ void FITS_tutorial8()
    TArrayD *arr2 = hdu->GetTabVarLengthVectorCell(rownum, colname2);
    printf("(%f, %f) \n", arr2->At(0), arr2->At(1)); 
    
+   // printing only the first 5 values in the array
    printf("reading in row %d, column %s \n", rownum+1, colname3.Data());
    TArrayD *arr3 = hdu->GetTabVarLengthVectorCell(rownum, colname3);
-   printf("(%f, %f) \n", arr3->At(0), arr3->At(1)); 
+   printf("(%e, %e, %e, %e, %e, ...) \n", arr3->At(0), arr3->At(1), arr3->At(2), arr3->At(3), arr3->At(4)); 
 
 }
