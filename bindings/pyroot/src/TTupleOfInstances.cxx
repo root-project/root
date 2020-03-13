b714733c251a891c1ff33b594de74ca8ec7f0134
Mattias Ellert <mattias.ellert@physics.uu.se>
2020-03-13 11:45:27 +0100

    Fix -Wmissing-field-initializers in python bindings for Python 3.8 and 3.9
    Warnings with python 3.8:
    .../bindings/pyroot/src/MethodProxy.cxx:891:1: warning: missing initializer for member '_typeobject::tp_vectorcall' [-Wmissing-field-initializers]
    .../bindings/pyroot/src/MethodProxy.cxx:891:1: warning: missing initializer for member '_typeobject::tp_print' [-Wmissing-field-initializers]
    
    Warnings with python 3.9:
    .../bindings/pyroot/src/MethodProxy.cxx:891:1: warning: missing initializer for member '_typeobject::tp_vectorcall' [-Wmissing-field-initializers]

--- bindings/pyroot/src/TTupleOfInstances.cxx
+++ bindings/pyroot/src/TTupleOfInstances.cxx
@@ -42,7 +42,8 @@ PyTypeObject TTupleOfInstances_Type = {
    0,                         // tp_basicsize
    0,                         // tp_itemsize
    0,                         // tp_dealloc
-   0,                         // tp_print
+   0,                         // tp_print (python < 3.8)
+                              // tp_vectorcall_offset (python >= 3.8)
    0,                         // tp_getattr
    0,                         // tp_setattr
    0,                         // tp_compare
@@ -91,6 +92,12 @@ PyTypeObject TTupleOfInstances_Type = {
 #endif
 #if PY_VERSION_HEX >= 0x03040000
    , 0                        // tp_finalize
+#endif
+#if PY_VERSION_HEX >= 0x03080000
+   , 0                        // tp_vectorcall
+#if PY_VERSION_HEX < 0x03090000
+   , 0                        // tp_print (python 3.8 only)
+#endif
 #endif
 };
 
