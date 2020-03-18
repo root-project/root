8954548503e7f112695c907686906233432c65fa
Jakob Blomer <jblomer@cern.ch>
2020-03-18 23:04:01 +0100

    [v7, exceptions] improve comments, add RResult<void>

--- core/base/v7/inc/ROOT/RError.hxx
+++ core/base/v7/inc/ROOT/RError.hxx
@@ -23,8 +23,45 @@
 #include <new>
 #include <stdexcept>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
+
+// The RResult<T> class and their related classes are used for call chains that can throw exceptions,
+// such as I/O code paths.  Throwing of the exception is deferred to allow for `if (result)` style error
+// checking where it makes sense.
+//
+// A function returning an RResult might look like this:
+//
+//     RResult<int> MyIOFunc()
+//     {
+//        int rv = syscall(...);
+//        if (rv == -1)
+//           R__FAIL("user-facing error message");
+//        if (rv == kShortcut)
+//           return 42;
+//        R__FORWARD_RESULT(FuncThatReturnsRResultOfInt());
+//     }
+//
+// Code using MyIOFunc might look like this:
+//
+//     auto result = MyIOOperation();
+//     if (!result) {
+//        /* custom error handling or result.Throw() */
+//     }
+//     switch (result.Get()) {
+//        ...
+//     }
+//
+// Note that RResult<void> can be used for a function without return value, like this
+//
+//     RResult<void> DoSomething()
+//     {
+//        if (failure)
+//           R__FAIL("user-facing error messge");
+//        R__SUCCESS
+//     }
+
 
 namespace ROOT {
 namespace Experimental {
@@ -79,6 +116,20 @@ public:
    const RError &GetError() const { return fError; }
 };
 
+
+/// Wrapper class that generates a data member of type T in RResult<T> for all Ts except T == void
+namespace Internal {
+template <typename T>
+struct RResultType {
+   RResultType() = default;
+   explicit RResultType(const T &value) : fValue(value) {}
+   T fValue;
+};
+
+class RResultTypeVoid {};
+} // namespace Internal
+
+
 // clang-format off
 /**
 \class ROOT::Experimental::RResult
@@ -86,21 +137,30 @@ public:
 \brief The class is used as a return type for operations that can fail; wraps a value of type T or an RError
 
 The RResult enforces checking whether it contains a valid value or an error state. If the RResult leaves the scope
-unchecked, it will throw an exception.  RResult can only be allocated on the stack, which is enforced by deleting the
+unchecked, it will throw an exception.  RResult should only be allocated on the stack, which is helped by deleting the
 new operator.  RResult is movable but not copyable to avoid throwing exceptions multiple times.
 */
 // clang-format on
 template <typename T>
-class RResult {
+class RResult : public std::conditional_t<std::is_void<T>::value, Internal::RResultTypeVoid, Internal::RResultType<T>> {
 private:
-   T fValue;
+   /// This is the nullptr for an RResult representing success
    std::unique_ptr<RError> fError;
    /// This is safe because checking an RResult is not a multi-threaded operation
    mutable bool fIsChecked{false};
 
 public:
-   RResult(const T &value) : fValue(value) {}
+   /// Constructor is _not_ explicit in order to allow for `return T();` for functions returning RResult<T>
+   /// Only available if T is not void
+   template <typename Dummy = T, typename = typename std::enable_if_t<!std::is_void<T>::value, Dummy>>
+   RResult(const Dummy &value) : Internal::RResultType<T>(value) {}
+   /// Constructor is _not_ explicit such that the RError returned by R__FAIL can be converted into an RResult<T>
+   /// for any T
    RResult(std::unique_ptr<RError> error) : fError(std::move(error)) {}
+
+   /// RResult<void> has a default contructor that creates an object representing success
+   template <typename Dummy = T, typename = typename std::enable_if_t<std::is_void<T>::value, Dummy>>
+   RResult() {}
 
    RResult(const RResult &other) = delete;
    RResult(RResult &&other) = default;
@@ -122,30 +182,41 @@ public:
       }
    }
 
-   const T &Get()
+   /// Only available if T is not void
+   template <typename Dummy = T>
+   typename std::enable_if_t<!std::is_void<T>::value, const Dummy &>
+   Get()
    {
       if (R__unlikely(fError)) {
          fError->AppendMessage(" (invalid access)");
          throw RException(*fError);
       }
-      return fValue;
+      return Internal::RResultType<T>::fValue;
    }
+
    explicit operator bool() const
    {
       fIsChecked = true;
       return !fError;
    }
+
    RError *GetError() { return fError.get(); }
+
    void Throw() { throw RException(*fError); }
 
-   // Prevent heap construction of RResult objects
+   // Help prevent heap construction of RResult objects. Unchecked RResult objects in failure state should throw
+   // an exception close to the error location. For stack allocated RResult objects, an exception is thrown
+   // the latest when leaving the scope. Heap allocated ill RResult objects can live much longer making it difficult
+   // to trace back the original failure.
    void *operator new(std::size_t size) = delete;
    void *operator new(std::size_t, void *) = delete;
    void *operator new[](std::size_t) = delete;
    void *operator new[](std::size_t, void *) = delete;
 };
 
-/// Short-hand to return an RResult<T> in an error state
+/// Short-hand to return an RResult<void> indicating success
+#define R__SUCCESS return ROOT::Experimental::RResult<void>();
+/// Short-hand to return an RResult<T> in an error state; the RError is implicitly converted into RResult<T>
 #define R__FAIL(msg) return std::make_unique<ROOT::Experimental::RError>(msg, __func__, __FILE__, __LINE__)
 /// Short-hand to return an RResult<T> value from a subroutine to the calling stack frame
 #define R__FORWARD_RESULT(res) if (res.GetError()) { res.GetError()->AddFrame(__func__, __FILE__, __LINE__); } \
