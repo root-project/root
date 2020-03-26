d48d2c8121838c8ef300363b97374383299f2fe6
Oksana Shadura <oksana.shadura@cern.ch>
2020-03-26 17:11:18 +0100

    Update gtest to latest version 1.10.0
    More info: https://github.com/google/googletest/releases/tag/release-1.10.0

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -1572,7 +1572,7 @@ if (testing)
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_SHALLOW 1
-    GIT_TAG release-1.8.0
+    GIT_TAG release-1.10.0
     UPDATE_COMMAND ""
     # TIMEOUT 10
     # # Force separate output paths for debug and release builds to allow easy
