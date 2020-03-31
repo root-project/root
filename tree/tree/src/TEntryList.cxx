b2d0868ca3f873a88d056aab59b7f434c638373e
Philippe Canal <pcanal@fnal.gov>
2020-03-30 20:55:48 -0500

    In TEntryList properly check error cases.
    In TEntryList::Remove and TEntryList::GetEntryAndTree the error case of GetEntry (received
    indirectly by TEntryList::Remove) was not treated and consequently 'undefined' behavior
    followed.  stressEntryList triggered a crash in GetEntryAndTree when reading random
    memory to fill the input TTree led to a situation where a TEntryList was created on a
    large chain but then applied to only a subset of that chain __and__ the selection/random-number
    was such that none of the entries of the subset was selected (but some of the other part of
    the large chain was selected).  This lead GetEntryAndTree to call GetEntry in a state that
    it returned an error code (-1) and left the TEntryList in a state unexpected by GetEntryAndTree
    (namely that fCurrent is nullptr even though fLists is not nullptr)

--- tree/tree/src/TEntryList.cxx
+++ tree/tree/src/TEntryList.cxx
@@ -614,7 +614,9 @@ Bool_t TEntryList::Enter(Long64_t entry, TTree *tree)
 
 Bool_t TEntryList::Remove(Long64_t entry, TTree *tree)
 {
-   if (!tree){
+   if (entry < 0)
+     return kFALSE;
+   if (!tree) {
       if (!fLists) {
          if (!fBlocks) return 0;
          TEntryListBlock *block = 0;
@@ -740,11 +742,17 @@ Long64_t TEntryList::GetEntryAndTree(Int_t index, Int_t &treenum)
 //third sublist will be returned
 
    Long64_t result = GetEntry(index);
+   if (result < 0) {
+      treenum = -1;
+      return result;
+   }
+   R__ASSERT(fLists == nullptr || (fLists != nullptr && fCurrent != nullptr));
    if (fCurrent)
       treenum = fCurrent->fTreeNumber;
    else
       treenum = fTreeNumber;
-   if (treenum<0) return -1;
+   if (treenum < 0)
+      return -1;
 
    return result;
 }
