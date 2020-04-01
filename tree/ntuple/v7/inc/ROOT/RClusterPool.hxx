20699f8186e9657635f4940b2b9f6acd5bc2512f
Jakob Blomer <jblomer@cern.ch>
2020-04-01 14:59:44 +0200

    [ntuple] add RClusterPool skeleton

--- tree/ntuple/v7/inc/ROOT/RClusterPool.hxx
+++ tree/ntuple/v7/inc/ROOT/RClusterPool.hxx
@@ -16,11 +16,62 @@
 #ifndef ROOT7_RClusterPool
 #define ROOT7_RClusterPool
 
+#include <ROOT/RCluster.hxx>
+#include <ROOT/RNTupleUtil.hxx>
+
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <future>
+#include <queue>
+#include <thread>
+#include <vector>
+
 namespace ROOT {
 namespace Experimental {
 namespace Detail {
 
+class RPageSource;
 
+class RClusterPool {
+private:
+   /**
+    * Maximum number of queued cluster requests for the I/O thread. A single request can span mutliple clusters.
+    */
+   static constexpr unsigned int kWorkQueueLimit = 4;
+
+   /**
+    * Request to load a a particular cluster.  Work items come in groups and are executed to the page source.
+    */
+   struct RWorkItem {
+      std::promise<std::unique_ptr<RCluster>> fPromise;
+      DescriptorId_t fClusterId = kInvalidDescriptorId;
+
+      RWorkItem() = default;
+   };
+   using RWorkItemGroup = std::vector<RWorkItem>;
+
+   RPageSource *fPageSource;
+   std::shared_ptr<RCluster> fCurrent;
+   std::future<std::unique_ptr<RCluster>> fNext;
+   std::mutex fLock;
+
+   std::thread fThreadIo;
+   std::mutex fLockWorkQueue;
+   std::condition_variable fCvHasWork;
+   std::condition_variable fCvHasSpace;
+   std::queue<RWorkItemGroup> fWorkQueue;
+
+   void ExecLoadClusters();
+
+public:
+   explicit RClusterPool(RPageSource *pageSource);
+   explicit RClusterPool(const RClusterPool &other) = delete;
+   RClusterPool &operator =(const RClusterPool &other) = delete;
+   ~RClusterPool();
+
+   std::shared_ptr<RCluster> GetCluster(DescriptorId_t clusterId);
+}; // class RClusterPool
 
 } // namespace Detail
 } // namespace Experimental
--- tree/ntuple/v7/src/RClusterPool.cxx
+++ tree/ntuple/v7/src/RClusterPool.cxx
@@ -14,3 +14,91 @@
  *************************************************************************/
 
 #include <ROOT/RClusterPool.hxx>
+#include <ROOT/RNTupleDescriptor.hxx>
+#include <ROOT/RPageStorage.hxx>
+
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <utility>
+
+ROOT::Experimental::Detail::RClusterPool::RClusterPool(RPageSource *pageSource)
+   : fPageSource(pageSource)
+   , fThreadIo(&RClusterPool::ExecLoadClusters, this)
+{
+}
+
+ROOT::Experimental::Detail::RClusterPool::~RClusterPool()
+{
+   {
+      std::unique_lock<std::mutex> lock(fLockWorkQueue);
+      fCvHasSpace.wait(lock, [&]{ return fWorkQueue.size() < kWorkQueueLimit; });
+      fWorkQueue.emplace(RWorkItemGroup());
+      if (fWorkQueue.size() == 1)
+         fCvHasWork.notify_one();
+   }
+   fThreadIo.join();
+}
+
+void ROOT::Experimental::Detail::RClusterPool::ExecLoadClusters()
+{
+   while (true) {
+      RWorkItemGroup workItems;
+      {
+         std::unique_lock<std::mutex> lock(fLockWorkQueue);
+         fCvHasWork.wait(lock, [&]{ return !fWorkQueue.empty(); });
+         workItems = std::move(fWorkQueue.front());
+         fWorkQueue.pop();
+         if (fWorkQueue.empty())
+            fCvHasSpace.notify_one();
+      }
+
+      if (workItems.empty())
+         break;
+
+      //workItem.fPromise.set_value(fPageSource.LoadCluster(workItem.fClusterId));
+   }
+}
+
+std::shared_ptr<ROOT::Experimental::Detail::RCluster>
+ROOT::Experimental::Detail::RClusterPool::GetCluster(ROOT::Experimental::DescriptorId_t clusterId)
+{
+   std::lock_guard<std::mutex> lockGuard(fLock);
+
+   auto nextId = kInvalidDescriptorId;
+
+   if (!fCurrent) {
+      //fCurrent = fPageSource.LoadCluster(clusterId);
+      nextId = fPageSource->GetDescriptor().FindNextClusterId(clusterId);
+   }
+
+   auto cluster = fCurrent;
+   if (cluster->GetId() != clusterId) {
+      cluster = std::move(fNext.get());
+      if (cluster->GetId() != clusterId) {
+         //cluster = std::move(fPageSource.LoadCluster(clusterId));
+      }
+      fCurrent = cluster;
+      nextId = fPageSource->GetDescriptor().FindNextClusterId(clusterId);
+   }
+
+   if (nextId != kInvalidDescriptorId) {
+      std::promise<std::unique_ptr<RCluster>> promise;
+      fNext = promise.get_future();
+      RWorkItem workItem;
+      workItem.fPromise = std::move(promise);
+      workItem.fClusterId = nextId;
+      //RWorkItemGroup itemGroup{workItem};
+      RWorkItemGroup itemGroup;
+      itemGroup.emplace_back(std::move(workItem));
+
+      std::unique_lock<std::mutex> lock(fLockWorkQueue);
+      fCvHasSpace.wait(lock, [&]{ return fWorkQueue.size() < kWorkQueueLimit; });
+      fWorkQueue.emplace(std::move(itemGroup));
+      if (fWorkQueue.size() == 1)
+         fCvHasWork.notify_one();
+   }
+
+   return cluster;
+}
+
