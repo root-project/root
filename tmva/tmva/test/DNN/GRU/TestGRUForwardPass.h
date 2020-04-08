8e039f0becffd3a45d029056c938efc59645e80f
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2020-04-08 11:34:03 +0200

    Improve and fix rnn forward tests for simple RNN cells and GRU
    This commit fixes failures observed in incremental builds with debug mode

--- tmva/tmva/test/DNN/GRU/TestGRUForwardPass.h
+++ tmva/tmva/test/DNN/GRU/TestGRUForwardPass.h
@@ -41,7 +41,7 @@ auto printTensor1(const typename Architecture::Tensor_t &A, const std::string & 
          std::cout << "\n";
       }
       std::cout << "********\n";
-  } 
+  }
 }
 
 
@@ -62,15 +62,15 @@ auto printMatrix1(const typename Architecture::Matrix_t &A, const std::string na
    std::cout << "********\n";
 }
 
-double sigmoid(double x) { 
-   return 1 /( 1 + exp(-x)); 
+double sigmoid(double x) {
+   return 1 /( 1 + exp(-x));
 }
 
 /*! Generic sample test for forward propagation in GRU network. */
 //______________________________________________________________________________
 template <typename Architecture>
-auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_t inputSize)
--> Double_t
+auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_t inputSize, bool debug = false, double tol = 1.E-5)
+-> Bool_t
 {
    using Matrix_t = typename Architecture::Matrix_t;
    using Tensor_t = typename Architecture::Tensor_t;
@@ -79,9 +79,9 @@ auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_
 
 
    //______________________________________________________________________________
-   /* Input Gate: Numerical example. 
-    * Reference: https://medium.com/@aidangomez/let-s-do-this-f9b699de31d9 
-    * TODO: Numerical example for other gates to verify forward pass values and 
+   /* Input Gate: Numerical example.
+    * Reference: https://medium.com/@aidangomez/let-s-do-this-f9b699de31d9
+    * TODO: Numerical example for other gates to verify forward pass values and
     * backward pass values. */
    //______________________________________________________________________________
 
@@ -89,22 +89,25 @@ auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_
    Tensor_t XRef;
    Tensor_t XArch, arr_XArch;
 
+   // std::cout << "Testing GRU Forward for  bs = " << batchSize << " timeSteps = " << timeSteps
+   //       << " inputSize = " << inputSize << " outputSize = " << stateSize << std::endl;
 
    XRef = Architecture::CreateTensor( timeSteps,batchSize, inputSize);
    XArch = Architecture::CreateTensor( batchSize, timeSteps,inputSize);
    arr_XArch = Architecture::CreateTensor( timeSteps,batchSize, inputSize);
-   
-   
+
+
    for (size_t i = 0; i < timeSteps; ++i) {
       Matrix_t m = XRef[i];
       randomMatrix(m);
    }
 
    Architecture::Copy(arr_XArch, XRef); // B x T x D
-   Architecture::Rearrange( XArch, XRef);  // Copy from XRef to XArch 
+   Architecture::Rearrange( XArch, XRef);  // Copy from XRef to XArch
 
    Net_t gru(batchSize, batchSize, timeSteps, inputSize, 0, 0, 0, ELossFunction::kMeanSquaredError, EInitialization::kGauss);
-   GRULayer_t* layer = gru.AddBasicGRULayer(stateSize, inputSize, timeSteps);
+   // make a GRU returning the full sequence
+   GRULayer_t* layer = gru.AddBasicGRULayer(stateSize, inputSize, timeSteps, false, true);
 
    layer->Initialize();
    layer->Print();
@@ -119,11 +122,11 @@ auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_
    Matrix_t resetBiases = layer->GetResetGateBias();                     // H x 1
    Matrix_t candidateBiases = layer->GetCandidateBias();                 // H x 1
    Matrix_t updateBiases = layer->GetUpdateGateBias();                   // H x 1
- 
+
    /*! Get previous hidden state and previous cell state. */
    Matrix_t hiddenState(batchSize, stateSize);       // B x H
    Architecture::Copy(hiddenState, layer->GetState());
-  
+
    /*! Get each gate values. */
    Matrix_t resetGate = layer->GetResetGateValue();            // B x H
    Matrix_t candidateValue = layer->GetCandidateValue();       // B x H
@@ -133,25 +136,30 @@ auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_
    Matrix_t resetTmp(batchSize, stateSize);
    Matrix_t candidateTmp(batchSize, stateSize);
    Matrix_t updateTmp(batchSize, stateSize);
-   Matrix_t Tmp(batchSize, stateSize);
+   Matrix_t tmp(batchSize, stateSize);
 
    gru.Forward(XArch);
 
    Tensor_t outputArch = layer->GetOutput();
 
    Tensor_t arr_outputArch = Architecture::CreateTensor( timeSteps,batchSize, stateSize);
- 
+
    //Architecture::PrintTensor(outputArch,"GRU output");
 
    Architecture::Rearrange(arr_outputArch, outputArch); // B x T x H
 
+   if (debug)
+      Architecture::PrintTensor(arr_outputArch,"GRU FOrward output");
+
    Double_t maximumError = 0.0;
+
+   Architecture::InitializeZero(hiddenState);
 
    /*! Element-wise matrix multiplication of previous hidden
     *  state and weights of previous state followed by computing
     *  next hidden state and next cell state. */
    for (size_t t = 0; t < timeSteps; ++t) {
-      
+
       Architecture::MultiplyTranspose(resetTmp, hiddenState, weightsResetState);
       Architecture::MultiplyTranspose(resetGate, XRef[t], weightsReset);
       Architecture::ScaleAdd(resetGate, resetTmp);
@@ -167,45 +175,63 @@ auto testForwardPass(size_t timeSteps, size_t batchSize, size_t stateSize, size_
       applyMatrix(resetGate, [](double i) { return sigmoid(i); });
       applyMatrix(updateGate, [](double f) { return sigmoid(f); });
 
+      if (debug)
+         Architecture::PrintTensor(Tensor_t(resetGate), "reset gate");
+
       Architecture::Hadamard(resetGate, hiddenState);
-      
+
       Architecture::MultiplyTranspose(candidateTmp, resetGate, weightsCandidateState);
       Architecture::MultiplyTranspose(candidateValue, XRef[t], weightsCandidate);
       Architecture::ScaleAdd(candidateValue, candidateTmp);
 
       Architecture::AddRowWise(candidateValue, candidateBiases);
       applyMatrix(candidateValue, [](double c) { return tanh(c); });
-         
+
       /*! Computing next cell state and next hidden state. */
 
-      Matrix_t tmp(updateGate); // H X 1
+      if (debug) {
+         Architecture::PrintTensor(Tensor_t(updateGate), "update gate");
+         Architecture::PrintTensor(Tensor_t(candidateValue), "candidate value");
+      }
+
+      Architecture::Copy(tmp, updateGate); // H X 1
       for (size_t j = 0; j < (size_t) tmp.GetNcols(); j++) {
          for (size_t i = 0; i < (size_t) tmp.GetNrows(); i++) {
             tmp(i,j) = 1 - tmp(i,j);
          }
       }
 
+      if (debug) Architecture::PrintTensor(Tensor_t(tmp), "tmp");
+
       // Update state
-      Architecture::Hadamard(hiddenState, tmp);
-      Architecture::Hadamard(updateGate, candidateValue);
-      Architecture::ScaleAdd(hiddenState, updateGate);
+      Architecture::Hadamard(updateGate, hiddenState);
+      Architecture::Hadamard(candidateValue, tmp);
+      Architecture::ScaleAdd(candidateValue, updateGate);
+      Architecture::Copy(hiddenState, candidateValue);
 
 
-      TMatrixT<Double_t> output = arr_outputArch[t]; 
+      TMatrixT<Double_t> output = arr_outputArch[t];
+
+
+      if (debug) {
+         std::cout << "GRU output at current time " << std::endl;
+         output.Print();
+         Architecture::PrintTensor(Tensor_t(hiddenState),"expected output at current time");
+      }
+
       Double_t error = maximumRelativeError(output, hiddenState);
       std::cout << "Time " << t << " Delta: " << error << "\n";
 
       maximumError = std::max(error, maximumError);
    }
 
-   if (maximumError > 0.01)  
+   if (maximumError > tol)
       std::cout << "ERROR: - GRU Forward pass test failed !  - Max dev is ";
-         // " bs = " << batchSize << " timeSteps = " << timeSteps 
-         // << " inputSize = " << inputSize << " outputSize = " << stateSize << std::endl;
-   else 
+   else
       std::cout << " Test GRU forward passed ! -   Max dev is ";
- 
-   return maximumError; 
+
+   std::cout << maximumError << std::endl;
+   return maximumError < tol;
 }
 
 #endif // TMVA_TEST_DNN_TEST_RNN_TEST_GRU_FWDPASS_H
