6a4f1c1fd18847b1a06d9807a72c69dc57b8e6c0
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-04-08 11:12:20 +0200

    [DF][NFC] Fix typo in comment

--- tree/dataframe/test/dataframe_simple.cxx
+++ tree/dataframe/test/dataframe_simple.cxx
@@ -185,8 +185,7 @@ TEST_P(RDFSimpleTests, Define_jitted_type_unknown_to_interpreter)
    auto d = tdf.Define("foo", [](){return RFoo();});
    auto d2 = tdf.Define("foo2", [](){return std::array<RFoo, 2>();});
 
-   // We check that the if nothing is done with RFoo in jitted strings
-   // everything works fine
+   // We check that if nothing is done with RFoo in jitted strings everything works fine
    EXPECT_EQ(10U, *d.Count());
 
    EXPECT_ANY_THROW(d.Define("foo3", "foo*2"));
