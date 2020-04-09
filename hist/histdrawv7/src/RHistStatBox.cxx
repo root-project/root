1ed7f3d4cc6fcb2cb69cdff7f6a54cdc0ddb1d64
Sergey Linev <S.Linev@gsi.de>
2020-04-09 16:19:05 +0200

    [rdrawable] implement request interface for clients
    Client can submit request to the server and can assign callback
    to get reply. Requests are derived from dedicated class and via
    sub-classing can get arbitrary access to RDrawable API
    
    As first example, use RHistStatBox for this. When working online,
    statistic will be filled by the server through such special requests

--- hist/histdrawv7/src/RHistStatBox.cxx
+++ hist/histdrawv7/src/RHistStatBox.cxx
@@ -8,40 +8,57 @@
 
 #include "ROOT/RHistStatBox.hxx"
 
+#include <string>
+
 using namespace ROOT::Experimental;
+using namespace std::string_literals;
+
+std::unique_ptr<RDrawableRequest> RHistStatRequest::Process(std::shared_ptr<RDrawable> &drawable)
+{
+   auto stat = std::dynamic_pointer_cast<RHistStatBoxBase>(drawable);
+
+   auto reply = std::make_unique<RHistStatReply>();
+
+   if (stat)
+      stat->FillStatistic(*this, *reply.get());
+
+   return reply;
+}
 
 
-void RHist1StatBox::FillStatistic(RDisplayHistStat &stat, const RPadBase &) const
+void RHist1StatBox::FillStatistic(const RHistStatRequest &req, RHistStatReply &reply) const
 {
    // need to implement statistic fill for RHist1
 
-   stat.AddLine("Entries = 1");
-   stat.AddLine("Mean = 2");
-   stat.AddLine("Std dev = 3");
+   reply.AddLine("Entries = 1");
+   reply.AddLine("Mean = 2");
+   reply.AddLine("Std dev = 3");
+   reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
+   reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
 }
 
 
-void RHist2StatBox::FillStatistic(RDisplayHistStat &stat, const RPadBase &) const
+void RHist2StatBox::FillStatistic(const RHistStatRequest &req, RHistStatReply &reply) const
 {
    // need to implement statistic fill for RHist2
 
-   stat.AddLine("Entries = 1");
-   stat.AddLine("Mean x = 2");
-   stat.AddLine("Mean y = 3");
-   stat.AddLine("Std dev x = 4");
-   stat.AddLine("Std dev y = 5");
+   reply.AddLine("Entries = 1");
+   reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
+   reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
+   reply.AddLine("Y min = "s + std::to_string(req.GetMin(1)));
+   reply.AddLine("Y max = "s + std::to_string(req.GetMax(1)));
 }
 
-void RHist3StatBox::FillStatistic(RDisplayHistStat &stat, const RPadBase &) const
+void RHist3StatBox::FillStatistic(const RHistStatRequest &, RHistStatReply &reply) const
 {
    // need to implement statistic fill for RHist3
 
-   stat.AddLine("Entries = 1");
-   stat.AddLine("Mean x = 2");
-   stat.AddLine("Mean y = 3");
-   stat.AddLine("Mean z = 4");
-   stat.AddLine("Std dev x = 5");
-   stat.AddLine("Std dev y = 6");
-   stat.AddLine("Std dev z = 7");
+   reply.AddLine("Entries = 1");
+   reply.AddLine("Mean x = 2");
+   reply.AddLine("Mean y = 3");
+   reply.AddLine("Mean z = 4");
+   reply.AddLine("Std dev x = 5");
+   reply.AddLine("Std dev y = 6");
+   reply.AddLine("Std dev z = 7");
 }
 
