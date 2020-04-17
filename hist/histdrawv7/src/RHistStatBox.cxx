87907d1e2cac2a9e4ff5e44574075c20a1508b79
Sergey Linev <S.Linev@gsi.de>
2020-04-17 14:52:52 +0200

    [rstatbox] let enable/disable lines in stat box
    Exact info about lines delivered with object - can be adjusted for each
    statbox subclass in the future

--- hist/histdrawv7/src/RHistStatBox.cxx
+++ hist/histdrawv7/src/RHistStatBox.cxx
@@ -19,45 +19,98 @@ std::unique_ptr<RDrawableReply> RHistStatRequest::Process()
 
    auto reply = std::make_unique<RHistStatReply>();
 
-   if (stat)
-      stat->FillStatistic(*this, *reply.get());
+   if (stat) {
+      stat->fShowMask = GetMask();
+
+      if (GetMask() & RHistStatBoxBase::kShowTitle)
+         reply->AddLine(stat->GetTitle());
+
+      stat->FillStatistic(*this, *reply);
+   }
 
    return reply;
 }
 
+
+const std::vector<std::string> &RHistStatBoxBase::GetEntriesNames() const
+{
+   static const std::vector<std::string> sEntries = {"Title","Entries", "Mean","Deviation", "Ranges"};
+   return sEntries;
+}
+
+
 void RHist1StatBox::FillStatistic(const RHistStatRequest &req, RHistStatReply &reply) const
 {
-   // need to implement statistic fill for RHist1
+   // TODO: need to implement statistic fill for RHist1
 
-   reply.AddLine("Entries = 1");
-   reply.AddLine("Mean = 2");
-   reply.AddLine("Std dev = 3");
-   reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
-   reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
+   if (req.GetMask() & kShowEntries)
+      reply.AddLine("Entries = 1");
+
+   if (req.GetMask() & kShowMean)
+      reply.AddLine("Mean = 2");
+
+   if (req.GetMask() & kShowDev)
+      reply.AddLine("Std dev = 3");
+
+   if (req.GetMask() & kShowRange) {
+      reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
+      reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
+   }
 }
 
 
 void RHist2StatBox::FillStatistic(const RHistStatRequest &req, RHistStatReply &reply) const
 {
-   // need to implement statistic fill for RHist2
+   // TODO: need to implement statistic fill for RHist2
 
-   reply.AddLine("Entries = 1");
-   reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
-   reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
-   reply.AddLine("Y min = "s + std::to_string(req.GetMin(1)));
-   reply.AddLine("Y max = "s + std::to_string(req.GetMax(1)));
+   if (req.GetMask() & kShowEntries)
+      reply.AddLine("Entries = 1");
+
+   if (req.GetMask() & kShowMean) {
+      reply.AddLine("Mean x = 2");
+      reply.AddLine("Mean y = 3");
+   }
+
+   if (req.GetMask() & kShowDev) {
+      reply.AddLine("Std dev x = 5");
+      reply.AddLine("Std dev y = 6");
+   }
+
+   if (req.GetMask() & kShowRange) {
+      reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
+      reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
+      reply.AddLine("Y min = "s + std::to_string(req.GetMin(1)));
+      reply.AddLine("Y max = "s + std::to_string(req.GetMax(1)));
+   }
 }
 
-void RHist3StatBox::FillStatistic(const RHistStatRequest &, RHistStatReply &reply) const
+void RHist3StatBox::FillStatistic(const RHistStatRequest &req, RHistStatReply &reply) const
 {
-   // need to implement statistic fill for RHist3
+   // TODO: need to implement statistic fill for RHist3
 
-   reply.AddLine("Entries = 1");
-   reply.AddLine("Mean x = 2");
-   reply.AddLine("Mean y = 3");
-   reply.AddLine("Mean z = 4");
-   reply.AddLine("Std dev x = 5");
-   reply.AddLine("Std dev y = 6");
-   reply.AddLine("Std dev z = 7");
+   if (req.GetMask() & kShowEntries)
+      reply.AddLine("Entries = 1");
+
+   if (req.GetMask() & kShowMean) {
+      reply.AddLine("Mean x = 2");
+      reply.AddLine("Mean y = 3");
+      reply.AddLine("Mean z = 4");
+   }
+
+   if (req.GetMask() & kShowDev) {
+      reply.AddLine("Std dev x = 5");
+      reply.AddLine("Std dev y = 6");
+      reply.AddLine("Std dev z = 7");
+   }
+
+   if (req.GetMask() & kShowRange) {
+      reply.AddLine("X min = "s + std::to_string(req.GetMin(0)));
+      reply.AddLine("X max = "s + std::to_string(req.GetMax(0)));
+      reply.AddLine("Y min = "s + std::to_string(req.GetMin(1)));
+      reply.AddLine("Y max = "s + std::to_string(req.GetMax(1)));
+      reply.AddLine("Z min = "s + std::to_string(req.GetMin(2)));
+      reply.AddLine("Z max = "s + std::to_string(req.GetMax(2)));
+   }
+
 }
 
