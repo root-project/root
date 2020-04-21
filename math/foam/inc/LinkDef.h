0eff54b4606573314e5c4bf1c9ef30071dea9789
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-04-21 10:17:54 +0200

    [Math] Fix crash in TFoam.
    [ROOT-10277] When creating a lot of datasets in RooFit, TFoam eventually
    crashed, since it was using a TRefArray, which does some global UID
    magic that overflows. Replacing the TRefArray by a simple vector of
    indices fixes that.
    This is possible, since the TRefArray only points to active elements in
    another collection, so simply storing the index in that collection is
    sufficient.
    
    A ROOT-6.20 TFoam was created on lxplus, and written to a file. Reading
    this file, and generating the 10 next random numbers is tested now.

--- math/foam/inc/LinkDef.h
+++ math/foam/inc/LinkDef.h
@@ -10,5 +10,20 @@
 #pragma link C++ class TFoamCell+;
 #pragma link C++ class TFoam+;
 #pragma link C++ class TFoamSampler+;
-
+#pragma read sourceClass="TFoam" targetClass="TFoam" version="[1]" \
+  source="Int_t fNCells; TFoamCell **fCells; TRefArray *fCellsAct" target="fNCells,fCells,fCellsAct"\
+  include="TRefArray.h" \
+  code="{fNCells = onfile.fNCells; \
+         fCells = onfile.fCells; \
+         onfile.fCells = nullptr; \
+         fCellsAct.clear(); \
+         for (Int_t i=0; i < onfile.fCellsAct->GetEntries(); ++i) { \
+            const TObject* cellp = onfile.fCellsAct->At(i); \
+            for (Int_t j=0; j < fNCells; ++j) { \
+               if (cellp == fCells[j]) { \
+                 fCellsAct.push_back(j); \
+                 break; \
+               } \
+            } \
+         }}";
 #endif
