4a0d579516f493c83f35cfab9aecf48d21ca7f3a
Vassil Vassilev <v.g.vassilev@gmail.com>
2020-04-23 15:01:21 +0000

    [cxxmodules] Load the dependent modules for both rootcling and rootcling_stage1.

--- core/dictgen/src/rootcling_impl.cxx
+++ core/dictgen/src/rootcling_impl.cxx
@@ -4303,21 +4303,8 @@ int RootClingMain(int argc,
       // RuntimeUniverse.h (via <new>).
       // FIXME: This should really go in the build system as for the rest of the
       // implicitly created modules.
-      if (gOptCxxModule) {
+      if (gOptCxxModule)
          interpPtr->loadModule("_Builtin_intrinsics", /*Complain*/ true);
-         for (llvm::StringRef DepMod : gOptModuleDependencies) {
-            if (DepMod.endswith("_rdict.pcm")) {
-               ROOT::TMetaUtils::Warning(0, "'%s' value is deprecated. Please use [<fullpat>]%s.pcm\n",
-                                         DepMod.data(),
-                                         GetModuleNameFromRdictName(DepMod).str().data());
-            }
-            DepMod = GetModuleNameFromRdictName(DepMod);
-            if (!interpPtr->loadModule(DepMod, /*complain*/false)) {
-               ROOT::TMetaUtils::Error(0, "Module '%s' failed to load.\n",
-                                       DepMod.data());
-            }
-         }
-      }
    }
    cling::Interpreter &interp = *interpPtr;
    clang::CompilerInstance *CI = interp.getCI();
@@ -4367,6 +4354,22 @@ int RootClingMain(int argc,
 
    interp.getOptions().ErrorOut = true;
    interp.enableRawInput(true);
+
+   if (gOptCxxModule) {
+      for (llvm::StringRef DepMod : gOptModuleDependencies) {
+         if (DepMod.endswith("_rdict.pcm")) {
+            ROOT::TMetaUtils::Warning(0, "'%s' value is deprecated. Please use [<fullpat>]%s.pcm\n",
+                                      DepMod.data(),
+                                      GetModuleNameFromRdictName(DepMod).str().data());
+         }
+         DepMod = GetModuleNameFromRdictName(DepMod);
+         if (!interp.loadModule(DepMod, /*complain*/false)) {
+            ROOT::TMetaUtils::Error(0, "Module '%s' failed to load.\n",
+                                    DepMod.data());
+         }
+      }
+   }
+
    if (interp.declare("namespace std {} using namespace std;") != cling::Interpreter::kSuccess) {
       ROOT::TMetaUtils::Error(0, "Error loading the default header files.\n");
       return 1;
