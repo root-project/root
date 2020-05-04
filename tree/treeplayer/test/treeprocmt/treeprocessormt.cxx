4a104edf78d2ac448bfa1f4a2c0a09bf61bc1807
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-05-04 13:56:58 +0200

    [TreeProcMT] Only run test w/ remote file reads if xrootd is ON
    This fixes ROOT-10733.

--- tree/treeplayer/test/treeprocmt/treeprocessormt.cxx
+++ tree/treeplayer/test/treeprocmt/treeprocessormt.cxx
@@ -288,24 +288,6 @@ TEST(TreeProcessorMT, LimitNTasks_CheckClusters)
    gSystem->Unlink(filename);
 }
 
-#if !defined(_MSC_VER) || defined(R__ENABLE_BROKEN_WIN_TESTS)
-TEST(TreeProcessorMT, PathName)
-{
-   auto fname = "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/ZZTo4mu.root";
-   auto f = std::unique_ptr<TFile>(TFile::Open(fname));
-   ASSERT_TRUE(f != nullptr) << "Could not open remote file\n";
-   auto tree = f->Get<TTree>("Events");
-   ROOT::TTreeProcessorMT p(*tree);
-   std::atomic<unsigned int> n(0U);
-   auto func = [&n](TTreeReader &t) {
-      while (t.Next())
-         n++;
-   };
-   p.Process(func);
-   EXPECT_EQ(n.load(), 1499064U) << "Wrong number of events processed!\n";
-}
-#endif
-
 TEST(TreeProcessorMT, TreeWithFriendTree)
 {
    std::vector<std::string> fileNames = {"TreeWithFriendTree_Tree.root", "TreeWithFriendTree_Friend.root"};
