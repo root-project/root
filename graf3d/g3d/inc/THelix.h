975a33a717cf08803e4bf4b09d5940119b2dc07b
Olivier Couet <olivier.couet@cern.ch>
2020-05-04 15:25:03 +0200

    Change THelix Ctor and doc (#5428)
    * Change THelix Ctor and doc
    
    * use xyz instead of p

--- graf3d/g3d/inc/THelix.h
+++ graf3d/g3d/inc/THelix.h
@@ -12,19 +12,6 @@
 #ifndef ROOT_THelix
 #define ROOT_THelix
 
-
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// THelix                                                               //
-//                                                                      //
-// A Helix with axis // z-axis:                                         //
-//                                                                      //
-//  X(t) = X0 - vt / w sin(-wt+phi0)                                    //
-//  Y(t) = Y0 + vt / w cos(-wt+phi0)                                    //
-//  Z(t) = Z0 + vz t                                                    //
-//                                                                      //
-//////////////////////////////////////////////////////////////////////////
-
 #include "TPolyLine3D.h"
 #include "TRotMatrix.h"
 
@@ -59,9 +46,9 @@ public:
    THelix(Double_t x,  Double_t y,  Double_t z,
           Double_t vx, Double_t vy, Double_t vz,
           Double_t w);
-   THelix(Double_t * xyz, Double_t * v, Double_t w,
-          Double_t * range=0, EHelixRangeType rtype=kHelixZ,
-          Double_t * axis=0);
+   THelix(Double_t const* xyz, Double_t const* v, Double_t w,
+          Double_t const* range=0, EHelixRangeType rtype=kHelixZ,
+          Double_t const* axis=0);
    THelix(const THelix &helix);
    virtual ~THelix();
 
@@ -71,13 +58,13 @@ public:
    virtual void    Print(Option_t *option="") const;
    virtual void    SavePrimitive(std::ostream &out, Option_t *option = "");
    virtual void    SetOption(Option_t *option="") {fOption = option;}
-   virtual void    SetAxis(Double_t * axis);       //Define new axis
+   virtual void    SetAxis(Double_t const* axis);       //Define new axis
    virtual void    SetAxis(Double_t x, Double_t y, Double_t z);
    virtual void    SetRange(Double_t * range, EHelixRangeType rtype=kHelixZ);
    virtual void    SetRange(Double_t r1, Double_t r2, EHelixRangeType rtype=kHelixZ);
-   void            SetHelix(Double_t *xyz,  Double_t *v, Double_t w,
-                            Double_t *range=0, EHelixRangeType type=kUnchanged,
-                            Double_t *axis=0);
+   void            SetHelix(Double_t const* xyz,  Double_t const* v, Double_t w,
+                            Double_t const* range=0, EHelixRangeType type=kUnchanged,
+                            Double_t const* axis=0);
 
    ClassDef(THelix,2)  //A Helix drawn as a PolyLine3D
 };
