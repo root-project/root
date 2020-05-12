f53afa9696eaea7d8fd4210060507829fef8f89d
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-05-12 08:27:13 +0200

    [RF] Fix crash in RooAbsAnaConvPdf.
    RooAbsAnaConvPdf was carrying around a RooArgSet, which was
    not used anywhere. This RooArgSet, however, was carrying around a
    pointer to a variable, which becomes a dangling pointer when one creates
    copies of the RooAbsAnaConvPdf and deletes the original.
    Removing this member fixes crashes when invoking copy constructors.

--- roofit/roofitcore/inc/RooAbsAnaConvPdf.h
+++ roofit/roofitcore/inc/RooAbsAnaConvPdf.h
@@ -100,7 +100,6 @@ protected:
 
   RooListProxy _convSet  ;             //  Set of (resModel (x) basisFunc) convolution objects
   RooArgList _basisList ;              //!  List of created basis functions
-  mutable RooArgSet* _convNormSet ;    //!  Subset of last normalization that applies to convolutions
 
 
   class CacheElem : public RooAbsCacheElement {
