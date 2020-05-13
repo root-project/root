d57cdce4c7668db887d1406f2033be4ac66a63c5
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-05-13 18:41:27 +0200

    Fix dataframe_interface test on Windows

--- tree/dataframe/test/dataframe_interface.cxx
+++ tree/dataframe/test/dataframe_interface.cxx
@@ -395,19 +395,40 @@ TEST(RDataFrameInterface, TypeUnknownToInterpreter)
    auto make_s = [] { return SimpleType{0, 0}; };
    auto df = ROOT::RDataFrame(1).Define("res", make_s);
    bool hasThrown = false;
-   EXPECT_RUNTIME_ERROR_WITH_MSG(
-      df.Snapshot("result", "RESULT2.root"),
-      "The type of custom column \"res\" (RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody()::SimpleType) "
-      "is not known to the interpreter, but a just-in-time-compiled Snapshot call requires this column. Make sure to "
-      "create and load ROOT dictionaries for this column's class.");
-   EXPECT_RUNTIME_ERROR_WITH_MSG(
-      df.Define("res2", "res"),
-      "The type of custom column \"res\" (RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody()::SimpleType) "
-      "is not known to the interpreter, but a just-in-time-compiled Define call requires this column. Make sure to "
-      "create and load ROOT dictionaries for this column's class.");
-   EXPECT_RUNTIME_ERROR_WITH_MSG(
-      df.Filter("res; return true;"),
-      "The type of custom column \"res\" (RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody()::SimpleType) "
-      "is not known to the interpreter, but a just-in-time-compiled Filter call requires this column. Make sure to "
-      "create and load ROOT dictionaries for this column's class.");
+   #ifdef _WIN32
+      EXPECT_RUNTIME_ERROR_WITH_MSG(
+         df.Snapshot("result", "RESULT2.root"),
+         "The type of custom column \"res\" (struct `private: virtual void __thiscall "
+         "RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody(void)'::`2'::SimpleType) is not known to the interpreter, "
+         "but a just-in-time-compiled Snapshot call requires this column. Make sure to create and load ROOT dictionaries for "
+         "this column's class.");
+      EXPECT_RUNTIME_ERROR_WITH_MSG(
+         df.Define("res2", "res"),
+         "The type of custom column \"res\" (struct `private: virtual void __thiscall "
+         "RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody(void)'::`2'::SimpleType) is not known to the interpreter, "
+         "but a just-in-time-compiled Define call requires this column. Make sure to create and load ROOT dictionaries for "
+         "this column's class.");
+      EXPECT_RUNTIME_ERROR_WITH_MSG(
+         df.Filter("res; return true;"),
+         "The type of custom column \"res\" (struct `private: virtual void __thiscall "
+         "RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody(void)'::`2'::SimpleType) is not known to the interpreter, "
+         "but a just-in-time-compiled Filter call requires this column. Make sure to create and load ROOT dictionaries for "
+         "this column's class.");
+   #else
+      EXPECT_RUNTIME_ERROR_WITH_MSG(
+         df.Snapshot("result", "RESULT2.root"),
+         "The type of custom column \"res\" (RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody()::SimpleType) "
+         "is not known to the interpreter, but a just-in-time-compiled Snapshot call requires this column. Make sure to "
+         "create and load ROOT dictionaries for this column's class.");
+      EXPECT_RUNTIME_ERROR_WITH_MSG(
+         df.Define("res2", "res"),
+         "The type of custom column \"res\" (RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody()::SimpleType) "
+         "is not known to the interpreter, but a just-in-time-compiled Define call requires this column. Make sure to "
+         "create and load ROOT dictionaries for this column's class.");
+      EXPECT_RUNTIME_ERROR_WITH_MSG(
+         df.Filter("res; return true;"),
+         "The type of custom column \"res\" (RDataFrameInterface_TypeUnknownToInterpreter_Test::TestBody()::SimpleType) "
+         "is not known to the interpreter, but a just-in-time-compiled Filter call requires this column. Make sure to "
+         "create and load ROOT dictionaries for this column's class.");
+   #endif
 }
