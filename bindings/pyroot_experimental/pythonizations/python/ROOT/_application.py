79eec7992e2389e5411f86923d15aac4e92bfc23
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-05-15 16:39:19 +0200

    [PyROOT] Conditionally add thread to process ROOT events
    The PR spawns a separate thread to process ROOT events if we cannot
    attach the update mechanism to an exisiting hook such as existing in the
    interactive Python mode or for ipython/notebooks.
    
    Also adds backward compatibility for ROOT.PyROOT.StartGUIThread and
    shuts down the thread safely at teardown time.
    
    Related to ROOT-10774

--- bindings/pyroot_experimental/pythonizations/python/ROOT/_application.py
+++ bindings/pyroot_experimental/pythonizations/python/ROOT/_application.py
@@ -73,9 +73,27 @@ class PyROOTApplication(object):
     def init_graphics(self):
         """Configure ROOT graphics to be used interactively"""
 
+        # Note that we only end up in this function if gROOT.IsBatch() is false
+        import __main__
         if self._is_ipython and 'IPython' in sys.modules and sys.modules['IPython'].version_info[0] >= 5:
+            # ipython and notebooks, register our event processing with their hooks
             self._ipython_config()
-        else:
+        elif sys.flags.interactive == 1 or not hasattr(__main__, '__file__'):
+            # Python in interactive mode, use the PyOS_InputHook to call our event processing
+            # - sys.flags.interactive checks for the -i flags passed to python
+            # - __main__ does not have the attribute __file__ if the Python prompt is started directly
             self._inputhook_config()
+        else:
+            # Python in script mode, start a separate thread for the event processing
+            def _process_root_events(self):
+                while self.keep_polling:
+                    gSystem.ProcessEvents()
+                    time.sleep(0.01)
+            import threading
+            self.keep_polling = True # Used to shut down the thread safely at teardown time
+            update_thread = threading.Thread(None, _process_root_events, None, (self,))
+            self.process_root_events = update_thread # The thread is joined at teardown time
+            update_thread.daemon = True
+            update_thread.start()
 
         self._set_display_hook()
