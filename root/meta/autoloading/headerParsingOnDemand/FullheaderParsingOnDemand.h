979aa8d2b8002e689b27e993be2bc1f209fd0c94
Axel Naumann <Axel.Naumann@cern.ch>
2020-05-18 14:08:23 +0200

    Add test against ROOT-10777.

--- root/meta/autoloading/headerParsingOnDemand/FullheaderParsingOnDemand.h
+++ root/meta/autoloading/headerParsingOnDemand/FullheaderParsingOnDemand.h
@@ -7,4 +7,13 @@
 
 myClass8<E> a;
 
+// ROOT-10777
+template <class T, bool> class TemplSpec;
+template <class T> class TemplSpec<T*, true> {
+  void func();
+};
+
+template <class T>
+void TemplSpec<T*, true>::func() {}
+
 #endif
