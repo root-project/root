4574521e0e8ee845ab99487eb60916039a7fe445
Max Orok <maxwellorok@gmail.com>
2020-05-25 16:05:30 -0400

    [ntuple] factor out rfield_string test suite

--- tree/ntuple/v7/test/ntuple.cxx
+++ tree/ntuple/v7/test/ntuple.cxx
@@ -450,34 +450,6 @@ TEST(RNTuple, Descriptor)
    delete[] headerBuffer;
 }
 
-// Tests ReadV() in RColumn.hxx (the case where a std::string overflows to the next page)
-TEST(RNTuple, ReadString)
-{
-   const std::string_view ntupleName = "rs";
-   constexpr int numEntries = 2500;
-   const std::string contentString = "foooooo";
-
-   FileRaii fileGuard("test_ntuple_readstring.root");
-   {
-      auto model = RNTupleModel::Create();
-      auto st = model->MakeField<std::string>("st");
-      auto ntuple = RNTupleWriter::Recreate(std::move(model), ntupleName, fileGuard.GetPath());
-
-      for (int i = 0; i < numEntries; ++i) {
-         *st = contentString;
-         ntuple->Fill();
-      }
-   }
-
-   auto ntuple = RNTupleReader::Open(ntupleName, fileGuard.GetPath());
-   auto viewSt = ntuple->GetView<std::string>("st");
-   if (ntuple->GetDescriptor().GetClusterDescriptor(0).GetPageRange(1).fPageInfos.size() < 2) {
-      FAIL(); // This means all entries are inside the same page and numEntries should be increased.
-   }
-   int nElementsPerPage = ntuple->GetDescriptor().GetClusterDescriptor(0).GetPageRange(1).fPageInfos.at(1).fNElements;
-   EXPECT_EQ(contentString, viewSt(nElementsPerPage/7));
-}
-
 #if !defined(_MSC_VER) || defined(R__ENABLE_BROKEN_WIN_TESTS)
 TEST(RNTuple, LargeFile)
 {
