117df72af1af97a9810af06b25c007a5b10a461f
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-05-27 20:54:38 +0200

    Fix pointer formatting on Windows
    Fix this kind of error on Windows:
    14: test_return_labels (ttree_asmatrix.TTreeAsMatrix) ... input_line_80:2:13: error: invalid digit 'A' in octal constant
    14:  *((long*)00A3D0E8) = (long)sizeof(double)
    14:             ^
    14: input_line_81:2:13: error: invalid digit 'A' in octal constant
    14:  *((long*)00A3D0A8) = reinterpret_cast<long>(reinterpret_cast<std::vector<double>*>(17FEB368)->data())
    14:             ^
    14: input_line_81:2:87: error: invalid digit 'F' in decimal constant
    14:  *((long*)00A3D0A8) = reinterpret_cast<long>(reinterpret_cast<std::vector<double>*>(17FEB368)->data())
    14:                                                                                       ^
    14: ERROR

--- bindings/pyroot_experimental/pythonizations/src/RTensorPyz.cxx
+++ bindings/pyroot_experimental/pythonizations/src/RTensorPyz.cxx
@@ -88,7 +88,7 @@ PyObject *PyROOT::AsRTensor(PyObject * /*self*/, PyObject * obj)
    // Construct an RTensor of the correct data-type
    const std::string klassname = "TMVA::Experimental::RTensor<" + cppdtype + ",std::vector<" + cppdtype + ">>";
    std::stringstream code;
-   code << "new " << klassname << "(reinterpret_cast<" << cppdtype << "*>(" << data << "),{";
+   code << "new " << klassname << "(reinterpret_cast<" << cppdtype << "*>(" << std::hex << std::showbase << (intptr_t)data << "),{";
    for (auto s: shape) code << s << ",";
    code << "},{";
    for (auto s: strides) code << s << ",";
