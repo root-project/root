d4bc5fac9231bcb426ecca48b818300d63b3203a
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-05-27 16:44:10 +0200

    Fix several unresolved external symbol errors on Windows
    Fix the following linker errors on Windows:
    
      PyzPythonHelpers.obj : error LNK2001: unresolved external symbol "struct _object * CPyCppyy::PyStrings::gMRO" (?gMRO@PyStrings@CPyCppyy@@3PAU_object@@A) [C:\Users\bellenot\build\pyroot_experimental\bindings\pyroot_experimental\pythonizations\ROOTPythonizations3_8.vcxproj]
      TPyClassGenerator.obj : error LNK2001: unresolved external symbol "struct _object * CPyCppyy::PyStrings::gBases" (?gBases@PyStrings@CPyCppyy@@3PAU_object@@A) [C:\Users\bellenot\build\pyroot_experimental\bindings\tpython\ROOTTPython.vcxproj]
      TPython.obj : error LNK2001: unresolved external symbol "struct _object * CPyCppyy::PyStrings::gBases" (?gBases@PyStrings@CPyCppyy@@3PAU_object@@A) [C:\Users\bellenot\build\pyroot_experimental\bindings\tpython\ROOTTPython.vcxproj]
      TPython.obj : error LNK2001: unresolved external symbol "struct _object * CPyCppyy::PyStrings::gCppName" (?gCppName@PyStrings@CPyCppyy@@3PAU_object@@A) [C:\Users\bellenot\build\pyroot_experimental\bindings\tpython\ROOTTPython.vcxproj]
      TPython.obj : error LNK2001: unresolved external symbol "struct _object * CPyCppyy::PyStrings::gModule" (?gModule@PyStrings@CPyCppyy@@3PAU_object@@A) [C:\Users\bellenot\build\pyroot_experimental\bindings\tpython\ROOTTPython.vcxproj]
      TPython.obj : error LNK2001: unresolved external symbol "struct _object * CPyCppyy::PyStrings::gName" (?gName@PyStrings@CPyCppyy@@3PAU_object@@A) [C:\Users\bellenot\build\pyroot_experimental\bindings\tpython\ROOTTPython.vcxproj]

--- bindings/pyroot_experimental/cppyy/CPyCppyy/src/PyStrings.h
+++ bindings/pyroot_experimental/cppyy/CPyCppyy/src/PyStrings.h
@@ -1,64 +1,67 @@
 #ifndef CPYCPPYY_PYSTRINGS_H
 #define CPYCPPYY_PYSTRINGS_H
 
+// ROOT
+#include "DllImport.h"
+
 namespace CPyCppyy {
 
 // python strings kept for performance reasons
 
 namespace PyStrings {
 
-    extern PyObject* gAssign;
-    extern PyObject* gBases;
-    extern PyObject* gBase;
-    extern PyObject* gCppName;
-    extern PyObject* gDeref;
-    extern PyObject* gPreInc;
-    extern PyObject* gPostInc;
-    extern PyObject* gDict;
-    extern PyObject* gEmptyString;
-    extern PyObject* gEq;
-    extern PyObject* gFollow;
-    extern PyObject* gGetItem;
-    extern PyObject* gGetNoCheck;
-    extern PyObject* gInit;
-    extern PyObject* gIter;
-    extern PyObject* gLen;
-    extern PyObject* gLifeLine;
-    extern PyObject* gModule;
-    extern PyObject* gMRO;
-    extern PyObject* gName;
-    extern PyObject* gNe;
-    extern PyObject* gTypeCode;
-    extern PyObject* gCTypesType;
+    CPYCPPYY_IMPORT PyObject* gAssign;
+    CPYCPPYY_IMPORT PyObject* gBases;
+    CPYCPPYY_IMPORT PyObject* gBase;
+    CPYCPPYY_IMPORT PyObject* gCppName;
+    CPYCPPYY_IMPORT PyObject* gDeref;
+    CPYCPPYY_IMPORT PyObject* gPreInc;
+    CPYCPPYY_IMPORT PyObject* gPostInc;
+    CPYCPPYY_IMPORT PyObject* gDict;
+    CPYCPPYY_IMPORT PyObject* gEmptyString;
+    CPYCPPYY_IMPORT PyObject* gEq;
+    CPYCPPYY_IMPORT PyObject* gFollow;
+    CPYCPPYY_IMPORT PyObject* gGetItem;
+    CPYCPPYY_IMPORT PyObject* gGetNoCheck;
+    CPYCPPYY_IMPORT PyObject* gInit;
+    CPYCPPYY_IMPORT PyObject* gIter;
+    CPYCPPYY_IMPORT PyObject* gLen;
+    CPYCPPYY_IMPORT PyObject* gLifeLine;
+    CPYCPPYY_IMPORT PyObject* gModule;
+    CPYCPPYY_IMPORT PyObject* gMRO;
+    CPYCPPYY_IMPORT PyObject* gName;
+    CPYCPPYY_IMPORT PyObject* gNe;
+    CPYCPPYY_IMPORT PyObject* gTypeCode;
+    CPYCPPYY_IMPORT PyObject* gCTypesType;
 
-    extern PyObject* gUnderlying;
+    CPYCPPYY_IMPORT PyObject* gUnderlying;
 
-    extern PyObject* gAdd;
-    extern PyObject* gSub;
-    extern PyObject* gMul;
-    extern PyObject* gDiv;
+    CPYCPPYY_IMPORT PyObject* gAdd;
+    CPYCPPYY_IMPORT PyObject* gSub;
+    CPYCPPYY_IMPORT PyObject* gMul;
+    CPYCPPYY_IMPORT PyObject* gDiv;
 
-    extern PyObject* gLShift;
-    extern PyObject* gLShiftC;
+    CPYCPPYY_IMPORT PyObject* gLShift;
+    CPYCPPYY_IMPORT PyObject* gLShiftC;
 
-    extern PyObject* gAt;
-    extern PyObject* gBegin;
-    extern PyObject* gEnd;
-    extern PyObject* gFirst;
-    extern PyObject* gSecond;
-    extern PyObject* gSize;
-    extern PyObject* gTemplate;
-    extern PyObject* gVectorAt;
+    CPYCPPYY_IMPORT PyObject* gAt;
+    CPYCPPYY_IMPORT PyObject* gBegin;
+    CPYCPPYY_IMPORT PyObject* gEnd;
+    CPYCPPYY_IMPORT PyObject* gFirst;
+    CPYCPPYY_IMPORT PyObject* gSecond;
+    CPYCPPYY_IMPORT PyObject* gSize;
+    CPYCPPYY_IMPORT PyObject* gTemplate;
+    CPYCPPYY_IMPORT PyObject* gVectorAt;
 
-    extern PyObject* gCppReal;
-    extern PyObject* gCppImag;
+    CPYCPPYY_IMPORT PyObject* gCppReal;
+    CPYCPPYY_IMPORT PyObject* gCppImag;
 
-    extern PyObject* gThisModule;
+    CPYCPPYY_IMPORT PyObject* gThisModule;
 
-    extern PyObject* gNoImplicit;
+    CPYCPPYY_IMPORT PyObject* gNoImplicit;
 
-    extern PyObject* gExPythonize;
-    extern PyObject* gPythonize;
+    CPYCPPYY_IMPORT PyObject* gExPythonize;
+    CPYCPPYY_IMPORT PyObject* gPythonize;
 
 } // namespace PyStrings
 
