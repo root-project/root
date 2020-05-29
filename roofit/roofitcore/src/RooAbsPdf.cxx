34af44fb1c1680a0075d4a99d015339386f9f352
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-05-29 11:35:32 +0200

    [RF][NFC] Remove dead overrides of operModeHook() in several classes.

--- roofit/roofitcore/src/RooAbsPdf.cxx
+++ roofit/roofitcore/src/RooAbsPdf.cxx
@@ -3388,16 +3388,7 @@ Int_t RooAbsPdf::verboseEval()
 
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Dummy implementation
-
-void RooAbsPdf::CacheElem::operModeHook(RooAbsArg::OperMode)
-{
-}
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-/// Destructor of normalization cache element. If this element
+/// Destructor of normalization cache element. If this element 
 /// provides the 'current' normalization stored in RooAbsPdf::_norm
 /// zero _norm pointer here before object pointed to is deleted here
 
