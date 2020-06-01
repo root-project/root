c82daa75dc598b87cf283ce7416e4ba967f49bbb
Philippe Canal <pcanal@fnal.gov>
2020-06-01 16:25:38 -0500

    Factor multiple calls to InheritsFrom

--- tree/tree/src/TTree.cxx
+++ tree/tree/src/TTree.cxx
@@ -2816,8 +2816,9 @@ Int_t TTree::CheckBranchAddressType(TBranch* branch, TClass* ptrClass, EDataType
       // Something went wrong, the warning message has already been issued.
       return kInternalError;
    }
+   bool isBranchElement = branch->InheritsFrom( TBranchElement::Class() );
    if (expectedClass && datatype == kOther_t && ptrClass == 0) {
-      if (branch->InheritsFrom( TBranchElement::Class() )) {
+      if (isBranchElement) {
          TBranchElement* bEl = (TBranchElement*)branch;
          bEl->SetTargetClass( expectedClass->GetName() );
       }
@@ -2866,7 +2867,7 @@ Int_t TTree::CheckBranchAddressType(TBranch* branch, TClass* ptrClass, EDataType
 
    if( expectedClass && ptrClass &&
        expectedClass != ptrClass &&
-       branch->InheritsFrom( TBranchElement::Class() ) &&
+       isBranchElement &&
        ptrClass->GetSchemaRules() &&
        ptrClass->GetSchemaRules()->HasRuleWithSourceClass( expectedClass->GetName() ) ) {
       TBranchElement* bEl = (TBranchElement*)branch;
@@ -2895,7 +2896,7 @@ Int_t TTree::CheckBranchAddressType(TBranch* branch, TClass* ptrClass, EDataType
    } else if (expectedClass && ptrClass && !expectedClass->InheritsFrom(ptrClass)) {
 
       if (expectedClass->GetCollectionProxy() && ptrClass->GetCollectionProxy() &&
-          branch->InheritsFrom( TBranchElement::Class() ) &&
+          isBranchElement &&
           expectedClass->GetCollectionProxy()->GetValueClass() &&
           ptrClass->GetCollectionProxy()->GetValueClass() )
       {
@@ -2915,7 +2916,7 @@ Int_t TTree::CheckBranchAddressType(TBranch* branch, TClass* ptrClass, EDataType
       }
 
       Error("SetBranchAddress", "The pointer type given (%s) does not correspond to the class needed (%s) by the branch: %s", ptrClass->GetName(), expectedClass->GetName(), branch->GetName());
-      if (branch->InheritsFrom( TBranchElement::Class() )) {
+      if (isBranchElement) {
          TBranchElement* bEl = (TBranchElement*)branch;
          bEl->SetTargetClass( expectedClass->GetName() );
       }
@@ -2934,7 +2935,7 @@ Int_t TTree::CheckBranchAddressType(TBranch* branch, TClass* ptrClass, EDataType
       if (expectedClass) {
          Error("SetBranchAddress", "The pointer type given \"%s\" (%d) does not correspond to the type needed \"%s\" by the branch: %s",
                TDataType::GetTypeName(datatype), datatype, expectedClass->GetName(), branch->GetName());
-         if (branch->InheritsFrom( TBranchElement::Class() )) {
+         if (isBranchElement) {
             TBranchElement* bEl = (TBranchElement*)branch;
             bEl->SetTargetClass( expectedClass->GetName() );
          }
@@ -2989,13 +2990,13 @@ Int_t TTree::CheckBranchAddressType(TBranch* branch, TClass* ptrClass, EDataType
    if (expectedClass && expectedClass->GetCollectionProxy() && dynamic_cast<TEmulatedCollectionProxy*>(expectedClass->GetCollectionProxy())) {
       Error("SetBranchAddress", writeStlWithoutProxyMsg,
             expectedClass->GetName(), branch->GetName(), expectedClass->GetName());
-      if (branch->InheritsFrom( TBranchElement::Class() )) {
+      if (isBranchElement) {
          TBranchElement* bEl = (TBranchElement*)branch;
          bEl->SetTargetClass( expectedClass->GetName() );
       }
       return kMissingCompiledCollectionProxy;
    }
-   if (expectedClass && branch->InheritsFrom( TBranchElement::Class() )) {
+   if (expectedClass && isBranchElement) {
       TBranchElement* bEl = (TBranchElement*)branch;
       bEl->SetTargetClass( expectedClass->GetName() );
    }
