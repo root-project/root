ea42dcc730d736077cb3e8a06a5b1db319b8ffb2
Philippe Canal <pcanal@fnal.gov>
2020-06-01 19:55:30 -0500

    TChain: record and carry forward MakeClass state of branches.
    If branches are individual set in MakeClass (Decomposed Object) mode, record this information in TChainElement
    and apply it to the corresponding branches in each of the TTree.

--- tree/tree/src/TChain.cxx
+++ tree/tree/src/TChain.cxx
@@ -1374,6 +1374,10 @@ Long64_t TChain::LoadTree(Long64_t entry)
                      if (!frelement->GetCheckedType()) {
                         Int_t res = CheckBranchAddressType(br, TClass::GetClass(frelement->GetBaddressClassName()),
                                                          (EDataType) frelement->GetBaddressType(), frelement->GetBaddressIsPtr());
+                        if ((res & kNeedEnableDecomposedObj) && !br->GetMakeClass()) {
+                           br->SetMakeClass(kTRUE);
+                        }
+                        frelement->SetDecomposedObj(br->GetMakeClass());
                         frelement->SetCheckedType(kTRUE);
                      }
                      // FIXME: We may have to tell the branch it should
@@ -1650,6 +1654,10 @@ Long64_t TChain::LoadTree(Long64_t entry)
             if (!element->GetCheckedType()) {
                Int_t res = CheckBranchAddressType(br, TClass::GetClass(element->GetBaddressClassName()),
                                                   (EDataType) element->GetBaddressType(), element->GetBaddressIsPtr());
+               if ((res & kNeedEnableDecomposedObj) && !br->GetMakeClass()) {
+                  br->SetMakeClass(kTRUE);
+               }
+               element->SetDecomposedObj(br->GetMakeClass());
                element->SetCheckedType(kTRUE);
             }
             // FIXME: We may have to tell the branch it should
@@ -2494,6 +2502,10 @@ Int_t TChain::SetBranchAddress(const char *bname, void* add, TBranch** ptr)
       }
       if (branch) {
          res = CheckBranchAddressType(branch, TClass::GetClass(element->GetBaddressClassName()), (EDataType) element->GetBaddressType(), element->GetBaddressIsPtr());
+         if ((res & kNeedEnableDecomposedObj) && !branch->GetMakeClass()) {
+            branch->SetMakeClass(kTRUE);
+         }
+         element->SetDecomposedObj(branch->GetMakeClass());
          element->SetCheckedType(kTRUE);
          if (fClones) {
             void* oldAdd = branch->GetAddress();
@@ -2503,9 +2515,13 @@ Int_t TChain::SetBranchAddress(const char *bname, void* add, TBranch** ptr)
                if (cloneBr && (cloneBr->GetAddress() == oldAdd)) {
                   // the clone's branch is still pointing to us
                   cloneBr->SetAddress(add);
+                  if ((res & kNeedEnableDecomposedObj) && !cloneBr->GetMakeClass()) {
+                     cloneBr->SetMakeClass(kTRUE);
+                  }
                }
             }
          }
+
          branch->SetAddress(add);
       } else {
          Error("SetBranchAddress", "unknown branch -> %s", bname);
