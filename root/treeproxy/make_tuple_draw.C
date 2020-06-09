476a917b18e882a77c672a95bdcdb4d300fb22eb
Sergey Linev <S.Linev@gsi.de>
2020-06-09 13:58:25 +0200

    add missing includes to treeproxy test

--- root/treeproxy/make_tuple_draw.C
+++ root/treeproxy/make_tuple_draw.C
@@ -1,11 +1,13 @@
-Double_t make_tuple_draw() {
-   cout.width(7);
-   cout << std::setprecision(2)  << evt.val1 << ", " << evt.val2 << ", " << evt.val3 << endl;
+Double_t make_tuple_draw()
+{
+   std::cout.width(7);
+   std::cout.precision(2);
+   std::cout << evt.val1 << ", " << evt.val2 << ", " << evt.val3 << std::endl;
    int n = trk.N;
-   cout << n << endl;
+   std::cout << n << std::endl;
    for(int i=0; i<n; ++i) {
       float f = trk.arr[i];
-      cout << i << " " << (int)f << endl;
+      std::cout << i << " " << (int)f << std::endl;
    }
    return evt.val1;
 }
