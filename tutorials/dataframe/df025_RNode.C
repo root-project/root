3a3e33869a87c19f44cfd58ee8c726fae69d1eab
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-06-09 09:56:09 +0200

    [DF] Improve RNode.C tutorial
    Show a few more cool tricks, in particular building computation graphs
    using C++ control flow.
    
    This also removes a memleak of the return value of
    TClassEdit::DemangleName (thanks for pointing it out Sergei!).

--- tutorials/dataframe/df025_RNode.C
+++ tutorials/dataframe/df025_RNode.C
@@ -1,38 +1,43 @@
 /// \file
 /// \ingroup tutorial_dataframe
 /// \notebook
-/// RNode is a generic type which represents any transformation node in the computation graph.
-/// This tutorial shows how to take advantage of the RNode class.
+/// Each RDataFrame object has its own type. It helps with performance,
+/// but sometimes it gets in the way of writing simple code that manages RDF objects.
+/// Luckily, every RDF object can be converted to the generic RNode type.
+/// This tutorial shows how to take advantage of RNode to easily manipulate RDataFrames.
 ///
 /// \macro_code
 /// \macro_output
 ///
-/// \date December 2018
+/// \date June 2020
 /// \author Danilo Piparo
+/// \author Enrico Guiraud
 
-/// This function does not need to be a template: the RNode type accommodates all
-/// possible nodes.
+/// A generic function that takes an RDF and applies a string filter
 ROOT::RDF::RNode AddFilter(ROOT::RDF::RNode node, string_view filterStr)
 {
    return node.Filter(filterStr);
 }
 
-/// Trivial helper function which returns the demangled typename from a typeid
-template<typename T>
-std::string GetName(T&)
-{
-   int dummy;
-   return TClassEdit::DemangleName(typeid(T).name(), dummy);
-}
-
 void df025_RNode()
 {
    ROOT::RDataFrame df(8);
-   std::cout << "Type name of input node: " << GetName(df) << std::endl;
+
+   // Using the generic AddFilter helper function defined above: RNode in, RNode out
    auto f1 = AddFilter(df, "rdfentry_ > 0");
    auto f2 = f1.Filter([](ULong64_t e) { return e > 1; }, {"rdfentry_"});
-   std::cout << "Type name of input node: " << GetName(f2) << std::endl;
-   auto f3 = AddFilter(f2, "rdfentry_ > 2");
 
-   std::cout << "Entries passing the selection: " << *f3.Count() << std::endl;
+   // Conditionally applying a filter is simple with ROOT::RDF::RNode
+   bool someCondition = true;
+   auto maybe_filtered = ROOT::RDF::RNode(f2);
+   if (someCondition)
+      maybe_filtered = maybe_filtered.Filter("rdfentry_ > 3");
+
+   // Adding new columns with Define in a loop is simple thanks to ROOT::RDF::RNode
+   auto with_columns = ROOT::RDF::RNode(maybe_filtered);
+   for (auto i = 0; i < 3; ++i)
+      with_columns = with_columns.Define("x" + std::to_string(i), "42");
+
+   // RNode's can be used exactly like any other RDF object
+   std::cout << "Entries passing the selection: " << with_columns.Count().GetValue() << std::endl;
 }
