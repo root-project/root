b04ec63910b53ff788a9b33bdf6877407e1067b8
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-06-11 18:46:08 +0200

    [RF] Add RooBinWidthFunction.
    Added a function that returns the bin width (volume) or the inverse
    bin width. This can be used to convert densities to event counts and
    vice versa, e.g. in HistFactoryModels.

---
+++ roofit/roofitcore/src/RooBinWidthFunction.cxx
@@ -0,0 +1,67 @@
+// Author Stephan Hageboeck, CERN, 10/2020
+/*****************************************************************************
+ * Project: RooFit                                                           *
+ * Package: RooFitCore                                                       *
+ *    File: $Id$
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2020, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+
+/**
+ * \class RooBinWidthFunction
+ *
+ * RooBinWidthFunction is a class that returns the bin width (or volume) given a RooHistFunc.
+ * It can be used to normalise by bin width or to compute event densities. Using the extra
+ * argument of the constructor, it can also return the inverse of the bin width (or volume).
+ */
+
+#include "RooBinWidthFunction.h"
+
+#include "RooDataHist.h"
+#include "RunContext.h"
+
+
+/// Compute current bin of observable, and return its volume or inverse volume, depending
+/// on configuration chosen in the constructor.
+/// If the bin is not valid, return a volume of 1.
+double RooBinWidthFunction::evaluate() const {
+  const RooDataHist& dataHist = _histFunc->dataHist();
+  const auto idx = _histFunc->getBin();
+  auto volumes = dataHist.binVolumes(0, dataHist.numEntries());
+  const double volume = idx >= 0 ? volumes[idx] : 1.;
+
+  return _divideByBinWidth ? 1./volume : volume;
+}
+
+
+/// Compute bin index for all values of the observable(s) in `evalData`, and return their volumes or inverse volumes, depending
+/// on the configuration chosen in the constructor.
+/// If a bin is not valid, return a volume of 1.
+RooSpan<double> RooBinWidthFunction::evaluateSpan(RooBatchCompute::RunContext& evalData, const RooArgSet* /*normSet*/) const {
+  const RooDataHist& dataHist = _histFunc->dataHist();
+  std::vector<Int_t> bins = _histFunc->getBins(evalData);
+  auto volumes = dataHist.binVolumes(0, dataHist.numEntries());
+
+  auto results = evalData.makeBatch(this, bins.size());
+
+  if (_divideByBinWidth) {
+    for (std::size_t i=0; i < bins.size(); ++i) {
+      results[i] = bins[i] >= 0 ? 1./volumes[bins[i]] : 1.;
+    }
+  } else {
+    for (std::size_t i=0; i < bins.size(); ++i) {
+      results[i] = bins[i] >= 0 ? volumes[bins[i]] : 1.;
+    }
+  }
+
+  return results;
+}
