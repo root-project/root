c876ab5bed20298fc78e3d7c8f4775355d1c1f9e
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-06-12 19:16:26 +0200

    [RF][ROOT-10845] Correct IsOnHeap() for RooFit objects.
    Since RooDataSet and RooArgSet use their own operator new, IsOnHeap was
    returning false. Now, memory is allocated using TStorage, which
    correctly sets the kIsOnHeap bit if necessary.

--- roofit/roofitcore/src/MemPoolForRooSets.h
+++ roofit/roofitcore/src/MemPoolForRooSets.h
@@ -24,6 +24,8 @@
 #ifndef ROOFIT_ROOFITCORE_SRC_MEMPOOLFORROOSETS_H_
 #define ROOFIT_ROOFITCORE_SRC_MEMPOOLFORROOSETS_H_
 
+#include "TStorage.h"
+
 #include <vector>
 #include <algorithm>
 #include <set>
@@ -33,7 +35,7 @@ class MemPoolForRooSets {
 
   struct Arena {
     Arena()
-      : ownedMemory{static_cast<RooSet_t *>(::operator new(POOLSIZE * sizeof(RooSet_t)))},
+      : ownedMemory{static_cast<RooSet_t *>(TStorage::ObjectAlloc(POOLSIZE * sizeof(RooSet_t)))},
         memBegin{ownedMemory}, nextItem{ownedMemory},
         memEnd{memBegin + POOLSIZE}, refCount{0}
     {
