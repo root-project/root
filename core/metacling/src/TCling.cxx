64cadd34abbe9ca62e2302d146d9736b0e19b3c3
Philippe Canal <pcanal@fnal.gov>
2020-06-13 11:48:14 -0500

    Revert "[TOREMOVE] Instrument RegisterCxxModules for rstandardscaler"
    This reverts commit 11795550cd77ab10f4df736295b23e67b816e194.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1208,14 +1208,10 @@ const static bool isrstandardscaler = false;
          if (!HasASTFileOnDisk(M, PP, &FullASTFilePath)) {
             if (isrstandardscaler) fprintf(stderr, "DEBUG pcm: false returned by HasASTFileOnDisk %s %s\n", M->Name.c_str(), FullASTFilePath.c_str());
             continue;
-         } else {
-            if (isrstandardscaler) fprintf(stderr, "DEBUG pcm: true returned by HasASTFileOnDisk %s %s\n", M->Name.c_str(), FullASTFilePath.c_str());
          }
 
-         if (GlobalIndex && KnownModuleFileNames.count(FullASTFilePath)) {
-            if (isrstandardscaler) fprintf(stderr, "DEBUG pcm: continue because GlobalIndex && KnownModuleFileNames %s %s\n", M->Name.c_str(), FullASTFilePath.c_str());
+         if (GlobalIndex && KnownModuleFileNames.count(FullASTFilePath))
             continue;
-         }
 
          if (M->IsMissingRequirement) {
             if (isrstandardscaler) fprintf(stderr, "DEBUG pcm: true returned by M->IsMissingRequirement %s %s\n", M->Name.c_str(), FullASTFilePath.c_str());
