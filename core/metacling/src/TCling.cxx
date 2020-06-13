547ee7e56429974e6b080e529b80fffba437d249
Philippe Canal <pcanal@fnal.gov>
2020-06-13 11:48:15 -0500

    Revert "[core,hf] Move debug output to hist2workspace."
    This reverts commit 90e01d27f108b4d79bfab3d0b8ef3a57beec081f.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -2539,12 +2539,12 @@ void TCling::AddIncludePath(const char *path)
 ////////////////////////////////////////////////////////////////////////////////
 /// Visit all members over members, recursing over base classes.
 
-extern bool DEBUG_CRASH_AXEL;
-
 void TCling::InspectMembers(TMemberInspector& insp, const void* obj,
                             const TClass* cl, Bool_t isTransient)
 {
-   if (DEBUG_CRASH_AXEL) {
+   static const bool debugInfo = nullptr != gSystem->Getenv("JENKINS_SERVER_COOKIE") && GetTopLevelMacroName()
+   && (strstr(GetTopLevelMacroName(), "StandardHypoTestDemo.C") || strstr(GetTopLevelMacroName(), "makeExample.C"));
+   if (debugInfo) {
       Info("InspectMembers","In %s, inspecting members for %s", GetTopLevelMacroName(), cl->GetName());
    }
 
