aa90c0ffde2badf209b070d5b1da0a42803f580b
Jakob Blomer <jblomer@cern.ch>
2020-06-15 10:35:58 +0200

    [ntuple] Fix class name spelling in class rule

--- tree/ntuple/inc/LinkDef.h
+++ tree/ntuple/inc/LinkDef.h
@@ -20,7 +20,7 @@
 // Support for auto-loading in the RNTuple tutorials
 #pragma link C++ class ROOT::Experimental::Detail::RFieldBase-;
 #pragma link C++ class ROOT::Experimental::Detail::RFieldBase::RSchemaIterator-;
-#pragma link C++ class ROOT::Experimental::RFieldVector-;
+#pragma link C++ class ROOT::Experimental::RVectorField-;
 #pragma link C++ class ROOT::Experimental::RNTupleReader-;
 #pragma link C++ class ROOT::Experimental::RNTupleWriter-;
 #pragma link C++ class ROOT::Experimental::RNTupleModel-;
