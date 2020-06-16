2a93f7222d9d1d1c4a445336c3d0216b354da63e
Jakob Blomer <jblomer@cern.ch>
2020-06-16 18:21:55 +0200

    [ntuple] Minor improvements to RPageSource::RColumnHandle

--- tree/ntuple/v7/inc/ROOT/RPageStorage.hxx
+++ tree/ntuple/v7/inc/ROOT/RPageStorage.hxx
@@ -74,8 +74,9 @@ public:
       DescriptorId_t fId = kInvalidDescriptorId;
       const RColumn *fColumn = nullptr;
 
-      /// Returns true for a valid column handle
-      operator bool() const { return fId != kInvalidDescriptorId; }
+      /// Returns true for a valid column handle; fColumn and fId should always either both
+      /// be valid or both be invalid.
+      operator bool() const { return fId != kInvalidDescriptorId && fColumn; }
    };
    /// The column handle identifies a column with the current open page storage
    using ColumnHandle_t = RColumnHandle;
