8914d885d65258de0a99f42774dab9680c18f30f
Max Orok <maxwellorok@gmail.com>
2020-06-23 18:21:35 -0400

    add simple liburing smoke test

--- io/io/test/liburing_basics.cxx
+++ io/io/test/liburing_basics.cxx
@@ -3,13 +3,38 @@
 
 #include "gtest/gtest.h"
 
-TEST(Liburing, MakeRing)
+TEST(Liburing, NopRoundTrip)
 {
    struct io_uring ring;
    int ret = io_uring_queue_init(
-      8 /* queue depth */,
+      4 /* queue depth */,
       &ring,
       0 /* no setup flags */
    );
-   EXPECT_EQ(ret, 0); // ring setup succeeded 
+
+   // ring setup succeeded
+   ASSERT_EQ(ret, 0);
+
+   // can make sqes
+   struct io_uring_sqe *sqe;
+   sqe = io_uring_get_sqe(&ring);
+   ASSERT_NE(sqe, (io_uring_sqe*) NULL);
+
+   // can submit sqes to the ring
+   io_uring_prep_nop(sqe);
+   unsigned long udata = 42;
+   io_uring_sqe_set_data(sqe, (void*) udata);
+   ret = io_uring_submit(&ring);
+   // submitted a single sqe
+   ASSERT_EQ(ret, 1);
+
+   // successful wait for cqe
+   struct io_uring_cqe *cqe;
+   ret = io_uring_wait_cqe(&ring, &cqe);
+   ASSERT_EQ(ret, 0);
+   // cqe userdata matches sqe userdata
+   ASSERT_EQ((unsigned long) io_uring_cqe_get_data(cqe), udata);
+
+   io_uring_cqe_seen(&ring, cqe);
+   io_uring_queue_exit(&ring);
 }
