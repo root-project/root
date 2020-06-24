85bef0fd9a2ac8ae4ac6747faf4f7b083346ac08
Jakob Blomer <jblomer@cern.ch>
2020-06-24 14:12:57 +0200

    Use C++11 thread_local in TError.cxx

--- core/base/src/TError.cxx
+++ core/base/src/TError.cxx
@@ -21,7 +21,6 @@ to be replaced by the proper DefaultErrorHandler()
 */
 
 #include "TError.h"
-#include "ThreadLocalStorage.h"
 
 #include <cstdarg>
 #include <cstdio>
@@ -111,8 +110,8 @@ ErrorHandlerFunc_t GetErrorHandler()
 
 void ErrorHandler(Int_t level, const char *location, const char *fmt, std::va_list ap)
 {
-   TTHREAD_TLS(Int_t) buf_size(256);
-   TTHREAD_TLS(char*) buf_storage(nullptr);
+   thread_local Int_t buf_size(256);
+   thread_local char *buf_storage(nullptr);
 
    char small_buf[256];
    char *buf = buf_storage ? buf_storage : small_buf;
