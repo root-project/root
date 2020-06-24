4977573b56fe13c75c6d0d29f5da40bf3dd510ae
Sergey Linev <S.Linev@gsi.de>
2020-06-24 18:15:41 +0200

    Introduce RAttrValue class to access single value from attributes
    It is templated class and used as:
    
        RAttrValue<double> fDoubleValue{this, "width", 5.}; ///<!
    
    It can be add to RDrawable or class, derived from RAttrBase.

---
+++ graf2d/gpadv7/inc/ROOT/RAttrValue.hxx
@@ -0,0 +1,61 @@
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RAttrValue
+#define ROOT7_RAttrValue
+
+#include <ROOT/RAttrBase.hxx>
+
+namespace ROOT {
+namespace Experimental {
+
+/** \class RAttrValue
+\ingroup GpadROOT7
+\author Sergey Linev <s.linev@gsi.de>
+\date 2020-06-24
+\brief Template class to access single value from drawable or other attributes
+\warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
+*/
+
+
+template<typename T>
+class RAttrValue : public RAttrBase {
+protected:
+   RAttrMap  fDefaults;    ///<!    map with default values
+
+   const RAttrMap &GetDefaults() const override { return fDefaults; }
+
+public:
+
+   RAttrValue() = default;
+
+   RAttrValue(RDrawable *drawable, const std::string &name, const T &dflt = T())
+   {
+      fDefaults.AddValue("", dflt);
+      AssignDrawable(drawable, name);
+   }
+
+   RAttrValue(RAttrBase *parent, const std::string &name, const T &dflt = T())
+   {
+      fDefaults.AddValue("", dflt);
+      AssignParent(parent, name);
+   }
+
+   void Set(const T &v) { SetValue("", v); }
+   T Get() const { return GetValue<T>(""); }
+   void Clear() { ClearValue(""); }
+
+   RAttrValue &operator=(const T &v) { Set(v); return *this; }
+
+   operator T() const { return Get(); }
+};
+
+} // namespace Experimental
+} // namespace ROOT
+
+#endif // ROOT7_RAttrValue
