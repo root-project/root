59cbcd771a850460c9c3766ee6512987db88749c
Max Orok <maxwellorok@gmail.com>
2020-06-29 13:12:57 -0400

    [io] add basic io_uring header

---
+++ io/io/inc/ROOT/RIoUring.hxx
@@ -0,0 +1,40 @@
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_RIoUring
+#define ROOT_RIoUring
+
+extern "C" { 
+  #include <liburing.h>
+  #include <liburing/io_uring.h>
+}
+
+#include <ROOT/RError.h>
+
+namespace ROOT {
+namespace Internal {
+
+class IoUring { 
+private: 
+   struct io_uring fRing; 
+public: 
+   explicit IoUring(size_t size) { 
+      int ret = io_uring_queue_init(size, &fRing, 0 /* no flags */);
+      if (ret) { 
+         throw R__FAIL("couldn't open ring"); 
+      }
+   }
+   ~IoUring() { 
+      io_uring_queue_exit(&fRing);
+   }
+}; 
+
+} // namespace Internal
+} // namespace ROOT
+
+#endif
