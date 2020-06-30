a965bf567f0f1fbf577effb204bb0df1b8c2241e
Jakob Blomer <jblomer@cern.ch>
2020-06-30 16:06:35 +0200

    Revert "Use C++11 varargs in TError.cxx"
    This reverts commit 5e55626791ee56c713fcd049f5f08c1e835399d3.

--- core/base/inc/TError.h
+++ core/base/inc/TError.h
@@ -73,7 +73,7 @@ typedef void (*ErrorHandlerFunc_t)(int level, Bool_t abort, const char *location
               const char *msg);
 
 extern "C" void ErrorHandler(int level, const char *location, const char *fmt,
-                             std::va_list va);
+                             va_list va);
 
 extern void DefaultErrorHandler(int level, Bool_t abort, const char *location,
                                 const char *msg);
