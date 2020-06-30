8e6560b46b36c60000e636cbcf9217f4a7f3a79f
Jakob Blomer <jblomer@cern.ch>
2020-06-30 16:06:35 +0200

    Revert "Use C++11 thread_local in TError.cxx"
    This reverts commit 80556a5c51414d218aacf40453e856e4559d2d8a.

--- core/base/src/TError.cxx
+++ core/base/src/TError.cxx
@@ -22,6 +22,7 @@ to be replaced by the proper DefaultErrorHandler()
 
 #include "Varargs.h"
 #include "TError.h"
+#include "ThreadLocalStorage.h"
 
 #include <cstdio>
 #include <cstdlib>
@@ -110,8 +111,8 @@ ErrorHandlerFunc_t GetErrorHandler()
 
 void ErrorHandler(Int_t level, const char *location, const char *fmt, va_list ap)
 {
-   thread_local Int_t buf_size(256);
-   thread_local char* buf_storage(0);
+   TTHREAD_TLS(Int_t) buf_size(256);
+   TTHREAD_TLS(char*) buf_storage(0);
 
    char small_buf[256];
    char *buf = buf_storage ? buf_storage : small_buf;
