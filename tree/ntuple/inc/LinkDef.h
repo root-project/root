0c5dda4984a92f62e6d39708359ab40c35e3fd95
Jakob Blomer <jblomer@cern.ch>
2020-06-30 14:13:11 +0200

    [ntuple] Remove RNTuple classes from Linkdef.h

--- tree/ntuple/inc/LinkDef.h
+++ tree/ntuple/inc/LinkDef.h
@@ -14,17 +14,4 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ nestedtypedefs;
-#pragma link C++ nestedclasses;
-
-// Support for auto-loading in the RNTuple tutorials
-#pragma link C++ class ROOT::Experimental::Detail::RFieldBase;
-#pragma link C++ class ROOT::Experimental::Detail::RFieldBase::RSchemaIterator;
-#pragma link C++ class ROOT::Experimental::RVectorField;
-#pragma link C++ class ROOT::Experimental::RNTupleReader;
-#pragma link C++ class ROOT::Experimental::RNTupleWriter;
-#pragma link C++ class ROOT::Experimental::RNTupleModel;
-
-#pragma link C++ class ROOT::Experimental::RNTuple+;
-
 #endif
