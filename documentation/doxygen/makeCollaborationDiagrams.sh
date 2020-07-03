f02529d86fb3381247082f5054935f9da1ff3315
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-07-03 18:15:29 +0000

    [Doxygen] Streamline doxygen collaboration diagrams.
    - Doxygen was creating one svg for every class, showing in which library it resides. This created
      6000 svgs, which were mostly identical. Now, only ~100 are created for every library in ROOTSYS/lib/,
      and the relevant webpages link to those.
      This speeds up the "listlibs" step of the doxygen process signficantly.
    - Add parallel processing for collaboration diagrams of class webpages.
    - Remove scanning of rootmap files. It doesn't work, any more. TCling needs
      a fix for ROOT-10900, so gInterpreter->GetClassSharedLib() will always
      return the right thing.
    - Further, parallelise the modification of class webpages.

---
+++ documentation/doxygen/makeCollaborationDiagrams.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Scan libraries in ROOTSYS, and create a collaboration diagram for each. Put those in the
+# doxygen html directory to be picked up by the class overview pages.
+
+HTMLPATH=$DOXYGEN_OUTPUT_DIRECTORY/html
+DOXYGEN_LDD=${DOXYGEN_LDD:=ldd}
+dotFile=$(mktemp /tmp/libraries_XXXX.dot)
+
+test -d "$HTMLPATH" || { echo "HTMLPATH '$HTMLPATH' not found."; exit 1; }
+test -d "$ROOTSYS"  || { echo "ROOTSYS not set"; exit 1; }
+
+for libname in ${ROOTSYS}/lib/lib[A-Z]*.so; do
+   libsList=$(${DOXYGEN_LDD} ${libname} | grep -v ${libname})
+   libname=${libname%.so}
+   libname=${libname##*/}
+
+   # Picture name containing the "coll graph"
+   PICNAME="${HTMLPATH}/${libname}__coll__graph.svg"
+
+   libsList=$(echo "$libsList" | sed -e "s/\.so.*$/\";/" | grep -v "\.dylib" | grep 'lib[A-Z]' |
+	   sed -e "s/\(.*\)\(lib.*;\)$/   mainlib->\"\2/")
+
+   # Generate the dot file describing the graph for libraries
+   cat <<EOF > ${dotFile}
+digraph G {
+   rankdir=TB;
+   node [shape=box, fontname=Arial];
+   mainlib [label="${libname}"];
+   ${libsList}
+   mainlib [shape=box, fillcolor="#ABACBA", style=filled];
+}
+EOF
+
+   # Generate the SVG image of the graph
+   dot -Tsvg ${dotFile} -o $PICNAME
+done
