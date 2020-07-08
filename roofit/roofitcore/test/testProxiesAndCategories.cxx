b5456a8313c2f1aa85b9df58b807e1ccd9193d0b
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-07-08 13:43:42 +0200

    [RF] Fix computation of current label in RooAbsCategory.
    [ROOT-10913] When the current label was requested, the category ignored
    whether the state has to be recomputed or not.

--- roofit/roofitcore/test/testProxiesAndCategories.cxx
+++ roofit/roofitcore/test/testProxiesAndCategories.cxx
@@ -99,9 +99,9 @@ public:
 
 protected:
   enum State_t {one = 0, two = 1, three = 2, four = 3};
-  RooCategory* cat;
-  RooDataSet* data;
-  RooCategory* catFromDataset;
+  RooCategory* cat{nullptr};
+  RooDataSet* data{nullptr};
+  RooCategory* catFromDataset{nullptr};
 };
 
 TEST_P(RooCategoryIO, ReadWithRanges) {
@@ -152,6 +152,12 @@ TEST(RooCategory, OverwriteActiveState) {
   myCat["1Lep"] = 2;
 
   EXPECT_EQ(myCat.getCurrentIndex(), 1);
+
+  RooCategory otherCat;
+  otherCat["test1"] = 1;
+  otherCat["test2"] = 2;
+
+  EXPECT_STREQ(otherCat.getCurrentLabel(), "test1");
 }
 
 
