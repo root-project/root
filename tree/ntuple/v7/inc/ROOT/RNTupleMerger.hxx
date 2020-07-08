abeeeaa47c3a1c7ca8d3355a60910aa0ec815559
Max Orok <maxwellorok@gmail.com>
2020-07-08 11:30:52 -0400

    [ntuple] add merger files skeleton to be filled in

---
+++ tree/ntuple/v7/inc/ROOT/RNTupleMerger.hxx
@@ -0,0 +1,44 @@
+/// \file ROOT/RNTupleMerger.hxx
+/// \ingroup NTuple ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch> & Max Orok <maxwellorok@gmail.com>
+/// \date 2020-07-08
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RNTupleMerger
+#define ROOT7_RNTupleMerger
+
+#include <ROOT/RError.hxx>
+#include <ROOT/RNTupleDescriptor.hxx>
+#include <ROOT/RNTupleUtil.hxx>
+
+namespace ROOT {
+namespace Experimental {
+
+// clang-format off
+/**
+\class ROOT::Experimental::RFieldMerger
+\ingroup NTuple
+\brief Two-way merge between NTuple fields
+*/
+// clang-format on
+class RFieldMerger {
+private:
+   /// The merged field descriptor
+   RFieldDescriptor fMergedField = RFieldDescriptor();
+public:
+   static RResult<RFieldMerger> Merge(const RFieldDescriptor &lhs, const RFieldDescriptor &rhs);
+};
+
+} // namespace Experimental
+} // namespace ROOT
+
+#endif
---
+++ tree/ntuple/v7/src/RNTupleMerger.cxx
@@ -0,0 +1,27 @@
+/// \file RNTupleMerger.cxx
+/// \ingroup NTuple ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch> & Max Orok <maxwellorok@gmail.com>
+/// \date 2020-07-08
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RError.hxx>
+#include <ROOT/RNTupleDescriptor.hxx>
+#include <ROOT/RNTupleMerger.hxx>
+#include <ROOT/RNTupleUtil.hxx>
+
+ROOT::Experimental::RResult<ROOT::Experimental::RFieldMerger>
+ROOT::Experimental::RFieldMerger::Merge(const ROOT::Experimental::RFieldDescriptor &lhs,
+   const ROOT::Experimental::RFieldDescriptor &rhs)
+{
+   return R__FAIL("couldn't merge field " + lhs.GetFieldName() + " with field "
+      + rhs.GetFieldName() + " (unimplemented!)");
+}
