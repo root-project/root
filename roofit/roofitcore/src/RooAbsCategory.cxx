1b8e643274cc85aa00653308192291b964fffdb1
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-07-09 15:11:45 +0200

    [RF] Convert static member to static function.
    Makes life easier on windows.

--- roofit/roofitcore/src/RooAbsCategory.cxx
+++ roofit/roofitcore/src/RooAbsCategory.cxx
@@ -64,8 +64,12 @@ using namespace std;
 
 ClassImp(RooAbsCategory);
 
-
-const std::map<std::string, RooAbsCategory::value_type>::value_type RooAbsCategory::_invalidCategory {"", std::numeric_limits<RooAbsCategory::value_type>::min()};
+/// A category state to signify an invalid category. The category name is empty,
+/// the index is the minimal int.
+const decltype(RooAbsCategory::_stateNames)::value_type& RooAbsCategory::invalidCategory() {
+  static const decltype(RooAbsCategory::_stateNames)::value_type invalid{"", std::numeric_limits<value_type>::min()};
+  return invalid;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Constructor
@@ -200,7 +204,7 @@ const std::string& RooAbsCategory::lookupName(value_type index) const {
       return item.first;
   }
 
-  return _invalidCategory.first;
+  return invalidCategory().first;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -238,13 +242,13 @@ const std::map<std::string, RooAbsCategory::value_type>::value_type& RooAbsCateg
   if (hasIndex(index)) {
     coutE(InputArguments) << "RooAbsCategory::" << __func__ << "(" << GetName() << "): index "
 			  << index << " already assigned" << endl ;
-    return _invalidCategory;
+    return invalidCategory();
   }
 
   if (hasLabel(label)) {
     coutE(InputArguments) << "RooAbsCategory::" << __func__ << "(" << GetName() << "): label "
 			  << label << " already assigned or not allowed" << endl ;
-    return _invalidCategory;
+    return invalidCategory();
   }
 
   const auto result = theStateNames.emplace(label, index);
@@ -267,7 +271,7 @@ void RooAbsCategory::clearTypes()
 {
   _stateNames.clear();
   _insertionOrder.clear();
-  _currentIndex = _invalidCategory.second;
+  _currentIndex = invalidCategory().second;
   setShapeDirty() ;
 }
 
@@ -282,7 +286,7 @@ RooAbsCategory::value_type RooAbsCategory::lookupIndex(const std::string& stateN
     return item->second;
   }
 
-  return _invalidCategory.second;
+  return invalidCategory().second;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -571,7 +575,7 @@ const std::map<std::string, RooAbsCategory::value_type>::value_type& RooAbsCateg
   auto& theStateNames = stateNames();
 
   if (n >= theStateNames.size())
-    return _invalidCategory;
+    return invalidCategory();
 
   if (theStateNames.size() != _insertionOrder.size())
     return *std::next(theStateNames.begin(), n);
@@ -580,7 +584,7 @@ const std::map<std::string, RooAbsCategory::value_type>::value_type& RooAbsCateg
   if (item != theStateNames.end())
     return *item;
 
-  return _invalidCategory;
+  return invalidCategory();
 }
 
 
