4a1162bb9e6985c71ae3422ecc13daafe6abdebd
Olivier Couet <olivier.couet@cern.ch>
2020-07-22 13:23:26 +0200

    fix macro3 (#6067)

--- documentation/primer/macros/macro3.C
+++ documentation/primer/macros/macro3.C
@@ -2,18 +2,20 @@
 
 void macro3(){
     auto c = new TCanvas("myCanvas","myCanvas",600,600);
-    Double_t rmin=0.;
-    Double_t rmax=TMath::Pi()*6.;
-    const Int_t npoints=1000;
-    Double_t r[npoints];
-    Double_t theta[npoints];
-    for (Int_t ipt = 0; ipt < npoints; ipt++) {
-        r[ipt] = ipt*(rmax-rmin)/npoints+rmin;
-        theta[ipt] = TMath::Sin(r[ipt]);
+    double rmin = 0.;
+    double rmax = TMath::Pi()*6.;
+    const int npoints = 1000;
+    double r[npoints];
+    double theta[npoints];
+    for (int ipt = 0; ipt < npoints; ipt++) {
+        theta[ipt] = ipt*(rmax-rmin)/npoints+rmin;
+        r[ipt] = TMath::Sin(theta[ipt]);
     }
-    TGraphPolar grP1 (npoints,r,theta);
-    grP1.SetTitle("A Fan");
-    grP1.SetLineWidth(3);
-    grP1.SetLineColor(2);
-    grP1.DrawClone("L");
+    auto grP1 = new TGraphPolar(npoints,theta,r);
+    grP1->SetTitle("A Fan");
+    grP1->SetLineWidth(3);
+    grP1->SetLineColor(2);
+    grP1->Draw("L");
+    gPad->Update();
+    grP1->GetPolargram()->SetToRadian();
 }
