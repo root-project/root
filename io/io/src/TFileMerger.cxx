4292cd40dcc70752e94cc1ba9030e2b7ccd89aee
Max Orok <maxwellorok@gmail.com>
2020-07-24 08:53:02 -0400

    [io] only skip previously seen mergeable TObjects
    Unmergeable TObjects should be passed through unchanged to the output
    file. We can't skip them with a continue statement.

--- io/io/src/TFileMerger.cxx
+++ io/io/src/TFileMerger.cxx
@@ -609,8 +609,6 @@ Bool_t TFileMerger::MergeRecursive(TDirectory *target, TList *sourcelist, Int_t 
                   }
                }
             } else if (cl->IsTObject()) {
-               if (alreadyseen) continue;
-
                // try synthesizing the Merge method call according to the TObject
                TList listH;
                TString listHargs;
@@ -624,6 +622,10 @@ Bool_t TFileMerger::MergeRecursive(TDirectory *target, TList *sourcelist, Int_t 
                   canBeMerged = kFALSE;
                }
                if (canBeMerged) {
+                  if (alreadyseen) {
+                     // skip already seen mergeable objects, don't skip unmergeable objects
+                     continue;
+                  }
                   // Loop over all source files and merge same-name object
                   TFile *nextsource = current_file ? (TFile*)sourcelist->After( current_file ) : (TFile*)sourcelist->First();
                   if (nextsource == 0) {
