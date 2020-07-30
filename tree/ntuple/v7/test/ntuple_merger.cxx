8c923d60a4b0888be13e5a493ada2838428906ab
Max Orok <maxwellorok@gmail.com>
2020-07-30 16:32:07 -0400

    [ntuple] set up unit test fixture for testing page-storage level RNTuple merges

--- tree/ntuple/v7/test/ntuple_merger.cxx
+++ tree/ntuple/v7/test/ntuple_merger.cxx
@@ -1,5 +1,56 @@
 #include "ntuple_test.hxx"
 
+class RNTupleMergerTest : public ::testing::Test {
+protected:
+   std::vector<std::string> fFiles;
+   std::vector<std::unique_ptr<RPageSource>> fMergeSources;
+   std::string fNtplName = "some_ntuple";
+
+   void SetUp() override {
+      auto modelWrite = RNTupleModel::Create();
+      auto wrPt = modelWrite->MakeField<float>("pt", 42.0);
+      auto wrEnergy = modelWrite->MakeField<float>("energy", 7.0);
+      auto wrTag = modelWrite->MakeField<std::string>("tag", "xyz");
+      auto wrBools = modelWrite->MakeField<std::vector<bool>>("bools", std::vector<bool>{true, false, true, false});
+      auto wrJets = modelWrite->MakeField<std::vector<float>>("jets");
+      wrJets->push_back(1.0);
+      wrJets->push_back(2.0);
+      auto wrNnlo = modelWrite->MakeField<std::vector<std::vector<float>>>("nnlo");
+      wrNnlo->push_back(std::vector<float>());
+      wrNnlo->push_back(std::vector<float>{1.0});
+      wrNnlo->push_back(std::vector<float>{1.0, 2.0, 4.0, 8.0});
+
+      auto numFiles = 10;
+      for (int i = 0; i < numFiles; ++i) {
+         auto file = "merger_input" + std::to_string(i) + ".root";
+         fFiles.push_back(file);
+         auto ntuple = RNTupleWriter::Recreate(
+            std::unique_ptr<RNTupleModel>(modelWrite->Clone()), fNtplName, file);
+         ntuple->Fill();
+      }
+      for (int i = 0; i < numFiles; ++i) {
+         fMergeSources.push_back(RPageSource::Create(fNtplName, fFiles.at(i)));
+      }
+   }
+   void TearDown() override {
+      for (auto file: fFiles) {
+         std::remove(file.c_str());
+      }
+   }
+};
+
+TEST_F(RNTupleMergerTest, LowLevelMerge)
+{
+   if (fMergeSources.size() == 0) {
+      FAIL() << "no page sources opened to merge";
+   }
+   const auto& ref_desc = fMergeSources.front()->GetDescriptor();
+   for (std::size_t i = 0; i < fMergeSources.size(); ++i) {
+      std::cout << "file: " << fFiles.at(i) << "\n";
+      EXPECT_EQ(ref_desc, fMergeSources.at(i)->GetDescriptor());
+   }
+}
+
 TEST(RFieldMerger, Merge)
 {
    auto mergeResult = RFieldMerger::Merge(RFieldDescriptor(), RFieldDescriptor());
