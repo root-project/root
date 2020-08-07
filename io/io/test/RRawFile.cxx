62e6cb9b12d6ee2890f449318c947c2a25f8cdc6
Max Orok <maxwellorok@gmail.com>
2020-08-07 15:50:22 -0400

    [io] extract common io test header

--- io/io/test/RRawFile.cxx
+++ io/io/test/RRawFile.cxx
@@ -1,42 +1,6 @@
-#include "RConfigure.h"
-#include "ROOT/RRawFile.hxx"
-#include "ROOT/RMakeUnique.hxx"
-
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <fstream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <utility>
-
-#include "gtest/gtest.h"
-
-using RRawFile = ROOT::Internal::RRawFile;
+#include "io_test.hxx"
 
 namespace {
-
-/**
- * An RAII wrapper around an open temporary file on disk. It cleans up the guarded file when the wrapper object
- * goes out of scope.
- */
-class FileRaii {
-private:
-   std::string fPath;
-public:
-   FileRaii(const std::string &path, const std::string &content) : fPath(path)
-   {
-      std::ofstream ostrm(path, std::ios::binary | std::ios::out | std::ios::trunc);
-      ostrm << content;
-   }
-   FileRaii(const FileRaii&) = delete;
-   FileRaii& operator=(const FileRaii&) = delete;
-   ~FileRaii() {
-      std::remove(fPath.c_str());
-   }
-};
-
 
 /**
  * A minimal RRawFile implementation that serves data from a string. It keeps a counter of the number of read calls
