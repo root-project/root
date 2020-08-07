8bc19ca2eef388c509b9ddd2dc0e33441f2433a2
Vassil Vassilev <v.g.vassilev@gmail.com>
2020-08-07 14:41:33 +0000

    Try to fix classification

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -2916,8 +2916,6 @@ void TCling::InspectMembers(TMemberInspector& insp, const void* obj,
    // inspect bases
    // TNamed::ShowMembers(R__insp);
    unsigned iNBase = 0;
-   // Iteration over base classes might deserialize.
-   cling::Interpreter::PushTransactionRAII deserRAII(GetInterpreterImpl());
    for (clang::CXXRecordDecl::base_class_const_iterator iBase
         = recordDecl->bases_begin(), eBase = recordDecl->bases_end();
         iBase != eBase; ++iBase, ++iNBase) {
