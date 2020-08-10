cdaf2b2ae1e2bffd25bdc62cf190c242dd56169b
Jakob Blomer <jblomer@cern.ch>
2020-08-10 17:12:41 +0200

    [ntuple] Add skeleton for RPageSourceFriends

---
+++ tree/ntuple/v7/inc/ROOT/RPageSourceFriends.hxx
@@ -0,0 +1,19 @@
+/// \file ROOT/RPageSourceFriends.hxx
+/// \ingroup NTuple ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2020-08-10
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RPageSourceFriends
+#define ROOT7_RPageSourceFriends
+
+#endif
---
+++ tree/ntuple/v7/src/RPageSourceFriends.cxx
@@ -0,0 +1,14 @@
+/// \file RPageSourceFriends.cxx
+/// \ingroup NTuple ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2019-08-10
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
