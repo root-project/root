8b6d739b5476328ef0c8e704c6a630195c76bbf3
Axel Naumann <Axel.Naumann@cern.ch>
2020-08-11 18:16:50 +0200

    Revert "[cling] Fix clang::CodeGen EH assert on MacOS:"
    This reverts commit 42ddd559b3ef0e970dbb1da4935235a362cf0a5b.
    Not needed anymore after fixing the EST update.

--- interpreter/llvm/src/tools/clang/lib/CodeGen/CGException.cpp
+++ interpreter/llvm/src/tools/clang/lib/CodeGen/CGException.cpp
@@ -448,7 +448,7 @@ void CodeGenFunction::EmitStartEHSpec(const Decl *D) {
   ExceptionSpecificationType EST = Proto->getExceptionSpecType();
 
   // Might need to deserialize
-  if (EST == EST_Uninstantiated || EST == EST_Unevaluated) {
+  if (EST == EST_Uninstantiated) {
     FD = FD->getMostRecentDecl();
     Proto = FD->getType()->getAs<FunctionProtoType>();
     EST = Proto->getExceptionSpecType();
