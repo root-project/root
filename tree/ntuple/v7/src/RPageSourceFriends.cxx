876749f3533257d586ab4081d40cacc56d777bea
Jakob Blomer <jblomer@cern.ch>
2020-08-11 12:09:52 +0200

    [ntuple] Merge origin fields for friend descriptor

--- tree/ntuple/v7/src/RPageSourceFriends.cxx
+++ tree/ntuple/v7/src/RPageSourceFriends.cxx
@@ -12,3 +12,102 @@
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
+
+#include <ROOT/RCluster.hxx>
+#include <ROOT/RLogger.hxx>
+#include <ROOT/RNTupleOptions.hxx>
+#include <ROOT/RPageSourceFriends.hxx>
+
+#include <utility>
+
+ROOT::Experimental::Detail::RPageSourceFriends::RPageSourceFriends(
+   std::string_view ntupleName, std::span<std::unique_ptr<RPageSource>> sources)
+   : RPageSource(ntupleName, RNTupleReadOptions())
+   , fMetrics("RPageSourceFriends")
+{
+   for (auto &s : sources) {
+      fSources.emplace_back(std::move(s));
+      fMetrics.ObserveMetrics(fSources.back()->GetMetrics());
+   }
+}
+
+ROOT::Experimental::Detail::RPageSourceFriends::~RPageSourceFriends() = default;
+
+void ROOT::Experimental::Detail::RPageSourceFriends::AddVirtualField(
+   const RNTupleDescriptor &originDesc,
+   const RFieldDescriptor &originChild,
+   DescriptorId_t virtualParent)
+{
+   auto virtualFieldId = fNextId++;
+   auto virtualField = RDanglingFieldDescriptor(originChild)
+      .FieldId(virtualFieldId)
+      .MakeDescriptor().Unwrap();
+   fBuilder.AddField(virtualField);
+   fBuilder.AddFieldLink(virtualParent, virtualFieldId);
+
+   for (const auto &f : originDesc.GetFieldRange(originChild))
+      AddVirtualField(originDesc, f, virtualFieldId);
+
+   // TODO(jblomer): add column range and dangling columns
+   std::uint32_t columnIdx = 0;
+   DescriptorId_t columnId;
+   while ((columnId = originDesc.FindColumnId(originChild.GetId(), columnIdx)) != kInvalidDescriptorId) {
+      const auto &column = originDesc.GetColumnDescriptor(columnId);
+      fBuilder.AddColumn(fNextId++, virtualFieldId, column.GetVersion(), column.GetModel(), columnIdx);
+      columnIdx++;
+   }
+}
+
+#include <iostream>
+ROOT::Experimental::RNTupleDescriptor ROOT::Experimental::Detail::RPageSourceFriends::AttachImpl()
+{
+   fBuilder.SetNTuple(fNTupleName, "", "", RNTupleVersion(), RNTupleUuid());
+   fBuilder.AddField(RDanglingFieldDescriptor()
+      .FieldId(0)
+      .Structure(ENTupleStructure::kRecord)
+      .MakeDescriptor()
+      .Unwrap());
+
+   for (auto &s : fSources) {
+      s->Attach();
+      const auto &desc = s->GetDescriptor();
+      for (const auto &f : desc.GetTopLevelFields())
+         AddVirtualField(desc, f, 0);
+   }
+
+   fBuilder.EnsureValidDescriptor();
+   fBuilder.GetDescriptor().PrintInfo(std::cout);
+   return fBuilder.MoveDescriptor();
+}
+
+
+std::unique_ptr<ROOT::Experimental::Detail::RPageSource>
+ROOT::Experimental::Detail::RPageSourceFriends::Clone() const
+{
+
+}
+
+ROOT::Experimental::Detail::RPage
+ROOT::Experimental::Detail::RPageSourceFriends::PopulatePage(ColumnHandle_t columnHandle, NTupleSize_t globalIndex)
+{
+
+}
+
+ROOT::Experimental::Detail::RPage
+ROOT::Experimental::Detail::RPageSourceFriends::PopulatePage(
+   ColumnHandle_t columnHandle, const RClusterIndex &clusterIndex)
+{
+
+}
+
+void ROOT::Experimental::Detail::RPageSourceFriends::ReleasePage(RPage &page)
+{
+
+}
+
+
+std::unique_ptr<ROOT::Experimental::Detail::RCluster>
+ROOT::Experimental::Detail::RPageSourceFriends::LoadCluster(DescriptorId_t clusterId, const ColumnSet_t &columns)
+{
+
+}
--- tree/ntuple/v7/test/ntuple_friends.cxx
+++ tree/ntuple/v7/test/ntuple_friends.cxx
@@ -2,4 +2,31 @@
 
 TEST(RPageStorageFriends, Basic)
 {
+   FileRaii fileGuard1("test_ntuple_friends1.root");
+   FileRaii fileGuard2("test_ntuple_friends2.root");
+
+   auto model1 = RNTupleModel::Create();
+   auto fieldPt = model1->MakeField<float>("pt", 42.0);
+
+   auto model2 = RNTupleModel::Create();
+   auto fieldEta = model1->MakeField<float>("eta", 24.0);
+
+   {
+      RPageSinkFile sink("ntpl", fileGuard1.GetPath(), RNTupleWriteOptions());
+      sink.Create(*model1.get());
+      sink.CommitDataset();
+      model1 = nullptr;
+   }
+   {
+      RPageSinkFile sink("ntpl", fileGuard2.GetPath(), RNTupleWriteOptions());
+      sink.Create(*model2.get());
+      sink.CommitDataset();
+      model2 = nullptr;
+   }
+
+   std::vector<std::unique_ptr<RPageSource>> realSources;
+   realSources.emplace_back(std::make_unique<RPageSourceFile>("ntpl", fileGuard1.GetPath(), RNTupleReadOptions()));
+   realSources.emplace_back(std::make_unique<RPageSourceFile>("ntpl", fileGuard2.GetPath(), RNTupleReadOptions()));
+   RPageSourceFriends friendSource("myNTuple", realSources);
+   friendSource.Attach();
 }
