81b5bdff4df86fd57a88868e733d5fb23f7a1a77
vepadulano <v.e.padulano@gmail.com>
2020-08-14 09:49:22 +0200

    GLOBbing is fine, WARNING if install_name_tool not found

--- cmake/modules/XROOTDApplePostInstall.cmake
+++ cmake/modules/XROOTDApplePostInstall.cmake
@@ -8,22 +8,7 @@
 
 function(xrootd_libs_change_rpath build_libdir install_libdir)
 
-  set(XROOTD_ALL_LIBRARIES libXrdAppUtils.dylib libXrdBlacklistDecision-4.so
-                           libXrdBwm-4.so libXrdCksCalczcrc32-4.so
-                           libXrdCl.dylib libXrdClProxyPlugin-4.so
-                           libXrdClient.dylib libXrdCmsRedirectLocal-4.so
-                           libXrdCrypto.dylib libXrdCryptoLite.dylib
-                           libXrdFfs.dylib libXrdFileCache-4.so
-                           libXrdN2No2p-4.so libXrdOssSIgpfsT-4.so
-                           libXrdPosix.dylib libXrdPosixPreload.dylib
-                           libXrdPss-4.so libXrdSec-4.so libXrdSecProt-4.so
-                           libXrdSeckrb5-4.so libXrdSecpwd-4.so
-                           libXrdSecsss-4.so libXrdSecunix-4.so
-                           libXrdServer.dylib libXrdSsi-4.so
-                           libXrdSsiLib.dylib libXrdSsiLog-4.so
-                           libXrdSsiShMap.dylib libXrdThrottle-4.so
-                           libXrdUtils.dylib libXrdXml.dylib libXrdXrootd-4.so
-  )
+  file(GLOB XROOTD_ALL_LIBRARIES "${install_libdir}/libXrd*")
 
   find_program(INSTALL_NAME_TOOL install_name_tool)
 
@@ -31,13 +16,15 @@ function(xrootd_libs_change_rpath build_libdir install_libdir)
     message(STATUS "Found tool ${INSTALL_NAME_TOOL}")
     message(STATUS "Adjusting LC_RPATH variable of XRootD libraries in ${install_libdir}")
 
-    foreach(XRD_LIB_PATH IN LISTS XROOTD_ALL_LIBRARIES)
+    foreach(XRD_LIB_PATH ${XROOTD_ALL_LIBRARIES})
       execute_process(COMMAND ${INSTALL_NAME_TOOL} -rpath 
                               ${build_libdir} ${install_libdir} 
                               ${install_libdir}/${XRD_LIB_PATH}
                       ERROR_QUIET
       )
     endforeach()
+  else()
+    message(WARNING "install_name_tool was not found. LC_RPATH variable will not be modified.")
   endif()
 
 endfunction()
