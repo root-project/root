c1fa4b97f572acc7bc9312be6074e8c3eecc30ee
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2020-08-17 09:53:10 +0200

    [JupyROOT] ROOT kernel files have same license as ROOT

--- bindings/jupyroot/python/JupyROOT/helpers/handlers.py
+++ bindings/jupyroot/python/JupyROOT/helpers/handlers.py
@@ -1,11 +1,7 @@
 # -*- coding:utf-8 -*-
 #-----------------------------------------------------------------------------
-#  Copyright (c) 2015, ROOT Team.
 #  Authors: Danilo Piparo
 #           Omar Zapata <Omar.Zapata@cern.ch> http://oproject.org
-#  Distributed under the terms of the Modified LGPLv3 License.
-#
-#  The full license is in the file COPYING.rst, distributed with this software.
 #-----------------------------------------------------------------------------
 from threading import Thread
 from time import sleep as timeSleep
