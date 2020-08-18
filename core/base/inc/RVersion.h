19278fc088f382110697a83a1e223427c00ef586
Axel Naumann <Axel.Naumann@cern.ch>
2020-08-18 17:09:26 +0200

    "Update ROOT version files to v6.20/09."

--- core/base/inc/RVersion.h
+++ core/base/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.20/08"
+#define ROOT_RELEASE "6.20/09"
 #define ROOT_RELEASE_DATE "Aug 18 2020"
-#define ROOT_RELEASE_TIME "16:13:04"
+#define ROOT_RELEASE_TIME "17:09:24"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,20,8) /* 398344 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,20,9) /* 398345 */
 
 #endif
