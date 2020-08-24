0c6081418c3c04375ac52c8f7fa9c75cb0e5a78a
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-08-24 11:32:23 +0200

    [Tutorial] Work on FitHistoInFile.C tutorial.
    - Add plotting of signal function.
    - Enumerate parameters to facilitate bookkeeping.
    - Add possibility to name parameters.

--- tutorials/fit/FitHistoInFile.C
+++ tutorials/fit/FitHistoInFile.C
@@ -19,26 +19,35 @@
 #include "TFile.h"
 #include "TStyle.h"
 
-// How many parameters does the background model have?
-// Update this number if you change the background function
-constexpr int nbgdpar=3;
+// Function parameters are passed as an array to TF1. Here, we
+// define the position of each parameter in this array.
+// Note: N_PAR will give us the total number of parameters. Make
+// sure it is always the last entry!
+enum ParIndex_t {
+   Bkg0=0, Bkg1=1, Bkg2,
+   SigScale, SigSigma, SigMean,
+   N_PAR};
+// Use this map to (re-)name parameters for the plot
+const std::map<ParIndex_t,std::string> parNames{
+   {Bkg0, "Bkg0"}, {Bkg1, "Bkg1"}, {Bkg2, "Bkg2"},
+   {SigScale, "Gauss scale"}, {SigSigma, "Gauss #sigma"}, {SigMean, "Gauss #mu"}
+};
+
 
 // Quadratic background function
 Double_t background(Double_t *x, Double_t *par) {
-   return par[0] + par[1]*x[0] + par[2]*x[0]*x[0];
+   return par[Bkg0] + par[Bkg1]*x[0] + par[Bkg2]*x[0]*x[0];
 }
 
 // Gauss Peak function
 Double_t signal(Double_t *x, Double_t *par) {
-   // par[0] scale, par[1] sigma, par[2] peakposition
-   return par[0]*TMath::Gaus(x[0], par[2], par[1], true);
+   return par[SigScale]*TMath::Gaus(x[0], par[SigMean], par[SigSigma], true);
 }
 
 // Sum of background and peak function. We pass x and the fit parameters
-// down to the signal and background functions. Note that `nbgdpar` needs
-// to be correct for this to work.
+// down to the signal and background functions.
 Double_t fitFunction(Double_t *x, Double_t *par) {
-  return background(x,par) + signal(x,&par[nbgdpar]);
+   return background(x, par) + signal(x, par);
 }
 
 // Fit "fitFunction" to the histogram, and draw results on the canvas `c1`.
@@ -50,6 +59,11 @@ void FitRoutine(TCanvas* c1, TH1* histo, float fitxmin, float fitxmax, TString f
    fitFcn.SetLineWidth(2);
    fitFcn.SetLineColor(kBlue);
 
+   // Assign the names from the map "parNames". Optional, but makes a nicer plot.
+   for (auto& idx_name : parNames) {
+     fitFcn.SetParName(idx_name.first, idx_name.second.c_str());
+   }
+
    // Fit. First set ok-ish starting values for the parameters
    fitFcn.SetParameters(30,0,0,50.,0.1,1.);
    histo->GetXaxis()->SetRange(2,40);
@@ -57,24 +71,26 @@ void FitRoutine(TCanvas* c1, TH1* histo, float fitxmin, float fitxmax, TString f
 
    // improve the picture:
    // Draw signal and background functions separately
-   TF1 backFcn("backFcn",background,fitxmin,fitxmax,3);
+   TF1 backFcn("backFcn",background,fitxmin,fitxmax,N_PAR);
    backFcn.SetLineColor(kRed);
-   TF1 signalFcn("signalFcn",signal,fitxmin,fitxmax,3);
+   TF1 signalFcn("signalFcn",signal,fitxmin,fitxmax,N_PAR);
    signalFcn.SetLineColor(kBlue);
    signalFcn.SetNpx(500);
 
    // Retrieve fit parameters, and copy them to the signal and background functions
-   Double_t par[6];
+   Double_t par[N_PAR];
    fitFcn.GetParameters(par);
 
    backFcn.SetParameters(par);
    backFcn.DrawCopy("same");
 
-   signalFcn.SetParameters(&par[nbgdpar]));
+   signalFcn.SetParameters(par);
+   signalFcn.SetLineColor(kGreen);
+   signalFcn.DrawCopy("same");
 
-   const double binwidth = (histo->GetXaxis()->GetXmax()-histo->GetXaxis()->GetXmin())/histo->GetNbinsX();
+   const double binwidth = histo->GetXaxis()->GetBinWidth(1);
    const double integral = signalFcn.Integral(0.,3.);
-   cout << "number of signal events = " << integral/binwidth << endl;
+   cout << "number of signal events = " << integral/binwidth << " " << binwidth<< endl;
 
    // draw the legend
    TLegend legend(0.15,0.7,0.28,0.85);
@@ -82,6 +98,7 @@ void FitRoutine(TCanvas* c1, TH1* histo, float fitxmin, float fitxmax, TString f
    legend.SetTextSize(0.03);
    legend.AddEntry(histo,"Data","lpe");
    legend.AddEntry(&backFcn,"Bgd","l");
+   legend.AddEntry(&signalFcn,"Sig","l");
    legend.AddEntry(&fitFcn,"Sig+Bgd","l");
    legend.DrawClone();
    histo->Draw("esame");
@@ -100,7 +117,7 @@ void CreateRootFile(){
                            26,39,29,35,32,21,21,15,25,15};
 
    TFile* f = new TFile("exampleRootFile.root","RECREATE");
-   TH1D *histo = new TH1D("histo", "Gauss Peak on Quadratic Background",60,0,3);
+   TH1D *histo = new TH1D("histo", "Gauss Peak on Quadratic Background;x;Events/0.05",60,0,3);
    for(int i=0; i < nBins;  i++) histo->SetBinContent(i+1,data[i]);
    f->Write();
    f->Close();
