90973e11e1daff9e96547f6d67c7b60431033e1a
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-08-24 14:11:49 +0200

    [RF] Simplify includes and printing of RooAddPdf.

--- roofit/roofitcore/src/RooAddPdf.cxx
+++ roofit/roofitcore/src/RooAddPdf.cxx
@@ -63,34 +63,24 @@ An (enforced) condition for this assumption is that each \f$ \mathrm{PDF}_i \f$ 
 
 */
 
-
-#include "RooFit.h"
-#include "RooMsgService.h"
-
-#include "TIterator.h"
-#include "TList.h"
 #include "RooAddPdf.h"
+
 #include "RooDataSet.h"
 #include "RooRealProxy.h"
-#include "RooPlot.h"
 #include "RooRealVar.h"
 #include "RooAddGenContext.h"
 #include "RooRealConstant.h"
-#include "RooNameReg.h"
 #include "RooRecursiveFraction.h"
 #include "RooGlobalFunc.h"
 #include "RooRealIntegral.h"
-#include "RooTrace.h"
 #include "RooNaNPacker.h"
 
-#include "Riostream.h"
 #include <algorithm>
-
+#include <sstream>
 
 using namespace std;
 
 ClassImp(RooAddPdf);
-;
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -726,14 +716,15 @@ void RooAddPdf::updateCoefficients(CacheElem& cache, const RooArgSet* nset) cons
       if (coefDegen > 1.E-5) {
         myCoefCache[_coefList.getSize()] = RooNaNPacker::packFloatIntoNaN(100.f*coefDegen);
 
+        std::stringstream msg;
         if (_coefErrCount-->0) {
-          coutW(Eval) << "RooAddPdf::updateCoefCache(" << GetName()
-		            << " WARNING: sum of PDF coefficients not in range [0-1], value="
-		            << 1-lastCoef ;
+          msg << "RooAddPdf::updateCoefCache(" << GetName()
+              << " WARNING: sum of PDF coefficients not in range [0-1], value="
+		      << 1-lastCoef ;
           if (_coefErrCount==0) {
-            coutW(Eval) << " (no more will be printed)"  ;
+            msg << " (no more will be printed)"  ;
           }
-          coutW(Eval) << endl ;
+          coutW(Eval) << msg.str() << std::endl;
         }
       } 
     }
