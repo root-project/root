c908aaabb18e282598c2f5b285b6006e010f611e
Philippe Canal <pcanal@fnal.gov>
2020-08-25 12:38:11 -0500

    test for ROOT-11013

---
+++ root/io/rootcint/weirdpair_LinkDef.h
@@ -0,0 +1,9 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class std::vector <std::unique_ptr<std::pair<void (*)(void *, void *), void * > > >+;
+#pragma link C++ class std::pair<int>+; // Intentionally broken spelling.
+#endif
