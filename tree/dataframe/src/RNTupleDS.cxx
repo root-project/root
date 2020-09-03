969daac76270bc504ae066821f5cff7d9a698b2c
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-09-03 18:52:31 +0200

    [RNTupleDS] Use lazy RNTupleColumnReaders

--- tree/dataframe/src/RNTupleDS.cxx
+++ tree/dataframe/src/RNTupleDS.cxx
@@ -1,18 +1,20 @@
 /// \file RNTupleDS.cxx
 /// \ingroup NTuple ROOT7
 /// \author Jakob Blomer <jblomer@cern.ch>
+/// \author Enrico Guiraud <enrico.guiraud@cern.ch>
 /// \date 2018-10-04
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
 /// is welcome!
 
 /*************************************************************************
- * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
+#include <ROOT/RDF/RColumnReaderBase.hxx>
 #include <ROOT/RField.hxx>
 #include <ROOT/RFieldValue.hxx>
 #include <ROOT/RNTupleDescriptor.hxx>
@@ -30,6 +32,43 @@
 
 namespace ROOT {
 namespace Experimental {
+namespace Detail {
+class RNTupleColumnReader final : public ROOT::Detail::RDF::RColumnReaderBase {
+   using RFieldBase = ROOT::Experimental::Detail::RFieldBase;
+   using RFieldValue = ROOT::Experimental::Detail::RFieldValue;
+   using RPageSource = ROOT::Experimental::Detail::RPageSource;
+
+   std::unique_ptr<RFieldBase> fField;
+   RFieldValue fValue;
+   Long64_t fLastEntry; ///< Last entry number that was read
+
+   std::unique_ptr<RFieldBase> MakeField(const std::string &colName, RPageSource &source)
+   {
+      const auto &descriptor = source.GetDescriptor();
+      const auto fieldId = descriptor.FindFieldId(colName);
+      const auto &fieldDescriptor = descriptor.GetFieldDescriptor(fieldId);
+      const auto typeName = fieldDescriptor.GetTypeName();
+      auto fieldBasePtr = Detail::RFieldBase::Create(fieldDescriptor.GetFieldName(), typeName);
+      Detail::RFieldFuse::ConnectRecursively(fieldId, source, *fieldBasePtr);
+      return std::unique_ptr<RFieldBase>(fieldBasePtr);
+   }
+
+public:
+   RNTupleColumnReader(const std::string &colName, RPageSource &source)
+      : fField(MakeField(colName, source)), fValue(fField->GenerateValue()), fLastEntry(-1)
+   {
+   }
+
+   void *GetImpl(Long64_t entry) final
+   {
+      if (entry != fLastEntry) {
+         fField->Read(entry, &fValue);
+         fLastEntry = entry;
+      }
+      return fValue.GetRawPtr();
+   }
+};
+} // namespace Detail
 
 void RNTupleDS::AddFields(const RNTupleDescriptor &desc, DescriptorId_t parentId)
 {
@@ -52,39 +91,20 @@ RNTupleDS::RNTupleDS(std::unique_ptr<Detail::RPageSource> pageSource)
    fSources.emplace_back(std::move(pageSource));
 }
 
-
-RDF::RDataSource::Record_t RNTupleDS::GetColumnReadersImpl(std::string_view name, const std::type_info& /* ti */)
+RDF::RDataSource::Record_t RNTupleDS::GetColumnReadersImpl(std::string_view /* name */, const std::type_info & /* ti */)
 {
-   const auto colIdx = std::distance(
-      fColumnNames.begin(), std::find(fColumnNames.begin(), fColumnNames.end(), name));
-   // TODO(jblomer): check expected type info like in, e.g., RRootDS.cxx
-
-   std::vector<void*> ptrs;
-   for (unsigned int slot = 0; slot < fNSlots; ++slot) {
-      if (!fValuePtrs[slot][colIdx]) {
-         const auto &descriptor = fSources[slot]->GetDescriptor();
-         auto colName = fColumnNames[colIdx];
-         auto typeName = fColumnTypes[colIdx];
-         auto fieldId = descriptor.FindFieldId(colName);
-         fFields[slot][colIdx] = std::unique_ptr<Detail::RFieldBase>(
-            Detail::RFieldBase::Create(descriptor.GetFieldDescriptor(fieldId).GetFieldName(), typeName));
-         Detail::RFieldFuse::ConnectRecursively(fieldId, *fSources[slot], *fFields[slot][colIdx]);
-         fValues[slot][colIdx] = fFields[slot][colIdx]->GenerateValue();
-         fValuePtrs[slot][colIdx] = fValues[slot][colIdx].GetRawPtr();
-         if (slot == 0)
-            fActiveColumns.emplace_back(colIdx);
-      }
-      ptrs.push_back(&fValuePtrs[slot][colIdx]);
-   }
-
-   return ptrs;
+   // This datasource uses the GetColumnReaders2 API instead (better name in the works)
+   return {};
 }
 
-bool RNTupleDS::SetEntry(unsigned int slot, ULong64_t entryIndex)
+std::unique_ptr<ROOT::Detail::RDF::RColumnReaderBase>
+RNTupleDS::GetColumnReaders(unsigned int slot, std::string_view name, const std::type_info & /*tid*/)
 {
-   for (auto colIdx : fActiveColumns) {
-      fFields[slot][colIdx]->Read(entryIndex, &fValues[slot][colIdx]);
-   }
+   return std::make_unique<ROOT::Experimental::Detail::RNTupleColumnReader>(std::string(name), *fSources[slot]);
+}
+
+bool RNTupleDS::SetEntry(unsigned int, ULong64_t)
+{
    return true;
 }
 
@@ -143,20 +163,10 @@ void RNTupleDS::SetNSlots(unsigned int nSlots)
    R__ASSERT(nSlots > 0);
    fNSlots = nSlots;
 
-   fFields.resize(fNSlots);
-   fValues.resize(fNSlots);
-   fValuePtrs.resize(fNSlots);
    for (unsigned int i = 1; i < fNSlots; ++i) {
       fSources.emplace_back(fSources[0]->Clone());
       R__ASSERT(i == (fSources.size() - 1));
       fSources[i]->Attach();
-   }
-
-   auto nColumns = fColumnNames.size();
-   for (unsigned int i = 0; i < fNSlots; ++i) {
-      fFields[i].resize(nColumns);
-      fValues[i].resize(nColumns);
-      fValuePtrs[i].resize(nColumns, nullptr);
    }
 }
 } // ns Experimental
