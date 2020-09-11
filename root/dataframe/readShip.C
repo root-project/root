1d39c39fd612a36ecfd98ba7cd9f304aa03be4b4
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-09-11 14:32:24 +0200

    [DF] Fix test for ROOT-9674
    Column type is RVec<int>, not int, and RDF gets it wrong.
    https://github.com/root-project/root/pull/6258 fixes the type inference
    problem (and therefore this test) for master.

--- root/dataframe/readShip.C
+++ root/dataframe/readShip.C
@@ -3,8 +3,8 @@ int readShip()
 {
    ROOT::RDataFrame f("cbmsim","ship_ROOT_9674.root");
    auto name = f.GetColumnType("MCTrack.fPdgCode");
-   if(name != "Int_t") {
-      std::cerr << "ERROR: expecting the type name Int_t for column MCTrack.fPdgCode and got '" << name << "' instead!" << std::endl;
+   if(name != "ROOT::VecOps::RVec<Int_t>") {
+      std::cerr << "ERROR: expecting the type name ROOT::VecOps::RVec<Int_t> for column MCTrack.fPdgCode and got '" << name << "' instead!" << std::endl;
       return 1;
    }
    return 0;
