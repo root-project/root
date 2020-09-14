abc8a418d740d1604f0c4d9a8c0b4e8b5ad7b3cc
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-09-14 18:33:57 +0200

    [RF] Add stub to dynamically load architecture-specific libraries.
    Extend RooFitCore's static initialisation to support the loading of
    specialised RooFit libraries based on the CPU capabilities returned by
    gcc's/clangs __builtin_cpu_supports().

--- roofit/roofitcore/src/Initialisation.cxx
+++ roofit/roofitcore/src/Initialisation.cxx
@@ -1,7 +1,11 @@
 #include "RConfigure.h"
 #include "TEnv.h"
+#include "TSystem.h"
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <exception>
 
 /**
 \file Initialisation.cxx
@@ -32,10 +36,60 @@ void doBanner() {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Inspect cpu capabilities, and load architecture-specific libraries for RooFitCore/RooFit computations.
+void loadComputeLibrary() {
+  std::vector<std::string> libNames;
+//  libNames.push_back("libRooFitCoreCompute");
+//  libNames.push_back("libRooFitCompute");
+#ifdef R__HAS_MATHMORE
+//  libNames.push_back("libRooFitMoreCompute");
+#endif
+  {
+    // Try to load HistFactory compute library as well. Need to check first if it exists.
+    TString libName("libHistFactory");
+//    if (gSystem->FindDynamicLibrary(libName, true) != nullptr)
+//      libNames.push_back("libHistFactoryCompute");
+  }
+
+  std::string libSuffix;
+#if defined(R__RF_ARCHITECTURE_SPECIFIC_LIBS) && (defined(__GNUC__) || defined(__clang__))
+  if (__builtin_cpu_supports("avx2")) {
+    libSuffix = "_AVX2";
+  } else if (__builtin_cpu_supports("avx")) {
+    libSuffix = "_AVX";
+  } else if (__builtin_cpu_supports("sse4.1")) {
+    libSuffix = "_SSE4.1";
+  }
+
+#if __GNUC__ > 5 || defined(__clang__)
+  if (__builtin_cpu_supports("avx512f")) {
+    libSuffix = "_AVX512f";
+  }
+#endif
+
+#endif
+
+  for (auto&& libName : libNames) {
+    libName += libSuffix;
+    const auto returnValue = gSystem->Load(libName.c_str());
+
+    if (returnValue == -1 || returnValue == -2) {
+      throw std::runtime_error("RooFit was unable to load its computation library " + libName);
+    }
+    // Library should not have been loaded before we tried to do it.
+    if (returnValue == 1) {
+      throw std::logic_error("RooFit computation library " + libName + " was loaded before RooFit initialisation began.");
+    }
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// A RAII that performs RooFit's static initialisation.
 static struct RooFitInitialiser {
   RooFitInitialiser() {
     doBanner();
+    loadComputeLibrary();
   }
 } __rooFitInitialiser;
 
