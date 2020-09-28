2b86d01c4cd0afcb5c2d70df8b62204eecfe0be5
Axel Naumann <Axel.Naumann@cern.ch>
2020-09-28 14:06:07 +0200

    Silence warning ("unused parameter").

--- root/meta/genreflex/noStreamer_noInputOperator/foo_custom_input_operator.h
+++ root/meta/genreflex/noStreamer_noInputOperator/foo_custom_input_operator.h
@@ -10,5 +10,5 @@ private:
   ClassDefNV(Foo2, 1);
 };
 
-TBuffer &operator<<(TBuffer &buffer, const Foo2 *foo) { return buffer; }
+TBuffer &operator<<(TBuffer &buffer, const Foo2 * /*foo*/) { return buffer; }
 TBuffer &operator>>(TBuffer &buffer, Foo2 *&) { return buffer; }
