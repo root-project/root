1d98c336617bd3e21c7d1265fb6cee41f7727ed5
Axel Naumann <Axel.Naumann@cern.ch>
2020-09-28 14:04:02 +0200

    Remove anachronism.

--- root/meta/genreflex/ROOT-5696/warnMe.h
+++ root/meta/genreflex/ROOT-5696/warnMe.h
@@ -1,17 +1,7 @@
-
-// to be probably replaced by #if __cplusplus >= 201103L
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-  #define CONSTEXPR constexpr 
-#else
-  #define CONSTEXPR const 
-#endif
-
-
 class A{
 
 public:
 
-   CONSTEXPR float _growth_factor() { return 1.5; } //from boost
+   constexpr float _growth_factor() { return 1.5; } //from boost
    int returnInt (int i) { int a=12; if (a=12) return a*i; else return 0; }
 };
-
