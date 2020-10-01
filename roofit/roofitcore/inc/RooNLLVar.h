26dc1da5e5143f5bcb4780630c5ae40a611514e1
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-10-01 18:36:19 +0200

    [RF] Add code to activate new batch interface in RooNLLVar.

--- roofit/roofitcore/inc/RooNLLVar.h
+++ roofit/roofitcore/inc/RooNLLVar.h
@@ -23,12 +23,15 @@
 #include <utility>
 
 class RooRealSumPdf ;
+namespace BatchHelpers {
+struct RunContext;
+}
 
 class RooNLLVar : public RooAbsOptTestStatistic {
 public:
 
   // Constructors, assignment etc
-  RooNLLVar() { _first = kTRUE ; }
+  RooNLLVar();
   RooNLLVar(const char *name, const char* title, RooAbsPdf& pdf, RooAbsData& data,
 	    const RooCmdArg& arg1=RooCmdArg::none(), const RooCmdArg& arg2=RooCmdArg::none(),const RooCmdArg& arg3=RooCmdArg::none(),
 	    const RooCmdArg& arg4=RooCmdArg::none(), const RooCmdArg& arg5=RooCmdArg::none(),const RooCmdArg& arg6=RooCmdArg::none(),
@@ -75,15 +78,16 @@ private:
   std::tuple<double, double, double> computeScalar(
         std::size_t stepSize, std::size_t firstEvent, std::size_t lastEvent) const;
 
-  Bool_t _extended ;
+  Bool_t _extended{false};
   bool _batchEvaluations{false};
-  Bool_t _weightSq ; // Apply weights squared?
-  mutable Bool_t _first ; //!
-  Double_t _offsetSaveW2; //!
-  Double_t _offsetCarrySaveW2; //!
+  Bool_t _weightSq{false}; // Apply weights squared?
+  mutable Bool_t _first{true}; //!
+  Double_t _offsetSaveW2{false}; //!
+  Double_t _offsetCarrySaveW2{false}; //!
 
   mutable std::vector<Double_t> _binw ; //!
-  mutable RooRealSumPdf* _binnedPdf ; //!
+  mutable RooRealSumPdf* _binnedPdf{nullptr}; //!
+  mutable std::unique_ptr<BatchHelpers::RunContext> _evalData; //! Struct to store function evaluation workspaces.
    
   ClassDef(RooNLLVar,3) // Function representing (extended) -log(L) of p.d.f and dataset
 };
