70c6b138a4d83a08d8a35995a3799471c31f05cd
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2020-10-02 19:38:34 +0200

    Add support for tf.keras, keras version relased with TensorFlow 2
    The support is provided by adding an option in MethodPyKeras, tf.keras=1

--- tmva/pymva/inc/TMVA/MethodPyKeras.h
+++ tmva/pymva/inc/TMVA/MethodPyKeras.h
@@ -76,14 +76,17 @@ namespace TMVA {
       /// Get the Keras backend (can be: TensorFlow, Theano or CNTK)
       EBackendType GetKerasBackend();
       TString GetKerasBackendName();
+      // flag to indicate we are using the Keras shipped with Tensorflow 2
+      Bool_t UseTFKeras() const { return fUseTFKeras; }
 
-    private:
+   private:
 
       TString fFilenameModel; // Filename of the previously exported Keras model
       UInt_t fBatchSize {0}; // Training batch size
       UInt_t fNumEpochs {0}; // Number of training epochs
       Int_t fNumThreads {0}; // Number of CPU threads (if 0 uses default values)
       Int_t fVerbose; // Keras verbosity during training
+      Bool_t fUseTFKeras;   // use Keras from Tensorflow
       Bool_t fContinueTraining; // Load weights from previous training
       Bool_t fSaveBestOnly; // Store only weights with smallest validation loss
       Int_t fTriesEarlyStopping; // Stop training if validation loss is not decreasing for several epochs
@@ -92,6 +95,7 @@ namespace TMVA {
       TString fNumValidationString;  // option string defining the number of validation events
       TString fGpuOptions;    // GPU options (for Tensorflow to set in session_config.gpu_options)
       TString fUserCodeName; // filename of an optional user script that will be executed before loading the Keras model
+      TString fKerasString;  // string identifying keras or tf.keras
 
       bool fModelIsSetup = false; // flag whether model is loaded, needed for getMvaValue during evaluation
       float* fVals = nullptr; // variables array used for GetMvaValue
