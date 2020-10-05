5d5711b97a9f447cb53eda129ac2cab1078817e6
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-10-05 12:21:39 +0200

    [DF] Refactor dataframe_regression test suite
    Use gtest fixture rather than preprocessor tricks to execute the same
    tests with and without implicit multi-threading activated.

--- tree/dataframe/test/dataframe_regression.cxx
+++ tree/dataframe/test/dataframe_regression.cxx
@@ -1,16 +1,1 @@
-#define TEST_CATEGORY RDataFrameSeq
 #include "dataframe_regression_tests.hxx"
-
-#ifdef R__USE_IMT
-
-#include "TROOT.h"
-TEST(dataframe_regression, EnableImplicitMT)
-{
-   ROOT::EnableImplicitMT(4);
-}
-
-#undef TEST_CATEGORY
-#define TEST_CATEGORY RDataFrameMT
-#include "dataframe_regression_tests.hxx"
-
-#endif
