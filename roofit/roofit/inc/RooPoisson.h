e8fef357ee58e2462e80492573a898c7209087bd
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-10-06 12:35:25 +0200

    [RF] Activate protection against negative means in RooPoisson.

--- roofit/roofit/inc/RooPoisson.h
+++ roofit/roofit/inc/RooPoisson.h
@@ -30,7 +30,9 @@ public:
   Int_t getGenerator(const RooArgSet& directVars, RooArgSet &generateVars, Bool_t staticInitOK=kTRUE) const override;
   void generateEvent(Int_t code) override;
   
+  /// Switch off/on rounding of `x` to the nearest integer.
   void setNoRounding(bool flag = kTRUE) {_noRounding = flag;}
+  /// Switch on or off protection against negative means.
   void protectNegativeMean(bool flag = kTRUE) {_protectNegative = flag;}
 
 protected:
@@ -38,7 +40,7 @@ protected:
   RooRealProxy x ;
   RooRealProxy mean ;
   Bool_t  _noRounding ;
-  Bool_t  _protectNegative ;
+  Bool_t  _protectNegative{true};
   
   Double_t evaluate() const override;
   RooSpan<double> evaluateBatch(std::size_t begin, std::size_t batchSize) const override;
