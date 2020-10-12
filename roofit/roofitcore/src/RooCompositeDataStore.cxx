9f7923f8eefe70ef0c779db090a38d84a55f91f5
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-10-12 15:10:38 +0200

    [RF] Make RooAbsData::getWeightBatch pure virtual.
    RooSpan has a special mode of operation where it owns the data it's
    pointing to. This was a workaround to support pointing to data when
    objects don't own a slab of memory that spans could point to.
    By implementing getWeightBatch in derived classes, and by adding a
    member that owns some memory, the workaround can be removed.

--- roofit/roofitcore/src/RooCompositeDataStore.cxx
+++ roofit/roofitcore/src/RooCompositeDataStore.cxx
@@ -511,3 +511,19 @@ void RooCompositeDataStore::dump()
 }
 
 
+////////////////////////////////////////////////////////////////////////////////
+/// Get the weights of the events in the range [first, first+len).
+/// This implementation will fill a vector with every event retrieved one by one
+/// (even if the weight is constant). Then, it returns a span.
+RooSpan<const double> RooCompositeDataStore::getWeightBatch(std::size_t first, std::size_t len) const {
+  if (!_weightBuffer) {
+    _weightBuffer.reset(new std::vector<double>());
+    _weightBuffer->reserve(len);
+
+    for (std::size_t i = 0; i < static_cast<std::size_t>(numEntries()); ++i) {
+      _weightBuffer->push_back(weight(i));
+    }
+  }
+
+  return {_weightBuffer->data() + first, len};
+}
