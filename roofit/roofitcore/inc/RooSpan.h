d8ade33a735ffd11963896892ae900d985942b08
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-10-12 15:16:56 +0200

    [RF] Remove possibility that RooSpan owns memory.
    With getValBatch(RunContext&), RooSpan doesn't need to own memory, any
    more. This makes almost all of the span's functions constexpr, and
    allows for better inlining.

--- roofit/roofitcore/inc/RooSpan.h
+++ roofit/roofitcore/inc/RooSpan.h
@@ -37,22 +37,16 @@ public:
   using value_type = typename std::remove_cv<T>::type;
 
   constexpr RooSpan() :
-  _auxStorage{},
   _span{} { }
 
   constexpr RooSpan(RooSpan&& other) :
-  _auxStorage{std::move(other._auxStorage)},
   _span{other._span.data(), other._span.size()}
   { }
 
   constexpr RooSpan(const RooSpan& other) :
-  _auxStorage{other._auxStorage},
   _span{other._span}
   { }
 
-  // Declare the const version as friend here, so one can
-  // construct RooSpan<const T> from RooSpan<const T> AND share the memory.
-  friend class RooSpan<const T>;
 
   /// Conversion constructor from <T> to <const T>
   /// If the input span owns some memory, the const-version of the
@@ -60,7 +54,6 @@ public:
   template<typename NON_CONST_T,
       typename = typename std::enable_if<std::is_same<const NON_CONST_T, T>::value>::type >
   constexpr RooSpan(const RooSpan<NON_CONST_T>& other) :
-  _auxStorage{other._auxStorage},
   _span{other.data(), other.size()}
   { }
 
@@ -68,7 +61,6 @@ public:
   /// Construct from a range. Data held by foreign object.
   template < class InputIterator>
   constexpr RooSpan(InputIterator beginIn, InputIterator endIn) :
-  _auxStorage{},
   _span{beginIn, endIn}
   { }
 
@@ -76,7 +68,6 @@ public:
   /// Construct from start and end pointers.
   constexpr RooSpan(typename std::span<T>::pointer beginIn,
       typename std::span<T>::pointer endIn) :
-    _auxStorage{},
     _span{beginIn, endIn}
   { }
 
@@ -84,29 +75,21 @@ public:
   /// Construct from start pointer and size.
   constexpr RooSpan(typename std::span<T>::pointer beginIn,
       typename std::span<T>::index_type sizeIn) :
-  _auxStorage{},
   _span{beginIn, sizeIn}
   { }
 
 
   constexpr RooSpan(const std::vector<typename std::remove_cv<T>::type>& vec) noexcept :
-  _auxStorage{},
   _span{vec}
   { }
 
   constexpr RooSpan(std::vector<typename std::remove_cv<T>::type>& vec) noexcept :
-  _auxStorage{},
   _span{vec}
   { }
 
 
-  /// Hand data over to this span. This will mean that the data will get
-  /// deleted when it goes out of scope. Try to avoid this because
-  /// unnecessary copies will be made.
-  constexpr RooSpan(std::vector<value_type>&& payload) :
-  _auxStorage{new std::vector<value_type>(std::forward<std::vector<value_type>>(payload))},
-  _span{_auxStorage->begin(), _auxStorage->end()}
-  { }
+  /// We cannot point to temporary data.
+  constexpr RooSpan(std::vector<value_type>&& payload) = delete;
 
 
   RooSpan<T>& operator=(const RooSpan<T>& other) = default;
@@ -124,10 +107,16 @@ public:
     return _span.data();
   }
 
+#ifdef NDEBUG
+  constexpr typename std::span<T>::reference operator[](typename std::span<T>::index_type i) const noexcept {
+    return _span[i];
+  }
+#else
   typename std::span<T>::reference operator[](typename std::span<T>::index_type i) const noexcept {
     assert(i < _span.size());
     return _span[i];
   }
+#endif
 
   constexpr typename std::span<T>::index_type size() const noexcept {
     return _span.size();
@@ -144,23 +133,19 @@ public:
 
   ///Test if the span overlaps with `other`.
   template <class Span_t>
-  bool overlaps(const Span_t& other) const {
+  constexpr bool overlaps(const Span_t& other) const {
     return insideSpan(other.begin()) || insideSpan(other.end()-1)
         || other.insideSpan(begin()) || other.insideSpan(end()-1);
   }
 
   ///Test if the given pointer/iterator is inside the span.
   template <typename ptr_t>
-  bool insideSpan(ptr_t ptr) const {
+  constexpr bool insideSpan(ptr_t ptr) const {
     return begin() <= ptr && ptr < end();
   }
 
 private:
 
-  /// If a class does not own a contiguous block of memory, which
-  /// could be used to create a span, the memory has to be kept alive
-  /// until all referring spans are destroyed.
-  std::shared_ptr<std::vector<value_type>> _auxStorage;
   std::span<T> _span;
 };
 
