c4c680838781bab4cd9324f99eebb8d2ce029bff
Sergey Linev <S.Linev@gsi.de>
2020-10-19 12:53:01 +0200

    Use json_fwd.hpp in REveElement.hxx
    It is forward declaration of classes from nlohmann/json.hpp
    Before was not a part of official release

--- graf3d/eve7/inc/ROOT/REveElement.hxx
+++ graf3d/eve7/inc/ROOT/REveElement.hxx
@@ -20,45 +20,7 @@
 
 class TGeoMatrix;
 
-/// use temporary solution for forwarding of nlohmann::json
-/// after version of 3.1.0 it is included in official releases
-/// see https://github.com/nlohmann/json/issues/314
-
-#ifndef INCLUDE_NLOHMANN_JSON_FWD_HPP_
-#define INCLUDE_NLOHMANN_JSON_FWD_HPP_
-
-#include <cstdint> // int64_t, uint64_t
-#include <map> // map
-#include <memory> // allocator
-#include <string> // string
-#include <vector> // vector
-
-namespace nlohmann
-{
-
-// see json_fwd.hpp
-template<typename T = void, typename SFINAE = void>
-struct adl_serializer;
-
-template<template<typename U, typename V, typename... Args> class ObjectType =
-         std::map,
-         template<typename U, typename... Args> class ArrayType = std::vector,
-         class StringType = std::string, class BooleanType = bool,
-         class NumberIntegerType = std::int64_t,
-         class NumberUnsignedType = std::uint64_t,
-         class NumberFloatType = double,
-         template<typename U> class AllocatorType = std::allocator,
-         template<typename T, typename SFINAE = void> class JSONSerializer =
-         adl_serializer>
-class basic_json;
-
-template<typename BasicJsonType>
-class json_pointer;
-
-using json = basic_json<>;
-}  // namespace nlohmann
-
-#endif
+#include "json_fwd.hpp"
 
 
 namespace ROOT {
