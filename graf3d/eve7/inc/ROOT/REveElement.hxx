59f21e93c42ba07a3ff5159c41253e507442e602
Sergey Linev <S.Linev@gsi.de>
2020-10-19 13:27:05 +0200

    Update nlohmann/json.hpp forward declarations in eve7

--- graf3d/eve7/inc/ROOT/REveElement.hxx
+++ graf3d/eve7/inc/ROOT/REveElement.hxx
@@ -33,10 +33,20 @@ class TGeoMatrix;
 #include <string> // string
 #include <vector> // vector
 
+/*!
+@brief namespace for Niels Lohmann
+@see https://github.com/nlohmann
+@since version 1.0.0
+*/
 namespace nlohmann
 {
+/*!
+@brief default JSONSerializer template argument
 
-// see json_fwd.hpp
+This serializer ignores the template arguments and uses ADL
+([argument-dependent lookup](https://en.cppreference.com/w/cpp/language/adl))
+for serialization.
+*/
 template<typename T = void, typename SFINAE = void>
 struct adl_serializer;
 
@@ -49,16 +59,49 @@ template<template<typename U, typename V, typename... Args> class ObjectType =
          class NumberFloatType = double,
          template<typename U> class AllocatorType = std::allocator,
          template<typename T, typename SFINAE = void> class JSONSerializer =
-         adl_serializer>
+         adl_serializer,
+         class BinaryType = std::vector<std::uint8_t>>
 class basic_json;
 
+/*!
+@brief JSON Pointer
+
+A JSON pointer defines a string syntax for identifying a specific value
+within a JSON document. It can be used with functions `at` and
+`operator[]`. Furthermore, JSON pointers are the base for JSON patches.
+
+@sa [RFC 6901](https://tools.ietf.org/html/rfc6901)
+
+@since version 2.0.0
+*/
 template<typename BasicJsonType>
 class json_pointer;
 
+/*!
+@brief default JSON class
+
+This type is the default specialization of the @ref basic_json class which
+uses the standard template types.
+
+@since version 1.0.0
+*/
 using json = basic_json<>;
+
+template<class Key, class T, class IgnoredLess, class Allocator>
+struct ordered_map;
+
+/*!
+@brief ordered JSON class
+
+This type preserves the insertion order of object keys.
+
+@since version 3.9.0
+*/
+using ordered_json = basic_json<nlohmann::ordered_map>;
+
 }  // namespace nlohmann
 
-#endif
+#endif  // INCLUDE_NLOHMANN_JSON_FWD_HPP_
 
 
 namespace ROOT {
