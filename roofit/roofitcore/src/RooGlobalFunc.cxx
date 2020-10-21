75e766f15075a077ba4edc8677589b6b756b6b4c
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-10-21 17:56:49 +0200

    [RF] Add IntegrateBins() command argument.
    - Forward it to RooAbsOptTestStatistic from RooAbsPdf / RooNLLVar.

--- roofit/roofitcore/src/RooGlobalFunc.cxx
+++ roofit/roofitcore/src/RooGlobalFunc.cxx
@@ -157,7 +157,9 @@ namespace RooFit {
   RooCmdArg DataError(Int_t etype) { return RooCmdArg("DataError",(Int_t)etype,0,0,0,0,0,0,0) ; }
   RooCmdArg NumCPU(Int_t nCPU, Int_t interleave)   { return RooCmdArg("NumCPU",nCPU,interleave,0,0,0,0,0,0) ; }
   RooCmdArg BatchMode(bool flag) { return RooCmdArg("BatchMode", flag); }
-  
+  /// Integrate the PDF over bins. Improves accuracy for binned fits. Switch off using `0.` as argument. \see RooAbsPdf::fitTo().
+  RooCmdArg IntegrateBins(double precision) { return RooCmdArg("IntegrateBins", 0, 0, precision); }
+
   // RooAbsCollection::printLatex arguments
   RooCmdArg Columns(Int_t ncol)                           { return RooCmdArg("Columns",ncol,0,0,0,0,0,0,0) ; }
   RooCmdArg OutputFile(const char* fileName)              { return RooCmdArg("OutputFile",0,0,0,0,fileName,0,0,0) ; }
