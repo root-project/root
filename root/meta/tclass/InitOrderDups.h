84468361e56aeae81e5bd579b77bd7f0419b5177
Philippe Canal <pcanal@fnal.gov>
2020-10-22 14:51:46 -0500

    Add test for issue 6670 (broken TClass initialization order)
    This includes also:
      load, unload and reload of library
      switch TClass from kInterpreted to kHasTClassInit
      load duplicate dictionary of dict without pcm
    
    And also testing that #pragma ... typedef does not lead to the
    generation of a dictionary.

---
+++ root/meta/tclass/InitOrderDups.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Math/Point3D.h"
+#include "Math/Vector4D.h"
+#include "Math/SMatrix.h"
+
+typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >          Vector4D;
+typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<double> > Vector3D;
