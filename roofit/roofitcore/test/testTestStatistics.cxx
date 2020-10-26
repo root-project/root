b14ef224ef73865166f3642563a6cfb23f90836a
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2020-10-26 16:11:07 +0100

    [RF] Add test for IntegrateBins() in RooChi2Var.

--- roofit/roofitcore/test/testTestStatistics.cxx
+++ roofit/roofitcore/test/testTestStatistics.cxx
@@ -259,3 +259,51 @@ TEST(RooNLLVar, IntegrateBins_RooDataHist) {
 //  fit2->Print();
 }
 
+
+TEST(RooChi2Var, IntegrateBins) {
+  RooRandom::randomGenerator()->SetSeed(1337ul);
+
+  RooRealVar x("x", "x", 0.1, 5.1);
+  x.setBins(10);
+
+  RooRealVar a("a", "a", -0.3, -5., 5.);
+  RooArgSet targetValues;
+  RooArgSet(a).snapshot(targetValues);
+
+  RooGenericPdf pdf("pow", "std::pow(x, a)", RooArgSet(x, a));
+  std::unique_ptr<RooDataHist> dataH(pdf.generateBinned(x,  10000));
+
+  std::unique_ptr<RooPlot> frame( x.frame() );
+  dataH->plotOn(frame.get(), RooFit::MarkerColor(kRed));
+
+
+  a.setVal(3.);
+  std::unique_ptr<RooFitResult> fit1( pdf.chi2FitTo(*dataH, RooFit::Save(), RooFit::PrintLevel(-1)) );
+  pdf.plotOn(frame.get(), RooFit::LineColor(kRed), RooFit::Name("standard"));
+
+  a.setVal(3.);
+  std::unique_ptr<RooFitResult> fit2( pdf.chi2FitTo(*dataH, RooFit::Save(), RooFit::PrintLevel(-1),
+      RooFit::BatchMode(true),
+      RooFit::IntegrateBins(1.E-3)) );
+  pdf.plotOn(frame.get(), RooFit::LineColor(kBlue), RooFit::Name("highRes"));
+
+
+  auto getVal = [](const char* name, const RooArgSet& set) {
+    return dynamic_cast<const RooRealVar&>(set[name]).getVal();
+  };
+  auto getErr = [](const char* name, const RooArgSet& set) {
+    return dynamic_cast<const RooRealVar&>(set[name]).getError();
+  };
+
+  EXPECT_GT(fabs(getVal("a", targetValues) - getVal("a", fit1->floatParsFinal())), 1. * getErr("a", fit1->floatParsFinal()))
+      << "Expecting a bias when sampling PDF in bin centre.";
+
+  EXPECT_NEAR(getVal("a", targetValues), getVal("a", fit2->floatParsFinal()), 1. * getErr("a", fit2->floatParsFinal()))
+      << "Expect reduced bias with high-resolution sampling.";
+
+  EXPECT_GT(frame->chiSquare("standard", nullptr, 1) * 0.9, frame->chiSquare("highRes", nullptr, 1))
+      << "Expect chi2/ndf at least 10% better.";
+
+//  fit1->Print();
+//  fit2->Print();
+}
