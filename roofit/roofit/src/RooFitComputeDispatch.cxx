6f358905073704319546ba0c013a4bb795012603
Emmanouil <emmanouil.michalainas@cern.ch>
2020-10-26 15:59:08 +0100

    [RF] Added doxygen documentation for the RooFitCompute Library.

--- roofit/roofit/src/RooFitComputeDispatch.cxx
+++ roofit/roofit/src/RooFitComputeDispatch.cxx
@@ -1,3 +1,10 @@
 #include "RooFitComputeInterface.h"
 
+/**
+ * The dispatch pointer always points to the most recently created RooFitComputeClass object. Effectively, function calls through the object resolve to:
+ *  - a generic cpu architecture implementation if the platform cpu doesn't support vector instruction sets, or
+ *  - an optimized implementation that uses vector instructions, always compatible with the platform cpu.
+ * The pointer is of type RooFitComputeInterface*, so that calling functions through it are always virtual calls.
+ * \see RooFitComputeInterface, RooFitComputeClass, RF_ARCH
+ */
 RooFitCompute::RooFitComputeInterface* RooFitCompute::dispatch=nullptr;
