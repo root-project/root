cbb663416733f5bf86fc92a6bc3f748730becdb7
Sergey Linev <S.Linev@gsi.de>
2020-10-28 17:04:37 +0100

    [rcanvas] Fix openui5 resize glitch
    onAfterRendering method invoked before valid dimension of the HTML
    element is assigned. One should wait either resize event or timeout and
    check if valid size is there. Only then normal rendering can be started

--- ui5/canv/controller/CanvasPanel.controller.js
+++ ui5/canv/controller/CanvasPanel.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
    return Controller.extend("rootui5.canv.controller.CanvasPanel", {
 
       preserveCanvasContent: function () {
-         // workaround, openui5 does not preserve
+         // workaround, openui5 does not preserve DOM elements when calling onBeforeRendering
          let dom = this.getView().getDomRef();
          if (this.canvas_painter && dom && dom.children.length && !this._mainChild) {
             this._mainChild = dom.children[0];
@@ -17,6 +17,7 @@ sap.ui.define([
 
       onBeforeRendering: function() {
          this.preserveCanvasContent();
+         this._has_after_rendering = false;
       },
 
       setPainter: function(painter) {
@@ -28,70 +29,75 @@ sap.ui.define([
       },
 
       onAfterRendering: function() {
-         let dom = this.getView().getDomRef(), check_resize = false;
-
-         if (dom && this._mainChild) {
-            dom.appendChild(this._mainChild)
-            delete this._mainChild;
-            check_resize = true;
-         }
-
-         if (dom && !dom.children.length) {
-             let d = document.createElement("div");
-             d.style = "position:relative;left:0;right:0;top:0;bottom:0;height:100%;width:100%";
-             dom.appendChild(d);
-          }
-
-         if (this.canvas_painter) {
-            this.canvas_painter.SetDivId(dom.lastChild, -1);
-            if (check_resize) this.canvas_painter.CheckCanvasResize();
-         }
-
-         if (this.canvas_painter && this.canvas_painter._window_handle) {
-            this.canvas_painter.UseWebsocket(this.canvas_painter._window_handle, this.canvas_painter._window_handle_href);
-            delete this.canvas_painter._window_handle;
-         }
+         // workaround for openui5 problem - called before actual dimension of HTML element is assigned
+         // using timeout and resize event to handle it correctly
+         this._has_after_rendering = true;
+         this.invokeResizeTimeout(10);
       },
 
-      onResize: function(/* event */) {
-         // use timeout
-         if (this.resize_tmout) clearTimeout(this.resize_tmout);
-         this.resize_tmout = setTimeout(this.onResizeTimeout.bind(this), 300); // minimal latency
+      hasValidSize: function() {
+         return (this.getView().$().width() > 0) && (this.getView().$().height() > 0);
       },
 
-      drawCanvas: function(can, opt, call_back) {
-         if (this.canvas_painter) {
-            this.canvas_painter.Cleanup();
-            delete this.canvas_painter;
-         }
+      invokeResizeTimeout: function(tmout) {
+        if (this.resize_tmout) {
+            clearTimeout(this.resize_tmout);
+            delete this.resize_tmout;
+        }
 
-         let dom = this.getView().getDomRef();
-
-         if (!dom)
-            return JSROOT.CallBack(call_back, null);
-
-         if (dom.children.length == 0) {
-             let d = document.createElement("div");
-             d.style= "position:relative;left:0;right:0;top:0;bottom:0;height:100%;width:100%";
-             dom.appendChild(d);
-          }
-
-         JSROOT.draw(dom.lastChild, can, opt).then(painter => {
-            this.canvas_painter = painter;
-            JSROOT.CallBack(call_back, painter);
-         });
+        if (this.hasValidSize() && this._has_after_rendering)
+           this.onResizeTimeout();
+        else
+           this.resize_tmout = setTimeout(this.onResizeTimeout.bind(this), tmout);
       },
 
+      /** @summary Handle openui5 resize glitch
+        * @desc onAfterRendering method does not provide valid dimension of the HTML element
+        * One should wait either resize event or timeout and check if valid size is there
+        * Only then normal rendering can be started
+        * Method also used to check resize events  */
       onResizeTimeout: function() {
          delete this.resize_tmout;
-         if (this.canvas_painter)
+
+         if (!this.hasValidSize())
+             return this.invokeResizeTimeout(5000); // very rare check if something changed
+
+         let check_resize = true;
+
+         if (this._has_after_rendering) {
+
+            this._has_after_rendering = false;
+            check_resize = false;
+
+            let dom = this.getView().getDomRef();
+
+            if (dom && this._mainChild) {
+               dom.appendChild(this._mainChild)
+               delete this._mainChild;
+               check_resize = true;
+            }
+
+            if (dom && !dom.children.length) {
+               let d = document.createElement("div");
+               d.style = "position:relative;left:0;right:0;top:0;bottom:0;height:100%;width:100%";
+               dom.appendChild(d);
+            }
+
+            if (this.canvas_painter)
+               this.canvas_painter.SetDivId(dom.lastChild, -1);
+
+            if (this.canvas_painter && this.canvas_painter._window_handle) {
+               this.canvas_painter.UseWebsocket(this.canvas_painter._window_handle, this.canvas_painter._window_handle_href);
+               delete this.canvas_painter._window_handle;
+            }
+         }
+
+         if (this.canvas_painter && check_resize)
             this.canvas_painter.CheckCanvasResize();
       },
 
       onInit: function() {
-         console.log("INIT CANVAS PANEL");
-
-         ResizeHandler.register(this.getView(), this.onResize.bind(this));
+         ResizeHandler.register(this.getView(), this.invokeResizeTimeout.bind(this, 200));
       },
 
       onExit: function() {
