525a7368847f99d37836bf3631582516f021f7fd
Xavier Valls Pla <xavier.valls.pla@cern.ch>
2020-11-04 16:08:21 +0100

    revert FitExecutionPolicy deletion and deprecate it

---
+++ math/mathcore/inc/Fit/FitExecutionPolicy.h
@@ -0,0 +1,11 @@
+#include "ExecutionPolicy.hxx"
+#include "ROOT/RConfig.hxx"
+
+namespace ROOT {
+namespace Fit {
+
+using ExecutionPolicy = _R__DEPRECATED_626("Using GetPoolSize to unify the Executor's interfaces") ROOT::Internal::ExecutionPolicy;
+
+
+} // ROOT ns
+} // Fit ns
