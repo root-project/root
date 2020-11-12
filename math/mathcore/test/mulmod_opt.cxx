b7b42f97e4840cb5f4f9f6936b220ae43f4ffda3
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2020-11-12 12:41:26 +0100

    Use __int128 in multiply9x9 if available
    This reduces the time per number from around 19ns to less than 11ns if
    the compiler exposes this non-standard extension. Add test to make sure
    that the portable fallback path gives the same results.

---
+++ math/mathcore/test/mulmod_opt.cxx
@@ -0,0 +1,12 @@
+// @(#)root/mathcore:$Id$
+// Author: Jonas Hahnfeld 11/2020
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "mulmod.icc"
