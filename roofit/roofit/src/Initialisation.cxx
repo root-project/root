d6174232215334e2674dd74bc24a1d4fd9e35548
Emmanouil <emmanouil.michalainas@cern.ch>
2020-11-13 15:47:17 +0100

    [RF] Transfer RooFitComputeLib loading from RooFitcore to RooFit Initialisation.

--- roofit/roofit/src/Initialisation.cxx
+++ roofit/roofit/src/Initialisation.cxx
@@ -1,4 +1,12 @@
+/**
+\file Initialisation.cxx
+\ingroup Roofit
+
+Run static initialisers on first load of RooFit.
+**/
+
 #include "RooFitComputeInterface.h"
+#include "InitUtils.h"
 
 /**
  * The dispatch pointer always points to the most recently created RooFitComputeClass object. Effectively, function calls through the object resolve to:
@@ -8,3 +16,11 @@
  * \see RooFitComputeInterface, RooFitComputeClass, RF_ARCH
  */
 RooFitCompute::RooFitComputeInterface* RooFitCompute::dispatch=nullptr;
+
+
+/// A RAII that performs RooFit's static initialisation.
+static struct RooFitInitialiser {
+  RooFitInitialiser() {
+    RooFit::InitUtils::loadComputeLibrary("libRooFitCompute");
+  }
+} __rooFitInitialiser;
---
+++ roofit/roofitcore/inc/InitUtils.h
@@ -0,0 +1,14 @@
+#ifndef INITUTILS_H
+#define INITUTILS_H
+
+#include <string>
+
+namespace RooFit{
+namespace InitUtils {
+
+void loadComputeLibrary(std::string libName);
+
+}
+}
+
+#endif
