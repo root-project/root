772728f3a4c00e4a63a81f3da1963c9fc2fb251d
Sergey Linev <S.Linev@gsi.de>
2020-11-13 09:36:59 +0100

    Use RAttrBase::CollectDefaults()
    Provide specialilzed implementation for some RAttr classes
    Required when class members does not reflect all
    possible values. Or when class does not have dictioanry -
    as in the test

--- graf2d/gpadv7/test/attribute.cxx
+++ graf2d/gpadv7/test/attribute.cxx
@@ -10,6 +10,9 @@ class CustomAttrs : public RAttrBase {
    RAttrBox fAttrBox{this, "box_"};
    RAttrText fAttrText{this, "text_"};
 
+protected:
+   RAttrMap CollectDefaults() const override { return RAttrMap().AddDefaults(fAttrBox).AddDefaults(fAttrText); }
+
    R__ATTR_CLASS(CustomAttrs, "custom_", AddDefaults(fAttrBox).AddDefaults(fAttrText));
 
    const RAttrBox &GetAttrBox() const { return fAttrBox; }
@@ -87,23 +90,19 @@ TEST(OptsTest, AttribDiffer) {
    CustomAttrs attrs2;
    CustomAttrs attrs3;
 
-   auto &al1 = attrs1.AttrBox().AttrBorder();
-   auto &al2 = attrs2.AttrBox().AttrBorder();
-   auto &al3 = attrs3.AttrBox().AttrBorder();
+   attrs1.AttrBox().AttrBorder().SetWidth(7.);
+   EXPECT_NE(attrs1, attrs2);
+   EXPECT_NE(attrs2, attrs1);
+   EXPECT_NE(attrs1, attrs3);
+   EXPECT_EQ(attrs2, attrs3);
+   EXPECT_EQ(attrs3, attrs2);
 
-   al1.SetWidth(7.);
-   EXPECT_NE(al1, al2);
-   EXPECT_NE(al2, al1);
-   EXPECT_NE(al1, al3);
-   EXPECT_EQ(al2, al3);
-   EXPECT_EQ(al3, al2);
-
-   al2.SetColor(RColor::kRed);
-   EXPECT_NE(al1, al2);
-   EXPECT_NE(al2, al1);
-   EXPECT_NE(al1, al3);
-   EXPECT_NE(al2, al3);
-   EXPECT_NE(al3, al2);
+   attrs2.AttrBox().AttrBorder().SetColor(RColor::kRed);
+   EXPECT_NE(attrs1, attrs2);
+   EXPECT_NE(attrs2, attrs1);
+   EXPECT_NE(attrs1, attrs3);
+   EXPECT_NE(attrs2, attrs3);
+   EXPECT_NE(attrs3, attrs2);
 }
 
 
