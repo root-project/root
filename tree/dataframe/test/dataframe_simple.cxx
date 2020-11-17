e3b05877afe59299c558914634f4d27a331ebd14
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-11-17 18:26:46 +0100

    [DF][NFC] Adapt test now that we do not suppress warnings

--- tree/dataframe/test/dataframe_simple.cxx
+++ tree/dataframe/test/dataframe_simple.cxx
@@ -853,6 +853,11 @@ TEST_P(RDFSimpleTests, NonExistingFileInChain)
    const auto errmsg = "file %s/doesnotexist.root does not exist";
    TString expecteddiag;
    expecteddiag.Form(errmsg, gSystem->pwd());
+   // in the single-thread case the error happens when TTreeReader is calling LoadTree the first time
+   // otherwise we notice the file does not exist beforehand, e.g. in TTreeProcessorMT
+   if (!ROOT::IsImplicitMTEnabled())
+      expecteddiag += "\nWarning in <TTreeReader::SetEntryBase()>: There was an issue opening the last file associated "
+                      "to the TChain being processed.";
 
    bool exceptionCaught = false;
    try {
