77f37eee9de099eac3c65f842e877dcafa28f7f3
Advait Dhingra <advaittheboss@gmail.com>
2021-01-14 14:44:47 +0100

    Fixed negative value segfaults

--- core/base/src/TString.cxx
+++ core/base/src/TString.cxx
@@ -126,7 +126,7 @@ TString::TString(const std::string &s)
 TString::TString(const char *cs, Ssiz_t n)
 {
    if (n < 0) {
-      Error("TString::TString", "Negative length!");
+      Zero();
       return;
    }
    char *data = Init(n, n);
@@ -155,6 +155,12 @@ TString::TString(char c)
 
 TString::TString(char c, Ssiz_t n)
 {
+   if (n < 0)
+   {
+      Zero();
+	  return;
+   }
+	
    char *data = Init(n, n);
    while (n--) data[n] = c;
 }
@@ -209,11 +215,11 @@ TString::TString(const TSubString& substr)
 TString::TString(const char *a1, Ssiz_t n1, const char *a2, Ssiz_t n2)
 {
    if (n1 < 0) {
-      Error("TString::TString", "Negative first length!");
+      Zero();
       return;
    }
    if (n2 < 0) {
-      Error("TString::TString", "Negative second length!");
+      Zero();
       return;
    }
    if (!a1) n1=0;
@@ -238,6 +244,12 @@ TString::~TString()
 
 char *TString::Init(Ssiz_t capacity, Ssiz_t nchar)
 {
+
+   if (capacity < 0 || nchar < 0)
+   {
+      char *data = 0; // sets pointer to zero 
+      return data;
+   }
    if (capacity > MaxSize()) {
       Error("TString::Init", "capacity too large (%d, max = %d)", capacity, MaxSize());
       capacity = MaxSize();
@@ -361,7 +373,8 @@ TString& TString::operator=(const TSubString &substr)
 
 TString& TString::Append(char c, Ssiz_t rep)
 {
-   if (!rep) return *this;
+   if (!rep || rep < 0 ) return *this; // appends nothing
+   
 
    Ssiz_t len = Length();
    Ssiz_t tot = len + rep;  // Final string length
