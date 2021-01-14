91628e325eff15533f8958f7d4fc14b5edc57c00
Sergey Linev <S.Linev@gsi.de>
2021-01-14 15:50:25 +0100

    [qt5web] provide widget example with geometry viewer
    Show how viewer can be configured without showing hierarchy by default
    Use again CMS geometry as demo

--- tutorials/webgui/qt5web/ExampleWidget.cpp
+++ tutorials/webgui/qt5web/ExampleWidget.cpp
@@ -14,9 +14,13 @@
 #include "TH1.h"
 #include "TH2.h"
 #include "TMath.h"
+#include "TFile.h"
+#include "TGeoManager.h"
+#include "TGeoVolume.h"
 
 #include <ROOT/RCanvas.hxx>
 #include <ROOT/TObjectDrawable.hxx>
+#include <ROOT/REveGeomViewer.hxx>
 
 #include <QMessageBox>
 
@@ -50,15 +54,86 @@ ExampleWidget::ExampleWidget(QWidget *parent, const char* name) :
    }
 
    fxRCanvasWidget->getCanvas()->Draw<ROOT::Experimental::TObjectDrawable>(fHisto2, "col");
+
+   CreateDummyGeometry();
 }
 
 ExampleWidget::~ExampleWidget()
 {
 }
 
+
+void ExampleWidget::CreateDummyGeometry()
+{
+   auto viewer = fxGeomViewerWidget->getGeomViewer();
+
+   new TGeoManager("tubeseg", "poza3");
+   TGeoMaterial *mat = new TGeoMaterial("Al", 26.98,13,2.7);
+   TGeoMedium *med = new TGeoMedium("MED",1,mat);
+   TGeoVolume *top = gGeoManager->MakeBox("TOP",med,100,100,100);
+   gGeoManager->SetTopVolume(top);
+   TGeoVolume *vol = gGeoManager->MakeTubs("TUBESEG",med, 20,30,40,-30,270);
+   vol->SetLineColor(kRed);
+   vol->SetLineWidth(2);
+   top->AddNode(vol,1);
+   gGeoManager->CloseGeometry();
+//   gGeoManager->SetNsegments(40);
+   gGeoManager->SetNsegments(80);
+
+   viewer->SetGeometry(gGeoManager);
+}
+
+void ExampleWidget::ImportCmsGeometry()
+{
+   TFile::SetCacheFileDir(".");
+
+   if (!TGeoManager::Import("https://root.cern/files/cms.root")) {
+      CreateDummyGeometry();
+      return;
+   }
+
+   auto viewer = fxGeomViewerWidget->getGeomViewer();
+
+   gGeoManager->DefaultColors();
+   gGeoManager->SetVisLevel(4);
+   gGeoManager->GetVolume("TRAK")->InvisibleAll();
+   gGeoManager->GetVolume("HVP2")->SetTransparency(20);
+   gGeoManager->GetVolume("HVEQ")->SetTransparency(20);
+   gGeoManager->GetVolume("YE4")->SetTransparency(10);
+   gGeoManager->GetVolume("YE3")->SetTransparency(20);
+   gGeoManager->GetVolume("RB2")->SetTransparency(99);
+   gGeoManager->GetVolume("RB3")->SetTransparency(99);
+   gGeoManager->GetVolume("COCF")->SetTransparency(99);
+   gGeoManager->GetVolume("HEC1")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP1")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP2")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP3")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP4")->SetLineColor(7);
+   gGeoManager->GetVolume("HTC1")->SetLineColor(2);
+
+   viewer->SetGeometry(gGeoManager);
+
+   // select volume to draw
+   viewer->SelectVolume("CMSE");
+
+   // specify JSROOT draw options - here clipping on X,Y,Z axes
+   viewer->SetDrawOptions("clipxyz");
+
+   // set default limits for number of visible nodes and faces
+   // when viewer created, initial values exported from TGeoManager
+   viewer->SetLimits();
+}
+
 void ExampleWidget::InfoButton_clicked()
 {
    QMessageBox::information(this,"QtRoot standalone example","Demo how QRootCanvas can be inserted into the QWidget");
+}
+
+
+void ExampleWidget::CmsButton_clicked()
+{
+   ImportCmsGeometry();
+   fxTabWidget->setCurrentIndex(3);
 }
 
 void ExampleWidget::ExitButton_clicked()
