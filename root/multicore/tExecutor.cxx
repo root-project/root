9278b939fc352fb6d3343a979aa5d983a76aadcd
Xavier Valls Pla <xavier.valls.pla@cern.ch>
2021-01-15 15:16:56 +0100

    fix return codes

--- root/multicore/tExecutor.cxx
+++ root/multicore/tExecutor.cxx
@@ -9,47 +9,47 @@ int main() {
    ROOT::Internal::TExecutor ex{4u};
    if (!ROOT::IsImplicitMTEnabled()) {
       if(ex.Policy()!= ROOT::EExecutionPolicy::kSequential) {
-         return 0;
+         return 1;
       }
    }
 #ifdef R__USE_IMT
    else {
       if(ex.Policy()!= ROOT::EExecutionPolicy::kSequential) {
-         return 1;
+         return 2;
       }
    }
 
    ROOT::Internal::TExecutor ex1{ROOT::EExecutionPolicy::kMultiThread, 4u};
    if(ex1.Policy()!= ROOT::EExecutionPolicy::kMultiThread) {
-         return 2;
+         return 3;
    }
 #endif
 
 #ifndef _MSC_VER
    ROOT::Internal::TExecutor ex2{ROOT::EExecutionPolicy::kMultiProcess, 4u};
    if(ex2.Policy()!= ROOT::EExecutionPolicy::kMultiProcess) {
-         return 3;
+         return 4;
    }
 #endif
 
    ROOT::Internal::TExecutor ex3{ROOT::EExecutionPolicy::kSequential};
    if(ex3.Policy()!= ROOT::EExecutionPolicy::kSequential) {
-         return 4;
+         return 5;
    }
 
    auto offset = 14; //number of tests in TExecutorTest
    auto res = 0;
    res = ExecutorTest(ex);
-   if(res) return res;
+   if(res) return res+5;
 #ifdef R__USE_IMT
    res = ExecutorTest(ex1);
-   if(res) return res+offset+4;
+   if(res) return res+offset+5;
 #endif
 #ifndef _MSC_VER
    res = ExecutorTest(ex2);
-   if(res) return res+2*offset+4;
+   if(res) return res+2*offset+5;
 #endif
    res = ExecutorTest(ex3);
-   if(res) return res+3*offset+4;
-   return res? res+4*offset: 0;
+   if(res) return res+3*offset+5;
+   return res? res+4*offset+5: 0;
 }
