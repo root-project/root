d3d3b3fc8f8b8920130c92275418f6ee42c2fa20
Xavier Valls Pla <xavier.valls.pla@cern.ch>
2021-01-18 10:56:12 +0100

    set TBB_PREVIEW_GLOBAL_CONTROL for older tbb versions

--- core/imt/src/RTaskArena.cxx
+++ core/imt/src/RTaskArena.cxx
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <thread>
 #include "tbb/task_arena.h"
+#define TBB_PREVIEW_GLOBAL_CONTROL 1 // required for TBB versions preceding 2019_U4
 #include "tbb/global_control.h"
 
 //////////////////////////////////////////////////////////////////////////
