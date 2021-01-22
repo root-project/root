868d4067a1724ed5dda4d1ada056cdeee3166de7
Sergey Linev <S.Linev@gsi.de>
2021-01-22 15:43:47 +0100

    [rbrowser] Introduce BrowserWidget abstraction
    It is internal (not-public) classes which allows to load
    different widgets on demand.
    As a first example Geometry viewer is used.
    It is unavoidable while Geometry viewer itself depends on browser
    Idea that later all other widgets will be implemented same way.
    
    Main advantage - Gpad, Gpadv7 or ROOTEve libraries will be loaded
    by demand into the browser

--- ui5/canv/controller/CanvasPanel.controller.js
+++ ui5/canv/controller/CanvasPanel.controller.js
@@ -89,7 +89,7 @@ sap.ui.define([
             }
 
             if (this.canvas_painter && this.canvas_painter._window_handle) {
-               this.canvas_painter.useWebsocket(this.canvas_painter._window_handle, this.canvas_painter._window_handle_href);
+               this.canvas_painter.useWebsocket(this.canvas_painter._window_handle);
                delete this.canvas_painter._window_handle;
             }
          }
