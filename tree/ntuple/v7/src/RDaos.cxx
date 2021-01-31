340b3c5a7efbc752ccce224827206c0883361f64
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2021-01-31 20:56:53 +0100

    [ntuple] DAOS support: move RDaosObject member function definitions to translation unit

--- tree/ntuple/v7/src/RDaos.cxx
+++ tree/ntuple/v7/src/RDaos.cxx
@@ -15,6 +15,9 @@
 
 #include <ROOT/RDaos.hxx>
 
+#include <numeric>
+#include <stdexcept>
+
 ROOT::Experimental::Detail::RDaosPool::RDaosPool(std::string_view poolUuid, std::string_view serviceReplicas) {
    struct SvcRAII {
       d_rank_list_t *rankList;
@@ -29,6 +32,73 @@ ROOT::Experimental::Detail::RDaosPool::RDaosPool(std::string_view poolUuid, std:
 
 ROOT::Experimental::Detail::RDaosPool::~RDaosPool() {
    daos_pool_disconnect(fPoolHandle, nullptr);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <typename DKeyT, typename AKeyT>
+ROOT::Experimental::Detail::RDaosObject<DKeyT, AKeyT>::FetchUpdateArgs::FetchUpdateArgs(FetchUpdateArgs&& fua)
+  : fDkey(fua.fDkey), fAkey(fua.fAkey),
+    fIods{fua.fIods[0]}, fSgls{fua.fSgls[0]}, fIovs(std::move(fua.fIovs)), fEv(fua.fEv)
+{
+   d_iov_set(&fDistributionKey, key_data(fDkey), key_size(fDkey));
+   d_iov_set(&fIods[0].iod_name, key_data(fAkey), key_size(fAkey));
+}
+
+template <typename DKeyT, typename AKeyT>
+ROOT::Experimental::Detail::RDaosObject<DKeyT, AKeyT>::FetchUpdateArgs::FetchUpdateArgs
+(DistributionKey_t &d, AttributeKey_t &a, std::vector<d_iov_t> &v, daos_event_t *p)
+  : fDkey(d), fAkey(a), fIovs(v), fEv(p)
+{
+   d_iov_set(&fDistributionKey, key_data(fDkey), key_size(fDkey));
+
+   d_iov_set(&fIods[0].iod_name, key_data(fAkey), key_size(fAkey));
+   fIods[0].iod_nr = 1;
+   fIods[0].iod_size = std::accumulate(v.begin(), v.end(), 0,
+                                       [](daos_size_t _a, d_iov_t _b) { return _a + _b.iov_len; });
+   fIods[0].iod_recxs = nullptr;
+   fIods[0].iod_type = DAOS_IOD_SINGLE;
+
+   fSgls[0].sg_nr_out = 0;
+   fSgls[0].sg_nr = fIovs.size();
+   fSgls[0].sg_iovs = fIovs.data();
+}
+
+template <typename DKeyT, typename AKeyT>
+ROOT::Experimental::Detail::RDaosObject<DKeyT, AKeyT>::RDaosObject(RDaosContainer &container, daos_obj_id_t oid,
+                                                                   daos_oclass_id_t cid)
+{
+   daos_ofeat_t ofeats{};
+   if (std::is_same<std::uint64_t, DKeyT>::value)
+      ofeats |= DAOS_OF_DKEY_UINT64;
+   if (std::is_same<std::uint64_t, AKeyT>::value)
+      ofeats |= DAOS_OF_AKEY_UINT64;
+   daos_obj_generate_id(&oid, ofeats /*| DAOS_OF_ARRAY_BYTE*/, cid, 0);
+   if (int err = daos_obj_open(container.fContainerHandle, oid, DAOS_OO_RW, &fObjectHandle, nullptr))
+      throw std::runtime_error("daos_obj_open: error: " + std::string(d_errstr(err)));
+}
+
+template <typename DKeyT, typename AKeyT>
+ROOT::Experimental::Detail::RDaosObject<DKeyT, AKeyT>::~RDaosObject()
+{
+   daos_obj_close(fObjectHandle, nullptr);
+}
+
+template <typename DKeyT, typename AKeyT>
+int ROOT::Experimental::Detail::RDaosObject<DKeyT, AKeyT>::Fetch(FetchUpdateArgs &args)
+{
+   args.fIods[0].iod_size = (daos_size_t)DAOS_REC_ANY;
+   return daos_obj_fetch(fObjectHandle, DAOS_TX_NONE, 0, &args.fDistributionKey, 1,
+                         args.fIods, args.fSgls, nullptr, args.fEv);
+}
+
+template <typename DKeyT, typename AKeyT>
+int ROOT::Experimental::Detail::RDaosObject<DKeyT, AKeyT>::Update(FetchUpdateArgs &args)
+{
+   return daos_obj_update(fObjectHandle, DAOS_TX_NONE, DAOS_COND_DKEY_INSERT, &args.fDistributionKey, 1,
+                          args.fIods, args.fSgls, args.fEv);
 }
 
 
@@ -93,3 +163,6 @@ static struct RDaosRAII {
    ~RDaosRAII() { daos_fini(); }
 } RAII{};
 } // anonymous namespace
+
+// Explicit instantiations of `RDaosObject` for specific dkey/akey types
+template class ROOT::Experimental::Detail::RDaosObject<uint64_t, uint64_t>;
