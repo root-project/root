32e4e18b97462cbb12b0a239a630b24707c48b90
Philippe Canal <pcanal@fnal.gov>
2021-02-01 11:32:31 -0600

    In TBranchElement init offset avoid stripping part of the name.
    A prefix needs to be followed by a delimiter
    
    This fixes ROOOT-5983.

--- tree/tree/src/TBranchElement.cxx
+++ tree/tree/src/TBranchElement.cxx
@@ -53,12 +53,14 @@ ClassImp(TBranchElement);
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace {
-   void RemovePrefix(TString& str, const char* prefix) {
+   void RemovePrefix(TString& str, const TString &prefix) {
       // -- Remove a prefix from a string.
-      if (str.Length() && prefix && strlen(prefix)) {
-         if (!str.Index(prefix)) {
+      // -- Require a '.' after the prefix.
+      if (prefix.Length() && prefix.Length() <= str.Length()
+          && (str.Data()[prefix.Length()] == '.' || (prefix[prefix.Length()-1]=='.')))
+      {
+         if (!str.Index(prefix))
             str.Remove(0, strlen(prefix));
-         }
       }
    }
    struct R__PushCache {
