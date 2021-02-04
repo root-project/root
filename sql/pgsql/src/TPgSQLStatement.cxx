0575b4d3c5531a2627189efcb92b7404416f7a5f
Sergey Linev <S.Linev@gsi.de>
2021-02-04 17:32:06 +0100

    [pgsql] fix memory leak in TPgSQLStatement::SetNull

--- sql/pgsql/src/TPgSQLStatement.cxx
+++ sql/pgsql/src/TPgSQLStatement.cxx
@@ -1172,8 +1172,10 @@ Bool_t TPgSQLStatement::SetSQLParamType(Int_t, int, bool, int)
 
 Bool_t TPgSQLStatement::SetNull(Int_t npar)
 {
-   if ((npar >= 0) && (npar < fNumBuffers))
-      fBind[npar] = 0;
+   if ((npar >= 0) && (npar < fNumBuffers)) {
+      delete [] fBind[npar];
+      fBind[npar] = nullptr;
+   }
    return kFALSE;
 }
 
