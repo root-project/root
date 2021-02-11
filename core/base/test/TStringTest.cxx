de6c9ca2c1db29f1684f5e7bd68a7c16e753168a
Advait Dhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-02-11 18:37:29 +0100

    Added ROOTUnitTestSupport and removed gmock

--- core/base/test/TStringTest.cxx
+++ core/base/test/TStringTest.cxx
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
-#include "gmock/gmock.h"
+
+#include "ROOTUnitTestSupport.h"
 
 #include "TString.h"
 
@@ -7,9 +8,11 @@ TEST(TString, Basics)
 {
    TString n("Test", -5);
    EXPECT_STREQ("", n);
+   ROOT_EXPECT_ERROR(s = new TString("Test", -5), "TString::TString", "Negative length!");
    TString p("Test", 1);
    EXPECT_STREQ("T", p);
    TString a = "test";
    a.Append("s", -5);
    EXPECT_STREQ("test", a);
+   ROOT_EXPECT_ERROR(a.Append("s", -5, "TString::Append", "Negative length!");
 }
