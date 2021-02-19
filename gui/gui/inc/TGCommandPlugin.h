30d5b13caefd831a8fc836c2b2c912cd88edf2ee
Bertrand Bellenot <bellenot@users.noreply.github.com>
2021-02-19 13:18:40 +0100

    Add up and down arrow keys handling (resolves #7180) (#7220)
    * Add up and down arrow keys handling (fix #7180)
    
    * Improve the command plugin behaviour
    
     - Add <tab> completion (thanks @ferdymercury)
     - Improve the `up` and `down` key handling
     - Add a temporary string to save the current user input
     - Update copyright
    
    * Document the `SetHistAdd()` method
    
    * Doxygen formatting (thanks Olivier)

--- gui/gui/inc/TGCommandPlugin.h
+++ gui/gui/inc/TGCommandPlugin.h
@@ -1,8 +1,20 @@
 // @(#)root/gui:$Id$
 // Author: Bertrand Bellenot   26/09/2007
 
+/*************************************************************************
+ * Copyright (C) 1995-2021, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 #ifndef ROOT_TGCommandPlugin
 #define ROOT_TGCommandPlugin
+
+/** \class TGCommandPlugin
+Class used to redirect the command line input/output.
+**/
 
 #include "TGFrame.h"
 
@@ -16,7 +28,9 @@ class TTimer;
 class TGCommandPlugin : public TGMainFrame {
 
 protected:
+   Bool_t             fHistAdd;           // flag to add commands to history
    Int_t              fPid;               // current process id
+   Int_t              fPos;               // current history position
    TGHorizontalFrame *fHf;                // horizontal frame
    TGLabel           *fLabel;             // "command :" label
    TGComboBox        *fComboCmd;          // commands combobox
@@ -24,6 +38,7 @@ protected:
    TGTextBuffer      *fCommandBuf;        // command text buffer
    TGTextView        *fStatus;            // output capture view
    TTimer            *fTimer;             // for local/remote update
+   TString           fTempString;         // temporary command string
 
 public:
 
@@ -31,7 +46,12 @@ public:
    virtual ~TGCommandPlugin();
 
    void           CheckRemote(const char * /*str*/);
+   void           HandleArrows(Int_t keysym);
    void           HandleCommand();
+   void           HandleTab();
+   void           HandleTextChanged(const char *);
+   Bool_t         GetHistAdd() { return fHistAdd; }
+   void           SetHistAdd(Bool_t add = kTRUE);
 
    virtual Bool_t HandleTimer(TTimer *t);
 
