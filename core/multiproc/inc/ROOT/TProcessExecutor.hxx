730f77cc898c83b2373e6e677ea91574313f4a1c
Axel Naumann <Axel.Naumann@cern.ch>
2021-03-03 18:23:26 +0100

    [multiproc] Remove deprecated interfaces.

--- core/multiproc/inc/ROOT/TProcessExecutor.hxx
+++ core/multiproc/inc/ROOT/TProcessExecutor.hxx
@@ -16,7 +16,6 @@
 #include "MPCode.h"
 #include "MPSendRecv.h"
 #include "PoolUtils.h"
-#include "ROOT/RConfig.hxx" //R__DEPRECATED
 #include "ROOT/TExecutorCRTP.hxx"
 #include "ROOT/TSeq.hxx"
 #include "TError.h"
@@ -64,9 +63,6 @@ public:
    using TExecutorCRTP<TProcessExecutor>::Reduce;
 
    void SetNWorkers(unsigned n) { TMPClient::SetNWorkers(n); }
-
-   /// \deprecated Use GetPoolSize()
-   unsigned GetNWorkers() const _R__DEPRECATED_626("Please use GetPoolSize instead") { return TMPClient::GetNWorkers(); }
 
    //////////////////////////////////////////////////////////////////////////
    /// \brief Return the number of pooled parallel workers.
