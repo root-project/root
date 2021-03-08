6150bc8031e8996b4bd9f3ff5f5945fa9a52bdaf
John Yoon <johnyoon13@gmail.com>
2021-03-08 11:40:53 -0500

    [ntuple] Update formatting for ROOTv7 revised climate tutorial

--- tutorials/v7/global_temperatures.cxx
+++ tutorials/v7/global_temperatures.cxx
@@ -1,20 +1,22 @@
-// Tutorial that highlights ROOT 7 features such as RNTuple and visualizations, as well as ROOT 6 features like RDataframe.
+/// \file
+/// \ingroup tutorial_v7
+///
+/// This ROOT 7 example demonstrates how to use RNTuple in combination with ROOT 6 features like RDataframe and visualizations.
+/// It ingests climate data and creates a model with fields like AverageTemperature. Then it uses RDataframe to process and filter the climate data for
+/// average temperature per city by season. Then it does the same for average temperature per city for the years between 1993-2002, and 2003-2013.
+/// Finally, the tutorial visualizes this processed data through histograms.
+///
+/// \macro_code
+///
+/// \date 2021-02-26
+/// \warning The RNTuple classes are experimental at this point.
+/// Functionality, interface, and data format is still subject to changes.
+/// Do not use for real data! During ROOT setup, configure the following flags: "-DCMAKE_CXX_STANDARD=14 -Droot7=ON -Dwebgui=ON"
+/// \author John Yoon
 
-// The tutorial first uses RNTuple to ingest climate data and create a model with fields like
-// AverageTemperature. Then it uses RDataframe to process and filter the climate data for
-// average temperature per city by season. Then it does the same for average temperature
-// per city for the years between 1993-2002, and 2003-2013. Finally, the tutorial
-// visualizes this processed data through histograms.
-
-//During ROOT setup, configure the following flags: "-DCMAKE_CXX_STANDARD=14 -Droot7=ON -Dwebgui=ON"
-
-// NOTE: The RNTuple classes are experimental at this point.
-// Functionality, interface, and data format is still subject to changes.
-// Do not use for real data!
-
-// Until C++ runtime modules are universally used, we explicitly load the ntuple library.  Otherwise
-// triggering autoloading from the use of templated types would require an exhaustive enumeration
-// of "all" template instances in the LinkDef file.
+/// NOTE: Until C++ runtime modules are universally used, we explicitly load the ntuple library.  Otherwise
+/// triggering autoloading from the use of templated types would require an exhaustive enumeration
+/// of "all" template instances in the LinkDef file.
 R__LOAD_LIBRARY(ROOTNTuple)
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RNTuple.hxx>
