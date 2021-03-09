5a27977d02d6ee81b9217cadc58408514bac91ca
Sergey Linev <S.Linev@gsi.de>
2021-03-09 16:07:02 +0100

    [browsable] let RNTuple draw on RCanvas
    Make it default action, keep support of drawing on TCanvas as well

--- gui/browsable/src/RNTupleDraw7Provider.cxx
+++ gui/browsable/src/RNTupleDraw7Provider.cxx
@@ -6,42 +6,50 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#include "TVirtualPad.h"
 #include "TClass.h"
+
+#include <ROOT/RCanvas.hxx>
+#include <ROOT/TObjectDrawable.hxx>
 
 #include "RFieldProvider.hxx"
 
+using namespace ROOT::Experimental;
 
 // ==============================================================================================
 
-/** \class RNTupleDraw6Provider
+/** \class RNTupleDraw7Provider
 \ingroup rbrowser
-\brief Provider for RNTuple drawing on TCanvas
+\brief Provider for RNTuple drawing on RCanvas
 \author Sergey Linev <S.Linev@gsi.de>
 \date 2021-03-09
 \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
 */
 
-class RNTupleDraw6Provider : public RFieldProvider<void> {
+class RNTupleDraw7Provider : public RFieldProvider<void> {
 
 public:
 
-   RNTupleDraw6Provider()
+   RNTupleDraw7Provider()
    {
-      RegisterDraw6(TClass::GetClass<ROOT::Experimental::RNTuple>(), [this](TVirtualPad *pad, std::unique_ptr<RHolder> &obj, const std::string &opt) -> bool {
+      RegisterDraw7(TClass::GetClass<ROOT::Experimental::RNTuple>(), [this](std::shared_ptr<RPadBase> &subpad, std::unique_ptr<RHolder> &obj, const std::string &opt) -> bool {
 
          auto h1 = DrawField(dynamic_cast<RFieldHolder*> (obj.get()));
          if (!h1) return false;
 
-         pad->GetListOfPrimitives()->Clear();
+         if (subpad->NumPrimitives() > 0) {
+            subpad->Wipe();
+            subpad->GetCanvas()->Modified();
+            subpad->GetCanvas()->Update(true);
+         }
 
-         pad->GetListOfPrimitives()->Add(h1, opt.c_str());
+         std::shared_ptr<TH1> shared;
+         shared.reset(h1);
 
+         subpad->Draw<ROOT::Experimental::TObjectDrawable>(shared, opt);
+         subpad->GetCanvas()->Update(true);
          return true;
       });
-
-
    }
 
-} newRNTupleDraw6Provider;
+} newRNTupleDraw7Provider;
 
