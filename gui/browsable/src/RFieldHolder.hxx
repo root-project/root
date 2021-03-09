b4a7ff327421a92b72f2d037a49149c9bbdf31b0
Sergey Linev <S.Linev@gsi.de>
2021-03-09 11:41:49 +0100

    [browsable] Keep parent prefix when browsing RField in RNTuple
    Such prefix required to create view for the field.
    Later one could switch to RPageSource

--- gui/browsable/src/RFieldHolder.hxx
+++ gui/browsable/src/RFieldHolder.hxx
@@ -22,11 +22,12 @@
 template<typename T>
 class RFieldHolder : public ROOT::Experimental::Browsable::RHolder {
    std::shared_ptr<ROOT::Experimental::RNTupleReader> fNTuple;
+   std::string fParentName;
 
    ROOT::Experimental::DescriptorId_t fFieldId;
 
 public:
-   RFieldHolder(std::shared_ptr<ROOT::Experimental::RNTupleReader> tuple, ROOT::Experimental::DescriptorId_t id) : fNTuple(tuple), fFieldId(id) {}
+   RFieldHolder(std::shared_ptr<ROOT::Experimental::RNTupleReader> tuple, const std::string &parent_name, ROOT::Experimental::DescriptorId_t id) : fNTuple(tuple), fParentName(parent_name), fFieldId(id) {}
 
    const TClass *GetClass() const override { return TClass::GetClass<ROOT::Experimental::RNTuple>(); }
 
@@ -34,6 +35,7 @@ public:
    const void *GetObject() const override { return nullptr; }
 
    auto GetNTuple() const { return fNTuple; }
+   auto GetParentName() const { return fParentName; }
    auto GetId() const { return fFieldId; }
 
 };
