f58c7dc43ce91bfe0814e10bc1c373abbd01c6a1
Sergey Linev <S.Linev@gsi.de>
2021-03-09 13:36:18 +0100

    [browsable] improve generated histograms for RNTuple drawing
    Provide better-looking icons and meaningfull title

--- gui/browsable/src/RNTupleDraw6Provider.cxx
+++ gui/browsable/src/RNTupleDraw6Provider.cxx
@@ -17,6 +17,7 @@
 
 #include "TClass.h"
 #include "TH1.h"
+#include "TMath.h"
 #include "TVirtualPad.h"
 
 #include "RFieldHolder.hxx"
@@ -38,12 +39,66 @@ using namespace ROOT::Experimental::Browsable;
 
 class RNTupleDraw6Provider : public RProvider {
 
-   template<typename T>
-   void FillHistogram(std::shared_ptr<ROOT::Experimental::RNTupleReader> &tuple, const std::string &field_name, TH1 *hist)
+   /** Test collected entries if it looks like integer values and one can use better binning */
+   TH1F *TestHistBuffer(TH1F *hist)
    {
+      auto len = hist->GetBufferLength();
+      auto buf = hist->GetBuffer();
+
+      if (!buf || (len < 5)) return hist;
+
+      Double_t min = buf[1], max = buf[1];
+      Bool_t is_integer = kTRUE;
+
+      for (Int_t n = 0; n < len; ++n) {
+         Double_t v = buf[2 + 2*n];
+         if (v > max) max = v;
+         if (v < min) min = v;
+         if (TMath::Abs(v - TMath::Nint(v)) > 1e-5) { is_integer = kFALSE; break; }
+      }
+
+      // special case when only integer values in short range - better binning
+      if (is_integer && (max-min < 100)) {
+         max += 2;
+         if (min > 1) min -= 2;
+         int npoints = TMath::Nint(max - min);
+         TH1F *h1 = new TH1F(hist->GetName(), hist->GetTitle(), npoints, min, max);
+         h1->SetDirectory(nullptr);
+         for (Int_t n = 0; n < len; ++n)
+            h1->Fill(buf[2 + 2*n], buf[1 + 2*n]);
+         delete hist;
+         return h1;
+      }
+
+      return hist;
+   }
+
+
+   template<typename T>
+   TH1 *FillHistogram(std::shared_ptr<ROOT::Experimental::RNTupleReader> &tuple, const std::string &field_name)
+   {
+      std::string title = "Drawing of RField "s + field_name.c_str();
+
+      auto h1 = new TH1F("hdraw", title.c_str(), 100, 0, 0);
+      h1->SetDirectory(nullptr);
+
+      auto bufsize = (h1->GetBufferSize() - 1) / 2;
+      int cnt = 0;
+      if (bufsize > 10) bufsize-=3; else bufsize = -1;
+
       auto view = tuple->GetView<T>(field_name);
-      for (auto i : tuple->GetEntryRange())
-         hist->Fill(view(i));
+      for (auto i : tuple->GetEntryRange()) {
+         h1->Fill(view(i));
+         if (++cnt == bufsize) {
+            h1 = TestHistBuffer(h1);
+            ++cnt;
+         }
+      }
+      if (cnt < bufsize)
+         h1 = TestHistBuffer(h1);
+
+      h1->BufferEmpty();
+      return h1;
    }
 
 public:
@@ -63,25 +118,18 @@ public:
          auto &field = tuple->GetDescriptor().GetFieldDescriptor(id);
          name.append(field.GetFieldName());
 
-         std::string title = "Drawing of RField "s + name;
-
-         auto h1 = new TH1F("hdraw", title.c_str(), 100, 0, 0);
-         h1->SetDirectory(nullptr);
+         TH1 *h1 = nullptr;
 
          if (field.GetTypeName() == "double"s)
-            FillHistogram<double>(tuple, name, h1);
+            h1 = FillHistogram<double>(tuple, name);
          else if (field.GetTypeName() == "float"s)
-            FillHistogram<float>(tuple, name, h1);
+            h1 = FillHistogram<float>(tuple, name);
          else if (field.GetTypeName() == "int"s)
-            FillHistogram<int>(tuple, name, h1);
+            h1 = FillHistogram<int>(tuple, name);
          else if (field.GetTypeName() == "std::int32_t"s)
-            FillHistogram<int32_t>(tuple, name, h1);
-         else {
-            delete h1;
-            return false;
-         }
+            h1 = FillHistogram<int32_t>(tuple, name);
 
-         h1->BufferEmpty();
+         if (!h1) return false;
 
          pad->GetListOfPrimitives()->Clear();
 
