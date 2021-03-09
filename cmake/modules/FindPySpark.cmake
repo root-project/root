4e598a4676d84197c4fa0b7bf1067f7881c54fab
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2021-03-09 12:15:07 +0100

    Build option for distributed RDF tests that use pyspark
    The option `dataframe_distpyspark` is superseded by `test_distrdf_pyspark`, to clarify that finding pyspark and its dependencies (like Java) during configuration is strictly needed only for the tests.
    
    Generally speaking, pyspark (and Java) are optional runtime dependencies that interest only users of the distributed RDataFrame python module with the Spark backend.

--- cmake/modules/FindPySpark.cmake
+++ cmake/modules/FindPySpark.cmake
@@ -9,8 +9,7 @@
 # 2. py4j
 #
 # Java is a hard requirement in pyspark but still doing `import pyspark` does not
-# report an error if it is not installed or found. For that reason we look for Java
-# on the system with REQUIRED flag.
+# report an error if it is not installed or found.
 #
 # On the other hand, py4j should be present on the system if pyspark was installed
 # either through pip, conda or via downloading the official binaries. Thus, we can
@@ -19,38 +18,43 @@
 # Only the main Python executable that is used in the ROOT build will be used to find pyspark.
 # This module sets the following variables
 #  PySpark_FOUND - system has pyspark and it is usable
+#  PySpark_DEPENDENCIES_READY - the environment could import pyspark and Java runtime was found
 #  PySpark_VERSION_STRING - pyspark version string
 
 message(STATUS "Looking for PySpark dependency: Java")
-find_package(Java 1.8 REQUIRED COMPONENTS Runtime)
-
-message(STATUS "Found Java ${Java_JAVA_EXECUTABLE}")
-message(STATUS "Java version ${Java_VERSION_STRING}")
-
-# Import pyspark using the main Python executable, print its version and path to the __init__.py file
-execute_process(
-    COMMAND ${PYTHON_EXECUTABLE_Development_Main}
-            -c "import re, pyspark; print(pyspark.__version__); print(re.compile('/__init__.py.*').sub('',pyspark.__file__))" 
-    RESULT_VARIABLE _PYSPARK_IMPORT_SUCCESS
-    OUTPUT_VARIABLE _PYSPARK_VALUES_OUTPUT
-    ERROR_VARIABLE _PYSPARK_ERROR_VALUE
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-if(_PYSPARK_IMPORT_SUCCESS EQUAL 0)
-    # Convert the process output into a list
-    string(REGEX REPLACE "\n" ";" _PYSPARK_VALUES ${_PYSPARK_VALUES_OUTPUT})
-    # Just in case there is unexpected output from the Python command.
-    list(GET _PYSPARK_VALUES -2 _PYSPARK_VERSION)
-    list(GET _PYSPARK_VALUES -1 PySpark_HOME)
-    string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" PySpark_VERSION_STRING "${_PYSPARK_VERSION}")
+if(PySpark_FIND_REQUIRED)
+    find_package(Java 1.8 REQUIRED COMPONENTS Runtime)
 else()
-    message(STATUS "Python package 'pyspark' could not be imported with ${PYTHON_EXECUTABLE_Development_Main}\n"
-                   "${_PYSPARK_ERROR_VALUE}"
-    )
+    find_package(Java 1.8 COMPONENTS Runtime)
 endif()
 
+if(Java_FOUND)
+    message(STATUS "Found Java ${Java_JAVA_EXECUTABLE}")
+    message(STATUS "Java version ${Java_VERSION_STRING}")
+
+    # Import pyspark using the main Python executable, print its version and path to the __init__.py file
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE_Development_Main} -c "import pyspark; print(pyspark.__version__)" 
+        RESULT_VARIABLE _PYSPARK_IMPORT_EXIT_STATUS
+        OUTPUT_VARIABLE _PYSPARK_VALUES_OUTPUT
+        ERROR_VARIABLE _PYSPARK_ERROR_VALUE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    # Exit status equal to zero means success
+    if(_PYSPARK_IMPORT_EXIT_STATUS EQUAL 0)
+        # Build the version string
+        string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" PySpark_VERSION_STRING "${_PYSPARK_VALUES_OUTPUT}")
+        # Signal to CMake that the environment could import pyspark and Java runtime was found
+        set(PySpark_DEPENDENCIES_READY TRUE)
+    else()
+        message(STATUS "Python package 'pyspark' could not be imported with ${PYTHON_EXECUTABLE_Development_Main}\n"
+                    "${_PYSPARK_ERROR_VALUE}"
+        )
+    endif()
+
 find_package_handle_standard_args(PySpark
-    REQUIRED_VARS PySpark_HOME
+    REQUIRED_VARS PySpark_DEPENDENCIES_READY
     VERSION_VAR PySpark_VERSION_STRING
 )
+endif()
