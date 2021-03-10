a32ce79154cf3d01e4a97def2685a38617c76545
Sergey Linev <S.Linev@gsi.de>
2021-03-10 15:24:43 +0100

    [metacling] add suggested clang-tidy nullptr checks #7426

--- core/metacling/src/TClingTypedefInfo.cxx
+++ core/metacling/src/TClingTypedefInfo.cxx
@@ -200,6 +200,8 @@ int TClingTypedefInfo::Size() const
    }
    clang::ASTContext &context = fDecl->getASTContext();
    const clang::TypedefNameDecl *td = llvm::dyn_cast<clang::TypedefNameDecl>(fDecl);
+   if (!td)
+      return 0; // should never happens
    clang::QualType qt = td->getUnderlyingType();
    if (qt->isDependentType()) {
       // The underlying type is dependent on a template parameter,
@@ -235,6 +237,8 @@ const char *TClingTypedefInfo::TrueName(const ROOT::TMetaUtils::TNormalizedCtxt 
    TTHREAD_TLS_DECL( std::string, truename);
    truename.clear();
    const clang::TypedefNameDecl *td = llvm::dyn_cast<clang::TypedefNameDecl>(fDecl);
+   if (!td)
+      return "(badcast)";
    clang::QualType underlyingType = td->getUnderlyingType();
    if (underlyingType->isBooleanType()) {
       return "bool";
@@ -242,7 +246,7 @@ const char *TClingTypedefInfo::TrueName(const ROOT::TMetaUtils::TNormalizedCtxt 
    const clang::ASTContext &ctxt = fInterp->getCI()->getASTContext();
    ROOT::TMetaUtils::GetNormalizedName(truename, ctxt.getTypedefType(td), *fInterp, normCtxt);
 
-   return truename.c_str();
+   return truename.c_str();  // NOLINT
 }
 
 ////////////////////////////////////////////////////////////////////////////////
