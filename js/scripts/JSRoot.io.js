2bffa1d3ae9bd788b531e3ab6ef4ae3e6f3af5fe
Sergey Linev <S.Linev@gsi.de>
2021-03-15 12:53:58 +0100

    [jsroot] 6.24 branch

--- js/scripts/JSRoot.io.js
+++ js/scripts/JSRoot.io.js
@@ -118,18 +118,18 @@ JSROOT.define(['rawinflate'], () => {
       /** @summary Get bytes size of the type */
       GetTypeSize: function(typname) {
          switch (typname) {
-            case jsrio.kBool: return 1;
-            case jsrio.kChar: return 1;
-            case jsrio.kShort: return 2;
-            case jsrio.kInt: return 4;
-            case jsrio.kLong: return 8;
-            case jsrio.kFloat: return 4;
-            case jsrio.kDouble: return 8;
+            case jsrio.kBool:
+            case jsrio.kChar:
             case jsrio.kUChar: return 1;
+            case jsrio.kShort:
             case jsrio.kUShort: return 2;
+            case jsrio.kInt:
+            case jsrio.kFloat:
             case jsrio.kUInt: return 4;
-            case jsrio.kULong: return 8;
-            case jsrio.kLong64: return 8;
+            case jsrio.kLong:
+            case jsrio.kDouble:
+            case jsrio.kULong:
+            case jsrio.kLong64:
             case jsrio.kULong64: return 8;
          }
          return -1;
@@ -493,12 +493,6 @@ JSROOT.define(['rawinflate'], () => {
             for (; i < n; ++i, o += 4)
                array[i] = view.getInt32(o);
             break;
-         case jsrio.kBits:
-         case jsrio.kUInt:
-            array = new Uint32Array(n);
-            for (; i < n; ++i, o += 4)
-               array[i] = view.getUint32(o);
-            break;
          case jsrio.kShort:
             array = new Int16Array(n);
             for (; i < n; ++i, o += 2)
@@ -529,6 +523,8 @@ JSROOT.define(['rawinflate'], () => {
             throw new Error('kDouble32 should not be used in readFastArray');
          case jsrio.kFloat16:
             throw new Error('kFloat16 should not be used in readFastArray');
+         // case jsrio.kBits:
+         // case jsrio.kUInt:
          default:
             array = new Uint32Array(n);
             for (; i < n; ++i, o += 4)
@@ -1944,7 +1940,7 @@ JSROOT.define(['rawinflate'], () => {
                if (buf.ntou1() === 1)
                   obj[this.name] = buf.readFastArray(obj[this.cntname], this.type - jsrio.kOffsetP);
                else
-                  obj[this.name] = new Array();
+                  obj[this.name] = [];
             };
             break;
          case jsrio.kOffsetP + jsrio.kChar:
@@ -2575,7 +2571,7 @@ JSROOT.define(['rawinflate'], () => {
       cs['TMap'] = function(buf, map) {
          if (!map._typename) map._typename = "TMap";
          map.name = "";
-         map.arr = new Array();
+         map.arr = [];
          const ver = buf.last_read_version;
          if (ver > 2) buf.classStreamer(map, "TObject");
          if (ver > 1) map.name = buf.readTString();
@@ -2718,7 +2714,7 @@ JSROOT.define(['rawinflate'], () => {
                if (arr.length === 3) nbits = parseInt(arr[2]);
                if (isNaN(nbits) || (nbits < 2) || (nbits > 32)) nbits = 32;
 
-               function parse_range(val) {
+               let parse_range = val => {
                   if (!val) return 0;
                   if (val.indexOf("pi") < 0) return parseFloat(val);
                   val = val.trim();
@@ -2737,9 +2733,12 @@ JSROOT.define(['rawinflate'], () => {
                element.fXmin = parse_range(arr[0]);
                element.fXmax = parse_range(arr[1]);
 
-               const bigint = (nbits < 32) ? (1 << nbits) : 0xffffffff;
-               if (element.fXmin < element.fXmax) element.fFactor = bigint / (element.fXmax - element.fXmin);
-               else if (nbits < 15) element.fXmin = nbits;
+               // avoid usage of 1 << nbits, while only works up to 32 bits
+               let bigint = ((nbits >= 0) && (nbits < 32)) ? Math.pow(2, nbits) : 0xffffffff;
+               if (element.fXmin < element.fXmax)
+                  element.fFactor = bigint / (element.fXmax - element.fXmin);
+               else if (nbits < 15)
+                  element.fXmin = nbits;
             }
          }
       }
