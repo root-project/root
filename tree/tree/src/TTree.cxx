3d9f27a32e5c9dd0f7f1960d2d5ce47d02a7f813
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-03-17 17:02:49 +0100

    [TTree][NFC] Improve TTree::Draw docs
    Clarify behavior of `selection`s that return arrays.

--- tree/tree/src/TTree.cxx
+++ tree/tree/src/TTree.cxx
@@ -3805,8 +3805,8 @@ Long64_t TTree::Draw(const char* varexp, const TCut& selection, Option_t* option
    return TTree::Draw(varexp, selection.GetTitle(), option, nentries, firstentry);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-/// Draw expression varexp for specified entries.
+/////////////////////////////////////////////////////////////////////////////////////////
+/// \brief Draw expression varexp for entries and objects that pass a (optional) selection.
 ///
 /// \return -1 in case of error or number of selected events in case of success.
 ///
@@ -3821,53 +3821,44 @@ Long64_t TTree::Draw(const char* varexp, const TCut& selection, Option_t* option
 ///                   (to create histograms in the 2, 3, and 4 dimensional case,
 ///                   see section "Saving the result of Draw to an histogram")
 ///
-///   Example:
-///    -  varexp = x     simplest case: draw a 1-Dim distribution of column named x
-///    -  varexp = sqrt(x)            : draw distribution of sqrt(x)
-///    -  varexp = x*y/z
-///    -  varexp = y:sqrt(x) 2-Dim distribution of y versus sqrt(x)
-///    -  varexp = px:py:pz:2.5*E  produces a 3-d scatter-plot of px vs py ps pz
-///               and the color number of each marker will be 2.5*E.
-///               If the color number is negative it is set to 0.
-///               If the color number is greater than the current number of colors
-///               it is set to the highest color number.The default number of
-///               colors is 50. see TStyle::SetPalette for setting a new color palette.
+///   Examples:
+///    - "x": the simplest case, it draws a 1-Dim histogram of column x
+///    - "sqrt(x)", "x*y/z": draw histogram with the values of the specified numerical expression across TTree events
+///    - "y:sqrt(x)": 2-Dim histogram of y versus sqrt(x)
+///    - "px:py:pz:2.5*E": produces a 3-d scatter-plot of px vs py ps pz
+///                        and the color number of each marker will be 2.5*E.
+///                        If the color number is negative it is set to 0.
+///                        If the color number is greater than the current number of colors
+///                        it is set to the highest color number. The default number of
+///                        colors is 50. See TStyle::SetPalette for setting a new color palette.
 ///
-///   Note that the variables e1, e2 or e3 may contain a selection.
-///   example, if e1= x*(y<0), the value histogrammed will be x if y<0
-///   and will be 0 otherwise.
-///
-///   The expressions can use all the operations and build-in functions
-///   supported by TFormula (See TFormula::Analyze), including free
-///   standing function taking numerical arguments (TMath::Bessel).
+///   The expressions can use all the operations and built-in functions
+///   supported by TFormula (see TFormula::Analyze()), including free
+///   functions taking numerical arguments (e.g. TMath::Bessel()).
 ///   In addition, you can call member functions taking numerical
-///   arguments. For example:
+///   arguments. For example, these are two valid expressions:
 ///   ~~~ {.cpp}
 ///       TMath::BreitWigner(fPx,3,2)
 ///       event.GetHistogram()->GetXaxis()->GetXmax()
 ///   ~~~
-///   Note: You can only pass expression that depend on the TTree's data
-///   to static functions and you can only call non-static member function
-///   with 'fixed' parameters.
 ///
 /// \param [in] selection is an expression with a combination of the columns.
-///   In a selection all the C++ operators are authorized.
+///   In a selection all usual C++ mathematical and logical operators are allowed.
 ///   The value corresponding to the selection expression is used as a weight
-///   to fill the histogram.
-///   If the expression includes only boolean operations, the result
-///   is 0 or 1. If the result is 0, the histogram is not filled.
-///   In general, the expression may be of the form:
-///   ~~~ {.cpp}
-///       value*(boolean expression)
-///   ~~~
-///   if boolean expression is true, the histogram is filled with
-///   a `weight = value`.
+///   to fill the histogram (a weight of 0 is equivalent to not filling the histogram).\n
+///   \n
 ///   Examples:
-///    -  selection1 = "x<y && sqrt(z)>3.2"
-///    -  selection2 = "(x+y)*(sqrt(z)>3.2)"
-///    -  selection1 returns a weight = 0 or 1
-///    -  selection2 returns a weight = x+y if sqrt(z)>3.2
-///                  returns a weight = 0 otherwise.
+///    - "x<y && sqrt(z)>3.2": returns a weight = 0 or 1
+///    - "(x+y)*(sqrt(z)>3.2)": returns a weight = x+y if sqrt(z)>3.2, 0 otherwise\n
+///   \n
+///   If the selection expression returns an array, it is iterated over in sync with the
+///   array returned by the varexp argument (as described below in "Drawing expressions using arrays and array
+///   elements"). For example, if, for a given event, varexp evaluates to
+///   `{1., 2., 3.}` and selection evaluates to `{0, 1, 0}`, the resulting histogram is filled with the value 2. For example, for each event here we perform a simple object selection:
+///   ~~~{.cpp}
+///   // Muon_pt is an array: fill a histogram with the array elements > 100 in each event
+///   tree->Draw('Muon_pt', 'Muon_pt > 100')
+///   ~~~
 ///
 /// \param [in] option is the drawing option.
 ///    - When an histogram is produced it can be any histogram drawing option
@@ -4164,7 +4155,7 @@ Long64_t TTree::Draw(const char* varexp, const TCut& selection, Option_t* option
 /// -  `Sum$(formula )`  : return the sum of the value of the elements of the
 ///     formula given as a parameter.  For example the mean for all the elements in
 ///     one entry can be calculated with: `Sum$(formula )/Length$(formula )`
-/// -  `Min$(formula )` : return the minimun (within one TTree entry) of the value of the
+/// -  `Min$(formula )` : return the minimum (within one TTree entry) of the value of the
 ///     elements of the formula given as a parameter.
 /// -  `Max$(formula )` : return the maximum (within one TTree entry) of the value of the
 ///     elements of the formula given as a parameter.
