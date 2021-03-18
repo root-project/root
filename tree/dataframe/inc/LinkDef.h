5515131aca1a2299a2c96568066b88e355a8b6c2
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-03-18 11:36:24 +0100

    [DF] Add RJittedAction to LinkDef file
    After the latest LLVM upgrade, changes in cling's symbol resolution
    logic cause some unresolved symbol failures in RDF jitting.
    
    Adding RJittedAction to the rootmap should help cling autoloading
    and work around the problem.

--- tree/dataframe/inc/LinkDef.h
+++ tree/dataframe/inc/LinkDef.h
@@ -21,6 +21,7 @@
 #pragma link C++ namespace ROOT::Detail::RDF;
 #pragma link C++ namespace ROOT::RDF;
 #pragma link C++ class ROOT::Internal::RDF::RActionBase-;
+#pragma link C++ class ROOT::Internal::RDF::RJittedAction-;
 #pragma link C++ class ROOT::Detail::RDF::RFilterBase-;
 #pragma link C++ class ROOT::Detail::RDF::RJittedFilter-;
 #pragma link C++ class ROOT::Detail::RDF::RDefineBase-;
