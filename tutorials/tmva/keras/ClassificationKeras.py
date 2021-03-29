5b1d1ce262b96ddf406e01980f19cb7fbea157df
James Carroll <eloquism@gmail.com>
2021-03-29 23:27:57 +0100

    Re-order parameters for curl

--- tutorials/tmva/keras/ClassificationKeras.py
+++ tutorials/tmva/keras/ClassificationKeras.py
@@ -28,7 +28,7 @@ factory = TMVA.Factory('TMVAClassification', output,
 
 # Load data
 if not isfile('tmva_class_example.root'):
-    call(['curl', '-O', 'http://root.cern.ch/files/tmva_class_example.root', '-L'])
+    call(['curl', '-L', '-O', 'http://root.cern.ch/files/tmva_class_example.root'])
 
 data = TFile.Open('tmva_class_example.root')
 signal = data.Get('TreeS')
