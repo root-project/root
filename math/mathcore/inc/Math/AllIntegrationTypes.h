bafe175ffe165477af777ddc0f132ed7525b46fd
Olivier Couet <olivier.couet@cern.ch>
2021-03-30 16:05:58 +0200

    Doxygen fixes in integrator classes

--- math/mathcore/inc/Math/AllIntegrationTypes.h
+++ math/mathcore/inc/Math/AllIntegrationTypes.h
@@ -17,7 +17,6 @@
 #define ROOT_Math_AllIntegrationTypes
 
 
-
 namespace ROOT {
 namespace Math {
 
@@ -25,45 +24,33 @@ namespace Math {
     // type of integration
 
 
-
     //for 1-dim integration
   namespace IntegrationOneDim {
 
-
-    /**
-     enumeration specifying the integration types.
-     <ul>
-         <li>kDEFAULT: default type specifiend in the static options
-         <li>kGAUSS: simple Gauss integration method with fixed rule
-         <li>kLEGENDRE: Gauss-Legendre integration
-     <li>kNONADAPTIVE : to be used for smooth functions
-     <li>kADAPTIVE : to be used for general functions without singularities.
-     <li>kADAPTIVESINGULAR: default adaptive integration type which can be used in the case of the presence of singularities.
-     </ul>
-     @ingroup Integration
-    */
-     enum Type { kDEFAULT = -1, kGAUSS, kLEGENDRE, kADAPTIVE, kADAPTIVESINGULAR, kNONADAPTIVE};
-
+     /// enumeration specifying the integration types.
+     /// @ingroup Integration
+     enum Type {
+        kDEFAULT = -1,     ///< default type specifiend in the static options
+        kGAUSS,            ///< simple Gauss integration method with fixed rule
+        kLEGENDRE,         ///< Gauss-Legendre integration
+        kADAPTIVE,         ///< to be used for general functions without singularities
+        kADAPTIVESINGULAR, ///< default adaptive integration type which can be used in the case of the presence of singularities.
+        kNONADAPTIVE       ///< to be used for smooth functions
+     };
   }
 
     //for multi-dim integration
   namespace IntegrationMultiDim {
 
-
-    /**
-     enumeration specifying the integration types.
-     <ul>
-     <li>kDEFAULT  : default type specified in the static option
-     <li>kADAPTIVE : adaptive multi-dimensional integration
-     <li>kPLAIN    MC integration
-     <li>kMISER    MC integration
-     <li>kVEGAS    MC integration
-     </ul>
-     @ingroup MCIntegration
-     */
-
-     enum Type {kDEFAULT = -1, kADAPTIVE, kVEGAS, kMISER, kPLAIN};
-
+     /// enumeration specifying the integration types.
+     /// @ingroup MCIntegration
+     enum Type {
+        kDEFAULT = -1, ///< default type specified in the static option
+        kADAPTIVE,     ///< adaptive multi-dimensional integration
+        kVEGAS,        ///< MC integration
+        kMISER,        ///< MC integration
+        kPLAIN         ///< MC integration
+     };
   }
 
 
