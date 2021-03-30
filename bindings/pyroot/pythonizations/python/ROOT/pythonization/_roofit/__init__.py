8d40b5686c64fd80461ebd325729ead5a15d0929
Jonas Rembser <jonas.rembser@cern.ch>
2021-03-31 01:31:23 +0200

    [RF] Refactor RooFit pythonization code to reflect inheritance hierachy
    The unit test `rooabsreal_ploton` does not work. The problem is that the
    `RooAbsReal::plotOn` pythonization only applies to `RooAbsReal`, but it
    did not apply to the override in `RooAbsPdf`.
    
    To solve this problem, the pythinization functions are now organized in
    classes that reflect the original inheritance hierachy in RooFit.

--- bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/__init__.py
+++ bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/__init__.py
@@ -1,2 +1,67 @@
-from ._roopythonize import pythonize_roofit
+# Authors:
+# * Harshal Shende  03/2021
+# * Hinnerk C. Schmidt 02/2021
+# * Jonas Rembser 03/2021
 
+################################################################################
+# Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.                      #
+# All rights reserved.                                                         #
+#                                                                              #
+# For the licensing terms see $ROOTSYS/LICENSE.                                #
+# For the list of contributors see $ROOTSYS/README/CREDITS.                    #
+################################################################################
+
+import cppyy
+
+from ROOT import pythonization
+
+from ._rooabsdata import RooAbsData
+from ._rooabspdf import RooAbsPdf
+from ._rooabsreal import RooAbsReal
+from ._rooworkspace import RooWorkspace
+
+
+# list of python classes that are used to pythonize RooFit classes
+python_classes = [RooAbsData, RooAbsPdf, RooAbsReal, RooWorkspace]
+
+# create a dictionary for convenient access to python classes
+python_classes_dict = dict()
+for python_class in python_classes:
+    python_classes_dict[python_class.__name__] = python_class
+
+
+def ismagicfunc(name):
+    return name.startswith("__") and name.endswith("__")
+
+
+@pythonization()
+def pythonize_roofit(klass, name):
+    # Parameters:
+    # klass: class to pythonize
+    # name: string containing the name of the class
+
+    if not name.startswith("Roo"):
+        return
+
+    if not name in python_classes_dict:
+        return
+
+    python_klass = python_classes_dict[name]
+
+    # list of functions to pythonize, which are assumed to be all functions in
+    # the pyton classes that are not magic functions
+    func_names = [f for f in dir(python_klass) if not ismagicfunc(f)]
+
+    for func_name in func_names:
+
+        # if the RooFit class already has a function with the same name as our
+        # pythonization, we rename it and prefix it with an underscore
+        if hasattr(klass, func_name):
+            # new name for original function
+            func_name_orig = "_" + func_name
+
+            setattr(klass, func_name_orig, getattr(klass, func_name))
+
+        setattr(klass, func_name, getattr(python_klass, func_name))
+
+    return
