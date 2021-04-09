ad9f4f4c1bdc53c53be3f5726b9ef171608ff6cc
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-04-09 13:30:12 +0200

    Style changes as proposed by @couet

--- tutorials/graphics/schroedinger_hydrogen.C
+++ tutorials/graphics/schroedinger_hydrogen.C
@@ -4,55 +4,60 @@
 /// Plot the Amplitude of a Hydrogen Atom.
 ///
 /// Visualize the Amplitude of a Hydrogen Atom in the n = 2, l = 0, m = 0 state.
-/// Demonstrates how TH2F and TGraph2D can be used in Quantum Mechanics.
+/// Demonstrates how TH2F can be used in Quantum Mechanics.
 ///
 /// \macro_image
 /// \macro_code
-/// 
+///
 /// \author Advait Dhingra
 
 #include <cmath>
 
 double WaveFunction(double x, double y) {
-    double r = sqrt(x *x + y*y);
+   double r = sqrt(x * x + y * y);
 
-    double w = (1/pow((4*sqrt(2*TMath::Pi())* 1), 1.5)) * (2 - (r / 1)*pow(TMath::E(), (-1 * r)/2)); // Wavefunction formula for psi 2,0,0
+   double w = (1 / pow((4 * sqrt(2 * TMath::Pi()) * 1), 1.5)) *
+              (2 - (r / 1) * pow(TMath::E(), (-1 * r) / 2)); // Wavefunction formula for psi 2,0,0
 
-    return w*w; // amplitude
-
+   return w * w; // Amplitude
 }
 
 void schroedinger_hydrogen() {
-	   TH2F *h2D = new TH2F("Hydrogen Atom",
+   TH2F *h2D = new TH2F("Hydrogen Atom",
                         "#Psi^{2}_{200} i.e. n = 2, l = 0, m = 0; Position in x direction; Position in y direction",
-                        1000, -10, 10, 1000, -10, 10); // for 2D view
-
-   TGraph2D *h3D = new TGraph2D(); // for 3D view
-   h3D->SetTitle("3D view of probability amplitude");
-   
+                        200, -10, 10, 200, -10, 10);
 
    for (float i = -10; i < 10; i += 0.01) {
       for (float j = -10; j < 10; j += 0.01) {
          h2D->Fill(i, j, WaveFunction(i, j));
-         h3D->SetPoint(h3D->GetN(), i, j, WaveFunction(i, j));
       }
-    }
+   }
 
-    gStyle->SetPalette(kDarkBodyRadiator);
-    gStyle->SetOptStat(0);
-    gStyle->SetLabelSize(0.02, "Z");
-    
-    TCanvas *c1 = new TCanvas("c1", "Schroedinger's Hydrogen Atom", 1500, 750);
-    c1->Divide(2, 1);
+   gStyle->SetPalette(kCividis);
+   gStyle->SetOptStat(0);
 
-    c1->cd(1);
-    h2D->Draw("colz");
-    
-    TLatex *l = new TLatex(-10, -12.5, "#scale[0.4]{The Electron is more likely to be found in the yellow areas and less likely to be found in the dark areas.}");
-    l->Draw();
-	
-    c1->cd(2);
-    
-    h3D->Draw("surf1");
+   TCanvas *c1 = new TCanvas("c1", "Schroedinger's Hydrogen Atom", 1500, 750);
+   c1->Divide(2, 1);
+
+   auto c1_1 = c1->cd(1);
+   c1_1->SetRightMargin(0.14);
+   h2D->GetXaxis()->SetLabelSize(0.03);
+   h2D->GetYaxis()->SetLabelSize(0.03);
+   h2D->GetZaxis()->SetLabelSize(0.03);
+   h2D->SetContour(50);
+   h2D->Draw("colz");
+
+   TLatex *l = new TLatex(
+      -10, -12.43,
+      "The Electron is more likely to be found in the yellow areas and less likely to be found in the blue areas.");
+   l->SetTextFont(42);
+   l->SetTextSize(0.02);
+   l->Draw();
+
+   auto c1_2 = c1->cd(2);
+   c1_2->SetTheta(42.);
+
+   TH2D *h2Dc = (TH2D *)h2D->Clone();
+   h2Dc->SetTitle("3D view of probability amplitude;;");
+   h2Dc->Draw("surf2");
 }
-
