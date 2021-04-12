bae22ddd368bcd16686bbb9dc1351e27f374c0eb
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-04-12 18:43:38 +0200

    small fix

--- graf2d/feynman/src/TFeynman.cxx
+++ graf2d/feynman/src/TFeynman.cxx
@@ -15,19 +15,18 @@ of the Canvas that you would like.
 You can access the particle classes using an arrow pointer.
 This example plots the feynman.C diagram in the tutorials:
 ~~~
-  f->Lepton(10, 10, 30, 30, 7, 6, "e", true);
-  f->Lepton(10, 50, 30, 30, 5, 55, "e", false);
-  f->CurvedPhoton(30, 30, 12.5*sqrt(2), 135, 225, 7, 30);
-  f->Photon(30, 30, 55, 30, 42.5, 37.7);
-  f->QuarkAntiQuark(70, 30, 15, 55, 45, "q");
-  f->Gluon(70, 45, 70, 15, 77.5, 30);
-  f->WeakBoson(85, 30, 110, 30, 100, 37.5, "Z^{0}");
-  f->Quark(110, 30, 130, 50, 135, 55, "q", true);
-  f->Quark(110, 30, 130, 10, 135, 6, "q", false);
-  f->CurvedGluon(110, 30, 12.5*sqrt(2), 315, 45, 135, 30);
+  f->Lepton(10, 10, 30, 30, 7, 6, "e", true)->Draw();
+  f->Lepton(10, 50, 30, 30, 5, 55, "e", false)->Draw();
+  f->CurvedPhoton(30, 30, 12.5*sqrt(2), 135, 225, 7, 30)->Draw();
+  f->Photon(30, 30, 55, 30, 42.5, 37.7)->Draw();
+  f->QuarkAntiQuark(70, 30, 15, 55, 45, "q")->Draw();
+  f->Gluon(70, 45, 70, 15, 77.5, 30)->Draw();
+  f->WeakBoson(85, 30, 110, 30, 100, 37.5, "Z^{0}")->Draw();
+  f->Quark(110, 30, 130, 50, 135, 55, "q", true)->Draw();
+  f->Quark(110, 30, 130, 10, 135, 6, "q", false)->Draw();
+  f->CurvedGluon(110, 30, 12.5*sqrt(2), 315, 45, 135, 30)->Draw();
 ~~~
-No drawing is needed, as this is all done in the method itself. 
-One can simply use the methods like Legos.
+
 */
 
 #include "../inc/TFeynman.h"
