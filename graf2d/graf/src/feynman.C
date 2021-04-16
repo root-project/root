1b8cd588709a0d722b991dd42ee082642eca4224
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-04-16 13:51:10 +0200

    Small fix to test file

--- graf2d/graf/src/feynman.C
+++ graf2d/graf/src/feynman.C
@@ -1,11 +1,13 @@
 #include "TFeynman.h"
 
-void test()
+void feynman()
 {
     TCanvas *c1 = new TCanvas();
     TFeynman *f = new TFeynman();
 
     f->AddItem("fermion", 10, 10, 30, 30);
+
+    f->Paint();
 
     f->Draw();
 }
