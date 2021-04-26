d6cd359341286eca4e10f67830cf0218e80826eb
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-04-26 10:56:57 +0200

    [Doc][NFC] Add initial tutorials for DF and RVec linking to doxygen
    The idea is to catch people looking at DF or VecOps/RVec and remember
    them of the exhaustive documentation on doxygen.
    
    Fixes #7700
    Fixes #7701

---
+++ tutorials/dataframe/df000_links.C
@@ -0,0 +1,12 @@
+/// \file
+/// \ingroup tutorial_dataframe
+/// \notebook -nodraw
+/// Summary of RDataFrame documentation.
+///
+/// You have found the tutorials showing specific features of RDataFrame with code!
+///
+/// Please note that you can find the exhaustive documentation here:
+/// https://root.cern/doc/master/classROOT_1_1RDataFrame.html
+///
+/// \date April 2021
+/// \author Stefan Wunsch (KIT, CERN)
---
+++ tutorials/vecops/vo000_links.C
@@ -0,0 +1,12 @@
+/// \file
+/// \ingroup tutorial_vecops
+/// \notebook -nodraw
+/// Summary of VecOps and RVec documentation.
+///
+/// You have found the tutorials showing specific features of VecOps and RVec with code!
+///
+/// Please note that you can find the exhaustive documentation here:
+/// https://root.cern/doc/master/classROOT_1_1VecOps_1_1RVec.html
+///
+/// \date April 2021
+/// \author Stefan Wunsch (KIT, CERN)
