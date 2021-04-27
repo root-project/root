bea79f15497db64e5173985d4b590b4dd5976543
AdvaitDhingra <advait.dhingra1006@gmail.com>
2021-04-27 11:25:23 +0200

    Better example

--- graf2d/graf/src/TFeynmanEntry.cxx
+++ graf2d/graf/src/TFeynmanEntry.cxx
@@ -11,7 +11,7 @@
 
 #include <cstdio>
 #include <iostream>
-#include "TFeynmanEntry.h"
+#include "../inc/TFeynmanEntry.h"
 
 ClassImp(TFeynmanEntry);
 
--- graf2d/graf/src/feynman.C
+++ graf2d/graf/src/feynman.C
@@ -1,5 +1,5 @@
-#include "TFeynman.h"
-#include "TFeynmanEntry.h"
+#include "TFeynman.cxx"
+#include "TFeynmanEntry.cxx"
 
 void feynman()
 {
@@ -7,7 +7,7 @@ void feynman()
   TFeynman *f = new TFeynman();
 
   // proton decay (beta minus)
-  f->AddPair("q", 50, 30, 6);
+  //f->AddPair("q", 50, 30, 6);
   f->AddItem("fermion", 10, 10, 30, 30, 5, 6, "d");
   f->AddItem("fermion", 30, 30, 10, 50, 5, 50, "d");
   f->AddItem("fermion", 15, 10, 35, 30, 10, 6, "u");
@@ -17,6 +17,7 @@ void feynman()
   f->AddItem("boson", 40, 30, 70, 30, 55, 35, "W^{+}");
   f->AddItem("anti-fermion", 70, 30, 90, 50, 95, 55, "e^{+}");
   f->AddItem("fermion", 70, 30, 90, 10, 85, 5, "#bar{#nu}");
-  f->AddCurved("e^{-}", 50, 40, 12, 90, 275, true);
+  f->AddCurved("#gamma", 80, 30, 12, 275, 90, true);
+  f->AddPair("u", 55, 30, 10);
   f->Draw();
 }
