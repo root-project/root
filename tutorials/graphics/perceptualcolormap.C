e0b7da81ed9f2b264b6437820b5ca2eb6f3c67cd
Olivier Couet <olivier.couet@cern.ch>
2021-05-11 14:45:18 +0200

    fix some typos (#8105)

--- tutorials/graphics/perceptualcolormap.C
+++ tutorials/graphics/perceptualcolormap.C
@@ -41,7 +41,7 @@ void Perceptual_Colormap(TH2D *h, Double_t val_cut) {
 
    TColor::CreateGradientColorTable(Number,Stops,Red,Green,Blue,nb);
 
-   // Histogram drawaing
+   // Histogram drawing
    h->Draw("colz");
 }
 
