b24bc7d23656b9d4459254eae7e33eaf52e8b225
Sergey Linev <S.Linev@gsi.de>
2021-05-11 14:19:50 +0200

    [doxygen] integrate JSON code into macro HTML code
    No need for extra reading of JSON files from doxygen HTML

--- documentation/doxygen/makejsonfile.C
+++ documentation/doxygen/makejsonfile.C
@@ -24,6 +24,13 @@ void makejsonfile(const char *MacroName, const char *IN, const char *OutDir, boo
    TString json_file = TString::Format("%s/html/%s.json",OutDir,IN);
    canv->SaveAs(json_file.Data());
 
+   std::string json_str;
+   {
+       std::ifstream fjson(json_file.Data());
+      json_str = std::string((std::istreambuf_iterator<char>(fjson)), std::istreambuf_iterator<char>());
+   }
+   gSystem->Unlink(json_file.Data());
+
    // Build the html file inlining the json picture
    FILE *fh = fopen(TString::Format("%s/macros/%s.html",OutDir,IN), "w");
    fprintf(fh,"<div id=\"draw_json_%s\" style=\"width:700px; height:500px\"></div>\n", IN);
@@ -39,8 +46,9 @@ void makejsonfile(const char *MacroName, const char *IN, const char *OutDir, boo
    fprintf(fh,"   }\n");
    fprintf(fh,"   load_jsroot_%s().then(() => { \n", IN);
    fprintf(fh,"      JSROOT.settings.HandleKeys = false;\n");
-   fprintf(fh,"      return JSROOT.httpRequest('./%s.json','object');\n", IN);
-   fprintf(fh,"   }).then(obj => JSROOT.draw('draw_json_%s', obj));\n", IN);
+   fprintf(fh,"      let obj = JSROOT.parse(%s);\n", json_str.c_str());
+   fprintf(fh,"      JSROOT.draw('draw_json_%s', obj);\n", IN);
+   fprintf(fh,"   });\n");
    fprintf(fh,"</script>\n");
    fclose(fh);
 }
