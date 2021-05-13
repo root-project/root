aa2eb1408b0ce3223902c5271f8fd89ad998a5e2
Hans Dembinski <hans.dembinski@gmail.com>
2021-05-13 23:05:58 +0200

    fix: properly set fReachedCallLimit status

--- math/minuit2/inc/Minuit2/FunctionMinimum.h
+++ math/minuit2/inc/Minuit2/FunctionMinimum.h
@@ -64,8 +64,8 @@ public:
       fPtr->fStates.push_back(state);
       // LM : update also the user state
       fPtr->fUserState = MnUserParameterState(State(), Up(), Seed().Trafo());
-      // reset maxedm flag. If new state has edm over max other method must be used
       fPtr->fAboveMaxEdm = status == MnAboveMaxEdm;
+      fPtr->fReachedCallLimit = status == MnReachedCallLimit;
    }
 
    const MinimumSeed &Seed() const { return fPtr->fSeed; }
