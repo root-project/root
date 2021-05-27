679e0bfc5a794467a47ab6dcb093226bdc8ec745
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-05-27 10:19:39 +0200

    [VecOps][NFC] Update year in a copyright notice

--- math/vecops/inc/LinkDef.h
+++ math/vecops/inc/LinkDef.h
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2021, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * Author: Danilo Piparo - CERN                                          *
