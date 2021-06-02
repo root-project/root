9228c266830d2b25af669dce6a178a7a70d5c9ab
Sergey Linev <S.Linev@gsi.de>
2021-06-02 12:16:11 +0200

    Fix warning about strcpy in TGTextLine constructor

--- gui/gui/src/TGText.cxx
+++ gui/gui/src/TGText.cxx
@@ -71,8 +71,7 @@ TGTextLine::TGTextLine(const char *string)
    if (string) {
       fLength = strlen(string);
       fString = new char[fLength+1];
-      strncpy(fString, string, fLength);
-      fString[fLength] = 0;
+      strlcpy(fString, string, fLength+1);
    } else {
       fLength = 0;
       fString = 0;
