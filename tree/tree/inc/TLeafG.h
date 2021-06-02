1d28dff2b7277163df214d89ca8c206df74e7712
Philippe Canal <pcanal@fnal.gov>
2021-06-02 17:49:27 -0500

    Always accept to return the serialized buffer.
    The user can always find a way to deserialize it. It might be as simple as byte swapping or might requires
    to call a full StreamerInfoAction sequence.

--- tree/tree/inc/TLeafG.h
+++ tree/tree/inc/TLeafG.h
@@ -53,7 +53,6 @@ public:
    virtual void    ReadBasket(TBuffer &b);
    virtual void    ReadBasketExport(TBuffer &b, TClonesArray *list, Int_t n);
    virtual bool    ReadBasketFast(TBuffer&, Long64_t);
-   virtual bool    ReadBasketSerialized(TBuffer&, Long64_t) { return GetDeserializeType() > DeserializeType::kExternal; }
    virtual void    ReadValue(std::istream& s, Char_t delim = ' ');
    virtual void    SetAddress(void *add=0);
    virtual void    SetMaximum(Long_t max) {fMaximum = max;}
