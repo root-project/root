166900632fc1d4048bd835da328ec79097f0a0da
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2021-06-02 20:03:31 +0200

    [RF] New UniqueId class to add unique ID class member to any class
    The new templated UniqueId class can be used in any class to get a
    unique ID class member. The uniqueness of the ID is achieved by
    increasing a static counter everytime a UniqueId is constructed.
    
    This purpose of unique IDs in RooFit is to replace object
    identification by memory address, which can give wrong results if memory
    addresses are reused for different objects.

---
+++ roofit/roofitcore/inc/UniqueId.h
@@ -0,0 +1,72 @@
+// Author: Jonas Rembser, CERN, Jun 2021
+
+/// A UniqueId can be added as a class member to enhance any class with a
+/// unique identifier for each instantiated object.
+///
+/// Example:
+/// ~~~{.cpp}
+/// class MyClass {
+///
+/// public:
+///    /// Return unique ID by reference.
+///    /// Please always use the name `uniqueId` for the getter.
+///    UniqueId<MyClass> const& uniqueId() const { return _uniqueId; }
+///
+/// private:
+///    const UniqueId<MyClass> _uniqueId; //! should be non-persistent
+///
+/// };
+/// ~~~
+
+#ifndef roofit_roofitcore_UniqueId_h
+#define roofit_roofitcore_UniqueId_h
+
+#include <atomic>
+
+template <class Class>
+struct UniqueId {
+public:
+  using Value_t = unsigned long;
+
+  /// Create a new UniqueId with the next value from the static counter.
+  UniqueId() : _val{++counter} {}
+
+  // Disable all sorts of copying and moving to ensure uniqueness.
+  UniqueId(const UniqueId &) = delete;
+  UniqueId &operator=(const UniqueId &) = delete;
+  UniqueId(UniqueId &&) = delete;
+  UniqueId &operator=(UniqueId &&) = delete;
+
+  /// Return numerical value of ID.
+  /// Use only if necessary, as the UniqueId type information is lost and
+  /// copying/moving is not prohibited for the value type.
+  /// Please don't turn this into a cast operator, as a function with an
+  /// explicit name is easier to track in the codebase.
+  constexpr Value_t value() const { return _val; }
+
+  bool operator==(UniqueId const &other) const { return _val == other._val; }
+  bool operator<(UniqueId const &other) const { return _val < other._val; }
+
+  static const UniqueId nullid;  /// A value that is less than the ID of any object (similar to nullptr).
+
+private:
+  Value_t _val;  /// Numerical value of the ID.
+
+  static std::atomic<Value_t> counter;  /// The static object counter to get the next ID value.
+};
+
+template <class Class>
+std::atomic<typename UniqueId<Class>::Value_t> UniqueId<Class>::counter{0UL};
+
+template <class Class>
+const UniqueId<Class> UniqueId<Class>::nullid{0UL};
+
+/// A helper function to replace pointer comparisons with UniqueId comparisons.
+/// With pointer comparisons, we can also have `nullptr`. In the UniqueId case,
+/// this translates to the `nullid`.
+template <class Class>
+UniqueId<Class> getUniqueId(Class const *ptr) {
+  return ptr ? ptr->uniqueId() : UniqueId<Class>::nullid;
+}
+
+#endif
