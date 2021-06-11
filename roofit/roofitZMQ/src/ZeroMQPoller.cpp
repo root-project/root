2bddfa40876fbe257d2db621c870e98e3c86bb0f
E. G. Patrick Bos <egpbos@gmail.com>
2021-06-11 11:43:14 +0200

    add ZeroMQPoller::ppoll implementation
    Should have been added in ppoll commit before.

--- roofit/roofitZMQ/src/ZeroMQPoller.cpp
+++ roofit/roofitZMQ/src/ZeroMQPoller.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 
+#include "RooFit_ZMQ/ppoll.h"
 #include "RooFit_ZMQ/ZeroMQPoller.h"
 
 std::vector<std::pair<size_t, int>> ZeroMQPoller::poll(int timeo) {
@@ -47,6 +48,36 @@ std::vector<std::pair<size_t, int>> ZeroMQPoller::poll(int timeo) {
   }
   return r;
 }
+
+// This function can throw (from inside ZMQ::ppoll), so wrap in try-catch!
+std::vector<std::pair<size_t, int>> ZeroMQPoller::ppoll(int timeo, const sigset_t * sigmask_) {
+   if (m_items.empty()) {
+      throw std::runtime_error("No sockets registered");
+   }
+
+   std::vector<std::pair<size_t, int>> r;
+
+   auto n = ZMQ::ppoll(m_items, timeo, sigmask_);
+   if (n == 0) return r;
+
+   for (auto& m_item : m_items) {
+      size_t index = 0;
+      int flags = 0;
+      if (m_item.socket == nullptr) {
+         // an fd was registered
+         std::tie(index, flags) = m_fds[m_item.fd];
+      } else {
+         // a socket was registered
+         const zmq::socket_t* s;
+         std::tie(index, flags, s) = m_sockets[m_item.socket];
+      }
+      if (m_item.revents & short(flags)) {
+         r.emplace_back(index, flags);
+      }
+   }
+   return r;
+}
+
 
 size_t ZeroMQPoller::size() const {
   return m_items.size();
