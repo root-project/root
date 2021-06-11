398a179ed525f438db226f2b12d253176b6aecbc
Sitong An <s.an@cern.ch>
2021-06-11 11:59:21 +0200

    [TMVA SOFIE] Fast Inference Code Generation Initial Commit (#7544)
    * initial commit for tmva-sofie fast inference code generation system.
    
    * Fix cmake issues for tmva-sofie. Specifically, fix cmake issues related to protobuf
    
    * Clean up cmake files
    
    * fix DeepCode memory leak commplaint, change unidirectional_broadcast to return a T*
    
    * fix DeepCode ofstream warning
    
    * fix Deepcode std ector empty warning
    
    * fix cmake comments for sofie pr
    
    * fix Cmake file include_dir
    
    * add build option
    
    * add Rmodel.hxx sstream include
    
    * Add readme
    
    * Update and test sofie cmake as per Oksana's request
    
    * remove empty line
    
    * remove NO_CXXMODULE line

---
+++ tmva/sofie/inc/TMVA/RModelParser_ONNX.hxx
@@ -0,0 +1,56 @@
+#ifndef TMVA_SOFIE_RMODELPARSER_ONNX
+#define TMVA_SOFIE_RMODELPARSER_ONNX
+
+
+
+#include "SOFIE_common.hxx"
+#include "RModel.hxx"
+#include "OperatorList.hxx"
+
+#include <string>
+#include <fstream>
+#include <memory>
+#include <ctime>
+
+//forward delcaration
+namespace onnx{
+   class NodeProto;
+   class GraphProto;
+}
+
+namespace TMVA{
+namespace Experimental{
+namespace SOFIE{
+
+namespace INTERNAL{
+
+std::unique_ptr<ROperator> make_ROperator_Transpose(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
+std::unique_ptr<ROperator> make_ROperator_Relu(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
+std::unique_ptr<ROperator> make_ROperator_Gemm(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
+
+
+using factoryMethodMap = std::unordered_map<std::string, std::unique_ptr<ROperator> (*)(const onnx::NodeProto&, const onnx::GraphProto&, std::unordered_map<std::string, ETensorType>&)>;
+const factoryMethodMap mapOptypeOperator = {
+      {"Gemm", &make_ROperator_Gemm},
+      {"Transpose", &make_ROperator_Transpose},
+      {"Relu", &make_ROperator_Relu}
+   };
+
+
+std::unique_ptr<ROperator> make_ROperator(size_t idx, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
+}//INTERNAL
+
+
+
+class RModelParser_ONNX{
+public:
+   RModel Parse(std::string filename);
+};
+
+
+
+}//SOFIE
+}//Experimental
+}//TMVA
+
+#endif //TMVA_SOFIE_RMODELPARSER_ONNX
