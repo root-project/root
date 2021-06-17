d431b8070a20c1a0e800560ebb1b950e54a9d1e4
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-06-17 11:25:26 +0200

    [IO] Remove deprecated spelling Experimental::TBufferMerger from LinkDef

--- io/io/inc/LinkDef.h
+++ io/io/inc/LinkDef.h
@@ -51,8 +51,6 @@
 #pragma link C++ class TStreamerInfoActions::TActionSequence+;
 #pragma link C++ class TStreamerInfoActions::TConfiguration-;
 #pragma link C++ class ROOT::Internal::RRawFile+;
-#pragma link C++ class ROOT::Experimental::TBufferMerger;
-#pragma link C++ class ROOT::Experimental::TBufferMergerFile;
 #pragma link C++ class ROOT::TBufferMerger;
 #pragma link C++ class ROOT::TBufferMergerFile;
 
