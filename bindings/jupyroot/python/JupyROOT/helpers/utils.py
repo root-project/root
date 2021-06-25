19afa218fe55535596c668b91863791b5e4b9bfe
Sergey Linev <S.Linev@gsi.de>
2021-06-25 11:17:53 +0200

    [jupyter] adopt changes in utility.py to correctly load JSROOT
    Try with notebook and with lab local version and then remote version

--- bindings/jupyroot/python/JupyROOT/helpers/utils.py
+++ bindings/jupyroot/python/JupyROOT/helpers/utils.py
@@ -47,14 +47,28 @@ _jsCode = """
      style="width: {jsCanvasWidth}px; height: {jsCanvasHeight}px">
 </div>
 <script>
-if (typeof require !== 'undefined') {{
+
+function display_{jsDivId}(Core) {{
+   let obj = Core.JSONR_unref({jsonContent});
+   Core.draw("{jsDivId}", obj, "{jsDrawOptions}");
+}}
+
+function script_load_{jsDivId}(src, on_error) {{
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = function() {{ console.log("loading done", script.src); display_{jsDivId}(JSROOT); }};
+    script.onerror = function() {{ script.remove(); on_error(); }};
+    document.head.appendChild(script);
+}}
+
+if (typeof requirejs !== 'undefined') {{
 
     // We are in jupyter notebooks, use require.js which should be configured already
-    require(['scripts/JSRootCore'],
-        function(Core) {{
-           display_{jsDivId}(Core);
-        }}
-    );
+    requirejs.config({{
+       paths: {{ 'JSRootCore' : [ 'scripts/JSRootCore', 'https://root.cern/js/5.9.1/scripts/JSRootCore.min', 'https://jsroot.gsi.de/5.9.1/scripts/JSRootCore.min' ] }}
+    }})(['JSRootCore'],  function(Core) {{
+       display_{jsDivId}(Core);
+    }});
 
 }} else if (typeof JSROOT !== 'undefined') {{
 
@@ -72,30 +86,14 @@ if (typeof require !== 'undefined') {{
     }}
 
     // Try loading a local version of requirejs and fallback to cdn if not possible.
-    script_load(base_url + 'static/scripts/JSRootCore.js', script_success, function(){{
-        console.error('Fail to load JSROOT locally, please check your jupyter_notebook_config.py file')
-        script_load('https://root.cern/js/5.9.1/scripts/JSRootCore.min.js', script_success, function(){{
+    script_load_{jsDivId}(base_url + 'static/scripts/JSRootCore.js', function(){{
+        console.error('Fail to load JSROOT locally, please check your jupyter_notebook_config.py file');
+        script_load_{jsDivId}('https://root.cern/js/5.9.1/scripts/JSRootCore.min.js', function(){{
             document.getElementById("{jsDivId}").innerHTML = "Failed to load JSROOT";
         }});
     }});
 }}
 
-function script_load(src, on_load, on_error) {{
-    var script = document.createElement('script');
-    script.src = src;
-    script.onload = on_load;
-    script.onerror = on_error;
-    document.head.appendChild(script);
-}}
-
-function script_success() {{
-   display_{jsDivId}(JSROOT);
-}}
-
-function display_{jsDivId}(Core) {{
-   var obj = Core.JSONR_unref({jsonContent});
-   Core.draw("{jsDivId}", obj, "{jsDrawOptions}");
-}}
 </script>
 """
 
