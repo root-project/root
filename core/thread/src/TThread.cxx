e277e1cd46bcb02460533e37574af0df07417e68
ferdymercury <ferdymercury@users.noreply.github.com>
2021-06-28 16:43:10 +0200

    pass affinity setting

--- core/thread/src/TThread.cxx
+++ core/thread/src/TThread.cxx
@@ -565,7 +565,7 @@ Long_t TThread::SelfId()
 /// the arg argument. Returns 0 on success, otherwise an error number will
 /// be returned.
 
-Int_t TThread::Run(void *arg)
+Int_t TThread::Run(void *arg, const int affinity)
 {
    if (arg) fThreadArg = arg;
 
@@ -573,7 +573,7 @@ Int_t TThread::Run(void *arg)
    ThreadInternalLock();
    SetComment("Run: MainMutex locked");
 
-   int iret = fgThreadImp->Run(this);
+   int iret = fgThreadImp->Run(this, affinity);
 
    fState = iret ? kInvalidState : kRunningState;
 
