4c90e7dff974db178212ffa239edbd6d36ee955b
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2021-06-28 13:32:29 +0200

    [ntuple] Support load/store of class hierarchies via TClass
    Improve support of storage of arbitrary types via TClass by handling also
    members inherited from base classes. A subfield named `:XXX` is created for each
    inherited XXX class.
    
    As per a discussion in a previous I/O meeting, always use `CaptureValue()` in
    `ReadGlobalImpl()` (so that reading a new entry is roughly equal to copy
    assignment).

--- tree/ntuple/v7/inc/ROOT/RField.hxx
+++ tree/ntuple/v7/inc/ROOT/RField.hxx
@@ -290,8 +290,25 @@ public:
 /// The field for a class with dictionary
 class RClassField : public Detail::RFieldBase {
 private:
+   enum ESubFieldRole {
+      kBaseClass,
+      kDataMember,
+   };
+   struct RSubFieldInfo {
+      ESubFieldRole fRole;
+      std::size_t fOffset;
+   };
+   /// Prefix used in the subfield names generated for base classes
+   static constexpr const char *kPrefixInherited{":"};
+
    TClass* fClass;
+   /// Additional information kept for each entry in `fSubFields`
+   std::vector<RSubFieldInfo> fSubFieldsInfo;
    std::size_t fMaxAlignment = 1;
+
+private:
+   RClassField(std::string_view fieldName, std::string_view className, TClass *classp);
+   void Attach(std::unique_ptr<Detail::RFieldBase> child, RSubFieldInfo info);
 
 protected:
    std::unique_ptr<Detail::RFieldBase> CloneImpl(std::string_view newName) const final;
