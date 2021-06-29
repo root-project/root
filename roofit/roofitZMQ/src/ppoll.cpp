1fde1c3bef663d65836ac30f242744aa37da1167
E. G. Patrick Bos <egpbos@gmail.com>
2021-06-29 12:11:52 +0200

    remove ppoll copyright headers

--- roofit/roofitZMQ/src/ppoll.cpp
+++ roofit/roofitZMQ/src/ppoll.cpp
@@ -1,17 +1,3 @@
-/*
- * Project: RooFit
- * Authors:
- *   PB, Patrick Bos, Netherlands eScience Center, p.bos@esciencecenter.nl
- *
- * Copyright (c) 2016-2019, Netherlands eScience Center
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- */
-
 #include <RooFit_ZMQ/ppoll.h>
 
 #include <unistd.h> // pselect
