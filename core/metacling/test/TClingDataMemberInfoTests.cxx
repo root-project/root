488e214b3f558d523d7a1f6ec4afdc78c90cdcad
Sergey Linev <S.Linev@gsi.de>
2021-06-30 21:10:48 +0200

    [metacling] adjust test to change RBox class member

--- core/metacling/test/TClingDataMemberInfoTests.cxx
+++ core/metacling/test/TClingDataMemberInfoTests.cxx
@@ -234,7 +234,7 @@ TEST(TClingDataMemberInfo, issue8553)
    TClass *clBox = nullptr;
    ASSERT_TRUE(clBox = TClass::GetClass("ROOT::Experimental::RBox"));
    TDataMember *dmAttrBorder = nullptr;
-   ASSERT_TRUE(dmAttrBorder = clBox->GetDataMember("fAttrBorder"));
+   ASSERT_TRUE(dmAttrBorder = clBox->GetDataMember("border"));
    EXPECT_NE(dmAttrBorder->GetOffsetCint(), 0);
 }
 #endif
