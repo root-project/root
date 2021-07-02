47c8e7dfc1fbf221e037fd5d3bd367f947b0b9bf
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-07-02 15:46:59 +0200

    Added Pi Infinite Series tutorial

---
+++ tutorials/math/RamanujanPiInfiniteSeries.C
@@ -0,0 +1,31 @@
+/// \file
+/// \ingroup tutorial_math
+/// \notebook -nodraw
+/// Tutorial demonstrating how infinite series can be used with
+/// S. Ramanujan's Formula for calculating Pi.
+///
+/// \macro_output
+/// \macro_code
+///
+/// \author Advait Dhingra
+
+#include <TMath.h>
+#include <iostream>
+
+void RamanujanPiInfiniteSeries() {
+   Int_t iterations;
+
+   std::cout << "Number of Iterations: " << std::endl;
+   std::cin >> iterations;
+
+   Double_t summation;
+
+   for (int n = 0; n < iterations; n++) {
+      summation += (TMath::Factorial(4*n) / std::pow(TMath::Factorial(n), 4)) * ((26390*n + 1103) / std::pow(396, 4*n));
+   }
+
+   Double_t pi = 1/((TMath::Sqrt(8) / 9801) * summation);
+
+      std::cout << std::setprecision(iterations) << pi << std::endl;
+
+}
