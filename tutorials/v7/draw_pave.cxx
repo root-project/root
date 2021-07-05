d36d12eb9ece56bd2609f035111ac4cfa21d6d1e
Sergey Linev <S.Linev@gsi.de>
2021-07-05 11:12:03 +0200

    Provide RFont example in draw_paves.cxx tutorial

--- tutorials/v7/draw_pave.cxx
+++ tutorials/v7/draw_pave.cxx
@@ -1,18 +1,18 @@
 /// \file
 /// \ingroup tutorial_v7
 ///
-/// This macro generates a small V7 TH1D, fills it and draw it in a V7 canvas.
-/// The canvas is display in the web browser
+/// This macro draw different variants of RPave on the RCanvas.
+/// Also usage of custom font is demonstrated.
 ///
 /// \macro_image (rcanvas_js)
 /// \macro_code
 ///
-/// \date 2015-03-22
+/// \date 2020-06-18
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
-/// \author Axel Naumann <axel@cern.ch>
+/// \author Sergey Linev <s.linev@gsi.de>
 
 /*************************************************************************
- * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2021, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -22,17 +22,14 @@
 #include "ROOT/RCanvas.hxx"
 #include "ROOT/RPave.hxx"
 #include "ROOT/RPaveText.hxx"
-
-// macro must be here while cling is not capable to load
-// library automatically for outlined function see ROOT-10336
-R__LOAD_LIBRARY(ROOTGpadv7)
+#include "ROOT/RFont.hxx"
 
 using namespace ROOT::Experimental;
 
 void draw_pave()
 {
    // Create a canvas to be displayed.
-   auto canvas = RCanvas::Create("Canvas Title");
+   auto canvas = RCanvas::Create("RPave example");
 
    // RFrame will be automatically created as well
    auto pave = canvas->Draw<RPave>();
@@ -43,12 +40,28 @@ void draw_pave()
    pave->height = 0.2_normal;
 
    auto text = canvas->Draw<RPaveText>();
-   text->AddLine("This is RTextPave");
+   text->AddLine("This is RPaveText");
    text->AddLine("It can have several lines");
    text->AddLine("It should be positioned below RPave");
    text->fill.color = RColor::kYellow;
    text->cornerY = 0.25_normal;
    text->height = 0.3_normal;
+
+   std::string fname = __FILE__;
+   auto pos = fname.find("draw_pave.cxx");
+   if (pos > 0) { fname.resize(pos); fname.append("comic.woff2"); }
+           else fname = "comic.woff2";
+   canvas->Draw<RFont>("CustomFont", fname);
+
+   auto text2 = canvas->Draw<RPaveText>();
+   text2->AddLine("Example of custom font");
+   text2->AddLine("It loaded from comic.woff2 file");
+   text2->AddLine("One also can provide valid URL");
+   text2->fill.color = RColor::kGreen;
+   text2->cornerX = -0.4_normal;
+   text2->cornerY = -0.03_normal;
+   text2->height = 0.2_normal;
+   text2->text.font.family = "CustomFont";
 
    canvas->SetSize(1000, 700);
    canvas->Show();
