1b5cbd0d0bbb4388190474575a5b223aabeed45a
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-07-07 17:01:25 +0200

    Made requested changes

--- tutorials/hist/NormalizeHistogram.C
+++ tutorials/hist/NormalizeHistogram.C
@@ -10,16 +10,6 @@
 /// \macro_image
 /// \macro_code
 ///
-/// The methods are:
-/// 1:
-/// ```
-/// hist2->Scale(1. / hist2->Integral());
-///```
-/// 2:
-/// ```
-///  hist3->Scale(1. / hist3->Integral("width"));
-/// ```
-///
 /// \author Advait Dhingra
 
 #include "TH2F.h"
@@ -30,36 +20,31 @@ void NormalizeHistogram()
 {
 
    // all of the histograms
-
-   TH1F *orig = new TH1F("orig", "Original histogram before normalization", 100, 0, 100);
-   orig->SetStats(0);
-   TH1F *norm = new TH1F("norm", "Histogram normalized with bin content", 100, 0, 100);
-   norm->SetStats(0);
-   TH1F *normw = new TH1F("normw", "Histogram normalized with bin content times bin width", 100, 0, 100);
-   hist3->SetStats(0);
+   std::array<double, 6> binsx{0, 5, 10, 20, 50, 100};
+   TH1F *hist1 = new TH1F("Unnormalized Histogram", "Unnormalized Hist", binsx.size() - 1, &binsx[0]);
+   TH1F *hist2 = new TH1F("Method 1 Normalized", "Method 1 Normalized", binsx.size() - 1, &binsx[0]);
+   TH1F *hist3 = new TH1F("Method 2 Normalized", "Method 2 Normalized", binsx.size() - 1, &binsx[0]);
 
    gStyle->SetTitleFontSize(0.1);
 
    TRandom2 rand;
 
    // Filling histograms with random entries
-
    for (int i = 0; i < 1000; ++i) {
-      double r = rand->Rndm() * 100;
+      double r = rand.Rndm() * 100;
       hist1->Fill(r);
       hist2->Fill(r);
       hist3->Fill(r);
    }
 
-   // method 1
+   // method 1: Normalization by division of bin content.
    hist2->Scale(1. / hist2->Integral());
 
-   // method 2
+   // method 2: Normalization by sum of the bins's content times the respective widths.
    hist3->Scale(1. / hist3->Integral("width"));
 
    // Drawing everything
-
-   TCanvas *c1 = new TCanvas("c1", "Histogram Normalization", 1000, 600);
+   TCanvas *c1 = new TCanvas("c1", "Histogram Normalization", 700, 900);
    c1->Divide(1, 3);
 
    c1->cd(1);
@@ -68,6 +53,4 @@ void NormalizeHistogram()
    hist2->Draw();
    c1->cd(3);
    hist3->Draw();
-
-   c1->Print("c1.png");
 }
