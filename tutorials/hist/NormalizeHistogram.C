f99e5534b61457f2c1f992bff712419c5d7f287b
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-07-08 18:02:22 +0200

    Cloned first histogram twice

--- tutorials/hist/NormalizeHistogram.C
+++ tutorials/hist/NormalizeHistogram.C
@@ -21,9 +21,7 @@ void NormalizeHistogram()
 
    // all of the histograms
    std::array<double, 6> binsx{0, 5, 10, 20, 50, 100};
-   TH1F *hist1 = new TH1F("Unnormalized Histogram", "Unnormalized Hist", binsx.size() - 1, &binsx[0]);
-   TH1F *hist2 = new TH1F("Method 1 Normalized", "Method 1 Normalized", binsx.size() - 1, &binsx[0]);
-   TH1F *hist3 = new TH1F("Method 2 Normalized", "Method 2 Normalized", binsx.size() - 1, &binsx[0]);
+   TH1F *orig = new TH1F("orig", "Original histogram before normalization", binsx.size() - 1, &binsx[0]);
 
    gStyle->SetTitleFontSize(0.1);
 
@@ -32,25 +30,28 @@ void NormalizeHistogram()
    // Filling histograms with random entries
    for (int i = 0; i < 1000; ++i) {
       double r = rand.Rndm() * 100;
-      hist1->Fill(r);
-      hist2->Fill(r);
-      hist3->Fill(r);
+      orig->Fill(r);
    }
 
+   TH1F *norm = (TH1F *)orig->Clone("norm");
+   norm->SetTitle("Histogram normalized with bin content");
+   TH1F *normw = (TH1F *)orig->Clone("normw");
+   normw->SetTitle("Histogram normalized with bin content times bin width");
+
    // method 1: Normalization by division of bin content.
-   hist2->Scale(1. / hist2->Integral());
+   norm->Scale(1. / norm->Integral());
 
    // method 2: Normalization by sum of the bins's content times the respective widths.
-   hist3->Scale(1. / hist3->Integral("width"));
+   normw->Scale(1. / normw->Integral("width"));
 
    // Drawing everything
    TCanvas *c1 = new TCanvas("c1", "Histogram Normalization", 700, 900);
    c1->Divide(1, 3);
 
    c1->cd(1);
-   hist1->Draw();
+   orig->Draw();
    c1->cd(2);
-   hist2->Draw();
+   norm->Draw();
    c1->cd(3);
-   hist3->Draw();
+   normw->Draw();
 }
