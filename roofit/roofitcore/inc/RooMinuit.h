397bd54daee0e862b7454aa3909669bd68ecd53e
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2021-07-08 14:12:30 +0200

    [RF] Add deprecation header for RooMinuit to be backwards compatible
    If you include the `RooMinuit.h` header now, you'll get a deprecation
    warning and the information that you can still find the class in
    `RooFitLegacy/RooMinuit.h`.

---
+++ roofit/roofitcore/inc/RooMinuit.h
@@ -0,0 +1,5 @@
+#warning "The deprecated RooMinuit class was replaced by the more general RooMinimizer. \
+Migrating to the RooMinimizer is easy because the interface is backwards compatible. \
+If you still want to use the old RooMinuit, #include \"RooFitLegacy/RooMinuit\" instead."
+
+#include "RooFitLegacy/RooMinuit.h"
