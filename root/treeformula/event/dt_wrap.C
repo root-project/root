c1e0c686240d1af5e38c0c4eca47839491d2cb78
Costa Bushnaq <bushnaq.costa@gmail.com>
2021-07-13 15:03:06 +0200

    move treeformula/event Makefile over to CMake (#716)
    * move treeformula/event Makefile to cmake
    
    * add first iteration of DrawTest tests format
    
    Added a macro for easier creation of tests
    
    Locally tested new-split0 fails on an internal error
    
    * fix .root file generation + windows library build
    
    I have applied changes suggested by Bertrand for Windows build.
    
    The test is still failing and I suspect we have a bug with
    ROOT_LINKER_LIBRARY.
    
    * trying without 'lib' prefix
    
    * change dictionary name to avoid confusion
    
    * Reversing dt_wrap.C «fix» ; add linker to tests
    
    * add utils functions ; correct .C macros ; derive remaining tests
    
    * improve POSTCMD command to fix Windows build
    
    * remove Makefile & files which are not needed anymore

--- root/treeformula/event/dt_wrap.C
+++ root/treeformula/event/dt_wrap.C
@@ -1,5 +1,11 @@
 void dt_wrap(const char* from, Int_t mode = 0, Int_t verboseLevel = 0) {
+   int status;
+   stringstream ss{"dt_RunDrawTest"};
+
+   ss << "(\"" << from << "\",";
+   ss << to_string(mode) << ",";
+   ss << to_string(verboseLevel) << ")";
    gROOT->ProcessLine(".L dt_RunDrawTest.C+");
-   int status = !dt_RunDrawTest(from,mode,verboseLevel);
+   gROOT->ProcessLine(ss.str().c_str(), &status);
    if (verboseLevel==0) gSystem->Exit(status);
 }
