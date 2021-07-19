e7d1bb9c3db9eda5b5ccd7de5f389948337ff1f7
E. G. Patrick Bos <egpbos@gmail.com>
2021-07-19 16:42:48 +0200

    add RooFit::TestStatistics
    This commit introduces a major refactoring of the RooAbs(Opt)TestStatistic-RooNLLVar inheritance tree into:
    1. statistics-based classes on the one hand;
    2. calculation/evaluation/optimization based classes on the other hand.
    
    The likelihood is the central unit on the statistics side. The RooAbsL class is implemented for four kinds of likelihoods: binned, unbinned, "subsidiary" (an optimization for numerical stability that gathers components like global observables) and "sum" (over multiple components of the other types). These classes provide ways to compute their components in parallelizable chunks that can be used by the calculator classes as they see fit. On top of the likelihood classes, we also provide for convenience a set of likelihood builders.
    
    The calculator "Wrapper" classes are abstract interfaces. These can be implemented for different kinds of algorithms, or with different kinds of optimization "back-ends" in mind. In an upcoming PR, we will introduce the fork-based multi-processing implementation based on RooFit::MultiProcess. Other possible implementations could use the GPU or external tools like TensorFlow.
    
    The coupling of all these classes to RooMinimizer is made via the MinuitFcnGrad class, which owns the Wrappers that calculate the likelihood components.

---
+++ roofit/roofitcore/inc/TestStatistics/MinuitFcnGrad.h
@@ -0,0 +1,166 @@
+// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
+
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2021, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+#ifndef ROOT_ROOFIT_TESTSTATISTICS_MinuitFcnGrad
+#define ROOT_ROOFIT_TESTSTATISTICS_MinuitFcnGrad
+
+#include "RooArgList.h"
+#include "RooRealVar.h"
+#include "TestStatistics/RooAbsL.h"
+#include "TestStatistics/LikelihoodWrapper.h"
+#include "TestStatistics/LikelihoodGradientWrapper.h"
+//#include "TestStatistics/LikelihoodJob.h"
+//#include "TestStatistics/LikelihoodGradientJob.h"
+#include "RooAbsMinimizerFcn.h"
+
+#include <Fit/ParameterSettings.h>
+#include "Math/IFunction.h" // ROOT::Math::IMultiGradFunction
+
+// forward declaration
+class RooAbsReal;
+class RooMinimizer;
+
+namespace RooFit {
+namespace TestStatistics {
+
+// forward declaration
+class LikelihoodSerial;
+class LikelihoodGradientSerial;
+
+// -- for communication with wrappers: --
+struct WrapperCalculationCleanFlags {
+   // indicate whether that part has been calculated since the last parameter update
+   bool likelihood = false;
+   bool gradient = false;
+
+   void set_all(bool value) {
+      likelihood = value;
+      gradient = value;
+   }
+};
+
+class MinuitFcnGrad : public ROOT::Math::IMultiGradFunction, public RooAbsMinimizerFcn {
+public:
+   // factory
+   template <typename LikelihoodWrapperT = RooFit::TestStatistics::LikelihoodSerial,
+             typename LikelihoodGradientWrapperT = RooFit::TestStatistics::LikelihoodGradientSerial>
+   static MinuitFcnGrad *create(const std::shared_ptr<RooFit::TestStatistics::RooAbsL> &likelihood,
+                                RooMinimizer *context, bool verbose = false);
+
+   inline ROOT::Math::IMultiGradFunction *Clone() const override { return new MinuitFcnGrad(*this); }
+
+   // override to include gradient strategy synchronization:
+   Bool_t Synchronize(std::vector<ROOT::Fit::ParameterSettings> &parameter_settings, Bool_t optConst,
+                      Bool_t verbose = kFALSE) override;
+
+   // used inside Minuit:
+   inline bool returnsInMinuit2ParameterSpace() const override { return gradient->usesMinuitInternalValues(); }
+
+   inline void setOptimizeConstOnFunction(RooAbsArg::ConstOpCode opcode, Bool_t doAlsoTrackingOpt) override
+   {
+      likelihood->constOptimizeTestStatistic(opcode, doAlsoTrackingOpt);
+   }
+
+private:
+   // IMultiGradFunction overrides necessary for Minuit: DoEval, Gradient
+   double DoEval(const double *x) const override;
+
+public:
+   void Gradient(const double *x, double *grad) const override;
+
+   // part of IMultiGradFunction interface, used widely both in Minuit and in RooFit:
+   inline unsigned int NDim() const override { return _nDim; }
+
+   inline std::string getFunctionName() const override { return likelihood->GetName(); }
+
+   inline std::string getFunctionTitle() const override { return likelihood->GetTitle(); }
+
+   inline void setOffsetting(Bool_t flag) override { likelihood->enableOffsetting(flag); }
+
+private:
+   template <typename LikelihoodWrapperT /*= RooFit::TestStatistics::LikelihoodJob*/,
+             typename LikelihoodGradientWrapperT /*= RooFit::TestStatistics::LikelihoodGradientJob*/>
+   MinuitFcnGrad(const std::shared_ptr<RooFit::TestStatistics::RooAbsL> &_likelihood, RooMinimizer *context,
+                 bool verbose,
+                 LikelihoodWrapperT * /* used only for template deduction */ =
+                    static_cast<LikelihoodWrapperT *>(nullptr),
+                 LikelihoodGradientWrapperT * /* used only for template deduction */ =
+                    static_cast<LikelihoodGradientWrapperT *>(nullptr));
+
+   // The following three overrides will not actually be used in this class, so they will throw:
+   double DoDerivative(const double *x, unsigned int icoord) const override;
+
+   bool syncParameterValuesFromMinuitCalls(const double *x, bool minuit_internal) const;
+
+   // members
+   std::shared_ptr<LikelihoodWrapper> likelihood;
+   std::shared_ptr<LikelihoodGradientWrapper> gradient;
+
+public:
+   mutable std::shared_ptr<WrapperCalculationCleanFlags> calculation_is_clean;
+private:
+   mutable std::vector<double> minuit_internal_x_;
+   mutable std::vector<double> minuit_external_x_;
+public:
+   mutable bool minuit_internal_roofit_x_mismatch_ = false;
+};
+
+} // namespace TestStatistics
+} // namespace RooFit
+
+
+// include here to avoid circular dependency issues in class definitions
+#include "RooMinimizer.h"
+
+
+namespace RooFit {
+namespace TestStatistics {
+
+template <typename LikelihoodWrapperT, typename LikelihoodGradientWrapperT>
+MinuitFcnGrad::MinuitFcnGrad(const std::shared_ptr<RooFit::TestStatistics::RooAbsL> &_likelihood, RooMinimizer *context,
+                             bool verbose, LikelihoodWrapperT * /* value unused */,
+                             LikelihoodGradientWrapperT * /* value unused */)
+   : RooAbsMinimizerFcn(RooArgList(*_likelihood->getParameters()), context, verbose), minuit_internal_x_(NDim(), 0),
+     minuit_external_x_(NDim(), 0)
+{
+   auto parameters = _context->fitter()->Config().ParamsSettings();
+   synchronizeParameterSettings(parameters, kTRUE, verbose);
+
+   calculation_is_clean = std::make_shared<WrapperCalculationCleanFlags>();
+   likelihood = std::make_shared<LikelihoodWrapperT>(_likelihood, calculation_is_clean/*, _context*/);
+   gradient = std::make_shared<LikelihoodGradientWrapperT>(_likelihood, calculation_is_clean, getNDim(), _context);
+
+   likelihood->synchronizeParameterSettings(parameters);
+   gradient->synchronizeParameterSettings(this, parameters);
+
+   // Note: can be different than RooGradMinimizerFcn, where default options are passed (ROOT::Math::MinimizerOptions::DefaultStrategy() and ROOT::Math::MinimizerOptions::DefaultErrorDef())
+   likelihood->synchronizeWithMinimizer(ROOT::Math::MinimizerOptions());
+   gradient->synchronizeWithMinimizer(ROOT::Math::MinimizerOptions());
+}
+
+// static function
+template <typename LikelihoodWrapperT, typename LikelihoodGradientWrapperT>
+MinuitFcnGrad *MinuitFcnGrad::create(const std::shared_ptr<RooFit::TestStatistics::RooAbsL>& likelihood,
+                                     RooMinimizer *context, bool verbose)
+{
+   return new MinuitFcnGrad(likelihood, context, verbose, static_cast<LikelihoodWrapperT *>(nullptr),
+                            static_cast<LikelihoodGradientWrapperT *>(nullptr));
+}
+
+} // namespace TestStatistics
+} // namespace RooFit
+
+#endif // ROOT_ROOFIT_TESTSTATISTICS_MinuitFcnGrad
---
+++ roofit/roofitcore/src/TestStatistics/optimization.cxx
@@ -0,0 +1,165 @@
+// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
+
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2021, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+#include <TestStatistics/optimization.h>
+
+#include <RooMsgService.h>
+#include <RooVectorDataStore.h> // complete type for dynamic cast
+#include <RooAbsReal.h>
+#include <RooArgSet.h>
+#include <RooAbsData.h>
+
+namespace RooFit {
+namespace TestStatistics {
+
+RooArgSet ConstantTermsOptimizer::requiredExtraObservables()
+{
+   // TODO: the RooAbsOptTestStatistics::requiredExtraObservables() call this code was copied
+   //       from was overloaded for RooXYChi2Var only; implement different options when necessary
+   return RooArgSet();
+}
+
+void ConstantTermsOptimizer::enableConstantTermsOptimization(RooAbsReal *function, RooArgSet *norm_set, RooAbsData *dataset,
+                                        bool applyTrackingOpt)
+{
+   // Trigger create of all object caches now in nodes that have deferred object creation
+   // so that cache contents can be processed immediately
+   function->getVal(norm_set);
+
+   // Apply tracking optimization here. Default strategy is to track components
+   // of RooAddPdfs and RooRealSumPdfs. If these components are a RooProdPdf
+   // or a RooProduct respectively, track the components of these products instead
+   // of the product term
+   RooArgSet trackNodes;
+
+   // Add safety check here - applyTrackingOpt will only be applied if present
+   // dataset is constructed in terms of a RooVectorDataStore
+   if (applyTrackingOpt) {
+      if (!dynamic_cast<RooVectorDataStore *>(dataset->store())) {
+         oocoutW((TObject *)nullptr, Optimization)
+            << "enableConstantTermsOptimization(function: " << function->GetName()
+            << ", dataset: " << dataset->GetName()
+            << ") WARNING Cache-and-track optimization (Optimize level 2) is only available for datasets"
+            << " implemented in terms of RooVectorDataStore - ignoring this option for current dataset" << std::endl;
+         applyTrackingOpt = kFALSE;
+      }
+   }
+
+   if (applyTrackingOpt) {
+      RooArgSet branches;
+      function->branchNodeServerList(&branches);
+      RooFIter iter = branches.fwdIterator();
+      RooAbsArg *arg;
+      while ((arg = iter.next())) {
+         arg->setCacheAndTrackHints(trackNodes);
+      }
+      // Do not set CacheAndTrack on constant expressions
+      auto constNodes = (RooArgSet *)trackNodes.selectByAttrib("Constant", kTRUE);
+      trackNodes.remove(*constNodes);
+      delete constNodes;
+
+      // Set CacheAndTrack flag on all remaining nodes
+      trackNodes.setAttribAll("CacheAndTrack", kTRUE);
+   }
+
+   // Find all nodes that depend exclusively on constant parameters
+   RooArgSet cached_nodes;
+
+   function->findConstantNodes(*dataset->get(), cached_nodes);
+
+   // Cache constant nodes with dataset - also cache entries corresponding to zero-weights in data when using
+   // BinnedLikelihood
+   // NOTE: we pass nullptr as cache-owner here, because we don't use the cacheOwner() anywhere in TestStatistics
+   // TODO: make sure this (nullptr) is always correct
+   dataset->cacheArgs(nullptr, cached_nodes, norm_set, !function->getAttribute("BinnedLikelihood"));
+
+   // Put all cached nodes in AClean value caching mode so that their evaluate() is never called
+   TIterator *cIter = cached_nodes.createIterator();
+   RooAbsArg *cacheArg;
+   while ((cacheArg = (RooAbsArg *)cIter->Next())) {
+      cacheArg->setOperMode(RooAbsArg::AClean);
+   }
+   delete cIter;
+
+   RooArgSet *constNodes = (RooArgSet *)cached_nodes.selectByAttrib("ConstantExpressionCached", kTRUE);
+   RooArgSet actualTrackNodes(cached_nodes);
+   actualTrackNodes.remove(*constNodes);
+   if (constNodes->getSize() > 0) {
+      if (constNodes->getSize() < 20) {
+         oocoutI((TObject*)nullptr, Minimization)
+            << " The following expressions have been identified as constant and will be precalculated and cached: "
+            << *constNodes << std::endl;
+      } else {
+         oocoutI((TObject*)nullptr, Minimization) << " A total of " << constNodes->getSize()
+                             << " expressions have been identified as constant and will be precalculated and cached."
+                             << std::endl;
+      }
+   }
+   if (actualTrackNodes.getSize() > 0) {
+      if (actualTrackNodes.getSize() < 20) {
+         oocoutI((TObject*)nullptr, Minimization) << " The following expressions will be evaluated in cache-and-track mode: "
+                             << actualTrackNodes << std::endl;
+      } else {
+         oocoutI((TObject*)nullptr, Minimization) << " A total of " << constNodes->getSize()
+                             << " expressions will be evaluated in cache-and-track-mode." << std::endl;
+      }
+   }
+   delete constNodes;
+
+   // Disable reading of observables that are no longer used
+   dataset->optimizeReadingWithCaching(*function, cached_nodes, requiredExtraObservables());
+
+   //   _optimized = kTRUE;
+}
+
+void ConstantTermsOptimizer::disableConstantTermsOptimization(RooAbsReal *function, RooArgSet *norm_set, RooArgSet *observables,
+                                                                 RooAbsData *dataset)
+{
+   // Delete the cache
+   dataset->resetCache();
+
+   // Reactivate all tree branches
+   dataset->setArgStatus(*dataset->get(), kTRUE);
+
+   // Reset all nodes to ADirty
+   optimizeCaching(function, norm_set, observables, dataset);
+
+   // Disable propagation of dirty state flags for observables
+   dataset->setDirtyProp(kFALSE);
+
+   //   _cachedNodes.removeAll();
+
+   //   _optimized = kFALSE;
+}
+
+void ConstantTermsOptimizer::optimizeCaching(RooAbsReal *function, RooArgSet *norm_set, RooArgSet *observables, RooAbsData *dataset)
+{
+   // Trigger create of all object caches now in nodes that have deferred object creation
+   // so that cache contents can be processed immediately
+   function->getVal(norm_set);
+
+   // Set value caching mode for all nodes that depend on any of the observables to ADirty
+   function->optimizeCacheMode(*observables);
+
+   // Disable propagation of dirty state flags for observables
+   dataset->setDirtyProp(kFALSE);
+
+   // Disable reading of observables that are not used
+   dataset->optimizeReadingWithCaching(*function, RooArgSet(), requiredExtraObservables()) ;
+}
+
+} // namespace TestStatistics
+} // namespace RooFit
