d98b12a7fa2dfa92e37fdfa16dd87c828462c6a2
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-07-21 17:10:46 +0200

    [minuit2] Fix an unused variable warning with gcc 11

--- math/minuit2/src/NumericalDerivator.cxx
+++ math/minuit2/src/NumericalDerivator.cxx
@@ -221,14 +221,12 @@ double NumericalDerivator::DInt2Ext(const ROOT::Fit::ParameterSettings &paramete
 // MODIFIED:
 /// This function was not implemented as in Minuit2. Now it copies the behavior
 /// of InitialGradientCalculator. See https://github.com/roofit-dev/root/issues/10
-void NumericalDerivator::SetInitialGradient(const ROOT::Math::IBaseFunctionMultiDim *function,
+void NumericalDerivator::SetInitialGradient(const ROOT::Math::IBaseFunctionMultiDim *,
                                             const std::vector<ROOT::Fit::ParameterSettings> &parameters,
                                             std::vector<DerivatorElement> &gradient)
 {
    // set an initial gradient using some given steps
    // (used in the first iteration)
-
-   assert(function != nullptr && "function is a nullptr");
 
    double eps2 = fPrecision.Eps2();
 
