f7b2bd98ab4e9c2bb22952d5385fcb7610e3ada9
Jonas Rembser <jonas.rembser@cern.ch>
2021-08-03 14:01:11 +0200

    [RF] Update reference files for testProxiesAndCategories
    * update the categories_v622.root file to actually use the class
      versionse from ROOT 6.22
    
    * add new categories_v624.root reference file and use it in RooCategory
      I/O unit tests

--- roofit/roofitcore/test/testProxiesAndCategories.cxx
+++ roofit/roofitcore/test/testProxiesAndCategories.cxx
@@ -127,7 +127,7 @@ TEST_P(RooCategoryIO, TestThatRangesAreShared) {
 }
 
 INSTANTIATE_TEST_SUITE_P(IO_SchemaEvol, RooCategoryIO,
-    testing::Values("categories_v620.root", "categories_v621.root", "categories_v622.root"));
+    testing::Values("categories_v620.root", "categories_v621.root", "categories_v622.root", "categories_v624.root"));
 
 
 
