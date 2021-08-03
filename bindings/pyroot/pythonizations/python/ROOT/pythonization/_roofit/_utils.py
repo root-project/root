09178672d8a692a4e2bb45cc439392c4f38c7e64
Jonas Rembser <jonas.rembser@cern.ch>
2021-08-03 16:55:02 +0200

    [RF][PyROOT] Attempt to unpack Python sets in keyword arg Pythonization
    This change is made so that it's possible to pass a simple Python set
    to a function that creates a RooArgSet via variadic template parameter
    forwarding.

--- bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/_utils.py
+++ bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/_utils.py
@@ -12,47 +12,47 @@
 ################################################################################
 
 
-def _getter(k, v):
-    # helper function to get CmdArg attribute from `RooFit`
-    # Parameters:
-    # k: key of the kwarg
-    # v: value of the kwarg
-
-    # We have to use ROOT here and not cppy.gbl, because the RooFit namespace is pythonized itself.
-    import ROOT
-    import libcppyy
-
-    func = getattr(ROOT.RooFit, k)
-
-    if isinstance(func, libcppyy.CPPOverload):
-        # Pythonization for functions that don't pass any RooCmdArgs like ShiftToZero() and MoveToBack(). For Eg,
-        # Default bindings: pdf.plotOn(frame, ROOT.RooFit.MoveToBack())
-        # With pythonizations: pdf.plotOn(frame, MoveToBack=True)
-
-        if "()" in func.func_doc:
-            if not isinstance(v, bool):
-                raise TypeError("The keyword argument " + k + " can only take bool values.")
-            return func() if v else ROOT.RooCmdArg.none()
-
-    try:
-        # If the keyword argument value is a tuple, list, or dict, first
-        # try to unpack it as parameters to the RooCmdArg-generating
-        # function. If this doesn't succeed, the tuple, list, or dict,
-        # will be passed directly to the function as it's only argument.
-        if isinstance(v, (tuple, list)):
-            return func(*v)
-        elif isinstance(v, (dict,)):
-            return func(**v)
-    except:
-        pass
-
-    return func(v)
-
-
 def _kwargs_to_roocmdargs(*args, **kwargs):
-    """Helper function to check kwargs and pythonize the arguments using _getter"""
+    """Helper function to check kwargs with keys that correspond to a function that creates RooCmdArg."""
+
+    def getter(k, v):
+        # helper function to get CmdArg attribute from `RooFit`
+        # Parameters:
+        # k: key of the kwarg
+        # v: value of the kwarg
+
+        # We have to use ROOT here and not cppy.gbl, because the RooFit namespace is pythonized itself.
+        import ROOT
+        import libcppyy
+
+        func = getattr(ROOT.RooFit, k)
+
+        if isinstance(func, libcppyy.CPPOverload):
+            # Pythonization for functions that don't pass any RooCmdArgs like ShiftToZero() and MoveToBack(). For Eg,
+            # Default bindings: pdf.plotOn(frame, ROOT.RooFit.MoveToBack())
+            # With pythonizations: pdf.plotOn(frame, MoveToBack=True)
+
+            if "()" in func.func_doc:
+                if not isinstance(v, bool):
+                    raise TypeError("The keyword argument " + k + " can only take bool values.")
+                return func() if v else ROOT.RooCmdArg.none()
+
+        try:
+            # If the keyword argument value is a tuple, list, set, or dict, first
+            # try to unpack it as parameters to the RooCmdArg-generating
+            # function. If this doesn't succeed, the tuple, list, or dict,
+            # will be passed directly to the function as it's only argument.
+            if isinstance(v, (tuple, list, set)):
+                return func(*v)
+            elif isinstance(v, (dict,)):
+                return func(**v)
+        except:
+            pass
+
+        return func(v)
+
     if kwargs:
-        args = args + tuple((_getter(k, v) for k, v in kwargs.items()))
+        args = args + tuple((getter(k, v) for k, v in kwargs.items()))
     return args, {}
 
 
