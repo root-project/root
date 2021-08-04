330c47b927f824b6395af84004b4277c21ec9dd0
ferdymercury <ferdymercury@users.noreply.github.com>
2021-08-04 09:12:04 +0200

    grab CTRL+s in TGMainFrame also if NumLock active (#8677)
    * grab CTRL+s in TGMainFrame also if NumLock active
    
    Fixes https://github.com/root-project/root/issues/8665
    
    * NumLock incompatibility in TRootGuiBuilder
    
    * both variants are needed

--- gui/gui/src/TGFrame.cxx
+++ gui/gui/src/TGFrame.cxx
@@ -1468,7 +1468,9 @@ TGMainFrame::TGMainFrame(const TGWindow *p, UInt_t w, UInt_t h,
    fWMInitState = (EInitialState) 0;
 
    gVirtualX->GrabKey(fId, gVirtualX->KeysymToKeycode(kKey_s),
-                      kKeyControlMask, kTRUE);
+                      kKeyControlMask, kTRUE);//grab CTRL+s
+   gVirtualX->GrabKey(fId, gVirtualX->KeysymToKeycode(kKey_s),
+                      kKeyControlMask | kKeyMod2Mask, kTRUE);//grab CTRL+s also if NumLock is active
    if (p == fClient->GetDefaultRoot()) {
       fMWMValue    = kMWMDecorAll;
       fMWMFuncs    = kMWMFuncAll;
@@ -1509,6 +1511,10 @@ TGMainFrame::~TGMainFrame()
       fBindList->Delete();
       delete fBindList;
    }
+   gVirtualX->GrabKey(fId, gVirtualX->KeysymToKeycode(kKey_s),
+                      kKeyControlMask, kFALSE);
+   gVirtualX->GrabKey(fId, gVirtualX->KeysymToKeycode(kKey_s),
+                      kKeyControlMask | kKeyMod2Mask, kFALSE);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
