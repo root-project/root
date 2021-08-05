aada54a29505f9ee3219269588490f76fe0c6020
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2021-08-06 04:12:20 +0530

    Add: Tests for serialisation of RModel

--- tmva/sofie/test/EmitFromONNX.cxx
+++ tmva/sofie/test/EmitFromONNX.cxx
@@ -2,7 +2,7 @@
 // Last modified: 2021/07/30
 // Description:
 //    SOFIE command line compiler.
-//    This program is automatically run when the target 'TestCustomModels' is built.
+//    This program is automatically run when the target 'TestCustomModelsFromONNX' is built.
 //    Usage example: $./sofiec indir/mymodel.onnx outdir/myname.hxx
 
 #include <iostream>
@@ -12,22 +12,17 @@
 
 using namespace TMVA::Experimental::SOFIE;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
    if (argc < 2) {
       std::cerr << "ERROR: missing input file\n";
       return -1;
    }
 
-   std::string outname;
-   if (argc == 3) {
-      outname = argv[2];
-   }
-
+   std::string outname=argv[2];
    RModelParser_ONNX parser;
    RModel model = parser.Parse(argv[1]);
    model.Generate();
-   model.OutputGenerated(outname);
+   model.OutputGenerated(outname+"_FromONNX.hxx");
 
    return 0;
 }
