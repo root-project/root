83cb226b24d8be2283e6d5464fe5029615ed92de
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-08-18 11:21:20 +0200

    [DF] Test entry ranges in RDataBlockID

--- tree/dataframe/test/dataframe_datablockcallback.cxx
+++ tree/dataframe/test/dataframe_datablockcallback.cxx
@@ -19,7 +19,7 @@
 // fixture for all tests in this file
 struct RDFDataBlockCallback : ::testing::TestWithParam<bool> {
    unsigned int NSLOTS;
-   unsigned int NENTRIES = std::max(10u, std::thread::hardware_concurrency() * 2);
+   ULong64_t NENTRIES = std::max(10u, std::thread::hardware_concurrency() * 2);
 
    RDFDataBlockCallback() : NSLOTS(GetParam() ? std::min(4u, std::thread::hardware_concurrency()) : 1u)
    {
@@ -62,7 +62,7 @@ TEST_P(RDFDataBlockCallback, EmptySource) {
    ROOT::RDataFrame df(NENTRIES);
    auto result = df.Book<>(CounterHelper(), {});
    // RDF with empty sources tries to produce 2 tasks per slot when MT is enabled
-   const auto expected = ROOT::IsImplicitMTEnabled() ? std::min(NENTRIES, df.GetNSlots() * 2u) : 1u;
+   const auto expected = ROOT::IsImplicitMTEnabled() ? std::min(NENTRIES, df.GetNSlots() * 2ull) : 1u;
    EXPECT_EQ(*result, expected);
 }
 
@@ -70,7 +70,7 @@ TEST_P(RDFDataBlockCallback, DataSource) {
    auto df = ROOT::RDF::MakeTrivialDataFrame(NENTRIES);
    auto result = df.Book<>(CounterHelper(), {});
    // RTrivialDS tries to produce NSLOTS tasks
-   const auto expected = ROOT::IsImplicitMTEnabled() ? std::min(NENTRIES, df.GetNSlots()) : 1u;
+   const auto expected = ROOT::IsImplicitMTEnabled() ? std::min(NENTRIES, ULong64_t(df.GetNSlots())) : 1ull;
    EXPECT_EQ(*result, expected);
 }
 
@@ -118,51 +118,68 @@ public:
    std::string GetActionName() { return "DataBlockHelper"; }
 };
 
-TEST_P(RDFDataBlockCallback, EmptySourceDataBlockNames) {
+TEST_P(RDFDataBlockCallback, EmptySourceDataBlockID) {
    ROOT::RDataFrame df(NENTRIES);
    auto result = df.Book<>(DataBlockHelper(), {});
    if (ROOT::IsImplicitMTEnabled()) {
       // RDF with empty sources tries to produce 2 tasks per slot when MT is enabled
-      const auto expectedSize = std::min(NENTRIES, df.GetNSlots() * 2u);
+      const auto expectedSize = std::min(NENTRIES, df.GetNSlots() * 2ull);
       ASSERT_EQ(result->size(), expectedSize);
       for (auto &id : *result) {
          // check that all entries start with the expected string
          EXPECT_TRUE(id.AsString().rfind("Empty source, range: {", 0) == 0);
+         EXPECT_EQ(id.NEntries(), NENTRIES / expectedSize);
       }
    } else {
+      ASSERT_EQ(result->size(), 1);
+      const auto &id = result->at(0);
       const std::string expectedStr = "Empty source, range: {0, " + std::to_string(NENTRIES) + "}";
-      EXPECT_EQ(result->at(0).AsString(), expectedStr);
+      EXPECT_EQ(id.AsString(), expectedStr);
+      EXPECT_EQ(id.NEntries(), NENTRIES);
+      EXPECT_EQ(id.EntryRange(), std::make_pair(0ull, NENTRIES));
    }
 }
 
-TEST_P(RDFDataBlockCallback, TTreeDataBlockNames) {
+TEST_P(RDFDataBlockCallback, TTreeDataBlockID) {
    const std::string prefix = "rdfdatablockcallback_ttreedbnames";
    InputFilesRAII file(1u, prefix);
    ROOT::RDataFrame df("t", prefix + "*");
    auto result = df.Book<>(DataBlockHelper(), {});
    ASSERT_EQ(result->size(), 1u);
-   EXPECT_TRUE(result->at(0).Contains(prefix));
-   EXPECT_TRUE(result->at(0).AsString().rfind("/t") == result->at(0).AsString().size() - 2);
+   const auto &id = result->at(0);
+   EXPECT_TRUE(id.Contains(prefix));
+   EXPECT_TRUE(id.AsString().rfind("/t") == id.AsString().size() - 2);
+   EXPECT_EQ(id.NEntries(), 1u);
+   EXPECT_EQ(id.EntryRange(), std::make_pair(0ull, 1ull));
 }
 
-TEST_P(RDFDataBlockCallback, TChainDataBlockNames) {
+TEST_P(RDFDataBlockCallback, TChainDataBlockID) {
    const std::string prefix = "rdfdatablockcallback_tchain";
    InputFilesRAII file(5u, prefix);
    ROOT::RDataFrame df("t", prefix + "*");
    auto result = df.Book<>(DataBlockHelper(), {});
    ASSERT_EQ(result->size(), 5u);
    std::vector<char> fileNumbers(5);
+   std::vector<std::pair<ULong64_t, ULong64_t>> entryRanges(5);
    for (auto i = 0u; i < 5u; ++i) {
       EXPECT_TRUE(result->at(i).Contains(prefix));
       const auto &id = result->at(i).AsString();
       EXPECT_TRUE(id.rfind("/t") == id.size() - 2);
       fileNumbers[i] = id[id.size() - 8];
+      entryRanges[i] = result->at(i).EntryRange();
    }
 
    std::sort(fileNumbers.begin(), fileNumbers.end());
-   for (int i = 0; i < 5; ++i)
+   std::sort(
+      entryRanges.begin(), entryRanges.end(),
+      [](std::pair<ULong64_t, ULong64_t> p1, std::pair<ULong64_t, ULong64_t> p2) { return p1.first < p2.first; });
+   for (int i = 0; i < 5; ++i) {
       // '0' == 48, '1' == 49, etc.
       EXPECT_EQ(fileNumbers[i], i + 48);
+      // local entry numbers are reported
+      EXPECT_EQ(entryRanges[i].first, 0ull);
+      EXPECT_EQ(entryRanges[i].second, 1ull);
+   }
 }
 
 /* TODO: data-block IDs for RDataSources are not supported yet
