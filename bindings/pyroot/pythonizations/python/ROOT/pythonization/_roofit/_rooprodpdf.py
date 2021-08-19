562afe981cd90333d0420bc3072cadf69975064f
Harshal.S <harshal2015@gmail.com>
2021-08-19 16:57:37 +0530

    [RF] Documentation for RooFit Pythonizations

--- bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/_rooprodpdf.py
+++ bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/_rooprodpdf.py
@@ -11,42 +11,35 @@
 ################################################################################
 
 
-r"""
-/**
-\class RooProdPdf
-\brief \parblock \endparblock
-\htmlonly
-<div class="pyrootbox">
-\endhtmlonly
-
-## PyROOT
-
-RooProdPdf() constructor takes a RooCmdArg as argument also supports keyword arguments.
-For example, the following code is equivalent in PyROOT:
-\code{.py}
-# Directly passing a RooCmdArg:
-model = ROOT.RooProdPdf(
-    "model", "model", ROOT.RooArgSet(shapePdf), ROOT.RooFit.Conditional(ROOT.RooArgSet(effPdf), ROOT.RooArgSet(cut))
-)
-
-# With keyword arguments:
-model = ROOT.RooProdPdf(
-    "model", "model", ROOT.RooArgSet(shapePdf), Conditional=(ROOT.RooArgSet(effPdf), ROOT.RooArgSet(cut))
-)
-\endcode
-
-\htmlonly
-</div>
-\endhtmlonly
-*/
-"""
-
-from ._utils import _kwargs_to_roocmdargs
+from ._utils import _kwargs_to_roocmdargs, cpp_signature
 
 
 class RooProdPdf(object):
+    """RooProdPdf() constructor takes a RooCmdArg as argument also supports keyword arguments.
+    For example, the following code is equivalent in PyROOT:
+    \code{.py}
+    # Directly passing a RooCmdArg:
+    model = ROOT.RooProdPdf(
+        "model", "model", ROOT.RooArgSet(shapePdf), ROOT.RooFit.Conditional(ROOT.RooArgSet(effPdf), ROOT.RooArgSet(cut))
+        )
+
+    # With keyword arguments:
+    model = ROOT.RooProdPdf(
+        "model", "model", ROOT.RooArgSet(shapePdf), Conditional=(ROOT.RooArgSet(effPdf), ROOT.RooArgSet(cut))
+        )
+    \endcode
+    """
+
+    @cpp_signature(
+        "RooProdPdf(const char* name, const char* title, const RooArgSet& fullPdfSet,"
+        "    const RooCmdArg& arg1            , const RooCmdArg& arg2=RooCmdArg(),"
+        "    const RooCmdArg& arg3=RooCmdArg(), const RooCmdArg& arg4=RooCmdArg(),"
+        "    const RooCmdArg& arg5=RooCmdArg(), const RooCmdArg& arg6=RooCmdArg(),"
+        "    const RooCmdArg& arg7=RooCmdArg(), const RooCmdArg& arg8=RooCmdArg()) ;"
+    )
     def __init__(self, *args, **kwargs):
-        # Redefinition of `RooProdPdf` constructor for keyword arguments.
-        # The keywords must correspond to the CmdArg of the constructor function.
+        """The RooProdPdf constructor is pythonized with the command argument pythonization.
+        The keywords must correspond to the CmdArgs of the constructor.
+        """
         args, kwargs = _kwargs_to_roocmdargs(*args, **kwargs)
         self._init(*args, **kwargs)
