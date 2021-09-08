a0c4188c460a273fe0e39824c18d87fff398b1ad
Andrei Gheata <andrei.gheata@cern.ch>
2021-09-08 07:48:07 +0200

    Demoting change of TGeo units from Warning to Info. (#8967)

--- geom/geom/src/TGeoManager.cxx
+++ geom/geom/src/TGeoManager.cxx
@@ -4030,10 +4030,10 @@ void TGeoManager::SetDefaultUnits(EDefaultUnits new_value)
 	      "Alternatively unlock the default units at own risk.");
    }
    else if ( new_value == kG4Units )   {
-      ::Warning("TGeoManager","Changing system of units to Geant4 units (mm, ns, MeV).");
+      ::Info("TGeoManager","Changing system of units to Geant4 units (mm, ns, MeV).");
    }
    else if ( new_value == kRootUnits )   {
-      ::Warning("TGeoManager","Changing system of units to ROOT units (cm, s, GeV).");
+      ::Info("TGeoManager","Changing system of units to ROOT units (cm, s, GeV).");
    }
    fgDefaultUnits = new_value;
 }
