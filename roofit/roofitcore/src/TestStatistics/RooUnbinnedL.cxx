7712d672cf7a5b354e9fa4fff5e2c22f21716d83
E. G. Patrick Bos <egpbos@gmail.com>
2021-09-08 14:41:53 +0200

    unify unbinned likelihood calculation in RooNLLVar and RooUnbinnedL
    RooUnbinnedL contained an old version of the "scalar" calculation method from RooNLLVar. This code switches RooUnbinnedL to instead use the scalar compuation method from RooNLLVar by default. To do this, the computation was factored out into a static member function inside RooNLLVar, so that RooUnbinnedL can reach it too and we avoid code duplication.
    
    In addition, RooUnbinnedL can now also be set into batch-evaluation
    mode. This too uses the existing batch calculation methods inside
    RooNLLVar, which also was factored out into a static member function.
    testLikelihoodSerial also contains a test that covers the batch mode
    now.
    
    Based on code review by @hageboeck in PR #8700.

--- roofit/roofitcore/src/TestStatistics/RooUnbinnedL.cxx
+++ roofit/roofitcore/src/TestStatistics/RooUnbinnedL.cxx
@@ -33,19 +33,24 @@ In extended mode, a
 #include "RooAbsData.h"
 #include "RooAbsPdf.h"
 #include "RooAbsDataStore.h"
+#include "RooNLLVar.h"  // RooNLLVar::ComputeScalar
+#include "RunContext.h" // complete type BatchCompute::RunContext
 
 #include "Math/Util.h" // KahanSum
 
 namespace RooFit {
 namespace TestStatistics {
 
-RooUnbinnedL::RooUnbinnedL(RooAbsPdf *pdf, RooAbsData *data, RooAbsL::Extended extended)
-   : RooAbsL(RooAbsL::ClonePdfData{pdf, data}, data->numEntries(), 1, extended)
+RooUnbinnedL::RooUnbinnedL(RooAbsPdf *pdf, RooAbsData *data, RooAbsL::Extended extended,
+                           bool useBatchedEvaluations)
+   : RooAbsL(RooAbsL::ClonePdfData{pdf, data}, data->numEntries(), 1, extended),
+     useBatchedEvaluations_(useBatchedEvaluations)
 {
 }
 
 RooUnbinnedL::RooUnbinnedL(const RooUnbinnedL &other)
-   : RooAbsL(other), apply_weight_squared(other.apply_weight_squared), _first(other._first)
+   : RooAbsL(other), apply_weight_squared(other.apply_weight_squared), _first(other._first),
+     useBatchedEvaluations_(other.useBatchedEvaluations_)
 {
 }
 
@@ -62,6 +67,10 @@ bool RooUnbinnedL::setApplyWeightSquared(bool flag)
    return false;
 }
 
+void RooUnbinnedL::setUseBatchedEvaluations(bool flag) {
+   useBatchedEvaluations_ = flag;
+}
+
 //////////////////////////////////////////////////////////////////////////////////
 /// Calculate and return likelihood on subset of data from firstEvent to lastEvent
 /// processed with a step size of 'stepSize'. If this an extended likelihood and
@@ -76,41 +85,43 @@ RooUnbinnedL::evaluatePartition(Section events, std::size_t /*components_begin*/
    // straight addition, but since evaluating the PDF is usually much more
    // expensive than that, we tolerate the additional cost...
    ROOT::Math::KahanSum<double> result;
+   double sumWeight;
 
    data_->store()->recalculateCache(nullptr, events.begin(N_events_), events.end(N_events_), 1, kTRUE);
 
-   ROOT::Math::KahanSum<double> sumWeight;
-
-   for (std::size_t i = events.begin(N_events_); i < events.end(N_events_); ++i) {
-      data_->get(i);
-      if (!data_->valid()) {
-         continue;
-      }
-
-      Double_t eventWeight = data_->weight();
-      if (0. == eventWeight * eventWeight) {
-         continue;
-      }
-      if (apply_weight_squared) {
-         eventWeight = data_->weightSquared();
-      }
-
-      Double_t term = -eventWeight * pdf_->getLogVal(normSet_.get());
-
-      sumWeight += eventWeight;
-      result += term;
+   if (useBatchedEvaluations_) {
+      std::tie(result, sumWeight) = RooNLLVar::computeBatchedFunc(pdf_.get(), data_.get(), evalData_, normSet_.get(), apply_weight_squared,
+                                                                  1, events.begin(N_events_), events.end(N_events_));
+   } else {
+      std::tie(result, sumWeight) = RooNLLVar::computeScalarFunc(pdf_.get(), data_.get(), normSet_.get(), apply_weight_squared,
+                                                                 1, events.begin(N_events_), events.end(N_events_));
    }
 
    // include the extended maximum likelihood term, if requested
    if (extended_) {
       if (apply_weight_squared) {
 
+         // TODO: the following should also be factored out into free/static functions like RooNLLVar::Compute*
          // Calculate sum of weights-squared here for extended term
-         ROOT::Math::KahanSum<double> sumW2;
-
-         for (Int_t i = 0; i < data_->numEntries(); i++) {
-            data_->get(i);
-            sumW2 += data_->weightSquared();
+         Double_t sumW2;
+         if (useBatchedEvaluations_) {
+            const RooSpan<const double> eventWeights = data_->getWeightBatch(0, N_events_);
+            if (eventWeights.empty()) {
+               sumW2 = (events.end(N_events_) - events.begin(N_events_)) * data_->weightSquared();
+            } else {
+               ROOT::Math::KahanSum<double, 4u> kahanWeight;
+               for (std::size_t i = 0; i < eventWeights.size(); ++i) {
+                  kahanWeight.AddIndexed(eventWeights[i] * eventWeights[i], i);
+               }
+               sumW2 = kahanWeight.Sum();
+            }
+         } else { // scalar mode
+            ROOT::Math::KahanSum<double> sumW2KahanSum;
+            for (Int_t i = 0; i < data_->numEntries(); i++) {
+               data_->get(i);
+               sumW2KahanSum += data_->weightSquared();
+            }
+            sumW2 = sumW2KahanSum.Sum();
          }
 
          Double_t expected = pdf_->expectedEvents(data_->get());
