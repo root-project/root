d043fae727377a3cd43a562626b1a0156ba3735e
alja <amraktadel@ucsd.edu>
2021-09-08 09:58:04 -0700

    Set size of canvas in the painter through the resize handler

--- ui5/eve7/controller/Lego.controller.js
+++ ui5/eve7/controller/Lego.controller.js
@@ -25,16 +25,17 @@ sap.ui.define([
 
         onInit: function () {
             let data = this.getView().getViewData();
-
             if (data) {
                 this.setupManagerAndViewType(data.eveViewerId, data.mgr);
             }
             else {
-                UIComponent.getRouterFor(this).getRoute("View").attachPatternMatched(this.onViewObjectMatched, this);
+                UIComponent.getRouterFor(this).getRoute("Lego").attachPatternMatched(this.onViewObjectMatched, this);
             }
+
+            ResizeHandler.register(this.getView(), this.onResize.bind(this));
         },
 
-        onTableObjectMatched: function (oEvent) {
+        onViewObjectMatched: function (oEvent) {
             let args = oEvent.getParameter("arguments");
             this.setupManagerAndViewType(JSROOT.$eve7tmp.eveViewerId, JSROOT.$eve7tmp.mgr);
             delete JSROOT.$eve7tmp;
@@ -51,35 +52,40 @@ sap.ui.define([
             this.mgr.RegisterSceneReceiver(sceneId, this);
 
             let scene = this.mgr.GetElement(sceneId);
-
             let chld = scene.childs[0];
             let element = this.byId("legoX");
             element.setHtmlText("Pointset infected by TCanvas / Lego Stack");
 
-            ResizeHandler.register(this.getView(), this.onResize.bind(this));
-
             this.eve_lego = chld;
-            this.canvas_json = JSROOT.parse( atob(chld.fTitle) );
-            // console.log(JSON.stringify(this.canvas_json));
+            this.canvas_json = JSROOT.parse(atob(chld.fTitle));
         },
-        onResize()
-        {
+        onResize : function() {
+            // use timeout
+            if (this.resize_tmout) clearTimeout(this.resize_tmout);
+            if (this.canvas_painter) this.canvas_painter.CheckCanvasResize();
+            this.resize_tmout = setTimeout(this.onResizeTimeout.bind(this), 250); // minimal latency
+        },
+        drawHist: function () {
             let domref = this.byId("legoPlotPlace").getDomRef();
-
-            if ( ! this.jst_ptr)
-            {
+            if (!this.jst_ptr) {
                 this.jst_ptr = 1;
-                JSROOT.draw(domref, this.canvas_json);
+                JSROOT.draw(domref, this.canvas_json).then(hist_painter => {
+                    this.canv_painter = hist_painter.getCanvPainter();
+                });
             }
-            else
-            {
+            else {
                 // if completely different, call JSROOT.cleanup(dom) first.
-                JSROOT.redraw(domref, this.canvas_json);
+                JSROOT.redraw(domref, this.canvas_json).then(hist_painter => {
+                    this.canv_painter = hist_painter.getCanvPainter();
+                });
             }
         },
-
+        onResizeTimeout: function () {
+            this.drawHist(); // AMT when to draw hist ??
+            delete this.resize_tmout;
+        },
         onSceneCreate: function (element, id) {
-            //console.log("LEGO onSceneCreate", id);
+            // console.log("LEGO onSceneCreate", id);
         },
 
         sceneElementChange: function (el) {
