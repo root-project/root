cbd3c0a05d34f0ed7f5c427ee05937d7e8111df6
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2021-09-10 12:09:21 +0200

    [RF] Don't return `std::shared_ptr`s in likelihood builders
    Returning a `std::shared_ptr` is unnessecarily restrictive. One should
    return `std::unique_ptr`, which can be implicitely moved to
    `std::shared_ptr`.

--- roofit/roofitcore/inc/TestStatistics/buildLikelihood.h
+++ roofit/roofitcore/inc/TestStatistics/buildLikelihood.h
@@ -29,18 +29,18 @@ class RooAbsData;
 namespace RooFit {
 namespace TestStatistics {
 
-std::shared_ptr<RooAbsL>
+std::unique_ptr<RooAbsL>
 buildLikelihood(RooAbsPdf *pdf, RooAbsData *data, RooAbsL::Extended extended = RooAbsL::Extended::Auto,
                 ConstrainedParameters constrained_parameters = {}, ExternalConstraints external_constraints = {},
                 GlobalObservables global_observables = {}, std::string global_observables_tag = {});
 
 // delegating builder calls, for more convenient "optional" parameter passing
-std::shared_ptr<RooAbsL>
+std::unique_ptr<RooAbsL>
 buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, ConstrainedParameters constrained_parameters);
-std::shared_ptr<RooAbsL> buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, ExternalConstraints external_constraints);
-std::shared_ptr<RooAbsL> buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, GlobalObservables global_observables);
-std::shared_ptr<RooAbsL> buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, std::string global_observables_tag);
-std::shared_ptr<RooAbsL>
+std::unique_ptr<RooAbsL> buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, ExternalConstraints external_constraints);
+std::unique_ptr<RooAbsL> buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, GlobalObservables global_observables);
+std::unique_ptr<RooAbsL> buildLikelihood(RooAbsPdf* pdf, RooAbsData* data, std::string global_observables_tag);
+std::unique_ptr<RooAbsL>
 buildLikelihood(RooAbsPdf *pdf, RooAbsData *data, ConstrainedParameters constrained_parameters, GlobalObservables global_observables);
 
 }
