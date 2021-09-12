7c428c7e128317b6447d82d99fc25334213af259
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2021-09-12 12:12:33 +0200

    [RF] Change copyright of roofitcore/TestStatistics to `(c) CERN`
    As requested by @Axel-Naumann.

--- roofit/roofitcore/inc/TestStatistics/RooAbsL.h
+++ roofit/roofitcore/inc/TestStatistics/RooAbsL.h
@@ -1,18 +1,14 @@
-// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
-
-/*****************************************************************************
- * RooFit
- * Authors:                                                                  *
- *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
- *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
- *                                                                           *
- * Copyright (c) 2000-2021, Regents of the University of California          *
- *                          and Stanford University. All rights reserved.    *
- *                                                                           *
- * Redistribution and use in source and binary forms,                        *
- * with or without modification, are permitted according to the terms        *
- * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
- *****************************************************************************/
+/*
+ * Project: RooFit
+ * Authors:
+ *   PB, Patrick Bos, Netherlands eScience Center, p.bos@esciencecenter.nl
+ *
+ * Copyright (c) 2021, CERN
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted according to the terms
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
 
 #ifndef ROOT_ROOFIT_TESTSTATISTICS_RooAbsL
 #define ROOT_ROOFIT_TESTSTATISTICS_RooAbsL
