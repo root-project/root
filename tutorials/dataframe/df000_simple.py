17d6eca546d1f2e9507d1c0f456e565a738c7485
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2021-09-14 16:06:36 +0200

    [DF] Add two simple tutorials in C++ and Python
    To be linked from the ROOT manual in the "First Steps" section.

---
+++ tutorials/dataframe/df000_simple.py
@@ -0,0 +1,25 @@
+## \file
+## \ingroup tutorial_dataframe
+## Simple RDataFrame example in Python.
+##
+## This tutorial shows a minimal example of RDataFrame that creates an
+## empty-source data frame, adds a new column `x` with random numbers
+## and finally creates and draws a histogram for `x`.
+##
+## \macro_code
+## \macro_output
+##
+## \date September 2021
+## \author Enric Tejedor (CERN)
+
+import ROOT
+
+# Create a data frame with 100 rows
+rdf = ROOT.RDataFrame(100)
+
+# Define a new column `x` that contains random numbers
+rdf_x = rdf.Define("x", "gRandom->Rndm()")
+
+# Create a histogram from `x` and draw it
+h = rdf_x.Histo1D("x")
+h.Draw()
