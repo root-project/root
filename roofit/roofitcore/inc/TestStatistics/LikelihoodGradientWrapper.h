egpbos/root-roofit-dev

add RooFit::TestStatistics
This commit introduces a major refactoring of the RooAbs(Opt)TestStatistic-RooNLLVar inheritance tree into:
1. statistics-based classes on the one hand;
2. calculation/evaluation/optimization based classes on the other hand.

The likelihood is the central unit on the statistics side. The RooAbsL class is implemented for four kinds of likelihoods: binned, unbinned, "subsidiary" (an optimization for numerical stability that gathers components like global observables) and "sum" (over multiple components of the other types). These classes provide ways to compute their components in parallelizable chunks that can be used by the calculator classes as they see fit. On top of the likelihood classes, we also provide for convenience a set of likelihood builders.

The calculator "Wrapper" classes are abstract interfaces. These can be implemented for different kinds of algorithms, or with different kinds of optimization "back-ends" in mind. In an upcoming PR, we will introduce the fork-based multi-processing implementation based on RooFit::MultiProcess. Other possible implementations could use the GPU or external tools like TensorFlow.

The coupling of all these classes to RooMinimizer is made via the MinuitFcnGrad class, which owns the Wrappers that calculate the likelihood components.

roofit/roofitcore/inc/TestStatistics/LikelihoodGradientWrapper.h
@@ -1,0 +1,66 @@
+// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
+
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2021, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+#ifndef ROOT_ROOFIT_TESTSTATISTICS_LikelihoodGradientWrapper
+#define ROOT_ROOFIT_TESTSTATISTICS_LikelihoodGradientWrapper
+
+#include <Fit/ParameterSettings.h>
+#include <Math/IFunctionfwd.h>
+#include "Math/MinimizerOptions.h"
+
+#include <vector>
+#include <memory> // shared_ptr
+
+// forward declaration
+class RooMinimizer;
+
+namespace RooFit {
+namespace TestStatistics {
+
+// forward declaration
+class RooAbsL;
+struct WrapperCalculationCleanFlags;
+
+class LikelihoodGradientWrapper {
+public:
+   LikelihoodGradientWrapper(std::shared_ptr<RooAbsL> likelihood, std::shared_ptr<WrapperCalculationCleanFlags> calculation_is_clean, std::size_t N_dim, RooMinimizer* minimizer);
+   virtual ~LikelihoodGradientWrapper() = default;
+   virtual LikelihoodGradientWrapper* clone() const = 0;
+
+   virtual void fillGradient(double *grad) = 0;
+
+   // synchronize minimizer settings with calculators in child classes
+   virtual void synchronizeWithMinimizer(const ROOT::Math::MinimizerOptions &options);
+   virtual void synchronizeParameterSettings(const std::vector<ROOT::Fit::ParameterSettings> &parameter_settings);
+   virtual void synchronizeParameterSettings(ROOT::Math::IMultiGenFunction* function, const std::vector<ROOT::Fit::ParameterSettings> &parameter_settings) = 0;
+   // Minuit passes in parameter values that may not conform to RooFit internal standards (like applying range clipping),
+   // but that the specific calculator does need. This function can be implemented to receive these Minuit-internal values:
+   virtual void updateMinuitInternalParameterValues(const std::vector<double>& minuit_internal_x);
+   virtual void updateMinuitExternalParameterValues(const std::vector<double>& minuit_external_x);
+
+   // completely depends on the implementation, so pure virtual
+   virtual bool usesMinuitInternalValues() = 0;
+
+protected:
+   std::shared_ptr<RooAbsL> likelihood;
+   RooMinimizer* _minimizer;
+   std::shared_ptr<WrapperCalculationCleanFlags> calculation_is_clean;
+};
+
+} // namespace TestStatistics
+} // namespace RooFit
+
+#endif // ROOT_ROOFIT_TESTSTATISTICS_LikelihoodGradientWrapper

roofit/roofitcore/inc/TestStatistics/RooAbsL.h
@@ -1,0 +1,129 @@
+// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
+
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2021, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+#ifndef ROOT_ROOFIT_TESTSTATISTICS_RooAbsL
+#define ROOT_ROOFIT_TESTSTATISTICS_RooAbsL
+
+#include "RooArgSet.h"
+#include "RooAbsArg.h" // enum ConstOpCode
+
+#include <cstddef> // std::size_t
+#include <string>
+
+// forward declarations
+class RooAbsPdf;
+class RooAbsData;
+
+namespace RooFit {
+namespace TestStatistics {
+
+class RooAbsL {
+public:
+   enum class Extended { Auto, Yes, No };
+   static bool isExtendedHelper(RooAbsPdf *pdf, Extended extended);
+
+   /// wrapper class used to distinguish ctors
+   struct ClonePdfData {
+      RooAbsPdf *pdf;
+      RooAbsData *data;
+   };
+
+   //   RooAbsL() = default;
+private:
+   RooAbsL(std::shared_ptr<RooAbsPdf> pdf, std::shared_ptr<RooAbsData> data, std::size_t N_events,
+           std::size_t N_components, Extended extended);
+
+public:
+   RooAbsL(RooAbsPdf *pdf, RooAbsData *data, std::size_t N_events, std::size_t N_components,
+           Extended extended = Extended::Auto);
+   RooAbsL(ClonePdfData in, std::size_t N_events, std::size_t N_components, Extended extended = Extended::Auto);
+   RooAbsL(const RooAbsL &other);
+   virtual ~RooAbsL() = default;
+
+   void initClones(RooAbsPdf &inpdf, RooAbsData &indata);
+
+   /// A part of some range delimited by two fractional points between 0 and 1 (inclusive).
+   struct Section {
+      Section(double begin, double end) : begin_fraction(begin), end_fraction(end)
+      {
+         if ((begin > end) || (begin < 0) || (end > 1)) {
+            throw std::domain_error("Invalid input values for section; begin must be >= 0, end <= 1 and begin < end.");
+         }
+      }
+
+      Section(const Section &section) = default;
+
+      std::size_t begin(std::size_t N_total) const { return static_cast<std::size_t>(N_total * begin_fraction); }
+
+      std::size_t end(std::size_t N_total) const
+      {
+         if (end_fraction == 1) {
+            return N_total;
+         } else {
+            return static_cast<std::size_t>(N_total * end_fraction);
+         }
+      }
+
+      double begin_fraction;
+      double end_fraction;
+   };
+
+   virtual double evaluatePartition(Section events, std::size_t components_begin, std::size_t components_end) = 0;
+   inline double getCarry() const { return eval_carry_; }
+
+   // necessary from MinuitFcnGrad to reach likelihood properties:
+   virtual RooArgSet *getParameters();
+   virtual void constOptimizeTestStatistic(RooAbsArg::ConstOpCode opcode, bool doAlsoTrackingOpt);
+
+   virtual std::string GetName() const;
+   virtual std::string GetTitle() const;
+
+   // necessary in RooMinimizer (via LikelihoodWrapper)
+   inline virtual double defaultErrorLevel() const { return 0.5; }
+
+   // necessary in LikelihoodJob
+   virtual std::size_t numDataEntries() const;
+   inline std::size_t getNEvents() const { return N_events; }
+   inline std::size_t getNComponents() const { return N_components; }
+   inline bool isExtended() const { return extended_; }
+   inline void setSimCount(std::size_t value) { sim_count_ = value; }
+
+protected:
+   virtual void optimizePdf();
+   // Note: pdf_ and data_ can be constructed in two ways, one of which implies ownership and the other does not.
+   // Inspired by this: https://stackoverflow.com/a/61227865/1199693.
+   // The owning variant is used for classes that need a pdf/data clone (RooBinnedL and RooUnbinnedL), whereas the
+   // non-owning version is used for when a reference to the external pdf/dataset is good enough (RooSumL).
+   // This means that pdf_ and data_ are not meant to actually be shared! If there were a unique_ptr with optional
+   // ownership, we would have used that instead.
+   std::shared_ptr<RooAbsPdf> pdf_;
+   std::shared_ptr<RooAbsData> data_;
+   std::unique_ptr<RooArgSet> _normSet; // Pointer to set with observables used for normalization
+
+   std::size_t N_events = 1;
+   std::size_t N_components = 1;
+
+   bool extended_ = false;
+
+   std::size_t sim_count_ = 1; // Total number of component p.d.f.s in RooSimultaneous (if any)
+
+   mutable double eval_carry_ = 0; //! carry of Kahan sum in evaluatePartition
+};
+
+} // namespace TestStatistics
+} // namespace RooFit
+
+#endif // ROOT_ROOFIT_TESTSTATISTICS_RooAbsL

root-project/root

roofit/roofitcore/inc/TestStatistics/LikelihoodGradientWrapper.h
@@ -1,0 +1,66 @@
+// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
+
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2021, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+#ifndef ROOT_ROOFIT_TESTSTATISTICS_LikelihoodGradientWrapper
+#define ROOT_ROOFIT_TESTSTATISTICS_LikelihoodGradientWrapper
+
+#include <Fit/ParameterSettings.h>
+#include <Math/IFunctionfwd.h>
+#include "Math/MinimizerOptions.h"
+
+#include <vector>
+#include <memory> // shared_ptr
+
+// forward declaration
+class RooMinimizer;
+
+namespace RooFit {
+namespace TestStatistics {
+
+// forward declaration
+class RooAbsL;
+struct WrapperCalculationCleanFlags;
+
+class LikelihoodGradientWrapper {
+public:
+   LikelihoodGradientWrapper(std::shared_ptr<RooAbsL> likelihood, std::shared_ptr<WrapperCalculationCleanFlags> calculation_is_clean, std::size_t N_dim, RooMinimizer* minimizer);
+   virtual ~LikelihoodGradientWrapper() = default;
+   virtual LikelihoodGradientWrapper* clone() const = 0;
+
+   virtual void fillGradient(double *grad) = 0;
+
+   // synchronize minimizer settings with calculators in child classes
+   virtual void synchronizeWithMinimizer(const ROOT::Math::MinimizerOptions &options);
+   virtual void synchronizeParameterSettings(const std::vector<ROOT::Fit::ParameterSettings> &parameter_settings);
+   virtual void synchronizeParameterSettings(ROOT::Math::IMultiGenFunction* function, const std::vector<ROOT::Fit::ParameterSettings> &parameter_settings) = 0;
+   // Minuit passes in parameter values that may not conform to RooFit internal standards (like applying range clipping),
+   // but that the specific calculator does need. This function can be implemented to receive these Minuit-internal values:
+   virtual void updateMinuitInternalParameterValues(const std::vector<double>& minuit_internal_x);
+   virtual void updateMinuitExternalParameterValues(const std::vector<double>& minuit_external_x);
+
+   // completely depends on the implementation, so pure virtual
+   virtual bool usesMinuitInternalValues() = 0;
+
+protected:
+   std::shared_ptr<RooAbsL> likelihood_;
+   RooMinimizer * minimizer_;
+   std::shared_ptr<WrapperCalculationCleanFlags> calculation_is_clean_;
+};
+
+} // namespace TestStatistics
+} // namespace RooFit
+
+#endif // ROOT_ROOFIT_TESTSTATISTICS_LikelihoodGradientWrapper

roofit/roofitcore/inc/TestStatistics/RooAbsL.h
@@ -1,0 +1,129 @@
+// Author: Patrick Bos, Netherlands eScience Center / NIKHEF 2021
+
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2021, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
+#ifndef ROOT_ROOFIT_TESTSTATISTICS_RooAbsL
+#define ROOT_ROOFIT_TESTSTATISTICS_RooAbsL
+
+#include "RooArgSet.h"
+#include "RooAbsArg.h" // enum ConstOpCode
+
+#include <cstddef> // std::size_t
+#include <string>
+
+// forward declarations
+class RooAbsPdf;
+class RooAbsData;
+
+namespace RooFit {
+namespace TestStatistics {
+
+class RooAbsL {
+public:
+   enum class Extended { Auto, Yes, No };
+   static bool isExtendedHelper(RooAbsPdf *pdf, Extended extended);
+
+   /// wrapper class used to distinguish ctors
+   struct ClonePdfData {
+      RooAbsPdf *pdf;
+      RooAbsData *data;
+   };
+
+   //   RooAbsL() = default;
+private:
+   RooAbsL(std::shared_ptr<RooAbsPdf> pdf, std::shared_ptr<RooAbsData> data, std::size_t N_events,
+           std::size_t N_components, Extended extended);
+
+public:
+   RooAbsL(RooAbsPdf *pdf, RooAbsData *data, std::size_t N_events, std::size_t N_components,
+           Extended extended = Extended::Auto);
+   RooAbsL(ClonePdfData in, std::size_t N_events, std::size_t N_components, Extended extended = Extended::Auto);
+   RooAbsL(const RooAbsL &other);
+   virtual ~RooAbsL() = default;
+
+   void initClones(RooAbsPdf &inpdf, RooAbsData &indata);
+
+   /// A part of some range delimited by two fractional points between 0 and 1 (inclusive).
+   struct Section {
+      Section(double begin, double end) : begin_fraction(begin), end_fraction(end)
+      {
+         if ((begin > end) || (begin < 0) || (end > 1)) {
+            throw std::domain_error("Invalid input values for section; begin must be >= 0, end <= 1 and begin < end.");
+         }
+      }
+
+      Section(const Section &section) = default;
+
+      std::size_t begin(std::size_t N_total) const { return static_cast<std::size_t>(N_total * begin_fraction); }
+
+      std::size_t end(std::size_t N_total) const
+      {
+         if (end_fraction == 1) {
+            return N_total;
+         } else {
+            return static_cast<std::size_t>(N_total * end_fraction);
+         }
+      }
+
+      double begin_fraction;
+      double end_fraction;
+   };
+
+   virtual double evaluatePartition(Section events, std::size_t components_begin, std::size_t components_end) = 0;
+   inline double getCarry() const { return eval_carry_; }
+
+   // necessary from MinuitFcnGrad to reach likelihood properties:
+   virtual RooArgSet *getParameters();
+   virtual void constOptimizeTestStatistic(RooAbsArg::ConstOpCode opcode, bool doAlsoTrackingOpt);
+
+   virtual std::string GetName() const;
+   virtual std::string GetTitle() const;
+
+   // necessary in RooMinimizer (via LikelihoodWrapper)
+   inline virtual double defaultErrorLevel() const { return 0.5; }
+
+   // necessary in LikelihoodJob
+   virtual std::size_t numDataEntries() const;
+   inline std::size_t getNEvents() const { return N_events_; }
+   inline std::size_t getNComponents() const { return N_components_; }
+   inline bool isExtended() const { return extended_; }
+   inline void setSimCount(std::size_t value) { sim_count_ = value; }
+
+protected:
+   virtual void optimizePdf();
+   // Note: pdf_ and data_ can be constructed in two ways, one of which implies ownership and the other does not.
+   // Inspired by this: https://stackoverflow.com/a/61227865/1199693.
+   // The owning variant is used for classes that need a pdf/data clone (RooBinnedL and RooUnbinnedL), whereas the
+   // non-owning version is used for when a reference to the external pdf/dataset is good enough (RooSumL).
+   // This means that pdf_ and data_ are not meant to actually be shared! If there were a unique_ptr with optional
+   // ownership, we would have used that instead.
+   std::shared_ptr<RooAbsPdf> pdf_;
+   std::shared_ptr<RooAbsData> data_;
+   std::unique_ptr<RooArgSet> normSet_; // Pointer to set with observables used for normalization
+
+   std::size_t N_events_ = 1;
+   std::size_t N_components_ = 1;
+
+   bool extended_ = false;
+
+   std::size_t sim_count_ = 1; // Total number of component p.d.f.s in RooSimultaneous (if any)
+
+   mutable double eval_carry_ = 0; //! carry of Kahan sum in evaluatePartition
+};
+
+} // namespace TestStatistics
+} // namespace RooFit
+
+#endif // ROOT_ROOFIT_TESTSTATISTICS_RooAbsL