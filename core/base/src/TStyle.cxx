e488a397e38ace3b86968193caf95518939c3b0c
Olivier Couet <olivier.couet@cern.ch>
2021-09-15 09:31:47 +0200

    better comments

--- core/base/src/TStyle.cxx
+++ core/base/src/TStyle.cxx
@@ -150,12 +150,12 @@ TStyle::TStyle() :TNamed()
 ////////////////////////////////////////////////////////////////////////////////
 /// Create a new TStyle.
 /// The following names are reserved to create special styles
-///   - `Classic`
+///   - `Classic`: Similar to 'Default` style set in TStyle::Reset
 ///   - `Plain`: a black&white oriented style
 ///   - `Bold`
 ///   - `Video`
 ///   - `Pub`
-///   - `Modern` : the default style set in TStyle::Reset
+///   - `Modern` Used when ROOT starts
 ///   - `ATLAS`: style used by the ATLAS experiment
 ///   - `BELLE2`: style used by the BELLE II experiment
 ///     (see the definition of these styles below).
