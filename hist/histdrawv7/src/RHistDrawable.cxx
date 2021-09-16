d561622a790c56eee2269f3170c1f29a803818f7
Olivier Couet <olivier.couet@cern.ch>
2021-09-16 18:28:37 +0200

    More in HistV7

--- hist/histdrawv7/src/RHistDrawable.cxx
+++ hist/histdrawv7/src/RHistDrawable.cxx
@@ -1,5 +1,5 @@
 /// \file RHistDrawable.cxx
-/// \ingroup Hist ROOT7
+/// \ingroup HistV7
 /// \author Axel Naumann <axel@cern.ch>
 /// \date 2015-09-11
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
