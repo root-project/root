9dfe3468346d4cccd42e7ad431a267cb7a309436
Jakob Blomer <jblomer@cern.ch>
2021-09-17 09:34:35 +0200

    [ntuple] Clarify constness of local variable
    Co-authored-by: Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>

--- tree/ntuple/v7/src/RNTuple.cxx
+++ tree/ntuple/v7/src/RNTuple.cxx
@@ -339,7 +339,7 @@ void ROOT::Experimental::RNTupleWriter::CommitCluster()
    }
    const float nbytes = fSink->CommitCluster(fNEntries);
    // Cap the compression factor at 1000 to prevent overflow of fMinUnzippedClusterSizeEst
-   float compressionFactor = std::min(1000.f, static_cast<float>(fUnzippedClusterSize) / nbytes);
+   const float compressionFactor = std::min(1000.f, static_cast<float>(fUnzippedClusterSize) / nbytes);
    fUnzippedClusterSizeEst =
       compressionFactor * static_cast<float>(fSink->GetWriteOptions().GetApproxZippedClusterSize());
 
