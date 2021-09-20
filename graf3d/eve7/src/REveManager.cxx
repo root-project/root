b7010eeb1fb1e41d36e0b817d51524ee54679919
alja <amraktadel@ucsd.edu>
2021-09-20 11:23:59 -0700

    Use std::timespec on LINUX osx and std::time on ther system when storing sample time of server status

--- graf3d/eve7/src/REveManager.cxx
+++ graf3d/eve7/src/REveManager.cxx
@@ -1109,7 +1109,11 @@ void REveManager::GetServerStatus(REveServerStatus& st)
 {
    std::unique_lock<std::mutex> lock(fServerState.fMutex);
    gSystem->GetProcInfo(&fServerStatus.fProcInfo);
+#ifdef R__LINUX
    std::timespec_get(&fServerStatus.fTReport, TIME_UTC);
+#else
+   fServerStatus.fTReport = std::time_t(nullptr);
+#endif
    st = fServerStatus;
 }
 
