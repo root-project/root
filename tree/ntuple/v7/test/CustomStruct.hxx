3990a2b39687e72cf356f0231f8bf04a7730c67f
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2021-09-26 19:18:25 +0200

    [ntuple] RClassField: add test for types that inherit from multiple classes

--- tree/ntuple/v7/test/CustomStruct.hxx
+++ tree/ntuple/v7/test/CustomStruct.hxx
@@ -16,8 +16,17 @@ struct DerivedA : public CustomStruct {
    std::string a_s;
 };
 
+struct DerivedA2 : public CustomStruct {
+   float a2_f{};
+};
+
 struct DerivedB : public DerivedA {
    float b_f1 = 0.0;
    float b_f2 = 0.0; //!
    std::string b_s;
+};
+
+struct DerivedC : public DerivedA, public DerivedA2 {
+   int c_i{};
+   DerivedA2 c_a2;
 };
