558e665f1e854094ac247499fb7a5b4e58d00a4e
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-09-28 10:37:20 +0200

    [DF][NFC] Add docs for DefinePerSample, RSampleInfo

--- tree/dataframe/inc/ROOT/RDF/RSampleInfo.hxx
+++ tree/dataframe/inc/ROOT/RDF/RSampleInfo.hxx
@@ -21,8 +21,14 @@
 namespace ROOT {
 namespace RDF {
 
-/// This type represents a data-block identifier, to be used in conjunction with RDataFrame features such as
-/// DefinePerSample() and data-block callbacks.
+/// This type represents a sample identifier, to be used in conjunction with RDataFrame features such as
+/// DefinePerSample() and per-sample callbacks.
+///
+/// When the input data comes from a TTree, the string representation of RSampleInfo (which is returned by AsString()
+/// and that can be queried e.g. with Contains()) is of the form "<filename>/<treename>".
+///
+/// In multi-thread runs different tasks might process different entry ranges of the same sample,
+/// so RSampleInfo also provides methods to inspect which part of a sample is being taken into consideration.
 class RSampleInfo {
    // Currently backed by a simple string, might change in the future as we get usage experience.
    std::string fID;
@@ -40,23 +46,34 @@ public:
    RSampleInfo &operator=(RSampleInfo &&) = default;
    ~RSampleInfo() = default;
 
+   /// Check whether the sample name contains the given substring.
    bool Contains(std::string_view substr) const
    {
       // C++14 needs the conversion from std::string_view to std::string
       return fID.find(std::string(substr)) != std::string::npos;
    }
 
+   /// Check whether the sample name is empty.
+   ///
+   /// This is the case e.g. when using a RDataFrame with no input data, constructed as `RDataFrame(nEntries)`.
    bool Empty() const {
       return fID.empty();
    }
 
+   /// Return a string representation of the sample name.
+   ///
+   /// The representation is of the form "<filename>/<treename>" if the input data comes from a TTree or a TChain.
    const std::string &AsString() const
    {
       return fID;
    }
 
+   /// Return the entry range in this sample that is being taken into consideration.
+   ///
+   /// Multiple multi-threading tasks might process different entry ranges of the same sample.
    std::pair<ULong64_t, ULong64_t> EntryRange() const { return fEntryRange; }
 
+   /// Return the number of entries of this sample that is being taken into consideration.
    ULong64_t NEntries() const { return fEntryRange.second - fEntryRange.first; }
 
    bool operator==(const RSampleInfo &other) const { return fID == other.fID; }
