30e06f313e54e0212790f4700b483001b3e969e2
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2021-09-29 11:22:59 +0200

    [Rint] Test for issue #8772
    Test that backslash-newline works in Rint; more details available at
    https://github.com/root-project/root/pull/8772

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -20,6 +20,13 @@ if(CTEST_BUILD_NAME MATCHES slc6|centos7)
               roottest-root-multicore-fork)
 endif()
 
+if(WIN32)
+  # driveTabCom.py: `import pty` is not supported on Windows
+  list(APPEND CTEST_CUSTOM_TESTS_IGNORE
+              roottest-root-rint-TabCom
+              roottest-root-rint-BackslashNewline)
+endif()
+
 if(CTEST_BUILD_NAME MATCHES fst)
   list(APPEND CTEST_CUSTOM_TESTS_IGNORE roottest-python-JsMVA-NewMethods)
 endif()
