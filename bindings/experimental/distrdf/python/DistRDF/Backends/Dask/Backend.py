5b87f21dcf1f623eb8445eeaab4f687ac4b5753c
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2021-10-07 14:16:20 +0200

    Add support for distributing files(headers, libraries) to the workers

--- bindings/experimental/distrdf/python/DistRDF/Backends/Dask/Backend.py
+++ bindings/experimental/distrdf/python/DistRDF/Backends/Dask/Backend.py
@@ -9,6 +9,8 @@
 # For the licensing terms see $ROOTSYS/LICENSE.                                #
 # For the list of contributors see $ROOTSYS/README/CREDITS.                    #
 ################################################################################
+import os
+
 from DistRDF import DataFrame
 from DistRDF import HeadNode
 from DistRDF.Backends import Base
@@ -16,7 +18,7 @@ from DistRDF.Backends import Utils
 
 try:
     import dask
-    from dask.distributed import Client, progress
+    from dask.distributed import Client, progress, get_worker
 except ImportError:
     raise ImportError(("cannot import a Dask component. Refer to the Dask documentation "
                        "for installation instructions."))
@@ -53,7 +55,44 @@ class DaskBackend(Base.BaseBackend):
         if self.client is None:
             self.client = Client()
 
-        dmapper = dask.delayed(mapper)
+        # With the Spark backend these need to be passed as variables.
+        # Doing the same here for coherency, might change in the future.
+        headers = self.headers
+        shared_libraries = self.shared_libraries
+
+        def dask_mapper(current_range):
+            """
+            Gets the paths to the file(s) in the current executor, then
+            declares the headers found.
+
+            Args:
+                current_range (tuple): The current range of the dataset being
+                    processed on the executor.
+
+            Returns:
+                function: The map function to be executed on each executor,
+                complete with all headers needed for the analysis.
+            """
+            # Retrieve the current worker local directory
+            localdir = get_worker().local_directory
+
+            # Get and declare headers on each worker
+            headers_on_executor = [
+                os.path.join(localdir, filepath)
+                for filepath in headers
+            ]
+            Utils.declare_headers(headers_on_executor)
+
+            # Get and declare shared libraries on each worker
+            shared_libs_on_ex = [
+                os.path.join(localdir, filepath)
+                for filepath in shared_libraries
+            ]
+            Utils.declare_shared_libraries(shared_libs_on_ex)
+
+            return mapper(current_range)
+
+        dmapper = dask.delayed(dask_mapper)
         dreducer = dask.delayed(reducer)
 
         mergeables_lists = [dmapper(range) for range in ranges]
@@ -69,9 +108,13 @@ class DaskBackend(Base.BaseBackend):
 
     def distribute_unique_paths(self, paths):
         """
-        Not implemented yet!
+        Dask supports sending files to the workes via the `Client.upload_file`
+        method. Its stated purpose is to send local Python packages to the
+        nodes, but in practice it uploads the file to the path stored in the
+        `local_directory` attribute of each worker.
         """
-        pass
+        for filepath in paths:
+            self.client.upload_file(filepath)
 
     def make_dataframe(self, *args, **kwargs):
         """
