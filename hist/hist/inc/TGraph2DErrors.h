09c584fed88a8dc3b60be18180bdbcedb6ee9da8
ferdymercury <ferdymercury@users.noreply.github.com>
2021-10-19 16:55:08 +0200

    Add public function to scale TGraph and derived classes (#8143)
    * add Scale function for TGraph
    
    in a similar fashion to TH1
    
    * set default to scale y
    
    * scale function in TGraphErrors
    
    * scale implementation in tgraph and tgrapherrors
    
    * scale tgraph2d
    
    * scale tgraph2derrors and fix missing line in tgrapherr
    
    * scale tgraphassym
    
    * scale tgraphbent
    
    * scale tgraphmultierr
    
    Co-authored-by: Fernando Hueso González <58-ferhue@users.noreply.igit.ific.uv.es>

--- hist/hist/inc/TGraph2DErrors.h
+++ hist/hist/inc/TGraph2DErrors.h
@@ -55,6 +55,7 @@ public:
    Double_t        GetZminE() const;
    virtual void    Print(Option_t *chopt="") const;
    Int_t           RemovePoint(Int_t ipoint); // *MENU*
+   virtual void    Scale(Double_t c1=1., Option_t *option="z"); // *MENU*
    virtual void    Set(Int_t n);
    virtual void    SetPoint(Int_t i, Double_t x, Double_t y, Double_t z);
    virtual void    SetPointError(Int_t i, Double_t ex, Double_t ey, Double_t ez);
