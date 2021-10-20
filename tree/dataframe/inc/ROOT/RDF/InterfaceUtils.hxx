e770230873bc393e3976adfa0efa42595a2a7273
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2021-10-20 19:24:30 +0200

    [DF] Internal function to trigger computation graph
    Defines a new function for internal use in RDataFrame that accepts an `RNode` and triggers the computation graph through the `RLoopManager::Run` method.
    This is useful because it gives an extra possibility for triggering the computation graph that doesn't depend on one of the `RResultPtr`s connected to some RDF action node.
    In particular, in DistRDF this function is pythonized via setting the attribute `__release_gil__` to `True`, so that the RDF event loop runs with the Python GIL released.
    
    Co-authored-by: Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
    Co-authored-by: Enrico Guiraud <enrico.guiraud@cern.ch>

--- tree/dataframe/inc/ROOT/RDF/InterfaceUtils.hxx
+++ tree/dataframe/inc/ROOT/RDF/InterfaceUtils.hxx
@@ -681,6 +681,8 @@ struct IsDeque_t<std::deque<T>> : std::true_type {};
 
 void CheckForDuplicateSnapshotColumns(const ColumnNames_t &cols);
 
+void TriggerRun(ROOT::RDF::RNode &node);
+
 } // namespace RDF
 } // namespace Internal
 
