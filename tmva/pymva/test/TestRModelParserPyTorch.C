a00297b1a034409804e60195b56ce6d9d3b85938
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2021-10-27 20:43:28 +0530

    Add: Test for Parsing Conv operator from PyTorch model
    Co-authored-by: Lorenzo Moneta <Lorenzo.Moneta@cern.ch>

--- tmva/pymva/test/TestRModelParserPyTorch.C
+++ tmva/pymva/test/TestRModelParserPyTorch.C
@@ -1,5 +1,8 @@
+#include <numeric>
+
 #include "PyTorchModuleModel.hxx"
 #include "PyTorchSequentialModel.hxx"
+#include "PyTorchConvolutionModel.hxx"
 
 #include <Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
@@ -10,14 +13,14 @@
 
 constexpr float DEFAULT_TOLERANCE = 1e-6f;
 
-TEST(RModelParser_PyTorch, SEQUENTIAL)
+TEST(RModelParser_PyTorch, SEQUENTIAL_MODEL)
 {
    constexpr float TOLERANCE = DEFAULT_TOLERANCE;
    float inputSequential[]={-1.6207,  0.6133,
                              0.5058, -1.2560,
                             -0.7750, -1.6701,
                              0.8171, -0.2858};
-    TMVA_SOFIE_PyTorchModelSequential::Session s("PyTorchSequentialModel.dat");                    
+    TMVA_SOFIE_PyTorchModelSequential::Session s("PyTorchSequentialModel.dat");
     std::vector<float> outputSequential = s.infer(inputSequential);
 
     Py_Initialize();
@@ -52,7 +55,7 @@ TEST(RModelParser_PyTorch, SEQUENTIAL)
     }
 }
 
-TEST(RModelParser_PyTorch, MODULE)
+TEST(RModelParser_PyTorch, MODULE_MODEL)
 {
    constexpr float TOLERANCE = DEFAULT_TOLERANCE;
    float inputModule[]={0.5516,  0.3585,
@@ -95,5 +98,43 @@ TEST(RModelParser_PyTorch, MODULE)
     //Testing the actual and expected output tensor values
     for (size_t i = 0; i < outputModule.size(); ++i) {
       EXPECT_LE(std::abs(outputModule[i] - pOutputModule[i]), TOLERANCE);
+    }
+}
+
+TEST(RModelParser_PyTorch, CONVOLUTION_MODEL)
+{
+    constexpr float TOLERANCE = 1.E-3;
+    std::vector<float> inputConv(5*6*5*5);
+    std::iota(inputConv.begin(), inputConv.end(), 1.0f);
+
+    TMVA_SOFIE_PyTorchModelConvolution::Session s("PyTorchConvolutionModel.dat");
+    std::vector<float> outputConv = s.infer(inputConv.data());
+
+    Py_Initialize();
+    PyObject* main = PyImport_AddModule("__main__");
+    PyObject* fGlobalNS = PyModule_GetDict(main);
+    PyObject* fLocalNS = PyDict_New();
+    if (!fGlobalNS) {
+        throw std::runtime_error("Can't init global namespace for Python");
+        }
+    if (!fLocalNS) {
+        throw std::runtime_error("Can't init local namespace for Python");
+        }
+    PyRun_String("import torch",Py_single_input,fGlobalNS,fLocalNS);
+    PyRun_String("model=torch.jit.load('PyTorchModelConvolution.pt')",Py_single_input,fGlobalNS,fLocalNS);
+    PyRun_String("input=torch.arange(1,751,dtype=torch.float).reshape(5,6,5,5)",Py_single_input,fGlobalNS,fLocalNS);
+    PyRun_String("output=model(input).detach().numpy().reshape(5,5,2,2)",Py_single_input,fGlobalNS,fLocalNS);
+    PyRun_String("outputSize=output.size",Py_single_input,fGlobalNS,fLocalNS);
+    std::size_t pOutputConvSize=(std::size_t)PyLong_AsLong(PyDict_GetItemString(fLocalNS,"outputSize"));
+
+    //Testing the actual and expected output tensor sizes
+    EXPECT_EQ(outputConv.size(), pOutputConvSize);
+
+    PyArrayObject* pConvValues=(PyArrayObject*)PyDict_GetItemString(fLocalNS,"output");
+    float* pOutputConv=(float*)PyArray_DATA(pConvValues);
+
+    //Testing the actual and expected output tensor values
+    for (size_t i = 0; i < outputConv.size(); ++i) {
+      EXPECT_LE(std::abs(outputConv[i] - pOutputConv[i]), TOLERANCE);
     }
 }
