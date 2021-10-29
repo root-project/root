e0135ff10eb960f738bcecfa7e9d7e934c2a209e
Sergey Linev <S.Linev@gsi.de>
2021-10-29 13:51:30 +0200

    [openui5] update Component.js and manifest.json files
    Provide ROOT version, adjust minimal ui5 version, remove unused code

--- ui5/canv/Component.js
+++ ui5/canv/Component.js
@@ -1,18 +1,12 @@
 sap.ui.define([
-   'sap/ui/core/UIComponent' 
+   'sap/ui/core/UIComponent'
 ], function(UIComponent) {
    "use strict";
 
-   var Component = UIComponent.extend("rootui5.canv.Component", {
+   return UIComponent.extend("rootui5.canv.Component", {
       metadata : {
          manifest: "json"
-      },
-      init: function() {
-         UIComponent.prototype.init.apply(this, arguments);
-         // this.getRouter().initialize();
       }
    });
-
-   return Component;
 
 });
