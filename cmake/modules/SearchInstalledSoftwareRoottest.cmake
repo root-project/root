5728f8afdfb4c363fcb8141dbfee4b8c6b0782b8
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2021-11-01 12:15:03 +0100

    bump built-in gtest's version from 1.10 to 1.11

--- cmake/modules/SearchInstalledSoftwareRoottest.cmake
+++ cmake/modules/SearchInstalledSoftwareRoottest.cmake
@@ -29,7 +29,7 @@ if(NOT TARGET gtest)
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_SHALLOW 1
-    GIT_TAG release-1.10.0
+    GIT_TAG release-1.11.0
     UPDATE_COMMAND ""
     # TIMEOUT 10
     # # Force separate output paths for debug and release builds to allow easy
