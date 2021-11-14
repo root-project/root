e4723f445679d857bdd3b5c4dd12757f2fe31afb
Jakob Blomer <jblomer@cern.ch>
2021-11-14 14:04:56 +0100

    [ntuple] Make RNTupleModel interface simpler and more robust

--- tutorials/v7/ntuple/ntpl003_lhcbOpenData.C
+++ tutorials/v7/ntuple/ntpl003_lhcbOpenData.C
@@ -73,7 +73,10 @@ void Convert() {
       // Hand over ownership of the field to the ntuple model.  This will also create a memory location attached
       // to the model's default entry, that will be used to place the data supposed to be written
       model->AddField(std::move(field));
-
+   }
+   model->Freeze();
+   for (auto b : TRangeDynCast<TBranch>(*tree->GetListOfBranches())) {
+      auto l = static_cast<TLeaf *>(b->GetListOfLeaves()->First());
       // We connect the model's default entry's memory location for the new field to the branch, so that we can
       // fill the ntuple with the data read from the TTree
       void *fieldDataPtr = model->GetDefaultEntry()->GetValue(l->GetName()).GetRawPtr();
