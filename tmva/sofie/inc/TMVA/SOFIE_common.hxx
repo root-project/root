1db68b363db88f1d6962fb0d5144bee91a5ae183
lmoneta <lorenzo.moneta@cern.ch>
2021-11-21 18:04:09 +0100

    [TMVA][SOFIE] Use new version of im2col in the Conv operator
    With this new version the output matrix is obtained in a row-major format.
    This gives consecutive data read and data write from/to memory
    and therefore is few times faster.
    
    Furthermore,  using this new version there is no need to traspose
    anymore the input data when using Gemm and we are also faster in this operation.
    
    The new function is implemented in SOFIE_Common and it is inspired from the Caffe C++ implementation of im2col

--- tmva/sofie/inc/TMVA/SOFIE_common.hxx
+++ tmva/sofie/inc/TMVA/SOFIE_common.hxx
@@ -105,7 +105,70 @@ namespace UTILITY{
 template<typename T>
 T* Unidirectional_broadcast(const T* original_data, const std::vector<size_t> original_shape, const std::vector<size_t> target_shape);
 std::string Clean_name(std::string input_tensor_name);
+
+
+/// function to check if a >> 0 and a < MAX using a single comparison
+//// use trick casting to unsigned values so it becomes a single comparison
+inline bool is_a_ge_zero_and_a_lt_b(int a, int b) {
+   return static_cast<unsigned>(a) < static_cast<unsigned>(b);
 }
+
+
+/// im2col : efficient function to re-arrange input data of convolution to a matrix 
+/// that can be used by BLAS
+/// Use trick to loop on each element of filtered region first and follow input data layout
+/// By doing this reads and writes are of consecutive data in memory and one gains in efficiency
+/// The resulting matrix will be already transposed and can be used directly in BLAS 
+/// since output will be a matrix : (channels*kernel_h*kernel_w , output_h*output_w)
+/// Example: with an input matrix 
+///    a1 a2 a3 
+///    b1 b2 b3    and a 2x2 kernel    (k1,k2,k3,k4) and padding 1 : 
+///    c1 c2 c3  
+///     outpout will be a matrix (4 x 16)                  
+///  the routine will follow output order : 
+//     first all elements which will be operated by k1 then k2 then k3
+///  -> ( 0  0  0  0  0  a1 a2 a3 0  b1 b2 b3  0 c1 c2 c3  )    all elements for k1
+///     ( 0  0  0  0  a1 a2 a3  0 b1 b2 b3  0 c1 c2 c3  0  )     for k2 
+///     ( 0  a1 a2 a3 0  b1 b2 b3 0  c1 c2 c3  0  0  0  0  )     for k3 
+///     ( a1 a2 a3 0  b1 b2 b3  0 c1 c2 c3  0  0  0  0  0  )     for k4     
+///
+
+template <typename T>
+void Im2col(const T *data_im, const int channels, const int height, const int width, const int kernel_h,
+                const int kernel_w, const int pad_h, const int pad_w, const int stride_h, const int stride_w,
+                const int dilation_h, const int dilation_w, T *data_col)
+{
+   const int output_h = (height + 2 * pad_h - (dilation_h * (kernel_h - 1) + 1)) / stride_h + 1;
+   const int output_w = (width + 2 * pad_w - (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
+   const int channel_size = height * width;
+   for (int channel = channels; channel--; data_im += channel_size) {
+      for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
+         for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
+            int input_row = -pad_h + kernel_row * dilation_h;
+            for (int output_rows = output_h; output_rows; output_rows--) {
+               if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
+                  for (int output_cols = output_w; output_cols; output_cols--) {
+                     *(data_col++) = 0;
+                  }
+               } else {
+                  int input_col = -pad_w + kernel_col * dilation_w;
+                  for (int output_col = output_w; output_col; output_col--) {
+                     if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
+                        *(data_col++) = data_im[input_row * width + input_col];
+                     } else {
+                        *(data_col++) = 0;
+                     }
+                     input_col += stride_w;
+                  }
+               }
+               input_row += stride_h;
+            }
+         }
+      }
+   }
+}
+
+}  // end namespace UTILITY
 
 namespace BLAS{
 extern "C" void sgemm_(const char * transa, const char * transb, const int * m, const int * n, const int * k,
