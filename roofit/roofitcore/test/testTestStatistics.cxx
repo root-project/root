c9d78f763ffc77b6243c373d1d6bad665a965106
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2021-12-09 01:15:58 +0100

    [RF] Disable `DISABLED_IntegrateBins_SubRange` in testTestStatistics
    The `DISABLED_IntegrateBins_SubRange` test covers the minimization of a
    `RooBinSamplingPdf` in a sub-range.
    
    This test does not work with the new RooFit batch mode yet, and there
    was no time to fix this issue before the ROOT v6.26 branch point.

--- roofit/roofitcore/test/testTestStatistics.cxx
+++ roofit/roofitcore/test/testTestStatistics.cxx
@@ -77,7 +77,7 @@ TEST(RooNLLVar, IntegrateBins) {
 /// Prepare a RooDataSet that looks like the one that HistFactory uses:
 /// It pretends to be an unbinned dataset, but instead of single events,
 /// events are aggregated in the bin centres using weights.
-TEST(RooNLLVar, IntegrateBins_SubRange) {
+TEST(RooNLLVar, DISABLED_IntegrateBins_SubRange) {
   RooRandom::randomGenerator()->SetSeed(1337ul);
 
   RooRealVar x("x", "x", 0.1, 5.1);
