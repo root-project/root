c946ef4bbf4a29431a301e3533c80c5af67046d2
Olivier Couet <olivier.couet@cern.ch>
2021-12-16 18:54:38 +0100

    Same fix for TMathText

--- graf2d/graf/src/TMathText.cxx
+++ graf2d/graf/src/TMathText.cxx
@@ -489,12 +489,23 @@ void TMathText::GetBoundingBox(UInt_t &w, UInt_t &h, Bool_t /*angle*/)
    const TString newText = GetTitle();
    const Int_t length = newText.Length();
    const Char_t *text = newText.Data();
-   const Double_t size = GetTextSize();
 
    Double_t x0;
    Double_t y0;
    Double_t x1;
    Double_t y1;
+
+   Double_t size = GetTextSize();
+   if (fTextFont%10 == 3) {
+      UInt_t w = TMath::Abs(gPad->XtoAbsPixel(gPad->GetX2()) -
+                            gPad->XtoAbsPixel(gPad->GetX1()));
+      UInt_t h = TMath::Abs(gPad->YtoAbsPixel(gPad->GetY2()) -
+                            gPad->YtoAbsPixel(gPad->GetY1()));
+      if (w < h)
+         size = size/w;
+      else
+         size = size/h;
+   }
 
    GetSize(x0, y0, x1, y1, size, 0, text, length);
    w = (UInt_t)(TMath::Abs(gPad->XtoAbsPixel(x1) - gPad->XtoAbsPixel(x0)));
