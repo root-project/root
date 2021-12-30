1fd328a6d18c4fbbaa407b3218c579e5ac2bba25
Carsten Burgard <cburgard@cern.ch>
2021-12-30 18:22:52 +0100

    added templated functions and operators

--- roofit/hs3/src/JSONInterface.cxx
+++ roofit/hs3/src/JSONInterface.cxx
@@ -7,3 +7,9 @@ std::ostream &operator<<(std::ostream &os, JSONNode const &s)
    s.writeJSON(os);
    return os;
 }
+
+template<> int JSONNode::val_t<int>() const { return val_int(); }
+template<> float JSONNode::val_t<float>() const { return val_float(); }
+template<> double JSONNode::val_t<double>() const { return val_float(); }
+template<> bool JSONNode::val_t<bool>() const { return val_bool(); }
+template<> std::string JSONNode::val_t<std::string>() const { return val(); }  
