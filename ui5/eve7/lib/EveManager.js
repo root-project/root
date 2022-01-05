00fbe8368fc695b943f42252afc2aad4dab9ddc1
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2022-01-05 14:59:38 -0800

    Display client messages with openui5 message view and add icon indicator in the menu bar for highest serverity messages

--- ui5/eve7/lib/EveManager.js
+++ ui5/eve7/lib/EveManager.js
@@ -37,36 +37,6 @@ sap.ui.define([], function() {
 
       this.initialized = false;
       this.busyProcessingChanges = false;
-
-
-      // ---------------------------------
-      JSROOT.EVE.console = {};
-      JSROOT.EVE.console.txt = "";
-
-      JSROOT.EVE.console.stdlog = console.log.bind(console);
-      console.log = function () {
-         JSROOT.EVE.console.txt += "<p>";
-         JSROOT.EVE.console.txt += Array.from(arguments);
-         JSROOT.EVE.console.stdlog.apply(console, arguments);
-         if (JSROOT.EVE.console.refresh) JSROOT.EVE.console.refresh();
-      }
-
-      JSROOT.EVE.console.stderror = console.error.bind(console);
-      console.error = function () {
-         JSROOT.EVE.console.txt += "<p style=\"color:red;\">";
-         JSROOT.EVE.console.txt += Array.from(arguments);
-         JSROOT.EVE.console.stderror.apply(console, arguments);
-         if (JSROOT.EVE.console.refresh) JSROOT.EVE.console.refresh();
-      }
-
-      JSROOT.EVE.console.stdwarn = console.warn.bind(console);
-      console.warning = function () {
-         JSROOT.EVE.console.txt += "<p style=\"color:yellow;\">";
-         JSROOT.EVE.console.txt += Array.from(arguments);
-         JSROOT.EVE.console.stdwarn.apply(console, arguments);
-         if (JSROOT.EVE.console.refresh) JSROOT.EVE.console.refresh();
-      }
-
    }
 
    //==============================================================================
