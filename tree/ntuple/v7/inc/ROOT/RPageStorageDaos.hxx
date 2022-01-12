8ce43a71fa63992ca0f94e0a4ea72f3c301638fb
Jakob Blomer <jblomer@cern.ch>
2022-01-12 14:03:47 +0100

    [ntuple] Fix typo in code comment (NFC)
    Co-authored-by: Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>

--- tree/ntuple/v7/inc/ROOT/RPageStorageDaos.hxx
+++ tree/ntuple/v7/inc/ROOT/RPageStorageDaos.hxx
@@ -104,7 +104,7 @@ private:
    std::string fURI;
    /// Tracks the number of bytes committed to the current cluster
    std::uint64_t fNBytesCurrentCluster{0};
-   /// Used to keeo the column and field IDs issued during header serialization for the footer serialization
+   /// Used to keep the column and field IDs issued during header serialization for the footer serialization
    Internal::RNTupleSerializer::RContext fSerializationContext;
 
    RDaosNTupleAnchor fNTupleAnchor;
