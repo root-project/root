e3559fef6e235f8cb3da1d00f25704508021e413
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-01-26 10:28:51 +0100

    [DF] Remove dependency from exact column ordering from a test

--- root/dataframe/test_ROOT9975.C
+++ root/dataframe/test_ROOT9975.C
@@ -2,9 +2,13 @@ void test_ROOT9975()
 {
    ROOT::RDataFrame df("t", "ROOT9975.root");
    std::vector<std::string> types;
-   for (auto &&c : df.GetColumnNames())
+   auto cols = df.GetColumnNames();
+   std::sort(cols.begin(), cols.end());
+   for (auto &&c : cols) {
       types.emplace_back(df.GetColumnType(c));
-   const std::vector<std::string> expected_types = {"Int_t", "Int_t", "Int_t", "B", "A", "A", "Int_t", "Int_t"};
+      std::cout << df.GetColumnType(c) << " ";
+   }
+   const std::vector<std::string> expected_types = {"A", "Int_t", "B", "A", "Int_t", "Int_t", "Int_t", "Int_t"};
 
    R__ASSERT(types == expected_types);
 
