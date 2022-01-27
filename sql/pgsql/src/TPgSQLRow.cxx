b42e74c5c473bdd2b97c2d8000e76eb2c4c09704
Sergey Linev <S.Linev@gsi.de>
2022-01-27 22:11:00 +0100

    [pgsql] use less void* casting
    Keep PgSQL type names,
    Use nullptr for pointers

--- sql/pgsql/src/TPgSQLRow.cxx
+++ sql/pgsql/src/TPgSQLRow.cxx
@@ -19,10 +19,10 @@ ClassImp(TPgSQLRow);
 ////////////////////////////////////////////////////////////////////////////////
 /// Single row of query result.
 
-TPgSQLRow::TPgSQLRow(void *res, ULong_t rowHandle)
+TPgSQLRow::TPgSQLRow(PGresult *res, ULong_t rowHandle)
 {
-   fResult = (PGresult *) res;
-   fRowNum = (ULong_t) rowHandle;
+   fResult = res;
+   fRowNum = rowHandle;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -42,7 +42,7 @@ void TPgSQLRow::Close(Option_t *)
    if (!fRowNum)
       return;
 
-   fResult = 0;
+   fResult = nullptr;
    fRowNum = 0;
 }
 
@@ -82,7 +82,7 @@ ULong_t TPgSQLRow::GetFieldLength(Int_t field)
 const char *TPgSQLRow::GetField(Int_t field)
 {
    if (!IsValid(field))
-      return 0;
+      return nullptr;
 
    return PQgetvalue(fResult, fRowNum, field);
 }
