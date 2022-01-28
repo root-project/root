a6c99072b7f69859882f840bf9ea1a955e8ebfc7
Jonas Rembser <jonas.rembser@cern.ch>
2022-01-28 20:37:30 +0100

    [RF][PyROOT] Add Python mirror class templace for RooJSONFactoryWSTool

---
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_roofit/_roojsonfactorywstool.py
@@ -0,0 +1,14 @@
+# Authors:
+# * Jonas Rembser 01/2022
+
+################################################################################
+# Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.                      #
+# All rights reserved.                                                         #
+#                                                                              #
+# For the licensing terms see $ROOTSYS/LICENSE.                                #
+# For the list of contributors see $ROOTSYS/README/CREDITS.                    #
+################################################################################
+
+
+class RooJSONFactoryWSTool(object):
+    pass
