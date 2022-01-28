f0678e0d46ed5717c573fd207d8a885c9142be7b
Carsten Burgard <cburgard@cern.ch>
2022-01-28 16:26:51 +0100

    [RF] RooFitHS3 performance improvements
    This commit refactors the iterator implementation for the JSON interface
    to avoid performance issues with reading workspaces from JSON.

--- roofit/hs3/src/JSONInterface.cxx
+++ roofit/hs3/src/JSONInterface.cxx
@@ -1,7 +1,47 @@
 #include <RooFitHS3/JSONInterface.h>
 
+namespace {
+template <class Nd>
+class childItImpl : public RooFit::Experimental::JSONNode::child_iterator_t<Nd>::Impl {
+public:
+   using child_iterator = RooFit::Experimental::JSONNode::child_iterator_t<Nd>;
+   childItImpl(Nd &n, size_t p) : node(n), pos(p) {}
+   childItImpl(const childItImpl &other) : node(other.node), pos(other.pos) {}
+   virtual std::unique_ptr<typename child_iterator::Impl> clone() const override
+   {
+      return std::make_unique<childItImpl>(node, pos);
+   }
+   virtual void forward() override { ++pos; }
+   virtual void backward() override { --pos; }
+   virtual Nd &current() override { return node.child(pos); }
+   virtual bool equal(const typename child_iterator::Impl &other) const override
+   {
+      auto it = dynamic_cast<const childItImpl<Nd> *>(&other);
+      return it && &(it->node) == &(this->node) && (it->pos) == this->pos;
+   }
+
+private:
+   Nd &node;
+   size_t pos;
+};
+} // namespace
+
 namespace RooFit {
 namespace Experimental {
+
+template class JSONNode::child_iterator_t<JSONNode>;
+template class JSONNode::child_iterator_t<const JSONNode>;
+
+JSONNode::children_view JSONNode::children()
+{
+   return {child_iterator(std::make_unique<::childItImpl<JSONNode>>(*this, 0)),
+           child_iterator(std::make_unique<::childItImpl<JSONNode>>(*this, this->num_children()))};
+}
+JSONNode::const_children_view JSONNode::children() const
+{
+   return {const_child_iterator(std::make_unique<::childItImpl<const JSONNode>>(*this, 0)),
+           const_child_iterator(std::make_unique<::childItImpl<const JSONNode>>(*this, this->num_children()))};
+}
 
 std::ostream &operator<<(std::ostream &os, JSONNode const &s)
 {
@@ -35,5 +75,5 @@ std::string JSONNode::val_t<std::string>() const
    return val();
 }
 
-}
-}
+} // namespace Experimental
+} // namespace RooFit
