d2150a8d22e856766464df3a324034607ae648ef
Jonas Rembser <jonas.rembser@cern.ch>
2022-01-28 20:22:55 +0100

    [RF] Add virual destructor to `JSONNode::child_iterator_t::Impl`
    This fixes clang warnings.

--- roofit/hs3/src/JSONInterface.cxx
+++ roofit/hs3/src/JSONInterface.cxx
@@ -2,21 +2,21 @@
 
 namespace {
 template <class Nd>
-class childItImpl : public RooFit::Experimental::JSONNode::child_iterator_t<Nd>::Impl {
+class ChildItImpl final : public RooFit::Experimental::JSONNode::child_iterator_t<Nd>::Impl {
 public:
    using child_iterator = RooFit::Experimental::JSONNode::child_iterator_t<Nd>;
-   childItImpl(Nd &n, size_t p) : node(n), pos(p) {}
-   childItImpl(const childItImpl &other) : node(other.node), pos(other.pos) {}
+   ChildItImpl(Nd &n, size_t p) : node(n), pos(p) {}
+   ChildItImpl(const ChildItImpl &other) : node(other.node), pos(other.pos) {}
    virtual std::unique_ptr<typename child_iterator::Impl> clone() const override
    {
-      return std::make_unique<childItImpl>(node, pos);
+      return std::make_unique<ChildItImpl>(node, pos);
    }
    virtual void forward() override { ++pos; }
    virtual void backward() override { --pos; }
    virtual Nd &current() override { return node.child(pos); }
    virtual bool equal(const typename child_iterator::Impl &other) const override
    {
-      auto it = dynamic_cast<const childItImpl<Nd> *>(&other);
+      auto it = dynamic_cast<const ChildItImpl<Nd> *>(&other);
       return it && &(it->node) == &(this->node) && (it->pos) == this->pos;
    }
 
@@ -34,13 +34,13 @@ template class JSONNode::child_iterator_t<const JSONNode>;
 
 JSONNode::children_view JSONNode::children()
 {
-   return {child_iterator(std::make_unique<::childItImpl<JSONNode>>(*this, 0)),
-           child_iterator(std::make_unique<::childItImpl<JSONNode>>(*this, this->num_children()))};
+   return {child_iterator(std::make_unique<::ChildItImpl<JSONNode>>(*this, 0)),
+           child_iterator(std::make_unique<::ChildItImpl<JSONNode>>(*this, this->num_children()))};
 }
 JSONNode::const_children_view JSONNode::children() const
 {
-   return {const_child_iterator(std::make_unique<::childItImpl<const JSONNode>>(*this, 0)),
-           const_child_iterator(std::make_unique<::childItImpl<const JSONNode>>(*this, this->num_children()))};
+   return {const_child_iterator(std::make_unique<::ChildItImpl<const JSONNode>>(*this, 0)),
+           const_child_iterator(std::make_unique<::ChildItImpl<const JSONNode>>(*this, this->num_children()))};
 }
 
 std::ostream &operator<<(std::ostream &os, JSONNode const &s)
