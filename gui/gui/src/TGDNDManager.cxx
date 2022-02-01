4d2a756f84c93c12a8920ce694cac83fc63d3fe6
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2022-02-01 14:45:35 +0100

    clang-format again

--- gui/gui/src/TGDNDManager.cxx
+++ gui/gui/src/TGDNDManager.cxx
@@ -424,12 +424,12 @@ Bool_t TGDNDManager::HandleClientMessage(Event_t *event)
       HandleDNDLeave((Window_t) event->fUser[0]);
 
    } else if (event->fHandle == fgDNDPosition) {
-      Atom_t action = (Atom_t) event->fUser[4] ? event->fUser[4] : 1;
-      HandleDNDPosition((Window_t) event->fUser[0],
-                       (Int_t) (event->fUser[2] >> 16) & 0xFFFF, // x_root
-                       (Int_t) (event->fUser[2] & 0xFFFF),       // y_root
-                       (Atom_t) action,                          // action
-                       (Time_t) event->fUser[3]);                // timestamp
+      Atom_t action = (Atom_t)event->fUser[4] ? event->fUser[4] : 1;
+      HandleDNDPosition((Window_t)event->fUser[0],
+                        (Int_t)(event->fUser[2] >> 16) & 0xFFFF, // x_root
+                        (Int_t)(event->fUser[2] & 0xFFFF),       // y_root
+                        (Atom_t)action,                          // action
+                        (Time_t)event->fUser[3]);                // timestamp
 
    } else if (event->fHandle == fgDNDStatus) {
       Rectangle_t skip;
