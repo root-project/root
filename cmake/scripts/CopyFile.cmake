27f060f520c488406d04277a2c162cdb19ae2d81
Sergey Linev <S.Linev@gsi.de>
2022-02-04 10:06:06 +0100

    [cmake] suppress output for copying artefact files
    Only when file is updated debug output will be printed
    and real copy operation will be performed.
    Requires comparing of timestamps of files.
    
    cmake function IS_NEWER_THAN does not work, while
    file(COPY...) does not copy complete time stamp, but only seconds

--- cmake/scripts/CopyFile.cmake
+++ cmake/scripts/CopyFile.cmake
@@ -1,6 +1,22 @@
 #arguments SRC - source file
-#          TGT - target file, use only directory
+#          TGT - target file
 
-get_filename_component(dir ${TGT} DIRECTORY)
+if(NOT EXISTS "${TGT}")
+   message("Create ${TGT}")
+   set(do_copy ON)
+else()
+   # not possible to use file operation IS_NEWER_THAN while by copying of the
+   # file sub-seconds of file NOT copied !!!
+   # get time in UTC seconds, only they are preserved by cmake file copy
+   file(TIMESTAMP ${SRC} src_time "%s")
+   file(TIMESTAMP ${TGT} tgt_time "%s")
+   if(src_time GREATER tgt_time)
+      message("Update ${TGT}")
+      set(do_copy ON)
+   endif()
+endif()
 
-file(COPY ${SRC} DESTINATION ${dir})
+if(do_copy)
+   get_filename_component(dir ${TGT} DIRECTORY)
+   file(COPY ${SRC} DESTINATION ${dir})
+endif()
