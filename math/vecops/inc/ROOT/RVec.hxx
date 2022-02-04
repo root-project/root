85c159628f0dead65e0c084b1d864ec8c2359a06
Sergey Linev <S.Linev@gsi.de>
2022-02-04 16:09:01 +0100

    Fix Windows warning in RVec.hxx
    Fix Windows warning
    
    ROOT/RVec.hxx(2062,16):
    
         warning C4805: '==': unsafe mix of type 'const T' and type 'bool'
         in operation [C:\Soft\root_64\math\vecops\test\vecops_rvec.vcxproj]
        with
          [
              T=int
          ]
    
    Co-authored-by: Enrico Guiraud <enrico.guiraud@cern.ch>

--- math/vecops/inc/ROOT/RVec.hxx
+++ math/vecops/inc/ROOT/RVec.hxx
@@ -2059,7 +2059,7 @@ template <typename T>
 auto Any(const RVec<T> &v) -> decltype(v[0] == true)
 {
    for (auto &&e : v)
-      if (e == true)
+      if (static_cast<bool>(e) == true)
          return true;
    return false;
 }
@@ -2078,7 +2078,7 @@ template <typename T>
 auto All(const RVec<T> &v) -> decltype(v[0] == false)
 {
    for (auto &&e : v)
-      if (e == false)
+      if (static_cast<bool>(e) == false)
          return false;
    return true;
 }
