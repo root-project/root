3a0f81021e661191e20f1d322a32abb64efb2224
Philippe Canal <pcanal@fnal.gov>
2022-02-16 17:51:42 -0600

    Extend datamodelevolution/00/test2.cxx

--- root/io/datamodelevolution/00/test2.cxx
+++ root/io/datamodelevolution/00/test2.cxx
@@ -39,15 +39,30 @@ void test2() {
    c->a.Print();
 
    TTree *tree; f->GetObject("tree",tree);
+   cout << "TTree::Scan object branch\n";
    tree->Scan("*");
 
-   ACache *ptr = 0;
+   ACache *ptr = new ACache;
    tree->SetBranchAddress("obj",&ptr);
+   cout << "TTree::GetEntry object branch\n";
    tree->GetEntry(0);
    if (ptr==0) {
       ::Error("test2","No object read from the TTree");
    } else {
       ptr->Print();
+   }
+
+   delete ptr;
+   ACache *ptr2 = new ACache;
+   // tree->SetBranchAddress("obj",&ptr2);
+   auto b = tree->GetBranch("obj");
+   b->SetAddress(&ptr2);
+   cout << "TTree::GetEntry object branch with 'new' object.\n";
+   tree->GetEntry(0);
+   if (ptr2 == nullptr) {
+      ::Error("test2","No object read from the TTree");
+   } else {
+      ptr2->Print();
    }
 
    //tree->Print("debugInfo");
