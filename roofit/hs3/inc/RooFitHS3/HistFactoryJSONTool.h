778c5bf8ab0bf5c2c3d01dd5dc291685b225eff0
Jonas Rembser <jonas.rembser@cern.ch>
2022-02-26 13:50:46 +0100

    [RF] Hide away implementation details of HistFactoryJSONTool
    * the `JSONTool::Export` function should not be part of the public
      interface, which should only provide high-level JSON printing and
      writing functionality
    
    * the signature of the constructor was changed to take a reference
      instead of a pointer, because it can't be `nullptr`

--- roofit/hs3/inc/RooFitHS3/HistFactoryJSONTool.h
+++ roofit/hs3/inc/RooFitHS3/HistFactoryJSONTool.h
@@ -16,36 +16,25 @@
 #include <iostream>
 #include <string>
 
-namespace RooFit {
-namespace Experimental {
-class JSONNode;
-}
-} // namespace RooFit
-
 namespace RooStats {
 namespace HistFactory {
 
-class Channel;
 class Measurement;
-class Sample;
 
 class JSONTool {
-protected:
-   RooStats::HistFactory::Measurement *_measurement;
-
-   void Export(const RooStats::HistFactory::Channel &c, RooFit::Experimental::JSONNode &t) const;
-   void Export(const RooStats::HistFactory::Sample &s, RooFit::Experimental::JSONNode &t) const;
-
 public:
-   JSONTool(RooStats::HistFactory::Measurement *);
+   JSONTool(RooStats::HistFactory::Measurement &m) : _measurement(m) {}
 
    void PrintJSON(std::ostream &os = std::cout);
    void PrintJSON(std::string const &filename);
    void PrintYAML(std::ostream &os = std::cout);
    void PrintYAML(std::string const &filename);
-   void Export(RooFit::Experimental::JSONNode &t) const;
+
+private:
+   RooStats::HistFactory::Measurement &_measurement;
 };
 
 } // namespace HistFactory
 } // namespace RooStats
+
 #endif
