e094c819fc62b554f034aba4a70afe46a0657e1a
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-02-28 17:50:31 +0100

    [DF][NFC] Disable a test with jitted exceptions on windows

--- tree/dataframe/test/dataframe_vary.cxx
+++ tree/dataframe/test/dataframe_vary.cxx
@@ -77,6 +77,8 @@ TEST(RDFVary, RequireVariationsHaveConsistentType)
                 std::runtime_error);
 }
 
+// throwing exceptions from jitted code cause problems on windows
+#if !defined(_MSC_VER) || defined(R__ENABLE_BROKEN_WIN_TESTS)
 TEST(RDFVary, RequireVariationsHaveConsistentTypeJitted)
 {
    auto df = ROOT::RDataFrame(10).Define("x", [] { return 1.f; });
@@ -96,6 +98,7 @@ TEST(RDFVary, RequireVariationsHaveConsistentTypeJitted)
       EXPECT_THROW(ss2["nominal"], std::runtime_error);
    }
 }
+#endif
 
 TEST(RDFVary, RequireReturnTypeIsRVec)
 {
