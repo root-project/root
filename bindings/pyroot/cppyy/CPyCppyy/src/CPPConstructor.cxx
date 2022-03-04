0b9f6e7cb8eaeb17cc1c8985fe53dabde270c731
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2022-03-04 11:32:29 +0100

    [PyROOT] Py_TYPE is changed to an inline static function in Py3.11
    As mentioned in the Python docs:
    https://docs.python.org/3.11/whatsnew/3.11.html
    
    Already available upstream in:
    https://github.com/wlav/CPyCppyy/commit/a8f41df0618f40ecc9a2a0b5a51fd12ddf2e6673

--- bindings/pyroot/cppyy/CPyCppyy/src/CPPConstructor.cxx
+++ bindings/pyroot/cppyy/CPyCppyy/src/CPPConstructor.cxx
@@ -122,7 +122,7 @@ PyObject* CPyCppyy::CPPConstructor::Call(
             if (pyclass) {
                 self->SetSmart((PyObject*)Py_TYPE(self));
                 Py_DECREF((PyObject*)Py_TYPE(self));
-                Py_TYPE(self) = (PyTypeObject*)pyclass;
+                Py_SET_TYPE(self, (PyTypeObject*)pyclass);
             }
         }
 
