328bf4a5e054fc1c95618586dd6ae07f85cd6519
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2022-03-10 11:33:13 +0100

    [RF] Don't used fixed-size stack arrays in RooBatchCompute in CPU case
    In the CPU case in `RooBatchCompute`, we can use `std::vector` instead
    of fixed-size stack arrays to pass data around, and the `Batches` object
    can be passed to the compute functions by reference.
    
    This fixes crashes with models where the numer of inputs for a RooAbsArg
    exceeds the maximum number of parameters supported by the fixed-size
    array buffer, at least in the CPU case.
    
    In the GPU case, we now throw a nicer error if the buffers are to small,
    instead of just crashing.
    
    The `stressRooFit` tests for the CPU or GPU batch mode still pass after
    this commit.

--- roofit/batchcompute/src/Batches.h
+++ roofit/batchcompute/src/Batches.h
@@ -29,8 +29,12 @@ so that they can contain data for every kind of compute function.
 
 namespace RooBatchCompute {
 
+#ifdef __CUDACC__
+// In the CPU case we use std::vector instead of fixed size arrays to pass
+// around data, so no maximum size variables are necessary.
 constexpr uint8_t maxParams = 8;
 constexpr uint8_t maxExtraArgs = 16;
+#endif // #ifdef __CUDACC__
 constexpr uint16_t bufferSize = 64;
 
 namespace RF_ARCH {
@@ -64,22 +68,40 @@ public:
 
 class Batches {
 private:
+#ifdef __CUDACC__
+   // In the GPU case, we used fixed-size buffers to pass around input arrays by value.
    Batch _arrays[maxParams];
-   size_t _nEvents = 0;
    double _extraArgs[maxExtraArgs];
+#else
+   std::vector<Batch> _arrays;
+   std::vector<double> _extraArgs;
+#endif // #ifdef __CUDACC__
+   size_t _nEvents = 0;
    uint8_t _nBatches = 0;
-   uint8_t _nExtraArgs;
+   uint8_t _nExtraArgs = 0;
 
 public:
    RestrictArr _output = nullptr;
 
    Batches(RestrictArr output, size_t nEvents, const DataMap &varData, const VarVector &vars,
-           const ArgVector &extraArgs = {}, double stackArr[maxParams][bufferSize] = nullptr);
+           const ArgVector &extraArgs = {}, double *buffer = nullptr);
+
+#ifdef __CUDACC__
+#else
+   // As we don't pass around Batches by value in the CPU case, delete copying
+   // and moving to it's not done accidentally.
+   Batches(const Batches &) = delete;
+   Batches &operator=(const Batches &) = delete;
+   Batches(Batches &&) = delete;
+   Batches &operator=(Batches &&) = delete;
+#endif // #ifdef __CUDACC__
+
    __roodevice__ size_t getNEvents() const { return _nEvents; }
    __roodevice__ uint8_t getNExtraArgs() const { return _nExtraArgs; }
    __roodevice__ double extraArg(uint8_t i) const { return _extraArgs[i]; }
+   __roodevice__ void setExtraArg(uint8_t i, double val) { _extraArgs[i] = val; }
    __roodevice__ Batch operator[](int batchIdx) const { return _arrays[batchIdx]; }
-   inline void setNEvents(size_t n = bufferSize) { _nEvents = n; }
+   inline void setNEvents(size_t n) { _nEvents = n; }
    inline void advance(size_t nEvents)
    {
       for (int i = 0; i < _nBatches; i++)
@@ -87,6 +109,14 @@ public:
       _output += nEvents;
    }
 }; // end class Batches
+
+#ifdef __CUDACC__
+// In the GPU case, we have to pass the Batches object to the compute functions by value.
+using BatchesHandle = Batches;
+#else
+using BatchesHandle = Batches &;
+#endif // #ifdef __CUDACC__
+
 } // End namespace RF_ARCH
 } // end namespace RooBatchCompute
 #endif // #ifdef ROOFIT_BATCHCOMPUTE_BATCHES_H
