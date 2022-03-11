a06e75c1c33f01d2c13628e548080ab075af3a6a
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2022-03-11 14:08:41 +0100

    [RF] Add RooNormalizedPdf class
    A RooNormalizedPdf wraps a pdf divided by it's integral for a given
    normalization set into a new self-normalized pdf.
    
    It is an implementation detail of the RooFit batch mode with the
    RooFitDriver.

---
+++ roofit/roofitcore/src/RooNormalizedPdf.cxx
@@ -0,0 +1,38 @@
+/*
+ * Project: RooFit
+ * Authors:
+ *   Jonas Rembser, CERN 2022
+ *
+ * Copyright (c) 2022, CERN
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted according to the terms
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
+
+#include "RooNormalizedPdf.h"
+
+/**
+ * \class RooNormalizedPdf
+ *
+ * A RooNormalizedPdf wraps a pdf divided by its integral for a given
+ * normalization set into a new self-normalized pdf.
+ */
+
+void RooNormalizedPdf::computeBatch(cudaStream_t * /*stream*/, double *output, size_t nEvents,
+                                    RooBatchCompute::DataMap &dataMap) const
+{
+   auto nums = dataMap[*_pdf];
+   auto integralSpan = dataMap[*_normIntegral];
+
+   if (integralSpan.size() == 1) {
+      for (std::size_t i = 0; i < nEvents; ++i) {
+         output[i] = normalizeWithNaNPacking(nums[i], integralSpan[0]);
+      }
+   } else {
+      assert(integralSpan.size() == nEvents);
+      for (std::size_t i = 0; i < nEvents; ++i) {
+         output[i] = normalizeWithNaNPacking(nums[i], integralSpan[i]);
+      }
+   }
+}
---
+++ roofit/roofitcore/src/RooNormalizedPdf.h
@@ -0,0 +1,80 @@
+/*
+ * Project: RooFit
+ * Authors:
+ *   Jonas Rembser, CERN 2022
+ *
+ * Copyright (c) 2022, CERN
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted according to the terms
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
+
+#ifndef RooFit_RooNormalizedPdf_h
+#define RooFit_RooNormalizedPdf_h
+
+#include <RooAbsPdf.h>
+#include <RooRealProxy.h>
+
+class RooNormalizedPdf : public RooAbsPdf {
+public:
+   RooNormalizedPdf(RooAbsPdf &pdf, RooArgSet const &normSet)
+      : _pdf("numerator", "numerator", this, pdf),
+        _normIntegral("denominator", "denominator", this,
+                      *pdf.createIntegral(normSet, *pdf.getIntegratorConfig(), nullptr), true, false, true),
+        _normSet{normSet}
+   {
+      auto name = std::string(pdf.GetName()) + "_over_" + _normIntegral->GetName();
+      SetName(name.c_str());
+      SetTitle(name.c_str());
+   }
+
+   RooNormalizedPdf(const RooNormalizedPdf &other, const char *name)
+      : RooAbsPdf(other, name), _pdf("numerator", this, other._pdf),
+        _normIntegral("denominator", this, other._normIntegral), _normSet{other._normSet}
+   {
+   }
+
+   TObject *clone(const char *newname) const override { return new RooNormalizedPdf(*this, newname); }
+
+   bool selfNormalized() const override { return true; }
+
+   Bool_t forceAnalyticalInt(const RooAbsArg & /*dep*/) const override { return true; }
+   /// Forward determination of analytical integration capabilities to input p.d.f
+   Int_t getAnalyticalIntegralWN(RooArgSet &allVars, RooArgSet &analVars, const RooArgSet * /*normSet*/,
+                                 const char *rangeName = 0) const override
+   {
+      return _pdf->getAnalyticalIntegralWN(allVars, analVars, &_normSet, rangeName);
+   }
+   /// Forward calculation of analytical integrals to input p.d.f
+   Double_t analyticalIntegralWN(Int_t code, const RooArgSet * /*normSet*/, const char *rangeName = 0) const override
+   {
+      return _pdf->analyticalIntegralWN(code, &_normSet, rangeName);
+   }
+
+   ExtendMode extendMode() const override { return static_cast<RooAbsPdf &>(*_pdf).extendMode(); }
+   Double_t expectedEvents(const RooArgSet *nset) const override
+   {
+      return static_cast<RooAbsPdf &>(*_pdf).expectedEvents(nset);
+   }
+
+protected:
+   void computeBatch(cudaStream_t *, double *output, size_t size, RooBatchCompute::DataMap &) const override;
+   double evaluate() const override
+   {
+      // The RooNormalizedPdf overloads RooAbsReal::getValV() directly, so evaluate() should not be called.
+      throw std::bad_function_call();
+      return 0.0;
+   }
+   double getValV(const RooArgSet * /*normSet*/) const override
+   {
+      return normalizeWithNaNPacking(_pdf->getVal(), _normIntegral->getVal());
+   };
+
+private:
+   RooRealProxy _pdf;
+   RooRealProxy _normIntegral;
+   RooArgSet const &_normSet;
+};
+
+#endif
