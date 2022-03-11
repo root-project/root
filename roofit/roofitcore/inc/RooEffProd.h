f239f144d55436c816fc68437525199a562e761e
Jonas Rembser <jonas.rembser@cern.ch>
2022-03-11 18:39:44 +0100

    [RF] Remove integration code from RooEffProd
    The `RooEffProd` class contains some caching logic to always provide a
    normalization set to the integrated pdf. However, this should better not
    be done, because as the the former documentation sais it "breaks the
    default RooAbsPdf normalization handling". It is also inconsistent,
    because, other RooFit classes like `RooProduct` don't do that.
    
    Also, it is not the task of the RooEffProd to protect pdfs from being
    evaluated without normalization set if their shape depends on it. The
    concerned pdfs (like RooAddPdf) already do that themselves.
    
    The reason why this is removed now it that the caching of normalization
    integrals doesn't work with the new RooFit batch mode.
    
    This commit is tested by the `stressRooFit` unit test.

--- roofit/roofitcore/inc/RooEffProd.h
+++ roofit/roofitcore/inc/RooEffProd.h
@@ -20,8 +20,6 @@
 class RooEffProd: public RooAbsPdf {
 public:
   // Constructors, assignment etc
-  inline RooEffProd() : _cacheMgr(this,10), _nset(0), _fixedNset(0) { };
-  ~RooEffProd() override;
   RooEffProd(const char *name, const char *title, RooAbsPdf& pdf, RooAbsReal& efficiency);
   RooEffProd(const RooEffProd& other, const char* name=0);
 
@@ -30,47 +28,13 @@ public:
   RooAbsGenContext* genContext(const RooArgSet &vars, const RooDataSet *prototype,
                                        const RooArgSet* auxProto, Bool_t verbose) const override;
 
-  /// Return kTRUE to force RooRealIntegral to offer all observables for internal integration
-  Bool_t forceAnalyticalInt(const RooAbsArg& /*dep*/) const override {
-    return kTRUE ;
-  }
-  Int_t getAnalyticalIntegralWN(RooArgSet& allVars, RooArgSet& numVars, const RooArgSet* normSet, const char* rangeName=0) const override ;
-  Double_t analyticalIntegralWN(Int_t code, const RooArgSet* normSet, const char* rangeName=0) const override ;
-
 protected:
-
-  /// Return pointer to pdf in product
-  const RooAbsPdf* pdf() const {
-    return (RooAbsPdf*) _pdf.absArg() ;
-  }
-  /// Return pointer to efficiency function in product
-  const RooAbsReal* eff() const {
-    return (RooAbsReal*) _eff.absArg() ;
-  }
 
   // Function evaluation
   Double_t evaluate() const override ;
 
-  class CacheElem : public RooAbsCacheElement {
-  public:
-    CacheElem() : _clone(0), _int(0) {}
-    ~CacheElem() override { delete _int ; delete _clone ; }
-    // Payload
-    RooArgSet   _intObs ;
-    RooEffProd* _clone ;
-    RooAbsReal* _int ;
-    // Cache management functions
-    RooArgList containedArgs(Action) override ;
-  } ;
-  mutable RooObjCacheManager _cacheMgr ; ///<! The cache manager
-
-
-  // the real stuff...
   RooRealProxy _pdf ;               ///< Probability Density function
   RooRealProxy _eff;                ///< Efficiency function
-  mutable const RooArgSet* _nset  ; ///<! Normalization set to be used in evaluation
-
-  RooArgSet* _fixedNset ; ///<! Fixed normalization set overriding default normalization set (if provided)
 
   ClassDefOverride(RooEffProd,2) // Product operator p.d.f of (PDF x efficiency) implementing optimized generator context
 };
