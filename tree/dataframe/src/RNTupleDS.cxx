1bb1ba73745eef7fa4943c7c25533a872d4cfaea
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-03-15 12:02:36 +0100

    [DF][NFC] Document the `#var` syntax for RNTupleDS

--- tree/dataframe/src/RNTupleDS.cxx
+++ tree/dataframe/src/RNTupleDS.cxx
@@ -30,6 +30,20 @@
 #include <typeinfo>
 #include <utility>
 
+// clang-format off
+/**
+* \class ROOT::Experimental::RNTupleDS
+* \ingroup dataframe
+* \brief The RDataSource implementation for RNTuple. It lets RDataFrame read RNTuple data.
+*
+* An RDataFrame that reads RNTuple data can be constructed using MakeNTupleDataFrame().
+*
+* For each column containing an array or a collection, a corresponding column `#colname` is available to access
+* `colname.size()` without reading and deserializing the collection values.
+*
+**/
+// clang-format on
+
 namespace ROOT {
 namespace Experimental {
 namespace Internal {
