b13ee109188831ad732786f6ab79903f48a073b4
ferdymercury <ferdymercury@users.noreply.github.com>
2022-03-16 19:23:50 +0100

    simplify by pointing to manual

--- core/base/src/root-argparse.py
+++ core/base/src/root-argparse.py
@@ -22,5 +22,5 @@ An extensive Users Guide is available from that site (see below).
 	parser.add_argument('--web=<browser>', help='Display graphics in specified web browser')
 	parser.add_argument('[dir]', help='if dir is a valid directory cd to it before executing')
 	parser.add_argument('[data1.root...dataN.root]', help='Open the given ROOT files; remote protocols (such as http://) are supported')
-	parser.add_argument('[file1.C...fileN.C]', help='Execute the the ROOT macro file1.C ... fileN.C.\nArguments can be passed to the macro with round parentheses after each filename.\nBetween filename and args, add "+" to precompile if needed\n"++" to force recompile, and "g" to include debug symbols')
+	parser.add_argument('[file1.C...fileN.C]', help='Execute the the ROOT macro file1.C ... fileN.C.\nPrecompilation flags as well as macro arguments can be passed, see format in https://root.cern/manual/root_macros_and_shared_libraries/')
 	return parser
