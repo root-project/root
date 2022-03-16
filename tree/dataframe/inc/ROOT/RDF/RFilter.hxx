e863275372ed7819facb6a6f13905d16dcb9dbf8
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-03-16 21:25:19 +0100

    [DF] Make getter const, give it a better name

--- tree/dataframe/inc/ROOT/RDF/RFilter.hxx
+++ tree/dataframe/inc/ROOT/RDF/RFilter.hxx
@@ -174,7 +174,7 @@ public:
       /* If the returned node is not new, there is no need to perform any other operation.
        * This is a likely scenario when building the entire graph in which branches share
        * some nodes. */
-      if (!thisNode->GetIsNew()) {
+      if (!thisNode->IsNew()) {
          return thisNode;
       }
 
