97a043b241ba3901ca1d06b8be379b19632fe333
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-03-16 12:12:56 +0100

    [DF] Better names for fill helpers
    The generic fill helper was called FillParHelper, now it is just
    called FillHelper, which suggests it is what we use by default.
    
    The special case of the one-dim histogram filling helper that buffers
    values and delayes the actual histogram filling to the end of the
    processing had the deceptively simple name FillHelper, it is now
    called BufferedFillHelper to call out that it is doing something
    interesting.

--- tree/dataframe/inc/ROOT/RDF/Utils.hxx
+++ tree/dataframe/inc/ROOT/RDF/Utils.hxx
@@ -224,7 +224,7 @@ constexpr std::size_t CacheLineStep() {
 void CheckReaderTypeMatches(const std::type_info &colType, const std::type_info &requestedType,
                             const std::string &colName, const std::string &where);
 
-// TODO in C++17 this could be a lambda within FillParHelper::Exec
+// TODO in C++17 this could be a lambda within FillHelper::Exec
 template <typename T>
 constexpr std::size_t FindIdxTrue(const T &arr)
 {
