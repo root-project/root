c045bed4c0186229442c8e725e79f3392bca7c78
moneta <lorenzo.moneta@cern.ch>
2022-03-23 16:19:20 +0100

    Add SofieFunctor to adapt Sofie inference for RDataframe
    Add tutorial, TMVA_SOFIE_Keras_HiggsModel.C to parse the Keras trained Higgs model obtained running MVA_Higgs_Classification and generate the Sofie header file for the inference of the model
    
    Add tutorial, TMVA_SOFIE_RDataframe.C showing how one can use RDF and SOFIE to perform inference of ML models with compiled code
    Add tutorial, TMVA_SOFIE_RDataframe_JIT.C showing how one can use RDF and SOFIE to perform inference of ML models by parsing the input Keras file, generate internally the header and use it for inference
    
    Improve tutorial TMVA_SOFIE_Keras.C to parse any input given keras file.

---
+++ tutorials/tmva/TMVA_SOFIE_RDataFrame.C
@@ -0,0 +1,57 @@
+/// \file
+/// \ingroup tutorial_tmva
+/// \notebook -nodraw
+/// This macro provides an example of using a trained model with Keras
+/// and make inference using SOFIE and RDataFrame
+/// This macro uses as input a Keras model generated with the
+/// TMVA_Higgs_Classification.C tutorial
+/// You need to run that macro before to generate the trained Keras model
+/// Then you need to run the macro TMVA_SOFIE_Keras_HiggsModel.C to generate the  corresponding
+/// header file using SOFIE.
+///
+/// Execute in this order:
+/// ```
+/// root TMVA_Higgs_Classification.C
+/// root TMVA_SOFIE_Keras_HiggsModel.C
+/// root TMVA_SOFIE_RDataFrame.C
+/// ```
+///
+/// \macro_code
+/// \macro_output
+/// \author Lorenzo Moneta
+
+using namespace TMVA::Experimental;
+
+// need to add the current directory (from where we are running this macro)
+// to the include path for Cling
+R__ADD_INCLUDE_PATH($PWD)
+#include "Higgs_trained_model.hxx"
+#include "TMVA/SOFIEHelpers.hxx"
+
+using namespace TMVA::Experimental;
+
+void TMVA_SOFIE_RDataFrame(){
+
+   std::string inputFileName = "Higgs_data.root";
+   std::string inputFile = "http://root.cern.ch/files/" + inputFileName;
+
+    int nthreads = ROOT::GetThreadPoolSize();
+    ROOT::RDataFrame df1("sig_tree", inputFile);
+    auto h1 = df1.DefineSlot("DNN_Value", SofieFunctor<7,TMVA_SOFIE_Higgs_trained_model::Session>(nthreads), {"m_jj", "m_jjj", "m_lv", "m_jlv", "m_bb", "m_wbb", "m_wwbb"})
+                   .Histo1D({"h_sig", "", 100, 0, 1},"DNN_Value");
+
+    ROOT::RDataFrame df2("bkg_tree", inputFile);
+    auto h2 = df2.DefineSlot("DNN_Value", SofieFunctor<7,TMVA_SOFIE_Higgs_trained_model::Session>(nthreads), {"m_jj", "m_jjj", "m_lv", "m_jlv", "m_bb", "m_wbb", "m_wwbb"})
+                   .Histo1D({"h_bkg", "", 100, 0, 1},"DNN_Value");
+
+    h1->SetLineColor(kRed);
+    h2->SetLineColor(kBlue);
+
+    auto c1 = new TCanvas();
+    gStyle->SetOptStat(0);
+
+    h2->DrawClone();
+    h1->DrawClone("SAME");
+    c1->BuildLegend();
+
+}
