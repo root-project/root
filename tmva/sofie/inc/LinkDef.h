9c483526de2de3474e6d45982ec532555e030987
Mattias Ellert <mattias.ellert@physics.uu.se>
2022-03-24 15:23:59 +0100

    Add namespace TMVA::Experimental::SOFIE to LinkDef
    Fixes error:
    
    IncrementalExecutor::executeFunction: symbol '_ZN4TMVA12Experimental5SOFIE19ConvertTypeToStringB5cxx11ENS1_11ETensorTypeE' unresolved while linking function '_GLOBAL__sub_I_cling_module_8'!
    You are probably missing the definition of TMVA::Experimental::SOFIE::ConvertTypeToString[abi:cxx11](TMVA::Experimental::SOFIE::ETensorType)
    Maybe you need to load the corresponding shared library?
    Symbol found in '/builddir/build/BUILD/root-6.26.00/redhat-linux-build/lib/libROOTTMVASofie.so.6.26.00'; did you mean to load it with '.L /builddir/build/BUILD/root-6.26.00/redhat-linux-build/lib/libROOTTMVASofie.so.6.26.00'?

--- tmva/sofie/inc/LinkDef.h
+++ tmva/sofie/inc/LinkDef.h
@@ -7,6 +7,7 @@
 
 #pragma link C++ nestedclass;
 
+#pragma link C++ namespace TMVA::Experimental::SOFIE;
 #pragma link C++ class TMVA::Experimental::SOFIE::RModel-;
 #pragma link C++ class TMVA::Experimental::SOFIE::ROperator+;
 #pragma link C++ struct TMVA::Experimental::SOFIE::InitializedTensor+;
