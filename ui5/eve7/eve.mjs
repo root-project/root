064995436ee52e0a422d9edec3fcf127947a7b2e
Sergey Linev <S.Linev@gsi.de>
2022-03-28 10:06:08 +0200

    [eve7] move initEVE to ui5/eve7/ subfolder
    Make method async while JSROOT scripts loaded via dynamic import

---
+++ ui5/eve7/eve.mjs
@@ -0,0 +1,25 @@
+// initialization of EVE
+
+function initEVE(source_dir) {
+   if (globalThis.EVE)
+      return Promise.resolve(globalThis.EVE);
+
+   let mpath = source_dir + "modules/";
+
+   return Promise.all([import(mpath+'three.mjs'),
+                       import(mpath+'core.mjs'),
+                       import(mpath+'draw.mjs'),
+                       import(mpath+'base/TAttLineHandler.mjs'),
+                       import(mpath+'gui/menu.mjs'),
+                       import(mpath+'base/colors.mjs'),
+                       import(mpath+'base/base3d.mjs'),
+                       import(mpath+'geom/geobase.mjs'),
+                       import(mpath+'geom/TGeoPainter.mjs')])
+    .then(arr => {
+       globalThis.THREE = Object.assign({}, arr.shift());
+       globalThis.EVE = Object.assign({}, ...arr);
+       return globalThis.EVE;
+     });
+}
+
+export { initEVE };
