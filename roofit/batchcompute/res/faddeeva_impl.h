6a42c91a3571df7d918f26985b9121bc9703c6ca
Jonas Rembser <jonas.rembser@cern.ch>
2022-04-08 13:58:03 +0200

    [RF] Move `RooMath` from RooBatchCompute back into RooFitCore
    Moving `RooMath` to RooBatchCompute caused linker errors for users that
    we want to avoid: RooBatchCompute is a RooFit implementation detail and
    we don't want poeple to link against it.
    
    For this reason, RooMath is moved back into RooFitCore, and the
    Faddeeva implementation that is also used in RooBatchCompute is split
    off into a new file `faddeeva_impl.h` that is not part of the public
    interface.

--- roofit/batchcompute/res/faddeeva_impl.h
+++ roofit/batchcompute/res/faddeeva_impl.h
@@ -1,26 +1,18 @@
-/*****************************************************************************
- * Project: RooFit                                                           *
- * Package: RooFitCore                                                       *
- * @(#)root/roofitcore:$Id$
- * Authors:                                                                  *
- *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
- *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
- *                                                                           *
- * Copyright (c) 2000-2005, Regents of the University of California          *
- *                          and Stanford University. All rights reserved.    *
- *                                                                           *
- * Redistribution and use in source and binary forms,                        *
- * with or without modification, are permitted according to the terms        *
- * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
- *****************************************************************************/
+/*
+ * Project: RooFit
+ *
+ * Copyright (c) 2022, CERN
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted according to the terms
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
 
-// -- CLASS DESCRIPTION [MISC] --
-// RooMath is a singleton class implementing various mathematical
-// functions not found in TMath, mostly involving complex algebra
-//
-//
-#include "RooMath.h"
-#include "RooBatchCompute.h"
+#ifndef roofit_batchcompute_faddeeva_impl_h 
+#define roofit_batchcompute_faddeeva_impl_h 
+
+#include <RooBatchComputeTypes.h>
+#include <RooBatchCompute.h>
 
 #include <algorithm>
 #include <cmath>
@@ -473,19 +465,19 @@ __roodevice__ static const double taylorarr24[24 * 12] = {
     1.79368667683853708e-16,  9.50473084594884184e-02
     };
 
-    const double npi11[11] = { // precomputed values n * pi
+    __roodevice__ const double npi11[11] = { // precomputed values n * pi
    0.00000000000000000e+00, 3.14159265358979324e+00, 6.28318530717958648e+00,
    9.42477796076937972e+00, 1.25663706143591730e+01, 1.57079632679489662e+01,
    1.88495559215387594e+01, 2.19911485751285527e+01, 2.51327412287183459e+01,
    2.82743338823081391e+01, 3.14159265358979324e+01
     };
-    const double a11[11] = { // precomputed Fourier coefficient prefactors
+   __roodevice__  const double a11[11] = { // precomputed Fourier coefficient prefactors
    4.43113462726379007e-01, 3.79788034073635143e-01, 2.39122407410867584e-01,
    1.10599187402169792e-01, 3.75782250080904725e-02, 9.37936104296856288e-03,
    1.71974046186334976e-03, 2.31635559000523461e-04, 2.29192401420125452e-05,
    1.66589592139340077e-06, 8.89504561311882155e-08
     };
-    const double taylorarr11[11 * 6] = {
+    __roodevice__ const double taylorarr11[11 * 6] = {
    // real part imaginary part, low order coefficients last
    // nsing = 0
    -1.00000000000000000e+00,  0.00000000000000000e+00,
@@ -532,146 +524,20 @@ __roodevice__ static const double taylorarr24[24 * 12] = {
    -1.57660578509526722e-06, -4.09165023743669707e-02,
     2.00739683204152177e-07,  1.48879348585662670e-01
     };
-}
 
-__roodevice__ __roohost__ std::complex<double> RooMath::faddeeva(std::complex<double> z)
-{
-    return faddeeva_impl::faddeeva_smabmq_impl<double, 24, 6, 9>(
-       z.real(), z.imag(), 12., faddeeva_impl::a24,
-       faddeeva_impl::npi24, faddeeva_impl::taylorarr24);
-}
-
-std::complex<double> RooMath::faddeeva_fast(std::complex<double> z)
-{
-    return faddeeva_impl::faddeeva_smabmq_impl<double, 11, 3, 3>(
-       z.real(), z.imag(), 8., faddeeva_impl::a11,
-       faddeeva_impl::npi11, faddeeva_impl::taylorarr11);
-}
-
-std::complex<double> RooMath::erfc(const std::complex<double> z)
-{
-    double re = -z.real() * z.real() + z.imag() * z.imag();
-    double im = -2. * z.real() * z.imag();
-    faddeeva_impl::cexp(re, im);
-    return (z.real() >= 0.) ?
-   (std::complex<double>(re, im) *
-    faddeeva(std::complex<double>(-z.imag(), z.real()))) :
-   (2. - std::complex<double>(re, im) *
-    faddeeva(std::complex<double>(z.imag(), -z.real())));
-}
-
-std::complex<double> RooMath::erfc_fast(const std::complex<double> z)
-{
-    double re = -z.real() * z.real() + z.imag() * z.imag();
-    double im = -2. * z.real() * z.imag();
-    faddeeva_impl::cexp(re, im);
-    return (z.real() >= 0.) ?
-   (std::complex<double>(re, im) *
-    faddeeva_fast(std::complex<double>(-z.imag(), z.real()))) :
-   (2. - std::complex<double>(re, im) *
-    faddeeva_fast(std::complex<double>(z.imag(), -z.real())));
-}
-
-std::complex<double> RooMath::erf(const std::complex<double> z)
-{
-    double re = -z.real() * z.real() + z.imag() * z.imag();
-    double im = -2. * z.real() * z.imag();
-    faddeeva_impl::cexp(re, im);
-    return (z.real() >= 0.) ?
-   (1. - std::complex<double>(re, im) *
-    faddeeva(std::complex<double>(-z.imag(), z.real()))) :
-   (std::complex<double>(re, im) *
-    faddeeva(std::complex<double>(z.imag(), -z.real())) - 1.);
-}
-
-std::complex<double> RooMath::erf_fast(const std::complex<double> z)
-{
-    double re = -z.real() * z.real() + z.imag() * z.imag();
-    double im = -2. * z.real() * z.imag();
-    faddeeva_impl::cexp(re, im);
-    return (z.real() >= 0.) ?
-   (1. - std::complex<double>(re, im) *
-    faddeeva_fast(std::complex<double>(-z.imag(), z.real()))) :
-   (std::complex<double>(re, im) *
-    faddeeva_fast(std::complex<double>(z.imag(), -z.real())) - 1.);
-}
-
-
-Double_t RooMath::interpolate(Double_t ya[], Int_t n, Double_t x)
-{
-  // Interpolate array 'ya' with 'n' elements for 'x' (between 0 and 'n'-1)
-
-  // Int to Double conversion is faster via array lookup than type conversion!
-  static Double_t itod[20] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,
-               10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0} ;
-  int i,m,ns=1 ;
-  Double_t den,dif,dift/*,ho,hp,w*/,y,dy ;
-  Double_t c[20], d[20] ;
-
-  dif = fabs(x) ;
-  for(i=1 ; i<=n ; i++) {
-    if ((dift=fabs(x-itod[i-1]))<dif) {
-      ns=i ;
-      dif=dift ;
+    __roodevice__ __roohost__ inline std::complex<double> faddeeva(std::complex<double> z)
+    {
+        return faddeeva_impl::faddeeva_smabmq_impl<double, 24, 6, 9>(
+           z.real(), z.imag(), 12., faddeeva_impl::a24,
+           faddeeva_impl::npi24, faddeeva_impl::taylorarr24);
     }
-    c[i] = ya[i-1] ;
-    d[i] = ya[i-1] ;
-  }
 
-  y=ya[--ns] ;
-  for(m=1 ; m<n; m++) {
-    for(i=1 ; i<=n-m ; i++) {
-      den=(c[i+1] - d[i])/itod[m] ;
-      d[i]=(x-itod[i+m-1])*den ;
-      c[i]=(x-itod[i-1])*den ;
+    __roodevice__ __roohost__ inline std::complex<double> faddeeva_fast(std::complex<double> z)
+    {
+        return faddeeva_impl::faddeeva_smabmq_impl<double, 11, 3, 3>(
+           z.real(), z.imag(), 8., faddeeva_impl::a11,
+           faddeeva_impl::npi11, faddeeva_impl::taylorarr11);
     }
-    dy = (2*ns)<(n-m) ? c[ns+1] : d[ns--] ;
-    y += dy ;
-  }
-  return y ;
 }
 
-
-
-Double_t RooMath::interpolate(Double_t xa[], Double_t ya[], Int_t n, Double_t x)
-{
-  // Interpolate array 'ya' with 'n' elements for 'xa'
-
-  // Int to Double conversion is faster via array lookup than type conversion!
-//   static Double_t itod[20] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,
-//                10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0} ;
-  int i,m,ns=1 ;
-  Double_t den,dif,dift,ho,hp,w,y,dy ;
-  Double_t c[20], d[20] ;
-
-  dif = fabs(x-xa[0]) ;
-  for(i=1 ; i<=n ; i++) {
-    if ((dift=fabs(x-xa[i-1]))<dif) {
-      ns=i ;
-      dif=dift ;
-    }
-    c[i] = ya[i-1] ;
-    d[i] = ya[i-1] ;
-  }
-
-  y=ya[--ns] ;
-  for(m=1 ; m<n; m++) {
-    for(i=1 ; i<=n-m ; i++) {
-      ho=xa[i-1]-x ;
-      hp=xa[i-1+m]-x ;
-      w=c[i+1]-d[i] ;
-      den=ho-hp ;
-      if (den==0.) {
-        std::cerr << "In " << __func__ << "(), " << __FILE__ << ":" << __LINE__ <<
-          ": Zero distance between points not allowed." << std::endl;
-        return 0 ;
-      }
-      den = w/den ;
-      d[i]=hp*den ;
-      c[i]=ho*den;
-    }
-    dy = (2*ns)<(n-m) ? c[ns+1] : d[ns--] ;
-    y += dy ;
-  }
-  return y ;
-}
+#endif
