f9facdf1e94fea8844fc1f1486204f7401611735
Axel Naumann <Axel.Naumann@cern.ch>
2022-04-12 18:46:45 +0200

    "Update ROOT version files to v6.26/03."

--- core/foundation/inc/RVersion.h
+++ core/foundation/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.26/02"
+#define ROOT_RELEASE "6.26/03"
 #define ROOT_RELEASE_DATE "Apr 12 2022"
-#define ROOT_RELEASE_TIME "18:24:02"
+#define ROOT_RELEASE_TIME "18:46:45"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,26,2) /* 399874 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,26,3) /* 399875 */
 
 #endif
