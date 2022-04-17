53d07dd65276b9bdd2ba9564ec2ef18a58749f93
Mattias Ellert <mattias.ellert@physics.uu.se>
2022-04-17 22:17:56 +0200

    Backward compatibility with older googletest versions in EPEL

--- math/vecops/test/vecops_rvec.cxx
+++ math/vecops/test/vecops_rvec.cxx
@@ -13,6 +13,10 @@
 #include <sstream>
 #include <cmath>
 
+#ifndef INSTANTIATE_TEST_SUITE_P
+#define INSTANTIATE_TEST_SUITE_P INSTANTIATE_TEST_CASE_P
+#endif
+
 using namespace ROOT;
 using namespace ROOT::VecOps;
 using namespace ROOT::Detail::VecOps; // for `IsSmall` and `IsAdopting`
