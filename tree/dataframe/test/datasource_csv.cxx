a53d4fec922b70960f5664d6490e17b30b9b4f49
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2022-04-28 19:44:38 +0200

    [DF] Fix #9561. Allow `RCsvDS` to handle NaN values
    `RCsvDS` now supports reading empty cells and passing special keywords
    "nan" and "NaN" to be read as `nan`. Note that only type double has
    internal `nan`, other numeric types use 0 instead.
    
    If no type is specified for a column, the first non-empty cell (from the
    first 10 rows) will determine the column type, otherwise type is double.
    
    Corresponding tests added.

--- tree/dataframe/test/datasource_csv.cxx
+++ tree/dataframe/test/datasource_csv.cxx
@@ -1,6 +1,7 @@
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RCsvDS.hxx>
 #include <ROOT/TSeq.hxx>
+#include <ROOT/TestSupport.hxx>
 #include <TROOT.h>
 
 #include <gtest/gtest.h>
@@ -13,6 +14,7 @@ auto fileName0 = "RCsvDS_test_headers.csv";
 auto fileName1 = "RCsvDS_test_noheaders.csv";
 auto fileName2 = "RCsvDS_test_empty.csv";
 auto fileName3 = "RCsvDS_test_win.csv";
+auto fileName4 = "RCsvDS_test_NaNs.csv";
 
 // must use http: we cannot use https on macOS until we upgrade to the newest Davix
 // and turn on the macOS SecureTransport layer.
@@ -332,9 +334,9 @@ TEST(RCsvDS, SpecifyColumnTypes)
 
    EXPECT_THROW(
       try {
-         ROOT::RDF::MakeCsvDataFrame(fileName1, false, ',', -1LL, {{"Col2", 'L'}, {"wrong1", 'D'}, {"wrong2", 'O'}});
+         ROOT::RDF::MakeCsvDataFrame(fileName1, false, ',', -1LL, {{"Col2", 'L'}, {"wrong", 'O'}});
       } catch (const std::runtime_error &err) {
-         std::string msg = "There is no column with name \"wrong2\".\n";
+         std::string msg = "There is no column with name \"wrong\".\n";
          msg += "Since the input csv file does not contain headers, valid column names are [\"Col0\", ..., \"Col3\"].";
          EXPECT_EQ(std::string(err.what()), msg);
          throw;
@@ -343,7 +345,7 @@ TEST(RCsvDS, SpecifyColumnTypes)
 
    EXPECT_THROW(
       try {
-         ROOT::RDF::MakeCsvDataFrame(fileName1, false, ',', -1LL, {{"Col0", 'W'}, {"Col3", 'X'}});
+         ROOT::RDF::MakeCsvDataFrame(fileName1, false, ',', -1LL, {{"Col0", 'T'}, {"Col3", 'X'}});
       } catch (const std::runtime_error &err) {
          std::string msg = "Type alias 'X' is not supported.\n";
          msg += "Supported type aliases are 'O' for boolean, 'D' for double, 'L' for Long64_t, 'T' for std::string.";
@@ -351,6 +353,70 @@ TEST(RCsvDS, SpecifyColumnTypes)
          throw;
       },
       std::runtime_error);
+}
+
+TEST(RCsvDS, NaNTypeIndentification)
+{
+   RCsvDS tds(fileName4);
+
+   EXPECT_STREQ("double", tds.GetTypeName("col1").c_str());
+   EXPECT_STREQ("double", tds.GetTypeName("col2").c_str());
+   EXPECT_STREQ("bool", tds.GetTypeName("col3").c_str());
+   EXPECT_STREQ("double", tds.GetTypeName("col4").c_str());
+   EXPECT_STREQ("Long64_t", tds.GetTypeName("col5").c_str());
+   EXPECT_STREQ("Long64_t", tds.GetTypeName("col6").c_str());
+   EXPECT_STREQ("std::string", tds.GetTypeName("col7").c_str());
+   EXPECT_STREQ("std::string", tds.GetTypeName("col8").c_str());
+}
+
+TEST(RCsvDS, NanWarningChecks)
+{
+   ROOT::DisableImplicitMT(); // to allow usage of display
+
+   auto rdf = ROOT::RDF::MakeCsvDataFrame(fileName4);
+   auto d = rdf.Display<double, bool, Long64_t, std::string>({"col1", "col3", "col6", "col8"}, 12);
+
+   const std::string Warn =
+      "Column \"col3\" of type bool contains empty cell(s) or NaN(s).\n"
+      "There is no `nan` equivalent for type bool, hence `false` is stored.\n"
+      "Column \"col5\" of type Long64_t contains empty cell(s) or NaN(s).\n"
+      "There is no `nan` equivalent for type Long64_t, hence `0` is stored.\n"
+      "Column \"col6\" of type Long64_t contains empty cell(s) or NaN(s).\n"
+      "There is no `nan` equivalent for type Long64_t, hence `0` is stored.\n"
+      "Please manually set the column type to `double` (with `D`) in `MakeCsvDataFrame` to read NaNs instead.\n";
+
+   ROOT_EXPECT_WARNING(d->AsString(), "RCsvDS", Warn);
+
+   const std::string AsString = "+-----+------------+-------+------+----------------+\n"
+                                "| Row | col1       | col3  | col6 | col8           | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 0   | 3.1400000  | false | 0    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 1   | 4.1400000  | false | 2    | \"R,Data,Frame\" | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 2   | nan        | true  | 3    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 3   | 5.1400000  | true  | 4    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 4   | -6.1400000 | true  | 5    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 5   | -7.1400000 | true  | 6    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 6   | nan        | true  | 7    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 7   | nan        | true  | 8    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 8   | 3.1500000  | true  | 9    | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 9   | 5.1500000  | true  | 10   | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 10  | 6.1500000  | true  | 11   | \"nan\"          | \n"
+                                "+-----+------------+-------+------+----------------+\n"
+                                "| 11  | 6.1500000  | true  | 12   | \"nan\"          | \n"
+                                "|     |            |       |      |                | \n"
+                                "+-----+------------+-------+------+----------------+\n";
+
+   EXPECT_EQ(d->AsString(), AsString);
 }
 
 #endif // R__USE_IMT
