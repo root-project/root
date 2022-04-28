aed9d196cd794e9b3e21e0fa289fc5588f5a8f53
Sergey Linev <S.Linev@gsi.de>
2022-04-28 14:50:55 +0200

    Use override syntax in asimage

--- graf2d/asimage/inc/TASImage.h
+++ graf2d/asimage/inc/TASImage.h
@@ -82,124 +82,124 @@ public:
    TASImage();
    TASImage(UInt_t w, UInt_t h);
    TASImage(const char *file, EImageFileTypes type = kUnknown);
-   TASImage(const char *name, const Double_t *imageData, UInt_t width, UInt_t height, TImagePalette *palette = 0);
-   TASImage(const char *name, const TArrayD &imageData, UInt_t width, TImagePalette *palette = 0);
-   TASImage(const char *name, const TVectorD &imageData, UInt_t width, TImagePalette *palette = 0);
+   TASImage(const char *name, const Double_t *imageData, UInt_t width, UInt_t height, TImagePalette *palette = nullptr);
+   TASImage(const char *name, const TArrayD &imageData, UInt_t width, TImagePalette *palette = nullptr);
+   TASImage(const char *name, const TVectorD &imageData, UInt_t width, TImagePalette *palette = nullptr);
    TASImage(const TASImage &img);
    TASImage &operator=(const TASImage &img);
    virtual ~TASImage();
 
-   TObject *Clone(const char *newname) const;
+   TObject *Clone(const char *newname) const override;
 
-   void  SetEditable(Bool_t on = kTRUE) { fEditable = on; }             //*TOGGLE*
-   Bool_t IsEditable() const { return fEditable; }
-   void  Browse(TBrowser *);
-   void  SetTitle(const char *title="");                                // *MENU*
-   const char *GetTitle() const;
-   const char *GetIconName() const {  return GetTitle(); }
+   void  SetEditable(Bool_t on = kTRUE) override { fEditable = on; }             //*TOGGLE*
+   Bool_t IsEditable() const override { return fEditable; }
+   void  Browse(TBrowser *) override;
+   void  SetTitle(const char *title = "") override;                              // *MENU*
+   const char *GetTitle() const override;
+   const char *GetIconName() const override {  return GetTitle(); }
 
    // Pad conversions
    void  FromPad(TVirtualPad *pad, Int_t x = 0, Int_t y = 0,
-                 UInt_t w = 0, UInt_t h = 0);
-   void  Draw(Option_t *option = "");
-   void  Paint(Option_t *option = "");
-   Int_t DistancetoPrimitive(Int_t px, Int_t py);
-   void  ExecuteEvent(Int_t event, Int_t px, Int_t py);
-   char *GetObjectInfo(Int_t px, Int_t py) const;
+                 UInt_t w = 0, UInt_t h = 0) override;
+   void  Draw(Option_t *option = "") override;
+   void  Paint(Option_t *option = "") override;
+   Int_t DistancetoPrimitive(Int_t px, Int_t py) override;
+   void  ExecuteEvent(Int_t event, Int_t px, Int_t py) override;
+   char *GetObjectInfo(Int_t px, Int_t py) const override;
 
    // Transformations
-   void  SetPalette(const TImagePalette *palette);
-   void  Zoom(UInt_t offX, UInt_t offY, UInt_t width, UInt_t height);   //*MENU*
-   void  UnZoom();                                                      //*MENU*
-   void  Flip(Int_t flip = 180);                                        //*MENU*
-   void  Mirror(Bool_t vert = kTRUE);                                   //*MENU*
-   void  Scale(UInt_t width, UInt_t height);                            //*MENU*
+   void  SetPalette(const TImagePalette *palette) override;
+   void  Zoom(UInt_t offX, UInt_t offY, UInt_t width, UInt_t height) override;   //*MENU*
+   void  UnZoom() override;                                                      //*MENU*
+   void  Flip(Int_t flip = 180) override;                                        //*MENU*
+   void  Mirror(Bool_t vert = kTRUE) override;                                   //*MENU*
+   void  Scale(UInt_t width, UInt_t height) override;                            //*MENU*
    void  Slice(UInt_t xStart, UInt_t xEnd, UInt_t yStart, UInt_t yEnd,
-               UInt_t toWidth, UInt_t toHeight);                        //*MENU*
-   void  Tile(UInt_t width, UInt_t height);                             //*MENU*
-   void  Crop(Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0); //*MENU*
+               UInt_t toWidth, UInt_t toHeight) override;                        //*MENU*
+   void  Tile(UInt_t width, UInt_t height) override;                             //*MENU*
+   void  Crop(Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0) override; //*MENU*
    void  Pad(const char *color = "#00FFFFFF", UInt_t left = 0,
-             UInt_t right = 0, UInt_t top = 0, UInt_t bottom = 0);      //*MENU*
-   void  Blur(Double_t hr = 3, Double_t vr = 3);                        //*MENU*
-   Double_t *Vectorize(UInt_t max_colors = 256, UInt_t dither = 4, Int_t opaque_threshold = 1);
-   void  Gray(Bool_t on = kTRUE);                                       //*TOGGLE* *GETTER=IsGray
-   void  StartPaletteEditor();                                          //*MENU*
+             UInt_t right = 0, UInt_t top = 0, UInt_t bottom = 0) override;      //*MENU*
+   void  Blur(Double_t hr = 3, Double_t vr = 3) override;                        //*MENU*
+   Double_t *Vectorize(UInt_t max_colors = 256, UInt_t dither = 4, Int_t opaque_threshold = 1) override;
+   void  Gray(Bool_t on = kTRUE) override;                                       //*TOGGLE* *GETTER=IsGray
+   void  StartPaletteEditor() override;                                          //*MENU*
    void  HSV(UInt_t hue = 0, UInt_t radius = 360, Int_t H = 0, Int_t S = 0, Int_t V = 0,
-             Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0);
-   void  Merge(const TImage *im, const char *op = "alphablend", Int_t x = 0, Int_t y = 0);
-   void  Append(const TImage *im, const char * option = "+", const char *color = "#00000000");
+             Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0) override;
+   void  Merge(const TImage *im, const char *op = "alphablend", Int_t x = 0, Int_t y = 0) override;
+   void  Append(const TImage *im, const char * option = "+", const char *color = "#00000000") override;
    void  Gradient(UInt_t angle = 0, const char *colors = "#FFFFFF #000000", const char *offsets = 0,
-                  Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0);
+                  Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0) override;
    void  Bevel(Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0, const char *hi = "#ffdddddd",
-               const char *lo = "#ff555555", UShort_t thick = 1, Bool_t pressed = kFALSE);
+               const char *lo = "#ff555555", UShort_t thick = 1, Bool_t pressed = kFALSE) override;
    void  DrawText(Int_t  x = 0, Int_t y = 0, const char *text = "", Int_t size = 12,
                   const char *color = 0, const char *font = "fixed", EText3DType type = TImage::kPlain,
-                  const char *fore_file = 0, Float_t angle = 0);
-   void DrawText(TText *text, Int_t x = 0, Int_t y = 0);
+                  const char *fore_file = 0, Float_t angle = 0) override;
+   void DrawText(TText *text, Int_t x = 0, Int_t y = 0) override;
 
    // Vector graphics
-   void  BeginPaint(Bool_t fast = kTRUE);
-   void  EndPaint();
-   void  DrawLine(UInt_t x1, UInt_t y1, UInt_t x2, UInt_t y2, const char *col = "#000000", UInt_t thick = 1);
-   void  DrawDashLine(UInt_t x1, UInt_t y1, UInt_t x2, UInt_t y2, UInt_t nDash, const char *pDash, const char *col = "#000000", UInt_t thick = 1);
-   void  DrawBox(Int_t x1, Int_t y1, Int_t x2, Int_t y2, const char *col = "#000000", UInt_t thick = 1, Int_t mode = 0);
-   void  DrawRectangle(UInt_t x, UInt_t y, UInt_t w, UInt_t h, const char *col = "#000000", UInt_t thick = 1);
-   void  FillRectangle(const char *col = 0, Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0);
-   void  DrawPolyLine(UInt_t nn, TPoint *xy, const char *col = "#000000", UInt_t thick = 1, TImage::ECoordMode mode = kCoordModeOrigin);
-   void  PutPixel(Int_t x, Int_t y, const char *col = "#000000");
-   void  PolyPoint(UInt_t npt, TPoint *ppt, const char *col = "#000000", TImage::ECoordMode mode = kCoordModeOrigin);
-   void  DrawSegments(UInt_t nseg, Segment_t *seg, const char *col = "#000000", UInt_t thick = 1);
-   void  FillPolygon(UInt_t npt, TPoint *ppt, const char *col = "#000000", const char *stipple = 0, UInt_t w = 16, UInt_t h = 16);
-   void  FillPolygon(UInt_t npt, TPoint *ppt, TImage *tile);
-   void  CropPolygon(UInt_t npt, TPoint *ppt);
-   void  DrawFillArea(UInt_t npt, TPoint *ppt, const char *col = "#000000", const char *stipple = 0, UInt_t w = 16, UInt_t h = 16);
-   void  DrawFillArea(UInt_t npt, TPoint *ppt, TImage *tile);
-   void  FillSpans(UInt_t npt, TPoint *ppt, UInt_t *widths, const char *col = "#000000", const char *stipple = 0, UInt_t w = 16, UInt_t h = 16);
-   void  FillSpans(UInt_t npt, TPoint *ppt, UInt_t *widths, TImage *tile);
-   void  CropSpans(UInt_t npt, TPoint *ppt, UInt_t *widths);
-   void  CopyArea(TImage *dst, Int_t xsrc, Int_t ysrc, UInt_t w, UInt_t h, Int_t xdst = 0, Int_t ydst = 0, Int_t gfunc = 3, EColorChan chan = kAllChan);
-   void  DrawCellArray(Int_t x1, Int_t y1, Int_t x2, Int_t y2, Int_t nx, Int_t ny, UInt_t *ic);
-   void  FloodFill(Int_t x, Int_t y, const char *col, const char *min_col, const char *max_col = 0);
-   void  DrawCubeBezier(Int_t x1, Int_t y1, Int_t x2, Int_t y2, Int_t x3, Int_t y3, const char *col = "#000000", UInt_t thick = 1);
-   void  DrawStraightEllips(Int_t x, Int_t y, Int_t rx, Int_t ry, const char *col = "#000000", Int_t thick = 1);
-   void  DrawCircle(Int_t x, Int_t y, Int_t r, const char *col = "#000000", Int_t thick = 1);
-   void  DrawEllips(Int_t x, Int_t y, Int_t rx, Int_t ry, Int_t angle, const char *col = "#000000", Int_t thick = 1);
-   void  DrawEllips2(Int_t x, Int_t y, Int_t rx, Int_t ry, Int_t angle, const char *col = "#000000", Int_t thick = 1);
+   void  BeginPaint(Bool_t fast = kTRUE) override;
+   void  EndPaint() override;
+   void  DrawLine(UInt_t x1, UInt_t y1, UInt_t x2, UInt_t y2, const char *col = "#000000", UInt_t thick = 1) override;
+   void  DrawDashLine(UInt_t x1, UInt_t y1, UInt_t x2, UInt_t y2, UInt_t nDash, const char *pDash, const char *col = "#000000", UInt_t thick = 1) override;
+   void  DrawBox(Int_t x1, Int_t y1, Int_t x2, Int_t y2, const char *col = "#000000", UInt_t thick = 1, Int_t mode = 0) override;
+   void  DrawRectangle(UInt_t x, UInt_t y, UInt_t w, UInt_t h, const char *col = "#000000", UInt_t thick = 1) override;
+   void  FillRectangle(const char *col = 0, Int_t x = 0, Int_t y = 0, UInt_t width = 0, UInt_t height = 0) override;
+   void  DrawPolyLine(UInt_t nn, TPoint *xy, const char *col = "#000000", UInt_t thick = 1, TImage::ECoordMode mode = kCoordModeOrigin) override;
+   void  PutPixel(Int_t x, Int_t y, const char *col = "#000000") override;
+   void  PolyPoint(UInt_t npt, TPoint *ppt, const char *col = "#000000", TImage::ECoordMode mode = kCoordModeOrigin) override;
+   void  DrawSegments(UInt_t nseg, Segment_t *seg, const char *col = "#000000", UInt_t thick = 1) override;
+   void  FillPolygon(UInt_t npt, TPoint *ppt, const char *col = "#000000", const char *stipple = nullptr, UInt_t w = 16, UInt_t h = 16) override;
+   void  FillPolygon(UInt_t npt, TPoint *ppt, TImage *tile) override;
+   void  CropPolygon(UInt_t npt, TPoint *ppt) override;
+   void  DrawFillArea(UInt_t npt, TPoint *ppt, const char *col = "#000000", const char *stipple = nullptr, UInt_t w = 16, UInt_t h = 16) override;
+   void  DrawFillArea(UInt_t npt, TPoint *ppt, TImage *tile) override;
+   void  FillSpans(UInt_t npt, TPoint *ppt, UInt_t *widths, const char *col = "#000000", const char *stipple = nullptr, UInt_t w = 16, UInt_t h = 16) override;
+   void  FillSpans(UInt_t npt, TPoint *ppt, UInt_t *widths, TImage *tile) override;
+   void  CropSpans(UInt_t npt, TPoint *ppt, UInt_t *widths) override;
+   void  CopyArea(TImage *dst, Int_t xsrc, Int_t ysrc, UInt_t w, UInt_t h, Int_t xdst = 0, Int_t ydst = 0, Int_t gfunc = 3, EColorChan chan = kAllChan) override;
+   void  DrawCellArray(Int_t x1, Int_t y1, Int_t x2, Int_t y2, Int_t nx, Int_t ny, UInt_t *ic) override;
+   void  FloodFill(Int_t x, Int_t y, const char *col, const char *min_col, const char *max_col = nullptr) override;
+   void  DrawCubeBezier(Int_t x1, Int_t y1, Int_t x2, Int_t y2, Int_t x3, Int_t y3, const char *col = "#000000", UInt_t thick = 1) override;
+   void  DrawStraightEllips(Int_t x, Int_t y, Int_t rx, Int_t ry, const char *col = "#000000", Int_t thick = 1) override;
+   void  DrawCircle(Int_t x, Int_t y, Int_t r, const char *col = "#000000", Int_t thick = 1) override;
+   void  DrawEllips(Int_t x, Int_t y, Int_t rx, Int_t ry, Int_t angle, const char *col = "#000000", Int_t thick = 1) override;
+   void  DrawEllips2(Int_t x, Int_t y, Int_t rx, Int_t ry, Int_t angle, const char *col = "#000000", Int_t thick = 1) override;
 
    // Input / output
-   void  ReadImage(const char *file, EImageFileTypes type = TImage::kUnknown);
-   void  WriteImage(const char *file, EImageFileTypes type = TImage::kUnknown); //*MENU*
-   void  SetImage(const Double_t *imageData, UInt_t width, UInt_t height, TImagePalette *palette = 0);
-   void  SetImage(const TArrayD &imageData, UInt_t width, TImagePalette *palette = 0);
-   void  SetImage(const TVectorD &imageData, UInt_t width, TImagePalette *palette = 0);
-   void  SetImage(Pixmap_t pxm, Pixmap_t mask = 0);
-   void  FromWindow(Drawable_t wid, Int_t x = 0, Int_t y = 0, UInt_t w = 0, UInt_t h = 0);
-   void  FromGLBuffer(UChar_t* buf, UInt_t w, UInt_t h);
+   void  ReadImage(const char *file, EImageFileTypes type = TImage::kUnknown) override;
+   void  WriteImage(const char *file, EImageFileTypes type = TImage::kUnknown) override; //*MENU*
+   void  SetImage(const Double_t *imageData, UInt_t width, UInt_t height, TImagePalette *palette = nullptr) override;
+   void  SetImage(const TArrayD &imageData, UInt_t width, TImagePalette *palette = nullptr) override;
+   void  SetImage(const TVectorD &imageData, UInt_t width, TImagePalette *palette = nullptr) override;
+   void  SetImage(Pixmap_t pxm, Pixmap_t mask = 0) override;
+   void  FromWindow(Drawable_t wid, Int_t x = 0, Int_t y = 0, UInt_t w = 0, UInt_t h = 0) override;
+   void  FromGLBuffer(UChar_t* buf, UInt_t w, UInt_t h) override;
 
    // Utilities
-   UInt_t     GetWidth() const;
-   UInt_t     GetHeight() const;
+   UInt_t     GetWidth() const override;
+   UInt_t     GetHeight() const override;
    UInt_t     GetScaledWidth() const;
    UInt_t     GetScaledHeight() const;
-   Bool_t     IsValid() const { return fImage ? kTRUE : kFALSE; }
-   Bool_t     IsGray() const { return fIsGray; }
+   Bool_t     IsValid() const override { return fImage ? kTRUE : kFALSE; }
+   Bool_t     IsGray() const override { return fIsGray; }
    ASImage   *GetImage() const { return fImage; }
    void       SetImage(ASImage *image) { DestroyImage(); fImage = image; }
-   TImage    *GetScaledImage() const { return fScaledImage; }
-   Pixmap_t   GetPixmap();
-   Pixmap_t   GetMask();
-   TArrayL   *GetPixels(Int_t x = 0, Int_t y = 0, UInt_t w = 0, UInt_t h = 0);
-   TArrayD   *GetArray(UInt_t w = 0, UInt_t h = 0, TImagePalette *pal = gWebImagePalette);
-   UInt_t    *GetArgbArray();
-   UInt_t    *GetRgbaArray();
-   Double_t  *GetVecArray();
-   UInt_t    *GetScanline(UInt_t y);
-   void       GetImageBuffer(char **buffer, int *size, EImageFileTypes type = TImage::kPng);
+   TImage    *GetScaledImage() const override { return fScaledImage; }
+   Pixmap_t   GetPixmap() override;
+   Pixmap_t   GetMask() override;
+   TArrayL   *GetPixels(Int_t x = 0, Int_t y = 0, UInt_t w = 0, UInt_t h = 0) override;
+   TArrayD   *GetArray(UInt_t w = 0, UInt_t h = 0, TImagePalette *pal = gWebImagePalette) override;
+   UInt_t    *GetArgbArray() override;
+   UInt_t    *GetRgbaArray() override;
+   Double_t  *GetVecArray() override;
+   UInt_t    *GetScanline(UInt_t y) override;
+   void       GetImageBuffer(char **buffer, int *size, EImageFileTypes type = TImage::kPng) override;
    void       GetZoomPosition(UInt_t &x, UInt_t &y, UInt_t &w, UInt_t &h) const;
-   Bool_t     SetImageBuffer(char **buffer, EImageFileTypes type = TImage::kPng);
-   void       PaintImage(Drawable_t wid, Int_t x, Int_t y, Int_t xsrc = 0, Int_t ysrc = 0, UInt_t wsrc = 0, UInt_t hsrc = 0, Option_t *opt = "");
-   void       SetPaletteEnabled(Bool_t on = kTRUE);  // *TOGGLE*
-   void       SavePrimitive(std::ostream &out, Option_t *option = "");
+   Bool_t     SetImageBuffer(char **buffer, EImageFileTypes type = TImage::kPng) override;
+   void       PaintImage(Drawable_t wid, Int_t x, Int_t y, Int_t xsrc = 0, Int_t ysrc = 0, UInt_t wsrc = 0, UInt_t hsrc = 0, Option_t *opt = "") override;
+   void       SetPaletteEnabled(Bool_t on = kTRUE) override;  // *TOGGLE*
+   void       SavePrimitive(std::ostream &out, Option_t *option = "") override;
 
    static const ASVisual *GetVisual();
    static UInt_t AlphaBlend(UInt_t bot, UInt_t top);
@@ -208,7 +208,7 @@ public:
    // some static functions
    Bool_t SetJpegDpi(const char *name, UInt_t dpi = 72);
 
-   ClassDef(TASImage,2)  // image processing class
+   ClassDefOverride(TASImage,2)  // image processing class
 };
 
 #endif
