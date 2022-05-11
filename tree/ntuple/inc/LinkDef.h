14a51075551fd6227f6c0ee302392ca04cfa6846
Jakob Blomer <jblomer@cern.ch>
2022-05-11 09:51:10 +0200

    [ntuple] Remove unused linkdef pragma

--- tree/ntuple/inc/LinkDef.h
+++ tree/ntuple/inc/LinkDef.h
@@ -14,7 +14,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class ROOT::Experimental::Internal::RFileNTupleAnchor + ;
 #pragma link C++ class ROOT::Experimental::RNTuple - ;
 
 #endif
