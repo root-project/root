f4f7c66f0c3e45ae5fabb7280b560ca545570200
Zeff020 <zefwolffs@gmail.com>
2022-06-01 10:41:26 +0200

    Cache-and-tracking in teststatistics likelihoods
    Co-authored-by: Jonas Rembser <jonas.rembser@cern.ch>
    Co-authored-by: Patrick Bos <egpbos@gmail.com>

--- roofit/roofitcore/src/TestStatistics/RooUnbinnedL.cxx
+++ roofit/roofitcore/src/TestStatistics/RooUnbinnedL.cxx
@@ -31,8 +31,7 @@ In extended mode, a
 #include "RooAbsDataStore.h"
 #include "RooNLLVar.h"  // RooNLLVar::ComputeScalar
 #include "RunContext.h"
-
-#include "Math/Util.h" // KahanSum
+#include "RooChangeTracker.h"
 
 namespace RooFit {
 namespace TestStatistics {
@@ -42,13 +41,18 @@ RooUnbinnedL::RooUnbinnedL(RooAbsPdf *pdf, RooAbsData *data, RooAbsL::Extended e
    : RooAbsL(RooAbsL::ClonePdfData{pdf, data}, data->numEntries(), 1, extended),
      useBatchedEvaluations_(useBatchedEvaluations)
 {
+   std::unique_ptr<RooArgSet> params(pdf->getParameters(data));
+   paramTracker_ = std::make_unique<RooChangeTracker>("chtracker","change tracker",*params,true);
 }
 
 RooUnbinnedL::RooUnbinnedL(const RooUnbinnedL &other)
    : RooAbsL(other), apply_weight_squared(other.apply_weight_squared), _first(other._first),
      useBatchedEvaluations_(other.useBatchedEvaluations_)
 {
+   paramTracker_ = std::make_unique<RooChangeTracker>(*other.paramTracker_);
 }
+
+RooUnbinnedL::~RooUnbinnedL() = default;
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -83,7 +87,10 @@ RooUnbinnedL::evaluatePartition(Section events, std::size_t /*components_begin*/
    ROOT::Math::KahanSum<double> result;
    double sumWeight;
 
-   data_->store()->recalculateCache(nullptr, events.begin(N_events_), events.end(N_events_), 1, kTRUE);
+   // Do not reevaluate likelihood if parameters have not changed
+   if (!paramTracker_->hasChanged(true) & (cachedResult_ != 0)) return cachedResult_;
+
+   data_->store()->recalculateCache(nullptr, events.begin(N_events_), events.end(N_events_), 1, true);
 
    if (useBatchedEvaluations_) {
       std::unique_ptr<RooBatchCompute::RunContext> evalData;
@@ -161,6 +168,7 @@ RooUnbinnedL::evaluatePartition(Section events, std::size_t /*components_begin*/
       pdf_->wireAllCaches();
    }
 
+   cachedResult_ = result;
    return result;
 }
 
