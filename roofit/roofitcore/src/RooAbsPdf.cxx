0e14a10f82cb9f26704b2f4b539a308f144e8ea7
Jonas Rembser <jonas.rembser@cern.ch>
2022-06-10 21:25:35 +0200

    [RF] Implement likelihood offsetting in new RooFit BatchMode
    This is necessary to make the more complicated fits converge, like the
    ATLAS Higgs combination fits.

--- roofit/roofitcore/src/RooAbsPdf.cxx
+++ roofit/roofitcore/src/RooAbsPdf.cxx
@@ -1118,7 +1118,8 @@ RooAbsReal* RooAbsPdf::createNLL(RooAbsData& data, const RooLinkedList& cmdList)
                                                projDeps,
                                                ext,
                                                pc.getDouble("IntegrateBins"),
-                                               batchMode).release();
+                                               batchMode,
+                                               doOffset).release();
   }
 
   // Construct NLL
