cffcdb578b39d64079bb906729b2b1bdb3415d5b
Petr Stepanov <stepanovps@gmail.com>
2022-06-17 09:15:25 -0400

    Update DataSetInfo.cxx
    Fixed formatting issues

--- tmva/tmva/src/DataSetInfo.cxx
+++ tmva/tmva/src/DataSetInfo.cxx
@@ -250,13 +250,13 @@ void TMVA::DataSetInfo::AddVariablesArray(const TString &expression, Int_t size,
 
       // move "external" pointer to the next variable in the array
       if (varType == 'F') {
-         float* ptr = (float*) external;
+         float* ptr = (float *)external;
          ++ptr;
-         external = (void*) ptr;
+         external = (void *) ptr;
       } else if (varType == 'I') {
-         int* ptr = (int*) external;
+         int* ptr = (int *)external;
          ++ptr;
-         external = (void*) ptr;
+         external = (void *)ptr;
       } else {
          Error("TMVA::DataSetInfo::AddVariablesArray", "'%c' variable type is not supported", varType);
       }
