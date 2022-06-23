5cc3d294343821191f29b97d79e14bc5af072bc6
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2022-06-23 18:39:17 +0200

    [DF][RDatasetSpec] Avoid meaningless moves

--- tree/dataframe/src/RDatasetSpec.cxx
+++ tree/dataframe/src/RDatasetSpec.cxx
@@ -15,16 +15,15 @@ namespace ROOT {
 namespace RDF {
 
 RDatasetSpec::RDatasetSpec(const std::string &treeName, const std::string &fileNameGlob, const REntryRange &entryRange)
-   : fTreeNames(std::vector<std::string>{std::move(treeName)}),
-     fFileNameGlobs(std::vector<std::string>{std::move(fileNameGlob)}), fStartEntry(entryRange.fStartEntry),
+   : fTreeNames({treeName}), fFileNameGlobs({fileNameGlob}), fStartEntry(entryRange.fStartEntry),
      fEndEntry(entryRange.fEndEntry)
 {
 }
 
 RDatasetSpec::RDatasetSpec(const std::string &treeName, const std::vector<std::string> &fileNameGlobs,
                            const REntryRange &entryRange)
-   : fTreeNames(std::vector<std::string>{std::move(treeName)}), fFileNameGlobs(std::move(fileNameGlobs)),
-     fStartEntry(entryRange.fStartEntry), fEndEntry(entryRange.fEndEntry)
+   : fTreeNames({treeName}), fFileNameGlobs(fileNameGlobs), fStartEntry(entryRange.fStartEntry),
+     fEndEntry(entryRange.fEndEntry)
 {
 }
 
@@ -35,30 +34,30 @@ RDatasetSpec::RDatasetSpec(const std::vector<std::pair<std::string, std::string>
    fTreeNames.reserve(treeAndFileNameGlobs.size());
    fFileNameGlobs.reserve(treeAndFileNameGlobs.size());
    for (auto &p : treeAndFileNameGlobs) {
-      fTreeNames.emplace_back(std::move(p.first));
-      fFileNameGlobs.emplace_back(std::move(p.second));
+      fTreeNames.emplace_back(p.first);
+      fFileNameGlobs.emplace_back(p.second);
    }
 }
 
 void RDatasetSpec::AddFriend(const std::string &treeName, const std::string &fileNameGlob, const std::string &alias)
 {
-   fFriendInfo.fFriendNames.emplace_back(std::make_pair(std::move(treeName), std::move(alias)));
-   fFriendInfo.fFriendFileNames.emplace_back(std::vector<std::string>{std::move(fileNameGlob)});
+   fFriendInfo.fFriendNames.emplace_back(std::make_pair(treeName, alias));
+   fFriendInfo.fFriendFileNames.emplace_back(std::vector<std::string>{fileNameGlob});
    fFriendInfo.fFriendChainSubNames.emplace_back(); // this is a tree
 }
 
 void RDatasetSpec::AddFriend(const std::string &treeName, const std::vector<std::string> &fileNameGlobs,
                              const std::string &alias)
 {
-   fFriendInfo.fFriendNames.emplace_back(std::make_pair("", std::move(alias)));
-   fFriendInfo.fFriendFileNames.emplace_back(std::move(fileNameGlobs));
-   fFriendInfo.fFriendChainSubNames.emplace_back(std::vector<std::string>(fileNameGlobs.size(), std::move(treeName)));
+   fFriendInfo.fFriendNames.emplace_back(std::make_pair("", alias));
+   fFriendInfo.fFriendFileNames.emplace_back(fileNameGlobs);
+   fFriendInfo.fFriendChainSubNames.emplace_back(std::vector<std::string>(fileNameGlobs.size(), treeName));
 }
 
 void RDatasetSpec::AddFriend(const std::vector<std::pair<std::string, std::string>> &treeAndFileNameGlobs,
                              const std::string &alias)
 {
-   fFriendInfo.fFriendNames.emplace_back(std::make_pair("", std::move(alias)));
+   fFriendInfo.fFriendNames.emplace_back(std::make_pair("", alias));
    fFriendInfo.fFriendFileNames.emplace_back();
    fFriendInfo.fFriendChainSubNames.emplace_back();
    auto &fileNames = fFriendInfo.fFriendFileNames.back();
@@ -66,8 +65,8 @@ void RDatasetSpec::AddFriend(const std::vector<std::pair<std::string, std::strin
    fileNames.reserve(treeAndFileNameGlobs.size());
    chainSubNames.reserve(treeAndFileNameGlobs.size());
    for (auto &p : treeAndFileNameGlobs) {
-      chainSubNames.emplace_back(std::move(p.first));
-      fileNames.emplace_back(std::move(p.second));
+      chainSubNames.emplace_back(p.first);
+      fileNames.emplace_back(p.second);
    }
 }
 
