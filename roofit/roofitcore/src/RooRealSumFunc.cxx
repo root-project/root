68ea2be17b22f368298dfebda46a9360f3597ce8
Jonas Rembser <jonas.rembser@cern.ch>
2022-06-28 00:30:40 +0200

    [RF][squash-on-merge] Avoid code duplication in RooRealSumPdf/Func

--- roofit/roofitcore/src/RooRealSumFunc.cxx
+++ roofit/roofitcore/src/RooRealSumFunc.cxx
@@ -34,18 +34,8 @@
 /// - RooRealSumFunc is a sum of functions. It is neither normalised, nor need it be positive.
 
 #include "RooRealSumFunc.h"
-
-#include "RooRealVar.h"
-#include "RooAddGenContext.h"
-#include "RooRealConstant.h"
-#include "RooRealIntegral.h"
-#include "RooMsgService.h"
-#include "RooNameReg.h"
+#include "RooRealSumPdf.h"
 #include "RooTrace.h"
-
-#include "Riostream.h"
-
-#include <memory>
 
 ClassImp(RooRealSumFunc);
 
@@ -71,8 +61,7 @@ RooRealSumFunc::RooRealSumFunc(const char *name, const char *title)
 //_____________________________________________________________________________
 RooRealSumFunc::RooRealSumFunc(const char *name, const char *title, RooAbsReal &func1, RooAbsReal &func2,
                                RooAbsReal &coef1)
-   : RooAbsReal(name, title), _normIntMgr(this, 10), _haveLastCoef(false),
-     _funcList("!funcList", "List of functions", this), _coefList("!coefList", "List of coefficients", this)
+   : RooRealSumFunc{name, title}
 {
    // Construct p.d.f consisting of coef1*func1 + (1-coef1)*func2
    // The input coefficients and functions are allowed to be negative
@@ -89,8 +78,7 @@ RooRealSumFunc::RooRealSumFunc(const char *name, const char *title, RooAbsReal &
 //_____________________________________________________________________________
 RooRealSumFunc::RooRealSumFunc(const char *name, const char *title, const RooArgList &inFuncList,
                                const RooArgList &inCoefList)
-   : RooAbsReal(name, title), _normIntMgr(this, 10), _haveLastCoef(false),
-     _funcList("!funcList", "List of functions", this), _coefList("!coefList", "List of coefficients", this)
+   : RooRealSumFunc{name, title}
 {
    // Constructor p.d.f implementing sum_i [ coef_i * func_i ], if N_coef==N_func
    // or sum_i [ coef_i * func_i ] + (1 - sum_i [ coef_i ] )* func_N if Ncoef==N_func-1
@@ -98,51 +86,7 @@ RooRealSumFunc::RooRealSumFunc(const char *name, const char *title, const RooArg
    // All coefficients and functions are allowed to be negative
    // but the sum is not, which is enforced at runtime.
 
-   const std::string ownName(GetName() ? GetName() : "");
-   if (!(inFuncList.getSize() == inCoefList.getSize() + 1 || inFuncList.getSize() == inCoefList.getSize())) {
-      coutE(InputArguments) << "RooRealSumFunc::RooRealSumFunc(" << ownName
-                            << ") number of pdfs and coefficients inconsistent, must have Nfunc=Ncoef or Nfunc=Ncoef+1"
-                            << "\n";
-      assert(0);
-   }
-
-   // Constructor with N functions and N or N-1 coefs
-
-   std::string funcName;
-   for (unsigned int i = 0; i < inCoefList.size(); ++i) {
-      const auto& func = inFuncList[i];
-      const auto& coef = inCoefList[i];
-
-      if (!dynamic_cast<RooAbsReal const*>(&coef)) {
-         const std::string coefName(coef.GetName() ? coef.GetName() : "");
-         coutW(InputArguments) << "RooRealSumFunc::RooRealSumFunc(" << ownName << ") coefficient " << coefName
-                               << " is not of type RooAbsReal, ignored"
-                               << "\n";
-         continue;
-      }
-      if (!dynamic_cast<RooAbsReal const*>(&func)) {
-         funcName = (func.GetName() ? func.GetName() : "");
-         coutW(InputArguments) << "RooRealSumFunc::RooRealSumFunc(" << ownName << ") func " << funcName
-                               << " is not of type RooAbsReal, ignored"
-                               << "\n";
-         continue;
-      }
-      _funcList.add(func);
-      _coefList.add(coef);
-   }
-
-   if (inFuncList.size() == inCoefList.size() + 1) {
-      const auto& func = inFuncList[inFuncList.size()-1];
-      if (!dynamic_cast<RooAbsReal const*>(&func)) {
-         funcName = (func.GetName() ? func.GetName() : "");
-         coutE(InputArguments) << "RooRealSumFunc::RooRealSumFunc(" << ownName << ") last func " << funcName
-                               << " is not of type RooAbsReal, fatal error\n";
-         throw std::invalid_argument("RooRealSumFunc: Function passed as is not of type RooAbsReal.");
-      }
-      _funcList.add(func);
-   } else {
-      _haveLastCoef = true;
-   }
+   RooRealSumPdf::initializeFuncsAndCoefs(*this, inFuncList, inCoefList, _funcList, _coefList);
 
    TRACE_CREATE
 }
@@ -199,17 +143,7 @@ std::list<double> *RooRealSumFunc::binBoundaries(RooAbsRealLValue &obs, double x
 //_____________________________________________________________________________B
 bool RooRealSumFunc::isBinnedDistribution(const RooArgSet &obs) const
 {
-   // If all components that depend on obs are binned that so is the product
-
-   RooFIter iter = _funcList.fwdIterator();
-   RooAbsReal *func;
-   while ((func = (RooAbsReal *)iter.next())) {
-      if (func->dependsOn(obs) && !func->isBinnedDistribution(obs)) {
-         return false;
-      }
-   }
-
-   return true;
+   return RooRealSumPdf::isBinnedDistribution(_funcList, obs);
 }
 
 //_____________________________________________________________________________
@@ -221,16 +155,7 @@ std::list<double> *RooRealSumFunc::plotSamplingHint(RooAbsRealLValue &obs, doubl
 //_____________________________________________________________________________
 void RooRealSumFunc::setCacheAndTrackHints(RooArgSet &trackNodes)
 {
-   // Label OK'ed components of a RooRealSumFunc with cache-and-track
-   RooFIter siter = funcList().fwdIterator();
-   RooAbsArg *sarg;
-   while ((sarg = siter.next())) {
-      if (sarg->canNodeBeCached() == Always) {
-         trackNodes.add(*sarg);
-         // cout << "tracking node RealSumFunc component " << sarg->ClassName() << "::" << sarg->GetName() << endl
-         // ;
-      }
-   }
+   RooRealSumPdf::setCacheAndTrackHints(_funcList, trackNodes);
 }
 
 /// Customized printing of arguments of a RooRealSumFunc to more intuitively
