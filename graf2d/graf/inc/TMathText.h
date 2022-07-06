df4c759584e83517d14dff18827970ac09ef051e
Sergey Linev <S.Linev@gsi.de>
2022-07-06 10:01:01 +0200

    Fix significant memory leak in TMathText
    Special render instance were never deleted

--- graf2d/graf/inc/TMathText.h
+++ graf2d/graf/inc/TMathText.h
@@ -18,7 +18,7 @@ class TMathTextRenderer;
 
 class TMathText : public TText, public TAttFill {
 protected:
-      void *fRenderer; //!TMathText Painter
+      TMathTextRenderer *fRenderer; //!TMathText Painter
       TMathText &operator=(const TMathText &);
 
       void Render(const Double_t x, const Double_t y,
