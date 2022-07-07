dda9a46db9c734764d2ea36099c11a9704da4d42
Bertrand Bellenot <bellenot@users.noreply.github.com>
2022-07-07 14:52:43 +0200

    [skip-ci][win] Allow to start root from a PATH containing space(s)

--- config/thisroot.bat
+++ config/thisroot.bat
@@ -5,11 +5,11 @@ rem Author: Axel Naumann, 10/07/2007
 
 set OLDPATH=%CD%
 set THIS=%0
-set THIS=%THIS:~0,-12%.
+set THIS=%THIS:~0,-13%.
 cd /D %THIS%\..
 set ROOTSYS=%CD%
 cd /D %OLDPATH%
-set PATH=%ROOTSYS%\bin;%PATH%
+set PATH=^"%ROOTSYS%\bin^";%PATH%
 set CMAKE_PREFIX_PATH=%ROOTSYS%;%CMAKE_PREFIX_PATH%
 set PYTHONPATH=%ROOTSYS%\bin;%PYTHONPATH%
 set OLDPATH=
