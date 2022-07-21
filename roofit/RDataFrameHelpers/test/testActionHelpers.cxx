8323e504ae187954dcb9bcdfd5df2f9a5ed2abca
Jonas Rembser <jonas.rembser@cern.ch>
2022-07-21 19:26:52 +0200

    [RF] Skip out-of-range events in RDataFrame to RooDataSet Helper
    The RDataFrameHelper should be consistent with creating a RooDataSet
    from a TTree, meaning out-of-range events should be skipped. This is
    implemented in this commit, borrowing the logic from
    `RooTreeDataStore::loadValues()`. A unit test is also implemented.
    
    The previous logic of just taking just all values to fill the dataset
    was very dangerous, because these values then clipped to the RooRealVar
    limits and biased the number of events observed at the boundaries.
    
    Closes #11017.

--- roofit/RDataFrameHelpers/test/testActionHelpers.cxx
+++ roofit/RDataFrameHelpers/test/testActionHelpers.cxx
@@ -4,8 +4,12 @@
 /// \author Stephan Hageboeck (CERN)
 
 #include <RooAbsDataHelper.h>
+#include <RooGlobalFunc.h>
+#include <RooMsgService.h>
 
 #include <TROOT.h>
+#include <TRandom.h>
+#include <TSystem.h>
 #include <Rtypes.h>
 
 #include "gtest/gtest.h"
@@ -76,3 +80,50 @@ TEST(RooAbsDataHelper, MTConstruction)
   EXPECT_NEAR(rooDataHist->moment(y, 2.), 0.25, 1.E-2); // Variance is affected in a binned distribution
 }
 
+
+/// This test verifies that out-of-range events are correctly skipped,
+/// consistent with the construction of a RooDataSet from a TTree.
+TEST(RooAbsDataHelper, SkipEventsOutOfRange) {
+
+  RooMsgService::instance().getStream(0).removeTopic(RooFit::DataHandling);
+  RooMsgService::instance().getStream(1).removeTopic(RooFit::DataHandling);
+
+  std::size_t nEvents = 100;
+  const char * filename = "testRooAbsDataHelper_SkipEventsOutOfRange_tree.root";
+  const char * treename = "tree";
+
+  {
+    // Create the ROOT file with the dataset
+    ROOT::RDataFrame rdf(nEvents);
+    auto rdf_x = rdf.Define("x", [](){ return gRandom->Gaus(0.0, 1.0); });
+    rdf_x.Snapshot(treename, filename);
+    // We can't reuse the same RDataFrame now, because when we rerun the event
+    // loop it would generate new random values. So this scope ends here and we
+    // open a new RDF from the file later.
+  }
+
+  // Open dataset with RDataFrame and TTree
+  std::unique_ptr<TFile> file{TFile::Open(filename, "READ")};
+  auto tree = file->Get<TTree>(treename);
+  ROOT::RDataFrame rdf(treename, filename);
+
+  // Create RooFit variable
+  RooRealVar x{"x", "x", 0.0, -2.0, 2.0};
+
+  // Create a RooDataset from the TTree, and one from the RDataFrame
+  RooDataSet dataSetTree{"dataSetTree", "dataSetTree", tree, x};
+  auto dataSetRDF = rdf.Book<double>(RooDataSetHelper("dataSetRDF", "dataSetRDF", RooArgSet(x)), {"x"});
+
+  // Check if in the creation of the datasets, the entries outside the
+  // variable range were sucessfully discarded.
+  double nPassing = *rdf.Filter("x >= -2 && x <= 2.0").Count();
+
+  EXPECT_EQ(dataSetRDF->numEntries(), nPassing);
+  EXPECT_EQ(dataSetTree.numEntries(), nPassing);
+
+  file.reset(); // Close file
+  gSystem->Unlink(filename); // delete temporary file
+
+  RooMsgService::instance().getStream(0).addTopic(RooFit::DataHandling);
+  RooMsgService::instance().getStream(1).addTopic(RooFit::DataHandling);
+}
