323db3978fdd1a1ea56a4e9eeae141f0d6fdb98f
Axel Naumann <Axel.Naumann@cern.ch>
2022-07-22 15:54:03 +0200

    [cmake,test] Get rid of `lsb_release`:
    It is available less and less often, and we do not actually
    benefit a lot from printing the distro (stress) or we can get
    the same info from /etc/os-release (cmake).

--- test/stressTMVA.cxx
+++ test/stressTMVA.cxx
@@ -3193,10 +3193,6 @@ int main(int argc, char **argv)
       TString sp = gSystem->GetFromPipe("uname -a");
       sp.Resize(60);
       printf("*  SYS: %s\n",sp.Data());
-      if (strstr(gSystem->GetBuildNode(),"Linux")) {
-         sp = gSystem->GetFromPipe("lsb_release -d -s");
-         printf("*  SYS: %s\n",sp.Data());
-      }
       if (strstr(gSystem->GetBuildNode(),"Darwin")) {
          sp  = gSystem->GetFromPipe("sw_vers -productVersion");
          sp += " Mac OS X ";
