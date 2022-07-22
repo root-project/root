b6dd75250573d0b614861a923f81356aac06df98
moneta <lorenzo.moneta@cern.ch>
2022-07-22 18:41:52 +0200

    Fix error message in Tanh operator

--- tmva/sofie/inc/TMVA/ROperator_Tanh.hxx
+++ tmva/sofie/inc/TMVA/ROperator_Tanh.hxx
@@ -49,7 +49,7 @@ public:
    std::string Generate(std::string OpName){
       OpName = "op_" + OpName;
       if (fShape.empty()) {
-         throw std::runtime_error("TMVA SOFIE Transpose Tanh called to Generate without being initialized first");
+         throw std::runtime_error("TMVA SOFIE Tanh operator called to Generate without being initialized first");
       }
       std::stringstream out;
       size_t length = ConvertShapeToLength(fShape);
