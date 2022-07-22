db1065c35fdd0eb06ec4a87689344525a798b0ad
Julia Mathe <julia.mathe@cern.ch>
2022-07-22 14:36:14 +0200

    Fixes of grammar mistakes in the df012 tutorials

--- tutorials/dataframe/df012_DefinesAndFiltersAsStrings.C
+++ tutorials/dataframe/df012_DefinesAndFiltersAsStrings.C
@@ -15,22 +15,22 @@
 void df012_DefinesAndFiltersAsStrings()
 {
    // We will inefficiently calculate an approximation of pi by generating
-   // some data and doing very simple filtering and analysis on it
+   // some data, and doing very simple filtering and analysis on it.
 
    // We start by creating an empty dataframe where we will insert 10 million
    // random points in a square of side 2.0 (that is, with an inscribed circle
-   // of radius 1.0)
+   // of radius 1.0).
 
    size_t npoints = 10000000;
    ROOT::RDataFrame df(npoints);
 
    // Define what we want inside the dataframe. We do not need to define p as an array,
-   // but we do it here to demonstrate how to use jitting with RDataFrame
+   // but we do it here to demonstrate how to use jitting with RDataFrame.
 
    // NOTE: Although it's possible to use "for (auto&& x : p)" below, it will
    // shadow the name of the data column "x", and may cause compilation failures
-   // if the local variable and the data column are of different types or the
-   // local x variable is declared in the global scope of the lambda function
+   // if the local variable and the data column are of different types, or the
+   // local x variable is declared in the global scope of the lambda function.
 
    auto pidf = df.Define("x", "gRandom->Uniform(-1.0, 1.0)")
                  .Define("y", "gRandom->Uniform(-1.0, 1.0)")
@@ -44,7 +44,7 @@ void df012_DefinesAndFiltersAsStrings()
    //
    //     A_circle / A_square = pi r*r / l * l, where r = 1.0, and l = 2.0
    //
-   // Therefore, we can approximate pi with 4 times the number of points inside the
+   // Therefore, we can approximate pi with four times the number of points inside the
    // unit circle over the total number of points in our dataframe:
 
    auto incircle = *(pidf.Filter("r <= 1.0").Count());
