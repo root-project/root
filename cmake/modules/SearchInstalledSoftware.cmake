7570b7da9a2048a31ba6a584cb6bac0c91e9d625
Axel Naumann <Axel.Naumann@cern.ch>
2022-07-27 18:27:35 +0200

    [cmake] Bump gtest to v1.12.1: matchers are nice!

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -1973,7 +1973,7 @@ if (testing)
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_SHALLOW 1
-    GIT_TAG release-1.11.0
+    GIT_TAG release-1.12.1
     UPDATE_COMMAND ""
     # TIMEOUT 10
     # # Force separate output paths for debug and release builds to allow easy
