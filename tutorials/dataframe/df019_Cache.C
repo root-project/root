495944153b072e25b46529ff542c2916f30fac01
Julia Mathe <julia.mathe@cern.ch>
2022-08-01 15:18:32 +0200

    [skip-ci][DF] Fixes of the documentation for the df019 tutorials

--- tutorials/dataframe/df019_Cache.C
+++ tutorials/dataframe/df019_Cache.C
@@ -4,14 +4,14 @@
 /// Cache a processed RDataFrame in memory for further usage.
 ///
 /// This tutorial shows how the content of a data frame can be cached in memory
-/// in form of a data frame. The content of the columns is stored in memory in
+/// in form of a dataframe. The content of the columns is stored in memory in
 /// contiguous slabs of memory and is "ready to use", i.e. no ROOT IO operation
 /// is performed.
 ///
 /// Creating a cached data frame storing all of its content deserialised and uncompressed
 /// in memory is particularly useful when dealing with datasets of a moderate size
 /// (small enough to fit the RAM) over which several explorative loops need to be
-/// performed at as fast as possible. In addition, caching can be useful when no file
+/// performed as fast as possible. In addition, caching can be useful when no file
 /// on disk needs to be created as a side effect of checkpointing part of the analysis.
 ///
 /// All steps in the caching are lazy, i.e. the cached data frame is actually filled
@@ -25,19 +25,19 @@
 
 void df019_Cache()
 {
-   // We create a data frame on top of the hsimple example
+   // We create a data frame on top of the hsimple example.
    auto hsimplePath = gROOT->GetTutorialDir();
    hsimplePath += "/hsimple.root";
    ROOT::RDataFrame df("ntuple", hsimplePath.Data());
 
-   // We apply a simple cut and define a new column
+   // We apply a simple cut and define a new column.
    auto df_cut = df.Filter([](float py) { return py > 0.f; }, {"py"})
                     .Define("px_plus_py", [](float px, float py) { return px + py; }, {"px", "py"});
 
    // We cache the content of the dataset. Nothing has happened yet: the work to accomplish
    // has been described. As for `Snapshot`, the types and columns can be written out explicitly
-   // or left for the jitting to handle (`df_cached` is intentionally unused - it shows how to
-   // to create a *cached* data frame specifying column types explicitly):
+   // or left for the jitting to handle (`df_cached` is intentionally unused - it shows how
+   // to create a *cached* dataframe specifying column types explicitly):
    auto df_cached = df_cut.Cache<float, float>({"px_plus_py", "py"});
    auto df_cached_implicit = df_cut.Cache();
    auto h = df_cached_implicit.Histo1D<float>("px_plus_py");
