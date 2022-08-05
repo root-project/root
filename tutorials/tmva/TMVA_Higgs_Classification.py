02cbe17804bc5368787cce2020d83e389ec16cee
Harshal.S <harshal2015@gmail.com>
2022-08-05 23:56:56 +0530

    TMVA Tutorial translations and Pythonizations.

---
+++ tutorials/tmva/TMVA_Higgs_Classification.py
@@ -0,0 +1,330 @@
+## \file
+## \ingroup tutorial_tmva
+## \notebook
+## Classification example of TMVA based on public Higgs UCI dataset
+##
+##  The UCI data set is a public HIGGS data set , see http://archive.ics.uci.edu/ml/datasets/HIGGS
+## used in this paper: Baldi, P., P. Sadowski, and D. Whiteson. “Searching for Exotic Particles in High-energy Physics
+##                     with Deep Learning.” Nature Communications 5 (July 2, 2014).
+##
+## \macro_image
+## \macro_output
+## \macro_code
+##
+## \author Harshal Shende
+
+## Declare Factory
+
+
+## Create the Factory class. Later you can choose the methods
+## whose performance you'd like to investigate.
+
+## The factory is the major TMVA object you have to interact with. Here is the list of parameters you need to pass
+
+## - The first argument is the base of the name of all the output
+## weightfiles in the directory weight/ that will be created with the
+##    method parameters
+
+## - The second argument is the output file for the training results
+
+## - The third argument is a string option defining some general configuration for the TMVA session. For example all TMVA output can be suppressed by removing the "!" (not) in front of the "Silent" argument in the option string
+
+import ROOT
+from ROOT import TMVA, TFile
+
+
+TMVA.Tools.Instance()
+## For PYMVA methods
+TMVA.PyMethodBase.PyInitialize()
+
+
+# options to control used methods
+useLikelihood = True  # likelihood based discriminant
+useLikelihoodKDE = False  # likelihood based discriminant
+useFischer = True  # Fischer discriminant
+useMLP = False  # Multi Layer Perceptron (old TMVA NN implementation)
+useBDT = True  # Boosted Decision Tree
+useDL = True  # TMVA Deep learning ( CPU or GPU)
+
+TMVA.Tools.Instance()
+outputFile = TFile.Open("Higgs_ClassificationOutput.root", "RECREATE")
+factory = TMVA.Factory(
+    "TMVA_Higgs_Classification", outputFile, V=False, ROC=True, Silent=False, Color=True, AnalysisType="Classification"
+)
+
+
+## Setup Dataset(s)
+
+# Define now input data file and signal and background trees
+
+inputFileName = "Higgs_data.root"
+inputFileLink = "http://root.cern.ch/files/" + inputFileName
+
+
+if ROOT.gSystem.AccessPathName(inputFileName):
+    # file exists
+    ROOT.Info("TMVA_Higgs_Classification", "Download Higgs_data.root file")
+    TFile.SetCacheFileDir(".")
+    inputFile = TFile.Open(inputFileLink, "CACHEREAD")
+    if inputFile is None:
+        raise FileNotFoundError("Input file cannot be downloaded - exit")
+else:
+    inputFile = TFile.Open(inputFileName)
+
+
+# --- Register the training and test trees
+signalTree = inputFile.Get("sig_tree")
+backgroundTree = inputFile.Get("bkg_tree")
+signalTree.Print()
+
+## Declare DataLoader(s)
+
+# The next step is to declare the DataLoader class that deals with input variables
+# Define the input variables that shall be used for the MVA training
+# note that you may also use variable expressions, which can be parsed by TTree::Draw( "expression" )]
+loader = TMVA.DataLoader("dataset")
+
+loader.AddVariable("m_jj")
+loader.AddVariable("m_jjj")
+loader.AddVariable("m_lv")
+loader.AddVariable("m_jlv")
+loader.AddVariable("m_bb")
+loader.AddVariable("m_wbb")
+loader.AddVariable("m_wwbb")
+
+# We set now the input data trees in the TMVA DataLoader class
+# global event weights per tree (see below for setting event-wise weights)
+signalWeight = 1.0
+backgroundWeight = 1.0
+# You can add an arbitrary number of signal or background trees
+loader.AddSignalTree(signalTree, signalWeight)
+loader.AddBackgroundTree(backgroundTree, backgroundWeight)
+
+# Set individual event weights (the variables must exist in the original TTree)
+#   for signal    : factory->SetSignalWeightExpression    ("weight1*weight2");
+#   for background: factory->SetBackgroundWeightExpression("weight1*weight2");
+#   loader->SetBackgroundWeightExpression( "weight" );
+
+
+# Apply additional cuts on the signal and background samples (can be different)
+mycuts = ROOT.TCut("")  # for example: TCut mycuts = "abs(var1)<0.5 && abs(var2-0.5)<1";
+mycutb = ROOT.TCut("")  # for example: TCut mycutb = "abs(var1)<0.5";
+
+# Tell the factory how to use the training and testing events
+#
+# If no numbers of events are given, half of the events in the tree are used
+# for training, and the other half for testing:
+#    loader->PrepareTrainingAndTestTree( mycut, "SplitMode=random:!V" );
+# To also specify the number of testing events, use:
+
+loader.PrepareTrainingAndTestTree(
+    mycuts, mycutb, nTrain_Signal=7000, nTrain_Background=7000, SplitMode="Random", NormMode="NumEvents", V=False
+)
+
+## Booking Methods
+
+# Here we book the TMVA methods. We book first a Likelihood based on KDE (Kernel Density Estimation), a Fischer discriminant, a BDT
+# and a shallow neural network
+# Likelihood ("naive Bayes estimator")
+if useLikelihood:
+    factory.BookMethod(
+        loader,
+        TMVA.Types.kLikelihood,
+        "Likelihood",
+        H=True,
+        V=False,
+        TransformOutput=True,
+        PDFInterpol="Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmoothBkg[1]=10",
+        NSmooth=1,
+        NAvEvtPerBin=50,
+    )
+
+# Use a kernel density estimator to approximate the PDFs
+if useLikelihoodKDE:
+    factory.BookMethod(
+        loader,
+        TMVA.Types.kLikelihood,
+        "LikelihoodKDE",
+        H=False,
+        V=False,
+        TransformOutput=False,
+        PDFInterpol="KDE",
+        KDEtype="Gauss",
+        KDEiter="Adaptive",
+        KDEFineFactor=0.3,
+        KDEborder=None,
+        NAvEvtPerBin=50,
+    )
+
+# Fisher discriminant (same as LD)
+if useFischer:
+    factory.BookMethod(
+        loader,
+        TMVA.Types.kFisher,
+        "Fisher",
+        H=True,
+        V=False,
+        Fisher=True,
+        VarTransform=None,
+        CreateMVAPdfs=True,
+        PDFInterpolMVAPdf="Spline2",
+        NbinsMVAPdf=50,
+        NsmoothMVAPdf=10,
+    )
+
+# Boosted Decision Trees
+if useBDT:
+    factory.BookMethod(
+        loader,
+        TMVA.Types.kBDT,
+        "BDT",
+        V=False,
+        NTrees=200,
+        MinNodeSize="2.5%",
+        MaxDepth=2,
+        BoostType="AdaBoost",
+        AdaBoostBeta=0.5,
+        UseBaggedBoost=True,
+        BaggedSampleFraction=0.5,
+        SeparationType="GiniIndex",
+        nCuts=20,
+    )
+
+# Multi-Layer Perceptron (Neural Network)
+if useMLP:
+    factory.BookMethod(
+        loader,
+        TMVA.Types.kMLP,
+        "MLP",
+        H=False,
+        V=False,
+        NeuronType="tanh",
+        VarTransform="N",
+        NCycles=100,
+        HiddenLayers="N+5",
+        TestRate=5,
+        UseRegulator=False,
+    )
+
+## Here we book the new DNN of TMVA if we have support in ROOT. We will use GPU version if ROOT is enabled with GPU
+
+
+## Booking Deep Neural Network
+
+# Here we define the option string for building the Deep Neural network model.
+
+#### 1. Define DNN layout
+
+# The DNN configuration is defined using a string. Note that whitespaces between characters are not allowed.
+
+# We define first the DNN layout:
+
+# - **input layout** :   this defines the input data format for the DNN as  ``input depth | height | width``.
+# In case of a dense layer as first layer the input layout should be  ``1 | 1 | number of input variables`` (features)
+# - **batch layout**  : this defines how are the input batch. It is related to input layout but not the same.
+# If the first layer is dense it should be ``1 | batch size ! number of variables`` (features)
+
+# *(note the use of the character `|` as  separator of  input parameters for DNN layout)*
+
+# note that in case of only dense layer the input layout could be omitted but it is required when defining more
+# complex architectures
+
+# - **layer layout** string defining the layer architecture. The syntax is
+# - layer type (e.g. DENSE, CONV, RNN)
+# - layer parameters (e.g. number of units)
+# - activation function (e.g  TANH, RELU,...)
+
+# *the different layers are separated by the ``","`` *
+
+#### 2. Define Training Strategy
+
+# We define here the training strategy parameters for the DNN. The parameters are separated by the ``","`` separator.
+# One can then concatenate different training strategy with different parameters. The training strategy are separated by
+# the ``"|"`` separator.
+
+# - Optimizer
+# - Learning rate
+# - Momentum (valid for SGD and RMSPROP)
+# - Regularization and Weight Decay
+# - Dropout
+# - Max number of epochs
+# - Convergence steps. if the test error will not decrease after that value the training will stop
+# - Batch size (This value must be the same specified in the input layout)
+# - Test Repetitions (the interval when the test error will be computed)
+
+
+#### 3. Define general DNN options
+
+# We define the general DNN options concatenating in the final string the previously defined layout and training strategy.
+# Note we use the ``":"`` separator to separate the different higher level options, as in the other TMVA methods.
+# In addition to input layout, batch layout and training strategy we add now:
+
+# - Type of Loss function (e.g. CROSSENTROPY)
+# - Weight Initizalization (e.g XAVIER, XAVIERUNIFORM, NORMAL )
+# - Variable Transformation
+# - Type of Architecture (e.g. CPU, GPU, Standard)
+
+# We can then book the DL method using the built option string
+if useDL:
+    useDLGPU = ROOT.gSystem.GetFromPipe("root-config --has-tmva-gpu") == "yes"
+
+    # Define DNN layout
+    # Define Training strategies
+    # one can catenate several training strategies
+    training1 = ROOT.TString(
+        "LearningRate=1e-3,Momentum=0.9,"
+        "ConvergenceSteps=10,BatchSize=128,TestRepetitions=1,"
+        "MaxEpochs=30,WeightDecay=1e-4,Regularization=None,"
+        "Optimizer=ADAM,ADAM_beta1=0.9,ADAM_beta2=0.999,ADAM_eps=1.E-7,"  # ADAM default parameters
+        "DropConfig=0.0+0.0+0.0+0."
+    )
+    #   training2 = ROOT.TString("LearningRate=1e-3,Momentum=0.9"
+    #                      "ConvergenceSteps=10,BatchSize=128,TestRepetitions=1,"
+    #                       "MaxEpochs=20,WeightDecay=1e-4,Regularization=None,"
+    #                       "Optimizer=SGD,DropConfig=0.0+0.0+0.0+0.")
+
+    # General Options.
+    dnnMethodName = ROOT.TString("DNN_CPU")
+
+    if useDLGPU:
+        arch = "GPU"
+        dnnMethodName = "DNN_GPU"
+    else:
+        arch = "CPU"
+
+    factory.BookMethod(
+        loader,
+        TMVA.Types.kDL,
+        dnnMethodName,
+        H=False,
+        V=True,
+        ErrorStrategy="CROSSENTROPY",
+        VarTransform="G",
+        WeightInitialization="XAVIER",
+        InputLayout="1|1|7",
+        BatchLayout="1|128|7",
+        Layout="DENSE|64|TANH,DENSE|64|TANH,DENSE|64|TANH,DENSE|64|TANH,DENSE|1|LINEAR",
+        TrainingStrategy=training1,
+        Architecture=arch,
+    )
+
+## Train Methods
+
+# Here we train all the previously booked methods.
+
+factory.TrainAllMethods()
+## Test  all methods
+
+# Now we test and evaluate all methods using the test data set
+factory.TestAllMethods()
+
+factory.EvaluateAllMethods()
+
+# after we get the ROC curve and we display
+
+c1 = factory.GetROCCurve(loader)
+c1.Draw()
+# at the end we close the output file which contains the evaluation result of all methods and it can be used by TMVAGUI
+# to display additional plots
+
+outputFile.Close()
---
+++ tutorials/tmva/TMVA_RNN_Classification.py
@@ -0,0 +1,474 @@
+## \file
+## \ingroup tutorial_tmva
+## \notebook
+##  TMVA Classification Example Using a Recurrent Neural Network
+##
+## This is an example of using a RNN in TMVA. We do classification using a toy time dependent data set
+## that is generated when running this example macro
+##
+## \macro_image
+## \macro_output
+## \macro_code
+##
+## \author Harshal Shende
+
+
+# TMVA Classification Example Using a Recurrent Neural Network
+
+# This is an example of using a RNN in TMVA.
+# We do the classification using a toy data set containing a time series of data sample ntimes
+# and with dimension ndim that is generated when running the provided function `MakeTimeData (nevents, ntime, ndim)`
+
+
+import ROOT
+from ROOT import TMVA
+
+
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+
+TMVA.Tools.Instance()
+TMVA.Config.Instance()
+
+
+##  Helper function to generate the time data set
+##  make some time data but not of fixed length.
+##  use a poisson with mu = 5 and truncated at 10
+
+
+def MakeTimeData(n, ntime, ndim):
+    # ntime = 10;
+    # ndim = 30; // number of dim/time
+
+    fname = "time_data_t" + str(ntime) + "_d" + str(ndim) + ".root"
+    v1 = []
+    v2 = []
+
+    for i in range(ntime):
+        v1.append(ROOT.TH1D("h1_" + str(i), "h1", ndim, 0, 10))
+        v2.append(ROOT.TH1D("h2_" + str(i), "h2", ndim, 0, 10))
+
+    f1 = ROOT.TF1("f1", "gaus")
+    f2 = ROOT.TF1("f2", "gaus")
+
+    sgn = ROOT.TTree("sgn", "sgn")
+    bkg = ROOT.TTree("bkg", "bkg")
+    f = ROOT.TFile(fname, "RECREATE")
+
+    x1 = []
+    x2 = []
+
+    for i in range(ntime):
+        x1.append(ROOT.std.vector["float"](ndim))
+        x2.append(ROOT.std.vector["float"](ndim))
+
+    for i in range(ntime):
+        bkg.Branch("vars_time" + str(i), "std::vector<float>", x1[i])
+        sgn.Branch("vars_time" + str(i), "std::vector<float>", x2[i])
+
+    sgn.SetDirectory(f)
+    bkg.SetDirectory(f)
+    ROOT.gRandom.SetSeed(0)
+
+    mean1 = ROOT.std.vector["double"](ntime)
+    mean2 = ROOT.std.vector["double"](ntime)
+    sigma1 = ROOT.std.vector["double"](ntime)
+    sigma2 = ROOT.std.vector["double"](ntime)
+
+    for j in range(ntime):
+        mean1[j] = 5.0 + 0.2 * ROOT.TMath.Sin(ROOT.TMath.Pi() * j / float(ntime))
+        mean2[j] = 5.0 + 0.2 * ROOT.TMath.Cos(ROOT.TMath.Pi() * j / float(ntime))
+        sigma1[j] = 4 + 0.3 * ROOT.TMath.Sin(ROOT.TMath.Pi() * j / float(ntime))
+        sigma2[j] = 4 + 0.3 * ROOT.TMath.Cos(ROOT.TMath.Pi() * j / float(ntime))
+
+    for i in range(n):
+        if i % 1000 == 0:
+            print("Generating  event ... %d", i)
+
+        for j in range(ntime):
+            h1 = v1[j]
+            h2 = v2[j]
+            h1.Reset()
+            h2.Reset()
+
+            f1.SetParameters(1, mean1[j], sigma1[j])
+            f2.SetParameters(1, mean2[j], sigma2[j])
+
+            h1.FillRandom("f1", 1000)
+            h2.FillRandom("f2", 1000)
+
+            for k in range(ntime):
+                # std::cout << j*10+k << "   ";
+                x1[j][k] = h1.GetBinContent(k + 1) + ROOT.gRandom.Gaus(0, 10)
+                x2[j][k] = h2.GetBinContent(k + 1) + ROOT.gRandom.Gaus(0, 10)
+
+        sgn.Fill()
+        bkg.Fill()
+
+        if n == 1:
+            c1 = ROOT.TCanvas()
+            c1.Divide(ntime, 2)
+            for j in range(ntime):
+                c1.cd(j + 1)
+                v1[j].Draw()
+            for j in range(ntime):
+                c1.cd(ntime + j + 1)
+                v2[j].Draw()
+
+            ROOT.gPad.Update()
+
+    if n > 1:
+        sgn.Write()
+        bkg.Write()
+        sgn.Print()
+        bkg.Print()
+        f.Close()
+
+
+## macro for performing a classification using a Recurrent Neural Network
+## @param use_type
+##    use_type = 0    use Simple RNN network
+##    use_type = 1    use LSTM network
+##    use_type = 2    use GRU
+##    use_type = 3    build 3 different networks with RNN, LSTM and GRU
+
+
+use_type = 1
+ninput = 30
+ntime = 10
+batchSize = 100
+maxepochs = 20
+
+nTotEvts = 10000  # total events to be generated for signal or background
+
+useKeras = True
+
+useTMVA_RNN = True
+useTMVA_DNN = True
+useTMVA_BDT = False
+
+rnn_types = ["RNN", "LSTM", "GRU"]
+use_rnn_type = [1, 1, 1]
+
+if 0 <= use_type < 3:
+    use_rnn_type = [0, 0, 0]
+    use_rnn_type[use_type] = 1
+
+useGPU = True  # use GPU for TMVA if available
+
+useGPU = ROOT.gSystem.GetFromPipe("root-config --has-tmva-gpu") == "yes"
+useTMVA_RNN = ROOT.gSystem.GetFromPipe("root-config --has-tmva-cpu") == "yes"
+
+if useTMVA_RNN:
+    ROOT.Warning(
+        "TMVA_RNN_Classification",
+        "TMVA is not build with GPU or CPU multi-thread support. Cannot use TMVA Deep Learning for RNN",
+    )
+
+archString = "GPU" if useGPU else "CPU"
+
+writeOutputFile = True
+
+rnn_type = "RNN"
+
+if ROOT.gSystem.GetFromPipe("root-config --has-tmva-pymva") == "yes":
+    TMVA.PyMethodBase.PyInitialize()
+else:
+    useKeras = False
+
+num_threads = 0  # use by default all threads
+# do enable MT running
+if num_threads >= 0:
+    ROOT.EnableImplicitMT(num_threads)
+    if num_threads > 0:
+        ROOT.gSystem.Setenv("OMP_NUM_THREADS", ROOT.TString.Format("%d", num_threads))
+else:
+    ROOT.gSystem.Setenv("OMP_NUM_THREADS", "1")
+
+
+print("Running with nthreads  = {}".format(ROOT.GetThreadPoolSize()))
+
+inputFileName = "time_data_t10_d30.root"
+
+fileDoesNotExist = ROOT.gSystem.AccessPathName(inputFileName)
+
+# if file does not exists create it
+if fileDoesNotExist:
+    MakeTimeData(nTotEvts, ntime, ninput)
+
+
+inputFile = ROOT.TFile.Open(inputFileName)
+if inputFile is None:
+    raise ROOT.Error("Error opening input file %s - exit", inputFileName.Data())
+
+
+print("--- RNNClassification  : Using input file: {}".format(inputFile.GetName()))
+
+# Create a ROOT output file where TMVA will store ntuples, histograms, etc.
+outfileName = "data_RNN_" + archString + ".root"
+outputFile = None
+
+
+if writeOutputFile:
+    outputFile = ROOT.TFile.Open(outfileName, "RECREATE")
+
+
+## Declare Factory
+
+# Create the Factory class. Later you can choose the methods
+# whose performance you'd like to investigate.
+
+# The factory is the major TMVA object you have to interact with. Here is the list of parameters you need to
+# pass
+
+# - The first argument is the base of the name of all the output
+# weightfiles in the directory weight/ that will be created with the
+#     method parameters
+
+# - The second argument is the output file for the training results
+#
+# - The third argument is a string option defining some general configuration for the TMVA session.
+# For example all TMVA output can be suppressed by removing the "!" (not) in front of the "Silent" argument in
+# the option string
+
+
+# // Creating the factory object
+factory = TMVA.Factory(
+    "TMVAClassification",
+    outputFile,
+    V=False,
+    Silent=False,
+    Color=True,
+    DrawProgressBar=True,
+    Transformations=None,
+    Correlations=False,
+    AnalysisType="Classification",
+    ModelPersistence=True,
+)
+dataloader = TMVA.DataLoader("dataset")
+
+signalTree = inputFile.Get("sgn")
+background = inputFile.Get("bkg")
+
+nvar = ninput * ntime
+
+## add variables - use new AddVariablesArray function
+for i in range(ntime):
+    dataloader.AddVariablesArray("vars_time" + str(i), ninput)
+
+
+dataloader.AddSignalTree(signalTree, 1.0)
+dataloader.AddBackgroundTree(background, 1.0)
+
+# check given input
+datainfo = dataloader.GetDataSetInfo()
+vars = datainfo.GetListOfVariables()
+print("number of variables is {}".format(vars.size()))
+
+
+for v in vars:
+    print(v)
+
+nTrainSig = 0.8 * nTotEvts
+nTrainBkg = 0.8 * nTotEvts
+
+# Apply additional cuts on the signal and background samples (can be different)
+mycuts = ""  # for example: TCut mycuts = "abs(var1)<0.5 && abs(var2-0.5)<1";
+mycutb = ""
+
+# build the string options for DataLoader::PrepareTrainingAndTestTree
+dataloader.PrepareTrainingAndTestTree(
+    mycuts,
+    mycutb,
+    nTrain_Signal=nTrainSig,
+    nTrain_Background=nTrainBkg,
+    SplitMode="Random",
+    SplitSeed=100,
+    NormMode="NumEvents",
+    V=False,
+    CalcCorrelations=False,
+)
+
+print("prepared DATA LOADER ")
+
+
+## Book TMVA  recurrent models
+
+# Book the different types of recurrent models in TMVA  (SimpleRNN, LSTM or GRU)
+
+
+if useTMVA_RNN:
+    for i in range(3):
+        if not use_rnn_type[i]:
+            continue
+
+        rnn_type = rnn_types[i]
+
+        ## Define RNN layer layout
+        ##  it should be   LayerType (RNN or LSTM or GRU) |  number of units | number of inputs | time steps | remember output (typically no=0 | return full sequence
+        rnnLayout = str(rnn_type) + "|10|" + str(ninput) + "|" + str(ntime) + "|0|1,RESHAPE|FLAT,DENSE|64|TANH,LINEAR"
+
+        ## Defining Training strategies. Different training strings can be concatenate. Use however only one
+        trainingString1 = "LearningRate=1e-3,Momentum=0.0,Repetitions=1,ConvergenceSteps=5,BatchSize=" + str(batchSize)
+        trainingString1 += ",TestRepetitions=1,WeightDecay=1e-2,Regularization=None,MaxEpochs=" + str(maxepochs)
+        trainingString1 += "Optimizer=ADAM,DropConfig=0.0+0.+0.+0."
+
+        ## define the inputlayout string for RNN
+        ## the input data should be organize as   following:
+        ##/ input layout for RNN:    time x ndim
+        ## add after RNN a reshape layer (needed top flatten the output) and a dense layer with 64 units and a last one
+        ## Note the last layer is linear because  when using Crossentropy a Sigmoid is applied already
+        ## Define the full RNN Noption string adding the final options for all network
+        rnnName = "TMVA_" + str(rnn_type)
+        factory.BookMethod(
+            dataloader,
+            TMVA.Types.kDL,
+            rnnName,
+            H=False,
+            V=True,
+            ErrorStrategy="CROSSENTROPY",
+            VarTransform=None,
+            WeightInitialization="XAVIERUNIFORM",
+            ValidationSize=0.2,
+            RandomSeed=1234,
+            InputLayout=str(ntime) + "|" + str(ninput),
+            Layout=rnnLayout,
+            TrainingStrategy=trainingString1,
+            Architecture=archString,
+        )
+
+
+## Book TMVA  fully connected dense layer  models
+if useTMVA_DNN:
+    # Method DL with Dense Layer
+    # Training strategies.
+    trainingString1 = ROOT.TString(
+        "LearningRate=1e-3,Momentum=0.0,Repetitions=1,"
+        "ConvergenceSteps=10,BatchSize=256,TestRepetitions=1,"
+        "WeightDecay=1e-4,Regularization=None,MaxEpochs=20"
+        "DropConfig=0.0+0.+0.+0.,Optimizer=ADAM:"
+    )  # + "|" + trainingString2
+    # General Options.
+    trainingString1.Append(archString)
+    dnnName = "TMVA_DNN"
+    factory.BookMethod(
+        dataloader,
+        TMVA.Types.kDL,
+        dnnName,
+        H=False,
+        V=True,
+        ErrorStrategy="CROSSENTROPY",
+        VarTransform=None,
+        WeightInitialization="XAVIER",
+        RandomSeed=0,
+        InputLayout="1|1|" + str(ntime * ninput),
+        Layout="DENSE|64|TANH,DENSE|TANH|64,DENSE|TANH|64,LINEAR",
+        TrainingStrategy=trainingString1,
+    )
+
+
+## Book Keras recurrent models
+
+# Book the different types of recurrent models in Keras  (SimpleRNN, LSTM or GRU)
+
+
+if useKeras:
+    for i in range(3):
+        if use_rnn_type[i]:
+            modelName = "model_" + rnn_types[i] + ".h5"
+            trainedModelName = "trained_" + modelName
+            print("Building recurrent keras model using a", rnn_types[i], "layer")
+            # create python script which can be executed
+            # create 2 conv2d layer + maxpool + dense
+            from keras.models import Sequential
+            from keras.optimizers import Adam
+
+            # from keras.initializers import TruncatedNormal
+            # from keras import initializations
+            from keras.layers import Input, Dense, Dropout, Flatten, SimpleRNN, GRU, LSTM, Reshape, BatchNormalization
+
+            model = Sequential()
+            model.add(Reshape((10, 30), input_shape=(10 * 30,)))
+            # add recurrent neural network depending on type / Use option to return the full output
+            if rnn_types[i] == "LSTM":
+                model.add(LSTM(units=10, return_sequences=True))
+            elif rnn_types[i] == "GRU":
+                model.add(GRU(units=10, return_sequences=True))
+            else:
+                model.add(SimpleRNN(units=10, return_sequences=True))
+                # m.AddLine("model.add(BatchNormalization())");
+                model.add(Flatten())  # needed if returning the full time output sequence
+                model.add(Dense(64, activation="tanh"))
+                model.add(Dense(2, activation="sigmoid"))
+                model.compile(loss="binary_crossentropy", optimizer=Adam(lr=0.001), metrics=["accuracy"])
+                model.save(modelName)
+                model.summary()
+
+            if ROOT.gSystem.AccessPathName(modelName):
+                useKeras = False
+                raise FileNotFoundError("Error creating Keras recurrent model file - Skip using Keras")
+            else:
+                # book PyKeras method only if Keras model could be created
+                print("Booking Keras  model ", rnn_types[i])
+                factory.BookMethod(
+                    dataloader,
+                    TMVA.Types.kPyKeras,
+                    "PyKeras_" + rnn_types[i],
+                    H=True,
+                    V=False,
+                    VarTransform=None,
+                    FilenameModel=modelName,
+                    FilenameTrainedModel="trained_" + modelName,
+                    NumEpochs=maxepochs,
+                    BatchSize=batchSize,
+                    GpuOptions="allow_growth=True",
+                )
+
+
+# use BDT in case not using Keras or TMVA DL
+if not useKeras or not useTMVA_BDT:
+    useTMVA_BDT = True
+
+
+## Book TMVA BDT
+
+
+if useTMVA_BDT:
+    factory.BookMethod(
+        dataloader,
+        TMVA.Types.kBDT,
+        "BDTG",
+        H=True,
+        V=False,
+        NTrees=100,
+        MinNodeSize="2.5%",
+        BoostType="Grad",
+        Shrinkage=0.10,
+        UseBaggedBoost=True,
+        BaggedSampleFraction=0.5,
+        nCuts=20,
+        MaxDepth=2,
+    )
+
+
+## Train all methods
+factory.TrainAllMethods()
+
+print("nthreads  = {}".format(ROOT.GetThreadPoolSize()))
+
+# ---- Evaluate all MVAs using the set of test events
+factory.TestAllMethods()
+
+# ----- Evaluate and compare performance of all configured MVAs
+factory.EvaluateAllMethods()
+
+# check method
+
+# plot ROC curve
+c1 = factory.GetROCCurve(dataloader)
+c1.Draw()
+
+if outputFile:
+    outputFile.Close()
