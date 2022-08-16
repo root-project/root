4b5afbe89d2f2c011e350300c265a6f5429fd4f0
Jonas Rembser <jonas.rembser@cern.ch>
2022-08-16 17:42:43 +0200

    [RF] Use `recursiveRedirectServers()` in NormalizationHelpers
    In the `foldIntegrals()` function that reverses the unfolting of
    normalization integrals in the computation graph, some custom logic was
    used to redirect the servers. However, it caused some failures in the
    ASAN builds because it accessed invalid memory.
    
    This commit suggests to fix that by using the already existing
    `RooAbsArg::recursiveRedirectServers()` function for the same logic.
    
    Furthermore, in `unfoldIntegrals()`, clients outside the computation
    graph are not considered for the server redirection, which also fixes
    ASAN build failures.

--- roofit/roofitcore/src/NormalizationHelpers.cxx
+++ roofit/roofitcore/src/NormalizationHelpers.cxx
@@ -203,7 +203,7 @@ void treeNodeServerListAndNormSets(const RooAbsArg &arg, RooAbsCollection &list,
 
 std::vector<std::unique_ptr<RooAbsArg>> unfoldIntegrals(RooAbsArg const &topNode, RooArgSet const &normSet,
                                                         std::unordered_map<DataKey, RooArgSet *> &normSets,
-                                                        RooArgSet &replacedArgs)
+                                                        RooArgSet &replacedArgs, RooArgSet &newArgs)
 {
    std::vector<std::unique_ptr<RooAbsArg>> newNodes;
 
@@ -241,22 +241,23 @@ std::vector<std::unique_ptr<RooAbsArg>> unfoldIntegrals(RooAbsArg const &topNode
       const std::string attrib = std::string("ORIGNAME:") + oldArg.GetName();
 
       newArg.setAttribute(attrib.c_str());
-      newArg.setStringAttribute("_replaced_arg", oldArg.GetName());
 
       RooArgList newServerList{newArg};
 
       RooArgList originalClients;
       for (auto *client : oldArg.clients()) {
-         originalClients.add(*client);
+         if (nodes.containsInstance(*client)) {
+            originalClients.add(*client);
+         }
       }
       for (auto *client : originalClients) {
-         if (!nodes.containsInstance(*client))
-            continue;
          if (dynamic_cast<RooAbsCachedPdf *>(client))
             continue;
          client->redirectServers(newServerList, false, true);
       }
+
       replacedArgs.add(oldArg);
+      newArgs.add(newArg);
 
       newArg.setAttribute(attrib.c_str(), false);
    };
@@ -300,28 +301,18 @@ std::vector<std::unique_ptr<RooAbsArg>> unfoldIntegrals(RooAbsArg const &topNode
    return newNodes;
 }
 
-void foldIntegrals(RooAbsArg const &topNode, RooArgSet &replacedArgs)
+void foldIntegrals(RooAbsArg &topNode, RooArgSet &replacedArgs, RooArgSet &newArgs)
 {
-   RooArgSet nodes;
-   topNode.treeNodeServerList(&nodes);
+   assert(replacedArgs.size() == newArgs.size());
 
-   for (RooAbsArg *normalizedPdf : nodes) {
+   for (std::size_t i = 0; i < replacedArgs.size(); ++i) {
+      replacedArgs[i]->setAttribute((std::string("ORIGNAME:") + newArgs[i]->GetName()).c_str());
+   }
 
-      if (auto const &replacedArgName = normalizedPdf->getStringAttribute("_replaced_arg")) {
+   topNode.recursiveRedirectServers(replacedArgs, false, true);
 
-         auto pdf = &replacedArgs[replacedArgName];
-
-         pdf->setAttribute((std::string("ORIGNAME:") + normalizedPdf->GetName()).c_str());
-
-         RooArgList newServerList{*pdf};
-         for (auto *client : normalizedPdf->clients()) {
-            client->redirectServers(newServerList, false, true);
-         }
-
-         pdf->setAttribute((std::string("ORIGNAME:") + normalizedPdf->GetName()).c_str(), false);
-
-         normalizedPdf->removeStringAttribute("_replaced_arg");
-      }
+   for (std::size_t i = 0; i < replacedArgs.size(); ++i) {
+      replacedArgs[i]->setAttribute((std::string("ORIGNAME:") + newArgs[i]->GetName()).c_str(), false);
    }
 }
 
@@ -351,7 +342,7 @@ RooFit::NormalizationIntegralUnfolder::NormalizationIntegralUnfolder(RooAbsArg c
    : _topNodeWrapper{std::make_unique<RooAddition>("_dummy", "_dummy", RooArgList{topNode})}, _normSetWasEmpty{
                                                                                                  normSet.empty()}
 {
-   auto ownedArgs = unfoldIntegrals(*_topNodeWrapper, normSet, _normSets, _replacedArgs);
+   auto ownedArgs = unfoldIntegrals(*_topNodeWrapper, normSet, _normSets, _replacedArgs, _newArgs);
    for (std::unique_ptr<RooAbsArg> &arg : ownedArgs) {
       _topNodeWrapper->addOwnedComponents(std::move(arg));
    }
@@ -365,7 +356,7 @@ RooFit::NormalizationIntegralUnfolder::~NormalizationIntegralUnfolder()
    if (_normSetWasEmpty)
       return;
 
-   foldIntegrals(*_topNodeWrapper, _replacedArgs);
+   foldIntegrals(*_topNodeWrapper, _replacedArgs, _newArgs);
 
    for (auto &item : _normSets) {
       delete item.second;
