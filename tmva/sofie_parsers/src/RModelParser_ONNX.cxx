febe9c5352ee752ae37b1d16b5c1535d365535fe
Neel Shah <neelshah29042002@gmail.com>
2022-08-17 14:57:13 +0530

    Pow ONNX Operator implemented as one of the Binary Operator enum to reduce the code

--- tmva/sofie_parsers/src/RModelParser_ONNX.cxx
+++ tmva/sofie_parsers/src/RModelParser_ONNX.cxx
@@ -159,38 +159,6 @@ std::unique_ptr<ROperator> make_ROperator_Transpose(const onnx::NodeProto& nodep
    return op;
 }
 
-std::unique_ptr<ROperator> make_ROperator_Pow(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
-
-   ETensorType input_type;
-
-   auto input_name =  nodeproto.input(0);
-   auto it = tensor_type.find(input_name);
-   if (it != tensor_type.end()){
-      input_type = it->second;
-   }else{
-      throw std::runtime_error("TMVA::SOFIE ONNX Parser pow op has input tensor" + input_name + " but its type is not yet registered");
-   }
-
-   std::unique_ptr<ROperator> op;
-
-
-   switch(input_type){
-   case ETensorType::FLOAT:
-      op.reset(new ROperator_Pow<float>(nodeproto.input(0), nodeproto.input(1), nodeproto.output(0)));
-      break;
-   default:
-      throw std::runtime_error("TMVA::SOFIE - Unsupported - Operator Pow does not yet support input type " + std::to_string(static_cast<int>(input_type)));
-   }
-
-   ETensorType output_type = (op->TypeInference({input_type}))[0];
-   auto it2 = tensor_type.find(nodeproto.output(0));
-   if (it2 == tensor_type.end()){
-      tensor_type[nodeproto.output(0)] = output_type;
-   }
-
-   return op;
-}
-
 std::unique_ptr<ROperator> make_ROperator_Identity(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
 
    ETensorType input_type;
