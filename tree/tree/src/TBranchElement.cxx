0684d08579c18636ce4830c08cd111eea72b11be
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2022-08-26 17:46:21 +0200

    [tree] Use base class function in TBranchElement::GetFullName().

--- tree/tree/src/TBranchElement.cxx
+++ tree/tree/src/TBranchElement.cxx
@@ -2845,11 +2845,8 @@ TString TBranchElement::GetFullName() const
       // The parent's name is already included in the name for split TClonesArray and STL collections
       return fName;
    }
-   TString motherName(mother->GetName());
-   if (motherName.Length() && (motherName[motherName.Length()-1] == '.')) {
-      return fName;
-   }
-   return motherName + "." + fName;
+
+   return TBranch::GetFullName();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
