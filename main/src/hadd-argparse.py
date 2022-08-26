2286b204cedd3d578db00aa0d5b4e71b67789ccf
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2022-08-26 10:11:24 +0200

    [main] Use textwrap.fill for a more consistent help message

--- main/src/hadd-argparse.py
+++ main/src/hadd-argparse.py
@@ -1,4 +1,5 @@
 import argparse
+import textwrap
 
 def get_argparse():
 	DESCRIPTION = """This program will add histograms from a list of root files and write them to a target root file.\n
@@ -17,21 +18,31 @@ The prefix can be optionally followed by B whose casing is ignored, eg. 1k, 1K, 
 	parser.add_argument("-k", help="Skip corrupt or non-existent files, do not exit")
 	parser.add_argument("-T", help="Do not merge Trees")
 	parser.add_argument("-O", help="Re-optimize basket size when merging TTree")
-	parser.add_argument("-v", help="Explicitly set the verbosity level: 0 request no output, 99 is the default")
+	parser.add_argument("-v", help=textwrap.fill(
+		"Explicitly set the verbosity level: 0 request no output, 99 is the default"))
 	parser.add_argument("-j", help="Parallelize the execution in multiple processes")
-	parser.add_argument("-dbg", help="Parallelize the execution in multiple processes in debug mode (Does not delete partial files stored inside working directory)")
-	parser.add_argument("-d", help="Carry out the partial multiprocess execution in the specified directory")
-	parser.add_argument("-n", help="Open at most 'maxopenedfiles' at once (use 0 to request to use the system maximum)")
-	parser.add_argument("-cachesize", help="Resize the prefetching cache use to speed up I/O operations(use 0 to disable)")
-	parser.add_argument("-experimental-io-features", help="Used with an argument provided, enables the corresponding experimental feature for output trees")
-	parser.add_argument("-f", help="Gives the ability to specify the compression level of the target file(by default 4) ")
-	parser.add_argument("-fk", help="""Sets the target file to contain the baskets with the same compression
-as the input files (unless -O is specified). Compresses the meta data
-using the compression level specified in the first input or the
-compression setting after fk (for example 206 when using -fk206)""")
+	parser.add_argument("-dbg", help=textwrap.fill(
+		"Parallelize the execution in multiple processes in debug mode "
+		"(Does not delete partial files stored inside working directory)"))
+	parser.add_argument("-d", help=textwrap.fill(
+		"Carry out the partial multiprocess execution in the specified directory"))
+	parser.add_argument("-n", help=textwrap.fill(
+		"Open at most 'maxopenedfiles' at once (use 0 to request to use the system maximum)"))
+	parser.add_argument("-cachesize", help=textwrap.fill(
+		"Resize the prefetching cache use to speed up I/O operations(use 0 to disable)"))
+	parser.add_argument("-experimental-io-features", help=textwrap.fill(
+		"Used with an argument provided, enables the corresponding experimental feature for output trees"))
+	parser.add_argument("-f", help=textwrap.fill(
+		"Gives the ability to specify the compression level of the target file (by default 4) "))
+	parser.add_argument("-fk", help=textwrap.fill(
+		"Sets the target file to contain the baskets with the same compression "
+		"as the input files (unless -O is specified). Compresses the meta data "
+		"using the compression level specified in the first input or the "
+		"compression setting after fk (for example 206 when using -fk206)"))
 	parser.add_argument("-ff", help="The compression level use is the one specified in the first input")
 	parser.add_argument("-f0", help="Do not compress the target file")
-	parser.add_argument("-f6", help="Use compression level 6. (See TFile::SetCompressionSettings for the support range of value.)")
+	parser.add_argument("-f6", help=textwrap.fill(
+		"Use compression level 6. (See TFile::SetCompressionSettings for the support range of value.)"))
 	parser.add_argument("TARGET", help="Target file")
 	parser.add_argument("SOURCES", help="Source files")
 	return parser
