4297114c19413318d839b7a005e5684667f8479f
Neel Shah <neelshah29042002@gmail.com>
2022-09-04 16:05:32 +0530

    Concat Operator is added with a newAxis attribute needed for the implementation of GNN

--- tmva/sofie/inc/TMVA/ROperator_Concat.hxx
+++ tmva/sofie/inc/TMVA/ROperator_Concat.hxx
@@ -20,7 +20,8 @@
      class ROperator_Concat final : public ROperator
      {
      private:
-         int fAxis;
+         int fAxis=0;
+         int fnewAxis=0;
          std::vector<std::string> fInputs;
          std::string fOutput;
          std::vector<size_t>fOutputShape;
@@ -28,8 +29,8 @@
 
      public:
          ROperator_Concat(){}
-         ROperator_Concat(std::vector<std::string> inputs, int axis, std::string output):
-         fAxis(axis), fOutput(UTILITY::Clean_name(output)) {
+         ROperator_Concat(std::vector<std::string> inputs, int axis, int newAxis, std::string output):
+         fAxis(axis), fnewAxis(newAxis), fOutput(UTILITY::Clean_name(output)) {
             fInputs.reserve(inputs.size());
             for (auto & name : inputs)
                fInputs.push_back(UTILITY::Clean_name(name));
@@ -51,21 +52,28 @@
 
             int concat_dim=0;
             for(size_t i = 0; i < inputs.size(); i++) {
-               if (i > 0 && inputs[i].size() != inputs[i-1].size() )
-                 throw std::runtime_error("TMVA SOFIE Concat Op - input tensors have different shapes " +
-                  ConvertShapeToString(inputs[i]) + " and " + ConvertShapeToString(inputs[i-1]));
-               for (size_t iaxis = 0; iaxis < inputs[i].size(); iaxis++) {
-                 if ((int) iaxis == fAxis)
-                  concat_dim += inputs[i][iaxis];
-                 else
-                    if (i> 0 && inputs[i][iaxis] != inputs[i-1][iaxis])
-                     throw std::runtime_error("TMVA SOFIE Concat Op - input tensors have wrong shapes " +
-                      ConvertShapeToString(inputs[i]) + " and " + ConvertShapeToString(inputs[i-1]));
+                  if (i > 0 && inputs[i].size() != inputs[i-1].size() )
+                  throw std::runtime_error("TMVA SOFIE Concat Op - input tensors have different shapes " +
+                     ConvertShapeToString(inputs[i]) + " and " + ConvertShapeToString(inputs[i-1]));
+                  for (size_t iaxis = 0; iaxis < inputs[i].size(); iaxis++) {
+                  if ((int) iaxis == fAxis)
+                     concat_dim += inputs[i][iaxis];
+                  else
+                     if (i> 0 && inputs[i][iaxis] != inputs[i-1][iaxis])
+                        throw std::runtime_error("TMVA SOFIE Concat Op - input tensors have wrong shapes " +
+                        ConvertShapeToString(inputs[i]) + " and " + ConvertShapeToString(inputs[i-1]));
+                  }
                }
+            if(fnewAxis == 0){
+               // output shape
+               ret[0] = inputs[0];
+               ret[0][fAxis] = concat_dim;
             }
-            // output shape
-            ret[0] = inputs[0];
-            ret[0][fAxis] = concat_dim;
+
+            if(fnewAxis == 1){
+               ret[0].insert(ret[0].begin() + fAxis, inputs.size());
+               ret[0][fAxis] = concat_dim;
+            }
             return ret;
          }
 
@@ -88,53 +96,57 @@
             }
             std::stringstream out;
             out<<"\n//--------- Concat\n";
-            // special case for 0 axis that memory is contigous
-            if (fAxis == 0) {
-               for(auto &it : fInputs){
-                  out<<SP<<"tensor_" << fOutput<<".insert("<<"tensor_" << fOutput<<".end(),tensor_" << it<<".begin(),tensor_"<<it<<".end());\n";
+            // special case for 0 axis that memory is contiguous
+            if(fnewAxis == 0){
+               if (fAxis == 0) {
+                  for(auto &it : fInputs){
+                     out<<SP<<"tensor_" << fOutput<<".insert("<<"tensor_" << fOutput<<".end(),tensor_" << it<<".begin(),tensor_"<<it<<".end());\n";
+                  }
                }
-            }
-            else {
+               else {
 
-               std::vector<size_t> outStride = UTILITY::ComputeStrideFromShape(fOutputShape);
-               std::vector<std::vector<size_t>> inStrides(fInputs.size());
-               int idx = 0;
-               for ( auto &s : inStrides) {
-                  s = UTILITY::ComputeStrideFromShape(fInputShapes[idx]);
-                  idx++;
-               }
-               for (int i = 0; i < fAxis; ++i) {
-                  // loop on dimensions
-                  out << SP << "for (size_t i" << i << " = 0; i" << i << " < " << fOutputShape[i] << "; ++i" << i <<") {\n";
-               }
+                  std::vector<size_t> outStride = UTILITY::ComputeStrideFromShape(fOutputShape);
+                  std::vector<std::vector<size_t>> inStrides(fInputs.size());
+                  int idx = 0;
+                  for ( auto &s : inStrides) {
+                     s = UTILITY::ComputeStrideFromShape(fInputShapes[idx]);
+                     idx++;
+                  }
+                  for (int i = 0; i < fAxis; ++i) {
+                     // loop on dimensions
+                     out << SP << "for (size_t i" << i << " = 0; i" << i << " < " << fOutputShape[i] << "; ++i" << i <<") {\n";
+                  }
 
-               out << SP << SP << SP << "int idxOut =";
-               for (int k = 0; k < fAxis; k++)
-                  out << " + " << outStride[k] << "*i" << k;
-               out << ";\n";
-
-               for (size_t j = 0; j < fInputs.size(); j++) {
-                  if (j>0)
-                  out << SP << SP << SP << "idxOut += " << fInputShapes[j-1][fAxis] << ";\n";
-                  out << SP << SP << SP << "int idxIn" << j <<" =";
+                  out << SP << SP << SP << "int idxOut =";
                   for (int k = 0; k < fAxis; k++)
-                     out << " + " << inStrides[j][k] << "*i" << k;
+                     out << " + " << outStride[k] << "*i" << k;
                   out << ";\n";
-                  out << SP << SP << SP << "for (size_t iC = 0; iC < " << fInputShapes[j][fAxis] << "; ++iC) {\n";
-                  out << SP << SP << SP << SP << "tensor_" << fOutput << "[idxOut+iC] = tensor_" << fInputs[j] << "[idxIn" << j << "+iC];\n";
-                  out << SP << SP << SP << "}\n";
-               // concatenate the axis values
+
+                  for (size_t j = 0; j < fInputs.size(); j++) {
+                     if (j>0)
+                     out << SP << SP << SP << "idxOut += " << fInputShapes[j-1][fAxis] << ";\n";
+                     out << SP << SP << SP << "int idxIn" << j <<" =";
+                     for (int k = 0; k < fAxis; k++)
+                        out << " + " << inStrides[j][k] << "*i" << k;
+                     out << ";\n";
+                     out << SP << SP << SP << "for (size_t iC = 0; iC < " << fInputShapes[j][fAxis] << "; ++iC) {\n";
+                     out << SP << SP << SP << SP << "tensor_" << fOutput << "[idxOut+iC] = tensor_" << fInputs[j] << "[idxIn" << j << "+iC];\n";
+                     out << SP << SP << SP << "}\n";
+                  // concatenate the axis values
+                  }
+                  for (int i = 0; i < fAxis; ++i) {
+                     out << SP << "}\n";
+                  }
                }
-                for (int i = 0; i < fAxis; ++i) {
-                    out << SP << "}\n";
-                }
             }
-
-
-             return out.str();
+            else if(fnewAxis == 1){
+               for(auto &it : fInputs){
+                  out<<SP<<"tensor_" << fOutput<<".append(tensor_"<<it<<");\n";
+                  out<<SP<<"tensor_" << fOutput<<".append(,);\n";
+               }
+            }
+            return out.str();
          }
-
-
      };
  }//SOFIE
  }//Experimental
