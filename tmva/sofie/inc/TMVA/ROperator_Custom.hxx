64bda91738e37a48e951d06eebc5e75b5af0be3d
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-09-06 17:39:22 +0200

    fix: fTensor name for input to custom op

--- tmva/sofie/inc/TMVA/ROperator_Custom.hxx
+++ tmva/sofie/inc/TMVA/ROperator_Custom.hxx
@@ -63,7 +63,7 @@ public:
       out << "\n//------ "<<fOpName<<" \n";
       out << SP << "std::vector<std::vector<float>> " << fOpName << "_input;\n";
       for(auto i = 0; i<fInputNames.size(); ++i){
-        out << SP << fOpName << "_input.emplace_back(" << fInputNames[i] << ");\n"; 
+        out << SP << fOpName << "_input.emplace_back(fTensor_" << fInputNames[i] << ");\n"; 
       }
       out << SP << "std::vector<float>"<<fOpName<<"_result = "<<fOpName<<"::Compute("<<fOpName<<"_input"<<");\n";
       for(auto i = 0; i<fOutputNames.size(); ++i){
