c73e46d7e37cfca3604ab2ddb8a45a7d28f50728
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-09-09 13:40:38 +0200

    [DF] Add logging to RunGraphs

--- tree/dataframe/src/RDFHelpers.cxx
+++ tree/dataframe/src/RDFHelpers.cxx
@@ -11,8 +11,11 @@
 #include "ROOT/RDFHelpers.hxx"
 #include "TROOT.h"      // IsImplicitMTEnabled
 #include "TError.h"     // Warning
+#include "TStopwatch.h"
 #include "RConfigure.h" // R__USE_IMT
+#include "ROOT/RLogger.hxx"
 #include "ROOT/RDF/RLoopManager.hxx" // for RLoopManager
+#include "ROOT/RDF/Utils.hxx"
 #include "ROOT/RResultHandle.hxx"    // for RResultHandle, RunGraphs
 #ifdef R__USE_IMT
 #include "ROOT/TThreadExecutor.hxx"
@@ -46,18 +49,37 @@ void ROOT::RDF::RunGraphs(std::vector<RResultHandle> handles)
    std::vector<RResultHandle> uniqueLoops(s.begin(), s.end());
 
    // Trigger jitting. One call is enough to jit the code required by all computation graphs.
-   uniqueLoops[0].fLoopManager->Jit();
+   TStopwatch sw;
+   sw.Start();
+   {
+      auto silenceRDFLogs = ROOT::Experimental::RLogScopedVerbosity(ROOT::Detail::RDF::RDFLogChannel(),
+                                                                    ROOT::Experimental::ELogLevel::kError);
+      uniqueLoops[0].fLoopManager->Jit();
+   }
+   sw.Stop();
+   R__LOG_INFO(ROOT::Detail::RDF::RDFLogChannel())
+      << "Just-in-time compilation phase for RunGraphs (" << uniqueLoops.size()
+      << " unique computation graphs) completed"
+      << (sw.RealTime() > 1e-3 ? " in " + std::to_string(sw.RealTime()) + " seconds." : " in less than 1ms.");
 
    // Trigger the unique event loops
    auto run = [](RResultHandle &h) {
       if (h.fLoopManager)
          h.fLoopManager->Run(/*jit=*/false);
    };
+
+   sw.Start();
 #ifdef R__USE_IMT
    if (ROOT::IsImplicitMTEnabled()) {
       ROOT::TThreadExecutor{}.Foreach(run, uniqueLoops);
-      return;
+   } else {
+#endif
+      std::for_each(uniqueLoops.begin(), uniqueLoops.end(), run);
+#ifdef R__USE_IMT
    }
-#endif // R__USE_IMT
-   std::for_each(uniqueLoops.begin(), uniqueLoops.end(), run);
+#endif
+   sw.Stop();
+   R__LOG_INFO(ROOT::Detail::RDF::RDFLogChannel())
+      << "Finished RunGraphs run (" << uniqueLoops.size() << " unique computation graphs, " << sw.CpuTime() << "s CPU, "
+      << sw.RealTime() << "s elapsed).";
 }
