be6162e20805822dca48aa9929df19a6f104b9f9
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-09-11 21:16:25 +0200

    fix: test description for custom operator

--- tmva/pymva/test/EmitCustomModel.cxx
+++ tmva/pymva/test/EmitCustomModel.cxx
@@ -1,12 +1,12 @@
 // Author: Sanjiban Sengupta, 2022
 // Description:
-//           This is to test the usability of the Custom Operator
+//           This is to test the Custom Operator
 //           in TMVA SOFIE. The program is run when the
 //           target 'TestCustomOp' is built. The program
 //           first creates a Keras .h5 file, parses it and adds
 //           a custom operator and then generates the inference header 
-//           file. It also creates another Keras .h5 file with the library 
-//           definition of the custom operator included to test the equality.
+//           file. During the test, the custom operator is added as a 
+//           Lambda layer in the Keras model and is tested for equality.
 
 #include "Python.h"
 
