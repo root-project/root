d8495e0ebe949d09e2479362a4e57439e94b0ec8
Sergey Linev <S.Linev@gsi.de>
2022-09-13 08:40:11 +0200

    Use nullptr in core/textinput classes

--- core/textinput/src/Getline.cxx
+++ core/textinput/src/Getline.cxx
@@ -26,8 +26,8 @@
 #include "TInterpreter.h"
 
 extern "C" {
-   int (* Gl_in_key)(int ch) = 0;
-   int (* Gl_beep_hook)() = 0;
+   int (* Gl_in_key)(int ch) = nullptr;
+   int (* Gl_beep_hook)() = nullptr;
 }
 
 
@@ -208,24 +208,23 @@ Gl_histadd(const char* buf) {
 /* Wrapper around textinput.
  * Modes: -1 = init, 0 = line mode, 1 = one char at a time mode, 2 = cleanup, 3 = clear input line
  */
-const char*
-Getlinem(EGetLineMode mode, const char* prompt) {
+const char *Getlinem(EGetLineMode mode, const char* prompt) {
 
    if (mode == kClear) {
       TextInputHolder::getHolder().TakeInput(true);
-      return 0;
+      return nullptr;
    }
 
    if (mode == kCleanUp) {
       TextInputHolder::get().ReleaseInputOutput();
-      return 0;
+      return nullptr;
    }
 
    if (mode == kOneChar) {
       // Check first display: if !TTY, read full line.
       const textinput::Display* disp
          = TextInputHolder::get().GetContext()->GetDisplays()[0];
-      const textinput::TerminalDisplay* tdisp = 0;
+      const textinput::TerminalDisplay* tdisp = nullptr;
       if (disp) tdisp = dynamic_cast<const textinput::TerminalDisplay*>(disp);
       if (tdisp && !tdisp->IsTTY()) {
          mode = kLine1;
@@ -239,7 +238,7 @@ Getlinem(EGetLineMode mode, const char* prompt) {
       // Trigger attach:
       TextInputHolder::get().Redraw();
       if (mode == kInit) {
-         return 0;
+         return nullptr;
       }
       TextInputHolder::get().SetBlockingUntilEOL();
    } else {
@@ -264,7 +263,7 @@ Getlinem(EGetLineMode mode, const char* prompt) {
       return "";
    }
 
-   return NULL;
+   return nullptr;
 }
 
 const char*
