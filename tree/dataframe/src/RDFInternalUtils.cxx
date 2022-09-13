6247d9c5b2d0d3ae65583cd244fa2013526f0024
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2022-09-13 13:49:39 +0200

    [DF][NFC] Add header guard for MakeDataFrameFromSpec

--- tree/dataframe/src/RDFInternalUtils.cxx
+++ tree/dataframe/src/RDFInternalUtils.cxx
@@ -1,3 +1,13 @@
+// Author: Ivan Kabadzhov CERN, Vincenzo Eduardo Padulano CERN/UPV 06/2022
+
+/*************************************************************************
+ * Copyright (C) 1995-2022, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 #include "ROOT/RDF/InternalUtils.hxx"
 
 namespace ROOT {
