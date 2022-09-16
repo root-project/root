d2afcb30053bb6194e6e85e53e4fc0adc49f0bd2
Philippe Canal <pcanal@fnal.gov>
2022-09-16 16:37:50 -0500

    Fix #11383 (deadlock at initialization)
    Previously if a message (for example warning of duplicate dictionary)
    happened during intialization, it would trigger the initialization of gROOT
    and could trigger a nested Warning. for example due to the duplicate rootmap
    file entry and would cause a dead lock (recursiverly taking the
    non recursive lock GetErrorMutex().

--- core/base/src/TErrorDefaultHandler.cxx
+++ core/base/src/TErrorDefaultHandler.cxx
@@ -101,12 +101,14 @@ again:
 void DefaultErrorHandler(Int_t level, Bool_t abort_bool, const char *location, const char *msg)
 {
    if (gErrorIgnoreLevel == kUnset) {
+      // This can also print error messages, so we need to do it outside the lock
+      auto cstrlevel = gEnv->GetValue("Root.ErrorIgnoreLevel", "Print");
+
       std::lock_guard<std::mutex> guard(*GetErrorMutex());
 
       gErrorIgnoreLevel = 0;
       if (gEnv) {
          std::string slevel;
-         auto cstrlevel = gEnv->GetValue("Root.ErrorIgnoreLevel", "Print");
          while (cstrlevel && *cstrlevel) {
             slevel.push_back(tolower(*cstrlevel));
             cstrlevel++;
