092f4e4a5f464a6bc6bce62fa3e9d63afbdcf3b9
Philippe Canal <pcanal@fnal.gov>
2022-09-20 11:56:16 -0500

    #11383: switch early warning from main to static

--- root/core/recursive/recursivewarning.cxx
+++ root/core/recursive/recursivewarning.cxx
@@ -1,14 +1,18 @@
 #include "TError.h"
 #include "TROOT.h"
 
+int func() {
+   Info("RecursiveMessage", "We are before the start of main, gROOT is not yet initialized");
+   return 0;
+}
+
+// This will trigger the initialization of gROOT and
+// trigger a nested Warning due to the duplicate rootmap file
+// This used to cause a dead lock (recursiverly taking the
+// non recursive lock GetErrorMutex() ).
+auto i = func();
+
 int main(int, char**)
 {
-  // gROOT;
-  // This will trigger the initialization of gROOT and
-  // trigger a nested Warning due to the duplicate rootmap file
-  // This used to cause a dead lock (recursiverly taking the
-  // non recursive lock GetErrorMutex() ).
-  Info("RecursiveMessage", "We are at the start of main, gROOT is not yet initialized");
-
   return 0;
 }
