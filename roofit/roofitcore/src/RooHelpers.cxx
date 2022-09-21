21d6372e500cd5fd8e72c6f906a2eb7b377ae479
Jonas Rembser <jonas.rembser@cern.ch>
2022-09-21 13:05:25 +0200

    [RF] Consider the `SplitRange` in overlap checks for simultaneous PDFs
    When the `SplitRange` command arguemnt is used in
    `RooAbsPdf::createNLL`, the actual range names used for the fit depend
    on the channel, with the range names suffixed by the category name. This
    should be considered correctly in the overlap checks.
    
    Closes #11396.

--- roofit/roofitcore/src/RooHelpers.cxx
+++ roofit/roofitcore/src/RooHelpers.cxx
@@ -14,17 +14,19 @@
  * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
  *****************************************************************************/
 
-#include "RooHelpers.h"
-#include "RooAbsPdf.h"
-#include "RooAbsData.h"
-#include "RooDataHist.h"
-#include "RooDataSet.h"
-#include "RooAbsRealLValue.h"
-#include "RooArgList.h"
-#include "RooAbsCategory.h"
+#include <RooHelpers.h>
 
-#include "ROOT/StringUtils.hxx"
-#include "TClass.h"
+#include <RooAbsCategory.h>
+#include <RooAbsData.h>
+#include <RooAbsPdf.h>
+#include <RooAbsRealLValue.h>
+#include <RooArgList.h>
+#include <RooDataHist.h>
+#include <RooDataSet.h>
+#include <RooSimultaneous.h>
+
+#include <ROOT/StringUtils.hxx>
+#include <TClass.h>
 
 namespace RooHelpers {
 
@@ -179,9 +181,38 @@ std::pair<double, double> getRangeOrBinningInterval(RooAbsArg const* arg, const 
 
 
 /// Check if there is any overlap when a list of ranges is applied to a set of observables.
-/// \param[in] arg RooAbsCollection with the observables to check for overlap.
-/// \param[in] rangeName The names of the ranges.
-bool checkIfRangesOverlap(RooAbsPdf const& pdf, RooAbsData const& data, std::vector<std::string> const& rangeNames) {
+/// \param[in] pdf the PDF
+/// \param[in] data RooAbsCollection with the observables to check for overlap.
+/// \param[in] rangeNames The names of the ranges.
+/// \param[in] splitRange If `true`, each component of a RooSimultaneous will
+///                       be checked individually for overlaps, with the range
+///                       names in that component suffixed by `_<cat_label>`.
+///                       See the `SplitRange()` command argument of
+///                       RooAbsPdf::fitTo()` to understand where this is used.
+bool checkIfRangesOverlap(RooAbsPdf const& pdf,
+                          RooAbsData const& data,
+                          std::vector<std::string> const& rangeNames,
+                          bool splitRange)
+{
+  // If the PDF is a RooSimultaneous and the `splitRange` option is set, we
+  // have to check each component PDF with a different set of rangeNames, each
+  // suffixed by the category name.
+  if(splitRange && dynamic_cast<RooSimultaneous const*>(&pdf)) {
+    auto const& simPdf = static_cast<RooSimultaneous const&>(pdf);
+    bool hasOverlap = false;
+    std::vector<std::string> rangeNamesSplit;
+    for (const auto& catState : simPdf.indexCat()) {
+      const std::string& catName = catState.first;
+      for(std::string const& rangeName : rangeNames) {
+        rangeNamesSplit.emplace_back(rangeName + "_" + catName);
+      }
+      hasOverlap |= checkIfRangesOverlap(*simPdf.getPdf(catName.c_str()), data, rangeNamesSplit, false);
+
+      rangeNamesSplit.clear();
+    }
+
+    return hasOverlap;
+  }
 
   auto observables = *pdf.getObservables(data);
 
