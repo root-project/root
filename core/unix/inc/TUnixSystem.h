98c91dae98ec57c64351c44bb7f9fce04b5630a5
ferdymercury <ferdymercury@users.noreply.github.com>
2022-10-04 14:52:59 +0200

    mark exit with noreturn attribute

--- core/unix/inc/TUnixSystem.h
+++ core/unix/inc/TUnixSystem.h
@@ -115,7 +115,7 @@ public:
    Int_t             Exec(const char *shellcmd) override;
    FILE             *OpenPipe(const char *shellcmd, const char *mode) override;
    int               ClosePipe(FILE *pipe) override;
-   void              Exit(int code, Bool_t mode = kTRUE) override;
+   void              Exit [[ noreturn ]] (int code, Bool_t mode = kTRUE) override;
    void              Abort(int code = 0) override;
    int               GetPid() override;
    void              StackTrace() override;
