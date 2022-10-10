9685bac8e7b1f9acc069f1175e8ab9440c921748
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2022-10-10 09:46:20 +0200

    [DF] Add RMetaData standalone
    Introducing a class acting as a heterogenuous container to store
    metadata. Since the intended use case would be obtaining metadata from
    json files, the nlohmann::json is used as the underlying container.
    This required an extra dependency in tree/dataframe.

---
+++ tree/dataframe/inc/ROOT/RDF/RMetaData.hxx
@@ -0,0 +1,43 @@
+// Author: Ivan Kabadzhov CERN  10/2022
+
+/*************************************************************************
+ * Copyright (C) 1995-2022, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_RDF_RMETADATA
+#define ROOT_RDF_RMETADATA
+
+#include <nlohmann/json.hpp>
+
+#include <string>
+
+namespace ROOT {
+namespace RDF {
+namespace Experimental {
+
+class RMetaData {
+   nlohmann::json fJson;
+
+public:
+   void AddMetaData(const std::string &cat, int val);
+   void AddMetaData(const std::string &cat, double val);
+   void AddMetaData(const std::string &cat, const std::string &val);
+   void AddJSONMetaData(const std::string &cat, const nlohmann::json &val);
+
+   // getter always returning the string representation
+   std::string Dump(const std::string &cat) const;
+   // type-safe! getters
+   int GetI(const std::string &cat) const;
+   double GetD(const std::string &cat) const;
+   std::string GetS(const std::string &cat) const;
+};
+
+} // namespace Experimental
+} // namespace RDF
+} // namespace ROOT
+
+#endif // ROOT_RDF_RMETADATA
