5ccd2f52c128990463a829c2a0a770683eee4cbb
Jonas Rembser <jonas.rembser@cern.ch>
2022-10-18 13:42:15 +0200

    [RF] Make sure value servers of integrals are next direct value servers
    Integrals of derived PDFs have their client-server interface messed up,
    and it's inconsistent with non-derived PDFs. Here is a reproducer:
    
    ```C++
    void repro() {
        using namespace RooFit;
        RooRealVar x("x", "", 0, 1);
    
        RooRealVar par("par", "", -0.005, -5, 5);
        RooProduct parMod("par_mod", "", RooArgSet(par, RooConst(10)));
    
        RooGaussian gauss("gauss", "", x, parMod, RooConst(2.0));
        RooGenericPdf pdf("pdf", "gauss", gauss);
    
        std::unique_ptr<RooAbsReal> integ1{gauss.createIntegral(x, *pdf.getIntegratorConfig(), nullptr)};
        integ1->Print("v");
    
        std::cout << std::endl;
    
        std::unique_ptr<RooAbsReal> integ2{pdf.createIntegral(x, *pdf.getIntegratorConfig(), nullptr)};
        integ2->Print("v");
    }
    ```
    
    The integral of the Gaussian has the correct value (`V`) servers (the parameters of the Gaussian):
    ```
        (0x7ffc9b9bf798,-S) RooRealVar::x ""
        (0x7ffc9b9bf240,--) RooGaussian::gauss ""
        (0x7ffc9b9bff68,V-) RooProduct::par_mod ""
        (0x55ac531cd470,V-) RooConstVar::2 "2"
    ```
    
    The integral of the `RooGenericPdf` should have the same value servers,
    but it doesn't. Instead, it uses the leaves of the computation graph:
    ```
        (0x7ffc9b9bf798,-S) RooRealVar::x ""
        (0x7ffc9b9bec88,--) RooRealSumPdf::pdf ""
        (0x7ffc9b9bfb80,V-) RooRealVar::par ""
        (0x55ac51984c50,V-) RooConstVar::10 "10"
        (0x55ac531cd470,V-) RooConstVar::2 "2"
    ```
    
    That means, the direct value server `par_mod` is **skipped** now,
    misrepresenting the client-server relationship.
    
    The new BatchMode makes strong use of the value-server interface for the
    `RooFitDriver`, so it's very sensitive to `RooRealIntegrals` getting it
    wrong.
    
    That's why this commit suggests to change the logic in `RooRealIntegral`
    to always add the next direct value servers that don't depend on the
    integration variable, and not the leaves.
    
    A unit test that verifies that the value servers of the integral are the
    mathematically direct value servers and not the computation graph leaves
    is also implemented.
    
    Closes #11578.

---
+++ roofit/roofitcore/test/testRooRealIntegral.cxx
@@ -0,0 +1,135 @@
+// Tests for RooRealIntegral
+// Authors: Jonas Rembser, CERN 10/2022
+
+#include <RooConstVar.h>
+#include <RooGaussian.h>
+#include <RooGenericPdf.h>
+#include <RooProduct.h>
+#include <RooProjectedPdf.h>
+#include <RooRealVar.h>
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+// Verify that the value servers of a RooRealIntegral are the direct
+// mathematical value servers of the integral, and not the leaves in the
+// computation graphs. For the Batch mode, it is important for the evaluation
+// of the computation graph that no direct value servers are skipped. See also
+// GitHub issue #11578.
+TEST(RooRealIntegral, ClientServerInterface1)
+{
+   using namespace RooFit;
+
+   RooRealVar x{"x", "", 0, 1};
+
+   RooRealVar mu{"mu", "", -0.005, -5, 5};
+
+   // This is the key in this test: the mathematically direct value server of
+   // the integral is the derived "muMod", and not the leaf of the computation
+   // graph "mu"
+   RooProduct muMod{"mu_mod", "", {mu, RooConst(10)}};
+
+   RooGaussian gauss{"gauss", "", x, muMod, RooConst(2.0)};
+   RooGenericPdf pdf{"gaussWrapped", "gauss", gauss};
+
+   std::unique_ptr<RooAbsReal> integ1{gauss.createIntegral(x, *pdf.getIntegratorConfig(), nullptr)};
+   std::unique_ptr<RooAbsReal> integ2{pdf.createIntegral(x, *pdf.getIntegratorConfig(), nullptr)};
+
+   RooArgList servers1{integ1->servers().begin(), integ1->servers().end()};
+   RooArgList servers2{integ2->servers().begin(), integ2->servers().end()};
+
+   // Sort alphabetically in case the two integrals didn't add the servers in
+   // the same order:
+   servers1.sort();
+   servers2.sort();
+
+   // Check that the server structure of the Gaussian integral looks as
+   // expected, which should be, if you use Print("v"):
+   //
+   //     (-S) RooRealVar::x ""
+   //     (--) RooGenericPdf::pdf "gauss"
+   //     (V-) RooProduct::mu_mod ""
+   //     (V-) RooConstVar::2 "2"
+   //
+   // What is important is that the indirect value server "mu" doesn't appear
+   // among the servers, and the direct value server "mu_mod" does.
+
+   EXPECT_EQ(servers1.size(), 4);
+
+   // Respect the alphabetical order here
+   EXPECT_EQ(std::string(servers1[0].GetName()), "2");
+   EXPECT_EQ(std::string(servers1[1].GetName()), "gauss");
+   EXPECT_EQ(std::string(servers1[2].GetName()), "mu_mod");
+   EXPECT_EQ(std::string(servers1[3].GetName()), "x");
+
+   EXPECT_TRUE(servers1[0].isValueServer(*integ1));
+   EXPECT_FALSE(servers1[1].isValueServer(*integ1));
+   EXPECT_TRUE(servers1[2].isValueServer(*integ1));
+   EXPECT_FALSE(servers1[3].isValueServer(*integ1));
+
+   EXPECT_FALSE(servers1[0].isShapeServer(*integ1));
+   EXPECT_FALSE(servers1[1].isShapeServer(*integ1));
+   EXPECT_FALSE(servers1[2].isShapeServer(*integ1));
+   EXPECT_TRUE(servers1[3].isShapeServer(*integ1));
+
+   // The Gaussian PDF wrapped in a RooGenericPdf should have exactly the same
+   // server structure, so let's check that:
+
+   EXPECT_EQ(servers2.size(), servers1.size());
+
+   for (std::size_t i = 0; i < servers1.size(); ++i) {
+      RooAbsArg const &s1 = servers1[i];
+      RooAbsArg const &s2 = servers2[i];
+
+      // The 2nd server is the integrated function, which doesn't have the same
+      // name (it's "gaussWrapped" for the second integral instead of "gauss")
+      if (i != 1) {
+         EXPECT_EQ(std::string(s1.GetName()), s2.GetName());
+      }
+
+      EXPECT_EQ(s1.isValueServer(*integ1), s2.isValueServer(*integ2));
+      EXPECT_EQ(s1.isShapeServer(*integ1), s2.isShapeServer(*integ2));
+   }
+}
+
+/// Here we are integrating a function that has shape servers to verify that
+/// they are correctly propagated as shape servers to the integral.
+TEST(RooRealIntegral, IntegrateFuncWithShapeServers)
+{
+   using namespace RooFit;
+
+   RooRealVar x("x", "", 0, 1);
+
+   RooRealVar mu("mu", "", -0.005, -5, 5);
+   RooProduct muMod("mu_mod", "", RooArgSet(mu, RooConst(10)));
+   RooRealVar sigma("sigma", "", 1, 0.5, 2);
+
+   RooGaussian gauss("gauss", "", x, muMod, sigma);
+   RooGenericPdf pdf("pdf", "gauss", gauss);
+
+   // Project over sigma, meaning sigma should now become a shape server
+   RooProjectedPdf gaussProj("gaussProj", "", gauss, sigma);
+
+   EXPECT_TRUE(x.isValueServer(gaussProj));
+   EXPECT_FALSE(x.isShapeServer(gaussProj));
+   EXPECT_TRUE(muMod.isValueServer(gaussProj));
+   EXPECT_FALSE(muMod.isShapeServer(gaussProj));
+   EXPECT_FALSE(sigma.isValueServer(gaussProj));
+   EXPECT_TRUE(sigma.isShapeServer(gaussProj));
+
+   // Integrating also over x, so both x and sigma should now be shape servers of the integral
+   std::unique_ptr<RooAbsReal> integ1{gaussProj.createIntegral(x, *pdf.getIntegratorConfig(), nullptr)};
+
+   EXPECT_FALSE(x.isValueServer(*integ1)); // x is now not a value server anymore
+   EXPECT_TRUE(x.isShapeServer(*integ1));
+   EXPECT_TRUE(muMod.isValueServer(*integ1));
+   EXPECT_FALSE(muMod.isShapeServer(*integ1));
+   EXPECT_FALSE(sigma.isValueServer(*integ1));
+   EXPECT_TRUE(sigma.isShapeServer(*integ1)); // sigma should still be shape server!
+
+   // Also check that that the number of servers is right (should be 3 for x,
+   // mu, and sigma, and 1 more for the underlying PDF)
+   EXPECT_EQ(gaussProj.servers().size(), 4);
+   EXPECT_EQ(integ1->servers().size(), 4);
+}
