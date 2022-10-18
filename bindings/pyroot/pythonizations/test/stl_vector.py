e6fcad7d14d6a7ffa0b59e4557005d88b05ce948
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2022-10-18 10:54:43 +0200

    [PyROOT][11581] Add test for std::vector<const char*>

--- bindings/pyroot/pythonizations/test/stl_vector.py
+++ bindings/pyroot/pythonizations/test/stl_vector.py
@@ -21,5 +21,13 @@ class STL_vector(unittest.TestCase):
         for elem in v:
             self.assertEqual(elem, elems.pop(0))
 
+    def test_vec_const_char_p(self):
+        '''
+        Test that creating a std::vector<const char*> does not raise any
+        exception (#11581).
+        '''
+        ROOT.std.vector['const char*']()
+
+
 if __name__ == '__main__':
     unittest.main()
