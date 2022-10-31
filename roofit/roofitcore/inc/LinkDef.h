b2b31fdc006433ca8c02b315b10668bdcd6697f3
Jonas Rembser <jonas.rembser@cern.ch>
2022-10-31 15:26:16 +0100

    [RF] Officially deprecate RooMinuit
    Officially deprecate RooMinuit with attributes for the compiler,
    indicating that it will be removed in ROOT 6.30.
    
    The dictionary generation via `ClassDef` etc. is also removed, because
    otherwise we would get the deprecation warning in the dictionary
    generation.

--- roofit/roofitcore/inc/LinkDef.h
+++ roofit/roofitcore/inc/LinkDef.h
@@ -149,7 +149,6 @@
 #pragma read sourceClass="RooMappedCategory::Entry" targetClass="RooMappedCategory::Entry" version="[1]" include="RooFitLegacy/RooCatTypeLegacy.h" \
     source="RooCatType _cat" target="_catIdx" code="{ _catIdx = onfile._cat.getVal(); }"
 #pragma link C++ class RooMCIntegrator+ ;
-#pragma link C++ class RooMinuit+ ;
 #pragma link C++ class RooMPSentinel+ ;
 #pragma link C++ class RooMultiCategory+ ;
 #pragma link off class RooNameReg+ ;
