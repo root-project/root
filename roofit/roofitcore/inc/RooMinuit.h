d1d93235c763d0d1be11d68c8ccfdb1f3366932e
Jonas Rembser <jonas.rembser@cern.ch>
2022-10-31 15:26:16 +0100

    [RF] Officially deprecate RooMinuit
    Officially deprecate RooMinuit with attributes for the compiler,
    indicating that it will be removed in ROOT 6.30.
    
    The dictionary generation via `ClassDef` etc. is also removed, because
    otherwise we would get the deprecation warning in the dictionary
    generation.

--- roofit/roofitcore/inc/RooMinuit.h
+++ roofit/roofitcore/inc/RooMinuit.h
@@ -1,5 +1,5 @@
 #warning "The deprecated RooMinuit class was replaced by the more general RooMinimizer. \
-Migrating to the RooMinimizer is easy because the interface is backwards compatible. \
-If you still want to use the old RooMinuit, #include \"RooFitLegacy/RooMinuit\" instead."
+The RooMinuit class will be gone in ROOT 6.30! \
+Migrating to the RooMinimizer is easy because the interface is backwards compatible.
 
 #include "RooFitLegacy/RooMinuit.h"
