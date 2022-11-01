80b20c8837722229bf33c4f491c1aeebebb3a132
Olivier Couet <olivier.couet@cern.ch>
2022-11-01 17:25:32 +0100

    [skip-ci] clarify which methods are not implemented in TH2Poly (#11648)

--- hist/hist/src/TH2Poly.cxx
+++ hist/hist/src/TH2Poly.cxx
@@ -1560,13 +1560,13 @@ Bool_t TH2PolyBin::IsInside(Double_t x, Double_t y) const
    return in;
 }
 
-////////////////////////////////////////////////////////////////////////
-/// RE-implement dummy functions to avoid users calling the
-/// corresponding implementations in TH1 or TH2
-//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// RE-implement dummy functions to avoid users calling the
+// corresponding implementations in TH1 or TH2
+////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Performs the operation: this = this + c1*f1. NOT IMPLEMENTED for TH2Poly
+/// NOT IMPLEMENTED for TH2Poly
 Bool_t TH2Poly::Add(TF1 *, Double_t, Option_t *)
 {
    Error("Add","Not implement for TH2Poly");
@@ -1574,7 +1574,7 @@ Bool_t TH2Poly::Add(TF1 *, Double_t, Option_t *)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Replace contents of this histogram by the addition of h1 and h2. NOT IMPLEMENTED for TH2Poly
+/// NOT IMPLEMENTED for TH2Poly
 Bool_t TH2Poly::Add(const TH1 *, const TH1 *, Double_t, Double_t)
 {
    Error("Add","Not implement for TH2Poly");
@@ -1582,7 +1582,7 @@ Bool_t TH2Poly::Add(const TH1 *, const TH1 *, Double_t, Double_t)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Performs the operation: this = this / c1*f1. NOT IMPLEMENTED for TH2Poly
+/// NOT IMPLEMENTED for TH2Poly
 Bool_t TH2Poly::Divide(TF1 *, Double_t)
 {
    Error("Divide","Not implement for TH2Poly");
