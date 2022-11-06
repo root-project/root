86ac290490e401258e1e52a1d82c7b8be056860c
Jonas Rembser <jonas.rembser@cern.ch>
2022-11-06 11:13:36 +0100

    [RF] Exclude `RooGrid` class from IO
    The `RooGrid` is a utility class for the `RooMCIntegrator`, which
    doesn't support IO itself. Therefore, it doesn't make sense to have a
    `ClassDef(1)` macro. It is only putting the unnecessary burden of
    keeping backwards compatibility on the developers.
    
    Therefore, this commit suggests to leave out the `ClassDef` macro out of
    `RooGrid`, and also remove the unnecessary base classes `TObject` and
    `RooPrintable`. There is only one printing function that makes sense
    anyway, which is kept without implementing the full `RooPrintable`
    interface.

--- roofit/roofitcore/inc/LinkDef.h
+++ roofit/roofitcore/inc/LinkDef.h
@@ -124,7 +124,6 @@
 #pragma link C++ class RooGenContext+ ;
 #pragma link C++ class RooGenericPdf+ ;
 #pragma link C++ class RooGenProdProj+ ;
-#pragma link C++ class RooGrid+ ;
 #pragma link C++ class RooHistError+ ;
 #pragma link C++ class RooHist+ ;
 #pragma link C++ class RooImproperIntegrator1D+ ;
