27f64cff987538b5929ddaf901e847f172a3dde2
Giovanna Lazzari Miotto <giovanna.lazzari.miotto@cern.ch>
2022-11-13 02:13:26 +0100

    [ntuple,daos] Introduce container-level ntuple locator by name
    Introduce a dedicated structure `RDaosContainerNTupleLocator` that maps an ntuple's name to its index and its anchor data in the container.
    
    Auxiliary functions will resolve the ntuple name by checking its presence in the container, fetching metadata and instantiating a descriptor builder.
    
    The source and sink ctors call these locator functions and retrieve the results; i.e., a partially-filled descriptor builder, the ntuple anchor and index in the container.
    
    The source throws an exception if no ntuple exists in storage with the given name.

--- tree/ntuple/v7/inc/ROOT/RPageStorageDaos.hxx
+++ tree/ntuple/v7/inc/ROOT/RPageStorageDaos.hxx
@@ -29,6 +29,34 @@
 #include <string>
 
 using ntuple_index_t = std::uint32_t;
+namespace ROOT::Experimental::Detail {
+struct RDaosNTupleAnchor;
+}
+
+struct RDaosContainerNTupleLocator {
+   std::string fName{};
+   std::uint32_t fHashedName{};
+   ntuple_index_t fIndex{};
+   std::unique_ptr<ROOT::Experimental::Detail::RDaosNTupleAnchor> fAnchor;
+   ROOT::Experimental::RNTupleDescriptorBuilder fDescBuilder{};
+   static const ntuple_index_t kReservedHash = 0;
+
+   RDaosContainerNTupleLocator() = default;
+   explicit RDaosContainerNTupleLocator(const std::string &ntupleName) : fName(ntupleName), fHashedName(Hash(ntupleName))
+   {
+      fIndex = fHashedName;
+      fAnchor = std::make_unique<ROOT::Experimental::Detail::RDaosNTupleAnchor>();
+   };
+
+   ntuple_index_t GetIndex() const { return fIndex; };
+   static ntuple_index_t Hash(const std::string &ntupleName)
+   {
+      // Use `std::hash` implementation followed by XOR to fit in `ntuple_index_t` (i.e., 32-bit)
+      std::size_t h = std::hash<std::string>{}(ntupleName);
+      ntuple_index_t hash = (h & 0xffffffff) ^ ((h >> 32));
+      return (hash == kReservedHash) ? kReservedHash + 1 : hash;
+   }
+};
 
 namespace ROOT {
 
@@ -181,6 +209,8 @@ private:
    std::string fURI;
    /// The cluster pool asynchronously preloads the next few clusters
    std::unique_ptr<RClusterPool> fClusterPool;
+
+   RNTupleDescriptorBuilder fDescriptorBuilder;
 
    RPage PopulatePageFromCluster(ColumnHandle_t columnHandle, const RClusterInfo &clusterInfo,
                                  ClusterSize_t::ValueType idxInCluster);
