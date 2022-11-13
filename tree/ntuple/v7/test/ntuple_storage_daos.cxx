b8f40f99404b8b9bb3c873668fc2a5139fd69321
Giovanna Lazzari Miotto <giovanna.lazzari.miotto@cern.ch>
2022-11-13 02:13:27 +0100

    [ntuple,daos] Add unit test coverage for multiple ntuples
    Add test `MultipleNTuplesPerContainer` to test suite for `RPageStorageDaos`, covering:
    
    - management of multiple ntuples in container;
    
    - read attempt for an ntuple name not in the container;
    
    The commit also applies small changes to the test containers' and test ntuples' names and clang-formatting.

--- tree/ntuple/v7/test/ntuple_storage_daos.cxx
+++ tree/ntuple/v7/test/ntuple_storage_daos.cxx
@@ -7,7 +7,8 @@ TEST(RPageStorageDaos, Basics)
 {
    ROOT::TestSupport::CheckDiagsRAII diags;
    diags.optionalDiag(kWarning, "in int daos_init()", "This RNTuple build uses libdaos_mock. Use only for testing!");
-   diags.requiredDiag(kWarning, "ROOT::Experimental::Detail::RPageSinkDaos::RPageSinkDaos", "The DAOS backend is experimental and still under development.", false);
+   diags.requiredDiag(kWarning, "ROOT::Experimental::Detail::RPageSinkDaos::RPageSinkDaos",
+                      "The DAOS backend is experimental and still under development.", false);
    diags.requiredDiag(kWarning, "[ROOT.NTuple]", "Pre-release format version: RC 1", false);
 
    std::string daosUri("daos://" R__DAOS_TEST_POOL "/container-test-1");
@@ -17,7 +18,7 @@ TEST(RPageStorageDaos, Basics)
 
    {
       RNTupleWriteOptions options;
-      auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple", daosUri, options);
+      auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple-1", daosUri, options);
       ntuple->Fill();
       ntuple->CommitCluster();
       *wrPt = 24.0;
@@ -26,7 +27,7 @@ TEST(RPageStorageDaos, Basics)
       ntuple->Fill();
    }
 
-   auto ntuple = RNTupleReader::Open("f", daosUri);
+   auto ntuple = RNTupleReader::Open("ntuple-1", daosUri);
    EXPECT_EQ(3U, ntuple->GetNEntries());
    auto rdPt = ntuple->GetModel()->GetDefaultEntry()->Get<float>("pt");
 
@@ -49,13 +50,13 @@ TEST(RPageStorageDaos, Extended)
    double chksumWrite = 0.0;
    {
       RNTupleWriteOptions options;
-      auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple", daosUri, options);
+      auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple-2", daosUri, options);
       constexpr unsigned int nEvents = 32000;
       for (unsigned int i = 0; i < nEvents; ++i) {
          auto nVec = 1 + floor(rnd.Rndm() * 1000.);
          wrVector->resize(nVec);
          for (unsigned int n = 0; n < nVec; ++n) {
-            auto val = 1 + rnd.Rndm()*1000. - 500.;
+            auto val = 1 + rnd.Rndm() * 1000. - 500.;
             (*wrVector)[n] = val;
             chksumWrite += val;
          }
@@ -67,7 +68,7 @@ TEST(RPageStorageDaos, Extended)
 
    RNTupleReadOptions options;
    options.SetClusterBunchSize(5);
-   auto ntuple = RNTupleReader::Open("f", daosUri, options);
+   auto ntuple = RNTupleReader::Open("ntuple-2", daosUri, options);
    auto rdVector = ntuple->GetModel()->GetDefaultEntry()->Get<std::vector<double>>("vector");
 
    double chksumRead = 0.0;
@@ -89,9 +90,9 @@ TEST(RPageStorageDaos, Options)
       RNTupleWriteOptionsDaos options;
       options.SetObjectClass("UNKNOWN");
       try {
-         auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple", daosUri, options);
+         auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple-3", daosUri, options);
          FAIL() << "unknown object class should throw";
-      } catch (const RException& err) {
+      } catch (const RException &err) {
          EXPECT_THAT(err.what(), testing::HasSubstr("UNKNOWN"));
       }
    }
@@ -102,16 +103,68 @@ TEST(RPageStorageDaos, Options)
 
       RNTupleWriteOptionsDaos options;
       options.SetObjectClass("RP_XSF");
-      auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple", daosUri, options);
+      auto ntuple = RNTupleWriter::Recreate(std::move(model), "ntuple-3", daosUri, options);
       ntuple->Fill();
       ntuple->CommitCluster();
    }
 
    auto readOptions = RNTupleReadOptions();
    readOptions.SetClusterBunchSize(3);
-   ROOT::Experimental::Detail::RPageSourceDaos source("ntuple", daosUri, readOptions);
+   ROOT::Experimental::Detail::RPageSourceDaos source("ntuple-3", daosUri, readOptions);
    source.Attach();
    EXPECT_STREQ("RP_XSF", source.GetObjectClass().c_str());
    EXPECT_EQ(3U, source.GetReadOptions().GetClusterBunchSize());
    EXPECT_EQ(1U, source.GetNEntries());
+}
+
+TEST(RPageStorageDaos, MultipleNTuplesPerContainer)
+{
+   std::string daosUri("daos://" R__DAOS_TEST_POOL "/container-test-4");
+
+   RNTupleWriteOptions options;
+
+   {
+      auto modelA = RNTupleModel::Create();
+      auto wrPt = modelA->MakeField<float>("pt", 34.0);
+      auto ntuple = RNTupleWriter::Recreate(std::move(modelA), "ntupleA", daosUri, options);
+      ntuple->Fill();
+      *wrPt = 160.0;
+      ntuple->Fill();
+   }
+   {
+      auto modelB = RNTupleModel::Create();
+      auto wrPt = modelB->MakeField<float>("pt", 81.0);
+      auto ntuple = RNTupleWriter::Recreate(std::move(modelB), "ntupleB", daosUri, options);
+      ntuple->Fill();
+      *wrPt = 96.0;
+      ntuple->Fill();
+      *wrPt = 54.0;
+      ntuple->Fill();
+   }
+   {
+      auto ntupleA = RNTupleReader::Open("ntupleA", daosUri);
+      auto ntupleB = RNTupleReader::Open("ntupleB", daosUri);
+      EXPECT_EQ(2U, ntupleA->GetNEntries());
+      EXPECT_EQ(3U, ntupleB->GetNEntries());
+
+      {
+         auto rdPt = ntupleA->GetModel()->GetDefaultEntry()->Get<float>("pt");
+         ntupleA->LoadEntry(0);
+         EXPECT_EQ(34.0, *rdPt);
+         ntupleA->LoadEntry(1);
+         EXPECT_EQ(160.0, *rdPt);
+      }
+      {
+         auto rdPt = ntupleB->GetModel()->GetDefaultEntry()->Get<float>("pt");
+         ntupleB->LoadEntry(0);
+         EXPECT_EQ(81.0, *rdPt);
+         ntupleB->LoadEntry(1);
+         EXPECT_EQ(96.0, *rdPt);
+         ntupleB->LoadEntry(2);
+         EXPECT_EQ(54.0, *rdPt);
+      }
+   }
+
+   // Nonexistent ntuple
+   EXPECT_THROW(RNTupleReader::Open("ntupleC", daosUri), ROOT::Experimental::RException);
 }
