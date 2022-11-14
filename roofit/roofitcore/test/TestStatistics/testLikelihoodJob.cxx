342a61e84dd5ba172056c90adae15d4b1b86412d
Mattias Ellert <mattias.ellert@physics.uu.se>
2022-11-14 15:56:07 +0100

    Backward compatibility with older googletest versions in EPEL

--- roofit/roofitcore/test/TestStatistics/testLikelihoodJob.cxx
+++ roofit/roofitcore/test/TestStatistics/testLikelihoodJob.cxx
@@ -31,6 +31,12 @@
 #include <stdexcept> // runtime_error
 
 #include "gtest/gtest.h"
+
+// Backward compatibility for gtest version < 1.10.0
+#ifndef INSTANTIATE_TEST_SUITE_P
+#define INSTANTIATE_TEST_SUITE_P INSTANTIATE_TEST_CASE_P
+#endif
+
 #include "../test_lib.h" // generate_1D_gaussian_pdf_nll
 
 using RooFit::TestStatistics::LikelihoodWrapper;
