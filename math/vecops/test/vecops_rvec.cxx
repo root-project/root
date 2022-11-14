8bfbf2b3737bb5baf606da8bf8409e42ed5ae83a
Mattias Ellert <mattias.ellert@physics.uu.se>
2022-11-14 14:52:35 +0100

    Add comments about gtest backward compatibility

--- math/vecops/test/vecops_rvec.cxx
+++ math/vecops/test/vecops_rvec.cxx
@@ -13,6 +13,7 @@
 #include <sstream>
 #include <cmath>
 
+// Backward compatibility for gtest version < 1.10.0
 #ifndef INSTANTIATE_TEST_SUITE_P
 #define INSTANTIATE_TEST_SUITE_P INSTANTIATE_TEST_CASE_P
 #endif
