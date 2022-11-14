2ecca47692ed581855fd3e932b0015c2f6862b22
Mattias Ellert <mattias.ellert@physics.uu.se>
2022-11-14 14:52:35 +0100

    Add comments about gtest backward compatibility

--- tree/tree/test/TOffsetGeneration.cxx
+++ tree/tree/test/TOffsetGeneration.cxx
@@ -9,6 +9,7 @@
 #include "ROOT/TestSupport.hxx"
 #include "gtest/gtest.h"
 
+// Backward compatibility for gtest version < 1.10.0
 #ifndef INSTANTIATE_TEST_SUITE_P
 #define SetUpTestSuite SetUpTestCase
 #endif
