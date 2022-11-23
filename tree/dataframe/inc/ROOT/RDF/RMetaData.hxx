6a96414de514b3d77cf8a342e3366687900db238
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2022-11-23 13:44:53 +0100

    [DF] Change internals, the spec now stores a vector of groups

--- tree/dataframe/inc/ROOT/RDF/RMetaData.hxx
+++ tree/dataframe/inc/ROOT/RDF/RMetaData.hxx
@@ -12,7 +12,6 @@
 #define ROOT_RDF_RMETADATA
 
 #include <nlohmann/json.hpp>
-
 #include <string>
 
 namespace ROOT {
@@ -43,60 +42,6 @@ public:
    double GetD(const std::string &key, double defaultVal) const;
    const std::string GetS(const std::string &key, std::string defaultVal) const;
 };
-
-/**
-\class ROOT::RDF::Experimental::RDatasetGroup
-\ingroup dataframe
-\brief Class representing a dataset group (mapping of trees (and their fileglobs) to metadata)
- 
- This class should be passed to RSpecBuilder in order to build a RDataFrame.
-*/
-class RDatasetGroup {
-   std::string fGroupName;
-   /**
-    * A list of names of trees.
-    * This list should go in lockstep with fFileNameGlobs, only in case this dataset is a TChain where each file
-    * contains its own tree with a different name from the global name of the dataset.
-    * Otherwise, fTreeNames contains 1 treename, that is common for all file globs.
-    */
-   std::vector<std::string> fTreeNames;
-   /**
-    * A list of file names.
-    * They can contain the globbing characters supported by TChain. See TChain::Add for more information.
-    */
-   std::vector<std::string> fFileNameGlobs;
-   RMetaData fMetaData;
-
-   unsigned int fGroupId{0}; // global group index, set inside of the RDatasetSpec
-
-public:
-   RDatasetGroup(const std::string &groupName, const std::string &treeName, const std::string &fileNameGlob,
-                 const RMetaData &metaData = {});
-
-   RDatasetGroup(const std::string &groupName, const std::string &treeName,
-                 const std::vector<std::string> &fileNameGlobs, const RMetaData &metaData = {});
-
-   RDatasetGroup(const std::string &groupName,
-                 const std::vector<std::pair<std::string, std::string>> &treeAndFileNameGlobs,
-                 const RMetaData &metaData = {});
-
-   RDatasetGroup(const std::string &groupName, const std::vector<std::string> &treeNames,
-                 const std::vector<std::string> &fileNameGlobs, const RMetaData &metaData = {});
-
-   /// \cond HIDDEN_SYMBOLS
-   RDatasetGroup() {} // empty constructor to make RSampleInfo happy
-   /// \endcond
-
-   const std::string &GetGroupName() const;
-   const std::vector<std::string> &GetTreeNames() const;
-   const std::vector<std::string> &GetFileNameGlobs() const;
-   const RMetaData &GetMetaData() const;
-
-   /// \cond HIDDEN_SYMBOLS
-   unsigned int GetGroupId() const; // intended to be used only after the RDataSpec is build, otherwise is 0
-   void SetGroupId(unsigned int id);
-   /// \endcond
-   };
 
 } // namespace Experimental
 } // namespace RDF
