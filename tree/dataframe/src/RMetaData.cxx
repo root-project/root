f0b5d32c6e16b9f2f312ce5e3f6b11f7f29ebfbf
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2022-11-23 13:44:53 +0100

    [DF] Change internals, the spec now stores a vector of groups

--- tree/dataframe/src/RMetaData.cxx
+++ tree/dataframe/src/RMetaData.cxx
@@ -11,8 +11,6 @@
 #include "ROOT/RDF/RMetaData.hxx"
 #include <stdexcept> // std::logic_error
 
-#include <iostream>
-
 namespace ROOT {
 namespace RDF {
 namespace Experimental {
@@ -89,69 +87,6 @@ const std::string RMetaData::GetS(const std::string &key, std::string defaultVal
    if (!fJson[key].is_string())
       throw std::logic_error("Key " + key + " is not of type string.");
    return fJson[key].get<std::string>();
-}
-
-RDatasetGroup::RDatasetGroup(const std::string &groupName, const std::string &treeName, const std::string &fileNameGlob,
-                             const RMetaData &metaData)
-: fGroupName(groupName), fTreeNames({treeName}), fFileNameGlobs({fileNameGlob}), fMetaData(metaData)
-{
-}
-
-RDatasetGroup::RDatasetGroup(const std::string &groupName, const std::string &treeName,
-                             const std::vector<std::string> &fileNameGlobs, const RMetaData &metaData)
-: fGroupName(groupName), fTreeNames(std::vector<std::string>(fileNameGlobs.size(), treeName)), fFileNameGlobs(fileNameGlobs), fMetaData(metaData)
-{
-}
-
-RDatasetGroup::RDatasetGroup(const std::string &groupName,
-                             const std::vector<std::pair<std::string, std::string>> &treeAndFileNameGlobs,
-                             const RMetaData &metaData)
-: fGroupName(groupName), fMetaData(metaData)
-{
-   fTreeNames.reserve(treeAndFileNameGlobs.size());
-   fFileNameGlobs.reserve(treeAndFileNameGlobs.size());
-   for (auto &p : treeAndFileNameGlobs) {
-      fTreeNames.emplace_back(p.first);
-      fFileNameGlobs.emplace_back(p.second);
-   }
-}
-
-RDatasetGroup::RDatasetGroup(const std::string &groupName, const std::vector<std::string> &treeNames,
-                             const std::vector<std::string> &fileNameGlobs, const RMetaData &metaData)
-: fGroupName(groupName), fTreeNames(treeNames), fFileNameGlobs(fileNameGlobs), fMetaData(metaData)
-{
-   if (treeNames.size() != 1 && treeNames.size() != fileNameGlobs.size())
-      throw std::logic_error("Mismatch between number of trees and file globs.");
-}
-
-const std::string &RDatasetGroup::GetGroupName() const
-{
-   return fGroupName;
-}
-
-const std::vector<std::string> &RDatasetGroup::GetTreeNames() const
-{
-   return fTreeNames;
-}
-
-const std::vector<std::string> &RDatasetGroup::GetFileNameGlobs() const
-{
-   return fFileNameGlobs;
-}
-
-const RMetaData &RDatasetGroup::GetMetaData() const
-{
-   return fMetaData;
-}
-
-unsigned int RDatasetGroup::GetGroupId() const
-{
-   return fGroupId;
-}
-
-void RDatasetGroup::SetGroupId(unsigned int id)
-{
-   fGroupId = id;
 }
 
 } // namespace Experimental
