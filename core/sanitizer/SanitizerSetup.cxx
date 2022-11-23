e5fa0b030a58b98e07fb04543bd0b66ec62b3c2b
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2022-11-23 16:22:49 +0100

    Disable ASan check for initialization order
    It finds a false positive in llvm::DataLayout::reset accessing
    DefaultAlignments while loading the Net library. At this point,
    Cling and LLVM have definitely been initialized. This is a known
    limitation of the order check, which is why it was disabled by
    default. Remove our config to enable it for now to allow checking
    the LLVM upgrade with AddressSanitizer instrumentation.

--- core/sanitizer/SanitizerSetup.cxx
+++ core/sanitizer/SanitizerSetup.cxx
@@ -22,9 +22,12 @@ const char* __asan_default_options() {
 #define DETECT_LEAKS ":detect_leaks=0"
 #endif
 
+  // TODO: Re-enable check_initialization_order=1, currently finds a false
+  // positive in llvm::DataLayout::reset accessing DefaultAlignments while
+  // loading the Net library.
+
   return "strict_string_checks=1"
          ":detect_stack_use_after_return=1"
-         ":check_initialization_order=1"
          ":detect_container_overflow=1"
          ":alloc_dealloc_mismatch=0"
          DETECT_LEAKS
