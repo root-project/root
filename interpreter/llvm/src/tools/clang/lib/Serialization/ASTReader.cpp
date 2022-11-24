23bc250948dca018fed6885e441d8d6b597479f4
Vassil Vassilev <v.g.vassilev@gmail.com>
2022-11-24 22:48:49 +0000

    Bring back root-project/root@fdc3fb382d5

--- interpreter/llvm/src/tools/clang/lib/Serialization/ASTReader.cpp
+++ interpreter/llvm/src/tools/clang/lib/Serialization/ASTReader.cpp
@@ -4017,9 +4017,13 @@ ASTReader::ReadModuleMapFileBlock(RecordData &Record, ModuleFile &F,
 
     assert(M && M->Name == F.ModuleName && "found module with different name");
 
+    // Check any additional module map files (e.g. module.private.modulemap)
+    // that are not in the pcm.
+    bool DisableValidation = PP.getPreprocessorOpts().DisablePCHOrModuleValidation !=
+                                  DisableValidationForModuleKind::None;
     // Check the primary module map file.
     auto StoredModMap = FileMgr.getFile(F.ModuleMapPath);
-    if (!StoredModMap || *StoredModMap != ModMap) {
+    if (!DisableValidation && (!StoredModMap || *StoredModMap != ModMap)) {
       assert(ModMap && "found module is missing module map file");
       assert((ImportedBy || F.Kind == MK_ImplicitModule) &&
              "top-level import should be verified");
@@ -4044,11 +4048,7 @@ ASTReader::ReadModuleMapFileBlock(RecordData &Record, ModuleFile &F,
       AdditionalStoredMaps.insert(*SF);
     }
 
-    // Check any additional module map files (e.g. module.private.modulemap)
-    // that are not in the pcm.
-    bool DisableValidation = bool(PP.getPreprocessorOpts().DisablePCHOrModuleValidation &
-                                  DisableValidationForModuleKind::All);
-    if (DisableValidation) {
+    if (!DisableValidation) {
     if (auto *AdditionalModuleMaps = Map.getAdditionalModuleMapFiles(M)) {
       for (const FileEntry *ModMap : *AdditionalModuleMaps) {
         // Remove files that match
