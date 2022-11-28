42ec0a5ef502181d3c690d13238732ef4c3ae6a2
Jakob Blomer <jblomer@cern.ch>
2022-11-28 23:20:51 +0100

    [ntuple] Add first unit test for RNTupleImporter

--- tree/ntupleutil/v7/test/ntuple_importer.cxx
+++ tree/ntupleutil/v7/test/ntuple_importer.cxx
@@ -1,10 +1,47 @@
 #include "gtest/gtest.h"
 
+#include <ROOT/RNTuple.hxx>
 #include <ROOT/RNTupleImporter.hxx>
 
-TEST(RNTupleImporter, Basics)
+#include <TFile.h>
+#include <TTree.h>
+
+#include <cstdio>
+#include <string>
+
+using ROOT::Experimental::RNTupleImporter;
+using ROOT::Experimental::RNTupleReader;
+
+/**
+ * An RAII wrapper around an open temporary file on disk. It cleans up the guarded file when the wrapper object
+ * goes out of scope.
+ */
+class FileRaii {
+private:
+   std::string fPath;
+
+public:
+   explicit FileRaii(const std::string &path) : fPath(path) {}
+   FileRaii(const FileRaii &) = delete;
+   FileRaii &operator=(const FileRaii &) = delete;
+   ~FileRaii() { std::remove(fPath.c_str()); }
+   std::string GetPath() const { return fPath; }
+};
+
+TEST(RNTupleImporter, Empty)
 {
-   auto importer =
-      ROOT::Experimental::RNTupleImporter::Create("/data/B2HHH~zstd.root", "DecayTree", "test.root").Unwrap();
+   FileRaii fileGuard("test_ntuple_importer_empty.root");
+   {
+      std::unique_ptr<TFile> file(TFile::Open(fileGuard.GetPath().c_str(), "RECREATE"));
+      auto tree = std::make_unique<TTree>("tree", "");
+      tree->Write();
+   }
+
+   auto importer = RNTupleImporter::Create(fileGuard.GetPath(), "tree", fileGuard.GetPath()).Unwrap();
+   importer->SetIsQuiet(true);
+   EXPECT_THROW(importer->Import(), ROOT::Experimental::RException);
+   importer->SetNTupleName("ntuple");
    importer->Import();
+   auto reader = RNTupleReader::Open("ntuple", fileGuard.GetPath());
+   EXPECT_EQ(0U, reader->GetNEntries());
 }
