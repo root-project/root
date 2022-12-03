f5fe186d41664b2979372a98b754debc44307de5
Jakob Blomer <jblomer@cern.ch>
2022-12-03 21:18:18 +0100

    [ntuple] add importer tutorial

---
+++ tutorials/v7/ntuple/ntpl008_import.C
@@ -0,0 +1,59 @@
+/// \file
+/// \ingroup tutorial_ntuple
+/// \notebook
+/// Example of converting data stored in a TTree into an RNTuple
+///
+/// \macro_image
+/// \macro_code
+///
+/// \date December 2022
+/// \author The ROOT Team
+
+// NOTE: The RNTuple classes are experimental at this point.
+// Functionality, interface, and data format is still subject to changes.
+// Do not use for real data!
+
+// Until C++ runtime modules are universally used, we explicitly load the ntuple library.  Otherwise
+// triggering autoloading from the use of templated types would require an exhaustive enumeration
+// of "all" template instances in the LinkDef file.
+R__LOAD_LIBRARY(ROOTNTupleUtil)
+
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleImporter.hxx>
+
+#include <TFile.h>
+#include <TROOT.h>
+
+// Import classes from experimental namespace for the time being
+using RNTuple = ROOT::Experimental::RNTuple;
+using RNTupleImporter = ROOT::Experimental::RNTupleImporter;
+using RNTupleReader = ROOT::Experimental::RNTupleReader;
+
+// Where to store the ntuple of this example
+constexpr char const *kTreeFileName = "http://root.cern.ch/files/HiggsTauTauReduced/GluGluToHToTauTau.root";
+constexpr char const *kTreeName = "Events";
+constexpr char const *kNTupleFileName = "ntpl008_import.root";
+
+void ntpl008_import()
+{
+    // Use multiple threads to compress RNTuple data
+    ROOT::EnableImplicitMT();
+
+    // RNTupleImporter::Create() returns a wrapper for an RNTupleImporter. If the creation fails,
+    // the result will contain an exception. Calling Unwrap() returns the actual RNTupleImporter
+    // class or throws the exception in case of errors.
+    auto importer = RNTupleImporter::Create(kTreeFileName, kTreeName, kNTupleFileName).Unwrap();
+
+    // Using `ThrowOnError()` makes sure that the macro abort if the import fails.
+    importer->Import().ThrowOnError();
+
+    // Inspect the schema of the written RNTuple
+    auto file = std::unique_ptr<TFile>(TFile::Open(kNTupleFileName));
+    if (!file || file->IsZombie()) {
+        std::cerr << "cannot open " << kNTupleFileName << std::endl;
+        return;
+    }
+    auto ntpl = file->Get<RNTuple>("Events");
+    auto reader = RNTupleReader::Open(ntpl);
+    reader->PrintInfo();
+}
