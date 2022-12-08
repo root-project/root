72838f9a21121e586de30a754f4ff03a79ad5bb9
will buttinger <will@cern.ch>
2022-12-08 11:00:54 +0000

    fix linkdef

--- roofit/xroofit/inc/LinkDef.h
+++ roofit/xroofit/inc/LinkDef.h
@@ -4,9 +4,9 @@
 #pragma link off all classes;
 
 // Can't seem to use the XROOFIT_NAMESPACE definition in the following lines :-(
-#pragma link C++ class RooFit::xRooFit::xRooNode+;
-#pragma link C++ class RooFit::xRooFit::xRooNode::InteractiveObject+;
-#pragma link C++ class RooFit::xRooFit::xRooBrowser+;
+#pragma link C++ class RooFit::XRooFit::xRooNode+;
+#pragma link C++ class RooFit::XRooFit::xRooNode::InteractiveObject+;
+#pragma link C++ class RooFit::XRooFit::xRooBrowser+;
 #pragma link C++ class RooBrowser+;
 
 #endif
