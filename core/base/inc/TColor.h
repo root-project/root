3e5f1f48c46d63659228e46aea249a688b8e16af
alexandertuna <alexander.tuna@gmail.com>
2022-12-09 08:28:20 +0100

    Update TColor enum since rainbow is one word (#11850)

--- core/base/inc/TColor.h
+++ core/base/inc/TColor.h
@@ -125,6 +125,7 @@ public:
                        kStarryNight=102,     kSunset=103,      kTemperatureMap=104,
                        kThermometer=105,     kValentine=106,   kVisibleSpectrum=107,
                        kWaterMelon=108,      kCool=109,        kCopper=110,
-                       kGistEarth=111,       kViridis=112,     kCividis=113};
+                       kGistEarth=111,       kViridis=112,     kCividis=113,
+                       kRainbow=kRainBow,    kDarkRainbow=kDarkRainBow};
 #endif
 
