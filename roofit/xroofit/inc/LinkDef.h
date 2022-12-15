a0c8ba31fbea538bb144984577e26e7910bcf3a5
will buttinger <will@cern.ch>
2022-12-15 11:59:57 +0000

    library and namespace name changes

--- roofit/xroofit/inc/LinkDef.h
+++ roofit/xroofit/inc/LinkDef.h
@@ -16,8 +16,8 @@
 #pragma link off all classes;
 
 // Can't seem to use the XROOFIT_NAMESPACE definition in the following lines :-(
-#pragma link C++ class RooFit::XRooFit::xRooNode+;
-#pragma link C++ class RooFit::XRooFit::xRooNode::InteractiveObject+;
-#pragma link C++ class RooFit::XRooFit::xRooBrowser+;
+#pragma link C++ class RooFit::Detail::XRooFit::xRooNode+;
+#pragma link C++ class RooFit::Detail::XRooFit::xRooNode::InteractiveObject+;
+#pragma link C++ class RooFit::Detail::XRooFit::xRooBrowser+;
 
 #endif
