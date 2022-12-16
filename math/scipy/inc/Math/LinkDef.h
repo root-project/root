8fe3e78f3f0460e2771e76a5d16c9f0f7546c724
Omar Zapata <Omar.Zapata@cern.ch>
2022-12-16 03:32:20 -0500

    Math: working in the initial prototype for Scipy optimizers

---
+++ math/scipy/inc/Math/LinkDef.h
@@ -0,0 +1,11 @@
+// @(#)root/math/ipopt:$Id$
+// Authors: Omar.Zapata@cern.ch  12/2022
+
+#ifdef __CINT__
+
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class ROOT::Math::Experimental::ScipyMinimizer;
+
+#endif //__CINT__
