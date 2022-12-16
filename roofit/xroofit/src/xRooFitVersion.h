4d1e2dea08a5673e0eca816fd3a2334ec5653bee
will buttinger <will@cern.ch>
2022-12-16 15:20:14 +0000

    add documentation and update version number

--- roofit/xroofit/src/xRooFitVersion.h
+++ roofit/xroofit/src/xRooFitVersion.h
@@ -1,4 +1,16 @@
+/*
+* Project: xRooFit
+* Author:
+*   Will Buttinger, RAL 2022
+*
+* Copyright (c) 2022, CERN
+*
+* Redistribution and use in source and binary forms,
+* with or without modification, are permitted according to the terms
+* listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
+
 #pragma once
 
-#define GIT_COMMIT_HASH "35b2463"
-#define GIT_COMMIT_DATE "2022-12-08 10:48:49 +0000"
+#define GIT_COMMIT_HASH "73512cf"
+#define GIT_COMMIT_DATE "2022-12-16 15:18:37 +0000"
