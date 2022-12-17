ca84e23814cd8179afb95dd87cf8c772f7377a92
Jakob Blomer <jblomer@cern.ch>
2022-12-17 16:12:18 +0100

    [ntuple] unit test importing complex classes/collections

--- tree/ntupleutil/v7/test/CustomStructUtilLinkDef.h
+++ tree/ntupleutil/v7/test/CustomStructUtilLinkDef.h
@@ -4,6 +4,10 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class BaseUtil + ;
 #pragma link C++ class CustomStructUtil + ;
+#pragma link C++ class HitUtil + ;
+#pragma link C++ class TrackUtil + ;
+#pragma link C++ class ComplexStructUtil + ;
 
 #endif
