b907675bfb6c305b56aea2b2d978b57f49298428
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2022-12-21 16:37:26 +0100

    [RF] Remove deprecated RooFit parts that are marked for removal in 6.30

--- roofit/roofitcore/inc/RooMinuit.h
+++
@@ -1,5 +0,0 @@
-#warning "The deprecated RooMinuit class was replaced by the more general RooMinimizer. \
-The RooMinuit class will be gone in ROOT 6.30! \
-Migrating to the RooMinimizer is easy because the interface is backwards compatible.
-
-#include "RooFitLegacy/RooMinuit.h"
