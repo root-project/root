eb9ab0613a134de00d8a656bdb8b3f207fae9904
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2023-01-09 22:59:55 +0100

    [DF] Replace deprecated call to `ROOT::RDF::MakeCsvDataFrame()`

--- root/dataframe/test_progressiveCSV.cxx
+++ root/dataframe/test_progressiveCSV.cxx
@@ -10,13 +10,13 @@ int test_progressiveCSV()
 
    // Create a CSV data source that reads in chunks of 2000 lines
    const auto chunkSize = 2000LL;
-   auto rdf = ROOT::RDF::MakeCsvDataFrame(fileName, true, ',', chunkSize);
+   auto rdf = ROOT::RDF::FromCSV(fileName, true, ',', chunkSize);
 
    auto rdflines = *rdf.Count();
    std::cout << "Total num lines: " << rdflines << std::endl;
 
    // Now create a CSV data source that reads the entire file into memory at once
-   auto rdf2 = ROOT::RDF::MakeCsvDataFrame(fileName);
+   auto rdf2 = ROOT::RDF::FromCSV(fileName);
 
    rdflines = *rdf2.Count();
    std::cout << "Total num lines: " << rdflines << std::endl;
