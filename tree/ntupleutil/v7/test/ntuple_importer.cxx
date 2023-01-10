76d6298a11b50a98e421e2fa9574f4eaddebe013
Florine de Geus <git@fwdg.nl>
2023-01-10 14:01:04 +0100

    [ntuple] Move shared RNTupleUtil test code to header

--- tree/ntupleutil/v7/test/ntuple_importer.cxx
+++ tree/ntupleutil/v7/test/ntuple_importer.cxx
@@ -1,42 +1,4 @@
-#include "gtest/gtest.h"
-
-#include <ROOT/RNTuple.hxx>
-#include <ROOT/RNTupleImporter.hxx>
-
-#include <TFile.h>
-#include <TTree.h>
-
-#include <cstdio>
-#include <string>
-#include <tuple>
-#include <vector>
-#include <utility>
-
-#include "CustomStructUtil.hxx"
-
-using ROOT::Experimental::RNTupleImporter;
-using ROOT::Experimental::RNTupleReader;
-
-/**
- * An RAII wrapper around an open temporary file on disk. It cleans up the guarded file when the wrapper object
- * goes out of scope.
- */
-class FileRaii {
-private:
-   static constexpr bool kDebug = false; // if true, don't delete the file on destruction
-   std::string fPath;
-
-public:
-   explicit FileRaii(const std::string &path) : fPath(path) {}
-   FileRaii(const FileRaii &) = delete;
-   FileRaii &operator=(const FileRaii &) = delete;
-   ~FileRaii()
-   {
-      if (!kDebug)
-         std::remove(fPath.c_str());
-   }
-   std::string GetPath() const { return fPath; }
-};
+#include "ntupleutil_test.hxx"
 
 TEST(RNTupleImporter, Empty)
 {
--- tree/ntupleutil/v7/test/ntuple_inspector.cxx
+++ tree/ntupleutil/v7/test/ntuple_inspector.cxx
@@ -1,46 +1,4 @@
-#include "gtest/gtest.h"
-
-#include <ROOT/RNTuple.hxx>
-#include <ROOT/RNTupleInspector.hxx>
-#include <ROOT/RNTupleModel.hxx>
-#include <ROOT/RNTupleOptions.hxx>
-
-#include <TFile.h>
-
-#include <cstdio>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
-
-#include "CustomStructUtil.hxx"
-
-using ROOT::Experimental::RNTupleInspector;
-using ROOT::Experimental::RNTupleModel;
-using ROOT::Experimental::RNTupleReader;
-using ROOT::Experimental::RNTupleWriteOptions;
-using ROOT::Experimental::RNTupleWriter;
-
-/**
- * An RAII wrapper around an open temporary file on disk. It cleans up the
- * guarded file when the wrapper object goes out of scope.
- */
-class FileRaii {
-private:
-   static constexpr bool kDebug = false; // if true, don't delete the file on destruction
-   std::string fPath;
-
-public:
-   explicit FileRaii(const std::string &path) : fPath(path) {}
-   FileRaii(const FileRaii &) = delete;
-   FileRaii &operator=(const FileRaii &) = delete;
-   ~FileRaii()
-   {
-      if (!kDebug)
-         std::remove(fPath.c_str());
-   }
-   std::string GetPath() const { return fPath; }
-};
+#include "ntupleutil_test.hxx"
 
 TEST(RNTupleInspector, Name)
 {
