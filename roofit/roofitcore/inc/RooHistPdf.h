9b3411c9a36705e453a7d516160112f88d914717
will buttinger <will@cern.ch>
2023-01-13 16:21:40 +0000

    add cloneAndOwnDataHist method for cloning (if necessary) the underlying dataHist in order to take ownership of it

--- roofit/roofitcore/inc/RooHistPdf.h
+++ roofit/roofitcore/inc/RooHistPdf.h
@@ -49,6 +49,15 @@ public:
     return *_dataHist ;
   }
 
+  /// Replaces underlying RooDataHist with a clone, which is now owned, and returns the clone.
+  /// If the underlying RooDataHist is already owned, then that is returned instead of being cloned.
+  RooDataHist* cloneAndOwnDataHist(const char* newname="") {
+     if (_ownedDataHist) return _ownedDataHist.get();
+     _ownedDataHist.reset(static_cast<RooDataHist*>(_dataHist->Clone(newname)));
+     _dataHist = _ownedDataHist.get();
+     return _dataHist;
+  }
+
   void setInterpolationOrder(Int_t order) {
     // Set histogram interpolation order
     _intOrder = order ;
