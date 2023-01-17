85822f74185aa2fabfa51fcf2e51bfb5e2f77ed5
ferdymercury <ferdymercury@users.noreply.github.com>
2023-01-17 08:36:36 +0100

    TGeoVolume class is designed for Int_t, not Long64_t (#12028)
    * whole class is designed for Int_t, not Long64_t
    
    prevent overflow with an error message
    
    * Apply agheata suggestion
    
    To catch more overflow cases

--- geom/geom/src/TGeoVoxelFinder.cxx
+++ geom/geom/src/TGeoVoxelFinder.cxx
@@ -1687,6 +1687,12 @@ void TGeoVoxelFinder::SortAll(Option_t *)
       boundaries[2*id+4*nd] = fBoxes[6*id+5]-fBoxes[6*id+2];
       boundaries[2*id+4*nd+1] = fBoxes[6*id+5]+fBoxes[6*id+2];
    }
+   auto prod = nmaxslices*nperslice;
+   if (nmaxslices != prod / nperslice) {
+      Error("SortAll", "Data type (Int_t) overflow for volume %s", fVolume->GetName());
+      return;
+   }
+   
    Int_t *index = new Int_t[2*nd]; // indexes for sorted boundaries on one axis
    UChar_t *ind = new UChar_t[nmaxslices*nperslice]; // ind[fOBx[i]] = ndghts in slice fInd[i]--fInd[i+1]
    Int_t *extra = new Int_t[nmaxslices*4];
