254ecf8e1b9a75c29ab1738a45126df030eb4451
Philippe Canal <pcanal@fnal.gov>
2023-01-19 13:18:07 -0600

    [NFC] Extend TDirectoryAtomicAdapter comments

--- core/base/inc/TDirectory.h
+++ core/base/inc/TDirectory.h
@@ -311,12 +311,29 @@ public:
 
 namespace ROOT {
 namespace Internal {
+   // The objects of type TDirectoryAtomicAdapter should only be used inside the
+   // thread that created them.  The intent is for this to be used (indirectly) solely as
+   // temporary objects.  For example:
+   //
+   // gDirectory = newvalue;
+   // gDirectory->ls();
+   // TDirectory *current = gDirectory;
+   //
+   // Note that:
+   //   auto dir = gDirectory;
+   // currently behaves "unexpectedly" as changing dir will change gDirectory:
+   //   dir = newvalue;
+   // leads to
+   //   gDirectory == newvalue
+   // To prevent this we would need a new mechanism such that the type
+   // used by 'auto' in that case is `TDirectory*` rather than the Internal
+   // type TDirectoryAtomicAdapter.
    struct TDirectoryAtomicAdapter {
+      // The shared pointer's lifetime is that of the thread creating this object
+      // (with the default constructor)
       TDirectory::SharedGDirectory_t &fValue;
 
       TDirectoryAtomicAdapter() : fValue(TDirectory::GetSharedLocalCurrentDirectory()) {}
-
-      TDirectoryAtomicAdapter(TDirectory::SharedGDirectory_t &value) : fValue(value) {}
 
       template <typename T>
       explicit operator T*() const {
