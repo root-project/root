f6e1ec4e56b97e37adf358b5a401c10867da7051
Florine <git@fwdg.nl>
2023-01-20 17:28:15 +0100

    [ntuple] Show all fields unless a model is specified (#12026)
    * [ntuple] Add option to show all fields unless a model is specified
    
    * [ntuple] Generate model unless already present for kCurrentModelJSON
    
    * [ntuple] Don't clang-format expected output
    
    * [ntuple] Remove unnecessary model generation trigger

--- tree/ntuple/v7/test/ntuple_show.cxx
+++ tree/ntuple/v7/test/ntuple_show.cxx
@@ -26,7 +26,6 @@ TEST(RNTupleShow, Empty)
    EXPECT_EQ(fString1, os1.str());
 }
 
-
 TEST(RNTupleShow, BasicTypes)
 {
    std::string rootFileName{"test_ntuple_show_basictypes.root"};
@@ -69,6 +68,7 @@ TEST(RNTupleShow, BasicTypes)
 
    std::ostringstream os;
    ntuple2->Show(0, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, os);
+   // clang-format off
    std::string fString{ std::string("")
       + "{\n"
       + "  \"pt\": 5,\n"
@@ -80,10 +80,12 @@ TEST(RNTupleShow, BasicTypes)
       + "  \"boolean\": true,\n"
       + "  \"uint8\": 97\n"
       + "}\n" };
+   // clang-format on
    EXPECT_EQ(fString, os.str());
 
    std::ostringstream os1;
    ntuple2->Show(1, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, os1);
+   // clang-format off
    std::string fString1{ std::string("")
       + "{\n"
       + "  \"pt\": 8.5,\n"
@@ -95,6 +97,7 @@ TEST(RNTupleShow, BasicTypes)
       + "  \"boolean\": false,\n"
       + "  \"uint8\": 98\n"
       + "}\n" };
+   // clang-format on
    EXPECT_EQ(fString1, os1.str());
 
    // TODO(jblomer): this should fail to an exception instead
@@ -115,7 +118,8 @@ TEST(RNTupleShow, Vectors)
 
       *fieldIntVec = std::vector<int>{4, 5, 6};
       *fieldFloatVecVec = std::vector<std::vector<float>>{std::vector<float>{0.1, 0.2}, std::vector<float>{1.1, 1.2}};
-      *fieldBoolVecVec = std::vector<std::vector<bool>>{std::vector<bool>{false, true, false}, std::vector<bool>{false, true}, std::vector<bool>{true, false, false }};
+      *fieldBoolVecVec = std::vector<std::vector<bool>>{
+         std::vector<bool>{false, true, false}, std::vector<bool>{false, true}, std::vector<bool>{true, false, false}};
       ntuple->Fill();
 
       fieldIntVec->emplace_back(7);
@@ -131,22 +135,26 @@ TEST(RNTupleShow, Vectors)
 
    std::ostringstream os;
    ntuple2->Show(0, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os);
+   // clang-format off
    std::string fString{ std::string("")
       + "{\n"
       + "  \"intVec\": [4, 5, 6],\n"
       + "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2]],\n"
       + "  \"booleanVecVec\": [[false, true, false], [false, true], [true, false, false]]\n"
       + "}\n" };
+   // clang-format on
    EXPECT_EQ(fString, os.str());
 
    std::ostringstream os1;
    ntuple2->Show(1, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os1);
+   // clang-format off
    std::string fString1{ std::string("")
       + "{\n"
       + "  \"intVec\": [4, 5, 6, 7],\n"
       + "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2], [2.2, 2.3]],\n"
       + "  \"booleanVecVec\": [[false, true, false], [false, true], [true, false, false], [false, true]]\n"
       + "}\n" };
+   // clang-format on
    EXPECT_EQ(fString1, os1.str());
 }
 
@@ -167,18 +175,22 @@ TEST(RNTupleShow, Arrays)
 
       *Intarrayfield = {1, 3};
       *Floatarrayfield = {3.5f, 4.6f, 5.7f};
-      *Vecarrayfield = {std::vector<double>{1, 2}, std::vector<double>{4, 5}, std::vector<double>{7, 8, 9}, std::vector<double>{11} };
+      *Vecarrayfield = {std::vector<double>{1, 2}, std::vector<double>{4, 5}, std::vector<double>{7, 8, 9},
+                        std::vector<double>{11}};
       *StringArray = {"First", "Second"};
-      *arrayOfArray = { std::array<bool,2>{ true, false }, std::array<bool,2>{ false, true }, std::array<bool,2>{ false, false } };
-      *arrayVecfield = { std::array<float, 2>{ 0, 1 }, std::array<float, 2>{ 2, 3 }, std::array<float, 2>{ 4, 5 } };
+      *arrayOfArray = {std::array<bool, 2>{true, false}, std::array<bool, 2>{false, true},
+                       std::array<bool, 2>{false, false}};
+      *arrayVecfield = {std::array<float, 2>{0, 1}, std::array<float, 2>{2, 3}, std::array<float, 2>{4, 5}};
       ntuple->Fill();
 
       *Intarrayfield = {2, 5};
       *Floatarrayfield = {2.3f, 5.7f, 11.13f};
-      *Vecarrayfield = {std::vector<double>{17, 19}, std::vector<double>{23, 29}, std::vector<double>{31, 37, 41}, std::vector<double>{43} };
+      *Vecarrayfield = {std::vector<double>{17, 19}, std::vector<double>{23, 29}, std::vector<double>{31, 37, 41},
+                        std::vector<double>{43}};
       *StringArray = {"Third", "Fourth"};
-      *arrayOfArray = { std::array<bool,2>{ true, true }, std::array<bool,2>{ false, true }, std::array<bool,2>{ true, true } };
-      *arrayVecfield = { std::array<float, 2>{ 6, 7 }, std::array<float, 2>{ 8, 9 } };
+      *arrayOfArray = {std::array<bool, 2>{true, true}, std::array<bool, 2>{false, true},
+                       std::array<bool, 2>{true, true}};
+      *arrayVecfield = {std::array<float, 2>{6, 7}, std::array<float, 2>{8, 9}};
       ntuple->Fill();
    }
    auto model2 = RNTupleModel::Create();
@@ -192,6 +204,7 @@ TEST(RNTupleShow, Arrays)
 
    std::ostringstream os;
    ntuple2->Show(0, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os);
+   // clang-format off
    std::string fString{ std::string("")
       + "{\n"
       + "  \"IntArray\": [1, 3],\n"
@@ -201,10 +214,12 @@ TEST(RNTupleShow, Arrays)
       + "  \"ArrayOfArray\": [[true, false], [false, true], [false, false]],\n"
       + "  \"VecOfArray\": [[0, 1], [2, 3], [4, 5]]\n"
       + "}\n"};
+   // clang-format on
    EXPECT_EQ(fString, os.str());
 
    std::ostringstream os1;
    ntuple2->Show(1, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os1);
+   // clang-format off
    std::string fString1{ std::string("")
       + "{\n"
       + "  \"IntArray\": [2, 5],\n"
@@ -214,6 +229,7 @@ TEST(RNTupleShow, Arrays)
       + "  \"ArrayOfArray\": [[true, true], [false, true], [true, true]],\n"
       + "  \"VecOfArray\": [[6, 7], [8, 9]]\n"
       + "}\n"};
+   // clang-format on
    EXPECT_EQ(fString1, os1.str());
 }
 
@@ -230,16 +246,21 @@ TEST(RNTupleShow, Objects)
       auto derivedAfield = model->MakeField<DerivedA>("DerivedA");
       auto ntuple = RNTupleWriter::Recreate(std::move(model), ntupleName, rootFileName);
 
-      *customStructfield = CustomStruct{4.1f, std::vector<float>{0.1f, 0.2f, 0.3f}, std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "Example1String"};
+      *customStructfield =
+         CustomStruct{4.1f, std::vector<float>{0.1f, 0.2f, 0.3f},
+                      std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "Example1String"};
       *customStructVec = {
-         CustomStruct{4.2f, std::vector<float>{0.1f, 0.2f, 0.3f}, std::vector<std::vector<float>>{{1.1f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "Example2String"},
-         CustomStruct{4.3f, std::vector<float>{0.1f, 0.2f, 0.3f}, std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.3f}}, "Example3String"},
-         CustomStruct{4.4f, std::vector<float>{0.1f, 0.3f}, std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "Example4String"}
-      };
+         CustomStruct{4.2f, std::vector<float>{0.1f, 0.2f, 0.3f},
+                      std::vector<std::vector<float>>{{1.1f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "Example2String"},
+         CustomStruct{4.3f, std::vector<float>{0.1f, 0.2f, 0.3f},
+                      std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.3f}}, "Example3String"},
+         CustomStruct{4.4f, std::vector<float>{0.1f, 0.3f},
+                      std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "Example4String"}};
       *customStructArray = {
-      CustomStruct{4.5f, std::vector<float>{0.1f, 0.2f, 0.3f}, std::vector<std::vector<float>>{{1.1f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "AnotherString1"},
-      CustomStruct{4.6f, std::vector<float>{0.1f, 0.2f, 0.3f}, std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.3f}}, "AnotherString2"}
-      };
+         CustomStruct{4.5f, std::vector<float>{0.1f, 0.2f, 0.3f},
+                      std::vector<std::vector<float>>{{1.1f, 1.3f}, {2.1f, 2.2f, 2.3f}}, "AnotherString1"},
+         CustomStruct{4.6f, std::vector<float>{0.1f, 0.2f, 0.3f},
+                      std::vector<std::vector<float>>{{1.1f, 1.2f, 1.3f}, {2.1f, 2.3f}}, "AnotherString2"}};
       *derivedAfield = {};
       ntuple->Fill();
    }
@@ -252,6 +273,7 @@ TEST(RNTupleShow, Objects)
 
    std::ostringstream os;
    ntuple2->Show(0, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os);
+   // clang-format off
    std::string fString{ std::string("")
       + "{\n"
       + "  \"CustomStruct\": {\n"
@@ -279,9 +301,9 @@ TEST(RNTupleShow, Objects)
       + "    \"a_s\": \"\"\n"
       + "  }\n"
       + "}\n" };
+   // clang-format on
    EXPECT_EQ(fString, os.str());
 }
-
 
 TEST(RNTupleShow, Collections)
 {
@@ -302,19 +324,22 @@ TEST(RNTupleShow, Collections)
       *float_field = 20.0;
       collection->Fill();
       ntuple->Fill();
-    }
+   }
 
    auto ntuple = RNTupleReader::Open(ntupleName, rootFileName);
    std::ostringstream osData;
    ntuple->Show(0, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, osData);
+   // clang-format off
    std::string outputData{ std::string("")
       + "{\n"
       + "  \"collection\": [{\"myInt\": 0, \"myFloat\": 10}, {\"myInt\": 1, \"myFloat\": 20}]\n"
       + "}\n" };
+   // clang-format on
    EXPECT_EQ(outputData, osData.str());
 
    std::ostringstream osFields;
    ntuple->PrintInfo(ROOT::Experimental::ENTupleInfo::kSummary, osFields);
+   // clang-format off
    std::string outputFields{ std::string("")
       + "************************************ NTUPLE ************************************\n"
       + "* N-Tuple : Collections                                                        *\n"
@@ -325,6 +350,7 @@ TEST(RNTupleShow, Collections)
       + "*     Field 1.1.1   : myInt (std::int32_t)                                     *\n"
       + "*     Field 1.1.2   : myFloat (float)                                          *\n"
       + "********************************************************************************\n" };
+   // clang-format on
    EXPECT_EQ(outputFields, osFields.str());
 }
 
@@ -358,18 +384,30 @@ TEST(RNTupleShow, RVec)
 
    std::ostringstream os;
    r->Show(0, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os);
-   std::string expected =
-      "{\n  \"intVec\": [1, 2, 3],\n  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2]],\n  \"customStructVec\": [{\"a\": 0, "
-      "\"v1\": [], \"v2\": [], \"s\": \"\"}, {\"a\": 1, \"v1\": [2, 3], \"v2\": [[4], [5]], \"s\": \"foo\"}]\n}\n";
-   EXPECT_EQ(os.str(), expected);
+   // clang-format off
+   std::string expected1{std::string("")
+      + "{\n"
+      + "  \"intVec\": [1, 2, 3],\n"
+      + "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2]],\n"
+      + "  \"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"},"
+      + " {\"a\": 1, \"v1\": [2, 3], \"v2\": [[4], [5]], \"s\": \"foo\"}]\n"
+      + "}\n"};
+   // clang-format on
+   EXPECT_EQ(os.str(), expected1);
 
    std::ostringstream os2;
    r->Show(1, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os2);
-   expected =
-      "{\n  \"intVec\": [1, 2, 3, 4],\n  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2], [2.1, 2.2]],\n  "
-      "\"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"}, {\"a\": 1, \"v1\": [2, 3], \"v2\": [[4], "
-      "[5]], \"s\": \"foo\"}, {\"a\": 6, \"v1\": [7, 8], \"v2\": [[9], [10]], \"s\": \"bar\"}]\n}\n";
-   EXPECT_EQ(os2.str(), expected);
+   // clang-format off
+   std::string expected2{std::string("")
+      + "{\n"
+      + "  \"intVec\": [1, 2, 3, 4],\n"
+      + "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2], [2.1, 2.2]],\n"
+      + "  \"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"},"
+      + " {\"a\": 1, \"v1\": [2, 3], \"v2\": [[4], [5]], \"s\": \"foo\"},"
+      + " {\"a\": 6, \"v1\": [7, 8], \"v2\": [[9], [10]], \"s\": \"bar\"}]\n"
+      + "}\n"};
+   // clang-format on
+   EXPECT_EQ(os2.str(), expected2);
 }
 
 TEST(RNTupleShow, RVecTypeErased)
@@ -413,21 +451,29 @@ TEST(RNTupleShow, RVecTypeErased)
 
    std::ostringstream os;
    ntuple->Show(0, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, os);
-   std::string fString{std::string("") + "{\n" + "  \"intVec\": [1, 2, 3],\n" +
-                       "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2]],\n" +
-                       "  \"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"}, {\"a\": 1, \"v1\": "
-                       "[2, 3], \"v2\": [[4], [5]], \"s\": \"foo\"}]\n" +
-                       "}\n"};
+   // clang-format off
+   std::string fString{std::string("")
+      + "{\n"
+      + "  \"intVec\": [1, 2, 3],\n"
+      + "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2]],\n"
+      + "  \"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"},"
+      + " {\"a\": 1, \"v1\": [2, 3], \"v2\": [[4], [5]], \"s\": \"foo\"}]\n"
+      + "}\n"};
+   // clang-format on
    EXPECT_EQ(fString, os.str());
 
    std::ostringstream os1;
    ntuple->Show(1, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, os1);
-   std::string fString1{
-      std::string("") + "{\n" + "  \"intVec\": [1, 2, 3, 4],\n" +
-      "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2], [2.1, 2.2]],\n" +
-      "  \"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"}, {\"a\": 1, \"v1\": [2, 3], \"v2\": "
-      "[[4], [5]], \"s\": \"foo\"}, {\"a\": 6, \"v1\": [7, 8], \"v2\": [[9], [10]], \"s\": \"bar\"}]\n" +
-      "}\n"};
+   // clang-format off
+   std::string fString1{std::string("")
+      + "{\n"
+      + "  \"intVec\": [1, 2, 3, 4],\n"
+      + "  \"floatVecVec\": [[0.1, 0.2], [1.1, 1.2], [2.1, 2.2]],\n"
+      + "  \"customStructVec\": [{\"a\": 0, \"v1\": [], \"v2\": [], \"s\": \"\"},"
+      + " {\"a\": 1, \"v1\": [2, 3], \"v2\": [[4], [5]], \"s\": \"foo\"},"
+      + " {\"a\": 6, \"v1\": [7, 8], \"v2\": [[9], [10]], \"s\": \"bar\"}]\n"
+      + "}\n"};
+   // clang-format off
    EXPECT_EQ(fString1, os1.str());
 }
 
@@ -459,7 +505,66 @@ TEST(RNTupleShow, CollectionProxy)
 
       std::ostringstream os;
       ntuple->Show(0, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, os);
-      std::string expected{"{\n  \"proxyF\": [42, 24],\n  \"vecProxyF\": [[1, 2], [1, 2]]\n}\n"};
+      // clang-format off
+      std::string expected{std::string("")
+         + "{\n"
+         + "  \"proxyF\": [42, 24],\n"
+         + "  \"vecProxyF\": [[1, 2], [1, 2]]\n"
+         + "}\n"};
+      // clang-format on
       EXPECT_EQ(os.str(), expected);
    }
+}
+
+TEST(RNTupleShow, CompleteOrCurrentModel)
+{
+   FileRaii fileGuard("test_ntuple_show_completeorcurrentmodel.root");
+   {
+      auto model = RNTupleModel::Create();
+      auto fldInt = model->MakeField<int>("int");
+      auto fldFloat = model->MakeField<float>("float");
+      auto ntuple = RNTupleWriter::Recreate(std::move(model), "f", fileGuard.GetPath());
+
+      *fldInt = 42;
+      *fldFloat = 3.14;
+      *fldFloat = ntuple->Fill();
+   }
+
+   auto ntuple1 = RNTupleReader::Open("f", fileGuard.GetPath());
+
+   auto model = RNTupleModel::Create();
+   auto fldInt = model->MakeField<int>("int");
+   auto ntuple2 = RNTupleReader::Open(std::move(model), "f", fileGuard.GetPath());
+
+   std::ostringstream os1;
+   ntuple1->Show(0, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os1);
+   // clang-format off
+   std::string expected1{std::string("")
+      + "{\n"
+      + "  \"int\": 42,\n"
+      + "  \"float\": 3.14\n"
+      + "}\n"};
+   // clang-format on
+   EXPECT_EQ(expected1, os1.str());
+
+   std::ostringstream os2;
+   ntuple2->Show(0, ROOT::Experimental::ENTupleShowFormat::kCurrentModelJSON, os2);
+   // clang-format off
+   std::string expected2{std::string("")
+      + "{\n"
+      + "  \"int\": 42\n"
+      + "}\n"};
+   // clang-format on
+   EXPECT_EQ(expected2, os2.str());
+
+   std::ostringstream os3;
+   ntuple2->Show(0, ROOT::Experimental::ENTupleShowFormat::kCompleteJSON, os3);
+   // clang-format off
+   std::string expected3{std::string("")
+      + "{\n"
+      + "  \"int\": 42,\n"
+      + "  \"float\": 3.14\n"
+      + "}\n"};
+   // clang-format on
+   EXPECT_EQ(expected3, os3.str());
 }
