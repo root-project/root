396267e12ceb1ee8f364342817c32fd44e080195
Olivier Couet <olivier.couet@cern.ch>
2023-01-23 17:18:47 +0100

    Better wording

--- core/base/inc/TROOT.h
+++ core/base/inc/TROOT.h
@@ -314,7 +314,7 @@ public:
    void              Reset(Option_t *option="");
    void              SaveContext();
    void              SetApplication(TApplication *app) { fApplication = app; }
-   /// Set the "Batch mode". "True" means the session is without interactive graphics
+   /// Set the "Batch mode".  If the argument evaluates to `true`, the session does not use interactive graphics.
    void              SetBatch(Bool_t batch = kTRUE) { fIsWebDisplayBatch = fBatch = batch; }
    void              SetWebDisplay(const char *webdisplay = "");
    void              SetCutClassName(const char *name = "TCutG");
