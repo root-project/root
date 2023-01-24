c0f0c94b05c882a45c1f3804a55a2ca66aa05bad
moneta <lorenzo.moneta@cern.ch>
2023-01-24 12:31:05 +0100

    [tmva] Fix code generation in RSofieReader
    Create a separate scope for the parsing code to avoid creating objects in global ROOT interprter space.
    Handle better also errors when jitting the code by throwing run-time exceptions
    Improve debug messages in verbose mode

--- tmva/tmva/inc/TMVA/RSofieReader.hxx
+++ tmva/tmva/inc/TMVA/RSofieReader.hxx
@@ -76,8 +76,7 @@ public:
          }
       }
       if (type == kNotDef) {
-         Error("RSofieReader","Input file is not an ONNX or Keras or PyTorch file");
-         return;
+         throw std::runtime_error("Input file is not an ONNX or Keras or PyTorch file");
       }
       if (pos1 == std::string::npos)
          pos1 = 0;
@@ -87,16 +86,16 @@ public:
       std::string fileType = path.substr(pos2+1, path.length()-pos2-1);
       if (verbose) std::cout << "Parsing SOFIE model " << modelName << " of type " << fileType << std::endl;
 
-
+      // create code for parsing model and generate C++ code for inference
+      // make it in a separate scope to avoid polluting global interpreter space
       std::string parserCode;
       if (type == kONNX) {
          // check first if we can load the SOFIE parser library
          if (gSystem->Load("libROOTTMVASofieParser") < 0) {
-            Error("RSofieReader","Cannot use SOFIE with ONNX since libROOTTMVASofieParser is missing");
-            return;
+            throw std::runtime_error("RSofieReader: cannot use SOFIE with ONNX since libROOTTMVASofieParser is missing");
          }
          gInterpreter->Declare("#include \"TMVA/RModelParser_ONNX.hxx\"");
-         parserCode += "TMVA::Experimental::SOFIE::RModelParser_ONNX parser; \n";
+         parserCode += "{\nTMVA::Experimental::SOFIE::RModelParser_ONNX parser ; \n";
          if (verbose == 2)
             parserCode += "TMVA::Experimental::SOFIE::RModel model = parser.Parse(\"" + path + "\",true); \n";
          else
@@ -105,20 +104,17 @@ public:
       else if (type == kKeras) {
          // use Keras direct parser
          if (gSystem->Load("libPyMVA") < 0) {
-            Error("RSofieReader","Cannot use SOFIE with Keras since libPyMVA is missing");
-            return;
+            throw std::runtime_error("RSofieReader: cannot use SOFIE with Keras since libPyMVA is missing");
          }
-         parserCode += "TMVA::Experimental::SOFIE::RModel model = TMVA::Experimental::SOFIE::PyKeras::Parse(\"" + path + "\"); \n";
+         parserCode += "{\nTMVA::Experimental::SOFIE::RModel model = TMVA::Experimental::SOFIE::PyKeras::Parse(\"" + path + "\"); \n";
       }
       else if (type == kPt) {
          // use PyTorch direct parser
          if (gSystem->Load("libPyMVA") < 0) {
-            Error("RSofieReader","Cannot use SOFIE with PyTorch since libPyMVA is missing");
-            return;
+            throw std::runtime_error("RSofieReader: cannot use SOFIE with PyTorch since libPyMVA is missing");
          }
          if (inputShape.size() == 0) {
-            Error("RSofieReader","Cannot use SOFIE with PyTorch since the input tensor shape is missing and is needed by the PyTorch parser");
-            return;
+            throw std::runtime_error("RSofieReader: cannot use SOFIE with PyTorch since the input tensor shape is missing and is needed by the PyTorch parser");
          }
          std::string inputShapeStr = "{";
          for (unsigned int i = 0; i < inputShape.size(); i++) {
@@ -131,12 +127,12 @@ public:
             if (i < inputShape.size()-1) inputShapeStr += ", ";
          }
          inputShapeStr += "}";
-         parserCode += "TMVA::Experimental::SOFIE::RModel model = TMVA::Experimental::SOFIE::PyTorch::Parse(\"" + path + "\", "
+         parserCode += "{\nTMVA::Experimental::SOFIE::RModel model = TMVA::Experimental::SOFIE::PyTorch::Parse(\"" + path + "\", "
                     + inputShapeStr + "); \n";
       }
       else if (type == kROOT) {
          // use  parser from ROOT
-         parserCode += "auto fileRead = TFile::Open(\"" + path + "\",\"READ\");\n";
+         parserCode += "{\nauto fileRead = TFile::Open(\"" + path + "\",\"READ\");\n";
          parserCode += "TMVA::Experimental::SOFIE::RModel * modelPtr;\n";
          parserCode += "auto keyList = fileRead->GetListOfKeys(); TString name;\n";
          parserCode += "for (const auto&& k : *keyList)  { \n";
@@ -145,38 +141,41 @@ public:
          parserCode += "TMVA::Experimental::SOFIE::RModel & model = *modelPtr;\n";
       }
 
-      if (verbose) std::cout << parserCode << std::endl;
-      gROOT->ProcessLine(parserCode.c_str());
-
       int batchSize = 1;
       if (inputShape.size() > 0 && inputShape[0].size() > 0) {
          batchSize = inputShape[0][0];
          if (batchSize < 1) batchSize = 1;
       }
       if (verbose) std::cout << "generating the code with batch size = " << batchSize << " ...\n";
-      parserCode = "model.Generate(TMVA::Experimental::SOFIE::Options::kDefault,"
+      parserCode += "model.Generate(TMVA::Experimental::SOFIE::Options::kDefault,"
                    + ROOT::Math::Util::ToString(batchSize) + "); \n";
-      if (verbose)
+      if (verbose > 1)
          parserCode += "model.PrintGenerated(); \n";
       parserCode += "model.OutputGenerated();\n";
 
-      if (verbose) std::cout << parserCode << std::endl;
+      //end of parsing code, close the scope and return 1 to indicate a success
+      parserCode += "return 1;\n }\n";
 
-      gROOT->ProcessLine(parserCode.c_str());
+      if (verbose) std::cout << "//ParserCode being executed:\n" << parserCode << std::endl;
 
-      if (verbose) std::cout << "compile generated code \n";
+      auto iret = gROOT->ProcessLine(parserCode.c_str());
+      if (iret != 1) {
+         std::string msg = "RSofieReader: error processing the parser code: \n" + parserCode;
+         throw std::runtime_error(msg);
+      }
 
       // compile now the generated code and create Session class
       std::string modelHeader = modelName + ".hxx";
+      if (verbose) std::cout << "compile generated code from file " <<modelHeader << std::endl;
       if (gSystem->AccessPathName(modelHeader.c_str())) {
-         //std::string msg = "input header file is not existing";
-         Error("RSofieReader","Input header file %s is not existing",modelHeader.c_str());
-         return;
+         std::string msg = "RSofieReader: input header file " + modelHeader + " is not existing";
+         throw std::runtime_error(msg);
       }
-      if (verbose) std::cout << "Parsing SOFIE model " << modelName << std::endl;
+      if (verbose) std::cout << "Creating Inference function for model " << modelName << std::endl;
       std::string declCode;
       declCode += "#pragma cling optimize(2)\n";
       declCode += "#include \"" + modelHeader + "\"\n";
+      // create global session instance: use UUID to have an unique name
       std::string sessionClassName = "TMVA_SOFIE_" + modelName + "::Session";
       TUUID uuid;
       std::string uidName = uuid.AsString();
@@ -186,11 +185,13 @@ public:
       std::string sessionName = "session_" + uidName;
       declCode += sessionClassName + " " + sessionName + ";";
 
-      if (verbose) std::cout << declCode << std::endl;
+      if (verbose) std::cout << "//global session declaration\n" << declCode << std::endl;
 
-      gInterpreter->Declare(declCode.c_str());
-      fInitialized = true;
-      TClass * sessionClass = TClass::GetClass(sessionClassName.c_str());
+      bool ret = gInterpreter->Declare(declCode.c_str());
+      if (!ret) {
+         std::string msg = "RSofieReader: error compiling inference code and creating session class\n" + declCode;
+         throw std::runtime_error(msg);
+      }
 
       fSessionPtr = (void*) gInterpreter->Calc(sessionName.c_str());
 
@@ -198,16 +199,21 @@ public:
       std::stringstream ifuncCode;
       std::string funcName = "SofieInference_" + uidName;
       ifuncCode << "std::vector<float> " + funcName + "( void * ptr, float * data) {\n";
-      ifuncCode << sessionClassName << " * s = " << "(" << sessionClassName << "*) (ptr);\n";
-      ifuncCode << "return s->infer(data);\n";
+      ifuncCode << "   " << sessionClassName << " * s = " << "(" << sessionClassName << "*) (ptr);\n";
+      ifuncCode << "   return s->infer(data);\n";
       ifuncCode << "}\n";
 
-      if (verbose) std::cout << ifuncCode.str() << std::endl;
+      if (verbose) std::cout << "//Inference function code using global session instance\n"
+                              << ifuncCode.str() << std::endl;
 
-      gInterpreter->Declare(ifuncCode.str().c_str());
+      ret = gInterpreter->Declare(ifuncCode.str().c_str());
+      if (!ret) {
+         std::string msg = "RSofieReader: error compiling inference function\n" + ifuncCode.str();
+         throw std::runtime_error(msg);
+      }
       auto fptr = gInterpreter->Calc(funcName.c_str());
       fFuncPtr = reinterpret_cast<std::vector<float> (*)(void *, const float *)>(fptr);
-      //fFuncPtr = fptr2;
+      fInitialized = true;
    }
 
    /// Compute model prediction on vector
