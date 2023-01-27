fe5a63ffbe912c65df87dab3d8f1fd2482f25d5f
Axel Naumann <Axel.Naumann@cern.ch>
2023-01-27 18:29:24 +0100

    [cmake] There is no CMAKE_EXE_FLAGS, use CMAKE_EXE_LINKER_FLAGS:
    This might fix e.g. `-mmacosx-version-min=12.6` not being passed to ACLiC, causing
    linker warnings about incompatible deployment targets.

--- cmake/modules/RootConfiguration.cmake
+++ cmake/modules/RootConfiguration.cmake
@@ -837,7 +837,7 @@ else()
   execute_process(COMMAND ${CMAKE_SOURCE_DIR}/build/unix/compiledata.sh
     ${CMAKE_BINARY_DIR}/ginclude/compiledata.h "${CMAKE_CXX_COMPILER}"
         "${CMAKE_CXX_FLAGS_RELEASE}" "${CMAKE_CXX_FLAGS_DEBUG}" "${CMAKE_CXX_FLAGS}"
-        "${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS}" "${CMAKE_EXE_FLAGS}" "so"
+        "${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS}" "${CMAKE_EXE_LINKER_FLAGS}" "so"
         "${libdir}" "-lCore" "-lRint" "${incdir}" "" "" "${ROOT_ARCHITECTURE}" "${ROOTBUILD}")
 endif()
 
