4ccf11b00969dea38bb3a2473551a42f42aa55a7
Ole Morud <82065181+olemorud@users.noreply.github.com>
2023-01-31 17:57:30 +0100

    [skip-ci] group log output in CI (#12188)
    * [ci] Add ROOT CI workflow
    
    * [CI] Group log lines

--- .github/workflows/rootci-installers/build_root.py
+++ .github/workflows/rootci-installers/build_root.py
@@ -115,6 +115,7 @@ def main():
 
     # Download and extract previous build artifacts
     if incremental:
+        print("::group::Download previous build artifacts")
         print("Attempting incremental build")
 
         try:
@@ -132,10 +133,15 @@ def main():
 
             incremental = False
 
+        print("::endgroup::")
 
+
+    print("Doing non-incremental build")
+
+    print("::group::Pull base branch")
     # Add remote on non incremental builds
     if not incremental:
-        print("Doing non-incremental build")
+        print("::group::Pull base branch")
 
         result, shell_log = subprocess_with_log(f"""
             git clone --branch {base_ref} --single-branch {repository} "{workdir}/src"
@@ -166,6 +172,7 @@ def main():
         git reset --hard @{{u}} || exit 4
     """, shell_log)
 
+    print("::endgroup::")
     if result not in (0, 123):
         die(result, f"Failed to pull {base_ref}", shell_log)
 
@@ -177,11 +184,15 @@ def main():
         if not re.match(release_branches, base_ref):
             warning("{base_ref} is not a release branch, skipping artifact upload")
         else:
+            print("::group::Upload base branch")
             try:
                 print(f"Archiving build artifacts of {base_ref}")
                 archive_and_upload(yyyy_mm_dd, workdir, connection, compressionlevel, s3_prefix)
             except Exception as err:
                 warning("failed to archive/upload artifacts: ", err)
+
+            print("::endgroup::")
+
         print(f"Successfully built branch {base_ref}")
 
     if not rebase:
@@ -192,20 +203,24 @@ def main():
     shell_log = rebase_and_build(base_ref, head_ref, buildtype, workdir, shell_log)
 
     try:
-        print("Archiving build artifacts to run tests in a new workflow")
         if "pull" in head_ref:
             name = "pr-" + head_ref.split('/')[-2]
         else:
             name = head_ref
         test_prefix = f'to-test/{name}/{platform}/{buildtype}/{option_hash}'
-        archive_and_upload(f"test{yyyy_mm_dd}", workdir, connection, compressionlevel, test_prefix)
+
+        archive_and_upload(f"{yyyy_mm_dd}", workdir, connection, compressionlevel, test_prefix)
     except Exception as err:
         warning("failed to archive/upload artifacts: ", err)
+
+
 
     print_shell_log(shell_log)
 
 
 def archive_and_upload(archive_name, workdir, connection, compressionlevel, prefix):
+    print("::group::Archive and upload changes to test in different workflow")
+
     new_archive = f"{archive_name}.tar.gz"
 
     with tarfile.open(f"{workdir}/{new_archive}", "x:gz", compresslevel=compressionlevel) as targz:
@@ -219,8 +234,13 @@ def archive_and_upload(archive_name, workdir, connection, compressionlevel, pref
         path=f"{workdir}/{new_archive}"
     )
 
+    print("::endgroup::")
+
 
 def build_base(base_ref, incremental, workdir, options, buildtype, shell_log) -> str:
+    print("::group::Build base branch")
+
+
     if not incremental:
         result, shell_log = subprocess_with_log(f"""
             mkdir -p '{workdir}/build'
@@ -238,13 +258,15 @@ def build_base(base_ref, incremental, workdir, options, buildtype, shell_log) ->
     if result != 0:
         die(result, f"Failed to build {base_ref}", shell_log)
 
+    print("::endgroup::")
+
     return shell_log
 
 
 def rebase_and_build(base_ref, head_ref, buildtype, workdir, shell_log) -> str:
     """rebases and builds head_ref on base_ref, returns shell log"""
 
-    print(f"Rebasing {head_ref} onto {base_ref}...")
+    print(f"::group::Rebase {head_ref} onto {base_ref}")
 
     result, shell_log = subprocess_with_log(f"""
         cd '{workdir}/src' || exit 1
@@ -261,7 +283,10 @@ def rebase_and_build(base_ref, head_ref, buildtype, workdir, shell_log) -> str:
     if result != 0:
         die(result, "Rebase failed", shell_log)
 
-    print("Building changes...")
+    print("::endgroup::")
+
+    print("::group::Build changes")
+
     result, shell_log = subprocess_with_log(f"""
         mkdir -p {workdir}/build
         cmake --build '{workdir}/build' --config '{buildtype}' --parallel '{os.cpu_count()}'
@@ -270,7 +295,7 @@ def rebase_and_build(base_ref, head_ref, buildtype, workdir, shell_log) -> str:
     if result != 0:
         die(result, "Build step after rebase failed", shell_log)
 
-    print(f"Rebase and build of {head_ref} onto {base_ref} successful!")
+    print("::endgroup::")
 
     return shell_log
 
