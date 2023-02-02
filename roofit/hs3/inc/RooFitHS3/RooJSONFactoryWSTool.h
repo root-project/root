dc2213bfe703f698882f0766e7bb79fd5b50c83c
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-02-02 01:20:41 +0100

    [RF] Decouple `RooDataHist` reading from rest of workspace in RooFitHS3
    RooDataHists are self-contained entities in RooFit, and writing and
    reading of their representation in RooFitHS3 should also not depend on
    the rest of the RooWorkspace context.
    
    This also makes the code easier to understand and work with.

--- roofit/hs3/inc/RooFitHS3/RooJSONFactoryWSTool.h
+++ roofit/hs3/inc/RooFitHS3/RooJSONFactoryWSTool.h
@@ -108,8 +108,8 @@ public:
                                const TH1 *errH = nullptr, bool writeObservables = true, bool writeErrors = true);
    static void writeObservables(const TH1 &h, RooFit::Detail::JSONNode &n, const std::vector<std::string> &varnames);
 
-   static std::unique_ptr<RooDataHist> readBinnedData(RooWorkspace &ws, const RooFit::Detail::JSONNode &n,
-                                                      const std::string &namecomp, RooArgList observables);
+   static std::unique_ptr<RooDataHist> readBinnedData(const RooFit::Detail::JSONNode &n, const std::string &namecomp);
+   static std::unique_ptr<RooDataHist> readBinnedData(const RooFit::Detail::JSONNode &n, const std::string &namecomp, RooArgList varlist);
 
    static void
    getObservables(RooWorkspace &ws, const RooFit::Detail::JSONNode &n, const std::string &obsnamecomp, RooArgSet &out);
