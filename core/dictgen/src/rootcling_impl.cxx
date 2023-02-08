6cd0f5c1a9b8bfa7d8d8696bd90cd15d7ad9ddf3
Vassil Vassilev <v.g.vassilev@gmail.com>
2023-02-08 00:10:22 +0000

    [rootcling] Remove unused flags.
    These flags are used mostly when we generate debug information which we don't as
    rootcling is a syntax-only action.

--- core/dictgen/src/rootcling_impl.cxx
+++ core/dictgen/src/rootcling_impl.cxx
@@ -4230,10 +4230,6 @@ int RootClingMain(int argc,
 #endif
    clingArgs.push_back("-Xclang");
    clingArgs.push_back("-fmodules-embed-all-files");
-   clingArgs.push_back("-Xclang");
-   clingArgs.push_back("-main-file-name");
-   clingArgs.push_back("-Xclang");
-   clingArgs.push_back((dictname + ".h").c_str());
 
    ROOT::TMetaUtils::SetPathsForRelocatability(clingArgs);
 
