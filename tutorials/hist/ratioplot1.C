0122c1f3b37824ab9cfcd1216427660581cf3790
Olivier Couet <olivier.couet@cern.ch>
2023-02-15 12:53:29 +0100

    Improve example (#12318)

--- tutorials/hist/ratioplot1.C
+++ tutorials/hist/ratioplot1.C
@@ -3,8 +3,9 @@
 /// \notebook
 /// Example creating a simple ratio plot of two histograms using the `pois` division option.
 /// Two histograms are set up and filled with random numbers. The constructor of `TRatioPlot`
-/// takes the to histograms, name and title for the object, drawing options for the histograms (`hist` and `E` in this case)
-/// and a drawing option for the output graph.
+/// takes the two histograms, name and title for the object, drawing options for the histograms
+/// (`hist` and `E` in this case) and a drawing option for the output graph.
+/// The histograms drawing options can be changed with `SetH1DrawOpt` and `SetH2DrawOpt`.
 ///
 /// \macro_image
 /// \macro_code
@@ -13,8 +14,8 @@
 
 void ratioplot1() {
    gStyle->SetOptStat(0);
-   auto c1 = new TCanvas("c1", "A ratio example");
-   auto h1 = new TH1D("h1", "h1", 50, 0, 10);
+   auto C  = new TCanvas("C", "A ratio example");
+   auto h1 = new TH1D("h1", "TRatioPlot Example; x; y", 50, 0, 10);
    auto h2 = new TH1D("h2", "h2", 50, 0, 10);
    auto f1 = new TF1("f1", "exp(- x/[0] )");
    f1->SetParameter(0, 3);
@@ -22,10 +23,18 @@ void ratioplot1() {
    h2->FillRandom("f1", 2000);
    h1->Sumw2();
    h2->Scale(1.9 / 2.);
-   h1->GetXaxis()->SetTitle("x");
-   h1->GetYaxis()->SetTitle("y");
+   h2->SetLineColor(kRed);
+
+   // Create and draw the ratio plot
    auto rp = new TRatioPlot(h1, h2);
-   c1->SetTicks(0, 1);
+   C->SetTicks(0, 1);
    rp->Draw();
    rp->GetLowYaxis()->SetNdivisions(505);
+
+   // Add a legend to the ratio plot
+   rp->GetUpperPad()->cd();
+   TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
+   legend->AddEntry("h1","First histogram","l");
+   legend->AddEntry("h2","Second histogram","le");
+   legend->Draw();
 }
