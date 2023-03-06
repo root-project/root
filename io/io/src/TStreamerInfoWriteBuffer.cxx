05c88f24f86ec27a96d2dffbc61aa5f5f6940b97
Philippe Canal <pcanal@fnal.gov>
2023-03-06 15:59:46 -0600

    Do not record kIsOnHeap nor kNotDeleted
    When reading back the "heap status" is determine by how the
    read-into object was allocated, so there is no need to record
    that information.
    
    When reading back, the kNotDeleted is always on, (and should
    always be on when writing :) ), so there is no need to record it.
    
    Recording '0' for both allow for (slightly) better compression.
    
    Fix #12438

--- io/io/src/TStreamerInfoWriteBuffer.cxx
+++ io/io/src/TStreamerInfoWriteBuffer.cxx
@@ -404,7 +404,7 @@ Int_t TStreamerInfo::WriteBufferAux(TBuffer &b, const T &arr,
 
          // special case for TObject::fBits in case of a referenced object
          case TStreamerInfo::kBits: { DOLOOP {
-            UInt_t *x=(UInt_t*)(arr[k]+ioffset); b << *x;
+            UInt_t *x=(UInt_t*)(arr[k]+ioffset); b << (*x & (~kIsOnHeap & ~kNotDeleted));
             if ((*x & kIsReferenced) != 0) {
                TObject *obj = (TObject*)(arr[k]+eoffset);
                TProcessID *pid = TProcessID::GetProcessWithUID(obj->GetUniqueID(),obj);
