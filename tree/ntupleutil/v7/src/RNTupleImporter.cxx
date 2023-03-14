4d8fda1bf13d444f58e38d2d64bed0cdae02e7bb
Florine de Geus <florine.willemijn.de.geus@cern.ch>
2023-03-14 17:09:57 +0100

    [ntuple] Remove unnecessary pointer to source tree

--- tree/ntupleutil/v7/src/RNTupleImporter.cxx
+++ tree/ntupleutil/v7/src/RNTupleImporter.cxx
@@ -90,14 +90,14 @@ ROOT::Experimental::RNTupleImporter::Create(std::string_view sourceFileName, std
    if (!importer->fSourceFile || importer->fSourceFile->IsZombie()) {
       return R__FAIL("cannot open source file " + std::string(sourceFileName));
    }
-   importer->fSourceTree = std::unique_ptr<TTree>(importer->fSourceFile->Get<TTree>(std::string(treeName).c_str()));
+
+   importer->fSourceTree = importer->fSourceFile->Get<TTree>(std::string(treeName).c_str());
    if (!importer->fSourceTree) {
       return R__FAIL("cannot read TTree " + std::string(treeName) + " from " + std::string(sourceFileName));
    }
-   importer->fSourceTreePtr = importer->fSourceTree.get();
-   // If we have IMT enabled, its best use is for parallel page compression
-   importer->fSourceTreePtr->SetImplicitMT(false);
 
+   // If we have IMT enabled, its best use is for parallel page compression
+   importer->fSourceTree->SetImplicitMT(false);
    importer = InitDestination(std::move(importer), destFileName).Unwrap();
 
    return importer;
@@ -108,11 +108,10 @@ ROOT::Experimental::RNTupleImporter::Create(TTree *sourceTree, std::string_view 
 {
    auto importer = std::unique_ptr<RNTupleImporter>(new RNTupleImporter());
    importer->fNTupleName = sourceTree->GetName();
-   importer->fSourceTreePtr = sourceTree;
+   importer->fSourceTree = sourceTree;
 
    // If we have IMT enabled, its best use is for parallel page compression
-   importer->fSourceTreePtr->SetImplicitMT(false);
-
+   importer->fSourceTree->SetImplicitMT(false);
    importer = InitDestination(std::move(importer), destFileName).Unwrap();
 
    return importer;
@@ -156,7 +155,7 @@ ROOT::Experimental::RResult<void> ROOT::Experimental::RNTupleImporter::PrepareSc
    // Browse through all branches and their leaves, create corresponding fields and prepare the memory buffers for
    // reading and writing. Usually, reading and writing share the same memory buffer, i.e. the object is read from TTree
    // and written as-is to the RNTuple. There are exceptions, e.g. for leaf count arrays and C strings.
-   for (auto b : TRangeDynCast<TBranch>(*fSourceTreePtr->GetListOfBranches())) {
+   for (auto b : TRangeDynCast<TBranch>(*fSourceTree->GetListOfBranches())) {
       assert(b);
       const auto firstLeaf = static_cast<TLeaf *>(b->GetListOfLeaves()->First());
       assert(firstLeaf);
@@ -184,7 +183,7 @@ ROOT::Experimental::RResult<void> ROOT::Experimental::RNTupleImporter::PrepareSc
          c.fMaxLength = firstLeaf->GetMaximum();
          c.fCountVal = std::make_unique<Int_t>(); // count leafs are integers
          // Casting to void * makes it work for both Int_t and UInt_t
-         fSourceTreePtr->SetBranchAddress(b->GetName(), static_cast<void *>(c.fCountVal.get()));
+         fSourceTree->SetBranchAddress(b->GetName(), static_cast<void *>(c.fCountVal.get()));
          fLeafCountCollections.emplace(firstLeaf->GetName(), std::move(c));
          continue;
       }
@@ -280,9 +279,9 @@ ROOT::Experimental::RResult<void> ROOT::Experimental::RNTupleImporter::PrepareSc
                            std::string(b->GetName()));
          }
          auto ptrBuf = reinterpret_cast<void **>(ib.fBranchBuffer.get());
-         fSourceTreePtr->SetBranchAddress(b->GetName(), ptrBuf, klass, EDataType::kOther_t, true /* isptr*/);
+         fSourceTree->SetBranchAddress(b->GetName(), ptrBuf, klass, EDataType::kOther_t, true /* isptr*/);
       } else {
-         fSourceTreePtr->SetBranchAddress(b->GetName(), reinterpret_cast<void *>(ib.fBranchBuffer.get()));
+         fSourceTree->SetBranchAddress(b->GetName(), reinterpret_cast<void *>(ib.fBranchBuffer.get()));
       }
 
       // If the TTree branch type and the RNTuple field type match, use the branch read buffer as RNTuple write buffer
@@ -368,7 +367,7 @@ ROOT::Experimental::RResult<void> ROOT::Experimental::RNTupleImporter::Import()
    }
 
    for (decltype(nEntries) i = 0; i < nEntries; ++i) {
-      fSourceTreePtr->GetEntry(i);
+      fSourceTree->GetEntry(i);
 
       for (const auto &[_, c] : fLeafCountCollections) {
          for (Int_t l = 0; l < *c.fCountVal; ++l) {
