cda3147d9d88fb3a7ed7ef8db887c73c47de1545
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-03-21 20:46:58 +0100

    explicitly handle 0 column case to avoid instantiation errors when used with jitted version of Book

--- roofit/roofitcore/inc/RooAbsDataHelper.h
+++ roofit/roofitcore/inc/RooAbsDataHelper.h
@@ -150,6 +150,15 @@ public:
       ExecImpl(sizeof...(values), vector);
    }
 
+   // 0 column version
+   void Exec(unsigned int)
+   {
+      if (_eventSize) {
+         throw std::invalid_argument(std::string("RooDataSet can hold ") + std::to_string(_eventSize) +
+                                     " variables per event, but RDataFrame passed 0 columns.");
+      }
+   }
+
    DataSet_t &GetAbsData() override { return *_dataset; }
 
 private:
