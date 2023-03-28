bd0aa0e3e1f79685927ba6cba9f68c414d3eeb9b
Sergey Linev <S.Linev@gsi.de>
2023-03-28 13:52:09 +0200

    [jsroot] 7.3.1 28/03/2023
    Bugfix release, tagged in JSROOT repository

--- js/modules/core.mjs
+++ js/modules/core.mjs
@@ -1,11 +1,11 @@
 
 /** @summary version id
   * @desc For the JSROOT release the string in format 'major.minor.patch' like '7.0.0' */
-let version_id = '7.3.x';
+let version_id = '7.3.1';
 
 /** @summary version date
   * @desc Release date in format day/month/year like '14/04/2022' */
-let version_date = '23/02/2023';
+let version_date = '28/03/2023';
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
