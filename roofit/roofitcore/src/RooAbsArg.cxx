efc20de9ed3cbad88960c3b9d3789900fe27dc7c
Jonas Rembser <jonas.rembser@cern.ch>
2023-04-11 19:23:04 +0200

    [RF] Use the right `TRefArray` constructor in RooAbsArg.cxx
    The TRefArray constructor with only one argument takes a process ID, and
    not a size!
    
    We can just use the default constructor, and then the process ID will be
    automatically assigned to the one of the first object.
    
    This mistake had no consequence so far, because the desired number of
    elements doesn't correspond to a valid process ID anyway.

--- roofit/roofitcore/src/RooAbsArg.cxx
+++ roofit/roofitcore/src/RooAbsArg.cxx
@@ -2461,7 +2461,7 @@ void RooRefArray::Streamer(TBuffer &R__b)
      R__c = R__b.WriteVersion(RooRefArray::IsA(), true);
 
      // Make a temporary refArray and write that to the streamer
-     TRefArray refArray(GetEntriesFast());
+     TRefArray refArray;
      for(TObject * tmpObj : *this) {
        refArray.Add(tmpObj) ;
      }
