root-project/root

commit bd508481ce104817ad7e421f3159d4093e12abbe

Extent the 'cursor' for basket.
There is now one per branch and it will contain more information than just the index.

tree/tree/src/TTreeCache.cxx
@@ -1167,16 +1167,25 @@ Bool_t TTreeCache::FillBuffer()
    Int_t clusterIterations = 0;
    Long64_t minEntry = fEntryCurrent;
    Int_t prevNtot;
-   Int_t minBasket = 0;  // We will use this to avoid re-checking the first baskets in the 2nd (or more) run in the while loop.
    Long64_t maxReadEntry = minEntry; // If we are stopped before the end of the 2nd pass, this marker will where we need to start next time.
    auto perfStats = GetTree()->GetPerfStats();
    do {
       prevNtot = ntotCurrentBuf;
-      Int_t nextMinBasket = INT_MAX;
       UInt_t pass = 0;
 
-      auto CollectBaskets = [this, elist, chainOffset, entry, clusterIterations, resetBranchInfo,
-       &lowestMaxEntry, &maxReadEntry, &nextMinBasket, &minBasket, &minEntry,
+      struct collectionInfo {
+         Int_t  fClusterStart{-1}; // First basket belonging to the current cluster
+         Int_t  fCurrent{0};       // Currently visited basket
+         Bool_t fLoadedOnce{kFALSE};
+
+         void Rewind() {
+            fCurrent = (fClusterStart >=0) ? fClusterStart : 0;
+         }
+      };
+      std::vector<collectionInfo> cursor(fNbranches);
+
+      auto CollectBaskets = [this, elist, chainOffset, entry, clusterIterations, resetBranchInfo, perfStats,
+       &cursor, &lowestMaxEntry, &maxReadEntry, &minEntry,
        &ranges, &memRanges, &reqRanges,
        &ntotCurrentBuf, &nReadPrefRequest](UInt_t pass, Bool_t narrow) {
         // The first pass we add one basket per branches around the requested entry
@@ -1202,9 +1211,8 @@ Bool_t TTreeCache::FillBuffer()
             //we have found the branch. We now register all its baskets
             //from the requested offset to the basket below fEntrymax
             Int_t blistsize = b->GetListOfBaskets()->GetSize();
-            Int_t j = minBasket;  // We need this out of the loop so we can find out how far we went.
             Bool_t firstBasketSeen = kFALSE;
-            for (;j<nb;j++) {
+            for (auto &j = cursor[i].fCurrent; j<nb; j++) {
                // This basket has already been read, skip it
 
                if (j<blistsize && b->GetListOfBaskets()->UncheckedAt(j)) {
@@ -1361,7 +1369,6 @@ Bool_t TTreeCache::FillBuffer()
          if (minEntry >= fEntryCurrentMax && fEntryCurrentMax >0)
             break;
       }
-      minBasket = nextMinBasket;
       fEntryNext = clusterIter.GetNextEntry();
       if (fEntryNext > fEntryMax) fEntryNext = fEntryMax;
    } while (kTRUE);

commit ad23a97da09373a959e354c707ff29c664df67b1

TTreeCache correct place cache (avoid O(N^2) behavior
Prior to this change, the cache of which basket to start the search
on would restart from the first basket at each cluster iteration
(i.e. for filling the cache with with clusters)

On an extreme example:
        15,272,928 entries
           152,739 baskets (and as many clusters)
            10,000 Actual TTreeCache buffer size (minimum allowed)
             8,442 estimated buffer size of TTreeCache (1.5 times compressed buffer size)
               400 bytes per baskets
               100 entries per baskets (i.e. per clusters)
                25 number of cluster per TTreeCache buffer for single branch with default size.
                 1 float per entry (reading a single branch).

This ends up repairing the performance of a simple `TTree::Draw` of a single branch
from 1 hour back down to 7s (performance seem in v6.12).

This correct an issue introduced by commit bd508481ce1 first since in v6.14/00

This fixes #12649

tree/tree/src/TTreeCache.cxx
@@ -1332,18 +1332,20 @@ Bool_t TTreeCache::FillBuffer()
    Long64_t maxReadEntry = minEntry; // If we are stopped before the end of the 2nd pass, this marker will where we need to start next time.
    Int_t nReadPrefRequest = 0;
    auto perfStats = GetTree()->GetPerfStats();
+
+   struct collectionInfo {
+      Int_t fClusterStart{-1}; // First basket belonging to the current cluster
+      Int_t fCurrent{0};       // Currently visited basket
+      Bool_t fLoadedOnce{kFALSE};
+
+      void Rewind() { fCurrent = (fClusterStart >= 0) ? fClusterStart : 0; }
+   };
+   std::vector<collectionInfo> cursor(fNbranches);
+
    do {
       prevNtot = ntotCurrentBuf;
       Long64_t lowestMaxEntry = fEntryMax; // The lowest maximum entry in the TTreeCache for each branch for each pass.
 
-      struct collectionInfo {
-         Int_t fClusterStart{-1}; // First basket belonging to the current cluster
-         Int_t fCurrent{0};       // Currently visited basket
-         Bool_t fLoadedOnce{kFALSE};
-
-         void Rewind() { fCurrent = (fClusterStart >= 0) ? fClusterStart : 0; }
-      };
-      std::vector<collectionInfo> cursor(fNbranches);
       Bool_t reachedEnd = kFALSE;
       Bool_t skippedFirst = kFALSE;
       Bool_t oncePerBranch = kFALSE;