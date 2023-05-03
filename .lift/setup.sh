e9eb79f57acda4ae124d3827d0d0015bde5a02cd
Axel Naumann <Axel.Naumann@cern.ch>
2023-05-03 16:57:11 +0200

    [lift] Fix setup / cmake invocation.

--- .lift/setup.sh
+++ .lift/setup.sh
@@ -2,5 +2,5 @@
 wget https://raw.githubusercontent.com/root-project/root-ci-images/main/ubuntu22/packages.txt
 sudo apt update && sudo apt install -y ninja-build `cat packages.txt`
 # Generate a compilation database next
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DALLOW_IN_SOURCE -G Ninja .. || cat CMakeFiles/CMakeOutput.log
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DALLOW_IN_SOURCE=On -G Ninja . || cat CMakeFiles/CMakeOutput.log
 ninja
