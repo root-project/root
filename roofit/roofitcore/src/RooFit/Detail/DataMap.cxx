f0096582730eb96595ca744da62c868aaeb42640
Jonas Rembser <jonas.rembser@cern.ch>
2023-05-25 12:21:37 +0200

    [RF] Support multiple NLL with BatchMode existing at the same time
    As explained in the last commit, it's not possible to evaluate two
    different likelihoods that share RooAbsArgs by different RooFitDrivers.
    
    However, this usecase needs to be supported, because in RooStats is
    often happens that different likelihoods based on the same model are
    instantiated.
    
    The key to the solution in this commit is the insight that for each
    likelihood, almost the full compute graph is cloned anyway, *except* for
    the parameters. Therefore, the solution proposed in this commit is to
    not set the data tokens for the parameters and not fill the data map
    with their values. Instead, when querying their values, they are just
    retrieved from the object itself. This is possible because parameters
    are always scalar, and the RooAbsReal can already cache scalar members
    in the `_value` field.

---
+++ roofit/roofitcore/src/RooFit/Detail/DataMap.cxx
@@ -0,0 +1,33 @@
+/*
+ * Project: RooFit
+ * Authors:
+ *   Garima Singh, CERN 2023
+ *   Jonas Rembser, CERN 2023
+ *
+ * Copyright (c) 2023, CERN
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted according to the terms
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
+
+#include <RooFit/Detail/DataMap.h>
+
+#include <RooRealVar.h>
+
+namespace RooFit {
+namespace Detail {
+
+RooSpan<const double> DataMap::at(RooAbsArg const *arg, RooAbsArg const * /*caller*/)
+{
+   if (!arg->hasDataToken()) {
+      auto var = static_cast<RooRealVar const *>(arg);
+      return {&var->_value, 1};
+   }
+   std::size_t idx = arg->dataToken();
+   return _dataMap[idx];
+}
+
+
+} // namespace Detail
+} // namespace RooFit
