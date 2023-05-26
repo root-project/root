6293b81474353aa035c0aa677cb9df8896468442
Philippe Canal <pcanal@fnal.gov>
2023-05-26 13:00:46 -0500

    dlopen race: add explicit dup STL dict.
    Since we no longer load/reload libHist, we were no longer testing the reloading STL
    dictionary.  So we now explicit add one dictionary in libUser that is a duplicate
    of a dictionary in libCore (and in libHist).

--- root/meta/race-12552/User.hpp
+++ root/meta/race-12552/User.hpp
@@ -1,6 +1,13 @@
 #ifndef USER_CLASS_HPP
 #define USER_CLASS_HPP
 
-class UserClass {};
+#include <vector>
+#include <TString.h>
+
+struct UserClass {
+   // The dictionary for this class is generated using genreflex
+   // so the following is not enough to trigger a GenerateInitInstance
+   std::vector<TString> fValues;
+};
 
 #endif
