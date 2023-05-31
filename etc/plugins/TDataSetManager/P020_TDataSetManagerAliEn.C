8d4a44a47a6deb57e0d26c457efd1bb6dca289b2
Axel Naumann <Axel.Naumann@cern.ch>
2023-05-31 14:27:13 +0200

    [proof] Remove TDataSetManagerAliEn, now that `alien` is removed from ROOT.

--- etc/plugins/TDataSetManager/P020_TDataSetManagerAliEn.C
+++
@@ -1,5 +0,0 @@
-void P020_TDataSetManagerAliEn()
-{
-   gPluginMgr->AddHandler("TDataSetManager", "^alien", "TDataSetManagerAliEn", "Proof",
-                          "TDataSetManagerAliEn(const char *,const char *,const char *)");
-}
