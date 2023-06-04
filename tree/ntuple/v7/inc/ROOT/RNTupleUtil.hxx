cc1a7e8fe8887ec12e39d63b7ffe0a1bc4487131
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2023-06-04 19:31:56 +0200

    [ntuple] Introduce locator type `kTypePageZero`
    This locator type represents unmaterialized (i.e., in-memory only) zero
    pages for deferred columns.
    The corresponding `RPageRange`s should be generated, where required, upon
    deserialization of the page lists.

--- tree/ntuple/v7/inc/ROOT/RNTupleUtil.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleUtil.hxx
@@ -138,11 +138,15 @@ struct RNTupleLocatorObject64 {
 /// and therefore we need to store their actual size.
 /// TODO(jblomer): consider moving this to `RNTupleDescriptor`
 struct RNTupleLocator {
-   /// Values for the _Type_ field in non-disk locators; see `doc/specifications.md` for details
+   /// Values for the _Type_ field in non-disk locators.  Serializable types must have the MSb == 0; see
+   /// `doc/specifications.md` for details
    enum ELocatorType : std::uint8_t {
       kTypeFile = 0x00,
       kTypeURI = 0x01,
       kTypeDAOS = 0x02,
+
+      kLastSerializableType = 0x7f,
+      kTypePageZero = kLastSerializableType + 1,
    };
 
    /// Simple on-disk locators consisting of a 64-bit offset use variant type `uint64_t`; extended locators have
