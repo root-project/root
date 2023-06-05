2b01b66fb14c94261dd6554bd3c639a44806b47f
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2023-06-05 22:56:41 +0200

    [ntuple] Claim co-authorship of RNTuple(Descriptor|Serialize), RPageSinkBuf

--- tree/ntuple/v7/inc/ROOT/RNTupleDescriptor.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleDescriptor.hxx
@@ -1,6 +1,7 @@
 /// \file ROOT/RNTupleDescriptor.hxx
 /// \ingroup NTuple ROOT7
 /// \author Jakob Blomer <jblomer@cern.ch>
+/// \author Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
 /// \date 2018-07-19
 /// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
 /// is welcome!
