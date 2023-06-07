38b1876af2fd40c86408a0854fcf3873201f06f2
Axel Naumann <Axel.Naumann@cern.ch>
2023-06-07 11:56:53 +0200

    cling/function/runlongArgTest: del unused code; provide actual .ref.

--- cling/function/longarg.C
+++ cling/function/longarg.C
@@ -27,30 +27,3 @@ void longarg()
    setter->Execute(name, param.Data());
    cout << name->GetName() << endl;
 }
-
-void longargWorkAround() 
-{
-
-   TNamed* name = new TNamed("name", "title");
-   cout << name->GetName() << endl;
-   TDataMember* thevar = name->IsA()->GetDataMember("fName");
-   TMethodCall* setter = thevar->SetterMethod(name->IsA());
-
-   TString param ("\"start \"");
-   for(Int_t i = 0; i < 1011; ++i){
-      param.Insert(param.Last('\"'), "1");
-   }
-   param.Insert(param.Last('\"'), " end");
-
-   cout << param.Length() << endl;
-
-   setter->Execute(name, Form("(const char*)%p",param.Data()));
-   cout << name->GetName() << endl;
-   
-   param.Insert(param.Last('\"'), "2");
-   cout << param.Length() << endl;
-   
-   setter->Execute(name, Form("(const char*)%p",param.Data()));
-   cout << name->GetName() << endl;
-
-}
