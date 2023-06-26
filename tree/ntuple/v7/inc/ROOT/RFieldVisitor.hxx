db324c3f673caa33c9c0d3ec7f79117472fae8cb
Florine de Geus <florine.willemijn.de.geus@cern.ch>
2023-06-26 17:28:47 +0200

    [ntuple] Add support for type-erased std::set fields

--- tree/ntuple/v7/inc/ROOT/RFieldVisitor.hxx
+++ tree/ntuple/v7/inc/ROOT/RFieldVisitor.hxx
@@ -48,7 +48,7 @@ public:
    virtual void VisitBitsetField(const RBitsetField &field) { VisitField(field); }
    virtual void VisitBoolField(const RField<bool> &field) { VisitField(field); }
    virtual void VisitClassField(const RClassField &field) { VisitField(field); }
-   virtual void VisitCollectionClassField(const RCollectionClassField &field) { VisitField(field); }
+   virtual void VisitProxiedCollectionField(const RProxiedCollectionField &field) { VisitField(field); }
    virtual void VisitRecordField(const RRecordField &field) { VisitField(field); }
    virtual void VisitClusterSizeField(const RField<ClusterSize_t> &field) { VisitField(field); }
    virtual void VisitCardinalityField(const RCardinalityField &field) { VisitField(field); }
@@ -69,7 +69,6 @@ public:
    virtual void VisitVectorField(const RVectorField &field) { VisitField(field); }
    virtual void VisitVectorBoolField(const RField<std::vector<bool>> &field) { VisitField(field); }
    virtual void VisitRVecField(const RRVecField &field) { VisitField(field); }
-   virtual void VisitSetField(const RSetField &field) { VisitField(field); }
 }; // class RFieldVisitor
 
 } // namespace Detail
@@ -215,14 +214,13 @@ public:
    void VisitArrayField(const RArrayField &field) final;
    void VisitClassField(const RClassField &field) final;
    void VisitRecordField(const RRecordField &field) final;
-   void VisitCollectionClassField(const RCollectionClassField &field) final;
+   void VisitProxiedCollectionField(const RProxiedCollectionField &field) final;
    void VisitVectorField(const RVectorField &field) final;
    void VisitVectorBoolField(const RField<std::vector<bool>> &field) final;
    void VisitRVecField(const RRVecField &field) final;
    void VisitBitsetField(const RBitsetField &field) final;
    void VisitNullableField(const RNullableField &field) final;
    void VisitEnumField(const REnumField &field) final;
-   void VisitSetField(const RSetField &field) final;
 };
 
 
