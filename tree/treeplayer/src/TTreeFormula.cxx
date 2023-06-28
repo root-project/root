592fcb91e9261cb34a2c702aed3b1c3305b8e249
Sergey Linev <S.Linev@gsi.de>
2023-06-28 16:43:23 +0200

    [treeplayer] fix compiler warnings concerning enum combination
    Messages like this:
    ```
    tree/treeplayer/src/TFormLeafInfo.cxx: In member function ‘virtual
    Bool_t TFormLeafInfo::IsString() const’:
    tree/treeplayer/src/TFormLeafInfo.cxx:361:36: warning: arithmetic
    between different enumeration types ‘TStreamerInfo::EReadWrite’ and
    ‘EDataType’ is deprecated [-Wdeprecated-enum-enum-conversion]
      361 |       case TStreamerInfo::kOffsetL + kChar_t:
          |            ~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~
    tree/treeplayer/src/TFormLeafInfo.cxx: In member function ‘T
    TFormLeafInfo::ReadValueImpl(char*, Int_t)’:
    tree/treeplayer/src/TFormLeafInfo.cxx:890:36: warning: arithmetic
    between different enumeration types ‘TStreamerInfo::EReadWrite’ and
    ‘EDataType’ is deprecated [-Wdeprecated-enum-enum-conversion]
      890 |       case TStreamerInfo::kOffsetL + kULong64_t:
          |            ~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~
    ```

--- tree/treeplayer/src/TTreeFormula.cxx
+++ tree/treeplayer/src/TTreeFormula.cxx
@@ -4755,7 +4755,7 @@ Bool_t  TTreeFormula::IsLeafString(Int_t code) const
                // let's assume it is NOT a string.
                return kFALSE;
             }
-            if (elem->GetNewType()== TStreamerInfo::kOffsetL +kChar_t) {
+            if (elem->GetNewType() == TStreamerInfo::kOffsetL + TStreamerInfo::kChar) {
                // Check whether a specific element of the string is specified!
                if (fIndexes[code][fNdimensions[code]-1] != -1) return kFALSE;
                return kTRUE;
