5c2dbc26f5f03c9c478dfd204394763d824e00c3
Yash Solanki <67216443+yashnator@users.noreply.github.com>
2023-06-28 17:15:50 +0530

    Feat: Change roofit/HS3 for Pythonic dictionary-based object creation
    - Allow creation of p.d.f.s, functions and variables in RooWorkspace in
    PyROOT using a dictionary.
    
    - Add JSONIOUtils for comparing string suffix with an input string
    
    - Implement the C++ side this Pythnoization

--- roofit/hs3/src/JSONFactories_HistFactory.cxx
+++ roofit/hs3/src/JSONFactories_HistFactory.cxx
@@ -34,6 +34,7 @@
 #include <RooWorkspace.h>
 
 #include "static_execute.h"
+#include "JSONIOUtils.h"
 
 using RooFit::Detail::JSONNode;
 
@@ -78,11 +79,6 @@ constexpr auto staterror = "staterror";
 bool startsWith(std::string_view str, std::string_view prefix)
 {
    return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
-}
-
-bool endsWith(std::string_view str, std::string_view suffix)
-{
-   return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
 }
 
 template <class T>
---
+++ roofit/hs3/src/JSONIOUtils.h
@@ -0,0 +1,8 @@
+#ifndef JSONIOUtils_h
+#define JSONIOUtils_h
+
+#include <ROOT/RStringView.hxx>
+
+bool endsWith(std::string_view str,std::string_view suffix);
+
+#endif
