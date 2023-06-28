667b826733b6fb25fa350a571b9dc78c40295f04
Yash Solanki <67216443+yashnator@users.noreply.github.com>
2023-06-28 17:22:23 +0530

    Feat: Create RooWorkspace objects using dictionary in PyROOT
    - Allow creation of p.d.f.s, functions and variables using dictionary in
    RooWorkspace in PyROOT using a dictionary.
    
    - Implement Python side of the Pythonization

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_roofit/_rooworkspace.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_roofit/_rooworkspace.py
@@ -12,6 +12,50 @@
 from ._utils import _kwargs_to_roocmdargs, cpp_signature
 
 
+def make_json_for_variable(var_name, var_dict):
+    val = None
+    max_value = None
+    min_value = None
+    is_constant = None
+    if "value" in var_dict:
+        val = var_dict["value"]
+        is_constant = True
+    if ("max" in var_dict) and ("min" in var_dict):
+        max_value = var_dict["max"]
+        min_value = var_dict["min"]
+        is_constant = False
+    if is_constant is None:
+        raise ValueError(
+            "Invalid Syntax: Please provide either 'value' or 'min' and 'max' or both"
+        )
+    else:
+        if not is_constant:
+            val = (max_value + min_value) / 2
+        # Create dictionary with domains and parameter points
+        json_dict = {
+            "domains": [
+                {
+                    "axes": [{"name": var_name}],
+                    "name": "default_domain",
+                    "type": "product_domain",
+                }
+            ],
+            "parameter_points": [
+                {
+                    "name": "default_values",
+                    "parameters": [{"name": var_name, "value": val}],
+                }
+            ],
+        }
+        if is_constant:
+            json_dict["parameter_points"][0]["parameters"][0]["const"] = True
+            json_dict["misc"] = {"ROOT_internal": {var_name: {"tags": "Constant"}}}
+        if not is_constant:
+            json_dict["domains"][0]["axes"][0]["max"] = max_value
+            json_dict["domains"][0]["axes"][0]["min"] = min_value
+        return json_dict
+
+
 class RooWorkspace(object):
     r"""The RooWorkspace::import function can't be used in PyROOT because `import` is a reserved python keyword.
     For this reason, an alternative with a capitalized name is provided:
@@ -41,28 +85,57 @@ class RooWorkspace(object):
 
     def __setitem__(self, key, value):
         # Check if initialization is done with string
-        if(isinstance(value, str)):
-            if(not self.obj(key)):
+        if isinstance(value, str):
+            if not self.obj(key):
                 parenthesis_index = -1
-                for i in range(0,len(value)):
-                    if(value[i]=='[' or value[i]=='('):
+                for i in range(0, len(value)):
+                    if value[i] == "[" or value[i] == "(":
                         parenthesis_index = i
                         break
                 # Initializes variables
-                if value[parenthesis_index]=='[' and parenthesis_index!=-1:
+                if value[parenthesis_index] == "[" and parenthesis_index != -1:
                     expr = key + value
                     self.factory(expr)
                 # Initializes functions and p.d.f.s
-                elif value[parenthesis_index]=='(' and parenthesis_index!=-1:
-                    expr = value[0:parenthesis_index] + "::" + key + value[parenthesis_index:]
+                elif value[parenthesis_index] == "(" and parenthesis_index != -1:
+                    expr = (
+                        value[0:parenthesis_index]
+                        + "::"
+                        + key
+                        + value[parenthesis_index:]
+                    )
                     self.factory(expr)
                 # Else raises a Syntax error
                 else:
                     raise SyntaxError("Invalid syntax")
             else:
-                raise RuntimeError("ERROR importing object named " + key + " another instance with same name already in the workspace and no conflict resolution protocol specified")
+                raise RuntimeError(
+                    "ERROR importing object named "
+                    + key
+                    + " another instance with same name already in the workspace and no conflict resolution protocol specified"
+                )
+        elif isinstance(value, dict):
+            import ROOT
+            import json
+
+            # Add name attribute to the dictionary, and create the JSON string for the object's JSONTree
+            is_variable = not "type" in value
+            if is_variable:
+                # Import variable
+                json_dict = make_json_for_variable(key, value)
+                json_string = json.dumps(json_dict, separators=(",", ":"))
+                ROOT.RooJSONFactoryWSTool(self).importVarfromString(json_string)
+            else:
+                # Imports functions/p.d.f.s
+                value["name"] = key
+                json_string = json.dumps(value, separators=(",", ":"))
+                ROOT.RooJSONFactoryWSTool(self).importFunction(json_string, False)
         else:
-            raise TypeError("Object of type 'str' expected but " + type(value) + " was given")
+            raise TypeError(
+                "Object of type 'str' or 'dict' expected but "
+                + type(value)
+                + " was given"
+            )
 
     @cpp_signature(
         [
@@ -95,7 +168,11 @@ class RooWorkspace(object):
         # here, which results in a clearer error to the user than an infinite
         # call stack involving the internal pythonization code.
         if name in ["_import", "import", "Import"]:
-            raise AttributeError("Resetting the \"" + name + "\" attribute of a RooWorkspace is not allowed!")
+            raise AttributeError(
+                'Resetting the "'
+                + name
+                + '" attribute of a RooWorkspace is not allowed!'
+            )
         object.__setattr__(self, name, value)
 
 
