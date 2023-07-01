6fdcfb23b56f03631c94664fb9540d2b0575e2a5
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-07-01 16:42:22 +0200

    [RF][HS3] Refactor HS3 HistFactory implementation to reduce code

--- roofit/hs3/src/JSONIOUtils.cxx
+++ roofit/hs3/src/JSONIOUtils.cxx
@@ -1,5 +1,11 @@
 #include "JSONIOUtils.h"
 
-bool endsWith(std::string_view str, std::string_view suffix) {
+bool startsWith(std::string_view str, std::string_view prefix)
+{
+   return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
+}
+
+bool endsWith(std::string_view str, std::string_view suffix)
+{
    return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
-} 
+}
