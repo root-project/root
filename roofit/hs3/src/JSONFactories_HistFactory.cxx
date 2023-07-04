d5307c9a03c0c1fe9f1cce46c3c360c7e1d3dca2
Jonas Rembser <jonas.rembser@cern.ch>
2023-07-04 10:32:26 +0200

    [RF][HS3][HF] Use `setPositiveDefinite()` for RooPiecewiseInterpolation
    This is done to be consistent with the old HistFactory implementation.

--- roofit/hs3/src/JSONFactories_HistFactory.cxx
+++ roofit/hs3/src/JSONFactories_HistFactory.cxx
@@ -337,6 +337,7 @@ bool importHistSample(RooJSONFactoryWSTool &tool, RooDataHist &dh, RooArgSet con
       }
       if (!histNps.empty()) {
          auto &v = tool.wsEmplace<PiecewiseInterpolation>("histoSys_" + prefixedName, hf, histoLo, histoHi, histNps);
+         v.setPositiveDefinite();
          v.setAllInterpCodes(4); // default interpCode for HistFactory
          shapeElems.add(v);
       } else {
