d6b30ec873aeac8b97355db731ead46398b0e1e3
Daniel Alvarez Conde <danielalvcon@gmail.com>
2023-07-06 15:48:19 +0200

    Merge development into master

--- roofit/roofit/inc/RooGaussian.h
+++ roofit/roofit/inc/RooGaussian.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Project: RooFit                                                           *
  * Package: RooFitModels                                                     *
- *    File: $Id: RooGaussian.h,v 1.16 2007/07/12 20:30:49 wouter Exp $
+ *    File: $Id: RooGaussian.h,v 1.16 2007/07/12 20:30:49 wouter Exp $       
  * Authors:                                                                  *
  *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
  *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
