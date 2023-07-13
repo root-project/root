db5f3764e219c57aa9f8891f87a816875aaaa543
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2023-07-13 11:02:01 +0200

    header files updated as stated in https://github.com/root-project/root/pull/13205#discussion_r1261563778

--- tutorials/tree/dictionary/data2Tree.cpp
+++ tutorials/tree/dictionary/data2Tree.cpp
@@ -1,7 +1,13 @@
-// ##########################################################################
-// # Alvaro Tolosa Delgado @ IFIC(Valencia,Spain)  alvaro.tolosa@ific.uv.es #
-// # Copyright (c) 2018 Alvaro Tolosa. All rights reserved.		 #
-// ##########################################################################
+// Author: Alvaro Tolosa-Delgado CERN 07/2023
+// Author: Jorge Agramunt Ros IFIC(Valencia,Spain) 07/2023
+
+/*************************************************************************
+ * Copyright (C) 1995-2023, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
 
 // This file contains the definition of the class method
 // The class method was declared in the corresponding .hpp file
--- tutorials/tree/dictionary/data2TreeLinkDef.hpp
+++ tutorials/tree/dictionary/data2TreeLinkDef.hpp
@@ -1,7 +1,13 @@
-// ##########################################################################
-// # Alvaro Tolosa Delgado @ IFIC(Valencia,Spain)  alvaro.tolosa@ific.uv.es #
-// # Copyright (c) 2018 Alvaro Tolosa. All rights reserved.		 #
-// ##########################################################################
+// Author: Alvaro Tolosa-Delgado CERN 07/2023
+// Author: Jorge Agramunt Ros IFIC(Valencia,Spain) 07/2023
+
+/*************************************************************************
+ * Copyright (C) 1995-2023, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
 
 // This file contains a selection of types that will be described in the ROOT dictionary.
 //     Further details in: https://root.cern/manual/io_custom_classes/#selecting-dictionary-entries-linkdefh
