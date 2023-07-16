56b9d0f50d91376f3b521fc48acd7ba53170c5ae
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-07-16 15:23:13 +0200

    [RF][HS3] Generalize JSONIO importer interface
    It didn't really make sense to have different interfaces for pdfs and
    functions.

--- roofit/hs3/src/JSONFactories_HistFactory.cxx
+++ roofit/hs3/src/JSONFactories_HistFactory.cxx
@@ -356,7 +356,7 @@ bool importHistSample(RooJSONFactoryWSTool &tool, RooDataHist &dh, RooArgSet con
 
 class HistFactoryImporter : public RooFit::JSONIO::Importer {
 public:
-   bool importPdf(RooJSONFactoryWSTool *tool, const JSONNode &p) const override
+   bool importArg(RooJSONFactoryWSTool *tool, const JSONNode &p) const override
    {
       RooWorkspace &ws = *tool->workspace();
 
@@ -513,7 +513,7 @@ public:
 
 class PiecewiseInterpolationFactory : public RooFit::JSONIO::Importer {
 public:
-   bool importFunction(RooJSONFactoryWSTool *tool, const JSONNode &p) const override
+   bool importArg(RooJSONFactoryWSTool *tool, const JSONNode &p) const override
    {
       std::string name(RooJSONFactoryWSTool::name(p));
 
@@ -539,7 +539,7 @@ public:
 
 class FlexibleInterpVarFactory : public RooFit::JSONIO::Importer {
 public:
-   bool importFunction(RooJSONFactoryWSTool *tool, const JSONNode &p) const override
+   bool importArg(RooJSONFactoryWSTool *tool, const JSONNode &p) const override
    {
       std::string name(RooJSONFactoryWSTool::name(p));
       if (!p.has_child("high")) {
