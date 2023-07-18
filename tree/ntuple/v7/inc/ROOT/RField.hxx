4933e9d5d7f9b2d9aa5b8d8bacb5725eed6e68df
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2023-07-18 18:55:36 +0200

    [ntuple] Rename `normalizedType` to `canonicalType`

--- tree/ntuple/v7/inc/ROOT/RField.hxx
+++ tree/ntuple/v7/inc/ROOT/RField.hxx
@@ -221,7 +221,7 @@ protected:
    /// Factory method to resurrect a field from the stored on-disk type information.  This overload takes an already
    /// normalized type name and type alias
    static RResult<std::unique_ptr<RFieldBase>>
-   Create(const std::string &fieldName, const std::string &normalizedType, const std::string &typeAlias);
+   Create(const std::string &fieldName, const std::string &canonicalType, const std::string &typeAlias);
 
 public:
    /// Iterates over the sub tree of fields in depth-first search order
