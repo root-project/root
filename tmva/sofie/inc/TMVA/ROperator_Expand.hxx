a3f0749513463527fa4a7955eed6fa05f23b01c7
joannapng <ipanagou@uth.gr>
2023-07-21 17:32:41 +0200

    added expand operator

--- tmva/sofie/inc/TMVA/ROperator_Expand.hxx
+++ tmva/sofie/inc/TMVA/ROperator_Expand.hxx
@@ -107,7 +107,39 @@ public:
    }
 
    std::string GenerateGPU(std::string OpName) override {
-      return std::string();
+      OpName = "op_" + OpName;
+
+      if (fShapeY.empty()) {
+         throw std::runtime_error("TMVA SOFIE Expand Op called to Generate without being initialized first");
+      }
+      std::stringstream out;
+      out << "\n" << SP*3 << "//------ Expand Op" << "\n";
+      size_t length = ConvertShapeToLength(fShapeY);
+      // No need to broadcast A if it's an initialized tensor
+      if (fInitialized) {
+         out << SP*3 << "// Copying initialized tensor " << fNX << " to " << fNY << "\n";
+         out << SP*3 << "q.submit([&](cl::sycl::handler &cgh){\n";
+         out << SP*4 << "auto acc_tensor_" << fNX << " = cl::sycl::accessor{buf_tensor_" << fNX;
+         out << ", cgh, cl::sycl::read_only};\n";
+         out << SP*4 << "auto acc_tensor_" << fNY << " = cl::sycl::accessor{buf_tensor_" << fNY;
+         out << ", cgh, cl::sycl::write_only, cl::sycl::no_init};\n\n";
+         out << SP*4 << "cgh.copy_in_device(acc_tensor_" << fNX << ", acc_tensor_" << fNY << ");\n";
+         out << SP*4 << "});\n";
+         out << SP*3 << "});\n";
+      }
+      else {
+         out << SP*3 << "// Broadcasting uninitialized tensor " << fNX << "\n";
+         out << SP*3 << "{\n";
+         out << SP*4 << "float* data = TMVA::Experimental::SOFIE::UTILITY::UnidirectionalBroadcast<float>(tensor_" << fNX << ", " << ConvertShapeToString(fShapeX) << ", " << ConvertShapeToString(fShapeY) << ");\n";
+         out << SP*4 << "q.submit([&](cl::sycl::handler& cgh){\n";
+         out << SP*5 << "auto acc_tensor_" << fNY << " = cl::sycl::accessor{buf_tensor_";
+         out << fNY << ", cgh, cl::sycl::write_only, cl::sycl::no_init};\n";
+         out << SP*6 << "cgh.copy(data, tensor_" << "fNY);\n";
+         out << SP*5 << "});\n";
+         out << SP*4 << "delete[] data;\n";
+         out << SP << "}\n";
+      }
+      return out.str();
    }
 
 };
