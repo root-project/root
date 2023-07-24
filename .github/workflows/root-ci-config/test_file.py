ab60b6bcbc8fc2eaa553f6e50b76786516c22979
Aniq <2aniqqjavedd4493@gmail.com>
2023-07-24 13:26:57 +0200

    print platform

--- .github/workflows/root-ci-config/test_file.py
+++ .github/workflows/root-ci-config/test_file.py
@@ -1,76 +1,100 @@
-import os
-from build_utils import (
-    cmake_options_from_dict,
-    die,
-    download_latest,
-    github_log_group,
-    print_info,
-    load_config,
-    print_shell_log,
-    subprocess_with_log,
-    upload_file,
-    print_warning,
-)
+# import os
+# from build_utils import (
+#     cmake_options_from_dict,
+#     die,
+#     download_latest,
+#     github_log_group,
+#     print_info,
+#     load_config,
+#     print_shell_log,
+#     subprocess_with_log,
+#     upload_file,
+#     print_warning,
+# )
 
-WORKDIR = '/tmp/workspace'
+# WORKDIR = '/tmp/workspace'
 
 
-def main():
-    shell_log = ""
-    contents = create_coverage_xml(shell_log)
-    print(contents)
+# def main():
+#     shell_log = ""
+#     contents = create_coverage_xml(shell_log)
+#     print(contents)
     
 
 
-# @github_log_group("Run tests")
-# def run_ctest(shell_log: str, extra_ctest_flags: str) -> str:
-#     result, shell_log = subprocess_with_log(f"""
-#         cd '{WORKDIR}/build'
-#         ctest --output-on-failure --parallel {os.cpu_count()} --output-junit TestResults.xml {extra_ctest_flags}
-#     """, shell_log)
+# # @github_log_group("Run tests")
+# # def run_ctest(shell_log: str, extra_ctest_flags: str) -> str:
+# #     result, shell_log = subprocess_with_log(f"""
+# #         cd '{WORKDIR}/build'
+# #         ctest --output-on-failure --parallel {os.cpu_count()} --output-junit TestResults.xml {extra_ctest_flags}
+# #     """, shell_log)
 
-#     if result != 0:
-#         die(result, "Some tests failed", shell_log)
+# #     if result != 0:
+# #         die(result, "Some tests failed", shell_log)
 
-#     return shell_log
+# #     return shell_log
 
-# @github_log_group("Create Test Coverage")
-# def create_coverage_html(shell_log: str) -> str:
-#     #directory = f"{WORKDIR}/build/interpreter/llvm-project/llvm/lib/ProfileData/Coverage/CMakeFiles"
-#     directory = f"../root-ci-config/"
-#     result, shell_log = subprocess_with_log(f"""
-#         cd '{directory}'
-#         lcov --directory . --capture --output-file coverage.info
-#         genhtml coverage.info --output-directory coverage_report
-#         cd coverage_report
-#         firefox index.html
-#     """, shell_log)
-#     if directory == "":
-#         print("No content")
-#     #contents = os.listdir(directory)
-#     print(result)
-#     print("---------")
-#     print(shell_log)
-#     #return contents
+# # @github_log_group("Create Test Coverage")
+# # def create_coverage_html(shell_log: str) -> str:
+# #     #directory = f"{WORKDIR}/build/interpreter/llvm-project/llvm/lib/ProfileData/Coverage/CMakeFiles"
+# #     directory = f"../root-ci-config/"
+# #     result, shell_log = subprocess_with_log(f"""
+# #         cd '{directory}'
+# #         lcov --directory . --capture --output-file coverage.info
+# #         genhtml coverage.info --output-directory coverage_report
+# #         cd coverage_report
+# #         firefox index.html
+# #     """, shell_log)
+# #     if directory == "":
+# #         print("No content")
+# #     #contents = os.listdir(directory)
+# #     print(result)
+# #     print("---------")
+# #     print(shell_log)
+# #     #return contents
 
 
-@github_log_group("Create Test Coverage in XML")
-def create_coverage_xml(shell_log: str) -> str:
-    directory = f"{WORKDIR}/build/interpreter/llvm-project/llvm/lib/ProfileData/Coverage"
-    #directory = f"../root-ci-config"
-    result, shell_log = subprocess_with_log(f"""
-        cd '{directory}'
-        pwd
-        gcovr --cobertura-pretty -r ~/{WORKDIR}/src . -o XMLCoverage.xml
-        pwd
-    """, shell_log)
-    if directory == "":
-        print("No content")
-    #contents = os.listdir(directory)
-    print(result)
-    print("---------")
-    print(shell_log)
+# # @github_log_group("Create Test Coverage in XML")
+# # def create_coverage_xml(shell_log: str) -> str:
+# #     directory = f"{WORKDIR}/build/interpreter/llvm-project/llvm/lib/ProfileData/Coverage"
+# #     #directory = f"../root-ci-config"
+# #     result, shell_log = subprocess_with_log(f"""
+# #         cd '{directory}'
+# #         pwd
+# #         gcovr --cobertura-pretty -r ~/{WORKDIR}/src . -o XMLCoverage.xml
+# #         pwd
+# #     """, shell_log)
+# #     if directory == "":
+# #         print("No content")
+# #     #contents = os.listdir(directory)
+# #     print(result)
+# #     print("---------")
+# #     print(shell_log)
 
+
+# if __name__ == "__main__":
+#     main()
+
+
+# build_root.py
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--buildtype', required=True)
+    parser.add_argument('--platform', required=True)
+    parser.add_argument('--incremental', type=bool, default=False)
+    parser.add_argument('--base_ref', required=True)
+    parser.add_argument('--repository', required=True)
+    args = parser.parse_args()
+
+    # Accessing the matrix.image value
+    platform = args.platform
+    print(platform)
+    # Now you can use the 'platform' variable in your script as needed.
+
+    # Rest of the script logic...
 
 if __name__ == "__main__":
     main()
