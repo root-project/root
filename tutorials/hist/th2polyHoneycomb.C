fa9aa2c1350d25d62cff932da614e2c2a51cab5d
Olivier Couet <olivier.couet@cern.ch>
2023-07-28 12:00:11 +0200

    Implement options for Honeycomb

--- tutorials/hist/th2polyHoneycomb.C
+++ tutorials/hist/th2polyHoneycomb.C
@@ -11,14 +11,25 @@
 /// \author  Olivier Couet
 
 void th2polyHoneycomb(){
-   TH2Poly *hc = new TH2Poly();
-   hc->SetTitle("Honeycomb example");
-   hc->Honeycomb(0,0,.1,25,25);
+   TCanvas *C = new TCanvas("C","C",1200,600);
+   C->Divide(2,1);
 
-   TRandom ran;
-   for (int i = 0; i<30000; i++) {
-      hc->Fill(ran.Gaus(2.,1), ran.Gaus(2.,1));
-   }
+   TH2Poly *hc1 = new TH2Poly();
+   hc1->Honeycomb(0,0,.1,5,5);
+   hc1->SetTitle("Option V (default)");
+   hc1->SetStats(0);
+   hc1->Fill(.1, .1, 15.);
+   hc1->Fill(.4, .4, 10.);
+   hc1->Fill(.5, .5, 20.);
 
-   hc->Draw("colz");
+   TH2Poly *hc2 = new TH2Poly();
+   hc2->Honeycomb(0,0,.1,5,5,"h");
+   hc2->SetTitle("Option H");
+   hc2->SetStats(0);
+   hc2->Fill(.1, .1, 15.);
+   hc2->Fill(.4, .4, 10.);
+   hc2->Fill(.5, .5, 20.);
+
+   C->cd(1)->SetGrid(); hc1->Draw("colz L");
+   C->cd(2)->SetGrid(); hc2->Draw("colz L");
 }
