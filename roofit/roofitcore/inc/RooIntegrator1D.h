895c0a8f4d9e3719aad08da5b9ecc596383764b7
Jonas Rembser <jonas.rembser@cern.ch>
2023-08-02 15:05:30 +0200

    [RF] Code-format `RooIntegrator1D`
    This class will be changed a lot in the next commits and even renamed.
    So we might as well format it now.

--- roofit/roofitcore/inc/RooIntegrator1D.h
+++ roofit/roofitcore/inc/RooIntegrator1D.h
@@ -35,54 +35,58 @@ std::pair<double, int> integrate1d(std::function<double(double)> func, bool doTr
 
 class RooIntegrator1D : public RooAbsIntegrator {
 public:
+   // Constructors, assignment etc
+   enum SummationRule { Trapezoid, Midpoint };
 
-  // Constructors, assignment etc
-  enum SummationRule { Trapezoid, Midpoint };
+   RooIntegrator1D(const RooAbsFunc &function, SummationRule rule = Trapezoid, int maxSteps = 0, double eps = 0);
+   RooIntegrator1D(const RooAbsFunc &function, double xmin, double xmax, SummationRule rule = Trapezoid,
+                   int maxSteps = 0, double eps = 0);
 
-  RooIntegrator1D(const RooAbsFunc& function, SummationRule rule= Trapezoid,
-        int maxSteps= 0, double eps= 0) ;
-  RooIntegrator1D(const RooAbsFunc& function, double xmin, double xmax,
-        SummationRule rule= Trapezoid, int maxSteps= 0, double eps= 0) ;
+   RooIntegrator1D(const RooAbsFunc &function, const RooNumIntConfig &config);
+   RooIntegrator1D(const RooAbsFunc &function, double xmin, double xmax, const RooNumIntConfig &config);
 
-  RooIntegrator1D(const RooAbsFunc& function, const RooNumIntConfig& config) ;
-  RooIntegrator1D(const RooAbsFunc& function, double xmin, double xmax,
-        const RooNumIntConfig& config) ;
+   bool checkLimits() const override;
+   double integral(const double *yvec = nullptr) override;
 
-  bool checkLimits() const override;
-  double integral(const double *yvec=nullptr) override ;
-
-  using RooAbsIntegrator::setLimits ;
-  bool setLimits(double* xmin, double* xmax) override;
-  bool setUseIntegrandLimits(bool flag) override {_useIntegrandLimits = flag ; return true ; }
+   using RooAbsIntegrator::setLimits;
+   bool setLimits(double *xmin, double *xmax) override;
+   bool setUseIntegrandLimits(bool flag) override
+   {
+      _useIntegrandLimits = flag;
+      return true;
+   }
 
 protected:
+   friend class RooNumIntFactory;
+   static void registerIntegrator(RooNumIntFactory &fact);
 
-  friend class RooNumIntFactory ;
-  static void registerIntegrator(RooNumIntFactory& fact) ;
+   bool initialize();
 
-  bool initialize();
+   bool _useIntegrandLimits; ///< If true limits of function binding are used
 
-  bool _useIntegrandLimits;  ///< If true limits of function binding are used
+   // Integrator configuration
+   SummationRule _rule;
+   int _maxSteps;           ///< Maximum number of steps
+   int _minStepsZero = 999; ///< Minimum number of steps to declare convergence to zero
+   int _fixSteps = 0;       ///< Fixed number of steps
+   double _epsAbs;          ///< Absolute convergence tolerance
+   double _epsRel;          ///< Relative convergence tolerance
+   bool _doExtrap = true;   ///< Apply conversion step?
+   double _xmin;            ///<! Lower integration bound
+   double _xmax;            ///<! Upper integration bound
 
-  // Integrator configuration
-  SummationRule _rule;
-  int _maxSteps ;          ///< Maximum number of steps
-  int _minStepsZero = 999; ///< Minimum number of steps to declare convergence to zero
-  int _fixSteps = 0;       ///< Fixed number of steps
-  double _epsAbs ;         ///< Absolute convergence tolerance
-  double _epsRel ;         ///< Relative convergence tolerance
-  bool _doExtrap = true;   ///< Apply conversion step?
-  double _xmin;            ///<! Lower integration bound
-  double _xmax;            ///<! Upper integration bound
+   // Numerical integrator workspace
+   std::vector<double> _wksp; ///<! Integrator workspace
 
-  // Numerical integrator workspace
-  std::vector<double> _wksp ;   ///<! Integrator workspace
+   double *xvec(double &xx)
+   {
+      _x[0] = xx;
+      return _x.data();
+   }
 
-  double* xvec(double& xx) { _x[0] = xx ; return _x.data(); }
+   std::vector<double> _x; //! do not persist
 
-  std::vector<double> _x ; //! do not persist
-
-  ClassDefOverride(RooIntegrator1D,0) // 1-dimensional numerical integration engine
+   ClassDefOverride(RooIntegrator1D, 0) // 1-dimensional numerical integration engine
 };
 
 #endif
