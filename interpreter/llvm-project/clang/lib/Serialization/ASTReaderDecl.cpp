2bc24ad7f8775f77c6d0e509b22b04535a79bb29
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2023-08-02 10:14:27 +0200

    [clang] Backport patch to merge template member parent
    Original message of upstream commit by Richard Smith,
    https://github.com/llvm/llvm-project/commit/61c7a9140b:
    ---
    Commit to a primary definition for a class when we load its first
    member.
    
    Previously, we wouldn't do this if the first member loaded is within a
    definition that's added to a class via an update record, which happens
    when template instantiation adds a class definition to a declaration
    that was imported from an AST file.
    
    This would lead to classes having member functions whose getParent
    returned a class declaration that wasn't the primary definition, which
    in turn caused the vtable builder to build broken vtables.
    
    I don't yet have a reduced testcase for the wrong-code bug here, because
    the setup required to get us into the broken state is very subtle, but
    have confirmed that this fixes it.
    ---
    
    This fixes an assertion in CodeGenFunction::EmitCXXDestructorCall():
    Assertion `ThisTy->getAsCXXRecordDecl() == DtorDecl->getParent() &&
    "Pointer/Object mixup"' failed.
    which was already seen during the upgrade to LLVM 13 in one tutorial
    on CentOS 8 and "solved" by commit ffe867985b ("Relax assertion on
    generating destructor call"). Due to the nature of this problem, the
    assertion failure went away with unrelated changes so I reverted the
    change in 2b997add1d. Now the problem comes back with the upgrade to
    LLVM 16 and also in master when trying to enable the RBatchGenerator
    tutorials in https://github.com/root-project/root/pull/13302, both
    on macOS this time. Luckily, the underlying cause was properly fixed
    in upstream LLVM just last week, so backport that commit.

--- interpreter/llvm-project/clang/lib/Serialization/ASTReaderDecl.cpp
+++ interpreter/llvm-project/clang/lib/Serialization/ASTReaderDecl.cpp
@@ -183,6 +183,13 @@ namespace clang {
     static void setAnonymousDeclForMerging(ASTReader &Reader, DeclContext *DC,
                                            unsigned Index, NamedDecl *D);
 
+    /// Commit to a primary definition of the class RD, which is known to be
+    /// a definition of the class. We might not have read the definition data
+    /// for it yet. If we haven't then allocate placeholder definition data
+    /// now too.
+    static CXXRecordDecl *getOrFakePrimaryClassDefinition(ASTReader &Reader,
+                                                          CXXRecordDecl *RD);
+
     /// Results from loading a RedeclarableDecl.
     class RedeclarableResult {
       Decl *MergeWith;
@@ -587,7 +594,13 @@ void ASTDeclReader::VisitDecl(Decl *D) {
     auto *LexicalDC = readDeclAs<DeclContext>();
     if (!LexicalDC)
       LexicalDC = SemaDC;
-    DeclContext *MergedSemaDC = Reader.MergedDeclContexts.lookup(SemaDC);
+    // If the context is a class, we might not have actually merged it yet, in
+    // the case where the definition comes from an update record.
+    DeclContext *MergedSemaDC;
+    if (auto *RD = dyn_cast<CXXRecordDecl>(SemaDC))
+      MergedSemaDC = getOrFakePrimaryClassDefinition(Reader, RD);
+    else
+      MergedSemaDC = Reader.MergedDeclContexts.lookup(SemaDC);
     // Avoid calling setLexicalDeclContext() directly because it uses
     // Decl::getASTContext() internally which is unsafe during derialization.
     D->setDeclContextsImpl(MergedSemaDC ? MergedSemaDC : SemaDC, LexicalDC,
@@ -3309,6 +3322,32 @@ static bool isSameEntity(NamedDecl *X, NamedDecl *Y) {
   return false;
 }
 
+CXXRecordDecl *
+ASTDeclReader::getOrFakePrimaryClassDefinition(ASTReader &Reader,
+                                               CXXRecordDecl *RD) {
+  // Try to dig out the definition.
+  auto *DD = RD->DefinitionData;
+  if (!DD)
+    DD = RD->getCanonicalDecl()->DefinitionData;
+
+  // If there's no definition yet, then DC's definition is added by an update
+  // record, but we've not yet loaded that update record. In this case, we
+  // commit to DC being the canonical definition now, and will fix this when
+  // we load the update record.
+  if (!DD) {
+    DD = new (Reader.getContext()) struct CXXRecordDecl::DefinitionData(RD);
+    RD->setCompleteDefinition(true);
+    RD->DefinitionData = DD;
+    RD->getCanonicalDecl()->DefinitionData = DD;
+
+    // Track that we did this horrible thing so that we can fix it later.
+    Reader.PendingFakeDefinitionData.insert(
+        std::make_pair(DD, ASTReader::PendingFakeDefinitionKind::Fake));
+  }
+
+  return DD->Definition;
+}
+
 /// Find the context in which we should search for previous declarations when
 /// looking for declarations to merge.
 DeclContext *ASTDeclReader::getPrimaryContextForMerging(ASTReader &Reader,
@@ -3316,29 +3355,8 @@ DeclContext *ASTDeclReader::getPrimaryContextForMerging(ASTReader &Reader,
   if (auto *ND = dyn_cast<NamespaceDecl>(DC))
     return ND->getOriginalNamespace();
 
-  if (auto *RD = dyn_cast<CXXRecordDecl>(DC)) {
-    // Try to dig out the definition.
-    auto *DD = RD->DefinitionData;
-    if (!DD)
-      DD = RD->getCanonicalDecl()->DefinitionData;
-
-    // If there's no definition yet, then DC's definition is added by an update
-    // record, but we've not yet loaded that update record. In this case, we
-    // commit to DC being the canonical definition now, and will fix this when
-    // we load the update record.
-    if (!DD) {
-      DD = new (Reader.getContext()) struct CXXRecordDecl::DefinitionData(RD);
-      RD->setCompleteDefinition(true);
-      RD->DefinitionData = DD;
-      RD->getCanonicalDecl()->DefinitionData = DD;
-
-      // Track that we did this horrible thing so that we can fix it later.
-      Reader.PendingFakeDefinitionData.insert(
-          std::make_pair(DD, ASTReader::PendingFakeDefinitionKind::Fake));
-    }
-
-    return DD->Definition;
-  }
+  if (auto *RD = dyn_cast<CXXRecordDecl>(DC))
+    return getOrFakePrimaryClassDefinition(Reader, RD);
 
   if (auto *ED = dyn_cast<EnumDecl>(DC))
     return ED->getASTContext().getLangOpts().CPlusPlus? ED->getDefinition()
