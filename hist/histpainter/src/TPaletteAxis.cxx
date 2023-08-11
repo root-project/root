f40adcdbc1ea11cd1ebefa22d9ddddc19b45a787
Sergey Linev <S.Linev@gsi.de>
2023-08-11 14:33:27 +0200

    Reduce Form usage in histpainter

--- hist/histpainter/src/TPaletteAxis.cxx
+++ hist/histpainter/src/TPaletteAxis.cxx
@@ -551,8 +551,8 @@ void TPaletteAxis::Paint(Option_t *)
          }
          // make sure labels dont overlap
          if (i == 0 || (b1 - prevlab) > 1.5*lsize_user) {
-            if (kHorizontal) label->PaintLatex(b1, ymin - lof, 0, lsize, Form("%g", zlab));
-            else             label->PaintLatex(xmax + lof, b1, 0, lsize, Form("%g", zlab));
+            if (kHorizontal) label->PaintLatex(b1, ymin - lof, 0, lsize, TString::Format("%g", zlab));
+            else             label->PaintLatex(xmax + lof, b1, 0, lsize, TString::Format("%g", zlab));
             prevlab = b1;
          }
          if (kHorizontal) line->PaintLine(b2, ymin+tlength, b2, ymin);
@@ -560,8 +560,8 @@ void TPaletteAxis::Paint(Option_t *)
          if (i == ndivz-1) {
             // label + tick at top of axis
             if (fH && (b2 - prevlab > 1.5*lsize_user)) {
-               if (kHorizontal) label->PaintLatex(b2, ymin - lof, 0, lsize, Form("%g",fH->GetMaximum()));
-               else             label->PaintLatex(xmax + lof, b2, 0, lsize, Form("%g",fH->GetMaximum()));
+               if (kHorizontal) label->PaintLatex(b2, ymin - lof, 0, lsize, TString::Format("%g",fH->GetMaximum()));
+               else             label->PaintLatex(xmax + lof, b2, 0, lsize, TString::Format("%g",fH->GetMaximum()));
             }
             if (kHorizontal) line->PaintLine(b1, ymin+tlength, b1, ymin);
             else             line->PaintLine(xmax-tlength, b2, xmax, b2);
