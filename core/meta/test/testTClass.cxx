5690885895bb6da47699d9593634583f15e804e6
Danilo Piparo <danilo.piparo@cern.ch>
2023-08-11 08:58:10 +0200

    Unit test for #9029

--- core/meta/test/testTClass.cxx
+++ core/meta/test/testTClass.cxx
@@ -1,6 +1,9 @@
 #include "TClass.h"
 #include "THashTable.h"
 #include "TInterpreter.h"
+#include "TSystem.h"
+
+#include <ROOT/TSeq.hxx>
 
 #include "gtest/gtest.h"
 
@@ -21,4 +24,25 @@ TEST(TClass, DictCheck)
    }
 
    EXPECT_STREQ(errMsg.c_str(), "Missing dictionary for C, ") << errMsg;
+}
+
+// This test is for issue #9029
+TEST(TClass, GetClassWithFundType)
+{
+   ProcInfo_t info;
+   gSystem->GetProcInfo(&info);
+   auto start_mem = info.fMemResident;
+
+   constexpr auto name = "std::vector<int>::value_type";
+   TClass *cl = nullptr;
+   for (auto i : ROOT::TSeqI(8192)) {
+      cl = TClass::GetClass(name);
+      i = (int)i; // avoids unused variable warning
+   }
+
+   gSystem->GetProcInfo(&info);
+   auto end_mem = info.fMemResident;
+
+   EXPECT_TRUE(nullptr == cl);
+   EXPECT_NEAR(start_mem, end_mem, 16384);
 }
