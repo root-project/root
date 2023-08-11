9b543e7b2b213375a2c9bfdbfd14988e41bc5dbc
Sergey Linev <S.Linev@gsi.de>
2023-08-11 14:03:25 +0200

    Reduce Form function usage in core classes
    Not good idea to use returned buffer with ProcessLine() call

--- core/base/src/TRemoteObject.cxx
+++ core/base/src/TRemoteObject.cxx
@@ -89,10 +89,10 @@ void TRemoteObject::Browse(TBrowser *b)
       if (b->GetRefreshFlag())
          b->SetRefreshFlag(kFALSE);
       gApplication->SetBit(TApplication::kProcessRemotely);
-      TObject *obj = (TObject *)gROOT->ProcessLine(Form("((TApplicationServer *)gApplication)->BrowseKey(\"%s\");", GetName()));
+      TObject *obj = (TObject *)gROOT->ProcessLine(TString::Format("((TApplicationServer *)gApplication)->BrowseKey(\"%s\");", GetName()));
       if (obj) {
          if (obj->IsA()->GetMethodWithPrototype("SetDirectory", "TDirectory*"))
-            gROOT->ProcessLine(Form("((%s *)0x%zx)->SetDirectory(0);", obj->ClassName(), (size_t)obj));
+            gROOT->ProcessLine(TString::Format("((%s *)0x%zx)->SetDirectory(0);", obj->ClassName(), (size_t)obj));
          obj->Browse(b);
          b->SetRefreshFlag(kTRUE);
       }
@@ -101,7 +101,7 @@ void TRemoteObject::Browse(TBrowser *b)
       if (b->GetRefreshFlag())
          b->SetRefreshFlag(kFALSE);
       gApplication->SetBit(TApplication::kProcessRemotely);
-      ret = (TList *)gROOT->ProcessLine(Form("((TApplicationServer *)gApplication)->BrowseDirectory(\"%s\");", GetTitle()));
+      ret = (TList *)gROOT->ProcessLine(TString::Format("((TApplicationServer *)gApplication)->BrowseDirectory(\"%s\");", GetTitle()));
       if (ret) {
          TIter next(ret);
          while ((robj = (TRemoteObject *)next())) {
@@ -117,7 +117,7 @@ void TRemoteObject::Browse(TBrowser *b)
       if (b->GetRefreshFlag())
          b->SetRefreshFlag(kFALSE);
       gApplication->SetBit(TApplication::kProcessRemotely);
-      ret = (TList *)gROOT->ProcessLine(Form("((TApplicationServer *)gApplication)->BrowseFile(\"%s\");", GetName()));
+      ret = (TList *)gROOT->ProcessLine(TString::Format("((TApplicationServer *)gApplication)->BrowseFile(\"%s\");", GetName()));
       if (ret) {
          TIter next(ret);
          while ((robj = (TRemoteObject *)next())) {
