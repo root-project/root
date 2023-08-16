a4413ceadf410e9f686f8fc2a91012d5f610d71a
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-08-16 10:32:38 +0200

    [RF] Fix bug in HistFactory component plots due to RooBinWidthFunction
    The RooHistFunc is used in the RooBinWidthFunction to access this
    histogram observables in a convenient way, but it should **not** be used
    as an "indirect proxy" to the histogram observables.
    
    The reason why we can't simply use the histFunc as an "indirect proxy" is
    the way HistFactory is implemented. The same RooBinWidthFunction is used
    for all samples (e.g. signal and backgrounds), but uses the RooHistFunc
    of only one of the samples (this is okay because the binnings for all
    samples in the template histogram stack is the same). This entangling of
    the computation graph for the different samples messes up the component
    selection when plotting only some samples with
    `plotOn(..., RooFit::Components(...))`.
    
    The bug affects for example the user in this forum post:
    https://root-forum.cern.ch/t/problems-plotting-individual-components-with-roofit-histfactory
    
    To illustrate the problem, the RooBinWidthFunction in the forum post
    looks likt this:
    ```
    RooBinWidthFunction::BMCorr_model_binWidth[ HistFuncForBinWidth=histoSig_BMCorr_Hist_alphanominal ]
    ```
    The important thing here is that it uses the `histoSig`, but then it's
    also used for the background (`histoBG`) shapes:
    ```
    RooProduct::histoBG_BMCorr_shapes[ histoBG_BMCorr_Hist_alphanominal * BMCorr_model_binWidth ] = 4.495
    ```
    
    This messes up a plot with `Components("*histoSig*")`, as this
    matches also background sample.
    
    It is a regression starting from ROOT 6.26, because the
    RooBinWidthFunction was not there before. This bugfix should be
    backported to the ROOT 6.28 patch release branch.

--- roofit/roofitcore/inc/RooBinWidthFunction.h
+++ roofit/roofitcore/inc/RooBinWidthFunction.h
@@ -24,34 +24,24 @@
 
 class RooBinWidthFunction : public RooAbsReal {
   static bool _enabled;
-  
-public:  
+
+public:
   static void enableClass();
   static void disableClass();
-  static bool isClassEnabled();    
-   
+  static bool isClassEnabled();
+
   /// Create an empty instance.
   RooBinWidthFunction() :
     _histFunc("HistFuncForBinWidth", "Handle to a RooHistFunc, whose bin volumes should be returned.", this,
         /*valueServer=*/true, /*shapeServer=*/true) { }
 
-  /// Create an instance.
-  /// \param name Name to identify the object.
-  /// \param title Title for e.g. plotting.
-  /// \param histFunc RooHistFunc object whose bin widths should be returned.
-  /// \param divideByBinWidth If true, return inverse bin width.
-  RooBinWidthFunction(const char* name, const char* title, const RooHistFunc& histFunc, bool divideByBinWidth) :
-    RooAbsReal(name, title),
-    _histFunc("HistFuncForBinWidth", "Handle to a RooHistFunc, whose bin volumes should be returned.", this, histFunc, /*valueServer=*/true, /*shapeServer=*/true),
-    _divideByBinWidth(divideByBinWidth) { }
+  RooBinWidthFunction(const char* name, const char* title, const RooHistFunc& histFunc, bool divideByBinWidth);
 
   /// Copy an existing object.
   RooBinWidthFunction(const RooBinWidthFunction& other, const char* newname = nullptr) :
     RooAbsReal(other, newname),
     _histFunc("HistFuncForBinWidth", this, other._histFunc),
     _divideByBinWidth(other._divideByBinWidth) { }
-
-  ~RooBinWidthFunction() override { }
 
   std::unique_ptr<RooAbsArg> compileForNormSet(RooArgSet const &normSet, RooFit::Detail::CompileContext & ctx) const override;
 
