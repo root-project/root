7e240961b379e3fb49236bea9258b8722cdc336f
Bertrand Bellenot <bellenot@users.noreply.github.com>
2023-08-21 13:15:57 +0200

    Fix another place

--- tmva/tmva/inc/TMVA/RTensor.hxx
+++ tmva/tmva/inc/TMVA/RTensor.hxx
@@ -214,7 +214,7 @@ public:
    {
       fSize = Internal::GetSizeFromShape(shape);
       fStrides = Internal::ComputeStridesFromShape(shape, layout);
-      fData = &(*container->begin());
+      fData = fContainer->data();
    }
 
    /// \brief Construct a tensor owning data initialized with new container
