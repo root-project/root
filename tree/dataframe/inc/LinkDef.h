3bf30c85c0e5b96faf6aeb518d5f5d9edd1fb87b
siliataider <siliataider@gmail.com>
2023-08-23 15:59:13 +0200

    Add GraphAsymmErrors to DistRDF operations

--- tree/dataframe/inc/LinkDef.h
+++ tree/dataframe/inc/LinkDef.h
@@ -55,6 +55,7 @@
 #pragma link C++ class ROOT::Detail::RDF::RMergeableValue<TH3D>+;
 #pragma link C++ class ROOT::Detail::RDF::RMergeableValue<THnD>+;
 #pragma link C++ class ROOT::Detail::RDF::RMergeableValue<TGraph>+;
+#pragma link C++ class ROOT::Detail::RDF::RMergeableValue<TGraphAsymmErrors>+;
 #pragma link C++ class ROOT::Detail::RDF::RMergeableValue<TStatistic>+;
 #pragma link C++ class ROOT::Detail::RDF::RMergeableValue<TProfile>+;
 #pragma link C++ class ROOT::Detail::RDF::RMergeableValue<TProfile2D>+;
