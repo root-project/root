6223928da38cfb1493c20c6c091c4a7b6b16d180
Axel Naumann <Axel.Naumann@cern.ch>
2023-08-23 10:05:44 +0200

    [ci] Use Ninja as build system.

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -296,7 +296,7 @@ def archive_and_upload(archive_name, prefix):
 @github_log_group("Configure")
 def cmake_configure(options, buildtype):
     result = subprocess_with_log(f"""
-        cmake -S '{WORKDIR}/src' -B '{WORKDIR}/build' {options} -DCMAKE_BUILD_TYPE={buildtype}
+        cmake -S '{WORKDIR}/src' -B '{WORKDIR}/build' {options} -DCMAKE_BUILD_TYPE={buildtype} -G Ninja
     """)
 
     if result != 0:
