10df7007023d225fb5ecf8ee926e298d21415c4d
Axel Naumann <Axel.Naumann@cern.ch>
2023-09-04 12:48:32 +0200

    Revert "[ci] Use Ninja as build system everywhere but Windows."
    Ninja shows dependency issues in our build system; fix those first before
    switching to Ninja.xs
    
    This reverts commit fcfca4ccdaf97099a180e154c60bc3c621abfafe.

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -295,9 +295,8 @@ def archive_and_upload(archive_name, prefix):
 
 @github_log_group("Configure")
 def cmake_configure(options, buildtype):
-    generator = "" if WINDOWS else "-G Ninja"
     result = subprocess_with_log(f"""
-        cmake -S '{WORKDIR}/src' -B '{WORKDIR}/build' {options} -DCMAKE_BUILD_TYPE={buildtype} {generator}
+        cmake -S '{WORKDIR}/src' -B '{WORKDIR}/build' {options} -DCMAKE_BUILD_TYPE={buildtype}
     """)
 
     if result != 0:
