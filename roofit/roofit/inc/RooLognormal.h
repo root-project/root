807e256fecd59656fa7f8065a3dba680a6dd1a0f
Carsten Burgard <carsten.burgard@gmail.com>
2023-09-08 12:07:41 +0200

    [RF] Added a few getters and setters

--- roofit/roofit/inc/RooLognormal.h
+++ roofit/roofit/inc/RooLognormal.h
@@ -11,19 +11,16 @@
 #ifndef ROO_LOGNORMAL
 #define ROO_LOGNORMAL
 
-#include "RooAbsPdf.h"
-#include "RooRealProxy.h"
-
-class RooRealVar;
+#include <RooAbsPdf.h>
+#include <RooRealProxy.h>
 
 class RooLognormal : public RooAbsPdf {
 public:
-  RooLognormal() {} ;
+  RooLognormal() {}
   RooLognormal(const char *name, const char *title,
          RooAbsReal& _x, RooAbsReal& _m0, RooAbsReal& _k);
   RooLognormal(const RooLognormal& other, const char* name=nullptr) ;
   TObject* clone(const char* newname) const override { return new RooLognormal(*this,newname); }
-  inline ~RooLognormal() override { }
 
   Int_t getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars, const char* rangeName=nullptr) const override ;
   double analyticalIntegral(Int_t code, const char* rangeName=nullptr) const override ;
@@ -35,11 +32,20 @@ public:
   std::string
   buildCallToAnalyticIntegral(int code, const char *rangeName, RooFit::Detail::CodeSquashContext &ctx) const override;
 
+  /// Get the x variable.
+  RooAbsReal const& getX() const { return x.arg(); }
+
+  /// Get the median parameter.
+  RooAbsReal const& getMedian() const { return m0.arg(); }
+
+  /// Get the shape parameter.
+  RooAbsReal const& getShapeK() const { return k.arg(); }
+
 protected:
 
-  RooRealProxy x ;
-  RooRealProxy m0 ;
-  RooRealProxy k ;
+  RooRealProxy x;  ///< the variable
+  RooRealProxy m0; ///< the median, exp(mu)
+  RooRealProxy k;  ///< the shape parameter, exp(sigma)
 
   double evaluate() const override ;
   void computeBatch(double* output, size_t nEvents, RooFit::Detail::DataMap const&) const override;
