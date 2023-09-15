d50187999bd93d07b3894693b1d795eac3694645
Axel Naumann <Axel.Naumann@cern.ch>
2023-09-15 15:21:46 +0200

    [build] Do not include `etc/gitinfo.txt` in source tarball:
    It can be generated at buildtime.

--- build/unix/makedistsrc.sh
+++ build/unix/makedistsrc.sh
@@ -6,15 +6,6 @@ ROOTSRCDIR=$3
 
 TARFILE=root_v$FILEVERS.source.tar
 
-( cd $ROOTSRCDIR; git archive -v -o ../$TARFILE --prefix=root-$FILEVERS/ $GITTAG )
-
-mkdir -p etc/root-$FILEVERS/etc
-cp etc/gitinfo.txt etc/root-$FILEVERS/etc/
-cd etc
-tar -r -vf ../../$TARFILE root-$FILEVERS/etc/gitinfo.txt
-cd ..
-rm -rf etc/root-$FILEVERS
-cd ..
-gzip $TARFILE
-
-exit 0
+cd $ROOTSRCDIR \
+&& git archive -v -o ../$TARFILE --prefix=root-$FILEVERS/ $GITTAG \
+&& gzip $TARFILE
