b49ca566e287564092649bd33ec0a3c30cefa6d1
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-09-29 04:51:17 +0200

    [cmake] Migrate to `XRootDConfig.cmake` shipped by XRootD
    Migrates finding XRootD to the `XRootDConfig.cmake` from XRootD itself.

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -988,7 +988,7 @@ endforeach()
 
 if(xrootd AND NOT builtin_xrootd)
   message(STATUS "Looking for XROOTD")
-  find_package(XROOTD)
+  find_package(XRootD)
   if(NOT XROOTD_FOUND)
     if(fail-on-missing)
       message(FATAL_ERROR "XROOTD not found. Set environment variable XRDSYS to point to your XROOTD installation, "
