eb911c6c891dd4dc18b210dfa639db7c2323115d
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2023-10-03 09:32:08 +0200

    [df] Avoid clearing sample callbacks after event loop
    Sample callbacks can be registered by an RAction or an RDefinePerSample
    instance. In both cases, the lifetime of the callback is tied to the lifetime of
    the object itself. Avoid eager clearing of the callbacks so to not interfer with
    the normal functioning.

--- tree/dataframe/src/RLoopManager.cxx
+++ tree/dataframe/src/RLoopManager.cxx
@@ -761,7 +761,6 @@ void RLoopManager::CleanUpNodes()
 
    fCallbacksEveryNEvents.clear();
    fCallbacksOnce.clear();
-   fSampleCallbacks.clear();
 }
 
 /// Perform clean-up operations. To be called at the end of each task execution.
