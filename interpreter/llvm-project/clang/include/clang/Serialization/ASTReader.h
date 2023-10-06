2e2385386367c1211f18083aa1e53b4a92cf7dd7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-10-06 14:08:38 +0200

    [llvm-project] Introduce paged vector
    The goal of the class is to be an (almost) drop in replacement for
    SmallVector and std::vector when those are presized and filled later, as
    it happens in SourceManager and ASTReader.
    
    By doing so, sparsely accessed PagedVector can profit from reduced
    memory footprint.
    
    Co-authored-by: Jonas Hahnfeld <jonas.hahnfeld@cern.ch>

--- interpreter/llvm-project/clang/include/clang/Serialization/ASTReader.h
+++ interpreter/llvm-project/clang/include/clang/Serialization/ASTReader.h
@@ -18,6 +18,7 @@
 #include "clang/Basic/DiagnosticOptions.h"
 #include "clang/Basic/IdentifierTable.h"
 #include "clang/Basic/OpenCLOptions.h"
+#include "clang/Basic/PagedVector.h"
 #include "clang/Basic/SourceLocation.h"
 #include "clang/Basic/Version.h"
 #include "clang/Lex/ExternalPreprocessorSource.h"
@@ -485,7 +486,7 @@ private:
   ///
   /// When the pointer at index I is non-NULL, the type with
   /// ID = (I + 1) << FastQual::Width has already been loaded
-  std::vector<QualType> TypesLoaded;
+  llvm::PagedVector<QualType> TypesLoaded;
 
   using GlobalTypeMapType =
       ContinuousRangeMap<serialization::TypeID, ModuleFile *, 4>;
@@ -499,7 +500,7 @@ private:
   ///
   /// When the pointer at index I is non-NULL, the declaration with ID
   /// = I + 1 has already been loaded.
-  std::vector<Decl *> DeclsLoaded;
+  llvm::PagedVector<Decl *> DeclsLoaded;
 
   using GlobalDeclMapType =
       ContinuousRangeMap<serialization::DeclID, ModuleFile *, 4>;
