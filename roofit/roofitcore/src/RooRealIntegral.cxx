ab8538ee4fc115a9d03610b410d72c0ae601f02b
Jonas Rembser <jonas.rembser@cern.ch>
2023-10-06 02:39:23 +0200

    [RF] Support PDFs produced by `RooClassFactory` in codegen + AD
    This also includes rudimentary support for numeric integrals in 1D with
    the codegen backend as a demonstrator.

--- roofit/roofitcore/src/RooRealIntegral.cxx
+++ roofit/roofitcore/src/RooRealIntegral.cxx
@@ -28,25 +28,26 @@ The actual analytical integrations (if any) are done in the PDF themselves, the 
 integration is performed in the various implementations of the RooAbsIntegrator base class.
 **/
 
-#include "RooRealIntegral.h"
+#include <RooRealIntegral.h>
 
-#include "RooMsgService.h"
-#include "RooArgSet.h"
-#include "RooAbsRealLValue.h"
-#include "RooAbsCategoryLValue.h"
-#include "RooRealBinding.h"
-#include "RooInvTransform.h"
-#include "RooSuperCategory.h"
-#include "RooNumIntFactory.h"
-#include "RooNumIntConfig.h"
-#include "RooNameReg.h"
-#include "RooExpensiveObjectCache.h"
-#include "RooConstVar.h"
-#include "RooDouble.h"
-#include "RooTrace.h"
-#include "RooHelpers.h"
+#include <RooAbsCategoryLValue.h>
+#include <RooAbsRealLValue.h>
+#include <RooArgSet.h>
+#include <RooConstVar.h>
+#include <RooDouble.h>
+#include <RooExpensiveObjectCache.h>
+#include <RooFuncWrapper.h>
+#include <RooHelpers.h>
+#include <RooInvTransform.h>
+#include <RooMsgService.h>
+#include <RooNameReg.h>
+#include <RooNumIntConfig.h>
+#include <RooNumIntFactory.h>
+#include <RooRealBinding.h>
+#include <RooSuperCategory.h>
+#include <RooTrace.h>
 
-#include "TClass.h"
+#include <TClass.h>
 
 #include <iostream>
 #include <memory>
@@ -1088,14 +1089,71 @@ void RooRealIntegral::setAllowComponentSelection(bool allow){
 
 void RooRealIntegral::translate(RooFit::Detail::CodeSquashContext &ctx) const
 {
-   if (!_sumList.empty() || !_intList.empty()) {
+   if (_sumList.empty() && _intList.empty()) {
+      ctx.addResult(this, _function.arg().buildCallToAnalyticIntegral(_mode, RooNameReg::str(_rangeName), ctx));
+      return;
+   }
+
+   if (intVars().size() != 1 || _intList.size() != 1) {
       std::stringstream errorMsg;
-      errorMsg << "Only analytical integrals are supported for AD for class" << _function.GetName();
+      errorMsg << "Only analytical integrals and 1D numeric integrals are supported for AD for class"
+               << _function.GetName();
       coutE(Minimization) << errorMsg.str() << std::endl;
       throw std::runtime_error(errorMsg.str().c_str());
    }
 
-   ctx.addResult(this, _function.arg().buildCallToAnalyticIntegral(_mode, RooNameReg::str(_rangeName), ctx));
+   auto &intVar = static_cast<RooAbsRealLValue &>(*_intList[0]);
+
+   std::string wrapperName = ctx.makeValidVarName(std::string{GetName()} + "_wrapper");
+
+   RooFuncWrapper wrapper{wrapperName.c_str(), GetTitle(), *_function, {}, nullptr, nullptr, false};
+
+   RooArgSet params;
+   _function->getParameters(nullptr, params);
+
+   std::string paramsName = ctx.getTmpVarName();
+
+   std::stringstream ss;
+
+   std::string resName = ctx.makeValidVarName(GetName()) + "Result";
+   ctx.addResult(this, resName);
+   ctx.addToGlobalScope("double " + resName + " = 0.0;\n");
+
+   ss  << "double " << paramsName << "[] = {";
+   std::string args;
+   int intVarIdx = 0;
+   for (RooAbsArg *param : params) {
+      // Fill the integration variable with dummy value for now. This will then
+      // be reset in the sampling loop.
+      if (param->namePtr() == intVar.namePtr()) {
+         args += "0.0,";
+      } else if (!param->isConstant()) {
+         args += ctx.getResult(*param) + ",";
+         intVarIdx++;
+      }
+   }
+   if (!args.empty()) {
+      args.pop_back();
+   }
+   ss << args << "};\n";
+
+   // TODO: once Clad has support for higher-order functions (follow also the
+   // Clad issue #637), we could refactor this code into an actual function
+   // instead of hardcoding it here as a string.
+   ss << "{\n"
+      << "   const int n = 1000; // number of sampling points\n"
+      << "   double d = " << intVar.getMax(intRange()) << " - " << intVar.getMin(intRange()) << ";\n"
+      << "   double eps = d / n;\n"
+      << "   for (int i = 0; i < n; ++i) {\n"
+      << "      " << paramsName << "[" << intVarIdx << "] = " << intVar.getMin(intRange()) << " + eps * i;\n"
+      // TODO: the second "paramsName" should be nullptr, but until Clad issue
+      // 636 is fixed, we have to pass a non-nullptr dummy.
+      << "      " << resName << " += " << " + " << ctx.buildCall(wrapperName, paramsName, paramsName) << ";\n"
+      << "   }\n"
+      << "   " << resName << " *= " << " d / n;\n"
+      << "}\n";
+
+   ctx.addToGlobalScope(ss.str());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
