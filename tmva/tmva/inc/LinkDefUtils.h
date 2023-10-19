d5c0f480dcf9c8d1f73b8d282ffcf1b8adce7032
Jonas Rembser <jonas.rembser@cern.ch>
2023-10-19 21:27:14 +0200

    [TMVA] Merge RBDT with FastForest
    Consolidate RBDT as specified in the ROOT plan of work 2024.
    
    The backends of RBDT are replaced with a single new backend:
    the logic from the FastForest library:
    https://github.com/guitargeek/XGBoost-FastForest
    
    The logic in that library was originally taken from the GBRForest in
    CMSSW:
    https://github.com/cms-sw/cmssw/blob/master/CommonTools/MVAUtils/interface/GBRForestTools.h
    
    The interface remains the same, only that the template parameter
    specifying the backend is gone.
    
    This change adds support for unbalanced trees.

--- tmva/tmva/inc/LinkDefUtils.h
+++ tmva/tmva/inc/LinkDefUtils.h
@@ -11,8 +11,7 @@
 
 #ifdef R__HAS_DATAFRAME
 // BDT inference
-#pragma link C++ class TMVA::Experimental::RBDT<TMVA::Experimental::BranchlessForest<float>>+;
-#pragma link C++ class TMVA::Experimental::RBDT<TMVA::Experimental::BranchlessJittedForest<float>>+;
+#pragma link C++ class TMVA::Experimental::RBDT+;
 #endif
 
 // RTensor will have its own streamer function
