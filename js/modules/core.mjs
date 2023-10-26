37363aa587204f9b9e3b45989ad93fbf42898ad4
Sergey Linev <S.Linev@gsi.de>
2023-10-26 11:02:16 +0200

    [jsroot] 7.5.x 26/10/2023 with proper stats box size

--- js/modules/core.mjs
+++ js/modules/core.mjs
@@ -4,7 +4,7 @@ const version_id = '7.5.x',
 
 /** @summary version date
   * @desc Release date in format day/month/year like '14/04/2022' */
-version_date = '25/10/2023',
+version_date = '26/10/2023',
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
