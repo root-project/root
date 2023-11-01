50c2f52665a079259a6b04d1afa1519ebe69b550
Sergey Linev <S.Linev@gsi.de>
2023-11-01 17:58:26 +0100

    Adjust GL tutorials using transparent colors
    Now these tutorials can be used with web-based canvas or
    even with normal graphics - with created gradient colors.
    In normal graphics gradient and alpha are ignored.
    
    Remove customcolorgl.h utility while now colors can be
    created much-much easily with TColor::GetColor method

--- tutorials/gl/transp_text.C
+++ tutorials/gl/transp_text.C
@@ -2,14 +2,12 @@
 /// \ingroup tutorial_gl
 /// This macro is based on labels1.C by Rene Brun.
 /// Updated by Timur Pocheptsov to use transparent text.
-/// The macro requires OpenGL - either enable by
-/// `gStyle->SetCanvasPreferGL(kTRUE);` or by setting
-/// `OpenGL.CanvasPreferGL` to 1 in `$ROOTSYS/etc/system.rootrc`.
+/// The macro requires OpenGL or Web-based canvas
 ///
 /// \macro_image(nobatch)
 /// \macro_code
 ///
-/// \author Timur Pocheptsov
+/// \authors Timur Pocheptsov, Sergey Linev
 
 //Includes for ACLiC (cling does not need them).
 #include "TPaveText.h"
@@ -20,32 +18,18 @@
 #include "TStyle.h"
 #include "TH1F.h"
 
-//Aux. functions for tutorials/gl.
-#include "customcolorgl.h"
-
-void transp_text()
+void transp_text(bool gl = true)
 {
-   //1. Try to 'allocate' free indices for our custom colors -
-   //we can use hard-coded indices like 1001, 1002, 1003 ... but
-   //I prefer to find free indices in a ROOT's color table
-   //to avoid possible conflicts with other tutorials.
-   Int_t indices[2] = {};
-   if (ROOT::GLTutorials::FindFreeCustomColorIndices(indices) != 2) {
-      Error("transp_text", "failed to create new custom colors");
-      return;
-   }
+   // 1. Create special transparent colors for both pavetext fill color and text color.
+   auto grayColorIndex = TColor::GetColor((Float_t) 0.8, 0.8, 0.8, 0.85);
+   auto blackColorIndex = TColor::GetColor((Float_t) 0., 0., 0., 0.5);
 
-   //2. Create special transparent colors for both pavetext fill color and text color.
-   const Int_t grayColorIndex = indices[0], blackColorIndex = indices[1];
-   new TColor(grayColorIndex, 0.8, 0.8, 0.8, "transparent_gray", 0.85);
-   new TColor(blackColorIndex, 0., 0., 0., "transparent_black", 0.5);
+   // 2. Create a TCanvas.
+   gStyle->SetCanvasPreferGL(gl);
 
-   //3. Create a TCanvas.
-   gStyle->SetCanvasPreferGL(kTRUE);
-
-   TCanvas * const c1 = new TCanvas("transparent text","transparent text demo", 10, 10, 900, 500);
-   if (!c1->UseGL())
-      ::Warning("transp_text", "to use this macro you need OpenGL");
+   auto c1 = new TCanvas("transp_text","transparent text demo", 10, 10, 900, 500);
+   if (!c1->UseGL() && !c1->IsWeb())
+      ::Warning("transp_text", "to use this macro you need either OpenGL or Web");
 
    c1->SetGrid();
    c1->SetBottomMargin(0.15);
@@ -56,7 +40,7 @@ void transp_text()
       "Bjarne","Anton","Otto","Eddy","Peter","Pasha",
       "Philippe","Suzanne","Jeff","Valery"};
 
-   TH1F * const h = new TH1F("h4", "test", nx, 0, nx);
+   auto h = new TH1F("h4", "test", nx, 0, nx);
 
    h->SetFillColor(38);
    for (Int_t i = 0; i < 5000; ++i)
@@ -68,7 +52,7 @@ void transp_text()
 
    h->Draw();
 
-   TPaveText * const pt = new TPaveText(0.3, 0.3, 0.98, 0.98, "brNDC");
+   auto pt = new TPaveText(0.3, 0.3, 0.98, 0.98, "brNDC");
    //Transparent 'rectangle' with transparent text.
    pt->SetFillColor(grayColorIndex);
    pt->SetTextColor(blackColorIndex);
--- tutorials/gl/transparentpad.C
+++ tutorials/gl/transparentpad.C
@@ -1,12 +1,12 @@
 /// \file
 /// \ingroup tutorial_gl
 /// This macro demonstrates semi-transparent pads.
-/// Requires OpenGL.
+/// Requires OpenGL or Web-based canvas.
 ///
 /// \macro_image(nobatch)
 /// \macro_code
 ///
-/// \author Timur Pocheptsov
+/// \authors Timur Pocheptsov, Sergey Linev
 
 //Includes for ACLiC (cling does not need them).
 #include "TCanvas.h"
@@ -15,66 +15,46 @@
 #include "TColor.h"
 #include "TH1F.h"
 
-//Aux. functions for tutorials/gl.
-#include "customcolorgl.h"
-
-void transparentpad()
+void transparentpad(bool gl = true)
 {
-   //1. Try to 'allocate' free indices for our custom colors -
-   //we can use hard-coded indices like 1001, 1002, 1003 ... but
-   //I prefer to find free indices in a ROOT's color table
-   //to avoid possible conflicts with other tutorials.
-   gStyle->SetCanvasPreferGL(kTRUE);
+   gStyle->SetCanvasPreferGL(gl);
 
-   Int_t indices[3] = {};
-   if (ROOT::GLTutorials::FindFreeCustomColorIndices(indices) != 3) {
-      ::Error("transparentpad", "failed to create new custom colors");
-      return;
-   }
-
-   //2. Create a TCanvas.
-   TCanvas * const c1 = new TCanvas("transparent pad","transparent pad demo", 10, 10, 900, 500);
-   //We can check gVirtualX (its type):
-   if (!c1->UseGL()) {
+   // 1. Create canvas and check if it support transparent colors
+   auto c1 = new TCanvas("transparentpad","transparent pad demo", 10, 10, 900, 500);
+   if (!c1->UseGL() && !c1->IsWeb())
       ::Warning("transparentpad", "You can see the transparency ONLY in a pdf or png output (\"File\"->\"Save As\" ->...)\n"
-                                  "To have transparency in a canvas graphics, you need OpenGL rendering enabled");
-   }
+                                  "To have transparency in a canvas graphics, you need either OpenGL or Web rendering enabled");
 
-   //2. Create special transparent colors.
-   new TColor(indices[0], 1., 0.2, 0.2, "transparent_pink", 0.25);
-   new TColor(indices[1], 0.2, 1., 0.2, "transparent_green", 0.25);
-   new TColor(indices[2], 0.2, 2., 1., "transparent_blue", 0.15);
-
-   //3. Some arbitrary histograms.
-   TH1F * const h1 = new TH1F("TH1F 1", "TH1F 1", 100, -1.5, 1.5);
+   // 2. Some arbitrary histograms.
+   auto h1 = new TH1F("TH1F 1", "TH1F 1", 100, -1.5, 1.5);
    h1->FillRandom("gaus");
 
-   TH1F * const h2 = new TH1F("TH1F 2", "TH1F 2", 100, -1.5, 0.);
+   auto h2 = new TH1F("TH1F 2", "TH1F 2", 100, -1.5, 0.);
    h2->FillRandom("gaus");
 
-   TH1F * const h3 = new TH1F("TH1F 3", "TH1F 3", 100, 0.5, 2.);
+   auto h3 = new TH1F("TH1F 3", "TH1F 3", 100, 0.5, 2.);
    h3->FillRandom("landau");
 
-   //4. Now overlapping transparent pads.
-   TPad * const pad1 = new TPad("transparent pad 1", "transparent pad 1", 0.1, 0.1, 0.7, 0.7);
-   pad1->SetFillColor(indices[0]);//here's the magic!
-   pad1->cd();
-   h1->Draw("lego2");
+   // 3. Now overlapping transparent pads.
+   auto pad1 = new TPad("transparent pad 1", "transparent pad 1", 0.1, 0.1, 0.7, 0.7);
+   pad1->SetFillColor(TColor::GetColor((Float_t) 1., 0.2, 0.2, 0.25)); // transparent pink, here's the magic!
    c1->cd();
    pad1->Draw();
+   pad1->cd();
+   h1->Draw("lego2");
 
-   TPad * const pad2 = new TPad("transparent pad 2", "transparent pad 2", 0.2, 0.2, 0.8, 0.8);
-   pad2->SetFillColor(indices[1]);//here's the magic!
-   pad2->cd();
-   h2->Draw();
+   auto pad2 = new TPad("transparent pad 2", "transparent pad 2", 0.2, 0.2, 0.8, 0.8);
+   pad2->SetFillColor(TColor::GetColor((Float_t) 0.2, 1., 0.2, 0.25)); // transparent green, here's the magic!
    c1->cd();
    pad2->Draw();
+   pad2->cd();
+   h2->Draw();
 
-   TPad * const pad3 = new TPad("transparent pad 3", "transparent pad 3", 0.3, 0.3, 0.9, 0.9);
-   pad3->SetFillColor(indices[2]);//here's the magic!
-   pad3->cd();
-   h3->Draw();
+   auto pad3 = new TPad("transparent pad 3", "transparent pad 3", 0.3, 0.3, 0.9, 0.9);
+   pad3->SetFillColor(TColor::GetColor((Float_t) 0.2, 1., 1., 0.15)); // transparent blue, here's the magic!
    c1->cd();
    pad3->Draw();
+   pad3->cd();
+   h3->Draw();
 }
 
