7af9cdc262b7a0cf2679a0df6c639c93656958c0
Axel Naumann <Axel.Naumann@cern.ch>
2023-11-06 17:45:30 +0100

    [foundation] Update version file to 6.30.01: dev phase for upcoming patch release.

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 30
-#define ROOT_VERSION_PATCH 00
+#define ROOT_VERSION_PATCH 01
 #define ROOT_RELEASE_DATE "Nov 06 2023"
 
 /* Don't change the lines below. */
