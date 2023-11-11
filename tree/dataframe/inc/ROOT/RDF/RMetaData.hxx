8ef4bd7013fe549fe607a6845efdec8bc9448e7a
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-11-11 13:14:02 +0100

    [DF] Avoid public dependency of RDataFrame on `nlohmann::json`
    It's better if the RDataFrame doesn't depend on `nlohmann::json` for
    this reason:
    
    If you build ROOT **without** the builtin nlohmannjson, it finds the
    json header with `find_package(nlohmann_json)`, which is fine.
    
    However, if the `nlohmann/json.hpp` header is not in the default include
    path, one is now forced to also do `find_package(nlohmann_json)` in
    every compiled project that uses RDataFrame!
    
    This happened to one of our students recently, which is why I got aware
    of the problem. On most Linux systems, this is not a problem because
    `json.hpp` is in a standard location, but for macOS this doesn't seem to
    be the case necessarily.

--- tree/dataframe/inc/ROOT/RDF/RMetaData.hxx
+++ tree/dataframe/inc/ROOT/RDF/RMetaData.hxx
@@ -11,10 +11,21 @@
 #ifndef ROOT_RDF_RMETADATA
 #define ROOT_RDF_RMETADATA
 
-#include <nlohmann/json.hpp>
 #include <string>
+#include <memory>
 
 namespace ROOT {
+
+namespace Internal {
+namespace RDF {
+// To avoid unnecessary dependence on nlohman json in the interface. Note that
+// we should not forward declare nlohmann::json directly, since its declaration
+// might change (it is currently a typedef). With this wrapper type, we are
+// completely decoupled on nlohmann::json in the RMetaData interface.
+struct RMetaDataJson;
+}
+}
+
 namespace RDF {
 namespace Experimental {
 
@@ -27,9 +38,17 @@ namespace Experimental {
  Once a dataframe is built with RMetaData object, it could be accessed via DefinePerSample.
 */
 class RMetaData {
-   nlohmann::json fJson;
-
 public:
+
+   RMetaData();
+   // Note: each RMetaData instance should own its own fJson object, just as if
+   // the underlying nlohmann::json object would be owned by value.
+   RMetaData(RMetaData const&);
+   RMetaData(RMetaData &&);
+   RMetaData & operator=(RMetaData const&);
+   RMetaData & operator=(RMetaData &&);
+   ~RMetaData();
+
    void Add(const std::string &key, int val);
    void Add(const std::string &key, double val);
    void Add(const std::string &key, const std::string &val);
@@ -41,6 +60,9 @@ public:
    int GetI(const std::string &key, int defaultVal) const;
    double GetD(const std::string &key, double defaultVal) const;
    const std::string GetS(const std::string &key, const std::string &defaultVal) const;
+
+private:
+   std::unique_ptr<Internal::RDF::RMetaDataJson> fJson;
 };
 
 } // namespace Experimental
