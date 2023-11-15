54fde87d0b432cf8f0bf707c25bc696874a5e27a
Sergey Linev <S.Linev@gsi.de>
2023-11-15 16:27:52 +0100

    [webgui] improve handling of $profile argument
    Avoid double slash,
    Correctly replace it in exec string
    Catch other singals in trap command of runfirefox.sh

--- etc/runfirefox.sh
+++ etc/runfirefox.sh
@@ -8,7 +8,7 @@ firefox=$1
 shift
 
 if [ "$profile" != "<dummy>" ]; then
-   trap "rm -rf $profile; echo remove $profile at exit" EXIT   
+   trap "rm -rf $profile; echo remove $profile at exit" 0 1 2 3 6
 fi
 
 args=
