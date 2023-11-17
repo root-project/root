8614b4ea8eab84b355d0f7deed4a5cee39a4a4d0
Florine de Geus <florine.willemijn.de.geus@cern.ch>
2023-11-17 13:57:48 +0100

    [ntuple] Move STL dictionary entries to separate file

---
+++ tree/ntuple/v7/test/ProxiedSTLContainer.hxx
@@ -0,0 +1,9 @@
+#ifndef ROOT7_RNTuple_Test_ProxiedSTLContainer
+#define ROOT7_RNTuple_Test_ProxiedSTLContainer
+
+#include "CustomStruct.hxx"
+
+#include <set>
+#include <unordered_set>
+
+#endif // ROOT7_RNTuple_Test_ProxiedSTLContainer
