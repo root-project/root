515a162560df6f29cc97564e98f7007813d3eb33
Jonas Rembser <jonas.rembser@cern.ch>
2023-11-23 16:50:56 +0100

    [CMake] Always build Minuit2
    In the discussion thread to JIRA ticket ROOT-10864, Axel suggested the
    removal of the option to disable minuit2 as a possible solution.
    
    I think this is a very good idea good idea, because:
    
      * Minuit2 only depends on `Hist` and `MathCore` anyway.
    
      * Now that Minuit2 is the default for *all* minimization in ROOT,
        building ROOT without Minuit2 changes the behavior of ROOT way too
        much, in a ways that people that set `minuit2=OFF` probably are not
        intending or aware of.
    
    This closes the following JIRA ticket:
    https://sft.its.cern.ch/jira/browse/ROOT-10864

--- cmake/modules/RootBuildOptions.cmake
+++ cmake/modules/RootBuildOptions.cmake
@@ -143,7 +143,7 @@ ROOT_BUILD_OPTION(macos_native OFF "Disable looking for libraries, includes and 
 ROOT_BUILD_OPTION(mathmore OFF "Build libMathMore extended math library (requires GSL) [GPL]")
 ROOT_BUILD_OPTION(memory_termination OFF "Free internal ROOT memory before process termination (experimental, used for leak checking)")
 ROOT_BUILD_OPTION(mlp ON "Enable support for TMultilayerPerceptron classes' federation")
-ROOT_BUILD_OPTION(minuit2 ON "Build Minuit2 minimization library")
+ROOT_BUILD_OPTION(minuit2 IGNORE "Build Minuit2 minimization library (deprecated, Minuit2 is always built)")
 ROOT_BUILD_OPTION(minuit2_mpi OFF "Enable support for MPI in Minuit2")
 ROOT_BUILD_OPTION(minuit2_omp OFF "Enable support for OpenMP in Minuit2")
 ROOT_BUILD_OPTION(mpi OFF "Enable support for Message Passing Interface (MPI)")
@@ -241,7 +241,6 @@ if(all)
  set(fcgi_defvalue ON)
  set(imt_defvalue ON)
  set(mathmore_defvalue ON)
- set(minuit2_defvalue ON)
  set(mlp_defvalue ON)
  set(monalisa_defvalue ON)
  set(mysql_defvalue ON)
@@ -375,12 +374,6 @@ endif()
 #---Define at moment the options with the selected default values------------------------------
 ROOT_APPLY_OPTIONS()
 
-# RooFit multiprocess only works with Minuit2. In fact, it depends on it at build time.
-if(roofit_multiprocess AND NOT minuit2)
-    message(WARNING "Option 'roofit_multiprocess' requires option `minuit2`. We're setting `minuit2=ON` for you. Consider setting `minuit2=ON` yourself to silence this warning.")
-    set(minuit2 ON CACHE BOOL "" FORCE)
-endif()
-
 #---roottest option implies testing
 if(roottest OR rootbench)
   set(testing ON CACHE BOOL "" FORCE)
@@ -422,7 +415,7 @@ foreach(opt afdsmgrd afs alien bonjour castor chirp cxx11 cxx14 cxx17 geocad gfa
 endforeach()
 
 #---Deprecated options------------------------------------------------------------------------
-foreach(opt cxxmodules exceptions oracle pythia6 pythia6_nolink pyroot-python2)
+foreach(opt cxxmodules exceptions oracle pythia6 pythia6_nolink pyroot-python2 minuit2)
   if(${opt})
     message(DEPRECATION ">>> Option '${opt}' is deprecated and will be removed in the next release of ROOT. Please contact root-dev@cern.ch should you still need it.")
   endif()
