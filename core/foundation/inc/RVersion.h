ad63ceea241da7ebbd162df8dc12fa9b31d5275a
Danilo Piparo <danilo.piparo@cern.ch>
2023-11-27 21:46:05 +0100

    "Update ROOT version files to v6.28/11."

--- core/foundation/inc/RVersion.h
+++ core/foundation/inc/RVersion.h
@@ -14,10 +14,10 @@
  *
 */
 
-#define ROOT_RELEASE "6.28/10"
+#define ROOT_RELEASE "6.28/11"
 #define ROOT_RELEASE_DATE "Nov 27 2023"
-#define ROOT_RELEASE_TIME "21:38:12"
+#define ROOT_RELEASE_TIME "21:46:04"
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define ROOT_VERSION_CODE ROOT_VERSION(6,28,10) /* 400394 */
+#define ROOT_VERSION_CODE ROOT_VERSION(6,28,11) /* 400395 */
 
 #endif
