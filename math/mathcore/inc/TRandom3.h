2c353ba67c9accaf6b0387515b7f7bf852c99f67
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2023-11-30 12:11:27 +0100

    Update doc in TRandom3::GetSeed
    Document better what GetSeed returns for TRandom3

--- math/mathcore/inc/TRandom3.h
+++ math/mathcore/inc/TRandom3.h
@@ -33,8 +33,9 @@ private:
 public:
    TRandom3(UInt_t seed=4357);
    ~TRandom3() override;
-   /// return current element of the state used for generate the random number
-   /// Note that it is not the seed of the generator that was used in the SetSeed function
+    /// Return one element of the generator state used to generate the random numbers.
+    /// Note that it is not the seed of the generator that was used in the SetSeed function and 
+    /// the full state (624 numbers) is required to define the generator and have a reproducible output.
     UInt_t    GetSeed() const override { return fMt[fCount624 % 624];}
    using TRandom::Rndm;
     Double_t  Rndm( ) override;
