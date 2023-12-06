7e69eb9ada3b8f85d9bc180ac7137cd9d198ca67
Sergey Linev <S.Linev@gsi.de>
2023-12-06 17:29:11 +0100

    Disable WebGui.Display rootrc parameter

--- core/base/src/TROOT.cxx
+++ core/base/src/TROOT.cxx
@@ -817,9 +817,19 @@ TROOT::TROOT(const char *name, const char *title, VoidFuncPtr_t *initfunc)
    TStyle::BuildStyles();
    SetStyle(gEnv->GetValue("Canvas.Style", "Modern"));
 
+   // this is required by rootssh
    const char *webdisplay = gSystem->Getenv("ROOT_WEBDISPLAY");
-   if (!webdisplay || !*webdisplay)
-      webdisplay = gEnv->GetValue("WebGui.Display", "");
+   // not allow to configure web display via rootrc
+   if (!webdisplay || !*webdisplay) {
+      const char *webdisplay_rc = gEnv->GetValue("WebGui.Display", "");
+      if (webdisplay_rc && *webdisplay_rc && (strcmp(webdisplay_rc, "off") != 0)) {
+         printf("\nWARNING!\n");
+         printf("rootrc parameter \"WebGui.Display\" is disabled for security reason.\n");
+         printf("See https://root.cern/about/security/#2023-11-26-open-port-for-control-of-web-gui-allows-read-and-write-access-to-file-system for more information.\n");
+         printf("For environments controlling the security issues you can enable web display by calling\n");
+         printf("gROOT->SetWebDisplay(); in ROOT prompt or in startup scripts\n\n");
+      }
+   }
    if (webdisplay && *webdisplay)
       SetWebDisplay(webdisplay);
 
