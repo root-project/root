6ccf8f9e59f9e47ea0b5b1459e1d45d64f9a131e
Bertrand Bellenot <bellenot@users.noreply.github.com>
2023-12-14 09:10:32 +0100

    Add a safety check on `std::multimap::empty()`
    Prevent a potential crash on Windows in Debug mode when the multimap is empty

--- gui/browsable/inc/ROOT/Browsable/RProvider.hxx
+++ gui/browsable/inc/ROOT/Browsable/RProvider.hxx
@@ -145,6 +145,8 @@ private:
    template<class Map_t>
    void CleanThis(Map_t &fmap)
    {
+      if (fmap.empty())
+         return;
       auto fiter = fmap.begin();
       while (fiter != fmap.end()) {
          if (fiter->second.provider == this)
