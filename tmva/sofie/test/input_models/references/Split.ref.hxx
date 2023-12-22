4224c4b53be09bfdd37873dbd1c0aeb415597cc4
Neel-Shah-29 <neelshah29042002@gmail.com>
2023-12-22 20:57:54 +0530

    Split test updated for Equal Split condition. Dynamic split support will be added later.

--- tmva/sofie/test/input_models/references/Split.ref.hxx
+++ tmva/sofie/test/input_models/references/Split.ref.hxx
@@ -1,4 +1,4 @@
 namespace Split_ExpectedOutput{
-   float output[2][4] = { {1.0,2.0},
-                    {3.0,4.0,5.0,6.0}};
+   float output[3][3] = { {1.0,2.0, 3.0},
+                           {4.0,5.0,6.0}};
 }
