e601bf8032175c63780dcf5fb441bea2fa82bf5a
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-01-08 10:21:54 +0000

    [core] Reduce symbol search only to when autoloading is enabled.
    The llvm9 JIT issued callbacks when a symbol was missing and we reacted on it
    by loading the relevant library. In root-project/root@9b2041e3 we have kept the
    logic but now the JIT started querying more often even for symbols which are
    okay to be missing. In turn that leads to scanning all libraries causing
    performance issues.
    
    This patch tries to limit this functionality only in contexts where automatic
    loading is allowed.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -4726,6 +4726,9 @@ TInterpreter::DeclId_t TCling::GetDataMember(ClassInfo_t *opaque_cl, const char 
    DeclId_t d;
    TClingClassInfo *cl = (TClingClassInfo*)opaque_cl;
 
+   // Could trigger deserialization of decls.
+   cling::Interpreter::PushTransactionRAII RAII(GetInterpreterImpl());
+
    if (cl) {
       d = cl->GetDataMember(name);
       // We check if the decl of the data member has an annotation which indicates
@@ -4756,8 +4759,6 @@ TInterpreter::DeclId_t TCling::GetDataMember(ClassInfo_t *opaque_cl, const char 
    LookupResult R(SemaR, DName, SourceLocation(), Sema::LookupOrdinaryName,
                   Sema::ForExternalRedeclaration);
 
-   // Could trigger deserialization of decls.
-   cling::Interpreter::PushTransactionRAII RAII(GetInterpreterImpl());
    cling::utils::Lookup::Named(&SemaR, R);
 
    LookupResult::Filter F = R.makeFilter();
