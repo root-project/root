06d2f37754c7623774c5162264a52224729f2af4
Michael K. Wilkinson <goi42@users.noreply.github.com>
2024-01-08 14:35:05 -0500

    [TMVA] RReader: allow spectators in xml file (#7899)
    Implements missing feature reported in the forum [here](https://root-forum.cern.ch/t/rreader-and-spectator-variables/42844).
    
    Closes #7900.
    
    ---------
    
    Co-authored-by: Pieter David <pieter.david@gmail.com>

--- tmva/tmva/inc/TMVA/RReader.hxx
+++ tmva/tmva/inc/TMVA/RReader.hxx
@@ -22,12 +22,17 @@ enum AnalysisType : unsigned int { Undefined = 0, Classification, Regression, Mu
 struct XMLConfig {
    unsigned int numVariables;
    std::vector<std::string> variables;
-   std::vector<std::string> expressions;
+   std::vector<std::string> variable_expressions;
+   unsigned int numSpectators;
+   std::vector<std::string> spectators;
+   std::vector<std::string> spectator_expressions;
    unsigned int numClasses;
    std::vector<std::string> classes;
    AnalysisType analysisType;
    XMLConfig()
-      : numVariables(0), variables(std::vector<std::string>(0)), numClasses(0), classes(std::vector<std::string>(0)),
+      : numVariables(0), variables(std::vector<std::string>(0)),
+        numSpectators(0), spectators(std::vector<std::string>(0)),
+        numClasses(0), classes(std::vector<std::string>(0)),
         analysisType(Internal::AnalysisType::Undefined)
    {
    }
@@ -54,11 +59,22 @@ inline XMLConfig ParseXMLConfig(const std::string &filename)
       if (nodeName.compare("Variables") == 0) {
          c.numVariables = std::atoi(xml.GetAttr(node, "NVar"));
          c.variables = std::vector<std::string>(c.numVariables);
-         c.expressions = std::vector<std::string>(c.numVariables);
+         c.variable_expressions = std::vector<std::string>(c.numVariables);
          for (auto thisNode = xml.GetChild(node); thisNode; thisNode = xml.GetNext(thisNode)) {
             const auto iVariable = std::atoi(xml.GetAttr(thisNode, "VarIndex"));
             c.variables[iVariable] = xml.GetAttr(thisNode, "Title");
-            c.expressions[iVariable] = xml.GetAttr(thisNode, "Expression");
+            c.variable_expressions[iVariable] = xml.GetAttr(thisNode, "Expression");
+         }
+      }
+      // Read out input spectators
+      else if (nodeName.compare("Spectators") == 0) {
+         c.numSpectators = std::atoi(xml.GetAttr(node, "NSpec"));
+         c.spectators = std::vector<std::string>(c.numSpectators);
+         c.spectator_expressions = std::vector<std::string>(c.numSpectators);
+         for (auto thisNode = xml.GetChild(node); thisNode; thisNode = xml.GetNext(thisNode)) {
+            const auto iVariable = std::atoi(xml.GetAttr(thisNode, "SpecIndex"));
+            c.spectators[iVariable] = xml.GetAttr(thisNode, "Title");
+            c.spectator_expressions[iVariable] = xml.GetAttr(thisNode, "Expression");
          }
       }
       // Read out output classes
@@ -93,6 +109,11 @@ inline XMLConfig ParseXMLConfig(const std::string &filename)
       ss << "Failed to parse input variables from TMVA config " << filename << ".";
       throw std::runtime_error(ss.str());
    }
+   if (c.numSpectators != c.spectators.size()) {
+      std::stringstream ss;
+      ss << "Failed to parse input spectators from TMVA config " << filename << ".";
+      throw std::runtime_error(ss.str());
+   }
    if (c.numClasses != c.classes.size() || c.numClasses == 0) {
       std::stringstream ss;
       ss << "Failed to parse output classes from TMVA config " << filename << ".";
@@ -115,9 +136,12 @@ inline XMLConfig ParseXMLConfig(const std::string &filename)
 class RReader {
 private:
    std::unique_ptr<Reader> fReader;
-   std::vector<float> fValues;
+   std::vector<float> fVariableValues;
    std::vector<std::string> fVariables;
-   std::vector<std::string> fExpressions;
+   std::vector<std::string> fVariableExpressions;
+   std::vector<float> fSpectatorValues;
+   std::vector<std::string> fSpectators;
+   std::vector<std::string> fSpectatorExpressions;
    unsigned int fNumClasses;
    const char *name = "RReader";
    Internal::AnalysisType fAnalysisType;
@@ -129,16 +153,23 @@ public:
       // Load config
       auto c = Internal::ParseXMLConfig(path);
       fVariables = c.variables;
-      fExpressions = c.expressions;
+      fVariableExpressions = c.variable_expressions;
+      fSpectators = c.spectators;
+      fSpectatorExpressions = c.spectator_expressions;
       fAnalysisType = c.analysisType;
       fNumClasses = c.numClasses;
 
       // Setup reader
       fReader = std::make_unique<Reader>("Silent");
       const auto numVars = fVariables.size();
-      fValues = std::vector<float>(numVars);
+      fVariableValues = std::vector<float>(numVars);
       for (std::size_t i = 0; i < numVars; i++) {
-         fReader->AddVariable(TString(fExpressions[i]), &fValues[i]);
+         fReader->AddVariable(TString(fVariableExpressions[i]), &fVariableValues[i]);
+      }
+      const auto numSpecs = fSpectators.size();
+      fSpectatorValues = std::vector<float>(numSpecs);
+      for (std::size_t i = 0; i < numSpecs; i++) {
+         fReader->AddSpectator(TString(fSpectatorExpressions[i]), &fSpectatorValues[i]);
       }
       fReader->BookMVA(name, path.c_str());
    }
@@ -146,12 +177,16 @@ public:
    /// Compute model prediction on vector
    std::vector<float> Compute(const std::vector<float> &x)
    {
-      if (x.size() != fVariables.size())
+      if (x.size() != (fVariables.size()+fSpectators.size()))
          throw std::runtime_error("Size of input vector is not equal to number of variables.");
 
       // Copy over inputs to memory used by TMVA reader
-      for (std::size_t i = 0; i < x.size(); i++) {
-         fValues[i] = x[i];
+      const auto nVars = fVariables.size();
+      for (std::size_t i = 0; i != nVars ; ++i) {
+         fVariableValues[i] = x[i];
+      }
+      for (std::size_t i = 0; i != fSpectators.size(); ++i) {
+         fSpectatorValues[i] = x[nVars+i];
       }
 
       // Take lock to protect model evaluation
@@ -187,7 +222,7 @@ public:
 
       const auto numEntries = shape[0];
       const auto numVars = shape[1];
-      if (numVars != fVariables.size())
+      if (numVars != (fVariables.size()+fSpectators.size()))
          throw std::runtime_error("Second dimension of input tensor is not equal to number of variables.");
 
       // Define shape of output tensor based on analysis type
@@ -199,9 +234,13 @@ public:
          y = y.Reshape({numEntries, numClasses});
 
       // Fill output tensor
+      const auto nVars = fVariables.size(); // number of non-spectator variables
       for (std::size_t i = 0; i < numEntries; i++) {
-         for (std::size_t j = 0; j < numVars; j++) {
-            fValues[j] = x(i, j);
+         for (std::size_t j = 0; j < nVars; j++) {
+            fVariableValues[j] = x(i, j);
+         }
+         for (std::size_t j = 0; j < fSpectators.size(); ++j) {
+            fSpectatorValues[j] = x(i, nVars+j);
          }
          R__WRITE_LOCKGUARD(ROOT::gCoreMutex);
          // Classification
@@ -224,6 +263,7 @@ public:
    }
 
    std::vector<std::string> GetVariableNames() { return fVariables; }
+   std::vector<std::string> GetSpectatorNames() { return fSpectators; }
 };
 
 } // namespace Experimental
