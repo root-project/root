b6fdc507524ab8ece64860f55bc6489c219401ad
Jonas Rembser <jonas.rembser@cern.ch>
2024-01-11 11:46:18 +0100

    [RF] Clarify in rf408_RDataFrameToRooFit that `Book` yields RResultPtr
    Don't rely on implicit `GetValue()` via dereferencing in the RooFit
    tutorial `rf408_RDataFrameToRooFit`, because it obscures the fact that
    `Book` returns an RResultPtr, which is important to know for the user.
    
    Closes #1429.

--- tutorials/roofit/rf408_RDataFrameToRooFit.py
+++ tutorials/roofit/rf408_RDataFrameToRooFit.py
@@ -20,8 +20,7 @@ import math
 # ------------------------
 
 # We create an RDataFrame with two columns filled with 2 million random numbers.
-d = ROOT.RDataFrame(2000000)
-dd = d.Define("x", "gRandom->Uniform(-5.,  5.)").Define("y", "gRandom->Gaus(1., 3.)")
+df = ROOT.RDataFrame(2000000).Define("x", "gRandom->Uniform(-5.,  5.)").Define("y", "gRandom->Gaus(1., 3.)")
 
 
 # We create RooFit variables that will represent the dataset.
@@ -41,12 +40,20 @@ y.setBins(20)
 # - the RDataFrame column types as template parameters
 # - the constructor arguments for RooDataSet (they follow the same syntax as the usual RooDataSet constructors)
 # - the column names that RDataFrame should fill into the dataset
-#
+
 # NOTE: RDataFrame columns are matched to RooFit variables by position, *not by name*!
-rooDataSet = dd.Book(
+#
+# The returned object is not yet a RooDataSet, but an RResultPtr that will be
+# lazy-evaluated once you call GetValue() on it. We will only evaluate the
+# RResultPtr once all other RDataFrame related actions are declared. This way
+# we trigger the event loop computation only once, which will improve the
+# runtime significantly.
+#
+# To learn more about lazy actions, see:
+#     https://root.cern/doc/master/classROOT_1_1RDataFrame.html#actions
+roo_data_set_result = df.Book(
     ROOT.std.move(ROOT.RooDataSetHelper("dataset", "Title of dataset", ROOT.RooArgSet(x, y))), ("x", "y")
 )
-
 
 # Method 2:
 # ---------
@@ -54,17 +61,21 @@ rooDataSet = dd.Book(
 rdhMaker = ROOT.RooDataHistHelper("dataset", "Title of dataset", ROOT.RooArgSet(x, y))
 
 # Then, we move it into an RDataFrame action:
-rooDataHist = dd.Book(ROOT.std.move(rdhMaker), ("x", "y"))
+roo_data_hist_result = df.Book(ROOT.std.move(rdhMaker), ("x", "y"))
 
 
 # Run it and inspect the results
 # -------------------------------
 
+# At this point, all RDF actions were defined (namely, the `Book` operations),
+# so we can get values from the RResultPtr objects, triggering the event loop
+# and getting the actual RooFit data objects.
+roo_data_set = roo_data_set_result.GetValue()
+roo_data_hist = roo_data_hist_result.GetValue()
+
 # Let's inspect the dataset / datahist.
-# Note that the first time we touch one of those objects, the RDataFrame event loop will run.
 
-
-def printData(data):
+def print_data(data):
     print("")
     data.Print()
     for i in range(min(data.numEntries(), 20)):
@@ -78,5 +89,5 @@ def printData(data):
     print("mean(y) = {0:.3f}".format(data.mean(y)) + "\tsigma(y) = {0:.3f}\n".format(math.sqrt(data.moment(y, 2.0))))
 
 
-printData(rooDataSet)
-printData(rooDataHist)
+print_data(roo_data_set)
+print_data(roo_data_hist)
