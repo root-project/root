083085523c9e6a459522c4a5d7596b05d242068c
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-01-12 16:10:33 +0100

    [DF] Add new test for constructor with multiple files

--- tree/dataframe/test/dataframe_unified_constructor.cxx
+++ tree/dataframe/test/dataframe_unified_constructor.cxx
@@ -11,34 +11,41 @@ using ROOT::Experimental::RNTupleWriter;
 
 class UnifiedConstructor : public ::testing::Test {
 protected:
-   std::string fFileName = "dataframe_unified_constructor.root";
+   std::vector<std::string> fFileNames{"dataframe_unified_constructor_0.root", "dataframe_unified_constructor_1.root"};
    std::string fNTupleName = "ntuple";
    std::string fTTreeName = "tree";
 
    void SetUp() override
    {
-      {
-         auto modelWrite = RNTupleModel::Create();
-         auto pt = modelWrite->MakeField<float>("ntuple_pt", 11.f);
-         auto ntuple = RNTupleWriter::Recreate(std::move(modelWrite), fNTupleName, fFileName);
-         ntuple->Fill();
-      }
-      {
-         TFile f{fFileName.c_str(), "UPDATE"};
-         float ttree_pt{22.f};
-         TTree t{fTTreeName.c_str(), ""};
-         t.Branch("ttree_pt", &ttree_pt);
-         t.Fill();
-         f.WriteObject(&t, fTTreeName.c_str());
+      for (const auto &fName : fFileNames) {
+         {
+            auto modelWrite = RNTupleModel::Create();
+            auto pt = modelWrite->MakeField<float>("ntuple_pt", 11.f);
+            auto ntuple = RNTupleWriter::Recreate(std::move(modelWrite), fNTupleName, fName);
+            ntuple->Fill();
+         }
+         {
+            TFile f{fName.c_str(), "UPDATE"};
+            float ttree_pt{22.f};
+            TTree t{fTTreeName.c_str(), ""};
+            t.Branch("ttree_pt", &ttree_pt);
+            t.Fill();
+            f.WriteObject(&t, fTTreeName.c_str());
+         }
       }
    }
 
-   void TearDown() override { std::remove(fFileName.c_str()); }
+   void TearDown() override
+   {
+      for (const auto &fName : fFileNames) {
+         std::remove(fName.c_str());
+      }
+   }
 };
 
 TEST_F(UnifiedConstructor, FromTTree)
 {
-   ROOT::RDataFrame df{fTTreeName, fFileName};
+   ROOT::RDataFrame df{fTTreeName, fFileNames[0]};
    auto pt = df.Take<float>("ttree_pt");
    EXPECT_EQ(pt->size(), 1);
    EXPECT_EQ(pt->at(0), 22.f);
@@ -46,8 +53,24 @@ TEST_F(UnifiedConstructor, FromTTree)
 
 TEST_F(UnifiedConstructor, FromRNTuple)
 {
-   ROOT::RDataFrame df{fNTupleName, fFileName};
+   ROOT::RDataFrame df{fNTupleName, fFileNames[0]};
    auto pt = df.Take<float>("ntuple_pt");
    EXPECT_EQ(pt->size(), 1);
+   EXPECT_EQ(pt->at(0), 11.f);
+}
+
+TEST_F(UnifiedConstructor, FromTTrees)
+{
+   ROOT::RDataFrame df{fTTreeName, fFileNames};
+   auto pt = df.Take<float>("ttree_pt");
+   EXPECT_EQ(pt->size(), 2);
+   EXPECT_EQ(pt->at(0), 22.f);
+}
+
+TEST_F(UnifiedConstructor, FromRNTuples)
+{
+   ROOT::RDataFrame df{fNTupleName, fFileNames};
+   auto pt = df.Take<float>("ntuple_pt");
+   EXPECT_EQ(pt->size(), 2);
    EXPECT_EQ(pt->at(0), 11.f);
 }
