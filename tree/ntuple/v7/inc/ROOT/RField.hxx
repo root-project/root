bfd92eee7e1ec36d735650e0db7dde31c5d60189
Jakob Blomer <jblomer@cern.ch>
2024-01-14 14:47:41 +0100

    [ntuple] constness of RFieldBase::GetSubFields()

--- tree/ntuple/v7/inc/ROOT/RField.hxx
+++ tree/ntuple/v7/inc/ROOT/RField.hxx
@@ -603,7 +603,8 @@ public:
    std::size_t GetNRepetitions() const { return fNRepetitions; }
    NTupleSize_t GetNElements() const { return fPrincipalColumn->GetNElements(); }
    const RFieldBase *GetParent() const { return fParent; }
-   std::vector<RFieldBase *> GetSubFields() const;
+   std::vector<RFieldBase *> GetSubFields();
+   std::vector<const RFieldBase *> GetSubFields() const;
    bool IsSimple() const { return fIsSimple; }
    /// Get the field's description
    std::string GetDescription() const { return fDescription; }
