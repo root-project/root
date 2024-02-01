7577754d584bb5d9b291e699eb1d0c71e7d4b9a3
Olivier Couet <olivier.couet@cern.ch>
2024-02-01 11:15:14 +0100

    formating

--- tutorials/graphics/inside.C
+++ tutorials/graphics/inside.C
@@ -8,51 +8,40 @@
 ///
 /// \author Olivier Couet
 
-TGraph   *gr;
-TEllipse *el;
-TBox     *bx;
-TPave    *pv;
-TDiamond *di;
-TCrown   *cr;
-
-void InPoint(double x, double y) {
-   auto p = new TMarker(x,y,7);
-   p->Draw();
-
-   if (el->IsInside(x,y) ||
-       bx->IsInside(x,y) ||
-       pv->IsInside(x,y) ||
-       di->IsInside(x,y) ||
-       cr->IsInside(x,y) ||
-       gr->IsInside(x,y)) {
-      p->SetMarkerColor(kGreen);
-   } else {
-      p->SetMarkerColor(kRed);
-   }
-}
-
 void inside(){
-   el = new TEllipse(0.75, 0.25, .2,.15,45,315,62);
+   auto el = new TEllipse(0.75, 0.25, .2,.15,45,315,62);
    el->Draw();
 
-   gr = new TGraph();
+   auto gr = new TGraph();
    double gr_x1[5] = { 0.1, 0.3388252, 0.03796561, 0.4176218, 0.1 };
    double gr_y1[5] = { 0.5, 0.9644737, 0.7776316, 0.6960526, 0.5 };
    gr = new TGraph(5,gr_x1,gr_y1);
    gr->Draw("L");
 
-   bx = new TBox(.7,.8,.9,.95);
+   auto bx = new TBox(.7,.8,.9,.95);
    bx->Draw();
 
-   pv = new TPave(.05,.1,.3,.2);
+   auto pv = new TPave(.05,.1,.3,.2);
    pv->Draw();
 
-   di = new TDiamond(.05,.25,.3,.4);
+   auto di = new TDiamond(.05,.25,.3,.4);
    di->Draw();
 
-    cr = new TCrown(.5,.5,.1,.15);
-    cr->SetFillColor(19);
+   auto cr = new TCrown(.5,.5,.1,.15);
+   cr->SetFillColor(19);
    cr->Draw();
 
-   for (int i=0; i<20000; i++) InPoint(gRandom->Rndm(),gRandom->Rndm());
+   for (int i=0; i<10000; i++) {
+      double x = gRandom->Rndm();
+      double y = gRandom->Rndm();
+      auto p   = new TMarker(x,y,7);
+      p->Draw();
+      if (el->IsInside(x,y) || bx->IsInside(x,y) ||
+          pv->IsInside(x,y) || di->IsInside(x,y) ||
+          cr->IsInside(x,y) || gr->IsInside(x,y)) {
+         p->SetMarkerColor(kGreen);
+      } else {
+         p->SetMarkerColor(kRed);
+      }
+   }
 }
