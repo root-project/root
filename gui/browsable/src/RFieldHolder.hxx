fc470354395be520fa287dde93a1d5dd22465c1b
Jakob Blomer <jblomer@cern.ch>
2024-02-02 22:14:40 +0100

    [ntuple] remove page source from pulic view interface

--- gui/browsable/src/RFieldHolder.hxx
+++ gui/browsable/src/RFieldHolder.hxx
@@ -25,23 +25,24 @@ class RPageSource;
 }
 
 class RFieldHolder : public ROOT::Browsable::RHolder {
-   std::shared_ptr<ROOT::Experimental::Detail::RPageSource> fNtplSource;
+   std::shared_ptr<ROOT::Experimental::RNTupleReader> fNtplReader;
    std::string fParentName;
 
    ROOT::Experimental::DescriptorId_t fFieldId;
 
 public:
-   RFieldHolder(std::shared_ptr<ROOT::Experimental::Detail::RPageSource> ntplSource,
-                const std::string &parent_name,
+   RFieldHolder(std::shared_ptr<ROOT::Experimental::RNTupleReader> ntplReader, const std::string &parent_name,
                 ROOT::Experimental::DescriptorId_t id)
-                : fNtplSource(ntplSource), fParentName(parent_name), fFieldId(id) {}
+      : fNtplReader(ntplReader), fParentName(parent_name), fFieldId(id)
+   {
+   }
 
    const TClass *GetClass() const override { return TClass::GetClass<ROOT::Experimental::RNTuple>(); }
 
    /** Returns direct (temporary) object pointer */
    const void *GetObject() const override { return nullptr; }
 
-   auto GetNtplSource() const { return fNtplSource; }
+   auto GetNtplReader() const { return fNtplReader; }
    auto GetParentName() const { return fParentName; }
    auto GetId() const { return fFieldId; }
 };
