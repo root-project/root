1be1402a2b5be7d1a8607f48cee476dd8af66e06
Sergey Linev <S.Linev@gsi.de>
2024-02-09 10:27:15 +0100

    [reve] remove usage of json_fwd.hpp
    It was necessary before while interpreter had problems
    to correctly parse it. Starting from llvm 13 it is no longer the problem

--- graf3d/eve7/inc/ROOT/REveElement.hxx
+++ graf3d/eve7/inc/ROOT/REveElement.hxx
@@ -22,57 +22,7 @@
 #include <set>
 #include <string>
 
-#if __has_include(<nlohmann/json_fwd.hpp>)
-#include <nlohmann/json_fwd.hpp>
-#else
-// This forward declaration works for versions before 3.11.0, which introduced
-// a "versioned, ABI-tagged inline namespace". The json_fwd.hpp header is
-// present in multi-header installations (the default as of version 3.11.0), and
-// single-header installations of version 3.11.2+.
-
-#include <cstdint> // int64_t, uint64_t
-#include <map> // map
-#include <vector> // vector
-
-namespace nlohmann
-{
-template<typename T = void, typename SFINAE = void>
-struct adl_serializer;
-
-/// a class to store JSON values
-/// @sa https://json.nlohmann.me/api/basic_json/
-template<template<typename U, typename V, typename... Args> class ObjectType =
-         std::map,
-         template<typename U, typename... Args> class ArrayType = std::vector,
-         class StringType = std::string, class BooleanType = bool,
-         class NumberIntegerType = std::int64_t,
-         class NumberUnsignedType = std::uint64_t,
-         class NumberFloatType = double,
-         template<typename U> class AllocatorType = std::allocator,
-         template<typename T, typename SFINAE = void> class JSONSerializer =
-         adl_serializer,
-         class BinaryType = std::vector<std::uint8_t>>
-class basic_json;
-
-/// @brief JSON Pointer defines a string syntax for identifying a specific value within a JSON document
-/// @sa https://json.nlohmann.me/api/json_pointer/
-template<typename BasicJsonType>
-class json_pointer;
-
-using json = basic_json<>;
-
-/// @brief a minimal map-like container that preserves insertion order
-/// @sa https://json.nlohmann.me/api/ordered_map/
-template<class Key, class T, class IgnoredLess, class Allocator>
-struct ordered_map;
-
-/// @brief specialization that maintains the insertion order of object keys
-/// @sa https://json.nlohmann.me/api/ordered_json/
-using ordered_json = basic_json<nlohmann::ordered_map>;
-
-}  // namespace nlohmann
-
-#endif
+#include <nlohmann/json.hpp>
 
 class TGeoMatrix;
 
