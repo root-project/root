cb98850b3d9425b6abd289b557d0741f15d7e3b8
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-02-14 16:55:54 +0100

    [math] Leave TRandom1 methods definition untouched

--- math/mathcore/inc/TRandom1.h
+++ math/mathcore/inc/TRandom1.h
@@ -46,13 +46,18 @@ public:
    TRandom1(UInt_t seed, Int_t lux = 3);
    TRandom1(Int_t rowIndex, Int_t colIndex, Int_t lux);
    ~TRandom1() override;
-   // Returns current luxury value.
-   virtual Int_t GetLuxury() const;
-   // Returns the current seed (first element of the seed table)
-   UInt_t GetSeed() const override;
-   // Returns the pointer to the current seeds array.
-   const UInt_t *GetTheSeeds() const;
-   // Returns the current array of seeds.
+   /////////////////////////////////////////////////////////////////////////////
+   /// Returns current luxury value.
+   virtual Int_t GetLuxury() const {return fLuxury;}
+   /////////////////////////////////////////////////////////////////////////////
+   /// Returns the current seed (first element of the seed table)
+   /// \warning This is not the initial seed!
+   UInt_t GetSeed() const override { return  UInt_t ( fFloatSeedTable[0] /  fMantissaBit24 ) ; }
+   /////////////////////////////////////////////////////////////////////////////
+   /// Returns the pointer to the current seeds array.
+   const UInt_t *GetTheSeeds() const {return fTheSeeds;}
+   /////////////////////////////////////////////////////////////////////////////
+   /// Returns an array of seed values stored in the table, given the index.
    static void GetTableSeeds(UInt_t *seeds, Int_t index);
    using TRandom::Rndm;
    Double_t Rndm() override;
