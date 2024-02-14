9ab8cfea5206ce896e18950be72f32eb417887c2
Florine de Geus <florine.de.geus@cern.ch>
2024-02-14 13:50:52 +0100

    [rcanvas] Remove use of `R__LOAD_LIBRARY` in tutorial
    It turns out that this is not required anymore, even for
    builds/platforms without runtime modules enabled.

--- tutorials/rcanvas/rh2_large.cxx
+++ tutorials/rcanvas/rh2_large.cxx
@@ -26,10 +26,6 @@
 #include "ROOT/RFrame.hxx"
 #include "TString.h"
 
-// macro must be here while cling is not capable to load
-// library automatically for outlined function see ROOT-10336
-R__LOAD_LIBRARY(libROOTHistDraw)
-
 using namespace ROOT::Experimental;
 
 void rh2_large()
