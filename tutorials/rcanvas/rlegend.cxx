3fa99d2122581a8ef0133e4ea6ac53fe46a7d64f
Florine de Geus <florine.de.geus@cern.ch>
2024-02-14 13:50:52 +0100

    [rcanvas] Remove use of `R__LOAD_LIBRARY` in tutorial
    It turns out that this is not required anymore, even for
    builds/platforms without runtime modules enabled.

--- tutorials/rcanvas/rlegend.cxx
+++ tutorials/rcanvas/rlegend.cxx
@@ -25,11 +25,6 @@
 #include "ROOT/RLegend.hxx"
 #include "TRandom.h"
 
-// macro must be here while cling is not capable to load
-// library automatically for outlined function see ROOT-10336
-R__LOAD_LIBRARY(libROOTHistDraw)
-R__LOAD_LIBRARY(libROOTGraphicsPrimitives)
-
 using namespace ROOT::Experimental;
 
 void rlegend()
