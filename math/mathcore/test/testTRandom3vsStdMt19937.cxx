6ed8fba5e05b68ebc990640066dcee64b8870bde
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-02-14 12:49:10 +0100

    Formatting

--- math/mathcore/test/testTRandom3vsStdMt19937.cxx
+++ math/mathcore/test/testTRandom3vsStdMt19937.cxx
@@ -8,24 +8,24 @@
 
 TEST(TRandom3Test, TRandom3vsStdMt19937)
 {
-  unsigned int seed = 28u;
+   unsigned int seed = 28u;
 
-  std::mt19937 gen_std(seed);
-  TRandom3 gen_root(seed);
+   std::mt19937 gen_std(seed);
+   TRandom3 gen_root(seed);
 
-  // Test TRandom3 generator against other implementation of Mersenne Twister
-  // 32-bit algoritm.
-  //
-  // Test inspired by code of Giovanni Cerretani in this issue:
-  // https://its.cern.ch/jira/browse/ROOT-9733
-  // https://github.com/root-project/root/issues/14581
-  for (size_t generation = 0; generation < 5; ++generation) {
-    unsigned int iteration = 0;
-    while (++iteration) {
-      double rnd_std = gen_std() * 2.3283064365386963e-10;
-      double rnd_root = gen_root.Rndm();
+   // Test TRandom3 generator against other implementation of Mersenne Twister
+   // 32-bit algoritm.
+   //
+   // Test inspired by code of Giovanni Cerretani in this issue:
+   // https://its.cern.ch/jira/browse/ROOT-9733
+   // https://github.com/root-project/root/issues/14581
+   for (size_t generation = 0; generation < 5; ++generation) {
+      unsigned int iteration = 0;
+      while (++iteration) {
+         double rnd_std = gen_std() * 2.3283064365386963e-10;
+         double rnd_root = gen_root.Rndm();
 
-      ASSERT_EQ(rnd_std, rnd_root);
-    }
-  }
+         ASSERT_EQ(rnd_std, rnd_root);
+      }
+   }
 }
