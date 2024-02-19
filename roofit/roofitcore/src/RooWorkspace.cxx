5710a619d753d63af15ce8106dbd163c7350d8b5
will-cern <will@cern.ch>
2024-02-19 11:25:03 +0000

    set workspace on imported roofit objects

--- roofit/roofitcore/src/RooWorkspace.cxx
+++ roofit/roofitcore/src/RooWorkspace.cxx
@@ -689,6 +689,7 @@ bool RooWorkspace::import(const RooAbsArg& inArg,
             << node->GetName() << endl ;
       }
       _allOwnedNodes.addOwned(std::unique_ptr<RooAbsArg>{node});
+      node->setWorkspace(*this);
       if (_openTrans) {
         _sandboxNodes.add(*node) ;
       } else {
