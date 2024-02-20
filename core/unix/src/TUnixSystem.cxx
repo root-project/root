9a8f21a573dd2bd46cf3d9f34d7a89ced255303a
Sergey Linev <S.Linev@gsi.de>
2024-02-20 13:52:35 +0100

    [core] add suffix in TSystem::TempFileName method
    Allows to create temporary file
    with suffix like custom file extension

--- core/unix/src/TUnixSystem.cxx
+++ core/unix/src/TUnixSystem.cxx
@@ -1481,20 +1481,24 @@ const char *TUnixSystem::TempDirectory() const
 /// Create a secure temporary file by appending a unique
 /// 6 letter string to base. The file will be created in
 /// a standard (system) directory or in the directory
-/// provided in dir. The full filename is returned in base
+/// provided in dir. Optionally one can provide suffix
+/// append to the final name - like extension ".txt" or ".html".
+/// The full filename is returned in base
 /// and a filepointer is returned for safely writing to the file
 /// (this avoids certain security problems). Returns 0 in case
 /// of error.
 
-FILE *TUnixSystem::TempFileName(TString &base, const char *dir)
+FILE *TUnixSystem::TempFileName(TString &base, const char *dir, const char *suffix)
 {
    char *b = ConcatFileName(dir ? dir : TempDirectory(), base);
    base = b;
    base += "XXXXXX";
+   if (suffix && *suffix)
+      base.Append(suffix);
    delete [] b;
 
    char *arg = StrDup(base);
-   int fd = mkstemp(arg);
+   int fd = suffix && *suffix ? mkstemps(arg, strlen(suffix)) : mkstemp(arg);
    base = arg;
    delete [] arg;
 
