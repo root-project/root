root-project/root

commit 2b3250dc2a9cd828f6c8e398e69ecde0e1412111

Enable builtins only when they are not found
The option fail-on-missing doesn't work when builtins
are enabled by default and not on demand. See ROOT-9385.

cmake/modules/RootBuildOptions.cmake
@@ -74,15 +74,15 @@ endfunction()
 ROOT_BUILD_OPTION(alien OFF "Enable support for AliEn (requires libgapiUI from ALICE)")
 ROOT_BUILD_OPTION(arrow OFF "Enable support for Apache Arrow")
 ROOT_BUILD_OPTION(asimage ON "Enable support for image processing via libAfterImage")
-ROOT_BUILD_OPTION(builtin_afterimage ON "Build bundled copy of libAfterImage")
+ROOT_BUILD_OPTION(builtin_afterimage OFF "Build bundled copy of libAfterImage")
 ROOT_BUILD_OPTION(builtin_cfitsio OFF "Build CFITSIO internally (requires network)")
 ROOT_BUILD_OPTION(builtin_clang ON "Build bundled copy of Clang")
 ROOT_BUILD_OPTION(builtin_davix OFF "Build Davix internally (requires network)")
 ROOT_BUILD_OPTION(builtin_fftw3 OFF "Build FFTW3 internally (requires network)")
 ROOT_BUILD_OPTION(builtin_freetype OFF "Build bundled copy of freetype")
-ROOT_BUILD_OPTION(builtin_ftgl ON "Build bundled copy of FTGL")
+ROOT_BUILD_OPTION(builtin_ftgl OFF "Build bundled copy of FTGL")
 ROOT_BUILD_OPTION(builtin_gl2ps OFF "Build bundled copy of gl2ps")
-ROOT_BUILD_OPTION(builtin_glew ON "Build bundled copy of GLEW")
+ROOT_BUILD_OPTION(builtin_glew OFF "Build bundled copy of GLEW")
 ROOT_BUILD_OPTION(builtin_gsl OFF "Build GSL internally (requires network)")
 ROOT_BUILD_OPTION(builtin_llvm ON "Build bundled copy of LLVM")
 ROOT_BUILD_OPTION(builtin_lz4 OFF "Build bundled copy of lz4")

commit 00e71a95680c2e15c1119104ae0dc77dcd961e7c

Use always bundled afterimage which contains many fixes
that the obsolete upstream version does not (unmaintained package)

Fixes https://github.com/root-project/root/issues/14247

This is an exception of the general rule introduced by https://github.com/root-project/root/commit/2b3250dc2a9cd828f6c8e398e69ecde0e1412111

cmake/modules/RootBuildOptions.cmake
@@ -80,7 +80,7 @@ endfunction()
 ROOT_BUILD_OPTION(arrow OFF "Enable support for Apache Arrow")
 ROOT_BUILD_OPTION(asimage ON "Enable support for image processing via libAfterImage")
 ROOT_BUILD_OPTION(asserts OFF "Enable asserts (defaults to ON for CMAKE_BUILD_TYPE=Debug and/or dev=ON)")
-ROOT_BUILD_OPTION(builtin_afterimage OFF "Build bundled copy of libAfterImage")
+ROOT_BUILD_OPTION(builtin_afterimage ON "Build bundled copy of libAfterImage")
 ROOT_BUILD_OPTION(builtin_cfitsio OFF "Build CFITSIO internally (requires network)")
 ROOT_BUILD_OPTION(builtin_clang ON "Build bundled copy of Clang")
 ROOT_BUILD_OPTION(builtin_cling ON "Build bundled copy of Cling. Only build with an external cling if you know what you are doing: associating ROOT commits with cling commits is tricky.")