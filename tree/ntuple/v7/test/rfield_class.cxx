ad9b3e15b8f6e8d648f123592596f2ebc2969234
Jakob Blomer <jblomer@cern.ch>
2024-02-26 14:19:10 +0100

    [ntuple] forbid RNTuple I/O of unsplittable classes

--- tree/ntuple/v7/test/rfield_class.cxx
+++ tree/ntuple/v7/test/rfield_class.cxx
@@ -11,6 +11,9 @@ TEST(RNTuple, TClass) {
    auto model = RNTupleModel::Create();
    auto ptrKlass = model->MakeField<CustomStruct>("klass");
 
+   // TDatime would be a supported class layout but it is unsplittable due to its custom streamer
+   EXPECT_THROW(model->MakeField<TDatime>("datime"), RException);
+
    FileRaii fileGuard("test_ntuple_tclass.root");
    auto ntuple = RNTupleWriter::Recreate(std::move(model), "f", fileGuard.GetPath());
 }
