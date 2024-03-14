4373b2740dfaf09fa528790f0b36d86c18e9a601
Jakob Blomer <jblomer@cern.ch>
2024-03-14 10:23:25 +0100

    [ntuple] remove deprecated parts from unit test Linkdef
    Co-authored-by: Philippe Canal <pcanal@fnal.gov>

--- tree/ntuple/v7/test/UnsplitLinkDef.h
+++ tree/ntuple/v7/test/UnsplitLinkDef.h
@@ -1,9 +1,5 @@
 #ifdef __CLING__
 
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-
 #pragma link C++ class ClassWithUnsplitMember + ;
 #pragma link C++ class CustomStreamer - ;
 #pragma link C++ options = rntuplesplit class CustomStreamerForceSplit - ;
