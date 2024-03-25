d91e4ee5e960e6db6b0625205c47f60c30c53655
Jonas Rembser <jonas.rembser@cern.ch>
2024-03-25 20:49:16 +0100

    Remove spash screen to avoid `root.exe` dependence on x11 or cocoa
    Arguments for removing the splash screen:
    
      * It introduces a dependency of the `root` executable of graphics
        libraries, like X11 on Linux or Cocoa on macOS.
    
      * The Windows version is not kept up to date ([see the list of core
        developers](https://github.com/root-project/root/blob/master/core/winnt/src/Win32Splash.cxx#L32)).
    
      * According to
        [Wikipedia](https://en.wikipedia.org/wiki/Splash_screen), "splash
        screens are typically used by particularly large applications to
        notify the user that the program is in the process of loading". The
        ROOT interpreter takes almost no time to load today. Probably that
        was different in the past, and a splash screen was justified back
        then.
    
      * Users don't see it unless they run `root` with the `-a` option. In
        this case, the interpreter quits immediately (I think the option
        only exists just to show the splash screen for the historical
        record).
    
      * It [doesn't work on multi-display setups](https://github.com/root-project/root/issues/14819).
    
      * `root -b -a` turns on display even if you think it should run in
        batch mode
    
      * It doesn't work on Wayland (see #15723) --> EDIT: It does work on
        wayland, it's just that the Xs were not fine.
    
    Closes #14819.
    Closes [ROOT-10948](https://its.cern.ch/jira/browse/ROOT-10948).

--- tree/treeviewer/src/TTreeViewer.cxx
+++ tree/treeviewer/src/TTreeViewer.cxx
@@ -2060,10 +2060,6 @@ bool TTreeViewer::ProcessMessage(Longptr_t msg, Longptr_t parm1, Longptr_t parm2
                      break;
                   case kHelpAbout:
                      {
-#ifdef R__UNIX
-                        TString rootx = TROOT::GetBinDir() + "/root -a &";
-                        gSystem->Exec(rootx);
-#else
 #ifdef WIN32
                         new TWin32SplashThread(true);
 #else
@@ -2072,7 +2068,6 @@ bool TTreeViewer::ProcessMessage(Longptr_t msg, Longptr_t parm1, Longptr_t parm2
                         hd = new TRootHelpDialog(this, str, 600, 400);
                         hd->SetText(gHelpAbout);
                         hd->Popup();
-#endif
 #endif
                      }
                      break;
