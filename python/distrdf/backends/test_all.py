b094886c0519722600afcecfd843dbb8ec92ebd3
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-03-26 16:25:55 +0100

    [df] Homogenize distributed testing for different parameters
    Create a single set of testing scripts to run according to different parameters for distributed execution.
    
    The biggest change with this commit is moving from two separate sets of files for Spark and Dask backends to a single set that can run with either backend connection depending on the current parameter in the `payload` fixture set via the `conftest.py` file.
    
    Also, create a directory with input TTree datasets used throughout the tests. This has two advantages. First, it avoids spending extra time in creating and writing the dataset to disk. Second, it sets the ground for a future update of the testing suite to include the data source as another parameter (i.e. running the tests reading either TTree or RNTuple data).
    
    For reproducibility, the `_create_datasets.py` script is added to the input data directory.

---
+++ python/distrdf/backends/test_all.py
@@ -0,0 +1,20 @@
+import sys
+
+import pytest
+
+from check_backend import *
+from check_definepersample import *
+from check_friend_trees_alignment import *
+from check_friend_trees import *
+from check_histo_write import *
+from check_include_headers import *
+from check_inv_mass import *
+from check_live_visualize import *
+from check_reducer_merge import *
+from check_rungraphs import *
+from check_variations import *
+
+if __name__ == "__main__":
+    # The call to sys.exit is needed otherwise CTest would just ignore the
+    # results returned by pytest, even in case of errors.
+    sys.exit(pytest.main(args=[__file__]))
