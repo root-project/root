87eecae1646f20c6a7fb43bcb444e022698a06c7
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-03-27 17:31:49 +0100

    [tree] Add regression test for writing long strings

--- tree/tree/test/TTreeRegressions.cxx
+++ tree/tree/test/TTreeRegressions.cxx
@@ -115,3 +115,33 @@ TEST(TTreeRegressions, GetLeafAndFriends)
    EXPECT_EQ(t.GetLeaf("asdklj", "x"), nullptr);
    EXPECT_EQ(t.GetLeaf("asdklj", "vec"), nullptr);
 }
+
+// PR #14887
+TEST(TTreeRegressions, LeafLongString)
+{
+   TTree t("t", "t");
+   char s[1000];
+   memset(s, 'a', 999);
+   s[999] = 0;
+   t.Branch("s", &s, "s/C");
+
+   s[254] = 0;
+   t.Fill();
+   s[254] = 'a';
+   s[255] = 0;
+   t.Fill();
+   s[255] = 'a';
+   t.Fill();
+
+   s[0] = 0;
+   t.GetEntry(0);
+   EXPECT_EQ(strlen(s), 254);
+
+   s[0] = 0;
+   t.GetEntry(1);
+   EXPECT_EQ(strlen(s), 255);
+
+   s[0] = 0;
+   t.GetEntry(2);
+   EXPECT_EQ(strlen(s), 999);
+}
