8e50a7c2d01279b88733d19b3f9e0b2d7830f2ea
Philippe Canal <pcanal@fnal.gov>
2024-04-01 11:32:48 -0500

    meta: Remove kClassSaved in favor of a thread local set.
    Remove (rather than deprecate) TClass::kClassSaved has using it will no longer have any effect.
    
    Rather than updating `fBits` in the TClass which leads to other usage of the same bits to become undetermined behavior
    in multi-thread mode, we now record the same information in a thread local std::set.  This has the added benefit of
    moving towards the SavePrimitives function to be one step closer to be thread safe.

--- core/meta/inc/TClass.h
+++ core/meta/inc/TClass.h
@@ -92,7 +92,7 @@ public:
    enum EStatusBits {
       kReservedLoading = BIT(7), // Internal status bits, set and reset only during initialization
 
-      kClassSaved  = BIT(12),
+      /* had kClassSaved  = BIT(12), */
       kHasLocalHashMember = BIT(14),
       kIgnoreTObjectStreamer = BIT(15),
       kUnloaded    = BIT(16), // The library containing the dictionary for this class was
