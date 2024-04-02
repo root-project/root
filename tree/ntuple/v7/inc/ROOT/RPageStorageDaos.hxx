b4fe403e2ab452ccbfccdf46db37160a8c71653d
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-04-02 14:09:07 +0200

    [ntuple] Add RPageSink::IsInitialized()
    Keep track if Init() was called, and throw an error if it is called
    again. Requires renaming the current Init() method to InitImpl().
    
    (cherry picked from commit d21642b6bd58dad509bff130e2c48869b59de1f0)

--- tree/ntuple/v7/inc/ROOT/RPageStorageDaos.hxx
+++ tree/ntuple/v7/inc/ROOT/RPageStorageDaos.hxx
@@ -122,6 +122,7 @@ private:
    uint32_t fCageSizeLimit{};
 
 protected:
+   using RPagePersistentSink::InitImpl;
    void InitImpl(unsigned char *serializedHeader, std::uint32_t length) final;
    RNTupleLocator CommitPageImpl(ColumnHandle_t columnHandle, const RPage &page) final;
    RNTupleLocator
