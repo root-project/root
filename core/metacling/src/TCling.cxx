4f1e0879665b0f89c5b937382d12362110312332
Philippe Canal <pcanal@fnal.gov>
2024-04-02 11:30:30 -0500

    Add missing lock in TCling::FindSym.
    This fixes https://github.com/cms-sw/cmssw/issues/44438

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -7419,6 +7419,7 @@ int TCling::DisplayIncludePath(FILE *fout) const
 
 void* TCling::FindSym(const char* entry) const
 {
+   R__LOCKGUARD(gInterpreterMutex);
    return fInterpreter->getAddressOfGlobal(entry);
 }
 
