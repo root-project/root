57caa0a6823f897c88f23851580fb1072b0dc5b1
Jakob Blomer <jblomer@cern.ch>
2024-04-02 15:30:08 +0200

    [ntuple] add unit test for unsplit comment marker

--- tree/ntuple/v7/test/Unsplit.hxx
+++ tree/ntuple/v7/test/Unsplit.hxx
@@ -29,4 +29,9 @@ struct CustomStreamerForceUnsplit {
    float a;
 };
 
+// For the time being, RNTuple ignores the unsplit comment marker and does _not_ use an RUnsplitField for such members.
+class IgnoreUnsplitComment {
+   std::vector<float> v; //||
+};
+
 #endif // ROOT7_RNTuple_Test_Unsplit
