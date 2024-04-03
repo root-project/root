9c21430570a8d0c0704a9cbd01a10d2715160b5f
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-04-03 17:12:13 +0200

    [io] Remove RRawFile::kFeatureHasAsyncIo flag
    It was only set by RRawFileNetXNG, but not used otherwise.

--- net/netxng/inc/ROOT/RRawFileNetXNG.hxx
+++ net/netxng/inc/ROOT/RRawFileNetXNG.hxx
@@ -45,7 +45,7 @@ public:
    RRawFileNetXNG(std::string_view url, RRawFile::ROptions options);
    ~RRawFileNetXNG();
    std::unique_ptr<RRawFile> Clone() const final;
-   int GetFeatures() const final { return kFeatureHasSize | kFeatureHasAsyncIo; }
+   int GetFeatures() const final { return kFeatureHasSize; }
    RIOVecLimits GetReadVLimits() final;
 };
 
