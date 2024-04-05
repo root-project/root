0fff8b87c111fb6563e9b8ab62b555ffcd8fda37
ferdymercury <ferdymercury@users.noreply.github.com>
2024-04-05 02:16:21 +0200

    [io] do not use single-file-merge shortcut if filters are specified
    Fixes https://github.com/root-project/root/issues/13359

--- io/io/test/TFileMergerTests.cxx
+++ io/io/test/TFileMergerTests.cxx
@@ -4,6 +4,7 @@
 
 #include "TMemFile.h"
 #include "TTree.h"
+#include "TH1.h"
 
 static void CreateATuple(TMemFile &file, const char *name, double value)
 {
@@ -51,7 +52,8 @@ TEST(TFileMerger, CreateWithTFilePointer)
    merger.AddFile(&a, false);
    merger.AddFile(&b, false);
    // FIXME: Calling merger.Merge() will call Close() and *delete* output.
-   merger.PartialMerge();
+   success = merger.PartialMerge();
+   ASSERT_TRUE(success);
 
    auto &result = *static_cast<TMemFile *>(merger.GetOutputFile());
    CheckTree(result, "a_tree", 1);
@@ -67,4 +69,35 @@ TEST(TFileMerger, CreateWithUnwritableTFilePointer)
    output->SetWritable(false);
    ROOT_EXPECT_ERROR(merger.OutputFile(std::move(output)), "TFileMerger::OutputFile",
                      "output file output.root is not writable");
+}
+
+TEST(TFileMerger, MergeSingleOnlyListed)
+{
+   TMemFile a("hist4.root", "CREATE");
+
+   auto hist1 = new TH1F("hist1", "hist1", 1 , 0 , 2);
+   auto hist2 = new TH1F("hist2", "hist2", 1 , 0 , 2);
+   auto hist3 = new TH1F("hist3", "hist3", 1 , 0 , 2);
+   auto hist4 = new TH1F("hist4", "hist4", 1 , 0 , 2);
+   hist1->Fill(1);
+   hist2->Fill(1);   hist2->Fill(2);
+   hist3->Fill(1);   hist3->Fill(1);   hist3->Fill(1);
+   hist4->Fill(1);   hist4->Fill(1);   hist4->Fill(1);   hist4->Fill(1);
+   a.Write();
+   
+   TFileMerger merger;
+   auto output = std::unique_ptr<TFile>(new TFile("SingleOnlyListed.root", "RECREATE"));
+   bool success = merger.OutputFile(std::move(output));
+   ASSERT_TRUE(success);
+   
+   merger.AddObjectNames("hist1");
+   merger.AddObjectNames("hist2");
+   merger.AddFile(&a, false);
+   const Int_t mode = (TFileMerger::kAll | TFileMerger::kRegular | TFileMerger::kOnlyListed);
+   success = merger.PartialMerge(mode); // This will delete fOutputFile as we are not using kIncremental, so merger.GetOutputFile() will return a nullptr
+   ASSERT_TRUE(success);
+
+   output = std::unique_ptr<TFile>(TFile::Open("SingleOnlyListed.root"));
+   ASSERT_TRUE(output.get() && output->GetListOfKeys());
+   EXPECT_EQ(output->GetListOfKeys()->GetSize(), 2);
 }
