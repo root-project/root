a61a4bea43a0fcff4207c59954cf1506435d5d41
alja <amraktadel@ucsd.edu>
2024-04-08 15:52:05 -0700

    Fix tooltip error when REveCaloData is not streamed

--- ui5/eve7/lib/EveElementsRCore.js
+++ ui5/eve7/lib/EveElementsRCore.js
@@ -414,9 +414,8 @@ sap.ui.define(['rootui5/eve7/lib/EveManager'], function (EveManager)
          let idxBuff = eve_el.render_data.idxBuff;
          // let bin =  idxBuff[idx*2 + 1];
          let val = eve_el.render_data.nrmBuff[idx];
-         let caloData =  this.top_obj.scene.mgr.GetElement(eve_el.dataId);
          let slice = idxBuff[idx*2];
-         let sname = caloData.sliceInfos[slice].name;
+         let sname = "Slice " + slice;
 
          let vbuff =  eve_el.render_data.vtxBuff;
          let p = idx*12;
