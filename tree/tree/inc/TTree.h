d49ef349bd89098992e249325e6968c10849e02b
ferdymercury <ferdymercury@users.noreply.github.com>
2024-04-11 15:17:23 +0200

    [skip-ci] mention default
    Co-authored-by: Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>

--- tree/tree/inc/TTree.h
+++ tree/tree/inc/TTree.h
@@ -643,7 +643,7 @@ public:
    virtual void            SetParallelUnzip(bool opt=true, Float_t RelSize=-1);
    virtual void            SetPerfStats(TVirtualPerfStats* perf);
    /**
-    * \brief Sets the maximum number of rows to be shown before `<CR>` when calling Scan().
+    * \brief Sets the default maximum number of rows to be shown before `<CR>` when calling Scan().
     * \param n the maximum number of rows. Default=50, if 0, all rows of the Tree are shown
     * and there is no need to press `<CR>` or quit to exit the function.
     * \see TTreePlayer::Scan for more details on how to redirect the output to an ASCII file
