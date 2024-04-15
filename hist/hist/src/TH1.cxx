c38b39ed3ee3621fb9cd189248522abab16e9476
ferdymercury <ferdymercury@users.noreply.github.com>
2024-04-15 18:10:17 +0200

    [skip-ci] mention any dimension
    as suggested by lmoneta

--- hist/hist/src/TH1.cxx
+++ hist/hist/src/TH1.cxx
@@ -5809,7 +5809,7 @@ Bool_t TH1::Multiply(const TH1 *h1, const TH1 *h2, Double_t c1, Double_t c2, Opt
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Normalize a 1D histogram to unit integral.
+/// Normalize a histogram to unit integral. (Works for TH1, TH2, TH3, ...)
 ///
 /// If option contains "width", this histogram is normalized by 1/(integral * bin width),
 /// else, this histogram is normalized by 1/integral.
