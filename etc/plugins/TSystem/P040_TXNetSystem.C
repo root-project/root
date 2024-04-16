af091c773cd84115000772da91250fa575bd1832
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-04-16 13:05:43 +0200

    Remove dead netx configuration variables

--- etc/plugins/TSystem/P040_TXNetSystem.C
+++ etc/plugins/TSystem/P040_TXNetSystem.C
@@ -1,13 +1,7 @@
 TSystem* ROOT_Plugin_TNetXNGSystem(const char *,Bool_t);
-TSystem* ROOT_Plugin_TXNetSystem(const char *,Bool_t);
 
 void P040_TXNetSystem()
 {
-   if (!gEnv->GetValue("XNet.UseOldClient", 0)) {
-      gPluginMgr->AddHandler("TSystem", "^[x]?root:", "TNetXNGSystem",
-         "NetxNG", "::ROOT_Plugin_TNetXNGSystem(const char *,Bool_t)");
-   } else {
-      gPluginMgr->AddHandler("TSystem", "^[x]?root:", "TXNetSystem",
-         "Netx", "::ROOT_Plugin_TXNetSystem(const char *,Bool_t)");
-   }
+   gPluginMgr->AddHandler("TSystem", "^[x]?root:", "TNetXNGSystem",
+      "NetxNG", "::ROOT_Plugin_TNetXNGSystem(const char *,Bool_t)");
 }
