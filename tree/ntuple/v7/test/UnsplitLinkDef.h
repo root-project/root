6e4966c943bfb8c5c7cc900e60ae097607f7de21
Jakob Blomer <jblomer@cern.ch>
2024-04-16 23:56:25 +0200

    [ntuple] change syntax of Linkdef RNTuple split option

--- tree/ntuple/v7/test/UnsplitLinkDef.h
+++ tree/ntuple/v7/test/UnsplitLinkDef.h
@@ -3,8 +3,8 @@
 #pragma link C++ struct CyclicMember;
 #pragma link C++ class ClassWithUnsplitMember + ;
 #pragma link C++ class CustomStreamer - ;
-#pragma link C++ options = rntuplesplit class CustomStreamerForceSplit - ;
-#pragma link C++ options = rntupleunsplit class CustomStreamerForceUnsplit + ;
+#pragma link C++ options = rntupleSplit(true) class CustomStreamerForceSplit - ;
+#pragma link C++ options = rntupleSplit(false) class CustomStreamerForceUnsplit + ;
 #pragma link C++ class IgnoreUnsplitComment + ;
 
 #endif
