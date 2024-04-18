311b78edba8b8b02b355f76f8f6ba15790b864de
will-cern <will@cern.ch>
2024-04-18 23:41:28 +0100

    [RF] Update xroofit
    * update xroofit
    
    * allow evaluation fits to be considered valid if no floating pars
    
    * [xroofit] add missing dependency on libFitPanel
    
    to avoid fatal error: TFitParametersDialog.h: No such file or directory
    
    * further update
    
    * fix hash

--- roofit/xroofit/src/xRooFitVersion.h
+++ roofit/xroofit/src/xRooFitVersion.h
@@ -1,16 +1,16 @@
 /*
- * Project: xRooFit
- * Author:
- *   Will Buttinger, RAL 2022
- *
- * Copyright (c) 2022, CERN
- *
- * Redistribution and use in source and binary forms,
- * with or without modification, are permitted according to the terms
- * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+* Project: xRooFit
+* Author:
+*   Will Buttinger, RAL 2024
+*
+* Copyright (c) 2024, CERN
+*
+* Redistribution and use in source and binary forms,
+* with or without modification, are permitted according to the terms
+* listed in LICENSE (http://roofit.sourceforge.net/license.txt)
  */
 
 #pragma once
 
-#define GIT_COMMIT_HASH "73512cf"
-#define GIT_COMMIT_DATE "2022-12-16 15:18:37 +0000"
+#define GIT_COMMIT_HASH "b752c67"
+#define GIT_COMMIT_DATE "2024-04-11 14:09:41 +0200"
