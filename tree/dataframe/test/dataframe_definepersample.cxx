7ffc8f1fafda031f9256ef5e510104c4f20caf52
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-04-26 14:56:22 +0200

    Globally assume at least GTest version 1.10.0
    It appears ROOT already depends on at least GTest 1.10.0, by using
    the following in some places without compatibility code:
     * GTEST_SKIP
     * TestWithParam with non-default constructible type
     * The new "test suite" naming instead of the old "test case" (I found
       at least TYPED_TEST_SUITE and SetUpTestSuite + TearDownTestSuite).
    
    Given that an older version is only found on EL8, where we have been
    using builtin_gtest since a while, I propose to drop the remaining
    compatibility code.

--- tree/dataframe/test/dataframe_definepersample.cxx
+++ tree/dataframe/test/dataframe_definepersample.cxx
@@ -6,11 +6,6 @@
 
 #include <gtest/gtest.h>
 
-// Backward compatibility for gtest version < 1.10.0
-#ifndef INSTANTIATE_TEST_SUITE_P
-#define INSTANTIATE_TEST_SUITE_P INSTANTIATE_TEST_CASE_P
-#endif
-
 #include <atomic>
 #include <memory>
 #include <thread> // std::thread::hardware_concurrency
