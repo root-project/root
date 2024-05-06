2cec21e8aeea7de0137e974fe7b32b468fd31e22
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2024-05-06 23:12:27 +0200

    [PyROOT] Correct JsMVA module path and always include JsMVA in build
    Follow-up to de3f8e98b.
    
    Before that commit, the `JsMVA` Python sources were always included in
    the build, even if `tmva=OFF`. This was accidentally changed, and breaks
    `import ROOT` in a notebook if `tmva` is not enabled.

--- bindings/pyroot/pythonizations/python/ROOT/JsMVA/__init__.py
+++ bindings/pyroot/pythonizations/python/ROOT/JsMVA/__init__.py
@@ -11,7 +11,7 @@ from IPython.core.extensions import ExtensionManager
 def loadExtensions():
     ip = get_ipython()
     extMgr = ExtensionManager(ip)
-    extMgr.load_extension("JsMVA.JsMVAMagic")
+    extMgr.load_extension("ROOT.JsMVA.JsMVAMagic")
 
 
 loadExtensions()
