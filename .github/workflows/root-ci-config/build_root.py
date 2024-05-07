f6f37d00a9052d9b7cf3c627880b3c95220ad524
Bertrand Bellenot <bellenot@users.noreply.github.com>
2024-05-07 09:20:06 +0200

    [CI][Windows] Increase the test retries

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -148,7 +148,7 @@ def main():
     if testing:
         extra_ctest_flags = ""
         if WINDOWS:
-            extra_ctest_flags += "--repeat until-pass:4 "
+            extra_ctest_flags += "--repeat until-pass:5 "
             extra_ctest_flags += "--build-config " + args.buildtype
 
         ctest_returncode = run_ctest(extra_ctest_flags)
