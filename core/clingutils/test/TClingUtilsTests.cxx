091e25264d0142161d7f22c1e9344c8c31cea365
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-05-07 10:36:05 +0200

    [TCling] Separate test for GetCppName
    Also enable it on Windows.

--- core/clingutils/test/TClingUtilsTests.cxx
+++ core/clingutils/test/TClingUtilsTests.cxx
@@ -22,6 +22,18 @@
 
 #include <fstream>
 
+TEST(TClingUtilsTests, GetCppName)
+{
+   using namespace ROOT::TMetaUtils;
+
+   // Test for converting a string to a valid C/C++ variable name
+   std::string validVarName;
+   GetCppName(validVarName,
+              "some+input-string*with/special&characters123+-*/&%|^><=~.()[]{};#?`!,$:\"@\'\\@abc$and spaces");
+   ASSERT_EQ(validVarName, "somepLinputmIstringmUwithdIspecialaNcharacters123pLmImUdIaNpEoRhAgRlEeQwAdOoPcPoBcBlBrBsChS"
+                           "qMbTnOcOdAcLdQaTsQfIaTabcdAandsPspaces");
+}
+
 TEST(TClingUtilsTests, GetRealPath)
 {
 #ifndef R__WIN32
@@ -71,13 +83,5 @@ TEST(TClingUtilsTests, GetRealPath)
    std::remove("./symlink_realfile2");
    std::remove("./realfile1");
    std::remove("./realfile2");
-
-   // Test for converting a string to a valid C/C++ variable name
-   std::string validVarName;
-   GetCppName(validVarName,
-              "some+input-string*with/special&characters123+-*/&%|^><=~.()[]{};#?`!,$:\"@\'\\@abc$and spaces");
-   ASSERT_EQ(validVarName, "somepLinputmIstringmUwithdIspecialaNcharacters123pLmImUdIaNpEoRhAgRlEeQwAdOoPcPoBcBlBrBsChS"
-                           "qMbTnOcOdAcLdQaTsQfIaTabcdAandsPspaces");
-
 #endif // not R__WIN32
 }
