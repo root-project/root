8bd3de6585a0b0b99c598cc9646d10e252722721
Jonas Rembser <jonas.rembser@cern.ch>
2024-05-12 13:36:17 +0200

    [RF] Move remaining RooFit tests in `test/` into `roofit/`
    Having tests in the `test/` directory is quite fragile for configuration
    reasons, because this is separate CMake project. Therefore, it doesn't
    share the same configuration variables, which is easy to forget.
    
    The RooFit stress tests have been moved already, and this commit moves
    the remaining RooFit tests.
    
    The code is reformatted with the ROOT `clang-format` style on this
    occasion.

--- roofit/roofit/test/testRooFit.cxx
+++ roofit/roofit/test/testRooFit.cxx
@@ -24,8 +24,6 @@
 #include "Fit/BinData.h"
 #include "Fit/Fitter.h"
 
-
-
 #include "WrapperRooPdf.h"
 
 #include <string>
@@ -41,30 +39,30 @@
 const int N = 3; // n must be greater than 1
 const int nfit = 1;
 const int nEvents = 10000;
-double iniPar[2*N];
+double iniPar[2 * N];
 
-//#define DEBUG
+// #define DEBUG
 
 typedef ROOT::Math::IParamMultiFunction Func;
 
-void fillTree(TTree & t2) {
+void fillTree(TTree &t2)
+{
 
-
-   double  x[N];
+   double x[N];
    Int_t ev;
    for (int j = 0; j < N; ++j) {
       std::string xname = "x_" + ROOT::Math::Util::ToString(j);
       std::string xname2 = "x_" + ROOT::Math::Util::ToString(j) + "/D";
-      t2.Branch(xname.c_str(),&x[j],xname2.c_str());
+      t2.Branch(xname.c_str(), &x[j], xname2.c_str());
    }
-   t2.Branch("ev",&ev,"ev/I");
-   //fill the tree
+   t2.Branch("ev", &ev, "ev/I");
+   // fill the tree
    TRandom3 r;
-   for (Int_t i=0;i<nEvents;i++) {
-      for (int j = 0;  j < N; ++j) {
-         double mu = double(j)/10.;
-         double s  = 1.0 + double(j)/10.;
-         x[j] = r.Gaus(mu,s);
+   for (Int_t i = 0; i < nEvents; i++) {
+      for (int j = 0; j < N; ++j) {
+         double mu = double(j) / 10.;
+         double s = 1.0 + double(j) / 10.;
+         x[j] = r.Gaus(mu, s);
       }
 
       ev = i;
@@ -73,26 +71,27 @@ void fillTree(TTree & t2) {
    // t2.Print();
    // std::cout << "number of branches " << t2.GetNbranches() << std::endl;
 
-   t2.ResetBranchAddresses() ;
+   t2.ResetBranchAddresses();
 }
 
-void FillUnBinData(ROOT::Fit::UnBinData &d, TTree * tree ) {
+void FillUnBinData(ROOT::Fit::UnBinData &d, TTree *tree)
+{
 
    // fill the unbin data set from a TTree
 
    // large tree
    unsigned int n = tree->GetEntries();
    std::cout << "number of unbin data is " << n << " of dim " << N << std::endl;
-   d.Append(n,N);
+   d.Append(n, N);
 
    double vx[N];
-   for (int j = 0; j <N; ++j) {
+   for (int j = 0; j < N; ++j) {
       std::string bname = "x_" + ROOT::Math::Util::ToString(j);
-      TBranch * bx = tree->GetBranch(bname.c_str());
+      TBranch *bx = tree->GetBranch(bname.c_str());
       bx->SetAddress(&vx[j]);
    }
 
-   std::vector<double>  m(N);
+   std::vector<double> m(N);
    for (int unsigned i = 0; i < n; ++i) {
       tree->GetEntry(i);
       d.Add(vx);
@@ -103,71 +102,64 @@ void FillUnBinData(ROOT::Fit::UnBinData &d, TTree * tree ) {
 #ifdef DEBUG
    std::cout << "average values of means :\n";
    for (int j = 0; j < N; ++j)
-      std::cout << m[j]/n << "  ";
+      std::cout << m[j] / n << "  ";
    std::cout << "\n";
 #endif
 
-   tree->ResetBranchAddresses() ;
+   tree->ResetBranchAddresses();
 
    return;
-
 }
-
-
 
 // class describing product of gaussian pdf
 class MultiGaussRooPdf {
 
- public:
-
+public:
    // create all pdf
-   MultiGaussRooPdf(unsigned int n) :
-      x(n), m(n), s(n), g(n), pdf(n)
-      {
-         //assert(n >= 2);
+   MultiGaussRooPdf(unsigned int n) : x(n), m(n), s(n), g(n), pdf(n)
+   {
+      // assert(n >= 2);
 
-         // create the gaussians
-         for (unsigned int j = 0; j < n; ++j) {
+      // create the gaussians
+      for (unsigned int j = 0; j < n; ++j) {
 
-            std::string xname = "x_" + ROOT::Math::Util::ToString(j);
-            x[j] = new RooRealVar(xname.c_str(),xname.c_str(),-10000,10000) ;
+         std::string xname = "x_" + ROOT::Math::Util::ToString(j);
+         x[j] = new RooRealVar(xname.c_str(), xname.c_str(), -10000, 10000);
 
-            std::string mname = "m_" + ROOT::Math::Util::ToString(j);
-            std::string sname = "s_" + ROOT::Math::Util::ToString(j);
+         std::string mname = "m_" + ROOT::Math::Util::ToString(j);
+         std::string sname = "s_" + ROOT::Math::Util::ToString(j);
 
+         //             m[j] = new RooRealVar(mname.c_str(),mname.c_str(),iniPar[2*j],-10000,10000) ;
+         //             s[j] = new RooRealVar(sname.c_str(),sname.c_str(),iniPar[2*j+1],-10000,10000) ;
+         m[j] = new RooRealVar(mname.c_str(), mname.c_str(), iniPar[2 * j], -10, 10);
+         s[j] = new RooRealVar(sname.c_str(), sname.c_str(), iniPar[2 * j + 1], -10, 10);
 
-//             m[j] = new RooRealVar(mname.c_str(),mname.c_str(),iniPar[2*j],-10000,10000) ;
-//             s[j] = new RooRealVar(sname.c_str(),sname.c_str(),iniPar[2*j+1],-10000,10000) ;
-            m[j] = new RooRealVar(mname.c_str(),mname.c_str(),iniPar[2*j],-10,10) ;
-            s[j] = new RooRealVar(sname.c_str(),sname.c_str(),iniPar[2*j+1],-10,10) ;
+         std::string gname = "g_" + ROOT::Math::Util::ToString(j);
+         g[j] = new RooGaussian(gname.c_str(), "gauss(x,mean,sigma)", *x[j], *m[j], *s[j]);
 
-            std::string gname = "g_" + ROOT::Math::Util::ToString(j);
-            g[j] = new RooGaussian(gname.c_str(),"gauss(x,mean,sigma)",*x[j],*m[j],*s[j]);
-
-            std::string pname = "prod_" + ROOT::Math::Util::ToString(j);
-            if (j == 0)
-               pdf[0] = g[0];
-            else if (j == 1) {
-               pdf[1] = new RooProdPdf(pname.c_str(),pname.c_str(),RooArgSet(*g[1],*g[0]) );
-            }
-            else if (j > 1) {
-               pdf[j] = new RooProdPdf(pname.c_str(),pname.c_str(),RooArgSet(*g[j],*pdf[j-1]) );
-            }
+         std::string pname = "prod_" + ROOT::Math::Util::ToString(j);
+         if (j == 0)
+            pdf[0] = g[0];
+         else if (j == 1) {
+            pdf[1] = new RooProdPdf(pname.c_str(), pname.c_str(), RooArgSet(*g[1], *g[0]));
+         } else if (j > 1) {
+            pdf[j] = new RooProdPdf(pname.c_str(), pname.c_str(), RooArgSet(*g[j], *pdf[j - 1]));
          }
-
-
+      }
    }
 
-   RooAbsPdf & getPdf() { return *pdf.back(); }
+   RooAbsPdf &getPdf() { return *pdf.back(); }
 
-   std::unique_ptr<RooArgSet>  getVars() {
-      std::unique_ptr<RooArgSet> vars(new RooArgSet() );
+   std::unique_ptr<RooArgSet> getVars()
+   {
+      std::unique_ptr<RooArgSet> vars(new RooArgSet());
       for (unsigned int i = 0; i < x.size(); ++i)
          vars->add(*x[i]);
       return vars;
    }
 
-   ~MultiGaussRooPdf() {
+   ~MultiGaussRooPdf()
+   {
       // free
       int n = x.size();
       for (int j = 0; j < n; ++j) {
@@ -175,50 +167,51 @@ class MultiGaussRooPdf {
          delete m[j];
          delete s[j];
          delete g[j];
-         if (j> 0) delete pdf[j]; // no pdf allocated for j = 0
+         if (j > 0)
+            delete pdf[j]; // no pdf allocated for j = 0
       }
    }
 
-   private:
+private:
+   std::vector<RooRealVar *> x;
+   std::vector<RooRealVar *> m;
+   std::vector<RooRealVar *> s;
 
-      std::vector<RooRealVar *> x;
-      std::vector<RooRealVar *> m;
-      std::vector<RooRealVar *> s;
-
-      std::vector<RooAbsPdf *> g;
-      std::vector<RooAbsPdf *> pdf;
-
+   std::vector<RooAbsPdf *> g;
+   std::vector<RooAbsPdf *> pdf;
 };
 
-
-//unbinned roo fit (large tree)
-int  FitUsingRooFit(TTree & tree, RooAbsPdf & pdf, RooArgSet & xvars) {
+// unbinned roo fit (large tree)
+int FitUsingRooFit(TTree &tree, RooAbsPdf &pdf, RooArgSet &xvars)
+{
 
    int iret = 0;
    std::cout << "\n************************************************************\n";
    std::cout << "\tFit using RooFit (Likelihood Fit) on " << pdf.GetName() << std::endl;
-
-
 
    TStopwatch w;
    w.Start();
 
    for (int i = 0; i < nfit; ++i) {
 
-      RooDataSet data("unbindata","unbin dataset with x", xvars, RooFit::Import(tree));
+      RooDataSet data("unbindata", "unbin dataset with x", xvars, RooFit::Import(tree));
 
 #ifdef DEBUG
       int level = 2;
       std::cout << "num entries = " << data.numEntries() << std::endl;
       bool save = true;
-      std::unique_ptr<RooArgSet>{pdf.getVariables()}->Print("v"); // print the parameters
+      std::unique_ptr<RooArgSet>
+      {
+         pdf.getVariables()
+      } -> Print("v"); // print the parameters
       std::cout << "\n\nDo the fit now \n\n";
 #else
       int level = -1;
       bool save = false;
 #endif
 
-      std::unique_ptr<RooFitResult> result{pdf.fitTo(data, RooFit::Minos(0), RooFit::Hesse(0) , RooFit::PrintLevel(level), RooFit::Save(save) )};
+      std::unique_ptr<RooFitResult> result{
+         pdf.fitTo(data, RooFit::Minos(0), RooFit::Hesse(0), RooFit::PrintLevel(level), RooFit::Save(save))};
 
 #ifdef DEBUG
       assert(result != nullptr);
@@ -226,7 +219,6 @@ int  FitUsingRooFit(TTree & tree, RooAbsPdf & pdf, RooArgSet & xvars) {
       result->Print();
 #endif
       iret |= (result == nullptr);
-
    }
 
    w.Stop();
@@ -235,37 +227,35 @@ int  FitUsingRooFit(TTree & tree, RooAbsPdf & pdf, RooArgSet & xvars) {
    std::unique_ptr<RooArgSet> params{pdf.getParameters(xvars)};
    params->Print("v");
 
-
    std::cout << "\nTime: \t" << w.RealTime() << " , " << w.CpuTime() << std::endl;
    std::cout << "\n************************************************************\n";
    return iret;
 }
 
-
 // unbin fit
 template <class MinType>
-int DoFit(TTree * tree, Func & func, bool debug = false, bool = false ) {
+int DoFit(TTree *tree, Func &func, bool debug = false, bool = false)
+{
 
    ROOT::Fit::UnBinData d;
    // need to have done Tree->Draw() before fit
-   FillUnBinData(d,tree);
+   FillUnBinData(d, tree);
 
-   //std::cout << "Fit parameter 2  " << f.Parameters()[2] << std::endl;
+   // std::cout << "Fit parameter 2  " << f.Parameters()[2] << std::endl;
 
    ROOT::Fit::Fitter fitter;
    fitter.Config().MinimizerOptions().SetPrintLevel(0);
-   fitter.Config().SetMinimizer(MinType::name().c_str(),MinType::name2().c_str());
+   fitter.Config().SetMinimizer(MinType::name().c_str(), MinType::name2().c_str());
    fitter.Config().MinimizerOptions().SetTolerance(1.); // to be consistent with RooFit
 
    if (debug)
       fitter.Config().MinimizerOptions().SetPrintLevel(3);
 
-
    // create the function
 
    fitter.SetFunction(func);
    // need to fix param 0 , normalization in the unbinned fits
-   //fitter.Config().ParSettings(0).Fix();
+   // fitter.Config().ParSettings(0).Fix();
 
    bool ret = fitter.Fit(d);
    if (!ret) {
@@ -274,28 +264,30 @@ int DoFit(TTree * tree, Func & func, bool debug = false, bool = false ) {
    }
    fitter.Result().Print(std::cout);
    return 0;
-
 }
 
 template <class MinType, class FitObj>
-int FitUsingNewFitter(FitObj * fitobj, Func & func, bool useGrad=false) {
+int FitUsingNewFitter(FitObj *fitobj, Func &func, bool useGrad = false)
+{
 
    std::cout << "\n************************************************************\n";
    std::cout << "\tFit using new Fit::Fitter\n";
    std::cout << "\tMinimizer is " << MinType::name() << "  " << MinType::name2() << std::endl;
 
    int iret = 0;
-   TStopwatch w; w.Start();
+   TStopwatch w;
+   w.Start();
 
 #ifdef DEBUG
    func.SetParameters(iniPar);
-   iret |= DoFit<MinType>(fitobj,func,true, useGrad);
+   iret |= DoFit<MinType>(fitobj, func, true, useGrad);
 
 #else
    for (int i = 0; i < nfit; ++i) {
       func.SetParameters(iniPar);
-      iret = DoFit<MinType>(fitobj,func, false, useGrad);
-      if (iret != 0) break;
+      iret = DoFit<MinType>(fitobj, func, false, useGrad);
+      if (iret != 0)
+         break;
    }
 #endif
    w.Stop();
@@ -307,18 +299,16 @@ int FitUsingNewFitter(FitObj * fitobj, Func & func, bool useGrad=false) {
 
 template <int N>
 struct GausNorm {
-   static double F(const double *x, const double *p) {
-      return ROOT::Math::normal_pdf(x[N-1], p[2*N-1], p[2*N-2] ) * GausNorm<N-1>::F(x,p);
+   static double F(const double *x, const double *p)
+   {
+      return ROOT::Math::normal_pdf(x[N - 1], p[2 * N - 1], p[2 * N - 2]) * GausNorm<N - 1>::F(x, p);
    }
 };
 template <>
 struct GausNorm<1> {
 
-   static double F(const double *x, const double *p) {
-      return ROOT::Math::normal_pdf(x[0], p[1], p[0] );
-   }
+   static double F(const double *x, const double *p) { return ROOT::Math::normal_pdf(x[0], p[1], p[0]); }
 };
-
 
 // double gausnorm(
 //    return ROOT::Math::normal_pdf(x[0], p[1], p[0] );
@@ -329,46 +319,42 @@ struct GausNorm<1> {
 //    // return std::exp(-0.5 * tmp*tmp ) * sqrt_2pi * invsig;
 // }
 
+int main()
+{
 
-
-int main() {
-
-   TTree tree("t","a large Tree with many gaussian variables");
+   TTree tree("t", "a large Tree with many gaussian variables");
    fillTree(tree);
 
    // set initial parameters
    for (int i = 0; i < N; ++i) {
-      iniPar[2*i] = 1;
-      iniPar[2*i+1] = 1;
+      iniPar[2 * i] = 1;
+      iniPar[2 * i + 1] = 1;
    }
 
    // start counting the time
    MultiGaussRooPdf multipdf(N);
-   RooAbsPdf & pdf = multipdf.getPdf();
+   RooAbsPdf &pdf = multipdf.getPdf();
 
    std::unique_ptr<RooArgSet> xvars(multipdf.getVars());
 
-   WrapperRooPdf  wpdf( &pdf, *xvars );
-
+   WrapperRooPdf wpdf(&pdf, *xvars);
 
    std::cout << "ndim " << wpdf.NDim() << std::endl;
    std::cout << "npar " << wpdf.NPar() << std::endl;
    for (unsigned int i = 0; i < wpdf.NPar(); ++i)
-      std::cout << " par " << i << " is " <<  wpdf.ParameterName(i) << " value " << wpdf.Parameters()[i] << std::endl;
+      std::cout << " par " << i << " is " << wpdf.ParameterName(i) << " value " << wpdf.Parameters()[i] << std::endl;
 
-
-   FitUsingNewFitter<TMINUIT>(&tree,wpdf);
+   FitUsingNewFitter<TMINUIT>(&tree, wpdf);
 
    // reset pdf original values
    wpdf.SetParameters(iniPar);
 
-   FitUsingRooFit(tree,pdf, *xvars);
+   FitUsingRooFit(tree, pdf, *xvars);
 
    // in case of N = 1 do also a simple gauss fit
    // using TF1 gausN
-//   if (N == 1) {
-   ROOT::Math::WrappedParamFunction<> gausn(&GausNorm<N>::F,2*N,iniPar,iniPar+2*N);
-   FitUsingNewFitter<TMINUIT>(&tree,gausn);
+   //   if (N == 1) {
+   ROOT::Math::WrappedParamFunction<> gausn(&GausNorm<N>::F, 2 * N, iniPar, iniPar + 2 * N);
+   FitUsingNewFitter<TMINUIT>(&tree, gausn);
    //}
-
 }
