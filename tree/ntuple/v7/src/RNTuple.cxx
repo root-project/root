4fe5d8f806d896aaa05dc5bf7d8e891d66c5a0e1
silverweed <silverweed14@proton.me>
2024-05-15 09:13:01 +0200

    [ntuple] Manually read RNTuple fields in Streamer()
    ...instead of relying on ReadClassBuffer(). This is necessary for
    schema evolution with RNTuple's custom streamer.

--- tree/ntuple/v7/src/RNTuple.cxx
+++ tree/ntuple/v7/src/RNTuple.cxx
@@ -42,7 +42,20 @@ void ROOT::Experimental::RNTuple::Streamer(TBuffer &buf)
       auto offCkData = offClassBuf + sizeof(UInt_t) + sizeof(Version_t);
       auto checksum = XXH3_64bits(buf.Buffer() + offCkData, lenCkData);
 
-      buf.ReadClassBuffer(RNTuple::Class(), this, classVersion, offClassBuf, bcnt);
+      buf >> fVersionEpoch;
+      buf >> fVersionMajor;
+      buf >> fVersionMinor;
+      buf >> fVersionPatch;
+      buf >> fSeekHeader;
+      buf >> fNBytesHeader;
+      buf >> fLenHeader;
+      buf >> fSeekFooter;
+      buf >> fNBytesFooter;
+      buf >> fLenFooter;
+      // New versions may add members here ...
+      // ... so we skip all but the last 8 bytes for fwd compatibility
+      buf.SetBufferOffset(offClassBuf + sizeof(UInt_t) + bcnt - sizeof(fChecksum));
+      buf >> fChecksum;
 
       if (checksum != fChecksum)
          throw RException(R__FAIL("checksum mismatch in RNTuple anchor"));
