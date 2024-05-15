8ba3d1f1fe4935d1e0e7b5151835c31e4a32fa48
kristupaspranc <kristupaspranc@cern.ch>
2024-05-15 18:05:46 +0200

    [tmva] Remove pure stupidness in RBatchGenerator

--- tmva/tmva/inc/TMVA/RBatchLoader.hxx
+++ tmva/tmva/inc/TMVA/RBatchLoader.hxx
@@ -119,24 +119,6 @@ public:
       fBatchCondition.notify_all();
    }
 
-   /// \brief Create a batch filled with the events on given indices with default batch size
-   /// \param chunkTensor
-   /// \param idx
-   /// \return
-   std::unique_ptr<TMVA::Experimental::RTensor<float>>
-   CreateBatch(const TMVA::Experimental::RTensor<float> &chunkTensor, const std::vector<std::size_t> idx)
-   {  
-      auto batch =
-         std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>({fBatchSize, fNumColumns}));
-
-      for (std::size_t i = 0; i < fBatchSize; i++) {
-         std::copy(chunkTensor.GetData() + (idx[i] * fNumColumns), chunkTensor.GetData() + ((idx[i] + 1) * fNumColumns),
-                   batch->GetData() + i * fNumColumns);
-      }
-
-      return batch;
-   }
-
    /// @brief Create a batch filled with the events on given indices with parametrized batch size
    /// @param chunkTensor 
    /// @param idx 
@@ -165,16 +147,11 @@ public:
    CreateFirstBatch(const TMVA::Experimental::RTensor<float> &remainderTensor,
                   std::size_t remainderTensorRow, std::vector<std::size_t> eventIndices){
       auto batch = std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>({fBatchSize, fNumColumns}));
-      
-      {
-         std::vector<std::size_t> idx = std::vector<std::size_t>(remainderTensorRow);
-         std::iota(idx.begin(), idx.end(), 0);
 
-         for(size_t i = 0; i < remainderTensorRow; i++){
-            std::copy(remainderTensor.GetData() + idx[i] * fNumColumns, remainderTensor.GetData() + (idx[i] + 1) * fNumColumns,
-                     batch->GetData() + i * fNumColumns);
+      for(size_t i = 0; i < remainderTensorRow; i++){
+         std::copy(remainderTensor.GetData() + i * fNumColumns, remainderTensor.GetData() + (i + 1) * fNumColumns,
+                  batch->GetData() + i * fNumColumns);
          }
-      }
 
       std::vector<std::size_t> idx;
       for (std::size_t i = 0; i < (fBatchSize - remainderTensorRow); i++) {
@@ -260,7 +237,7 @@ public:
          }
 
          // Fill a batch
-         batches.emplace_back(CreateBatch(fChunkTensor, idx));
+         batches.emplace_back(CreateBatch(fChunkTensor, idx, fBatchSize));
       }
 
       {
@@ -301,7 +278,7 @@ public:
             idx.push_back(eventIndices[i]);
          }
 
-         fValidationBatchQueue.push(CreateBatch(fChunkTensor, idx));
+         fValidationBatchQueue.push(CreateBatch(fChunkTensor, idx, fBatchSize));
       }
 
       fValidationRemainderRow = eventIndices.size() - start;
