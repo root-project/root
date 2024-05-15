a966c9c8e7c88070fb9fe947797a64712bd9098f
Jakob Blomer <jblomer@cern.ch>
2024-05-15 14:35:03 +0200

    [ntuple] add test for unsplit polymorphic type

--- tree/ntuple/v7/test/Unsplit.hxx
+++ tree/ntuple/v7/test/Unsplit.hxx
@@ -3,6 +3,7 @@
 
 #include <Rtypes.h>
 
+#include <memory>
 #include <vector>
 
 struct CyclicMember {
@@ -32,6 +33,25 @@ struct CustomStreamerForceUnsplit {
 // For the time being, RNTuple ignores the unsplit comment marker and does _not_ use an RUnsplitField for such members.
 class IgnoreUnsplitComment {
    std::vector<float> v; //||
+};
+
+// Test unsplit storage of polymorphic type
+
+struct PolyBase {
+   virtual ~PolyBase() {}
+   int x;
+};
+
+struct PolyA : public PolyBase {
+   int a;
+};
+
+struct PolyB : public PolyBase {
+   int b;
+};
+
+struct PolyContainer {
+   std::unique_ptr<PolyBase> fPoly;
 };
 
 #endif // ROOT7_RNTuple_Test_Unsplit
