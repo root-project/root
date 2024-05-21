17769d0bf4c79b55d8524c3aad49dfdb20735cee
Jakob Blomer <jblomer@cern.ch>
2024-05-21 14:00:54 +0200

    [ntuple] add more testing for std::variant

--- tree/ntuple/v7/test/rfield_variant.cxx
+++ tree/ntuple/v7/test/rfield_variant.cxx
@@ -1,5 +1,7 @@
 #include "ntuple_test.hxx"
 
+#include <optional>
+
 TEST(RNTuple, Variant)
 {
    FileRaii fileGuard("test_ntuple_variant.root");
@@ -31,4 +33,26 @@ TEST(RNTuple, Variant)
    EXPECT_EQ(4, *std::get_if<int>(rdVariant));
    reader->LoadEntry(2);
    EXPECT_EQ(8.0, *std::get_if<double>(rdVariant));
+}
+
+TEST(RNTuple, VariantComplex)
+{
+   FileRaii fileGuard("test_ntuple_variant_complex.root");
+
+   {
+      auto model = RNTupleModel::Create();
+      auto ptrVec = model->MakeField<std::vector<std::variant<std::optional<int>, float>>>("vvar");
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath());
+
+      for (int i = 0; i < 10; i++) {
+         ptrVec->clear();
+
+         for (int j = 0; j < 5; ++j) {
+            std::variant<std::optional<int>, float> var(1.0);
+            ptrVec->emplace_back(var);
+         }
+
+         writer->Fill();
+      }
+   }
 }
