749e3cc3f0ef589e44957dabcae3d82421dc4157
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2024-05-27 13:23:55 +0200

    [cling] Correct the assignment of `LangOptions`

--- interpreter/cling/lib/Interpreter/CIFactory.cpp
+++ interpreter/cling/lib/Interpreter/CIFactory.cpp
@@ -1489,7 +1489,7 @@ namespace {
           bool ReadLanguageOptions(const LangOptions &LangOpts,
                                    bool /*Complain*/,
                                    bool /*AllowCompatibleDifferences*/) override {
-            *m_Invocation.getLangOpts() = LangOpts;
+            m_Invocation.getLangOpts() = LangOpts;
             m_ReadLang = true;
             return false;
           }
