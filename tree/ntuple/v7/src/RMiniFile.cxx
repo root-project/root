97a6732e4afa21d866b2c71c1e152f387d0830c5
Jakob Blomer <jblomer@cern.ch>
2024-05-27 23:02:04 +0200

    Revert "[ntuple] add ClassDefInline to RKeyBlob"
    This reverts commit b09317699794dfae30d36df78f8d7f7807fe661f.

--- tree/ntuple/v7/src/RMiniFile.cxx
+++ tree/ntuple/v7/src/RMiniFile.cxx
@@ -13,7 +13,6 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#include "Rtypes.h"
 #include <ROOT/RConfig.hxx>
 #include <ROOT/RError.hxx>
 
@@ -959,8 +958,6 @@ constexpr char const *kNTupleClassName = "ROOT::Experimental::RNTuple";
 /// like a TBasket.
 class RKeyBlob : public TKey {
 public:
-   RKeyBlob() = default;
-
    explicit RKeyBlob(TFile *file) : TKey(file)
    {
       fClassName = kBlobClassName;
@@ -974,8 +971,6 @@ public:
       Create(nbytes);
       *seekKey = fSeekKey;
    }
-
-   ClassDefInline(RKeyBlob, 3)
 };
 
 } // anonymous namespace
