245ef802d1ccff6a1aba3e71bbb2b8bffa20854e
silverweed <giacomo.parolini@cern.ch>
2024-05-28 11:45:24 +0200

    [ntuple] RKeyBlob: ClassDefInline -> ClassDefInlineOverride

--- tree/ntuple/v7/src/RMiniFile.cxx
+++ tree/ntuple/v7/src/RMiniFile.cxx
@@ -989,7 +989,7 @@ constexpr char const *kNTupleClassName = "ROOT::Experimental::RNTuple";
 class RKeyBlob : public TKey {
 public:
    RKeyBlob() = default;
-   
+
    explicit RKeyBlob(TFile *file) : TKey(file)
    {
       fClassName = kBlobClassName;
@@ -1004,9 +1004,8 @@ public:
       *seekKey = fSeekKey;
    }
 
-   ClassDefInline(RKeyBlob, 0)
+   ClassDefInlineOverride(RKeyBlob, 0)
 };
-
 
 namespace ROOT {
 namespace Experimental {
