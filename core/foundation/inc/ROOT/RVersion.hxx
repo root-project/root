391e4cd5708a81ddf513da8cd305d7ec0d7d4395
Danilo Piparo <danilo.piparo@cern.ch>
2024-05-28 06:19:25 +0200

    Update ROOT version files to v6.32.01

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 32
-#define ROOT_VERSION_PATCH 00
+#define ROOT_VERSION_PATCH 01
 #define ROOT_RELEASE_DATE "May 28 2024"
 
 /* Don't change the lines below. */
