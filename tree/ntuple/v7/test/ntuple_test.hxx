0e1166b188cb3f828f9bef0ec722ae04de001551
silverweed <giacomo.parolini@cern.ch>
2024-05-29 11:25:53 +0200

    [ntuple] add a way to preserve a test file for local debugging
    reformat ntuple_test.hxx

--- tree/ntuple/v7/test/ntuple_test.hxx
+++ tree/ntuple/v7/test/ntuple_test.hxx
@@ -117,12 +117,22 @@ using RResult = ROOT::Experimental::RResult<T>;
 class FileRaii {
 private:
    std::string fPath;
+   bool fPreserveFile = false;
+
 public:
-   explicit FileRaii(const std::string &path) : fPath(path) { }
-   FileRaii(const FileRaii&) = delete;
-   FileRaii& operator=(const FileRaii&) = delete;
-   ~FileRaii() { std::remove(fPath.c_str()); }
+   explicit FileRaii(const std::string &path) : fPath(path) {}
+   FileRaii(const FileRaii &) = delete;
+   FileRaii &operator=(const FileRaii &) = delete;
+   ~FileRaii()
+   {
+      if (!fPreserveFile)
+         std::remove(fPath.c_str());
+   }
    std::string GetPath() const { return fPath; }
+
+   // Useful if you want to keep a test file after the test has finished running
+   // for debugging purposes. Should only be used locally and never pushed.
+   void PreserveFile() { fPreserveFile = true; }
 };
 
 #endif
