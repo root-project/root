a067ba367d1b920910f1b8012c0e67b7beefa953
Jakob Blomer <jblomer@cern.ch>
2024-05-29 16:00:14 +0200

    [ntuple] add fHasChecksum member to RPageInfo
    The page info descriptor part stores if the page comes with a checksum.
    The page list serialization uses the information to set the checksum
    bit.

--- tree/ntuple/v7/test/ntuple_serialize.cxx
+++ tree/ntuple/v7/test/ntuple_serialize.cxx
@@ -649,8 +649,14 @@ TEST(RNTuple, SerializeFooter)
    clusterBuilder.ClusterId(84).FirstEntryIndex(0).NEntries(100);
    ROOT::Experimental::RClusterDescriptor::RPageRange pageRange;
    pageRange.fPhysicalColumnId = 17;
-   pageInfo.fNElements = 100;
+   // Two pages adding up to 100 elements, one with checksum one without
+   pageInfo.fNElements = 40;
    pageInfo.fLocator.fPosition = 7000U;
+   pageInfo.fHasChecksum = true;
+   pageRange.fPageInfos.emplace_back(pageInfo);
+   pageInfo.fNElements = 60;
+   pageInfo.fLocator.fPosition = 8000U;
+   pageInfo.fHasChecksum = false;
    pageRange.fPageInfos.emplace_back(pageInfo);
    clusterBuilder.CommitColumnRange(17, 0, 100, pageRange);
    builder.AddCluster(clusterBuilder.MoveDescriptor().Unwrap());
@@ -721,9 +727,13 @@ TEST(RNTuple, SerializeFooter)
    EXPECT_EQ(100u, columnRange.fNElements);
    EXPECT_EQ(0u, columnRange.fFirstElementIndex);
    pageRange = clusterDesc.GetPageRange(0).Clone();
-   EXPECT_EQ(1u, pageRange.fPageInfos.size());
-   EXPECT_EQ(100u, pageRange.fPageInfos[0].fNElements);
+   EXPECT_EQ(2u, pageRange.fPageInfos.size());
+   EXPECT_EQ(40u, pageRange.fPageInfos[0].fNElements);
    EXPECT_EQ(7000u, pageRange.fPageInfos[0].fLocator.GetPosition<std::uint64_t>());
+   EXPECT_TRUE(pageRange.fPageInfos[0].fHasChecksum);
+   EXPECT_EQ(60u, pageRange.fPageInfos[1].fNElements);
+   EXPECT_EQ(8000u, pageRange.fPageInfos[1].fLocator.GetPosition<std::uint64_t>());
+   EXPECT_FALSE(pageRange.fPageInfos[1].fHasChecksum);
 }
 
 TEST(RNTuple, SerializeFooterXHeader)
