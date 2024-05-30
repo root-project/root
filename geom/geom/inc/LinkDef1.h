3721ad8f972d8a18b424610abdd6cb2f39c1673e
Danilo Piparo <danilo.piparo@cern.ch>
2024-05-30 16:59:27 +0200

    LinkDefs: __CINT__-->__CLING__

--- geom/geom/inc/LinkDef1.h
+++ geom/geom/inc/LinkDef1.h
@@ -8,7 +8,7 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#ifdef __CINT__
+#ifdef __CLING__
 
 #pragma link C++ global gGeoManager;
 #pragma link C++ global gGeoIdentity;
--- gui/fitpanelv7/inc/LinkDef.h
+++ gui/fitpanelv7/inc/LinkDef.h
@@ -6,7 +6,7 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#ifdef __CINT__
+#ifdef __CLING__
 
 #pragma link C++ struct ROOT::Experimental::RFitPanelModel+;
 #pragma link C++ struct ROOT::Experimental::RFitPanelModel::RComboBoxItem+;
