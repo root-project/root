802d26de10a6f42d0a2148a215389f9262d8bfc4
Silverweed <7806878+silverweed@users.noreply.github.com>
2024-06-06 13:55:16 +0200

    [ntuple] Add tests for forward compatibility (#15618)
    * [ntuple] correctly write ntuple anchor blob and re-read it
    
    WIP: trying to craft a valid ntuple anchor by hand
    
    WIP: handcrafted ntuple + key now parse properly
    
    * [ntuple] add fwd compat test
    
    * [ntuple] add ClassDef to RHandcraftedKeyBlob
    
    * [ntuple] Remove byte-swapped strings from fwd compat tests
    
    The ROOT format is always big-endian, so it makes no sense to swap the
    bytes depending on the endianness.
    
    WIP: some fiddling around
    
    WIP: new way of doing fwd compat
    
    WIP: make rxtuple jit properly
    
    WIP: fix checksumming of rxtuple
    
    * [ntuple] remove unused code from fwd compat test
    
    * [ntuple] make fwd compat test work
    
    now RXTuple's StreamerInfo is being saved uncompressed and correctly patched
    
    * [ntuple] read back future ntuple with RMiniFile in fwd compat test
    
    * [ntuple] some fixes to RXTuple
    
    * [ntuple] tentative fix for CMakeLists.txt on Windows
    
    * [ntuple] check that the file size is small in test RXTuple
    
    * [ntuple] potentially fix CMakeLists for RXTuple
    
    * [ntuple] check ret val of fread and fwrite in ntuple_compat

---
+++ tree/ntuple/v7/test/RXTuple.cxx
@@ -0,0 +1,20 @@
+#include <ROOT/RError.hxx>
+#include "RXTuple.hxx"
+
+#include <TBuffer.h>
+#include <TError.h>
+#include <TFile.h>
+
+#include <xxhash.h>
+
+void ROOT::Experimental::RXTuple::Streamer(TBuffer &buf)
+{
+   if (buf.IsReading()) {
+      assert(!"This class should never be read!");
+   } else {
+      auto offCkData = buf.Length() + sizeof(UInt_t) + sizeof(Version_t);
+      buf.WriteClassBuffer(RXTuple::Class(), this);
+      std::uint64_t checksum = XXH3_64bits(buf.Buffer() + offCkData, buf.Length() - offCkData);
+      buf << checksum; 
+   }
+}
