208061dc34d9275bf9535064032004f17c2bac5c
Jakob Blomer <jblomer@cern.ch>
2024-06-07 21:24:02 +0200

    [ntuple] fix unbuffered compression of large pages

--- tree/ntuple/v7/test/ntuple_extended.cxx
+++ tree/ntuple/v7/test/ntuple_extended.cxx
@@ -5,6 +5,8 @@
 #include <TRandom3.h>
 #include <TROOT.h>
 
+#include <random>
+
 TEST(RNTuple, RealWorld1)
 {
 #ifdef R__USE_IMT
@@ -268,6 +270,44 @@ TEST(RNTuple, LargeFile2)
    }
 }
 #endif
+
+TEST(RNTuple, LargePages)
+{
+   FileRaii fileGuard("test_ntuple_large_pages.root");
+
+   for (const auto useBufferedWrite : {true, false}) {
+      {
+         auto model = RNTupleModel::Create();
+         auto fldRnd = model->MakeField<std::uint32_t>("rnd");
+         RNTupleWriteOptions options;
+         // Larger than the 16MB compression block limit
+         options.SetApproxUnzippedPageSize(32 * 1024 * 1024);
+         options.SetUseBufferedWrite(useBufferedWrite);
+         auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath(), options);
+
+         std::mt19937 gen;
+         std::uniform_int_distribution<std::uint32_t> distrib;
+         for (int i = 0; i < 25 * 1000 * 1000; ++i) { // 100 MB of int data
+            *fldRnd = distrib(gen);
+            writer->Fill();
+         }
+         writer.reset();
+      }
+
+      auto reader = RNTupleReader::Open("ntpl", fileGuard.GetPath());
+      const auto &desc = reader->GetDescriptor();
+      const auto rndColId = desc.FindPhysicalColumnId(desc.FindFieldId("rnd"), 0);
+      const auto &clusterDesc = desc.GetClusterDescriptor(desc.FindClusterId(rndColId, 0));
+      EXPECT_GT(clusterDesc.GetPageRange(rndColId).Find(0).fLocator.fBytesOnStorage, kMAXZIPBUF);
+
+      auto viewRnd = reader->GetView<std::uint32_t>("rnd");
+      std::mt19937 gen;
+      std::uniform_int_distribution<std::uint32_t> distrib;
+      for (const auto i : reader->GetEntryRange()) {
+         EXPECT_EQ(distrib(gen), viewRnd(i));
+      }
+   }
+}
 
 // FIXME: apparently, this test continues to be broken for some CI configs, which needs to be investigated carefully;
 // thus disable temporarily.
