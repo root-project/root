bde89315ec92964c33fd715cdcf8bafd1b433080
Jonas Rembser <jonas.rembser@cern.ch>
2024-06-07 16:32:34 +0200

    [PyROOT] Use PyROOT and not just cppyy in tests dealing with ROOT types
    It is not a feature of cppyy, but only of our patches that cppyy can
    deal with `Long64_t` and the other basic ROOT typedefs.
    
    In tests that have to do with these types, we should therefore use ROOT,
    so we don't rely on a patched cppyy and can move dealing with the ROOT
    types into our pythonizations.

--- python/regression/exec_root_6023.py
+++ python/regression/exec_root_6023.py
@@ -5,10 +5,10 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__))) # for common
 from common import check_cppyy_backend
 check_cppyy_backend()
 
-import cppyy
-cppyy.gbl.gROOT.ProcessLine('.L root_6023.h+')
+import ROOT
+ROOT.gROOT.ProcessLine('.L root_6023.h+')
 
-c = cppyy.gbl.instance()
+c = ROOT.instance()
 
 a = c.data
 b = c.value()
