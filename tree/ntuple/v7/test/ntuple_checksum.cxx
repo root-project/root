28b47cde4928a4dc5f2d05ceea093825bfdb34fb
Jakob Blomer <jblomer@cern.ch>
2024-06-07 10:09:46 +0200

    [ntuple] make test helper for corrupted file generic

--- tree/ntuple/v7/test/ntuple_checksum.cxx
+++ tree/ntuple/v7/test/ntuple_checksum.cxx
@@ -1,33 +1,58 @@
 #include "ntuple_test.hxx"
 
+#include <cstring>
+
 namespace {
 
-void CreateCorruptedFile(const std::string &fileName)
+void CreateCorruptedFile(const std::string &uri)
 {
+   RNTupleWriteOptions options;
+   options.SetCompression(0);
+
    auto model = RNTupleModel::Create();
    auto ptrPx = model->MakeField<float>("px", 1.0);
    auto ptrPy = model->MakeField<float>("py", 2.0);
-   RNTupleWriteOptions options;
-   options.SetCompression(0);
-   auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileName, options);
+   model->Freeze();
+   auto modelClone = model->Clone(); // required later to write the corrupted version
+   auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", uri, options);
    writer->Fill();
    writer.reset();
 
-   auto pageSource = RPageSource::Create("ntpl", fileName);
+   // Load sealed pages to memory
+   auto pageSource = RPageSource::Create("ntpl", uri);
    pageSource->Attach();
    auto descGuard = pageSource->GetSharedDescriptorGuard();
    const auto pxColId = descGuard->FindPhysicalColumnId(descGuard->FindFieldId("px"), 0);
+   const auto pyColId = descGuard->FindPhysicalColumnId(descGuard->FindFieldId("py"), 0);
    const auto clusterId = descGuard->FindClusterId(pxColId, 0);
-   const auto &clusterDesc = descGuard->GetClusterDescriptor(clusterId);
-   const auto pageInfo = clusterDesc.GetPageRange(pxColId).fPageInfos[0];
-   EXPECT_EQ(4u, pageInfo.fLocator.fBytesOnStorage);
-   EXPECT_TRUE(pageInfo.fHasChecksum);
+   RClusterIndex index{clusterId, 0};
 
-   std::uint64_t wrongChecksum = 0;
-   FILE *f = fopen(fileName.c_str(), "r+b");
-   fseek(f, pageInfo.fLocator.GetPosition<std::uint64_t>() + 4, SEEK_SET);
-   fwrite(&wrongChecksum, sizeof(wrongChecksum), 1, f);
-   fclose(f);
+   constexpr std::size_t bufSize = sizeof(float) + RPageStorage::kNBytesPageChecksum;
+   unsigned char pxBuffer[bufSize];
+   RPageStorage::RSealedPage pxSealedPage;
+   pxSealedPage.SetBufferSize(bufSize);
+   pxSealedPage.SetBuffer(pxBuffer);
+   pageSource->LoadSealedPage(pxColId, index, pxSealedPage);
+   EXPECT_EQ(bufSize, pxSealedPage.GetBufferSize());
+   unsigned char pyBuffer[bufSize];
+   RPageStorage::RSealedPage pySealedPage;
+   pySealedPage.SetBufferSize(bufSize);
+   pySealedPage.SetBuffer(pyBuffer);
+   pageSource->LoadSealedPage(pyColId, index, pySealedPage);
+   EXPECT_EQ(bufSize, pySealedPage.GetBufferSize());
+
+   // Corrupt px sealed page's checksum
+   memset(pxBuffer + sizeof(float), 0, RPageStorage::kNBytesPageChecksum);
+
+   // Rewrite RNTuple with valid py page and corrupted px page
+   auto pageSink = ROOT::Experimental::Internal::RPagePersistentSink::Create("ntpl", uri, options);
+   pageSink->Init(*modelClone);
+   pageSink->CommitSealedPage(pxColId, pxSealedPage);
+   pageSink->CommitSealedPage(pyColId, pySealedPage);
+   pageSink->CommitCluster(1);
+   pageSink->CommitClusterGroup();
+   pageSink->CommitDataset();
+   modelClone.reset();
 }
 
 #ifdef R__USE_IMT
