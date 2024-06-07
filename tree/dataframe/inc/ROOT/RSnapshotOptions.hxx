703351ada84482c2f14bed958ef18d71650681bf
silverweed <giacomo.parolini@cern.ch>
2024-06-07 17:19:24 +0200

    [skip-ci][RDF] clang-format some RDF tests

--- tree/dataframe/inc/ROOT/RSnapshotOptions.hxx
+++ tree/dataframe/inc/ROOT/RSnapshotOptions.hxx
@@ -26,8 +26,13 @@ struct RSnapshotOptions {
    RSnapshotOptions(RSnapshotOptions &&) = default;
    RSnapshotOptions(std::string_view mode, ECAlgo comprAlgo, int comprLevel, int autoFlush, int splitLevel, bool lazy,
                     bool overwriteIfExists = false)
-      : fMode(mode), fCompressionAlgorithm(comprAlgo), fCompressionLevel{comprLevel}, fAutoFlush(autoFlush),
-        fSplitLevel(splitLevel), fLazy(lazy), fOverwriteIfExists(overwriteIfExists)
+      : fMode(mode),
+        fCompressionAlgorithm(comprAlgo),
+        fCompressionLevel{comprLevel},
+        fAutoFlush(autoFlush),
+        fSplitLevel(splitLevel),
+        fLazy(lazy),
+        fOverwriteIfExists(overwriteIfExists)
    {
    }
    std::string fMode = "RECREATE";             ///< Mode of creation of output file
@@ -38,7 +43,7 @@ struct RSnapshotOptions {
    bool fLazy = false;                         ///< Do not start the event loop when Snapshot is called
    bool fOverwriteIfExists = false; ///< If fMode is "UPDATE", overwrite object in output file if it already exists
 };
-} // ns RDF
-} // ns ROOT
+} // namespace RDF
+} // namespace ROOT
 
 #endif
