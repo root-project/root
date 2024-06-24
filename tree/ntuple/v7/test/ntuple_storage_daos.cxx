0dc1b796467e3a388a6d33c087a61cfe5c231a90
Jakob Blomer <jblomer@cern.ch>
2024-06-24 14:53:01 +0200

    [ntuple] minor improvement in DAOS test
    Co-authored-by: Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>

--- tree/ntuple/v7/test/ntuple_storage_daos.cxx
+++ tree/ntuple/v7/test/ntuple_storage_daos.cxx
@@ -293,7 +293,7 @@ TEST_F(RPageStorageDaos, CagedPages)
       auto elem = ROOT::Experimental::Internal::RColumnElementBase::Generate<std::uint32_t>(colType);
       auto page = pageSource->UnsealPage(sealedPage, *elem, colId);
       EXPECT_GT(page.GetNElements(), 0);
-      auto ptrData = static_cast<uint32_t *>(page.GetBuffer());
+      auto ptrData = static_cast<std::uint32_t *>(page.GetBuffer());
       for (std::uint32_t i = 0; i < page.GetNElements(); ++i) {
          EXPECT_EQ(i, *(ptrData + i));
       }
