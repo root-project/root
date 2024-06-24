a95e3f59244f7231b3413d1244287cdc3d5f6a20
Sergey Linev <S.Linev@gsi.de>
2024-06-24 10:42:57 +0200

    [web] use web-based geometry painter by default
    Disable it when --web=off specified.
    Let configure default kind of geometry painter with
    GeomPainter.Name parameter in rootrc file.

--- core/base/src/TROOT.cxx
+++ core/base/src/TROOT.cxx
@@ -2836,6 +2836,7 @@ void TROOT::SetWebDisplay(const char *webdisplay)
    static TString canName = gEnv->GetValue("Canvas.Name", "");
    static TString brName = gEnv->GetValue("Browser.Name", "");
    static TString trName = gEnv->GetValue("TreeViewer.Name", "");
+   static TString geomName = gEnv->GetValue("GeomPainter.Name", "");
 
    fIsWebDisplayBatch = fBatch;
 
@@ -2871,10 +2872,12 @@ void TROOT::SetWebDisplay(const char *webdisplay)
       gEnv->SetValue("Canvas.Name", canName);
       gEnv->SetValue("Browser.Name", brName);
       gEnv->SetValue("TreeViewer.Name", trName);
+      gEnv->SetValue("GeomPainter.Name", geomName);
    } else {
       gEnv->SetValue("Canvas.Name", "TRootCanvas");
       gEnv->SetValue("Browser.Name", "TRootBrowser");
       gEnv->SetValue("TreeViewer.Name", "TTreeViewer");
+      gEnv->SetValue("GeomPainter.Name", "root");
    }
 }
 
