4047c57d1c9c4381b33f7c14a88338771c1f5d88
Jakob Blomer <jblomer@cern.ch>
2024-07-05 10:37:07 +0200

    [ntuple] move EColumnType to RNTupleUtil.hxx

--- tree/ntuple/v7/inc/ROOT/RColumnModel.hxx
+++ tree/ntuple/v7/inc/ROOT/RColumnModel.hxx
@@ -23,58 +23,6 @@
 namespace ROOT {
 namespace Experimental {
 
-// clang-format off
-/**
-\class ROOT::Experimental::EColumnType
-\ingroup NTuple
-\brief The available trivial, native content types of a column
-
-More complex types, such as classes, get translated into columns of such simple types by the RField.
-New types need to be accounted for in RColumnElementBase::Generate() and RColumnElementBase::GetBitsOnStorage(), too.
-When changed, remember to update
-  - RColumnElement::Generate()
-  - RColumnElement::GetBitsOnStorage()
-  - RColumnElement::GetTypeName()
-  - RColumnElement template specializations / packing & unpacking
-  - If necessary, endianess handling for the packing + unit test in ntuple_endian
-  - RNTupleSerializer::[Des|S]erializeColumnType
-*/
-// clang-format on
-enum class EColumnType {
-   kUnknown = 0,
-   // type for root columns of (nested) collections; offsets are relative to the current cluster
-   kIndex64,
-   kIndex32,
-   // 96 bit column that is a pair of a kIndex64 and a 32bit dispatch tag to a column ID;
-   // used to serialize std::variant.
-   kSwitch,
-   kByte,
-   kChar,
-   kBit,
-   kReal64,
-   kReal32,
-   kReal16,
-   kInt64,
-   kUInt64,
-   kInt32,
-   kUInt32,
-   kInt16,
-   kUInt16,
-   kInt8,
-   kUInt8,
-   kSplitIndex64,
-   kSplitIndex32,
-   kSplitReal64,
-   kSplitReal32,
-   kSplitInt64,
-   kSplitUInt64,
-   kSplitInt32,
-   kSplitUInt32,
-   kSplitInt16,
-   kSplitUInt16,
-   kMax,
-};
-
 } // namespace Experimental
 } // namespace ROOT
 
