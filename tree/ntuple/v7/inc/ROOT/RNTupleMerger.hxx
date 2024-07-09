35219f2e5766afb8e927e3effb07ae7805339775
silverweed <giacomo.parolini@cern.ch>
2024-07-09 16:49:39 +0200

    [ntuple] add late model extension to RNTupleMerger
    [ntuple] extract ExtendOutputModel from Merge

--- tree/ntuple/v7/inc/ROOT/RNTupleMerger.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleMerger.hxx
@@ -31,11 +31,22 @@ namespace ROOT {
 namespace Experimental {
 namespace Internal {
 
+enum class RNTupleMergingMode {
+   /// The merger will discard all columns that aren't present in the prototype model (i.e. the model of the first
+   /// source)
+   kFilter,
+   /// The merger will update the output model to include all columns from all sources. Entries corresponding to columns
+   /// that are not present in a source will be set to the default value of the type.
+   kUnion
+};
+
 struct RNTupleMergeOptions {
    /// If `fCompressionSettings == kUnknownCompressionSettings` (the default), the merger will not change the
    /// compression of any of its sources (fast merging). Otherwise, all sources will be converted to the specified
    /// compression algorithm and level.
    int fCompressionSettings = kUnknownCompressionSettings;
+   /// Determines how the merging treats sources with different models (\see RNTupleMergingMode).
+   RNTupleMergingMode fMergingMode = RNTupleMergingMode::kFilter;
 };
 
 // clang-format off
@@ -47,39 +58,51 @@ struct RNTupleMergeOptions {
  */
 // clang-format on
 class RNTupleMerger {
-
 private:
+   // Internal map that holds column name, type, and type id : output ID information
+   std::unordered_map<std::string, DescriptorId_t> fOutputIdMap;
+
    // Struct to hold column information
    struct RColumnInfo {
-      std::string fColumnName; ///< The qualified field name to which the column belongs, followed by the column index
-      std::string fColumnTypeAndVersion; ///< "<type>.<version>" of the field to which the column belongs
+      /// The qualified field name to which the column belongs, followed by the column index, type and version
+      std::string fColumnNameTypeAndVersion;
       DescriptorId_t fColumnInputId;
       DescriptorId_t fColumnOutputId;
 
       RColumnInfo(const std::string &name, const std::string &typeAndVersion, const DescriptorId_t &inputId,
                   const DescriptorId_t &outputId)
-         : fColumnName(name), fColumnTypeAndVersion(typeAndVersion), fColumnInputId(inputId), fColumnOutputId(outputId)
+         : fColumnNameTypeAndVersion(name + "." + typeAndVersion), fColumnInputId(inputId), fColumnOutputId(outputId)
       {
       }
    };
 
-   /// Build the internal column id map from the first source
-   /// This is where we assign the output ids for the first source
-   void BuildColumnIdMap(std::vector<RColumnInfo> &columns);
-
    /// Validate the columns against the internal map that is built from the first source
    /// This is where we assign the output ids for the remaining sources
-   void ValidateColumns(std::vector<RColumnInfo> &columns);
+   void ValidateColumns(std::vector<RColumnInfo> &columns) const;
 
-   /// Recursively add columns from a given field
-   void AddColumnsFromField(std::vector<RColumnInfo> &columns, const RNTupleDescriptor &desc,
-                            const RFieldDescriptor &fieldDesc, const std::string &prefix = "");
+   /// Recursively add columns from a given field.
+   /// The columns are added in such a way that all already-seen columns (i.e. the ones whose name and version
+   /// were already in `fOutputIdMap`) are at the end of `columns` and all new ones are at the start of it.
+   /// Old and new columns preserve their relative order, so this input:
+   ///
+   ///    [old0, old1, new0, old2, new1, new2]
+   ///
+   /// will be mapped to this output:
+   ///
+   ///    [new0, new1, new2, old0, old1, old2]
+   ///
+   /// Returns the number of new columns.
+   size_t AddColumnsFromField(std::vector<RColumnInfo> &columns, const RNTupleDescriptor &desc,
+                              const RFieldDescriptor &fieldDesc, const std::string &prefix = "") const;
 
    /// Recursively collect all the columns for all the fields rooted at field zero
-   void CollectColumns(const RNTupleDescriptor &descriptor, std::vector<RColumnInfo> &columns);
+   /// Returns the number of new columns added.
+   size_t CollectColumns(const RNTupleDescriptor &descriptor, RNTupleMergingMode mergingMode,
+                         std::vector<RColumnInfo> &columns);
 
-   // Internal map that holds column name, type, and type id : output ID information
-   std::unordered_map<std::string, DescriptorId_t> fOutputIdMap;
+   /// Adds the new columns `newCols` to the destination's model `model`.
+   void ExtendOutputModel(RNTupleModel &model, std::span<RColumnInfo> newCols, int nDstEntries,
+                          const RNTupleDescriptor &descriptor, RPageSink &destination) const;
 
 public:
    /// Merge a given set of sources into the destination
