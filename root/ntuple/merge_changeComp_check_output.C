5e3e8d2bfd925fc9b456772531c58783166223d2
silverweed <giacomo.parolini@cern.ch>
2024-07-11 17:29:21 +0200

    WIP: use PRECMD/POSTCMD for merge tests

---
+++ root/ntuple/merge_changeComp_check_output.C
@@ -0,0 +1,36 @@
+#include <ROOT/RLogger.hxx>
+#include <ROOT/RNTupleModel.hxx>
+#include <ROOT/RNTupleReader.hxx>
+#include <ROOT/RNTupleWriter.hxx>
+#include <ROOT/RPageStorageFile.hxx>
+#include <Compression.h>
+
+#include <TSystem.h>
+
+#include <string>
+#include <utility>
+
+int merge_changeComp_check_output(int expectedCompression, const char *fnameOut, const char *fnameIn1, const char *fnameIn2)
+{
+   using namespace ROOT::Experimental;
+   
+   auto noPrereleaseWarning = RLogScopedVerbosity(NTupleLog(), ROOT::Experimental::ELogLevel::kError);
+
+   Internal::RPageSourceFile source("ntpl", fnameOut, RNTupleReadOptions());
+   source.Attach();
+
+   // print out actual compression
+   const auto &desc = source.GetSharedDescriptorGuard();
+   auto clusterIter = desc->GetClusterIterable();
+   for (const auto &cluster : clusterIter) {
+      const auto &cDesc = desc->GetClusterDescriptor(cluster.GetId());
+      if (cDesc.GetColumnRange(0).fCompressionSettings != expectedCompression)
+         return 1;
+   }
+
+   gSystem->Unlink(fnameOut);
+   gSystem->Unlink(fnameIn1);
+   gSystem->Unlink(fnameIn2);
+
+   return 0;
+}
