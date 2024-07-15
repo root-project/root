c1d68bada8aba86cdbbc6d7e952fd3c35bae1a4e
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-07-15 13:17:48 +0200

    [pyroot] Isolate potentially leaking function in test

--- bindings/pyroot/pythonizations/test/memory.py
+++ bindings/pyroot/pythonizations/test/memory.py
@@ -3,12 +3,21 @@ import ROOT
 import unittest
 
 
+def _leak(obj):
+    gc.collect()
+    for _ in range(1000000):
+        __ = obj.leak(2048)
+    gc.collect()
+
+
 class MemoryStlString(unittest.TestCase):
 
     def test_15703(self):
         """Regression test for https://github.com/root-project/root/issues/15703"""
 
         ROOT.gInterpreter.Declare("""
+        #ifndef TEST_15703
+        #define TEST_15703
         #include <string>
         class foo {
             public:
@@ -18,26 +27,19 @@ class MemoryStlString(unittest.TestCase):
                 return result;
             }
         };
-                                  
-        auto get_rss_MB() {
+
+        auto get_rss_KB() {
             ProcInfo_t info;
             gSystem->GetProcInfo(&info);
             return info.fMemResident;
         }
+        #endif // TEST_15703
         """)
         obj = ROOT.foo()
-        for _ in range(1000000):
-            __ = obj.leak(2048)
-
-        gc.collect()
-
-        before = ROOT.get_rss_MB()
-
-        for _ in range(1000000):
-            __ = obj.leak(2048)
-
-        gc.collect()
-        after = ROOT.get_rss_MB()
+        _leak(obj)
+        before = ROOT.get_rss_KB()
+        _leak(obj)
+        after = ROOT.get_rss_KB()
         delta = after - before
         self.assertLess(delta, 16)
 
