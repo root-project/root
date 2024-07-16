d4839fa46f57a288bad57d4b0d2bfe11d1092aff
silverweed <giacomo.parolini@cern.ch>
2024-07-16 09:25:13 +0200

    [hadd] don't compile merge tests; replace floats with longs
    (to avoid having to write float comparing code)

--- root/io/hadd/merge_check_output.C
+++ root/io/hadd/merge_check_output.C
@@ -13,41 +13,7 @@
 #include <cstdint>
 #include <cstring>
 
-// Copy-pasted from gtest-internal.h
-namespace {
-const uint32_t kMaxUlps = 4;
-const size_t kBitCount = 8 * sizeof(float);
-const uint32_t kSignBitMask = static_cast<uint32_t>(1) << (kBitCount - 1);
-
-uint32_t SignAndMagnitudeToBiased(const uint32_t &sam)
-{
-   if (kSignBitMask & sam) {
-      // sam represents a negative number.
-      return ~sam + 1;
-   } else {
-      // sam represents a positive number.
-      return kSignBitMask | sam;
-   }
-}
-
-uint32_t DistanceBetweenSignAndMagnitudeNumbers(const uint32_t &sam1, const uint32_t &sam2)
-{
-   const uint32_t biased1 = SignAndMagnitudeToBiased(sam1);
-   const uint32_t biased2 = SignAndMagnitudeToBiased(sam2);
-   return (biased1 >= biased2) ? (biased1 - biased2) : (biased2 - biased1);
-}
-
-bool FloatAlmostEquals(float a, float b)
-{
-   uint32_t ia, ib;
-   memcpy(&ia, &a, sizeof(float));
-   memcpy(&ib, &b, sizeof(float));
-   return DistanceBetweenSignAndMagnitudeNumbers(ia, ib) <= kMaxUlps;
-}
-} // namespace
-
-int merge_check_output(int expectedCompression, const char *fnameOut, const char *fnameIn1,
-                                  const char *fnameIn2)
+int merge_check_output(const char *fnameOut, const char *fnameIn1, const char *fnameIn2)
 {
    using namespace ROOT::Experimental;
 
@@ -57,15 +23,15 @@ int merge_check_output(int expectedCompression, const char *fnameOut, const char
    {
       auto ntuple = RNTupleReader::Open("ntpl", fnameOut);
       auto viewI = ntuple->GetView<int>("I");
-      auto viewF = ntuple->GetView<float>("F");
+      auto viewL = ntuple->GetView<long>("L");
       if (auto v = viewI(0); v != 1337)
          errs << "Expected v to be 1337 but it's " << v << "\n";
       if (auto v = viewI(1); v != 123)
          errs << "Expected v to be 123 but it's " << v << "\n";
-      if (auto v = viewF(0); !FloatAlmostEquals(v, 666.f))
-         errs << "Expected v to be 666.f but it's " << v << "\n";
-      if (auto v = viewF(1); !FloatAlmostEquals(v, 420.f))
-         errs << "Expected v to be 420.f but it's " << v << "\n";
+      if (auto v = viewL(0); v != 666)
+         errs << "Expected v to be 666 but it's " << v << "\n";
+      if (auto v = viewL(1); v != 420)
+         errs << "Expected v to be 420 but it's " << v << "\n";
    }
 
    gSystem->Unlink(fnameOut);
