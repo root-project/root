55bea03abe9c128bf8976f85560a891cfc3983b2
Jakob Blomer <jblomer@cern.ch>
2024-07-16 14:12:30 +0200

    [ntuple] fix-up column flags in RNTupleSerializer

--- tree/ntuple/v7/inc/ROOT/RNTupleSerialize.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleSerialize.hxx
@@ -67,10 +67,7 @@ public:
    static constexpr std::uint16_t kFlagRepetitiveField = 0x01;
    static constexpr std::uint16_t kFlagProjectedField = 0x02;
 
-   static constexpr std::uint32_t kFlagSortAscColumn     = 0x01;
-   static constexpr std::uint32_t kFlagSortDesColumn     = 0x02;
-   static constexpr std::uint32_t kFlagNonNegativeColumn = 0x04;
-   static constexpr std::uint32_t kFlagDeferredColumn    = 0x08;
+   static constexpr std::uint32_t kFlagDeferredColumn = 0x08;
 
    static constexpr DescriptorId_t kZeroFieldId = std::uint64_t(-2);
 
