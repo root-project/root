4208e223c51d8583f53ac63ee42a1d1fde9731a8
Sergey Linev <S.Linev@gsi.de>
2024-07-17 11:15:01 +0200

    [webgui] extend custom class example
    Mark JS code as suitable for batch mode
    Provide interactivity like dragging and context menu
    Allow use TTriangle class without custom JavaScript

--- tutorials/webgui/custom/custom.cxx
+++ tutorials/webgui/custom/custom.cxx
@@ -1,9 +1,16 @@
 /// \file
 /// \ingroup tutorial_webgui
-///  This is example how custom user class can be used in the TWebCanvas
+/// This is example how custom user class can be used in the TWebCanvas
 /// Custom class is just triangle which drawn on the frame with NDC coordinates
-///  custom.mjs includes JavaScript code for painting
-///  And demo shows how custom class can be registered to the TWebCanvas
+/// `custom.mjs` provides JavaScript code for object painting and interactivity
+/// It is also possible to use such "simple" class without loading of custom JS code,
+/// but then it requires appropriate Paint() method and will miss interactivity in browser
+///
+/// This macro must be executed with ACLiC as follows:
+///
+/// ~~~{.cpp}
+/// .x custom.cxx+
+/// ~~~
 ///
 /// \macro_code
 ///
@@ -55,36 +62,61 @@ class TTriangle : public TNamed, public TAttLine, public TAttFill {
    ClassDefOverride(TTriangle, 1);   // Example of triangle drawing in web canvas
 };
 
-void custom()
+void custom(bool ignore_jsmodule = false)
 {
-   std::string fdir = __FILE__;
-   auto pos = fdir.find("custom.cxx");
-   if (pos > 0)
-      fdir.resize(pos);
-   else
-      fdir = gROOT->GetTutorialsDir() + std::string("/webgui/custom/");
+   if (ignore_jsmodule) {
+      printf("Custom JS module will NOT be provided for TTriangle class\n");
+      printf("No interactive features will be available\n");
+      printf("TTriangle::Paint() method will be used for object painting - also in web case\n");
+   } else {
+      #ifdef __CLING__
+         printf("Please run this script in compiled mode by running \".x custom.cxx+\"\n");
+         printf("Requires to properly generate dictionary for TTriangle class\n");
+         return;
+      #endif
 
-   printf("fdir = %s\n", fdir.c_str());
+      std::string fdir = __FILE__;
+      auto pos = fdir.find("custom.cxx");
+      if (pos > 0)
+         fdir.resize(pos);
+      else
+         fdir = gROOT->GetTutorialsDir() + std::string("/webgui/custom/");
 
-   // location required to load files
-   // also it is name of modules path used in importmap
-   ROOT::RWebWindowsManager::AddServerLocation("triangle", fdir);
+      // location required to load files
+      // also it is name of modules path used in importmap
+      ROOT::RWebWindowsManager::AddServerLocation("triangle", fdir);
 
-   // mark TTriangle as supported on the client
-   TWebCanvas::AddCustomClass("TTriangle");
+      // mark TTriangle as supported on the client
+      TWebCanvas::AddCustomClass("TTriangle");
 
-   // specify which extra module should be loaded,
-   // "triangle/" is registered path from server locations
-   TWebCanvas::SetCustomScripts("modules:triangle/custom.mjs");
+      // specify which extra module should be loaded,
+      // "triangle/" is registered path from server locations
+      TWebCanvas::SetCustomScripts("modules:triangle/custom.mjs");
+   }
 
+   auto tr1 = new TTriangle("tr1", "first triangle");
+   tr1->SetPoints(0.4, 0.5, 0.8, 0.3, 0.8, 0.7);
+   tr1->SetLineColor(kRed);
+   tr1->SetLineWidth(3);
+   tr1->SetFillColor(kBlue);
 
-   auto tr = new TTriangle("tr1", "title of triangle");
-   tr->SetPoints(0.3, 0.5, 0.7, 0.3, 0.7, 0.7);
+   auto tr2 = new TTriangle("tr2", "second triangle");
+   tr2->SetPoints(0.2, 0.2, 0.2, 0.6, 0.5, 0.4);
+   tr2->SetLineColor(kGreen);
+   tr2->SetLineStyle(2);
+   tr2->SetFillColor(kYellow);
 
-   tr->SetLineColor(kRed);
-   tr->SetLineWidth(3);
-   tr->SetFillColor(kBlue);
+   auto tr3 = new TTriangle("tr3", "third triangle");
+   tr3->SetPoints(0.4, 0.8, 0.6, 0.8, 0.5, 0.2);
+   tr3->SetLineColor(kCyan);
+   tr3->SetFillColor(kMagenta);
 
-   auto c1 = new TCanvas("c1", "Triangle");
-   c1->Add(tr, "f");
+   auto c1 = new TCanvas("c1", "Triangles");
+   c1->Add(tr1, "f");
+   c1->Add(tr2, "f");
+   c1->Add(tr3, "f");
+
+   // test image saving with web browser, chrome or firefox are required
+   // c1->Update();
+   // c1->SaveAs("triangle.png");
 }
