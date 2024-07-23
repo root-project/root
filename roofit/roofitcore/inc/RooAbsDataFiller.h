d53a5d47b3b5a4657303343583220b29d5d9af9f
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2024-07-23 12:34:49 +0200

    [RF] Support weighted filling from RDF to RooFit in RooAbsDataHelper
    Support weighted filling from RDF to RooFit in RooAbsDataHelper and also
    implement a test for it.
    
    Closes #7223.

--- roofit/roofitcore/inc/RooAbsDataFiller.h
+++ roofit/roofitcore/inc/RooAbsDataFiller.h
@@ -53,11 +53,15 @@ public:
 protected:
    void FillAbsData(const std::vector<double> &events, unsigned int eventSize);
 
-   std::mutex _mutex_dataset;
+   std::mutex _mutexDataset;
    std::size_t _numInvalid = 0;
 
    std::vector<std::vector<double>> _events; // One vector of values per data-processing slot
    std::size_t _eventSize;                   // Number of variables in dataset
+   std::size_t _nValues;                     // Number of variables in dataframe
+
+   bool _isWeighted = false;
+   bool _isDataHist = false;
 };
 
 } // namespace Detail
--- roofit/roofitcore/src/RooAbsDataFiller.cxx
+++ roofit/roofitcore/src/RooAbsDataFiller.cxx
@@ -29,7 +29,11 @@ RooAbsDataFiller::RooAbsDataFiller()
 
 void RooAbsDataFiller::Initialize()
 {
-   _eventSize = GetAbsData().get()->size();
+   RooAbsData &absData = GetAbsData();
+
+   _eventSize = absData.get()->size();
+   _isWeighted = absData.isWeighted();
+   _isDataHist = std::string{absData.ClassName()} != "RooDataSet";
 }
 
 /// Append all `events` to the internal RooDataSet or increment the bins of a RooDataHist at the given locations.
@@ -46,7 +50,14 @@ void RooAbsDataFiller::FillAbsData(const std::vector<double> &events, unsigned i
    RooAbsData &absData = GetAbsData();
    const RooArgSet &argSet = *absData.get();
 
+   // Relevant for weighted RooDataSet
+   RooRealVar *weightVar = !_isDataHist && _isWeighted ? static_cast<RooDataSet &>(absData).weightVar() : nullptr;
+
    for (std::size_t i = 0; i < events.size(); i += eventSize) {
+
+      // The RooDataHist has no dedicated RooRealVar for the weight. So we just
+      // use a double.
+      double weightVal = 1.0;
 
       // Creating a RooDataSet from an RDataFrame should be consistent with the
       // creation from a TTree. The construction from a TTree discards entries
@@ -55,10 +66,15 @@ void RooAbsDataFiller::FillAbsData(const std::vector<double> &events, unsigned i
 
       bool allOK = true;
       for (std::size_t j = 0; j < eventSize; ++j) {
-         auto *destArg = static_cast<RooAbsRealLValue *>(argSet[j]);
+         RooAbsRealLValue *destArg = nullptr;
+         if (j < argSet.size()) {
+            destArg = static_cast<RooAbsRealLValue *>(argSet[j]);
+         } else {
+            destArg = weightVar;
+         }
          double sourceVal = events[i + j];
 
-         if (!destArg->inRange(sourceVal, nullptr)) {
+         if (destArg && !destArg->inRange(sourceVal, nullptr)) {
             _numInvalid++;
             allOK = false;
             const auto prefix = std::string(absData.ClassName()) + "Helper::FillAbsData(" + absData.GetName() + ") ";
@@ -73,10 +89,14 @@ void RooAbsDataFiller::FillAbsData(const std::vector<double> &events, unsigned i
             }
             break;
          }
-         destArg->setVal(sourceVal);
+         if (destArg) {
+            destArg->setVal(sourceVal);
+         } else {
+            weightVal = sourceVal;
+         }
       }
       if (allOK) {
-         absData.add(argSet);
+         absData.add(argSet, weightVar ? weightVar->getVal() : weightVal);
       }
    }
 }
@@ -87,7 +107,7 @@ void RooAbsDataFiller::Finalize()
    RooAbsData &absData = GetAbsData();
 
    for (auto &vector : _events) {
-      FillAbsData(vector, _eventSize);
+      FillAbsData(vector, _nValues);
       vector.clear();
    }
 
@@ -97,17 +117,19 @@ void RooAbsDataFiller::Finalize()
    }
 }
 
-void RooAbsDataFiller::ExecImpl(std::size_t nValues, std::vector<double>& vector)
+void RooAbsDataFiller::ExecImpl(std::size_t nValues, std::vector<double> &vector)
 {
-   if (nValues != _eventSize) {
-      throw std::invalid_argument(std::string("RooDataSet can hold ") + std::to_string(_eventSize) +
-                                  " variables per event, but RDataFrame passed " +
+   if (nValues != _eventSize && !(_isWeighted && nValues == _eventSize + 1)) {
+      throw std::invalid_argument(std::string("RooAbsData can hold ") + std::to_string(_eventSize) +
+                                  " variables per event (plus an optional weight in case of weighted data), but RDataFrame passed " +
                                   std::to_string(nValues) + " columns.");
    }
 
-   if (vector.size() > 1024 && _mutex_dataset.try_lock()) {
-      const std::lock_guard<std::mutex> guard(_mutex_dataset, std::adopt_lock_t());
-      FillAbsData(vector, _eventSize);
+   _nValues = nValues;
+
+   if (vector.size() > 1024 && _mutexDataset.try_lock()) {
+      const std::lock_guard<std::mutex> guard(_mutexDataset, std::adopt_lock_t());
+      FillAbsData(vector, _nValues);
       vector.clear();
    }
 }
