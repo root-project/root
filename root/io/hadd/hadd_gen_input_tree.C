870f57f0871279a045adbde20898f805ed7468c9
silverweed <giacomo.parolini@cern.ch>
2024-07-24 14:25:21 +0200

    [hadd] add test for hadd compression algorithm change for TTree
    see https://github.com/root-project/root/pull/16082

---
+++ root/io/hadd/hadd_gen_input_tree.C
@@ -0,0 +1,16 @@
+#include <TTree.h>
+#include <TFile.h>
+#include <TSystem.h>
+
+#include <memory>
+
+void hadd_gen_input_tree(const char *fname)
+{
+   std::unique_ptr<TFile> file { TFile::Open(fname, "RECREATE") };
+   TTree tree("t", "t");
+   int x;
+   tree.Branch("x", &x);
+   x = 42;
+   tree.Fill();
+   tree.Write();
+}
