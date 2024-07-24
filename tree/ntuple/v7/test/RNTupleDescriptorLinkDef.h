6ad0f192e33839e8a7185069fe0ea03b7af2aa51
Jakob Blomer <jblomer@cern.ch>
2024-07-24 14:00:41 +0200

    [ntuple] fix dictionaries for descriptor tests

---
+++ tree/ntuple/v7/test/RNTupleDescriptorLinkDef.h
@@ -0,0 +1,7 @@
+#ifdef __CLING__
+
+#pragma link C++ class std::map < int, CustomStruct> + ;
+#pragma link C++ class std::map < int, float> + ;
+#pragma link C++ class std::unordered_map < int, float> + ;
+
+#endif
