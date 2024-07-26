c87dd612ae09e7b023ed2b6e00bba2b51a532246
silverweed <giacomo.parolini@cern.ch>
2024-07-26 10:13:30 +0200

    [hadd] explicitly set compression to 101 in hadd_gen_input_tree.C

--- root/io/hadd/hadd_gen_input_tree.C
+++ root/io/hadd/hadd_gen_input_tree.C
@@ -7,9 +7,10 @@
 void hadd_gen_input_tree(const char *fname)
 {
    std::unique_ptr<TFile> file { TFile::Open(fname, "RECREATE") };
+   file->SetCompressionSettings(101);
    TTree tree("t", "t");
    int x;
-   tree.Branch("x", &x);
+   tree.Branch("x", &x)->SetCompressionSettings(101);
    x = 42;
    tree.Fill();
    tree.Write();
