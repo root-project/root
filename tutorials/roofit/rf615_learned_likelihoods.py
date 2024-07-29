af0218302692127d57b00a1ec8a5da91b1963578
Robin Syring <syring.robin@gmx.de>
2024-07-29 11:14:09 +0200

    Fixed issues on Class implementation

--- tutorials/roofit/rf615_learned_likelihoods.py
+++ tutorials/roofit/rf615_learned_likelihoods.py
@@ -23,42 +23,15 @@ import ROOT
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 
-
-# The samples used for training the classifier in this tutorial
+# The samples used for training the classifier in this tutorial / rescale for more accuracy
 n_samples = 100000
 
 # different mean values used for training the classifier
 mu_vals = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
 
-# defining a cpp wrapper 
-ROOT.gInterpreter.Declare("""
-class RooPyLikelihood : public RooAbsReal {
-public:
-   RooPyLikelihood(const char *name, const char *title, RooArgList &varlist)
-      : RooAbsReal(name, title), m_varlist("!varlist", "All variables(list)", this)
-   {
-      m_varlist.add(varlist);
-   }
-   // copy constructor
-   RooPyLikelihood(const RooPyLikelihood &right, const char *name = nullptr)
-      : RooAbsReal(right, name), m_varlist("!varlist", this, right.m_varlist)
-   {
-   }
-   // virtual destructor
-   virtual ~RooPyLikelihood() {}
-   // clone method
-   RooPyLikelihood *clone(const char *name) const override { return new RooPyLikelihood(*this, name); }
-   // the actual evaluation of function (will be redefined in Python!)
-   Double_t evaluate() const override { return 1; }
-   // getter for varlist
-   const RooArgList &varlist() const { return m_varlist; }
-protected:
-   RooListProxy m_varlist; // all variables as list of variables
-};
-""")
 
 
-# Overwriting the cpp function
+# Overwriting the cpp function RooPyLikelihood
 def make_likelihood(name, title, func, variables):
     class MyLlh(ROOT.RooPyLikelihood):
         def __init__(self, name, title, variables):
@@ -77,7 +50,6 @@ def make_likelihood(name, title, func, variables):
 
 # class used in this case to demonstate the use of SBI in Root
 class SBI:
-
     # initializing the class SBI
     def __init__(self, workspace):
         # Choose the hyperparameters for training the neural network
@@ -120,7 +92,7 @@ class SBI:
         thetas_reference = np.repeat(self.mu_vals, repeats_reference).reshape(-1, 1)
         thetas = np.concatenate((thetas_model, thetas_reference), axis=0)
         X = np.concatenate([self.data_model, self.data_ref])
-        self.y_train = np.concatenate([np.zeros(len(self.data_model)), np.ones(len(self.data_ref))])
+        self.y_train = np.concatenate([np.ones(len(self.data_model)), np.zeros(len(self.data_ref))])
         self.X_train = np.concatenate([X, thetas], axis=1)
 
     # train the classifier
@@ -132,7 +104,7 @@ class SBI:
 n_samples_train = n_samples
 
 # The "observed" data 
-mu_observed = 2.
+mu_observed = 3.5
 
 # define the "observed" data
 x_var = ROOT.RooRealVar("x", "x", -12, 12)
@@ -157,63 +129,73 @@ model.preprocessing()
 model.train_classifier()
 sbi_model = model
 
-
 # compute the likelihood ratio of the classifier for analysis purposes
 def compute_likelihood_ratio(x, mu):
     data_point = np.array([[x, mu]])
     prob = sbi_model.classifier.predict_proba(data_point)[:, 1]
-    return 1 - prob[0]
+    return prob[0]
 
 # compute the negative logarithmic likelihood ratio summed
 # the function depends just on one variable, the mean value mu
-def compute_likelihood_sum(mu):
+def compute_log_likelihood_sum(mu):
     mu_arr = np.repeat(mu, obs_data.numEntries()).reshape(-1, 1)
     data_point = np.concatenate([obs_data.to_numpy()["x"].reshape(-1, 1), mu_arr], axis=1)
     prob = sbi_model.classifier.predict_proba(data_point)[:, 1]
-    return -np.sum(np.log(1 - prob))
+    return np.sum(np.log((1 - prob)/prob))
 
 
 # compute the likelihood ratio
-nll_ratio = make_likelihood("MyLlh", "My Llh", compute_likelihood_ratio, ROOT.RooArgList(x_var, mu_var))
+llhr_learned = make_likelihood("MyLlh", "My Llh", compute_likelihood_ratio, ROOT.RooArgList(x_var, mu_var))
 
 # compute the real likelihood ration
-real_ratio = ROOT.RooFormulaVar("real_ratio", "x[0] / (x[0] + x[1])", [gauss, uniform])
+llhr_calc = ROOT.RooFormulaVar("llhr_calc", "x[0] / (x[0] + x[1])", [gauss, uniform])
 
+# Create negativ log likelihood functions for Gaussian and Uniform models
+nll_gauss = gauss.createNLL(obs_data)
+nll_uniform = uniform.createNLL(obs_data)
 
-# compute the summed logarithmic likelihood
-nll_learned = make_likelihood("MyLlh", "My Llh", compute_likelihood_sum, ROOT.RooArgList(mu_var))
+# compute the "real" negative log likelihood 
+nllr__calc = ROOT.RooFormulaVar("nllr__calc", "nllr__calc", "x[1]-x[0]", ROOT.RooArgList(nll_gauss, nll_uniform))
 
-# Create likelihood function
-nll_ref = gauss.createNLL(obs_data)
+# compute the "learned" negative log likelihood ratio
+nllr_learned = make_likelihood("MyLlh", "My Llh", compute_log_likelihood_sum, ROOT.RooArgList(mu_var))
 
-
-# Plot the logarithmic summed likelihood
-c1 =ROOT.TCanvas()
-frame = mu_var.frame(Title="Learned vs analytical summed logarithmic Likelihood", Range=(mu_observed-0.1, mu_observed+0.1))
-# Set the y-axis range
-frame.SetMinimum(0)  # Replace y_min with your desired minimum value
-frame.SetMaximum(0.3)  # Replace y_max with your desired maximum value
-nll_ref.plotOn(frame, ShiftToZero=True)
-nll_learned.plotOn(frame, LineColor="r", ShiftToZero=True)
+# Plot the negative logarithmic summed likelihood
+c1 = ROOT.TCanvas()
+frame = mu_var.frame(Title="Learned vs analytical summed logarithmic Likelihood", Range=(mu_observed-5, mu_observed+5)) 
+nll_uniform.plotOn(frame, Name="uni")
+nllr__calc.plotOn(frame, LineColor='y',Name="ratio" )
+nll_gauss.plotOn(frame, ShiftToZero=True, LineColor='g', LineStyle='--', Name="gauss") # ShiftToZero to compare it to the learned case
+nllr_learned.plotOn(frame, LineColor="r", ShiftToZero=True, Name="learned")
 frame.Draw()
+# Create a legend and add entries
+legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)  # Adjust coordinates as needed
+legend.AddEntry("uni", "Uniform", "l")
+legend.AddEntry("ratio", "nll_u-nll_g", "l")
+legend.AddEntry("gauss", "Gaussian", "l")
+legend.AddEntry("learned", "sum(log((1 - prob)/prob))", "l")
+legend.Draw()
 c1.SaveAs("Logarithmic_summed.png")
 
+
 # Plot the likelihood functions
-c2 =ROOT.TCanvas()
+c2 = ROOT.TCanvas()
 frame_x = x_var.frame(Title="Learned vs analytical likelihhood function")
-real_ratio.plotOn(frame_x, )
-nll_ratio.plotOn(frame_x, LineColor="r",)
+llhr_learned.plotOn(frame_x, LineColor="r", Name="learned")
+llhr_calc.plotOn(frame_x, Name="exact" )
 frame_x.Draw()
+# Create a legend and add entries
+legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)  # Adjust coordinates as needed
+legend.AddEntry("learned", "learned", "l")
+legend.AddEntry("exact", "exact", "l")
+legend.Draw()
 c2.SaveAs("llh_function.png")
 
-
-# compute the minimum via minuit and display the results
-for i in [nll_ref, nll_learned]:
+# Compute the minimum via minuit and display the results
+for i in [nll_gauss, nllr_learned]:
     min = minimizer = ROOT.RooMinimizer(i)
     minimizer.setErrorLevel(0.5)    # adjust the error level in the minimization to work with likelihoods
     minimizer.setPrintLevel(-1)
     minimizer.minimize("Minuit2")
     result = minimizer.save()
     result.Print()
-
-
