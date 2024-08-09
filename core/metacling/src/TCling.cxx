0291a266fa262e0e08b05b632b6c848cf62d3b6d
Philippe Canal <pcanal@fnal.gov>
2024-08-09 17:23:48 -0500

    std::tuple: properly reload ClassInfo and StreamerInfo.
    When/if a TClass for a std::tuple is first loaded in forward declared state and then
    the interpreter information is loaded, we now properly replace the ClassInfo and
    StreamerInfo

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -6663,6 +6663,18 @@ void TCling::RefreshClassInfo(TClass *cl, const clang::NamedDecl *def, bool alia
       }
    } else if (!cl->TestBit(TClass::kLoading) && !cl->fHasRootPcmInfo) {
       cl->ResetCaches();
+      if (strncmp(cl->GetName(),"tuple<",strlen("tuple<"))==0) {
+         // We need to use the Emulated Tuple but we should not trigger parsing
+         // yet, so delay the creation of the ClassInfo
+         delete ((TClingClassInfo *)cl->fClassInfo);
+         cl->fClassInfo = nullptr;
+         cl->fCanLoadClassInfo = true;
+         cl->RemoveStreamerInfo(cl->fClassVersion);
+         if (cl->fState != TClass::kHasTClassInit) {
+            cl->fState = TClass::kInterpreted;
+         }
+         return;
+      }
       // yes, this is almost a waste of time, but we do need to lookup
       // the 'type' corresponding to the TClass anyway in order to
       // preserve the opaque typedefs (Double32_t)
