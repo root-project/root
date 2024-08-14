ce97000043a80b58101897f2809fe4c2c95c664f
Philippe Canal <pcanal@fnal.gov>
2024-08-14 14:02:06 -0500

    Revert "[hadd] properly change compression when levels are the same"
    This reverts commit 5303d8beee491030d11f5a47c97b96b2f8863142.

--- io/io/src/TFileMerger.cxx
+++ io/io/src/TFileMerger.cxx
@@ -175,8 +175,7 @@ Bool_t TFileMerger::AddFile(const char *url, Bool_t cpProgress)
          Error("AddFile", "cannot open file %s", url);
       return kFALSE;
    } else {
-      if (fOutputFile && fOutputFile->GetCompressionSettings() != newfile->GetCompressionSettings())
-         fCompressionChange = kTRUE;
+      if (fOutputFile && fOutputFile->GetCompressionLevel() != newfile->GetCompressionLevel()) fCompressionChange = kTRUE;
 
       newfile->SetBit(kCanDelete);
       fFileList.Add(newfile);
