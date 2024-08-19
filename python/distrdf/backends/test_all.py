43767b258f9f6f2fcc3dafdc96852b15af91175f
Marta Czurylo <mczurylo@cern.ch>
2024-08-19 11:27:35 +0200

    [DF] Test interfaces for distributing headers, shared libraries, files and cppcode
    The following functions are tested: DistributeHeaders,
    `DistributeSharedLibs`, `DistributeFiles`
    and `DistributeCppCode`.

--- python/distrdf/backends/test_all.py
+++ python/distrdf/backends/test_all.py
@@ -4,11 +4,12 @@ import pytest
 
 from check_backend import *
 from check_cloned_actions import *
+from check_distribute_cppcode import * 
 from check_definepersample import *
 from check_friend_trees_alignment import *
 from check_friend_trees import *
 from check_histo_write import *
-from check_include_headers import *
+from check_distribute_headers_sharedlibs_files import *
 from check_inv_mass import *
 from check_live_visualize import *
 from check_reducer_merge import *
