53d9f7baebd842250de399efd5fa722be466f0cc
Sergey Linev <S.Linev@gsi.de>
2024-08-20 14:00:23 +0200

    [webgeom] support TGeoTrack creation
    For some reason implemented in painter and not in TGeoManager itself
    
    Fixes https://github.com/root-project/root/issues/16167

--- geom/webviewer/inc/ROOT/RGeoPainter.hxx
+++ geom/webviewer/inc/ROOT/RGeoPainter.hxx
@@ -29,8 +29,8 @@ public:
    ~RGeoPainter() override;
 
    void       AddSize3D(Int_t, Int_t, Int_t) override {}
-   TVirtualGeoTrack *AddTrack(Int_t, Int_t, TObject *) override { return nullptr; }
-   void       AddTrackPoint(Double_t *, Double_t *, Bool_t =kFALSE) override {}
+   TVirtualGeoTrack *AddTrack(Int_t, Int_t, TObject *) override;
+   void       AddTrackPoint(Double_t *, Double_t *, Bool_t =kFALSE) override;
    void       BombTranslation(const Double_t *, Double_t *) override {}
    void       CheckPoint(Double_t =0, Double_t =0, Double_t =0, Option_t * ="", Double_t = 0.) override {}
    void       CheckShape(TGeoShape *, Int_t, Int_t, Option_t *) override {}
