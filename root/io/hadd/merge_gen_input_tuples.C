c3e3a2eb7edcd32ef14f8c7299219b303574bba5
silverweed <giacomo.parolini@cern.ch>
2024-08-21 15:38:18 +0200

    [hadd] add missing files for merge_changecomp test

---
+++ root/io/hadd/merge_gen_input_tuples.C
@@ -0,0 +1,37 @@
+#include <ROOT/RLogger.hxx>
+#include <ROOT/RNTupleModel.hxx>
+#include <ROOT/RNTupleReader.hxx>
+#include <ROOT/RNTupleWriter.hxx>
+#include <ROOT/RPageStorageFile.hxx>
+#include <Compression.h>
+
+#include <TSystem.h>
+
+#include <string>
+#include <utility>
+
+void merge_gen_input_tuples(const char *fname1 = "test_rntuple_input1.root", const char *fname2 = "test_rntuple_input2.root")
+{
+   using namespace ROOT::Experimental;
+   
+   auto noPrereleaseWarning = RLogScopedVerbosity(NTupleLog(), ROOT::Experimental::ELogLevel::kError);
+
+   {
+      auto model = RNTupleModel::Create();
+      model->MakeField<int>("I", 1337);
+      model->MakeField<long>("L", 666);
+      auto opts = RNTupleWriteOptions{};
+      opts.SetCompression(ROOT::CompressionSettings(ROOT::RCompressionSetting::EAlgorithm::kZSTD, 5));
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fname1, opts);
+      writer->Fill();
+   }
+   {
+      auto model = RNTupleModel::Create();
+      model->MakeField<int>("I", 123);
+      model->MakeField<long>("L", 420);
+      auto opts = RNTupleWriteOptions{};
+      opts.SetCompression(ROOT::CompressionSettings(ROOT::RCompressionSetting::EAlgorithm::kZLIB, 1));
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fname2, opts);
+      writer->Fill();
+   }
+}
