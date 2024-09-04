0c5904af09911a404c335eb9dfe48b4634adae0c
Jonas Rembser <jonas.rembser@cern.ch>
2024-09-04 02:37:50 +0200

    [RF] Faster Hesse in RooFit by advertising which params are independent
    This reduces the time to run Hesse in the ATLAS Higgs benchmark from
    123 s to 92 seconds.
    
    Given that some models take hours for this, this is a significant
    improvement for the user experience.
    
    Further improvement is possible by analyzing the computation graph a bit
    more to find more independent parameters (e.g., the different gammas for
    stat uncertainties from different bins).

--- math/minuit2/inc/Minuit2/FCNBase.h
+++ math/minuit2/inc/Minuit2/FCNBase.h
@@ -127,6 +127,11 @@ public:
    virtual bool HasHessian() const { return false; }
 
    virtual bool HasG2() const { return false; }
+
+   /// Indicate whether a given second order derivative with respect to
+   /// parameters i and j is always zero. This can help to avoid
+   /// expensive function calls in Hessian evaluations.
+   virtual bool VanishingSecondDerivative(int /*i*/, int /*j*/) const { return false; }
 };
 
 } // namespace ROOT::Minuit2
