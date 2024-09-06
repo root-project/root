671fff7663581a60a7082dec65d4bcaf8511cab1
Jakob Blomer <jblomer@cern.ch>
2024-09-06 15:25:06 +0200

    [NFC][ntuple] add clarifying comment

--- tree/ntuple/v7/inc/ROOT/RNTupleUtil.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleUtil.hxx
@@ -197,6 +197,8 @@ struct RNTupleLocator {
    /// Values for the _Type_ field in non-disk locators.  Serializable types must have the MSb == 0; see
    /// `doc/specifications.md` for details
    enum ELocatorType : std::uint8_t {
+      // The kTypeFile locator may translate to an on-disk standard locator (type 0x00) or a large locator (type 0x01),
+      // if the size of the referenced data block is >2GB
       kTypeFile = 0x00,
       kTypeDAOS = 0x02,
 
