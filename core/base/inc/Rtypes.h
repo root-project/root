cfb74c65cbfe8fc5007497f16acf4e9c68123b2d
Olivier Couet <olivier.couet@cern.ch>
2024-09-10 16:03:56 +0200

    - change the color indices for the new colors to avoid holes in the table - make GetColorDark and GetColorBright better - new method GetFirstFreeColor

--- core/base/inc/Rtypes.h
+++ core/base/inc/Rtypes.h
@@ -65,10 +65,10 @@ enum ESysConstants {
 enum EColor { kWhite   =0,   kBlack    =1,   kGray   =920,
               kRed     =632, kGreen    =416, kBlue   =600, kYellow =400, kMagenta =616, kCyan    =432,
               kOrange  =800, kSpring   =820, kTeal   =840, kAzure  =860, kViolet  =880, kPink    =900,
-              kGrape   =340, kBrown    =341, kAsh    =342,
-              kP6Blue  =343, kP6Yellow =344, kP6Red  =345, kP6Grape=346, kP6Gray   =347, kP6Violet=348,
-              kP8Blue  =349, kP8Orange =350, kP8Red  =351, kP8Pink =352, kP8Green  =353, kP8Cyan  =354, kP8Azure  =355, kP8Gray  =356,
-              kP10Blue =357, kP10Yellow=358, kP10Red =359, kP10Gray=360, kP10Violet=361, kP10Brown=362, kP10Orange=363, kP10Green=364, kP10Ash=365, kP10Cyan=366
+              kGrape   =100, kBrown    =101, kAsh    =102,
+              kP6Blue  =103, kP6Yellow =104, kP6Red  =105, kP6Grape=106, kP6Gray   =107, kP6Violet=108,
+              kP8Blue  =109, kP8Orange =110, kP8Red  =111, kP8Pink =112, kP8Green  =113, kP8Cyan  =114, kP8Azure  =115, kP8Gray  =116,
+              kP10Blue =117, kP10Yellow=118, kP10Red =119, kP10Gray=120, kP10Violet=121, kP10Brown=122, kP10Orange=123, kP10Green=124, kP10Ash=125, kP10Cyan=126
             };
 
 // There is several streamer concepts.
