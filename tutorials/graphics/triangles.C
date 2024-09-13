b7060a305018a70149dc57d64193963fe2c1a00f
Olivier Couet <olivier.couet@cern.ch>
2024-09-13 15:47:42 +0200

    Fix the example triangles.C. The color indices were not picked correctly in the palette

--- tutorials/graphics/triangles.C
+++ tutorials/graphics/triangles.C
@@ -1,39 +1,39 @@
 /// \file
 /// \ingroup tutorial_graphics
 /// \notebook
-/// Generate small triangles randomly in the canvas.
-/// Each triangle has a unique id and a random color in the color palette
+/// Create small triangles at random positions on the canvas.
+/// Assign a unique ID to each triangle, and give each one a random color from the color palette.
 ///
 /// ~~~{.cpp}
 /// root > .x triangles.C
 /// ~~~
 ///
-/// Then click on any triangle. A message showing the triangle number
-/// and its color will be printed.
+/// When a triangle is clicked, a message displaying its unique number and color will be printed.
 ///
 /// \macro_image
 /// \macro_code
 ///
 /// \author Rene Brun
 
-void triangles(Int_t ntriangles=50)
+void triangles(int ntriangles=50)
 {
-   TCanvas *c1 = new TCanvas("c1","triangles",10,10,700,700);
+   auto c1 = new TCanvas("c1","triangles",10,10,700,700);
+   gStyle->SetPalette(kCMYK);
    TRandom r;
-   Double_t dx = 0.2; Double_t dy = 0.2;
-   Int_t ncolors = gStyle->GetNumberOfColors();
-   Double_t x[4],y[4];
-   for (Int_t i=0;i<ntriangles;i++) {
+   double dx = 0.2; double dy = 0.2;
+   int ncolors = TColor::GetNumberOfColors();
+   double x[4],y[4];
+   for (int i=0;i<ntriangles;i++) {
       x[0] = r.Uniform(.05,.95); y[0] = r.Uniform(.05,.95);
       x[1] = x[0] + dx*r.Rndm(); y[1] = y[0] + dy*r.Rndm();
       x[2] = x[1] - dx*r.Rndm(); y[2] = y[1] - dy*r.Rndm();
       x[3] = x[0];               y[3] = y[0];
-      TPolyLine *pl = new TPolyLine(4,x,y);
+      auto pl = new TPolyLine(4,x,y);
       pl->SetUniqueID(i);
-      Int_t ci = ncolors*r.Rndm();
+      int ci = ncolors*r.Rndm();
       TColor *c = gROOT->GetColor(TColor::GetColorPalette(ci));
       c->SetAlpha(r.Rndm());
-      pl->SetFillColor(ci);
+      pl->SetFillColor(c->GetNumber());
       pl->Draw("f");
    }
    c1->AddExec("ex","TriangleClicked()");
