ef049553a00dbe9d5308b8f75d87a207b4084384
Philippe Canal <pcanal@fnal.gov>
2024-09-23 12:49:26 -0500

    TClassEdit::GetNameForIO now returns typename with trailing stars.
    Since the function also strip the unique_ptr from the inside template parameters, we can not
    tell from 'just' a name 'update/change' whether the IO type is a pointer or a non-pointer
    template instance.

--- core/meta/src/TStreamerElement.cxx
+++ core/meta/src/TStreamerElement.cxx
@@ -1743,13 +1743,12 @@ TStreamerSTL::TStreamerSTL(const char *name, const char *title, Int_t offset,
    std::string inside = (inside_type.find("const ")==0) ? inside_type.substr(6) : inside_type;
 
    // Let's treat the unique_ptr case
-   bool nameChanged = false;
-   std::string intype = TClassEdit::GetNameForIO(inside, TClassEdit::EModType::kNone, &nameChanged);
+   std::string intype = TClassEdit::GetNameForIO(inside, TClassEdit::EModType::kNone);
 
-   bool isPointer = nameChanged; // unique_ptr is considered a pointer
+   bool isPointer = false;
    // The incoming name is normalized (it comes from splitting the name of a TClass),
    // so all we need to do is drop the last trailing star (if any) and record that information.
-   while (!nameChanged && intype[intype.length()-1] == '*') {
+   while (intype[intype.length()-1] == '*') {
       isPointer = true;
       intype.pop_back();
    }
