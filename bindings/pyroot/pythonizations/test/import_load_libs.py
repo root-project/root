c78971c725ba02f68837d7feed795b9f5202de08
Philippe Canal <pcanal@fnal.gov>
2024-10-08 03:07:50 -0500

    build: The builtin TBB sets a debug suffix for its library on linux too

--- bindings/pyroot/pythonizations/test/import_load_libs.py
+++ bindings/pyroot/pythonizations/test/import_load_libs.py
@@ -40,6 +40,7 @@ class ImportLoadLibs(unittest.TestCase):
             'libssl',
             'libcrypt.*', # by libssl
             'libtbb',
+            'libtbb_debug',
             'libtbbmalloc',
             'liburing', # by libRIO if uring option is enabled
             # On centos7 libssl links against kerberos pulling in all dependencies below, removed with libssl1.1.0
