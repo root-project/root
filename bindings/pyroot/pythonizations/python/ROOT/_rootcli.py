e274dfff575f0c9a21bcc1e7c11b71ee800add35
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-10-11 15:05:00 +0200

    Preserve cmd line args to ROOT cli

--- bindings/pyroot/pythonizations/python/ROOT/_rootcli.py
+++ bindings/pyroot/pythonizations/python/ROOT/_rootcli.py
@@ -1,12 +1,24 @@
-import os, sys, subprocess
+import os
+import subprocess
+import sys
 
-MYHOME = os.path.dirname(__file__)
+ROOT_HOME = os.path.dirname(__file__)
 
-def main():
-    os.environ['LD_LIBRARY_PATH'] = os.path.join(MYHOME, 'lib')
-    rootexe = os.path.join(MYHOME, 'bin', 'root.exe')
 
-    return subprocess.call(rootexe)
+def main() -> None:
+    # Ensure all ROOT libraries are found at runtime by the process
+    os.environ['LD_LIBRARY_PATH'] = os.path.join(ROOT_HOME, 'lib')
+    # Finds the ROOT process from the current installation directory
+    rootexe = os.path.join(ROOT_HOME, 'bin', 'root.exe')
+    if not os.path.exists(rootexe):
+        msg = (f"Could not find 'root' executable in directory '{os.path.join(ROOT_HOME, "bin")}'. "
+               "Something is wrong in the ROOT installation.")
+        raise FileNotFoundError(msg)
+    # Make sure command line arguments are preserved
+    args = [rootexe] + sys.argv[1:]
+    # Run the actual ROOT executable and return the exit code to the main Python process
+    out = subprocess.run(args)
+    return out.returncode
 
 
 if __name__ == "__main__":
