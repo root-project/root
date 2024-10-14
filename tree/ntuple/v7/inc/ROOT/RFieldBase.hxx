dd90a7b5fb228767ca4a96ddcb4ebb26b611c453
Florine de Geus <florine.de.geus@cern.ch>
2024-10-14 10:17:04 +0200

    [ntuple] Add `RNTupleJoinProcessor`
    The `RNTupleJoinProcessor` allows for horizontal concatenations of
    RNTuples, which are either fully aligned or unaligned, in which case
    entries are joined using the `RNTupleIndex`.

--- tree/ntuple/v7/inc/ROOT/RFieldBase.hxx
+++ tree/ntuple/v7/inc/ROOT/RFieldBase.hxx
@@ -32,6 +32,7 @@ namespace ROOT {
 namespace Experimental {
 
 class RFieldBase;
+class RNTupleJoinProcessor;
 
 namespace Internal {
 struct RFieldCallbackInjector;
@@ -65,6 +66,7 @@ This is and can only be partially enforced through C++.
 */
 // clang-format on
 class RFieldBase {
+   friend class ROOT::Experimental::RNTupleJoinProcessor;                    // needs ConstuctValue
    friend struct ROOT::Experimental::Internal::RFieldCallbackInjector;       // used for unit tests
    friend struct ROOT::Experimental::Internal::RFieldRepresentationModifier; // used for unit tests
    friend void Internal::CallFlushColumnsOnField(RFieldBase &);
