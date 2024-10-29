dee0afa6b1c56ebf2cdc94445de6b128c47cd043
ferdymercury <ferdymercury@users.noreply.github.com>
2024-10-29 15:23:57 +0100

    [skip-ci] clarify default behavior

--- hist/hist/src/TH1.cxx
+++ hist/hist/src/TH1.cxx
@@ -4552,6 +4552,7 @@ TVirtualHistPainter *TH1::GetPainter(Option_t *option)
 ///
 /// \param[in] n maximum size of array xp and size of array p (if given)
 /// \param[out] xp array to be filled with nq quantiles evaluated at (p)
+/// If p is null (default value), then xp is actually set to the histogram bin edges
 /// \param[in] p array of probabilities where quantiles should be evaluated.
 ///   - if p is null, the CDF of the histogram will be used instead as array, and will
 ///     have a size = number of bins + 1 in h. it will correspond to the
