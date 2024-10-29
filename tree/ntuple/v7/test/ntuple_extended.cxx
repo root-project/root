f7a9916020c0bf21eb78a87e3541aeee5245ed64
Jakob Blomer <jblomer@cern.ch>
2024-10-29 23:32:22 +0100

    [ntuple] add test for expanding columns in different types

--- tree/ntuple/v7/test/ntuple_extended.cxx
+++ tree/ntuple/v7/test/ntuple_extended.cxx
@@ -5,6 +5,7 @@
 #include <TRandom3.h>
 #include <TROOT.h>
 
+#include <algorithm>
 #include <random>
 
 TEST(RNTuple, RealWorld1)
@@ -75,6 +76,87 @@ TEST(RNTuple, RealWorld1)
    EXPECT_EQ(chksumRead, chksumWrite);
 }
 
+TEST(RNTuple, Double32IMT)
+{
+   // Tests if parallel decompression correctly compresses the on-disk float to an in-memory double
+#ifdef R__USE_IMT
+   IMTRAII _;
+#endif
+   FileRaii fileGuard("test_ntuple_double32_imt.root");
+
+   constexpr int kNEvents = 10;
+
+   {
+      auto model = RNTupleModel::Create();
+      model->AddField(RFieldBase::Create("pt", "Double32_t").Unwrap());
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath());
+
+      auto ptrPt = writer->GetModel().GetDefaultEntry().GetPtr<double>("pt");
+
+      for (int i = 0; i < kNEvents; ++i) {
+         *ptrPt = i;
+         writer->Fill();
+      }
+   }
+
+   auto reader = RNTupleReader::Open("ntpl", fileGuard.GetPath());
+   auto viewPt = reader->GetView<double>("pt");
+   for (int i = 0; i < kNEvents; ++i) {
+      EXPECT_DOUBLE_EQ(i, viewPt(i));
+   }
+}
+
+TEST(RNTuple, MultiColumnExpansion)
+{
+   // Tests if on-disk columns that expand to multiple in-memory types are correctly handled
+#ifdef R__USE_IMT
+   IMTRAII _;
+#endif
+   FileRaii fileGuard("test_ntuple_multi_column_expansion.root");
+
+   constexpr int kNEvents = 1000;
+
+   {
+      auto model = RNTupleModel::Create();
+      model->AddField(RFieldBase::Create("pt", "Double32_t").Unwrap());
+      RNTupleWriteOptions options;
+      options.SetMaxUnzippedPageSize(32);
+      options.SetInitialNElementsPerPage(1);
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath(), options);
+
+      auto ptrPt = writer->GetModel().GetDefaultEntry().GetPtr<double>("pt");
+
+      for (int i = 0; i < kNEvents; ++i) {
+         *ptrPt = i;
+         writer->Fill();
+         if (i % 50 == 0)
+            writer->CommitCluster();
+      }
+   }
+
+   auto reader = RNTupleReader::Open("ntpl", fileGuard.GetPath());
+   auto viewPt = reader->GetView<double>("pt");
+   auto viewPtAsFloat = reader->GetView<float>("pt");
+
+   std::random_device rd;
+   std::mt19937 gen(rd());
+   std::vector<unsigned int> indexes;
+   indexes.reserve(kNEvents);
+   for (unsigned int i = 0; i < kNEvents; ++i)
+      indexes.emplace_back(i);
+   std::shuffle(indexes.begin(), indexes.end(), gen);
+
+   std::bernoulli_distribution dist(0.5);
+   for (auto idx : indexes) {
+      if (dist(gen)) {
+         EXPECT_DOUBLE_EQ(idx, viewPt(idx));
+         EXPECT_DOUBLE_EQ(idx, viewPtAsFloat(idx));
+      } else {
+         EXPECT_DOUBLE_EQ(idx, viewPtAsFloat(idx));
+         EXPECT_DOUBLE_EQ(idx, viewPt(idx));
+      }
+   }
+}
 
 // Stress test the asynchronous cluster pool by a deliberately unfavourable read pattern
 TEST(RNTuple, RandomAccess)
