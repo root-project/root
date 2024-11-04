b39c15962a18211db24c19fbf7cf33f348dc843f
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2024-11-04 17:54:09 +0100

    [RDF] Move iterator functions of FillHelper out of the class.
    These functions are useful also for other action helpers, so they are now outside
    of the class in the namespace ROOT::Internal::RDF.
    A few TODOs were also taken care of.

--- tree/dataframe/inc/ROOT/RDF/ActionHelpers.hxx
+++ tree/dataframe/inc/ROOT/RDF/ActionHelpers.hxx
@@ -350,33 +350,62 @@ public:
    }
 };
 
+// class which wraps a pointer and implements a no-op increment operator
+template <typename T>
+class ScalarConstIterator {
+   const T *obj_;
+
+public:
+   using iterator_category = std::forward_iterator_tag;
+   using difference_type = std::ptrdiff_t;
+   using value_type = T;
+   using pointer = T *;
+   using reference = T &;
+   ScalarConstIterator(const T *obj) : obj_(obj) {}
+   const T &operator*() const { return *obj_; }
+   ScalarConstIterator<T> &operator++() { return *this; }
+};
+
+// return unchanged value for scalar
+template <typename T>
+auto MakeBegin(const T &val)
+{
+   if constexpr (IsDataContainer<T>::value) {
+      return std::begin(val);
+   } else {
+      return ScalarConstIterator<T>(&val);
+   }
+}
+
+// return container size for containers, and 1 for scalars
+template <typename T>
+std::size_t GetSize(const T &val)
+{
+   if constexpr (IsDataContainer<T>::value) {
+      return std::size(val);
+   } else {
+      return 1;
+   }
+}
+
+// Helpers for dealing with histograms and similar:
+template <typename H, typename = decltype(std::declval<H>().Reset())>
+void ResetIfPossible(H *h)
+{
+   h->Reset();
+}
+
+void ResetIfPossible(TStatistic *h);
+void ResetIfPossible(...);
+
+void UnsetDirectoryIfPossible(TH1 *h);
+void UnsetDirectoryIfPossible(...);
+
 /// The generic Fill helper: it calls Fill on per-thread objects and then Merge to produce a final result.
 /// For one-dimensional histograms, if no axes are specified, RDataFrame uses BufferedFillHelper instead.
 template <typename HIST = Hist_t>
 class R__CLING_PTRCHECK(off) FillHelper : public RActionImpl<FillHelper<HIST>> {
    std::vector<HIST *> fObjects;
-
-   template <typename H = HIST, typename = decltype(std::declval<H>().Reset())>
-   void ResetIfPossible(H *h)
-   {
-      h->Reset();
-   }
-
-   void ResetIfPossible(TStatistic *h) { *h = TStatistic(); }
-
-   // cannot safely re-initialize variations of the result, hence error out
-   void ResetIfPossible(...)
-   {
-      throw std::runtime_error(
-         "A systematic variation was requested for a custom Fill action, but the type of the object to be filled does "
-         "not implement a Reset method, so we cannot safely re-initialize variations of the result. Aborting.");
-   }
-
-   void UnsetDirectoryIfPossible(TH1 *h) {
-      h->SetDirectory(nullptr);
-   }
-
-   void UnsetDirectoryIfPossible(...) {}
 
    // Merge overload for types with Merge(TCollection*), like TH1s
    template <typename H, typename = std::enable_if_t<std::is_base_of<TObject, H>::value, int>>
@@ -405,61 +434,10 @@ class R__CLING_PTRCHECK(off) FillHelper : public RActionImpl<FillHelper<HIST>> {
                     "The type passed to Fill does not provide a Merge(TCollection*) or Merge(const std::vector&) method.");
    }
 
-   // class which wraps a pointer and implements a no-op increment operator
-   template <typename T>
-   class ScalarConstIterator {
-      const T *obj_;
-
-   public:
-      ScalarConstIterator(const T *obj) : obj_(obj) {}
-      const T &operator*() const { return *obj_; }
-      ScalarConstIterator<T> &operator++() { return *this; }
-   };
-
-   // helper functions which provide one implementation for scalar types and another for containers
-   // TODO these could probably all be replaced by inlined lambdas and/or constexpr if statements
-   // in c++17 or later
-
-   // return unchanged value for scalar
-   template <typename T, std::enable_if_t<!IsDataContainer<T>::value, int> = 0>
-   ScalarConstIterator<T> MakeBegin(const T &val)
-   {
-      return ScalarConstIterator<T>(&val);
-   }
-
-   // return iterator to beginning of container
-   template <typename T, std::enable_if_t<IsDataContainer<T>::value, int> = 0>
-   auto MakeBegin(const T &val)
-   {
-      return std::begin(val);
-   }
-
-   // return 1 for scalars
-   template <typename T, std::enable_if_t<!IsDataContainer<T>::value, int> = 0>
-   std::size_t GetSize(const T &)
-   {
-      return 1;
-   }
-
-   // return container size
-   template <typename T, std::enable_if_t<IsDataContainer<T>::value, int> = 0>
-   std::size_t GetSize(const T &val)
-   {
-#if __cplusplus >= 201703L
-      return std::size(val);
-#else
-      return val.size();
-#endif
-   }
-
    template <std::size_t ColIdx, typename End_t, typename... Its>
    void ExecLoop(unsigned int slot, End_t end, Its... its)
    {
-      auto *thisSlotH = fObjects[slot];
-      // loop increments all of the iterators while leaving scalars unmodified
-      // TODO this could be simplified with fold expressions or std::apply in C++17
-      auto nop = [](auto &&...) {};
-      for (; GetNthElement<ColIdx>(its...) != end; nop(++its...)) {
+      for (auto *thisSlotH = fObjects[slot]; GetNthElement<ColIdx>(its...) != end; (std::advance(its, 1), ...)) {
          thisSlotH->Fill(*its...);
       }
    }
