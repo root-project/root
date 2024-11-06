540957eef00648e6ee282c4fae94805ec370e8fb
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-11-06 15:04:00 +0100

    [pyroot] Simplify memory regulation logic
    * Remove usage of lambda with capture of class data member, move to free
      functions with singleton.
    * Remove TMemoryRegulator class and only keep the needed functionality in RegulatorCleanup
    * Remove extra call to `ClearProxiedObjects`, as those objects are automatically deregistered by the
      Python GC through cppyy's memory regulator.
    
    The only remaining feature for the PyROOT atexit cleanup logic is the
    `CallCppyyRecursiveRemove` function that is still required for TObject-derived instances.
    Unfortunately, it is still somewhat fragile. With the changes in this commit,
    which bring back our full suite of CI platforms to green, the interplay between
    `CPyCppyy::MemoryRegulator::RecursiveRemove` and our own map of tracked objects
    is subtle. At the moment it looks like the call to `RecursiveRemove` may
    invalidate the memory pointed to by the map bucket, so we erase that first.

--- bindings/pyroot/pythonizations/src/TMemoryRegulator.cxx
+++ bindings/pyroot/pythonizations/src/TMemoryRegulator.cxx
@@ -3,7 +3,7 @@
 // Author: Vincenzo Eduardo Padulano CERN 05/2024
 
 /*************************************************************************
- * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2024, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -15,94 +15,70 @@
 #include "../../cppyy/CPyCppyy/src/ProxyWrappers.h"
 #include "../../cppyy/CPyCppyy/src/CPPInstance.h"
 
-////////////////////////////////////////////////////////////////////////////
-/// \brief Constructor. Registers the hooks to run on Cppyy's object
-///        construction and destruction
-PyROOT::TMemoryRegulator::TMemoryRegulator()
+namespace {
+// We use a map because we do not control the deregistration anyway, that is
+// managed by the Python GC.
+// key: object address; value: object class id
+using TrackedObjects_t = std::unordered_map<Cppyy::TCppObject_t, Cppyy::TCppType_t>;
+
+TrackedObjects_t &GetTrackedObjects()
 {
-   CPyCppyy::MemoryRegulator::SetRegisterHook(
-      [this](Cppyy::TCppObject_t cppobj, Cppyy::TCppType_t klass) { return this->RegisterHook(cppobj, klass); });
-   CPyCppyy::MemoryRegulator::SetUnregisterHook(
-      [this](Cppyy::TCppObject_t cppobj, Cppyy::TCppType_t klass) { return this->UnregisterHook(cppobj, klass); });
+   thread_local TrackedObjects_t trackedObjects{};
+   return trackedObjects;
 }
 
-////////////////////////////////////////////////////////////////////////////
-/// \brief Register a hook that Cppyy runs when constructing an object.
-/// \param[in] cppobj Address of the object.
-/// \param[in] klass Class id of the object.
-/// \return Pair of two booleans. First indicates success, second tells
-///         Cppyy if we want to continue running RegisterPyObject
-std::pair<bool, bool> PyROOT::TMemoryRegulator::RegisterHook(Cppyy::TCppObject_t cppobj, Cppyy::TCppType_t klass)
+Cppyy::TCppType_t &GetTObjectTypeID()
 {
-   static Cppyy::TCppType_t tobjectTypeID = (Cppyy::TCppType_t)Cppyy::GetScope("TObject");
+   static Cppyy::TCppType_t tobjectTypeID{Cppyy::GetScope("TObject")};
+   return tobjectTypeID;
+}
 
-   if (Cppyy::IsSubtype(klass, tobjectTypeID)) {
-      fObjectMap.insert({cppobj, klass});
+auto GetObjectIt(const TrackedObjects_t &objects, Cppyy::TCppObject_t key)
+{
+   return objects.find(key);
+}
+
+std::pair<bool, bool> RegisterHook(Cppyy::TCppObject_t cppobj, Cppyy::TCppType_t klass)
+{
+   if (Cppyy::IsSubtype(klass, GetTObjectTypeID())) {
+      GetTrackedObjects().insert({cppobj, klass});
    }
-
    return {true, true};
 }
 
-////////////////////////////////////////////////////////////////////////////
-/// \brief Register a hook that Cppyy runs when deleting an object.
-/// \param[in] cppobj Address of the object.
-/// \param[in] klass Class id of the object.
-/// \return Pair of two booleans. First indicates success, second tells
-///         Cppyy if we want to continue running UnRegisterPyObject
-std::pair<bool, bool> PyROOT::TMemoryRegulator::UnregisterHook(Cppyy::TCppObject_t cppobj, Cppyy::TCppType_t klass)
+std::pair<bool, bool> UnregisterHook(Cppyy::TCppObject_t cppobj, Cppyy::TCppType_t klass)
 {
-
-   static Cppyy::TCppType_t tobjectTypeID = (Cppyy::TCppType_t)Cppyy::GetScope("TObject");
-
-   if (Cppyy::IsSubtype(klass, tobjectTypeID)) {
-      if (auto it = fObjectMap.find(cppobj); it != fObjectMap.end())
-         fObjectMap.erase(it);
+   if (Cppyy::IsSubtype(klass, GetTObjectTypeID())) {
+      if (auto it = GetObjectIt(GetTrackedObjects(), cppobj); it != GetTrackedObjects().end()) {
+         GetTrackedObjects().erase(it);
+      }
    }
-
    return {true, true};
+}
+
+} // namespace
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Injects custom registration/deregistration logic into cppyy's memory regulator
+PyROOT::RegulatorCleanup::RegulatorCleanup()
+{
+   CPyCppyy::MemoryRegulator::SetRegisterHook(RegisterHook);
+   CPyCppyy::MemoryRegulator::SetUnregisterHook(UnregisterHook);
 }
 
 ////////////////////////////////////////////////////////////////////////////
 /// \brief Get the class id of the TObject being deleted and run Cppyy's
 ///        RecursiveRemove.
 /// \param[in] object Object being destructed.
-void PyROOT::TMemoryRegulator::CallCppyyRecursiveRemove(TObject *object)
+void PyROOT::CallCppyyRecursiveRemove(TObject *object)
 {
-   auto cppobj = reinterpret_cast<Cppyy::TCppObject_t>(object);
-
-   if (auto it = fObjectMap.find(cppobj); it != fObjectMap.end()) {
-      CPyCppyy::MemoryRegulator::RecursiveRemove(cppobj, it->second);
-      fObjectMap.erase(it);
-   }
-}
-
-////////////////////////////////////////////////////////////////////////////
-/// \brief Clean up all tracked objects.
-void PyROOT::TMemoryRegulator::ClearProxiedObjects()
-{
-   while (!fObjectMap.empty()) {
-      auto elem = fObjectMap.begin();
-      auto cppobj = elem->first;
-      auto klassid = elem->second;
-      auto pyclass = CPyCppyy::CreateScopeProxy(klassid);
-      auto pyobj = (CPyCppyy::CPPInstance *)CPyCppyy::MemoryRegulator::RetrievePyObject(cppobj, pyclass);
-
-      if (pyobj && (pyobj->fFlags & CPyCppyy::CPPInstance::kIsOwner)) {
-         // Only delete the C++ object if the Python proxy owns it.
-         // If it is a value, cppyy deletes it in RecursiveRemove as part of
-         // the proxy cleanup.
-         auto o = static_cast<TObject *>(cppobj);
-         bool isValue = pyobj->fFlags & CPyCppyy::CPPInstance::kIsValue;
-         CallCppyyRecursiveRemove(o);
-         if (!isValue)
-            delete o;
-      } else {
-         // Non-owning proxy, just unregister to clean tables.
-         // The proxy deletion by Python will have no effect on C++, so all good
-         bool ret = CPyCppyy::MemoryRegulator::UnregisterPyObject(pyobj, pyclass);
-         if (!ret) {
-            fObjectMap.erase(elem);
-         }
-      }
+   auto &trackedObjects = GetTrackedObjects();
+   if (auto it = GetObjectIt(trackedObjects, reinterpret_cast<Cppyy::TCppObject_t>(object));
+       it != trackedObjects.end()) {
+      // The iterator may be invalidated in RecursiveRemove, so we erase it from our tracked objects first.
+      const auto cppobj = it->first;
+      const auto klassid = it->second;
+      trackedObjects.erase(it);
+      CPyCppyy::MemoryRegulator::RecursiveRemove(cppobj, klassid);
    }
 }
