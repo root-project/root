32d7a6179e662bd66a79ba8337ae2cea92be78c3
Danilo Piparo <danilo.piparo@cern.ch>
2024-11-13 16:30:26 +0100

    [core] Set patch number to 7 and not 07
    otherwise, the release version will be 6.32.007

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 32
-#define ROOT_VERSION_PATCH 07
+#define ROOT_VERSION_PATCH 7
 #define ROOT_RELEASE_DATE "September 21 2024"
 
 /* Don't change the lines below. */
