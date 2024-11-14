dd086f7a9a00e7bafda387ec11b12817a71c558e
Danilo Piparo <danilo.piparo@cern.ch>
2024-11-14 14:15:33 +0100

    [core] Bump up version to 6.32.09

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 32
-#define ROOT_VERSION_PATCH 8
+#define ROOT_VERSION_PATCH 9
 #define ROOT_RELEASE_DATE "November 14 2024"
 
 /* Don't change the lines below. */
