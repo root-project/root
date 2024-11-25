c7c19db3d3f45428c730150e028ca778623158ca
silverweed <giacomo.parolini@cern.ch>
2024-11-25 15:30:08 +0100

    [tut] move ZoomHistogram.C to hist008_TH1_zoom.C

--- tutorials/hist/hist008_TH1_zoom.C
+++ tutorials/hist/hist008_TH1_zoom.C
@@ -11,23 +11,32 @@
 /// \macro_image
 /// \macro_code
 ///
+/// \date November 2024
 /// \author Advait Dhingra
 
-void ZoomHistogram()
+void hist008_TH1_zoom()
 {
+   // Create and fill a histogram
    TH1F *orig = new TH1F("Normal Histogram", "Normal Histogram", 100, 0, 100);
 
-   for (int i = 0; i < 100; ++i) {
-     Double_t x = gRandom->Gaus(50, 10);
-     orig->Fill(x);
+   TRandom3 rng;
+   for (int i = 0; i < 1000; ++i) {
+      double x = rng.Gaus(50, 10);
+      orig->Fill(x);
    }
 
-   TH1F *zoom = (TH1F *)orig->Clone("zoom");
+   // Clone the histogram into one called "zoom"
+   TH1F *zoom = static_cast<TH1F *>(orig->Clone("zoom"));
    zoom->SetTitle("Zoomed-in Histogram");
+   // "Zoom" in the histogram by setting a new range to the X axis
    zoom->GetXaxis()->SetRangeUser(50, 100);
 
+   // Draw both histograms to a canvas
    TCanvas *c1 = new TCanvas("c1", "Histogram", 1500, 700);
-   c1->Divide(2, 1);
+   // split the canvas horizontally in 2
+   int nsubdivX = 2;
+   int nsubdivY = 1;
+   c1->Divide(nsubdivX, nsubdivY);
 
    c1->cd(1);
    orig->Draw();
