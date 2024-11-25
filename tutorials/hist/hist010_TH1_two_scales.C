f27bf087e86a431ed87f5eb319c50932b422b8ca
silverweed <giacomo.parolini@cern.ch>
2024-11-25 17:32:45 +0100

    [tut] modernize a bit hist010

--- tutorials/hist/hist010_TH1_two_scales.C
+++ tutorials/hist/hist010_TH1_two_scales.C
@@ -7,13 +7,8 @@
 /// \macro_image
 /// \macro_code
 ///
+/// \date November 2024
 /// \author Rene Brun
-
-#include "TCanvas.h"
-#include "TStyle.h"
-#include "TH1.h"
-#include "TGaxis.h"
-#include "TRandom.h"
 
 void hist010_TH1_two_scales()
 {
@@ -21,24 +16,24 @@ void hist010_TH1_two_scales()
 
    // create/fill draw h1
    gStyle->SetOptStat(kFALSE);
-   TH1F *h1 = new TH1F("h1", "my histogram", 100, -3, 3);
-   Int_t i;
-   for (i = 0; i < 10000; i++)
-      h1->Fill(gRandom->Gaus(0, 1));
+   TH1D *h1 = new TH1D("h1", "my histogram", 100, -3, 3);
+   TRandom3 rng;
+   for (int i = 0; i < 10000; i++)
+      h1->Fill(rng.Gaus(0, 1));
    h1->Draw();
    c1->Update();
 
    // create hint1 filled with the bins integral of h1
-   TH1F *hint1 = new TH1F("hint1", "h1 bins integral", 100, -3, 3);
-   Float_t sum = 0;
-   for (i = 1; i <= 100; i++) {
+   TH1D *hint1 = new TH1D("hint1", "h1 bins integral", 100, -3, 3);
+   double sum = 0;
+   for (int i = 1; i <= 100; i++) {
       sum += h1->GetBinContent(i);
       hint1->SetBinContent(i, sum);
    }
 
    // scale hint1 to the pad coordinates
-   Float_t rightmax = 1.1 * hint1->GetMaximum();
-   Float_t scale = gPad->GetUymax() / rightmax;
+   double rightmax = 1.1 * hint1->GetMaximum();
+   double scale = gPad->GetUymax() / rightmax;
    hint1->SetLineColor(kRed);
    hint1->Scale(scale);
    hint1->Draw("same");
