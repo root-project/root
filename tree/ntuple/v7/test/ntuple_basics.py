214fed3676dab94128137982fa0e381e98fddcea
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-11-25 14:39:19 +0100

    [ntuple] Test writing and reading from Python

---
+++ tree/ntuple/v7/test/ntuple_basics.py
@@ -0,0 +1,22 @@
+import unittest
+
+import ROOT
+
+RNTupleModel = ROOT.Experimental.RNTupleModel
+RNTupleReader = ROOT.Experimental.RNTupleReader
+RNTupleWriter = ROOT.Experimental.RNTupleWriter
+
+class RNTupleBasics(unittest.TestCase):
+    """Basic tests of using RNTuple from Python"""
+
+    def test_write_read(self):
+        """Can write and read a basic RNTuple."""
+
+        model = RNTupleModel.Create()
+        model.MakeField["int"]("f")
+        writer = RNTupleWriter.Recreate(model, "ntpl", "test_ntuple_py_write_read.root")
+        writer.Fill()
+        del writer
+
+        reader = RNTupleReader.Open("ntpl", "test_ntuple_py_write_read.root")
+        self.assertEqual(reader.GetNEntries(), 1)
