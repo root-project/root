2f03dc925bcb2eecb3d1e81574886b1d9c84aeb1
silverweed <giacomo.parolini@cern.ch>
2024-11-25 14:57:05 +0100

    [tut] modernize hist007_TH1_liveupdate

--- tutorials/hist/hist007_TH1_liveupdate.C
+++ tutorials/hist/hist007_TH1_liveupdate.C
@@ -8,22 +8,22 @@
 /// \macro_image
 /// \macro_code
 ///
+/// \date November 2024
 /// \author Rene Brun
 
-void hsum()
+void hist007_TH1_liveupdate()
 {
-   TCanvas *c1 = new TCanvas("c1", "The HSUM example", 200, 10, 600, 400);
+   TCanvas *c1 = new TCanvas("c1", "Live update of histograms", 200, 10, 600, 400);
    c1->SetGrid();
 
-   gBenchmark->Start("hsum");
-
    // Create some histograms.
-   auto total = new TH1F("total", "This is the total distribution", 100, -4, 4);
-   auto main = new TH1F("main", "Main contributor", 100, -4, 4);
-   auto s1 = new TH1F("s1", "This is the first signal", 100, -4, 4);
-   auto s2 = new TH1F("s2", "This is the second signal", 100, -4, 4);
+   auto *total = new TH1D("total", "This is the total distribution", 100, -4, 4);
+   auto *main = new TH1D("main", "Main contributor", 100, -4, 4);
+   auto *s1 = new TH1D("s1", "This is the first signal", 100, -4, 4);
+   auto *s2 = new TH1D("s2", "This is the second signal", 100, -4, 4);
    total->Sumw2(); // store the sum of squares of weights
-   total->SetMarkerStyle(21);
+   // set some style properties
+   total->SetMarkerStyle(21); // shape of the markers (\see EMarkerStyle)
    total->SetMarkerSize(0.7);
    main->SetFillColor(16);
    s1->SetFillColor(42);
@@ -31,13 +31,12 @@ void hsum()
    TSlider *slider = nullptr;
 
    // Fill histograms randomly
-   gRandom->SetSeed();
-   const Int_t kUPDATE = 500;
-   Float_t xs1, xs2, xmain;
-   for (Int_t i = 0; i < 10000; i++) {
-      xmain = gRandom->Gaus(-1, 1.5);
-      xs1 = gRandom->Gaus(-0.5, 0.5);
-      xs2 = gRandom->Landau(1, 0.15);
+   TRandom3 rng;
+   const int kUPDATE = 500;
+   for (int i = 0; i < 10000; i++) {
+      float xmain = rng.Gaus(-1, 1.5);
+      float xs1 = rng.Gaus(-0.5, 0.5);
+      float xs2 = rng.Landau(1, 0.15);
       main->Fill(xmain);
       s1->Fill(xs1, 0.3);
       s2->Fill(xs2, 0.2);
@@ -62,5 +61,4 @@ void hsum()
    }
    slider->SetRange(0., 1.);
    c1->Modified();
-   gBenchmark->Show("hsum");
 }
