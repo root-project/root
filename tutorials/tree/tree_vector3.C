9724bf17db20b55b01f8186c84c2647395f18e26
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2024-11-25 17:19:00 +0100

    modernize and rename tv3 tutorial + add doxy doc

--- tutorials/tree/tree_vector3.C
+++ tutorials/tree/tree_vector3.C
@@ -1,3 +1,13 @@
+/// \file
+/// \ingroup tutorial_tree
+/// \notebook -js
+/// Create a tree containing a Vector3 class and read it back
+///
+/// \macro_image
+/// \macro_code
+///
+/// \author The ROOT Team
+
 class Vector3
 {
    Double_t fX;
@@ -5,7 +15,7 @@ class Vector3
    Double_t fZ;
 
 public:
-   Vector3() : fX(0),fY(0),fZ(0) {}
+   Vector3() : fX(0), fY(0), fZ(0) {}
 
    Double_t x() { return fX; }
    Double_t y() { return fY; }
@@ -18,26 +28,27 @@ public:
    }
 };
 
-void tv3Write() {
+void write_vector3() {
    //creates the Tree
    Vector3 *v = new Vector3();
-   TFile *f = new TFile("v3.root","recreate");
-   TTree *T = new TTree("T","v3 Tree");
-   T->Branch("v3",&v,32000,1);
+   auto f = TFile::Open("v3.root", "recreate");
+   TTree *T = new TTree("T", "v3 Tree");
+   T->Branch("v3", &v, 32000, 1);
    TRandom r;
    for (Int_t i=0;i<10000;i++) {
-      v->SetXYZ(r.Gaus(0,1),r.Landau(0,1),r.Gaus(100,10));
+      v->SetXYZ(r.Gaus(0,1), r.Landau(0,1), r.Gaus(100,10));
       T->Fill();
    }
    T->Write();
    T->Print();
    delete f;
 }
-void tv3Read1() {
+
+void read_vector3_1() {
    //first read example showing how to read all branches
    Vector3 *v = 0;
-   TFile *f = new TFile("v3.root");
-   TTree *T = (TTree*)f->Get("T");
+   auto f = TFile::Open("v3.root");
+   auto T = f->Get<TTree>("T");
    T->SetBranchAddress("v3",&v);
    TH1F *h1 = new TH1F("x","x component of Vector3",100,-3,3);
    Long64_t nentries = T->GetEntries();
@@ -48,14 +59,14 @@ void tv3Read1() {
    h1->Draw();
 }
 
- void tv3Read2() {
+ void read_vector3_2() {
    //second read example illustrating how to read one branch only
    Vector3 *v = 0;
-   TFile *f = new TFile("v3.root");
-   TTree *T = (TTree*)f->Get("T");
-   T->SetBranchAddress("v3",&v);
+   auto f = TFile::Open("v3.root");
+   auto T = f->Get<TTree>("T");
+   T->SetBranchAddress("v3", &v);
    TBranch *by = T->GetBranch("fY");
-   TH1F *h2 = new TH1F("y","y component of Vector3",100,-5,20);
+   TH1F *h2 = new TH1F("y", "y component of Vector3", 100, -5, 20);
    Long64_t nentries = T->GetEntries();
    for (Long64_t i=0;i<nentries;i++) {
       by->GetEntry(i);
@@ -64,12 +75,12 @@ void tv3Read1() {
    h2->Draw();
 }
 
-void tv3() {
-  TCanvas *c1 = new TCanvas("c1","demo of Trees",10,10,600,800);
-  c1->Divide(1,2);
-  tv3Write();
-  c1->cd(1);
-  tv3Read1();
-  c1->cd(2);
-  tv3Read2();
+void tree_vector3() {
+   TCanvas *c1 = new TCanvas("c1", "demo of Trees", 10, 10, 600, 800);
+   c1->Divide(1, 2);
+   write_vector3();
+   c1->cd(1);
+   read_vector3_1();
+   c1->cd(2);
+   read_vector3_2();
 }
