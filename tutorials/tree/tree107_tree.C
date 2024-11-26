66bbee035f9518e00fd5cacac1ea40c247a89264
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2024-11-26 16:09:42 +0100

    Modernize and adapt the code to the new names

--- tutorials/tree/tree107_tree.C
+++ tutorials/tree/tree107_tree.C
@@ -5,7 +5,7 @@
 /// A second Tree is created and filled in parallel.
 /// Run this script with
 /// ~~~
-///   .x tree3.C
+///   .x tree107_tree.C
 /// ~~~
 /// In the function treer, the first Tree is open.
 /// The second Tree is declared friend of the first tree.
@@ -19,8 +19,8 @@
 #include "TRandom.h"
 #include "TCanvas.h"
 
-void tree3w() {
-
+void tree107_write()
+{
    const Int_t kMaxTrack = 500;
    Int_t ntrack;
    Int_t stat[kMaxTrack];
@@ -33,37 +33,37 @@ void tree3w() {
    Float_t chi2[kMaxTrack];
    Double_t sumstat;
 
-   TFile f("tree3.root","recreate");
-   TTree *t3 = new TTree("t3","Reconst ntuple");
-   t3->Branch("ntrack",&ntrack,"ntrack/I");
-   t3->Branch("stat",stat,"stat[ntrack]/I");
-   t3->Branch("sign",sign,"sign[ntrack]/I");
-   t3->Branch("px",px,"px[ntrack]/F");
-   t3->Branch("py",py,"py[ntrack]/F");
-   t3->Branch("pz",pz,"pz[ntrack]/F");
-   t3->Branch("zv",zv,"zv[ntrack]/F");
-   t3->Branch("chi2",chi2,"chi2[ntrack]/F");
+   TFile f("tree108.root", "recreate");
+   auto t3 = new TTree("t3", "Reconst ntuple");
+   t3->Branch("ntrack", &ntrack, "ntrack/I");
+   t3->Branch("stat", stat, "stat[ntrack]/I");
+   t3->Branch("sign", sign, "sign[ntrack]/I");
+   t3->Branch("px", px, "px[ntrack]/F");
+   t3->Branch("py", py, "py[ntrack]/F");
+   t3->Branch("pz", pz, "pz[ntrack]/F");
+   t3->Branch("zv", zv, "zv[ntrack]/F");
+   t3->Branch("chi2", chi2, "chi2[ntrack]/F");
 
-   TFile fr("tree3f.root","recreate");
-   TTree *t3f = new TTree("t3f","a friend Tree");
-   t3f->Branch("ntrack",&ntrack,"ntrack/I");
-   t3f->Branch("sumstat",&sumstat,"sumstat/D");
-   t3f->Branch("pt",pt,"pt[ntrack]/F");
+   TFile fr("tree108f.root", "recreate");
+   auto t3f = new TTree("t3f", "a friend Tree");
+   t3f->Branch("ntrack", &ntrack, "ntrack/I");
+   t3f->Branch("sumstat", &sumstat, "sumstat/D");
+   t3f->Branch("pt", pt, "pt[ntrack]/F");
 
-   for (Int_t i=0;i<1000;i++) {
-      Int_t nt = gRandom->Rndm()*(kMaxTrack-1);
+   for (Int_t i=0; i<1000; i++) {
+      Int_t nt = gRandom->Rndm() * (kMaxTrack - 1);
       ntrack = nt;
       sumstat = 0;
-      for (Int_t n=0;n<nt;n++) {
-         stat[n] = n%3;
-         sign[n] = i%2;
-         px[n]   = gRandom->Gaus(0,1);
-         py[n]   = gRandom->Gaus(0,2);
-         pz[n]   = gRandom->Gaus(10,5);
-         zv[n]   = gRandom->Gaus(100,2);
-         chi2[n] = gRandom->Gaus(0,.01);
+      for (Int_t n=0; n<nt; n++) {
+         stat[n] = n % 3;
+         sign[n] = i % 2;
+         px[n]   = gRandom->Gaus(0, 1);
+         py[n]   = gRandom->Gaus(0, 2);
+         pz[n]   = gRandom->Gaus(10, 5);
+         zv[n]   = gRandom->Gaus(100, 2);
+         chi2[n] = gRandom->Gaus(0, .01);
          sumstat += chi2[n];
-         pt[n]   = TMath::Sqrt(px[n]*px[n] + py[n]*py[n]);
+         pt[n]   = TMath::Sqrt(px[n] * px[n] + py[n] * py[n]);
       }
       t3->Fill();
       t3f->Fill();
@@ -75,28 +75,29 @@ void tree3w() {
    t3f->Write();
 }
 
-void tree3r()
+void tree107_read()
 {
-   TFile *f = new TFile("tree3.root");
-   TTree *t3 = (TTree*)f->Get("t3");
-   t3->AddFriend("t3f","tree3f.root");
-   t3->Draw("pz","pt>3");
+   auto f = TFile::Open("tree108.root");
+   auto t3 = f->Get<TTree>("t3");
+   t3->AddFriend("t3f", "tree108f.root");
+   t3->Draw("pz", "pt>3");
 }
 
-void tree3r2()
+void tree107_read2()
 {
-   TPad *p = new TPad("p","p",0.6, 0.4, 0.98, 0.8);
-   p->Draw(); p->cd();
-   TFile *f1 = new TFile("tree3.root");
-   TFile *f2 = new TFile("tree3f.root");
-   TTree *t3 = (TTree*)f1->Get("t3");
-   t3->AddFriend("t3f",f2);
-   t3->Draw("pz","pt>3");
+   auto p = new TPad("p", "p", 0.6, 0.4, 0.98, 0.8);
+   p->Draw();
+   p->cd();
+   auto f1 = TFile::Open("tree108.root");
+   auto f2 = TFile::Open("tree108f.root");
+   auto t3 = f1->Get<TTree>("t3");
+   t3->AddFriend("t3f", f2);
+   t3->Draw("pz", "pt>3");
 }
 
-void tree3()
+void tree107_tree()
 {
-   tree3w();
-   tree3r();
-   tree3r2();
+   tree107_write();
+   tree107_read();
+   tree107_read2();
 }
