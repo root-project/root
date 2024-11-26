80b0d9b805bc54a21f46edeaba935a34458578e7
skkwan <kystephkwan2@gmail.com>
2024-11-26 11:09:16 +0100

    implement suggested changes, fix and improve links, tweak text

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_th1.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_th1.py
@@ -8,37 +8,50 @@
 # For the list of contributors see $ROOTSYS/README/CREDITS.                    #
 ################################################################################
 
-r'''
+r"""
 /**
-\class ROOT::TH1
+\class TH1
 \brief \parblock \endparblock
 \htmlonly
+<details open>
+<summary  style="font-size:20px; color: #425788;"><b>Python interface</b></summary>
 <div class="pyrootbox">
 \endhtmlonly
-\anchor python
 
+## Fitting histograms in Python
 
-## Fitting histograms in PyROOT
-One-dimensional histograms can be fit in [PyROOT](https://root.cern/manual/python) with a similar syntax as in C++. To fit a 1D histogram to one of the ROOT standard functions (e.g Gaussian):
-~~~{.py}
+One-dimensional histograms can be fit in [Python](https://root.cern/manual/python) with a similar syntax as in C++.
+To fit a 1D histogram to one of the ROOT standard functions (e.g. a Gaussian):
+
+\code{.py}
 # Create and initialize a test histogram to fit
 myTH1D = ROOT.TH1D("th1d", "Histogram for fitting", 200, 0, 10)
 myTH1D.FillRandom("gaus", 1000)
+
 # Fit to a ROOT pre-defined Gaussian function "gaus"
 myTH1D.Fit("gaus")
-~~~
+\endcode
 
-The list of standard functions in ROOT can be accessed with the [gROOT](classTROOT.html) `GetListOfFunctions()`:
-~~~{.py}
+The list of standard functions in ROOT can be accessed with the TROOT::GetListOfFunctions.
+In Python, the standard functions for TF1 can be printed as follows:
+
+\code{.py}
 ROOT.TF1.InitStandardFunctions()
+
 # Print a list of available functions and their definitions
 ROOT.gROOT.GetListOfFunctions().Print()
-~~~
+\endcode
 
-## Accessing results of the fit in PyROOT
-To access the results of the fit, run the `.Fit()` method with the `s` option which returns a [TFitResult](classTFitResult.html). All other [TFitResult](classTFitResult.html) methods can be called with same name as the C++ method. Continuing the short example from before:
-~~~{.py}
-# Call .Fit() with the "s" method to save results
+## Accessing results of the fit in Python
+
+To access the results of the fit, run the TH1::Fit method with the "s" option (please see the TH1::Fit(TF1*, Option_t*, Option_t*, Double_t, Double_t)
+documentation for a list of possible options).
+This will return a TFitResult which can be examined with the corresponding TFitResult methods, with the same names in Python as in C++.
+
+For example:
+
+\code{.py}
+# Re-using the TH1D defined in the earlier example code
 myResult = myTH1D.Fit("gaus", "s")
 
 # Get the fitted parameters as a vector
@@ -46,12 +59,15 @@ myResult.Parameters()
 
 # Get the error of the first parameter
 myResult.ParError(0) 
-~~~
-For a full list and description of options to `.Fit()` (valid in both C++ and PyROOT) please see the [.Fit() method documentation](classTH1.html).
+\endcode
 
-## Fitting to user-defined functions in PyROOT
-1D histograms can also be fit to any user-defined `TF1` (see the `TF1` documentation). The `TF1` can be defined in a ROOT constructor, for example:
-~~~{.py}
+
+## Fitting to user-defined functions in Python
+1D histograms can also be fit to any user-defined function expressed as a TF1 (see the TF1 documentation for examples on how to do this).
+
+For example, a TF1 can be defined and initialized with its ROOT constructor:
+
+\code{.py}
 # Define the function, e.g. a polynomial with two parameters: y(x) = a * x^b
 myTF1 = ROOT.TF1("myFunction", "[0] * pow(x, [1])", 0, 10)
 
@@ -62,14 +78,15 @@ myTF1.SetParameters(10.0, 4.0)
 myTH1D = ROOT.TH1D("th1d", "My histogram to fit", 200, 0, 10)
 myTH1D.FillRandom("myFunction", 1000)
 myTH1D.Fit("myFunction")
-~~~
+\endcode
 
-The `TF1` function can also be initialized from a Python function (see the `TF1` documentation), for example:
-~~~{.py}
+A TF1 can also be defined using a Python function, for example:
+
+\code{.py}
 def myGaussian(x, pars):
-    """
+    '''
     Defines a Gaussian function
-    """
+    '''
     return pars[0]*np.exp(-0.5* pow(x[0] - pars[1], 2)) 
 
 # Initialize from the Python function with the range -5 to +5, with two parameters to fit, and a one-dimensional input x
@@ -81,9 +98,9 @@ myTH1D.FillRandom("gaus", 1000)
 
 # Fit the 1D histogram to our custom Python function
 myTH1D.Fit("myFunction")
-~~~
+\endcode
 
-## Further PyROOT fitting examples
+## Further Python fitting examples
 Further examples can be found in the tutorials:
 - [combinedFit.py](combinedFit__8py.html) performs a combined (simultaneous) fit of two 1D histograms with separate functions and some common parameters.
 - [fit1.py](fit1__8py.html) reads a `TF1` and 1D histogram (created and saved in an earlier example [fillrandom.py](fillrandom__8py.html)), and fits the histogram.
@@ -91,13 +108,13 @@ Further examples can be found in the tutorials:
 - [fitNormSum.py](fitNormSum__8py.html) fits a 1D histogram to the normalized sum of two functions (here, a background exponential and a crystal ball function).
 - [multifit.py](multifit__8py.html) fits multiple functions to different ranges of a 1D histogram.
 
+
 \htmlonly
 </div>
+</details>
 \endhtmlonly
-
-\anchor reference
 */
-'''
+"""
 
 from . import pythonization
 
