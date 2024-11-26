4ac56d34a75f2eb836312eae5efbc85ff833b07b
Jakob Blomer <jblomer@cern.ch>
2024-11-26 11:01:23 +0100

    [ntuple] adapt to MakeField API changes

--- root/io/hadd/merge_gen_input_tuples.C
+++ root/io/hadd/merge_gen_input_tuples.C
@@ -13,13 +13,13 @@
 void merge_gen_input_tuples(const char *fname1 = "test_rntuple_input1.root", const char *fname2 = "test_rntuple_input2.root")
 {
    using namespace ROOT::Experimental;
-   
+
    auto noPrereleaseWarning = RLogScopedVerbosity(NTupleLog(), ROOT::Experimental::ELogLevel::kError);
 
    {
       auto model = RNTupleModel::Create();
-      auto fi = model->MakeField<int>("I", 1337);
-      auto fl = model->MakeField<long>("L", 666);
+      auto fi = model->MakeField<int>("I");
+      auto fl = model->MakeField<long>("L");
       auto opts = RNTupleWriteOptions{};
       opts.SetCompression(ROOT::CompressionSettings(ROOT::RCompressionSetting::EAlgorithm::kZSTD, 5));
       auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fname1, opts);
@@ -31,8 +31,8 @@ void merge_gen_input_tuples(const char *fname1 = "test_rntuple_input1.root", con
    }
    {
       auto model = RNTupleModel::Create();
-      auto fi = model->MakeField<int>("I", 123);
-      auto fl = model->MakeField<long>("L", 420);
+      auto fi = model->MakeField<int>("I");
+      auto fl = model->MakeField<long>("L");
       auto opts = RNTupleWriteOptions{};
       opts.SetCompression(ROOT::CompressionSettings(ROOT::RCompressionSetting::EAlgorithm::kZLIB, 1));
       auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fname2, opts);
