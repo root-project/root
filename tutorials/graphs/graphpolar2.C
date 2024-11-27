ee7977ec61201e8c789388f9d9c055276e7894e3
Sergey Linev <S.Linev@gsi.de>
2024-11-27 11:27:42 +0100

    Adjust graphpolar tutorials to use new draw options
    No need to update pad to configure coordinate system

--- tutorials/graphs/graphpolar2.C
+++ tutorials/graphs/graphpolar2.C
@@ -10,21 +10,21 @@
 
 void graphpolar2()
 {
-   TCanvas * CPol = new TCanvas("CPol","TGraphPolar Example",500,500);
+   auto CPol = new TCanvas("CPol","TGraphPolar Example",500,500);
 
    Double_t theta[8];
    Double_t radius[8];
    Double_t etheta[8];
    Double_t eradius[8];
 
-   for (int i=0; i<8; i++) {
-      theta[i]   = (i+1)*(TMath::Pi()/4.);
-      radius[i]  = (i+1)*0.05;
-      etheta[i]  = TMath::Pi()/8.;
+   for (int i = 0; i < 8; i++) {
+      theta[i] = (i + 1) * (TMath::Pi() / 4.);
+      radius[i] = (i + 1) * 0.05;
+      etheta[i] = TMath::Pi() / 8.;
       eradius[i] = 0.05;
    }
 
-   TGraphPolar * grP1 = new TGraphPolar(8, theta, radius, etheta, eradius);
+   auto grP1 = new TGraphPolar(8, theta, radius, etheta, eradius);
    grP1->SetTitle("");
 
    grP1->SetMarkerStyle(20);
@@ -32,10 +32,5 @@ void graphpolar2()
    grP1->SetMarkerColor(4);
    grP1->SetLineColor(2);
    grP1->SetLineWidth(3);
-   grP1->Draw("PE");
-
-   CPol->Update();
-
-   if (grP1->GetPolargram())
-      grP1->GetPolargram()->SetToRadian();
+   grP1->Draw("RPE"); // R - radian, P - markers, E - errors
 }
