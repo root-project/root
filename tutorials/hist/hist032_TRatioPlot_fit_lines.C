677335419c6dcd1cc2053cf87e63c8e8581fe1ec
aprozo <alexandr.a.prozorov@gmail.com>
2024-11-27 17:09:33 +0100

    only hist/*.C files change and intorudce new naming convention

---
+++ tutorials/hist/hist032_TRatioPlot_fit_lines.C
@@ -0,0 +1,30 @@
+/// \file
+/// \ingroup tutorial_hist
+/// \notebook
+/// Example that shows custom dashed lines on the lower plot, specified by a vector of floats.
+///
+/// By default, dashed lines are drawn at certain points. You can either disable them, or specify
+/// where you want them to appear.
+///
+/// \macro_image
+/// \macro_code
+///
+/// \date January 2020
+/// \author Paul Gessinger
+
+void hist032_TRatioPlot_fit_lines()
+{
+   gStyle->SetOptStat(0);
+   auto c1 = new TCanvas("c1", "fit residual simple");
+   auto h1 = new TH1D("h1", "h1", 50, -5, 5);
+   h1->FillRandom("gaus", 2000);
+   h1->Fit("gaus", "0");
+   h1->GetXaxis()->SetTitle("x");
+   h1->GetYaxis()->SetTitle("y");
+   auto rp1 = new TRatioPlot(h1);
+   std::vector<double> lines = {-3, -2, -1, 0, 1, 2, 3};
+   rp1->SetGridlines(lines);
+   rp1->Draw();
+   rp1->GetLowerRefGraph()->SetMinimum(-4);
+   rp1->GetLowerRefGraph()->SetMaximum(4);
+}
