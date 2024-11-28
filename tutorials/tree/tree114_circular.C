e34544d99679cc93d93f039ae0f0d4e0c1f8b7fe
Bertrand Bellenot <bellenot@users.noreply.github.com>
2024-11-28 14:00:32 +0100

    Adapt code to the new name

--- tutorials/tree/tree114_circular.C
+++ tutorials/tree/tree114_circular.C
@@ -12,21 +12,22 @@
 ///
 /// \author Rene Brun
 
-void circular() {
-   auto T = new TTree("T","test circular buffers");
+void tree114_circular()
+{
    TRandom r;
    Float_t px,py,pz;
    Double_t randomNum;
    UShort_t i;
-   T->Branch("px",&px,"px/F");
-   T->Branch("py",&py,"px/F");
-   T->Branch("pz",&pz,"px/F");
-   T->Branch("random",&randomNum,"random/D");
-   T->Branch("i",&i,"i/s");
-   T->SetCircular(20000); //keep a maximum of 20000 entries in memory
+   auto T = new TTree("T", "test circular buffers");
+   T->Branch("px", &px, "px/F");
+   T->Branch("py", &py, "px/F");
+   T->Branch("pz", &pz, "px/F");
+   T->Branch("random", &randomNum, "random/D");
+   T->Branch("i", &i, "i/s");
+   T->SetCircular(20000); // keep a maximum of 20000 entries in memory
    for (i = 0; i < 65000; i++) {
-      r.Rannor(px,py);
-      pz = px*px + py*py;
+      r.Rannor(px, py);
+      pz = px * px + py * py;
       randomNum = r.Rndm();
       T->Fill();
    }
