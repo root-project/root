1329c6b92fea986d9bac9ec2d8d4a72cad4706e0
Danilo Piparo <danilo.piparo@cern.ch>
2024-11-29 15:41:37 +0100

    Update to odd patch version

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 34
-#define ROOT_VERSION_PATCH 0
+#define ROOT_VERSION_PATCH 1
 #define ROOT_RELEASE_DATE "Nov 29 2024"
 
 /* Don't change the lines below. */
