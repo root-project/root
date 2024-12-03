960d80aa336260bf8642fc4b724451a171e1b3ca
Monica Dessole <36501030+mdessole@users.noreply.github.com>
2024-12-03 11:39:26 +0100

    Fix paths (#17177)

--- tutorials/multicore/mp103_processSelector.C
+++ tutorials/multicore/mp103_processSelector.C
@@ -32,7 +32,7 @@ int mp103_processSelector()
    gROOT->SetBatch(kTRUE);
 
    TString selectorPath = gROOT->GetTutorialDir();
-   selectorPath += "/tree/h1analysis.C+";
+   selectorPath += "/io/tree/h1analysis.C+";
    std::cout << "selector used is: " << selectorPath << "\n";
    auto sel = TSelector::GetSelector(selectorPath);
 
