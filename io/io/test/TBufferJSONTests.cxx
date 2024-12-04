23dbd25c844b0a4fceb913ce181970f80441c25c
Sergey Linev <S.Linev@gsi.de>
2024-12-04 17:50:11 +0100

    [json] add test for new TList functionality
    Check that after reading plain Add was used
    and GetOption() really returns object options

--- io/io/test/TBufferJSONTests.cxx
+++ io/io/test/TBufferJSONTests.cxx
@@ -1,5 +1,6 @@
 #include "TBufferJSON.h"
 #include "TNamed.h"
+#include "TList.h"
 #include <string>
 
 #include "gtest/gtest.h"
@@ -45,5 +46,51 @@ TEST(TBufferJSON, utf8_3)
    auto named1 = TBufferJSON::FromJSON<TNamed>(json.Data());
 
    EXPECT_EQ(str0, named1->GetTitle());
+}
+
+TEST(TBufferJSON, TList_Add_Without_Option)
+{
+   TList lst0;
+   lst0.Add(new TNamed("name0", "title0"));  // without option
+   lst0.Add(new TNamed("name1", "title1"), ""); // with empty option
+   lst0.Add(new TNamed("name2", "title2"), "option2"); // with non-empty option
+   auto json = TBufferJSON::ToJSON(&lst0);
+
+   // auto f = new TMemFile("test.root", "RECREATE");
+   // f->WriteTObject(&lst0, "list");
+
+   lst0.Delete();
+
+   auto lst1 = TBufferJSON::FromJSON<TList>(json.Data());
+
+   // auto lst1 = f->Get<TList>("list");
+   // delete f;
+
+   EXPECT_NE(lst1, nullptr);
+
+   auto link = lst1->FirstLink();
+
+   EXPECT_STREQ("name0", link->GetObject()->GetName());
+   EXPECT_STREQ("title0", link->GetObject()->GetTitle());
+   EXPECT_STREQ("", link->GetAddOption()); // by default empty string returned
+   EXPECT_EQ(dynamic_cast<TObjOptLink *>(link), nullptr);
+
+   link = link->Next();
+
+   EXPECT_STREQ("name1", link->GetObject()->GetName());
+   EXPECT_STREQ("title1", link->GetObject()->GetTitle());
+   EXPECT_STREQ("", link->GetAddOption());
+   EXPECT_NE(dynamic_cast<TObjOptLink *>(link), nullptr); // this will fail in normal ROOT I/O
+
+   link = link->Next();
+
+   EXPECT_STREQ("name2", link->GetObject()->GetName());
+   EXPECT_STREQ("title2", link->GetObject()->GetTitle());
+   EXPECT_STREQ("option2", link->GetAddOption());
+   EXPECT_NE(dynamic_cast<TObjOptLink *>(link), nullptr);
+
+   link = link->Next();
+
+   EXPECT_EQ(link, nullptr);
 }
 
