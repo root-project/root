2019143eff14a67e4519262e78337ebf2a240261
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-12-09 18:50:09 +0100

    [python] Give ownership to C++ for objects with directory registration logic
    This commit streamlines the ownership rules for objects that are able to register
    to directories in ROOT. There are two main cases: automatic registration at object
    creation and manual registration via `SetDirectory`.
    
    In the first scenario, we avoid being too restrictive when giving back ownership
    to C++ and do it with any kind of directory the object is registering to. The only
    exception is if the object is registering to ROOT.gROOT, to avoid backwards-incompatible
    changes.
    
    In the second scenario, if a user explicitly calls `SetDirectory` via the Python API and
    if the directory is a valid object, we release ownership to C++.
    
    The commit also adds tests for all classes that follow the registration logic.
    
    Co-authored-by: Jonas Rembser <jonas.rembser@cern.ch>

--- bindings/pyroot/pythonizations/test/memory.py
+++ bindings/pyroot/pythonizations/test/memory.py
@@ -85,5 +85,113 @@ class MemoryStlString(unittest.TestCase):
 
         os.remove(filename)
 
+    def _check_object_in_subdir(self, klass, args):
+        """
+        Test that an object which automatically registers with a subdirectory
+        does not give ownership to Python
+        """
+        filename = "test_object_in_subdir.root"
+        try:
+            with ROOT.TFile(filename, "recreate") as f:
+                f.mkdir("subdir")
+                f.cd("subdir")
+                x = klass(*args)
+                x.Write()
+        finally:
+            os.remove(filename)
+
+    def test_objects_ownership_with_subdir(self):
+        """
+        Test interaction of various types of objects with automatic directory
+        registration with a subdirectory of a TFile.
+        """
+
+        objs = {
+            "TH1D": ("h", "h", 10, 0, 10),
+            "TH1C": ("h", "h", 10, 0, 10),
+            "TH1S": ("h", "h", 10, 0, 10),
+            "TH1I": ("h", "h", 10, 0, 10),
+            "TH1L": ("h", "h", 10, 0, 10),
+            "TH1F": ("h", "h", 10, 0, 10),
+            "TH1D": ("h", "h", 10, 0, 10),
+            "TH1K": ("h", "h", 10, 0, 10),
+            "TProfile": ("h", "h", 10, 0, 10),
+            "TH2C": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2S": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2I": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2L": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2F": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2D": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2Poly": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH2PolyBin": tuple(),
+            "TProfile2D": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TProfile2PolyBin": tuple(),
+            "TProfile2Poly": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH3C": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TH3S": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TH3I": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TH3L": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TH3F": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TH3D": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TProfile3D": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TGraph2D": (100,),
+            "TEntryList": ("name", "title"),
+            "TEventList": ("name", "title"),
+            "TTree": ("name", "title"),
+            "TNtuple": ("name", "title", "x:y:z"),
+        }
+        for klass, args in objs.items():
+            with self.subTest(klass=klass):
+                self._check_object_in_subdir(getattr(ROOT, klass), args)
+
+    def _check_object_setdirectory(self, klass, classname, args):
+        """
+        Test that registering manually an object with a directory also triggers
+        a release of ownership from Python to C++.
+        """
+        f1 = ROOT.TMemFile(
+            "_check_object_setdirectory_in_memory_file_begin", "recreate")
+
+        x = klass(*args)
+        # TEfficiency does not automatically register with the directory
+        if not classname == "TEfficiency":
+            self.assertIs(x.GetDirectory(), f1)
+            x.SetDirectory(ROOT.nullptr)
+        self.assertFalse(x.GetDirectory())
+        # Make sure that at this point the ownership of the object is with Python
+        ROOT.SetOwnership(x, True)
+
+        f1.Close()
+
+        f2 = ROOT.TMemFile("_check_object_setdirectory_in_memory_file_end", "recreate")
+
+        # The pythonization should trigger the release of ownership to C++
+        x.SetDirectory(f2)
+        self.assertIs(x.GetDirectory(), f2)
+
+        f2.Close()
+
+    def test_objects_interaction_with_setdirectory(self):
+        """
+        Test interaction of various types of objects with manual registration
+        to a directory.
+        """
+
+        objs = {
+            "TH1D": ("h", "h", 10, 0, 10),
+            "TH2D": ("h", "h", 10, 0, 10, 10, 0, 10),
+            "TH3D": ("h", "h", 10, 0, 10, 10, 0, 10, 10, 0, 10),
+            "TGraph2D": (100,),
+            "TEfficiency": (ROOT.TH1D("h1", "h1", 10, 0, 10), ROOT.TH1D("h2", "h2", 10, 0, 10)),
+            "TEntryList": ("name", "title"),
+            "TEventList": ("name", "title"),
+            "TTree": ("name", "title"),
+            "TNtuple": ("name", "title", "x:y:z"),
+        }
+        for classname, args in objs.items():
+            with self.subTest(classname=classname):
+                self._check_object_setdirectory(getattr(ROOT, classname), classname, args)
+
+
 if __name__ == '__main__':
     unittest.main()
