efdabdf9c17eb326ead9319541992bbe05429790
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2024-12-09 18:50:09 +0100

    [python] Give ownership to C++ for objects with directory registration logic
    This commit streamlines the ownership rules for objects that are able to register
    to directories in ROOT. There are two main cases: automatic registration at object
    creation and manual registration via `SetDirectory`.
    
    In the first scenario, we avoid being too restrictive when giving back ownership
    to C++ and do it with any kind of directory the object is registering to. The only
    exception is if the object is registering to ROOT.gROOT, to avoid backwards-incompatible
    changes.
    
    In the second scenario, if a user explicitly calls `SetDirectory` via the Python API and
    if the directory is a valid object, we release ownership to C++.
    
    The commit also adds tests for all classes that follow the registration logic.
    
    Co-authored-by: Jonas Rembser <jonas.rembser@cern.ch>

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_th2.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_th2.py
@@ -1,7 +1,7 @@
-# Author: Enric Tejedor CERN  02/2019
+# Author: Vincenzo Eduardo Padulano 12/2024
 
 ################################################################################
-# Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.                      #
+# Copyright (C) 1995-2024, Rene Brun and Fons Rademakers.                      #
 # All rights reserved.                                                         #
 #                                                                              #
 # For the licensing terms see $ROOTSYS/LICENSE.                                #
@@ -9,18 +9,6 @@
 ################################################################################
 
 from . import pythonization
-
-
-# Multiplication by constant
-
-def _imul(self, c):
-    # Parameters:
-    # - self: histogram
-    # - c: constant by which to multiply the histogram
-    # Returns:
-    # - A multiplied histogram (in place)
-    self.Scale(c)
-    return self
 
 
 def _should_give_up_ownership(object):
@@ -53,34 +41,19 @@ def inject_constructor_releasing_ownership(klass):
 
 
 # The constructors need to be pythonized for each derived class separately:
-_th1_derived_classes_to_pythonize = [
-    "TH1C",
-    "TH1S",
-    "TH1I",
-    "TH1L",
-    "TH1F",
-    "TH1D",
-    "TH1K",
-    "TProfile",
+_th2_derived_classes_to_pythonize = [
+    "TH2C",
+    "TH2S",
+    "TH2I",
+    "TH2L",
+    "TH2F",
+    "TH2D",
+    # "TH2Poly", # Derives from TH2 but does not automatically register
+    # "TH2PolyBin", Does not derive from TH2
+    "TProfile2D",
+    # "TProfile2PolyBin", Derives from TH2PolyBin which does not derive from TH2
+    "TProfile2Poly",
 ]
 
-for klass in _th1_derived_classes_to_pythonize:
+for klass in _th2_derived_classes_to_pythonize:
     pythonization(klass)(inject_constructor_releasing_ownership)
-
-def _SetDirectory_SetOwnership(self, dir):
-    self._Original_SetDirectory(dir)
-    if dir:
-        # If we are actually registering with a directory, give ownership to C++
-        import ROOT
-        ROOT.SetOwnership(self, False)
-
-@pythonization('TH1')
-def pythonize_th1(klass):
-    # Parameters:
-    # klass: class to be pythonized
-
-    # Support hist *= scalar
-    klass.__imul__ = _imul
-
-    klass._Original_SetDirectory = klass.SetDirectory
-    klass.SetDirectory = _SetDirectory_SetOwnership
