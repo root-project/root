4e8ad8c07f40528f68197f5f0e13e7de7dde283f
Monica Dessole <36501030+mdessole@users.noreply.github.com>
2024-12-12 10:53:22 +0100

    [tutorials] Move to legacy and reorganize (#17244)

--- tutorials/legacy/hist/ratioplot.C
+++ tutorials/legacy/hist/ratioplot.C
@@ -1,15 +1,16 @@
 /// \file
-/// \ingroup tutorial_hist
+/// \ingroup tutorial_hist_legacy
 /// Example displaying two histograms and their ratio. This macro does not use the
 /// class TRatioPlot. For ROOT version >= 6.08 TRatioPlot should be used. See
-/// the other ratio plots examples in this folder.
+/// the other ratio plots examples in `tutorials/hist`.
 ///
 /// \macro_image
 /// \macro_code
 ///
 /// \author Olivier Couet
 
-void ratioplotOld( ) {
+void ratioplot()
+{
    // Define two gaussian histograms. Note the X and Y title are defined
    // at booking time using the convention "Hist_title ; X_title ; Y_title"
    TH1F *h1 = new TH1F("h1", "Two gaussian plots and their ratio;x title; h1 and h2 gaussian histograms", 100, -5, 5);
@@ -30,7 +31,7 @@ void ratioplotOld( ) {
    h1->Draw();               // Draw h1
    h2->Draw("same");         // Draw h2 on top of h1
 
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6,8,0)
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 8, 0)
    // Avoid the first label (0) to be clipped.
    TAxis *axis = h1->GetYaxis();
    axis->ChangeLabel(1, -1, -1, -1, -1, -1, " ");
@@ -40,34 +41,34 @@ void ratioplotOld( ) {
    // Do not draw the Y axis label on the upper plot and redraw a small
    // axis instead, in order to avoid the first label (0) to be clipped.
    h1->GetYaxis()->SetLabelSize(0.);
-   TGaxis *axis = new TGaxis( -5, 20, -5, 220, 20,220,510,"");
+   TGaxis *axis = new TGaxis(-5, 20, -5, 220, 20, 220, 510, "");
    axis->SetLabelFont(43); // Absolute font size in pixel (precision 3)
    axis->SetLabelSize(15);
    axis->Draw();
 #endif
 
    // lower plot will be in pad
-   c->cd();          // Go back to the main canvas before defining pad2
+   c->cd(); // Go back to the main canvas before defining pad2
    TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
    pad2->SetTopMargin(0);
    pad2->SetBottomMargin(0.2);
    pad2->SetGridx(); // vertical grid
    pad2->Draw();
-   pad2->cd();       // pad2 becomes the current pad
+   pad2->cd(); // pad2 becomes the current pad
 
    // Define the ratio plot
-   TH1F *h3 = (TH1F*)h1->Clone("h3");
+   TH1F *h3 = (TH1F *)h1->Clone("h3");
    h3->SetLineColor(kBlack);
    h3->SetMinimum(0.8);  // Define Y ..
    h3->SetMaximum(1.35); // .. range
    h3->Sumw2();
-   h3->SetStats(0);      // No statistics on lower plot
+   h3->SetStats(0); // No statistics on lower plot
    h3->Divide(h2);
    h3->SetMarkerStyle(21);
-   h3->Draw("ep");       // Draw the ratio plot
+   h3->Draw("ep"); // Draw the ratio plot
 
    // h1 settings
-   h1->SetLineColor(kBlue+1);
+   h1->SetLineColor(kBlue + 1);
    h1->SetLineWidth(2);
 
    // Y axis h1 plot settings
