eecfcca382bc18bd3c0ee20f046ad2d3c442ce3e
Bertrand Bellenot <bellenot@users.noreply.github.com>
2024-12-12 11:20:40 +0100

    Fix assertion failure in roofitcore
    Fix Assertion failed: can't dereference out of range vector iterator

--- roofit/roofitcore/inc/RooVectorDataStore.h
+++ roofit/roofitcore/inc/RooVectorDataStore.h
@@ -292,7 +292,7 @@ public:
       auto beg = std::min(_vec.cbegin() + first, _vec.cend());
       auto end = std::min(_vec.cbegin() + last,  _vec.cend());
 
-      return std::span<const double>(&*beg, std::distance(beg, end));
+      return std::span<const double>(_vec.data(), std::distance(beg, end));
     }
 
     std::size_t size() const { return _vec.size() ; }
