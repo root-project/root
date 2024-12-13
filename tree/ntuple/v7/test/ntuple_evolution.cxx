0131097e38e1d4f531a074e1b4848b7ab3d7611f
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-12-13 10:09:50 +0100

    [ntuple] Add evolution test for added second base class
    It must come last because otherwise it would change the field names
    in existing base classes.

--- tree/ntuple/v7/test/ntuple_evolution.cxx
+++ tree/ntuple/v7/test/ntuple_evolution.cxx
@@ -457,6 +457,75 @@ struct AddedBaseDerived : public AddedBaseIntermediate {
    EXPECT_EVALUATE_EQ("ptrAddedBaseDerived->fDerived", 93);
 }
 
+TEST(RNTupleEvolution, AddedSecondBaseClass)
+{
+   FileRaii fileGuard("test_ntuple_evolution_added_second_base_class.root");
+
+   WriteOldInFork([&] {
+      // The child process writes the file and exits, but the file must be preserved to be read by the parent.
+      fileGuard.PreserveFile();
+
+      ASSERT_TRUE(gInterpreter->Declare(R"(
+struct AddedSecondBaseFirst {
+   int fFirstBase = 1;
+};
+struct AddedSecondBaseDerived : public AddedSecondBaseFirst {
+   int fDerived = 3;
+};
+)"));
+
+      auto model = RNTupleModel::Create();
+      model->AddField(RFieldBase::Create("f", "AddedSecondBaseDerived").Unwrap());
+
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath());
+      writer->Fill();
+
+      void *ptr = writer->GetModel().GetDefaultEntry().GetPtr<void>("f").get();
+      DeclarePointer("AddedSecondBaseDerived", "ptrAddedSecondBaseDerived", ptr);
+      ProcessLine("ptrAddedSecondBaseDerived->fFirstBase = 71;");
+      ProcessLine("ptrAddedSecondBaseDerived->fDerived = 93;");
+      writer->Fill();
+
+      // Reset / close the writer and flush the file.
+      {
+         // TStreamerInfo::Build will report a warning for interpreted classes (but only for base classes).
+         // See also https://github.com/root-project/root/issues/9371
+         ROOT::TestSupport::CheckDiagsRAII diagRAII;
+         diagRAII.optionalDiag(kWarning, "TStreamerInfo::Build", "has no streamer or dictionary",
+                               /*matchFullMessage=*/false);
+         writer.reset();
+      }
+   });
+
+   ASSERT_TRUE(gInterpreter->Declare(R"(
+struct AddedSecondBaseFirst {
+   int fFirstBase = 1;
+};
+struct AddedSecondBaseSecond {
+   int fSecondBase = 2;
+};
+struct AddedSecondBaseDerived : public AddedSecondBaseFirst, public AddedSecondBaseSecond {
+   int fDerived = 3;
+};
+)"));
+
+   auto reader = RNTupleReader::Open("ntpl", fileGuard.GetPath());
+   ASSERT_EQ(2, reader->GetNEntries());
+
+   void *ptr = reader->GetModel().GetDefaultEntry().GetPtr<void>("f").get();
+   DeclarePointer("AddedSecondBaseDerived", "ptrAddedSecondBaseDerived", ptr);
+
+   reader->LoadEntry(0);
+   EXPECT_EVALUATE_EQ("ptrAddedSecondBaseDerived->fFirstBase", 1);
+   EXPECT_EVALUATE_EQ("ptrAddedSecondBaseDerived->fSecondBase", 2);
+   EXPECT_EVALUATE_EQ("ptrAddedSecondBaseDerived->fDerived", 3);
+
+   reader->LoadEntry(1);
+   EXPECT_EVALUATE_EQ("ptrAddedSecondBaseDerived->fFirstBase", 71);
+   EXPECT_EVALUATE_EQ("ptrAddedSecondBaseDerived->fSecondBase", 2);
+   EXPECT_EVALUATE_EQ("ptrAddedSecondBaseDerived->fDerived", 93);
+}
+
 TEST(RNTupleEvolution, RemovedBaseClass)
 {
    FileRaii fileGuard("test_ntuple_evolution_removed_base_class.root");
