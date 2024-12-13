735c2f4b2b9884f21b544c9587e9779085c38182
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-12-13 13:29:30 +0100

    [ntuple] Compare type names of RClassField

--- tree/ntuple/v7/test/ntuple_types.cxx
+++ tree/ntuple/v7/test/ntuple_types.cxx
@@ -1918,7 +1918,7 @@ TEST(RNTuple, TClass)
          auto viewKlass = ntuple->GetView<DerivedA>("klass");
          FAIL() << "GetView<a_base_class_of_T> should throw";
       } catch (const RException& err) {
-         EXPECT_THAT(err.what(), testing::HasSubstr("No on-disk field information"));
+         EXPECT_THAT(err.what(), testing::HasSubstr("incompatible type name for field"));
       }
    }
 }
