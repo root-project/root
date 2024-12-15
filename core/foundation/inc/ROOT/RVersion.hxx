aeb64854b57213510230084c1bfad6e745d146c5
Danilo Piparo <danilo.piparo@cern.ch>
2024-12-15 13:40:45 +0100

    Update version numberUpdate version number

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 34
-#define ROOT_VERSION_PATCH 2
+#define ROOT_VERSION_PATCH 3
 #define ROOT_RELEASE_DATE "Dec 16 2024"
 
 /* Don't change the lines below. */
