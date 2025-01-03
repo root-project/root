89698751aaa8ec7528eeb0c89b14dc7d2f85cbe4
Philippe Canal <pcanal@fnal.gov>
2025-01-03 16:12:33 -0600

    io: add missing array declaration

--- root/io/evolution/array/MyClassOld.cxx
+++ root/io/evolution/array/MyClassOld.cxx
@@ -4,6 +4,6 @@
 #ifdef __MAKECINT__
 // to be included only if compiling the new version of the class
 #pragma read sourceClass="MyClass" targetClass="MyClass" \
-source="Int_t farray" version="[1]" target="fentries, farrayPointer" targetType="Int_t*, Int_t" \
+source="Int_t farray[5]" version="[1]" target="fentries, farrayPointer" targetType="Int_t*, Int_t" \
 code="{fentries = 5; farrayPointer = new Int_t[fentries]; memcpy(farrayPointer, &(onfile.farray), fentries*sizeof(Int_t)); }"
 #endif
