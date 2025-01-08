3ad8697dd33f67730f071962584a92fa16e515a8
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-01-08 11:16:54 +0100

    [ntuple] Simplify pythonization _REntry_setitem
    For non-fundamental types, just call the assignment operator on the
    pointee. This avoids using a special helper function in C++.

--- tree/ntuple/v7/inc/ROOT/RNTupleUtil.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleUtil.hxx
@@ -302,14 +302,6 @@ static_assert(kTestLocatorType < RNTupleLocator::ELocatorType::kLastSerializable
 /// Check whether a given string is a valid name according to the RNTuple specification
 RResult<void> EnsureValidNameForRNTuple(std::string_view name, std::string_view where);
 
-/// Helper for the Python interface to perform the assignment to the pointee of a shared_ptr.
-template <typename T, typename U>
-void AssignValueToPointee(T &&value, std::shared_ptr<U> &ptr)
-{
-   static_assert(std::is_convertible_v<T, U>, "Cannot convert type of input argument to type of the pointee.");
-   *ptr = std::forward<T>(value);
-}
-
 } // namespace Internal
 
 } // namespace Experimental
