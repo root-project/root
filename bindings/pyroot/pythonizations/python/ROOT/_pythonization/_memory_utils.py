f9c8f8dfee0886e431385af6323e902a8157f5c9
Jonas Rembser <jonas.rembser@cern.ch>
2025-01-10 06:15:42 +0100

    [python] Pythonize `TObject::Clone()` overrides for correct ownership
    Following up on #17250, also pythonizing the `TObject::Clone()`
    overrides to automatically give up the ownership.
    
    Thanks to the following forum post for noticing this problem:
    https://root-forum.cern.ch/t/a-question-about-root/62617

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_memory_utils.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_memory_utils.py
@@ -8,14 +8,17 @@
 # For the list of contributors see $ROOTSYS/README/CREDITS.                    #
 ################################################################################
 
+
 def _should_give_up_ownership(object):
     """
     Ownership of objects which automatically register to a directory should be
     left to C++, except if the object is gROOT.
     """
     import ROOT
+
     tdir = object.GetDirectory()
     return bool(tdir) and tdir is not ROOT.gROOT
+
 
 def _constructor_releasing_ownership(self, *args, **kwargs):
     """
@@ -30,9 +33,33 @@ def _constructor_releasing_ownership(self, *args, **kwargs):
     if _should_give_up_ownership(self):
         ROOT.SetOwnership(self, False)
 
+
+def _Clone_releasing_ownership(self, *args, **kwargs):
+    """
+    Analogous to _constructor_releasing_ownership, but for the TObject::Clone()
+    implementation.
+    """
+    import ROOT
+
+    out = self._Original_Clone(*args, **kwargs)
+    if _should_give_up_ownership(out):
+        ROOT.SetOwnership(out, False)
+    return out
+
+
+def inject_constructor_releasing_ownership(klass):
+    klass._cpp_constructor = klass.__init__
+    klass.__init__ = _constructor_releasing_ownership
+
+def inject_clone_releasing_ownership(klass):
+    klass._Original_Clone = klass.Clone
+    klass.Clone = _Clone_releasing_ownership
+
+
 def _SetDirectory_SetOwnership(self, dir):
     self._Original_SetDirectory(dir)
     if dir:
         # If we are actually registering with a directory, give ownership to C++
         import ROOT
+
         ROOT.SetOwnership(self, False)
