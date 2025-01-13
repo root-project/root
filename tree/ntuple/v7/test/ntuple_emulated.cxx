4fb01805641fe6e44617b3ea55d1b43045c914ef
silverweed <silverweed14@proton.me>
2025-01-13 15:10:29 +0100

    [ntuple] add more unit tests for field emulation

--- tree/ntuple/v7/test/ntuple_emulated.cxx
+++ tree/ntuple/v7/test/ntuple_emulated.cxx
@@ -199,3 +199,152 @@ struct Outer {
    reader = RNTupleReader::Open(cmOpts, "ntpl", fileGuard.GetPath());
    reader->LoadEntry(0);
 }
+
+TEST(RNTupleEmulated, EmulatedFields_EmptyStruct)
+{
+   // Test struct containing an empty struct
+
+   FileRaii fileGuard("test_ntuple_emulated_emptystruct.root");
+
+   ExecInFork([&] {
+      // The child process writes the file and exits, but the file must be preserved to be read by the parent.
+      fileGuard.PreserveFile();
+
+      ASSERT_TRUE(gInterpreter->Declare(R"(
+struct Inner {
+   ClassDefNV(Inner, 2);
+};
+
+struct Outer {
+   Inner fInner;
+   ClassDefNV(Outer, 2);
+};
+)"));
+
+      auto model = RNTupleModel::Create();
+      model->AddField(RFieldBase::Create("f", "Outer").Unwrap());
+
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath());
+      writer->Fill();
+
+      // TStreamerInfo::Build will report a warning for interpreted classes (but only for members).
+      // See also https://github.com/root-project/root/issues/9371
+      ROOT::TestSupport::CheckDiagsRAII diagRAII;
+      diagRAII.optionalDiag(kWarning, "TStreamerInfo::Build", "has no streamer or dictionary",
+                            /*matchFullMessage=*/false);
+      writer.reset();
+   });
+
+   auto reader = RNTupleReader::Open("ntpl", fileGuard.GetPath());
+   const auto &desc = reader->GetDescriptor();
+
+   {
+      RNTupleDescriptor::RCreateModelOptions opts;
+      opts.fEmulateUnknownTypes = false;
+      try {
+         auto model = desc.CreateModel(opts);
+         FAIL() << "Creating a model without fEmulateUnknownTypes should fail";
+      } catch (const ROOT::RException &ex) {
+         ASSERT_THAT(ex.GetError().GetReport(), testing::HasSubstr("unknown type"));
+      }
+   }
+
+   {
+      RNTupleDescriptor::RCreateModelOptions opts;
+      opts.fEmulateUnknownTypes = true;
+      auto model = desc.CreateModel(opts);
+      ASSERT_NE(model, nullptr);
+
+      const auto &outer = model->GetConstField("f");
+      ASSERT_EQ(outer.GetTypeName(), "Outer");
+      ASSERT_EQ(outer.GetStructure(), ENTupleStructure::kRecord);
+      ASSERT_EQ(outer.GetSubFields().size(), 1);
+
+      const auto subfields = outer.GetSubFields();
+      const auto *inner = subfields[0];
+      ASSERT_EQ(inner->GetTypeName(), "Inner");
+      ASSERT_EQ(inner->GetFieldName(), "fInner");
+      ASSERT_EQ(inner->GetStructure(), ENTupleStructure::kRecord);
+      ASSERT_NE(inner->GetTraits() & RFieldBase::kTraitEmulatedField, 0);
+      ASSERT_EQ(inner->GetSubFields().size(), 0);
+   }
+
+   // Now test loading entries with a reader
+   RNTupleDescriptor::RCreateModelOptions cmOpts;
+   cmOpts.fEmulateUnknownTypes = true;
+   reader = RNTupleReader::Open(cmOpts, "ntpl", fileGuard.GetPath());
+   reader->LoadEntry(0);
+}
+
+TEST(RNTupleEmulated, EmulatedFields_EmptyVec)
+{
+   // Test vector of empty structs
+
+   FileRaii fileGuard("test_ntuple_emulated_emptyvec.root");
+
+   ExecInFork([&] {
+      // The child process writes the file and exits, but the file must be preserved to be read by the parent.
+      fileGuard.PreserveFile();
+
+      ASSERT_TRUE(gInterpreter->Declare(R"(
+struct Inner {
+   ClassDefNV(Inner, 2);
+};
+)"));
+
+      auto model = RNTupleModel::Create();
+      model->AddField(RFieldBase::Create("f", "std::vector<Inner>").Unwrap());
+
+      auto writer = RNTupleWriter::Recreate(std::move(model), "ntpl", fileGuard.GetPath());
+      writer->Fill();
+
+      void *ptr = writer->GetModel().GetDefaultEntry().GetPtr<void>("f").get();
+      DeclarePointer("std::vector<Inner>", "ptrInners", ptr);
+
+      ProcessLine("ptrInners->push_back(Inner{});");
+      ProcessLine("ptrInners->push_back(Inner{});");
+      ProcessLine("ptrInners->push_back(Inner{});");
+
+      // TStreamerInfo::Build will report a warning for interpreted classes (but only for members).
+      // See also https://github.com/root-project/root/issues/9371
+      ROOT::TestSupport::CheckDiagsRAII diagRAII;
+      diagRAII.optionalDiag(kWarning, "TStreamerInfo::Build", "has no streamer or dictionary",
+                            /*matchFullMessage=*/false);
+      writer.reset();
+   });
+
+   auto reader = RNTupleReader::Open("ntpl", fileGuard.GetPath());
+   const auto &desc = reader->GetDescriptor();
+
+   {
+      RNTupleDescriptor::RCreateModelOptions opts;
+      opts.fEmulateUnknownTypes = false;
+      try {
+         auto model = desc.CreateModel(opts);
+         FAIL() << "Creating a model without fEmulateUnknownTypes should fail";
+      } catch (const ROOT::RException &ex) {
+         ASSERT_THAT(ex.GetError().GetReport(), testing::HasSubstr("unknown type"));
+      }
+   }
+
+   {
+      RNTupleDescriptor::RCreateModelOptions opts;
+      opts.fEmulateUnknownTypes = true;
+      auto model = desc.CreateModel(opts);
+      ASSERT_NE(model, nullptr);
+
+      const auto &outer = model->GetConstField("f");
+      ASSERT_EQ(outer.GetTypeName(), "std::vector<Inner>");
+      ASSERT_EQ(outer.GetStructure(), ENTupleStructure::kCollection);
+      ASSERT_EQ(outer.GetSubFields().size(), 1);
+      ASSERT_EQ(outer.GetTraits() & RFieldBase::kTraitEmulatedField, 0);
+
+      const auto subfields = outer.GetSubFields();
+      const auto *inner = subfields[0];
+      ASSERT_EQ(inner->GetTypeName(), "Inner");
+      ASSERT_EQ(inner->GetFieldName(), "_0");
+      ASSERT_EQ(inner->GetStructure(), ENTupleStructure::kRecord);
+      ASSERT_NE(inner->GetTraits() & RFieldBase::kTraitEmulatedField, 0);
+      ASSERT_EQ(inner->GetSubFields().size(), 0);
+   }
+}
