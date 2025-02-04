53b8c3e869ead906cfbcddf1f2718366660b5ac6
Philippe Canal <pcanal@fnal.gov>
2025-02-04 11:41:20 -0600

    ci: announce clang-format version used in the check

--- .ci/format_script.sh
+++ .ci/format_script.sh
@@ -3,6 +3,7 @@
 set -ex
 
 echo "Running clang-format against branch $TRAVIS_BRANCH, with hash $BASE_COMMIT"
+clang-format --version
 COMMIT_FILES=$(git diff --name-status $BASE_COMMIT | grep -i -v '.mjs$' | grep -i -v LinkDef | grep -v -E '^D +' | sed -E 's,^.[[:space:]]+,,')
 
 RESULT_OUTPUT="no modified files to format"
