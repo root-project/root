7cb56d1aec4b56585263029799744dd55d252cf2
Philippe Canal <pcanal@fnal.gov>
2025-02-04 12:06:35 -0600

    ci: clang-format step, reorder and reword error output

--- .ci/format_script.sh
+++ .ci/format_script.sh
@@ -17,14 +17,14 @@ if [ "$RESULT_OUTPUT" == "no modified files to format" ] \
   echo "clang-format passed."
   exit 0
 else
-  echo "clang-format failed."
-  echo "To reproduce it locally please run"
-  echo -e "\tgit checkout $TRAVIS_PULL_REQUEST_BRANCH"
-  echo -e "\tgit-clang-format --commit $BASE_COMMIT --diff --binary $(which clang-format)"
+  echo  -e "clang-format failed with the following diff:\n"
   echo "$RESULT_OUTPUT"
 
-  echo -e "\n\nPlease apply the code formatting changes without bloating the history."
-  echo -e "\tConsider running:"
+  echo -e "\nTo reproduce it locally please run"
+  echo -e "\tgit checkout $TRAVIS_PULL_REQUEST_BRANCH"
+  echo -e "\tgit-clang-format --commit $BASE_COMMIT --diff --binary $(which clang-format) # adjust to point to the local clang-format"
+
+  echo -e "\nConsider running the following to apply the code formatting changes without bloating the history."
   echo -e "\t\tgit checkout $TRAVIS_PULL_REQUEST_BRANCH"
   echo -e "\t\tgit rebase -i -x \"git-clang-format master && git commit -a --allow-empty --fixup=HEAD\" --strategy-option=theirs origin/master"
   echo -e "\t Then inspect the results with git log --oneline"
