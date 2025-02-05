86e3faefa250c57f515bd930554c73ef585cdfdd
ferdymercury <ferdymercury@users.noreply.github.com>
2025-02-05 15:35:22 +0100

    move arg to right constructor

--- net/net/inc/TMessage.h
+++ net/net/inc/TMessage.h
@@ -62,11 +62,11 @@ protected:
    enum EStatusBits {
      kIsOwnerComp = BIT(19) // if TMessage owns fBufComp
    };
-   TMessage(void *buf, Int_t bufsize);   // only called by T(P)Socket::Recv()
+   TMessage(void *buf, Int_t bufsize, Bool_t adopt = kTRUE);   // only called by T(P)Socket::Recv()
    void SetLength() const;               // only called by T(P)Socket::Send()
 
 public:
-   TMessage(UInt_t what = kMESS_ANY, Int_t bufsiz = TBuffer::kInitialSize, Bool_t adopt = kTRUE);
+   TMessage(UInt_t what = kMESS_ANY, Int_t bufsiz = TBuffer::kInitialSize);
    virtual ~TMessage();
 
    void     ForceWriteInfo(TVirtualStreamerInfo *info, Bool_t force) override;
