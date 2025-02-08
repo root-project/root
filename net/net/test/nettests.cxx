5ccdaeeecee8cb2773d7b8db486abe43eeceb682
Danilo Piparo <danilo.piparo@cern.ch>
2025-02-08 09:42:23 +0100

    [net] TServerSocket: let ROOT find a port for the test (#17670)
    (cherry picked from commit 1f3f4cab6844aac5dd1763e9b745a0caaa97caae)

--- net/net/test/nettests.cxx
+++ net/net/test/nettests.cxx
@@ -5,7 +5,8 @@
 
 TEST(TServerSocket, SocketBinding)
 {
-   TServerSocket theSocket(2020, false, TServerSocket::kDefaultBacklog, -1, ESocketBindOption::kInaddrLoopback);
+   // The socket is 0 to let ROOT find a free port for this test
+   TServerSocket theSocket(0, false, TServerSocket::kDefaultBacklog, -1, ESocketBindOption::kInaddrLoopback);
    const auto addr = theSocket.GetLocalInetAddress().GetHostAddress();
    const auto expectedAddr = "0.0.0.0";
    ASSERT_THAT(addr, ::testing::StrNe(expectedAddr))
