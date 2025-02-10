64e2624584bf6c91d58acc50e7244b4d8d3bffbc
ferdymercury <ferdymercury@users.noreply.github.com>
2025-02-10 14:56:30 +0100

    [skip-ci] reformat doxygen brief convention

--- math/mathcore/inc/Math/Minimizer.h
+++ math/mathcore/inc/Math/Minimizer.h
@@ -132,16 +132,16 @@ public:
    Minimizer(Minimizer &&) = delete;
    Minimizer &operator=(Minimizer &&) = delete;
 
-   /// reset for consecutive minimization - implement if needed
+   /// Reset for consecutive minimization - implement if needed.
    virtual void Clear() {}
 
-   /// set the function to minimize
+   /// Set the function to minimize.
    virtual void SetFunction(const ROOT::Math::IMultiGenFunction & func) = 0;
 
-   /// set the function implementing Hessian computation (re-implemented by Minimizer using it)
+   /// Set the function implementing Hessian computation (re-implemented by Minimizer using it).
    virtual void SetHessianFunction(std::function<bool(std::span<const double>, double *)> ) {}
 
-   /// add variables  . Return number of variables successfully added
+   /// Add variables. @return number of variables successfully added
    template<class VariableIterator>
    int SetVariables(const VariableIterator & begin, const VariableIterator & end) {
       unsigned int ivar = 0;
@@ -164,14 +164,14 @@ public:
       }
       return ivar;
    }
-   /// set a new free variable
+   /// Set a new free variable.
    virtual bool SetVariable(unsigned int ivar, const std::string & name, double val, double step) = 0;
-   /// set initial second derivatives
+   /// Set initial second derivatives.
    virtual bool SetCovarianceDiag(std::span<const double> d2, unsigned int n);
-   /// set initial covariance matrix
+   /// Set initial covariance matrix.
    virtual bool SetCovariance(std::span<const double> cov, unsigned int nrow);
 
-   /// set a new lower limit variable  (override if minimizer supports them), leave upper bound unlimited
+   /// Set a new lower limit variable (override if minimizer supports them), leave upper bound unlimited.
    /// @see Minimizer::SetLimitedVariable
    /// @param ivar the index of this variable in the array
    /// @param name the variable name
@@ -181,7 +181,7 @@ public:
    virtual bool SetLowerLimitedVariable(unsigned int ivar, const std::string & name, double val, double step, double lower) {
       return SetLimitedVariable(ivar, name, val, step, lower, std::numeric_limits<double>::infinity());
    }
-   /// set a new upper limit variable (override if minimizer supports them), leave lower bound unlimited
+   /// Set a new upper limit variable (override if minimizer supports them), leave lower bound unlimited.
    /// @see Minimizer::SetLimitedVariable
    /// @param ivar the index of this variable in the array
    /// @param name the variable name
@@ -195,7 +195,7 @@ public:
                                    double lower, double upper);
    virtual bool SetFixedVariable(unsigned int  ivar, const std::string &  name, double val);
    virtual bool SetVariableValue(unsigned int ivar, double value);
-   /// set the values of all existing variables (array must be dimensioned to the size of the existing parameters)
+   /// Set the values of all existing variables (array must be dimensioned to the size of the existing parameters).
    virtual bool SetVariableValues(const double * x) {
       bool ret = true;
       unsigned int i = 0;
@@ -207,7 +207,7 @@ public:
    virtual bool SetVariableStepSize(unsigned int ivar, double value);
    virtual bool SetVariableLowerLimit(unsigned int ivar, double lower);
    virtual bool SetVariableUpperLimit(unsigned int ivar, double upper);
-   /// set the limits of an already existing variable
+   /// Set the limits of an already existing variable.
    virtual bool SetVariableLimits(unsigned int ivar, double lower, double upper) {
       return SetVariableLowerLimit(ivar,lower) && SetVariableUpperLimit(ivar,upper);
    }
@@ -216,45 +216,45 @@ public:
    virtual bool IsFixedVariable(unsigned int ivar) const;
    virtual bool GetVariableSettings(unsigned int ivar, ROOT::Fit::ParameterSettings & pars) const;
 
-   /// set the initial range of an existing variable
+   /// Set the initial range of an existing variable.
    virtual bool SetVariableInitialRange(unsigned int /* ivar */, double /* mininitial */, double /* maxinitial */) {
      return false;
    }
 
-   /// method to perform the minimization
+   /// Method to perform the minimization.
    virtual  bool Minimize() = 0;
 
-   /// return minimum function value
+   /// @return minimum function value
    virtual double MinValue() const = 0;
 
-   /// return  pointer to X values at the minimum
+   /// @return pointer to X values at the minimum
    virtual const double *  X() const = 0;
 
-   /// return expected distance reached from the minimum (re-implement if minimizer provides it
+   /// @return expected distance reached from the minimum (re-implement if minimizer provides it
    virtual double Edm() const { return -1; }
 
-   /// return pointer to gradient values at the minimum
+   /// @return pointer to gradient values at the minimum
    virtual const double *  MinGradient() const { return nullptr; }
 
-   /// number of function calls to reach the minimum
+   /// Number of function calls to reach the minimum.
    virtual unsigned int NCalls() const { return 0; }
 
-   /// number of iterations to reach the minimum
+   /// Number of iterations to reach the minimum.
    virtual unsigned int NIterations() const { return NCalls(); }
 
    /// this is <= Function().NDim() which is the total
    /// number of variables (free+ constrained ones)
    virtual unsigned int NDim() const = 0;
 
-   /// number of free variables (real dimension of the problem)
+   /// Number of free variables (real dimension of the problem).
    /// this is <= Function().NDim() which is the total
    /// (re-implement if minimizer supports bounded parameters)
    virtual unsigned int NFree() const { return NDim(); }
 
-   /// minimizer provides error and error matrix
+   /// Minimizer provides error and error matrix.
    virtual bool ProvidesError() const { return false; }
 
-   /// return errors at the minimum
+   /// @return errors at the minimum
    virtual const double * Errors() const { return nullptr; }
 
    virtual double CovMatrix(unsigned int  ivar , unsigned int jvar ) const;
@@ -262,7 +262,7 @@ public:
    virtual bool GetHessianMatrix(double * hMat) const;
 
 
-   ///return status of covariance matrix
+   /// @return status of covariance matrix
    /// using Minuit convention {0 not calculated 1 approximated 2 made pos def , 3 accurate}
    /// Minimizer who implements covariance matrix calculation will re-implement the method
    virtual int CovMatrixStatus() const {
@@ -270,8 +270,8 @@ public:
    }
 
    /**
-      return correlation coefficient between variable i and j.
-      If the variable is fixed or const the return value is zero
+    * @return correlation coefficient between variable i and j.
+    * If the variable is fixed or const the return value is zero
     */
    virtual double Correlation(unsigned int i, unsigned int j ) const {
       double tmp = CovMatrix(i,i) * CovMatrix(j,j);
@@ -287,89 +287,89 @@ public:
    virtual bool Contour(unsigned int ivar , unsigned int jvar, unsigned int & npoints,
                         double *  xi , double * xj );
 
-   /// return reference to the objective function
-   ///virtual const ROOT::Math::IGenFunction & Function() const = 0;
+   /// @return reference to the objective function
+   /// virtual const ROOT::Math::IGenFunction & Function() const = 0;
 
-   /// print the result according to set level (implemented for TMinuit for maintaining Minuit-style printing)
+   /// Print the result according to set level (implemented for TMinuit for maintaining Minuit-style printing).
    virtual void PrintResults() {}
 
    virtual std::string VariableName(unsigned int ivar) const;
 
    virtual int VariableIndex(const std::string & name) const;
 
-   /** minimizer configuration parameters **/
+   /* minimizer configuration parameters */
 
-   /// set print level
+   /// Set print level.
    int PrintLevel() const { return fOptions.PrintLevel(); }
 
-   ///  max number of function calls
+   /// Max number of function calls.
    unsigned int MaxFunctionCalls() const { return fOptions.MaxFunctionCalls(); }
 
-   /// max iterations
+   /// Max iterations.
    unsigned int MaxIterations() const { return fOptions.MaxIterations(); }
 
-   /// absolute tolerance
+   /// Absolute tolerance.
    double Tolerance() const { return  fOptions.Tolerance(); }
 
-   /// precision of minimizer in the evaluation of the objective function
-   /// ( a value <=0 corresponds to the let the minimizer choose its default one)
+   /// Precision of minimizer in the evaluation of the objective function.
+   /// (a value <=0 corresponds to the let the minimizer choose its default one)
    double Precision() const { return fOptions.Precision(); }
 
-   /// strategy
+   /// Strategy.
    int Strategy() const { return fOptions.Strategy(); }
 
-   /// status code of minimizer
+   /// Status code of minimizer.
    int Status() const { return fStatus; }
 
-   /// status code of Minos (to be re-implemented by the minimizers supporting Minos)
+   /// Status code of Minos (to be re-implemented by the minimizers supporting Minos).
    virtual int MinosStatus() const { return -1; }
 
-   /// return the statistical scale used for calculate the error
+   /// @return the statistical scale used for calculate the error
    /// is typically 1 for Chi2 and 0.5 for likelihood minimization
    double ErrorDef() const { return fOptions.ErrorDef(); }
 
-   ///return true if Minimizer has performed a detailed error validation (e.g. run Hesse for Minuit)
+   /// @return true if Minimizer has performed a detailed error validation (e.g. run Hesse for Minuit)
    bool IsValidError() const { return fValidError; }
 
-   /// retrieve the minimizer options (implement derived class if needed)
+   /// Retrieve the minimizer options (implement derived class if needed).
    virtual MinimizerOptions  Options() const {
       return fOptions;
    }
 
-   /// set print level
+   /// Set print level.
    void SetPrintLevel(int level) { fOptions.SetPrintLevel(level); }
 
-   ///set maximum of function calls
+   /// Set maximum of function calls.
    void SetMaxFunctionCalls(unsigned int maxfcn) { if (maxfcn > 0) fOptions.SetMaxFunctionCalls(maxfcn); }
 
-   /// set maximum iterations (one iteration can have many function calls)
+   /// Set maximum iterations (one iteration can have many function calls).
    void SetMaxIterations(unsigned int maxiter) { if (maxiter > 0) fOptions.SetMaxIterations(maxiter); }
 
-   /// set the tolerance
+   /// Set the tolerance.
    void SetTolerance(double tol) { fOptions.SetTolerance(tol); }
 
-   /// set in the minimizer the objective function evaluation precision
-   /// ( a value <=0 means the minimizer will choose its optimal value automatically, i.e. default case)
+   /// Set in the minimizer the objective function evaluation precision.
+   /// (a value <=0 means the minimizer will choose its optimal value automatically, i.e. default case)
    void SetPrecision(double prec) { fOptions.SetPrecision(prec); }
 
-   ///set the strategy
+   /// Set the strategy.
    void SetStrategy(int strategyLevel) { fOptions.SetStrategy(strategyLevel); }
 
-   /// set scale for calculating the errors
+   /// Set scale for calculating the errors.
    void SetErrorDef(double up) { fOptions.SetErrorDef(up); }
 
-   /// flag to check if minimizer needs to perform accurate error analysis (e.g. run Hesse for Minuit)
+   /// Flag to check if minimizer needs to perform accurate error analysis (e.g. run Hesse for Minuit).
    void SetValidError(bool on) { fValidError = on; }
 
-   /// set all options in one go
+   /// Set all options in one go.
    void SetOptions(const MinimizerOptions & opt) {
       fOptions = opt;
    }
 
-   /// set only the extra options
+   /// Set only the extra options.
    void SetExtraOptions(const IOptions & extraOptions) { fOptions.SetExtraOptions(extraOptions); }
 
-   /// reset the default options (defined in MinimizerOptions)
+   /// Reset the default options (defined in MinimizerOptions).
    void SetDefaultOptions() {
       fOptions.ResetToDefaultOptions();
    }
