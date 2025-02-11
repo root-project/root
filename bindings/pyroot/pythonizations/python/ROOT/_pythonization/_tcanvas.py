cb14a217ce3252f7499d002cb5feeb5189a2c19c
Sergey Linev <S.Linev@gsi.de>
2025-02-11 14:21:29 +0100

    Add block flag to TCanvas.Draw pythonization
    Same as with `c.Update(True)`,
    `c.Draw(True)` will run event loop until key is pressed. Also update will be performed once.

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tcanvas.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tcanvas.py
@@ -73,26 +73,10 @@ def wait_press_posix():
    finally:
       termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-
-def _TCanvas_Update(self, block = False):
-   """
-   Updates the canvas.
-   Also blocks script execution and runs the ROOT graphics event loop until the <space> keyword is pressed,
-   but only if the following conditions are met:
-   * The `block` optional argument is set to `True`.
-   * ROOT graphics are enabled, i.e. `ROOT.gROOT.IsBatch() == False`.
-   * The script is running not in ipython notebooks.
-   """
-
+def run_root_event_loop():
    from ROOT import gROOT
    import os
    import sys
-
-   self._Update()
-
-   # blocking flag is not set
-   if not block:
-      return
 
    # no special handling in batch mode
    if gROOT.IsBatch():
@@ -110,11 +94,52 @@ def _TCanvas_Update(self, block = False):
       wait_press_posix()
 
 
+def _TCanvas_Update(self, block = False):
+   """
+   Updates the canvas.
+   Also blocks script execution and runs the ROOT graphics event loop until the <space> keyword is pressed,
+   but only if the following conditions are met:
+   * The `block` optional argument is set to `True`.
+   * ROOT graphics are enabled, i.e. `ROOT.gROOT.IsBatch() == False`.
+   * The script is running not in ipython notebooks.
+   """
+
+   self._Update()
+
+   # run loop if block flag is set
+   if block:
+      run_root_event_loop()
+
+
+def _TCanvas_Draw(self, block = False):
+   """
+   Draw the canvas.
+   Also blocks script execution and runs the ROOT graphics event loop until the <space> keyword is pressed,
+   but only if the following conditions are met:
+   * The `block` optional argument is set to `True`.
+   * ROOT graphics are enabled, i.e. `ROOT.gROOT.IsBatch() == False`.
+   * The script is running not in ipython notebooks.
+   """
+
+   if isinstance(block, str):
+      self._Draw(block)
+      return
+
+   self._Draw()
+
+   # run loop if block flag is set
+   if block == True:
+      self._Update()
+      run_root_event_loop()
+
+
 @pythonization('TCanvas')
 def pythonize_tcanvas(klass):
     # Parameters:
     # klass: class to be pythonized
 
     klass._Update = klass.Update
+    klass._Draw = klass.Draw
     klass.Update = _TCanvas_Update
+    klass.Draw = _TCanvas_Draw
 
