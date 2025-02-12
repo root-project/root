root-project/root

commit bfbfe8783d0a216e8efff5f4aaff991bdba47563

Properly handle TTree aliases containing array indices. This fixes Savannah #57598.
git-svn-id: http://root.cern.ch/svn/root/trunk@30877 27541ba8-7e3a-0410-8455-c3a389f83636

tree/treeplayer/src/TTreeFormula.cxx
@@ -1987,8 +1987,10 @@ Int_t TTreeFormula::ParseWithLeaf(TLeaf* leaf, const char* subExpression, Bool_t
       const char *funcname = 0;
       if (objClass == TString::Class()) {
          funcname = "Data";
+         //tobetested: numberOfVarDim += RegisterDimensions(code,1,0); // Register the dim of the implied char*
       } else if (objClass == stdStringClass) {
          funcname = "c_str";
+         //tobetested: numberOfVarDim += RegisterDimensions(code,1,0); // Register the dim of the implied char*
       }
       if (funcname) {
          TMethodCall *method = new TMethodCall(objClass, funcname, "");
@@ -2051,6 +2053,7 @@ Int_t TTreeFormula::ParseWithLeaf(TLeaf* leaf, const char* subExpression, Bool_t
             else fLookupType[code] = kTreeMember;
          }            
 
+         //tobetested: numberOfVarDim += RegisterDimensions(code,1,0); // Register the dim of the implied char*
          return kDefinedString;
       }
       if (method->IsValid()
@@ -2552,9 +2555,9 @@ Int_t TTreeFormula::FindLeafForExpression(const char* expression, TLeaf*& leaf, 
 
       // Check for an alias.
       const char *aliasValue = fTree->GetAlias(left);
-      if (aliasValue && strcspn(aliasValue,"+*/-%&!=<>|")==strlen(aliasValue)) {
+      if (aliasValue && strcspn(aliasValue,"[]+*/-%&!=<>|")==strlen(aliasValue)) {
          // First check whether we are using this alias recursively (this would
-         // lead to an infinite recursion.
+         // lead to an infinite recursion).
          if (find(aliasUsed.begin(),
                   aliasUsed.end(),
                   left) != aliasUsed.end()) {
@@ -2780,27 +2783,42 @@ Int_t TTreeFormula::DefinedVariable(TString &name, Int_t &action)
             return -3;
          }
 
-         std::vector<std::string> aliasSofar = fAliasesUsed;
-         aliasSofar.push_back( cname );
 
-         // Need to check the aliases used so far
-         TTreeFormula *subform = new TTreeFormula(cname,aliasValue,fTree,aliasSofar); // Need to pass the aliases used so far.
+         if (strcspn(aliasValue,"+*/-%&!=<>|")!=strlen(aliasValue)) {
+            // If the alias contains an operator, we need to use a nested formula
+            // (since DefinedVariable must only add one entry to the operation's list).
+            
+            // Need to check the aliases used so far
+            std::vector<std::string> aliasSofar = fAliasesUsed;
+            aliasSofar.push_back( cname );
 
-         if (subform->GetNdim()==0) {
-            Error("DefinedVariable",
-                  "The substitution of the alias \"%s\" by \"%s\" failed.",cname,aliasValue);
-            return -3;
-         }
+            TString subValue( aliasValue );
+            if (dims[0]) {
+               subValue += dims;
+            }
 
-         fManager->Add(subform);
-         fAliases.AddAtAndExpand(subform,fNoper);
+            TTreeFormula *subform = new TTreeFormula(cname,subValue,fTree,aliasSofar); // Need to pass the aliases used so far.
 
-         if (subform->IsString()) {
-            action = kAliasString;
-            return 0;
-         } else {
-            action = kAlias;
-            return 0;
+            if (subform->GetNdim()==0) {
+               Error("DefinedVariable",
+                     "The substitution of the alias \"%s\" by \"%s\" failed.",cname,aliasValue);
+               return -3;
+            }
+
+            fManager->Add(subform);
+            fAliases.AddAtAndExpand(subform,fNoper);
+
+            if (subform->IsString()) {
+               action = kAliasString;
+               return 0;
+            } else {
+               action = kAlias;
+               return 0;
+            }
+         } else { /* assumes strcspn(aliasValue,"[]")!=strlen(aliasValue) */
+            TString thisAlias( aliasValue );
+            thisAlias += dims;
+            return DefinedVariable(thisAlias,action);
          }
       }
    }

commit 2f88edbd29d32b0829f1abaa9f96334b584a8b7b

[tree] Allow a constant as alias in formula
Fixes https://its.cern.ch/jira/browse/ROOT-4012

Regression since https://github.com/root-project/root/commit/bfbfe8783d0a216e8efff5f4aaff991bdba47563

tree/treeplayer/src/TTreeFormula.cxx
@@ -51,6 +51,7 @@
 #include <cstdlib>
 #include <typeinfo>
 #include <algorithm>
+#include <sstream>
 
 const Int_t kMaxLen     = 2048;
 
@@ -101,6 +102,18 @@ inline static void R__LoadBranch(TBranch* br, Long64_t entry, bool quickLoad)
    if (!quickLoad || (br->GetReadEntry() != entry)) {
       br->GetEntry(entry);
    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Helper function checking if a string contains a literal number.
+/// Whitespaces are not allowed as part of a valid number-string
+/// \return true if it can be converted to a valid number (floating or integer),
+/// false otherwise.
+
+bool IsNumberConstant(const std::string& str) {
+   std::istringstream iss(str);
+   double number;
+   return iss >> std::noskipws >> number && iss.eof();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -2870,7 +2883,7 @@ Int_t TTreeFormula::DefinedVariable(TString &name, Int_t &action)
          }
 
 
-         if (strcspn(aliasValue,"()+*/-%&!=<>|")!=strlen(aliasValue)) {
+         if (strcspn(aliasValue,"()+*/-%&!=<>|")!=strlen(aliasValue) || IsNumberConstant(aliasValue)) {
             // If the alias contains an operator, we need to use a nested formula
             // (since DefinedVariable must only add one entry to the operation's list).
 

tree/treeplayer/test/regressions.cxx
@@ -4,6 +4,8 @@
 #include "TTree.h"
 #include "TTreeReader.h"
 #include "TTreeReaderValue.h"
+#include "TTreeFormula.h"
+#include "TString.h"
 
 #include "gtest/gtest.h"
 
@@ -172,4 +174,16 @@ TEST(TTreeReaderRegressions, IndexedFriend)
    }
 
    gSystem->Unlink(fname);
+}
+
+// ROOT-4012 (JIRA)
+TEST(TTreeFormulaRegressions, ConstantAlias)
+{
+   TTree t("t","ti");
+   t.SetAlias("w","3");
+   TTreeFormula tf;
+   tf.SetTree(&t);
+   Int_t action;
+   TString expr = "w";
+   EXPECT_EQ(tf.DefinedVariable(expr, action), 0); // was -3 during the regression
 }