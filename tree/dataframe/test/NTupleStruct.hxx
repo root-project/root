4ddb7a344eab74553080911d53621cdd50db7b93
Florine de Geus <florine.de.geus@cern.ch>
2025-02-12 19:55:01 +0100

    [DF] Add collection type tests

--- tree/dataframe/test/NTupleStruct.hxx
+++ tree/dataframe/test/NTupleStruct.hxx
@@ -1,11 +1,15 @@
 #ifndef ROOT7_RDataFrame_Test_NTupleStruct
 #define ROOT7_RDataFrame_Test_NTupleStruct
 
+#include <set>
+
 /**
  * Used to test serialization and deserialization of classes in RNTuple with TClass
  */
 struct Electron {
    float pt;
+
+   friend bool operator<(const Electron &left, const Electron &right) { return left.pt < right.pt; }
 };
 
 #endif
