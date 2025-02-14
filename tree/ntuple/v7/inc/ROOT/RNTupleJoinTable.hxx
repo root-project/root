314033a2a9223591db9d4df0cf26a3dbe40ea443
Florine de Geus <florine.de.geus@cern.ch>
2025-02-14 16:04:27 +0100

    [ntuple] Update `RNTupleJoinTable` documentation
    To reflect the name change from `RNTupleIndex` to `RNTupleJoinTable`
    (and fix some typos along the way).

--- tree/ntuple/v7/inc/ROOT/RNTupleJoinTable.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleJoinTable.hxx
@@ -30,7 +30,7 @@ namespace Internal {
 /**
 \class ROOT::Experimental::Internal::RNTupleJoinTable
 \ingroup NTuple
-\brief Builds an index on one or several fields of an RNTuple so it can be joined onto other RNTuples.
+\brief Builds a join table on one or several fields of an RNTuple so it can be joined onto other RNTuples.
 */
 // clang-format on
 class RNTupleJoinTable {
@@ -39,7 +39,7 @@ public:
 
 private:
    /////////////////////////////////////////////////////////////////////////////
-   /// Container for the hashes of the indexed fields.
+   /// Container for the hashes of the join fields.
    class RCombinedJoinFieldValue {
    public:
       std::vector<NTupleJoinValue_t> fFieldValues;
@@ -52,7 +52,7 @@ private:
    };
 
    /////////////////////////////////////////////////////////////////////////////
-   /// Hash combinining the individual index value hashes from RCombinedJoinFieldValue. Uses the implementation from
+   /// Hash combining the individual join field value hashes from RCombinedJoinFieldValue. Uses the implementation from
    /// `boost::hash_combine` (see
    /// https://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine).
    struct RCombinedJoinFieldValueHash {
@@ -66,31 +66,31 @@ private:
       }
    };
 
-   /// The index itself. Maps field values (or combinations thereof in case the index is defined for multiple fields) to
-   /// their respsective entry numbers.
+   /// The join table itself. Maps field values (or combinations thereof in case the join table is defined for multiple
+   /// fields) to their respective entry numbers.
    std::unordered_map<RCombinedJoinFieldValue, std::vector<ROOT::NTupleSize_t>, RCombinedJoinFieldValueHash> fJoinTable;
 
-   /// The page source belonging to the RNTuple for which to build the index.
+   /// The page source belonging to the RNTuple for which to build the join table.
    std::unique_ptr<RPageSource> fPageSource;
 
-   /// The fields for which the index is built. Used to compute the hashes for each entry value.
+   /// The fields for which the join table is built. Used to compute the hashes for each entry value.
    std::vector<std::unique_ptr<RFieldBase>> fJoinFields;
 
-   /// Only built indexes can be queried.
+   /// Only built join tables can be queried.
    bool fIsBuilt = false;
 
    /////////////////////////////////////////////////////////////////////////////
    /// \brief Create an a new RNTupleJoinTable for the RNTuple represented by the provided page source.
    ///
-   /// \param[in] fieldNames The names of the fields to index. Only integral-type fields can be specified as index
-   /// fields.
-   /// \param[in] pageSource The page source.
+   /// \param[in] fieldNames The names of the join fields to use for the join table. Only integral-type fields are
+   /// allowed.
+   //  \param[in] pageSource The page source.
    RNTupleJoinTable(const std::vector<std::string> &fieldNames, const RPageSource &pageSource);
 
    /////////////////////////////////////////////////////////////////////////////
    /// \brief Ensure the RNTupleJoinTable has been built.
    ///
-   /// \throws RException If the index has not been built, and can therefore not be used yet.
+   /// \throws RException If the join table has not been built, and can therefore not be used yet.
    void EnsureBuilt() const;
 
 public:
@@ -103,30 +103,30 @@ public:
    /////////////////////////////////////////////////////////////////////////////
    /// \brief Create an RNTupleJoinTable from an existing RNTuple.
    ///
-   /// \param[in] fieldNames The names of the fields to index. Only integral-type fields can be specified as index
-   /// fields.
+   /// \param[in] fieldNames The names of the join fields to use for the join table. Only integral-type fields are
+   /// allowed.
    /// \param[in] pageSource The page source.
-   /// \param[in] deferBuild When set to `true`, an empty index will be created. A call to RNTupleJoinTable::Build is
-   /// required before the index can actually be used.
+   /// \param[in] deferBuild When set to `true`, an empty join table will be created. A call to RNTupleJoinTable::Build
+   /// is required before the join table can actually be used.
    ///
-   /// \return A pointer to the newly-created index.
+   /// \return A pointer to the newly-created join table.
    static std::unique_ptr<RNTupleJoinTable>
    Create(const std::vector<std::string> &fieldNames, const RPageSource &pageSource, bool deferBuild = false);
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Build the index.
+   /// \brief Build the join table.
    ///
-   /// Only a built index can be queried (with RNTupleJoinTable::GetFirstEntryNumber or
+   /// Only a built join table can be queried (with RNTupleJoinTable::GetFirstEntryNumber or
    /// RNTupleJoinTable::GetAllEntryNumbers).
    void Build();
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Get the number of indexed values.
+   /// \brief Get the number of entries in the join table.
    ///
-   /// \return The number of indexed values.
+   /// \return The number of entries in the join table.
    ///
    /// \note This does not have to correspond to the number of entries in the original RNTuple. If the original RNTuple
-   /// contains duplicate index values, they are counted as one.
+   /// contains duplicate join field values, they are counted as one.
    std::size_t GetSize() const
    {
       EnsureBuilt();
@@ -134,27 +134,27 @@ public:
    }
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Whether the index has been built (and therefore ready to be used).
+   /// \brief Whether the join table has been built (and therefore ready to be used).
    ///
-   /// \return `true` if the index has been built.
+   /// \return `true` if the join table has been built.
    ///
-   /// Only built indexes can be queried.
+   /// Only built join tables can be queried.
    bool IsBuilt() const { return fIsBuilt; }
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Get the first entry number containing the given index value.
+   /// \brief Get the first entry number corresponding to the given join field value(s).
    ///
-   /// \param[in] valuePtrs A vector of pointers to the index values to look up.
+   /// \param[in] valuePtrs A vector of pointers to the join field values to look up.
    ///
    /// \return The first entry number that corresponds to `valuePtrs`. When no such entry exists, `kInvalidNTupleIndex`
    /// is returned.
    ///
-   /// Note that in case multiple entries corresponding to the provided index value exist, the first occurrence is
+   /// Note that in case multiple entries corresponding to the provided join field value exist, the first occurrence is
    /// returned. Use RNTupleJoinTable::GetAllEntryNumbers to get all entries.
    ROOT::NTupleSize_t GetFirstEntryNumber(const std::vector<void *> &valuePtrs) const;
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Get the entry number containing the given index value.
+   /// \brief Get the entry number corresponding to the given join field value(s).
    ///
    /// \sa GetFirstEntryNumber(std::vector<void *> valuePtrs)
    template <typename... Ts>
@@ -171,16 +171,16 @@ public:
    }
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Get all entry numbers for the given index.
+   /// \brief Get all entry numbers for the given join field value(s).
    ///
-   /// \param[in] valuePtrs A vector of pointers to the index values to look up.
+   /// \param[in] valuePtrs A vector of pointers to the join field values to look up.
    ///
    /// \return The entry numbers that corresponds to `valuePtrs`. When no such entry exists, an empty vector is
    /// returned.
    const std::vector<ROOT::NTupleSize_t> *GetAllEntryNumbers(const std::vector<void *> &valuePtrs) const;
 
    /////////////////////////////////////////////////////////////////////////////
-   /// \brief Get all entry numbers for the given index.
+   /// \brief Get all entry numbers for the given join field value(s).
    ///
    /// \sa GetAllEntryNumbers(std::vector<void *> valuePtrs)
    template <typename... Ts>
