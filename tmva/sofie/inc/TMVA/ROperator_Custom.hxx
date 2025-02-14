392798dfc03ad4df8b5c23ea649b11b53bb68995
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2025-02-14 12:37:26 +0100

    fix: custom opertor input should be const span

--- tmva/sofie/inc/TMVA/ROperator_Custom.hxx
+++ tmva/sofie/inc/TMVA/ROperator_Custom.hxx
@@ -72,6 +72,7 @@ public:
          for (auto & i : fOutputNames) std::cout << " " << i;
          std::cout << "\n";
       }
+      model.AddNeededStdLib("span");
    }
 
     std::string Generate(std::string OpName){
@@ -80,7 +81,7 @@ public:
       out << "\n//------ "<<fOpName<<" \n";
       std::string args;
       for(long unsigned int i = 0; i<fInputNames.size(); ++i){
-        args+="std::span<"+ConvertTypeToString(fInputType)+">(tensor_"+std::string(fInputNames[i])+", "+fInputSizes[i]+"),";
+        args+="std::span<const "+ConvertTypeToString(fInputType)+">(tensor_"+std::string(fInputNames[i])+", "+fInputSizes[i]+"),";
       }
 
       for(long unsigned int i = 0; i<fOutputNames.size(); ++i){
