b8ce7a2e65bbe2dcff0e4260b582d84cde8197fe
silverweed <giacomo.parolini@cern.ch>
2025-02-18 15:39:50 +0100

    [ntuple] merger test: only do uncompressible check when needed
    If the compression is the expected one we don't need to check for
    uncompressible pages

--- tree/ntuple/v7/test/ntuple_merger.cxx
+++ tree/ntuple/v7/test/ntuple_merger.cxx
@@ -838,17 +838,17 @@ static bool VerifyPageCompression(const std::string_view fileName, std::uint32_t
       R__getCompressionAlgorithm((const unsigned char *)sealedPage.GetBuffer(), sealedPage.GetDataSize());
    if (compAlgo == ROOT::RCompressionSetting::EAlgorithm::kUndefined)
       compAlgo = 0;
-   if (compAlgo == 0) {
-      // This page might be uncompressed because compressing it wouldn't have saved space: check if that's the case.
-      const auto nbytesComp =
-         RNTupleCompressor::Zip(sealedPage.GetBuffer(), sealedPage.GetDataSize(), expectedComp, buffer.get());
-      if (nbytesComp == sealedPage.GetDataSize()) {
-         // Yep, the page was uncompressible. Accept that and return true.
-         return true;
-      }
-      // Not good, the page should have been compressed! Fall through and follow the usual error flow.
-   }
    if (compAlgo != (expectedComp / 100)) {
+      if (compAlgo == 0) {
+         // This page might be uncompressed because compressing it wouldn't have saved space: check if that's the case.
+         const auto nbytesComp =
+            RNTupleCompressor::Zip(sealedPage.GetBuffer(), sealedPage.GetDataSize(), expectedComp, buffer.get());
+         if (nbytesComp == sealedPage.GetDataSize()) {
+            // Yep, the page was uncompressible. Accept that and return true.
+            return true;
+         }
+         // Not good, the page should have been compressed! Fall through and follow the usual error flow.
+      }
       std::cerr << "Actual compression is wrong: " << compAlgo << " instead of " << (expectedComp / 100) << "\n";
       ok = false;
    }
