23f0857148547b0d0e1b6560ec82c06c059b62fa
ferdymercury <ferdymercury@users.noreply.github.com>
2025-02-19 18:34:56 +0100

    [test] missing semicolon after declaration

--- hist/hist/test/test_TH1.cxx
+++ hist/hist/test/test_TH1.cxx
@@ -129,7 +129,7 @@ TEST(TH1, Normalize)
    EXPECT_FLOAT_EQ(h4.Integral(), 1);   
    EXPECT_FLOAT_EQ(h4.Integral("width"), 0.1);
    EXPECT_FLOAT_EQ(h4.GetMaximum(), 5./9);
-   const Float_t xbins[10+1] {0.,0.05,0.1,0.15,0.2,0.3,0.5,0.7,0.8,0.95,1.}
+   const Float_t xbins[10+1] {0.,0.05,0.1,0.15,0.2,0.3,0.5,0.7,0.8,0.95,1.};
    TH1F v1("v1","vbw", 10, xbins);
    v1.SetBinContent(1,1.);
    v1.SetBinContent(3,5.);
