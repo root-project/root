eef419c8ebd0c4198228233f72f7fac7a1125b74
Erik Jensen <erik@tenku.dk>
2025-02-19 20:13:39 +0100

    [FreeBSD] fix building on FreeBSD

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -526,6 +526,9 @@ if(asimage)
     if(builtin_zlib)
       set(_after_cflags "${_after_cflags} -I${ZLIB_INCLUDE_DIR}")
     endif()
+    if(CMAKE_SYSTEM_NAME MATCHES FreeBSD)
+      set(AFTERIMAGE_LIBRARIES ${CMAKE_BINARY_DIR}/AFTERIMAGE-prefix/src/AFTERIMAGE/libAfterImage${CMAKE_STATIC_LIBRARY_SUFFIX})
+    endif()
     ExternalProject_Add(
       AFTERIMAGE
       DOWNLOAD_COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/graf2d/asimage/src/libAfterImage AFTERIMAGE
@@ -544,6 +547,9 @@ if(asimage)
       TIMEOUT 600
     )
     set(AFTERIMAGE_INCLUDE_DIR ${CMAKE_BINARY_DIR}/include/libAfterImage)
+    if(CMAKE_SYSTEM_NAME MATCHES FreeBSD)
+      set(AFTERIMAGE_INCLUDE_DIR ${CMAKE_BINARY_DIR}/AFTERIMAGE-prefix/src/AFTERIMAGE)
+    endif()
   endif()
   if(builtin_freetype)
     add_dependencies(AFTERIMAGE FREETYPE)
