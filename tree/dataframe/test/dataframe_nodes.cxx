19f8ef7e5e3b191c1580bdcee0567dcf2ec4d9f1
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2025-02-24 23:43:36 +0100

    [df] Use appropriate constructor in test
    Use the RLoopManager constructor without a data source in the simple
    tests that just check for the validity of the constructed RLoopManager.

--- tree/dataframe/test/dataframe_nodes.cxx
+++ tree/dataframe/test/dataframe_nodes.cxx
@@ -45,13 +45,13 @@ TEST(RDataFrameNodes, RSlotStackPutBackTooMany)
 
 TEST(RDataFrameNodes, RLoopManagerGetLoopManagerUnchecked)
 {
-   ROOT::Detail::RDF::RLoopManager lm(nullptr, {});
+   ROOT::Detail::RDF::RLoopManager lm{};
    ASSERT_EQ(&lm, lm.GetLoopManagerUnchecked());
 }
 
 TEST(RDataFrameNodes, RLoopManagerJitWrongCode)
 {
-   ROOT::Detail::RDF::RLoopManager lm(nullptr, {});
+   ROOT::Detail::RDF::RLoopManager lm{};
    lm.ToJitExec("souble d = 3.14");
    EXPECT_THROW(lm.Run(), std::runtime_error) << "Bogus C++ code was jitted and nothing was detected!";
 }
