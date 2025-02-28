41edb0994a6638fc90126c09d1d7ea65ea83cbae
Sergey Linev <S.Linev@gsi.de>
2025-02-28 17:23:46 +0100

    Simplify TGraph::SaveHistogramAndFunctions
    Use internal static counter,
    invoke Name/Title storage,
    Avoid quote usage

--- hist/hist/inc/TGraph.h
+++ hist/hist/inc/TGraph.h
@@ -66,7 +66,7 @@ protected:
    Double_t         **ShrinkAndCopy(Int_t size, Int_t iend);
    virtual Bool_t     DoMerge(const TGraph * g);
 
-   void               SaveHistogramAndFunctions(std::ostream &out, const char *varname, Int_t &frameNumber, Option_t *option);
+   void               SaveHistogramAndFunctions(std::ostream &out, const char *varname, Option_t *option);
 
 public:
    // TGraph status bits
