368e658a9582ca38bc4ac3d93afd7600b4d5ad4a
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2025-03-06 17:14:11 +0100

    [df] Simplify nslots in RDataSource
    The fNSlots data member is used by all data sources, thus it can be upstreamed to the base class.

--- tree/dataframe/inc/ROOT/RArrowDS.hxx
+++ tree/dataframe/inc/ROOT/RArrowDS.hxx
@@ -32,7 +32,6 @@ private:
    std::shared_ptr<arrow::Table> fTable;
    std::vector<std::pair<ULong64_t, ULong64_t>> fEntryRanges;
    std::vector<std::string> fColumnNames;
-   size_t fNSlots = 0U;
 
    std::vector<std::pair<size_t, size_t>> fGetterIndex; // (columnId, visitorId)
    std::vector<std::unique_ptr<ROOT::Internal::RDF::TValueGetter>> fValueGetters; // Visitors to be used to track and get entries. One per column.
