95e6d65e228b3ad363a71e9fb2bb31052524061c
silverweed <giacomo.parolini@cern.ch>
2025-03-07 10:07:57 +0100

    [ntuple] Move RFieldUtils out of Experimental

--- tree/ntuple/v7/src/RFieldSequenceContainer.cxx
+++ tree/ntuple/v7/src/RFieldSequenceContainer.cxx
@@ -16,11 +16,11 @@
 
 ROOT::Experimental::RArrayField::RArrayField(std::string_view fieldName, std::unique_ptr<RFieldBase> itemField,
                                              std::size_t arrayLength)
-   : ROOT::Experimental::RFieldBase(fieldName,
-                                    "std::array<" + itemField->GetTypeName() + "," +
-                                       Internal::GetNormalizedInteger(static_cast<unsigned long long>(arrayLength)) +
-                                       ">",
-                                    ROOT::ENTupleStructure::kLeaf, false /* isSimple */, arrayLength),
+   : ROOT::Experimental::RFieldBase(
+        fieldName,
+        "std::array<" + itemField->GetTypeName() + "," +
+           ROOT::Internal::GetNormalizedInteger(static_cast<unsigned long long>(arrayLength)) + ">",
+        ROOT::ENTupleStructure::kLeaf, false /* isSimple */, arrayLength),
      fItemSize(itemField->GetValueSize()),
      fArrayLength(arrayLength)
 {
