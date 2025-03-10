30cf055c2b3d075404377e7a24e7ddc1075351d5
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2025-03-10 07:27:36 +0100

    [df] Improve semantics of data member
    Change the data member holding the column names which should be ignored in case
    of TTreeReader errors regarding missing branches, from std::vector to std::set.
    The column names should not be repeated to avoid having multiple times the same
    column name left in the data member, which results in leftover names even after
    the corresponding operation of the computation graph has finished. Once a column
    is found, it will be available for all nodes of the computation graph anyway.

--- tree/dataframe/inc/ROOT/RDF/RLoopManager.hxx
+++ tree/dataframe/inc/ROOT/RDF/RLoopManager.hxx
@@ -196,7 +196,7 @@ class RLoopManager : public RNodeBase {
    // List of branches for which we want to suppress the printed error about
    // missing branch when switching to a new tree. This is modified by readers,
    // so must be declared before them in this class.
-   std::vector<std::string> fSuppressErrorsForMissingBranches{};
+   std::set<std::string> fSuppressErrorsForMissingBranches{};
    ROOT::Internal::RDF::RStringCache fCachedColNames;
    std::set<std::pair<std::string_view, std::unique_ptr<ROOT::Internal::RDF::RDefinesWithReaders>>>
       fUniqueDefinesWithReaders;
@@ -293,15 +293,22 @@ public:
       return fUniqueVariationsWithReaders;
    }
 
-   std::vector<std::string> &GetSuppressErrorsForMissingBranches() { return fSuppressErrorsForMissingBranches; }
-   const std::vector<std::string> &GetSuppressErrorsForMissingBranches() const
-   {
-      return fSuppressErrorsForMissingBranches;
-   }
-
    void SetTTreeLifeline(std::any lifeline);
 
    void SetDataSource(std::unique_ptr<ROOT::RDF::RDataSource> dataSource);
+
+   void InsertSuppressErrorsForMissingBranch(const std::string &branchName)
+   {
+      fSuppressErrorsForMissingBranches.insert(branchName);
+   }
+   void EraseSuppressErrorsForMissingBranch(const std::string &branchName)
+   {
+      fSuppressErrorsForMissingBranches.erase(branchName);
+   }
+   const std::set<std::string> &GetSuppressErrorsForMissingBranches() const
+   {
+      return fSuppressErrorsForMissingBranches;
+   }
 };
 
 /// \brief Create an RLoopManager that reads a TChain.
