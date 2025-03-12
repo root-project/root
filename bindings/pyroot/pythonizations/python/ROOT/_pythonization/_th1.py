5589f318d5d902c404f332e52029746dd9c3ab19
Silia Taider <siliataider@gmail.com>
2025-03-12 17:35:56 +0100

    [UHI] Add TH1 pythonizations for Unified Histogram Indexing Support

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_th1.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_th1.py
@@ -240,17 +240,17 @@ _th1_derived_classes_to_pythonize = [
 for klass in _th1_derived_classes_to_pythonize:
     pythonization(klass)(inject_constructor_releasing_ownership)
 
-    from ROOT._pythonization._uhi import add_plotting_features
-    
-    # Add UHI components
-    uhi_components = [add_plotting_features]
-    for uc in uhi_components:
-        pythonization(klass)(uc)
+    from ROOT._pythonization._uhi import _add_plotting_features
+
+    # Add UHI plotting features
+    pythonization(klass)(_add_plotting_features)
+
 
 @pythonization('TH1')
 def pythonize_th1(klass):
     # Parameters:
     # klass: class to be pythonized
+    from ROOT._pythonization._uhi import _add_indexing_features
 
     # Support hist *= scalar
     klass.__imul__ = _imul
@@ -261,5 +261,8 @@ def pythonize_th1(klass):
 
     klass._Original_SetDirectory = klass.SetDirectory
     klass.SetDirectory = _SetDirectory_SetOwnership
+
+    # Add UHI indexing features
+    _add_indexing_features(klass)
 
     inject_clone_releasing_ownership(klass)
