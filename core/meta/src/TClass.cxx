08fa0a6131c6c2129fe665ab56893cd9bad0d1d5
Philippe Canal <pcanal@fnal.gov>
2025-03-13 16:38:57 +0000

    Remove spurrious statement.
    At this point, if `cl` was found it was from a simple `FindObject` on the list class
    which guarantees that the name is already normalized

--- core/meta/src/TClass.cxx
+++ core/meta/src/TClass.cxx
@@ -3068,7 +3068,6 @@ TClass *TClass::GetClass(const char *name, Bool_t load, Bool_t silent, size_t hi
      }
    } else {
       normalizedName = cl->GetName(); // Use the fact that all TClass names are normalized.
-      checkTable = load && (normalizedName != name);
    }
 
    if (!load) return nullptr;
