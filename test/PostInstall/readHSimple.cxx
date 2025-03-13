4119b41286a7063675162a938c166a7cc7bfcbf7
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2025-03-13 13:11:52 +0100

    fixup! Add a simple post-install test.

--- test/PostInstall/readHSimple.cxx
+++ test/PostInstall/readHSimple.cxx
@@ -1,15 +1,19 @@
 #include <TH1.h>
 #include <TFile.h>
 
-int main() {
-  TFile file("hsimple.root", "READ");
-  if (!file.IsOpen()) return 1;
+int main()
+{
+   TFile file("hsimple.root", "READ");
+   if (!file.IsOpen())
+      return 1;
 
-  TH1 * histo = file.Get<TH1>("hpx");
-  if (!histo) return 2;
+   TH1 *histo = file.Get<TH1>("hpx");
+   if (!histo)
+      return 2;
 
-  if (histo->GetEntries() != 25000) return 3;
-  histo->Print();
+   if (histo->GetEntries() != 25000)
+      return 3;
+   histo->Print();
 
-  return 0;
+   return 0;
 }
