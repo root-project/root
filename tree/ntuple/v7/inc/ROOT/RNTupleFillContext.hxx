42355b769be0a98b198f8f3c1b727a98c42530c2
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-03-19 12:02:18 +0100

    [ntuple] Mark some CreateEntry const
    RNTupleReader::CreateEntry() needs to call GetModel(), which is not
    const.

--- tree/ntuple/v7/inc/ROOT/RNTupleFillContext.hxx
+++ tree/ntuple/v7/inc/ROOT/RNTupleFillContext.hxx
@@ -125,7 +125,7 @@ public:
    void CommitStagedClusters();
 
    const RNTupleModel &GetModel() const { return *fModel; }
-   std::unique_ptr<REntry> CreateEntry() { return fModel->CreateEntry(); }
+   std::unique_ptr<REntry> CreateEntry() const { return fModel->CreateEntry(); }
 
    /// Return the entry number that was last flushed in a cluster.
    ROOT::NTupleSize_t GetLastFlushed() const { return fLastFlushed; }
