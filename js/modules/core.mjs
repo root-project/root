3a43c022cf29d4f718c387fa103f03773d6bbd91
Sergey Linev <S.Linev@gsi.de>
2025-03-19 13:23:20 +0100

    [jsroot] 7.8.x 12/03/2025
    1. Fix - latex super-script without leading symbol, https://root-forum.cern.ch/t/63114/
    2. Fix - correctly read `std::pair<>` without dictionary, https://root-forum.cern.ch/t/63114/

--- js/modules/core.mjs
+++ js/modules/core.mjs
@@ -4,7 +4,7 @@ const version_id = '7.8.x',
 
 /** @summary version date
   * @desc Release date in format day/month/year like '14/04/2022' */
-version_date = '10/03/2025',
+version_date = '12/03/2025',
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
