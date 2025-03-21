91f21ccc97bb840955801cd698906151807a1bf8
Aditya Saroha <143502508+Aditya-138-12@users.noreply.github.com>
2025-03-21 06:31:44 +0530

    [RDF] Update _rvec.py
    Add support for NumPy boolean type 'b1' in RVec conversion
    
    Modify _get_cpp_type_from_numpy_type in _rvec.py to map NumPy's 'b1'

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_rvec.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_rvec.py
@@ -75,7 +75,7 @@ _array_interface_dtype_map = {
 
 
 def _get_cpp_type_from_numpy_type(dtype):
-    cpptypes = {"i4": "int", "u4": "unsigned int", "i8": "Long64_t", "u8": "ULong64_t", "f4": "float", "f8": "double"}
+    cpptypes = {"i4": "int", "u4": "unsigned int", "i8": "Long64_t", "u8": "ULong64_t", "f4": "float", "f8": "double", "b1":"bool", "i1":"int8_t", "u1":"uint8_t", "i2":"int16_t", "u2":"uint16_t"}
 
     if not dtype in cpptypes:
         raise RuntimeError("Object not convertible: Python object has unknown data-type '" + dtype + "'.")
