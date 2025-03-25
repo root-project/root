1b26b571b14473bf8eba7823e9ab7cedc8c38417
Costa Bushnaq <bushnaq.costa@gmail.com>
2025-03-25 16:28:55 +0100

    root/treeformula/references make to cmake
    * implemented the tests in cmake
    
    * remove Makefile
    
    * Avoid variable shadowing

--- root/treeformula/references/dt_RefProxyTest.C
+++ root/treeformula/references/dt_RefProxyTest.C
@@ -1,10 +1,12 @@
+#define ClingWorkAroundMissingDynamicScope
+
 bool dt_RefProxyTest() {
   gSystem->Load("libTreePlayer");
-  gSystem->Load("libEvent");
+  gSystem->Load("libTreeFormulaReferencesGeneration");
 
   const char* fname;
 
-  fname = "Event.new.split0.root";
+  fname = "TreeFormulaReferences.new.split0.root";
   printf("\n\n\n=====================  %s  =================================\n\n\n",fname);
   TFile::Open(fname);
 #ifdef ClingWorkAroundMissingDynamicScope
@@ -17,7 +19,7 @@ bool dt_RefProxyTest() {
   T->Scan("fLastTrack.GetUniqueID():fLastTrack@.GetUniqueID():fLastTrack.GetUniqueID()&0xFFFF:fTracks[fTracks@.size()-1].fUniqueID&0xFFFF:fTracks[0].GetUniqueID()&0xFFFF","","precision=3 col=9d");
   T->Scan("fHighPt.fUniqueID:fHighPt.fUniqueID&0xFFFF:fHighPt.fPx:fHighPt.fPy:fHighPt.fPz:fTracks[fHighPt.fUniqueID&0xFFFF-1].fUniqueID&0xFFFF:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPx:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPy:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPz","","precision=3 col=9d",1);
 
-  fname = "Event.new.split1.root";
+  fname = "TreeFormulaReferences.new.split1.root";
   printf("\n\n\n=====================  %s  =================================\n\n\n",fname);
   TFile::Open(fname);
 #ifdef ClingWorkAroundMissingDynamicScope
@@ -29,7 +31,7 @@ bool dt_RefProxyTest() {
   T->Scan("fLastTrack.GetUniqueID():fLastTrack@.GetUniqueID():fLastTrack.GetUniqueID()&0xFFFF:fTracks[fTracks@.size()-1].fUniqueID&0xFFFF:fTracks[0].GetUniqueID()&0xFFFF","","precision=3 col=9d");
   T->Scan("fHighPt.fUniqueID:fHighPt.fUniqueID&0xFFFF:fHighPt.fPx:fHighPt.fPy:fHighPt.fPz:fTracks[fHighPt.fUniqueID&0xFFFF-1].fUniqueID&0xFFFF:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPx:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPy:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPz","","precision=3 col=9d",1);
 
-  fname = "Event.new.split2.root";
+  fname = "TreeFormulaReferences.new.split2.root";
   printf("\n\n\n=====================  %s  =================================\n\n\n",fname);
   TFile::Open(fname);
 #ifdef ClingWorkAroundMissingDynamicScope
@@ -42,7 +44,7 @@ printf("crap\n");
   T->Scan("fLastTrack.GetUniqueID():fLastTrack@.GetUniqueID():fLastTrack.GetUniqueID()&0xFFFF:fTracks[fTracks@.size()-1].GetUniqueID()&0xFFFF:fTracks[0].GetUniqueID()&0xFFFF","","precision=3 col=9d");
   T->Scan("fHighPt.fUniqueID:fHighPt.fUniqueID&0xFFFF:fHighPt.fPx:fHighPt.fPy:fHighPt.fPz:fTracks[fHighPt.fUniqueID&0xFFFF-1].fUniqueID&0xFFFF:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPx:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPy:fTracks[fHighPt.fUniqueID&0xFFFF-1].fPz","","precision=3 col=9d",1);
 
-  fname = "Event.new.split9.root";
+  fname = "TreeFormulaReferences.new.split9.root";
   printf("\n\n\n=====================  %s  =================================\n\n\n",fname);
   TFile::Open(fname);
 #ifdef ClingWorkAroundMissingDynamicScope
