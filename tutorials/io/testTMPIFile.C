a00788f422452f3d2ddba1ae7b12b0a71f803c29
Marta Czurylo <mczurylo@cern.ch>
2025-03-26 16:21:50 +0100

    [tutorials] Organize free tutorials in the IO section.

--- tutorials/io/testTMPIFile.C
+++ tutorials/io/testTMPIFile.C
@@ -1,5 +1,6 @@
 /// \file
 /// \ingroup tutorial_io
+/// \notebook
 /// This macro shows the usage of TMPIFile to simulate event
 /// reconstruction and merging them in parallel.
 /// The JetEvent class is in $ROOTSYS/tutorials/io/tree/JetEvent.h,cxx
