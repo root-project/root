cb8c8935461c0e811d063b18ad2c78c9f7a40600
Jamie Gooding <74370983+GoodingJamie@users.noreply.github.com>
2025-03-26 08:30:36 +0000

    [tutorials] Add Python tutorial for gr004_errors_asym

---
+++ tutorials/visualisation/graphs/gr004_errors_asym.py
@@ -0,0 +1,28 @@
+## \file
+## \ingroup tutorial_graphs
+## \notebook
+## \preview This tutorial demonstrates the use of TGraphAsymmErrors to plot a graph with asymmetrical errors on both the x and y axes.
+## The errors for the x values are divided into low (left side of the marker) and high (right side of the marker) errors.
+## Similarly, for the y values, there are low (lower side of the marker) and high (upper side of the marker) errors.
+##
+## \macro_image
+## \macro_code
+## 
+## \author Miro Helbich, Jamie Gooding
+
+c2 = ROOT.TCanvas("c2","", 700, 500)
+
+c2.SetGrid()
+npoints=3
+xaxis = (1.,2.,3.)
+yaxis = (10.,20.,30.)
+
+exl = (0.5,0.2,0.1) # Lower x errors
+exh = (0.5,0.3,0.4) # Higher x errors
+eyl = (3.,5.,4.) # Lower y errors
+eyh = (3.,5.,4.) # Higher y errors
+
+gr = ROOT.TGraphAsymmErrors(npoints,xaxis,yaxis,exl,exh,eyl,eyh) # Create the TGraphAsymmErrors object with data and asymmetrical errors
+
+gr.SetTitle("A simple graph with asymmetrical errors")
+gr.Draw("A*") # "A" = draw axes and "*" = draw markers at the points with error bars
