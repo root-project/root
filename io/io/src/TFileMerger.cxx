2dab735c19a272e0ce769e8435b4cad99746d99c
ferdymercury <ferdymercury@users.noreply.github.com>
2025-03-27 13:04:20 +0100

    [tree] rename bit to avoid confusion with const TFile
    as suggested by pcanal

--- io/io/src/TFileMerger.cxx
+++ io/io/src/TFileMerger.cxx
@@ -978,7 +978,7 @@ Bool_t TFileMerger::PartialMerge(Int_t in_type)
    }
 
    fOutputFile->SetBit(kMustCleanup);
-   fOutputFile->SetBit(TFile::kCannotChange);
+   fOutputFile->SetBit(TFile::kCancelTTreeChangeRequest);
 
    TDirectory::TContext ctxt;
 
@@ -1040,7 +1040,7 @@ Bool_t TFileMerger::PartialMerge(Int_t in_type)
       Clear();
    } else {
       fOutputFile->ResetBit(kMustCleanup);
-      fOutputFile->ResetBit(TFile::kCannotChange);
+      fOutputFile->ResetBit(TFile::kCancelTTreeChangeRequest);
       SafeDelete(fOutputFile);
    }
    return result;
