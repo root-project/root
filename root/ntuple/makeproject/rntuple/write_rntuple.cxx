b4ce74d693ff48a945b5bbcc2c6cc9fa923db36d
silverweed <silverweed14@proton.me>
2025-03-28 09:31:22 +0100

    [ntuple] RNTupleWriter moved out of Experimental

--- root/ntuple/makeproject/rntuple/write_rntuple.cxx
+++ root/ntuple/makeproject/rntuple/write_rntuple.cxx
@@ -2,14 +2,13 @@
 #include <ROOT/RNTupleModel.hxx>
 #include <ROOT/RNTupleWriter.hxx>
 
-using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
-
 int main()
 {
    auto model = ROOT::RNTupleModel::Create();
    auto fldStlEvent = model->MakeField<MySTLEvent>("test");
 
-   auto ntuple = RNTupleWriter::Recreate(std::move(model), "events", "ntuple_makeproject_stl_example_rntuple.root");
+   auto ntuple =
+      ROOT::RNTupleWriter::Recreate(std::move(model), "events", "ntuple_makeproject_stl_example_rntuple.root");
    ntuple->Fill();
    return 0;
 }
