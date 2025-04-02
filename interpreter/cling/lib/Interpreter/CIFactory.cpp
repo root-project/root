170ac1a986694bdb1ff1f00914c4c043b5787b09
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-04-02 18:35:42 +0200

    [runtime-cxxmodules] Generate `std_darwin.modulemap` from active SDK
    Dynamically extract and wrap the system modulemap from the active
    SDK in a top-level 'std' module
    
    Also remove 'std_ctype_h' module to break cyclic dependencies.
    
    (cherry picked from commit dfc83fa305092260c5d2de3b67c9d3be435dbb18)

--- interpreter/cling/lib/Interpreter/CIFactory.cpp
+++ interpreter/cling/lib/Interpreter/CIFactory.cpp
@@ -696,18 +696,10 @@ namespace {
 #elif __APPLE__
     if (Triple.isMacOSX())
       if (CI.getTarget().getSDKVersion() >= VersionTuple(14, 4)) {
-        if (CI.getTarget().getSDKVersion() < VersionTuple(15, 4)) {
-          maybeAppendOverlayEntry(stdIncLoc.str(),
-                                  "std_darwin.MacOSX15.2.sdk.modulemap",
-                                  clingIncLoc.str().str(), MOverlay,
-                                  /*RegisterModuleMap=*/true,
-                                  /*AllowModulemapOverride=*/false);
-        } else {
-          maybeAppendOverlayEntry(stdIncLoc.str(), "std_darwin.modulemap",
-                                  clingIncLoc.str().str(), MOverlay,
-                                  /*RegisterModuleMap=*/true,
-                                  /*AllowModulemapOverride=*/false);
-        }
+        maybeAppendOverlayEntry(stdIncLoc.str(), "std_darwin.modulemap",
+                                clingIncLoc.str().str(), MOverlay,
+                                /*RegisterModuleMap=*/true,
+                                /*AllowModulemapOverride=*/false);
       }
 #else
     maybeAppendOverlayEntry(cIncLoc.str(), "libc.modulemap",
