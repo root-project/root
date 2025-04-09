c4cace9023cb23537a54c912df7b82fbfefa368f
ferdymercury <ferdymercury@users.noreply.github.com>
2025-04-09 09:57:28 +0200

    [RF] fix memleak in rooabscollection::find
    Fixes https://github.com/root-project/root/issues/18322

--- roofit/roofitcore/src/RooAbsCollection.cxx
+++ roofit/roofitcore/src/RooAbsCollection.cxx
@@ -929,6 +929,7 @@ RooAbsArg * RooAbsCollection::find(const char *name) const
 
   if (_hashAssistedFind || _list.size() >= _sizeThresholdForMapSearch) {
     if (!_hashAssistedFind || !_hashAssistedFind->isValid()) {
+      delete _hashAssistedFind; // needed for the case !_hashAssistedFind->isValid(), does not harm for the case of !_hashAssistedFind
       _hashAssistedFind = new HashAssistedFind{_list.begin(), _list.end()};
     }
 
