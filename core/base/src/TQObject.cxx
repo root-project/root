87d254fdf4f03272ca69472263f38880b73929cf
ferdymercury <ferdymercury@users.noreply.github.com>
2025-04-09 12:41:36 +0200

    [nfc] format

--- core/base/src/TQObject.cxx
+++ core/base/src/TQObject.cxx
@@ -314,11 +314,11 @@ private:
 public:
    TQConnectionList(const char *name, Int_t nsigargs) : TList()
       { fName = name; fSignalArgs = nsigargs; }
-      ~TQConnectionList() override;
+   ~TQConnectionList() override;
 
-      Bool_t Disconnect(void *receiver = nullptr, const char *slot_name = nullptr);
-      Int_t GetNargs() const { return fSignalArgs; }
-      void ls(Option_t *option = "") const override;
+   Bool_t Disconnect(void *receiver = nullptr, const char *slot_name = nullptr);
+   Int_t GetNargs() const { return fSignalArgs; }
+   void ls(Option_t *option = "") const override;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
