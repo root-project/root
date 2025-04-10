b6d21ed41a86f344f1d11b24351d34f5ce9bef54
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2025-04-10 11:22:14 +0200

    feat: add CastLike operator

--- tmva/sofie_parsers/src/ParseCast.cxx
+++ tmva/sofie_parsers/src/ParseCast.cxx
@@ -33,6 +33,33 @@ ParserFuncSignature ParseCast = [](RModelParser_ONNX &parser, const onnx::NodePr
    return op;
 };
 
+ParserFuncSignature ParseCastLike = [](RModelParser_ONNX &parser, const onnx::NodeProto &nodeproto) {
+   auto input_name = nodeproto.input(0);
+   auto target_type_tensor_name = nodeproto.input(1);
+   if (!parser.IsRegisteredTensorType(input_name)) {
+      throw std::runtime_error("TMVA::SOFIE ONNX Parser Cast op has input tensor" + input_name +
+                               "  but its type is not yet registered");
+   }
+   if (!parser.IsRegisteredTensorType(target_type_tensor_name)) {
+      throw std::runtime_error("TMVA::SOFIE ONNX Parser Cast op has target type tensor" + target_type_tensor_name +
+                               "  but its type is not yet registered");
+   }
+
+   std::unique_ptr<ROperator> op;
+   std::string target_type = parser.GetTensorType(target_type_tensor_name);
+   std::string output_name = nodeproto.output(0);
+   op.reset(new ROperator_Cast(target_type, input_name, output_name));
+
+   if (!parser.IsRegisteredTensorType(output_name)) {
+      ETensorType output_type = ConvertStringToType(attr_type);
+      parser.RegisterTensorType(output_name, output_type);
+   }
+
+   return op;
+};
+
+ 
+
 } // namespace SOFIE
 } // namespace Experimental
 } // namespace TMVA
