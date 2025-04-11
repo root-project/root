c897689691eddff57485e4698886ec6c9ece8454
TomasDado <38049043+TomasDado@users.noreply.github.com>
2025-04-11 20:25:03 +0200

    [RF] Fixing memory leak in RooAbsCollection::find
    Fixes memory leaks found by valgrind. Similar to #18327

--- roofit/roofitcore/src/RooAbsCollection.cxx
+++ roofit/roofitcore/src/RooAbsCollection.cxx
@@ -929,8 +929,7 @@ RooAbsArg * RooAbsCollection::find(const char *name) const
 
   if (_hashAssistedFind || _list.size() >= _sizeThresholdForMapSearch) {
     if (!_hashAssistedFind || !_hashAssistedFind->isValid()) {
-      if (_hashAssistedFind)
-         delete _hashAssistedFind;
+       delete _hashAssistedFind;
       _hashAssistedFind = new HashAssistedFind{_list.begin(), _list.end()};
     }
 
@@ -951,6 +950,7 @@ RooAbsArg * RooAbsCollection::find(const RooAbsArg& arg) const
 
   if (_hashAssistedFind || _list.size() >= _sizeThresholdForMapSearch) {
     if (!_hashAssistedFind || !_hashAssistedFind->isValid()) {
+       delete _hashAssistedFind;
       _hashAssistedFind = new HashAssistedFind{_list.begin(), _list.end()};
     }
 
