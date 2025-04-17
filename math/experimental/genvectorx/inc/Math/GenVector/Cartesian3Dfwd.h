5e583f4134bfc1cf9c53ed59023318870b349e64
mdessole <monicadessole@gmail.com>
2025-04-17 10:45:10 +0200

    [GenVectorX] Add source code in experimental

---
+++ math/experimental/genvectorx/inc/Math/GenVector/Cartesian3Dfwd.h
@@ -0,0 +1,21 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef ROOT_Math_GenVector_Cartesian3Dfwd
+#define ROOT_Math_GenVector_Cartesian3Dfwd  1
+
+#include "Math/GenVector/AccHeaders.h"
+
+namespace ROOT_MATH_ARCH {
+
+  namespace Math {
+
+template <class T>
+class Cartesian3D;
+
+  } // end namespace Math
+
+} // end namespace ROOT
+
+
+#endif /* ROOT_Math_GenVector_Cartesian3Dfwd  */
---
+++ math/experimental/genvectorx/inc/Math/LorentzRotation.h
@@ -0,0 +1,11 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef ROOT_Math_LorentzRotation
+#define ROOT_Math_LorentzRotation
+
+
+#include "Math/GenVector/LorentzRotation.h"
+
+
+#endif
