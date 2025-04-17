f53812ab97384eff39f0243c94798c2038eaed9d
mdessole <monicadessole@gmail.com>
2025-04-17 10:45:10 +0200

    [GenVectorX] Add source code in experimental

---
+++ math/experimental/genvectorx/inc/Math/GenVector/RotationZfwd.h
@@ -0,0 +1,22 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef ROOT_Math_GenVector_RotationZfwd
+#define ROOT_Math_GenVector_RotationZfwd 1
+
+#include "Math/GenVector/AccHeaders.h"
+
+namespace ROOT {
+
+namespace ROOT_MATH_ARCH {
+
+/**
+    Class describing a rotation about the Z axis
+ */
+
+class RotationZ;
+
+} // namespace ROOT_MATH_ARCH
+} // namespace ROOT
+
+#endif // ROOT_Math_GenVector_RotationZfwd
