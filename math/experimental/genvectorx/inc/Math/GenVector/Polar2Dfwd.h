1491ffa87f3b473daf7713f018e013f89a3367f1
mdessole <monicadessole@gmail.com>
2025-04-17 10:45:10 +0200

    [GenVectorX] Add source code in experimental

---
+++ math/experimental/genvectorx/inc/Math/GenVector/Polar2Dfwd.h
@@ -0,0 +1,24 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef ROOT_Math_GenVector_Polar2Dfwd
+#define ROOT_Math_GenVector_Polar2Dfwd 1
+
+#include "Math/GenVector/AccHeaders.h"
+
+namespace ROOT {
+
+namespace ROOT_MATH_ARCH {
+
+/**
+    Class describing a 2D Polar coordinate system
+(r, phi coordinates)
+*/
+
+template <class T>
+class Polar2D;
+
+} // namespace ROOT_MATH_ARCH
+} // namespace ROOT
+
+#endif
---
+++ math/experimental/genvectorx/inc/Math/GenVector/Polar3Dfwd.h
@@ -0,0 +1,24 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef ROOT_Math_GenVector_Polar3Dfwd
+#define ROOT_Math_GenVector_Polar3Dfwd 1
+
+#include "Math/GenVector/AccHeaders.h"
+
+namespace ROOT {
+
+namespace ROOT_MATH_ARCH {
+
+/**
+    Class describing a 3D Polar coordinate system
+    (r, theta, phi coordinates)
+ */
+
+template <class T>
+class Polar3D;
+
+} // namespace ROOT_MATH_ARCH
+} // namespace ROOT
+
+#endif
