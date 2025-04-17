9f4476d2b027eac12631881eba0a2857eccbfe43
mdessole <monicadessole@gmail.com>
2025-04-17 10:45:10 +0200

    [GenVectorX] Add source code in experimental

---
+++ math/experimental/genvectorx/inc/Math/GenVector/PositionVector2Dfwd.h
@@ -0,0 +1,23 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef ROOT_Math_GenVector_PositionVector2Dfwd
+#define ROOT_Math_GenVector_PositionVector2Dfwd 1
+
+#include "Math/GenVector/AccHeaders.h"
+
+namespace ROOT {
+
+namespace ROOT_MATH_ARCH {
+
+/**
+    Class describing a 2D Position vector
+*/
+
+template <class CoordSystem, class Tag>
+class PositionVector2D;
+
+} // namespace ROOT_MATH_ARCH
+} // namespace ROOT
+
+#endif
