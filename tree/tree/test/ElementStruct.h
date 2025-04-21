91dbbf3470842b8e361bc84b2c6692980de2d2a1
ferdymercury <ferdymercury@users.noreply.github.com>
2025-04-21 11:40:04 +0200

    [test] fix dictionary generation

--- tree/tree/test/ElementStruct.h
+++ tree/tree/test/ElementStruct.h
@@ -1,3 +1,5 @@
+#ifndef ELEMENTSTRUCT_H
+#define ELEMENTSTRUCT_H
 /**
  * The ElementStruct has no purpose except to provide
  * inputs to the test cases.
@@ -8,3 +10,4 @@ public:
    int    i;
    double *d; //[i]
 };
+#endif
