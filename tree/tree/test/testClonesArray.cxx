2d728f785e43f77f94410c9d7f2a88377964f1cf
ferdymercury <ferdymercury@users.noreply.github.com>
2025-04-21 11:40:04 +0200

    [test] fix dictionary generation

--- tree/tree/test/testClonesArray.cxx
+++ tree/tree/test/testClonesArray.cxx
@@ -1,6 +1,5 @@
 #include "TObject.h"
-#include "TRef.h"
-#include "TRefArray.h"
+#include "TestRefObj.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TRandom.h"
@@ -11,19 +10,6 @@
 #include "gtest/gtest.h"
 
 // https://its.cern.ch/jira/browse/ROOT-7249
-class TestRefObj : public TObject {
-protected:
-   TRefArray lChildren;
-
-public:
-   TestRefObj() : lChildren() {}
-   virtual ~TestRefObj() {}
-   virtual void Clear(Option_t *opt = "C") { lChildren.Clear(opt); }
-   virtual void SetChild(TestRefObj *aChild) { lChildren.Add(aChild); }
-   virtual const TestRefObj *GetChild(Int_t idx = 0) const { return static_cast<TestRefObj *>(lChildren.At(idx)); }
-   virtual Bool_t HasChild() const { return (lChildren.GetEntriesFast() > 0); }
-   ClassDef(TestRefObj, 1);
-};
 TEST(TClonesArray, RefArrayClearChildren)
 {
    Bool_t resetObjectCount = kTRUE;
