5506f40b22c40721ac8a06393dcaa3b94d3d5f38
Philippe Canal <pcanal@fnal.gov>
2025-04-22 13:32:50 -0500

    REVERTME [coverage] only test RNtuple for now

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -318,7 +318,7 @@ def run_ctest(extra_ctest_flags: str) -> int:
     builddir = os.path.join(WORKDIR, "build")
     ctest_result = subprocess_with_log(f"""
         cd '{builddir}'
-        ctest --output-on-failure --parallel {os.cpu_count()} --output-junit TestResults.xml {extra_ctest_flags}
+        ctest -R ntuple -E tutorial-io-tree-ntuple1-py\|large\|limits --output-on-failure --parallel {os.cpu_count()} --output-junit TestResults.xml {extra_ctest_flags}
     """)
 
     return ctest_result
