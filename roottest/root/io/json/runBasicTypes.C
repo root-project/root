26549ad9ca4b2136acff8beaa71a24e90f4db0a3
Sergey Linev <S.Linev@gsi.de>
2025-04-23 15:54:31 +0200

    [json] improve roottest/io/json testing
    1. Generate dictionary for custom tested classes
    2. Load this library when running test macros
    3. Avoid multiple cling workarounds
    4. All ref files remains as is

--- roottest/root/io/json/runBasicTypes.C
+++ roottest/root/io/json/runBasicTypes.C
@@ -1,13 +1,8 @@
-{
-// Fill out the code of the actual test
-#ifndef SECOND_RUN
-   gROOT->ProcessLine(".L test_classes.h+");
-#endif
+#include "test_classes.h"
 
-#if defined(ClingWorkAroundMissingDynamicScope) && !defined(SECOND_RUN)
-#define SECOND_RUN
-   gROOT->ProcessLine(".x runBasicTypes.C");
-#else
+void runBasicTypes()
+{
+   gSystem->Load("libJsonTestClasses");
 
    TJsonEx1 ex1; ex1.Init();
    TJsonEx2 ex2; ex2.Init();
@@ -15,31 +10,24 @@
    TJsonEx11 ex11; ex11.Init();
    TString json;
 
-   cout << " ====== basic data types TJsonEx1 ===== " << endl;
+   std::cout << " ====== basic data types TJsonEx1 ===== " << std::endl;
    json = TBufferJSON::ToJSON(&ex1);
    testJsonReading(json);
-   cout << json << endl << endl;
-   cout << " ====== inheritance from TJsonEx1, TJsonEx11 ===== " << endl;
+   std::cout << json << std::endl << std::endl;
+   std::cout << " ====== inheritance from TJsonEx1, TJsonEx11 ===== " << std::endl;
    json = TBufferJSON::ToJSON(&ex11);
    testJsonReading(json);
-   cout << json << endl << endl;
-   cout << " ====== arrays of basic data types TJsonEx2 ===== " << endl;
+   std::cout << json << std::endl << std::endl;
+   std::cout << " ====== arrays of basic data types TJsonEx2 ===== " << std::endl;
    json = TBufferJSON::ToJSON(&ex2);
    testJsonReading(json);
-   cout << json << endl << endl;
-   cout << " ====== dynamic arrays of basic data types TJsonEx3 ===== " << endl;
+   std::cout << json << std::endl << std::endl;
+   std::cout << " ====== dynamic arrays of basic data types TJsonEx3 ===== " << std::endl;
    json = TBufferJSON::ToJSON(&ex3);
    testJsonReading(json);
-   cout << json << endl << endl;
-   cout << " ============ selected data members ======== " << endl;
-   cout << "ex2.fInt = " << TBufferJSON::ToJSON(&ex2, 0, "fInt") << endl;
-   cout << "ex3.fChar = " << TBufferJSON::ToJSON(&ex3, 0, "fChar") << endl;
-   cout << "ex3.fLong = " << TBufferJSON::ToJSON(&ex3, 0, "fLong") << endl;
-
-#endif
-#ifdef ClingWorkAroundBrokenUnnamedReturn
-   gApplication->Terminate(0);
-#else
-   return 0;
-#endif
+   std::cout << json << std::endl << std::endl;
+   std::cout << " ============ selected data members ======== " << std::endl;
+   std::cout << "ex2.fInt = " << TBufferJSON::ToJSON(&ex2, 0, "fInt") << std::endl;
+   std::cout << "ex3.fChar = " << TBufferJSON::ToJSON(&ex3, 0, "fChar") << std::endl;
+   std::cout << "ex3.fLong = " << TBufferJSON::ToJSON(&ex3, 0, "fLong") << std::endl;
 }
