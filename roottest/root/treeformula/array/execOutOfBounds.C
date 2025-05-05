3526a25ab805e7198c7efc283309ca88896a2a54
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-05 19:51:38 +0200

    [test] Improve test explicit check for NaN
    Moreover:
     - Adapt to the new functionality
     - Get entry before evalInstance
     - Use explicit value and entry
     - Void index 0 which is special def case fNData==1

--- roottest/root/treeformula/array/execOutOfBounds.C
+++ roottest/root/treeformula/array/execOutOfBounds.C
@@ -4,11 +4,20 @@ int execOutOfBounds() {
 {
 #endif
   TFile *_file0 = TFile::Open("run133.root");
-  TTree *t; _file0->GetObject("t",t);
+  auto t = _file0->Get<TTree>("t");
+  // ThetaDeg is a dynamic array with indices going from 0 to (ringtouche_DE1 - 1)
+  TTreeFormula tf("tf", "ThetaDeg", t); 
+  t->GetEntry(0); // ringtouche for entry 0 is 0
+  auto res = tf.EvalInstance(1); // ThetaDeg[1] goes out of bonds
+  if (!TMath::IsNaN(res)) {
+     printf("Error: evaluated instance is %f rather than NaN\n", res);
+     return 1;
+  }
   TH2F *h2 = new TH2F("h2","h2",5,0,5,100,0,100);
   t->Draw("ThetaDeg[ringtouche_DE1]:ringtouche_DE1>>h2","","colz goff");
-  if ( (int)(h2->GetMean(1)*1000) != 3) {
-     printf("Error: x mean is %f rather than 0.003\n",h2->GetMean(1));
+  // Filling NaNs increases entry number but does not change any bin content
+  if ( (int)(h2->GetMean(1)*1000) != 0) {
+     printf("Error: x mean is %f rather than 0.000\n",h2->GetMean(1));
      return 1;
   } 
   if ( (int)(h2->GetMean(2)*10) != 0) {
