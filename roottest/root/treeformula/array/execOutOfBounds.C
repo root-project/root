25e22a383779ced7e823b5499212c88a6a798201
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-07 17:45:16 +0200

    [treeformula] improve test explicit check for NaN

--- roottest/root/treeformula/array/execOutOfBounds.C
+++ roottest/root/treeformula/array/execOutOfBounds.C
@@ -4,9 +4,17 @@ int execOutOfBounds() {
 {
 #endif
   TFile *_file0 = TFile::Open("run133.root");
-  TTree *t; _file0->GetObject("t",t);
+  auto t = _file0->Get<TTree>("t");
+  // ThetaDeg is a dynamic array with indices going from 0 to (ringtouche_DE1 - 1)
+  TTreeFormula tf("tf", "ThetaDeg[ringtouche_DE1]", t); 
+  auto res = tf.EvalInstance();
+  if (!TMath::IsNaN(res)) {
+     printf("Error: evaluated instance is %f rather than NaN\n", res);
+     return 1;
+  }
   TH2F *h2 = new TH2F("h2","h2",5,0,5,100,0,100);
   t->Draw("ThetaDeg[ringtouche_DE1]:ringtouche_DE1>>h2","","colz goff");
+  // Filling NaNs increases entry number but does not change any bin content
   if ( (int)(h2->GetMean(1)*1000) != 0) {
      printf("Error: x mean is %f rather than 0.000\n",h2->GetMean(1));
      return 1;
