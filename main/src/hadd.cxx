d3e090ecbd0f339c2825c2e47eccb718d809cf93
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-09 19:24:33 +0200

    [nfc] fix typo

--- main/src/hadd.cxx
+++ main/src/hadd.cxx
@@ -45,7 +45,7 @@
   \param -experimental-io-features <FEATURES> Enables the corresponding experimental feature for output trees.
                                               \see ROOT::Experimental::EIOFeatures
   \param -f            Force overwriting of output file.
-  \param -f[0-9]       Set target compression algorithm `i` and level `j` passing the number `i*100 + j`, e.g. `-f505.`
+  \param -f[0-9]       Set target compression algorithm `i` and level `j` passing the number `i*100 + j`, e.g. `-f505`.
                        The last digit (`j`) can be set from 0 = uncompressed to 9 = highly compressed.
                        The first digit (`i`) is 1 for ZLIB, 2 for LZMA, 4 for LZ4 and 5 for ZSTD.
                        Recommended flags are 101 (ZLIB), 207 (LZMA), 404 (LZ4), 505 (ZSTD),
