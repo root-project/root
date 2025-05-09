e8788eca0442aa6485519af3492c983ed4d42d9c
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-09 19:33:56 +0200

    [nfc] reword

--- main/src/hadd-argparse.py
+++ main/src/hadd-argparse.py
@@ -28,7 +28,7 @@ def get_argparse():
         "by passing the number i*100 + j, e.g. -f505. "
         "The last digit (j) can be set from 0 = uncompressed to 9 = highly compressed. "
         "The first digit (i) is 1 for ZLIB, 2 for LZMA, 4 for LZ4 and 5 for ZSTD. "
-        "Recommended flags are 101 (ZLIB), 207 (LZMA), 404 (LZ4), 505 (ZSTD). "
+        "Recommended numbers are 101 (ZLIB), 207 (LZMA), 404 (LZ4), 505 (ZSTD). "
         "The default value for this flag is 101 (kDefaultZLIB). "
         "See ROOT::RCompressionSetting and TFile::TFile documentation for more details."), action = 'store_true')
     parser.add_argument("-fk", help=textwrap.fill(
