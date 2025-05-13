c1ec7defa28094080c2817c9c4443f06236f94c3
Sergey Linev <S.Linev@gsi.de>
2025-05-13 16:18:14 +0200

    [roottest] use cmake for io/abstractclasses
    Build dictionary first
    Use ROOT files from v3 and v4 to check reading functionality
    Remove data.root while it is identical with data_v3_05.root
    Add -Wno-delete-non-virtual-dtor to avoid warning

--- roottest/root/io/abstractclass/read_test.C
+++ roottest/root/io/abstractclass/read_test.C
@@ -1,26 +1,17 @@
+#include "DataBlock1.h"
+#include "DataBlock2.h"
+
+void read_test(const char *fname = "data.root")
 {
-gROOT->Reset();
+   gSystem->Load("libAbstractClasses");
 
-gSystem->Load("libData");
-
-#ifdef ClingWorkAroundMissingDynamicScope
-gROOT->ProcessLine(
-                   // create and initialize file
-"TFile *hfile = TFile::Open(\"data.root\",\"READ\");"
-"DataBlock1 *db1 = (DataBlock1 *) hfile->Get(\"DataBlock1\");"
-"DataBlock2 *db2 = (DataBlock2 *) hfile->Get(\"DataBlock2\");"
-"db1->Print();"
-"db2->Print();"
-);
-#else
    // create and initialize file
-   TFile *hfile = TFile::Open("data.root","READ");
-   
-   DataBlock1 *db1 = (DataBlock1 *) hfile->Get("DataBlock1");
-DataBlock2 *db2 = (DataBlock2 *) hfile->Get("DataBlock2");
+   auto hfile = TFile::Open(fname,"READ");
 
-db1->Print();
-db2->Print();
-#endif
+   DataBlock1 *db1 = (DataBlock1 *) hfile->Get("DataBlock1");
+   DataBlock2 *db2 = (DataBlock2 *) hfile->Get("DataBlock2");
+
+   db1->Print();
+   db2->Print();
 
 }
