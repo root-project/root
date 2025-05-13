8ac35567cfc58bd87b1dc7f360e27c99d6adea83
Sergey Linev <S.Linev@gsi.de>
2025-05-13 14:07:35 +0200

    [roottest] use cmake for io/treeproblem
    Using FIXTURES_SETUP to configure dependencies

--- roottest/root/io/treeproblem/reader.C
+++ roottest/root/io/treeproblem/reader.C
@@ -1,56 +1,30 @@
-#ifndef ROOT_TFile
 #include "TFile.h"
-#endif
-#ifndef ROOT_TTree
 #include "TTree.h"
-#endif
-#ifndef ROOT_TClonesArray
 #include "TClonesArray.h"
-#endif
-#ifndef ROOT_TROOT
-#include "TROOT.h"
-#endif
-#ifndef __IOSTREAM__
+
 #include <iostream>
-#endif
-#ifndef ROOT_TSystem
-#include "TSystem.h"
-#endif
 
-using namespace std;
-
-int reader() 
+int reader()
 {
-#ifndef ClingWorkAroundMissingDynamicScope
-  gSystem->Load("Foo");
-#endif
-
-//  gDebug = 2; 
+  gSystem->Load("libTreeProblemFoo");
 
   Int_t  foo_     = 0;
   Int_t  foo_fFoo[3];
-  TFile* file     = TFile::Open("file.root", "READ"); 
-  TTree* tree     = (TTree*)file->Get("tree");
+  TFile* file     = TFile::Open("file.root", "READ");
+  TTree* tree     = (TTree *) file->Get("tree");
   tree->SetMakeClass(1);
   tree->SetBranchAddress("foo", (void*)&foo_);
   tree->SetBranchAddress("foo.fFoo", &foo_fFoo);
-  
+
   Int_t  n = (Int_t)tree->GetEntries();
 
   for (Int_t i = 0; i < n; i++) {
     tree->GetEntry(i);
-    for (Int_t j = 0; j < foo_; j++) 
-      cout << "Foo # " << i << "," << j << ": " << foo_fFoo[j] << endl;
+    for (Int_t j = 0; j < foo_; j++)
+      std::cout << "Foo # " << i << "," << j << ": " << foo_fFoo[j] << std::endl;
   }
   file->Close();
-  
+
   return 0;
 }
-
-#ifndef __CLING__
-int main(int argc, char** argv) 
-{
-  return reader();
-}
-#endif
 
--- roottest/root/io/treeproblem/writer.C
+++ roottest/root/io/treeproblem/writer.C
@@ -1,51 +1,29 @@
-#ifndef ROOT_TFile
 #include "TFile.h"
-#endif
-#ifndef ROOT_TTree
 #include "TTree.h"
-#endif
-#ifndef ROOT_TClonesArray
 #include "TClonesArray.h"
-#endif
-#ifndef TREEPROBLEM_Foo
 #include "Foo.h"
-#endif
-#ifndef ROOT_TROOT
-#include "TROOT.h"
-#endif
-#ifndef __IOSTREAM__
-#include <iostream>
-#endif
 
 int writer()
 {
-#if defined(__CLING__) && !defined(ClingWorkAroundMissingDynamicScope) 
-  gSystem->Load("Foo");
-#endif
+  gSystem->Load("libTreeProblemFoo");
 
   TClonesArray* array = new TClonesArray("Foo");
-  TFile* file         = TFile::Open("file.root", "RECREATE"); 
+  TFile* file         = TFile::Open("file.root", "RECREATE");
   TTree* tree         = new TTree("tree", "tree");
   tree->Branch("foo", &array);
-  
+
   Int_t  n = 100;
   for (Int_t i = 0; i < n; i++) {
-    array->Clear(); 
-    for (Int_t j = 0; j < 3; j++) 
+    array->Clear();
+    for (Int_t j = 0; j < 3; j++)
       new ((*array)[j]) Foo(i * 10 + j);
     tree->Fill();
   }
-  
+
   tree->Print();
   file->Write();
   file->Close();
-  
+
   return 0;
 }
 
-#ifndef __CLING__
-int main(int argc, char** argv)
-{
-  return writer();
-}
-#endif
