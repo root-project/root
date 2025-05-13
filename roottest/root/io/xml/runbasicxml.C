ef987352e5b6eba222baa7af8db4473e76561369
Sergey Linev <S.Linev@gsi.de>
2025-05-13 11:04:54 +0200

    [roottest] modernize io-xml tests
    Use nullptr, auto variables, std::cout and std::endl

--- roottest/root/io/xml/runbasicxml.C
+++ roottest/root/io/xml/runbasicxml.C
@@ -6,101 +6,93 @@ void runbasicxml()
 
    TString sbuf;
 
-   cout << endl << "Doing TXmlEx1 - basic data types" << endl;
+   std::cout << std::endl << "Doing TXmlEx1 - basic data types" << std::endl;
 
-   TXmlEx1* ex1 = new TXmlEx1;
-   sbuf = TBufferXML::ConvertToXML(ex1, gROOT->GetClass("TXmlEx1"));
-   delete ex1;
-   ex1 = (TXmlEx1*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex1==0)
-      cout << "TXmlEx1 cannot be read from xml" << endl;
+   TXmlEx1 iex1;
+   sbuf = TBufferXML::ConvertToXML(&iex1, gROOT->GetClass("TXmlEx1"));
+   auto ex1 = (TXmlEx1 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex1)
+      std::cout << "TXmlEx1 cannot be read from xml" << std::endl;
    else
       ex1->Print();
    delete ex1;
 
-   cout << endl << "Doing TXmlEx2 - fixed-size arrays of basic types" << endl;
+   std::cout << std::endl << "Doing TXmlEx2 - fixed-size arrays of basic types" << std::endl;
 
-   TXmlEx2* ex2 = new TXmlEx2;
-   sbuf = TBufferXML::ConvertToXML(ex2, gROOT->GetClass("TXmlEx2"));
-   delete ex2;
-   ex2 = (TXmlEx2*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex2==0)
-      cout << "TXmlEx2 cannot be read from xml" << endl;
+   TXmlEx2 iex2;
+   sbuf = TBufferXML::ConvertToXML(&iex2, gROOT->GetClass("TXmlEx2"));
+   auto ex2 = (TXmlEx2 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex2)
+      std::cout << "TXmlEx2 cannot be read from xml" << std::endl;
    else
       ex2->Print();
    delete ex2;
 
-   cout << endl << "Doing TXmlEx3 - dynamic arrays of basic types" << endl;
+   std::cout << std::endl << "Doing TXmlEx3 - dynamic arrays of basic types" << std::endl;
 
-   TXmlEx3* ex3 = new TXmlEx3;
-   sbuf = TBufferXML::ConvertToXML(ex3, gROOT->GetClass("TXmlEx3"));
-   delete ex3;
-   ex3 = (TXmlEx3*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex3==0)
-      cout << "TXmlEx3 cannot be read from xml" << endl;
+   TXmlEx3 iex3;
+   sbuf = TBufferXML::ConvertToXML(&iex3, gROOT->GetClass("TXmlEx3"));
+   auto ex3 = (TXmlEx3 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex3)
+      std::cout << "TXmlEx3 cannot be read from xml" << std::endl;
    else
       ex3->Print();
    delete ex3;
 
-   cout << endl << "Doing TXmlEx4 - inheritance and different strings" << endl;
+   std::cout << std::endl << "Doing TXmlEx4 - inheritance and different strings" << std::endl;
 
-   TXmlEx4* ex4 = new TXmlEx4(true);
-   sbuf = TBufferXML::ConvertToXML(ex4, gROOT->GetClass("TXmlEx4"));
-   delete ex4;
-   ex4 = (TXmlEx4*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex4==0)
-      cout << "TXmlEx4 cannot be read from xml" << endl;
+   TXmlEx4 iex4(true);
+   sbuf = TBufferXML::ConvertToXML(&iex4, gROOT->GetClass("TXmlEx4"));
+   auto ex4 = (TXmlEx4 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex4)
+      std::cout << "TXmlEx4 cannot be read from xml" << std::endl;
    else
       ex4->Print();
    delete ex4;
 
-   cout << endl << "Doing TXmlEx5 - objects as data memebers" << endl;
+   std::cout << std::endl << "Doing TXmlEx5 - objects as data memebers" << std::endl;
 
-   TXmlEx5* ex5 = new TXmlEx5(true);
-   sbuf = TBufferXML::ConvertToXML(ex5, gROOT->GetClass("TXmlEx5"));
-   delete ex5;
-   ex5 = (TXmlEx5*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex5==0)
-      cout << "TXmlEx5 cannot be read from xml" << endl;
+   TXmlEx5 iex5(true);
+   sbuf = TBufferXML::ConvertToXML(&iex5, gROOT->GetClass("TXmlEx5"));
+   auto ex5 = (TXmlEx5 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex5)
+      std::cout << "TXmlEx5 cannot be read from xml" << std::endl;
    else
       ex5->Print();
    delete ex5;
 
-   cout << endl << "Doing TXmlEx6 - arrays of objects as data memebers" << endl;
+   std::cout << std::endl << "Doing TXmlEx6 - arrays of objects as data memebers" << std::endl;
 
-   TXmlEx6* ex6 = new TXmlEx6(true);
-   sbuf = TBufferXML::ConvertToXML(ex6, gROOT->GetClass("TXmlEx6"));
-   delete ex6;
-   ex6 = (TXmlEx6*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex6==0)
-      cout << "TXmlEx6 cannot be read from xml" << endl;
+   TXmlEx6 iex6(true);
+   sbuf = TBufferXML::ConvertToXML(&iex6, gROOT->GetClass("TXmlEx6"));
+   auto ex6 = (TXmlEx6 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex6)
+      std::cout << "TXmlEx6 cannot be read from xml" << std::endl;
    else
       ex6->Print();
    delete ex6;
 
-   cout << endl << "Doing TXmlEx7 - all kinds of STL containers" << endl;
+   std::cout << std::endl << "Doing TXmlEx7 - all kinds of STL containers" << std::endl;
 
-   TXmlEx7* ex7 = new TXmlEx7(true);
-   sbuf = TBufferXML::ConvertToXML(ex7, gROOT->GetClass("TXmlEx7"));
-   delete ex7;
-   ex7 = (TXmlEx7*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex7==0)
-      cout << "TXmlEx7 cannot be read from xml" << endl;
+   TXmlEx7 iex7(true);
+   sbuf = TBufferXML::ConvertToXML(&iex7, gROOT->GetClass("TXmlEx7"));
+   auto ex7 = (TXmlEx7*) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex7)
+      std::cout << "TXmlEx7 cannot be read from xml" << std::endl;
    else
       ex7->Print();
    delete ex7;
 
-   cout << endl << "Doing TXmlEx8 - inheritance from std::vector<int>" << endl;
+   std::cout << std::endl << "Doing TXmlEx8 - inheritance from std::vector<int>" << std::endl;
 
-   TXmlEx8* ex8 = new TXmlEx8(true);
-   sbuf = TBufferXML::ConvertToXML(ex8, gROOT->GetClass("TXmlEx8"));
-   delete ex8;
-   ex8 = (TXmlEx8*) TBufferXML::ConvertFromXMLAny(sbuf);
-   if (ex8==0)
-      cout << "TXmlEx8 cannot be read from xml" << endl;
+   TXmlEx8 iex8(true);
+   sbuf = TBufferXML::ConvertToXML(&iex8, gROOT->GetClass("TXmlEx8"));
+   auto ex8 = (TXmlEx8 *) TBufferXML::ConvertFromXMLAny(sbuf);
+   if (!ex8)
+      std::cout << "TXmlEx8 cannot be read from xml" << std::endl;
    else
       ex8->Print();
    delete ex8;
 
-   cout << "Done " << endl;
+   std::cout << "Done " << std::endl;
 }
