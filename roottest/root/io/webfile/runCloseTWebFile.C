cbe2733cdcd30a33355612b3098d3071dbeac0e3
Sergey Linev <S.Linev@gsi.de>
2025-05-13 13:27:32 +0200

    [roottest] use cmake for CloseTWebFile test

--- roottest/root/io/webfile/runCloseTWebFile.C
+++ roottest/root/io/webfile/runCloseTWebFile.C
@@ -1,18 +1,18 @@
 #include "TFile.h"
-#include "Riostream.h"
+#include <iostream>
 
 void runCloseTWebFile()
 {
-   // Make sure there is no crash when quitting Root after closing a TWebFile 
+   // Make sure there is no crash when quitting Root after closing a TWebFile
 
-   TFile *f = TFile::Open("http://root.cern.ch/files/na49.root");
+   auto f = TFile::Open("https://root.cern/files/na49.root");
    if (f) {
       f->Close();
-      // deleting the file was solving the problem, but not deleting the file 
+      // deleting the file was solving the problem, but not deleting the file
       // should not lead to a segfault when quitting Root anyway...
       // delete f;
    }
    else {
-      cout << "failed to open http://root.cern.ch/files/na49.root" << endl;
+      std::cerr << "failed to open https://root.cern/files/na49.root" << std::endl;
    }
 }
