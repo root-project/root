36a21c4db67aef7c2f5bbf1f5036099c6569226f
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-14 20:30:34 +0200

    [math] fix compile error and clangformat

--- math/genvector/inc/Math/GenVector/LorentzVector.h
+++ math/genvector/inc/Math/GenVector/LorentzVector.h
@@ -370,10 +370,11 @@ ROOT provides specialisations and aliases to them of the ROOT::Math::LorentzVect
           \f[ \Delta R = - \sqrt { \Delta \eta ^2 - \Delta phi ^2 } \f]
           \param useRapidity true to use Rapidity(), false to use Eta()
        */
-       Scalar DeltaR(const LorentzVector<CoordSystem>& v, const Bool_t useRapidity = false) const {
+       Scalar DeltaR(const LorentzVector &v, const Bool_t useRapidity = false) const
+       {
           const Double_t delta = useRapidity ? Rapidity() - v.Rapidity() : Eta() - v.Eta();
           const Double_t dphi = VectorUtil::Phi_mpi_pi(Phi() - v.Phi());
-          return std::sqrt(delta* delta + dphi * dphi);
+          return std::sqrt(delta * delta + dphi * dphi);
        }
 
        /**
