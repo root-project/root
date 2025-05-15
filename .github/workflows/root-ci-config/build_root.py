63cb7e094d287068dda4a12cb1dad75e13fdf641
Philippe Canal <pcanal@fnal.gov>
2025-05-15 15:30:41 -0500

    [coverage] Ignore missing source file (eg. ACLiC)

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -503,7 +503,7 @@ def create_coverage_xml() -> None:
     builddir = os.path.join(WORKDIR, "build")
     ignore_directories="runtutorials|interpreter|.*-prefix|bindings/pyroot/cppyy"
     ignore_subpattern="runtutorials|externals|ginclude|googletest-prefix|macosx|winnt|geombuilder|cocoa|quartz|win32gdk|x11|x11ttf|eve|fitpanel|ged|gui|guibuilder|guihtml|qtgsi|qtroot|recorder|sessionviewer|tmvagui|treeviewer|geocad|fitsio|gviz|qt|gviz3d|x3d|spectrum|spectrumpainter|dcache|hdfs|foam|genetic|mlp|quadp|splot|memstat|rpdutils|proof|odbc|llvm|test|interpreter"
-    ignore_errors="--gcov-ignore-errors=no_working_dir_found"
+    ignore_errors="--gcov-ignore-errors=source_not_found --gcov-ignore-errors=no_working_dir_found"
     # The output of -v is huge (several 10s of MB at least), we could filter
     # the output of -v to keep just the line with ` Processing file:`
     result = subprocess_with_log(f"""
