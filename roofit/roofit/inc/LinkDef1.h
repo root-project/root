8a579992e594d6a34c1e9e1874120a847fe4ae52
GalinBistrev2 <gbistrev@uni-sofia.bg>
2025-05-20 16:21:19 +0300

    [RF] Integrate the RooMultiPdf class from CMS combine

--- roofit/roofit/inc/LinkDef1.h
+++ roofit/roofit/inc/LinkDef1.h
@@ -3,6 +3,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class Roo2DKeysPdf+ ;
+#pragma link C++ class RooMultiPdf + ;
 #pragma link C++ class RooArgusBG+ ;
 #pragma link C++ class RooBCPEffDecay+ ;
 #pragma link C++ class RooBCPGenDecay+ ;
