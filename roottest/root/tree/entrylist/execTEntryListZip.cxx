943050003f8fc9fdea88c29c5d746d8e28d71424
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-23 14:55:57 +0200

    [nfc] clangformat

--- roottest/root/tree/entrylist/execTEntryListZip.cxx
+++ roottest/root/tree/entrylist/execTEntryListZip.cxx
@@ -7,18 +7,18 @@
 
 int execTEntryListZip()
 {
-    TChain chain("T");
-    chain.Add("cernstaff.zip#cernstaff.root");
-    chain.Draw(">> elist0", "Age > 40", "entrylist");
+   TChain chain("T");
+   chain.Add("cernstaff.zip#cernstaff.root");
+   chain.Draw(">> elist0", "Age > 40", "entrylist");
 
-    // gDirectory->ls();
-    auto el = static_cast<TEntryList*>(gDirectory->Get("elist0"));
-    if (!el)
-       return 2;
-    // el->Print("V");
+   // gDirectory->ls();
+   auto el = static_cast<TEntryList*>(gDirectory->Get("elist0"));
+   if (!el)
+      return 2;
+   // el->Print("V");
 
-    chain.SetEntryList(nullptr);
-    chain.SetEntryList(el, "");
+   chain.SetEntryList(nullptr);
+   chain.SetEntryList(el, "");
 
-    return (el == chain.GetEntryList()) ? 0 : 1;
+   return (el == chain.GetEntryList()) ? 0 : 1;
 }
