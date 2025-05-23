bd0d1aa085bb0dcdd68afbb1185166ffce6daa34
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2025-05-23 14:44:43 +0200

    cosmetics

--- tutorials/visualisation/webgui/browserv7/browser.cxx
+++ tutorials/visualisation/webgui/browserv7/browser.cxx
@@ -15,7 +15,7 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
- // macro must be here to let macro work on Windows
+// macro must be here to let macro work on Windows
 R__LOAD_LIBRARY(libROOTBrowserv7)
 
 #include <ROOT/RBrowser.hxx>
