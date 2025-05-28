955cc8441ab683f8a8a331865ed1090a847efc01
Martin Foll <martin.foll@cern.ch.>
2025-05-28 16:54:15 +0200

    Update RBatchGenerator tutorials

--- tutorials/machine_learning/RBatchGenerator_NumPy.py
+++ tutorials/machine_learning/RBatchGenerator_NumPy.py
@@ -14,24 +14,31 @@ tree_name = "sig_tree"
 file_name = str(ROOT.gROOT.GetTutorialDir()) + "/machine_learning/data/Higgs_data.root"
 
 batch_size = 128
-chunk_size = 5_000
+chunk_size = 5000
+block_size = 400
 
 rdataframe = ROOT.RDataFrame(tree_name, file_name)
 
+target = "Type"
+
+num_of_epochs = 2
+
 gen_train, gen_validation = ROOT.TMVA.Experimental.CreateNumPyGenerators(
     rdataframe,
-    batch_size,
+    batch_size,    
     chunk_size,
-    validation_split=0.3,
-    shuffle=True,
-    drop_remainder=False
+    block_size,        
+    target = target,
+    validation_split = 0.3,
+    shuffle = True,
+    drop_remainder = True
 )
 
-# Loop through training set
-for i, b in enumerate(gen_train):
-    print(f"Training batch {i} => {b.shape}")
+for i in range(num_of_epochs):
+    # Loop through training set
+    for i, (x_train, y_train) in enumerate(gen_train):
+        print(f"Training batch {i + 1} => x: {x_train.shape}, y: {y_train.shape}")
 
-
-# Loop through Validation set
-for i, b in enumerate(gen_validation):
-    print(f"Validation batch {i} => {b.shape}")
+    # Loop through Validation set
+    for i, (x_validation, y_validation) in enumerate(gen_validation):
+        print(f"Validation batch {i + 1} => x: {x_validation.shape}, y: {y_validation.shape}")
