5331f3385eca85cc35af1bef67fc9c1848893cfd
Danilo Piparo <danilo.piparo@cern.ch>
2025-05-29 22:21:15 +0200

    [core][nfc] Remind not to add leading 0s to release version numbers

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -3,8 +3,8 @@
 
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
-#define ROOT_VERSION_MINOR 36
-#define ROOT_VERSION_PATCH 1
+#define ROOT_VERSION_MINOR 36 
+#define ROOT_VERSION_PATCH 1 // When changing the version number here, never add leading zeroes!
 #define ROOT_RELEASE_DATE "May 26 2025"
 
 /* Don't change the lines below. */
