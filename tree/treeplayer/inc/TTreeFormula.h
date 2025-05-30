97bf2e8ec918a927e2c4f1e2aadba12579ec5c01
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-30 15:44:15 +0200

    [tree] match nondef arg, clangf and English varname

--- tree/treeplayer/inc/TTreeFormula.h
+++ tree/treeplayer/inc/TTreeFormula.h
@@ -161,7 +161,7 @@ protected:
    bool      StringToNumber(Int_t code) override;
 
    void              Convert(UInt_t fromVersion) override;
-   Bool_t            AnalyzePrimitive(TString &chaine, TObjArray& args, Int_t &err, Int_t offset=0) override;
+   Bool_t            AnalyzePrimitive(TString &chain, TObjArray &args, Int_t &err, Int_t offset) override;
    void              Optimize() override;
 
 private:
