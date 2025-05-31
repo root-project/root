a48ec3a5f02965476c6eeca594318cdfbb740114
ferdymercury <ferdymercury@users.noreply.github.com>
2025-05-31 11:46:31 +0200

    [test] add proper override in rqobjecttester

--- core/base/inc/RQ_OBJECT.h
+++ core/base/inc/RQ_OBJECT.h
@@ -64,19 +64,22 @@
 //
 
 #define RQ_OBJECT1(sender_class)\
+   RQ_OBJECT1_(sender_class,)
+
+#define RQ_OBJECT1_(sender_class,overrd)\
 private:\
 TQObjSender fQObject;\
 public:\
-TList *GetListOfSignals() const {return fQObject.GetListOfSignals();}\
-Bool_t Connect(const char *sig,const char *cl,void *rcvr,const char *slt)\
+TList *GetListOfSignals() const overrd {return fQObject.GetListOfSignals();}\
+Bool_t Connect(const char *sig,const char *cl,void *rcvr,const char *slt) overrd\
 {fQObject.SetSender(this);fQObject.SetSenderClassName(sender_class);return fQObject.Connect(sig,cl,rcvr,slt);}\
-Bool_t Disconnect(const char *sig=nullptr,void *rcvr=nullptr,const char *slt=nullptr){return fQObject.Disconnect(sig,rcvr,slt);}\
-void HighPriority(const char *signal_name,const char *slot_name=nullptr){fQObject.HighPriority(signal_name,slot_name);}\
-void LowPriority(const char *signal_name,const char *slot_name=nullptr){fQObject.LowPriority(signal_name,slot_name);}\
-template <typename... T> void EmitVA(const char *signal_name, Int_t nargs, const T&... params) \
+Bool_t Disconnect(const char *sig=nullptr,void *rcvr=nullptr,const char *slt=nullptr) overrd {return fQObject.Disconnect(sig,rcvr,slt);}\
+void HighPriority(const char *signal_name,const char *slot_name=nullptr) overrd {fQObject.HighPriority(signal_name,slot_name);}\
+void LowPriority(const char *signal_name,const char *slot_name=nullptr) overrd {fQObject.LowPriority(signal_name,slot_name);}\
+template <typename... T> void EmitVA(const char *signal_name, Int_t nargs, const T&... params) overrd \
 { fQObject.EmitVA(signal_name,nargs,params...); } \
 void Emit(const char *signal){EmitVA(signal, /*nargs*/0);} \
-template <typename T> void Emit(const char *signal, const T& arg) { EmitVA(signal, /*nargs*/ 1, arg); } \
+template <typename T> void Emit(const char *signal, const T& arg) overrd { EmitVA(signal, /*nargs*/ 1, arg); } \
 
 #define RQ_OBJECT2(sender_class)\
 void Destroyed(){Emit("Destroyed()");}\
