fa13727f2048c851ee689fda03a0fe0e4fa4f584
Philippe Canal <pcanal@fnal.gov>
2025-06-02 16:56:46 -0500

    NOT READY: [tree] Fix handling of poorly cluster file in the retain previous cluster's basket code.
    Without this all the baskets of a poorly clustered branch are retaining until the deletion of the TTree object

--- tree/tree/src/TBranch.cxx
+++ tree/tree/src/TBranch.cxx
@@ -1992,7 +1992,7 @@ TBasket *TBranch::GetFreshCluster(TBuffer* user_buffer)
    // basket, just iterate backwards until the correct basket is reached. This should
    // be fast as long as the number of baskets per cluster is small
    Int_t basketToUnload = fReadBasket;
-   while (fBasketEntry[basketToUnload] != entryToUnload) {
+   while (fBasketEntry[basketToUnload] > entryToUnload) {
       basketToUnload--;
       if (basketToUnload < 0) {
          return CreateOrReuseBasket();
