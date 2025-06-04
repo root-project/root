e3b022efcc64d8fc08322aba2d2729b80dda62d3
ferdymercury <ferdymercury@users.noreply.github.com>
2025-06-04 16:38:09 +0200

    [test] add test for duplicate keys in ttreeindex

---
+++ roottest/root/tree/index/runDupe.C
@@ -0,0 +1,12 @@
+int runDupe()
+{
+  TTree tree("testTree", "my test tree");
+  ULong64_t run = 5;
+  ULong64_t event = 1;
+  tree.Branch("run", &run, "run/l");
+  tree.Branch("event", &event, "event/l");
+  tree.Fill();
+  tree.Fill();
+
+  tree.BuildIndex("run", "event");
+}
