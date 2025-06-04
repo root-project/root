c896a240c6ac341dd0953024caf8f16b085de6c1
Sergey Linev <S.Linev@gsi.de>
2025-06-04 11:04:18 +0200

    [roottest] use cmake for io/datamoduelevolution/cms-00
    Change logic as output produced in the the tests.
    Dumper create different files, which are compared by tests themselfs.
    If content is the same - just print that everything ok.
    If not - print content of both files.
    
    Special handling for std::pair<> printing - exclude base class handling
    while Linux and Mac has different base class for them
    
    Adjust several includes to avoid need setting
    source dir as include dir for ACLiC
    
    Disable test2 and test3, use ClingWorkAroundNoPrivateClassIO variable
    which also was defined in Makefile-based tests
    
    Adjust XML file content to use &lt; and &gt; to confirm XML standard

--- roottest/cmake/modules/RoottestCTest.cmake
+++ roottest/cmake/modules/RoottestCTest.cmake
@@ -28,32 +28,34 @@ set(CINT_VERSION cling)
 # Set of macros to avoid using features not yet implemented by cling.
 
 add_definitions(
-  -DClingWorkAroundMissingDynamicScope 
+  -DClingWorkAroundMissingDynamicScope
   -DClingWorkAroundUnnamedInclude
   -DClingWorkAroundMissingSmartInclude
   -DClingWorkAroundNoDotInclude
   -DClingWorkAroundMissingAutoLoadingForTemplates
   -DClingWorkAroundAutoParseUsingNamespace
   -DClingWorkAroundTClassUpdateDouble32
-  -DClingWorkAroundAutoParseDeclaration  
+  -DClingWorkAroundAutoParseDeclaration
   -DClingWorkAroundMissingUnloading
   -DClingWorkAroundBrokenUnnamedReturn
   -DClingWorkAroundUnnamedDetection2
+  -DClingWorkAroundNoPrivateClassIO
 )
 
 # Variables to be used in CMakeLists.txt files.
 
 set(ClingWorkAroundMissingDynamicScope              TRUE)
-set(ClingWorkAroundUnnamedInclude                   TRUE)
-set(ClingWorkAroundMissingSmartInclude              TRUE)
-set(ClingWorkAroundNoDotInclude                     TRUE)
-set(ClingWorkAroundMissingAutoLoadingForTemplates   TRUE)
-set(ClingWorkAroundAutoParseUsingNamespace          TRUE)
-set(ClingWorkAroundTClassUpdateDouble32             TRUE)
-set(ClingWorkAroundAutoParseDeclaration             TRUE)
-set(ClingWorkAroundMissingUnloading                 TRUE)
-set(ClingWorkAroundBrokenUnnamedReturn              TRUE)
-set(ClingWorkAroundUnnamedDetection2                TRUE)
+set(ClingWorkAroundUnnamedInclude                   TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-4763
+set(ClingWorkAroundMissingSmartInclude              TRUE)      # disabled in Makefile-based?
+set(ClingWorkAroundNoDotInclude                     TRUE)      # See trello card about .include
+set(ClingWorkAroundMissingAutoLoadingForTemplates   TRUE)      # See: https://sft.its.cern.ch/jira/browse/ROOT-4786
+set(ClingWorkAroundAutoParseUsingNamespace          TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-6317
+set(ClingWorkAroundTClassUpdateDouble32             TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-5857
+set(ClingWorkAroundAutoParseDeclaration             TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-6320
+set(ClingWorkAroundMissingUnloading                 TRUE)      # disabled in Makefile-based?
+set(ClingWorkAroundBrokenUnnamedReturn              TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-4719
+set(ClingWorkAroundNoPrivateClassIO                 TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-4865
+set(ClingWorkAroundUnnamedDetection2                TRUE)      # See https://sft.its.cern.ch/jira/browse/ROOT-8025
 
 set(PYROOT_EXTRAFLAGS --fixcling)
 
@@ -62,7 +64,7 @@ if(APPLE)
   execute_process(COMMAND sw_vers "-productVersion"
                   COMMAND cut -d . -f 1-2
                   OUTPUT_VARIABLE osvers OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(ROOTTEST_OS_ID MacOSX)                
+  set(ROOTTEST_OS_ID MacOSX)
   set(ROOTTEST_OS_VERSION ${osvers})
 elseif(WIN32)
   set(ROOTTEST_OS_ID Windows)
@@ -71,24 +73,24 @@ else()
   execute_process(COMMAND lsb_release -rs OUTPUT_VARIABLE osvers OUTPUT_STRIP_TRAILING_WHITESPACE)
   if(osid MATCHES Ubuntu)
     string(REGEX REPLACE "([0-9]+)[.].*" "\\1" osvers "${osvers}")
-    set(ROOTTEST_OS_ID Ubuntu)                
-    set(ROOTTEST_OS_VERSION ${osvers}) 
+    set(ROOTTEST_OS_ID Ubuntu)
+    set(ROOTTEST_OS_VERSION ${osvers})
   elseif(osid MATCHES Scientific)
     string(REGEX REPLACE "([0-9]+)[.].*" "\\1" osvers "${osvers}")
-    set(ROOTTEST_OS_ID Scientific)                
-    set(ROOTTEST_OS_VERSION ${osvers}) 
+    set(ROOTTEST_OS_ID Scientific)
+    set(ROOTTEST_OS_VERSION ${osvers})
   elseif(osid MATCHES Fedora)
     string(REGEX REPLACE "([0-9]+)" "\\1" osvers "${osvers}")
-    set(ROOTTEST_OS_ID Fedora)                
-    set(ROOTTEST_OS_VERSION ${osvers}) 
+    set(ROOTTEST_OS_ID Fedora)
+    set(ROOTTEST_OS_VERSION ${osvers})
   elseif(osid MATCHES CentOS)
     string(REGEX REPLACE "([0-9]+)[.].*" "\\1" osvers "${osvers}")
-    set(ROOTTEST_OS_ID CentOS)                
-    set(ROOTTEST_OS_VERSION ${osvers}) 
+    set(ROOTTEST_OS_ID CentOS)
+    set(ROOTTEST_OS_VERSION ${osvers})
   else()
     string(REGEX REPLACE "([0-9]+)[.].*" "\\1" osvers "${osvers}")
-    set(ROOTTEST_OS_ID ${osid})                
-    set(ROOTTEST_OS_VERSION ${osvers}) 
+    set(ROOTTEST_OS_ID ${osid})
+    set(ROOTTEST_OS_VERSION ${osvers})
   endif()
 endif()
 
