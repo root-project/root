ba8a9b7c5d02efca224b772adb97d170cef084fc
Sergey Linev <S.Linev@gsi.de>
2025-06-06 14:39:20 +0200

    [roottest] use cmake for io/double32 test
    Change reference file to exclude real addresses from printout
    Remove real offsets from ref file - to let work properly
    on 32-bit and 64-bit platforms

--- roottest/root/io/double32/runtemplate32.C
+++ roottest/root/io/double32/runtemplate32.C
@@ -23,18 +23,18 @@ int runtemplate32 ()
    gROOT->GetClass("MyVector<double>")->GetStreamerInfo()->ls();
    gROOT->GetClass("MyVector<Double32_t>")->GetStreamerInfo()->ls();
    gROOT->GetClass("Contains")->GetStreamerInfo()->ls();
-   gROOT->GetClass("Contains")->GetListOfRealData()->ls();
+   gROOT->GetClass("Contains")->GetListOfRealData()->ls("noaddr");
 
    TRealData *r = (TRealData*)gROOT->GetClass("Contains")->GetListOfRealData()->At(1);
    cout << "The following should be a Double32_t: " << r->GetDataMember()->GetTypeName() << endl;
 
-   TFile *f = new TFile("double32.root","RECREATE");
+   TFile *f = new TFile("double32.root", "RECREATE");
    Contains *c = new Contains;
    f->WriteObject(c,"myobj");
    delete f;
 
    TClass *cl = TClass::GetClass("m02<Double32_t>");
    cl->GetStreamerInfo()->ls("noaddr");
-   
+
    return 0;
 }
