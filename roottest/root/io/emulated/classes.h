49e656eccf2da40c6afc20fe74e80d719e8a5046
Sergey Linev <S.Linev@gsi.de>
2025-06-06 15:54:34 +0200

    [roottest] use cmake for io/emulated
    Use override syntax

--- roottest/root/io/emulated/classes.h
+++ roottest/root/io/emulated/classes.h
@@ -1,57 +1,55 @@
 #ifndef classes_h
-#ifdef ClingWorkAroundMultipleInclude
 #define classes_h
-#endif
 
 #include "TObject.h"
 #include "marker.h"
 #include <vector>
 
-class TopLevel 
+class TopLevel
 {
 public:
    Int_t fTopLevel;
    Marker fMarker;
-   TopLevel() : fMarker(Class_Name()) {};
+   TopLevel() : fMarker(Class_Name()) {}
    virtual ~TopLevel() {
       // if (Marker::fgDebug==2) fprintf(stdout, "TopLevel destructor for 0x%lx\n", (long)this);
    }
-   
+
    ClassDef(TopLevel,2);
 };
 
-class MidLevel : public TopLevel 
+class MidLevel : public TopLevel
 {
 public:
    Int_t fMidLevel;
    Marker fMarker;
-   MidLevel() : fMarker(Class_Name()) {};
+   MidLevel() : fMarker(Class_Name()) {}
    ClassDef(MidLevel,2);
 };
 
-class TObjTopLevel : public TObject 
+class TObjTopLevel : public TObject
 {
 public:
    Double_t fTObjTopLevel;
    Marker fMarker;
-   TObjTopLevel() : fMarker(Class_Name()) {};
+   TObjTopLevel() : fMarker(Class_Name()) {}
    ClassDef(TObjTopLevel,2);
 };
 
-class Bottom : public MidLevel 
+class Bottom : public MidLevel
 {
 public:
    TObjTopLevel fObject;
    Marker fMarker;
-   Bottom() : fMarker(Class_Name()) {};
+   Bottom() : fMarker(Class_Name()) {}
    ClassDef(Bottom,2);
 };
 
-class TObjBottom : public TObject, public MidLevel 
+class TObjBottom : public TObject, public MidLevel
 {
 public:
    Marker fMarker;
-   TObjBottom() : fMarker(Class_Name()) {};
+   TObjBottom() : fMarker(Class_Name()) {}
    ClassDef(TObjBottom,2);
 };
 
@@ -59,10 +57,10 @@ class Side
 {
 public:
    Marker fMarker;
-   Side() : fMarker(Class_Name()) {};
+   Side() : fMarker(Class_Name()) {}
    virtual ~Side() {
       // if (Marker::fgDebug==2) fprintf(stdout, "Side destructor for 0x%lx\n",(long)this);
-   }   
+   }
    ClassDef(Side,2);
 };
 
@@ -70,15 +68,15 @@ class BottomDouble : public MidLevel, public Side
 {
 public:
    Marker fMarker;
-   BottomDouble() : fMarker(Class_Name()) {};
+   BottomDouble() : fMarker(Class_Name()) {}
    ClassDef(BottomDouble,2);
 };
 
-class TObjFirst : public TObject, public TopLevel 
+class TObjFirst : public TObject, public TopLevel
 {
 public:
    Marker fMarker;
-   TObjFirst() : fMarker(Class_Name()) {};
+   TObjFirst() : fMarker(Class_Name()) {}
    ClassDef(TObjFirst,2);
 };
 
@@ -86,7 +84,7 @@ class TObjSecond : public TopLevel, public TObject
 {
 public:
    Marker fMarker;
-   TObjSecond() : fMarker(Class_Name()) {};
+   TObjSecond() : fMarker(Class_Name()) {}
    ClassDef(TObjSecond,2);
 };
 
@@ -101,7 +99,7 @@ public:
    TopLevel *fTObjSecond;
    vector<TopLevel*> fVec;
    Marker    *fMarker;
-   
+
    void Init() {
       fMidLevel = new MidLevel();
       fTObjTopLevel = new TObjTopLevel();
