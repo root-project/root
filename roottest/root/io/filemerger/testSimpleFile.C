dfdae6d26bc5331f8d0e8a51f25d947b10e3683d
Sergey Linev <S.Linev@gsi.de>
2025-06-10 13:14:03 +0200

    [roottest] use fixtures in io/filemerger
    Avoid DEPENDS while single tests cannot be run by such method
    Instead using PRE_CMD, separate hadd invocation as separate test
    to let create many files in parallel and then process them also independent.
    Use more clear fixtures names to be able correctly follow dependencies
    Adjust testSimpleFile.C macro

--- roottest/root/io/filemerger/testSimpleFile.C
+++ roottest/root/io/filemerger/testSimpleFile.C
@@ -1,39 +1,35 @@
-#ifdef R__HAS_DEFAULT_LZ4
-constexpr int expectedcomplevel = 404;
-#elif defined R__HAS_DEFAULT_ZLIB
-constexpr int expectedcomplevel = 101;
-#endif
-
-int testSimpleFile(const char *filename, Long64_t entries, Int_t compSetting, Long64_t fileSize, UInt_t tolerance = 0)
+int testSimpleFile(const char *filename, long entries, int compSetting, long fileSize, unsigned tolerance = 0)
 {
    fprintf(stdout,"Checking %s\n",filename);
-   TFile *file = TFile::Open(filename);
-   if (file == nullptr || file->IsZombie()) {
+   auto file = TFile::Open(filename);
+   if (!file || file->IsZombie()) {
       Error("testSimpleFile", "Could not open %s.",filename);
       return 1;
    }
-   //file->ls();
+
    if (file->GetCompressionSettings() != compSetting) {
-      Error("testSimpleFile","Compression level of %s should have been %d but is %d\n",file->GetName(), expectedcomplevel, file->GetCompressionSettings() );
+      Error("testSimpleFile","Compression level of %s should have been %d but is %d\n",file->GetName(), compSetting, (int) file->GetCompressionSettings() );
       return 100;
    }
 
-
-   if (abs(file->GetSize()-fileSize) > tolerance) {
-      Error("testSimpleFile","Disk size of %s should have been %lld but is %lld (tolerance %u bytes)\n",file->GetName(), fileSize, file->GetSize(), tolerance);
+   if (abs(file->GetSize() - fileSize) > tolerance) {
+      Error("testSimpleFile","Disk size of %s should have been %ld but is %ld (tolerance %u bytes)\n",file->GetName(), fileSize, (long) file->GetSize(), tolerance);
       return 1000;
    }
 
-   TTree *ntuple;
+   TTree *ntuple = nullptr;
    file->GetObject("ntuple",ntuple);
-   if (ntuple == 0) {
+   if (!ntuple) {
       Error("testSimpleFile", "Could not retrieve ntuple from %s.",file->GetName());
       return 10;
    }
+
    if (ntuple->GetEntries() != entries) {
-      Error("testSimpleFile","Number of entries in ntuple in %s should have been %lld but is %lld\n",file->GetName(), entries, ntuple->GetEntries());
+      Error("testSimpleFile","Number of entries in ntuple in %s should have been %ld but is %ld\n",file->GetName(), entries, (long) ntuple->GetEntries());
       return 10000;
    }
+
    delete file;
+
    return 0;
 }
