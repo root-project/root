4cf94bd72caea5c3d5ade5cb782e94b3fc8ee395
Sergey Linev <S.Linev@gsi.de>
2025-06-10 12:50:32 +0200

    [roottest] use cmake for io/fakeClass
    Major tests was failing already with Makefile - mark them so also in cmake
    Provide also ref file for printLittle macro, it works normally

--- roottest/root/io/fakeClass/printLittle.C
+++ roottest/root/io/fakeClass/printLittle.C
@@ -1,4 +1,4 @@
 {
-gROOT->ProcessLine(".L little.C+");
-TClass::GetClass("little")->GetStreamerInfo()->ls();
+   gROOT->ProcessLine(".L little.C+");
+   TClass::GetClass("little")->GetStreamerInfo()->ls("noaddr");
 }
