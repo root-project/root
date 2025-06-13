9822f0f3edd8f66029fdd2b82fa66343fd2fd0a3
ferdymercury <ferdymercury@users.noreply.github.com>
2025-06-13 20:34:50 +0200

    [tree] Double32/Float16 avoid overwriting title from base class since it contains leafcounter (#18829)
    Add test for GetLeafCounter with truncated data types, where name != title in TLeaf / TNamed
    
    Bump class version and customize streamer
    
    [nfc] improve TTree docu of custom-truncated arrays of fixed or var length
    
    Co-authored-by: Philippe Canal <pcanal@fnal.gov>

--- tree/tree/test/TTreeTruncatedDatatypes.cxx
+++ tree/tree/test/TTreeTruncatedDatatypes.cxx
@@ -1,6 +1,8 @@
 #include "TFile.h"
 #include "TSystem.h"
 #include "TTree.h"
+#include "TLeaf.h"
+#include "TBranch.h"
 
 #include "gtest/gtest.h"
 
@@ -55,5 +57,98 @@ TEST(TTreeTruncatedDatatypes, float16double32leaves)
 
    delete t;
    f.Close();
+   gSystem->Unlink(ofileName);
+}
+
+// https://its.cern.ch/jira/browse/ROOT-10149
+TEST(TTreeTruncatedDatatypes, LeafCounter)
+{
+   const auto ofileName = "leafcounter10149.root";
+   Long64_t nEntries = 10; // must be < 63
+   {
+      TFile f(ofileName, "RECREATE");
+      TTree *t = new TTree("t", "t");
+      int n;
+      Double32_t arr[64];
+      t->Branch("n", &n);
+      t->Branch("arr", arr, "arr[n]/d[0,100,32]");
+      t->Branch("arr_def", arr, "arr_def[n]/d");
+      t->Branch("arr_fix", arr, "arr_fix[64]/d[0,100,32]");
+      t->Branch("arr_fix_def", arr, "arr_fix_def[64]/d");
+      t->Branch("single", arr, "single/d[0,100,32]");
+      t->Branch("single_def", arr, "single_def/d");
+      for (auto name : {"arr", "arr_def", "arr_fix", "arr_fix_def", "single", "single_def"}) {
+         auto b = t->GetBranch(name);
+         EXPECT_NE(b, nullptr);
+         auto l = b->GetLeaf(name);
+         EXPECT_NE(l, nullptr);
+         int len;
+         auto b_c = l->GetLeafCounter(len);
+         if (strcmp(name, "arr") == 0 || strcmp(name, "arr_def") == 0) {
+            EXPECT_NE(b_c, nullptr);
+            EXPECT_STREQ(b_c->GetName(), "n");
+         } else if (strcmp(name, "arr_fix") == 0 || strcmp(name, "arr_fix_def") == 0) {
+            EXPECT_EQ(b_c, nullptr);
+            EXPECT_EQ(len, 64);
+         } else if (strcmp(name, "single") == 0 || strcmp(name, "single_def") == 0) {
+            EXPECT_EQ(b_c, nullptr);
+            EXPECT_EQ(len, 1);
+         }
+      }
+      // Fill tree
+      for (Long64_t i = 0; i < nEntries; ++i) {
+         n = i;
+         for (int j = 0; j < 64; ++j) {
+            arr[j] = i + 0.01 * j;
+         }
+         t->Fill();
+      }
+      t->Write();
+      f.Close();
+   }
+   {
+      TFile f(ofileName, "READ");
+      TTree *t = f.Get<TTree>("t");
+      for (auto name : {"arr", "arr_def", "arr_fix", "arr_fix_def", "single", "single_def"}) {
+         auto b = t->GetBranch(name);
+         EXPECT_NE(b, nullptr);
+         auto l = b->GetLeaf(name);
+         EXPECT_NE(l, nullptr);
+         int len;
+         auto b_c = l->GetLeafCounter(len);
+         if (strcmp(name, "arr") == 0 || strcmp(name, "arr_def") == 0) {
+            EXPECT_NE(b_c, nullptr);
+            EXPECT_STREQ(b_c->GetName(), "n");
+         } else if (strcmp(name, "arr_fix") == 0 || strcmp(name, "arr_fix_def") == 0) {
+            EXPECT_EQ(b_c, nullptr);
+            EXPECT_EQ(len, 64);
+         } else if (strcmp(name, "single") == 0 || strcmp(name, "single_def") == 0) {
+            EXPECT_EQ(b_c, nullptr);
+            EXPECT_EQ(len, 1);
+         }
+         int n;
+         Double32_t arr[64];
+         t->SetBranchAddress("n", &n);
+         t->SetBranchAddress(name, arr);
+         const auto tol = 0.001;
+         for (Long64_t i = 0; i < nEntries; ++i) {
+            t->GetEntry(i);
+            EXPECT_EQ(n, i);
+            if (strcmp(name, "arr") == 0 || strcmp(name, "arr_def") == 0) {
+               for (int j = 0; j < n; ++j) {
+                  EXPECT_NEAR(i + 0.01 * j, arr[j], tol);
+               }
+            } else if (strcmp(name, "arr_fix") == 0 || strcmp(name, "arr_fix_def") == 0) {
+               for (int j = 0; j < 64; ++j) {
+                  EXPECT_NEAR(i + 0.01 * j, arr[j], tol);
+               }
+            } else if (strcmp(name, "single") == 0 || strcmp(name, "single_def") == 0) {
+               EXPECT_NEAR(i, arr[0], tol);
+            }
+         }
+         t->ResetBranchAddresses();
+      }
+      f.Close();
+   }
    gSystem->Unlink(ofileName);
 }
