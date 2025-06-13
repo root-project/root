d4bc202d5e8f49aae45ebddaaf9820f86cd39d46
Sergey Linev <S.Linev@gsi.de>
2025-06-13 14:08:56 +0200

    [roottest] use cmake for io/newClassDef/current

--- roottest/root/io/newClassDef/current/Run.C
+++ roottest/root/io/newClassDef/current/Run.C
@@ -1,18 +1,9 @@
 void Run() {
-   
-   //TString library = ".L namespace.";
-   //gROOT->ProcessLine(library+dllsuf);
-   //library = ".L template.";
-   //gROOT->ProcessLine(library+dllsuf);
-   //library = ".L nstemplate.";
-   //// gROOT->ProcessLine(library+dllsuf);
-   
-   //library = ".L InheritMulti.";
-   //gROOT->ProcessLine(library+dllsuf);
-   gSystem->Load("./namespace");
-   gSystem->Load("./template");
-   gSystem->Load("./InheritMulti");
-   
+
+   gSystem->Load("libIoNewClassInheritMulti");
+   gSystem->Load("libIoNewClassnamespace");
+   gSystem->Load("libIoNewClasstemplate");
+
 #ifdef ClingWorkAroundMissingDynamicScope
 #ifdef ClingWorkAroundFunctionForwardDeclarations
    gROOT->ProcessLine("#include \"driverFunctionsFwdDecl.h\"");
@@ -32,7 +23,7 @@ void Run() {
    //nstemplate_driver();
    if (! InheritMulti_driver() ) exit(1);
 #endif
-   
+
    gROOT->ProcessLine(".L array.cxx+");
 #ifdef ClingWorkAroundMissingDynamicScope
    gROOT->ProcessLine("array_driver();");
