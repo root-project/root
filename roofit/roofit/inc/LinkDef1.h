46c6ce35fd5fd1db4db587b02de80ed57ee771a2
ferdymercury <ferdymercury@users.noreply.github.com>
2025-06-14 18:22:26 +0200

    [RF] New PDF: gaussian with double sided exponential tails
    Fixes https://its.cern.ch/jira/browse/ROOT-8093
    
    It's a simpler alternative to RooCrystalBall with potentially less instabilities

--- roofit/roofit/inc/LinkDef1.h
+++ roofit/roofit/inc/LinkDef1.h
@@ -23,6 +23,7 @@
 #pragma link C++ class RooExponential+ ;
 #pragma link C++ class RooLegacyExpPoly+ ;
 #pragma link C++ class RooPowerSum+ ;
+#pragma link C++ class RooGaussExpTails+ ;
 #pragma link C++ class RooGaussian+ ;
 #pragma link C++ class RooLognormal+ ;
 #pragma link C++ class RooGamma+ ;
