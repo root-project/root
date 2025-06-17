1a0686204a0fff041dc1ce1e361e014731a54375
Danilo Piparo <danilo.piparo@cern.ch>
2025-06-17 09:09:47 +0200

    [math] Include functional header for std::reference_wrapper
    (cherry picked from commit 0737de3d528075cacf6949a7c297b18842954a38)

--- roofit/roofitcore/inc/RooCmdConfig.h
+++ roofit/roofitcore/inc/RooCmdConfig.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Project: RooFit                                                           *
  * Package: RooFitCore                                                       *
- *    File: $Id: RooCmdConfig.h,v 1.12 2007/05/11 09:11:30 verkerke Exp $
+ *    File: $Id: RooCmdConfig.h,v 1.12 2007/05/11 09:11:30 verkerke Exp $    *
  * Authors:                                                                  *
  *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
  *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
@@ -25,6 +25,7 @@
 #include <TObject.h>
 #include <TString.h>
 
+#include <functional>
 #include <string>
 
 class RooArgSet;
