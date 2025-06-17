9da1cfacfc0f0bbf2aea08bff736ac1d7fc5cc75
silverweed <giacomo.parolini@cern.ch>
2025-06-17 11:38:15 +0200

    [rfile] Add test for writing/reading in python

--- io/v7/rfile/test/rfile.py
+++ io/v7/rfile/test/rfile.py
@@ -1,6 +1,6 @@
 import unittest
 import ROOT
-
+import os
 
 RFile = ROOT.Experimental.RFile
 
@@ -13,7 +13,7 @@ class RFileTests(unittest.TestCase):
         # Create a root file to open
         with ROOT.TFile.Open(fileName, "RECREATE") as tfile:
             hist = ROOT.TH1D("hist", "", 100, -10, 10)
-            hist.FillRandom("gaus", 1000)
+            hist.FillRandom("gaus", 100)
             tfile.WriteObject(hist, "hist")
 
         with RFile.OpenForReading(fileName) as rfile:
@@ -23,8 +23,29 @@ class RFileTests(unittest.TestCase):
             self.assertEqual(rfile.Get[ROOT.TH1F]("hist"), None)
             self.assertNotEqual(rfile.Get[ROOT.TH1]("hist"), None)
 
-            foo = "foo"
-            self.assertRaises(rfile.Put("foo", foo))
+            with self.assertRaises(ROOT.RException) as _cm:
+                rfile.Put("foo", hist)
+
+        os.remove(fileName)
+
+
+    def test_writing_reading(self):
+        """A RFile can be written into and read from"""
+
+        fileName = "test_rfile_writeread_py.root"
+
+        with RFile.Recreate(fileName) as rfile:
+            hist = ROOT.TH1D("hist", "", 100, -10, 10)
+            hist.FillRandom("gaus", 10)
+            rfile.Put("hist", hist)
+            with self.assertRaises(ROOT.RException) as _cm:
+                rfile.Put("hist/2", hist)
+
+        with RFile.OpenForReading(fileName) as rfile:
+            hist = rfile.Get("hist")
+            self.assertNotEqual(hist, None)
+
+        os.remove(fileName)
         
 
 if __name__ == "__main__":
