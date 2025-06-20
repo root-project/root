923ee9f4020709788ac1661e4bdf3699322b550e
Carsten Burgard <carsten.burgard@gmail.com>
2025-06-20 10:49:11 +0200

    [RF][HS3] Important bugfix to extraction of hand-coded formulas

--- roofit/hs3/src/RooJSONFactoryWSTool.cxx
+++ roofit/hs3/src/RooJSONFactoryWSTool.cxx
@@ -178,39 +178,35 @@ struct Var {
  */
 bool isNumber(const std::string &str)
 {
-    bool seen_digit = false;
-    bool seen_dot = false;
-    bool seen_e = false;
-    bool after_e = false;
-    bool sign_allowed = true;
+   bool seen_digit = false;
+   bool seen_dot = false;
+   bool seen_e = false;
+   bool after_e = false;
+   bool sign_allowed = true;
 
-    for (size_t i = 0; i < str.size(); ++i) {
-        char c = str[i];
+   for (size_t i = 0; i < str.size(); ++i) {
+      char c = str[i];
 
-        if (std::isdigit(c)) {
-            seen_digit = true;
-            sign_allowed = false;
-        }
-        else if ((c == '+' || c == '-') && sign_allowed) {
-            // Sign allowed at the beginning or right after 'e'/'E'
-            sign_allowed = false;
-        }
-        else if (c == '.' && !seen_dot && !after_e) {
-            seen_dot = true;
-            sign_allowed = false;
-        }
-        else if ((c == 'e' || c == 'E') && seen_digit && !seen_e) {
-            seen_e = true;
-            after_e = true;
-            sign_allowed = true; // allow sign immediately after 'e'
-            seen_digit = false; // reset: we now expect digits after e
-        }
-        else {
-            return false;
-        }
-    }
+      if (std::isdigit(c)) {
+         seen_digit = true;
+         sign_allowed = false;
+      } else if ((c == '+' || c == '-') && sign_allowed) {
+         // Sign allowed at the beginning or right after 'e'/'E'
+         sign_allowed = false;
+      } else if (c == '.' && !seen_dot && !after_e) {
+         seen_dot = true;
+         sign_allowed = false;
+      } else if ((c == 'e' || c == 'E') && seen_digit && !seen_e) {
+         seen_e = true;
+         after_e = true;
+         sign_allowed = true; // allow sign immediately after 'e'
+         seen_digit = false;  // reset: we now expect digits after e
+      } else {
+         return false;
+      }
+   }
 
-    return seen_digit;
+   return seen_digit;
 }
 
 /**
@@ -923,7 +919,7 @@ RooRealVar *RooJSONFactoryWSTool::requestImpl<RooRealVar>(const std::string &obj
    if (RooRealVar *retval = _workspace.var(objname))
       return retval;
    if (const auto *vars = getVariablesNode(*_rootnodeInput)) {
-      if (const auto& node = vars->find(objname)) {
+      if (const auto &node = vars->find(objname)) {
          this->importVariable(*node);
          if (RooRealVar *retval = _workspace.var(objname))
             return retval;
@@ -937,8 +933,8 @@ RooAbsPdf *RooJSONFactoryWSTool::requestImpl<RooAbsPdf>(const std::string &objna
 {
    if (RooAbsPdf *retval = _workspace.pdf(objname))
       return retval;
-   if (const auto& distributionsNode = _rootnodeInput->find("distributions")) {
-      if (const auto& child = findNamedChild(*distributionsNode, objname)) {
+   if (const auto &distributionsNode = _rootnodeInput->find("distributions")) {
+      if (const auto &child = findNamedChild(*distributionsNode, objname)) {
          this->importFunction(*child, true);
          if (RooAbsPdf *retval = _workspace.pdf(objname))
             return retval;
@@ -958,8 +954,8 @@ RooAbsReal *RooJSONFactoryWSTool::requestImpl<RooAbsReal>(const std::string &obj
       return pdf;
    if (RooRealVar *var = requestImpl<RooRealVar>(objname))
       return var;
-   if (const auto& functionNode = _rootnodeInput->find("functions")) {
-      if (const auto& child = findNamedChild(*functionNode, objname)) {
+   if (const auto &functionNode = _rootnodeInput->find("functions")) {
+      if (const auto &child = findNamedChild(*functionNode, objname)) {
          this->importFunction(*child, true);
          if (RooAbsReal *retval = _workspace.function(objname))
             return retval;
