17ceb6772f6cbbd0c14d0755f0cfa27c55028795
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2025-06-20 10:50:56 +0200

    Include cstddef for std::byte

--- roottest/root/ntuple/atlas-datavector/AtlasLikeDataVector.hxx
+++ roottest/root/ntuple/atlas-datavector/AtlasLikeDataVector.hxx
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <cstddef>
 
 // https://gitlab.cern.ch/atlas/athena/-/blob/26885cbcf82f069cddff6cd30c123572e362d8fa/Control/AthContainers/AthContainers/tools/DVLNoBase.h#L32
 namespace DataModel_detail {
@@ -48,10 +49,10 @@ class AtlasLikeDataVector : KeepFirstTemplateArguments<1>, SelectNoInstance {};
 } // namespace ROOT::Meta::Selection
 
 struct CustomStruct {
-   float a = 0.0;
-   std::vector<float> v1;
-   std::vector<std::vector<float>> v2;
-   std::string s;
+   float a{};
+   std::vector<float> v1{};
+   std::vector<std::vector<float>> v2{};
+   std::string s{};
    std::byte b{};
 };
 
