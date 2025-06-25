a01d71b04cfef2a398db9cbdc64b394443647a98
Sergey Linev <S.Linev@gsi.de>
2025-06-25 10:02:06 +0200

    [roottest] adjust macros in io/references code
    Add  `#pragma link C++ class A+;` to A.C to satisfy
    windows

--- roottest/root/io/references/A.C
+++ roottest/root/io/references/A.C
@@ -1,15 +1,17 @@
-#ifndef A_C
-#define A_C
+#ifndef MACRO_A_C
+#define MACRO_A_C
 
 #include "TRef.h"
 
 class A : public TObject {
 public:
-A(A* other = 0) { _o = other; }
-
-TRef _o;
-
-ClassDef(A, 1)
+  A(A* other = nullptr) { _o = other; }
+  TRef _o;
+  ClassDef(A, 1)
 };
+
+#ifdef __ROOTCLING__
+#pragma link C++ class A+;
+#endif
 
 #endif
