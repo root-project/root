238a5060cf39d18334ba7be8164b8b66e7fcc3f2
Sergey Linev <S.Linev@gsi.de>
2025-06-25 10:02:06 +0200

    [roottest] adjust macros in io/references code
    Add  `#pragma link C++ class A+;` to A.C to satisfy
    windows

--- roottest/root/io/references/lotsRef.C
+++ roottest/root/io/references/lotsRef.C
@@ -1,18 +1,9 @@
 #include "TFile.h"
 #include "TProcessID.h"
 #include "TRef.h"
-#include "Riostream.h"
-
-void Check(TObject* obj) {
-
-   if (obj) cout << "Found the referenced object\n";
-   else cout << "Error: Could not find the referenced object\n";
-}
-
 #include "TClonesArray.h"
 #include "TTree.h"
 #include "TObject.h"
-#include "TRef.h"
 
 #if defined(__ROOTCLING__) || !defined(__CLING__)
 #include "A.C"
@@ -23,6 +14,17 @@ void Check(TObject* obj) {
 #include "A.C+"
 #endif
 #endif
+
+#include <iostream>
+
+void Check(TObject* obj) {
+
+   if (obj)
+      std::cout << "Found the referenced object\n";
+   else
+      std::cout << "Error: Could not find the referenced object\n";
+}
+
 
 void AddTree() {
    TTree* tree = new TTree("tree", "");
@@ -56,14 +58,14 @@ void ReadTree() {
    TClonesArray* c2 = new TClonesArray("A", 1);
    tree->SetBranchAddress("C1", &c1);
    tree->SetBranchAddress("C2", &c2);
-  
+
    tree->GetEntry(0);
    tree->GetEntry(1);
 }
 
 const char *filename = "lotsRef.root";
 void lotsRef(int what) {
-   if (what>2) {
+   if (what > 2) {
       Int_t size = what;
       TFile *_file0 = TFile::Open(filename,"RECREATE");
       for(int i=0;i<size;++i) {
@@ -71,37 +73,46 @@ void lotsRef(int what) {
          _file0->WriteProcessID(id);
       }
       AddTree();
-      _file0->Write(); delete _file0;
-   } else if (what==2) {
+      _file0->Write();
+      delete _file0;
+   } else if (what == 2) {
       TFile *_file0 = TFile::Open(filename,"UPDATE");
 
-      TNamed *n = new TNamed("mine","title"); TRef *r = new TRef(n);
+      TNamed *n = new TNamed("mine", "title");
+      TRef *r = new TRef(n);
       n->Write();
       r->Write();
       AddTree();
-      _file0->Write(); delete _file0;
-   } else if (what==1) {
+      _file0->Write();
+      delete _file0;
+   } else if (what == 1) {
       TFile *_file0 = TFile::Open(filename,"UPDATE");
 
-      int i=0;
+      int i = 0;
       while( _file0->ReadProcessID(++i) ) {};
 
-      TNamed *n;_file0->GetObject("mine",n);
-      TRef *r;_file0->GetObject("TRef",r);
-      if (r==0) { cerr << "Could not find the TRef on file \n"; return; }
-      if (n==0) { cerr << "Could not find the TNamed on file \n"; return;}
+      TNamed *n = nullptr;
+      _file0->GetObject("mine",n);
+      TRef *r = nullptr;
+      _file0->GetObject("TRef",r);
+      if (!r) { std::cerr << "Could not find the TRef on file \n"; return; }
+      if (!n) { std::cerr << "Could not find the TNamed on file \n"; return;}
       ReadTree();
       Check( r->GetObject() );
       AddTree();
       n->Write();
       r->Write();
       _file0->Write();
+      delete _file0;
    } else {
       TFile *_file0 = TFile::Open(filename);
-      TRef *r;_file0->GetObject("TRef",r);
-      TNamed *n;_file0->GetObject("mine",n);
-      Check( r->GetObject() );
+      TRef *r = nullptr;
+      _file0->GetObject("TRef", r);
+      TNamed *n = nullptr;
+      _file0->GetObject("mine", n);
+      Check( r ? r->GetObject() : nullptr );
       ReadTree();
+      delete _file0;
    }
 }
 
