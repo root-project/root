31f7417790194513c8c1b98892ec0335e284b215
Sergey Linev <S.Linev@gsi.de>
2025-06-25 12:59:21 +0200

    [roottest] use libTestIoEvent in tree/cache test
    Instead build new library and manage own sources -
    just reuse existing library. As it was before used in makefile

--- roottest/root/tree/cache/execperfstattest.C
+++ roottest/root/tree/cache/execperfstattest.C
@@ -9,36 +9,33 @@
 #include "TROOT.h"
 #include <iostream>
 
-#if defined(__CINT__) && !defined(__MAKECINT__) 
-#include "libEvent.so"
-#else 
 #include "Event.h"
-#endif
 
-void write()
+bool write()
 {
    // check to see if the event class is in the dictionary
    // if it is not load the definition in libEvent.so
    if (!TClassTable::GetDict("Event")) {
-      gSystem->Load("$ROOTSYS/test/libEvent");
+      std::cerr << "Dictionary for Event class is not provided!" << std::endl;
+      return false;
    }
- 
+
   //create a Tree file tree4.root
   TFile f("perftest.root","RECREATE");
- 
+
   // Create 2 ROOT Trees
   TTree tree1("tree1","One of two simultaneous trees");
   TTree tree2("tree2","A tree made simultaneously with tree 1");
-  
+
   // Create a pointer to an Event object
   Event *event1 = new Event();
-  Event *event2 = new Event();  
-    
+  Event *event2 = new Event();
+
   // Create single branch of different split levels for each tree.
   tree1.Branch("event", &event1, 8000,2);
   tree2.Branch("event2", &event2, 8000,2);
-    
-  // a local variable for the event type 
+
+  // a local variable for the event type
   char etype[20];
 
   // Fill the tree
@@ -61,12 +58,12 @@ void write()
     event2->SetNvertex(Int_t(1+20*gRandom->Rndm()));
     event2->SetFlag(UInt_t(random+0.5));
     event2->SetTemperature(random+20.);
-    
+
     for(UChar_t m = 0; m < 10; m++) {
       event1->SetMeasure(m, Int_t(gRandom->Gaus(m,m+1)));
       event2->SetMeasure(m, Int_t(gRandom->Gaus(m,m+1)));
     }
-    
+
     // fill the matrix
     for(UChar_t j = 0; j < 4; j++) {
       for(UChar_t k = 0; k < 4; k++) {
@@ -79,21 +76,23 @@ void write()
     for (Int_t t = 0; t < ntrack; t++){
       event1->AddTrack(random);
       event2->AddTrack(random);}
-    
+
     // Fill the tree
     tree1.Fill();
     tree2.Fill();
-    
-    // Clear the event before reloading it 
+
+    // Clear the event before reloading it
     event1->Clear();
     event2->Clear();
   }
-  
+
   // Write the file header
   f.Write();
+  return true;
 }
 
-void simultaneous(){
+void simultaneous()
+{
 
    TFile *f = TFile::Open("perftest.root");
 
@@ -128,10 +127,12 @@ void simultaneous(){
 }
 
 
-int execperfstattest() {
-   Event::Reset(); // Allow for re-run this script by cleaning static variables.
-   write();
-   Event::Reset(); // Allow for re-run this script by cleaning static variables.
+int execperfstattest()
+{
+   // Event::Reset(); // Allow for re-run this script by cleaning static variables.
+   if (!write())
+      return 1;
+   // Event::Reset(); // Allow for re-run this script by cleaning static variables.
    simultaneous();
    return 0;
 }
