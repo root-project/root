e9ad3f44242efc7f00374afd32ec64b6f678bf48
Sergey Linev <S.Linev@gsi.de>
2025-06-26 18:34:10 +0200

    [roottest] use cmake for treeformula/schemaEvolution
    Disable final compare of files while only sed can be used there

--- roottest/root/treeformula/schemaEvolution/schemaRun3.C
+++ roottest/root/treeformula/schemaEvolution/schemaRun3.C
@@ -1,21 +1,13 @@
+int schemaRun3()
 {
-#if defined(ClingWorkAroundIncorrectTearDownOrder)
-   if (1) {
-#endif
-      
    TFile f("Event.root");
-   gSystem->Load("./libEvent_2"); 
-   
-#ifdef ClingWorkAroundMissingDynamicScope
-   TTree *T; f.GetObject("T",T);
-#endif
-   
+   gSystem->Load("libTreeFormulaScemaEvolution2");
+
+   TTree *T = nullptr;
+   f.GetObject("T",T);
+
    T->Show(5); //ok
    T->Scan("fTemperature"); //ok
-   // gSystem->Load("libTreePlayer");
    auto tf = new TTreeFormula("tf","fTemperature",T);
-      
-#if defined(ClingWorkAroundIncorrectTearDownOrder)
-   }
-#endif
+   return 0;
 }
