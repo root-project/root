244b925aa283c931f9cc674bdc040698744cf8a8
Sergey Linev <S.Linev@gsi.de>
2025-06-26 17:17:13 +0200

    [roottest] use cmake for treeformula/retobj tests
    Reuse libTestIoEvent with small modifications
    Simplify runretobjTest.C to avoid testing old CINT problems

--- roottest/root/treeformula/retobj/helper.C
+++
@@ -1,9 +0,0 @@
-void check(const char*arg) {
-  tf = new TTreeFormula("test",arg,tree);
-  tree->GetEntry(0);
-  o = (TObject*)tf->EvalObject();
-  o = (TObject*)tf->EvalObject();
-  if (o) o->IsA()->Print();
-  if (tf->EvalClass())
-     tf->EvalClass()->Print();
-}
