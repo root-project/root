0a4e8322a28a8847f413cf0b98e0a7cde96a34f6
ferdymercury <ferdymercury@users.noreply.github.com>
2025-06-26 15:53:56 +0200

    Update misc/minicern/src/cernlib.c

--- misc/minicern/src/cernlib.c
+++ misc/minicern/src/cernlib.c
@@ -81,7 +81,7 @@ void vxinvb_(int *ixv, int *n)
             ((in >> 8) & 0x0000ff00) |
             ((in << 8) & 0x00ff0000) |
             ((in << 24) & 0xff000000);
-      printf("%d ", ixv[jloop]);
+      printf("%04x ", ixv[jloop]);
    }
    printf("\n");
    return;
