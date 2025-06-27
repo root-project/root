149a7c9cb5567e2adbf87dc5b885f988a8cb7569
Sergey Linev <S.Linev@gsi.de>
2025-06-27 15:30:19 +0200

    [roottest] re-enable tree/fastcloningeventtree
    Very similar to treeformula/event testing,
    therefore reuse code from there

--- roottest/root/tree/fastcloningeventtree/ctrans.C
+++ roottest/root/tree/fastcloningeventtree/ctrans.C
@@ -1,12 +1,16 @@
-void ctrans(const char *filename)
+void ctrans(const char *filename, const char *outfilename)
 {
   TFile *_file0 = TFile::Open(filename);
-  gSystem->Load("libEvent.so");
   TTree *from = (TTree*) _file0->Get("T");
-  auto f = new TFile("clone.root","RECREATE");
+  auto f = new TFile(outfilename, "RECREATE");
+  printf("Call CloneTree\n");
   from->CloneTree(-1,"fast");
   //to->Import(from);
+  printf("Call CloneTree done\n");
+
   f->Write();
-  delete _file0; delete f;
-  gSystem->Exec(Form("mv clone.root %s",filename));
+  printf("delete _file0\n");
+  delete _file0;
+  printf("delete f\n");
+  delete f;
 }
