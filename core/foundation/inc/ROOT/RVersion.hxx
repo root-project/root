6fced0c5041f8548a1be8f9c8e2b3b08ff3e2424
Danilo Piparo <danilo.piparo@cern.ch>
2025-06-27 09:17:46 +0200

    Update release version

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,8 +4,8 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 34
-#define ROOT_VERSION_PATCH 9
-#define ROOT_RELEASE_DATE "Apr 8 2025"
+#define ROOT_VERSION_PATCH 10
+#define ROOT_RELEASE_DATE "Jun 27 2025"
 
 /* Don't change the lines below. */
 
