18f02628d8febd7fd439a46cd45450742209db19
Sergey Linev <S.Linev@gsi.de>
2025-06-27 12:42:07 +0200

    [roottest] use cmake for tree/friend tests
    Re-enable several tests like all testFriend.C macros
    Delete unused files

--- roottest/root/tree/friend/testFriend2.cc
+++ roottest/root/tree/friend/testFriend2.cc
@@ -1,18 +1,11 @@
 
-TChain * c;
-
-TChain* testFriend2()
+void testFriend2()
 {
   TChain *ch = new TChain("ntp1");
   TChain *as = new TChain("ntp2");
-  ch.Add("MC*.root");
-  as.Add("MC*.root");
-  ch.AddFriend("ntp2");
+  ch->Add("MC*.root");
+  as->Add("MC*.root");
+  ch->AddFriend("ntp2");
 
-  ch.Draw("ntp2.runNumber"); // segmentation violation
-
-  int a;
-//  scanf("%d",&a);
-  c = ch;
-  return ch;
+  ch->Draw("ntp2.runNumber"); // segmentation violation
 }
