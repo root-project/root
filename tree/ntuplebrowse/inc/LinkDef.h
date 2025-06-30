1898c34420e710498a5d4201bdaf6a6f95a42c98
Jakob Blomer <jblomer@cern.ch>
2025-06-30 23:33:43 +0200

    [ntuple] Add initial RNTupleBrowse library
    Implementation of the browsing logic with the classic T(Root)Browser.

---
+++ tree/ntuplebrowse/inc/LinkDef.h
@@ -0,0 +1,13 @@
+// Author: Jakob Blomer CERN 06/2025
+
+/*************************************************************************
+ * Copyright (C) 1995-2025, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __CLING__
+
+#endif
