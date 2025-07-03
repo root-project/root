fa366481ba54b2d73232369c10160b958bf99c7b
Sergey Linev <S.Linev@gsi.de>
2025-07-03 16:15:35 +0200

    [roottest] use override in io/newdelete classes

--- roottest/root/io/newdelete/Classes.h
+++ roottest/root/io/newdelete/Classes.h
@@ -3,14 +3,14 @@
 
 bool Classes();
 
-//void *operator new(int); 
+//void *operator new(int);
 
 class PrivateDestructor {
   friend class TObject;
   ~PrivateDestructor();
 public:
   int a;
-  PrivateDestructor(int b) : a(b) {};
+  PrivateDestructor(int b) : a(b) {}
 };
 
 class Struct {
@@ -21,28 +21,28 @@ public:
 class Nodefault {
 public:
   int a;
-  Nodefault(int aa) : a(aa) {};
+  Nodefault(int aa) : a(aa) {}
   ~Nodefault() { fprintf(stderr,"deleting a Nodefault %d\n",a); }
 };
 
 class NodefaultOpNew {
 public:
   int a;
-  NodefaultOpNew(int aa) : a(aa) {};
+  NodefaultOpNew(int aa) : a(aa) {}
   void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); }
 };
 
 class OpNew {
 public:
   int a;
-  OpNew() : a(0) {};
+  OpNew() : a(0) {}
   void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); }
 };
 
 class OpNewPlacement {
 public:
   int a;
-  OpNewPlacement() : a(0) {};
+  OpNewPlacement() : a(0) {}
   void *operator new(size_t sz, void *p) { return TStorage::ObjectAlloc(sz,p); }
   void     operator delete(void *ptr, void *vp);
 };
@@ -50,7 +50,7 @@ public:
 class OpNewBoth {
 public:
   int a;
-  OpNewBoth() : a(0) {};
+  OpNewBoth() : a(0) {}
   void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); }
   void *operator new(size_t sz, void *p) { return TStorage::ObjectAlloc(sz,p); }
 /*   void     operator delete(void *ptr, void *vp); */
@@ -67,69 +67,69 @@ class PartialDefault {
 public:
   TClass *c;
   int a;
-  PartialDefault(TClass* cl,Int_t all=0): c(cl),a(all) {};
+  PartialDefault(TClass* cl,Int_t all=0): c(cl),a(all) {}
 };
 
 class Normal {
 public:
   int a;
-  Normal() : a(0) {};
-  Normal(int aa) : a(aa) {};
+  Normal() : a(0) {}
+  Normal(int aa) : a(aa) {}
 };
 
 
 class TStruct : public TObject {
 public:
   int a;
-  ClassDef(TStruct,0);
+  ClassDefOverride(TStruct,0);
 };
 
 class TNodefault : public TObject {
 public:
   int a;
-  TNodefault(int aa) : a(aa) {};
-  virtual ~TNodefault() { fprintf(stderr,"deleting a TNodefault %d\n",a); }
-  ClassDef(TNodefault,1); //
+  TNodefault(int aa) : a(aa) {}
+  ~TNodefault() override { fprintf(stderr,"deleting a TNodefault %d\n",a); }
+  ClassDefOverride(TNodefault,1); //
 };
 
 class TNodefaultOpNew : public TObject  {
 public:
   int a;
-  TNodefaultOpNew(int aa) : a(aa) {};
-  void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); };
-  ClassDef(TNodefaultOpNew,0);
+  TNodefaultOpNew(int aa) : a(aa) {}
+  void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); }
+  ClassDefOverride(TNodefaultOpNew,0);
 };
 
 class TOpNew : public TObject  {
 public:
   int a;
-  TOpNew() : a(0) {};
-  void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); };
-  ClassDef(TOpNew,1);
+  TOpNew() : a(0) {}
+  void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); }
+  ClassDefOverride(TOpNew,1);
 };
 
 class TOpNewPlacement : public TObject {
 public:
   int a;
-  TOpNewPlacement() : a(0) {};
+  TOpNewPlacement() : a(0) {}
   void *operator new(size_t sz, void *p) { return TStorage::ObjectAlloc(sz,p); }
-  ClassDef(TOpNewPlacement,1);
+  ClassDefOverride(TOpNewPlacement,1);
 };
 
 class TOpNewBoth : public TObject {
 public:
   int a;
-  TOpNewBoth() : a(0) {};
+  TOpNewBoth() : a(0) {}
   void *operator new(size_t sz) { return TStorage::ObjectAlloc(sz); }
   void *operator new(size_t sz, void *p) { return TStorage::ObjectAlloc(sz,p); }
-  ClassDef(TOpNewBoth,1);
+  ClassDefOverride(TOpNewBoth,1);
 };
 
 class TDefault  : public TObject{
 public:
   int a;
-  TDefault(int aa = 0) : a(aa) {};
-  ClassDef(TDefault,0);
+  TDefault(int aa = 0) : a(aa) {}
+  ClassDefOverride(TDefault,0);
 };
 
 class TPartialDefault  : public TObject  {
@@ -137,25 +137,25 @@ public:
   TClass *c;
   int a;
   TPartialDefault(TClass* cl,Int_t all=0) : c(cl),a(all) {}
-  ClassDef(TPartialDefault,0);
+  ClassDefOverride(TPartialDefault,0);
 };
 
 class TPrivateDefaultConstructor : public TObject {
 private:
-   TPrivateDefaultConstructor() {};
+   TPrivateDefaultConstructor() {}
 public:
-   TPrivateDefaultConstructor(int) {};
-   ClassDef(TPrivateDefaultConstructor,1); 
+   TPrivateDefaultConstructor(int) {}
+   ClassDefOverride(TPrivateDefaultConstructor,1);
 };
 
 class TNormal  : public TObject{
 public:
   int a;
   TPrivateDefaultConstructor *s;
-  TNormal() : a(0) {};
-  TNormal(int aa) : a(aa) {};
+  TNormal() : a(0) {}
+  TNormal(int aa) : a(aa) {}
 
-  ClassDef(TNormal,1);
+  ClassDefOverride(TNormal,1);
 };
 
 // See ROOT-7014.
