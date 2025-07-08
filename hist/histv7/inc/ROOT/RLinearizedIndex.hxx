0d7a3d766663574a41c63ffef233e90c3512e510
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-07-08 16:12:18 +0200

    [hist] Implement initial RRegularAxis
    The first version of this class exposes only ComputeLinearizedIndex
    with a simple index computation formula. It does not (yet) implement
    the recent changes to improve its precision available in TAxis.
    
    This commit also includes basic build system additions and setup for
    unit testing.

---
+++ hist/histv7/inc/ROOT/RLinearizedIndex.hxx
@@ -0,0 +1,28 @@
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+#ifndef ROOT_RLinearizedIndex
+#define ROOT_RLinearizedIndex
+
+#include <cstddef>
+
+namespace ROOT {
+namespace Experimental {
+
+/**
+A linearized index that can be invalid.
+
+For example, when an argument is outside the axis and underflow / overflow bins are disabled.
+
+\warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is
+welcome!
+*/
+struct RLinearizedIndex final {
+   std::size_t fIndex;
+   bool fValid;
+};
+
+} // namespace Experimental
+} // namespace ROOT
+
+#endif
