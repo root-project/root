065d1c5e22b29e96ed760a87eec282bc3d50a60d
Danilo Piparo <danilo.piparo@cern.ch>
2025-07-10 11:14:02 +0200

    Change version to 6.36.03

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,7 +4,7 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 36 
-#define ROOT_VERSION_PATCH 2 // When changing the version number here, never add leading zeroes!
+#define ROOT_VERSION_PATCH 3 // When changing the version number here, never add leading zeroes!
 #define ROOT_RELEASE_DATE "July 8 2025"
 
 /* Don't change the lines below. */
