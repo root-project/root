e6b9c6590bfc2f3c7e290746b62cdd810e9fdb9e
Jonas Rembser <jonas.rembser@cern.ch>
2025-07-22 01:03:17 +0200

    [roottest] Don't set cppyy backend path manually
    This should not be needed anymore, and not setting the backend path also
    helps to validate that the mechanism in the ROOT module to find the
    cppyy backend library works.

--- roottest/python/common.py
+++ roottest/python/common.py
@@ -3,7 +3,7 @@
 # Created: 09/24/10
 # Last: 04/04/14
 
-__all__ = [ 'pylong', 'maxvalue', 'MyTestCase', 'run_pytest', 'FIXCLING', 'check_cppyy_backend' ]
+__all__ = [ 'pylong', 'maxvalue', 'MyTestCase', 'run_pytest', 'FIXCLING' ]
 
 import os, sys, unittest, warnings
 import pytest
@@ -37,12 +37,3 @@ def run_pytest(test_file=None):
     if test_file: args += [test_file]
     # actual test run
     return pytest.main(args)
-
-def check_cppyy_backend():
-    # Helper function to check if CPPYY_BACKEND_LIBRARY
-    # points to an existing file.
-    # If it does not, the variable is unset to let cppyy
-    # look for the library in its default directories.
-    cbl = os.environ.get('CPPYY_BACKEND_LIBRARY')
-    if cbl is not None and not os.path.isfile(cbl):
-        del os.environ['CPPYY_BACKEND_LIBRARY']
--- roottest/python/regression/exec_root_6023.py
+++ roottest/python/regression/exec_root_6023.py
@@ -2,9 +2,6 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__))) # for common
 
-from common import check_cppyy_backend
-check_cppyy_backend()
-
 import ROOT
 ROOT.gROOT.ProcessLine('.L root_6023.h+')
 
