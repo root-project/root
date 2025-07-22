b893283505bd853b9327ba15009d25fc671bd46d
Martin Foll <martin.foll@cern.ch.>
2025-07-22 09:55:04 +0200

    Add documentation and comments

--- tmva/tmva/inc/TMVA/BatchGenerator/RBatchLoader.hxx
+++ tmva/tmva/inc/TMVA/BatchGenerator/RBatchLoader.hxx
@@ -31,8 +31,18 @@ namespace TMVA {
 namespace Experimental {
 namespace Internal {
 
+// clang-format off
+/**
+\class ROOT::TMVA::Experimental::Internal::RBatchLoader
+\ingroup tmva
+\brief Building and loading the batches from loaded chunks in RChunkLoader
+
+In this class the chunks that are loaded into memory (see RChunkLoader) are split into batches used in the ML training which are loaded into a queue. This is done for both the training and validation chunks separatly.
+*/
+
 class RBatchLoader {
 private:
+   // clang-format on      
    std::size_t fChunkSize;
    std::size_t fBatchSize;
    std::size_t fNumColumns;
@@ -45,14 +55,18 @@ private:
    std::mutex fBatchLock;
    std::condition_variable fBatchCondition;
 
+   // queuse of tensors of the training and validation batches
    std::queue<std::unique_ptr<TMVA::Experimental::RTensor<float>>> fTrainingBatchQueue;
    std::queue<std::unique_ptr<TMVA::Experimental::RTensor<float>>> fValidationBatchQueue;
 
+   // number of training and validation batches in the queue
    std::size_t fNumTrainingBatchQueue;
    std::size_t fNumValidationBatchQueue;
 
+   // current batch that is loaded into memeory
    std::unique_ptr<TMVA::Experimental::RTensor<float>> fCurrentBatch;
 
+   // primary and secondary batches used to create batches from a chunk
    std::unique_ptr<TMVA::Experimental::RTensor<float>> fPrimaryLeftoverTrainingBatch;
    std::unique_ptr<TMVA::Experimental::RTensor<float>> fSecondaryLeftoverTrainingBatch;
 
@@ -104,6 +118,8 @@ public:
 
    /// \brief Return a batch of data as a unique pointer.
    /// After the batch has been processed, it should be destroyed.
+   /// \param[in] chunkTensor RTensor with the data from the chunk
+   /// \param[in] idxs Index of batch in the chunk
    /// \return Training batch
    std::unique_ptr<TMVA::Experimental::RTensor<float>>
    CreateBatch(TMVA::Experimental::RTensor<float> &chunkTensor, std::size_t idxs)
@@ -116,6 +132,9 @@ public:
       return batch;
    }
 
+   
+   /// \brief Loading the training batch from the queue
+   /// \return Training batch
    TMVA::Experimental::RTensor<float> GetTrainBatch()
    {
 
@@ -130,6 +149,8 @@ public:
       return *fCurrentBatch;
    }
 
+   /// \brief Loading the validation batch from the queue
+   /// \return Training batch
    TMVA::Experimental::RTensor<float> GetValidationBatch()
    {
 
@@ -144,6 +165,11 @@ public:
       return *fCurrentBatch;
    }
 
+   /// \brief Creating the training batches from a chunk and add them to the queue.
+   /// \param[in] chunkTensor RTensor with the data from the chunk
+   /// \param[in] lastbatch Check if the batch in the chunk is the last one
+   /// \param[in] leftoverBatchSize Size of the leftover batch in the training dataset
+   /// \param[in] dromRemainder Bool to drop the remainder batch or not
    void CreateTrainingBatches(TMVA::Experimental::RTensor<float> &chunkTensor, int lastbatch,
                               std::size_t leftoverBatchSize, bool dropRemainder)
    {
@@ -151,27 +177,33 @@ public:
       std::size_t Batches = ChunkSize / fBatchSize;
       std::size_t LeftoverBatchSize = ChunkSize % fBatchSize;
 
+      // create a vector of batches
       std::vector<std::unique_ptr<TMVA::Experimental::RTensor<float>>> batches;
 
+      // fill the full batches from the chunk into a vector
       for (std::size_t i = 0; i < Batches; i++) {
          // Fill a batch
          batches.emplace_back(CreateBatch(chunkTensor, i));
       }
 
+      // copy the remaining entries from the chunk into a leftover batch
       TMVA::Experimental::RTensor<float> LeftoverBatch({LeftoverBatchSize, fNumColumns});
       std::copy(chunkTensor.GetData() + (Batches * fBatchSize * fNumColumns),
                 chunkTensor.GetData() + (Batches * fBatchSize * fNumColumns + LeftoverBatchSize * fNumColumns),
                 LeftoverBatch.GetData());
 
+      // calculate how many empty slots are left in fPrimaryLeftoverTrainingBatch
       std::size_t PrimaryLeftoverSize = (*fPrimaryLeftoverTrainingBatch).GetShape()[0];
       std::size_t emptySlots = fBatchSize - PrimaryLeftoverSize;
 
+      // copy LeftoverBatch to end of fPrimaryLeftoverTrainingBatch
       if (emptySlots >= LeftoverBatchSize) {
          (*fPrimaryLeftoverTrainingBatch) =
             (*fPrimaryLeftoverTrainingBatch).Resize({PrimaryLeftoverSize + LeftoverBatchSize, fNumColumns});
          std::copy(LeftoverBatch.GetData(), LeftoverBatch.GetData() + (LeftoverBatchSize * fNumColumns),
                    fPrimaryLeftoverTrainingBatch->GetData() + (PrimaryLeftoverSize * fNumColumns));
 
+         // copy LeftoverBatch to end of fPrimaryLeftoverTrainingBatch and add it to the batch vector
          if (emptySlots == LeftoverBatchSize) {
             auto copy =
                std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>{fBatchSize, fNumColumns});
@@ -179,32 +211,43 @@ public:
                       fPrimaryLeftoverTrainingBatch->GetData() + (fBatchSize * fNumColumns), copy->GetData());
             batches.emplace_back(std::move(copy));
 
+            // reset fPrimaryLeftoverTrainingBatch and fSecondaryLeftoverTrainingBatch
             *fPrimaryLeftoverTrainingBatch = *fSecondaryLeftoverTrainingBatch;
             fSecondaryLeftoverValidationBatch =
                std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>{0, fNumColumns});
          }
       }
 
+      // copy LeftoverBatch to both fPrimaryLeftoverTrainingBatch and fSecondaryLeftoverTrainingBatch
       else if (emptySlots < LeftoverBatchSize) {
+         // copy the first part of LeftoverBatch to end of fPrimaryLeftoverTrainingBatch 
          (*fPrimaryLeftoverTrainingBatch) = (*fPrimaryLeftoverTrainingBatch).Resize({fBatchSize, fNumColumns});
          std::copy(LeftoverBatch.GetData(), LeftoverBatch.GetData() + (emptySlots * fNumColumns),
                    fPrimaryLeftoverTrainingBatch->GetData() + (PrimaryLeftoverSize * fNumColumns));
 
+         // copy the last part of LeftoverBatch to the end of fSecondaryLeftoverTrainingBatch
          (*fSecondaryLeftoverTrainingBatch) =
             (*fSecondaryLeftoverTrainingBatch).Resize({LeftoverBatchSize - emptySlots, fNumColumns});
          std::copy(LeftoverBatch.GetData() + (emptySlots * fNumColumns),
                    LeftoverBatch.GetData() + (LeftoverBatchSize * fNumColumns),
                    fSecondaryLeftoverTrainingBatch->GetData());
+         
+         // add fPrimaryLeftoverTrainingBatch to the batch vector
          auto copy =
             std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>{fBatchSize, fNumColumns});
          std::copy(fPrimaryLeftoverTrainingBatch->GetData(),
                    fPrimaryLeftoverTrainingBatch->GetData() + (fBatchSize * fNumColumns), copy->GetData());
          batches.emplace_back(std::move(copy));
+         
+         // exchange fPrimaryLeftoverTrainingBatch and fSecondaryLeftoverValidationBatch
          *fPrimaryLeftoverTrainingBatch = *fSecondaryLeftoverTrainingBatch;
+         
+         // restet fSecondaryLeftoverValidationBatch
          fSecondaryLeftoverValidationBatch =
             std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>{0, fNumColumns});
       }
 
+      // copy the content of fPrimaryLeftoverTrainingBatch to the leftover batch from the chunk
       if (lastbatch == 1) {
 
          if (dropRemainder == false && leftoverBatchSize > 0) {
@@ -221,11 +264,17 @@ public:
             std::make_unique<TMVA::Experimental::RTensor<float>>(std::vector<std::size_t>{0, fNumColumns});
       }
 
+      // append the batches from the batch vector from the chunk to the training batch queue
       for (std::size_t i = 0; i < batches.size(); i++) {
          fTrainingBatchQueue.push(std::move(batches[i]));
       }
    }
-
+   
+   /// \brief Creating the validation batches from a chunk and adding them to the queue
+   /// \param[in] chunkTensor RTensor with the data from the chunk
+   /// \param[in] lastbatch Check if the batch in the chunk is the last one
+   /// \param[in] leftoverBatchSize Size of the leftover batch in the validation dataset
+   /// \param[in] dromRemainder Bool to drop the remainder batch or not
    void CreateValidationBatches(TMVA::Experimental::RTensor<float> &chunkTensor, std::size_t lastbatch,
                                 std::size_t leftoverBatchSize, bool dropRemainder)
    {
