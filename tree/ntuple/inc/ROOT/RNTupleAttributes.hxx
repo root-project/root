95300788a5c4ee7b0632058ea50d6d8dd7cfad8c
silverweed <giacomo.parolini@cern.ch>
2025-07-22 15:20:38 +0200

    [ntuple] Merge RNTupleAttrEntry and RNTupleAttributes

--- tree/ntuple/inc/ROOT/RNTupleAttributes.hxx
+++ tree/ntuple/inc/ROOT/RNTupleAttributes.hxx
@@ -22,7 +22,6 @@
 
 #include <ROOT/REntry.hxx>
 #include <ROOT/RNTupleFillContext.hxx>
-#include <ROOT/RNTupleAttrEntry.hxx>
 
 namespace ROOT {
 
@@ -30,6 +29,111 @@ class RNTupleModel;
 class RNTuple;
 
 namespace Experimental {
+
+class RNTupleAttrSetWriter;
+
+namespace Internal {
+
+struct RNTupleAttrEntryPair {
+   REntry &fMetaEntry;
+   REntry &fScopedEntry;
+   ROOT::RNTupleModel &fMetaModel;
+
+   std::size_t Append();
+   ROOT::DescriptorId_t GetModelId() const { return fMetaEntry.GetModelId(); }
+};
+
+namespace RNTupleAttributes {
+
+const std::string kRangeStartName = "_rangeStart";
+const std::string kRangeLenName = "_rangeLen";
+
+} // namespace RNTupleAttributes
+
+} // namespace Internal
+
+class RNTupleAttrRange final {
+   ROOT::NTupleSize_t fStart = 0;
+   ROOT::NTupleSize_t fLength = 0;
+
+   RNTupleAttrRange(ROOT::NTupleSize_t start, ROOT::NTupleSize_t length) : fStart(start), fLength(length) {}
+
+public:
+   static RNTupleAttrRange FromStartLength(ROOT::NTupleSize_t start, ROOT::NTupleSize_t length)
+   {
+      return RNTupleAttrRange{start, length};
+   }
+
+   /// Creates an AttributeRange from [start, end), where `end` is one past the last valid entry of the range
+   /// (`FromStartEnd(0, 10)` will create a range whose last valid index is 9).
+   static RNTupleAttrRange FromStartEnd(ROOT::NTupleSize_t start, ROOT::NTupleSize_t end)
+   {
+      R__ASSERT(end >= start);
+      return RNTupleAttrRange{start, end - start};
+   }
+
+   RNTupleAttrRange() = default;
+
+   /// Returns the first valid entry index in the range. Returns nullopt if the range has zero length.
+   std::optional<ROOT::NTupleSize_t> First() const { return fLength ? std::make_optional(fStart) : std::nullopt; }
+   /// Returns the start of the range. Note that this is *not* a valid index in the range if the range has zero length.
+   ROOT::NTupleSize_t Start() const { return fStart; }
+   /// Returns the last valid entry index in the range. Returns nullopt if the range has zero length.
+   std::optional<ROOT::NTupleSize_t> Last() const
+   {
+      return fLength ? std::make_optional(fStart + fLength - 1) : std::nullopt;
+   }
+   /// Returns one past the last valid index of the range, equal to `Start() + Length()`.
+   ROOT::NTupleSize_t End() const { return fStart + fLength; }
+   ROOT::NTupleSize_t Length() const { return fLength; }
+
+   /// Returns the pair { firstEntryIdx, lastEntryIdx } (inclusive). Returns nullopt if the range has zero length.
+   std::optional<std::pair<ROOT::NTupleSize_t, ROOT::NTupleSize_t>> GetFirstLast() const
+   {
+      return fLength ? std::make_optional(std::make_pair(fStart, fStart + fLength - 1)) : std::nullopt;
+   }
+   /// Returns the pair { start, length }.
+   std::pair<ROOT::NTupleSize_t, ROOT::NTupleSize_t> GetStartLength() const { return {Start(), Length()}; }
+};
+
+/// A range used for writing. It has a well-defined start but not a length/end yet.
+/// It is artificially made non-copyable in order to clarify the semantics of Begin/CommitRange.
+/// For the same reason, it can only be created by the AttrSetWriter.
+class RNTupleAttrPendingRange final {
+   friend class ROOT::Experimental::RNTupleAttrSetWriter;
+
+   ROOT::NTupleSize_t fStart = 0;
+   ROOT::DescriptorId_t fModelId = kInvalidDescriptorId;
+
+   explicit RNTupleAttrPendingRange(ROOT::NTupleSize_t start, ROOT::DescriptorId_t modelId)
+      : fStart(start), fModelId(modelId)
+   {
+   }
+
+public:
+   RNTupleAttrPendingRange(const RNTupleAttrPendingRange &) = delete;
+   RNTupleAttrPendingRange &operator=(const RNTupleAttrPendingRange &) = delete;
+
+   // NOTE: explicitly implemented to make sure that 'other' gets invalidated upon move.
+   RNTupleAttrPendingRange(RNTupleAttrPendingRange &&other) { *this = std::move(other); }
+
+   // NOTE: explicitly implemented to make sure that 'other' gets invalidated upon move.
+   RNTupleAttrPendingRange &operator=(RNTupleAttrPendingRange &&other)
+   {
+      std::swap(fStart, other.fStart);
+      std::swap(fModelId, other.fModelId);
+      return *this;
+   }
+
+   ROOT::NTupleSize_t Start() const
+   {
+      if (fModelId == kInvalidDescriptorId)
+         throw ROOT::RException(R__FAIL("Tried to commit an already-committed attribute range."));
+      return fStart;
+   }
+
+   ROOT::DescriptorId_t GetModelId() const { return fModelId; }
+};
 
 // clang-format off
 /**
