2e4d57cdd7c41a1c3e3386b97732d41fa9c192d0
Philippe Canal <pcanal@fnal.gov>
2025-07-23 17:10:04 +0000

    roottest: revive, extend simple nested map dict test

--- roottest/cling/stl/map/testing.C
+++ roottest/cling/stl/map/testing.C
@@ -1,22 +1,14 @@
-#include <iostream>
-#include <fstream>
-#include <map>
-
-typedef std::map<int, int> nmap;
-typedef std::map<int, nmap > runnmap;
-
-class test {
-         public:
-                 test() {};
-                 ~test() {};
-         private:
-                 runnmap _lbn;
-};
-
-void testing() {
-  test t;
-  
+void testing()
+{
+   test t;
+   auto cl = TClass::GetClass(typeid(t));
+   cl->GetStreamerInfo()->ls("noaddr");
+   auto el = cl->GetStreamerInfo()->GetElement(0);
+   auto collcl = el->GetClass();
+   if (!collcl)
+      std::cout << "Error: missing TClass for nested map\n";
+   else if (!collcl->GetCollectionProxy())
+      std::cout << "Error: missing collection proxy for nested map\n";
+   else if (collcl->GetCollectionProxy()->GetProperties() & TVirtualCollectionProxy::kIsEmulated)
+      std::cout << "Error: collection proxy for nested map is emulated\n";
 }
-#ifdef __CINT__
-#pragma link C++ class test+;
-#endif
