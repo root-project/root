e1c1f4c6e40e3ac72b726eadd38caf5f46cd01c4
Silia Taider <siliataider@gmail.com>
2025-07-28 18:38:31 +0200

    [Python][RDF] Ruff lint and format

--- bindings/pyroot/pythonizations/test/rdf_define_pyz.py
+++ bindings/pyroot/pythonizations/test/rdf_define_pyz.py
@@ -15,65 +15,71 @@ class PyDefine(unittest.TestCase):
 
     def test_with_dtypes(self):
         """
-        Tests the pythonized define with all the numba declare datatypes and 
+        Tests the pythonized define with all the numba declare datatypes and
         """
-        numba_declare_dtypes = ['float', 'double', 'int', 'unsigned int', 'long', 'unsigned long', 'bool']
+        numba_declare_dtypes = ["float", "double", "int", "unsigned int", "long", "unsigned long", "bool"]
         rdf = ROOT.RDataFrame(10)
         for type in numba_declare_dtypes:
-            col_name = "col_" + type.replace(" ","") 
+            col_name = "col_" + type.replace(" ", "")
             rdf = rdf.Define(col_name, f"({type}) rdfentry_")
-            rdf = rdf.Define(col_name + "_arr", lambda col: np.array([col,col]), [col_name])
+            rdf = rdf.Define(col_name + "_arr", lambda col: np.array([col, col]), [col_name])
             arr = np.arange(0, 10)
-            if type == 'bool':
-                arr = np.array(arr, dtype='bool')
+            if type == "bool":
+                arr = np.array(arr, dtype="bool")
             flag1 = np.array_equal(rdf.AsNumpy()[col_name], arr)
             flag2 = True
             for idx, entry in enumerate(rdf.AsNumpy()[col_name + "_arr"]):
                 if not (entry[0] == arr[idx] and entry[1] == arr[idx]):
                     flag2 = False
             self.assertTrue(flag1 and flag2)
-    
+
     def test_define_overload1(self):
         rdf = ROOT.RDataFrame(10).Define("x", "rdfentry_")
-        rdf = rdf.Define("x2", lambda y: y*y, ["x"])
+        rdf = rdf.Define("x2", lambda y: y * y, ["x"])
         arr = np.arange(0, 10)
-        flag = np.array_equal(rdf.AsNumpy()["x2"], arr*arr)
+        flag = np.array_equal(rdf.AsNumpy()["x2"], arr * arr)
         self.assertTrue(flag)
 
     def test_define_overload2(self):
         rdf = ROOT.RDataFrame(10).Define("x", "rdfentry_")
-        rdf = rdf.Define("x2", lambda x: x*x)
+        rdf = rdf.Define("x2", lambda x: x * x)
         arr = np.arange(0, 10)
-        flag = np.array_equal(rdf.AsNumpy()["x2"], arr*arr)
+        flag = np.array_equal(rdf.AsNumpy()["x2"], arr * arr)
         self.assertTrue(flag)
 
     def test_define_extra_args(self):
         rdf = ROOT.RDataFrame(10).Define("x", "rdfentry_")
+
         def x_y(x, y):
-            return x*y
-        rdf = rdf.Define("x_y", x_y , extra_args = {"y": 0.5})
+            return x * y
+
+        rdf = rdf.Define("x_y", x_y, extra_args={"y": 0.5})
         arr = np.arange(0, 10)
-        flag = np.array_equal(rdf.AsNumpy()["x_y"], arr*0.5)
+        flag = np.array_equal(rdf.AsNumpy()["x_y"], arr * 0.5)
         self.assertTrue(flag)
 
     def test_capture_from_scope(self):
         rdf = ROOT.RDataFrame(10).Define("x", "rdfentry_")
         y = 0.5
+
         def x_times_y(x):
-            return x*y
-        rdf = rdf.Define("x_y", x_times_y )
+            return x * y
+
+        rdf = rdf.Define("x_y", x_times_y)
         arr = np.arange(0, 10)
-        flag = np.array_equal(rdf.AsNumpy()["x_y"], arr*0.5)
+        flag = np.array_equal(rdf.AsNumpy()["x_y"], arr * 0.5)
         self.assertTrue(flag)
-        
+
     def test_arrays(self):
         rdf = ROOT.RDataFrame(5).Define("x", "rdfentry_")
-        rdf = rdf.Define("x_arr", lambda x:  np.array([x, x]))
+        rdf = rdf.Define("x_arr", lambda x: np.array([x, x]))
+
         def norm(x_arr):
-            return np.sqrt(x_arr[0]**2 + x_arr[1]**2)
+            return np.sqrt(x_arr[0] ** 2 + x_arr[1] ** 2)
+
         rdf = rdf.Define("mag", norm)
         arr = np.arange(0, 5)
-        arr = np.sqrt(arr*arr + arr*arr )
+        arr = np.sqrt(arr * arr + arr * arr)
         flag = np.array_equal(rdf.AsNumpy()["mag"], arr)
         self.assertTrue(flag)
 
@@ -94,8 +100,8 @@ class PyDefine(unittest.TestCase):
         rdf = ROOT.RDataFrame(5)
         rdf2 = rdf.Define("x", f, ["rdfentry_"])
 
-        for x,y in zip(rdf2.Take['ULong64_t']("rdfentry_"), rdf2.Take['ULong64_t']("x")):
-           self.assertEqual(x*x, y)
+        for x, y in zip(rdf2.Take["ULong64_t"]("rdfentry_"), rdf2.Take["ULong64_t"]("x")):
+            self.assertEqual(x * x, y)
 
     def test_std_function(self):
         """
@@ -110,8 +116,8 @@ class PyDefine(unittest.TestCase):
         rdf = ROOT.RDataFrame(5)
         rdf2 = rdf.Define("x", ROOT.myfun, ["rdfentry_"])
 
-        for x,y in zip(rdf2.Take['ULong64_t']("rdfentry_"), rdf2.Take['ULong64_t']("x")):
-           self.assertEqual(x*x, y)
+        for x, y in zip(rdf2.Take["ULong64_t"]("rdfentry_"), rdf2.Take["ULong64_t"]("x")):
+            self.assertEqual(x * x, y)
 
     def test_cpp_free_function(self):
         """
@@ -170,14 +176,11 @@ class PyDefine(unittest.TestCase):
                 """,
                 "coltype": "MyStruct2",
                 "define_args": ["s_col", "int_col"],
-                "setup_columns": {
-                    "s_col": "MyStruct2()",
-                    "int_col": "(int)rdfentry_"
-                },
+                "setup_columns": {"s_col": "MyStruct2()", "int_col": "(int)rdfentry_"},
                 "callable": lambda: ROOT.my_free_function_two_args,
                 "extract_fn": lambda s: s.value,
                 "expected_fn": lambda i: i,
-            }
+            },
         ]
 
         for case in test_cases:
@@ -236,5 +239,5 @@ class PyDefine(unittest.TestCase):
             self.assertEqual(x, y)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
