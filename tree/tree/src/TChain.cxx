c54c51831df4ecdd93ebe058732edab3bd20a98d
ferdymercury <ferdymercury@users.noreply.github.com>
2025-07-29 22:58:02 +0200

    [tree] major and minor indices to long64, it was forgotten to change from 32bit of old interfaces (#14967)
    Fixes https://its.cern.ch/jira/browse/ROOT-9028
    
    [treeindex] print error when requesting out of bonds combination 31bit restriction was old, now they have separate 64-bit registers better document old2new
    
    [roottest] adapt to new 64bit interface
    
    [test] disable tests reaching the limits of ulong64_t
    since the implicit conversion to long double later leads to platform-dependent test failures
    
    [treeindex][nfc] mention that warning is printed at runtime when overflow detected
    
    ---------
    
    Co-authored-by: Philippe Canal <pcanal@fnal.gov>

--- tree/tree/src/TChain.cxx
+++ tree/tree/src/TChain.cxx
@@ -972,13 +972,14 @@ Long64_t TChain::GetEntryNumber(Long64_t entry) const
 ////////////////////////////////////////////////////////////////////////////////
 /// Return entry corresponding to major and minor number.
 ///
-/// The function returns the total number of bytes read.
+/// The function returns the total number of bytes read; -1 if entry not found.
 /// If the Tree has friend trees, the corresponding entry with
 /// the index values (major,minor) is read. Note that the master Tree
 /// and its friend may have different entry serial numbers corresponding
 /// to (major,minor).
+/// \note See TTreeIndex::GetEntryNumberWithIndex for information about the maximum values accepted for major and minor
 
-Int_t TChain::GetEntryWithIndex(Int_t major, Int_t minor)
+Int_t TChain::GetEntryWithIndex(Long64_t major, Long64_t minor)
 {
    Long64_t serial = GetEntryNumberWithIndex(major, minor);
    if (serial < 0) return -1;
