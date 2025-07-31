65af8e9ba2b4e2cef5e2f075a065b9a1b2aecd1d
Silia Taider <siliataider@gmail.com>
2025-07-31 14:03:43 +0200

    [Python][RDF] Add support for std::vector in numba jitted functions

--- bindings/pyroot/pythonizations/python/ROOT/_numbadeclare.py
+++ bindings/pyroot/pythonizations/python/ROOT/_numbadeclare.py
@@ -33,9 +33,37 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
     except:
         raise Exception('Failed to import cffi')
     import re, sys
+    from typing import Union
 
     if hasattr(nb, 'version_info') and nb.version_info >= (0, 54):
         import cppyy.numba_ext
+
+    CONTAINER_TYPES = {
+        'RVec': {
+            'match_pattern': r'RVec\w+|RVec<[\w\s]+>',
+            'cpp_name': 'ROOT::RVec',
+        },
+        'std::vector': {
+            'match_pattern': r'std::vector<[\w\s]+>',
+            'cpp_name': 'std::vector',
+        },
+        'std::array': {
+            'match_pattern': r'std::array<[\w\s,<>]+>',
+            'cpp_name': 'std::array',
+        },
+    }
+
+    def get_container_type(cpp_type: str) -> Union[str, None]:
+        return next((name for name in CONTAINER_TYPES if name in cpp_type), None)
+
+    def is_container_type(cpp_type: str) -> bool:
+        return get_container_type(cpp_type) is not None
+
+    def get_container_cpp_name(cpp_type: str) -> Union[str, None]:
+        container_type = get_container_type(cpp_type)
+        if container_type is None:
+            return None
+        return CONTAINER_TYPES[container_type]['cpp_name']
 
     # Normalize input types by stripping ROOT and VecOps namespaces from input types
     def normalize_typename(t):
@@ -48,7 +76,7 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
     return_type = normalize_typename(return_type) if return_type is not None else None
 
     # Helper functions to determine types
-    def get_inner_type(t):
+    def get_inner_type(t: str, with_dims: bool = False) -> str:
         '''
         Get inner typename of a templated C++ typename
         '''
@@ -72,7 +100,11 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
                         'Unrecognized type {}. Valid shorthand aliases of RVec are {}'
                         .format(t, list('RVec' + i for i in typemap.keys())))
             g = re.match('(.*)<(.*)>', t).groups(0)
-            return g[1]
+            inner_type = g[1]
+            # Handle std::array<T, N> by splitting the inner type, if requested
+            if ',' in inner_type and not with_dims:
+                inner_type = inner_type.split(',')[0].strip()
+            return inner_type
         except:
             raise Exception('Failed to extract template argument of type {}'.format(t))
 
@@ -94,7 +126,7 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
         if t in typemap:
             return typemap[t]
         raise Exception(
-                'Type {} is not supported for jitting with numba. Valid fundamental types and RVecs thereof are {}'.format(
+                'Type {} is not supported for jitting with numba. Valid fundamental types and container types (RVec, std::vector, std::array) thereof are {}'.format(
                     t, list(typemap.keys())))
 
     def get_c_signature(input_types, return_type):
@@ -105,12 +137,12 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
         '''
         c_input_types = []
         for t in input_types:
-            if 'RVec' in t:
+            if is_container_type(t):
                 c_input_types += [nb.types.CPointer(get_numba_type(get_inner_type(t))), nb.int32]
             else:
                 c_input_types.append(get_numba_type(t))
-        if 'RVec' in return_type:
-            # We return an RVec through pointers as part of the input arguments. Note that the
+        if is_container_type(return_type):
+            # We return an container through pointers as part of the input arguments. Note that the
             # pointer type in numba is always an int64 and is later on cast in C++ to the correct type.
             # In addition, we provide the size of the data type of the array for preallocating memory of
             # the returned array.
@@ -129,12 +161,12 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
         '''
         nb_input_types = []
         for t in input_types:
-            if 'RVec' in t:
+            if is_container_type(t):
                 nb_input_types.append(get_numba_type(get_inner_type(t))[:])
             else:
                 nb_input_types.append(get_numba_type(t))
         if return_type is not None:
-            if 'RVec' in return_type:
+            if is_container_type(return_type):
                 nb_return_type = get_numba_type(get_inner_type(return_type))[:]
             else:
                 nb_return_type = get_numba_type(return_type)
@@ -142,20 +174,23 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
             nb_return_type = None
         return nb_return_type, nb_input_types
 
-    def add_rvec_input_type_ref(input_types_ref, const_mod, rvect):
+    def add_container_input_type_ref(input_types_ref: list, const_mod: str, container_t: str) -> None:
         '''
-        Construct the type of an RVec input parameter for its use in the C++
+        Construct the type of a container input parameter for its use in the C++
         wrapper function signature.
         '''
-        tref = '{}ROOT::{}&'.format(const_mod, rvect)
+        container_base = get_container_type(container_t)
+        cpp_name = get_container_cpp_name(container_t)
+        full_type = container_t.replace(container_base, cpp_name, 1)
+        tref = f"{const_mod}{full_type}&"
         input_types_ref.append(tref)
 
-    def add_rvec_func_ptr_input_type(func_ptr_input_types, const_mod, rvect):
+    def add_container_func_ptr_input_type(func_ptr_input_types: list, const_mod: str, container_t: str) -> None:
         '''
-        Construct the type of an RVec input parameter for its use in the cast
+        Construct the type of a container input parameter for its use in the cast
         of the function pointer of the jitted Python wrapper.
         '''
-        innert = get_inner_type(rvect)
+        innert = get_inner_type(container_t)
         if innert == 'bool':
             # Special treatment for bool: In numpy, bools have 1 byte
             innert = 'char'
@@ -203,21 +238,21 @@ def _NumbaDeclareDecorator(input_types, return_type = None, name=None):
 
         # Define signature
         pywrapper_signature = [
-                'ptr_{0}, size_{0}'.format(i) if 'RVec' in t else 'x_{}'.format(i) \
+                'ptr_{0}, size_{0}'.format(i) if is_container_type(t) else 'x_{}'.format(i) \
                         for i, t in enumerate(input_types)]
-        if 'RVec' in return_type:
-            # If we return an RVec, we return via pointer the pointer of the allocated data,
+        if is_container_type(return_type):
+            # If we return a container, we return via pointer the pointer of the allocated data,
             # the size in elements. In addition, we provide the size of the datatype in bytes.
             pywrapper_signature += ['ptrptr_r, ptrsize_r']
 
         # Define arguments for jit function
         pywrapper_args_def = [
-                'x_{0} = nb.carray(ptr_{0}, (size_{0},))'.format(i) if 'RVec' in t else 'x_{}'.format(i) \
+                'x_{0} = nb.carray(ptr_{0}, (size_{0},))'.format(i) if is_container_type(t) else 'x_{}'.format(i) \
                         for i, t in enumerate(input_types)]
         pywrapper_args = ['x_{}'.format(i) for i in range(len(input_types))]
 
         # Define return operation
-        if 'RVec' in return_type:
+        if is_container_type(return_type):
             innert = get_inner_type(return_type)
             dtypesize = 1 if innert == 'bool' else int(get_numba_type(innert).bitwidth / 8)
             pywrapper_return = '\n    '.join([
@@ -238,7 +273,7 @@ def pywrapper({SIGNATURE}):
     """
     Wrapper function for the jitted Python callable with special treatment of arrays
     """
-    # If an RVec is given, define numba carray wrapper for the input types
+    # If a container is given, define numba carray wrapper for the input types
     {ARGS_DEF}
     # Call the jitted Python function
     r = nbjit({ARGS})
@@ -260,7 +295,7 @@ def pywrapper({SIGNATURE}):
         C = ffi.dlopen(None)
         glob['malloc'] = C.malloc
 
-        if 'RVec' in return_type:
+        if is_container_type(return_type):
             glob['dtype_r'] = get_numba_type(get_inner_type(return_type))
 
         # Execute the pywrapper code and generate the wrapper function
@@ -301,25 +336,26 @@ def pywrapper({SIGNATURE}):
         input_types_ref = []
         func_ptr_input_types = []
         for t in input_types:
-            m = re.match(r'\s*(const\s+)?(RVec\w+|RVec<[\w\s]+>)', t)
+            container_match_group = '|'.join(entry['match_pattern'] for entry in CONTAINER_TYPES.values())
+            m = re.match(rf'\s*(const\s+)?({container_match_group})', t)
             if m:
                 const_mod = '' if m.group(1) is None else 'const '
-                rvect = m.group(2)
+                container_t = m.group(2)
 
-                add_rvec_input_type_ref(input_types_ref, const_mod, rvect)
-                add_rvec_func_ptr_input_type(func_ptr_input_types, const_mod, rvect)
+                add_container_input_type_ref(input_types_ref, const_mod, container_t)
+                add_container_func_ptr_input_type(func_ptr_input_types, const_mod, container_t)
             else:
                 input_types_ref.append(t)
                 func_ptr_input_types.append(t)
 
         input_signature = ', '.join('{} x_{}'.format(t, i) for i, t in enumerate(input_types_ref))
 
-        if 'RVec' in return_type:
+        if is_container_type(return_type):
             # See C++ wrapper code for the reason using these types
             innert = get_inner_type(return_type)
             func_ptr_input_types += ['{}**, long*'.format('char' if innert == 'bool' else innert)]
         func_ptr_type = '{RETURN_TYPE}(*)({INPUT_TYPES})'.format(
-                RETURN_TYPE='void*' if 'RVec' in return_type else return_type,
+                RETURN_TYPE='void*' if is_container_type(return_type) else return_type,
                 INPUT_TYPES=', '.join(func_ptr_input_types)
                 )
 
@@ -327,32 +363,53 @@ def pywrapper({SIGNATURE}):
         vecbool_conversion = []
         func_args = []
         for i, t in enumerate(input_types):
-            if 'RVec' in t:
+            if is_container_type(t):
                 func_args += ['x_{0}.data(), x_{0}.size()'.format(i)]
                 if get_inner_type(t) == 'bool':
-                    # Copy the RVec<bool> to a RVec<char> to match the numpy memory layout
+                    # Copy the container<bool> to a container<char> to match the numpy memory layout
                     func_args[-1] = func_args[-1].replace('x_', 'xb_')
-                    vecbool_conversion += ['ROOT::RVec<char> xb_{0} = x_{0};'.format(i)]
+                    vecbool_conversion += [f'{get_container_cpp_name(t)}<char> xb_{i} = x_{i};']
             else:
                 func_args += ['x_{}'.format(i)]
-        if 'RVec' in return_type:
+        if is_container_type(return_type):
             # See C++ wrapper code for the reason using these arguments
             func_args += ['&ptr, &size']
 
         # Define return operation
-        if 'RVec' in return_type:
+        if is_container_type(return_type):
             innert = get_inner_type(return_type)
-            if innert == 'bool': innert = 'char'
+            container_cpp = get_container_cpp_name(return_type)
+            inner_with_dims = get_inner_type(return_type, with_dims=True)
+
+            if innert == 'bool':
+                innert = 'char'
+                inner_with_dims = 'char' + inner_with_dims[4:]
+
+            # Default case: create container from pointer and size (e.g. std::vector, RVec)
+            copy_lines = [
+                f'{container_cpp}<{innert}> x_r(ptr, ptr + size);'
+            ]
+
+            # Special case: if inner type includes dimensions (e.g. std::array<T, N>)
+            # We construct the array then copy the data manually
+            if innert != inner_with_dims:
+                innert, dims = inner_with_dims.split(',')
+                copy_lines = [
+                    f'{container_cpp}<{innert}, {dims}> x_r;'
+                    f'std::copy(ptr, ptr + size, x_r.begin());'
+                ]
+
             return_op = '\n    '.join([
-                '// Because an RVec cannot take the ownership of external data, we have to copy the returned array',
+                '// Because a container type cannot take the ownership of external data, we have to copy the returned array',
                 'long size; // Size of the returned array',
-                '{}* ptr; // Pointer to the data of the returned array'.format(innert),
-                'funcptr({});'.format(', '.join(func_args)),
-                # TODO: Remove this copy as soon as RVec can adopt the ownership
-                'ROOT::RVec<{}> x_r(ptr, ptr + size);'.format(innert),
+                f'{innert}* ptr; // Pointer to the data of the returned array',
+                f'funcptr({", ".join(func_args)});',
+                # TODO: Remove this copy as soon as the container type can adopt the ownership
+                *copy_lines,
                 'free(ptr);',
-                # If we return a RVec<bool>, we rely here on the automatic conversion of RVec<char> to RVec<bool>
-                'return x_r;'])
+                # If we return a container_type<bool>, we rely here on the automatic conversion of container_type<char> to container_type<bool>
+                'return x_r;'
+            ])
         else:
             return_op = 'return funcptr({});'.format(', '.join(func_args))
 
@@ -365,7 +422,7 @@ namespace Numba {{
 {RETURN_TYPE} {FUNC_NAME}({INPUT_SIGNATURE}) {{
     // Create a function pointer from the jitted Python wrapper
     const auto funcptr = reinterpret_cast<{FUNC_PTR_TYPE}>({FUNC_PTR});
-    // Perform conversion of RVec<bool>
+    // Perform conversion of container_type<bool>
     {VECBOOL_CONVERSION}
     // Return the result
     {RETURN_OP}
