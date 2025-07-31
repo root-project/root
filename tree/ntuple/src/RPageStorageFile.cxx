0434eb3cf9dfe5301680f32835690730d2ebe156
silverweed <giacomo.parolini@cern.ch>
2025-07-31 15:12:12 +0200

    [ntuple] Allow merging unknown types via RNTupleMerger
    With this PR, we exploit field emulation to be able to successfully merge
    RNTuples containing unknown types. To do that we also need to ensure the
    StreamerInfo is loaded from the underlying files as that allows us to
    find the (emulated) TClass of all unknown types, which is currently
    needed to properly build the streamer info upon calling CommitDataset().
    
    Note that this can only work for TFile-based RNTuple sources, as for
    other sources we don't necessarily store the StreamerInfo of all types
    (we only store it for Streamer fields).

--- tree/ntuple/src/RPageStorageFile.cxx
+++ tree/ntuple/src/RPageStorageFile.cxx
@@ -670,3 +670,8 @@ ROOT::Internal::RPageSourceFile::LoadClusters(std::span<RCluster::RKey> clusterK
 
    return clusters;
 }
+
+void ROOT::Internal::RPageSourceFile::LoadStreamerInfo()
+{
+   fReader.LoadStreamerInfo();
+}
