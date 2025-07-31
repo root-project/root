6eef796fff204217628e6370ffbacbfd552ce95b
Sergey Linev <S.Linev@gsi.de>
2025-07-31 17:53:59 +0200

    [roottest] use cmake for root/tree/selector tests

--- roottest/root/tree/selector/run.C
+++ roottest/root/tree/selector/run.C
@@ -1,12 +1,13 @@
+void run()
 {
 // Avoid loading the library
 gInterpreter->UnloadLibraryMap("sel_C");
-new TFile("Event1.root");
-tree1 = T1;
-new TFile("Event2.root");
-tree2 = T2;
-new TFile("Event3.root");
-tree3 = T3;
+TFile::Open("Event1.root");
+auto tree1 = (TTree*) gFile->Get("T1");
+TFile::Open("Event2.root");
+auto tree2 = (TTree*) gFile->Get("T2");
+TFile::Open("Event3.root");
+auto tree3 = (TTree*) gFile->Get("T3");
 tree1->Process("sel.C","T1");
 tree2->Process("sel.C","T2");
 tree3->Process("sel.C","T3");
@@ -15,5 +16,4 @@ TSelector *sel = TSelector::GetSelector("sel.C");
 tree1->Process(sel,"T1");
 tree2->Process(sel,"T2");
 tree3->Process(sel,"T3");
-return 0;
 }
