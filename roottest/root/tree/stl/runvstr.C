2a5f57f3a57925d422715efd5faf3a3873d443f3
Sergey Linev <S.Linev@gsi.de>
2025-07-31 13:21:28 +0200

    [roottest] use cmake for root/tree/stl tests
    Extra rule to copy rootlogin.C script
    Remove unused mixing.ref file

--- roottest/root/tree/stl/runvstr.C
+++ roottest/root/tree/stl/runvstr.C
@@ -1,44 +1,43 @@
 #include "TFile.h"
 #include "TTree.h"
-#include "Riostream.h"
+#include <iostream>
 #include <vector>
 #include <string>
 
-void print(std::vector<std::string> &what) 
+void print(std::vector<std::string> &what)
 {
    cout << "Printing vector\n";
    for(unsigned int i=0;i<what.size(); ++i) {
-      cout << i << ":";
-      cout << what[i];
-      cout << '\t';
+      std::cout << i << ":";
+      std::cout << what[i];
+      std::cout << '\t';
    }
-   cout << endl;
+   std::cout << std::endl;
 }
 
 void runvstr() {
    std::vector<std::string> RProcess;
-   RProcess.reserve(1000); 
+   RProcess.reserve(1000);
 
-   string tmp("two");
-   cout << tmp << endl;
+   std::string tmp("two");
+   std::cout << tmp << std::endl;
    RProcess.push_back(tmp);
-   RProcess.push_back("123"); 
+   RProcess.push_back("123");
 
    print(RProcess);
 
-   TTree *t1 = new TTree("t1","t1"); 
-   t1 -> Branch("RProcess", &RProcess); 
+   TTree *t1 = new TTree("t1","t1");
+   t1 -> Branch("RProcess", &RProcess);
    t1->Fill();
 
    RProcess.clear();
    print(RProcess);
 
    t1->GetEntry(0);
-   
+
    print(RProcess);
 
    t1->Scan("RProcess.c_str()");
    t1->ResetBranchAddresses();
 }
 
-      
