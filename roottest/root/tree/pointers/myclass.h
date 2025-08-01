37a5c57125cad4614c827310bdabb707a04adfc6
Sergey Linev <S.Linev@gsi.de>
2025-08-01 11:26:19 +0200

    [rootest] activate in cmake root/tree/pointers
    Was not used before
    Run provided methods
    Use compression 0 to avoid difference on different platforms

--- roottest/root/tree/pointers/myclass.h
+++ roottest/root/tree/pointers/myclass.h
@@ -1,3 +1,6 @@
+#ifndef myclass_h
+#define myclass_h
+
 #include "TObject.h"
 
 class wrapper;
@@ -10,12 +13,12 @@ public:
 
    myclass() : myself(this),indirect(0),fValue(0) {}
    virtual ~myclass() {}
-      
+
    void set();
 
    void verify();
 
-   ClassDefOverride(myclass,1);
+   ClassDef(myclass,1);
 };
 
 class wrapper {
@@ -26,7 +29,7 @@ public:
    wrapper(myclass *p = 0) : fParent(p),fIndex(0) {}
    virtual ~wrapper() {}
 
-   ClassDefOverride(wrapper,1);
+   ClassDef(wrapper,1);
 };
 
 inline void myclass::set() {
@@ -50,64 +53,4 @@ inline void myclass::verify() {
    }
 }
 
-#include "TFile.h"
-#include "TTree.h"
-
-const char *defname = "myclass.root";
-
-void write(const char *filename = defname) 
-{
-   TFile f(filename,"RECREATE");
-   myclass *m = new myclass();
-   m->set();
-
-   f.WriteObject(m,"obj");
-
-   TTree *tree = new TTree("T","T");
-   // tree->Branch("split_1.","myclass",&m,32000,-1);
-   tree->Branch("split0.","myclass",&m,32000,0);
-   tree->Branch("split1.",&m,32000,1);
-   tree->Branch("split99.",&m,32000,99);
-
-   tree->Fill();
-   tree->Write();
-
-   f.Close();
-}
-
-void readobj(TTree *tree, const char *where)
-{
-   myclass *m = 0;
-   tree->SetBranchAddress(where,&m);
-   tree->GetEntry(0);
-   if (!m) {
-      fprintf(stdout,"reading failed in %s\n",where);
-   } else {
-      fprintf(stdout,"Verifying %s\n",where);
-      m->verify();
-   }
-   tree->ResetBranchAddresses();
-}
-
-void read(const char *filename = defname)
-{
-   TFile f(filename,"READ");
-   myclass *m;
-   f.GetObject("obj",m);
-   m->verify();
-
-   TTree *tree;
-   f.GetObject("T",tree);
-   tree->Print();
-
-   readobj(tree,"split0.");
-   readobj(tree,"split1.");
-   readobj(tree,"split99.");
-}
-
-   
-
-
-
-
-
+#endif
