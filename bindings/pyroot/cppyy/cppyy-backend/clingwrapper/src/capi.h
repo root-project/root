3521facc9015e75f1725c14b566b80b5305f330f
Jonas Rembser <jonas.rembser@cern.ch>
2025-08-03 13:03:42 +0200

    [Python] Add Cppyy backend API to check if constructor is explicit
    (cherry picked from commit bb2b0ccdea9044ddacbe5bc34694a0965651c8b7)

--- bindings/pyroot/cppyy/cppyy-backend/clingwrapper/src/capi.h
+++ bindings/pyroot/cppyy/cppyy-backend/clingwrapper/src/capi.h
@@ -202,6 +202,8 @@ extern "C" {
     int cppyy_is_destructor(cppyy_method_t);
     RPY_EXPORTED
     int cppyy_is_staticmethod(cppyy_method_t);
+    RPY_EXPORTED
+    int cppyy_is_explicit(cppyy_method_t);
 
     /* data member reflection information ------------------------------------- */
     RPY_EXPORTED
