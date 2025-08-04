2e0ace35afa3290e5dc4ad7700d7186a2bb5458e
Sergey Linev <S.Linev@gsi.de>
2025-08-04 10:10:44 +0200

    [roottest] use cmake for root/tree/address tests
    Build dictionary in convertIssues/ subdir and correspondent lib before running test
    Remove exe bit from many sources
    Remove rootlogon.C script - used only with runiobug.C, where necessary script load
    with ROOTEXE_OPTS

--- roottest/root/tree/addresses/relationsRun.C
+++ roottest/root/tree/addresses/relationsRun.C
@@ -1,8 +1,8 @@
 {
 
-if (!gSystem->CompileMacro("relations_load.C","k")) gApplication->Terminate(1);
-if (!gSystem->CompileMacro("relations_write.C","k")) gApplication->Terminate(1);
-if (!gSystem->CompileMacro("relations_read.C","k")) gApplication->Terminate(1);
+// if (!gSystem->CompileMacro("relations_load.C","k")) gApplication->Terminate(1);
+// if (!gSystem->CompileMacro("relations_write.C","k")) gApplication->Terminate(1);
+// if (!gSystem->CompileMacro("relations_read.C","k")) gApplication->Terminate(1);
 
 #ifdef ClingWorkAroundMissingDynamicScope
    gROOT->ProcessLine("Write(true); Read(true);");
