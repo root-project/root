286334180a4b5195d8b3d0aaa4453a263c3d2107
Sergey Linev <S.Linev@gsi.de>
2025-08-05 11:05:10 +0200

    [roottest] remove windows.h usage from utils.cc
    Allows to use this script on Windows
    Extra redirection of debug output is not really necessary
    Use c++ includes

--- roottest/scripts/utils.cc
+++ roottest/scripts/utils.cc
@@ -1,18 +1,14 @@
 // Utilities to be used by some scripts.
 // To load them up copy the rootlogon_template.C into the current directory.
 
-#ifdef WIN32
-#include <windows.h>
-#endif
-
-#include <stdio.h>
+#include <cstdio>
 #include <cstdarg>
+#include <iostream>
 #include "snprintf.h"
 
 #include "TError.h"
 #include "TSystem.h"
 
-#include "Riostream.h"
 
 // This redirect the root warning to be on stdout instead of stderr.
 
@@ -42,10 +38,6 @@ again:
    va_end(arg_ptr);
 
    fprintf(stdout, "%s", buf);
-
-#ifdef WIN32
-   ::OutputDebugString(buf);
-#endif
 }
 
 //______________________________________________________________________________
@@ -95,7 +87,7 @@ void SetROOTMessageToStdout() {
    SetErrorHandler(StdoutErrorHandler);
 }
 
-Bool_t CompareLines(const char*left,const char*right,UInt_t maxlen) 
+Bool_t CompareLines(const char*left,const char*right,UInt_t maxlen)
 {
    // Compare two string, ignore the difference between MSDOS and linux style
    // of line ending
@@ -105,17 +97,17 @@ Bool_t CompareLines(const char*left,const char*right,UInt_t maxlen)
    while (i<maxlen && left[i] && right[i]) {
       if (left[i]!=right[i]) {
          if (left[i]==0x0a && left[i+1]==0) {
-            if ((right[i]==0x0d && right[i+1]==0x0a && right[i+2] == 0) || 
+            if ((right[i]==0x0d && right[i+1]==0x0a && right[i+2] == 0) ||
                 (right[i]==0x0a && right[i+1]==0) || right[i]==0) {
                return kTRUE;
             }
          }
          if (right[i]==0x0a && right[i+1]==0) {
-            if ((left[i]==0x0d && left[i+1]==0x0a && left[i+2] == 0) || 
+            if ((left[i]==0x0d && left[i+1]==0x0a && left[i+2] == 0) ||
                 (left[i]==0x0a && left[i+1]==0) || left[i]==0) {
                return kTRUE;
             }
-            
+
          }
          return kFALSE;
      }
@@ -128,21 +120,21 @@ Bool_t ComparePostscript(const char *from, const char *to)
 {
    FILE *left = fopen(from,"r");
    FILE *right = fopen(to,"r");
-   
+
    if (left==0) std::cout << "Could not open " << from << std::endl;
    if (right==0) std::cout << "Could not open " << to << std::endl;
    if (left==0 || right==0) return false;
 
    char leftbuffer[256];
    char rightbuffer[256];
-   
+
    char *lvalue,*rvalue;
-   
+
    Bool_t areEqual = kTRUE;
    do {
       lvalue = fgets(leftbuffer, sizeof(leftbuffer), left);
       rvalue = fgets(rightbuffer, sizeof(rightbuffer), right);
-      
+
       if (lvalue&&rvalue) {
          if (strstr(lvalue,"%%CreationDate")
              || strstr(lvalue,"%%Creator")) {
@@ -153,11 +145,11 @@ Bool_t ComparePostscript(const char *from, const char *to)
       }
       if (lvalue&&!rvalue) areEqual = kFALSE;
       if (rvalue&&!lvalue) areEqual = kFALSE;
-      
+
    } while(areEqual && lvalue && rvalue);
-   
+
    fclose(left);
    fclose(right);
-   
+
    return areEqual;
 }
