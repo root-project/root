70e76d5f70e268dd38750fa953e5948b7c3c78df
Sergey Linev <S.Linev@gsi.de>
2025-08-05 14:18:18 +0200

    [roottest] use cmake for root/treeformula/array/runArrayTest.C

--- roottest/root/treeformula/array/runFillTree.C
+++ roottest/root/treeformula/array/runFillTree.C
@@ -1,6 +1,6 @@
 {
-if (!gSystem->CompileMacro("Data.cxx","k")) gApplication->Terminate(1);
-if (!gSystem->CompileMacro("fillTree.cxx","k")) gApplication->Terminate(1);
+// if (!gSystem->CompileMacro("Data.cxx","k")) gApplication->Terminate(1);
+// if (!gSystem->CompileMacro("fillTree.cxx","k")) gApplication->Terminate(1);
 #ifdef ClingWorkAroundMissingDynamicScope
    gROOT->ProcessLine("fillTree();");
 #else
