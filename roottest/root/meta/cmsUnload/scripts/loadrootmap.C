701dd6753aa426f2428001b6c5911458230f6d67
Sergey Linev <S.Linev@gsi.de>
2025-08-08 15:07:45 +0200

    [rootest] convert to cmake meta/cmsUnload
    First generate all dicitonaries
    Provide runscript.C macro which executes line by line
    existing scripts from scripts/ sub-directory
    Minimally adjust ref files

--- roottest/root/meta/cmsUnload/scripts/loadrootmap.C
+++
@@ -1,8 +0,0 @@
-{
-int old = gInterpreter->SetClassAutoloading(kFALSE);
-gInterpreter->LoadLibraryMap("lib/libEdm.rootmap");
-gInterpreter->LoadLibraryMap("lib/libStrip.rootmap");
-gInterpreter->LoadLibraryMap("lib/libCluster.rootmap");
-gInterpreter->SetClassAutoloading(old);
-}
-
