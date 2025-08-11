4e2ef7db58693b8ced2d1f996f5ac2f2bed3a3a5
Silia Taider <siliataider@gmail.com>
2025-08-11 17:32:55 +0200

    add test_tutorials dir

---
+++ test_tutorials/test_tutorials.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import pathlib
+import ROOT
+import os
+import pytest
+
+# ROOT.gROOT.SetBatch(True)
+
+tutorial_dir = pathlib.Path(os.environ.get("ROOTSYS")) / "tutorials"
+tutorials = list(tutorial_dir.rglob("*.py"))
+
+@pytest.mark.parametrize("tutorial", tutorials)
+def test_tutorial(tutorial):
+    # subprocess.run([sys.executable, str(tutorial)], check=True)
+    print(f"Running tutorial: {tutorial}")
+    assert len(tutorials) > 0
+je siiosi
