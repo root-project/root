ad69c8ff4d8e8a27682aa62c6874672e31ba5c0a
Silia Taider <siliataider@gmail.com>
2025-08-12 17:35:36 +0200

    actually run the tutorials

--- test_tutorials/test_tutorials.py
+++ test_tutorials/test_tutorials.py
@@ -5,14 +5,18 @@ import ROOT
 import os
 import pytest
 
-# ROOT.gROOT.SetBatch(True)
+ROOT.gROOT.SetBatch(True)
 
-tutorial_dir = pathlib.Path(str(ROOT.gROOT.GetTutorialDir())) or pathlib.Path(ROOT.__file__).parent.parent.parent  / "tutorials"
+tutorial_dir = pathlib.Path(str(ROOT.gROOT.GetTutorialDir()))
 tutorials = list(tutorial_dir.rglob("*.py"))
 
 
-@pytest.mark.parametrize("tutorial", tutorials)
-def test_tutorial(tutorial):
-    # subprocess.run([sys.executable, str(tutorial)], check=True)
-    print(f"Running tutorial: {tutorial}")
+def test_tutorials_are_detected():
     assert len(tutorials) > 0
+
+@pytest.mark.parametrize("tutorial", tutorials, ids=lambda p: p.name)
+def test_tutorial(tutorial):
+    env = dict(**os.environ)
+    # force matplotlib to use a non-GUI backend
+    env["MPLBACKEND"] = "Agg"
+    subprocess.run([sys.executable, str(tutorial)], check=True, env=env)
