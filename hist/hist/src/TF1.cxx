12eaa32066aed3166f89dd1b0d09e99d9371f72e
Olivier Couet <olivier.couet@gmail.com>
2025-08-18 09:18:40 +0200

    Backport fix to TF1::DrawCopy

--- hist/hist/src/TF1.cxx
+++ hist/hist/src/TF1.cxx
@@ -1029,7 +1029,13 @@ void TF1::Copy(TObject &obj) const
    ((TF1 &)obj).fParMax    = fParMax;
    ((TF1 &)obj).fParent    = fParent;
    ((TF1 &)obj).fSave      = fSave;
-   ((TF1 &)obj).fHistogram = nullptr;
+   if (fHistogram) {
+      auto *h1 = (TH1 *)fHistogram->Clone();
+      h1->SetDirectory(nullptr);
+      ((TF1 &)obj).fHistogram = h1;
+   } else {
+      ((TF1 &)obj).fHistogram = nullptr;
+   }
    ((TF1 &)obj).fMethodCall = nullptr;
    ((TF1 &)obj).fNormalized = fNormalized;
    ((TF1 &)obj).fNormIntegral = fNormIntegral;
