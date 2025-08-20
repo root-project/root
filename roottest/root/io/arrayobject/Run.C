9a72e7e020fb599eb7fec096c89f7e6619a3951a
Sergey Linev <S.Linev@gsi.de>
2025-08-20 13:35:06 +0200

    [roottest] use cmake for root/io/arrayobject tests
    As in makefile - do not generate separate dictionary, used pre-compiled
    foo.C and bar.C files.
    Trying to read very old Event.3.2.0.root file fails. Run.C test also
    was not used in Makefile, seems to be class members not equipped correctly
    Remove unused Eventr.root file,
    Remove .rootrc

--- roottest/root/io/arrayobject/Run.C
+++ roottest/root/io/arrayobject/Run.C
@@ -1,12 +1,10 @@
-void Run(char* file= "Event.3.2.0.root") {
-gROOT->ProcessLine(".L libFoo.so");
-
-  TFile *h = new TFile(file, "READ");
-  TTree * tree = (TTree*)h->Get("T");
-  bar *b = 0;
+void Run(const char* filename = "Event.3.2.0.root")
+{
+  auto f = TFile::Open(filename);
+  TTree * tree = (TTree*) f->Get("T");
+  bar *b = nullptr;
   tree->SetBranchAddress("a/b",&b);
-  
+
   tree->GetEntry(3);
   b->print();
-  
 }
