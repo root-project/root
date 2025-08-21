dee7714da055b67e523fd37508cb7b0d4d792ede
silverweed <giacomo.parolini@cern.ch>
2025-08-21 11:16:22 +0200

    WIP: add python test for GetKeys() (currently failing)

--- io/v7/rfile/test/rfile.py
+++ io/v7/rfile/test/rfile.py
@@ -46,6 +46,38 @@ class RFileTests(unittest.TestCase):
             self.assertNotEqual(hist, None)
 
         os.remove(fileName)
+
+    def test_getkeys(self):
+        """A RFile can query the keys of its objects"""
+
+        fileName = "test_rfile_getkeys_py.root"
+
+        with RFile.Recreate(fileName) as rfile:
+            hist = ROOT.TH1D("hist", "", 100, -10, 10)
+            hist.FillRandom("gaus", 10)
+            rfile.Put("hist", hist)
+            rfile.Put("foo/hist", hist)
+            rfile.Put("foo/bar/hist", hist)
+            rfile.Put("foo/bar/hist2", hist)
+            rfile.Put("foo/hist2", hist)
+
+        with RFile.OpenForReading(fileName) as rfile:
+            keys = []
+            for key in rfile.GetKeys():
+                keys.append(key.fName)
+            self.assertEqual(keys, ["hist"])
+
+            keys = []
+            for key in rfile.GetKeys("foo"):
+                keys.append(key.fName)
+            self.assertEqual(keys, ["foo/hist", "foo/hist2"])
+
+            keys = []
+            for key in rfile.GetKeys("foo/bar"):
+                keys.append(key.fName)
+            self.assertEqual(keys, ["foo/bar/hist", "foo/bar/hist2"])
+
+        os.remove(fileName)
         
 
 if __name__ == "__main__":
