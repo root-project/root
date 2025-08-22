6f822db5a9378606d60191ee55e44984d3ca8330
Sergey Linev <S.Linev@gsi.de>
2025-08-22 14:37:42 +0200

    [roottest] move text scripts into runscript.C
    Use of ProcessLine does not work on CI nodes

--- roottest/root/meta/ROOT-7181/runscript.C
+++ roottest/root/meta/ROOT-7181/runscript.C
@@ -3,7 +3,10 @@
 #include <sstream>
 #include <string>
 
-void runscript(const char *fname, bool with_rootmap = false)
+
+#include "DataVector.h"
+
+void runscript(const std::string &name, bool with_rootmap = false)
 {
    if (with_rootmap) {
       int old = gInterpreter->SetClassAutoloading(kFALSE);
@@ -13,16 +16,31 @@ void runscript(const char *fname, bool with_rootmap = false)
       gInterpreter->SetClassAutoloading(old);
    }
 
-   std::ifstream f(fname);
-   if (!f) {
-      std::cout << "Not able to open " << fname << std::endl;
-      return;
-   }
-
-   std::string line;
-   while (std::getline(f, line)) {
-      // std::cout << "Process: " << line << std::endl;
-      gROOT->ProcessLine(line.c_str());
+   if (name == "case1") {
+      gSystem->Load("libjet_dictrflx");
+      gSystem->Load("libbtag_dictrflx");
+      TClass::GetClass("SG::AuxTypeVectorFactory<std::vector<ElementLink<DataVector<xAOD::Jet_v1> > > >");
+      auto c = TClass::GetClass("DataVector<xAOD::Jet_v1>")->GetClassInfo();
+      printf("Classinfo for DataVector<xAOD::Jet_v1> is %s\n",gInterpreter->ClassInfo_IsValid(c) ? "valid" : "invalid");
+   } else if (name == "case2") {
+      gSystem->Load("libsjet_dictrflx");
+      gSystem->Load("libbtag_dictrflx");
+      TClass::GetClass("SG::AuxTypeVectorFactory<std::vector<ElementLink<SDataVector<xAOD::SJet_v1> > > >");
+      auto c = TClass::GetClass("SDataVector<xAOD::SJet_v1>")->GetClassInfo();
+      printf("Classinfo for SDataVector<xAOD::SJet_v1> is %s\n",gInterpreter->ClassInfo_IsValid(c) ? "valid" : "invalid");
+   } else if (name == "case3") {
+      gSystem->Load("libjet_dictrflx");
+      gSystem->Load("libbtag_dictrflx");
+      auto c = TClass::GetClass("DataVector<xAOD::Jet_v1>");
+      printf("TClass for DataVector<xAOD::Jet_v1> is %s\n",c->IsLoaded() ? "loaded" : "not loaded");
+   } else if (name == "case4") {
+      gSystem->Load("libjet_dictrflx");
+      gSystem->Load("libbtag_dictrflx");
+      gROOT->ProcessLine("DataVector<xAOD::Jet_v1> obj;");
+      std::string name;
+      TClassEdit::GetNormalizedName(name,"DataVector<xAOD::Jet_v1>");
+      // TClass::GetClass("DataVector<xAOD::Jet_v1>");
+      printf("Normalized name for DataVector<xAOD::Jet_v1> is : %s\n",name.c_str());
    }
 }
 
