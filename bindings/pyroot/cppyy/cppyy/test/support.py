fd8337398241778c469a76dc1526b89c774e210e
Jonas Rembser <jonas.rembser@cern.ch>
2025-08-25 23:48:24 +0200

    [cppyy] New context to check whether ROOT errors have happended
    Use this in one of the `test_operators` tests, which actually prints jit
    errors.

--- bindings/pyroot/cppyy/cppyy/test/support.py
+++ bindings/pyroot/cppyy/cppyy/test/support.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os, py, sys, subprocess
+from contextlib import contextmanager
 
 currpath = py.path.local(__file__).dirpath()
 
@@ -45,6 +46,49 @@ if 'darwin' in sys.platform:
 
 IS_MAC = IS_MAC_ARM or IS_MAC_X86
 IS_LINUX = not (IS_WINDOWS or IS_MAC) 
+
+def _register_root_error_counter():
+
+    import cppyy
+
+    # already registered
+    if hasattr(cppyy.gbl, "rootErrorCount"):
+        return
+
+    cppyy.cppdef("""
+    auto originalErrorHandler = ::GetErrorHandler();
+
+    int &rootErrorCount()
+    {
+        static int count = 0;
+        return count;
+    }
+
+    void handleErrorWithException(int severity,
+                              bool abort,
+                              const char * location,
+                              const char * msg)
+    {
+        originalErrorHandler(severity, abort, location, msg);
+        rootErrorCount()++;
+    }
+
+    ::SetErrorHandler(handleErrorWithException);
+    """)
+
+@contextmanager
+def no_root_errors():
+    """Context manager to ensure no new ROOT errors occur."""
+    import cppyy
+
+    _register_root_error_counter()
+
+    start_count = cppyy.gbl.rootErrorCount()
+    yield
+    end_count = cppyy.gbl.rootErrorCount()
+    if end_count != start_count:
+        raise AssertionError(f"ROOT emitted {end_count - start_count} error(s) during block!")
+
 
 try:
     import __pypy__
