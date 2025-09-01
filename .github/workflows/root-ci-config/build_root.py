57590e3020ebb626fb023cd09b35a82926e8a861
ferdymercury <ferdymercury@users.noreply.github.com>
2025-09-01 16:05:03 +0200

    [toremove] test escaping in ci

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -46,7 +46,7 @@ except Exception as exc:
     CONNECTION = None
 
 WINDOWS = (os.name == 'nt')
-WORKDIR = (os.environ['HOME'] + '/ROOT-CI') if not WINDOWS else 'C:/ROOT-CI'
+WORKDIR = (os.environ['HOME'] + '/ROOT\ CI') if not WINDOWS else 'C:/ROOT^ CI'
 COMPRESSIONLEVEL = 6 if not WINDOWS else 1
 
 
