ae4c94afdf0d17dbaab122effceb4b06095d91db
Jakob Blomer <jblomer@cern.ch>
2025-09-04 23:44:08 +0200

    [net] add TCurlFile skeleton

---
+++ net/curl/src/TCurlFile.cxx
@@ -0,0 +1,12 @@
+// @(#)root/net:$Id$
+// Author: Jakob Blomer
+
+/*************************************************************************
+ * Copyright (C) 1995-2025, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "TCurlFile.h"
