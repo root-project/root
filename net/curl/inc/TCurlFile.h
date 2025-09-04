f2e03a5a25a9f12e458be3da883a0020e96d71ac
Jakob Blomer <jblomer@cern.ch>
2025-09-04 23:44:08 +0200

    [net] add TCurlFile skeleton

---
+++ net/curl/inc/TCurlFile.h
@@ -0,0 +1,30 @@
+// @(#)root/net:$Id$
+// Author: Jakob Blomer
+
+/*************************************************************************
+ * Copyright (C) 1995-2025, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TCurlFile
+#define ROOT_TCurlFile
+
+#include "TFile.h"
+
+class TCurlFile : public TFile {
+public:
+   TCurlFile(const char *url, Option_t *option = "");
+
+   Long64_t GetSize() const override;
+   void Seek(Long64_t offset, ERelativeTo pos = kBeg) override;
+   Bool_t ReadBuffer(char *buf, Int_t len) override;
+   Bool_t ReadBuffer(char *buf, Long64_t pos, Int_t len) override;
+   Bool_t ReadBuffers(char *buf, Long64_t *pos, Int_t *len, Int_t nbuf) override;
+
+   ClassDefOverride(TCurlFile, 0)
+};
+
+#endif
---
+++ net/curl/test/tfile_curl.cxx
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+#include "TCurlFile.h"
+
+TEST(TCurlFile, GetSize)
+{
+   // TODO
+}
