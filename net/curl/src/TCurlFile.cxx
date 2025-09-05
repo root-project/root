b02637b7560f5813aa101ab3bf1f54813903520f
Jakob Blomer <jblomer@cern.ch>
2025-09-05 21:51:46 +0200

    [net] add basic TCurlFile implementation

--- net/curl/src/TCurlFile.cxx
+++ net/curl/src/TCurlFile.cxx
@@ -10,3 +10,95 @@
  *************************************************************************/
 
 #include "TCurlFile.h"
+#include "TError.h"
+
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+TCurlFile::TCurlFile(const char *url, Option_t *opt)
+   : TFile(url, strstr(opt, "_WITHOUT_GLOBALREGISTRATION") != nullptr ? "WEB_WITHOUT_GLOBALREGISTRATION" : "WEB"),
+     fConnection(new ROOT::Internal::RCurlConnection(url))
+{
+   TFile::Init(kFALSE);
+   fOffset = 0;
+   fD = -2; // so TFile::IsOpen() will return true when in ~TFile
+}
+
+Long64_t TCurlFile::GetSize() const
+{
+   std::uint64_t size;
+   auto status = fConnection->SendHeadReq(size);
+   if (!status)
+      return -1;
+   return size;
+}
+
+void TCurlFile::Seek(Long64_t offset, ERelativeTo pos)
+{
+   switch (pos) {
+   case kBeg: fOffset = offset + fArchiveOffset; break;
+   case kCur: fOffset += offset; break;
+   case kEnd:
+      // this option is not used currently in the ROOT code
+      if (fArchiveOffset)
+         Error("Seek", "seeking from end in archive is not (yet) supported");
+      fOffset = fEND - offset; // is fEND really EOF or logical EOF?
+      break;
+   }
+}
+
+Bool_t TCurlFile::ReadBuffer(char *buf, Int_t len)
+{
+   ROOT::Internal::RCurlConnection::RUserRange range;
+   range.fDestination = reinterpret_cast<unsigned char *>(buf);
+   range.fOffset = fOffset;
+   range.fLength = len;
+   auto status = fConnection->SendRangesReq(1, &range);
+   if (!status) {
+      Error("TCurlFile", "can not read data: %s", status.fStatusMsg.c_str());
+      return kTRUE;
+   }
+
+   fOffset += range.fNBytesRecv;
+   return kFALSE;
+}
+
+Bool_t TCurlFile::ReadBuffer(char *buf, Long64_t pos, Int_t len)
+{
+   ROOT::Internal::RCurlConnection::RUserRange range;
+   range.fDestination = reinterpret_cast<unsigned char *>(buf);
+   range.fOffset = pos;
+   range.fLength = len;
+   auto status = fConnection->SendRangesReq(1, &range);
+   if (!status) {
+      Error("TCurlFile", "can not read data: %s", status.fStatusMsg.c_str());
+      return kTRUE;
+   }
+   return kFALSE;
+}
+
+Bool_t TCurlFile::ReadBuffers(char *buf, Long64_t *pos, Int_t *len, Int_t nbuf)
+{
+   if (nbuf == 0)
+      return kFALSE;
+
+   std::vector<ROOT::Internal::RCurlConnection::RUserRange> ranges;
+   ranges.reserve(nbuf);
+   std::size_t bufPos = 0;
+   for (Int_t i = 0; i < nbuf; ++i) {
+      ROOT::Internal::RCurlConnection::RUserRange r;
+      r.fDestination = reinterpret_cast<unsigned char *>(&buf[bufPos]);
+      r.fOffset = pos[i];
+      r.fLength = len[i];
+      ranges.emplace_back(r);
+      bufPos += len[i];
+   }
+
+   auto status = fConnection->SendRangesReq(nbuf, &ranges[0]);
+   if (!status) {
+      Error("TCurlFile", "can not read data: %s", status.fStatusMsg.c_str());
+      return kTRUE;
+   }
+   return kFALSE;
+}
--- net/curl/test/tfile_curl.cxx
+++ net/curl/test/tfile_curl.cxx
@@ -1,8 +1,20 @@
 #include "gtest/gtest.h"
 
 #include "TCurlFile.h"
+#include "TObject.h"
+
+#include <memory>
+#include <utility>
 
 TEST(TCurlFile, GetSize)
 {
-   // TODO
+   auto f = std::make_unique<TCurlFile>("https://root.cern.ch/files/galaxy.root");
+   EXPECT_LT(0, f->GetSize());
+}
+
+TEST(TCurlFile, Read)
+{
+   auto f = std::make_unique<TCurlFile>("https://root.cern.ch/files/galaxy.root");
+   auto obj = f->Get("n4254");
+   EXPECT_TRUE(obj != nullptr);
 }
