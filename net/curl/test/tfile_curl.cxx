c032adefec92471cb6208a95db140dab34b76946
Jakob Blomer <jblomer@cern.ch>
2025-09-05 21:51:46 +0200

    [net] add basic TCurlFile implementation

--- net/curl/test/tfile_curl.cxx
+++ net/curl/test/tfile_curl.cxx
@@ -2,7 +2,18 @@
 
 #include "TCurlFile.h"
 
+#include <memory>
+#include <utility>
+
 TEST(TCurlFile, GetSize)
 {
-   // TODO
+   auto f = std::make_unique<TCurlFile>("https://root.cern.ch/files/tutorials/hsimple.root");
+   EXPECT_LT(0, f->GetSize());
+}
+
+TEST(TCurlFile, Read)
+{
+   auto f = std::make_unique<TCurlFile>("https://root.cern.ch/files/tutorials/hsimple.root");
+   auto obj = f->Get("hpx");
+   EXPECT_TRUE(obj != nullptr);
 }
