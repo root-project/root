861ea04bffc74ab87b9495ea95a5ca5a800a6e31
Jakob Blomer <jblomer@cern.ch>
2025-09-06 23:06:14 +0200

    [net] add functional version of RRawFileCurl

--- net/curl/inc/ROOT/RRawFileCurl.hxx
+++ net/curl/inc/ROOT/RRawFileCurl.hxx
@@ -21,20 +21,29 @@ namespace Internal {
 
 class RCurlConnection;
 
-/// Provides read-only access to files on HTTP(S) resources. Uses libcurl for the transport layer.
+/// \class RRawFileCurl
+/// \ingroup net
+///
+/// The RRawFileCurl class reads HTTP(S) resources using the curl library. The passed URL of the file
+/// needs to start with http:// or https://. The URL does not need to be escaped; URL encoding is handled internally.
 class RRawFileCurl : public RRawFile {
 private:
-   std::unique_ptr<RCurlConnection> fCurlConnection;
+   std::unique_ptr<RCurlConnection> fConnection;
 
 protected:
    void OpenImpl() final;
    size_t ReadAtImpl(void *buffer, size_t nbytes, std::uint64_t offset) final;
+   void ReadVImpl(RIOVec *ioVec, unsigned int nReq) final;
    std::uint64_t GetSizeImpl() final;
 
 public:
+   static constexpr int kDefaultBlockSize = 128 * 1024; // relatively large 128k blocks for better network utilization
+
    RRawFileCurl(std::string_view url, RRawFile::ROptions options);
    ~RRawFileCurl();
    std::unique_ptr<RRawFile> Clone() const final;
+
+   RCurlConnection &GetConnection();
 };
 
 } // namespace Internal
