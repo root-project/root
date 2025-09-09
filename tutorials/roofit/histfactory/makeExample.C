84751f99abdfa21f7fe30877fee3f022b9052317
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2025-09-09 10:38:06 +0200

    [hf] Explicitly write histograms in makeExample.C tutorial.
    If implicit object ownership gets in TDirectories gets switched off, the
    example histograms need to be added to directories or written
    explicitly.

--- tutorials/roofit/histfactory/makeExample.C
+++ tutorials/roofit/histfactory/makeExample.C
@@ -5,15 +5,18 @@ void makeDataDriven()
   TFile* file = new TFile("dataDriven.root", "RECREATE");
 
   TH1F* FlatHist = new TH1F("FlatHist","FlatHist", 2,0,2);
+  FlatHist->SetDirectory(file);
   FlatHist->SetBinContent( 1, 1.0 );
   FlatHist->SetBinContent( 2, 1.0 );
 
   TH1F* Signal = new TH1F("Signal", "Signal", 2,0,2);
+  Signal->SetDirectory(file);
   Signal->SetBinContent(1, 10);
   Signal->SetBinContent(2, 80);
 
   // MC Background
   TH1F* Background1 = new TH1F("Background1", "Background1", 2,0,2);
+  Background1->SetDirectory(file);
   Background1->SetBinContent(1, 20);
   Background1->SetBinContent(2, 20);
 
@@ -21,16 +24,19 @@ void makeDataDriven()
   // that represents Background2
   // Assume the extrapolation factor is 1.0
   TH1F* ControlRegion = new TH1F("ControlRegion", "ControlRegion", 2,0,2);
+  ControlRegion->SetDirectory(file);
   ControlRegion->SetBinContent(1, 75);
   ControlRegion->SetBinContent(2, 35);
 
 
   TH1F* StatUncert = new TH1F("StatUncert", "StatUncert", 2,0,2);
+  StatUncert->SetDirectory(file);
   StatUncert->SetBinContent(1, .15);  // 15% uncertainty
   StatUncert->SetBinContent(2, .15);  // 15% uncertainty
 
 
   TH1F* data = new TH1F("data", "data", 2,0,2);
+  data->SetDirectory(file);
   data->SetBinContent(1, 90);
   data->SetBinContent(2, 110);
 
@@ -49,6 +55,7 @@ void makeShapeSys2DDataset()
   TFile* file = new TFile("ShapeSys2D.root", "RECREATE");
 
   TH2F* signal = new TH2F("signal", "signal", 2,0,2, 2,0,2);
+  signal->SetDirectory(file);
   signal->SetBinContent(1, 1, 10);
   signal->SetBinContent(2, 1, 10);
   signal->SetBinContent(1, 2, 20);
@@ -56,6 +63,7 @@ void makeShapeSys2DDataset()
 
   // Background 1
   TH2F* background1 = new TH2F("background1", "background1", 2,0,2, 2,0,2);
+  background1->SetDirectory(file);
   background1->SetBinContent(1, 1, 100);
   background1->SetBinContent(2, 1, 100);
   background1->SetBinContent(1, 2, 10);
@@ -63,6 +71,7 @@ void makeShapeSys2DDataset()
 
   // Background 1 Error
   TH2F* bkg1ShapeError = new TH2F("bkg1ShapeError", "bkg1ShapeError", 2,0,2, 2,0,2);
+  bkg1ShapeError->SetDirectory(file);
   bkg1ShapeError->SetBinContent(1, 1, .10);  // 10%
   bkg1ShapeError->SetBinContent(2, 1, .15);  // 15%
   bkg1ShapeError->SetBinContent(1, 2, .10);  // 10%
@@ -71,6 +80,7 @@ void makeShapeSys2DDataset()
 
   // Background 2
   TH2F* background2 = new TH2F("background2", "background2", 2,0,2, 2,0,2);
+  background2->SetDirectory(file);
   background2->SetBinContent(1, 1, 10);
   background2->SetBinContent(2, 1, 10);
   background2->SetBinContent(1, 2, 100);
@@ -78,6 +88,7 @@ void makeShapeSys2DDataset()
 
   // Background 2 Error
   TH2F* bkg2ShapeError = new TH2F("bkg2ShapeError", "bkg2ShapeError", 2,0,2, 2,0,2);
+  bkg2ShapeError->SetDirectory(file);
   bkg2ShapeError->SetBinContent(1, 1, .05);  // 5%
   bkg2ShapeError->SetBinContent(2, 1, .20);  // 20%
   bkg2ShapeError->SetBinContent(1, 2, .05);  // 5%
@@ -85,6 +96,7 @@ void makeShapeSys2DDataset()
 
 
   TH2F* data = new TH2F("data", "data", 2,0,2, 2,0,2);
+  data->SetDirectory(file);
   data->SetBinContent(1, 1, 122);
   data->SetBinContent(2, 1, 122);
   data->SetBinContent(1, 2, 132);
@@ -132,7 +144,10 @@ void makeShapeSysDataset()
   data->SetBinContent(1, 122);
   data->SetBinContent(2, 112);
 
-  file->Write();
+  for (auto histo : {signal, background1, bkg1ShapeError, background2, bkg2ShapeError, data}) {
+    file->WriteTObject(histo);
+  }
+
   file->Close();
 
 
@@ -171,11 +186,11 @@ void makeStatErrorDataSet()
   data->SetBinContent(1, 122);
   data->SetBinContent(2, 112);
 
+  for (auto histo : {FlatHist, signal, background1, bkg1StatUncert, background2, data}) {
+    file->WriteTObject(histo);
+  }
 
-  file->Write();
   file->Close();
-
-
 }
 
 
@@ -202,8 +217,10 @@ void makeSimpleExample(){
   statUncert->SetBinContent(1, .05);  // 5% uncertainty
   statUncert->SetBinContent(2, .05);  // 5% uncertainty
 
+  for (auto histo : {data, signal, background1, background2, statUncert}) {
+    example->WriteTObject(histo);
+  }
 
-  example->Write();
   example->Close();
   //////////////////////
 
