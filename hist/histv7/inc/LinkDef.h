0b50bf31bbf4f331a7a3c45823d2038f0bc6f9f5
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-09-23 15:41:04 +0200

    [hist] Request dictionaries for common RHist<T>
    Also request unsigned integer types and RBinWithError for RHistEngine.

--- hist/histv7/inc/LinkDef.h
+++ hist/histv7/inc/LinkDef.h
@@ -1,10 +1,24 @@
-// For RHistEngine, we request dictionaries for the most commonly used bin content types. This results in proper error
-// messages when trying to stream. Other instantiations will be caught by the RAxes member.
+// For RHist and RHistEngine, we request dictionaries for the most commonly used bin content types. This results in
+// proper error messages when trying to stream. Other instantiations will be caught by the RAxes member.
+#pragma link C++ class ROOT::Experimental::RHist<int>-;
+#pragma link C++ class ROOT::Experimental::RHist<unsigned>-;
+#pragma link C++ class ROOT::Experimental::RHist<long>-;
+#pragma link C++ class ROOT::Experimental::RHist<unsigned long>-;
+#pragma link C++ class ROOT::Experimental::RHist<long long>-;
+#pragma link C++ class ROOT::Experimental::RHist<unsigned long long>-;
+#pragma link C++ class ROOT::Experimental::RHist<float>-;
+#pragma link C++ class ROOT::Experimental::RHist<double>-;
+#pragma link C++ class ROOT::Experimental::RHist<ROOT::Experimental::RBinWithError>-;
+
 #pragma link C++ class ROOT::Experimental::RHistEngine<int>-;
+#pragma link C++ class ROOT::Experimental::RHistEngine<unsigned>-;
 #pragma link C++ class ROOT::Experimental::RHistEngine<long>-;
+#pragma link C++ class ROOT::Experimental::RHistEngine<unsigned long>-;
 #pragma link C++ class ROOT::Experimental::RHistEngine<long long>-;
+#pragma link C++ class ROOT::Experimental::RHistEngine<unsigned long long>-;
 #pragma link C++ class ROOT::Experimental::RHistEngine<float>-;
 #pragma link C++ class ROOT::Experimental::RHistEngine<double>-;
+#pragma link C++ class ROOT::Experimental::RHistEngine<ROOT::Experimental::RBinWithError>-;
 
 #pragma link C++ class ROOT::Experimental::RHistStats-;
 #pragma link C++ class ROOT::Experimental::RRegularAxis-;
