f9355653c292614a176ebe19a12a685569b933e1
aviralgarg05 <gargaviral99@gmail.com>
2025-10-07 02:18:02 +0530

    pythonization(tcolor): preserve __init__ metadata using functools.wraps (fixes Jupyter TColor.DefinedColors behavior)

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tcolor.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tcolor.py
@@ -7,19 +7,30 @@
 # For the licensing terms see $ROOTSYS/LICENSE.                                #
 # For the list of contributors see $ROOTSYS/README/CREDITS.                    #
 ################################################################################
+
+import functools
+
 from . import pythonization
 
-def _TColor_constructor(self, *args, **kwargs):
+
+def _tcolor_constructor(original_init):
     """
-    Forward the arguments to the C++ constructor and retain ownership. This
-    helps avoiding double deletes due to ROOT automatic memory management.
+    Wrapper for TColor constructor that retains ownership to avoid double deletes.
+    Uses functools.wraps to preserve the original function's attributes.
     """
-    self._cpp_constructor(*args, **kwargs)
-    import ROOT
-    ROOT.SetOwnership(self, False)
+    @functools.wraps(original_init)
+    def wrapper(self, *args, **kwargs):
+        """
+        Forward the arguments to the C++ constructor and retain ownership. This
+        helps avoiding double deletes due to ROOT automatic memory management.
+        """
+        original_init(self, *args, **kwargs)
+        import ROOT
+        ROOT.SetOwnership(self, False)
+    return wrapper
 
 
 @pythonization("TColor")
 def pythonize_tcolor(klass):
-    klass._cpp_constructor = klass.__init__
-    klass.__init__ = _TColor_constructor
+    klass.__init__ = _tcolor_constructor(klass.__init__)
+
