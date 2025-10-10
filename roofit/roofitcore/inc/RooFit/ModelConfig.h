00ec4c0aedca16f09f92ae74ba8c5fe136a78767
Jonas Rembser <jonas.rembser@cern.ch>
2025-10-10 17:27:14 +0200

    Fix warnings when compiling with Clang 21
    Fixing warnings like:
    ```txt
    In file included from /home/rembserj/code/root/root_src/core/metacling/src/TClingCallFunc.cxx:32:
    /home/rembserj/code/root/root_src/core/metacling/src/TCling.h:372:17: warning: virtual method 'GetFunctionName' is inside a 'final' class and can never be overridden [-Wunnecessary-virtual-specifier]
      372 |    virtual void GetFunctionName(const clang::Decl *decl, std::string &name) const;
          |                 ^
    1 warning generated.
    ```
    or
    ```
    /home/rembserj/code/root/root_src/tree/ntuple/test/ntuple_parallel_writer.cxx:97:29: warning: variable 'pt1' is uninitialized when passed as a const pointer argument here [-Wuninitialized-const-pointer]
       97 |       e1->BindRawPtr("pt", &pt1);
          |
    ```

--- roofit/roofitcore/inc/RooFit/ModelConfig.h
+++ roofit/roofitcore/inc/RooFit/ModelConfig.h
@@ -68,7 +68,7 @@ public:
    /// Set a workspace that owns all the necessary components for the analysis.
    void SetWS(RooWorkspace &ws) override;
    //// alias for SetWS(...)
-   virtual void SetWorkspace(RooWorkspace &ws) { SetWS(ws); }
+   void SetWorkspace(RooWorkspace &ws) { SetWS(ws); }
 
    /// Remove the existing reference to a workspace and replace it with this new one.
    void ReplaceWS(RooWorkspace *ws) override
@@ -78,28 +78,28 @@ public:
    }
 
    /// Set the proto DataSet, add to the workspace if not already there
-   virtual void SetProtoData(RooAbsData &data)
+   void SetProtoData(RooAbsData &data)
    {
       ImportDataInWS(data);
       SetProtoData(data.GetName());
    }
 
    /// Set the Pdf, add to the workspace if not already there
-   virtual void SetPdf(const RooAbsPdf &pdf)
+   void SetPdf(const RooAbsPdf &pdf)
    {
       ImportPdfInWS(pdf);
       SetPdf(pdf.GetName());
    }
 
    /// Set the Prior Pdf, add to the workspace if not already there
-   virtual void SetPriorPdf(const RooAbsPdf &pdf)
+   void SetPriorPdf(const RooAbsPdf &pdf)
    {
       ImportPdfInWS(pdf);
       SetPriorPdf(pdf.GetName());
    }
 
    /// Specify parameters of the PDF.
-   virtual void SetParameters(const RooArgSet &set)
+   void SetParameters(const RooArgSet &set)
    {
       if (!SetHasOnlyParameters(set, "ModelConfig::SetParameters"))
          return;
@@ -108,7 +108,7 @@ public:
    }
 
    /// Specify parameters of interest.
-   virtual void SetParametersOfInterest(const RooArgSet &set)
+   void SetParametersOfInterest(const RooArgSet &set)
    {
       if (!SetHasOnlyParameters(set, "ModelConfig::SetParametersOfInterest"))
          return;
@@ -117,7 +117,7 @@ public:
 
    /// Specify parameters
    /// using a list of comma-separated list of arguments already in the workspace.
-   virtual void SetParameters(const char *argList)
+   void SetParameters(const char *argList)
    {
       if (!GetWS())
          return;
@@ -126,10 +126,10 @@ public:
 
    /// Specify parameters of interest
    /// using a comma-separated list of arguments already in the workspace.
-   virtual void SetParametersOfInterest(const char *argList) { SetParameters(argList); }
+   void SetParametersOfInterest(const char *argList) { SetParameters(argList); }
 
    /// Specify the nuisance parameters (parameters that are not POI).
-   virtual void SetNuisanceParameters(const RooArgSet &set)
+   void SetNuisanceParameters(const RooArgSet &set)
    {
       if (!SetHasOnlyParameters(set, "ModelConfig::SetNuisanceParameters"))
          return;
@@ -139,7 +139,7 @@ public:
 
    /// Specify the nuisance parameters
    /// using a comma-separated list of arguments already in the workspace.
-   virtual void SetNuisanceParameters(const char *argList)
+   void SetNuisanceParameters(const char *argList)
    {
       if (!GetWS())
          return;
@@ -147,7 +147,7 @@ public:
    }
 
    /// Specify the constraint parameters
-   virtual void SetConstraintParameters(const RooArgSet &set)
+   void SetConstraintParameters(const RooArgSet &set)
    {
       if (!SetHasOnlyParameters(set, "ModelConfig::SetConstrainedParameters"))
          return;
@@ -156,7 +156,7 @@ public:
    }
    /// Specify the constraint parameters
    /// through a comma-separated list of arguments already in the workspace.
-   virtual void SetConstraintParameters(const char *argList)
+   void SetConstraintParameters(const char *argList)
    {
       if (!GetWS())
          return;
@@ -164,7 +164,7 @@ public:
    }
 
    /// Specify the observables.
-   virtual void SetObservables(const RooArgSet &set)
+   void SetObservables(const RooArgSet &set)
    {
       if (!SetHasOnlyParameters(set, "ModelConfig::SetObservables"))
          return;
@@ -173,27 +173,27 @@ public:
    }
    /// specify the observables
    /// through a comma-separated list of arguments already in the workspace.
-   virtual void SetObservables(const char *argList)
+   void SetObservables(const char *argList)
    {
       if (!GetWS())
          return;
       SetObservables(GetWS()->argSet(argList));
    }
 
-   virtual void SetConditionalObservables(const RooArgSet &set);
+   void SetConditionalObservables(const RooArgSet &set);
    /// Specify the conditional observables
    /// through a comma-separated list of arguments already in the workspace.
-   virtual void SetConditionalObservables(const char *argList)
+   void SetConditionalObservables(const char *argList)
    {
       if (!GetWS())
          return;
       SetConditionalObservables(GetWS()->argSet(argList));
    }
 
-   virtual void SetGlobalObservables(const RooArgSet &set);
+   void SetGlobalObservables(const RooArgSet &set);
    /// Specify the global observables
    /// through a comma-separated list of arguments already in the workspace.
-   virtual void SetGlobalObservables(const char *argList)
+   void SetGlobalObservables(const char *argList)
    {
       if (!GetWS())
          return;
@@ -203,7 +203,7 @@ public:
    void SetExternalConstraints(const RooArgSet &set);
    /// Specify the external constraints
    /// through a comma-separated list of arguments already in the workspace.
-   virtual void SetExternalConstraints(const char *argList)
+   void SetExternalConstraints(const char *argList)
    {
       if (!GetWS())
          return;
@@ -212,10 +212,10 @@ public:
 
    /// Set parameter values for a particular hypothesis if using a common PDF
    /// by saving a snapshot in the workspace.
-   virtual void SetSnapshot(const RooArgSet &set);
+   void SetSnapshot(const RooArgSet &set);
 
    /// Specify the name of the PDF in the workspace to be used.
-   virtual void SetPdf(const char *name)
+   void SetPdf(const char *name)
    {
       if (!GetWS())
          return;
@@ -232,7 +232,7 @@ public:
    }
 
    /// Specify the name of the PDF in the workspace to be used.
-   virtual void SetPriorPdf(const char *name)
+   void SetPriorPdf(const char *name)
    {
       if (!GetWS())
          return;
@@ -249,7 +249,7 @@ public:
    }
 
    /// Specify the name of the dataset in the workspace to be used.
-   virtual void SetProtoData(const char *name)
+   void SetProtoData(const char *name)
    {
       if (!GetWS())
          return;
