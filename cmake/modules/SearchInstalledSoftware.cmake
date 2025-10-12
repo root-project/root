cf27cd8ce16b3b9e50c11a5396ae1a5f2ca59997
Jonas Rembser <jonas.rembser@cern.ch>
2025-10-12 12:59:33 +0200

    [Python] Raise minimum required Python version from 3.8 to 3.9
    Python 3.8 has been end-of-life for one year already. The last platform
    that used it was AlmaLinux 8, but also there it has reached end of life.
    Instead, RHEL has committed to support Python 3.12 until the end-of-life
    of RHEL 8 itself:
    https://access.redhat.com/support/policy/updates/rhel-app-streams-life-cycle
    
    Therefore, our CI images for `alma8` also use Python 3.12 now.
    
    And although Python 3.9 has also reached end-of-life now, we still need
    to support it because it's the system Python version on macOS.
    
    See:
    https://devguide.python.org/versions/

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -708,7 +708,7 @@ endif()
 if(tmva-pymva)
   list(APPEND python_components NumPy)
 endif()
-find_package(Python3 3.8 COMPONENTS ${python_components})
+find_package(Python3 3.9 COMPONENTS ${python_components})
 
 #---Check for OpenGL installation-------------------------------------------------------
 # OpenGL is required by various graf3d features that are enabled with opengl=ON,
