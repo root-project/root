2beeb3eb22d7ae6985aac5b2445f99c148346a2a
ferdymercury <ferdymercury@users.noreply.github.com>
2025-10-16 11:42:18 +0200

    [core] also print cxx standard into start message
    This is useful, since some bug reports could be traced back to a specific cxx standard that can not be reproduced with others

--- core/rint/src/TRint.cxx
+++ core/rint/src/TRint.cxx
@@ -542,8 +542,8 @@ void TRint::PrintLogo(Bool_t lite)
                                             gROOT->GetGitBranch(),
                                             gROOT->GetGitCommit()));
       }
-      lines.emplace_back(TString::Format("With %s %%s",
-                                         gSystem->GetBuildCompilerVersionStr()));
+      lines.emplace_back(TString::Format("With %s std%ld %%s",
+                                         gSystem->GetBuildCompilerVersionStr(), __cplusplus));
       lines.emplace_back(TString("Try '.help'/'.?', '.demo', '.license', '.credits', '.quit'/'.q'%s"));
 
       // Find the longest line and its length:
