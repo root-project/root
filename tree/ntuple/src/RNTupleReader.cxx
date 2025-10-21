65945e0ea5e2af067d5aacc3c5ad02535ff06e1a
Jakob Blomer <jblomer@cern.ch>
2025-10-21 12:39:04 +0200

    [ntuple] add active entry tokens to reader
    API extension to tell RNTuple about the lifetime of entries. Useful when
    multiple streams (threads) share a single reader.
    
    The active entry tokens are linked to the reader by a shared control
    block. Active entry tokens can be copied and moved and take care of the
    reference counting of active entry numbers to clusters, such that the
    corresponding clusters are pinned and unpinned as needed.

--- tree/ntuple/src/RNTupleReader.cxx
+++ tree/ntuple/src/RNTupleReader.cxx
@@ -22,6 +22,97 @@
 
 #include <TROOT.h>
 
+#include <cassert>
+
+void ROOT::RNTupleReader::RActiveEntryToken::ActivateEntry(NTupleSize_t entryNumber)
+{
+   const auto descGuard = fPtrControlBlock->fPageSource->GetSharedDescriptorGuard();
+   const auto clusterId = Internal::CallFindClusterIdOn(descGuard.GetRef(), entryNumber);
+   if (clusterId == kInvalidDescriptorId)
+      throw RException(R__FAIL(std::string("entry number ") + std::to_string(entryNumber) + " out of range"));
+
+   auto [itr, _] = fPtrControlBlock->fActiveClusters.try_emplace(clusterId, 0);
+   if (itr->second++ == 0)
+      fPtrControlBlock->fPageSource->PinCluster(clusterId);
+}
+
+void ROOT::RNTupleReader::RActiveEntryToken::DeactivateEntry(NTupleSize_t entryNumber)
+{
+   const auto descGuard = fPtrControlBlock->fPageSource->GetSharedDescriptorGuard();
+   const auto clusterId = Internal::CallFindClusterIdOn(descGuard.GetRef(), entryNumber);
+
+   if (clusterId == kInvalidDescriptorId)
+      throw RException(R__FAIL(std::string("entry number ") + std::to_string(entryNumber) + " out of range"));
+
+   auto itr = fPtrControlBlock->fActiveClusters.find(clusterId);
+   assert(itr != fPtrControlBlock->fActiveClusters.end());
+
+   if (--(itr->second) == 0) {
+      fPtrControlBlock->fActiveClusters.erase(itr);
+      fPtrControlBlock->fPageSource->UnpinCluster(clusterId);
+   }
+}
+
+void ROOT::RNTupleReader::RActiveEntryToken::SetEntryNumber(NTupleSize_t entryNumber)
+{
+   if (entryNumber == fEntryNumber || entryNumber == kInvalidNTupleIndex)
+      return;
+
+   const std::lock_guard<std::mutex> _(fPtrControlBlock->fLock);
+   if (fPtrControlBlock->fPageSource) {
+      ActivateEntry(entryNumber);
+      if (fEntryNumber != kInvalidNTupleIndex)
+         DeactivateEntry(fEntryNumber);
+   }
+   fEntryNumber = entryNumber;
+}
+
+void ROOT::RNTupleReader::RActiveEntryToken::Reset()
+{
+   if (fEntryNumber == kInvalidNTupleIndex)
+      return;
+
+   const std::lock_guard<std::mutex> _(fPtrControlBlock->fLock);
+   if (fPtrControlBlock->fPageSource) {
+      DeactivateEntry(fEntryNumber);
+   }
+   fEntryNumber = kInvalidNTupleIndex;
+}
+
+ROOT::RNTupleReader::RActiveEntryToken::RActiveEntryToken(const RActiveEntryToken &other)
+{
+   fPtrControlBlock = other.fPtrControlBlock;
+   SetEntryNumber(other.fEntryNumber);
+}
+
+ROOT::RNTupleReader::RActiveEntryToken::RActiveEntryToken(RActiveEntryToken &&other)
+{
+   std::swap(fEntryNumber, other.fEntryNumber);
+   std::swap(fPtrControlBlock, other.fPtrControlBlock);
+   assert(other.fEntryNumber == kInvalidNTupleIndex);
+   assert(!other.fPtrControlBlock);
+}
+
+ROOT::RNTupleReader::RActiveEntryToken &
+ROOT::RNTupleReader::RActiveEntryToken::operator=(const RActiveEntryToken &other)
+{
+   if (this == &other)
+      return *this;
+   if (other.fPtrControlBlock.get() != fPtrControlBlock.get()) {
+      Reset();
+      fPtrControlBlock = other.fPtrControlBlock;
+   }
+   SetEntryNumber(other.fEntryNumber);
+   return *this;
+}
+
+ROOT::RNTupleReader::RActiveEntryToken &ROOT::RNTupleReader::RActiveEntryToken::operator=(RActiveEntryToken &&other)
+{
+   std::swap(fEntryNumber, other.fEntryNumber);
+   std::swap(fPtrControlBlock, other.fPtrControlBlock);
+   return *this;
+}
+
 void ROOT::RNTupleReader::ConnectModel(ROOT::RNTupleModel &model, bool allowFieldSubstitutions)
 {
    auto &fieldZero = ROOT::Internal::GetFieldZeroOfModel(model);
@@ -56,6 +147,7 @@ void ROOT::RNTupleReader::InitPageSource(bool enableMetrics)
       EnableMetrics();
    fSource->Attach();
    fNEntries = fSource->GetNEntries();
+   fActiveEntriesControlBlock = std::make_shared<RActiveEntriesControlBlock>(fSource.get());
 }
 
 ROOT::RNTupleReader::RNTupleReader(std::unique_ptr<ROOT::RNTupleModel> model,
@@ -80,7 +172,11 @@ ROOT::RNTupleReader::RNTupleReader(std::unique_ptr<ROOT::Internal::RPageSource> 
    InitPageSource(options.GetEnableMetrics());
 }
 
-ROOT::RNTupleReader::~RNTupleReader() = default;
+ROOT::RNTupleReader::~RNTupleReader()
+{
+   const std::lock_guard<std::mutex> _(fActiveEntriesControlBlock->fLock);
+   fActiveEntriesControlBlock->fPageSource = nullptr;
+}
 
 std::unique_ptr<ROOT::RNTupleReader> ROOT::RNTupleReader::Open(std::unique_ptr<ROOT::RNTupleModel> model,
                                                                std::string_view ntupleName, std::string_view storage,
