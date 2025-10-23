973fe448ab38702df0678270316233ae25f5560d
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2025-10-23 12:37:51 +0200

    [df] Fix logic to check when implicit MT is enabled
    RTTreeDS needs to act differently w.r.t. TTreeReader creation whether implicit
    MT is enabled or not. Fix the logic to use the more appropriate
    ROOT::IsImplicitMTEnabled rather than just checking for the number of active
    slots.

--- tree/dataframe/test/datasource_tree.cxx
+++ tree/dataframe/test/datasource_tree.cxx
@@ -56,3 +56,47 @@ TEST(RTTreeDS, BranchWithNestedSameName)
 
    expect_vec_eq(branchNames, expectedBranchNames);
 }
+
+#ifdef R__USE_IMT
+struct Dataset20164RAIII {
+   const char *fTreeName{"tree_20164"};
+   const char *fFileName{"tree_20164.root"};
+
+   Dataset20164RAIII()
+   {
+      std::unique_ptr<TFile> ofile{TFile::Open(fFileName, "recreate")};
+
+      std::unique_ptr<TTree> myTree = std::make_unique<TTree>(fTreeName, fTreeName);
+
+      int v1{42};
+
+      myTree->Branch("v1", &v1);
+      myTree->Fill();
+      ofile->Write();
+
+      // It's important to use 1 as the logic in RTTreeDS was faulty only for the case fNSlots==1
+      ROOT::EnableImplicitMT(1);
+   }
+
+   ~Dataset20164RAIII()
+   {
+      ROOT::DisableImplicitMT();
+      std::remove(fFileName);
+   }
+};
+
+TEST(RTTreeDS, Regression20164)
+{
+   // Test that TTree thread tasks coherently use the TTreeReader created for the specific task and do not access
+   // a TTreeReader from the RTTreeDS.
+   Dataset20164RAIII dataset{};
+
+   auto chain = std::make_unique<TChain>(dataset.fTreeName);
+   chain->Add(dataset.fFileName);
+
+   auto val = ROOT::RDataFrame(*chain).Sum<int>("v1").GetValue();
+
+   ASSERT_EQ(val, 42);
+}
+
+#endif
