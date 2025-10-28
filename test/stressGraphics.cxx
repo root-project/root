4c701bee3650f28839aace8a95ed2fa6b818eaec
Sergey Linev <S.Linev@gsi.de>
2025-10-28 17:41:03 +0100

    Add testing of TGraphPolar to stressGraphics
    Saving in macro does not work at all
    SVG output varies on several platforms - to be investigated later

--- test/stressGraphics.cxx
+++ test/stressGraphics.cxx
@@ -100,6 +100,8 @@
 #include "TGraph2DErrors.h"
 #include "TGraph2DAsymmErrors.h"
 #include "TGraphMultiErrors.h"
+#include "TGraphPolar.h"
+#include "TGraphPolargram.h"
 #include "TParallelCoord.h"
 #include "TImage.h"
 #include "TMath.h"
@@ -3903,6 +3905,91 @@ void profile_2poly()
    TestReport(C, "TH2Poly and TPofile2Poly", "", 0, "profile_2dpoly");
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// TGraphPolar
+
+void graphpolar()
+{
+   // Illustrates how to use TGraphPolar
+
+   auto C = StartTest(1200,600);
+   C->Divide(2,1);
+   C->cd(1);
+
+   Double_t xmin=0;
+   Double_t xmax=TMath::Pi()*2;
+
+   Double_t x[1000];
+   Double_t y[1000];
+   Double_t xval1[20];
+   Double_t yval1[20];
+
+   for (Int_t ipt = 0; ipt < 1000; ipt++){
+      x[ipt] = ipt*(xmax-xmin)/1000+xmin;
+      y[ipt] = cos(2*x[ipt])*cos(20*x[ipt]);
+   }
+
+   TGraphPolar * grP = new TGraphPolar(1000,x,y);
+   grP->SetLineColor(2);
+   grP->SetLineWidth(2);
+   grP->SetFillStyle(3012);
+   grP->SetFillColor(2);
+   grP->Draw("AFL");
+
+   for (Int_t ipt = 0; ipt < 20; ipt++){
+      xval1[ipt] = x[1000/20*ipt];
+      yval1[ipt] = y[1000/20*ipt];
+   }
+
+   TGraphPolar *grP1 = new TGraphPolar(20,xval1,yval1);
+   grP1->SetMarkerStyle(29);
+   grP1->SetMarkerSize(2);
+   grP1->SetMarkerColor(4);
+   grP1->SetLineColor(4);
+   grP1->Draw("CP");
+
+   // Update, otherwise GetPolargram returns 0
+   gPad->Update();
+   if (grP1->GetPolargram()) {
+      grP1->GetPolargram()->SetTextColor(8);
+      grP1->GetPolargram()->SetRangePolar(-TMath::Pi(),TMath::Pi());
+      grP1->GetPolargram()->SetNdivPolar(703);
+      grP1->GetPolargram()->SetToRadian();
+   }
+
+   C->cd(2);
+   Double_t x2[30];
+   Double_t y2[30];
+   Double_t ex[30];
+   Double_t ey[30];
+   for (Int_t ipt = 0; ipt < 30; ipt++){
+      x2[ipt] = x[1000/30*ipt];
+      y2[ipt] = 1.2 + 0.4*sin(TMath::Pi()*2*ipt/30);
+      ex[ipt] = 0.2+0.1*cos(2*TMath::Pi()/30*ipt);
+      ey[ipt] = 0.2;
+   }
+
+   TGraphPolar *grPE = new TGraphPolar(30,x2,y2,ex,ey);
+   grPE->SetMarkerStyle(22);
+   grPE->SetMarkerSize(1.5);
+   grPE->SetMarkerColor(5);
+   grPE->SetLineColor(6);
+   grPE->SetLineWidth(2);
+   grPE->Draw("EP");
+   // Update, otherwise GetPolargram returns 0
+   gPad->Update();
+
+   if (grPE->GetPolargram()) {
+      grPE->GetPolargram()->SetTextSize(0.03);
+      grPE->GetPolargram()->SetTwoPi();
+      grPE->GetPolargram()->SetToRadian();
+   }
+
+   TestReport(C, "TGraphPolar", kSkipCCode, kSkipSvgTest, "graphpolar");
+}
+
+
+
 void PrintRefHeader()
 {
    if (gWebMode)
@@ -4041,6 +4128,7 @@ void stressGraphics(Int_t verbose = 0, Bool_t generate = kFALSE, Bool_t keep_fil
    efficiency_test();
    profile_2d    ();
    profile_2poly ();
+   graphpolar    ();
    print_reports ();
 
    start_block("High Level 3D Primitives");
