e48634c64a6ce886a24a6300c0df475f853b03a4
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-10-28 17:30:42 +0100

    [hist] Conversion of RBinWithError

--- hist/histv7util/test/histutil_test.hxx
+++ hist/histv7util/test/histutil_test.hxx
@@ -2,11 +2,13 @@
 #define histutil_test
 
 #include <ROOT/Hist/ConvertToTH1.hxx>
+#include <ROOT/RBinWithError.hxx>
 #include <ROOT/RHist.hxx>
 #include <ROOT/RHistEngine.hxx>
 #include <ROOT/RWeight.hxx>
 
 using namespace ROOT::Experimental::Hist;
+using ROOT::Experimental::RBinWithError;
 using ROOT::Experimental::RHist;
 using ROOT::Experimental::RHistEngine;
 using ROOT::Experimental::RRegularAxis;
