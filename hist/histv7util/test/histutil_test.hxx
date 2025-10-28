3788b238fd8ffe35899b38445656f822f599c3b7
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-10-28 10:13:16 +0100

    [hist] Conversion functions from RHist<int> to TH1I
    Introduce a new package ROOTHistUtil that depends both on the "old"
    TH1* Hist library and the "new" histv7 package. Conversion is offered
    via free functions in the newly added Hist namespace. In this initial
    implementation, only one-dimensional regular histograms with integer
    bin content type are supported.

---
+++ hist/histv7util/test/histutil_test.hxx
@@ -0,0 +1,15 @@
+#ifndef histutil_test
+#define histutil_test
+
+#include <ROOT/Hist/ConvertToTH1.hxx>
+#include <ROOT/RHist.hxx>
+#include <ROOT/RHistEngine.hxx>
+
+using namespace ROOT::Experimental::Hist;
+using ROOT::Experimental::RHist;
+using ROOT::Experimental::RHistEngine;
+using ROOT::Experimental::RRegularAxis;
+
+#include <gtest/gtest.h>
+
+#endif
