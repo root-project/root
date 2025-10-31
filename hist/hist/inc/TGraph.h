3cfd7badcbac7d527c14886e08f0017153a4a02e
ferdymercury <ferdymercury@users.noreply.github.com>
2025-10-31 09:06:44 +0100

    [hist] move to source

--- hist/hist/inc/TGraph.h
+++ hist/hist/inc/TGraph.h
@@ -182,12 +182,11 @@ public:
    virtual void          Scale(Double_t c1=1., Option_t *option="y"); // *MENU*
    virtual void          SetEditable(Bool_t editable=kTRUE); // *TOGGLE* *GETTER=GetEditable
    virtual void          SetHighlight(Bool_t set = kTRUE); // *TOGGLE* *GETTER=IsHighlight
-   /// Set the histogram underlying the TGraph. This transfers the ownership of h to the TGraph. The preexisting fHistogram will be deleted.
-   virtual void          SetHistogram(TH1F *h) {delete fHistogram; fHistogram = h;}
+   virtual void          SetHistogram(TH1F *h);
    virtual void          SetMaximum(Double_t maximum=-1111); // *MENU*
    virtual void          SetMinimum(Double_t minimum=-1111); // *MENU*
    virtual void          Set(Int_t n);
-   virtual void SetOption(Option_t *option = " ") { fOption = option; }
+   virtual void          SetOption(Option_t *option = " ") { fOption = option; }
    virtual void          SetPoint(Int_t i, Double_t x, Double_t y);
    virtual void          SetPointX(Int_t i, Double_t x);
    virtual void          SetPointY(Int_t i, Double_t y);
