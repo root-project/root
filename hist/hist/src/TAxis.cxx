bfcf2b3b8c5ea1fa203a36f08b2aa618dfaf90f3
ferdymercury <ferdymercury@users.noreply.github.com>
2025-11-03 14:39:28 +0100

    [hist] Unzoom: add sanity check before casting

--- hist/hist/src/TAxis.cxx
+++ hist/hist/src/TAxis.cxx
@@ -1268,7 +1268,7 @@ void TAxis::UnZoom()
 
    //unzoom object owning this axis
    SetRange(0,0);
-   TH1 *hobj1 = (TH1*)GetParent();
+   TH1 *hobj1 = GetParent() && GetParent()->InheritsFrom(TH1::Class()) ? static_cast<TH1*>(GetParent()) : nullptr;
    if (!strstr(GetName(),"xaxis")) {
       if (!hobj1) return;
       if (hobj1->GetDimension() == 2) {
