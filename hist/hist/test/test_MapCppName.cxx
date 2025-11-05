a75a8aee4b52e74f167ace45af44fbed1eb282e4
Sergey Linev <S.Linev@gsi.de>
2025-11-05 18:58:58 +0100

    adjust hist tests after SavePrimitive update

--- hist/hist/test/test_MapCppName.cxx
+++ hist/hist/test/test_MapCppName.cxx
@@ -47,7 +47,7 @@ TEST(TH1, MapCppNameTest)
    if (!gSystem->GetPathInfo(CFile.Data(), fs))
       FileSize = (Int_t)fs.fSize;
 
-   EXPECT_NEAR(FileSize, 5867, 200);
+   EXPECT_NEAR(FileSize, 6350, 200);
 
    gSystem->Unlink(CFile.Data());
 }
