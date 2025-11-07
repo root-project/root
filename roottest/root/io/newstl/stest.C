c500714938983373a503cdcdc6205b9a14eb7b0f
Sergey Linev <S.Linev@gsi.de>
2025-11-07 10:05:41 +0100

    [roottest] run io-newstl tests only in current directory
    Older version of the test tried to list
    current directory for files created by previous version of tests.
    Now such situation is not possible,  therefore remove such listing

--- roottest/root/io/newstl/stest.C
+++ roottest/root/io/newstl/stest.C
@@ -1,12 +1,17 @@
 #include "setHolder.h"
 #include "test.C"
 
-void stest(bool readother=false) {
+void stest(const char *dirname = "") {
    const char* testname = "set";
    typedef setHolder holder;
 
-   std::cout << "Running test " << testname << std::endl;
-   checkHolder<holder>(testname);
-   write<holder>(testname);
-   read<holder>(testname,0,readother);
+   if (dirname && *dirname) {
+      std::cout << "Running reading test " << testname << std::endl;
+      read<holder>(dirname, testname);
+   } else {
+      std::cout << "Running test " << testname << std::endl;
+      checkHolder<holder>(testname);
+      write<holder>(testname);
+      read<holder>("", testname);
+   }
 }
