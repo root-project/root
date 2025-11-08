059f6c0cddee35a660fbb77b396d6deec9690ddc
Jonas Rembser <jonas.rembser@cern.ch>
2025-11-08 11:36:01 +0100

    Don't add CITATION.cff and keep release notes formatted as before
    This reverts parts of the previous commit, but keeps the nice script to
    convert the author list to the CITATION.cff. That could be useful later.

--- cmake/unix/makeCITATION.py
+++ cmake/unix/makeCITATION.py
@@ -7,55 +7,67 @@ import json
 import datetime
 import os, sys
 
+import ROOT  # for ROOT.__version__
+
+"""
+Generate a CITATION.cff-style YAML block for the ROOT project based on a Markdown input file.
+
+This script reads a Markdown file (e.g. `makeCITATION_example_input.md`, but
+typically the ROOT release notes) containing a bullet list of authors and
+optional ORCIDs, converts it to a Pandoc JSON AST using pypandoc, extracts
+author information.
+"""
+
+
 def printNames(nameBlock):
-    #print(json.dumps(nameBlock))
-    nameParts = [ c for c in nameBlock ]
-    firstLastNames = [ n['c'].replace('\u00a0', ' ') for n in nameParts if n['t'] == 'Str' ]
+    # print(json.dumps(nameBlock))
+    nameParts = [c for c in nameBlock]
+    firstLastNames = [n["c"].replace("\u00a0", " ") for n in nameParts if n["t"] == "Str"]
     firstName = firstLastNames[0]
     lastName = firstLastNames[1]
-    if lastName[-1] == ',':
+    if lastName[-1] == ",":
         lastName = lastName[0:-1]
-    print('  - family-names:', lastName)
-    print('    given-names:', firstName)
+    print("  - family-names:", lastName)
+    print("    given-names:", firstName)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("releasenotes")
-parser.add_argument("--dump-ast", action='store_true')
+parser.add_argument("--dump-ast", action="store_true")
 args = parser.parse_args()
 
-jsonRN = json.loads(pypandoc.convert_file(args.releasenotes, 'json'))
+jsonRN = json.loads(pypandoc.convert_file(args.releasenotes, "json"))
 if args.dump_ast:
     print(json.dumps(jsonRN, indent=1))
     sys.exit(0)
 
 
-ROOTsrcdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(args.releasenotes))))
-versionFileName = os.path.join(ROOTsrcdir, 'build', 'version_number')
-with open(versionFileName) as versionFile:
-    version = versionFile.read()
-
-print('''cff-version: 1.1.0
+print(
+    """cff-version: 1.1.0
 message: "If you use ROOT, please cite it as below."
-authors:''')
+authors:"""
+)
 
-for block in jsonRN['blocks']:
-    if 't' in block and block['t'] == "BulletList":
-        for item in block['c']:
-            itemC = item[0]['c']
-            if itemC[0]['t'] == 'Link':
+for block in jsonRN["blocks"]:
+    if "t" in block and block["t"] == "BulletList":
+        for item in block["c"]:
+            itemC = item[0]["c"]
+            if itemC[0]["t"] == "Link":
                 # 0th element is ["", [], []] ?
-                printNames(itemC[0]['c'][1])
-                orcid = itemC[0]['c'][2][0]
-                print('    orcid:', orcid)
-            elif itemC[0]['t'] == 'Str':
+                printNames(itemC[0]["c"][1])
+                orcid = itemC[0]["c"][2][0]
+                print("    orcid:", orcid)
+            elif itemC[0]["t"] == "Str":
                 printNames(itemC)
             else:
                 print('ERROR: expected "Link" or "Str" in', json.dumps(itemC[0], indent=1))
                 sys.exit(1)
         break
 
-print(f'''title: "ROOT: analyzing, storing and visualizing big data, scientifically"
-version: {version.strip()}
+print(
+    f"""title: "ROOT: analyzing, storing and visualizing big data, scientifically"
+version: {ROOT.__version__}
 doi: 10.5281/zenodo.848818
 date-released: {datetime.datetime.now().strftime("%Y-%m-%d")}
-''')
+"""
+)
