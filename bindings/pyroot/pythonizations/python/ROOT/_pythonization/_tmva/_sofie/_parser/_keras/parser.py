99a8742c1c8086c7e068cdd206d3cce214506a4c
Jonas Rembser <jonas.rembser@cern.ch>
2025-11-08 13:23:37 +0100

    Correctly inject RModelParser_Keras class into Python interfaces
    This is not a pythonization of a C++ class, but a dedicated Python class
    that should be injected in the namespace where it should be.

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_sofie/_parser/_keras/parser.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_sofie/_parser/_keras/parser.py
@@ -537,9 +537,3 @@ class RModelParser_Keras:
             outputNames.append(output_layer_name)
         rmodel.AddOutputTensorNameList(outputNames)
         return rmodel
-
-@pythonization("RModelParser_Keras", ns="TMVA::Experimental::SOFIE")
-def pythonize_rmodelparser_keras(klass):
-    # Parameters:
-    # klass: class to be pythonized 
-    setattr(klass, "Parse", RModelParser_Keras.Parse)
