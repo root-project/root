d94fe2601da2e2a9a2dd4bf508d6eeeeb3f827c7
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2025-11-24 15:23:41 +0100

    TGeoVGConverter: Introduce shape-type filtering for conversion process
    - Added functions 'SelectShapeType()' and 'ExcludeShapeType()'. Conversion is
      performed only on selected shapes (if set), and excluded shapes are always
      skipped.
    
    - TGeoVGShape: Disabled conversion for problematic edge cases not covered by
      exclusion: 'TGeoBBox' with shifted origin and 'TGeoScaledShape'.

--- geom/vecgeom/src/TGeoVGConverter.cxx
+++ geom/vecgeom/src/TGeoVGConverter.cxx
@@ -16,7 +16,34 @@ Geometry converter to VecGeom
 
 #include "TGeoVGConverter.h"
 #include "TGeoVGShape.h"
+#include "TGeoBBox.h"
+#include "TClass.h"
 
+#include <iostream>
+
+namespace {
+
+Bool_t  isInSelection(const TGeoShape* shape,  const std::set<TGeoShape::EShapeType>& selection) {
+   for (auto element : selection) {
+
+      // Special treatment of TGeo::kGeoBox, which is base class for all other shapes
+      if (element == TGeoShape::kGeoBox) {
+         if (typeid(*shape) == typeid(TGeoBBox) ) {
+            return true;
+         } else {
+            continue;
+         }
+      }
+
+      // Just test bit for other shapes
+      if (shape->TestShapeBit(element)) {
+        return true;
+      }
+   }
+   return false;
+}
+
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Default constructor.
@@ -48,14 +75,37 @@ void TGeoVGConverter::ConvertGeometry()
       nconverted++;
       top->SetShape(vgshape);
    }
+
+   // Uncomment to print info about selected/excluded shapes
+   // if ( ! fSelectedShapeTypes.empty()) {
+   //   printf("# %zu selected shape type for conversion \n", fSelectedShapeTypes.size());
+   // }
+   // if ( ! fExcludedShapeTypes.empty()) {
+   //   printf("# %zu shape types excluded from conversion \n", fExcludedShapeTypes.size());
+   // }
+
    // Now iterate the active geometry tree
    TGeoIterator next(fGeom->GetTopVolume());
    TGeoNode *node;
    while ((node = next.Next())) {
       TGeoVolume *vol = node->GetVolume();
-      // If shape not already converted, convert it
+      // Skip shape if already converted
       if (vol->GetShape()->IsVecGeom())
          continue;
+
+      // If fSelectedShapeTypes is not empty, convert only selected shapes
+      if ((! fSelectedShapeTypes.empty()) &&
+          (! isInSelection(vol->GetShape(), fSelectedShapeTypes))) {
+         printf("# Shape type %s is not selected for conversion\n", vol->GetShape()->IsA()->GetName());
+         continue;
+      }
+
+      // Skip shapes excluded from conversion
+      if (isInSelection(vol->GetShape(), fExcludedShapeTypes)) {
+         printf("# Shape type %s is excluded from conversion\n", vol->GetShape()->IsA()->GetName());
+         continue;
+      }
+
       // printf("Converting %s\n", vol->GetName());
       vgshape = TGeoVGShape::Create(vol->GetShape());
       if (vgshape) {
@@ -64,4 +114,21 @@ void TGeoVGConverter::ConvertGeometry()
       }
    }
    printf("# Converted %d shapes to VecGeom ones\n", nconverted);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Select shape(s) for conversion.
+/// Conversion is performed only on selected shapes (if set)
+
+void TGeoVGConverter::SelectShapeType(TGeoShape::EShapeType shapeType)
+{
+   fSelectedShapeTypes.insert(shapeType);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Exclude shape(s) from conversion.
+
+void TGeoVGConverter::ExcludeShapeType(TGeoShape::EShapeType shapeType)
+{
+   fExcludedShapeTypes.insert(shapeType);
 }
