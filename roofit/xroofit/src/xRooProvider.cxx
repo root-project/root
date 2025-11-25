d572b92d3dacda0af78b3e450c0cc0a49862ce97
Sergey Linev <S.Linev@gsi.de>
2025-11-25 16:42:57 +0100

    [browsable] use xRooNode interface to browse elements

--- roofit/xroofit/src/xRooProvider.cxx
+++ roofit/xroofit/src/xRooProvider.cxx
@@ -6,9 +6,12 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#include <ROOT/Browsable/TObjectElement.hxx>
+#include <ROOT/Browsable/RElement.hxx>
 #include <ROOT/Browsable/RProvider.hxx>
+#include <ROOT/Browsable/TObjectItem.hxx>
 #include <ROOT/Browsable/RLevelIter.hxx>
+
+#include <RooFit/xRooFit/xRooNode.h>
 
 
 #include "RooWorkspace.h"
@@ -17,57 +20,109 @@ using namespace ROOT::Browsable;
 using namespace std::string_literals;
 
 
-class xRooBrowsingElement : public TObjectElement {
+class xRooBrowsingElement : public RElement {
+   std::shared_ptr<ROOT::Experimental::XRooFit::xRooNode> fNode;
 public:
-   xRooBrowsingElement(std::unique_ptr<RHolder> &obj) : TObjectElement(obj) {}
-
-   /** Check if want to perform action */
-   bool IsCapable(EActionKind action) const override
-   {
-      return (action == kActDraw6);
-   }
-
+   xRooBrowsingElement(std::shared_ptr<ROOT::Experimental::XRooFit::xRooNode> node);
+   bool IsCapable(EActionKind action) const override;
    /** Get default action */
-   EActionKind GetDefaultAction() const override
+   EActionKind GetDefaultAction() const override;
+
+   std::string GetName() const override { return fNode->GetName(); }
+
+   std::unique_ptr<RLevelIter> GetChildsIter() override;
+};
+
+class xRooLevelIter : public RLevelIter {
+
+   std::shared_ptr<ROOT::Experimental::XRooFit::xRooNode> fNode;
+
+   int fCounter{-1};
+
+public:
+   explicit xRooLevelIter(std::shared_ptr<ROOT::Experimental::XRooFit::xRooNode> node) { fNode = node; }
+
+   ~xRooLevelIter() override = default;
+
+   auto NumElements() const { return fNode->size(); }
+
+   bool Next() override { return ++fCounter < (int) fNode->size(); }
+
+   // use default implementation for now
+   // bool Find(const std::string &name) override { return FindDirEntry(name); }
+
+   std::string GetItemName() const override { return (*fNode)[fCounter]->GetName(); }
+
+   bool CanItemHaveChilds() const override
    {
-      return kActDraw6;
+      return (*fNode)[fCounter]->IsFolder();
    }
 
-   std::string GetContent(const std::string &kind) override
+   /** Create item for the browser */
+   std::unique_ptr<RItem> CreateItem() override
    {
-      return TObjectElement::GetContent(kind);
+      auto xnode = (*fNode)[fCounter];
+
+      auto item = std::make_unique<TObjectItem>(xnode->GetName(), xnode->size());
+
+      item->SetTitle(xnode->GetTitle());
+
+      item->SetClassName(xnode->get()->ClassName());
+      item->SetIcon(RProvider::GetClassIcon(xnode->get()->IsA(), xnode->IsFolder()));
+
+      return item;
+   }
+
+   /** Returns full information for current element */
+   std::shared_ptr<RElement> GetElement() override
+   {
+      auto xnode = (*fNode)[fCounter];
+      return std::make_shared<xRooBrowsingElement>(xnode);
+   }
+
+   bool Find(const std::string &name, int indx = -1) override
+   {
+      if ((indx >= 0) && (indx < (int) fNode->size()) && (name == (*fNode)[indx]->GetName())) {
+         fCounter = indx;
+         return true;
+      }
+
+      return RLevelIter::Find(name, -1);
    }
 
 };
 
 
-////////////////////////////////////////////////////////////
-/// Representing RooWorkspace in browsables
-
-class xRooWorkspaceElement : public TObjectElement {
-
-public:
-   xRooWorkspaceElement(std::unique_ptr<RHolder> &br) : TObjectElement(br) {}
-
-   Long64_t GetSize() const override
-   {
-      auto ws = dynamic_cast<const RooWorkspace *>(CheckObject());
-      return ws ? 100 : -1;
-   }
-
-   /** Get default action */
-   EActionKind GetDefaultAction() const override
-   {
-      return kActBrowse;
-   }
+xRooBrowsingElement::xRooBrowsingElement(std::shared_ptr<ROOT::Experimental::XRooFit::xRooNode> node)
+{
+   fNode = node;
+}
 
    /** Check if want to perform action */
-   bool IsCapable(EActionKind action) const override
-   {
-      return action == kActBrowse;
-   }
+bool xRooBrowsingElement::IsCapable(RElement::EActionKind action) const
+{
+   return action == kActDraw6;
+}
 
-};
+   /** Get default action */
+RElement::EActionKind xRooBrowsingElement::GetDefaultAction() const
+{
+   return kActDraw6;
+}
+
+std::unique_ptr<RLevelIter> xRooBrowsingElement::GetChildsIter()
+{
+   // here central part of hole story
+   // one need to provide list of sub-items via iterator
+
+   fNode->browse();
+
+   if (fNode->size() == 0)
+      return nullptr;
+
+   return std::make_unique<xRooLevelIter>(fNode);
+}
+
 
 // ==============================================================================================
 
@@ -77,7 +132,13 @@ public:
    xRooProvider()
    {
       RegisterBrowse(RooWorkspace::Class(), [](std::unique_ptr<RHolder> &object) -> std::shared_ptr<RElement> {
-         return std::make_shared<xRooWorkspaceElement>(object);
+         auto wk = object->get_shared<RooWorkspace>();
+
+         printf("Create entry for workspace %s %s\n", wk->GetName(), wk->ClassName());
+
+         auto wkNode = std::make_shared<ROOT::Experimental::XRooFit::xRooNode>(wk);
+
+         return std::make_shared<xRooBrowsingElement>(wkNode);
       });
    }
 
