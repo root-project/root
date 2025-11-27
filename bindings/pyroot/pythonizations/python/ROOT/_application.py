4e575fea3cc4d8c98e55b69b6634ff84d639e7d4
Sergey Linev <S.Linev@gsi.de>
2025-11-27 12:25:46 +0100

    Support interactive graphics in python scripts  #17587
    * [pyroot] introduce TCanvas.Update pythonization
    
    If extra `block` flag specified, python script execution
    is blocked until <space> key is pressed.
    At the same time ROOT events loop continue running.
    
    Solves problem of ROOT graphics usage in
    non-interactive python
    
    * [pyroot] do not create extra thread for events processing
    
    While now events processing has to be done in the canvas.Update method,
    additional processing is not necessary.
    Such approach was not thread safe and did not work on Windows at all
    
    * Do not use PyOS_InputHook on windows
    
    * [pyroot] handle ProcessEvents() return value
    
    It is interrupt flag, which can be triggered from canvas context menu.
    Has similar effect as prssing space button
    
    * Add documentation for TCanvas.Update pythonization
    
    * Add block flag to TCanvas.Draw pythonization
    
    Same as with `c.Update(True)`,
    `c.Draw(True)` will run event loop until key is pressed. Also update will be performed once.
    
    * Adjust pythonization of TCanvas::Draw method
    
    Add block as second option keeping str option as first. So one can use old code with option as is.
    And if necessary call `canv.Draw(block = True)`

--- bindings/pyroot/pythonizations/python/ROOT/_application.py
+++ bindings/pyroot/pythonizations/python/ROOT/_application.py
@@ -83,29 +83,17 @@ class PyROOTApplication(object):
         if self._is_ipython and "IPython" in sys.modules and sys.modules["IPython"].version_info[0] >= 5:
             # ipython and notebooks, register our event processing with their hooks
             self._ipython_config()
-        elif sys.flags.interactive == 1 or not hasattr(__main__, "__file__") or gSystem.InheritsFrom("TMacOSXSystem"):
+        elif (sys.flags.interactive == 1 or not hasattr(__main__, '__file__')) and not gSystem.InheritsFrom('TWinNTSystem'):
             # Python in interactive mode, use the PyOS_InputHook to call our event processing
             # - sys.flags.interactive checks for the -i flags passed to python
             # - __main__ does not have the attribute __file__ if the Python prompt is started directly
-            # - MacOS does not allow to run a second thread to process events, fall back to the input hook
+            # - does not work properly on Windows
             self._inputhook_config()
+            gEnv.SetValue("WebGui.ExternalProcessEvents", "yes")
         else:
-            # Python in script mode, start a separate thread for the event processing
+            # Python in script mode, instead of separate thread methods like canvas.Update should run events
 
             # indicate that ProcessEvents called in different thread, let ignore thread id checks in RWebWindow
             gEnv.SetValue("WebGui.ExternalProcessEvents", "yes")
-
-            def _process_root_events(self):
-                while self.keep_polling:
-                    gSystem.ProcessEvents()
-                    time.sleep(0.01)
-
-            import threading
-
-            self.keep_polling = True  # Used to shut down the thread safely at teardown time
-            update_thread = threading.Thread(None, _process_root_events, None, (self,))
-            self.process_root_events = update_thread  # The thread is joined at teardown time
-            update_thread.daemon = True
-            update_thread.start()
 
         self._set_display_hook()
---
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tcanvas.py
@@ -0,0 +1,141 @@
+# Author: Sergey Linev GSI  01/2025
+
+################################################################################
+# Copyright (C) 1995-2025, Rene Brun and Fons Rademakers.                      #
+# All rights reserved.                                                         #
+#                                                                              #
+# For the licensing terms see $ROOTSYS/LICENSE.                                #
+# For the list of contributors see $ROOTSYS/README/CREDITS.                    #
+################################################################################
+
+r'''
+\pythondoc TCanvas
+
+Functionality of TCanvas::Update() method was extended to support interactive
+graphics in the python scripts. If extra block parameter is True, script execution
+will be suspended until <space> key pressed by user. Simple example:
+
+\code{.py}
+\endcode
+import ROOT
+
+c = ROOT.TCanvas()
+h = ROOT.TH1I("h1", "h1", 100, -5, 5)
+h.FillRandom("gaus", 10000)
+h.Draw("")
+
+# block here until space is pressed
+c.Update(True)
+
+# continues after <space> is pressed
+c.SaveAs("canvas.root")
+\endpythondoc
+'''
+
+from . import pythonization
+
+def wait_press_windows():
+   from ROOT import gSystem
+   import msvcrt
+   import time
+
+   while not gSystem.ProcessEvents():
+      if msvcrt.kbhit():
+         k = msvcrt.getch()
+         if k[0] == 32:
+            break
+      else:
+         time.sleep(0.01)
+
+
+def wait_press_posix():
+   from ROOT import gSystem
+   import sys
+   import select
+   import tty
+   import termios
+   import time
+
+   old_settings = termios.tcgetattr(sys.stdin)
+
+   tty.setcbreak(sys.stdin.fileno())
+
+   try:
+
+      while not gSystem.ProcessEvents():
+         c = ''
+         if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            c = sys.stdin.read(1)
+         if (c == '\x20'):
+            break
+         time.sleep(0.01)
+
+   finally:
+      termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+def run_root_event_loop():
+   from ROOT import gROOT
+   import os
+   import sys
+
+   # no special handling in batch mode
+   if gROOT.IsBatch():
+      return
+
+   # no special handling in case of notebooks
+   if 'IPython' in sys.modules and sys.modules['IPython'].version_info[0] >= 5:
+      return
+
+   print("Press <space> key to continue")
+
+   if os.name == 'nt':
+      wait_press_windows()
+   else:
+      wait_press_posix()
+
+
+def _TCanvas_Update(self, block = False):
+   """
+   Updates the canvas.
+   Also blocks script execution and runs the ROOT graphics event loop until the <space> keyword is pressed,
+   but only if the following conditions are met:
+   * The `block` optional argument is set to `True`.
+   * ROOT graphics are enabled, i.e. `ROOT.gROOT.IsBatch() == False`.
+   * The script is running not in ipython notebooks.
+   """
+
+   self._Update()
+
+   # run loop if block flag is set
+   if block:
+      run_root_event_loop()
+
+
+def _TCanvas_Draw(self, option: str = "", block: bool = False):
+   """
+   Draw the canvas.
+   Also blocks script execution and runs the ROOT graphics event loop until the <space> keyword is pressed,
+   but only if the following conditions are met:
+   * The `block` optional argument is set to `True`.
+   * ROOT graphics are enabled, i.e. `ROOT.gROOT.IsBatch() == False`.
+   * The script is running not in ipython notebooks.
+   """
+
+   self._Draw(option)
+
+   # run loop if block flag is set
+   if block:
+      self._Update()
+      run_root_event_loop()
+
+
+@pythonization('TCanvas')
+def pythonize_tcanvas(klass):
+    # Parameters:
+    # klass: class to be pythonized
+
+    klass._Update = klass.Update
+    klass._Draw = klass.Draw
+    klass.Update = _TCanvas_Update
+    klass.Draw = _TCanvas_Draw
+
