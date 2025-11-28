root-project/root

commit e8b4e421865b78fbdc40e78cf17ec2e6caafa6f5

[Geom] Add auxiliary user-data export in TGDMLWrite This adds the option of exporting auxiliary of TGeoVolumes in TGDMLWrite Fixes #20356

geom/gdml/src/TGDMLWrite.cxx
@@ -156,7 +156,10 @@ See that function for details.
 #include "TGeoElement.h"
 #include "TGeoShape.h"
 #include "TGeoCompositeShape.h"
+#include "TGeoExtension.h"
 #include "TGeoOpticalSurface.h"
+#include "TMap.h"
+#include "TObjString.h"
 #include <cstdlib>
 #include <string>
 #include <map>
@@ -756,6 +759,41 @@ void TGDMLWrite::ExtractVolumes(TGeoNode *node)
       // create division node
       childN = CreateDivisionN(offset, width, ndiv, axis.Data(), unit.Data(), nodeVolNameBak.Data());
       fGdmlE->AddChild(volumeN, childN);
+   }
+
+   // export auxiliary user-data if present (TMap of TObjString->TObjString)
+   {
+      TGeoRCExtension *rcext = (TGeoRCExtension *)volume->GetUserExtension();
+      if (rcext) {
+         TMap *auxmap = nullptr;
+         TObject *userObj = rcext->GetUserObject();
+         if (userObj && userObj->InheritsFrom("TMap")) {
+            TMap *auxmap = (TMap *)userObj;
+            TIterator *it = auxmap->MakeIterator();
+            TObject *k = nullptr;
+            while (k = it->Next()) {
+               TObject *valobj = auxmap->GetValue(k);
+               if (!valobj || !k->InheritsFrom("TObjString") || !valobj->InheritsFrom("TObjString"))
+                  continue;
+               TObjString *key = (TObjString *)k;
+               TObjString *val = (TObjString *)valobj;
+               TString auxtype = key->GetString();
+               TString auxvalue = val->GetString();
+               TString auxunit = "";
+               Int_t pos = auxvalue.Index('*');
+               if (pos >= 0) {
+                  auxunit = auxvalue(pos + 1, auxvalue.Length());
+                  auxvalue = auxvalue(0, pos);
+               }
+               XMLNodePointer_t auxN = fGdmlE->NewChild(nullptr, nullptr, "auxiliary", nullptr);
+               fGdmlE->NewAttr(auxN, nullptr, "auxtype", auxtype.Data());
+               fGdmlE->NewAttr(auxN, nullptr, "auxvalue", auxvalue.Data());
+               if (!auxunit.IsNull())
+                  fGdmlE->NewAttr(auxN, nullptr, "auxunit", auxunit.Data());
+               fGdmlE->AddChild(volumeN, auxN);
+            }
+         }
+      }
    }
 
    fVolCnt++;

commit 53c3e052188885540b97725cc9e96b853f507153

[Geom] Fix compiler warnings in TGDMLWrite
This follows up on e8b4e421865, which introduced some compiler warnings
that cause the Debian CI to fail.

geom/gdml/src/TGDMLWrite.cxx
@@ -765,13 +765,12 @@ void TGDMLWrite::ExtractVolumes(TGeoNode *node)
    {
       TGeoRCExtension *rcext = (TGeoRCExtension *)volume->GetUserExtension();
       if (rcext) {
-         TMap *auxmap = nullptr;
          TObject *userObj = rcext->GetUserObject();
          if (userObj && userObj->InheritsFrom("TMap")) {
             TMap *auxmap = (TMap *)userObj;
             TIterator *it = auxmap->MakeIterator();
             TObject *k = nullptr;
-            while (k = it->Next()) {
+            while ((k = it->Next())) {
                TObject *valobj = auxmap->GetValue(k);
                if (!valobj || !k->InheritsFrom("TObjString") || !valobj->InheritsFrom("TObjString"))
                   continue;