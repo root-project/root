046f9143a5e98c48c89c8f73a5623530f8ff0773
ferdymercury <ferdymercury@users.noreply.github.com>
2025-12-01 11:03:26 +0100

    [test] adapt tabcom test with public member

--- core/rint/test/TTabComTests.cxx
+++ core/rint/test/TTabComTests.cxx
@@ -95,8 +95,8 @@ TEST(TTabComTests, CompleteTProfile)
 
 TEST(TTabComTests, CompleteTObj)
 {
-   std::string expected = "TObjArray TObjArrayIter TObjLink TObjOptLink TObjString"
-      " TObject TObjectRefSpy TObjectSpy TObjectTable";
+   std::string expected = "TObjArray TObjArrayIter TObjLink TObjLinkPtr_t TObjOptLink"
+      " TObjString TObject TObjectRefSpy TObjectSpy TObjectTable";
    // FIXME: See ROOT-10989
    ASSERT_STREQ(expected.c_str(), GetCompletions("TObj",
                                                  /*ignore=*/{"TObjectDisplayItem", "TObjectDrawable", "TObjectHolder",
