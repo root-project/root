acc3a769698a4edc8e399902935e7a5976ac8c3e
silverweed <giacomo.parolini@cern.ch>
2025-12-02 10:02:10 +0100

    [core] Move FileRaii to TestSupport.hxx
    This is a general-purpose utility that can be used by many unit tests
    so it makes sense to deduplicate it and share it.

--- io/io/test/rfile.cxx
+++ io/io/test/rfile.cxx
@@ -13,37 +13,7 @@
 #include <ROOT/RLogger.hxx>
 
 using ROOT::Experimental::RFile;
-
-namespace {
-
-/**
- * An RAII wrapper around an open temporary file on disk. It cleans up the guarded file when the wrapper object
- * goes out of scope.
- */
-class FileRaii {
-private:
-   std::string fPath;
-   bool fPreserveFile = false;
-
-public:
-   explicit FileRaii(const std::string &path) : fPath(path) {}
-   FileRaii(FileRaii &&) = default;
-   FileRaii(const FileRaii &) = delete;
-   FileRaii &operator=(FileRaii &&) = default;
-   FileRaii &operator=(const FileRaii &) = delete;
-   ~FileRaii()
-   {
-      if (!fPreserveFile)
-         std::remove(fPath.c_str());
-   }
-   const std::string &GetPath() const { return fPath; }
-
-   // Useful if you want to keep a test file after the test has finished running
-   // for debugging purposes. Should only be used locally and never pushed.
-   void PreserveFile() { fPreserveFile = true; }
-};
-
-} // anonymous namespace
+using ROOT::TestSupport::FileRaii;
 
 static std::string JoinKeyNames(const ROOT::Experimental::RFileKeyIterable &iterable)
 {
