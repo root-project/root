0a625923b801ea09a6cabc006a72b7de6a1377ac
silverweed <giacomo.parolini@cern.ch>
2025-12-02 10:02:10 +0100

    [core] Move FileRaii to TestSupport.hxx
    This is a general-purpose utility that can be used by many unit tests
    so it makes sense to deduplicate it and share it.

--- tree/ntupleutil/test/ntupleutil_test.hxx
+++ tree/ntupleutil/test/ntupleutil_test.hxx
@@ -9,30 +9,8 @@
 #include <ROOT/RNTupleReader.hxx>
 #include <ROOT/RNTupleWriteOptions.hxx>
 #include <ROOT/RNTupleWriter.hxx>
+#include <ROOT/TestSupport.hxx>
 
-/**
- * An RAII wrapper around an open temporary file on disk. It cleans up the
- * guarded file when the wrapper object goes out of scope.
- */
-class FileRaii {
-private:
-   std::string fPath;
-   bool fPreserveFile = false;
-
-public:
-   explicit FileRaii(const std::string &path) : fPath(path) {}
-   FileRaii(const FileRaii &) = delete;
-   FileRaii &operator=(const FileRaii &) = delete;
-   ~FileRaii()
-   {
-      if (!fPreserveFile)
-         std::remove(fPath.c_str());
-   }
-   std::string GetPath() const { return fPath; }
-
-   // Useful if you want to keep a test file after the test has finished running
-   // for debugging purposes. Should only be used locally and never pushed.
-   void PreserveFile() { fPreserveFile = true; }
-};
+using ROOT::TestSupport::FileRaii;
 
 #endif // ROOT7_RNTupleUtil_Test
