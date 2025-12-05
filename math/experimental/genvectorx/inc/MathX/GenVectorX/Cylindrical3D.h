b151ee4037d4d11d0e78de5f8cfdf19beb770168
ferdymercury <ferdymercury@users.noreply.github.com>
2025-12-05 18:18:19 +0100

    [mathx] rename CINT to CLING and MAKECINT to ROOTCLING
    (cherry picked from commit cc8d7f87404347fd6b836af33e52286ba6da4b13)

--- math/experimental/genvectorx/inc/MathX/GenVectorX/Cylindrical3D.h
+++ math/experimental/genvectorx/inc/MathX/GenVectorX/Cylindrical3D.h
@@ -217,7 +217,7 @@ public:
 
    // (none)
 
-#if defined(__MAKECINT__) || defined(G__DICTIONARY)
+#if defined(__ROOTCLING__) || defined(G__DICTIONARY)
 
    // ====== Set member functions for coordinates in other systems =======
 
@@ -247,7 +247,7 @@ private:
 
 #include "MathX/GenVectorX/Cartesian3D.h"
 
-#if defined(__MAKECINT__) || defined(G__DICTIONARY)
+#if defined(__ROOTCLING__) || defined(G__DICTIONARY)
 #include "MathX/GenVectorX/GenVector_exception.h"
 #include "MathX/GenVectorX/CylindricalEta3D.h"
 #include "MathX/GenVectorX/Polar3D.h"
@@ -263,7 +263,7 @@ void Cylindrical3D<T>::SetXYZ(Scalar xx, Scalar yy, Scalar zz)
    *this = Cartesian3D<Scalar>(xx, yy, zz);
 }
 
-#if defined(__MAKECINT__) || defined(G__DICTIONARY)
+#if defined(__ROOTCLING__) || defined(G__DICTIONARY)
 #if !defined(ROOT_MATH_SYCL) && !defined(ROOT_MATH_CUDA)
 
 // ====== Set member functions for coordinates in other systems =======
