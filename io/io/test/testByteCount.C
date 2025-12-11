5359dcb5e18fb485b374e2f82afab9b97bbf9b74
Philippe Canal <pcanal@fnal.gov>
2025-12-11 13:02:11 -0600

    [NFC] io white space / byte count related

--- io/io/test/testByteCount.C
+++ io/io/test/testByteCount.C
@@ -6,8 +6,8 @@
    // SetBufferOffset does not check against the size,
    // so we can provide and use a larger buffer.
    std::vector<char> databuffer{};
-   databuffer.reserve( 4*1024*1024*1024ll );
-   TBufferFile b(TBuffer::kWrite, 2*1024*1024*1024ll-100, databuffer.data(), false /* don't adopt */);
+   databuffer.reserve(4 * 1024 * 1024 * 1024ll);
+   TBufferFile b(TBuffer::kWrite, 2 * 1024 * 1024 * 1024ll - 100, databuffer.data(), false /* don't adopt */);
    {
       // Regular object at offset 0
       UInt_t R__c = b.WriteVersion(TExMap::Class(), kTRUE);
@@ -23,13 +23,13 @@
    {
       // Object larger than 1GB
       UInt_t R__c = b.WriteVersion(TExMap::Class(), kTRUE);
-      b.SetBufferOffset(4000+1*1024*1024*1024ll);
+      b.SetBufferOffset(4000 + 1 * 1024 * 1024 * 1024ll);
       b.SetByteCount(R__c, kTRUE);
    }
    {
       // Regular object located past 1GB
       UInt_t R__c = b.WriteVersion(TExMap::Class(), kTRUE);
-      b.SetBufferOffset(8000+1*1024*1024*1024ll);
+      b.SetBufferOffset(8000 + 1 * 1024 * 1024 * 1024ll);
       b.SetByteCount(R__c, kTRUE);
    }
    {
@@ -40,18 +40,17 @@
       // However the lifting might be temporary, so this might need to be
       // moved to a test that stored objects in a TFile.
       UInt_t R__c = b.WriteVersion(TExMap::Class(), kTRUE);
-      b.SetBufferOffset(12000+2*1024*1024*1024ll);
+      b.SetBufferOffset(12000 + 2 * 1024 * 1024 * 1024ll);
       b.SetByteCount(R__c, kTRUE);
    }
 
-
    // To make a copy instead of using the const references:
-   auto bytecounts{ b.GetByteCounts() };
+   auto bytecounts{b.GetByteCounts()};
    if (bytecounts.size() != expectedByteCounts) {
       ++errors;
       std::cerr << "The number of bytecount is not as expected (1), it is " << bytecounts.size() << '\n';
       std::cerr << "The full list is:\n";
-      for(auto bc : bytecounts)
+      for (auto bc : bytecounts)
          std::cerr << "values: " << bc.first << " , " << bc.second << '\n';
    }
 
@@ -85,7 +84,7 @@
    {
       // Object larger than 1GB
       auto version = b.ReadVersion(&R__s, &R__c, TExMap::Class());
-      b.SetBufferOffset(4000+1*1024*1024*1024ll);
+      b.SetBufferOffset(4000 + 1 * 1024 * 1024 * 1024ll);
       auto res = b.CheckByteCount(R__s, R__c, TExMap::Class());
       if (res != 0) {
          ++errors;
@@ -95,7 +94,7 @@
    {
       // Regular object located past 1GB
       auto version = b.ReadVersion(&R__s, &R__c, TExMap::Class());
-      b.SetBufferOffset(8000+1*1024*1024*1024ll);
+      b.SetBufferOffset(8000 + 1 * 1024 * 1024 * 1024ll);
       auto res = b.CheckByteCount(R__s, R__c, TExMap::Class());
       if (res != 0) {
          ++errors;
@@ -106,7 +105,7 @@
       // Object larger than 1GB start after 1GB
       // NOTE: this does not yet fit.
       auto version = b.ReadVersion(&R__s, &R__c, TExMap::Class());
-      b.SetBufferOffset(12000+2*1024*1024*1024ll);
+      b.SetBufferOffset(12000 + 2 * 1024 * 1024 * 1024ll);
       auto res = b.CheckByteCount(R__s, R__c, TExMap::Class());
       if (res != 0) {
          ++errors;
