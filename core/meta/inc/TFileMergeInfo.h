b190537047d2b1c741a749ec45d83d40e09713be
Danilo Piparo <danilo.piparo@cern.ch>
2025-12-15 11:57:27 +0100

    [core] Remove type alias

--- core/meta/inc/TFileMergeInfo.h
+++ core/meta/inc/TFileMergeInfo.h
@@ -40,18 +40,17 @@ class TIOFeatures;
 
 class TFileMergeInfo {
 private:
-   using TIOFeatures = ROOT::TIOFeatures;
-
    TFileMergeInfo() = delete;
    TFileMergeInfo(const TFileMergeInfo&) = delete;
    TFileMergeInfo& operator=(const TFileMergeInfo&) = delete;
 
 public:
+
    TDirectory  *fOutputDirectory{nullptr}; // Target directory where the merged object will be written.
    Bool_t       fIsFirst{kTRUE};           // True if this is the first call to Merge for this series of object.
    TString      fOptions;                  // Additional text based option being passed down to customize the merge.
    TObject     *fUserData{nullptr};        // Place holder to pass extra information.  This object will be deleted at the end of each series of objects.
-   TIOFeatures *fIOFeatures{nullptr};      // Any ROOT IO features that should be explicitly enabled.
+   ROOT::TIOFeatures *fIOFeatures{nullptr};      // Any ROOT IO features that should be explicitly enabled.
 
    TFileMergeInfo(TDirectory *outputfile) : fOutputDirectory(outputfile) {}
    virtual ~TFileMergeInfo() { delete fUserData; } ;
