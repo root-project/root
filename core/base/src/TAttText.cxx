f56079db71459dc86a1484cb5de8092d9c734a13
Aryan Srivastava <aryansriva05@gmail.com>
2026-01-09 20:13:10 +0530

    Update core/base/src/TAttText.cxx
    Co-authored-by: ferdymercury <ferdymercury@users.noreply.github.com>

--- core/base/src/TAttText.cxx
+++ core/base/src/TAttText.cxx
@@ -4,7 +4,7 @@
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
- * *
+ *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
