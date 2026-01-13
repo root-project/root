2194bdbb26131548f2424ac23640adda4457df59
Shlok <shlok.saxena.cse22@itbhu.ac.in>
2026-01-13 21:26:00 +0000

    [tmva][sofie] Handle ONNX Gelu approximate attribute

--- tmva/sofie_parsers/src/ParseGelu.cxx
+++ tmva/sofie_parsers/src/ParseGelu.cxx
@@ -2,6 +2,9 @@
 #include "TMVA/ROperator_Gelu.hxx"
 #include "onnx_proto3.pb.h"
 
+#include <stdexcept>
+#include <string>
+
 namespace TMVA {
 namespace Experimental {
 namespace SOFIE {
@@ -9,11 +12,35 @@ namespace SOFIE {
 ParserFuncSignature ParseGelu = [](RModelParser_ONNX &parser, const onnx::NodeProto &nodeproto) {
    ETensorType input_type;
 
+   // --- Handle ONNX Gelu attribute: approximate ---
+   // ONNX Gelu has attribute "approximate": "none" (default) or "tanh"
+   std::string approximate = "none";
+   for (const auto &attr : nodeproto.attribute()) {
+      if (attr.name() == "approximate") {
+         if (attr.type() != onnx::AttributeProto::STRING)
+            throw std::runtime_error(
+               "TMVA::SOFIE ONNX Parser: Gelu attribute 'approximate' must be a string");
+
+         approximate = attr.s();
+      }
+   }
+
+   if (approximate != "none") {
+      if (approximate == "tanh") {
+         throw std::runtime_error(
+            "TMVA::SOFIE ONNX Parser: Gelu attribute approximate='tanh' not supported yet");
+      }
+
+      throw std::runtime_error(
+         "TMVA::SOFIE ONNX Parser: Gelu attribute approximate='" + approximate +
+         "' not supported (expected 'none' or 'tanh')");
+   }
+
    auto input_name = nodeproto.input(0);
    if (parser.IsRegisteredTensorType(input_name)) {
       input_type = parser.GetTensorType(input_name);
    } else {
-      throw std::runtime_error("TMVA::SOFIE ONNX Parser Gelu op has input tensor" + input_name +
+      throw std::runtime_error("TMVA::SOFIE ONNX Parser Gelu op has input tensor " + input_name +
                                " but its type is not yet registered");
    }
 
