1c869fa2e00e0fd4bcc88b0123ad1a701a098307
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2026-01-14 10:45:44 +0100

    [roottest] Fix TMPIFile macro

--- roottest/root/io/tmpifile/split.C
+++ roottest/root/io/tmpifile/split.C
@@ -1,10 +1,4 @@
 void split()
 {
-   try
-   {
-      TMPIFile *newfile = new TMPIFile("test_split.root", "RECREATE", 2);
-   } catch(const std::exception&)
-   {
-      return EXIT_FAILURE;
-   }
+   TMPIFile *newfile = new TMPIFile("test_split.root", "RECREATE", 2);
 }
