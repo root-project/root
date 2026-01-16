aba00c04fb3f69275b4f31d97c809e9aafe0aad2
Jakob Blomer <jblomer@cern.ch>
2026-01-16 14:39:36 +0100

    style fix in RRawFile::Create() plugin selection

--- io/io/src/RRawFile.cxx
+++ io/io/src/RRawFile.cxx
@@ -71,12 +71,10 @@ ROOT::Internal::RRawFile::Create(std::string_view url, ROptions options)
       return std::unique_ptr<RRawFile>(new RRawFileUnix(url, options));
 #endif
    }
-   if (transport == "http" || transport == "https" ||
-       transport == "root" || transport == "roots" ) {
-      std::string plgclass = transport.compare( 0, 4, "http" ) == 0 ?
-                             "RRawFileDavix" : "RRawFileNetXNG";
-      if (TPluginHandler *h = gROOT->GetPluginManager()->
-          FindHandler("ROOT::Internal::RRawFile", std::string(url).c_str())) {
+   if (transport == "http" || transport == "https" || transport == "root" || transport == "roots") {
+      std::string plgclass = transport.compare(0, 4, "http") == 0 ? "RRawFileDavix" : "RRawFileNetXNG";
+      if (TPluginHandler *h =
+             gROOT->GetPluginManager()->FindHandler("ROOT::Internal::RRawFile", std::string(url).c_str())) {
          if (h->LoadPlugin() == 0) {
             return std::unique_ptr<RRawFile>(reinterpret_cast<RRawFile *>(h->ExecPlugin(2, &url, &options)));
          }
