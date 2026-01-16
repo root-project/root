root-project/root

commit b8e9a6a6c1e8852805e7196a57b2741ca8d782ee

[RF] Don't recreate hash tables in `RooLinkedList::Add()`
Thanks to #7818, where the `RooHashTable` was migrated to
`std::unordered_map`, we can now use proper `reserve()` functions to
increase the hash table size when adding elements.

The current mechanism deleting and recreating the hash tables from
scratch when the list size changed incurred a pretty large overhead.
In fact, it made adding to the list a `O(n)` operation, because it has
to iterate over the collection to fill the hash table again.
Consequently, copying a `RooLinkedList` was `O(n^2)`, which is
unexpected to the user.

This change will speed up the handling of objects in a RooWorkspace,
which uses `RooLinkedLists` to manage the elemens.

roofit/roofitcore/src/RooLinkedList.cxx
@@ -327,33 +327,25 @@ RooLinkedList& RooLinkedList::operator=(const RooLinkedList& other)
 
 void RooLinkedList::setHashTableSize(Int_t size)
 {
-  if (size<0) {
-    coutE(InputArguments) << "RooLinkedList::setHashTable() ERROR size must be positive" << std::endl ;
-    return ;
-  }
-  if (size==0) {
-    if (!_htableName) {
-      // No hash table present
-      return ;
-    } else {
+   if (size < 0) {
+      coutE(InputArguments) << "RooLinkedList::setHashTable() ERROR size must be positive" << std::endl;
+      return;
+   }
+   if (size == 0) {
       // Remove existing hash table
       _htableName.reset();
       _htableLink.reset();
-    }
-  } else {
+      return;
+   }
 
-    // (Re)create hash tables
-    _htableName = std::make_unique<HashTableByName>(size) ;
-    _htableLink = std::make_unique<HashTableByLink>(size) ;
+   if (!_htableName) {
+      // (Re)create hash tables
+      _htableName = std::make_unique<HashTableByName>(size);
+      _htableLink = std::make_unique<HashTableByLink>(size);
+   }
 
-    // Fill hash table with existing entries
-    RooLinkedListElem* ptr = _first ;
-    while(ptr) {
-      _htableName->insert({ptr->_arg->GetName(), ptr->_arg}) ;
-      _htableLink->insert({ptr->_arg, reinterpret_cast<TObject*>(ptr)}) ;
-      ptr = ptr->_next ;
-    }
-  }
+   _htableName->reserve(size);
+   _htableLink->reserve(size);
 }
 
 ////////////////////////////////////////////////////////////////////////////////

commit c25a34e8e0a5ed6d38beb20858edd6407073ee48

[RF] Fixup `RooLinkedList::setHashTableSize()`
This fixes a regression introduced in b8e9a6a, where I forgot to
backfill newly-created hash tables.

A unit test with the reproducer from the issue is also implemented.

Closes #20904.

roofit/roofitcore/src/RooLinkedList.cxx
@@ -342,6 +342,11 @@ void RooLinkedList::setHashTableSize(Int_t size)
       // (Re)create hash tables
       _htableName = std::make_unique<HashTableByName>(size);
       _htableLink = std::make_unique<HashTableByLink>(size);
+
+      for (RooLinkedListElem *elem = _first; elem; elem = elem->_next) {
+         _htableName->insert({elem->_arg->GetName(), elem->_arg});
+         _htableLink->insert({elem->_arg, reinterpret_cast<TObject *>(elem)});
+      }
    }
 
    _htableName->reserve(size);

roofit/roofitcore/test/CMakeLists.txt
@@ -117,3 +117,5 @@ ROOT_ADD_GTEST(testRooWorkspace testRooWorkspace.cxx LIBRARIES RooFit RooStats
                ${CMAKE_CURRENT_SOURCE_DIR}/data/test_workspace_01.root
                ${CMAKE_CURRENT_SOURCE_DIR}/data/workspace_with_model_config_classdef_6.root
                )
+
+ROOT_ADD_GTEST(testRooLinkedList testRooLinkedList.cxx LIBRARIES RooFitCore)

roofit/roofitcore/test/testRooLinkedList.cxx
@@ -1,0 +1,33 @@
+#include <RooDataSet.h>
+#include <RooLinkedList.h>
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <sstream>
+
+// Covers https://github.com/root-project/root/issues/20904
+TEST(RooLinkedList, InitializeHashTableForNonEmptyLinkedList)
+{
+   const int max_n = 100;
+
+   for (int n = 1; n < max_n; ++n) {
+
+      RooLinkedList dataList;
+
+      for (int i = 0; i < n; ++i) {
+         auto d = new RooDataSet{"dataset_" + std::to_string(i), "", {}};
+
+         if (dataList.size() > 50 && dataList.getHashTableSize() == 0) {
+            dataList.setHashTableSize(200);
+         }
+         dataList.Add(d);
+      }
+
+      auto found = dataList.FindObject("dataset_0");
+
+      ASSERT_TRUE(found) << "dataset retrieval failed with " << n << " datasets";
+
+      dataList.Delete();
+   }
+}