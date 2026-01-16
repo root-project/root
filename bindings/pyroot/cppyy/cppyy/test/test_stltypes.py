d6634eac072046d381bca56c901c9bfa757578ae
Jonas Rembser <jonas.rembser@cern.ch>
2026-01-16 11:33:56 +0100

    [CPyCppyy] Fix `std::wstring` Pythonization
    Follows up on 760b6cc5c7c5, where not all possible type names were used
    to match `std::wstring`. Somtimes, the initial `basic_string` part is
    still prefixed with `std::`.
    
    This enables the `test02_string_data_access` unit test in
    `test_stltypes` from the cppyy test suite on all platforms.
    
    (cherry picked from commit 91e7128f73d967e8835f2e2464418ebfc78f3f8a)

--- bindings/pyroot/cppyy/cppyy/test/test_stltypes.py
+++ bindings/pyroot/cppyy/cppyy/test/test_stltypes.py
@@ -861,7 +861,6 @@ class TestSTLSTRING:
 
             raises(TypeError, c.get_string2, "temp string")
 
-    @mark.xfail()
     def test02_string_data_access(self):
         """Test access to std::string object data members"""
 
