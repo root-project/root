root-project/root

commit a5b1ed9080aca63347d3156540864f774ac49d4d

[core] Generalize TROOT::GetIncludeDir() using build tree marker
Introducing a new marker file generalizes `TROOT::GetIncludeDir()` to
work in all cases for both relocated install and build trees, even if
the `CMAKE_INSTALL_INCLUDEDIR` is different from `include`.

This follows up on 43ec084a46, which was incomplete and left ROOT
configuration in a broken state, because `ROOTINCDIR` was not defined
anymore but still used.

The `RConfigure.h` header still defines a few other internal macros with
hardcoded paths, and if the suggested mechanism proves to be stable for
the include directory, we can also migrate the other macros. They are
fragile anyway. For example, they contain relative paths for
`gnuinstall=OFF` and absolute paths for `gnuinstall=ON`, which is
nowhere documented and breaks relocatable builds. Also, they use
`ROOTSYS` in the case of `gnuinstall=OFF`, which we want to migrate away
from.

The `ROOT::FoundationUtils::GetIncludeDir()` function was also removed
and completely absorbed into `TROOT::GetIncludeDir()`, as it was not
used in any other place.

Also, allow absolute `CMAKE_INSTALL_INCLUDEDIR` paths.

core/base/CMakeLists.txt
@@ -236,9 +236,21 @@ if(core_soversion)
 else()
     set(full_core_filename "${core_prefix}Core${core_suffix}")
 endif()
+
+# Absolue CMAKE_INSTALL_<dir> paths are discouraged in CMake, but some
+# packagers use them anyway. So we support it.
+if(IS_ABSOLUTE ${CMAKE_INSTALL_INCLUDEDIR})
+   set(install_includedir_is_absolute 1)
+else()
+   set(install_includedir_is_absolute 0)
+endif()
+file(TO_NATIVE_PATH "${CMAKE_INSTALL_INCLUDEDIR}" install_includedir_native)
+
 target_compile_options(Core PRIVATE -DLIB_CORE_NAME=${full_core_filename}
-                                    -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
+                                    -DINSTALL_INCLUDEDIR=${install_includedir_native}
+                                    -DINSTALL_INCLUDEDIR_IS_ABSOLUTE=${install_includedir_is_absolute}
 )
+add_dependencies(Core ensure_build_tree_marker)
 
 if(PCRE2_FOUND)
   target_link_libraries(Core PRIVATE PCRE2::PCRE2)

core/base/src/TROOT.cxx
@@ -1993,9 +1993,7 @@ void TROOT::InitSystem()
       if (gSystem->Init())
          fprintf(stderr, "Fatal in <TROOT::InitSystem>: can't init operating system layer\n");
 
-      TString rootincludedir = _R_QUOTEVAL_(CMAKE_INSTALL_INCLUDEDIR);
-      gSystem->PrependPathName(GetRootSys(), rootincludedir);
-      gSystem->SetIncludePath(("-I" + rootincludedir).Data());
+      gSystem->SetIncludePath(("-I" + GetIncludeDir()).Data());
 
       if (!gSystem->HomeDirectory()) {
          fprintf(stderr, "Fatal in <TROOT::InitSystem>: HOME directory not set\n");
@@ -3098,11 +3096,29 @@ const TString& TROOT::GetSharedLibDir() {
 ////////////////////////////////////////////////////////////////////////////////
 /// Get the include directory in the installation. Static utility function.
 
-const TString& TROOT::GetIncludeDir() {
-   // Avoid returning a reference to a temporary because of the conversion
-   // between std::string and TString.
-   const static TString includedir = ROOT::FoundationUtils::GetIncludeDir();
-   return includedir;
+const TString &TROOT::GetIncludeDir()
+{
+   static TString rootincdir;
+
+   if (!rootincdir.IsNull())
+      return rootincdir;
+
+   const std::string &sep = ROOT::FoundationUtils::GetPathSeparator();
+
+   // Check if we are in the build tree using the build tree marker file
+   const bool isBuildTree = std::filesystem::exists((GetSharedLibDir() + sep + "root-build-tree-marker").Data());
+
+   if (isBuildTree) {
+      rootincdir = GetRootSys() + sep + "include";
+   } else {
+#if INSTALL_INCLUDEDIR_IS_ABSOLUTE
+      rootincdir = _R_QUOTEVAL_(INSTALL_INCLUDEDIR);
+#else
+      rootincdir = GetRootSys() + sep + _R_QUOTEVAL_(INSTALL_INCLUDEDIR);
+#endif
+   }
+
+   return rootincdir;
 }
 
 ////////////////////////////////////////////////////////////////////////////////

cmake/modules/RootConfiguration.cmake
@@ -548,6 +548,19 @@ endif()
 string(REGEX MATCH "__cplusplus[=| ]([0-9]+)" __cplusplus "${__cplusplus_PPout}")
 set(__cplusplus ${CMAKE_MATCH_1}L)
 
+# To mark the build tree. Important for automatic resolution of relative paths,
+# for example to the include directory. Use custom target to ensure re-creation
+# when someone deletes the marker.
+set(build_tree_marker "${localruntimedir}/root-build-tree-marker")
+add_custom_command(
+  OUTPUT "${build_tree_marker}"
+  COMMAND ${CMAKE_COMMAND} -E touch "${build_tree_marker}"
+  COMMENT "Ensuring that \"${build_tree_marker}\" exists"
+)
+add_custom_target(ensure_build_tree_marker ALL
+  DEPENDS "${build_tree_marker}"
+)
+
 configure_file(${PROJECT_SOURCE_DIR}/config/RConfigure.in ginclude/RConfigure.h NEWLINE_STYLE UNIX)
 install(FILES ${CMAKE_BINARY_DIR}/ginclude/RConfigure.h DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 

config/RConfigure.in
@@ -9,7 +9,6 @@
 #define ROOTPREFIX    "@prefix@"
 #define ROOTBINDIR    "@bindir@"
 #define ROOTLIBDIR    "@libdir@"
-#define ROOTINCDIR    "@incdir@"
 #define ROOTETCDIR    "@etcdir@"
 #define ROOTDATADIR   "@datadir@"
 #define ROOTDOCDIR    "@docdir@"

core/foundation/res/ROOT/FoundationUtils.hxx
@@ -67,10 +67,6 @@ namespace FoundationUtils {
    ///
    const std::string& GetRootSys();
 
-   ///\ returns the include directory in the installation.
-   ///
-   const std::string& GetIncludeDir();
-
    ///\returns the sysconfig directory in the installation.
    const std::string& GetEtcDir();
 

core/foundation/src/FoundationUtils.cxx
@@ -167,30 +167,16 @@ const std::string& GetRootSys() {
    if (rootsys.empty()) {
       if (const char* envValue = std::getenv("ROOTSYS")) {
          rootsys = envValue;
+#ifndef WIN32
          // We cannot use gSystem->UnixPathName.
          ConvertToUnixPath(rootsys);
+#endif
       }
    }
    // FIXME: Should this also call UnixPathName for consistency?
    if (rootsys.empty())
       rootsys = GetFallbackRootSys();
    return rootsys;
-}
-
-
-const std::string& GetIncludeDir() {
-#ifdef ROOTINCDIR
-   if (!IgnorePrefix()) {
-      const static std::string rootincdir = ROOTINCDIR;
-      return rootincdir;
-   }
-#endif
-   static std::string rootincdir;
-   if (rootincdir.empty()) {
-      const std::string& sep = GetPathSeparator();
-      rootincdir = GetRootSys() + sep + "include" + sep;
-   }
-   return rootincdir;
 }
 
 const std::string& GetEtcDir() {

commit 6b8877ebffe85c891f538ebe386f43b41fdbf0d5

[core] Use shared library dir as anchor in `GetIncludeDir()`
Using the shared library directory as the anchor to resolve the resource
directories like `GetIncludeDir()` is more direct and robust than using
`GetRootSys()`, which loses its meaning in some installations (e.g. what
should it be when ROOT is installed by the package manager? Just `/`?).

Most importantly, this change lifts some more constraint from how the
install tree can be reorganized after the fact. One can for example move
the include and library directory around as wanted, as long as their
relative paths don't change. This fixes the Python wheels, which were
broken by commit a5b1ed9.

As of commit a5b1ed9, ROOT became smarter about finding the include path
in the install tree, inferring the correct relative path from $ROOTSYS
and the CMAKE_INSTALL_INCLUDEDIR variable at build time. Before, that
only happened for gnuinstall=ON, but now it always happens.

However, for the Python wheel, we are breaking the assumptions that ROOT
makes by moving around directories in the install tree. This commit
fixes that situation.

core/base/CMakeLists.txt
@@ -233,15 +233,19 @@ set(full_core_filename $<TARGET_FILE_NAME:Core>)
 # Absolue CMAKE_INSTALL_<dir> paths are discouraged in CMake, but some
 # packagers use them anyway. So we support it.
 if(IS_ABSOLUTE ${CMAKE_INSTALL_INCLUDEDIR})
-   set(install_includedir_is_absolute 1)
+  set(install_lib_to_include "${CMAKE_INSTALL_INCLUDEDIR}")
 else()
-   set(install_includedir_is_absolute 0)
+  if(IS_ABSOLUTE ${CMAKE_INSTALL_LIBDIR})
+    set(libdir "${CMAKE_INSTALL_LIBDIR}")
+  else()
+    set(libdir "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+  endif()
+  file(RELATIVE_PATH install_lib_to_include "${libdir}" "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
+  unset(libdir)
 endif()
-file(TO_NATIVE_PATH "${CMAKE_INSTALL_INCLUDEDIR}" install_includedir_native)
 
 target_compile_options(Core PRIVATE -DLIB_CORE_NAME=${full_core_filename}
-                                    -DINSTALL_INCLUDEDIR=${install_includedir_native}
-                                    -DINSTALL_INCLUDEDIR_IS_ABSOLUTE=${install_includedir_is_absolute}
+                                    -DINSTALL_LIB_TO_INCLUDE="${install_lib_to_include}"
 )
 add_dependencies(Core ensure_build_tree_marker)
 

core/base/src/TROOT.cxx
@@ -3182,20 +3182,26 @@ const TString &TROOT::GetIncludeDir()
    if (!rootincdir.IsNull())
       return rootincdir;
 
-   const std::string &sep = ROOT::FoundationUtils::GetPathSeparator();
+   namespace fs = std::filesystem;
+
+   // The shared library directory can be found automatically, because the
+   // libCore is loaded by definition when using TROOT. It's used as the anchor
+   // to resolve the ROOT include directory, using the correct relative path
+   // for either the build or install tree.
+   fs::path libPath = GetSharedLibDir().Data();
 
    // Check if we are in the build tree using the build tree marker file
-   const bool isBuildTree = std::filesystem::exists((GetSharedLibDir() + sep + "root-build-tree-marker").Data());
+   const bool isBuildTree = fs::exists(libPath / "root-build-tree-marker");
 
-   if (isBuildTree) {
-      rootincdir = GetRootSys() + sep + "include";
-   } else {
-#if INSTALL_INCLUDEDIR_IS_ABSOLUTE
-      rootincdir = _R_QUOTEVAL_(INSTALL_INCLUDEDIR);
-#else
-      rootincdir = GetRootSys() + sep + _R_QUOTEVAL_(INSTALL_INCLUDEDIR);
-#endif
+   fs::path includePath = isBuildTree ? "../include" : INSTALL_LIB_TO_INCLUDE;
+
+   // The INSTALL_LIB_TO_INCLUDE might already be absolute
+   if (!includePath.is_absolute()) {
+      includePath = libPath / includePath;
    }
+
+   // Normalize to get rid of the "../" in relative paths
+   rootincdir = includePath.lexically_normal().string();
 
    return rootincdir;
 }

core/base/test/CMakeLists.txt
@@ -20,6 +20,7 @@ if(ROOT_NEED_STDCXXFS)
     target_link_libraries(CoreBaseTests PRIVATE stdc++fs)
 endif()
 target_compile_options(CoreBaseTests PRIVATE -DEXPECTED_SHARED_LIBRARY_DIR="${localruntimedir}")
+target_compile_options(CoreBaseTests PRIVATE -DEXPECTED_INCLUDE_DIR="${CMAKE_BINARY_DIR}/include")
 
 ROOT_ADD_GTEST(CoreErrorTests TErrorTests.cxx LIBRARIES Core)
 

core/base/test/TROOTTests.cxx
@@ -41,3 +41,13 @@ TEST(TROOT, GetSharedLibDir)
 
    EXPECT_EQ(libDir, libDirRef);
 }
+
+TEST(TROOT, GetIncludeDir)
+{
+   namespace fs = std::filesystem;
+
+   fs::path includeDir = gROOT->GetIncludeDir().Data();
+   fs::path includeDirRef = EXPECTED_INCLUDE_DIR;
+
+   EXPECT_EQ(includeDir, includeDirRef);
+}