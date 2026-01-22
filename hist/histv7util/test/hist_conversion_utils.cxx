b9c21ddcd23e10c4203a4e37aa975eb991ab5330
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2026-01-22 09:52:02 +0100

    [hist] Implement conversion of RVariableBinAxis and RCategoricalAxis

--- hist/histv7util/test/hist_conversion_utils.cxx
+++ hist/histv7util/test/hist_conversion_utils.cxx
@@ -2,6 +2,9 @@
 
 #include <TAxis.h>
 
+#include <string>
+#include <vector>
+
 using ROOT::Experimental::Hist::Internal::ConvertAxis;
 
 TEST(ConvertAxis, RegularAxis)
@@ -16,4 +19,44 @@ TEST(ConvertAxis, RegularAxis)
    EXPECT_EQ(dst.GetNbins(), Bins);
    EXPECT_EQ(dst.GetXmin(), 0.0);
    EXPECT_EQ(dst.GetXmax(), Bins);
+   EXPECT_EQ(dst.GetXbins()->size(), 0);
+}
+
+TEST(ConvertAxis, VariableBinAxis)
+{
+   static constexpr std::size_t Bins = 20;
+   std::vector<double> bins;
+   for (std::size_t i = 0; i < Bins; i++) {
+      bins.push_back(i);
+   }
+   bins.push_back(Bins);
+   const RAxisVariant src{RVariableBinAxis(bins)};
+
+   TAxis dst;
+   ConvertAxis(dst, src);
+
+   EXPECT_TRUE(dst.IsVariableBinSize());
+   EXPECT_EQ(dst.GetNbins(), Bins);
+   EXPECT_EQ(dst.GetXmin(), 0.0);
+   EXPECT_EQ(dst.GetXmax(), Bins);
+
+   ASSERT_EQ(dst.GetXbins()->size(), Bins + 1);
+   for (std::size_t i = 0; i <= Bins; i++) {
+      EXPECT_EQ(dst.GetXbins()->At(i), bins[i]);
+   }
+}
+
+TEST(ConvertAxis, CategoricalAxis)
+{
+   const std::vector<std::string> categories = {"a", "b", "c"};
+   const RAxisVariant src{RCategoricalAxis(categories)};
+
+   TAxis dst;
+   ConvertAxis(dst, src);
+
+   EXPECT_FALSE(dst.IsVariableBinSize());
+   EXPECT_EQ(dst.GetNbins(), categories.size());
+   for (std::size_t i = 0; i < categories.size(); i++) {
+      EXPECT_EQ(dst.GetBinLabel(i + 1), categories[i]);
+   }
 }
--- hist/histv7util/test/histutil_test.hxx
+++ hist/histv7util/test/histutil_test.hxx
@@ -5,17 +5,22 @@
 #include <ROOT/Hist/ConvertToTH1.hxx>
 #include <ROOT/RAxisVariant.hxx>
 #include <ROOT/RBinWithError.hxx>
+#include <ROOT/RCategoricalAxis.hxx>
 #include <ROOT/RHist.hxx>
 #include <ROOT/RHistEngine.hxx>
+#include <ROOT/RRegularAxis.hxx>
+#include <ROOT/RVariableBinAxis.hxx>
 #include <ROOT/RWeight.hxx>
 
 using namespace ROOT::Experimental::Hist;
 using ROOT::Experimental::RAxisVariant;
 using ROOT::Experimental::RBinIndex;
 using ROOT::Experimental::RBinWithError;
+using ROOT::Experimental::RCategoricalAxis;
 using ROOT::Experimental::RHist;
 using ROOT::Experimental::RHistEngine;
 using ROOT::Experimental::RRegularAxis;
+using ROOT::Experimental::RVariableBinAxis;
 using ROOT::Experimental::RWeight;
 
 #include <gtest/gtest.h>
