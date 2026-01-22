6176e0985f5744055b42c7c08c9b79c3cc670363
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2026-01-22 09:52:02 +0100

    [hist] Implement conversion of RVariableBinAxis and RCategoricalAxis

--- hist/histv7util/src/ConversionUtils.cxx
+++ hist/histv7util/src/ConversionUtils.cxx
@@ -4,7 +4,9 @@
 
 #include <ROOT/Hist/ConversionUtils.hxx>
 #include <ROOT/RAxisVariant.hxx>
+#include <ROOT/RCategoricalAxis.hxx>
 #include <ROOT/RRegularAxis.hxx>
+#include <ROOT/RVariableBinAxis.hxx>
 
 #include <TAxis.h>
 
@@ -17,6 +19,15 @@ void ConvertAxis(TAxis &dst, const RAxisVariant &src)
 {
    if (auto *regular = src.GetRegularAxis()) {
       dst.Set(regular->GetNNormalBins(), regular->GetLow(), regular->GetHigh());
+   } else if (auto *variable = src.GetVariableBinAxis()) {
+      dst.Set(variable->GetNNormalBins(), variable->GetBinEdges().data());
+   } else if (auto *categorical = src.GetCategoricalAxis()) {
+      const auto &categories = categorical->GetCategories();
+      dst.Set(categories.size(), 0, categories.size());
+
+      for (std::size_t i = 0; i < categories.size(); i++) {
+         dst.SetBinLabel(i + 1, categories[i].c_str());
+      }
    } else {
       throw std::logic_error("unimplemented axis type in ConvertAxis"); // GCOVR_EXCL_LINE
    }
