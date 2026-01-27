20d43ae8a3cdec7db178bc2598f142f23eeb6f5e
moneta <lorenzo.moneta@cern.ch>
2026-01-27 16:14:13 +0100

    [tmva][sofie] Add support for parsing external ONNX data file for weights
    Support external_data when parsing the initialized tensor from an ONNX file
    To test the feture, run tutorial RMVA_SOFIE_ONNX.py changing when exporting to the ONNX file from external_data=False to external_data=True

--- tmva/sofie_parsers/inc/TMVA/RModelParser_ONNX.hxx
+++ tmva/sofie_parsers/inc/TMVA/RModelParser_ONNX.hxx
@@ -6,12 +6,14 @@
 #include <memory>
 #include <functional>
 #include <unordered_map>
+#include <fstream>
 
 // forward declaration
 namespace onnx {
 class NodeProto;
 class GraphProto;
 class ModelProto;
+class TensorProto;
 } // namespace onnx
 
 namespace TMVA {
@@ -41,6 +43,11 @@ private:
 
    // List of fused operators storing as key the second operator and a value a pair of fusion type and parent operator
    std::map<int, std::pair<EFusedOp, int>> fFusedOperators;
+
+   //  weight data file
+   std::ifstream fDataFile;
+   // filename of model
+   std::string fDataFileName;
 
 
 public:
@@ -80,6 +87,10 @@ public:
    std::unique_ptr<onnx::ModelProto> LoadModel(const std::string &filename);
    std::unique_ptr<onnx::ModelProto> LoadModel(std::istream &input);
 
+   std::shared_ptr<void> GetInitializedTensorData(onnx::TensorProto *tensorproto, size_t tensor_length, ETensorType type );
+
+public:
+
    RModelParser_ONNX() noexcept;
 
    RModel Parse(std::string const &filename, bool verbose = false);
@@ -87,6 +98,12 @@ public:
 
    // check the model for missing operators - return false in case some operator implementation is missing
    bool CheckModel(std::string filename, bool verbose = false);
+
+   //set external data full path (needed if external data are not stored in the default modelName.onnx.data)
+   // call this function before parsing
+   void SetExternalDataFile(const std::string & dataFileName) {
+      fDataFileName = dataFileName;
+   }
 
    ~RModelParser_ONNX();
 };
