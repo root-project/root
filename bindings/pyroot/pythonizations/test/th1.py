addca2fdf75b42ed359595f5978d6d5d343fd205
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-02-03 13:50:42 +0100

    [PyROOT] Add test for TH1.Fit with IMT.
    The timeout of the test is reduced because the deadlock would otherwise
    show only after 1500s.

--- bindings/pyroot/pythonizations/test/th1.py
+++ bindings/pyroot/pythonizations/test/th1.py
@@ -15,7 +15,7 @@ class TH1Operators(unittest.TestCase):
         h = ROOT.TH1F("testHist", "", nbins, -4, 4)
         h.FillRandom("gaus")
 
-        initial_bins = [ h.GetBinContent(i) for i in range(nbins) ]
+        initial_bins = [h.GetBinContent(i) for i in range(nbins)]
         c = 2
 
         # Multiply in place
@@ -26,5 +26,45 @@ class TH1Operators(unittest.TestCase):
             self.assertEqual(h.GetBinContent(i), initial_bins[i] * c)
 
 
-if __name__ == '__main__':
+class TH1IMT(unittest.TestCase):
+    """
+    Test a deadlock when IMT is used in conjunction with a fit function in Python.
+    Since TH1.Fit held the GIL, the fit function could never be evaluated
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        ROOT.ROOT.EnableImplicitMT(4)
+
+    @classmethod
+    def tearDownClass(cls):
+        ROOT.ROOT.DisableImplicitMT()
+
+    def test_fit_python_function(self):
+        xmin = 0
+        xmax = 1
+
+        h1 = ROOT.TH1F("h1", "", 20, xmin, xmax)
+        h1.FillRandom("gaus", 1000)
+
+        def func(x, pars):
+            return pars[0] + pars[1] * x[0]
+
+        my_func = ROOT.TF1("f1", func, xmin, xmax, npar=2, ndim=1)
+
+        my_func.SetParNames(
+            "A",
+            "B",
+        )
+        my_func.SetParameter(0, 1)
+        my_func.SetParameter(1, -1)
+
+        r = h1.Fit(my_func, "SE0Q", "", xmin, xmax)
+
+        self.assertFalse(r.IsEmpty())
+        self.assertTrue(r.IsValid())
+        self.assertGreater(r.Parameter(0), 0)
+
+
+if __name__ == "__main__":
     unittest.main()
