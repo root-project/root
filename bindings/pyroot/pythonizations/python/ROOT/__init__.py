a42f5598e66d4448a32ddc3194224eed06fa8bc6
Jonas Rembser <jonas.rembser@cern.ch>
2026-02-08 21:43:05 +0100

    [Python] Assert ABI-compatible Python version on `import ROOT`
    We should give the user a helpful error message if the ROOT and Python
    interpreter versions don't match. Otherwise, one will get confusing
    `undefined symbol` error or other unclear behavior at runtime.
    
    This came up sometimes in forum posts or GitHub issues, so the clear
    error would have helped.
    
    (cherry picked from commit 2401d1ffd720ae85742204c2b427384828993f99)

--- bindings/pyroot/pythonizations/python/ROOT/__init__.py
+++ bindings/pyroot/pythonizations/python/ROOT/__init__.py
@@ -11,6 +11,7 @@
 import atexit
 import builtins
 import os
+import platform
 import sys
 import types
 from importlib.abc import Loader, MetaPathFinder
@@ -22,7 +23,27 @@ import cppyy.types
 
 from . import _asan  # noqa: F401  # imported for side effects for setup specific to AddressSanitizer environments
 from ._facade import ROOTFacade
+from ._python_version import _root_python_version
 from ._pythonization import _register_pythonizations
+
+_runtime_version = platform.python_version()
+
+
+def _major_minor(v):
+    return ".".join(v.split(".")[:2])
+
+
+# Check for Python ABI compatibility with this ROOT build. This check prevents
+# hard crashes and undefined behavior, yielding helpful error messages instead.
+if _major_minor(_runtime_version) != _major_minor(_root_python_version):
+    import textwrap
+
+    message = f"""
+    ROOT was built for Python {_root_python_version}, but you are running Python {_runtime_version}.
+    Python major.minor versions must match. Use a matching Python or ROOT build.
+    """
+    raise ImportError(textwrap.dedent(message))
+
 
 # Prevent cppyy's check for extra header directory
 os.environ["CPPYY_API_PATH"] = "none"
