916172c43ac0cbf6884d3d0f19a2f215a5783278
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-02-10 11:52:15 +0100

    [RDF] Refactor progress bar and shorten its output.
    This removes redundant information from the line printed to the
    terminal and shortens the output. Otherwise, the progress bar
    frequently overflows the terminal.
    
    When the progress bar prints to a file, reduce the frequency to
    every 10 seconds, and limit its width to 60 chars to avoid cluttering
    the file.
    
    Furthermore, significantly improve how completion is estimated.
    The following two heuristics are employed:
    - Files that have been opened count as
      fractionOfFilesAlreadyOpened * eventsProcessed/totalEvents
      This tracks the progress of all files for which the number of events
      has been seen.
    - Files that have not been opened count as 1/totalFiles until they have
      been opened. This means that the progress bar e.g. can't reach 50% if
      half of the files haven't been opened yet.
    This change significantly reduces the jumps of the progress bar when a
    new file is opened.
    
    Finally, the code was refactored:
    - Handle locking logic for updates with a single RAII, update locks to C++17.
    - Remove a lock and mutex that didn't have any effect.
    - Reduce repeated function calls to functions that hold locks.
    - Simplify computation of average number of events.
    - Relax memory order of the atomics to what's necessary.
    - Outline as many functions as possible. Since RDFHelpers.hxx goes
      through JITting and pcms, it gets compiled frequently, so outlining is
      probably beneficial.
    - Make a lot of members const to avoid unintenional modifications. This makes
      it clear that no mutex needs to be locked to read these.
    - Collect helper functions in one anonymous namespace.
    - Remove a constructor argument that was ignored.

--- tree/dataframe/src/RDFHelpers.cxx
+++ tree/dataframe/src/RDFHelpers.cxx
@@ -32,11 +32,7 @@
 #include <iostream>
 #include <set>
 
-// TODO, this function should be part of core libraries
 #include <numeric>
-#if (!defined(_WIN32)) && (!defined(_WIN64))
-#include <unistd.h>
-#endif
 
 #if defined(_WIN32) || defined(_WIN64)
 #define WIN32_LEAN_AND_MEAN
@@ -45,11 +41,14 @@
 #include <Windows.h>
 #else
 #include <sys/ioctl.h>
+#include <unistd.h>
 #endif
 
 class TTreeReader;
 
+namespace {
 // Get terminal size for progress bar
+// TODO: Put this in core libraries?
 int get_tty_size()
 {
 #if defined(_WIN32) || defined(_WIN64)
@@ -67,6 +66,30 @@ int get_tty_size()
    width = (int)(w.ws_col);
    return width;
 #endif
+}
+
+/// Restore an output stream to its previous state using RAII.
+struct RestoreStreamState {
+   RestoreStreamState(std::ostream &stream) : fStream(stream) { fPreservedState.copyfmt(stream); }
+   ~RestoreStreamState() { fStream.copyfmt(fPreservedState); }
+
+   std::ostream &fStream;
+   std::ios fPreservedState{nullptr};
+};
+} // namespace
+
+/// Format std::chrono::seconds as `1:30m`.
+std::ostream &operator<<(std::ostream &stream, std::chrono::seconds elapsedSeconds)
+{
+   RestoreStreamState restore(stream);
+   auto h = std::chrono::duration_cast<std::chrono::hours>(elapsedSeconds);
+   auto m = std::chrono::duration_cast<std::chrono::minutes>(elapsedSeconds - h);
+   auto s = (elapsedSeconds - h - m).count();
+
+   if (h.count() > 0)
+      stream << h.count() << ':' << std::setw(2) << std::right << std::setfill('0');
+   stream << m.count() << ':' << std::setw(2) << std::right << std::setfill('0') << s;
+   return stream << (h.count() > 0 ? 'h' : 'm');
 }
 
 using ROOT::RDF::RResultHandle;
@@ -148,40 +171,73 @@ void ThreadsPerTH3(unsigned int N)
    ROOT::Internal::RDF::NThreadPerTH3() = N;
 }
 
-ProgressHelper::ProgressHelper(std::size_t increment, unsigned int totalFiles, unsigned int progressBarWidth,
-                               unsigned int printInterval, bool useColors)
-   : fPrintInterval(printInterval),
-     fIncrement{increment},
-     fBarWidth{progressBarWidth = int(get_tty_size() / 4)},
-     fTotalFiles{totalFiles},
+ProgressHelper::ProgressHelper(std::size_t increment, unsigned int totalFiles, unsigned int printInterval,
+                               bool useColors)
+   :
 #if defined(_WIN32) || defined(_WIN64)
      fIsTTY{_isatty(_fileno(stdout)) != 0},
-     fUseShellColours{false && useColors}
+     fUseShellColours{false && useColors},
 #else
      fIsTTY{isatty(fileno(stdout)) == 1},
-     fUseShellColours{useColors && fIsTTY} // Control characters only with terminals.
+     fUseShellColours{useColors && fIsTTY}, // Control characters only with terminals.
 #endif
+     fIncrement{increment},
+     fNColumns(fIsTTY ? (get_tty_size() == 0 ? 60 : get_tty_size()) : 50),
+     fTotalFiles{totalFiles},
+     fPrintInterval{printInterval == 0 ? (fIsTTY ? 1 : 10) : printInterval}
 {
+   std::fill(fEventsPerSecondStatistics.begin(), fEventsPerSecondStatistics.end(), -1.);
+}
+
+/// Register a new sample for completion statistics.
+/// \see ROOT::RDF::RInterface::DefinePerSample().
+/// The *id.AsString()* refers to the name of the currently processed file.
+/// The idea is to populate the  event entries in the *fSampleNameToEventEntries* map
+/// by selecting the greater of the two values:
+/// *id.EntryRange().second* which is the upper event entry range of the processed sample
+/// and the current value of the event entries in the *fSampleNameToEventEntries* map.
+/// In the single threaded case, the two numbers are the same as the entry range corresponds
+/// to the number of events in an individual file (each sample is simply a single file).
+/// In the multithreaded case, the idea is to accumulate the higher event entry value until
+/// the total number of events in a given file is reached.
+void ProgressHelper::RegisterNewSample(unsigned int /*slot*/, const ROOT::RDF::RSampleInfo &id)
+{
+   std::scoped_lock lock(fSampleNameToEventEntriesMutex);
+   fSampleNameToEventEntries[id.AsString()] = std::max(id.NEntriesTotal(), fSampleNameToEventEntries[id.AsString()]);
 }
 
 /// Compute a running mean of events/s.
 double ProgressHelper::EvtPerSec() const
 {
-   if (fEventsPerSecondStatisticsIndex < fEventsPerSecondStatistics.size())
-      return std::accumulate(fEventsPerSecondStatistics.begin(),
-                             fEventsPerSecondStatistics.begin() + fEventsPerSecondStatisticsIndex, 0.) /
-             fEventsPerSecondStatisticsIndex;
-   else
-      return std::accumulate(fEventsPerSecondStatistics.begin(), fEventsPerSecondStatistics.end(), 0.) /
-             fEventsPerSecondStatistics.size();
+   double sum = 0;
+   unsigned int n = 0;
+   for (auto item : fEventsPerSecondStatistics) {
+      if (item >= 0.) {
+         sum += item;
+         ++n;
+      }
+   }
+
+   return n > 0 ? sum / n : 0.;
+}
+
+/// Compute total events in all open files.
+std::size_t ProgressHelper::ComputeTotalEvents() const
+{
+   std::scoped_lock lock(fSampleNameToEventEntriesMutex);
+   std::size_t result = 0;
+   for (const auto &item : fSampleNameToEventEntries)
+      result += item.second;
+   return result;
 }
 
 /// Record current event counts and time stamp, populate evts/s statistics array.
+/// The function assumes that a lock on the update mutex is held.
 std::pair<std::size_t, std::chrono::seconds> ProgressHelper::RecordEvtCountAndTime()
 {
    using namespace std::chrono;
 
-   auto currentEventCount = fProcessedEvents.load();
+   auto currentEventCount = fProcessedEvents.load(std::memory_order_acquire);
    auto eventsPerTimeInterval = currentEventCount - fLastProcessedEvents;
    fLastProcessedEvents = currentEventCount;
 
@@ -190,97 +246,123 @@ std::pair<std::size_t, std::chrono::seconds> ProgressHelper::RecordEvtCountAndTi
    fLastPrintTime = newPrintTime;
 
    duration<double> secondsCurrentInterval = newPrintTime - oldPrintTime;
-   fEventsPerSecondStatistics[fEventsPerSecondStatisticsIndex++ % fEventsPerSecondStatistics.size()] =
+   fEventsPerSecondStatisticsCounter = (fEventsPerSecondStatisticsCounter + 1) % fEventsPerSecondStatistics.size();
+   fEventsPerSecondStatistics[fEventsPerSecondStatisticsCounter] =
       eventsPerTimeInterval / secondsCurrentInterval.count();
 
    return {currentEventCount, duration_cast<seconds>(newPrintTime - fBeginTime)};
 }
 
-namespace {
-
-struct RestoreStreamState {
-   RestoreStreamState(std::ostream &stream) : fStream(stream) {
-      fPreservedState.copyfmt(stream);
-   }
-   ~RestoreStreamState()
-   {
-      fStream.copyfmt(fPreservedState);
-   }
-
-   std::ostream &fStream;
-   std::ios fPreservedState{nullptr};
-};
-
-/// Format std::chrono::seconds as `1:30m`.
-std::ostream &operator<<(std::ostream &stream, std::chrono::seconds elapsedSeconds)
-{
-   RestoreStreamState restore(stream);
-   auto h = std::chrono::duration_cast<std::chrono::hours>(elapsedSeconds);
-   auto m = std::chrono::duration_cast<std::chrono::minutes>(elapsedSeconds - h);
-   auto s = (elapsedSeconds - h - m).count();
-
-   if (h.count() > 0)
-      stream << h.count() << ':' << std::setw(2) << std::right << std::setfill('0');
-   stream << m.count() << ':' << std::setw(2) << std::right << std::setfill('0') << s;
-   return stream << (h.count() > 0 ? 'h' : 'm');
-}
-
-} // namespace
-
 /// Print event and time statistics.
-void ProgressHelper::PrintStats(std::ostream &stream, std::size_t currentEventCount,
-                                std::chrono::seconds elapsedSeconds) const
+void ProgressHelper::PrintProgressAndStats(std::ostream &stream, std::size_t currentEventCount,
+                                           std::chrono::seconds elapsedSeconds) const
 {
-   RestoreStreamState restore(stream);
+   std::ostringstream buffer;
    auto evtpersec = EvtPerSec();
-   auto GetNEventsOfCurrentFile = ComputeNEventsSoFar();
-   auto currentFileIdx = ComputeCurrentFileIdx();
-   auto totalFiles = fTotalFiles;
+   auto totalEventsInOpenFiles = ComputeTotalEvents();
+   std::size_t currentFileIdx;
+   {
+      std::scoped_lock lock(fSampleNameToEventEntriesMutex);
+      currentFileIdx = fSampleNameToEventEntries.size();
+   }
 
+   if (totalEventsInOpenFiles == 0)
+      return;
+
+   double completion = 0.;
+   if (currentFileIdx < fTotalFiles) {
+      const double fractionSeenFiles = double(currentFileIdx) / fTotalFiles;
+      completion = fractionSeenFiles * (double(currentEventCount) / totalEventsInOpenFiles);
+   } else {
+      completion = double(currentEventCount) / totalEventsInOpenFiles;
+   }
+
+   // Print the bar
+   {
+      const auto barWidth = fNColumns / 4;
+      unsigned int nBar = std::min(completion, 1.) * barWidth;
+      std::string bars(std::max(nBar, 1u), '=');
+      bars.back() = (nBar == barWidth) ? '=' : '>';
+
+      if (fUseShellColours)
+         buffer << "\033[33m";
+      buffer << '|' << std::setfill(' ') << std::setw(barWidth) << std::left << bars << "|   ";
+      if (fUseShellColours)
+         buffer << "\033[0m";
+   }
+
+   // Elapsed time
+   buffer << "[";
    if (fUseShellColours)
-      stream << "\033[35m";
-   stream << "["
-          << "Elapsed time: " << elapsedSeconds << "  ";
+      buffer << "\033[35m";
+   buffer << "Elapsed: " << elapsedSeconds << "  ";
    if (fUseShellColours)
-      stream << "\033[0m";
-   stream << "processing file: " << currentFileIdx << " / " << totalFiles << "  ";
+      buffer << "\033[0m";
+   buffer << "files: " << currentFileIdx << " / " << fTotalFiles << "  ";
 
    // Event counts:
    if (fUseShellColours)
-      stream << "\033[32m";
+      buffer << "\033[32m";
 
-   stream << "processed evts: " << currentEventCount;
-   if (GetNEventsOfCurrentFile != 0) {
-      stream << " / " << std::scientific << std::setprecision(2) << GetNEventsOfCurrentFile;
+   buffer << "events: " << currentEventCount;
+   if (totalEventsInOpenFiles != 0) {
+      buffer << " / " << std::scientific << std::setprecision(2);
+      if (currentFileIdx == fTotalFiles)
+         buffer << totalEventsInOpenFiles;
+      else
+         buffer << "(" << totalEventsInOpenFiles << " + x)";
    }
-   stream << "  ";
+   buffer << "  ";
 
    if (fUseShellColours)
-      stream << "\033[0m";
+      buffer << "\033[0m";
 
    // events/s
-   stream << std::scientific << std::setprecision(2) << evtpersec << " evt/s";
+   buffer << std::scientific << std::setprecision(2) << evtpersec << " evt/s";
 
    // Time statistics:
-   if (GetNEventsOfCurrentFile != 0) {
+   // As long as not all files have been opened, estimate when 100% completion will be reached
+   // based on current completion elapsed time. (This assumes that unopened files have about the
+   // same size as the files that have been seen.)
+   // Once the total event count is known, use "missing events / evt/s".
+   if (totalEventsInOpenFiles != 0) {
       if (fUseShellColours)
-         stream << "\033[35m";
-      std::chrono::seconds remainingSeconds(
-         static_cast<long long>((ComputeNEventsSoFar() - currentEventCount) / evtpersec));
-      stream << " " << remainingSeconds << " "
-             << " remaining time (per file being processed)";
+         buffer << "\033[35m";
+
+      std::chrono::seconds remainingSeconds;
+      if (currentFileIdx == fTotalFiles) {
+         remainingSeconds =
+            std::chrono::seconds{static_cast<long long>((ComputeTotalEvents() - currentEventCount) / evtpersec)};
+      } else {
+         remainingSeconds =
+            std::chrono::seconds{static_cast<long long>(elapsedSeconds.count() / completion - elapsedSeconds.count())};
+      }
+      buffer << "  remaining ca.: " << remainingSeconds;
+
       if (fUseShellColours)
-         stream << "\033[0m";
+         buffer << "\033[0m";
    }
 
-   stream << "]   ";
+   buffer << "]";
+
+   RestoreStreamState restore(stream);
+   stream << std::left << std::setw(fNColumns - 1) << buffer.str();
 }
 
-void ProgressHelper::PrintStatsFinal(std::ostream &stream, std::chrono::seconds elapsedSeconds) const
+void ProgressHelper::PrintStatsFinal() const
 {
+   auto &stream = std::cout;
    RestoreStreamState restore(stream);
-   auto totalEvents = ComputeNEventsSoFar();
-   auto totalFiles = fTotalFiles;
+   const auto elapsedSeconds =
+      std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - fBeginTime);
+   const auto totalEvents = ComputeTotalEvents();
+
+   // The next line resets the current line output in the terminal.
+   // Brings the cursor at the beginning ('\r'), prints whitespace with the
+   // same length as the terminal size, then resets the cursor again so we
+   // can print the final stats on a clean line.
+   if (fIsTTY)
+      stream << '\r' << std::string(fNColumns, ' ') << '\r';
 
    if (fUseShellColours)
       stream << "\033[35m";
@@ -288,45 +370,58 @@ void ProgressHelper::PrintStatsFinal(std::ostream &stream, std::chrono::seconds 
           << "Total elapsed time: " << elapsedSeconds << "  ";
    if (fUseShellColours)
       stream << "\033[0m";
-   stream << "processed files: " << totalFiles << " / " << totalFiles << "  ";
+   stream << "processed files: " << fTotalFiles << "  ";
 
    // Event counts:
    if (fUseShellColours)
       stream << "\033[32m";
 
-   stream << "processed evts: " << totalEvents;
-   if (totalEvents != 0) {
-      stream << " / " << std::scientific << std::setprecision(2) << totalEvents;
+   stream << "processed events: " << totalEvents;
+
+   if (fUseShellColours)
+      stream << "\033[0m";
+
+   stream << "  " << std::scientific << std::setprecision(2) << (double)totalEvents / elapsedSeconds.count()
+          << " evt/s";
+
+   stream << "]\n";
+}
+
+/// Record number of events processed and update progress bar.
+/// This function will atomically record elapsed times and event statistics, and one thread will udpate the progress bar
+/// every n seconds (set by the fPrintInterval).
+void ProgressHelper::Update()
+{
+   using namespace std::chrono;
+   // ***************************************************
+   // Warning: Here, everything needs to be thread safe:
+   // ***************************************************
+   fProcessedEvents.fetch_add(fIncrement, std::memory_order_relaxed);
+
+   // We only print every n seconds.
+   if (duration_cast<seconds>(system_clock::now() - fLastPrintTime) < fPrintInterval) {
+      return;
    }
 
-   if (fUseShellColours)
-      stream << "\033[0m";
-
-   stream << "]   ";
-}
-
-/// Print a progress bar of width `ProgressHelper::fBarWidth` if `fGetNEventsOfCurrentFile` is known.
-void ProgressHelper::PrintProgressBar(std::ostream &stream, std::size_t currentEventCount) const
-{
-   auto GetNEventsOfCurrentFile = ComputeNEventsSoFar();
-   if (GetNEventsOfCurrentFile == 0)
+   // ******************************************************
+   // Update the progress bar. Only one thread can proceed.
+   // ******************************************************
+   std::unique_lock<std::mutex> lockGuard(fUpdateMutex, std::try_to_lock);
+   if (!lockGuard)
       return;
 
-   RestoreStreamState restore(stream);
+   auto const [eventCount, elapsedSeconds] = RecordEvtCountAndTime();
 
-   double completion = double(currentEventCount) / GetNEventsOfCurrentFile;
-   unsigned int nBar = std::min(completion, 1.) * fBarWidth;
+   if (fIsTTY)
+      std::cout << "\r";
 
-   std::string bars(std::max(nBar, 1u), '=');
-   bars.back() = (nBar == fBarWidth) ? '=' : '>';
+   PrintProgressAndStats(std::cout, eventCount, elapsedSeconds);
 
-   if (fUseShellColours)
-      stream << "\033[33m";
-   stream << '|' << std::setfill(' ') << std::setw(fBarWidth) << std::left << bars << "|   ";
-   if (fUseShellColours)
-      stream << "\033[0m";
+   if (fIsTTY)
+      std::cout << std::flush;
+   else
+      std::cout << std::endl;
 }
-//*/
 
 class ProgressBarAction final : public ROOT::Detail::RDF::RActionImpl<ProgressBarAction> {
 public:
@@ -346,22 +441,7 @@ public:
 
    void Exec(unsigned int) {}
 
-   void Finalize()
-   {
-      std::mutex fPrintMutex;
-      if (!fPrintMutex.try_lock())
-         return;
-      std::lock_guard<std::mutex> lockGuard(fPrintMutex, std::adopt_lock);
-      const auto &[eventCount, elapsedSeconds] = fHelper->RecordEvtCountAndTime();
-
-      // The next line resets the current line output in the terminal.
-      // Brings the cursor at the beginning ('\r'), prints whitespace with the
-      // same length as the terminal size, then resets the cursor again so we
-      // can print the final stats on a clean line.
-      std::cout << '\r' << std::string(get_tty_size(), ' ') << '\r';
-      fHelper->PrintStatsFinal(std::cout, elapsedSeconds);
-      std::cout << '\n';
-   }
+   void Finalize() { fHelper->PrintStatsFinal(); }
 
    std::string GetActionName() { return "ProgressBar"; }
    // dummy implementation of PartialUpdate
@@ -369,20 +449,19 @@ public:
 
    ROOT::RDF::SampleCallback_t GetSampleCallback() final
    {
-      return [this](unsigned int slot, const ROOT::RDF::RSampleInfo &id) {
-         this->fHelper->registerNewSample(slot, id);
-         return this->fHelper->ComputeNEventsSoFar();
-      };
+      return
+         [this](unsigned int slot, const ROOT::RDF::RSampleInfo &id) { this->fHelper->RegisterNewSample(slot, id); };
    }
 };
 
 void AddProgressBar(ROOT::RDF::RNode node)
 {
+   constexpr std::size_t callbackEveryNEvents = 1000;
    auto total_files = node.GetNFiles();
-   auto progress = std::make_shared<ProgressHelper>(1000, total_files);
+   auto progress = std::make_shared<ProgressHelper>(callbackEveryNEvents, total_files);
    ProgressBarAction c(progress);
    auto r = node.Book<>(c);
-   r.OnPartialResultSlot(1000, [progress](unsigned int slot, auto &&arg) { (*progress)(slot, arg); });
+   r.OnPartialResultSlot(callbackEveryNEvents, [progress](unsigned int slot, auto &&arg) { (*progress)(slot, arg); });
 }
 
 void AddProgressBar(ROOT::RDataFrame dataframe)
