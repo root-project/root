aefc7a713ed337d31127bf410a9f39a8bfc8cbb0
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-02-10 18:31:14 +0100

    [core] Update deprecation macros up to ROOT 6.44.

--- core/foundation/inc/ROOT/RConfig.hxx
+++ core/foundation/inc/ROOT/RConfig.hxx
@@ -477,14 +477,6 @@
 #define _R_DEPRECATED_REMOVE_NOW(REASON) __attribute__((REMOVE_THIS_NOW))
 #endif
 
-/* USE AS `R__DEPRECATED(6,38, "Not threadsafe; use TFoo::Bar().")`
-   To be removed by 6.38 */
-#if ROOT_VERSION_CODE <= ROOT_VERSION(6,37,0)
-# define _R__DEPRECATED_638(REASON) _R__DEPRECATED_LATER(REASON)
-#else
-# define _R__DEPRECATED_638(REASON) _R_DEPRECATED_REMOVE_NOW(REASON)
-#endif
-
 /* USE AS `R__DEPRECATED(6,40, "Not threadsafe; use TFoo::Bar().")`
    To be removed by 6.40 */
 #if ROOT_VERSION_CODE <= ROOT_VERSION(6, 39, 0)
@@ -493,12 +485,16 @@
 #define _R__DEPRECATED_640(REASON) _R_DEPRECATED_REMOVE_NOW(REASON)
 #endif
 
-/* USE AS `R__DEPRECATED(6,42, "Not threadsafe; use TFoo::Bar().")`
-   To be removed by 6.42 */
 #if ROOT_VERSION_CODE <= ROOT_VERSION(6, 41, 0)
 #define _R__DEPRECATED_642(REASON) _R__DEPRECATED_LATER(REASON)
 #else
 #define _R__DEPRECATED_642(REASON) _R_DEPRECATED_REMOVE_NOW(REASON)
+#endif
+
+#if ROOT_VERSION_CODE <= ROOT_VERSION(6, 43, 0)
+#define _R__DEPRECATED_644(REASON) _R__DEPRECATED_LATER(REASON)
+#else
+#define _R__DEPRECATED_644(REASON) _R_DEPRECATED_REMOVE_NOW(REASON)
 #endif
 
 /* USE AS `R__DEPRECATED(7,00, "Not threadsafe; use TFoo::Bar().")`
