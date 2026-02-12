5934848947b34b310e1072fb82ad01f455a321d4
ferdymercury <ferdymercury@users.noreply.github.com>
2026-02-12 09:02:43 +0100

    [tutorials][nfc] Avoid Doxygen documentation errors without disturbing transient I/O mark

--- tutorials/io/tree/tree106_tree.C
+++ tutorials/io/tree/tree106_tree.C
@@ -33,22 +33,22 @@ public:
    Float_t  vect[7];
    Float_t  getot;
    Float_t  gekin;
-   Float_t  vout[7];   //! not persistent
+   Float_t  vout[7];   ///<! not persistent
    Int_t    nmec;
    Int_t   *lmec;      //[nmec]
    Int_t   *namec;     //[nmec]
-   Int_t    nstep;     //! not persistent
+   Int_t    nstep;     ///<! not persistent
    Int_t    pid;
    Float_t  destep;
-   Float_t  destel;    //! not persistent
-   Float_t  safety;    //! not persistent
-   Float_t  sleng;     //! not persistent
-   Float_t  step;      //! not persistent
-   Float_t  snext;     //! not persistent
-   Float_t  sfield;    //! not persistent
-   Float_t  tofg;      //! not persistent
-   Float_t  gekrat;    //! not persistent
-   Float_t  upwght;    //! not persistent
+   Float_t  destel;    ///<! not persistent
+   Float_t  safety;    ///<! not persistent
+   Float_t  sleng;     ///<! not persistent
+   Float_t  step;      ///<! not persistent
+   Float_t  snext;     ///<! not persistent
+   Float_t  sfield;    ///<! not persistent
+   Float_t  tofg;      ///<! not persistent
+   Float_t  gekrat;    ///<! not persistent
+   Float_t  upwght;    ///<! not persistent
 
    Gctrak() {
       lmec = nullptr;
