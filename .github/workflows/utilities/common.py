1929405a8e3a820b7a775d98e04530dc0f7cd155
Danilo Piparo <danilo.piparo@cern.ch>
2026-02-13 11:06:17 +0100

    [ci] Treat commands as lists correctly

--- .github/workflows/utilities/common.py
+++ .github/workflows/utilities/common.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 def printError(msg):
@@ -9,24 +10,25 @@ def printWarning(msg):
 def printInfo(msg):
     print(f"Info: {msg}")
 
-def execCommand(cmd, thisCwd="./", theInput=None, desc="", replace=''):
+def execCommand(cmd, thisCwd="./", theInput=None, desc="", replace='', theEnv = os.environ):
     """
     Execute a command and return the output. For logging reasons, the command
     is also printed.
     If "desc" is specificed, the command is not printed but "desc".
     """
+    cmdAsStr, shellVal = (' '.join (cmd), False) if type(cmd) == type([]) else (cmd, True)
     if "" == desc:
-        echoCmd = f"In directory {thisCwd} *** {cmd} {'with std input' if theInput else ''}"
+        echoCmd = f"In directory {thisCwd} *** {cmdAsStr} {'with std input' if theInput else ''}"
         if "" != replace:
             echoCmd = echoCmd.replace(replace, '***')
         printInfo(echoCmd)
     else:
         print(desc)
     compProc = subprocess.run(
-        cmd, shell=True, capture_output=True, text=True, cwd=thisCwd, input=theInput, encoding="latin1"
+        cmd, shell=shellVal, capture_output=True, text=True, cwd=thisCwd, input=theInput, encoding="latin1", env=theEnv
     )
     if 0 != compProc.returncode:
         printError(f"{compProc.stderr.strip()}")
-        raise ValueError(f'Command "{cmd}" failed ({compProc.returncode})')
+        raise ValueError(f'Command "{cmdAsStr}" failed ({compProc.returncode})')
     out = compProc.stdout.strip()
     return out
