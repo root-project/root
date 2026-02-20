1ffd0a70c0fbf0fc23683db38cb7343daed1da9f
Jakob Blomer <jblomer@cern.ch>
2026-02-20 11:11:31 +0100

    [ntuple] remove abandoned tutorial
    The ntpl011_global_temperatures.C tutorial was initially meant to show
    several ROOT 7 features in concert. It never quite got there and
    meanwhile can essentially be considered abandoned.

--- test/wheels/test_tutorials.py
+++ test/wheels/test_tutorials.py
@@ -17,7 +17,6 @@ subdirs = ["analysis/dataframe", "analysis/tree", "hist", "io/ntuple", "roofit/r
 SKIP_TUTORIALS = {
     "ntpl004_dimuon.C",  # requires reading remote data via HTTP
     "ntpl008_import.C",  # requires reading remote data via HTTP
-    "ntpl011_global_temperatures.C",  # requires reading remote data via HTTP
     "distrdf004_dask_lxbatch.py",  # only works on lxplus
     "_SQlite",  # requires SQLite, not supported yet in ROOT wheels
     "h1analysisProxy.C",  # helper macro, not meant to run standalone
