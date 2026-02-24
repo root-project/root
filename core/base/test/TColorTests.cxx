678547dfcbb3a78efd89180271cdf8d1d28ce0f1
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-02-24 11:08:49 +0100

    [core] Add a test for a crash in TColorGradient (#21367).
    On mac beta, the tutorial about gl gradient colours crashes with a stack
    corruption. This test is supposed to reproduce the problem, taking the
    interpreter out of the equation.

--- core/base/test/TColorTests.cxx
+++ core/base/test/TColorTests.cxx
@@ -26,3 +26,24 @@ TEST(TColor, Hex8String)
    EXPECT_NEAR(color->GetBlue(), 0.690196, 1e-4);
    EXPECT_NEAR(color->GetAlpha(), 0.466667, 1e-4);
 }
+
+// ROOT-21367: On mac beta, the constructor of TColorGradient crashes with a stack corruption
+TEST(TColor, Gradient)
+{
+   auto fcol1 = TColor::GetColor((Float_t)0.25, 0.25, 0.25, 0.55); // special frame color 1
+   auto fcol2 = TColor::GetColor((Float_t)1., 1., 1., 0.05);       // special frame color 2
+
+   auto frameGradient = TColor::GetLinearGradient(0., {fcol1, fcol2, fcol2, fcol1}, {0., 0.2, 0.8, 1.});
+
+   // This gradient is a mixture of two standard colors.
+   auto padGradient = TColor::GetLinearGradient(0., {30, 38});
+
+   // Another gradient built from three standard colors.
+   auto histGradient = TColor::GetLinearGradient(45., {kYellow, kOrange, kRed});
+
+   EXPECT_NE(fcol1, frameGradient);
+   EXPECT_NE(fcol1, padGradient);
+   EXPECT_NE(fcol1, histGradient);
+   EXPECT_NE(frameGradient, padGradient);
+   EXPECT_NE(padGradient, histGradient);
+}
