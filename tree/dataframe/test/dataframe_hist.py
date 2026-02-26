8b9f52dcc0a34218c42c3966825cd9eba77e2c4f
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2026-02-26 17:31:55 +0100

    [df] Additional JIT overloads for RHist filling
    This works around limitations for automatic pythonization of variadic
    function templates with template parameter packs.

--- tree/dataframe/test/dataframe_hist.py
+++ tree/dataframe/test/dataframe_hist.py
@@ -2,6 +2,9 @@ import unittest
 
 import ROOT
 
+RRegularAxis = ROOT.Experimental.RRegularAxis
+RVariableBinAxis = ROOT.Experimental.RVariableBinAxis
+
 
 class RDFHist(unittest.TestCase):
     def test_Regular(self):
@@ -10,8 +13,22 @@ class RDFHist(unittest.TestCase):
         hist = dfX.Hist(10, (5.0, 15.0), "x")
         self.assertEqual(hist.GetNEntries(), 10)
 
+    def test_MultiDim(self):
+        df = ROOT.RDataFrame(10)
+        dfXY = df.Define("x", "rdfentry_ + 5.5").Define("y", "2 * rdfentry_ + 0.5")
+
+        regularAxis = RRegularAxis(10, (5.0, 15.0))
+        bins = [i for i in range(0, 21)]
+        variableBinAxis = RVariableBinAxis(bins)
+        hist = dfXY.Hist([regularAxis, variableBinAxis], ["x", "y"])
+        self.assertEqual(hist.GetNEntries(), 10)
+
     def test_Weight(self):
         df = ROOT.RDataFrame(10)
         dfXW = df.Define("x", "rdfentry_ + 5.5").Define("w", "0.1 + rdfentry_ * 0.03")
         hist = dfXW.Hist(10, (5.0, 15.0), "x", "w")
+        self.assertEqual(hist.GetNEntries(), 10)
+
+        regularAxis = RRegularAxis(10, (5.0, 15.0))
+        hist = dfXW.Hist([regularAxis], ["x"], "w")
         self.assertEqual(hist.GetNEntries(), 10)
