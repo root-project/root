a17bb3e1274920ecce47bf5f9a9cf3cbddd130b9
Sergey Linev <S.Linev@gsi.de>
2026-02-26 14:50:32 +0100

    Introduce TVirtualPad::PaintSegments
    Idea to have interface which handle painting of many segments at once. On platforms like X11 drawing of many lines at once can be performed with single call

--- core/base/src/TVirtualPad.cxx
+++ core/base/src/TVirtualPad.cxx
@@ -159,6 +159,18 @@ Bool_t TVirtualPad::PadInHighlightMode() const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Paint N individual segments
+/// Provided arrays should have 2*n elements
+/// IMPORTANT! Provided arrays can be modified after function call!
+
+void TVirtualPad::PaintSegments(Int_t n, Double_t *x, Double_t *y, Option_t *)
+{
+   for (Int_t i = 0; i < 2*n; i += 2)
+      PaintLine(x[i], y[i], x[i+1], y[i+1]);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 /// Does nothing, unless you implement your own picking.
 /// When complex object containing sub-objects (which can be picked)
 /// is painted in a pad, this "top-level" object is pushed into
