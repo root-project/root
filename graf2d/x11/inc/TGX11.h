81150484a5c33f9edaf6b544d7141a8b24dd1582
Sergey Linev <S.Linev@gsi.de>
2026-02-27 10:22:51 +0100

    Implement TGX11::DrawLinesSegments
    Like with TGX11::DrawPolyLine,
    split drawing on smaller portions if more than 500000
     segments provide at a time.
    
     Use direct cast from `TPoint *` to `XSegment *` while
     segment is just two points one after another

--- graf2d/x11/inc/TGX11.h
+++ graf2d/x11/inc/TGX11.h
@@ -168,6 +168,7 @@ public:
    void      DrawFillArea(Int_t n, TPoint *xy) override;
    void      DrawLine(Int_t x1, Int_t y1, Int_t x2, Int_t y2) override;
    void      DrawPolyLine(Int_t n, TPoint *xy) override;
+   void      DrawLinesSegments(Int_t n, TPoint *xy) override;
    void      DrawPolyMarker(Int_t n, TPoint *xy) override;
    void      DrawText(Int_t x, Int_t y, Float_t angle, Float_t mgn, const char *text, ETextMode mode) override;
    void      DrawText(Int_t, Int_t, Float_t, Float_t, const wchar_t *, ETextMode) override {}
