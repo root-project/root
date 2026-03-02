467b1da3eae4802d43c793ef93b94ceccd19b70e
Jakob Blomer <jblomer@cern.ch>
2026-03-02 14:32:21 +0100

    [ntuple] add ROOT::Internal::IsRNTupleSoAType()

---
+++ tree/ntuple/test/SoAField.hxx
@@ -0,0 +1,14 @@
+#ifndef ROOT_RNTuple_Test_SoAField
+#define ROOT_RNTuple_Test_SoAField
+
+#include <Rtypes.h>
+
+struct Record {
+   ClassDefNV(Record, 2);
+};
+
+struct SoA {
+   ClassDefNV(SoA, 2);
+};
+
+#endif // ROOT_RNTuple_Test_SoAField
