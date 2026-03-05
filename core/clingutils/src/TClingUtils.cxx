2bcab57ff96f50811089041ebf92573ce028ccae
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2026-03-05 15:56:23 +0100

    [TCling] API Update function signature: getDependentSizedArrayType

--- core/clingutils/src/TClingUtils.cxx
+++ core/clingutils/src/TClingUtils.cxx
@@ -4866,8 +4866,7 @@ clang::QualType ROOT::TMetaUtils::ReSubstTemplateArg(clang::QualType input, cons
          QT = Ctxt.getDependentSizedArrayType (newQT,
                                               arr->getSizeExpr(),
                                               arr->getSizeModifier(),
-                                              arr->getIndexTypeCVRQualifiers(),
-                                              arr->getBracketsRange());
+                                              arr->getIndexTypeCVRQualifiers());
 
       } else if (const auto arr = dyn_cast<IncompleteArrayType>(QT.getTypePtr())) {
          QualType newQT = ReSubstTemplateArg(arr->getElementType(),instance);
@@ -4884,8 +4883,7 @@ clang::QualType ROOT::TMetaUtils::ReSubstTemplateArg(clang::QualType input, cons
          QT = Ctxt.getVariableArrayType (newQT,
                                         arr->getSizeExpr(),
                                         arr->getSizeModifier(),
-                                        arr->getIndexTypeCVRQualifiers(),
-                                        arr->getBracketsRange());
+                                        arr->getIndexTypeCVRQualifiers());
       }
 
       // Add back the qualifiers.
