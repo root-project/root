992c662c8c19d6bb51c3a52315a1d6ae14fd8ab8
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2026-03-05 15:56:23 +0100

    [core][LLVM22] Adapt to LLVM 22 API changes
    - Update function signatures
      - getDependentSizedArrayType
      - TextDiagnosticPrinter
      - InstantiatingTemplate
      -  ASTWriter
    
    - LANGOPT change
      - https://github.com/llvm/llvm-project/commit/fee1689

--- core/clingutils/src/TClingUtils.cxx
+++ core/clingutils/src/TClingUtils.cxx
@@ -4869,8 +4869,7 @@ clang::QualType ROOT::TMetaUtils::ReSubstTemplateArg(clang::QualType input, cons
          QT = Ctxt.getDependentSizedArrayType (newQT,
                                               arr->getSizeExpr(),
                                               arr->getSizeModifier(),
-                                              arr->getIndexTypeCVRQualifiers(),
-                                              arr->getBracketsRange());
+                                              arr->getIndexTypeCVRQualifiers());
 
       } else if (const auto arr = dyn_cast<IncompleteArrayType>(QT.getTypePtr())) {
          QualType newQT = ReSubstTemplateArg(arr->getElementType(),instance);
@@ -4887,8 +4886,7 @@ clang::QualType ROOT::TMetaUtils::ReSubstTemplateArg(clang::QualType input, cons
          QT = Ctxt.getVariableArrayType (newQT,
                                         arr->getSizeExpr(),
                                         arr->getSizeModifier(),
-                                        arr->getIndexTypeCVRQualifiers(),
-                                        arr->getBracketsRange());
+                                        arr->getIndexTypeCVRQualifiers());
       }
 
       // Add back the qualifiers.
