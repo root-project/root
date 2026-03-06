ffd98c50e6d93cd766762dc56bcbb0d5b9ca42a3
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2026-03-06 11:18:20 +0100

    [cling] StringSet API update

--- interpreter/cling/lib/Interpreter/IncrementalJIT.cpp
+++ interpreter/cling/lib/Interpreter/IncrementalJIT.cpp
@@ -837,7 +837,7 @@ void* IncrementalJIT::getSymbolAddress(StringRef Name, bool IncludeHostSymbols){
   if (!IncludeHostSymbols)
     G.lock();
 
-  std::pair<llvm::StringMapIterator<std::nullopt_t>, bool> insertInfo;
+  std::pair<llvm::StringSet<>::iterator, bool> insertInfo;
   if (!IncludeHostSymbols)
     insertInfo = m_ForbidDlSymbols.insert(Name);
 
