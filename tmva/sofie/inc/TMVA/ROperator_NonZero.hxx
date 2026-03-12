855380cedbb7d25c2cc65187d3d0a7c017110acb
Jonas Rembser <jonas.rembser@cern.ch>
2026-03-12 09:20:41 +0100

    Revert "[tmva][sofie] Fix NonZero to define max output shape values in Session ctor"
    This reverts commit 1f747b0bafd3a05e54217771a29a2cdb6b91da0f.
    
    The reason for the revert is that it's actually useful to have the
    maximum dynamic tensor size as a datamember of the Session, because then
    we can refactor the generated code such that it can be differentiated
    with Clad.

--- tmva/sofie/inc/TMVA/ROperator_NonZero.hxx
+++ tmva/sofie/inc/TMVA/ROperator_NonZero.hxx
@@ -92,8 +92,8 @@ public:
          fShapeY.resize(2);
          fShapeY[0] = fShapeX.size();
 
-         // flag -1 to define the shape variable in the constructor code and not in the constructor signature
-         fShapeY[1] = Dim{std::string("v_NonZero_") + fNX, static_cast<size_t>(-1) };
+         // identify as -1 since we will declare maximum as size of input
+         fShapeY[1] = Dim{std::string("v_NonZero_") + fNX, static_cast<size_t>(-1)};
 
          model.AddIntermediateTensor(fNY, ETensorType::INT64, fShapeY);
          if (model.Verbose()) {
@@ -101,7 +101,7 @@ public:
          }
       }
    }
-   std::string GenerateSessionCtorCode() override {
+   std::string GenerateSessionMembersCode(std::string /*opName*/) override {
       if (fIsOutputConstant) return "";
       // define output value used as max non zero with max size = input shape * N
       auto inputLength = ConvertDimShapeToLength(fShapeX);
@@ -133,7 +133,7 @@ public:
 
       // loop on input indices
       out << SP << "size_t offset_" << opName << " = 0;\n";
-      out << SP << "size_t " << vnonzero << " = 0;\n";
+      out << SP << vnonzero << " = 0;\n";
       for (size_t j = 0; j < dims; j++) {
          std::string index = "i_" + std::to_string(j);
          for (size_t k = 0; k <= j; k++) out << SP;
