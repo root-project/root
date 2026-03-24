5d423009b16f2e159dc66b7f7f857bcbf0ca83d6
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2026-03-24 16:20:08 +0100

    [Python] Replace more equality checks with identity checks
    This is continuing the preparation for the more strict comparison
    operator implementation in cppyy.

--- bindings/pyroot/cppyy/cppyy/test/test_datatypes.py
+++ bindings/pyroot/cppyy/cppyy/test/test_datatypes.py
@@ -644,7 +644,7 @@ class TestDATATYPES:
 
         d = gbl.get_global_pod()
         assert gbl.is_global_pod(d)
-        assert c == d
+        assert c is d
         assert id(c) == id(d)
 
         e = gbl.CppyyTestPod()
