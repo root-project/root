db6f746832d0092cbe7e8401a2bbdd836a95b084
Sergey Linev <S.Linev@gsi.de>
2026-03-31 11:05:39 +0200

    [win32] implement ClearWindowW and UpdateWindowW

--- graf2d/win32gdk/inc/TGWin32.h
+++ graf2d/win32gdk/inc/TGWin32.h
@@ -219,6 +219,8 @@ public:
    void      SetAttText(WinContext_t wctxt, const TAttText &att) override;
    void      SetDrawModeW(WinContext_t wctxt, EDrawMode mode) override;
    EDrawMode GetDrawModeW(WinContext_t wctxt) override;
+   void      ClearWindowW(WinContext_t wctxt) override;
+   void      UpdateWindowW(WinContext_t wctxt, Int_t mode) override;
 
    void      DrawBoxW(WinContext_t wctxt, Int_t x1, Int_t y1, Int_t x2, Int_t y2, EBoxMode mode) override;
    void      DrawFillAreaW(WinContext_t wctxt, Int_t n, TPoint *xy) override;
