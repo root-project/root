26035be08f1a299f73fc6ee11b15ee812ed0aea0
Jonas Rembser <jonas.rembser@cern.ch>
2026-04-05 23:49:46 +0200

    Format TMVA SOFIE tutorial code
    User-facing code like tutorials should be nicely formatted, in
    particular avoiding non-standard indentations with three spaces that can
    annoy users when copy-pasting code.

--- tutorials/machine_learning/TMVA_SOFIE_Keras.py
+++ tutorials/machine_learning/TMVA_SOFIE_Keras.py
@@ -9,7 +9,6 @@
 ### \author Sanjiban Sengupta and Lorenzo Moneta
 
 
-
 import ROOT
 
 # Enable ROOT in batch mode (same effect as -nodraw)
@@ -23,22 +22,22 @@ import numpy as np
 from tensorflow.keras.layers import Activation, Dense, Input, Softmax
 from tensorflow.keras.models import Model
 
-input=Input(shape=(4,),batch_size=2)
-x=Dense(32)(input)
-x=Activation('relu')(x)
-x=Dense(16,activation='relu')(x)
-x=Dense(8,activation='relu')(x)
-x=Dense(2)(x)
-output=Softmax()(x)
-model=Model(inputs=input,outputs=output)
+input = Input(shape=(4,), batch_size=2)
+x = Dense(32)(input)
+x = Activation("relu")(x)
+x = Dense(16, activation="relu")(x)
+x = Dense(8, activation="relu")(x)
+x = Dense(2)(x)
+output = Softmax()(x)
+model = Model(inputs=input, outputs=output)
 
-randomGenerator=np.random.RandomState(0)
-x_train=randomGenerator.rand(4,4)
-y_train=randomGenerator.rand(4,2)
+randomGenerator = np.random.RandomState(0)
+x_train = randomGenerator.rand(4, 4)
+y_train = randomGenerator.rand(4, 2)
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss="mse", optimizer="adam")
 model.fit(x_train, y_train, epochs=3, batch_size=2)
-model.save('KerasModel.keras')
+model.save("KerasModel.keras")
 model.summary()
 
 # -----------------------------------------------------------------------------
@@ -54,7 +53,7 @@ model = ROOT.TMVA.Experimental.SOFIE.PyKeras.Parse("KerasModel.keras")
 # Generate inference code
 model.Generate()
 model.OutputGenerated()
-#print generated code
+# print generated code
 print("\n**************************************************")
 print("           Generated code")
 print("**************************************************\n")
@@ -69,14 +68,13 @@ ROOT.gInterpreter.Declare('#include "KerasModel.hxx"')
 # Step 3: Run inference
 # -----------------------------------------------------------------------------
 
-#instantiate SOFIE session class
+# instantiate SOFIE session class
 session = ROOT.TMVA_SOFIE_KerasModel.Session()
 
 # Input tensor (same shape as training input)
-x = np.array([[0.1, 0.2, 0.3, 0.4],[0.5, 0.6, 0.7, 0.8]], dtype=np.float32)
+x = np.array([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]], dtype=np.float32)
 
 # Run inference
 y = session.infer(x)
 
 print("Inference output:", y)
-
