bb493ca17ae9937f5066a3c67733005a47a05078
Danilo Piparo <danilo.piparo@cern.ch>
2026-04-06 12:09:57 +0200

    [net] Check received message length
    thanks to @Sebasteuo
    
    Partially cherry picked from commit f6e811576ab2db571723f5af40ecda2484c1cc84
    Modifications were necessary for the TSocket.cxx file
    
    (cherry picked from commit 3b1dfaabb48f410a68cd8aef639493d7d3a38b2c)

--- net/net/src/TSocket.cxx
+++ net/net/src/TSocket.cxx
@@ -9,16 +9,17 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// TSocket                                                              //
-//                                                                      //
-// This class implements client sockets. A socket is an endpoint for    //
-// communication between two machines.                                  //
-// The actual work is done via the TSystem class (either TUnixSystem    //
-// or TWinNTSystem).                                                    //
-//                                                                      //
-//////////////////////////////////////////////////////////////////////////
+/**
+\file TSocket.cxx
+\class TSocket
+\brief This class implements client sockets.
+\note This class deals with sockets: the user is entirely responsible for the security of their usage, for example, but
+not limited to, the management of the connections to said sockets.
+
+A socket is an endpoint for communication between two machines. The actual work is done via the TSystem class (either
+TUnixSystem or TWinNTSystem).
+
+**/
 
 #include "Bytes.h"
 #include "Compression.h"
@@ -36,6 +37,7 @@
 #include "TStreamerInfo.h"
 #include "TProcessID.h"
 
+#include <limits>
 
 ULong64_t TSocket::fgBytesSent = 0;
 ULong64_t TSocket::fgBytesRecv = 0;
@@ -837,6 +839,11 @@ oncemore:
       return n;
    }
    len = net2host(len);  //from network to host byte order
+
+   if (len > (std::numeric_limits<decltype(len)>::max() - sizeof(decltype(len)))) {
+      Error("Recv", "Buffer length is %u and %u+sizeof(UInt_t) cannot be represented as an UInt_t.", len, len);
+      return -1;
+   }
 
    ResetBit(TSocket::kBrokenConn);
    char *buf = new char[len+sizeof(UInt_t)];
