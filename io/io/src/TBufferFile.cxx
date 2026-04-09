3c7ef8affec9ffc16fa59cbbb21314342e5e1352
Danilo Piparo <danilo.piparo@cern.ch>
2026-04-09 07:45:34 +0200

    [io] TBufferFile: more checks when reading arrays
    i.e. in the TBufferFile::Read*Array* methods.
    
    Previously, the collection length was checked to be positive
    and that it was less than the total buffer size.
    
    Those checks have been improved with a battery of 3 checks:
        1. The collection's number of elements is checked to be positive
        2. The collection length is checked to be positive
        3. The collection length is checked to be less than the portion
        of the buffer that remains to be read
    
    The test roottest-root-io-prefetching-run had to be modified as well
    because of what seems to be a corrupted input file.
    
    thanks also to @Sebasteuo
    
    (cherry picked from commit 6ab4902db19a7c05522c7bef7e3176fecfd92fec)

--- io/io/src/TBufferFile.cxx
+++ io/io/src/TBufferFile.cxx
@@ -738,7 +738,7 @@ Int_t TBufferFile::ReadArray(Bool_t *&b)
    Int_t n;
    *this >> n;
 
-   if (n <= 0 || n > fBufSize) return 0;
+   if (ShouldNotReadCollection(n)) return 0;
 
    if (!b) b = new Bool_t[n];
 
@@ -767,7 +767,7 @@ Int_t TBufferFile::ReadArray(Char_t *&c)
    *this >> n;
    Int_t l = sizeof(Char_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!c) c = new Char_t[n];
 
@@ -790,7 +790,7 @@ Int_t TBufferFile::ReadArray(Short_t *&h)
    *this >> n;
    Int_t l = sizeof(Short_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!h) h = new Short_t[n];
 
@@ -823,7 +823,7 @@ Int_t TBufferFile::ReadArray(Int_t *&ii)
    *this >> n;
    Int_t l = sizeof(Int_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!ii) ii = new Int_t[n];
 
@@ -856,7 +856,7 @@ Int_t TBufferFile::ReadArray(Long_t *&ll)
    *this >> n;
    Int_t l = sizeof(Long_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!ll) ll = new Long_t[n];
 
@@ -882,7 +882,7 @@ Int_t TBufferFile::ReadArray(Long64_t *&ll)
    *this >> n;
    Int_t l = sizeof(Long64_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!ll) ll = new Long64_t[n];
 
@@ -910,7 +910,7 @@ Int_t TBufferFile::ReadArray(Float_t *&f)
    *this >> n;
    Int_t l = sizeof(Float_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!f) f = new Float_t[n];
 
@@ -943,7 +943,7 @@ Int_t TBufferFile::ReadArray(Double_t *&d)
    *this >> n;
    Int_t l = sizeof(Double_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!d) d = new Double_t[n];
 
@@ -971,7 +971,7 @@ Int_t TBufferFile::ReadArrayFloat16(Float_t *&f, TStreamerElement *ele)
    Int_t n;
    *this >> n;
 
-   if (n <= 0 || 3*n > fBufSize) return 0;
+   if (ShouldNotReadCollection(3*n, n)) return 0;
 
    if (!f) f = new Float_t[n];
 
@@ -993,7 +993,7 @@ Int_t TBufferFile::ReadArrayDouble32(Double_t *&d, TStreamerElement *ele)
    Int_t n;
    *this >> n;
 
-   if (n <= 0 || 3*n > fBufSize) return 0;
+   if (ShouldNotReadCollection(3*n, n)) return 0;
 
    if (!d) d = new Double_t[n];
 
@@ -1013,7 +1013,7 @@ Int_t TBufferFile::ReadStaticArray(Bool_t *b)
    Int_t n;
    *this >> n;
 
-   if (n <= 0 || n > fBufSize) return 0;
+   if (ShouldNotReadCollection(n)) return 0;
 
    if (!b) return 0;
 
@@ -1041,7 +1041,7 @@ Int_t TBufferFile::ReadStaticArray(Char_t *c)
    *this >> n;
    Int_t l = sizeof(Char_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!c) return 0;
 
@@ -1063,7 +1063,7 @@ Int_t TBufferFile::ReadStaticArray(Short_t *h)
    *this >> n;
    Int_t l = sizeof(Short_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!h) return 0;
 
@@ -1095,7 +1095,7 @@ Int_t TBufferFile::ReadStaticArray(Int_t *ii)
    *this >> n;
    Int_t l = sizeof(Int_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!ii) return 0;
 
@@ -1127,7 +1127,7 @@ Int_t TBufferFile::ReadStaticArray(Long_t *ll)
    *this >> n;
    Int_t l = sizeof(Long_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!ll) return 0;
 
@@ -1152,7 +1152,7 @@ Int_t TBufferFile::ReadStaticArray(Long64_t *ll)
    *this >> n;
    Int_t l = sizeof(Long64_t)*n;
 
-   if (l <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!ll) return 0;
 
@@ -1179,7 +1179,7 @@ Int_t TBufferFile::ReadStaticArray(Float_t *f)
    *this >> n;
    Int_t l = sizeof(Float_t)*n;
 
-   if (n <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!f) return 0;
 
@@ -1211,7 +1211,7 @@ Int_t TBufferFile::ReadStaticArray(Double_t *d)
    *this >> n;
    Int_t l = sizeof(Double_t)*n;
 
-   if (n <= 0 || l > fBufSize) return 0;
+   if (ShouldNotReadCollection(l, n)) return 0;
 
    if (!d) return 0;
 
@@ -1238,7 +1238,7 @@ Int_t TBufferFile::ReadStaticArrayFloat16(Float_t *f, TStreamerElement *ele)
    Int_t n;
    *this >> n;
 
-   if (n <= 0 || 3*n > fBufSize) return 0;
+   if (ShouldNotReadCollection(3*n, n)) return 0;
 
    if (!f) return 0;
 
@@ -1259,7 +1259,7 @@ Int_t TBufferFile::ReadStaticArrayDouble32(Double_t *d, TStreamerElement *ele)
    Int_t n;
    *this >> n;
 
-   if (n <= 0 || 3*n > fBufSize) return 0;
+   if (ShouldNotReadCollection(3*n, n)) return 0;
 
    if (!d) return 0;
 
@@ -1273,7 +1273,7 @@ Int_t TBufferFile::ReadStaticArrayDouble32(Double_t *d, TStreamerElement *ele)
 
 void TBufferFile::ReadFastArray(Bool_t *b, Int_t n)
 {
-   if (n <= 0 || n > fBufSize) return;
+   if (ShouldNotReadCollection(n)) return;
 
    if (sizeof(Bool_t) > 1) {
       for (int i = 0; i < n; i++)
@@ -1290,7 +1290,7 @@ void TBufferFile::ReadFastArray(Bool_t *b, Int_t n)
 
 void TBufferFile::ReadFastArray(Char_t *c, Int_t n)
 {
-   if (n <= 0 || n > fBufSize) return;
+   if (ShouldNotReadCollection(n)) return;
 
    Int_t l = sizeof(Char_t)*n;
    memcpy(c, fBufCur, l);
@@ -1311,7 +1311,7 @@ void TBufferFile::ReadFastArrayString(Char_t *c, Int_t n)
       *this >> len;
    }
    if (len) {
-      if (len <= 0 || len > fBufSize) return;
+      if (ShouldNotReadCollection(len)) return;
       Int_t blen = len;
       if (len >= n) len = n-1;
 
@@ -1331,7 +1331,7 @@ void TBufferFile::ReadFastArrayString(Char_t *c, Int_t n)
 void TBufferFile::ReadFastArray(Short_t *h, Int_t n)
 {
    Int_t l = sizeof(Short_t)*n;
-   if (n <= 0 || l > fBufSize) return;
+   if (ShouldNotReadCollection(l, n)) return;
 
 #ifdef R__BYTESWAP
 # ifdef USE_BSWAPCPY
@@ -1353,7 +1353,7 @@ void TBufferFile::ReadFastArray(Short_t *h, Int_t n)
 void TBufferFile::ReadFastArray(Int_t *ii, Int_t n)
 {
    Int_t l = sizeof(Int_t)*n;
-   if (l <= 0 || l > fBufSize) return;
+   if (ShouldNotReadCollection(l, n)) return;
 
 #ifdef R__BYTESWAP
 # ifdef USE_BSWAPCPY
@@ -1375,7 +1375,7 @@ void TBufferFile::ReadFastArray(Int_t *ii, Int_t n)
 void TBufferFile::ReadFastArray(Long_t *ll, Int_t n)
 {
    Int_t l = sizeof(Long_t)*n;
-   if (l <= 0 || l > fBufSize) return;
+   if (ShouldNotReadCollection(l, n)) return;
 
    TFile *file = (TFile*)fParent;
    if (file && file->GetVersion() < 30006) {
@@ -1391,7 +1391,7 @@ void TBufferFile::ReadFastArray(Long_t *ll, Int_t n)
 void TBufferFile::ReadFastArray(Long64_t *ll, Int_t n)
 {
    Int_t l = sizeof(Long64_t)*n;
-   if (l <= 0 || l > fBufSize) return;
+   if (ShouldNotReadCollection(l, n)) return;
 
 #ifdef R__BYTESWAP
    for (int i = 0; i < n; i++)
@@ -1408,7 +1408,7 @@ void TBufferFile::ReadFastArray(Long64_t *ll, Int_t n)
 void TBufferFile::ReadFastArray(Float_t *f, Int_t n)
 {
    Int_t l = sizeof(Float_t)*n;
-   if (l <= 0 || l > fBufSize) return;
+   if (ShouldNotReadCollection(l, n)) return;
 
 #ifdef R__BYTESWAP
 # ifdef USE_BSWAPCPY
@@ -1430,7 +1430,7 @@ void TBufferFile::ReadFastArray(Float_t *f, Int_t n)
 void TBufferFile::ReadFastArray(Double_t *d, Int_t n)
 {
    Int_t l = sizeof(Double_t)*n;
-   if (l <= 0 || l > fBufSize) return;
+   if (ShouldNotReadCollection(l, n)) return;
 
 #ifdef R__BYTESWAP
    for (int i = 0; i < n; i++)
@@ -1447,7 +1447,7 @@ void TBufferFile::ReadFastArray(Double_t *d, Int_t n)
 
 void TBufferFile::ReadFastArrayFloat16(Float_t *f, Int_t n, TStreamerElement *elem)
 {
-   if (n <= 0 || 3*n > fBufSize) return;
+   if (ShouldNotReadCollection(3*n, n)) return;
 
    // The parameter should be const, however we have not yet decided how to
    // transition the signature since the function is virtual.  This ensures
@@ -1493,7 +1493,7 @@ void TBufferFile::ReadFastArrayFloat16(Float_t *f, Int_t n, TStreamerElement *el
 
 void TBufferFile::ReadFastArrayWithFactor(Float_t *ptr, Int_t n, Double_t factor, Double_t minvalue)
 {
-   if (n <= 0 || 3*n > fBufSize) return;
+   if (ShouldNotReadCollection(3*n, n)) return;
 
    //a range was specified. We read an integer and convert it back to a float
    for (int j=0;j < n; j++) {
@@ -1507,7 +1507,7 @@ void TBufferFile::ReadFastArrayWithFactor(Float_t *ptr, Int_t n, Double_t factor
 
 void TBufferFile::ReadFastArrayWithNbits(Float_t *ptr, Int_t n, Int_t nbits)
 {
-   if (n <= 0 || 3*n > fBufSize) return;
+   if (ShouldNotReadCollection(3*n, n)) return;
 
    if (!nbits) nbits = 12;
    //we read the exponent and the truncated mantissa of the float
@@ -1540,7 +1540,7 @@ void TBufferFile::ReadFastArrayDouble32(Double_t *d, Int_t n, TStreamerElement *
    // that the function does not inadvertently use non-const parts of
    // TStreamerElement.
    const TStreamerElement *ele = elem;
-   if (n <= 0 || 3*n > fBufSize) return;
+   if (ShouldNotReadCollection(3*n, n)) return;
 
    if (ele && ele->GetFactor() != 0) {
       //a range was specified. We read an integer and convert it back to a double.
@@ -1588,7 +1588,7 @@ void TBufferFile::ReadFastArrayDouble32(Double_t *d, Int_t n, TStreamerElement *
 
 void TBufferFile::ReadFastArrayWithFactor(Double_t *d, Int_t n, Double_t factor, Double_t minvalue)
 {
-   if (n <= 0 || 3*n > fBufSize) return;
+   if (ShouldNotReadCollection(3*n, n)) return;
 
    //a range was specified. We read an integer and convert it back to a double.
    for (int j=0;j < n; j++) {
@@ -1602,7 +1602,7 @@ void TBufferFile::ReadFastArrayWithFactor(Double_t *d, Int_t n, Double_t factor,
 
 void TBufferFile::ReadFastArrayWithNbits(Double_t *d, Int_t n, Int_t nbits)
 {
-   if (n <= 0 || 3*n > fBufSize) return;
+   if (ShouldNotReadCollection(3*n, n)) return;
 
    if (!nbits) {
       //we read a float and convert it to double
