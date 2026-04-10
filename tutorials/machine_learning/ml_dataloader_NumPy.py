929985a5cf0385b0f8d37971998f5043ad9fe06a
Silia Taider <siliataider@gmail.com>
2026-04-10 19:44:28 +0200

    [Python][ML] Adapt tutorials to the new RDataLoader interface

--- tutorials/machine_learning/ml_dataloader_NumPy.py
+++ tutorials/machine_learning/ml_dataloader_NumPy.py
@@ -14,8 +14,6 @@ tree_name = "sig_tree"
 file_name = str(ROOT.gROOT.GetTutorialDir()) + "/machine_learning/data/Higgs_data.root"
 
 batch_size = 128
-chunk_size = 5000
-block_size = 400
 
 rdataframe = ROOT.RDataFrame(tree_name, file_name)
 
@@ -23,22 +21,21 @@ target = "Type"
 
 num_of_epochs = 2
 
-gen_train, gen_validation = ROOT.Experimental.ML.CreateNumPyGenerators(
+dl = ROOT.Experimental.ML.RDataLoader(
     rdataframe,
     batch_size,
-    chunk_size,
-    block_size,
     target=target,
-    validation_split=0.3,
     shuffle=True,
     drop_remainder=True,
 )
 
+gen_train, gen_validation = dl.train_test_split(test_size=0.3)
+
 for i in range(num_of_epochs):
     # Loop through training set
-    for i, (x_train, y_train) in enumerate(gen_train):
+    for i, (x_train, y_train) in enumerate(gen_train.as_numpy()):
         print(f"Training batch {i + 1} => x: {x_train.shape}, y: {y_train.shape}")
 
     # Loop through Validation set
-    for i, (x_validation, y_validation) in enumerate(gen_validation):
+    for i, (x_validation, y_validation) in enumerate(gen_validation.as_numpy()):
         print(f"Validation batch {i + 1} => x: {x_validation.shape}, y: {y_validation.shape}")
--- tutorials/machine_learning/ml_dataloader_TensorFlow.py
+++ tutorials/machine_learning/ml_dataloader_TensorFlow.py
@@ -18,33 +18,32 @@ tree_name = "sig_tree"
 file_name = str(ROOT.gROOT.GetTutorialDir()) + "/machine_learning/data/Higgs_data.root"
 
 batch_size = 128
-chunk_size = 5000
-block_size = 300
+approx_batches_in_memory = 50
 
 rdataframe = ROOT.RDataFrame(tree_name, file_name)
 target = ["Type"]
 
 # Returns two TF.Dataset for training and validation batches.
-ds_train, ds_valid = ROOT.Experimental.ML.CreateTFDatasets(
+dl = ROOT.Experimental.ML.RDataLoader(
     rdataframe,
     batch_size,
-    chunk_size,
-    block_size,
+    approx_batches_in_memory,
     target=target,
-    validation_split=0.3,
     shuffle=True,
     drop_remainder=True,
 )
 
+ds_train, ds_valid = dl.train_test_split(test_size=0.3)
+
 num_of_epochs = 2
 
 # Datasets have to be repeated as many times as there are epochs
-ds_train_repeated = ds_train.repeat(num_of_epochs)
-ds_valid_repeated = ds_valid.repeat(num_of_epochs)
+ds_train_repeated = ds_train.as_tensorflow().repeat(num_of_epochs)
+ds_valid_repeated = ds_valid.as_tensorflow().repeat(num_of_epochs)
 
 # Number of batches per epoch must be given for model.fit
-train_batches_per_epoch = ds_train.number_of_batches
-validation_batches_per_epoch = ds_valid.number_of_batches
+train_batches_per_epoch = ds_train.num_batches
+validation_batches_per_epoch = ds_valid.num_batches
 
 # Get a list of the columns used for training
 input_columns = ds_train.train_columns
