42abb759bccac7c80a6775877450980e260eb1e6
ferdymercury <ferdymercury@users.noreply.github.com>
2026-04-20 18:03:54 +0200

    [test] add tiff test

--- roottest/root/graf/simpleImages.C
+++ roottest/root/graf/simpleImages.C
@@ -15,7 +15,7 @@ int simpleImages()
    TH1D h("myHisto", "The Title;the X;the Y", 64, -4, 4);
    h.FillRandom("gaus");
    h.Draw();
-   std::vector<std::string> fileNames{"f.jpeg", "f.png", "f.gif", "f.bmp"}; // TIFF not always there: only ON if asimage_tiff and libtiff installed in system (it's not a builtin like jpeg, png, gif)
+   std::vector<std::string> fileNames{"f.jpeg", "f.png", "f.gif", "f.bmp", "f.tiff"};
    ImagesRAII iraii(fileNames);
    for (auto &&fileName : fileNames) {
       c.SaveAs(fileName.c_str());
