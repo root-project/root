06a86afd762c19f82117d50c082c8f9f026b103c
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-04-23 10:54:31 +0200

    Update ROOT version files to v6.41.01

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -3,8 +3,8 @@
 
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
-#define ROOT_VERSION_MINOR 39
-#define ROOT_VERSION_PATCH 99  // When changing the version number here, never add leading zeroes!
+#define ROOT_VERSION_MINOR 41
+#define ROOT_VERSION_PATCH 1 // When changing the version number here, never add leading zeroes!
 #define ROOT_RELEASE_DATE "Apr 23 2026"
 
 /* Don't change the lines below. */
