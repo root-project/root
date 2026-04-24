5cd6d592b221580ffee0e053f89bcd27b3a93189
Aaron Jomy <aaronjomyjoseph@gmail.com>
2026-04-24 16:46:53 +0200

    [CPyCppyy] Rename PyInit_libcppyy to Init for ROOT module split  [ROOT-patch]
    Source: ROOT-master
    
    ROOT runs CPyCppyy and libROOTPythonizations as two shared libs
    that under the same libcppyy Python extension module. The real PyInit_libcppyy
    lives in libROOTPythonizations and calls CPyCppyy::Init() to start the extension.

--- bindings/pyroot/cppyy/CPyCppyy/src/CPyCppyyModule.cxx
+++ bindings/pyroot/cppyy/CPyCppyy/src/CPyCppyyModule.cxx
@@ -1089,7 +1089,7 @@ static struct PyModuleDef moduledef = {
 namespace CPyCppyy {
 
 //----------------------------------------------------------------------------
-extern "C" PyObject* PyInit_libcppyy()
+PyObject* Init()
 {
 // Initialization of extension module libcppyy.
 
