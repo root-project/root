68b10d7d6f12f3c355a42ba04340e0fe5265056d
Sergey Linev <S.Linev@gsi.de>
2026-05-05 17:22:48 +0200

    [jsroot] 7.11.0
    (cherry picked from commit c21ff0b2d8b816db7f8178a0b86f6ed4f9a0135c)

--- js/modules/core.mjs
+++ js/modules/core.mjs
@@ -2,11 +2,11 @@
 
 /** @summary version id
   * @desc For the JSROOT release the string in format 'major.minor.patch' like '7.0.0' */
-const version_id = 'dev',
+const version_id = '7.11.0',
 
 /** @summary version date
   * @desc Release date in format day/month/year like '14/04/2022' */
-version_date = '16/04/2026',
+version_date = '5/05/2026',
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
