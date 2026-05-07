b176e86a7a58ee122315bd3af028b76cbc294bfc
Bertrand Bellenot <bellenot@users.noreply.github.com>
2026-05-07 16:54:33 +0200

    [IO] Fix potential buffer overflow
    Fixes https://github.com/root-project/root/security/code-scanning/1846

--- io/io/src/TFile.cxx
+++ io/io/src/TFile.cxx
@@ -4410,7 +4410,7 @@ Bool_t TFile::ShrinkCacheFileDir(Long64_t shrinksize, Long_t cleanupinterval)
    cmd.Form("perl -e 'my $cachepath = \"%s\"; my $cachesize = %lld;my $findcommand=\"find $cachepath -type f -exec stat -c \\\"\\%%x::\\%%n::\\%%s\\\" \\{\\} \\\\\\;\";my $totalsize=0;open FIND, \"$findcommand | sort -k 1 |\";while (<FIND>) { my ($accesstime, $filename, $filesize) = split \"::\",$_; $totalsize += $filesize;if ($totalsize > $cachesize) {if ( ( -e \"${filename}.ROOT.cachefile\" ) || ( -e \"${filename}\" ) ) {unlink \"$filename.ROOT.cachefile\";unlink \"$filename\";}}}close FIND;' ", fgCacheFileDir.Data(),shrinksize);
 #endif
 
-   tagfile->WriteBuffer(cmd, 4096);
+   tagfile->WriteBuffer(cmd, cmd.Sizeof());
    delete tagfile;
 
    if ((gSystem->Exec(cmd)) != 0) {
