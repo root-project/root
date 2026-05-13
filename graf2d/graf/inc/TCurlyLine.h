14f436eac506df69f2fe69556d3d922c93be3dfb
Sergey Linev <S.Linev@gsi.de>
2026-05-13 10:31:22 +0200

    Adjust TCurlyLine::ExecuteEvent and bbox
    Use original object coordinates - pixels only when needed
    Reduce number of static variables
    Correctly use conversion to/from pixels
    Simplify bbox handling

--- graf2d/graf/inc/TCurlyLine.h
+++ graf2d/graf/inc/TCurlyLine.h
@@ -64,8 +64,6 @@ public:
    static Bool_t   GetDefaultIsCurly   ();
 
    Rectangle_t  GetBBox() override;
-   TPoint       GetBBoxCenter() override;
-   void         SetBBoxCenter(const TPoint &p) override;
    void         SetBBoxCenterX(const Int_t x) override;
    void         SetBBoxCenterY(const Int_t y) override;
    void         SetBBoxX1(const Int_t x) override;
