1585e59900927021b23a7c468b944be903cdf761
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2026-05-13 11:01:24 +0200

    [cling][macos] Allow framework includes in System group in CopyIncludePaths
    CopyIncludePaths assumed that a framework entry always implies
    `Group==Angled` and called `report_fatal_error` for any other group.
    
    This changed after LLVM commit: https://github.com/llvm/llvm-project/commit/515b4a4
    
    Framework entries land in UserEntries with `IsFramework=true` and
    `Group=frontend::System` and cling fails on macos with
    "Invalid option set!"
    
    Fix by also allowing `Group=frontend::System` for framework entries.
    
    See https://github.com/compiler-research/CppInterOp/pull/672#issuecomment-3723244654

--- interpreter/cling/lib/Utils/Paths.cpp
+++ interpreter/cling/lib/Utils/Paths.cpp
@@ -65,7 +65,8 @@ void CopyIncludePaths(const clang::HeaderSearchOptions& Opts,
   /// User specified include entries.
   for (unsigned i = 0, e = Opts.UserEntries.size(); i != e; ++i) {
     const HeaderSearchOptions::Entry &E = Opts.UserEntries[i];
-    if (E.IsFramework && E.Group != frontend::Angled)
+    if (E.IsFramework && E.Group != frontend::Angled &&
+        E.Group != frontend::System)
       llvm::report_fatal_error("Invalid option set!");
     switch (E.Group) {
     case frontend::After:
