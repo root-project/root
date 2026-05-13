90b84d0fc68dc2bf0102fd0ba85ee7134aa360c3
Sergey Linev <S.Linev@gsi.de>
2026-05-13 13:18:47 +0200

    Optimize TCurlyArc::ExecuteEvent and BBox
    Use proper coordinates transformation
    Avoid gVirtualX
    Correctly calculate distance to primitive

--- graf2d/graf/inc/TCurlyArc.h
+++ graf2d/graf/inc/TCurlyArc.h
@@ -53,7 +53,6 @@ public:
 
    Rectangle_t  GetBBox() override;
    TPoint       GetBBoxCenter() override;
-   void         SetBBoxCenter(const TPoint &p) override;
    void         SetBBoxCenterX(const Int_t x) override;
    void         SetBBoxCenterY(const Int_t y) override;
    void         SetBBoxX1(const Int_t x) override;
