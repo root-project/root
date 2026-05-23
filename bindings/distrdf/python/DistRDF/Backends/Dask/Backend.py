ef7a765adb13a8644c0990d57ada96ebe42a6784
JAGANNATHANJP <jpjagannathan07@gmail.com>
2026-05-23 19:59:16 +0530

    Prevent threaded Dask workers in DistRDF backend

--- bindings/distrdf/python/DistRDF/Backends/Dask/Backend.py
+++ bindings/distrdf/python/DistRDF/Backends/Dask/Backend.py
@@ -104,6 +104,17 @@ class DaskBackend(Base.BaseBackend):
         # N is the number of cores on the local machine.
         self.client = (daskclient if daskclient is not None else
                        Client(LocalCluster(n_workers=os.cpu_count(), threads_per_worker=1, processes=True)))
+        
+        workers = self.client.scheduler_info()["workers"]
+
+        for worker in workers.values():
+            threads = worker.get("nthreads", 1)
+
+            if threads > 1:
+               raise RuntimeError(
+                "DistRDF with Dask does not support threaded workers. "
+                "Please use processes=True and threads_per_worker=1."
+            )
 
     def optimize_npartitions(self) -> int:
         """
