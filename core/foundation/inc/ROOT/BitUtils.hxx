739a94d5b0c5ab9c0d41afd71760affffc5a4219
Jakob Blomer <jblomer@cern.ch>
2026-05-24 22:45:14 +0200

    [core] add IsPowerOfTwo() to bit utils

--- core/foundation/inc/ROOT/BitUtils.hxx
+++ core/foundation/inc/ROOT/BitUtils.hxx
@@ -15,6 +15,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <type_traits>
 
 #ifdef _MSC_VER
@@ -24,12 +25,17 @@
 namespace ROOT {
 namespace Internal {
 
+inline bool IsPowerOfTwo(std::uint64_t v)
+{
+   return (v & (v - 1)) == 0;
+}
+
 /// Return true if \p align is a valid C++ alignment value: strictly positive
 /// and a power of two.  This is the set of values accepted by
 /// `::operator new[](n, std::align_val_t(align))`.
 inline constexpr bool IsValidAlignment(std::size_t align) noexcept
 {
-   return align > 0 && (align & (align - 1)) == 0;
+   return align > 0 && IsPowerOfTwo(align);
 }
 
 /// Round \p value up to the next multiple of \p align.
