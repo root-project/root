c6c3a0267200a04fb49e1f34be6ac3910751bb35
Jakob Blomer <jblomer@cern.ch>
2026-05-27 17:27:52 +0200

    [ntuple] replace std::endl by \n

--- tree/ntupleutil/src/RNTupleInspector.cxx
+++ tree/ntupleutil/src/RNTupleInspector.cxx
@@ -265,21 +265,20 @@ void ROOT::Experimental::RNTupleInspector::PrintColumnTypeInfo(ENTupleInspectorP
       output << " column type    | count   | # elements  | compressed bytes | uncompressed bytes | compression ratio | "
                 "# pages \n"
              << "----------------|---------|-------------|------------------|--------------------|-------------------|-"
-                "------"
-             << std::endl;
+                "------\n";
       for (const auto &[colType, typeInfo] : colTypeInfo)
          output << std::setw(15) << RColumnElementBase::GetColumnTypeName(colType) << " |" << std::setw(8)
                 << typeInfo.count << " |" << std::setw(12) << typeInfo.nElems << " |" << std::setw(17)
                 << typeInfo.compressedSize << " |" << std::setw(19) << typeInfo.uncompressedSize << " |" << std::fixed
                 << std::setprecision(3) << std::setw(18) << typeInfo.GetCompressionFactor() << " |" << std::setw(6)
-                << typeInfo.nPages << " " << std::endl;
+                << typeInfo.nPages << " \n";
       break;
    case ENTupleInspectorPrintFormat::kCSV:
-      output << "columnType,count,nElements,compressedSize,uncompressedSize,compressionFactor,nPages" << std::endl;
+      output << "columnType,count,nElements,compressedSize,uncompressedSize,compressionFactor,nPages\n";
       for (const auto &[colType, typeInfo] : colTypeInfo) {
          output << RColumnElementBase::GetColumnTypeName(colType) << "," << typeInfo.count << "," << typeInfo.nElems
                 << "," << typeInfo.compressedSize << "," << typeInfo.uncompressedSize << "," << std::fixed
-                << std::setprecision(3) << typeInfo.GetCompressionFactor() << "," << typeInfo.nPages << std::endl;
+                << std::setprecision(3) << typeInfo.GetCompressionFactor() << "," << typeInfo.nPages << '\n';
       }
       break;
    default: R__ASSERT(false && "Invalid print format");
