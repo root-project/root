d074c3a9b2dbfbe7d3299eee3445ea7f28ae0df5
Jakob Blomer <jblomer@cern.ch>
2026-05-27 17:14:36 +0200

    [ntuple] move RAuxiliaryProcessorField to anonymous namespace

--- tree/ntuple/src/RNTupleProcessor.cxx
+++ tree/ntuple/src/RNTupleProcessor.cxx
@@ -398,13 +398,13 @@ void ROOT::Experimental::RNTupleChainProcessor::PrintStructureImpl(std::ostream 
 
 //------------------------------------------------------------------------------
 
-namespace ROOT::Experimental::Internal {
+namespace {
 class RAuxiliaryProcessorField final : public ROOT::RRecordField {
 private:
    using RFieldBase::GenerateColumns;
    void GenerateColumns() final
    {
-      throw RException(R__FAIL("RAuxiliaryProcessorField fields must only be used for reading"));
+      throw ROOT::RException(R__FAIL("RAuxiliaryProcessorField fields must only be used for reading"));
    }
 
 public:
@@ -414,7 +414,7 @@ public:
       AttachItemFields(std::move(itemFields));
    }
 };
-} // namespace ROOT::Experimental::Internal
+} // anonymous namespace
 
 ROOT::Experimental::RNTupleJoinProcessor::RNTupleJoinProcessor(std::unique_ptr<RNTupleProcessor> primaryProcessor,
                                                                std::unique_ptr<RNTupleProcessor> auxProcessor,
