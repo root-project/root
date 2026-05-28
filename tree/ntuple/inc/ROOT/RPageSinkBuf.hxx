2105800216a4e1b019bf7a5b397c4d9402337d36
Jakob Blomer <jblomer@cern.ch>
2026-05-28 11:31:55 +0200

    [ntuple] mark some move constructors noexcept

--- tree/ntuple/inc/ROOT/RPageSinkBuf.hxx
+++ tree/ntuple/inc/ROOT/RPageSinkBuf.hxx
@@ -52,8 +52,8 @@ class RPageSinkBuf : public RPageSink {
       RColumnBuf() = default;
       RColumnBuf(const RColumnBuf&) = delete;
       RColumnBuf& operator=(const RColumnBuf&) = delete;
-      RColumnBuf(RColumnBuf&&) = default;
-      RColumnBuf& operator=(RColumnBuf&&) = default;
+      RColumnBuf(RColumnBuf &&) noexcept = default;
+      RColumnBuf &operator=(RColumnBuf &&) noexcept = default;
       ~RColumnBuf() { DropBufferedPages(); }
 
       /// Returns a reference to the newly buffered page. The reference remains
