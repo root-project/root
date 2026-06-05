a7763014177a7c926900236bfad7bd1bec046f9b
ferdymercury <ferdymercury@users.noreply.github.com>
2026-06-05 13:39:22 +0200

    [nfc][io] clarify how to check for health status of TFile (#22477)
    Co-authored-by: Philippe Canal <pcanal@fnal.gov>
    Co-authored-by: Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>

--- io/io/src/TFile.cxx
+++ io/io/src/TFile.cxx
@@ -362,6 +362,14 @@ TFile::TFile() : TDirectoryFile(), fCompress(ROOT::RCompressionSetting::EAlgorit
 /// ~~~{.cpp}
 ///   TFile *f = TFile::Open("tmpname.root?reproducible=fixedname","RECREATE","File title");
 /// ~~~
+///
+/// To check for the health status of a TFile and detect corruption, you can perform the following checks after opening it:
+/// ~~~{.cpp}
+///   std::unique_ptr<TFile> f{TFile::Open("name.root", "READ")};
+///   auto bad_input = (f == nullptr); // File could not be open, e.g. if input url was incorrect or incorrect permissions.
+///   auto bad_initalization = (f && f->IsZombie()); // something went wrong in the constructor, for example when TFile is corrupt
+///   auto bad_storage = (f && f->TestBit(TFile::kRecovered)); // The TFile had to run the recovery mechanism when opening the file; often due to the file being incorrectly closed.
+/// ~~~
 
 TFile::TFile(const char *fname1, Option_t *option, const char *ftitle, Int_t compress)
            : TDirectoryFile(), fCompress(compress), fUrl(fname1,kTRUE)
