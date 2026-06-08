07b91f068c86e3e4676ccc29ebad1a77ab2b2bc0
Lukas Johannes Breitwieser <lukas.breitwieser@gmail.com>
2026-06-08 14:16:39 +0200

    Apply ruff formatting suggestions

--- bindings/pyroot/pythonizations/test/th1_addfunction.py
+++ bindings/pyroot/pythonizations/test/th1_addfunction.py
@@ -2,6 +2,7 @@ import unittest
 
 import ROOT
 
+
 class TH1AddFunction(unittest.TestCase):
     def test_adding_a_function_does_not_segfault(self):
         """
@@ -10,6 +11,7 @@ class TH1AddFunction(unittest.TestCase):
         h = ROOT.TH1F("h", "h", 10, 0, 1)
         box = ROOT.TBox(0.1, 0.1, 0.9, 0.9)
         h.GetListOfFunctions().Add(box)
+
 
 if __name__ == "__main__":
     unittest.main()
