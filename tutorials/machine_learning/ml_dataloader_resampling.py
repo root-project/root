516685d5c9196884fa064138addb02b12196e126
Student2 <student2@mdessolevictus.cern.ch>
2026-06-10 12:45:06 +0200

    Fixed formatting with ruff

--- tutorials/machine_learning/ml_dataloader_resampling.py
+++ tutorials/machine_learning/ml_dataloader_resampling.py
@@ -9,10 +9,11 @@
 
 import ROOT
 import torch
+
 torch.manual_seed(42)
 
-# Create a skewed dataset with two classes, one of which is underrepresented. 
-# Here, we'll create two files, one with even numbers and one with odd numbers, 
+# Create an imbalanced dataset with two classes, one of which is underrepresented.
+# Here, we'll create two files, one with even numbers and one with odd numbers,
 # and then merge them to form a dataset with underrepresented odd numbers.
 ROOT.RDataFrame(100000).Define("b1", "(int) 2 * rdfentry_").Define("b2", "(int) b1%2").Snapshot("tree", "major.root")
 ROOT.RDataFrame(100).Define("b1", "(int) 2 * rdfentry_ + 1").Define("b2", "(int) b1%2").Snapshot("tree", "minor.root")
@@ -24,6 +25,7 @@ batches_in_memory = 10
 num_epochs = 10
 
 loss_fn = torch.nn.BCEWithLogitsLoss()
+
 
 def train_model(model, optimizer, dataloader):
     train, val = dataloader.train_test_split(test_size=0.2)
@@ -39,16 +41,17 @@ def train_model(model, optimizer, dataloader):
         with torch.no_grad():
             loss = loss_fn(model(X), y)
         losses.append(loss.item())
-    print(f"Validation Loss: {sum(losses)/len(losses)}")
+    print(f"Validation Loss: {sum(losses) / len(losses)}")
+
 
 # First, let's try to create a dataloader without resampling and see how it handles the underrepresented class.
 dl = ROOT.Experimental.ML.RDataLoader(
-                [df_major, df_minor],
-                batch_size=batch_size,
-                batches_in_memory=batches_in_memory,
-                target="b2",
-                set_seed=42,
-            )
+    [df_major, df_minor],
+    batch_size=batch_size,
+    batches_in_memory=batches_in_memory,
+    target="b2",
+    set_seed=42,
+)
 
 basic_model = torch.nn.Linear(1, 1)  # Simple linear model for binary classification
 basic_optimizer = torch.optim.Adam(basic_model.parameters())
@@ -60,14 +63,14 @@ train_model(basic_model, basic_optimizer, dl)
 # Strategy: more batches of the underrepresented class
 # Takes more time per epoch, but each epoch is more effective
 dl_oversampled = ROOT.Experimental.ML.RDataLoader(
-                [df_major, df_minor],
-                batch_size=batch_size,
-                batches_in_memory=batches_in_memory,
-                target="b2",
-                set_seed=42,
-                load_eager=True, # Must be enabled for resampling
-                sampling_type="oversampling", # Can also be "undersampling"
-            )
+    [df_major, df_minor],
+    batch_size=batch_size,
+    batches_in_memory=batches_in_memory,
+    target="b2",
+    set_seed=42,
+    load_eager=True,  # Must be enabled for resampling
+    sampling_type="oversampling",  # Can also be "undersampling"
+)
 
 oversampling_model = torch.nn.Linear(1, 1)
 oversampling_optimizer = torch.optim.Adam(oversampling_model.parameters())
@@ -76,5 +79,6 @@ print("Training with oversampling:")
 train_model(oversampling_model, oversampling_optimizer, dl_oversampled)
 
 import os
+
 os.remove("major.root")
 os.remove("minor.root")
