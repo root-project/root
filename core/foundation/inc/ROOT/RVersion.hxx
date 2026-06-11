33dbe67384135e954581a5992dba9a2d4fcbce76
Danilo Piparo <danilo.piparo@cern.ch>
2026-06-11 15:53:36 +0200

    Increase release patch number

--- core/foundation/inc/ROOT/RVersion.hxx
+++ core/foundation/inc/ROOT/RVersion.hxx
@@ -4,8 +4,8 @@
 /* Update on release: */
 #define ROOT_VERSION_MAJOR 6
 #define ROOT_VERSION_MINOR 38
-#define ROOT_VERSION_PATCH 5  // When changing the version number here, never add leading zeroes!
-#define ROOT_RELEASE_DATE "Mar 12 2026"
+#define ROOT_VERSION_PATCH 6  // When changing the version number here, never add leading zeroes!
+#define ROOT_RELEASE_DATE "Jun 12 2026"
 
 /* Don't change the lines below. */
 
