e0735ac0a6636b618b025f6d7622c8b5106c7100
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2026-06-17 10:32:43 +0200

    [df] Ruff changes in test file

--- roottest/python/distrdf/backends/test_all.py
+++ roottest/python/distrdf/backends/test_all.py
@@ -1,25 +1,25 @@
+import shlex
 import sys
 
 import pytest
 
-from check_backend import *
-from check_cloned_actions import *
-from check_distribute_cppcode import * 
-from check_definepersample import *
-from check_fromspec import *
-from check_explicit_api import *
-from check_friend_trees_alignment import *
-from check_friend_trees import *
-from check_histo_write import *
-from check_distribute_headers_sharedlibs_files import *
-from check_inv_mass import *
-from check_live_visualize import *
-from check_missing_values import *
-from check_reducer_merge import *
-from check_rungraphs import *
-from check_variations import *
-
-import shlex
+# Avoid errors from linters like "unable to detect undefined names"
+from check_backend import *  # noqa: F403
+from check_cloned_actions import *  # noqa: F403
+from check_definepersample import *  # noqa: F403
+from check_distribute_cppcode import *  # noqa: F403
+from check_distribute_headers_sharedlibs_files import *  # noqa: F403
+from check_explicit_api import *  # noqa: F403
+from check_friend_trees import *  # noqa: F403
+from check_friend_trees_alignment import *  # noqa: F403
+from check_fromspec import *  # noqa: F403
+from check_histo_write import *  # noqa: F403
+from check_inv_mass import *  # noqa: F403
+from check_live_visualize import *  # noqa: F403
+from check_missing_values import *  # noqa: F403
+from check_reducer_merge import *  # noqa: F403
+from check_rungraphs import *  # noqa: F403
+from check_variations import *  # noqa: F403
 
 if __name__ == "__main__":
     # The call to sys.exit is needed otherwise CTest would just ignore the
