a9d2dccfeebdff8ce6ecd0e8fc7fe59861fcd87c
matildamarjamaki <matilda.marjamaki@helsinki.fi>
2026-06-19 16:29:42 +0200

    Use join terminology in RNTupleProcessor test

--- tree/ntuple/test/ntuple_processor.cxx
+++ tree/ntuple/test/ntuple_processor.cxx
@@ -810,10 +810,10 @@ protected:
       "gh16805_rntuple_stepzero_1.root"
    };
 
-   const std::vector<std::string> fFriendFiles{
-      "gh16805_rntuple_friend_0.root",
-      "gh16805_rntuple_friend_1.root",
-      "gh16805_rntuple_friend_2.root"
+   const std::vector<std::string> fJoinFiles{
+      "gh16805_rntuple_join_0.root",
+      "gh16805_rntuple_join_1.root",
+      "gh16805_rntuple_join_2.root"
    };
 
    const std::string fStepOneFile = "gh16805_rntuple_stepone.root";
@@ -848,14 +848,14 @@ protected:
       }
    }
 
-   void WriteFriend(const std::string &fileName, int begin, int end)
+   void WriteJoin(const std::string &fileName, int begin, int end)
    {
       auto model = ROOT::RNTupleModel::Create();
 
-      auto br1 = model->MakeField<int>("friendBr1");
-      auto br2 = model->MakeField<int>("friendBr2");
+      auto br1 = model->MakeField<int>("joinBr1");
+      auto br2 = model->MakeField<int>("joinBr2");
 
-      auto writer = ROOT::RNTupleWriter::Recreate(std::move(model), "topLevelFriend", fileName);
+      auto writer = ROOT::RNTupleWriter::Recreate(std::move(model), "topLevelJoin", fileName);
 
       for (int i = begin; i < end; ++i) {
          *br1 = i;
@@ -869,9 +869,9 @@ protected:
       WriteStepZero(fStepZeroFiles[0], 0, 10);
       WriteStepZero(fStepZeroFiles[1], 10, 20);
 
-      WriteFriend(fFriendFiles[0], 200, 207);
-      WriteFriend(fFriendFiles[1], 207, 214);
-      WriteFriend(fFriendFiles[2], 214, 220);
+      WriteJoin(fJoinFiles[0], 200, 207);
+      WriteJoin(fJoinFiles[1], 207, 214);
+      WriteJoin(fJoinFiles[2], 214, 220);
 
       WriteStepOne(fStepOneFile, 100, 120);
    }
@@ -881,7 +881,7 @@ protected:
       for (const auto &f : fStepZeroFiles)
          std::remove(f.c_str());
 
-      for (const auto &f : fFriendFiles)
+      for (const auto &f : fJoinFiles)
          std::remove(f.c_str());
 
       std::remove(fStepOneFile.c_str());
@@ -899,10 +899,10 @@ TEST_F(GH16805ProcessorTest, JoinReading)
       {"stepzero", fStepZeroFiles[1]}
    };
 
-   std::vector<RNTupleOpenSpec> friendSpecs{
-      {"topLevelFriend", fFriendFiles[0]},
-      {"topLevelFriend", fFriendFiles[1]},
-      {"topLevelFriend", fFriendFiles[2]}
+   std::vector<RNTupleOpenSpec> joinSpecs{
+      {"topLevelJoin", fJoinFiles[0]},
+      {"topLevelJoin", fJoinFiles[1]},
+      {"topLevelJoin", fJoinFiles[2]}
    };
 
    auto stepOneProc =
@@ -911,26 +911,26 @@ TEST_F(GH16805ProcessorTest, JoinReading)
    auto stepZeroProc =
       RNTupleProcessor::CreateChain(stepZeroSpecs, "stepzero");
 
-   auto friendProc =
-      RNTupleProcessor::CreateChain(friendSpecs, "topLevelFriend");
+   auto joinProc =
+      RNTupleProcessor::CreateChain(joinSpecs, "topLevelJoin");
 
-   auto joinedWithFriend =
+   auto joinedWithJoin =
       RNTupleProcessor::CreateJoin(
          std::move(stepOneProc),
-         std::move(friendProc),
+         std::move(joinProc),
          {}
       );
 
    auto joinedAll =
       RNTupleProcessor::CreateJoin(
-         std::move(joinedWithFriend),
+         std::move(joinedWithJoin),
          std::move(stepZeroProc),
          {}
       );
 
    auto stepOneBr1 = joinedAll->RequestField<int>("stepOneBr1");
-   auto friendBr1 = joinedAll->RequestField<int>("topLevelFriend.friendBr1");
-   auto friendBr2 = joinedAll->RequestField<int>("topLevelFriend.friendBr2");
+   auto joinBr1 = joinedAll->RequestField<int>("topLevelJoin.joinBr1");
+   auto joinBr2 = joinedAll->RequestField<int>("topLevelJoin.joinBr2");
    auto stepZeroBr1 = joinedAll->RequestField<int>("stepzero.stepZeroBr1");
    auto stepZeroBr2 = joinedAll->RequestField<int>("stepzero.stepZeroBr2");
 
@@ -942,8 +942,8 @@ TEST_F(GH16805ProcessorTest, JoinReading)
       EXPECT_EQ(static_cast<int>(i), *stepZeroBr1);
       EXPECT_EQ(static_cast<int>(2 * i), *stepZeroBr2);
       EXPECT_EQ(static_cast<int>(100 + i), *stepOneBr1);
-      EXPECT_EQ(static_cast<int>(200 + i), *friendBr1);
-      EXPECT_EQ(static_cast<int>(2 * (200 + i)), *friendBr2);
+      EXPECT_EQ(static_cast<int>(200 + i), *joinBr1);
+      EXPECT_EQ(static_cast<int>(2 * (200 + i)), *joinBr2);
 
       ++i;
    }
