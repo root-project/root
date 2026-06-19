cd1face548058acb4766e88726287156e5553530
Jakob Blomer <jblomer@cern.ch>
2026-06-19 14:05:47 +0200

    [ci] add clang-tidy code analysis

--- .ci/tidy_script.sh
+++ .ci/tidy_script.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-CLANG_TIDY_CHECKS='-*'
-if [[ $TOOL == clang-tidy-analyzer ]]; then
-  CLANG_TIDY_CHECKS+=',clang-analyzer-*,-clang-analyzer-alpha*,bugprone*'
-elif [[ $TOOL == clang-tidy-modernize ]]; then
-  CLANG_TIDY_CHECKS+=',modernize*'
+echo "Running $(realpath $0) from $PWD"
+
+WORKSPACE="${GITHUB_WORKSPACE:-${PWD}}"
+echo "Setting WORKSPACE to $WORKSPACE"
+
+SRC_DIR="${WORKSPACE}/ROOT-CI/src"
+BUILD_DIR="${WORKSPACE}/ROOT-CI/build"
+
+mkdir -v -p "$SRC_DIR" "$BUILD_DIR"
+
+cmake -B "$BUILD_DIR" -S $SRC_DIR \
+  -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
+  -Dminimal=on -Dasserts=on
+
+clang-tidy --version
+
+HAVE_RUN=0
+EXIT_CODE=0
+for f in $(find "$SRC_DIR" -mindepth 2 -not -path '*/interpreter/*' -not -path '*/roofit/*' -name .clang-tidy); do
+   set -x
+   run-clang-tidy -use-color 1 -j $(nproc) -p "$BUILD_DIR" -config-file "$f" "$(dirname $f)/" || EXIT_CODE=1
+   set +x
+   HAVE_RUN=1
+done
+
+if [ $HAVE_RUN -eq 0 ]; then
+   echo "clang-tidy ran nowhere; that doesn't seem right."
+   exit 1
 fi
 
-echo "Running clang-tidy only against the changes in branch $TRAVIS_BRANCH."
-
-cd ../root/
-
-# Workaround for travis issue: travis-ci/travis-ci#6069
-git remote set-branches --add origin master
-git fetch
-
-# clang-tidy-diff.py not installed on travis
-wget https://raw.githubusercontent.com/llvm-mirror/clang-tools-extra/release_50/clang-tidy/tool/clang-tidy-diff.py
-
-RESULT_OUTPUT="$(git diff -U0 origin/master | python clang-tidy-diff.py -p1 -clang-tidy-binary $(which clang-tidy) \
-                 -checks=$CLANG_TIDY_CHECKS)"
-if [[ $? -eq 0 ]]; then
-  echo "$TOOL passed."
-  exit 0
-else
-  echo "To reproduce it locally please run"
-  echo -e "\tgit checkout $TRAVIS_BRANCH"
-  echo -e "Command: git diff -U0 $TRAVIS_BRANCH..origin/master | clang-tidy-diff.py -p1 -clang-tidy-binary \$(which clang-tidy) -checks=$CLANG_TIDY_CHECKS"
-  echo "$RESULT_OUTPUT"
-  exit 1
-fi
+exit $EXIT_CODE
