288cc078b1783ef7bda58240386c08d8ee675361
Sergey Linev <S.Linev@gsi.de>
2026-06-26 08:13:24 +0200

    [webgui] remove experimental notice from comments

--- gui/cefdisplay/src/gui_handler.cxx
+++ gui/cefdisplay/src/gui_handler.cxx
@@ -1,7 +1,6 @@
 /// \file gui_handler.cxx
 // Author: Sergey Linev <S.Linev@gsi.de>
 // Date: 2017-06-29
-// Warning: This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
 
 /*************************************************************************
  * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
--- gui/webdisplay/src/RWebWindowsManager.cxx
+++ gui/webdisplay/src/RWebWindowsManager.cxx
@@ -1,6 +1,5 @@
 // Author: Sergey Linev <s.linev@gsi.de>
 // Date: 2017-10-16
-// Warning: This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback is welcome!
 
 /*************************************************************************
  * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
