5a31e4728af30aa056dec72c90c9478cf78964f0
silverweed <giacomo.parolini@cern.ch>
2026-06-29 12:04:58 +0200

    [core] Add comment about `--` in TApplication help msg

--- core/base/src/TApplication.cxx
+++ core/base/src/TApplication.cxx
@@ -55,7 +55,7 @@ TApplication (see TRint).
 constexpr static const char *kCommandLineOptionsHelp = R"RAW(
 usage: root [-b] [-x] [-e] [-n] [-t] [-q] [-l] [-config] [-h|--help] [--version]
             [--notebook] [--web=<type|off>] [dir] [data1.root...dataN.root] [file1.C...fileN.C]
-            [file1_C.so...fileN_C.so] [anyfile1..anyfileN]
+            [file1_C.so...fileN_C.so] [anyfile1..anyfileN] [-- [macro args]]
 
 
 root is an interactive interpreter of C++ code using Cling and the ROOT framework.
@@ -86,7 +86,8 @@ OPTIONS:
   [file1_C.so...fileN_C.so]            Load and execute file1_C.so ... fileN_C.so (or .dll if on Windows)
                                        They should be already-compiled ROOT macros (shared libraries) or:
                                        regular user shared libraries e.g. userlib.so with a function userlib(args)
-  [anyfile1..anyfileN]                 All other arguments pointing to existing files will be checked to see if they are ROOT Files (checking the MIME type inside the file) and if they are not they will be handled as a  ROOT macro file
+  [anyfile1..anyfileN]                 All other arguments pointing to existing files will be checked to see if they are ROOT Files (checking the MIME type inside the file) and if they are not they will be handled as a ROOT macro file
+  [-- [macro args]]                    Any arguments after a `--` will be treated as arguments to the last macro passed as a positional argument (e.g. `root myMacro.C -- 1 2 3` will be equivalent to `root "myMacro.C(1,2,3)"`)
 )RAW";
 
 TApplication *gApplication = nullptr;
