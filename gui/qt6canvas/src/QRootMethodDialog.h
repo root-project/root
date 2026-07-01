3406fadcd480bc7f9b7b41d95d53b3eba7c29b81
Sergey Linev <S.Linev@gsi.de>
2026-07-01 09:55:54 +0200

    [qt6canv] provide mouse events handling
    Like press, move, release mouse button.
    On the right mouse button actiate context menu.
    For methods with arguments provide special dialog.
    
    Code partially copied from Go4 project
    and originally was developed in 2002
    by Denis Bertini and improved later by Sergey Linev

---
+++ gui/qt6canvas/src/QRootMethodDialog.h
@@ -0,0 +1,48 @@
+// Original code derived from QRootDialog
+// from https://go4.gsi.de project
+// Author : Denis Bertini 01.11.2000
+
+// Author: Sergey Linev, GSI  30/06/2026
+
+/*************************************************************************
+ * Copyright (C) 1995-2026, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+
+#ifndef QRootMethodDialog_h
+#define QRootMethodDialog_h
+
+#include <QtCore/QVector>
+#include <QDialog>
+
+class QLineEdit;
+class QVBoxLayout;
+class TObject;
+class TMethod;
+
+class QRootMethodDialog: public QDialog {
+   Q_OBJECT
+
+   public:
+      QRootMethodDialog();
+
+      void addArg(const char *argname, const char *value, const char *type);
+
+      QString getArg(int n);
+
+      void methodDialog(TObject *object, TMethod* method);
+
+   signals:
+      void MenuCommandExecuted(TObject *obj, const char *method_name);
+
+   protected:
+      QVBoxLayout *argLayout{nullptr};
+
+      QVector<QLineEdit*> fArgs;
+};
+
+#endif
