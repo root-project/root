6009e51438784327c33db8770d095ceaeb0118e3
Sergey Linev <S.Linev@gsi.de>
2026-07-02 14:23:49 +0200

    [qt6canv] use list of TContextMenuItem from TClass
    This is default place where all normal or custom items
    for the class are collected. By large copy code from
    TRootContextMenu.

--- gui/qt6canvas/src/QRootContextMenu.cpp
+++ gui/qt6canvas/src/QRootContextMenu.cpp
@@ -26,7 +26,9 @@ The picture below shows a canvas with a pop-up menu.
 #include "TContextMenu.h"
 #include "TCanvas.h"
 #include "TMethod.h"
-
+#include "TDataMember.h"
+#include "TToggle.h"
+#include "TClassMenuItem.h"
 
 #include "QRootMethodDialog.h"
 #include "QPaintWidget.h"
@@ -35,6 +37,12 @@ The picture below shows a canvas with a pop-up menu.
 #include <QtCore/QSignalMapper>
 #include <QMenu>
 #include <QAction>
+
+enum EContextMenu {
+   kToggleStart       = 1000, // first id of toggle menu items
+   kToggleListStart   = 2000, // first id of toggle list menu items
+   kUserFunctionStart = 3000  // first id of user added functions/methods, etc...
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Create context menu.
@@ -51,10 +59,7 @@ QRootContextMenu::QRootContextMenu(TContextMenu *c, const char *)
 QRootContextMenu::~QRootContextMenu()
 {
    gROOT->GetListOfCleanups()->Remove(this);
-
-   fMenuObj = nullptr;
-   delete fMenuMethods;
-   fMenuMethods = nullptr;
+   fTrash.Delete();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -65,11 +70,13 @@ void QRootContextMenu::DisplayPopup(Int_t x, Int_t y)
    // add menu items to popup menu
    // CreateMenu(fContextMenu->GetSelectedObject());
 
-   fMenuObj = nullptr;
-   delete fMenuMethods;
-   fMenuMethods = nullptr;
+   auto object = fContextMenu->GetSelectedObject();
+   if (!object)
+      return;
 
-   printf("Start menu for %p\n", fContextMenu->GetSelectedObject());
+   fCustomArg.clear();
+   fTrash.Delete();
+
 
    auto canv = dynamic_cast<TCanvas *>(fContextMenu->GetSelectedCanvas());
    auto canvimp = dynamic_cast<TQt6Canvas *>(canv->GetCanvasImp());
@@ -83,31 +90,148 @@ void QRootContextMenu::DisplayPopup(Int_t x, Int_t y)
    QObject::connect(&map, &QSignalMapper::mappedInt,
                     this, &QRootContextMenu::executeMenu);
 
-   fMousePosX = x;
-   fMousePosY = y;
-
-   fMenuObj = fContextMenu->GetSelectedObject();
-   fMenuMethods = new TList;
-   TClass *cl = fMenuObj->IsA();
-   int curId = -1;
-
-   QString buffer = TString::Format("%s::%s", cl->GetName(), fMenuObj->GetName()).Data();
-   addMenuAction(&menu, &map, buffer, curId++);
-
-   cl->GetMenuItems(fMenuMethods);
+   // Add a title
+   QString buffer = fContextMenu->CreatePopupTitle(object);
+   addMenuAction(&menu, &map, buffer, -1, nullptr);
    menu.addSeparator();
+   bool last_separ = true;
 
-   TIter iter(fMenuMethods);
-   while (auto method = dynamic_cast<TMethod*>(iter())) {
-      buffer = method->GetName();
-      addMenuAction(&menu, &map, buffer, curId++);
+
+
+   // addMenuAction(&menu, &map, buffer, curId++);
+
+   int entry = 0, toggle = kToggleStart, togglelist = kToggleListStart;
+   int userfunction = kUserFunctionStart;
+
+   // Get list of menu items from the selected object's class
+   TList *menuItemList = object->IsA()->GetMenuList();
+
+   TIter nextItem(menuItemList);
+
+   while (auto menuItem = (TClassMenuItem*) nextItem()) {
+      switch (menuItem->GetType()) {
+         case TClassMenuItem::kPopupSeparator: {
+            if (!last_separ)
+               menu.addSeparator();
+            last_separ = true;
+            break;
+         }
+         case TClassMenuItem::kPopupStandardList: {
+            // Standard list of class methods. Rebuild from scratch.
+            // Get linked list of objects menu items (i.e. member functions
+            // with the token *MENU in their comment fields.
+            TList *methodList = new TList;
+            object->IsA()->GetMenuItems(methodList);
+
+            TMethod *method;
+            TClass  *classPtr = nullptr;
+            TIter next(methodList);
+            Bool_t needSep = kFALSE;
+
+            while ((method = (TMethod*) next())) {
+               if (classPtr != method->GetClass()) {
+                  needSep = kTRUE;
+                  classPtr = method->GetClass();
+               }
+
+               EMenuItemKind menuKind = method->IsMenuItem();
+               TString last_component;
+
+               switch (menuKind) {
+                  case kMenuDialog:
+                     // search for arguments to the MENU statement
+                     if (needSep) {
+                        menu.addSeparator();
+                        needSep = kFALSE;
+                     }
+                     addMenuAction(&menu, &map, method->GetName(), entry++, method);
+                     break;
+                  case kMenuSubMenu:
+                     if (auto m = method->FindDataMember()) {
+                        if (needSep) {
+                           menu.addSeparator();
+                           needSep = kFALSE;
+                        }
+
+                        if (m->GetterMethod()) {
+
+                           QMenu *r = menu.addMenu(method->GetName());
+                           TIter nxt(m->GetOptions());
+                           while (auto it = (TOptionListItem*) nxt()) {
+                              const char *name = it->fOptName;
+                              Long_t val = it->fValue;
+
+                              TToggle *t = new TToggle;
+                              t->SetToggledObject(object, method);
+                              t->SetOnValue(val);
+                              fTrash.Add(t);
+
+                              auto act = addMenuAction(r, &map, name, togglelist++, t);
+                              act->setCheckable(true);
+                              if (t->GetState())
+                                 act->setChecked(true);
+                           }
+                        } else {
+                           addMenuAction(&menu, &map, method->GetName(), entry++, method);
+                        }
+                     }
+                     break;
+
+                  case kMenuToggle: {
+                     if (needSep) {
+                        menu.addSeparator();
+                        needSep = kFALSE;
+                     }
+
+                     TToggle *t = new TToggle;
+                     t->SetToggledObject(object, method);
+                     t->SetOnValue(1);
+                     fTrash.Add(t);
+
+                     auto act = addMenuAction(&menu, &map, method->GetName(), toggle++, t);
+                     act->setCheckable(true);
+                     if (t->GetState())
+                        act->setChecked(true);
+                     break;
+                  }
+                  default:
+                     break;
+               }
+            }
+            delete methodList;
+         }
+         break;
+         case TClassMenuItem::kPopupUserFunction: {
+            const char* menuItemTitle = menuItem->GetTitle();
+            if (menuItem->IsToggle()) {
+               TMethod* method = object->IsA()->GetMethodWithPrototype(menuItem->GetFunctionName(),menuItem->GetArgs());
+               if (method) {
+                  TToggle *t = new TToggle;
+                  t->SetToggledObject(object, method);
+                  t->SetOnValue(1);
+                  fTrash.Add(t);
+
+                  if (strlen(menuItemTitle)==0)
+                     menuItemTitle = method->GetName();
+                  auto act = addMenuAction(&menu, &map, menuItemTitle, toggle++, t);
+                  act->setCheckable(true);
+                  if (t->GetState())
+                     act->setChecked(true);
+               }
+            } else {
+               if (strlen(menuItemTitle)==0)
+                  menuItemTitle = menuItem->GetFunctionName();
+               addMenuAction(&menu, &map, menuItemTitle, userfunction++, menuItem);
+            }
+            break;
+         }
+
+         default:
+            break;
+      }
    }
 
-   if (menu.exec(screenPos + QPoint(x, y)) == nullptr) {
-      fMenuObj = nullptr;
-      delete fMenuMethods;
-      fMenuMethods = nullptr;
-   }
+   menu.exec(screenPos + QPoint(x, y));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -145,7 +269,7 @@ void QRootContextMenu::RecursiveRemove(TObject *obj)
    }
 }
 
-QAction* QRootContextMenu::addMenuAction(QMenu* menu, QSignalMapper* map, const QString& text, int id)
+QAction* QRootContextMenu::addMenuAction(QMenu* menu, QSignalMapper* map, const QString& text, int id, void *arg)
 {
    bool enabled = true;
 
@@ -164,59 +288,47 @@ QAction* QRootContextMenu::addMenuAction(QMenu* menu, QSignalMapper* map, const 
    menu->addAction(act);
    map->setMapping(act, id);
 
+   fCustomArg[id] = arg;
+
    return act;
 }
 
 void QRootContextMenu::executeMenu(int id)
 {
-   QString text;
-   bool ok = false;
-   if (id >= 0) {
+   if (id < 0)
+      return;
+   void *ud = fCustomArg[id];
 
-      // save global to Pad before calling TObject::Execute()
-
-      TVirtualPad *psave = gROOT->GetSelectedPad();
-
-      auto canv = dynamic_cast<TCanvas *>(fContextMenu->GetSelectedCanvas());
-
-      TMethod *method = (TMethod *) fMenuMethods->At(id);
-
-      /// test: do this in any case!
-      canv->HandleInput(kButton3Up, gPad->XtoAbsPixel(fMousePosX), gPad->YtoAbsPixel(fMousePosY));
-
-      // change current dir that all new histograms appear here
-      gROOT->cd();
-
-      if (method->GetListOfMethodArgs()->First()) {
-         QRootMethodDialog dlg;
-
-         dlg.methodDialog(fMenuObj, method);
+   if (ud) {
+      // retrieve the highlighted function
+      TFunction *function = nullptr;
+      if (id < kToggleStart) {
+         TMethod *m = (TMethod *)ud;
+         function = (TFunction *)m;
+      } else if (id >= kToggleStart && id < kUserFunctionStart) {
+         TToggle *t = (TToggle *)ud;
+         TMethodCall *mc = (TMethodCall *)t->GetSetter();
+         function = (TFunction *)mc->GetMethod();
       } else {
-         gROOT->SetFromPopUp(kTRUE);
-         fMenuObj->Execute(method->GetName(), "");
-
-         if (fMenuObj->TestBit(TObject::kNotDeleted)) {
-            // emit MenuCommandExecuted(fMenuObj, method->GetName());
-         } else {
-            fMenuObj = nullptr;
-         }
-
+         TClassMenuItem *mi = (TClassMenuItem *)ud;
+         function = gROOT->GetGlobalFunctionWithPrototype(mi->GetFunctionName());
       }
-
-      canv->GetPadSave()->Update();
-      canv->GetPadSave()->Modified();
-
-      gROOT->SetSelectedPad(psave);
-
-      gROOT->GetSelectedPad()->Update();
-      gROOT->GetSelectedPad()->Modified();
-
-      canv->Modified();
-      canv->ForceUpdate();
-      gROOT->SetFromPopUp(kFALSE);
+      if (function)
+         fContextMenu->SetMethod(function);
    }
 
-   fMenuObj = nullptr;
-   delete fMenuMethods;
-   fMenuMethods = nullptr;
+   if (id < kToggleStart) {
+      TMethod *m = (TMethod *) ud;
+      fContextMenu->Action(m);
+   } else if (id >= kToggleStart && id < kToggleListStart) {
+      TToggle *t = (TToggle *) ud;
+      fContextMenu->Action(t);
+   } else if (id >= kToggleListStart && id < kUserFunctionStart) {
+      TToggle *t = (TToggle *) ud;
+      if (t->GetState() == 0)
+         t->SetState(1);
+   } else {
+      TClassMenuItem *mi = (TClassMenuItem*)ud;
+      fContextMenu->Action(mi);
+   }
 }
