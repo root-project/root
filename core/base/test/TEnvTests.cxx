ccb07e92cadd28f59190677bc4a0ef79acd31c6a
Jakob Blomer <jblomer@cern.ch>
2026-07-08 13:41:10 +0200

    [core] make test fixture for TEnv unit tests

--- core/base/test/TEnvTests.cxx
+++ core/base/test/TEnvTests.cxx
@@ -7,43 +7,68 @@
 
 #include <string>
 
-TEST(TEnv, ROOTENV_USER_PATH)
-{
-   auto uuid = TUUID::UUIDv4();
+class TEnvTest : public testing::Test {
+protected:
+   TString fRcName;
+   TString fUserDir;
+   TString fUserConfig;
+   TString fLocalDir;
+   TString fLocalConfig;
 
-   // Create directories structure <UUID>/local with user-level config in <UUID> and local-level config
-   // in <UUID>/local
-   const TString rcname = "testenv";
-   const TString userDir = uuid.AsString();
-   TString userConfig = rcname;
-   gSystem->PrependPathName(userDir.Data(), userConfig);
-   const TString userBak = userConfig + ".bak";
-   TString localDir = "local";
-   gSystem->PrependPathName(userDir, localDir);
-   TString localConfig = rcname;
-   gSystem->PrependPathName(localDir.Data(), localConfig);
-   TString homeConfig = rcname;
+   void SetUp() override
+   {
+      fRcName = "testenv";
+
+      // Create directories structure <UUID>/local with user-level config in <UUID> and local-level config
+      // in <UUID>/local
+
+      const auto uuid = TUUID::UUIDv4();
+
+      fUserDir = uuid.AsString();
+      fUserConfig = fRcName;
+      gSystem->PrependPathName(fUserDir.Data(), fUserConfig);
+
+      fLocalDir = "local";
+      gSystem->PrependPathName(fUserDir, fLocalDir);
+      fLocalConfig = fRcName;
+      gSystem->PrependPathName(fLocalDir.Data(), fLocalConfig);
+
+      fLocalDir.ReplaceAll("\\", "\\\\");
+      ASSERT_EQ(0, gSystem->mkdir(fLocalDir.Data(), /*recursive=*/true));
+
+      gSystem->Setenv("ROOTENV_USER_PATH", fUserDir.Data());
+   }
+
+   void TearDown() override
+   {
+      gSystem->Unsetenv("ROOTENV_USER_PATH");
+
+      gSystem->Unlink((fLocalConfig + ".bak").Data());
+      gSystem->Unlink(fLocalConfig.Data());
+      gSystem->Unlink(fLocalDir.Data());
+      gSystem->Unlink((fUserConfig + ".bak").Data());
+      gSystem->Unlink(fUserConfig.Data());
+      gSystem->Unlink(fUserDir.Data());
+   }
+};
+
+TEST_F(TEnvTest, ROOTENV_USER_PATH)
+{
+   TString homeConfig = fRcName;
    gSystem->PrependPathName(gSystem->HomeDirectory(), homeConfig);
 
-   localDir.ReplaceAll("\\", "\\\\");
-   ASSERT_EQ(0, gSystem->mkdir(localDir.Data(), /*recursive=*/true));
+   // Initially, neither the local nor the user config file exist
+   EXPECT_TRUE(gSystem->AccessPathName(fLocalConfig.Data()));
+   EXPECT_TRUE(gSystem->AccessPathName(fUserConfig.Data()));
 
-   gSystem->Setenv("ROOTENV_USER_PATH", userDir.Data());
-   EXPECT_TRUE(gSystem->AccessPathName(localConfig.Data()));
-   EXPECT_TRUE(gSystem->AccessPathName(userConfig.Data()));
-
-   TEnv env("testenv");
+   TEnv env(fRcName);
+   EXPECT_FALSE(env.IsLocalLevelDisabled());
    env.SetValue("EnvRec");
    EXPECT_EQ(1, env.GetValue("EnvRec", 0));
    env.SaveLevel(kEnvUser);
 
-   EXPECT_TRUE(gSystem->AccessPathName(localConfig.Data()));
-   EXPECT_FALSE(gSystem->AccessPathName(userConfig.Data()));
+   // Now, the user config file should have been created in the custom directory
+   EXPECT_TRUE(gSystem->AccessPathName(fLocalConfig.Data()));
+   EXPECT_FALSE(gSystem->AccessPathName(fUserConfig.Data()));
    EXPECT_TRUE(gSystem->AccessPathName(homeConfig.Data()));
-
-   EXPECT_EQ(0, gSystem->Unlink(localDir.Data()));
-   EXPECT_EQ(0, gSystem->Unlink(userConfig.Data()));
-   EXPECT_EQ(0, gSystem->Unlink(userBak.Data()));
-   EXPECT_EQ(0, gSystem->Unlink(userDir.Data()));
-   gSystem->Unsetenv("ROOTENV_USER_PATH");
 }
