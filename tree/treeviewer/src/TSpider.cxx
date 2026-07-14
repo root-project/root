1109e3ce82acf5c2eabe044d5b64fc5718e2d9ae
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-07-14 15:12:07 +0200

    [NFC] Remove dead mentions of TEventList.

--- tree/treeviewer/src/TSpider.cxx
+++ tree/treeviewer/src/TSpider.cxx
@@ -730,8 +730,6 @@ Long64_t TSpider::GetEntriesToProcess(Long64_t firstentry, Long64_t nentries) co
       lastentry  = fTree->GetEntriesFriend() - 1;
       nentries   = lastentry - firstentry + 1;
    }
-   //TEventList *elist = fTree->GetEventList();
-   //if (elist && elist->GetN() < nentries) nentries = elist->GetN();
    TEntryList *elist = fTree->GetEntryList();
    if (elist && elist->GetN() < nentries) nentries = elist->GetN();
    return nentries;
