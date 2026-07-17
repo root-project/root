e29aecadcca4a3918663cf20a0dcd41e406a8c33
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2026-07-17 15:49:43 +0200

    [graf3d] Move TGeometry (Geant 3 shapes) into their own library.

--- graf3d/g3d/inc/LinkDef.h
+++ graf3d/g3d/inc/LinkDef.h
@@ -15,40 +15,12 @@
 #pragma link off all functions;
 
 #pragma link C++ enum EHelixRangeType;
-
-#pragma link C++ global gGeometry;
-
 #pragma link C++ class TAxis3D+;
-#pragma link C++ class TBRIK+;
-#pragma link C++ class TCONE+;
-#pragma link C++ class TCONS+;
-#pragma link C++ class TCTUB-;
-#pragma link C++ class TELTU+;
-#pragma link C++ class TGTRA+;
-#pragma link C++ class TGeometry-;
-#pragma link C++ class THelix-;
-#pragma link C++ class THYPE+;
-#pragma link C++ class TMaterial-;
-#pragma link C++ class TMixture-;
-#pragma link C++ class TNode-;
-#pragma link C++ class TNodeDiv+;
-#pragma link C++ class TPARA+;
-#pragma link C++ class TPCON-;
-#pragma link C++ class TPGON+;
-#pragma link C++ class TPolyLine3D-;
-#pragma link C++ class TPolyMarker3D-;
-#pragma link C++ class TPointSet3D-;
-#pragma link C++ class TRotMatrix-;
-#pragma link C++ class TShape-;
-#pragma link C++ class TSPHE-;
-#pragma link C++ class TTRAP+;
-#pragma link C++ class TTRD1+;
-#pragma link C++ class TTRD2+;
-#pragma link C++ class TTUBE-;
-#pragma link C++ class TTUBS+;
 #pragma link C++ class TMarker3DBox-;
 #pragma link C++ class TPoints3DABC;
-#pragma link C++ class TXTRU+;
+#pragma link C++ class TPointSet3D-;
+#pragma link C++ class TPolyLine3D-;
+#pragma link C++ class TPolyMarker3D-;
 #pragma link C++ class TView3D-;
 
 #endif
