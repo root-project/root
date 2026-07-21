9dcfe3b6daadb6d61ee8de1fdeab6517616de1b9
silverweed <giacomo.parolini@cern.ch>
2026-07-21 17:29:48 +0200

    [net] Restore TGrid* classes
    They are critical for the ALICE experiment so they cannot be removed
    at this time.

--- net/net/inc/LinkDef.h
+++ net/net/inc/LinkDef.h
@@ -14,6 +14,9 @@
 #pragma link C++ enum ESockOptions;
 #pragma link C++ enum ESendRecvOptions;
 
+#pragma link C++ global ROOT::Deprecated::gGrid;
+#pragma link C++ global gGridJobStatusList;
+
 #pragma link C++ class TServerSocket;
 #pragma link C++ class TSocket;
 #pragma link C++ class TPServerSocket;
@@ -27,10 +30,24 @@
 #pragma link C++ class TSQLTableInfo;
 #pragma link C++ class TSQLColumnInfo;
 #pragma link C++ class TSQLMonitoringWriter;
+#pragma link C++ class ROOT::Deprecated::TGrid;
+#pragma link C++ class ROOT::Deprecated::TGridResult+;
+#pragma link C++ class ROOT::Deprecated::TGridJDL+;
+#pragma link C++ class ROOT::Deprecated::TGridJob+;
+#pragma link C++ class ROOT::Deprecated::TGridJobStatus+;
+#pragma link C++ class ROOT::Deprecated::TGridJobStatusList+;
+#pragma link C++ class ROOT::Deprecated::TGridCollection+;
 #pragma link C++ class TFileStager;
 #pragma link C++ class TApplicationRemote;
 #pragma link C++ class TApplicationServer;
 #pragma link C++ class TUDPSocket;
 #pragma link C++ class TParallelMergingFile+;
+
+#pragma read sourceClass="TGridCollection" version="[-1]" targetClass="ROOT::Deprecated::TGridCollection"
+#pragma read sourceClass="TGridJDL" version="[-1]" targetClass="ROOT::Deprecated::TGridJDL"
+#pragma read sourceClass="TGridJob" version="[-1]" targetClass="ROOT::Deprecated::TGridJob"
+#pragma read sourceClass="TGridJobStatus" version="[-1]" targetClass="ROOT::Deprecated::TGridJobStatus"
+#pragma read sourceClass="TGridJobStatusList" version="[-1]" targetClass="ROOT::Deprecated::TGridJobStatusList"
+#pragma read sourceClass="TGridResult" version="[-1]" targetClass="ROOT::Deprecated::TGridResult"
 
 #endif
