29a6625031e5d6b5e612ab7305d937e91c5f9def
ferdymercury <ferdymercury@users.noreply.github.com>
2026-08-06 15:22:10 +0200

    [clib] mv to builtins folder since it's hard-coded external
    This is all C code that was copy-pasted from external packages in 1990x and 200x. It was then slightly adapted for ROOT but most of the structure is the same.
    
    Here, the code is put into the builtins directory. This is done for several reasons:
    
    - consistency with libafterimage, mathtext, glad: they have been all forked from upstream and fixed / adapted for ROOT. Likewise for core/clib, put them in builtins directory to have a better overview of all "dependencies" or borrowed things with upstream counterparts.
    - makes it clearer to deal with licensing and copyright notices by better separating source of the code
    - builtins folder gives kind of more recognition/overview to original authors (before adaptions and modifications)
    - system packagers (eg Debian) might complain less by better clarifying externals vs internals
    - when a bug is found in those builtins, it easens the opportunity to crosscheck with upstream whether it was fixed there
    - it allows in the future a more easy drop-in replacement with a more up-to-date library rather than a hard-coded copy paste
    - Less annoying surface for clang-tidy etc, things from builtins can be filtered out.
    
    [macosx] missing include path

--- documentation/doxygen/makeinput.sh
+++ documentation/doxygen/makeinput.sh
@@ -74,7 +74,6 @@ echo "        ../../bindings/tpython/          \\" >> Doxyfile_INPUT
 echo "        ../../bindings/pyroot/           \\" >> Doxyfile_INPUT
 echo "        ../../bindings/pyroot/pythonizations/python/ROOT/_pythonization/          \\" >> Doxyfile_INPUT
 
-# echo "        ../../core/clib/                 \\" >> Doxyfile_INPUT
 # echo "        ../../core/lzma/                 \\" >> Doxyfile_INPUT
 # echo "        ../../core/newdelete/            \\" >> Doxyfile_INPUT
 # echo "        ../../core/textinput/            \\" >> Doxyfile_INPUT
