63a0e5cca44e3a2a904dc72a61d17b4717ad9981
Arjun Varma <arj25835@gmail.com>
2026-08-15 15:52:49 +0530

    [df] Introduce Median operation
    This commit introduces the Median lazy action, which computes the median of the
    values in the input column.
    
    Median always returns an RResultPtr<double> irrespective of the input column type.
    
    If the input column is of a type which does not make sense to compute the median
    on, the action will throw a std::runtime_error during execution when the input
    value is tried to be coerced into a double.
    
    The current implementation works for local RDataFrame in single- and
    multi-threaded mode. It stores all column values in a buffer and computes the
    exact median at the end of processing.

--- tree/dataframe/inc/ROOT/RDF/InterfaceUtils.hxx
+++ tree/dataframe/inc/ROOT/RDF/InterfaceUtils.hxx
@@ -103,6 +103,7 @@ struct Sum{};
 struct Mean{};
 struct Fill{};
 struct StdDev{};
+struct Median{};
 struct Display{};
 struct Snapshot{};
 struct Book{};
@@ -297,6 +298,17 @@ BuildAction(const ColumnNames_t &bl, const std::shared_ptr<double> &stdDeviation
    using Helper_t = StdDevHelper;
    using Action_t = RAction<Helper_t, PrevNodeType, TTraits::TypeList<ColType>>;
    return std::make_unique<Action_t>(Helper_t(stdDeviationV, nSlots), bl, prevNode, colRegister);
+}
+
+// Median action
+template <typename ColType, typename PrevNodeType>
+std::unique_ptr<RActionBase>
+BuildAction(const ColumnNames_t &bl, const std::shared_ptr<double> &meanV, const unsigned int nSlots,
+            std::shared_ptr<PrevNodeType> prevNode, ActionTags::Median, const RColumnRegister &colRegister)
+{
+   using Helper_t = MedianHelper;
+   using Action_t = RAction<Helper_t, PrevNodeType, TTraits::TypeList<ColType>>;
+   return std::make_unique<Action_t>(Helper_t(meanV, nSlots), bl, std::move(prevNode), colRegister);
 }
 
 using displayHelperArgs_t = std::pair<size_t, std::shared_ptr<ROOT::RDF::RDisplay>>;
