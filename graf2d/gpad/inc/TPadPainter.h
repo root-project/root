678fc457d32c73dfd67fcec9a6d017e7d1ab9130
Sergey Linev <S.Linev@gsi.de>
2026-08-21 13:29:10 +0200

    [padpainter] Implement text drawing in TPadPainterBase
    Text metrics with TTF already was implemented there,
    now also text drawing via gVirtualX can be done already in TPadPainterBase
    
    In case of TTF glyph drawing is implemented in the TPadPainter
    
    In case of none-TTF gVirtualX used directly if window context is assigned

--- graf2d/gpad/inc/TPadPainter.h
+++ graf2d/gpad/inc/TPadPainter.h
@@ -76,10 +76,7 @@ public:
    void     DrawPolyMarker(Int_t n, const Double_t *x, const Double_t *y) override;
    void     DrawPolyMarker(Int_t n, const Float_t *x, const Float_t *y) override;
 
-   void     DrawText(Double_t x, Double_t y, const char *text, ETextMode mode) override;
-   void     DrawText(Double_t x, Double_t y, const wchar_t *text, ETextMode mode) override;
-   void     DrawTextNDC(Double_t u, Double_t v, const char *text, ETextMode mode) override;
-   void     DrawTextNDC(Double_t u, Double_t v, const wchar_t *text, ETextMode mode) override;
+   void     DrawTTFglyphs(Int_t x, Int_t y, TTFhandle &ttf, ETextMode mode) override;
 
    //jpg, png, bmp, gif output.
    void     SaveImage(TVirtualPad *pad, const char *fileName, Int_t type) const override;
