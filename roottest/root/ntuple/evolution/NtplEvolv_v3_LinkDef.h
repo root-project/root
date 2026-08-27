16b0a59e561ed6cdd5bc733e1da994ffecd6b9f0
Jakob Blomer <jblomer@cern.ch>
2026-08-28 00:18:14 +0200

    [ntuple] load streamer info for schema evolution
    If during schema evolution a conversion streamer info is required, we
    have to load the schema info records from the ROOT file. If the RNTuple
    is loaded through the RMiniFile, this requires an extra call.
    
    (cherry picked from commit 8888c5537289e1d39b945b8ae927e7aad8a357d8)

--- roottest/root/ntuple/evolution/NtplEvolv_v3_LinkDef.h
+++ roottest/root/ntuple/evolution/NtplEvolv_v3_LinkDef.h
@@ -2,6 +2,7 @@
 
 #pragma link C++ class NtplEvolv+;
 
-#pragma read sourceClass="NtplEvolv" version="[1-]" source="" targetClass="NtplEvolv" target="fA" code = "{ fA = 13; }"
+#pragma read sourceClass="NtplEvolv" version="[1-]" source="int fA;" targetClass="NtplEvolv" target="fA" \
+   code = "{ fA = onfile.fA + 13; }"
 
 #endif
