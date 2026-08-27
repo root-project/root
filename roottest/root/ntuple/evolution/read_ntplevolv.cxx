2f4ae07799216947e2c97bd365c6a3afec959d03
Jakob Blomer <jblomer@cern.ch>
2026-08-28 00:18:14 +0200

    [ntuple] load streamer info for schema evolution
    If during schema evolution a conversion streamer info is required, we
    have to load the schema info records from the ROOT file. If the RNTuple
    is loaded through the RMiniFile, this requires an extra call.
    
    (cherry picked from commit 8888c5537289e1d39b945b8ae927e7aad8a357d8)

--- roottest/root/ntuple/evolution/read_ntplevolv.cxx
+++ roottest/root/ntuple/evolution/read_ntplevolv.cxx
@@ -1,17 +1,40 @@
 #include <ROOT/RNTupleReader.hxx>
 
+#include <TObject.h>
+#include <TROOT.h>
+#include <TSeqCollection.h>
+
 #include "NtplEvolv_v3.hxx"
 
 #include <iostream>
+#include <string>
 
 int main()
 {
+   // At this point, we expect NtplEvolv _not_ being present in the global streamer infos
+   for (auto si : TRangeDynCast<TObject>(gROOT->GetListOfStreamerInfo())) {
+      if (std::string(si->GetName()) == "NtplEvolv")
+         return 2;
+   }
+
    auto reader = ROOT::RNTupleReader::Open("ntpl", "root_test_ntpl_evolution.root");
+
+   reader->GetModel();
+   // Now, reader should have loaded the streamer info for NtplEvolv
+   bool streamerInfoFound = false;
+   for (auto si : TRangeDynCast<TObject>(gROOT->GetListOfStreamerInfo())) {
+      if (std::string(si->GetName()) == "NtplEvolv") {
+         streamerInfoFound = true;
+         break;
+      }
+   }
+   if (!streamerInfoFound)
+      return 3;
 
    reader->LoadEntry(0);
 
    auto a = reader->GetModel().GetDefaultEntry().GetPtr<NtplEvolv>("event")->fA;
    std::cout << "Result of event.fA: " << a << std::endl;
 
-   return a != 13;
+   return a != 14;
 }
