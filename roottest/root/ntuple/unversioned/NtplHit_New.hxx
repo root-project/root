fae78b976d47224c7eb2f3673dae0641c519047f
Jakob Blomer <jblomer@cern.ch>
2026-08-30 23:34:29 +0200

    [ntuple] fix schema evolution of unversioned classes
    (cherry picked from commit c0423bd685dac967b67c2a649ba2642576ff7680)

---
+++ roottest/root/ntuple/unversioned/NtplHit_New.hxx
@@ -0,0 +1,11 @@
+#ifndef NTPL_HIT_NEW_H
+#define NTPL_HIT_NEW_H
+
+struct Hit {
+   int fA_r{0};
+   int fB_r{0};
+   double fX_r{0};
+   double fY_r{0};
+};
+
+#endif // NTPL_HIT_NEW_H
---
+++ roottest/root/ntuple/unversioned/NtplHit_Old.hxx
@@ -0,0 +1,11 @@
+#ifndef NTPL_HIT_OLD_H
+#define NTPL_HIT_OLD_H
+
+struct Hit {
+   int fA{0};
+   int fB{0};
+   double fX{0};
+   double fY{0};
+};
+
+#endif // NTPL_HIT_OLD_H
