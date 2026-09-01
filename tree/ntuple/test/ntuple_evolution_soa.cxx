135f31664b2531b5ca3a442094c3fa48419c14a9
Jakob Blomer <jblomer@cern.ch>
2026-09-01 10:40:42 +0200

    [ntuple] factor out evolution unit test helper

--- tree/ntuple/test/ntuple_evolution_soa.cxx
+++ tree/ntuple/test/ntuple_evolution_soa.cxx
@@ -12,24 +12,9 @@
 
 #include "gtest/gtest.h"
 #include "ntuple_fork.hxx"
+#include "ntuple_test_evolution.hxx"
 
 namespace {
-
-void EvaluateIntImpl(const char *expression, int *value)
-{
-   auto interpreterValue = gInterpreter->MakeInterpreterValue();
-   ASSERT_TRUE(gInterpreter->Evaluate(expression, *interpreterValue));
-   *value = interpreterValue->GetAsLong();
-}
-
-#define EXPECT_EVALUATE_EQ(expression, expected) \
-   do {                                          \
-      int _value;                                \
-      EvaluateIntImpl(expression, &_value);      \
-      if (::testing::Test::HasFatalFailure())    \
-         return;                                 \
-      EXPECT_EQ(expected, _value);               \
-   } while (0)
 
 void MakeSoALink(const std::string &recordName, const std::string &soaName)
 {
