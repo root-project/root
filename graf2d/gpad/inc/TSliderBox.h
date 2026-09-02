9cd12cef5d5a3ec124fa2f8ee296b768527c50d0
Sergey Linev <S.Linev@gsi.de>
2026-09-02 10:37:32 +0200

    Rewrite TSliderBox::ExecuteEvent
    Use functionality of TBox::ExecuteEvent instead reimplementing it again.
    
    Depending on vertical or horizontal orientation of TSlider, either only X or only Y axis can be modified by the ExecuteEvent
    
    Also when setting min/max value back to TSlider, respect border values of TSlider itself.

--- graf2d/gpad/inc/TSliderBox.h
+++ graf2d/gpad/inc/TSliderBox.h
@@ -2,7 +2,7 @@
 // Author: Rene Brun   23/11/96
 
 /*************************************************************************
- * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * Copyright (C) 1995-2026, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
  *                                                                       *
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
@@ -12,7 +12,6 @@
 #ifndef ROOT_TSliderBox
 #define ROOT_TSliderBox
 
-
 #include "TWbox.h"
 
 class TSlider;
@@ -20,22 +19,19 @@ class TSlider;
 class TSliderBox : public TWbox {
 
 protected:
-   TSlider   *fSlider;     ///< Pointer to slider
+   TSlider   *fSlider = nullptr;     ///< Pointer to slider
 
-   TSliderBox(const TSliderBox& sb)
-     : TWbox(sb), fSlider(sb.fSlider) { }
-   TSliderBox& operator=(const TSliderBox& sb)
-     {if(this!=&sb) {TWbox::operator=(sb); fSlider=sb.fSlider;}
-     return *this; }
+   TSliderBox(const TSliderBox&) = delete;
+   TSliderBox& operator=(const TSliderBox&) = delete;
 
 public:
    TSliderBox();
    TSliderBox(Double_t x1, Double_t y1,Double_t x2 ,Double_t y2,
-              Color_t color=18, Short_t bordersize=2 ,Short_t bordermode=-1);
+              Color_t color=18, Short_t bordersize=2, Short_t bordermode=-1);
    ~TSliderBox() override;
    void  ExecuteEvent(Int_t event, Int_t px, Int_t py) override;
    void  SavePrimitive(std::ostream &out, Option_t *option = "") override;
-   void  SetSlider(TSlider*slider) { fSlider=slider; }
+   void  SetSlider(TSlider*slider) { fSlider = slider; }
 
    ClassDefOverride(TSliderBox,1)  //The moving box of a TSlider
 };
