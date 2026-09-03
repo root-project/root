cb8f846c805f96d18bfdbb2882db8158e5adf200
Sergey Linev <S.Linev@gsi.de>
2026-09-03 10:46:19 +0200

    [imagedump] implement DrawImage method
    Just call Merge for target image

--- graf2d/postscript/inc/TImageDump.h
+++ graf2d/postscript/inc/TImageDump.h
@@ -50,6 +50,8 @@ public:
    void  CellArrayBegin(Int_t W, Int_t H, Double_t x1, Double_t x2, Double_t y1, Double_t y2) override;
    void  CellArrayFill(Int_t r, Int_t g, Int_t b) override;
    void  CellArrayEnd() override;
+   void  DrawImage(TImage *img, Int_t x, Int_t y , Int_t flags = 0) override;
+
    void  Close(Option_t *opt = "") override;
    void  DrawBox(Double_t x1, Double_t y1,Double_t x2, Double_t  y2) override;
    void  DrawFrame(Double_t xl, Double_t yl, Double_t xt, Double_t  yt,
