757cdd68993740a828019ed9bead548764464a5d
Sergey Linev <S.Linev@gsi.de>
2026-09-03 13:34:21 +0200

    [pdf] implement draw image
    Simplify logic how image placed in the PDF file
    Old API was not clear enough for that

--- graf2d/postscript/inc/TPDF.h
+++ graf2d/postscript/inc/TPDF.h
@@ -103,6 +103,9 @@ public:
    void CellArrayBegin(Int_t W, Int_t H, Double_t x1, Double_t x2, Double_t y1, Double_t y2) override;
    void CellArrayFill(Int_t r, Int_t g, Int_t b) override;
    void CellArrayEnd() override;
+
+   void  DrawImage(TImage *img, Int_t x, Int_t y, Int_t flags = 0) override;
+
    void Close(Option_t *opt = "") override;
    Double_t CMtoPDF(Double_t u) { return Int_t(0.5 + 72 * u / 2.54); }
    void ComputeRect(const char* chars, Double_t fontsize, Double_t a, Double_t b, Double_t c, Double_t d, Double_t e, Double_t f);
