5fdcb1b31457039977fae5be891fee031a4d14c5
Jakob Blomer <jblomer@cern.ch>
2026-09-04 08:39:07 +0200

    [ntuple] always load streamer info on file open
    Populate the streamer infos of the file on LoadStructure(). This will
    make sure it is loaded together with the header and footer. We don't
    load the streamer info directly on file open in order to prevent
    computational work (decompressing the record) taking place when we open
    the file asynchronously.

--- tree/ntuple/src/RMiniFile.cxx
+++ tree/ntuple/src/RMiniFile.cxx
@@ -719,16 +719,15 @@ std::uint64_t ROOT::Internal::RMiniFileReader::SearchInDirectory(std::uint64_t &
 
 void ROOT::Internal::RMiniFileReader::LoadStreamerInfo()
 {
-   RTFHeader fileHeader;
-   ReadBuffer(&fileHeader, sizeof(fileHeader), 0);
-
-   const std::uint64_t seekKeyInfo = fileHeader.GetSeekInfo();
+   if (fIsBare)
+      return;
 
    RTFKey key;
-   ReadBuffer(&key, sizeof(key), seekKeyInfo);
+   ReadBuffer(&key, sizeof(key), fSeekKeyInfo);
 
-   const std::uint64_t nbytesInfo = fileHeader.GetNbytesInfo() - key.fKeyLen;
-   const std::uint64_t seekInfo = seekKeyInfo + key.fKeyLen;
+   R__ASSERT(fNbytesKeyAndInfo >= key.fKeyLen);
+   const std::uint64_t nbytesInfo = fNbytesKeyAndInfo - key.fKeyLen;
+   const std::uint64_t seekInfo = fSeekKeyInfo + key.fKeyLen;
    const std::uint32_t uncompLenInfo = key.fObjLen;
    auto streamerInfo = MakeUninitArray<char>(uncompLenInfo);
    if (nbytesInfo == uncompLenInfo) {
@@ -765,6 +764,9 @@ ROOT::RResult<ROOT::RNTuple> ROOT::Internal::RMiniFileReader::GetNTupleProper(st
 {
    RTFHeader fileHeader;
    ReadBuffer(&fileHeader, sizeof(fileHeader), 0);
+
+   fSeekKeyInfo = fileHeader.GetSeekInfo();
+   fNbytesKeyAndInfo = fileHeader.GetNbytesInfo();
 
    RTFKey key;
    RTFString name;
@@ -813,6 +815,7 @@ ROOT::RResult<ROOT::RNTuple> ROOT::Internal::RMiniFileReader::GetNTupleProper(st
 
    const auto objNbytes = key.GetSize() - key.fKeyLen;
    auto res = GetNTupleProperAtOffset(offset, objNbytes, key.fObjLen);
+
    return res;
 }
 
