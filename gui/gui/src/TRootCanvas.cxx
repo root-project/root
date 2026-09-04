4b97235197eb666ce494164dd1638e94483cf317
Sergey Linev <S.Linev@gsi.de>
2026-09-04 10:18:14 +0200

    Implement TRootCanvas::SetCursor
    Just redirect to gVirtualX

--- gui/gui/src/TRootCanvas.cxx
+++ gui/gui/src/TRootCanvas.cxx
@@ -1358,6 +1358,15 @@ void TRootCanvas::SetCanvasSize(UInt_t w, UInt_t h)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Set cursor for the canvas
+
+void TRootCanvas::SetCursor(ECursor cursor)
+{
+   if (fCanvasID != -1)
+      gVirtualX->SetCursor(fCanvasID, cursor);
+}
+
+////////////////////////////////////////////////////////////////////////////////
 /// Set canvas position (units in pixels).
 
 void TRootCanvas::SetWindowPosition(Int_t x, Int_t y)
