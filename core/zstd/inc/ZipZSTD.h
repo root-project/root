ecd87f2264d0f946817869038f0d231fd79be473
ferdymercury <ferdymercury@users.noreply.github.com>
2026-09-06 00:02:40 +0200

    [core] deprecate implementation detail zip headers since there is RZip public interface

--- core/zstd/inc/ZipZSTD.h
+++ core/zstd/inc/ZipZSTD.h
@@ -10,6 +10,8 @@
 #ifndef ROOT_ZipZSTD
 #define ROOT_ZipZSTD
 
+#warning "This header is deprecated and will be removed in 6.46, use instead methods within RZip.h"
+
 // NOTE: the ROOT compression libraries aren't consistently written in C++; hence the
 // #ifdef's to avoid problems with C code.
 #ifdef __cplusplus
