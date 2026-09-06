84c56e58b3881faf3f5f38f328ba0bbb25164ee6
ferdymercury <ferdymercury@users.noreply.github.com>
2026-09-06 11:10:48 +0200

    [zip] do not use deprecated header

--- builtins/zip/ZInflate.c
+++ builtins/zip/ZInflate.c
@@ -19,8 +19,63 @@ static const int qflag = 0;
 
 #include "zlib.h"
 #include "RConfigure.h"
-#include "ZipLZMA.h"
-#include "ZipLZ4.h"
+
+// @(#)root/lzma:$Id$
+// Author: David Dagenhart   May 2011
+
+/*************************************************************************
+ * Copyright (C) 1995-2011, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_ZipLZMA
+#define ROOT_ZipLZMA
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void R__zipLZMA(int cxlevel, int *srcsize, const char *src, int *tgtsize, char *tgt, int *irep);
+
+void R__unzipLZMA(int *srcsize, const unsigned char *src, int *tgtsize, unsigned char *tgt, int *irep);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+// Author: Brian Bockelman March 2015
+
+/*************************************************************************
+ * Copyright (C) 1995-2017, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_ZipLZ4
+#define ROOT_ZipLZ4
+
+// NOTE: the ROOT compression libraries aren't consistently written in C++; hence the
+// #ifdef's to avoid problems with C code.
+#ifdef __cplusplus
+extern "C" {
+#endif
+void R__zipLZ4(int cxlevel, int *srcsize, const char *src, int *tgtsize, char *tgt, int *irep);
+void R__unzipLZ4(int *srcsize, const unsigned char *src, int *tgtsize, unsigned char *tgt, int *irep);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+
 
 /* inflate.c -- put in the public domain by Mark Adler
    version c14o, 23 August 1994 */
