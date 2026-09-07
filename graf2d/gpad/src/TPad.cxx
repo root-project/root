3ee9ad1d7d26c90f2311475aa8fd105547d9e257
Sergey Linev <S.Linev@gsi.de>
2026-09-07 11:01:31 +0200

    [gpad] Introduce TPad::FeedbackMode()
    Feedback mode used to implement specialized interactive painters
    like projections display. On some platforms (like web or GL) such
    plain invert painting not implemented.
    
    But future idea to collect some of custom operations
    in internal buffers and paint them in supported mode
    after main objects painting.

--- graf2d/gpad/src/TPad.cxx
+++ graf2d/gpad/src/TPad.cxx
@@ -2686,6 +2686,16 @@ void TPad::ExecuteEventAxis(Int_t event, Int_t px, Int_t py, TAxis *axis)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Switch feedback mode for the canvas
+/// Only for internal use when implementing special painters
+
+void TPad::FeedbackMode(Bool_t set)
+{
+   if (fCanvas)
+      fCanvas->FeedbackMode(set);
+}
+
+////////////////////////////////////////////////////////////////////////////////
 /// Search if object named name is inside this pad or in pads inside this pad.
 ///
 /// In case name is in several sub-pads the first one is returned.
