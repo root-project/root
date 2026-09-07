7f5cc17f9bc091a275f66aca6602ec315a174707
ferdymercury <ferdymercury@users.noreply.github.com>
2026-09-07 13:20:49 +0200

    [nfc] more wsp

--- roofit/roofitcore/src/RooProfileLL.cxx
+++ roofit/roofitcore/src/RooProfileLL.cxx
@@ -30,22 +30,22 @@ as a MIGRAD minimization step is executed for each function evaluation
 
 #include <ostream>
 
- ////////////////////////////////////////////////////////////////////////////////
- /// Constructor of profile likelihood given input likelihood nll w.r.t
- /// the given set of variables. The input log likelihood is minimized w.r.t
- /// to all other variables of the likelihood at each evaluation and the
- /// value of the global log likelihood minimum is always subtracted.
+////////////////////////////////////////////////////////////////////////////////
+/// Constructor of profile likelihood given input likelihood nll w.r.t
+/// the given set of variables. The input log likelihood is minimized w.r.t
+/// to all other variables of the likelihood at each evaluation and the
+/// value of the global log likelihood minimum is always subtracted.
 
- RooProfileLL::RooProfileLL(const char *name, const char *title, RooAbsReal &nllIn, const RooArgSet &observables)
-    : RooAbsReal(name, title),
-      _nll("input", "-log(L) function", this, nllIn),
-      _obs("paramOfInterest", "Parameters of interest", this),
-      _par("nuisanceParam", "Nuisance parameters", this, false, false)
- {
-    // Determine actual parameters and observables
-    nllIn.getObservables(&observables, _obs);
-    nllIn.getParameters(&observables, _par);
- }
+RooProfileLL::RooProfileLL(const char *name, const char *title, RooAbsReal &nllIn, const RooArgSet &observables)
+   : RooAbsReal(name, title),
+     _nll("input", "-log(L) function", this, nllIn),
+     _obs("paramOfInterest", "Parameters of interest", this),
+     _par("nuisanceParam", "Nuisance parameters", this, false, false)
+{
+   // Determine actual parameters and observables
+   nllIn.getObservables(&observables, _obs);
+   nllIn.getParameters(&observables, _par);
+}
 
 
 
