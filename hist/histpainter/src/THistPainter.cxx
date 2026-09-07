89b4a344774dfd3d2b8123661c34d7b9adc03db5
Sergey Linev <S.Linev@gsi.de>
2026-09-07 16:26:33 +0200

    [histpainter] do not show TH3 projection with GL
    In such mode drawing must be done differently and completely fails when GL painter is activated

--- hist/histpainter/src/THistPainter.cxx
+++ hist/histpainter/src/THistPainter.cxx
@@ -10947,6 +10947,11 @@ void THistPainter::SetShowProjection(const char *option,Int_t nbins)
 
    if (nbins <= 0) return;
 
+   if ((fH->GetDimension() == 3) && (gPad->GetGLDevice() != -1)) {
+      Error("SetShowProjection", "TH3 projections do not work in GL mode");
+      return;
+   }
+
    TString opt = option;
    opt.ToLower();
    Int_t projection = 0;
@@ -10985,6 +10990,10 @@ void THistPainter::SetShowProjectionXY(const char *option,Int_t nbinsY,Int_t nbi
 
    if ((nbinsX <= 0) || (nbinsY <= 0)) return;
 
+   if ((fH->GetDimension() == 3) && (gPad->GetGLDevice() != -1)) {
+      Error("SetShowProjection", "TH3 projections do not work in GL mode");
+      return;
+   }
 
    TString opt = option;
    opt.ToLower();
@@ -11210,6 +11219,10 @@ void THistPainter::ShowProjection3(Int_t px, Int_t py)
          return;
       }
    }
+
+   // 3D protection does not work with GL painters
+   if (gPad->GetGLDevice() != -1)
+      return;
 
    // turn off double buffer mode and draw primitives in invert mode
    gPad->FeedbackMode(kTRUE);
