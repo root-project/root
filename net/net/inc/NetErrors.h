2dc01fbe53f0f74f7e9047aa16be3ea6f6cdee4c
ferdymercury <ferdymercury@users.noreply.github.com>
2026-09-16 18:36:58 +0200

    [nfc] reword deprecation message
    Co-authored-by: Philippe Canal <pcanal@fnal.gov>

--- net/net/inc/NetErrors.h
+++ net/net/inc/NetErrors.h
@@ -14,7 +14,7 @@
 
 
 #if !defined(ROOT_NetErrors_cxx) && !defined(G__DICTIONARY)
-#warning "This header is an implementation detail, is deprecated and will be removed from the public interface in ROOT 6.44. Use instead TSocket public interface."
+#warning "This header is deprecated and will be removed from the public interface in ROOT 6.44. Use instead TSocket public interface."
 #endif
 
 //////////////////////////////////////////////////////////////////////////
