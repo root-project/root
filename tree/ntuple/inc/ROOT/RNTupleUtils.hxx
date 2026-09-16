244408e21cb25ef15979231448f68f59ea6b35bd
Jakob Blomer <jblomer@cern.ch>
2026-09-16 15:40:12 +0200

    [ntuple] add RStringPool for string interning
    An internal utility class that maintains a sorted list of immutable
    strings.

--- tree/ntuple/inc/ROOT/RNTupleUtils.hxx
+++ tree/ntuple/inc/ROOT/RNTupleUtils.hxx
@@ -17,7 +17,9 @@
 
 #include <cstddef>
 #include <memory>
+#include <string>
 #include <string_view>
+#include <vector>
 
 namespace ROOT {
 
@@ -47,6 +49,29 @@ std::unique_ptr<T[]> MakeUninitArray(std::size_t size)
 
 /// Check whether a given string is a valid name according to the RNTuple specification
 RResult<void> EnsureValidNameForRNTuple(std::string_view name, std::string_view where);
+
+/// Used for string interning of repeated type names, field names, etc. The call operator returns for every
+/// given string the one immutable pointer to that string in the pool. After calling Freeze(), no new string
+/// must be added and the pool is safe to use from multiple threads.
+class RStringPool {
+   std::vector<std::unique_ptr<std::string>> fStrings; // String pointers are always valid and sorted by their pointee
+   bool fFrozen = false;
+
+   static bool Less(const std::unique_ptr<std::string> &a, const std::string_view &b) { return *a < b; }
+
+public:
+   RStringPool() = default;
+   ~RStringPool() = default;
+   RStringPool(const RStringPool &other) = delete;
+   RStringPool(RStringPool &&other) = default;
+   RStringPool &operator=(RStringPool &&other) = default;
+   RStringPool &operator=(const RStringPool &other) = delete;
+
+   /// Searches for str in the pool. If not present, adds the string to the pool before returning a pointer to it.
+   /// Throws if frozen and str is not yet in the pool.
+   const std::string *Intern(std::string_view str);
+   void Freeze() { fFrozen = true; }
+};
 
 } // namespace Internal
 } // namespace ROOT
