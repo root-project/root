09027866278a7f2cc428d46f689443aa275bae0e
ferdymercury <ferdymercury@users.noreply.github.com>
2026-09-17 16:55:25 +0200

    [hist] add deprecation notice and fallback headers

---
+++ hist/hist/inc/v5/TFormula.h
@@ -0,0 +1,10 @@
+#ifndef ROOT_v5_TFormula_fwd
+#define ROOT_v5_TFormula_fwd
+
+#include "ROOT/v5/TFormula.h"
+
+#ifndef G__DICTIONARY
+#warning "v5 legacy header has been moved to ROOT subfolder. This header is deprecated and will be removed in ROOT 6.44. Prepend `ROOT/` to your include statement."
+#endif
+
+#endif
